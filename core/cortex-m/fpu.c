2a009cd03d7eb08d4d4e8d43c7b4dd3561c5a0ab
Vic Yang <victoryang@chromium.org>
2012-02-11 23:16:54 +0800

    Change TMP006 temperature calculation to use FP.
    The temperature calculation currently uses fixed point operations.
    Change it to use floating point for better readability and maintenance.
    
    BUG=chrome-os-partner:7801
    TEST=In console, tempremote "tempremote 29715 -105000 6390" gives 28506.
    
    Change-Id: Ib766904b8feb9a78eac9f7cd53afeca85091c5a5
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- core/cortex-m/fpu.c
+++ core/cortex-m/fpu.c
@@ -10,17 +10,17 @@
 void enable_fpu(void)
 {
 	interrupt_disable();
-	asm("mrs r0, control\n"
-	    "orr r0, r0, #(1 << 2)\n"
-	    "msr control, r0\n"
-	    "isb");
+	asm volatile("mrs r0, control;"
+		     "orr r0, r0, #(1 << 2);"
+		     "msr control, r0;"
+		     "isb;");
 }
 
 void disable_fpu(void)
 {
-	asm("mrs r0, control\n"
-	    "bic r0, r0, #(1 << 2)\n"
-	    "msr control, r0\n"
-	    "isb");
+	asm volatile("mrs r0, control;"
+		     "bic r0, r0, #(1 << 2);"
+		     "msr control, r0;"
+		     "isb;");
 	interrupt_enable();
 }
