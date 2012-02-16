dd3fc87570cddf382ac16c0b27d07b225398e195
Vic Yang <victoryang@chromium.org>
2012-02-16 13:07:32 -0800

    Temporarily remove image RW.B due to code size.
    To accomodate our growing code, image RW.B is removed so that RO and
    RW.A can both have 60K space. This is just a temporary solution. Add
    RW.B back when we have enough space for all three images.
    
    Also add license header to firmware_image.lds.S so that repo does not
    complain about it.
    
    BUG=chrome-os-partner:8079
    TEST=build image and run on proto-0.
    
    Change-Id: I85b723f3e645c12fd89b189263ca44d58c4621eb
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- chip/lm4/config.h
+++ chip/lm4/config.h
@@ -11,7 +11,8 @@
 #define CONFIG_RAM_SIZE         0x00008000
 
 /* Size of one firmware image in flash */
-#define CONFIG_FW_IMAGE_SIZE    (40 * 1024)
+/* TODO: Change this to appropriate value when we get image B back. */
+#define CONFIG_FW_IMAGE_SIZE    (60 * 1024)
 #define CONFIG_FW_RO_OFF        0
 #define CONFIG_FW_A_OFF         CONFIG_FW_IMAGE_SIZE
 #define CONFIG_FW_B_OFF         (2 * CONFIG_FW_IMAGE_SIZE)
