34d17104874f8a72345ad205baa42dd86c112be9
Rong Chang <rongchang@chromium.org>
2012-03-01 18:17:12 +0800

    Add battery charge state machine and task
    This CL adds a charge state machine for SMB compliant battery pack.
    Vendor specific charge constraints can be applied through function
    call, defined in battery_pack.h .
    
    BUG=chrome-os-partner:7526
    TEST=Attach EC serial console
      Unplug AC adapter: state ==> "discharge"
      Plug AC adapter:   state ==> "charge"
      Battery full:      state ==> "idle"
      Unplug battery:    state ==> "error"
    
    Change-Id: Iabff0988a6067d37c17c11b060bbb7e66505c118

--- common/build.mk
+++ common/build.mk
@@ -23,4 +23,6 @@ common-$(CONFIG_LIGHTBAR)+=leds.o
 
 # Board driver modules
 common-$(CONFIG_CHARGER_BQ24725)+=charger_bq24725.o
+common-$(CONFIG_BATTERY_ATL706486)+=battery_atl706486.o
 common-$(CONFIG_SMART_BATTERY)+=smart_battery.o
+common-$(CONFIG_SMART_BATTERY)+=charge_state.o
