9947d20d2acf5a2f6d8f4efc83c0f9ba453c77fc
Rong Chang <rongchang@chromium.org>
2012-03-05 22:40:01 +0800

    Add battery lpc commands
    This CL adds LPC commands to provide the following information:
    - Design Capacity (dword)
    - Last Full Charge Capacity (dword)
    - Design Voltage (dword)
    - Design Capacity of Warning (dword)
    - Design Capacity of Low (dword)
    - Battery Type (ascii)
    - Model Number (ascii)
    - Serial Number (ascii)
    - OEM (usually Vendor) (ascii)
    - Battery charge cycle count (dword)
    
    BUG=chrome-os-partner:8181
    TEST=none
    
    Change-Id: Iabaf7d9862e15c5b21cf5170cf43450e472b7836

--- include/lpc_commands.h
+++ include/lpc_commands.h
@@ -338,4 +338,41 @@ struct lpc_response_thermal_get_threshold {
 /* Toggling automatic fan control */
 #define EC_LPC_COMMAND_THERMAL_AUTO_FAN_CTRL 0x52
 
+/*****************************************************************************/
+/* Battery commands */
+
+/* Maximum asciiz length of battery text data  */
+#define EC_LPC_BATT_SIZE_MAX 32
+
+/* Get battery info */
+#define EC_LPC_COMMAND_BATTERY_INFO 0x60
+struct lpc_response_battery_info {
+	uint32_t designed_capacity;
+	uint32_t last_full_charge_capacity;
+	uint32_t designed_output_voltage;
+	uint32_t designed_capacity_warning;
+	uint32_t designed_capacity_low;
+} __attribute__ ((packed));
+
+/* Get battery chemistry */
+#define EC_LPC_COMMAND_BATTERY_TYPE 0x61
+struct lpc_response_battery_text {
+	char text[EC_LPC_BATT_SIZE_MAX];
+} __attribute__ ((packed));
+
+/* Get battery model number */
+#define EC_LPC_COMMAND_BATTERY_MODEL_NUMBER 0x62
+
+/* Get battery serial number */
+#define EC_LPC_COMMAND_BATTERY_SERIAL_NUMBER 0x63
+
+/* Get battery OEM name */
+#define EC_LPC_COMMAND_BATTERY_OEM 0x64
+
+/* Get battery charge cycle count */
+#define EC_LPC_COMMAND_BATTERY_CYCLE_COUNT 0x65
+struct lpc_response_battery_cycle_count {
+	uint32_t cycle;
+} __attribute__ ((packed));
+
 #endif  /* __CROS_EC_LPC_COMMANDS_H */
