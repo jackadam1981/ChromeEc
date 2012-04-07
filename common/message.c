c016a062d08f8fee01c8b2d6b04b6df3e3485016
Simon Glass <sjg@chromium.org>
2012-04-07 09:13:42 -0700

    Add a message format and processing function
    Whatever means is used to talk to the AP there is no justifcation for
    putting message processing directly in drivers. Create a suitable
    header file to define the interface, and provide a processing function
    which can provide responses to incoming messages.
    
    BUG=chromium-os:28925
    TEST=build on daisy and discovery; run on daisy
    
    Change-Id: If09ea3e30d42d8c5f226dc4421d4895adc54f937
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ common/message.c
@@ -0,0 +1,79 @@
+/*
+ * Command handling for Chrome EC.
+ *
+ * This handles incoming commands and provides responses.
+ *
+ * Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "board.h"
+#include "message.h"
+#include "util.h"
+
+/* Our ID message */
+static const char proto_id[] = "Google Chrome MKBP v1";
+
+
+static int message_get_response(int cmd, uint8_t **buffp, int max_len)
+{
+	/*
+	 * Invalid commands are ignored, just returning a stream of 0xff
+	 * bytes.
+	 */
+	switch (cmd & MSG_CMD_MASK) {
+	case CMDC_NOP:
+		return 0;
+	case CMDC_ID:
+		*buffp = (char *)proto_id;
+		return sizeof(proto_id) - 1;
+	default:
+		return -1;
+	}
+
+	return 0;
+}
+
+int message_process_cmd(int cmd, uint8_t *out_msg, int max_len)
+{
+	uint8_t *msg;
+	int msg_len;
+	int need_copy;
+	int sum = 0;
+	int len;
+	int i;
+
+	msg = out_msg + MSG_HEADER_BYTES;
+	msg_len = message_get_response(cmd, &msg, max_len - MSG_PROTO_BYTES);
+	if (msg_len < 0)
+		return msg_len;
+
+	/*
+	 * We add MSG_PROTO_BYTES bytes of overhead: truncate the reply
+	 * if needed.
+	 */
+	if (msg_len + MSG_PROTO_BYTES > max_len)
+		msg_len = max_len - MSG_PROTO_BYTES;
+	len = msg_len + 4;
+	ASSERT(msg_len >= 0);
+	need_copy = msg != out_msg + MSG_HEADER_BYTES;
+	ASSERT(!need_copy ||
+		msg + msg_len < out_msg ||
+		msg > out_msg + sizeof(out_msg));
+
+	out_msg[0] = MSG_HEADER;
+	out_msg[1] = len;
+	out_msg[2] = len >> 8;
+	sum += MSG_HEADER + len + (len >> 8);
+
+	for (i = 0; i < msg_len; i++) {
+		if (need_copy)
+			out_msg[i + 3] = *msg;
+		sum += *msg;
+	}
+	out_msg[i + 3] = sum;
+	out_msg[i + 4] = MSG_PREAMBLE;
+
+	return msg_len + MSG_PROTO_BYTES;
+}
