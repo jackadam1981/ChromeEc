fda80326376e85780d12978de06471c2f0bd1e09
David Hendricks <dhendrix@chromium.org>
2012-04-13 12:51:02 -0700

    stm32: add stm32f register definitions
    This CL adds STM32F register definitions alongside STM32L definitions.
    
    This also adds a header in the board's subdirectory, ec.h, which
    provides the exact part number to the chip/stm32l/config.h header
    so that proper memory size, flash size, etc. will be used.
    
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    
    BUG=none
    TEST=compile tested for Daisy
    
    Change-Id: Ieb97b6d39d2e5197393edafdbaa5766750a5205c

---
+++ board/daisy/ec.h
@@ -0,0 +1,11 @@
+/* Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __EC_H
+#define __EC_H
+
+#define EC_STM32L151R8
+
+#endif /* __EC_H */
