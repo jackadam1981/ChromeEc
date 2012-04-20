c225c78ce07ae7de61ba07b78dc325ffc3f5aa43
David Hendricks <dhendrix@chromium.org>
2012-04-20 13:53:19 -0700

    daisy: move i2c port initialization back into configure_board
    This patch moves all I2C port initialization into configure_board
    to ensure the alternate function gets set properly.
    
    I2C ports should come up in their high-impedance state (Output,
    open-drain, output set) and then get set as alternate function.
    However, configure_board() runs before gpio_pre_init(), so the
    mode register was getting set set back to general purpose output
    instead of alt. function.
    
    TODO: Fix gpio_pre_init() so we do not need to explicitly handle port
    configuration in configure_board().
    
    BUG=none
    TEST=tested on daisy
    
    Change-Id: If837acd4f4204e467e7ed276f048b5b70ecbdb25
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- board/daisy/board.c
+++ board/daisy/board.c
@@ -73,10 +73,6 @@ const struct gpio_info gpio_list[GPIO_COUNT] = {
 	{"KB_OUT10",    GPIO_C, (1<<5),  GPIO_KB_OUTPUT, NULL},
 	{"KB_OUT11",    GPIO_C, (1<<6),  GPIO_KB_OUTPUT, NULL},
 	{"KB_OUT12",    GPIO_C, (1<<7),  GPIO_KB_OUTPUT, NULL},
-
-	/* bi-directional, initialized as hi-Z until alt. function is set */
-	{"I2C_SCL",     GPIO_B, (1<<10), GPIO_HI_Z, NULL},
-	{"I2C_SDA",     GPIO_B, (1<<11), GPIO_HI_Z, NULL},
 };
 
 void configure_board(void)
@@ -109,6 +105,17 @@ void configure_board(void)
 					(1<<5) |
 					(1<<4), GPIO_ALT_SPI);
 	STM32L_GPIO_OSPEEDR_OFF(GPIO_A) |= 0xff00;
+
+	/*
+	 * I2C SCL/SDA on PB10-11, bi-directional, initialized as hi-Z until
+	 * alt. function is set
+	 */
+	STM32L_GPIO_PUPDR_OFF(GPIO_B) &= ~((3 << (11 * 2)) | (3 << (10 * 2)));
+	STM32L_GPIO_MODER_OFF(GPIO_B) &= ~((3 << (11 * 2)) | (3 << (10 * 2)));
+	STM32L_GPIO_MODER_OFF(GPIO_B) |= (1 << (11 * 2)) | (1 << (10 * 2));
+	STM32L_GPIO_OTYPER_OFF(GPIO_B) |= (1<<11) | (1<<10);
+	STM32L_GPIO_BSRR_OFF(GPIO_B) |= (1<<11) | (1<<10);
+	gpio_set_alternate_function(GPIO_B, (1<<11) | (1<<10), GPIO_ALT_I2C);
 
 	/* Select Alternate function for USART1 on pins PA9/PA10 */
 	gpio_set_alternate_function(GPIO_A, (1<<9) | (1<<10), GPIO_ALT_USART);
