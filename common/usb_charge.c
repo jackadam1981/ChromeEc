e975c51db838acf92a7e797ed918ab82090bc899
Vic Yang <victoryang@google.com>
2012-04-24 10:08:14 +0800

    Automatically switch USB charging mode
    Disable USB ports when system is down and set USB ports to standard
    downstream mode when system is up or sleeping.
    
    Signed-off-by: Vic Yang <victoryang@google.com>
    
    BUG=chrome-os-partner:9249
    TEST=Plug in a phone and see it charge when system is on.
    Turn off the system and see it stop charging.
    
    Change-Id: I02850dee7051ed6589e0f176a933069203f0efdf

--- common/usb_charge.c
+++ common/usb_charge.c
@@ -6,6 +6,7 @@
 /* USB charging control module for Chrome EC */
 
 #include "board.h"
+#include "chipset.h"
 #include "console.h"
 #include "gpio.h"
 #include "hooks.h"
@@ -43,6 +44,22 @@ static void usb_charge_set_ilim(int port_id, int sel)
 		gpio_set_level(GPIO_USB1_ILIM_SEL, sel);
 	else
 		gpio_set_level(GPIO_USB2_ILIM_SEL, sel);
+}
+
+
+int usb_charge_update_mode(void)
+{
+	if (chipset_in_state(CHIPSET_STATE_SOFT_OFF)) {
+		usb_charge_set_mode(0, USB_CHARGE_MODE_DISABLED);
+		usb_charge_set_mode(1, USB_CHARGE_MODE_DISABLED);
+	}
+	else if (chipset_in_state(CHIPSET_STATE_SUSPEND) ||
+		 chipset_in_state(CHIPSET_STATE_ON)) {
+		usb_charge_set_mode(0, USB_CHARGE_MODE_DOWNSTREAM_500MA);
+		usb_charge_set_mode(1, USB_CHARGE_MODE_DOWNSTREAM_500MA);
+	}
+
+	return EC_SUCCESS;
 }
 
 
--- common/x86_power.c
+++ common/x86_power.c
@@ -17,6 +17,7 @@
 #include "task.h"
 #include "timer.h"
 #include "uart.h"
+#include "usb_charge.h"
 #include "util.h"
 #include "x86_power.h"
 
@@ -323,6 +324,9 @@ void x86_power_task(void)
 			/* Wait 5ms for SUSCLK to stabilize */
 			usleep(5000);
 
+			/* Turn off USB ports. */
+			usb_charge_update_mode();
+
 			state = X86_S5;
 			break;
 
@@ -335,6 +339,9 @@ void x86_power_task(void)
 			 * reset, so they can wake the system from suspend. */
 			gpio_set_level(GPIO_ENABLE_TOUCHPAD, 1);
 			gpio_set_level(GPIO_TOUCHSCREEN_RESETn, 1);
+
+			/* Turn on USB ports as we go into S3 or S0. */
+			usb_charge_update_mode();
 
 			state = X86_S3;
 			break;
@@ -408,6 +415,9 @@ void x86_power_task(void)
 
 			/* Turn off power to RAM */
 			gpio_set_level(GPIO_ENABLE_1_5V_DDR, 0);
+
+			/* Turn off USB ports. */
+			usb_charge_update_mode();
 
 			state = X86_S5;
 			break;
