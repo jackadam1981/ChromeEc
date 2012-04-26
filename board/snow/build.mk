c43ebd4c119fb6af3756d34771feed00528ecc56
David Hendricks <dhendrix@chromium.org>
2012-04-25 21:10:25 -0700

    Initial Snow board port
    Mostly stolen from Daisy.
    
    Notable differences:
    - No SYSCFGEN required?
    - No GPIO_H --> CODEC_INT and ENTERING_RW changed?
    
    Change-Id: I955e2ff180d064294d67b630ae2ee6cfcfe52ab9

---
+++ board/snow/build.mk
@@ -0,0 +1,10 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+
+# the IC is STmicro STM32F100R8
+CHIP:=stm32f
+
+board-y=board.o
