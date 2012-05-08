c43062fb56c172605f7604e57be1312dbef8179a
Louis Yung-Chieh Lo <yjlou@chromium.org>
2012-05-09 01:05:15 +0800

    Fix some behaviors of keyboard command handlers.
    The phenomenon is that there is a char on-hold in port 0x60 and the kernel
    never picks it up. Hence the keyboard cannnot be recognized after resume.
    It comes from multiple reasons:
    
    1. The command I8042_CMD_RESET_BAT(0xff) and I8042_CMD_ENABLE(0xf4) didn't
       clean the buffer.
    
    2. clean_underlying_buffer() has clean the queue, but forgot to clean the
       TOH (TO Host). Add keyboard_clean_buffer() to clean the TOH (To Host).
    
    3. When KB interrupt is just enabled, the IRQ didn't sent if there is
       a char queued in buffer already. keyboard_resume_interrupt() solves
       this.
    
    4. Not all keyboard reset should reset the buffer. Only the enable/disble
       of controller RAM  should NOT reset buffer. Other enable/disable
       should clean the buffer.
    
    5. i8042 commands (those commands to port 0x64) should NOT return ACK
       even the parameter byte(s) goes to port 0x60.
    
    6. Keyboard was disabled by kernel, but key stroke still sent to host
       (this needs the BIOS to fix).
    
    Also fix the minor issues:
    
    1. I8042_CMD_RESEND should not return I8042_RET_ACK.
    
    2. I8042_DIS_KB/I8042_ENA_KB should effect the controller RAM content.
    
    3. only send out the scan code when keyboard is enabled.
    
    4. add kblog command for future debug (disabled by default because it
    neeeds 1KB of memory).
    
    Signed-off-by: Louis Yung-Chieh Lo <yjlou@chromium.org>
    
    BUG=chrome-os-partner:9525
    TEST=tested on link. Start from S0.
    1. Run powerd_suspend.
    2. Expect system is in S3.
    3. Press any key to wake up system.
    4. Expect system is up and keyboard is working.
    5. repeat for 20+ times.
    
    Change-Id: I1c48822687d7c1f7ef0e8d8bca54bf9b05fd785f

--- include/keyboard.h
+++ include/keyboard.h
@@ -16,6 +16,10 @@
 
 #define MAX_SCAN_CODE_LEN 4
 
+/* Log the traffic between EC and host. For debug. This would increase about
+ * 1KB of RAM footprint. */
+#undef KB_LOG
+
 enum scancode_set_list {
   SCANCODE_GET_SET = 0,
   SCANCODE_SET_1,
@@ -49,6 +53,13 @@ int handle_keyboard_command(uint8_t command, uint8_t *output);
 /* Send make/break code of power button to host.
  */
 void keyboard_set_power_button(int pressed);
+
+
+#ifdef KB_LOG
+void kblog_put(char type, uint8_t byte);
+#else
+#define kblog_put(type, byte) do {} while (0)
+#endif
 
 
 /* Register the board-specific keyboard matrix translation function.
@@ -91,10 +102,17 @@ enum ec_error_list keyboard_register_callback(keyboard_callback cb);
  */
 enum ec_error_list keyboard_get_state(uint8_t *bit_array);
 
-/* Return true if the TOH is still set */
+/* Returns true if the to-host-buffer is non-empty. */
 int keyboard_has_char(void);
 
+/* Sends a char to host and triggers IRQ if specified. */
 void keyboard_put_char(uint8_t chr, int send_irq);
 
+/* Cleans the keyboard buffer to host. */
+void keyboard_clean_buffer(void);
+
+/* Host just resumes the interrupt. Sends an interrupt if buffer is non-empty.
+ */
+void keyboard_resume_interrupt(void);
 
 #endif  /* __INCLUDE_KEYBOARD_H */
