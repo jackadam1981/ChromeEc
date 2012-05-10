94726405ddb4e5f4c0f1bb98c0bed208b47931c6
Vincent Palatin <vpalatin@chromium.org>
2012-05-10 22:47:11 +0000

    Slightly update the host commands API
    Preparatory work to use common host command code between ARM and x86.
    
    Every command sends back explicitly the size of the response payload.
    The size of the response defaults to 0 ond can be updated.
    
    Add a protocol version number returned as command 0x00 to help with
    backward compatibility.
    
    move a couple of function from lpc specific header to host commands to
    be able to implement them for the I2C link.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BUG=chrome-os-partner:9614
    TEST=make BOARD=link
    
    Change-Id: Id456dcd49f89dfdb89dd74fbd3a32c2c0f5db14d

--- common/lightbar.c
+++ common/lightbar.c
@@ -718,7 +718,7 @@ static void do_cmd_rgb(uint8_t led,
 /* Host commands via LPC bus */
 /****************************************************************************/
 
-static enum lpc_status lpc_cmd_lightbar(uint8_t *data)
+static int lpc_cmd_lightbar(uint8_t *data, int *resp_size)
 {
 	struct lpc_params_lightbar_cmd *ptr =
 		(struct lpc_params_lightbar_cmd *)data;
