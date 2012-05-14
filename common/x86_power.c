dc7b03b8c10ba212797f1112e399df10cd63a6f3
Randall Spangler <rspangler@chromium.org>
2012-05-14 13:55:51 -0700

    Change polarity of PROCHOT signal to match EVT
    This would throttle proto1 systems, if it weren't for a HW bug which
    means we don't have prochot control over proto1 systems at all.
    
    Signed-off-by: Randall Spangler <rspangler@chromium.org>
    
    BUG=chrome-os-partner:8982
    TEST=system still boots
    
    Change-Id: Ie42c034141f24795ec2bfee592e194001d3cd174

--- common/x86_power.c
+++ common/x86_power.c
@@ -93,6 +93,8 @@ static enum x86_state state;  /* Current state */
 static uint32_t in_signals;   /* Current input signal states (IN_PGOOD_*) */
 static uint32_t in_want;      /* Input signal state we're waiting for */
 static int want_g3_exit;      /* Should we exit the G3 state? */
+static int throttle_cpu;      /* Throttle CPU? */
+
 
 /* Update input signal state */
 static void update_in_signals(void)
@@ -261,6 +263,16 @@ void chipset_exit_hard_off(void)
 	/* Set a flag to leave G3, then wake the task */
 	want_g3_exit = 1;
 	task_wake(TASK_ID_X86POWER);
+}
+
+
+void chipset_throttle_cpu(int throttle)
+{
+	throttle_cpu = throttle;
+
+	/* Immediately set throttling if CPU is on */
+	if (state == X86_S0)
+		gpio_set_level(GPIO_CPU_PROCHOT, throttle);
 }
 
 /*****************************************************************************/
@@ -442,6 +454,9 @@ void x86_power_task(void)
 			break;
 
 		case X86_S3S0:
+			/* Deassert prochot so it's low when +VCCP powers up */
+			gpio_set_flags(GPIO_CPU_PROCHOT, 0);
+
 			/* Turn on power rails */
 			gpio_set_level(GPIO_ENABLE_VS, 1);
 
@@ -466,6 +481,9 @@ void x86_power_task(void)
 			/* Set PCH_PWROK */
 			gpio_set_level(GPIO_PCH_PWROK, 1);
 
+			/* Reassert prochot if necessary */
+			gpio_set_flags(GPIO_CPU_PROCHOT, throttle_cpu);
+
 			state = X86_S0;
 			break;
 
@@ -486,6 +504,13 @@ void x86_power_task(void)
 			gpio_set_level(GPIO_ENABLE_WLAN, 0);
 			gpio_set_level(GPIO_RADIO_ENABLE_WLAN, 0);
 			gpio_set_level(GPIO_RADIO_ENABLE_BT, 0);
+
+			/* Deassert prochot since CPU is now shut down */
+			/* TODO: If PROCHOT is externally pulled up, we should
+			 * set PROCHOT here instead, so we don't leak power
+			 * through the pullup - and we should set its default
+			 * state in board.h to HIGH.  See crosbug.com/p/8982. */
+			gpio_set_flags(GPIO_CPU_PROCHOT, 0);
 
 			/* Turn off power rails */
 			gpio_set_level(GPIO_ENABLE_VS, 0);
@@ -510,7 +535,6 @@ void x86_power_task(void)
 
 			/* Switch off +5V always-on */
 			gpio_set_level(GPIO_ENABLE_5VALW, 0);
-
 			state = X86_S5;
 			break;
 
