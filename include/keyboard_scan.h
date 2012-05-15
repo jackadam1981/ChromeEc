f5cdaafb5a9c87fb977999cabefbdbde4e264a4e
David Hendricks <dhendrix@chromium.org>
2012-05-15 12:11:13 -0700

    stm32: add a FIFO for keyboard usage
    This CL adds a FIFO and a task for interrupting the AP to notify it
    if FIFO entries exist. For now it is only used to store keystrokes
    but could eventually be made more generic.
    
    The change was prompted by the realization that the AP can be very
    slow at scheduling the task which queries the EC for keystroke data.
    The actual transfer of data is fast, but the work might get preempted
    for hundreds millisecond.
    
    This CL includes:
    - Keyboard FIFO and modifications to Daisy/Snow to use it.
    - FIFO task that wakes up periodically. If there is something in the
      FIFO then it will interrupt the host.
    - Console commands for debugging
    
    The added console commands are:
    kb_status: print keyboard FIFO variables and content
    kb_clear: clear keyboard FIFO entries
    kb_inject <row> <col> <num_presses>: Simulate key presses
    
    BUG=chrome-os-partner:9594
    TEST=tested on daisy
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    
    Testing was mostly subjectively done by typing a sentence quickly
    at the Chrome UI. kb_inject() was also used to rapidly fill the FIFO
    to test overflow cases.
    
    Change-Id: I6faff41b0f16b75d6426df13a239f84eb62634ec

--- include/keyboard_scan.h
+++ include/keyboard_scan.h
@@ -28,10 +28,22 @@ int keyboard_scan_recovery_pressed(void);
  * The supplied buffer can be used directly if required, but in that case
  * the number of bytes available is limited to 'max_bytes'.
  *
- * @param buffp		Pointer to buffer to contain data
+ * @param buffp		Buffer to fill
  * @param max_bytes	Maximum number of bytes available in *buffp
  * @return number of bytes available, or -1 for error
  */
 int keyboard_get_scan(uint8_t **buffp, int max_bytes);
+
+/* let FIFO task know that work is imminent to avoid excessive AP interrupts */
+void keyboard_fifo_work_in_progress(void);
+
+/* clear any saved keyboard state (empty FIFO, etc) */
+void keyboard_clear_state(void);
+
+/* push an entry onto the keyboard FIFO */
+int keyboard_fifo_push(uint8_t *buffp);
+
+/* pop an entry from the keyboard FIFO */
+int keyboard_fifo_pop(uint8_t *buffp);
 
 #endif  /* __CROS_KEYBOARD_SCAN_H */
