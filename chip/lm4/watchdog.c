12d33624bebfe8d6764f5dfaa6657ac2993ad195
Simon Glass <sjg@chromium.org>
2012-06-01 21:43:25 -0700

    Move watchdog defines into watchdog.h
    BUG=chrome-os-partner:10145
    TEST=build for all boards
    
    Change-Id: I6076daa5fd9f19ea6c61716f1286d8b14b4a2afc
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- chip/lm4/watchdog.c
+++ chip/lm4/watchdog.c
@@ -16,6 +16,7 @@
 #include "timer.h"
 #include "uart.h"
 #include "util.h"
+#include "watchdog.h"
 
 /*
  * We use watchdog 0 which is clocked on the system clock
@@ -24,11 +25,6 @@
 
 /* magic value to unlock the watchdog registers */
 #define LM4_WATCHDOG_MAGIC_WORD  0x1ACCE551
-
-#define WATCHDOG_PERIOD_MS 1100  /* Watchdog period in ms */
-#define WATCHDOG_RELOAD_MS 500   /* Interval in ms between reloads of the
-				  * watchdog timer.  Should be less than half
-				  * of the watchdog period. */
 
 static uint32_t watchdog_period;     /* Watchdog counter initial value */
 
@@ -131,7 +127,7 @@ static int watchdog_freq_changed(void)
 DECLARE_HOOK(HOOK_FREQ_CHANGE, watchdog_freq_changed, HOOK_PRIO_DEFAULT);
 
 
-int watchdog_init(void)
+int watchdog_init(int ignored)
 {
 	volatile uint32_t scratch  __attribute__((unused));
 
