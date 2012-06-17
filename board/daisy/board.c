823f72c4bea4a0c5ed052b61de66a72b77751723
Rong Chang <rongchang@chromium.org>
2012-06-17 13:43:15 +0800

    Enable daisy I2C host auto detection
    The host port on Daisy can be configured as I2C1 or I2C2. PMU is
    connected directly to the host port, hence the host port can be
    detected. This change unifies ec firmware image for different I2C
    configurations.
    
    Signed-off-by: Rong Chang <rongchang@chromium.org>
    BUG=chrome-os-partner:10612
    TEST=manual
      Build daisy ec firmware. Flash it to daisy boards with different
      I2C port config. Check uart console commands:
        'i2c r 0x90 4' - single byte pmu read
        'battery' - double bytes battery read
    
    Change-Id: I70f66581d0e921c83bc2051b2a521b332e18aa50

--- board/daisy/board.c
+++ board/daisy/board.c
@@ -7,6 +7,7 @@
 #include "board.h"
 #include "common.h"
 #include "dma.h"
+#include "i2c.h"
 #include "gpio.h"
 #include "registers.h"
 #include "spi.h"
@@ -81,6 +82,28 @@ const struct gpio_info gpio_list[GPIO_COUNT] = {
 	{"KB_OUT11",    GPIO_C, (1<<6),  GPIO_KB_OUTPUT, NULL},
 	{"KB_OUT12",    GPIO_C, (1<<7),  GPIO_KB_OUTPUT, NULL},
 };
+
+/* Auto detect I2C host port
+ * Daisy board has two I2C ports, I2C1(0) and I2C2(1), that can be configured
+ * as host. PMU chip is connected directly to the EC, and hence can be used
+ * for port detection
+ */
+#ifdef CONFIG_I2C_HOST_AUTO
+static int i2c_host_port = -1;
+
+int board_i2c_host_port(void)
+{
+	int d;
+
+	/* Default I2C host configuration is I2C1(0).
+	 * If PMU doesn't ack on I2C2(1), set the host port to 0.
+	 */
+	if (i2c_host_port == -1)
+		i2c_host_port = i2c_read8(1, 0x90, 0x4, &d) ? 0 : 1;
+
+	return i2c_host_port;
+}
+#endif /* CONFIG_I2C_HOST_AUTO */
 
 void configure_board(void)
 {
