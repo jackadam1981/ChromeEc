694d023fa509204a54ae8ba79177bf8c8a0f19c7
Simon Glass <sjg@chromium.org>
2012-06-26 16:12:29 -0700

    dma: Add dma_get_avail() to return available bytes in a dma channel
    By subtracting the current dma count from the number of bytes originally
    requested to be transfered, we can find out how many bytes have been
    transfered so far.
    
    BUG=chrome-os-partner:10533
    TEST=build and boot on snow
    
    Change-Id: Ideee1ed27c08b56882f5d2095341fe04bbe9c34b
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- chip/stm32/dma.h
+++ chip/stm32/dma.h
@@ -118,6 +118,18 @@ int dma_start_rx(unsigned channel, unsigned count, void *periph,
 void dma_disable(unsigned channel);
 
 /**
+ * Get the number of bytes available to read, or nunber of bytes written
+ *
+ * Since the DMA controller counts downwards, if we know the starting value
+ * we can work out how many bytes have been completed so far.
+ *
+ * @param chan		DMA channel to check (use dma_get_channel())
+ * @param orig_count	Original number of bytes requested on channel
+ * @return number of bytes completed on a channel, or 0 if this channel is
+ *		not enabled
+ */
+int dma_get_avail(struct dma_channel *chan, int orig_count);
+
 /**
  * Start a previously-prepared dma channel
  *
