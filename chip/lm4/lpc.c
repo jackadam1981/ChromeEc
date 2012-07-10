4ac3744f6d6c9d28878cce8fd726859b5e3a9b7e
Louis Yung-Chieh Lo <yjlou@chromium.org>
2012-07-10 12:11:20 +0800

    Ensure at least one LPC status bit is 0 at init time.
    Clear bit 8 of LM4_LPC_ST(LPC_CH_KERNEL) and LM4_LPC_ST(LPC_CH_USER) in
    lpc_init(). Also defined other bit masks for future allocation.
    
    BUG=chrome-os-partner:10963
    Signed-off-by: Louis Yung-Chieh Lo <yjlou@chromium.org>
    TEST=build in chroot only.
    
    Change-Id: Iaa61e18d38153cc25d064554df44d2a642e1bb40

--- chip/lm4/lpc.c
+++ chip/lm4/lpc.c
@@ -27,8 +27,12 @@
 
 #define LPC_SYSJUMP_TAG 0x4c50  /* "LP" */
 
-/* TO Host bit in LPCCH?ST) */
-#define TOH (1 << 0)
+/* Bit masks for LPCCH?ST */
+#define LPC_STATUS_MASK_BUSY     (1 << 12)
+#define LPC_STATUS_MASK_SMI      (1 << 10)
+#define LPC_STATUS_MASK_SCI      (1 <<  9)
+#define LPC_STATUS_MASK_PRESENT  (1 <<  8)
+#define LPC_STATUS_MASK_TOH      (1 <<  0)  /* TO Host bit */
 
 static uint32_t host_events;     /* Currently pending SCI/SMI events */
 static uint32_t event_mask[3];   /* Event masks for each type */
@@ -142,7 +146,7 @@ static void send_result(int slot, enum ec_status result)
 
 	/* Clear the busy bit */
 	task_disable_irq(LM4_IRQ_LPC);
-	LM4_LPC_ST(ch) &= ~(1 << 12);
+	LM4_LPC_ST(ch) &= ~LPC_STATUS_MASK_BUSY;
 	task_enable_irq(LM4_IRQ_LPC);
 
 	/* ACPI 5.0-12.6.1: Generate SCI for Output Buffer Full
@@ -167,7 +171,7 @@ void host_send_response(int slot, enum ec_status result, const uint8_t *data,
 
 /* Return true if the TOH is still set */
 int lpc_keyboard_has_char(void) {
-	return (LM4_LPC_ST(LPC_CH_KEYBOARD) & TOH) ? 1 : 0;
+	return (LM4_LPC_ST(LPC_CH_KEYBOARD) & LPC_STATUS_MASK_TOH) ? 1 : 0;
 }
 
 
@@ -186,7 +190,7 @@ void lpc_keyboard_clear_buffer(void)
 	/* Make sure the previous TOH and IRQ has been sent out. */
 	wait_irq_sent();
 
-	LM4_LPC_ST(LPC_CH_KEYBOARD) &= ~TOH;
+	LM4_LPC_ST(LPC_CH_KEYBOARD) &= ~LPC_STATUS_MASK_TOH;
 
 	/* Ensure there is no TOH set in this period. */
 	wait_irq_sent();
@@ -237,24 +241,24 @@ static void update_host_event_status(void) {
 
 	if (host_events & event_mask[LPC_HOST_EVENT_SMI]) {
 		/* Only generate SMI for first event */
-		if (!(LM4_LPC_ST(LPC_CH_USER) & (1 << 10)) ||
-		    !(LM4_LPC_ST(LPC_CH_KERNEL) & (1 << 10)))
+		if (!(LM4_LPC_ST(LPC_CH_USER) & LPC_STATUS_MASK_SMI) ||
+		    !(LM4_LPC_ST(LPC_CH_KERNEL) & LPC_STATUS_MASK_SMI))
 			need_smi = 1;
-		LM4_LPC_ST(LPC_CH_USER) |= (1 << 10);
-		LM4_LPC_ST(LPC_CH_KERNEL) |= (1 << 10);
+		LM4_LPC_ST(LPC_CH_USER) |= LPC_STATUS_MASK_SMI;
+		LM4_LPC_ST(LPC_CH_KERNEL) |= LPC_STATUS_MASK_SMI;
 	} else {
-		LM4_LPC_ST(LPC_CH_USER) &= ~(1 << 10);
-		LM4_LPC_ST(LPC_CH_KERNEL) &= ~(1 << 10);
+		LM4_LPC_ST(LPC_CH_USER) &= ~LPC_STATUS_MASK_SMI;
+		LM4_LPC_ST(LPC_CH_KERNEL) &= ~LPC_STATUS_MASK_SMI;
 	}
 
 	if (host_events & event_mask[LPC_HOST_EVENT_SCI]) {
 		/* Generate SCI for every event */
 		need_sci = 1;
-		LM4_LPC_ST(LPC_CH_USER) |= (1 << 9);
-		LM4_LPC_ST(LPC_CH_KERNEL) |= (1 << 9);
+		LM4_LPC_ST(LPC_CH_USER) |= LPC_STATUS_MASK_SCI;
+		LM4_LPC_ST(LPC_CH_KERNEL) |= LPC_STATUS_MASK_SCI;
 	} else {
-		LM4_LPC_ST(LPC_CH_USER) &= ~(1 << 9);
-		LM4_LPC_ST(LPC_CH_KERNEL) &= ~(1 << 9);
+		LM4_LPC_ST(LPC_CH_USER) &= ~LPC_STATUS_MASK_SCI;
+		LM4_LPC_ST(LPC_CH_KERNEL) &= ~LPC_STATUS_MASK_SCI;
 	}
 
 	/* Copy host events to mapped memory */
@@ -331,7 +335,7 @@ static void lpc_interrupt(void)
 	/* Handle host kernel/user command writes */
 	if (mis & LM4_LPC_INT_MASK(LPC_CH_KERNEL, 4)) {
 		/* Set the busy bit */
-		LM4_LPC_ST(LPC_CH_KERNEL) |= (1 << 12);
+		LM4_LPC_ST(LPC_CH_KERNEL) |= LPC_STATUS_MASK_BUSY;
 
 		/* Read the command byte and pass to the host command handler.
 		 * This clears the FRMH bit in the status byte. */
@@ -343,7 +347,7 @@ static void lpc_interrupt(void)
 	}
 	if (mis & LM4_LPC_INT_MASK(LPC_CH_USER, 4)) {
 		/* Set the busy bit */
-		LM4_LPC_ST(LPC_CH_USER) |= (1 << 12);
+		LM4_LPC_ST(LPC_CH_USER) |= LPC_STATUS_MASK_BUSY;
 
 		/* Read the command byte and pass to the host command handler.
 		 * This clears the FRMH bit in the status byte. */
@@ -439,6 +443,7 @@ static int lpc_init(void)
 	 * data writes, pool bytes 0(data)/1(cmd) */
 	LM4_LPC_ADR(LPC_CH_KERNEL) = EC_LPC_ADDR_KERNEL_DATA;
 	LM4_LPC_CTL(LPC_CH_KERNEL) = (LPC_POOL_OFFS_KERNEL << (5 - 1));
+	LM4_LPC_ST(LPC_CH_KERNEL) = 0;
 	/* Unmask interrupt for host command writes */
 	LM4_LPC_LPCIM |= LM4_LPC_INT_MASK(LPC_CH_KERNEL, 4);
 
@@ -475,6 +480,7 @@ static int lpc_init(void)
 	 * data writes, pool bytes 0(data)/1(cmd) */
 	LM4_LPC_ADR(LPC_CH_USER) = EC_LPC_ADDR_USER_DATA;
 	LM4_LPC_CTL(LPC_CH_USER) = (LPC_POOL_OFFS_USER << (5 - 1));
+	LM4_LPC_ST(LPC_CH_USER) = 0;
 	/* Unmask interrupt for host command writes */
 	LM4_LPC_LPCIM |= LM4_LPC_INT_MASK(LPC_CH_USER, 4);
 
