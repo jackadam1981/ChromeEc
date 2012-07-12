ace639a50c9acdde2324f419a64a9a3965632ee8
Vic Yang <victoryang@chromium.org>
2012-07-12 16:33:10 +0800

    Set power LED to green when we are trickle charging nearly full
    When battery is nearly full, battery sometimes demands for very low
    current and we are actually trickle charging. This causes the last part
    of charging process very long, but the actual charged amount is only few
    mAh. Let's set power LED to green in this case so that user doesn't feel
    the device is charging forever.
    
    BUG=chrome-os-partner:11248
    TEST=Charge the battery to nearly full. Disconnect and connect AC power.
         Check the power LED is green when we are trickle charging.
    
    Change-Id: Ide108778232e9f1d3abe6b61af7518af25040d10

--- common/charge_state.c
+++ common/charge_state.c
@@ -248,6 +248,9 @@ static int state_common(struct power_state_context *ctx)
  */
 static enum power_state state_init(struct power_state_context *ctx)
 {
+	/* Set LED to green first, other states should change it as desired. */
+	ctx->curr.led_color = POWERLED_GREEN;
+
 	/* Stop charger, unconditionally */
 	charger_set_current(0);
 	charger_set_voltage(0);
@@ -278,6 +281,8 @@ static enum power_state state_idle(struct power_state_context *ctx)
 {
 	struct batt_params *batt = &ctx->curr.batt;
 	const struct charger_info *c_info = ctx->charger;
+
+	ctx->curr.led_color = POWERLED_GREEN;
 
 	if (!ctx->curr.ac)
 		return PWR_STATE_INIT;
@@ -322,6 +327,8 @@ static enum power_state state_idle(struct power_state_context *ctx)
  */
 static enum power_state state_charge(struct power_state_context *ctx)
 {
+	ctx->curr.led_color = POWERLED_YELLOW;
+
 	if (ctx->curr.error)
 		return PWR_STATE_ERROR;
 
@@ -366,6 +373,9 @@ static enum power_state state_charge(struct power_state_context *ctx)
 static enum power_state state_discharge(struct power_state_context *ctx)
 {
 	struct batt_params *batt = &ctx->curr.batt;
+
+	ctx->curr.led_color = POWERLED_OFF;
+
 	if (ctx->curr.ac)
 		return PWR_STATE_INIT;
 
@@ -390,6 +400,8 @@ static enum power_state state_discharge(struct power_state_context *ctx)
 static enum power_state state_error(struct power_state_context *ctx)
 {
 	static int logged_error;
+
+	ctx->curr.led_color = POWERLED_RED;
 
 	if (!ctx->curr.error) {
 		logged_error = 0;
@@ -454,11 +466,12 @@ void charge_state_machine_task(void)
 	int sleep_usec = POLL_PERIOD_SHORT, diff_usec, sleep_next;
 	enum power_state new_state;
 	uint8_t batt_flags;
-	enum powerled_color led_color = POWERLED_OFF;
 	int rv_setled = 0;
 
 	ctx.prev.state = PWR_STATE_INIT;
 	ctx.curr.state = PWR_STATE_INIT;
+	ctx.prev.led_color = POWERLED_OFF;
+	ctx.curr.led_color = POWERLED_OFF;
 	ctx.trickle_charging_time.val = 0;
 	ctx.battery = battery_get_info();
 	ctx.charger = charger_get_info();
@@ -506,16 +519,16 @@ void charge_state_machine_task(void)
 				state_name[new_state]);
 		}
 
+		if ((ctx.curr.led_color != ctx.prev.led_color || rv_setled) &&
+		    new_state != PWR_STATE_DISCHARGE)
+			rv_setled = powerled_set(ctx.curr.led_color);
+
 		switch (new_state) {
 		case PWR_STATE_IDLE:
 			batt_flags = *ctx.memmap_batt_flags;
 			batt_flags &= ~EC_BATT_FLAG_CHARGING;
 			batt_flags &= ~EC_BATT_FLAG_DISCHARGING;
 			*ctx.memmap_batt_flags = batt_flags;
-
-			/* Charge done */
-			led_color = POWERLED_GREEN;
-			rv_setled = powerled_set(POWERLED_GREEN);
 
 			sleep_usec = POLL_PERIOD_LONG;
 			break;
@@ -532,23 +545,13 @@ void charge_state_machine_task(void)
 			batt_flags &= ~EC_BATT_FLAG_DISCHARGING;
 			*ctx.memmap_batt_flags = batt_flags;
 
-			/* Charging */
-			led_color = POWERLED_YELLOW;
-			rv_setled = powerled_set(POWERLED_YELLOW);
-
 			sleep_usec = POLL_PERIOD_CHARGE;
 			break;
 		case PWR_STATE_ERROR:
-			/* Error */
-			led_color = POWERLED_RED;
-			rv_setled = powerled_set(POWERLED_RED);
-
 			sleep_usec = POLL_PERIOD_CHARGE;
 			break;
 		case PWR_STATE_UNCHANGE:
 			/* Don't change sleep duration */
-			if (rv_setled)
-				rv_setled = powerled_set(led_color);
 			break;
 		default:
 			/* Other state; poll quickly and hope it goes away */
