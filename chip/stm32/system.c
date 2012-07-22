d4a21db32a4602df57583d00e77ee3fe95cf74d9
Vic Yang <victoryang@chromium.org>
2012-07-22 13:17:23 +0800

    stm32f: Flash write protect
    Implement STM32F write protect to match refactored flash module.
    Also move fake write-protect pin to use backup register to preserve
    value across reboot.
    
    BUG=chrome-os-partner:11699
    TEST=1. 'flashinfo'      -> no flags
         2. 'fakewp 1'       -> 'wp_gpio_asserted'
         3. 'flashwp enable' -> 'wp_gpio_asserted ro_at_boot'
         4. 'reboot'         -> 'wp_gpio_asserted ro_at_boot ro_now'
         5. 'fakewp 0'       -> 'ro_at_boot ro_now'
         6. 'reboot'         -> 'ro_at_boot'
         7. 'fakewp 1'       -> 'wp_gpio_asserted ro_at_boot'
         8. 'flashwp rw'     -> 'wp_gpio_asserted ro_at_boot rw_at_boot'
         9. 'reboot'         -> 'wp_gpio_asserted ro_at_boot ro_now
                                 rw_at_boot rw_now'
         10.'flashwp disable'-> error 7
         11.'flashwp norw'   -> 'wp_gpio_asserted ro_at_boot ro_now rw_now'
         12.'reboot'         -> 'wp_gpio_asserted ro_at_boot ro_now'
    
    Change-Id: I40405c266e30b10793ccae2f1d26fb9710ce304b

--- chip/stm32/system.c
+++ chip/stm32/system.c
@@ -16,6 +16,10 @@ enum bkpdata_index {
 	BKPDATA_INDEX_SCRATCHPAD,	/* General-purpose scratchpad */
 	BKPDATA_INDEX_WAKE,		/* Wake reasons for hibernate */
 	BKPDATA_INDEX_SAVED_RESET_FLAGS,/* Saved reset flags */
+	BKPDATA_INDEX_FAKE_WP,		/* Fake write-protect pin */
+					/* TODO: Remove this when we have real
+					 *       write protect pin.
+					 */
 };
 
 /* Wake reason flags for hibernate */
@@ -269,12 +273,26 @@ const char *system_get_chip_vendor(void)
 	return "stm";
 }
 
+
 const char *system_get_chip_name(void)
 {
 	return STRINGIFY(CHIP_VARIANT);
 }
 
+
 const char *system_get_chip_revision(void)
 {
 	return "";
+}
+
+
+int system_set_fake_wp(int val)
+{
+	return bkpdata_write(BKPDATA_INDEX_FAKE_WP, (uint16_t)val);
+}
+
+
+int system_get_fake_wp(void)
+{
+	return bkpdata_read(BKPDATA_INDEX_FAKE_WP);
 }
