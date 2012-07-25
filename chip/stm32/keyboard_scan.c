9feabd9c72a4b4e6bd2185c5392977acd9a27f70
Charlie Mooney <charliemooney@chromium.org>
2012-07-24 17:29:27 -0700

    Stop keyboard scans from triggering ARM EC reset
    The ARM EC was being rebooted when both the power and one of several other keys
    were pressed. (LCtrl, Tab, Reload, t, [, ], y, Dim Screen and Mute) It should
    only do this when the key combo PWR + Reload is pressed.
    
    To fix it, keyboard scanning is disabled whenever the power button is
    pressed.  It sets a flag indicating that scanning should be disabled,
    and then any time the EC tries to select a scan column, it will just set
    it to Hi-Z instead.
    
    BUG=chrome-os-partner:10889
    TEST=Pick one of the troublesome keys.  First press it, then quickly
    press the power button.  Then press the power button followed
    by the troublesome key.  Repeat this process several times for each key,
    it should not reset the system.
    Press power + reload, this should still reset the system.
    Pressing and holding power should initiate a shutdown.
    
    Change-Id: Ib60d2ebbb57eb8a3c135662514ec622c37a7eb07
    Signed-off-by: Charlie Mooney <charliemooney@chromium.org>

--- chip/stm32/keyboard_scan.c
+++ chip/stm32/keyboard_scan.c
@@ -39,6 +39,9 @@ enum COL_INDEX {
 /* 15:14, 12:8, 2 */
 #define IRQ_MASK 0xdf04
 
+/* Initially enabling keyboard scanning for boot up */
+static int scanning_enabled = 1;
+
 /* The keyboard state from the last read */
 static uint8_t raw_state[KB_OUTPUTS];
 
@@ -169,12 +172,12 @@ static void select_column(int col)
 			if (gpio_list[j].port != ports[i])
 				continue;
 
-			if (col == COL_ASSERT_ALL) {
-				/* drive low (clear output data) */
-				bsrr |= gpio_list[j].mask << 16;
-			} else if (col == COL_TRI_STATE_ALL) {
+			if (col == COL_TRI_STATE_ALL || !scanning_enabled) {
 				/* put column in hi-Z state (set output data) */
 				bsrr |= gpio_list[j].mask;
+			} else if (col == COL_ASSERT_ALL) {
+				/* drive low (clear output data) */
+				bsrr |= gpio_list[j].mask << 16;
 			} else {
 				/* drive specified column low, others => hi-Z */
 				if (j - GPIO_KB_OUT00 == col) {
@@ -385,7 +388,7 @@ void keyboard_scan_task(void)
 			/* sleep for debounce. */
 			usleep(SCAN_LOOP_DELAY);
 			/* Check for keys down */
-			if (check_keys_changed()) {
+			if (scanning_enabled && check_keys_changed()) {
 				key_press_timer = 0;
 			} else {
 				if (++key_press_timer >=
@@ -454,3 +457,12 @@ static int keyboard_get_info(struct host_cmd_handler_args *args)
 DECLARE_HOST_COMMAND(EC_CMD_MKBP_INFO,
 		     keyboard_get_info,
 		     EC_VER_MASK(0));
+
+void keyboard_enable_scanning(int enable)
+{
+	scanning_enabled = enable;
+	if (enable)
+		task_wake(TASK_ID_KEYSCAN);
+	else
+		select_column(COL_TRI_STATE_ALL);
+}
