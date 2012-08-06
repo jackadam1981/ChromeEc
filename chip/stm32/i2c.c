f10ef7283cb1cdfbb7b240c37d44b30e54aee31c
Charlie Mooney <charliemooney@chromium.org>
2012-08-06 10:38:14 -0700

    Fix stack overflow in i2c stack for EC
    There were a number of problems resulting from i2c crashes,
    particularly when trying to access the battery.  The problem is that
    the stack was overflowing on this particularly deep path, all the way
    down to wait_status.  This in itself was fine, but if there was a
    timeout, debugging information would be printed to the uart, and that
    function would cause an exception and restart the EC.
    
    To fix it, I stripped the debugging CPRINTFs from wait_status.  This
    allows everything to work fine, but looses some information for
    debugging.  To allow future developers to still see what event the i2c
    was waiting for, I added an additional variable to store it in, so that
    it can be displayed/handled further up the stack.
    
    BUG=chrome-os-partner:12245
    TEST=Boot the machine using a Servo.  On the AP's UART, run
    "cros_test i2c" to start pounding the i2c bus.  Then from the EC, run
    "pmu 1000" and then "battery 1000" there should be no error messages,
    exceptions, and the EC should not restart.
    
    Change-Id: Idd2f017d3557652bf3e8536c4ac776c1f70319cb
    Signed-off-by: Charlie Mooney <charliemooney@chromium.org>

--- chip/stm32/i2c.c
+++ chip/stm32/i2c.c
@@ -40,6 +40,7 @@
 #define I2C1      STM32_I2C1_PORT
 #define I2C2      STM32_I2C2_PORT
 
+
 enum {
 	/* A stop condition should take 2 clocks, so allow 8 */
 	TIMEOUT_STOP_SENT_US	= I2C_PERIOD_US * 8,
@@ -381,6 +382,7 @@ enum wait_t {
 	WAIT_RX_NE_STOP,
 	WAIT_RX_NE_STOP_SIZE2,
 };
+enum wait_t i2c_last_wait_status;
 
 static int wait_status(int port, uint32_t mask, enum wait_t wait)
 {
@@ -388,16 +390,11 @@ static int wait_status(int port, uint32_t mask, enum wait_t wait)
 	timestamp_t t1, t2;
 
 	t1 = t2 = get_time();
+	i2c_last_wait_status = wait;
 	r = STM32_I2C_SR1(port);
 	while (mask ? ((r & mask) != mask) : r) {
 		t2 = get_time();
 		if (t2.val - t1.val > I2C_TX_TIMEOUT) {
-#ifdef CONFIG_DEBUG_I2C
-			CPRINTF(" m %016b\n", mask);
-			CPRINTF(" - %016b\n", r);
-#endif /* CONFIG_DEBUG_I2C */
-			CPRINTF("i2c wait_status timeout type %d, %d us\n",
-				wait, (unsigned)t2.val - (unsigned)t1.val);
 			return EC_ERROR_TIMEOUT;
 		} else if (t2.val - t1.val > 150) {
 			usleep(100);
@@ -405,6 +402,7 @@ static int wait_status(int port, uint32_t mask, enum wait_t wait)
 		r = STM32_I2C_SR1(port);
 	}
 
+	i2c_last_wait_status = WAIT_NONE;
 	return EC_SUCCESS;
 }
 
@@ -470,6 +468,11 @@ static void handle_i2c_error(int port, int rv)
 	if (rv == EC_ERROR_BUSY)
 		return;
 
+
+#ifdef CONFIG_DEBUG_I2C
+	if (rv == EC_ERROR_TIMEOUT && i2c_last_wait_status != WAIT_NONE)
+		CPRINTF("Last wait_status: %d", i2c_last_wait_status);
+#endif
 	if (rv)
 		dump_i2c_reg(port);
 
