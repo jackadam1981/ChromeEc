3e8235c657d9e8665b37ec77f6cc8c24fb428b9a
Simon Que <sque@chromium.org>
2012-08-09 10:51:39 -0700

    Handle IRQ from TPS65090, pass AC status to AP
    Changes made by this patch:
    1. Create IRQ handler for the TPS65090 IRQ.  IRQ wakes up charger task.
    2. Charger task sets the AC_STATUS GPIO based on the AC status.
    3. Initialize PMU at power-on.
    
    BUG=chrome-os-partner:11739
    TEST=Power on the system, with servo v2 connected to EC console.  Plug
    and unplug AC.  The IRQ handler should be triggered.
    
    Change-Id: Ice23411c275111fdb56d2c47ba28c3c44dee4d71
    Signed-off-by: Simon Que <sque@chromium.org>

--- common/pmu_tps65090_charger.c
+++ common/pmu_tps65090_charger.c
@@ -322,6 +322,9 @@ void pmu_charger_task(void)
 	pmu_init();
 
 	while (1) {
+		pmu_clear_irq();
+		gpio_set_level(GPIO_AC_STATUS, get_ac());
+
 		next_state = calc_next_state(state);
 		if (next_state != state) {
 			CPRINTF("[batt] state %s -> %s\n",
@@ -331,6 +334,6 @@ void pmu_charger_task(void)
 		}
 
 		/* TODO(sjg@chromium.org): root cause crosbug.com/p/11285 */
-		usleep(5000 * 1000);
+		task_wait_event(5000 * 1000);
 	}
 }
