cf63763b1ca94e03c7699af7c02ebdde5e56963e
Randall Spangler <rspangler@chromium.org>
2012-09-06 13:08:15 -0700

    Check boot key combos if refresh key is held down
    BUG=chrome-os-partner:13753
    BRANCH=link
    TEST=manual
    
    1. Boot normally.  Works.
    2. Power+Refresh.  Boots normally.
    3. Power+Refresh+Esc.  Boots to recovery.
    4. Power+Refresh+Down.  EC reboots, system powers down.
    5. Hold down Esc and reboot from EC console.  Boots normally.
    6. Hold down Refresh+Esc and reboot from EC console.  Boots to recovery.
    
    Change-Id: Iabe4fd13589428a40b83f591ea679cbc6f83959d
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- chip/lm4/keyboard_scan.c
+++ chip/lm4/keyboard_scan.c
@@ -258,6 +258,34 @@ static int check_key(int index, int mask)
 	return 1;
 }
 
+static void keyboard_scan_check_boot_key(void)
+{
+	const struct boot_key_entry *k = boot_key_list;
+	int i;
+
+	/*
+	 * If we jumped to this image, ignore boot keys.  This prevents
+	 * re-triggering events in RW firmware that were already processed by
+	 * RO firmware.
+	 */
+	if (system_jumped_to_this_image())
+		return;
+
+	/* If reset was not caused by reset pin, refresh must be held down */
+	if (!(system_get_reset_flags() & RESET_FLAG_RESET_PIN) &&
+	    !(raw_state[MASK_INDEX_REFRESH] & MASK_VALUE_REFRESH))
+		return;
+
+	/* Check what single key is down */
+	for (i = 0; i < ARRAY_SIZE(boot_key_list); i++, k++) {
+		if (check_key(k->mask_index, k->mask_value)) {
+			CPRINTF("[%T KB boot key %d]\n", i);
+			boot_key_value = i;
+			break;
+		}
+	}
+}
+
 enum boot_key keyboard_scan_get_boot_key(void)
 {
 	return boot_key_value;
@@ -290,27 +318,12 @@ int keyboard_scan_init(void)
 	/* Initialize raw state */
 	update_key_state();
 
-	/*
-	 * If we're booting due to a reset-pin-caused reset, check what other
-	 * single key is held down (if any).
-	 */
-	if ((system_get_reset_flags() & RESET_FLAG_RESET_PIN) &&
-	    !system_jumped_to_this_image()) {
-		const struct boot_key_entry *k = boot_key_list;
-		int i;
+	/* Check for keys held down at boot */
+	keyboard_scan_check_boot_key();
 
-		for (i = 0; i < ARRAY_SIZE(boot_key_list); i++, k++) {
-			if (check_key(k->mask_index, k->mask_value)) {
-				CPRINTF("[%T KB boot key %d]\n", i);
-				boot_key_value = i;
-				break;
-			}
-		}
-
-		/* Trigger event if recovery key was pressed */
-		if (boot_key_value == BOOT_KEY_ESC)
-			host_set_single_event(EC_HOST_EVENT_KEYBOARD_RECOVERY);
-	}
+	/* Trigger event if recovery key was pressed */
+	if (boot_key_value == BOOT_KEY_ESC)
+		host_set_single_event(EC_HOST_EVENT_KEYBOARD_RECOVERY);
 
 	return EC_SUCCESS;
 }
