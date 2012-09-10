411cb6cf073f02e7558eb8993abdc212ed0ebf7a
Rong Chang <rongchang@chromium.org>
2012-09-10 15:33:43 +0800

    WIP: snow: Calculate relative state of charge moving average
    Signed-off-by: Rong Chang <rongchang@chromium.org>
    BRANCH=snow
    BUG=chrome-os-partner:13846
    TEST=none
    
    Change-Id: Iab9797d0aa6b159bedd8ce0d2fa72c6458cd14ac

--- common/pmu_tps65090_charger.c
+++ common/pmu_tps65090_charger.c
@@ -129,6 +129,35 @@ static int notify_battery_low(void)
 	return ST_DISCHARGING;
 }
 
+/*
+ * Calculate relative state of charge moving average
+ */
+static int rsoc_moving_average(int state_of_charge)
+{
+	static uint8_t rsoc[4];
+	static uint8_t index = sizeof(rsoc);
+	int moving_average = 0;
+	int i;
+
+	if (index == sizeof(rsoc)) {
+		for (i = 0; i < sizeof(rsoc); i++)
+			rsoc[i] = (uint8_t)state_of_charge;
+		index = 0;
+		return state_of_charge;
+	}
+
+	rsoc[index] = (uint8_t)state_of_charge;
+	index++;
+	index %= sizeof(rsoc);
+
+	for (i = 0; i < sizeof(rsoc); i++)
+		moving_average += (int)rsoc[i];
+	moving_average /= sizeof(rsoc);
+
+	return moving_average;
+}
+
+
 static int config_low_current_charging(int charge)
 {
 	/* Disable low current termination */
@@ -305,7 +334,7 @@ static int calc_next_state(int state)
 		}
 		/* Check remaining charge % */
 		if (battery_state_of_charge(&capacity) == 0) {
-			if (capacity < 3) {
+			if (rsoc_moving_average(capacity) < 3) {
 				system_off();
 				return ST_IDLE;
 			} else if (capacity < 10) {
