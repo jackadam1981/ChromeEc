28ee9059e14ddb376d2b2748e37bd248deb501f8
Randall Spangler <rspangler@chromium.org>
2012-09-19 14:22:58 -0700

    EC recalculates RW code hash after all sysjumps
    This solves the problem where the AP updates EC-RW as part of software
    sync, then tells the EC to jump to EC-RW.  On the next boot the EC
    still returned the saved old RW hash, causing a needless cold boot.
    
    This change is backwards/forwards compatible; existing RO code will
    simply save a hash new RW code won't look at, and existing RW code
    already knows to recompute the hash if the RO code didn't save it.
    
    Hash computation is done in the background and takes ~350ms, so this
    should have no noticeable effect on performance.
    
    BUG=chrome-os-partner:13511
    
    BRANCH=all (not needed for FSI, since RW code with this change will do
    the right thing with existing RO code)
    
    TEST=at the EC console, "sysjump RW" and look at the EC debug log for
    "hash done".
    
    Change-Id: Ie5255727b9d896b7c4e4f537e91d831682afc7f6
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- common/vboot_hash.c
+++ common/vboot_hash.c
@@ -88,9 +88,9 @@ static void vboot_hash_abort(void)
 		want_abort = 1;
 }
 
-
 static void vboot_hash_init(void)
 {
+#ifdef CONFIG_SAVE_VBOOT_HASH
 	const struct vboot_hash_tag *tag;
 	int version, size;
 
@@ -103,7 +103,9 @@ static void vboot_hash_init(void)
 		hash = tag->hash;
 		data_offset = tag->offset;
 		data_size = tag->size;
-	} else {
+	} else
+#endif
+	{
 		/* Start computing the hash of firmware A */
 		vboot_hash_start(CONFIG_FW_RW_OFF,
 				 system_get_image_used(SYSTEM_IMAGE_RW),
@@ -154,6 +156,8 @@ void vboot_hash_task(void)
 /*****************************************************************************/
 /* Hooks */
 
+#ifdef CONFIG_SAVE_VBOOT_HASH
+
 static int vboot_hash_preserve_state(void)
 {
 	struct vboot_hash_tag tag;
@@ -171,6 +175,8 @@ static int vboot_hash_preserve_state(void)
 	return EC_SUCCESS;
 }
 DECLARE_HOOK(HOOK_SYSJUMP, vboot_hash_preserve_state, HOOK_PRIO_DEFAULT);
+
+#endif
 
 /****************************************************************************/
 /* Console commands */
