73ceab2da275c8f2bc093ce246c00186f8b68b83
Simon Glass <sjg@chromium.org>
2012-09-29 17:27:21 -0700

    WIP: Add end-to-end test to check keyboard debouncing
    (This includes an implementation of configurable keyscan parameters
    in stm32, although the ectool part is not yet complete. Key scan
    frequency has been increased to about 300Hz from 100Hz).
    
    Add a facility in the EC keyboard_scan driver to play back a series of
    key scans received in a message from the AP. Each scan is timestamped
    with a microsecond time when it becomes live.
    
    Add infrastructure in ectool to send a simple sequence file, which
    uses ascii characters, to the EC for playing back. The sequence file
    has the expected output from the test, so that the test result can be
    determined.
    
    Each new key scan is presented by the EC when its timestamp arrives.
    To avoid large numbers, use the concept of 'beats' in the sequence file,
    where the length of each beat is configurable. This means that a
    sequence with events on beat 0, 1, 2, 3 with a beat length of 20ms will
    result in events changing a 0us, 10,000us, 20,000us, 30,000us.
    
    BUG=chrome-os-partner:12179
    BRANCH=snow,link
    TEST=manual
    On snow:
    ./ectool keyscan 10000 key_sequence.txt
    
    See that the test passes.
    
    Change-Id: Id690d6c5181d373c41ce030bd051017324b8ffbf
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -189,6 +189,7 @@ enum ec_status {
 	EC_RES_IN_PROGRESS = 8,		/* Accepted, command in progress */
 	EC_RES_UNAVAILABLE = 9,		/* No response available */
 	EC_RES_TIMEOUT = 10,		/* We got a timeout */
+	EC_RES_OVERFLOW = 11,		/* Table / data overflow */
 };
 
 /*
@@ -913,6 +914,39 @@ enum ec_mkbp_config_cmd {
 struct ec_params_mkbp_config {
 	uint8_t cmd;		/* command (enum ec_mkbp_config_cmd) */
 	struct ec_mkbp_config config;
+} __packed;
+
+/* Program the EC for key scan emulation sequence (fold into 0x66?) */
+#define EC_CMD_KEYSCAN_SEQ_ADD 0x65
+
+struct ec_params_keyscan_seq_item {
+	uint16_t beat;
+	uint8_t scan[13];
+} __packed;
+
+struct ec_params_keyscan_seq_add {
+	uint8_t num_items;
+	struct ec_params_keyscan_seq_item item[0];
+} __packed;
+
+/* Run the key scan emulation */
+#define EC_CMD_KEYSCAN_SEQ_CTRL 0x66
+
+enum ec_keyscan_seq_cmd {
+	EC_KEYSCAN_SEQ_CLEAR = 0,
+	EC_KEYSCAN_SEQ_START = 1,
+	EC_KEYSCAN_SEQ_COLLECT = 2,
+};
+
+struct ec_params_keyscan_seq_ctrl {
+	uint32_t beat_us;	/* Beat length in microseconds */
+	uint8_t cmd;	/* Command to send (enum ec_keyscan_seq_cmd) */
+} __packed;
+
+/* Optional reponse (only used for collect) */
+struct ec_result_keyscan_seq_ctrl {
+	uint8_t num_items;
+	uint8_t done[0];
 } __packed;
 
 /*****************************************************************************/
