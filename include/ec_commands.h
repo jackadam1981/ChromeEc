971719869e3115e49007c37c0b93a982d0f1a909
Simon Glass <sjg@chromium.org>
2012-10-04 12:27:36 -0700

    Define key scan parameters and get/set command
    At present the keyboard scan parameters are hard-coded, so changing them
    requires a new EC image. This can be problematic if we want to adjust
    the behavior of keyboard scanning since we must send an EC update.
    
    Define the keyboard scan parameters and commands to get/set these
    parameters.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    
    BUG=chrome-os-partner:12179
    BRANCH=snow,link
    TEST=manual
    Build for all boards
    
    Change-Id: I715755cb5357503723b27ae33053dba1452e48e0

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -866,6 +866,54 @@ struct ec_params_mkbp_simulate_key {
 	uint8_t pressed;
 } __packed;
 
+/* Configure keyboard scanning */
+#define EC_CMD_MKBP_SET_CONFIG 0x64
+#define EC_CMD_MKBP_GET_CONFIG 0x65
+
+/* flags */
+enum mkbp_config_flags {
+	EC_MKBP_FLAGS_ENABLE = 1,	/* Enable keyboard scanning */
+};
+
+enum mkbp_config_valid {
+	EC_MKBP_VALID_SCAN_PERIOD	= 1 << 0,
+	EC_MKBP_VALID_POLL_TIMEOUT	= 1 << 1,
+	EC_MKBP_VALID_PRE_SCAN		= 1 << 2,
+	EC_MKBP_VALID_POST_SCAN_RELAX	= 1 << 3,
+	EC_MKBP_VALID_OUTPUT_SETTLE	= 1 << 4,
+	EC_MKBP_VALID_DEBOUNCE_DOWN	= 1 << 5,
+	EC_MKBP_VALID_DEBOUNCE_UP	= 1 << 6,
+	EC_MKBP_VALID_DISABLE_WAIT	= 1 << 7,
+	EC_MKBP_VALID_FIFO_MAX_DEPTH	= 1 << 8,
+};
+
+/* Configuration for our key scanning algorithm */
+struct ec_mkbp_config {
+	uint32_t valid_mask;		/* valid fields */
+	uint8_t flags;		/* some flags (enum mkbp_config_flags) */
+	uint8_t valid_flags;		/* which flags are valid */
+	uint16_t scan_period_us;	/* period between scans */
+	/* revert to interrupt mode after no activity for this long */
+	uint32_t poll_timeout_us;
+	uint16_t pre_scan_us;		/* time between irq and first scan */
+	uint16_t post_scan_relax_us;	/* minimum post-scan relax time */
+	/* delay between setting up output and waiting for it to settle */
+	uint16_t output_settle_us;
+	uint16_t debounce_down_us;	/* time for debounce on key down */
+	uint16_t debounce_up_us;	/* time for debounce on key up */
+	uint16_t disable_wait_us;	/* time to wait when disabled */
+	/* maximum depth to allow for fifo (0 = disable) */
+	uint8_t fifo_max_depth;
+} __packed;
+
+struct ec_params_mkbp_set_config {
+	struct ec_mkbp_config config;
+} __packed;
+
+struct ec_response_mkbp_get_config {
+	struct ec_mkbp_config config;
+} __packed;
+
 /*****************************************************************************/
 /* Temperature sensor commands */
 
