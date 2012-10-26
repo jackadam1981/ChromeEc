eacd7066565ce3342b7f4c5a2b40c9170b0f5872
Randall Spangler <rspangler@chromium.org>
2012-10-26 09:29:40 -0700

    Consolidate emergency debug output
    This removes the duplicate uart_emergency_printf() vs. panic_printf()
    / uart_emergency_puts() vs. panic_puts() implementation and saves
    ~0.5kb of code size.
    
    BUG=chrome-os-partner:15579
    BRANCH=none
    TEST=crash unaligned; should print well-formatted crash dump
    
    Change-Id: Ifae756203dd1881806be563308077c1d68302e1f
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- include/uart.h
+++ include/uart.h
@@ -42,17 +42,26 @@ int uart_vprintf(const char *format, va_list args);
 /* Flushes output.  Blocks until UART has transmitted all output. */
 void uart_flush_output(void);
 
+/*****************************************************************************/
+/* Unbuffered output functions, for use at interrupt or panic level */
+
 /* Flushes output.
  *
  * Blocks until UART has transmitted all output, even in a high priority
  * interrupt context. */
-void uart_emergency_flush(void);
+void panic_flush(void);
+
+/* Like uart_puts(), but bypasses the transmit buffer.
+ *
+ * Blocks until UART has transmitted the formatted output, even in a high
+ * priority interrupt context. */
+void panic_puts(const char *outstr);
 
 /* Like uart_printf(), but bypasses the transmit buffer.
  *
  * Blocks until UART has transmitted the formatted output, even in a high
  * priority interrupt context. */
-int uart_emergency_printf(const char *format, ...);
+void panic_printf(const char *format, ...);
 
 /*****************************************************************************/
 /* Input functions
