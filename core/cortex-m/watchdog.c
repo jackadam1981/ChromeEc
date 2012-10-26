d8589648f9e0f9231bee7064a39ddde2fbdd3eb9
Randall Spangler <rspangler@chromium.org>
2012-10-26 12:29:34 -0700

    Clean up core routines - cpu, task, watchdog
    No functional changes.
    
    BUG=chrome-os-partner:15579
    BRANCH=none
    TEST=boot system
    
    Change-Id: I55cf9c60e92177fd441614a8f9fce2d3acca3d0e
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- core/cortex-m/watchdog.c
+++ core/cortex-m/watchdog.c
@@ -5,15 +5,12 @@
 
 /* Watchdog common code */
 
-#include "board.h"
 #include "common.h"
-#include "config.h"
 #include "registers.h"
 #include "task.h"
 #include "timer.h"
 #include "uart.h"
 #include "watchdog.h"
-
 
 void watchdog_trace(uint32_t excep_lr, uint32_t excep_sp)
 {
@@ -45,7 +42,6 @@ void watchdog_trace(uint32_t excep_lr, uint32_t excep_sp)
 	task_print_list();
 	uart_emergency_flush();
 }
-
 
 /* Low priority task to reload the watchdog */
 void watchdog_task(void)
