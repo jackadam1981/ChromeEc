e632c1ff7f12a6466ce443e7449cf9a6075e8557
Vic Yang <victoryang@chromium.org>
2013-02-08 09:53:37 +0800

    spring: Limit charger current by default
    Now that current limit is controlled automatically, we should limit
    current on EC startup. This handles the case where a low power charger
    is plugged in when EC is dead.
    
    BUG=chrome-os-partner:17754
    TEST=none
    BRANCH=none
    
    Change-Id: I161d82d1914ed44924f2af450ed04b1c7dd292cf
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- board/spring/board.c
+++ board/spring/board.c
@@ -102,7 +102,7 @@ const struct gpio_info gpio_list[GPIO_COUNT] = {
 	{"KB_OUT11",    GPIO_C, (1<<6),  GPIO_KB_OUTPUT, NULL},
 	{"KB_OUT12",    GPIO_C, (1<<7),  GPIO_KB_OUTPUT, NULL},
 	{"BOOST_EN",    GPIO_B, (1<<3),  GPIO_OUT_HIGH, NULL},
-	{"ILIM",	GPIO_B, (1<<4),  GPIO_OUT_LOW, NULL},
+	{"ILIM",	GPIO_B, (1<<4),  GPIO_OUT_HIGH, NULL},
 };
 
 /* ADC channels */
