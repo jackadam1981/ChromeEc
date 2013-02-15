5f298671fb642227fc81a8c44a313fbd1edaa1fe
David Hendricks <dhendrix@chromium.org>
2013-02-15 11:51:45 -0800

    Initial McCroskey support
    This adds a board target for McCroskey. It is loosely based on
    Snow.
    
    The clock will be set up differently and the keyboard GPIOs are
    not the same, so some stm32 code needed to be updated.
    
    BUG=none
    BRANCH=none
    TEST=tested on McCroskey (we get to the serial console)
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    
    Change-Id: I5e11a3257fed1797f4cc4dcccf3530585b78da82

---
+++ board/mccroskey/board.h
@@ -0,0 +1,114 @@
+/* Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* McCroskey board configuration */
+
+#ifndef __BOARD_H
+#define __BOARD_H
+
+/* 48 MHz SYSCLK clock frequency */
+/* FIXME(dhendrix): We'll eventually switch to the HSE instead of HSI */
+#define CPU_CLOCK 48000000
+
+/* Use USART1 as console serial port */
+#define CONFIG_CONSOLE_UART 1
+
+/* Debug features */
+#define CONFIG_PANIC_HELP
+#define CONFIG_ASSERT_HELP
+#define CONFIG_CONSOLE_CMDHELP
+
+#undef  CONFIG_TASK_PROFILING
+#define CONFIG_WATCHDOG_HELP
+
+/* use STOP mode when we have nothing to do */
+/*
+ * FIXME(dhendrix): This causes the UART to drop characters and likely
+ * other bad side-effects. Disable for now.
+ */
+#if 0
+#define CONFIG_LOW_POWER_IDLE
+#endif
+
+#ifndef __ASSEMBLER__
+
+/* By default, enable all console messages except keyboard */
+#define CC_DEFAULT	(CC_ALL)
+
+#define USB_CHARGE_PORT_COUNT 0
+
+/* EC drives 13 outputs to keyboard matrix */
+#define KB_OUTPUTS 13
+
+#define I2C_PORT_BATTERY I2C_PORT_HOST
+#define I2C_PORT_CHARGER I2C_PORT_HOST
+#define I2C_PORT_SLAVE 1
+#define CONFIG_ARBITRATE_I2C I2C_PORT_HOST
+
+/* Timer selection */
+#define TIM_CLOCK_MSB 3
+#define TIM_CLOCK_LSB 4
+
+/* GPIO signal list */
+enum gpio_signal {
+	GPIO_KB_IN00,
+	GPIO_KB_IN01,
+	GPIO_KB_IN02,
+	GPIO_KB_IN03,
+	GPIO_KB_IN04,
+	GPIO_KB_IN05,
+	GPIO_KB_IN06,
+	GPIO_KB_IN07,
+	GPIO_KBD_PWR_BUTTON,
+	GPIO_OMZO_RDY_L,
+	GPIO_OZMO_RST_L,
+	GPIO_VBUS_UP_DET,
+	GPIO_OZMO_REQ_L,
+	GPIO_CHARGE_ZERO,
+	GPIO_CHARGE_SHUNT,
+	GPIO_PMIC_INT_L,
+	GPIO_I2C_SCL,
+	GPIO_I2C_SDA,
+	GPIO_KB_OUT00,
+	GPIO_KB_OUT01,
+	GPIO_KB_OUT02,
+	GPIO_KB_OUT03,
+	GPIO_KB_OUT04,
+	GPIO_KB_OUT05,
+	GPIO_KB_OUT06,
+	GPIO_KB_OUT07,
+	GPIO_KB_OUT08,
+	GPIO_KB_OUT09,
+	GPIO_KB_OUT10,
+	GPIO_KB_OUT11,
+	GPIO_KB_OUT12,
+	GPIO_USB_VBUS_CTRL,
+	GPIO_HUB_RESET,
+	GPIO_WRITE_PROTECTn,
+
+	/* FIXME: this will be an alt. function GPIO, so remove it from here */
+	GPIO_BL_PWM,
+#if 0
+	GPIO_STM_USBDM,
+	GPIO_STM_USBDP,
+	GPIO_JTMS_SWDIO,
+	GPIO_JTCK_SWCLK,
+	GPIO_JTDI,
+	GPIO_JTDO,
+	GPIO_JNTRST,
+	GPIO_OSC32_OUT,
+#endif
+	GPIO_COUNT
+};
+
+void configure_board(void);
+
+void matrix_interrupt(enum gpio_signal signal);
+
+void system_warm_reboot(void);
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __BOARD_H */
