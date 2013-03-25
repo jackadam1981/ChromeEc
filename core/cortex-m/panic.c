8b3d29d803fed30d11c89b9b3acb438e5f617f3a
Daisuke Nojiri <dnojiri@google.com>
2013-03-25 16:07:08 -0700

    Dumping stack contents on crash.
    > crash divzero
    
    === EXCEPTION: 06 ====== xPSR: 81000000 ===========
    r0 :00000000 r1 :0000da5c r2 :4000c018 r3 :90000000
    r4 :00000001 r5 :00000000 r6 :200056e4 r7 :00000000
    r8 :00000000 r9 :200056f2 r10:00000000 r11:00000000
    r12:00000001 sp :20002960 lr :00000357 pc :0000035a
    Divide by 0
    mmfs = 2000000, shcsr = 70008, hfsr = 0, dfsr = 0
    
    =========== Process Stack Contents ===========
    20002980: 00000000 00000000 00000000 00000000
    20002990: 00000000 00000000 00000000 00000000
    200029a0: 0000d548 0000d198 0000033d 200056ea
    200029b0: 0000d198 200056e4 00000000 00000000
    
    Rebooting...
    
    BUG=chrome-os-partner:16901
    TEST=build link
    BRANCH=none
    
    Change-Id: I040a9cadf443bfdb3781ffc25a3376bf4aea30ef
    Signed-off-by: Daisuke Nojiri <dnojiri@google.com>

--- core/cortex-m/panic.c
+++ core/cortex-m/panic.c
@@ -234,6 +234,9 @@ static void show_fault(uint32_t mmfs, uint32_t hfsr, uint32_t dfsr)
  */
 static void panic_show_extra(const struct panic_data *pdata)
 {
+	uint32_t psp;
+	int i;
+
 	show_fault(pdata->mmfs, pdata->hfsr, pdata->dfsr);
 	if (pdata->mmfs & CPU_NVIC_MMFS_BFARVALID)
 		panic_printf(", bfar = %x", pdata->bfar);
@@ -243,6 +246,20 @@ static void panic_show_extra(const struct panic_data *pdata)
 	panic_printf("shcsr = %x, ", pdata->shcsr);
 	panic_printf("hfsr = %x, ", pdata->hfsr);
 	panic_printf("dfsr = %x\n", pdata->dfsr);
+
+	/* Print stack contents stored above the exception frame */
+	panic_printf("\n=========== Process Stack Contents ===========\n");
+	psp = pdata->regs[0] + 8 * sizeof(uint32_t);
+	for (i = 0; i < 16; i++) {
+		if (psp >= CONFIG_RAM_BASE + CONFIG_RAM_SIZE)
+			break;
+		if (i % 4 == 0)
+			panic_printf("%08x: ", psp);
+		panic_printf("%08x%c", *(uint32_t *)psp,
+			     i % 4 != 3 ? ' ' : '\n');
+		psp += sizeof(uint32_t);
+	}
+	panic_printf("\n");
 }
 #endif /* CONFIG_PANIC_HELP */
 
@@ -299,7 +316,7 @@ void report_panic(void)
 
 	/* If stack is valid, save exception frame */
 	if (psp >= CONFIG_RAM_BASE &&
-	    psp <= CONFIG_RAM_BASE + CONFIG_RAM_SIZE + 8 * sizeof(uint32_t)) {
+	    psp <= CONFIG_RAM_BASE + CONFIG_RAM_SIZE - 8 * sizeof(uint32_t)) {
 		const uint32_t *sregs = (const uint32_t *)psp;
 		int i;
 
