ec88894d3296b347ced44fde1efdfbdfc370fd9a
Todd Broch <tbroch@chromium.org>
2013-04-03 11:14:58 -0700

    Inhibit host interrupt for battery_key.
    Battery key is used to signal to the host that USB charging status has
    changed.  This virtual keystroke should not wake the device like
    physical keystroke does.
    
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=spring
    BUG=chrome-os-partner:18333
    TEST=manual,
    
    Scenario1:
      suspend device then plug/unplug USB charging.  Device doesn't wake.
      When resume 'cat /sys/class/power_supply/cros_ec-charger/online' is
      correct.
    
    Scenario2:
      while true ; do
        cat /sys/class/power_supply/cros_ec-charger/online
      done
    
      plug/unplug USB charging and device online status changes in a
      timely manner
    
    Change-Id: If37c1d3195ab6eb1e3f13daaaf7b9a3ef452d893
    Original-Change-Id: I8938798b30e70c0c5021405d4fc5da9ce398c311
    Reviewed-on: https://gerrit.chromium.org/gerrit/47251
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

--- chip/stm32/keyboard_scan.c
+++ chip/stm32/keyboard_scan.c
@@ -689,7 +689,9 @@ void keyboard_send_battery_key()
 {
 	mutex_lock(&scanning_enabled);
 	debounced_state[BATTERY_KEY_COL] ^= BATTERY_KEY_ROW_MASK;
-	if (kb_fifo_add(debounced_state) == EC_SUCCESS)
+	/* Add to FIFO only if AP is on or else it will wake from suspend */
+	if ((chipset_in_state(CHIPSET_STATE_ON)) &&
+	    kb_fifo_add(debounced_state) == EC_SUCCESS)
 		board_interrupt_host(1);
 	else
 		CPRINTF("dropped battery keystroke\n");
