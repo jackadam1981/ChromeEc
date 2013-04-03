934c5e51cb57dad08ba2c3e8d63a6b999e64b959
Daisuke Nojiri <dnojiri@google.com>
2013-04-02 17:49:59 -0700

    Introducing MPU module for Cortex-M3
    Preventing instruction fetch from RAM for Link except iram.text, which is used
    for hibernation. Stm32 on Snow does not support MPU.
    
    Tested on Link using commands 'crash nxtext/nxdata/nxstack', which run code from
    .iram.text, .data section, and the stack, respectively:
    
    ...
    RAM lock down successful.
    ...
    
    > crash nxtext
    Running from 20005984
    
    > crash nxdata
    
    === PROCESS EXCEPTION: 04 ====== xPSR: 61000000 ===
    r0 :00000000 r1 :0000dff2 r2 :00000005 r3 :0000086d
    r4 :00000000 r5 :00000032 r6 :2000544c r7 :00000000
    r8 :00000000 r9 :20005456 r10:00000000 r11:00000000
    r12:20005961 sp :20002748 lr :000008d7 pc :20005960
    Instruction access violation
    mmfs = 1, shcsr = 70001, hfsr = 0, dfsr = 0
    
    =========== Process Stack Contents ===========
    200027b0: 0000086d 00000002 0000d504 00009f27
    200027c0: 2000544c 20005452 00000000 00000000
    200027d0: 00000000 00000000 00000000 00000000
    200027e0: 00000000 00000000 00000000 00000cbb
    
    Rebooting...
    
    > crash nxstack
    
    === PROCESS EXCEPTION: 04 ====== xPSR: 20000200 ===
    r0 :00000070 r1 :00000047 r2 :00000000 r3 :200027a8
    r4 :00000000 r5 :00000001 r6 :2000544c r7 :00000000
    r8 :00000000 r9 :20005456 r10:00000000 r11:00000000
    r12:00000002 sp :20002740 lr :00000913 pc :200027ac
    Instruction access violation
    mmfs = 1, shcsr = 70001, hfsr = 0, dfsr = 0
    
    =========== Process Stack Contents ===========
    200027ac: 00000070 00000047 00000002 0000d57c
    200027bc: 00009f9f 2000544c 20005452 00000000
    200027cc: 00000000 00000000 00000000 00000000
    200027dc: 00000000 00000000 00000000 00000000
    
    Rebooting...
    
    BUG=chrome-os-partner:16904
    BRANCH=master
    TEST=stated above
    
    Change-Id: I7c6593c527f29609442f33550f9d16755f32297c
    Signed-off-by: Daisuke Nojiri <dnojiri@google.com>

--- common/system_common.c
+++ common/system_common.c
@@ -13,6 +13,7 @@
 #include "hooks.h"
 #include "host_command.h"
 #include "lpc.h"
+#include "mpu.h"
 #include "panic.h"
 #include "system.h"
 #include "task.h"
@@ -210,7 +211,26 @@ const uint8_t *system_get_jump_tag(uint16_t tag, int *version, int *size)
 
 void system_disable_jump(void)
 {
+	int mpu_error;
+
 	disable_jump = 1;
+
+#ifdef CONFIG_MPU
+	/*
+	 * Lock down memory
+	 * TODO: Lock down other images (RO or RW) not running.
+	 */
+	mpu_error = mpu_protect_ram();
+	if (mpu_error == EC_SUCCESS) {
+		mpu_enable();
+		CPRINTF("RAM lock down successful, excluding %08x-%08x\n",
+			&__iram_text_start, &__iram_text_end);
+	} else {
+		CPRINTF("RAM lock down failed. mpu_type:%08x. error:%d.\n",
+			mpu_get_type(), mpu_error);
+	}
+#endif
+
 }
 
 enum system_image_copy_t system_get_image_copy(void)
