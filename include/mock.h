15b71029baf957aa42b0e6ab898f43db87fc409a
Vic Yang <victoryang@chromium.org>
2013-04-07 15:58:58 +0800

    Add macros for mocking functions
    This introduces two symbols for mocking functions in existing code:
      - test_mockable
      - test_mockable_static
    
    BUG=chrome-os-partner:18598
    TEST=none
    BRANCH=none
    
    Change-Id: Ia7251a9b609136c8f3b155c221634bac7dcb1d68
    Signed-off-by: Vic Yang <victoryang@chromium.org>

---
+++ include/mock.h
@@ -0,0 +1,12 @@
+/* Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Macros for mocking functions */
+
+#ifndef __CROS_EC_MOCK_H
+#define __CROS_EC_MOCK_H
+
+
+#endif  /* __CROS_EC_MOCK_H */
