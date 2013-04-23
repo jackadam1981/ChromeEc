07f5bef29c19de6033be6ade09afe8e215dce400
Bill Richardson <wfrichar@chromium.org>
2013-04-23 13:36:00 -0700

    Allow building without battery charging
    Some experimental boards may want to build without a battery charging task
    to make bringup easier.
    
    BUG=chrome-os-partner:18343
    TEST=build all boards
    BRANCH=none
    
    Change-Id: I4269fea4046325241ad7720ec3457b0534aadda3
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>

--- chip/lm4/switch.c
+++ chip/lm4/switch.c
@@ -373,8 +373,10 @@ static void state_machine(uint64_t tnow)
 		 * Otherwise we could power on the AP only to shut it right
 		 * back down due to insufficient battery.
 		 */
+#ifdef CONFIG_BATTERY_LINK
 		if (charge_get_state() == PWR_STATE_INIT)
 			break;
+#endif
 
 		/*
 		 * Power the system on if possible.  Gating due to insufficient
