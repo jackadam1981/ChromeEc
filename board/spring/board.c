7481a1a39ddbb64759d515cc9e39a762993df60c
Vincent Palatin <vpalatin@chromium.org>
2013-04-29 09:18:18 -0700

    spring: new TPS65090 parameters to avoid cap ringing
    Starting DVT, Spring boards have updated hardware to prevent cap ringing
    when a charger is plugged.
    Adjust T01, T12, T23 and T34 current limit to 87.5% as expected by this
    mechanism.
    
    On pre-DVT boards, this will just reduce the max charging rate by 12.5%.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=spring
    BUG=chrome-os-partner:18987
    TEST=on Spring EVT, use "pmu" command on the EC console and see
    registers 0x5 to 0x9 contain : "26 4e be f6 e7"
    
    Change-Id: Ie078a38251cd2cb50ee605c34a6818bf994d148d

--- board/spring/board.c
+++ board/spring/board.c
@@ -274,6 +274,13 @@ int board_pmu_init(void)
 {
 	int failure = 0;
 
+	/* Tune charging parameters to fix cap ringing on DVT+ hardware */
+	failure |= pmu_set_term_current(RANGE_T01, TERM_I0875);
+	failure |= pmu_set_term_current(RANGE_T12, TERM_I0875);
+	failure |= pmu_set_term_current(RANGE_T23, TERM_I0875);
+	failure |= pmu_set_term_current(RANGE_T34, TERM_I0875);
+	failure |= pmu_set_term_current(RANGE_T40, TERM_I1000);
+
 	/* Set fast charging timeout to 6 hours*/
 	if (!failure)
 		failure = pmu_set_fastcharge(TIMEOUT_6HRS);
@@ -287,25 +294,6 @@ int board_pmu_init(void)
 	/* Set NOITERM bit */
 	if (!failure)
 		failure = pmu_low_current_charging(1);
-
-	/*
-	 * High temperature charging
-	 *   termination voltage: 2.1V
-	 *   termination current: 100%
-	 */
-	if (!failure)
-		failure = pmu_set_term_voltage(RANGE_T34, TERM_V2100);
-	if (!failure)
-		failure = pmu_set_term_current(RANGE_T34, TERM_I1000);
-	/*
-	 * Standard temperature charging
-	 *   termination voltage: 2.1V
-	 *   termination current: 100%
-	 */
-	if (!failure)
-		failure = pmu_set_term_voltage(RANGE_T23, TERM_V2100);
-	if (!failure)
-		failure = pmu_set_term_current(RANGE_T23, TERM_I1000);
 
 	return failure ? EC_ERROR_UNKNOWN : EC_SUCCESS;
 }
