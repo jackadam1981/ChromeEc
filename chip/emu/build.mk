95686abf8ce9ce850bbb9a28deda45ba3ee6e25e
Vic Yang <victoryang@chromium.org>
2013-05-02 00:37:07 +0800

    RFC: Pthread-based emulator for unit testing
    BUG=None
    TEST=Run utils, pingpong, and mutex tests.
    BRANCH=None
    
    Change-Id: I1f5fcd76aa84bdb46c7d35c5e60ae5d92fd3a319
    Signed-off-by: Vic Yang <victoryang@chromium.org>

---
+++ chip/emu/build.mk
@@ -0,0 +1,12 @@
+# -*- makefile -*-
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# emulator specific files build
+#
+
+CORE:=emu
+
+chip-y=system.o gpio.o console.o
+chip-$(HAS_TASK_KEYSCAN)+=keyboard_raw.o
