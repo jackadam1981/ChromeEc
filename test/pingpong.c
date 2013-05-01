394a169d83028952f279c37318ac0b89345114bd
Vic Yang <victoryang@chromium.org>
2013-05-02 00:37:07 +0800

    Pthread-based emulator for unit testing
    This is the first version of pthread-based RTOS emulator. With this, we
    will be able to test high-level modules entirely on the host machine.
    
    BUG=None
    TEST='make runtests' and see tests passing.
    BRANCH=None
    
    Change-Id: I1f5fcd76aa84bdb46c7d35c5e60ae5d92fd3a319
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- test/pingpong.c
+++ test/pingpong.c
@@ -58,10 +58,15 @@ int TaskTick(void *data)
 	return EC_SUCCESS;
 }
 
-static int command_run_test(int argc, char **argv)
+void run_test(void *d)
 {
 	task_wake(TASK_ID_TICK);
 	task_wake(TASK_ID_TESTA);
+}
+
+static int command_run_test(int argc, char **argv)
+{
+	run_test(NULL);
 	return EC_SUCCESS;
 }
 DECLARE_CONSOLE_COMMAND(runtest, command_run_test,
