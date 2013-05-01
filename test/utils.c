462036665009683c8cf931ea0fe5a8571fddc83a
Vic Yang <victoryang@chromium.org>
2013-05-02 00:37:07 +0800

    Pthread-based emulator for unit testing
    BUG=None
    TEST=Run utils, pingpong, and mutex tests.
    BRANCH=None
    
    Change-Id: I1f5fcd76aa84bdb46c7d35c5e60ae5d92fd3a319
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- test/utils.c
+++ test/utils.c
@@ -89,7 +89,7 @@ static int test_shared_mem(void)
 	for (i = 0; i < 256; ++i) {
 		memset(mem, i, sz);
 		for (j = 0; j < sz; ++j)
-			TEST_ASSERT(mem[j] == i);
+			TEST_ASSERT(mem[j] == (char)i);
 
 		if ((i & 0xf) == 0)
 			msleep(20); /* Yield to other tasks */
@@ -100,7 +100,7 @@ static int test_shared_mem(void)
 	return EC_SUCCESS;
 }
 
-static int command_run_test(int argc, char **argv)
+void test_runner(void *d)
 {
 	error_count = 0;
 
@@ -113,11 +113,15 @@ static int command_run_test(int argc, char **argv)
 
 	if (error_count) {
 		ccprintf("Failed %d tests!\n", error_count);
-		return EC_ERROR_UNKNOWN;
 	} else {
 		ccprintf("Pass!\n");
-		return EC_SUCCESS;
 	}
+}
+
+static int command_run_test(int argc, char **argv)
+{
+	test_runner(NULL);
+	return EC_SUCCESS;
 }
 DECLARE_CONSOLE_COMMAND(runtest, command_run_test,
 			NULL, NULL, NULL);
