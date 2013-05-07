3f7e14fc4490487a627b5f0e292b79c4ad58b4e8
Vic Yang <victoryang@chromium.org>
2013-05-08 02:43:59 +0800

    stm32f: Set ADC single read timeout
    If an ADC read fails and EOC bit is somehow never set, we will be stuck
    in the read function holding mutex lock forever, which is really bad.
    Let's set a timeout for this.
    
    BUG=chrome-os-partner:18997
    TEST=Boot Spring. Check ADC works.
    BRANCH=spring
    
    Change-Id: I19b108326f34f380497606fe92eabfaf0a778bb4
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- chip/stm32/adc.c
+++ chip/stm32/adc.c
@@ -14,6 +14,8 @@
 #include "timer.h"
 #include "util.h"
 
+#define ADC_SINGLE_READ_TIMEOUT 3000 /* 3 ms */
+
 struct mutex adc_lock;
 
 static int watchdog_ain_id;
@@ -170,6 +172,7 @@ int adc_read_channel(enum adc_channel ch)
 	const struct adc_t *adc = adc_channels + ch;
 	int value;
 	int restore_watchdog = 0;
+	timestamp_t st;
 
 	if (!adc_powered())
 		return EC_ERROR_UNKNOWN;
@@ -190,9 +193,14 @@ int adc_read_channel(enum adc_channel ch)
 	STM32_ADC_CR2 |= (1 << 0); /* ADON */
 
 	/* Wait for EOC bit set */
-	while (!adc_conversion_ended())
-		;
-	value = STM32_ADC_DR & ADC_READ_MAX;
+	st = get_time();
+	value = ADC_READ_ERROR;
+	do {
+		if (adc_conversion_ended()) {
+			value = STM32_ADC_DR & ADC_READ_MAX;
+			break;
+		}
+	} while (get_time().val - st.val < ADC_SINGLE_READ_TIMEOUT);
 
 	if (restore_watchdog)
 		adc_enable_watchdog_no_lock();
