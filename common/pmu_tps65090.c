58b59fe89ff1c22bbeb3dcb026700998d5749b42
Vic Yang <victoryang@chromium.org>
2013-05-20 16:25:57 +0800

    Reset charge state machine on charge timeout
    For Spring, the charging time can be quite long and TPS65090 ends up in
    timeout state and stops charging. Let's put charge state machine back to
    re-init so that the device continues charging after checking charging
    condition is good.
    
    BUG=chrome-os-partner:19405
    TEST=Pass charger test
    BRANCH=spring
    
    Change-Id: I838741e7283eb31ed76cf3979dbad7f070947aea
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- common/pmu_tps65090.c
+++ common/pmu_tps65090.c
@@ -67,6 +67,8 @@
 /* A temperature threshold to force charger hardware error */
 #define CG_TEMP_THRESHOLD_ERROR 0
 
+/* Timeout indication */
+#define STATUS_TIMEOUT       0xc
 
 /* IRQ events */
 #define EVENT_VACG    (1 << 1) /* AC voltage good */
@@ -190,6 +192,15 @@ int pmu_is_charger_alarm(void)
 	 * non-critical and wait for the next read without send the alert.
 	 */
 	if (!pmu_read(CG_STATUS1, &status) && (status & CHARGER_ALARM))
+		return 1;
+	return 0;
+}
+
+int pmu_is_charge_timeout(void)
+{
+	int status;
+
+	if (!pmu_read(CG_STATUS1, &status) && (status & STATUS_TIMEOUT))
 		return 1;
 	return 0;
 }
