f7545db4c934af458eda30a30052469f2eddea00
Randall Spangler <rspangler@chromium.org>
2013-07-11 14:30:41 -0700

    Split wireless power/radio control out of switch.c
    Chipset control of wireless power uses the new API instead of overriding
    the wireless power itself.
    
    Refactor board-specific support for it to just a few config #defines
    instead of board-specific functions.  This makes some assumptions
    about the polarity of the enable signals.  Not making those
    assumptions would require defining an array of structs or some other
    heavier-weight board-specific info.  Since the assumptions hold for
    all current boards, let's make them now because this is a step in the
    right direction, and reserve doing something more general until we
    actually have a use case for it (so we build in just the flexibility
    we need).
    
    BUG=chrome-os-partner:18343
    BRANCH=none
    TEST=build all platforms; see that link wifi turns on at boot and off at
         shutdown (verify via 'gpioget' from EC console)
    
    Change-Id: Ic036e76158198d2d5e3dd244c3c7b9b1e8d62982
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- board/falco/board.h
+++ board/falco/board.h
@@ -37,6 +37,7 @@
 #define CONFIG_PWM_FAN
 #define CONFIG_TEMP_SENSOR
 #define CONFIG_USB_PORT_POWER_DUMB
+#define CONFIG_WIRELESS
 
 #ifndef __ASSEMBLER__
 
@@ -208,6 +209,11 @@ enum board_version {
 	BOARD_VERSION_PROTO1 = 0,
 	BOARD_VERSION_EVT = 1,
 };
+
+/* Wireless signals */
+#define WIRELESS_GPIO_WLAN GPIO_WLAN_OFF_L
+#define WIRELESS_GPIO_WWAN GPIO_PP3300_LTE_EN
+#define WIRELESS_GPIO_WLAN_POWER GPIO_PP3300_WLAN_EN
 
 #endif /* !__ASSEMBLER__ */
 
