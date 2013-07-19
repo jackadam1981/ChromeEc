48b3037b8285c2928d555c406ed2c12b4f885d67
Bill Richardson <wfrichar@chromium.org>
2013-07-19 12:40:54 -0700

    Add mock_charger.c for use in simulation tests
    I'm gonna need this to test some charger changes.
    
    BUG=chrome-os-partner:20739
    BRANCH=falco,peppy
    TEST=manual
    
      make BOARD=${BOARD} runtests
    
    Change-Id: Ic6f6796bbcdc2a3fdaef263ba167d346c1f7e44f
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>

--- board/host/board.h
+++ board/host/board.h
@@ -19,6 +19,7 @@
 #define CONFIG_LID_SWITCH
 #define CONFIG_POWER_BUTTON
 #define CONFIG_TEMP_SENSOR
+#define CONFIG_MOCK_CHARGER
 
 /* Keyboard protocol */
 #ifdef KB_8042
--- common/build.mk
+++ common/build.mk
@@ -22,6 +22,7 @@ common-$(CONFIG_CHARGER_BQ24725)+=charger_bq24725.o
 common-$(CONFIG_CHARGER_BQ24707A)+=charger_bq24707a.o
 common-$(CONFIG_CHARGER_BQ24738)+=charger_bq24738.o
 common-$(CONFIG_CHARGER_TPS65090)+=pmu_tps65090_charger.o
+common-$(CONFIG_MOCK_CHARGER)+=mock_charger.o
 common-$(CONFIG_CHIPSET_GAIA)+=chipset_gaia.o
 common-$(CONFIG_CHIPSET_HASWELL)+=chipset_haswell.o chipset_x86_common.o
 common-$(CONFIG_CHIPSET_IVYBRIDGE)+=chipset_ivybridge.o chipset_x86_common.o
