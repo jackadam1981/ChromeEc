aaadc71f6229460686007fc6fac9a6d16bc206d7
Doug Anderson <dianders@chromium.org>
2013-07-19 14:36:23 +0530

    WIP: util: Add a util for watching the EC battery
    Not quite ready for comitting.
    
    BRANCH=none
    BUG=None
    TEST=./watchbat.py /dev/pts/10 1 300
      ...and try reading the battery from sysfs while running.
    
    Change-Id: I97d5ee5f78d52c704ddf6bfb73156be9152bd445
    Signed-off-by: Doug Anderson <dianders@chromium.org>

---
+++ util/watchbat.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Script for watching the battery using the EC console.
+
+Sample usage: ./watchbat.py /dev/pts/10 .2 30
+
+TODO: Make this less hacky and create a library to do other things with the EC.
+"""
+
+import ctypes
+import math
+import os
+import select
+import StringIO
+import sys
+import time
+
+def DataAvail(fd):
+  """Return True if there's data ready for reading on the file descriptor."""
+  avail, _, _ = select.select([fd], [], [], 0)
+  return fd in avail
+
+def Clear(fd):
+  """Throw away any existing data on the file descriptor."""
+  while DataAvail(fd):
+    os.read(fd, 1)
+
+def ReadTill(fd, till_ch):
+  """Read until the given character arrives; return all data except till_ch."""
+  sio = StringIO.StringIO()
+  while True:
+    ch = os.read(fd, 1)
+    if ch == till_ch:
+      break
+    sio.write(ch)
+
+  return sio.getvalue()
+
+class EC_Battery(object):
+  def __init__(self, port):
+
+    self._f = open(port, "r+b")
+    self._fd = self._f.fileno()
+
+  @staticmethod
+  def _ParseS16(s):
+    """Parse the first (signed 16-bit) hex value at the start of a string.
+
+    Args:
+      s: The value to parse
+    Returns:
+      val: The value
+    """
+    s = s.split()[0]
+    val = int(s, 0)
+    val = ctypes.c_int16(val).value
+    return val
+
+  def _PollBattery(self):
+    """Poll the battery and return a dictionary of results; may fail sometimes.
+
+    See PollBattery() for details.
+
+    Returns:
+      result: A dictionary containing results; None if we should retry.
+    """
+    Clear(self._fd)
+    os.write(self._fd, "battery\n")
+
+    ec_result = ReadTill(self._fd, ">")
+
+    # If there's a '+' character in the result then the EC probably dropped
+    # characters.  Let's just retry since I don't really trust results then.
+    if '+' in ec_result:
+      return None
+
+    data_lines = (x for x in ec_result.splitlines() if ':' in x)
+    partitioned_data = (x.partition(':') for x in data_lines)
+    raw_dict = dict((k.strip(), v.strip()) for k, _, v in partitioned_data)
+
+    try:
+      result = {}
+      result['current'] = self._ParseS16(raw_dict['I']) / 1000.
+      result['discharging'] = raw_dict['I'].endswith('(DISCHG)')
+      result['voltage'] = self._ParseS16(raw_dict['V']) / 1000.
+      result['power'] = result['current'] * result['voltage']
+      result['remaining'] = int(raw_dict['Remaining'].split()[0]) / 1000.
+    except KeyError:
+      result = None
+
+    return result
+
+  def PollBattery(self):
+    """Poll the battery and return a dictionary of results.
+
+    This sends the 'battery' command to the EC and then tries to parse the
+    result.  It may retry if EC communication had an error.
+
+    Returns:
+      result: A dictionary containing results.  Keys:
+        current: Amps of current flowing to the battery: negative for discharge.
+        discharging: Should be True if current is negative.
+        voltage: Volts of the battery.
+        power: Power going into the battery.  current * voltage.
+        remaining: AmpHours left in the battery.
+    Raises:
+      RuntimeError: if we failed.
+    """
+    tries = 0
+    while True:
+      tries += 1
+
+      result = self._PollBattery()
+      if result is not None:
+        return result
+
+      if tries > 5:
+        raise RuntimeError("Failed to communicate with EC")
+
+def PrintResult(prefix, result):
+  if 'remaining' in result:
+    print "%s%6.3fA %6.3fV %6.3fW (%.3f Ah left)" % (
+      prefix, result['current'], result['voltage'], result['power'],
+      result['remaining']
+    )
+  else:
+    print "%s%6.3fA %6.3fV %6.3fW" % (
+      prefix, result['current'], result['voltage'], result['power'],
+    )
+
+def _Average_Std(nums):
+  """Return the average and sttdev of a list of numbers."""
+  nums = list(nums)
+  mean = sum(nums) / float(len(nums))
+  sum_x2 = sum(x * x for x in nums)
+
+  # TODO: I think this is right for stddev?
+  stdev = math.sqrt((sum_x2 / len(nums)) - (mean * mean))
+
+  return mean, stdev
+
+
+def main(port, poll_every, how_long):
+  print "Opening serial port: %s" % port
+
+  poll_every = float(poll_every)
+  how_long = float(how_long)
+
+  if poll_every < 1:
+    print "WARNING: Less than 1 second polling is probably not useful"
+
+  bat = EC_Battery(port)
+
+  if not bat.PollBattery()['discharging']:
+    print "Please unplug charger and try again"
+    return
+
+  start_time = time.time()
+  this_time = start_time
+  results = []
+  while True:
+    result = bat.PollBattery()
+    PrintResult("% 7.2fs: " % (this_time - start_time), result)
+    results.append(result)
+    this_time += poll_every
+    if this_time > (start_time + how_long):
+      this_time -= poll_every
+      break
+
+    sleep_time = this_time - time.time()
+    if sleep_time < 0:
+      print "ERROR: can't poll that fast!"
+      this_time = time.time()
+      sleep_time = 0
+
+    time.sleep(sleep_time)
+
+  print
+
+  average = {}
+  stdev = {}
+  for k in ('current', 'voltage', 'power'):
+    average[k], stdev[k] = _Average_Std(x[k] for x in results)
+  PrintResult(" Average: ", average)
+  PrintResult(" Std dev: ", stdev)
+
+  amp_hours_used = results[0]['remaining'] - results[-1]['remaining']
+  if amp_hours_used > .003:
+    hours_passed = (this_time - start_time) / (60. * 60.)
+    amps_used = amp_hours_used / hours_passed
+    power_used = amps_used * average['voltage']
+
+    print "Gas gauge reports:"
+    print "  %.3f Ah used" % amp_hours_used
+    print "  %.3f W average" % power_used
+  else:
+    print "Test too short to print gas gauge results"
+
+
+if __name__ == '__main__':
+  # TODO: option parse!
+  main(*sys.argv[1:])
