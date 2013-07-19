e833739b6cf98ca705729dbe6e4edb28cd4dd232
Randall Spangler <rspangler@chromium.org>
2013-07-19 16:26:36 -0700

    Clean up and document PMU config options
    No functional changes, just documenting and renaming.
    
    All boards which use the PMU must provide pmu_board_init().  This was
    already true - except that Daisy's pmu_board_init() was implemented as
    part of pmu_init() instead of its code living in board.c; I've moved
    the code there now.
    
    BUG=chrome-os-partner:18343
    BRANCH=none
    TEST=build all platforms
    
    Change-Id: I85ad06dc3b6287ad917fe13acf83182f24a8f23d
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- common/pmu_tps65090.c
+++ common/pmu_tps65090.c
@@ -588,25 +588,9 @@ void pmu_init(void)
 
 	while (--retries_remaining >= 0) {
 		failure = 0;
-#ifdef CONFIG_PMU_BOARD_INIT
 		if (!failure)
 			failure = pmu_board_init();
-#else
-		/* Init configuration
-		 *   Fast charge timer    : 2 hours
-		 *   Charger              : disable
-		 *   External pin control : enable
-		 *
-		 * TODO: move settings to battery pack specific init
-		 */
-		if (!failure)
-			failure = pmu_write(CG_CTRL0, 2);
-		/* Limit full charge current to 50%
-		 * TODO: remove this temporary hack.
-		 */
-		if (!failure)
-			failure = pmu_write(CG_CTRL3, 0xbb);
-#endif
+
 		/* Enable interrupts */
 		if (!failure) {
 			failure = pmu_write(IRQ1MASK,
