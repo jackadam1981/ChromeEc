d2f495beb021f3f7ba653aadf1c8f2c5677921dc
Rong Chang <rongchang@chromium.org>
2013-07-24 08:22:48 -0700

    Move TPSChrome charging temperature range to battery pack
    This change moves vendor specific temperature ranges to battery pack
    files or board setup files. And added a host test case to verify that
    does not change x86 smart battery charging state machine behavior.
    
    BUG=chrome-os-partner:21181
    BRANCH=None
    TEST=manual
      build test: util/ecmakeall.sh
      hosttests: make hosttests && make runtests
    
    Change-Id: I48e76826b5555f64b78e3c063ce5f02416c72aa2
    Signed-off-by: Rong Chang <rongchang@chromium.org>

--- board/host/board.h
+++ board/host/board.h
@@ -16,6 +16,13 @@
 #define CONFIG_POWER_BUTTON
 #define CONFIG_TEMP_SENSOR
 
+/* Host test config */
+#ifdef SMART_BATTERY_CHARGER
+#define CONFIG_CHARGER
+#define CONFIG_MOCK_BATTERY
+#define CONFIG_CHARGER_INPUT_CURRENT 4032
+#endif
+
 /* Keyboard protocol */
 #ifdef KB_8042
 #define CONFIG_KEYBOARD_PROTOCOL_8042
