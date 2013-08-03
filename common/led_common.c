448228be47bd1f29dd26295c43332dd3a8d55e63
Dave Parker <dparker@chromium.org>
2013-08-02 18:20:57 -0700

    Peppy ectool led command
    Glue between the existing ectool led command and the
    led control logic.
    
    BUG=chrome-os-partner:20776
    BRANCH=peppy
    TEST=Manual. Run "ectool led" commands:
    
    Should pass:
    ectool led power blue|yellow|off|auto|blue=1 yellow=1
    ectool led battery blue|yellow|off|auto|blue=1 yellow=1
    
    Should fail:
    ectool led adapter <color>
    ectool led power|battery red|green|white
    
    Signed-off-by: Dave Parker <dparker@chromium.org>
    Change-Id: I2540940baa553866760dd9ae62278b6b845793ef

---
+++ common/led_common.c
@@ -0,0 +1,96 @@
+/* Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Common functions for blinking LEDs.
+ */
+
+#include "console.h"
+#include "ec_commands.h"
+#include "hooks.h"
+#include "host_command.h"
+#include "led_common.h"
+#include "util.h"
+
+static uint32_t led_auto_control_flags = ~0x00;
+
+static int led_is_supported(enum ec_led_id led_id)
+{
+	int i;
+
+	for (i = 0; i < supported_led_ids_count; i++)
+		if (led_id == supported_led_ids[i])
+			return 1;
+
+	return 0;
+}
+
+void led_auto_control(enum ec_led_id led_id, int enable)
+{
+	uint32_t led_auto_control_flag = 0;
+
+	switch (led_id) {
+	case EC_LED_ID_ADAPTER_LED:
+		led_auto_control_flag = LED_AUTO_CONTROL_ADAPTER;
+		break;
+	case EC_LED_ID_BATTERY_LED:
+		led_auto_control_flag = LED_AUTO_CONTROL_BATTERY;
+		break;
+	case EC_LED_ID_POWER_LED:
+		led_auto_control_flag = LED_AUTO_CONTROL_POWER;
+		break;
+	default:
+		break;
+	}
+
+	if (enable)
+		led_auto_control_flags |= led_auto_control_flag;
+	else
+		led_auto_control_flags &= ~led_auto_control_flag;
+}
+
+int led_auto_control_is_enabled(enum ec_led_id led_id)
+{
+	uint32_t led_auto_control_flag = 0;
+
+	switch (led_id) {
+	case EC_LED_ID_ADAPTER_LED:
+		led_auto_control_flag = LED_AUTO_CONTROL_ADAPTER;
+		break;
+	case EC_LED_ID_BATTERY_LED:
+		led_auto_control_flag = LED_AUTO_CONTROL_BATTERY;
+		break;
+	case EC_LED_ID_POWER_LED:
+		led_auto_control_flag = LED_AUTO_CONTROL_POWER;
+		break;
+	default:
+		break;
+	}
+	return (led_auto_control_flags & led_auto_control_flag) != 0;
+}
+
+static int led_command_control(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_led_control *p = args->params;
+	struct ec_response_led_control *r = args->response;
+	int i;
+
+	args->response_size = sizeof(struct ec_response_led_control);
+	memset(r->brightness_range, 0, sizeof(r->brightness_range));
+
+	if (!led_is_supported(p->led_id))
+		return EC_RES_INVALID_PARAM;
+
+	led_brightness_range(p->led_id, r->brightness_range);
+	for (i = 0; i < EC_LED_COLOR_COUNT; i++)
+		if (r->brightness_range[i] == 0 && p->brightness[i] != 0)
+			return EC_RES_INVALID_PARAM;
+
+	if (p->flags & EC_LED_FLAGS_AUTO)
+		led_auto_control(p->led_id, 1);
+	else if (led_set_brightness(p->led_id, p->brightness) != EC_SUCCESS)
+			return EC_RES_INVALID_PARAM;
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_LED_CONTROL, led_command_control, EC_VER_MASK(1));
