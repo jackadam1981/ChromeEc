8ec66313f89f8c4b1e49da8512e516be9ccd9e93
Hung-ying Tyan <tyanh@chromium.org>
2013-08-16 16:20:20 +0800

    pit: i2c: add wedge command
    This command wedges the I2C bus by writing part of a byte to or reading part
    of the response from the slave device.
    
    BUG=chrome-os-partner:19286
    TEST=Manual test on peach pit. Define config in board/pit/board.h to enable
    the command:
    
    On the EC console, execute the following "i2cwedge" command
    
       i2cwedge 0x90 0 1    (wedge write)
    or
       i2cwedge 0x90 0 2    (wedge read)
    
    and then "battery". Observe that the command reports an error.
    
    Similarly, execute
    
       i2cwedge 0x90 0 5    (wedge write + reboot)
    or
       i2cwedge 0x90 0 6    (wedge read + reboot)
    
    on the EC console and observe a reboot. Then execute "battery" and observe
    that the command works properly.
    BRANCH=pit
    
    Change-Id: I10ccb21b047df907a4dfdbd84c0f582cfa2d939a
    Signed-off-by: Hung-ying Tyan <tyanh@chromium.org>
    Signed-off-by: Doug Anderson <dianders@chromium.org>

--- common/build.mk
+++ common/build.mk
@@ -45,6 +45,7 @@ common-$(CONFIG_FLASH)+=flash_common.o
 common-$(CONFIG_FMAP)+=fmap.o
 common-$(CONFIG_I2C)+=i2c_common.o
 common-$(CONFIG_I2C_ARBITRATION)+=i2c_arbitration.o
+common-$(CONFIG_CMD_I2CWEDGE)+=i2c_wedge.o
 common-$(CONFIG_KEYBOARD_PROTOCOL_8042)+=keyboard_8042.o
 common-$(CONFIG_KEYBOARD_PROTOCOL_MKBP)+=keyboard_mkbp.o
 common-$(CONFIG_KEYBOARD_TEST)+=keyboard_test.o
