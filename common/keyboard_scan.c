76dd849b8c15972cf0809fe09c467fa3fecd47a5
ChromeOS Developer <dparker@chromium.org>
2013-08-23 10:49:57 -0700

    Peppy: Enable PP5000 while checking initial keyboard state
    BRANCH ONLY! Not for ToT.
    
    Active ESD components on the keyboard scan matrix require 5V
    power. These components must be powered to read the keyboard
    state correctly.
    
    BUG=chrome-os-partner:22127
    BRANCH=peppy
    TEST=Manual.
      Boot normally.
      Boot normally with Esc + F3 + Power + another key.
      Boot into recovery with Esc + F3 + Power.
    
    Change-Id: I064cf6eb98b47c897079f145e945b4ab3ae4ef23
    Signed-off-by: Dave Parker <dparker@chromium.org>

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -20,6 +20,11 @@
 #include "timer.h"
 #include "util.h"
 
+#ifdef BOARD_peppy
+/* HORRIBE HACK: DO NOT RE-USE */
+#include "gpio.h"
+#endif /* BOARD_peppy */
+
 /* Console output macros */
 #define CPUTS(outstr) cputs(CC_KEYSCAN, outstr)
 #define CPRINTF(format, args...) cprintf(CC_KEYSCAN, format, ## args)
@@ -462,6 +467,11 @@ const uint8_t *keyboard_scan_get_state(void)
 
 void keyboard_scan_init(void)
 {
+#ifdef BOARD_peppy
+	/* HORRIBE HACK: DO NOT RE-USE */
+	gpio_set_level(GPIO_PP5000_EN, 1);
+#endif /* BOARD_peppy */
+
 	/* Configure GPIO */
 	keyboard_raw_init();
 
@@ -478,6 +488,11 @@ void keyboard_scan_init(void)
 	/* Trigger event if recovery key was pressed */
 	if (boot_key_value == BOOT_KEY_ESC)
 		host_set_single_event(EC_HOST_EVENT_KEYBOARD_RECOVERY);
+
+#ifdef BOARD_peppy
+	/* HORRIBE HACK: DO NOT RE-USE */
+	gpio_set_level(GPIO_PP5000_EN, 0);
+#endif /* BOARD_peppy */
 }
 
 void keyboard_scan_task(void)
