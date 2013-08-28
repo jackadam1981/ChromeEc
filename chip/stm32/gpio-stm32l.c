e562c5e396bacc2b9bb3ddac74da47afe1ebf1d7
Vic Yang <victoryang@chromium.org>
2013-08-28 10:28:08 +0800

    Fix a bug that GPIO cannot be set as input on stm32l
    GPIO_INPUT is defined as 0, and thus the if condition is never true. Fix
    it so that if GPIO_OUTPUT is not set, assume GPIO_INPUT.
    
    BUG=chrome-os-partner:22275
    TEST=Set a GPIO to output and pull it low, and then set it back to
    input. Check it can be pull high externally.
    BRANCH=None (unless this bug hits some other boards.)
    
    Change-Id: I84b9936c24af538ac59c36129fda27ca879bf9d1
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- chip/stm32/gpio-stm32l.c
+++ chip/stm32/gpio-stm32l.c
@@ -57,7 +57,7 @@ void gpio_set_flags_by_mask(uint32_t port, uint32_t mask, uint32_t flags)
 		val |= 0x55555555 & mask2;
 		STM32_GPIO_MODER(port) = val;
 
-	} else if (flags & GPIO_INPUT) {
+	} else {
 		/* Input, MODE=00 */
 		STM32_GPIO_MODER(port) = val;
 	}
