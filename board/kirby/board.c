018700acee5cceec3dba0360087e629258c68b3b
Vic Yang <victoryang@chromium.org>
2013-08-29 16:48:36 +0800

    Add Kirby ADC channel definition
    Kirby has the same ADC channels as Spring, but with different scales and
    pin assignments.
    
    BUG=chrome-os-partner:22055
    TEST=Read ADC channel voltage
    BRANCH=None
    
    Change-Id: Ibb618a77647c0477eeb0e8d983df07a258fdb75a
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- board/kirby/board.c
+++ board/kirby/board.c
@@ -4,6 +4,7 @@
  */
 /* Pit board-specific configuration */
 
+#include "adc.h"
 #include "battery_pack.h"
 #include "common.h"
 #include "extpower.h"
@@ -14,6 +15,7 @@
 #include "lid_switch.h"
 #include "registers.h"
 #include "spi.h"
+#include "stm32_adc.h"
 #include "task.h"
 #include "util.h"
 
@@ -111,6 +113,24 @@ const struct battery_temperature_ranges bat_temp_ranges = {
 	.discharging_min_c    = 0,
 	.discharging_max_c    = 100,
 };
+
+/* ADC channels */
+const struct adc_t adc_channels[] = {
+	/*
+	 * VBUS voltage sense pin.
+	 * Sense pin 1.8V is converted to 4096. Accounting for the 3x
+	 * voltage divider, the conversion factor is 5400mV/4096.
+	 */
+	[ADC_CH_USB_VBUS_SNS] = {"USB_VBUS_SNS", 5400, 4096, 0, STM32_AIN(12)},
+	/*
+	 * Micro USB D+ sense pin. Voltage divider = 2/3.
+	 * Converted to mV (2700mV/4096).
+	 */
+	[ADC_CH_USB_DP_SNS] = {"USB_DP_SNS", 2700, 4096, 0, STM32_AIN(10)},
+	/* Micro USB D- sense pin. Same scale as for D+. */
+	[ADC_CH_USB_DN_SNS] = {"USB_DN_SNS", 2700, 4096, 0, STM32_AIN(11)},
+};
+BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
 /* I2C ports */
 const struct i2c_port_t i2c_ports[] = {
