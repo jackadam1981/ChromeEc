f1e1259ac6e2d13e765c0cb5b26d5363815cdcc2
Daisuke Nojiri <dnojiri@google.com>
2013-10-10 11:51:44 -0700

    Disable EC jump after RW image starts
    BUG=None
    Signed-off-by: dnojiri@google.com
    Change-Id: If1a3cf6158b3bc97c965298d2ab958b5fa7a5d7e

--- common/system_common.c
+++ common/system_common.c
@@ -217,11 +217,10 @@ void system_disable_jump(void)
 	disable_jump = 1;
 
 #ifdef CONFIG_MPU
-	/*
-	 * Lock down memory
-	 * TODO: Lock down other images (RO or RW) not running.
-	 */
-	{
+	if (system_is_locked()) {
+		/*
+		 * Protect memory from code execution
+		 */
 		int mpu_error = mpu_protect_ram();
 		if (mpu_error == EC_SUCCESS) {
 			mpu_enable();
@@ -231,6 +230,10 @@ void system_disable_jump(void)
 			CPRINTF("Failed to lock RAM. mpu_type:%08x. error:%d\n",
 				mpu_get_type(), mpu_error);
 		}
+		/*
+		 * Protect the other image from code execution
+		 * TODO: https://chromium-review.googlesource.com/#/c/169050/
+		 */
 	}
 #endif
 }
