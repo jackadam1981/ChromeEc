826767ac16bafaf5db80b0d796bb38e16d8390d7
Vincent Palatin <vpalatin@chromium.org>
2013-10-25 15:37:11 -0700

    ite: Port OS layer to Andestar v3m architecture
    This will be used to support ITE IT8380 chip which contains an Andes
    N801 core.
    
    ! Work In Progress ! mostly the skeleton for now
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=chrome-os-partner:23574
    TEST=make BOARD=it8380dev
    
    Change-Id: I91f9380c51c7712aa6a6418223a11551ab0091ce

---
+++ core/nds32/atomic.h
@@ -0,0 +1,44 @@
+/* Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Atomic operations for Andes */
+
+#ifndef __CROS_EC_ATOMIC_H
+#define __CROS_EC_ATOMIC_H
+
+#include "common.h"
+
+static inline void atomic_clear(uint32_t *addr, uint32_t bits)
+{
+	/* TODO(crosbug.com/p/23574): IMPLEMENT ME ! */
+	*addr &= ~bits;
+}
+
+static inline void atomic_or(uint32_t *addr, uint32_t bits)
+{
+	/* TODO(crosbug.com/p/23574): IMPLEMENT ME ! */
+	*addr |= bits;
+}
+
+static inline void atomic_add(uint32_t *addr, uint32_t value)
+{
+	/* TODO(crosbug.com/p/23574): IMPLEMENT ME ! */
+	*addr += value;
+}
+
+static inline void atomic_sub(uint32_t *addr, uint32_t value)
+{
+	/* TODO(crosbug.com/p/23574): IMPLEMENT ME ! */
+	*addr -= value;
+}
+
+static inline uint32_t atomic_read_clear(uint32_t *addr)
+{
+	/* TODO(crosbug.com/p/23574): IMPLEMENT ME ! */
+	uint32_t val = *addr;
+	*addr = 0;
+	return val;
+}
+#endif  /* __CROS_EC_ATOMIC_H */
