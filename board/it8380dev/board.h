da90c332d8f96b7339874d1113ee6e251f6e6bfc
Vincent Palatin <vpalatin@chromium.org>
2013-10-25 15:33:41 -0700

    ite: Add skeleton for ITE IT8380 support
    Initial support for the ITE IT8380 chip with all function stubbed,
    also their dev board configuration as a test vehicle.
    
    Skip that new board in make_all.sh testing to avoid issues for people
    not having the toolchain yet.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=chrome-os-partner:23575
    TEST=make BOARD=it8380dev
    
    Change-Id: Id4bf37d1beb21d1a4bee404c9a0bc500025fe787

---
+++ board/it8380dev/board.h
@@ -0,0 +1,25 @@
+/* Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* IT8380 development board configuration */
+
+#ifndef __BOARD_H
+#define __BOARD_H
+
+#ifndef __ASSEMBLER__
+
+/* stubbed features */
+#undef CONFIG_LID_SWITCH
+
+enum gpio_signal {
+	/* Unimplemented GPIOs */
+	GPIO_ENTERING_RW,
+
+	/* Number of GPIOs; not an actual GPIO */
+	GPIO_COUNT
+};
+
+#endif /* !__ASSEMBLER__ */
+#endif /* __BOARD_H */
