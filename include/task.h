d77ce88c4cdd7fe2d47e81a660e19eef3d22bdde
Vincent Palatin <vpalatin@chromium.org>
2013-10-25 15:37:11 -0700

    ite: Port OS layer to Andestar v3m architecture
    This will be used to support ITE IT8380 chip which contains an Andes
    N801 core.
    
    ! Work In Progress ! mostly the skeleton for now
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=chrome-os-partner:23574
    TEST=make BOARD=it8380dev
    
    Change-Id: I91f9380c51c7712aa6a6418223a11551ab0091ce

--- include/task.h
+++ include/task.h
@@ -203,6 +203,7 @@ struct irq_priority {
  * Macro to connect the interrupt handler "routine" to the irq number "irq" and
  * ensure it is enabled in the interrupt controller with the right priority.
  */
+#ifndef __nds32__
 #define DECLARE_IRQ(irq, routine, priority)                     \
 	void IRQ_HANDLER(irq)(void)				\
 	{							\
@@ -214,5 +215,14 @@ struct irq_priority {
 	const struct irq_priority IRQ_PRIORITY(irq)		\
 	__attribute__((section(".rodata.irqprio")))		\
 			= {irq, priority}
+#else /* NDS32 arch */
+/* TODO(crosbug.com/p/23574): CLEAN ME ! */
+#define DECLARE_IRQ(irq, routine, priority)                     \
+	void IRQ_HANDLER(irq)(void)				\
+		__attribute__ ((alias (STRINGIFY(routine))));	\
+	const struct irq_priority IRQ_PRIORITY(irq)		\
+	__attribute__((section(".rodata.irqprio")))		\
+			= {irq, priority}
+#endif
 
 #endif  /* __CROS_EC_TASK_H */
