9d0b55e17a821adde8ebe0179774327a3a3ab9df
Hung-Te Lin <hungte@chromium.org>
2013-10-29 17:11:52 +0800

    nyan: Correct chipset configuration.
    Nyan is using LTM32L15x, not LTM32L100.
    
    BUG=none
    TEST=Successfully loaded and boots on Nyan.
    BRANCH=none
    
    Change-Id: I2d1c68cfb9638ef162561cdc9b447887a248c855
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- board/nyan/build.mk
+++ board/nyan/build.mk
@@ -5,9 +5,9 @@
 #
 # Board specific files build
 
-# the IC is STmicro STM32L100RBT6
+# the IC is STmicro STM32L151R8
 CHIP:=stm32
 CHIP_FAMILY:=stm32l
-CHIP_VARIANT:=stm32l100
+CHIP_VARIANT:=stm32l15x
 
 board-y=board.o battery.o
