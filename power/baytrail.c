3407d20fe6ddf3d857e0b7a72a2ccee2876a8465
Randall Spangler <rspangler@chromium.org>
2013-10-29 14:34:09 -0700

    WIP - enable low power idle on Rambi
    Change-Id: Ifce0b4d50599e11aa0ea2206f60b4ef26ee29cde
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- power/baytrail.c
+++ power/baytrail.c
@@ -115,6 +115,10 @@ enum x86_state x86_chipset_init(void)
 	if (system_jumped_to_this_image()) {
 		if ((x86_get_signals() & IN_ALL_S0) == IN_ALL_S0) {
 			CPRINTF("[%T x86 already in S0]\n");
+
+			/* Disable idle task deep sleep when in S0. */
+			disable_sleep(SLEEP_MASK_AP_RUN);
+
 			return X86_S0;
 		} else {
 			/* Force all signals to their G3 states */
@@ -267,6 +271,12 @@ enum x86_state x86_handle_state(enum x86_state state)
 		/* Call hooks now that rails are up */
 		hook_notify(HOOK_CHIPSET_RESUME);
 
+		/*
+		 * Disable idle task deep sleep. This means that the low power
+		 * idle task will not go into deep sleep while in S0.
+		 */
+		disable_sleep(SLEEP_MASK_AP_RUN);
+
 		/* Wait 100ms after all voltages good */
 		msleep(100);
 
@@ -297,6 +307,12 @@ enum x86_state x86_handle_state(enum x86_state state)
 
 		/* Disable wireless */
 		wireless_enable(0);
+
+		/*
+		 * Enable idle task deep sleep.  Allow the low power idle task
+		 * to go into deep sleep in S3 or lower.
+		 */
+		enable_sleep(SLEEP_MASK_AP_RUN);
 
 		/*
 		 * Deassert prochot since CPU is off and we're about to drop
