3b29071c9eba9e044dc4a47254ffdabfef057d72
Duncan Laurie <dlaurie@chromium.org>
2013-11-12 14:12:10 +0800

    samus: Fix backlight panel interrupt
    The backlight_interrupt() function is defined to NULL if the magic
    CONFIG_BACKLIGHT_REQ_GPIO is not defined.
    
    Enabling that exposed an issue where the backlight workaround was
    attempted in interrupt context and should instead be deferred since
    it involves i2c transactions.
    
    BUG=chrome-os-partner:23752
    BRANCH=none
    TEST=build and boot on samus proto1b and see recovery screen
    
    Change-Id: Id1377033c791a5c279fdb4faeecc4b2c0d142eaa
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>

--- board/samus/panel.c
+++ board/samus/panel.c
@@ -21,7 +21,7 @@
 /**
  * Enable PWM mode in backlight controller and turn it on.
  */
-static int lp8555_enable_pwm_mode(void)
+static void lp8555_enable_pwm_mode(void)
 {
 	int reg;
 
@@ -39,9 +39,8 @@ static int lp8555_enable_pwm_mode(void)
 	reg |= LP8555_REG_COMMAND_ON;
 	i2c_write8(I2C_PORT_BACKLIGHT, I2C_ADDR_BACKLIGHT,
 		   LP8555_REG_COMMAND, reg);
-
-	return EC_SUCCESS;
 }
+DECLARE_DEFERRED(lp8555_enable_pwm_mode);
 
 /**
  * Host command to toggle backlight.
@@ -67,7 +66,7 @@ void backlight_interrupt(enum gpio_signal signal)
 	 * PCH indicates it is turning on backlight so we should
 	 * attempt to put the backlight controller into PWM mode.
 	 */
-	lp8555_enable_pwm_mode();
+	hook_call_deferred(lp8555_enable_pwm_mode, 0);
 }
 
 /**
