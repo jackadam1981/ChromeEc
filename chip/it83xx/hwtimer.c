e1f4ccc993b537f1648ace8e86391e4e7346c230
Vincent Palatin <vpalatin@chromium.org>
2013-11-12 17:00:50 -0800

    DEBUG: example code
    !!! DO NOT SUBMIT !!!
    
    how to use UART with panic_printf
    and put your own code
    
    demonstrate a slow IRQ firing from the timer
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I2699ac1f3516f9ce59682d9d3c216d23e948714e

--- chip/it83xx/hwtimer.c
+++ chip/it83xx/hwtimer.c
@@ -8,6 +8,7 @@
 #include "common.h"
 #include "hooks.h"
 #include "hwtimer.h"
+#include "panic.h"
 #include "registers.h"
 #include "task.h"
 #include "timer.h"
@@ -46,6 +47,7 @@ void __hw_clock_source_set(uint32_t ts)
 
 static void __hw_clock_source_irq(void)
 {
+	panic_puts("HWTMR\n");
 	/* clear interrupt status */
 	IT83XX_INTC_ISR7 = 0x40;
 
@@ -60,7 +62,7 @@ static void __hw_clock_source_irq(void)
 	else if (time_us == 0)
 		process_timers(1);
 }
-DECLARE_IRQ(IT83XX_IRQ_TMR_B0, __hw_clock_source_irq, 1);
+DECLARE_IRQ(1 /*should 3 the CPU_INT number for IT83XX_IRQ_TMR_B0 */, __hw_clock_source_irq, 1);
 
 static void setup_gpio(void)
 {
@@ -89,7 +91,7 @@ int __hw_clock_source_init(uint32_t start_t)
 
 	/* Set prescaler divider value (/8 for B and /1 for A). */
 	/* TODO: depends on clock source */
-	IT83XX_TMR_PRSC = 0x14;
+	IT83XX_TMR_PRSC = 0xFF;
 
 	/*
 	 * Tim A: 16 bit pulse mode, 8MHz clock
@@ -101,7 +103,7 @@ int __hw_clock_source_init(uint32_t start_t)
 	/* Set the 16-bit cycle time, duty time for timers. */
 	IT83XX_TMR_CTR_A0 = 0xff;
 	IT83XX_TMR_CTR_A1 = 0xff;
-	IT83XX_TMR_CTR_B0 = 0x08;	// cycle set for 1us
+	IT83XX_TMR_CTR_B0 = 0xFF;	// cycle set for 1us
 	IT83XX_TMR_DCR_B0 = 0x04;
 
 	/* Enable the cycle time interrupt for timer B0. */
