b5f6ae25a3722edd590974e0f89e5e761e810acc
Alec Berg <alecaberg@chromium.org>
2013-11-14 10:53:56 -0800

    ite: GPIO module
    Start of GPIO module. This sets up the general properties of GPIOs,
    input/output, pull-up/pull-down, use alternate function or not.
    Also supports setting interrupts on edges.
    
    This CL also adds a bunch of LED output points and switch inputs for
    the it8380dev eval board.
    
    BRANCH=none
    BUG=chrome-os-partner:23575
    TEST=Used LEDs as outputs and switch as inputs to test all
    GPIO activity. Tested: setting outputs, reading inputs, setting
    pull-up/down, using alternate function for UART, and setting
    an edge interrupt on an input.
    
    Change-Id: I94f697342593bfcf18f6f470b5fc38bb39701a32
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- board/it8380dev/board.c
+++ board/it8380dev/board.c
@@ -11,20 +11,55 @@
 #include "task.h"
 #include "util.h"
 
+/* Test GPIO interrupt function that toggles one LED. */
+void test_interrupt(enum gpio_signal signal)
+{
+	static int led0_state = 0;
+
+	/* toggle LED */
+	led0_state = !led0_state;
+	gpio_set_level(GPIO_L_LED0, led0_state);
+}
+
 /* GPIO signal list.  Must match order from enum gpio_signal. */
 /*const*/ struct gpio_info gpio_list[] = {
 	/* Unimplemented signals which we need to emulate for now */
 	GPIO_SIGNAL_NOT_IMPLEMENTED("ENTERING_RW"),
+
+	{"A0", GPIO_A, (1<<0), GPIO_OUTPUT | GPIO_LOW},
+	{"A1", GPIO_A, (1<<1), GPIO_OUTPUT | GPIO_LOW},
+	{"A2", GPIO_A, (1<<2), GPIO_OUTPUT | GPIO_LOW},
+	{"A3", GPIO_A, (1<<3), GPIO_OUTPUT | GPIO_LOW},
+	{"A4", GPIO_A, (1<<4), GPIO_OUTPUT | GPIO_LOW},
+	{"A5", GPIO_A, (1<<5), GPIO_OUTPUT | GPIO_LOW},
+	{"A6", GPIO_A, (1<<6), GPIO_OUTPUT | GPIO_LOW},
+	{"A7", GPIO_A, (1<<7), GPIO_OUTPUT | GPIO_LOW},
+	{"I0", GPIO_I, (1<<0), GPIO_OUTPUT | GPIO_LOW},
+	{"I1", GPIO_I, (1<<1), GPIO_OUTPUT | GPIO_LOW},
+	{"I2", GPIO_I, (1<<2), GPIO_OUTPUT | GPIO_LOW},
+	{"I3", GPIO_I, (1<<3), GPIO_OUTPUT | GPIO_LOW},
+	{"I4", GPIO_I, (1<<4), GPIO_OUTPUT | GPIO_LOW},
+	{"I5", GPIO_I, (1<<5), GPIO_OUTPUT | GPIO_LOW},
+	{"I6", GPIO_I, (1<<6), GPIO_OUTPUT | GPIO_LOW},
+	{"I7", GPIO_I, (1<<7), GPIO_OUTPUT | GPIO_LOW},
+
+	{"E0", GPIO_E, (1<<0), GPIO_INPUT | GPIO_INT_F_RISING, test_interrupt},
+	{"E1", GPIO_E, (1<<1), GPIO_INPUT},
+	{"E2", GPIO_E, (1<<2), GPIO_INPUT},
+	{"E3", GPIO_E, (1<<3), GPIO_INPUT},
 };
 BUILD_ASSERT(ARRAY_SIZE(gpio_list) == GPIO_COUNT);
 
 /* Pins with alternate functions */
 /*const*/ struct gpio_alt_func gpio_alt_funcs[] = {
+	{GPIO_B, 0x03, 1, MODULE_UART, GPIO_PULL_UP},	/* UART0 */
 };
 /*const*/ int gpio_alt_funcs_count = ARRAY_SIZE(gpio_alt_funcs);
 
 /* Initialize board. */
 static void board_init(void)
 {
+	gpio_enable_interrupt(GPIO_SW_1);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
--- board/it8380dev/board.h
+++ board/it8380dev/board.h
@@ -16,6 +16,26 @@
 enum gpio_signal {
 	/* Unimplemented GPIOs */
 	GPIO_ENTERING_RW,
+	GPIO_H_LED0,
+	GPIO_H_LED1,
+	GPIO_H_LED2,
+	GPIO_H_LED3,
+	GPIO_H_LED4,
+	GPIO_H_LED5,
+	GPIO_H_LED6,
+	GPIO_H_LED7,
+	GPIO_L_LED0,
+	GPIO_L_LED1,
+	GPIO_L_LED2,
+	GPIO_L_LED3,
+	GPIO_L_LED4,
+	GPIO_L_LED5,
+	GPIO_L_LED6,
+	GPIO_L_LED7,
+	GPIO_SW_1,
+	GPIO_SW_2,
+	GPIO_SW_3,
+	GPIO_SW_4,
 
 	/* Number of GPIOs; not an actual GPIO */
 	GPIO_COUNT
