c77fa74f36324399044b3d0724896ca9a613d68f
Alec Berg <alecaberg@chromium.org>
2013-11-14 10:53:56 -0800

    ite: GPIO module
    Start of GPIO module. This sets up the general properties of GPIOs,
    input/output, pull-up/pull-down, use alternate function or not.
    Also supports setting interrupts on edges.
    
    DO NOT COMMIT
    
    BRANCH=none
    BUG=chrome-os-partner:23575
    TEST=Defined a test output and test input and played with various
    configurations to make sure setting outputs works, reading inputs
    works, setting pull-up/down works, and alternate function works.
    Also, tested edge interrupts on an input pin.
    
    Change-Id: I94f697342593bfcf18f6f470b5fc38bb39701a32
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- board/it8380dev/board.c
+++ board/it8380dev/board.c
@@ -11,15 +11,24 @@
 #include "task.h"
 #include "util.h"
 
+/* Test interrupt function. */
+void c1_interrupt(enum gpio_signal signal)
+{
+	panic_puts("|");
+}
+
 /* GPIO signal list.  Must match order from enum gpio_signal. */
 /*const*/ struct gpio_info gpio_list[] = {
 	/* Unimplemented signals which we need to emulate for now */
 	GPIO_SIGNAL_NOT_IMPLEMENTED("ENTERING_RW"),
+	{"C2", GPIO_C, (1<<2), GPIO_OUTPUT | GPIO_LOW},
+	{"C1", GPIO_C, (1<<1), GPIO_INPUT | GPIO_PULL_UP | GPIO_INT_F_RISING, c1_interrupt},
 };
 BUILD_ASSERT(ARRAY_SIZE(gpio_list) == GPIO_COUNT);
 
 /* Pins with alternate functions */
 /*const*/ struct gpio_alt_func gpio_alt_funcs[] = {
+	{GPIO_B, 0x03, 1, MODULE_UART, GPIO_PULL_UP},	/* UART0 */
 };
 /*const*/ int gpio_alt_funcs_count = ARRAY_SIZE(gpio_alt_funcs);
 
@@ -28,3 +37,4 @@ static void board_init(void)
 {
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
