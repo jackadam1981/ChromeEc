b2f1de9ff6c49c9835a0f53b9fb8b32e4bbe9d45
Alec Berg <alecaberg@chromium.org>
2013-11-14 17:40:35 -0800

    ite: add Clock module
    Implemented a couple of clock module functions. Cleaned up global
    assumption of clock speed.
    
    BRANCH=none
    BUG=chrome-os-partner:23575
    TEST=Tested UART still working.
    
    Signed-off-by: Alec Berg <alecaberg@chromium.org>
    Change-Id: I9296a293ec4dea1711ce66670bba66e6fe2d7c10

--- chip/it83xx/clock.c
+++ chip/it83xx/clock.c
@@ -13,12 +13,75 @@
 #include "timer.h"
 #include "util.h"
 
-/* Console output macros */
+/* Console output macros. */
 #define CPUTS(outstr) cputs(CC_CLOCK, outstr)
 #define CPRINTF(format, args...) cprintf(CC_CLOCK, format, ## args)
 
+static int freq;
+
+struct clock_gate_ctrl {
+	volatile uint8_t *reg;
+	uint8_t mask;
+};
+
 void clock_init(void)
 {
-	/* Turn off auto clock gating */
+#if PLL_CLOCK == 48000000
+	/* Set PLL frequency to 48MHz. */
+	IT83XX_ECPM_PLLFREQR = 0x04;
+	freq = PLL_CLOCK;
+#else
+#error "Support only for PLL clock speed of 48MHz."
+#endif
+
+	/* Set EC Clock Frequency to PLL frequency. */
+	IT83XX_ECPM_SCDCR3 &= 0xf0;
+
+	/* Turn off auto clock gating. */
 	IT83XX_ECPM_AUTOCG = 0x00;
+}
+
+int clock_get_freq(void)
+{
+	return freq;
+}
+
+/**
+ * Enable clock to specified peripheral
+ *
+ * @param offset Should be element of clock_gate_offsets enum.
+ *               Bits 8-15 specify the ECPM offset of the specific clock reg.
+ *               Bits 0-7 specify the mask for the clock register.
+ * @param mask   Unused
+ * @param mode   Unused
+ */
+void clock_enable_peripheral(uint32_t offset, uint32_t mask, uint32_t mode)
+{
+	volatile uint8_t * reg = (volatile uint8_t *)
+			(IT83XX_ECPM_BASE + (offset >> 8));
+	uint8_t reg_mask = offset & 0xff;
+
+	*reg &= reg_mask;
+}
+
+/**
+ * Disable clock to specified peripheral
+ *
+ * @param offset Should be element of clock_gate_offsets enum.
+ *               Bits 8-15 specify the ECPM offset of the specific clock reg.
+ *               Bits 0-7 specify the mask for the clock register.
+ * @param mask   Unused
+ * @param mode   Unused
+ */
+void clock_disable_peripheral(uint32_t offset, uint32_t mask, uint32_t mode)
+{
+	volatile uint8_t * reg = (volatile uint8_t *)
+			(IT83XX_ECPM_BASE + (offset >> 8));
+	uint8_t reg_mask = offset & 0xff;
+	uint8_t tmp_mask = 0;
+
+	/* CGCTRL3R, bit 6, must always write a 1. */
+	tmp_mask |= ((offset >> 8) == IT83XX_ECPM_CGCTRL3R_OFF) ? 0x40 : 0x00;
+
+	*reg |= reg_mask | tmp_mask;
 }
