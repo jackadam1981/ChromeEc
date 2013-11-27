8b5104d5a7ac27f28f7f54906a7ef16b228fac96
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2013-11-27 15:18:20 +0800

    mec1322: LPC host command support
    With this, basic host command functionality is working.
    
    We don't have the correct description of LPC memory BAR register yet, so
    we have to use EMI (embedded memory interface) module for 0x800-0x9ff
    region. This requires a slightly different protocol, which is in the
    next CL.
    
    BUG=chrome-os-partner:24107
    TEST=Wire EVB to Stumpy. 'ectool hello' and 'ectool version' working.
    BRANCH=None
    
    Change-Id: I873b4a455cf692e479321a5c6e18c8f33df60e66
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>

--- chip/mec1322/registers.h
+++ chip/mec1322/registers.h
@@ -172,6 +172,15 @@ static inline uintptr_t gpio_port_base(int port_id)
 #define MEC1322_MBX_IMR        REG8(MEC1322_MBX_BASE + 0xc)
 #define MEC1322_MBX_REG(x)     REG8(MEC1322_MBX_BASE + 0x10 + (x))
 
+
+/* ACPI */
+#define MEC1322_ACPI_EC_BASE(x)     (0x400f0c00 + (x) * 0x400)
+#define MEC1322_ACPI_EC_EC2OS(x, y) REG8(MEC1322_ACPI_EC_BASE(x) + 0x100 + (y))
+#define MEC1322_ACPI_EC_STATUS(x)   REG8(MEC1322_ACPI_EC_BASE(x) + 0x104)
+#define MEC1322_ACPI_EC_BYTE_CTL(x) REG8(MEC1322_ACPI_EC_BASE(x) + 0x105)
+#define MEC1322_ACPI_EC_OS2EC(x, y) REG8(MEC1322_ACPI_EC_BASE(x) + 0x108 + (y))
+
+
 /* IRQ Numbers */
 #define MEC1322_IRQ_I2C_0        0
 #define MEC1322_IRQ_I2C_1        1
