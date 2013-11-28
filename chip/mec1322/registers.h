72c115ebc5eb1e6c748849c9b57a05fd46bee03e
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2013-11-28 15:56:56 +0800

    mec1322: Add PWM driver
    This adds a PWM driver, which now generates 30KHz PWM output. Note that
    this is different from fan control module driver.
    
    BUG=chrome-os-partner:24107
    TEST=Set GPIO136 to PWM1. Attach logic analyzer to monitor its output.
          - Set to active high and 30%, see 30% duty PWM at ~29.1KHz.
          - Set to active low and 20%, see 80% duty PWM at ~29.1KHz.
    BRANCH=None
    
    Change-Id: I5f1001d5a4701e19fa87c4cabfd4ae5ae7ccb30c
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>

--- chip/mec1322/registers.h
+++ chip/mec1322/registers.h
@@ -181,6 +181,13 @@ static inline uintptr_t gpio_port_base(int port_id)
 #define MEC1322_ACPI_EC_OS2EC(x, y) REG8(MEC1322_ACPI_EC_BASE(x) + 0x108 + (y))
 
 
+/* PWM */
+#define MEC1322_PWM_BASE(x)    (0x40005800 + (x) * 0x10)
+#define MEC1322_PWM_ON(x)      REG32(MEC1322_PWM_BASE(x) + 0x00)
+#define MEC1322_PWM_OFF(x)     REG32(MEC1322_PWM_BASE(x) + 0x04)
+#define MEC1322_PWM_CFG(x)     REG32(MEC1322_PWM_BASE(x) + 0x08)
+
+
 /* IRQ Numbers */
 #define MEC1322_IRQ_I2C_0        0
 #define MEC1322_IRQ_I2C_1        1
