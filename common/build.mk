76a6e8f9403af1b93a2d225f49e138d3a9f9ee2d
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2013-12-05 17:37:49 +0800

    GPIO UART
    BUG=None
    TEST=None
    BRANCH=None
    
    Change-Id: Ie9698c919f843c61bb05a91e2fdd8ac1fe88f60d
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>

--- common/build.mk
+++ common/build.mk
@@ -10,6 +10,8 @@ common-y=main.o util.o console_output.o uart_buffering.o
 common-y+=memory_commands.o shared_mem.o system.o hooks.o
 common-y+=gpio.o version.o printf.o queue.o
 
+common-y+=gpio_uart.o
+
 common-$(CONFIG_ALS)+=als.o
 common-$(CONFIG_BACKLIGHT_LID)+=backlight_lid.o
 # TODO(crosbug.com/p/23821): Why do these include battery_common but
