6e731f8ffac860111ea0df8355996120d53b1b1d
ChromeOS Developer <dparker@chromium.org>
2014-01-03 17:35:26 -0800

    Trim down boards needed on clapper branch
    BUG=None
    BRANCH=None
    TEST=make buildall -j 32
    
    Change-Id: I5af570873b562763d714200d6c203c1c2c6082b3
    Signed-off-by: Dave Parker <dparker@chromium.org>

--- test/build.mk
+++ test/build.mk
@@ -21,7 +21,7 @@ test-list-$(BOARD_SAMUS)=
 # Emulator tests
 test-list-host=mutex pingpong utils kb_scan kb_mkbp lid_sw power_button hooks
 test-list-host+=thermal flash queue kb_8042 extpwr_gpio console_edit system
-test-list-host+=sbs_charging adapter host_command thermal_falco led_spring
+test-list-host+=sbs_charging adapter host_command thermal_falco
 test-list-host+=bklight_lid bklight_passthru
 
 adapter-y=adapter.o
@@ -35,7 +35,6 @@ host_command-y=host_command.o
 kb_8042-y=kb_8042.o
 kb_mkbp-y=kb_mkbp.o
 kb_scan-y=kb_scan.o
-led_spring-y=led_spring.o led_spring_impl.o
 lid_sw-y=lid_sw.o
 mutex-y=mutex.o
 pingpong-y=pingpong.o
