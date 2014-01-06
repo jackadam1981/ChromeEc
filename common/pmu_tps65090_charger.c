69c26b3066f87eabb3fa5b6afca89ab4256ad1bb
Jaehoon Kim <jh228.kim@samsung.com>
2014-01-06 18:59:31 +0900

    Pit: Add Green LED function in tps65090 charger driver.
    From DV2 board of pit, we'll use 3 color-LED instead of power LED on keyboard.
    So, we have to add the function to control 3 color-LED in pmu driver.
    
    BUG=chrome-os-partner:24855
    TEST=Tested on the pi and pit board about all power status.
    BRANCH=pit
    
    Change-Id: I683e012e7032077039cbcd1c9eaaeb25e1157687
    Signed-off-by: Jaehoon Kim <jh228.kim@samsung.com>

--- common/pmu_tps65090_charger.c
+++ common/pmu_tps65090_charger.c
@@ -44,6 +44,11 @@
 #define BATTERY_AP_OFF_LEVEL 0
 #endif
 
+#ifdef CONFIG_3LED_PIT
+#define GREEN_LED_ON 1
+#define GREEN_LED_OFF 0
+#endif
+
 static const char * const state_list[] = POWER_STATE_NAME_TABLE;
 
 /* States for throttling PMU task */
@@ -51,6 +56,34 @@ static timestamp_t last_waken; /* Initialized to 0 */
 static int has_pending_event;
 
 static enum charging_state current_state = ST_IDLE0;
+
+#ifdef CONFIG_3LED_PIT
+static void battery_charging_led(int led_on)
+{
+	led_on = led_on ? 1 : 0;
+	if (gpio_get_level(GPIO_CHG_COMP_L) != led_on)
+		gpio_set_level(GPIO_CHG_COMP_L, led_on);
+}
+
+static int battery_charging_stat(void)
+{
+	return gpio_get_level(GPIO_CHARGER_EN);
+}
+
+static void change_green_led_stat(void)
+{
+	int alarm;
+	if(extpower_is_present()){
+		battery_status(&alarm);
+		if((alarm & ALARM_CHARGED) && !battery_charging_stat())
+			battery_charging_led(GREEN_LED_ON);
+		else
+			battery_charging_led(GREEN_LED_OFF);
+	}
+	else
+		battery_charging_led(GREEN_LED_OFF);
+}
+#endif
 
 static void enable_charging(int enable)
 {
@@ -407,6 +440,10 @@ void charger_task(void)
 
 #ifdef CONFIG_EXTPOWER_SPRING
 		extpower_charge_update(0);
+#endif
+
+#ifdef CONFIG_3LED_PIT
+		change_green_led_stat();
 #endif
 
 		/*
