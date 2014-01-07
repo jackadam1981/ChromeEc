9e78f82294571db9aabb2ed354e63893f09ecdcc
Alec Berg <alecaberg@chromium.org>
2014-01-06 18:07:27 -0800

    rambi: Accelerometer driver for kxcj9.
    Added accelerometer driver for kxcj9 accelerometers. Currently
    the accelerometers aren't being used by anything, but there are
    console commands, accelwrite and accelread, to perform transactions.
    
    BUG=none
    BRANCH=rambi
    TEST=Used EC console commands to test that accelerometers respond
    and data looks reasonable.
    
    Change-Id: I6ddcf04ec278adeacb148c19b10c3c296b467954
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

---
+++ include/accelerometer.h
@@ -0,0 +1,35 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* This array must be defined in board.c. */
+extern const int accel_addr[];
+
+/* This enum must be defined in board.h. */
+enum accel_id;
+
+
+/* Number of counts from accelerometer that represents 1G acceleration. */
+#define ACCEL_G  1024
+
+/**
+ * Read all three accelerations of an accelerometer.
+ *
+ * @param id Target accelerometer
+ * @param x_acc Pointer to location to store X-axis acceleration.
+ * @param y_acc Pointer to location to store Y-axis acceleration.
+ * @param z_acc Pointer to location to store Z-axis acceleration.
+ *
+ * @return EC_SUCCESS if successful, non-zero if error.
+ */
+int accel_read(enum accel_id id, int *x_acc, int *y_acc, int *z_acc);
+
+/**
+ * Initiailze accelerometers.
+ *
+ * @param id Target accelerometer
+ *
+ * @return EC_SUCCESS if successful, non-zero if error.
+ */
+int accel_init(enum accel_id id);
