43e616750797b17cd6b37a8c6cc8c7a19944d900
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2014-01-08 15:28:53 +0800

    emulator: Guard interrupt status with mutex lock
    This prevents an interrupt from being triggered when we happen to be
    enabling/disabling global interrupt.
    
    BUG=chrome-os-partner:19235
    TEST=Repeatedly run interrupt test
    BRANCH=None
    
    Change-Id: I0163aff801ddbcee4aedba7a78966d97336c79ca
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>

--- core/host/task.c
+++ core/host/task.c
@@ -97,12 +97,16 @@ int in_interrupt_context(void)
 
 void interrupt_disable(void)
 {
+	pthread_mutex_lock(&interrupt_lock);
 	interrupt_disabled = 1;
+	pthread_mutex_unlock(&interrupt_lock);
 }
 
 void interrupt_enable(void)
 {
+	pthread_mutex_lock(&interrupt_lock);
 	interrupt_disabled = 0;
+	pthread_mutex_unlock(&interrupt_lock);
 }
 
 void _task_execute_isr(int sig)
@@ -121,9 +125,11 @@ void task_register_interrupt(void)
 
 void task_trigger_test_interrupt(void (*isr)(void))
 {
-	if (interrupt_disabled)
-		return;
 	pthread_mutex_lock(&interrupt_lock);
+	if (interrupt_disabled) {
+		pthread_mutex_unlock(&interrupt_lock);
+		return;
+	}
 
 	/* Suspend current task and excute ISR */
 	pending_isr = isr;
