2c105f451191ab066e7b18213155174f0b176051
Randall Spangler <rspangler@chromium.org>
2014-01-28 13:14:51 -0800

    rambi: Enable low-power idle
    This should reduce EC power consumption in S3 and S5.
    
    BUG=chrome-os-partner:25377
    BRANCH=baytrail
    TEST=boot system; suspend and resume; type on EC console while suspended
    
    Change-Id: I47e33e158c1b90077f944a6af4374f39efa68d94
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- include/config.h
+++ include/config.h
@@ -508,6 +508,9 @@
  * Low power idle options. These are disabled by default and all boards that
  * want to use low power idle must define it. When using the LFIOSC, the low
  * frequency clock will be used to conserve even more power when possible.
+ *
+ * GPIOs which need to trigger interrupts in low power idle must specify the
+ * GPIO_INT_DSLEEP flag in gpio_list[].
  */
 #undef CONFIG_LOW_POWER_IDLE
 #undef CONFIG_LOW_POWER_USE_LFIOSC
