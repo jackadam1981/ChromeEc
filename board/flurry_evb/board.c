f3c09f34e0adbef36e38ffc1157564c460033a4a
Jeremy Thorpe <jeremyt@chromium.org>
2014-02-03 11:51:40 -0800

    Add flurry_evb target.
    Add a target for the Flurry evaluation setup provided by TI.
    
    BUG=chrome-os-partner:25519
    TEST=With following Changes, boot board and see console output:
    
    --- UART initialized after reboot ---
    [Reset cause: reset-pin]
    [Image: RO, flurry_evb_v1.1.1611-6e86bc4-dirty 2014-02-04 14:46:21 jeremyt@jeremyt.mtv.corp.google.com]
    [0.012531 Inits done]
    Console is enabled; type HELP for help.
    > help
    Known commands:
    chan           help           panicinfo      sysinfo        timerinfo
    crash          hibernate      reboot         sysjump        version
    gettime        history        rtc            syslock        waitms
    gpioget        i2cscan        rw             taskinfo       ww
    gpioset        i2cxfer        shmem          taskready
    HELP LIST = more info; HELP CMD = help on CMD.
    
    BRANCH=None
    
    Change-Id: Ia9ad3c33cad3e8bcb4df82dbf8dced5ed973e22a
    Signed-off-by: Jeremy Thorpe <jeremyt@chromium.org>

---
+++ board/flurry_evb/board.c
@@ -0,0 +1,59 @@
+/* Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* Stellaris EKB-LM4F-EAC board-specific configuration */
+
+#include "adc.h"
+#include "adc_chip.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "registers.h"
+#include "util.h"
+
+/* ADC channels. Must be in the exactly same order as in enum adc_channel. */
+const struct adc_t adc_channels[] = {
+	/* EC internal temperature is calculated by
+	 * 273 + (295 - 450 * ADC_VALUE / ADC_READ_MAX) / 2
+	 * = -225 * ADC_VALUE / ADC_READ_MAX + 420.5
+	 */
+	{"ECTemp", LM4_ADC_SEQ0, -225, ADC_READ_MAX, 420,
+	 LM4_AIN_NONE, 0x0e /* TS0 | IE0 | END0 */, 0, 0},
+
+	/* Charger current is mapped from 0~4000mA to 0~1.6V.
+	 * And ADC maps 0~3.3V to ADC_READ_MAX.
+	 *
+	 * Note that on BDS, this is really just the turn pot on the Badger
+	 * board, but that's good enough for debugging the ADC.
+	 */
+	{"BDSPot", LM4_ADC_SEQ1, 33 * 4000, ADC_READ_MAX * 16, 0,
+	 LM4_AIN(0), 0x06 /* IE0 | END0 */, LM4_GPIO_E, (1<<3)},
+};
+BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
+
+
+/* I2C ports */
+const struct i2c_port_t i2c_ports[] = {
+	{"lightbar", 5, 400},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+
+/* GPIO signal list.  Must match order from enum gpio_signal. */
+const struct gpio_info gpio_list[] = {
+	{"RECOVERY_L", GPIO_D, (1<<1), GPIO_PULL_UP, NULL},
+	{"DEBUG_LED", GPIO_A, (1<<7), GPIO_OUT_LOW, NULL},
+	/* Unimplemented signals which we need to emulate for now */
+	GPIO_SIGNAL_NOT_IMPLEMENTED("WP"),
+	GPIO_SIGNAL_NOT_IMPLEMENTED("ENTERING_RW"),
+};
+BUILD_ASSERT(ARRAY_SIZE(gpio_list) == GPIO_COUNT);
+
+/* Pins with alternate functions */
+const struct gpio_alt_func gpio_alt_funcs[] = {
+	{GPIO_A, 0x03, 1, MODULE_UART},		/* UART0 */
+	{GPIO_G, 0x40, 3, MODULE_I2C},		/* I2C5 SCL */
+	{GPIO_G, 0x80, 3, GPIO_OPEN_DRAIN},	/* I2C5 SDA */
+	{GPIO_B, 0x03, 1, MODULE_UART},		/* UART1 */
+};
+const int gpio_alt_funcs_count = ARRAY_SIZE(gpio_alt_funcs);
