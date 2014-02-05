41607f3fe10e956e81ef273ac8d6292c08432433
Jeremy Thorpe <jeremyt@chromium.org>
2014-02-04 16:32:35 -0800

    Modify lm4 timer to work with flurry.
    DO NOT SUBMIT
    
    Change from timer 6 (which does not exist on flurry) to timer 0.
    
    BUG=chrome-os-partner:25519
    TEST=None
    BRANCH=None
    
    Change-Id: I2da9ee4c4c21dca550894e2d4035f3d42a6615fe
    Signed-off-by: Jeremy Thorpe <jeremyt@chromium.org>

--- chip/lm4/hwtimer.c
+++ chip/lm4/hwtimer.c
@@ -5,6 +5,7 @@
 
 /* Hardware timers driver */
 
+#include "console.h"
 #include "clock.h"
 #include "common.h"
 #include "hooks.h"
@@ -16,38 +17,38 @@
 void __hw_clock_event_set(uint32_t deadline)
 {
 	/* set the match on the deadline */
-	LM4_TIMER_TAMATCHR(6) = 0xffffffff - deadline;
+	LM4_TIMER_TAMATCHR(0) = 0xffffffff - deadline;
 	/* Set the match interrupt */
-	LM4_TIMER_IMR(6) |= 0x10;
+	LM4_TIMER_IMR(0) |= 0x10;
 }
 
 uint32_t __hw_clock_event_get(void)
 {
-	return 0xffffffff - LM4_TIMER_TAMATCHR(6);
+	return 0xffffffff - LM4_TIMER_TAMATCHR(0);
 }
 
 void __hw_clock_event_clear(void)
 {
 	/* Disable the match interrupt */
-	LM4_TIMER_IMR(6) &= ~0x10;
+	LM4_TIMER_IMR(0) &= ~0x10;
 }
 
 uint32_t __hw_clock_source_read(void)
 {
-	return 0xffffffff - LM4_TIMER_TAV(6);
+	return 0xffffffff - LM4_TIMER_TAV(0);
 }
 
 void __hw_clock_source_set(uint32_t ts)
 {
-	LM4_TIMER_TAV(6) = 0xffffffff - ts;
+	LM4_TIMER_TAV(0) = 0xffffffff - ts;
 }
 
 static void __hw_clock_source_irq(void)
 {
-	uint32_t status = LM4_TIMER_RIS(6);
+	uint32_t status = LM4_TIMER_RIS(0);
 
 	/* Clear interrupt */
-	LM4_TIMER_ICR(6) = status;
+	LM4_TIMER_ICR(0) = status;
 
 	/*
 	 * Find expired timers and set the new timer deadline; check the IRQ
@@ -55,7 +56,7 @@ static void __hw_clock_source_irq(void)
 	 */
 	process_timers(status & 0x01);
 }
-DECLARE_IRQ(LM4_IRQ_TIMERW0A, __hw_clock_source_irq, 1);
+DECLARE_IRQ(LM4_IRQ_TIMER0A, __hw_clock_source_irq, 1);
 
 static void update_prescaler(void)
 {
@@ -63,37 +64,37 @@ static void update_prescaler(void)
 	 * Set the prescaler to increment every microsecond.  This takes
 	 * effect immediately, because the TAILD bit in TAMR is clear.
 	 */
-	LM4_TIMER_TAPR(6) = clock_get_freq() / SECOND;
+	LM4_TIMER_TAPR(0) = clock_get_freq() / SECOND;
 }
 DECLARE_HOOK(HOOK_FREQ_CHANGE, update_prescaler, HOOK_PRIO_DEFAULT);
 
 int __hw_clock_source_init(uint32_t start_t)
 {
 	/*
-	 * Use WTIMER0 (timer 6) configured as a free running counter with 1 us
+	 * Use WTIMER0 (timer 0) configured as a free running counter with 1 us
 	 * period.
 	 */
 
 	/* Enable WTIMER0 clock in run and sleep modes. */
-	clock_enable_peripheral(CGC_OFFSET_WTIMER, 0x1,
+	clock_enable_peripheral(CGC_OFFSET_TIMER, 0x1,
 			CGC_MODE_RUN | CGC_MODE_SLEEP);
 
 	/* Ensure timer is disabled : TAEN = TBEN = 0 */
-	LM4_TIMER_CTL(6) &= ~0x101;
+	LM4_TIMER_CTL(0) &= ~0x101;
 	/* Set overflow interrupt */
-	LM4_TIMER_IMR(6) = 0x1;
+	LM4_TIMER_IMR(0) = 0x1;
 	/* 32-bit timer mode */
-	LM4_TIMER_CFG(6) = 4;
+	LM4_TIMER_CFG(0) = 0;
 
 	/* Set initial prescaler */
 	update_prescaler();
 
 	/* Periodic mode, counting down */
-	LM4_TIMER_TAMR(6) = 0x22;
+	LM4_TIMER_TAMR(0) = 0x22;
 	/* Use the full 32-bits of the timer */
-	LM4_TIMER_TAILR(6) = 0xffffffff;
+	LM4_TIMER_TAILR(0) = 0xffffffff;
 	/* Starts counting in timer A */
-	LM4_TIMER_CTL(6) |= 0x1;
+	LM4_TIMER_CTL(0) |= 0x1;
 
 	/*
 	 * Override the count with the start value now that counting has
@@ -102,7 +103,7 @@ int __hw_clock_source_init(uint32_t start_t)
 	__hw_clock_source_set(start_t);
 
 	/* Enable interrupt */
-	task_enable_irq(LM4_IRQ_TIMERW0A);
+	task_enable_irq(LM4_IRQ_TIMER0A);
 
-	return LM4_IRQ_TIMERW0A;
+	return LM4_IRQ_TIMER0A;
 }
