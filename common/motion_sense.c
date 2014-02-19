41a81669053e9c8ae523e094d669429b2ad75d78
Alec Berg <alecaberg@chromium.org>
2014-02-19 14:44:01 -0800

    rambi: Rotate accelerometer data into standard reference frame
    Added rotation of accelerometer data into a standard reference frame
    so that the host does not have to know about the orientation of the
    sensors.
    
    Also added a calibration routine to calibrate the rotation matrix to
    get to the standard reference frame. Cleanup up calibration in the
    process to make it more user friendly.
    
    Changed the default accelerometer sampling rate to 100Hz.
    
    BUG=chrome-os-partner:25599
    BRANCH=rambi
    TEST=Tested the full calibration routine on a glimmer at my desk.
    Used 'taskinfo' and verified that the higher sampling rate does not
    bog down the EC. I found that the motion sense task is running for
    about 200ms every 10 seconds, so about 2% CPU load.
    
    Change-Id: Id554511f7cc9549dfc9ed2d6337216bfa639359d
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- common/motion_sense.c
+++ common/motion_sense.c
@@ -25,10 +25,11 @@
 
 /* Current acceleration vectors and current lid angle. */
 static vector_3_t acc_lid_raw, acc_lid, acc_base;
+static vector_3_t acc_lid_host, acc_base_host;
 static float lid_angle_deg;
 
 /* Sampling interval for measuring acceleration and calculating lid angle. */
-static int accel_interval_ms = 250;
+static int accel_interval_ms = 10;
 
 #ifdef CONFIG_CMD_LID_ANGLE
 static int accel_disp;
@@ -119,7 +120,7 @@ void motion_get_accel_base(vector_3_t *v)
 
 void motion_sense_task(void)
 {
-	timestamp_t ts0, ts1;
+	static timestamp_t ts0, ts1;
 	int wait_us;
 	int ret;
 	uint8_t *lpc_status, *lpc_data;
@@ -159,6 +160,12 @@ void motion_sense_task(void)
 
 		/* TODO(crosbug.com/p/25597): Add filter to smooth lid angle. */
 
+		/* Rotate accels into standard reference frame for the host. */
+		rotate(acc_base, &p_acc_orient->rot_standard_ref,
+				&acc_base_host);
+		rotate(acc_lid, &p_acc_orient->rot_standard_ref,
+				&acc_lid_host);
+
 		/*
 		 * Set the busy bit before writing the sensor data. Increment
 		 * the counter and clear the busy bit after writing the sensor
@@ -173,12 +180,12 @@ void motion_sense_task(void)
 		 * assumes little endian, which is what the host expects.
 		 */
 		((uint16_t *)lpc_data)[0] = (int)lid_angle_deg;
-		((uint16_t *)lpc_data)[1] = acc_base[X];
-		((uint16_t *)lpc_data)[2] = acc_base[Y];
-		((uint16_t *)lpc_data)[3] = acc_base[Z];
-		((uint16_t *)lpc_data)[4] = acc_lid[X];
-		((uint16_t *)lpc_data)[5] = acc_lid[Y];
-		((uint16_t *)lpc_data)[6] = acc_lid[Z];
+		((uint16_t *)lpc_data)[1] = acc_base_host[X];
+		((uint16_t *)lpc_data)[2] = acc_base_host[Y];
+		((uint16_t *)lpc_data)[3] = acc_base_host[Z];
+		((uint16_t *)lpc_data)[4] = acc_lid_host[X];
+		((uint16_t *)lpc_data)[5] = acc_lid_host[Y];
+		((uint16_t *)lpc_data)[6] = acc_lid_host[Z];
 
 		/*
 		 * Increment sample id and clear busy bit to signal we finished
