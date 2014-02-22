1811bb8691ee7b5a56f711faf8ad41441be3d97a
Vincent Palatin <vpalatin@chromium.org>
2014-02-21 17:04:33 -0800

    add support for PD experiment on Discovery board
    Add PD task and setup pins.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I627693a057b7408ab29e74245a585634454ea5ee

--- board/discovery/board.c
+++ board/discovery/board.c
@@ -4,6 +4,8 @@
  */
 /* STM32L-discovery board configuration */
 
+#include "adc.h"
+#include "adc_chip.h"
 #include "common.h"
 #include "gpio.h"
 #include "hooks.h"
@@ -11,17 +13,23 @@
 #include "task.h"
 #include "util.h"
 
-void button_event(enum gpio_signal signal)
-{
-}
+void button_event(enum gpio_signal signal);
 
 /* GPIO signal list.  Must match order from enum gpio_signal. */
 const struct gpio_info gpio_list[] = {
 	/* Inputs with interrupt handlers are first for efficiency */
 	{"USER_BUTTON", GPIO_A, (1<<0),  GPIO_INT_BOTH, button_event},
+	{"DAC_OUT1", GPIO_A, (1<<4),  GPIO_ANALOG, NULL},
+	{"PD_RX", GPIO_B, (1<<4),  GPIO_ANALOG, NULL},
 	/* Outputs */
+	{"PD_TX", GPIO_A, (1<<11),  GPIO_INPUT, NULL},
+	{"PD_TX_GND", GPIO_A, (1<<13),  GPIO_ODR_HIGH, NULL},
 	{"LED_BLUE",    GPIO_B, (1<<6),  GPIO_OUT_LOW, NULL},
 	{"LED_GREEN",   GPIO_B, (1<<7),  GPIO_OUT_LOW, NULL},
+
+	{"TEST0", GPIO_C, (1<<0),  GPIO_OUT_LOW, NULL},
+	{"TEST1", GPIO_C, (1<<1),  GPIO_OUT_LOW, NULL},
+	{"TEST2", GPIO_C, (1<<2),  GPIO_OUT_LOW, NULL},
 
 	/* Unimplemented signals which we need to emulate for now */
 	GPIO_SIGNAL_NOT_IMPLEMENTED("ENTERING_RW"),
@@ -38,6 +46,18 @@ DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
 /* Pins with alternate functions */
 const struct gpio_alt_func gpio_alt_funcs[] = {
+	/* PD SPI: PA5:SCK PA11:MISO PA12:MOSI */
+	{GPIO_A, 0x1820, GPIO_ALT_SPI, MODULE_USB_PD},
+	/* USART1 on PA9/PA10 */
 	{GPIO_A, 0x0600, GPIO_ALT_USART, MODULE_UART},
+	/* PD Clock: PB12:TIM10_CH1 */
+	{GPIO_B, 0x1000, GPIO_ALT_TIM9_11, MODULE_USB_PD},
 };
 const int gpio_alt_funcs_count = ARRAY_SIZE(gpio_alt_funcs);
+
+/* ADC channels */
+const struct adc_t adc_channels[] = {
+	/* USB PD RX. Converted to mV (3300mV/4096). */
+	[ADC_CH_PD_RX] = {"PD_RX", 3300, 4096, 0, STM32_AIN(2)},
+};
+BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
