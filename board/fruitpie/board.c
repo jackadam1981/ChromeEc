f3be7989979330f6bcde5a3d96adc445c754ef84
Vincent Palatin <vpalatin@chromium.org>
2014-03-19 10:53:53 -0700

    [TEST ONLY] ADCs configuration on STM32F072B Discovery
    XxX DO NOT SUBMIT XxX
    
    For testing only
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=none
    TEST=run "adc" command on STM32F072B Discovery.
    
    Change-Id: I5913831c4ca77dfea818a06c5d728bc5f5c4078b

--- board/fruitpie/board.c
+++ board/fruitpie/board.c
@@ -4,6 +4,8 @@
  */
 /* Fruitpie board configuration */
 
+#include "adc.h"
+#include "adc_chip.h"
 #include "common.h"
 #include "gpio.h"
 #include "hooks.h"
@@ -19,6 +21,15 @@ void button_event(enum gpio_signal signal)
 const struct gpio_info gpio_list[] = {
 	/* Inputs with interrupt handlers are first for efficiency */
 	{"USER_BUTTON", GPIO_A, (1<<0),  GPIO_INT_BOTH, button_event},
+	{"AIN1", GPIO_A, (1<<1),  GPIO_ANALOG, NULL},
+	{"AIN2", GPIO_A, (1<<2),  GPIO_ANALOG, NULL},
+	{"AIN3", GPIO_A, (1<<3),  GPIO_ANALOG, NULL},
+	{"AIN4", GPIO_A, (1<<4),  GPIO_ANALOG, NULL},
+	{"AIN5", GPIO_A, (1<<5),  GPIO_ANALOG, NULL},
+	{"AIN6", GPIO_A, (1<<6),  GPIO_ANALOG, NULL},
+	{"AIN7", GPIO_A, (1<<7),  GPIO_ANALOG, NULL},
+	{"AIN8", GPIO_B, (1<<0),  GPIO_ANALOG, NULL},
+	{"AIN9", GPIO_B, (1<<1),  GPIO_ANALOG, NULL},
 	/* Outputs */
 	{"LED_BLUE",    GPIO_B, (1<<6),  GPIO_OUT_LOW, NULL},
 	{"LED_GREEN",   GPIO_B, (1<<7),  GPIO_OUT_LOW, NULL},
@@ -28,6 +39,20 @@ const struct gpio_info gpio_list[] = {
 	GPIO_SIGNAL_NOT_IMPLEMENTED("WP_L"),
 };
 BUILD_ASSERT(ARRAY_SIZE(gpio_list) == GPIO_COUNT);
+
+/* ADC channels */
+const struct adc_t adc_channels[] = {
+	[ADC_CH_AIN0] = {"PA0", 3300, 4096, 0, STM32_AIN(0)},
+	[ADC_CH_AIN1] = {"PA1", 3300, 4096, 0, STM32_AIN(1)},
+	[ADC_CH_AIN2] = {"PA2", 3300, 4096, 0, STM32_AIN(2)},
+	[ADC_CH_AIN3] = {"PA3", 3300, 4096, 0, STM32_AIN(3)},
+	[ADC_CH_AIN4] = {"PA4", 3300, 4096, 0, STM32_AIN(4)},
+	[ADC_CH_AIN5] = {"PA5", 3300, 4096, 0, STM32_AIN(5)},
+	[ADC_CH_AIN6] = {"PA6", 3300, 4096, 0, STM32_AIN(6)},
+	[ADC_CH_AIN7] = {"PA7", 3300, 4096, 0, STM32_AIN(7)},
+	[ADC_CH_AIN8] = {"PB0", 3300, 4096, 0, STM32_AIN(8)},
+	[ADC_CH_AIN9] = {"PB1", 3300, 4096, 0, STM32_AIN(9)},
+};
 
 /* Initialize board. */
 static void board_init(void)
