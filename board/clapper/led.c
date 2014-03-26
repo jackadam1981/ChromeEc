2eebada200dd2f7958c3bcf38698fdeb2f1f0603
git-CarryBK_Chen.compal.com <CarryBK_Chen@compal.com>
2014-03-26 20:31:18 +0800

    Clapper: fix battery led's behavior when 0% battery on charge
    Condition   : charge & 0% battery
    Battery LED : 500ms off, 1 sec amber
    
    BRANCH=clapper
    BUG=none
    TEST=Let battery in 0% and plug-in AC, the behavior of battery led is "500ms off, 1 sec amber"
    Signed-off-by: Carry Chen <CarryBK_Chen@compal.com>
    
    Change-Id: I5330baec7325c179f9b5e4c7ebac2e5b3527ed8a

--- board/clapper/led.c
+++ board/clapper/led.c
@@ -12,6 +12,7 @@
 #include "led_common.h"
 #include "pwm.h"
 #include "util.h"
+#include "console.h"
 
 const enum ec_led_id supported_led_ids[] = {
 	EC_LED_ID_BATTERY_LED, EC_LED_ID_POWER_LED};
@@ -126,12 +127,15 @@ static void battery_led_update(void)
 	case PWR_STATE_CHARGE:
 	case PWR_STATE_CHARGE_NEAR_FULL: /* with AC */
 	case PWR_STATE_IDLE:		 /* with AC */
-		/* 1% ~ 4% */
-		if (battery > 0 && battery < 5) {
-			/* 1000 ms on */
-			if (timer%15 < 10)
-				set_color_battery(LED_AMBER);
-			/* 500 ms off */
+		/* 80% ~ */
+		if (battery >= 80)
+			set_color_battery(LED_WHITE);
+		/* 20% ~ 79% */
+		else if (battery >= 20 && battery < 80) {
+			/* 5000 ms on */
+			if (timer%51 < 50)
+				set_color_battery(LED_WHITE);
+			/* 100 ms off */
 			else
 				set_color_battery(LED_OFF);
 		}
@@ -144,25 +148,8 @@ static void battery_led_update(void)
 			else
 				set_color_battery(LED_OFF);
 		}
-		/* 20% ~ 79% */
-		else if (battery >= 20 && battery < 80) {
-			/* 5000 ms on */
-			if (timer%51 < 50)
-				set_color_battery(LED_WHITE);
-			/* 100 ms off */
-			else
-				set_color_battery(LED_OFF);
-		}
-		/* 80%i ~ 100% */
-		else if (battery >= 80 && battery <= 100)
-			set_color_battery(LED_WHITE);
-		break;
-	case PWR_STATE_DISCHARGE:	 /* without AC */
-		/* not S0 */
-		if (!chipset_in_state(CHIPSET_STATE_ON))
-			set_color_battery(LED_OFF);
-		/* 1% ~ 4% */
-		else if (battery > 0 && battery < 5) {
+		/* 0% ~ 4% */
+		else {
 			/* 1000 ms on */
 			if (timer%15 < 10)
 				set_color_battery(LED_AMBER);
@@ -170,12 +157,26 @@ static void battery_led_update(void)
 			else
 				set_color_battery(LED_OFF);
 		}
+		break;
+	case PWR_STATE_DISCHARGE:	 /* without AC */
+		/* not S0 */
+		if (!chipset_in_state(CHIPSET_STATE_ON))
+			set_color_battery(LED_OFF);
+		/* 20% ~ */
+		else if (battery >= 20)
+			set_color_battery(LED_WHITE);
 		/* 5% ~ 19% */
 		else if (battery >= 5 && battery < 20)
 			set_color_battery(LED_AMBER);
-		/* 20% ~ 100% */
-		else if (battery >= 20 && battery <= 100)
-			set_color_battery(LED_WHITE);
+		/* 1% ~ 4% */
+		else {
+			/* 1000 ms on */
+			if (timer%15 < 10)
+				set_color_battery(LED_AMBER);
+			/* 500 ms off */
+			else
+				set_color_battery(LED_OFF);
+		}
 		break;
 	default:
 		/* Other states don't alter LED behavior */
