b5df65e19c3f450aec0f14977d4e0d20a4a7f2b5
Bill Richardson <wfrichar@chromium.org>
2014-03-26 16:56:38 -0700

    Add charge_state_v2 algorithm for use by Samus
    This is a complete rewrite of the charge_state task used by x86 platforms.
    Rather than having a bunch of state-specific functions, each with their own
    error handling and special cases, this is organized like so:
    
      Forever:
      1. Read everything we can from the battery and charger.
      2. Figure out what we'd like to do (including error handling).
      3. Allow for customization to override that.
      4. Do it.
    
    There's a lot to this CL (and more to come), so this is preliminary and
    won't be committed exactly as-is.
    
    Things I'm still working on are marked with "HEY:".
    Things I need to file bugs for are marked with "TODO(HEY)".
    
    None of the battery and charger error responses are handled yet (search for
    "PROBLEM").
    
    I also need to figure out why some tests are failing.
    
    BUG=chrome-os-partner:20881
    BRANCH=ToT
    TEST=manual
    
      make buildall -j
    
    Undefine CONFIG_BATTERY_PRESENT_CUSTOM, plug and unplug the battery to be
    sure the trickle charging logic is correct when it can't tell if the battery
    is present.
    
    Change-Id: I3935cd3b87f322eb52178f8a675a886c16b75d58
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>

--- include/charge_state_v2.h
+++ include/charge_state_v2.h
@@ -4,10 +4,50 @@
  */
 
 #include "battery.h"
+#include "charger.h"
 #include "timer.h"
 
 #ifndef __CROS_EC_CHARGE_STATE_V2_H
 #define __CROS_EC_CHARGE_STATE_V2_H
+
+/*
+ * The values exported by charge_get_state() and charge_get_flags() are used
+ * only to control the LEDs (with one not-quite-correct exception). For V2
+ * we use a different set of states internally.
+ */
+enum charge_state_v2 {
+	ST_IDLE = 0,
+	ST_DISCHARGE,
+	ST_CHARGE,
+	ST_PRECHARGE,
+
+	NUM_STATES_V2
+};
+
+struct charge_state_data {
+	timestamp_t ts;
+	int ac;
+	struct charger_params chg;
+	struct batt_params batt;
+	enum charge_state_v2 state;
+	int requested_voltage;
+	int requested_current;
+};
+
+/*
+ * Optional customization.
+ * On input, the struct reflects the default behavior. The function can make
+ * changes to the state, requested_voltage, or requested_current.
+ *
+ * The return value is the poll period in usec. If zero or negative, the
+ * default will be used (varies according to the state).
+ */
+int charger_profile_override(struct charge_state_data *);
+/*
+ * HEY: We should provide a way to read the current setting, too. At least for
+ * the LEDs or lightbar, but the AP might want to know too.
+ */
+int charger_profile_override_enable(int enable);
 
 
 #endif /* __CROS_EC_CHARGE_STATE_V2_H */
