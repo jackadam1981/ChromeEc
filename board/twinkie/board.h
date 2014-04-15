07b2cc142da4b1b0c6bed58ee92ce7cc7fc4d50f
Vincent Palatin <vpalatin@chromium.org>
2014-04-15 13:07:21 -0700

    Add basic support for Twinkie dongle
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I3468a651c97aba48a7d3c07ff6a34a676988e426

---
+++ board/twinkie/board.h
@@ -0,0 +1,99 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Twinkie dongle configuration */
+
+#ifndef __BOARD_H
+#define __BOARD_H
+
+/* 48 MHz SYSCLK clock frequency */
+#define CPU_CLOCK 48000000
+
+/* the UART console is on USART2 (PA14/PA15) */
+#undef CONFIG_UART_CONSOLE
+#define CONFIG_UART_CONSOLE 2
+#undef CONFIG_UART_BAUD_RATE
+#define CONFIG_UART_BAUD_RATE 3000000
+
+/* Optional features */
+#define CONFIG_STM_HWTIMER32
+#define CONFIG_USB
+#define CONFIG_ADC
+#define CONFIG_BOARD_PRE_INIT
+#undef CONFIG_WATCHDOG_HELP
+#undef CONFIG_LID_SWITCH
+#undef CONFIG_TASK_PROFILING
+
+/* I2C ports configuration */
+#define I2C_PORT_MASTER 1
+#define I2C_PORT_SLAVE  0
+
+/* USB configuration */
+#define CONFIG_USB_PID 0x5008
+/* By default, enable all console messages excepted USB */
+#define CC_DEFAULT     (CC_ALL & ~CC_MASK(CC_USB))
+
+/*
+ * Allow dangerous commands all the time, since we don't have a write protect
+ * switch.
+ */
+#define CONFIG_SYSTEM_UNLOCKED
+
+#ifndef __ASSEMBLER__
+
+/* Timer selection */
+#define TIM_CLOCK_PD_RX 1
+#define TIM_CLOCK32 2
+#define TIM_ADC     3
+
+/* GPIO signal list */
+enum gpio_signal {
+	/* PD RX/TX */
+	GPIO_USB_CC1_PD,
+	GPIO_PD_REF1,
+	GPIO_PD_REF2,
+	GPIO_USB_CC2_PD,
+	GPIO_PD_CLK_OUT,
+	GPIO_PD_TX_EN,
+#if 0
+	GPIO_PD_CLK_IN,
+	GPIO_PD_TX_DATA,
+#endif
+	/* Unimplemented signals we emulate */
+	GPIO_ENTERING_RW,
+	GPIO_WP_L,
+	/* Number of GPIOs; not an actual GPIO */
+	GPIO_COUNT
+};
+
+/* ADC signal */
+enum adc_channel {
+	ADC_CH_CC1_PD = 0,
+	ADC_CH_CC2_PD,
+	/* Number of ADC channels */
+	ADC_CH_COUNT
+};
+
+/* USB endpoint indexes */
+enum usb_endpoint {
+	USB_EP_CONTROL = 0,
+	USB_EP_HID,
+	USB_EP_SERIAL_TX,
+	USB_EP_SERIAL_RX,
+
+	USB_EP_COUNT
+};
+/* USB string indexes */
+enum usb_strings {
+	USB_STR_DESC = 0,
+	USB_STR_VENDOR,
+	USB_STR_PRODUCT,
+	USB_STR_VERSION,
+
+	USB_STR_COUNT
+};
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __BOARD_H */
