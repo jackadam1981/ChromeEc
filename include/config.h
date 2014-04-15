5da700081060b46cbc324684f7e29124a986dcef
Alec Berg <alecaberg@chromium.org>
2014-04-15 13:53:57 -0700

    WIP: lm4: store FPU regs on context switch
    Created a config option, CONIFG_FPU_STORE_CONTEXT, enabled by default for lm4,
    which when turned on will store FPU registers on context switches.
    
    When this option is turned on, stack space for tasks which use FP will
    increase by 68 bytes and the context switching time will increase by roughly
    68 clock cycles. For tasks that don't use FP, the context switching time will
    be only a few clocks longer, but, stack space actually decreases. This is
    because previously we were reserving stack space for FPU regs S0-S15
    on every context switch even though we weren't doing anything with them.
    
    BUG=chrome-os-partner:27971
    BRANCH=none
    TEST=Tested on a glimmer. Added the following code, which uses the
    FPU in both the hooks task and the console task.
    
    float dummy = 2.0f;
    static void hook_fpu(void)
    {
    	union {
    		float f;
    		int i;
    	} tmp;
    
    	/* do a dummy FP calculation to set CONTROL.FPCA high. */
    	dummy = 2.3f*7.8f;
    
    	/* read and print FP reg. */
    	asm volatile("vmov %0, s29" : "=r"(tmp.f));
    	ccprintf("Hook float 0x%08x\n", tmp.i);
    
    	/* write FP reg. */
    	tmp.i = 0x1234;
    	asm volatile("vmov s29, %0" : : "r"(tmp.f));
    }
    DECLARE_HOOK(HOOK_SECOND, hook_fpu, HOOK_PRIO_DEFAULT);
    
    static int command_fpu_test(int argc, char **argv)
    {
    	union {
    		float f;
    		int i;
    	} tmp;
    
    	/* do a dummy FP calculation to set CONTROL.FPCA high. */
    	dummy = 2.7f*7.8f;
    
    	/* read and print FP reg. */
    	asm volatile("vmov %0, s29" : "=r"(tmp.f));
    	ccprintf("Console float 0x%08x\n", tmp.i);
    
    	if (argc == 2) {
    		char *e;
    
    		tmp.i = strtoi(argv[1], &e, 0);
    		if (*e)
    			return EC_ERROR_PARAM1;
    
    		/* write FP reg. */
    		asm volatile("vmov s29, %0" : : "r"(tmp.f));
    	}
    
    	return EC_SUCCESS;
    }
    DECLARE_CONSOLE_COMMAND(fputest, command_fpu_test, "", "", NULL);
    
    With CONIFG_FPU_STORE_CONTEXT turned off, when you call fputest 5 from EC
    console then on the next HOOK_SECOND, the value of register s29 is 5,
    instead of 0x1234 because register s29 is not saved on context switches:
    
    Hook float 0x00001234
    > fputest 5
    Console float 0x00001234
    Hook float 0x00000005
    
    When CONFIG_FPU_STORE_CONTEXT is turned on, the register holds the
    correct value for each task:
    
    Hook float 0x00001234
    > fputest 5
    Console float 0x00000000
    Hook float 0x00001234
    > fputest
    Console float 0x00000005
    Hook float 0x00001234
    
    Change-Id: Ifb1b5cbf1c6fc9193f165f8d69c96443b35bf981
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- include/config.h
+++ include/config.h
@@ -452,6 +452,9 @@
 /* Enable support for floating point unit */
 #undef CONFIG_FPU
 
+/* Enable support for storing FP regs on context switches when task uses FP. */
+#undef CONIFG_FPU_STORE_CONTEXT
+
 /*****************************************************************************/
 /* Firmware region configuration */
 
