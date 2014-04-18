05efd23a562735c035c534d1481f3d501c46ad91
Vincent Palatin <vpalatin@chromium.org>
2014-04-18 08:01:39 -0700

    Add support for the Firefly board
    Firefly is the test/burn-in board for Zinger production.
    It's acting a Power Delivery sink.
    
    The plug polarity is not managed correctly yet.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I57f525ad04c519f2fa458635a1c6cc45a503e5ac

---
+++ board/firefly/usb_pd_config.h
@@ -0,0 +1,91 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* USB Power delivery board configuration */
+
+#ifndef __USB_PD_CONFIG_H
+#define __USB_PD_CONFIG_H
+
+/* Timer selection for baseband PD communication */
+#define TIM_CLOCK_PD_TX 17
+#define TIM_CLOCK_PD_RX 1
+
+/* use the hardware accelerator for CRC */
+#define CONFIG_HW_CRC
+
+/* TX is using SPI2 on PB3/PB4 or PA6 */
+#define SPI_REGS STM32_SPI1_REGS
+#define DMAC_SPI_TX STM32_DMAC_CH3
+
+static inline void spi_enable_clock(void)
+{
+	STM32_RCC_APB2ENR |= STM32_RCC_PB2_SPI1;
+}
+
+/* RX is using COMP1 triggering TIM1 CH1 */
+#define DMAC_TIM_RX STM32_DMAC_CH2
+#define TIM_CCR_IDX 1
+#define TIM_CCR_CS  1
+#define EXTI_COMP 21
+#define IRQ_COMP STM32_IRQ_COMP
+/* triggers packet detection on comparator falling edge */
+#define EXTI_XTSR STM32_EXTI_FTSR
+
+
+/* Clock divider for RX edges timings (2.4Mhz counter from 48Mhz clock) */
+#define RX_CLOCK_DIV (20 - 1)
+
+/* the pins used for communication need to be hi-speed */
+static inline void pd_set_pins_speed(void)
+{
+	/* 40 MHz pin speed on SPI1 PA4/6/7 */
+	STM32_GPIO_OSPEEDR(GPIO_B) |= 0x0000F300;
+	/* 40 MHz pin speed on SPI1 PB3/4/5 */
+	STM32_GPIO_OSPEEDR(GPIO_B) |= 0x00000FC0;
+	/* 40 MHz pin speed on TIM17_CH1 (PB9) */
+	STM32_GPIO_OSPEEDR(GPIO_B) |= 0x000C0000;
+}
+
+/* Drive the CC line from the TX block */
+static inline void pd_tx_enable(void)
+{
+	/* set the low level reference */
+	gpio_set_level(GPIO_PD_CC1_TX_EN, 0);
+	/* put SPI function on TX pin */
+	gpio_config_module(MODULE_USB_PD, 1);
+}
+
+/* Put the TX driver in Hi-Z state */
+static inline void pd_tx_disable(void)
+{
+	/* put SPI TX in Hi-Z */
+	gpio_config_module(MODULE_USB_PD, 0);
+	/* put the low level reference in Hi-Z */
+	gpio_set_level(GPIO_PD_CC1_TX_EN, 1);
+}
+
+/* Initialize pins used for TX and put them in Hi-Z */
+static inline void pd_tx_init(void)
+{
+	gpio_config_module(MODULE_USB_PD, 0);
+}
+
+static inline void pd_set_host_mode(int enable)
+{
+}
+
+/* Standard-current DFP : no-connect voltage is 1.55V */
+#define PD_SRC_VNC 1550 /* mV */
+
+/* UFP-side : threshold for DFP connection detection */
+#define PD_SNK_VA   200 /* mV */
+
+/* we are acting only as a sink */
+#define PD_DEFAULT_STATE PD_STATE_SNK_DISCONNECTED
+
+/* we are never a source : don't care about power supply */
+#define PD_POWER_SUPPLY_TRANSITION_DELAY 0
+
+#endif /* __USB_PD_CONFIG_H */
