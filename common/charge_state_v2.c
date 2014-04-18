a49d9646d2173f5eaf2f1278dfca6f1868757677
Louis Yung-Chieh Lo <yjlou@chromium.org>
2014-04-18 12:59:57 -0700

    nyan: add info_precharge for drained battery
    Charger v2 assumes the battery_get_info() always returns non-NULL even
    if the battery is not detected, for example, in the over-drained
    situation. Thus, add a new struct so that we know what the conservative
    setting is to pre-charge the unknown battery.
    
    BUG=chrome-os-partner:28112
    BRANCH=nyan,big,blaze
    TEST=See issue tracker for the test procedure.
    
    Change-Id: Ica4fe75d154e2f195eb1da19ba045346da383b6c
    Signed-off-by: Louis Yung-Chieh Lo <yjlou@chromium.org>

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -98,6 +98,9 @@ static int update_static_battery_info(void)
 	 */
 	int rv;
 
+	/* Before we ask battery info, clear the flag */
+	*host_get_memmap(EC_MEMMAP_BATTERY_VERSION) = 0;
+
 	/* Smart battery serial number is 16 bits */
 	batt_str = (char *)host_get_memmap(EC_MEMMAP_BATT_SERIAL);
 	memset(batt_str, 0, EC_MEMMAP_TEXT_MAX);
@@ -284,12 +287,12 @@ static int charge_request(int voltage, int current)
 
 	CPRINTF("[%T %s(%dmV, %dmA)]\n", __func__, voltage, current);
 
-	if (voltage > 0)
+	if (voltage >= 0)
 		r1 = charger_set_voltage(voltage);
 	if (r1 != EC_SUCCESS)
 		problem(PR_SET_VOLTAGE, r1);
 
-	if (current > 0)
+	if (current >= 0)
 		r2 = charger_set_current(current);
 	if (r2 != EC_SUCCESS)
 		problem(PR_SET_CURRENT, r2);
@@ -537,9 +540,14 @@ void charger_task(void)
 			goto wait_for_it;
 		} else {
 			/* The battery is responding. Yay. Try to use it. */
-			if (curr.state == ST_PRECHARGE ||
-			    battery_seems_to_be_dead)
+			if (curr.state == ST_PRECHARGE) {
 				ccprintf("[%T battery woke up]\n");
+
+				/* Update the battery-specific values */
+				batt_info = battery_get_info();
+				need_static = 1;
+			}
+
 			battery_seems_to_be_dead = 0;
 			curr.state = ST_CHARGE;
 		}
