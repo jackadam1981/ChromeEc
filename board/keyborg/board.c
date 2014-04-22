bcccd551c0708807382de2df77cb8748ead38621
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2014-04-22 10:05:48 +0800

    Keyborg: implement matrix scanning
    This implements dual chip matrix scanning. Now the scan result is only
    dumped to debug output.
    
    BUG=None
    TEST=Put a finger on the panel and see its shape.
    BRANCH=None
    
    Change-Id: I015c901b42e24fe4a6249c12c37bc5bfcb308c9f
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>

--- board/keyborg/board.c
+++ board/keyborg/board.c
@@ -12,12 +12,123 @@
 #include "spi_comm.h"
 #include "system.h"
 #include "task.h"
+#include "touch_scan.h"
 #include "util.h"
+
+const struct ts_pin row_pins[] = {
+	{TS_GPIO_E, 1 <<  0}, /* R1 */
+	{TS_GPIO_E, 1 <<  4}, /* R2 */
+	{TS_GPIO_E, 1 <<  8}, /* R3 */
+	{TS_GPIO_E, 1 <<  1}, /* R4 */
+	{TS_GPIO_E, 1 << 11}, /* R5 */
+	{TS_GPIO_E, 1 << 12}, /* R6 */
+	{TS_GPIO_E, 1 << 15}, /* R7 */
+	{TS_GPIO_E, 1 << 13}, /* R8 */
+	{TS_GPIO_D, 1 <<  3}, /* R9 */
+	{TS_GPIO_D, 1 <<  4}, /* R10 */
+	{TS_GPIO_D, 1 <<  5}, /* R11 */
+	{TS_GPIO_D, 1 <<  0}, /* R12 */
+	{TS_GPIO_D, 1 <<  6}, /* R13 */
+	{TS_GPIO_D, 1 <<  8}, /* R14 */
+	{TS_GPIO_D, 1 << 11}, /* R15 */
+	{TS_GPIO_D, 1 << 10}, /* R16 */
+	{TS_GPIO_D, 1 << 12}, /* R17 */
+	{TS_GPIO_D, 1 << 13}, /* R18 */
+	{TS_GPIO_D, 1 << 14}, /* R19 */
+	{TS_GPIO_D, 1 << 15}, /* R20 */
+	{TS_GPIO_C, 1 <<  8}, /* R21 */
+	{TS_GPIO_C, 1 <<  7}, /* R22 */
+	{TS_GPIO_C, 1 << 15}, /* R23 */
+	{TS_GPIO_E, 1 <<  6}, /* R24 */
+	{TS_GPIO_E, 1 <<  5}, /* R25 */
+	{TS_GPIO_E, 1 <<  2}, /* R26 */
+	{TS_GPIO_E, 1 <<  3}, /* R27 */
+	{TS_GPIO_E, 1 << 10}, /* R28 */
+	{TS_GPIO_E, 1 <<  9}, /* R29 */
+	{TS_GPIO_E, 1 << 14}, /* R30 */
+	{TS_GPIO_E, 1 <<  7}, /* R31 */
+	{TS_GPIO_D, 1 <<  2}, /* R32 */
+	{TS_GPIO_D, 1 <<  7}, /* R33 */
+	{TS_GPIO_D, 1 <<  1}, /* R34 */
+	{TS_GPIO_D, 1 <<  9}, /* R35 */
+	{TS_GPIO_C, 1 <<  5}, /* R36 */
+	{TS_GPIO_C, 1 <<  6}, /* R37 */
+	{TS_GPIO_C, 1 << 10}, /* R38 */
+	{TS_GPIO_C, 1 << 13}, /* R39 */
+	{TS_GPIO_C, 1 << 14}, /* R40 */
+	{TS_GPIO_C, 1 << 12}, /* R41 */
+};
+BUILD_ASSERT(ARRAY_SIZE(row_pins) == ROW_COUNT);
+
+const struct ts_pin col_pins[] = {
+	{TS_GPIO_B, 1 <<  5}, /* C1 */
+	{TS_GPIO_H, 1 <<  1}, /* C2 */
+	{TS_GPIO_H, 1 <<  0}, /* C3 */
+	{TS_GPIO_H, 1 <<  5}, /* C4 */
+	{TS_GPIO_H, 1 << 10}, /* C5 */
+	{TS_GPIO_H, 1 <<  6}, /* C6 */
+	{TS_GPIO_H, 1 <<  4}, /* C7 */
+	{TS_GPIO_H, 1 <<  3}, /* C8 */
+	{TS_GPIO_H, 1 <<  9}, /* C9 */
+	{TS_GPIO_H, 1 << 12}, /* C10 */
+	{TS_GPIO_H, 1 << 11}, /* C11 */
+	{TS_GPIO_H, 1 << 15}, /* C12 */
+	{TS_GPIO_H, 1 <<  2}, /* C13 */
+	{TS_GPIO_H, 1 << 14}, /* C14 */
+	{TS_GPIO_G, 1 <<  5}, /* C15 */
+	{TS_GPIO_G, 1 <<  9}, /* C16 */
+	{TS_GPIO_G, 1 <<  4}, /* C17 */
+	{TS_GPIO_G, 1 << 15}, /* C18 */
+	{TS_GPIO_G, 1 << 10}, /* C19 */
+	{TS_GPIO_G, 1 << 12}, /* C20 */
+	{TS_GPIO_G, 1 <<  0}, /* C21 */
+	{TS_GPIO_G, 1 << 11}, /* C22 */
+	{TS_GPIO_B, 1 <<  0}, /* C23 */
+	{TS_GPIO_G, 1 <<  2}, /* C24 */
+	{TS_GPIO_G, 1 <<  1}, /* C25 */
+	{TS_GPIO_A, 1 << 10}, /* Fake C26. C26 is used by debugger. */
+	{TS_GPIO_A, 1 << 10}, /* Fake C27. C27 is used by debugger. */
+	{TS_GPIO_B, 1 <<  3}, /* C28 */
+	{TS_GPIO_A, 1 << 10}, /* Fake C29. C29 is used as UART Tx. */
+	{TS_GPIO_B, 1 <<  8}, /* C30 */
+	{TS_GPIO_A, 1 << 10}, /* C31 */
+	{TS_GPIO_B, 1 <<  1}, /* C32 */
+	{TS_GPIO_G, 1 << 13}, /* C33 */
+	{TS_GPIO_B, 1 <<  7}, /* C34 */
+	{TS_GPIO_B, 1 <<  2}, /* C35 */
+	{TS_GPIO_G, 1 << 14}, /* C36 */
+	{TS_GPIO_G, 1 <<  3}, /* C37 */
+	{TS_GPIO_G, 1 <<  7}, /* C38 */
+	{TS_GPIO_H, 1 << 13}, /* C39 */
+	{TS_GPIO_H, 1 <<  7}, /* C40 */
+	{TS_GPIO_B, 1 <<  4}, /* C41 */
+	{TS_GPIO_H, 1 <<  8}, /* C42 */
+	{TS_GPIO_B, 1 <<  6}, /* C43 */
+	{TS_GPIO_B, 1 <<  9}, /* C44 */
+	{TS_GPIO_I, 1 << 10}, /* C45 */
+	{TS_GPIO_I, 1 << 11}, /* C46 */
+	{TS_GPIO_I, 1 <<  9}, /* C47 */
+	{TS_GPIO_G, 1 <<  8}, /* C48 */
+	{TS_GPIO_G, 1 <<  6}, /* C49 */
+	{TS_GPIO_I, 1 <<  4}, /* C50 */
+	{TS_GPIO_I, 1 <<  3}, /* C51 */
+	{TS_GPIO_I, 1 <<  5}, /* C52 */
+	{TS_GPIO_I, 1 << 14}, /* C53 */
+	{TS_GPIO_I, 1 << 12}, /* C54 */
+	{TS_GPIO_I, 1 <<  8}, /* C55 */
+	{TS_GPIO_I, 1 <<  6}, /* C56 */
+	{TS_GPIO_I, 1 << 15}, /* C57 */
+	{TS_GPIO_I, 1 <<  0}, /* C58 */
+	{TS_GPIO_I, 1 << 13}, /* C59 */
+	{TS_GPIO_I, 1 <<  7}, /* C60 */
+};
+BUILD_ASSERT(ARRAY_SIZE(col_pins) == COL_COUNT);
 
 int main(void)
 {
 	int i = 0;
 	hardware_init();
+	touch_scan_init();
 	debug_printf("Keyborg starting...\n");
 
 	master_slave_init();
@@ -43,6 +154,14 @@ int main(void)
 				debug_printf("Passed\n");
 			else
 				debug_printf("Failed\n");
+			task_wait_event(SECOND);
+			debug_printf("Scan...");
+			if (touch_scan_full_matrix() == EC_SUCCESS)
+				debug_printf("Passed\n");
+			else {
+				debug_printf("Failed\n");
+				task_wait_event(-1);
+			}
 		}
 	}
 }
