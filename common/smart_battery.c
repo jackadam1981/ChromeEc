415d93e968673a0584ba2b283b36406fb5812cd0
Wonjoon Lee <woojoo.lee@samsung.com>
2014-04-29 17:18:07 +0900

    Adding waiting function when battery boot-up
    Some battery uses clock stretching feature, and this could disturb
    PMU communication before battery going stable.
    AP does not know and will attempt PMU setting, and could get fail
    For various battery indicates usually 1s for stable
    (even if it is much less in real world 200ms~700ms)
    Let's checking 'battery is ready' when first pump-up power.
    
    BUG=chrome-os-partner:28289
    TEST=Going battery shipmode and plug-in AC, See booting and EC log
    Disconnect battery, and plug-in and see booting and EC log
    
    Change-Id: Id5c02c6e7ac36014e7a085af38ac8e3520d3e390
    Signed-off-by: Wonjoon Lee <woojoo.lee@samsung.com>

--- common/smart_battery.c
+++ common/smart_battery.c
@@ -11,6 +11,9 @@
 #include "timer.h"
 #include "util.h"
 
+#define BATTERY_WAIT_TIMEOUT 		(2800*MSEC)
+#define BATTERY_NO_RESPONSE_TIMEOUT 	(1000*MSEC)
+
 /* Read battery discharging current
  * unit: mA
  * negative value: charging
@@ -125,6 +128,37 @@ int battery_device_chemistry(char *device_chemistry, int buf_size)
 {
 	return i2c_read_string(I2C_PORT_BATTERY, BATTERY_ADDR,
 		SB_DEVICE_CHEMISTRY, device_chemistry, buf_size);
+}
+
+/* Wait until battery is totally stable */
+int battery_wait_for_stable(void)
+{
+	int status, got_response;
+	uint64_t wait_timeout = get_time().val + BATTERY_WAIT_TIMEOUT;
+	uint64_t no_response_timeout = get_time().val + BATTERY_NO_RESPONSE_TIMEOUT;
+
+	got_response = 0;
+
+	ccprintf("[%T Wait for battery stabilized during %d\n", BATTERY_WAIT_TIMEOUT);
+	while (get_time().val < wait_timeout) {
+		/* Starting pinging battery */
+		if (battery_status(&status) == EC_SUCCESS) {
+			got_response = 1;
+			/* Battery is stable */
+			if (status & STATUS_INITIALIZED) {
+				ccprintf("[%T Battery initialized!!]\n");
+				return EC_SUCCESS;
+			}
+		}
+		/* Assume no battery connected if no response during max count */
+		else if (!got_response && get_time().val > no_response_timeout) {
+			ccprintf("[%T There is no batttery, Go ahead!!]\n");
+			return EC_SUCCESS;
+		}
+		msleep(25); /* clock stretching could hold 25ms */
+	}
+	ccprintf("%T Battery waiting is Timeout!!\n");
+	return EC_ERROR_TIMEOUT;
 }
 
 /*****************************************************************************/
