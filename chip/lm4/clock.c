61dee96d1f2c5d6c07dcb79f62a99073488bf6ff
Mohammed Habibulla <moch@google.com>
2014-05-13 22:32:43 +0000

    Revert "CHERRY-PICK: lm4: Fixed low power idle doesn't always wake up."
    This reverts commit 08794e43283f596aecd7e6efbb1b49d908c1fa1a.
    
    Change-Id: Ic79642b12e7a6e1ce1a982dd935380d239fb0274

--- chip/lm4/clock.c
+++ chip/lm4/clock.c
@@ -235,7 +235,7 @@ void __idle(void)
 	timestamp_t t0, t1, rtc_t0, rtc_t1;
 	int next_delay = 0;
 	int time_for_dsleep, margin_us;
-	int use_low_speed_clock;
+	int use_lfiosc;
 
 	/* Enable the hibernate IRQ used to wake up from deep sleep */
 	system_enable_hib_interrupt();
@@ -300,37 +300,25 @@ void __idle(void)
 			}
 
 			/*
-			 * Determine if we should use a lower clock speed or
-			 * keep the same (16MHz) clock in deep sleep. Use the
-			 * lower speed only if the sleep mask specifies that low
+			 * Determine if we should use the LFIOSC (30kHz) or the
+			 * PIOSC (16MHz) for the clock in deep sleep. Use the
+			 * LFIOSC only if the sleep mask specifies that low
 			 * speed sleep is allowed, the console UART TX is not
 			 * busy, and the console UART buffer is empty.
 			 */
-			use_low_speed_clock = LOW_SPEED_DEEP_SLEEP_ALLOWED &&
+			use_lfiosc = LOW_SPEED_DEEP_SLEEP_ALLOWED &&
 				!uart_tx_in_progress() && uart_buffer_empty();
 
-#ifdef CONFIG_LOW_POWER_USE_LFIOSC
-			/* Set the deep sleep clock register. Use either the
-			 *  normal PIOSC (16MHz) or the LFIOSC (32kHz). */
-			LM4_SYSTEM_DSLPCLKCFG = use_low_speed_clock ?
-					0x32 : 0x10;
-#else
-			/*
-			 * Set the deep sleep clock register. Use either the
-			 * PIOSC with no divider (16MHz) or the PIOSC with
-			 * a /64 divider (250kHz).
-			 */
-			LM4_SYSTEM_DSLPCLKCFG = use_low_speed_clock ?
-					0x1f800010 : 0x10;
-#endif
+			/* Set the deep sleep clock register. */
+			LM4_SYSTEM_DSLPCLKCFG = use_lfiosc ? 0x32 : 0x10;
 
 			/*
-			 * If using low speed clock, disable console.
+			 * If using low speed (LFIOSC) clock, disable console.
 			 * This will also convert the console RX pin to a GPIO
 			 * and set an edge interrupt to wake us from deep sleep
 			 * if any action occurs on console.
 			 */
-			if (use_low_speed_clock)
+			if (use_lfiosc)
 				uart_enter_dsleep();
 
 			/* Set deep sleep bit. */
@@ -361,7 +349,7 @@ void __idle(void)
 			force_time(t1);
 
 			/* If using low speed clock, re-enable the console. */
-			if (use_low_speed_clock)
+			if (use_lfiosc)
 				uart_exit_dsleep();
 
 			/* Record time spent in deep sleep. */
@@ -369,8 +357,6 @@ void __idle(void)
 
 			/* Calculate how close we were to missing deadline */
 			margin_us = next_delay - (int)(rtc_t1.val - rtc_t0.val);
-			if (margin_us < 0)
-				CPRINTF("[%T overslept by %dus]\n", -margin_us);
 
 			/* Record the closest to missing a deadline. */
 			if (margin_us < dsleep_recovery_margin_us)
@@ -711,7 +697,7 @@ DECLARE_CONSOLE_COMMAND(idlestats, command_idle_stats,
 /**
  * Configure deep sleep clock settings.
  */
-static int command_dsleep(int argc, char **argv)
+static int command_dsleepmask(int argc, char **argv)
 {
 	int v;
 
@@ -746,7 +732,7 @@ static int command_dsleep(int argc, char **argv)
 
 	return EC_SUCCESS;
 }
-DECLARE_CONSOLE_COMMAND(dsleep, command_dsleep,
+DECLARE_CONSOLE_COMMAND(dsleepmask, command_dsleepmask,
 			"[ on | off | <timeout> sec]",
 			"Deep sleep clock settings:\nUse 'on' to force deep "
 			"sleep not to use low speed clock.\nUse 'off' to "
