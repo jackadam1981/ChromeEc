0fa8a8ee215649d8483e33265f9d8a47a1aa6d4e
Bill Richardson <wfrichar@chromium.org>
2014-05-14 10:58:08 -0700

    Replace magic numbers with #defines for charger bq24773.c
    BUG=none
    BRANCH=none
    TEST=buildall -j
    
    Change-Id: I125fbe7716f6c8b78011c3a39745ec4992074795
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>

--- driver/charger/bq24773.h
+++ driver/charger/bq24773.h
@@ -28,4 +28,21 @@
 #define OPTION0_CHARGE_INHIBIT          (1 << 0)
 #define OPTION0_LEARN_ENABLE            (1 << 5)
 
+
+/* ChargeCurrent Register - 0x14 (mA) */
+#define CHARGE_I_OFF                    0
+#define CHARGE_I_MIN                    128
+#define CHARGE_I_MAX                    8128
+#define CHARGE_I_STEP                   64
+
+/* MaxChargeVoltage Register - 0x15 (mV) */
+#define CHARGE_V_MIN                    1024
+#define CHARGE_V_MAX                    19200
+#define CHARGE_V_STEP                   16
+
+/* InputCurrent Register - 0x3f (mA) */
+#define INPUT_I_MIN                    128
+#define INPUT_I_MAX                    8064
+#define INPUT_I_STEP                   64
+
 #endif /* __CROS_EC_CHARGER_BQ24773_H */
