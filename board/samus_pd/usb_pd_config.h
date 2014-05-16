fb618ef9839cf4e5b61c4fc64afc9f62705286dc
Alec Berg <alecaberg@chromium.org>
2014-05-15 19:33:50 -0700

    samus_pd: PD configuration bug fixes
    Fix bug to get PD transmit working. Fix config bug
    for reading the CC ADCs.
    
    BUG=chrome-os-partner:28350
    BRANCH=none
    TEST=connect fruitpie and samus via USB-C and run
    pd dev on samus and pd charger on fruitpie.
    
    Change-Id: Ic981a1f1e621ef4b69dedc61a02751346274aa4e
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- board/samus_pd/usb_pd_config.h
+++ board/samus_pd/usb_pd_config.h
@@ -21,7 +21,7 @@
 
 static inline void spi_enable_clock(void)
 {
-	STM32_RCC_APB1ENR |= STM32_RCC_PB1_SPI2;
+	STM32_RCC_APB2ENR |= STM32_RCC_PB2_SPI1;
 }
 
 /* RX is using COMP1 triggering TIM1 CH1 */
@@ -39,7 +39,7 @@ static inline void pd_set_pins_speed(void)
 	/* 40 MHz pin speed on SPI PB3/4/5 */
 	STM32_GPIO_OSPEEDR(GPIO_B) |= 0x00000FC0;
 	/* 40 MHz pin speed on TIM14_CH1 (PB1) */
-	STM32_GPIO_OSPEEDR(GPIO_B) |= 0x000000C0;
+	STM32_GPIO_OSPEEDR(GPIO_B) |= 0x0000000C;
 }
 
 /* Drive the CC line from the TX block */
@@ -76,12 +76,12 @@ static inline void pd_set_host_mode(int enable)
 {
 	if (enable) {
 		/* High-Z is used for host mode. */
-		gpio_set_flags(GPIO_USB_C0_CC1_ODL, GPIO_INPUT);
-		gpio_set_flags(GPIO_USB_C0_CC2_ODL, GPIO_INPUT);
+		gpio_set_level(GPIO_USB_C0_CC1_ODL, 1);
+		gpio_set_level(GPIO_USB_C0_CC2_ODL, 1);
 	} else {
 		/* Pull low for device mode. */
-		gpio_set_flags(GPIO_USB_C0_CC1_ODL, GPIO_OUT_LOW);
-		gpio_set_flags(GPIO_USB_C0_CC2_ODL, GPIO_OUT_LOW);
+		gpio_set_level(GPIO_USB_C0_CC1_ODL, 0);
+		gpio_set_level(GPIO_USB_C0_CC2_ODL, 0);
 	}
 
 }
