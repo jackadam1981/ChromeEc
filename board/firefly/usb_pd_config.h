6b993d6d55977cba267bf76dfd980ea3fc1f8855
Alec Berg <alecaberg@chromium.org>
2014-05-20 09:04:57 -0700

    samus: pd: dual USB-PD port support for samus
    Adds dual USB-PD port support for samus. Both ports are in dual-role
    and can perform either role.
    
    Both ports work fine when only one of the ports is in use. But,
    still having problems with PD errors on the lower priority port (port
    0). If you have a charger plugged into port 0, and a type-C USB dongle
    plugged into port 1, then port 1 has higher priority, and in the
    SRC_DISCONNECTED state, every 1.5 seconds when it sends source cap
    packet, we occasionally drop pings on port 0, which results in a
    lot of start/stop charging.
    
    BUG=chrome-os-partner:28585
    BRANCH=none
    TEST=Tested on samus to make sure both ports work when I
    plug in a charger and a type-C USB dongle with a pull-down on the CC
    line.
    
    Change-Id: Ie7bde3e258f5cd23a0b82b626c0993a45b0df074
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- board/firefly/usb_pd_config.h
+++ board/firefly/usb_pd_config.h
@@ -8,33 +8,58 @@
 #ifndef __USB_PD_CONFIG_H
 #define __USB_PD_CONFIG_H
 
+#include "dma.h"
+#include "util.h"
+
 /* Timer selection for baseband PD communication */
-#define TIM_CLOCK_PD_TX 17
-#define TIM_CLOCK_PD_RX 1
+#define TIM_CLOCK_PD_TX_C0 17
+#define TIM_CLOCK_PD_RX_C0 1
+
+#define TIM_CLOCK_PD_TX(p) TIM_CLOCK_PD_TX_C0
+#define TIM_CLOCK_PD_RX(p) TIM_CLOCK_PD_RX_C0
+
+static timer_ctlr_t * const pd_timers_tx[] = {
+	(void *)STM32_TIM_BASE(TIM_CLOCK_PD_TX_C0),
+};
+BUILD_ASSERT(ARRAY_SIZE(pd_timers_tx) == PD_PORT_COUNT);
+static timer_ctlr_t * const pd_timers_rx[] = {
+	(void *)STM32_TIM_BASE(TIM_CLOCK_PD_RX_C0),
+};
+BUILD_ASSERT(ARRAY_SIZE(pd_timers_rx) == PD_PORT_COUNT);
 
 /* use the hardware accelerator for CRC */
 #define CONFIG_HW_CRC
 
-/* TX is using SPI2 on PB3/PB4 or PA6 */
-#define SPI_REGS STM32_SPI1_REGS
-#define DMAC_SPI_TX STM32_DMAC_CH3
+/* TX is using SPI1 on PB3/PB4 or PA6 */
+#define SPI_REGS(p) STM32_SPI1_REGS
 
-static inline void spi_enable_clock(void)
+static inline void spi_enable_clock(int port)
 {
 	STM32_RCC_APB2ENR |= STM32_RCC_PB2_SPI1;
 }
 
+#define DMAC_SPI_TX(p) STM32_DMAC_CH3
+
 /* RX is using COMP1 triggering TIM1 CH1 */
-#define DMAC_TIM_RX STM32_DMAC_CH2
-#define TIM_CCR_IDX 1
+#define CMP1OUTSEL STM32_COMP_CMP1OUTSEL_TIM1_IC1
+#define CMP2OUTSEL 0
+#define TIM_CCR_IDX(p) 1
 #define TIM_CCR_CS  1
-#define EXTI_COMP_MASK ((1 << 21) | (1 << 22))
+#define EXTI_COMP_MASK(p) ((1 << 21) | (1 << 22))
 #define IRQ_COMP STM32_IRQ_COMP
 /* triggers packet detection on comparator falling edge */
 #define EXTI_XTSR STM32_EXTI_FTSR
 
+#define DMAC_TIM_RX(p) STM32_DMAC_CH2
+static const struct dma_option dma_tim_option[] = {
+	{DMAC_TIM_RX(0),
+		(void *)&STM32_TIM_CCRx(TIM_CLOCK_PD_RX_C0, TIM_CCR_IDX(0)),
+		STM32_DMA_CCR_MSIZE_8_BIT | STM32_DMA_CCR_PSIZE_16_BIT}
+};
+BUILD_ASSERT(ARRAY_SIZE(dma_tim_option) == PD_PORT_COUNT);
+
 /* the pins used for communication need to be hi-speed */
-static inline void pd_set_pins_speed(void)
+static inline void pd_set_pins_speed(int port)
 {
 	/* 40 MHz pin speed on SPI1 PA4/6/7 */
 	STM32_GPIO_OSPEEDR(GPIO_B) |= 0x0000F300;
@@ -45,7 +70,7 @@ static inline void pd_set_pins_speed(void)
 }
 
 /* Reset SPI peripheral used for TX */
-static inline void pd_tx_spi_reset(void)
+static inline void pd_tx_spi_reset(int port)
 {
 	/* Reset SPI1 */
 	STM32_RCC_APB2RSTR |= (1 << 12);
@@ -53,7 +78,7 @@ static inline void pd_tx_spi_reset(void)
 }
 
 /* Drive the CC line from the TX block */
-static inline void pd_tx_enable(int polarity)
+static inline void pd_tx_enable(int port, int polarity)
 {
 	/* put SPI function on TX pin */
 	if (polarity) /* PB4 is SPI1 MISO */
@@ -66,7 +91,7 @@ static inline void pd_tx_enable(int polarity)
 }
 
 /* Put the TX driver in Hi-Z state */
-static inline void pd_tx_disable(int polarity)
+static inline void pd_tx_disable(int port, int polarity)
 {
 	/* put SPI TX in Hi-Z */
 	if (polarity)
@@ -78,7 +103,7 @@ static inline void pd_tx_disable(int polarity)
 }
 
 /* we know the plug polarity, do the right configuration */
-static inline void pd_select_polarity(int polarity)
+static inline void pd_select_polarity(int port, int polarity)
 {
 	/* use the right comparator */
 	STM32_COMP_CSR =
@@ -93,11 +118,11 @@ static inline void pd_tx_init(void)
 	gpio_config_module(MODULE_USB_PD, 1);
 }
 
-static inline void pd_set_host_mode(int enable)
+static inline void pd_set_host_mode(int port, int enable)
 {
 }
 
-static inline int pd_adc_read(int cc)
+static inline int pd_adc_read(int port, int cc)
 {
 	if (cc == 0)
 		return adc_read_channel(ADC_CH_CC1_PD);
@@ -105,7 +130,7 @@ static inline int pd_adc_read(int cc)
 		return adc_read_channel(ADC_CH_CC2_PD);
 }
 
-static inline int pd_snk_is_vbus_provided(void)
+static inline int pd_snk_is_vbus_provided(int port)
 {
 	/* VBUS_WAKE is broken (not detecting 5V), use the ADC instead */
 	return adc_read_channel(ADC_CH_VBUS_SENSE) > 4000;
