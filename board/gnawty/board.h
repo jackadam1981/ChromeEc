3cdd5c2818d804e2218574665f941d320e87bfd3
Jimmy Lin <jimmy.lin@quantatw.com>
2014-05-28 15:16:03 +0800

    Gnawty: Add 8ms delay for gnawty issue 29024.
    BUG=none
    BRANCH=gnawty
    TEST=Run "make BOARD=gnawty" and "make buildall -j" to build
    code and ec.bin can be generated.
    
    Change-Id: I85a65c86217237231f9da1c5e639f17f4b4adb24
    Signed-off-by: Jimmy Lin <jimmy.Lin@quantatw.com>

--- board/gnawty/board.h
+++ board/gnawty/board.h
@@ -9,6 +9,7 @@
 #define __BOARD_H
 
 /* Optional features */
+#define CONFIG_GNAWTY_POWER_SEQUENCE
 #define CONFIG_AP_HANG_DETECT
 #define CONFIG_BACKLIGHT_LID
 #define CONFIG_BATTERY_SMART
--- power/baytrail.c
+++ power/baytrail.c
@@ -344,6 +344,9 @@ enum power_state power_handle_state(enum power_state state)
 
 		/* Turn off power rails */
 		msleep(7);  /* Small delay; see crosbug.com/p/26561 */
+#ifdef CONFIG_GNAWTY_POWER_SEQUENCE
+		msleep(8);  /* Small delay; see Gnawty issue #29024 */
+#endif
 		gpio_set_level(GPIO_PP3300_DX_EN, 0);
 
 #ifdef CONFIG_USB_PORT_POWER_IN_S3
