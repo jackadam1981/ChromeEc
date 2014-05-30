3ac475dc09779ff190fc074254ddb527de0e7048
Vincent Palatin <vpalatin@chromium.org>
2014-05-29 20:51:34 -0700

    twinkie: add USB PD sniffer
    Record the edges on the CC lines to sniff the USB PD traffic.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=chrome-os-partner:28337
    TEST=make BOARD=twinkie
    
    Change-Id: I05c3135e47d0dc848875cbc99e4b57aff52ccbf6

--- common/uart_buffering.c
+++ common/uart_buffering.c
@@ -39,9 +39,9 @@
 				 (CONFIG_UART_RX_DMA_RECHECKS + 1))
 
 /* Transmit and receive buffers */
-static volatile char tx_buf[CONFIG_UART_TX_BUF_SIZE];
-static volatile int tx_buf_head;
-static volatile int tx_buf_tail;
+/*static*/ volatile char tx_buf[CONFIG_UART_TX_BUF_SIZE];
+/*static*/ volatile int tx_buf_head;
+/*static*/ volatile int tx_buf_tail;
 /*static*/ volatile char rx_buf[CONFIG_UART_RX_BUF_SIZE];
 /*static*/ volatile int rx_buf_head;
 /*static*/ volatile int rx_buf_tail;
