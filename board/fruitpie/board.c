98a11042ecfd7029645ba6f7ac317336503abeb0
Dominic Chen <ddchen@chromium.org>
2014-06-02 14:34:16 -0700

    fruitpie: set pin definitions and add new config options for spi flash
    BRANCH=none
    BUG=chromium:346807
    TEST=manually verify spi flash driver functionality
    
    Change-Id: I0bc5e676f91036b32fdad816665b763179cf97dc
    Signed-off-by: Dominic Chen <ddchen@chromium.org>

--- board/fruitpie/board.c
+++ board/fruitpie/board.c
@@ -42,10 +42,8 @@ const struct gpio_info gpio_list[] = {
 	{"USB_CC2_PD",        GPIO_A, (1<<4),  GPIO_ANALOG, NULL},
 	{"PD_CLK_OUT",        GPIO_B, (1<<9),  GPIO_OUT_LOW, NULL},
 	{"PD_TX_EN",          GPIO_B, (1<<12), GPIO_OUT_LOW, NULL},
-#if 0
 	{"PD_CLK_IN",         GPIO_B, (1<<13), GPIO_OUT_LOW, NULL},
 	{"PD_TX_DATA",        GPIO_B, (1<<14), GPIO_OUT_LOW, NULL},
-#endif
 
 	/* Power and muxes control */
 	{"PP5000_EN",         GPIO_A, (1<<5),  GPIO_OUT_HIGH, NULL},
