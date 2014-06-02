479a268ade096f1661cdbe9ab62fddcc0724f95f
Dominic Chen <ddchen@chromium.org>
2014-06-02 14:34:16 -0700

    fruitpie: add new config options for spi flash
    BRANCH=none
    BUG=none
    TEST=manually verify spi flash driver functionality
    
    Change-Id: I0bc5e676f91036b32fdad816665b763179cf97dc
    Signed-off-by: Dominic Chen <ddchen@chromium.org>

--- include/config.h
+++ include/config.h
@@ -740,6 +740,15 @@
 /* Support SPI interfaces */
 #undef CONFIG_SPI
 
+/* Support SPI flash */
+#undef CONFIG_SPI_FLASH
+
+/* Size (bytes) of SPI flash memory */
+#undef CONFIG_SPI_FLASH_SIZE
+
+/* SPI module register used for flash interface */
+#undef CONFIG_SPI_FLASH_REGISTER
+
 /* Default stack size to use for tasks, in bytes */
 #undef CONFIG_STACK_SIZE
 
