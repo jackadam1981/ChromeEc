ad2c37d456a98690d4de17be26b56e6954389a77
Dominic Chen <ddchen@chromium.org>
2014-06-06 14:46:00 -0700

    usb_pd: add support for suspending the task
    used by usb debug, which uses the same spi port
    
    BRANCH=none
    BUG=none
    TEST=make buildall
    
    Change-Id: I9d7e963fc27dc5303a8b87a9ddb68e97600a5a10
    Signed-off-by: Dominic Chen <ddchen@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -196,6 +196,7 @@ static uint8_t pd_polarity;
 
 static enum {
 	PD_STATE_DISABLED,
+	PD_STATE_SUSPENDED,
 #ifdef CONFIG_USB_PD_DUAL_ROLE
 	PD_STATE_SNK_DISCONNECTED,
 	PD_STATE_SNK_DISCOVERY,
@@ -790,6 +791,11 @@ void pd_task(void)
 		case PD_STATE_DISABLED:
 			/* Nothing to do */
 			break;
+		case PD_STATE_SUSPENDED:
+			/* Wait for resume */
+			task_wait_event(-1);
+			pd_hw_init();
+			break;
 		case PD_STATE_SRC_DISCONNECTED:
 			/* Vnc monitoring */
 			cc1_volt = pd_adc_read(0);
@@ -930,6 +936,24 @@ void pd_rx_event(void)
 }
 
 #ifdef CONFIG_COMMON_RUNTIME
+int pd_set_suspend(int enable)
+{
+	int rv = EC_SUCCESS;
+
+	if (enable) {
+		pd_task_state = PD_STATE_SUSPENDED;
+
+		pd_rx_disable_monitoring();
+		pd_power_supply_reset();
+	} else {
+		pd_task_state = PD_DEFAULT_STATE;
+	}
+
+	task_wake(TASK_ID_PD);
+
+	return rv;
+}
+
 void pd_request_source_voltage(int mv)
 {
 	pd_set_max_voltage(mv);
