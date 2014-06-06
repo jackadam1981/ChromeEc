47d1e945a1de30e1c77fd612410b611cea985ca9
Dominic Chen <ddchen@chromium.org>
2014-06-06 14:46:00 -0700

    pd: add support for suspending the task
    used by usb debug, which uses the same spi port
    
    BRANCH=none
    BUG=none
    TEST=verify PD communication works after suspend with two fruitpies
    
    Change-Id: I9d7e963fc27dc5303a8b87a9ddb68e97600a5a10
    Signed-off-by: Dominic Chen <ddchen@chromium.org>

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -316,6 +316,12 @@ void pd_tx_done(int polarity);
  */
 int pd_rx_started(void);
 
+/**
+ * Suspend the PD task.
+ * @param enable pass 0 to resume, anything else to suspend
+ */
+void pd_set_suspend(int enable);
+
 /* Callback when the hardware has detected an incoming packet */
 void pd_rx_event(void);
 /* Start sampling the CC line for reception */
