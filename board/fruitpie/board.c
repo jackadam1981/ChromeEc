3bd86aacf160440d954feef47062a871da166864
Dominic Chen <ddchen@chromium.org>
2014-06-11 15:15:42 -0700

    fruitpie: add board specific initialization and debug code
    remap DMA at startup, add function to switch mux and set gpio for usb debug
    
    BRANCH=none
    BUG=none
    TEST=verify that gpio/mux set correctly for debug
    
    Change-Id: I2ca39025c0cba3b5d04946ec4685a81c4de2d49f
    Signed-off-by: Dominic Chen <ddchen@chromium.org>

--- board/fruitpie/board.c
+++ board/fruitpie/board.c
@@ -13,7 +13,11 @@
 #include "i2c.h"
 #include "registers.h"
 #include "task.h"
+#include "timer.h"
+#include "usb_pd.h"
 #include "util.h"
+
+#include "driver/tsu6721.h"
 
 void rohm_event(enum gpio_signal signal)
 {
@@ -125,6 +129,91 @@ const struct i2c_port_t i2c_ports[] = {
 		GPIO_MASTER_I2C_SCL, GPIO_MASTER_I2C_SDA},
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+void board_config_pre_init(void)
+{
+	/* enable SYSCFG clock */
+	STM32_RCC_APB2ENR |= 1 << 0;
+
+	/* Remap SPI2 to DMA channels 6 and 7 */
+	STM32_SYSCFG_CFGR1 |= (1 << 24);
+}
+
+int board_set_debug(int enable)
+{
+	int rv = EC_SUCCESS;
+
+	if (enable) {
+		/* Suspend the USB PD task */
+		pd_set_suspend(1);
+
+		/* Put power on USB_C_5V_EN pin to allow switching debug mux*/
+		gpio_set_level(GPIO_USB_C_5V_EN, 1);
+
+		/* Decrease BCDv1.2 timer to 0.6s */
+		tsu6721_write(TSU6721_REG_TIMER, 0x05);
+
+		/* Wait for device to be detected */
+		while (!(tsu6721_read(TSU6721_REG_DEV_TYPE3) & 0x74)) {
+			ccputs("Sleeping for 1s, waiting for TSU6721...\n");
+			usleep(1000*MSEC);
+		}
+
+		/* Enable manual switching */
+		rv = tsu6721_mux(TSU6721_MUX_USB);
+		if (rv)
+			return rv;
+
+		/* Switch debug mux */
+		tsu6721_set_pins(TSU6721_PIN_MANUAL2_BOOT);
+
+		/* Set pins PD_CLK_IN, PD_TX_DATA, and
+		 * VCONN1_EN to alternate function. */
+		/* Set pin PD_TX_EN (NSS) to general purpose output mode. */
+		STM32_GPIO_MODER(GPIO_B) &= ~0xff000000;
+		STM32_GPIO_MODER(GPIO_B) |= 0xa9000000;
+
+		/* Set all four pins to alternate function 0 */
+		STM32_GPIO_AFRH(GPIO_B) &= ~(0xffff0000);
+
+		/* Set all four pins to output push-pull */
+		STM32_GPIO_OTYPER(GPIO_B) &= ~(0xf000);
+
+		/* Set all four pins to high speed */
+		STM32_GPIO_OSPEEDR(GPIO_B) |= 0xff000000;
+
+		/* Enable clocks to SPI2 module */
+		STM32_RCC_APB1ENR |= STM32_RCC_PB1_SPI2;
+	} else {
+		/* Disable clocks to SPI2 module */
+		STM32_RCC_APB1ENR &= ~STM32_RCC_PB1_SPI2;
+
+		/* Set all but VCONN1_EN to input mode */
+		STM32_GPIO_MODER(GPIO_B) &= ~0x3f000000;
+
+		/* Set all four pins to low speed */
+		STM32_GPIO_OSPEEDR(GPIO_B) &= ~0xff000000;
+
+		/* Turn off debug mux */
+		tsu6721_set_pins(0);
+
+		/* Disable manual switching */
+		rv = tsu6721_mux(TSU6721_MUX_AUTO);
+		if (rv)
+			return rv;
+
+		/* Restore BCDv1.2 timer to 1.6s */
+		tsu6721_write(TSU6721_REG_TIMER, 0x15);
+
+		/* Disable power on USB_C_5V_EN pin */
+		gpio_set_level(GPIO_USB_C_5V_EN, 0);
+
+		/* Restore the USB PD task */
+		pd_set_suspend(0);
+	}
+
+	return rv;
+}
 
 void board_set_usb_mux(enum typec_mux mux)
 {
