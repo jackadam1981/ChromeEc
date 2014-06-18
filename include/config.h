437399e8f7b2a453e64cd79eda4eb4a5c9ae576a
Alec Berg <alecaberg@chromium.org>
2014-06-17 19:49:17 -0700

    zinger: fix bug, increase watchdog timeout to 1.8s
    Fix bug and actually increase watchdog timeout to 1.8s.
    
    BUG=none
    BRANCH=none
    TEST=put a 3 second blocking delay in pd_task and make
    sure watchdog reboots. set blocking delay to 1.5seconds
    and make sure no reboot.
    
    Change-Id: Ie66621a3bd98354f9fd22b9b10a866d004277340
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- include/config.h
+++ include/config.h
@@ -932,7 +932,7 @@
 #undef CONFIG_WATCHDOG_HELP
 
 /* Watchdog period in ms; must be at least twice HOOK_TICK_INTERVAL */
-#define WATCHDOG_PERIOD_MS 1100
+#define CONFIG_WATCHDOG_PERIOD_MS 1100
 
 /*****************************************************************************/
 
