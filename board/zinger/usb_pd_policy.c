6d43384743be3af558d039238628acc96bd19c37
Vincent Palatin <vpalatin@chromium.org>
2014-06-18 11:00:00 -0700

    zinger: display fast OCP current value
    record and print the current value which triggered the fast OCP watchdog.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=none
    TEST=plug Zinger to Firefly and an electronic load, trigger OCP and read the
    result from the Zinger serial port.
    
    Change-Id: Ied76c453b42446fca6db7833171acb64bc50c998

--- board/zinger/usb_pd_policy.c
+++ board/zinger/usb_pd_policy.c
@@ -182,6 +182,9 @@ void pd_power_supply_reset(void)
 	adc_disable_watchdog();
 }
 
+/* record the current when the last watchdog-triggered OCP happened */
+int last_ocp_current;
+
 int pd_board_checks(void)
 {
 	int vbus_volt, vbus_amp;
@@ -202,6 +205,9 @@ int pd_board_checks(void)
 		adc_enable_watchdog(ADC_CH_A_SENSE, MAX_CURRENT, 0);
 
 	if ((fault == FAULT_FAST_OCP) || (vbus_amp > MAX_CURRENT)) {
+		if (fault == FAULT_FAST_OCP)
+			debug_printf("Fast OCP : %d mA\n",  last_ocp_current *
+			      VDDA_MV / CURR_GAIN * 1000 / R_SENSE / ADC_SCALE);
 		debug_printf("OverCurrent : %d mA\n",
 		  vbus_amp * VDDA_MV / CURR_GAIN * 1000 / R_SENSE / ADC_SCALE);
 		fault = FAULT_OCP;
@@ -241,6 +247,8 @@ int pd_power_negotiation_allowed(void)
 
 void IRQ_HANDLER(STM32_IRQ_ADC_COMP)(void)
 {
+	/* record the current spike */
+	last_ocp_current = STM32_ADC_DR;
 	/* cut the power output */
 	pd_power_supply_reset();
 	/* Clear flags */
