c16b1d21b4d254981ed46639b57871bf09fece3d
Sheng-Liang Song <ssl@chromium.org>
2014-06-24 16:31:58 -0700

    EC: Add util for battery firmware update
    BUG=chrome-os-partner:24741
    BRANCH=ToT
    TEST=TBD
    CQ-DEPEND=CL:204621
    
    Change-Id: Ife4e245c96a833083563ab2e328709388e921309
    Signed-off-by: Sheng-Liang Song <ssl@chromium.org>

--- util/ectool.c
+++ util/ectool.c
@@ -21,6 +21,7 @@
 #include "lock/gec_lock.h"
 #include "misc_util.h"
 #include "panic.h"
+#include "ec_sb_firmware_update.h"
 
 #define GEC_LOCK_TIMEOUT_SECS	30  /* 30 secs */
 
@@ -38,6 +39,8 @@ const char help_str[] =
 	"      Cut off battery output power\n"
 	"  batteryparam\n"
 	"      Read or write board-specific battery parameter\n"
+	"  batteryfwupdate <image>\n"
+	"      Update Smart Battery Firmware\n"
 	"  boardversion\n"
 	"      Prints the board version\n"
 	"  chargecurrentlimit\n"
@@ -3805,6 +3808,16 @@ static int cmd_hang_detect(int argc, char *argv[])
 	return -1;
 }
 
+static int cmd_battery_firmware_update(int argc, char *argv[])
+{
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <fw_filename>\n", argv[0]);
+		return -1;
+	}
+	ec_sb_firmware_update(argv[2]);
+	return 0;
+}
+
 struct command {
 	const char *name;
 	int (*handler)(int argc, char *argv[]);
@@ -3818,6 +3831,7 @@ const struct command commands[] = {
 	{"battery", cmd_battery},
 	{"batterycutoff", cmd_battery_cut_off},
 	{"batteryparam", cmd_battery_vendor_param},
+	{"batteryfwupdate", cmd_battery_firmware_update},
 	{"boardversion", cmd_board_version},
 	{"chargecurrentlimit", cmd_charge_current_limit},
 	{"chargedump", cmd_charge_dump},
