2a7829937895f310bbeba04b26a2ff66bee3b06e
Sheng-Liang Song <ssl@chromium.org>
2014-06-23 17:11:49 -0700

    EC: Add smart battery firmware update driver
    Ref: Common Smart Battery System Inferface Specification v8.0.
    
    BUG=chrome-os-partner:24741
    BRANCH=ToT,glimmer
    TEST=TBD
    
    Change-Id: Ice6e60b1b04762217ae7613356d6925777c06abf
    Signed-off-by: Sheng-Liang Song <ssl@chromium.org>

--- driver/build.mk
+++ driver/build.mk
@@ -39,3 +39,6 @@ driver-$(CONFIG_TEMP_SENSOR_TMP432)+=temp_sensor/tmp432.o
 
 # USB switches
 driver-$(CONFIG_USB_SWITCH_TSU6721)+=usb_switch_tsu6721.o
+
+# Firmware Update
+driver-$(CONFIG_SB_FIRMWARE_UPDATE)+=battery/sb_fw_update.o
