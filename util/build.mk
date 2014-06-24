3a765a5218effdcf879025a42efe9e01bcfa6977
Sheng-Liang Song <ssl@chromium.org>
2014-06-24 16:31:58 -0700

    EC: Add util for battery firmware update
    Using Battery Firmware Update API to update firmware.
    
    BUG=chrome-os-partner:24741
    BRANCH=ToT
    TEST=./ectool batteryfwupdate 45N1748SVT_00000.bin; ./ectool console
    CQ-DEPEND=CL:204621
    
    Change-Id: Ife4e245c96a833083563ab2e328709388e921309
    Signed-off-by: Sheng-Liang Song <ssl@chromium.org>

--- util/build.mk
+++ util/build.mk
@@ -6,7 +6,7 @@
 # Host tools build
 #
 
-host-util-bin=ectool lbplay burn_my_ec
+host-util-bin=ectool lbplay burn_my_ec ec_sb_firmware_update
 
 comm-objs=$(util-lock-objs:%=lock/%) comm-host.o comm-dev.o
 ifeq ($(CHIP),mec1322)
@@ -17,6 +17,9 @@ else
 comm-objs+=comm-i2c.o
 endif
 ectool-objs=ectool.o ectool_keyscan.o misc_util.o ec_flash.o $(comm-objs)
+
+ec_sb_firmware_update-objs=ec_sb_firmware_update.o $(comm-objs) misc_util.o
+
 lbplay-objs=lbplay.o $(comm-objs)
 burn_my_ec-objs=ec_flash.o $(comm-objs) misc_util.o
 
