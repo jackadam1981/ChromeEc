acdab8da630353b366773db57bc7932104870e98
yoojin <yoojin7.lee@samsung.com>
2014-07-02 18:33:48 +0900

    winky : Update charge voltage for NVDC to meet spec.
    Set the charge voltage(0x15) in non-charging state.
    
    When EC set charge voltage below 4096mA,
    charge voltage of charger was actually 9008mA(POR).
    POR can not ensure battery stability.
    
    BUG=chrome-os-partner:30194
    TEST=emerge-winky chromeos-ec
         Check charger values was set normally by read i2c.
         To check the battery can be charging normally.
    
    Change-Id: I3a4f91951f8f8f24ea7741c429ee2b8fa7d19202

--- driver/charger/bq24715.c
+++ driver/charger/bq24715.c
@@ -163,6 +163,12 @@ int charger_get_voltage(int *voltage)
 int charger_set_voltage(int voltage)
 {
 	cached_voltage = voltage;
+
+#ifdef CONFIG_CHARGER_NVDC_VOLTAGE
+	if(voltage == 0)
+		voltage = CONFIG_CHARGER_NVDC_VOLTAGE;
+#endif
+
 	return sbc_write(SB_CHARGING_VOLTAGE, voltage);
 }
 
