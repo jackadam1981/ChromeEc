55958d737f149e86f36e0ba96e5119fda0c5ed2a
yoojin <yoojin7.lee@samsung.com>
2014-07-02 18:33:48 +0900

    winky : Update charger setting values to meet spec.
    Initialize system minimum voltage.
    Set the voltage in none-charging state.
    Change input current value.
    
    BUG=chrome-os-partner:30194
    TEST=emerge-winky chromeos-ec
         Check charger values was set normally by read i2c.
         To check the battery can be charging normally.
    
    Change-Id: I3a4f91951f8f8f24ea7741c429ee2b8fa7d19202

--- board/winky/board.h
+++ board/winky/board.h
@@ -17,7 +17,9 @@
 #define CONFIG_CHARGER_BQ24715
 #define CONFIG_CHARGER_DISCHARGE_ON_AC
 /* TODO(crosbug.com/p/25416): Add correct input current limit for Winky */
-#define CONFIG_CHARGER_INPUT_CURRENT 1700   /* 33 W adapter, 19 V, 1.75 A */
+#define CONFIG_CHARGER_INPUT_CURRENT 2112   /* 33 W adapter, 19 V, 1.75 A */
+#define CONFIG_CHARGER_MIN_SYSTEM_VOLTAGE 5888 /* system minimum voltage */
+#define CONFIG_CHARGER_NVDC_VOLTAGE 8592    /* charging voltage */
 #define CONFIG_CHARGER_SENSE_RESISTOR 10    /* Charge sense resistor, mOhm */
 #define CONFIG_CHARGER_SENSE_RESISTOR_AC 10 /* Input senso resistor, mOhm */
 #define CONFIG_CHIPSET_BAYTRAIL
