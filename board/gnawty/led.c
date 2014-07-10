db279985ad5c3fbf0b19572861ec75b42b877f3f
Jimmy Lin <jimmy.lin@quantatw.com>
2014-07-10 14:43:09 +0800

    Gnawty: Fix Charge during low battery (0%~7%),power LED light show orange and twinkle.
    BUG=chrome-os-partner:29937
    BRANCH=gnawty
    TEST=manual
    Low Battery Case:
    To config Battery LED blinks orange during low Battery(=<13%),
    Check battery LED show amber directly when plugging in adpater.
    Critical Battery Case:
    To config Battery LED blinks orange during Critical Battery(=<7%),
    Check battery LED show amber directly when plugging in adpater.
    Battery full Case:
    To config Battery LED blinks full during Battery near full(=>95%),
    Check battery LED show blue directly when plugging in adpater.
    
    Change-Id: I6bcb205c48508b05f460d87511c85d3dec002dd5
    Signed-off-by: Jimmy Lin <jimmy.Lin@quantatw.com>

--- board/gnawty/led.c
+++ board/gnawty/led.c
@@ -12,6 +12,7 @@
 #include "led_common.h"
 #include "pwm.h"
 #include "util.h"
+#include "extpower.h"
 
 const enum ec_led_id supported_led_ids[] = {
 	EC_LED_ID_BATTERY_LED , EC_LED_ID_POWER_LED};
@@ -146,17 +147,21 @@ static void battery_led(void)
 	/* If Battery critical Low, blink orange, 50% duty cycle,
 	 * 2 sec period.
 	 */
-	if (batt.state_of_charge <= BATTERY_LEVEL_CRITICAL) {
-		set_color_battery_led((battery_ticks & 0x4) ?
-			     LED_ORANGE : LED_OFF);
+	if (!extpower_is_present() && chipset_in_state(CHIPSET_STATE_ON)) {
+		if (batt.state_of_charge <= CONFIG_BATTERY_LEVEL_CRITICAL) {
+			set_color_battery_led((battery_ticks & 0x4) ?
+				     LED_ORANGE : LED_OFF);
 		return;
+		}
 	}
 
 	/* If Battery Low, blink orange, 25% duty cycle, 4 sec period */
-	if (batt.state_of_charge <= BATTERY_LEVEL_LOW) {
-		set_color_battery_led((battery_ticks % 16) < 4 ?
-			     LED_ORANGE : LED_OFF);
+	if (!extpower_is_present() && chipset_in_state(CHIPSET_STATE_ON)) {
+		if (batt.state_of_charge <= CONFIG_BATTERY_LEVEL_LOW) {
+			set_color_battery_led((battery_ticks % 16) < 4 ?
+				     LED_ORANGE : LED_OFF);
 		return;
+		}
 	}
 
 	/* If the system is charging, solid orange */
