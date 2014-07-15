b4ee36098b0ad5e5dbf2330fb88744d9f89f72bf
Louis Yung-Chieh Lo <yjlou@chromium.org>
2014-07-15 10:41:16 -0700

    hadoken: initial commit.
    Board bring up. GPIO / UART / timer / console / task / hook are
    working now.
    
    BRANCH=tot
    BUG=none
    TEST=run on evaluation board and see LED 0/1 are blinking.
    Console commands are available to use.
    
    Change-Id: If93a2c94b8abe1c2c931c03a7a12ddd2bed9d9f6
    Signed-off-by: Louis Yung-Chieh Lo <yjlou@chromium.org>

---
+++ board/hadoken/board.h
@@ -0,0 +1,24 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Hadoken board configuration */
+
+#ifndef __BOARD_H
+#define __BOARD_H
+
+#ifndef __ASSEMBLER__
+
+#undef CONFIG_FLASH /* TODO: implement me */
+#undef CONFIG_FMAP /* TODO: implement me */
+#undef CONFIG_WATCHDOG
+#undef CONFIG_LID_SWITCH
+
+#include "gpio_signal.h"
+
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __BOARD_H */
+
