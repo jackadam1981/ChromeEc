110126c299f3edb24e1b8c8a5f1418160a76f8e4
Dominic Chen <ddchen@chromium.org>
2014-07-15 10:44:16 -0700

    samus: change default battery current to 512mA instead of 2048mA
    this solves brownouts on the host usb port with closed case debugging
    
    BRANCH=none
    BUG=none
    TEST=verify that no brownouts occur
    
    Change-Id: Idf857ac05880cd495e3e231def25af869c8f8242
    Signed-off-by: Dominic Chen <ddchen@chromium.org>

--- driver/battery/samus.c
+++ driver/battery/samus.c
@@ -54,7 +54,7 @@ const struct battery_info *battery_get_info(void)
  * wanted it in the first place.
  */
 /* FIXME(crosbug.com/p/24461): determine correct values for this */
-#define MAX_INPUT_CURRENT 2048
+#define MAX_INPUT_CURRENT 512
 #define INPUT_CURRENT_INCR 64
 
 static int fast_charging_allowed;
