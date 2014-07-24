01607b051a5588f199f8970fdf82efba0c6223de
Sheng-Liang Song <ssl@chromium.org>
2014-07-24 09:22:11 -0700

    EC: Added smbus interface read & write APIs
    Ref: http://smbus.org/specs/smbus20.pdf
    
    BUG=chrome-os-partner:24741
    BRANCH=ToT,glimmer
    TEST=Verified with smart battery firmware update application.
    
    Change-Id: Ic2e7f759af80c06741ed49fee1826213429fbf8a
    Signed-off-by: Sheng-Liang Song <ssl@chromium.org>

--- common/build.mk
+++ common/build.mk
@@ -1,5 +1,5 @@
 # -*- makefile -*-
-# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
@@ -77,4 +77,5 @@ common-$(HAS_TASK_KEYSCAN)+=keyboard_scan.o
 common-$(HAS_TASK_LIGHTBAR)+=lb_common.o lightbar.o
 common-$(HAS_TASK_MOTIONSENSE)+=motion_sense.o math_util.o
 common-$(TEST_BUILD)+=test_util.o
-
+common-$(CONFIG_SMBUS)+= smbus.o
+common-$(CONFIG_CRC8)+= crc8.o
