d3f4669c73fb854ded7fdd4179eae0d950b81b94
Sheng-Liang Song <ssl@chromium.org>
2014-07-24 11:39:06 -0700

    EC: clean up i2c_read_string
    - Removed duplicate (similar) i2c_read_string functions.
    - Kept one copy in common/i2c.c.
    
    BUG=chrome-os-partner:23569
    BRANCH=ToT
    TEST=Verified with smart battery firmware update application on glimmer.
    Passed LGC & Simplo Battery.
    
    Change-Id: I6d9446c60b6a36aef9a6179242c081084199c8e2
    Signed-off-by: Sheng-Liang Song <ssl@chromium.org>

--- common/i2c.c
+++ common/i2c.c
@@ -120,6 +120,35 @@ int i2c_write8(int port, int slave_addr, int offset, int data)
 	return rv;
 }
 
+/* Maximum transfer of a SMBUS block transfer */
+#define SMBUS_MAX_BLOCK 32
+int i2c_read_string(int port, int slave_addr, int offset, uint8_t *data,
+	int len)
+{
+	int rv;
+	uint8_t reg, block_length;
+
+	if ((len <= 0) || (len > SMBUS_MAX_BLOCK))
+		return EC_ERROR_INVAL;
+
+	i2c_lock(port, 1);
+
+	/* Read the counted string into the output buffer */
+	reg = offset;
+	rv = i2c_xfer(port, slave_addr, &reg, 1, data, len, I2C_XFER_SINGLE);
+	if (rv == EC_SUCCESS) {
+		/* Block length is the first byte of the returned buffer */
+		block_length = MIN(data[0], len - 1);
+
+		/* Move data down, then null-terminate it */
+		memmove(data, data + 1, block_length);
+		data[block_length] = 0;
+	}
+
+	i2c_lock(port, 0);
+	return rv;
+}
+
 int get_sda_from_i2c_port(int port, enum gpio_signal *sda)
 {
 	int i;
