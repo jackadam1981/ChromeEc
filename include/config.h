e54aa5fcc0ef953fa2832f2feb6a53d6d01b3ee3
yoojin <yoojin7.lee@samsung.com>
2014-07-28 10:39:38 +0900

    Winky : Update charge voltage for NVDC to meet spec.
    Set the charge voltage(0x15) in non-charging state.
    
    When EC set charge voltage below 4096mA,
    charge voltage of charger was actually 9008mA(POR).
    POR can not ensure battery stability.
    
    BUG=chrome-os-partner:30194
    TEST=emerge-winky chromeos-ec
         Check charger values was set normally by read i2c.
         To check the battery can be charging normally.
    
    Change-Id: Ib90bc91bad7f726b893056c9c705415f51001e00

--- include/config.h
+++ include/config.h
@@ -210,6 +210,12 @@
  */
 #undef CONFIG_CHARGER_INPUT_CURRENT
 
+/*
+ * NVDC needs to keep voltage level though charging stop.
+ * This value should depend on charger.
+*/
+#undef CONFIG_CHARGER_NVDC_VOLTAGE
+
 /* Value of the charge sense resistor, in mOhms */
 #undef CONFIG_CHARGER_SENSE_RESISTOR
 
