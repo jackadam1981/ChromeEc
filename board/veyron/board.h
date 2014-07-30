f46a12a9eccedb2651c266a013f8bbf100ea6fe3
Alexandru M Stan <amstan@chromium.org>
2014-07-30 12:56:37 -0700

    veyron: Porting to the new EC(STM32F072)
    I had to disable a bunch of things to make it work:
    * UART DMA
    * Watchdog
    
    TODO:
    * Check if SPI changes don't break other platforms
    * Lower main clock 48MHz->16MHz
    
    BUG=None
    TEST=Flash the new chip; UART Debug, SPI HC, I2C Battery should work. Everything
    else should work (besides power usage)
    BRANCH=None
    
    Change-Id: Ia73f3f6b4e8b7b42af4dfa592b7ff435ee379206
    Signed-off-by: Alexandru M Stan <amstan@chromium.org>

--- board/veyron/board.h
+++ board/veyron/board.h
@@ -31,6 +31,9 @@
 
 #ifndef __ASSEMBLER__
 
+/* 48 MHz SYSCLK clock frequency */
+#define CPU_CLOCK 48000000
+
 /* Keyboard output port list */
 #define KB_OUT_PORT_LIST GPIO_A, GPIO_B, GPIO_C
 
@@ -40,10 +43,15 @@
 #define I2C_PORT_CHARGER I2C_PORT_MASTER
 
 /* Timer selection */
-#define TIM_CLOCK_MSB 3
-#define TIM_CLOCK_LSB 9
-#define TIM_POWER_LED 2
+#define TIM_CLOCK32 2
 #define TIM_WATCHDOG  4
+
+#undef CONFIG_WATCHDOG_HELP
+#undef CONFIG_UART_TX_DMA
+
+/* Optional features */
+#define CONFIG_STM_HWTIMER32
+#define CONFIG_HW_CRC
 
 #include "gpio_signal.h"
 
