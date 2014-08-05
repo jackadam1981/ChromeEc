b5872f9ab4f9c707ffe1666c73f2b8bc501598e5
yoojin <yoojin7.lee@samsung.com>
2014-08-05 21:22:00 +0900

    Winky : Battery protection by low voltage during 2hours
    Stop to charge and enter battery error state
    when system keeps on low voltage during 2hours.
    
    Battery error state by this reason is cleared only once
    when battery was reconnected.
    
    BUG=chrome-os-partner:30951
    TEST=emerge-winky chromeos-ec
         Check to stop and start charging
         by low voltage during 2hours.
    
    Change-Id: I23f125b822f319de57dd3dad008b8e9c0f4e0479

--- board/winky/battery.c
+++ board/winky/battery.c
@@ -67,6 +67,7 @@ DECLARE_CONSOLE_COMMAND(battcutoff, command_battcutoff,
 			NULL);
 
 #ifdef CONFIG_BATTERY_OVERRIDE_PARAMS
+static timestamp_t start_low_voltage;
 static int oem_battery_state;
 #define OEM_BATTERY_STATE_COMMON 0x00
 #define OEM_BATTERY_STATE_ERROR 0x01
@@ -106,6 +107,17 @@ void battery_override_params(struct batt_params *batt)
 			if(batt->voltage > info.voltage_max) {
 				oem_battery_state |= OEM_BATTERY_STATE_ERROR;
 			}
+			else if(batt->voltage < info.voltage_min) {
+				if((get_time().val - start_low_voltage.val) > 2*HOUR) {
+					oem_battery_state |= OEM_BATTERY_STATE_ERROR;
+				}
+			}
+			else {
+				start_low_voltage = get_time();
+			}
+		}
+		else if((chstate == PWR_STATE_IDLE) || (chstate == PWR_STATE_IDLE0)) {
+			start_low_voltage = get_time();
 		}
 	}
 	if(oem_battery_state != OEM_BATTERY_STATE_COMMON) {
