33712c115997d8de8798fa3c0ae3cf408877df56
Sheng-Liang Song <ssl@chromium.org>
2014-08-13 14:17:07 -0700

    samus: modified i2c driver for ST gyro
    BUG=chrome-os-partner:27313
    BRANCH=ToT
    TEST=TBD
    
    Signed-off-by: Sheng-Liang Song <ssl@chromium.org>
    Change-Id: I5752b00c03e1942c790ea4f28610fda83fa2dcbc

--- include/motion_sense.h
+++ include/motion_sense.h
@@ -97,10 +97,18 @@ enum sensor_location_t {
 	LOCATION_LID,
 };
 
+enum sensor_type_t {
+	SENSOR_ACCELEROMETER = 0x01,
+	SENSOR_GYRO          = 0x02,
+	SENSOR_ACCELGYRO     = SENSOR_ACCELEROMETER | SENSOR_GYRO,
+};
+
 struct motion_sensor_t {
 	char *name;
+	enum sensor_type_t type;
 	enum sensor_location_t location;
-	struct accelgyro_info *drv;
+	struct accelgyro_method *method;
+	struct mutex *mutex;
 	void *drv_data;
 	uint8_t i2c_addr;
 };
