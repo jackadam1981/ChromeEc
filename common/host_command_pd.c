f6e6a1075628970cbf4440c784b5a44afcfc457d
Alec Berg <alecaberg@chromium.org>
2014-08-20 18:07:33 -0700

    samus: add ability for PD MCU to send host event to AP
    Add host event for PD up to AP. The PD toggles a gpio line to
    EC causing an interrupt on EC. The EC then sends host command
    down to PD MCU to get its status. There is a new status bit for
    PD host event, so when EC see's the PD host event status bit,
    it sends a PD host event to the AP.
    
    There is currently only one host event for PD to AP.
    
    BUG=chrome-os-partner:31361
    BRANCH=none
    TEST=added PD console command pdevent, which initiates the host
    event. when sent, verified on EC that it sets the correct host
    event bit using hostevent console command
    
    Change-Id: If1a59a3232e2f9a49f272c6dee5319254d87b9a9
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- common/host_command_pd.c
+++ common/host_command_pd.c
@@ -61,6 +61,10 @@ static void pd_exchange_status(void)
 					CONFIG_CHARGER_INPUT_CURRENT));
 	if (rv < 0)
 		CPRINTS("Failed to set input current limit from PD MCU");
+
+	/* If PD is signalling host event, then pass it up to AP */
+	if (pd_status.status | EC_CMD_PD_STATUS_HOST_EVENT)
+		host_set_single_event(EC_HOST_EVENT_PD_DEVICE);
 }
 
 void pd_command_task(void)
