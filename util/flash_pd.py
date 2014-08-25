cd7a3bf8f39103ff74e2e5ba1618e319ba1599bf
Todd Broch <tbroch@chromium.org>
2014-08-25 12:15:18 -0700

    Append rw_hash to ec.RW.flat for CrOS type-C accessories.
    BRANCH=none
    BUG=chrome-os-partner:31552
    TEST=manual,
    
    BOARD=zinger make
     - See 'RW_HASH ec.RW.flat' during make stdout
    
    make buildall -j
     - Only see 'RW_HASH' cmd executed for hoho & zinger
    
    util/flash_pd.py bulid/zinger/ec.RW.flat
     - flashes zinger rw correctly and see zinger console rboot and jumpt
       to RW.
    
       6.700054] VDM/1 [15] 18d1000f�Power supply started ... RO
       Jump to RW
       Power supply started ... RW
    
    Change-Id: I26983165bca6435b518cf6d5dc1d6a57016af6a0
    Signed-off-by: Todd Broch <tbroch@chromium.org>

--- util/flash_pd.py
+++ util/flash_pd.py
@@ -27,7 +27,7 @@ from servo import multiservo
 VERSION = '0.0.1'
 
 # RW area is half of the 32-kB flash minus the hash storage area
-MAX_FW_SIZE = 16 * 1024 - 32
+MAX_FW_SIZE = 16 * 1024 - 12
 # Hash of RW when erased (set to all F's)
 ERASED_RW_HASH = 'd582e94d 0d12a61c 1199927e 5610c036 2e2870a9'
 
@@ -193,16 +193,12 @@ def flash_pd(options):
 
   with open(options.firmware) as fd:
     fw = fd.read()
-    fw_size = len(fw)
-    # Compute SHA-1 hash for the full (padded) RW firmware
-    padded_fw = fw + '\xff' * (MAX_FW_SIZE - fw_size)
-    sha = hashlib.sha1(padded_fw).digest()
-    sha_str = ' '.join(['%08x' % (w) for w in array.array('I', sha)])
 
-    # pad the firmware to a multiple of 6 U32
-    if fw_size % 24:
-      fw += '\xff'*(24 - fw_size % 24)
-    words = array.array('I', fw)
+  # pad the firmware to a multiple of 6 U32
+  fw_size = len(fw)
+  if fw_size % 24:
+    fw += '\xff'*(24 - fw_size % 24)
+  words = array.array('I', fw)
 
   logging.info('Current PD FW version is %s', ec.get_version())
   if options.versiononly:
@@ -240,13 +236,10 @@ def flash_pd(options):
     if not i % 0x10:
       logging.info('Chunk %d of %d done.', i, len(words) / 6)
 
-  # write new firmware hash
-  ec.flash_command('hash ' + sha_str)
   # reboot in RW
   ec.reboot()
 
   logging.info('Flashing DONE.')
-  logging.info('SHA-1: %s', sha_str)
   logging.info('New PD FW version is %s', ec.get_version())
 
 
