4ababc5fa0053c9f265bdab6297bf366920aa847
Todd Broch <tbroch@chromium.org>
2014-08-25 12:15:18 -0700

    Append rw_hash to ec.RW.flat for CrOS type-C accessories.
    CL adds SHA1 rw_hash to ec.RW.flat file at build time and removes
    inserting it at flash write time from ectool's flash_pd and
    util/flash_pd.py
    
    BRANCH=none
    BUG=chrome-os-partner:31552
    TEST=manual,
    
    BOARD=zinger make
     - See 'RW_HASH ec.RW.flat' during make stdout
    
    make buildall -j
     - Only see 'RW_HASH' cmd executed for hoho & zinger
    
    util/flash_pd.py --board zinger build/zinger/ec.RW.flat
     - flashes zinger rw correctly and see zinger console rboot and jump
       to RW.
    
       6.700054] VDM/1 [15] 18d1000f�Power supply started ... RO
       Jump to RW
       Power supply started ... RW
    
       Also see new version string,
    
        2014-09-03 15:24:41,117 - root - INFO - Current PD FW version is zinger_v1.1.2056-ab8
        2014-09-03 15:24:41,117 - root - INFO - Flashing 16372 bytes
        2014-09-03 15:24:47,489 - root - INFO - Successfully erased flash.
        2014-09-03 15:24:47,600 - root - INFO - Chunk 0 of 683 done.
        ...
        2014-09-03 15:26:10,708 - root - INFO - Flashing DONE.
        2014-09-03 15:26:10,727 - root - INFO - New PD FW version is zinger_v1.1.2186-5e0
    
    ectool --dev=1 --interface=lpc flashpd 0 0 zinger.RW.flat
      - passes
    
    Change-Id: I26983165bca6435b518cf6d5dc1d6a57016af6a0
    Signed-off-by: Todd Broch <tbroch@chromium.org>

--- util/flash_pd.py
+++ util/flash_pd.py
@@ -5,12 +5,11 @@
 """Flash PD PSU RW firmware over the USBPD comm channel using console.
 
   Example:
-    util/flash_pd.py ./build/zinger/ec.RW.flat
+    util/flash_pd.py --board zinger ./build/zinger/ec.RW.flat
 """
 
 import array
 import errno
-import hashlib
 import logging
 import optparse
 import os
@@ -23,11 +22,10 @@ import serial
 # TODO(tbroch): Discuss adding hdctools as an EC package RDEPENDS
 from servo import client
 from servo import multiservo
+from flash_utils import get_rw_flash_size
 
 VERSION = '0.0.1'
 
-# RW area is half of the 32-kB flash minus the hash storage area
-MAX_FW_SIZE = 16 * 1024 - 32
 # Hash of RW when erased (set to all F's)
 ERASED_RW_HASH = 'd582e94d 0d12a61c 1199927e 5610c036 2e2870a9'
 
@@ -125,13 +123,14 @@ class FlashPD(client.ServoClient):
       logging.debug("Expect '%s' missing", val)
     return (done, l)
 
-  def flash_command(self, cmd, expect='DONE 0', retries=2):
+  def flash_command(self, cmd, expect='DONE 0', retries=2, ignore_fail=False):
     """Send PD Flash command and interrogate output.
 
     Args:
-      cmd     : string of 'pd port flash' command to execute
-      expect  : string of expected response after 'cmd'
-      retries : integer number of times to repeat command if it fails.
+      cmd         : string of 'pd port flash' command to execute
+      expect      : string of expected response after 'cmd'
+      retries     : integer number of times to repeat command if it fails.
+      ignore_fail : boolean to ignore failure
 
     Returns:
       tuple :
@@ -151,7 +150,7 @@ class FlashPD(client.ServoClient):
         logging.debug("pd flash cmd Retry%d for '%s'", i, cmd)
       if found:
         break
-    if (i + 1) == tries and not found:
+    if (i + 1) == tries and not found and not ignore_fail:
       raise FlashPDError("Failed pd flash cmd: '%s' after %d retries\n" %
                          (cmd, retries))
     return (found, line)
@@ -193,16 +192,12 @@ def flash_pd(options):
 
   with open(options.firmware) as fd:
     fw = fd.read()
-    fw_size = len(fw)
-    # Compute SHA-1 hash for the full (padded) RW firmware
-    padded_fw = fw + '\xff' * (MAX_FW_SIZE - fw_size)
-    sha = hashlib.sha1(padded_fw).digest()
-    sha_str = ' '.join(['%08x' % (w) for w in array.array('I', sha)])
 
-    # pad the firmware to a multiple of 6 U32
-    if fw_size % 24:
-      fw += '\xff'*(24 - fw_size % 24)
-    words = array.array('I', fw)
+  # pad the firmware to a multiple of 6 U32
+  fw_size = len(fw)
+  if fw_size % 24:
+    fw += '\xff'*(24 - fw_size % 24)
+  words = array.array('I', fw)
 
   logging.info('Current PD FW version is %s', ec.get_version())
   if options.versiononly:
@@ -217,13 +212,25 @@ def flash_pd(options):
   # erase all RW partition
   ec.flash_command('erase')
 
-  # verify that erase was successful by reading hash of RW
-  (done, _) = ec.flash_command('rw_hash', expect=ERASED_RW_HASH)
+  # TODO(tbroch) deprecate rw_hash if/when command is completely deprecated in
+  # favor of 'info'
+  in_rw = 0
+  (done, _) = ec.flash_command('rw_hash', expect=ERASED_RW_HASH, retries=0,
+                               ignore_fail=True)
   if done:
     done = ec.expect('DONE')
+  else:
+    # verify that erase was successful by reading hash of RW
+    (done, line) = ec.flash_command('info', expect=r'INFO')
+    m = re.match(r'INFO.*(18d1\S{4})', line)
+    if done and m:
+      done = ec.expect('DONE 0')
+      in_rw = int(m.group(1), 16) & 0x1
 
   if not done:
     raise FlashPDError('Erase failed')
+  if in_rw:
+    raise FlashPDError('Not in RO after erase')
 
   logging.info('Successfully erased flash.')
 
@@ -240,13 +247,10 @@ def flash_pd(options):
     if not i % 0x10:
       logging.info('Chunk %d of %d done.', i, len(words) / 6)
 
-  # write new firmware hash
-  ec.flash_command('hash ' + sha_str)
   # reboot in RW
   ec.reboot()
 
   logging.info('Flashing DONE.')
-  logging.info('SHA-1: %s', sha_str)
   logging.info('New PD FW version is %s', ec.get_version())
 
 
@@ -273,6 +277,8 @@ def parse_args():
       )
   parser = optparse.OptionParser(version='%prog ' + VERSION)
   parser.description = description
+  parser.add_option('-b', '--board', action='store', type='string',
+                    default=None, help='board name to flash pd to')
   parser.add_option('-d', '--debug', action='store_true', default=False,
                     help='enable debug messages.')
   parser.add_option('-s', '--server', help='host where servod is running',
@@ -308,8 +314,9 @@ def parse_args():
     raise FlashPDError('Unable to find file %s' % options.firmware)
 
   fw_size = os.path.getsize(options.firmware)
-  if fw_size > MAX_FW_SIZE:
-    raise FlashPDError('Firmware too large %d/%d' % (fw_size, MAX_FW_SIZE))
+  max_rw_fw_size = get_rw_flash_size(options.board)
+  if fw_size > max_rw_fw_size:
+    raise FlashPDError('Firmware too large %d/%d' % (fw_size, max_rw_fw_size))
 
   return options
 
