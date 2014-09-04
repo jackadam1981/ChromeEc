9350d941bc199601e16ba84cc2ed54ab6c811624
Alec Berg <alecaberg@chromium.org>
2014-09-03 21:04:23 -0700

    stm32f0: add RTC alarm functionality
    Implement RTC alarm, with resolution 50us, for stm32f0. This
    is useful for using low power modes and waking up after set
    period of time.
    
    BUG=chrome-os-partner:31226, chrome-os-partner:28335
    BRANCH=none
    TEST=tested on samus_pd with CONFIG_CMD_RTC_ALARM defined and
    used rtc_alarm console command to test various timeout periods.
    
    Change-Id: Ibabd8662cfbea654c7de387669f7be83af4fd79d
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- chip/stm32/clock-stm32f0.c
+++ chip/stm32/clock-stm32f0.c
@@ -12,6 +12,8 @@
 #include "cpu.h"
 #include "hooks.h"
 #include "registers.h"
+#include "task.h"
+#include "timer.h"
 #include "util.h"
 
 /* use 48Mhz USB-synchronized High-speed oscillator */
@@ -19,6 +21,128 @@
 
 /* use PLL at 38.4MHz as system clock. */
 #define PLL_CLOCK 38400000
+
+/*
+ * RTC clock frequency (connected to LSI clock)
+ *
+ * TODO(crosbug.com/p/12281): Calibrate LSI frequency on a per-chip basis.  The
+ * LSI on any given chip can be between 30 kHz to 60 kHz.  Without calibration,
+ * LSI frequency may be off by as much as 50%.  Fortunately, we don't do any
+ * high-precision delays based solely on LSI.
+ */
+#define RTC_FREQ (40000 / 2) /* Hz */
+#define RTC_PREDIV_S (RTC_FREQ - 1)
+#define US_PER_RTC_TICK (1000000 / RTC_FREQ)
+
+/* Convert between RTC regs in BCD and seconds */
+static inline uint32_t rtc_to_sec(uint32_t rtc)
+{
+	uint32_t sec;
+
+	/* convert the hours field */
+	sec = (((rtc & 0x300000) >> 20) * 10 + ((rtc & 0xf0000) >> 16)) * 3600;
+	/* convert the minutes field */
+	sec += (((rtc & 0x7000) >> 12) * 10 + ((rtc & 0xf00) >> 8)) * 60;
+	/* convert the seconds field */
+	sec += ((rtc & 0x70) >> 4) * 10 + (rtc & 0xf);
+
+	return sec;
+}
+static inline uint32_t sec_to_rtc(uint32_t sec)
+{
+	uint32_t rtc;
+
+	/* convert the hours field */
+	rtc = ((sec / 36000) << 20) | (((sec / 3600) % 10) << 16);
+	/* convert the minutes field */
+	rtc |= (((sec % 3600) / 600) << 12) | (((sec % 600) / 60) << 8);
+	/* convert the seconds field */
+	rtc |= (((sec % 60) / 10) << 4) | (sec % 10);
+
+	return rtc;
+}
+
+uint32_t set_rtc_alarm(unsigned delay_s, unsigned delay_us)
+{
+	uint32_t rtc, rtcss;
+	uint64_t rtc_us;
+	uint32_t alarm_sec, alarm_us;
+
+	/* Alarm must be within 1 day (86400 seconds) */
+	ASSERT((delay_s + delay_us / SECOND) < 86400);
+
+	/* Make sure alarm is disabled */
+	STM32_RTC_CR &= ~STM32_RTC_CR_ALRAE;
+	while (!(STM32_RTC_ISR & STM32_RTC_ISR_ALRAWF))
+		;
+	STM32_RTC_ISR &= ~STM32_RTC_ISR_ALRAF;
+
+	/* Read current time */
+	rtc = STM32_RTC_TR;
+	rtcss = STM32_RTC_SSR;
+	/*
+	 * Dummy read of date register necessary after reading other two regs
+	 * because value of other regs is locked until after DR is read to
+	 * avoid synchronization issues.
+	 */
+	(void)STM32_RTC_DR;
+
+	/* Calculate alarm time */
+	alarm_sec = rtc_to_sec(rtc);
+	alarm_us = (RTC_PREDIV_S - rtcss) * US_PER_RTC_TICK;
+	rtc_us = (uint64_t)alarm_sec * SECOND + alarm_us;
+	alarm_us += delay_us;
+	alarm_sec = (alarm_sec + delay_s + alarm_us / SECOND) % 86400;
+	alarm_us = alarm_us % 1000000;
+
+	/* Set alarm time */
+	STM32_RTC_ALRMAR = sec_to_rtc(alarm_sec);
+	STM32_RTC_ALRMASSR = RTC_PREDIV_S - (alarm_us / US_PER_RTC_TICK);
+	/* Check for match on hours, minutes, seconds, and subsecond */
+	STM32_RTC_ALRMAR |= 0xc0000000;
+	STM32_RTC_ALRMASSR |= 0x0f000000;
+
+	/* Enable alarm and alarm interrupt */
+	STM32_EXTI_PR = (1 << 17);
+	STM32_EXTI_IMR |= (1 << 17);
+	STM32_RTC_CR |= STM32_RTC_CR_ALRAE;
+
+	/* Return current time */
+	return rtc_us;
+}
+
+uint32_t reset_rtc_alarm(void)
+{
+	uint32_t rtc, rtcss;
+
+	/* Disable alarm */
+	STM32_RTC_CR &= ~STM32_RTC_CR_ALRAE;
+	STM32_RTC_ISR &= ~STM32_RTC_ISR_ALRAF;
+
+	/* Disable RTC alarm interrupt */
+	STM32_EXTI_IMR &= (1 << 17);
+	STM32_EXTI_PR = (1 << 17);
+
+	/* Read current time */
+	rtc = STM32_RTC_TR;
+	rtcss = STM32_RTC_SSR;
+	/*
+	 * Dummy read of date register necessary after reading other two regs
+	 * because value of other regs is locked until after DR is read to
+	 * avoid synchronization issues.
+	 */
+	(void)STM32_RTC_DR;
+
+	/* Return current time */
+	return (uint64_t)rtc_to_sec(rtc) * SECOND +
+		(RTC_PREDIV_S - rtcss) * US_PER_RTC_TICK;
+}
+
+void __rtc_alarm_irq(void)
+{
+	reset_rtc_alarm();
+}
+DECLARE_IRQ(STM32_IRQ_RTC_WAKEUP, __rtc_alarm_irq, 1);
 
 int clock_get_freq(void)
 {
@@ -98,4 +222,63 @@ void clock_init(void)
 #else
 #error "CPU_CLOCK must be either 48MHz or 38.4MHz"
 #endif
+
+	/* Unlock RTC write access */
+	STM32_RTC_WPR = 0xca;
+	STM32_RTC_WPR = 0x53;
+
+	/* Enter RTC initialize mode */
+	STM32_RTC_ISR |= STM32_RTC_ISR_INIT;
+	while (!(STM32_RTC_ISR & STM32_RTC_ISR_INITF))
+		STM32_RTC_ISR |= STM32_RTC_ISR_INIT;
+
+	/*
+	 * Set asynchronous clock freq to HSI/2 (20kHz) to maximize subsecond
+	 * resolution. Set synchronous clock to 1 Hz.
+	 */
+	STM32_RTC_PRER = (1 << 16) | RTC_PREDIV_S;
+
+	/* Start RTC timer */
+	STM32_RTC_ISR &= ~STM32_RTC_ISR_INIT;
+	while (STM32_RTC_ISR & STM32_RTC_ISR_INITF)
+		;
+
+	/* Enable RTC alarm interrupt */
+	STM32_RTC_CR |= STM32_RTC_CR_ALRAIE;
+	STM32_EXTI_RTSR |= (1 << 17);
+	task_enable_irq(STM32_IRQ_RTC_WAKEUP);
 }
+
+/*****************************************************************************/
+/* Console commands */
+
+#ifdef CONFIG_CMD_RTC_ALARM
+static int command_rtc_alarm_test(int argc, char **argv)
+{
+	int s = 1, us = 0;
+	char *e;
+
+	ccprintf("Setting RTC alarm\n");
+
+	if (argc > 1) {
+		s = strtoi(argv[1], &e, 10);
+		if (*e)
+			return EC_ERROR_PARAM1;
+
+	}
+	if (argc > 2) {
+		us = strtoi(argv[2], &e, 10);
+		if (*e)
+			return EC_ERROR_PARAM2;
+
+	}
+
+	set_rtc_alarm(s, us);
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(rtc_alarm, command_rtc_alarm_test,
+			"[seconds [microseconds]]",
+			"Test alarm",
+			NULL);
+#endif /* CONFIG_CMD_RTC_ALARM */
+
