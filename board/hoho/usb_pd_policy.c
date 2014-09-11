5a17885b497fff4896389c410e2d511c1ae97b76
Todd Broch <tbroch@chromium.org>
2014-09-11 15:42:38 -0700

    hoho: Enable USB PD support.
    CL to allow hoho to receive initial USB PD communication (source
    capabilities payload).
    
    BRANCH=none
    BUG=chrome-os-partner:31192
    TEST=manual,
    
    When attaching hoho to fruitpie and configured via
      'pd dualrole source'
    
    I see on hoho side:
    --- UART initialized after reboot ---
    [Reset cause: reset-pin power-on]
    [Image: RO, hoho_v1.1.2213-2bf6a29-dirty 2014-09-15 12:10:22 tbroch@brisket.mtv.corp.google.com]
    [0.000466 Inits done]
    C0 st2
    Console is enabled; type HELP for help.
    > [0.250678 USB PD initialized]
    C0 st3
    [0.264629 PD TMOUT RX 1/1]
    RX ERR (-1)
    Request [1] 5V 3000mA
    C0 st4
    C0 st5
    C0 st6
    
    > pd 0 state
    Port C0, Enabled - Role: SNK Polarity: CC2 State: SNK_READY
    
    Change-Id: Ic5871946425f0ff12d717fbbbbb9e81c6b67cc6f
    Signed-off-by: Todd Broch <tbroch@chromium.org>

---
+++ board/hoho/usb_pd_policy.c
@@ -0,0 +1,121 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "adc.h"
+#include "board.h"
+#include "common.h"
+#include "console.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "registers.h"
+#include "task.h"
+#include "timer.h"
+#include "util.h"
+#include "usb_pd.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
+
+/* Source PDOs */
+const uint32_t pd_src_pdo[] = {
+		PDO_FIXED(5000,   500, PDO_FIXED_EXTERNAL),
+};
+static const int pd_src_pdo_cnts[1] = {
+		[0] = 1,
+};
+
+const int pd_src_pdo_cnt = ARRAY_SIZE(pd_src_pdo);
+
+static int pd_src_pdo_idx;
+
+/* Fake PDOs : we just want our pre-defined voltages */
+const uint32_t pd_snk_pdo[] = {
+		PDO_FIXED(5000,   500, 0),
+};
+const int pd_snk_pdo_cnt = ARRAY_SIZE(pd_snk_pdo);
+
+/* Desired voltage requested as a sink (in millivolts) */
+static unsigned select_mv = 5000;
+
+int pd_choose_voltage(int cnt, uint32_t *src_caps, uint32_t *rdo)
+{
+	int i;
+	int ma;
+	int set_mv = select_mv;
+
+	/* Default to 5V */
+	if (set_mv <= 0)
+		set_mv = 5000;
+
+	/* Get the selected voltage */
+	for (i = cnt; i >= 0; i--) {
+		int mv = ((src_caps[i] >> 10) & 0x3FF) * 50;
+		int type = src_caps[i] & PDO_TYPE_MASK;
+		if ((mv == set_mv) && (type == PDO_TYPE_FIXED))
+			break;
+	}
+	if (i < 0)
+		return -EC_ERROR_UNKNOWN;
+
+	/* request all the power ... */
+	ma = 10 * (src_caps[i] & 0x3FF);
+	*rdo = RDO_FIXED(i + 1, ma, ma, 0);
+	ccprintf("Request [%d] %dV %dmA\n", i, set_mv/1000, ma);
+	return ma;
+}
+
+void pd_set_input_current_limit(uint32_t max_ma)
+{
+	/* No battery, nothing to do */
+	return;
+}
+
+void pd_set_max_voltage(unsigned mv)
+{
+	select_mv = mv;
+}
+
+int requested_voltage_idx;
+int pd_request_voltage(uint32_t rdo)
+{
+	int op_ma = rdo & 0x3FF;
+	int max_ma = (rdo >> 10) & 0x3FF;
+	int idx = rdo >> 28;
+	uint32_t pdo;
+	uint32_t pdo_ma;
+
+	if (!idx || idx > pd_src_pdo_cnts[pd_src_pdo_idx])
+		return EC_ERROR_INVAL; /* Invalid index */
+
+	/* check current ... */
+	pdo = pd_src_pdo[idx - 1];
+	pdo_ma = (pdo & 0x3ff);
+	if (op_ma > pdo_ma)
+		return EC_ERROR_INVAL; /* too much op current */
+	if (max_ma > pdo_ma)
+		return EC_ERROR_INVAL; /* too much max current */
+
+	ccprintf("Switch to %d V %d mA (for %d/%d mA)\n",
+		 ((pdo >> 10) & 0x3ff) * 50, (pdo & 0x3ff) * 10,
+		 ((rdo >> 10) & 0x3ff) * 10, (rdo & 0x3ff) * 10);
+
+	requested_voltage_idx = idx;
+
+	return EC_SUCCESS;
+}
+
+int pd_set_power_supply_ready(int port)
+{
+	return EC_SUCCESS;
+}
+
+void pd_power_supply_reset(int port)
+{
+}
+
+int pd_board_checks(void)
+{
+	return EC_SUCCESS;
+}
+
