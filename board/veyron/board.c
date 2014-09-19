39008e48e421ad3c5b1a72a332a242a970751f52
Alexandru Stan <amstan@chromium.org>
2014-09-19 16:32:33 -0700

    WIP: Trying the pwm power led
    Change-Id: Ie1186db4e1ebaafe0fab9f2666713490b4be3495

--- board/veyron/board.c
+++ board/veyron/board.c
@@ -45,8 +45,8 @@ const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
 /* PWM channels. Must be in the exactly same order as in enum pwm_channel. */
 const struct pwm_t pwm_channels[] = {
-	{STM32_TIM(2), STM32_TIM_CH(3),
-	 PWM_CONFIG_ACTIVE_LOW},
+	{STM32_TIM(1), STM32_TIM_CH(4),
+	 PWM_CONFIG_ACTIVE_LOW, GPIO_BAT_LED1},
 };
 BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
 
