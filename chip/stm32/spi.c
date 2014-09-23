002dc81847311bde8f4f2ab0402856ac9ec45a15
Alexandru M Stan <amstan@chromium.org>
2014-09-23 14:26:00 -0700

    stm32f0: deep sleep in S0 option
    Enabled deep sleeping during S0. This was previously inhibited by
    SLEEP_MASK_AP_RUN but the new config option (CONFIG_LOW_POWER_S0) will ignore it.
    
    SPI needed a sleep mask, it essentially gets enabled and disabled whenever the
    CS changes. Warning: waking up when the CS falls takes a little time, if the AP
    sends fast data before the 48MHz clock is enabled characters will get corrupted;
    leave about 30uS time for the clock to start.
    
    BUG=chrome-os-partner:32223
    BRANCH=none
    TEST=load onto pinky-proto1, boot the AP(S0), use idlestats command to verify
    that we are going into deep sleep (STOP mode). Verify that everything works
    normally during S0(SPI, keyboard, anything that requires the EC to wakeup).
    
    Change-Id: I81c4fa472332f1ad8ad93301089a99dcdb108948
    Signed-off-by: Alexandru M Stan <amstan@chromium.org>

--- chip/stm32/spi.c
+++ chip/stm32/spi.c
@@ -16,6 +16,7 @@
 #include "host_command.h"
 #include "registers.h"
 #include "spi.h"
+#include "system.h"
 #include "timer.h"
 #include "util.h"
 
@@ -442,6 +443,8 @@ void spi_event(enum gpio_signal signal)
 	/* Check chip select.  If it's high, the AP ended a transaction. */
 	nss_reg = gpio_get_level_reg(GPIO_SPI1_NSS, &nss_mask);
 	if (REG16(nss_reg) & nss_mask) {
+		enable_sleep(SLEEP_MASK_SPI);
+
 		/*
 		 * If the buffer is still used by the host command, postpone
 		 * the DMA rx setup.
@@ -456,6 +459,7 @@ void spi_event(enum gpio_signal signal)
 		setup_for_transaction();
 		return;
 	}
+	disable_sleep(SLEEP_MASK_SPI);
 
 	/* Chip select is low = asserted */
 	if (state != SPI_STATE_READY_TO_RX) {
--- include/system.h
+++ include/system.h
@@ -291,6 +291,7 @@ enum {
 	SLEEP_MASK_I2C      = (1 << 2), /* I2C master communication on-going */
 	SLEEP_MASK_CHARGING = (1 << 3), /* Charging loop on-going */
 	SLEEP_MASK_USB_PWR  = (1 << 4), /* USB power loop on-going */
+	SLEEP_MASK_SPI      = (1 << 5), /* SPI communications on-going */
 
 	SLEEP_MASK_FORCE_NO_DSLEEP    = (1 << 15), /* Force disable. */
 
@@ -314,7 +315,13 @@ extern uint32_t sleep_mask;
  * Macros to use to get whether deep sleep is allowed or whether
  * low speed deep sleep is allowed.
  */
+
+#ifndef CONFIG_LOW_POWER_S0
 #define DEEP_SLEEP_ALLOWED           (!(sleep_mask & 0x0000ffff))
+#else
+#define DEEP_SLEEP_ALLOWED           (!(sleep_mask & 0x0000ffff & \
+				       (~SLEEP_MASK_AP_RUN)))
+#endif
 #define LOW_SPEED_DEEP_SLEEP_ALLOWED (!(sleep_mask & 0xffff0000))
 
 /**
