3d9a702863947469c8a6122b139578e5f4377460
Eric Caruso <ejcaruso@chromium.org>
2014-09-29 18:03:38 -0700

    lightbar: remove erroneous multiplications by FP_SCALE
    Not sure why these were around. They were making RAMP_ONCE and
    CYCLE_ONCE do obviously incorrect things.
    
    BUG=None
    BRANCH=ToT
    TEST=Manual, visual inspection
    
    Change-Id: I618078e8bef8824f5449d5b5453924beb36d50e1
    Signed-off-by: Eric Caruso <ejcaruso@chromium.org>

--- common/lightbar.c
+++ common/lightbar.c
@@ -1167,7 +1167,7 @@ static uint32_t lightbyte_RAMP_ONCE(void)
 	}
 
 	for (w = 0; w < 128; w++) {
-		f = cycle_010(w) * FP_SCALE;
+		f = cycle_010(w);
 		for (i = 0; i < NUM_LEDS; i++) {
 			r = get_interp_value(i, LB_COL_RED, f);
 			g = get_interp_value(i, LB_COL_GREEN, f);
@@ -1203,7 +1203,7 @@ static uint32_t lightbyte_CYCLE_ONCE(void)
 	}
 
 	for (w = 0; w < 256; w++) {
-		f = cycle_010(w) * FP_SCALE;
+		f = cycle_010(w);
 		for (i = 0; i < NUM_LEDS; i++) {
 			r = get_interp_value(i, LB_COL_RED, f);
 			g = get_interp_value(i, LB_COL_GREEN, f);
