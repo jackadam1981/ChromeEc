4c1e8066d2eb8849308e96e8a7246f546f28579a
Bill Richardson <wfrichar@chromium.org>
2014-10-07 13:18:19 -0700

    Add initial support for cr50 FPGA SoC
    The serial console works. Nothing else is implemented yet.
    
    BUG=none
    BRANCH=ToT
    TEST=make buildall -j
    
    To build,
    
      make BOARD=cr50 hex
    
    Testing the result requires a Xilinx FPGA development board.
    I have one. It works with HW revision m3.dist_20140918_094011
    
    Change-Id: I718d93572d315d13e96ef6f296c3c2796e928e66
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>

---
+++ chip/g/clock.c
@@ -0,0 +1,11 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "clock.h"
+
+void clock_init(void)
+{
+	/* TODO(wfrichar): What do we need to do here? */
+}
