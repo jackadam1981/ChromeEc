5c5ff6fc6e8d7fe7956c636d5d72665d66bdc32e
Todd Broch <tbroch@chromium.org>
2014-10-13 15:45:50 -0700

    pd: dingdong/hoho: HPD over USB PD.
    HPD needs to be transported of USB PD as both SBU lines are consumed
    for differential AUX signalling.
    
    This CL does the following:
    1. Enables GPIO DP_HPD as interrupt
    2. Sends debounced HPD across CC via the SVDM DP status message
    
    BRANCH=none
    BUG=chrome-os-partner:31192,chrome-os-partner:31193
    TEST=manual
    
    Change-Id: I976c268467ece84cedab7ba4943fb59d1e48c113
    Signed-off-by: Todd Broch <tbroch@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -1751,6 +1751,20 @@ static int remote_flashing(int argc, char **argv)
 	return EC_SUCCESS;
 }
 
+#if defined(CONFIG_USB_PD_ALT_MODE) && !defined(CONFIG_USB_PD_ALT_MODE_DFP)
+void pd_send_hpd(enum hpd_event hpd)
+{
+	uint32_t data[1];
+	data[0] = VDO_DP_STATUS((hpd == hpd_irq),
+				(hpd == hpd_high),
+				0, 0, 0, 0, 0, 0);
+	pd_send_vdm(0, USB_SID_DISPLAYPORT, CMD_DP_STATUS, data, 1);
+	/* Wait until VDM is done */
+	while (pd[0].vdm_state > 0)
+		task_wait_event(300);
+}
+#endif
+
 void pd_request_source_voltage(int port, int mv)
 {
 	pd_set_max_voltage(mv);
