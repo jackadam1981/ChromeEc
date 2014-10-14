0a1171ee671d51248fe69fa1d44d1967d98e566f
Vic Yang <victoryang@chromium.org>
2014-10-14 15:27:16 -0700

    Extend INA231 driver to support INA219 on Plankton
    The register format of INA231 and INA219 are very much alike. In our
    use case, we only need to use different coversion coefficient.
    
    BUG=chrome-os-partner:32764
    TEST='ina 0' on Plankton V2.
    TEST=Build twinkie.
    BRANCH=None
    
    do not submit
    
    Change-Id: I9c8e21e30ed844566793dcc1221f865400c3d90d
    Signed-off-by: Vic Yang <victoryang@chromium.org>

---
+++ driver/ina2xx.c
@@ -0,0 +1,167 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * TI INA219/231 Current/Power monitor driver.
+ */
+
+#include "console.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "system.h"
+#include "timer.h"
+#include "ina2xx.h"
+#include "uart.h"
+#include "util.h"
+
+/* Console output macros */
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+
+/* 8-bit I2C base address */
+#define INA2XX_I2C_ADDR (0x40 << 1)
+
+/*
+ * Bit mask indicating INA chip type. Bit 'x' corresponds to chip with
+ * index 'x'. If the bit is set, it's an INA231. Otherwise, INA219.
+ */
+static uint16_t ina_type;
+
+#define IS_INA231(x) (ina_type & (1 << (x)))
+
+uint16_t ina2xx_read(uint8_t idx, uint8_t reg)
+{
+	int res;
+	int val;
+	uint8_t addr = INA2XX_I2C_ADDR | (idx << 1);
+
+	res = i2c_read16(I2C_PORT_MASTER, addr, reg, &val);
+	if (res) {
+		CPRINTS("INA2XX I2C read failed");
+		return 0x0bad;
+	}
+	return (val >> 8) | ((val & 0xff) << 8);
+}
+
+int ina2xx_write(uint8_t idx, uint8_t reg, uint16_t val)
+{
+	int res;
+	uint8_t addr = INA2XX_I2C_ADDR | (idx << 1);
+	uint16_t be_val = (val >> 8) | ((val & 0xff) << 8);
+
+	res = i2c_write16(I2C_PORT_MASTER, addr, reg, be_val);
+	if (res)
+		CPRINTS("INA2XX I2C write failed");
+	return res;
+}
+
+int ina2xx_init(uint8_t idx, uint16_t config, uint16_t calib, int type)
+{
+	int res;
+	uint16_t mask = 1 << idx;
+
+	res = ina2xx_write(idx, INA2XX_REG_CONFIG, config);
+	/* TODO(crosbug.com/p/29730): assume 1mA/LSB, revisit later */
+	res |= ina2xx_write(idx, INA2XX_REG_CALIB, calib);
+
+	ina_type = (ina_type & ~mask) | (type << mask);
+
+	return res;
+}
+
+int ina2xx_get_voltage(uint8_t idx)
+{
+	uint16_t bv = ina2xx_read(idx, INA2XX_REG_BUS_VOLT);
+	if (IS_INA231(idx))
+		return INA231_BUS_MV((int)bv);
+	else
+		return INA219_BUS_MV((int)bv);
+}
+
+int ina2xx_get_current(uint8_t idx)
+{
+	int16_t curr = ina2xx_read(idx, INA2XX_REG_CURRENT);
+	/* Current calibration: LSB = 1mA/bit */
+	return (int)curr;
+}
+
+int ina2xx_get_power(uint8_t idx)
+{
+	uint16_t pow = ina2xx_read(idx, INA2XX_REG_POWER);
+	if (IS_INA231(idx))
+		return INA231_POW_MW((int)pow);
+	else
+		return INA219_POW_MW((int)pow);
+}
+
+static void ina2xx_dump(uint8_t idx)
+{
+	uint16_t cfg = ina2xx_read(idx, INA2XX_REG_CONFIG);
+	int16_t sv = ina2xx_read(idx, INA2XX_REG_SHUNT_VOLT);
+	int sv_uv = IS_INA231(idx) ? INA231_SHUNT_UV((int)sv) :
+				     INA219_SHUNT_UV((int)sv);
+	uint16_t bv = ina2xx_read(idx, INA2XX_REG_BUS_VOLT);
+	int bv_mv = IS_INA231(idx) ? INA231_BUS_MV((int)bv) :
+				     INA219_BUS_MV((int)bv);
+	uint16_t pow = ina2xx_read(idx, INA2XX_REG_POWER);
+	int pow_mw = IS_INA231(idx) ? INA231_POW_MW((int)pow) :
+				      INA219_POW_MW((int)pow);
+	int16_t curr = ina2xx_read(idx, INA2XX_REG_CURRENT);
+	uint16_t calib = ina2xx_read(idx, INA2XX_REG_CALIB);
+	uint16_t mask = ina2xx_read(idx, INA2XX_REG_MASK);
+	uint16_t alert = ina2xx_read(idx, INA2XX_REG_ALERT);
+
+	ccprintf("Configuration: %04x\n", cfg);
+	ccprintf("Shunt voltage: %04x => %d uV\n", sv, sv_uv);
+	ccprintf("Bus voltage  : %04x => %d mV\n", bv, bv_mv);
+	ccprintf("Power        : %04x => %d mW\n", pow, pow_mw);
+	ccprintf("Current      : %04x => %d mA\n", curr, curr);
+	ccprintf("Calibration  : %04x\n", calib);
+	ccprintf("Mask/Enable  : %04x\n", mask);
+	ccprintf("Alert limit  : %04x\n", alert);
+}
+
+/*****************************************************************************/
+/* Console commands */
+
+static int command_ina(int argc, char **argv)
+{
+	char *e;
+	int idx;
+	uint16_t val;
+
+	if (argc < 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	idx = strtoi(argv[1], &e, 10);
+	if (*e)
+		return EC_ERROR_PARAM1;
+
+	if (2 == argc) { /* dump all registers */
+		ina2xx_dump(idx);
+		return EC_SUCCESS;
+	} else if (4 == argc) {
+		val = strtoi(argv[3], &e, 16);
+		if (*e)
+			return EC_ERROR_PARAM3;
+
+		if (!strcasecmp(argv[2], "config")) {
+			ina2xx_write(idx, INA2XX_REG_CONFIG, val);
+		} else if (!strcasecmp(argv[2], "calib")) {
+			ina2xx_write(idx, INA2XX_REG_CALIB, val);
+		} else if (!strcasecmp(argv[2], "mask")) {
+			ina2xx_write(idx, INA2XX_REG_MASK, val);
+		} else if (!strcasecmp(argv[2], "alert")) {
+			ina2xx_write(idx, INA2XX_REG_ALERT, val);
+		} else { /* read one register */
+			ccprintf("Invalid register: %s\n", argv[1]);
+			return EC_ERROR_INVAL;
+		}
+		return EC_SUCCESS;
+	}
+
+	return EC_ERROR_INVAL;
+}
+DECLARE_CONSOLE_COMMAND(ina, command_ina,
+			"<index> [config|calib|mask|alert <val>]",
+			"INA2XX power/current sensing",
+			NULL);
