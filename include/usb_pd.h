3380d59357546d9275fe20804b52443ceb1f4c84
Vic Yang <victoryang@chromium.org>
2014-10-14 14:07:03 -0700

    plankton: Implement cable flip
    When the cable flip button is pressed, instead of only flipping on
    Plankton side, we should also signal the port partner to flip. This is
    done by sending a custom VDM. Upon receiving the flip VDM, the port
    partner is responsible of flipping the DP/USB polarity.
    
    BUG=chrome-os-partner:32163
    TEST=COnnect Plankton and Ryu. Press the button on Plankton and make
    sure the polarity GPIOs on Ryu are negated.
    BRANCH=Samus
    
    Change-Id: I7ee5ea70067de4f422a7478623fe7fe8d3724372
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -215,6 +215,7 @@ struct pd_policy {
 #define VDO_CMD_ERASE_SIG    VDO_CMD_VENDOR(8)
 #define VDO_CMD_PING_ENABLE  VDO_CMD_VENDOR(10)
 #define VDO_CMD_CURRENT      VDO_CMD_VENDOR(11)
+#define VDO_CMD_FLIP         VDO_CMD_VENDOR(12)
 
 #define PD_VDO_VID(vdo)  ((vdo) >> 16)
 #define PD_VDO_SVDM(vdo) (((vdo) >> 15) & 1)
@@ -680,6 +681,13 @@ void board_set_usb_mux(int port, enum typec_mux mux, int polarity);
  * @return Non-zero if superspeed connection is enabled; otherwise, zero.
  */
 int board_get_usb_mux(int port, const char **dp_str, const char **usb_str);
+
+/**
+ * Flip the superspeed muxes on type-C port.
+ *
+ * @param port port number.
+ */
+void board_flip_usb_mux(int port);
 
 /* --- Physical layer functions : chip specific --- */
 
