5c82fa2d418935bb1d1c57cf178bd5c5684569d8
Todd Broch <tbroch@chromium.org>
2014-10-16 18:17:00 -0700

    pd: alternate mode: simplify mode entry choice.
    Original implementation was complicated by belief that we'd want PD
    MCU to manage entry of multiple alternate modes.  This simply won't be
    practical given the upper level system policies that would need to
    weigh in on these decisions as well as the seemingly endless additions
    to the alternate mode ecosystem.
    
    Longer term we'll need to pass the generic alternate mode discovery
    VDO info to the kernel/userland to implement remainder of policy.
    
    However, for short term lets implement single mode entry instead.
    
    BRANCH=none
    BUG=chrome-os-partner:30645
    TEST=manual, mode entry is successful.
    
    Change-Id: Ia24f5ee4d59c13c62d68b30f8587b5e5fbdb2fa0
    Signed-off-by: Todd Broch <tbroch@chromium.org>

--- common/usb_pd_policy.c
+++ common/usb_pd_policy.c
@@ -96,46 +96,34 @@ static int dfp_consume_modes(int port, uint32_t *payload)
 	return (pe[port].svid_idx < pe[port].svid_cnt);
 }
 
-static int dfp_enter_modes(int port, uint32_t *payload)
+static int dfp_enter_mode(int port, uint32_t *payload)
 {
-	struct svdm_amode_data *modep;
-	if (pe[port].amodes == NULL)
-		pd_dfp_choose_modes(&pe[port]);
-
-	modep = &pe[port].amodes[pe[port].amode_idx];
-	if (modep->amode == dfp_amode_none)
+	struct svdm_amode_data *modep = &pe[port].amode;
+	pd_dfp_choose_mode(&pe[port]);
+	if (modep->index == -1)
 		return 0;
 
-	modep->fx->enter(port, *modep->mode_caps);
+	modep->fx->enter(port, modep->mode_caps);
 	payload[0] = VDO(modep->fx->svid, 1,
 			 CMD_ENTER_MODE |
-			 VDO_OPOS(modep->amode));
-	pe[port].amode_idx++;
+			 VDO_OPOS((modep->index + 1)));
 	return 1;
 }
 
-int pd_exit_modes(int port, uint32_t *payload)
+int pd_exit_mode(int port, uint32_t *payload)
 {
-	struct svdm_amode_data *modep;
-	if (pe[port].amodes == NULL)
-		return 0;
-
-	modep = &pe[port].amodes[pe[port].amode_idx];
-	if (modep->amode == dfp_amode_none)
-		return 1;
-
+	struct svdm_amode_data *modep = &pe[port].amode;
 	modep->fx->exit(port);
 	payload[0] = VDO(modep->fx->svid, 1,
 			 CMD_EXIT_MODE |
-			 VDO_OPOS(modep->amode));
-	pe[port].amode_idx++;
+			 VDO_OPOS((modep->index + 1)));
 	return 1;
 }
 
 static void dump_pe(int port)
 {
 	int i, j;
-	struct svdm_amode_data *modep = pe[port].amodes;
+	struct svdm_amode_data *modep = &pe[port].amode;
 
 	for (i = 0; i < pe[port].svid_cnt; i++) {
 		ccprintf("SVID[%d]: %04x", i, pe[port].svids[i].svid);
@@ -144,11 +132,8 @@ static void dump_pe(int port)
 				 pe[port].svids[i].mode_vdo[j]);
 		ccprintf("\n");
 	}
-	for (i = 0; i < pe[port].amode_cnt; i++) {
-		ccprintf("MODE[%d]: svid:%04x mode:%d caps:%08x\n", i,
-			 modep->fx->svid, modep->amode, *modep->mode_caps);
-		modep++;
-	}
+	ccprintf("MODE[%d]: svid:%04x mode:%d caps:%08x\n", i,
+		 modep->fx->svid, modep->index + 1, modep->mode_caps);
 }
 
 static int command_pe(int argc, char **argv)
@@ -235,12 +220,12 @@ int pd_svdm(int port, int cnt, uint32_t *payload, uint32_t **rpayload)
 			if (dfp_consume_modes(port, payload))
 				rsize = dfp_discover_modes(port, payload);
 			else
-				rsize = dfp_enter_modes(port, payload);
+				rsize = dfp_enter_mode(port, payload);
 			break;
 		case CMD_ENTER_MODE:
-			rsize = dfp_enter_modes(port, payload);
+			rsize = dfp_enter_mode(port, payload);
 		case CMD_EXIT_MODE:
-			rsize = pd_exit_modes(port, payload);
+			rsize = pd_exit_mode(port, payload);
 			break;
 		}
 		payload[0] &= ~VDO_CMDT(0);
@@ -292,7 +277,7 @@ int pd_vdm(int port, int cnt, uint32_t *payload, uint32_t **rpayload)
 #endif /* !CONFIG_USB_PD_CUSTOM_VDM */
 
 #ifndef CONFIG_USB_PD_ALT_MODE_DFP
-int pd_exit_modes(int port, uint32_t *payload)
+int pd_exit_mode(int port, uint32_t *payload)
 {
 	return 0;
 }
