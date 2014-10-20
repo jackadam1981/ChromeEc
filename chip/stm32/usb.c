4320ad1a6417dd77b4cbf35d97d1d7a508d346c2
Todd Broch <tbroch@chromium.org>
2014-10-20 16:58:56 -0700

    pd: dingdong/hoho: inhibit USB Billboard.
    USB Billboard class can be used to advertise an alternate mode capable
    device that hasn't entered a mode.  Additionally it can remain after
    mode entry providing its Billboard capabilities descriptor is
    updated.
    
    This CL postpones enumeration which previously occurred after boot
    until tAMETimeout has passed and alternate mode has NOT been entered.
    Future CL could choose to also (re)enumerate with mode capabilities
    although this is not required by the USB PD specification.
    
    BRANCH=none
    BUG=chrome-os-partner:31192,chrome-os-partner:31193
    TEST=manual,
    With DFP_U which does not enter mode see Billboard class enumerate
    else it does not.
    
    Change-Id: I59a0815cd0ea551ba9a878907c0184df4ba9480c
    Signed-off-by: Todd Broch <tbroch@chromium.org>

--- chip/stm32/usb.c
+++ chip/stm32/usb.c
@@ -312,6 +312,30 @@ void usb_init(void)
 DECLARE_HOOK(HOOK_INIT, usb_init, HOOK_PRIO_DEFAULT);
 #endif
 
+void usb_disconnect(void)
+{
+	/* disable pull-up on DP to disconnect */
+#ifdef CHIP_VARIANT_STM32L15X
+	STM32_SYSCFG_PMC &= ~1;
+#elif defined(CHIP_FAMILY_STM32F0)
+	STM32_USB_BCDR &= ~(1 << 15) /* DPPU */;
+#else
+#error "usb disconnect not implemented for this chip family"
+#endif
+}
+
+void usb_connect(void)
+{
+	/* enable pull-up on DP to connect */
+#ifdef CHIP_VARIANT_STM32L15X
+	STM32_SYSCFG_PMC |= 1;
+#elif defined(CHIP_FAMILY_STM32F0)
+	STM32_USB_BCDR |= (1 << 15) /* DPPU */;
+#else
+#error "usb connect not implemented for this chip family"
+#endif
+}
+
 void usb_release(void)
 {
 	/* signal disconnect to host */
