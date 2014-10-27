750d85145b4c4719c449d900d94cadcee40cc96b
Yen Lin <yelin@nvidia.com>
2014-10-27 15:45:06 -0700

    kitty: add keyboard support
    Add keyboard support back to kitty so FAFTSetup test can pass because
    the test uses EC keyboard simulation.
    
    Note: I removed USB_CTL1, USB_CTL2, and USB_CTL3 gpio definitions as
    they conflict with KB_INx definitions and are not used.
    
    BUG=chrome-os-partner:33063
    BRANCH=kitty
    TEST=pass firmware_FAFTSetup test on nyan_kitty
    
    Change-Id: If5010dc163974247b1a6334f240f274d7ba7384c
    Signed-off-by: Yen Lin <yelin@nvidia.com>

--- board/kitty/board.c
+++ board/kitty/board.c
@@ -9,6 +9,8 @@
 #include "extpower.h"
 #include "gpio.h"
 #include "i2c.h"
+#include "keyboard_raw.h"
+#include "lid_switch.h"
 #include "power.h"
 #include "power_button.h"
 #include "pwm.h"
@@ -19,6 +21,8 @@
 #include "util.h"
 #include "timer.h"
 
+#define GPIO_KB_INPUT  (GPIO_INPUT | GPIO_PULL_UP | GPIO_INT_BOTH)
+#define GPIO_KB_OUTPUT GPIO_ODR_HIGH
 
 /* GPIO signal list.  Must match order from enum gpio_signal. */
 const struct gpio_info gpio_list[] = {
@@ -33,9 +37,14 @@ const struct gpio_info gpio_list[] = {
 	{"SPI1_NSS",            GPIO_A, (1<<4),  GPIO_INT_BOTH | GPIO_PULL_UP,
 	 spi_event},
 	{"AC_PRESENT",          GPIO_A, (1<<0),  GPIO_DEFAULT, NULL},
-	{"USB_CTL3",            GPIO_C, (1<<11),  GPIO_ODR_HIGH, NULL},
-	{"USB_CTL2",            GPIO_C, (1<<12),  GPIO_ODR_HIGH, NULL},
-	{"USB_CTL1",            GPIO_C, (1<<15),  GPIO_OUT_LOW, NULL},
+	{"KB_IN00",             GPIO_C, (1<<8),  GPIO_KB_INPUT,  NULL},
+	{"KB_IN01",             GPIO_C, (1<<9),  GPIO_KB_INPUT,  NULL},
+	{"KB_IN02",             GPIO_C, (1<<10), GPIO_KB_INPUT,  NULL},
+	{"KB_IN03",             GPIO_C, (1<<11), GPIO_KB_INPUT,  NULL},
+	{"KB_IN04",             GPIO_C, (1<<12), GPIO_KB_INPUT,  NULL},
+	{"KB_IN05",             GPIO_C, (1<<14), GPIO_KB_INPUT,  NULL},
+	{"KB_IN06",             GPIO_C, (1<<15), GPIO_KB_INPUT,  NULL},
+	{"KB_IN07",             GPIO_D, (1<<2),  GPIO_KB_INPUT,  NULL},
 	{"WP_L",                GPIO_B, (1<<4),  GPIO_INPUT, NULL},
 	{"AP_RESET_L",          GPIO_B, (1<<3),  GPIO_ODR_HIGH, NULL},
 	{"EC_INT",              GPIO_B, (1<<9),  GPIO_ODR_HIGH, NULL},
@@ -44,6 +53,19 @@ const struct gpio_info gpio_list[] = {
 	{"I2C1_SDA",            GPIO_B, (1<<7),  GPIO_ODR_HIGH, NULL},
 	{"PMIC_PWRON_L",        GPIO_A, (1<<12),  GPIO_OUT_HIGH, NULL},
 	{"PMIC_RESET",          GPIO_A, (1<<15),  GPIO_OUT_LOW, NULL},
+	{"KB_OUT00",            GPIO_B, (1<<0),  GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT01",            GPIO_B, (1<<8),  GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT02",            GPIO_B, (1<<12), GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT03",            GPIO_B, (1<<13), GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT04",            GPIO_B, (1<<14), GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT05",            GPIO_B, (1<<15), GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT06",            GPIO_C, (1<<0),  GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT07",            GPIO_C, (1<<1),  GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT08",            GPIO_C, (1<<2),  GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT09",            GPIO_B, (1<<1),  GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT10",            GPIO_C, (1<<5),  GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT11",            GPIO_C, (1<<4),  GPIO_KB_OUTPUT, NULL},
+	{"KB_OUT12",            GPIO_A, (1<<13), GPIO_KB_OUTPUT, NULL},
 	{"PWR_LED0",            GPIO_B, (1<<10),  GPIO_OUT_LOW, NULL},
 	{"USB_ILIM_SEL",        GPIO_A, (1<<8),  GPIO_OUT_LOW, NULL},
 	{"USB_STATUS_L",        GPIO_A, (1<<11),  GPIO_INT_BOTH, NULL},
