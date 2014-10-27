17ea57e889ef87a19e75774a5c8c200b9b12c600
Gwendal Grignou <gwendal@chromium.org>
2014-10-27 14:46:23 -0700

    ectool: Add decoding for MOTION_GET_DATA command
    Similar to MOTION_CMD_DUMP but can handle a dynamic
    number of sensors.
    - The host sends the number of sensor it has allocated space for.
    - If 0, the EC just sends the number of sensors available
    - Otherwise returns sensor information up to the limit imposed by the host.
    
    Remvoe MOTIONSENSE_GET_STATUS: not needed. It is only useful for LPC,
    to guarantee atomicity of the data.
    
    BRANCH=ToT
    BUG=chrome-os-partner:31071,chromium:430792
    TEST=Compile. On a firmware that support the new command:
    /usr/sbin/ectool --name=cros_sh motionsense
    Motion sensing active
    Sensor 0: 92    15      1030
    Sensor 1: -94   -63     718
    /usr/sbin/ectool --name=cros_sh motionsense active
    0
    On a machine with older firmware (samus), check these
    functions are still working.
    
    Change-Id: Id3c062c9430fabdc18d454bda00f856de067503b
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- util/ectool.c
+++ util/ectool.c
@@ -2335,7 +2335,8 @@ static int cmd_lightbar(int argc, char **argv)
 		sizeof(((struct ec_params_motion_sense *)0)->data) \
 		+ sizeof(((struct ec_params_motion_sense *)0)->cmd), \
 		sizeof(((struct ec_response_motion_sense *)0)->data) \
-		+ ECTOOL_MAX_SENSOR * sizeof(struct sensor_data) }
+		+ sizeof(struct ec_response_motion_sensor_data) * \
+		  ECTOOL_MAX_SENSOR }
 static const struct {
 	uint8_t insize;
 	uint8_t outsize;
@@ -2346,7 +2347,6 @@ static const struct {
 	MS_SIZES(sensor_odr),
 	MS_SIZES(sensor_range),
 	MS_SIZES(kb_wake_angle),
-	MS_SIZES(status),
 	MS_DATA_SIZE(),
 };
 BUILD_ASSERT(ARRAY_SIZE(ms_command_sizes) == MOTIONSENSE_NUM_CMDS);
@@ -2368,67 +2368,103 @@ static int ms_help(const char *cmd)
 
 static int cmd_motionsense(int argc, char **argv)
 {
-	int i, rv;
+	int i, rv, status_only = (argc == 2);
 	struct ec_params_motion_sense param;
 	struct ec_response_motion_sense resp;
 	char *e;
+	/*
+	 * Warning: the following strings printed out are read in an
+	 * autotest. Do not change string without consulting autotest
+	 * for kernel_CrosECSysfsAccel.
+	 */
+	const char *motion_status_string[2][2] = {
+		{ "Motion sensing inactive", "0"},
+		{ "Motion sensing active", "1"},
+	};
 
 	/* No motionsense command has more than 5 args. */
 	if (argc > 5)
 		return ms_help(argv[0]);
 
-	if (argc == 1) {
-		/* No args, dump motion data. */
-		param.cmd = MOTIONSENSE_CMD_DUMP;
-		rv = ec_command(EC_CMD_MOTION_SENSE_CMD, 0,
-				&param, ms_command_sizes[param.cmd].insize,
-				&resp, ms_command_sizes[param.cmd].outsize);
+	if ((argc == 1) ||
+	    (argc == 2 && !strcasecmp(argv[1], "active"))) {
+		/* Let's try the new version first */
+		param.cmd = MOTIONSENSE_CMD_GET_DATA;
+		param.data.max_sensor_count = ECTOOL_MAX_SENSOR;
+		rv = ec_command(
+			EC_CMD_MOTION_SENSE_CMD, 0,
+			&param, ms_command_sizes[param.cmd].insize,
+			&resp, ms_command_sizes[param.cmd].outsize);
+		if (rv > 0) {
+			printf("%s\n", motion_status_string[
+					!!(resp.data.module_flags &
+					   MOTIONSENSE_MODULE_FLAG_ACTIVE)][
+					status_only]);
+			if (status_only)
+				return 0;
 
-		if (rv < 0)
-			return rv;
-
-		if (resp.dump.module_flags & MOTIONSENSE_MODULE_FLAG_ACTIVE)
-			printf("Motion sensing active\n");
-		else
-			printf("Motion sensing inactive\n");
-
-		for (i = 0; i < EC_MOTION_SENSOR_COUNT; i++) {
-			printf("Sensor %d: ", i);
-			if (resp.dump.sensor_flags[i] &
-					MOTIONSENSE_SENSOR_FLAG_PRESENT)
-				printf("%d\t%d\t%d\n", resp.dump.data[3*i],
-							resp.dump.data[3*i+1],
-							resp.dump.data[3*i+2]);
-			else
+			if (resp.data.sensor_count > ECTOOL_MAX_SENSOR) {
+				printf("Too many sensors to handle: %d",
+						resp.data.sensor_count);
+				return -1;
+			}
+			for (i = 0; i < resp.data.sensor_count; i++) {
 				/*
 				 * Warning: the following string printed out
 				 * is read by an autotest. Do not change string
 				 * without consulting autotest for
 				 * kernel_CrosECSysfsAccel.
 				 */
-				printf("None\n");
+				printf("Sensor %d: ", i);
+				if (resp.data.sensor[i].flags &
+						MOTIONSENSE_SENSOR_FLAG_PRESENT)
+					printf("%d\t%d\t%d\n",
+						resp.data.sensor[i].data[0],
+						resp.data.sensor[i].data[1],
+						resp.data.sensor[i].data[2]);
+				else
+					printf("None\n");
+			}
+			return 0;
 		}
-
-		return 0;
-	}
-
-	if (argc == 2 && !strcasecmp(argv[1], "active")) {
-		param.cmd = MOTIONSENSE_CMD_DUMP;
-		rv = ec_command(EC_CMD_MOTION_SENSE_CMD, 0,
+		if (rv == -EECRESULT - EC_RES_INVALID_PARAM) {
+			/* dump motion data. */
+			param.cmd = MOTIONSENSE_CMD_DUMP;
+			rv = ec_command(
+				EC_CMD_MOTION_SENSE_CMD, 0,
 				&param, ms_command_sizes[param.cmd].insize,
 				&resp, ms_command_sizes[param.cmd].outsize);
+			if (rv < 0)
+				return rv;
 
-		/*
-		 * Warning: the following strings printed out are read in an
-		 * autotest. Do not change string without consulting autotest
-		 * for kernel_CrosECSysfsAccel.
-		 */
-		if (resp.dump.module_flags & MOTIONSENSE_MODULE_FLAG_ACTIVE)
-			printf("1\n");
-		else
-			printf("0\n");
+			printf("%s\n", motion_status_string[
+					!!(resp.dump.module_flags &
+					   MOTIONSENSE_MODULE_FLAG_ACTIVE)][
+					status_only]);
+			if (status_only)
+				return 0;
 
-		return 0;
+			for (i = 0; i < EC_MOTION_SENSOR_COUNT; i++) {
+				/*
+				 * Warning: the following string printed out
+				 * is read by an autotest. Do not change string
+				 * without consulting autotest for
+				 * kernel_CrosECSysfsAccel.
+				 */
+				printf("Sensor %d: ", i);
+				if (resp.dump.sensor_flags[i] &
+						MOTIONSENSE_SENSOR_FLAG_PRESENT)
+					printf("%d\t%d\t%d\n",
+							resp.dump.data[3*i],
+							resp.dump.data[3*i+1],
+							resp.dump.data[3*i+2]);
+				else
+					printf("None\n");
+			}
+			return 0;
+		} else {
+			return rv;
+		}
 	}
 
 	if (argc == 3 && !strcasecmp(argv[1], "info")) {
