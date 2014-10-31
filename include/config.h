6d334d47f6a726eacf39731dc0493eb394de6843
Gwendal Grignou <gwendal@chromium.org>
2014-10-30 18:00:53 -0700

    ryu: add a dummy power module for controlling sensors.
    With this change we can use power event to configure sensors
    and trigger motion detection in suspend.
    
    BUG=chrome-os-partner:31071
    BRANCH=ToT
    TEST=Check power states. Check power up messages and commands are
    present at the console.
    
    Message at boot:
    [0.007142 hash start 0x00010000 0x000096dd]
    [0.007293 Inits done]
    [0.007506 power state 2 = S3, in 0x0000]
    [0.007765 power state 3 = S0, in 0x0000]
    [0.007908 event set 0x00002000]
    [0.008021 hostcmd init 0x2000]
    [0.146870 hash done
    f87d7824b439db923d270df016af5aabec51b73505b7c4faa6e40c16b12dd392]
    
    Change-Id: I9c56fe5203506462f0820bbc8a5fe4528f6805ac
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- include/config.h
+++ include/config.h
@@ -321,6 +321,7 @@
 #undef CONFIG_CMD_JUMPTAGS
 #undef CONFIG_CMD_PLL
 #undef CONFIG_CMD_PMU
+#define CONFIG_CMD_POWER_AP
 #define CONFIG_CMD_POWERINDEBUG
 #undef CONFIG_CMD_POWERLED
 #undef CONFIG_CMD_RTC_ALARM
@@ -473,6 +474,9 @@
  * options stored in internal data EEPROM.
  */
 #undef CONFIG_EOPTION
+
+/* Include code for handling external power */
+#define CONFIG_EXTPOWER
 
 /* Support turbo-mode chargers */
 #undef CONFIG_EXTPOWER_FALCO
