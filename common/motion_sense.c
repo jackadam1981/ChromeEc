924879e9dfc3937848803af036ac4b7d034beb6e
Gwendal Grignou <gwendal@chromium.org>
2014-10-30 18:00:53 -0700

    ryu: add a dummy power module for controlling sensors.
    With this change we can use power event to configure sensors
    and trigger motion detection in suspend.
    
    BUG=chrome-os-partner:31071
    BRANCH=ToT
    TEST=Check power states. Check power up messages and commands are
    present at the console.
    
    Message at boot:
    [0.007142 hash start 0x00010000 0x000096dd]
    [0.007293 Inits done]
    [0.007506 power state 2 = S3, in 0x0000]
    [0.007765 power state 3 = S0, in 0x0000]
    [0.007908 event set 0x00002000]
    [0.008021 hostcmd init 0x2000]
    [0.146870 hash done
    f87d7824b439db923d270df016af5aabec51b73505b7c4faa6e40c16b12dd392]
    
    Change-Id: I9c56fe5203506462f0820bbc8a5fe4528f6805ac
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- common/motion_sense.c
+++ common/motion_sense.c
@@ -60,15 +60,6 @@ static int accel_interval_ms;
 static int accel_disp;
 #endif
 
-/*
- * Angle threshold for how close the hinge aligns with gravity before
- * considering the lid angle calculation unreliable. For computational
- * efficiency, value is given unit-less, so if you want the threshold to be
- * at 15 degrees, the value would be cos(15 deg) = 0.96593.
- */
-#define HINGE_ALIGNED_WITH_GRAVITY_THRESHOLD 0.96593F
-
-
 static void motion_sense_shutdown(void)
 {
 	int i;
@@ -197,21 +188,14 @@ static inline void motion_sense_init(struct motion_sensor_t *sensor)
 
 static int motion_sense_read(struct motion_sensor_t *sensor)
 {
-	int ret;
-
 	if (sensor->state != SENSOR_INITIALIZED)
 		return EC_ERROR_UNKNOWN;
 
 	/* Read all raw X,Y,Z accelerations. */
-	ret = sensor->drv->read(sensor,
+	return sensor->drv->read(sensor,
 		&sensor->xyz[X],
 		&sensor->xyz[Y],
 		&sensor->xyz[Z]);
-
-	if (ret != EC_SUCCESS)
-		return EC_ERROR_UNKNOWN;
-
-	return EC_SUCCESS;
 }
 
 /*
@@ -285,7 +269,6 @@ void motion_sense_task(void)
 #ifdef CONFIG_LID_ANGLE
 		if (rd_cnt == motion_sensor_count)
 			motion_lid_calc();
-
 #endif
 #ifdef CONFIG_CMD_ACCEL_INFO
 		if (accel_disp) {
