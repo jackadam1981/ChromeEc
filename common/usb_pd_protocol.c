2bc2288588bc017576223dfb0e978def4af46e92
Alec Berg <alecaberg@chromium.org>
2014-11-04 08:46:26 -0800

    pd: for non dual-role device, always send reject to PR_SWAP
    For non dual-role devices, always send reject to power swap request,
    PR_SWAP.
    
    BUG=none
    BRANCH=samus
    TEST=make buildall
    
    Change-Id: I54ce4810cf08a02a1d841308507a0f8de284d987
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -912,8 +912,10 @@ static void handle_ctrl_request(int port, uint16_t head,
 		} else {
 			send_control(port, PD_CTRL_REJECT);
 		}
-		break;
+#else
+		send_control(port, PD_CTRL_REJECT);
 #endif
+		break;
 	case PD_CTRL_PROTOCOL_ERR:
 	case PD_CTRL_WAIT:
 	default:
