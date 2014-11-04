8de0fffb5253bad423b3ad6c6376641db7cdb3dc
Alec Berg <alecaberg@chromium.org>
2014-11-04 14:34:00 -0800

    pd: add delay for power supply startup before sending src cap
    Add delay for power supply startup time before sending source
    capabilities packets.
    
    BUG=none
    BRANCH=samus
    TEST=load on samus, plug in C to A receptacle adapter, see that
    VBUS enabled before source cap packet sent.
    
    Change-Id: If3b595e1671d089e859693b420841a639fdb146b
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- board/hoho/usb_pd_config.h
+++ board/hoho/usb_pd_config.h
@@ -138,5 +138,5 @@ static inline int pd_snk_is_vbus_provided(int port)
 #define PD_DEFAULT_STATE PD_STATE_SNK_DISCONNECTED
 
 /* delay necessary for the voltage transition on the power supply */
-#define PD_POWER_SUPPLY_TRANSITION_DELAY 50000 /* us */
+#define PD_POWER_SUPPLY_TRANSITION_DELAY 0 /* us */
 #endif /* __USB_PD_CONFIG_H */
