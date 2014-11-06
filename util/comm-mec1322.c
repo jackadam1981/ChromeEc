e9c15430ee5840abf2ac8e9271def6f73b511f99
Gwendal Grignou <gwendal@chromium.org>
2014-11-06 14:17:43 -0800

    util: Fix return value of ec_command
    When ec_command fails because of transport issue,
    it should returns an error between -1 and -EECRESULT.
    If the command fails because of the EC, the error should be less
    than -EECRESULT.
    Unify all transports to return the error in the same manner.
    
    BRANCH=ToT
    BUG=None
    TEST=Check that unsupported command fails with the correct error
    number over lpc (samus).
    
    Change-Id: I2e43d0cb003d75318b0edd3745e534c700d7d7d8
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- util/comm-mec1322.c
+++ util/comm-mec1322.c
@@ -148,7 +148,7 @@ static int ec_command_lpc(int command, int version,
 	i = read_byte(EC_LPC_ADDR_HOST_DATA);
 	if (i) {
 		fprintf(stderr, "EC returned error result code %d\n", i);
-		return -i;
+		return -EECRESULT - i;
 	}
 
 	/* Read back args */
