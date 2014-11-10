88ae77b80686f2471edff6fdc6b6409d966dbdaa
Vincent Palatin <vpalatin@chromium.org>
2014-11-10 14:05:53 -0800

    twinkie: add Python framework
    Framework for testing USB Power Delivery using the Twinkie dongle.
    
    XxX TODO XxX
    
    /!\  Work In Progress /!\
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=chrome-os-partner:28338
    TEST=cros lint util/twinkie/*.py
    sudo python util/twinkie/test.py
    and see tests passing
    
    Change-Id: Ie8df3fc2143fafe7fde2306cd51b6b976e57d6ba

---
+++ util/twinkie/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from __future__ import print_function
+
+from twinkie import Twinkie
+from injector import CmdFsm
+from test import TestSequence
+from utils import Log
+
