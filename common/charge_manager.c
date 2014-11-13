c36f3b54d471e62a449097754272276785019387
Shawn Nematbakhsh <shawnn@chromium.org>
2014-11-13 10:50:49 -0800

    charge_manager: Don't charge from dual-role capable ports
    Don't charge from dual-role capable ports, unless an override is set to
    force charge. Also, if an override is set to charge from a dual-role
    capable port and a dedicated charger is attached, remove the override.
    
    BUG=chrome-os-partner:32003
    TEST=Pass unit tests
    BRANCH=Samus
    
    Change-Id: I4e81e39a9f6ba325fa8a5e8f0d77fd5bfc7a672c
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -95,8 +95,20 @@ static void charge_manager_refresh(void)
 		 */
 		for (i = 0; i < CHARGE_SUPPLIER_COUNT; ++i)
 			for (j = 0; j < PD_PORT_COUNT; ++j) {
+				/*
+				 * Don't select this port if we have a
+				 * charge on another override port.
+				 */
 				if (override_port != OVERRIDE_OFF &&
 				    override_port == new_port &&
+				    override_port != j)
+					continue;
+
+				/*
+				 * Don't charge from a dual-role port unless
+				 * it is our override port.
+				 */
+				if (pd_get_partner_dualrole_capable(j) &&
 				    override_port != j)
 					continue;
 
@@ -175,6 +187,18 @@ void charge_manager_update(int supplier,
 	/* Update charge table if needed. */
 	if (available_charge[supplier][port].current != charge->current ||
 		available_charge[supplier][port].voltage != charge->voltage) {
+		/*
+		 * If override is set to a dualrole port and a dedicated
+		 * charger becomes available, remove the override.
+		 */
+		if (available_charge[supplier][port].current == 0 &&
+		    charge->current > 0 &&
+		    override_port >= 0 &&
+		    pd_get_partner_dualrole_capable(override_port) &&
+		    !pd_get_partner_dualrole_capable(port))
+			override_port = OVERRIDE_OFF;
+
+
 		available_charge[supplier][port].current = charge->current;
 		available_charge[supplier][port].voltage = charge->voltage;
 
