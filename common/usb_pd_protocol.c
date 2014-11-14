637265f16f6cdc6c51f82b20541336ecd073db49
Bernard Shyu <bernard_shyu@bizlinktech.com>
2014-11-14 15:14:16 +0800

    pd: fix negative timeout for waiting task
    pd[port].timeout will be 0 immediate after set_state(), thus will cause
    a negative value result for timeout calculation in the pd_task's ending
    loop. This will cause task_wait_event to receive a negative timeout value.
    Obviously it causes no harm to the current implementation, but should be
    fixed by being paranoid.
    
    Change-Id: I66bc202e2d2e0b6fb74495686d4ecd74ae55ce63

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -1886,12 +1886,16 @@ void pd_task(void)
 		 * timeout value to wake up on the next state timeout.
 		 */
 		now = get_time();
-		if (pd[port].timeout && now.val >= pd[port].timeout) {
-			set_state(port, pd[port].timeout_state);
-			/* On a state timeout, run next state soon */
-			timeout = timeout < 10*MSEC ? timeout : 10*MSEC;
-		} else if (pd[port].timeout - now.val < timeout) {
-			timeout = pd[port].timeout - now.val;
+		if (pd[port].timeout) {
+			if (now.val >= pd[port].timeout) {
+				set_state(port, pd[port].timeout_state);
+				/* On a state timeout, run next state soon */
+				timeout = timeout < 10*MSEC ? timeout : 10*MSEC;
+			} else if (pd[port].timeout - now.val < timeout) {
+				timeout = pd[port].timeout - now.val;
+			}
+		} else {
+			timeout = 0;
 		}
 
 		/* Check for disconnection */
