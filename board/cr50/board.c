f73e624c8eb46b3e8b74faf722517628149051e9
Vincent Palatin <vpalatin@chromium.org>
2014-11-19 14:37:10 -0800

    cr50: add USB support
    Add a USB device driver for the Synopsys DWC USB device controller.
    
    /!\ W.I.P. /!\
    
    TODO: de-duplicate common USB code with STM32
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=chrome-os-partner:33919
    TEST=plug Cr50 to a Linux workstation and see USB descriptors using
    "lsusb -v -d 18d1:5014"
    
    Change-Id: I4a367241053de2c2d94aa06f82ea4bee51f9f89a

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -4,9 +4,11 @@
  */
 
 #include "common.h"
+#include "ec_version.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "task.h"
+#include "usb.h"
 #include "util.h"
 
 #include "gpio_list.h"
@@ -18,3 +20,12 @@ static void board_init(void)
 	/* gpio_enable_interrupt(GPIO_CAMO0_BREACH_INT); */
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+const void * const usb_strings[] = {
+	[USB_STR_DESC] = usb_string_desc,
+	[USB_STR_VENDOR] = USB_STRING_DESC("Google Inc."),
+	[USB_STR_PRODUCT] = USB_STRING_DESC("Cr50"),
+	[USB_STR_VERSION] = USB_STRING_DESC(CROS_EC_VERSION32),
+	[USB_STR_CONSOLE_NAME] = USB_STRING_DESC("Shell"),
+};
+BUILD_ASSERT(ARRAY_SIZE(usb_strings) == USB_STR_COUNT);
