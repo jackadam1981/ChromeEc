4e405113a85460239d7df6a5f855d077cb38f9be
Gwendal Grignou <gwendal@chromium.org>
2014-11-19 16:15:57 -0800

    ryu_sh_load: Add board directory for load image
    Ryu sensor hub has asymectric RO/RW images. The first one is very limited
    (not i2c master, no sensor drivers, gesture recognition).
    
    Image size is alter to offer more space for the RW firmware image,
    compiled with ryu_sh board.
    
    To write RO image and basic RW image:
    flashrom -V -p ec:type=sh,block=0x800 --fast-verify -w /tmp/ryu_sh_load/ec.bin
    To write the expected RW image:
    flashrom -V -p ec:block=0x800 --fast-verify -w -i EC_RW:/tmp/ryu_sh/ec.bin
    
    WIP: had to disable flash verification in RW mode.
    
    BRANCH=ToT
    BUG=chrome-os-partner:33908
    CQ-DEPEND=CL:231970
    TEST=load on Ryu, confirmed limited operation.
    
    Change-Id: Ib976e2b048935adfb9b2b072c071db5be2bc1c09
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

---
+++ chip/stm32/config-stm32f07x-asymetric.h
@@ -0,0 +1,12 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/*
+ * Asymetric RO/RW region.
+ * Different images, coming from different board.
+ * Size of the loader.
+ */
+#define CONFIG_FW_IMAGE_SIZE (40*1024)
+#include "config-stm32f07x.h"
