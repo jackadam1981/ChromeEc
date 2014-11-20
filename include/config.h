36fb634cc0dac406611ed0dd9da2a3dd2146b8d8
Gwendal Grignou <gwendal@chromium.org>
2014-11-19 16:15:57 -0800

    ryu_sh_loader: Add board directory for load image
    Ryu sensor hub has asymectric RO/RW images. The first one is very limited
    (not i2c master, no sensor drivers, gesture recognition).
    
    Image size is alter to offer more space for the RW firmware image,
    compiled with ryu_sh board.
    
    To write RO image and basic RW image:
    flashrom -V -p ec:type=sh,block=0x800 --fast-verify -w /tmp/ryu_sh_loader/ec.bin
    To write the expected RW image:
    flashrom -V -p ec:type=sh,block=0x800 --fast-verify -w -i EC_RW:/tmp/ryu_sh/ec.bin
    
    BRANCH=ToT
    BUG=chrome-os-partner:33908
    CQ-DEPEND=CL:231970
    TEST=load on Ryu, confirmed limited operation.
    
    Change-Id: Ib976e2b048935adfb9b2b072c071db5be2bc1c09
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- include/config.h
+++ include/config.h
@@ -548,6 +548,7 @@
 #undef CONFIG_FW_RW_SIZE
 #undef CONFIG_FW_WP_RO_OFF
 #undef CONFIG_FW_WP_RO_SIZE
+#define CONFIG_INCLUDE_RO
 
 /*****************************************************************************/
 /* Motion sensor based gesture recognition information */
