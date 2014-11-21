a1bcca30b02d199cf2abf9fc47ee1a508f3d0f08
Sheng-Liang Song <ssl@chromium.org>
2014-11-21 15:00:28 -0800

    cr50: Added memory dump CLI for bringup
    Usage: md [addr] [num of words (4B)]
    
    BRANCH=none
    BUG=none
    TEST="make BOARD=cr50" and "md 0 100"
    
    > md 0 100
    [00000000] : 00018000 0000011d 00000101 00000101
    [00000010] : 00000101 00000101 00000101 00000101
    ...
    
    Signed-off-by: Sheng-Liang Song <ssl@chromium.org>
    Change-Id: I1de8c690cca006ec3aae42d4e6bd0ba30f7c3238

--- common/memory_commands.c
+++ common/memory_commands.c
@@ -74,3 +74,37 @@ DECLARE_CONSOLE_COMMAND(rw, command_read_word,
 			"addr [value]",
 			"Read or write a word in memory",
 			NULL);
+
+static int command_mem_dump(int argc, char **argv)
+{
+	volatile uint32_t *address;
+	uint32_t value, num = 1, i;
+	char *e;
+
+	if (argc < 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	address = (uint32_t *)(uintptr_t)strtoi(argv[1], &e, 0);
+	if (*e)
+		return EC_ERROR_PARAM1;
+
+	if (argc >= 3)
+		num = strtoi(argv[2], &e, 0);
+
+	for (i = 0; i < num; i++) {
+		value = address[i];
+		if (0 == (i%4))
+			ccprintf("\n[%08X] : %08x", address+i, value);
+		else
+			ccprintf(" %08x", value);
+		cflush();
+	}
+	ccprintf("\n");
+	cflush();
+	return EC_SUCCESS;
+}
+
+DECLARE_CONSOLE_COMMAND(md, command_mem_dump,
+			"addr [num]",
+			"dump num of words (4B) in memory",
+			NULL);
