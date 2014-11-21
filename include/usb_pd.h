d43ff4c3b591e0ae139275e698a7a318e2777237
Todd Broch <tbroch@chromium.org>
2014-11-20 17:24:27 -0800

    ectool: Add USB-PD mode commands.
    These commands (pdgetmode & pdsetmode) will provide host with ability
    to identify USB-PD alternate mode devices SVIDs and supported modes.
    It will also allow host to set mode on devices which support multiple
    SVIDs.
    
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=samus
    BUG=chrome-os-partner:33946
    TEST=manual
    
    1. Plug hoho/dingdong into samus at port <port>
    
    ectool --name cros_pd pdgetmode <port>
    *SVID:0xff01 *0x00001085  0x00000000  0x00000000  0x00000000  0x00000000  0x00000000
     SVID:0x18d1  0x00000001  0x00000000  0x00000000  0x00000000  0x00000000  0x00000000
    
    2. Unplug hoho/dingdong into samus at port <port>
    
    ectool --name cros_pd pdgetmode <port>
    
    3. Plug hoho/dingdong into samus at port <port>
    ectool --name cros_pd pdsetmode <port> 0x18d1 1
    
    ectool --name cros_pd pdgetmode <port>
     SVID:0xff01  0x00001085  0x00000000  0x00000000  0x00000000  0x00000000  0x00000000
    *SVID:0x18d1 *0x00000001  0x00000000  0x00000000  0x00000000  0x00000000  0x00000000
    
    ectool --name cros_pd infopddev <port>
    <output here>
    
    Change-Id: I4e442bcb39ec1ff3cb6efff196a660819077ad76

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -862,13 +862,12 @@ int pd_svdm(int port, int cnt, uint32_t *payload, uint32_t **rpayload);
 int pd_custom_flash_vdm(int port, int cnt, uint32_t *payload);
 
 /**
- * Exit alternate mode
+ * Exit alternate mode.
  *
  * @param port     USB-C port number
- * @param payload  payload data.
- * @return if >0, number of VDOs to send back.
+ * @param payload  payload data.  If NULL assume disconnect.
  */
-int pd_exit_mode(int port, uint32_t *payload);
+void pd_exit_mode(int port, uint32_t *payload);
 
 /**
  * Store Device ID & RW hash of device
