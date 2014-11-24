3746c5c523a799435cd6d3afbdd730945c5466c7
Todd Broch <tbroch@chromium.org>
2014-11-24 10:20:41 -0800

    pd: Re-factor common flash vdms.
    CL to migrate the flashing VDMs from zinger's custom vdm to
    common/usb_pd_flash.c such that other updateable type-C devices can
    share.
    
    Additionally adds gaskets to call standard runtime flashing facilities
    for USB-PD devices using it.
    
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=samus
    BUG=chrome-os-partner:31192,chrome-os-partner:31193
    TEST=manual,
    
    Try following:
        1. From samus_pd console w/ zinger in port 1
    
        pd 1 flash version
        pd 1 flash reboot
        pd 1 flash info
    
        2. From samus linux prompt  w/ zinger in port 1
    
        ectool --name cros_pd flashpd 1 1 <zinger RW payload>
    
        Reading 16384 bytes from
        /usr/local/zinger_v1.1.2528-d809e42.ec.RW.bin...
        Erasing expected RW hash
        Rebooting
        Erasing RW flash
        Writing RW flash
        Rebooting PD into new RW
        Complete
    
        3. Repeat 1&2 above on hoho & dingdong.
    
    Change-Id: I018055fa9de128f937c57debdc21dea026137bcf

--- common/usb_pd_flash.c
+++ common/usb_pd_flash.c
@@ -2,8 +2,12 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+#include "flash.h"
 #include "registers.h"
 #include "system.h"
+#include "usb_pd.h"
+#include "util.h"
+#include "version.h"
 
 #ifndef CONFIG_COMMON_RUNTIME
 #include "debug.h"
@@ -34,4 +38,73 @@ int pd_is_ro_mode(void)
 #else
 	return (uint32_t)&pd_jump_to_rw < (uint32_t)rw_rst;
 #endif
+}
+
+#ifdef CONFIG_COMMON_RUNTIME
+static int flash_erase_rw(void)
+{
+	return flash_physical_erase(CONFIG_FW_RW_OFF, CONFIG_FW_RW_SIZE);
+}
+
+static int flash_write_rw(int offset, int size, const char *data)
+{
+	return flash_physical_write(offset, size, data);
+}
+#endif
+
+int pd_custom_flash_vdm(int port, int cnt, uint32_t *payload)
+{
+	static int flash_offset;
+	int rsize = 1; /* default is just VDM header returned */
+
+	switch (PD_VDO_CMD(payload[0])) {
+	case VDO_CMD_VERSION:
+		memcpy(payload + 1, &version_data.version, 24);
+		rsize = 7;
+		break;
+	case VDO_CMD_REBOOT:
+		/* ensure the power supply is in a safe state */
+		pd_power_supply_reset(0);
+#ifndef CONFIG_COMMON_RUNTIME
+		cpu_reset();
+#else
+		system_reset(0);
+#endif
+		break;
+	case VDO_CMD_READ_INFO:
+		/* copy info into response */
+		pd_get_info(payload + 1);
+		rsize = 7;
+		break;
+	case VDO_CMD_FLASH_ERASE:
+		/* do not kill the code under our feet */
+		if (!pd_is_ro_mode())
+			break;
+		flash_offset = 0;
+		flash_erase_rw();
+		break;
+	case VDO_CMD_FLASH_WRITE:
+		/* do not kill the code under our feet */
+		if (!pd_is_ro_mode())
+			break;
+		flash_write_rw(flash_offset, 4*(cnt - 1),
+			       (const char *)(payload+1));
+		flash_offset += 4*(cnt - 1);
+		break;
+	case VDO_CMD_ERASE_SIG:
+		/* this is not touching the code area */
+		{
+			uint32_t zero = 0;
+			int offset;
+			/* zeroes the area containing the RSA signature */
+			for (offset = CONFIG_FW_RW_SIZE - 256;
+			     offset < CONFIG_FW_RW_SIZE; offset += 4)
+				flash_write_rw(offset, 4, (const char *)&zero);
+		}
+		break;
+	default:
+		/* Unknown : do not answer */
+		return 0;
+	}
+	return rsize;
 }
