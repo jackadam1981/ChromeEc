749efe5ee7f8b5808effa11dc07530a7dd143b8e
Todd Broch <tbroch@chromium.org>
2014-11-24 10:20:41 -0800

    pd: Re-factor common flash vdms.
    CL to migrate the flashing VDMs from zinger's custom vdm to
    common/usb_pd_flash.c such that other updateable type-C devices can
    share.
    
    Additionally adds gaskets to call standard runtime flashing facilities
    for USB-PD devices using it.
    
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=samus
    BUG=chrome-os-partner:31192,chrome-os-partner:31193
    TEST=manual,
    
    Try following:
        1. From samus_pd console w/ zinger in port 1
    
        pd 1 flash version
        pd 1 flash reboot
        pd 1 flash info
    
        2. From samus linux prompt  w/ zinger in port 1
    
        ectool --name cros_pd flashpd 1 1 <zinger RW payload>
    
        Reading 16384 bytes from
        /usr/local/zinger_v1.1.2528-d809e42.ec.RW.bin...
        Erasing expected RW hash
        Rebooting
        Erasing RW flash
        Writing RW flash
        Rebooting PD into new RW
        Complete
    
        3. Repeat 1&2 above on hoho & dingdong.
    
    Change-Id: I018055fa9de128f937c57debdc21dea026137bcf

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -821,6 +821,16 @@ int pd_vdm(int port, int cnt, uint32_t *payload, uint32_t **rpayload);
 int pd_svdm(int port, int cnt, uint32_t *payload, uint32_t **rpayload);
 
 /**
+ * Handle Custom VDMs for flashing.
+ *
+ * @param port     USB-C port number
+ * @param cnt      number of data objects in the payload.
+ * @param payload  payload data.
+ * @return if >0, number of VDOs to send back.
+ */
+int pd_custom_flash_vdm(int port, int cnt, uint32_t *payload);
+
+/**
  * Exit alternate mode.
  *
  * @param port     USB-C port number
