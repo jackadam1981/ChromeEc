0c550f51e42158695e9c8ef8e405a2888f80885d
Todd Broch <tbroch@chromium.org>
2014-11-24 10:20:41 -0800

    pd: Re-factor common flash vdms.
    CL to migrate the flashing VDMs from zinger's custom vdm to
    common/usb_pd_flash.c such that other updateable type-C devices can
    share.
    
    Additionally adds gaskets to call standard runtime flashing facilities
    for USB-PD devices using it.
    
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=samus
    BUG=chrome-os-partner:31192,chrome-os-partner:31193
    TEST=manual,
    
    Try following:
        1. From samus_pd console w/ zinger in port 1
    
        pd 1 flash version
        pd 1 flash reboot
        pd 1 flash info
    
        2. From samus linux prompt  w/ zinger in port 1
    
        ectool --name cros_pd flashpd 1 1 <zinger RW payload>
    
        Reading 16384 bytes from
        /usr/local/zinger_v1.1.2528-d809e42.ec.RW.bin...
        Erasing expected RW hash
        Rebooting
        Erasing RW flash
        Writing RW flash
        Rebooting PD into new RW
        Complete
    
        3. Repeat 1&2 above on hoho & dingdong.
    
    Change-Id: I018055fa9de128f937c57debdc21dea026137bcf

--- board/zinger/usb_pd_policy.c
+++ board/zinger/usb_pd_policy.c
@@ -14,7 +14,6 @@
 #include "timer.h"
 #include "util.h"
 #include "usb_pd.h"
-#include "version.h"
 
 /* ------------------------- Power supply control ------------------------ */
 
@@ -491,59 +490,23 @@ const struct svdm_response svdm_rsp = {
 static int pd_custom_vdm(int port, int cnt, uint32_t *payload,
 			 uint32_t **rpayload)
 {
-	static int flash_offset;
 	int cmd = PD_VDO_CMD(payload[0]);
-	int rsize = 1;
+	int rsize;
 
 	if (PD_VDO_VID(payload[0]) != USB_VID_GOOGLE || !gfu_mode)
 		return 0;
 
 	debug_printf("%T] VDM/%d [%d] %08x\n", cnt, cmd, payload[0]);
-
 	*rpayload = payload;
+
+	rsize = pd_custom_flash_vdm(port, cnt, payload);
+	if (rsize)
+		goto VDM_DONE;
+
 	switch (cmd) {
-	case VDO_CMD_VERSION:
-		memcpy(payload + 1, &version_data.version, 24);
-		rsize = 7;
-		break;
-	case VDO_CMD_REBOOT:
-		/* ensure the power supply is in a safe state */
-		pd_power_supply_reset(0);
-		cpu_reset();
-		break;
-	case VDO_CMD_READ_INFO:
-		/* copy info into response */
-		memcpy(payload + 1, board_get_info(), 24);
-		rsize = 7;
-		break;
-	case VDO_CMD_FLASH_ERASE:
-		/* do not kill the code under our feet */
-		if (!is_ro_mode())
-			break;
-		flash_offset = 0;
-		flash_erase_rw();
-		break;
-	case VDO_CMD_FLASH_WRITE:
-		/* do not kill the code under our feet */
-		if (!is_ro_mode())
-			break;
-		flash_write_rw(flash_offset, 4*(cnt - 1),
-			       (const char *)(payload+1));
-		flash_offset += 4*(cnt - 1);
-		break;
-	case VDO_CMD_ERASE_SIG:
-		/* this is not touching the code area */
-		{
-			uint32_t zero = 0;
-			int offset;
-			/* zeroes the area containing the RSA signature */
-			for (offset = CONFIG_FW_RW_SIZE - 256;
-			     offset < CONFIG_FW_RW_SIZE; offset += 4)
-				flash_write_rw(offset, 4, (const char *)&zero);
-		}
-		break;
 	case VDO_CMD_PING_ENABLE:
 		pd_ping_enable(0, payload[1]);
+		rsize = 1;
 		break;
 	case VDO_CMD_CURRENT:
 		/* return last measured current */
@@ -554,6 +517,8 @@ static int pd_custom_vdm(int port, int cnt, uint32_t *payload,
 		/* Unknown : do not answer */
 		return 0;
 	}
+
+VDM_DONE:
 	debug_printf("%T] DONE\n");
 	/* respond (positively) to the request */
 	payload[0] |= VDO_SRC_RESPONDER;
