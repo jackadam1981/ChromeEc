656e906eb4abb21d1a7c4fdf7ace65d355af30d7
Hsu Henry <Henry.Hsu@quantatw.com>
2014-12-09 06:33:06 +0800

    Paine, Yuna: Fan table implement.
    Yuna, Paine define the thermal table for fan. The table is below:
    	on	off	RPM
    step1	43	37	3200
    step2	48	43	3700
    step3	52	47	4000
    step4	56	51	4400
    step5	60	55	4900
    step6	70	65	5500
    step7	95	90	6500
    
    Where the temperature scale is celsius degree.
    BUG=chrome-os-partner:34418
    BRANCH=paine, yuna
    TEST=Use hot gun, check the step 1, 2, 3, 4 with yuna. The fan behavior works properly.
    
    Change-Id: Id4f5957d63a81046539bc96a95e30b6b022803fe
    Signed-off-by: Henry Hsu <Henry.Hsu@quantatw.com>

--- board/paine/board.c
+++ board/paine/board.c
@@ -9,6 +9,7 @@
 #include "backlight.h"
 #include "chipset.h"
 #include "common.h"
+#include "console.h"
 #include "driver/temp_sensor/g781.h"
 #include "extpower.h"
 #include "fan.h"
@@ -31,6 +32,77 @@
 #include "util.h"
 
 #include "gpio_list.h"
+
+#ifdef CONFIG_FAN_RPM_CUSTOM
+struct fan_step {
+	int on;
+	int off;
+	int rpm;
+};
+
+const struct fan_step fan_table[9] = {
+	{0, 0, 0},
+	{8, 2, 3200},
+	{13, 8, 3700},
+	{17, 12, 4000},
+	{21, 16, 4400},
+	{25, 20, 4900},
+	{35, 30, 5500},
+	{60, 55, 6500},
+	{100, 100, 6500},
+};
+
+int fan_percent_to_rpm(int fan, int pct)
+{
+	static int index;
+	static int pervious_pct;
+	int i;
+	int is_down;
+	int temp_index;
+
+	temp_index = 0;
+	is_down = (pct < pervious_pct) ? 1 : 0;
+
+	/* Keep the current RPM if pct no change */
+	if (pct == pervious_pct)
+		return fan_get_rpm_target(fans[fan].ch);
+
+	pervious_pct = pct;
+
+	/* Check it first for init temperature */
+	if (!index) {
+		for (i = 0; i < 9; i++) {
+			if (pct >= fan_table[i].on)
+				index = i;
+		}
+	/*
+	 * Compare the pct and pervious pct, we have the two paths :
+	 * 1. down path (check the off point)
+	 * 2. up path (chek the on point)
+	 */
+	} else {
+		if (is_down) {
+			for (i = 0; i <= index; i++) {
+				if (pct > fan_table[i].off)
+					temp_index = i;
+			}
+		} else {
+			for (i = 8; i > index; i--) {
+				if (pct < fan_table[i].on)
+					temp_index = i - 1;
+			}
+		}
+		index = temp_index;
+	}
+
+	if (fan_table[index].rpm != fan_get_rpm_target(fans[fan].ch))
+		cprintf(CC_THERMAL, "[%T Setting fan RPM to %d]\n",
+			fan_table[index].rpm);
+
+	return fan_table[index].rpm;
+}
+
+#endif /* CONFIG_FAN_RPM_CUSTOM */
 
 /* power signal list.  Must match order of enum power_signal. */
 const struct power_signal_info power_signal_list[] = {
@@ -100,7 +172,7 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
  */
 struct ec_thermal_config thermal_params[] = {
 	/* Only the AP affects the thermal limits and fan speed. */
-	{{C_TO_K(95), C_TO_K(97), C_TO_K(99)}, C_TO_K(55), C_TO_K(85)},
+	{{C_TO_K(95), C_TO_K(97), C_TO_K(99)}, C_TO_K(35), C_TO_K(135)},
 	{{0, 0, 0}, 0, 0},
 	{{0, 0, 0}, 0, 0},
 	{{0, 0, 0}, 0, 0},
