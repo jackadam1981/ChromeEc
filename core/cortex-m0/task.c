58cc284e041d386a2e7eb92a2e7c7647f4cfcd1b
Alexandru M Stan <amstan@chromium.org>
2014-12-11 18:29:14 -0800

    TEST-ONLY: stm32f0: Add stack dump on watchdog reset
    BUG=None
    TEST=make the system watchdog reset, see printout
    BRANCH=veyron
    
    Change-Id: Ia768929eacda40deeadfa03cdd89e8f31ef49298
    Signed-off-by: Alexandru M Stan <amstan@chromium.org>

--- core/cortex-m0/task.c
+++ core/cortex-m0/task.c
@@ -14,6 +14,7 @@
 #include "timer.h"
 #include "uart.h"
 #include "util.h"
+#include "watchdog.h"
 
 typedef union {
 	struct {
@@ -518,6 +519,48 @@ void task_print_list(void)
 			 task_names[i], tasks[i].events, tasks[i].runtime,
 			 stackused, tasks_init[i].stack_size);
 		cflush();
+	}
+}
+
+void dump_stack_trace(uint32_t *current_location) {
+	uint32_t *stack_entry;
+	int i;
+
+	ccprintf("Task stack contents:\n");
+	for (stack_entry = (uint32_t *)task_stacks; stack_entry < (uint32_t *)task_stacks + sizeof(task_stacks); stack_entry++) {
+		ccprintf("[%08x] = %08x",stack_entry,*stack_entry);
+
+		for (i = 0; i < TASK_ID_COUNT; i++) {
+			if (stack_entry == tasks[i].stack)
+				ccprintf(" VV Task %d VV", i);
+			if ((uint32_t)stack_entry == tasks[i].sp)
+				ccprintf(" *Task %d", i);
+		}
+
+		if (((*stack_entry) & 0xff000000) == 0x08000000)
+			ccprintf(" function? %x", (*stack_entry)&(~0x1));
+
+		if (stack_entry == current_location)
+			ccprintf(" current location");
+
+		ccprintf("\n");
+		cflush();
+		watchdog_reload();
+	}
+	ccprintf("End of task stacks");
+
+	if ((stack_entry < (uint32_t *)task_stacks) || (stack_entry >= (uint32_t *)task_stacks + sizeof(task_stacks))) {
+		stack_entry = current_location;
+		ccprintf("\nOutside tasks:\n");
+		while(*stack_entry != STACK_UNUSED_VALUE) {
+			ccprintf("[%08x] = %08x",stack_entry,*stack_entry);
+			if (((*stack_entry) & 0xff000000) == 0x08000000)
+				ccprintf(" function? %x", (*stack_entry)&(~0x1));
+			ccprintf("\n");
+			stack_entry++;
+			cflush();
+			watchdog_reload();
+		}
 	}
 }
 
