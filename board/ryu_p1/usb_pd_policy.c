939a05155a2426d19f4e093ff9b5a9d630791afe
Todd Broch <tbroch@chromium.org>
2014-12-13 15:02:39 -0800

    pd: vdm: remove replicated VDM code.
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=samus
    BUG=chrome-os-partner:30645
    TEST=manual,
    Still see alternate mode entry and can use flash VDMS
    
    Change-Id: Id7371960a20e7d26a15b3a40ca40aa03b6595956

--- board/ryu_p1/usb_pd_policy.c
+++ board/ryu_p1/usb_pd_policy.c
@@ -115,7 +115,7 @@ void pd_execute_data_swap(int port, int data_role)
 }
 
 /* ----------------- Vendor Defined Messages ------------------ */
-static int pd_custom_vdm(int port, int cnt, uint32_t *payload,
+int pd_custom_vdm(int port, int cnt, uint32_t *payload,
 			 uint32_t **rpayload)
 {
 	int cmd = PD_VDO_CMD(payload[0]);
@@ -157,12 +157,4 @@ static int pd_custom_vdm(int port, int cnt, uint32_t *payload,
 	}
 
 	return 0;
-}
-
-int pd_vdm(int port, int cnt, uint32_t *payload, uint32_t **rpayload)
-{
-	if (PD_VDO_SVDM(payload[0]))
-		return pd_svdm(port, cnt, payload, rpayload);
-	else
-		return pd_custom_vdm(port, cnt, payload, rpayload);
 }
