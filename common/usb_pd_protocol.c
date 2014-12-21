76518e5c8f31248f6ca4dde74cc41cd8ccd8399e
Vincent Palatin <vpalatin@chromium.org>
2014-12-20 18:03:47 -0800

    twinkie: add simple text tracing mechanism
    When using the Twinkie dongle without a protocol decoder on the host,
    add a simple text tracing mechanism, so the user can get the timestamped traces
    of the packets on the wire (in a best effort fashion).
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=none
    TEST=On Twinkie command-line, type "tw trace on"
    then plug a DingDong to Samus through Twinkie and
    see the PD message traces on the console.
    
    Change-Id: I4fa35d6783cc6279c95209c86f37e6d717de7301

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -432,7 +432,6 @@ int prepare_message(int port, uint16_t header, uint8_t cnt,
 	return pd_write_last_edge(port, off);
 }
 
-static int analyze_rx(int port, uint32_t *payload);
 static void analyze_rx_bist(int port);
 
 int send_hard_reset(int port)
@@ -517,7 +516,7 @@ static int send_validate_message(int port, uint16_t header,
 			pd_rx_start(port);
 		}
 		/* read the incoming packet if any */
-		head = analyze_rx(port, payload);
+		head = pd_analyze_rx(port, payload);
 		pd_rx_complete(port);
 		/* keep RX monitoring on to avoid collisions */
 		pd_rx_enable_monitoring(port);
@@ -1246,7 +1245,7 @@ static void analyze_rx_bist(int port)
 			total_invalid_bits);
 }
 
-static int analyze_rx(int port, uint32_t *payload)
+int pd_analyze_rx(int port, uint32_t *payload)
 {
 	int bit;
 	char *msg = "---";
@@ -1690,7 +1689,7 @@ void pd_task(void)
 		/* incoming packet ? */
 		if (pd_rx_started(port) && pd_comm_enabled) {
 			incoming_packet = 1;
-			head = analyze_rx(port, payload);
+			head = pd_analyze_rx(port, payload);
 			pd_rx_complete(port);
 			if (head > 0)
 				handle_request(port,  head, payload);
