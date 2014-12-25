6c8b555b84682d5bb5e2c7a52ac38179faa0ce4a
Vic Yang <victoryang@chromium.org>
2014-12-24 23:05:47 -0800

    plankton: treat USB mode as DP detached
    When a DP cable is attached, if we toggle the USB/DP mux to USB mode,
    the DP data link is broken. In this case, we should send a HPD event to
    notify the host. Similarily, when we switch back to DP mode when a cable
    is attached, we should send a HPD attachment event and then a HPD IRQ to
    notify the host that the DP link is ready.
    
    BRANCH=None
    BUG=chrome-os-partner:34296
    TEST=Connect Plankton to Samus. Perform the following tests:
          1. Attach a cable, and switch between USB and DP mode.
          2. Attach a cable in DP mode, switch to USB mode, unplug the
             cable, and then switch back to DP mode.
          3. Similar to 2 but start without the cable.
    
    Change-Id: I5db0c036ce45850b2f4ca142957f4a235673df6f
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- board/plankton/board.c
+++ board/plankton/board.c
@@ -55,15 +55,17 @@ static volatile int hpd_possible_irq;
 void hpd_lvl_deferred(void)
 {
 	int level = gpio_get_level(GPIO_DPSRC_HPD);
+	int dp_mode = !gpio_get_level(GPIO_USBC_SS_USB_MODE);
 
 	if (level != hpd_prev_level) {
 		/* Stable level changed. Send HPD event */
 		hpd_prev_level = level;
-		pd_send_hpd(0, level ? hpd_high : hpd_low);
+		if (dp_mode)
+			pd_send_hpd(0, level ? hpd_high : hpd_low);
 	}
 
 	/* Send queued IRQ if the cable is attached */
-	if (hpd_possible_irq && level)
+	if (hpd_possible_irq && level && dp_mode)
 		pd_send_hpd(0, hpd_irq);
 	hpd_possible_irq = 0;
 
@@ -118,6 +120,7 @@ enum board_src_cap src_cap_mapping[USBC_ACT_COUNT] =
 static void set_usbc_action(enum usbc_action act)
 {
 	int need_soft_reset;
+	int in_usb_mode;
 
 	switch (act) {
 	case USBC_ACT_5V_TO_DUT:
@@ -133,8 +136,20 @@ static void set_usbc_action(enum usbc_action act)
 		pd_set_dual_role(PD_DRP_FORCE_SINK);
 		break;
 	case USBC_ACT_USBDP_TOGGLE:
-		gpio_set_level(GPIO_USBC_SS_USB_MODE,
-			       !gpio_get_level(GPIO_USBC_SS_USB_MODE));
+		in_usb_mode = !gpio_get_level(GPIO_USBC_SS_USB_MODE);
+		gpio_set_level(GPIO_USBC_SS_USB_MODE, in_usb_mode);
+		if (!gpio_get_level(GPIO_DPSRC_HPD))
+			break;
+		/*
+		 * DP cable is connected. Send HPD event according to USB/DP
+		 * mux state.
+		 */
+		if (in_usb_mode) {
+			pd_send_hpd(0, hpd_low);
+		} else {
+			pd_send_hpd(0, hpd_high);
+			pd_send_hpd(0, hpd_irq);
+		}
 		break;
 	case USBC_ACT_USB_EN:
 		gpio_set_level(GPIO_USBC_SS_USB_MODE, 1);
