2ee50d3debb9e26385e5fd6a6b0b513a5c968119
Vic Yang <victoryang@chromium.org>
2014-12-24 23:10:21 -0800

    plankton: Enable debug USB path
    If the USB path is enabled, also enable the debug USB path. Otherwise,
    disable the debug USB path so that it can be used as AUX for DP.
    
    BRANCH=None
    BUG=chrome-os-partner:34296
    TEST=Connect Plankton to Ryu and put Ryu EC in DFU mode. Switch between
    USB and DP mode and check if we can see Ryu EC from the debug USB port.
    
    Change-Id: I59106b284e63c317c945eb4aab195128108a93b9
    Signed-off-by: Vic Yang <victoryang@chromium.org>

--- board/plankton/board.c
+++ board/plankton/board.c
@@ -138,6 +138,10 @@ static void set_usbc_action(enum usbc_action act)
 	case USBC_ACT_USBDP_TOGGLE:
 		in_usb_mode = !gpio_get_level(GPIO_USBC_SS_USB_MODE);
 		gpio_set_level(GPIO_USBC_SS_USB_MODE, in_usb_mode);
+
+		/* Enable debug USB path if we are in USB mode */
+		gpio_set_level(GPIO_CASE_CLOSE_EN, in_usb_mode);
+
 		if (!gpio_get_level(GPIO_DPSRC_HPD))
 			break;
 		/*
