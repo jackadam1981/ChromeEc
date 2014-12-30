a8f55986c80fb0e68e15fd017fad9357027f7905
Shawn Nematbakhsh <shawnn@chromium.org>
2014-12-30 14:28:14 -0800

    charge_manager: Log charge-related change events
    Keep a 20-entry ring buffer of charge_manager port changes that can be
    retrieved through a new EC command.
    
    BUG=chrome-os-partner:33248
    TEST=Manual on Samus. Run `ectool --name cros_pd chargelog`, verify that
    all log entries are dumped and the content matches expectation. Verify
    that `ectool --name cros_pd chargelog 0 2` dumps the oldtest two table
    entries.
    BRANCH=Samus
    
    Change-Id: I65dd5696cc0487856ab42aff24134bcdfa1a8219
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -2821,6 +2821,39 @@ enum usb_pd_override_ports {
 struct ec_params_charge_port_override {
 	int16_t override_port; /* Override port# */
 } __packed;
+
+/* Read PD log of charge state changes */
+#define EC_CMD_PD_GET_CHARGE_STATE_LOG 0x115
+
+struct ec_params_charge_state_log {
+	uint8_t starting_entry; /* Initial entry to retrieve (0 = oldest) */
+	uint8_t num_entries;    /* Maximum number of entries to reply with */
+} __packed;
+
+/* Port state flags */
+enum ec_charge_log_entry_status {
+	/* Log entry is valid (ex. is initialized) */
+	CHARGE_STATUS_VALID_ENTRY =      (1 << 0),
+	/* Port is the override port */
+	CHARGE_STATUS_OVERRIDE =         (1 << 1),
+	/* Port is the pending override port */
+	CHARGE_STATUS_DELAYED_OVERRIDE = (1 << 2),
+};
+
+struct ec_charge_state_log_entry {
+	uint8_t port;                                    /* Port number */
+	uint8_t reserved;
+	uint16_t status;                                 /* Status flags */
+	uint32_t timestamp;                              /* Entry timestamp */
+	struct ec_response_usb_pd_power_info power_info; /* Port details */
+} __packed;
+
+struct ec_response_charge_state_log {
+	uint8_t num_entries;                            /* log_entries size */
+	uint8_t reserved;
+	uint16_t reserved2;
+	struct ec_charge_state_log_entry log_entries[]; /* entries */
+} __packed;
 #endif  /* !__ACPI__ */
 
 /*****************************************************************************/
