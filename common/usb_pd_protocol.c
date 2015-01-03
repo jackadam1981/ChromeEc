24d9b20c436f0ace4481ec944f7a3facd5c2669a
Todd Broch <tbroch@chromium.org>
2015-01-02 20:30:21 -0800

    pd: Add timeout for HC flash commands.
    For flash commands add timeout so if VDM doesn't return properly host
    can receive error message.
    
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=samus
    BUG=chrome-os-partner:33947
    TEST=manual, if VDM doesn't return by timeout HC command is properly returned
    EC_RES_TIMEOUT error code.
    
    Change-Id: I33c515200c2999dd97fdd690a7e900c5548b2d47

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -2961,6 +2961,7 @@ static int hc_remote_flash(struct host_cmd_handler_args *args)
 	int port = p->port;
 	const uint32_t *data = &(p->size) + 1;
 	int i, size;
+	timestamp_t timeout;
 
 	if (p->size + sizeof(*p) > args->params_size)
 		return EC_RES_INVALID_PARAM;
@@ -2986,18 +2987,12 @@ static int hc_remote_flash(struct host_cmd_handler_args *args)
 
 	case USB_PD_FW_FLASH_ERASE:
 		pd_send_vdm(port, USB_VID_GOOGLE, VDO_CMD_FLASH_ERASE, NULL, 0);
-
-		/* Wait until VDM is done */
-		while (pd[port].vdm_state > 0)
-			task_wait_event(100*MSEC);
+		timeout.val = get_time().val + 500*MSEC;
 		break;
 
 	case USB_PD_FW_ERASE_SIG:
 		pd_send_vdm(port, USB_VID_GOOGLE, VDO_CMD_ERASE_SIG, NULL, 0);
-
-		/* Wait until VDM is done */
-		while (pd[port].vdm_state > 0)
-			task_wait_event(100*MSEC);
+		timeout.val = get_time().val + 500*MSEC;
 		break;
 
 	case USB_PD_FW_FLASH_WRITE:
@@ -3009,22 +3004,33 @@ static int hc_remote_flash(struct host_cmd_handler_args *args)
 		for (i = 0; i < size; i += VDO_MAX_SIZE - 1) {
 			pd_send_vdm(port, USB_VID_GOOGLE, VDO_CMD_FLASH_WRITE,
 				    data + i, MIN(size - i, VDO_MAX_SIZE - 1));
+			timeout.val = get_time().val + 500*MSEC;
 
 			/* Wait until VDM is done */
-			while (pd[port].vdm_state > 0)
+			while ((pd[port].vdm_state > 0) &&
+			       (get_time().val < timeout.val))
 				task_wait_event(10*MSEC);
+
+			if (pd[port].vdm_state > 0)
+				return EC_RES_TIMEOUT;
 		}
-		break;
+		return EC_RES_SUCCESS;
 
 	default:
 		return EC_RES_INVALID_PARAM;
 		break;
 	}
 
+	/* Wait until VDM is done or timeout */
+	while ((pd[port].vdm_state > 0) && (get_time().val < timeout.val))
+		task_wait_event(100*MSEC);
+
 	if (pd[port].vdm_state < 0)
 		return EC_RES_ERROR;
-	else
-		return EC_RES_SUCCESS;
+	if (pd[port].vdm_state > 0)
+		return EC_RES_TIMEOUT;
+
+	return EC_RES_SUCCESS;
 }
 DECLARE_HOST_COMMAND(EC_CMD_USB_PD_FW_UPDATE,
 		     hc_remote_flash,
