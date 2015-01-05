93aca151ae218fdfea698c7eca7df5ef59817db8
Todd Broch <tbroch@chromium.org>
2015-01-05 12:42:17 -0800

    pd: Remove console command 'typec' by default.
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=samus
    BUG=chrome-os-partner:34489
    TEST=manual,
    
    - Compile and saves ~500bytes.
    - Type 'typec <port>' at console returns
      Command 'typec' not found or ambiguous.
    
    Change-Id: I02c0ec1dd503b02f86d8ac3d5e99ed6ad493c95c

--- include/config.h
+++ include/config.h
@@ -343,6 +343,7 @@
 #undef CONFIG_CMD_SPI_FLASH
 #undef CONFIG_CMD_STACKOVERFLOW
 #undef CONFIG_CMD_TASKREADY
+#undef CONFIG_CMD_TYPEC
 #undef CONFIG_CMD_USB_PD_PE
 
 /*****************************************************************************/
