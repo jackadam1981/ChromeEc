b62de2da64dd5290adfcc336197d9cca668bd04c
james_chao <james_chao@asus.com>
2015-01-12 21:41:27 +0800

    Minnie: add accel kx002 driver
    minnie use the acceleration sensor: kx002, implement the driver.
    
    BUG=chrome-os-partner:34924
    TEST=Use console command "accelinfo on 100", shake piece of board, check the output.
    BRANCH=firmware-veyron-6588.B
    
    Change-Id: I51f15f10474dcfb7a4142749933a734723b7fabc

--- include/config.h
+++ include/config.h
@@ -41,6 +41,7 @@
 
 /* Specify type of accelerometers attached. */
 #undef CONFIG_ACCEL_KXCJ9
+#undef CONFIG_ACCEL_KX022
 
 /* Compile chip support for analog-to-digital convertor */
 #undef CONFIG_ADC
