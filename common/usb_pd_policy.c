a5ea4ce27aa1d789e8c941a8ce683fc787a5a4ec
Todd Broch <tbroch@chromium.org>
2015-01-16 11:14:51 -0800

    pd: refactor object position index.
    Renaming this to 'opos' for consistency with USP-PD specifications
    'object position' in VDM header.
    
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=samus
    BUG=chrome-os-partner:35495
    TEST=manual, On hoho
    1. Still successfully enter default mode DP
    2. Using ectool's pdgetmode pdsetmode can successfully enter/exit
    other modes
    3. Works across hard & soft resets
    
    Change-Id: I08cb8e003ced4de481adcb503bcba3437ebb1ab7

--- common/usb_pd_policy.c
+++ common/usb_pd_policy.c
@@ -165,12 +165,11 @@ unsigned pd_get_max_voltage(void)
 
 static struct pd_policy pe[PD_PORT_COUNT];
 
-#define AMODE_VALID(port) (pe[port].amode.index != -1)
+#define AMODE_VALID(port) (!!pe[port].amode.opos)
 
 void pd_dfp_pe_init(int port)
 {
 	memset(&pe[port], 0, sizeof(struct pd_policy));
-	pe[port].amode.index = -1;
 }
 
 static void dfp_consume_identity(int port, uint32_t *payload)
@@ -257,7 +256,7 @@ int pd_alt_mode(int port)
 		/* zero is reserved */
 		return 0;
 
-	return pe[port].amode.index + 1;
+	return pe[port].amode.opos;
 }
 
 /* Enter default mode or attempt to enter mode via svid & index arguments */
@@ -276,7 +275,7 @@ static int dfp_enter_mode(int port, uint32_t *payload, int use_payload)
 				continue;
 			modep->fx = &supported_modes[i];
 			modep->mode_caps = pe[port].svids[j].mode_vdo[0];
-			modep->index = (opos && (opos < 7)) ? opos - 1 : 0;
+			modep->opos = (opos && (opos < 7)) ? opos : 1;
 			done = 1;
 			break;
 		}
@@ -327,7 +326,7 @@ uint32_t pd_dfp_exit_mode(int port)
 	 * to exit all modes.
 	 */
 	if (pd_is_connected(port)) {
-		modep->index = -1;
+		modep->opos = 0;
 		return VDO(modep->fx->svid, 1,
 			   (CMD_EXIT_MODE | VDO_OPOS(pd_alt_mode(port))));
 	} else {
