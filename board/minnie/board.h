a38a271e25bb7c0acecd58110717d793ef80861c
james_chao <james_chao@asus.com>
2015-01-16 16:48:13 +0800

    Minnie: Add g-sensor kx022 to minnie board.
    Minnie has two kx002 g-sensors, modify to support them.
    
    BUG=None
    TEST=ectool motionsense
    BRANCH=firmware-veyron-6588.B
    
    Change-Id: I6a5d9bb13313bff1dc6f023371e189a11ab7af57
    Signed-off-by: james_chao <james_chao@asus.com>

--- board/minnie/board.h
+++ board/minnie/board.h
@@ -9,6 +9,7 @@
 #define __BOARD_H
 
 /* Optional features */
+#define CONFIG_ACCEL_KX022
 #define CONFIG_AP_HANG_DETECT
 #define CONFIG_BATTERY_CUT_OFF
 #define CONFIG_BATTERY_SMART
@@ -18,6 +19,8 @@
 #define CONFIG_CHARGER_DISCHARGE_ON_AC
 #define CONFIG_CHARGER_V2
 #define CONFIG_CHIPSET_ROCKCHIP
+#define CONFIG_CMD_ACCEL_INFO
+#define CONFIG_CMD_ACCELS
 #define CONFIG_EXTPOWER_GPIO
 #define CONFIG_FORCE_CONSOLE_RESUME
 #define CONFIG_HOST_COMMAND_STATUS
@@ -52,8 +55,11 @@
 
 /* Single I2C port, where the EC is the master. */
 #define I2C_PORT_MASTER 0
+#define I2C_PORT_ACCEL   I2C_PORT_MASTER
 #define I2C_PORT_BATTERY I2C_PORT_MASTER
 #define I2C_PORT_CHARGER I2C_PORT_MASTER
+
+#define CONFIG_SENSOR_BASE 0
 
 /* Timer selection */
 #define TIM_CLOCK32  2
