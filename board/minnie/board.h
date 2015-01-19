c4db5a22d78b948f862e2dd751b1fcb76ded2ca9
james_chao <james_chao@asus.com>
2015-01-19 19:50:27 +0800

    Minnie: Set the minnie board charge IC configure.
    Minnie use the charge IC BQ24770 and need to set
    the charge IC configures.
    
    BUG=None
    TEST=Use the UART command 'charger' and check the output.
    BRANCH=firmware-veyron-6588.B
    
    Change-Id: Ica11d1167ab1d730d91d331d4a8e83342b4cd10a

--- board/minnie/board.h
+++ board/minnie/board.h
@@ -14,8 +14,9 @@
 #define CONFIG_BATTERY_SMART
 #define CONFIG_BOARD_PRE_INIT
 #define CONFIG_CHARGER
-#define CONFIG_CHARGER_BQ24715
+#define CONFIG_CHARGER_BQ24770
 #define CONFIG_CHARGER_DISCHARGE_ON_AC
+#define CONFIG_CHARGER_MAXIMUM_CHARGE_VOLTAGE 4350
 #define CONFIG_CHARGER_V2
 #define CONFIG_CHIPSET_ROCKCHIP
 #define CONFIG_EXTPOWER_GPIO
@@ -76,12 +77,36 @@ enum pwm_channel {
 
 /* Charger module */
 #define CONFIG_CHARGER_SENSE_RESISTOR 10 /* Charge sense resistor, mOhm */
-#define CONFIG_CHARGER_SENSE_RESISTOR_AC 20 /* Input sensor resistor, mOhm */
-/* Input current limit for 45W AC adapter:
- * 45W/19V*85%=2013mA, choose the closest charger setting = 2048mA
+#define CONFIG_CHARGER_SENSE_RESISTOR_AC 25 /* Input sensor resistor, mOhm */
+
+/* Input current limit for 24W/12V AC adapter
+ *
+ * Minnie use the INOM Prochot Function
+ * INOM: adapter average current (110% of input currnet limit)
+ *
+ * 24W/12V adapter, current = 2A
+ * Set the input current = 2A / 110% = 1.818A
  */
-#define CONFIG_CHARGER_INPUT_CURRENT 2048 /* mA, based on Link HW design */
-#define CONFIG_CHARGER_CURRENT_LIMIT 3000 /* PL102 inductor 3.0A(3.8A) */
+#define CONFIG_CHARGER_INPUT_CURRENT 1818 /* mA */
+
+/* Board specific flag for charge IC */
+#define CONFIG_CHARGER_ACOC_LIMIT_300_PERCENTAGE_OF_IDPM
+#define CONFIG_CHARGER_AUTO_WAKEUP_DISABLE
+#define CONFIG_CHARGER_ENVELOP_SELECTOR_ICRIT
+#define CONFIG_CHARGER_ENVELOP_SELECTOR_IDCHG
+#define CONFIG_CHARGER_ENVELOP_SELECTOR_INOM
+#define CONFIG_CHARGER_ENVELOP_SELECTOR_VSYS
+#define CONFIG_CHARGER_ICRIT_COMPARATOR_THRESHOLD_150_PERCENTAGE
+#define CONFIG_CHARGER_IDCHG_COMPARATOR_DEGLITCH_1P6_MS
+#define CONFIG_CHARGER_IDCHG_COMPARATOR_THRESHOLD_4096_MA
+#define CONFIG_CHARGER_ILIM_PIN_DISABLED
+#define CONFIG_CHARGER_INOM_COMPARATOR_DEGLITCH_50MS
+#define CONFIG_CHARGER_LOW_POWER_MODE_DISABLE
+#define CONFIG_CHARGER_LSFET_OCP_THRESHOLD_290MV
+#define CONFIG_CHARGER_PROCHOT_HOST_CLEAR
+#define CONFIG_CHARGER_PROCHOT_PULSE_EXTENSION_ENABLE
+#define CONFIG_CHARGER_PROCHOT_PULSE_WIDTH_1MS
+#define CONFIG_CHARGER_VSYS_COMPARATOR_THRESHOLD_3350_MV
 
 /* Discharge battery when on AC power for factory test. */
 int board_discharge_on_ac(int enable);
