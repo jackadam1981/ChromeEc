240b4fab8fbeb1b6060a1ec041d52b2906dbeb56
Alexandru M Stan <amstan@chromium.org>
2015-01-22 16:56:16 -0800

    hwtimer/hwtimer32: Remove task_resched_if_needed from watchdog help
    Remove task_resched_if_needed, since we don't actually care about scheduled
    tasks after watchdog printing comes, we're going to crash anyway.
    Just return instead.
    
    This makes it work on F0 as well, where we don't have task_resched_if_needed
    
    BUG=None
    TEST=With series, see watchdog help work on any veyron
    BRANCH=veyron
    
    Change-Id: I93cce722b6d53008b015c7cdd56b7e77dc07bbff
    Signed-off-by: Alexandru M Stan <amstan@chromium.org>

--- chip/stm32/hwtimer.c
+++ chip/stm32/hwtimer.c
@@ -390,8 +390,7 @@ void IRQ_HANDLER(IRQ_WD)(void)
 		      * R0=LR so we can pass it to task_resched_if_needed. */
 		     "push {r0, lr}\n"
 		     "bl watchdog_check\n"
-		     "pop {r0, lr}\n"
-		     "b task_resched_if_needed\n");
+		     "pop {r0,pc}\n");
 }
 const struct irq_priority IRQ_PRIORITY(IRQ_WD)
 	__attribute__((section(".rodata.irqprio")))
