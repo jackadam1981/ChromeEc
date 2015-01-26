1ca6610fd6d139bdcec81f4576413acd4bde0f8d
Todd Broch <tbroch@chromium.org>
2015-01-26 13:52:24 -0800

    pd: Fix reverse of log size & port params
    BRANCH=samus
    BUG=chrome-os-partner:35935
    TEST=manual, port & size params are no longer reversed when running
    ectool --name cros_pd pdlog
    
    Change-Id: I83e5ad92c260ff71330b7b5c69b30cf3b7e81a98
    Signed-off-by: Todd Broch <tbroch@chromium.org>

--- common/pd_log.c
+++ common/pd_log.c
@@ -146,7 +146,7 @@ void pd_log_recv_vdm(int port, int cnt, uint32_t *payload)
 	struct ec_response_pd_log *r = (void *)&payload[1];
 	/* update port number from MCU point of view */
 	size_t size = PD_LOG_SIZE(r->size_port);
-	uint8_t size_port = PD_LOG_PORT_SIZE(size, port);
+	uint8_t port_size = PD_LOG_PORT_SIZE(port, size);
 	uint32_t timestamp;
 
 	if ((cnt < 2 + DIV_ROUND_UP(size, sizeof(uint32_t))) ||
@@ -157,7 +157,7 @@ void pd_log_recv_vdm(int port, int cnt, uint32_t *payload)
 	if (r->type != PD_EVENT_NO_ENTRY) {
 		timestamp = (get_time().val >> PD_LOG_TIMESTAMP_SHIFT)
 			  - r->timestamp;
-		log_add_event(r->type, size_port, r->data, r->payload,
+		log_add_event(r->type, port_size, r->data, r->payload,
 			      timestamp);
 		/* record that we have enqueued new content */
 		incoming_logs++;
