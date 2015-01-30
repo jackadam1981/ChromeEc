b4ee39a14bffb44137c61c619007201644b39887
Divya Jyothi <divya.jyothi@intel.com>
2015-01-30 00:29:07 -0800

    Strago: Changed CONFIG_FLASH to CONFIG_FLASH_INTERNAL
    To reflect flash that is internal to the chip
    
    BUG=None
    TEST=None
    BRANCH=None
    
    Change-Id: Ia15cd88a3415faf4dc801761edc439ac5065eac8
    Signed-off-by: Divya Jyothi <divya.jyothi@intel.com>

--- chip/stm32/build.mk
+++ chip/stm32/build.mk
@@ -40,8 +40,8 @@ chip-$(CONFIG_WATCHDOG)+=watchdog.o
 chip-$(HAS_TASK_CONSOLE)+=uart.o
 chip-$(HAS_TASK_KEYSCAN)+=keyboard_raw.o
 chip-$(HAS_TASK_POWERLED)+=power_led.o
-chip-$(CONFIG_FLASH)+=flash-$(CHIP_FAMILY).o
-ifdef CONFIG_FLASH
+chip-$(CONFIG_FLASH_INTERNAL)+=flash-$(CHIP_FAMILY).o
+ifdef CONFIG_FLASH_INTERNAL
 chip-$(CHIP_FAMILY_STM32F)+=flash-f.o
 chip-$(CHIP_FAMILY_STM32F0)+=flash-f.o
 chip-$(CHIP_FAMILY_STM32F3)+=flash-f.o
