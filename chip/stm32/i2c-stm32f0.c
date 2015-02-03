afdfe1da4bfcc2627e58e4cabf5b075db11c001e
Alexandru M Stan <amstan@chromium.org>
2015-02-02 16:48:34 -0800

    i2c-stm32f0: Change RXing to interrupt based
    This should prevent bytes from getting missed.
    
    BUG=chrome-os-partner:33314
    TEST=No more stuck I2C transfer because of the EC's fault
    TEST=No more SPI Errors
    BRANCH=veyron
    
    Change-Id: I102ceb1145c1a136a8448f5cfb9f8bd06740be75
    Signed-off-by: Alexandru M Stan <amstan@chromium.org>

--- chip/stm32/i2c-stm32f0.c
+++ chip/stm32/i2c-stm32f0.c
@@ -289,6 +289,24 @@ void i2c2_event_interrupt(void) { i2c_event_handler(I2C_PORT_EC); }
 DECLARE_IRQ(IRQ_SLAVE, i2c2_event_interrupt, 2);
 #endif
 
+volatile int in_bytes_int;
+volatile uint8_t *in_int;
+
+void i2c_event_interrupt(void) {
+	int port = 0;
+	int isr = STM32_I2C_ISR(port);
+
+	if (isr & STM32_I2C_ISR_RXNE) {
+		ASSERT(in_bytes_int);
+		*(in_int++) = STM32_I2C_RXDR(port);
+		in_bytes_int--;
+	} else {
+		CPRINTS("wrong ISR %04x", isr);
+// 		ASSERT(0); /* This should never happen */
+	}
+}
+DECLARE_IRQ(STM32_IRQ_I2C1, i2c_event_interrupt, 2); /* TODO: make this work for other ports */
+
 /*****************************************************************************/
 /* Interface */
 
@@ -310,6 +328,8 @@ int i2c_xfer(int port, int slave_addr, const uint8_t *out, int out_bytes,
 	/* Clear status */
 	STM32_I2C_ICR(port) = 0x3F38;
 	STM32_I2C_CR2(port) = 0;
+
+	ASSERT(port==0); /* TODO: make this work for other ports */
 
 	if (out_bytes || !in_bytes) {
 		/* Configure the write transfer */
@@ -333,25 +353,19 @@ int i2c_xfer(int port, int slave_addr, const uint8_t *out, int out_bytes,
 			if (rv)
 				goto xfer_exit;
 		}
+
+		in_bytes_int = in_bytes;
+		in_int = in;
+
+		STM32_I2C_CR1(port) |= STM32_I2C_CR1_RXIE;
+
 		/* Configure the read transfer and (re)start */
 		STM32_I2C_CR2(port) = ((in_bytes & 0xFF) << 16)
 				    | STM32_I2C_CR2_RD_WRN | slave_addr
 				    | STM32_I2C_CR2_AUTOEND
 				    | STM32_I2C_CR2_START;
 
-		for (i = 0; i < in_bytes; i++) {
-			/* Wait for receive buffer not empty */
-			rv = wait_isr(port, STM32_I2C_ISR_RXNE);
-			if (rv)
-				goto xfer_exit;
-
-			in[i] = STM32_I2C_RXDR(port);
-
-			/*
-			 * TODO: Make this safer: it's still possible we get
-			 * interrupted by another task in here and miss bytes
-			 */
-		}
+		/* The RXing will happen during the following wait_ISR(STOP). */
 	}
 	rv = wait_isr(port, STM32_I2C_ISR_STOP);
 	if (rv)
@@ -360,6 +374,9 @@ int i2c_xfer(int port, int slave_addr, const uint8_t *out, int out_bytes,
 xfer_exit:
 	/* clear status */
 	STM32_I2C_ICR(port) = 0x3F38;
+
+	STM32_I2C_CR1(port) &= STM32_I2C_CR1_TXIE | STM32_I2C_CR1_RXIE;
+
 	/* On error, queue a stop condition */
 	if (rv) {
 		/* queue a STOP condition */
@@ -444,6 +461,7 @@ static void i2c_init(void)
 	STM32_I2C_OAR1(I2C_PORT_EC) = 0x8000 | CONFIG_HOSTCMD_I2C_SLAVE_ADDR;
 	task_enable_irq(IRQ_SLAVE);
 #endif
+	task_enable_irq(STM32_IRQ_I2C1); /*TODO: fix this */
 }
 DECLARE_HOOK(HOOK_INIT, i2c_init, HOOK_PRIO_INIT_I2C);
 
