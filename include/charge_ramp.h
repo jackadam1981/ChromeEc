2d334c465d2f5b0129a12f45d550a0e221b81745
Alec Berg <alecaberg@chromium.org>
2015-02-06 11:01:18 -0800

    charge_ramp: initial commit of charge ramp module
    Add new charge_ramp module which works with charge_manager to
    slowly increase input current limit in order to find the optimal
    charging current. To do this it looks for either VBUS drooping
    too low or for the charger to over-current.
    
    BUG=chrome-os-partner:34946
    BRANCH=samus
    TEST=tested with a variety of BC1.2 chargers, type-C only chargers,
    and PD chargers to make sure we always stabilize charging at an
    appropriate current limit.
    
    Change-Id: Icc95aa2738ddb221f163f91c14a342a0674f9e0f
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

---
+++ include/charge_ramp.h
@@ -0,0 +1,58 @@
+/* Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Charge input current limit ramp header for Chrome EC */
+
+#ifndef __CROS_EC_CHG_RAMP_H
+#define __CROS_EC_CHG_RAMP_H
+
+/**
+ * Check if ramping is allowed for given supplier
+ *
+ * @supplier Supplier to check
+
+ * @return Ramping is allowed for given supplier
+ */
+int board_is_ramp_allowed(int supplier);
+
+/**
+ * Check if board is consuming full input current
+ *
+ * @return Board is consuming full input current
+ */
+int board_is_consuming_full_charge(void);
+
+/**
+ * Check if VBUS is too low
+ *
+ * @return VBUS is sagging low
+ */
+int board_is_vbus_too_low(void);
+
+#ifdef HAS_TASK_CHG_RAMP
+/**
+ * Notify charge ramp module of supplier type change on a port. If port
+ * is CHARGE_PORT_NONE, the call indicates the last charge supplier went
+ * away.
+ *
+ * @port Active charging port
+ * @supplier Active charging supplier
+ */
+void chg_ramp_charge_supplier_change(int port, int supplier);
+
+/**
+ * Set minimum input current limit for charge ramp module 
+ *
+ * @current Minimum input current limit
+ */
+void chg_ramp_set_min_current(int current);
+#else
+static inline void chg_ramp_charge_supplier_change(int port, int supplier) { }
+
+/* Point directly to board function to set charge limit */
+#define chg_ramp_set_min_current board_set_charge_limit
+#endif
+
+#endif /* __CROS_EC_CHG_RAMP_H */
