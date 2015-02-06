36e67220ede7d05a509a8647b573876058c1c750
Alec Berg <alecaberg@chromium.org>
2015-02-06 11:01:18 -0800

    charge_ramp: initial commit of charge ramp module
    Add new charge_ramp module which works with charge_manager to
    slowly increase input current limit in order to find the optimal
    charging current. To do this it looks for either VBUS drooping
    too low or for the charger to over-current.
    
    BUG=chrome-os-partner:34946
    BRANCH=samus
    TEST=tested with a variety of BC1.2 chargers, type-C only chargers,
    and PD chargers to make sure we always stabilize charging at an
    appropriate current limit.
    
    Change-Id: Icc95aa2738ddb221f163f91c14a342a0674f9e0f
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -5,6 +5,7 @@
 
 #include "adc.h"
 #include "charge_manager.h"
+#include "charge_ramp.h"
 #include "console.h"
 #include "gpio.h"
 #include "hooks.h"
@@ -388,9 +389,11 @@ static void charge_manager_refresh(void)
 			available_charge[new_supplier][new_port].voltage;
 	}
 
-	/* Change the charge limit + charge port if modified. */
-	if (new_port != charge_port || new_charge_current != charge_current) {
-		board_set_charge_limit(new_charge_current);
+	/* Change the charge limit + charge port/supplier if modified. */
+	if (new_port != charge_port || new_charge_current != charge_current ||
+	    new_supplier != charge_supplier) {
+		chg_ramp_charge_supplier_change(new_port, new_supplier);
+		chg_ramp_set_min_current(new_charge_current);
 		CPRINTS("CL: p%d s%d i%d v%d", new_port, new_supplier,
 			new_charge_current, new_charge_voltage);
 	}
