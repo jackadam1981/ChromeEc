63aee9ff69e3747ffe92489bb2f7d49a5b1ced11
Alec Berg <alecaberg@chromium.org>
2015-02-06 11:01:18 -0800

    charge_ramp: initial commit of charge ramp module
    Add new charge_ramp module which works with charge_manager to
    slowly increase input current limit in order to find the optimal
    charging current. To do this it looks for either VBUS drooping
    too low or for the charger to over-current.
    
    BUG=chrome-os-partner:34946
    BRANCH=samus
    TEST=tested with a variety of BC1.2 chargers, type-C only chargers,
    and PD chargers to make sure we always stabilize charging at an
    appropriate current limit.
    
    Change-Id: Icc95aa2738ddb221f163f91c14a342a0674f9e0f
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -5,6 +5,7 @@
 
 #include "adc.h"
 #include "charge_manager.h"
+#include "charge_ramp.h"
 #include "console.h"
 #include "gpio.h"
 #include "hooks.h"
@@ -25,6 +26,9 @@
 /* Keep track of available charge for each charge port. */
 static struct charge_port_info available_charge[CHARGE_SUPPLIER_COUNT]
 					       [PD_PORT_COUNT];
+
+/* Keep track of when the supplier on each port is registered. */
+static timestamp_t registration_time[PD_PORT_COUNT];
 
 /*
  * Charge ceiling for ports. This can be set to temporarily limit the charge
@@ -175,7 +179,11 @@ static void charge_manager_fill_power_info(int port,
 			r->type = USB_CHG_TYPE_OTHER;
 		}
 		r->meas.voltage_max = available_charge[sup][port].voltage;
+#ifdef HAS_TASK_CHG_RAMP
+		r->meas.current_max = chg_ramp_get_current_limit();
+#else
 		r->meas.current_max = available_charge[sup][port].current;
+#endif
 		r->max_power = POWER(available_charge[sup][port]);
 
 		/*
@@ -385,9 +393,13 @@ static void charge_manager_refresh(void)
 			available_charge[new_supplier][new_port].voltage;
 	}
 
-	/* Change the charge limit + charge port if modified. */
-	if (new_port != charge_port || new_charge_current != charge_current) {
-		board_set_charge_limit(new_charge_current);
+	/* Change the charge limit + charge port/supplier if modified. */
+	if (new_port != charge_port || new_charge_current != charge_current ||
+	    new_supplier != charge_supplier) {
+		chg_ramp_charge_supplier_change(
+				new_port, new_supplier,
+				registration_time[new_port]);
+		chg_ramp_set_min_current(new_charge_current);
 		CPRINTS("CL: p%d s%d i%d v%d", new_port, new_supplier,
 			new_charge_current, new_charge_voltage);
 	}
@@ -508,6 +520,7 @@ static void charge_manager_make_change(enum charge_manager_change_type change,
 	if (change == CHANGE_CHARGE) {
 		available_charge[supplier][port].current = charge->current;
 		available_charge[supplier][port].voltage = charge->voltage;
+		registration_time[port] = get_time();
 
 		/*
 		 * If we have a charge on our delayed override port within
