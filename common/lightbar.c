0b21b16499df330220a7aada7f91968db6d6c1c3
Alec Berg <alecaberg@chromium.org>
2015-03-04 17:07:55 -0800

    samus: change tap for battery to not show dimmed last segment
    Change tap for battery lightbar sequence to not show the last segment
    dimmed as a percentage of your battery.
    
    BUG=chrome-os-partner:37335
    BRANCH=samus
    TEST=use battfake console command to test out every increment of 10%
    and use "lightbar seq tap" to show tap.
    
    Change-Id: I4f38d26a8cfbecfa6efc86fcc8751ca8cb34879b
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- common/lightbar.c
+++ common/lightbar.c
@@ -873,16 +873,6 @@ static uint32_t sequence_KONAMI(void)
 	return r;
 }
 
-/* Returns 0.0 to 1.0 for val in [min, min + ofs] */
-static int range(int val, int min, int ofs)
-{
-	if (val <= min)
-		return 0;
-	if (val >= min+ofs)
-		return FP_SCALE;
-	return (val - min) * FP_SCALE / ofs;
-}
-
 /* Handy constant */
 #define CUT (100 / NUM_LEDS)
 
@@ -892,12 +882,10 @@ static uint32_t sequence_TAP_inner(int dir)
 	timestamp_t start, now;
 	uint32_t elapsed_time = 0;
 	int i, l, ci, max_led;
-	int f_min, f_delta, f_osc, f_power, f_mult;
+	int f_osc, f_mult;
 	int gi, gr, gate[NUM_LEDS] = {0, 0, 0, 0};
 	uint8_t w = 0;
 
-	f_min = st.p.tap_seg_min_on * FP_SCALE / 100;
-	f_delta = (st.p.tap_seg_max_on - st.p.tap_seg_min_on) * FP_SCALE / 100;
 	f_osc = st.p.tap_seg_osc * FP_SCALE / 100;
 
 	start = get_time();
@@ -924,26 +912,10 @@ static uint32_t sequence_TAP_inner(int dir)
 
 		for (i = 0; i < NUM_LEDS; i++) {
 
-			if (max_led > i) {
+			if (max_led >= i)
 				f_mult = FP_SCALE;
-			} else if (max_led < i) {
+			else if (max_led < i)
 				f_mult = 0;
-			} else {
-				switch (base_color) {
-				case RED:
-					f_power = range(st.battery_percent, 0,
-						st.p.battery_threshold[0] - 1);
-					break;
-				case YELLOW:
-					f_power = range(st.battery_percent,
-							i * CUT, CUT - 1);
-					break;
-				case GREEN:
-					/* green is always full on */
-					f_power = FP_SCALE;
-				}
-				f_mult = f_min + f_power * f_delta / FP_SCALE;
-			}
 
 			f_mult = f_mult * gate[i] / FP_SCALE;
 
