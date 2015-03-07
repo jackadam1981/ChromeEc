e372aa47457d9702b10cd3f6a8e8fd3e8455f0de
Gwendal Grignou <gwendal@chromium.org>
2015-03-07 10:24:01 -0800

    driver: Add BMI160 basic driver support
    Basic initialization of BMI160.
    
    BUG=chrome-os-partner:36018
    BRANCH=none
    TEST=Ability to read samples with accelinfo.
    
    Change-Id: I2d47c845cf8c2c5e965b37356192e780f4d7722e
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -1577,6 +1577,7 @@ enum motionsense_command {
 enum motionsensor_type {
 	MOTIONSENSE_TYPE_ACCEL = 0,
 	MOTIONSENSE_TYPE_GYRO = 1,
+	MOTIONSENSE_TYPE_MAG = 2,
 };
 
 /* List of motion sensor locations. */
@@ -1589,6 +1590,7 @@ enum motionsensor_location {
 enum motionsensor_chip {
 	MOTIONSENSE_CHIP_KXCJ9 = 0,
 	MOTIONSENSE_CHIP_LSM6DS0 = 1,
+	MOTIONSENSE_CHIP_BMI160 = 2,
 };
 
 /* Module flag masks used for the dump sub-command. */
