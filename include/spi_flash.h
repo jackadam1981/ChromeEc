baf1a676074e0a60ea4ec5ac23f119778cba74dd
Shawn Nematbakhsh <shawnn@chromium.org>
2015-03-10 11:50:39 -0700

    spi_flash: Add protect support for W25X40
    W25X40 uses a different protection register encoding than our existing
    W25Q64 code. Move the SPI ROM option to a config, and add support for
    the new part.
    
    BUG=chrome-os-partner:37688
    TEST=`make buildall -j`. W25X40 protection code tested in a subsequent
    commit.
    BRANCH=None
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: Iaaeabf42c6c62c20debc91afd2cf8671c14244c8

--- include/spi_flash.h
+++ include/spi_flash.h
@@ -143,9 +143,9 @@ uint64_t spi_flash_get_unique_id(void);
  *
  * @param wp Status register write protection mode
  *
- * @return EC_SUCCESS for no protection, or non-zero if error.
+ * @return enum spi_flash_wp status based on protection
  */
-int spi_flash_check_wp(void);
+enum spi_flash_wp spi_flash_check_wp(void);
 
 /**
  * Set SPI flash status register write protection
