4b302e7cdfb2e0c2fd53dfc1df80f2d849e3ae59
Gwendal Grignou <gwendal@chromium.org>
2015-03-18 10:44:22 -0700

    board: add Ryu P4 sensor hub image
    The sensor hub is a STM32F411, connected to p0-p3 sensors.
    
    BUG=chrome-os-partner:36018
    TEST=Sensors are found, working, system is stable
    BRANCH=none
    
    Change-Id: Ib45473fe001dc537f65e64a950bff4bb591b641a
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- board/ryu_sh/board.h
+++ board/ryu_sh/board.h
@@ -3,25 +3,19 @@
  * found in the LICENSE file.
  */
 
-/* ryu sensor board configuration */
+/* ryu sensor hub configuration */
 
 #ifndef __BOARD_H
 #define __BOARD_H
 
-/* 48 MHz SYSCLK clock frequency */
-#define CPU_CLOCK 48000000
+/* 84 MHz CPU/AHB/APB2 clock frequency (APB1 = 42 Mhz) */
+#define CPU_CLOCK 84000000
+#define CONFIG_VOLTAGE_CORE_1800
+#define CONFIG_FLASH_WRITE_SIZE CONFIG_FLASH_WRITE_SIZE_1800
+#define CONFIG_FLASH_WS_DIV CONFIG_FLASH_WS_DIV_1800
 
 /* the UART console is on USART1 (PA9/PA10) */
-#undef CONFIG_UART_CONSOLE
 #define CONFIG_UART_CONSOLE 1
-
-/*
- * The RO firmware image size is limited to 40K to
- * leave more space to the RW image.
- */
-#undef CONFIG_FW_IMAGE_SIZE
-#define CONFIG_FW_IMAGE_SIZE (40*1024)
-#undef CONFIG_FW_INCLUDE_RO
 
 /* By default, enable all console messages  */
 #define CC_DEFAULT     CC_ALL
@@ -31,8 +25,11 @@
 #undef CONFIG_HIBERNATE
 #define CONFIG_ACCELGYRO_LSM6DS0
 #define CONFIG_STM_HWTIMER32
+#define CONFIG_WATCHDOG_HELP
+#define CONFIG_TASK_PROFILING
+#define CONFIG_DMA_HELP
+#undef CONFIG_FLASH
 #define CONFIG_I2C
-#define CONFIG_BOARD_PRE_INIT
 #undef  CONFIG_LID_SWITCH
 #undef CONFIG_CMD_POWER_AP
 #define CONFIG_POWER_COMMON
@@ -40,12 +37,25 @@
 #define CONFIG_CMD_ACCELS
 #define CONFIG_CMD_ACCEL_INFO
 #define CONFIG_VBOOT_HASH
-#undef CONFIG_WATCHDOG_HELP
+
+#define CONFIG_UART_TX_DMA
+/* Use a bigger console output buffer */
+#undef CONFIG_UART_TX_BUF_SIZE
+#define CONFIG_UART_TX_BUF_SIZE 8192
+#undef CONFIG_UART_RX_DMA
+#define CONFIG_UART_TX_DMA_CH STM32_DMAS_USART1_TX
+#define CONFIG_UART_RX_DMA_CH STM32_DMAS_USART1_RX
+#define CONFIG_UART_TX_REQ_CH STM32_REQ_USART1_TX
+#define CONFIG_UART_RX_REQ_CH STM32_REQ_USART1_RX
+
+#define CONFIG_CMD_FLASH
+#undef CONFIG_FLASH_ERASE_SUPPORT
+#define CONFIG_FLASH_ERASE_SUPPORT 3
 
 /* I2C ports configuration */
+#define CONFIG_I2C_DEBUG
 #define I2C_PORT_MASTER 1
 #define I2C_PORT_SLAVE  0
-#define I2C_PORT_EC I2C_PORT_SLAVE
 #define I2C_PORT_ACCEL I2C_PORT_MASTER
 #define I2C_PORT_COMPASS I2C_PORT_MASTER
 
@@ -64,9 +74,7 @@
 
 /* Timer selection */
 #define TIM_CLOCK32 2
-#define TIM_ADC     3
-
-#include "gpio_signal.h"
+#define TIM_WATCHDOG 11
 
 enum power_signal {
 	ECDRIVEN_SUSPEND_ASSERTED,
@@ -74,6 +82,8 @@ enum power_signal {
 	/* Number of power signals */
 	POWER_SIGNAL_COUNT
 };
+
+#include "gpio_signal.h"
 
 #endif /* !__ASSEMBLER__ */
 
