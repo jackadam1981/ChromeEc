2a8eaa375c8a3a5c436d5eec02551dd434f61881
Ben Lok <ben.lok@mediatek.com>
2015-03-23 23:08:38 +0800

    WIP: oak_pd: setup the gpio.inc for the Oak rev1.
    - Config the pinouts of Oak rev1.
    - Undefined the CONFIG_USBC_SS_MUX.
    - Undefined CONFIG_WATCHDOG for debugging.
    - Modify usb_pd_config.h to match the pin definition of gpio.inc
    - Set PLLCLK [HSI(8MHz)/2*12)=48MHz] as SYSCLK,
      due to HSI48 isn't available for STM32F05X.
    
    BUG=none
    BRANCH=none
    TEST=make -j BOARD=oak_pd
    
    Signed-off-by: Ben Lok <ben.lok@mediatek.com>
    
    Change-Id: Ie4ce4468946ce2fda28859903783c4640ba02101

--- board/oak_pd/usb_pd_policy.c
+++ board/oak_pd/usb_pd_policy.c
@@ -87,9 +87,11 @@ void pd_transition_voltage(int idx)
 
 int pd_set_power_supply_ready(int port)
 {
+/* TODO: Call EC to do that */
+#if 0
 	/* provide VBUS */
 	gpio_set_level(port ? GPIO_USB_C1_5V_EN : GPIO_USB_C0_5V_EN, 1);
-
+#endif
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
 
@@ -98,9 +100,11 @@ int pd_set_power_supply_ready(int port)
 
 void pd_power_supply_reset(int port)
 {
+/* TODO: Call EC to do that */
+#if 0
 	/* Kill VBUS */
 	gpio_set_level(port ? GPIO_USB_C1_5V_EN : GPIO_USB_C0_5V_EN, 0);
-
+#endif
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
 }
