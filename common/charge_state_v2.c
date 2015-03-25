07c6d85777972902b7b1d487b8890921faa3621d
Vincent Palatin <vpalatin@chromium.org>
2015-03-25 12:31:39 -0700

    charge_state_v2: properly print static battery info errors
    When calling problem() with 0 as value, it's never printed on the EC
    console since the problem() function is de-duplicating the messages by
    checking against the last value (which is initialized at zero).
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=chrome-os-partner:38401
    TEST=run on the current Ryu code (which has a faulty BQ27742 driver) and
    see the "charge problem: static update [...]" message.
    
    Change-Id: Iedfbc95e3751bc5b22452187b404a09b633160d7

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -156,7 +156,7 @@ static int update_static_battery_info(void)
 	*host_get_memmap(EC_MEMMAP_BATT_FLAG) = 0;
 
 	if (rv)
-		problem(PR_STATIC_UPDATE, 0);
+		problem(PR_STATIC_UPDATE, rv);
 	else
 		/* No errors seen. Battery data is now present */
 		*host_get_memmap(EC_MEMMAP_BATTERY_VERSION) = 1;
