1574430a21be790d53cff241012939b14073b412
Andrey Petrov <andrey.petrov@intel.com>
2015-03-27 11:09:44 -0700

    mec1322: initial version of lfw loader
    This is an initial version of the loader.
    Error handling is very basic, sysjump support is missing.
    
    <DRAFT - For review only>
    
    BUG=chrome-os-partner:37510
    BRANCH=strago-mec
    TEST=make buildall; manually tested booting/copying on Strago
    
    Change-Id: I13627974308609b15640e81707b95c6f1ce9f365
    Signed-off-by: Andrey Petrov <andrey.petrov@intel.com>

--- chip/mec1322/build.mk
+++ chip/mec1322/build.mk
@@ -29,10 +29,22 @@ SCRIPTDIR:=./chip/${CHIP}/util
 # Allow SPI size to be overridden by board specific size, default to 256KB.
 CHIP_SPI_SIZE_KB?=256
 
+mec1322-lfw = chip/mec1322/lfw/ec_lfw
+mec1322-lfw-flat = $(out)/$(mec1322-lfw)-lfw.flat
+
+# build these specifically for lfw with -lfw suffix
+objs_lfw = $(addsuffix -lfw.o, $(out)/core/$(CORE)/cpu \
+		$(addprefix $(out)/common/, util gpio version) \
+		$(addprefix $(out)/chip/$(CHIP)/, spi dma gpio clock hwtimer) \
+		$(out)/$(mec1322-lfw))
+
 # Command to convert $^ to $@.tmp
-cmd_obj_to_bin = $(OBJCOPY) --gap-fill=0xff -O binary $^ $@.tmp1 ; \
+cmd_obj_to_bin = $(OBJCOPY) --gap-fill=0xff -O binary $< $@.tmp1 ; \
 		${SCRIPTDIR}/pack_ec.py -o $@.tmp -i $@.tmp1 \
-		--loader_file ${SCRIPTDIR}/ecloader.bin \
+		--loader_file $(mec1322-lfw-flat) \
 		--payload_key ${SCRIPTDIR}/rsakey_sign_payload.pem \
 		--header_key ${SCRIPTDIR}/rsakey_sign_header.pem \
 		--spi_size=${CHIP_SPI_SIZE_KB} ; rm -f $@.tmp1
+
+# add dependency on loader
+$(out)/$(PROJECT).bin: $(mec1322-lfw-flat)
