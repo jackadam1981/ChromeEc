69a920e8692cf72fcc171407fde66f2b133dc309
Steven Jian <steven.jian@intel.com>
2015-04-01 01:25:42 +0800

    mec1322: Add a config to indicate if chip needs PORT to set up gpio
    For now, only mec1322 does not need port data and change
    glower/mec1322_evb/strago gpio configs.
    
    BUG=None
    BRANCH=None
    TEST=compile with board=glower successfully.
    
    Change-Id: Ibda97c6563ad447d16dab39ecadab43ccb25174b
    Signed-off-by: Steven Jian <steven.jian@intel.com>

--- include/gpio_list.h
+++ include/gpio_list.h
@@ -3,6 +3,7 @@
  * found in the LICENSE file.
  */
 
+#ifdef CONFIG_GPIO_PORT
 #ifdef CONFIG_COMMON_GPIO_SHORTNAMES
 #define GPIO(name, port, pin, flags, signal) \
 	{#port#pin, GPIO_##port, (1 << pin), flags, signal},
@@ -10,9 +11,19 @@
 #define GPIO(name, port, pin, flags, signal) \
 	{#name, GPIO_##port, (1 << pin), flags, signal},
 #endif
+#else
+#ifdef CONFIG_COMMON_GPIO_SHORTNAMES
+#define GPIO(name, gpionum, flags, signal) \
+        {#gpionum, (gpionum/10), (1<<(gpionum%10)), flags, signal},
+#else
+#define GPIO(name, gpionum, flags, signal) \
+        {#name, (gpionum/10), (1<<(gpionum%10)), flags, signal},
+#endif
+#endif
 
 #define UNIMPLEMENTED(name) \
 	{#name, DUMMY_GPIO_BANK, 0, GPIO_DEFAULT, NULL},
+
 
 /* GPIO signal list. */
 const struct gpio_info gpio_list[] = {
@@ -25,11 +36,18 @@ BUILD_ASSERT(ARRAY_SIZE(gpio_list) == GPIO_COUNT);
  * Construct the gpio_alt_funcs array.  This array is used by gpio_config_module
  * to enable and disable GPIO alternate functions on a module by module basis.
  */
+#ifdef CONFIG_GPIO_PORT
 #define ALTERNATE(port, mask, function, module, flags)	\
 	{GPIO_##port, mask, function, module, flags},
+#else
+#define ALTERNATE(pinbase, mask, function, module, flags)	\
+	{ pinbase, mask, function, module, flags},
+#endif
+
 
 const struct gpio_alt_func gpio_alt_funcs[] = {
 	#include "gpio.wrap"
 };
 
 const int gpio_alt_funcs_count = ARRAY_SIZE(gpio_alt_funcs);
+
