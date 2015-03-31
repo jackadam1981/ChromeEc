d8d43560acf0c406b3d99208c52aaf38efaa4046
Steven Jian <steven.jian@intel.com>
2015-04-01 01:25:42 +0800

    mec1322: Simplify GPIO lists
    Our existing GPIO macros use port# / gpio#, but the concept of different
    GPIO ports does not exist on the mec1322. Therefore, add new GPIO macros
    for chips which do not have distinct GPIO ports.
    
    BUG=None
    BRANCH=None
    TEST=make buildall -j
    
    Change-Id: Ibda97c6563ad447d16dab39ecadab43ccb25174b
    Signed-off-by: Steven Jian <steven.jian@intel.com>

--- chip/npcx/config_chip.h
+++ chip/npcx/config_chip.h
@@ -109,4 +109,7 @@
 /* Compile for running from RAM instead of flash */
 /* #define COMPILE_FOR_RAM */
 
+#define GPIO_PIN(port, index) GPIO_##port, (1 << index)
+#define GPIO_PIN_MASKS(port, mask) GPIO_##port, (mask)
+
 #endif  /* __CROS_EC_CONFIG_CHIP_H */
