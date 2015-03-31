a1c0103646c8ee907d79a4eed7cf6e4682ac5495
Alec Berg <alecaberg@chromium.org>
2015-03-31 16:09:24 -0700

    pd: treat externally powered dualrole devices as dedicated chargers
    Treat externally powered dualrole devices as dedicated chargers.
    This allows us to default to consuming power from externally powered
    dualrole devices and cancels a charger override when one is attached.
    
    BUG=none
    BRANCH=none
    TEST=tested with third-party dualrole device that can be externally
    powered.
    
    Change-Id: I850eba668e86d311d9353aa3881fc3a518409630
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- board/samus_pd/usb_pd_policy.c
+++ board/samus_pd/usb_pd_policy.c
@@ -157,18 +157,23 @@ void pd_execute_data_swap(int port, int data_role)
 	set_usb_switches(port, (data_role == PD_ROLE_UFP));
 }
 
-void pd_check_pr_role(int port, int pr_role, int partner_pr_swap)
+void pd_check_pr_role(int port, int pr_role, int flags)
 {
-	/* If sink, and dual role toggling is on, then switch to source */
-	if (partner_pr_swap && pr_role == PD_ROLE_SINK &&
+	/*
+	 * If partner is dual-role power, and is not externally powered, and
+	 * we are a sink, and dual role toggling is on, then switch to source
+	 */
+	if ((flags & PD_FLAGS_PARTNER_DR_POWER) &&
+	    !(flags & PD_FLAGS_PARTNER_EXTPOWER) &&
+	    pr_role == PD_ROLE_SINK &&
 	    pd_get_dual_role() == PD_DRP_TOGGLE_ON)
 		pd_request_power_swap(port);
 }
 
-void pd_check_dr_role(int port, int dr_role, int partner_dr_swap)
+void pd_check_dr_role(int port, int dr_role, int flags)
 {
 	/* If UFP, try to switch to DFP */
-	if (partner_dr_swap && dr_role == PD_ROLE_UFP)
+	if ((flags & PD_FLAGS_PARTNER_DR_DATA) && dr_role == PD_ROLE_UFP)
 		pd_request_data_swap(port);
 }
 /* ----------------- Vendor Defined Messages ------------------ */
