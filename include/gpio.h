1adae985753e50e7bafe640e39bfdf2b7caea06e
Aseda Aboagye <aaboagye@google.com>
2015-04-08 16:42:55 -0700

    gpio: Refactor IRQ handler pointer out of gpio_list
    In the gpio_info struct, we had a irq_handler pointer defined even
    though a majority of the GPIOs did not have irq handlers associated. By
    removing the irq_handler pointer out of the struct, we can save some
    space with some targets saving more than others. (For example, ~260
    bytes for samus_pd).
    
    This change also brings about a new define:
    
         GPIO_INT(name, port, pin, flags, signal)
    
    And the existing GPIO macro has had the signal parameter removed since
    they were just NULL.
    
         GPIO(name, port, pin, flags)
    
    In each of the gpio.inc files, all the GPIOs with irq handlers must be
    defined at the top of the file. This is because their enum values from
    gpio_signal are used as the index to the gpio_irq_handlers table.
    
    BUG=chromium:471331
    BRANCH=none
    TEST=Flashed ec to samus and samus_pd, verified lightbar tap, lid, power
    button, keyboard, charging, all still working.
    TEST=Moved a GPIO_INT declaration after a GPIO declaration and watched the build
    fail.
    
    Change-Id: Id6e261b0a3cd63223ca92f2e96a80c95e85cdefb
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- include/gpio.h
+++ include/gpio.h
@@ -58,17 +58,19 @@ struct gpio_info {
 
 	/* Flags (GPIO_*; see above) */
 	uint32_t flags;
-
-	/*
-	 * Interrupt handler.  If non-NULL, and the signal's interrupt is
-	 * enabled, this will be called in the context of the GPIO interrupt
-	 * handler.
-	 */
-	void (*irq_handler)(enum gpio_signal signal);
 };
 
 /* Signal information from board.c.  Must match order from enum gpio_signal. */
 extern const struct gpio_info gpio_list[];
+
+/* Interrupt handler table for those GPIOs which have IRQ handlers.
+ *
+ * If the signal's interrupt is enabled, this will be called in the
+ * context of the GPIO interrupt handler.
+ */
+extern void (* const gpio_irq_handlers[])(enum gpio_signal signal);
+extern const int gpio_ih_count;
+#define GPIO_IH_COUNT gpio_ih_count
 
 /* GPIO alternate function structure, for use by board.c */
 struct gpio_alt_func {
