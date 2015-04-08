3f341311155ee90a58e5188f4e53a3268057ed49
Alec Berg <alecaberg@chromium.org>
2015-04-08 09:42:43 -0700

    pd: samus: support vconn swap and enable on Samus when in S0/S3
    Support VCONN swap on samus and always accept VCONN swap when in
    S0 or S3. In S5, we can't provide VCONN, so reject VCONN swap
    requests.
    
    BUG=chrome-os-partner:34978
    BRANCH=samus
    TEST=load on two samus' and use "pd 1 swap vconn" to swap which
    side is source vconn. also run in S5 and verify swap request is
    rejected.
    
    Change-Id: I04be8d1d910a2d6c5ad8b27a790f8e33121c86ee
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- board/samus_pd/usb_pd_policy.c
+++ board/samus_pd/usb_pd_policy.c
@@ -151,6 +151,11 @@ int pd_check_data_swap(int port, int data_role)
 	return (data_role == PD_ROLE_UFP) ? 1 : 0;
 }
 
+int pd_check_vconn_swap(int port)
+{
+	return gpio_get_level(GPIO_PCH_SLP_S5_L);
+}
+
 void pd_execute_data_swap(int port, int data_role)
 {
 	/* Open USB switches when taking UFP role */
