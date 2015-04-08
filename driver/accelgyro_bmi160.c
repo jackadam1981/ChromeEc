c10187b478b0bfa174f49311570547e7fdd68d49
Gwendal Grignou <gwendal@chromium.org>
2015-04-08 13:33:03 -0700

    WIP: bmi160: Add double tap recognition, lift and fifo support.
    Add interrupt support and Fifo decoding
    
    BUG=chrome-os-partner:39900
    TEST=Testing on Nucleo boad and Smaug
    BRANCH=none
    
    Change-Id: I2fd5c25cbf42d6525ceacffd541ae5ef19620a12
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- driver/accelgyro_bmi160.c
+++ driver/accelgyro_bmi160.c
@@ -20,6 +20,7 @@
 
 #define CPUTS(outstr) cputs(CC_ACCEL, outstr)
 #define CPRINTF(format, args...) cprintf(CC_ACCEL, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_ACCEL, format, ## args)
 
 /*
  * Struct for pairing an engineering value with the register value for a
@@ -110,6 +111,19 @@ static inline const struct accel_param_pair *get_odr_table(
 	}
 }
 
+static inline enum bmi160_sensor_type get_bmi160_type(
+		enum motionsensor_type type)
+{
+	switch (type) {
+	case MOTIONSENSE_TYPE_ACCEL:
+		return BMI160_ACC;
+	case MOTIONSENSE_TYPE_GYRO:
+		return BMI160_GYR;
+	case MOTIONSENSE_TYPE_MAG:
+		return BMI160_MAG;
+	}
+}
+
 static inline int get_xyz_reg(enum motionsensor_type type)
 {
 	switch (type) {
@@ -123,7 +137,6 @@ static inline int get_xyz_reg(enum motionsensor_type type)
 		return -1;
 	}
 }
-
 /**
  * @return reg value that matches the given engineering value passed in.
  * The round_up flag is used to specify whether to round up or down.
@@ -270,12 +283,272 @@ static int get_data_rate(const struct motion_sensor_t *s,
 	return EC_SUCCESS;
 }
 
+int normalize(const struct motion_sensor_t *s, vector_3_t v, uint8_t *data)
+{
+	int ret, range = 0;
+
+	v[0] = ((int16_t)((data[1] << 8) | data[0]));
+	v[1] = ((int16_t)((data[3] << 8) | data[2]));
+	v[2] = ((int16_t)((data[5] << 8) | data[4]));
+
+	ret = get_range(s, &range);
+	if (ret)
+		return EC_ERROR_UNKNOWN;
+
+	v[0] *= range;
+	v[1] *= range;
+	v[2] *= range;
+
+	/* normalize the accel scale: 1G = 1024 */
+	if (MOTIONSENSE_TYPE_ACCEL == s->type) {
+		v[0] >>= 5;
+		v[1] >>= 5;
+		v[2] >>= 5;
+	} else {
+		v[0] >>= 8;
+		v[1] >>= 8;
+		v[2] >>= 8;
+	}
+	return EC_SUCCESS;
+}
+
 #ifdef CONFIG_ACCEL_INTERRUPTS
+
+#define TASK_EVENT_SENSOR_PENDING TASK_EVENT_CUSTOM(1)
+
+void bmi160_interrupt(enum gpio_signal signal)
+{
+	CPRINTS("Interrupt from sensor: %d, %d!", signal,
+			gpio_get_level(signal));
+	task_set_event(TASK_ID_BMI160SENSE, TASK_EVENT_SENSOR_PENDING, 0);
+}
+
+static int config_interrupt(const struct motion_sensor_t *s)
+{
+	int ret, tmp;
+	mutex_lock(s->mutex);
+
+	/* configure int2 as an external input */
+	/* Latch until intterupts */
+	ret = raw_read8(s->i2c_addr, BMI160_INT_LATCH, &tmp);
+	tmp |= BMI160_INT2_INPUT_EN | 0xF;
+	ret = raw_write8(s->i2c_addr, BMI160_INT_LATCH, tmp);
+
+	/* set flat interrupt */
+	gpio_enable_interrupt(GPIO_BMI160_INT1_L);
+
+	/* configure int1 as an interupt */
+	ret = raw_write8(s->i2c_addr, BMI160_INT_OUT_CTRL,
+		BMI160_INT_CTRL(1, OUTPUT_EN) |
+		BMI160_INT_CTRL(1, EDGE_CTRL));
+
+	/* Map Simple/Double Tap to int 1
+	 * Map Flat interrupt to int 1
+	 */
+	ret = raw_write8(s->i2c_addr, BMI160_INT_MAP_REG(1),
+		BMI160_INT_FLAT | BMI160_INT_D_TAP | BMI160_INT_S_TAP);
+
+#if 0
+	/* map fifo water mark to int 1 */
+	ret = raw_write8(s->i2c_addr, BMI160_INT_FIFO_MAP,
+		BMI160_INT_MAP(1, FWM));
+
+	/* configure fifo watermark at 50% */
+	ret = raw_write8(s->i2c_addr, BMI160_FIFO_CONFIG_0,
+			512 / sizeof(uint32_t));
+	ret = raw_write8(s->i2c_addr, BMI160_FIFO_CONFIG_1,
+			BMI160_FIFO_TAG_TIME_EN |
+			BMI160_FIFO_TAG_INT1_EN |
+			BMI160_FIFO_TAG_INT2_EN |
+			BMI160_FIFO_HEADER_EN |
+			BMI160_FIFO_MAG_EN |
+			BMI160_FIFO_ACC_EN |
+			BMI160_FIFO_GYR_EN);
+#endif
+
+	/* Set double tap interrupt and fifo*/
+	ret = raw_read8(s->i2c_addr, BMI160_INT_EN_0, &tmp);
+	tmp |= BMI160_INT_FLAT_EN | BMI160_INT_D_TAP_EN | BMI160_INT_S_TAP_EN;
+	ret = raw_write8(s->i2c_addr, BMI160_INT_EN_0, tmp);
+
+#if 0
+	ret = raw_read8(s->i2c_addr, BMI160_INT_EN_1, &tmp);
+	tmp |= BMI160_INT_FWM_EN;
+	ret = raw_write8(s->i2c_addr, BMI160_INT_EN_1, tmp);
+#endif
+
+	mutex_unlock(s->mutex);
+	return ret;
+}
 static int set_interrupt(const struct motion_sensor_t *s,
 			       unsigned int threshold)
 {
 	/* Currently unsupported. */
 	return EC_ERROR_UNKNOWN;
+}
+
+enum fifo_state {
+	FIFO_HEADER,
+	FIFO_DATA_SKIP,
+	FIFO_DATA_TIME,
+	FIFO_DATA_CONFIG,
+};
+
+
+#define BMI160_FIFO_BUFFER 64
+uint8_t buffer[BMI160_FIFO_BUFFER];
+int buffer_time;
+/*
+ * Decode the header from the fifo.
+ * Return 0 if we need further processing.
+ */
+int bmi160_decode_header(enum fifo_header hdr, uint8_t *bp, int *nb)
+{
+	if ((hdr & BMI160_FH_MODE_MASK) == BMI160_EMPTY &&
+			(hdr & BMI160_FH_PARM_MASK) != 0) {
+		int i;
+		/* Fast past, we have data */
+		/* do our own __builtin_popcount,
+		 * __popcountsi2 missing
+		 */
+		int x = (hdr & 0x1c) >> 2;
+		x = x - ((x >> 1) & 0x55555555);
+		/* Every 2 bits holds the sum of every pair of bits */
+		x = ((x >> 2) & 0x33333333) + (x & 0x33333333);
+		if ((bp + x * 6) > (buffer + BMI160_FIFO_BUFFER)) {
+			/* frame is not complete, it
+			 * will be retransmitted.
+			 */
+			bp = buffer + BMI160_FIFO_BUFFER;
+			return 1;
+		}
+		for (i = BMI160_MAG; i <= BMI160_ACC; i++) {
+			if (hdr & (1 << (i + 2))) {
+				vector_3_t v;
+				/* normalize: need a proper conversion */
+				normalize(&motion_sensors[1 - i], v, bp);
+				/*
+				 * CPRINTF("%d: X: %4d, Y: %4d, Z: %4d ",
+				 * i, v[X], v[Y], v[Z]);
+				 */
+				bp += 6;
+			}
+		}
+		(*nb)++;
+		if (hdr & BMI160_FH_EXT_MASK)
+			CPRINTF("%s%s\n",
+				(hdr & 0x1 ? "INT1" : ""),
+				(hdr & 0x2 ? "INT2" : ""));
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+void bmi160_load_fifo(const struct motion_sensor_t *s)
+{
+	int tmp, fifo_length, done = 0;
+	int nb = 0;
+	/* Read fifo */
+	raw_read8(s->i2c_addr, BMI160_FIFO_LENGTH_1, &tmp);
+	fifo_length = tmp << 8;
+	raw_read8(s->i2c_addr, BMI160_FIFO_LENGTH_0, &tmp);
+	fifo_length |= tmp;
+	CPRINTS("FIFO byte to read: %d", fifo_length);
+	/* we want to collect the timestamp and last byte */
+	fifo_length += 1 + 3 + 1;
+	do {
+		const uint8_t fifo = BMI160_FIFO_DATA;
+		enum fifo_state state = FIFO_HEADER;
+		uint8_t *bp = buffer;
+		i2c_lock(I2C_PORT_ACCEL, 1);
+		i2c_xfer(I2C_PORT_ACCEL, s->i2c_addr,
+				&fifo, 1, buffer,
+				BMI160_FIFO_BUFFER, I2C_XFER_SINGLE);
+		i2c_lock(I2C_PORT_ACCEL, 0);
+		while (!done && bp != buffer + BMI160_FIFO_BUFFER) {
+			switch (state) {
+			case FIFO_HEADER: {
+				enum fifo_header hdr = *bp++;
+				if (bmi160_decode_header(hdr, bp, &nb))
+					continue;
+				/* Other cases */
+				hdr &= 0xdc;
+				switch (hdr) {
+				case BMI160_EMPTY:
+					done = 1;
+					break;
+				case BMI160_SKIP:
+					state = FIFO_DATA_SKIP;
+					break;
+				case BMI160_TIME:
+					state = FIFO_DATA_TIME;
+					break;
+				case BMI160_CONFIG:
+					state = FIFO_DATA_CONFIG;
+					break;
+				default:
+					CPRINTS("Unknown header: 0x%02x", hdr);
+				}
+				break;
+			}
+			case FIFO_DATA_SKIP:
+				CPRINTF("skipped %d frames\n", *bp++);
+				state = FIFO_HEADER;
+				break;
+			case FIFO_DATA_CONFIG:
+				CPRINTF("config change: 0x%02x\n", *bp++);
+				state = FIFO_HEADER;
+				break;
+			case FIFO_DATA_TIME:
+				if (bp + 3 > buffer + BMI160_FIFO_BUFFER) {
+					bp = buffer + BMI160_FIFO_BUFFER;
+					continue;
+				}
+				buffer_time = (bp[2] << 16) | (bp[1] << 8) |
+					bp[0];
+				/* CPRINTF("timestamp %d\n", buffertime); */
+				state = FIFO_HEADER;
+				bp += 3;
+				break;
+			default:
+				CPRINTS("Unknown data: 0x%02x\n", *bp++);
+				state = FIFO_HEADER;
+			}
+		}
+	} while (!done);
+	CPRINTS("%d vect", nb);
+}
+
+void bmi160_sense_task(void)
+{
+	int i, tmp, interrupt, iter;
+	const struct motion_sensor_t *s = &motion_sensors[0];
+	do {
+		if (task_wait_event_mask(TASK_EVENT_SENSOR_PENDING, -1) ==
+				TASK_EVENT_SENSOR_PENDING) {
+			iter = 1;
+			do {
+				interrupt = 0;
+				/* order is important */
+				for (i = 0; i < 1; i++) {
+					raw_read8(s->i2c_addr,
+						  BMI160_INT_STATUS_0 + i,
+						  &tmp);
+					if (tmp)
+						raw_write8(
+							s->i2c_addr,
+							BMI160_INT_STATUS_0 + i,
+							0);
+					interrupt |= (tmp & 0xFF) << (i * 8);
+				}
+				CPRINTS("INT_STATUS: 0x%08x - %d", interrupt,
+					iter);
+				bmi160_load_fifo(s);
+				iter++;
+			} while (interrupt != 0);
+		}
+	} while (1);
 }
 #endif
 
@@ -298,7 +571,7 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 {
 	uint8_t data[6];
 	uint8_t xyz_reg;
-	int ret, tmp = 0, range = 0;
+	int ret, tmp = 0;
 
 	ret = is_data_ready(s, &tmp);
 	if (ret != EC_SUCCESS)
@@ -329,31 +602,7 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 			s->name, s->type, ret);
 		return ret;
 	}
-
-	v[0] = ((int16_t)((data[1] << 8) | data[0]));
-	v[1] = ((int16_t)((data[3] << 8) | data[2]));
-	v[2] = ((int16_t)((data[5] << 8) | data[4]));
-
-	ret = get_range(s, &range);
-	if (ret)
-		return EC_ERROR_UNKNOWN;
-
-	v[0] *= range;
-	v[1] *= range;
-	v[2] *= range;
-
-	/* normalize the accel scale: 1G = 1024 */
-	if (MOTIONSENSE_TYPE_ACCEL == s->type) {
-		v[0] >>= 5;
-		v[1] >>= 5;
-		v[2] >>= 5;
-	} else {
-		v[0] >>= 8;
-		v[1] >>= 8;
-		v[2] >>= 8;
-	}
-
-	return EC_SUCCESS;
+	return normalize(s, v, data);
 }
 
 static int init(const struct motion_sensor_t *s)
@@ -381,6 +630,12 @@ static int init(const struct motion_sensor_t *s)
 	set_data_rate(s, 100000, 0);
 
 	/* Fifo setup is done elsewhere */
+	raw_write8(s->i2c_addr, BMI160_CMD_REG, BMI160_CMD_FIFO_FLUSH);
+	msleep(30);
+
+#ifdef CONFIG_ACCEL_INTERRUPTS
+	config_interrupt(s);
+#endif
 
 	CPRINTF("[%T %s: MS Done Init type:0x%X range:%d odr:%d]\n",
 			s->name, s->type, s->range, s->odr);
