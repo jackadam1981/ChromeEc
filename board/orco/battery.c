02f90c98e5ce193ed1c2fac79d176a88c7e43790
H.J. Liao <h.j.liao@quantatw.com>
2015-04-21 13:27:38 +0800

    Orco: Add battery support
    Change the battery parameters from battery spec.
    The shipping commands for battery is same as Glimmer's battery.
    Also, change the charger's setting. (Disable LDO mode, Enable SYSOVP and
    Enable green book).
    
    BUG=chrome-os-partner:39355
    BRANCH=orco
    TEST=Build FW and check battery charge to full.
    
    Change-Id: I4120736e8888cfe873ab41b0a7430118df820681
    Signed-off-by: H.J. Liao <h.j.liao@quantatw.com>

--- board/orco/battery.c
+++ board/orco/battery.c
@@ -7,24 +7,41 @@
 
 #include "battery.h"
 #include "battery_smart.h"
+#include "charger.h"
+#include "charge_state.h"
 #include "console.h"
+#include "driver/charger/bq24715.h"
+#include "extpower.h"
 #include "gpio.h"
+#include "hooks.h"
 #include "host_command.h"
+#include "system.h"
 #include "util.h"
 
-/* Shutdown mode parameter to write to manufacturer access register */
-#define SB_SHUTDOWN_DATA	0x0010
+/* FET ON/OFF cammand write to fet off register */
+#define	SB_FET_OFF	0x34
+#define	SB_FETOFF_DATA1	0x0000
+#define	SB_FETOFF_DATA2	0x1000
+#define	SB_FETON_DATA1	0x2000
+#define	SB_FETON_DATA2	0x4000
+#define	BATTERY_FETOFF	0x0100
+
+/*
+ * Green book support parameter
+ * Enable this will make battery meet JEITA standard
+ */
+#define GREEN_BOOK_SUPPORT	(1 << 2)
 
 static const struct battery_info info = {
-	.voltage_max    = 12600,		/* mV */
-	.voltage_normal = 11100,
-	.voltage_min    = 9000,
-	.precharge_current  = 256,	/* mA */
+	.voltage_max    = 8600,		/* mV */
+	.voltage_normal = 7600,
+	.voltage_min    = 6000,
+	.precharge_current  = 230,	/* mA */
 	.start_charging_min_c = 0,
-	.start_charging_max_c = 45,
+	.start_charging_max_c = 60,
 	.charging_min_c       = 0,
-	.charging_max_c       = 45,
-	.discharging_min_c    = 0,
+	.charging_max_c       = 60,
+	.discharging_min_c    = -20,
 	.discharging_max_c    = 60,
 };
 
@@ -33,17 +50,46 @@ const struct battery_info *battery_get_info(void)
 	return &info;
 }
 
+static void wakeup_deferred(void)
+{
+	int d;
+	int mode;
+
+	/* Add Green Book support */
+	sb_read(SB_BATTERY_MODE, &mode);
+	mode |= GREEN_BOOK_SUPPORT;
+	sb_write(SB_BATTERY_MODE, mode);
+
+	sb_read(SB_FET_OFF, &d);
+
+	if (extpower_is_present() && (BATTERY_FETOFF == d)) {
+		sb_write(SB_FET_OFF, SB_FETON_DATA1);
+		sb_write(SB_FET_OFF, SB_FETON_DATA2);
+	}
+}
+DECLARE_DEFERRED(wakeup_deferred);
+
+static void wakeup(void)
+{
+	/*
+	 *The deferred call ensures that wakeup_deferred is called from a
+	 *task. This is required to talk to the battery over I2C.
+	 */
+	hook_call_deferred(wakeup_deferred, 0);
+}
+DECLARE_HOOK(HOOK_INIT, wakeup, HOOK_PRIO_DEFAULT);
+
 static int cutoff(void)
 {
 	int rv;
 
 	/* Ship mode command must be sent twice to take effect */
-	rv = sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+	rv = sb_write(SB_FET_OFF, SB_FETOFF_DATA1);
 
 	if (rv != EC_SUCCESS)
 		return rv;
 
-	return sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+	return sb_write(SB_FET_OFF, SB_FETOFF_DATA2);
 }
 
 static int battery_command_cut_off(struct host_cmd_handler_args *args)
@@ -61,3 +107,23 @@ DECLARE_CONSOLE_COMMAND(battcutoff, command_battcutoff,
 			NULL,
 			"Enable battery cutoff (ship mode)",
 			NULL);
+/*
+ * Initialize charger additional option value
+ */
+static void charger_init(void)
+{
+	if ((charge_get_state() == PWR_STATE_INIT) ||
+	   (charge_get_state() == PWR_STATE_REINIT)) {
+		int option;
+
+		charger_get_option(&option);
+
+		/* Disable LDO mode */
+		option &= ~OPT_LDO_MODE_MASK;
+		/* Enable SYSOVP */
+		option |= OPT_SYSOVP_MASK;
+
+		charger_set_option(option);
+	}
+}
+DECLARE_HOOK(HOOK_CHARGE_STATE_CHANGE, charger_init, HOOK_PRIO_DEFAULT);
