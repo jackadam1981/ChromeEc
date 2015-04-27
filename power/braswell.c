6a27a9c3873599edda8f809c1dbff16b2375bfd2
Hsu Henry <Henry.Hsu@quantatw.com>
2015-04-27 19:36:10 +0800

    Braswell: Turn on/off the USB power while S5->S3/S3->S5.
    The USB power is off in S5 with previous ChromeBook.
    The braswell platfrom should be the same as before.
    
    BUG=chrome-os-partner:39507
    BRANCH=cyan
    TEST=The usb power is off in G3/S5 and is on in S3/S0 by ec console.
    
    Change-Id: I719f213a9eb0180f7e95e4c2717c038c79ef56fe
    Signed-off-by: Henry Hsu <Henry.Hsu@quantatw.com>

--- power/braswell.c
+++ power/braswell.c
@@ -65,6 +65,10 @@ void chipset_force_shutdown(void)
 	 */
 	gpio_set_level(GPIO_PCH_SYS_PWROK, 0);
 	gpio_set_level(GPIO_PCH_RSMRST_L, 0);
+
+	/* Froce off the USB power. */
+	gpio_set_level(GPIO_USB2_PWR_EN, 0);
+	gpio_set_level(GPIO_USB3_PWR_EN, 0);
 }
 
 void chipset_reset(int cold_reset)
@@ -157,6 +161,9 @@ enum power_state power_handle_state(enum power_state state)
 		break;
 
 	case POWER_S5S3:
+		/* Turn on the USB power */
+		gpio_set_level(GPIO_USB2_PWR_EN, 1);
+		gpio_set_level(GPIO_USB3_PWR_EN, 1);
 
 		/* Call hooks now that rails are up */
 		hook_notify(HOOK_CHIPSET_STARTUP);
@@ -278,6 +285,10 @@ enum power_state power_handle_state(enum power_state state)
 		return POWER_S3;
 
 	case POWER_S3S5:
+		/* Turn off the USB power */
+		gpio_set_level(GPIO_USB2_PWR_EN, 0);
+		gpio_set_level(GPIO_USB3_PWR_EN, 0);
+
 		/* Call hooks before we remove power rails */
 		hook_notify(HOOK_CHIPSET_SHUTDOWN);
 
