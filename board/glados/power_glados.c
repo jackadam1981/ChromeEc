cd29a5b010139ea61930b533cf4f63b7f52949ae
Shawn Nematbakhsh <shawnn@chromium.org>
2015-05-01 10:45:48 -0700

    glados: Initial mainboard commit
    WIP - don't review
    
    Change-Id: If470b00fec56db0884dbd4c9974140951fc214fd

---
+++ board/glados/power_glados.c
@@ -0,0 +1,19 @@
+#include "power.h"
+
+void chipset_reset(int cold_reset)
+{
+}
+
+void chipset_force_shutdown(void)
+{
+}
+
+enum power_state power_chipset_init(void)
+{
+	return 0;
+}
+
+enum power_state power_handle_state(enum power_state state)
+{
+	return 0;
+}
