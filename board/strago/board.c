5c8f0119fb37f8b968675a165bfb9623bf3d7874
Vijay Hiremath <vijay.p.hiremath@intel.com>
2015-05-05 09:36:58 -0700

    Strago: Enable L3GD20H gyro module
    BUG=None
    BRANCH=None
    TEST=Read gyro values from accelinfo console command.
    
    Change-Id: Ib71581fe93ecdfe0ed17219fffcbf5a6c0f75dfb
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- board/strago/board.c
+++ board/strago/board.c
@@ -9,6 +9,7 @@
 #include "charge_state.h"
 #include "driver/accel_kxcj9.h"
 #include "driver/als_isl29035.h"
+#include "driver/gyro_l3gd20h.h"
 #include "driver/temp_sensor/tmp432.h"
 #include "extpower.h"
 #include "gpio.h"
@@ -109,6 +110,11 @@ int board_discharge_on_ac(int enable)
 static struct mutex g_kxcj9_mutex[2];
 struct kxcj9_data g_kxcj9_data[2];
 
+/* Gyro sensor */
+/* l3gd20h mutex and local/private data*/
+static struct mutex g_l3gd20h_mutex;
+struct l3gd20_data g_l3gd20h_data;
+
 /* Matrix to rotate accelrator into standard reference frame */
 const matrix_3x3_t base_standard_ref = {
 	{ 0,  FLOAT_TO_FP(1),  0},
@@ -151,6 +157,21 @@ struct motion_sensor_t motion_sensors[] = {
 	 .default_config = {
 		 .odr = 100000,
 		 .range = 2
+	 }
+	},
+	{.name = "Lid Gyro",
+	 .active_mask = SENSOR_ACTIVE_S0,
+	 .chip = MOTIONSENSE_CHIP_L3GD20H,
+	 .type = MOTIONSENSE_TYPE_GYRO,
+	 .location = MOTIONSENSE_LOC_LID,
+	 .drv = &l3gd20h_drv,
+	 .mutex = &g_l3gd20h_mutex,
+	 .drv_data = &g_l3gd20h_data,
+	 .i2c_addr = L3GD20_ADDR1,
+	 .rot_standard_ref = NULL,
+	 .default_config = {
+		 .odr = 190000,
+		 .range = 2000
 	 }
 	},
 };
