db28cfbbc94c7c33291849b0a24ce80343aef2a9
Alec Berg <alecaberg@chromium.org>
2015-05-11 17:31:16 -0700

    pd: move non-phy layer config out of usb_pd_config.h
    Move parts of usb_pd_config.h that are not part of the phy layer
    out of usb_pd_config.h and into board.h. This cleans up the
    division between the TCPC and TCPM as only the TCPC needs to
    use usb_pd_config.h.
    
    BUG=none
    BRANCH=none
    TEST=make -j buildall
    
    Change-Id: I946cceb38bea8233095b8a4b287102bb8a3a296d
    Signed-off-by: Alec Berg <alecaberg@chromium.org>

--- board/plankton/board.h
+++ board/plankton/board.h
@@ -71,6 +71,34 @@ enum board_src_cap {
 	SRC_CAP_20V,
 };
 
+/* USB PD board configuration */
+/* USB-PD configuration */
+#define PD_PORT_COUNT 1
+#define PORT_TO_TASK_ID(port) TASK_ID_PD
+#define TASK_ID_TO_PORT(id)   0
+
+/* 3.0A DFP : no-connect voltage is 2.45V */
+#define PD_SRC_VNC 2450 /* mV */
+
+/* UFP-side : threshold for DFP connection detection */
+#define PD_SNK_VA   250 /* mV */
+
+/* we are acting only as a sink */
+#define PD_DEFAULT_STATE PD_STATE_SNK_DISCONNECTED
+
+/* delay necessary for the voltage transition on the power supply */
+#define PD_POWER_SUPPLY_TURN_ON_DELAY  50000 /* us */
+#define PD_POWER_SUPPLY_TURN_OFF_DELAY 50000 /* us */
+
+/* Define typical operating power and max power */
+#define PD_OPERATING_POWER_MW 5000
+#define PD_MAX_POWER_MW       60000
+#define PD_MAX_CURRENT_MA     3000
+#define PD_MAX_VOLTAGE_MV     20000
+
+/* Return if VBUS is detected on type-C port */
+int pd_snk_is_vbus_provided(int port);
+
 /* Set USB PD source capability */
 void board_set_source_cap(enum board_src_cap cap);
 
