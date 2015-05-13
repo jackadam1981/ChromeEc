9f28ea4fdc2592092f94782c36d2d3ab300b71ff
Anton Staaf <robotboy@chromium.org>
2015-05-13 13:26:12 -0700

    Producer/Consumer: Refactor to use Queue policies
    Previously the Producer and Consumer interfaces tracked the
    Consumer and Producer respectively at the other end of the
    queue that they interacted with.  This was done to avoid
    modifying the queue implementation, but resulted in a rougher
    interface that required additional initialization steps and
    prevented alternative configurations; many producers and one
    consumer for example.
    
    This commit uses the new queue policies to track this
    information.  The new direct policy behaves as the old producer
    and consumers did.  Now the producers and consumers are just
    named references to the queue that they work on and a convenient
    location for a notification callback when the queue is updated in
    a way that is relevent to the producer or consumer.
    
    All users of Producer and Consumer have been updated including the
    stream adaptors which are in use by the echo test code and the
    mcdp28x0 driver.  Use of the stream adaptors has also been
    simplified.
    
    Signed-off-by: Anton Staaf <robotboy@chromium.org>
    
    BRANCH=None
    BUG=None
    TEST=make buildall -j
         Manual testing of Ryu (P5) and discovery board echo task
    
    Change-Id: I704be6378a31b4e20f5063295eff9943e4900409

---
+++ common/queue_policies.c
@@ -0,0 +1,33 @@
+/* Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Queue policies.
+ */
+#include "queue_policies.h"
+#include "util.h"
+
+#include <stddef.h>
+
+static void queue_add_direct(struct queue_policy const *policy, size_t count)
+{
+	struct queue_policy_direct const *direct =
+		DOWNCAST(policy, struct queue_policy_direct, policy);
+
+	if (count && direct->consumer->ops->written)
+		direct->consumer->ops->written(direct->consumer, count);
+}
+
+static void queue_remove_direct(struct queue_policy const *policy, size_t count)
+{
+	struct queue_policy_direct const *direct =
+		DOWNCAST(policy, struct queue_policy_direct, policy);
+
+	if (count && direct->producer->ops->read)
+		direct->producer->ops->read(direct->producer, count);
+}
+
+struct queue_policy_ops const queue_policy_direct_ops = {
+	.add    = queue_add_direct,
+	.remove = queue_remove_direct,
+};
