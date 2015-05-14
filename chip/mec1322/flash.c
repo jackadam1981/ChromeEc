c44cca0084986384edf82b2cd436be909aec3150
Divya Jyothi <divya.jyothi@intel.com>
2015-05-14 10:11:13 -0700

    mec1322: Added support for sysjump.
    changes added to support flashrom are:
    sysjump support to be able to copy the RO/RW image
    and jump to it without causing AP to reboot while
    its alreday ON.
    
    LPC init should be reinitialized on sysjump
    
    corrected gpio_set_flags_by_mask to make sure we
    update the register only for GPIO_LOW condition and not
    all else conditions.
    
    BUG=chrome-os-partner:38103
    TEST=commands : flashrom -p ec -w ec.bin
                    flashrom -p ec -r ec.bin
    BRANCH=none
    
    Change-Id: I23892f0378d756052030e73034c3acdd41477e34
    Signed-off-by: Shamile Khan <shamile.khan@intel.com>
    Signed-off-by: Divya Jyothi <divya.jyothi@intel.com>

--- chip/mec1322/flash.c
+++ chip/mec1322/flash.c
@@ -12,7 +12,6 @@
 #include "spi_flash.h"
 #include "system.h"
 #include "util.h"
-#include "watchdog.h"
 
 #define PAGE_SIZE 256
 
@@ -25,7 +24,7 @@
  */
 int flash_physical_read(int offset, int size, char *data)
 {
-	int ret;
+	int ret, i, read_size;
 
 	offset += CONFIG_FLASH_BASE_SPI;
 
@@ -34,8 +33,16 @@ int flash_physical_read(int offset, int size, char *data)
 		return EC_ERROR_INVAL;
 
 	spi_enable(1);
-	ret = spi_flash_read((uint8_t *)data, offset, size);
+	for (i = 0; i < size; i += read_size) {
+		read_size = MIN((size - i), SPI_FLASH_MAX_READ_SIZE);
+		ret = spi_flash_read((uint8_t *)(data + i),
+					offset + i,
+					read_size);
+		if (ret != EC_SUCCESS)
+			break;
+	}
 	spi_enable(0);
+
 	return ret;
 }
 
@@ -60,7 +67,7 @@ int flash_physical_write(int offset, int size, const char *data)
 
 	spi_enable(1);
 	for (i = 0; i < size; i += write_size) {
-		write_size = MIN(size, SPI_FLASH_MAX_WRITE_SIZE);
+		write_size = MIN((size - i), SPI_FLASH_MAX_WRITE_SIZE);
 		ret = spi_flash_write(offset + i,
 				      write_size,
 				      (uint8_t *)data + i);
@@ -100,6 +107,13 @@ int flash_physical_erase(int offset, int size)
  */
 int flash_physical_get_protect(int bank)
 {
+#ifdef CONFIG_WP_ENABLE
+/* TODO:crosbug/p/40908  . This section was causing SPI to lock up
+ * while installing via firmware updater. Disabled it temporarily.
+ * will get re-enabled after Flash protection is tested under all
+ * scenarios
+ */
+
 	uint32_t addr = bank * CONFIG_FLASH_BANK_SIZE;
 	int ret;
 
@@ -107,6 +121,9 @@ int flash_physical_get_protect(int bank)
 	ret = spi_flash_check_protect(addr, CONFIG_FLASH_BANK_SIZE);
 	spi_enable(0);
 	return ret;
+#else
+	return 0;
+#endif
 }
 
 /**
@@ -117,6 +134,13 @@ int flash_physical_get_protect(int bank)
  */
 int flash_physical_protect_now(int all)
 {
+#ifdef CONFIG_WP_ENABLE
+/* TODO:crosbug/p/40908  . This section was causing SPI to lock up
+ * while installing via firmware updater. Disabled it temporarily.
+ * will get re-enabled after Flash protection is tested under all
+ * scenarios
+ */
+
 	int offset, size, ret;
 
 	if (all) {
@@ -131,6 +155,9 @@ int flash_physical_protect_now(int all)
 	ret = spi_flash_set_protect(offset, size);
 	spi_enable(0);
 	return ret;
+#else
+	return 0;
+#endif
 }
 
 /**
@@ -144,6 +171,13 @@ uint32_t flash_physical_get_protect_flags(void)
 {
 	uint32_t flags = 0;
 
+#ifdef CONFIG_WP_ENABLE
+/* TODO:crosbug/p/40908  . This section was causing SPI to lock up
+ * while installing via firmware updater. Disabled it temporarily.
+ * will get re-enabled after Flash protection is tested under all
+ * scenarios
+ */
+
 	spi_enable(1);
 	if (spi_flash_check_protect(CONFIG_RO_STORAGE_OFF, CONFIG_RO_SIZE)) {
 		flags |= EC_FLASH_PROTECT_RO_AT_BOOT | EC_FLASH_PROTECT_RO_NOW;
@@ -152,6 +186,7 @@ uint32_t flash_physical_get_protect_flags(void)
 			flags |= EC_FLASH_PROTECT_ALL_NOW;
 	}
 	spi_enable(0);
+#endif
 	return flags;
 }
 
@@ -176,13 +211,21 @@ uint32_t flash_physical_get_valid_flags(void)
 uint32_t flash_physical_get_writable_flags(uint32_t cur_flags)
 {
 	uint32_t ret = 0;
+
+#ifdef CONFIG_WP_ENABLE
+/* TODO:crosbug/p/40908  . This section was causing SPI to lock up
+ * while installing via firmware updater. Disabled it temporarily.
+ * will get re-enabled after Flash protection is tested under all
+ * scenarios
+ */
+
 	enum spi_flash_wp wp_status = spi_flash_check_wp();
 
 	if (wp_status == SPI_WP_NONE || (wp_status == SPI_WP_HARDWARE &&
 	   !(cur_flags & EC_FLASH_PROTECT_GPIO_ASSERTED)))
 		ret = EC_FLASH_PROTECT_RO_AT_BOOT | EC_FLASH_PROTECT_RO_NOW |
 		      EC_FLASH_PROTECT_ALL_NOW;
-
+#endif
 	return ret;
 }
 
@@ -199,6 +242,13 @@ uint32_t flash_physical_get_writable_flags(uint32_t cur_flags)
  */
 int flash_physical_protect_at_boot(enum flash_wp_range range)
 {
+#ifdef CONFIG_WP_ENABLE
+/* TODO:crosbug/p/40908  . This section was causing SPI to lock up
+ * while installing via firmware updater. Disabled it temporarily.
+ * will get re-enabled after Flash protection is tested under all
+ * scenarios
+ */
+
 	int offset, size, ret;
 
 	switch (range) {
@@ -219,6 +269,9 @@ int flash_physical_protect_at_boot(enum flash_wp_range range)
 	ret = spi_flash_set_protect(offset, size);
 	spi_enable(0);
 	return ret;
+#else
+	return 0;
+#endif
 }
 
 /**
