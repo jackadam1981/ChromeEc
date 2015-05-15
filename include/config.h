27403ef714e5a65636b58be5fd6eddc3918da9e2
Gwendal Grignou <gwendal@chromium.org>
2015-05-15 13:07:26 -0700

    driver: Add BMM150 behind BMI160 support.
    Add support for Bosh Sensortec BMM160 compass.
    We access it through BMI150.
    
    BRANCH=none
    BUG=chrome-os-partner:39900
    TEST=Test on a nucleo board and smaug.
    
    Change-Id: I5b959cab4f9341ba0fcd3ed9bad815fa92f80a37
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- include/config.h
+++ include/config.h
@@ -42,6 +42,9 @@
 /* Specify type of accelerometers attached. */
 #undef CONFIG_ACCEL_KXCJ9
 #undef CONFIG_ACCELGYRO_LSM6DS0
+#undef CONFIG_ACCELGYRO_BMI160
+
+#undef CONFIG_MAG_BMI160_BMM150
 
 /* Compile chip support for analog-to-digital convertor */
 #undef CONFIG_ADC
