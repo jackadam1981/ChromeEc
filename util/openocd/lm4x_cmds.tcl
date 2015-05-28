bbc0c996b48ba6aa5c72bf271544d8b92f5fb792
Myles Watson <mylesgw@chromium.org>
2015-05-28 13:12:43 -0700

    flash_ec: remove redundant code from lm4x_cmds.tcl
    All boards in ToT have the same RO configuration.
    Boards which define CONFIG_FLASH_PSTATE_BANK have pstate following RO.
    
    BRANCH=none
    BUG=chrome-os-partner:22990
    TEST="git grep" to see that none of the affected functions are called.
    
    Change-Id: Ie1eb9a726e1fa157852b0c55d474c9b4587c41f0
    Signed-off-by: Myles Watson <mylesgw@chromium.org>

--- util/openocd/lm4x_cmds.tcl
+++ util/openocd/lm4x_cmds.tcl
@@ -7,90 +7,25 @@
 # Program internal flash
 
 proc flash_lm4 {path offset} {
-	#set firstsect [expr {$offset / 1024}];
-	#set lastsect [expr {($offset + $size) / 1024 - 1}];
 	reset halt;
 	flash write_image erase $path $offset;
 	reset
 }
 
-proc flash_link { } {
-	flash_lm4 ../../../build/link/ec.bin 0
+proc flash_lm4_board {board} {
+	flash_lm4 ../../../build/$board/ec.bin 0
 }
 
-proc flash_link_ro { } {
-	flash_lm4 ../../../build/link/ec.RO.flat 0
+proc flash_lm4_ro {board} {
+	flash_lm4 ../../../build/$board/ec.RO.flat 0
 }
 
-# Link originally had 80KB images, but TOT builds 128KB images
-proc flash_link_rw { } {
-	flash_lm4 ../../../build/link/ec.RW.bin 131072
+proc flash_lm4_rw {board} {
+	flash_lm4 ../../../build/$board/ec.RW.bin 131072
 }
 
-proc flash_auron { } {
-	flash_lm4 ../../../build/auron/ec.bin 0
-}
-
-proc flash_bds { } {
-	flash_lm4 ../../../build/bds/ec.bin 0
-}
-
-proc flash_rambi { } {
-	flash_lm4 ../../../build/rambi/ec.bin 0
-}
-
-proc flash_samus { } {
-	flash_lm4 ../../../build/samus/ec.bin 0
-}
-
-proc flash_samus_ro { } {
-	flash_lm4 ../../../build/samus/ec.RO.flat 0
-}
-
-proc flash_samus_rw { } {
-	flash_lm4 ../../../build/samus/ec.RW.bin 131072
-}
-
-proc flash_squawks { } {
-	flash_lm4 ../../../build/squawks/ec.bin 0
-}
-
-proc flash_squawks_ro { } {
-	flash_lm4 ../../../build/squawks/ec.RO.flat 0
-}
-
-proc flash_falco { } {
-	flash_lm4 ../../../build/falco/ec.bin 0
-}
-
-proc flash_peppy { } {
-	flash_lm4 ../../../build/peppy/ec.bin 0
-}
-
-proc flash_rambi_ro { } {
-	flash_lm4 ../../../build/rambi/ec.RO.flat 0
-}
-
-proc flash_rambi_rw { } {
-	flash_lm4 ../../../build/rambi/ec.RW.bin 131072
-}
-
-# link has pstate in last sector
-proc unprotect_link { } {
-	reset halt
-	flash erase_sector 0 254 255
-	reset
-}
-
-# Peppy/falco have pstate following RO
-proc unprotect_peppy { } {
-	reset halt
-	flash erase_sector 0 126 127
-	reset
-}
-
-# Auron have pstate following RO
-proc unprotect_auron { } {
+# Boards with CONFIG_FLASH_PSTATE_BANK have pstate following RO
+proc unprotect_pstate { } {
 	reset halt
 	flash erase_sector 0 126 127
 	reset
@@ -105,16 +40,12 @@ proc ramboot_lm4 {path} {
 	resume
 }
 
-proc ramboot_link { } {
-	ramboot_lm4 ../../../build/link/ec.RO.flat
+proc ramboot_lm4_board {board} {
+	ramboot_lm4 ../../../build/$board/ec.RO.flat
 }
 
-proc ramboot_bds { } {
-	ramboot_lm4 ../../../build/bds/ec.RO.flat
-}
-
-proc flash_emerged_link { } {
-	set firmware_image ../../../../../../chroot/build/link/firmware/ec.bin
+proc flash_emerged_board { } {
+	set firmware_image ../../../../../../chroot/build/$board/firmware/ec.bin
 
 	flash_lm4 $firmware_image 0
 }
