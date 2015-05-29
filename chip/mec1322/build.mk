d1e1be72338c2a7a31cdaa656943371c0fb08fae
Aseda Aboagye <aaboagye@google.com>
2015-05-29 11:33:27 -0700

    build-infr: Add support for shared RO library.
    This commit introduces the build infrastructure changes needed for
    creating a shared RO library. (libsharedobjs).  The end goal is for the
    library to contain various objects that can be shared with both the RO
    and RW EC images.
    
    Now, there are 3 make goals: ro, rw, and libsharedobjs.
    
    In order for changes that are only specific to a single image (ie: RW
    only) to be applied correctly, the object files are now built separately
    for the RO and RW targets.
    
    Additionally, there are now 2 separate linker scripts for building RO
    and RW.  Currently, the RW linker script contains the strings FIXUP_ADDR
    and FIXUP_LENGTH.  The shared_lib_fixup python script will "fixup" the
    RW linker script with where the shared objects library will reside in
    the RO section along with the size.
    
    NOTE: Certain EC targets are incompatible with this model due to the
    fact that only one image is present within flash at a time.
    
    BRANCH=none
    BUG=None
    TEST=make -j buildall tests
    TEST=make -j BOARD=cr50 xrefs
    TEST=make BOARD=samus dis
    TEST=Built samus EC image and compared that the final EC image was
    identical to the upstream version (except for the git SHAs & version
    strings).
    
    Change-Id: I8e67f089710be9c6d7017718109262394bdad2f5
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- chip/mec1322/build.mk
+++ chip/mec1322/build.mk
@@ -11,6 +11,9 @@ CORE:=cortex-m
 # Allow the full Cortex-M4 instruction set
 CFLAGS_CPU+=-march=armv7e-m -mcpu=cortex-m4
 
+# Is NOT compatible with current shared objects library model.
+skip_lib:=yes
+
 # Required chip modules
 chip-y=clock.o gpio.o hwtimer.o system.o uart.o jtag.o
 chip-$(CONFIG_ADC)+=adc.o
@@ -39,27 +42,28 @@ cmd_obj_to_bin = $(OBJCOPY) --gap-fill=0xff -O binary $< $@.tmp1 ; \
 		--spi_size ${CHIP_SPI_SIZE_KB} ; rm -f $@.tmp1
 
 mec1322-lfw = chip/mec1322/lfw/ec_lfw
-mec1322-lfw-flat = $(out)/$(mec1322-lfw)-lfw.flat
+mec1322-lfw-flat = $(out)/RW/$(mec1322-lfw)-lfw.flat
 
 # build these specifically for lfw with -lfw suffix
-objs_lfw = $(patsubst %, $(out)/%-lfw.o, \
+objs_lfw = $(patsubst %, $(out)/RW/%-lfw.o, \
 		$(addprefix common/, util gpio) \
 		$(addprefix chip/$(CHIP)/, spi dma gpio clock hwtimer) \
 		core/$(CORE)/cpu $(mec1322-lfw))
 
 # reuse version.o (and its dependencies) from main board
-objs_lfw += $(out)/common/version.o
+objs_lfw += $(out)/RW/common/version.o
 
 dirs-y+=chip/$(CHIP)/lfw
 
 # objs with -lfw suffix are to include lfw's gpio
-$(out)/%-lfw.o: private CC+=-Iboard/$(BOARD)/lfw
-$(out)/%-lfw.o: %.c
+$(out)/RW/%-lfw.o: private CC+=-Iboard/$(BOARD)/lfw
+$(out)/RW/%-lfw.o: %.c
 	$(call quiet,c_to_o,CC     )
 
 # let lfw's elf link only with selected objects
-$(out)/%-lfw.elf: private objs = $(objs_lfw)
-$(out)/%-lfw.elf: %.ld $(objs_lfw)
+$(out)/RW/%-lfw.elf: private objs = $(objs_lfw)
+$(out)/RW/%-lfw.elf: override shlib :=
+$(out)/RW/%-lfw.elf: %.ld $(objs_lfw)
 	$(call quiet,elf,LD     )
 
 # final image needs lfw loader
