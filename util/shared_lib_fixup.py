73b30ee6d67770bb970473c85dd40d073b644b89
Aseda Aboagye <aaboagye@google.com>
2015-05-29 11:33:27 -0700

    build-infr: Add support for shared RO library.
    This commit introduces the build infrastructure changes needed for
    creating a shared RO library. (libsharedobjs).  The end goal is for the
    library to contain various objects that can be shared with both the RO
    and RW EC images.
    
    Now, there are 3 make goals: ro, rw, and libsharedobjs.
    
    In order for changes that are only specific to a single image (ie: RW
    only) to be applied correctly, the object files are now built separately
    for the RO and RW targets.
    
    Additionally, there are now 2 separate linker scripts for building RO
    and RW.  Currently, the RW linker script contains the strings FIXUP_ADDR
    and FIXUP_LENGTH.  The shared_lib_fixup python script will "fixup" the
    RW linker script with where the shared objects library will reside in
    the RO section along with the size.
    
    NOTE: Certain EC targets are incompatible with this model due to the
    fact that only one image is present within flash at a time.
    
    BRANCH=none
    BUG=None
    TEST=make -j buildall tests
    TEST=make -j BOARD=cr50 xrefs
    TEST=make BOARD=samus dis
    TEST=Built samus EC image and compared that the final EC image was
    identical to the upstream version (except for the git SHAs & version
    strings).
    
    Change-Id: I8e67f089710be9c6d7017718109262394bdad2f5
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

---
+++ util/shared_lib_fixup.py
@@ -0,0 +1,89 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+#!/usr/bin/python
+"""Script to fixup the RW linker script with the address and length of the
+shared objects library present in RO.
+"""
+import os
+import re
+
+if len(os.sys.argv) < 4:
+    print "Usage: %s outdir project(stem match) skip_lib V\n" % os.sys.argv[0]
+    quit()
+
+OUTDIR = os.sys.argv[1]
+STEM = os.sys.argv[2]
+SKIP_LIB = os.sys.argv[3]
+if len(os.sys.argv) >= 5:
+    VERBOSE = int(os.sys.argv[4])
+else:
+    VERBOSE = 0
+
+if VERBOSE == 1:
+    print os.sys.argv
+
+DIR = os.path.join(OUTDIR, os.path.dirname(STEM))
+BASENAME = os.path.basename(STEM)
+PROJECT = BASENAME.split('.')[0]
+
+if VERBOSE == 1:
+    print ' outdir: %s\n stem: %s\n skip_lib: %s' % (OUTDIR,STEM,SKIP_LIB)
+    print ' dir: %s\n basename: %s\n proj: %s' % (DIR,BASENAME,PROJECT)
+
+rw_linker_script = open(os.path.join(DIR, BASENAME + '.lds'), 'r')
+tmp_out_file = open(os.path.join(DIR, BASENAME + '.lds.tmp'), 'w')
+
+if VERBOSE == 1:
+    print 'Fixing up %s...' % rw_linker_script.name
+
+shared_lib_is_empty = True
+
+if PROJECT == "ec" and SKIP_LIB == "no":
+    # Read the map file and determine the address of the .roshared section
+    ro_map_file = open(os.path.join(OUTDIR, 'RO', PROJECT + '.RO.map'),'r')
+    for line in ro_map_file:
+        if '.roshared' in line:
+            # Read in the address and length from the map file.
+            data = re.split('\s+', line)
+            # Check to see if there's anything in the .roshared section
+            if len(data) < 3:
+                break
+            else:
+                shared_lib_is_empty = False
+                load_address = data[1]
+                length = data[2]
+                if VERBOSE == 1:
+                    print ' .roshared section is NOT empty.'
+                    print '   addr=%08x len=%04x' % (load_address, length)
+                break
+    ro_map_file.close()
+
+if shared_lib_is_empty:
+    load_address = "0x00000000"
+    length = "0x00000000"
+    if VERBOSE == 1:
+        print ' .roshared section is empty. Setting address and' + \
+            ' length to 0.'
+
+for line in rw_linker_script:
+    if 'SHARED_LIB (rx)' in line:
+        # Fixup the address and length for the shared lib
+        line = re.sub('FIXUP_ADDR', load_address, line, flags=re.M)
+        if VERBOSE != 1:
+            print('  FIXUP   %s' % STEM + '.lds')
+        tmp_out_file.write(re.sub('FIXUP_LENGTH', length, line, flags=re.M))
+        if shared_lib_is_empty != True:
+            print('The shared object library (size: %s bytes) is present at %s' % (length, load_address))
+        continue
+    tmp_out_file.write(line)
+
+rw_linker_script.close()
+tmp_out_file.close()
+
+# Now, save the fixed up version of the linker script.
+try:
+    os.rename(tmp_out_file.name, rw_linker_script.name)
+except OSError:
+    print "%s does not exist!" % tmp_out_file.name
