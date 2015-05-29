39186205e37f60765c5907aef8644fb41e83e883
Aseda Aboagye <aaboagye@google.com>
2015-05-29 13:25:38 -0700

    libsharedobjs: Create config option for sharedlib.
    This commit introduces the config option to enable the creation of the
    shared objects library for a particular board.  To use, simply define it
    in board.h.
    
       #define CONFIG_SHAREDLIB
    
    In order to actually add objects to the shared library, one must utilize
    the SHAREDLIB() macro around the definition of the object.  All of the
    objects in the shared library will be placed into the '.roshared'
    section.  The SHAREDLIB() macro simply adds this attribute and prevents
    the RW image from compiling them in.
    
    For example,
    
       # In foo/build.mk
       foo-$(CONFIG_BAR)+=bar_shared.o
    
       # In foo/bar_shared.c
       #include "libsharedobjs.h"
       SHAREDLIB(const uint8_t shared_var = 0x43);
    
       # In include/bar_shared.h
       extern const uint8_t shared_var;
    
    BUG=none
    BRANCH=none
    TEST=make -j buildall tests
    TEST=Enabled config option and saw the sharedlib being created.
    TEST=Enabled config option on unsupported board and saw build fail.
    CQ-DEPEND=CL:274079
    
    Change-Id: I2abeb1be248ab161fa81c897d2f5793f5a599456
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

---
+++ include/libsharedobjs.h
@@ -0,0 +1,38 @@
+/* Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Helper macros for shared objects library.
+ */
+#ifndef __CROS_EC_LIBSHAREDOBJS_H
+#define __CROS_EC_LIBSHAREDOBJS_H
+
+#include "common.h"
+
+#ifdef CONFIG_SHAREDLIB
+/*
+ * The shared library currently only works with those platforms in which both
+ * the RO and RW images are loaded simultaneously in some executable memory.
+ */
+#if (CONFIG_RO_MEM_OFF + CONFIG_FW_IMAGE_SIZE) > CONFIG_RW_MEM_OFF
+#error "The shared library is NOT compatible with this EC."
+#endif
+
+/*
+ * All of the objects in the shared library will be placed into the '.roshared'
+ * section.  The SHAREDLIB() macro simply adds this attribute and prevents the
+ * RW image from compiling them in.
+ */
+#undef SHAREDLIB
+#ifdef RO_IMAGE
+#define SHAREDLIB(...) __attribute__ ((section(".roshared"))) __VA_ARGS__
+#else /* !defined(RO_IMAGE) */
+#define SHAREDLIB(...)
+#endif /* defined(RO_IMAGE) */
+
+#else /* !defined(CONFIG_SHAREDLIB) */
+
+/* By default, the SHAREDLIB() macro maps to its contents. */
+#define SHAREDLIB(...) __VA_ARGS__
+#endif /* defined(CONFIG_SHAREDLIB) */
+#endif /* __CROS_EC_LIBSHAREDOBJS_H */
