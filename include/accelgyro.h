3ea8a322c16ca269d2c67181d5abea9621172516
Gwendal Grignou <gwendal@chromium.org>
2015-05-30 11:52:28 -0700

    driver: bmi160: Add FIFO and interrupt support
    Add FIFO support, where bmi160 hardware FIFO is copied in local fifo.
    Add rudimentary support for single/double tap and lift detection.
    
    BUG=chrome-os-partner:39900
    BRANCH=none
    TEST=Check on F411 that FIFO data is retrieved and correct.
    Check on Smaug as well.
    
    Change-Id: Ic2317c27fad0ef31dacd6e18cd5f71ccd2cec807
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- include/accelgyro.h
+++ include/accelgyro.h
@@ -87,6 +87,21 @@ struct accelgyro_drv {
 	 */
 	int (*set_interrupt)(const struct motion_sensor_t *s,
 			     unsigned int threshold);
+
+	/**
+	 * handler for interrupts triggered by the sensor: it runs in task and
+	 * process the events that triggered an interrupt.
+	 * @s Pointer to sensor data.
+	 */
+	int (*irq_handler)(const struct motion_sensor_t *s);
+#endif
+#ifdef CONFIG_ACCEL_FIFO
+	/**
+	 * Retrieve hardware FIFO from sensor, and put data in Sensor Hub fifo.
+	 * We put raw data in hub fifo and process data from theres.
+	 * @s Pointer to sensor data.
+	 */
+	int (*load_fifo)(const struct motion_sensor_t *s);
 #endif
 };
 
