c4b6cc8256fac2f6258e72f0b958d66c6f94864c
Kyoung Kim <kyoung.il.kim@intel.com>
2015-06-18 17:03:40 -0700

    Braswell: Add support for PMIC
    Added support for PMIC in Braswell power sequencing code to support
    the PMIC enabled Braswell devices.
    
    BUG=none
    TEST=Tested S3, S5, G3 & PG3 on BCRD2.
    BRANCH=none
    
    Change-Id: I247ef9506d0e8065c761bfb00b9141ec8ff5ada8
    Signed-off-by: Kyoung Kim <kyoung.il.kim@intel.com>
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>
    Signed-off-by: Kevin K Wong <kevin.k.wong@intel.com>

--- include/config.h
+++ include/config.h
@@ -397,6 +397,9 @@
 /* Support firmware long press power-off timer */
 #undef CONFIG_PMIC_FW_LONG_PRESS_TIMER
 
+/* Support PMIC control for the board */
+#undef CONFIG_BOARD_PMIC_ENABLE
+
 /*****************************************************************************/
 /*
  * Optional console commands
