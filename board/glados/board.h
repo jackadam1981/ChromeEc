6b2d2655a778b5f82bcfba42545318b369a99143
Aseda Aboagye <aaboagye@google.com>
2015-06-23 11:15:46 -0700

    glados: Add battery temp to temp_sensors list.
    BUG=chrome-os-partner:15461
    BUG=chrome-os-partner:40599
    BRANCH=none
    TEST=Flashed EC image on glados and verified "temps" command on EC
    console displayed battery temperature.
    TEST=make -j buildall tests
    
    Change-Id: I1df6aab054aee0b5658a90ad736af7dc9a9679e3
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/glados/board.h
+++ board/glados/board.h
@@ -49,6 +49,7 @@
 #define CONFIG_SPI_FLASH_SIZE 524288
 #define CONFIG_SPI_FLASH_W25Q64
 
+#define CONFIG_TEMP_SENSOR
 /*
  * Allow dangerous commands.
  * TODO(shawnn): Remove this config before production.
@@ -107,6 +108,13 @@ enum charge_supplier {
 	CHARGE_SUPPLIER_OTHER,
 	CHARGE_SUPPLIER_VBUS,
 	CHARGE_SUPPLIER_COUNT
+};
+
+enum temp_sensor_id {
+	/* Battery temperature sensor */
+	TEMP_SENSOR_BATTERY,
+
+	TEMP_SENSOR_COUNT
 };
 
 /* supplier_priority table defined in board.c */
