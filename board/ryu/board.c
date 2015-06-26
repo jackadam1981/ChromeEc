42e9940351ec43a2f5e1e1b01affeb23f34c95c1
Gwendal Grignou <gwendal@chromium.org>
2015-06-26 16:15:37 -0700

    motion: Add sample frequency per sensor
    Store at which frequency each sensor should be sampled.
    This frequency is different the sensor frequency.
    If 2 sensors must be sampled at very different frequency,
    we don't want to oversample the slow one, and filling the
    software FIFO unnecessarily.
    
    BRANCH=smaug
    TEST=Unit test. Check that frequency is correct when sensor
    frequencies change from IIO driver.
    BUG=chrome-os-partner:39900
    
    Change-Id: I4272963413f53d4ca004e26639dc7a2affd317eb
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- board/ryu/board.c
+++ board/ryu/board.c
@@ -298,6 +298,7 @@ struct motion_sensor_t motion_sensors[] = {
 	 .default_config = {
 		 .odr = 100000,
 		 .range = 8,
+		 .ec_rate = SUSPEND_SAMPLING_INTERVAL,
 	 }
 	},
 
@@ -312,8 +313,9 @@ struct motion_sensor_t motion_sensors[] = {
 	 .i2c_addr = BMI160_ADDR0,
 	 .rot_standard_ref = NULL,
 	 .default_config = {
-		 .odr = 100000,
-		 .range = 2000
+		 .odr = 0,
+		 .range = 2000,
+		 .ec_rate = MAX_MOTION_SENSE_WAIT_TIME,
 	 }
 	},
 
@@ -328,8 +330,9 @@ struct motion_sensor_t motion_sensors[] = {
 	 .i2c_addr = BMI160_ADDR0,
 	 .rot_standard_ref = NULL,
 	 .default_config = {
-		 .odr = 25000,
-		 .range = 1
+		 .odr = 0,
+		 .range = 1,
+		 .ec_rate = MAX_MOTION_SENSE_WAIT_TIME,
 	 }
 	},
 };
