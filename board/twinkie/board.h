50c4dd0c2328f67aa89917599915d78bccad2a4f
Dawei Li <daweili@google.com>
2015-06-29 11:29:23 -0700

    twinkie firmware: add support for vbus voltage and current
    The format of an ep packet header (8 bytes) :
    Packets for cc1: [sample_tstamp, sample_seq, vbus_voltage, tstamp_offset]
    Packets for cc2: [sample_tstamp, sample_seq, vbus_current, tstamp_offset]
    Updated: use array to implement a circular queue; when a forming a packet,
    remove one element from the queue. In this way, we can capture more samples
    of vbus information.
    Updated: when sniffer task has not started sending packets, most recent vbus
    info is always stored at the first position of the queue, making sure that
    the first reported vbus info is the most recent one.
    
    Updated: backword compatibility by using a diffrent product id. New firmware
    reporting vbus uses 0x500B as product ID. Previous firmware without vbus
    sniffing uses 0x500A as its product ID. We have made corresponding changes
    in the libsigrok project to accepts and distingush these two product IDs.
    
    Updated: allow user to compile the original version or the new version of the
    source code. In ec/board/twinkie/board.h
    Use #define CONFIG_USBC_SNIFFER_HEADER_V1, if you do not want twinkie to
    send out vbus info;
    Use #define CONFIG_USBC_SNIFFER_HEADER_V2, if you want twinkie to send out
    vbus info.
    
    Signed-off-by: Dawei Li <daweili@google.com>
    
    BUG=chrome-os-partner:42703
    BRANCH=none
    TEST=BEGIN
    connect Twinkie to a waveform generator, record data using:
    sudo sigrok-cli -d chromium-twinkie --continuous -o test.sr
    then check the resulting waveforms on pulseview.
    END
    
    Change-Id: Ifdb6402eb7d998ffb25128c510d1780491b11872

--- board/twinkie/board.h
+++ board/twinkie/board.h
@@ -25,6 +25,13 @@
 #define CONFIG_USB_PD_TCPC
 #define CONFIG_USB_PD_TCPM_STUB
 #define CONFIG_PD_USE_DAC_AS_REF
+/*
+ * use #define CONFIG_USBC_SNIFFER_HEADER_V1
+ * if you do not want twinkie to send out vbus info;
+ * use #define CONFIG_USBC_SNIFFER_HEADER_V2
+ * if you want twinkie to send out vbus info.
+ * */
+#define CONFIG_USBC_SNIFFER_HEADER_V2
 #define CONFIG_HW_CRC
 
 #ifndef HAS_TASK_PD /* PD sniffer mode */
@@ -46,7 +53,12 @@
 #define I2C_PORT_MASTER 0
 
 /* USB configuration */
+#ifdef CONFIG_USBC_SNIFFER_HEADER_V2
+#define CONFIG_USB_PID 0x500B
+#else
 #define CONFIG_USB_PID 0x500A
+#endif
+
 /* By default, enable all console messages excepted USB */
 #define CC_DEFAULT     (CC_ALL & ~CC_MASK(CC_USB))
 
