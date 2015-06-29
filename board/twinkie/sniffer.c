6b90528e93a9a35bed91e2a1d61ca7879e79052b
Dawei Li <daweili@google.com>
2015-06-29 11:29:23 -0700

    twinkie: add support for vbus and current
    the format of a ep packet header (8 bytes):
    packets for cc1: [sample_tstamp, sample_seq, vbus_voltage, tstamp_offset]
    packets for cc2: [sample_tstamp, sample_seq, vbus_current, tstamp_offset]
    updated: use queue to hold multiple vbus values; when a forming a packet,
    remove one element from the queue. In this way, we can capture more samples
     of vbus information.
    
    Signed-off-by: Dawei Li <daweili@google.com>
    
    BUG=chrome-os-partner:42703
    BRANCH=none
    TEST=BEGIN
    connect Twinkie to a waveform generator, record data using:
    sudo sigrok-cli -d chromium-twinkie --continuous -o test.sr -r 200 -R 200
    then check the resulting waveforms on pulseview.
    END
    
    Change-Id: Ifdb6402eb7d998ffb25128c510d1780491b11872

--- board/twinkie/sniffer.c
+++ board/twinkie/sniffer.c
@@ -18,11 +18,19 @@
 #include "timer.h"
 #include "usb.h"
 #include "util.h"
-
+#include "ina2xx.h"
+#include "queue.h"
+struct sniffer_sample_header {
+	uint16_t seq;
+	uint16_t tstamp;
+	uint16_t vbus_value; /* can be voltage or current */
+	int16_t sample_tstamp;
+};
 /* Size of one USB packet buffer */
 #define EP_BUF_SIZE 64
+#define EP_PACKET_HEADER_SIZE (sizeof(struct sniffer_sample_header))
 /* Size of the payload (packet minus the header) */
-#define EP_PAYLOAD_SIZE (EP_BUF_SIZE - 4)
+#define EP_PAYLOAD_SIZE (EP_BUF_SIZE - EP_PACKET_HEADER_SIZE)
 
 /* Buffer enough to avoid overflowing due to USB latencies on both sides */
 #define RX_COUNT (16 * EP_PAYLOAD_SIZE)
@@ -31,6 +39,10 @@
 #define USB_EVENTS TASK_EVENT_CUSTOM(3)
 
 /* Bitmap of enabled capture channels : CC1+CC2 by default */
+
+/* after how long the deferred reads will wake up for the next read */
+#define DEFERRED_READ_TIME_US 8000
+
 static uint8_t channel_mask = 0x3;
 
 /* edge timing samples */
@@ -42,10 +54,56 @@ static uint16_t sample_tstamp[4];
 /* sequence number of the beginning of DMA buffers */
 static uint16_t sample_seq[4];
 
+/* vbus voltage information: the voltage value and the timestamp offset */
+struct vbus_vol_info {
+	uint16_t vbus_vol;
+	uint16_t vbus_tstamp_bf; /* before the read */
+	uint16_t vbus_tstamp_af; /* after the read */
+};
+
+/* vbus current information: the voltage value and the timestamp offset */
+struct vbus_curr_info {
+	int16_t vbus_curr;
+	uint16_t vbus_tstamp_bf;
+	uint16_t vbus_tstamp_af;
+};
+
+/* a queue to hold multiple vbus values */
+static struct queue vbus_vol_queue = QUEUE_NULL(16, struct vbus_vol_info);
+static struct queue vbus_curr_queue = QUEUE_NULL(16, struct vbus_curr_info);
+
 /* Bulk endpoint double buffer */
 static usb_uint ep_buf[2][EP_BUF_SIZE / 2] __usb_ram;
 /* USB Buffers not used, ready to be filled */
 static volatile uint32_t free_usb = 3;
+
+static void vbus_vol_read_deferred(void)
+{
+	struct vbus_vol_info vvi_new;
+	/* Unit: ms */
+	vvi_new.vbus_tstamp_bf = __hw_clock_source_read() / 1000;
+	/* Unit: mV */
+	vvi_new.vbus_vol = ina2xx_read(0, INA2XX_REG_BUS_VOLT)*125 / 100;
+	vvi_new.vbus_tstamp_af = __hw_clock_source_read() / 1000;
+	queue_add_unit(&vbus_vol_queue, &vvi_new);
+
+	hook_call_deferred(vbus_vol_read_deferred, DEFERRED_READ_TIME_US);
+}
+DECLARE_DEFERRED(vbus_vol_read_deferred);
+
+static void vbus_curr_read_deferred(void)
+{
+	struct vbus_curr_info vci_new;
+	/* Unit: ms */
+	vci_new.vbus_tstamp_bf = __hw_clock_source_read() / 1000;
+	/* Unit: mA */
+	vci_new.vbus_curr = ina2xx_read(0, INA2XX_REG_CURRENT);
+	vci_new.vbus_tstamp_af = __hw_clock_source_read() / 1000;
+	queue_add_unit(&vbus_curr_queue, &vci_new);
+
+	hook_call_deferred(vbus_curr_read_deferred, DEFERRED_READ_TIME_US);
+}
+DECLARE_DEFERRED(vbus_curr_read_deferred);
 
 static inline void led_set_activity(int ch)
 {
@@ -151,6 +209,11 @@ static volatile uint32_t seq;
 /* Buffer overflow count */
 static uint32_t oflow;
 
+#define SNIFFER_CHANNEL_CC1 0
+#define SNIFFER_CHANNEL_CC2 1
+
+#define get_channel(b)   (((b) >> 12) & 0x1)
+
 void tim_rx1_handler(uint32_t stat)
 {
 	stm32_dma_regs_t *dma = STM32_DMA1_REGS;
@@ -158,8 +221,9 @@ void tim_rx1_handler(uint32_t stat)
 	uint32_t mask = idx ? 0xFF00 : 0x00FF;
 	uint32_t next = idx ? 0x0001 : 0x0100;
 
-	sample_tstamp[idx] = __hw_clock_source_read();
-	sample_seq[idx] = ((seq++ << 3) & 0x0ff8) | (0<<12) /* CC1 */;
+	sample_tstamp[idx] = __hw_clock_source_read() / 1000;
+	sample_seq[idx] = ((seq++ << 3) & 0x0ff8) |
+			(SNIFFER_CHANNEL_CC1<<12);
 	if (filled_dma & next) {
 		oflow++;
 		sample_seq[idx] |= 0x8000;
@@ -179,8 +243,9 @@ void tim_rx2_handler(uint32_t stat)
 	uint32_t next = idx ? 0x00010000 : 0x01000000;
 
 	idx += 2;
-	sample_tstamp[idx] = __hw_clock_source_read();
-	sample_seq[idx] = ((seq++ << 3) & 0x0ff8) | (1<<12) /* CC2 */;
+	sample_tstamp[idx] = __hw_clock_source_read() / 1000;
+	sample_seq[idx] = ((seq++ << 3) & 0x0ff8) |
+			(SNIFFER_CHANNEL_CC2<<12);
 	if (filled_dma & next) {
 		oflow++;
 		sample_seq[idx] |= 0x8000;
@@ -237,8 +302,13 @@ static void rx_timer_init(int tim_id, timer_ctlr_t *tim, int ch_idx, int up_idx)
 	tim->sr = 0;
 }
 
+
+
 void sniffer_init(void)
 {
+	hook_call_deferred(vbus_vol_read_deferred, 0);
+	hook_call_deferred(vbus_curr_read_deferred, 0);
+
 	/* remap TIM1 CH1/2/3 to DMA channel 6 */
 	STM32_SYSCFG_CFGR1 |= 1 << 28;
 
@@ -279,11 +349,12 @@ void sniffer_task(void)
 	int u = 0; /* current USB buffer index */
 	int d = 0; /* current DMA buffer index */
 	int off = 0; /* DMA buffer offset */
-
+	int ch; /* sniffer channel */
+	struct vbus_vol_info vvi_read;
+	struct vbus_curr_info vci_read; /* vbus current information */
 	while (1) {
 		/* Wait for a new buffer of samples or a new USB free buffer */
 		task_wait_event(-1);
-
 		/* send the available samples over USB if we have a buffer*/
 		while (filled_dma && free_usb) {
 			while (!(filled_dma & (1 << d))) {
@@ -294,8 +365,34 @@ void sniffer_task(void)
 			}
 			ep_buf[u][0] = sample_seq[d >> 3] | (d & 7);
 			ep_buf[u][1] = sample_tstamp[d >> 3];
-			memcpy_to_usbram(((void *)usb_sram_addr(ep_buf[u] + 2)),
-					 samples[d >> 4]+off, EP_PAYLOAD_SIZE);
+
+			ch = get_channel(ep_buf[u][0]);
+
+			if (SNIFFER_CHANNEL_CC1 == ch) {
+				while (queue_count(&vbus_vol_queue) > 8) {
+					/* at most 8 vbus values are copied to the usb packet at almost the same time for a USB transfer*/
+					/* queue is generally less than 8, we want to get the latest values */
+					queue_remove_unit(&vbus_vol_queue, &vvi_read);
+				}
+				queue_remove_unit(&vbus_vol_queue, &vvi_read);
+				ep_buf[u][2] = vvi_read.vbus_vol;
+				/* the read may be interrupted, we use the average of start and end time as the tstamp */
+				ep_buf[u][3] = (vvi_read.vbus_tstamp_af + vvi_read.vbus_tstamp_bf)/2 - ep_buf[u][1];
+
+			} else if (SNIFFER_CHANNEL_CC2 == ch) {
+				while (queue_count(&vbus_curr_queue) > 8) {
+					queue_remove_unit(&vbus_curr_queue, &vci_read);
+				}
+				queue_remove_unit(&vbus_curr_queue, &vci_read);
+				ep_buf[u][2] = vci_read.vbus_curr;
+				ep_buf[u][3] = (vci_read.vbus_tstamp_af + vci_read.vbus_tstamp_bf)/2 - ep_buf[u][1];
+			}
+
+			memcpy_to_usbram(
+					((void *)usb_sram_addr(ep_buf[u]
+						+ (EP_PACKET_HEADER_SIZE>>1))),
+					samples[d >> 4]+off,
+					EP_PAYLOAD_SIZE);
 			atomic_clear((uint32_t *)&free_usb, 1 << u);
 			u = !u;
 			atomic_clear((uint32_t *)&filled_dma, 1 << d);
