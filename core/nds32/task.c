994c6fa993660614b3857d264f95b87598773248
Aseda Aboagye <aaboagye@google.com>
2015-07-01 09:31:26 -0700

    hooks: Move HOOK_INIT to after task switching.
    This commit changes the way in which tasks are started.  Instead of
    having all tasks marked as ready to run upon initialization, only the
    hooks task is marked as ready to run.  HOOK_INITs are now run at the
    beginning of the hooks task.  After the HOOK_INITs, the hooks task calls
    back to enable the rest of the tasks, reschedules, and proceeds as
    usual.  This also allows the removal of checks for task_start_called().
    
    BUG=chrome-os-partner:27226
    BRANCH=None
    TEST=Built and flash EC image for samus and verified that EC boot was
    successful as well as AP boot.  Additionally, verified that charging,
    keyboard, tap-for-battery were all still functional.
    TEST=make -j buildall tests
    
    Change-Id: Iea53670222c803c2985e9c86c96974386888a4fe
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- core/nds32/task.c
+++ core/nds32/task.c
@@ -139,9 +139,11 @@ int need_resched;
 /*
  * Bitmap of all tasks ready to be run.
  *
- * Currently all tasks are enabled at startup.
+ * Start off with only the hooks task marked as ready such that all the modules
+ * can do their init within a task switching context.  The hooks task will then
+ * make a call to enable all tasks.
  */
-static uint32_t tasks_ready = (1<<TASK_ID_COUNT) - 1;
+static uint32_t tasks_ready = (1 << TASK_ID_HOOKS);
 
 static int start_called;  /* Has task swapping started */
 
@@ -304,6 +306,15 @@ static void set_int_mask(uint32_t val)
 static void set_int_priority(uint32_t val)
 {
 	asm volatile ("mtsr %0, $INT_PRI" : : "r"(val));
+}
+
+void task_enable_all_tasks(void)
+{
+	task_id_t t = TASK_ID_COUNT-1;
+	/* Mark all tasks are ready to run. */
+	tasks_ready = (1 << TASK_ID_COUNT) - 1;
+	/* Reschedule the highest priority task. */
+	syscall_handler(0, t, 0);
 }
 
 void task_enable_irq(int irq)
