8b652abadf11831a7f51f48c870011fa97da62f3
li feng <li1.feng@intel.com>
2015-07-07 13:58:24 -0700

    Strago/Cyan: Change USB power pin name to generic one.
    BUG=none
    TEST=Verified on Acer EVT GPIO USB1_ENABLE and USB2_ENABLE value
    changed when state switch between S3 and S5.
    BRANCH=none
    
    Change-Id: I85f2047c1a40aebf36743a17d353ff3bc481d867
    Signed-off-by: li feng <li1.feng@intel.com>

--- power/braswell.c
+++ power/braswell.c
@@ -158,8 +158,8 @@ enum power_state power_handle_state(enum power_state state)
 
 	case POWER_S5S3:
 		/* Turn on the USB power */
-		gpio_set_level(GPIO_USB2_PWR_EN, 1);
-		gpio_set_level(GPIO_USB3_PWR_EN, 1);
+		gpio_set_level(GPIO_USB1_ENABLE, 1);
+		gpio_set_level(GPIO_USB2_ENABLE, 1);
 
 		/* Call hooks now that rails are up */
 		hook_notify(HOOK_CHIPSET_STARTUP);
@@ -284,8 +284,8 @@ enum power_state power_handle_state(enum power_state state)
 
 	case POWER_S3S5:
 		/* Turn off the USB power */
-		gpio_set_level(GPIO_USB2_PWR_EN, 0);
-		gpio_set_level(GPIO_USB3_PWR_EN, 0);
+		gpio_set_level(GPIO_USB1_ENABLE, 0);
+		gpio_set_level(GPIO_USB2_ENABLE, 0);
 
 		/* Call hooks before we remove power rails */
 		hook_notify(HOOK_CHIPSET_SHUTDOWN);
