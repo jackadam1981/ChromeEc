b96a74685735394c9f82bbe01f02df93f2433358
Shawn Nematbakhsh <shawnn@chromium.org>
2015-07-15 17:13:22 -0700

    mec1322: Allow multiple hibernate wake sources
    Allow multiple GPIOs to wake the EC from hibernate by requiring boards
    to define hibernate_wake_pins and hibernate_wake_pins_used. In addition,
    clean up the GPIO-skipping hibernate code, and skip setting PCH_RTCRST
    as an input due to a bug on certain boards.
    
    BUG=chrome-os-partner:42104
    TEST=Manual on Glados. Run 'hibernate' from EC console, verify that EC
    wakes with power button press or with "dut-control lid_open:no".
    BRANCH=None
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: I13a6e062393cab8ed7129eda253585951f771109

--- board/glower/board.c
+++ board/glower/board.c
@@ -45,6 +45,12 @@ const struct i2c_port_t i2c_ports[] = {
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
+const enum gpio_signal hibernate_wake_pins[] = {
+	GPIO_POWER_BUTTON_L,
+};
+
+const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
+
 /**
  * Discharge battery when on AC power for factory test.
  */
--- include/config.h
+++ include/config.h
@@ -1684,10 +1684,6 @@
 /* Support computing hash of code for verified boot */
 #undef CONFIG_VBOOT_HASH
 
-/******************************************************************************/
-/* GPIO pin to wake from hibernate. */
-#undef CONFIG_WAKE_PIN
-
 /*****************************************************************************/
 /* Watchdog config */
 
