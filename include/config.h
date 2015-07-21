cd575da988ef3b448ad560cf5887e195483a740e
Gwendal Grignou <gwendal@chromium.org>
2015-07-21 15:01:05 -0700

    BACKPORT: common: Untie math_util from motion sense.
    math library can be set independentely.
    It is implied when motion sensor drivers are compiled in.
    
    BRANCH=samus
    TEST=Build strago board specific tests, host test and ran
    ryu image.
    BUG=chromium:512329, b:27849483
    
    Change-Id: I743ea7b44e4a3783602c11f3928cb3fa4b105ec4
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/287371
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
    (cherry picked from commit d1002e798ec91e9c570f11613f80e73f381123ae)
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- include/config.h
+++ include/config.h
@@ -864,6 +864,9 @@
 /* Support LPC interface */
 #undef CONFIG_LPC
 
+/* Need for a math library */
+#undef CONFIG_MATH_UTIL
+
 /* Presence of a Bosh Sensortec BMM150 magnetometer behind a BMI160. */
 #undef CONFIG_MAG_BMI160_BMM150
 
@@ -1396,8 +1399,6 @@
  */
 #include "test_config.h"
 
-
-/*****************************************************************************/
 /*
  * Sanity checks to make sure some of the configs above make sense.
  */
