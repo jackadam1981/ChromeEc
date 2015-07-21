02922dae276095f98418843b72436b10da26de4f
Vadim Bendebury <vbendeb@chromium.org>
2015-07-21 13:54:00 -0700

    Cr50: Enable TPM-protocol data over the SPI bus
    This patch adds a module which runs on top of the SPS driver and
    implements the TCG SPI TPM protocol.
    
    Basic register read and write functions are implemented as well as
    rudimentary TPM state machine (claiming/releasing locality).
    
    An enhancement is made to the SPS driver to ensure that when the CS is
    deasserted the transmit FIFO is reset too, on the off chance of the CS
    going away mid transaction for whatever reason.
    
    In this implementation the slave is guaranteed to stall the master for
    a few bytes in both receive and transmit transactions, which is
    further aggravated by the fact that RX FIFO threshold is set to 8
    (this is the minimum number of bytes the master has to send to wake up
    the slave). This could be fine tuned later, for instance made a
    parameter of the receive callback registration function.
    
    BRANCH=none
    BUG=chrome-os-partner:43025
    TEST=trunksd initialization (with minor changes to accommodate new
         VID/DID and some status bits, to be published) succeeds with the
         cr50 connected to the USB/SPI cable.
    
    Change-Id: I28d37c3b57dde9adf59e81426efe4f58880cf0b0
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1448,6 +1448,12 @@
 #undef CONFIG_TEMP_SENSOR_POWER_GPIO
 
 /*****************************************************************************/
+/* TPM-like configuration */
+
+/* Speak the TPM SPI Hardware Protocol on the SPI slave interface */
+#undef CONFIG_TPM_SPS
+
+/*****************************************************************************/
 /* USART stream config */
 #undef CONFIG_STREAM_USART
 
