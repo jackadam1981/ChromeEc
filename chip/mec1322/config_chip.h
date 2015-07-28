d4a67cf35e22440bd0a165def3e3785adf6f6e24
Divagar Mohandass <divagar.mohandass@intel.com>
2015-07-28 17:08:41 +0530

    cyan: Increase chipset stack size.
    Chipset task is overflowing and causing runtime crash.
    Increasing the chipset task stack size by 128 bytes.
    
    BRANCH=None
    BUG=None
    TEST=Build/flash EC and boot the platform to OS.
    
    Change-Id: I4e444cc48979c74810851ab2625b982fdabdeb73
    Signed-off-by: Divagar Mohandass <divagar.mohandass@intel.com>

--- chip/mec1322/config_chip.h
+++ chip/mec1322/config_chip.h
@@ -77,6 +77,7 @@
 #define HOOKS_TASK_STACK_SIZE		640
 #define CONSOLE_TASK_STACK_SIZE		640
 #define HOST_CMD_TASK_STACK_SIZE	640
+#define CHIPSET_TASK_STACK_SIZE		640
 
 /* Default task stack size */
 #define TASK_STACK_SIZE			512
