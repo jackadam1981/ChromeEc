275ef9a449f87d92743afc2e7d7296c5dbf433be
Vijay Hiremath <vijay.p.hiremath@intel.com>
2015-07-29 15:51:06 -0700

    Kunimitsu: Add PMIC low power mode
    BUG=none
    TEST=none
    BRANCH=none
    
    Change-Id: Ia22d1b3d7612d4bc586355e737ff624a144170d8
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- board/kunimitsu/board.c
+++ board/kunimitsu/board.c
@@ -415,3 +415,55 @@ void board_set_charge_limit(int charge_ma)
 					   CONFIG_CHARGER_INPUT_CURRENT));
 }
 
+void pmic_init(void)
+{
+	int data;
+	int ret;
+
+	ret = i2c_read8(I2C_PORT_PMIC, 0x60, 0x36, &data);
+	if (ret) {
+		CPRINTS("Low power init failed");
+		return;
+	}
+	ret = i2c_write8(I2C_PORT_PMIC, 0x60, 0x36, data | (1 << 7));
+	if (ret) {
+		CPRINTS("Low power init failed");
+		return;
+	}
+
+	ret = i2c_read8(I2C_PORT_PMIC, 0x60, 0x34, &data);
+	if (ret) {
+		CPRINTS("Low power init failed");
+		return;
+	}
+	ret = i2c_write8(I2C_PORT_PMIC, 0x60, 0x34, (data & 0x3F) | (1 << 7));
+	if (ret) {
+		CPRINTS("Low power init failed");
+		return;
+	}
+
+	ret = i2c_read8(I2C_PORT_PMIC, 0x60, 0x32, &data);
+	if (ret) {
+		CPRINTS("Low power init failed");
+		return;
+	}
+	ret = i2c_write8(I2C_PORT_PMIC, 0x60, 0x32, (data & 0x3F) | (1 << 7));
+	if (ret) {
+		CPRINTS("Low power init failed");
+		return;
+	}
+
+	ret = i2c_read8(I2C_PORT_PMIC, 0x60, 0x31, &data);
+	if (ret) {
+		CPRINTS("Low power init failed");
+		return;
+	}
+	ret = i2c_write8(I2C_PORT_PMIC, 0x60, 0x31, (data & 0x3F) | (1 << 7));
+	if (ret) {
+		CPRINTS("Low power init failed");
+		return;
+	}
+
+	CPRINTS("PMIC init done");
+}
+DECLARE_HOOK(HOOK_CHIPSET_PRE_INIT, pmic_init, HOOK_PRIO_DEFAULT);
