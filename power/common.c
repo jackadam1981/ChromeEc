c0cd4edce093cef553199732035e147bffb3f14a
Kaiyen Chang <kaiyen.chang@intel.com>
2015-08-03 21:16:11 +0800

    Fixed a failure of power button press while entering G3 state.
    If the power button is pressed while S5 inactivity timer is about to
    expire, EC need to give CPU a little time to start up before changing
    the state from S5 to G3 (the hard off state); otherwise the system will
    not start up. This issue can be reproduced on Rambi.
    
    BUG=chrome-os-partner:42728, chrome-os-partner:42811
    BRANCH=None
    TEST=Implement an ec command to simulate power button press while S5
         inactivity timer is about to expire, and then make sure that the
         patch did solve the issue.
    
    Change-Id: I022e8e14fd41447898760a4d57a4702e2c00a0d5
    Signed-off-by: Kaiyen Chang <kaiyen.chang@intel.com>

--- power/common.c
+++ power/common.c
@@ -124,6 +124,8 @@ void power_set_state(enum power_state new_state)
 	/* Record the time we go into G3 */
 	if (new_state == POWER_G3)
 		last_shutdown_time = get_time().val;
+	else if (state == POWER_S5 && new_state == POWER_S5S3)
+		want_g3_exit = 0;
 
 	state = new_state;
 }
@@ -169,12 +171,13 @@ static enum power_state power_common_state(enum power_state state)
 		break;
 
 	case POWER_S5:
+		want_g3_exit = 0;
+
 		/* Wait for inactivity timeout */
 		power_wait_signals(0);
 		if (task_wait_event(S5_INACTIVITY_TIMEOUT) ==
 		    TASK_EVENT_TIMER) {
-			/* Drop to G3; wake not requested yet */
-			want_g3_exit = 0;
+			/* Prepare to drop to G3; wake not requested yet */
 			return POWER_S5G3;
 		}
 		break;
@@ -248,13 +251,24 @@ int chipset_in_state(int state_mask)
 
 void chipset_exit_hard_off(void)
 {
-	/* If not in the hard-off state nor headed there, nothing to do */
-	if (state != POWER_G3 && state != POWER_S5G3)
+	/*
+	 * If not in the soft-off state, hard-off state, or headed there,
+	 * nothing to do.
+	 */
+	if (state != POWER_G3 && state != POWER_S5G3 && state != POWER_S5)
 		return;
 
 	/* Set a flag to leave G3, then wake the task */
 	want_g3_exit = 1;
 
+	/*
+	 * If the power state is in POWER_S5 and S5 inactivity timer is
+	 * running, to wake the chipset task can cancel S5 inactivity timer and
+	 * then restart the timer. This will give cpu a chance to start up if S5
+	 * inactivity timer is about to expire while power button is pressed.
+	 * For other states here, to wake the chipset task to trigger the event
+	 * for leaving G3 is necessary.
+	 */
 	if (task_start_called())
 		task_wake(TASK_ID_CHIPSET);
 }
