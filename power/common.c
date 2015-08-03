2774d72937f896f4e10208a7d52c664a4976c651
Kaiyen Chang <kaiyen.chang@intel.com>
2015-08-03 21:16:11 +0800

    Fixed a failure of power button press while entering G3 state.
    If the power button is pressed while the S5 inactivity timer is
    about to expire, EC need to give CPU a little time to start up
    before changing the state from S5 to G3 (the hard off state).
    Otherwise the system will not start up. This issue can be
    reproduced on Rambi.
    
    BUG=chrome-os-partner:42728, chrome-os-partner:42811
    BRANCH=None
    TEST=Implement an ec command to simulate power button press while
         S5 inactivity timer is about to expire, and then make sure
         that the patch did solve the issue.
    
    Change-Id: I022e8e14fd41447898760a4d57a4702e2c00a0d5
    Signed-off-by: Kaiyen Chang <kaiyen.chang@intel.com>

--- power/common.c
+++ power/common.c
@@ -171,10 +171,13 @@ static enum power_state power_common_state(enum power_state state)
 	case POWER_S5:
 		/* Wait for inactivity timeout */
 		power_wait_signals(0);
+
+		/* Reset want_g3_exit flag */
+		want_g3_exit = 0;
+
 		if (task_wait_event(S5_INACTIVITY_TIMEOUT) ==
 		    TASK_EVENT_TIMER) {
-			/* Drop to G3; wake not requested yet */
-			want_g3_exit = 0;
+			/* Prepare to drop to G3; wake not requested yet */
 			return POWER_S5G3;
 		}
 		break;
@@ -248,8 +251,11 @@ int chipset_in_state(int state_mask)
 
 void chipset_exit_hard_off(void)
 {
-	/* If not in the hard-off state nor headed there, nothing to do */
-	if (state != POWER_G3 && state != POWER_S5G3)
+	/*
+	 * If not in the soft-off state, hard-off state, or headed there,
+	 * nothing to do.
+	 */
+	if (state != POWER_G3 && state != POWER_S5G3 && state != POWER_S5)
 		return;
 
 	/* Set a flag to leave G3, then wake the task */
