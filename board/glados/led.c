2d2ec3503688a2bc56bbef9cb9f1685670d2ab13
Mike Hsieh <mike.m.hsieh@intel.com>
2015-08-03 19:15:41 +0800

    glaDOS: Implement LED functionality on EC
    Implement LED control for glaDOS for both red and green LED.
    
    BUG=chrome-os-partner:40848
    BRANCH=none
    TEST=Tested on glaDOS with command:
    	ectool led battery red
    	ectool led battery green
    	ectool led battery off
    
    Change-Id: I1b4f8c8c8f26779a11185ea8bbc6536d1d7f97b1
    Signed-off-by: Mike Hsieh <mike.m.hsieh@intel.com>

--- board/glados/led.c
+++ board/glados/led.c
@@ -14,17 +14,124 @@
 #include "host_command.h"
 #include "led_common.h"
 #include "util.h"
+#include "lid_switch.h"
+
+#ifdef CONFIG_LED_BAT_ACTIVE_LOW
+#define BAT_LED_ON 0
+#define BAT_LED_OFF 1
+#else
+#define BAT_LED_ON 1
+#define BAT_LED_OFF 0
+#endif
 
 const enum ec_led_id supported_led_ids[] = {
 	EC_LED_ID_POWER_LED, EC_LED_ID_BATTERY_LED};
 
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 
+enum led_color {
+	LED_OFF = 0,
+	LED_RED,
+	LED_AMBER,
+	LED_GREEN,
+	LED_WHITE,
+	LED_COLOR_COUNT  /* Number of colors, not a color itself */
+};
+
+static int bat_led_set_color(enum led_color color)
+{
+	switch (color) {
+	case LED_OFF:
+		gpio_set_level(GPIO_CHARGE_LED_1, BAT_LED_OFF);
+		gpio_set_level(GPIO_CHARGE_LED_2, BAT_LED_OFF);
+		break;
+	case LED_RED:
+		gpio_set_level(GPIO_CHARGE_LED_1, BAT_LED_ON);
+		gpio_set_level(GPIO_CHARGE_LED_2, BAT_LED_OFF);
+		break;
+	case LED_AMBER:
+		gpio_set_level(GPIO_CHARGE_LED_1, BAT_LED_ON);
+		gpio_set_level(GPIO_CHARGE_LED_2, BAT_LED_ON);
+		break;
+	case LED_GREEN:
+		gpio_set_level(GPIO_CHARGE_LED_1, BAT_LED_OFF);
+		gpio_set_level(GPIO_CHARGE_LED_2, BAT_LED_ON);
+		break;
+	default:
+		return EC_ERROR_UNKNOWN;
+	}
+	return EC_SUCCESS;
+}
+
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 {
+	switch (led_id) {
+	case EC_LED_ID_BATTERY_LED:
+		gpio_set_level(GPIO_CHARGE_LED_1,
+				(brightness[EC_LED_COLOR_RED] != 0) ?
+					BAT_LED_ON : BAT_LED_OFF);
+		gpio_set_level(GPIO_CHARGE_LED_2,
+				(brightness[EC_LED_COLOR_GREEN] != 0) ?
+					BAT_LED_ON : BAT_LED_OFF);
+		break;
+	default:
+		return EC_ERROR_UNKNOWN;
+	}
 	return EC_SUCCESS;
 }
 
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
+	brightness_range[EC_LED_COLOR_RED] = 1;
+	brightness_range[EC_LED_COLOR_GREEN] = 1;
 }
+
+static void glados_led_set_battery(void)
+{
+	static int battery_second;
+	uint32_t chflags = charge_get_flags();
+
+	battery_second++;
+
+	/* BAT LED behavior:
+	 * Same as the chromeos spec
+	 * Green/Amber for CHARGE_FLAG_FORCE_IDLE
+	 */
+	switch (charge_get_state()) {
+	case PWR_STATE_CHARGE:
+		bat_led_set_color(LED_AMBER);
+		break;
+	case PWR_STATE_DISCHARGE:
+		if (charge_get_percent() < 3)
+			bat_led_set_color((battery_second & 1)
+					? LED_OFF : LED_AMBER);
+		else if (charge_get_percent() < 10)
+			bat_led_set_color((battery_second & 3)
+					? LED_OFF : LED_AMBER);
+		else
+			bat_led_set_color(LED_OFF);
+		break;
+	case PWR_STATE_ERROR:
+		bat_led_set_color((battery_second & 1) ? LED_OFF : LED_RED);
+		break;
+	case PWR_STATE_CHARGE_NEAR_FULL:
+		bat_led_set_color(LED_GREEN);
+		break;
+	case PWR_STATE_IDLE:
+		if (chflags & CHARGE_FLAG_FORCE_IDLE)
+			bat_led_set_color(
+				(battery_second & 0x2) ? LED_GREEN : LED_AMBER);
+		else
+			bat_led_set_color(LED_GREEN);
+		break;
+	default:
+		break;
+	}
+}
+
+static void led_second(void)
+{
+	if (led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED))
+		glados_led_set_battery();
+}
+DECLARE_HOOK(HOOK_SECOND, led_second, HOOK_PRIO_DEFAULT);
