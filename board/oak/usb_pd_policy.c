394114ba4dc59247b3d7d9ceab7cb0272bd6a981
Rong Chang <rongchang@chromium.org>
2015-08-05 17:37:15 +0800

    oak: enable oak TCPM power status
    Oak doesn't have enough interrupt pins to detect VBUS change. This CL
    enables oak TCPM power status alert.
    
    BRANCH=none
    BUG=chrome-os-partner:41226
    BUG=chrome-os-partner:42610
    TEST=manual
      load on oak rev2. plug in normal type-c charger or legacy type-c
      chargers and check charging status.
    
    Change-Id: I9659e749b515f999fe9e81373567f52ec3fe956a
    Signed-off-by: Rong Chang <rongchang@chromium.org>

--- board/oak/usb_pd_policy.c
+++ board/oak/usb_pd_policy.c
@@ -17,6 +17,7 @@
 #include "util.h"
 #include "usb_mux.h"
 #include "usb_pd.h"
+#include "usb_pd_tcpm.h"
 
 #define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
@@ -128,7 +129,7 @@ void typec_set_input_current_limit(int port, uint32_t max_ma,
 
 int pd_snk_is_vbus_provided(int port)
 {
-	return !gpio_get_level(GPIO_VBUS_WAKE_L);
+	return tcpm_get_vbus_level(port);
 }
 
 int pd_board_checks(void)
