7392cb6b48c366b001b4472cbe55067dbb83c757
Vadim Bendebury <vbendeb@chromium.org>
2015-08-07 14:34:37 -0700

    cr50: add polling uart implementation
    This code kicks in when the target is compiled with
    CONFIG_POLLING_UART defined.
    
    This ensures that each message sent to the console is drained
    completely before the code proceeds, which helps debugging early
    bringup issues.
    
    BRANCH=none
    BUG=chrome-os-partner:43791
    TEST=with this code enabled was able to debug cr50 bringup on the new
         core version.
    
    Change-Id: Iab42370d64d17ecc5210bd4db1f2c5f19b40bce8
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- chip/g/uart.c
+++ chip/g/uart.c
@@ -3,13 +3,94 @@
  * found in the LICENSE file.
  */
 
-#include "clock.h"
 #include "common.h"
-#include "gpio.h"
 #include "registers.h"
+#include "uart.h"
+
+#define UART_NCO ((16 * (1 << UART_NCO_WIDTH) *				\
+		   (long long)CONFIG_UART_BAUD_RATE) / PCLK_FREQ)
+
+#if defined CONFIG_POLLING_UART
+
+/* 115200N81 uart0, TX on A0, RX on A1 */
+void uart_init(void)
+{
+	/* Pinmux init also turns on all clocks. */
+	GREG32(PMU, PERICLKSET0) = 0xffffffff;
+	GREG32(PMU, PERICLKSET1) = 0xffffffff;
+
+	/*
+	 * hardwire clocks to some value... just to get going
+	 * Set source of trim to calibration logic during dynamic trim
+	 */
+	GWRITE_FIELD(XO, CLK_TIMER_TRIM_CTRL, RC_COARSE_TRIM_SRC, 0);
+
+	/* Set initial coarse trim value (slowest) */
+	GREG32(XO, CLK_TIMER_RC_COARSE_ATE_TRIM) = 100;
+
+	/* Set initial trim stabilization period */
+	GWRITE_FIELD(XO, CLK_TIMER_TRIM_CTRL, RC_INITIAL_TRIM_PERIOD, 10);
+
+	/* enable trim */
+	GWRITE_FIELD(XO, CLK_TIMER_TRIM_CTRL, RC_TRIM_EN, 1);
+
+	/* domain crossing sync */
+	GREG32(XO, CLK_TIMER_SYNC_CONTENTS) =  0x1;
+
+
+	GREG32(PINMUX, DIOA0_SEL) = GC_PINMUX_UART0_TX_SEL;
+	GREG32(PINMUX, UART0_RX_SEL) = GC_PINMUX_DIOA1_SEL;
+	GREG32(PINMUX, DIOA1_CTL) =
+		GC_PINMUX_DIOA1_CTL_DS_MASK  | GC_PINMUX_DIOA1_CTL_IE_MASK;
+
+	GREG32(PMU, PWRDN_SCRATCH3) = 0xbeefcafe;
+
+	GREG32(UART, FIFO) = 3;  /* clear RX,TX FIFO */
+
+	GREG32(UART, NCO) = UART_NCO;  /* 115200N81 */
+
+	GREG32(UART, CTRL) = 3;  /* TX,RX enable */
+	uart_write_char('\n');
+	uart_write_char('\r');
+}
+
+int uart_tx_ready(void)
+{
+	/*
+	 * This makes sure that transmit FIFO is fully flashed, so that TX
+	 * FIFO is not used.
+	 */
+	return GREAD_FIELD(UART, STATE, TXIDLE);
+}
+
+void uart_tx_flush(void)
+{
+}
+
+int uart_init_done(void)
+{
+	return 1;
+}
+void uart_tx_start(void)
+{
+}
+void uart_tx_stop(void)
+{
+}
+
+void uart_write_char(char c)
+{
+	while (!uart_tx_ready())
+		;
+	GREG32(UART, WDATA) = c;
+}
+
+#else  /* CONFIG_POLLING_UART   ^^^^^ YES    vvvvvv NO */
+
+#include "clock.h"
+#include "gpio.h"
 #include "system.h"
 #include "task.h"
-#include "uart.h"
 #include "util.h"
 
 static int done_uart_init_yet;
@@ -129,14 +210,11 @@ DECLARE_IRQ(GC_IRQNUM_UART0_RXINT, uart_ec_rx_interrupt, 1);
 
 void uart_init(void)
 {
-	long long setting = (16 * (1 << UART_NCO_WIDTH) *
-			     (long long)CONFIG_UART_BAUD_RATE / PCLK_FREQ);
-
 	/* turn on uart clock */
 	clock_enable_module(MODULE_UART, 1);
 
 	/* set frequency */
-	GR_UART_NCO(0) = setting;
+	GR_UART_NCO(0) = UART_NCO;
 
 	/* Interrupt when RX fifo has anything, when TX fifo <= half empty */
 	/* Also reset (clear) both FIFOs */
@@ -154,3 +232,4 @@ void uart_init(void)
 
 	done_uart_init_yet = 1;
 }
+#endif  /* CONFIG_POLLING_UART   ^^^^^  NO */
