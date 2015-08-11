922ae26d73ed0c16dc72441f41421194aa73ab0c
Ben Lok <ben.lok@mediatek.com>
2015-08-11 19:23:54 +0800

    wip: pd: add low power support
    Add low power register to TCPCI register map, and let PD enter
    into deep sleep if no any PD port is connected.
    
    BRANCH=none
    BUG=chrome-os-partner:43118
    TEST=make -j buildall
    1. Check the current consumption of PD PHY MCU, it should be reduced
       if no any PD port is connected.
    2. Or, check the sleep mask of PD MCU by console command: sleepmask
       The result should be
       sleep mask: 00000002
       rater than
       sleep mask: 00000022
    
    Change-Id: I48b67db0fc9e26aa2242a89f840871b7d0f886f4
    Signed-off-by: Ben Lok <ben.lok@mediatek.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -297,10 +297,19 @@ static inline void set_state(int port, enum pd_states next_state)
 		if (pd_is_connected(i))
 			break;
 	}
-	if (i == CONFIG_USB_PD_PORT_COUNT)
+	if (i == CONFIG_USB_PD_PORT_COUNT) {
+#ifndef CONFIG_USB_PD_TCPM_TCPCI
 		enable_sleep(SLEEP_MASK_USB_PD);
-	else
+#else
+		tcpm_set_sleep(0, 1);
+#endif
+	} else {
+#ifndef CONFIG_USB_PD_TCPM_TCPCI
 		disable_sleep(SLEEP_MASK_USB_PD);
+#else
+		tcpm_set_sleep(0, 0);
+#endif
+	}
 #endif
 
 	CPRINTF("C%d st%d\n", port, next_state);
