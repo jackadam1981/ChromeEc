90cc9a28cc2c1613e04bf94b7f74af90b64e5724
Shawn Nematbakhsh <shawnn@chromium.org>
2015-08-12 18:05:22 -0700

    glados: Reduce power in S0ix
    Allow VCCIO to decay to 0 and reduce the voltage of other supplies in
    S0ix.
    
    BUG=chrome-os-partner:43908
    TEST=Stress-test glados stability coming into and out of S0ix
    BRANCH=None
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: I5aa61164c43ac624cef86f478919294e4cd4e581

--- board/glados/board.c
+++ board/glados/board.c
@@ -206,12 +206,25 @@ static void board_pmic_init(void)
 	if (system_jumped_to_this_image())
 		return;
 
+	/* Set CSDECAYEN / VCCIO decays to 0V at assertion of SLP_S0# */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x30, 0x4a);
+
+	/*
+	 * Set V100ACNT / V1.00A Control Register:
+	 * Lower power mode = 0.85V.
+	 * Nominal output = 1.0V.
+	 */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x37, 0x5a);
+
 	/*
 	 * Set V085ACNT / V0.85A Control Register:
 	 * Lower power mode = 0.7V.
 	 * Nominal output = 1.0V.
 	 */
 	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x38, 0x7a);
+
+	/* VRMODECTRL - enable low-power mode for V0.85A and V1.00A */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x3b, 0x15);
 }
 DECLARE_HOOK(HOOK_INIT, board_pmic_init, HOOK_PRIO_DEFAULT);
 
