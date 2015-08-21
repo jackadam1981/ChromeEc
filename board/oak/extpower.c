7fe5449112ab1852ae9c06bf5b06092ad1b7c112
Ben Lok <ben.lok@mediatek.com>
2015-08-21 19:28:32 +0800

    oak: shutdown tmp432 if AC isn't present in non-S0 state
    To reduce the power consumption in non-S0 AP power state,
    shutdown tmp432 if external power isn't present.
    
    BRANCH=none
    BUG=chrome-os-partner:44170
    TEST=manual
    1. make BOARD=oak -j
    2. shutdown AP by EC console command:
       > apshutdown
    3. plug external power
    4. check whether tmp432 is still running:
       > tmp432
    5. unplug external power
    6. check whether tmp432 is shutdown:
       > tmp432
    
    Change-Id: I4726a18c8754dbe60070d878dff143c76d586dcc
    Signed-off-by: Ben Lok <ben.lok@mediatek.com>

--- board/oak/extpower.c
+++ board/oak/extpower.c
@@ -12,13 +12,39 @@
 #include "common.h"
 #include "gpio.h"
 #include "hooks.h"
+#include "host_command.h"
 #include "system.h"
 #include "util.h"
+
+#define EXTPOWER_DEBOUNCE_US  (30 * MSEC)
+
+static int debounced_extpower_presence;
 
 int extpower_is_present(void)
 {
 	return gpio_get_level(GPIO_AC_PRESENT);
 }
+
+/**
+ * Deferred function to handle external power change
+ */
+static void extpower_deferred(void)
+{
+        int extpower_presence = gpio_get_level(GPIO_AC_PRESENT);
+
+        if (extpower_presence == debounced_extpower_presence)
+                return;
+
+        debounced_extpower_presence = extpower_presence;
+        hook_notify(HOOK_AC_CHANGE);
+
+        /* Forward notification to host */
+        if (extpower_presence)
+                host_set_single_event(EC_HOST_EVENT_AC_CONNECTED);
+        else
+                host_set_single_event(EC_HOST_EVENT_AC_DISCONNECTED);
+}
+DECLARE_DEFERRED(extpower_deferred);
 
 static void extpower_buffer_to_soc(void)
 {
@@ -39,10 +65,22 @@ void extpower_interrupt(enum gpio_signal signal)
 {
 	/* Trigger notification of external power change */
 	extpower_buffer_to_soc();
+	/* Trigger deferred notification of external power change */
+	hook_call_deferred(extpower_deferred, EXTPOWER_DEBOUNCE_US);
 }
 
 static void extpower_init(void)
 {
+	uint8_t *memmap_batt_flags = host_get_memmap(EC_MEMMAP_BATT_FLAG);
+
+	debounced_extpower_presence = gpio_get_level(GPIO_AC_PRESENT);
+
+	/* Initialize the memory-mapped AC_PRESENT flag */
+	if (debounced_extpower_presence)
+		*memmap_batt_flags |= EC_BATT_FLAG_AC_PRESENT;
+	else
+		*memmap_batt_flags &= ~EC_BATT_FLAG_AC_PRESENT;
+
 	extpower_buffer_to_soc();
 
 	/* Enable interrupts, now that we've initialized */
