14600c23c3f836920e67e0a8541484d434079f3f
Vadim Bendebury <vbendeb@chromium.org>
2015-08-22 20:23:23 -0700

    cr50: provision signer tool build
    what happened
    
    BUG=chromium:
    BUG=chrome-os-partner:
    TEST=manual
    
    Change-Id: Iacc22561de67fadfaf8e049bf9578cbd08cfad86
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- chip/g/build.mk
+++ chip/g/build.mk
@@ -33,3 +33,7 @@ chip-$(CONFIG_USB_CONSOLE)+=usb_console.o
 chip-$(CONFIG_USB_HID)+=usb_hid.o
 # TODO(wfrichar): Document this (and all other CONFIG_USB_*) in config.h
 chip-$(CONFIG_USB_BLOB)+=usb_blob.o
+
+$(out)/RO/ec.RO.flat: $(out)/util/signer
+
+$(out)/RO/ec.RO.hex: $(out)/RO/ec.RO.flat.signed
