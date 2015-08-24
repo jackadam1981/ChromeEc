d228e520d162a5246261fd79fa6864b7266f9bf2
Ben Lok <ben.lok@mediatek.com>
2015-08-24 22:35:14 +0800

    oak: usb_pd: avoid broken SS connection during enter DP mode.
    For type-c multi-function accessory (for example: apple's HDMI adapter,
    or type-c dock), it has SS & DP connection. The SS connection may be
    established before enter DP mode. The SS connection will be broken if
    the mux is disabled during enter DP mode. So, it needs to check the mux
    status and avoid to disable the mux if SS connection is already existed.
    
    BRANCH:none
    BUG=chrome-os-partner:43096
    TEST=Manual on Oak rev3
    1. connect apple type-C HDMI adapter to right side port(C0).
    2. list usb devices in AP console:
       > lsusb
       Bus 002 Device 017: ID 05ac:100e Apple, Inc.
    3. confirm that apple's device is present.
    
    Change-Id: I47a52076de45da9f9a78d114f20c4ff6bb4aa2bc
    Signed-off-by: Ben Lok <ben.lok@mediatek.com>

--- board/oak/usb_pd_policy.c
+++ board/oak/usb_pd_policy.c
@@ -242,11 +242,22 @@ static uint32_t dp_status[CONFIG_USB_PD_PORT_COUNT];
 
 static void svdm_safe_dp_mode(int port)
 {
+	const char *dp_str, *usb_str;
 	/* make DP interface safe until configure */
 	dp_flags[port] = 0;
 	dp_status[port] = 0;
-	usb_mux_set(port, TYPEC_MUX_NONE,
-			USB_SWITCH_CONNECT, pd_get_polarity(port));
+
+	/* Get current status of the SS mux */
+	usb_mux_get(port, &dp_str, &usb_str);
+	/*
+	 * Check current status, due to the mux may be switched to SS
+	 * and SS device was attached before (for example: Type-C dock).
+	 * To avoid broken the SS connection,
+	 * keep the current setting if SS connection is enabled already.
+	 */
+	if (usb_str == NULL)
+		usb_mux_set(port, TYPEC_MUX_NONE,
+			    USB_SWITCH_CONNECT, pd_get_polarity(port));
 }
 
 static int svdm_enter_dp_mode(int port, uint32_t mode_caps)
