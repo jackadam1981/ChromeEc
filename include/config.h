7abbd3e6541b5d719420658f2119ab6be3e60686
Gabe Noblesmith <gabe.noblesmith@fairchildsemi.com>
2015-08-28 10:34:47 -0400

    Initial commit of TCPM driver for FUSB302.
    BUG=none
    BRANCH=none
    TEST=PD contract established with various devices
    
    Change-Id: I4b452befe9ccd9d67bd6ad5c8cf77ae58320f6af
    Signed-off-by: Gabe Noblesmith <gabe.noblesmith@fairchildsemi.com>

--- include/config.h
+++ include/config.h
@@ -1751,6 +1751,7 @@
  */
 #undef CONFIG_USB_PD_TCPM_STUB
 #undef CONFIG_USB_PD_TCPM_TCPCI
+#undef CONFIG_USB_PD_TCPM_FUSB302
 
 /*
  * Use this option if the TCPC port controller is on a seperate chip from
