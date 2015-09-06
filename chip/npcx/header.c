80050613ab992ead3bbe9fbb849d81b372f84435
Shawn Nematbakhsh <shawnn@chromium.org>
2015-09-05 17:27:37 -0700

    Geometry constant cleanup Pt.2
    DO NOT COMMIT / In progress
    
    Expunge CDRAM / CODERAM configs:
    NPCX:
    
    MAPPED_STORAGE_BASE ==> 0x64000000 (old PROGRAM_MEMORY_BASE)
    PROGRAM_MEMORY_BASE ==> 0x100A8000 (old CDRAM_BASE)
    
    Change-Id: I0e054ab4c939f9dcf54abee8e5ebd9b2e42fe9c4

--- chip/npcx/header.c
+++ chip/npcx/header.c
@@ -11,8 +11,9 @@
  */
 
 #include <stdint.h>
+
+#include "config.h"
 #include "registers.h"
-#include "config_chip.h"
 
 /* Signature used by fw header */
 #define SIG_FW_EC           0x2A3B4D5E
@@ -24,7 +25,8 @@
 #define ERROR_DETECTION_DIS 0x00
 
 /* Code RAM addresses use by header */
-#define FW_START_ADDR  CONFIG_CDRAM_BASE /* Put FW at the begin of CODE RAM   */
+/* Put FW at the begin of CODE RAM */
+#define FW_START_ADDR  CONFIG_PROGRAM_MEMORY_BASE 
 
 /* TODO: It will be filled automatically by ECST */
 /* The entry point of reset handler (filled by ECST tool)*/
