270cbec87575c6110b61ca590ebec16740882634
Gwendal Grignou <gwendal@chromium.org>
2015-09-08 11:34:42 -0700

    common: accel: Add error code for irq handler
    When IRQ handler is not processing any event raised,
    return NOT_HANDLED.
    
    Without this change, any event would set the light sensor
    process timestamp and, if the light sensor frequency was lower
    than BM160 fifo interrupt frequency, we would never read from
    the light sensor.
    
    BRANCH=smaug
    BUG=chrome-os-partner:43800
    TEST=Compile. Check that light sensor data get updated.
    
    Change-Id: I302f80c5cd9b4f3c926362fdafdc8b5074cabb60
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- include/accelgyro.h
+++ include/accelgyro.h
@@ -105,6 +105,9 @@ struct accelgyro_drv {
 	 * process the events that triggered an interrupt.
 	 * @s Pointer to sensor data.
 	 * @event Event to process. May add add other events for the next processor.
+	 *
+	 * Return EC_SUCCESS when one event is handled, EC_ERROR_NOT_HANDLED
+	 * when no events have been proccessed.
 	 */
 	int (*irq_handler)(struct motion_sensor_t *s, uint32_t *event);
 #endif
