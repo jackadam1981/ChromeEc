10b1a73f0a74cef6f169737383916e1408af1c87
Vijay Hiremath <vijay.p.hiremath@intel.com>
2015-09-08 18:27:08 -0700

    Kunimitsu: PMIC: Emergency reset on long press of power button
    Configured the power button configuration register to issue an emergency
    reset on long press of the power button (8s time).
    
    BUG=none
    TEST=Hold the power button for 8s and DUT turns off the DSW rails.
    BRANCH=none
    
    Change-Id: Ibc0064e8ee88127cc6e3e418b97370e617b61b1d
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- board/kunimitsu/board.c
+++ board/kunimitsu/board.c
@@ -338,6 +338,17 @@ static void board_pmic_init(void)
 	if (ret)
 		goto pmic_error;
 
+	/*
+	 * Power button configuration
+	 * [7]  :    0b Power button debounce time, 30ms
+	 * [6]  :    0b Reset of power button timer logic, no action
+	 * [5:0]: 1000b Time that the button must be held to force an
+	 *              emergency reset, 8s
+	 */
+	ret = I2C_PMIC_WRITE(TPS650830_REG_PBCONFIG, 0x08);
+	if (ret)
+		goto pmic_error;
+
 	CPRINTS("PMIC initialisation done");
 	return;
 
