c1f9d40769eea9df507356d5a42e08de77f711ce
Vincent Palatin <vpalatin@chromium.org>
2015-09-10 14:05:48 -0700

    pd: add option to save power in S5
    When we are in sink mode and not dual-role toggling, add the option to
    disable the CC lines polling every 10 ms (thus waking up on the VBUS
    transition or the 1-minute slow polling).
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=smaug
    BUG=chrome-os-partner:44229
    TEST=on Smaug DVT, measure power consumption in S5 and wake-up time when
    plugging a power supply.
    
    Change-Id: Idee6581af550de01760feffe604d7bd453a625a8
    Reviewed-on: https://chromium-review.googlesource.com/299022
    Reviewed-by: Alec Berg <alecaberg@chromium.org>
    Commit-Queue: Vincent Palatin <vpalatin@chromium.org>
    Trybot-Ready: Vincent Palatin <vpalatin@chromium.org>
    Tested-by: Vincent Palatin <vpalatin@chromium.org>
    (cherry picked from commit d14a0045568e61a36695ffee48ed39ddc60ebb3a)

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -1921,7 +1921,12 @@ void pd_task(void)
 #endif
 			break;
 		case PD_STATE_SNK_DISCONNECTED:
+#ifdef CONFIG_USB_PD_LOW_POWER
+			timeout = drp_state == PD_DRP_TOGGLE_OFF ? MINUTE
+								 : 10*MSEC;
+#else
 			timeout = 10*MSEC;
+#endif
 			tcpm_get_cc(port, &cc1, &cc2);
 
 			/* Source connection monitoring */
@@ -1934,6 +1939,7 @@ void pd_task(void)
 							PD_T_CC_DEBOUNCE;
 				set_state(port,
 					PD_STATE_SNK_DISCONNECTED_DEBOUNCE);
+				timeout = 10*MSEC;
 				break;
 			}
 
