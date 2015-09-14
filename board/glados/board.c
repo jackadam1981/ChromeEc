bb49f3ca7dd6d7eb057958ecbfb88ecde82cf9a9
Aseda Aboagye <aaboagye@google.com>
2015-09-14 14:14:25 -0700

    GLaDOS: Add base accel & gyro.
    This commit adds the base accelerometer as well as the gyroscope to the
    list of motion sensors on the board.  They are currently wrapped behind an
    ifdef for HAS_TASK_MOTIONSENSE due to space constraints.
    
    BUG=chrome-os-partner:43494
    BRANCH=None
    TEST=Build GLaDOS EC with driver enabled and verify that we can calcuate
    a valid lid angle.
    TEST=Verify that signs of accelerometer conform to those shown in the
    Chrome/Android/HTML5 doc/spec. See description in accelerometer_types.h
    TEST=Verify that signs of gyroscope conform to those shown in the
    "Sysfs interface to EC accelerometers" document.
    TEST=make buildall tests
    
    CQ-DEPEND=CL:299504
    CQ-DEPEND=CL:300510
    
    Change-Id: I34026813431f0df6211f9c781ab5b8c7b4dd8403
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/glados/board.c
+++ board/glados/board.c
@@ -17,6 +17,7 @@
 #include "driver/als_opt3001.h"
 #include "driver/accel_kionix.h"
 #include "driver/accel_kx022.h"
+#include "driver/accelgyro_bmi160.h"
 #include "extpower.h"
 #include "gpio.h"
 #include "hooks.h"
@@ -427,16 +428,22 @@ static void board_handle_reboot(void)
 }
 DECLARE_HOOK(HOOK_INIT, board_handle_reboot, HOOK_PRIO_FIRST);
 
-/* Motion sensors */
 #ifdef HAS_TASK_MOTIONSENSE
-/* Lid Sensor mutex */
+/* Motion sensors */
+/* Mutexes */
 static struct mutex g_lid_mutex;
+static struct mutex g_base_mutex;
 
 /* KX022 private data */
 struct kionix_accel_data g_kx022_data = {
 	.variant = KX022,
 };
 
+const matrix_3x3_t base_standard_ref = {
+	{FLOAT_TO_FP(1),  0,  0},
+	{ 0,  FLOAT_TO_FP(1), 0},
+	{ 0,  0, FLOAT_TO_FP(1)}
+};
 const matrix_3x3_t lid_standard_ref = {
 	{FLOAT_TO_FP(1),  0,  0},
 	{ 0,  FLOAT_TO_FP(1), 0},
@@ -444,6 +451,81 @@ const matrix_3x3_t lid_standard_ref = {
 };
 
 struct motion_sensor_t motion_sensors[] = {
+	/*
+	 * Note: bmi160: supports accelerometer and gyro sensor
+	 * Requirement: accelerometer sensor must init before gyro sensor
+	 * DO NOT change the order of the following table.
+	 */
+	{.name = "Base Accel",
+	 .active_mask = SENSOR_ACTIVE_S0,
+	 .chip = MOTIONSENSE_CHIP_BMI160,
+	 .type = MOTIONSENSE_TYPE_ACCEL,
+	 .location = MOTIONSENSE_LOC_BASE,
+	 .drv = &bmi160_drv,
+	 .mutex = &g_base_mutex,
+	 .drv_data = &g_bmi160_data,
+	 .addr = BMI160_ADDR0,
+	 .rot_standard_ref = &base_standard_ref,
+	 .default_range = 2,  /* g, enough for laptop. */
+	 .config = {
+		 /* AP: by default use EC settings */
+		 [SENSOR_CONFIG_AP] = {
+			 .odr = 0,
+			 .ec_rate = 0,
+		 },
+		 /* EC use accel for angle detection */
+		 [SENSOR_CONFIG_EC_S0] = {
+			 .odr = 10000 | ROUND_UP_FLAG,
+			 .ec_rate = 100,
+		 },
+		 /* Sensor off in S3/S5 */
+		 [SENSOR_CONFIG_EC_S3] = {
+			 .odr = 0,
+			 .ec_rate = 0
+		 },
+		 /* Sensor off in S3/S5 */
+		 [SENSOR_CONFIG_EC_S5] = {
+			 .odr = 0,
+			 .ec_rate = 0
+		 },
+	 },
+	},
+
+	{.name = "Base Gyro",
+	 .active_mask = SENSOR_ACTIVE_S0,
+	 .chip = MOTIONSENSE_CHIP_BMI160,
+	 .type = MOTIONSENSE_TYPE_GYRO,
+	 .location = MOTIONSENSE_LOC_BASE,
+	 .drv = &bmi160_drv,
+	 .mutex = &g_base_mutex,
+	 .drv_data = &g_bmi160_data,
+	 .addr = BMI160_ADDR0,
+	 .default_range = 1000, /* dps */
+	 .rot_standard_ref = &base_standard_ref,
+	 .config = {
+		 /* AP: by default shutdown all sensors */
+		 [SENSOR_CONFIG_AP] = {
+			 .odr = 0,
+			 .ec_rate = 0,
+		 },
+		 /* EC does not need in S0 */
+		 [SENSOR_CONFIG_EC_S0] = {
+			 .odr = 0,
+			 .ec_rate = 0,
+		 },
+		 /* Sensor off in S3/S5 */
+		 [SENSOR_CONFIG_EC_S3] = {
+			 .odr = 0,
+			 .ec_rate = 0,
+		 },
+		 /* Sensor off in S3/S5 */
+		 [SENSOR_CONFIG_EC_S5] = {
+			 .odr = 0,
+			 .ec_rate = 0,
+		 },
+	 },
+	},
+
 	{.name = "Lid Accel",
 	 .active_mask = SENSOR_ACTIVE_S0,
 	 .chip = MOTIONSENSE_CHIP_KX022,
