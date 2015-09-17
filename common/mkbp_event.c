ae42b1ec02501427c3c7617883e2e22bf7634ef8
Ben Lok <ben.lok@mediatek.com>
2015-09-17 13:31:51 +0800

    mkbp_event: prevent power connector insertion related events to wakeup AP.
    It may use MKBP event to send PD power change event to AP by interrupt.
    According to the spec, For power connector insertion, it does not allow to
    wake up AP in S3, but, internal keyboard, lid ... does. So, prevent interrupt
    AP in non-S0 state if the event is defined in NON_WAKEUP_EVENT_MASK.
    
    BRANCH=none
    BUG=chrome-os-partner:45127
    TEST=manual
    Plug PD power adapter to oak, if system is in S3/S5 and it should
    starts charging, but should not wake up system/AP at all.
    
    Change-Id: I2f86697d5d3bd24d7de840e21064b91e8841f0eb
    Signed-off-by: Ben Lok <ben.lok@mediatek.com>

--- common/mkbp_event.c
+++ common/mkbp_event.c
@@ -6,6 +6,7 @@
  */
 
 #include "atomic.h"
+#include "chipset.h"
 #include "gpio.h"
 #include "host_command.h"
 #include "link_defs.h"
@@ -40,7 +41,16 @@ static void set_host_interrupt(int active)
 
 void mkbp_send_event(uint8_t event_type)
 {
+	uint32_t events;
 	set_event(event_type);
+
+	events = *(uint32_t *)host_get_memmap(EC_MEMMAP_HOST_EVENTS);
+	/* blocking specific events to interrupt AP in non-S0 state */
+	if (event_type == EC_MKBP_EVENT_HOST_EVENT &&
+	    !chipset_in_state(CHIPSET_STATE_ON) &&
+	    (events & NON_WAKEUP_EVENT_MASK))
+		return;
+
 	set_host_interrupt(1);
 }
 
