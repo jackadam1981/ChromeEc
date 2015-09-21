e58a5d6d4cd2366ad29a7ca8ed0373389014bd1a
Vincent Palatin <vpalatin@chromium.org>
2015-09-21 14:00:15 -0700

    ryu: delay VCONN power-up until next VBUS transition
    When the USB PD state machine wants to turn on VCONN,
    record the expected action and do it only on the next VBUS edge in order
    to avoid turning on VCONN before VBUS.
    Still turn off VCONN immediatly.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=smaug
    BUG=chrome-os-partner:40160
    TEST=On Ryu DVT, scope VBUS and VCONN sequences when plugging a
    DingDong.
    
    Change-Id: I060aef1a5b21016ebbcf4a56f12c199e27070155

--- board/ryu/usb_pd_config.h
+++ board/ryu/usb_pd_config.h
@@ -191,11 +191,20 @@ static inline int pd_adc_read(int port, int cc)
 		return adc_read_channel(ADC_CC2_PD);
 }
 
+/* record we need to switch on VCONN */
+extern int delayed_vconn;
+
 static inline void pd_set_vconn(int port, int polarity, int enable)
 {
-	/* Set VCONN on the opposite CC line from the polarity */
-	gpio_set_level(polarity ? GPIO_USBC_VCONN1_EN_L :
-				  GPIO_USBC_VCONN2_EN_L, !enable);
+	/* Never turn on VCONN before seeing VBUS */
+	if (!enable || gpio_get_level(CHGR_ACOK)) {
+		delayed_vconn = -1;
+		/* Set VCONN on the opposite CC line from the polarity */
+		gpio_set_level(polarity ? GPIO_USBC_VCONN1_EN_L :
+					  GPIO_USBC_VCONN2_EN_L, !enable);
+	} else { /* record to turn it on later when VBUS appears */
+		delayed_vconn = polarity;
+	}
 }
 
 #endif /* __USB_PD_CONFIG_H */
