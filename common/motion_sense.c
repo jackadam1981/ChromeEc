e2cb73b15dd95315e38d17832d1414e92fd1bc65
Furquan Shaikh <furquan@google.com>
2015-09-23 08:44:27 -0700

    motion_sense: Fix issues in SET_ACTIVITY
    ret always evaluated to INVALID_PARAM and so SET_ACTIVITY bailed out
    early and returned an error to host command.
    
    BUG=chrome-os-partner:45710
    BRANCH=None
    TEST=Compiles successfully. Disable double-tap works as expected.
    
    Change-Id: I58ae9cd5009fadedb3ea78a4eec0452124747707
    Signed-off-by: Furquan Shaikh <furquan@google.com>

--- common/motion_sense.c
+++ common/motion_sense.c
@@ -1052,6 +1052,7 @@ static int host_cmd_motion_sense(struct host_cmd_handler_args *args)
 		   MOTION_SENSE_ACTIVITY_SENSOR_ID)
 			return EC_RES_INVALID_PARAM;
 		mask = CONFIG_GESTURE_DETECTION_MASK;
+		ret = EC_RES_SUCCESS;
 		while (mask && ret == EC_RES_SUCCESS) {
 			i = get_next_bit(&mask);
 			sensor = &motion_sensors[i];
