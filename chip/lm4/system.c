f8cdf20d504d984ddc4fdd92283f93b44a2d5ad3
David Huang <David.Huang@quantatw.com>
2015-09-25 10:06:18 +0800

    Leon: Add special hibernate for Leon G781 alarm
    Add special hibernate function alert_system_hibernate() and
    alert_hibernate for G781 alarm not support wake up from hibernate.
    
    BRANCH=leon
    BUG=chrome-os-partner:45786
    TEST=None
    
    Change-Id: I93e2c3239780be8ac2849ab39ae754380c87b5a9
    Signed-off-by: David Huang <David.Huang@quantatw.com>

--- chip/lm4/system.c
+++ chip/lm4/system.c
@@ -381,6 +381,37 @@ void system_hibernate(uint32_t seconds, uint32_t microseconds)
 	hibernate(seconds, microseconds, HIBDATA_WAKE_PIN);
 }
 
+static void alert_hibernate(void)
+{
+	uint32_t hibctl;
+
+	/* Set EC enter hibernate and not support wake up */
+	hibctl = LM4_HIBERNATE_HIBCTL;
+
+	/* Set pin wake up disable */
+	hibctl &= ~ LM4_HIBCTL_PINWEN;
+
+	/* Set RTC wake up enable but not set wake up time
+	 * for EC not wake up by RTC timer
+	 */
+	hibctl |=LM4_HIBCTL_RTCWEN;
+
+	wait_for_hibctl_wc();
+	LM4_HIBERNATE_HIBCTL = hibctl;
+
+	/* Store hibernate flags */
+	hibdata_write(HIBDATA_INDEX_WAKE, 0);
+
+	__enter_hibernate(hibctl | LM4_HIBCTL_HIBREQ);
+}
+
+void alert_system_hibernate(void)
+{
+	/* Flush console before hibernating */
+	cflush();
+	alert_hibernate();
+}
+
 void system_pre_init(void)
 {
 	uint32_t hibctl;
