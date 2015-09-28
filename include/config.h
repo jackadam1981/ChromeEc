85aaed2b7b68d022c89b1a2a2cbcd386b6939a54
Gwendal Grignou <gwendal@chromium.org>
2015-09-28 13:17:19 -0700

    driver: bmm150: Add ability to smooth data from compass.
    To fight the noise inside the device,
    implement a low bypass filter using the expodential moving avarage method.
    http://en.wikipedia.org/wiki/Moving_average#Exponential_moving_average
    
    Define only for axis that need it.
    
    BRANCH=smaug
    BUG=chrome-os-partner:45436
    TEST=Using androsensor, check the data on the X and Z axis is smoother.
    
    Change-Id: I1724375d0ed92dfe9066db5d710ece30af56d578
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1262,6 +1262,16 @@
 /* Presence of a Bosh Sensortec BMM150 magnetometer behind a BMI160. */
 #undef CONFIG_MAG_BMI160_BMM150
 
+/*
+ * Smooth magnetometer input:
+ * If defined the number of samples a given sample have effect on.
+ *
+ * It is defined in the axis of the compass, before device rotation.
+ */
+#undef CONFIG_MAG_X_SMOOTH_SAMPLES
+#undef CONFIG_MAG_Y_SMOOTH_SAMPLES
+#undef CONFIG_MAG_Z_SMOOTH_SAMPLES
+
 /* Microchip EC SRAM start address */
 #undef CONFIG_MEC_SRAM_BASE_START
 
