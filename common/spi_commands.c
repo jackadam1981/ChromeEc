842bcbefdc24356fd318b5359998086751eb830a
Gwendal Grignou <gwendal@chromium.org>
2015-10-14 10:14:24 -0700

    common: spi: Add a delay after write commands.
    Some SPI slave devices need a delay to digest write commands. (BMI160).
    Add a 1ms delay in the write command.
    
    BRANCH=smaug
    BUG=none
    TEST=Check on the logic analyzer that there is ~1.5ms delay between back
    to back spixfer w ... commands.
    
    Change-Id: I7cc6ed0da9ae39550e58457b9431eb01b5ab36d8
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- common/spi_commands.c
+++ common/spi_commands.c
@@ -9,6 +9,7 @@
 #include "common.h"
 #include "console.h"
 #include "spi.h"
+#include "timer.h"
 #include "util.h"
 
 static int command_spixfer(int argc, char **argv)
@@ -52,6 +53,7 @@ static int command_spixfer(int argc, char **argv)
 		uint8_t cmd[2] = { offset, v };
 
 		rv = spi_transaction(&spi_devices[dev_id], cmd, 2, NULL, 0);
+		msleep(1);
 	} else {
 		return EC_ERROR_PARAM1;
 	}
