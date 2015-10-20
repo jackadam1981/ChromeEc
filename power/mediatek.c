e7ae99dd1ff965c15a16b55c469d809545b03513
Ben Lok <ben.lok@mediatek.com>
2015-10-20 19:48:29 +0800

    oak: ensure PMIC power button is released after SYSJUMP.
    There is a race condition between SYSJUMP and function
    release_pmic_pwron_deferred().
    Process of EC SW Sync will delay the execution time of
    release_pmic_pwron_deferred(). PMIC will shutdown the power, if
    PMIC power button can not be released within 8 seconds (depends
    on PMIC spec). In order to ensure PMIC power button will be
    released in time, just release it after SYSJUMP.
    
    BUG=chrome-os-partner:46392
    BRANCH=none
    TEST=make buildall -j;
    Enable EC SW sync and normal mode in coreboot,
    Kernel should bootup successfully.
    
    Change-Id: I45d4aa0f0d4280e68282ea11ccfda05201f88aae
    Signed-off-by: Ben Lok <ben.lok@mediatek.com>

--- power/mediatek.c
+++ power/mediatek.c
@@ -416,6 +416,18 @@ enum power_state power_chipset_init(void)
 		/* In the SYSJUMP case, we check if the AP is on */
 		if (is_power_good_asserted()) {
 			CPRINTS("SOC ON");
+			/*
+			 * Ensure PMIC power button is released, because:
+			 * SYSJUMP may delay the execution time of
+			 * release_pmic_pwron_deferred(), and let it can not be
+			 * excuted in time, if SYSJUMP is executed before it.
+			 * It is a good timing to release PMIC power key here,
+			 * because power from PMIC is stable after EC SW sync,
+			 * and the PMIC power key should be released.
+			 */
+			if (gpio_get_level(GPIO_PMIC_PWRON_H))
+				set_pmic_pwron(0);
+
 			init_power_state = POWER_S0;
 			if (is_suspend_asserted())
 				enable_sleep(SLEEP_MASK_AP_RUN);
