8340aca4fecabd632ec427f1496798696f04b7e4
Vadim Bendebury <vbendeb@chromium.org>
2015-11-12 16:44:46 -0800

    add 'backdoor' command framework
    This patch introduces a facility which would allow to register
    callbacks for arbitrary commands.
    
    Registered commands expect to receive a sized buffer, containing the
    command's argument and a sized buffer to return the result.
    
    This is going to be used for testing, when various test commands are
    multiplexed over the existing protocol.
    
    BRANCH=none
    BUG=chrome-os-partner:47524
    TEST=none yet
    
    Change-Id: I8ae16a78ca7d72176a5e7f74dd7a232078e7c06c
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- include/tpm_registers.h
+++ include/tpm_registers.h
@@ -35,6 +35,7 @@ struct tpm_cmd_header {
 	uint16_t tag;
 	uint32_t size;
 	uint32_t command_code;
+	uint16_t subcommand_code;  /* Not a standard field. */
 } __packed;
 
 #endif	/* __CROS_EC_TPM_REGISTERS_H */
