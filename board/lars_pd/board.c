9acb0bbd1626132e1dcfc1ad2c0cd423eacd59f9
Ryan Zhang <Ryan.Zhang@quantatw.com>
2015-12-02 21:07:53 +0800

    Lars: Remove second port of PD firmware
    two port PD will keep interrupt low, and cause
    EC.PDCMD task stuck with exchange status loop before
    entering task-while-loop
    
    BUG=chrome-os-partner:48232
    BRANCH=lars
    TEST=`make BOARD=lars -j`, OS can boot up normally
    
    Change-Id: I493c6d02170c731af430f28abf8ade38b47aff0f
    Signed-off-by: Ryan Zhang <Ryan.Zhang@quantatw.com>

--- board/lars_pd/board.c
+++ board/lars_pd/board.c
@@ -53,7 +53,6 @@ static void board_init(void)
 {
 	/* Enable interrupts on VBUS transitions. */
 	gpio_enable_interrupt(GPIO_USB_C0_VBUS_WAKE_L);
-	gpio_enable_interrupt(GPIO_USB_C1_VBUS_WAKE_L);
 
 	/* Set PD MCU system status bits */
 	if (system_jumped_to_this_image())
@@ -66,10 +65,8 @@ DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 /* ADC channels */
 const struct adc_t adc_channels[] = {
 	/* USB PD CC lines sensing. Converted to mV (3300mV/4096). */
-	[ADC_C1_CC1_PD] = {"C1_CC1_PD", 3300, 4096, 0, STM32_AIN(0)},
 	[ADC_C0_CC1_PD] = {"C0_CC1_PD", 3300, 4096, 0, STM32_AIN(2)},
 	[ADC_C0_CC2_PD] = {"C0_CC2_PD", 3300, 4096, 0, STM32_AIN(4)},
-	[ADC_C1_CC2_PD] = {"C1_CC2_PD", 3300, 4096, 0, STM32_AIN(5)},
 };
 BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
