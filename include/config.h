a5621e1a5f1923a225b818c831792c5f3a8eb5d0
Duncan Laurie <dlaurie@chromium.org>
2015-12-01 18:42:23 -0800

    ec: Add temporary secure storage for the host during boot
    This adds a temporary secure storage interface for the EC to be able
    to store small amounts of data from the host that is locked until the
    chipset resets.  This is used by pre-memory verified boot on x86 systems
    where we need to know which RW slot to boot and what the hash is to
    ensure that we can resume from S3 safely.
    
    BUG=chrome-os-partner:46049
    BRANCH=none
    TEST=tested on glados and samus
    
    Change-Id: I5fa91046437479bcae69a8fca4c989b0ef554bbf
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1963,6 +1963,12 @@
 /* Support computing hash of code for verified boot */
 #undef CONFIG_VBOOT_HASH
 
+/* Support for secure temporary storage for verified boot */
+#undef CONFIG_VSTORE
+
+/* Number of supported slots for secure temporary storage */
+#undef CONFIG_VSTORE_SLOTS
+
 /*****************************************************************************/
 /* Watchdog config */
 
