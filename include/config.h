d7b8dc0566c08dff7555fb1acac6b68d183a6258
Ben Lok <ben.lok@mediatek.com>
2015-12-09 13:44:38 +0800

    pd: add audio/debug accessory mode notification
    In order to notify application processor the audio/debug
    accessory is present or not, add PD event notification for
    audio and debug accessory.
    
    BUG=chrome-os-partner:47892
    BRANCH=None
    TEST=Manual on oak. Using Twinkie, advertise Ra/Ra.
    Device reports that analog audio adapters are unsupported.
    
    Change-Id: Id2a79bda2b05e3ab041252b369669a825944f66a
    Signed-off-by: Ben Lok <ben.lok@mediatek.com>

--- include/config.h
+++ include/config.h
@@ -1835,6 +1835,9 @@
 /* Use DAC as reference for comparator at 850mV. */
 #undef CONFIG_PD_USE_DAC_AS_REF
 
+/* Use this option if need to notify the PD accessory mode to SoC */
+#undef CONFIG_PD_ACC_MODE_NOTIFY
+
 /* USB Product ID. */
 #undef CONFIG_USB_PID
 
