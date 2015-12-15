d2e26b259f86946dfb29c62dc670dcfa189b922a
Ryan Zhang <Ryan.Zhang@quantatw.com>
2015-12-15 18:05:33 +0800

    battery
    Change-Id: I1bb8bdc21e969d28447d50d95b23292335eab628

--- board/lars/battery.c
+++ board/lars/battery.c
@@ -13,10 +13,10 @@
 #define SB_SHUTDOWN_DATA	0x0010
 
 static const struct battery_info info = {
-	.voltage_max = 8700,/* mV */
-	.voltage_normal = 7600,
-	.voltage_min = 6000,
-	.precharge_current = 150,/* mA */
+	.voltage_max = 13200,/* mV */
+	.voltage_normal = 13050,
+	.voltage_min = 9000,
+	.precharge_current = 256,/* mA */
 	.start_charging_min_c = 0,
 	.start_charging_max_c = 45,
 	.charging_min_c = 0,
