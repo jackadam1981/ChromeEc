349ba1d40f88e23181a8ef9464f0dce59a911e3b
Ben Lok <ben.lok@mediatek.com>
2015-12-18 14:24:11 +0800

    oak: Enable TI OPT3001 ambient light sensor for rev5
    refer to commit 3f2dc441, enable light sensor driver for oak rev5
    
    Change-Id: I896cf99fd781f5d4d8dad206d43f5a3d8faeb9a2
    Signed-off-by: Ben Lok <ben.lok@mediatek.com>

--- board/oak/board.h
+++ board/oak/board.h
@@ -13,6 +13,12 @@
 
 #define CONFIG_ADC
 #undef  CONFIG_ADC_WATCHDOG
+
+/* Add for Ambient Light Sensor */
+#define CONFIG_ALS
+#define CONFIG_ALS_OPT3001
+#define CONFIG_CMD_ALS
+
 /* Add for AC adaptor, charger, battery */
 #define CONFIG_BATTERY_CUT_OFF
 #define CONFIG_BATTERY_PRESENT_GPIO GPIO_BAT_PRESENT_L
@@ -126,14 +132,18 @@
 #define KB_OUT_PORT_LIST GPIO_A, GPIO_B, GPIO_C, GPIO_D
 
 /* 2 I2C master ports, connect to battery, charger, pd and USB switches */
-#define I2C_PORT_MASTER 0
+#define I2C_PORT_MASTER  0
+#define I2C_PORT_ALS     0
 #define I2C_PORT_BATTERY 0
 #define I2C_PORT_CHARGER 0
 #define I2C_PORT_PERICOM 0
 #define I2C_PORT_THERMAL 0
-#define I2C_PORT_PD_MCU 1
+#define I2C_PORT_PD_MCU  1
 #define I2C_PORT_USB_MUX 1
-#define I2C_PORT_TCPC 1
+#define I2C_PORT_TCPC    1
+
+/* Ambient Light Sensor address */
+#define OPT3001_I2C_ADDR OPT3001_I2C_ADDR1
 
 /* Timer selection */
 #define TIM_CLOCK32 2
@@ -180,6 +190,13 @@ enum temp_sensor_id {
 	TEMP_SENSOR_BATTERY,
 
 	TEMP_SENSOR_COUNT
+};
+
+/* Light sensors */
+enum als_id {
+	ALS_OPT3001 = 0,
+
+	ALS_COUNT
 };
 
 /* start as a sink in case we have no other power supply/battery */
