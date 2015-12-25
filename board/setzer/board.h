3ade1faf148ec6e304a1fef36dcc72ef82de454c
Lennon Chen <Lennon_Chen@wistron.com>
2015-12-25 10:25:09 +0800

    setzer: Modify LED behavior
    BUG=none
    BRANCH=none
    TEST=make -j buildall/tests
    
    Change-Id: I22d3c1df5f105b5cfc58ed4d637b97f67cd7494a
    Signed-off-by: Lennon Chen <Lennon_Chen@wistron.com>

--- board/setzer/board.h
+++ board/setzer/board.h
@@ -120,9 +120,9 @@ enum power_signal {
 };
 
 enum pwm_channel {
-	PWM_CH_LED_RED,
-	PWM_CH_LED_BLUE,
-	PWM_CH_LED_GREEN,
+	PWM_CH_BAT_LED0,
+	PWM_CH_BAT_LED1,
+	PWM_CH_PWR_LED,
 	/* Number of PWM channels */
 	PWM_CH_COUNT
 };
