9dae99a1b825b89a783c6ba772539014238ff3d8
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-01-14 13:48:59 -0800

    Driver: ISL9237: Configure Prochot# Debounce time to maximum
    Configured the Prochot# Debounce time to maximum (1ms) to avoid false
    triggers on the Prochot pin.
    
    BUG=none
    TEST=Manually tested on Kunimitsu, Prochot pin in not triggered when
         the battery current ripples or the adapter current ripples are
         less than 1ms.
    BRANCH=none
    
    Change-Id: I3ccebcfbced2285dcacff8a3750090c0500a102c
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- driver/charger/isl9237.c
+++ driver/charger/isl9237.c
@@ -226,6 +226,17 @@ int charger_post_init(void)
 		return rv;
 #endif
 
+	/*
+	 * [10:9]: Prochot# Debounce time, 11: 1ms
+	 */
+	rv = raw_read16(ISL9237_REG_CONTROL2, &reg);
+	if (rv)
+		return rv;
+
+	rv = raw_write16(ISL9237_REG_CONTROL2, reg | (0x3 << 9));
+	if (rv)
+		return rv;
+
 	rv = charger_get_option(&reg);
 	if (rv)
 		return rv;
