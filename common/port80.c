943186530473dfec6aaf1ae2dee5ad7a28c5d436
james_chao <james_chao@asus.com>
2016-01-15 15:49:57 +0800

    system: add CONFIG_PORT80_DISABLE_PRINT_IN_INT
    Call print in port_80_write (called by lpc interrupt)
    maybe occur race condition.
    
    BUG=chrome-os-partner:49318
    BRANCH=firmware-strago-7287.B
    TEST=run the power_s5.conf stress test
    
    Change-Id: I29dc24ef6dc5f311aa203fb0e4ced35979b238b5
    Signed-off-by: james_chao <james_chao@asus.com>

--- common/port80.c
+++ common/port80.c
@@ -23,7 +23,11 @@ static uint16_t history[HISTORY_LEN];
 static int writes;    /* Number of port 80 writes so far */
 static int last_boot; /* Last code from previous boot */
 static int scroll;
+#ifdef CONFIG_PORT80_DISABLE_PRINT_IN_INT
+static int print_in_int;
+#else
 static int print_in_int = 1;
+#endif
 #ifdef HAS_TASK_PORT80
 static int task_en;   /* Port 80 task control */
 static int task_timeout = -1;
