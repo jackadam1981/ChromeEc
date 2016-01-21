4b67ebe8df629008523bd232de7b39da31d99e2d
JowieChung <paul.xu@paradetech.com>
2016-01-21 10:57:34 -0800

    Updated the TCPCI register 0x18 access and 0xD0 vendor define register for HPD control
    Change-Id: I2f34bbd5bf3331c33e1ca72345b9d81cfad1c6eb

--- common/usb_pd_policy.c
+++ common/usb_pd_policy.c
@@ -21,6 +21,7 @@
 #include "usb_api.h"
 #include "usb_pd.h"
 #include "version.h"
+#include "tcpm.h"
 
 #ifdef CONFIG_COMMON_RUNTIME
 #define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
@@ -433,7 +434,7 @@ static void dfp_consume_attention(int port, uint32_t *payload)
 	uint16_t svid = PD_VDO_VID(payload[0]);
 	int opos = PD_VDO_OPOS(payload[0]);
 	struct svdm_amode_data *modep = get_modep(port, svid);
-	int reg;
+
 
 	if (!modep || !validate_mode_request(modep, svid, opos))
 		return;
@@ -441,17 +442,10 @@ static void dfp_consume_attention(int port, uint32_t *payload)
 	if (modep->fx->attention)
 		modep->fx->attention(port, payload);
 
-	tcpc_read(port, 0xd0, &reg);
-//	tcpc_read(port, TCPC_REG_CC_STATUS, &reg);
-//	if ( ( reg & 0x03 ) == 0x02 )//CC1
-		{
-			tcpc_write(port, 0xd0 , reg | 0x80);
-		}
-//	else if ( ( reg & 0x0C ) == 0x08 )//CC2
-		{
-//			tcpc_write(port, 0xd0 , 0xA0);
-		}		
-	tcpc_write(port, 0xd1 , 0x80);//HPD
+        //MODE SWITCH
+	tcpc_write(port, 0x18, 0x08);
+        //HPD
+	tcpc_write(port, 0xd0, 0x01);
 }
 
 /*
