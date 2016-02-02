fef67761f6c97bdbea77327d8f2627cdbb6a221d
Ryan Zhang <Ryan.Zhang@quantatw.com>
2016-02-02 09:40:10 +0800

    Lars: Update TypeC Adapter settings
    Lars use 45W, 2.25A adapter and Acer asked for only allowing 90% of max
    current from input to protect Adapter and System.
    Current = 2.25*0.9 == 2.025
    
    BUG=none
    BRANCH=firmware-glados-7820.B
    TEST=`make -j buildall`, OS start normally, get input current = 2024mA
    from EC console.
    Signed-off-by: Ryan Zhang <Ryan.Zhang@quantatw.com>
    
    Change-Id: I6b99b4d8d1c869503752c5a190d1133cfd1b264f

--- board/lars/board.h
+++ board/lars/board.h
@@ -206,8 +206,8 @@ enum temp_sensor_id {
 
 /* Define typical operating power and max power */
 #define PD_OPERATING_POWER_MW 15000
-#define PD_MAX_POWER_MW       60000
-#define PD_MAX_CURRENT_MA     3000
+#define PD_MAX_POWER_MW       45000
+#define PD_MAX_CURRENT_MA     2025
 #define PD_MAX_VOLTAGE_MV     20000
 
 /* Reset PD MCU */
