513a2726780c06b757b639c231625f637527297d
Shawn Nematbakhsh <shawnn@chromium.org>
2016-02-05 16:27:25 -0800

    mec1322: timer16: Ensure timer interrupt is disabled before timer
    When the timer block is disabled and an interrupt fires simultaneously,
    the block clock is gated off, so the interrupt may be impossible to
    clear. Ensure that we disable our timer interrupt prior to disabling the
    timer so that we don't fall into this interrupt storm case.
    
    BUG=chrome-os-partner:48499
    TEST=Pass 2000s of continuous calls to port_80_interrupt_disable() /
    port_80_interrupt_enable() without WDT being triggered.
    BRANCH=glados
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: I6d073cb50122182bd3ad8cb7c20ed70089a0693b

--- chip/mec1322/port80.c
+++ chip/mec1322/port80.c
@@ -56,30 +56,31 @@ static void port_80_interrupt_init(void)
 	MEC1322_INT_SOURCE(23) = (1 << 1);
 	/* Enable IRQ vector 23. */
 	MEC1322_INT_BLK_EN |= (1 << 23);
-	/* Enable the interrupt. */
-	MEC1322_TMR16_IEN(1) |= 1;
+	/* Setup the interrupt. */
 	MEC1322_INT_ENABLE(23) = (1 << 1);
 	task_enable_irq(MEC1322_IRQ_TIMER16_1);
 
 	/* Enable and start the timer. */
 	MEC1322_TMR16_CTL(1) |= 1 | (1 << 5);
+	/* Enable the interrupt. */
+	MEC1322_TMR16_IEN(1) |= 1;
 }
 DECLARE_HOOK(HOOK_INIT, port_80_interrupt_init, HOOK_PRIO_DEFAULT);
 
 static void port_80_interrupt_enable(void)
 {
-	/* Enable the interrupt. */
+	/* Enable the timer block and its interrupt. */
 	task_enable_irq(MEC1322_IRQ_TIMER16_1);
-	/* Enable the timer block. */
 	MEC1322_TMR16_CTL(1) |= 1;
+	MEC1322_TMR16_IEN(1) |= 1;
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, port_80_interrupt_enable, HOOK_PRIO_DEFAULT);
 
 static void port_80_interrupt_disable(void)
 {
-	/* Disable the timer block. */
+	/* Disable the timer block and its interrupt. */
+	MEC1322_TMR16_IEN(1) &= ~1;
 	MEC1322_TMR16_CTL(1) &= ~1;
-	/* Disable the interrupt. */
 	task_disable_irq(MEC1322_IRQ_TIMER16_1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, port_80_interrupt_disable,
