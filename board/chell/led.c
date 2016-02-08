6da0862c479ff1d56f9030c6cc9deeb7466debdf
Duncan Laurie <dlaurie@chromium.org>
2016-02-08 12:32:25 -0800

    chell: Blink LED in S0iX
    Blink the LED in S3 as well as S0iX states so there is no user visible
    difference in their behavior.
    
    BUG=chrome-os-partner:49274
    BRANCH=glados
    TEST=Enter S0iX on chell (need SKU4) and verify LED blinks
    
    Change-Id: I91b123de17787159f4e7d6aca2e86b80885b8f4e
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>

--- board/chell/led.c
+++ board/chell/led.c
@@ -85,7 +85,8 @@ static void board_led_set_battery(void)
 	battery_ticks++;
 	power_ticks++;
 
-	if (chipset_in_state(CHIPSET_STATE_SUSPEND)) {
+	if (chipset_in_state(CHIPSET_STATE_SUSPEND) ||
+	    chipset_in_state(CHIPSET_STATE_STANDBY)) {
 		/*
 		 * Reset ticks if entering suspend so LED turns white
 		 * as soon as possible.
