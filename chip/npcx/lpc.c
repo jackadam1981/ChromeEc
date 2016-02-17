fac19ac87b517ff59e5809c2aef375b46e91556e
Archana Patni <archana.patni@intel.com>
2016-02-17 16:18:15 +0530

    skylake: do not clear masks in S0ix -> S0 transition
    EC clears the SCI/SMI/Wake masks in the resume sequence for S3
    and S0ix. This works in the S3 case because Coreboot reprograms
    the masks after EC. But in S0ix, these masks stay cleared forever.
    
    This means that no further events are sent to the host.
    
    This patch conditionally clears the masks only in the S3 transition.
    
    BRANCH=none
    BUG=chrome-os-partner:48834
    TEST=hostevent in EC console before and after S0ix to ensure SCI masks are preserve
    
    Change-Id: I23751680788ee7a239e321309a1334d37adc4f43
    Signed-off-by: Archana Patni <archana.patni@intel.com>
    Signed-off-by: Subramony Sesha <subramony.sesha@intel.com>
    Signed-off-by: Jenny TC <jenny.tc@intel.com>

--- chip/npcx/lpc.c
+++ chip/npcx/lpc.c
@@ -901,10 +901,12 @@ DECLARE_HOOK(HOOK_INIT, lpc_init, HOOK_PRIO_INIT_LPC);
 
 static void lpc_resume(void)
 {
-	/* Mask all host events until the host unmasks them itself.  */
-	lpc_set_host_event_mask(LPC_HOST_EVENT_SMI, 0);
-	lpc_set_host_event_mask(LPC_HOST_EVENT_SCI, 0);
-	lpc_set_host_event_mask(LPC_HOST_EVENT_WAKE, 0);
+	if (chipset_in_state(CHIPSET_STATE_SUSPEND | CHIPSET_STATE_ON)) {
+		/* Mask all host events until the host unmasks them itself.  */
+		lpc_set_host_event_mask(LPC_HOST_EVENT_SMI, 0);
+		lpc_set_host_event_mask(LPC_HOST_EVENT_SCI, 0);
+		lpc_set_host_event_mask(LPC_HOST_EVENT_WAKE, 0);
+	}
 
 	/* Store port 80 event so we know where resume happened */
 	port_80_write(PORT_80_EVENT_RESUME);
