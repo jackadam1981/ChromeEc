a23b1c7194e4ddbb4fed023924d651b637476d6d
Leon-Lee <leon.lee@ite.com.tw>
2016-02-22 16:48:47 -0800

    IT8320 for Chrome EC TCPC
    BRANCH=none
    BUG=none
    TEST=none
    
    1.Clean up the code and follow coding convention
    2.Modify tx as isr mode and reduce busy polling
    3.Add usb_pd_tcpc.c in chip/it83xx
    4.Add "check cc_line" function in tcpc_get_cc
    
    Change-Id: I9ca1822deeb4b4dce1279a09490ed4175890cf3a
    Signed-off-by: Leon-Lee <leon.lee@ite.com.tw>

--- chip/it83xx/intc.c
+++ chip/it83xx/intc.c
@@ -79,6 +79,17 @@ void intc_cpu_int_group_12(void)
 		peci_interrupt();
 		break;
 #endif
+#ifdef CONFIG_USB_PD_TCPM_ITE8320
+	case IT83XX_IRQ_USBPD0:
+		task_clear_pending_irq(IT83XX_IRQ_USBPD0);
+		ite8320_pd_irq(0);
+		break;
+
+	case IT83XX_IRQ_USBPD1:
+		task_clear_pending_irq(IT83XX_IRQ_USBPD1);
+		ite8320_pd_irq(1);
+		break;
+#endif /* CONFIG_USB_PD_TCPM_8320 */
 	default:
 		break;
 	}
