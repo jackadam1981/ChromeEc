c447256586dc1895e9997e046fb908b9d67b285d
Kyoung Kim <kyoung.il.kim@intel.com>
2016-03-02 20:47:26 -0800

    Lars: fan Fail-Safe
    fan: fail-safe for dptf
    Thermal thread monitors temperature of each sensors periodically.
    If any of temp. sensors reaches beyond limit, than fan will be set
    to max speed.
    
    BUG=None
    BRANCH=firmware-glados-7820.B
    TEST=heatup system and kill dptf and check if fan starts at max speed.
    or, use 'thermalset' command to change fake 'thermal_param' which is
    lower than current temperature and check if fan spins at max speed.
    
    Change-Id: Ia6677f5da835544ad6e4ebac0a38332c9e3c8c62
    Signed-off-by: Kyoung Kim <kyoung.il.kim@intel.com>

--- board/lars/board.c
+++ board/lars/board.c
@@ -297,7 +297,7 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
  */
 struct ec_thermal_config thermal_params[] = {
 	/* {Twarn, Thigh, Thalt}, fan_off, fan_max */
-	{{0, 0, 0}, 0, 0},	/* TMP432_Internal */
+	{{0, 0, 0}, 0, C_TO_K(57)},	/* TMP432_Internal */
 	{{0, 0, 0}, 0, 0},	/* TMP432_Sensor_1 */
 	{{0, 0, 0}, 0, 0},	/* TMP432_Sensor_2 */
 	{{0, 0, 0}, 0, 0},	/* Battery */
