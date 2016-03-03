5283c0a5f7a3ccfb76f6aa0b9bb75c848c173309
Kyoung Kim <kyoung.il.kim@intel.com>
2016-03-02 20:47:26 -0800

    Lars: Fan fail-safe
    Fan: fail-safe for dptf
    Thermal thread monitors temperature of each sensors periodically.
    If any of temp. sensors reaches beyond limit, than fan will be set
    to max speed.
    And if dptf is not installed or does not start at boot time, EC will
    control fan speed alone.
    
    BUG=None
    BRANCH=firmware-glados-7820.B
    TEST=heatup system and kill dptf and check if fan starts at max speed.
    or, use 'thermalset' command to change fake 'thermal_param' which is
    lower than current temperature and check if fan spins at max speed.
    
    Change-Id: Ia6677f5da835544ad6e4ebac0a38332c9e3c8c62
    Signed-off-by: Kyoung Kim <kyoung.il.kim@intel.com>

--- common/fan.c
+++ common/fan.c
@@ -24,6 +24,14 @@ static int thermal_control_enabled[CONFIG_FANS];
 static int fan_update_counter[CONFIG_FANS];
 #endif
 
+static void set_enabled(int fan, int enable)
+{
+	fan_set_enabled(fans[fan].ch, enable);
+
+	if (fans[fan].enable_gpio >= 0)
+		gpio_set_level(fans[fan].enable_gpio, enable);
+}
+
 #ifndef CONFIG_FAN_RPM_CUSTOM
 /* This is the default implementation. It's only called over [0,100].
  * Convert the percentage to a target RPM. We can't simply scale all
@@ -72,15 +80,9 @@ test_mockable void fan_set_percent_needed(int fan, int pct)
 	    new_rpm < fans[fan].rpm_start)
 		new_rpm = fans[fan].rpm_start;
 
+	/* enable the fan when non-zero duty */
+	set_enabled(fan, (pct > 0) ? 1 : 0);
 	fan_set_rpm_target(fans[fan].ch, new_rpm);
-}
-
-static void set_enabled(int fan, int enable)
-{
-	fan_set_enabled(fans[fan].ch, enable);
-
-	if (fans[fan].enable_gpio >= 0)
-		gpio_set_level(fans[fan].enable_gpio, enable);
 }
 
 static void set_thermal_control_enabled(int fan, int enable)
@@ -106,7 +108,6 @@ static void set_duty_cycle(int fan, int percent)
 	/* Set the duty cycle */
 	fan_set_duty(fans[fan].ch, percent);
 }
-
 /*****************************************************************************/
 /* Console commands */
 
