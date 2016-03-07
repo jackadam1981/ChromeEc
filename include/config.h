8b22e2ff87058c27f89babd80f1776995c8d44e2
Aman Kumar <akumar@analogixsemi.com>
2016-03-07 23:53:25 +0530

    TCPM : Added driver for Analogix's anx74xx chips
    Driver implements TCPC for ANX74xx chips. Enables Type C
    port for USB and DP alt mode. Enable port role swap feature.
    
    BUG=chrome-os-partner:49510
    BRANCH=none
    TEST=tested compiled binary for pdeval-stm32f072 board with this patch.
    Power contract establishment, port role swap, DP alt mode works fine.
    
    Change-Id: Iae6322510605a08d3bdd08446116ef5f9e4f7a7c
    Signed-off-by: Aman Kumar <akumar@analogixsemi.com>

--- include/config.h
+++ include/config.h
@@ -1837,6 +1837,7 @@
 #undef CONFIG_USB_PD_TCPM_STUB
 #undef CONFIG_USB_PD_TCPM_TCPCI
 #undef CONFIG_USB_PD_TCPM_FUSB302
+#undef CONFIG_USB_PD_TCPM_ANX74XX
 
 /*
  * Use this option if the TCPC port controller supports the optional register
