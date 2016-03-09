5586ace2474d0e1c325f8a161d6e51cdd6ee36e8
Koro Chen <koro.chen@mediatek.com>
2016-03-09 09:26:46 +0800

    TEST-ONLY: elm: Add elm changes
    This adds changes compared to Oak rev5, including:
      - motion sensors are two KX022, BMI160 is removed
      - ALS sensor is removed
      - LEDs are two bi-color LED (blue and orange)
      - Add EN_TP_INT_L to enable/disable touchpad interrupt
      - pi3usb30532 is removed (need driver for 7688?)
    
    This also removes codes using system_get_board_version(), and a little
    bit of coding style.
    
    Note that ANX7688 related changes are not here.
    
    Battery setting is kept to support 2S, and this should be modified
    when we don't need to support 2S anymore.
    
    BRANCH=none
    BUG=none
    TEST=make BOARD=elm -j
    
    Change-Id: Ibe6b47824814684b9bfb53bf52b152e0e1b5550b
    Signed-off-by: Koro Chen <koro.chen@mediatek.com>

--- board/elm/led.c
+++ board/elm/led.c
@@ -2,7 +2,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  *
- * Battery LED and Power LED control for Oak Board.
+ * Battery LED and Power LED control for Elm Board.
  */
 
 #include "battery.h"
@@ -15,17 +15,16 @@
 #include "system.h"
 
 const enum ec_led_id supported_led_ids[] = {
-	EC_LED_ID_BATTERY_LED
+	EC_LED_ID_BATTERY_LED,
+	EC_LED_ID_POWER_LED
 };
 
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 
 enum led_color {
-	BAT_LED_GREEN = 0,
+	BAT_LED_BLUE = 0,
 	BAT_LED_ORANGE,
-	BAT_LED_RED,
-	BAT_LED_AMBER,
-	PWR_LED_GREEN,
+	PWR_LED_BLUE,
 	PWR_LED_ORANGE,
 	LED_COLOR_COUNT		/* Number of colors, not a color itself */
 };
@@ -33,21 +32,14 @@ enum led_color {
 static int bat_led_set(enum led_color color, int on)
 {
 	switch (color) {
-	case BAT_LED_GREEN:
-		gpio_set_level(GPIO_BAT_LED0, on); /* BAT_LED_GREEN */
+	case BAT_LED_BLUE:
+		gpio_set_level(GPIO_BAT_LED0, on); /* BAT_LED_BLUE */
 		break;
 	case BAT_LED_ORANGE:
 		gpio_set_level(GPIO_BAT_LED1, on); /* BAT_LED_ORANGE */
 		break;
-	case BAT_LED_RED:
-		gpio_set_level(GPIO_BAT_LED1, on); /* BAT_LED_RED */
-		break;
-	case BAT_LED_AMBER:
-		gpio_set_level(GPIO_BAT_LED0, on); /* BAT_LED_AMBER */
-		gpio_set_level(GPIO_BAT_LED1, on);
-		break;
-	case PWR_LED_GREEN:
-		gpio_set_level(GPIO_PWR_LED0, on); /* PWR_LED_GREEN */
+	case PWR_LED_BLUE:
+		gpio_set_level(GPIO_PWR_LED0, on); /* PWR_LED_BLUE */
 		break;
 	case PWR_LED_ORANGE:
 		gpio_set_level(GPIO_PWR_LED1, on); /* PWR_LED_ORANGE */
@@ -61,37 +53,33 @@ static int bat_led_set(enum led_color color, int on)
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
 	/* Ignoring led_id as both leds support the same colors */
-	brightness_range[EC_LED_COLOR_RED] = 1;
-	brightness_range[EC_LED_COLOR_GREEN] = 1;
-	brightness_range[EC_LED_COLOR_YELLOW] = 1;
+	brightness_range[EC_LED_COLOR_BLUE] = 1;
+	brightness_range[EC_LED_COLOR_AMBER] = 1;
 }
 
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 {
 	if (EC_LED_ID_BATTERY_LED == led_id) {
-		if (brightness[EC_LED_COLOR_GREEN] != 0) {
-			bat_led_set(BAT_LED_GREEN, 1);
+		if (brightness[EC_LED_COLOR_BLUE] != 0) {
+			bat_led_set(BAT_LED_BLUE, 1);
 			bat_led_set(BAT_LED_ORANGE, 0);
-		} else if (brightness[EC_LED_COLOR_YELLOW] != 0) {
-			bat_led_set(BAT_LED_GREEN, 1);
+		} else if (brightness[EC_LED_COLOR_AMBER] != 0) {
+			bat_led_set(BAT_LED_BLUE, 0);
 			bat_led_set(BAT_LED_ORANGE, 1);
-		} else if (brightness[EC_LED_COLOR_RED] != 0) {
-			bat_led_set(BAT_LED_GREEN, 0);
-			bat_led_set(BAT_LED_RED, 1);
 		} else {
-			bat_led_set(BAT_LED_GREEN, 0);
+			bat_led_set(BAT_LED_BLUE, 0);
 			bat_led_set(BAT_LED_ORANGE, 0);
 		}
 		return EC_SUCCESS;
 	} else if (EC_LED_ID_POWER_LED == led_id) {
-		if (brightness[EC_LED_COLOR_GREEN] != 0) {
-			bat_led_set(PWR_LED_GREEN, 1);
+		if (brightness[EC_LED_COLOR_BLUE] != 0) {
+			bat_led_set(PWR_LED_BLUE, 1);
 			bat_led_set(PWR_LED_ORANGE, 0);
-		} else if (brightness[EC_LED_COLOR_YELLOW] != 0) {
-			bat_led_set(PWR_LED_GREEN, 1);
+		} else if (brightness[EC_LED_COLOR_AMBER] != 0) {
+			bat_led_set(PWR_LED_BLUE, 0);
 			bat_led_set(PWR_LED_ORANGE, 1);
 		} else {
-			bat_led_set(PWR_LED_GREEN, 0);
+			bat_led_set(PWR_LED_BLUE, 0);
 			bat_led_set(PWR_LED_ORANGE, 0);
 		}
 		return EC_SUCCESS;
@@ -100,65 +88,70 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 	}
 }
 
-static void elm_led_set_power(int board_version)
+static void elm_led_set_power(void)
 {
-	/* Do nothing */
+	static int power_second;
+
+	power_second++;
+
+	/*
+	 * PWR LED behavior:
+	 * Power on: Blue ON
+	 * Suspend: Orange in breeze mode ( 1 sec on/ 3 sec off)
+	 * Power off: OFF
+	 */
+	if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
+		bat_led_set(PWR_LED_BLUE, 0);
+		bat_led_set(PWR_LED_ORANGE, 0);
+	} else if (chipset_in_state(CHIPSET_STATE_ON)) {
+		bat_led_set(PWR_LED_BLUE, 1);
+		bat_led_set(PWR_LED_ORANGE, 0);
+	} else if (chipset_in_state(CHIPSET_STATE_SUSPEND)) {
+		bat_led_set(PWR_LED_BLUE, 0);
+		bat_led_set(PWR_LED_ORANGE,
+			    (power_second & 3) ? 0 : 1);
+	}
 }
 
-static void elm_led_set_battery(int board_version)
+static void elm_led_set_battery(void)
 {
 	static int battery_second;
 
 	battery_second++;
 
 	/*
-	 * Put power control here since we are using the "battery" LED.
-	 * This allows LED autocontrol to be turned off by cmd during factory
-	 * test.
-	 *
-	 * PWR LED behavior:
-	 * Power on: Green
-	 * Suspend: Green in breeze mode ( 1 sec on/ 3 sec off)
-	 * Power off: OFF
-	 */
-	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
-		bat_led_set(BAT_LED_GREEN, 0);
-	else if (chipset_in_state(CHIPSET_STATE_ON))
-		bat_led_set(BAT_LED_GREEN, 1);
-	else if (chipset_in_state(CHIPSET_STATE_SUSPEND))
-		bat_led_set(BAT_LED_GREEN,
-			    (battery_second & 3) ? 0 : 1);
-
-	/* BAT LED behavior:
-	 * Fully charged / idle: Off
-	 * Under charging: Orange
-	 * Bat. low (10%): Orange in breeze mode (1s on, 3s off)
-	 * Bat. critical low (less than 3%) or abnormal battery
-	 *   situation: Orange in blinking mode (1s on, 1s off)
-	 * Using battery or not connected to AC power: OFF
+	 * BAT LED behavior:
+	 * - Fully charged / idle: Blue ON
+	 * - Charging: Orange ON
+	 * - Battery discharging capacity<10%, Orange blink
+	 * - Battery error: Orange blink
 	 */
 	switch (charge_get_state()) {
 	case PWR_STATE_CHARGE:
+		bat_led_set(BAT_LED_BLUE, 0);
 		bat_led_set(BAT_LED_ORANGE, 1);
 		break;
 	case PWR_STATE_CHARGE_NEAR_FULL:
-		bat_led_set(BAT_LED_ORANGE, 1);
+		bat_led_set(BAT_LED_BLUE, 1);
+		bat_led_set(BAT_LED_ORANGE, 0);
 		break;
 	case PWR_STATE_DISCHARGE:
+		bat_led_set(BAT_LED_BLUE, 0);
 		if (charge_get_percent() < 3)
 			bat_led_set(BAT_LED_ORANGE,
-				  (battery_second & 1) ? 0 : 1);
+				    (battery_second & 1) ? 0 : 1);
 		else if (charge_get_percent() < 10)
 			bat_led_set(BAT_LED_ORANGE,
-				  (battery_second & 3) ? 0 : 1);
+				    (battery_second & 3) ? 0 : 1);
 		else
 			bat_led_set(BAT_LED_ORANGE, 0);
 		break;
 	case PWR_STATE_ERROR:
-		bat_led_set(BAT_LED_ORANGE,
-			    (battery_second & 1) ? 0 : 1);
+		bat_led_set(BAT_LED_BLUE, 0);
+		bat_led_set(BAT_LED_ORANGE, (battery_second & 1) ? 0 : 1);
 		break;
 	case PWR_STATE_IDLE: /* Ext. power connected in IDLE. */
+		bat_led_set(BAT_LED_BLUE, 1);
 		bat_led_set(BAT_LED_ORANGE, 0);
 		break;
 	default:
@@ -173,8 +166,8 @@ static void elm_led_set_battery(int board_version)
 static void led_second(void)
 {
 	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
-		elm_led_set_power(system_get_board_version());
+		elm_led_set_power();
 	if (led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED))
-		elm_led_set_battery(system_get_board_version());
+		elm_led_set_battery();
 }
 DECLARE_HOOK(HOOK_SECOND, led_second, HOOK_PRIO_DEFAULT);
