6bcb944d045608798d808e16eebd06545f920043
Kyoung Kim <kyoung.il.kim@intel.com>
2016-03-11 15:29:19 +0530

    Skylake: Enter S0ix on host command from kernel, exit via lid open
    This patch changes the entry/exit model for S0ix from a PCH
    SLP_S0 signal based model to a hybrid host event/direct interrupt
    model. The kernel will send host events on kernel freeze/thaw exit;
    EC will initiate the S0ix entry based on host command and exit via
    a direct hook to lid open interrupt.
    
    The assertion of SLP_S0 comes later than HC(suspend) and deasserion
    of SLP_S0 comes earlier than HC(resume).
            ________                        ________
    SLP_S0          |______________________|
            _____                             ________
    HC           |___________________________|
    
    Doing S0ix exit on host command leads to unfavorable interactions
    between EC and platform. Here the lid open interrupt is to use to
    pulse the PCH_WAKE_L signal and then EC starts initiating the
    S0ix -> S0 transition.
    
    BRANCH=Glados
    BUG=chrome-os-partner:50627
    TEST=Build/flash EC and check 'echo freeze > /sys/power/state'
    command in OS shell. Verify idle state transitions during display off
    and periodic wakes from S0ix do not lead to state transitions in EC.
    CQ-DEPEND=CL:328955
    
    Change-Id: I4bf114235c4d542dd7cf0dad6427c771e54d4611
    Signed-off-by: Kyoung Kim <kyoung.il.kim@intel.com>
    Signed-off-by: Archana Patni <archana.patni@intel.com>
    Signed-off-by: Subramony Sesha <subramony.sesha@intel.com>
    Signed-off-by: Divagar Mohandass <divagar.mohandass@intel.com>

--- power/skylake.c
+++ power/skylake.c
@@ -18,27 +18,20 @@
 #include "util.h"
 #include "wireless.h"
 #include "lpc.h"
+#include "lid_switch.h"
 
 /* Console output macros */
 #define CPUTS(outstr) cputs(CC_CHIPSET, outstr)
 #define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ## args)
 
 /* Input state flags */
-#define IN_PCH_SLP_S0_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S0_DEASSERTED)
 #define IN_PCH_SLP_S3_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S3_DEASSERTED)
 #define IN_PCH_SLP_S4_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S4_DEASSERTED)
 #define IN_PCH_SLP_SUS_DEASSERTED POWER_SIGNAL_MASK(X86_SLP_SUS_DEASSERTED)
 
-#ifdef CONFIG_POWER_S0IX
-#define IN_ALL_PM_SLP_DEASSERTED (IN_PCH_SLP_S0_DEASSERTED | \
-				  IN_PCH_SLP_S3_DEASSERTED | \
-				  IN_PCH_SLP_S4_DEASSERTED | \
-				  IN_PCH_SLP_SUS_DEASSERTED)
-#else
 #define IN_ALL_PM_SLP_DEASSERTED (IN_PCH_SLP_S3_DEASSERTED | \
 				  IN_PCH_SLP_S4_DEASSERTED | \
 				  IN_PCH_SLP_SUS_DEASSERTED)
-#endif
 
 /*
  * DPWROK is NC / stuffing option on initial boards.
@@ -54,6 +47,19 @@
 static int throttle_cpu;      /* Throttle CPU? */
 static int forcing_shutdown;  /* Forced shutdown in progress? */
 static int power_s5_up;       /* Chipset is sequencing up or down */
+
+#ifdef CONFIG_POWER_S0IX
+static int slp_s0_track = 1;
+int get_slp_s0_track(void)
+{
+	return slp_s0_track;
+}
+
+void set_slp_s0_track(int val)
+{
+	slp_s0_track = val ? 1 : 0;
+}
+#endif
 
 void chipset_force_shutdown(void)
 {
@@ -237,7 +243,7 @@ static enum power_state _power_handle_state(enum power_state state)
 			chipset_force_shutdown();
 			return POWER_S0S3;
 #ifdef CONFIG_POWER_S0IX
-		} else if ((gpio_get_level(GPIO_PCH_SLP_S0_L) == 0) &&
+		} else if ((get_slp_s0_track() == 0) &&
 			   (gpio_get_level(GPIO_PCH_SLP_S3_L) == 1)) {
 			return POWER_S0S0ix;
 #endif
@@ -253,7 +259,7 @@ static enum power_state _power_handle_state(enum power_state state)
 		/*
 		 * TODO: add code for unexpected power loss
 		 */
-		if ((gpio_get_level(GPIO_PCH_SLP_S0_L) == 1) &&
+		if ((get_slp_s0_track() == 1) &&
 		   (gpio_get_level(GPIO_PCH_SLP_S3_L) == 1)) {
 			return POWER_S0ixS0;
 		}
@@ -299,6 +305,11 @@ static enum power_state _power_handle_state(enum power_state state)
 
 		/* Call hooks now that rails are up */
 		hook_notify(HOOK_CHIPSET_STARTUP);
+
+#ifdef CONFIG_POWER_S0IX
+		/* re-init S0ix flag */
+		set_slp_s0_track(1);
+#endif
 		return POWER_S3;
 
 	case POWER_S3S0:
@@ -345,6 +356,10 @@ static enum power_state _power_handle_state(enum power_state state)
 		 */
 		enable_sleep(SLEEP_MASK_AP_RUN);
 
+#ifdef CONFIG_POWER_S0IX
+		/* re-init S0ix flag */
+		set_slp_s0_track(1);
+#endif
 		return POWER_S3;
 
 #ifdef CONFIG_POWER_S0IX
@@ -418,64 +433,52 @@ enum power_state power_handle_state(enum power_state state)
 }
 
 #ifdef CONFIG_POWER_S0IX
-static struct {
-	int required; /* indicates de-bounce required. */
-	int done;     /* debounced */
-} slp_s0_debounce = {
-	.required = 0,
-	.done = 1,
-};
-
-int chipset_get_ps_debounced_level(enum gpio_signal signal)
+static void lid_change(void)
 {
 	/*
-	 * If power state is updated in power_update_signal() by any interrupts
-	 * other than SLP_S0 during the 1 msec pulse(invalid SLP_S0 signal),
-	 * reading SLP_S0 should be corrected with slp_s0_debounce.done flag.
+	 * Using the lid open directly to initiate the EC resume from S0ix
+	 * sequence. It is important that the host event for lid open is
+	 * sent and PCH_WAKE_L pulsed before EC starts the state transition.
+	 *
+	 * Just setting the flag is enough for lidopen/close to work from
+	 * ec console. The physical lid open/close is debounced and initiates
+	 * the EC transition before the PCH_WAKE_L is pulsed. So this ordering
+	 * is enforced using HOOK_PRIO_FIRST and host_set_single_event.
 	 */
-	int level = gpio_get_level(signal);
-	return (signal == GPIO_PCH_SLP_S0_L) ?
-			(level & slp_s0_debounce.done) : level;
-}
 
-static void slp_s0_assertion_deferred(void)
-{
-	int s0_level = gpio_get_level(GPIO_PCH_SLP_S0_L);
-	/*
-	     (s0_level != 0) ||
-	     ((s0_level == 0) && (slp_s0_debounce.required == 0))
-	*/
-	if (s0_level == slp_s0_debounce.required) {
-		if (s0_level)
-			slp_s0_debounce.done = 1; /* debounced! */
-
-		power_signal_interrupt(GPIO_PCH_SLP_S0_L);
-	}
-
-	slp_s0_debounce.required = 0;
-}
-DECLARE_DEFERRED(slp_s0_assertion_deferred);
-
-void power_signal_interrupt_S0(enum gpio_signal signal)
-{
-	if (gpio_get_level(GPIO_PCH_SLP_S0_L)) {
-		slp_s0_debounce.required = 1;
-		hook_call_deferred(slp_s0_assertion_deferred, 3 * MSEC);
-	}
-	else if (slp_s0_debounce.required == 0) {
-		slp_s0_debounce.done = 0;
-		slp_s0_assertion_deferred();
+	if (lid_is_open()) {
+		host_set_single_event(EC_HOST_EVENT_LID_OPEN);
+		set_slp_s0_track(1);
 	}
 }
+DECLARE_HOOK(HOOK_LID_CHANGE, lid_change, HOOK_PRIO_FIRST);
+
+/*
+ * EC enters S0ix via a host command and exits S0ix via the above
+ * lid open hook. The host event for exit is received but is a no-op for now.
+ *
+ * EC will not react directly to SLP_S0 signal interrupts anymore.
+ */
 
 static int host_event_sleep_event(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_host_sleep_event *p = args->params;
 
-	if (p->sleep_event & HOST_SLEEP_EVENT_S0IX_SUSPEND)
+	if (p->sleep_event & HOST_SLEEP_EVENT_S0IX_SUSPEND) {
 		CPRINTS("S0ix sus evt");
-	else if (p->sleep_event & HOST_SLEEP_EVENT_S0IX_RESUME)
+		set_slp_s0_track(0);
+		task_wake(TASK_ID_CHIPSET);
+	} else if (p->sleep_event & HOST_SLEEP_EVENT_S0IX_RESUME) {
 		CPRINTS("S0ix res evt");
+		set_slp_s0_track(1);
+		/*
+		 * For all scenarios where lid is not open
+		 * this will be trigerred when other wake
+		 * sources like keyboard, trackpad are used.
+		 */
+		if (!chipset_in_state(CHIPSET_STATE_ON))
+			task_wake(TASK_ID_CHIPSET);
+	}
 
 	return EC_RES_SUCCESS;
 }
