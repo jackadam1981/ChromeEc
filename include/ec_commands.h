01aaeee2942347947efbd4936ea62646e24ffab8
Subramony Sesha <subramony.sesha@intel.com>
2016-03-11 13:42:39 +0530

    Skylake: Handle S0ix and S3 sleep event host commands from kernel.
    Handle host commands on kernel
    - S0ix freeze/thaw.
    - S3 suspend/resume in EC.
    
    EC can use these to commands for S0/S0ix and S0ix/S0
    transition triggers.
    
    BRANCH=Glados
    BUG=chrome-os-partner:50627
    TEST=Build/flash EC and Run 'echo freeze > /sys/power/state'
    command in OS shell and verify S0ix suspend/resume events in EC.
    CQ-DEPEND=CL:328896
    
    Change-Id: Ifa1dd2c9af8a572cf0662414f2ac44d1759c23da
    Signed-off-by: Subramony Sesha <subramony.sesha@intel.com>
    Signed-off-by: Kyoung Kim <kyoung.il.kim@intel.com>
    Signed-off-by: Divagar Mohandass <divagar.mohandass@intel.com>
    Signed-off-by: Shobhit Srivastava <shobhit.srivastava@intel.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -3015,6 +3015,19 @@ struct ec_response_hibernation_delay {
 	uint32_t hibernate_delay;
 };
 
+/* Inform the EC when entering a sleep state */
+#define EC_CMD_HOST_SLEEP_EVENT 0xa9
+
+enum host_sleep_event {
+	HOST_SLEEP_EVENT_S3_SUSPEND   = 0x01,
+	HOST_SLEEP_EVENT_S3_RESUME    = 0x02,
+	HOST_SLEEP_EVENT_S0IX_SUSPEND = 0x04,
+	HOST_SLEEP_EVENT_S0IX_RESUME  = 0x08,
+};
+
+struct ec_params_host_sleep_event {
+	uint32_t sleep_event;
+} __packed;
 
 /*****************************************************************************/
 /* Smart battery pass-through */
