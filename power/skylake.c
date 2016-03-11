1d56bfb81b8bbe003be6080ac19f5513d82d3c15
Kyoung Kim <kyoung.il.kim@intel.com>
2016-03-11 15:29:19 +0530

    Skylake: Enter/exit S0ix based on host command from kernel.
    This patch changes the entry/exit model for S0ix from a PCH
    SLP_S0 signal based model to a host event model based model. The
    kernel will now send a host event on kernel freeze/thaw exit and
    EC will initiate the S0ix entry/exit based on the events.
    
    With host command based S0ix entry/exit, assertion of SLP_S0 comes later
    than HC(suspend) and deasserion of SLP_S0 comes earlier than HC(resume).
            ________                        ________
    SLP_S0          |______________________|
            _____                             ________
    HC           |___________________________|
    
    On arrival of HC for suspend/resume, a flag is set and chipset_task
    relys on this flag for S0 and there is no need of SLP_S0 interrupt.
    
    BRANCH=Glados
    BUG=chrome-os-partner:50627
    TEST=Build/flash EC and check 'echo freeze > /sys/power/state'
    command in OS shell. Verify idle state transitions during display off
    and periodic wakes from S0ix do not lead to state transitions in EC.
    CQ-DEPEND=CL:328955
    
    Change-Id: I4bf114235c4d542dd7cf0dad6427c771e54d4611
    Signed-off-by: Kyoung Kim <kyoung.il.kim@intel.com>

--- power/skylake.c
+++ power/skylake.c
@@ -24,21 +24,13 @@
 #define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ## args)
 
 /* Input state flags */
-#define IN_PCH_SLP_S0_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S0_DEASSERTED)
 #define IN_PCH_SLP_S3_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S3_DEASSERTED)
 #define IN_PCH_SLP_S4_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S4_DEASSERTED)
 #define IN_PCH_SLP_SUS_DEASSERTED POWER_SIGNAL_MASK(X86_SLP_SUS_DEASSERTED)
 
-#ifdef CONFIG_POWER_S0IX
-#define IN_ALL_PM_SLP_DEASSERTED (IN_PCH_SLP_S0_DEASSERTED | \
-				  IN_PCH_SLP_S3_DEASSERTED | \
-				  IN_PCH_SLP_S4_DEASSERTED | \
-				  IN_PCH_SLP_SUS_DEASSERTED)
-#else
 #define IN_ALL_PM_SLP_DEASSERTED (IN_PCH_SLP_S3_DEASSERTED | \
 				  IN_PCH_SLP_S4_DEASSERTED | \
 				  IN_PCH_SLP_SUS_DEASSERTED)
-#endif
 
 /*
  * DPWROK is NC / stuffing option on initial boards.
@@ -54,6 +46,19 @@
 static int throttle_cpu;      /* Throttle CPU? */
 static int forcing_shutdown;  /* Forced shutdown in progress? */
 static int power_s5_up;       /* Chipset is sequencing up or down */
+
+#ifdef CONFIG_POWER_S0IX
+static int slp_s0_track = 1;
+int get_slp_s0_track(void)
+{
+	return slp_s0_track;
+}
+
+void set_slp_s0_track(int val)
+{
+	slp_s0_track = val ? 1 : 0;
+}
+#endif
 
 void chipset_force_shutdown(void)
 {
@@ -237,7 +242,7 @@ static enum power_state _power_handle_state(enum power_state state)
 			chipset_force_shutdown();
 			return POWER_S0S3;
 #ifdef CONFIG_POWER_S0IX
-		} else if ((gpio_get_level(GPIO_PCH_SLP_S0_L) == 0) &&
+		} else if ((get_slp_s0_track() == 0) &&
 			   (gpio_get_level(GPIO_PCH_SLP_S3_L) == 1)) {
 			return POWER_S0S0ix;
 #endif
@@ -253,7 +258,7 @@ static enum power_state _power_handle_state(enum power_state state)
 		/*
 		 * TODO: add code for unexpected power loss
 		 */
-		if ((gpio_get_level(GPIO_PCH_SLP_S0_L) == 1) &&
+		if ((get_slp_s0_track() == 1) &&
 		   (gpio_get_level(GPIO_PCH_SLP_S3_L) == 1)) {
 			return POWER_S0ixS0;
 		}
@@ -299,6 +304,11 @@ static enum power_state _power_handle_state(enum power_state state)
 
 		/* Call hooks now that rails are up */
 		hook_notify(HOOK_CHIPSET_STARTUP);
+
+#ifdef CONFIG_POWER_S0IX
+		/* re-init S0ix flag */
+		set_slp_s0_track(1);
+#endif
 		return POWER_S3;
 
 	case POWER_S3S0:
@@ -345,6 +355,10 @@ static enum power_state _power_handle_state(enum power_state state)
 		 */
 		enable_sleep(SLEEP_MASK_AP_RUN);
 
+#ifdef CONFIG_POWER_S0IX
+		/* re-init S0ix flag */
+		set_slp_s0_track(1);
+#endif
 		return POWER_S3;
 
 #ifdef CONFIG_POWER_S0IX
@@ -418,66 +432,20 @@ enum power_state power_handle_state(enum power_state state)
 }
 
 #ifdef CONFIG_POWER_S0IX
-static struct {
-	int required; /* indicates de-bounce required. */
-	int done;     /* debounced */
-} slp_s0_debounce = {
-	.required = 0,
-	.done = 1,
-};
-
-int chipset_get_ps_debounced_level(enum gpio_signal signal)
-{
-	/*
-	 * If power state is updated in power_update_signal() by any interrupts
-	 * other than SLP_S0 during the 1 msec pulse(invalid SLP_S0 signal),
-	 * reading SLP_S0 should be corrected with slp_s0_debounce.done flag.
-	 */
-	int level = gpio_get_level(signal);
-	return (signal == GPIO_PCH_SLP_S0_L) ?
-			(level & slp_s0_debounce.done) : level;
-}
-
-static void slp_s0_assertion_deferred(void)
-{
-	int s0_level = gpio_get_level(GPIO_PCH_SLP_S0_L);
-	/*
-	     (s0_level != 0) ||
-	     ((s0_level == 0) && (slp_s0_debounce.required == 0))
-	*/
-	if (s0_level == slp_s0_debounce.required) {
-		if (s0_level)
-			slp_s0_debounce.done = 1; /* debounced! */
-
-		power_signal_interrupt(GPIO_PCH_SLP_S0_L);
-	}
-
-	slp_s0_debounce.required = 0;
-}
-DECLARE_DEFERRED(slp_s0_assertion_deferred);
-
-void power_signal_interrupt_S0(enum gpio_signal signal)
-{
-	if (gpio_get_level(GPIO_PCH_SLP_S0_L)) {
-		slp_s0_debounce.required = 1;
-		hook_call_deferred(slp_s0_assertion_deferred, 3 * MSEC);
-	}
-	else if (slp_s0_debounce.required == 0) {
-		slp_s0_debounce.done = 0;
-		slp_s0_assertion_deferred();
-	}
-}
-
 static int host_event_sleep_event(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_host_sleep_event *p = args->params;
 
 	CPRINTS("Host sleep event 0x%08x", p->sleep_event);
 
-	if (p->sleep_event & HOST_SLEEP_EVENT_S0IX_SUSPEND)
+	if (p->sleep_event & HOST_SLEEP_EVENT_S0IX_SUSPEND) {
 		CPRINTS("Process S0ix suspend event from host");
-	else if (p->sleep_event & HOST_SLEEP_EVENT_S0IX_RESUME)
+		set_slp_s0_track(0);
+	} else if (p->sleep_event & HOST_SLEEP_EVENT_S0IX_RESUME) {
 		CPRINTS("Process S0ix resume event from host");
+		set_slp_s0_track(1);
+	}
+	task_wake(TASK_ID_CHIPSET);
 
 	return EC_RES_SUCCESS;
 }
