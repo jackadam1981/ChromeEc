77b26fb99a077fdcf45b81644848a810b6b49212
Subramony Sesha <subramony.sesha@intel.com>
2016-03-11 13:42:39 +0530

    Skylake: Handle S0ix and S3 sleep event host commands from kernel.
    Handle host commands on kernel
    - S0ix freeze/thaw.
    - S3 suspend/resume in EC.
    
    EC can use these to commands for S0/S0ix and S0ix/S0
    transition triggers.
    
    BRANCH=Glados
    BUG=chrome-os-partner:50627
    TEST=Build/flash EC and Run 'echo freeze > /sys/power/state'
    command in OS shell and verify S0ix suspend/resume events in EC.
    CQ-DEPEND=CL:328896
    
    Change-Id: Ifa1dd2c9af8a572cf0662414f2ac44d1759c23da
    Signed-off-by: Subramony Sesha <subramony.sesha@intel.com>
    Signed-off-by: Kyoung Kim <kyoung.il.kim@intel.com>
    Signed-off-by: Divagar Mohandass <divagar.mohandass@intel.com>
    Signed-off-by: Shobhit Srivastava <shobhit.srivastava@intel.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -2532,6 +2532,38 @@ struct ec_response_host_event_mask {
 #define EC_CMD_HOST_EVENT_SET_WAKE_MASK 0x8e
 #define EC_CMD_HOST_EVENT_CLEAR_B       0x8f
 
+enum host_sleep_event {
+	/* Host sleep event for S3 suspend */
+	HOST_SLEEP_EVENT_S3_SUSPEND   = 0x01,
+
+	/* Host sleep event for S3 resume */
+	HOST_SLEEP_EVENT_S3_RESUME    = 0x02,
+
+	/* Host sleep event for S0ix suspend */
+	HOST_SLEEP_EVENT_S0IX_SUSPEND = 0x04,
+
+	/* Host sleep event for S0ix resume */
+	HOST_SLEEP_EVENT_S0IX_RESUME  = 0x08,
+
+	/* Number of host sleep events */
+	HOST_SLEEP_EVENT,
+};
+
+/*
+ * Host event sleep params and response structures, shared by all of the host
+ * event commands below.
+ */
+struct ec_params_host_sleep_event {
+	uint32_t sleep_event;
+} __packed;
+
+struct ec_response_host_sleep_event {
+	uint32_t sleep_event;
+} __packed;
+
+/* This uses ec_params_host_sleep_event */
+#define EC_CMD_HOST_SLEEP_EVENT         0x9a
+
 /*****************************************************************************/
 /* Switch commands */
 
