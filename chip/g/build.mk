b83a55bca537cec66aad65108bc644cfaef1920e
Mary Ruthven <mruthven@chromium.org>
2016-03-14 09:44:51 -0700

    cr50: forward console through USB
    This change adds support for forwarding the EC console through USB.
    
    BUG=chrome-os-partner:49960
    BRANCH=none
    TEST=set CONFIG_USB_CONSOLE and test that the EC console can be accessed
    using the usb_console extra
    
    Change-Id: I35e0bb39fdc8f9485a14c03eb3a4d2f024884e17
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- chip/g/build.mk
+++ chip/g/build.mk
@@ -45,7 +45,7 @@ chip-$(CONFIG_TPM_SPS)+=sps_tpm.o
 chip-$(CONFIG_WATCHDOG)+=watchdog.o
 
 chip-$(CONFIG_USB)+=usb.o usb_endpoints.o
-chip-$(CONFIG_USB_CONSOLE)+=usb_console.o
+chip-$(CONFIG_USB_CONSOLE)+=usb_blob_console.o
 chip-$(CONFIG_USB_HID)+=usb_hid.o
 chip-$(CONFIG_USB_BLOB)+=usb_blob.o
 
