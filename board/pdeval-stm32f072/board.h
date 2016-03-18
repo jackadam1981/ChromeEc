4ab5ab59acc2568dc5e5f711c07731812cd505a5
Aman Kumar <akumar@analogixsemi.com>
2016-03-18 10:49:13 +0530

    pdeval-stm32f072 : Modification to test Analogix's TCPC driver
    Board configured with CONFIG_USB_PD_TCPM_ANX74XX. Implements
    power control sequence, DP pin mode configuration, HPD status
    update for anx74xx chip.
    
    BUG=none
    BRANCH=none
    TEST=tested compiled binary for this board with anx74xx TCPC driver
    
    Change-Id: I95ccbc3bae06524b64c20988d7e81c405bee383d
    Signed-off-by: Aman Kumar <akumar@analogixsemi.com>

--- board/pdeval-stm32f072/board.h
+++ board/pdeval-stm32f072/board.h
@@ -26,8 +26,11 @@
 #define CONFIG_USB_PD_ALT_MODE_DFP
 #define CONFIG_USB_PD_CUSTOM_VDM
 #define CONFIG_USB_PD_DUAL_ROLE
-#define CONFIG_USB_PD_PORT_COUNT 2
-#define CONFIG_USB_PD_TCPM_TCPCI
+#define CONFIG_USB_PD_PORT_COUNT 1
+#define CONFIG_USB_PD_TCPM_ANX74XX
+#define CONFIG_USBC_VCONN
+#define CONFIG_USB_PD_TCPM_VBUS
+#define CONFIG_USB_PD_TCPM_MUX
 
 /* start as a sink */
 #define PD_DEFAULT_STATE PD_STATE_SNK_DISCONNECTED
@@ -47,8 +50,7 @@
 #define I2C_PORT_PD_MCU 0
 
 /* TCPC I2C slave addresses */
-#define TCPC1_I2C_ADDR 0x9c
-#define TCPC2_I2C_ADDR 0x9e
+#define TCPC1_I2C_ADDR 0x50
 
 /* Timer selection */
 
@@ -94,6 +96,6 @@ enum usb_strings {
 };
 
 void board_reset_pd_mcu(void);
-
+void board_set_tcpc_power_mode(int port, int normal_mode);
 #endif /* !__ASSEMBLER__ */
 #endif /* __BOARD_H */
