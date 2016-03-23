603f4be40249c79a620479142a363bd65b4e5419
Shawn Nematbakhsh <shawnn@chromium.org>
2016-03-22 22:39:50 -0700

    kevin: Bring-up changes
    DO NOT COMMIT / test only
    Running hacks / tweaks for bring-up testing.
    
    Change-Id: Ia6590c81de58f9dea1c191a75a72a648f0e8f119

--- board/kevin/usb_pd_policy.c
+++ board/kevin/usb_pd_policy.c
@@ -7,6 +7,7 @@
 #include "charge_manager.h"
 #include "common.h"
 #include "console.h"
+#include "driver/charger/bd99955.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "host_command.h"
@@ -47,14 +48,16 @@ void pd_transition_voltage(int idx)
 	/* No-operation: we are always 5V */
 }
 
+int bd99955_select_input_port(enum bd99955_charge_port port);
+
 int pd_set_power_supply_ready(int port)
 {
 	/* Disable charging */
-	/* TODO: Add support for BD99955 */
+	bd99955_select_input_port(BD99955_CHARGE_PORT_NONE);
 
 	/* Provide VBUS */
 	gpio_set_level(port ? GPIO_C1_VOUT_EN_L :
-			      GPIO_C0_VOUT_EN_L, 1);
+			      GPIO_C0_VOUT_EN_L, 0);
 
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
@@ -66,7 +69,7 @@ void pd_power_supply_reset(int port)
 {
 	/* Disable VBUS */
 	gpio_set_level(port ? GPIO_C1_VOUT_EN_L :
-			      GPIO_C0_VOUT_EN_L, 0);
+			      GPIO_C0_VOUT_EN_L, 1);
 
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
