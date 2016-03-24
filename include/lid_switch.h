613d2ab45d4c0fa8e1fc9a433a127c8052727005
Archana Patni <archana.patni@intel.com>
2016-03-24 17:30:07 +0530

    common: add forced lid open function
    An earlier patch added a host command for forced lid
    open for a factory use case. This rafactors the code
    to add a common function which is available to other
    functions.
    
    In certain scenarios where we need to wake
    up the system in a lid closed scenario due to an error
    condition, we need to do a forced lid open.
    
    BRANCH=Glados
    BUG=chrome-os-partner:51441
    TEST=Build for Lars
    
    Signed-off-by: Archana Patni <archana.patni@intel.com>
    Signed-off-by: Subramony Sesha <subramony.sesha@intel.com>
    
    Change-Id: Ibc1a44b4704f4f8942cab815a896be273c93c92a

--- include/lid_switch.h
+++ include/lid_switch.h
@@ -10,6 +10,14 @@
 
 #include "common.h"
 
+/*
+ * Allow a forced lid open for special scenarios
+ *
+ * @param open 		Set to 1 for force lid open
+ */
+
+int force_lid_open(int open);
+
 /**
  * Return non-zero if lid is open.
  *
