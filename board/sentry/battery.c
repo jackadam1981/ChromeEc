cbf47a763cad9a3be336b123e295ec4bd09d2d95
Bruce <Bruce.Wan@quantatw.com>
2016-03-29 19:53:55 +0800

    Sentry: Let system into OS after battery FW update fail.
    After battery FW update fail, we plug in the battery.
    We can't get battery info, but the system still think battery is
    press. So add a condition for this case, let system will not
    reboot again and again.
    
    BUG=none
    BRANCH=firmware-glados-7820.B
    TEST=Check the system can boot into OS after battery FW update fail
    
    Change-Id: Ib529f52dd284ec7ecd452133393a67cbde83f2ef
    Signed-off-by: Bruce.Wan <Bruce.Wan@quantatw.com>

--- board/sentry/battery.c
+++ board/sentry/battery.c
@@ -8,6 +8,7 @@
 #include "adc.h"
 #include "battery.h"
 #include "battery_smart.h"
+#include "charge_state.h"
 #include "console.h"
 #include "extpower.h"
 #include "gpio.h"
@@ -52,8 +53,10 @@ static const struct battery_info info = {
 const struct battery_info *battery_get_info(void)
 {
 	static struct battery_info __bss_slow batt_info;
+	static struct charge_state_data curr;
 
-	if (battery_is_present() == BP_NO) {
+	battery_get_params(&curr.batt);
+	if (battery_is_present() == BP_NO || &curr.batt == 0) {
 		batt_info = info;
 		batt_info.voltage_min = batt_info.voltage_max;
 		return &batt_info;
