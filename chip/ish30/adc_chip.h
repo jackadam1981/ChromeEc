4ac986d03f7dfd43309fa64ba82e6c6b8fa9ecbd
Jaiber John <jaiber.j.john@intel.com>
2016-03-31 23:49:53 +0530

    ish30: Add support for ISH3.0 chip
    This patch adds the initial support for ISH3.0 chip to enable the EC
    firmware to boot on Intel Integrated Sensor Hub (ISH). The following are
    enabled:
    
    1. Inter-Processor Communication (IPC) driver that enables the ISH to
    communicate with the host Operating system via shared registers.
    2. High Precision Event Timer (HPET) driver that provides configurable
    timers for the FW to use in task scheduling.
    3. I2C bus driver for accessing sensors.
    4. UART console driver with TX support only.
    
    BUG=chrome-os-partner:51851
    BRANCH=None
    TEST=`make buildall -j`
    
    Change-Id: I15d4c201b799cfa79bed220ee573b75f5cd7b1f7
    Signed-off-by: Jaiber John <jaiber.j.john@intel.com>
    Signed-off-by: Alex Brill <alexander.brill@intel.com>
    Signed-off-by: Gomathi Kumar <gomathi.kumar@intel.com>

---
+++ chip/ish30/adc_chip.h
@@ -0,0 +1,10 @@
+/* Copyright (c) 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* ISH3.0-specific ADC module for Chrome EC
+ *
+ * ADC module is not required for ISH as of now,
+ * but as common/adc expects chip/$CHIP/adc_chip.h file,
+ * retaining an empty file. */
