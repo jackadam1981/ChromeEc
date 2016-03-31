c70ad859804e5908305a9626a5dcc8eff0b3186e
Jaiber John <jaiber.j.john@intel.com>
2016-04-01 00:24:12 +0530

    kunimitsu_ish: Add new board for Kunimitsu ISH
    This patch adds a new board (kunimitsu_ish) that enables Intel Sensor
    Hub (ISH) on kunimitsu platform. Basic board functions and tasklist are
    defined.
    
    BUG=chrome-os-partner:51851
    BRANCH=None
    TEST=`make buildall -j`
    
    Signed-off-by: Jaiber John <jaiber.j.john@intel.com>
    Signed-off-by: Gomathi Kumar <gomathi.kumar@intel.com>
    
    Change-Id: Ia86f8d2159fab7049441892e291c3b884901fdbe
    Signed-off-by: Jaiber John <jaiber.j.john@intel.com>

---
+++ board/kunimitsu_ish/build.mk
@@ -0,0 +1,11 @@
+# -*- makefile -*-
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+#
+
+CHIP:=ish30
+
+board-y=board.o chipset.o
