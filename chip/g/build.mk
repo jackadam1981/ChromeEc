c0f421a94b0e2b557949ca0d707d872116aaa7b2
Scott <scollyer@chromium.org>
2016-04-05 10:45:45 -0700

    Cr-50: NvMem: Framework for A/B partition design
    Only framework included at this point. New files in
    chip/g to contain NvMem functions and api. This is
    beginning point for A/B parition scheme that will
    overwrite older partition and thus guarantee atomic
    NvMem writes.
    
    BUG=chrome-os-partner:44745
    BRANCH=none
    TEST=Compile only as functionality is stubbed out.
    
    Change-Id: I3d6d8562b61682f5329a7e0900e80bb8623f7916
    Signed-off-by: Scott <scollyer@chromium.org>

--- chip/g/build.mk
+++ chip/g/build.mk
@@ -59,6 +59,7 @@ chip-$(CONFIG_STREAM_USART)+=usart.o
 chip-$(CONFIG_LOW_POWER_IDLE)+=idle.o
 
 chip-$(CONFIG_FLASH)+=flash.o
+chip-$(CONFIG_FLASH)+=nvmem_utils.o
 dirs-y += chip/g/dcrypto
 
 ifneq ($(CONFIG_CUSTOMIZED_RO),)
