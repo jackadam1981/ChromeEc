5cdbcdc3a80d116e6c85e0ae1a9a6c008002947e
Bruce <Bruce.Wan@quantatw.com>
2016-04-06 20:05:58 +0800

    Sentry: Add board support for asserting RTCRST
    Enable CONFIG_BOARD_HAS_RTC_RESET and add a board_rtc_reset() function
    to assert RTCRST to the PCH that will be called if the board fails to
    sequence out of the S5 state.
    
    Set gpio_pch_rtcrst_l to low when system init. Let system can jump
    to RW image when flash EC by usb.
    
    BUG=None
    BRANCH=glados
    TEST=make build sentry
    
    Change-Id: Ie4223f581019dd9c1a7b392b413e3507da1c656d
    Signed-off-by: Bruce.Wan <Bruce.Wan@quantatw.com>

--- board/sentry/board.c
+++ board/sentry/board.c
@@ -204,6 +204,14 @@ void board_reset_pd_mcu(void)
 	gpio_set_level(GPIO_PD_RST_L, 1);
 }
 
+void board_rtc_reset(void)
+{
+	CPRINTS("Asserting RTCRST# to PCH");
+	gpio_set_level(GPIO_PCH_RTCRST, 1);
+	udelay(100);
+	gpio_set_level(GPIO_PCH_RTCRST, 0);
+}
+
 #ifdef HAS_TASK_MOTIONSENSE
 /* Four Motion sensors */
 /* kxcj9 mutex and local/private data*/
@@ -505,6 +513,12 @@ pmic_error:
 	CPRINTS("PMIC initialization failed");
 }
 DECLARE_HOOK(HOOK_INIT, board_pmic_init, HOOK_PRIO_INIT_I2C + 1);
+
+void board_config_pre_init(void)
+{
+	/* Set RTCRST to low to prevent system reset, WA for issue:52036 */
+	gpio_set_level(GPIO_PCH_RTCRST, 0);
+}
 
 /* Initialize board. */
 static void board_init(void)
