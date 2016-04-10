3673d0ba3fdb9da89d7fe3189d601f796b9a97b9
Bill Richardson <wfrichar@chromium.org>
2016-04-09 20:53:35 -0700

    Cr50: Enable RW FW updates over USB
    This enables the Cr50 to accept RW firmware updates over USB.
    
    BUG=chrome-os-partner:50707, chrome-os-partner:50712
    BRANCH=none
    TEST=make buildall; test on Cr50
    
    Build and run the extra/usb_updater utility. Watch the console,
    and observe that the Cr50 updates and reboots into the new image
    correctly.
    
    Note that you'll have to rebuild the ec.bin image in order for
    the update to take effect. Just reflashing the same image doesn't
    cause the bootloader to change its selection.
    
    All the previously existing endpoints continue to function normally.
    
    Change-Id: I7bd22eae803c2ceeb14a767c06d3d5c9f1ac7c7a
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>

--- board/cr50/build.mk
+++ board/cr50/build.mk
@@ -40,6 +40,7 @@ board-y += tpm2/rsa.o
 board-y += tpm2/stubs.o
 board-y += tpm2/trng.o
 board-y += tpm2/upgrade.o
+board-y += usb_upgrade.o
 
 # Build and link with an external library
 EXTLIB := $(realpath ../../third_party/tpm2)
