12d3e75de880cd0a373349dae9ff872ac0e1648d
Shawn Nematbakhsh <shawnn@chromium.org>
2016-04-11 12:42:22 -0700

    kevin / gru: QOL changes for board bring-up
    DO NOT COMMIT
    
    - Force USB data switches closed
    - Set default backlight PWM duty at 50%
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: I84be0f05d880dea75d05c608c7b4988b979a3872

--- driver/charger/bd99955.c
+++ driver/charger/bd99955.c
@@ -557,6 +557,21 @@ static void bd99995_init(void)
 	reg &= ~BD99955_CMD_VM_CTRL_SET_EXTIADPEN;
 	ch_raw_write16(BD99955_CMD_VM_CTRL_SET, reg,
 		       BD99955_EXTENDED_COMMAND);
+
+	/* Close USB data switches */
+	ch_raw_read16(BD99955_CMD_VCC_UCD_SET, &reg,
+				BD99955_EXTENDED_COMMAND);
+	reg &= ~0x3;
+	reg |= 1;
+	ch_raw_write16(BD99955_CMD_VCC_UCD_SET, reg,
+				BD99955_EXTENDED_COMMAND);
+
+	ch_raw_read16(BD99955_CMD_VBUS_UCD_SET, &reg,
+				BD99955_EXTENDED_COMMAND);
+	reg &= ~0x3;
+	reg |= 1;
+	ch_raw_write16(BD99955_CMD_VBUS_UCD_SET, reg,
+				BD99955_EXTENDED_COMMAND);
 }
 DECLARE_HOOK(HOOK_INIT, bd99995_init, HOOK_PRIO_INIT_EXTPOWER);
 
