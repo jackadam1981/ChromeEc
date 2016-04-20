1ab0030850e170e34c0275d7ecd7b9baee11f555
Mary Ruthven <mruthven@chromium.org>
2016-04-19 18:48:54 -0700

    cr50: enable AP and EC flash access
    The cr50 SPI master can control the external AP and EC SPI ROM. This
    change adds support for doing spi_transactions, but does not use the SPI
    transactions for anything except console commands. This support will be
    used for flashing the AP and EC through CCD. For now AP and EC flash
    select must be done manually using the spi_flash_select console command.
    Flash select should be disabled after use, because it will prevent the
    system from booting.
    
    BUG=chrome-os-partner:50701
    BRANCH=none
    TEST=Select AP ROM and verify spi_flashinfo, read, erase, and write
    commands work properly. Select EC ROM and verify the same commands
    work properly.
    
    Change-Id: I16c55015794f8513effe0fa5712488a84bed2627
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- board/cr50/board.h
+++ board/cr50/board.h
@@ -20,6 +20,7 @@
 #define CONFIG_WP_ALWAYS
 /* TODO(crosbug.com/p/44745): For debugging only */
 #define CONFIG_CMD_FLASH
+#define CONFIG_CMD_SPI_FLASH
 
 /* Go to sleep when nothing else is happening */
 #define CONFIG_LOW_POWER_IDLE
@@ -37,6 +38,17 @@
 #define CONFIG_STREAM_USB
 
 #define CONFIG_USB_PID 0x5014
+
+#define CONFIG_SPI_FLASH
+#define CONFIG_SPI_FLASH_GD25Q41B
+#define CONFIG_SPI_FLASH_GD25Q64C
+#define CONFIG_SPI_FLASH_PORT 0
+#define CONFIG_SPI_FLASH_SELECT
+#define CONFIG_SPI_FLASH_DEFAULT SPI_FLASH_GD25Q64C
+
+#define CONFIG_SPI_MASTER
+#define CONFIG_SPI_MASTER_NO_CS_GPIOS
+#define CONFIG_SPI_MASTER_CONFIGURE_GPIOS
 
 /* Enable SPI Slave (SPS) module */
 #define CONFIG_SPS
