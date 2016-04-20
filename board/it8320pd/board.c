219cfb52f12e5e1465b34c46967fafb9b18e335c
Dino Li <dino.li@ite.com.tw>
2016-04-20 16:19:06 +0800

    board: it8320pd: initial board code
    For it8320 emulation board PD module validation.
    
    Signed-off-by: Dino Li <dino.li@ite.com.tw>
    
    BRANCH=none
    BUG=none
    TEST=manual
         console commands:
           - pd 0/1 dev [20|12|5]
           - pd 0/1 charger
           - pd 0/1 swap power
           - pd 0/1 vdm vers
             > version: zinger_v1.7.539-91a0
    
    Change-Id: Ia6b0f265b18c2257be076df57b88837ba201cffb

---
+++ board/it8320pd/board.c
@@ -0,0 +1,87 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* IT8320 development board configuration */
+
+#include "adc.h"
+#include "adc_chip.h"
+#include "clock.h"
+#include "common.h"
+#include "console.h"
+#include "driver/tcpm/it83xx_pd.h"
+#include "hooks.h"
+#include "registers.h"
+#include "switch.h"
+#include "system.h"
+#include "task.h"
+#include "timer.h"
+#include "uart.h"
+#include "usb_pd.h"
+#include "usb_pd_tcpm.h"
+#include "util.h"
+
+int board_get_battery_soc(void)
+{
+	return 100;
+}
+
+const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
+	{-1, -1, &it83xx_tcpm_drv},
+	{-1, -1, &it83xx_tcpm_drv},
+};
+
+void board_pd_vconn_ctrl(int port, int cc_pin, int enabled)
+{
+	int cc1_enabled = 0, cc2_enabled = 0;
+
+	if (cc_pin)
+		cc2_enabled = enabled;
+	else
+		cc1_enabled = enabled;
+
+	if (port) {
+		gpio_set_level(GPIO_USBPD_PORTB_CC2_VCONN, cc2_enabled);
+		gpio_set_level(GPIO_USBPD_PORTB_CC1_VCONN, cc1_enabled);
+	} else {
+		gpio_set_level(GPIO_USBPD_PORTA_CC2_VCONN, cc2_enabled);
+		gpio_set_level(GPIO_USBPD_PORTA_CC1_VCONN, cc1_enabled);
+	}
+}
+
+void board_pd_vbus_ctrl(int port, int enabled)
+{
+	if (port) {
+		gpio_set_level(GPIO_USBPD_PORTB_VBUS_INPUT, !enabled);
+		gpio_set_level(GPIO_USBPD_PORTB_VBUS_OUTPUT, enabled);
+		if (!enabled) {
+			gpio_set_level(GPIO_USBPD_PORTB_VBUS_DROP, 1);
+			udelay(MSEC);
+		}
+		gpio_set_level(GPIO_USBPD_PORTB_VBUS_DROP, 0);
+	} else {
+		gpio_set_level(GPIO_USBPD_PORTA_VBUS_INPUT, !enabled);
+		gpio_set_level(GPIO_USBPD_PORTA_VBUS_OUTPUT, enabled);
+		if (!enabled) {
+			gpio_set_level(GPIO_USBPD_PORTA_VBUS_DROP, 1);
+			udelay(MSEC);
+		}
+		gpio_set_level(GPIO_USBPD_PORTA_VBUS_DROP, 0);
+	}
+}
+
+#include "gpio_list.h"
+
+/* Initialize board. */
+static void board_init(void)
+{
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+/* ADC channels. Must be in the exactly same order as in enum adc_channel. */
+const struct adc_t adc_channels[] = {
+	/* Convert to mV (3000mV/1024). */
+	{"ADC_VBUSSA", 3000, 1024, 0, 0}, /*GPI0*/
+	{"ADC_VBUSSB", 3000, 1024, 0, 1}, /*GPI1*/
+};
+BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
---
+++ board/it8320pd/board.h
@@ -0,0 +1,77 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* IT8320 development board configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* Optional features */
+#undef CONFIG_LID_SWITCH
+#define CONFIG_LOW_POWER_IDLE
+
+/* PD */
+#define CONFIG_USB_PD_ALT_MODE
+#define CONFIG_USB_PD_ALT_MODE_DFP
+#define CONFIG_USB_PD_CHECK_MAX_REQUEST_ALLOWED
+#define CONFIG_USB_PD_CUSTOM_VDM
+#define CONFIG_USB_PD_DUAL_ROLE
+#define CONFIG_USB_PD_PORT_COUNT    2
+#define CONFIG_USB_PD_TCPM_ITE83XX
+#define CONFIG_USB_PD_TRY_SRC
+#define CONFIG_USB_POWER_DELIVERY
+#define CONFIG_USBC_VCONN
+#define CONFIG_USBC_VCONN_SWAP
+
+/* Optional console commands */
+#define CONFIG_CMD_STACKOVERFLOW
+
+/* Debug */
+#undef CONFIG_UART_TX_BUF_SIZE
+#define CONFIG_UART_TX_BUF_SIZE 4096
+
+/* Unused modules */
+#undef CONFIG_EC2I
+#undef CONFIG_LPC
+#undef CONFIG_PECI
+#undef CONFIG_PWM
+#undef CONFIG_SPI
+
+#ifndef __ASSEMBLER__
+
+#include "gpio_signal.h"
+
+enum adc_channel {
+	ADC_VBUSSA,
+	ADC_VBUSSB,
+	/* Number of ADC channels */
+	ADC_CH_COUNT
+};
+
+/* Define typical operating power and max power */
+#define PD_OPERATING_POWER_MW 15000
+#define PD_MAX_POWER_MW       60000
+#define PD_MAX_CURRENT_MA     3000
+/* Try to negotiate to 20V since i2c noise problems should be fixed. */
+#define PD_MAX_VOLTAGE_MV     20000
+/* start as a sink in case we have no other power supply/battery */
+#define PD_DEFAULT_STATE PD_STATE_SNK_DISCONNECTED
+/* TODO: determine the following board specific type-C power constants */
+/*
+ * delay to turn on the power supply max is ~16ms.
+ * delay to turn off the power supply max is about ~180ms.
+ */
+#define PD_POWER_SUPPLY_TURN_ON_DELAY  30000  /* us */
+#define PD_POWER_SUPPLY_TURN_OFF_DELAY 250000 /* us */
+
+/* delay to turn on/off vconn */
+#define PD_VCONN_SWAP_DELAY 5000 /* us */
+
+int board_get_battery_soc(void);
+void board_pd_vconn_ctrl(int port, int cc_pin, int enabled);
+void board_pd_vbus_ctrl(int port, int enabled);
+
+#endif /* !__ASSEMBLER__ */
+#endif /* __CROS_EC_BOARD_H */
