8d99daa04b736b8f26034c1aaf20dd04ed5e5480
Mary Ruthven <mruthven@chromium.org>
2016-04-19 18:48:54 -0700

    cr50: enable AP and EC flash access
    The cr50 SPI master can control the external AP and EC SPI ROM. This
    change adds support for doing spi_transactions, but does not use the SPI
    transactions for anything except console commands. This support will be
    used for flashing the AP and EC through CCD. For now AP and EC flash
    select must be done manually using the spi_flash_select console command.
    Flash select should be disabled after use, because it will prevent the
    system from booting.
    
    BUG=chrome-os-partner:50701
    BRANCH=none
    TEST=Select AP ROM and verify spi_flashinfo, read, erase, and write
    commands work properly. Select EC ROM and verify the same commands
    work properly.
    
    Change-Id: I16c55015794f8513effe0fa5712488a84bed2627
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- board/cr50/build.mk
+++ board/cr50/build.mk
@@ -31,6 +31,7 @@ dirs-y += $(BDIR)/tpm2
 # Objects that we need to build
 board-y =  board.o
 board-${CONFIG_RDD} += rdd.o
+board-${CONFIG_CMD_SPI_FLASH} += spi_flash.o
 board-y += tpm2/NVMem.o
 board-y += tpm2/aes.o
 board-y += tpm2/ecc.o
