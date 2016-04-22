24042c1ecb734cccd7bcf9e041bd656d52e8b594
Rong Chang <rongchang@chromium.org>
2016-04-22 19:03:06 +0800

    elm: anx7688: add anx7688 hpd driver
    ANX7688 is a TCPCI compatible port controller with HDMI to DP converter.
    The HDMI converter needs a reset every time after enabling its function.
    
    BRANCH=none
    BUG=chrome-os-partner:52815
    TEST=manual
      boot elm proto
      plug and unplug dingdong and check DP output
      plug/unplug adapter and check pd 0 state
    
    Signed-off-by: Rong Chang <rongchang@chromium.org>
    Signed-off-by: Koro Chen <koro.chen@mediatek.com>
    Change-Id: I774421d7b0b8d2cfd31e860fcd4eaed08ee48ac7
    Signed-off-by: Tang Zhentian1 <ztang@analogixsemi.com>

--- include/config.h
+++ include/config.h
@@ -1876,6 +1876,7 @@
 #undef CONFIG_USB_PD_TCPM_FUSB302
 #undef CONFIG_USB_PD_TCPM_ITE83XX
 #undef CONFIG_USB_PD_TCPM_ANX74XX
+#undef CONFIG_USB_PD_TCPM_ANX7688
 
 /*
  * Use this option if the TCPC port controller supports the optional register
