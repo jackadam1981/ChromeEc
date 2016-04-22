d96a83aac75eb044336f188c396d19ecb4f81984
Rong Chang <rongchang@chromium.org>
2016-04-22 19:03:06 +0800

    elm: anx7688: add anx7688 hpd driver
    ANX7688 is a TCPCI compatible port controller with HDMI to DP converter.
    The HDMI converter needs a reset every time after enabling its function.
    
    BRANCH=none
    BUG=chrome-os-partner:52815
    TEST=manual
      boot elm proto
      plug and unplug dingdong and check DP output
      plug/unplug adapter and check pd 0 state
    
    Change-Id: I774421d7b0b8d2cfd31e860fcd4eaed08ee48ac7
    Signed-off-by: Rong Chang <rongchang@chromium.org>
    Signed-off-by: Tang Zhentian1 <ztang@analogixsemi.com>
    Signed-off-by: Koro Chen <koro.chen@mediatek.com>

--- board/elm/usb_pd_policy.c
+++ board/elm/usb_pd_policy.c
@@ -7,6 +7,7 @@
 #include "charge_manager.h"
 #include "common.h"
 #include "console.h"
+#include "driver/tcpm/anx7688.h"
 #include "driver/tcpm/tcpci.h"
 #include "driver/tcpm/tcpm.h"
 #include "gpio.h"
@@ -24,7 +25,7 @@
 #define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
 
 #define PDO_FIXED_FLAGS (PDO_FIXED_DUAL_ROLE | PDO_FIXED_DATA_SWAP |\
-			 PDO_FIXED_COMM_CAP | PDO_FIXED_EXTERNAL)
+			 PDO_FIXED_COMM_CAP)
 
 /* TODO: fill in correct source and sink capabilities */
 const uint32_t pd_src_pdo[] = {
@@ -59,7 +60,7 @@ int pd_set_power_supply_ready(int port)
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
 
-	tcpc_write(port, TCPC_REG_COMMAND, 0x77);
+	anx7688_set_power_supply_ready(port);
 
 	return EC_SUCCESS;
 }
@@ -72,7 +73,7 @@ void pd_power_supply_reset(int port)
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
 
-	tcpc_write(port, TCPC_REG_COMMAND, 0x66);
+	anx7688_power_supply_reset(port);
 }
 
 void pd_set_input_current_limit(int port, uint32_t max_ma,
@@ -306,6 +307,9 @@ static int svdm_dp_attention(int port, uint32_t *payload)
 	int irq = PD_VDO_DPSTS_HPD_IRQ(payload[1]);
 	int ack = 1;
 
+	anx7688_update_hpd_level(port, lvl);
+	anx7688_update_hpd_irq(port, irq);
+
 	dp_status[port] = payload[1];
 	cur_lvl = gpio_get_level(GPIO_USB_DP_HPD);
 
@@ -327,6 +331,7 @@ static int svdm_dp_attention(int port, uint32_t *payload)
 static void svdm_exit_dp_mode(int port)
 {
 	svdm_safe_dp_mode(port);
+	board_reset_pd_mcu();
 }
 
 static int svdm_enter_gfu_mode(int port, uint32_t mode_caps)
