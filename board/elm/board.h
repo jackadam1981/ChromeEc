f54fc3330bfa6dd595498e22f023024f0a9d11d4
Rong Chang <rongchang@chromium.org>
2016-04-22 19:03:06 +0800

    elm: anx7688: add anx7688 hpd driver
    ANX7688 is a TCPCI compatible port controller with HDMI to DP converter.
    The HDMI converter needs a reset every time after enabling its function.
    
    BRANCH=none
    BUG=chrome-os-partner:52815
    TEST=manual
      boot elm proto
      plug and unplug dingdong and check DP output
      plug/unplug adapter and check pd 0 state
    
    Change-Id: I774421d7b0b8d2cfd31e860fcd4eaed08ee48ac7
    Signed-off-by: Rong Chang <rongchang@chromium.org>
    Signed-off-by: Tang Zhentian1 <ztang@analogixsemi.com>
    Signed-off-by: Koro Chen <koro.chen@mediatek.com>

--- board/elm/board.h
+++ board/elm/board.h
@@ -93,6 +93,7 @@
 
 #define CONFIG_USB_PD_PORT_COUNT 1
 #define CONFIG_USB_PD_TCPM_MUX
+#define CONFIG_USB_PD_TCPM_ANX7688
 #define CONFIG_USB_PD_TCPM_TCPCI
 #define CONFIG_USB_PD_TRY_SRC
 #undef  CONFIG_TCPC_I2C_BASE_ADDR
@@ -206,11 +207,6 @@ enum temp_sensor_id {
 void board_reset_pd_mcu(void);
 /* Set AP reset pin according to parameter */
 void board_set_ap_reset(int asserted);
-
-/* Control type-C DP route and hotplug detect signal */
-void board_typec_dp_on(int port);
-void board_typec_dp_off(int port, int *dp_flags);
-void board_typec_dp_set(int port, int level);
 
 #endif  /* !__ASSEMBLER__ */
 
--- include/config.h
+++ include/config.h
@@ -1894,6 +1894,7 @@
 #undef CONFIG_USB_PD_TCPM_FUSB302
 #undef CONFIG_USB_PD_TCPM_ITE83XX
 #undef CONFIG_USB_PD_TCPM_ANX74XX
+#undef CONFIG_USB_PD_TCPM_ANX7688
 
 /*
  * Use this option if the TCPC port controller supports the optional register
