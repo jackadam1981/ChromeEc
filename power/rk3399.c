67214949ce8240bc16789e7db9757c1df1e75368
Shawn Nematbakhsh <shawnn@chromium.org>
2016-04-25 11:20:58 -0700

    gru: Initial mainboard commit
    Clone of kevin w/ minor GPIO / LED changes.
    
    BUG=chrome-os-partner:52736
    BRANCH=None
    TEST=Verify image boots + sequences on kevin p1.
    
    Change-Id: I7d3f3ce97a8b080516b635a3d2b7bc3c6515c6d9
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- power/rk3399.c
+++ power/rk3399.c
@@ -179,6 +179,10 @@ enum power_state power_handle_state(enum power_state state)
 		/* Enable wireless */
 		wireless_set_state(WIRELESS_ON);
 
+#ifdef BOARD_GRU
+		gpio_set_level(GPIO_USB_A_EN, 1);
+#endif
+
 		/* Call hooks now that rails are up */
 		hook_notify(HOOK_CHIPSET_RESUME);
 
@@ -197,6 +201,10 @@ enum power_state power_handle_state(enum power_state state)
 
 		/* Suspend wireless */
 		wireless_set_state(WIRELESS_SUSPEND);
+
+#ifdef BOARD_GRU
+		gpio_set_level(GPIO_USB_A_EN, 0);
+#endif
 
 		/*
 		 * Enable idle task deep sleep. Allow the low power idle task
