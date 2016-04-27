315ddc5173aacb9d95eafc1dac6cdcb705526870
nagendra modadugu <ngm@google.com>
2016-04-27 00:07:11 -0700

    CR50: port dcrypto/cr50 code to depend on third_party/cryptoc
    Port SHA and P256 code to depend on third_party/cryptoc.
    Remove unused config options CONFIG_SHA384, CONFIG_SHA512.
    
    BRANCH=none
    BUG=chrome-os-partner:43025,chrome-os-partner:47524
    TEST=all tests in test/tpm_test/tpmtest.py pass
    
    Change-Id: If7da02849aba9703573559370af5fae721d594fc
    Signed-off-by: nagendra modadugu <ngm@google.com>

--- board/cr50/tpm2/hash.c
+++ board/cr50/tpm2/hash.c
@@ -42,6 +42,8 @@ uint16_t _cpri__GetHashBlockSize(TPM_ALG_ID alg)
 	return lookup_hash_info(alg)->blockSize;
 }
 
+/* Keep in sync with -DUSER_MIN_HASH_STATE_SIZE in build.mk */
+BUILD_ASSERT(sizeof(LITE_SHA256_CTX) == 112);
 BUILD_ASSERT(sizeof(CPRI_HASH_STATE) == sizeof(EXPORT_HASH_STATE));
 void _cpri__ImportExportHashState(CPRI_HASH_STATE *osslFmt,
 				EXPORT_HASH_STATE *externalFmt,
@@ -99,11 +101,11 @@ uint16_t _cpri__StartHash(TPM_ALG_ID alg, BOOL sequence,
 	switch (alg) {
 	case TPM_ALG_SHA1:
 		DCRYPTO_SHA1_init(ctx, sequence);
-		result = DCRYPTO_HASH_size(ctx);
+		result = HASH_size(ctx);
 		break;
 	case TPM_ALG_SHA256:
 		DCRYPTO_SHA256_init(ctx, sequence);
-		result = DCRYPTO_HASH_size(ctx);
+		result = HASH_size(ctx);
 		break;
 /* TODO: add support for SHA384 and SHA512
  *	case TPM_ALG_SHA384:
@@ -128,7 +130,7 @@ void _cpri__UpdateHash(CPRI_HASH_STATE *state, uint32_t in_len,
 {
 	struct HASH_CTX *ctx = (struct HASH_CTX *) state->state;
 
-	DCRYPTO_HASH_update(ctx, in, in_len);
+	HASH_update(ctx, in, in_len);
 }
 
 uint16_t _cpri__CompleteHash(CPRI_HASH_STATE *state,
@@ -136,8 +138,8 @@ uint16_t _cpri__CompleteHash(CPRI_HASH_STATE *state,
 {
 	struct HASH_CTX *ctx = (struct HASH_CTX *) state->state;
 
-	out_len = MIN(DCRYPTO_HASH_size(ctx), out_len);
-	memcpy(out, DCRYPTO_HASH_final(ctx), out_len);
+	out_len = MIN(HASH_size(ctx), out_len);
+	memcpy(out, HASH_final(ctx), out_len);
 	return out_len;
 }
 
