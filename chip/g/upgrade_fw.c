9ae4745df9b01b23d9a6051063eddad21584c6f5
nagendra modadugu <ngm@google.com>
2016-04-27 00:07:11 -0700

    CR50: port dcrypto/cr50 code to depend on third_party/cryptoc
    Port SHA and P256 code to depend on third_party/cryptoc.
    Remove config options CONFIG_SHA1, and CONFIG_SHA256 as
    these are provided by third_party/cryptoc.
    
    Also remove unused config options CONFIG_SHA384, CONFIG_SHA512.
    
    BRANCH=none
    BUG=chrome-os-partner:43025,chrome-os-partner:47524,chrome-os-partner:53782
    TEST=all tests in test/tpm_test/tpmtest.py pass
    
    Change-Id: If7da02849aba9703573559370af5fae721d594fc
    Signed-off-by: nagendra modadugu <ngm@google.com>

--- chip/g/upgrade_fw.c
+++ chip/g/upgrade_fw.c
@@ -10,8 +10,9 @@
 #include "flash.h"
 #include "hooks.h"
 #include "include/compile_time_macros.h"
-#include "sha1.h"
 #include "uart.h"
+
+#include "cryptoc/sha.h"
 
 #define CPRINTF(format, args...) cprintf(CC_EXTENSION, format, ## args)
 
@@ -90,7 +91,7 @@ void fw_upgrade_command_handler(void *body,
 {
 	struct upgrade_command *cmd_body = body;
 	uint8_t *rv = body;
-	uint8_t sha1_digest[SHA1_DIGEST_SIZE];
+	uint8_t sha1_digest[SHA_DIGEST_SIZE];
 	size_t body_size;
 	uint32_t block_offset;
 
