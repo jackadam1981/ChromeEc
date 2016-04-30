71f70ca646c5a81240fa1cfe9a31b93eea6df183
Wonjoon Lee <woojoo.lee@samsung.com>
2016-04-30 16:51:50 +0900

    npcx: spi: common disable based on board struct
    We have two port(as is FALSH_, ACCEL_) on SPI define
    Let's prevent build error so that We can use particular
    enable/disable port
    Also add higher priority for spi init
    
    BUG=None
    TEST=Buildall is OK
    
    Change-Id: Ib6fe14c4edd91947bde0a2da1c889da31db291a4
    Signed-off-by: Wonjoon Lee <woojoo.lee@samsung.com>

--- chip/npcx/spi.c
+++ chip/npcx/spi.c
@@ -186,12 +186,14 @@ int spi_transaction(const struct spi_device_t *spi_device,
  */
 static void spi_init(void)
 {
+	int i;
 	/* Enable clock for SPI peripheral */
 	clock_enable_peripheral(CGC_OFFSET_SPI, CGC_SPI_MASK,
 			CGC_MODE_RUN | CGC_MODE_SLEEP);
 
 	/* Disabling spi module */
-	spi_enable(CONFIG_SPI_FLASH_PORT, 0);
+	for (i = 0; i < spi_devices_used; i++)
+		spi_enable(i, 0);
 
 	/* Disabling spi irq */
 	CLEAR_BIT(NPCX_SPI_CTL1, NPCX_SPI_CTL1_EIR);
@@ -213,11 +215,12 @@ static void spi_init(void)
 	/* Cleaning junk data in the buffer */
 	clear_databuf();
 }
-DECLARE_HOOK(HOOK_INIT, spi_init, HOOK_PRIO_DEFAULT);
+//TODO : Need PRIO_SPI for future use?
+DECLARE_HOOK(HOOK_INIT, spi_init, HOOK_PRIO_DEFAULT - 1);
 
 /*****************************************************************************/
 /* Console commands */
-
+#ifdef CONFIG_CMD_SPI_FLASH
 static int printrx(const char *desc, const uint8_t *txdata, int txlen,
 		int rxlen)
 {
@@ -235,7 +238,6 @@ static int printrx(const char *desc, const uint8_t *txdata, int txlen,
 	CPUTS("\n");
 	return EC_SUCCESS;
 }
-
 
 static int command_spirom(int argc, char **argv)
 {
@@ -261,3 +263,4 @@ DECLARE_CONSOLE_COMMAND(spirom, command_spirom,
 		NULL,
 		"Test reading SPI EEPROM",
 		NULL);
+#endif
