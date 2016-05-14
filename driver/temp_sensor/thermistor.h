0c6e999298b6b3d57694a1a0903b127fb1f9e2c3
David Hendricks <dhendrix@chromium.org>
2016-05-13 20:24:56 -0700

    thermistor: Add generic linear interpolation algorithm
    The existing algorithm makes several assumptions for a particular
    thermistor circuit. This patch introduces a more generic version
    that can be used for multiple thermistors on a single board.
    
    The idea is to approximate a curve produced by solving for voltage
    measued by an ADC using the Steinhart-Hart equation. For a straight
    line one only needs two data points. For a steady curve data
    points can be distributed evenly. For the most part, though, data
    points should be provided after a significant change in slope.
    
    More data points give more accuracy at the expense of memory, and
    we mostly only care about accuracy in the range between "warm"
    and "too hot" so only a few data points should be used.
    
    BUG=chrome-os-partner:53035
    BRANCH=none
    TEST=added unit test, needs real testing
    
    Change-Id: I046e61dbfd1e8c26c2a533777f222f5413938556
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- driver/temp_sensor/thermistor.h
+++ driver/temp_sensor/thermistor.h
@@ -8,6 +8,39 @@
 #ifndef __CROS_EC_TEMP_SENSOR_THERMISTOR_H
 #define __CROS_EC_TEMP_SENSOR_THERMISTOR_H
 
+struct thermistor_data_pair {
+	uint8_t mv;	/* Scaled voltage level at ADC (in mV) */
+	uint8_t temp;	/* Temperature in Celsius */
+};
+
+struct thermistor_info {
+	uint8_t scaling_factor;	/* Scaling factor for voltage in data pair. */
+	uint8_t num_pairs;	/* Number of data pairs. */
+	/*
+	 * Must contain at least two pairs. Values between given data pairs
+	 * will be calculated as points on a line. Pairs must be sorted in
+	 * ascending order. More pairs gives better accuracy at the expense
+	 * of memory, so insert points where the curve changes significantly.
+	 */
+	struct thermistor_data_pair *data;
+};
+
+/**
+ * @brief Calculate temperature using linear interpolation of data points.
+ *
+ * Given a set of datapoints, the algorithm will calculate the "step" in
+ * between each one in order to interpolate missing entries. Data points
+ * should be derived using Steinhart-Hart equation. Data points should be
+ * used after significant changes in slope to increase accuracy.
+ *
+ * @param mv	Value read from ADC (in millivolts).
+ * @param info	Thermistor reference point info.
+ *
+ * @return	temperature in C, or negative value to indicate error
+ */
+int thermistor_linear_interpolate(uint16_t mv,
+				const struct thermistor_info *info);
+
 /**
  * ncp15wb temperature conversion routine.
  *
