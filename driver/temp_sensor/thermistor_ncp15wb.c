76e86ce6e0f03ce12dfb4ba8f14fece9bdaab627
David Hendricks <dhendrix@chromium.org>
2016-05-13 20:24:56 -0700

    thermistor: Add generic linear interpolation algorithm
    The existing algorithm makes several assumptions for a particular
    thermistor circuit. This patch introduces a more generic version
    that can be used for multiple thermistors on a single board.
    
    The idea is to approximate a curve produced by solving for voltage
    measued by an ADC using the Steinhart-Hart equation. For a straight
    line one only needs two data points. For a steady curve data
    points can be distributed evenly. For the most part, though, data
    points should be provided after a significant change in slope.
    
    More data points give more accuracy at the expense of memory, and
    we mostly only care about accuracy in the range between "warm"
    and "too hot" so only a few data points should be used.
    
    BUG=chrome-os-partner:53035
    BRANCH=none
    TEST=added unit test, needs real testing
    
    Change-Id: I046e61dbfd1e8c26c2a533777f222f5413938556
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- driver/temp_sensor/thermistor_ncp15wb.c
+++ driver/temp_sensor/thermistor_ncp15wb.c
@@ -98,3 +98,51 @@ int ncp15wb_calculate_temp(uint16_t adc)
 
 	return temp;
 }
+
+int thermistor_linear_interpolate(uint16_t mv,
+		const struct thermistor_info *info)
+{
+	int i, approx_temp;
+	const struct thermistor_data_pair *data = info->data;
+
+	/* We need at least two points to form a line. */
+	ASSERT(info->num_pairs >= 2);
+
+	/* If input value is out of bounds, return the lowest or highest
+	 * value in the data sets provided. */
+	if (mv < data[0].mv * info->scaling_factor)
+		return data[0].temp;
+	else if (mv > data[info->num_pairs - 1].mv * info->scaling_factor)
+		return data[info->num_pairs - 1].temp;
+
+	for (i = 0; i < info->num_pairs - 1; i++) {
+		int step_mv, num_steps;
+		int approx_mv;
+		int v0 = data[i].mv * info->scaling_factor;
+		int v1 = data[i + 1].mv * info->scaling_factor;
+
+		if ((mv < v0) || (mv > v1))
+			continue;
+
+		num_steps = data[i + 1].temp - data[i].temp;
+		step_mv = (v1 - v0) / num_steps;
+
+		approx_mv = v0;
+		approx_temp = data[i].temp;
+		while (approx_mv < mv) {
+			/* break if next step will be further away from mv */
+			if (approx_mv + step_mv > mv) {
+				int d0 = mv - approx_mv;
+				int d1 = approx_mv + step_mv - mv;
+
+				if (d0 < d1)
+					break;
+			}
+
+			approx_mv += step_mv;
+			approx_temp++;
+		}
+	}
+
+	return approx_temp;
+}
