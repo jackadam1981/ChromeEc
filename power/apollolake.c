025abf29ae10b8fe6ec26ac96ea40d33ebf70faf
David Hendricks <dhendrix@chromium.org>
2016-05-15 17:53:21 -0700

    apollolake: Add extra prints for debugging.
    Do not submit, this is just to help with initial bring-up.
    
    BUG=none
    BRANCH=none
    TEST=compiled
    
    Change-Id: I0791ba817b014a2c79b3db2575d99791d64837ca
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- power/apollolake.c
+++ power/apollolake.c
@@ -175,9 +175,11 @@ static enum power_state _power_handle_state(enum power_state state)
 
 	switch (state) {
 	case POWER_G3:
+		CPRINTS("%s: entered G3\n", __func__);
 		break;
 
 	case POWER_S5:
+		CPRINTS("%s: entering S5\n", __func__);
 #ifdef CONFIG_BOARD_HAS_RTC_RESET
 		/* Wait for S5 exit and attempt RTC reset it supported */
 		if (power_s5_up)
@@ -195,6 +197,7 @@ static enum power_state _power_handle_state(enum power_state state)
 		break;
 
 	case POWER_S3:
+		CPRINTS("%s: entering S3\n", __func__);
 		if (!power_has_signals(IN_PGOOD_ALL_CORE)) {
 			/* Required rail went away */
 			chipset_force_shutdown();
@@ -209,6 +212,7 @@ static enum power_state _power_handle_state(enum power_state state)
 		break;
 
 	case POWER_S0:
+		CPRINTS("%s: entering S0\n", __func__);
 		if (!power_has_signals(IN_PGOOD_ALL_CORE)) {
 			chipset_force_shutdown();
 			return POWER_S0S3;
@@ -226,6 +230,7 @@ static enum power_state _power_handle_state(enum power_state state)
 
 #ifdef CONFIG_POWER_S0IX
 	case POWER_S0ix:
+		CPRINTS("%s: entering S0ix\n", __func__);
 		/*
 		 * TODO: add code for unexpected power loss
 		 */
@@ -238,6 +243,7 @@ static enum power_state _power_handle_state(enum power_state state)
 #endif
 
 	case POWER_G3S5:
+		CPRINTS("%s: transitioning from G3 to S5\n", __func__);
 		/* Platform is powering up, clear forcing_coldreset */
 		forcing_coldreset = 0;
 
@@ -281,6 +287,7 @@ static enum power_state _power_handle_state(enum power_state state)
 		return POWER_S5;
 
 	case POWER_S5S3:
+		CPRINTS("%s: transitioning from S5 to S3\n", __func__);
 		if (!power_has_signals(IN_PGOOD_ALL_CORE)) {
 			/* Required rail went away */
 			chipset_force_shutdown();
@@ -292,6 +299,7 @@ static enum power_state _power_handle_state(enum power_state state)
 		return POWER_S3;
 
 	case POWER_S3S0:
+		CPRINTS("%s: transitioning from S3 to S0\n", __func__);
 		if (!power_has_signals(IN_PGOOD_ALL_CORE)) {
 			/* Required rail went away */
 			chipset_force_shutdown();
@@ -321,6 +329,7 @@ static enum power_state _power_handle_state(enum power_state state)
 		return POWER_S0;
 
 	case POWER_S0S3:
+		CPRINTS("%s: transitioning from S0 to S3\n", __func__);
 		/* Call hooks before we remove power rails */
 		hook_notify(HOOK_CHIPSET_SUSPEND);
 
@@ -339,6 +348,7 @@ static enum power_state _power_handle_state(enum power_state state)
 
 #ifdef CONFIG_POWER_S0IX
 	case POWER_S0S0ix:
+		CPRINTS("%s: transitioning from S0 to S0ix\n", __func__);
 		/* call hooks before standby */
 		hook_notify(HOOK_CHIPSET_SUSPEND);
 
@@ -354,6 +364,7 @@ static enum power_state _power_handle_state(enum power_state state)
 
 
 	case POWER_S0ixS0:
+		CPRINTS("%s: transitioning from S0ix to S0\n", __func__);
 		lpc_disable_wake_mask_for_lid_open();
 
 		/* Call hooks now that rails are up */
@@ -369,6 +380,7 @@ static enum power_state _power_handle_state(enum power_state state)
 #endif
 
 	case POWER_S3S5:
+		CPRINTS("%s: transitioning from S3 to S5\n", __func__);
 		/* Call hooks before we remove power rails */
 		hook_notify(HOOK_CHIPSET_SHUTDOWN);
 
@@ -382,6 +394,7 @@ static enum power_state _power_handle_state(enum power_state state)
 		return POWER_S5;
 
 	case POWER_S5G3:
+		CPRINTS("%s: transitioning from S5 to G3\n", __func__);
 		chipset_force_shutdown();
 
 		/* Power up the platform again for forced cold reset */
