e776fe022f123f4a728f796d960cdbd94509f96e
Ryan Zhang <Ryan.Zhang@quantatw.com>
2016-05-16 10:31:29 +0800

    COMMON: Add extend function for tmp432 IC
    + create interface to set ALERT# pin as THERM mode
    
    BUG=None
    BRANCH=master
    TEST=`make -j buildall`
    
    Change-Id: I7325eb9ddfcaca3ea873e1ee71da74258d7bec72
    Signed-off-by: Ryan Zhang <Ryan.Zhang@quantatw.com>

--- driver/temp_sensor/tmp432.c
+++ driver/temp_sensor/tmp432.c
@@ -37,9 +37,19 @@ static int raw_read8(const int offset, int *data_ptr)
 	return i2c_read8(I2C_PORT_THERMAL, TMP432_I2C_ADDR, offset, data_ptr);
 }
 
+static int raw_read16(const int offset, int *data_ptr)
+{
+	return i2c_read16(I2C_PORT_THERMAL, TMP432_I2C_ADDR, offset, data_ptr);
+}
+
 static int raw_write8(const int offset, int data)
 {
 	return i2c_write8(I2C_PORT_THERMAL, TMP432_I2C_ADDR, offset, data);
+}
+
+static int raw_write16(const int offset, int data)
+{
+	return i2c_write16(I2C_PORT_THERMAL, TMP432_I2C_ADDR, offset, data);
 }
 
 static int get_temp(const int offset, int *temp_ptr)
@@ -115,6 +125,31 @@ static int tmp432_shutdown(uint8_t want_shutdown)
 
 	is_sensor_shutdown = want_shutdown;
 	return ret;
+}
+
+int tmp432_set_therm_mode(int limit_degree, int hysteresis)
+{
+	int ret = 0;
+	int data = 0;
+
+	ret = raw_read16(TMP432_CONFIGURATION1_R, &data);
+	if (ret)
+		return EC_ERROR_UNKNOWN;
+
+	data |= TMP432_CONFIG1_MODE;
+	ret = raw_write8(TMP432_CONFIGURATION1_W, data);
+	if (ret)
+		return EC_ERROR_UNKNOWN;
+
+	ret = raw_write16(TMP432_LOCAL_HIGH_LIMIT_W, limit_degree);
+	if (ret)
+		return EC_ERROR_UNKNOWN;
+
+	ret = raw_write8(TMP432_THERM_HYSTERESIS, hysteresis);
+	if (ret)
+		return EC_ERROR_UNKNOWN;
+
+	return EC_SUCCESS;
 }
 
 static void temp_sensor_poll(void)
