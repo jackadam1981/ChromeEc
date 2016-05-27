ad95c6965e714a8c7556e5f3012ace3ffbfa3756
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-05-26 18:45:21 -0700

    reef: Initialize charge suppliers after change manager is init
    Initialize the charge suppliers after change manager is initialized,
    otherwise charge supplier current & voltage values will be overwritten
    to -1 by the charge manager ini function.
    
    BUG=chrome-os-partner:53788
    BRANCH=None
    TEST=Observed there are no "CL: p(port) s(supplier) i-1 v-1" prints
         on the EC console.
    
    Change-Id: Id0212c502d5833c016ac79ee15d21304d6d7ceb2
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- include/hooks.h
+++ include/hooks.h
@@ -39,6 +39,8 @@ enum hook_priority {
 	HOOK_PRIO_INIT_EXTPOWER = HOOK_PRIO_FIRST + 9,
 	/* Init VBOOT hash later, since it depends on deferred functions */
 	HOOK_PRIO_INIT_VBOOT_HASH = HOOK_PRIO_FIRST + 10,
+	/* Priority to initialize charger suppliers */
+	HOOK_PRIO_CHARGE_MANAGER_INIT = HOOK_PRIO_DEFAULT + 11,
 
 	/* Specific values to lump temperature-related hooks together */
 	HOOK_PRIO_TEMP_SENSOR = 6000,
