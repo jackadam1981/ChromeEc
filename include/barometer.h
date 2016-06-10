eb4263e348cc78081f74e229b23c4b5a339331a3
Divya Sasidharan <divya.s.sasidharan@intel.com>
2016-06-09 17:00:09 -0700

    barometer: Add barometer driver for BMP280 in EC
    BMP280 supports pressure and temperature sensor.
    This code adds only the pressure related APIs in
    the driver.
    
    Added baro_init and baro_read console commands
    baro_init: Initialize barometer with default configuration
    baro_read: Reads compensated pressure value in Pa
    baro_swm: Set barometer work mode
    baro_read_temp: Reads room temperature
    
    TEST=baro_init command return successful
         baro_read returns pressure value in Pa
    BRANCH=None
    BUG=None
    
    Change-Id: I3f4c0c33a77dd317aa1425624d3cc7f4ec6b45a1
    Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>

---
+++ include/barometer.h
@@ -0,0 +1,46 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_BAROMETER_H
+#define __CROS_EC_BAROMETER_H
+
+#include "common.h"
+
+enum barosensor_func {
+	BAROSENSOR_FUNC_PRESSURE = 1,
+	BAROSENSOR_FUNC_TEMPERATURE = 2,
+	BAROSENSOR_FUNC_MAX,
+};
+
+struct baro_sensor_t {
+	char *name;
+	uint32_t port;
+	uint32_t addr;
+	uint32_t mode;
+	/* Brometer may have multiple sensor options in-built into
+	 * Pressure or temperature - can be removed */
+	enum barosensor_func func;
+	int (*init)(const struct baro_sensor_t *s);
+	int (*read)(int *val);
+	/* Calculate and return compensated pressure value */
+	int (*comp)(int pressure);
+	/* Set work mode */
+	int (*set_work_mode)(uint8_t mode);
+
+};
+
+
+extern const struct baro_sensor_t baro[];
+/**
+ * Read pressure
+ *
+ * @param id		Which one?
+ * @param pressure      Value read in units
+ *
+ * @return EC_SUCCESS, or non-zero if error.
+ */
+int get_pressure(int id, int *pressure);
+
+#endif
