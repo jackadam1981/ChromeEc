5da605d412649a71e6612e73bd250228942e2081
David Hendricks <dhendrix@chromium.org>
2016-06-28 22:01:26 -0700

    reef: Wait until vboot hash finishes before going to S0
    This checks if the vboot hash is finished before letting the SoC
    go into S0. The reason for this is because with the previous patches
    applied, the SoC starts several hundred milliseconds earlier and
    coreboot gets to verstage before the EC finishes its hash.
    
    BUG=none
    BRANCH=none
    TEST=SoC boots without failing in verstage and rebooting first.
    
    Change-Id: I4ada79aa812c37416e297dd6318c91125b8c37f7
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- board/reef/board.c
+++ board/reef/board.c
@@ -48,6 +48,7 @@
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
 #include "util.h"
+#include "vboot_hash.h"
 
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
@@ -357,6 +358,9 @@ static void board_chipset_resume(void)
 		enable_sensor_i2c_bus();
 		sensor_bus_enabled = 1;
 	}
+
+	while (vboot_hash_in_progress())
+		msleep(1);
 }
 /* Pin config must happen before sensor tasks begin. */
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_chipset_resume, HOOK_PRIO_FIRST);
