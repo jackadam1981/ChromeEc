310cf53524b35143e03fc54910853779b38d5299
Chris Chen <twothreecc@google.com>
2016-07-01 13:29:00 -0700

    cts: First patch flashes blank tests.
    The first time you use this with a particular th,
    connect only th and run ./cts.py --th
    
    Then connect both boards and you can run
    ./cts.py to build/flash both boards.
    
    BRANCH=None
    BUG=None
    TEST=None
    
    Change-Id: Ib14fccabcd9fdad04f9b92817da597bc0dcb3d89

---
+++ cts/build.mk
@@ -0,0 +1,11 @@
+# -*- makefile -*-
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+ifeq ($(BOARD),stm32l476g-eval)
+	$(CTS_MODULE)-y+=$(CTS_MODULE)/th.o
+else
+	$(CTS_MODULE)-y+=$(CTS_MODULE)/dut.o
+endif
