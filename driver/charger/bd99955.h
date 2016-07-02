ee862cb79c65e5c90aba086e0d28fcececde4f9f
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-07-01 19:03:12 -0700

    Amenia/Reef: Add software charge ramp for BC1.2 & nonstandard BC1.2
    Setting the higher limit of input current for BC1.2 & nonstandard
    BC1.2 devices than their maximum current rating results in an
    anti-collapse. BD99955 does not have a way to do hardware charge
    ramp or to detect the anti-collapse for these chargers. Hence added
    code to support software charge ramp for BC1.2 & nonstandard BC1.2
    so that the input current is set to maximum of the respective
    charger.
    
    BUG=chrome-os-partner:54990
    BRANCH=none
    TEST=Manually tested on Amenia & Reef. BC1.2 & nonstandard BC12
         devices can negotiate their respective maximum current rating.
    
    Change-Id: I0033b3662362bd7822ad01cf4360d18caabd5249
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- driver/charger/bd99955.h
+++ driver/charger/bd99955.h
@@ -281,6 +281,8 @@ enum bd99955_charge_port {
 int bd99955_is_vbus_provided(int port);
 /* Select input port from {VCC, VBUS, VCC&VBUS, NONE}. */
 int bd99955_select_input_port(enum bd99955_charge_port port);
+/* Get input current limit for BC1.2 suppliers */
+int bd99955_get_bc12_ilim(int charge_supplier);
 /* Enable/Disable charging triggered by BC1.2 */
 int bd99955_bc12_enable_charging(enum bd99955_charge_port port, int enable);
 /* Interrupt handler for USB charger VBUS */
