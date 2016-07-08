2ff8888834ae8990ed47bbec9934f6cb6e621e66
David Hendricks <dhendrix@chromium.org>
2016-07-07 18:09:33 -0700

    reef: Update PD reset GPIOs
    This makes the following changes for EVT:
    - Rename USB_PD_RST_ODL to USB_C0_PD_RST_ODL
    - Add USB_C1_PD_RST_ODL
    
    BUG=chrome-os-partner:54952
    BRANCH=none
    TEST=needs testing
    
    Change-Id: Iafff8934824bb8c9bc5e7f46b8062acad87990be
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- board/reef/board.c
+++ board/reef/board.c
@@ -58,7 +58,8 @@
 
 static void tcpc_alert_event(enum gpio_signal signal)
 {
-	if (!gpio_get_level(GPIO_USB_PD_RST_ODL))
+	if (!gpio_get_level(GPIO_USB_C0_PD_RST_ODL) &&
+		!gpio_get_level(GPIO_USB_C1_PD_RST_ODL))
 		return;
 
 #ifdef HAS_TASK_PDCMD
@@ -167,10 +168,7 @@ void board_set_tcpc_power_mode(int port, int mode)
 	gpio_set_level(GPIO_EN_USB_TCPC_PWR, mode);
 	msleep(mode ? 10 : 1);
 
-	/* FIXME(dhendrix): This is also connected to the PS8751 which
-	 * we might not want to reset just because something happened
-	 * on the ANX3429. */
-	gpio_set_level(GPIO_USB_PD_RST_ODL, mode);
+	gpio_set_level(GPIO_USB_C0_PD_RST_ODL, mode);
 	msleep(10);
 }
 
@@ -181,14 +179,16 @@ void board_set_tcpc_power_mode(int port, int mode)
  */
 void board_reset_pd_mcu(void)
 {
-	gpio_set_level(GPIO_USB_PD_RST_ODL, 0);
+	gpio_set_level(GPIO_USB_C0_PD_RST_ODL, 0);
+	gpio_set_level(GPIO_USB_C1_PD_RST_ODL, 0);
 	msleep(1);
 	gpio_set_level(GPIO_EN_USB_TCPC_PWR, 0);
 	msleep(10);
 
 	gpio_set_level(GPIO_EN_USB_TCPC_PWR, 1);
 	msleep(10);
-	gpio_set_level(GPIO_USB_PD_RST_ODL, 1);
+	gpio_set_level(GPIO_USB_C0_PD_RST_ODL, 0);
+	gpio_set_level(GPIO_USB_C1_PD_RST_ODL, 0);
 	/*
 	 * ANX7688 needed 50ms to release RESET_N, but the ANX7428 datasheet
 	 * does not indicate such a long delay is necessary. Leave it in due
