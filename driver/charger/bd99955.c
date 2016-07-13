8e41d0ca30cc776d98d0a95585e78ae01e5a59fb
Aseda Aboagye <aaboagye@google.com>
2016-07-13 14:22:20 -0700

    bd99955: Make changes for new OTP change.
    There's a new OTP change for the BD99955 and therefore the following
    changes needed to be made.
    
     - Change VFASTCHG_SET1 to 8.704V before CHG_EN is set to 1.
     - Change VSYS_REG to 6.144V when starting Fast Charging.
     - Change VSYS_REG back to 8.906V when Fast Charge is finished.
     - Wait for 50ms to set CHG_EN to off (0) after Fast Charging has ended.
    
    BUG=chrome-os-partner:55220
    BUG=chrome-os-partner:55238
    BRANCH=None
    TEST=Flash kevin, plug discharged battery in. Plug AC in. `bd99955 r
    0x11 1' and verify that VSYSREG is set to 0x1800 (6144mV).
    TEST=`bd99955 r 0x1a 1' and verify that VFASTCHARGE is set to
    0x2200 (8704mV).
    TEST=Remove battery. `bd99955 r 0x11 1' and verify that VSYSREG is set
    to 0x2300 (8960 mV).
    TEST=Plug in battery and let charge to full. Verify that VSYSREG is set
    to 0x2300 (8960 mV).
    
    Change-Id: I5e5ca2cdcd4ead383416901c904df1e6fe5a9e28
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- driver/charger/bd99955.c
+++ driver/charger/bd99955.c
@@ -16,6 +16,7 @@
 #include "i2c.h"
 #include "task.h"
 #include "time.h"
+#include "timer.h"
 #include "util.h"
 #include "usb_charge.h"
 #include "usb_pd.h"
@@ -393,6 +394,15 @@ static void usb_charger_process(enum bd99955_charge_port port)
 }
 #endif /* HAS_TASK_USB_CHG */
 
+static int bd99955_set_vsysreg(int voltage)
+{
+	/* VSYS Regulation voltage is in 64mV steps. */
+	voltage &= 0x7F;
+
+	return ch_raw_write16(BD99955_CMD_VSYSREG_SETa, voltage,
+			      BD99955_EXTENDED_COMMAND);
+}
+
 /* chip specific interfaces */
 
 int charger_set_input_current(int input_current)
@@ -547,6 +557,28 @@ int charger_get_status(int *status)
 int charger_set_mode(int mode)
 {
 	int rv;
+
+	if (mode & CHARGE_FLAG_INHIBIT_CHARGE) {
+		rv |= bd99955_set_vsysreg(DISCHARGE_VSYSREG);
+		msleep(50);
+		rv |= bd99955_charger_enable(0);
+	} else {
+		rv |= bd99955_set_vsysreg(CHARGE_VSYSREG);
+		rv |= bd99955_charger_enable(1);
+	}
+	if (rv)
+		return rv;
+	if (!(mode & CHARGE_FLAG_INHIBIT_CHARGE)) {
+		/*
+		 * Set fast charge voltage before enabling charging.  The fast
+		 * charge voltage is the maximum voltage of the battery.
+		 */
+		rv = ch_raw_write16(BD99955_CMD_VFASTCHG_REG_SET1,
+				    battery_get_info()->voltage_max,
+				    BD99955_EXTENDED_COMMAND);
+		if (rv)
+			return rv;
+	}
 
 	rv = bd99955_charger_enable(mode & CHARGE_FLAG_INHIBIT_CHARGE ? 0 : 1);
 	if (rv)
