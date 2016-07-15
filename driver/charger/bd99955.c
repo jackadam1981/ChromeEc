a6554642c4ad67c7e3ebdd4a2b4bd56ce5460bcf
David Hendricks <dhendrix@chromium.org>
2016-07-14 18:56:26 -0700

    bd99955: Add function for reading temperature
    BUG=chrome-os-partner:54818
    BRANCH=none
    TEST=needs testing
    
    Change-Id: I3a33f79e7d57e6f94731a7d929dbcd083e0f1ca1
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- driver/charger/bd99955.c
+++ driver/charger/bd99955.c
@@ -757,6 +757,19 @@ int bd99955_select_input_port(enum bd99955_charge_port port)
 			      BD99955_EXTENDED_COMMAND);
 }
 
+int bd99955_get_temp(void)
+{
+	int rv, data;
+
+	rv = ch_raw_read16(BD99955_CMD_THERM_VAL, &data,
+			BD99955_EXTENDED_COMMAND);
+	if (rv)
+		return -1;
+
+	/* Degrees C = 200 - THERM_VAL, range is -55C-200C, 1C steps */
+	return 200 - data;
+}
+
 #ifdef HAS_TASK_USB_CHG
 int bd99955_bc12_enable_charging(enum bd99955_charge_port port, int enable)
 {
