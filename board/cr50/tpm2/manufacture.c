834d70e19ce355f334ccb4caf1aeb5ea43b7c9a3
nagendra modadugu <ngm@google.com>
2016-07-15 23:09:15 -0700

    CR50: zeroize manufacture related structs prior to use
    Zeroize structs are only partially populated by the caller.
    
    BRANCH=none
    BUG=chrome-os-partner:43025,chrome-os-partner:47524
    BUG=chrome-os-partner:50115,chrome-os-partner:55260
    TEST=build succeeds
    
    Change-Id: I82a0758bedcf7f0565c8a541dfdc49c6bc0e9d0c
    Signed-off-by: nagendra modadugu <ngm@google.com>

--- board/cr50/tpm2/manufacture.c
+++ board/cr50/tpm2/manufacture.c
@@ -383,6 +383,9 @@ static int store_cert(enum cros_perso_component_type component_type,
 	TPMA_NV space_attributes;
 	NV_Write_In in;
 
+	memset(&define_space, 0, sizeof(define_space));
+	memset(&space_attributes, 0, sizeof(space_attributes));
+
 	/* Indicate that a system reset has occurred, and currently
 	 * running with Platform auth.
 	 */
