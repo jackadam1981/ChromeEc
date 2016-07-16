a07cbc5427391c859a94a51cf284da41f88449c7
David Hendricks <dhendrix@chromium.org>
2016-07-15 21:32:26 -0700

    reef: Set IS_PROTO to 0 by default
    Also add a hook that will print an error to the console every second
    if the wrong EC firmware is being used.
    
    BUG=chrome-os-partner:54947
    BRANCH=none
    TEST=tested on proto and EVT units
    
    Change-Id: I8f42495e81d938f3d660d901930f7e71bcb42ceb
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- board/reef/board.c
+++ board/reef/board.c
@@ -498,6 +498,7 @@ static void board_chipset_startup(void)
 	gpio_set_level(GPIO_EN_USB_A_5V, 1);
 
 	hook_call_deferred(&enable_input_devices_data, 0);
+
 }
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup, HOOK_PRIO_DEFAULT);
 
@@ -828,3 +829,26 @@ int board_get_version(void)
 	CPRINTS("Board version: %d\n", version);
 	return version;
 }
+
+/* FIXME: Remove this once proto boards are obsolete */
+static void check_ec_fw_mismatch(void)
+{
+	int board_version = board_get_version();
+
+	if (board_version == BOARD_VERSION_UNKNOWN)
+		return;
+
+	/* FIXME: Hack: Remove after proto boards are obsolete */
+#if IS_PROTO == 1
+	if (board_version != BOARD_VERSION_1) {
+		CPRINTS("ERROR: Detected proto firmware on non-proto unit.");
+		CPRINTS("Set IS_PROTO correctly in board/reef/board.h");
+	}
+#else
+	if (board_version == BOARD_VERSION_1) {
+		CPRINTS("ERROR: Detected >=EVT firmware on proto unit.");
+		CPRINTS("Set IS_PROTO correctly in board/reef/board.h");
+	}
+#endif
+}
+DECLARE_HOOK(HOOK_SECOND, check_ec_fw_mismatch, HOOK_PRIO_DEFAULT);
