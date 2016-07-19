6f3671ce81664f31750fb7f8f6ed6958efc47c3d
Vincent Palatin <vpalatin@chromium.org>
2016-07-19 11:03:08 +0200

    cr50: reset only the TPM state on SYS_RST_L
    Rather than doing a full cr50 reset when the system reset SYS_RST_L is
    asserted, just reset the state of the TPM task.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=chrome-os-partner:52366
    TEST=trigger a SYS_RST by using the 'reboot' command
    see that the cr50 is not reset and the firmware and userspace are still
    happy about the TPM.
    
    Change-Id: I32cd2bb72316f68c74db77a20a8d09112b402d4b

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -214,8 +214,8 @@ void sys_rst_asserted(enum gpio_signal signal)
 	if (ap_spi_update_in_progress())
 		return;
 
-	cflush();
-	system_reset(0);
+	/* Re-initialize the TPM software state */
+	task_set_event(TASK_ID_TPM, TPM_EVENT_RESET, 0);
 }
 
 void nvmem_compute_sha(uint8_t *p_buf, int num_bytes,
