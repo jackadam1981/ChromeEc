1cbaf5e343b569e391dfecc61223b4b81f94fd93
Nick Sanders <nsanders@chromium.org>
2016-07-19 15:33:47 -0700

    servo_micro: add USB updater
    This adds a Google FW update endpoint to
    servo micro in place of a GPIO enpoint.
    
    BUG=chromium:571477
    TEST=successfully update servo micro via usb
    BRANCH=None
    
    Signed-off-by: Nick Sanders <nsanders@chromium.org>
    
    Change-Id: I3d6c501d515b3f1db6e8259fbb829abe18f72e00

--- board/servo_micro/board.c
+++ board/servo_micro/board.c
@@ -13,6 +13,7 @@
 #include "registers.h"
 #include "spi.h"
 #include "task.h"
+#include "update_fw.h"
 #include "usart-stm32f0.h"
 #include "usart_tx_dma.h"
 #include "usart_rx_dma.h"
@@ -22,36 +23,6 @@
 #include "usb-stream.h"
 #include "util.h"
 
-
-/******************************************************************************
- * Build GPIO tables and expose a subset of the GPIOs over USB.
- */
-
-#include "gpio_list.h"
-
-static enum gpio_signal const usb_gpio_list[] = {
-GPIO_UART1_EN_L,
-GPIO_SERVO_JTAG_TRST_L,
-GPIO_SPI1_BUF_EN_L,
-GPIO_SPI2_BUF_EN_L,
-GPIO_UART2_EN_L,
-GPIO_SPI1_VREF_33,
-GPIO_SPI1_VREF_18,
-GPIO_SPI2_VREF_33,
-GPIO_SPI2_VREF_18,
-GPIO_SERVO_JTAG_TRST_DIR,
-GPIO_SERVO_JTAG_TDI_DIR,
-GPIO_TCA6416_RESET_L
-};
-
-/*
- * This instantiates struct usb_gpio_config const usb_gpio, plus several other
- * variables, all named something beginning with usb_gpio_
- */
-USB_GPIO_CONFIG(usb_gpio,
-		usb_gpio_list,
-		USB_IFACE_GPIO,
-		USB_EP_GPIO);
 
 
 /******************************************************************************
@@ -164,6 +135,7 @@ const void *const usb_strings[] = {
 	[USB_STR_CONSOLE_NAME] = USB_STRING_DESC("Servo EC Shell"),
 	[USB_STR_USART3_STREAM_NAME]  = USB_STRING_DESC("Servo UART2"),
 	[USB_STR_USART2_STREAM_NAME]  = USB_STRING_DESC("Servo UART1"),
+	[USB_STR_UPDATE_NAME]  = USB_STRING_DESC("Firmware update"),
 };
 
 BUILD_ASSERT(ARRAY_SIZE(usb_strings) == USB_STR_COUNT);
@@ -216,7 +188,6 @@ void usb_spi_board_disable(struct usb_spi_config const *config)
 
 USB_SPI_CONFIG(usb_spi, USB_IFACE_SPI, USB_EP_SPI);
 
-
 /******************************************************************************
  * Support I2C bridging over USB, this requires usb_i2c_board_enable and
  * usb_i2c_board_disable to be defined to enable and disable the SPI bridge.
@@ -230,6 +201,26 @@ const struct i2c_port_t i2c_ports[] = {
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
 USB_I2C_CONFIG(usb_i2c, USB_IFACE_I2C, USB_EP_I2C);
+
+
+/******************************************************************************
+ * Support firmware upgrade over USB. We can update whichever section is not
+ * the current section.
+ */
+
+/*
+ * This array defines possible sections available for the firmware update.
+ * The section which does not map the current executing code is picked as the
+ * valid update area. The values are offsets into the flash space.
+ */
+const struct section_descriptor board_rw_sections[] = {
+	{CONFIG_RO_MEM_OFF,
+	 CONFIG_RO_MEM_OFF + CONFIG_RO_SIZE},
+	{CONFIG_RW_MEM_OFF,
+	 CONFIG_RW_MEM_OFF + CONFIG_RW_SIZE},
+};
+const struct section_descriptor * const rw_sections = board_rw_sections;
+const int num_rw_sections = ARRAY_SIZE(board_rw_sections);
 
 
 /******************************************************************************
