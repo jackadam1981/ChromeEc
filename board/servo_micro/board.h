9956aaab0d6de3ccac1e6ee9d225aa9eaa57958e
Nick Sanders <nsanders@chromium.org>
2016-07-19 15:33:47 -0700

    servo_micro: add USB updater
    This adds a Google FW update endpoint to
    servo micro in place of a GPIO enpoint.
    
    BUG=chromium:571477
    TEST=successfully update servo micro via usb
    BRANCH=None
    
    Signed-off-by: Nick Sanders <nsanders@chromium.org>
    
    Change-Id: I3d6c501d515b3f1db6e8259fbb829abe18f72e00

--- board/servo_micro/board.h
+++ board/servo_micro/board.h
@@ -22,6 +22,8 @@
 /* The UART console is on USART1 (PA9/PA10) */
 #undef CONFIG_UART_CONSOLE
 #define CONFIG_UART_CONSOLE 1
+#undef CONFIG_UART_TX_DMA
+#undef CONFIG_UART_RX_DMA
 
 /* Optional features */
 #define CONFIG_STM_HWTIMER32
@@ -31,30 +33,32 @@
 #define CONFIG_USB
 #define CONFIG_USB_PID 0x501a
 #define CONFIG_USB_CONSOLE
+#define CONFIG_USB_FW_UPDATE
+#define CONFIG_NON_HC_FW_UPDATE
+
+#define CONFIG_USB_SERIALNO
+#define DEFAULT_SERIALNO "Uninitialized"
 
 /* USB interface indexes (use define rather than enum to expand them) */
-#define USB_IFACE_USART4_STREAM  0
-#define USB_IFACE_GPIO    1
-#define USB_IFACE_SPI     2
-#define USB_IFACE_CONSOLE 3
-#define USB_IFACE_I2C     4
-#define USB_IFACE_USART3_STREAM  5
-#define USB_IFACE_USART2_STREAM  6
-#define USB_IFACE_COUNT   7
+#define USB_IFACE_USART4_STREAM	0
+#define USB_IFACE_UPGRADE	1
+#define USB_IFACE_SPI		2
+#define USB_IFACE_CONSOLE	3
+#define USB_IFACE_I2C		4
+#define USB_IFACE_USART3_STREAM	5
+#define USB_IFACE_USART2_STREAM	6
+#define USB_IFACE_COUNT		7
 
 /* USB endpoint indexes (use define rather than enum to expand them) */
-#define USB_EP_CONTROL 0
-#define USB_EP_USART4_STREAM  1
-#define USB_EP_GPIO    2
-#define USB_EP_SPI     3
-#define USB_EP_CONSOLE 4
-#define USB_EP_I2C     5
-#define USB_EP_USART3_STREAM  6
-#define USB_EP_USART2_STREAM  7
-#define USB_EP_COUNT   8
-
-/* Enable control of GPIOs over USB */
-#define CONFIG_USB_GPIO
+#define USB_EP_CONTROL		0
+#define USB_EP_USART4_STREAM	1
+#define USB_EP_UPGRADE		2
+#define USB_EP_SPI		3
+#define USB_EP_CONSOLE		4
+#define USB_EP_I2C		5
+#define USB_EP_USART3_STREAM	6
+#define USB_EP_USART2_STREAM	7
+#define USB_EP_COUNT		8
 
 /* Enable console recasting of GPIO type. */
 #define CONFIG_CMD_GPIO_EXTENDED
@@ -87,9 +91,6 @@
 
 #include "gpio_signal.h"
 
-#define CONFIG_USB_SERIALNO
-#define DEFAULT_SERIALNO "Uninitialized"
-
 /* USB string indexes */
 enum usb_strings {
 	USB_STR_DESC = 0,
@@ -101,6 +102,7 @@ enum usb_strings {
 	USB_STR_CONSOLE_NAME,
 	USB_STR_USART3_STREAM_NAME,
 	USB_STR_USART2_STREAM_NAME,
+	USB_STR_UPGRADE_NAME,
 
 	USB_STR_COUNT
 };
