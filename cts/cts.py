dd438ea9bf805c1f2adca864bca3af39ba04a60a
Chris Chen <twothreecc@google.com>
2016-07-21 16:34:40 -0700

    cts: Debug message support added to cts tests
    You can now add debug messages into a cts test and
    they will be displayed with the test report html page.
    
    The macro to use is CTS_DEBUG_PRINTF
    
    Adding debug messages can potentially change test
    results by slowing down the test, so you can choose
    when compiling a test suite if you want the debug
    messages present or not by adding --debug as an
    argument when you call ./cts --build.
    
    BRANCH=None
    BUG=None
    TEST=Manual
    - Add a debug statement to a test
    - Build the test suite with --debug specified
    - Flash the boards
    - Run './cts/cts.py -r'
    - Open /tmp/cts_results/<board_name>/<test_suite>.html
      to view see your debug message for the test
    
    Change-Id: Icad8e0ac5cc905010caa4e7616f81572ce6ac771

--- cts/cts.py
+++ cts/cts.py
@@ -23,6 +23,8 @@ CTS_COLOR_GREEN = '#7dfb9f'
 TH_BOARD = 'stm32l476g-eval'
 OCD_SCRIPT_DIR = '/usr/local/share/openocd/scripts'
 MAX_SUITE_TIME_SEC = 3
+CTS_DEBUG_START = '[DEBUG]'
+CTS_DEBUG_END = '[DEBUG_END]'
 
 class Board(object):
   """Class representing a single board connected to a host machine
@@ -78,8 +80,16 @@ class Board(object):
     args += ['-c', 'shutdown']
     sp.call(args)
 
-  def make(self, module, ec_dir):
-    """Builds test suite module for board"""
+  def make(self, module, ec_dir, debug=False):
+    """Builds test suite module for board
+
+    Args:
+      module: String of the test module you are building,
+        i.e. gpio, timer, etc.
+      ec_dir: String of the ec directory path
+      debug: True means compile in debug messages when building (may
+        affect test results)
+    """
     cmds = ['make',
         '--directory=' + ec_dir,
         'BOARD=' + self.board,
@@ -87,8 +97,13 @@ class Board(object):
         '-j',
         '-B']
 
+    if debug:
+      cmds.append('CTS_DEBUG=TRUE')
+
     print 'EC directory is ' + ec_dir
-    print 'Building module \'' + module + '\' for ' + self.board
+    print (
+        'Building module \'' + module + '\' for ' + self.model +
+        'with debug = ' + str(debug))
     sp.call(cmds)
 
   def flash(self):
@@ -297,18 +312,27 @@ class Cts(object):
     test_results: Dictionary of results of each test from module
     return_codes: List of strings of return codes, with a code's integer
       value being the index for the corresponding string representation
+    debug: Boolean that indicates whether or not on-board debug message
+      printing should be enabled when building.
+    debug_output: Dictionary mapping test name to an array contain debug
+      messages sent while it was running
   """
 
-  def __init__(self, ec_dir, dut='nucleo-f072rb', module='gpio'):
+  def __init__(self, ec_dir,
+               dut='nucleo-f072rb', module='gpio', debug=False):
     """Initializes cts class object with given arguments.
 
     Args:
       dut: Name of Device Under Test (DUT) board
       ec_dir: String path to ec directory
+      dut: Name of board to use for DUT
       module: Name of module to build/run tests for
+      debug: Boolean that indicates whether or not on-board debug message
+        printing should be enabled when building.
     """
     self.results_dir = '/tmp/cts_results'
     self.module = module
+    self.debug = debug
     self.ec_directory = ec_dir
     self.th = TestHarness()
     self.dut = DeviceUnderTest(dut, self.th)  # DUT constructor needs TH
@@ -335,6 +359,9 @@ class Cts(object):
     self.return_codes = Cts._getMacroArgs(
       return_codes_path, 'CTS_RC_')
     self.test_results = collections.OrderedDict()
+    self.debug_output = {}
+    for test in self.test_names:
+      self.debug_output[test] = []
 
   def set_dut_board(self, brd):
     """Sets the dut_board instance variable
@@ -352,9 +379,17 @@ class Cts(object):
     """
     self.module = mod
 
+  def setDebug(self, debug):
+    """Sets debug variable
+
+    Args:
+      debug: Boolean indicating whether to build with debug
+    """
+    self.debug = debug
+
   def make(self):
-    self.dut.make(self.module, self.ec_directory)
-    self.th.make(self.module, self.ec_directory)
+    self.dut.make(self.module, self.ec_directory, self.debug)
+    self.th.make(self.module, self.ec_directory, self.debug)
 
   def flashBoards(self):
     """Flashes th and dut boards with their most recently build ec.bin"""
@@ -421,6 +456,38 @@ class Cts(object):
         args.append(ln.strip('()').replace(',', ''))
     return args
 
+  def extractDebugOutput(self, output):
+    """Append the debug messages from output to self.debug_output
+
+    Args:
+      output: String containing output from which to extract debug
+        messages
+    """
+    lines = [ln.strip() for ln in output.split('\n')]
+    test_num = 0
+    i = 0
+    message_buf = []
+    while i < len(lines):
+      if test_num >= len(self.test_names):
+        break
+      if lines[i].strip() == CTS_DEBUG_START:
+        i += 1
+        msg = ''
+        while i < len(lines):
+          if lines[i] == CTS_DEBUG_END:
+            break
+          else:
+            msg += lines[i]
+            break
+        message_buf.append(msg)
+      else:
+        current_test = self.test_names[test_num]
+        if lines[i].strip().startswith(current_test):
+          self.debug_output[current_test] += message_buf
+          message_buf = []
+          test_num += 1
+      i += 1
+
   def _parseOutput(self, r1, r2):
     """Parse the outputs of the DUT and TH together
 
@@ -431,6 +498,9 @@ class Cts(object):
     self.test_results.clear()  # empty out any old results
 
     first_corrupted_test = len(self.test_names)
+
+    self.extractDebugOutput(r1)
+    self.extractDebugOutput(r2)
 
     for output_str in [r1, r2]:
       test_num = 0
@@ -494,19 +564,36 @@ class Cts(object):
                   'body {font-family: \"Lucida Console\", Monaco, monospace')
     body = et.SubElement(root, 'body')
     table = et.SubElement(body, 'table')
-    table.set('align', 'center')
+    table.set('style','width:100%')
     title_row = et.SubElement(table, 'tr')
     test_name_title = et.SubElement(title_row, 'th')
     test_name_title.text = 'Test Name'
+    test_name_title.set('style', 'white-space : nowrap')
     test_results_title = et.SubElement(title_row, 'th')
     test_results_title.text = 'Test Result'
+    test_results_title.set('style', 'white-space : nowrap')
+    test_debug_title = et.SubElement(title_row, 'th')
+    test_debug_title.text = 'Debug Output'
+    test_debug_title.set('style', 'width:99%')
 
     for name, result in self.test_results.items():
       row = et.SubElement(table, 'tr')
       name_e = et.SubElement(row, 'td')
       name_e.text = name
+      name_e.set('style', 'white-space : nowrap')
       result_e = et.SubElement(row, 'td')
       result_e.text = result
+      result_e.set('style', 'white-space : nowrap')
+      debug_e = et.SubElement(row, 'td')
+      debug_e.set('style', 'width:99%')
+      if len(self.debug_output[name]) == 0:
+        debug_e.text = 'None'
+      else:
+        combined_message = ''
+        for msg in self.debug_output[name]:
+          combined_message = '\n' + msg
+        combined_message = combined_message[1:]
+        debug_e.text = combined_message
       if result == self.return_codes[CTS_SUCCESS_CODE]:
         result_e.set('bgcolor', CTS_COLOR_GREEN)
       else:
@@ -522,16 +609,21 @@ class Cts(object):
 
     self.dut.readAvailableBytes()  # clear buffer
     self.th.readAvailableBytes()
+    bad_cat_message = (
+        'Output missing from boards.\n'
+        'If you are running cat on a ttyACMx file,\n'
+        'please kill that process and try again'
+        )
+
     self.resetBoards()
 
     time.sleep(MAX_SUITE_TIME_SEC)
 
     dut_results = self.dut.readAvailableBytes()
     th_results = self.th.readAvailableBytes()
+
     if not dut_results or not th_results:
-      raise ValueError('Output missing from boards.\n'
-               'If you are running cat on a ttyACMx file,\n'
-               'please kill that process and try again')
+      raise ValueError(bad_cat_message)
 
     self._parseOutput(dut_results, th_results)
     pretty_results = self._resultsAsString()
@@ -566,6 +658,10 @@ def main():
   parser.add_argument('-m',
             '--module',
             help='Specify module you want to build/flash')
+  parser.add_argument('--debug',
+            action='store_true',
+            help='If building, build with debug printing enabled. This may'
+                 'change test results')
   parser.add_argument('-s',
             '--setup',
             action='store_true',
@@ -584,6 +680,8 @@ def main():
             help='Reset boards and save test results (no flashing)')
 
   args = parser.parse_args()
+
+  cts_suite.setDebug(args.debug)
 
   if args.module:
     module = args.module
