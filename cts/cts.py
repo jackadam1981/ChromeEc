344acaf72afbcc727a5c9cce0e92c9fac9965fe2
Chris Chen <twothreecc@google.com>
2016-07-22 11:20:45 -0700

    cts: Added corruption detection
    If test suite doesn't finish or results are received
    out of order, this is likely an indication of reset
    or hang by one of the boards and all tests after the
    point of corruption are marked as corrupted.
    
    BRANCH=None
    BUG=None
    TEST=Manual
    - Edit the gpio th code to change ordering of tests
      or hang or reset, etc.
    - Build and flash tests
    - Run './cts/cts.py -r'
    - You should see the results for all of the tests,
      with all corrupted tests marked as corrupted
    
    Change-Id: I7925e37db285a4e90e6e09bf3b187400ddfe9edf

--- cts/cts.py
+++ cts/cts.py
@@ -14,10 +14,10 @@ import subprocess as sp
 import argparse
 import collections
 
-# For most tests, error codes should never conflict
-CTS_CONFLICTING_CODE = -1
-CTS_SUCCESS_CODE = 0
 
+CTS_CORRUPTED_CODE = -2  # The test didn't execute correctly
+CTS_CONFLICTING_CODE = -1 # Well written tests shouldn't conflict
+CTS_SUCCESS_CODE = 0
 
 class Cts(object):
   """Class that represents a CTS testing setup and provides
@@ -327,35 +327,48 @@ class Cts(object):
     """
     self.test_results.clear()  # empty out any old results
 
+    first_corrupted_test = len(self.test_names)
+
     for output_str in [r1, r2]:
+      test_num = 0
       for ln in [ln.strip() for ln in output_str.split('\n')]:
         tokens = ln.split()
         if len(tokens) != 2:
           continue
-        elif tokens[0].strip() not in self.test_names:
+        t_name = tokens[0].strip()
+        t_result = int(tokens[1].strip())
+        if t_name not in self.test_names:
           continue
-        elif tokens[0] in self.test_results.keys():
-          if self.test_results[tokens[0]] != int(tokens[1]):
-            if self.test_results[tokens[0]] == CTS_SUCCESS_CODE:
-              self.test_results[tokens[0]] = int(tokens[1])
-            elif int(tokens[1]) == CTS_SUCCESS_CODE:
-              continue
+        if t_name != self.test_names[test_num]:
+          first_corrupted_test = test_num
+          break # Results after this are corrupted
+        if t_name in self.test_results.keys():
+          if self.test_results[t_name] != t_result:
+            if self.test_results[t_name] == CTS_SUCCESS_CODE:
+              self.test_results[t_name] = t_result
             else:
-              self.test_results[tokens[0]] = CTS_CONFLICTING_CODE
-          else:
-            continue
+              self.test_results[t_name] = CTS_CONFLICTING_CODE
         else:
-          self.test_results[tokens[0]] = int(tokens[1])
+          self.test_results[t_name] = t_result
+        test_num += 1
+      if test_num != len(self.test_names): # If a suite didn't finish
+        first_corrupted_test = min(first_corrupted_test, test_num)
+    if first_corrupted_test < len(self.test_names):
+      print 'Tests corrupted at ' + str(first_corrupted_test)
+      print 'Corrupted tests: '
+      print self.test_names[first_corrupted_test]
+      for test in self.test_names[first_corrupted_test:]:
+        print 'Listing ' + test + ' as corrupted'
+        self.test_results[test] = CTS_CORRUPTED_CODE
 
     # Convert codes to strings
     for test, code in self.test_results.items():
       if code == CTS_CONFLICTING_CODE:
-        self.test_results[test] = 'RESULTS CONFLICT'
-      self.test_results[test] = self.return_codes[code]
-
-    for tn in self.test_names:
-      if tn not in self.test_results.keys():
-        self.test_results[tn] = 'NO RESULT RETURNED' # Exceptional case
+        self.test_results[test] = 'CONFLICT'
+      elif code == CTS_CORRUPTED_CODE:
+        self.test_results[test] = 'CORRUPTED'
+      else:
+        self.test_results[test] = self.return_codes[code]
 
   def resultsAsString(self):
     """Takes saved results and returns a string representation of them
