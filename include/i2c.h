66fbccfb9692e9736f13153fb49944e345aee662
Nick Sanders <nsanders@chromium.org>
2016-07-26 13:17:09 -0700

    stm32f446e-eval: add support for stm32f446
    This adds basic support for the stm32f446.
    This consists of:
    * New DMA model for stm32f4
    * New "FMP" I2C driver
    * I2C DMA support
    * New clock domain support.
    * MCO oscillator gpio export support.
    * Flash support for irregular bolocks.
    
    BUG=chromium:608039
    TEST=boots
    BRANCH=None
    
    Change-Id: I1c5cf6ddca09009c9dac60da8a3d0c5ceedfcf4d
    Signed-off-by: Nick Sanders <nsanders@chromium.org>

--- include/i2c.h
+++ include/i2c.h
@@ -35,6 +35,7 @@ struct i2c_port_t {
 	 * If the function is not defined, the default value is true. */
 	int (*passthru_allowed)(const struct i2c_port_t *port,
 				uint16_t address);
+	int type;             /* Interface type for nonuniform blocks. */
 };
 
 extern const struct i2c_port_t i2c_ports[];
@@ -250,4 +251,6 @@ int i2c_read_string(int port, int slave_addr, int offset, uint8_t *data,
  */
 int i2c_port_to_controller(int port);
 
+/* Allow per channel completion notification. */
+#define I2C_COMPLETION(port)    TASK_EVENT_CUSTOM(1 << port)
 #endif  /* __CROS_EC_I2C_H */
