b6cf8d2e92c54a2e896d7dce9c97c5f7622ee4c4
james_chao <james_chao@asus.com>
2016-07-26 12:03:00 +0800

    Cave: Switch DPTF table when TABLET/NOTEBOOK mode changes
    1. EC send EC_HOST_EVENT_MODE_CHANGE(29/0x1D) when mode changes
    2. Host read current "physical mode" from EC ERAM
    3. Host Nodify DPTF object
    
    BUG=chrome-os-partner:53526
    BRANCH=firmware-glados-7820.B
    TEST=make buildall
    
    Change-Id: I3ede1ffd203024199884b3a0c56347ae630e6062
    Signed-off-by: james_chao <james_chao@asus.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -305,6 +305,9 @@
 /* Value to disable DPTF battery charging limit */
 #define EC_ACPI_MEM_CHARGING_LIMIT_DISABLED  0xff
 
+/* Report tablet mode  */
+#define EC_ACPI_TABLET_MODE            0x09
+
 /*
  * ACPI addresses 0x20 - 0xff map to EC_MEMMAP offset 0x00 - 0xdf.  This data
  * is read-only from the AP.  Added in EC_ACPI_MEM_VERSION 2.
@@ -433,6 +436,18 @@ enum host_event_code {
 
 	/* Keyboard fastboot combo has been pressed */
 	EC_HOST_EVENT_KEYBOARD_FASTBOOT = 25,
+
+	/* EC RTC event occurred */
+	EC_HOST_EVENT_RTC = 26,
+
+	/* Emulate MKBP event */
+	EC_HOST_EVENT_MKBP = 27,
+
+	/* EC desires to change state of host-controlled USB mux */
+	EC_HOST_EVENT_USB_MUX = 28,
+
+	/* TABLET/LAPTOP mode event*/
+	EC_HOST_EVENT_MODE_CHANGE = 29,
 
 	/*
 	 * The high bit of the event mask is not used as a host event code.  If
