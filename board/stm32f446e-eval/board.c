62b5af5755a565da5e3a799ed736715cd497479d
Nick Sanders <nsanders@chromium.org>
2016-07-26 13:17:09 -0700

    stm32f446e-eval: add support for stm32f446
    This adds basic support for the stm32f446.
    This consists of:
    * New DMA model for stm32f4
    * New "FMP" I2C driver
    * I2C DMA support
    * New clock domain support.
    * MCO oscillator gpio export support.
    * Flash support for irregular blocks.
    
    BUG=chromium:608039
    TEST=boots w/ correct clock, stm32f0 also boots.
    BRANCH=None
    
    Change-Id: I1c5cf6ddca09009c9dac60da8a3d0c5ceedfcf4d
    Signed-off-by: Nick Sanders <nsanders@chromium.org>

---
+++ board/stm32f446e-eval/board.c
@@ -0,0 +1,70 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "dma.h"
+#include "ec_version.h"
+#include "gpio.h"
+#include "gpio_list.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "registers.h"
+#include "stm32-dma.h"
+
+
+/* I2C ports */
+const struct i2c_port_t i2c_ports[] = {
+	{"i2c1", I2C_PORT_0, 100,
+		GPIO_I2C1_SCL, GPIO_I2C1_SDA},
+	{"fmpi2c4", FMPI2C_PORT_3, 100,
+		GPIO_FMPI2C_SCL, GPIO_FMPI2C_SDA, 0, 1},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+
+#define GPIO_SET_HS(bank, number)	\
+	(STM32_GPIO_OSPEEDR(GPIO_##bank) |= (0x3 << (number * 2)))
+
+void board_config_post_gpio_init(void)
+{
+	/* We use MCO2 clock passthrough to provide a clock to USB HS */
+	gpio_config_module(MODULE_MCO, 1);
+	/* GPIO PC9 to high speed */
+	GPIO_SET_HS(C, 9);
+
+	/* Set USB GPIO to high speed */
+	GPIO_SET_HS(A, 11);
+	GPIO_SET_HS(A, 12);
+
+	GPIO_SET_HS(C,  3);
+	GPIO_SET_HS(C,  2);
+	GPIO_SET_HS(C,  0);
+	GPIO_SET_HS(A,  5);
+
+	GPIO_SET_HS(B,  5);
+	GPIO_SET_HS(B, 13);
+	GPIO_SET_HS(B, 12);
+	GPIO_SET_HS(B,  2);
+	GPIO_SET_HS(B, 10);
+	GPIO_SET_HS(B,  1);
+	GPIO_SET_HS(B,  0);
+	GPIO_SET_HS(A,  3);
+
+	/* Set I2C GPIO to HS */
+	GPIO_SET_HS(B,  6);
+	GPIO_SET_HS(B,  7);
+	GPIO_SET_HS(F,  1);
+	GPIO_SET_HS(F,  0);
+	GPIO_SET_HS(A,  8);
+	GPIO_SET_HS(B,  4);
+	GPIO_SET_HS(C,  6);
+	GPIO_SET_HS(C,  7);
+}
+
+static void board_init(void)
+{
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
