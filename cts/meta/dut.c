5a021e1b5fa47ca3b316dfff0e65164b34b35912
Chris Chen <twothreecc@google.com>
2016-07-29 10:13:26 -0700

    cts: Added meta tests (testing cts suite itself)
    Change-Id: Ia5c10c7ce0e0d852fe6b14e481798eaff2a82c9c

---
+++ cts/meta/dut.c
@@ -0,0 +1,12 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "watchdog.h"
+#include "uart.h"
+#include "timer.h"
+#include "watchdog.h"
+#include "dut_common.h"
+#include "cts_common.h"
