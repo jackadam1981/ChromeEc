f292160ded83c9b83c506794fc18ef93bf3295ff
Nick Sanders <nsanders@chromium.org>
2016-08-02 19:35:44 -0700

    sweetberry: add dwc usb support
    stm32f446 uses a synopsys designware USB block
    rather than the typical ST one. This change adds driver support
    for the new block, including usb console support.
    
    BUG=chromium:608039
    TEST=usb console works
    BRANCH=None
    
    Change-Id: I0e143758ae0b5285f1c94ea2ec5aee159e22e00c
    Signed-off-by: Nick Sanders <nsanders@chromium.org>

--- include/config.h
+++ include/config.h
@@ -2156,6 +2156,12 @@
 #undef CONFIG_USB_PD_PULLUP_3A
 
 /******************************************************************************/
+/* stm32f4 dwc usb configs. */
+
+/* Set USB speed to FS rather than HS */
+#undef CONFIG_DWC_USB_FS
+
+/******************************************************************************/
 /* USB port switch */
 
 /* Support the Pericom PI3USB9281 I2C USB switch */
