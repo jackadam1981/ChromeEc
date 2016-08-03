8c4491ee90d0a1717b37a59f8a434d7e5d3ff258
Nick Sanders <nsanders@chromium.org>
2016-08-02 19:35:44 -0700

    stm32f446: add dwc usb support
    stm32f446 uses a synopsys designware USB block
    rather than the typical ST one. This change add driver support
    for the new block, including usb serial and usb console support.
    
    BUG=chromium:608039
    TEST=usb console works
    BRANCH=None
    
    Change-Id: I0e143758ae0b5285f1c94ea2ec5aee159e22e00c
    Signed-off-by: Nick Sanders <nsanders@chromium.org>

--- board/stm32f446e-eval/board.h
+++ board/stm32f446e-eval/board.h
@@ -32,6 +32,21 @@
 #define I2C_PORT_0 0
 #define FMPI2C_PORT_3 3
 
+/* USB Configuration */
+#define CONFIG_USB
+#define CONFIG_DWC_DMA
+#define CONFIG_USB_PID 0x500f
+#define CONFIG_USB_CONSOLE
+
+/* USB interface indexes (use define rather than enum to expand them) */
+#define USB_IFACE_CONSOLE 0
+#define USB_IFACE_COUNT   1
+
+/* USB endpoint indexes (use define rather than enum to expand them) */
+#define USB_EP_CONTROL 0
+#define USB_EP_CONSOLE 1
+#define USB_EP_COUNT   2
+
 /* This is not actually an EC so disable some features. */
 #undef CONFIG_WATCHDOG_HELP
 #undef CONFIG_LID_SWITCH
@@ -54,6 +69,17 @@
 #define TIM_CLOCK32 5
 
 #include "gpio_signal.h"
+
+/* USB string indexes */
+enum usb_strings {
+	USB_STR_DESC = 0,
+	USB_STR_VENDOR,
+	USB_STR_PRODUCT,
+	USB_STR_SERIALNO,
+	USB_STR_VERSION,
+	USB_STR_CONSOLE_NAME,
+	USB_STR_COUNT
+};
 
 #endif /* !__ASSEMBLER__ */
 #endif /* __CROS_EC_BOARD_H */
---
+++ chip/stm32/usb-stm32f4.c
@@ -0,0 +1,17 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * STM32F0 Family specific USB functionality
+ */
+
+#include "registers.h"
+#include "usb_api.h"
+
+void usb_connect(void)
+{
+}
+
+void usb_disconnect(void)
+{
+}
