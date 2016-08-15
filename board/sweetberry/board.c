baca07c9cb5037749b5959342038790b9e9b3e59
Nick Sanders <nsanders@chromium.org>
2016-08-15 13:43:12 -0700

    sweetberry: add build target for sweetberry
    sweetberry is an stm32f446 based power monitoring
    board, with 48 channels of INA current sense chips
    
    BUG=chromium:608039
    TEST=boots
    BRANCH=none
    
    Change-Id: If263bcee3a648ba3605f991999d481b7a0e2a1db
    Signed-off-by: Nick Sanders <nsanders@chromium.org>

---
+++ board/sweetberry/board.c
@@ -0,0 +1,103 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* Sweetberry board configuration */
+
+#include "common.h"
+#include "dma.h"
+#include "ec_version.h"
+#include "gpio.h"
+#include "gpio_list.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "registers.h"
+#include "stm32-dma.h"
+#include "task.h"
+#include "util.h"
+
+
+/******************************************************************************
+ * Define the strings used in our USB descriptors.
+ */
+#ifdef CONFIG_USB
+const void *const usb_strings[] = {
+	[USB_STR_DESC]	 = usb_string_desc,
+	[USB_STR_VENDOR]       = USB_STRING_DESC("Google Inc."),
+	[USB_STR_PRODUCT]      = USB_STRING_DESC("Sweetberry"),
+	[USB_STR_SERIALNO]     = USB_STRING_DESC("1234-a"),
+	[USB_STR_VERSION]      = USB_STRING_DESC(CROS_EC_VERSION32),
+	[USB_STR_CONSOLE_NAME] = USB_STRING_DESC("Sweetberry EC Shell"),
+};
+
+BUILD_ASSERT(ARRAY_SIZE(usb_strings) == USB_STR_COUNT);
+
+/* USB power interface. */
+USB_POWER_CONFIG(sweetberry_power, USB_IFACE_POWER, USB_EP_POWER);
+
+
+struct dwc_usb usb_ctl = {
+	.ep = {&ep0_ctl, &ep_console_ctl, &sweetberry_power_ep_ctl},
+	.speed = 0,
+	.phy_type = 0,
+	.dma_en = 1,
+};
+
+#endif
+
+/* I2C ports */
+const struct i2c_port_t i2c_ports[] = {
+	{"i2c1", I2C_PORT_0, 800,
+		GPIO_I2C1_SCL, GPIO_I2C1_SDA, 0, 0},
+	{"i2c2", I2C_PORT_1, 800,
+		GPIO_I2C2_SCL, GPIO_I2C2_SDA, 0, 0},
+	{"i2c3", I2C_PORT_2, 800,
+		GPIO_I2C3_SCL, GPIO_I2C3_SDA, 0, 0},
+	{"fmpi2c4", FMPI2C_PORT_3, 800,
+		GPIO_FMPI2C_SCL, GPIO_FMPI2C_SDA, 0, 1},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+
+#define GPIO_SET_HS(bank, number)	\
+	(STM32_GPIO_OSPEEDR(GPIO_##bank) |= (0x3 << ((number) * 2)))
+void board_config_post_gpio_init(void)
+{
+	/* We use MCO2 clock passthrough to provide a clock to USB HS */
+	gpio_config_module(MODULE_MCO, 1);
+	/* GPIO PC9 to high speed */
+	GPIO_SET_HS(C,  9);
+
+	/* Set USB GPIO to high speed */
+	GPIO_SET_HS(A, 11);
+	GPIO_SET_HS(A, 12);
+
+	GPIO_SET_HS(C,  3);
+	GPIO_SET_HS(C,  2);
+	GPIO_SET_HS(C,  0);
+	GPIO_SET_HS(A,  5);
+
+	GPIO_SET_HS(B,  5);
+	GPIO_SET_HS(B, 13);
+	GPIO_SET_HS(B, 12);
+	GPIO_SET_HS(B,  2);
+	GPIO_SET_HS(B, 10);
+	GPIO_SET_HS(B,  1);
+	GPIO_SET_HS(B,  0);
+	GPIO_SET_HS(A,  3);
+
+	/* Set I2C GPIO to HS */
+	GPIO_SET_HS(B,  6);
+	GPIO_SET_HS(B,  7);
+	GPIO_SET_HS(F,  1);
+	GPIO_SET_HS(F,  0);
+	GPIO_SET_HS(A,  8);
+	GPIO_SET_HS(B,  4);
+	GPIO_SET_HS(C,  6);
+	GPIO_SET_HS(C,  7);
+}
+
+static void board_init(void)
+{
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
---
+++ board/sweetberry/board.h
@@ -0,0 +1,78 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Sweetberry configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* Use external clock */
+#define CONFIG_STM32_CLOCK_HSE_HZ 24000000
+
+/* Optional features */
+#undef CONFIG_WATCHDOG_HELP
+#undef CONFIG_LID_SWITCH
+#define CONFIG_BOARD_POST_GPIO_INIT
+
+/* Enable console recasting of GPIO type. */
+#define CONFIG_CMD_GPIO_EXTENDED
+
+/* The UART console can be on flax USART3 (PC10/PC11) */
+/* The UART console can be on header USART4 (PA0/PA1) */
+#undef CONFIG_UART_CONSOLE
+#define CONFIG_UART_CONSOLE 4
+#undef CONFIG_UART_TX_BUF_SIZE
+#define CONFIG_UART_TX_BUF_SIZE 4096
+/* Don't waste precious DMA channels on console. */
+#undef CONFIG_UART_TX_DMA
+#undef CONFIG_UART_RX_DMA
+
+#define CONFIG_UART_TX_REQ_CH 4
+#define CONFIG_UART_RX_REQ_CH 4
+
+#define CONFIG_I2C
+#define CONFIG_I2C_MASTER
+#define I2C_PORT_0	0
+#define I2C_PORT_1	1
+#define I2C_PORT_2	2
+#define FMPI2C_PORT_3	3
+
+/* This is not actually an EC so disable some features. */
+#undef CONFIG_WATCHDOG_HELP
+#undef CONFIG_LID_SWITCH
+#undef CONFIG_WATCHDOG
+
+/* Optional features */
+#define CONFIG_STM_HWTIMER32
+
+/*
+ * Allow dangerous commands all the time, since we don't have a write protect
+ * switch.
+ */
+#define CONFIG_SYSTEM_UNLOCKED
+
+#ifndef __ASSEMBLER__
+
+/* Timer selection */
+#define TIM_CLOCK32 5
+
+#include "gpio_signal.h"
+
+#define CONFIG_USB_SERIALNO
+#define DEFAULT_SERIALNO "Uninitialized"
+
+/* USB string indexes */
+enum usb_strings {
+	USB_STR_DESC = 0,
+	USB_STR_VENDOR,
+	USB_STR_PRODUCT,
+	USB_STR_SERIALNO,
+	USB_STR_VERSION,
+	USB_STR_CONSOLE_NAME,
+	USB_STR_COUNT
+};
+
+#endif /* !__ASSEMBLER__ */
+#endif /* __CROS_EC_BOARD_H */
