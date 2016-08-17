c7dd595f2abffddc9f6c2ca748e4206de1e27264
Vadim Bendebury <vbendeb@chromium.org>
2016-08-17 11:39:06 -0700

    g: implement firmware upgrade protocol version 3
    This patch modifies CR50 code to switch to firmware update protocol
    version 3. In this version both SPI and USB updates use exactly the
    same messages (apart from the size field added by USB to help
    reassembly of the fragmented frames).
    
    Bot unused RO and RW sections can be now programmed.
    
    BRANCH=none
    BUG=chrome-os-partner:55789
    TEST=on a Kevin running the existing cr50 image programmed the new
         cr50 image, restarted the device, programmed the cr50 image again
         (this time using version 3), restarted the device, then
         programmed both RO and RW images and restarted the device. Each
         time verified that the proper image is running.
    
    Change-Id: I0cf196ec6de1786510433f8252164a33ccdc6dec
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- chip/g/upgrade_fw.h
+++ chip/g/upgrade_fw.h
@@ -8,25 +8,51 @@
 
 #include <stddef.h>
 
-#define UPGRADE_PROTOCOL_VERSION 2
 
-/* This is the format of the header the flash update function expects. */
+/*
+ * This file contains structures used to facilitate cr50 firmware updates,
+ * which can be used on any g chip.
+ *
+ * The firmware update protocol consists of two phases: connection
+ * establishment and actual image transfer.
+ *
+ * Image transfer is done in 1K blocks. The host supplying the image
+ * encapsulates blocks in frames by prepending a header including the flash
+ * offset where the block is destined and its digest.
+ *
+ * The CR50 device responds to each frame with a confirmation which is 1 byte
+ * response. Zero value means succes, non zero value is the error code
+ * reported by CR50.
+ *
+ * To establish the connection, the host sends a different frame, which
+ * contains no data and is destined to offset 0. Receiving such a frame
+ * signals the CR50 that the host intends to transfer a new image.
+ *
+ * Version 3 connection istablishment response is 16 bytes in size, all values
+ * in network byte order. The first 4 bytes are the error code (if any), the
+ * second 4 bytes are the protocol version (set to 3) and then 4 byte offset
+ * of the RO section followed by the 4 byte offset of the RW section.
+ */
+
+#define UPGRADE_PROTOCOL_VERSION 3
+
+/* This is the format of the update frame header. */
 struct upgrade_command {
 	uint32_t  block_digest;  /* first 4 bytes of sha1 of the rest of the
-				  * block.
+				  * frame.
 				  */
-	uint32_t  block_base;    /* Offset of this block into the flash SPI. */
+	uint32_t  block_base;    /* Offset of this frame into the flash SPI. */
 	/* The actual payload goes here. */
 } __packed;
 
 /*
  * This is the format of the header the host uses when sending update PDUs
- * over USB. The PDUs are 1K bytes in size, they are fragmented into USB units
- * of 64 bytes each and reassembled on the receive side before being passed to
- * the flash update function.
+ * over USB. The header + 1K payload PDUs are fragmented into USB units of 64
+ * bytes each and reassembled on the receive side before being passed to the
+ * flash update function.
  *
  * The flash update function receives the PDU body starting at the cmd field
- * below, and puts its reply into the beginning of the same buffer.
+ * below, and puts its reply into the the same buffer.
  */
 struct update_pdu_header {
 	uint32_t block_size;    /* Total size of the block, including this
@@ -36,7 +62,7 @@ struct update_pdu_header {
 };
 
 /*
- * Response to the message initiating the update sequence.
+ * Response to the connection establishment request.
  *
  * When responding to the very first packet of the upgrade sequence, the
  * original USB update implementation was responding with a four byte value,
@@ -72,5 +98,18 @@ struct first_response_pdu {
 void fw_upgrade_command_handler(void *body,
 				size_t cmd_size,
 				size_t *response_size);
+
+
+/* Various upgrade command return values. */
+enum return_value {
+	UPGRADE_SUCCESS = 0,
+	UPGRADE_BAD_ADDR = 1,
+	UPGRADE_ERASE_FAILURE = 2,
+	UPGRADE_DATA_ERROR = 3,
+	UPGRADE_WRITE_FAILURE = 4,
+	UPGRADE_VERIFY_ERROR = 5,
+	UPGRADE_GEN_ERROR = 6,
+	UPGRADE_MALLOC_ERROR = 7,
+};
 
 #endif  /* ! __EC_CHIP_G_UPGRADE_FW_H */
