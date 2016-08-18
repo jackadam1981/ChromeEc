6812bf5bccab361117a11a9be58883de1c45ffbc
Gwendal Grignou <gwendal@chromium.org>
2016-08-18 15:34:07 -0700

    motion_lid: prevent angle 0 <-> 360 transition.
    When lid is closed, the lid angle can move to 358, 360, 0, 359 ...
    Prevent transition 0 from/to 360 by keeping the last caculated value.
    
    BRANCH=kevin
    BUG=chrome-os-partner:55702
    TEST=Check transition does not happen anymore.
    
    Change-Id: Ifa8415470f425c893e2c3662c84c8fd0156e0524
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1743,6 +1743,14 @@
  */
 #undef CONFIG_SYSTEM_UNLOCKED
 
+/*
+ * Compile tablet mode support.
+ *
+ * Add support for tablet mode detection: Send event when the device is in
+ * tablet mode.
+ */
+#undef CONFIG_TABLET_MODE
+
 /*****************************************************************************/
 /* Task config */
 
