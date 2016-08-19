1b7483c35eab684bb57da07e22eee50a4dd05cdc
Mary Ruthven <mruthven@chromium.org>
2016-08-18 18:35:45 -0700

    cr50: notify chipset hooks when the AP state changes
    Cr50 monitors UART1 RX to sense the state of the AP. In this change
    if cr50 sees that UART1 RX is not pulled up by the AP then it sets the
    state as off and then notifies HOOK_CHIPSET_SHUTDOWN. When UART1 RX is
    high again the the AP is back on and HOOK_CHIPSET_RESUME is notified.
    
    Currently nothing is done with these hooks.
    
    BUG=chrome-os-partner:55747
    BRANCH=none
    TEST=buildall
    
    Change-Id: I47fb38a4bbcd72424ec2535d61e87f820cf1bcd7
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -134,7 +134,6 @@ void pmu_wakeup_interrupt(void)
 	/* Trigger timer1 interrupt */
 	if (wakeup_src & GC_PMU_EXITPD_SRC_TIMELS0_PD_EXIT_TIMER1_MASK)
 		task_trigger_irq(GC_IRQNUM_TIMELS0_TIMINT1);
-
 }
 DECLARE_IRQ(GC_IRQNUM_PMU_INTR_WAKEUP_INT, pmu_wakeup_interrupt, 1);
 
@@ -371,15 +370,19 @@ void nvmem_compute_sha(uint8_t *p_buf, int num_bytes,
 	memcpy(p_sha, sha1_digest, sha_len);
 }
 
-static void device_state_changed(enum device_type device,
+static int device_state_changed(enum device_type device,
 				 enum device_state state)
 {
+	int state_changed = state != device_states[device].last_known_state;
+
 	device_set_state(device, state);
 
 	/*
 	 * We've determined the device state, so cancel any deferred callbacks.
 	 */
 	hook_call_deferred(device_states[device].deferred, -1);
+
+	return state_changed;
 }
 
 /*
@@ -395,12 +398,14 @@ static int servo_state_unknown(void)
 	return 0;
 }
 
-static void device_powered_off(enum device_type device, int uart)
+static int device_powered_off(enum device_type device, int uart)
 {
-	if (device_get_state(device) == DEVICE_STATE_ON)
-		return;
+	int state_changed;
 
-	device_state_changed(device, DEVICE_STATE_OFF);
+	if (device_get_state(device) == DEVICE_STATE_ON)
+		return EC_ERROR_UNKNOWN;
+
+	state_changed = device_state_changed(device, DEVICE_STATE_OFF);
 
 	if (uart) {
 		/* Disable RX and TX on the UART peripheral */
@@ -409,6 +414,7 @@ static void device_powered_off(enum device_type device, int uart)
 		/* Disconnect the TX pin from the UART peripheral */
 		uartn_tx_disconnect(uart);
 	}
+	return state_changed ? EC_SUCCESS : EC_ERROR_UNKNOWN;
 }
 
 static void servo_deferred(void)
@@ -422,7 +428,8 @@ DECLARE_DEFERRED(servo_deferred);
 
 static void ap_deferred(void)
 {
-	device_powered_off(DEVICE_AP, UART_AP);
+	if (device_powered_off(DEVICE_AP, UART_AP) == EC_SUCCESS)
+		hook_notify(HOOK_CHIPSET_SHUTDOWN);
 }
 DECLARE_DEFERRED(ap_deferred);
 
@@ -451,10 +458,11 @@ struct device_config device_states[] = {
 };
 BUILD_ASSERT(ARRAY_SIZE(device_states) == DEVICE_COUNT);
 
-static void device_powered_on(enum device_type device, int uart)
+/* Returns EC_SUCCESS if the device state changed to on */
+static int device_powered_on(enum device_type device, int uart)
 {
 	/* Update the device state */
-	device_state_changed(device, DEVICE_STATE_ON);
+	int state_changed = device_state_changed(device, DEVICE_STATE_ON);
 
 	/* Enable RX and TX on the UART peripheral */
 	uartn_enable(uart);
@@ -463,6 +471,8 @@ static void device_powered_on(enum device_type device, int uart)
 	if (device_get_state(DEVICE_SERVO) != DEVICE_STATE_ON &&
 	    !uartn_enabled(uart))
 		uartn_tx_connect(uart);
+
+	return state_changed ? EC_SUCCESS : EC_ERROR_UNKNOWN;
 }
 
 static void servo_attached(void)
@@ -484,7 +494,8 @@ void device_state_on(enum gpio_signal signal)
 
 	switch (signal) {
 	case GPIO_DETECT_AP:
-		device_powered_on(DEVICE_AP, UART_AP);
+		if (device_powered_on(DEVICE_AP, UART_AP) == EC_SUCCESS)
+			hook_notify(HOOK_CHIPSET_RESUME);
 		break;
 	case GPIO_DETECT_EC:
 		device_powered_on(DEVICE_EC, UART_EC);
@@ -515,10 +526,13 @@ void board_update_device_state(enum device_type device)
 		gpio_enable_interrupt(device_states[device].detect);
 
 		/*
-		 * Wait a bit. If cr50 detects this device is ever powered on
-		 * during this time then the status wont be set to powered off.
+		 * The signal is low now, but the detect signals are on UART RX
+		 * which may be receiving something. Wait long enough for an
+		 * entire data chunk to be sent to declare that the device is
+		 * off. If the detect signal remains low for 100us then the
+		 * signal is low because the device is off.
 		 */
-		hook_call_deferred(device_states[device].deferred, 50);
+		hook_call_deferred(device_states[device].deferred, 100);
 	}
 }
 
