af7c98360bb63cce4a78dcb14cf8d138c588cc85
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-08-26 13:04:32 -0700

    ectool: Add ectool command to do force shutdown
    Added support to do force shutdown from kernel console using
    the ectools.
    
    BUG=chrome-os-partner:56681
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I509678a67a6a54da67de99efb00e1f08a1ed1fea
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/system.c
+++ common/system.c
@@ -4,6 +4,7 @@
  */
 
 /* System module for Chrome EC : common functions */
+#include "chipset.h"
 #include "clock.h"
 #include "common.h"
 #include "console.h"
@@ -804,6 +805,9 @@ static int handle_pending_reboot(enum ec_reboot_cmd cmd)
 		/* That shouldn't return... */
 		return EC_ERROR_UNKNOWN;
 #endif
+	case EC_REBOOT_FORCE_SHUTDOWN:
+		CPRINTS("Force Shutdown");
+		chipset_force_shutdown();
 	default:
 		return EC_ERROR_INVAL;
 	}
@@ -1272,7 +1276,8 @@ int host_command_reboot(struct host_cmd_handler_args *args)
 	if (p.cmd == EC_REBOOT_JUMP_RO ||
 	    p.cmd == EC_REBOOT_JUMP_RW ||
 	    p.cmd == EC_REBOOT_COLD ||
-	    p.cmd == EC_REBOOT_HIBERNATE) {
+	    p.cmd == EC_REBOOT_HIBERNATE ||
+	    p.cmd == EC_REBOOT_FORCE_SHUTDOWN) {
 		/* Clean busy bits on host for commands that won't return */
 		args->result = EC_RES_SUCCESS;
 		host_send_response(args);
