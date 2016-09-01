3924b7cfe87c697fc401836b59d38bef883c49f8
james_chao <james_chao@asus.com>
2016-09-01 18:36:05 +0800

    Cave: set V5ADS3VSEL=Vnom+3%
    BUG=chrome-os-partner:56642
    BRANCH=firmware-glados-7820.B
    TEST=check the usb port vbus power
    
    Change-Id: I773e555ad4207a1248313422df3f392f7de8419a
    Signed-off-by: james_chao <james_chao@asus.com>

--- board/cave/board.c
+++ board/cave/board.c
@@ -261,8 +261,12 @@ static void board_pmic_init(void)
 	/* VRMODECTRL - enable low-power mode for VCCIO and V0.85A */
 	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x3b, 0x18);
 
-	/* V5ADS3CNT - Set CTLV5ADS3 = Force PWM */
+	/* V5ADS3CNT
+	 * [5:4] Set V5ADS3VSEL = Vnom+3%
+	 * [1:0] BIT 1:0 Set CTLV5ADS3 = Force PWM
+	 */
 	if (!i2c_read8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x31, &pmic_reg31)) {
+		pmic_reg31 &= 0xCF;
 		pmic_reg31 |= 0x03;
 		i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x31, pmic_reg31);
 	}
