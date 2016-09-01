2e6eaa8496bc0e4a7c2f7fdf5f574ffcf7aa93f3
Gwendal Grignou <gwendal@chromium.org>
2016-09-01 16:44:34 -0700

    mkbp: Send event when in tablet mode
    When tablet mode is detected, send an event to the AP.
    
    BUG=chromium:606718
    BRANCH=none
    TEST=Check with evtest that events are sent when the tablet goes in tablet
    mode and back to device mode.
    
    Change-Id: I49f2404b5ecf87e71fa5aef4c8ce9c9beda26a15
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/380414
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1670,6 +1670,11 @@
  */
 #undef CONFIG_SYSTEM_UNLOCKED
 
+/*
+ * Add a virtual switch to indicate when we are in tablet mode.
+ */
+#define CONFIG_TABLET_MODE_SWITCH
+
 /*****************************************************************************/
 /* Task config */
 
