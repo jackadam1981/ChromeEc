82732f141aef2f0ceee20c3e89ffd3b1fe9c5ecc
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-09-02 16:21:06 -0700

    charge_ramp: Reduce the maximum ramping current if the charger lost power
    If the same charger is connected and loses power because of over current
    ramping or charger voltage drops below charger operating range, reduce
    the maximum ramping current to last good ramped current.
    
    BUG=chrome-os-partner:56367
    BRANCH=none
    TEST=Manually tested on reef, DCP charger which can cut-off the power if
         the current is ramped more than 1.3A can settle to 1.3A.
    
    Change-Id: I43b6254f74cdabd3a445ce73dfa2ad00523b56ac
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/charge_ramp.c
+++ common/charge_ramp.c
@@ -23,6 +23,9 @@
 /* Maximum allowable time charger can be unplugged to be considered an OCP */
 #define OC_RECOVER_MAX_TIME (SECOND)
 
+/* Maximum allowable time to re-detect the charger which lost power */
+#define CHG_RECOVER_MAX_TIME (CHARGE_DETECT_DELAY + 2*RAMP_CURR_DELAY)
+
 /* Delay for running state machine when board is not consuming full current */
 #define CURRENT_DRAW_DELAY  (5*SECOND)
 
@@ -149,6 +152,9 @@ void chg_ramp_task(void)
 	int i;
 	uint64_t detect_end_time_us = 0, time_us;
 	int last_active_port = CHARGE_PORT_NONE;
+#ifndef TEST_BUILD
+	int time_prev = 0;
+#endif
 
 	/*
 	 * Static initializer so that we don't clobber early calls to this
@@ -187,6 +193,21 @@ void chg_ramp_task(void)
 				task_wait_time = CHARGE_DETECT_DELAY;
 				break;
 			}
+
+#ifndef TEST_BUILD
+			/*
+			 * Adjust the Max Input current limit as OCP reset
+			 * occurred on external charger or VBUS is less than
+			 * internal charger operating range.
+			 */
+			if (ramp_st_prev == ramp_st &&
+			    active_port != CHARGE_PORT_NONE &&
+			    active_port == last_active_port &&
+			    ACTIVE_OC_INFO.icl > min_icl &&
+			    get_time().val < time_prev + CHG_RECOVER_MAX_TIME)
+				max_icl = MAX(min_icl, ACTIVE_OC_INFO.icl -
+							      RAMP_ICL_BACKOFF);
+#endif
 
 			/* If detect delay has not passed, set wait time */
 			time_us = get_time().val;
@@ -265,6 +286,11 @@ void chg_ramp_task(void)
 				stablize_sup = active_sup;
 				break;
 			}
+
+#ifndef TEST_BUILD
+			/* Update last ramp time */
+			time_prev = get_time().val;
+#endif
 
 			/* Ramp the current limit if we haven't reached max */
 			if (active_icl == max_icl)
