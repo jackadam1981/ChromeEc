1674e613deea6ad083023e4e4e4c66acf7e28837
Aseda Aboagye <aaboagye@google.com>
2016-09-08 15:03:24 -0700

    bd99955: Add support for power save mode.
    The BD99956 charger has a power save mode that it can enter once VBUS or
    VCC is removed.  This commit adds an optional config option that can be
    used to select the power save mode:
    
      CONFIG_BD99955_POWER_SAVE_MODE
    
    By default, no power save mode will be enabled.  However, a board can
    device what level of power savings they wish to use.
    
    The levels are the following:
    
    BD99955_PWR_SAVE_LOW /* BGATE ON w/ PROCHOT# monitored only system
                          * voltage. */
    
    BD99955_PWR_SAVE_MED /* BGATE ON w/ PROCHOT# monitored only system
                          * voltage every 1ms. */
    
    BD99955_PWR_SAVE_HIGH /* BGATE ON w/o PROCHOT# monitoring. */
    
    BD99955_PWR_SAVE_MAX  /* BGATE OFF */
    
    BUG=chrome-os-partner:55631
    BRANCH=kevin
    TEST=make -j buildall
    
    Change-Id: Ibab7ad30d5f1ae9917b46b40d6f2800ef19e52dd
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- driver/charger/bd99955.c
+++ driver/charger/bd99955.c
@@ -621,6 +621,7 @@ int charger_set_voltage(int voltage)
 static void bd99995_init(void)
 {
 	int reg;
+	int val = 0;
 	const struct battery_info *bi = battery_get_info();
 
 	/* Enable BC1.2 detection on VCC */
@@ -693,6 +694,12 @@ static void bd99995_init(void)
 	ch_raw_write16(BD99955_CMD_ITRICH_SET,
 		       bi->precharge_current & 0x07C0,
 		       BD99955_EXTENDED_COMMAND);
+
+	/* Power save mode when VBUS/VCC is removed. */
+#ifdef CONFIG_BD99955_POWER_SAVE_MODE
+	val = CONFIG_BD99955_POWER_SAVE_MODE;
+#endif
+	ch_raw_write16(BD99955_CMD_SMBREG, val, BD99955_EXTENDED_COMMAND);
 }
 DECLARE_HOOK(HOOK_INIT, bd99995_init, HOOK_PRIO_INIT_EXTPOWER);
 
