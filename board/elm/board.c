cd81e99af5c61c54bae4df873de8abc03578fd1b
Gwendal Grignou <gwendal@chromium.org>
2016-09-20 12:34:10 -0700

    elm: Add FIFO support for ARC++
    FIFO needed for ARC++ support.
    
    BUG=b:27849483,b:31609073
    BRANCH=oak
    TEST=On elm, install R55-8795.0.0, in ARC++, install AIDA64.
    Check the lid accelerometer is seen.
    Ran CTS tests: SensorBatchingTests and SingleSensorTests:
    ./out/host/linux-x86/cts/android-cts/tools/cts-tradefed run \
    commandAndExit cts -s <IP>:22 -c android.hardware.cts.<TEST>
    
    Change-Id: Iefaaee874bbc32f464c3afe79d03b3087b211ee0

--- board/elm/board.c
+++ board/elm/board.c
@@ -531,7 +531,8 @@ const matrix_3x3_t lid_standard_ref = {
 struct kionix_accel_data g_kx022_data[2];
 
 struct motion_sensor_t motion_sensors[] = {
-	{.name = "Base Accel",
+	[BASE_ACCEL] = {
+	 .name = "Base Accel",
 	 .active_mask = SENSOR_ACTIVE_S0,
 	 .chip = MOTIONSENSE_CHIP_KX022,
 	 .type = MOTIONSENSE_TYPE_ACCEL,
@@ -545,8 +546,8 @@ struct motion_sensor_t motion_sensors[] = {
 	 .config = {
 		/* AP: by default use EC settings */
 		[SENSOR_CONFIG_AP] = {
-			.odr = 10000 | ROUND_UP_FLAG,
-			.ec_rate = 100 * MSEC,
+			.odr = 0,
+			.ec_rate = 0,
 		},
 		/* EC use accel for angle detection */
 		[SENSOR_CONFIG_EC_S0] = {
@@ -565,7 +566,8 @@ struct motion_sensor_t motion_sensors[] = {
 	 },
 	},
 
-	{.name = "Lid Accel",
+	[LID_ACCEL] = {
+	 .name = "Lid Accel",
 	 .active_mask = SENSOR_ACTIVE_S0,
 	 .chip = MOTIONSENSE_CHIP_KX022,
 	 .type = MOTIONSENSE_TYPE_ACCEL,
@@ -579,8 +581,8 @@ struct motion_sensor_t motion_sensors[] = {
 	 .config = {
 		/* AP: by default use EC settings */
 		[SENSOR_CONFIG_AP] = {
-			.odr = 10000 | ROUND_UP_FLAG,
-			.ec_rate = 100 * MSEC,
+			.odr = 0,
+			.ec_rate = 0,
 		},
 		/* EC use accel for angle detection */
 		[SENSOR_CONFIG_EC_S0] = {
