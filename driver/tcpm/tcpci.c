8ca18c096de662f2015f40bd0d62fff1d21738b9
Kevin K Wong <kevin.k.wong@intel.com>
2016-09-21 08:57:40 -0700

    reef: enable tcpc-controlled drp toggle
    BUG=chrome-os-partner:54668
    BRANCH=none
    TEST=Verified SNK is detected in S0 (toggle on), S3 (toggle off),
    and S5 (force sink). SRC is detect in S0 only, stays detected when
    entered S3, but unplug/plug while in S3 will not re-detect until
    system back in S0. When go to S5, SRC will get disconnected until
    back in S0, and hotplug SRC in S5 will not get detected. Checked
    power role swap with another chromebook in the above scenario also.
    
    Change-Id: I2a487fca5cb04c45524aa3efde84fcd10ff0579e
    Signed-off-by: Kevin K Wong <kevin.k.wong@intel.com>

--- driver/tcpm/tcpci.c
+++ driver/tcpm/tcpci.c
@@ -17,6 +17,9 @@
 
 static int tcpc_vbus[CONFIG_USB_PD_PORT_COUNT];
 
+/* Save the selected rp value */
+static int selected_rp[CONFIG_USB_PD_PORT_COUNT];
+
 static int init_alert_mask(int port)
 {
 	uint16_t mask;
@@ -91,15 +94,8 @@ static int tcpci_tcpm_get_power_status(int port, int *status)
 
 int tcpci_tcpm_select_rp_value(int port, int rp)
 {
-	int reg;
-	int rv;
-
-	rv = tcpc_read(port, TCPC_REG_ROLE_CTRL, &reg);
-	if (rv)
-		return rv;
-	reg = (reg & ~TCPC_REG_ROLE_CTRL_RP_MASK)
-	    | ((rp << 4) & TCPC_REG_ROLE_CTRL_RP_MASK);
-	return tcpc_write(port, TCPC_REG_ROLE_CTRL, reg);
+	selected_rp[port] = rp;
+	return EC_SUCCESS;
 }
 
 #ifdef CONFIG_USB_PD_DISCHARGE_TCPC
@@ -119,22 +115,33 @@ static void tcpci_tcpc_discharge_vbus(int port, int enable)
 }
 #endif
 
+static int set_role_ctrl(int port, int toggle, int rp, int pull)
+{
+	return tcpc_write(port, TCPC_REG_ROLE_CTRL,
+			  TCPC_REG_ROLE_CTRL_SET(toggle, rp, pull, pull));
+}
+
 int tcpci_tcpm_set_cc(int port, int pull)
 {
-	int reg, rv;
-	uint8_t rp;
-
-	rv = tcpc_read(port, TCPC_REG_ROLE_CTRL, &reg);
-	if (rv)
-		return rv;
-	rp = TCPC_REG_ROLE_CTRL_RP(reg);
-	/*
-	 * Set manual control of Rp/Rd, and set both CC lines to the same
-	 * pull.
-	 */
-	return tcpc_write(port, TCPC_REG_ROLE_CTRL,
-			  TCPC_REG_ROLE_CTRL_SET(0, rp, pull, pull));
+	/* Set manual control, and set both CC lines to the same pull */
+	return set_role_ctrl(port, 0, selected_rp[port], pull);
 }
+
+#ifdef CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE
+static int tcpci_tcpc_drp_toggle(int port)
+{
+	int rv;
+
+	/* Set auto drp toggle */
+	rv = set_role_ctrl(port, 1, TYPEC_RP_USB, TYPEC_CC_OPEN);
+
+	/* Set Look4Connection command */
+	rv |= tcpc_write(port, TCPC_REG_COMMAND,
+			 TCPC_REG_COMMAND_LOOK4CONNECTION);
+
+	return rv;
+}
+#endif
 
 int tcpci_tcpm_set_polarity(int port, int polarity)
 {
@@ -407,5 +414,8 @@ const struct tcpm_drv tcpci_tcpm_drv = {
 	.tcpc_alert		= &tcpci_tcpc_alert,
 #ifdef CONFIG_USB_PD_DISCHARGE_TCPC
 	.tcpc_discharge_vbus	= &tcpci_tcpc_discharge_vbus,
+#endif
+#ifdef CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE
+	.drp_toggle	= &tcpci_tcpc_drp_toggle,
 #endif
 };
