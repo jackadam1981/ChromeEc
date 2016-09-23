5d997d942a5f7acd8586060c44a9d6960d01aaa7
Scott <scollyer@chromium.org>
2016-09-23 07:59:04 -0700

    Cr50: I2CS: Set INT_AP_L as in input  when the AP is off
    INT_AP_L is used as a sync trigger to the host during i2cs
    transactions. This signal is nominally set high and pulled up to
    1.8V. However, when the AP is off, this line won't be pulled up which
    would lead to excessive power draw by the Cr50 if this signal remains
    driven high.
    
    Added a change to set this signal as an input while the AP is powered
    off. When the AP powers back up, it's changed back to an input and
    driven high which is the default level.
    
    BRANCH=none
    BUG=chrome-os-partner:57733
    TEST=manual
    
    Change-Id: I12f175a7899eff6b90acb5a82282e526db3c1b9f
    Signed-off-by: Scott <scollyer@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -458,8 +458,21 @@ DECLARE_DEFERRED(servo_deferred);
 
 static void ap_deferred(void)
 {
-	if (device_powered_off(DEVICE_AP, UART_AP) == EC_SUCCESS)
+	if (device_powered_off(DEVICE_AP, UART_AP) == EC_SUCCESS) {
 		hook_notify(HOOK_CHIPSET_SHUTDOWN);
+		/*
+		 * If I2C TPM is configured then the INT_AP_L signal is used as
+		 * a low pulse trigger to sync I2C transactions with the
+		 * host. By default Cr50 is driving this line high, but when the
+		 * AP powers off, the 1.8V rail that it's pulled up to will be
+		 * off and cause exessive power to be consumed by the Cr50. Set
+		 * INT_AP_L as an input while the AP is powered off.
+		 */
+		if (system_get_board_properties() & BOARD_SLAVE_CONFIG_I2C) {
+			gpio_set_flags(GPIO_INT_AP_L, GPIO_INPUT);
+			GWRITE(PINMUX, DIOA5_SEL, 0);
+		}
+	}
 }
 DECLARE_DEFERRED(ap_deferred);
 
@@ -525,8 +538,21 @@ void device_state_on(enum gpio_signal signal)
 
 	switch (signal) {
 	case GPIO_DETECT_AP:
-		if (device_powered_on(DEVICE_AP, UART_AP) == EC_SUCCESS)
+		if (device_powered_on(DEVICE_AP, UART_AP) == EC_SUCCESS) {
 			hook_notify(HOOK_CHIPSET_RESUME);
+			if (system_get_board_properties() &
+			    BOARD_SLAVE_CONFIG_I2C) {
+				/*
+				 * AP is powering up, set the I2C host sync
+				 * signal to output and set it high which is the
+				 * default level.
+				 */
+				GWRITE(PINMUX, DIOA5_SEL,
+				       GC_PINMUX_GPIO0_GPIO0_SEL);
+				gpio_set_flags(GPIO_INT_AP_L, GPIO_OUTPUT);
+				gpio_set_level(GPIO_INT_AP_L, 1);
+			}
+		}
 		break;
 	case GPIO_DETECT_EC:
 		device_powered_on(DEVICE_EC, UART_EC);
