c988b4628fe30895fb833bed96d07a670d17227a
Aseda Aboagye <aaboagye@google.com>
2016-09-23 10:48:35 -0700

    system: Add print_system_rtc().
    This commit adds a function that allows the real-time clock to be
    printed on the EC console.  This could be helpful in trying to correlate
    events between the EC's log and the kernel's.
    
    BUG=chrome-os-partner:57731
    BRANCH=gru
    TEST=make -j buildall
    
    Change-Id: I5e20692a173bddea3dc5c20cc0f2061cc170ce7d
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- include/system.h
+++ include/system.h
@@ -309,6 +309,11 @@ void board_hibernate_late(void) __attribute__((weak));
 timestamp_t system_get_rtc(void);
 
 /**
+ * Print out the current real-time clock value to the console.
+ */
+void print_system_rtc(void);
+
+/**
  * Enable hibernate interrupt
  */
 void system_enable_hib_interrupt(void);
