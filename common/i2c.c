30d43bbc8dc54301030464c7ceb08d7eff9f97aa
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-09-28 09:36:52 -0700

    I2C: Improve I2C stess tests
    Improved the I2C stress test code to select random, sequence,
    and read only tests.
    
    BUG=chrome-os-partner:57959
    BRANCH=none
    TEST=Manually tested on Reef. Stress tests pass.
    
    Change-Id: I3d235e1b03d3b85cd3b1d003dcf8d2ca4502540f
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/i2c.c
+++ common/i2c.c
@@ -858,163 +858,238 @@ DECLARE_CONSOLE_COMMAND(i2cxfer, command_i2cxfer,
 #endif
 
 #ifdef CONFIG_CMD_I2C_STRESS_TEST
-static void i2c_test_status(struct i2c_test_results *i2c_test, int test_dev)
+static void i2c_test_status(int test_dev_num)
 {
-	ccprintf("test_dev=%2d, ", test_dev);
+	struct i2c_stress_test_dev *dev =
+		i2c_stress_tests[test_dev_num].i2c_test;
+	struct i2c_test_results *i2c_test_dev = &dev->test_results;
+
+	ccprintf("test_dev=%2d, ", test_dev_num);
 	ccprintf("r=%5d, rs=%5d, rf=%5d, ",
-		i2c_test->read_success + i2c_test->read_fail,
-		i2c_test->read_success,
-		i2c_test->read_fail);
-
+		i2c_test_dev->read_success + i2c_test_dev->read_fail,
+		i2c_test_dev->read_success,
+		i2c_test_dev->read_fail);
 	ccprintf("w=%5d, ws=%5d, wf=%5d\n",
-		i2c_test->write_success + i2c_test->write_fail,
-		i2c_test->write_success,
-		i2c_test->write_fail);
+		i2c_test_dev->write_success + i2c_test_dev->write_fail,
+		i2c_test_dev->write_success,
+		i2c_test_dev->write_fail);
 
-	i2c_test->read_success = 0;
-	i2c_test->read_fail = 0;
-	i2c_test->write_success = 0,
-	i2c_test->write_fail = 0;
+	i2c_test_dev->read_success = 0;
+	i2c_test_dev->read_fail = 0;
+	i2c_test_dev->write_success = 0,
+	i2c_test_dev->write_fail = 0;
+}
+
+static int i2c_test_read(int test_dev_num, int *data)
+{
+	struct i2c_stress_test_dev *dev =
+		i2c_stress_tests[test_dev_num].i2c_test;
+	struct i2c_test_reg_info *reg_s_info = &dev->reg_info;
+
+	return dev->i2c_read ?
+		dev->i2c_read(i2c_stress_tests[test_dev_num].port,
+			i2c_stress_tests[test_dev_num].addr,
+			reg_s_info->read_reg,
+			data) :
+		dev->i2c_read_dev(reg_s_info->read_reg,
+			data);
+}
+
+static int i2c_test_write(int test_dev_num, int data)
+{
+	struct i2c_stress_test_dev *dev =
+		i2c_stress_tests[test_dev_num].i2c_test;
+	struct i2c_test_reg_info *reg_s_info = &dev->reg_info;
+
+	return dev->i2c_write ?
+		dev->i2c_write(i2c_stress_tests[test_dev_num].port,
+			i2c_stress_tests[test_dev_num].addr,
+			reg_s_info->write_reg,
+			data) :
+		dev->i2c_write_dev(reg_s_info->write_reg,
+			data);
 }
 
 #define I2C_STRESS_TEST_DATA_VERIFY_RETRY_COUNT 3
-static int command_i2ctest(int argc, char **argv)
+static int i2c_stress_test(int dev, int read, int rand_seq)
 {
-	char *e;
-	int i, j, rv, rand;
+	static int k;
+	int i = 1;
+	int j, rv, rand, test_dev;
 	int data, data_verify;
-	int port, addr;
-	int count = 10000;
-	int udelay = 100;
-	int test_dev = i2c_test_dev_used;
 	struct i2c_stress_test_dev *i2c_s_test;
 	struct i2c_test_reg_info *reg_s_info;
 	struct i2c_test_results *test_s_results;
 
-	if (argc > 1) {
-		count = strtoi(argv[1], &e, 0);
-		if (*e)
-			return EC_ERROR_PARAM2;
+	k++;
+	if (dev == i2c_test_dev_used) {
+		rand = rand_seq ? k : get_time().val;
+		test_dev = rand % i2c_test_dev_used;
+	} else
+		test_dev = dev;
+
+	i2c_s_test = i2c_stress_tests[test_dev].i2c_test;
+	reg_s_info = &i2c_s_test->reg_info;
+	test_s_results = &i2c_s_test->test_results;
+
+	if (!read)
+		read = get_time().val;
+
+	if (read & 0x1) {
+		/* read */
+		rv = i2c_test_read(test_dev, &data);
+		if (rv || data != reg_s_info->read_val)
+			test_s_results->read_fail++;
+		else
+			test_s_results->read_success++;
+	} else {
+		/*
+		 * Reads are more than writes in the system.
+		 * Read and then write same value to ensure we are
+		 * not changing any settings.
+		 */
+
+		/* Read the write register */
+		rv = i2c_test_read(test_dev, &data);
+		if (rv) {
+			/* Skip writing invalid data */
+			test_s_results->read_fail++;
+			return i;
+		}
+		test_s_results->read_success++;
+
+		j = i2c_test_dev_used;
+		do {
+			/* Write same value back */
+			rv = i2c_test_write(test_dev, data);
+			i++;
+			if (rv) {
+				/* Skip reading as write failed */
+				test_s_results->write_fail++;
+				break;
+			}
+			test_s_results->write_success++;
+
+			rv = i2c_test_read(test_dev, &data_verify);
+			i++;
+			if (rv) {
+				/* Read failed try next time */
+				test_s_results->read_fail++;
+				break;
+			} else if (!rv && data != data_verify) {
+				/* Either data writes/read is wrong */
+				j--;
+			} else {
+				j = 0;
+				test_s_results->read_success++;
+			}
+		} while (j);
 	}
 
-	if (argc > 2) {
-		udelay = strtoi(argv[2], &e, 0);
-		if (*e)
-			return EC_ERROR_PARAM3;
+	return i;
+}
+
+static int command_i2ctest(int argc, char **argv)
+{
+	int i, j, val;
+	int dev = i2c_test_dev_used;
+	int count = 10000, udelay = 100;
+	int rand_seq = 0, read = 0;
+	char *e;
+	static const char * const args[] = {
+		/* Arguments need a value */
+		[0] = "dev",	/* device number to be tested */
+		[1] = "count",	/* number of tests to be executed */
+		[2] = "udelay",	/* delay between read/write in micro seconds */
+		/* Arguments don't need a value */
+		[3] = "read",	/* only do reads */
+		[4] = "seq",	/* test devices sequentially */
+	};
+
+	for (i = 1; i < argc; i++) {
+		for (j = 0; j < ARRAY_SIZE(args); j++)
+			if (!strcasecmp(args[j], argv[i]))
+				break;
+
+		/* Invalid argument */
+		if (j == ARRAY_SIZE(args))
+			return EC_ERROR_PARAM1 + i;
+
+		/* Arguments need a value */
+		if (j < 3) {
+			i++;
+			/* Make sure we are not indexing out of arguments */
+			if (i == argc)
+				return EC_ERROR_PARAM1 + --i;
+
+			val = strtoi(argv[i], &e, 0);
+			if (*e || val < 0)
+				return EC_ERROR_PARAM1 + i;
+		}
+
+		switch (j) {
+		case 0:
+			if (val >= i2c_test_dev_used)
+				return EC_ERROR_PARAM1 + i;
+
+			dev = val;
+			break;
+		case 1:
+			if (!val)
+				return EC_ERROR_PARAM1 + i;
+
+			count = val;
+			break;
+		case 2:
+			if (!val)
+				return EC_ERROR_PARAM1 + i;
+
+			udelay = val;
+			break;
+		case 3:
+			read = 1;
+			break;
+		case 4:
+			rand_seq = 1;
+			break;
+		default:
+			break;
+		}
 	}
 
-	if (argc > 3) {
-		test_dev = strtoi(argv[3], &e, 0);
-		if (*e || test_dev < 1 || test_dev > i2c_test_dev_used)
-			return EC_ERROR_PARAM4;
-		test_dev--;
-	}
-
-	for (i = 0; i < count; i++) {
+	for (i = 0; i < count; ) {
+		i += i2c_stress_test(dev, read, rand_seq);
+		usleep(udelay);
 		if (!(i % 1000))
 			ccprintf("running test %d\n", i);
-
-		if (argc < 4) {
-			rand = get_time().val;
-			test_dev = rand % i2c_test_dev_used;
-		}
-
-		port = i2c_stress_tests[test_dev].port;
-		addr = i2c_stress_tests[test_dev].addr;
-		i2c_s_test = i2c_stress_tests[test_dev].i2c_test;
-		reg_s_info = &i2c_s_test->reg_info;
-		test_s_results = &i2c_s_test->test_results;
-
-		rand = get_time().val;
-		if (rand & 0x1) {
-			/* read */
-			rv = i2c_s_test->i2c_read ?
-				i2c_s_test->i2c_read(port, addr,
-					reg_s_info->read_reg, &data) :
-				i2c_s_test->i2c_read_dev(
-					reg_s_info->read_reg, &data);
-			if (rv || data != reg_s_info->read_val)
-				test_s_results->read_fail++;
-			else
-				test_s_results->read_success++;
-		} else {
-			/*
-			 * Reads are more than writes in the system.
-			 * Read and then write same value to ensure we are
-			 * not changing any settings.
-			 */
-
-			/* Read the write register */
-			rv = i2c_s_test->i2c_read ?
-				i2c_s_test->i2c_read(port, addr,
-					reg_s_info->read_reg, &data) :
-				i2c_s_test->i2c_read_dev(
-					reg_s_info->read_reg, &data);
-			if (rv) {
-				/* Skip writing invalid data */
-				test_s_results->read_fail++;
-				continue;
-			} else
-				test_s_results->read_success++;
-
-			j = I2C_STRESS_TEST_DATA_VERIFY_RETRY_COUNT;
-			do {
-				/* Write same value back */
-				rv = i2c_s_test->i2c_write ?
-					i2c_s_test->i2c_write(port, addr,
-					reg_s_info->write_reg, data) :
-					i2c_s_test->i2c_write_dev(
-					reg_s_info->write_reg, data);
-				i++;
-				if (rv) {
-					/* Skip reading as write failed */
-					test_s_results->write_fail++;
-					break;
-				}
-				test_s_results->write_success++;
-
-				/* Read back to verify the data */
-				rv = i2c_s_test->i2c_read ?
-					i2c_s_test->i2c_read(port, addr,
-					reg_s_info->read_reg, &data_verify) :
-					i2c_s_test->i2c_read_dev(
-					reg_s_info->read_reg, &data_verify);
-				i++;
-				if (rv) {
-					/* Read failed try next time */
-					test_s_results->read_fail++;
-					break;
-				} else if (!rv && data != data_verify) {
-					/* Either data writes/read is wrong */
-					j--;
-				} else {
-					j = 0;
-					test_s_results->read_success++;
-				}
-			} while (j);
-		}
-
-		usleep(udelay);
 	}
 
-	ccprintf("\n**********final result **********\n");
-
-	cflush();
-	if (argc > 3) {
-		i2c_test_status(&i2c_s_test->test_results, test_dev + 1);
+	/* Get the results */
+	if (dev == i2c_test_dev_used) {
+		i = 0;
+		j = i2c_test_dev_used;
 	} else {
-		for (i = 0; i < i2c_test_dev_used; i++) {
-			i2c_s_test = i2c_stress_tests[i].i2c_test;
-			i2c_test_status(&i2c_s_test->test_results, i + 1);
-			msleep(100);
-		}
+		i = dev;
+		j = dev + 1;
+	}
+
+	for (; i < j; i++) {
+		i2c_test_status(i);
+		msleep(100);
 	}
 	cflush();
 
 	return EC_SUCCESS;
 }
+/*
+ * dev - device number to be tested
+ * count - number of tests to be executed
+ * udelay - delay between read / write in micro seconds
+ * read - only do reads
+ * seq - test devices sequentially
+ */
 DECLARE_CONSOLE_COMMAND(i2ctest, command_i2ctest,
-			"i2ctest count|udelay|dev",
+			"dev <value>|count <value>|udelay <value>|read|seq",
 			"I2C stress test");
 #endif /* CONFIG_CMD_I2C_STRESS_TEST */
 
