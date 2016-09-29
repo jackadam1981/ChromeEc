10b80e5723207e736ea1e955c1f9f97e8cf04b23
Jenny TC <jenny.tc@intel.com>
2016-09-29 14:22:09 +0530

    s0ix: Exit s0ix on chipset reset
    If a chipset reset happens while the system is in S0iX,
    system would continue to remain in S0iX eventhough ap is
    in S0. This would cause power state transiotion and wakeup
    issues. To address this, system needs to exit from s0ix as
    soon as EC detects a chipset reset
    
    BUG=chrome-os-partner:57920
    TEST=enter s0ix, apreset from ec console. System should exit s0ix
    BRANCH=firmware-glados-7820.B
    
    Change-Id: I0f24f717f712dcd46e3ddca2a8c86888739f3deb
    Signed-off-by: Jenny TC <jenny.tc@intel.com>

--- power/skylake.c
+++ power/skylake.c
@@ -206,6 +206,18 @@ static enum power_state power_wait_s5_rtc_reset(void)
 }
 #endif
 
+#ifdef CONFIG_POWER_S0IX
+static void handle_chipset_reset(void)
+{
+	if ((get_slp_s0_track() == 0)) {
+		CPRINTS("chipset reset: exit s0ix");
+		set_slp_s0_track(1);
+		task_wake(TASK_ID_CHIPSET);
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESET, handle_chipset_reset, HOOK_PRIO_FIRST);
+#endif
+
 static enum power_state _power_handle_state(enum power_state state)
 {
 	int tries = 0;
