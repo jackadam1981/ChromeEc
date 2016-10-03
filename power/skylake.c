70b147cce00b3a8e193e63570948f9f2ce3b2645
Duncan Laurie <dlaurie@chromium.org>
2016-10-03 15:46:59 -0700

    bison: WIP
    Work in progress, do not submit.
    
    Change-Id: I46755f0fde7e06f3c04a5787c266cdaeb0575323
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>

--- power/skylake.c
+++ power/skylake.c
@@ -130,9 +130,15 @@ void chipset_reset(int cold_reset)
 		 */
 
 		/* Pulse must be at least 16 PCI clocks long = 500 ns */
+#ifdef CONFIG_VW_SIGNALS
+		espi_vw_set_wire(VW_RCIN_L, 0);
+		udelay(10);
+		espi_vw_set_wire(VW_RCIN_L, 1);
+#else
 		gpio_set_level(GPIO_PCH_RCIN_L, 0);
 		udelay(10);
 		gpio_set_level(GPIO_PCH_RCIN_L, 1);
+#endif
 	}
 }
 
