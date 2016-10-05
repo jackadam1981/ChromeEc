2e57304215946a696fa89f409d1ae36379d22825
Milton Chiang <milton.chiang@mediatek.com>
2016-10-05 19:37:58 +0800

    hana: initial hana mainboard
    Hana is an Elm variant platform. Clone board-related settings from
    elm folder and updated some minor changes of battery info.
    
    BUG=chrome-os-partner:57249
    BRANCH=oak
    TEST=manual
         make BOARD=hana -j
         load and test on hana-rev0
    
    Change-Id: I1b50cd94ada0f0c695cd582c51a7b1377e0ef801
    Signed-off-by: Milton Chiang <milton.chiang@mediatek.com>

--- board/elm/battery.c
+++ board/elm/battery.c
@@ -9,9 +9,11 @@
 #include "battery_smart.h"
 #include "util.h"
 
+#ifdef BOARD_ELM
 /* Shutdown mode parameter to write to manufacturer access register */
 #define SB_SHIP_MODE_REG	0x3a
-#define SB_SHUTDOWN_DATA	0xC574
+#define SB_SHUTDOWN_DATA1	0xC574
+#define SB_SHUTDOWN_DATA2	0xC574
 
 static const struct battery_info info = {
 	.voltage_max = 13200,
@@ -27,6 +29,28 @@ static const struct battery_info info = {
 	.discharging_min_c = 0,
 	.discharging_max_c = 60,
 };
+#elif defined(BOARD_HANA)
+/* Shutdown mode parameter to write to manufacturer access register */
+#define SB_SHIP_MODE_REG        0x34
+#define SB_SHUTDOWN_DATA1       0x0000
+#define SB_SHUTDOWN_DATA2       0x1000
+
+
+static const struct battery_info info = {
+        .voltage_max = 12600,
+	.voltage_normal = 11100,
+	.voltage_min = 9100,
+	/* Pre-charge values. */
+	.precharge_current = 256, /* mA */
+
+	.start_charging_min_c = 0,
+	.start_charging_max_c = 50,
+	.charging_min_c = 0,
+	.charging_max_c = 60,
+	.discharging_min_c = 0,
+	.discharging_max_c = 60,
+};
+#endif
 
 const struct battery_info *battery_get_info(void)
 {
@@ -38,10 +62,10 @@ int board_cut_off_battery(void)
 	int rv;
 
 	/* Ship mode command must be sent twice to take effect */
-	rv = sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA);
+	rv = sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA1);
 
 	if (rv != EC_SUCCESS)
 		return rv;
 
-	return sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA);
+	return sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA2);
 }
