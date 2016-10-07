7c9032d47f1041fe5035c148a2df77f4eede0063
Mary Ruthven <mruthven@chromium.org>
2016-10-07 16:36:17 -0700

    g: use devid 0 and 1 to create a serial number
    To be able to identify different cr50 devices connected to the same
    machine we need a serial number. This change uses dev id 0 and 1 to come
    up with one.
    
    BUG=chrome-os-partner:56641
    BUG=chrome-os-partner:58342
    BRANCH=none
    TEST=lsusb -vd 18d1:5014 | grep iSerial shows different numbers for
    different devices. Verify when ccd is disabled the serial number is 0.
    
    Change-Id: I85c54af4a21bdfd0542019c02aa8420d9a879fae
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -324,6 +324,8 @@ const void * const usb_strings[] = {
 	[USB_STR_EC_NAME] = USB_STRING_DESC("EC"),
 	[USB_STR_UPGRADE_NAME] = USB_STRING_DESC("Firmware upgrade"),
 	[USB_STR_SPI_NAME] = USB_STRING_DESC("AP EC upgrade"),
+	[USB_STR_SERIALNO] = USB_STRING_DESC(DEFAULT_SERIALNO),
+	[USB_STR_I2C_NAME] = USB_STRING_DESC("I2C"),
 };
 BUILD_ASSERT(ARRAY_SIZE(usb_strings) == USB_STR_COUNT);
 #endif
