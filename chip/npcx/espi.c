3c2a5926b03e70f3603e7e23b2f260ba16e92f56
Mulin Chao <mlchao@nuvoton.com>
2016-10-11 11:59:32 +0800

    npcx: Enable VW's valid bits of SCI#/SMI# if no CONFIG_SCI_GPIO.
    Enable valid bits of SCI#/SMI# of eSPI's VW if there's no CONFIG_SCI_GPIO
    definition in board-level driver. This CL also fixed the order of VW
    event bits in comments.
    
    Modified sources:
    1. espi.c: Enable valid bits of SCI#/SMI#.
    
    BRANCH=none
    BUG=chrome-os-partner:34346
    TEST=make all; test nuvoton IC specific drivers
    
    Change-Id: I8d094513284b4ed42c5c26fe1975d71bbf050aa4
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

--- chip/npcx/espi.c
+++ chip/npcx/espi.c
@@ -58,7 +58,7 @@ struct vw_event_t {
 
 /* Default settings of VWEVMS registers (Please refer Table.43/44) */
 static const struct vwevms_config_t espi_in_list[] = {
-	/*IDX EN ENPL ENESP IE             VW Event Bit 3 -0 (M->S)          */
+	/* IDX EN ENPL ENESP IE             VW Event Bit 0 - 3 (M->S)         */
 	{0x02,  1,  0,  0,  0},  /* SLP_S3#,   SLP_S4#,    SLP_S5,    Reserve */
 	{0x03,  1,  0,  1,  0},  /* SUS_STAT#, PLTRST#,    ORST_WARN, Reserve */
 	{0x07,  1,  1,  1,  0},  /* HRST_WARN, SMIOUT#,    NMIOUT#,   Reserve */
@@ -69,10 +69,14 @@ static const struct vwevms_config_t espi_in_list[] = {
 
 /* Default settings of VWEVSM registers (Please refer Table.43/44) */
 static const struct vwevsm_config_t espi_out_list[] = {
-	/*IDX EN ENPL ENCDR VDMASK         VW Event Bit 3 -0 (S->M)        */
+	/* IDX EN ENPL ENCDR VDMASK         VW Event Bit 0 - 3 (S->M)         */
 	{0x04,  1,  0,  0, 0x0D}, /* PME#,       WAKE#,   Reserve, ORST_ACK   */
 	{0x05,  1,  0,  0, 0x0F}, /* SLV_BL_STS, ERR_NF,  ERR_F,   SLV_BL_DONE*/
+#ifdef CONFIG_SCI_GPIO
 	{0x06,  1,  1,  0, 0x0C}, /* HRST_ACK,   RCIN#,   SMI#,    SCI#       */
+#else
+	{0x06,  1,  1,  0, 0x0F}, /* HRST_ACK,   RCIN#,   SMI#,    SCI#       */
+#endif
 	{0x40,  1,  0,  0, 0x01}, /* Reserve,    Reserve, Reserve, SUS_ACK#   */
 };
 
