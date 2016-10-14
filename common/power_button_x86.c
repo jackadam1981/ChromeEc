c093e5e867a6ee8a8761eed8722e96e55c047fdd
james_chao <james_chao@asus.com>
2016-10-14 15:08:38 +0800

    Braswell: boot system after release power button
    BUG=chrome-os-partner:58513
    BRANCH=firmware-terra-7287.154.B
    TEST=Press the power button and do not release, the system is not
         boot. Release the power button then system start boot.
    
    Change-Id: I3a67ad80fb8be37ef3e8394356141e90832914f8
    Signed-off-by: james_chao <james_chao@asus.com>

--- common/power_button_x86.c
+++ common/power_button_x86.c
@@ -89,6 +89,8 @@ enum power_button_state {
 	PWRBTN_STATE_BOOT_KB_RESET,
 	/* Power button pressed when chipset was off; stretching pulse */
 	PWRBTN_STATE_WAS_OFF,
+
+	PWRBTN_STATE_OFF_WAIT_RELEASE,
 };
 static enum power_button_state pwrbtn_state = PWRBTN_STATE_IDLE;
 
@@ -104,6 +106,7 @@ static const char * const state_names[] = {
 	"init-on",
 	"recovery",
 	"was-off",
+	"wait-release"
 };
 
 /*
@@ -290,7 +293,6 @@ static void state_machine(uint64_t tnow)
 			 * power button before the chipset finishes waking from
 			 * hard off state.
 			 */
-			chipset_exit_hard_off();
 			tnext_state = tnow + PWRBTN_INITIAL_US;
 			pwrbtn_state = PWRBTN_STATE_WAS_OFF;
 		} else {
@@ -359,7 +361,7 @@ static void state_machine(uint64_t tnow)
 		 * down. */
 		set_pwrbtn_to_pch(1);
 		if (power_button_is_pressed())
-			pwrbtn_state = PWRBTN_STATE_EAT_RELEASE;
+			pwrbtn_state = PWRBTN_STATE_OFF_WAIT_RELEASE;
 		else
 			pwrbtn_state = PWRBTN_STATE_IDLE;
 		break;
@@ -368,11 +370,15 @@ static void state_machine(uint64_t tnow)
 		 * true power button state to the PCH. */
 		if (power_button_is_pressed()) {
 			/* User is still holding the power button */
-			pwrbtn_state = PWRBTN_STATE_HELD;
+			pwrbtn_state = PWRBTN_STATE_OFF_WAIT_RELEASE;
 		} else {
 			/* Stop stretching the power button press */
 			power_button_released(tnow);
 		}
+		break;
+	case PWRBTN_STATE_OFF_WAIT_RELEASE:
+		if (!power_button_is_pressed())
+			chipset_exit_hard_off();
 		break;
 	case PWRBTN_STATE_IDLE:
 	case PWRBTN_STATE_HELD:
@@ -465,9 +471,12 @@ static void powerbtn_x86_changed(void)
 			CPRINTS("PB ignoring release");
 			pwrbtn_state = PWRBTN_STATE_IDLE;
 			return;
+		} else if (pwrbtn_state == PWRBTN_STATE_OFF_WAIT_RELEASE) {
+			chipset_exit_hard_off();
+			power_button_pressed(get_time().val + PWRBTN_DELAY_T0);
+		} else {
+			power_button_released(get_time().val);
 		}
-
-		power_button_released(get_time().val);
 	}
 
 	/* Wake the power button task */
