6af1b556446f9c7a525e01645f9c9e146396e0f0
Shawn Nematbakhsh <shawnn@chromium.org>
2016-10-17 14:12:38 -0700

    pd: Limit input current to 500mA on PD voltage transition
    Upon requesting a PD power contract at a new voltage, keep the input
    current limit at 500mA until PD_RDY is received.
    
    BUG=b:30744563
    BRANCH=Ryu
    TEST=Manual on kevin, set ilim to 5V through `chglim` console command,
    attach zinger. Set ilim to 20V through `chglim`, verify that ilim goes
    from 3A to 500mA to 3A.
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: I452f183cfb958780e336a9f99dc6398356de17a0

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -636,9 +636,16 @@ static int pd_send_request_msg(int port, int always_send_request)
 		 */
 		return -1;
 
-	/* Don't re-request the same voltage */
-	if (!always_send_request && pd[port].prev_request_mv == supply_voltage)
-		return EC_SUCCESS;
+	if (!always_send_request) {
+		/* Don't re-request the same voltage */
+		if (pd[port].prev_request_mv == supply_voltage)
+			return EC_SUCCESS;
+		/* Limit current to PD_MIN_MA during transition */
+		else
+			charge_manager_set_ceil(port,
+						CEIL_REQUESTOR_PD,
+						PD_MIN_MA);
+	}
 
 	CPRINTF("Req C%d [%d] %dmV %dmA", port, RDO_POS(rdo),
 		supply_voltage, curr_limit);
