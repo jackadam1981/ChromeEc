997f1c616076304150b9b44cb1cf85869c530ba2
Daisuke Nojiri <dnojiri@chromium.org>
2016-10-18 09:27:34 -0700

    ects: Nested interrupt test
    Add a nested interrupt test to eCTS.
    
    BUG=none
    BRANCH=none
    TEST=cts.py -m gpio, interrupt, timer; make buildall
    
    Change-Id: I34dc7b4e819051b9070a11e69d13d6be704f2e5f

--- board/nucleo-f072rb/board.h
+++ board/nucleo-f072rb/board.h
@@ -18,9 +18,12 @@
 /* Optional features */
 #define CONFIG_STM_HWTIMER32
 
+#ifdef CTS_MODULE
+#define STM32_IRQ_EXT2_3_PRIORITY	2
 #ifdef CTS_MODULE_I2C
 #define CONFIG_I2C
 #define CONFIG_I2C_MASTER
+#endif
 #endif
 
 #undef CONFIG_WATCHDOG_HELP
