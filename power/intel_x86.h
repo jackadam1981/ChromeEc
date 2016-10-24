42fb1559a8e2456fbe6fbbf5abc9e69c520a28b9
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-10-24 15:54:53 -0700

    power: Group intel x86 common code
    Groping the intel x86 common code so that the future power
    sequencing code implementation can make use of the existing
    code.
    
    BUG=none
    BRANCH=none
    TEST=make buildall -j
         Reef can boot to OS. S3, S5, hibernate are working.
    
    Change-Id: I29dc208eacb3db47c640d028e9551ab3d8d4288c
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

---
+++ power/intel_x86.h
@@ -0,0 +1,71 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Intel X86 chipset power control module for Chrome EC */
+
+
+#ifndef __CROS_EC_INTEL_X86_H
+#define __CROS_EC_INTEL_X86_H
+
+#include "power.h"
+
+#ifdef CONFIG_CHIPSET_APOLLOLAKE
+/* Input state flags */
+#define IN_RSMRST_N	POWER_SIGNAL_MASK(X86_RSMRST_N)
+#define IN_ALL_SYS_PG	POWER_SIGNAL_MASK(X86_ALL_SYS_PG)
+#define IN_SLP_S0_N	POWER_SIGNAL_MASK(X86_SLP_S0_N)
+#define IN_SLP_S3_N	POWER_SIGNAL_MASK(X86_SLP_S3_N)
+#define IN_SLP_S4_N	POWER_SIGNAL_MASK(X86_SLP_S4_N)
+#define IN_SUSPWRDNACK	POWER_SIGNAL_MASK(X86_SUSPWRDNACK)
+#define IN_SUS_STAT_N	POWER_SIGNAL_MASK(X86_SUS_STAT_N)
+
+#ifdef CONFIG_POWER_S0IX
+#define IN_ALL_PM_SLP_DEASSERTED (IN_SLP_S0_N | \
+				  IN_SLP_S3_N | \
+				  IN_SLP_S4_N)
+#else
+#define IN_ALL_PM_SLP_DEASSERTED (IN_SLP_S3_N | \
+				  IN_SLP_S4_N)
+#endif /* CONFIG_POWER_S0IX */
+
+#define IN_PGOOD_ALL_CORE (IN_RSMRST_N)
+#define IN_ALL_S0 (IN_PGOOD_ALL_CORE | IN_ALL_PM_SLP_DEASSERTED)
+#endif /* CONFIG_CHIPSET_APOLLOLAKE */
+
+#ifdef CONFIG_CHIPSET_SKYLAKE
+/* Input state flags */
+#define IN_PCH_SLP_S0_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S0_DEASSERTED)
+#define IN_PCH_SLP_S3_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S3_DEASSERTED)
+#define IN_PCH_SLP_S4_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S4_DEASSERTED)
+#define IN_PCH_SLP_SUS_DEASSERTED POWER_SIGNAL_MASK(X86_SLP_SUS_DEASSERTED)
+
+#ifdef CONFIG_POWER_S0IX
+#define IN_ALL_PM_SLP_DEASSERTED (IN_PCH_SLP_S0_DEASSERTED | \
+				  IN_PCH_SLP_S3_DEASSERTED | \
+				  IN_PCH_SLP_S4_DEASSERTED | \
+				  IN_PCH_SLP_SUS_DEASSERTED)
+#else
+#define IN_ALL_PM_SLP_DEASSERTED (IN_PCH_SLP_S3_DEASSERTED | \
+				  IN_PCH_SLP_S4_DEASSERTED | \
+				  IN_PCH_SLP_SUS_DEASSERTED)
+#endif /* CONFIG_POWER_S0IX */
+
+/*
+ * DPWROK is NC / stuffing option on initial boards.
+ * TODO(shawnn): Figure out proper control signals.
+ */
+#define IN_PGOOD_ALL_CORE 0
+#define IN_ALL_S0 (IN_PGOOD_ALL_CORE | IN_ALL_PM_SLP_DEASSERTED)
+#endif /* CONFIG_CHIPSET_SKYLAKE */
+
+extern int forcing_shutdown;  /* Forced shutdown in progress? */
+extern int forcing_coldreset; /* Forced coldreset in progress? */
+extern int power_s5_up;       /* Chipset is sequencing up or down */
+
+void handle_rsmrst(enum power_state state);
+void chipset_force_g3(void);
+enum power_state _power_handle_state(enum power_state state);
+
+#endif /* __CROS_EC_INTEL_X86_H */
