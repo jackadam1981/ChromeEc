50590d3e88cbf703c7818c7ea6269ef5b46a8af4
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-10-24 15:54:53 -0700

    power: Group Intel x86 power sequencing common code
    Grouping the Intel x86 power sequencing common code so that
    the future chipset power sequencing implementation can make
    use of the existing code.
    
    BUG=chrome-os-partner:59141
    BRANCH=none
    TEST=make buildall -j
         Reef can boot to OS. S3, S5, hibernate are working.
    
    Change-Id: I29dc208eacb3db47c640d028e9551ab3d8d4288c
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

---
+++ power/intel_x86.h
@@ -0,0 +1,38 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Intel X86 chipset power control module for Chrome EC */
+
+
+#ifndef __CROS_EC_INTEL_X86_H
+#define __CROS_EC_INTEL_X86_H
+
+#include "power.h"
+
+extern int power_s5_up;       /* Chipset is sequencing up or down */
+
+/**
+ * Handle RSMRST signal.
+ *
+ * @param state Current chipset state.
+ */
+void handle_rsmrst(enum power_state state);
+
+/**
+ * Force chipset to G3 state.
+ *
+ * @return power_state New chipset state.
+ */
+enum power_state chipset_force_g3(void);
+
+/**
+ * Handle power states.
+ *
+ * @param state        Current chipset state.
+ * @return power_state New chipset state.
+ */
+enum power_state _power_handle_state(enum power_state state);
+
+#endif /* __CROS_EC_INTEL_X86_H */
