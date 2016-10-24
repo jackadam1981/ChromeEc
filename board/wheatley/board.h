4f77937e8c28918d82e7b82ee6dd4f7d5ad92a7f
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-10-24 15:54:53 -0700

    power: Group intel X86 common code
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I29dc208eacb3db47c640d028e9551ab3d8d4288c
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- board/wheatley/board.h
+++ board/wheatley/board.h
@@ -148,10 +148,6 @@
 #undef CONFIG_CMD_TIMERINFO
 #undef CONFIG_CONSOLE_CMDHELP
 
-/* Features of eSPI */
-#undef CONFIG_ESPI       /* Use eSPI protocol for host interface of x86 CPU */
-#undef CONFIG_VW_SIGNALS /* Use VW signals instead of GPIOs */
-
 #ifndef __ASSEMBLER__
 
 #include "gpio_signal.h"
