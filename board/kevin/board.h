0d38557a8d916b193bd56d9800c991ab0719f63c
philipchen <philipchen@google.com>
2016-10-26 15:39:25 -0700

    gru/kevin: turn on flags to save code size
    Turn on CONFIG_HOSTCMD_ALIGNED and
    CONFIG_COMMON_GPIO_SHORTNAMES to squeeze more
    space for the upcoming new code.
    
    BUG=None
    BRANCH=gru, kevin
    TEST=Check the map to confirm the size reduction
    
    Change-Id: I7a9ca8fccf6d57a797c391dc76cacb0b929e14df

--- board/kevin/board.h
+++ board/kevin/board.h
@@ -160,6 +160,11 @@
 #undef  CONFIG_PECI
 #undef  CONFIG_PSTORE
 
+/* Reduce code cize */
+#define CONFIG_COMMON_GPIO_SHORTNAMES
+#define GPIO_NAME_BY_PIN(port, index) #port#index
+#define CONFIG_HOSTCMD_ALIGNED
+
 /* Modules we want to exclude */
 #undef CONFIG_CMD_ACCEL_INFO
 #undef CONFIG_CMD_FLASH
