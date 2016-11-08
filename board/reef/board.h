1a348ac66a7f246b01c2486754d6981024ab11c6
Archana Patni <archana.patni@intel.com>
2016-11-08 12:45:40 +0530

    Apollolake: Enter/exit from S0ix based on host commands from kernel
    This patch changes the entry/exit model for S0ix from a PCH
    SLP_S0 signal based model to a hybrid host event/direct interrupt
    model. The kernel will send host events on kernel freeze/thaw exit;
    EC will initiate the S0ix entry based on host command and exit via
    another host command from kernel.
    
    The assertion of SLP_S0 comes later than HC(suspend) and deasserion
    of SLP_S0 comes earlier than HC(resume).
            ________                        ________
    SLP_S0          |______________________|
            _____                             ________
    HC           |___________________________|
    
    BRANCH=Glados
    BUG=chrome-os-partner:58740
    TEST=Build/flash EC and check 'echo freeze > /sys/power/state'
    command in OS shell. Verify idle state transitions during display off
    and periodic wakes from S0ix do not lead to state transitions in EC.
    
    Change-Id: Ie18c6c2ac8998f59141641567d1d740cd72c2d2e
    Signed-off-by: Kyoung Kim <kyoung.il.kim@intel.com>
    Signed-off-by: Subramony Sesha <subramony.sesha@intel.com>
    Signed-off-by: Divagar Mohandass <divagar.mohandass@intel.com>
    Signed-off-by: Archana Patni <archana.patni@intel.com>

--- board/reef/board.h
+++ board/reef/board.h
@@ -109,6 +109,7 @@
 #define CONFIG_POWER_BUTTON
 #define CONFIG_POWER_BUTTON_X86
 #define CONFIG_POWER_COMMON
+#define CONFIG_POWER_S0IX
 
 /* EC */
 #define CONFIG_ADC
@@ -215,7 +216,6 @@ enum pwm_channel {
 
 enum power_signal {
 	X86_RSMRST_N = 0,
-	X86_SLP_S0_N,
 	X86_SLP_S3_N,
 	X86_SLP_S4_N,
 	X86_SUSPWRDNACK,
