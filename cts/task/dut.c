a50a17c7350f878e9087e618eca06baf97c4f80b
Daisuke Nojiri <dnojiri@chromium.org>
2016-11-09 15:29:24 -0800

    eCTS: Test task priority
    CTS task wakes up A and C then goes to sleep. Since C has a higher
    priority, C should run first. This should result in C running
    one more time than A (or B).
    
    BUG=chromium:663873
    BRANCH=none
    TEST=cts.py -m task
    
    Change-Id: I89c733ba3aab09b293edf8583d6ed73791531e59
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- cts/task/dut.c
+++ cts/task/dut.c
@@ -75,6 +75,32 @@ enum cts_rc test_task_switch(void)
 	return CTS_RC_SUCCESS;
 }
 
+enum cts_rc test_task_priority(void)
+{
+	uint32_t event;
+
+	repeat_count = 2;
+
+	task_wake(TASK_ID_TESTA);
+	task_wake(TASK_ID_TESTC);
+
+	event = task_wait_event(5 * SECOND);
+
+	if (event != TASK_EVENT_WAKE) {
+		CPRINTS("Woken up by unexpected event: 0x%08x", event);
+		return CTS_RC_FAILURE;
+	}
+
+	if (wake_count[0] != repeat_count - 1
+			|| wake_count[1] != repeat_count - 1) {
+		CPRINTS("Unexpected counter values: %d %d %d",
+			wake_count[0], wake_count[1], wake_count[2]);
+		return CTS_RC_FAILURE;
+	}
+
+	return CTS_RC_SUCCESS;
+}
+
 #include "cts_testlist.h"
 
 void cts_task(void)
