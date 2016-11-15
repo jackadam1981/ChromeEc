9cc6af91c8cd4513c1a96f408e937d0900584422
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-11-14 17:38:05 -0800

    charge_state_v2: Limit the input current to max allowed current
    When battery is not present, input current is set to PD_MAX_CURRENT_MA.
    If the input power set is greater than the system power, system might
    get damaged. Hence, limit the input current to max allowed current.
    
    BUG=chrome-os-partner:58498
    BRANCH=none
    TEST=Manually tested on Reef. Removed the battery & using 'charger'
         console command observed the following.
         With Zinger charger at 20V - Input current is set to 2.25A
         With Type-C & other chargers - Input current is set to 3A
    
    Change-Id: Ife8686f322e095aa74b740a7c469bfe87107fb9a
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -560,7 +560,8 @@ static void charge_manager_refresh(void)
 #ifdef HAS_TASK_CHG_RAMP
 		chg_ramp_charge_supplier_change(
 				new_port, new_supplier, new_charge_current,
-				registration_time[new_port]);
+				registration_time[new_port],
+				new_charge_voltage);
 #else
 #ifdef CONFIG_CHARGE_RAMP_HW
 		/* Enable or disable charge ramp */
@@ -568,7 +569,8 @@ static void charge_manager_refresh(void)
 #endif
 		board_set_charge_limit(new_port, new_supplier,
 					new_charge_current,
-					new_charge_current_uncapped);
+					new_charge_current_uncapped,
+					new_charge_voltage);
 #endif /* HAS_TASK_CHG_RAMP */
 
 		power_changed = 1;
