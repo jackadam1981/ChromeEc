5e1af5db219f90b20a0f2bdde9021ca7c91de173
Mary Ruthven <mruthven@chromium.org>
2016-11-18 11:16:40 -0800

    cr50: remove the pull down on uart1 tx
    When the uart is active we should have a pull down on uart 1 tx. It
    should also be removed during deep sleep.
    
    BUG=chrome-os-partner:60020
    BRANCH=none
    TEST=manual
    	Disconnect servo
    	Use the suzyq consoles to turn off the AP and put the EC into
    	hibernate
    
    	run ccd disable. Measure power and make sure vddiob is consuming
    	around 0.3mW
    
    Change-Id: I8a653c28800cfbeeb1b4b8598d166846124c6b53
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -175,6 +175,9 @@ void board_configure_deep_sleep_wakepins(void)
 	GWRITE_FIELD(PINMUX, EXITEN0, DIOA12, 0); /* SPS_CS_L */
 	/* TODO remove i2cs wake event */
 
+	/* Remove the pulldown on EC uart tx */
+	GWRITE_FIELD(PINMUX, DIOB5_CTL, PD, 0);
+
 	/*
 	 * DIOA3 is GPIO_DETECT_AP which is used to detect if the AP is in S0.
 	 * If the AP is in s0, cr50 should not be in deep sleep so wake up.
