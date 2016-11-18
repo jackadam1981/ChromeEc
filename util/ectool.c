f6dd3385e393553dab8ebe3d12b495f27f2b893f
Manoj Gupta <manojgupta@google.com>
2016-11-18 11:08:46 -0800

    Fix EC build for latest llvm on arm64
    util/ectool.c:465:29: error: taking address of packed member 'value' of class
    or structure 'ec_params_get_set_value' may result in an unaligned pointer
    value [-Werror,-Waddress-of-packed-member]
    
    "if (!parse_bool(argv[1], &p.value)) {"
    
    For this case, the pointer is always aligned but clang complains.
    Workaround using double pointer casts to char and uint.
    
    llvm bug: https://llvm.org/bugs/show_bug.cgi?id=31050
    
    BRANCH=none
    BUG=chromium:665240
    TEST=Builds now
    
    Change-Id: I5695a9605b5ab47d11f7e9ecacc52f2ba53385e7

--- util/ectool.c
+++ util/ectool.c
@@ -462,7 +462,12 @@ int cmd_s5(int argc, char *argv[])
 
 	if (argc > 1) {
 		p.flags |= EC_GSV_SET;
-		if (!parse_bool(argv[1], &p.value)) {
+		/* Double casting is a workaround to silent clang error.
+		 * The pointer is always aligned but clang complains.
+		 * https://crbug.com/665240
+		 * https://llvm.org/bugs/show_bug.cgi?id=31050
+		 */
+		if (!parse_bool(argv[1], (uint32_t*)((char *)&p.value))) {
 			fprintf(stderr, "invalid arg \"%s\"\n", argv[1]);
 			return -1;
 		}
