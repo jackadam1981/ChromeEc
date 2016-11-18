62b7b237e5ed646a54ae078ab21dc702e35e31cd
Mario Tesi <mario.tesi@st.com>
2016-11-18 09:48:42 +0100

    driver: accel: Add accel driver for LIS2DH/LIS2DH12
        This add basics for acc sensor ST lis2dh/lis2dh12
        Still need to add interrupt management for embedded
        functions and FIFO
    
        BUG=none
        BRANCH=master
        TEST=Tested on discovery BOARD with sensor connected on
        EC i2c master bus. Added motion sense task on discovery
        board task list, added gpio info in board configuration
        file and tested with motion sense console commands. Data
        for acc seems ok: can successfully change ODR and
        full scale range. Device tested is lis2dh (lis2dh12 simply
        differs for low pin count)
    
    Change-Id: I16abeac3f139a604094b38d8d8b857a62c93a242
    Signed-off-by: Mario Tesi <mario.tesi@st.com>

---
+++ driver/accel_lis2dh.c
@@ -0,0 +1,365 @@
+/* Copyright (c) 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * LIS2DH/LIS2DH12 accelerometer module for Chrome EC 3D digital accelerometer
+ */
+
+#include "accelgyro.h"
+#include "common.h"
+#include "console.h"
+#include "driver/accel_lis2dh.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "math_util.h"
+#include "task.h"
+#include "util.h"
+
+#define CPRINTF(format, args...) cprintf(CC_ACCEL, format, ## args)
+
+/* Internal data structure */
+struct lis2dh_data lis2dh_a_data;
+
+/**
+ * Read single register
+ */
+static inline int raw_read8(const int port, const int addr, const int reg,
+			    int *data_ptr)
+{
+	return i2c_read8(port, addr, reg, data_ptr);
+}
+
+/**
+ * Write single register
+ */
+static inline int raw_write8(const int port, const int addr, const int reg,
+			     int data)
+{
+	return i2c_write8(port, addr, reg, data);
+}
+
+/**
+ * Read n bytes from accelerometer
+ *
+ * NOTE: SUB address MSB must be set for autoincrement in multi read
+ */
+static int raw_read_n(const int port, const int addr, const uint8_t reg,
+		      uint8_t *data_ptr, const int len)
+{
+	int rv = -EC_ERROR_PARAM1;
+	uint8_t reg_a = reg | I2C_AUTO_INC;
+
+	i2c_lock(port, 1);
+	rv = i2c_xfer(port, addr, &reg_a, 1, data_ptr, len, I2C_XFER_SINGLE);
+	i2c_lock(port, 0);
+
+	return rv;
+}
+
+ /**
+ * write_data_with_mask - Write register with mask
+ * @s: Motion sensor pointer
+ * @reg: Device register
+ * @mask: The mask to search
+ * @data: Data pointer
+ */
+static int write_data_with_mask(const struct motion_sensor_t *s, int reg,
+				uint8_t mask, uint8_t data)
+{
+	int err;
+	int new_data = 0x00, old_data = 0x00;
+
+	err = raw_read8(s->port, s->addr, reg, &old_data);
+	if (err != EC_SUCCESS)
+		return err;
+
+	new_data = ((old_data & (~mask)) | ((data << __builtin_ctz(mask)) & mask));
+
+	if (new_data == old_data)
+		return EC_SUCCESS;
+
+	return raw_write8(s->port, s->addr, reg, new_data);
+}
+
+/**
+ * set_range - set full scale range
+ * @s: Motion sensor pointer
+ * @range: Range
+ * @rnd: Round up/down flag
+ */
+static int set_range(const struct motion_sensor_t *s,
+		     int range, int rnd)
+{
+	int err, normalized_rate;
+	struct lis2dh_data *data = s->drv_data;
+	int val;
+
+	val = LIS2DH_FS_TO_REG(range);
+	normalized_rate = LIS2DH_FS_TO_NORMALIZE(range);
+
+	if (rnd && (range < normalized_rate))
+		val++;
+
+	/* Adjust rounded values */
+	if (val > LIS2DH_FS_16G_VAL) {
+		val = LIS2DH_FS_16G_VAL;
+		normalized_rate = 16;
+	}
+
+	if (val < LIS2DH_FS_2G_VAL) {
+		val = LIS2DH_FS_2G_VAL;
+		normalized_rate = 2;
+	}
+	/*
+	 * Lock accel resource to prevent another task from attempting
+	 * to write accel parameters until we are done.
+	 */
+	mutex_lock(s->mutex);
+	err = write_data_with_mask(s, LIS2DH_CTRL4_ADDR, LIS2DH_FS_MASK, val);
+
+	if (err == EC_SUCCESS)
+		data->base.range = normalized_rate;
+
+	mutex_unlock(s->mutex);
+	return EC_SUCCESS;
+}
+
+static int get_range(const struct motion_sensor_t *s)
+{
+	struct lis2dh_data *data = s->drv_data;
+
+	return data->base.range;
+}
+
+/* TODO: must support multiple resolution */
+static int set_resolution(const struct motion_sensor_t *s, int res, int rnd)
+{
+	return EC_SUCCESS;
+}
+
+static int get_resolution(const struct motion_sensor_t *s)
+{
+	return LIS2DH_RESOLUTION;
+}
+
+static int set_data_rate(const struct motion_sensor_t *s, int rate, int rnd)
+{
+	int ret, normalized_rate;
+	struct lis2dh_data *data = s->drv_data;
+	uint8_t reg_val;
+
+	if (rate == 0) {
+		/* Power Off device */
+		ret = write_data_with_mask(s, LIS2DH_CTRL1_ADDR,
+					   LIS2DH_ACC_ODR_MASK,
+					   LIS2DH_ODR_POWER_OFF_VAL);
+		return ret;
+	}
+
+	reg_val = LIS2DH_ODR_TO_REG(rate);
+	normalized_rate = LIS2DH_ODR_TO_NORMALIZE(rate);
+
+	if (rnd && (normalized_rate < rate)) {
+		reg_val++;
+		normalized_rate = LIS2DH_REG_TO_NORMALIZE(reg_val);
+	}
+
+	/* Adjust rounded value */
+	if (reg_val > LIS2DH_ODR_400HZ_VAL) {
+		reg_val = LIS2DH_ODR_400HZ_VAL;
+		normalized_rate = 400000;
+	} else if (reg_val < LIS2DH_ODR_1HZ_VAL) {
+		reg_val = LIS2DH_ODR_1HZ_VAL;
+		normalized_rate = 1000;
+	}
+
+	/*
+	 * Lock accel resource to prevent another task from attempting
+	 * to write accel parameters until we are done
+	 */
+	mutex_lock(s->mutex);
+	CPRINTF("VAL %d", reg_val);
+	ret = write_data_with_mask(s, LIS2DH_CTRL1_ADDR, LIS2DH_ACC_ODR_MASK,
+				   reg_val);
+	if (ret == EC_SUCCESS)
+		data->base.odr = normalized_rate;
+
+	mutex_unlock(s->mutex);
+	return ret;
+}
+
+static int get_data_rate(const struct motion_sensor_t *s)
+{
+	struct lis2dh_data *data = s->drv_data;
+
+	return data->base.odr;
+}
+
+static int set_offset(const struct motion_sensor_t *s,
+		      const int16_t *offset, int16_t temp)
+{
+	struct lis2dh_data *data = s->drv_data;
+
+	data->offset[X] = offset[X];
+	data->offset[Y] = offset[Y];
+	data->offset[Z] = offset[Z];
+	return EC_SUCCESS;
+}
+
+static int get_offset(const struct motion_sensor_t *s,
+		      int16_t *offset, int16_t *temp)
+{
+	struct lis2dh_data *data = s->drv_data;
+
+	offset[X] = data->offset[X];
+	offset[Y] = data->offset[Y];
+	offset[Z] = data->offset[Z];
+	*temp = EC_MOTION_SENSE_INVALID_CALIB_TEMP;
+	return EC_SUCCESS;
+}
+
+static int is_data_ready(const struct motion_sensor_t *s, int *ready)
+{
+	int ret, tmp;
+
+	ret = raw_read8(s->port, s->addr, LIS2DH_STATUS_REG, &tmp);
+	if (ret != EC_SUCCESS) {
+		CPRINTF("[%T %s type:0x%X RS Error]", s->name, s->type);
+		return ret;
+	}
+
+	*ready = (LIS2DH_STS_XLDA_UP == (tmp & LIS2DH_STS_XLDA_UP));
+	return EC_SUCCESS;
+}
+
+/*
+ * TODO: Implement FIFO support
+ *
+ * Is not very efficient to collect the data in read: better have an interrupt
+ * and collect the FIFO, even if it has one item: we don't have to check if the
+ * sensor is ready (minimize I2C access)
+ */
+static int read(const struct motion_sensor_t *s, vector_3_t v)
+{
+	uint8_t raw[LIS2DH_OUT_XYZ_SIZE];
+	int ret, i, tmp = 0;
+	struct lis2dh_data *data = s->drv_data;
+
+	ret = is_data_ready(s, &tmp);
+	if (ret != EC_SUCCESS)
+		return ret;
+
+	/*
+	 * If sensor data is not ready, return the previous read data.
+	 * Note: return success so that motion senor task can read again
+	 * to get the latest updated sensor data quickly.
+	 */
+	if (!tmp) {
+		if (v != s->raw_xyz)
+			memcpy(v, s->raw_xyz, sizeof(s->raw_xyz));
+		return EC_SUCCESS;
+	}
+
+	/* Read output data bytes starting at LIS2DH_OUT_X_L_ADDR */
+	ret = raw_read_n(s->port, s->addr, LIS2DH_OUT_X_L_ADDR, raw,
+			 LIS2DH_OUT_XYZ_SIZE);
+	if (ret != EC_SUCCESS) {
+		CPRINTF("[%T %s type:0x%X RD XYZ Error]",
+			s->name, s->type);
+		return ret;
+	}
+
+	for (i = X; i <= Z; i++) {
+		/* 10 bit resolution */
+		v[i] = ((int16_t)((raw[i * 2 + 1] << 8) | raw[i * 2]) >> 6);
+		/* Multiply axis gain related to FS */
+		v[i] = v[i] * LIS2DH_FS_TO_GAIN(data->base.range);
+	}
+	/* Apply rotation matrix */
+	rotate(v, *s->rot_standard_ref, v);
+
+	/* apply offset in the device coordinates */
+	for (i = X; i <= Z; i++)
+		v[i] += (data->offset[i] << 5) / data->base.range;
+
+	return EC_SUCCESS;
+}
+
+static int init(const struct motion_sensor_t *s)
+{
+	int ret = 0, tmp;
+
+	ret = raw_read8(s->port, s->addr, LIS2DH_WHO_AM_I_REG, &tmp);
+	if (ret != EC_SUCCESS)
+		return EC_ERROR_UNKNOWN;
+
+	if (tmp != LIS2DH_WHO_AM_I)
+		return EC_ERROR_ACCESS_DENIED;
+
+	mutex_lock(s->mutex);
+	/* Device can be re-initialized after a reboot so any control
+	 * register must be restored to it's default
+	 */
+	/* Enable all accel axes data and clear old settings */
+	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL1_ADDR,
+			 LIS2DH_ENABLE_ALL_AXES);
+	if (ret != EC_SUCCESS)
+		goto err_unlock;
+
+	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL2_ADDR,
+			 LIS2DH_CTRL2_RESET_VAL);
+	if (ret != EC_SUCCESS)
+		goto err_unlock;
+
+	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL3_ADDR,
+			 LIS2DH_CTRL3_RESET_VAL);
+	if (ret != EC_SUCCESS)
+		goto err_unlock;
+
+	/* Enable BDU */
+	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL4_ADDR,
+			 LIS2DH_BDU_MASK);
+	if (ret != EC_SUCCESS)
+		goto err_unlock;
+
+	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL5_ADDR,
+			 LIS2DH_CTRL5_RESET_VAL);
+	if (ret != EC_SUCCESS)
+		goto err_unlock;
+
+	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL6_ADDR,
+			 LIS2DH_CTRL6_RESET_VAL);
+	if (ret != EC_SUCCESS)
+		goto err_unlock;
+	mutex_unlock(s->mutex);
+
+	/* Config initial Acc Range */
+	ret = set_range(s, s->default_range, 0);
+	if (ret != EC_SUCCESS)
+		return ret;
+
+	CPRINTF("[%T %s: MS Done Init type:0x%X range:%d]\n",
+		s->name, s->type, get_range(s));
+	return ret;
+
+err_unlock:
+	mutex_unlock(s->mutex);
+
+	return EC_ERROR_UNKNOWN;
+}
+
+const struct accelgyro_drv lis2dh_drv = {
+	.init = init,
+	.read = read,
+	.set_range = set_range,
+	.get_range = get_range,
+	.set_resolution = set_resolution,
+	.get_resolution = get_resolution,
+	.set_data_rate = set_data_rate,
+	.get_data_rate = get_data_rate,
+	.set_offset = set_offset,
+	.get_offset = get_offset,
+	.perform_calib = NULL,
+};
