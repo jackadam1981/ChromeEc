e73015dd334f45835e1756c80928e61bc0ccd23d
Divya Sasidharan <divya.s.sasidharan@intel.com>
2016-11-22 15:49:13 -0800

    pd_task: Add CC debounce time in DRP auto toggle
    When TCPC is put into low power mode and type-c cable
    is plugged in it returns to normal mode and then pd_task
    reads CC values to determine the type of device connected.
    In case of Analogix it is observed that CC values read are
    invalid and needs around 100ms debounce time.
    
    BUG=chrome-os-partner:60021
    BRANCH=none
    TEST=On Reef let analogix enter low power mode, and then plug in
    hoho/apple hdmi dongle and verified if it is detected. Verified
    multiple iterations.
    
    Change-Id: Ibf4d49e64b07d18b6d1402788b7b549fce80ac3c
    Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -2783,9 +2783,17 @@ defined(CONFIG_CASE_CLOSED_DEBUG_EXTERNAL)
 				  drp_state != PD_DRP_FORCE_SINK))
 				/* SRC allowed unless ForceSNK or Toggle Off */
 				next_state = PD_STATE_SRC_DISCONNECTED;
-			else
-				/* Anything else, keep toggling */
+			else {
+				/*
+				 * Anything else, keep toggling.
+				 * The CC values are invalid, give enough time
+				 * to do CC debounce
+				 */
 				next_state = PD_STATE_DRP_AUTO_TOGGLE;
+				timeout = 100*MSEC;
+				set_state(port, next_state);
+				break;
+			}
 
 			if (next_state == PD_STATE_SNK_DISCONNECTED) {
 				tcpm_set_cc(port, TYPEC_CC_RD);
