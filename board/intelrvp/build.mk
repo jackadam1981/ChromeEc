6e21ed947d34059257776f7fba5124de65fe8722
Vijay Hiremath <vijay.p.hiremath@intel.com>
2016-11-30 12:37:24 -0800

    Intel RVP: Add initial board bringup code
    Added bare minimum code to bringup the Intel RVP using
    Nuvoton AIC.
    
    BUG=none
    BRANCH=none
    TEST=Intel RVP boots to Chrome OS using Nuvoton AIC.
    
    Change-Id: I3c92203b5bfd8860b8a3ea11a26bc33b43541642
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>
    Signed-off-by: Kevin K Wong <kevin.k.wong@intel.com>

--- board/intelrvp/build.mk
+++ board/intelrvp/build.mk
@@ -1,5 +1,5 @@
 # -*- makefile -*-
-# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
@@ -9,6 +9,4 @@
 CHIP:=npcx
 CHIP_VARIANT:=npcx5m6g
 
-board-y=board.o led.o
-board-$(CONFIG_BATTERY_SMART)+=battery.o
-board-$(CONFIG_USB_POWER_DELIVERY)+=usb_pd_policy.o
+board-y=board.o
