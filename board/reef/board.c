ef4c2f941b6ca866c8460844b9e6dcb4b9249c76
Ryan Zhang <ryan.zhang.quanta@gmail.com>
2016-12-01 17:53:35 +0800

    Electro: Fix angle calculation
    Follow Cyan's & Glados's setting.
    
    BUG=chrome-os-partner:60477
    BRANCH=master
    TEST=angles and screen rotate as expect
    
    Change-Id: I2b71f93176e2aba43297158be2897a61a7e1b702
    Signed-off-by: Ryan Zhang <Ryan.Zhang@quantatw.com>

--- board/reef/board.c
+++ board/reef/board.c
@@ -751,8 +751,8 @@ static struct mutex g_base_mutex;
 /* Matrix to rotate accelrator into standard reference frame */
 const matrix_3x3_t lid_standard_ref = {
 	{ FLOAT_TO_FP(1), 0,  0},
-	{ 0, FLOAT_TO_FP(-1), 0},
-	{ 0, 0,  FLOAT_TO_FP(-1)}
+	{ 0, FLOAT_TO_FP(1), 0},
+	{ 0, 0,  FLOAT_TO_FP(1)}
 };
 
 const matrix_3x3_t base_standard_ref = {
