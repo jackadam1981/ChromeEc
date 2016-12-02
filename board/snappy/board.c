67cca1bfddee43f3576d691bbbc1c5e8e7cfedff
Bruce <Bruce.Wan@quantatw.com>
2016-12-02 16:58:27 +0800

    common: modify keyboard scan rate.
    Slow the keyboard scan rate from 50 us to 80 us.
    
    BUG=chrome-os-partner:60335
    BRANCH=none
    TEST=check press key "f3" then system only output "f3" scan code.
    
    Change-Id: Ic6d009fc45948267447b532beeeb12924ec77069
    Signed-off-by: Bruce.Wan <Bruce.Wan@quantatw.com>

--- board/snappy/board.c
+++ board/snappy/board.c
@@ -1008,6 +1008,20 @@ int board_get_version(void)
 	return version;
 }
 
+/* Keyboard scan setting */
+struct keyboard_scan_config keyscan_config = {
+	.output_settle_us = 80,
+	.debounce_down_us = 9 * MSEC,
+	.debounce_up_us = 30 * MSEC,
+	.scan_period_us = 3 * MSEC,
+	.min_post_scan_delay_us = 1000,
+	.poll_timeout_us = 100 * MSEC,
+	.actual_key_mask = {
+		0x14, 0xff, 0xff, 0xff, 0xff, 0xf5, 0xff,
+		0xa4, 0xff, 0xfe, 0x55, 0xfa, 0xca  /* full set */
+	},
+};
+
 #ifdef CONFIG_KEYBOARD_FACTORY_TEST
 /*
  * We have total 21 pins for keyboard connecter, {-1, -1} mean
