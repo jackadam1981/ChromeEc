c48585735bf3a7a995b299558cf97e572461e2b5
Gwendal Grignou <gwendal@chromium.org>
2016-12-02 15:23:04 -0800

    reef: Add BMI160 as a temperature sensor.
    Enable reading temperature from BMI160.
    
    BUG=chromium:58894
    BRANCH=reef
    TEST=check with EC commands the temperature is returned.
    
    Change-Id: I892b5c72e09dee905a813724ea1882b2293bbcaa
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- board/reef/board.c
+++ board/reef/board.c
@@ -445,6 +445,9 @@ const struct temp_sensor_t temp_sensors[] = {
 	{"Battery", TEMP_SENSOR_TYPE_BATTERY, charge_get_battery_temp, 0, 1},
 	{"Ambient", TEMP_SENSOR_TYPE_BOARD, board_get_ambient_temp, 0, 5},
 	{"Charger", TEMP_SENSOR_TYPE_BOARD, board_get_charger_temp, 1, 1},
+#ifdef CONFIG_ACCELGYRO_BMI160_TEMP
+	{"Gyro", TEMP_SENSOR_TYPE_BOARD, bmi_get_sensor_temp, BASE_GYRO, 1}
+#endif
 };
 BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 
