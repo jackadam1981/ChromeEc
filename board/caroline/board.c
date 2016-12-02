ad541fe53833aeb5c5a568a5d7822820814b75f4
Vincent Palatin <vpalatin@chromium.org>
2016-12-02 14:34:30 +0100

    caroline: delay disabling V0.85A on PMIC
    In order to guarantee the V1.00A rail has 100ms to discharge, we delay
    the disabling of V0.85A by 50ms, instead of letting go on
    GPIO_PMIC_SLP_SUS_L transitions as other PMIC rails.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=glados
    BUG=chrome-os-partner:60501
    TEST=hardware measurement
    
    Change-Id: I5047c765ab63aabe8c8ee6a5acdbdabb05192af1

--- board/caroline/board.c
+++ board/caroline/board.c
@@ -214,6 +214,8 @@ const struct button_config buttons[CONFIG_BUTTON_COUNT] = {
 
 static void board_pmic_init(void)
 {
+	/* DISCHGCNT3 - enable 100 ohm discharge on V1.00A */
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x3e, 0x04);
 	/*
 	* The older ec firmware set wrong value, so we need to re-write
 	* after system jump.
@@ -633,3 +635,34 @@ void lid_angle_peripheral_enable(int enable)
 	}
 }
 #endif
+
+/*
+ * Various voltage rails will be enabled / disabled by the PMIC when
+ * GPIO_PMIC_SLP_SUS_L changes. We need to delay the disable of V0.85A
+ * by approximately 50ms in order to allow V1.00A to sufficiently discharge
+ * first.
+ *
+ * Therefore, after GPIO_PMIC_SLP_SUS_L goes high, ignore the state of
+ * the V12_EN pin: Keep V0.85A enabled.
+ *
+ * When GPIO_PMIC_SLP_SUS_L goes low, delay 50ms, and make V12_EN function
+ * as normal - this should result in V0.85A discharging immediately after the
+ * i2c write completes.
+ */
+void chipset_set_pmic_slp_sus_l(int level)
+{
+	static int previous_level;
+	int val;
+
+	gpio_set_level(GPIO_PMIC_SLP_SUS_L, level);
+
+	if (previous_level != level) {
+		/* Rising edge: Force V0.85A enable. Falling: Pin control. */
+		val = level ? 0x80 : 0;
+		if (!level)
+			msleep(50);
+
+		i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x43, val);
+		previous_level = level;
+	}
+}
