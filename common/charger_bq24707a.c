932d9a14c08fe03957f83111b935fc19a0651b7b
David.Huang <David.Huang@quantatw.com>
2016-12-07 09:35:01 +0800

    Peppy: Change charge voltage for battery
    Change battery charge voltage for peppy battery.
    
    BRANCH=peppy
    BUG=chrome-os-partner:60681
    TEST=Use console command "charger" to check charge voltage.
    
    Change-Id: I5b4b6ad3fdedd95762899ac77d9da95fd420481f
    Signed-off-by: David Huang <David.Huang@quantatw.com>

--- common/charger_bq24707a.c
+++ common/charger_bq24707a.c
@@ -168,6 +168,10 @@ int charger_get_voltage(int *voltage)
 
 int charger_set_voltage(int voltage)
 {
+#ifdef BOARD_peppy
+	if (voltage != 0)
+		voltage = 12832;
+#endif
 	return sbc_write(SB_CHARGING_VOLTAGE, voltage);
 }
 
