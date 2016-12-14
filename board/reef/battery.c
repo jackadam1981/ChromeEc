756b842ba069d25d010fbfd115eba710c407c8d1
Ryan Zhang <ryan.zhang.quanta@gmail.com>
2016-12-14 22:04:49 +0800

    Electro: Fix power lost when release shipping mode
    1. When battery need some time to provide power when
       release shipping mode.
    
    2. Enter learn mode will also cut AC and cause shutdown
    
    3. return BP_NOT_SURE so that we can try to wake battery
       when battery is dead
    
    BUG=chrome-os-partner:60921, 59904
    BRANCH=master
    TEST=remove battery
    
    Change-Id: I33ca4df54e0b02e68ade9426864561dae8c57851
    Signed-off-by: Ryan Zhang <Ryan.Zhang@quantatw.com>

--- board/reef/battery.c
+++ board/reef/battery.c
@@ -35,6 +35,7 @@ struct board_batt_params {
 };
 
 #define DEFAULT_BATTERY_TYPE BATTERY_SONY_CORP
+#define SONY_DISCHARGE_FET_BIT (0x01 << 13)
 
 static enum battery_present batt_pres_prev = BP_NOT_SURE;
 
@@ -50,8 +51,10 @@ static int batt_smp_cos4870_init(void)
 
 static int batt_sony_corp_init(void)
 {
-	/* TODO: crosbug.com/p/59904 */
-	return 1;
+	int batt_status;
+
+	return sb_read(SB_MANUFACTURER_ACCESS, &batt_status) ? 0 :
+		!(batt_status & SONY_DISCHARGE_FET_BIT);
 }
 
 static const struct board_batt_params info[] = {
@@ -345,7 +348,8 @@ int charger_profile_override(struct charge_state_data *curr)
 			!battery_is_cut_off() &&
 			!(curr->batt.flags & BATT_FLAG_WANT_CHARGE) &&
 			curr->batt.status & STATUS_FULLY_CHARGED) ||
-			(!chg_ramp_is_detected() &&
+			(curr->batt.is_present == BP_YES &&
+			!chg_ramp_is_detected() &&
 			curr->batt.state_of_charge > 2);
 
 	charger_discharge_on_ac(disch_on_ac);
@@ -517,6 +521,8 @@ enum battery_present battery_is_present(void)
 			if (bd9995x_get_battery_voltage() >=
 			    board_get_batt_params()->batt_info.voltage_min)
 				batt_pres = BP_NO;
+			else
+				batt_pres = BP_NOT_SURE;
 		} else if (!board_get_batt_params()->batt_init())
 			batt_pres = BP_NO;
 	}
