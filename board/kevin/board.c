6e7c1ebd414865b1033a251ca0717d0388891290
Aseda Aboagye <aaboagye@google.com>
2016-12-15 18:52:25 -0800

    kevin: Set EC rate for lid accel to 100ms.
    BUG=chrome-os-partner:59480
    BRANCH=gru
    TEST=flash kevin; boot, verify lid angle is measured.
    
    Change-Id: I5279f2070f6693eb0964e5e1923f88871c8d640c
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/kevin/board.c
+++ board/kevin/board.c
@@ -630,7 +630,7 @@ struct motion_sensor_t motion_sensors[] = {
 		/* EC use accel for angle detection */
 		[SENSOR_CONFIG_EC_S0] = {
 			.odr = 10000 | ROUND_UP_FLAG,
-			.ec_rate = 0,
+			.ec_rate = 100 * MSEC,
 		},
 		 /* EC use accel for angle detection */
 		[SENSOR_CONFIG_EC_S3] = {
