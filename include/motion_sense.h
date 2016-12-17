4758b48a37d808e4147861d26c06c32c58986a24
Aseda Aboagye <aaboagye@google.com>
2016-12-16 18:24:08 -0800

    motion_sense: Add "spoof" mode
    This commit adds a "spoof" mode feature to the motionsense stack.  It
    allows the user to arbitrarily set the outputs of the sensor in order to
    "spoof" the readings of the sensor.  This can be useful in emulating
    tablet mode or device rotations.  A command is available from the EC
    console named `accelspoof` and there is a corresponding motionsense
    command in ectool called `spoof`.
    
    The usage is as follows:
    
    - EC console
     > accelspoof [id] [on/off] [X Y Z]
    
    - ectool
     # ectool motionsense spoof -- [id] [0/1] [X Y Z]
    
    If on or off(or 0/1) is not specified, the current spoof mode status of
    the sensor is returned.  If on is specified, but no components are
    provided, the sensor will lock the current values and provide those as
    the spoofed values.  If the components are provided, those will be used
    as the spoofed values.
    
    BUG=chromium:675263
    BRANCH=???
    TEST=Flash a DUT with accels.  From AP console, run ectool motionsense
    lid_angle in a loop, use 'accelspoof' to set spoofed values.  Verify
    that the angle is fixed regardless of the actual angle of the DUT.
    
    TEST=Flash a DUT with accels.  From AP console, use ectool motionsense
    spoof to spoof values and verify that `ectool motionsense` reflects the
    spoofed values.  Test with both provided component values and no
    component values.
    
    Change-Id: Ie30688d22f38054e7243b1af493a3092b2cdfb72
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- include/motion_sense.h
+++ include/motion_sense.h
@@ -109,6 +109,13 @@ struct motion_sensor_t {
 	vector_3_t raw_xyz;
 	vector_3_t xyz;
 
+	/*
+	 * Spoof mode will allow the EC to report arbitrary values for any of
+	 * the components.
+	 */
+	int in_spoof_mode;
+	vector_3_t spoof_xyz;
+
 	/* How many flush events are pending */
 	uint32_t flush_pending;
 
