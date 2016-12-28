ec68261e7c17270487c9c532db0cc94b492dcb6a
Gwendal Grignou <gwendal@chromium.org>
2016-12-28 10:59:19 -0800

    motion: Disable tablet mode if one accel is broken
    We need 2 accelerometer for tablet mode. If one of them is not working,
    disable tablet mode. We will stay in clamshell mode, lid angle will be
    always unreliable.
    
    BUG=chrome-os-partner:61141
    TEST=On older reef where the matrix does not match accelerometer
    orientation anymore. If both sensors working, wrong lid angle.
    After disabling the lid one, still in tablet mode.
    After this change, boot in clamshell mode.
    BRANCH=kevin,reef
    
    Change-Id: I7bf6cdc9d85370fce20e5183622b4bc18f4f5f99
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- common/motion_sense.c
+++ common/motion_sense.c
@@ -436,6 +436,17 @@ static void motion_sense_switch_sensor_rate(void)
 				if (ret != EC_SUCCESS) {
 					CPRINTS("%s: %d: init failed: %d",
 						sensor->name, i, ret);
+#ifdef CONFIG_TABLET_MODE
+					/*
+					 * No tablet mode allowed if an accel
+					 * is not working.
+					 */
+					if (i == CONFIG_LID_ANGLE_SENSOR_BASE ||
+					    i == CONFIG_LID_ANGLE_SENSOR_LID) {
+						tablet_set_mode(0);
+						hook_notify(HOOK_TABLET_MODE_CHANGE);
+					}
+#endif
 				}
 			}
 		} else {
