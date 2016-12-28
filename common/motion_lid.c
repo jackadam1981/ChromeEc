f67f42d15e907c419d6a371ff5c6eab4934b6fe9
Gwendal Grignou <gwendal@chromium.org>
2016-12-28 10:59:19 -0800

    motion: Disable tablet mode if one accel is broken
    We need 2 accelerometer for tablet mode. If one of them is not working,
    disable tablet mode. We will stay in clamshell mode, lid angle will be
    always unreliable.
    
    BUG=chrome-os-partner:61141
    TEST=On kevin with a single sensor. Check we are in clamshell mode
    when rebooting the EC.
    BRANCH=kevin
    
    Change-Id: Iccc0e0274520437506a7df3c1871ad6aa82f64f1
    ORIG-Change-Id: I7bf6cdc9d85370fce20e5183622b4bc18f4f5f99
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/424184
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- common/motion_lid.c
+++ common/motion_lid.c
@@ -354,7 +354,6 @@ static int calculate_lid_angle(const vector_3_t base, const vector_3_t lid,
 				tablet_set_mode(new_tablet_mode);
 				CPRINTS("TM %d %d deg", tablet_get_mode(),
 					*lid_angle);
-				hook_notify(HOOK_TABLET_MODE_CHANGE);
 				return reliable;
 			}
 			tablet_mode_debounce_cnt--;
