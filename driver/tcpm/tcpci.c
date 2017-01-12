aa4c6f945fba00738f80de206b3ded5aff6db3e1
Nicolas Boichat <drinkcat@google.com>
2017-01-12 11:06:15 +0800

    HACK: poppy: workaround for old parade TCPC firmware
    Change-Id: Ic0c7823eb9bf74cc07c24a10b1b41103b59b8218

--- driver/tcpm/tcpci.c
+++ driver/tcpm/tcpci.c
@@ -5,6 +5,7 @@
 
 /* Type-C port manager */
 
+#include "console.h"
 #include "task.h"
 #include "tcpci.h"
 #include "tcpm.h"
@@ -14,6 +15,8 @@
 #include "usb_pd.h"
 #include "usb_pd_tcpc.h"
 #include "util.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
 
 static int tcpc_vbus[CONFIG_USB_PD_PORT_COUNT];
 
@@ -198,6 +201,10 @@ int tcpci_tcpm_get_message(int port, uint32_t *payload, int *head)
 	int rv, cnt, reg = TCPC_REG_RX_DATA;
 
 	rv = tcpc_read(port, TCPC_REG_RX_BYTE_CNT, &cnt);
+
+	CPRINTS("get message cnt=%d", cnt);
+	/* HACK: parade fixup */
+	cnt++;
 
 	/* RX_BYTE_CNT includes 3 bytes for frame type and header */
 	if (rv != EC_SUCCESS || cnt < 3) {
