94ec1e1dc5c0b382ef14a335874bf0f3ecb9b5fe
Shelley Chen <shchen@chromium.org>
2017-01-12 13:23:17 -0800

    poppy: host command for configuring power button
    Specifically, we are using a bit to disable the
    SMI pulse on x86 systems so that we can use the
    power button for menu selection.
    
    BUG=chrome-os-partner:61275
    BRANCH=None
    TEST=Try running with depthcharge sending the host command
         during detachable FW menus and making sure power
         button select doesn't turn off device on reef.
    
    Change-Id: I4a68cf514d514a4abe98beb99e7934d6fb0f44bd
    Signed-off-by: Shelley Chen <shchen@chromium.org>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -2254,6 +2254,25 @@ struct __ec_align1 ec_params_force_lid_open {
 };
 
 /*****************************************************************************/
+/* Configure the behavior of the power button */
+#define EC_CMD_CONFIG_POWER_BUTTON 0x002D
+
+enum configure_power_button_cmds {
+	/* Enable/Disable SMI pulses for x86 devices */
+	ENABLE_SMI_PULSE = 0
+};
+
+struct __ec_align1 ec_params_config_powerbtn {
+	/*
+	 * bit 0 == 1, SMI pulse on
+	 *       == 0, SMI pulse disabled
+	 *
+	 * other bits can be used for future config options
+	 */
+	uint8_t flags;
+};
+
+/*****************************************************************************/
 /* USB charging control commands */
 
 /* Set USB port charging mode */
