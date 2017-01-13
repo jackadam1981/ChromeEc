2fee939ff175a99a4f7cb5f0a3b0eff29a887c0f
Gwendal Grignou <gwendal@chromium.org>
2017-01-13 14:14:35 -0800

    snappy: Remove barometer
    Snappy does not have a barometer.
    
    BUG=none
    BRANCH=reef
    TEST=Check with i2cscan there is no barometer.
    Check Snappy MB and Sensor board that no barometer are defined.
    Check messages "[1.787760 Base Baro: 4: init failed: 1]" disappers once
    BMP280 definition is removed.
    
    Change-Id: I4ce06551cb9e108570717bc9b15038f32d0911e6
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- board/snappy/board.h
+++ board/snappy/board.h
@@ -174,7 +174,6 @@
 #define I2C_PORT_GYRO			NPCX_I2C_PORT1
 #define I2C_PORT_LID_ACCEL		NPCX_I2C_PORT2
 #define I2C_PORT_ALS			NPCX_I2C_PORT2
-#define I2C_PORT_BARO			NPCX_I2C_PORT2
 #define I2C_PORT_BATTERY		NPCX_I2C_PORT3
 #define I2C_PORT_CHARGER		NPCX_I2C_PORT3
 /* Accelerometer and Gyroscope are the same device. */
@@ -192,7 +191,6 @@
 #define CONFIG_ACCEL_KX022
 #define CONFIG_ALS_OPT3001
 #define OPT3001_I2C_ADDR OPT3001_I2C_ADDR1
-#define CONFIG_BARO_BMP280
 #define CONFIG_LID_ANGLE
 #define CONFIG_LID_ANGLE_UPDATE
 #define CONFIG_LID_ANGLE_SENSOR_BASE BASE_ACCEL
@@ -263,7 +261,6 @@ enum sensor_id {
 	BASE_ACCEL,
 	BASE_GYRO,
 	BASE_MAG,
-	BASE_BARO,
 };
 
 enum snappy_board_version {
@@ -314,8 +311,7 @@ void board_set_tcpc_power_mode(int port, int mode);
 void board_print_tcpc_fw_version(int port);
 
 /* Sensors without hardware FIFO are in forced mode */
-#define CONFIG_ACCEL_FORCE_MODE_MASK \
-	((1 << LID_ACCEL) | (1 << BASE_BARO))
+#define CONFIG_ACCEL_FORCE_MODE_MASK (1 << LID_ACCEL)
 
 #endif /* !__ASSEMBLER__ */
 
