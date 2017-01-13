da5d38e73c4a356de51c2a8789a2a17d68799f89
Scott <scollyer@chromium.org>
2017-01-12 18:38:39 -0800

    servo_v4: pd: Added Device Test System support
    - Modified src attach state to enable vbus when debug accessory is
    detected.
    
    - servo_v4 has two pd ports, but each port requires a different default
    power role. Port 0 can only ever be a SNK, but port 1 which acts is
    intended to be a DTS port should default to a SRC so it can be be a
    source debug accessory. It may also act as a sink debug accessory, but
    is not intended to toggle automatically but will swap roles if
    necessary via pd role swap messaging.
    
    - Add hook for ccd enable/disable for DTS mode
    
    BUG=chrome-os-partner:61878
    BRANCH=None
    TEST=Manual Verfied that can still build servo_v4 project. All changes
    in this CL are contingent on config option CONFIG_USB_PD_DTS being enabled.
    
    Change-Id: Iab968b6fbdfc8f2d155c4f8618921b32f313b9ec
    Signed-off-by: Scott <scollyer@chromium.org>

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -765,6 +765,7 @@ void pd_set_dual_role(enum pd_dual_role_states state);
  * @param port Port number from which to get role
  */
 int pd_get_role(int port);
+
 #endif
 
 /* Control Message type */
@@ -812,10 +813,12 @@ enum pd_data_msg_type {
 #define PD_ROLE_VCONN_ON  1
 
 /* Port role at startup */
+#ifndef PD_ROLE_DEFAULT
 #ifdef CONFIG_USB_PD_DUAL_ROLE
-#define PD_ROLE_DEFAULT PD_ROLE_SINK
+#define PD_ROLE_DEFAULT(port) PD_ROLE_SINK
 #else
-#define PD_ROLE_DEFAULT PD_ROLE_SOURCE
+#define PD_ROLE_DEFAULT(port) PD_ROLE_SOURCE
+#endif
 #endif
 
 /* build message header */
