42a1401dc55aeab74985332e7315835527551acd
Mulin Chao <mlchao@nuvoton.com>
2017-01-13 10:14:05 +0800

    WIP: workaround for anx74xx without CONFIG_USB_PD_VBUS_DETECT_TCPC
    We don't have a alert event to indicate power status through
    TCPC on poppy since anx74xx doesn't support it. Using
    anx74xx_tcpm_get_vbus_level and tcpci_tcpm_get_vbus_level
    directly for detecting VBus without TCPC.
    
    BRANCH=none
    BUG=none
    TEST=make BOARD=poppy; test pd charging for anx74xx and ps8751.
    
    Change-Id: I74b5189900750d0ddece0885e7e3c67dd99a6350
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

--- driver/tcpm/anx74xx.h
+++ driver/tcpm/anx74xx.h
@@ -178,6 +178,7 @@ void anx74xx_tcpc_set_vbus(int port, int enable);
 void anx74xx_tcpc_update_hpd_status(int port, int hpd_lvl, int hpd_irq);
 void anx74xx_tcpc_clear_hpd_status(int port);
 int anx74xx_tcpc_get_fw_version(int port, int *version);
+int anx74xx_tcpm_get_vbus_level(int port);
 
 #ifdef CONFIG_CMD_I2C_STRESS_TEST_TCPC
 extern struct i2c_stress_test_dev anx74xx_i2c_stress_test_dev;
