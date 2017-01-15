ed8afaa608fc15f028525641a2224b3f753a20a6
Vadim Bendebury <vbendeb@chromium.org>
2017-01-15 13:35:39 -0800

    g: add a common block cipher function
    The common block cipher function accepts a 4 byte salt value to seed
    the IV and the blob to encrypt/decrypt. The key is derived from the
    hardware based on the type of the RW image signature (dev vs prod).
    
    A console command is added to exercise the cipher function.
    
    Since stack requirements of the dcrypto code exceed the console task
    allowance, the actual command is executed on the HOOKs task context.
    
    BRANCH=none
    BUG=chrome-os-partner:55331
    TEST=make buildall -j passes. Running the cipher command from the
         console succeeds:
    
      > cipher
      original data           ad67d44cb4feffff6b3b334635eb9612
      rv 0x01, out data       861dc395a2fc745ca886a703cb02a897, time 16636 us
      rv 0x01, orig. data     ad67d44cb4feffff6b3b334635eb9612, time 17004 us
      sha1 before and after match!
      >
    
    Change-Id: I7686d8c8489c1b8a984859c3be4f82c338573c6f
    Signed-off-by: Marius Schilder <mschilder@chromium.org>
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- chip/g/dcrypto/dcrypto.h
+++ chip/g/dcrypto/dcrypto.h
@@ -222,4 +222,26 @@ int DCRYPTO_appkey_init(enum dcrypto_appid id, struct APPKEY_CTX *ctx);
 int DCRYPTO_appkey_fingerprint(struct APPKEY_CTX *ctx);
 void DCRYPTO_appkey_finish(struct APPKEY_CTX *ctx);
 
+/**
+ * Encrypt/decrypt a flat blob.
+ *
+ * Encrypt or decrypt the input buffer, and write the correspondingly
+ * ciphered output to out.  The number of bytes produced is equal to
+ * the number of input bytes.
+ *
+ * This API is expected to be applied to a single contiguous region. WARNING:
+ * Presently calling this function more than once with "in" pointing to
+ * logically different buffers will result in using the same IV value
+ * internally and as such reduce encryption efficiency. Upcoming changes are
+ * expected to make proper use of blob_iv.
+ *
+ * @param salt pointer to a unique 4 byte value to be associated with this
+ *        blob, used for derivation of the proper IV.
+ * @param out Destination pointer where to write plaintext / ciphertext.
+ * @param in  Source pointer where to read ciphertext / plaintext.
+ * @param len Number of bytes to read from in / write to out.
+ * @return non-zero on success, and zero otherwise.
+ */
+int DCRYPTO_app_cipher(const void *salt, void *out, const void *in, size_t len);
+
 #endif  /* ! __EC_CHIP_G_DCRYPTO_DCRYPTO_H */
