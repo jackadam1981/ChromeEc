3e438e7ad1954d0d6ca60ee4eb7538415741d1dc
Peggy Chuang <PeggyChuang@ami.com.tw>
2017-01-18 14:47:29 +0800

    Wizpig: Adapt to new sensor stack
    As the sesnsor stack is update. But the sensor stack has been
    removed for wizpigw which make the accel not work. So adapt to new
     sensor stack.
    
    BUG=None
    TEST=make buildall -j
    BRANCH=firmware-strago-7287.B
    Signed-off-by: Peggy Chuang <peggychuang@ami.com.tw>
    
    Change-Id: I7c731fbdaceb5a95feff556c8ee74fa80b5a9654

--- board/wizpig/board.h
+++ board/wizpig/board.h
@@ -68,6 +68,12 @@
 
 #define CONFIG_I2C
 
+/* Accelerometer */
+#define CONFIG_ACCEL_KXCJ9
+#define CONFIG_CMD_ACCELS
+#define CONFIG_CMD_ACCEL_INFO
+#define CONFIG_LID_ANGLE
+
 /* Wireless signals */
 #define WIRELESS_GPIO_WLAN	GPIO_WLAN_OFF_L
 
@@ -91,6 +97,9 @@
 #define I2C_PORT_CHARGER	MEC1322_I2C0_0
 #define I2C_PORT_PD_MCU		MEC1322_I2C1
 #define I2C_PORT_TCPC		MEC1322_I2C1
+#define I2C_PORT_ACCEL		MEC1322_I2C2
+#define I2C_PORT_GYRO		MEC1322_I2C2
+
 #define I2C_PORT_THERMAL	MEC1322_I2C3
 
 /* ADC signal */
@@ -98,6 +107,22 @@ enum adc_channel {
 	/* Number of ADC channels */
 	ADC_CH_COUNT
 };
+
+/* Sensor index definition */
+enum sensor_id {
+	BASE_ACCEL = 0,
+	LID_ACCEL = 1,
+};
+
+/*
+ * We have not enabled the sensor FIFO on the accels, so we force the EC
+ * to collect at every sample.
+ */
+#define CONFIG_ACCEL_FORCE_MODE_MASK \
+	((1 << BASE_ACCEL) | (1 << LID_ACCEL))
+
+#define CONFIG_LID_ANGLE_SENSOR_BASE BASE_ACCEL
+#define CONFIG_LID_ANGLE_SENSOR_LID LID_ACCEL
 
 /* power signal definitions */
 enum power_signal {
