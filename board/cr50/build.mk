a175e9ddb0f1154afc03cb940f9a3265d75b51ae
Vincent Palatin <vpalatin@chromium.org>
2017-01-19 15:22:15 +0100

    cr50: add U2F support
    Implement U2F (universal second factor authentication) feature
    over TPM vendor commands.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=cr50
    BUG=b:35545754
    TEST=pass U2FTest and HIDTest.
    
    Change-Id: Ic2591f369763fb4ba67926e2b4a0c2cd35330a18

--- board/cr50/build.mk
+++ board/cr50/build.mk
@@ -52,6 +52,7 @@ board-y += tpm2/trng.o
 board-y += tpm2/upgrade.o
 board-y += tpm_nvmem_read.o
 board-y += wp.o
+board-$(CONFIG_U2F) += u2f.o
 
 # Build and link with an external library
 EXTLIB := $(realpath ../../third_party/tpm2)
