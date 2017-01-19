dd6656f4e81c2a8c8b071e625e98ca87e310d2d9
Dino Li <Dino.Li@ite.com.tw>
2017-01-19 16:36:49 +0800

    it83xx: add espi module
    We use CL:392587 to verify eSPI module.
    
    Signed-off-by: Dino Li <dino.li@ite.com.tw>
    
    BRANCH=none
    BUG=none
    TEST=1. it8390+Intel SKL-Y RVP3 and boot to shell.
         2. console command "kbpress 1 4" to test keyboard data.
    
    Change-Id: I1b32bd16f7e01abf07b9c9a68ebef2399cc9828d

--- chip/it83xx/intc.h
+++ chip/it83xx/intc.h
@@ -24,5 +24,8 @@ int gpio_clear_pending_interrupt(enum gpio_signal signal);
 void clock_sleep_mode_wakeup_isr(void);
 int clock_ec_wake_from_sleep(void);
 void __enter_hibernate(uint32_t seconds, uint32_t microseconds);
+void espi_interrupt(void);
+void espi_vw_interrupt(void);
+void espi_init(void);
 
 #endif /* __CROS_EC_INTC_H */
