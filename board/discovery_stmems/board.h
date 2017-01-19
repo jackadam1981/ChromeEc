b0f6f08d912c9e19fcee3e3c5cf6de4769d8c74d
Mario Tesi <mario.tesi@st.com>
2017-01-19 11:43:39 +0100

    board: discovery_stmems: Add support to LIS2DH12/LSM6DSM/LSM6DSL
    Add basics configuration to discovery_stmems target board for
    LIS2DH/LIS2DH12(acc) and LSM6DSM/LSM6DSL (acc + gyro)
    device driver. Added motionsensor_chip related chip defines.
    
    Basics configuration includes:
     - I2C master interface I2C2 pins PB10/11
     - Debug console on USART1 pins PA09/PA10
     - Motion Sense Task (with larger stack)
    
    LSM6DSM
     - Polling Mode
     - ODR/FS Range setting
    
    LIS2DH12
     - Interrupt routine on pin PA0 (LIS2DH int 1)
     - Acc. FIFO configuration
     - Acc. Whatermark on FIFO
     - Acc. Interrupt management on FIFO w. threshold
     - Polling Mode
     - ODR/FS Range setting
    
    BUG=b:73546254
    BRANCH=master
    TEST=Tested on discovery_stmems target board with sensor connected on I2C
    master interface.
    Added motion sense task to board task list, added gpio info in board
    configuration file and tested with motion sense console commands.
    Acc. sensor is configured as LID_ACCEL (index 0) in motion_sensors array
    I2C address is the first available LIS2DH_ADDR0
    (can be changed in LIS2DH_ADDR1 by pulling-up SDO sensor pin).
    LIS2DH/LIS2DH12 sensor data seems ok:
    can successfully change ODR (1...400 Hz) and full scale range (1...16 G)
    FIFO and interrupt tested.
    LSM6DSM acc + gyro in polling mode can successfully change ODR
    (0...416 Hz) and full scale range for acc (2...16 G)
    and gyro (245...2000 DPS).
    
    Change-Id: I985cf2687e0465a4557551231a6bd24235f3ae4c
    Signed-off-by: Mario Tesi <mario.tesi@st.com>
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

---
+++ board/discovery_stmems/board.h
@@ -0,0 +1,73 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* STM32L-discovery board configuration. */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/*
+ * Enable Sensors:
+ * Select LSM6DSM, LIS2DH or both
+ */
+#define CONFIG_ACCELGYRO_LSM6DSM
+#define CONFIG_ACCEL_LIS2DH
+
+/* Interrupt management. */
+#define CONFIG_ACCEL_INTERRUPTS
+
+/* Custom sensor option. */
+#define CONFIG_ACCEL_LIS2DH_INT_EVENT TASK_EVENT_CUSTOM(4)
+
+/* Optional features. */
+#undef CONFIG_LID_SWITCH
+#define CONFIG_I2C
+#define CONFIG_I2C_MASTER
+#define CONFIG_CMD_ACCELS
+#define CONFIG_CMD_ACCEL_INFO
+
+/* FIFO Support. */
+#define CONFIG_ACCEL_FIFO 32
+#define CONFIG_ACCEL_FIFO_THRES (CONFIG_ACCEL_FIFO / 2)
+
+/* I2C master port */
+#define I2C_PORT_MASTER STM32_I2C2_PORT
+
+/*
+ * Allow dangerous commands all the time, since we don't have a write protect
+ * switch.
+ */
+#define CONFIG_SYSTEM_UNLOCKED
+
+#ifndef __ASSEMBLER__
+
+/* Timer selection */
+#define TIM_CLOCK_MSB 3
+#define TIM_CLOCK_LSB 4
+#undef  CONFIG_WATCHDOG_HELP
+
+
+/* Motion sensors. */
+enum sensor_id {
+#ifdef CONFIG_ACCEL_LIS2DH
+	LID_ACCEL,
+#endif /* CONFIG_ACCEL_LIS2DH */
+
+#ifdef CONFIG_ACCELGYRO_LSM6DSM
+	BASE_ACCEL,
+	BASE_GYRO,
+#endif /* CONFIG_ACCELGYRO_LSM6DSM */
+};
+
+
+/* Accelerometer and Gyroscope are the same device. */
+#define I2C_PORT_GYRO			I2C_PORT_MASTER
+#define I2C_PORT_ACCEL			I2C_PORT_MASTER
+
+#include "gpio_signal.h"
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
