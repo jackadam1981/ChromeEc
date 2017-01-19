f7e112fb5b205178e35cb331aefc1effc0682194
Mario Tesi <mario.tesi@st.com>
2017-01-19 11:43:39 +0100

    board: discovery_stmems: Add support to LIS2DH12/LSM6DSM/LSM6DSL
    Add basics configuration to discovery_stmems target board for
    LIS2DH/LIS2DH12(acc) and LSM6DSM/LSM6DSL (acc + gyro)
    device driver. Added motionsensor_chip related chip defines.
    
    Basics configuration includes:
     - I2C master interface I2C2 pins PB10/11
     - Debug console on USART1 pins PA09/PA10
     - Motion Sense Task (with larger stack)
    
    LSM6DSM
     - Polling Mode
     - ODR/FS Range setting
    
    LIS2DH12
     - Interrupt routine on pin PA0 (LIS2DH int 1)
     - Acc. FIFO configuration
     - Acc. Whatermark on FIFO
     - Acc. Interrupt management on FIFO w. threshold
     - Polling Mode
     - ODR/FS Range setting
    
    BUG=none
    BRANCH=master
    TEST=Tested on discovery_stmems target board with sensor connected on I2C
    master interface.
    Added motion sense task to board task list, added gpio info in board
    configuration file and tested with motion sense console commands.
    Acc. sensor is configured as LID_ACCEL (index 0) in motion_sensors array
    I2C address is the first available LIS2DH_ADDR0
    (can be changed in LIS2DH_ADDR1 by pulling-up SDO sensor pin).
    LIS2DH/LIS2DH12 sensor data seems ok:
    can successfully change ODR (1...400 Hz) and full scale range (1...16 G)
    FIFO and interrupt tested.
    LSM6DSM acc + gyro in polling mode can successfully change ODR
    (0...416 Hz) and full scale range for acc (2...16 G)
    and gyro (245...2000 DPS).
    
    Change-Id: I985cf2687e0465a4557551231a6bd24235f3ae4c
    Signed-off-by: Mario Tesi <mario.tesi@st.com>
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

---
+++ board/discovery_stmems/board.c
@@ -0,0 +1,227 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* STM32L-discovery board configuration */
+
+#include "common.h"
+#include "console.h"
+#include "driver/accelgyro_lsm6dsm.h"
+#include "driver/accel_lis2dh.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "math_util.h"
+#include "motion_sense.h"
+#include "queue_policies.h"
+#include "registers.h"
+#include "task.h"
+#include "usart-stm32f0.h"
+#include "usart_rx_dma.h"
+#include "usart_tx_dma.h"
+#include "util.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+
+/*
+ * Interrupt handler for external mems int1/2
+ *
+ * Dispatch interrupt (FIFO, gesture recognition, data ready and so on)
+ * to service routine and toggle green led on discovery board
+ */
+void sensors_interrupt(enum gpio_signal signal)
+{
+
+#ifdef CONFIG_ACCEL_INTERRUPTS
+	static int count;
+
+#ifdef CONFIG_ACCEL_LIS2DH
+	lis2dh_interrupt(signal);
+#endif /* CONFIG_ACCEL_LIS2DH */
+
+	gpio_set_level(GPIO_LED_GREEN, ++count & 0x01);
+#endif /* CONFIG_ACCEL_INTERRUPTS */
+}
+
+#include "gpio_list.h"
+
+
+/* Setup I2C port where sensors are attached */
+const struct i2c_port_t i2c_ports[]  = {
+	{"sensor", I2C_PORT_MASTER, 100, GPIO_I2C_SCL, GPIO_I2C_SDA},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+/* Base Sensor mutex */
+static struct mutex g_base_mutex;
+
+/*
+ * Motion Sense
+ * Matrix to rotate accelrator into standard reference frame
+ */
+const matrix_3x3_t base_standard_ref = {
+	{ 0, FLOAT_TO_FP(-1), 0 },
+	{ FLOAT_TO_FP(1), 0,  0 },
+	{ 0, 0,  FLOAT_TO_FP(1) }
+};
+
+#ifdef CONFIG_ACCELGYRO_LSM6DSM
+struct stprivate_data lsm6dsm_a_data;
+struct stprivate_data lsm6dsm_g_data;
+#endif /* CONFIG_ACCELGYRO_LSM6DSM */
+
+#ifdef CONFIG_ACCEL_LIS2DH
+struct stprivate_data lis2dh_a_data;
+struct stprivate_data lis2dh_l_data;
+#endif /* CONFIG_ACCEL_LIS2DH */
+
+struct motion_sensor_t motion_sensors[] = {
+#ifdef CONFIG_ACCELGYRO_LSM6DSM
+
+	[BASE_ACCEL] = {
+		.name = "LSM6DSM ACC",
+		.active_mask = SENSOR_ACTIVE_S0,
+		.chip = MOTIONSENSE_CHIP_LSM6DSM,
+		.type = MOTIONSENSE_TYPE_ACCEL,
+		.location = MOTIONSENSE_LOC_BASE,
+		.drv = &lsm6dsm_drv,
+		.mutex = &g_base_mutex,
+		.drv_data = &lsm6dsm_a_data,
+		.port = I2C_PORT_ACCEL,
+		.addr = LSM6DSM_ADDR0,
+		.rot_standard_ref = &base_standard_ref,
+		.default_range = 2, /* g, enough for laptop. */
+		.min_frequency = LSM6DSM_ODR_MIN_VAL,
+		.max_frequency = LSM6DSM_ODR_MAX_VAL,
+		.config = {
+			/* AP: by default use EC settings */
+			[SENSOR_CONFIG_AP] = {
+				.odr = 0,
+				.ec_rate = 0,
+			},
+			/* EC use accel for angle detection */
+			[SENSOR_CONFIG_EC_S0] = {
+				.odr = 13000,
+				.ec_rate = 13 * MSEC,
+			},
+			/* Sensor off in S5 */
+			[SENSOR_CONFIG_EC_S3] = {
+				.odr = 0,
+				.ec_rate = 0
+			},
+			/* Sensor off in S5 */
+			[SENSOR_CONFIG_EC_S5] = {
+				.odr = 0,
+				.ec_rate = 0
+			},
+		},
+	},
+
+	[BASE_GYRO] = {
+		.name = "LSM6DSM GYRO",
+		.active_mask = SENSOR_ACTIVE_S0,
+		.chip = MOTIONSENSE_CHIP_LSM6DSM,
+		.type = MOTIONSENSE_TYPE_GYRO,
+		.location = MOTIONSENSE_LOC_BASE,
+		.drv = &lsm6dsm_drv,
+		.mutex = &g_base_mutex,
+		.drv_data = &lsm6dsm_g_data,
+		.port = I2C_PORT_GYRO,
+		.addr = LSM6DSM_ADDR0,
+		.rot_standard_ref = &base_standard_ref,
+		.default_range = 245, /* dps */
+		.min_frequency = LSM6DSM_ODR_MIN_VAL,
+		.max_frequency = LSM6DSM_ODR_MAX_VAL,
+		.config = {
+			/* AP: by default shutdown all sensors */
+			[SENSOR_CONFIG_AP] = {
+			.odr = 0,
+			.ec_rate = 0,
+			},
+			/* EC does not need in S0 */
+			[SENSOR_CONFIG_EC_S0] = {
+			.odr = 13000,
+			.ec_rate = 0,
+			},
+			/* Sensor off in S3/S5 */
+			[SENSOR_CONFIG_EC_S3] = {
+			.odr = 0,
+			.ec_rate = 0,
+			},
+			/* Sensor off in S3/S5 */
+			[SENSOR_CONFIG_EC_S5] = {
+			.odr = 0,
+			.ec_rate = 0,
+			},
+		},
+	},
+
+#endif /* CONFIG_ACCELGYRO_LSM6DSM */
+
+#ifdef CONFIG_ACCEL_LIS2DH
+	[LID_ACCEL] = {
+		.name = "LIS2DH BASE",
+		.active_mask = SENSOR_ACTIVE_S0,
+		.chip = MOTIONSENSE_CHIP_LIS2DH,
+		.type = MOTIONSENSE_TYPE_ACCEL,
+		.location = MOTIONSENSE_LOC_BASE,
+		.drv = &lis2dh_drv,
+		.mutex = &g_base_mutex,
+		.drv_data = &lis2dh_a_data,
+		.port = I2C_PORT_ACCEL,
+		.addr = LIS2DH_ADDR0,
+		.rot_standard_ref = &base_standard_ref,
+		.default_range = 2, /* g, enough for laptop. */
+		.min_frequency = LIS2DH_ODR_MIN_VAL,
+		.max_frequency = LIS2DH_ODR_MAX_VAL,
+		.config = {
+			/* AP: by default use EC settings */
+			[SENSOR_CONFIG_AP] = {
+				.odr = 0,
+				.ec_rate = 0,
+			},
+			/* EC use accel for angle detection */
+			[SENSOR_CONFIG_EC_S0] = {
+				.odr = 0,
+				.ec_rate = 0,
+			},
+			/* Sensor off in S3 */
+			[SENSOR_CONFIG_EC_S3] = {
+				.odr = 0,
+				.ec_rate = 0
+			},
+			/* Sensor off in S5 */
+			[SENSOR_CONFIG_EC_S5] = {
+				.odr = 0,
+				.ec_rate = 0
+			}
+		}
+	}
+#endif /* CONFIG_ACCEL_LIS2DH */
+};
+
+const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+void sensor_board_proc_double_tap(void)
+{
+	CPRINTS("Call LED status update");
+}
+
+/* Blue Led Blink on Timer Tick Event. */
+void gpio_tick(void)
+{
+	static int count;
+
+	gpio_set_level(GPIO_LED_BLUE, ++count & 0x01);
+}
+DECLARE_HOOK(HOOK_TICK, gpio_tick, HOOK_PRIO_DEFAULT);
+
+/* Initialize board */
+static void board_init(void)
+{
+	gpio_enable_interrupt(GPIO_USER_BUTTON);
+	gpio_set_level(GPIO_LED_BLUE, 0);
+	gpio_set_level(GPIO_LED_GREEN, 0);
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
