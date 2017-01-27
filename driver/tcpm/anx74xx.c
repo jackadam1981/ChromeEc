cb5d4feda5e56490e54f6d7ad3c766f86e69c3a4
Kevin K Wong <kevin.k.wong@intel.com>
2017-01-26 17:55:22 -0800

    ANX74xx: add TCPC low power mode for different DRP state
    Added code to put the ANX74xx in low power mode for different DRP state.
    1. When nothing attached or system is in S3 or S5 disable the auto
       toggling and put the system in Analog control mode.
    2. Using the CABLE_DET interrupt pin (attach event) enable high power
       mode.
    
    BUG=chrome-os-partner:59841, chrome-os-partner:61640
    BRANCH=None
    TEST=Manually tested on Reef using below dut-control command
         dut-control pp3300_pd_a_mw -r <n>
         1. S0, S3, S5 - Nothing connected 0mW
         2. In S0 SRC connected & put system to S3 - 10mW
         3. In S0 SRC connected & put system to S5 - 1.29mW
         3. In S0 nothing connected, put system to S3 or S5, attach
            SRC - 1.29mW
         4. Attach SRC at S5/S5 & boot to S0 - 10mW
         5. Chargers with/without eMark cable are detected in S0, S3, S5
    
    Change-Id: I30f7fd7a85e31987fb77e2cab2fe140d59dd3629
    Signed-off-by: Kevin K Wong <kevin.k.wong@intel.com>
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- driver/tcpm/anx74xx.c
+++ driver/tcpm/anx74xx.c
@@ -37,6 +37,8 @@ static int anx74xx_set_mux(int port, int polarity);
 /* Save the selected rp value */
 static int selected_rp[CONFIG_USB_PD_PORT_COUNT];
 
+static int anx74xx_tcpm_init(int port);
+
 static void anx74xx_tcpm_set_auto_good_crc(int port, int enable)
 {
 	int reg;
@@ -548,6 +550,79 @@ static int anx74xx_tcpm_set_cc(int port, int pull)
 	return rv;
 }
 
+#ifdef CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE
+void anx74xx_handle_power_mode(int port, int mode)
+{
+	static int prev_mode[CONFIG_USB_PD_PORT_COUNT] = {-1, -1};
+
+	if (mode == ANX74XX_STANDBY_MODE) {
+		anx74xx_set_power_mode(port, mode);
+	} else if (prev_mode[port] != ANX74XX_NORMAL_MODE) {
+		/*
+		 * TODO: Interrupt high follows CC line hence ignore multiple
+		 * interrupts.
+		 */
+		anx74xx_tcpm_init(port);
+	}
+
+	prev_mode[port] = mode;
+}
+
+static int anx74xx_drp_control(int port, int enable, enum tcpc_cc_pull pull)
+{
+	int reg;
+	int rv;
+
+	rv = tcpc_read(port, ANX74XX_REG_ANALOG_CTRL_6, &reg);
+	if (rv)
+		return rv;
+
+	if (enable)
+		reg |= ANX74XX_REG_DRP_EN;
+	else
+		reg &= ~ANX74XX_REG_DRP_EN;
+
+	rv = tcpc_write(port, ANX74XX_REG_ANALOG_CTRL_6, reg);
+	if (rv)
+		return rv;
+
+	if (pull == TYPEC_CC_OPEN)
+		return anx74xx_cc_software_ctrl(port, 0);
+	else
+		return anx74xx_tcpm_set_cc(port, pull);
+}
+
+static int anx74xx_tcpc_drp_toggle(int port, enum pd_dual_role_states drp_state)
+{
+	int rv;
+
+	switch (drp_state) {
+	case PD_DRP_FORCE_SOURCE:
+		/* Disable DRP control and set Rp to SOURCE */
+		rv = anx74xx_drp_control(port, 0, TYPEC_CC_RP);
+		break;
+	case PD_DRP_TOGGLE_ON:
+		/* Enable DRP control and set Rd to start as SINK */
+		rv = anx74xx_drp_control(port, 1, TYPEC_CC_RD);
+		break;
+	case PD_DRP_FORCE_SINK:
+	case PD_DRP_TOGGLE_OFF:
+		/* Disable DRP control and set Rd to SINK */
+		rv = anx74xx_drp_control(port, 0, TYPEC_CC_RD);
+		break;
+	default:
+		/* Disable DRP control and disable CC software control */
+		rv = anx74xx_drp_control(port, 0, TYPEC_CC_OPEN);
+		break;
+	}
+
+#ifdef CONFIG_USB_PD_TCPC_LOW_POWER
+	anx74xx_handle_power_mode(port, ANX74XX_STANDBY_MODE);
+#endif
+	return rv;
+}
+#endif
+
 static int anx74xx_tcpm_set_polarity(int port, int polarity)
 {
 	int reg, mux_state, rv = EC_SUCCESS;
@@ -825,7 +900,7 @@ void anx74xx_tcpc_alert(int port)
 	}
 }
 
-int anx74xx_tcpm_init(int port)
+static int anx74xx_tcpm_init(int port)
 {
 	int rv = 0, reg;
 
@@ -908,6 +983,9 @@ const struct tcpm_drv anx74xx_tcpm_drv = {
 	.tcpc_alert		= &anx74xx_tcpc_alert,
 #ifdef CONFIG_USB_PD_DISCHARGE_TCPC
 	.tcpc_discharge_vbus	= &anx74xx_tcpc_discharge_vbus,
+#endif
+#ifdef CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE
+	.drp_toggle		= &anx74xx_tcpc_drp_toggle,
 #endif
 };
 
