51199f59d624a925aa670daef3b63e36a38c2707
philipchen <philipchen@google.com>
2017-01-27 11:57:18 -0800

    scarlet: modify power-on sequence
    Merge PP900_USB_EN, PP900_PLL_EN, and PP900_PMU_EN.
    Add a new config flag to enable different power-on sequences
    on one SOC.
    
    BUG=chrome-os-partner:62207
    BRANCH=gru
    TEST=build kevin/gru/scarlet
    
    Change-Id: Iec3082384aa321636c59169b2bc55f773463f3d0

--- power/rk3399.c
+++ power/rk3399.c
@@ -204,8 +204,10 @@ enum power_state power_handle_state(enum power_state state)
 		gpio_set_level(GPIO_PP900_AP_EN, 1);
 		gpio_set_level(GPIO_PP900_PCIE_EN, 1);
 		msleep(2);
+#if CONFIG_CHIPSET_POWER_SEQ_VERSION == 0
 		gpio_set_level(GPIO_PP900_PMU_EN, 1);
 		gpio_set_level(GPIO_PP900_PLL_EN, 1);
+#endif
 		gpio_set_level(GPIO_PP900_USB_EN, 1);
 		msleep(2);
 
@@ -342,8 +344,10 @@ enum power_state power_handle_state(enum power_state state)
 		gpio_set_level(GPIO_PP1800_PMU_EN_L, 1);
 		msleep(2);
 		gpio_set_level(GPIO_PP900_USB_EN, 0);
+#if CONFIG_CHIPSET_POWER_SEQ_VERSION == 0
 		gpio_set_level(GPIO_PP900_PLL_EN, 0);
 		gpio_set_level(GPIO_PP900_PMU_EN, 0);
+#endif
 		msleep(6);
 		gpio_set_level(GPIO_PP900_PCIE_EN, 0);
 		gpio_set_level(GPIO_PP900_AP_EN, 0);
