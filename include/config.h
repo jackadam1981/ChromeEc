d9bff04d4114c9f7c2a0e84451498ef2ac16fb4a
Nadim Taha <ntaha@google.com>
2017-02-02 19:23:39 -0800

    common/spi_nor: Differential erase support
    If CONFIG_SPI_NOR_SMART_ERASE is defined will read the sector/block
    to be erased first and only initiate the erase operation if not
    already in an erased state. The read operation (performed in
    CONFIG_SPI_NOR_MAX_READ_SIZE chunks) is aborted early if a
    non "0xff" byte is encountered.
    
    Reduced erase time of a mostly erased EEPROM from 44s to 20s (16MB
    Winbond part) / 1m22s to 40s (32MB Macronix part) @24MHz.
    
    BUG=None
    BRANCH=None
    TEST=Built all targets. Successfully flashed various EEPROM images.
    
    Change-Id: I369b1fcf72140663b8dbce5a469ebad27f7571ec
    Signed-off-by: Nadim Taha <ntaha@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1844,6 +1844,13 @@
 /* If defined will enable block (64KiB) erase operations. */
 #undef CONFIG_SPI_NOR_BLOCK_ERASE
 
+/* If defined will read the sector/block to be erased first and only initiate
+ * the erase operation if not already in an erased state. The read operation
+ * (performed in CONFIG_SPI_NOR_MAX_READ_SIZE chunks) is aborted early if a
+ * non "0xff" byte is encountered.
+ */
+#undef CONFIG_SPI_NOR_SMART_ERASE
+
 /* SPI master feature */
 #undef CONFIG_SPI_MASTER
 
