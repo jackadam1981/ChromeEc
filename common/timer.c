e8414230020bf9199e1c3e6a02b8e89329a0a571
Vadim Bendebury <vbendeb@chromium.org>
2017-02-02 17:46:31 -0800

    common: add decimal seconds value to the timeirnfo output
    The timerinfo command shows the number of microseconds since boot,
    expressed as a hexadecimal value. Some of us are not as good in
    converting hexadecimal seconds value into decimal number of seconds
    and microseconds. This patch add the decimal value to the output.
    
    BRANCH=none
    BUG=none
    TEST=verified that timerinfo output makes sense:
       > time
       Time:     0x000000000b66d280 us, 191.287936 s
       ...
       > time
       Time:     0x000000000caec680 us, 212.780672 s
    
    Change-Id: I3bd4ba16f3cfb74ba8fcec4899fbff0ab259007c
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- common/timer.c
+++ common/timer.c
@@ -205,10 +205,10 @@ void timer_print_info(void)
 		__hw_clock_event_get();
 	int tskid;
 
-	ccprintf("Time:     0x%016lx us\n"
+	ccprintf("Time:     0x%016lx us, %d.%06d s\n"
 		 "Deadline: 0x%016lx -> %11.6ld s from now\n"
 		 "Active timers:\n",
-		 t, deadline, deadline - t);
+		 t, (int)(t/SECOND), (int)(t%SECOND), deadline, deadline - t);
 	cflush();
 
 	for (tskid = 0; tskid < TASK_ID_COUNT; tskid++) {
