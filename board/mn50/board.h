7c8427230fd53a1b39f45a293e714bc55ac19204
Nick Sanders <nsanders@chromium.org>
2017-02-03 11:54:22 -0800

    WIP mn50: initial checkin
    This firmware supports a board used to initialize firmware on new cr50
    parts.
    
    BUG=None
    BRANCH=None
    TEST=None yet
    
    Change-Id: I48818225393a6fc0db0c30bc79ad9787de608361

--- board/mn50/board.h
+++ board/mn50/board.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -7,13 +7,13 @@
 #define __CROS_EC_BOARD_H
 
 /*
- * The default watchdog timeout is 1.6 seconds, but there are some legitimate
- * flash-intensive TPM operations that actually take close to that long to
- * complete. Make sure we don't trigger the watchdog accidentally if the timing
+ * Make sure we don't trigger the watchdog accidentally if the timing
  * is just a little off.
  */
 #undef CONFIG_WATCHDOG_PERIOD_MS
 #define CONFIG_WATCHDOG_PERIOD_MS 5000
+
+#define CR50_DEV
 
 /* Features that we don't want */
 #undef CONFIG_CMD_LID_ANGLE
@@ -28,7 +28,6 @@
 
 #ifndef CR50_DEV
 /* Disable stuff that should only be in debug builds */
-#undef CONFIG_CMD_CRASH
 #undef CONFIG_CMD_MD
 #undef CONFIG_CMD_RW
 #undef CONFIG_CMD_SLEEPMASK
@@ -57,23 +56,9 @@
 #define NVMEM_PARTITION_SIZE CFG_TOP_SIZE
 /* Size in bytes of NvMem area */
 #define CONFIG_FLASH_NVMEM_SIZE (CFG_TOP_SIZE * NVMEM_NUM_PARTITIONS)
-/* Enable <key, value> variable support. */
-#define CONFIG_FLASH_NVMEM_VARS
-#define NVMEM_CR50_SIZE 272
-#define CONFIG_FLASH_NVMEM_VARS_USER_SIZE NVMEM_CR50_SIZE
-
-
-/* Go to sleep when nothing else is happening */
-#define CONFIG_LOW_POWER_IDLE
 
 /* Allow multiple concurrent memory allocations. */
 #define CONFIG_MALLOC
-
-/* Detect the states of other devices */
-#define CONFIG_DEVICE_STATE
-
-/* Enable debug cable detection */
-#define CONFIG_RDD
 
 /* USB configuration */
 #define CONFIG_USB
@@ -84,6 +69,7 @@
 #define CONFIG_USB_SPI
 #define CONFIG_USB_SERIALNO
 #define DEFAULT_SERIALNO "0"
+#define CONFIG_CMD_GPIO_EXTENDED
 
 #define CONFIG_STREAM_USART
 #define CONFIG_STREAM_USB
@@ -91,7 +77,7 @@
 /* Enable Case Closed Debugging */
 #define CONFIG_CASE_CLOSED_DEBUG
 
-#define CONFIG_USB_PID 0x5014
+#define CONFIG_USB_PID 0x502a
 #define CONFIG_USB_SELF_POWERED
 
 #undef CONFIG_USB_MAXPOWER_MA
@@ -103,10 +89,6 @@
 #define CONFIG_SPI_MASTER_CONFIGURE_GPIOS
 #define CONFIG_SPI_FLASH_PORT 0
 
-/* Enable SPI Slave (SPS) module */
-#define CONFIG_SPS
-#define CONFIG_TPM_SPS
-
 #define CONFIG_RBOX
 
 /* We don't need to send events to the AP */
@@ -114,7 +96,7 @@
 
 /* Make most commands restricted */
 #define CONFIG_CONSOLE_COMMAND_FLAGS
-#define CONFIG_RESTRICTED_CONSOLE_COMMANDS
+/* #define CONFIG_RESTRICTED_CONSOLE_COMMANDS */
 #define CONFIG_CONSOLE_COMMAND_FLAGS_DEFAULT CMD_FLAG_RESTRICTED
 
 /* Include crypto stuff, both software and hardware. */
@@ -144,44 +126,9 @@ enum usb_strings {
 	USB_STR_COUNT
 };
 
-/* Device indexes */
-enum device_type {
-	DEVICE_AP = 0,
-	DEVICE_EC,
-	DEVICE_SERVO,
-	DEVICE_BATTERY_PRESENT,
-	DEVICE_CCD_MODE,
-
-	DEVICE_COUNT
-};
-
-/* NVMem variables. */
-enum nvmem_vars {
-	NVMEM_VAR_CONSOLE_LOCKED = 0,
-	NVMEM_VAR_TEST_VAR,
-
-	NVMEM_VARS_COUNT
-};
-
-void board_configure_deep_sleep_wakepins(void);
-/* Interrupt handler */
-void tpm_rst_deasserted(enum gpio_signal signal);
-void device_state_on(enum gpio_signal signal);
 void post_reboot_request(void);
+void ccd_force_enable(void);
 
-/* Special controls over EC and AP */
-void assert_sys_rst(void);
-void deassert_sys_rst(void);
-int is_sys_rst_asserted(void);
-void assert_ec_rst(void);
-void deassert_ec_rst(void);
-int is_ec_rst_asserted(void);
-
-int board_has_ap_usb(void);
-int board_use_plt_rst(void);
-int board_rst_pullup_needed(void);
-int board_tpm_uses_i2c(void);
-int board_tpm_uses_spi(void);
 
 #endif /* !__ASSEMBLER__ */
 
@@ -205,9 +152,9 @@ int board_tpm_uses_spi(void);
 #define USB_EP_COUNT     7
 
 /* UART indexes (use define rather than enum to expand them) */
-#define UART_CR50	0
-#define UART_AP		1
-#define UART_EC		2
+#define UART_CR50       0
+#define UART_AP         1
+#define UART_EC         2
 
 #define UARTN UART_CR50
 
@@ -225,8 +172,6 @@ enum nvmem_users {
 };
 #endif
 
-#define CONFIG_FLASH_NVMEM_VARS_USER_NUM NVMEM_CR50
-
 /*
  * Let's be on the lookout for stack overflow, while debugging.
  *
@@ -241,8 +186,6 @@ enum nvmem_users {
 
 #define CONFIG_I2C
 #define CONFIG_I2C_MASTER
-#define CONFIG_I2C_SLAVE
-#define CONFIG_TPM_I2CS
 
 #define I2C_PORT_MASTER 0
 
