c2b3aa481c54e8ad2208eb194e8a43c1343a6fdf
Nick Sanders <nsanders@chromium.org>
2017-02-03 11:54:22 -0800

    WIP mn50: initial checkin
    This firmware supports a board used to initialize firmware on new cr50
    parts.
    
    BUG=None
    BRANCH=None
    TEST=None yet
    
    Change-Id: I48818225393a6fc0db0c30bc79ad9787de608361

--- chip/g/build.mk
+++ chip/g/build.mk
@@ -115,62 +115,67 @@ $(out)/$(PROJECT).obj: $(out)/RW/ec.RW_B.flat
 $(out)/RW/ec.RW_B.flat: $(out)/util/signer
 endif
 
+CR50_RO_KEY ?= rom-testkey-A.pem
+
 ifneq ($(CR50_DEV),)
+sign_fob=1
 CPPFLAGS += -DCR50_DEV=1
 endif
 
-MANIFEST := util/signer/ec_RW-manifest-dev.json
-CR50_RO_KEY ?= rom-testkey-A.pem
-ifeq ($(H1_DEVIDS),)
-CR50_RW_KEY = loader-testkey-A.pem
-SIGNER = $(out)/util/signer
-SIGNER_EXTRAS =
-SIGNER_MANIFEST := $(MANIFEST)
-else
-SIGNER = $(HOME)/bin/codesigner
-CR50_RW_KEY = cr50_rom0-dev-blsign.pem.pub
-RW_SIGNER_EXTRAS = -x util/signer/fuses.xml
+ifneq ($(FOB),)
 
-ifneq ($(CHIP_MK_INCLUDED_ONCE),)
-#
-# When building a node locked cr50 image for an H1 device with prod RO, the
-# manifest needs to be modifed to include the device ID of the chip the image
-# is built for.
-#
-# The device ID consists of two 32 bit numbers which can be retrieved by
-# running the 'sysinfo' command on the cr50 console. These two numbers
-# need to be spliced into the signer manifest after the '"fuses": {' line
-# for the signer to pick them up. Pass the numbers on the make command line
-# like this:
-#
-# H1_DEVIDS='<num 1> <num 2>' make ...
-#
-ifeq ($(SIGNER_MANIFEST),)
-SIGNER_MANIFEST := $(shell mktemp /tmp/h1.signer.XXXXXX)
-endif
-ifneq ($(CR50_DEV),)
+  SIGNER = $(HOME)/bin/codesigner
+  CR50_RW_KEY = cr50_rom0-dev-blsign.pem.pub
+  RW_SIGNER_EXTRAS = -x util/signer/fuses.xml
 
-#
-# When building a debug image, we don't want rollback protection to be in the
-# way - a debug image, which is guaranteed to be node locked should run on any
-# H1, whatever its info mask state is. The awk script below clears out the
-# info {} section of the manifest.
-#
-DUMMY := $(shell /usr/bin/awk 'BEGIN {skip = 0}; \
-	/^},/ {skip = 0}; \
-	{if (!skip) {print };} \
-	/\"info\": {/ {skip = 1};' $(MANIFEST) > $(SIGNER_MANIFEST))
-else
-DUMMY := $(shell /bin/cp $(MANIFEST) $(SIGNER_MANIFEST))
-endif
-REPLACEMENT := $(shell printf \
-	'\\n    \\"DEV_ID0\\": %d,\\n    \\"DEV_ID1\\": %d,' $(H1_DEVIDS))
-NODE_JSON :=  $(shell sed -i \
+  ifneq ($(CHIP_MK_INCLUDED_ONCE),)
+  ifneq ($(H1_DEVIDS),)
+  #
+  # When building a node locked cr50 image for an H1 device with prod RO, the
+  # manifest needs to be modifed to include the device ID of the chip the image
+  # is built for.
+  #
+  # The device ID consists of two 32 bit numbers which can be retrieved by
+  # running the 'sysinfo' command on the cr50 console. These two numbers
+  # need to be spliced into the signer manifest after the '"fuses": {' line
+  # for the signer to pick them up. Pass the numbers on the make command line
+  # like this:
+  #
+  # H1_DEVIDS='<num 1> <num 2>' make ...
+  #
+
+  SIGNER_MANIFEST := $(shell mktemp /tmp/h1.signer.XXXXXX)
+  DUMMY := $(shell /bin/cp util/signer/ec_RW-manifest-dev.json \
+           $(SIGNER_MANIFEST))
+  REPLACEMENT := $(shell printf \
+	'\\n    \\"DEV_ID0\\": %d,\\n    \\"DEV_ID1\\": %d,' $${H1_DEVIDS})
+  NODE_JSON :=  $(shell sed -i \
 	"s/\"fuses\": {/\"fuses\": {$(REPLACEMENT)/" $(SIGNER_MANIFEST))
 
-RW_SIGNER_EXTRAS += -j $(SIGNER_MANIFEST)
-endif  # CHIP_MK_INCLUDED_ONCE defined
-endif  # H1_DEVIDS defined
+  else  # H1_DEVIDS  ^^^^^ nonempty    vvvvvv empty
+
+  SIGNER_MANIFEST := util/signer/ec_RW-manifest-dev.json
+
+  endif  # H1_DEVIDS  ^^^^^ empty
+
+  RW_SIGNER_EXTRAS += -j $(SIGNER_MANIFEST)
+  endif  # CHIP_MK_INCLUDED_ONCE defined
+else
+ifneq ($(NODE_LOCK_RO),)
+  SIGNER = $(HOME)/bin/codesigner
+  CR50_RW_KEY = loader-testkey-A.pem
+
+  #SIGNER_MANIFEST := util/signer/ec_RW-manifest-dev.json
+  #RW_SIGNER_EXTRAS += -j $(SIGNER_MANIFEST)
+
+ else
+
+  CR50_RW_KEY = loader-testkey-A.pem
+  SIGNER = $(out)/util/signer
+  SIGNER_EXTRAS =
+
+endif  # NODE_LOCK_RO defined
+endif  # FOB defined
 
 
 # This file is included twice by the Makefile, once to determine the CHIP info
