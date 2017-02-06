e1cbeddc55a8f81a3479272f16efcb61c46dbb77
Rong Chang <rongchang@google.com>
2017-02-06 18:32:01 +0800

    rose: add stm32f401 and stm32f412 support
    This change sym-links stm32f446 config and adds them to chip config.
    
    BUG=chromium:688979
    TEST=boots on stm32f401 dev board
    BRANCH=none
    
    Change-Id: I939fd17f29f4b431d9c1358c184166c67fef18d3
    Signed-off-by: Rong Chang <rongchang@chromium.org>

--- chip/stm32/config_chip.h
+++ chip/stm32/config_chip.h
@@ -38,6 +38,10 @@
 #include "config-stm32l100.h"
 #elif defined(CHIP_VARIANT_STM32F446)
 #include "config-stm32f446.h"
+#elif defined(CHIP_VARIANT_STM32F412)
+#include "config-stm32f412.h"
+#elif defined(CHIP_VARIANT_STM32F401)
+#include "config-stm32f401.h"
 #elif defined(CHIP_VARIANT_STM32F373)
 #include "config-stm32f373.h"
 #elif defined(CHIP_VARIANT_STM32F09X)
@@ -65,7 +69,9 @@
 /* Program is run directly from storage */
 #define CONFIG_MAPPED_STORAGE_BASE CONFIG_PROGRAM_MEMORY_BASE
 
-#if !defined(CHIP_VARIANT_STM32F446)
+#if !(defined(CHIP_VARIANT_STM32F446) || \
+	defined(CHIP_VARIANT_STM32F412) || \
+	defined(CHIP_VARIANT_STM32F401))
 /* Compute the rest of the flash params from these */
 #include "config_std_internal_flash.h"
 #endif
