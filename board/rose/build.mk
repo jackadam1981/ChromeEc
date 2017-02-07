4ff494611f60710568c4f0696a84cc0590eb8879
Rong Chang <rongchang@google.com>
2017-02-07 17:22:50 +0800

    WIP: rose: initial commit
    Rose board has an stm32f412 mcu and SPI master to measure heatmap from
    sensors. Data will be reported to host through I2C with interrupt
    notification.
    
    BUG=chromium:688979
    TEST=build and load on dev board, check hardware button using UART
         console.
    BRANCH=none
    
    Change-Id: If3fa71509d4b7041c9ce5e81ccf03f0bcb0487a4
    Signed-off-by: Rong Chang <rongchang@chromium.org>

---
+++ board/rose/build.mk
@@ -0,0 +1,12 @@
+# -*- makefile -*-
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+
+CHIP:=stm32
+CHIP_FAMILY:=stm32f4
+CHIP_VARIANT:=stm32f412
+
+board-y=board.o
