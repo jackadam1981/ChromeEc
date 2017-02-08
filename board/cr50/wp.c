58979802a7fa161774a93978918e372503fbe395
Aseda Aboagye <aaboagye@google.com>
2017-02-07 18:52:13 -0800

    cr50: Use BATT_PRES_L as source of write protect.
    This commit changes the Cr50 write protect behaviour to simply follow
    the state of the battery present pin.  The state can still be overridden
    by using the `wp` console command.
    
    BUG=chrome-os-partner:62726
    BRANCH=None
    TEST=Plug in battery, verify that WP is enabled.  Plug in AC and unplug
    battery, verify that WP is disabled.
    TEST=Unplug battery, unplug AC, plug in AC, verify that WP is disabled.
    TEST=Unplug battery, verify that WP is disabled.  Use `wp' command to
    enable WP, verify that it is enabled.
    TEST=Plug in battery, disable WP using `wp` command, put cr50 into deep
    sleep, wake it up, verify that WP is still disabled.
    
    Change-Id: I83d9820067800801ddbde311eab0853c3c2216d3
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/cr50/wp.c
+++ board/cr50/wp.c
@@ -19,7 +19,7 @@
 #define CPRINTS(format, args...) cprints(CC_RBOX, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_RBOX, format, ## args)
 
-static void set_wp_state(int asserted)
+void set_wp_state(int asserted)
 {
 	/* Enable writing to the long life register */
 	GWRITE_FIELD(PMU, LONG_LIFE_SCRATCH_WR_EN, REG1, 1);
@@ -132,16 +132,16 @@ static void unlock_the_console(void)
 static void init_console_lock_and_wp(void)
 {
 	/*
-	 * On an unexpected reboot or a system rollback reset the console and
-	 * write protect states.
+	 * On an unexpected reboot or a system rollback reset the console lock
+	 * and write protect states.
 	 */
 	if (system_rollback_detected() ||
 	    !(system_get_reset_flags() & RESET_FLAG_HIBERNATE)) {
 		/* Reset the console lock to the default value */
 		set_console_lock_state(console_restricted_state);
 
-		/* Always assert WP on H1 cold resets, reboots or fallbacks. */
-		set_wp_state(1);
+		/* Use BATT_PRES_L as the source for write protect. */
+		set_wp_state(!gpio_get_level(GPIO_BATT_PRES_L));
 		return;
 	}
 
