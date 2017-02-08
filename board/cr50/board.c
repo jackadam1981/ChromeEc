0ff27318f5b6d68408c379d8970d09052f39952b
Aseda Aboagye <aaboagye@google.com>
2017-02-07 18:52:13 -0800

    cr50: Use BATT_PRES_L as source of write protect.
    This commit changes the Cr50 write protect behaviour to simply follow
    the state of the battery present pin.  The state can still be overridden
    by using the `wp` console command.
    
    BUG=chrome-os-partner:62726
    BRANCH=None
    TEST=Plug in battery, verify that WP is enabled.  Plug in AC and unplug
    battery, verify that WP is disabled.
    TEST=Unplug battery, unplug AC, plug in AC, verify that WP is disabled.
    TEST=Unplug battery, verify that WP is disabled.  Use `wp' command to
    enable WP, verify that it is enabled.
    TEST=Plug in battery, disable WP using `wp` command, put cr50 into deep
    sleep, wake it up, verify that WP is still disabled.
    
    Change-Id: I83d9820067800801ddbde311eab0853c3c2216d3
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -35,6 +35,7 @@
 #include "usb_spi.h"
 #include "usb_i2c.h"
 #include "util.h"
+#include "wp.h"
 
 /* Define interrupt and gpio structs */
 #include "gpio_list.h"
@@ -81,6 +82,7 @@ uint32_t nvmem_user_sizes[NVMEM_NUM_USERS] = {
 
 static int device_state_changed(enum device_type device,
 				enum device_state state);
+static int last_batt_pres = -1;
 
 /*  Board specific configuration settings */
 static uint32_t board_properties;
@@ -758,6 +760,11 @@ struct device_config device_states[] = {
 		.detect = GPIO_DETECT_EC,
 		.name = "EC"
 	},
+	[DEVICE_BATTERY_PRESENT] = {
+		.deferred = NULL,
+		.detect = GPIO_BATT_PRES_L,
+		.name = "BattPrsnt"
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(device_states) == DEVICE_COUNT);
 
@@ -801,6 +808,27 @@ void device_state_on(enum gpio_signal signal)
 
 void board_update_device_state(enum device_type device)
 {
+	if (device == DEVICE_BATTERY_PRESENT) {
+		/* The battery presence pin is active low. */
+		int bp = !gpio_get_level(device_states[device].detect);
+
+		/*
+		 * We use BATT_PRES_L as the source for write protect.  However,
+		 * since it can be overridden by a console command, only change
+		 * the write protect state when the battery presence pin has
+		 * changed.
+		 */
+		if ((last_batt_pres != -1) && (bp != last_batt_pres)) {
+			CPRINTS("battery %spresent", bp ? "" : "NOT ");
+			set_wp_state(bp);
+		}
+
+		last_batt_pres = bp;
+		device_set_state(device,
+				 bp ? DEVICE_STATE_ON : DEVICE_STATE_OFF);
+		return;
+	}
+
 	if (device == DEVICE_SERVO && servo_state_unknown())
 		return;
 
