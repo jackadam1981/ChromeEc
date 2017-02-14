a7dd8450155e70e963e12a1576a845b76941fd04
Vincent Palatin <vpalatin@chromium.org>
2017-02-14 11:49:16 +0100

    Add eve_fp board
    Eve FP MCU is using the STM32L442 microcontroller.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=chrome-os-partner:62893
    TEST=make BOARD=eve_fp
    run it on Nucleo-L432KC (STM32L432KC is mostly the same MCU without AES)
    
    Change-Id: I18dc57e9bf262c36283f8c835a2d4320bc5ee837

---
+++ board/eve_fp/board.h
@@ -0,0 +1,41 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Fingerprint microcontroller configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* Serial console */
+#define CONFIG_UART_CONSOLE 1
+#define CONFIG_UART_TX_DMA_CH STM32_DMAC_USART1_TX
+#define CONFIG_UART_TX_DMA_PH 2
+
+/* Optional features */
+#define CONFIG_CMD_SPI_XFER
+#undef  CONFIG_LID_SWITCH
+#define CONFIG_SPI
+#define CONFIG_SPI_MASTER
+#define CONFIG_STM_HWTIMER32
+
+/* TODO(crosbug.com/p/62893) implement flash support for STM32L4 */
+#undef CONFIG_FLASH
+#undef CONFIG_FLASH_PHYSICAL
+#define CONFIG_SYSTEM_UNLOCKED
+
+#define CONFIG_SPI_SENSOR_PORT    1  /* SPI3: second SPI master port */
+
+/* Timer selection */
+#define TIM_CLOCK32     2
+#define TIM_WATCHDOG   16
+
+#ifndef __ASSEMBLER__
+
+#include "gpio_signal.h"
+
+void fps_event(enum gpio_signal signal);
+
+#endif /* !__ASSEMBLER__ */
+#endif /* __CROS_EC_BOARD_H */
