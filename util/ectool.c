5e2201f3d300e920cd4c31a9b35eb23383c138aa
Daisuke Nojiri <dnojiri@chromium.org>
2017-02-14 09:14:34 -0800

    ectool: Force interface=dev when name is given
    '--name' option is used only for '--interface=dev'. Thus, the interface
    should be set to COMM_DEV implicitly. This prevents the version command
    with --name=cros_pd from printing EC's information instead of PD's when
    there is no cros_pd exists on the board.
    
    BUG=chromium:691901
    BRANCH=none
    TEST=Run ectool as follows:
    localhost# /tmp/ectool --name=cros_pd version
    Unable to establish host communication
    Couldn't find EC
    
    Change-Id: I4d669e647f6025f54b94200ca6b5502be54795c8
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- util/ectool.c
+++ util/ectool.c
@@ -277,7 +277,8 @@ int parse_bool(const char *s, int *dest)
 void print_help(const char *prog, int print_cmds)
 {
 	printf("Usage: %s [--dev=n] [--interface=dev|lpc|i2c] ", prog);
-	printf("[--name=cros_ec|cros_sh|cros_pd] <command> [params]\n\n");
+	printf("[--name=cros_ec|cros_sh|cros_pd] <command> [params]\n");
+	printf("The --name option implies --interface=dev.\n\n");
 	if (print_cmds)
 		puts(help_str);
 	else
@@ -7069,6 +7070,7 @@ int main(int argc, char *argv[])
 			break;
 		case OPT_NAME:
 			strncpy(device_name, optarg, 40);
+			interfaces = COMM_DEV;
 			break;
 		}
 	}
