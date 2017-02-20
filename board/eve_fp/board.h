9832310bdc2e3c16e6e763f0f61c92a4c0f32462
Vincent Palatin <vpalatin@chromium.org>
2017-02-20 19:51:23 +0100

    HACK: benchmark crypto speed
    NOT FOR SUBMISSION
    
    Compare public key crypto speeds with GCC and LLVM
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=none
    TEST=benchmark values:
    === full GCC build ===
    FLASH 37.3k / 128.0k  [ text 28.9k rodat  8.4k data  0.1k ]
    RAM    8.1k / 64.0k  [ data  0.1k bss    8.0k ]
    
                     @16Mhz        @80Mhz
    X25519 duration 137405 us     40746 us
    RSA    duration 185164 us     39285 us
    
    === with RSA and curve25519 as LLVM assembly ===
    FLASH 38.7k / 128.0k  [ text 30.3k rodat  8.4k data  0.1k ]
    RAM    8.1k / 64.0k  [ data  0.1k bss    8.0k ]
    
                     @16Mhz        @80Mhz
    X25519 duration 103902 us     35782 us
    RSA    duration 114122 us     25078 us
    
    Change-Id: I0be1d92cfc21d0136ef37104cf3b5f1ba6273f92

--- board/eve_fp/board.h
+++ board/eve_fp/board.h
@@ -28,7 +28,9 @@
 #define CONFIG_SPI
 #define CONFIG_SPI_MASTER
 #define CONFIG_STM_HWTIMER32
-
+#define CONFIG_CURVE25519
+#define CONFIG_SHA256
+#define CONFIG_RSA
 
 #define CONFIG_SPI_FP_PORT    1  /* SPI3: second SPI master port */
 
--- board/eve_fp/build.mk
+++ board/eve_fp/build.mk
@@ -10,3 +10,6 @@ CHIP_FAMILY:=stm32l4
 CHIP_VARIANT:=stm32l442
 
 board-y=board.o
+
+# Generate optimized assembler version from LLVM build
+use_opt_asm:=common/curve25519.o
