b01ec34714be2eae9024e943ce12613d99756bce
Rong Chang <rongchang@google.com>
2017-02-20 16:15:09 +0800

    rose: spi: add SPI master halfduplex mode
    This change adds 3-wire mode support in STM32 SPI master driver.
    
    BUG=chromium:688979
    TEST=manual
         enable CONFIG_SPI_HALFDUPLEX
         read id from SPI heatmap sensor
    BRANCH=none
    
    Change-Id: I09139dcbfe39a427721451db6842ea712abf2e33
    Signed-off-by: Rong Chang <rongchang@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1967,6 +1967,9 @@
 /* SPI master feature */
 #undef CONFIG_SPI_MASTER
 
+/* SPI master halfduplex/3-wire mode */
+#undef CONFIG_SPI_HALFDUPLEX
+
 /* Support STM32 SPI1 as master. */
 #undef CONFIG_STM32_SPI1_MASTER
 
