a756a653dbf2a197ec75cd9abbb97900a39e72b0
Nicolas Boichat <drinkcat@google.com>
2017-02-22 16:47:27 +0800

    stm32/usb: Add support for USB suspend
    This follows the basics of what is decribed in
    RM0091 Reference Manual 30.5.5 Suspend/Resume events .
    
    We call enable/disable_sleep at suspend/resume, to make
    sure the EC stays awake when USB is connected and active.
    
    We also call clock_enable/disable_module, which is stubbed on
    stm32f0, but can be used on other devices in the family.
    
    This also fixes interrupt handling in usb_interrupt, by only
    clearing interrupt bits that were handled instead of resetting
    them all, which is racy, and can potentially lead to issues
    if reset comes soon after a resume event.
    
    BRANCH=none
    BUG=chrome-os-partner:62325
    TEST=build and flash hammer, connect to chell, suspend/resume,
         and see that hammer prints USB suspend and USB resume lines.
    
    Change-Id: Ie9d02fd4a114add3ebc98dc9393680bc9a64a522

--- chip/stm32/usb-stm32f0.c
+++ chip/stm32/usb-stm32f0.c
@@ -6,15 +6,22 @@
  */
 
 #include "registers.h"
+#include "system.h"
 #include "usb_api.h"
 
 void usb_connect(void)
 {
 	STM32_USB_BCDR |= (1 << 15) /* DPPU */;
+
+	/* USB is in use */
+	disable_sleep(SLEEP_MASK_USB_DEVICE);
 }
 
 void usb_disconnect(void)
 {
 	/* disable pull-up on DP to disconnect */
 	STM32_USB_BCDR &= ~(1 << 15) /* DPPU */;
+
+	/* USB is off, so sleep whenever */
+	enable_sleep(SLEEP_MASK_USB_DEVICE);
 }
--- chip/stm32/usb-stm32l.c
+++ chip/stm32/usb-stm32l.c
@@ -6,15 +6,22 @@
  */
 
 #include "registers.h"
+#include "system.h"
 #include "usb_api.h"
 
 void usb_connect(void)
 {
 	STM32_SYSCFG_PMC |= 1;
+
+	/* USB is in use */
+	disable_sleep(SLEEP_MASK_USB_DEVICE);
 }
 
 void usb_disconnect(void)
 {
 	/* disable pull-up on DP to disconnect */
 	STM32_SYSCFG_PMC &= ~1;
+
+	/* USB is off, so sleep whenever */
+	enable_sleep(SLEEP_MASK_USB_DEVICE);
 }
