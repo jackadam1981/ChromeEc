27d48fb7696e3ff3c1c16749c8889caaaa77e2ce
Aseda Aboagye <aaboagye@google.com>
2017-02-24 13:48:45 -0800

    hooks: Add priorities to deferred functions.
    BUG=chromium:696022
    BRANCH=None
    TEST=make -j buildall
    
    Change-Id: I289613cad0cd0f78680c44286287a9415c5500ae
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- include/hooks.h
+++ include/hooks.h
@@ -268,9 +268,11 @@ int hook_call_deferred(const struct deferred_data *data, int us);
  *
  * @param routine	Function pointer, with prototype void routine(void)
  */
-#define DECLARE_DEFERRED(routine)					\
+#define DECLARE_DEFERRED(routine) \
+	DECLARE_PRIORITY_DEFERRED(50, routine)
+#define DECLARE_PRIORITY_DEFERRED(priority, routine)			\
 	const struct deferred_data __keep CONCAT2(routine, _data)	\
-	__attribute__((section(".rodata.deferred")))			\
+	__attribute__((section(".rodata.deferred." #priority)))		\
 	     = {routine}
 
 #else /* CONFIG_COMMON_RUNTIME */
