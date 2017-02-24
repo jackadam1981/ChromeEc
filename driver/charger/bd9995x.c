29bb78ebf7a613a9f5b4ee580024dbe55218c253
Bruce <Bruce.Wan@quantatw.com>
2017-02-24 19:39:33 +0800

    charger/bd9995x: enable IADP bit when charger init.
    When plug in adapter with dead battery, the charger will be flow back
    to the adapter. Then adapter will OVP.
    
    Enable IADP bit at address 0x40(BD9995X_CMD_VM_CTRL_SET) when charger
    init. It will fix this phenomenon.
    
    Enable IADP pin current limit due to we've already deployed the clearing
    to existing.
    
    BUG=chrome-os-partner:62646, 61691
    BRANCH=none
    TEST=check charger don't flow back to adapter with dead battery
    
    Change-Id: Id6202f3dd51cb7e1c43a124660e09de39f6c041b
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- driver/charger/bd9995x.c
+++ driver/charger/bd9995x.c
@@ -793,16 +793,7 @@ static void bd9995x_init(void)
 #ifdef CONFIG_CHARGER_BD9995X_CHGEN
 	reg |= BD9995X_CMD_CHGOP_SET2_CHG_EN;
 #endif
-	ch_raw_write16(BD9995X_CMD_CHGOP_SET2, reg,
-		       BD9995X_EXTENDED_COMMAND);
-
-	/* Disable IADP pin current limit */
-	if (ch_raw_read16(BD9995X_CMD_VM_CTRL_SET, &reg,
-			  BD9995X_EXTENDED_COMMAND))
-		return;
-	reg &= ~BD9995X_CMD_VM_CTRL_SET_EXTIADPEN;
-	ch_raw_write16(BD9995X_CMD_VM_CTRL_SET, reg,
-		       BD9995X_EXTENDED_COMMAND);
+	ch_raw_write16(BD9995X_CMD_CHGOP_SET2, reg, BD9995X_EXTENDED_COMMAND);
 
 	/* Define battery charging profile */
 	bd9995x_battery_charging_profile_settings();
