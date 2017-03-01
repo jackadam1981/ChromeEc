77fc2df4426c4b45dd3f528de3dc87ac598c3c2e
Vadim Bendebury <vbendeb@google.com>
2017-02-28 18:16:42 -0800

    g: erase RW INFO rollback space to match the header infomap field
    The cr50 RO image compares INFO rollback map space against the
    contents of the RW image's infomap field.
    
    To ensure that no rollback is possible, the RW should verify that the
    INFO space state is consistent with the current RW header, and if not,
    update the INFO state to comply.
    
    This should happen only when running prod images, so that debug images
    could be rolled back if so desired.
    
    BRANCH=none
    BUG=b:35774863
    TEST=as follows:
      - built and ran a new image, observed it start successfully;
      - modified the manifest to erase the first map location, built and
        ran a new image, observed it start successfully
      - restored the manifest, built and tried running a new image,
        observed that the earlier version is starting.
    
    Change-Id: I62253c3e98cd24ed24424b8bb9de22692a262d89
    Signed-off-by: Vadim Bendebury <vbendeb@google.com>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -537,6 +537,8 @@ static void board_init(void)
 	/* Initialize the persistent storage. */
 	initvars();
 
+	system_update_rollback_mask();
+
 	/* Indication that firmware is running, for debug purposes. */
 	GREG32(PMU, PWRDN_SCRATCH16) = 0xCAFECAFE;
 
