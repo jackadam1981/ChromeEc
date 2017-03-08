58b584effe030c6a5223ddaac19977d5d090cb33
Daisuke Nojiri <dnojiri@chromium.org>
2017-03-07 17:30:22 -0800

    pd: Disable PD communication if battery is not present
    NOT_FOR_REVIEW REFERENCE_ONLY
    
    This patch disables PD communication for RO if battery is not
    present.
    
    BUG=b:35648282
    BRANCH=none
    TEST=build for reef
    
    Change-Id: Ia74d3184852ef6d484202465c268a4827eccb16e
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -1619,7 +1619,8 @@ static void pd_init_tasks(void)
 	enable = 0;
 #elif defined(CONFIG_USB_PD_COMM_LOCKED)
 	/* Disable PD communication at init if we're in RO and locked. */
-	if (system_get_image_copy() != SYSTEM_IMAGE_RW && system_is_locked())
+	if (system_get_image_copy() != SYSTEM_IMAGE_RW &&
+			(system_is_locked() || !battery_is_present()))
 		enable = 0;
 #endif
 	for (i = 0; i < CONFIG_USB_PD_PORT_COUNT; i++)
