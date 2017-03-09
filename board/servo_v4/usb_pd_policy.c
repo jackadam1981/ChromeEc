058c11d292587328b25841dc17c60ecd53d7d13c
Scott <scollyer@chromium.org>
2017-03-08 18:56:32 -0800

    servo_v4: pd: Updated CC_NC and CC_RA macros
    Previous boards that implemented tcpc layer on chip didn't support
    variable Rp values. However, servo_v4 can present any of the 3
    possible Rp values and therefore the voltage thresholds that are used
    to determine a no-connect or Ra attach status need a way to be set
    based on the Rp value that's current attached to a given CC line.
    
    - Added port and cc line selection to both the CC_NC and CC_RA macros
      and now check if they are already defined before being defined in
      usb_pd_tcpc.c.
    - Defined each of these macros in board.h to use a function that's
      able to select the threshold based on the current Rp configuration.
    
    BUG=b:35586526
    BRANCH=servo
    TEST=Tested with servo_v4 against Electro and verified that it
    connects when a charger is and is not connected to CHG port which
    exercises the differnt Rp combinations that servo_v4 presents.
    
    Change-Id: I1a31e430c0f290486f0fa8a50bdafdddf20d23ca
    Signed-off-by: Scott <scollyer@chromium.org>

--- board/servo_v4/usb_pd_policy.c
+++ board/servo_v4/usb_pd_policy.c
@@ -58,6 +58,18 @@ static int vbus_volt_mv[CONFIG_USB_PD_PORT_COUNT] = {0, 0};
 static int vbus_curr_ma[CONFIG_USB_PD_PORT_COUNT] = {0, 0};
 static int vbus_ma_chg;
 static int vbus_mv_chg;
+/* Voltage thresholds for no connect */
+static int pd_src_vnc[TYPEC_RP_RESERVED][2] = {
+	{PD_SRC_3_0_VNC_MV, PD_SRC_1_5_VNC_MV},
+	{PD_SRC_1_5_VNC_MV, PD_SRC_DEF_VNC_MV},
+	{PD_SRC_3_0_VNC_MV, PD_SRC_DEF_VNC_MV},
+};
+/* Voltage thresholds for Ra attach */
+static int pd_src_rd_threshold[TYPEC_RP_RESERVED][2] = {
+	{PD_SRC_3_0_RD_THRESH_MV, PD_SRC_DEF_RD_THRESH_MV},
+	{PD_SRC_1_5_RD_THRESH_MV, PD_SRC_DEF_RD_THRESH_MV},
+	{PD_SRC_3_0_RD_THRESH_MV, PD_SRC_DEF_RD_THRESH_MV},
+};
 
 static void board_manage_dut_port(int port)
 {
@@ -154,6 +166,38 @@ static void board_manage_chg_port(int port, int max_ma, int vbus_mv)
 	else
 		/* Update CHG port status now since vbus is off */
 		board_update_chg_port();
+}
+
+int pd_tcpc_cc_nc(int port, int cc_volt, int cc_sel)
+{
+	int rp_index;
+
+	/* Can never be called from CHG port as it's sink only */
+	if (port == CHG)
+		return 0;
+
+	rp_index = vbus_rp;
+	/* Ensure that rp_index doens't exceed the array size */
+	if (rp_index >= TYPEC_RP_RESERVED)
+		rp_index = 0;
+
+	return cc_volt >= pd_src_vnc[rp_index][cc_sel];
+}
+
+int pd_tcpc_cc_ra(int port, int cc_volt, int cc_sel)
+{
+	int rp_index;
+
+	/* Can never be called from CHG port as it's sink only */
+	if (port == CHG)
+		return 0;
+
+	rp_index = vbus_rp;
+	/* Ensure that rp_index doens't exceed the array size */
+	if (rp_index >= TYPEC_RP_RESERVED)
+		rp_index = 0;
+
+	return cc_volt < pd_src_rd_threshold[rp_index][cc_sel];
 }
 
 int board_select_rp_value(int port, int rp)
