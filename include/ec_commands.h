98b0240d21c84c08c4bcc6bda80539b3c6e9b596
Mario Tesi <mario.tesi@st.com>
2017-03-13 12:10:32 +0100

    driver: accelgyro: LSM6DSM (LSM6DSL) add commons function
    	Added stm mems common function library to LSM6DSx
    	acc/gyro driver
    
            BUG=none
            BRANCH=master
            TEST=Tested on discovery BOARD with LSM6DSM connected
    	to EC i2c master bus. Data for acc + gyro seems ok: can
    	successfully change ODR and full scale range
    
    Change-Id: I6a59ee6f2f1a681fe715a6e3a736f4a264fa0e4b
    Signed-off-by: Mario Tesi <mario.tesi@st.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -1965,6 +1965,7 @@ enum motionsensor_chip {
 	MOTIONSENSE_CHIP_BMA255 = 8,
 	MOTIONSENSE_CHIP_BMP280 = 9,
 	MOTIONSENSE_CHIP_OPT3001 = 10,
+	MOTIONSENSE_CHIP_LSM6DSM = 11,
 };
 
 struct __ec_todo_packed ec_response_motion_sensor_data {
