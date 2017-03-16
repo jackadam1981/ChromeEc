6461688b171b39674b95cd5355a093301c6fc3c1
Nicolas Boichat <drinkcat@google.com>
2017-03-16 10:24:37 -0700

    WIP: mkbp: Add sysrq_x button
    BRANCH=none
    BUG=b:35775099
    TEST=sysrq_x available in EC console
    
    Change-Id: I3fd2b39a1818fb8e75ad0c3c3a92e98d6c96d415

--- common/keyboard_mkbp.c
+++ common/keyboard_mkbp.c
@@ -258,6 +258,31 @@ void keyboard_update_button(enum keyboard_button_type button, int is_pressed)
 		      (const uint8_t *)&mkbp_button_state);
 }
 
+static void mkbp_send_sysrq(void)
+{
+	/*
+	 * Press sysrq "button". Note that this is a one-shot button and that we
+	 * never release it (so we do not update mkbp_button_state): the AP
+	 * should react whenever we set the bit to 1, but does not need to
+	 * care about track its state.
+	 */
+	uint32_t button_state = mkbp_button_state | (1 << EC_MKBP_SYSRQ_X);
+
+	/* Add the new state to the FIFO. */
+	mkbp_fifo_add(EC_MKBP_EVENT_BUTTON,
+		      (const uint8_t *)&button_state);
+}
+
+static int console_sysrq_x(int argc, char **argv)
+{
+	mkbp_send_sysrq();
+
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(sysrq_x, console_sysrq_x,
+			NULL,
+			"Simulate sysrq x press");
+
 #ifdef CONFIG_POWER_BUTTON
 /**
  * Handle power button changing state.
