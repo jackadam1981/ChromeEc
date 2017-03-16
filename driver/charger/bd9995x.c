7ce6a7df081b206545a9376a9c0b2b4214c05b64
Vijay Hiremath <vijay.p.hiremath@intel.com>
2017-03-15 18:15:27 -0700

    BD9995X: Disable input current limiting for VBAT < VSYSREG_SET
    When the VBAT is less than the VSYSREG_SET input current is limited
    hence power is cut to the DUT. To avoid this issue, disable the input
    current limit for avoiding VSYS drop when VBAT is the dead-battery
    or VBAT is < VSYSREG_SET.
    
    BUG=b:35648317
    BRANCH=none
    TEST=Manually tested on Electro.
         a. DUT boots without the battery
         b. DUT boots from cut-off battery
         c. DUT boots from cold-reset without the battery
    
    Change-Id: Ief9960550f988e69ab4637db85450e91c70d3b51
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- driver/charger/bd9995x.c
+++ driver/charger/bd9995x.c
@@ -785,6 +785,9 @@ static void bd9995x_init(void)
 	ch_raw_write16(BD9995X_CMD_CHGOP_SET1, reg,
 		       BD9995X_EXTENDED_COMMAND);
 
+	/* Set VSYSREG_SET > VBAT so that the charger is in Pre-Charge state */
+	bd9995x_set_vsysreg(battery_get_info()->voltage_max + 200);
+
 	/* Enable BC1.2 USB charging and DC/DC converter @ 1200KHz */
 	if (ch_raw_read16(BD9995X_CMD_CHGOP_SET2, &reg,
 			  BD9995X_EXTENDED_COMMAND))
@@ -796,6 +799,17 @@ static void bd9995x_init(void)
 	reg |= BD9995X_CMD_CHGOP_SET2_CHG_EN;
 #endif
 	ch_raw_write16(BD9995X_CMD_CHGOP_SET2, reg,
+		       BD9995X_EXTENDED_COMMAND);
+
+	/*
+	 * Disable the input current limit for avoiding VSYS drop when VBAT
+	 * is the dead-battery, VBAT is < VSYSREG_SET.
+	 */
+	if (ch_raw_read16(BD9995X_CMD_VIN_CTRL_SET, &reg,
+			  BD9995X_EXTENDED_COMMAND))
+		return;
+	reg |= BD9995X_CMD_VIN_CTRL_SET_VSYS_PRIORITY;
+	ch_raw_write16(BD9995X_CMD_VIN_CTRL_SET, reg,
 		       BD9995X_EXTENDED_COMMAND);
 
 	/* Define battery charging profile */
