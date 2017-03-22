217cf6012ec2763afcf8770780baa6cc19d20ca9
Nicolas Boichat <drinkcat@google.com>
2017-03-22 10:50:39 +0800

    common/update: Update common code updater to latest chip/g version
    Let's move to protocol version 6, which provides most of the new features
    we want to update generic EC firmware.
    
    diff -u include/update_fw.h chip/g/upgrade_fw.h
    diff -u common/usb_update.c chip/g/usb_upgrade.c
    diff -u common/update_fw.c chip/g/upgrade_fw.c
    
    Only shows chip/g specific differences.
    
    BRANCH=none
    BUG=b:36375666
    TEST=make buildall -j
    TEST=Can update hammer over USB using usb_updater2
    
    Change-Id: I5b0f0281d844972dab572955d5495f808127e523

--- common/usb_update.c
+++ common/usb_update.c
@@ -7,13 +7,11 @@
 #include "common.h"
 #include "console.h"
 #include "consumer.h"
-#include "include/compile_time_macros.h"
+#include "extension.h"
 #include "queue_policies.h"
 #include "shared_mem.h"
 #include "system.h"
 #include "update_fw.h"
-#include "usb_api.h"
-#include "usb_hw.h"
 #include "usb-stream.h"
 #include "util.h"
 
@@ -30,12 +28,13 @@
  * programming blocks from the USB chunks, and invokes the programmer passing
  * it the full block.
  *
- * The programmer reports results by putting the return value of one or four
- * bytes into the same buffer where the block was passed in. This wrapper
- * retrieves the programmer's return value, normalizes it to 4 bytes and sends
- * it back to the host.
+ * The programmer reports results by putting the return value into the same
+ * buffer where the block was passed in. This wrapper retrieves the
+ * programmer's return value, and sends it back to the host. The return value
+ * is usually one byte in size, the only exception is the connection
+ * establishment phase where the return value is 16 bytes in size.
  *
- * In the end of the successful image transfer and programming, the host send
+ * In the end of the successful image transfer and programming, the host sends
  * the reset command, and the device reboots itself.
  */
 
@@ -68,26 +67,6 @@ enum rx_state {
 	rx_inside_block,   /* Assembling a block to pass to the programmer. */
 	rx_outside_block,  /* Waiting for the next block to start or for the
 			      reset command. */
-	rx_awaiting_reset /* Waiting for reset confirmation. */
-};
-
-/* This is the format of the header the programmer expects. */
-struct update_command {
-	uint32_t  block_digest;  /* first 4 bytes of sha1 of the rest of the
-				    block. */
-	uint32_t  block_base;    /* Offset of this block into the flash SPI. */
-};
-
-/* This is the format of the header the host uses. */
-struct update_pdu_header {
-	uint32_t block_size;    /* Total size of the block, including this
-				   field. */
-	union {
-		struct update_command cmd;
-		uint32_t resp; /* The programmer puts response to the same
-				  buffer where the command was. */
-	};
-	/* The actual payload goes here. */
 };
 
 enum rx_state rx_state_ = rx_idle;
@@ -96,12 +75,12 @@ static uint32_t block_size;
 static uint32_t block_index;
 
 /*
- * Verify that the contens of the USB rx queue is a valid transfer start
+ * Verify that the contents of the USB rx queue is a valid transfer start
  * message from host, and if so - save its contents in the passed in
- * update_pdu_header structure.
+ * update_frame_header structure.
  */
 static int valid_transfer_start(struct consumer const *consumer, size_t count,
-				struct update_pdu_header *pupdu)
+				struct update_frame_header *pupfr)
 {
 	int i;
 
@@ -112,21 +91,27 @@ static int valid_transfer_start(struct consumer const *consumer, size_t count,
 	 */
 	i = count;
 	while (i > 0) {
-		QUEUE_REMOVE_UNITS(consumer->queue, pupdu,
-				   MIN(i, sizeof(*pupdu)));
-		i -= sizeof(*pupdu);
+		QUEUE_REMOVE_UNITS(consumer->queue, pupfr,
+				   MIN(i, sizeof(*pupfr)));
+		i -= sizeof(*pupfr);
 	}
 
-	if (count != sizeof(struct update_pdu_header)) {
+	if (count != sizeof(struct update_frame_header)) {
 		CPRINTS("FW update: wrong first block, size %d", count);
 		return 0;
 	}
 
-	/* In the first block the payload (updu.cmd) must be all zeros. */
-	for (i = 0; i < sizeof(pupdu->cmd); i++)
-		if (((uint8_t *)&pupdu->cmd)[i])
+	/* In the first block the payload (pupfr->cmd) must be all zeros. */
+	for (i = 0; i < sizeof(pupfr->cmd); i++)
+		if (((uint8_t *)&pupfr->cmd)[i])
 			return 0;
 	return 1;
+}
+
+static int try_vendor_command(struct consumer const *consumer, size_t count)
+{
+	/* TODO(b/36375666): Vendor commands not implemented (yet). */
+	return 0;
 }
 
 /*
@@ -135,14 +120,18 @@ static int valid_transfer_start(struct consumer const *consumer, size_t count,
  */
 static uint64_t prev_activity_timestamp;
 
-#define UPDATE_PROTOCOL_VERSION 2
+/*
+ * A flag indicating that at least one valid PDU containing flash update block
+ * has been received in the current transfer session.
+ */
+static uint8_t  data_was_transferred;
 
 /* Called to deal with data from the host */
 static void update_out_handler(struct consumer const *consumer, size_t count)
 {
-	struct update_pdu_header updu;
+	struct update_frame_header upfr;
 	size_t resp_size;
-	uint32_t resp_value;
+	uint8_t resp_value;
 	uint64_t delta_time;
 
 	/* How much time since the previous USB callback? */
@@ -165,65 +154,50 @@ static void update_out_handler(struct consumer const *consumer, size_t count)
 
 	if (rx_state_ == rx_idle) {
 		/*
-		 * When responding to the very first packet of the update
-		 * sequence, the original implementation was responding with a
-		 * four byte value, just as to any other block of the transfer
-		 * sequence.
+		 * The payload must be an update initiating PDU.
 		 *
-		 * It became clear that there is a need to be able to enhance
-		 * the update protocol, while stayng backwards compatible. To
-		 * achieve that we respond to the very first packet with an 8
-		 * byte value, the first 4 bytes the same as before, the
-		 * second 4 bytes - the protocol version number.
-		 *
-		 * This way if on the host side receiving of a four byte value
-		 * in response to the first packet is an indication of the
-		 * 'legacy' protocol, version 0. Receiving of an 8 byte
-		 * response would communicate the protocol version in the
-		 * second 4 bytes.
+		 * The size of the response returned in the same buffer will
+		 * exceed the received frame size; Let's make sure there is
+		 * enough room for the response in the buffer.
 		 */
-		struct {
-			uint32_t value;
-			uint32_t version;
-		} startup_resp;
+		union {
+			struct update_frame_header upfr;
+			struct {
+				uint32_t unused;
+				struct first_response_pdu startup_resp;
+			};
+		} u;
 
-		if (!valid_transfer_start(consumer, count, &updu))
+		/* Check is this is a channeled TPM extension command. */
+		if (try_vendor_command(consumer, count))
 			return;
 
-		CPRINTS("FW update: starting...");
+		if (!valid_transfer_start(consumer, count, &u.upfr)) {
+			/*
+			 * Something is wrong, this payload is not a valid
+			 * update start PDU. Let'w indicate this by returning
+			 * a single byte error code.
+			 */
+			resp_value = UPDATE_GEN_ERROR;
+			CPRINTS("%s:%d", __FILE__, __LINE__);
+			QUEUE_ADD_UNITS(&update_to_usb, &resp_value, 1);
+			return;
+		}
 
-		fw_update_command_handler(&updu.cmd, count -
-					   offsetof(struct update_pdu_header,
+		CPRINTS("FW update: starting...");
+		fw_update_command_handler(&u.upfr.cmd, count -
+					   offsetof(struct update_frame_header,
 						    cmd),
 					   &resp_size);
 
-		if (resp_size == 4) {
-			/* Already in network order. */
-			startup_resp.value = updu.resp;
-			rx_state_ = rx_outside_block;
-		} else {
-			/* This must be a single byte error code. */
-			startup_resp.value = htobe32(*((uint8_t *)&updu.resp));
+		if (!u.startup_resp.return_value) {
+			rx_state_ = rx_outside_block;  /* We're in business. */
+			data_was_transferred = 0;   /* No data received yet. */
 		}
 
-		startup_resp.version = htobe32(UPDATE_PROTOCOL_VERSION);
-
 		/* Let the host know what updater had to say. */
-		QUEUE_ADD_UNITS(&update_to_usb, &startup_resp,
-				sizeof(startup_resp));
+		QUEUE_ADD_UNITS(&update_to_usb, &u.startup_resp, resp_size);
 		return;
-	}
-
-	if (rx_state_ == rx_awaiting_reset) {
-		/*
-		 * Any USB data received in this state triggers reset, no
-		 * response required.
-		 */
-		CPRINTS("reboot hard");
-		cflush();
-		system_reset(SYSTEM_RESET_HARD);
-		while (1)
-			;
 	}
 
 	if (rx_state_ == rx_outside_block) {
@@ -239,40 +213,48 @@ static void update_out_handler(struct consumer const *consumer, size_t count)
 			command = be32toh(command);
 			if (command == UPDATE_DONE) {
 				CPRINTS("FW update: done");
+
+				if (data_was_transferred) {
+					fw_update_complete();
+					data_was_transferred = 0;
+				}
+
 				resp_value = 0;
-				QUEUE_ADD_UNITS(&update_to_usb, &resp_value,
-						sizeof(resp_value));
-				rx_state_ = rx_awaiting_reset;
+				QUEUE_ADD_UNITS(&update_to_usb,
+						&resp_value, 1);
+				rx_state_ = rx_idle;
 				return;
-			} else {
-				CPRINTS("Unexpected packet command 0x%x",
-					command);
 			}
 		}
 
 		/*
 		 * At this point we expect a block start message. It is
-		 * sizeof(updu) bytes in size, but is not the transfer start
+		 * sizeof(upfr) bytes in size, but is not the transfer start
 		 * message, which also is of that size AND has the command
 		 * field of all zeros.
 		 */
-		if (valid_transfer_start(consumer, count, &updu) ||
-		    (count != sizeof(updu)))
+		if (valid_transfer_start(consumer, count, &upfr) ||
+		    (count != sizeof(upfr))) {
 			/*
 			 * Instead of a block start message we received either
 			 * a transfer start message or a chunk. We must have
 			 * gotten out of sync with the host.
 			 */
+			resp_value = UPDATE_GEN_ERROR;
+			CPRINTS("%s:%d", __FILE__, __LINE__);
+			QUEUE_ADD_UNITS(&update_to_usb, &resp_value, 1);
 			return;
+		}
 
 		/* Let's allocate a large enough buffer. */
-		block_size = be32toh(updu.block_size) -
-			offsetof(struct update_pdu_header, cmd);
+		block_size = be32toh(upfr.block_size) -
+			offsetof(struct update_frame_header, cmd);
 		if (shared_mem_acquire(block_size, (char **)&block_buffer)
 		    != EC_SUCCESS) {
-			/* TODO:(vbendeb) report out of memory here. */
 			CPRINTS("FW update: error: failed to alloc %d bytes.",
 				block_size);
+			resp_value = UPDATE_MALLOC_ERROR;
+			QUEUE_ADD_UNITS(&update_to_usb, &resp_value, 1);
 			return;
 		}
 
@@ -280,9 +262,9 @@ static void update_out_handler(struct consumer const *consumer, size_t count)
 		 * Copy the rest of the message into the block buffer to pass
 		 * to the updater.
 		 */
-		block_index = sizeof(updu) -
-			offsetof(struct update_pdu_header, cmd);
-		memcpy(block_buffer, &updu.cmd, block_index);
+		block_index = sizeof(upfr) -
+			offsetof(struct update_frame_header, cmd);
+		memcpy(block_buffer, &upfr.cmd, block_index);
 		block_size -= block_index;
 		rx_state_ = rx_inside_block;
 		return;
@@ -294,7 +276,7 @@ static void update_out_handler(struct consumer const *consumer, size_t count)
 	block_size -= count;
 
 	if (block_size) {
-		if (count == sizeof(updu)) {
+		if (count == sizeof(upfr)) {
 			/*
 			 * A block header size instead of chunk size message
 			 * has been received. There must have been some packet
@@ -302,14 +284,14 @@ static void update_out_handler(struct consumer const *consumer, size_t count)
 			 *
 			 * Let's copy its contents into the header structure.
 			 */
-			memcpy(&updu, block_buffer + block_index - count,
+			memcpy(&upfr, block_buffer + block_index - count,
 			       count);
 
 
 			/* And re-allocate a large enough buffer. */
 			shared_mem_release(block_buffer);
-			block_size = be32toh(updu.block_size) -
-				offsetof(struct update_pdu_header, cmd);
+			block_size = be32toh(upfr.block_size) -
+				offsetof(struct update_frame_header, cmd);
 			if (shared_mem_acquire(block_size,
 					       (char **)&block_buffer)
 			    != EC_SUCCESS) {
@@ -323,9 +305,9 @@ static void update_out_handler(struct consumer const *consumer, size_t count)
 			 * Copy the rest of the message into the block buffer
 			 * to pass to the updater.
 			 */
-			block_index = sizeof(updu) -
-				offsetof(struct update_pdu_header, cmd);
-			memcpy(block_buffer, &updu.cmd, block_index);
+			block_index = sizeof(upfr) -
+				offsetof(struct update_frame_header, cmd);
+			memcpy(block_buffer, &upfr.cmd, block_index);
 			block_size -= block_index;
 		}
 		return;	/* More to come. */
@@ -337,6 +319,11 @@ static void update_out_handler(struct consumer const *consumer, size_t count)
 	 */
 	fw_update_command_handler(block_buffer, block_index, &resp_size);
 
+	/*
+	 * There was at least an attempt to program the flash, set the
+	 * flag.
+	 */
+	data_was_transferred = 1;
 	resp_value = block_buffer[0];
 	QUEUE_ADD_UNITS(&update_to_usb, &resp_value, sizeof(resp_value));
 	rx_state_ = rx_outside_block;
