7404e4ec083df92d7dec88c131feb26fc1e91125
Nicolas Boichat <drinkcat@google.com>
2017-03-22 11:20:33 +0800

    chip/g/upgrade: Minor formatting fixups
    Also, turn return_value into an anonymous enum: we just use it
    to declare constants.
    
    BRANCH=none
    BUG=b:36375666
    TEST=make buildall -j
    TEST=Copy code to common updater, can update hammer
    
    Change-Id: I921384f92b56811f271f4f1dfac6ec94354832f3

--- chip/g/upgrade_fw.h
+++ chip/g/upgrade_fw.h
@@ -34,12 +34,16 @@
 
 #define UPDATE_PROTOCOL_VERSION 6
 
-/* This is the format of the update PDU header. */
+/*
+ * This is the format of the update PDU header.
+ *
+ * block digest: the first four bytes of the sha1 digest of the rest of the
+ *               structure (can be 0 on boards where digest is ignored).
+ * block_base:   offset of this PDU into the flash SPI.
+ */
 struct update_command {
-	uint32_t  block_digest;  /* first 4 bytes of sha1 of the rest of the
-				  * frame.
-				  */
-	uint32_t  block_base;    /* Offset of this PDU into the flash SPI. */
+	uint32_t  block_digest;
+	uint32_t  block_base;
 	/* The actual payload goes here. */
 } __packed;
 
@@ -54,9 +58,7 @@ struct update_command {
  * cmd field below), and puts its reply into the same buffer the PDU was in.
  */
 struct update_frame_header {
-	uint32_t block_size;    /* Total size of the block, including this
-				 * field.
-				 */
+	uint32_t block_size; /* Total frame size, including this field. */
 	struct update_command cmd;
 };
 
@@ -126,7 +128,7 @@ void fw_update_complete(void);
 int update_pdu_valid(struct update_command *cmd_body, size_t cmd_size);
 
 /* Various update command return values. */
-enum return_value {
+enum {
 	UPDATE_SUCCESS = 0,
 	UPDATE_BAD_ADDR = 1,
 	UPDATE_ERASE_FAILURE = 2,
