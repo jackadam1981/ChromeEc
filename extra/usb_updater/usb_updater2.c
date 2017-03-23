4158e01678202fb152972207da9a4872d9d0c2a4
Nicolas Boichat <drinkcat@google.com>
2017-03-23 15:07:34 +0800

    WIP: usb_updater2: Flush all data from endpoint before trying to update
    Change-Id: I0e0a9a51c88613920079a6f01a60d84e90bae64e

--- extra/usb_updater/usb_updater2.c
+++ extra/usb_updater/usb_updater2.c
@@ -777,6 +777,14 @@ static void setup_connection(struct transfer_descriptor *td)
 
 	if (td->ep_type == usb_xfer) {
 		struct update_frame_header ufh;
+		uint8_t inbuf[td->uep.chunk_len];
+		int actual = 0;
+
+		while (!libusb_bulk_transfer(td->uep.devh, td->uep.ep_num | 0x80,
+						(void *)&inbuf, td->uep.chunk_len,
+						&actual, 10)) {
+			printf("flush\n");
+		}
 
 		memset(&ufh, 0, sizeof(ufh));
 		ufh.block_size = htobe32(sizeof(ufh));
