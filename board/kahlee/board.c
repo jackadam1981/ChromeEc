cf4489cf55c16af12be5d7581318f0457574b94d
Cloud_Lin <cloud_lin@compal.com>
2017-04-06 16:39:25 +0800

    kahlee: Modify PD port2 I2C address
    PD vender suggests to use chip default address setting.
    
    BRANCH=None
    BUG=None
    TEST=Plug in type-c device and check behavior
    
    Change-Id: I44b7156c914b8de67630696e4534147cc3b7f2c3

--- board/kahlee/board.c
+++ board/kahlee/board.c
@@ -178,7 +178,7 @@ const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
 	},
 	[1] = {
 		.i2c_host_port = NPCX_I2C_PORT0_1,
-		.i2c_slave_addr = 0x36,
+		.i2c_slave_addr = 0x16,
 		.drv = &tcpci_tcpm_drv,
 		.pol = TCPC_ALERT_ACTIVE_LOW,
 	},
