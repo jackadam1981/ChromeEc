737bd372acab31a73b7c9be7e80b237d618200b2
Cloud_Lin <cloud_lin@compal.com>
2017-04-06 16:39:25 +0800

    kahlee: Modify PD port2 I2C address and clean up unused function
    1. PD vender suggests to use chip default address setting (0x16).
    2. Clean up board_print_tcpc_fw version function and related define.
    
    BRANCH=None
    BUG=None
    TEST=Plug in type-c device and check behavior
    
    Change-Id: I44b7156c914b8de67630696e4534147cc3b7f2c3

--- include/config.h
+++ include/config.h
@@ -2374,9 +2374,6 @@
 /* USB Device version of product */
 #undef CONFIG_USB_BCD_DEV
 
-/* Support TCPC FW version */
-#undef CONFIG_USB_PD_TCPC_FW_VERSION
-
 /*****************************************************************************/
 
 /* Compile chip support for the USB device controller */
