04adfba7c1f1c22959a591abea35307a4270ff1d
Aseda Aboagye <aaboagye@google.com>
2017-04-06 13:03:06 -0700

    kevin: Change PD max power to 40W.
    BUG=b:35647696
    BRANCH=gru
    TEST=Flash kevin.  Plug in 60W charger like zinger.  Verify that charge
    limit is set to 2000mA @ 20V.  Verify that we still pull 3A @ 5V using
    guppy.
    
    Change-Id: Iaaa681fc0cf9ab8a371b8b69bc5dcc065a42d461
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/kevin/board.h
+++ board/kevin/board.h
@@ -140,7 +140,7 @@
 #endif
 
 #define PD_OPERATING_POWER_MW 15000
-#define PD_MAX_POWER_MW       60000
+#define PD_MAX_POWER_MW       40000
 #define PD_MAX_CURRENT_MA     3000
 #define PD_MAX_VOLTAGE_MV     20000
 
