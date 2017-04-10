b970adac76e71367433f0b8cead524a1e5e6c623
Mulin Chao <mlchao@nuvoton.com>
2017-04-10 12:00:55 +0800

    npcx: fan: Simplified TACH_TO_RPM formula and fixed bugs.
    This CL simplified TACH_TO_RPM formula and abstracted two definitions
    (PULSES_ROUND and RPM_DEVIATION) for the pwm-type fan used on boards.
    The developers can modify them in board-level driver if fan doesn't meet
    the default spec.
    
    In this CL, it also fixed:
    1. Declare rpm_pre as array if FAN_CH_COUNT > 1.
    2. Add checking for the value of next duty of pwm.
    
    BRANCH=none
    BUG=none
    TEST=test fan used in kahlee and Sunon fan with fanset command on
    npcx_evb and use faninfo for verifying. Measure the actual rpm by scope.
    
    Change-Id: Ieb07482eb359912286414ccb9738341d98ea99e4
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

--- chip/npcx/fan.c
+++ chip/npcx/fan.c
@@ -73,29 +73,38 @@ struct fan_status_t {
 
 /* Global variables */
 static volatile struct fan_status_t fan_status[FAN_CH_COUNT];
+static int rpm_pre[FAN_CH_COUNT];
 
-/* Fan encoder spec. */
-#define RPM_SCALE    1 /* Fan RPM is multiplier of actual RPM */
-#define RPM_EDGES    1 /* Fan number of edges - 1 */
-#define POLES        2 /* Pole number of fan */
-/* Rounds per second */
-#define ROUNDS ((60 / POLES) * RPM_EDGES * RPM_SCALE)
 /*
- * RPM = (n - 1) * m * f * 60 / poles / TACH
- *   n = Fan number of edges = (RPM_EDGES + 1)
- *   m = Fan multiplier defined by RANGE
- *   f = PWM and MFT operation freq
- *   poles = 2
+ * Fan specifications. If they (PULSES_ROUND and RPM_DEVIATION) cannot meet
+ * the followings, please replace them with correct one in board-level driver.
+ */
+
+/* Pulses per round */
+#ifndef PULSES_ROUND
+#define PULSES_ROUND 2 /* 4-phases pwm-type fan. (2-phases should be 1) */
+#endif
+
+/* Rpm deviation (Unit:percent) */
+#ifndef RPM_DEVIATION
+#define RPM_DEVIATION 7
+#endif
+
+/*
+ * RPM = 60 * f / (n * TACH)
+ *   n = Pulses per round
+ *   f = Tachometer (MFT) operation freq
+ *   TACH = Counts of tachometer
  */
 #define TACH_TO_RPM(ch, tach) \
-	((fan_status[ch].mft_freq * ROUNDS) / MAX((tach), 1))
+	((fan_status[ch].mft_freq * 60 / PULSES_ROUND) / MAX((tach), 1))
 
 /* MFT TCNT default count */
-#define TACHO_MAX_CNT ((1<<16)-1)
+#define TACHO_MAX_CNT ((1 << 16) - 1)
 
-/* Smart fan control settings */
-#define RPM_MARGIN_PERCENT 3
-#define RPM_MARGIN_THRES(rpm_target) (((rpm_target)*RPM_MARGIN_PERCENT)/100)
+/* Margin of target rpm */
+#define RPM_MARGIN(rpm_target) (((rpm_target) * RPM_DEVIATION) / 100)
+
 /**
  * MFT get fan rpm value
  *
@@ -233,30 +242,29 @@ static void fan_config(int ch, int enable_mft_read_rpm)
  */
 enum fan_status fan_smart_control(int ch, int rpm_actual, int rpm_target)
 {
-	static int rpm_pre;
-	int duty, duty_diff, rpm_diff;
+	int duty, duty_step, rpm_diff;
 
 	/* wait rpm is stable */
-	if (ABS(rpm_actual - rpm_pre) > RPM_MARGIN_THRES(rpm_target)/2) {
-		rpm_pre = rpm_actual;
+	if (ABS(rpm_actual - rpm_pre[ch]) > RPM_MARGIN(rpm_actual)) {
+		rpm_pre[ch] = rpm_actual;
 		return FAN_STATUS_CHANGING;
 	}
 
 	/* Record previous rpm */
-	rpm_pre = rpm_actual;
+	rpm_pre[ch] = rpm_actual;
 
 	/* Find suitable duty step */
 	rpm_diff = rpm_target - rpm_actual;
 	if (ABS(rpm_diff) >= 2000)
-		duty_diff = 20;
+		duty_step = 20;
 	else if (ABS(rpm_diff) >= 1000)
-		duty_diff = 10;
+		duty_step = 10;
 	else if (ABS(rpm_diff) >= 500)
-		duty_diff = 5;
+		duty_step = 5;
 	else if (ABS(rpm_diff) >= 250)
-		duty_diff = 3;
+		duty_step = 3;
 	else
-		duty_diff = 1;
+		duty_step = 1;
 
 	/* Adjust PWM duty */
 	duty = fan_get_duty(ch);
@@ -264,22 +272,24 @@ enum fan_status fan_smart_control(int ch, int rpm_actual, int rpm_target)
 		return FAN_STATUS_STOPPED;
 
 	/* Increase PWM duty */
-	if (rpm_diff > RPM_MARGIN_THRES(rpm_target)) {
+	if (rpm_diff > RPM_MARGIN(rpm_target)) {
 		if (duty == 100)
 			return FAN_STATUS_FRUSTRATED;
 
-		fan_set_duty(ch, duty + duty_diff);
+		duty = (duty + duty_step) < 100 ? (duty + duty_step) : 100;
+		fan_set_duty(ch, duty);
 		CPRINTS("duty=%d, threshold %d, rpm target %d, actual %d", duty,
-			RPM_MARGIN_THRES(rpm_target), rpm_target, rpm_actual);
+			RPM_MARGIN(rpm_target), rpm_target, rpm_actual);
 		return FAN_STATUS_CHANGING;
 	/* Decrease PWM duty */
-	} else if (rpm_diff < -RPM_MARGIN_THRES(rpm_target)) {
+	} else if (rpm_diff < -RPM_MARGIN(rpm_target)) {
 		if (duty == 1 && rpm_target != 0)
 			return FAN_STATUS_FRUSTRATED;
 
-		fan_set_duty(ch, duty - duty_diff);
+		duty = (duty - duty_step) > 1 ? (duty - duty_step) : 1;
+		fan_set_duty(ch, duty);
 		CPRINTS("duty=%d, threshold %d, rpm target %d, actual %d", duty,
-			RPM_MARGIN_THRES(rpm_target), rpm_target, rpm_actual);
+			RPM_MARGIN(rpm_target), rpm_target, rpm_actual);
 		return FAN_STATUS_CHANGING;
 	}
 
