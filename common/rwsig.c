25f292204410be901fc81e2540ed0e5e28252551
Nicolas Boichat <drinkcat@google.com>
2017-04-11 11:28:52 +0800

    TEST-ONLY: Add rwsig console command to debug rwsig task.
    Also increase jump delay to 5s and add 5s delay in verification
    process itself, to make sure that we can test all cases.
    
    BRANCH=none
    BUG=b:35587171
    TEST=Flash hammer, board boots to RW after ~10s
         'rwsig abort' aborts the process
         'rwsig continue' makes the task jump to RW immediately
    
    Change-Id: Ibd5bf0d4e615e58a534b248d0367ff47e28bfb83

--- common/rwsig.c
+++ common/rwsig.c
@@ -190,6 +190,7 @@ int rwsig_check_signature(void)
 	rollback_lock();
 #endif
 out:
+	msleep(5000);
 	CPRINTS("RW verify %s", good ? "OK" : "FAILED");
 
 	if (!good) {
@@ -248,4 +249,24 @@ void rwsig_task(void)
 	else
 		rwsig_status = RWSIG_ABORTED;
 }
+
+static int command_rwsig(int argc, char **argv)
+{
+	if (argc < 2 || argv[1][0] == 's') {
+		ccprintf("status: %d\n", rwsig_status);
+		return EC_SUCCESS;
+	}
+
+	if (argv[1][0] == 'a')
+		rwsig_abort();
+	else if (argv[1][0] == 'c')
+		rwsig_continue();
+	else
+		return EC_ERROR_PARAM1;
+
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(rwsig, command_rwsig,
+			"[status] [abort|continue]",
+			"");
 #endif
