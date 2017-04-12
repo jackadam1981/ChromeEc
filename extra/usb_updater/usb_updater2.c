7d656465f602f77aa9aa84ed763d0392edc42185
Nicolas Boichat <drinkcat@google.com>
2017-04-12 16:09:04 +0800

    WIP: usb_updater2: Add support for extra commands
    BRANCH=none
    BUG=b:35587170
    TEST=cd extra/usb_updater; make
         # Jump to RW
         sudo ./usb_updater2 -j
         sleep 0.5
         # Update RO, then reboot
         sudo ./usb_updater2 ../../build/hammer/ec.bin
         sleep 0.5
         # Update RW (first tell RO to not jump to RW)
         sudo ./usb_updater2 -s
         sudo ./usb_updater2 ../../build/hammer/ec.bin
    TEST=cd extra/usb_updater; make
         # Tell RW to jump back to RO
         sudo ./usb_updater2 -w
         sleep 0.5
         # Update RW, then reboot
         sudo ./usb_updater2 -s
         sudo ./usb_updater2 ../../build/hammer/ec.bin
    
    Change-Id: I04d89c9ecc11b92437e5ba201a219389f6520dd8

--- extra/usb_updater/usb_updater2.c
+++ extra/usb_updater/usb_updater2.c
@@ -203,14 +203,18 @@ struct transfer_descriptor {
 
 static uint32_t protocol_version;
 static char *progname;
-static char *short_opts = "bd:fhu";
+static char *short_opts = "bd:fhjrsuw";
 static const struct option long_opts[] = {
 	/* name    hasarg *flag val */
 	{"binvers",	1,   NULL, 'b'},
 	{"device",	1,   NULL, 'd'},
 	{"fwver",	0,   NULL, 'f'},
 	{"help",	0,   NULL, 'h'},
-	{"upstart",	0,   NULL, 'u'},
+	{"jump_to_rw",	0,   NULL, 'j'},
+	{"reboot",	0,   NULL, 'r'},
+	{"stay_in_ro",	0,   NULL, 's'},
+	{"unlock_rollback",	0,   NULL, 'u'},
+	{"unlock_rw",	0,   NULL, 'w'},
 	{},
 };
 
@@ -236,8 +240,11 @@ static void usage(int errs)
 	       "  -d,--device  VID:PID     USB device (default %04x:%04x)\n"
 	       "  -f,--fwver               Report running firmware versions.\n"
 	       "  -h,--help                Show this message\n"
-	       "  -u,--upstart             "
-			"Upstart mode (strict header checks)\n"
+	       "  -j,--jump_to_rw          Tell EC to jump to RW\n"
+	       "  -r,--reboot              Tell EC to reboot\n"
+	       "  -s,--stay_in_ro          Tell EC to stay in RO\n"
+	       "  -u,--unlock_rollback     Tell EC to unlock the rollback region\n"
+	       "  -w,--unlock_rw           Tell EC to unlock the RW region\n"
 	       "\n", progname, VID, PID);
 
 	exit(errs ? update_error : noop);
@@ -745,12 +752,13 @@ static int ext_cmd_over_usb(struct usb_endpoint *uep, uint16_t subcommand,
 			    void *cmd_body, size_t body_size,
 			    void *resp, size_t *resp_size)
 {
-#if 0
 	struct update_frame_header *ufh;
 	uint16_t *frame_ptr;
 	size_t usb_msg_size;
+#if 0
 	SHA_CTX ctx;
-	uint8_t digest[SHA_DIGEST_LENGTH];
+#endif
+	uint8_t digest[SHA_DIGEST_LENGTH] = { 0 };
 
 	usb_msg_size = sizeof(struct update_frame_header) +
 		sizeof(subcommand) + body_size;
@@ -763,24 +771,25 @@ static int ext_cmd_over_usb(struct usb_endpoint *uep, uint16_t subcommand,
 	}
 
 	ufh->block_size = htobe32(usb_msg_size);
-	ufh->cmd.block_base = htobe32(CONFIG_EXTENSION_COMMAND);
+	ufh->cmd.block_base = htobe32(UPDATE_EXTRA_CMD);
 	frame_ptr = (uint16_t *)(ufh + 1);
 	*frame_ptr = htobe16(subcommand);
 
 	if (body_size)
 		memcpy(frame_ptr + 1, cmd_body, body_size);
 
+#if 0
 	/* Calculate the digest. */
 	SHA1_Init(&ctx);
 	SHA1_Update(&ctx, &ufh->cmd.block_base,
 		    usb_msg_size -
 		    offsetof(struct update_frame_header, cmd.block_base));
 	SHA1_Final(digest, &ctx);
+#endif
 	memcpy(&ufh->cmd.block_digest, digest, sizeof(ufh->cmd.block_digest));
 	xfer(uep, ufh, usb_msg_size, resp, resp_size ? *resp_size : 0);
 
 	free(ufh);
-#endif
 	return 0;
 }
 
@@ -797,6 +806,20 @@ static void send_done(struct usb_endpoint *uep)
 	/* Send stop request, ignoring reply. */
 	out = htobe32(UPDATE_DONE);
 	xfer(uep, &out, sizeof(out), &out, 1);
+}
+
+static void send_subcommand(struct transfer_descriptor *td, uint16_t subcommand)
+{
+	if (td->ep_type == usb_xfer) {
+		send_done(&td->uep);
+
+		if (protocol_version > 5) {
+			ext_cmd_over_usb(&td->uep, subcommand,
+					 NULL, 0,
+					 NULL, 0);
+			printf("sent command %x\n", subcommand);
+		}
+	}
 }
 
 /* Returns number of successfully transmitted image sections. */
@@ -863,7 +886,7 @@ static void generate_reset_request(struct transfer_descriptor *td)
 	/* Most common case. */
 	command_body_size = 0;
 	response_size = 1;
-	subcommand = 0xabcd; /* FIXME */
+	subcommand = UPDATE_EXTRA_CMD_IMMEDIATE_RESET;
 	if (td->ep_type == usb_xfer) {
 		ext_cmd_over_usb(&td->uep, subcommand,
 				 command_body, command_body_size,
@@ -893,6 +916,7 @@ int main(int argc, char *argv[])
 	int transferred_sections = 0;
 	int binary_vers = 0;
 	int show_fw_ver = 0;
+	int extra_command = -1;
 
 	progname = strrchr(argv[0], '/');
 	if (progname)
@@ -923,8 +947,20 @@ int main(int argc, char *argv[])
 		case 'h':
 			usage(errorcnt);
 			break;
+		case 'j':
+			extra_command = UPDATE_EXTRA_CMD_JUMP_TO_RW;
+			break;
+		case 'r':
+			extra_command = UPDATE_EXTRA_CMD_IMMEDIATE_RESET;
+			break;
+		case 's':
+			extra_command = UPDATE_EXTRA_CMD_STAY_IN_RO;
+			break;
 		case 'u':
-			td.upstart_mode = 1;
+			extra_command = UPDATE_EXTRA_CMD_UNLOCK_ROLLBACK;
+			break;
+		case 'w':
+			extra_command = UPDATE_EXTRA_CMD_UNLOCK_RW;
 			break;
 		case 0:				/* auto-handled option */
 			break;
@@ -949,7 +985,7 @@ int main(int argc, char *argv[])
 	if (errorcnt)
 		usage(errorcnt);
 
-	if (!show_fw_ver) {
+	if (!show_fw_ver && extra_command == -1) {
 		if (optind >= argc) {
 			fprintf(stderr,
 				"\nERROR: Missing required <binary image>\n\n");
@@ -996,7 +1032,8 @@ int main(int argc, char *argv[])
 
 		if (transferred_sections && !td.upstart_mode)
 			generate_reset_request(&td);
-	}
+	} else if (extra_command > -1)
+		send_subcommand(&td, extra_command);
 
 	if (td.ep_type == usb_xfer) {
 		libusb_close(td.uep.devh);
