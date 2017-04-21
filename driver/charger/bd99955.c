e6f263c7cd5ac24c1c0f4795f2696a20ded402af
james_chao <james_chao@asus.com>
2017-04-22 00:02:31 +0800

    BD99955: do not set charge current to 0 when charging
    Rohm said: If charging current of battery command in address 14h set to
    0mA during charging, reference of charge current feedback amp.
    (VREF_CHG) is set 0V. Then DCDC may stop switching. (Because of the EA
    offset)
    
    So make sure disable charging before set charge current to zero, and
    set charge current = 128 before enable charging.
    
    BUG=b:37415851
    BRANCH=gru
    TEST=test 'ectool chargecontrol normal/idle/discharge' are working
    
    Change-Id: Id31876afe365a476fb906e059ab519b7a0c9a7c6
    Signed-off-by: james_chao <james_chao@asus.com>

--- driver/charger/bd99955.c
+++ driver/charger/bd99955.c
@@ -122,9 +122,17 @@ static int bd99955_charger_enable(int enable)
 	if (rv)
 		return rv;
 
-	if (enable)
+	if (enable) {
+		/*
+		 * make sure the charge current > 0 before
+		 * enable charging
+		 */
+		rv = charger_set_current(
+			bd99955_charger_info.current_min);
+		if (rv)
+			return rv;
 		reg |= BD99955_CMD_CHGOP_SET2_CHG_EN;
-	else
+	} else
 		reg &= ~BD99955_CMD_CHGOP_SET2_CHG_EN;
 
 	return ch_raw_write16(BD99955_CMD_CHGOP_SET2, reg,
@@ -595,6 +603,13 @@ int charger_set_current(int current)
 	else if (current < bd99955_charger_info.current_min &&
 		!(charge_get_flags() & CHARGE_FLAG_FORCE_IDLE))
 		current = bd99955_charger_info.current_min;
+
+	if (current == 0) {
+		/* disable charge before set charge current to 0 */
+		rv = bd99955_charger_enable(0);
+		if (rv)
+			return rv;
+	}
 
 	rv = ch_raw_write16(BD99955_CMD_CHG_CURRENT, current,
 			    BD99955_BAT_CHG_COMMAND);
