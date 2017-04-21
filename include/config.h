a65604700eb7f6fcf8676acfb95cc1b32df6c355
Daisuke Nojiri <dnojiri@chromium.org>
2017-04-21 16:13:24 -0700

    power_button: Allow PB to be idle at power-on
    This changes adds CONFIG_POWER_BUTTON_INIT_IDLE. When it's set,
    the system starts with the power button state idle. It means
    when the board boots from cold state, it stays at G3.
    
    BUG=b:37536389
    BRANCH=none
    TEST=Fizz
    
    Change-Id: I09a62a69d9f201b2dc261838cc9b4425fe3a8dc1
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1740,6 +1740,9 @@
 /* Support sending the power button signal to x86 chipsets */
 #undef CONFIG_POWER_BUTTON_X86
 
+/* Set power button state idle at init */
+#undef CONFIG_POWER_BUTTON_INIT_IDLE
+
 /* Compile common code for AP power state machine */
 #undef CONFIG_POWER_COMMON
 
