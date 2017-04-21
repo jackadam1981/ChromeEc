3d9ddc4d9a1f8f6793210e255dd63e1c5cb66827
Daisuke Nojiri <dnojiri@chromium.org>
2017-04-20 17:44:38 -0700

    Fizz: Add recovery button
    BUG=none
    BRANCH=none
    TEST=Press recovery & power to enter recovery mode
    
    Change-Id: Ia413ffce84d67b6f24f983ccce8ae8277452ac2c
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/board.h
+++ board/fizz/board.h
@@ -17,6 +17,7 @@
 /* EC */
 #define CONFIG_ADC
 #define CONFIG_BOARD_VERSION
+#define CONFIG_BUTTON_COUNT 1
 #define CONFIG_BUTTON_RECOVERY
 #define CONFIG_CASE_CLOSED_DEBUG_EXTERNAL
 #define CONFIG_DPTF
@@ -167,6 +168,11 @@ enum adc_channel {
 	ADC_VBUS,
 	ADC_AMON_BMON,
 	ADC_CH_COUNT
+};
+
+enum button {
+	BUTTON_RECOVERY = 0,
+	BUTTON_COUNT
 };
 
 /* TODO(crosbug.com/p/61098): Verify the numbers below. */
