459c72519948d8ab70ba1ccf3463277c95f405c8
Furquan Shaikh <furquan@chromium.org>
2017-04-25 16:47:34 -0700

    common/button: Blink LED for 3 seconds when setting recovery HW_REINIT
    If user holds down volume up, volume down and power button for 30
    seconds, set HW_REINIT event and blink LED for 3 seconds to indicate
    to user that the request was accepted.
    
    BUG=b:37682514
    BRANCH=None
    TEST=make -j buildall
    
    Change-Id: I6b70d56f50d0a1cfae3fa7f337a34ac487943775
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>

--- common/button.c
+++ common/button.c
@@ -12,6 +12,7 @@
 #include "host_command.h"
 #include "hooks.h"
 #include "keyboard_protocol.h"
+#include "led_common.h"
 #include "power_button.h"
 #include "system.h"
 #include "timer.h"
@@ -59,6 +60,14 @@ static int raw_button_pressed(const struct button_config *button)
 }
 
 #ifdef CONFIG_BUTTON_RECOVERY
+void __attribute__((weak)) led_recovery_hw_reinit_control(int state)
+{
+	/*
+	 * Default weak implementation that does nothing. This can be used by
+	 * the boards that do not have a LED to support blinking.
+	 */
+}
+
 /*
  * If the EC is reset and recovery is requested, then check if HW_REINIT is
  * requested as well. Since the EC reset occurs after volup+voldn+power buttons
@@ -70,6 +79,7 @@ static int raw_button_pressed(const struct button_config *button)
 static void button_check_hw_reinit_required(void)
 {
 	timestamp_t deadline;
+	int led_state = LED_STATE_ON;
 	timestamp_t now = get_time();
 
 	deadline.val = now.val + (20 * SECOND);
@@ -89,6 +99,21 @@ static void button_check_hw_reinit_required(void)
 
 	CPRINTS("HW_REINIT requested");
 	host_set_single_event(EC_HOST_EVENT_KEYBOARD_RECOVERY_HW_REINIT);
+
+	/* Blink LED for 3 seconds. */
+	now = get_time();
+	deadline.val = now.val + (3 * SECOND);
+
+	while (!timestamp_expired(deadline, &now)) {
+		led_control(EC_LED_ID_RECOVERY_HW_REINIT_LED, led_state);
+		led_state = !led_state;
+		watchdog_reload();
+		msleep(100);
+		now = get_time();
+	}
+
+	/* Reset LED to default state. */
+	led_recovery_hw_reinit_control(LED_STATE_RESET);
 }
 #endif
 
