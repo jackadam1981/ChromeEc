d6ab953e54892ea09c025e43bffe10783c757461
Archana Patni <archana.patni@intel.com>
2017-04-25 18:37:22 +0530

    tablet_mode: add tablet_mode console command
    This patch adds a uart console command to simulate tablet_mode
    switch for debugging
    
    BRANCH=none
    BUG=b:37223093
    TEST=run command and check if event is generated
    
    Signed-off-by: Subramony Sesha <subramony.sesha@intel.com>
    Signed-off-by: Archana Patni <archana.patni@intel.com>
    Signed-off-by: Jenny TC <jenny.tc@intel.com>
    
    Change-Id: Ie4a487ebd8929f0584da0b59c094f9d9aff13fb7

--- common/tablet_mode.c
+++ common/tablet_mode.c
@@ -4,6 +4,9 @@
  */
 
 #include "hooks.h"
+#include "host_command.h"
+#include "console.h"
+#include "util.h"
 
 /* Return 1 if in tablet mode, 0 otherwise */
 static int tablet_mode = 1;
@@ -20,4 +23,21 @@ void tablet_set_mode(int mode)
 		hook_notify(HOOK_TABLET_MODE_CHANGE);
 	}
 }
+
+static int command_tablet_mode(int argc, char **argv)
+{
+	int mode = 1;
+
+	if (argc > 1 && !strcasecmp(argv[1], "on"))
+		mode = 1;
+	else if (argc > 1 && !strcasecmp(argv[1], "off"))
+		mode = 0;
+	host_set_single_event(EC_HOST_EVENT_MODE_CHANGE);
+	tablet_set_mode(mode);
+	return EC_SUCCESS;
+}
+
+DECLARE_CONSOLE_COMMAND(tablet_mode, command_tablet_mode,
+			"[on | off]",
+			"Simulate tablet mode");
 
