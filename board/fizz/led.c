8c4c4f592ccd6c82995f154272048f4a75be1dce
Daisuke Nojiri <dnojiri@chromium.org>
2017-04-24 17:04:36 -0700

    Fizz: Add LED control
    This patch adds code to control the power LED.
    
    BUG=b:37646390
    BRANCH=none
    TEST=Powered on Fizz.
    
    Change-Id: I1d7940d9bb4414d97c541ead802efeb8f279533e
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

---
+++ board/fizz/led.c
@@ -0,0 +1,114 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Power and battery LED control for Reef
+ */
+
+#include "chipset.h"
+#include "console.h"
+#include "ec_commands.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "led_common.h"
+#include "util.h"
+
+static int led_debug;
+
+enum led_color {
+	LED_OFF = 0,
+	LED_RED,
+	LED_GREEN,
+	LED_BLUE,
+	LED_WHITE,
+	LED_AMBER,
+
+	/* Number of colors, not a color itself */
+	LED_COLOR_COUNT
+};
+
+static int led_set_color_power(enum led_color color)
+{
+	switch (color) {
+	case LED_OFF:
+		gpio_set_level(GPIO_PWR_GRN_LED, 0);
+		gpio_set_level(GPIO_PWR_RED_LED, 0);
+		break;
+	case LED_GREEN:
+		gpio_set_level(GPIO_PWR_GRN_LED, 1);
+		gpio_set_level(GPIO_PWR_RED_LED, 0);
+		break;
+	case LED_RED:
+		gpio_set_level(GPIO_PWR_GRN_LED, 0);
+		gpio_set_level(GPIO_PWR_RED_LED, 1);
+		break;
+	case LED_AMBER:
+		gpio_set_level(GPIO_PWR_GRN_LED, 1);
+		gpio_set_level(GPIO_PWR_RED_LED, 1);
+		break;
+	default:
+		return EC_ERROR_UNKNOWN;
+	}
+	return EC_SUCCESS;
+}
+
+static int led_set_color(enum ec_led_id id, enum led_color color)
+{
+	switch (id) {
+	case EC_LED_ID_POWER_LED:
+		return led_set_color_power(color);
+	default:
+		return EC_ERROR_UNKNOWN;
+	}
+}
+
+static void led_set_power(void)
+{
+	if (chipset_in_state(CHIPSET_STATE_ON)) {
+		led_set_color(EC_LED_ID_POWER_LED, LED_GREEN);
+		return;
+	}
+}
+
+/**
+ * Called by hook task every 250 ms
+ */
+static void led_tick(void)
+{
+	if (led_debug)
+		return;
+	led_set_power();
+}
+DECLARE_HOOK(HOOK_TICK, led_tick, HOOK_PRIO_DEFAULT);
+
+static int command_led(int argc, char **argv)
+{
+	enum ec_led_id id = EC_LED_ID_POWER_LED;
+
+	if (argc < 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	if (!strcasecmp(argv[1], "debug")) {
+		led_debug ^= 1;
+		ccprintf("led_debug %s\n", led_debug ? "on" : "off");
+	} else if (!strcasecmp(argv[1], "off")) {
+		led_set_color(id, LED_OFF);
+	} else if (!strcasecmp(argv[1], "red")) {
+		led_set_color(id, LED_RED);
+	} else if (!strcasecmp(argv[1], "green")) {
+		led_set_color(id, LED_GREEN);
+	} else if (!strcasecmp(argv[1], "blue")) {
+		led_set_color(id, LED_BLUE);
+	} else if (!strcasecmp(argv[1], "white")) {
+		led_set_color(id, LED_WHITE);
+	} else if (!strcasecmp(argv[1], "amber")) {
+		led_set_color(id, LED_AMBER);
+	} else {
+		/* maybe handle charger_discharge_on_ac() too? */
+		return EC_ERROR_PARAM1;
+	}
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(led, command_led,
+			"[debug|red|green|off <0|1>]",
+			"Turn on/off LED");
