68e06189abfa588b400a5dd6602ad35d18e6dad9
Nick Vaccaro <nvaccaro@chromium.org>
2017-04-25 19:09:03 -0700

    common: sensors: add extra sensor attributes
    Adds min_frequency, max_frequency, and fifo_max_event_count to
    struct motion_sensor_t.
    
    New attributes min_frequency, max_frequency, and fifo_max_event_count
    are now returned in ectool's MOTIONSENSE_CMD_INFO response.
    
    Incremented ectool's MOTIONSENSE_CMD_INFO version to version 3.
    
    Add constants for MIN_FREQUENCY, MAX_FREQUENCY, and
    MAX_FIFO_EVENT_COUNT to each sensor's header file.
    
    BUG=chromium:615059
    TEST=build/boot and verify MOTIONSENSE_CMD_INFO response on caroline
    
    Change-Id: I66db9715c122ef6bb4665ad5d086a9ecc9c7c93a

--- board/caroline/board.c
+++ board/caroline/board.c
@@ -501,6 +501,9 @@ struct motion_sensor_t motion_sensors[] = {
 	 .addr = BMA2x2_I2C_ADDR1,
 	 .rot_standard_ref = &lid_standard_ref,
 	 .default_range = 8, /* g, to support tablet mode */
+	 .min_frequency = BMA255_ACCEL_MIN_FREQ,
+	 .max_frequency = BMA255_ACCEL_MAX_FREQ,
+	 .fifo_max_event_count = CONFIG_ACCEL_FIFO,
 	 .config = {
 		/* AP: by default use EC settings */
 		[SENSOR_CONFIG_AP] = {
@@ -537,6 +540,9 @@ struct motion_sensor_t motion_sensors[] = {
 	 .addr = BMI160_ADDR0,
 	 .rot_standard_ref = &base_standard_ref,
 	 .default_range = 8, /* g, to support tablet mode  */
+	 .min_frequency = BMI160_ACCEL_MIN_FREQ,
+	 .max_frequency = BMI160_ACCEL_MAX_FREQ,
+	 .fifo_max_event_count = CONFIG_ACCEL_FIFO,
 	 .config = {
 		 /* AP: by default use EC settings */
 		 [SENSOR_CONFIG_AP] = {
@@ -573,6 +579,9 @@ struct motion_sensor_t motion_sensors[] = {
 	 .addr = BMI160_ADDR0,
 	 .default_range = 1000, /* dps */
 	 .rot_standard_ref = &base_standard_ref,
+	 .min_frequency = BMI160_GYRO_MIN_FREQ,
+	 .max_frequency = BMI160_GYRO_MAX_FREQ,
+	 .fifo_max_event_count = CONFIG_ACCEL_FIFO,
 	 .config = {
 		 /* AP: by default shutdown all sensors */
 		 [SENSOR_CONFIG_AP] = {
