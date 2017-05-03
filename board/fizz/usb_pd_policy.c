f012ca97589e7c570814aba97870a3ed056c587b
Daisuke Nojiri <dnojiri@chromium.org>
2017-05-03 13:44:18 -0700

    Fizz: Set up charge suppliers at boot
    Fizz has two power sources: barrel jack and type-c port. It
    selects a power source at boot and does not dynamicall switch
    to the other ports after that.
    
    Fizz initializes all power suppliers of all ports to zero then
    initialize the source supplier (barrel jack or type-c port).
    
    When both sources are provided, it prefers a barrel jack. This
    detection is done by reading the voltage on PPVAR_PWR_IN.
    
    If barrel jack is detected as a sink, type-c port works as a
    source only. If type-c port is detected as a sink, type-c
    port works as a sink only.
    
    BUG=none
    BRANCH=none
    TEST=Boot on both type-c & barrel jack.
    
    Change-Id: If4f5ff0c6019d06ac9dacb5dd365f5aa96bffef3
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/usb_pd_policy.c
+++ board/fizz/usb_pd_policy.c
@@ -3,6 +3,7 @@
  * found in the LICENSE file.
  */
 
+#include "adc.h"
 #include "atomic.h"
 #include "extpower.h"
 #include "charge_manager.h"
@@ -55,19 +56,29 @@ void pd_transition_voltage(int idx)
 	/* No-operation: we are always 5V */
 }
 
-static uint8_t vbus_en;
+/* Indicates vbus is currently providing current to an external device. */
+static uint8_t vbus_source_en[CHARGE_PORT_COUNT];
 
 int board_vbus_source_enabled(int port)
 {
-	return vbus_en;
+	return vbus_source_en[port];
+}
+
+static void board_vbus_source_enable(int enable)
+{
+	/* Port = 0 is assumed because the baord has only one type-c port.
+	 * Barrel jack port stays disabled. */
+	gpio_set_level(GPIO_USB_C0_5V_EN, enable);
+	vbus_source_en[CHARGE_PORT_TYPEC0] = enable;
 }
 
 int pd_set_power_supply_ready(int port)
 {
 	/* Disable charging */
 	gpio_set_level(GPIO_USB_C0_CHARGE_L, 1);
-	/* Provide VBUS */
-	gpio_set_level(GPIO_USB_C0_5V_EN, 1);
+
+	/* Enable VBUS source */
+	board_vbus_source_enable(1);
 
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
@@ -77,8 +88,8 @@ int pd_set_power_supply_ready(int port)
 
 void pd_power_supply_reset(int port)
 {
-	/* Disable VBUS */
-	gpio_set_level(GPIO_USB_C0_5V_EN, 0);
+	/* Disable VBUS source */
+	board_vbus_source_enable(0);
 
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
@@ -95,6 +106,7 @@ void pd_set_input_current_limit(int port, uint32_t max_ma,
 #ifdef CONFIG_CHARGE_MANAGER
 	struct charge_port_info charge;
 
+	CPRINTS("%s: p%d %dma %dmv", __func__, port, max_ma, supply_voltage);
 	charge.current = max_ma;
 	charge.voltage = supply_voltage;
 	charge_manager_update_charge(CHARGE_SUPPLIER_PD, port, &charge);
@@ -107,6 +119,7 @@ void typec_set_input_current_limit(int port, uint32_t max_ma,
 #ifdef CONFIG_CHARGE_MANAGER
 	struct charge_port_info charge;
 
+	CPRINTS("%s: p%d %dma %dmv", __func__, port, max_ma, supply_voltage);
 	charge.current = max_ma;
 	charge.voltage = supply_voltage;
 	charge_manager_update_charge(CHARGE_SUPPLIER_TYPEC, port, &charge);
@@ -242,6 +255,43 @@ int pd_custom_vdm(int port, int cnt, uint32_t *payload,
 
 	return 0;
 }
+
+static void board_charge_manager_init(void)
+{
+	int input_voltage;
+	int i, j;
+	struct charge_port_info cpi = {
+		.voltage = USB_CHARGER_VOLTAGE_MV,
+		.current = 0,
+	};
+
+	/* Initialize all charge suppliers to 0 */
+	for (i = 0; i < CHARGE_PORT_COUNT; i++) {
+		for (j = 0; j < CHARGE_SUPPLIER_COUNT; j++)
+			charge_manager_update_charge(j, i, &cpi);
+	}
+
+	input_voltage = adc_read_channel(ADC_VBUS);
+
+	/* Initialize the power source supplier */
+	if (input_voltage > 5500) {
+		/* Power source is barrel jack */
+		CPRINTF("Source: BJ (%dmV)\n", input_voltage);
+		cpi.voltage = input_voltage;
+		cpi.current = 3330;	/* TODO: Set right value */
+		charge_manager_update_charge(CHARGE_SUPPLIER_PROPRIETARY, 1,
+					     &cpi);
+		/* Source only. Disable PD negotiation as a sink */
+	} else {
+		/* Power source is usb-c */
+		CPRINTF("Source: C0 (%dmV)\n", input_voltage);
+		cpi.voltage = input_voltage;
+		cpi.current = 3000;	/* TODO: Set right value */
+		charge_manager_update_charge(CHARGE_SUPPLIER_TYPEC, 0, &cpi);
+		/* Sink only. Disable PD negotiation as a source */
+	}
+}
+DECLARE_HOOK(HOOK_INIT, board_charge_manager_init, HOOK_PRIO_INIT_ADC + 1);
 
 #ifdef CONFIG_USB_PD_ALT_MODE_DFP
 static int dp_flags[CONFIG_USB_PD_PORT_COUNT];
