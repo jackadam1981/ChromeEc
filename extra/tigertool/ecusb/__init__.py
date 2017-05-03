e39c13ecc5e864df3e62efddf71b1ae50d34efc8
Nick Sanders <nsanders@chromium.org>
2017-05-02 21:31:10 -0700

    tigertail: tigertool command line api
    This tool allows an easy commandline interface to set the
    USB-C mux position, as well as init and reboot.
    
    BRANCH=None
    BUG=b:35849284
    TEST=flash, control tigertail successfully
    
    Change-Id: I8d60c215fee04de158c22edca5377c3c6cd48cf0

---
+++ extra/tigertool/ecusb/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+__all__ = ['tiny_servo_common', 'stm32usb', 'stm32uart', 'pty_driver']
+
---
+++ extra/tigertool/flash_dfu.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+FLAGS_timeout=600
+IMG=${1:-tigertail.bin}
+
+echo "Flashing ${IMG}"
+
+function die() {
+        [ -z "$*" ] || error "$@"
+        exit 1
+}
+
+function flash_stm32_dfu() {
+        DFU_DEVICE=0483:df11
+        ADDR=0x08000000
+        DFU_UTIL='./dfu-util'
+        which $DFU_UTIL &> /dev/null || die \
+                "no dfu-util util found.  Did you 'sudo emerge dfu-util'"
+
+        dev_cnt=$(lsusb -d $DFU_DEVICE | wc -l)
+        if [ $dev_cnt -eq 0 ] ; then
+                die "unable to locate dfu device at $DFU_DEVICE"
+        elif [ $dev_cnt -ne 1 ] ; then
+                die "too many dfu devices (${dev_cnt}). Disconnect all but one."
+        fi
+
+        SIZE=$(wc -c ${IMG} | cut -d' ' -f1)
+        # Remove read protection
+        sudo timeout -k 10 -s 9 "${FLAGS_timeout}" \
+                $DFU_UTIL -a 0 -s ${ADDR}:${SIZE}:force:unprotect -D "${IMG}"
+        # Wait for mass-erase and reboot after unprotection
+        sleep 1
+        # Actual image flashing
+        sudo timeout -k 10 -s 9 "${FLAGS_timeout}" \
+                $DFU_UTIL -a 0 -s ${ADDR}:${SIZE} -D "${IMG}"
+}
+
+flash_stm32_dfu
+
