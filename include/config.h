41242607b26be1e8a5a7946cd63a87b308ce79b9
Nick Vaccaro <nvaccaro@chromium.org>
2017-05-04 20:17:42 -0700

    common: allow report disabling of host events
    Adds a mechanism that allows a board to disable interrupting the AP /
    kernel when the status of any one of the EC_HOST_EVENTS included in
    CONFIG_HOST_EVENT_REPORT_MASK changes state.
    
    The mechanism is enabled via CONFIG_HOST_EVENT_REPORT_DISABLING.
    
    Side-effect of enabling this mechanism is that it will also suppress
    interrupting the kernel when an event's state didn't actually change state
    (i.e.  calling set_host_events() and clear_host_events() and passing an
    event that didn't actually change state will no longer interrupt the kernel
    for it to find out that nothing has changed).
    
    BUG=chromium:637061
    BRANCH=none
    TEST=make buildall passes, compiles for kevin, still need to verify
    functionality.
    Signed-off-by: Nick Vaccaro <nvaccaro@chromium.org>
    
    Change-Id: I678fb9d9dab6890848b94b314efd711842b1fd48

--- include/config.h
+++ include/config.h
@@ -1218,7 +1218,6 @@
 
 #undef CONFIG_GESTURE_SIGMO_EVENT
 
-
 /* Do we want to detect the lid angle? */
 #undef CONFIG_LID_ANGLE
 
@@ -1284,6 +1283,9 @@
  * of the previous command.
  */
 #undef CONFIG_HOST_COMMAND_STATUS
+
+/* mechanism to mask individual reporting of host events */
+#undef CONFIG_HOST_EVENT_REPORT_MASK
 
 /*
  * The host commands are sorted in the .rodata.hcmds section so use the binary
