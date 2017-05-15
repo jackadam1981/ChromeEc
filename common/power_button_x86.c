7912243a52c8fb65b6aedbf8e9c9d6b2d85e1d96
Daisuke Nojiri <dnojiri@chromium.org>
2017-05-15 15:17:53 -0700

    Skip power button check on soft-reset
    This patch makes ec skip power button check when it boots after
    soft-reset.
    
    Change-Id: I8ede3482922697f2db7ca6542b346a51749d0845
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- common/power_button_x86.c
+++ common/power_button_x86.c
@@ -227,6 +227,9 @@ static void set_initial_pwrbtn_state(void)
 		 */
 		CPRINTS("PB init-off");
 		power_button_pch_release();
+	} else if (reset_flags & RESET_FLAG_SOFT) {
+		CPRINTS("PB init-on");
+		pwrbtn_state = PWRBTN_STATE_INIT_ON;
 	} else if (power_button_is_pressed()) {
 		CPRINTS("PB init-on");
 		pwrbtn_state = PWRBTN_STATE_INIT_ON;
