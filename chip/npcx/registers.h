8eded9019ceec298bb3da3450b176f7194b929fe
CHLin <CHLIN56@nuvoton.com>
2017-05-16 16:35:02 +0800

    npcx: system: fix the incorrect checking of invalid BBRAM(IBBR) bit.
    This CL adds:
    1. Fixed the incorrect address of BKUP_STS register.
    2. Cleared the IBBR bit of BKUP_STS register at initial because
    its default value is 1(means the content of BBRAM is invalid) whenever
    VBAT is powered up.
    3. Add debug msg when IBBR bit is set to indicate the BBRAM's
    corruption.
    4. Modified the valid BBRAM offset from 1 to 0 and size from 63 to 64.
    
    BRANCH=none
    BUG=b:38187362
    TEST=No build error for make buildall; Check IBBR is cleared at initial.
    Check IBBR is set by changing the VBAT voltage below VBAT MIN.
    Test console command "reboot ap-off" on poppy.
    
    Change-Id: I69d98b50d4e0aec17b55a4a9b5e8f1a412a3fe45
    Signed-off-by: CHLin <CHLIN56@nuvoton.com>

--- chip/npcx/registers.h
+++ chip/npcx/registers.h
@@ -63,6 +63,7 @@
 #define DEBUG_CLK                        0
 #define DEBUG_LPC                        0
 #define DEBUG_ESPI                       0
+#define DEBUG_SYSTEM                     0
 
 /* Modules Map */
 #define NPCX_MDC_BASE_ADDR               0x4000C000
@@ -1124,12 +1125,12 @@ enum PM_CHANNEL_T {
 
 /******************************************************************************/
 /* Battery-Backed RAM (BBRAM) Registers */
-#define NPCX_BKUP_STS                REG8(NPCX_BBRAM_BASE_ADDR + 0x000)
-#define NPCX_BBRAM(offset)           REG8(NPCX_BBRAM_BASE_ADDR + 0x001 + offset)
+#define NPCX_BKUP_STS                REG8(NPCX_BBRAM_BASE_ADDR + 0x100)
+#define NPCX_BBRAM(offset)           REG8(NPCX_BBRAM_BASE_ADDR + offset)
 
 /* BBRAM register fields */
 #define NPCX_BKUP_STS_IBBR               7
-#define NPCX_BBRAM_SIZE                  63  /* Size of BBRAM */
+#define NPCX_BBRAM_SIZE                  64  /* Size of BBRAM */
 
 /******************************************************************************/
 /* Timer Watch Dog (TWD) Registers */
