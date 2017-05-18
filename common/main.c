31c116fb6fe45e52e638a33d7c1d9f91ebe5dafe
Daisuke Nojiri <dnojiri@chromium.org>
2017-05-18 13:37:21 -0700

    vboot_ec: Support vboot by EC
    Type-c adapters can provide only up to 15w without PD negotiation.
    Some APs require more than 15w to boot. PD communication is not allowed
    in RO image for security reason.
    
    Currently, EC can jump to RW only after Depthcharge (running on AP)
    validates EC's RW image. So, we need a new way for EC to jump to RW
    before AP runs.
    
    This patch adds vboot ec, which verifies RW image by itself and jumps
    to it if it's good.
    
    Vboot has to run after chargers are initialized but before chipset task
    starts. Also available voltage is usually read by ADC, which initializes
    itself in HOOK_INIT. So, this patch makes vboot_ec called as the last
    callback of HOOK_INIT hooks.
    
    BUG=b:38462249
    BRANCH=none
    TEST=Boot Fizz
    
    Change-Id: I7372349f37a63e5dadc51cecec7b21f1610311e7
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- common/main.c
+++ common/main.c
@@ -27,6 +27,7 @@
 #include "timer.h"
 #include "uart.h"
 #include "util.h"
+#include "vboot.h"
 #include "watchdog.h"
 
 /* Console output macros */
@@ -162,6 +163,7 @@ test_mockable __keep int main(void)
 	button_init();
 #endif
 
+#ifndef CONFIG_VBOOT_EC
 #if defined(CONFIG_RWSIG) && !defined(HAS_TASK_RWSIG)
 	/*
 	 * Check the RW firmware signature and jump to it if it is good.
@@ -184,6 +186,7 @@ test_mockable __keep int main(void)
 		}
 	}
 #endif
+#endif /* !CONFIG_VBOOT_EC */
 
 	/*
 	 * Print the init time.  Not completely accurate because it can't take
