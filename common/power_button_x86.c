6263e8cead985df8cc843c52dc52dff2fdd66252
Daisuke Nojiri <dnojiri@chromium.org>
2017-05-24 12:17:04 -0700

    power_button_x86: Set PD state to ON in recovery mode
    This patch sets the initial power button state to on if recovery
    mode is requested.
    
    BUG=b:37274183
    BRANCH=none
    TEST=Verify EC boots AP immediately in recovery mode on Fizz.
    Verify EC doesn't boot AP immediately in normal mode.
    
    Change-Id: Ib24eb6c6b7e9200cf7ba6af3e486337da3c68355
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- common/power_button_x86.c
+++ common/power_button_x86.c
@@ -212,6 +212,7 @@ static void set_initial_pwrbtn_state(void)
 		} else {
 			CPRINTS("PB init-jumped");
 		}
+		return;
 	} else if ((reset_flags & RESET_FLAG_AP_OFF) ||
 		   (keyboard_scan_get_boot_keys() == BOOT_KEY_DOWN_ARROW)) {
 		/*
@@ -227,8 +228,10 @@ static void set_initial_pwrbtn_state(void)
 		 */
 		CPRINTS("PB init-off");
 		power_button_pch_release();
+		return;
 	} else if (power_button_is_pressed()) {
-		CPRINTS("PB init-on");
+		pwrbtn_state = PWRBTN_STATE_INIT_ON;
+	} else if (host_is_event_set(EC_HOST_EVENT_KEYBOARD_RECOVERY)) {
 		pwrbtn_state = PWRBTN_STATE_INIT_ON;
 	} else {
 		/*
@@ -236,13 +239,13 @@ static void set_initial_pwrbtn_state(void)
 		 * it can verify the EC.
 		 */
 #if defined(CONFIG_BRINGUP) || defined(CONFIG_POWER_BUTTON_INIT_IDLE)
-		CPRINTS("PB idle");
 		pwrbtn_state = PWRBTN_STATE_IDLE;
 #else
-		CPRINTS("PB init-on");
 		pwrbtn_state = PWRBTN_STATE_INIT_ON;
 #endif
 	}
+	CPRINTS("PB %s",
+		pwrbtn_state == PWRBTN_STATE_INIT_ON ? "init-on" : "idle");
 }
 
 /**
