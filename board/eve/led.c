a031d4b89845cd4a28761fb99234466a465b77dd
Scott Collyer <scollyer@google.com>
2017-05-25 16:40:13 -0700

    eve: (WIP) Update LED behavior to match new spec
    The previous LED implementation was modelled on Kevin and enhanced to
    give developer feedback. This CL implements the Eve specific LED
    operation.
    
    BUG=b:35584895
    BRANCH=none
    TEST=Manual
    Used EC console command 'battfake' to force different charge levels
    and verified that the expected LED patterns were generated.
    
    Change-Id: I46bc2889540f320e8aadf3d9d634c36dbc38c161
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- board/eve/led.c
+++ board/eve/led.c
@@ -24,6 +24,10 @@
 #define LED_ON_TICKS 8
 
 static int led_debug;
+static int double_tap;
+static int double_tap_tick_count;
+static int led_pattern;
+static int ticks;
 
 const enum ec_led_id supported_led_ids[] = {
 	EC_LED_ID_LEFT_LED, EC_LED_ID_RIGHT_LED};
@@ -35,7 +39,8 @@ enum led_color {
 	LED_GREEN,
 	LED_BLUE,
 	LED_WHITE,
-	LED_AMBER,
+	LED_RED_2_3,
+	LED_RED_1_3,
 
 	/* Number of colors, not a color itself */
 	LED_COLOR_COUNT
@@ -47,16 +52,66 @@ enum led_side {
 	LED_BOTH
 };
 
+#define LED_TICKS_PER_BEAT 2
+#define LED_BEATS_PER_PHASE 2
+#define LED_TICKS_PER_PHASE (LED_TICKS_PER_BEAT * LED_BEATS_PER_PHASE)
+#define NUM_PHASE 2
+struct led_phase {
+	uint8_t color[NUM_PHASE];
+	uint8_t len[NUM_PHASE];
+};
+
+struct range_map {
+	uint8_t max;
+	uint8_t tap;
+};
+
+enum led_type {
+	SOLID_GREEN = 0,
+	WHITE_GREEN,
+	SOLID_WHITE,
+	WHITE_RED,
+	SOLID_RED,
+	PULSE_RED_1,
+	PULSE_RED_2,
+	BLINK_RED,
+	OFF,
+	LED_NUM_TYPES,
+};
+
+static const struct led_phase pattern[LED_NUM_TYPES] = {
+	{ {LED_GREEN, LED_GREEN}, {-1, -1} },
+	{ {LED_WHITE, LED_GREEN}, {2, 4} },
+	{ {LED_WHITE, LED_WHITE}, {-1, -1} },
+	{ {LED_WHITE, LED_RED}, {2, 4} },
+	{ {LED_RED, LED_RED}, {-1, -1} },
+	{ {LED_RED, LED_RED_2_3}, {4, 4} },
+	{ {LED_RED, LED_RED_1_3}, {2, 4} },
+	{ {LED_RED, LED_OFF}, {1, 6} },
+	{ {LED_OFF, LED_OFF}, {-1, -1} },
+};
 /* Brightness vs. color, in the order of off, red, green and blue */
 #define PWM_CHAN_PER_LED 3
 static const uint8_t color_brightness[LED_COLOR_COUNT][PWM_CHAN_PER_LED] = {
 	/* {Red, Green, Blue}, */
-	[LED_OFF]   = {100, 100, 100},
-	[LED_RED]   = {20,  100, 100},
-	[LED_GREEN] = {100, 20, 100},
-	[LED_BLUE]  = {100, 100, 20},
-	[LED_WHITE]  = {0, 0, 0},
-	[LED_AMBER]  = {0, 87, 100},
+	[LED_OFF]   = {0, 0, 0},
+	[LED_RED]   = {80,  0, 0},
+	[LED_GREEN] = {0, 80, 0},
+	[LED_BLUE] = {0, 0, 80},
+	[LED_WHITE]  = {100, 100, 100},
+	[LED_RED_2_3]  = {53, 0, 0},
+	[LED_RED_1_3]  = {27, 0, 0},
+};
+
+static const struct range_map pattern_tbl[] = {
+	{2, BLINK_RED},
+	{4, PULSE_RED_2},
+	{10, PULSE_RED_1},
+	{15, SOLID_RED},
+	{30, WHITE_RED},
+	{90, SOLID_WHITE},
+	{98, WHITE_GREEN},
+	{100, SOLID_GREEN},
 };
 
 /**
@@ -76,7 +131,7 @@ static void set_color(enum led_color color, enum led_side side)
 			if (saved_duty[LED_LEFT][i] !=
 			    color_brightness[color][i]) {
 				pwm_set_duty(PWM_CH_LED_L_RED + i,
-					     color_brightness[color][i]);
+					     100 - color_brightness[color][i]);
 				saved_duty[LED_LEFT][i] =
 					color_brightness[color][i];
 			}
@@ -89,7 +144,7 @@ static void set_color(enum led_color color, enum led_side side)
 			if (saved_duty[LED_RIGHT][i] !=
 			    color_brightness[color][i]) {
 				pwm_set_duty(PWM_CH_LED_R_RED + i,
-					     color_brightness[color][i]);
+					     100 - color_brightness[color][i]);
 				saved_duty[LED_RIGHT][i] =
 					color_brightness[color][i];
 			}
@@ -131,107 +186,91 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 	return EC_SUCCESS;
 }
 
+void led_register_double_tap(void)
+{
+	double_tap = 1;
+}
+
+static void led_manage_pattern(int side)
+{
+	int color;
+	int phase;
+
+	phase = ticks < LED_TICKS_PER_BEAT * pattern[led_pattern].len[0] ?
+		0 : 1;
+	color = pattern[led_pattern].color[phase];
+
+	set_color(color, side);
+
+	if (pattern[led_pattern].len[0] < 0)
+		ticks = 0;
+	else
+		if (++ticks == LED_TICKS_PER_BEAT *
+		    (pattern[led_pattern].len[0] +
+		     pattern[led_pattern].len[1]))
+			ticks = 0;
+
+	if (double_tap_tick_count)
+		double_tap_tick_count--;
+}
+
 static void eve_led_set_power_battery(void)
 {
-	static int power_ticks;
 	enum charge_state chg_state = charge_get_state();
 	int side;
+	int percent_chg;
+	enum led_type type;
+	int tap = 0;
+	int i;
 
+	if (double_tap) {
+		/* Clear double tap indication */
+		if (!chipset_in_state(CHIPSET_STATE_ON))
+			/* If not in S0, then set tap on */
+			tap = 1;
+		double_tap = 0;
+	}
 	/* Get active charge port which maps directly to left/right LED */
 	side = charge_manager_get_active_charge_port();
 	/* Ensure that side can be safely used as an index */
 	if (side < 0 || side >= CONFIG_USB_PD_PORT_COUNT)
 		side = LED_BOTH;
 
-	if (chipset_in_state(CHIPSET_STATE_ON)) {
-		enum led_side blueside = LED_BOTH;
+	/* Get percent charge */
+	percent_chg = charge_get_percent();
 
-		if (chg_state == PWR_STATE_CHARGE) {
-			set_color(LED_AMBER, side);
-			blueside = !side;
-		} else if (chg_state == PWR_STATE_IDLE || chg_state ==
-			   PWR_STATE_CHARGE_NEAR_FULL) {
-			set_color(LED_GREEN, side);
-			blueside = !side;
-		} else if (chg_state == PWR_STATE_DISCHARGE_FULL &&
-			   extpower_is_present()) {
-			set_color(LED_GREEN, side);
-			blueside = !side;
-		}
-		set_color(LED_BLUE, blueside);
-		return;
-	}
-
-	/* Flash red on critical battery, which usually inhibits AP power-on. */
-	if (battery_is_present() != BP_YES ||
-	    charge_get_percent() < CONFIG_CHARGER_MIN_BAT_PCT_FOR_POWER_ON) {
-		set_color(((power_ticks++ % LED_TOTAL_TICKS) < LED_ON_TICKS) ?
-			  LED_RED : LED_OFF, LED_BOTH);
-		return;
-	}
-
-	/* Suspend or Standby state */
-	if (chipset_in_state(CHIPSET_STATE_SUSPEND) ||
-	    chipset_in_state(CHIPSET_STATE_STANDBY)) {
-		enum led_side blinkside = LED_BOTH;
-
-		if (chg_state == PWR_STATE_CHARGE_NEAR_FULL ||
-		    chg_state == PWR_STATE_IDLE) {
-			set_color(LED_GREEN, side);
-			blinkside = !side;
+	if (!double_tap_tick_count) {
+		if (chg_state == PWR_STATE_CHARGE_NEAR_FULL) {
+			type = SOLID_GREEN;
 		} else if (chg_state == PWR_STATE_CHARGE) {
-			set_color(LED_AMBER, side);
-			blinkside = !side;
-		} else if (chg_state == PWR_STATE_DISCHARGE_FULL &&
-			   extpower_is_present()) {
-			set_color(LED_GREEN, side);
-			blinkside = !side;
+			type = SOLID_GREEN;
+		} else {
+			for (i = 0; i < ARRAY_SIZE(pattern_tbl); i++) {
+				if (percent_chg <= pattern_tbl[i].max) {
+					type = pattern_tbl[i].tap;
+					break;
+				}
+			}
+			/* Check for double tap event */
+			if (tap == 0) {
+				if (type <= WHITE_RED)
+					type = OFF;
+			} else {
+				double_tap_tick_count = LED_TICKS_PER_BEAT * 8;
+			}
 		}
-		if (chg_state == PWR_STATE_DISCHARGE ||
-		    chg_state == PWR_STATE_DISCHARGE_FULL ||
-		    chg_state == PWR_STATE_CHARGE ||
-		    chg_state == PWR_STATE_CHARGE_NEAR_FULL ||
-		    chg_state == PWR_STATE_IDLE) {
-			/*
-			 * If in S3/S0iX and not in some error
-			 * state, then flash non-charging LEDs white.
-			 */
-			set_color(((power_ticks++ % LED_TOTAL_TICKS) <
-				   LED_ON_TICKS) ?
-				  LED_WHITE : LED_OFF, blinkside);
-			return;
+
+		/* If the LED pattern will change, then reset tick count and set
+		 * new pattern.
+		 */
+		if (type != led_pattern) {
+			CPRINTS("led: old = %d, new = %d", led_pattern, type);
+			ticks = 0;
+			led_pattern = type;
 		}
 	}
 
-	/* CHIPSET_STATE_OFF */
-	switch (chg_state) {
-	case PWR_STATE_DISCHARGE_FULL:
-		set_color(LED_OFF, LED_BOTH);
-		if (extpower_is_present())
-			set_color(LED_GREEN, side);
-		break;
-	case PWR_STATE_DISCHARGE:
-		set_color(LED_OFF, LED_BOTH);
-		break;
-	case PWR_STATE_CHARGE:
-		set_color(LED_OFF, LED_BOTH);
-		set_color(LED_AMBER, side);
-		break;
-	case PWR_STATE_ERROR:
-		set_color(((power_ticks++ % LED_TOTAL_TICKS)
-			   < LED_ON_TICKS) ? LED_RED : LED_GREEN, LED_BOTH);
-		break;
-	case PWR_STATE_CHARGE_NEAR_FULL:
-	case PWR_STATE_IDLE: /* External power connected in IDLE. */
-		set_color(LED_OFF, LED_BOTH);
-		set_color(LED_GREEN, side);
-		break;
-	default:
-		set_color(LED_RED, LED_BOTH);
-		break;
-	}
-	if (chg_state != PWR_STATE_ERROR)
-		power_ticks = 0;
+	led_manage_pattern(side);
 }
 
 static void led_init(void)
@@ -251,6 +290,9 @@ static void led_init(void)
 	pwm_enable(PWM_CH_LED_R_BLUE, 1);
 
 	set_color(LED_OFF, LED_BOTH);
+	led_pattern = OFF;
+	ticks = 0;
+	double_tap_tick_count = 0;
 }
 /* After pwm_pin_init() */
 DECLARE_HOOK(HOOK_INIT, led_init, HOOK_PRIO_DEFAULT);
@@ -260,8 +302,12 @@ DECLARE_HOOK(HOOK_INIT, led_init, HOOK_PRIO_DEFAULT);
  */
 static void led_tick(void)
 {
-	if (led_debug)
+	if (led_debug == 1)
 		return;
+	else if (led_debug >= 2) {
+		led_manage_pattern(LED_BOTH);
+		return;
+	}
 
 	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED) &&
 	    led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED)) {
@@ -300,8 +346,6 @@ static int command_led(int argc, char **argv)
 			set_color(LED_BLUE, side);
 		} else if (!strcasecmp(argv[1], "white")) {
 			set_color(LED_WHITE, side);
-		} else if (!strcasecmp(argv[1], "amber")) {
-			set_color(LED_AMBER, side);
 		} else {
 			/* maybe handle charger_discharge_on_ac() too? */
 			return EC_ERROR_PARAM1;
@@ -312,3 +356,39 @@ static int command_led(int argc, char **argv)
 DECLARE_CONSOLE_COMMAND(led, command_led,
 			"[debug|red|green|blue|white|amber|off <0|1>]",
 			"Change LED color");
+
+/* Console commands */
+static int command_led_test(int argc, char **argv)
+{
+	int select;
+	char *e;
+
+	if (argc > 1) {
+
+		if (argc > 2) {
+			select = strtoi(argv[2], &e, 10);
+			if (*e)
+				return EC_ERROR_PARAM1;
+			if (select >= LED_NUM_TYPES)
+				return EC_ERROR_PARAM1;
+
+			ticks = 0;
+			led_pattern = select;
+			double_tap_tick_count = LED_TICKS_PER_BEAT * 8;
+		}
+
+		if (!strcasecmp(argv[1], "on")) {
+			led_debug = 2;
+		} else if (!strcasecmp(argv[1], "off")) {
+			led_debug = 0;
+		} else {
+			/* maybe handle charger_discharge_on_ac() too? */
+			return EC_ERROR_PARAM1;
+		}
+	}
+
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(led_test, command_led_test,
+			"[on|off 0 - 5]",
+			"Change LED pattern");
