628c45f6ac11a83388d315826ca6811c7460a092
Daisuke Nojiri <dnojiri@chromium.org>
2017-05-26 14:10:22 -0700

    Add system_can_boot_ap API
    This API checks battery charge level and current power supply
    to determine whether the AP has enough power to boot or not.
    
    BUG=b:38462249
    BRANCH=none
    TEST=make buildall
    
    Change-Id: I489f7ea92f230701b8f18c94d3e698aad90b4a03
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/system.h
+++ include/system.h
@@ -507,4 +507,14 @@ uintptr_t system_get_fw_reset_vector(uintptr_t base);
  */
 int system_is_reboot_warm(void);
 
+/**
+ * Check if the system can supply enough power to boot AP
+ *
+ * @param percent (OUT) Battery state of charge
+ * @param power   (OUT) Power supplied by a charger in mW
+ *
+ * @return true if the system is powered enough or false otherwise
+ */
+int system_can_boot_ap(int *percent, int *power);
+
 #endif  /* __CROS_EC_SYSTEM_H */
