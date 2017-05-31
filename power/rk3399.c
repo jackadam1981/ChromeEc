8b2877522f0f3ca7a0cf9da4fb0bc934b7067c65
Caesar Wang <wxt@rock-chips.com>
2017-05-31 11:33:39 +0800

    WIP: power: rk3399: change the TRACKPAD power consequence
    Change-Id: If62c905c2a1128e437b7fe349acd1439841eac04
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- power/rk3399.c
+++ power/rk3399.c
@@ -233,7 +233,6 @@ enum power_state power_handle_state(enum power_state state)
 		msleep(2);
 		gpio_set_level(GPIO_PP3300_USB_EN_L, 0);
 		gpio_set_level(GPIO_PP5000_EN, 1);
-		gpio_set_level(GPIO_PP3300_TRACKPAD_EN_L, 0);
 		msleep(1);
 		gpio_set_level(GPIO_PP1800_LID_EN_L, 0);
 
@@ -269,6 +268,7 @@ enum power_state power_handle_state(enum power_state state)
 		gpio_set_level(GPIO_PP1800_S0_EN_L, 0);
 		msleep(2);
 		gpio_set_level(GPIO_PP3300_S0_EN_L, 0);
+		gpio_set_level(GPIO_PP3300_TRACKPAD_EN_L, 0);
 
 		/* Release SYS_RST if we came from S5 */
 		if (sys_reset_asserted) {
@@ -299,6 +299,9 @@ enum power_state power_handle_state(enum power_state state)
 		/* Call hooks before we remove power rails */
 		hook_notify(HOOK_CHIPSET_SUSPEND);
 		MSLEEP_CHECK_ABORTED_SUSPEND(20);
+
+		gpio_set_level(GPIO_PP3300_TRACKPAD_EN_L, 1);
+		MSLEEP_CHECK_ABORTED_SUSPEND(1);
 
 		gpio_set_level(GPIO_PP3300_S0_EN_L, 1);
 		MSLEEP_CHECK_ABORTED_SUSPEND(20);
@@ -344,7 +347,6 @@ enum power_state power_handle_state(enum power_state state)
 		gpio_set_level(GPIO_PP1800_SENSOR_EN_L, 1);
 		gpio_set_level(GPIO_PP1800_SIXAXIS_EN_L, 1);
 		gpio_set_level(GPIO_PP1800_LID_EN_L, 1);
-		gpio_set_level(GPIO_PP3300_TRACKPAD_EN_L, 1);
 		gpio_set_level(GPIO_PP5000_EN, 0);
 		gpio_set_level(GPIO_PP3300_USB_EN_L, 1);
 		msleep(20);
