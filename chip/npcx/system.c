b5ce7460789207feb17f86cc22aca2cd4a9bc3eb
Furquan Shaikh <furquan@chromium.org>
2017-06-11 17:04:49 -0700

    chip/npcx: Add support for saving/restoring panic data
    For some platforms like poppy/eve where a PMIC reset is required on
    reboot/panic to ensure a complete power-cycle of the AP, there is a
    drop on VCC power rail thus resulting in a loss of panic data. For
    such cases, provide API to backup panic data in BBRAM before
    performing a PMIC reset. Additionally, check for panic data in
    system_pre_init and restore if available from BBRAM.
    
    BUG=b:62076222
    BRANCH=None
    TEST=make -j buildall
    
    Change-Id: I95cdd55e260487903e089653a47d3995d177daed
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>

--- chip/npcx/system.c
+++ chip/npcx/system.c
@@ -228,6 +228,80 @@ void system_set_rtc(uint32_t seconds)
 	udelay(MTC_TTC_LOAD_DELAY_US);
 }
 
+#ifdef CONFIG_CHIP_PANIC_BACKUP
+/*
+ * Following information from panic data is stored in BBRAM:
+ *
+ * index     |       data
+ * ==========|=============
+ *   36      |       MMFS
+ *   40      |       BFAR
+ *   44      |       MFAR
+ *   48      |      SHCSR
+ *   52      |       HFSR
+ *   56      |       DFSR
+ *   60      |     reserved
+ */
+#define BKUP_MMFS		(BBRM_DATA_INDEX_PANIC_BKUP + 0)
+#define BKUP_BFAR		(BBRM_DATA_INDEX_PANIC_BKUP + 4)
+#define BKUP_MFAR		(BBRM_DATA_INDEX_PANIC_BKUP + 8)
+#define BKUP_SHCSR		(BBRM_DATA_INDEX_PANIC_BKUP + 12)
+#define BKUP_HFSR		(BBRM_DATA_INDEX_PANIC_BKUP + 16)
+#define BKUP_DFSR		(BBRM_DATA_INDEX_PANIC_BKUP + 20)
+
+/*
+ * If BBRAM is valid and data in first panic data index (MMFS) is 0, then it is
+ * considered that panic data is not valid in BBRAM.
+ */
+#define PANIC_DATA_INVALID	(0x0)
+
+void chip_panic_data_backup(void)
+{
+	struct panic_data *d = panic_get_data();
+
+	if (!d)
+		return;
+
+	bbram_data_write(BKUP_MMFS, d->cm.mmfs);
+	bbram_data_write(BKUP_BFAR, d->cm.bfar);
+	bbram_data_write(BKUP_MFAR, d->cm.mfar);
+	bbram_data_write(BKUP_SHCSR, d->cm.shcsr);
+	bbram_data_write(BKUP_HFSR, d->cm.hfsr);
+	bbram_data_write(BKUP_DFSR, d->cm.dfsr);
+}
+
+void chip_panic_data_restore(void)
+{
+	uint32_t val;
+	struct panic_data *d = PANIC_DATA_PTR;
+
+	/* Ensure BBRAM is valid. */
+	if (!bbram_valid(BKUP_MMFS, 4))
+		return;
+
+	/* Ensure MMFS has non-zero data. */
+	val = bbram_data_read(BKUP_MMFS);
+	if (val == PANIC_DATA_INVALID)
+		return;
+
+	memset(d, 0, sizeof(*d));
+	d->magic = PANIC_DATA_MAGIC;
+	d->struct_size = sizeof(*d);
+	d->struct_version = 2;
+	d->arch = PANIC_ARCH_CORTEX_M;
+
+	d->cm.mmfs = val;
+	d->cm.bfar = bbram_data_read(BKUP_BFAR);
+	d->cm.mfar = bbram_data_read(BKUP_MFAR);
+	d->cm.shcsr = bbram_data_read(BKUP_SHCSR);
+	d->cm.hfsr = bbram_data_read(BKUP_HFSR);
+	d->cm.dfsr = bbram_data_read(BKUP_DFSR);
+
+	/* Reset panic data in BBRAM. */
+	bbram_data_write(BKUP_MMFS, PANIC_DATA_INVALID);
+}
+#endif /* CONFIG_CHIP_PANIC_BACKUP */
+
 void chip_save_reset_flags(int flags)
 {
 	bbram_data_write(BBRM_DATA_INDEX_SAVED_RESET_FLAGS, flags);
@@ -559,6 +633,10 @@ void system_pre_init(void)
 	 * and DATA RAM to prevent code execution
 	 */
 	system_mpu_config();
+
+#ifdef CONFIG_CHIP_PANIC_BACKUP
+	chip_panic_data_restore();
+#endif
 }
 
 void system_reset(int flags)
