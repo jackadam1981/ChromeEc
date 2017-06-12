c38864f0c03033e96a118811158a99666f7b7bec
Furquan Shaikh <furquan@chromium.org>
2017-06-11 17:04:49 -0700

    chip/npcx: Add support for saving/restoring panic data
    For some platforms like poppy/eve where a PMIC reset is required on
    reboot/panic to ensure a complete power-cycle of the AP, there is a
    drop on VCC power rail thus resulting in a loss of panic data. For
    such cases, provide API to backup panic data in BBRAM before
    performing a PMIC reset. Additionally, check for panic data in
    system_pre_init and restore if available from BBRAM.
    
    BUG=b:62076222
    BRANCH=None
    TEST=make -j buildall
    
    Change-Id: I95cdd55e260487903e089653a47d3995d177daed
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>

--- chip/npcx/system_chip.h
+++ chip/npcx/system_chip.h
@@ -21,6 +21,7 @@ enum bbram_data_index {
 	BBRM_DATA_INDEX_PD1 = 13,	       /* USB-PD saved port1 state */
 	BBRM_DATA_INDEX_VBNVCNTXT = 16,	       /* VbNvContext for ARM arch */
 	BBRM_DATA_INDEX_RAMLOG = 32,	       /* RAM log for Booter */
+	BBRM_DATA_INDEX_PANIC_BKUP = 36,       /* Panic data (index 36-63)*/
 };
 
 /* Issue a watchdog reset*/
