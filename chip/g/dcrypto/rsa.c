554a33182ed45754439ecde413d72599b47040a7
Marius Schilder <mschilder@google.com>
2017-06-19 18:40:12 -0700

    g: RSA randomization
    Make RSA decrypt and sign pass in a 2nd exponent to bn_modexp()
    Non-zero value signals the operation is sensitive and needs
    randomization.
    
    If one passes in non-zero, 1st exponent * 2nd exponent must be congruent 1
    modulo size of the multiplicative group for the exponentiation (i.e.
    pass in both private and public exponents in the RSA case)
    
    We only pass in non-zero for the cases where we wield the private
    exponent -- public operations do not need randomization.
    
    Refactored the p256+rsa dcrypto binary blob into two parts.
    Add unique first word to each dcrypto blob to make code caching reliable.
    
    We randomize the modexp operation by:
    1) pick 64 bit random R1 and compute R1 ** -1 and R1 ** pubexp, mod N.
    2) multiply input by R1 ** pubexp
    3) pick 64 bit random R2 and add (e*d*R2 - R2) to private exponent (i.e.
    a random multiple of phi(N))
    4) exponentiate
    5) multiply output w/ R1 ** -1 to obtain proper result
    
    Since we enlarge the exponent, the operation is slower when blinded. To
    retain previous performance for unblinded operations, we now have two
    flavors of dcrypto modexp.
    
    TPM task stack maxes out out 8040/8192
    
    BRANCH=cr50
    BUG=b:35587382,b:35587381
    TEST=buildall, tcg_test
    
    Change-Id: Ied1f908418f31f8025363179537aa4ebd2c80420

--- chip/g/dcrypto/rsa.c
+++ chip/g/dcrypto/rsa.c
@@ -501,7 +501,6 @@ int DCRYPTO_rsa_encrypt(struct RSA *rsa, uint8_t *out, uint32_t *out_len,
 	uint32_t e_buf[LITE_BN_BYTES / sizeof(uint32_t)];
 
 	struct LITE_BIGNUM padded;
-	struct LITE_BIGNUM e;
 	struct LITE_BIGNUM encrypted;
 
 	if (!check_modulus_params(&rsa->N, sizeof(padded_buf), out_len))
@@ -509,8 +508,6 @@ int DCRYPTO_rsa_encrypt(struct RSA *rsa, uint8_t *out, uint32_t *out_len,
 
 	bn_init(&padded, padded_buf, bn_size(&rsa->N));
 	bn_init(&encrypted, out, bn_size(&rsa->N));
-	bn_init(&e, e_buf, sizeof(e_buf));
-	BN_DIGIT(&e, 0) = rsa->e;
 
 	switch (padding) {
 	case PADDING_MODE_OAEP:
@@ -533,7 +530,7 @@ int DCRYPTO_rsa_encrypt(struct RSA *rsa, uint8_t *out, uint32_t *out_len,
 		/* If in_len < bn_size(&padded), padded will
 		 * have leading zero bytes. */
 		memcpy(&p[bn_size(&padded) - in_len], in, in_len);
-		/* TODO(ngm): in may be > N, bn_mont_mod_exp() should
+		/* TODO(ngm): in may be > N, bn_mod_exp() should
 		 * handle this case. */
 		break;
 	default:
@@ -542,7 +539,7 @@ int DCRYPTO_rsa_encrypt(struct RSA *rsa, uint8_t *out, uint32_t *out_len,
 
 	/* Reverse from big-endian to little-endian notation. */
 	reverse((uint8_t *) padded.d, bn_size(&padded));
-	bn_mont_modexp(&encrypted, &padded, &e, &rsa->N);
+	bn_modexp_word(&encrypted, &padded, rsa->e, &rsa->N);
 	/* Back to big-endian notation. */
 	reverse((uint8_t *) encrypted.d, bn_size(&encrypted));
 	*out_len = bn_size(&encrypted);
@@ -576,7 +573,7 @@ int DCRYPTO_rsa_decrypt(struct RSA *rsa, uint8_t *out, uint32_t *out_len,
 
 	/* Reverse from big-endian to little-endian notation. */
 	reverse((uint8_t *) encrypted.d, encrypted.dmax * LITE_BN_BYTES);
-	bn_mont_modexp(&padded, &encrypted, &rsa->d, &rsa->N);
+	bn_modexp_blinded(&padded, &encrypted, &rsa->d, &rsa->N, rsa->e);
 	/* Back to big-endian notation. */
 	reverse((uint8_t *) padded.d, padded.dmax * LITE_BN_BYTES);
 
@@ -643,7 +640,7 @@ int DCRYPTO_rsa_sign(struct RSA *rsa, uint8_t *out, uint32_t *out_len,
 
 	/* Reverse from big-endian to little-endian notation. */
 	reverse((uint8_t *) padded.d, bn_size(&padded));
-	bn_mont_modexp(&signature, &padded, &rsa->d, &rsa->N);
+	bn_modexp_blinded(&signature, &padded, &rsa->d, &rsa->N, rsa->e);
 	/* Back to big-endian notation. */
 	reverse((uint8_t *) signature.d, bn_size(&signature));
 	*out_len = bn_size(&rsa->N);
@@ -659,11 +656,9 @@ int DCRYPTO_rsa_verify(const struct RSA *rsa, const uint8_t *digest,
 {
 	uint32_t padded_buf[RSA_WORDS_4K];
 	uint32_t signature_buf[RSA_WORDS_4K];
-	uint32_t e_buf[LITE_BN_BYTES / sizeof(uint32_t)];
 
 	struct LITE_BIGNUM padded;
 	struct LITE_BIGNUM signature;
-	struct LITE_BIGNUM e;
 	int ret = 1;
 
 	if (!check_modulus_params(&rsa->N, sizeof(padded_buf), NULL))
@@ -674,12 +669,10 @@ int DCRYPTO_rsa_verify(const struct RSA *rsa, const uint8_t *digest,
 	bn_init(&signature, signature_buf, bn_size(&rsa->N));
 	memcpy(signature_buf, sig, bn_size(&rsa->N));
 	bn_init(&padded, padded_buf, bn_size(&rsa->N));
-	bn_init(&e, e_buf, sizeof(e_buf));
-	BN_DIGIT(&e, 0) = rsa->e;
 
 	/* Reverse from big-endian to little-endian notation. */
 	reverse((uint8_t *) signature.d, bn_size(&signature));
-	bn_mont_modexp(&padded, &signature, &e, &rsa->N);
+	bn_modexp_word(&padded, &signature, rsa->e, &rsa->N);
 	/* Back to big-endian notation. */
 	reverse((uint8_t *) padded.d, bn_size(&padded));
 
