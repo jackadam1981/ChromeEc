cf4f06196c900c56c85e4c7db14449f1fbecec94
Daisuke Nojiri <dnojiri@chromium.org>
2017-06-29 16:03:04 -0700

    npcx: Build RW_B and support sysjump to it
    This patch allows a board to include another RW image in ec.bin.
    The size of each copy is a quarter of the flash size on Fizz.
    
    BUG=b:38462249
    BRANCH=none
    TEST=Run sysjump RW/A/B. Run objdump -h build/fizz/ec.obj:
    
    Idx Name          Size      VMA       LMA       File off  Algn
      0 .image.RO     0001700c  10088000  10088000  00008000  2**0
    		  CONTENTS, ALLOC, LOAD, READONLY, CODE
      1 .image.RO.key 00000340  1009f00c  100a7c40  0001f00c  2**0
    		  CONTENTS, ALLOC, LOAD, READONLY, DATA
      2 .image.RW     00016ddc  1009f34c  100c8000  0001f34c  2**0
    		  CONTENTS, ALLOC, LOAD, READONLY, CODE
      3 .image.RW.sign 000001b8  100b6128  100e7c00  00036128  2**0
    		  CONTENTS, ALLOC, LOAD, READONLY, DATA
      4 .image.RW_B   00016ddc  100b62e0  100e8000  000362e0  2**0
    		  CONTENTS, ALLOC, LOAD, READONLY, CODE
      5 .image.RW_B.sign 000001b8  100cd0bc  10107c00  0004d0bc  2**0
    		  CONTENTS, ALLOC, LOAD, READONLY, DATA
      6 .padding      00000001  100cd274  10107fff  0004d274  2**0
    		  CONTENTS, ALLOC, LOAD, DATA
      7 .ARM.attributes 00000014  00000000  00000000  0004d275  2**0
    		  CONTENTS, READONLY
    
    Change-Id: Iaa687c1d7d704fec4cccfa127376c8db102267fa
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/board.h
+++ board/fizz/board.h
@@ -143,6 +143,29 @@
 #define CONFIG_VBOOT_HASH
 #define CONFIG_VSTORE
 #define CONFIG_VSTORE_SLOT_COUNT 1
+
+/*
+ * Flash layout. Since config_flash_layout.h is included before board.h,
+ * we can only overwrite (=undef/define) these parameters here.
+ *
+ * Flash stores 3 images: RO, RW_A, RW_B. We divide the size by 4.
+ * At the end of RO, there is a public key. At the end of RW_A and RW_B,
+ * there is a signature.
+ */
+#define CONFIG_RW_B
+#define CONFIG_RW_B_MEM_OFF	CONFIG_RO_MEM_OFF
+#undef  CONFIG_RO_SIZE
+#define CONFIG_RO_SIZE		(CONFIG_FLASH_SIZE >> 2)
+#undef  CONFIG_RW_SIZE
+#define CONFIG_RW_SIZE			CONFIG_RO_SIZE
+#define CONFIG_RW_A_STORAGE_OFF		CONFIG_RW_STORAGE_OFF
+#define CONFIG_RW_B_STORAGE_OFF		(CONFIG_RW_A_STORAGE_OFF + \
+					 CONFIG_RW_SIZE)
+#define CONFIG_RW_A_SIGN_STORAGE_OFF	(CONFIG_RW_A_STORAGE_OFF + \
+					 CONFIG_RW_SIZE - CONFIG_RW_SIG_SIZE)
+#define CONFIG_RW_B_SIGN_STORAGE_OFF	(CONFIG_RW_B_STORAGE_OFF + \
+					 CONFIG_RW_SIZE - CONFIG_RW_SIG_SIZE)
+
 #define CONFIG_RWSIG
 #define CONFIG_RWSIG_TYPE_RWSIG
 #define CONFIG_RSA
