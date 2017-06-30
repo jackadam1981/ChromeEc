c8ccee99419d10a35e2314aa04deb6b8dcf305f5
Daisuke Nojiri <dnojiri@chromium.org>
2017-06-30 14:53:28 -0700

    rwsig: Fix padding check
    This patch fixes check_padding and the padding size checked by
    check_padding.
    
    BUG=none
    BRANCH=none
    TEST=buildall
    
    Change-Id: I089141f0a103e1ea21ac901bb9b5e5df22c8a336
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- common/rwsig.c
+++ common/rwsig.c
@@ -82,7 +82,7 @@ static int check_padding(const uint8_t *data,
 	if ((start % 4) != 0 || (len % 4) != 0)
 		return 0;
 
-	for (i = start/4; i < len/4; i++) {
+	for (i = start/4; i < (start + len)/4; i++) {
 		if (data32[i] != 0xffffffff)
 			return 0;
 	}
@@ -103,6 +103,7 @@ int rwsig_check_signature(void)
 	int good = 0;
 
 	unsigned int rwlen;
+	int padlen;
 #ifdef CONFIG_RWSIG_TYPE_RWSIG
 	const struct vb21_packed_key *vb21_key;
 	const struct vb21_signature *vb21_sig;
@@ -177,8 +178,12 @@ int rwsig_check_signature(void)
 	/*
 	 * Check that unverified RW region is actually filled with ones.
 	 */
-	good = check_padding(rwdata, rwlen,
-			CONFIG_RW_SIZE - CONFIG_RW_SIG_SIZE);
+	padlen = CONFIG_RW_SIZE - rwlen - CONFIG_RW_SIG_SIZE;
+	if (padlen < 0) {
+		CPRINTS("Invalid data_size.");
+		goto out;
+	}
+	good = check_padding(rwdata, rwlen, padlen);
 	if (!good) {
 		CPRINTS("Invalid padding.");
 		goto out;
