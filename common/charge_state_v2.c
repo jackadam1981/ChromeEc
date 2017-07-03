b00bc2b7deb26abd95ebdaf9ebd5a4145ac9b78d
Ryan Zhang <Ryan.Zhang@quanta.corp-partner.google.com>
2017-07-03 16:14:15 +0800

    Common: some machines' battery will discharge when `ectool chargecontrol idle`
    Some machines' battery will discharge after executing `ectool chargecontrol idle`
    due to HW voltage tolerance.
    
    BUG=b:0
    BRANCH=firmware-oak-8438.B
    TEST=None
    Signed-off-by: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>
    
    Change-Id: I6d5a3471b9560e77c127b2818e8c101ec6d4a034

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -377,8 +377,8 @@ static int charge_request(int voltage, int current)
 #ifdef CONFIG_CHARGER_NARROW_VDC
 		current = 0;
 		/* With NVDC charger, keep VSYS voltage higher than battery */
-		voltage = charger_closest_voltage(
-			curr.batt.voltage + charger_get_info()->voltage_step);
+		voltage = batt_info->voltage_max;
+
 		/* And handle dead battery case */
 		voltage = MAX(voltage, battery_get_info()->voltage_min);
 #else
