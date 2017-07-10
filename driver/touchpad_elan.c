81668ece7ad3feccd7becd9899db630032a90c4f
Nicolas Boichat <drinkcat@google.com>
2017-07-10 08:24:13 +0800

    usb_update: Add a way to fetch touchpad information
    We'd like to know touchpad vendor/product id, as well as currently
    running FW version. This CL does that by adding a new
    UPDATE_EXTRA_CMD_TOUCHPAD_INFO command.
    
    We also make the interface more generic by adding a CONFIG_TOUCHPAD
    configuration option, even though we only support Elan touchpads
    currently.
    
    BRANCH=none
    BUG=b:63418037
    TEST=Flash hammer, ./usb_updater -t
    
    Change-Id: Icce3c785eb3235bcc50b2ae7c0227ce11cbc9f2b
    Signed-off-by: Nicolas Boichat <drinkcat@google.com>

--- driver/touchpad_elan.c
+++ driver/touchpad_elan.c
@@ -10,6 +10,7 @@
 #include "i2c.h"
 #include "task.h"
 #include "timer.h"
+#include "update_fw.h"
 #include "util.h"
 #include "usb_hid_touchpad.h"
 
@@ -22,10 +23,15 @@
 /* How to talk to the controller */
 /******************************************************************************/
 
+#define ELAN_VENDOR_ID			0x04f3
+
 #define ETP_I2C_RESET			0x0100
 #define ETP_I2C_WAKE_UP			0x0800
 #define ETP_I2C_SLEEP			0x0801
 #define ETP_I2C_STAND_CMD		0x0005
+#define ETP_I2C_UNIQUEID_CMD            0x0101
+#define ETP_I2C_FW_VERSION_CMD          0x0102
+#define ETP_I2C_SM_VERSION_CMD          0x0103
 #define ETP_I2C_XY_TRACENUM_CMD		0x0105
 #define ETP_I2C_MAX_X_AXIS_CMD		0x0106
 #define ETP_I2C_MAX_Y_AXIS_CMD		0x0107
@@ -276,6 +282,35 @@ out:
 	CPRINTS("%s:%d", __func__, rv);
 	return rv;
 }
+
+#ifdef CONFIG_USB_UPDATE
+int touchpad_get_info(struct touchpad_info *tp)
+{
+	int rv;
+	uint16_t val;
+
+	tp->status = EC_RES_SUCCESS;
+	tp->vendor = ELAN_VENDOR_ID;
+
+	/* Get unique ID, FW, SM version. */
+	rv = elan_tp_read_cmd(ETP_I2C_UNIQUEID_CMD, &val);
+	if (rv)
+		return -1;
+	tp->elan.id = val;
+
+	rv = elan_tp_read_cmd(ETP_I2C_FW_VERSION_CMD, &val);
+	if (rv)
+		return -1;
+	tp->elan.fw_version = val & 0xff;
+
+	rv = elan_tp_read_cmd(ETP_I2C_SM_VERSION_CMD, &val);
+	if (rv)
+		return -1;
+	tp->elan.sm_version = val;
+
+	return sizeof(*tp);
+}
+#endif
 
 void elan_tp_interrupt(enum gpio_signal signal)
 {
