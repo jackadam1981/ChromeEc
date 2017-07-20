88e172f6fcc703c0b0de53c2c55cbdc9f7410c86
Vadim Bendebury <vbendeb@chromium.org>
2017-07-20 09:51:11 -0700

    cr50: introduce means of detecting chip running in factory mode
    When installed in the factory test rig, the DIOB4 pin is set to high
    by the moment the RW section starts.
    
    Software behavior needs to change to comply with the factory test rig
    requirements. Define the GPIO and add a function to report the factory
    mode state.
    
    BRANCH=cr50
    BUG=b:63686091
    TEST=tested along with the rest of the stack of patches.
    
    Change-Id: I8c4158fc75138d717fc009496365c8e61b42a890
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- board/cr50/board.h
+++ board/cr50/board.h
@@ -209,6 +209,9 @@ int board_id_is_mismatched(void);
 
 void power_button_record(void);
 
+/* Returns True if chip is brought up in a factory test harness. */
+int chip_factory_mode(void);
+
 #endif /* !__ASSEMBLER__ */
 
 /* USB interface indexes (use define rather than enum to expand them) */
