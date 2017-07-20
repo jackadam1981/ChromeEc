919faf63fcfcb2d927b394e129b0eaa59d769a34
Vadim Bendebury <vbendeb@chromium.org>
2017-07-20 09:40:23 -0700

    cr50: revert tpm_endorse() error reporting logic
    Using zero to represent success make it cleaner when processing return
    values of this function.
    
    Note that nobody checks the return value yet.
    
    BRANCH=cr50
    BUG=b:63686091
    TEST=none
    
    Change-Id: If25d30e637701f6c37f3ce75e5a5d5315e0e86e9
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- board/cr50/tpm2/endorsement.c
+++ board/cr50/tpm2/endorsement.c
@@ -541,7 +541,7 @@ int tpm_endorse(void)
 	const uint32_t *c = (const uint32_t *) RO_CERTS_START_ADDR;
 	const struct ro_cert *rsa_cert;
 	const struct ro_cert *ecc_cert;
-	int result = 0;
+	int result = 1;
 	uint8_t eps[PRIMARY_SEED_SIZE];
 
 	LITE_HMAC_CTX hmac;
@@ -550,11 +550,11 @@ int tpm_endorse(void)
 
 	/* First boot, certs not yet installed. */
 	if (*c == 0xFFFFFFFF)
-		return 0;
+		return 1;
 
 	if (!get_decrypted_eps(eps)) {
 		CPRINTF("%s(): failed to read eps\n", __func__);
-		return 0;
+		return 1;
 	}
 
 	/* Unpack rsa cert struct. */
@@ -562,7 +562,7 @@ int tpm_endorse(void)
 	/* Sanity check cert region contents. */
 	if ((2 * sizeof(struct ro_cert)) +
 		rsa_cert->cert_response.cert_len > RO_CERTS_REGION_SIZE)
-		return 0;
+		return 1;
 
 	/* Unpack ecc cert struct. */
 	ecc_cert = (const struct ro_cert *) (p + sizeof(struct ro_cert) +
@@ -571,16 +571,16 @@ int tpm_endorse(void)
 	if ((2 * sizeof(struct ro_cert)) +
 		rsa_cert->cert_response.cert_len +
 		ecc_cert->cert_response.cert_len > RO_CERTS_REGION_SIZE)
-		return 0;
+		return 1;
 
 	/* Verify expected component types. */
 	if (rsa_cert->cert_info.component_type !=
 		CROS_PERSO_COMPONENT_TYPE_RSA_CERT) {
-		return 0;
+		return 1;
 	}
 	if (ecc_cert->cert_info.component_type !=
 		CROS_PERSO_COMPONENT_TYPE_P256_CERT) {
-		return 0;
+		return 1;
 	}
 
 	do {
@@ -648,7 +648,7 @@ int tpm_endorse(void)
 		endorsement_complete();
 
 		/* Chip has been marked as manufactured. */
-		result = 1;
+		result = 0;
 	} while (0);
 
 	always_memset(eps, 0, sizeof(eps));
