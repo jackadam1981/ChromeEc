867175571dbbd7f9b50dae74fae88c992ba678b6
Wei-Ning Huang <wnhuang@google.com>
2017-07-24 14:34:55 +0800

    flash: add flash selection support
    Some chips require special operations before flash can be accessed
    (read, write, erase), without it the flash operations could be
    corrupted.  The chip that requires this should enable the
    CONFIG_FLASH_INFO_SELECT_REQUIRED config, which exposes
    EC_FLASH_INFO_SELECT_REQUIRED in flashinfo flags. Before any flash
    operations is executed on the chip, EC_CMD_FLASH_SELECT should be issued
    to notify the chip to prepare for the flash operations.
    
    BRANCH=none
    BUG=b:63685022
    TEST=with depended CLs, touchpad interrupt should be disabled when
         flashrom is in progress.
    CQ-DEPEND=CL:*416548,CL:582676
    
    Change-Id: I96455adbe739d5f924edf382a2752404a7c5ad04
    Signed-off-by: Wei-Ning Huang <wnhuang@google.com>

--- include/config.h
+++ include/config.h
@@ -1030,6 +1030,8 @@
 #undef CONFIG_FLASH_ROW_SIZE
 /* Allow deferred (async) flash erase */
 #undef CONFIG_FLASH_DEFERRED_ERASE
+/* Flash must be selected for flash operations to succeed. */
+#undef CONFIG_FLASH_INFO_SELECT_REQUIRED
 
 /* Base address of program memory */
 #undef CONFIG_PROGRAM_MEMORY_BASE
