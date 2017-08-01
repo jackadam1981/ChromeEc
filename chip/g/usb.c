8e4761a97c9ff4d16e747097b7309c1edb8b4890
Mary Ruthven <mruthven@chromium.org>
2017-08-01 15:30:54 -0700

    cr50: add delay after soft reset
    Add a delay to allow the clocks and usb signals to settle.
    
    BUG=b:63767046,b:63867566
    BRANCH=cr50
    TEST=put the eve ec into hibernate, wait until cr50 enters deep sleep,
    use the uart to wake it up, and verify that it eventually reenters deep
    sleep
    
    Change-Id: I26463ce3e00996368a791a245b0f9de01737478c
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- chip/g/usb.c
+++ chip/g/usb.c
@@ -1209,6 +1209,13 @@ static void usb_softreset(void)
 		return;
 	}
 	/* TODO: Wait 3 PHY clocks before returning */
+
+	/*
+	 * TODO(b/63867566): This delay is added to get usb to suspend after
+	 * resume from deep sleep. Find out what the root cause is and add a
+	 * fix.
+	 */
+	usleep(100);
 }
 
 void usb_connect(void)
