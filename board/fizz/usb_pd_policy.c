a0269fb60f3bb291702b3576be5119cdf0197b8e
Daisuke Nojiri <dnojiri@chromium.org>
2017-08-02 14:38:54 -0700

    Fizz: Reject power role swap
    This patch makes Fizz refuse to swap power roles. If it boots on BJ,
    the type-c port will only work as a source. If it boots on type-c,
    it'll stay as a sink.
    
    Without this patch, PR swap (SRC->SNK) causes Fizz to brown out.
    
    BUG=b:64277216
    BRANCH=none
    TEST=Boot on BJ. Verify Pixel XL is charged by type-c port. Initiate
    PR swap from Pixel. Verify PR swap is rejected. Boot on type-c.
    
    Change-Id: I8c3161ee9873e52007d4236fc762bff652f3ce9c
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/usb_pd_policy.c
+++ board/fizz/usb_pd_policy.c
@@ -128,12 +128,8 @@ int pd_board_checks(void)
 
 int pd_check_power_swap(int port)
 {
-	/*
-	 * Allow power swap as long as we are acting as a dual role device,
-	 * otherwise assume our role is fixed (not in S0 or console command
-	 * to fix our role).
-	 */
-	return pd_get_dual_role() == PD_DRP_TOGGLE_ON ? 1 : 0;
+	CPRINTS("Reject PR swap");
+	return 0;
 }
 
 int pd_check_data_swap(int port, int data_role)
@@ -155,23 +151,7 @@ void pd_execute_data_swap(int port, int data_role)
 
 void pd_check_pr_role(int port, int pr_role, int flags)
 {
-	/*
-	 * If partner is dual-role power and dualrole toggling is on, consider
-	 * if a power swap is necessary.
-	 */
-	if ((flags & PD_FLAGS_PARTNER_DR_POWER) &&
-	    pd_get_dual_role() == PD_DRP_TOGGLE_ON) {
-		/*
-		 * If we are a sink and partner is not externally powered, then
-		 * swap to become a source. If we are source and partner is
-		 * externally powered, swap to become a sink.
-		 */
-		int partner_extpower = flags & PD_FLAGS_PARTNER_EXTPOWER;
-
-		if ((!partner_extpower && pr_role == PD_ROLE_SINK) ||
-		     (partner_extpower && pr_role == PD_ROLE_SOURCE))
-			pd_request_power_swap(port);
-	}
+	/* Fizz never swaps power role */
 }
 
 void pd_check_dr_role(int port, int dr_role, int flags)
