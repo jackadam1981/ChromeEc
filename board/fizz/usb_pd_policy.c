91abb49d5e2e83265261e9d943eafe764aee7bd0
Daisuke Nojiri <dnojiri@chromium.org>
2017-08-02 14:38:54 -0700

    Fizz: Reject power role swap
    This patch makes Fizz refuse to swap power roles. If it boots on BJ,
    the type-c port will be only work as a source. If it boots on type-c,
    it'll stay as a sink.
    
    Without this patch, PR swap (SRC->SNK) causes Fizz to brown out.
    
    BUG=b:64277216
    BRANCH=none
    TEST=Boot on BJ. Verify Pixel XL is charged by type-c port. Initiate
    PR swap from Pixel. Verify PR swap is rejected. Boot on type-c.
    
    Change-Id: I8c3161ee9873e52007d4236fc762bff652f3ce9c
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/usb_pd_policy.c
+++ board/fizz/usb_pd_policy.c
@@ -128,12 +128,9 @@ int pd_board_checks(void)
 
 int pd_check_power_swap(int port)
 {
-	/*
-	 * Allow power swap as long as we are acting as a dual role device,
-	 * otherwise assume our role is fixed (not in S0 or console command
-	 * to fix our role).
-	 */
-	return pd_get_dual_role() == PD_DRP_TOGGLE_ON ? 1 : 0;
+	/* Reject PR swap */
+	CPRINTS("dnojiri: Rejecting PR swap");
+	return 0;
 }
 
 int pd_check_data_swap(int port, int data_role)
