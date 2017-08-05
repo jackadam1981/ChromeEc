e1af79dd05666fbae2bd20812e81cc61b4702852
Sam Hurst <shurst@google.com>
2017-08-05 13:24:40 -0700

    PD: Make current USB PD Stack REV 3 compliant
    Implement the following required features in the USB PD Rev. 3.0
    specification.
    
    Not_supported control message: Inform a port partner that a
    particular message is not supported.
    Battery capabilities extended message: Report battery design
    capacity and last full charge capacity.
    Battery status data message: Report battery state of charge
    Source-coordinated collision avoidance: New scheme to avoid
    collisions caused when both source and sink want to send messages.
    
    Cable communication: Only the VCONN source can communicate with the
    cable plug. This is NOT implemented because although the drivers have
    the capability of communicating with a cable plug, the PD stack doesn't
    currently need to talk to a cable plug. This is okay since the current
    PD design doesn't source or sink more than 3 amps and all Type-C cables
    are required to be 3 amp capable.
    
    BUG=b:64411727
    BRANCH=None
    TEST=`make -j buildall`
    Passed relevant PD Rev 2.0 compliance tests
    Successful PD negotiation with PD Rev 2.0 and 3.0 chargers
    Tested with low power none PD charger.
    Modified a Kevin to operate as a PD 3.0 charger and sent
    all required messages and verified the return messages.
    Also tested collision avoidance by verifying that a sink only
    transmits when the source indicates it's okay.
    Signed-off-by: Sam Hurst <shurst@chromium.org>
    
    Change-Id: Ifd77e92ec4e9106236f9221393d2bfb97263d979

--- board/strago/usb_pd_policy.c
+++ board/strago/usb_pd_policy.c
@@ -231,7 +231,8 @@ static int svdm_dp_status(int port, uint32_t *payload)
 {
 	int opos = pd_alt_mode(port, USB_SID_DISPLAYPORT);
 	payload[0] = VDO(USB_SID_DISPLAYPORT, 1,
-			 CMD_DP_STATUS | VDO_OPOS(opos));
+			 CMD_DP_STATUS | VDO_OPOS(opos),
+			 pd_get_vdo_ver(port));
 	payload[1] = VDO_DP_STATUS(0, /* HPD IRQ  ... not applicable */
 				   0, /* HPD level ... not applicable */
 				   0, /* exit DP? ... no */
@@ -256,7 +257,8 @@ static int svdm_dp_config(int port, uint32_t *payload)
 		    USB_SWITCH_CONNECT, pd_get_polarity(port));
 
 	payload[0] = VDO(USB_SID_DISPLAYPORT, 1,
-			 CMD_DP_CONFIG | VDO_OPOS(opos));
+			 CMD_DP_CONFIG | VDO_OPOS(opos),
+			 pd_get_vdo_ver(port));
 	payload[1] = VDO_DP_CFG(pin_mode,      /* pin mode */
 				1,             /* DPv1.3 signaling */
 				2);            /* UFP connected */
