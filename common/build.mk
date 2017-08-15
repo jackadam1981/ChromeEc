a1a95aef5cd6d8d3dc6b38ae7678629a402ed88a
Nicolas Boichat <drinkcat@chromium.org>
2017-08-15 15:53:30 +0800

    Makefile: Generate hashes for the touchpad FW
    Based on the passed TOUCHPAD_FW parameter to the make command, the
    build system generates hashes for the touchpad FW.
    
    To generate the hashes, gen_touchpad_hash splits the touchpad FW
    in blocks of CONFIG_UPDATE_PDU_SIZE, that are hashed individually
    (SHA-256), and then stored in the EC image.
    
    This will allow the USB updater code to verify the integrity of
    the touchpad firmware being flashed.
    
    When no FW is provided, zeros are output, which do not match
    any valid data.
    
    BRANCH=none
    BUG=b:63993173
    TEST=make TOUCHPAD_FW=SA459C-1211_ForGoogleHammer_3.0.bin \
              BOARD=hammer -j
    
    Change-Id: Ie347270aa9c00342de13489c9422e45e681b94c2
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- common/build.mk
+++ common/build.mk
@@ -148,3 +148,23 @@ ifneq ($(CONFIG_RSA_OPTIMIZED),)
 $(out)/RW/common/rsa.o: CFLAGS+=-O3
 $(out)/RO/common/rsa.o: CFLAGS+=-O3
 endif
+
+ifneq ($(CONFIG_TOUCHPAD_HASH_FW),)
+common-$(CONFIG_TOUCHPAD_HASH_FW)+=touchpad_fw_hash.o
+
+$(out)/%/common/touchpad_fw_hash.o: CFLAGS+=-Wa,-I$(out)
+$(out)/RO/common/touchpad_fw_hash.o: $(out)/touchpad_fw_hash.bin
+$(out)/RW/common/touchpad_fw_hash.o: $(out)/touchpad_fw_hash.bin
+
+$(out)/touchpad_fw_hash.bin: $(out)/touchpad_fw_hash.bin.new
+	@if ! cmp -s $@ $<; then \
+		mv $< $@; \
+	else \
+		rm $<; \
+	fi
+
+$(out)/touchpad_fw_hash.bin.new: $(out)/util/gen_touchpad_hash $(TOUCHPAD_FW)
+	$(call quiet,tp_hash,TPHASH )
+
+.PHONY: $(out)/touchpad_fw_hash.bin.new
+endif
