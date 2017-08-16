ef8e0ed4284f044791d84b8abc2589bffbf83904
Ryan Zhang <Ryan.Zhang@quanta.corp-partner.google.com>
2017-08-16 13:50:04 +0800

    Coral: update Sanyo battery data
    + ship command follow spec.
    + check DFET status. See change#419881
    + add console command make debug easier
    
    BUG=None
    BRANCH=master
    TEST=enter shipping mode good, release shipping good.
    
    Change-Id: I060ee518192baa4e9d2b2e60883893187dfbe93f
    Signed-off-by: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>

--- board/coral/battery.c
+++ board/coral/battery.c
@@ -21,7 +21,7 @@
 
 #define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
 
-/* Shutdown mode parameter to write to manufacturer access register */
+#define SB_SHUTDOWN_OFFSET	0x3A
 #define SB_SHUTDOWN_DATA	0xC574
 
 enum battery_type {
@@ -114,11 +114,11 @@ int board_cut_off_battery(void)
 	int rv;
 
 	/* Ship mode command must be sent twice to take effect */
-	rv = sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+	rv = sb_write(SB_SHUTDOWN_OFFSET, SB_SHUTDOWN_DATA);
 	if (rv != EC_SUCCESS)
 		return EC_RES_ERROR;
 
-	rv = sb_write(SB_MANUFACTURER_ACCESS, SB_SHUTDOWN_DATA);
+	rv = sb_write(SB_SHUTDOWN_OFFSET, SB_SHUTDOWN_DATA);
 	return rv ? EC_RES_ERROR : EC_RES_SUCCESS;
 }
 
@@ -243,11 +243,18 @@ static int battery_init(void)
 
 
 /*
- * Physical detection of battery.
+ * 1. Physical detection of battery.
+ * 2. Check DFET is on/off by reading battery custom register
+ *
+ *    SANYO  : SB_MANUFACTURER_ACCESS.[14] : Discharge FET
+ *           : 0b - Not allowed to discharge
+ *           : 1b - Allowed to discharge
  */
+#define SANYO_DISCHARGE_FET_BIT   (0x01 << 14)
 enum battery_present battery_is_present(void)
 {
 	enum battery_present batt_pres;
+	int batt_discharge_fet = -1;
 
 	/* Get the physical hardware status */
 	batt_pres = battery_hw_present();
@@ -267,6 +274,13 @@ enum battery_present battery_is_present(void)
 	if (batt_pres == BP_YES && batt_pres_prev != batt_pres &&
 	    !battery_is_cut_off() && !battery_init()) {
 		batt_pres = BP_NO;
+	}
+
+	if (batt_pres == BP_YES) {
+		if (sb_read(SB_MANUFACTURER_ACCESS, &batt_discharge_fet))
+			batt_pres = BP_NOT_SURE;
+		else if ((batt_discharge_fet & SANYO_DISCHARGE_FET_BIT) == 0)
+			batt_pres = BP_NOT_SURE;
 	}
 
 	batt_pres_prev = batt_pres;
@@ -294,3 +308,31 @@ enum ec_status charger_profile_override_set_param(uint32_t param,
 {
 	return EC_RES_INVALID_PARAM;
 }
+
+static int command_battery_status(int argc, char **argv)
+{
+	int batt_discharge_fet = -1;
+
+	if (battery_hw_present() != BP_YES) {
+		ccprintf("battery HW is not present\n");
+		return 0;
+	}
+
+	if (battery_is_cut_off()) {
+		ccprintf("battery is cut off\n");
+		return 0;
+	}
+
+	if (sb_read(SB_MANUFACTURER_ACCESS, &batt_discharge_fet)) {
+		ccprintf("battery i2c failed\n");
+		return 0;
+	}
+
+	ccprintf("DFET= %s\n",
+		 (batt_discharge_fet & SANYO_DISCHARGE_FET_BIT) == 0 ?
+		 "off" : "on");
+
+	return 0;
+}
+DECLARE_CONSOLE_COMMAND(battery_status, command_battery_status,
+			"show DFET status", NULL);
