2a659e6daa27b62f5edc05284250cdfa74a6a9c8
Randall Spangler <rspangler@chromium.org>
2017-08-17 12:02:45 -0700

    cr50: Refactor Rdd state machine
    The code to mirror Rdd detect into CCD_MODE_L and handle keepalive is
    now inside chip/g/rdd.c It uses a HOOK_SECOND state machine similar to
    what's coming for EC/AP/Servo.  The Rdd connect interrupt is still
    enabled all the time, so that connecting a cable will wake Cr50 from
    deep sleep.
    
    This also removes the explicit 'ccd enable' / 'ccd disable' commands,
    since they'd be overridden by the HOOK_SECOND handler.  If you need to
    force CCD enabled, use 'ccd keepalive enable'.  Do we ever need to
    force CCD disabled?
    
    BUG=b:64799106
    BRANCH=cr50
    TEST=Manually pull RDCC1 and RDCC2 to 0.2V - 2.0V.  Cable detected.
         Pull them outside that range, cable not detected.
         idle d, wait for sleep, then pull RDCCx and see Cr50 wake.
    
    Change-Id: I65110b45e76f60390828e0fbbac8f36fc2cc9b37
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- chip/g/rdd.h
+++ chip/g/rdd.h
@@ -6,10 +6,21 @@
 #ifndef __CROS_RDD_H
 #define __CROS_RDD_H
 
-/* Detach from debug cable */
-void rdd_detached(void);
+/**
+ * Enable/disable forcing debug accessory detection.
+ *
+ * When enabled, the RDD module will assert CCD_MODE_L even if the CC value
+ * does not indicate a debug accessory is present.
+ *
+ * @param enable	Enable (1) or disable (0) keepalive.
+ */
+void force_rdd_detect(int enable);
 
-/* Attach to debug cable */
-void rdd_attached(void);
+/**
+ * Check if debug accessory detection is forced.
+ *
+ * @return 1 if keepalive is enabled, 0 if disabled.
+ */
+int rdd_detect_is_forced(void);
 
 #endif  /* __CROS_RDD_H */
