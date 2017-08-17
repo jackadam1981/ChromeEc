57932e652bbc1361a1286880883ec71d3b32e9ca
Randall Spangler <rspangler@chromium.org>
2017-08-17 13:01:32 -0700

    cr50: Split EC state machine into its own file
    The device state machines aren't quite similar enough to use common
    code.  Split the EC state machine out, the way we split out BattPrsnt
    and CCD_MODE.
    
    BUG=b:35587387
    BRANCH=cr50
    TEST=manual
    	Pull CCD_MODE_L high, so Cr50 detects/enables CCD
    	Pull EC_DETECT high.
    	reboot -> 'EC RX only', then 'EC on' at 1 second
    
    	Pull EC_DETECT low --> See 'EC off' message
    	ccd --> EC UART disabled
    
    	Pull EC_DETECT high --> See 'EC on' message
    	ccd --> EC UART RX+TX
    
    	Pull EC_DETECT low for <1 sec then back high
    	(don't see EC off/on messages)
    	ccd --> EC UART RX+TX
    
    	Reboot with EC_DETECT still low -> EC off at 1 second
    
    	Reboot with EC_DETECT still low and then assert EC_DETECT
    	within a second -> EC RX only, then EC connect at 1 second.
    
    Change-Id: I71687e651d625cadd656934f4cb2bbadc0b58816
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

---
+++ board/cr50/ec_state.c
@@ -0,0 +1,115 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * EC detect state machine.
+ */
+#include "common.h"
+#include "console.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "uart_bitbang.h"
+#include "uartn.h"
+
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
+
+static enum device_state state = DEVICE_STATE_INIT;
+
+int ec_is_on(void)
+{
+	/* Debouncing and on are both still on */
+	return (state == DEVICE_STATE_DEBOUNCING || state == DEVICE_STATE_ON);
+}
+
+/**
+ * Connect to the EC
+ *
+ * This can be deferred from both ec_detect() and the interrupt handler, so it
+ * needs to check the current state to determine whether we're already
+ * connected.
+ */
+static void ec_connect(void)
+{
+	if (state == DEVICE_STATE_INIT ||
+	    state == DEVICE_STATE_INIT_DEBOUNCING) {
+		/*
+		 * Enable the UART peripheral so we start receiving on EC RX,
+		 * but do not call uartn_tx_connect() to connect EC TX yet.  We
+		 * need to be able to use EC TX to detect servo, so if we drive
+		 * it right away that blocks us from detecting servo.
+		 */
+		CPRINTS("EC RX only");
+		uartn_enable(UART_EC);
+		state = DEVICE_STATE_INIT_RX_ONLY;
+		return;
+	}
+
+	/* If we were debouncing, we're done, and still on */
+	if (state == DEVICE_STATE_DEBOUNCING)
+		state = DEVICE_STATE_ON;
+
+	/* If we're already on, done */
+	if (state == DEVICE_STATE_ON)
+		return;
+
+	/* We were previously off */
+	CPRINTS("EC on");
+	state = DEVICE_STATE_ON;
+
+	/*
+	 * Enable UART RX if we're not bit-banging.  Note that we can't enable
+	 * transmit right away, because that will interfere with servo
+	 * detection during boot.  In the current implementation this isn't a
+	 * problem because we don't enable UART TX until an explicit console
+	 * command, but this will become an issue with CCD V1.
+	 */
+	if (!uart_bitbang_is_enabled(UART_EC))
+		enable_uart(UART_EC);
+}
+DECLARE_DEFERRED(ec_connect);
+
+/**
+ * Interrupt handler for EC detect asserted.
+ */
+void ec_detect_asserted(enum gpio_signal signal)
+{
+	gpio_disable_interrupt(GPIO_DETECT_EC);
+	hook_call_deferred(&ec_connect_data, 0);
+}
+
+/**
+ * Detect state machine
+ */
+static void ec_detect(void)
+{
+	/* Disable interrupts if we had them on for debouncing */
+	gpio_disable_interrupt(GPIO_DETECT_EC);
+
+	/* Handle detecting device */
+	if (gpio_get_level(GPIO_DETECT_EC)) {
+		/* Go back to connected */
+		ec_connect();
+		return;
+	}
+
+	/* EC wasn't detected.  If we're already off, done. */
+	if (state == DEVICE_STATE_OFF)
+		return;
+
+	/* If we were debouncing, we're now sure we're off */
+	if (state == DEVICE_STATE_DEBOUNCING ||
+	    state == DEVICE_STATE_INIT_DEBOUNCING) {
+		CPRINTS("EC off");
+		state = DEVICE_STATE_OFF;
+		disable_uart(UART_EC);
+		return;
+	}
+
+	/* Otherwise, we were on and need to start debouncing */
+	if (state == DEVICE_STATE_INIT)
+		state = DEVICE_STATE_INIT_DEBOUNCING;
+	else
+		state = DEVICE_STATE_DEBOUNCING;
+	gpio_enable_interrupt(GPIO_DETECT_EC);
+}
+DECLARE_HOOK(HOOK_SECOND, ec_detect, HOOK_PRIO_DEFAULT);
