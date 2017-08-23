91647bda24fc7490d2170fe3a75ab26e659115c3
Randall Spangler <rspangler@chromium.org>
2017-08-23 14:40:29 -0700

    cr50: Split AP state machine into its own file
    The device state machines aren't quite similar enough to use common
    code.  Split the AP state machine out, the way we split out the EC
    state machine in the previous CL.
    
    BUG=b:35587387
    BRANCH=cr50
    TEST=manual, with Cr50 strapped (or hard-coded) not to use platform reset:
            Pull CCD_MODE_L low, so Cr50 detects/enables CCD
            Pull AP_DETECT high.
    	Pull INT_AP_L low (with resistor).
    
            Pull AP_DETECT low --> See 'AP disconnect' message
    	gpioget --> INT_AP_L=0
            ccd --> AP UART disabled
    
            Pull AP_DETECT high --> See 'AP connect' message
    	gpioget --> INT_AP_L=1
            ccd --> AP UART RX+TX
    
            Pull AP_DETECT low for <1 sec then back high
            (don't see AP disconnect/connect message)
    	gpioget --> INT_AP_L=1
            ccd --> AP UART RX+TX
    
    	Repeat with Cr50 strapped/hard coded to use platform reset, but
    	using TPM_RST_L instead of AP_DETECT.  Note that this will also
    	show TPM reset debugging output when TPM_RST_L is asserted.
    
    Change-Id: Ief9e4e5f2585ff925de1595cc8fbd5306c94a806
    Signed-off-by: Randall Spangler <rspangler@chromium.org>

--- board/cr50/board.h
+++ board/cr50/board.h
@@ -173,8 +173,7 @@ enum usb_strings {
 
 /* Device indexes for devices that require debouncing */
 enum device_type {
-	DEVICE_AP = 0,
-	DEVICE_SERVO,
+	DEVICE_SERVO = 0,
 
 	DEVICE_COUNT
 };
@@ -229,6 +228,7 @@ enum nvmem_vars {
 void board_configure_deep_sleep_wakepins(void);
 /* Interrupt handler */
 void tpm_rst_deasserted(enum gpio_signal signal);
+void ap_detect_asserted(enum gpio_signal signal);
 void ec_detect_asserted(enum gpio_signal signal);
 void device_state_on(enum gpio_signal signal);
 void post_reboot_request(void);
@@ -260,12 +260,15 @@ int board_is_first_factory_boot(void);
 void enable_uart(int uart);
 void disable_uart(int uart);
 
+void init_ap_state(void);
 void init_ec_state(void);
 
 int ap_is_on(void);
 int ec_is_on(void);
 int rdd_is_connected(void);
 int servo_is_connected(void);
+
+void ap_connect_deferred(void);
 
 /* Returns True if chip is brought up in a factory test harness. */
 int chip_factory_mode(void);
