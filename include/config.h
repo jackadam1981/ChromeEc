1529b1c3900088b292eb207d7b4d0182b60f6b6b
Daisuke Nojiri <dnojiri@chromium.org>
2017-08-24 18:15:19 -0700

    EFS: Rename CONFIG_VBOOT_EC to _EFS
    This patch renames CONFIG_VBOOT_ET to CONFIG_VBOOT_EFS. It also
    adds the macro to config.h.
    
    BUG=none
    BRANCH=none
    TEST=make buidlall
    
    Change-Id: I7cb9f4c73da635b36119db74bac6fe26e77a07d2
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/config.h
+++ include/config.h
@@ -2805,6 +2805,9 @@
 
 /*****************************************************************************/
 
+/* Support early firmware selection */
+#undef CONFIG_VBOOT_EFS
+
 /* Support computing hash of code for verified boot */
 #undef CONFIG_VBOOT_HASH
 
