af774d16b1237a4b47f7a65034bdafa17292cf4e
Aseda Aboagye <aaboagye@google.com>
2017-08-25 18:12:53 -0700

    cr50: Add vendor cmd to query rec btn state.
    In order to test certain devices in the test lab using cased closed
    debugging (CCD), we need a mechanism to programmatically "press" the
    recovery button.  Even though this signal is being buffered through the
    EC to the AP, and the EC could theoretically set it if needed, we cannot
    trust the EC in its RO image since it may speak to the outside world
    over USB PD.
    
    Instead, this commit introduces a console command on cr50 that can be
    used to force the recovery button state to be pressed.  However, it is
    gated behind the CCD capabilities defaulting to the IfOpened state.
    Additionally, a new vendor command is added to query the trusted state
    of the recovery button.  The AP should use this command instead of
    trusting the GPIO connected to it.
    
    BUG=b:37751915, b:64146626
    BRANCH=cr50
    TEST=Flash cr50.  Press KEY0, verify that reported rec btn follows the
    state of the button.
    TEST=`recbtnforce enable` causes the button to be reported as pressed.
    Physical presses of the button do not alter the reported state.
    TEST=`recbtnforce disable` causes the reported state to follow that of
    the physical button.
    TEST=The `recbtnforce` command is set to the IfOpened capability by
    default.
    TEST=CCD locking the DUT and attempting to force the recovery button
    results in an error of access denied.  The physical state is reported.
    TEST=From the AP, issue the vendor command to retrieve the button state
    and verify that it behaves as expected.
    
    Change-Id: Ib8c2928e75e5f9a1a83c5361664efc3fa0ae2ddb
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -1851,3 +1851,63 @@ int chip_factory_mode(void)
 
 	return mode_set & 1;
 }
+
+/*
+ * The recovery button is wired to KEY0 in rbox.  For testing, we need to be
+ * able override the value.  We'll have a vendor command such that the AP can
+ * query the state of the recovery button.  However, the reported state can only
+ * be overridden with a console command given sufficient privileges.
+ */
+static uint8_t rec_btn_force_pressed;
+
+static uint8_t is_rec_btn_pressed(void)
+{
+	if (rec_btn_force_pressed)
+		return 1;
+
+	/*
+	 * If not force pressed, check the actual state of button.  Note,
+	 * the value is inverted because the button is active low.
+	 */
+	return !GREAD_FIELD(RBOX, CHECK_INPUT, KEY0_IN);
+}
+
+static int command_recbtnforce(int argc, char **argv)
+{
+	if (argc > 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	if (argc == 2) {
+		/* Make sure we're allowed to override the recovery button. */
+		if (!ccd_is_cap_enabled(CCD_CAP_CR50_RESTRICTED_CONSOLE))
+			return EC_ERROR_ACCESS_DENIED;
+
+		if (!strncasecmp(argv[1], "enable", 6))
+			rec_btn_force_pressed = 1;
+		else if (!strncasecmp(argv[1], "disable", 7))
+			rec_btn_force_pressed = 0;
+		else
+			return EC_ERROR_PARAM1;
+	}
+
+	ccprintf("RecBtn: %s%spressed\n",
+		 rec_btn_force_pressed ? "forced " : "",
+		 is_rec_btn_pressed() ? "" : "not ");
+
+	return EC_SUCCESS;
+}
+DECLARE_SAFE_CONSOLE_COMMAND(recbtnforce, command_recbtnforce,
+			     "[enable | disable]",
+			     "Force enable the reported recbtn state.");
+
+static enum vendor_cmd_rc vc_get_rec_btn(enum vendor_cmd_cc code,
+					 void *buf,
+					 size_t input_size,
+					 size_t *response_size)
+{
+	*(uint8_t *)buf = is_rec_btn_pressed();
+	*response_size = 1;
+
+	return VENDOR_RC_SUCCESS;
+}
+DECLARE_VENDOR_COMMAND(VENDOR_CC_GET_REC_BTN, vc_get_rec_btn);
