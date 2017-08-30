fddafa4e0137975cf6132f4b027ec4822708bca6
Cloud Lin <cloud_lin@compal.com>
2017-08-30 17:32:49 +0800

    Kahlee: set g-sensor ec rate
    Set g-sensor ec rate to update g-sensor data to EC MEMMAP.
    
    BRANCH=none
    BUG=b:62029360
    TEST=use "iotools io-read16 0x994" to check MEMMAP data.
    Signed-off-by: cloud lin <cloud_lin@compal.com>
    
    Change-Id: I7ceca0e2b6a4035d6564ac33ab43edeeeca65652

--- board/kahlee/board.c
+++ board/kahlee/board.c
@@ -574,7 +574,7 @@ struct motion_sensor_t motion_sensors[] = {
 		/* EC use accel for angle detection */
 		[SENSOR_CONFIG_EC_S0] = {
 			.odr = 10000 | ROUND_UP_FLAG,
-			.ec_rate = 0,
+			.ec_rate = 100 * MSEC,
 		},
 		 /* Sensor on for lid angle detection */
 		[SENSOR_CONFIG_EC_S3] = {
