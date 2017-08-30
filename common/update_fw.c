c2efc86fde8ad1ddf06dad5e64e4ae99ba828641
Nicolas Boichat <drinkcat@chromium.org>
2017-08-30 15:02:48 +0800

    update_fw: Store complete touchpad FW hash
    This will be used by the updater to first check that the touchpad
    FW on AP side matches the one for which we stored hashes on EC
    side.
    
    This guarantee that we do not accidentally try to flash an
    incorrect FW, which would render the touchpad non-functional.
    
    BRANCH=none
    BUG=b:63993173
    TEST=make TOUCHPAD_FW=SA459C-1211_ForGoogleHammer_3.0.bin \
              BOARD=hammer -j
    TEST=./usb_updater2 -t
         includes first 16 bytes of
         sha256sum A459C-1211_ForGoogleHammer_3.0.bin
    
    Change-Id: Id30ab2d7c7d7e2d0f25cc893f685d218c44c022e
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- common/update_fw.c
+++ common/update_fw.c
@@ -27,6 +27,8 @@
 BUILD_ASSERT(sizeof(touchpad_fw_hashes) ==
 		(CONFIG_TOUCHPAD_FW_CHUNKS * SHA256_DIGEST_SIZE));
 BUILD_ASSERT(sizeof(touchpad_fw_hashes[0]) == SHA256_DIGEST_SIZE);
+
+BUILD_ASSERT(sizeof(touchpad_fw_hash) == TOUCHPAD_FW_HASH_LENGTH);
 #endif
 
 #define CPRINTF(format, args...) cprintf(CC_USB, format, ## args)
