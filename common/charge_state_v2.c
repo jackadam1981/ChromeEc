202714b3a4b0a54a604ee0c61e47198ad496f32b
Ryan Zhang <Ryan.Zhang@quanta.corp-partner.google.com>
2017-09-01 14:12:57 +0800

    charger: Add CONFIG_CHARGE_STATE_DEBUG
    Servo / Suzy-Q related debugging methods is a big challenge
    in factory especially after servo debug header is removed.
    
    Expose some information to OS from EC will do a great help
    for massive production.
    
    + expose charge/battery related state to ectool
    	1. chg_ctl_mode
    	2. manual_mode
    	3. battery_seems_to_be_dead
    	4. battery_seems_to_be_disconnected
    	5. battery_was_removed
    	6. disch_on_ac (learn mode state)
    
    BUG=b:65265543
    BRANCH=master
    TEST=`ectool chargestate param 0x20000~0x20006 get correct state`
    
    Change-Id: Ic2ed38e2eb9def01be29729fa1fe1959eb73fe43
    Signed-off-by: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -1207,6 +1207,18 @@ static int charge_command_charge_state(struct host_cmd_handler_args *args)
 		    in->get_param.param <= CS_PARAM_CUSTOM_PROFILE_MAX) {
 			rv  = charger_profile_override_get_param(
 				in->get_param.param, &val);
+#endif
+#ifdef CONFIG_CHARGE_STATE_DEBUG
+#ifdef CONFIG_CHARGER_PROFILE_OVERRIDE
+		} else
+#endif
+			/* debug params */
+			if (in->get_param.param >= CS_PARAM_DEBUG_MIN &&
+			    in->get_param.param <= CS_PARAM_DEBUG_MAX) {
+				rv = charge_get_charge_state_debug(
+					in->get_param.param, &val);
+#endif
+#if defined(CONFIG_CHARGER_PROFILE_OVERRIDE) | defined(CONFIG_CHARGE_STATE_DEBUG)
 		} else
 #endif
 			/* standard params */
@@ -1357,3 +1369,31 @@ static int command_chgstate(int argc, char **argv)
 DECLARE_CONSOLE_COMMAND(chgstate, command_chgstate,
 			"[idle|discharge|debug on|off]",
 			"Get/set charge state machine status");
+
+#ifdef CONFIG_CHARGE_STATE_DEBUG
+int charge_get_charge_state_debug(int param, uint32_t *value)
+{
+	switch (param) {
+	case CS_PARAM_DEBUG_CTL_MODE:
+		*value = chg_ctl_mode;
+		break;
+	case CS_PARAM_DEBUG_MANUAL_MODE:
+		*value = manual_mode;
+		break;
+	case CS_PARAM_DEBUG_SEEMS_DEAD:
+		*value = battery_seems_to_be_dead;
+		break;
+	case CS_PARAM_DEBUG_SEEMS_DISCONNECTED:
+		*value = battery_seems_to_be_disconnected;
+		break;
+	case CS_PARAM_DEBUG_BATT_REMOVED:
+		*value = battery_was_removed;
+		break;
+	default:
+		*value = 0;
+		return EC_ERROR_INVAL;
+	}
+
+	return EC_SUCCESS;
+}
+#endif
