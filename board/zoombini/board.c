6e5f6a709b5e8cb4ba0cc2953d2cbefa1db5c1d2
Aseda Aboagye <aaboagye@google.com>
2017-09-16 17:26:45 -0700

    HACK: zoombini: Manually update Vbus suppliers.
    The Vbus charge supplier entries weren't being filled out which was
    causing charge manager not to be seeded.  Since we can't yet detect
    Vbus, assume that ACOK is Vbus for port 0 only.
    
    This is only temporary until the TCPCs can actually report Vbus status.
    
    Change-Id: Ib6401d672a8d1d2fc0d4f382cb52c62804298270

--- board/zoombini/board.c
+++ board/zoombini/board.c
@@ -189,10 +189,25 @@ int board_get_ramp_current_limit(int supplier, int sup_curr)
 
 static void board_init(void)
 {
+	struct charge_port_info chg = { 0 };
+
 	/* Enable TCPC interrupts. */
 	gpio_enable_interrupt(GPIO_USB_C0_PD_INT_L);
 	gpio_enable_interrupt(GPIO_USB_C1_PD_INT_L);
 	gpio_enable_interrupt(GPIO_USB_C2_PD_INT_L);
+
+	/* HACK.  Update VBUS suppliers. */
+	/* The 'ol assuming that AC present is Vbus on port 0... */
+	chg.voltage = gpio_get_level(GPIO_AC_PRESENT) ? 5000 : 0;
+	if (chg.voltage)
+		chg.current = USB_CHARGER_MIN_CURR_MA;
+
+	charge_manager_update_charge(CHARGE_SUPPLIER_VBUS, 0, &chg);
+	/* The others don't "have" Vbus... */
+	chg.voltage = 0;
+	chg.current = 0;
+	charge_manager_update_charge(CHARGE_SUPPLIER_VBUS, 1, &chg);
+	charge_manager_update_charge(CHARGE_SUPPLIER_VBUS, 2, &chg);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
