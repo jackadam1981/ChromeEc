649228033a820d863dbd6a3668416178fff068f1
Nicolas Boichat <drinkcat@chromium.org>
2017-09-18 17:02:09 +0800

    wand: Add wand board
    hammer derivative, with a battery.
    
    BRANCH=none
    BUG=b:66575472
    TEST=make BOARD=wand -j, flash to reworked hammer board,
         battery charging works.
    
    Change-Id: I42366f4738b35c352b9be0c44a17ebeb323adfd1
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

---
+++ board/hammer/battery.c
@@ -0,0 +1,83 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "battery.h"
+#include "battery_smart.h"
+#include "charge_state.h"
+#include "console.h"
+#include "ec_commands.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "util.h"
+
+/* Shutdown mode parameter to write to manufacturer access register */
+#define SB_SHIP_MODE_REG	SB_MANUFACTURER_ACCESS
+#define SB_SHUTDOWN_DATA        0x0010
+
+static const struct battery_info info = {
+	.voltage_max = 8800,
+	.voltage_normal = 7700,
+	.voltage_min = 6000,
+	/* Pre-charge values. */
+	.precharge_current = 256, /* mA */
+
+	.start_charging_min_c = 0,
+	.start_charging_max_c = 45,
+	.charging_min_c = 0,
+	.charging_max_c = 60,
+	.discharging_min_c = -20,
+	.discharging_max_c = 60,
+};
+
+const struct battery_info *battery_get_info(void)
+{
+	return &info;
+}
+
+int board_cut_off_battery(void)
+{
+	int rv;
+
+	/* Ship mode command must be sent twice to take effect */
+	rv = sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA);
+
+	if (rv != EC_SUCCESS)
+		return rv;
+
+	return sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA);
+}
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	const struct battery_info *batt_info;
+	int bat_temp_c;
+
+	batt_info = battery_get_info();
+
+	if ((curr->batt.flags & BATT_FLAG_BAD_ANY) == BATT_FLAG_BAD_ANY) {
+		curr->requested_current = batt_info->precharge_current;
+		curr->requested_voltage = batt_info->voltage_max;
+		return 1000;
+	}
+
+	return 0;
+}
+
+/* Customs options controllable by host command. */
+#define PARAM_FASTCHARGE (CS_PARAM_CUSTOM_PROFILE_MIN + 0)
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+/* TODO(b:66575472): Do we need to define functions like battery_is_present? */
