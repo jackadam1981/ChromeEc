9e95931e97591e427ffa4474c402d2a6b2f310b3
Daisuke Nojiri <dnojiri@chromium.org>
2017-09-22 10:53:26 -0700

    Fizz: Set proper max current and power
    i3/5/7 Fizz takes 90W @4.62A from BJ adapter. We're applying these
    numbers as the max power and current for Type-C adapters of all SKUs.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I9670d710e363c7db1136a7ce7a7f8401b0ad8240
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/board.h
+++ board/fizz/board.h
@@ -222,8 +222,8 @@ enum pwm_channel {
 
 /* Define typical operating power and max power */
 #define PD_OPERATING_POWER_MW		15000
-#define PD_MAX_POWER_MW			45000
-#define PD_MAX_CURRENT_MA		3000
+#define PD_MAX_POWER_MW			90000
+#define PD_MAX_CURRENT_MA		4620
 #define PD_MAX_VOLTAGE_MV		20000
 
 /* Board specific handlers */
