756587fe4d908bb7f2a610c258b31a14edf640ad
Daisuke Nojiri <dnojiri@chromium.org>
2017-09-25 14:53:55 -0700

    Fizz: Show critical error on LED for recovery request
    Fizz EC verifies RW by itself and jumps to RW before AP boots.
    If this fails, the system needs recovery. Since EC isn't
    capable of showing any info on a display, we use the power
    LED to inform the user.
    
    BUG=b:66914368
    BRANCH=none
    TEST=Make Fizz fail RW verification. Observe LED illuminates
    in red.
    
    Change-Id: Ia07de60a316b40e74b1917903996d78750b4ae43
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/led.c
+++ board/fizz/led.c
@@ -174,6 +174,13 @@ void led_alert(int enable)
 	}
 }
 
+void led_critical(void)
+{
+	hook_call_deferred(&led_tick_data, -1);
+	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
+		set_color(EC_LED_ID_POWER_LED, LED_RED, 100);
+}
+
 static int command_led(int argc, char **argv)
 {
 	enum ec_led_id id = EC_LED_ID_POWER_LED;
