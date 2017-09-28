ee4714e731a765d9c8e4c1d6dba921e5738cf25b
Nicolas Boichat <drinkcat@chromium.org>
2017-09-28 16:27:38 +0800

    lux: Add lux board
    lux is a dual-battery poppy derivative.
    
    BRANCH=none
    BUG=b:67029560
    TEST=make BOARD=lux -j
    
    Change-Id: I5efd5adc91cbafb6df365928a781d715c7199b10
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- board/poppy/board.h
+++ board/poppy/board.h
@@ -73,6 +73,14 @@
 #define CONFIG_BATTERY_PRESENT_CUSTOM
 #define CONFIG_BATTERY_SMART
 
+#ifdef BOARD_LUX
+#define CONFIG_UART_PAD_SWITCH
+
+#define CONFIG_EC_EC_COMM_MASTER
+#define CONFIG_EC_EC_COMM_BATTERY
+#define CONFIG_CRC8
+#endif
+
 /* Charger */
 #define CONFIG_CHARGE_MANAGER
 #define CONFIG_CHARGE_RAMP_HW /* This, or just RAMP? */
