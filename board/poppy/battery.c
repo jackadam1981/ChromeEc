c799030d81272365c36f7b65ba25bf3f0162891e
Nicolas Boichat <drinkcat@chromium.org>
2017-09-28 16:27:38 +0800

    lux: Add lux board
    For now, lux is mostly compatible with soraka, with hacks.
    
    We also add support for pad-switching UART.
    
    BRANCH=none
    BUG=b:67029560
    TEST=make BOARD=lux -j
    
    Change-Id: I5efd5adc91cbafb6df365928a781d715c7199b10
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- board/poppy/battery.c
+++ board/poppy/battery.c
@@ -20,7 +20,7 @@ static enum battery_present batt_pres_prev = BP_NOT_SURE;
 #define SB_SHIP_MODE_REG	SB_MANUFACTURER_ACCESS
 #define SB_SHUTDOWN_DATA        0x0010
 
-#ifdef BOARD_SORAKA
+#if defined(BOARD_SORAKA) || defined(BOARD_LUX)
 static const struct battery_info info = {
 	.voltage_max = 8800,
 	.voltage_normal = 7700,
