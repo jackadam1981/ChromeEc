339873745d6519088a6f40e731269115ca4cde5d
Vincent Palatin <vpalatin@chromium.org>
2017-10-03 17:54:35 +0200

    pd: support SOP'/SOP'' in software TCPC
    Add code to send and receive SOP' and SOP'' messages in the software
    implementation of the TCPC.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=twinkie
    BUG=none
    TEST=none
    
    Change-Id: I93669691eb8f64acbdf3765afb02499dd043d457

--- common/usb_pd_tcpc.c
+++ common/usb_pd_tcpc.c
@@ -298,11 +298,25 @@ int prepare_message(int port, uint16_t header, uint8_t cnt,
 	int off, i;
 	/* 64-bit preamble */
 	off = pd_write_preamble(port);
-	/* Start Of Packet: 3x Sync-1 + 1x Sync-2 */
-	off = pd_write_sym(port, off, BMC(PD_SYNC1));
-	off = pd_write_sym(port, off, BMC(PD_SYNC1));
-	off = pd_write_sym(port, off, BMC(PD_SYNC1));
-	off = pd_write_sym(port, off, BMC(PD_SYNC2));
+	if (pd[port].tx_type == TCPC_TX_SOP_PRIME) {
+		/* SOP': 2x Sync-1 + 2x Sync-3 */
+		off = pd_write_sym(port, off, BMC(PD_SYNC1));
+		off = pd_write_sym(port, off, BMC(PD_SYNC1));
+		off = pd_write_sym(port, off, BMC(PD_SYNC3));
+		off = pd_write_sym(port, off, BMC(PD_SYNC3));
+	} else if (pd[port].tx_type == TCPC_TX_SOP_PRIME_PRIME) {
+		/* SOP'': Sync-1, Sync-3, Sync-1, Sync-3 */
+		off = pd_write_sym(port, off, BMC(PD_SYNC1));
+		off = pd_write_sym(port, off, BMC(PD_SYNC3));
+		off = pd_write_sym(port, off, BMC(PD_SYNC1));
+		off = pd_write_sym(port, off, BMC(PD_SYNC3));
+	} else {
+		/* Start Of Packet: 3x Sync-1 + 1x Sync-2 */
+		off = pd_write_sym(port, off, BMC(PD_SYNC1));
+		off = pd_write_sym(port, off, BMC(PD_SYNC1));
+		off = pd_write_sym(port, off, BMC(PD_SYNC1));
+		off = pd_write_sym(port, off, BMC(PD_SYNC2));
+	}
 	/* header */
 	off = encode_short(port, off, header);
 
@@ -637,11 +651,9 @@ int pd_analyze_rx(int port, uint32_t *payload)
 		if (val == PD_SOP) {
 			break;
 		} else if (val == PD_SOP_PRIME) {
-			CPRINTF("SOP'\n");
-			return PD_RX_ERR_UNSUPPORTED_SOP;
+			break;
 		} else if (val == PD_SOP_PRIME_PRIME) {
-			CPRINTF("SOP''\n");
-			return PD_RX_ERR_UNSUPPORTED_SOP;
+			break;
 		}
 	}
 	if (bit < 0) {
@@ -792,6 +804,8 @@ int tcpc_run(int port, int evt)
 	if ((evt & PD_EVENT_TX) && pd[port].rx_enabled) {
 		switch (pd[port].tx_type) {
 		case TCPC_TX_SOP:
+		case TCPC_TX_SOP_PRIME:
+		case TCPC_TX_SOP_PRIME_PRIME:
 			res = send_validate_message(port,
 					pd[port].tx_head,
 					pd[port].tx_data);
