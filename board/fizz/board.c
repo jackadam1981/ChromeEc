f3f95d2d7eb39417a4403323f15a3a28a3db4be3
Daisuke Nojiri <dnojiri@chromium.org>
2017-10-03 18:28:57 -0700

    Fizz: Read board info from EEPROM
    We store board information in EEPROM.
    
    BUG=b:70294260
    BRANCH=none
    TEST=Read/write the data from/to EEPROM.
    
    Change-Id: I7eb4323188817d46b0450f1d65ac34d1b7e4e220

--- board/fizz/board.c
+++ board/fizz/board.c
@@ -16,6 +16,7 @@
 #include "charge_state.h"
 #include "charger.h"
 #include "chipset.h"
+#include "crc8.h"
 #include "console.h"
 #include "driver/pmic_tps650x30.h"
 #include "driver/temp_sensor/tmp432.h"
@@ -449,6 +450,8 @@ DECLARE_HOOK(HOOK_AC_CHANGE, board_extpower, HOOK_PRIO_DEFAULT);
 /* Initialize board. */
 static void board_init(void)
 {
+	CPRINTF("Board Version: 0x%x", board_get_version());
+
 	/* Provide AC status to the PCH */
 	board_extpower();
 
@@ -584,4 +587,132 @@ int fan_percent_to_rpm(int fan, int pct)
 			fan_table[current_level].rpm);
 
 	return fan_table[current_level].rpm;
+}
+
+#define BOARD_INFO_VERSION_MAJOR 0
+static const uint8_t board_info_magic[] = { 0x42, 0x44, 0x49 };
+static struct board_info bi;
+static int initialized;
+
+struct board_info_header {
+	uint8_t magic[3];
+	uint8_t crc;
+	union {
+		struct {
+			uint8_t minor;
+			uint8_t major;
+		};
+		uint16_t version;
+	};
+	uint16_t total_size;
+} __attribute__((packed));
+
+struct board_info {
+	struct board_info_header head;
+	union {
+		struct {
+			uint8_t minor;
+			uint8_t major;
+		};
+		uint16_t version;
+	};
+	uint8_t oem_id;
+	uint8_t sku_id;
+} __attribute__((packed));
+
+/**
+ * Get board information from EEPROM
+ *
+ * @param desc
+ * @param offset
+ * @return
+ */
+static int init_board_info(void)
+{
+	uint8_t buf[256];
+	uint8_t offset;
+
+	if (initialized)
+		return EC_SUCCESS;
+
+	/* Read header */
+	offset = 0;
+	if (i2c_xfer(NPCX_I2C_PORT0_1, I2C_ADDR_EEPROM,
+		     &offset, 1, buf, sizeof(bi.head), I2C_XFER_SINGLE)) {
+		CPRINTS("Failed to read BI header");
+		return EC_ERROR_INVAL;
+	}
+	memcpy(&bi.head, buf, sizeof(bi.head));
+
+	/* Check magic */
+	if (memcmp(bi.head.magic, board_info_magic, sizeof(bi.head.magic))) {
+		CPRINTS("Invalid BI magic");
+		return EC_ERROR_INVAL;
+	}
+
+	/* check version */
+	if (bi.head.major > BOARD_INFO_VERSION_MAJOR) {
+		CPRINTS("BI major version too high");
+		return EC_ERROR_INVAL;
+	}
+
+	/* Check the data size. It's expected to support up to 64k but our
+	 * buffer has practical limitation. */
+	if (bi.head.total_size > sizeof(buf)) {
+		CPRINTS("BI data too large");
+		return EC_ERROR_OVERFLOW;
+	}
+
+	/* Read the rest */
+	offset = sizeof(bi.head);
+	if (i2c_xfer(NPCX_I2C_PORT0_1, I2C_ADDR_EEPROM, &offset, 1,
+		     buf + sizeof(bi.head),
+		     bi.head.total_size - sizeof(bi.head),
+		     I2C_XFER_SINGLE)) {
+		CPRINTS("Failed to read BI body");
+		return EC_ERROR_INVAL;
+	}
+
+	/* Check CRC */
+	if (crc8(buf + 4, bi.head.total_size - 4) != bi.head.crc) {
+		CPRINTS("BI CRC mismatch");
+		return EC_ERROR_INVAL;
+	}
+
+	/* Every thing looks good. Save only the data we understand. */
+	memcpy(&bi.major, &buf[sizeof(bi.head)], sizeof(bi) - sizeof(bi.head));
+	initialized = 1;
+
+	return EC_SUCCESS;
+}
+
+int board_get_version(void)
+{
+	if (!initialized) {
+		if (init_board_info())
+			return -1;
+	}
+	return bi.version;
+}
+
+/*
+ * It can be named as system_get_sku_id() to plum it with the existing host
+ * command.
+ */
+int board_get_sku_id(void)
+{
+	if (!initialized) {
+		if (init_board_info())
+			return -1;
+	}
+	return bi.sku_id;
+}
+
+int board_get_oem_id(void)
+{
+	if (!initialized) {
+		if (init_board_info())
+			return -1;
+	}
+	return bi.oem_id;
 }
