a304579b122206cef5db81d9e6f6c31ef4bc5f3c
Daisuke Nojiri <dnojiri@chromium.org>
2017-10-10 16:53:25 -0700

    Fizz: Fix ADP_IN for Proto2
    ADP_IN on proto2 asserts to high but it currently uses the
    same logic as proto3 where ADP_IN asserts to low. This causes
    the active charge port to be detected wrongly.
    
    This problem is found on the following board:
      Board:   1
      RO:      fizz_v1.1.7102-86d5eb9b0
      RW_A:    fizz_v1.1.7102-86d5eb9b0
    
    BUG=none
    BRANCH=noe
    TEST=Verified the fix on the formentioned board.
    
    Change-Id: Ia23d8400c8fbebf031a5269da971681197dd7ba4
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/usb_pd_policy.c
+++ board/fizz/usb_pd_policy.c
@@ -245,7 +245,11 @@ static void board_charge_manager_init(void)
 		/* TODO(dnojiri): Remove this case after proto1 is deprecated */
 		input_port = input_voltage > 5500 ?
 			CHARGE_PORT_BARRELJACK : CHARGE_PORT_TYPEC0;
-	else /* proto2 and onward */
+	else if (system_get_board_version() == 1)
+		/* TODO(dnojiri): Remove this case after proto2 is deprecated */
+		input_port = gpio_get_level(GPIO_ADP_IN_L) ?
+			CHARGE_PORT_BARRELJACK : CHARGE_PORT_TYPEC0 ;
+	else /* proto3 and onward */
 		input_port = gpio_get_level(GPIO_ADP_IN_L) ?
 			CHARGE_PORT_TYPEC0 : CHARGE_PORT_BARRELJACK;
 	CPRINTS("Power Source: p%d (%dmV)", input_port, input_voltage);
