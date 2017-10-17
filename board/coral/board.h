662d76a97ecde28eac3da9d1552efa54316fcd81
Peggy Chuang <peggychuang@ami.corp-partner.google.com>
2017-10-17 16:41:14 +0800

    TEST-ONLY: Coral: Enable ACPI messages display
    Now Firmware_ECBootTime will detect hcdebug, ACPI query, and port 80,
    but currently hcdebug and ACPI are disable in EC console
    to cause ECboot time test item failed to detect those parts then timeout.
    so submit this cl to enable ACPI messages.
    
    BUG=b:63488727
    BRANCH=master
    TEST=emerge-coral chromeos-ec, run "Firmware_ECBootTime" passed on robo
    
    Change-Id: Ife7180ff58531b9fa392e5321cff4c1e709d28e8
    Signed-off-by: Peggy Chuang <peggychuang@ami.corp-partner.google.com>

--- board/coral/board.h
+++ board/coral/board.h
@@ -8,15 +8,6 @@
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
 
-/*
- * By default, enable all console messages excepted HC, ACPI and event:
- * The sensor stack is generating a lot of activity.
- */
-#define CC_DEFAULT     (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
-#undef CONFIG_HOSTCMD_DEBUG_MODE
-#define CONFIG_HOSTCMD_DEBUG_MODE HCDEBUG_OFF
-
-
 /* EC console commands  */
 #define CONFIG_CMD_ACCELS
 #define CONFIG_CMD_ACCEL_INFO
