0b72d070b75239e4f9d6fba567a7dd2f860f26c8
Nicolas Boichat <drinkcat@chromium.org>
2017-10-18 10:05:56 +0800

    isl9238: Add support for providing power using OTG
    BRANCH=none
    BUG=b:66575472
    TEST=Flash lux and wand, wand can provide power to lux.
    
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Change-Id: I59091c509b78bacf9f382550ab380a77fbf68ba9

--- include/charger.h
+++ include/charger.h
@@ -65,10 +65,21 @@ int charger_get_status(int *status);
 int charger_set_mode(int mode);
 
 /**
- * For chargers that are able to supply 5V output power for OTG dongle, this
- * function enables or disables 5V power output.
+ * For chargers that are able to supply output power for OTG dongle, this
+ * function enables or disables power output.
  */
 int charger_enable_otg_power(int enabled);
+
+/**
+ * Sets OTG current limit and voltage (independent of whether OTG power is
+ * currently enabled).
+ *
+ * @param output_current	Requested current limit in mA.
+ * @param output_voltage	Requested voltage in mV.
+ *
+ * @return EC_SUCCESS on success, an error otherwise.
+ */
+int charger_set_otg_current_voltage(int output_current, int output_voltage);
 
 /**
  * Return the closest match the charger can supply to the requested current.
