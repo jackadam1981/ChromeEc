6483852340b55f91a16a6c36d71da7f825779645
Nicolas Boichat <drinkcat@chromium.org>
2017-10-18 10:05:56 +0800

    isl9238: Add support for providing power using OTG
    BRANCH=none
    BUG=b:66575472
    TEST=Flash lux and wand, wand can provide power to lux.
    
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Change-Id: I59091c509b78bacf9f382550ab380a77fbf68ba9

--- driver/charger/isl923x.c
+++ driver/charger/isl923x.c
@@ -97,6 +97,68 @@ int charger_get_input_current(int *input_current)
 	return EC_SUCCESS;
 }
 
+#ifdef CONFIG_CHARGER_ISL9238
+int charger_enable_otg_power(int enabled)
+{
+	int rv, control1;
+
+	rv = raw_read16(ISL923X_REG_CONTROL1, &control1);
+	if (rv)
+		return rv;
+
+	if (enabled)
+		control1 |= ISL923X_C1_OTG;
+	else
+		control1 &= ~ISL923X_C1_OTG;
+
+	return raw_write16(ISL923X_REG_CONTROL1, control1);
+}
+
+/*
+ * TODO(b:67920792): OTG is not implemented for ISL9237 that has different
+ * register scale and range.
+ */
+int charger_set_otg_current_voltage(int output_current, int output_voltage)
+{
+	int rv;
+	uint16_t volt_reg = (output_voltage / ISL9238_OTG_VOLTAGE_STEP)
+			<< ISL9238_OTG_VOLTAGE_SHIFT;
+	uint16_t current_reg = (output_current / ISL923X_OTG_CURRENT_STEP)
+			<< ISL923X_OTG_CURRENT_SHIFT;
+	int current_prev;
+
+	if (output_current < 0 || output_current > ISL923X_OTG_CURRENT_MAX ||
+			output_voltage > ISL9238_OTG_VOLTAGE_MAX)
+		return EC_ERROR_INVAL;
+
+	/* Read currently provided current. */
+	rv = raw_read16(ISL923X_REG_OTG_CURRENT, &current_prev);
+	if (rv)
+		return rv;
+
+	/* Decrease current first, if needed. */
+	if (current_prev > current_reg) {
+		rv = raw_write16(ISL923X_REG_OTG_CURRENT, current_reg);
+		if (rv)
+			return rv;
+	}
+
+	/* Set voltage. */
+	rv = raw_write16(ISL923X_REG_OTG_VOLTAGE, volt_reg);
+	if (rv)
+		return rv;
+
+	/* Increase current. */
+	if (current_prev <= current_reg) {
+		rv = raw_write16(ISL923X_REG_OTG_CURRENT, current_reg);
+		if (rv)
+			return rv;
+	}
+
+	return EC_SUCCESS;
+}
+#endif /* CONFIG_CHARGER_ISL9238 */
+
 int charger_manufacturer_id(int *id)
 {
 	int rv;
@@ -253,7 +315,7 @@ int charger_post_init(void)
 		return rv;
 
 #ifdef CONFIG_CHARGE_RAMP_HW
-#ifdef CONFIG_CHARGER_ISL9237
+#ifdef CONFIG_ISL9237
 	rv = charger_get_option(&reg);
 	if (rv)
 		return rv;
