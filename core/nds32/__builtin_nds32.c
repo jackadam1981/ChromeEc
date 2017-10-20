e6f0dfc1f4949a9999280b79a8dff7ac0c4a2ef8
Dino Li <Dino.Li@ite.com.tw>
2017-10-20 16:11:15 +0800

    nds32: add built-in function
    A built-in function (__builtin_ffs) is missing after 'repo sync',
    so we add it.
    
    Signed-off-by: Dino Li <dino.li@ite.com.tw>
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I5210f85db05650545c9924940e8b24e350b82f71

---
+++ core/nds32/__builtin_nds32.c
@@ -0,0 +1,50 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+
+/*
+ * __builtin_ctz:
+ * Returns the number of trailing 0-bits in x,
+ * starting at the least significant bit position.
+ */
+int __ctzsi2(int x)
+{
+	int r = 0;
+
+	if (!x)
+		return 32;
+
+	if (!(x & 0x0000ffff)) {
+		x >>= 16;
+		r += 16;
+	}
+	if (!(x & 0x000000ff)) {
+		x >>= 8;
+		r += 8;
+	}
+	if (!(x & 0x0000000f)) {
+		x >>= 4;
+		r += 4;
+	}
+	if (!(x & 0x00000003)) {
+		x >>= 2;
+		r += 2;
+	}
+	if (!(x & 0x00000001))
+		r += 1;
+
+	return r;
+}
+
+/*
+ * __builtin_ffs:
+ * Returns one plus the index of the least significant 1-bit of x,
+ * or if x is zero, returns zero.
+ */
+int __ffssi2(int x)
+{
+	return (x == 0) ? 0 : (__builtin_ctz(x) + 1);
+}
