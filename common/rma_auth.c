dfeb7d220787f39e5ea70afbac1799b4a9345214
Vadim Bendebury <vbendeb@chromium.org>
2017-10-19 18:53:41 -0700

    rma: process proper auth code
    When RMA process has been initialized and the user entered the
    expected authentication code value, the Cr50 needs to wipe out TPM
    memory, open and unlock CCD and reboot the device.
    
    This patch adds a function to accomplish that. User response is
    validated on the TPM task context, and TPM reset request also requires
    TPM task processing. To decouple response processing from the
    following reset, the response processing is handled by a hook task
    callback, delayed for 1s to make sure that user receives the response.
    
    After TPM has successfully reset the CCD is reinitialized with RMA
    'opened' and the device is rebooted.
    
    Just in case something goes wrong with the unlock and the hook is not
    invoked, add a 10s deferred function to take the EC out of reset so
    that the device still can reboot.
    
    BRANCH=none
    BUG=b:67007905
    TEST=on a Bob device:
    
       - on the Cr50 console enter 'ccd lock', verify that ccd is locked
         (by examining output of the 'ccd' command)
    
       - at the bash prompt enter gsctool -r -s -t, copy the
         authentication code from the Cr50 console and pass it to gsctool.
    
       - observe the device reset TPM wiping out its memory, enable CCD
         and reboot.
    
    Change-Id: I6fafb5e642cb2b6f2040507a7f1989607fd31316
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- common/rma_auth.c
+++ common/rma_auth.c
@@ -8,10 +8,12 @@
 #include "common.h"
 #include "base32.h"
 #include "byteorder.h"
+#include "ccd_config.h"
 #include "chip/g/board_id.h"
 #include "console.h"
 #include "curve25519.h"
 #include "extension.h"
+#include "hooks.h"
 #include "rma_auth.h"
 #include "shared_mem.h"
 #include "system.h"
@@ -225,6 +227,61 @@ static enum vendor_cmd_rc get_challenge(uint8_t *buf, size_t *buf_size)
 	return VENDOR_RC_SUCCESS;
 }
 
+static uint8_t ccd_hook_active;
+
+static void ccd_config_changed(void)
+{
+	if (!ccd_hook_active)
+		return;
+
+	CPRINTF("%s: CCD change saved, rebooting\n", __func__);
+	cflush();
+	system_reset(SYSTEM_RESET_HARD);
+}
+DECLARE_HOOK(HOOK_CCD_CHANGE, ccd_config_changed, HOOK_PRIO_LAST);
+
+static void rma_reset_failed(void)
+{
+	ccd_hook_active = 0;
+	CPRINTF("%s: CCD RMA reset failed\n");
+	deassert_ec_rst();
+}
+DECLARE_DEFERRED(rma_reset_failed);
+
+static void enter_rma_mode(void)
+{
+	int rv;
+
+	CPRINTF("%s: resetiing TPM\n", __func__);
+
+	/*
+	 * Let's make sure the rest of the system is out of the way while TPM
+	 * is being wiped out.
+	 */
+	assert_ec_rst();
+
+	if (tpm_reset_request(1, 1) != EC_SUCCESS) {
+		CPRINTF("%s: TPM reset attempt failed\n", __func__);
+		return;
+	}
+
+	tpm_reinstate_nvmem_commits();
+
+	CPRINTF("%s: TPM reset succeeded, RMA resetting CCD\n", __func__);
+
+	ccd_hook_active = 1;
+	rv = ccd_reset_config(CCD_RESET_RMA);
+	if (rv != EC_SUCCESS)
+		rma_reset_failed();
+
+	/*
+	 * Make sure we never end up with the EC held in reset, no matter what
+	 * prevents the proper RMA flow from succeeding.
+	 */
+	hook_call_deferred(&rma_reset_failed_data, 10 * SECOND);
+}
+DECLARE_DEFERRED(enter_rma_mode);
+
 /*
  * Compare response sent by the operator with the pre-compiled auth code.
  * Return error code or success depending on the comparison results.
@@ -249,6 +306,7 @@ static enum vendor_cmd_rc process_response(uint8_t *buf,
 	if (rv == EC_SUCCESS) {
 		CPRINTF("%s: success!\n", __func__);
 		*response_size = 0;
+		hook_call_deferred(&enter_rma_mode_data, 1 * SECOND);
 		return VENDOR_RC_SUCCESS;
 	}
 
@@ -279,7 +337,6 @@ static enum vendor_cmd_rc rma_challenge_response(enum vendor_cmd_cc code,
 }
 DECLARE_VENDOR_COMMAND(VENDOR_CC_RMA_CHALLENGE_RESPONSE,
 		       rma_challenge_response);
-
 
 #define RMA_CMD_BUF_SIZE (sizeof(struct tpm_cmd_header) + \
 			  RMA_CHALLENGE_BUF_SIZE)
