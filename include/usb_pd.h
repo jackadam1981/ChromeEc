2360f7e5b32f0dea44ba2837d1dcade2367f9346
Daisuke Nojiri <dnojiri@chromium.org>
2017-10-24 16:47:07 -0700

    pd: Allow sending SOP' Discovery Identity
    Add a console command (e.g. 'pd 0 vdm ident') to send a Discover
    Identity with SOP' to read a cable e-marker information when we're
    in sink mode.
    
    BUG=chromium:776202
    BRANCH=none
    TEST=TBD
    
    Change-Id: I1f5b583fa958f1bb0819adca174a805c337cad65
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -1283,9 +1283,10 @@ void pd_log_recv_vdm(int port, int cnt, uint32_t *payload);
  * @param cmd      VDO command number
  * @param data     Pointer to payload to send
  * @param count    number of data objects in payload
+ * @param type     transmit type
  */
 void pd_send_vdm(int port, uint32_t vid, int cmd, const uint32_t *data,
-		 int count);
+		 int count, enum tcpm_transmit_type type);
 
 /* Power Data Objects for the source and the sink */
 extern const uint32_t pd_src_pdo[];
