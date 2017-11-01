30dc3fe862ac7fb3416d2a2e05ae4c20306ef1b0
Vadim Bendebury <vbendeb@chromium.org>
2017-10-31 18:54:34 -0700

    g: sps: at initialization wait for the master to finish SPI cycle
    TPM reset processing takes certain time, and conceivably the AP could
    start SPI transactions before TPM reset is finished. If the SPS
    interface comes up while the CS line is active, the H1 controller
    considers this a start of the SPI cycle, even though it is not - the
    AP has already transferred the header and is waiting for the flow
    control.
    
    Let's not complete SPS interface initialization while the CS line is
    kept active.
    
    BRANCH=cr50
    BUG=b:68012381
    TEST=verified that the AP firmware test passes
    
    Change-Id: I53cd49c6139f3c29c4b6d234c7ee4d527c8282f6
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- chip/g/sps.c
+++ chip/g/sps.c
@@ -185,6 +185,16 @@ static void sps_configure(enum sps_mode mode, enum spi_clock_mode clk_mode,
 	/* xfer 0xff when tx fifo is empty */
 	GREG32(SPS, DUMMY_WORD) = GC_SPS_DUMMY_WORD_DEFAULT;
 
+	if (!GREAD_FIELD(SPS, VAL, CSB)) {
+		/*
+		 * Reset while the external controller is mid SPI
+		 * transaction.
+		 */
+		ccprintf("%s: reset while CS active\n", __func__);
+		while (!GREAD_FIELD(SPS, VAL, CSB))
+			;
+	}
+
 	/* [5,4,3]           [2,1,0]
 	 * RX{DIS, EN, RST} TX{DIS, EN, RST}
 	 */
