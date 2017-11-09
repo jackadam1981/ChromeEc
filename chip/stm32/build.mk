25a5e67cf5fbed8ffcaffadd943e72ecfc81310e
Vincent Palatin <vpalatin@chromium.org>
2017-11-09 13:43:58 +0100

    stm32: Add STM32H7 family
    Start adding support for the STM32H7 family of device and the first
    available one the STM32H743.
    
    On this new family, all the register base addresses are different, so
    start a new dedicated registers.h rather than overloading the common
    one.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=b:67081508
    TEST=manual, run on stm32h743i-eval board and get a stable serial
    console.
    
    Change-Id: I9ae10f0d843e5318451713c21ed22d455a23758c

--- chip/stm32/build.mk
+++ chip/stm32/build.mk
@@ -16,6 +16,11 @@ else ifeq ($(CHIP_FAMILY),$(filter $(CHIP_FAMILY),stm32f3 stm32l4 stm32f4))
 CORE:=cortex-m
 # Allow the full Cortex-M4 instruction set
 CFLAGS_CPU+=-march=armv7e-m -mcpu=cortex-m4
+else ifeq ($(CHIP_FAMILY),$(filter $(CHIP_FAMILY),stm32h7))
+# STM32FH7xx family has a Cortex-M7 ARM core
+CORE:=cortex-m
+# Allow the full Cortex-M4 instruction set (identical to M7)
+CFLAGS_CPU+=-march=armv7e-m -mcpu=cortex-m4
 else
 # other STM32 SoCs have a Cortex-M3 ARM core
 CORE:=cortex-m
