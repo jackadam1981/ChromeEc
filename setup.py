fc45389b9a0e71477a2368048abca5523c6b272e
Nick Sanders <nsanders@chromium.org>
2017-11-09 16:32:53 -0800

    servo: export servo python
    Add python library exports for various servo things
    * servo_updater - update servos, sweetberry over usb
    * ecusb - library to have minimal console command channel.
    * powerlog - connect to sweetberry and query power
    
    Fix servo_updater to allow servo_micro to connect to console.
    
    BUG=b:69016431
    BRANCH=None
    TEST=python, import [...]
    
    Change-Id: I80adaafa6e70a3b7e246f23810b64baf98859b3c
    Signed-off-by: Nick Sanders <nsanders@chromium.org>

--- setup.py
+++ setup.py
@@ -15,3 +15,36 @@ setup(
     py_modules=["ec3po.console", "ec3po.interpreter"],
     description="EC console interpreter.",
 )
+
+setup(
+    name="ecusb",
+    version="1.0",
+    author="Nick Sanders",
+    author_email="nsanders@chromium.org",
+    url="https://www.chromium.org/chromium-os/ec-development",
+    package_dir={"" : "extra/tigertool"},
+    packages=["ecusb"],
+    description="Tiny implementation of servod.",
+)
+
+setup(
+    name="servo_updater",
+    version="1.0",
+    author="Nick Sanders",
+    author_email="nsanders@chromium.org",
+    url="https://www.chromium.org/chromium-os/ec-development",
+    package_dir={"" : "extra/usb_updater"},
+    py_modules=["servo_updater", "fw_update"],
+    description="Servo usb updater.",
+)
+
+setup(
+    name="powerlog",
+    version="1.0",
+    author="Nick Sanders",
+    author_email="nsanders@chromium.org",
+    url="https://www.chromium.org/chromium-os/ec-development",
+    package_dir={"" : "extra/usb_power"},
+    py_modules=["powerlog", "stats_manager"],
+    description="Sweetberry power logger.",
+)
