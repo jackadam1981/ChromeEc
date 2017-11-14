3fadce92c52a744ed1dac60945bfff411c31efc2
Aseda Aboagye <aaboagye@google.com>
2017-11-14 12:54:06 -0800

    sn5s330: Clear interrupts and setup masks at init.
    When the SN5S330 is powered initially, there will be certain events set
    that will assert its interrupt line.  As part of the initialization for
    this part, we should clear all the pending interrupts and setup the
    masks for the events that we care about.
    
    This commit clears all interrupts at init time and masks all interrupts
    except for overcurrent condition for PP1.
    
    BUG=b:69139844
    BRANCH=None
    TEST=Flash zoombini; With nothing plugged in, verify that the interrupt
    line is deasserted at boot.
    TEST=Repeat the above test with a charger plugged in.
    
    Change-Id: I7acc030184b76d6c38a729cb64658f71e376c819
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- driver/ppc/sn5s330.h
+++ driver/ppc/sn5s330.h
@@ -101,7 +101,16 @@ enum sn5s330_pp_idx {
 #define SN5S330_OVP_EN_CC (1 << 4)
 
 /* INT_STATUS_REG4 */
+#define SN5S330_DIG_RES (1 << 0)
 #define SN5S330_DB_BOOT (1 << 1)
+#define SN5S330_VSAFE0V_STAT (1 << 2)
+#define SN5S330_VSAFE0V_MASK (1 << 3)
+
+/* INT_MASK_RISE_EDGE_1 */
+#define SN5S330_ILIM_PP1_RISE_MASK (1 << 4)
+
+/* INT_MASK_FALL_EDGE_1 */
+#define SN5S330_ILIM_PP1_FALL_MASK (1 << 4)
 
 /**
  * Turn on/off the PP1 or PP2 FET.
