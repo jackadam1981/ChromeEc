87af42c6dcd23bdd2139b424820b43624ad9ec87
Moritz Fischer <moritz.fischer@ettus.com>
2017-11-14 21:10:50 -0800

    stm32f4: Refactor configuration headers
    Move configuration from config-stm32f446.h into
    common config-stm32f4x.h.
    
    This comes in handy when dealing with
    differences such as the different number of I2C
    ports on different STM32F4 MCUs.
    
    BRANCH=master
    
    Change-Id: Ia02cba1e107e05d4dc75c64565ea81feb4897e10
    Signed-off-by: Moritz Fischer <moritz.fischer@ettus.com>

--- chip/stm32/i2c-stm32f4.c
+++ chip/stm32/i2c-stm32f4.c
@@ -53,9 +53,11 @@ static const __unused struct dma_option dma_tx_option[I2C_PORT_COUNT] = {
 	{STM32_DMAC_I2C3_TX, (void *)&STM32_I2C_DR(STM32_I2C3_PORT),
 	 STM32_DMA_CCR_MSIZE_8_BIT | STM32_DMA_CCR_PSIZE_8_BIT |
 	 STM32_DMA_CCR_CHANNEL(STM32_I2C3_TX_REQ_CH)},
+#if !defined(CHIP_VARIANT_STM32F411)
 	{STM32_DMAC_FMPI2C4_TX, (void *)&STM32_FMPI2C_TXDR(STM32_FMPI2C4_PORT),
 	 STM32_DMA_CCR_MSIZE_8_BIT | STM32_DMA_CCR_PSIZE_8_BIT |
 	 STM32_DMA_CCR_CHANNEL(STM32_FMPI2C4_TX_REQ_CH)},
+#endif
 };
 
 static const struct dma_option dma_rx_option[I2C_PORT_COUNT] = {
@@ -68,9 +70,11 @@ static const struct dma_option dma_rx_option[I2C_PORT_COUNT] = {
 	{STM32_DMAC_I2C3_RX, (void *)&STM32_I2C_DR(STM32_I2C3_PORT),
 	 STM32_DMA_CCR_MSIZE_8_BIT | STM32_DMA_CCR_PSIZE_8_BIT |
 	 STM32_DMA_CCR_CHANNEL(STM32_I2C3_RX_REQ_CH)},
+#if !defined(CHIP_VARIANT_STM32F411)
 	{STM32_DMAC_FMPI2C4_RX, (void *)&STM32_FMPI2C_RXDR(STM32_FMPI2C4_PORT),
 	 STM32_DMA_CCR_MSIZE_8_BIT | STM32_DMA_CCR_PSIZE_8_BIT |
 	 STM32_DMA_CCR_CHANNEL(STM32_FMPI2C4_RX_REQ_CH)},
+#endif
 };
 
 /* Callabck for ISR to wake task on DMA complete. */
@@ -98,17 +102,21 @@ static void _i2c_dma_wake_callback_2(void *cb_data)
 	_i2c_dma_wake_callback(cb_data, 2);
 }
 
+#if !defined(CHIP_VARIANT_STM32F411)
 static void _i2c_dma_wake_callback_3(void *cb_data)
 {
 	_i2c_dma_wake_callback(cb_data, 3);
 }
+#endif
 
 /* void (*callback)(void *) */
 static void (*i2c_callbacks[I2C_PORT_COUNT])(void *) = {
 	_i2c_dma_wake_callback_0,
 	_i2c_dma_wake_callback_1,
 	_i2c_dma_wake_callback_2,
+#if !defined(CHIP_VARIANT_STM32F411)
 	_i2c_dma_wake_callback_3,
+#endif
 };
 
 /* Enable the I2C interrupt callback for this port. */
