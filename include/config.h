caec2d7588e43febc58e07eb9d641ff7f3e92f33
Furquan Shaikh <furquan@chromium.org>
2017-11-14 18:51:42 -0800

    host_events: Bump up host events and masks to 64-bit
    With the upcoming change to add a new command to get/set/clear host
    events and masks, it seems to be the right time to bump up the host
    events and masks to 64-bit. We are already out of available host
    events. This change opens up at least 32 bits for new host events.
    
    Old EC commands to operate on host events/masks will still deal with
    lower 32-bits of the events/mask. On the other hand, the new command
    being added will take care of the entire 64-bit events/masks. This
    ensures that old BIOS and kernel versions can still work with the
    newer EC versions.
    
    BUG=b:69329196
    BRANCH=None
    TEST=make -j buildall. Verified:
    1. hostevent set 0x4000 ==> Sets correct bit in host events
    2. hostevent clear 0x4000 ==> Clears correct bit in host events
    3. Kernel is able to query and read correct host event bits from
    EC. Verified using evtest.
    4. Coreboot is able to read correct wake reason from EC. Verified
    using mosys eventlog list.
    
    Change-Id: Idcb24ea364ac6c491efc2f8dd9e29a9df6149e07
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1435,7 +1435,10 @@
 #undef CONFIG_HOST_COMMAND_STATUS
 
 /* clear bit(s) to mask reporting of an EC_HOST_EVENT_XXX event(s) */
-#define CONFIG_HOST_EVENT_REPORT_MASK 0xffffffff
+#define CONFIG_HOST_EVENT_REPORT_MASK 0xffffffffffffffff
+
+/* Config option to support 64-bit hostevents and wake-masks. */
+#define CONFIG_HOST_EVENT64
 
 /*
  * The host commands are sorted in the .rodata.hcmds section so use the binary
