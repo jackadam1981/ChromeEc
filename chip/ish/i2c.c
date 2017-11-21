9c117df82f4377484b73f711d941708334fb2234
li feng <li1.feng@intel.com>
2017-11-20 17:19:41 -0800

    ish i2c: add I2C Fast Mode Plus 1Mbps speed option
    BUG=None
    BRANCH=None
    TEST=On Soraka modified board for ISH, set I2C stardard mode, fast mode
    and fast mode plus, measured I2C clock freq using scope respectively and
    confirmed the freq matched I2C mode.
    
    Change-Id: I426b50dc935c3760903360a50f6069e99bd0abff
    Signed-off-by: li feng <li1.feng@intel.com>

--- chip/ish/i2c.c
+++ chip/ish/i2c.c
@@ -24,6 +24,7 @@
 #define I2C_FLAG_REPEATED_START_DISABLED	0
 #define EVENT_FLAG_I2C_TIMEOUT			TASK_EVENT_CUSTOM(1 << 1)
 
+/*25MHz,50MHz,100MHz,120MHz,40MHz,20MHz,37MHz*/
 static uint16_t default_hcnt_scl_100[] = {
 	4000, 4420, 4920, 4400, 4000, 4000, 4300
 };
@@ -40,11 +41,19 @@ static uint16_t default_lcnt_scl_400[] = {
 	1320, 1380, 1300, 1300, 1300, 1200, 1250
 };
 
+static uint16_t default_hcnt_scl_1000[] = {
+	600, 820, 260, 260, 600, 600, 450
+};
+
+static uint16_t default_lcnt_scl_1000[] = {
+	1320, 1380, 500, 500, 1300, 1200, 1250
+};
+
 static uint16_t default_hcnt_scl_hs[] = { 160, 300, 160, 166, 175, 150, 162 };
 static uint16_t default_lcnt_scl_hs[] = { 320, 340, 320, 325, 325, 300, 297 };
 
 static uint8_t speed_val_arr[] = {
-	STD_SPEED_VAL, FAST_SPEED_VAL, HIGH_SPEED_VAL };
+	STD_SPEED_VAL, FAST_SPEED_VAL, FAST_PLUS_SPEED_VAL, HIGH_SPEED_VAL};
 
 static uint8_t bus_freq[ISH_I2C_PORT_COUNT] = {
 	I2C_FREQ_120, I2C_FREQ_120, I2C_FREQ_120
@@ -73,18 +82,21 @@ static struct i2c_bus_info board_config[ISH_I2C_PORT_COUNT] = {
 		.bus_id = 0,
 		.std_speed.sda_hold = DEFAULT_SDA_HOLD,
 		.fast_speed.sda_hold = DEFAULT_SDA_HOLD,
+		.fast_plus_speed.sda_hold = DEFAULT_SDA_HOLD,
 		.high_speed.sda_hold = DEFAULT_SDA_HOLD,
 	},
 	{
 		.bus_id = 1,
 		.std_speed.sda_hold = DEFAULT_SDA_HOLD,
 		.fast_speed.sda_hold = DEFAULT_SDA_HOLD,
+		.fast_plus_speed.sda_hold = DEFAULT_SDA_HOLD,
 		.high_speed.sda_hold = DEFAULT_SDA_HOLD,
 	},
 	{
 		.bus_id = 2,
 		.std_speed.sda_hold = DEFAULT_SDA_HOLD,
 		.fast_speed.sda_hold = DEFAULT_SDA_HOLD,
+		.fast_plus_speed.sda_hold = DEFAULT_SDA_HOLD,
 		.high_speed.sda_hold = DEFAULT_SDA_HOLD,
 	 },
 };
@@ -181,6 +193,19 @@ static void i2c_init_transaction(struct i2c_context *ctx,
 					     clk_in_val));
 		i2c_mmio_write(base, IC_SDA_HOLD,
 			       NS_2_COUNTERS(bus_info->fast_speed.sda_hold,
+					     clk_in_val));
+		break;
+
+
+	case I2C_SPEED_FAST_PLUS:
+		i2c_mmio_write(base, IC_FS_SCL_HCNT,
+			       NS_2_COUNTERS(bus_info->fast_plus_speed.hcnt,
+					     clk_in_val));
+		i2c_mmio_write(base, IC_FS_SCL_LCNT,
+			       NS_2_COUNTERS(bus_info->fast_plus_speed.lcnt,
+					     clk_in_val));
+		i2c_mmio_write(base, IC_SDA_HOLD,
+			       NS_2_COUNTERS(bus_info->fast_plus_speed.sda_hold,
 					     clk_in_val));
 		break;
 
@@ -387,8 +412,13 @@ static void i2c_initial_board_config(struct i2c_context *ctx)
 
 	bus_info->std_speed.hcnt = default_hcnt_scl_100[freq];
 	bus_info->std_speed.lcnt = default_lcnt_scl_100[freq];
+
 	bus_info->fast_speed.hcnt = default_hcnt_scl_400[freq];
 	bus_info->fast_speed.lcnt = default_lcnt_scl_400[freq];
+
+	bus_info->fast_plus_speed.hcnt = default_hcnt_scl_1000[freq];
+	bus_info->fast_plus_speed.lcnt = default_lcnt_scl_1000[freq];
+
 	bus_info->high_speed.hcnt = default_hcnt_scl_hs[freq];
 	bus_info->high_speed.lcnt = default_lcnt_scl_hs[freq];
 }
