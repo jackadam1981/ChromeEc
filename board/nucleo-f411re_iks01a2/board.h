0dc9af21406ee88ab2b8b7eb5961d954570333a7
Gwendal Grignou <gwendal@chromium.org>
2017-11-20 16:16:34 -0800

    board: Move Nucleo f411re to f411re_iks01a2
    The board f411re was defined for BMI160 bringup and was not working
    anymore.
    Move definition to f411re_iks01a2:
    A Nucleo F411RE + sensor board IKS01A2.
    
    BUG=none
    BRANCH=none
    TEST=Check Gyro/Accel LSM6DSL and Baro LPS22HB from board are working
    properly.
    
    Change-Id: If7b9dabfb2af1177a4bcc6a2525c77342f53375b
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

---
+++ board/nucleo-f411re_iks01a2/board.h
@@ -0,0 +1,150 @@
+/* Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Nucleo-F411RE development board configuration */
+
+#ifndef __BOARD_H
+#define __BOARD_H
+
+/* 84 MHz CPU/AHB/APB2 clock frequency (APB1 = 42 Mhz) */
+#define CPU_CLOCK 84000000
+#define CONFIG_FLASH_WRITE_SIZE STM32_FLASH_WRITE_SIZE_3300
+
+
+/* the UART console is on USART2 (PA2/PA3) */
+#undef CONFIG_UART_CONSOLE
+#define CONFIG_UART_CONSOLE 2
+
+/*
+ * To Enable Sensors, select:
+ *  CONFIG_MAG_LIS2MDL - Magnetometer
+ *  CONFIG_MAG_LSM6DSM_LIS2MDL - Magnetometer cascade with Accelerometer
+ *  CONFIG_ACCELGYRO_LSM6DSM - Accelerometer + Gyroscope
+ *  CONFIG_ACCEL_LIS2DH - Accelerometer
+ *  CONFIG_ACCEL_LIS2DS - Accelerometer
+ *  CONFIG_ACCEL_LIS2DE - Accelerometer
+ *  CONFIG_ACCEL_LIS2DW12 - Accelerometer
+ *  CONFIG_BARO_LPS22HB - Barometer
+ */
+#define CONFIG_ACCELGYRO_LSM6DSM
+#undef CONFIG_MAG_LIS2MDL
+#undef CONFIG_ACCEL_LIS2DH
+#undef CONFIG_MAG_LSM6DSM_LIS2MDL
+#define CONFIG_BARO_LPS22HB
+#undef CONFIG_ACCEL_LIS2DS
+#undef CONFIG_ACCEL_LIS2DE
+#undef CONFIG_ACCEL_LIS2DW12
+
+/* Interrupt management. */
+#define CONFIG_ACCEL_INTERRUPTS
+/* Gesture Configuration. */
+#define CONFIG_GESTURE_DETECTION
+#define CONFIG_GESTURE_HOST_DETECTION
+#define CONFIG_GESTURE_SAMPLING_INTERVAL_MS	5
+
+/* First sensor is motion_sensor is used for significant motion. */
+#define CONFIG_GESTURE_SIGMO			0
+#define CONFIG_GESTURE_SIGMO_PROOF_MS		500
+#define CONFIG_GESTURE_SIGMO_SKIP_MS		3000
+#define CONFIG_GESTURE_SIGMO_THRES_MG		500
+
+#define CONFIG_GESTURE_SENSOR_BATTERY_TAP	0
+#define CONFIG_GESTURE_TAP_THRES_MG		100
+#define CONFIG_GESTURE_TAP_MAX_INTERSTICE_T	500
+#define CONFIG_GESTURE_DETECTION_MASK \
+	((1 << CONFIG_GESTURE_SIGMO) | \
+	 (1 << CONFIG_GESTURE_SENSOR_BATTERY_TAP))
+#define CONFIG_GESTURE_TAP_EVENT		TASK_EVENT_CUSTOM(1024)
+#define CONFIG_GESTURE_SIGMO_EVENT		TASK_EVENT_CUSTOM(2048)
+
+/* Custom sensor option. */
+#define CONFIG_ACCEL_LIS2DH_INT_EVENT		TASK_EVENT_CUSTOM(4)
+#define CONFIG_ACCEL_LSM6DSM_INT_EVENT		TASK_EVENT_CUSTOM(5)
+#define CONFIG_ACCEL_LIS2DS_INT_EVENT		TASK_EVENT_CUSTOM(6)
+#define CONFIG_ACCEL_LIS2DE_INT_EVENT		TASK_EVENT_CUSTOM(7)
+#define CONFIG_ACCEL_LIS2DW12_INT_EVENT		TASK_EVENT_CUSTOM(8)
+
+/* Optional features */
+#undef CONFIG_LID_SWITCH
+#undef CONFIG_HIBERNATE
+#define CONFIG_STM_HWTIMER32
+#define CONFIG_TASK_PROFILING
+
+#define CONFIG_I2C
+#define CONFIG_I2C_DEBUG
+#define CONFIG_I2C_MASTER
+#undef CONFIG_UART_RX_DMA
+#define CONFIG_UART_TX_DMA_CH STM32_DMAS_USART2_TX
+#define CONFIG_UART_RX_DMA_CH STM32_DMAS_USART2_RX
+#define CONFIG_UART_TX_REQ_CH STM32_REQ_USART2_TX
+#define CONFIG_UART_RX_REQ_CH STM32_REQ_USART2_RX
+
+#define CONFIG_CMD_ACCELS
+#define CONFIG_CMD_ACCEL_INFO
+#define CONFIG_LTO
+#define CONFIG_CMD_FLASH
+
+/* FIFO Support. */
+#define CONFIG_ACCEL_FIFO			32
+#define CONFIG_ACCEL_FIFO_THRES			(CONFIG_ACCEL_FIFO - 1)
+
+/* I2C master port */
+#define I2C_PORT_MASTER				STM32_I2C1_PORT
+/* Accelerometer, Gyroscope, Mag and Barometer on the device port. */
+#define I2C_PORT_GYRO				I2C_PORT_MASTER
+#define I2C_PORT_ACCEL				I2C_PORT_MASTER
+#define I2C_PORT_BARO				I2C_PORT_MASTER
+#define I2C_PORT_MAG				I2C_PORT_MASTER
+
+
+/*
+ * Allow dangerous commands all the time, since we don't have a write protect
+ * switch.
+ */
+#define CONFIG_SYSTEM_UNLOCKED
+
+#ifndef __ASSEMBLER__
+
+#include "gpio_signal.h"
+
+/* Timer selection */
+#define TIM_CLOCK32 2
+#define TIM_WATCHDOG 11
+
+
+/* Motion sensors. */
+enum sensor_id {
+/*
+ * Suppose to have:
+ *  1) Lid Accelerometer (select LIS2DH, LIS2DS, LIS2DW12 or LIS2DE
+ *  2) Base Accelerometer (LSM6DSM)
+ *  3) Magnetometer (LIS2MDL or LSM6DSM_LIS2MDL)
+ *  4) Barometer (LPS22HB)
+ */
+#if defined(CONFIG_ACCEL_LIS2DH) || defined(CONFIG_ACCEL_LIS2DS) || \
+	defined(CONFIG_ACCEL_LIS2DW12) || defined(CONFIG_ACCEL_LIS2DE)
+	LID_ACCEL,
+#endif /* CONFIG_ACCEL_LIS2DH, CONFIG_ACCEL_LIS2DS, CONFIG_ACCEL_LIS2DW12 */
+
+#ifdef CONFIG_ACCELGYRO_LSM6DSM
+	BASE_ACCEL,
+	BASE_GYRO,
+#endif /* CONFIG_ACCELGYRO_LSM6DSM */
+
+#ifdef CONFIG_MAG_LIS2MDL
+	BASE_MAG,
+#endif /* CONFIG_MAG_LIS2MDL */
+
+#ifdef CONFIG_BARO_LPS22HB
+	BASE_BARO,
+#endif /* CONFIG_BARO_LPS22HB */
+};
+
+/* Sensors without hardware FIFO are in forced mode */
+#define CONFIG_ACCEL_FORCE_MODE_MASK ((1 << BASE_BARO))
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __BOARD_H */
