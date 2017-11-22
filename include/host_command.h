ea859e82c951016ab94b0cea39e215abd083311d
Jenny TC <jenny.tc@intel.com>
2017-11-22 09:58:19 +0530

    host_events: Introduce unified host event command
    Unified Host Event Programming Interface (UHEPI) enables a unified host
    command EC_CMD_PROGRAM_HOST_EVENT to set/get/clear different host events.
    Old host event commands (0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E,
    0x8F) is supported for backward compatibility. But newer version of
    BIOS/OS is expected to use UHEPI command (EC_CMD_PROGRAM_HOST_EVENT)
    
    The UHEPI also enables the active and lazy wake masks. Active wake mask
    is the mask that is programmed in the LPC driver (i.e. the mask that is
    actively used by LPC driver for waking the host during suspended state).
    It is same as the current wake mask that is set by the smihandler on host
    just before entering sleep state S3/S5. On the other hand, lazy wake masks
    are per-sleep masks (S0ix, S3, S5) so that they can be used by EC to set
    the active wake mask depending upon the type of sleep that the host has
    entered. This allows the host BIOS to perform one-time programming of
    the wake masks for each supported sleep type and then EC can take care
    of appropriately setting the active mask when host enters a particular
    sleep state.
    
    BRANCH=none
    BUG=b:63969337
    TEST=make buildall -j. And verfieid following scenario
    1). Verified wake masks with ec hostevent command on S0,S3,S5 and S0ix
    2). suspend_stress_test with S3 and S0ix
    3). Verified "mosys eventlog list" in S3 and s0ix resume to confirm
    	wake sources (Lid, power buttton and Mode change)
    4). Verified "mosys eventlog list" in S5 resume to confirm wake sources
    	(Power Button)
    5). Verified above scenarios with combination of Old BIOS + New EC and
        New BIOS + Old EC(making get_feature_flags1() return 0)
    
    Change-Id: Idb82ee87fffb475cd3fa9771bf7a5efda67af616
    Signed-off-by: Jenny TC <jenny.tc@intel.com>

--- include/host_command.h
+++ include/host_command.h
@@ -10,6 +10,7 @@
 
 #include "common.h"
 #include "ec_commands.h"
+enum power_state;
 
 /* Args for host command handler */
 struct host_cmd_handler_args {
@@ -182,6 +183,31 @@ host_event_t host_get_events(void);
  * @return true if <event> is set or false otherwise
  */
 int host_is_event_set(enum host_event_code event);
+
+#ifdef CONFIG_LPC
+
+/* If host doesn't program s0ix lazy wake mask, use default s0ix mask */
+#define DEFAULT_WAKE_MASK_S0IX  (EC_HOST_EVENT_MASK(EC_HOST_EVENT_LID_OPEN) | \
+				EC_HOST_EVENT_MASK(EC_HOST_EVENT_MODE_CHANGE))
+/*
+ * Get lazy wake masks for the sleep state provided
+ *
+ * @param state Sleep state
+ * @param mask  Lazy wake mask.
+ *
+ * @return EC_SUCCESS for success and EC_ERROR_INVAL for error
+ */
+
+int get_lazy_wake_mask(enum power_state state, uint64_t *mask);
+
+/*
+ * Check if active wake mask set by host
+ *
+ *
+ * @return 1 if active wake mask set by host else return 0
+ */
+uint8_t lpc_is_active_wm_set_by_host(void);
+#endif
 #endif
 
 /**
