9cdda9391ccbbb6f9092a100b6ced2b3a044c9f3
Vincent Palatin <vpalatin@chromium.org>
2017-11-24 16:45:34 +0100

    ectool: use poll interface
    Test polling for MKBP events through the kernel cros_ec/pd/fp/.. driver
    node.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=none
    BUG=b:69460856
    TEST=run on Eve EVT:
    ectool --name=cros_fp fpmode fingerdown && \
    ectool --name=cros_fp waitevent 5 10000
    
    Change-Id: Ibdec137a3b646cf017a29afcf24ff5bbfb731198

--- util/comm-dev.c
+++ util/comm-dev.c
@@ -219,6 +219,26 @@ static int ec_dev_is_v2(void)
 	return 0;
 }
 
+#include <poll.h>
+
+static int ec_pollevent_dev(unsigned long mask, void *buffer, size_t buf_size,
+			    int timeout)
+{
+	int rv;
+	struct pollfd pf = { .fd = fd, .events = POLLIN };
+
+	ioctl(fd, CROS_EC_DEV_IOCEVENTMASK_V2, mask);
+
+	rv = poll(&pf, 1, timeout);
+	if (rv != 1)
+		return rv;
+
+	if (pf.revents != POLLIN)
+		return -pf.revents;
+
+	return read(fd, buffer, buf_size);
+}
+
 int comm_init_dev(const char *device_name)
 {
 	int (*ec_cmd_readmem)(int offset, int bytes, void *dest);
@@ -257,6 +277,7 @@ int comm_init_dev(const char *device_name)
 	if (ec_cmd_readmem(EC_MEMMAP_ID, 2, version) == 2 &&
 	    version[0] == 'E' && version[1] == 'C')
 		ec_readmem = ec_cmd_readmem;
+	ec_pollevent = ec_pollevent_dev;
 
 	/*
 	 * Set temporary size, will be updated later.
