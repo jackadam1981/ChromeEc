5bb1f959a493cc335729b9402e40dc6df5499d44
Furquan Shaikh <furquan@chromium.org>
2017-11-27 14:46:44 -0800

    sb_fw_update: Get rid of CONFIG_SB_FIRMWARE_UPDATE
    CONFIG_SB_FIRMWARE_UPDATE is dead on ToT. So, get rid it completely.
    
    BUG=b:69695376
    BRANCH=None
    TEST=make -j buildall
    
    Change-Id: Ic1eedff21d82f729a73ec9a7c1d554b6b571e827
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>

--- util/build.mk
+++ util/build.mk
@@ -6,7 +6,7 @@
 # Host tools build
 #
 
-host-util-bin=ectool lbplay stm32mon ec_sb_firmware_update lbcc \
+host-util-bin=ectool lbplay stm32mon lbcc \
 	ec_parse_panicinfo
 build-util-bin=ec_uartd iteflash
 build-util-art+=util/export_taskinfo.so
@@ -18,8 +18,6 @@ comm-objs=$(util-lock-objs:%=lock/%) comm-host.o comm-dev.o
 comm-objs+=comm-lpc.o comm-i2c.o misc_util.o
 
 ectool-objs=ectool.o ectool_keyscan.o ec_flash.o ec_panicinfo.o $(comm-objs)
-ec_sb_firmware_update-objs=ec_sb_firmware_update.o $(comm-objs) misc_util.o
-ec_sb_firmware_update-objs+=powerd_lock.o
 lbplay-objs=lbplay.o $(comm-objs)
 
 ec_parse_panicinfo-objs=ec_parse_panicinfo.o ec_panicinfo.o
