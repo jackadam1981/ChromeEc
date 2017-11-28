b5886b72616ee151916c849ed59c7f78caf660b8
YB.Ha <ybha@samsung.com>
2017-11-28 10:59:28 +0900

    Modify ALS1730 for caroline
    Change-Id: Iab3dc8bb44d4086ca83db3cf7c41b7733b7d2db0

--- board/caroline/board.h
+++ board/caroline/board.h
@@ -144,6 +144,7 @@
 #define I2C_PORT_PD_MCU MEC1322_I2C1
 #define I2C_PORT_TCPC MEC1322_I2C1
 #define I2C_PORT_ACCEL MEC1322_I2C2
+#define I2C_PORT_ALS MEC1322_I2C2
 #define I2C_PORT_BATTERY MEC1322_I2C3
 #define I2C_PORT_CHARGER MEC1322_I2C3
 
@@ -158,6 +159,7 @@
 #define CONFIG_ACCELGYRO_BMI160_INT2_OUTPUT
 #define CONFIG_ACCEL_BMA255
 #define CONFIG_ACCEL_INTERRUPTS
+#define CONFIG_ALS_BH1730
 #define CONFIG_LID_ANGLE
 #define CONFIG_LID_ANGLE_SENSOR_BASE BASE_ACCEL
 #define CONFIG_LID_ANGLE_SENSOR_LID LID_ACCEL
@@ -228,7 +230,16 @@ enum sensor_id {
 	LID_ACCEL = 0,
 	BASE_ACCEL,
 	BASE_GYRO,
+        LID_ALS,
 };
+
+/*
+ * For backward compatibility, to report ALS via ACPI,
+ * Define the number of ALS sensors: motion_sensor copy the data to the ALS
+ * memmap region.
+ */
+#define CONFIG_ALS
+#define ALS_COUNT 1
 
 enum pwm_channel {
         PWM_CH_KBLIGHT,
@@ -277,7 +288,7 @@ int board_get_version(void);
 void board_rtc_reset(void);
 
 /* Sensors without hardware FIFO are in forced mode */
-#define CONFIG_ACCEL_FORCE_MODE_MASK (1 << LID_ACCEL)
+#define CONFIG_ACCEL_FORCE_MODE_MASK ((1 << LID_ACCEL) || (1 << LID_ALS))
 
 #endif /* !__ASSEMBLER__ */
 
