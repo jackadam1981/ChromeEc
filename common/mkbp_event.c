c373a21d35fea5da6dfd632b0155db791dce928f
Alexandru M Stan <amstan@chromium.org>
2017-11-30 20:43:15 -0800

    motion_sense: Lower jitter of EC->AP timestamp
    When the EC sends an interrupt to the AP notifying it of new
    accelerometer data we need to make sure the spot we record the timestamp
    of the event is virtually identical to the spot the AP records the same
    point in time.
    
    Therefore a better spot for that is right next to the gpio toggling of
    the interrupt line.
    
    BUG=b/67743747
    TEST=In the kernel, fifo_info->info.timestamp still has sane values.
    TEST=CTS should still pass
    
    Change-Id: Ic77101a045123e779f576c46b401c765304976fd
    Signed-off-by: Alexandru M Stan <amstan@chromium.org>

--- common/mkbp_event.c
+++ common/mkbp_event.c
@@ -9,12 +9,14 @@
 #include "chipset.h"
 #include "gpio.h"
 #include "host_command.h"
+#include "hwtimer.h"
 #include "link_defs.h"
 #include "mkbp_event.h"
 #include "power.h"
 #include "util.h"
 
 static uint32_t events;
+uint32_t mkbp_last_event_time;
 
 static void set_event(uint8_t event_type)
 {
@@ -77,6 +79,8 @@ int mkbp_send_event(uint8_t event_type)
 #endif
 
 	set_host_interrupt(1);
+	mkbp_last_event_time = __hw_clock_source_read();
+
 	return 1;
 }
 
