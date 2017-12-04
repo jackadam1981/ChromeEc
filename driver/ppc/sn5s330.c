2b6a5d93d9a0bccb5de6bf86dcce2a8cc2ada307
Aseda Aboagye <aaboagye@google.com>
2017-12-04 15:14:33 -0800

    USB PD: PPC: Add overcurrent handling.
    The Type-C Power Path Controllers provide overcurrent protection.  This
    commit adds support into the USB PD task for overcurrent events while we
    are in source role.  The USB PD 3.0 spec recommends that ports issue a
    hard reset when an overcurrent condition occurs on a port.  For PD
    capable port portners, we'll issue a hard reset.  Additionally, we'll
    allow a source port to overcurrent 3 times before latching off VBUS from
    the port entirely.  The FET will be re-enabled after ~1s after each
    overcurrent event.
    
    BUG=b:69935262
    BRANCH=None
    TEST=Add some extra code for zoombini to exercise the overcurrent
    limits.  Verify that the port overcurrents and for PD port partners, a
    hard reset message is sent.  Verify that a single port partner is
    allowed to overcurrent 3 times and no more until a physical
    disconnection.
    TEST=Repeat above tests for meowth.
    
    Change-Id: Ib070f261e98264cd88725ebce7d10e0798267e3b
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- driver/ppc/sn5s330.c
+++ driver/ppc/sn5s330.c
@@ -17,7 +17,7 @@
 #include "i2c.h"
 #include "system.h"
 #include "timer.h"
-#include "usb_pd_tcpm.h"
+#include "usb_pd.h"
 #include "usbc_ppc.h"
 #include "util.h"
 
@@ -479,13 +479,13 @@ static void sn5s330_interrupt_deferred(int port)
 	read_reg(port, SN5S330_INT_TRIP_RISE_REG1, &rise);
 	read_reg(port, SN5S330_INT_TRIP_FALL_REG1, &fall);
 
-	/* Let the board know about the overcurrent event. */
-	if (rise & SN5S330_ILIM_PP1_MASK)
-		board_overcurrent_event(port);
-
 	/* Clear the interrupt sources. */
 	write_reg(port, SN5S330_INT_TRIP_RISE_REG1, rise);
 	write_reg(port, SN5S330_INT_TRIP_FALL_REG1, fall);
+
+	/* Notify the system about the overcurrent event. */
+	if (rise & SN5S330_ILIM_PP1_MASK)
+		pd_handle_overcurrent(port);
 }
 
 static void c0_interrupt_deferred(void) { sn5s330_interrupt_deferred(0); }
