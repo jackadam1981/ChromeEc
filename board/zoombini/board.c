d8d6d961b9cad0e5425238859b6b82b7be91fef9
Aseda Aboagye <aaboagye@google.com>
2017-12-04 15:59:13 -0800

    meowth: Re-run through Type-C interrupt handler if asserted.
    BUG=None
    BRANCH=None
    TEST=Verify that PD, charging, and overcurrent works okay.
    
    Change-Id: I9c5df9fdc227e25405417d88dba9200825ea8c90
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/zoombini/board.c
+++ board/zoombini/board.c
@@ -360,8 +360,17 @@ void board_overcurrent_event(int port)
 		return;
 	};
 
-	/* TODO(aaboagye): Write a PD log entry for the OC event. */
-	CPRINTS("C%d: overcurrent!", port);
+#ifdef BOARD_MEOWTH
+	if (!gpio_get_level(GPIO_USB_C0_PD_INT_L)) {
+		ccprints("C0 INT still asserted, re-rollin'\n");
+		usb_c_interrupt(GPIO_USB_C0_PD_INT_L);
+	}
+
+	if (!gpio_get_level(GPIO_USB_C1_PD_INT_L)) {
+		ccprints("C1 INT still asserted, re-rollin'\n");
+		usb_c_interrupt(GPIO_USB_C1_PD_INT_L);
+	}
+#endif /* defined(BOARD_MEOWTH) */
 }
 
 static void board_pmic_init(void)
