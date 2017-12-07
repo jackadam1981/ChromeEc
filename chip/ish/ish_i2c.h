7a332a6d5f26304d34b900d759b4178635b53019
li feng <li1.feng@intel.com>
2017-12-06 18:17:48 -0800

    ish i2c: use i2c_ports[] to set bus speed
    BUG=None
    BRANCH=None
    TEST=Tested on Soraka ISH modified board, measured I2C speed for std,
    fast, fast plus mode.
    
    Change-Id: I0e07c3c73f5f0302fba41ad8e7f83e10e8f0af99
    Signed-off-by: li feng <li1.feng@intel.com>

--- chip/ish/ish_i2c.h
+++ chip/ish/ish_i2c.h
@@ -184,10 +184,11 @@ struct i2c_context {
 	uint8_t max_rx_depth;
 	uint8_t max_tx_depth;
 	uint8_t bus;
+	uint8_t speed;
 	uint32_t interrupts;
 	uint32_t reason;
 	uint8_t error_flag;
-	uint8_t speed;
+	int int_pin;
 	task_id_t wait_task_id;
 };
 
