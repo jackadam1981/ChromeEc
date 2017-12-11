7fe25439af9450dd0522999e502a4c8575ce698b
Wei-Han Chen <stimim@google.com>
2017-12-11 14:41:46 +0800

    stm32: add usb_isochronous
    Templates for USB isochronous implementation.  Current implementation
    only supports TX transmit.  Example of usage can be found in CL:803414.
    
    Basically, declare an USB isochronous interface by
    
    USB_ISOCHRONOUS_CONFIG_FULL(<NAME>,
                                <INTERFACE_NUM>,
                                <USB_CLASS>,
                                <USB_SUBCLASS>,
                                <SUB_PROTOCOL>,
                                <USB_STR_FOR_INTERFACE_NAME>,
                                <USB_EP_NUM>,
                                <PACKET_SIZE>,
                                <TX_CALLBACK>)
    
    where <PACKET_SIZE> is size of each USB packet, <TX_CALLBACK> is called
    when USB hardware has completed a packet.  The buffer that USB is not
    currently using will be passed to <TX_CALLBACK>, allow applications to
    write next packet to it.
    
    BUG=b:70482333
    TEST=None
    Signed-off-by: Wei-Han Chen <stimim@google.com>
    
    Change-Id: Ic6d41da6ddd7945edf0bdfff55ede38a97661783

---
+++ chip/stm32/usb_isochronous.c
@@ -0,0 +1,126 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "stddef.h"
+#include "common.h"
+#include "config.h"
+#include "link_defs.h"
+#include "registers.h"
+#include "util.h"
+#include "usb_api.h"
+#include "usb_hw.h"
+#include "usb_isochronous.h"
+
+
+/* Console output macro */
+#define CPRINTF(format, args...) cprintf(CC_USB, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_USB, format, ## args)
+
+
+/*
+ * Currently, we only support TX direction for USB isochronous transfer.
+ *
+ * According to RM0091, isochronous transfer is always double buffered.
+ * Addresses of buffers are pointed by `btable_ep[<endpoint>].tx_addr` and
+ * `btable_ep[<endpoint>].rx_addr`.
+ *
+ * DTOG | USB Buffer | App Buffer
+ * -----+------------+-----------
+ *   0  | tx_addr    | rx_addr
+ *   1  | rx_addr    | tx_addr
+ *
+ * That is, when DTOG bit is 0 (see `GET_TX_DTOG()`), USB hardware will read
+ * from `tx_addr`, and our application can write new data to `rx_addr` at the
+ * same time.
+ *
+ * Number of bytes in each buffer shall be tracked by `tx_count` and `rx_count`
+ * respectively.
+ *
+ * `APP_ADDR()`, `APP_COUNT()`, `SET_APP_ADDR()`, `SET_APP_COUNT()` help you to
+ * to select the correct variable to use by given DTOG value, which is available
+ * by `GET_TX_DTOG()`.
+ */
+static inline int get_tx_dtog(struct usb_isochronous_config const *config)
+{
+	/* Convert to 1 or 0 */
+	return !!(STM32_USB_EP(config->endpoint) & EP_TX_DTOG);
+}
+
+static inline usb_uint *get_app_addr(
+		struct usb_isochronous_config const *config,
+		int dtog_value)
+{
+	return config->tx_ram[dtog_value];
+}
+
+static inline usb_uint get_app_count(
+		struct usb_isochronous_config const *config,
+		int dtog_value)
+{
+	if (dtog_value)
+		return btable_ep[config->endpoint].tx_count;
+	else
+		return btable_ep[config->endpoint].rx_count;
+}
+
+static inline void set_app_count(struct usb_isochronous_config const *config,
+				 int dtog_value,
+				 usb_uint count)
+{
+	if (dtog_value)
+		btable_ep[config->endpoint].tx_count = count;
+	else
+		btable_ep[config->endpoint].rx_count = count;
+}
+
+
+void usb_isochronous_init(struct usb_isochronous_config const *config)
+{
+	int ep = config->endpoint;
+
+	btable_ep[ep].tx_addr = usb_sram_addr(get_app_addr(config, 1));
+	btable_ep[ep].rx_addr = usb_sram_addr(get_app_addr(config, 0));
+	set_app_count(config, 0, 0);
+	set_app_count(config, 1, 0);
+
+	STM32_USB_EP(ep) = ((ep << 0) | /* Endpoint Addr */
+			    EP_TX_VALID | /* start transmit */
+			    (2 << 9) | /* ISO EP */
+			    EP_RX_DISAB);
+}
+
+
+void usb_isochronous_event(struct usb_isochronous_config const *config,
+			   enum usb_ep_event evt)
+{
+	if (evt == USB_EVENT_RESET)
+		usb_isochronous_init(config);
+}
+
+
+void usb_isochronous_tx(struct usb_isochronous_config const *config)
+{
+	/*
+	 * Clear CTR_TX, note that EP_TX_VALID will *NOT* be cleared by
+	 * hardware, so we don't need to toggle it.
+	 */
+	STM32_TOGGLE_EP(config->endpoint, 0, 0, 0);
+	/*
+	 * Clear buffer count for buffer we just transmitted, so we do not
+	 * transmit the data twice.
+	 */
+	set_app_count(config, get_tx_dtog(config), 0);
+
+	hook_call_deferred(config->deferred, 0);
+}
+
+
+void usb_isochronous_deferred(struct usb_isochronous_config const *config)
+{
+	const int dtog_value = get_tx_dtog(config);
+	usb_uint *app_addr = get_app_addr(config, dtog_value);
+	size_t count = config->tx_callback(app_addr, config->tx_size);
+	set_app_count(config, dtog_value, count);
+}
---
+++ chip/stm32/usb_isochronous.h
@@ -0,0 +1,114 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_USB_ISOCHRONOUS_H
+#define __CROS_EC_USB_ISOCHRONOUS_H
+
+#include "compile_time_macros.h"
+#include "hooks.h"
+#include "queue.h"
+#include "usb_descriptor.h"
+#include "usb_hw.h"
+
+#include <stdint.h>
+
+/* Currently, we only support TX direction for USB isochronous transfer. */
+
+struct usb_isochronous_config {
+	int endpoint;
+
+	/*
+	 * Deferred function to call to handle USB and Queue request.
+	 */
+	const struct deferred_data *deferred;
+
+	/*
+	 * On TX complete, this function will be called to ask for more data to
+	 * transmit.
+	 *
+	 * @param  usb_addr	USB buffer, an uint8_t point that can be
+	 *			passed to memcpy_to_usbram()
+	 * @param  tx_size	config->tx_size
+	 * @return size_t	Number of bytes written to USB buffer
+	 */
+	size_t (*tx_callback) (usb_uint *usb_addr, size_t tx_size);
+
+	/* USB packet RAM buffer size. */
+	size_t tx_size;
+	/* USB packet RAM buffers. */
+	usb_uint *tx_ram[2];
+};
+
+/* Define an USB isochronous interface */
+#define USB_ISOCHRONOUS_CONFIG_FULL(NAME,				\
+				    INTERFACE,				\
+				    INTERFACE_CLASS,			\
+				    INTERFACE_SUBCLASS,			\
+				    INTERFACE_PROTOCOL,			\
+				    INTERFACE_NAME,			\
+				    ENDPOINT,				\
+				    TX_SIZE,				\
+				    TX_CALLBACK)			\
+	BUILD_ASSERT(TX_SIZE > 0);					\
+	BUILD_ASSERT((TX_SIZE <   64 && (TX_SIZE & 0x01) == 0) ||	\
+		     (TX_SIZE < 1024 && (TX_SIZE & 0x1f) == 0));	\
+	/* Declare buffer */						\
+	static usb_uint CONCAT2(NAME, _ep_tx_buffer_0)[TX_SIZE / 2] __usb_ram; \
+	static usb_uint CONCAT2(NAME, _ep_tx_buffer_1)[TX_SIZE / 2] __usb_ram; \
+	static void CONCAT2(NAME, _deferred_)(void);			\
+	DECLARE_DEFERRED(CONCAT2(NAME, _deferred_));			\
+	struct usb_isochronous_config const NAME = {			\
+		.endpoint  = ENDPOINT,					\
+		.deferred  = &CONCAT2(NAME, _deferred__data),		\
+		.tx_callback = TX_CALLBACK,				\
+		.tx_size   = TX_SIZE,					\
+		.tx_ram    = {						\
+			CONCAT2(NAME, _ep_tx_buffer_0),			\
+			CONCAT2(NAME, _ep_tx_buffer_1),			\
+		},							\
+	};								\
+	const struct usb_interface_descriptor				\
+	USB_IFACE_DESC(INTERFACE) = {					\
+		.bLength            = USB_DT_INTERFACE_SIZE,		\
+		.bDescriptorType    = USB_DT_INTERFACE,			\
+		.bInterfaceNumber   = INTERFACE,			\
+		.bAlternateSetting  = 0,				\
+		.bNumEndpoints      = 1,				\
+		.bInterfaceClass    = INTERFACE_CLASS,			\
+		.bInterfaceSubClass = INTERFACE_SUBCLASS,		\
+		.bInterfaceProtocol = INTERFACE_PROTOCOL,		\
+		.iInterface         = INTERFACE_NAME,			\
+	};								\
+	const struct usb_endpoint_descriptor				\
+	USB_EP_DESC(INTERFACE, 0) = {					\
+		.bLength          = USB_DT_ENDPOINT_SIZE,		\
+		.bDescriptorType  = USB_DT_ENDPOINT,			\
+		.bEndpointAddress = 0x80 | ENDPOINT,			\
+		.bmAttributes     = 0x01 /* Isochronous IN */,		\
+		.wMaxPacketSize   = TX_SIZE,				\
+		.bInterval        = 1,					\
+	};								\
+	static void CONCAT2(NAME, _ep_tx)(void)				\
+	{								\
+		usb_isochronous_tx(&NAME);				\
+	}								\
+	static void CONCAT2(NAME, _ep_event)(enum usb_ep_event evt)	\
+	{								\
+		usb_isochronous_event(&NAME, evt);			\
+	}								\
+	USB_DECLARE_EP(ENDPOINT,					\
+		       CONCAT2(NAME, _ep_tx),				\
+		       CONCAT2(NAME, _ep_tx),				\
+		       CONCAT2(NAME, _ep_event));			\
+	static void CONCAT2(NAME, _deferred_)(void)			\
+	{								\
+		usb_isochronous_deferred(&NAME);			\
+	}
+
+void usb_isochronous_deferred(struct usb_isochronous_config const *config);
+void usb_isochronous_tx(struct usb_isochronous_config const *config);
+void usb_isochronous_event(struct usb_isochronous_config const *config,
+			   enum usb_ep_event event);
+#endif /* __CROS_EC_USB_ISOCHRONOUS_H */
