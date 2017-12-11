91f34c55725121937c195680ad9a456501fd36aa
Wei-Han Chen <stimim@google.com>
2017-12-11 14:41:46 +0800

    stm32: add usb_isochronous
    Templates for USB isochronous implementation.  Current implementation
    only supports TX transmit.  Example of usage can be found in CL:803414.
    
    Basically, declare an USB isochronous interface by
    
    USB_ISOCHRONOUS_CONFIG_FULL(<NAME>,
                                <INTERFACE_NUM>,
                                <USB_CLASS>,
                                <USB_SUBCLASS>,
                                <SUB_PROTOCOL>,
                                <USB_STR_FOR_INTERFACE_NAME>,
                                <USB_EP_NUM>,
                                <PACKET_SIZE>,
                                <TX_CALLBACK>,
                                <SET_INTERFACE>)
    
    where <PACKET_SIZE> is size of each USB packet, <TX_CALLBACK> is called
    when USB hardware has completed a packet.  The buffer that USB is not
    currently using will be passed to <TX_CALLBACK>, allow applications to
    write next packet to it.
    
    When a SET_INTERFACE packet is received, <SET_INTERFACE> will be called
    with bAlternateSetting and bInterfaceNumber.
    
    We will declare interface descriptor with bAlternateSetting = 0 and 1
    for you, if you need more alternate settings, you need to declare by
    yourself.
    
    BUG=b:70482333
    TEST=manually on reworked staff board
    Signed-off-by: Wei-Han Chen <stimim@google.com>
    
    Change-Id: Ic6d41da6ddd7945edf0bdfff55ede38a97661783

--- include/config.h
+++ include/config.h
@@ -2832,6 +2832,9 @@
 /* Compile chip support for the USB device controller */
 #undef CONFIG_USB
 
+/* Support USB isochronous handler */
+#undef CONFIG_USB_ISOCHRONOUS
+
 /* Support USB blob handler. */
 #undef CONFIG_USB_BLOB
 
