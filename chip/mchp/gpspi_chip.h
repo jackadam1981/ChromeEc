1e9d04c25f210f6534d9198259fedbe823558402
Scott Worley <scott.worley@microchip.corp-partner.google.com>
2017-12-21 14:20:09 -0500

    ec_chip_mchp: Add SPI files
    Add Microchip MEC17xx family SPI master
    controllers and flash files. SPI implements
    public interface wrapper for QMSPI and GPSPI.
    MEC17xx family uses QMSPI for loading
    EC firmware. GPSPI is for general use (sensor).
    
    BRANCH=none
    BUG=
    TEST=Review only.
    
    Change-Id: I23001e254dd437caa0917949f4dd2eb903f1adb1
    Signed-off-by: Scott Worley <scott.worley@microchip.corp-partner.google.com>

---
+++ chip/mchp/gpspi_chip.h
@@ -0,0 +1,47 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Register map for MCHP MEC processor
+ */
+/** @file gpspi_chip.h
+ *MCHP MEC General Purpose SPI Master
+ */
+/** @defgroup MEC gpspi
+ */
+
+#ifndef _GPSPI_CHIP_H
+#define _GPSPI_CHIP_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+/* struct spi_device_t */
+#include "spi.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Place any C interfaces here */
+
+int gpspi_transaction_flush(const struct spi_device_t *spi_device);
+
+int gpspi_transaction_wait(const struct spi_device_t *spi_device);
+
+int gpspi_transaction_async(const struct spi_device_t *spi_device,
+				const uint8_t *txdata, int txlen,
+				uint8_t *rxdata, int rxlen);
+
+int gpspi_enable(int port, int enable);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* #ifndef _GPSPI_CHIP_H */
+/**   @}
+ */
+
