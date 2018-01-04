16686f8fb7149b81ab44a0e3e517fb191a31b1d8
Vadim Bendebury <vbendeb@chromium.org>
2018-01-04 15:44:17 -0800

    gsctool: use full path when invoking trunks_send
    The gsctool utility sometimes is used in environments where path to
    trunks_send is not set. As a result 'gsctool -t' invocations fail.
    
    Lets use the full path to trunks_send and hope its location does not
    change in the future.
    
    BRANCH=none
    BUG=none
    TEST=verified that gsctool invocations in crosh started in a tab and
         in a linux shell ran under user 'chronos' do not fail.
    
    Change-Id: Ib8af365dc5707cfec19acda9aa0228d33eb4573f
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -286,7 +286,7 @@ static int from_hexascii(char c)
 static FILE *tpm_output;
 static int ts_write(const void *out, size_t len)
 {
-	const char *cmd_head = "trunks_send --raw ";
+	const char *cmd_head = "/usr/sbin/trunks_send --raw ";
 	size_t head_size = strlen(cmd_head);
 	char full_command[head_size + 2 * len + 1];
 	size_t i;
