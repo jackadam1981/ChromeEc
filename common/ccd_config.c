aad5c4dd86627a45fe94cf57b7f1d540f76a044c
Vadim Bendebury <vbendeb@chromium.org>
2018-01-05 16:47:04 -0800

    ccd: refactor
    what happened
    
    BRANCH=none
    BUG=b:
    TEST=manual
    
    Change-Id: Id55da51d6edc5652591ad30160a4102b3026a186
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- common/ccd_config.c
+++ common/ccd_config.c
@@ -803,25 +803,15 @@ static int do_ccd_password(char *password)
 	return ccd_set_password(password);
 }
 
-static int command_ccd_password(int argc, char **argv)
+static int process_vendor_command(enum ccd_vendor_subcommands subcmd,
+				  void *payload, size_t size)
 {
-	struct ccd_vendor_cmd_header *vch;
 	int rv;
-	size_t password_size;
+	struct ccd_vendor_cmd_header *vch;
 	size_t command_size;
 
-	if (argc < 2)
-		return EC_ERROR_PARAM_COUNT;
 
-	password_size = strlen(argv[1]);
-
-	if (password_size > CCD_MAX_PASSWORD_SIZE) {
-		ccprintf("Password can not be longer than %d characters\n",
-			 CCD_MAX_PASSWORD_SIZE);
-		return EC_ERROR_PARAM1;
-	}
-
-	command_size = sizeof(*vch) + password_size;
+	command_size = sizeof(*vch) + size;
 	rv = shared_mem_acquire(command_size, (char **)&vch);
 	if (rv != EC_SUCCESS)
 		return rv;
@@ -831,9 +821,9 @@ static int command_ccd_password(int argc, char **argv)
 	vch->tpm_header.size = htobe32(command_size);
 	vch->tpm_header.command_code = htobe32(TPM_CC_VENDOR_BIT_MASK);
 	vch->tpm_header.subcommand_code = htobe16(VENDOR_CC_CCD);
-	vch->ccd_subcommand = CCDV_PASSWORD;
+	vch->ccd_subcommand = subcmd;
 
-	memcpy(vch + 1, argv[1], password_size);
+	memcpy(vch + 1, payload, size);
 	tpm_alt_extension(&vch->tpm_header, command_size);
 
 	/*
@@ -849,6 +839,24 @@ static int command_ccd_password(int argc, char **argv)
 
 	shared_mem_release(vch);
 	return EC_SUCCESS;
+}
+
+static int command_ccd_password(int argc, char **argv)
+{
+	size_t password_size;
+
+	if (argc < 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	password_size = strlen(argv[1]);
+
+	if (password_size > CCD_MAX_PASSWORD_SIZE) {
+		ccprintf("Password can not be longer than %d characters\n",
+			 CCD_MAX_PASSWORD_SIZE);
+		return EC_ERROR_PARAM1;
+	}
+
+	return process_vendor_command(CCDV_PASSWORD, argv[1], password_size);
 }
 
 static int command_ccd_open(int argc, char **argv)
