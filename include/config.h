32f964c69c9b829cb8e86265d004d199a2e64acc
Nicolas Boichat <drinkcat@chromium.org>
2018-01-09 16:31:44 +0800

    driver/charger/isl923x: Make sure CONFIG_CHARGER_NARROW_VDC is set
    Without this, the battery will discharge if we disallow battery
    charging (e.g. calling charge_request with either voltage == 0 or
    current == 0, either by policy, or when the battery is full).
    
    Also update config.h to set the option whenever isl923x is used.
    
    BRANCH=none
    BUG=b:66575472
    BUG=b:35585464
    TEST=make buildall -j
    
    Change-Id: Id5515d5ea82a393a3693a3da44cbdc2778296a95
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- include/config.h
+++ include/config.h
@@ -575,7 +575,7 @@
 /* Minimum battery percentage for power on */
 #undef CONFIG_CHARGER_MIN_BAT_PCT_FOR_POWER_ON
 
-/* Narrow VDC power path */
+/* Set this option when using a Narrow VDC (NVDC) charger, such as ISL9237/8. */
 #undef CONFIG_CHARGER_NARROW_VDC
 
 /*
@@ -3206,6 +3206,15 @@
 	defined(CONFIG_CHARGER_RT9466) || \
 	defined(CONFIG_CHARGER_RT9467)
 #define CONFIG_USB_PD_VBUS_MEASURE_CHARGER
+#endif
+
+/*****************************************************************************/
+/*
+ * Define CONFIG_CHARGER_NARROW_VDC for chargers that use a Narrow VDC power
+ * architecture.
+ */
+#if defined(CONFIG_CHARGER_ISL9237) || defined(CONFIG_CHARGER_ISL9238)
+#define CONFIG_CHARGER_NARROW_VDC
 #endif
 
 /*****************************************************************************/
