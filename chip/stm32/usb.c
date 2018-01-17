2c396e3d5058ba57e940f7b1f48276e8ceb076dc
Nicolas Boichat <drinkcat@chromium.org>
2018-01-17 09:29:21 +0800

    stm32/usb: Add logic to detect stuck controller in usb_wake
    When we tried to go back to sleep immediately after receiving a
    spurious USB resume event, the controller would get stuck in an
    unrecoverable state. Hopefully we fixed the resume logic, but
    this code would catch other cases, and recover the base.
    
    BRANCH=none
    BUG=b:35775088
    BUG=b:67766202
    BUG=b:71688150
    TEST=With badly implemented resume logic (e.g. call
         usb_resume_deferred directly from usb_resume), with USB
         autosuspend enabled on soraka, short D+/D- lines.
         Next wake event from staff crashes it, and recovers it.
    
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Change-Id: I7463a37682723be195bd6a72ea5d76c21bb6cb9a

--- chip/stm32/usb.c
+++ chip/stm32/usb.c
@@ -460,13 +460,33 @@ void board_usb_wake(void)
 	/* Side-band USB wake, do nothing by default. */
 }
 
+/* Called 10ms after usb_wake started. */
+static void usb_wake_deferred(void)
+{
+	if (esof_count == 3) {
+		/*
+		 * If we reach here, it means that we are not counting ESOF/SOF
+		 * properly (either of these interrupts should occur every 1ms).
+		 *
+		 * This should never happen if we implemented the resume logic
+		 * correctly.
+		 *
+		 * There does not seem to be a way to recover the controller
+		 * when it is in this "stuck" state, without a reset, so we just
+		 * crash.
+		 */
+		panic("USB is stuck");
+	}
+}
+DECLARE_DEFERRED(usb_wake_deferred);
+
 void usb_wake(void)
 {
 	if (!remote_wakeup_enabled ||
 	    !(STM32_USB_CNTR & STM32_USB_CNTR_FSUSP)) {
 		/*
 		 * USB wake not enabled, or already woken up, or already waking
-		 * up,nothing to do.
+		 * up, nothing to do.
 		 */
 		return;
 	}
@@ -476,6 +496,12 @@ void usb_wake(void)
 		return;
 
 	CPRINTF("WAKE\n");
+
+	/*
+	 * Sometimes the USB controller gets stuck, and does not count SOF/ESOF
+	 * frames anymore, detect that.
+	 */
+	hook_call_deferred(&usb_wake_deferred_data, 10 * MSEC);
 
 	/*
 	 * Set RESUME bit for 1 to 15 ms, then clear it. We ask the interrupt
