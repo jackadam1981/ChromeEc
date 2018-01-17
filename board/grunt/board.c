9e313dd1afe2a4fd263f987375a7cf5cd147a2e9
Simon Glass <sjg@chromium.org>
2018-01-17 10:28:07 -0800

    grunt: Add support for I2C bus 5
    This is used by the keyboard backlight driver. Add support for it so we
    can use this chip.
    
    BUG=b:69379749
    BRANCH=none
    TEST=manual:
    boot AP
    gpioset KB_BL_EN 1
    i2cscan
    See that bus 5 shows 0x6c now
    
    Change-Id: I37dd497688f7d280119bf2a226e23bee35063e0c
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- board/grunt/board.c
+++ board/grunt/board.c
@@ -127,11 +127,12 @@ BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
 
 /* I2C port map. */
 const struct i2c_port_t i2c_ports[] = {
-	{"power",   I2C_PORT_POWER,   100, GPIO_I2C0_SCL, GPIO_I2C0_SDA},
-	{"tcpc0",   I2C_PORT_TCPC0,   400, GPIO_I2C1_SCL, GPIO_I2C1_SDA},
-	{"tcpc1",   I2C_PORT_TCPC1,   400, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
-	{"thermal", I2C_PORT_THERMAL, 400, GPIO_I2C3_SCL, GPIO_I2C3_SDA},
-	{"sensor",  I2C_PORT_SENSOR,  400, GPIO_I2C7_SCL, GPIO_I2C7_SDA},
+	{"power",     I2C_PORT_POWER,   100, GPIO_I2C0_SCL, GPIO_I2C0_SDA},
+	{"tcpc0",     I2C_PORT_TCPC0,   400, GPIO_I2C1_SCL, GPIO_I2C1_SDA},
+	{"tcpc1",     I2C_PORT_TCPC1,   400, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
+	{"thermal",   I2C_PORT_THERMAL, 400, GPIO_I2C3_SCL, GPIO_I2C3_SDA},
+	{"eeprom_bl", I2C_PORT_KEYB_BL, 100, GPIO_I2C5_SCL, GPIO_I2C5_SDA},
+	{"sensor",    I2C_PORT_SENSOR,  400, GPIO_I2C7_SCL, GPIO_I2C7_SDA},
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
