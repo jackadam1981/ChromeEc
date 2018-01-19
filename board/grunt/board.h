e1016d4b296c6c3626684c7041ab9378eac10de5
Daniel Kurtz <djkurtz@chromium.org>
2018-01-18 19:51:13 -0800

    grunt: Add CONFIG_VSTORE
    Add a slot of secure temporary storage to support suspend/resume.
    
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    
    BUG=b:72188897
    TEST=On grunt w/ S3-enabled coreboot:
      powerd_dbus_suspend --wakeup_timeout=10
     => Device resumes throught coreboot to kernel
    BRANCH=none
    
    Change-Id: I37410a3e66fc6eee32e559069a33e904880cddc9

--- board/grunt/board.h
+++ board/grunt/board.h
@@ -23,6 +23,13 @@
 #define CONFIG_SPI_FLASH_REGS
 #define CONFIG_SPI_FLASH_W25Q80 /* Internal SPI flash type. */
 
+/*
+ * Enable 1 slot of secure temporary storage to support
+ * suspend/resume with read/write memory training.
+ */
+#define CONFIG_VSTORE
+#define CONFIG_VSTORE_SLOT_COUNT 1
+
 #define CONFIG_ADC
 #define CONFIG_BOARD_VERSION
 #define CONFIG_I2C
