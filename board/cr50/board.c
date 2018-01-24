40a60029b68721ca9abf2376ad5b9b0b1a6e7c2d
Mary Ruthven <mruthven@chromium.org>
2018-01-24 10:46:27 -0800

    cr50: create ap_uart state machine
    Separate ap uart detection from AP detection, so we can disable the AP
    uart without enabling deep sleep. If the AP is in S3 on ARM devices,
    Cr50 wont be in deep sleep, but the AP UART RX signal wont be pulled up.
    In this case we need cr50 ap rx to be disabled and deep sleep to be
    disabled.
    
    This change creates a state machine to handle ap uart detection. If cr50
    uses the uart to detect the AP state entirely, ap_uart_is_on/off will
    call ap_is_on and off as necessary.
    
    BUG=b:35647982
    BRANCH=cr50
    TEST=run firmware_Cr50DeviceState on scalet and electro
    
    Change-Id: I81336a9e232df8d44b325eef59327a1c06a80cba
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -592,12 +592,6 @@ static void configure_board_specific_gpios(void)
 		/* Enable powerdown exit on DIOM0 */
 		GWRITE_FIELD(PINMUX, EXITEN0, DIOM0, 1);
 	}
-	if (!board_detect_ap_with_tpm_rst()) {
-		/* Use AP UART TX as the DETECT AP signal. */
-		GWRITE(PINMUX, GPIO1_GPIO1_SEL, GC_PINMUX_DIOA3_SEL);
-		/* Enable the input */
-		GWRITE_FIELD(PINMUX, DIOA3_CTL, IE, 1);
-	}
 }
 
 void decrement_retry_counter(void)
@@ -828,7 +822,7 @@ static void deferred_tpm_rst_isr(void)
 	 * the only way those boards connect; they don't examine AP UART TX.
 	 */
 	if (board_detect_ap_with_tpm_rst())
-		set_ap_on_deferred();
+		set_ap_on();
 
 	/*
 	 * If no reboot request is posted, OR if the other RW's header is not
