6c30b0bd47c9d537d85dc48430dab0fbe1f9e896
Elthan_Huang <elthan_huang@comapl.corp-partner.google.com>
2018-01-25 13:24:50 +0800

    Nami: Update typeC adapter parameter
    Nami support 65W typeC adapter
    
    BUG=none
    BRANCH=none
    TEST=Type "ectool usbpdpower" in VT2 mode and check
    the watt when plug in typeC adapter(65W)
    
    Change-Id: I7c3a96f602270adc398d98b0e9a09dcb8348fec4
    Signed-off-by: Elthan_Huang <elthan_huang@compal.corp-partner.google.com>

--- board/nami/board.h
+++ board/nami/board.h
@@ -237,9 +237,9 @@ enum pwm_channel {
 #define PD_VCONN_SWAP_DELAY		5000   /* us */
 
 /* Define typical operating power and max power */
-#define PD_OPERATING_POWER_MW		15000
-#define PD_MAX_POWER_MW			45000
-#define PD_MAX_CURRENT_MA		3000
+#define PD_OPERATING_POWER_MW		10000
+#define PD_MAX_POWER_MW			65000
+#define PD_MAX_CURRENT_MA		3250
 #define PD_MAX_VOLTAGE_MV		20000
 
 /* Request the max voltage for type-c adapter */
