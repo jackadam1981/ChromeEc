143939c8d28821990482b9946a4af29a83778a5c
Sadashiva Rao Pv <sadashiva.rao.pv@intel.com>
2018-01-25 09:31:49 +0530

    ISH GPIO: GPIO functionality
    BUG=none
    BRANCH=none
    TEST=Tested on Soraka board with ISH and build is also success
    
    Change-Id: I45d8c1e3980cc8fc8d49f352f2fdc8d186923ae9
    Signed-off-by: Sadashiva Rao Pv <sadashiva.rao.pv@intel.com>
    Signed-off-by: Naresh Solanki <naresh.solanki@intel.com>
    Signed-off-by: li feng <li1.feng@intel.com>

--- include/config.h
+++ include/config.h
@@ -1709,12 +1709,6 @@
 #undef CONFIG_I2C_MULTI_PORT_CONTROLLER
 
 /*****************************************************************************/
-/* ISH config */
-
-/* Various ISH version */
-#undef CONFIG_ISH_30
-#undef CONFIG_ISH_40
-
 /* Current/Power monitor */
 
 /*
