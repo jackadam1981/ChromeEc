d9aa4ae92e2a62daf82079a34914bed7734173bb
Sadashiva Rao Pv <sadashiva.rao.pv@intel.com>
2018-01-25 09:31:49 +0530

    ISH GPIO: GPIO functionality
    BUG=none
    BRANCH=none
    TEST=Tested on Soraka board with ISH and build is also success
    
    Change-Id: I45d8c1e3980cc8fc8d49f352f2fdc8d186923ae9
    Signed-off-by: Sadashiva Rao Pv <sadashiva.rao.pv@intel.com>
    Signed-off-by: Naresh Solanki <naresh.solanki@intel.com>
    Signed-off-by: li feng <li1.feng@intel.com>

--- chip/ish/gpio.c
+++ chip/ish/gpio.c
@@ -14,27 +14,124 @@
 #include "timer.h"
 #include "util.h"
 
+#define ISH_TOTAL_GPIO_PINS 16
+
+struct gpio_int_mapping {
+	int8_t girq_id;
+	int8_t port_offset;
+};
+
 test_mockable int gpio_get_level(enum gpio_signal signal)
 {
-	return 0;
+	return  !!(ISH_GPIO_CTL(ISH_GPIO_GPLR) & gpio_list[signal].mask);
 }
 
 void gpio_set_level(enum gpio_signal signal, int value)
 {
+	if (value)
+		ISH_GPIO_CTL(ISH_GPIO_GPSR) |=  gpio_list[signal].mask;
+	else
+		ISH_GPIO_CTL(ISH_GPIO_GPCR) |=  gpio_list[signal].mask;
+}
+
+void gpio_set_flags_by_mask(uint32_t port, uint32_t mask, uint32_t flags)
+{
+	/* GPDR pin direction */
+	if (flags & GPIO_OUTPUT)
+		ISH_GPIO_CTL(ISH_GPIO_GPDR) &= ~mask;
+	if (flags & GPIO_INPUT)
+		ISH_GPIO_CTL(ISH_GPIO_GPDR) |= mask;
+
+	/* GPSR/GPCR Pin output set/clear */
+	if (flags & GPIO_HIGH) /* Rising edge detect */
+		ISH_GPIO_CTL(ISH_GPIO_GPSR) |= mask;
+	if (flags & GPIO_LOW) /* Level high detect */
+		ISH_GPIO_CTL(ISH_GPIO_GPCR) &= ~mask;
+
+	/* GRER/GFER interrupt trigger */
+	if (flags & GPIO_INT_F_RISING)
+		ISH_GPIO_CTL(ISH_GPIO_GRER) |= mask;
+	if (flags & GPIO_INT_F_HIGH)
+		ISH_GPIO_CTL(ISH_GPIO_GRER) |= mask;
+
+	if (flags & GPIO_INT_F_FALLING)
+		ISH_GPIO_CTL(ISH_GPIO_GFER) |= mask;
+	if (flags & GPIO_INT_F_LOW)
+		ISH_GPIO_CTL(ISH_GPIO_GFER) |= mask;
+}
+
+int gpio_enable_interrupt(enum gpio_signal signal)
+{
+	if ((gpio_list[signal].mask == 0) ||
+		gpio_list[signal].mask >= (1 << ISH_TOTAL_GPIO_PINS))
+		return EC_SUCCESS;
+
+	ISH_GPIO_CTL(ISH_GPIO_GIMR) |= gpio_list[signal].mask;
+
+	return EC_SUCCESS;
+}
+
+int gpio_disable_interrupt(enum gpio_signal signal)
+{
+	if ((gpio_list[signal].mask == 0) ||
+		gpio_list[signal].mask >= (1 << ISH_TOTAL_GPIO_PINS))
+		return EC_SUCCESS;
+
+	ISH_GPIO_CTL(ISH_GPIO_GIMR) &= ~gpio_list[signal].mask;
+
+	return EC_SUCCESS;
+}
+
+int gpio_clear_pending_interrupt(enum gpio_signal signal)
+{
+	if ((gpio_list[signal].mask == 0) ||
+                gpio_list[signal].mask >= (1 << ISH_TOTAL_GPIO_PINS))
+		return EC_SUCCESS;
+
+	ISH_GPIO_CTL(ISH_GPIO_GISR) |= gpio_list[signal].mask;
+	return EC_SUCCESS;
 }
 
 void gpio_pre_init(void)
 {
+	int i;
+	int flags;
+	int is_warm = system_is_reboot_warm();
+	const struct gpio_info *g = gpio_list;
+
+	for (i = 0; i < GPIO_COUNT; i++, g++) {
+		flags = g->flags;
+
+	if (flags & GPIO_DEFAULT)
+		continue;
+
+	/*
+	 *  *  * If this is a warm reboot, don't set the output levels or
+	 *   *   * we'll shut off the AP.
+	 *    *    */
+	if (is_warm)
+		flags &= ~(GPIO_LOW | GPIO_HIGH);
+
+	gpio_set_flags_by_mask(g->port, g->mask, flags);
+
+	}
 }
 
 static void gpio_init(void)
 {
-	/* TBD */
+	task_enable_irq(ISH_GPIO_IRQ);
 }
 
 static void gpio_interrupt(void)
 {
-	/*TODO*/
+	int i;
+	const struct gpio_info *g = gpio_list;
+	uint32_t gisr = ISH_GPIO_CTL(ISH_GPIO_GISR);
+
+	for (i = 0; i < GPIO_IH_COUNT; i++, g++) {
+		if (gisr & g->mask)
+			gpio_irq_handlers[i](i);
+	}
 }
 
 DECLARE_IRQ(ISH_GPIO_IRQ, gpio_interrupt);
