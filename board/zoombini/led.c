9ffc2267bef184f4a4fbe8b43bad82ddd82d584d
Nicolas Boichat <drinkcat@chromium.org>
2018-01-29 11:40:18 +0800

    zoombini: led: Reuse enumeration from ec_commands.h
    led.c used to define EC_LED_ID_COUNT, which is not really necessary,
    and would clash with the one defined in ec_commands.h.
    
    Let's reuse ec_led_id enumeration from ec_commands.h, instead of
    defining a new enum.
    
    BRANCH=none
    BUG=None
    TEST=make buildall -j
    
    Change-Id: I8dfcc04c821fd03c51fb505846c1c745711881c4
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- board/zoombini/led.c
+++ board/zoombini/led.c
@@ -14,27 +14,22 @@
 #include "charge_state.h"
 #include "chipset.h"
 #include "common.h"
+#include "ec_commands.h"
 #include "hooks.h"
 #include "pwm.h"
 #include "timer.h"
 
-enum led_id {
-	EC_LED_ID_POWER = 0,
-	EC_LED_ID_BATTERY,
-	EC_LED_ID_COUNT,
-};
-
-static enum pwm_channel led_pwm_ch_map[EC_LED_ID_COUNT] = {
+static enum pwm_channel led_pwm_ch_map[] = {
 #ifdef BOARD_MEOWTH
-	[EC_LED_ID_POWER] = PWM_CH_DB0_LED_GREEN,
-	[EC_LED_ID_BATTERY] = PWM_CH_DB0_LED_RED,
+	[EC_LED_ID_POWER_LED] = PWM_CH_DB0_LED_GREEN,
+	[EC_LED_ID_BATTERY_LED] = PWM_CH_DB0_LED_RED,
 #else /* !defined(BOARD_MEOWTH) */
-	[EC_LED_ID_POWER] = PWM_CH_LED_GREEN,
-	[EC_LED_ID_BATTERY] = PWM_CH_LED_RED,
+	[EC_LED_ID_POWER_LED] = PWM_CH_LED_GREEN,
+	[EC_LED_ID_BATTERY_LED] = PWM_CH_LED_RED,
 #endif /* defined(BOARD_MEOWTH) */
 };
 
-static void set_led_state(enum led_id id, int on)
+static void set_led_state(enum ec_led_id id, int on)
 {
 	int val;
 #ifdef BOARD_MEOWTH
@@ -60,9 +55,9 @@ static void pulse_power_led(void)
 	}
 
 	if (tick_count == 0)
-		set_led_state(EC_LED_ID_POWER, 1);
+		set_led_state(EC_LED_ID_POWER_LED, 1);
 	else
-		set_led_state(EC_LED_ID_POWER, 0);
+		set_led_state(EC_LED_ID_POWER_LED, 0);
 
 	/* 4 second period, 25% duty cycle. */
 	tick_count = (tick_count + 1) % 4;
@@ -75,7 +70,7 @@ static void power_led_update(void)
 		/* Make sure we stop pulsing. */
 		power_led_is_pulsing = 0;
 		/* The power LED must be on in the Active state. */
-		set_led_state(EC_LED_ID_POWER, 1);
+		set_led_state(EC_LED_ID_POWER_LED, 1);
 	} else if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
 		/* The power LED must pulse in the Suspend state. */
 		if (!power_led_is_pulsing) {
@@ -86,7 +81,7 @@ static void power_led_update(void)
 		/* Make sure we stop pulsing. */
 		power_led_is_pulsing = 0;
 		/* The power LED must be off in the Deep Sleep state. */
-		set_led_state(EC_LED_ID_POWER, 0);
+		set_led_state(EC_LED_ID_POWER_LED, 0);
 	}
 }
 
@@ -97,7 +92,7 @@ static void battery_led_update(void)
 	switch (chg_st) {
 	case PWR_STATE_DISCHARGE:
 	case PWR_STATE_IDLE:
-		set_led_state(EC_LED_ID_BATTERY, 0);
+		set_led_state(EC_LED_ID_BATTERY_LED, 0);
 		break;
 
 	/*
@@ -106,7 +101,7 @@ static void battery_led_update(void)
 	 */
 	case PWR_STATE_CHARGE_NEAR_FULL:
 	case PWR_STATE_CHARGE:
-		set_led_state(EC_LED_ID_BATTERY, 1);
+		set_led_state(EC_LED_ID_BATTERY_LED, 1);
 		break;
 
 	default:
