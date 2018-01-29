67fa2fbd6f10552e8df70b3b7f891208a012a03a
Elmo_Lan <elmo_lan@compal.corp-partner.google.com>
2018-01-29 04:42:28 -0500

    Nami: Add ALS source
    Add a new ALS source.
    (CAPELLA, CM32181E)
    
    BUG=none
    BRANCH=none
    TEST=Verify Nami can read ALS by cpu console.
    
    Change-Id: I9f9b78798158908759b9316dfe1b887f46608837
    Signed-off-by: Elmo_Lan <elmo_lan@compal.corp-partner.google.com>

---
+++ driver/als_cm32181e.h
@@ -0,0 +1,38 @@
+/* Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * TI CM32181E light sensor driver
+ */
+
+#ifndef __CROS_EC_ALS_CM32181E_H
+#define __CROS_EC_ALS_CM32181E_H
+
+/* I2C interface */
+// Address Select Pin pull low
+#define CM32181E_I2C_ADDR1		(0x10 << 1)
+// Address Select Pin pull high
+#define CM32181E_I2C_ADDR2		(0x48 << 1)
+
+/* CM32181E registers */
+#define CM32181E_REG_RESULT		0x04
+#define CM32181E_REG_CONFIGURE	0x00
+
+/* Min and Max sampling frequency in mHz */
+// initial as 0x00C0, refresh time is 1300ms, about 7000mhz
+#define CM32181E_LIGHT_MIN_FREQ          20
+#define CM32181E_LIGHT_MAX_FREQ          7000
+
+#define CM32181E_GET_DATA(_s)	((struct cm32181e_drv_data_t *)(_s)->drv_data)
+
+struct cm32181e_drv_data_t {
+	int rate;
+	int last_value;
+	/* the coef is scale.uscale */
+	int16_t scale;
+	uint16_t uscale;
+	int16_t offset;
+};
+
+extern const struct accelgyro_drv cm32181e_drv;
+#endif	/* __CROS_EC_ALS_CM32181E_H */
