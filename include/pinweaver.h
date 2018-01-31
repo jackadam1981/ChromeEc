58c4c3079a7ade9f7add3f5c4f64da200478fe3d
Allen Webb <allenwebb@google.com>
2018-01-31 11:21:20 -0800

    Cr50: Added Pinweaver base implementation.
    This adds some of the ground work for hardware backed brute force
    resistance on Cr50. The feature is called Pinweaver. It will
    initially be used to enable pin authentication on CrOS devices
    especially tablets without reducing the security of the platform.
    A merkel tree is used to validate encrypted metadata used to track
    login attempts.
    
    The metadata tracks counts of failed attempts along with a
    timestamp of the last failed attempt along with the secrets and any
    associated parameters. Instead of storing the metadata on Cr50 an
    AES-CTR is used along with an HMAC to encrypt the data so it can
    be stored off-chip and loaded when needed.
    
    The Merkle tree is used to track the current state of all the
    metadata to prevent replay attacks of previously exported copies.
    It is a tree of hashes whose root hash is stored on Cr50, and whose
    leaves are the HMACs of the encrypted metadata.
    
    BRANCH=none
    BUG=chromium:794010
    TEST=cd ~/src/platform/ec && V=1 make run-pinweaver -j
    
    Change-Id: Id10bb49d8ebc5a487dd90c6093bc0f51dadbd124
    Signed-off-by: Allen Webb <allenwebb@google.com>

---
+++ include/pinweaver.h
@@ -0,0 +1,62 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_PINWEAVER_H
+#define __CROS_EC_PINWEAVER_H
+
+/* This is required before pinweaver_types.h to provide __packed and __aligned
+ * while preserving the ability of pinweaver_types.h to be used in code outside
+ * of src/platform/ec.
+ */
+#include <common.h>
+#include <pinweaver_types.h>
+
+#include <dcrypto.h>
+
+#define PW_STORAGE_VERSION 0
+
+/* Persistent information used by this feature. */
+struct PW_PACKED merkle_tree_t {
+	/* log2(Fan out). */
+	struct bits_per_level_t bits_per_level;
+	/* Height of the tree or param_l / bits_per_level. */
+	struct height_t height;
+
+	/* Root hash of the Merkle tree. */
+	uint8_t root[PW_HASH_SIZE];
+
+	/* Key used to compute the HMACs of the metadata of the leaves. */
+	uint8_t hmac_key[32];
+
+	/* Key used to encrypt and decrypt the metadata of the leaves. */
+	uint8_t wrap_key[AES256_BLOCK_CIPHER_KEY_SIZE];
+};
+
+/* Handler for incoming messages after they have been reconstructed.
+ *
+ * merkle_tree->root needs to be updated with new_root outside of this function.
+ */
+int pw_handle_request(struct merkle_tree_t *merkle_tree,
+		      const struct pw_request_t *request,
+		      struct pw_response_t *response);
+
+/******************************************************************************/
+/* Utility functions exported for better test coverage.
+ */
+
+/* Computes the total number of the sibling hashes along a path. */
+int get_path_length(const struct merkle_tree_t *merkle_tree);
+
+/* Extract the child index from the label at the specified level of the tree. */
+struct index_t get_index(const struct merkle_tree_t *merkle_tree,
+			 struct label_t label, struct height_t level);
+
+/* Computes the parent hash for an array of child hashes. */
+void compute_hash(const uint8_t hashes[][PW_HASH_SIZE], uint16_t num_hashes,
+		  struct index_t location,
+		  const uint8_t child_hash[PW_HASH_SIZE],
+		  uint8_t result[PW_HASH_SIZE]);
+
+#endif  /* __CROS_EC_PINWEAVER_H */
