a47c191bd5f9512116fa7cad717c607ba6e4d0c5
Vadim Bendebury <vbendeb@chromium.org>
2018-02-01 17:23:27 -0800

    g: handle uart interrupt storms
    UART controller of the g chip does not process line break condition
    properly, instead of reporting it just once it keeps reporting
    received characters of zero at maximum rate, causing RX interrupt
    storms.
    
    Value of zero is never transmitted by valid sources, but when the AP
    on ARM devices goes to sleep it drives the AP UART TX line low, which
    causes H1 interrupt storms.
    
    This patch detects this condition by counting how many times in a row
    a zero is received, If more than three zeros are received, this
    receive channel's RX interrupts are disabled and a periodic hook is
    started to check if the channel is back. A channel is considered back
    if the FIFO is not full, or if there is at least one non-zero
    character in the FIFO.
    
    BRANCH=cr50
    BUG=b:35647982
    TEST=opened/closed Bob lid 10 times in a row. Observed messages about
         UART1 being disabled/enabled. Verified that AP console is still
         available over CCD and that H1 was falling into sleep when lid
         was closed.
    
    Change-Id: I5164bab05d3e73a0e93190c85dfd5afc2b42dd01
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- chip/g/uartn.c
+++ chip/g/uartn.c
@@ -6,6 +6,7 @@
 #include "clock.h"
 #include "common.h"
 #include "gpio.h"
+#include "hooks.h"
 #include "registers.h"
 #include "system.h"
 #include "task.h"
@@ -13,8 +14,12 @@
 #include "uart_bitbang.h"
 #include "util.h"
 
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
+
 #define USE_UART_INTERRUPTS (!(defined(CONFIG_CUSTOMIZED_RO) && \
 			       defined(SECTION_IS_RO)))
+
+#define UART_COUNT     3
 
 struct uartn_interrupts {
 	int tx_int;
@@ -138,9 +143,119 @@ void uartn_write_char(int uart, char c)
 	uartn_funcs[uart]._write_char(uart, c);
 }
 
+#if USE_UART_INTERRUPTS
+
+/*
+ * Counter of break characters received in a row, per UART channel. Receiving
+ * 3 or more is considered an indication of the UART RX line held low by the
+ * inactive TX.
+ *
+ * Once storm condition is identified, the same value is sued to control UART
+ * RX state transitions:
+ *
+ *   4 - interrupt disable pending
+ * 100 - interrupt disabled
+ */
+enum {
+	IRQS_MAX_BREAKS_IN_A_ROW = 3,
+	IRQS_INTERRUPT_DISABLE_PENDING = 4,
+	IRQS_INTERRUPT_DISABLED = 100,
+};
+static uint8_t breaks_in_a_row[UART_COUNT];
+
+/* Flag to tell if the reenable UART hook is running. */
+static uint8_t reenable_hook_is_running;
+
+/* G chip has large FIFOs. */
+#define G_RX_FIFO_SIZE 32
+
+const struct deferred_data reenable_rx_irq_when_ready_data;
+static void reenable_rx_irq_when_ready(void)
+{
+	size_t i;
+	int keep_going = 0;
+
+	for (i = 0; i < ARRAY_SIZE(breaks_in_a_row); i++) {
+		int loop_limit;
+		int got_nonzero_data;
+
+		if (breaks_in_a_row[i] != IRQS_INTERRUPT_DISABLED)
+			continue;
+
+		/*
+		 * For all channels which where interrupt storm was detected
+		 * check if storm is still on. This would be indicated by FIFO
+		 * filled up with 32 zeros. If FIFO is less than 32 characters
+		 * full, or there is a non-zero character, the storm is over.
+		 */
+		keep_going++;
+		loop_limit = 0;
+		got_nonzero_data = 0;
+		while (_uartn_rx_available(i) &&
+		       (loop_limit++ < G_RX_FIFO_SIZE))
+			if (GR_UART_RDATA(i)) {
+				got_nonzero_data = 1;
+				break;
+			}
+
+		if ((loop_limit < G_RX_FIFO_SIZE) || got_nonzero_data) {
+			breaks_in_a_row[i] = 0;
+			task_enable_irq(interrupt[i].rx_int);
+			CPRINTS("Re-enabled UART%d (limit at %d)",
+				i, loop_limit);
+			keep_going--;
+		}
+	}
+
+	if (!keep_going) {
+		reenable_hook_is_running = 0;
+		return;
+	}
+
+	hook_call_deferred(&reenable_rx_irq_when_ready_data, 1 * SECOND);
+}
+DECLARE_DEFERRED(reenable_rx_irq_when_ready);
+
+static void disable_rx_irq(void)
+{
+	size_t i;
+
+	for (i = 0; i < ARRAY_SIZE(breaks_in_a_row); i++)
+		if (breaks_in_a_row[i] == IRQS_INTERRUPT_DISABLE_PENDING) {
+			task_disable_irq(interrupt[i].rx_int);
+			CPRINTS("Disabled UART%d due to inerrupt storm", i);
+			breaks_in_a_row[i] = IRQS_INTERRUPT_DISABLED;
+			if (!reenable_hook_is_running)
+				reenable_hook_is_running = 1;
+			hook_call_deferred(&reenable_rx_irq_when_ready_data,
+					   1 * SECOND);
+		}
+}
+DECLARE_DEFERRED(disable_rx_irq);
+
+#endif
+
 int _uartn_read_char(int uart)
 {
-	return GR_UART_RDATA(uart);
+	int c;
+
+	c = GR_UART_RDATA(uart);
+
+#if USE_UART_INTERRUPTS
+	if (c) {
+		breaks_in_a_row[uart] = 0;
+	} else {
+		uint8_t breaks = breaks_in_a_row[uart];
+
+		if (breaks < IRQS_MAX_BREAKS_IN_A_ROW) {
+			breaks_in_a_row[uart] = breaks + 1;
+		} else if (breaks == IRQS_MAX_BREAKS_IN_A_ROW) {
+			breaks_in_a_row[uart] = IRQS_INTERRUPT_DISABLE_PENDING;
+			hook_call_deferred(&disable_rx_irq_data, 0);
+		}
+	}
+#endif
+	return c;
 }
 
 int uartn_read_char(int uart)
