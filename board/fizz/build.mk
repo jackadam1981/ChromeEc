1e23ef5fb4f8361d70f42eec3eb33c668173d25c
Daisuke Nojiri <dnojiri@chromium.org>
2018-02-05 12:54:56 -0800

    Fizz/CBI: Create CBI blobs
    This patch makes make create blobs for EEPROM which contains
    Cros Board Info.
    
    BUG=b:72949522
    BRANCH=none
    TEST=make buildall. make BOARD=fizz cbi_kench.
    
    Change-Id: Ie4c50f4707285b44c13afc7410a5ea823a26d98e
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/build.mk
+++ board/fizz/build.mk
@@ -12,3 +12,22 @@ CHIP_VARIANT:=npcx5m6g
 board-y=board.o
 board-$(CONFIG_USB_POWER_DELIVERY)+=usb_pd_policy.o
 board-y+=led.o
+
+brd_ver = 0
+
+make_cbi = $(out)/util/cbi-util --create $(1)_$(3).bin \
+		--board_version $(brd_ver) --oem_id $(2) --sku_id $(3) --size 256;
+
+cbi_kench: sku_ids = 0 1 2 3 4 5 6
+cbi_kench: $(out)/util/cbi-util
+	$(foreach s,$(sku_ids),$(call make_cbi,$(out)/$@,0,$(s)))
+
+cbi_teemo: sku_ids = 0 1 4 5
+cbi_teemo: $(out)/util/cbi-util
+	$(foreach s,$(sku_ids),$(call make_cbi,$(out)/$@,1,$(s)))
+
+cbi_sion: sku_ids = 0 1 2 3 4 5 6
+cbi_sion: $(out)/util/cbi-util
+	$(foreach s,$(sku_ids),$(call make_cbi,$(out)/$@,2,$(s)))
+
+PROJECT_EXTRA += cbi_kench cbi_teemo cbi_sion
