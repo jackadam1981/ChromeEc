a84ee03784a1236b7df17469659c2244a18f40cc
Aseda Aboagye <aaboagye@google.com>
2018-02-06 16:37:06 -0800

    USB PD: Save explicit contract state for port 2.
    pd_get/set_saved_active() made the assumption that there were only two
    ports.  But now, we have a board that turned that port count all the way
    up to 3.  This commit just adds in that new port BBRAM index.
    
    BUG=b:72838807
    BRANCH=None
    TEST=With some code to check for explicit contract state for port 2,
    verify it's functional.
    
    Change-Id: I6f062f67bd3c47dd43ea7e24e844a9286fa37af9
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -808,12 +808,28 @@ static int send_request(int port, uint32_t rdo)
 	return bit_len;
 }
 
+static int get_bbram_idx(int port)
+{
+	switch (port) {
+	case 2:
+		return SYSTEM_BBRAM_IDX_PD2;
+
+	case 1:
+		return SYSTEM_BBRAM_IDX_PD1;
+
+	case 0:
+		return SYSTEM_BBRAM_IDX_PD0;
+
+	default:
+		return -1;
+	}
+}
+
 static int pd_get_saved_active(int port)
 {
 	uint8_t val;
 
-	if (system_get_bbram(port ? SYSTEM_BBRAM_IDX_PD1 :
-				    SYSTEM_BBRAM_IDX_PD0, &val)) {
+	if (system_get_bbram(get_bbram_idx(port), &val)) {
 		CPRINTS("PD NVRAM FAIL");
 		return 0;
 	}
@@ -822,8 +838,7 @@ static int pd_get_saved_active(int port)
 
 static void pd_set_saved_active(int port, int val)
 {
-	if (system_set_bbram(port ? SYSTEM_BBRAM_IDX_PD1 :
-				    SYSTEM_BBRAM_IDX_PD0, val))
+	if (system_set_bbram(get_bbram_idx(port), val))
 		CPRINTS("PD NVRAM FAIL");
 }
 #endif /* CONFIG_USB_PD_DUAL_ROLE */
@@ -1148,9 +1163,6 @@ static void handle_data_request(int port, uint16_t head,
 				if (pd[port].rev == PD_REV30 &&
 					pd[port].power_role == PD_ROLE_SOURCE)
 					sink_can_xmit(port, SINK_TX_OK);
-#endif
-#ifdef CONFIG_USB_PD_DUAL_ROLE
-				pd_set_saved_active(port, 1);
 #endif
 				pd[port].requested_idx = RDO_POS(payload[0]);
 				set_state(port, PD_STATE_SRC_ACCEPTED);
