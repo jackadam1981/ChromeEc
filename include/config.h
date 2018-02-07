39b9ba0818296d9423141faed0cb24dd7c64b80b
Philip Chen <philipchen@google.com>
2018-02-06 18:31:43 -0800

    Introduce CONFIG_USB_PD_5V_CHARGER_CTRL
    Add a new config for the boards using charger (e.g. rt946x)
    to report if VBUS source is enabled instead of using GPIO.
    
    BUG=b:65446459
    BRANCH=none
    TEST=Charge Scarlet rev3 with SDP and DCP.
    
    Change-Id: Id0a07945f0f888b6a36c422c596b56c5aa5065c0
    Signed-off-by: Philip Chen <philipchen@google.com>

--- include/config.h
+++ include/config.h
@@ -2727,6 +2727,9 @@
  */
 #undef CONFIG_USB_PD_5V_EN_ACTIVE_LOW
 
+/* Ask charger if VBUS is enabled on a source port, instead of using GPIO */
+#undef CONFIG_USB_PD_5V_CHARGER_CTRL
+
 /* Dynamic USB PD source capability */
 #undef CONFIG_USB_PD_DYNAMIC_SRC_CAP
 
