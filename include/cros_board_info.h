e06e06c73211340281c33eb59d9ce5526fba3c21
Daisuke Nojiri <dnojiri@chromium.org>
2018-02-14 15:20:09 -0800

    CBI: Make data offset and size variable
    Currently CBI data offset and size are fixed. This patch makes them
    variable. Each data item consists of <tag><size><value> where <tag>
    is a numeric value assigned to each data item, <size> is the number
    of bytes used for <value>.
    
    BUG=
    BRANCH=none
    TEST=Use 'ectool cbi set' to set board version, oem, sku.
    Verify the contents by cbi console command and ectool cbi get.
    1. ectool cbi set 0 0x202 2 2 (Init CBI and write board ver. of size 2)
    2. ectool cbi set 1 1 1 (write oem id of size 1)
    3. ectool cbi set 2 2 1 (write sku id of size 1)
    4. ectool cbi get 0
     514 (0x202)
    5. ectool cbi get 1
     1 (0x1)
    6. ectool cbi get 2
     2 (0x2)
    7. Run cbi console command:
     CBI_VERSION: 0x0000
     TOTAL_SIZE: 18
     BOARD_VERSION: 514 (0x202)
     OEM_ID: 1 (0x1)
     SKU_ID: 2 (0x2)
      43 42 49 8c 00 00 12 00 00 02 02 02 01 01 01 02
      01 02
    
    Change-Id: I5a30a4076e3eb448f4808d2af8ec4ef4c016ae5e
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/cros_board_info.h
+++ include/cros_board_info.h
@@ -8,6 +8,7 @@
 #define __CROS_EC_CROS_BOARD_INFO_H
 
 #include "common.h"
+#include "ec_commands.h"
 
 #define CBI_VERSION_MAJOR 0
 #define CBI_VERSION_MINOR 0
@@ -31,22 +32,6 @@ struct cbi_header {
 	uint16_t total_size;
 } __attribute__((packed));
 
-struct board_info {
-	struct cbi_header head;
-	/* Board version */
-	union {
-		struct {
-			uint8_t minor_version;
-			uint8_t major_version;
-		};
-		uint16_t version;
-	};
-	/* OEM ID */
-	uint8_t oem_id;
-	/* SKU ID */
-	uint8_t sku_id;
-} __attribute__((packed));
-
 /**
  * Board info accessors
  *
@@ -56,5 +41,16 @@ struct board_info {
 int cbi_get_board_version(uint32_t *version);
 int cbi_get_sku_id(uint32_t *sku_id);
 int cbi_get_oem_id(uint32_t *oem_id);
+
+/**
+ * Primitive accessors
+ *
+ * @param tag   Tag of the target data.
+ * @param buf   Buffer where data is passed.
+ * @param size  (IN) Size of <buf>. (OUT) Size of the data returned.
+ * @return EC_SUCCESS on success or EC_ERROR_* otherwise.
+ */
+int cbi_get_board_info(enum cbi_data_tag tag, uint8_t *buf, uint8_t *size);
+int cbi_set_board_info(enum cbi_data_tag tag, const uint8_t *buf, uint8_t size);
 
 #endif /* __CROS_EC_CROS_BOARD_INFO_H */
