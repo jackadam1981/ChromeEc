6a3658aff51fd914840dc3df871c2d8876470590
Philip Chen <philipchen@google.com>
2018-02-21 20:19:19 -0800

    battery/max17055: Consider init failure when detecting battery presence
    If max17055 didn't init successfully, it can't report
    battery presence reliably.
    
    BUG=b:72697658
    BRANCH=scarlet
    TEST=boot w/o battery with both Type-C and PD chargers
    
    Change-Id: I5d97f5f282598a11016dab144bef59e855dfbb69
    Signed-off-by: Philip Chen <philipchen@google.com>

--- driver/battery/max17055.c
+++ driver/battery/max17055.c
@@ -53,6 +53,7 @@
 		} \
 	} while (0)
 
+static uint8_t init_ok;
 static int fake_state_of_charge = -1;
 
 static int max17055_read(int offset, int *data)
@@ -196,7 +197,7 @@ enum battery_present battery_is_present(void)
 {
 	int status = 0;
 
-	if (max17055_read(REG_STATUS, &status))
+	if (!init_ok || max17055_read(REG_STATUS, &status))
 		return BP_NOT_SURE;
 	if (status & STATUS_BST)
 		return BP_NO;
@@ -206,10 +207,6 @@ enum battery_present battery_is_present(void)
 void battery_get_params(struct batt_params *batt)
 {
 	int reg = 0;
-	const uint32_t flags_to_check = BATT_FLAG_BAD_TEMPERATURE |
-					BATT_FLAG_BAD_STATE_OF_CHARGE |
-					BATT_FLAG_BAD_VOLTAGE |
-					BATT_FLAG_BAD_CURRENT;
 
 	/* Reset flags */
 	batt->flags = 0;
@@ -245,12 +242,14 @@ void battery_get_params(struct batt_params *batt)
 	if (battery_full_charge_capacity(&batt->full_capacity))
 		batt->flags |= BATT_FLAG_BAD_FULL_CAPACITY;
 
-	/* If any of those reads worked, the battery is responsive */
-	if ((batt->flags & flags_to_check) != flags_to_check) {
+	/*
+	 * Assuming the battery is responsive as long as
+	 * max17055 finds battery is present.
+	 */
+	batt->is_present = battery_is_present();
+
+	if (batt->is_present == BP_YES)
 		batt->flags |= BATT_FLAG_RESPONSIVE;
-		batt->is_present = BP_YES;
-	} else
-		batt->is_present = BP_NOT_SURE;
 
 	/*
 	 * Charging allowed if both desired voltage and current are nonzero
@@ -356,6 +355,7 @@ static int max17055_init_config(void)
 	/* Restore the original HibCFG value. */
 	if (max17055_write(REG_HIBCFG, hib_cfg))
 		return EC_ERROR_UNKNOWN;
+
 	return EC_SUCCESS;
 }
 
@@ -368,6 +368,10 @@ static void max17055_init(void)
 		CPRINTS("Wrong max17055 id!");
 		return;
 	}
+
+	/* Set CONFIG.TSEL to measure temperature using external thermistor */
+	MAX17055_READ_DEBUG(REG_CONFIG, &reg);
+	MAX17055_WRITE_DEBUG(REG_CONFIG, (reg | CONF_TSEL));
 
 	MAX17055_READ_DEBUG(REG_STATUS, &reg);
 
@@ -396,10 +400,7 @@ static void max17055_init(void)
 	MAX17055_READ_DEBUG(REG_STATUS, &reg);
 	MAX17055_WRITE_DEBUG(REG_STATUS, (reg & ~STATUS_POR));
 
-	/* Set CONFIG.TSEL to measure temperature using external thermistor */
-	MAX17055_READ_DEBUG(REG_CONFIG, &reg);
-	MAX17055_WRITE_DEBUG(REG_CONFIG, (reg | CONF_TSEL));
-
+	init_ok = 1;
 	CPRINTS("max17055 configuration succeeded!");
 }
 DECLARE_HOOK(HOOK_INIT, max17055_init, HOOK_PRIO_DEFAULT);
