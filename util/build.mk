70651cf6d59402bb29bf55eb9a9d279d273abe83
Daisuke Nojiri <dnojiri@chromium.org>
2018-02-22 09:25:50 -0800

    CBI: Share common code between host tool and firmware
    This patch makes EC firmware and cbi-util share the common code.
    
    BUG=none
    BRANCH=none
    TEST=Set fields using ectool. Verify the contents by cbi command.
    Verify cbi-util creates the same binary as before.
    
    Change-Id: If5e65e48dd03960e0adf23ef775f67aecf785d85
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- util/build.mk
+++ util/build.mk
@@ -57,7 +57,8 @@ $(out)/util/gen_touchpad_hash: BUILD_CFLAGS += $(OPENSSL_CFLAGS)
 $(out)/util/gen_touchpad_hash: BUILD_LDFLAGS += $(OPENSSL_LDFLAGS)
 endif # CONFIG_TOUCHPAD_VIRTUAL_OFF
 
-cbi-util-objs=../common/crc8.o
+cbi-util-objs=../common/crc8.o ../common/cbi.o
+$(out)/util/cbi-util: BUILD_CFLAGS+=-DCBI_UTIL
 
 $(out)/util/export_taskinfo.so: $(out)/util/export_taskinfo_ro.o \
 			$(out)/util/export_taskinfo_rw.o
