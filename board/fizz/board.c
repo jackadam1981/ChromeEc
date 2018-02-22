d8d377ecf10c3e5b049c8182be2d0ccf9b70949e
Daisuke Nojiri <dnojiri@chromium.org>
2018-02-22 14:50:51 -0800

    Fizz: Set TMP431 to TEMP_SENSOR_TYPE_CPU
    This patch sets TMP431's local sensor to TEMP_SENSOR_TYPE_CPU, which
    will make the thermal controller skip reading it in S3/S5. This will
    avoid flodding EC console with 'Fan 0 stalled' messages.
    
    This patch also removes the two remote sensors. One doesn't exist.
    The other is placed next to the local sensor. So, it's redundant.
    
    BUG=b:73127788
    BRANCH=none
    TEST=Verify no 'Fan 0 stalled' is printed in S3 and S5. Verify the
    fan spins in S0.
    
    Change-Id: I8cf86c20491e95ec2b546777870584d57c700997
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/fizz/board.c
+++ board/fizz/board.c
@@ -271,12 +271,8 @@ uint16_t tcpc_get_alert_status(void)
  *     src/mainboard/google/${board}/acpi/dptf.asl
  */
 const struct temp_sensor_t temp_sensors[] = {
-	{"TMP432_Internal", TEMP_SENSOR_TYPE_BOARD, tmp432_get_val,
-			TMP432_IDX_LOCAL, 4},
-	{"TMP432_Sensor_1", TEMP_SENSOR_TYPE_BOARD, tmp432_get_val,
-			TMP432_IDX_REMOTE1, 4},
-	{"TMP432_Sensor_2", TEMP_SENSOR_TYPE_BOARD, tmp432_get_val,
-			TMP432_IDX_REMOTE2, 4},
+	{"TMP432_Internal", TEMP_SENSOR_TYPE_CPU,
+			tmp432_get_val, TMP432_IDX_LOCAL, 4},
 };
 BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 
@@ -287,12 +283,9 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 struct ec_thermal_config thermal_params[] = {
 	/* {Twarn, Thigh, Thalt}, <on>
 	 * {Twarn, Thigh, X    }, <off>
-	 * fan_off, fan_max
-	 */
+	 * fan_off, fan_max */
 	{{0, C_TO_K(80), C_TO_K(81)}, {0, C_TO_K(78), 0},
 		C_TO_K(4), C_TO_K(76)},	/* TMP432_Internal */
-	{{0, 0, 0}, {0, 0, 0}, 0, 0},	/* TMP432_Sensor_1 */
-	{{0, 0, 0}, {0, 0, 0}, 0, 0},	/* TMP432_Sensor_2 */
 };
 BUILD_ASSERT(ARRAY_SIZE(thermal_params) == TEMP_SENSOR_COUNT);
 
