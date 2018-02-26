95ce4c736db891014b1e8d345a71cf9552d6b8a9
Naresh G Solanki <naresh.solanki@intel.com>
2018-02-26 15:33:36 +0530

    soraka: Disable vr decay on V085ACNT
    In Kabylakem V085ACNT powers ROSC & XTAL.
    
    VR decay on V085ACNT needs to be kept disabled as XTAL & ROSC might not
    be gated fully during runtime.
    
    BUG=b:70881268
    BRANCH=None
    TEST= Build for Soraka & verify that runtime S0ix doesn't cause system
    reboot.
    
    Change-Id: I73eeafd3fbea477d4e6c4f90744cb6cc68709213
    Signed-off-by: Naresh G Solanki <naresh.solanki@intel.com>

--- board/poppy/board.c
+++ board/poppy/board.c
@@ -444,13 +444,13 @@ static void board_pmic_enable_slp_s0_vr_decay(void)
 	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x37, 0x5a);
 
 	/*
-	 * V085ACNT:
+	 * V085ACNT: Note: Disable VR decay on V085ACNT
 	 * Bits 7:6 (01) - Enable low power mode on SLP_S0# assertion
 	 * Bits 5:4 (11) - Nominal voltage 1.0V
 	 * Bits 3:2 (10) - VR set to AUTO on SLP_S0# de-assertion
 	 * Bits 1:0 (10) - VR set to AUTO operating mode
 	 */
-	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x38, 0x7a);
+	i2c_write8(I2C_PORT_PMIC, I2C_ADDR_BD99992, 0x38, 0x3a);
 }
 
 static void board_pmic_init(void)
