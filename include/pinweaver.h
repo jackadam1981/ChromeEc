347b39e91b1a488e6c690f055384d5cc86a4a97a
Allen Webb <allenwebb@google.com>
2018-02-28 15:42:47 -0800

    Cr50: Add logging functionality to PinWeaver.
    In order to be able to recover from the AP and Cr50 getting out of
    sync, this logging functionality gives Cr50 a way to track the
    state changes of the merkle tree so that the AP can be updated to
    the current state as long as it has a recent enough copy.
    
    This involves packing the important information so it can be stored
    efficiently on flash, and adding the necessary messages for the
    replay.
    
    BRANCH=none
    BUG=chromium:794010
    TEST=cd ~/src/platform/ec && V=1 make run-weaver_ng -j
    
    Change-Id: I40f98de2c8e9706cccb5b922215699f2132fa121
    Signed-off-by: Allen Webb <allenwebb@google.com>

--- include/pinweaver.h
+++ include/pinweaver.h
@@ -15,6 +15,8 @@
 
 #define PW_STORAGE_VERSION 0
 
+#define PW_LOG_ENTRY_COUNT 5
+
 /* Persistent information used by this feature. */
 struct PW_PACKED merkle_tree_t {
 	/* log2(Fan out). */
@@ -32,10 +34,41 @@ struct PW_PACKED merkle_tree_t {
 	uint8_t wrap_key[32];
 };
 
+/* Long term flash storage for tree metadata. */
+struct PW_PACKED pw_long_term_storage_t {
+	uint16_t storage_version;
+
+	/* log2(Fan out). */
+	struct bits_per_level_t bits_per_level;
+	/* Height of the tree or param_l / bits_per_level. */
+	struct height_t height;
+
+	/* Key used to compute the HMACs of the metadata of the leaves. */
+	uint8_t hmac_key[32];
+
+	/* Key used to encrypt and decrypt the metadata of the leaves. */
+	uint8_t wrap_key[32];
+};
+
+struct PW_PACKED pw_log_storage_t {
+	uint16_t storage_version;
+	uint32_t restart_count;
+	struct pw_get_log_entry_t entries[PW_LOG_ENTRY_COUNT];
+};
+
 struct PW_PACKED leaf_data_t {
 	struct leaf_public_data_t pub;
 	struct leaf_sensitive_data_t sec;
 };
+
+/* Key names for nvmem_vars */
+#define PW_TREE_VAR "pwT0"
+/* The log is split into two key value pairs because the max size is 255. */
+#define PW_LOG_VAR0 "pwL0"
+#define PW_LOG_VAR0_SIZE 255
+#define PW_LOG_VAR1 "pwL1"
+#define PW_LOG_VAR1_SIZE ((sizeof(struct pw_log_storage_t) - PW_LOG_VAR0_SIZE) \
+			  & 255)
 
 /* Handler for incoming messages after they have been reconstructed.
  *
@@ -87,5 +120,17 @@ void compute_hash(const uint8_t hashes[][PW_HASH_SIZE], uint16_t num_hashes,
 		  struct index_t location,
 		  const uint8_t child_hash[PW_HASH_SIZE],
 		  uint8_t result[PW_HASH_SIZE]);
+
+/* This should only be used in tests. */
+void force_restart_count(uint32_t mock_value);
+
+/* NV RAM log functions exported for use in test code. */
+int store_log_data(const struct pw_log_storage_t *log);
+int store_merkle_tree(const struct merkle_tree_t *merkle_tree);
+int log_insert_leaf(struct label_t label, const uint8_t root[PW_HASH_SIZE],
+		    const uint8_t hmac[PW_HASH_SIZE]);
+int log_remove_leaf(struct label_t label, const uint8_t root[PW_HASH_SIZE]);
+int log_auth(struct label_t label, const uint8_t root[PW_HASH_SIZE], int code,
+	     struct pw_timestamp_t timestamp);
 
 #endif  /* __CROS_EC_PINWEAVER_H */
