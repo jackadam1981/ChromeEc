fa87edc6552ec7b98ac10e870f306189c153bc9c
Daisuke Nojiri <dnojiri@chromium.org>
2018-03-05 09:49:40 -0800

    (DRAFT)fan: Allow board to select fans at run time
    Change-Id: Iff17573f110e07e88d097dd848cf91ee98b83176
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/nami/board.c
+++ board/nami/board.c
@@ -142,18 +142,16 @@ BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
 /******************************************************************************/
 /* Physical fans. These are logically separate from pwm_channels. */
-const struct fan_t fans[] = {
-	[FAN_CH_0] = {
-		.flags = FAN_USE_RPM_MODE,
-		.rpm_min = 2800,
-		.rpm_start = 3000,
-		.rpm_max = 6000,
-		.ch = MFT_CH_0,	/* Use MFT id to control fan */
-		.pgood_gpio = -1,
-		.enable_gpio = -1,
-	},
+const struct fan_t *fans[FAN_CH_COUNT];
+const struct fan_t fan_0 = {
+	.flags = FAN_USE_RPM_MODE,
+	.rpm_min = 2800,
+	.rpm_start = 3000,
+	.rpm_max = 6000,
+	.ch = MFT_CH_0,	/* Use MFT id to control fan */
+	.pgood_gpio = -1,
+	.enable_gpio = -1,
 };
-BUILD_ASSERT(ARRAY_SIZE(fans) == FAN_CH_COUNT);
 
 /******************************************************************************/
 /* MFT channels. These are logically separate from pwm_channels. */
@@ -433,6 +431,8 @@ static void board_init(void)
 
 	if (cbi_get_board_version(&version) == EC_SUCCESS)
 		CPRINTS("Board Version: 0x%04x", version);
+
+	fans[FAN_CH_0] = &fan_0;
 
 	/*
 	 * This enables pull-down on F_DIO1 (SPI MISO), and F_DIO0 (SPI MOSI),
