625e84aa352b4acfb422834dfa1b57903185c94a
Daisuke Nojiri <dnojiri@chromium.org>
2018-03-05 09:49:40 -0800

    fan: Allow board to configure fans at run time
    This patch splits struct fan_t into two parts: base configuration
    and RPM configuration. RPMs are expected to be different from
    model to model while a base configuration is most likely shared.
    
    BUG=b:73720175
    BRANCH=none
    TEST=make buildall
    
    Change-Id: Iff17573f110e07e88d097dd848cf91ee98b83176
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/kahlee/board.c
+++ board/kahlee/board.c
@@ -132,18 +132,22 @@ BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
 
 /******************************************************************************/
 /* Physical fans. These are logically separate from pwm_channels. */
-const struct fan_t fans[] = {
-	[FAN_CH_0] = {
-		.flags = FAN_USE_RPM_MODE,
-		.rpm_min = 1000,
-		.rpm_start = 1000,
-		.rpm_max = 4300,
-		.ch = 0,/* Use MFT id to control fan */
-		.pgood_gpio = -1,
-		.enable_gpio = -1,
-	},
+const struct fan_conf fan_conf_0 = {
+	.flags = FAN_USE_RPM_MODE,
+	.ch = 0,	/* Use MFT id to control fan */
+	.pgood_gpio = -1,
+	.enable_gpio = -1,
 };
-BUILD_ASSERT(ARRAY_SIZE(fans) == FAN_CH_COUNT);
+
+const struct fan_rpm fan_rpm_0 = {
+	.rpm_min = 1000,
+	.rpm_start = 1000,
+	.rpm_max = 4300,
+};
+
+struct fan_t fans[FAN_CH_COUNT] = {
+	[FAN_CH_0] = { .conf = &fan_conf_0, .rpm = &fan_rpm_0, },
+};
 
 /* MFT channels. These are logically separate from mft_channels. */
 const struct mft_t mft_channels[] = {
