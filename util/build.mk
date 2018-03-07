b670de859e1d42be3bd4815ebc338941927360fc
CHLin <CHLIN56@nuvoton.com>
2018-03-07 14:33:43 +0800

    util: uartupdatetool (UUT): Add tool to flash firmware by UART
    When the FLPRG# strap pin is set to active low, and npcx7 chip is reset,
    it will enter uut mode. This CL adds the host tool to communicate with
    npcx chip in uut mode to flash ec firmware via UART port.
    
    BRANCH=none
    BUG=none
    TEST=No build errors for make buildall.
    TEST=
    ------------------------------------------------------------------------
    1. Connect the servo connector (J24) on npcx7 EVB to servo board v2 via
    flex cable.
    2. Manually turn the switch SW1.6 to "ON" on npcx7 EVB.
    3. Reset ec by issuing Power-Up or VCC1_RST reset.
    4. Manually turn the switch SW1.6 to "OFF" on npcx7 EVB.
    5. Move npcx7_evb from array BOARDS_NPCX_7M7X_JTAG to BOARDS_NPCX_UUT in
    flash_ec.
    6. "./util/flash_ec --board=npcx7_evb."
    
    Change-Id: I2c588418e809e59f97ef4c3ad7ad13a3fef42f11
    Signed-off-by: Dror Goldstein <dror.goldstein@nuvoton.com>
    Signed-off-by: CHLin <CHLIN56@nuvoton.com>

--- util/build.mk
+++ util/build.mk
@@ -12,6 +12,10 @@ build-util-bin=ec_uartd iteflash
 build-util-art+=util/export_taskinfo.so
 ifeq ($(CHIP),npcx)
 build-util-bin+=ecst
+build-util-bin+=uartupdatetool
+uartupdatetool-objs=uut/main.o uut/cmd.o uut/opr.o uut/l_com_port.o \
+	uut/lib_crc.o
+$(out)/util/uartupdatetool: BUILD_CFLAGS+=-Iutil/
 endif
 # Build on a limited subset of boards to save build time
 ifeq ($(BOARD),meowth_fp)
