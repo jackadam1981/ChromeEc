286c9aef3aa3c1f6dfa5ea5dd6e1b0b371531aac
Aseda Aboagye <aaboagye@google.com>
2018-03-06 23:14:43 -0800

    driver: OPT3001: Add attenuation factor.
    This commit adds the ability for a board to define an attenuation factor
    that will be multiplied against the intermediate value prior to
    converting to lux.  This helps for cases where lux readout may be very
    low and higher precision is desired.
    
    BUG=b:72985601
    BRANCH=stabilize-meowth-10444.B
    TEST=Flash meowth; verify reported readings are significantly higher
    than before in low lux environments.
    
    Change-Id: Ibf46e574fccdde50ceb5f8174f9b4a29e60bfbfe
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/952500
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>
    Tested-by: Aseda Aboagye <aaboagye@chromium.org>
    (cherry picked from commit e700bfc2edeb3e431277c6c87c2c3a9992145fd4)

--- board/zoombini/board.c
+++ board/zoombini/board.c
@@ -197,6 +197,8 @@ static struct opt3001_drv_data_t g_opt3001_data = {
 	.scale = 1,
 	.uscale = 0,
 	.offset = 0,
+	/* Meowth has a dark cover glass. */
+	.attenuation_factor = 100,
 };
 
 struct motion_sensor_t motion_sensors[] = {
