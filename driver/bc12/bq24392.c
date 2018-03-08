a6987efbec08b7d83c219f0856429092fb938eeb
Jett Rink <jettrink@chromium.org>
2018-03-08 13:45:23 -0700

    bc12: add support for active low/high on all gpio signals
    yorp inverts both bc12 signals and the bc12 driver needs to handle
    the inverted logic
    
    BRANCH=none
    BUG=b:74127309
    TEST=none
    
    Change-Id: I6848375fc652251aecb553c3f53d62a5f775bec4
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- driver/bc12/bq24392.c
+++ driver/bc12/bq24392.c
@@ -12,6 +12,7 @@
  * the system will have to charge ramp.
  */
 
+#include "bq24392.h"
 #include "cannonlake.h"
 #include "charge_manager.h"
 #include "chipset.h"
@@ -26,21 +27,35 @@
 #include "usb_pd.h"
 #include "util.h"
 
-struct bq24392_pins {
-	enum gpio_signal chip_enable_l;
-	enum gpio_signal chg_det;
-};
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
 
-static const struct bq24392_pins pin_tbl[] = {
-	{ GPIO_USB_C0_BC12_VBUS_ON_L, GPIO_USB_C0_BC12_CHG_DET },
-#ifdef HAS_TASK_USB_CHG_P1
-	{ GPIO_USB_C1_BC12_VBUS_ON_L, GPIO_USB_C1_BC12_CHG_DET },
-#endif
-#ifdef HAS_TASK_USB_CHG_P2
-	{ GPIO_USB_C2_BC12_VBUS_ON_L, GPIO_USB_C2_BC12_CHG_DET },
-#endif
-};
+/**
+ * Returns true if the charger detect pin is activated.
+ *
+ * @parm drv driver for chip to read the charger detect pin for.
+ * @return 1 if charger detect is activated (high when active high or
+ *	low with active low), otherwise 0.
+ */
+static int is_chg_det_activate(const struct bq24392_config_t * const drv)
+{
+	return !!gpio_get_level(drv->chg_det_pin) ^
+		!!(drv->flags & BQ24392_FLAGS_CHG_DET_ACTIVE_LOW);
+}
 
+/**
+ * Activates the Chip Enable GPIO based on the enabled value.
+ *
+ * @param drv driver for chip that will set chip enable gpio.
+ * @param enable 1 to activate gpio (high for active high and low for active
+ *	low).
+ */
+static void activate_chip_enable(
+	const struct bq24392_config_t * const drv, const int enable)
+{
+	gpio_set_level(
+		drv->chip_enable_pin,
+		!!enable ^ !!(drv->flags & BQ24392_FLAGS_ENABLE_ACTIVE_LOW));
+}
 /**
  * Perform BC1.2 detection and update charge manager.
  *
@@ -48,13 +63,18 @@ static const struct bq24392_pins pin_tbl[] = {
  */
 static void bc12_detect(const int port)
 {
+	const struct bq24392_config_t * const drv = &bq24392_config[port];
 	struct charge_port_info new_chg;
 
+	if (port < 0 || port > bq24392_config_cnt) {
+		CPRINTS("%s(%d) Invalid port!", __func__, port);
+		return;
+	}
+
 	/*
-	 * Enable the IC to begin detection and connect switches if
-	 * necessary.  Note, the value is 0 because the enable is active low.
+	 * Enable the IC to begin detection and connect switches if necessary.
 	 */
-	gpio_set_level(pin_tbl[port].chip_enable_l, 0);
+	activate_chip_enable(drv, 1);
 
 	new_chg.voltage = USB_CHARGER_VOLTAGE_MV;
 #if defined(CONFIG_CHARGE_RAMP_SW) || defined(CONFIG_CHARGE_RAMP_HW)
@@ -66,11 +86,11 @@ static void bc12_detect(const int port)
 
 	/*
 	 * The driver assumes that CHG_AL_N and SW_OPEN are not connected,
-	 * therefore the value of CHG_DET indicates whether the source is NOT a
-	 * low-power standard downstream port (SDP).  The system will have to
+	 * therefore an activated CHG_DET indicates whether the source is NOT a
+	 * low-power standard downstream port (SDP). The system will have to
 	 * ramp the current to determine the limit.
 	 */
-	new_chg.current = gpio_get_level(pin_tbl[port].chg_det) ? 2400 : 500;
+	new_chg.current = is_chg_det_activate(drv) ? 2400 : 500;
 #else
 	/*
 	 * If the board doesn't support charge ramping, then assume the lowest
@@ -90,10 +110,16 @@ static void bc12_detect(const int port)
  */
 static void power_down_ic(const int port)
 {
+	const struct bq24392_config_t * const drv = &bq24392_config[port];
 	struct charge_port_info no_chg = { 0 };
 
+	if (port < 0 || port > bq24392_config_cnt) {
+		CPRINTS("%s(%d) Invalid port!", __func__, port);
+		return;
+	}
+
 	/* Turn off the IC. */
-	gpio_set_level(pin_tbl[port].chip_enable_l, 1);
+	activate_chip_enable(drv, 0);
 
 	/* Let charge manager know there's no more charge available. */
 	charge_manager_update_charge(CHARGE_SUPPLIER_OTHER, port, &no_chg);
@@ -107,6 +133,11 @@ static void power_down_ic(const int port)
 static void detect_or_power_down_ic(const int port)
 {
 	int vbus_present;
+
+	if (port < 0 || port > bq24392_config_cnt) {
+		CPRINTS("%s(%d) Invalid port!", __func__, port);
+		return;
+	}
 
 #ifdef CONFIG_USB_PD_VBUS_DETECT_TCPC
 	vbus_present = tcpm_get_vbus_level(port);
@@ -138,7 +169,8 @@ void usb_charger_task(void *u)
 	const int port = (intptr_t)u;
 	uint32_t evt;
 
-	ASSERT(port >= 0 && port <= 2);
+	ASSERT(bq24392_config_cnt == CONFIG_USB_PD_PORT_COUNT);
+	ASSERT(port >= 0 && port <= bq24392_config_cnt);
 
 	detect_or_power_down_ic(port);
 
