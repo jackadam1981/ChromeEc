edff4f084778cdb9d2a3f10d3b455bc36456e288
Elthan_Huang <elthan_huang@compal.corp-partner.google.com>
2018-03-09 23:14:29 +0800

    Nami: ectool cmd to control keyboard backlight
    To modify ectool cmd control keyboard backlight
    and get keyboard backlight percentage.
    Create LM3509 led driver 32 level brightness
    struct to transfer that ec cmd support 100 level
    brightness adjustment.
    
    BUG=b:74176833
    BRANCH=none
    TEST=Verify ectool pwmsetkblight cmd 0-100 level that
    can be control lm3509 led brightness and get percentage
    via ec console.
    
    Change-Id: Idd894c0469aa9bc5a189038885034eac3e5a5e04
    Signed-off-by: Elthan_Huang <elthan_huang@compal.corp-partner.google.com>

--- driver/led/lm3509.c
+++ driver/led/lm3509.c
@@ -7,6 +7,9 @@
 
 #include "i2c.h"
 #include "lm3509.h"
+#include "host_command.h"
+#include "ec_commands.h"
+#include "util.h"
 
 inline int lm3509_write(uint8_t reg, uint8_t val)
 {
@@ -41,3 +44,70 @@ int lm3509_poweroff(void)
 
 	return ret;
 }
+
+const int lm3509_Brightness[32] = {
+	0, 125, 625, 1000, 1125, 1313,
+	1688, 2063, 2438, 2813, 3125,
+	3750, 4375, 5525, 6250, 7500,
+	8750, 10000, 12500, 15000, 16875,
+	18750, 22500, 26250, 31250, 37500,
+	43750, 52500, 61250, 70000, 87500, 100000};
+
+/*****************************************************************************/
+/* Host commands */
+
+static int lm3509_ec_command_set_KB_light(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_pwm_set_keyboard_backlight *p = args->params;
+	int ret = 0;
+	int val = 0;
+	int i, levelhigh, levellow;
+
+	for (i = 0; i < 32; i++) {
+		levelhigh = lm3509_Brightness[i];
+		levellow = lm3509_Brightness[i-1];
+
+		if ((p->percent) == 0) {
+			val = 0;
+			break;
+		}
+
+		if (levelhigh >= (p->percent)*1000) {
+			if ((levelhigh - (p->percent)*1000) >
+				((p->percent)*1000 - levellow))
+				val = i-1;
+			else
+				val = i;
+			break;
+		}
+	}
+
+	ret |= lm3509_write(LM3509_REG_BMAIN, val);
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_PWM_SET_KEYBOARD_BACKLIGHT,
+		     lm3509_ec_command_set_KB_light,
+		     EC_VER_MASK(0));
+
+static int lm3509_ec_command_get_KB_light(struct host_cmd_handler_args *args)
+{
+	struct ec_response_pwm_get_keyboard_backlight *r = args->response;
+	int rv;
+
+	if (i2c_read8(I2C_PORT_KBLIGHT, LM3509_I2C_ADDR, 0xA0, &rv)
+		== EC_SUCCESS) {
+		r->percent = (lm3509_Brightness[rv&0x1F]+500)/1000;
+		if ((rv&0x1F) > 0)
+			r->enabled = 1;
+	}
+
+	args->response_size = sizeof(*r);
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_PWM_GET_KEYBOARD_BACKLIGHT,
+		     lm3509_ec_command_get_KB_light,
+		     EC_VER_MASK(0));
+
+/*****************************************************************************/
