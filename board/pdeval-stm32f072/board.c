9cb83fa66725f94bc6bbbf2e4addf7938315acec
Dylan Lai <dylai@analogixsemi.com>
2018-03-09 13:54:08 +0800

    TCPM: Add TCPM driver for Analogix anx7447 chip
    Driver implements TCPC for ANX7447 chip. Enable Type C
    port for USB and DP alt mode.
    
    BUG=b:73793947
    BRANCH=NONE
    TEST=tested compiled binary for pdeval-stm32f072 board with this patch.
    Power contract establishment, port role swap, DP alt mode works fine.
    
    Change-Id: Ic11e499fc5fb4aba7732c75e4cb2fee54828c616

--- board/pdeval-stm32f072/board.c
+++ board/pdeval-stm32f072/board.c
@@ -17,6 +17,7 @@
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
 #include "util.h"
+#include "anx7447.h"
 
 void button_event(enum gpio_signal signal);
 
@@ -52,15 +53,12 @@ void board_reset_pd_mcu(void)
 
 /* I2C ports */
 const struct i2c_port_t i2c_ports[] = {
-	{"tcpc", I2C_PORT_TCPC, 100 /* kHz */, GPIO_I2C0_SCL, GPIO_I2C0_SDA}
+	{"tcpc", I2C_PORT_TCPC, 400 /* kHz */, GPIO_I2C0_SCL, GPIO_I2C0_SDA}
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
 const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
-	{I2C_PORT_TCPC, TCPC1_I2C_ADDR, &tcpci_tcpm_drv},
-#if CONFIG_USB_PD_PORT_COUNT >= 2
-	{I2C_PORT_TCPC, TCPC2_I2C_ADDR, &tcpci_tcpm_drv},
-#endif
+	{I2C_PORT_TCPC, TCPC1_I2C_ADDR, &anx7447_tcpm_drv}
 };
 
 uint16_t tcpc_get_alert_status(void)
@@ -69,9 +67,6 @@ uint16_t tcpc_get_alert_status(void)
 
 	if (!gpio_get_level(GPIO_PD_MCU_INT)) {
 		status = PD_STATUS_TCPC_ALERT_0;
-#if CONFIG_USB_PD_PORT_COUNT >= 2
-		status |= PD_STATUS_TCPC_ALERT_1;
-#endif
 	}
 
 	return status;
