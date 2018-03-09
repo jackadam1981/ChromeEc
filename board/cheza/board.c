aa3ac2a594165edb06423afaf9feb0be5d9b8123
Wai-Hong Tam <waihong@google.com>
2018-03-09 15:06:55 -0800

    cheza: Add initial support for Cheza
    Add initial support, like UART, GPIO, I2C, buttons, etc.
    Add some hacks for early bring-up.
    
    BRANCH=none
    BUG=b:74395451
    TEST=BOARD=cheza make
    TEST=Flashed the EC image to cheza. Verified AP power-on, booting
    coreboot/depthcharge with UART, and then booting kernel from USB.
    
    Change-Id: Id057abb491553bbffd3c57a6f48187ac2f2ff9a6
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

---
+++ board/cheza/board.c
@@ -0,0 +1,62 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Cheza board-specific configuration */
+
+#include "adc_chip.h"
+#include "button.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "lid_switch.h"
+#include "power_button.h"
+#include "switch.h"
+
+#include "gpio_list.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+
+/* 8-bit I2C address */
+#define PI3USB9281_I2C_ADDR	0x4a
+#define DA9313_I2C_ADDR		0xd0
+
+/* Wake-up pins for hibernate */
+const enum gpio_signal hibernate_wake_pins[] = {
+	GPIO_LID_OPEN,
+	GPIO_AC_PRESENT,
+	GPIO_POWER_BUTTON_L,
+};
+const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
+
+const struct adc_t adc_channels[] = {};
+
+/* I2C port map */
+const struct i2c_port_t i2c_ports[] = {
+	{"power",   I2C_PORT_POWER,  400, GPIO_I2C0_SCL,  GPIO_I2C0_SDA},
+	{"tcpc0",   I2C_PORT_TCPC0, 1000, GPIO_I2C1_SCL,  GPIO_I2C1_SDA},
+	{"tcpc1",   I2C_PORT_TCPC1, 1000, GPIO_I2C2_SCL,  GPIO_I2C2_SDA},
+	{"eeprom",  I2C_PORT_EEPROM, 400, GPIO_I2C5_SCL,  GPIO_I2C5_SDA},
+	{"sensor",  I2C_PORT_SENSOR, 400, GPIO_I2C7_SCL,  GPIO_I2C7_SDA},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+/* Initialize board. */
+static void board_init(void)
+{
+	/*
+	 * Hack to enable the internal switch in BC1.2
+	 * TODO(waihong): Enable BC1.2 autodetection.
+	 */
+	i2c_write8(I2C_PORT_TCPC0, PI3USB9281_I2C_ADDR, 0x02, 0x1b);
+	i2c_write8(I2C_PORT_TCPC0, PI3USB9281_I2C_ADDR, 0x13, 0x24);
+	i2c_write8(I2C_PORT_TCPC1, PI3USB9281_I2C_ADDR, 0x02, 0x1b);
+	i2c_write8(I2C_PORT_TCPC1, PI3USB9281_I2C_ADDR, 0x13, 0x24);
+
+	/* Disable SwitchCap auto-boot and make EN pin level-trigger */
+	i2c_write8(I2C_PORT_POWER, DA9313_I2C_ADDR, 0x02, 0x34);
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
