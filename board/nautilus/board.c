ce04cc322f16f493b7db892242443f6c114f2c03
Furquan Shaikh <furquan@google.com>
2018-03-09 17:03:46 -0800

    nautilus: Use I2C callbacks to ensure minimum bus free time for battery
    Battery on nautilus requires a minimum bus free time of 5ms between
    I2C transactions. Use I2C_XFER_CALLBACK mechanism to ensure that this
    time is guaranteed before starting a new i2c transaction.
    
    BUG=b:73147310
    BRANCH=None
    TEST=make -j BOARD=nautilus
    
    Change-Id: Ieba168835190b5d1334e413dc4f74e0248bf5a15
    Signed-off-by: Furquan Shaikh <furquan@google.com>

--- board/nautilus/board.c
+++ board/nautilus/board.c
@@ -9,6 +9,7 @@
 #include "adc_chip.h"
 #include "bd99992gw.h"
 #include "board_config.h"
+#include "battery_smart.h"
 #include "button.h"
 #include "charge_manager.h"
 #include "charge_state.h"
@@ -777,4 +778,33 @@ int board_has_working_reset_flags(void)
 
 	/* All other board versions should have working reset flags */
 	return 1;
+}
+
+/*
+ * I2C callbacks to ensure bus free time for battery I2C transactions is at
+ * least 5ms.
+ */
+static timestamp_t battery_last_i2c_time;
+
+void i2c_start_xfer_notify(int port, int slave_addr)
+{
+	unsigned time_delta_us;
+	static const unsigned time_delta_min_us = 5 * MSEC;
+
+	if ((port != I2C_PORT_BATTERY) || (slave_addr != BATTERY_ADDR))
+		return;
+
+	time_delta_us = time_since32(battery_last_i2c_time);
+	if (time_delta_us >= time_delta_min_us)
+		return;
+
+	usleep(time_delta_min_us - time_delta_us);
+}
+
+void i2c_end_xfer_notify(int port, int slave_addr)
+{
+	if ((port != I2C_PORT_BATTERY) || (slave_addr != BATTERY_ADDR))
+		return;
+
+	battery_last_i2c_time = get_time();
 }
