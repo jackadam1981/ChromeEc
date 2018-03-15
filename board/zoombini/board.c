b4adda6e0d028bac07d42de3de11db23e0440d63
Edward Hill <ecgh@chromium.org>
2018-03-15 17:40:55 -0600

    grunt: GPIO changes for board version 2
    Define GPIOs for board versions 0 and 2. Read version in board_init()
    and configure GPIOs correctly.
    
    BUG=b:74538637
    BRANCH=none
    TEST=build + boot grunt (version 0)
    
    Change-Id: I1ff23d5c114cb12e3a32ef069f5e5dff50640d3f
    Signed-off-by: Edward Hill <ecgh@chromium.org>

--- board/zoombini/board.c
+++ board/zoombini/board.c
@@ -264,7 +264,7 @@ const unsigned int ppc_cnt = ARRAY_SIZE(ppc_chips);
 
 #ifdef BOARD_ZOOMBINI
 /* BC 1.2 chip Configuration */
-const struct bq24392_config_t bq24392_config[CONFIG_USB_PD_PORT_COUNT] = {
+struct bq24392_config_t bq24392_config[CONFIG_USB_PD_PORT_COUNT] = {
 	{
 		.chip_enable_pin = GPIO_USB_C0_BC12_VBUS_ON_L,
 		.chg_det_pin = GPIO_USB_C0_BC12_CHG_DET,
