0c1a76013ef9a4352183e7b0933389d9263edd84
Daisuke Nojiri <dnojiri@chromium.org>
2018-03-15 12:12:26 -0700

    EFS: Perform EFS even if flash WP is enabled
    This patch removes flash WP check in the EFS main. It means even
    if the SPI flash is not write protected, it performs EFS.
    
    This allows dogfood devices to boot just like normal devices.
    
    BUG=b:74983574
    BRANCH=none
    TEST=Boot Fizz. Verify EFS works as expected.
    
    Change-Id: I33b39463d6bd16fe0fc398cd49f3c92c76fa5103
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- common/vboot/vboot.c
+++ common/vboot/vboot.c
@@ -193,7 +193,8 @@ void vboot_main(void)
 {
 	CPRINTS("Main");
 
-	if (system_is_in_rw() || !system_is_locked()) {
+	if (system_is_in_rw() || !(flash_get_protect() &
+			EC_FLASH_PROTECT_GPIO_ASSERTED)) {
 		/*
 		 * If we're here, it means PD negotiation was attempted but
 		 * we didn't get enough power to boot AP. This happens on RW
