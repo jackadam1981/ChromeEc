61092736c664543905e218c2bcc9b11cca96df9e
Tino Liu <tino.liu@quanta.corp-partner.google.com>
2018-03-16 15:01:35 +0800

    Coral: Test git
    Tino's practice:
    when temp is >= X degree (C), discharge battery with AC attached.
    when temp is < X-2 degree (C), charge battery with 750 mA.
    
    BUG=none
    BRANCH=coral
    TEST=battery and chgstate command to check charge current
    
    Change-Id: Ic22ea3a0bc7105393a46c0842440a17724ec5e04
    Signed-off-by: Tino Liu <tino.liu@quanta.corp-partner.google.com>

--- board/coral/board.h
+++ board/coral/board.h
@@ -324,6 +324,8 @@ void board_tcpc_init(void);
 /* Sensors without hardware FIFO are in forced mode */
 #define CONFIG_ACCEL_FORCE_MODE_MASK (1 << LID_ACCEL)
 
+extern void tino_set_chg_ctrl_mode(int action);
 #endif /* !__ASSEMBLER__ */
 
 #endif /* __CROS_EC_BOARD_H */
+
