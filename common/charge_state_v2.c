65e9bb099013bea71d21522df539e0f53f9f32b6
Tino Liu <tino.liu@quanta.corp-partner.google.com>
2018-03-16 15:01:35 +0800

    Coral: Test git
    Tino's practice:
    when temp is >= X degree (C), discharge battery with AC attached.
    when temp is < X-2 degree (C), charge battery with 750 mA.
    
    Test log:
    Case 1:
    TINO: now temp is 42
    TINO: action 1, battery discharge!
    battery
      I:         0xfe7e = -386 mA(DISCHG)
    
    chgstate
    chg.*:
            current = -396mA
    batt.*:
    requested_current = 0mA
    
    Case 2:
    TINO: now temp is 32
    TINO: action 0, charge battery with current 750mA!
    battery
      I:         0x0303 = 771 mA(CHG)
    
    chgstate
    chg.*:
    	current = 767mA
    batt.*:
    requested_current = 750mA
    
    BUG=none
    BRANCH=coral
    TEST=battery and chgstate command to check charge current
    
    Change-Id: Ic22ea3a0bc7105393a46c0842440a17724ec5e04
    Signed-off-by: Tino Liu <tino.liu@quanta.corp-partner.google.com>

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -1436,3 +1436,21 @@ int charge_get_charge_state_debug(int param, uint32_t *value)
 	return EC_SUCCESS;
 }
 #endif
+
+void tino_set_chg_ctrl_mode(int action)
+{
+	if (action == 1) {
+		manual_current = 0;
+		manual_voltage = 0;
+		charger_discharge_on_ac(1);
+		CPRINTS("TINO: action 1, battery discharge!");
+	}
+	else if (action == 0) {
+		manual_current = 750;
+		manual_voltage = -1;
+		CPRINTS("TINO: action 0, charge battery with current 750mA!");
+	} else {
+		CPRINTS("TINO: paramenter invalid in tino_set_chg_ctrl_mode!");
+	}
+
+}
