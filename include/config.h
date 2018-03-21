10b793819a4466cdedec07c3d12694f102125ef2
Stefan Adolfsson <sadolfsson@chromium.org>
2018-03-21 12:01:07 +0100

    Fizz: CEC
    CEC-implementation for use in factory testing. This implementation
    is a stub used for the EVT build and should not be merged, just
    built with try-bots.
    
    BRANCH=fizz
    BUG=None
    TEST=on_tv and off_tv works on a connected TV using uservo
    Change-Id: Ib6dfe8e20aa7995742746560cf2069fe5c1b73e3

--- include/config.h
+++ include/config.h
@@ -3145,6 +3145,9 @@
 /* Support Wake-on-Voice */
 #undef CONFIG_WAKE_ON_VOICE
 
+/* Support CEC */
+#undef CONFIG_CEC_SUPPORT
+
 /*
  * Write protect signal is active-high.  If this is defined, there must be a
  * GPIO named GPIO_WP; if not defined, there must be a GPIO names GPIO_WP_L.
