2e2b1326fcbe0386fa33b9a2bf0dc83622793760
Stefan Adolfsson <sadolfsson@chromium.org>
2018-03-21 12:01:07 +0100

    Fizz: CEC
    CEC-implementation for use in factory testing. This implementation
    is a stub used for the EVT build and should not be merged, just
    built with try-bots.
    
    Change-Id: Ib6dfe8e20aa7995742746560cf2069fe5c1b73e3

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -4832,6 +4832,19 @@ struct __ec_align_size1 ec_params_charger_control {
 	uint8_t allow_charging;
 };
 
+/*
+ * Temporary command for testing CEC at factory
+ */
+#define EC_CMD_DISPLAY_POWER 0x0603
+
+struct __ec_align4 ec_params_display_power {
+	uint32_t turn_on_display;
+};
+
+struct __ec_align4 ec_response_display_power {
+	uint32_t result;
+};
+
 /*****************************************************************************/
 /*
  * Reserve a range of host commands for board-specific, experimental, or
