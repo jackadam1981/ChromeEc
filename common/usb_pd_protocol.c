cd556265e5f59b113011453e34cd2c069ce6b6c9
Aseda Aboagye <aaboagye@google.com>
2018-03-23 17:34:11 -0700

    pd: Properly assign data role on reset
    According to PD spec:
    - Data role shall not be reset on soft reset.
    - Data role shall be reset to power-role default on hard reset.
    
    Implement the above. Even if both ports follow spec, it's still possible
    for a data role conflict to occur if, for example, data role swap occurs
    (data role mismatches power role default) followed by a hardware reset
    of one port (such that data role gets reset to power role default).
    Handle such cases by taking error recovery actions.
    
    BUG=b:71333840,chromium:805040
    
    TEST=Connect scarlet to powered Apple accessory, verify scarlet comes up
    in SNK-DFP after soft reset and issuing "reboot" on EC console.  After
    issuing a hard reset, the port comes up in SNK-UFP (which is the
    power-role default).
    
    BRANCH=None
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Change-Id: I65139f277d59a0612f8323d711080f52425ff5e7

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -924,6 +924,33 @@ static void handle_vdm_request(int port, int cnt, uint32_t *payload)
 			port, PD_VDO_VID(payload[0]), payload[0] & 0xFFFF);
 }
 
+static void pd_set_data_role(int port, int role)
+{
+	pd[port].data_role = role;
+#ifdef CONFIG_USB_PD_DUAL_ROLE
+	pd_update_saved_port_flags(port, PD_BBRMFLG_DATA_ROLE, role);
+#endif /* defined(CONFIG_USB_PD_DUAL_ROLE) */
+
+#ifdef CONFIG_USBC_SS_MUX
+#ifdef CONFIG_USBC_SS_MUX_DFP_ONLY
+	/*
+	 * Need to connect SS mux for if new data role is DFP.
+	 * If new data role is UFP, then disconnect the SS mux.
+	 */
+	if (role == PD_ROLE_DFP)
+		usb_mux_set(port, TYPEC_MUX_USB, USB_SWITCH_CONNECT,
+			    pd[port].polarity);
+	else
+		usb_mux_set(port, TYPEC_MUX_NONE, USB_SWITCH_DISCONNECT,
+			    pd[port].polarity);
+#else
+	usb_mux_set(port, TYPEC_MUX_USB, USB_SWITCH_CONNECT,
+		    pd[port].polarity);
+#endif
+#endif
+	pd_update_roles(port);
+}
+
 void pd_execute_hard_reset(int port)
 {
 	if (pd[port].last_state == PD_STATE_HARD_RESET_SEND)
@@ -957,6 +984,8 @@ void pd_execute_hard_reset(int port)
 		pd_power_supply_reset(port);
 	}
 
+	/* Set initial data role (matching power role) */
+	pd_set_data_role(port, pd[port].power_role);
 	if (pd[port].power_role == PD_ROLE_SINK) {
 		/* Clear the input current limit */
 		pd_set_input_current_limit(port, 0, 0);
@@ -1273,31 +1302,6 @@ void pd_request_data_swap(int port)
 	task_wake(PD_PORT_TO_TASK_ID(port));
 }
 
-static void pd_set_data_role(int port, int role)
-{
-	pd[port].data_role = role;
-	pd_execute_data_swap(port, role);
-
-#ifdef CONFIG_USBC_SS_MUX
-#ifdef CONFIG_USBC_SS_MUX_DFP_ONLY
-	/*
-	 * Need to connect SS mux for if new data role is DFP.
-	 * If new data role is UFP, then disconnect the SS mux.
-	 */
-	if (role == PD_ROLE_DFP)
-		usb_mux_set(port, TYPEC_MUX_USB, USB_SWITCH_CONNECT,
-			    pd[port].polarity);
-	else
-		usb_mux_set(port, TYPEC_MUX_NONE, USB_SWITCH_DISCONNECT,
-			    pd[port].polarity);
-#else
-	usb_mux_set(port, TYPEC_MUX_USB, USB_SWITCH_CONNECT,
-		    pd[port].polarity);
-#endif
-#endif
-	pd_update_roles(port);
-}
-
 static void pd_set_power_role(int port, int role)
 {
 	pd[port].power_role = role;
@@ -1316,7 +1320,27 @@ static void handle_ctrl_request(int port, uint16_t head,
 		uint32_t *payload)
 {
 	int type = PD_HEADER_TYPE(head);
+	int data_role = PD_HEADER_DROLE(head);
 	int res;
+
+	/*
+	 * When a data role conflict is detected, USB-C ErrorRecovery
+	 * actions shall be performed, and transitioning to unattached state
+	 * is one such legal action.
+	 */
+	if (pd[port].data_role == data_role) {
+		/*
+		 * If the port doesn't support removing the terminations, just
+		 * go to the unattached state.
+		 */
+		if (tcpm_set_cc(port, TYPEC_CC_OPEN) == EC_SUCCESS)
+			msleep(PD_T_ERROR_RECOVERY);
+		set_state(port,
+			  DUAL_ROLE_IF_ELSE(port,
+					    PD_STATE_SNK_DISCONNECTED,
+					    PD_STATE_SRC_DISCONNECTED));
+		return;
+	}
 
 	switch (type) {
 	case PD_CTRL_GOOD_CRC:
