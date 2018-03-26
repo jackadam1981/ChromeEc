a7511eacd2586f649cc500710aeaed37efe52b94
Daisuke Nojiri <dnojiri@chromium.org>
2018-03-26 13:57:36 -0700

    Nami: Add charge state hook for LED
    BUG=b:73999799
    BRANCH=none
    TEST=Verify the followings:
    Charging               Amber on (S0/S3/S5)
    Charging (full)        White on (S0/S3/S5)
    Discharge in S0        White on
    Discharge in S3/S0ix   Pulsing (rising for 2 sec , falling for 2 sec)
    Discharge in S5        Off
    Battery Error          Amber on 1sec off 1sec
    
    Change-Id: I55b40742135a49f48044f561eb2dbd82b5556d07
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/nami/led.c
+++ board/nami/led.c
@@ -5,12 +5,14 @@
  * Power and battery LED control for Fizz
  */
 
+#include "charge_state.h"
 #include "chipset.h"
 #include "console.h"
 #include "ec_commands.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "led_common.h"
+#include "power.h"
 #include "pwm.h"
 #include "timer.h"
 #include "util.h"
@@ -23,15 +25,19 @@ enum led_color {
 	LED_RED,
 	LED_GREEN,
 	LED_AMBER,
+	LED_WHITE,
+	LED_WARM_WHITE,
 
 	/* Number of colors, not a color itself */
 	LED_COLOR_COUNT
 };
 
+static void led_hook(void);
+
 static int set_color_power(enum led_color color, int duty)
 {
-	int green = 0;
-	int red = 0;
+	int led1 = 0;
+	int led2 = 0;
 
 	if (duty < 0 || 100 < duty)
 		return EC_ERROR_UNKNOWN;
@@ -39,29 +45,22 @@ static int set_color_power(enum led_color color, int duty)
 	switch (color) {
 	case LED_OFF:
 		break;
-	case LED_GREEN:
-		green = 1;
-		break;
-	case LED_RED:
-		red = 1;
-		break;
 	case LED_AMBER:
-		green = 1;
-		red = 1;
+		led2 = 1;
+		break;
+	case LED_WHITE:
+		led1 = 1;
+		break;
+	case LED_WARM_WHITE:
+		led1 = 1;
+		led2 = 1;
 		break;
 	default:
 		return EC_ERROR_UNKNOWN;
 	}
 
-	if (red)
-		pwm_set_duty(PWM_CH_LED_RED, duty);
-	else
-		pwm_set_duty(PWM_CH_LED_RED, 0);
-
-	if (green)
-		pwm_set_duty(PWM_CH_LED_GREEN, duty);
-	else
-		pwm_set_duty(PWM_CH_LED_GREEN, 0);
+	pwm_set_duty(PWM_CH_LED1, led1 ? duty : 0);
+	pwm_set_duty(PWM_CH_LED2, led2 ? duty : 0);
 
 	return EC_SUCCESS;
 }
@@ -121,8 +120,8 @@ static void led_tick(void)
 	static uint8_t pwm_enabled = 0;
 
 	if (!pwm_enabled) {
-		pwm_enable(PWM_CH_LED_RED, 1);
-		pwm_enable(PWM_CH_LED_GREEN, 1);
+		pwm_enable(PWM_CH_LED1, 1);
+		pwm_enable(PWM_CH_LED2, 1);
 		pwm_enabled = 1;
 	}
 	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
@@ -132,58 +131,105 @@ static void led_tick(void)
 	hook_call_deferred(&led_tick_data, next);
 }
 
-static void led_suspend(void)
+static void led_s3(void)
 {
-	CONFIG_TICK(LED_PULSE_TICK_US, LED_GREEN);
+	CONFIG_TICK(LED_PULSE_TICK_US, LED_WHITE);
 	led_tick();
 }
-DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, led_suspend, HOOK_PRIO_DEFAULT);
 
-static void led_shutdown(void)
+static void led_s5(void)
 {
 	hook_call_deferred(&led_tick_data, -1);
 	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
 		set_color(EC_LED_ID_POWER_LED, LED_OFF, 0);
 }
-DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, led_shutdown, HOOK_PRIO_DEFAULT);
 
-static void led_resume(void)
+static void led_s0(void)
 {
 	/* Assume there is no race condition with led_tick, which also
 	 * runs in hook_task. */
 	hook_call_deferred(&led_tick_data, -1);
 	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
-		set_color(EC_LED_ID_POWER_LED, LED_GREEN, 100);
+		set_color(EC_LED_ID_POWER_LED, LED_WHITE, 100);
 }
-DECLARE_HOOK(HOOK_CHIPSET_RESUME, led_resume, HOOK_PRIO_DEFAULT);
 
-void led_alert(int enable)
+static void show_power_state(void)
+{
+	/* TODO: Verify timings. */
+	if (chipset_in_state(CHIPSET_STATE_ON) ||
+	    chipset_in_state(CHIPSET_STATE_SUSPEND | CHIPSET_STATE_ON) ||
+	    chipset_in_state(CHIPSET_STATE_SOFT_OFF | CHIPSET_STATE_SUSPEND))
+		led_s0();
+	else if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND))
+		led_s3();
+	else if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		led_s5();
+	else
+		ccprintf("Unknown power state: %d\n", power_get_state());
+}
+
+static void led_alert(int enable)
 {
 	if (enable) {
 		/* Overwrite the current signal */
-		config_tick(1 * SECOND, 100, LED_RED);
+		config_tick(1 * SECOND, 100, LED_AMBER);
 		led_tick();
 	} else {
-		/* Restore the previous signal */
-		if (chipset_in_state(CHIPSET_STATE_ON))
-			led_resume();
-		else if (chipset_in_state(CHIPSET_STATE_SUSPEND))
-			led_suspend();
-		else if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
-			led_shutdown();
+		led_hook();
 	}
 }
 
-void led_critical(void)
+/*
+ * Nami/Vayne:
+ * One dual color LED:
+ * Charging               Amber on (S0/S3/S5)
+ * Charging (full)        White on (S0/S3/S5)
+ * Discharge in S0        White on
+ * Discharge in S3/S0ix   Pulsing (rising for 2 sec , falling for 2 sec)
+ * Discharge in S5        Off
+ * Battery Error          Amber on 1sec off 1sec
+ */
+static void led_hook(void)
 {
-	hook_call_deferred(&led_tick_data, -1);
-	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
-		set_color(EC_LED_ID_POWER_LED, LED_RED, 100);
+	enum charge_state state = charge_get_state();
+
+	ccprintf("%s:%d\n", __func__, state);
+	if (!led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
+		return;
+
+	switch (state) {
+	case PWR_STATE_DISCHARGE:
+		show_power_state();
+		break;
+	/* When battery is full, plugging in AC triggers _CHARGE but _DISHCARGE
+	 * isn't followed. It looks like we're charging forever. This is most
+	 * likely caused by chgstv2 seeing no state change (full & discharge).
+	 * As a workaround, we treat _CHARGE_NEAR_FULL as _DISCHARGE_FULL. */
+	case PWR_STATE_DISCHARGE_FULL:
+	case PWR_STATE_CHARGE_NEAR_FULL:
+		hook_call_deferred(&led_tick_data, -1);
+		set_color(EC_LED_ID_POWER_LED, LED_WHITE, 100);
+		break;
+	case PWR_STATE_CHARGE:
+		hook_call_deferred(&led_tick_data, -1);
+		set_color(EC_LED_ID_POWER_LED, LED_AMBER, 100);
+		break;
+	case PWR_STATE_ERROR:
+		led_alert(1);
+		break;
+	default:
+		;
+	}
 }
+DECLARE_HOOK(HOOK_BATTERY_SOC_CHANGE, led_hook, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, led_hook, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, led_hook, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, led_hook, HOOK_PRIO_DEFAULT);
 
 static int command_led(int argc, char **argv)
 {
 	enum ec_led_id id = EC_LED_ID_POWER_LED;
+	static int alert = 0;
 
 	if (argc < 2)
 		return EC_ERROR_PARAM_COUNT;
@@ -195,21 +241,26 @@ static int command_led(int argc, char **argv)
 		set_color(id, LED_OFF, 0);
 	} else if (!strcasecmp(argv[1], "red")) {
 		set_color(id, LED_RED, 100);
-	} else if (!strcasecmp(argv[1], "green")) {
-		set_color(id, LED_GREEN, 100);
+	} else if (!strcasecmp(argv[1], "white")) {
+		set_color(id, LED_WHITE, 100);
 	} else if (!strcasecmp(argv[1], "amber")) {
 		set_color(id, LED_AMBER, 100);
 	} else if (!strcasecmp(argv[1], "alert")) {
-		led_alert(1);
-	} else if (!strcasecmp(argv[1], "crit")) {
-		led_critical();
+		alert = 1 - alert;
+		led_alert(alert);
+	} else if (!strcasecmp(argv[1], "s0")) {
+		led_s0();
+	} else if (!strcasecmp(argv[1], "s3")) {
+		led_s3();
+	} else if (!strcasecmp(argv[1], "s5")) {
+		led_s5();
 	} else {
 		return EC_ERROR_PARAM1;
 	}
 	return EC_SUCCESS;
 }
 DECLARE_CONSOLE_COMMAND(led, command_led,
-			"[debug|red|green|amber|off|alert|crit]",
+			"[debug|red|green|amber|off|alert|s0|s3|s5]",
 			"Turn on/off LED.");
 
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
