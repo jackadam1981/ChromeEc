b7b9647433b2100e2371bcb70ec8dab0c20271cf
Philip Chen <philipchen@google.com>
2018-04-03 15:10:01 -0700

    Throttle AP in high battery discharging currnet
    BUG=b:74321682, chromium:838754
    BRANCH=scarlet
    TEST=manually test on scarlet, confirm EC sends
    EC_HOST_EVENT_THROTTLE_START and EC_HOST_EVENT_THROTTLE_STOP
    host events when entering/exiting OCP.
    
    Change-Id: I1e55fc23249596d8afec52a3885655ca9c1f2151
    Signed-off-by: Philip Chen <philipchen@google.com>

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -25,6 +25,7 @@
 #include "printf.h"
 #include "system.h"
 #include "task.h"
+#include "throttle_ap.h"
 #include "timer.h"
 #include "util.h"
 
@@ -40,6 +41,12 @@
 	(CONFIG_BATTERY_CRITICAL_SHUTDOWN_TIMEOUT * SECOND)
 #define PRECHARGE_TIMEOUT_US (PRECHARGE_TIMEOUT * SECOND)
 #define LFCC_EVENT_THRESH 5 /* Full-capacity change reqd for host event */
+
+#if defined(CONFIG_HOSTCMD_EVENTS) && \
+	defined(CONFIG_THROTTLE_AP_ON_BAT_DISCHG_CURRENT)
+#define BAT_OCP_TIMEOUT_US (60 * SECOND)
+static timestamp_t ocp_throttle_start_time;
+#endif
 
 static int charge_request(int voltage, int current);
 
@@ -1326,6 +1333,34 @@ static void set_charge_state(enum charge_state_v2 state)
 	curr.state = state;
 }
 
+static void notify_host_of_over_current(struct batt_params *batt)
+{
+#if defined(CONFIG_THROTTLE_AP_ON_BAT_DISCHG_CURRENT) && \
+	defined(CONFIG_HOSTCMD_EVENTS)
+	if (batt->flags & BATT_FLAG_BAD_CURRENT)
+		return;
+
+	if ((!ocp_throttle_start_time.val &&
+	     (batt->current < -BAT_MAX_DISCHG_CURRENT)) ||
+	    (ocp_throttle_start_time.val &&
+	     (batt->current < -BAT_MAX_DISCHG_CURRENT + BAT_OCP_HYSTERESIS))) {
+		ocp_throttle_start_time = get_time();
+		throttle_ap(THROTTLE_ON, THROTTLE_SOFT,
+			    THROTTLE_SRC_BAT_DISCHG_CURRENT);
+	} else if (ocp_throttle_start_time.val &&
+		   (get_time().val > ocp_throttle_start_time.val +
+		    BAT_OCP_TIMEOUT_US)) {
+		/*
+		 * Clear the timer and notify AP to stop throttling if
+		 * we haven't seen over current for BAT_OCP_TIMEOUT_US.
+		 */
+		ocp_throttle_start_time.val = 0;
+		throttle_ap(THROTTLE_OFF, THROTTLE_SOFT,
+			    THROTTLE_SRC_BAT_DISCHG_CURRENT);
+	}
+#endif
+}
+
 const struct batt_params *charger_current_battery_params(void)
 {
 	return &curr.batt;
@@ -1499,6 +1534,8 @@ void charger_task(void *u)
 				curr.batt.state_of_charge);
 			curr.batt.flags |= BATT_FLAG_BAD_STATE_OF_CHARGE;
 		}
+
+		notify_host_of_over_current(&curr.batt);
 
 		/*
 		 * Now decide what we want to do about it. We'll normally just
@@ -1736,7 +1773,12 @@ wait_for_it:
 #endif
 
 		/* How long to sleep? */
-		if (problems_exist)
+		if (problems_exist
+#if defined(CONFIG_HOSTCMD_EVENTS) && \
+	defined(CONFIG_THROTTLE_AP_ON_BAT_DISCHG_CURRENT)
+		    || ocp_throttle_start_time.val
+#endif
+		   )
 			/* If there are errors, don't wait very long. */
 			sleep_usec = CHARGE_POLL_PERIOD_SHORT;
 		else if (sleep_usec <= 0) {
