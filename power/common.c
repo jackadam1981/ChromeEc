02980fa341055dda9e2a01998e1d334623610ad6
Daisuke Nojiri <dnojiri@chromium.org>
2018-04-11 18:25:56 -0700

    power: Add power_get_state API
    The power_get_state API returns the low-leve chipset power state.
    
    BUG=b:74617335
    BRANCH=none
    TEST=Verify the power state changes on Nami.
    
    Change-Id: I6409905cba0f97a6d6dd2fc574545c7e5d733b57
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- power/common.c
+++ power/common.c
@@ -209,6 +209,10 @@ void power_set_state(enum power_state new_state)
 		want_g3_exit = 0;
 }
 
+enum power_state power_get_state(void)
+{
+	return state;
+}
 #ifdef CONFIG_LPC
 
 /* If host doesn't program s0ix lazy wake mask, use default s0ix mask */
