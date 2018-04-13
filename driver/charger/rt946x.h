6e5178f91729d82250770cd66252fa77c36afacd
Philip Chen <philipchen@google.com>
2018-04-13 15:53:09 -0700

    scarlet: Clamp reported battery SOC when charge terminates
    After we set TE (CL:958295), rt946x terminates charging when
    the charge current is below IEOC in constant-voltage mode.
    
    When AC is plugged and rt946x terminates, we see cases that
    battery SOC falls below BATTERY_LEVEL_NEAR_FULL but rt946x doesn't
    re-enable charging yet, which leads to amber LED. The Chrome OS UI
    might also show battery is not full in this case.
    
    Let's clamp the reported battery SOC in this scenario to
    avoid user confusion.
    
    BUG=b:77870927
    BRANCH=scarlet
    TEST=When AC is on, charge terminates, and BATTERY_LEVEL_NEAR_FULL
    is hit, confirm battery SOC is overwritten.
    
    Change-Id: I559d328aa0d7c5c4cd5bf7178370ea039aa80204
    Signed-off-by: Philip Chen <philipchen@google.com>

--- driver/charger/rt946x.h
+++ driver/charger/rt946x.h
@@ -306,6 +306,12 @@ int rt946x_enable_charger_boost(int en);
 int rt946x_is_vbus_ready(void);
 
 /*
+ * Return 1 if rt946x triggers charge terminatation
+ * due to full charge.
+ */
+int rt946x_is_charge_done(void);
+
+/*
  * Cut off the battery (force BATFET to turn off).
  * Return 0 if it succeeds.
  */
