4ffd7799dc4d222d0554ddcd4c27a739f45ba6d2
Daisuke Nojiri <dnojiri@chromium.org>
2018-04-12 17:07:39 -0700

    Snappy: Don't discharge on AC if system is off
    When an AC charger is plugged in, the EC enable discharge-on-ac:
    stopping current from VBUS and draw current from the battery. This
    feature is introduced to avoid drawing too much current at once from
    an AC adapter. (CL:431138)
    
    We suspect this feature causes the system to choke when the battery is
    deeply discharged.
    
    This patch disables discharge-on-ac if the system is in G3 or S5.
    
    BUG=b:35584495,b:74586187
    BRANCH=reef
    TEST=none
    
    Change-Id: I2ddee48d4bbaa448b3d1f77b5ab079d76a4be8cb
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/snappy/battery.c
+++ board/snappy/battery.c
@@ -131,6 +131,16 @@ static int charger_should_discharge_on_ac(struct charge_state_data *curr)
 		return 0;
 
 	/*
+	 * Do not discharge on AC if the system is not consuming significant
+	 * power. This is any of the following states including transitional
+	 * states between them: G3, S5.
+	 * S3 is not included because we can't distinguish whether we're booting
+	 * up or we're quietly sleeping.
+	 */
+	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		return 0;
+
+	/*
 	 * In light load (<450mA being withdrawn from VSYS) the DCDC of the
 	 * charger operates intermittently i.e. DCDC switches continuously
 	 * and then stops to regulate the output voltage and current, and
