c9eaebd9517584aa5578f99cda03a966f987cc9c
Younghun Kim <young-h.kim@samsung.com>
2018-04-18 22:57:38 +0900

    nautilus: undefine CONFIG_HOSTCMD_DEBUG_MODE
    By default, enable all console messages excepted HC, ACPI event:
    The sensor stack is generating a lot of activity
    
    BUG=b:78220297
    TEST=build ec then test it
         1) make BOARD=nautilus
         2) flash_ec board=nautilus
         3) test sensor with android sensor apps
    
    Change-Id: I59eda50a9b168196355d45b3606abe47687b93dc
    Signed-off-by: Younghun Kim <young-h.kim@samsung.com>

--- board/nautilus/board.h
+++ board/nautilus/board.h
@@ -7,6 +7,14 @@
 
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
+i
+/*
+ * By default, enable all console messages excepted HC, ACPI and event:
+ * The sensor stack is generating a lot of activity.
+ */
+#define CC_DEFAULT     (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
+#undef CONFIG_HOSTCMD_DEBUG_MODE
+#define CONFIG_HOSTCMD_DEBUG_MODE HCDEBUG_OFF
 
 /* EC */
 #define CONFIG_ADC
