d5e1a3f051f6fc6b459d826c53c24f56481083da
Sam Hurst <shurst@google.com>
2018-04-19 10:44:54 -0700

    PD: Leave Vbus ON if Vconn and Vbus Requested is set to 0 in the AMA VDO
    While troubleshooting why a generic $19.99 Multiport (USB, HDMI, Type-C)
    Type-C dongle didn't work on Scarlet, I noticed that Vconn Req and Vbus
    Req were both set to zero in the AMA VDO. For a better user experience,
    default to Vbus ON if both Vconn and Vbus Req are both zero.
    
    BUG=b:78286905
    BRANCH=NONE
    TEST=manual
    Tested the generic dongle with USB-Keyboard, TypeC power adapter,
    and HP monitor.
    
    Change-Id: I170eef1372c3621334de2c457bd4533eea744cc0
    Signed-off-by: Sam Hurst <shurst@chromium.org>

--- common/usb_pd_policy.c
+++ common/usb_pd_policy.c
@@ -294,16 +294,22 @@ static void dfp_consume_identity(int port, int cnt, uint32_t *payload)
 	memcpy(&pe[port].identity, payload + 1, identity_size);
 	switch (ptype) {
 	case IDH_PTYPE_AMA:
-		/* TODO(tbroch) do I disable VBUS here if power contract
-		 * requested it
-		 */
-		if (!PD_VDO_AMA_VBUS_REQ(payload[VDO_I(AMA)]))
-			pd_power_supply_reset(port);
-
+/* Leave vbus ON if the following macro is false */
 #if defined(CONFIG_USB_PD_DUAL_ROLE) && defined(CONFIG_USBC_VCONN_SWAP)
-		/* Adapter is requesting vconn, try to supply it */
-		if (PD_VDO_AMA_VCONN_REQ(payload[VDO_I(AMA)]))
-			pd_try_vconn_src(port);
+		{
+			int vconn_req =
+				PD_VDO_AMA_VCONN_REQ(payload[VDO_I(AMA)]);
+			int vbus_req =
+				PD_VDO_AMA_VBUS_REQ(payload[VDO_I(AMA)]);
+
+			/* Adapter is requesting vconn, try to supply it */
+			if (vconn_req)
+				pd_try_vconn_src(port);
+
+			/* Only disable vbus if vconn was requested */
+			if (vconn_req && !vbus_req)
+				pd_power_supply_reset(port);
+		}
 #endif
 		break;
 	default:
