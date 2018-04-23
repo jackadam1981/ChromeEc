6507dc2efd9f0da4c16d49ce3ecfd0344b4eec1b
Nicolas Boichat <drinkcat@chromium.org>
2018-04-23 09:13:21 +0800

    kukui_tr: emulate eMMC boot mode using SPI controller
    BRANCH=none
    BUG=none
    TEST=make BOARD=kukui_tr -j && \
         ./util/flash_ec --board=kukui_tr --port=9000
    
    Change-Id: I70d9ca8719e4d5fb5c3e64a0a0d8f9be95480cc1
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

---
+++ board/kukui_tr/board.h
@@ -0,0 +1,46 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Kukui eMMC emulator board configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* TODO: Remove CONFIG_SYSTEM_UNLOCKED prior to building MP FW. */
+#define CONFIG_SYSTEM_UNLOCKED
+/* TODO(b:63378217): Define FLASH_PSTATE_LOCKED prior to building MP FW. */
+#undef CONFIG_FLASH_PSTATE_LOCKED
+
+/* 48 MHz SYSCLK clock frequency */
+#define CPU_CLOCK 48000000
+
+/* The UART console is on USART1 (PA9/PA10) */
+#undef CONFIG_UART_CONSOLE
+#define CONFIG_UART_CONSOLE 1
+
+/* Optional features */
+#define CONFIG_LTO
+#define CONFIG_FORCE_CONSOLE_RESUME
+#define CONFIG_STM_HWTIMER32
+
+/* Optional features */
+#define CONFIG_BOARD_PRE_INIT
+#define CONFIG_WATCHDOG_HELP
+
+/* No lid switch */
+#undef CONFIG_LID_SWITCH
+
+#ifndef __ASSEMBLER__
+
+/* Timer selection */
+#define TIM_CLOCK32 2
+#define TIM_WATCHDOG 16
+
+#include "gpio_signal.h"
+
+void emmc_cmd_interrupt(enum gpio_signal signal);
+
+#endif /* !__ASSEMBLER__ */
+#endif /* __CROS_EC_BOARD_H */
---
+++ board/kukui_tr/data2h.c
@@ -0,0 +1,86 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * gcc -Wall data2h.c -o data2h && ./data2h > data.h
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+uint16_t crc16_arg(uint8_t data, uint16_t previous_crc)
+{
+	unsigned int crc = previous_crc << 8;
+	int i;
+
+	crc ^= (data << 16);
+	for (i = 8; i; i--) {
+		if (crc & 0x800000)
+			crc ^= (0x11021 << 7);
+		crc <<= 1;
+	}
+
+	return (uint16_t)(crc >> 8);
+}
+
+int main1(void)
+{
+	uint16_t crc16;
+
+	crc16 = crc16_arg(0x12, 0);
+	crc16 = crc16_arg(0x34, crc16);
+	crc16 = crc16_arg(0x56, crc16);
+	crc16 = crc16_arg(0x78, crc16);
+	crc16 = crc16_arg(0x90, crc16);
+	printf("%04x\n", crc16);
+
+	return 0;
+}
+
+int main(void)
+{
+	uint8_t data[512];
+	int blk, j;
+	uint16_t crc16;
+	size_t cnt;
+
+	FILE *in = fopen("preloader.img", "r");
+
+	if (!in)
+		return 1;
+
+	printf("static const uint8_t raw_data[] __aligned(4) =\n"
+		"{\n"
+		"\t0xff, 0x97, /* Acknowledge boot mode: 1 S=0 010 E=1 11 */\n"
+		"\t0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,\n");
+
+	for (blk = 0;; blk++) {
+		crc16 = 0;
+		cnt = fread(data, 1, 512, in);
+
+		if (cnt == 0) {
+			break;
+		} else if (cnt < 512) {
+			memset(&data[cnt], 0xff, 512-cnt);
+		}
+
+		printf("\t/* Block %d (%ld) */\n", blk, cnt);
+		printf("\t0xff, 0xfe, /* idle, start bit. */");
+		for (j = 0; j < sizeof(data); j++) {
+			printf("%s0x%02x,",
+				(j % 8) == 0 ? "\n\t" : " ", data[j]);
+			crc16 = crc16_arg(data[j], crc16);
+		}
+		printf("\n");
+
+		printf("\t0x%02x, 0x%02x, 0xff, /* CRC, end bit, idle */\n",
+			crc16 >> 8, crc16 & 0xff);
+	}
+
+	printf("\t/* Last block: idle */\n");
+	printf("\t0xff, 0xff, 0xff, 0xff\n");
+	printf("};");
+
+	return 0;
+}
