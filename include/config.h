2482e3e87fbbddce9208ced152c717ee41083880
Jett Rink <jettrink@chromium.org>
2018-04-23 10:42:42 -0600

    mux: add mode for TCPCI mux that is not the TCPC
    We need to use the PS8751 as the USB mux without configuring
    it as the TCPC. Add mode that allows to passing in i2c port
    and address instead using tpcp_config_t values.
    
    BRANCH=none
    BUG=b:78341944
    TEST=build using bip
    
    Change-Id: I45b420ef890dfa8c5e5052864b7a2bb66d8734d6
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- include/config.h
+++ include/config.h
@@ -2886,6 +2886,13 @@
 #undef CONFIG_USB_PD_TCPM_MUX
 
 /*
+ * Use this option if the TCPC/MUX chip is only being used as a mux and the
+ * board tcpc_config_t does not specify the chip. The mux driver must use the
+ * MUX_PORT_AND_ADDR define to pack the port and address together.
+ */
+#undef CONFIG_TCPM_MUX_WITHOUT_TCPC
+
+/*
  * The TCPM must know whether VBUS is present in order to make proper state
  * transitions. In addition, charge_manager must know about VBUS presence in
  * order to make charging decisions. VBUS state can be determined by various
