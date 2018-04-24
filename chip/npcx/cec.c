0f4a3244c3e9f84e19385d5c7adbd2a4b399cd9c
Stefan Adolfsson <sadolfsson@google.com>
2018-04-24 10:33:24 +0200

    npcx: CEC: Add stub implementation of CEC
    Add CEC stub implementation and enable it for Fizz. All
    it does is print a message when the driver is initialized.
    
    Signed-off-by: Stefan Adolfsson <sadolfsson@chromium.org>
    
    BUG=b:76467407
    BRANCH=none
    TEST=Check that "CEC initialized" is printed on the
    console when the EC boots.
    
    Change-Id: I1cf674e664e091354e344e0c08a69bd09f415904

---
+++ chip/npcx/cec.c
@@ -0,0 +1,16 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "console.h"
+#include "hooks.h"
+
+#define CPRINTF(format, args...) cprintf(CC_CEC, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_CEC, format, ## args)
+
+static void cec_init(void)
+{
+	CPRINTS("CEC initialized");
+}
+DECLARE_HOOK(HOOK_INIT, cec_init, HOOK_PRIO_LAST);
