5811a0b14328335bfca535ce980dd978a751ec80
Daisuke Nojiri <dnojiri@chromium.org>
2018-05-01 15:25:36 -0700

    Akali: Add KX022 for lid accelerometer
    This patch adds KX022 as a lid accelerometer.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b:77496502
    BRANCH=none
    TEST=build
    
    Change-Id: I23e8351f457255bdd743b5157053efd8edd6ca4a

--- board/nami/board.c
+++ board/nami/board.c
@@ -20,6 +20,7 @@
 #include "driver/pmic_tps650x30.h"
 #include "driver/accelgyro_bmi160.h"
 #include "driver/accel_bma2x2.h"
+#include "driver/accel_kionix.h"
 #include "driver/als_opt3001.h"
 #include "driver/baro_bmp280.h"
 #include "driver/led/lm3509.h"
@@ -505,7 +506,9 @@ BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
 static struct mutex g_lid_mutex;
 static struct mutex g_base_mutex;
 
+/* Lid accel private data */
 static struct bmi160_drv_data_t g_bmi160_data;
+static struct kionix_accel_data g_kx022_data;
 
 /* BMA255 private data */
 static struct accelgyro_saved_data_t g_bma255_data;
@@ -526,6 +529,35 @@ const matrix_3x3_t lid_standard_ref = {
 	{ FLOAT_TO_FP(1), 0, 0},
 	{ 0, FLOAT_TO_FP(-1), 0},
 	{ 0, 0, FLOAT_TO_FP(-1)}
+};
+
+const struct motion_sensor_t lid_accel_1 = {
+	.name = "Lid Accel",
+	.active_mask = SENSOR_ACTIVE_S0_S3,
+	.chip = MOTIONSENSE_CHIP_KX022,
+	.type = MOTIONSENSE_TYPE_ACCEL,
+	.location = MOTIONSENSE_LOC_LID,
+	.drv = &kionix_accel_drv,
+	.mutex = &g_lid_mutex,
+	.drv_data = &g_kx022_data,
+	.port = I2C_PORT_ACCEL,
+	.addr = KX022_ADDR1,
+	.rot_standard_ref = NULL,
+	.min_frequency = KX022_ACCEL_MIN_FREQ,
+	.max_frequency = KX022_ACCEL_MAX_FREQ,
+	.default_range = 2, /* g, to support tablet mode */
+	.config = {
+		/* EC use accel for angle detection */
+		[SENSOR_CONFIG_EC_S0] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 0,
+		},
+		/* Sensor on in S3 */
+		[SENSOR_CONFIG_EC_S3] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 0,
+		},
+	},
 };
 
 struct motion_sensor_t motion_sensors[] = {
@@ -661,20 +693,17 @@ static void board_chipset_suspend(void)
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_chipset_suspend, HOOK_PRIO_DEFAULT);
 
-static void board_set_motion_sensor_count(void)
+static void setup_motion_sensors(void)
 {
-	/* There are two possible sensor configurations.
-	 * Vayne/Sona/Pantheon are without ALS sensor
-	 * Nami is with ALS sensor
-	 * Use the oem id to different them.
-	 */
-	uint32_t oem_id;
+	uint32_t oem_id = PROJECT_NAMI;
 
-	if (cbi_get_oem_id(&oem_id) == EC_SUCCESS) {
-		if (oem_id == PROJECT_VAYNE || oem_id == PROJECT_SONA
-			|| oem_id == PROJECT_PANTHEON)
-			motion_sensor_count = ARRAY_SIZE(motion_sensors) - 1;
-	}
+	cbi_get_oem_id(&oem_id);
+	if (oem_id != PROJECT_NAMI)
+		/* Only Nami has ALS */
+		motion_sensor_count = ARRAY_SIZE(motion_sensors) - 1;
+	if (oem_id == PROJECT_AKALI)
+		/* Akali uses KX022 */
+		motion_sensors[LID_ACCEL] = lid_accel_1;
 }
 
 /* Initialize board. */
@@ -710,8 +739,7 @@ static void board_init(void)
 	/* Enable Gyro interrupt for BMI160 */
 	gpio_enable_interrupt(GPIO_ACCELGYRO3_INT_L);
 
-	/* Update motion_sensor_count  */
-	board_set_motion_sensor_count();
+	setup_motion_sensors();
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
