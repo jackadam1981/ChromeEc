ee25f3e203df95cd7bb82fb2b9851ca9e90ca3c3
Jett Rink <jettrink@chromium.org>
2018-05-02 15:41:51 -0600

    octopus: move more implementation to baseboard
    Move driver configuration to baseboard in preparation for phaser board
    
    BRANCH=none
    BUG=none
    TEST=yorp still works
    
    Change-Id: Ifeb434d2d4103160acd6eb9f784533d1ae0ae35a
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- baseboard/octopus/baseboard.c
+++ baseboard/octopus/baseboard.c
@@ -5,14 +5,38 @@
 
 /* Octopus family-specific configuration */
 
+#include "adc.h"
+#include "adc_chip.h"
 #include "charge_manager.h"
 #include "charge_state.h"
 #include "common.h"
-#include "console.h"
+#include "driver/accel_kionix.h"
+#include "driver/accelgyro_lsm6dsm.h"
+#include "driver/bc12/bq24392.h"
+#include "driver/charger/bd9995x.h"
+#include "driver/ppc/nx20p3483.h"
+#include "driver/ppc/sn5s330.h"
+#include "driver/tcpm/anx7447.h"
+#include "driver/tcpm/it83xx_pd.h"
 #include "driver/tcpm/ps8xxx.h"
+#include "driver/tcpm/tcpci.h"
+#include "driver/tcpm/tcpm.h"
+#include "driver/usb_mux_it5205.h"
+#include "extpower.h"
 #include "gpio.h"
+#include "hooks.h"
+#include "i2c.h"
 #include "keyboard_scan.h"
+#include "lid_switch.h"
+#include "motion_sense.h"
 #include "power.h"
+#include "power_button.h"
+#include "switch.h"
+#include "system.h"
+#include "tcpci.h"
+#include "temp_sensor.h"
+#include "thermistor.h"
+#include "usb_mux.h"
 #include "usbc_ppc.h"
 #include "util.h"
 
@@ -68,6 +92,279 @@ struct keyboard_scan_config keyscan_config = {
 		0xa4, 0xff, 0xfe, 0x55, 0xfa, 0xca  /* full set */
 	},
 };
+
+/******************************************************************************/
+/* I2C port map configuration */
+const struct i2c_port_t i2c_ports[] = {
+#if defined(OCTOPUS_EC_NPCX796FB)
+	{"battery", I2C_PORT_BATTERY, 100, GPIO_I2C0_SCL, GPIO_I2C0_SDA},
+	{"tcpc0",   I2C_PORT_TCPC0,   100, GPIO_I2C1_SCL, GPIO_I2C1_SDA},
+	{"tcpc1",   I2C_PORT_TCPC1,   100, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
+	{"eeprom",  I2C_PORT_EEPROM,  100, GPIO_I2C3_SCL, GPIO_I2C3_SDA},
+	{"charger", I2C_PORT_CHARGER, 100, GPIO_I2C4_SCL, GPIO_I2C4_SDA},
+	{"sensor",  I2C_PORT_SENSOR,  100, GPIO_I2C7_SCL, GPIO_I2C7_SDA},
+#elif defined(OCTOPUS_EC_ITE8320)
+	{"power",  IT83XX_I2C_CH_A, 100, GPIO_I2C0_SCL, GPIO_I2C0_SDA},
+	{"sensor", IT83XX_I2C_CH_B, 100, GPIO_I2C1_SCL, GPIO_I2C1_SDA},
+	{"usbc0",  IT83XX_I2C_CH_C, 100, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
+	{"usbc1",  IT83XX_I2C_CH_E, 100, GPIO_I2C4_SCL, GPIO_I2C4_SDA},
+	{"eeprom", IT83XX_I2C_CH_F, 100, GPIO_I2C5_SCL, GPIO_I2C5_SDA},
+#endif /* OCTOPUS_EC_ variant */
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+/******************************************************************************/
+/* USB-A Configuration */
+const int usb_port_enable[USB_PORT_COUNT] = {
+#if USB_PORT_COUNT == 1
+	GPIO_EN_USB_A_5V,
+	/* TODO(b/74388692): Add second port control after hardware fix. */
+#else
+	GPIO_EN_USB_A0_5V,
+	GPIO_EN_USB_A1_5V,
+#endif
+};
+
+
+/******************************************************************************/
+/* USB-C Configuration */
+
+#if defined(OCTOPUS_USBC_STANDALONE_TCPCS)
+#define USB_PD_PORT_ANX7447	0
+#define USB_PD_PORT_PS8751	1
+#elif defined(OCTOPUS_USBC_ITE_EC_TCPCS)
+#define USB_PD_PORT_ITE_0	0
+#define USB_PD_PORT_ITE_1	1
+#endif
+
+/* USB-C TPCP Configuration */
+const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
+#if defined(OCTOPUS_USBC_STANDALONE_TCPCS)
+	[USB_PD_PORT_ANX7447] = {
+		.i2c_host_port = I2C_PORT_TCPC0,
+		.i2c_slave_addr = AN7447_TCPC0_I2C_ADDR,
+		.drv = &anx7447_tcpm_drv,
+		.pol = TCPC_ALERT_ACTIVE_LOW,
+	},
+	[USB_PD_PORT_PS8751] = {
+		.i2c_host_port = I2C_PORT_TCPC1,
+		.i2c_slave_addr = PS8751_I2C_ADDR1,
+		.drv = &ps8xxx_tcpm_drv,
+		.pol = TCPC_ALERT_ACTIVE_LOW,
+	},
+#elif defined(OCTOPUS_USBC_ITE_EC_TCPCS)
+	[USB_PD_PORT_ITE_0] = {
+		/* TCPC is embedded within EC so no i2c config needed */
+		.drv = &it83xx_tcpm_drv,
+		.pol = TCPC_ALERT_ACTIVE_LOW,
+	},
+	[USB_PD_PORT_ITE_1] = {
+		/* TCPC is embedded within EC so no i2c config needed */
+		.drv = &it83xx_tcpm_drv,
+		.pol = TCPC_ALERT_ACTIVE_LOW,
+	},
+#endif /* OCTOPUS_USBC_ variants */
+};
+
+/* USB-C MUX Configuration */
+
+/* TODO(crbug.com/826441): Consolidate this logic with other impls */
+#ifdef OCTOPUS_USBC_ITE_EC_TCPCS
+static void board_it83xx_hpd_status(int port, int hpd_lvl, int hpd_irq)
+{
+	enum gpio_signal gpio = port ?
+		GPIO_USB_C1_HPD_1V8_ODL : GPIO_USB_C0_HPD_1V8_ODL;
+
+	/* Invert HPD level since GPIOs are active low. */
+	hpd_lvl = !hpd_lvl;
+
+	gpio_set_level(gpio, hpd_lvl);
+	if (hpd_irq) {
+		gpio_set_level(gpio, 1);
+		msleep(1);
+		gpio_set_level(gpio, hpd_lvl);
+	}
+}
+#endif
+
+struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_COUNT] = {
+#if defined(OCTOPUS_USBC_STANDALONE_TCPCS)
+	[USB_PD_PORT_ANX7447] = {
+		.port_addr = USB_PD_PORT_ANX7447,
+		.driver = &anx7447_usb_mux_driver,
+		.hpd_update = &anx7447_tcpc_update_hpd_status,
+	},
+	[USB_PD_PORT_PS8751] = {
+		.port_addr = USB_PD_PORT_PS8751,
+		.driver = &tcpci_tcpm_usb_mux_driver,
+		.hpd_update = &ps8xxx_tcpc_update_hpd_status,
+	}
+#elif defined(OCTOPUS_USBC_ITE_EC_TCPCS)
+	[USB_PD_PORT_ITE_0] = {
+		/* Driver uses I2C_PORT_USB_MUX as I2C port */
+		.port_addr = IT5205_I2C_ADDR1,
+		.driver = &it5205_usb_mux_driver,
+		.hpd_update = &board_it83xx_hpd_status,
+	},
+	[USB_PD_PORT_ITE_1] = {
+		/* Use PS8751 as mux only */
+		.port_addr = MUX_PORT_AND_ADDR(
+			I2C_PORT_USBC1, PS8751_I2C_ADDR1),
+		.driver = &tcpci_tcpm_usb_mux_driver,
+		.hpd_update = &board_it83xx_hpd_status,
+	}
+#endif /* OCTOPUS_USBC_ variants */
+};
+
+/* USB-C PPC Configuration */
+const struct ppc_config_t ppc_chips[CONFIG_USB_PD_PORT_COUNT] = {
+#if defined(OCTOPUS_USBC_STANDALONE_TCPCS)
+	[USB_PD_PORT_ANX7447] = {
+		.i2c_port = I2C_PORT_TCPC0,
+		.i2c_addr = NX20P3483_ADDR2,
+		.drv = &nx20p3483_drv,
+	},
+	[USB_PD_PORT_PS8751] = {
+		.i2c_port = I2C_PORT_TCPC1,
+		.i2c_addr = NX20P3483_ADDR2,
+		.drv = &nx20p3483_drv,
+		.flags = PPC_CFG_FLAGS_GPIO_CONTROL,
+		.snk_gpio = GPIO_USB_C1_CHARGE_ON,
+		.src_gpio = GPIO_EN_USB_C1_5V_OUT,
+	},
+#elif defined(OCTOPUS_USBC_ITE_EC_TCPCS)
+	[USB_PD_PORT_ITE_0] = {
+		.i2c_port = I2C_PORT_USBC0,
+		.i2c_addr = SN5S330_ADDR0,
+		.drv = &sn5s330_drv
+	},
+	[USB_PD_PORT_ITE_1] = {
+		.i2c_port = I2C_PORT_USBC1,
+		.i2c_addr = SN5S330_ADDR0,
+		.drv = &sn5s330_drv
+	},
+#endif /* OCTOPUS_USBC_ variants */
+};
+const unsigned int ppc_cnt = ARRAY_SIZE(ppc_chips);
+
+/* BC 1.2 chip Configuration */
+const struct bq24392_config_t bq24392_config[CONFIG_USB_PD_PORT_COUNT] = {
+	{
+		.chip_enable_pin = GPIO_USB_C0_BC12_VBUS_ON,
+		.chg_det_pin = GPIO_USB_C0_BC12_CHG_DET_L,
+		.flags = BQ24392_FLAGS_CHG_DET_ACTIVE_LOW,
+	},
+	{
+		.chip_enable_pin = GPIO_USB_C1_BC12_VBUS_ON,
+		.chg_det_pin = GPIO_USB_C1_BC12_CHG_DET_L,
+		.flags = BQ24392_FLAGS_CHG_DET_ACTIVE_LOW,
+	},
+};
+
+/******************************************************************************/
+/* Chipset callbacks/hooks */
+
+/* Called by APL power state machine when transitioning from G3 to S5 */
+void chipset_pre_init_callback(void)
+{
+	/* Enable 5.0V and 3.3V rails, and wait for Power Good */
+	power_5v_enable(task_get_current(), 1);
+
+	gpio_set_level(GPIO_EN_PP3300, 1);
+	while (!gpio_get_level(GPIO_PP5000_PG) ||
+	       !gpio_get_level(GPIO_PP3300_PG))
+		;
+
+	/* Enable PMIC */
+	gpio_set_level(GPIO_PMIC_EN, 1);
+}
+
+/* Called by APL power state machine when transitioning to G3. */
+void chipset_do_shutdown(void)
+{
+	/* Disable PMIC */
+	gpio_set_level(GPIO_PMIC_EN, 0);
+
+	/* Disable 5.0V and 3.3V rails, and wait until they power down. */
+	power_5v_enable(task_get_current(), 0);
+
+	/*
+	 * Shutdown the 3.3V rail and wait for it to go down. We cannot wait
+	 * for the 5V rail since other tasks may be using it.
+	 */
+	gpio_set_level(GPIO_EN_PP3300, 0);
+	while (gpio_get_level(GPIO_PP3300_PG))
+		;
+}
+
+/******************************************************************************/
+/* Power Delivery and charing functions */
+
+#ifdef OCTOPUS_USBC_STANDALONE_TCPCS
+void board_tcpc_init(void)
+{
+	int count = 0;
+	int port;
+
+	/* Wait for disconnected battery to wake up */
+	while (battery_hw_present() == BP_YES &&
+	       battery_is_present() == BP_NO) {
+		usleep(100 * MSEC);
+		/* Give up waiting after 1 second */
+		if (++count > 10) {
+			ccprintf("TCPC_init: 1 second w/no battery\n");
+			break;
+		}
+	}
+
+	/* Only reset TCPC if not sysjump */
+	if (!system_jumped_to_this_image())
+		board_reset_pd_mcu();
+
+	/* Enable PPC interrupts. */
+	gpio_enable_interrupt(GPIO_USB_PD_C0_INT_L);
+	gpio_enable_interrupt(GPIO_USB_PD_C1_INT_L);
+
+	/* Enable TCPC interrupts. */
+	gpio_enable_interrupt(GPIO_USB_C0_PD_INT_ODL);
+	gpio_enable_interrupt(GPIO_USB_C1_PD_INT_ODL);
+
+	/*
+	 * Initialize HPD to low; after sysjump SOC needs to see
+	 * HPD pulse to enable video path
+	 */
+	for (port = 0; port < CONFIG_USB_PD_PORT_COUNT; port++) {
+		const struct usb_mux *mux = &usb_muxes[port];
+
+		mux->hpd_update(port, 0, 0);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, board_tcpc_init, HOOK_PRIO_INIT_I2C + 1);
+#endif /* OCTOPUS_USBC_STANDALONE_TCPCS */
+
+uint16_t tcpc_get_alert_status(void)
+{
+	uint16_t status = 0;
+
+#if defined(OCTOPUS_USBC_STANDALONE_TCPCS)
+	if (!gpio_get_level(GPIO_USB_C0_PD_INT_ODL))
+		status |= PD_STATUS_TCPC_ALERT_0;
+
+	if (!gpio_get_level(GPIO_USB_C1_PD_INT_ODL)) {
+		if (gpio_get_level(GPIO_USB_C1_PD_RST_ODL))
+			status |= PD_STATUS_TCPC_ALERT_1;
+	}
+#elif defined(OCTOPUS_USBC_STANDALONE_TCPCS)
+	/*
+	 * Since C0/C1 TCPC are embedded within EC, we don't need the PDCMD
+	 * tasks.The (embedded) TCPC status since chip driver code will
+	 * handles its own interrupts and forward the correct events to
+	 * the PD_C0 task. See it83xx/intc.c
+	 */
+#endif /* OCTOPUS_USBC_ variant */
+
+	return status;
+}
 
 int board_set_active_charge_port(int port)
 {
