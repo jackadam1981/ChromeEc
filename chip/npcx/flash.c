d7d4709dc113ce723fd57f78720ec7dd8691618b
Patrick Georgi <pgeorgi@google.com>
2018-05-02 19:03:38 +0200

    Shuffle const around
    gcc 8.1 complains about duplicate const, and while some of these really
    are duplicate, others look like they were supposed to tighten the API
    contract so that variables are "const pointer to const data", but didn't
    have that effect.
    
    BUG=b:65441143
    BRANCH=none
    TEST=building Chrome EC as part of upstream coreboot's build with a
    gcc 8.1 compiler now works (better. there are other issues left)
    
    Conflicts:
    	chip/g/idle.c: not in branch.
    
    Change-Id: I6016c5f282516471746f08d5714ea07ebdd10331
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1039812
    Commit-Ready: Patrick Georgi <pgeorgi@chromium.org>
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: Stefan Reinauer <reinauer@google.com>
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
    (cherry picked from commit 85ddb2ce533cb0276aab7780238ec98e1abec2fe)

--- chip/npcx/flash.c
+++ chip/npcx/flash.c
@@ -406,7 +406,7 @@ void flash_burst_write(unsigned int dest_addr, unsigned int bytes,
 }
 
 static int flash_program_bytes(uint32_t offset, uint32_t bytes,
-	const uint8_t const *data)
+	const uint8_t *data)
 {
 	int write_size;
 	int rv;
