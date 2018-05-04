f0176eece535ed635acf12d20e107d429b44a5ba
Zhuohao Lee <zhuohao@chromium.org>
2018-05-04 19:19:32 +0800

    Fizz: revise the active power source info
    This patch is proposed to fulfill the powerd requirement of
    b:78199132#comment39. The requirement are:
    1. If barrel jack is the active power source, the device node
       value should be AC/online=1 and CROS_USB_PD_CHARGER0/online=0.
    2. If USB PD is the active power source, the device node value
       should be AC/online:0 and CROS_USB_PD_CHARGER0/online:1
    
    BUG=b:78199132
    BRANCH=master
    TEST=Test on Fizz
    
    Change-Id: Ib8997423244235c84d3ce2a74833bdc99813fe91
    Signed-off-by: Zhuohao Lee <zhuohao@chromium.org>

--- board/fizz/usb_pd_policy.c
+++ board/fizz/usb_pd_policy.c
@@ -264,6 +264,7 @@ DECLARE_HOOK(HOOK_INIT, board_charge_manager_init,
 int board_set_active_charge_port(int port)
 {
 	const int active_port = charge_manager_get_active_charge_port();
+	uint8_t *memmap_batt_flags = host_get_memmap(EC_MEMMAP_BATT_FLAG);
 
 	if (port < 0 || CHARGE_PORT_COUNT <= port)
 		return EC_ERROR_INVAL;
@@ -283,6 +284,8 @@ int board_set_active_charge_port(int port)
 		gpio_set_level(GPIO_USB_C0_CHARGE_L, 0);
 		gpio_set_level(GPIO_AC_JACK_CHARGE_L, 1);
 		gpio_enable_interrupt(GPIO_ADP_IN_L);
+		/* Clear device node AC/online=0 */
+		*memmap_batt_flags &= ~EC_BATT_FLAG_AC_PRESENT;
 		break;
 	case CHARGE_PORT_BARRELJACK:
 		/* Make sure BJ adapter is sourcing power */
@@ -295,6 +298,8 @@ int board_set_active_charge_port(int port)
 		 * reverse current protection of PU3 but it's intended. */
 		gpio_set_level(GPIO_USB_C0_CHARGE_L, 1);
 		gpio_disable_interrupt(GPIO_ADP_IN_L);
+		/* Set device node AC/online=1 */
+		*memmap_batt_flags |= EC_BATT_FLAG_AC_PRESENT;
 		break;
 	default:
 		return EC_ERROR_INVAL;
