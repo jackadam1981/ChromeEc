d5240ca6140329e1a85e47873846daf520248641
Mulin Chao <MLChao@nuvoton.com>
2018-05-07 15:12:31 +0800

    npcx: system: Add chip generation info in system_get_chip_revision().
    In npcx5 series, there is no other chip generation and npcx's system
    driver fixed the first character of revision array as 'A'. But in npcx7
    series, there are two chip generations and it's better to show chip
    generation information by 'version' console command.
    
    In this CL, we used SRID_CR to distinguish the generation of npcx7
    series. It also adds the support for NPCX787G in system_get_chip_name()
    since it is used on the version 1 of npcx7 evb.
    
    BRANCH=none
    BUG=none
    TEST=No build errors for npcx5/7 series. Verified npcx5m5g, npcx7m6g and
    npcx7m7wb on evbs by 'version' console command.
    
    Change-Id: I7572b5688b4430c6febd21c25f36c3903fb97e27
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

--- chip/npcx/system.c
+++ chip/npcx/system.c
@@ -794,6 +794,8 @@ const char *system_get_chip_name(void)
 	case 0x17:
 		return "NPCX576G";
 #elif defined(CHIP_FAMILY_NPCX7)
+	case 0x1F:
+		return "NPCX787G";
 	case 0x21:
 		return "NPCX796F";
 	case 0x24:
@@ -809,15 +811,37 @@ const char *system_get_chip_name(void)
 
 const char *system_get_chip_revision(void)
 {
-	static char rev[5];
+	static char rev[15] = "Unknown-";
+	char *p = rev;
+	/* Read chip generation from SRID_CR */
+	uint8_t chip_gen = NPCX_SRID_CR;
 	/* Read ROM data for chip revision directly */
 	uint8_t rev_num = *((uint8_t *)CHIP_REV_ADDR);
 
-	*(rev) = 'A';
-	*(rev + 1) = '.';
-	*(rev + 2) = system_to_hex((rev_num & 0xF0) >> 4);
-	*(rev + 3) = system_to_hex(rev_num & 0x0F);
-	*(rev + 4) = '\0';
+	switch (chip_gen) {
+#if defined(CHIP_FAMILY_NPCX5)
+	case 0x05:
+		*(rev) = 'A';
+		break;
+#elif defined(CHIP_FAMILY_NPCX7)
+	case 0x06:
+		*(rev) = 'A';
+		break;
+	case 0x07:
+		*(rev) = 'B';
+		break;
+#endif
+	default:
+		*(p + 8) = system_to_hex((chip_gen & 0xF0) >> 4);
+		*(p + 9) = system_to_hex(chip_gen & 0x0F);
+		p = rev + 9;
+		break;
+	}
+
+	*(p + 1) = '.';
+	*(p + 2) = system_to_hex((rev_num & 0xF0) >> 4);
+	*(p + 3) = system_to_hex(rev_num & 0x0F);
+	*(p + 4) = '\0';
 
 	return rev;
 }
