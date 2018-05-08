85d2d799b77e4d40cd4517b92d20adf07d78df9f
Rong Chang <rongchang@chromium.org>
2018-05-08 17:21:50 -0400

    ish: add inline math functions
    BUG=b:120961468
    BRANCH=none
    TEST=none
    
    Change-Id: I92460b332b24b6d9971ce989c0cd799111cdd239
    Signed-off-by: Rong Chang <rongchang@chromium.org>

--- core/minute-ia/include/math.h
+++ core/minute-ia/include/math.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016 The Chromium OS Authors. All rights reserved.
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -9,6 +9,10 @@
 #define __CROS_EC_MATH_H
 
 #ifdef CONFIG_FPU
+
+#define M_PI            3.14159265358979323846
+#define M_PI_2          1.57079632679489661923
+
 static inline float sqrtf(float v)
 {
 	float root;
@@ -34,6 +38,175 @@ static inline float fabsf(float v)
 	);
 	return root;
 }
-#endif  /* CONFIG_FPU */
 
+static inline float logf(float v)
+{
+	float res;
+
+	asm volatile(
+		"fldln2\n"
+		"fxch\n"
+		"fyl2x\n"
+		: "=t" (res)
+		: "0" (v));
+
+	return res;
+}
+
+static inline float log(float v)
+{
+	return logf(v);
+}
+
+static inline float expf(float v)
+{
+	float res;
+
+	asm volatile(
+		"fldl2e\n"
+		"fmulp\n"
+		"fld %%st(0)\n"
+		"frndint\n"
+		"fsubr %%st(0),%%st(1)\n"
+		"fxch %%st(1)\n"
+		"f2xm1\n"
+		"fld1\n"
+		"faddp\n"
+		"fscale\n"
+		"fstp %%st(1)\n"
+		: "=t" (res)
+		: "0" (v));
+
+	return res;
+}
+
+static inline float powf(float x, float y)
+{
+	float res;
+
+	asm volatile(
+		"fyl2x\n"
+		"fld %%st\n"
+		"frndint\n"
+		"fsub %%st,%%st(1)\n"
+		"fxch\n"
+		"fchs\n"
+		"f2xm1\n"
+		"fld1\n"
+		"faddp\n"
+		"fxch\n"
+		"fld1\n"
+		"fscale\n"
+		"fstp %%st(1)\n"
+		"fmulp\n"
+		: "=t" (res)
+		: "0" (x), "u" (y)
+		: "st(1)");
+
+	return res;
+}
+
+static inline float ceilf(float v)
+{
+	float res;
+	unsigned short control_word, control_word_tmp;
+
+	asm volatile("fnstcw %0" : "=m" (control_word));
+	control_word_tmp = (control_word | 0x0800) & 0xfbff;
+	asm volatile(
+		"fld %3\n"
+		"fldcw %1\n"
+		"frndint\n"
+		"fldcw %2"
+		: "=t" (res)
+		: "m" (control_word_tmp), "m"(control_word), "m" (v));
+
+	return res;
+}
+
+static inline float atan2f(float x, float y)
+{
+	float res;
+
+	asm volatile("fpatan" : "=t" (res) : "0" (x), "u" (y) : "st(1)");
+
+	return res;
+}
+
+static inline float atanf(float v)
+{
+	float res;
+
+	asm volatile(
+		"fld1\n"
+		"fpatan\n"
+		: "=t" (res)
+		: "0" (v));
+
+	return res;
+}
+
+static inline float sinf(float v)
+{
+	float res;
+
+	asm volatile("fsin" : "=t" (res) : "0" (v));
+
+	return res;
+}
+
+static inline float cosf(float v)
+{
+	float res;
+
+	asm volatile("fcos" : "=t" (res) : "0" (v));
+
+	return res;
+}
+
+static inline float acosf(float v)
+{
+	return atan2f(sqrtf(1.0 - v * v), v);
+}
+
+#define CONDITION_MASK_C0   0x00000100
+#define CONDITION_MASK_C1   0x00000200
+#define CONDITION_MASK_C2   0x00000400
+#define CONDITION_MASK_C3   0x00004000
+
+static inline float __isnanf(float v)
+{
+	unsigned int stat;
+
+	asm volatile(
+		"fld %1\n"
+		"fxam\n"
+		"fnstsw %0\n"
+		"faddp\n"
+		: "=m" (stat)
+		: "m" (v));
+
+	return (float)((stat & CONDITION_MASK_C0)
+			&& !(stat & CONDITION_MASK_C2)
+			&& !(stat & CONDITION_MASK_C3));
+}
+
+static inline float __isinff(float v)
+{
+	unsigned int stat;
+
+	asm volatile(
+		"fld %1\n"
+		"fxam\n"
+		"fnstsw %0\n"
+		"faddp\n"
+		: "=m" (stat)
+		: "m" (v));
+
+	return (float)((stat & CONDITION_MASK_C0)
+			&& (stat & CONDITION_MASK_C2)
+			&& !(stat & CONDITION_MASK_C3));
+}
+
+#endif  /* CONFIG_FPU */
 #endif  /* __CROS_EC_MATH_H */
