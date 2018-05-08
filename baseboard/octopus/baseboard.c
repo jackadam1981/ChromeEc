753ee724e63cf294ad544dcdd1b36813096e2c1e
Jett Rink <jettrink@chromium.org>
2018-05-08 14:14:32 -0600

    yorp: add keyboard backlight control
    Enable PWM control of backlight in EC for yorp and phaser. Proto build
    of bip will not have backlight control in EC.
    
    BRANCH=none
    BUG=b:79422226
    TEST=none (no hardware to test with)
    
    Change-Id: Ib6ed4af4de3145b112ed43b4ca1ec9f931f3875f
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- baseboard/octopus/baseboard.c
+++ baseboard/octopus/baseboard.c
@@ -176,6 +176,20 @@ void chipset_do_shutdown(void)
 		;
 }
 
+/* Call from S3->S0 transition */
+static void board_kblight_init(void)
+{
+	gpio_set_level(GPIO_KB_BL_PWR_EN, 1);
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_kblight_init, HOOK_PRIO_DEFAULT);
+
+/* Call from S0->(S0ix|S3) transition */
+static void board_kblight_stop(void)
+{
+	gpio_set_level(GPIO_KB_BL_PWR_EN, 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_kblight_stop, HOOK_PRIO_DEFAULT);
+
 /******************************************************************************/
 /* Charger/PD functions */
 
