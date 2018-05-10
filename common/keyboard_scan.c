67dea8c65cb252aaa0de659d1bd152e906c8884a
paris_yeh <pyeh@google.com>
2018-05-10 15:38:05 +0800

    HACK: coral: Change VOL_UP/VOL_DN for Keyboard_ID#0/ID#1 as experimental
    Removed volume related settings and added CONFIG_KEYBOARD_LANGUAGE_ID
    
    TEST="make -j BOARD=coral" passed
         Keyboard results were caputred from the EC console
    
    Change-Id: I74f038a1f393332b951a04c6efc272b787fd0847

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -292,8 +292,12 @@ static void read_matrix_id(uint8_t *id)
 	int c;
 
 	for (c = 0; c < KEYBOARD_IDS; c++) {
+	/*
+	 * Reworked VOL_UP(KSO14/GPIO82) w/ ID #0 and
+	 * VOL_DOWN(KSO15/GPIO83) w/ ID #1.
+	 */
 		/* Select the ID pin, then wait a bit for it to settle */
-		keyboard_raw_drive_column(KEYBOARD_COLS + c);
+		keyboard_raw_drive_column(KEYBOARD_COLS + c + 1);
 		udelay(keyscan_config.output_settle_us);
 
 		/* Read and then the row state */
