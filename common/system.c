00d6c272ac1564210ba9cbe41226c15da1530488
Mary Ruthven <mruthven@google.com>
2018-05-16 15:10:30 -0700

    cr50: include sleepmask in all images
    sleepmask is really useful for debugging sleep issues. Add a read only
    version of sleepmask to non-DBG images. It will only be accessible once
    the console is unlocked.
    
    BUG=none
    BRANCH=cr50
    TEST=make sure sleepmask can be modified in DBG images and can only be
    read in prod images.
    
    Change-Id: I31ef966f6302d4a7602a014cb08c9b972d13f41e
    Signed-off-by: Mary Ruthven <mruthven@google.com>

--- common/system.c
+++ common/system.c
@@ -1224,6 +1224,7 @@ DECLARE_SAFE_CONSOLE_COMMAND(syslock, command_system_lock,
  */
 static int command_sleepmask(int argc, char **argv)
 {
+#ifdef CONFIG_CMD_SLEEPMASK_SET
 	int v;
 
 	if (argc >= 2) {
@@ -1242,7 +1243,7 @@ static int command_sleepmask(int argc, char **argv)
 			sleep_mask = v;
 		}
 	}
-
+#endif
 	ccprintf("sleep mask: %08x\n", sleep_mask);
 
 	return EC_SUCCESS;
