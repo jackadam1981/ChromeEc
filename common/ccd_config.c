f43528e803c332278aea77d1406b75a91848f93a
Mary Ruthven <mruthven@google.com>
2018-05-21 13:03:44 -0700

    cr50: add command for factory reset
    The factory reset command can open ccd if write protect is removed and
    ccd open hasn't been restricted. Right now we check FWMP and the ccd
    password before allowing factory mode enable, so factory reset cannot be
    used to get around anything that disables ccd.
    
    BUG=b:77543904
    BRANCH=cr50
    TEST=Try enabling factory mode using factory reset. Verify setting write
    protect, setting the FWMP disable ccd bit, or setting a ccd password
    prevents factory reset from enabling ccd.
    
    Change-Id: I6e203bf6068250f009881aa95c13bc56cb2aa9e7
    Signed-off-by: Mary Ruthven <mruthven@google.com>

--- common/ccd_config.c
+++ common/ccd_config.c
@@ -204,6 +204,18 @@ static int raw_has_password(void)
 }
 
 /**
+ * Read the ccd has password setting. This checks the raw password. It is
+ * possible that a pending ccd change would set or clear the password, but we
+ * don't think this is a big issue or risk for now.
+ *
+ * @return 1 if password is set, 0 if it's not
+ */
+int ccd_has_password(void)
+{
+	return raw_has_password();
+}
+
+/**
  * Calculate the expected digest for a password.
  *
  * Uses the unique device ID and the salt from the config.
