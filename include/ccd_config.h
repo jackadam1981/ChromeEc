4c1c908a21895445b00b857cf6efd96f410e4f37
Mary Ruthven <mruthven@google.com>
2018-05-22 15:55:23 -0700

    cr50: rename rma mode
    We're doing a bit of renaming to make factory mode stuff clearer.
    Factory reset and RMA reset can be used to enter factory mode. Factory
    mode can be disabled with the disable_factory vendor command.
    
    Factory mode means all ccd capabilities are set to Always and WP is
    permanently disabled. On factory mode is disabled all capabilities are
    reset to Default and WP is reset to follow battery presence.
    
    This uses 104 bytes
    
    BUG=none
    BRANCH=cr50
    TEST=verify rma reset will enable factory mode.
    
    Change-Id: I21c6f7b4341e3a18e213e438bbd17c67739b85fa
    Signed-off-by: Mary Ruthven <mruthven@google.com>

--- include/ccd_config.h
+++ include/ccd_config.h
@@ -255,7 +255,7 @@ enum ccd_reset_config_flags {
 	CCD_RESET_UNLOCKED_ONLY = (1 << 1),
 
 	/* Use RMA/factory defaults */
-	CCD_RESET_RMA = (1 << 2)
+	CCD_RESET_FACTORY = (1 << 2)
 };
 
 /**
@@ -272,4 +272,9 @@ int ccd_reset_config(unsigned int flags);
  */
 void ccd_tpm_reset_callback(void);
 
+/**
+ * Enter CCD factory mode. This will clear the TPM and do a hard reboot after
+ * updating the ccd config.
+ */
+void enable_ccd_factory_mode(void);
 #endif /* __CROS_EC_CCD_CONFIG_H */
