8bad80639b5e333375aa687b92cdc3f8c9b1ec37
paris_yeh <pyeh@google.com>
2018-05-23 06:25:37 +0800

    keyboard_scan: Add option to support keyboards with language ID
    ID pins are considered additional KSOs while keycode scanning works
    for the existing KSI0 ~ KSI7. While diriving ID pins, the state of
    interconnection between ID pins and KSI pins could be used for
    identifiers to tell keyboard itself. (e.g. US, Japan,and UK keyboard)
    
    BRANCH=master
    BUG=b:80168723
    TEST=make -j buildall
    
    Change-Id: I1d6e647df74c50d60bc1264c045b2587d0bf23d8
    Signed-off-by: paris_yeh <pyeh@google.com>

--- include/config.h
+++ include/config.h
@@ -1953,6 +1953,10 @@
  */
 #undef CONFIG_KEYBOARD_KSO_HIGH_DRIVE
 
+/*
+ * Add support for keyboards with language ID pins
+ */
+#undef CONFIG_KEYBOARD_LANGUAGE_ID
 /*****************************************************************************/
 
 /* Support common LED interface */
