bb2757aa2cc0381e9d6cb36593ea3f5990e6b4f1
Furquan Shaikh <furquan@google.com>
2018-05-24 05:16:40 -0700

    it83xx: Add a config option for enabling mouse LDN
    Not all boards using ITE83XX use mouse LDN. This change adds a config
    option to allow boards to explicity enable this device. Currently,
    this device is enabled only for glkrvp_ite and it83xx_evb. It is
    disabled for reef_ite and bip.
    
    Change-Id: I7149fd0cb35cc9f49f2b7b80f6c2deefe2edda55
    Signed-off-by: Furquan Shaikh <furquan@google.com>

--- include/config.h
+++ include/config.h
@@ -1817,6 +1817,9 @@
 /* Number of IRQs supported on the EC chip */
 #undef CONFIG_IRQ_COUNT
 
+/* Enable LDN for KBC mouse */
+#undef CONFIG_IT83XX_ENABLE_MOUSE_LDN
+
 /*
  * The IT8320 supports e-flash clock up to 48 MHz (IT8390 maximum is 32 MHz).
  * Enable it if we want better performance of fetching instruction from e-flash.
@@ -1827,6 +1830,7 @@
 
 /* To define it, if I2C channel C and PECI used at the same time. */
 #undef CONFIG_IT83XX_SMCLK2_ON_GPC7
+
 
 /*****************************************************************************/
 /* Keyboard config */
