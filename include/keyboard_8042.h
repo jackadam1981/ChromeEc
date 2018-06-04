daea290da53248dec54453240054b441f8e97643
david huang <david.huang@quantatw.com>
2018-06-04 13:12:30 +0800

    ec: Allow setting keyboard mapping dynamically
    A new CONFIG_KEYBOARD_DYNAMIC_MAPPING allows AP firmware to select
    different keyboard mappings. This is different from SCANCODE_MUTABLE and
    is very helpful if the system has to boot in different mode and map
    keyboards in different ways, for example only enabling Fn keys in AP
    legacy mode.
    
    SCANCODE_MUTABLE is more for "difference that won't change once EC
    boots" while DYNAMIC_MAPPING is for "difference changes per each AP
    reboot".
    
    In AP firmware, to select mapping it should invoke the ACPI
     EC_ACPI_MEM_KB_MAPPING_TYPE (0x0a) with expected mapping type.
    
    A board that wants to enable dynamic mapping should provide
    implementations of following functions:
    
     void keyboard_board_mapping_changed(int mapping);
    
    And when using 8042 protocol, the new mapping can be implemented by
    providing a board specific translation function:
     uint16_t keyboard_board_translate(...);
    
    BUG=b:72200093
    BRANCH=eve
    TEST=Builds and boot properly.
    
    Change-Id: If512931066aab6efa1467b038de46c6982af8bec
    Signed-off-by: David Huang <David.Huang@quantatw.com>

--- include/keyboard_8042.h
+++ include/keyboard_8042.h
@@ -8,6 +8,7 @@
 #ifndef __CROS_EC_KEYBOARD_8042_H
 #define __CROS_EC_KEYBOARD_8042_H
 
+#include <stddef.h>
 #include "common.h"
 #include "button.h"
 
@@ -27,5 +28,27 @@ void button_state_changed(enum keyboard_button_type button, int is_pressed);
  * @param is_cmd        Is byte command (!=0) or data (0)
  */
 void keyboard_host_write(int data, int is_cmd);
+
+
+/* Utilities for scan code and make code. */
+
+enum scancode_set_list {
+	SCANCODE_GET_SET = 0,
+	SCANCODE_SET_1,
+	SCANCODE_SET_2,
+	SCANCODE_SET_3,
+	SCANCODE_MAX = SCANCODE_SET_3,
+};
+
+/**
+ * Returns a board-specific translated make code.
+ *
+ * @param make_code	8042 make code
+ * @param pressed	Whether the key was pressed
+ * @param code_set	8042 scan code set
+ */
+uint16_t keyboard_board_translate(
+		uint16_t make_code, int8_t pressed,
+		enum scancode_set_list code_set);
 
 #endif  /* __CROS_EC_KEYBOARD_8042_H */
