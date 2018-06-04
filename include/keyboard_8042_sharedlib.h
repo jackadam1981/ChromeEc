7d359868b53b6f542aad42f70e725db34f2ae294
david huang <david.huang@quantatw.com>
2018-06-04 13:12:30 +0800

    eve-campfire: Setting different keyboard mapping
    Setting different keyboard mapping for different function.
    
    BUG=b:72200093
    BRANCH=eve
    TEST=Boot to alt-os and check keyboard mapping.
    
    Change-Id: If512931066aab6efa1467b038de46c6982af8bec
    Signed-off-by: David Huang <David.Huang@quantatw.com>

--- include/keyboard_8042_sharedlib.h
+++ include/keyboard_8042_sharedlib.h
@@ -27,6 +27,24 @@ extern const uint16_t scancode_set1[KEYBOARD_ROWS][KEYBOARD_COLS];
 extern const uint16_t scancode_set2[KEYBOARD_ROWS][KEYBOARD_COLS];
 #endif
 
+#ifdef CONFIG_KEYBOARD_MAPPING
+#ifdef CONFIG_KEYBOARD_SCANCODE_MUTABLE
+extern uint16_t alt_os_scancode_set1[KEYBOARD_ROWS][KEYBOARD_COLS];
+extern uint16_t alt_os_scancode_set2[KEYBOARD_ROWS][KEYBOARD_COLS];
+#else
+extern const uint16_t alt_os_scancode_set1[KEYBOARD_ROWS][KEYBOARD_COLS];
+extern const uint16_t alt_os_scancode_set2[KEYBOARD_ROWS][KEYBOARD_COLS];
+#endif
+
+#ifdef CONFIG_KEYBOARD_SCANCODE_MUTABLE
+extern uint16_t fn_scancode_set1[FN_KEYS_SETS][FN_KEY_COMBINE_SETS];
+extern uint16_t fn_scancode_set2[FN_KEYS_SETS][FN_KEY_COMBINE_SETS];
+#else
+extern const uint16_t fn_scancode_set1[FN_KEYS_SETS][FN_KEY_COMBINE_SETS];
+extern const uint16_t fn_scancode_set2[FN_KEYS_SETS][FN_KEY_COMBINE_SETS];
+#endif
+#endif
+
 /* Button scancodes (Power, Volume Down, Volume Up, etc.) */
 extern const struct button_8042_t buttons_8042[KEYBOARD_BUTTON_COUNT];
 
