e24b4144bb443780f2448c921ab1bb7f0852718f
Mulin Chao <mlchao@nuvoton.com>
2018-06-07 00:24:18 -0700

    npcx: adc: only enable ADC during conversion for power consumption.
    For better power consumption in npcx series ec, this CL only turns on
    ADC module before starting conversion and turns it off after the
    conversion is done.
    
    If ec enters deep sleep when ADC conversion is ongoing, the conveting
    process is suspended until next time ec wakes up from the deep sleep.
    Considering the frequency of ADC's source clock is up to 15MHz, it's
    more efficient to forbid ec enter deep sleep when the conversion
    is proceeding. (Each conversion time of ADC channel is ~200us based on
    this condition.) Forbidding ec enter deep sleep should have no harm.
    
    BRANCH=none
    BUG=none
    TEST=No build errors for npcx7 series. The current of AVCC is reduced
    from 0.4mA to ~0.01mA on yorp if this patch is applied.
    
    Change-Id: Ie9226e942eeefaadbca7c17e45f0b9ee9a2364e9
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

--- include/system.h
+++ include/system.h
@@ -426,6 +426,7 @@ enum {
 	SLEEP_MASK_PWM        = (1 << 10), /* PWM output is enabled */
 	SLEEP_MASK_PHYSICAL_PRESENCE  = (1 << 11), /* Physical presence
 						    * detection ongoing */
+	SLEEP_MASK_ADC        = (1 << 12), /* ADC conversion ongoing */
 	SLEEP_MASK_FORCE_NO_DSLEEP    = (1 << 15), /* Force disable. */
 
 
