f8d890eea4ba693ec49e07536474082fb48ff050
Wei-Han Chen <stimim@google.com>
2018-06-07 18:05:59 +0800

    driver/touchpad_st.c: support heat map
    BRANCH=none
    BUG=b:70482333
    TEST=`make BOARD=whiskers` works w/ & w/o CONFIG_USB_ISOCHRONOUS
    Signed-off-by: Wei-Han Chen <stimim@chromium.org>
    
    Change-Id: Ia05e778d9795a09ea6edceddd839992859d75050

--- chip/stm32/usb_isochronous.h
+++ chip/stm32/usb_isochronous.h
@@ -91,7 +91,8 @@ struct usb_isochronous_config {
 				    ENDPOINT,				\
 				    TX_SIZE,				\
 				    TASK_ID,			\
-				    SET_INTERFACE)			\
+				    SET_INTERFACE,			\
+				    NUM_EXTRA_ENDPOINTS)		\
 	BUILD_ASSERT(TX_SIZE > 0);					\
 	BUILD_ASSERT((TX_SIZE <   64 && (TX_SIZE & 0x01) == 0) ||	\
 		     (TX_SIZE < 1024 && (TX_SIZE & 0x1f) == 0));	\
@@ -126,7 +127,7 @@ struct usb_isochronous_config {
 		.bDescriptorType    = USB_DT_INTERFACE,			\
 		.bInterfaceNumber   = INTERFACE,			\
 		.bAlternateSetting  = 1,				\
-		.bNumEndpoints      = 1,				\
+		.bNumEndpoints      = 1 + NUM_EXTRA_ENDPOINTS,		\
 		.bInterfaceClass    = INTERFACE_CLASS,			\
 		.bInterfaceSubClass = INTERFACE_SUBCLASS,		\
 		.bInterfaceProtocol = INTERFACE_PROTOCOL,		\
