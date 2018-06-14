f4bcaf73d33196d9df344891156d152a799095c4
Paul Ma <magf@bitland.corp-partner.google.com>
2018-06-14 16:58:10 +0800

    phaser: add motion sensor config and tablet mode support.
    BRANCH=none
    BUG=b:110013316
    TEST=boot phaser board, sensor can be recognized sometimes,
         but lid sensor(lis3dh or lis3de) interupt never fired.
         base sensor works well.
    
    Change-Id: I5ab5c5988fc5bf79f8a1e653fff453c8f4544b75

--- driver/accel_lis2dh.c
+++ driver/accel_lis2dh.c
@@ -92,7 +92,7 @@ static int get_range(const struct motion_sensor_t *s)
 {
 	struct stprivate_data *data = s->drv_data;
 
-	return LIS2DH_GAIN_TO_FS(data->base.range);
+	return data->base.range;
 }
 
 static int set_data_rate(const struct motion_sensor_t *s, int rate, int rnd)
@@ -169,7 +169,7 @@ static int load_fifo(struct motion_sensor_t *s)
 	/* Try to Empty FIFO */
 	do {
 		/* Read samples number in status register */
-		ret = raw_read8(s->port, s->addr, LIS2DH_FIFO_SRC_REG, &tmp);
+		ret = st_raw_read8(s->port, s->addr, LIS2DH_FIFO_SRC_REG, &tmp);
 		if (ret != EC_SUCCESS)
 			return ret;
 
@@ -223,7 +223,7 @@ static int config_interrupt(const struct motion_sensor_t *s)
 	/* configure FIFO watermark level */
 	ret = st_write_data_with_mask(s, LIS2DH_FIFO_CTRL_REG,
 				   LIS2DH_FIFO_THR_MASK,
-				   CONFIG_ACCEL_FIFO_THRES);
+				   0x0f); //(uint8_t)CONFIG_ACCEL_FIFO_THRES);
 	if (ret != EC_SUCCESS)
 		return ret;
 	/* enable interrupt on FIFO watermask and route to int1 */
@@ -239,6 +239,7 @@ static int config_interrupt(const struct motion_sensor_t *s)
  */
 void lis2dh_interrupt(enum gpio_signal signal)
 {
+	CPRINTF("lis int");
 	task_set_event(TASK_ID_MOTIONSENSE,
 		       CONFIG_ACCEL_LIS2DH_INT_EVENT, 0);
 }
@@ -249,6 +250,7 @@ void lis2dh_interrupt(enum gpio_signal signal)
 static int irq_handler(struct motion_sensor_t *s, uint32_t *event)
 {
 	int interrupt;
+	int ret;
 
 	if ((s->type != MOTIONSENSE_TYPE_ACCEL) ||
 	    (!(*event & CONFIG_ACCEL_LIS2DH_INT_EVENT))) {
@@ -256,7 +258,7 @@ static int irq_handler(struct motion_sensor_t *s, uint32_t *event)
 	}
 
 	/* read interrupt status register to reset source */
-	raw_read8(s->port, s->addr, LIS2DH_INT1_SRC_REG, &interrupt);
+	st_raw_read8(s->port, s->addr, LIS2DH_INT1_SRC_REG, &interrupt);
 
 #ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
 	*event |= CONFIG_GESTURE_TAP_EVENT;
@@ -268,7 +270,9 @@ static int irq_handler(struct motion_sensor_t *s, uint32_t *event)
 	 * No need to read the FIFO here, motion sense task is
 	 * doing it on every interrupt.
 	 */
-	return EC_SUCCESS;
+	ret = load_fifo(s);
+
+	return ret;
 }
 #endif  /* CONFIG_ACCEL_INTERRUPTS */
 
@@ -276,7 +280,7 @@ static int is_data_ready(const struct motion_sensor_t *s, int *ready)
 {
 	int ret, tmp;
 
-	ret = raw_read8(s->port, s->addr, LIS2DH_STATUS_REG, &tmp);
+	ret = st_raw_read8(s->port, s->addr, LIS2DH_STATUS_REG, &tmp);
 	if (ret != EC_SUCCESS) {
 		CPRINTF("[%T %s type:0x%X RS Error]", s->name, s->type);
 		return ret;
@@ -290,8 +294,7 @@ static int is_data_ready(const struct motion_sensor_t *s, int *ready)
 static int read(const struct motion_sensor_t *s, vector_3_t v)
 {
 	uint8_t raw[OUT_XYZ_SIZE];
-	int ret, i, tmp = 0;
-	struct stprivate_data *data = s->drv_data;
+	int ret, tmp = 0;
 
 	ret = is_data_ready(s, &tmp);
 	if (ret != EC_SUCCESS)
@@ -328,7 +331,7 @@ static int init(const struct motion_sensor_t *s)
 	int ret = 0, tmp;
 	struct stprivate_data *data = s->drv_data;
 
-	ret = raw_read8(s->port, s->addr, LIS2DH_WHO_AM_I_REG, &tmp);
+	ret = st_raw_read8(s->port, s->addr, LIS2DH_WHO_AM_I_REG, &tmp);
 	if (ret != EC_SUCCESS)
 		return ret;
 
@@ -340,34 +343,34 @@ static int init(const struct motion_sensor_t *s)
 	 * register must be restored to it's default
 	 */
 	/* Enable all accel axes data and clear old settings */
-	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL1_ADDR,
+	ret = st_raw_write8(s->port, s->addr, LIS2DH_CTRL1_ADDR,
 			 LIS2DH_ENABLE_ALL_AXES);
 	if (ret != EC_SUCCESS)
 		goto err_unlock;
 
-	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL2_ADDR,
+	ret = st_raw_write8(s->port, s->addr, LIS2DH_CTRL2_ADDR,
 			 LIS2DH_CTRL2_RESET_VAL);
 	if (ret != EC_SUCCESS)
 		goto err_unlock;
 
-	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL3_ADDR,
+	ret = st_raw_write8(s->port, s->addr, LIS2DH_CTRL3_ADDR,
 			 LIS2DH_CTRL3_RESET_VAL);
 	if (ret != EC_SUCCESS)
 		goto err_unlock;
 
 	/* Enable BDU */
-	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL4_ADDR,
+	ret = st_raw_write8(s->port, s->addr, LIS2DH_CTRL4_ADDR,
 			 LIS2DH_BDU_MASK);
 	if (ret != EC_SUCCESS)
 		goto err_unlock;
 
-	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL5_ADDR,
+	ret = st_raw_write8(s->port, s->addr, LIS2DH_CTRL5_ADDR,
 			 LIS2DH_CTRL5_RESET_VAL);
 	if (ret != EC_SUCCESS)
 		goto err_unlock;
 
-	ret = raw_write8(s->port, s->addr, LIS2DH_CTRL6_ADDR,
-			 LIS2DH_CTRL6_RESET_VAL);
+	ret = st_raw_write8(s->port, s->addr, LIS2DH_CTRL6_ADDR,
+			 0x20); //LIS2DH_CTRL6_RESET_VAL);
 	if (ret != EC_SUCCESS)
 		goto err_unlock;
 
