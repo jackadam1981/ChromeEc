6154cc1aec49ed14d016a77062096062b6040a97
Mulin Chao <mlchao@nuvoton.com>
2018-06-15 01:29:05 -0700

    kevin: use coreboot toolchain
    This defaults kevin to use the more compact coreboot toolchain, as the
    flash is full.
    
    BRANCH=none
    BUG=none
    TEST=No build errors on kevin.
    
    Change-Id: Idad93fed72f1e7bd2bdacaabc42dd6a2c4758438
    Signed-off-by: Nick Sanders <nsanders@chromium.org>
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

--- board/kevin/build.mk
+++ board/kevin/build.mk
@@ -10,6 +10,9 @@
 CHIP:=npcx
 CHIP_VARIANT:=npcx5m5g
 
+$(call set-option,CROSS_COMPILE_arm,$(CROSS_COMPILE_arm),\
+	/opt/coreboot-sdk/bin/arm-eabi-)
+
 board-y=battery.o board.o charge_ramp.o usb_pd_policy.o
 board-$(BOARD_GRU)+=led_gru.o
 board-$(BOARD_KEVIN)+=led_kevin.o
