7180fdfbbad84cb0a9c366ca9334245b63eab490
Ravi Chandra Sadineni <ravisadineni@chromium.org>
2018-06-18 16:51:25 -0700

    Script to convert board file to servod config .py file.
    Script to convert .board file (input to powerlog.py) into .py file which
    can be used to generate .xml configuration file for servod.
    
    BUG=none
    BRANCH=none
    TEST= convert a .board file to .py file and verify it works.
    Change-Id: Ia67d1552c048895efee10d7a92830cfa25a51984
    Signed-off-by: Ravi Chandra Sadineni <ravisadineni@chromium.org>

---
+++ extra/usb_power/convert_power_log_board.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+Program to convert sweetberry config to power logging config.
+"""
+
+
+import json
+import os
+import sys
+
+# Map between header channel number (0-47)
+# and INA I2C bus/addr on sweetberry.
+CHMAP = {
+    0: (3, 0x40),
+    1: (1, 0x40),
+    2: (2, 0x40),
+    3: (0, 0x40),
+    4: (3, 0x41),
+    5: (1, 0x41),
+    6: (2, 0x41),
+    7: (0, 0x41),
+    8: (3, 0x42),
+    9: (1, 0x42),
+    10: (2, 0x42),
+    11: (0, 0x42),
+    12: (3, 0x43),
+    13: (1, 0x43),
+    14: (2, 0x43),
+    15: (0, 0x43),
+    16: (3, 0x44),
+    17: (1, 0x44),
+    18: (2, 0x44),
+    19: (0, 0x44),
+    20: (3, 0x45),
+    21: (1, 0x45),
+    22: (2, 0x45),
+    23: (0, 0x45),
+    24: (3, 0x46),
+    25: (1, 0x46),
+    26: (2, 0x46),
+    27: (0, 0x46),
+    28: (3, 0x47),
+    29: (1, 0x47),
+    30: (2, 0x47),
+    31: (0, 0x47),
+    32: (3, 0x48),
+    33: (1, 0x48),
+    34: (2, 0x48),
+    35: (0, 0x48),
+    36: (3, 0x49),
+    37: (1, 0x49),
+    38: (2, 0x49),
+    39: (0, 0x49),
+    40: (3, 0x4a),
+    41: (1, 0x4a),
+    42: (2, 0x4a),
+    43: (0, 0x4a),
+    44: (3, 0x4b),
+    45: (1, 0x4b),
+    46: (2, 0x4b),
+    47: (0, 0x4b),
+}
+
+
+def fetch_records(board_file):
+    """Import records from servo_ina file.
+
+    board files are json files, and have a list of tuples with
+    the INA data.
+    (name, rs, swetberry_num, net_name, channel)
+
+    Args:
+      board_file: board file
+
+    Returns:
+      list of tuples as described above.
+    """
+    data = None
+    with open(board_file) as f:
+        data = json.load(f)
+    return data
+
+
+def write_to_file(file, sweetberry, inas):
+    """
+    Writes records of |sweetberry| to |file|
+    Args:
+      file: file to write to.
+      sweetberry: sweetberry type. A or B.
+      inas: list of inas read from board file.
+    """
+
+    pyfile = open(file, 'w')
+
+    pyfile.write('inas = [\n')
+    start = True
+
+    for rec in inas:
+        if not rec['sweetberry'] == sweetberry:
+            continue
+        if start:
+            start = False
+        else:
+            pyfile.write(',\n')
+        # EX : ('sweetberry', 0x40, 'SB_FW_CAM_2P8', 5.0, 1.000, 3, False),
+        i2c_addr = CHMAP[rec['channel']][1]
+        channel = CHMAP[rec['channel']][0]
+        record = "    ('sweetberry', 0x%02x, '%s', 5.0, %f, %d, 'False')" % (
+            i2c_addr, rec['name'], rec['rs'], channel)
+        pyfile.write(record)
+
+    pyfile.write('\n')
+    pyfile.write(']\n')
+
+
+def main(argv):
+    if len(argv) != 2:
+        print "usage:"
+        print " %s input.board" % argv[0]
+        return
+
+    inputf = argv[1]
+    basename = os.path.splitext(inputf)[0]
+
+    inas = fetch_records(inputf)
+
+    has_sweetberry_a = False
+    has_sweetberry_b = False
+
+    for rec in inas:
+        if rec['sweetberry'] == 'A':
+            has_sweetberry_a = True
+        if rec['sweetberry'] == 'B':
+            has_sweetberry_a = True
+
+    if has_sweetberry_a and has_sweetberry_b:
+        print "Converting %s to %s and %s" % (inputf, basename + '_a.py',
+                                              basename + '_b.py')
+        write_to_file(basename + '_a.py', 'A', inas)
+        write_to_file(basename + '_b.py', 'B', inas)
+
+    else:
+        print "Converting %s to %s" % (inputf, basename + '.py')
+        if has_sweetberry_a:
+            write_to_file(basename + '.py', 'A', inas)
+        elif has_sweetberry_b:
+            write_to_file(basename + '.py', 'B', inas)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
