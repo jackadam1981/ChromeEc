31c010434709e4af45075021fe5ebe947761ad55
Namyoon Woo <namyoon@google.com>
2018-06-19 08:44:37 -0700

    gsctool: support for enabling/disabling TPM.
    This patch introduces '-m' flag, which either enables/disables TPM or
    queries the current TPM status.
    
    BUG=b:77543596
    BRANCH=gsctool
    TEST=manually
         (chroot) gsctool -m
         (chroot) gsctool -m enable
         (chroot) gsctool -m
    
         (dut) reboot
         (chroot) gsctool -m
         (chroot) gsctool -m disable
         (chroot) gsctool -m
    
         (chroot) gsctool -m abcdefg
         (chroot) gsctool -m 1234
    
         (dut) gsctool -a -m
         (dut) gsctool -a -m enable
         (dut) gsctool -a -m
    
         (dut) reboot
         (dut) gsctool -a -m
         (dut) gsctool -a -m disable
         (dut) gsctool -a -m
    
         (dut) gsctool -a -m abcdefg
         (dut) gsctool -a -m 1234
    Signed-off-by: Namyoon Woo <namyoon@google.com>
    
    Change-Id: I2feb8c51d4343a49f2f5d43291c304e3ad140057

--- include/tpm_vendor_cmds.h
+++ include/tpm_vendor_cmds.h
@@ -70,6 +70,10 @@ enum vendor_cmd_cc {
 	 */
 	VENDOR_CC_WP = 39,
 
+	VENDOR_CC_ENABLE_TPM_MODE = 40,
+	VENDOR_CC_DISABLE_TPM_MODE = 41,
+	VENDOR_CC_GET_TPM_MODE = 42,
+
 	LAST_VENDOR_COMMAND = 65535,
 };
 
@@ -97,6 +101,13 @@ enum vendor_cmd_rc {
 
 	/* Only 7 bits available; max is 127 */
 	VENDOR_RC_NO_SUCH_COMMAND = 127,
+};
+
+/* Tpm mode */
+enum tpm_modes {
+	tpm_mode_enabled_tentative = 0,
+	tpm_mode_enabled = 1,
+	tpm_mode_disabled = 2,
 };
 
 /*
