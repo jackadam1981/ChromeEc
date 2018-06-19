7630529c06fc7f26623c92190f9d2509fcf56439
Paul Ma <magf@bitland.corp-partner.google.com>
2018-06-19 16:44:04 +0800

    FIXUP: fix lsm6dsm driver specific data type declaration
    accelgyro_lsm6dsm driver use lsm6dsm_data as its driver specific
    data type rather than stprivate_data. Wrong data type will lead
    to random corrupted runtime data.
    
    Signed-off-by: Paul Ma <magf@bitland.corp-partner.google.com>
    
    BRANCH=none
    BUG=b:110013316
    TEST=make buildall -j
    
    Change-Id: I50e0da293921c2662e97aa6f47c83519609186b5

--- driver/accelgyro_lsm6dsm.c
+++ driver/accelgyro_lsm6dsm.c
@@ -491,6 +491,14 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 static int init(const struct motion_sensor_t *s)
 {
 	int ret = 0, tmp;
+	/*
+	 * Since 'stprivate_data a_data;' is the first member of lsm6dsm_data,
+	 * the address of lsm6dsm_data is the same as a_data's. Using this
+	 * fact, we can do the following conversion. This conversion is equal
+	 * to:
+	 *     struct lsm6dsm_data *lsm_data = s->drv_data;
+	 *     struct stprivate_data *data = &lsm_data->a_data;
+	 */
 	struct stprivate_data *data = s->drv_data;
 
 	ret = st_raw_read8(s->port, s->addr, LSM6DSM_WHO_AM_I_REG, &tmp);
