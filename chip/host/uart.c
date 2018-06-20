206253dbb490c8b57b05757edf80760408a9b2bf
Nicolas Boichat <drinkcat@chromium.org>
2018-06-20 14:21:43 +0800

    test: host_command_fuzz: fuzzing test
    Writing fuzzing tests is a little tricky, as clang takes over the main
    function. Instead, we start the test main function in a thread, and
    have LLVMFuzzerTestOneInput prepare the host command buffer, and
    wake the TEST_RUNNER task.
    
    To make fuzzing faster, we only send somehow correctly formed requests,
    with a valid checksum and length (this can be disabled with an option).
    
    We also make sure that the emulator does not hibernate, reboot or jump
    to a different image when fuzzing is enabled.
    
    BRANCH=none
    BUG=chromium:854975
    TEST=make buildfuzztests -j
         ASAN_OPTIONS="log_path=stderr" \
             build/host/host_command_fuzz/host_command_fuzz.exe -timeout=5
    
    Change-Id: I27b25e44c405f118dfc1296247479245e15e54b4
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- chip/host/uart.c
+++ chip/host/uart.c
@@ -6,6 +6,7 @@
 /* UART driver for emulator */
 
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <termio.h>
 #include <unistd.h>
@@ -20,7 +21,9 @@
 static int stopped = 1;
 static int init_done;
 
+#ifndef TEST_FUZZ
 static pthread_t input_thread;
+#endif
 
 #define INPUT_BUFFER_SIZE 16
 static int char_available;
@@ -132,17 +135,24 @@ void uart_inject_char(char *s, int sz)
 	}
 }
 
+/*
+ * We do not really need console input when fuzzing, and having it enabled
+ * breaks terminal when an error is detected.
+ */
+#ifndef TEST_FUZZ
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t uart_monitor_initialized = PTHREAD_COND_INITIALIZER;
 
 void *uart_monitor_stdin(void *d)
 {
-	struct termios org_settings, new_settings;
+	struct termios new_settings;
+	static struct termios org_settings;
 	char buf[INPUT_BUFFER_SIZE];
 	int rv;
 
 	pthread_mutex_lock(&mutex);
 	tcgetattr(0, &org_settings);
+
 	new_settings = org_settings;
 	new_settings.c_lflag &= ~(ECHO | ICANON);
 	new_settings.c_cc[VTIME] = 0;
@@ -170,14 +180,17 @@ void *uart_monitor_stdin(void *d)
 
 	return 0;
 }
+#endif /* !TEST_FUZZ */
 
 void uart_init(void)
 {
+#ifndef TEST_FUZZ
 	/* Create UART monitor thread and wait for it to initialize. */
 	pthread_mutex_lock(&mutex);
 	pthread_create(&input_thread, NULL, uart_monitor_stdin, NULL);
 	pthread_cond_wait(&uart_monitor_initialized, &mutex);
 	pthread_mutex_unlock(&mutex);
+#endif
 
 	stopped = 1;  /* Not transmitting yet */
 	init_done = 1;
