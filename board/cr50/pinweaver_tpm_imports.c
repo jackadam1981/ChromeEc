bf2587a5abba5cf442c703fd0f8582c935df5790
Igor <igorcov@chromium.org>
2018-06-21 16:39:57 +0200

    Cr50: Add valid PCR value for pinweaver
    In order to bind the PIN authentication to PCR4, required for
    additional security, a set of valid PCR criteria is added as metadata
    in the leaf of the tree. Each criteria has a bitmask of PCR indexes
    and the digest sha256 that should be obtained from concatenation of
    PCR values for the respective indexes. Pinweaver will validate
    the le_secret and release the he_secret only after reading the PCR
    values and checking that at least on criteria is satisfied.
    
    For migration of old leaves that don't have the new field, the new
    function pw_seal_to_pcr is added. Chrome OS detects when
    a leaf is not migrated and calls it instead of pw_handle_try_auth.
    
    For security the migration is not allowed without valid credentials.
    So, pw_seal_to_pcr does both, authentication and migration of the
    leaf. Also it checks that the leaf was not migrated already.
    
    The PCR criteria set is created on Chrome OS side. Details of that
    implementation is in
    https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1124856
    
    BRANCH=none
    BUG=chromium:812165
    TEST=sudo V=1 make run-pinweaver -j
    pinweaver_client selftest
    Deploy old image on a device and create an account setting a PIN code
    as well. Deploy the new image and new CR50 build. Login and check that the
    migration works well. Also try to put device to sleep and unlock. Check
    that a new credential creation with new version works as well and sleep +
    unlock work as expected. Extend PCR4 on device and check that login/unlock
    works only for the user which obfuscated_username was used to extend the
    PCR. Also check that authentication works with cases when old cryptohome
    and new pinweaver is deployed, or old pinweaver and new cryptohome.
    
    Change-Id: If778c4e46b9945afadfd2af7d58353005624d668
    Signed-off-by: igorcov@chromium.org

--- board/cr50/pinweaver_tpm_imports.c
+++ board/cr50/pinweaver_tpm_imports.c
@@ -6,10 +6,52 @@
 #include <pinweaver_tpm_imports.h>
 
 #include <Global.h>
+#include <InternalRoutines.h>
+#include <dcrypto.h>
 #include <util.h>
 
 void get_storage_seed(void *buf, size_t *len)
 {
 	*len = MIN(*len, sizeof(gp.SPSeed));
 	memcpy(buf, &gp.SPSeed, *len);
+}
+
+uint8_t get_current_pcr_digest(const uint8_t bitmask[2], uint8_t hash_size,
+		uint8_t *sha256_of_selected_pcr)
+{
+	LITE_SHA256_CTX ctx;
+	TPML_DIGEST digest;
+	uint32_t pcr_counter;
+	TPML_PCR_SELECTION selection;
+	size_t x;
+	size_t y;
+	uint8_t digest_index;
+
+	selection.count = 1;
+	selection.pcrSelections[0].hash = TPM_ALG_SHA256;
+	selection.pcrSelections[0].sizeofSelect = PCR_SELECT_MIN;
+	memset(&selection.pcrSelections[0].pcrSelect, 0, PCR_SELECT_MIN);
+	memcpy(&selection.pcrSelections[0].pcrSelect, bitmask, sizeof(bitmask));
+
+	PCRRead(&selection, &digest, &pcr_counter);
+	DCRYPTO_SHA256_init(&ctx, 0);
+	/** For each bit present in bitmask, include the corresponding
+	 *  PCR value in the digest.
+	 */
+	digest_index = 0;
+	for (x = 0; x < 2; ++x) {
+		if (selection.pcrSelections[0].pcrSelect[x] != bitmask[x])
+			return 1;
+		for (y = 0; y < 8; ++y) {
+			if ((bitmask[x] & (1 << y)) != 0) {
+				HASH_update(
+					&ctx,
+					digest.digests[digest_index].b.buffer,
+					hash_size);
+				digest_index++;
+			}
+		}
+	}
+	memcpy(sha256_of_selected_pcr, HASH_final(&ctx), hash_size);
+	return 0;
 }
