00d8cd760811bbb8f979c7de2e43a31034bfc42c
Igor <igorcov@chromium.org>
2018-06-21 16:39:57 +0200

    Cr50: Add valid PCR value for pinweaver
    In order to bind the PIN authentication to PCR4, required for
    additional security, a valid PCR value is added as metadata in the leaf
    of the tree. The pinweaver will validate the le_secret and release the
    he_secret only after reading the value of PCR4 and checking that it's
    either the default value (unchanged), or the value stored in the leaf.
    
    Each user will have it's own valid PCR4 value and it will be provided
    from the caller when insert_leaf is requested.
    
    BRANCH=none
    BUG=chromium:812165
    TEST=manual
    
    Change-Id: If778c4e46b9945afadfd2af7d58353005624d668
    Signed-off-by: igorcov@chromium.org

--- include/pinweaver.h
+++ include/pinweaver.h
@@ -74,6 +74,7 @@ struct PW_PACKED leaf_public_data_t {
 	/* State used to rate limit. */
 	struct pw_timestamp_t timestamp;
 	struct attempt_count_t attempt_count;
+	struct valid_pcr_value_t valid_pcr_criteria[PW_MAX_PCR_CRITERIA_COUNT];
 };
 
 /* Do not remove fields within the same PW_LEAF_MAJOR_VERSION. */
@@ -111,7 +112,7 @@ struct imported_leaf_data_t {
 	/* Covers .head, .pub, and .cipher_text. */
 	const uint8_t *hmac;
 	const uint8_t *iv;
-	const struct leaf_public_data_t *pub;
+	struct leaf_public_data_t *pub;
 	const uint8_t *cipher_text;
 	const uint8_t (*hashes)[PW_HASH_SIZE];
 };
