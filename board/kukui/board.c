e5e77c2c4b43f050a41fa9b133f914bdf0dd34ca
Yilun Lin <yllin@google.com>
2018-06-21 18:44:47 +0800

    kukui: emulate eMMC boot mode using SPI controller.
    This is modified from CL:1039105.
    Kukui is not able to boot from SPI-NOR, it can only boot from eMMC or
    UFS. Here we emulates the eMMC boot using SPI controller. AP tranmits on
    CMD line, and eMMC replies on DAT0 line.
    
    BRANCH=None
    BUG=b:80159522
    TEST=make BOARD=kukui BOOTBLOCK=abc -j # check build pass
    
    Change-Id: I988b1a93e41d84b9b7574ab0ca35da98b0d006c0
    Signed-off-by: Yilun Lin <yllin@google.com>

--- board/kukui/board.c
+++ board/kukui/board.c
@@ -6,13 +6,16 @@
 #include "adc_chip.h"
 #include "adc.h"
 #include "backlight.h"
+#include "bootblock.h"
 #include "button.h"
 #include "charge_manager.h"
 #include "charger.h"
 #include "charge_state.h"
 #include "chipset.h"
 #include "common.h"
+#include "clock.h"
 #include "console.h"
+#include "dma.h"
 #include "driver/accelgyro_bmi160.h"
 #include "driver/charger/rt946x.h"
 #include "driver/sync.h"
@@ -224,7 +227,6 @@ static void board_init(void)
 
 	/* Enable interrupt from PMIC. */
 	gpio_enable_interrupt(GPIO_PMIC_EC_RESETB);
-
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
@@ -238,6 +240,40 @@ void board_config_pre_init(void)
 	 */
 	STM32_DMA_CSELR(STM32_DMAC_CH4) = (1 << 15) | (1 << 19);
 }
+
+#ifdef SECTION_IS_RO
+void board_init_spi2(void)
+{
+	/* bootblock */
+	/* Set SPI2 PB13/14/15 pins to high speed */
+	STM32_GPIO_OSPEEDR(GPIO_B) |= 0xfc000000;
+
+	/* Reset SPI2 */
+	STM32_RCC_APB1RSTR |= STM32_RCC_PB1_SPI2;
+	STM32_RCC_APB1RSTR &= ~STM32_RCC_PB1_SPI2;
+
+	/* Enable clocks to SPI2 module */
+	STM32_RCC_APB1ENR |= STM32_RCC_PB1_SPI2;
+
+	clock_wait_bus_cycles(BUS_APB, 1);
+	gpio_config_module(MODULE_SPI, 1);
+
+	STM32_SPI2_REGS->cr2 = STM32_SPI_CR2_FRXTH | STM32_SPI_CR2_DATASIZE(8) |
+			       STM32_SPI_CR2_RXDMAEN | STM32_SPI_CR2_TXDMAEN;
+
+	/* Manual CS, disable. */
+	STM32_SPI2_REGS->cr1 = STM32_SPI_CR1_SPE;
+
+	STM32_SPI2_REGS->dr = 0xff;
+	STM32_SPI2_REGS->dr = 0xff;
+	STM32_SPI2_REGS->dr = 0xff;
+	STM32_SPI2_REGS->dr = 0xff;
+
+	/* Enable the SPI peripheral */
+	STM32_SPI2_REGS->cr1 |= STM32_SPI_CR1_SPE;
+}
+DECLARE_HOOK(HOOK_INIT, board_init_spi2, HOOK_PRIO_INIT_PWM - 1);
+#endif
 
 enum kukui_board_version {
 	BOARD_VERSION_UNKNOWN = -1,
@@ -408,3 +444,187 @@ int tablet_get_mode(void)
 void usb_charger_set_switches(int port, enum usb_switch setting)
 {
 }
+
+/******************************************************************************/
+/* Transfer bootblock over SPI by emulating eMMC protocol. */
+#ifdef SECTION_IS_RO
+
+static stm32_spi_regs_t *const spi_emmc = STM32_SPI2_REGS;
+/* 1024 bytes of buffer is enough for ~0.6ms @ 13Mhz */
+#define SPI_RX_BUF_SIZE 1024
+#define SPI_RX_BUF_SIZE_32 (SPI_RX_BUF_SIZE/4)
+static uint32_t in_msg[SPI_RX_BUF_SIZE_32] __aligned(4);
+
+/* Macros to advance in the circular buffer */
+#define RX_BUF_NEXT_32(i) (((i) + 1) & (SPI_RX_BUF_SIZE_32 - 1))
+#define RX_BUF_DEC_32(i, j) (((i) - (j)) & (SPI_RX_BUF_SIZE_32 - 1))
+#define RX_BUF_PREV_32(i) RX_BUF_DEC_32((i), 1)
+
+static int bootblock_transfer_try;
+
+enum emmc_cmd {
+	EMMC_ERROR = -1,
+	EMMC_IDLE = 0,
+	EMMC_PRE_IDLE,
+	EMMC_BOOT,
+};
+
+static const struct dma_option dma_tx_option = {
+	STM32_DMAC_SPI2_TX, (void *)&STM32_SPI2_REGS->dr,
+	STM32_DMA_CCR_MSIZE_8_BIT | STM32_DMA_CCR_PSIZE_8_BIT
+};
+
+static const struct dma_option dma_rx_option = {
+	STM32_DMAC_SPI2_RX, (void *)&STM32_SPI2_REGS->dr,
+	STM32_DMA_CCR_MSIZE_8_BIT | STM32_DMA_CCR_PSIZE_8_BIT |
+	STM32_DMA_CCR_CIRC
+};
+
+static void bootblock_transfer(void)
+{
+	const uint32_t bootblock_size = &bootblock_end - &bootblock_start;
+	dma_chan_t *txdma = dma_get_channel(STM32_DMAC_SPI2_TX);
+
+	dma_prepare_tx(&dma_tx_option, bootblock_size, &bootblock_start);
+	dma_go(txdma);
+
+	bootblock_transfer_try++;
+	CPRINTS("eMMC transfer(%d)", bootblock_transfer_try);
+}
+
+static void bootblock_stop(void)
+{
+	dma_disable(STM32_DMAC_SPI2_TX);
+	while ((spi_emmc->sr & STM32_SPI_SR_FTLVL) != 0)
+		;
+	spi_emmc->dr = 0xff;
+	spi_emmc->dr = 0xff;
+	spi_emmc->dr = 0xff;
+	spi_emmc->dr = 0xff;
+}
+
+static uint32_t letobe(uint32_t val)
+{
+	return (val & 0x000000ff) << 24 | (val & 0x0000ff00) << 8 |
+	       (val & 0x00ff0000) >> 8  | (val & 0xff000000) >> 24;
+}
+
+static enum emmc_cmd emmc_parse_command(int index)
+{
+	int32_t shift0, mask1;
+	uint32_t data[3];
+
+	mask1 = 0x00000000;
+
+	/* Figure out alignment (cmd starts with 01) */
+	if (in_msg[index] == 0xffffffff)
+		return EMMC_ERROR;
+
+	data[0] = letobe(in_msg[index]);
+	index = RX_BUF_NEXT_32(index);
+	data[1] = letobe(in_msg[index]);
+	index = RX_BUF_NEXT_32(index);
+	data[2] = letobe(in_msg[index]);
+
+	/* Number of leading ones. */
+	shift0 = __builtin_clz(~data[0]);
+
+	if (shift0 > 0)
+		mask1 = (int32_t)0x80000000 >> (shift0-1);
+
+	data[0] = (data[0] << shift0) | ((data[1] & mask1) >> (32-shift0));
+	data[1] = (data[1] << shift0) | ((data[2] & mask1) >> (32-shift0));
+
+	if (data[0] == 0x40000000 && data[1] == 0x0095ffff) {
+		/* 400000000095 GO_IDLE_STATE */
+		CPRINTS("eMMC goIdle");
+		return EMMC_IDLE;
+	}
+
+	if (data[0] == 0x40f0f0f0 && data[1] == 0xf0fdffff) {
+		/* 40f0f0f0f0fd GO_PRE_IDLE_STATE */
+		CPRINTS("eMMC goPreIdle");
+		return EMMC_PRE_IDLE;
+	}
+
+	if (data[0] == 0x40ffffff && data[1] == 0xfae5ffff) {
+		/* 40fffffffae5 BOOT_INITIATION */
+		CPRINTS("eMMC bootInit");
+		return EMMC_BOOT;
+	}
+
+	CPRINTS("eMMC error");
+	return EMMC_ERROR;
+}
+
+void emmc_cmd_interrupt(enum gpio_signal signal)
+{
+	task_wake(TASK_ID_EMMC);
+}
+
+void emmc_task(void *u)
+{
+	/* Both are 32-bit indexes. */
+	int dma_pos, i;
+	dma_chan_t *rxdma = dma_get_channel(STM32_DMAC_SPI2_RX);
+	int tx = 0;
+	enum emmc_cmd cmd;
+
+	gpio_enable_interrupt(GPIO_EMMC_CMD);
+
+	dma_start_rx(&dma_rx_option, sizeof(in_msg), in_msg);
+
+	/* Enable internal chip select */
+	spi_emmc->cr1 &= ~STM32_SPI_CR1_SSI;
+
+	while (1) {
+		/* Wait for a command */
+		task_wait_event(-1);
+
+		/* Since we round down, in theory we sould not  */
+		dma_pos = dma_bytes_done(rxdma, sizeof(in_msg)) / 4;
+		i = RX_BUF_PREV_32(dma_pos);
+
+		/*
+		 * By now, bus should be idle again (it takes <10us to transmit
+		 * a command).
+		 */
+		if (in_msg[i] != 0xffffffff) {
+			CPRINTF("?");
+			/* TODO: We should probably just retry. */
+			continue;
+		}
+
+		/* Look for a command, from the end of the buffer. */
+		while (i != dma_pos && in_msg[i] == 0xffffffff)
+			i = RX_BUF_PREV_32(i);
+
+		/* We missed the command? */
+		if (i == dma_pos) {
+			CPRINTF("!");
+			continue;
+		}
+
+		/* We found the end, now find the beginning. */
+		i = RX_BUF_DEC_32(i, 2);
+		while (i != dma_pos && in_msg[i] == 0xffffffff)
+			i = RX_BUF_NEXT_32(i);
+
+		cmd = emmc_parse_command(i);
+
+		/* When not transferring, all we care about is EMMC_BOOT. */
+		if (!tx) {
+			if (cmd == EMMC_BOOT) {
+				tx = 1;
+				bootblock_transfer();
+			}
+		} else {
+			/* Abort (should be Idle, but react to Pre-Idle too) */
+			if (cmd == EMMC_IDLE || cmd == EMMC_PRE_IDLE) {
+				bootblock_stop();
+				tx = 0;
+			}
+		}
+	}
+}
+#endif /* SECTION_IS_RO */
--- board/kukui/board.h
+++ board/kukui/board.h
@@ -40,6 +40,9 @@
 /* Region sizes are no longer a power of 2 so we can't enable MPU */
 #undef  CONFIG_MPU
 
+/* Bootblock */
+#define CONFIG_PACK_BOOTBLOCK
+
 /* Optional features */
 #define CONFIG_BOARD_PRE_INIT
 #define CONFIG_BOARD_VERSION_CUSTOM
@@ -200,6 +203,11 @@ enum sensor_id {
 
 #include "gpio_signal.h"
 #include "registers.h"
+
+#ifdef SECTION_IS_RO
+/* Interrupt handler for emmc task*/
+void emmc_cmd_interrupt(enum gpio_signal signal);
+#endif
 
 void board_reset_pd_mcu(void);
 int board_get_version(void);
