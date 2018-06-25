eaa925b01f2e6f948455267e95f5d46931ba0f94
Nicolas Boichat <drinkcat@chromium.org>
2018-06-25 16:12:46 +0800

    TEST-ONLY: Private overlay for board-specific feature
    For demonstration/explanation only.
    
    BRANCH=none
    BUG=b:124804731
    TEST=build and deploy kukui_scp, see all testprivate,
         testprivate2, and testprivatenl commands.
    
    Change-Id: I71a7af45df4e0f5cdfc121e765fc699df9c65bd0
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

---
+++ private-mtk/board/kukui_scp/build.mk
@@ -0,0 +1,23 @@
+#-*- makefile -*-
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Private board specific files build
+
+board-private-y = private-nolib.o
+
+# TODO(b/121406695): We do not include the archive in RO, since we do not care
+# about RO image
+PRIVPATH=private-mtk/board/kukui_scp
+RWPATH=$(out)/RW/$(PRIVPATH)
+
+libprivate-objs = private.o private2.o
+
+$(RWPATH)/libprivate.a: $(foreach o, $(libprivate-objs), $(RWPATH)/$(o))
+
+$(out)/RW/ec.RW.elf: $(RWPATH)/libprivate.a
+# Include the whole libprivate archive (not just the symbols that are used)
+$(out)/RW/ec.RW.elf: LDFLAGS_EXTRA += -L$(RWPATH) -Wl,--whole-archive -lprivate
+# Restore default behaviour
+$(out)/RW/ec.RW.elf: LDFLAGS_EXTRA += -Wl,--no-whole-archive
