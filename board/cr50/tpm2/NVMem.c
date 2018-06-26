b7cb144c2d30479e3e702c091f3cf0920e7e6ccd
Louis Collard <louiscollard@chromium.org>
2018-06-26 19:57:13 +0800

    cr50: Add support for virtual NV indexes.
    This is to allow reading of board ID and serial number
    through NV indexes.
    
    CQ-DEPEND=CL:1114675
    BRANCH=none
    BUG=b:110971075 chromium:846114
    TEST=manual NV_Read of virtual indices using trunks_send --raw (NV_Read command is simpler than NV_Certify, but also tested NV_Certify in the past)
    
    Change-Id: Iaf7256ea50e36021b61c92f8606d1a62af37df2d
    Signed-off-by: Louis Collard <louiscollard@chromium.org>

--- board/cr50/tpm2/NVMem.c
+++ board/cr50/tpm2/NVMem.c
@@ -16,6 +16,7 @@
 #include "TpmError.h"
 #include "assert.h"
 #include "nvmem.h"
+#include "virtual_nvmem.h"
 
 /* Local state */
 static struct {
@@ -114,6 +115,11 @@ void _plat__NvMemoryRead(unsigned int startOffset,
 				    unsigned int size,
 				    void *data)
 {
+	if (_plat__NvOffsetIsVirtual(startOffset)) {
+		_plat__NvVirtualMemoryRead(startOffset, size, data);
+		return;
+	}
+
 	assert(startOffset + size <= NV_MEMORY_SIZE);
 	/* Copy the data from the NV image */
 #ifdef CONFIG_FLASH_NVMEM
