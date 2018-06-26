929c883237faada903887cff292f342fe124799b
Paul Ma <magf@bitland.corp-partner.google.com>
2018-06-26 09:04:18 +0800

    phaser: enable tablet mode and lid angle
    Enable CONFIG_DYNAMIC_MOTION_SENSOR_COUNT so that clamshell
    skus and convertible skus get different sensor number. Enable
    lid angle function to make peripherals get correct state on
    convertible skus. Enable tablet mode for convertible skus as
    well.
    
    Signed-off-by: Paul Ma <magf@bitland.corp-partner.google.com>
    
    BRANCH=none
    BUG=b:110604678
    TEST=boot phaser360 sku, test keyboard function when hinge is
    at 135, 180, 270, 360 degree. test tablet mode when 360 degree.
    
    Change-Id: I8f15a2d3ea3ae7d0cf5d60333e079d1ffd8e906b

--- board/phaser/board.c
+++ board/phaser/board.c
@@ -18,15 +18,19 @@
 #include "extpower.h"
 #include "gpio.h"
 #include "hooks.h"
+#include "keyboard_scan.h"
 #include "lid_switch.h"
 #include "power.h"
 #include "power_button.h"
 #include "switch.h"
 #include "task.h"
+#include "tablet_mode.h"
 #include "tcpci.h"
 #include "temp_sensor.h"
 #include "thermistor.h"
 #include "util.h"
+
+static uint16_t sku_id;
 
 static void tcpc_alert_event(enum gpio_signal signal)
 {
@@ -196,12 +200,57 @@ struct motion_sensor_t motion_sensors[] = {
 	},
 };
 
-const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+static void cbi_init(void)
+{
+	uint32_t val;
+
+	if (cbi_get_sku_id(&val) == EC_SUCCESS)
+		sku_id = val;
+	ccprints("SKU: 0x%04x", sku_id);
+}
+DECLARE_HOOK(HOOK_INIT, cbi_init, HOOK_PRIO_INIT_I2C + 1);
+
+/*
+ * TODO: remove sku_id == 255 when ready
+ */
+static int board_is_convertible(void)
+{
+	return (sku_id > 0 && sku_id <= 6) || sku_id == 255;
+}
+
+static void board_set_motion_sensor_count(void)
+{
+	if (board_is_convertible())
+		motion_sensor_count = ARRAY_SIZE(motion_sensors);
+	else
+		motion_sensor_count = 0;
+}
 
 /* Initialize board. */
 static void board_init(void)
 {
 	/* Enable Base Accel interrupt */
 	gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
+
+	board_set_motion_sensor_count();
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+#ifndef TEST_BUILD
+/* This callback disables keyboard when convertibles are fully open */
+void lid_angle_peripheral_enable(int enable)
+{
+	/*
+	 * If the lid is in tablet position via other sensors,
+	 * ignore the lid angle, which might be faulty then
+	 * disable keyboard.
+	 */
+	if (tablet_get_mode())
+		enable = 0;
+
+	if (board_is_convertible())
+		keyboard_scan_enable(enable, KB_SCAN_DISABLE_LID_ANGLE);
+}
+#endif
