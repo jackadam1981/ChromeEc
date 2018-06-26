cf4cd40d365a02b2bd02d8b76bc6163bd1860628
Louis Collard <louiscollard@chromium.org>
2018-06-26 19:57:13 +0800

    cr50: Add support for virtual NV indexes.
    This is to allow reading of board ID and serial number
    through NV indexes.
    
    BRANCH=none
    BUG=b:110971075 chromium:846114
    TEST=todo
    
    Change-Id: Iaf7256ea50e36021b61c92f8606d1a62af37df2d
    Signed-off-by: Louis Collard <louiscollard@chromium.org>

---
+++ board/cr50/tpm2/virtual_nvmem.h
@@ -0,0 +1,34 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __EC_BOARD_CR50_TPM2_VIRTUAL_NVMEM_H
+#define __EC_BOARD_CR50_TPM2_VIRTUAL_NVMEM_H
+
+#include "bool.h"
+#include "stdint.h"
+
+/*
+ * If the specified handle represents a virtual NV index, this function returns
+ * the corresponding virtual offset. For all other handles, 0 is returned.
+ */
+uint32_t _plat__NvGetHandleVirtualOffset(uint32_t handle);
+
+/*
+ * Returns true iff the specified offset corresponds to virtual NV memory; if
+ * so, this offset must only be read using the _plat__NvVirtualMemoryRead()
+ * function below.
+ */
+BOOL _plat__NvOffsetIsVirtual(unsigned int startOffset);
+
+/*
+ * Equivalent of _plat__NvMemoryRead, but for virtual NV memory. Only a limited
+ * number of offsets can be read using this function. For any value x returned
+ * by _plat__NvGetHandleVirtualOffset, offsets x+4 and x+152 may be read using
+ * this function. No other offsets may be read.
+ */
+void _plat__NvVirtualMemoryRead(unsigned int startOffset, unsigned int size,
+				void *data);
+
+#endif /* __EC_BOARD_CR50_TPM2_VIRTUAL_NVMEM_H */
