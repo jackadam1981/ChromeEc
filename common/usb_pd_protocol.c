0e5d8474e423b3d34ceca47250732db6676d0f92
Daisuke Nojiri <dnojiri@chromium.org>
2018-06-26 17:34:50 -0700

    Nami: Don't restore USB-PD roles
    WORK_IN_PROGRESS
    
    This patch makes tcpm skip restoring previous data and power roles.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b:80052680
    BRANCH=none
    TEST=Verify external display shows picture after hard reset using
    Dingdong, HP Z27n, and Sona.
    
    Change-Id: Ibaa68f481fa6a09b5619152d2c55cc392afdce9e

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -2230,7 +2230,7 @@ void pd_task(void *u)
 
 	/* Initialize PD protocol state variables for each port. */
 	pd_set_power_role(port, PD_ROLE_DEFAULT(port));
-#ifdef CONFIG_USB_PD_DUAL_ROLE
+#if defined(CONFIG_USB_PD_DUAL_ROLE) && defined(CONFIG_USB_PD_RESTORE_ROLES)
 	/*
 	 * If there's an explicit contract in place, let's restore the data and
 	 * power roles such that any messages we send to the port partner will
