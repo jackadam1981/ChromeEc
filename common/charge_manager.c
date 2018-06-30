1e1f467e20925c15fc4d710ff9c5a720b544297e
Vijay Hiremath <vijay.p.hiremath@intel.com>
2018-06-30 11:24:13 -0700

    charge_manager: Code cleanup
    Check for charge ramp stable only if charge ramp is allowed.
    
    BUG=b:80279932
    BRANCH=none
    TEST=Manually tested on BIP.
         Charge ramp stable check is not called when PD & Type-C
         chargers are used.
    
    Change-Id: I4169da3c9f0111a43b490027a297d5cceb5f66e3
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -283,13 +283,6 @@ static void charge_manager_fill_power_info(int port,
 		r->meas.current_max = charge_manager_get_source_current(port);
 		r->max_power = 0;
 	} else {
-#if defined(HAS_TASK_CHG_RAMP) || defined(CONFIG_CHARGE_RAMP_HW)
-		/* Read ramped current if active charging port */
-		int use_ramp_current = (charge_port == port);
-#else
-		const int use_ramp_current = 0;
-#endif
-
 		switch (sup) {
 		case CHARGE_SUPPLIER_PD:
 			r->type = USB_CHG_TYPE_PD;
@@ -333,6 +326,15 @@ static void charge_manager_fill_power_info(int port,
 				     CHARGE_DETECT_DELAY)
 			r->type = USB_CHG_TYPE_UNKNOWN;
 
+		{
+#if defined(HAS_TASK_CHG_RAMP) || defined(CONFIG_CHARGE_RAMP_HW)
+		/* Read ramped current if active charging port */
+		int use_ramp_current = (charge_port == port) &&
+					chg_ramp_allowed(sup);
+#else
+		const int use_ramp_current = 0;
+#endif
+
 		if (use_ramp_current) {
 			/* Current limit is output of ramp module */
 			r->meas.current_lim = chg_ramp_get_current_limit();
@@ -346,16 +348,9 @@ static void charge_manager_fill_power_info(int port,
 			 * If ramp is not allowed, max current is just the
 			 * available charge current.
 			 */
-			if (chg_ramp_allowed(sup)) {
-				r->meas.current_max = chg_ramp_is_stable() ?
-					r->meas.current_lim :
-					chg_ramp_max(
-					  sup,
-					  available_charge[sup][port].current);
-			} else {
-				r->meas.current_max =
-					available_charge[sup][port].current;
-			}
+			r->meas.current_max = chg_ramp_is_stable() ?
+				r->meas.current_lim : chg_ramp_max(sup,
+					available_charge[sup][port].current);
 
 			r->max_power =
 				r->meas.current_max * r->meas.voltage_max;
@@ -363,6 +358,7 @@ static void charge_manager_fill_power_info(int port,
 			r->meas.current_max = r->meas.current_lim =
 				available_charge[sup][port].current;
 			r->max_power = POWER(available_charge[sup][port]);
+		}
 		}
 
 		/*
