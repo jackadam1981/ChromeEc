d26a4f825f2bf90dd39bce826e8553d3b570bdbf
Wei-Han Chen <stimim@google.com>
2018-07-06 15:33:25 +0800

    stm32: usb_hid_touchpad: add confidence
    `confidence` is expected from a PTP-compliant touchpad
    
    BRANCH=none
    BUG=b:70482333
    TEST=manual
    Signed-off-by: Wei-Han Chen <stimim@chromium.org>
    
    Change-Id: Ibe7166f43cae5fb36bbcc4e6dfd9ae6525a7225e

--- include/usb_hid_touchpad.h
+++ include/usb_hid_touchpad.h
@@ -8,20 +8,27 @@
 #ifndef __CROS_EC_USB_HID_TOUCHPAD_H
 #define __CROS_EC_USB_HID_TOUCHPAD_H
 
+#include <stdint.h>
+
 #define USB_HID_TOUCHPAD_TIMESTAMP_UNIT 100 /* usec */
+
+#define REPORT_ID_TOUCHPAD		0x01
+
+#define MAX_FINGERS			5
 
 struct usb_hid_touchpad_report {
 	uint8_t id; /* 0x01 */
 	struct {
-		unsigned tip:1;
-		unsigned inrange:1;
-		unsigned id:4;
-		unsigned pressure:10;
+		uint16_t confidence:1;
+		uint16_t tip:1;
+		uint16_t inrange:1;
+		uint16_t id:4;
+		uint16_t pressure:9;
 		unsigned width:12;
 		unsigned height:12;
 		unsigned x:12;
 		unsigned y:12;
-	} __packed finger[5];
+	} __packed finger[MAX_FINGERS];
 	uint8_t count:7;
 	uint8_t button:1;
 	uint16_t timestamp;
