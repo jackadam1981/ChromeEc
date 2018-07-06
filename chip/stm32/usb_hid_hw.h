a21d8f4dd35718f286425a0c2667036962e20e03
Wei-Han Chen <stimim@google.com>
2018-07-06 15:04:41 +0800

    stm32: usb_hid: refactor to support get_report
    BRANCH=none
    BUG=b:70482333
    TEST=manual on device
    Signed-off-by: Wei-Han Chen <stimim@chromium.org>
    
    Change-Id: I1543f019676259beb161db3195364998987ea529

--- chip/stm32/usb_hid_hw.h
+++ chip/stm32/usb_hid_hw.h
@@ -8,12 +8,17 @@
 #ifndef __CROS_EC_USB_HID_HW_H
 #define __CROS_EC_USB_HID_HW_H
 
+struct usb_hid_config_t {
+	const uint8_t *report_desc;
+	int report_size;
+	const struct usb_hid_descriptor *hid_desc;
+};
+
 /* internal callbacks for HID class drivers */
 void hid_tx(int ep);
 void hid_reset(int ep, usb_uint *hid_ep_tx_buf, int tx_len,
 	       usb_uint *hid_ep_rx_buf, int rx_len);
 int hid_iface_request(usb_uint *ep0_buf_rx, usb_uint *ep0_buf_tx,
-		      const uint8_t *report_desc, int report_size,
-		      const struct usb_hid_descriptor *hid_desc);
+		      const struct usb_hid_config_t *hid_config);
 
 #endif
