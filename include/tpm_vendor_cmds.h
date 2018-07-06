dd188ce8683f7318b513508acca628cc85601176
Louis Collard <louiscollard@chromium.org>
2018-07-06 16:38:38 +0800

    cr50: Add commands to get/set serial number bits.
    Allocates 96 bits of INFO1 space, in the 'board' section, and
    after the current Board ID data, to store the serial number
    bits for use by zero-touch enrollment.
    
    Adds a console command to read / set this data.
    
    Adds a TPM vendor command to set this data.
    
    CQ-DEPEND=CL:*657450
    BUG=b:111195266
    TEST=todo
    BRANCH=none
    
    Change-Id: I752aefad9654742b7719156202f29d635d2306df
    Signed-off-by: Louis Collard <louiscollard@chromium.org>

--- include/tpm_vendor_cmds.h
+++ include/tpm_vendor_cmds.h
@@ -80,6 +80,11 @@ enum vendor_cmd_cc {
 	 * it will response with the current tpm_mode value in uint8_t format.
 	 */
 	VENDOR_CC_TPM_MODE = 40,
+	/*
+	 * Sets SN bits. Takes three integers as parameters, which are written
+	 * to INFO1.
+	 */
+	VENDOR_CC_SET_SN_BITS = 41,
 
 	LAST_VENDOR_COMMAND = 65535,
 };
