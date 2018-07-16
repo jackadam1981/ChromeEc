bb65460aa5b66723814bfd99c215fd556252f58c
Ruben Rodriguez Buchillon <coconutruben@chromium.org>
2018-07-16 19:52:35 +0800

    stats_manager: remove expliting printing from stats_manager
    StatsManager is a library shared across some modules. It shouldn't print
    anything itself, but rather use logging & return a formatted summary
    that can be printed.
    
    Also take this opportunity to replace majority of explicity print calls
    in powerlog with logger module.
    
    BRANCH=None
    BUG=chromium:760267
    TEST=manual testing, output as expected still for powerlog
    
    Change-Id: I0ed2ccaa45bbd1261119a1446110beadb075c1a2
    Signed-off-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>

--- extra/usb_power/powerlog_unittest.py
+++ extra/usb_power/powerlog_unittest.py
@@ -27,14 +27,14 @@ class TestPowerlog(unittest.TestCase):
 
   def test_ProcessFilenameAbsoluteFilePath(self):
     """Absolute file path is returned unchanged."""
-    processed_fn = powerlog_util.process_filename(self.filepath)
+    processed_fn = powerlog.process_filename(self.filepath)
     self.assertEqual(self.filepath, processed_fn)
 
   def test_ProcessFilenameRelativeFilePathCWD(self):
     """Correct path is returned when using relative name inside cwd."""
     original_wd = os.getcwd()
     os.chdir(self.tempdir)
-    processed_fn = powerlog_util.process_filename(self.filename)
+    processed_fn = powerlog.process_filename(self.filename)
     try:
       self.assertEqual(self.filepath, processed_fn)
     finally:
@@ -42,29 +42,29 @@ class TestPowerlog(unittest.TestCase):
 
   def test_ProcessFilenameLibDir(self):
     """Correct path is returned when using relative name inside LIBDIR."""
-    original_lib_dir = powerlog_util.LIB_DIR
-    powerlog_util.LIB_DIR = self.tempdir
-    processed_fn = powerlog_util.process_filename(self.filename)
+    original_lib_dir = powerlog.LIB_DIR
+    powerlog.LIB_DIR = self.tempdir
+    processed_fn = powerlog.process_filename(self.filename)
     try:
       self.assertEqual(self.filepath, processed_fn)
     finally:
-      powerlog_util.LIB_DIR = original_lib_dir
+      powerlog.LIB_DIR = original_lib_dir
 
   def test_ProcessFilenameRelativeFilePathPyFile(self):
-    """Correct path is returned when filename is in same dir as powerlog_util"""
-    original__file__ = powerlog_util.__file__
-    powerlog_util.__file__ = os.path.join(self.tempdir,
+    """Correct path is returned when filename is in same dir as powerlog"""
+    original__file__ = powerlog.__file__
+    powerlog.__file__ = os.path.join(self.tempdir,
                                           os.path.basename(original__file__))
-    processed_fn = powerlog_util.process_filename(self.filename)
+    processed_fn = powerlog.process_filename(self.filename)
     try:
       self.assertEqual(self.filepath, processed_fn)
     finally:
-      powerlog_util.__file__ = original__file__
+      powerlog.__file__ = original__file__
 
   def test_ProcessFilenameInvalid(self):
     """IOError is raised when file cannot be found by any of the four ways."""
     with self.assertRaises(IOError):
-      powerlog_util.process_filename(self.filename)
+      powerlog.process_filename(self.filename)
 
 if __name__ == '__main__':
   unittest.main()
