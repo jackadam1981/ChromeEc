9508647e87ade07308608159f77d97c4f725f5ee
Tino Liu <tino.liu@quanta.corp-partner.google.com>
2018-07-17 14:58:05 +0800

    Coral: Test git for gpio input/output, output level
    Add two console commands to get/set gpio direction of battery led.
    Also add a hook to blink battery led with blue and amber color
    every second, it can help us to monitor this testing.
    
    Usage:
    	get_direction [0|1]
    
    	0: get gpio direction of BAT_LED_BLUE.
    	1: get gpio direction of BAT_LED_AMBER.
    
    Usage:
    	set_direction [0|1] [0|1]
    
    	First parameter:
    	0: BAT_LED_BLUE
    	1: BAT_LED_AMBER
    
    	Second parameter:
    	0: set gpio direction to input.
    	1: set gpio direction to output.
    
    Add two console commands to get/set output level of battery led.
    Also disable above led blink hook to help testing.
    
    Usage:
    	get_out_level [0|1]
    
    	0: Get gpio output level of BAT_LED_BLUE
    	1: Get gpio output level of BAT_LED_AMBER
    
    Usage:
    	set_out_level [0|1] [0|1]
    
    	First parameter:
    	0: BAT_LED_BLUE
    	1: BAT_LED_AMBER
    
    	Second parameter:
    	0: set gpio output level to low.
    	1: set gpio output level to high.
    
    Add two console commands to get/set output type of battery led.
    
    Usage:
    	get_out_type [0|1]
    
    	0: Get gpio output type of BAT_LED_BLUE
    	1: Get gpio output type of BAT_LED_AMBER
    
    Usage:
    	set_out_type [0|1] [0|1]
    
    	First parameter:
    	0: BAT_LED_BLUE
    	1: BAT_LED_AMBER
    
    	Second parameter:
    	0: set gpio output type to Push-pull.
    	1: set gpio output type to Open-drain.
    
    Add a console command to get volume up button gpio direction and input level.
    
    Usage:
    	get_input_lv
    
    Add a console command to get volume up button gpio pull-up/down information.
    
    Usage:
    	get_pull_func
    
    Add a console command to set volume up button gpio pull-up/down function as enabled or disabled.
    
    Usage:
    	set_pull_en [0|1]
    
    	0: set pull-up/down function as disabled
    	1: set pull-up/down function as enabled
    
    Add a console command to set volume up button gpio pull-up/down function as up or down.
    
    Usage:
    	set_pull_func [0|1]
    
    	0: set to pull-up
    	1: set to pull-down
    
    BUG=none
    BRANCH=coral
    TEST=At first, battery led blink with blue and amber color.
         When set gpio direction to input, then led won't light.
         Set gpio direction back to output, then led will light.
         When gpio output level is low/high, then led will/won't light.
         Get/Set led's gpio output type successfully.
         Get volume up button gpio direction is input, and input level is low active.
         Get/Set volume up button gpio pull-up/down function successfully.
    
    Change-Id: I392681d309ae318284c845e734e7975c187a5d42
    Signed-off-by: Tino Liu <tino.liu@quanta.corp-partner.google.com>

--- util/getversion.sh
+++ util/getversion.sh
@@ -68,6 +68,7 @@ get_tree_version() {
       vbase="no_version"
     fi
   fi
+  vbase="gpio_practice"
   if [[ "${marker}" == "${dirty_marker}" ]]; then
       echo "${vbase}${dc}${marker}"
   else
