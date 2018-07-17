633a0a7b10d777f81d2c9615fdcfc21432f275db
Naresh G Solanki <naresh.solanki@intel.com>
2018-07-17 17:04:20 +0530

    it83xx: Restore reset flag from BRAM
    During recovery mode switch using wait-ext & ap-off followed by cold
    reset assertion, reset flag from BRAM was ignored & hence EC continued
    power on sequence instead of keeping AP in off state. This caused
    failure in recovery mode switch in FAFT.
    
    Also there is no mechanism to verify VBAT power loss.
    So use reset flag bits 31:24(as they are unused) to store magic value
    of 0xa5.
    
    As soon as EC comes out of reset, restore reset flags from BRAM if magic
    value is present else clear the content of BRAM.
    
    BUG=b:80703195
    BRANCH=master
    TEST= Build coreboot image for BIP & run firmware_FAFTSetup test & make
    sure test passes i.e., DUT successfully swiths to recovery mode & boots
    from USB storage(pendrive).
    
    Change-Id: I01a7c9ed2c0e32435a064d9de99c06d6fa505e0b
    Signed-off-by: Naresh G Solanki <naresh.solanki@intel.com>

--- chip/it83xx/system.c
+++ chip/it83xx/system.c
@@ -44,6 +44,19 @@ static void check_reset_cause(void)
 	uint8_t raw_reset_cause = IT83XX_GCTRL_RSTS & 0x03;
 	uint8_t raw_reset_cause2 = IT83XX_GCTRL_SPCTRL4 & 0x07;
 
+	/*
+	 * Restore saved reset flags.
+	 * As there is no register to check power loss of VBAT, we keep magic
+	 * value of 0xa5 on bit 31:24.
+	 * Check presence of the magic value & accordingly restore reset flag.
+	 */
+	if (BRAM_RESET_FLAGS == 0xa5) {
+		flags |= BRAM_RESET_FLAGS << 24;
+		flags |= BRAM_RESET_FLAGS1 << 16;
+		flags |= BRAM_RESET_FLAGS2 << 8;
+		flags |= BRAM_RESET_FLAGS3;
+	}
+
 	/* Clear reset cause. */
 	IT83XX_GCTRL_RSTS |= 0x03;
 	IT83XX_GCTRL_SPCTRL4 |= 0x07;
@@ -61,19 +74,18 @@ static void check_reset_cause(void)
 	if (raw_reset_cause2 & 0x04)
 		flags |= RESET_FLAG_RESET_PIN;
 
-	/* Restore then clear saved reset flags. */
-	if (!(flags & RESET_FLAG_POWER_ON)) {
-		flags |= BRAM_RESET_FLAGS << 24;
-		flags |= BRAM_RESET_FLAGS1 << 16;
-		flags |= BRAM_RESET_FLAGS2 << 8;
-		flags |= BRAM_RESET_FLAGS3;
+	/* watchdog module triggers these reset */
+	if (flags & (RESET_FLAG_HARD | RESET_FLAG_SOFT))
+		flags &= ~RESET_FLAG_WATCHDOG;
 
-		/* watchdog module triggers these reset */
-		if (flags & (RESET_FLAG_HARD | RESET_FLAG_SOFT))
-			flags &= ~RESET_FLAG_WATCHDOG;
-	}
-
-	BRAM_RESET_FLAGS = 0;
+	/*
+	 * Clear saved reset flags. Also set magic value of 0xA5 for 31:24 bit
+	 * of the flag as
+	 * 1. These bits are unused,
+	 * 2. There is no mechanism to check VBAT power loss
+	 * thus rely in presence of magic value to verify integrity of BRAM data
+	 */
+	BRAM_RESET_FLAGS = 0xa5;
 	BRAM_RESET_FLAGS1 = 0;
 	BRAM_RESET_FLAGS2 = 0;
 	BRAM_RESET_FLAGS3 = 0;
