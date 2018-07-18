4429e814523064a1097ca429839327d79c139d95
Tino Liu <tino.liu@quanta.corp-partner.google.com>
2018-07-18 17:25:19 +0800

    F75303: Set fake temperature
    Add new console command `f75303_fake` to set fake temperature for F75303.
    
    Usage:
    	`f75303_fake` [<index>] [<value>|off]
    
    	<index>
    		0: F75303_Local
    		1: F75303_Remote1
    		2: F75303_Remote2
    
    	<value>
    		integer from 0 to 100 (Celsius degree)
    
    	"off"
    		turn off fake temperature mode, back to normal
    
    BUG=none
    BRANCH=master
    TEST=fan target speed can follow fake temperature, make buildall pass
    
    Change-Id: I341434207af71cf6aa63350887eac3499508d2ff
    Signed-off-by: Tino Liu <tino.liu@quanta.corp-partner.google.com>

--- driver/temp_sensor/f75303.c
+++ driver/temp_sensor/f75303.c
@@ -10,10 +10,12 @@
 #include "i2c.h"
 #include "hooks.h"
 #include "util.h"
+#include "console.h"
 
 static int temp_val_local;
 static int temp_val_remote1;
 static int temp_val_remote2;
+static int fake_temp[F75303_IDX_COUNT] = {-1, -1, -1};
 
 /**
  * Read 8 bits register from temp sensor.
@@ -57,14 +59,60 @@ int f75303_get_val(int idx, int *temp_ptr)
 
 static void f75303_sensor_poll(void)
 {
-	get_temp(F75303_TEMP_LOCAL, &temp_val_local);
-	temp_val_local = C_TO_K(temp_val_local);
+	if (fake_temp[F75303_IDX_LOCAL] != -1) {
+		temp_val_local = C_TO_K(fake_temp[F75303_IDX_LOCAL]);
+	} else {
+		get_temp(F75303_TEMP_LOCAL, &temp_val_local);
+		temp_val_local = C_TO_K(temp_val_local);
+	}
 
-	get_temp(F75303_TEMP_REMOTE1, &temp_val_remote1);
-	temp_val_remote1 = C_TO_K(temp_val_remote1);
+	if (fake_temp[F75303_IDX_REMOTE1] != -1) {
+		temp_val_remote1 = C_TO_K(fake_temp[F75303_IDX_REMOTE1]);
+	} else {
+		get_temp(F75303_TEMP_REMOTE1, &temp_val_remote1);
+		temp_val_remote1 = C_TO_K(temp_val_remote1);
+	}
 
-	get_temp(F75303_TEMP_REMOTE2, &temp_val_remote2);
-	temp_val_remote2 = C_TO_K(temp_val_remote2);
+	if (fake_temp[F75303_IDX_REMOTE2] != -1) {
+		temp_val_remote2 = C_TO_K(fake_temp[F75303_IDX_REMOTE2]);
+	} else {
+		get_temp(F75303_TEMP_REMOTE2, &temp_val_remote2);
+		temp_val_remote2 = C_TO_K(temp_val_remote2);
+	}
 }
 DECLARE_HOOK(HOOK_SECOND, f75303_sensor_poll, HOOK_PRIO_TEMP_SENSOR);
+
+static int f75303_set_fake_temp(int argc, char **argv)
+{
+	int index;
+	int value;
+	char *e;
+
+	if (argc != 3)
+		return EC_ERROR_PARAM_COUNT;
+
+	index = strtoi(argv[1], &e, 0);
+	if ((*e) || (index < 0) || (index >= F75303_IDX_COUNT))
+		return EC_ERROR_PARAM1;
+
+	if (!strcasecmp(argv[2], "off")) {
+		fake_temp[index] = -1;
+		ccprintf("Turn off fake temp mode for sensor %u.\n", index);
+		return EC_SUCCESS;
+	}
+
+	value = strtoi(argv[2], &e, 0);
+
+	if ((*e) || (value < 0) || (value > 100))
+		return EC_ERROR_PARAM2;
+
+	fake_temp[index] = value;
+	ccprintf("Set fake temp %uC for sensor %u.\n", value, index);
+	ccprintf("New temp will be updated 1 sec later.\n\n");
+
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(f75303_fake, f75303_set_fake_temp,
+		"[<index>] [<value>|off]",
+		"Set fake temperature of sensor f75303.");
 
