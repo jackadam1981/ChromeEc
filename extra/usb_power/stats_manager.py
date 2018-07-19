951f4ab7ec56656640062fc475b1718b11fd8f1c
Ruben Rodriguez Buchillon <coconutruben@chromium.org>
2018-07-19 20:44:26 +0800

    stats_manager: accept_nan support
    This CL introduces a flag to StatsManager that allows for 'NaN' values
    to be recorded inside StatsManager. The motivation here is that if a
    sample fails to record it might be more desirable to record a 'NaN'
    than to just skip the sample, to keep timelines correct, and to not hide
    errors in the test-run.
    Also adds necessary tests for that behavior.
    
    BRANCH=None
    BUG=chromium:806146, chromium:760267
    TEST=unit tests still pass
    
    Change-Id: If17b7f52ba4a05e9e007c73bfa5d667fe36b74b3
    Signed-off-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>

--- extra/usb_power/stats_manager.py
+++ extra/usb_power/stats_manager.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 import collections
 import json
 import logging
+import math
 import numpy
 import os
 
@@ -67,7 +68,8 @@ class StatsManager(object):
     CalculateStats() is called.
   """
 
-  def __init__(self, smid='', title='', hide_domains=[], order=[]):
+  def __init__(self, smid='', title='', hide_domains=[], order=[],
+               accept_nan=True):
     """Initialize infrastructure for data and their statistics."""
     self._title = title
     self._data = collections.defaultdict(list)
@@ -76,6 +78,7 @@ class StatsManager(object):
     self._order = order
     self._hide_domains = hide_domains
     self._summary = {}
+    self._accept_nan = accept_nan
     self._logger = logging.getLogger('StatsManager')
 
   def AddSample(self, domain, value):
@@ -85,13 +88,16 @@ class StatsManager(object):
       domain: the domain name for the value.
       value: one time sample for domain, expect type float.
     """
-    if isinstance(value, int):
+    try:
       value = float(value)
-    if isinstance(value, float):
-      self._data[domain].append(value)
-      return
-    self._logger.warn('value %s for domain %s is not a number, thus ignored.' %
-                      (value, domain))
+    except ValueError:
+      # if we don't accept nan this will be caught below
+      value = float('NaN')
+      self._logger.debug('value %s for domain %s is not a number. Making NaN'
+                         % (value, domain))
+    if not self._accept_nan and math.isnan(value):
+      raise StatsManagerError('accept_nan is false. Cannot add NaN value.')
+    self._data[domain].append(value)
 
   def SetUnit(self, domain, unit):
     """Set the unit for a domain.
@@ -117,10 +123,10 @@ class StatsManager(object):
     for domain, data in self._data.iteritems():
       data_np = numpy.array(data)
       self._summary[domain] = {
-          'mean': data_np.mean(),
-          'min': data_np.min(),
-          'max': data_np.max(),
-          'stddev': data_np.std(),
+          'mean': numpy.nanmean(data_np),
+          'min': numpy.nanmin(data_np),
+          'max': numpy.nanmax(data_np),
+          'stddev': numpy.nanstd(data_np),
           'count': data_np.size,
       }
 
