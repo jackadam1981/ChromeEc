057e6dc8330d95001be976a38b8751e6f9ff9379
Jett Rink <jettrink@chromium.org>
2018-07-27 15:14:21 -0600

    ps8751: add low power mode for PS8751 when only MUX
    Enter lower power mode for PS8751 when acting as mux only.
    
    BRANCH=none
    BUG=b:111664205,b:111876407,b:10937880
    TEST=PS8751 as MUX on C1 Bip goes into low power mode after disconnect.
    
    Change-Id: Ifac3b76556069e1e5f6acae550d8076c29d5f1cd
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- driver/tcpm/tcpci.c
+++ driver/tcpm/tcpci.c
@@ -587,10 +587,59 @@ int tcpci_tcpm_release(int port)
 
 #ifdef CONFIG_USB_PD_TCPM_MUX
 
+#ifdef CONFIG_USB_PD_TCPM_TCPCI_MUX_ONLY
+
+/*
+ * Since the TCPC/MUX device is only used for the MUX, we need to initialize it
+ * via mux init because tcpc_init won't run for the device. This is borrowed
+ * from tcpc_init.
+ */
+int tcpci_tcpm_mux_init(int i2c_port_addr)
+{
+	int error;
+	int power_status;
+	int tries = 30;
+
+	/* Wait for the device to exit low power state */
+	while (1) {
+		error = i2c_read8(MUX_PORT(i2c_port_addr),
+				  MUX_ADDR(i2c_port_addr),
+				  TCPC_REG_POWER_STATUS, &power_status);
+		/*
+		 * If read succeeds and the uninitialized bit is clear, then
+		 * initialization is complete.
+		 */
+		if (!error && !(power_status & TCPC_REG_POWER_STATUS_UNINIT))
+			break;
+		else if (error && --tries == 0)
+			return error;
+		msleep(10);
+	}
+
+	/* Turn off all alerts and acknowledge any pending IRQ */
+	error |= i2c_write16(MUX_PORT(i2c_port_addr), MUX_ADDR(i2c_port_addr),
+			     TCPC_REG_ALERT_MASK, 0);
+	error |= i2c_write16(MUX_PORT(i2c_port_addr), MUX_ADDR(i2c_port_addr),
+			     TCPC_REG_ALERT, 0xffff);
+
+	return error ? EC_ERROR_UNKNOWN : EC_SUCCESS;
+}
+
+static int tcpci_tcpm_mux_enter_low_power(int i2c_port_addr)
+{
+	return i2c_write8(MUX_PORT(i2c_port_addr), MUX_ADDR(i2c_port_addr),
+			  TCPC_REG_COMMAND, TCPC_REG_COMMAND_I2CIDLE);
+}
+
+#else /* !CONFIG_USB_PD_TCPM_TCPCI_MUX_ONLY */
+
+/* Nothing to init for mux since TCPC init will take care of it. */
 int tcpci_tcpm_mux_init(int i2c_addr)
 {
 	return EC_SUCCESS;
 }
+
+#endif /* CONFIG_USB_PD_TCPM_TCPCI_MUX_ONLY */
 
 int tcpci_tcpm_mux_set(int i2c_port_addr, mux_state_t mux_state)
 {
@@ -656,11 +705,18 @@ int tcpci_tcpm_mux_get(int i2c_port_addr, mux_state_t *mux_state)
 	return EC_SUCCESS;
 }
 
-
 const struct usb_mux_driver tcpci_tcpm_usb_mux_driver = {
-	.init = tcpci_tcpm_mux_init,
-	.set = tcpci_tcpm_mux_set,
-	.get = tcpci_tcpm_mux_get,
+	.init = &tcpci_tcpm_mux_init,
+	.set = &tcpci_tcpm_mux_set,
+	.get = &tcpci_tcpm_mux_get,
+#ifdef CONFIG_USB_PD_TCPM_TCPCI_MUX_ONLY
+	/*
+	 * This method is only needed when the TCPC/MUX device is acting solely
+	 * as a MUX (and not the TCPC). In that case, we need to put the device
+	 * into LPM via the mux path since the TCPC path won't do it for us.
+	 */
+	.enter_low_power_mode = &tcpci_tcpm_mux_enter_low_power,
+#endif
 };
 
 #endif /* CONFIG_USB_PD_TCPM_MUX */
