a071eb70d7f89f56bd892c6a34376293c6ddc94c
Kyoung Kim <kyoung.il.kim@intel.com>
2018-07-27 17:56:12 -0700

    ISH: add FPU context save/restore
    Add FPU context save/restore for scheduling next task.
    
    Change-Id: Id3c5ff1c9a6b3702a27b8ffc5f6a825877671ce4
    Signed-off-by: Kyoung Kim <kyoung.il.kim@intel.com>

--- core/minute-ia/irq_handler.h
+++ core/minute-ia/irq_handler.h
@@ -28,6 +28,7 @@ struct irq_data {
 #define DECLARE_IRQ(irq, routine) DECLARE_IRQ_(irq, routine, irq + 32 + 10)
 /* Each irq has a irq_data structure placed in .rodata.irqs section,
  * to be used for dynamically setting up interrupt gates */
+#ifndef CONFIG_FPU
 #define DECLARE_IRQ_(irq, routine, vector)				\
 	void __keep routine(void);					\
 	void IRQ_HANDLER(irq)(void); 					\
@@ -58,5 +59,38 @@ struct irq_data {
 			"popa\n"					\
 			"iret\n"					\
 		);
-
+#else
+#define DECLARE_IRQ_(irq, routine, vector)				\
+	void __keep routine(void);					\
+	void IRQ_HANDLER(irq)(void); 					\
+	__asm__ (".section .rodata.irqs\n");				\
+	const struct irq_data __keep CONCAT4(__irq_, irq, _, routine)	\
+	__attribute__((section(".rodata.irqs")))= {IRQ_HANDLER(irq), irq};\
+	__asm__ (							\
+		".section .text._irq_"#irq"_handler\n"			\
+		"_irq_"#irq"_handler:\n"				\
+			"pusha\n"					\
+			"add  $1, __in_isr\n"				\
+			"call "#routine"\n"				\
+			"push $0\n"					\
+			"push $0\n"					\
+			"call switch_handler\n"				\
+			"addl $0x08, %esp\n"				\
+			"test %eax, %eax\n"				\
+			"jz 1f\n"					\
+			"movl current_task, %eax\n"			\
+			"fnsave 20(%eax)\n"				\
+			"movl %esp, (%eax)\n"				\
+			"movl next_task, %eax\n"			\
+			"movl %eax, current_task\n"			\
+			"movl (%eax), %esp\n"				\
+			"frstor 20(%eax)\n"				\
+			"1:\n"						\
+			"movl $"#vector ", (0xFEC00040)\n"              \
+			"sub  $1, __in_isr\n"				\
+			"movl $0x00, (0xFEE000B0)\n" 			\
+			"popa\n"					\
+			"iret\n"					\
+		);
+#endif  /* CONFIG_FPU */
 #endif  /* __CROS_EC_IRQ_HANDLER_H */
