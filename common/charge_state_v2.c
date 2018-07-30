6392f5fff954f0c28d694b5ddfb49449e92f6860
Daisuke Nojiri <dnojiri@chromium.org>
2018-07-30 16:44:06 -0700

    chgstv2: Notify HOOK_BATTERY_SOC_CHANGE on battery removal
    Currently, when a battery isn't present when charger_task starts, the
    state machine doesn't trigger HOOK_BATTERY_SOC_CHANGE hooks. This causes
    Nami's LED driver to miss BP_NO state after sysjump.
    
    This patch makes charger_task call hook_notify when BP_NO is detected
    first time.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b:111969858
    BRANCH=none
    TEST=Verify battery & power LEDs reflect correct BP_NO status on Sona
    when a battery is disconnected on boot.
    
    Change-Id: I23bd7446dfacbf33f5719b7da567d07ef79788cc

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -1633,6 +1633,12 @@ void charger_task(void *u)
 				CPRINTS("running with no battery and no AC");
 			set_charge_state(ST_IDLE);
 			curr.batt_is_charging = 0;
+			if (!battery_was_removed) {
+				/* This is needed to make charge_get_state
+				 * show correct status to the hook handlers. */
+				battery_was_removed = 1;
+				hook_notify(HOOK_BATTERY_SOC_CHANGE);
+			}
 			battery_was_removed = 1;
 			goto wait_for_it;
 		}
