8bcb0e40ac5a29977a413cc5121ba0de4cefb123
Andrey Pronin <apronin@chromium.org>
2018-08-01 20:57:14 -0700

    cr50: implement _plat__ShallKeepSpecialIndex
    To persist some special indexes over TPM owner clear,
    implement a _plat__ShallKeepSpecialIndex callback that
    tells TPM2 stack which indexes shall be retained and
    how their attributes shall be modified while performing
    TPM2_Clear.
    
    BRANCH=cr50
    BUG=b:112099050
    TEST=1) Create FWMP.
            cryptohome --action=tpm_take_ownership [and wait]
            cryptohome --action=set_firmware_management_parameters \
                       --flags=0xbb00
         2) Clear the owner.
            crossystem clear_tpm_owner_request=1
            reboot
         3) Verify that FWMP still exists, but cannot be overwritten.
            cryptohome --action=get_firmware_management_parameters
            (but the following fails:)
            cryptohome --action=set_firmware_management_parameters \
                       --flags=0xcc00
            cryptohome --action=remove_firmware_management_parameters
         4) Verify that it can be written/removed after taking ownership.
            cryptohome --action=tpm_take_ownership [and wait]
            cryptohome --action=set_firmware_management_parameters \
                       --flags=0xdd00
            cryptohome --action=remove_firmware_management_parameters
    
    Change-Id: I3236d61adbfb7665c0a4bfc1a7addeaa1f0eb3ee
    Signed-off-by: Andrey Pronin <apronin@chromium.org>

--- board/cr50/tpm2/platform.c
+++ board/cr50/tpm2/platform.c
@@ -8,6 +8,7 @@
 
 #include "ccd_config.h"
 #include "pinweaver.h"
+#include "tpm_nvmem.h"
 #include "trng.h"
 #include "util.h"
 #include "version.h"
@@ -82,4 +83,16 @@ void _plat__StartupCallback(void)
 	 * so we don't yet need to tell CCD that a real TPM startup has
 	 * occurred.
 	 */
+}
+
+BOOL _plat__ShallKeepSpecialIndex(uint32_t  index,
+				  uint32_t *attrClear,
+				  uint32_t *attrSet)
+{
+	if (index == HR_NV_INDEX + FWMP_NV_INDEX) {
+		*attrClear = (1 << 11); /* WRITELOCKED */
+		*attrSet = 0;
+		return TRUE;
+	}
+	return FALSE;
 }
