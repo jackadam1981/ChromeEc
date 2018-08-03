089eb78ab68612fd2d652034999e1b4884342865
Jonathan Brandmeyer <jbrandmeyer@chromium.org>
2018-08-03 13:24:06 -0600

    battery: Optionally prevent boot at low SOC with cell imbalance.
    Measure the cell imbalance with method dispatch to a per-battery
    method, such that families with a plurality of configurable batteries
    can support them all.  By default, cell imbalance is taken to be 'zero'
    in case we don't support that battery's management IC.  Provide a driver
    for reading cell voltages for the TI BQ4050 family.  This IC is quite
    popular, but by no means universal.
    
    BUG=b:111214767
    BRANCH=none
    TEST=Boot on Careena with a custom debug print statement, showing that
    we can measure the typical battery voltages during and after the boot at
    the battery status polling interval.
    
    Change-Id: I235389b252ac9c373aa9706dbd1066f7c0bbce71
    Signed-off-by: Jonathan Brandmeyer <jbrandmeyer@chromium.org>

--- include/config.h
+++ include/config.h
@@ -266,6 +266,7 @@
 #undef CONFIG_BATTERY_BQ20Z453
 #undef CONFIG_BATTERY_BQ27541
 #undef CONFIG_BATTERY_BQ27621
+#undef CONFIG_BATTERY_BQ4050
 #undef CONFIG_BATTERY_MAX17055
 
 /* Compile mock battery support; used by tests. */
@@ -396,6 +397,12 @@
  * Number of batteries, only matters when CONFIG_BATTERY_V2 is used.
  */
 #undef CONFIG_BATTERY_COUNT
+
+/*
+ * Smart battery driver should measure the voltage cell imbalance in the battery
+ * pack.  This requires a battery driver capable of the measurement.
+ */
+#undef CONFIG_BATTERY_MEASURE_IMBALANCE
 
 /*
  * Expose some data when it is needed.
@@ -694,6 +701,9 @@
 
 /* Minimum charger power (in mW) required for powering on. */
 #undef CONFIG_CHARGER_MIN_POWER_MW_FOR_POWER_ON
+
+/* Minimum battery percentage for power on with an imbalanced pack */
+#undef CONFIG_CHARGER_MIN_BAT_PCT_IMBALANCED_POWER_ON
 
 /* Set this option when using a Narrow VDC (NVDC) charger, such as ISL9237/8. */
 #undef CONFIG_CHARGER_NARROW_VDC
@@ -3663,6 +3673,7 @@
 #if defined(CONFIG_BATTERY_BQ20Z453) || \
 	defined(CONFIG_BATTERY_BQ27541) || \
 	defined(CONFIG_BATTERY_BQ27621) || \
+	defined(CONFIG_BATTERY_BQ4050) || \
 	defined(CONFIG_BATTERY_MAX17055) || \
 	defined(CONFIG_BATTERY_SMART)
 #define CONFIG_BATTERY
@@ -3789,6 +3800,10 @@
 #define CONFIG_CHARGER_MIN_POWER_MW_FOR_POWER_ON 15000
 #endif /* !defined(CONFIG_CHARGER_MIN_POWER_MW_FOR_POWER_ON) */
 #endif /* defined(HAS_TASK_CHIPSET) */
+
+#ifndef CONFIG_CHARGER_MIN_BAT_PCT_IMBALANCED_POWER_ON
+#define CONFIG_CHARGER_MIN_BAT_PCT_IMBALANCED_POWER_ON 5
+#endif
 
 #ifndef HAS_TASK_KEYPROTO
 #undef CONFIG_KEYBOARD_PROTOCOL_8042
