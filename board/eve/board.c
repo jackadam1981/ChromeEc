80d7c46793bb2c878b7c7844bb01fbad28aa4b5b
Hung-Te Lin <hungte@chromium.org>
2018-08-07 12:55:15 +0800

    eve: Support dynamic mapping for legacy layout.
    For Eve, we want to enable dynamic mapping with few layout changes:
    
    1. Map ASSIST to WIN(Left) since it's having a similar location.
    2. Map MENU to APP.
    3. Map SEARCH to Fn.
    4. When Fn is hold, remap F1~F10 to similar functions as in Chrome OS.
    5. When Fn is hold, remap 1~10 and arrow keys to those available on
       typical 101-Keyboards and not available on Chrome OS keyboards.
    
    BUG=b:72200093
    BRANCH=eve
    TEST=Builds and boot properly.
    
    Change-Id: I0b02c709c780b468bc9bd70c053e85a07733ac50
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- board/eve/board.c
+++ board/eve/board.c
@@ -60,6 +60,11 @@
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
+/*
+ * Initialize keyboard mapping type.
+ */
+__attribute__((weak)) void board_init_keyboard_mapping(int reset) {}
+
 static void tcpc_alert_event(enum gpio_signal signal)
 {
 	if ((signal == GPIO_USB_C0_PD_INT_ODL) &&
@@ -465,7 +470,6 @@ int board_has_working_reset_flags(void)
 	/* All other board versions should have working reset flags */
 	return 1;
 }
-
 /*
  * Update status of the ACPRESENT pin on the PCH.  In order to prevent
  * Deep S3 when USB is inserted this will indicate that AC is present
@@ -505,6 +509,8 @@ static void board_init(void)
 		scancode_set2[3][9] = 0xe007;
 	}
 #endif
+
+	board_init_keyboard_mapping(0);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
@@ -686,6 +692,7 @@ static void board_chipset_startup(void)
 	/* Enable Trackpad */
 	gpio_set_level(GPIO_TRACKPAD_SHDN_L, 1);
 	hook_call_deferred(&enable_input_devices_data, 0);
+	board_init_keyboard_mapping(0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup, HOOK_PRIO_DEFAULT);
 
@@ -697,6 +704,7 @@ static void board_chipset_shutdown(void)
 	dsp_wake_enable(0);
 	gpio_set_level(GPIO_TRACKPAD_SHDN_L, 0);
 	hook_call_deferred(&enable_input_devices_data, 0);
+	board_init_keyboard_mapping(0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown, HOOK_PRIO_DEFAULT);
 
@@ -727,6 +735,7 @@ DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_chipset_resume, HOOK_PRIO_DEFAULT);
 static void board_chipset_reset(void)
 {
 	board_report_pmic_fault("CHIPSET RESET");
+	board_init_keyboard_mapping(1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESET, board_chipset_reset, HOOK_PRIO_DEFAULT);
 
