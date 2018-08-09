47f4bb3d9779479bf71d37fcd2e87cbb861e2721
Hung-Te Lin <hungte@chromium.org>
2018-08-09 11:45:15 +0800

    keyboard: Add CONFIG_KEYBOARD_SCANCODE_CALLBACK for board-specific hooks.
    Many devices may want board-specific keyboard hooks, for example
    allowing easter egg (to replace the key_special today), or to provide
    dynamic translation. Both can be done by having a callback whenever the
    key state is changed (after scancode is found).
    
    The new CONFIG_KEYBOARD_SCANCODE_CALLBACK allows boards to define their
    own hook keyboard_scancode_callback so the keystrokes can be either
    changed or monitored.
    
    BUG=b:72200093
    TEST=make buildall -j
    BRANCH=eve
    
    Change-Id: I02e3bf5c217b2f30b942d96ecb2c493ce200638f
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- include/keyboard_8042.h
+++ include/keyboard_8042.h
@@ -8,6 +8,8 @@
 #ifndef __CROS_EC_KEYBOARD_8042_H
 #define __CROS_EC_KEYBOARD_8042_H
 
+#include <stddef.h>
+
 #include "common.h"
 #include "button.h"
 
@@ -27,5 +29,20 @@ void button_state_changed(enum keyboard_button_type button, int is_pressed);
  * @param is_cmd        Is byte command (!=0) or data (0)
  */
 void keyboard_host_write(int data, int is_cmd);
+
+/*
+ * Board specific callback function when a key state is changed.
+ *
+ * A board may watch key events and create some easter eggs, or apply dynamic
+ * translation to the make code (i.e., remap keys).
+ *
+ * Returning EC_SUCCESS implies the make points to a valid make code and should
+ * be processed. Any other failure will abort key processing.
+ *
+ * @param make_code	Pointer to scan code (set 2) of key in action.
+ * @param pressed	Is the key being pressed (1) or released (0).
+ */
+enum ec_error_list keyboard_scancode_callback(uint16_t *make_code,
+					      int8_t pressed);
 
 #endif  /* __CROS_EC_KEYBOARD_8042_H */
