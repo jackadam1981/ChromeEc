b61666f610f981e1098b9187131eca1682d03252
Jett Rink <jettrink@chromium.org>
2018-08-09 12:07:21 -0600

    bip: put ite5202 mux into low power mode
    When the SS MUX is fully disconnected, put the mux into a low power
    state.
    
    BRANCH=none
    BUG=b:111006203,b:111520593
    TEST=BIP PD_A rail went from 3.97mW down to 2.65 mW during hibernate
    with the other corresponding PPC changes in CL:1166184
    
    Change-Id: Ief636b569fe01db7f129ca2d457352c3799c14f9
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- baseboard/octopus/variant_usbc_ec_tcpcs.c
+++ baseboard/octopus/variant_usbc_ec_tcpcs.c
@@ -73,6 +73,7 @@ struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_COUNT] = {
 		.port_addr = IT5205_I2C_ADDR1,
 		.driver = &it5205_usb_mux_driver,
 		.hpd_update = &board_it83xx_hpd_status,
+		.enter_low_power_mode = &it5205_enter_low_power_mode,
 	},
 	[USB_PD_PORT_ITE_1] = {
 		/* Use PS8751 as mux only */
