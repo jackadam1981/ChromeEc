5510a00a105eb78686313aab978e926a53634346
Wai-Hong Tam <waihong@google.com>
2018-08-17 09:27:55 -0700

    cheza: Don't need to set the switchcap register on init
    The switchcap register should be OTP'ed.
    
    BRANCH=none
    BUG=b:77957956
    TEST=On the rev-2 board, power cycle the board, check the switchcap
    register having a proper value.
    
    Change-Id: I558301772404618a1d189a567ea61e743f403e84
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

--- board/cheza/board.c
+++ board/cheza/board.c
@@ -52,9 +52,6 @@ static void anx74xx_cable_det_interrupt(enum gpio_signal signal);
 
 #include "gpio_list.h"
 
-/* 8-bit I2C address */
-#define DA9313_I2C_ADDR		0xd0
-
 /* GPIO Interrupt Handlers */
 static void tcpc_alert_event(enum gpio_signal signal)
 {
@@ -246,12 +243,6 @@ BUILD_ASSERT(ARRAY_SIZE(pi3usb9281_chips) ==
 /* Initialize board. */
 static void board_init(void)
 {
-	/*
-	 * Disable SwitchCap auto-boot and make EN pin level-trigger
-	 * TODO(b/77957956): Remove it after hardware fix.
-	 */
-	i2c_write8(I2C_PORT_POWER, DA9313_I2C_ADDR, 0x02, 0x34);
-
 	/* Enable BC1.2 VBUS detection */
 	gpio_enable_interrupt(GPIO_USB_C0_VBUS_DET_L);
 	gpio_enable_interrupt(GPIO_USB_C1_VBUS_DET_L);
