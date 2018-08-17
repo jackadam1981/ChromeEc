c39522acb4c8d039d3131d86b7051ebf755278e2
Jett Rink <jettrink@chromium.org>
2018-08-17 13:34:43 -0600

    octopus: use higher priority TCPC interrupt tasks
    See go/usb-pd-slow-response-time for more information
    
    BRANCH=none
    BUG=b:112088135
    TEST=CL stack on fleex and bobba consistently meet PD timing spec
    
    Change-Id: I9eabf8de8d866f5a0af7d1daba5ab585b418d26c
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- board/phaser/board.c
+++ board/phaser/board.c
@@ -35,14 +35,13 @@ static uint16_t sku_id;
 
 static void tcpc_alert_event(enum gpio_signal signal)
 {
+	const int port = (signal == GPIO_USB_C1_MUX_INT_ODL);
+
 	if ((signal == GPIO_USB_C1_MUX_INT_ODL) &&
 	    !gpio_get_level(GPIO_USB_C1_PD_RST_ODL))
 		return;
 
-#ifdef HAS_TASK_PDCMD
-	/* Exchange status with TCPCs */
-	host_command_pd_send_status(PD_CHARGE_NO_CHANGE);
-#endif
+	schedule_deferred_pd_interrupt(port);
 }
 
 static void ppc_interrupt(enum gpio_signal signal)
