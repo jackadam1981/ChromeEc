4f8701f000ee97e17d24d299ba76ee05bc142a78
Mary Ruthven <mruthven@google.com>
2018-08-17 17:57:03 -0700

    cr50: only enable rbox wakeups during sleep
    Enable rbox wakeups before entering any form of sleep. Disable them
    immediately on resume. Without rbox wakeups enabled during normal
    operation, we don't need to worry about clearing them after every rbox
    interrupt. In TOT we missed clearing the power button rbox wakeup. This
    was causing cr50 to wake up immediately after entering regular sleep. It
    caused a ton of pmu interrupts and prevented cr50 from staying asleep.
    With this change cr50 enters enters sleep and deep sleep normally. It
    only resumes when there's a real wakeup.
    
    BUG=none
    BRANCH=cr50
    TEST=run firmware_Cr50DeviceState with TOT
    
    Change-Id: I56bf81c19a6e32750dc9d21be7f27188635dd662
    Signed-off-by: Mary Ruthven <mruthven@google.com>

--- chip/g/idle.c
+++ chip/g/idle.c
@@ -97,6 +97,12 @@ static void prepare_to_sleep(void)
 		GC_PMU_EXITPD_MASK_TIMELS0_PD_EXIT_TIMER0_MASK |
 		GC_PMU_EXITPD_MASK_TIMELS0_PD_EXIT_TIMER1_MASK;
 
+	/*
+	 * Enable RBOX wakeups. They will immediately be disabled on resume in
+	 * rbox_init or pmu_wakeup_interrupt.
+	 */
+	GREG32(RBOX, WAKEUP) = GC_RBOX_WAKEUP_ENABLE_MASK;
+
 	if (utmi_wakeup_is_enabled() && idle_action != IDLE_DEEP_SLEEP)
 		GR_PMU_EXITPD_MASK |=
 			GC_PMU_EXITPD_MASK_UTMI_SUSPEND_N_MASK;
