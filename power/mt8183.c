6ef7574b360ad0f64927e889a0c4b6b84aacf1a4
Nicolas Boichat <drinkcat@chromium.org>
2018-08-22 15:54:46 +0800

    HACK: kukui: hacks and fixups to allow using MTK preloader
    Enable real eMMC (but switch back to EC to prevent leakage in G3).
    
    Reset PMIC on boot so that DRAM calibration does not fail.
    
    Also, fake signals so that EC knows that AP is in S0, and not S3.
    
    BRANCH=none
    BUG=none
    TEST=Boot kukui
    
    Change-Id: I65a7af205cf80c3f79a1d279e065f33d2e99a42f
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- power/mt8183.c
+++ power/mt8183.c
@@ -252,6 +252,10 @@ enum power_state power_handle_state(enum power_state state)
 		}
 
 		booted = 1;
+
+		/* HACK: Force eMMC switch to real eMMC */
+		gpio_set_flags(GPIO_BOOTBLOCK_EN_L, GPIO_OUT_HIGH);
+
 		/* Enable S3 power supplies, release AP reset. */
 		power_seq_run(s5s3_power_seq, ARRAY_SIZE(s5s3_power_seq));
 
@@ -315,6 +319,9 @@ enum power_state power_handle_state(enum power_state state)
 		hook_notify(HOOK_CHIPSET_SHUTDOWN);
 
 		power_seq_run(s3s5_power_seq, ARRAY_SIZE(s3s5_power_seq));
+
+		/* HACK: Force eMMC switch to float again */
+		gpio_set_flags(GPIO_BOOTBLOCK_EN_L, GPIO_ODR_HIGH);
 
 		/* Start shutting down */
 		return POWER_S5;
