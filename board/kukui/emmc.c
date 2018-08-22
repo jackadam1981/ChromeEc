0bdf3f21600faaa6a6e83f93826f333f862df599
Nicolas Boichat <drinkcat@chromium.org>
2018-08-22 15:54:46 +0800

    HACK: kukui: hacks and fixups to allow using MTK preloader
    Enable real eMMC (but switch back to EC to prevent leakage in G3).
    
    Reset PMIC on boot so that DRAM calibration does not fail.
    
    Also, fake signals so that EC knows that AP is in S0, and not S3.
    
    BRANCH=none
    BUG=none
    TEST=Boot kukui
    
    Change-Id: I65a7af205cf80c3f79a1d279e065f33d2e99a42f
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- board/kukui/emmc.c
+++ board/kukui/emmc.c
@@ -241,7 +241,8 @@ DECLARE_DEFERRED(emmc_check_status);
 
 static void emmc_enable_spi(void)
 {
-	if (spi_enabled)
+	/* HACK: eMMC emulation disabled. */
+	if (spi_enabled || 1)
 		return;
 
 	disable_sleep(SLEEP_MASK_EMMC);
@@ -293,6 +294,11 @@ void emmc_task(void *u)
 	enum emmc_cmd cmd;
 	/* Are we currently transmitting data? */
 	int tx = 0;
+
+
+	/* HACK: eMMC emulation disabled. */
+	while (1)
+		task_wait_event(-1);
 
 	rxdma = dma_get_channel(STM32_DMAC_SPI_EMMC_RX);
 
