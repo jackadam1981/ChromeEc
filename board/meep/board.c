dcb493c6fc8f8a0d9dc4e487ec1a96529d7833f8
Devin Lu <devin.lu@quantatw.com>
2018-08-22 15:49:16 +0800

    meep: correct charger driver including
    meep is using isl9238.
    
    BUG=none
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I0c561618ec6f511443d96a4a64de6b0c032c97e8
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>

--- board/meep/board.c
+++ board/meep/board.c
@@ -16,7 +16,7 @@
 #include "driver/accel_kionix.h"
 #include "driver/accelgyro_lsm6dsm.h"
 #include "driver/bc12/bq24392.h"
-#include "driver/charger/bd9995x.h"
+#include "driver/charger/isl923x.h"
 #include "driver/ppc/nx20p348x.h"
 #include "driver/tcpm/anx7447.h"
 #include "driver/tcpm/ps8xxx.h"
