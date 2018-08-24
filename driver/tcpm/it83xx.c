dec510a08703ae0b3b61c5a752f9d132eb4f7c99
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2018-08-24 16:23:04 +0800

    tcpm:it83xx: code modify for GRL-USB-PD test items
    1.Structured VDM:In VIF file we announce that pd port of UUT
    support discover ID, SVID, and mode commands. So add those
    functions and can pass in GRL test item.
    
    2.According to PD3.0 spec, when SRC pd port receive
    GET_SRC_CAP in SRC_READY state, we should send SRC_CAP
    and transit to SRC_NEGOCIATE state for waiting power
    request from SNK. With this modify it can pass
    GRL-USB-PD compliance TDA2.2.9 test item.
    
    3.BMC Rx threshold:According to PD3.0 spec, we should set
    different Rx threshold in SRC or SNK role for well
    performance of receiving data. So we add these settings
    and can pass GRL-USB-PD compliance TDA2.1.2.1 test item.
    
    4.BIST:In BIST test data mode, FW may not handle routines
    in time. So once enter this mode, Hw don't interrupt Fw
    when Rx receive packet. But it will cause that our pd port
    don't respond received packet(except GoodCRC) when port partner
    re-connect in this mode. So we reset our pd port policy engine,
    when disconnect. Then it can pass in GRL-USB-PD compliance
    TDA2.1.2.2 test item.
    
    <p.s>
    5.DR swap fail => in RO can't do that
    
    6.MsgID repeat fail => common code
    
    BUG=none
    BRANCH=none
    TEST=GRL-USB-PD compliance test.
    
    Change-Id: Ic866fe9b0e96d8156fc3f61a1317c80b3aaba7bb
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- driver/tcpm/it83xx.c
+++ driver/tcpm/it83xx.c
@@ -16,6 +16,7 @@
 #include "util.h"
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
+#include "hooks.h"
 
 #if defined(CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE) || \
 	defined(CONFIG_USB_PD_VBUS_DETECT_TCPC) || \
@@ -502,6 +503,8 @@ static int it83xx_tcpm_set_rx_enable(int port, int enable)
 	} else {
 		IT83XX_USBPD_IMR(port) |= USBPD_REG_MASK_MSG_RX_DONE;
 		USBPD_DISABLE_BMC_PHY(port);
+		/* exit BIST test data mode */
+		//USBPD_SW_RESET(port);
 	}
 
 	/* If any PD port is connected, then disable deep sleep */
@@ -574,6 +577,23 @@ static int it83xx_tcpm_get_chip_info(int port, int renew,
 
 	return EC_SUCCESS;
 }
+
+static void it83xx_tcpm_sw_reset(void)
+{
+	int port = TASK_ID_TO_PD_PORT(task_get_current());
+	/* exit BIST test data mode */
+	USBPD_SW_RESET(port);
+}
+
+//DECLARE_HOOK(HOOK_INIT, adc_init, HOOK_PRIO_INIT_ADC);
+DECLARE_HOOK(HOOK_USB_PD_DISCONNECT, it83xx_tcpm_sw_reset, HOOK_PRIO_DEFAULT);
+							   //HOOK_PRIO_FIRST
+
+//static void it83xx_tcpm_cb_sw_reset(port, int (*callback) (int, int))
+//{
+//	/* exit BIST test data mode */
+//	USBPD_SW_RESET(port);
+//}
 
 const struct tcpm_drv it83xx_tcpm_drv = {
 	.init			= &it83xx_tcpm_init,
