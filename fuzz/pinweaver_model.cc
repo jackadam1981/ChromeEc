6639f95ebff245425ce61dc7809d4185fe6b29ae
Allen Webb <allenwebb@google.com>
2018-08-28 16:48:02 -0700

    cr50_fuzz: Add a custom mutator for degrading valid messages to bytes.
    This requires an update to libprotobuf-mutator that adds support for custom
    mutations. It adds a mutation that converts valid pinweaver messages to
    random byte strings to allow the fuzzer more flexibility while still
    providing the scaffolding necessary to gain non-trivial coverage.
    
    CQ-DEPEND=CL:1194652
    BRANCH=none
    BUG=chromium:876582
    TEST=make -j buildfuzztests && ./build/host/cr50_fuzz/cr50_fuzz.exe
    
    Change-Id: I65f6fd2e0e6eba07e4120c17394c78309551b27f
    Signed-off-by: Allen Webb <allenwebb@google.com>

--- fuzz/pinweaver_model.cc
+++ fuzz/pinweaver_model.cc
@@ -39,8 +39,17 @@ struct pw_request_t* SerializeCommon(const fuzz::PinWeaver& pinweaver,
 // Public member functions.
 //******************************************************************************
 
+PinweaverModel* PinweaverModel::Get() {
+  return default_instance_;
+}
+
 PinweaverModel::PinweaverModel() {
   Reset();
+  default_instance_ = this;
+}
+
+PinweaverModel::~PinweaverModel() {
+  default_instance_ = nullptr;
 }
 
 void PinweaverModel::SendBuffer(fuzz::span<uint8_t> buffer) {
@@ -132,6 +141,8 @@ void PinweaverModel::Reset() {
 //******************************************************************************
 
 constexpr uint8_t PinweaverModel::kNullRootHash[PW_HASH_SIZE];
+
+PinweaverModel* PinweaverModel::default_instance_ = nullptr;
 
 //******************************************************************************
 // Private member functions.
