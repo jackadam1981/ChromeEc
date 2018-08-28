d3fa0835e6ef03a7e43db69e0083ecb466b53b64
Devin Lu <devin.lu@quantatw.com>
2018-08-28 17:35:51 +0800

    meep: Disable accel and gyro sensors with clamshell SKU
    meep and mimrock both share with the same board, but meep is unstuff
    accel and gyro sensors, so we don't need to initial those sensors.
    
    BUG=none
    BRANCH=none
    TEST=make buildall -j and make sure no initial event occurs
    with meep.
    
    Change-Id: I8141ad8160807288d7a886488b98e93ed9a257b9
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>

--- board/meep/board.c
+++ board/meep/board.c
@@ -143,6 +143,8 @@ static struct kionix_accel_data g_kx022_data;
 static struct lsm6dsm_data lsm6dsm_g_data;
 static struct lsm6dsm_data lsm6dsm_a_data;
 
+static uint16_t sku_id;
+
 /* Drivers */
 struct motion_sensor_t motion_sensors[] = {
 	[LID_ACCEL] = {
@@ -217,7 +219,36 @@ struct motion_sensor_t motion_sensors[] = {
 	},
 };
 
-const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+/*
+ * meep is clamshell SKU (SKU ID is 1)
+ * and mimrock is convertible SKU (SKU ID is 17)
+ */
+static int board_is_convertible(void)
+{
+	return sku_id == 17 || sku_id == 255;
+}
+
+static void board_set_motion_sensor_count(void)
+{
+	if (board_is_convertible())
+		motion_sensor_count = ARRAY_SIZE(motion_sensors);
+	else
+		motion_sensor_count = 0;
+}
+
+static void cbi_init(void)
+{
+	uint32_t val;
+
+	if (cbi_get_sku_id(&val) == EC_SUCCESS)
+		sku_id = val;
+	ccprints("SKU: 0x%04x", sku_id);
+
+	board_set_motion_sensor_count();
+}
+DECLARE_HOOK(HOOK_INIT, cbi_init, HOOK_PRIO_INIT_I2C + 1);
 
 /* Initialize board. */
 static void board_init(void)
@@ -253,6 +284,7 @@ void lid_angle_peripheral_enable(int enable)
 	if (tablet_get_mode())
 		enable = 0;
 
-	keyboard_scan_enable(enable, KB_SCAN_DISABLE_LID_ANGLE);
+	if (board_is_convertible())
+		keyboard_scan_enable(enable, KB_SCAN_DISABLE_LID_ANGLE);
 }
 #endif
