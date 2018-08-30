ee005f097c5656370a9b0b81afa7241420d735a0
Vadim Bendebury <vbendeb@chromium.org>
2018-08-29 17:21:55 -0700

    gsctool: refactor USB interface
    Communications with Cr50 exposed USB endpoints could be needed by
    other utilities, in particular, ./util/iteflash when it is extended to
    operate over Cr50.
    
    This patch moves USB interface functions into a separate file in the
    ./util directory and makes USB endpoint coordinates run time
    variables, so that the user of the interface can connect to various
    endpoints.
    
    Some refactoring is required to allow using the generic USB transfer
    function.
    
    BRANCH=none
    BUG=b:75976718
    TEST=verified that gsctool still operates properly - updated a Cr50,
         read Cr50 version number, etc.
    
    Change-Id: I3d77a93932f5395fff0f5823f0dd79e1d1d670c8
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

---
+++ util/usb_if.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __EC_EXTRA_USB_UPDATER_USB_IF_H
+#define __EC_EXTRA_USB_UPDATER_USB_IF_H
+
+#include <libusb.h>
+
+/* This describes USB endpoint used to communicate with Cr50. */
+struct usb_endpoint {
+	struct libusb_device_handle *devh;
+	uint8_t ep_num;
+	int     chunk_len;
+};
+
+/*
+ * Find the requested USB endpoint, as determined by vid, pid, subclass and
+ * protocol parameters. If found, fill up the uep structure.
+ *
+ * Return 0 on success, -1 on failure.
+ */
+int usb_findit(uint16_t vid, uint16_t pid, uint16_t subclass,
+	       uint16_t protocol, struct usb_endpoint *uep);
+
+/*
+ * Actual USB transfer function, the 'allow_less' flag indicates that the
+ * valid response could be shortef than allotted memory, the 'rxed_count'
+ * pointer, if provided along with 'allow_less', lets the caller know how mavy
+ * bytes were received.
+ */
+int usb_trx(struct usb_endpoint *uep, void *outbuf, int outlen,
+	    void *inbuf, int inlen, int allow_less,
+	    size_t *rxed_count);
+
+void usb_shut_down(struct usb_endpoint *uep);
+
+#define USB_ERROR(m, r) \
+	fprintf(stderr, "%s:%d, %s returned %d (%s)\n", __FILE__, __LINE__, \
+		m, r, libusb_strerror(r))
+
+#endif  /* ! __EC_EXTRA_USB_UPDATER_USB_IF_H */
