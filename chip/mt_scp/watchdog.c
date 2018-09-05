42ebb77c5b7fbe8b86d5d394a8e33b64673814b9
Rong Chang <rongchang@chromium.org>
2018-09-05 16:36:12 -0400

    scp: Add mt_scp chip
    SCP is a Cortex-M4 based sensor hub in Mediatek SoC. This change adds
    the chip folder and system level drivers.
    
    BRANCH=none
    BUG=b:114326670
    TEST=manual
      make BOARD=kukui_scp -j
      copy ec.bin to /lib/firmware/scp.img
      echo 'stop' > /sys/class/remoteproc/remoteproc0/state
      echo 'start' > /sys/class/remoteproc/remoteproc0/state
      check EC uart console
    
    Change-Id: I6629149f352184108fa520e80b59fd2ce94c76f7
    Signed-off-by: Rong Chang <rongchang@chromium.org>

---
+++ chip/mt_scp/watchdog.c
@@ -0,0 +1,35 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Watchdog driver */
+
+#include "common.h"
+#include "hooks.h"
+#include "panic.h"
+#include "registers.h"
+#include "watchdog.h"
+
+static struct panic_data * const pdata_ptr = PANIC_DATA_PTR;
+
+void watchdog_reload(void)
+{
+	SCP_WDT_RELOAD = SCP_WDT_RELOAD_VALUE;
+}
+DECLARE_HOOK(HOOK_TICK, watchdog_reload, HOOK_PRIO_DEFAULT);
+
+int watchdog_init(void)
+{
+	const uint32_t watchdog_timeout =
+		SCP_WDT_PERIOD(CONFIG_WATCHDOG_PERIOD_MS);
+
+	/* Disable watchdog */
+	SCP_WDT_CFG = watchdog_timeout;
+	/* Enable watchdog */
+	SCP_WDT_CFG = SCP_WDT_ENABLE | watchdog_timeout;
+	/* Reload watchdog */
+	watchdog_reload();
+
+	return EC_SUCCESS;
+}
