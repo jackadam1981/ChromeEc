4e64b76f6e2529366f0a6c5cd25630e239ecfae4
Allen Webb <allenwebb@google.com>
2018-09-07 16:17:36 -0700

    WIP:cr50_fuzz: Fuzz vendor specific interfaces.
    This is in progress but uploaded because it is on the same branch
    as the parent commits.
    
    Change-Id: I5dd5c7c94432cf406da6203193f4877925a4ac50

---
+++ board/host/wp.h
@@ -0,0 +1,11 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef EC_BOARD_HOST_WP_H
+#define EC_BOARD_HOST_WP_H
+
+#include "board/cr50/wp.h"
+
+#endif  /* ! EC_BOARD_HOST_WP_H */
