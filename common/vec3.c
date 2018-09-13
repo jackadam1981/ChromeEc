b501de79fbcfd3568bd3f14ef8d615c160bd2423
Yilun Lin <yllin@google.com>
2018-09-13 14:50:15 +0800

    vec3: Add type fpv3_t and related math functions.
    Modified from float version.
    Also, add tests for both float and fp type.
    
    TEST=make buildall -j
    TEST=make runtests -j
    TEST=make buildalltests -j
    BUG=b:113364863
    BRANCH=None
    
    Change-Id: Ieb6c3d822198f19dedde225dc510f3c1353558cb
    Signed-off-by: Yilun Lin <yllin@google.com>

--- common/vec3.c
+++ common/vec3.c
@@ -9,6 +9,7 @@
 #include "vec3.h"
 #include "util.h"
 
+/* TODO(b:113364863): Replace all float* types with fixed-point type fp_t. */
 void floatv3_scalar_mul(floatv3_t v, float c)
 {
 	v[X] *= c;
@@ -31,3 +32,24 @@ float floatv3_norm(const floatv3_t v)
 	return sqrtf(floatv3_norm_squared(v));
 }
 
+void fpv3_scalar_mul(fpv3_t v, fp_t c)
+{
+	v[X] = fp_mul(v[X], c);
+	v[Y] = fp_mul(v[Y], c);
+	v[Z] = fp_mul(v[Z], c);
+}
+
+fp_t fpv3_dot(const fpv3_t v, const fpv3_t w)
+{
+	return fp_mul(v[X], w[X]) + fp_mul(v[Y], w[Y]) + fp_mul(v[Z], w[Z]);
+}
+
+fp_t fpv3_norm_squared(const fpv3_t v)
+{
+	return fpv3_dot(v, v);
+}
+
+fp_t fpv3_norm(const fpv3_t v)
+{
+	return fp_sqrtf(fpv3_norm_squared(v));
+}
