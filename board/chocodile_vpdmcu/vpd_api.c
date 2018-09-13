4f94e08516a545cafda921aef70e1226bb1d2112
Sam Hurst <shurst@google.com>
2018-09-13 09:27:08 -0700

    chocodile_vpdmcu: Firmware for chocodile mcu
    Implement Chocodile Charge-Through Vconn Powered firmware for mcu
    using new Type-C/PD State machine stack.
    
    BUG=b:115626873
    BRANCH=none
    TEST=manual
    
    Signed-off-by: Sam Hurst <shurst@chromium.org>
    Change-Id: I847f1bcd2fc3ce41e66edd133a10c943d5e8c819

---
+++ board/chocodile_vpdmcu/vpd_api.c
@@ -0,0 +1,560 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "adc.h"
+#include "gpio.h"
+#include "registers.h"
+#include "vpd_api.h"
+#include "driver/tcpm/tcpm.h"
+#include "console.h"
+
+/*
+ * Polarity based on 'DFP Perspective' (see table USB Type-C Cable and Connector
+ * Specification)
+ *
+ * CC1    CC2    STATE             POSITION
+ * ----------------------------------------
+ * open   open   NC                N/A
+ * Rd     open   UFP attached      1
+ * open   Rd     UFP attached      2
+ * open   Ra     pwr cable no UFP  N/A
+ * Ra     open   pwr cable no UFP  N/A
+ * Rd     Ra     pwr cable & UFP   1
+ * Ra     Rd     pwr cable & UFP   2
+ * Rd     Rd     dbg accessory     N/A
+ * Ra     Ra     audio accessory   N/A
+ *
+ * Note, V(Rd) > V(Ra)
+ */
+#ifndef PD_SRC_RD_THRESHOLD
+#define PD_SRC_RD_THRESHOLD PD_SRC_DEF_RD_THRESH_MV
+#endif
+#ifndef PD_SRC_VNC
+#define PD_SRC_VNC PD_SRC_DEF_VNC_MV
+#endif
+
+#ifndef CC_RA
+#define CC_RA(port, cc, sel)  (cc < PD_SRC_RD_THRESHOLD)
+#endif
+#define CC_RD(cc) ((cc >= PD_SRC_RD_THRESHOLD) && (cc < PD_SRC_VNC))
+#ifndef CC_NC
+#define CC_NC(port, cc, sel)  (cc >= PD_SRC_VNC)
+#endif
+
+/*
+ * Polarity based on 'UFP Perspective'.
+ *
+ * CC1    CC2    STATE             POSITION
+ * ----------------------------------------
+ * open   open   NC                N/A
+ * Rp     open   DFP attached      1
+ * open   Rp     DFP attached      2
+ * Rp     Rp     Accessory attached N/A
+ */
+#ifndef PD_SNK_VA
+#define PD_SNK_VA PD_SNK_VA_MV
+#endif
+
+#define CC_RP(cc)  (cc >= PD_SNK_VA)
+
+/*
+ * Type C power source charge current limits are identified by their cc
+ * voltage (set by selecting the proper Rd resistor). Any voltage below
+ * TYPE_C_SRC_500_THRESHOLD will not be identified as a type C charger.
+ */
+#define TYPE_C_SRC_500_THRESHOLD        PD_SRC_RD_THRESHOLD
+#define TYPE_C_SRC_1500_THRESHOLD       660  /* mV */
+#define TYPE_C_SRC_3000_THRESHOLD       1230 /* mV */
+
+/* Charge-Through pull up/down enabled */
+static int ct_cc_pull;
+/* Charge-Through pull up value */
+static int ct_cc_rp_value;
+
+/* Charge-Through pull up/down enabled */
+static int host_cc_pull;
+/* Charge-Through pull up value */
+static int host_cc_rp_value;
+
+/* Convert CC voltage to CC status */
+static int vpd_cc_voltage_to_status(int cc_volt, int cc_pull)
+{
+	/* If we have a pull-up, then we are source, check for Rd. */
+	if (cc_pull == TYPEC_CC_RP) {
+		if (CC_NC(0, cc_volt, 0))
+			return TYPEC_CC_VOLT_OPEN;
+		else if (CC_RA(0, cc_volt, 0))
+			return TYPEC_CC_VOLT_RA;
+		else
+			return TYPEC_CC_VOLT_RD;
+	/* If we have a pull-down, then we are sink, check for Rp. */
+	} else if (cc_pull == TYPEC_CC_RD) {
+		if (cc_volt >= TYPE_C_SRC_3000_THRESHOLD)
+			return TYPEC_CC_VOLT_SNK_3_0;
+		else if (cc_volt >= TYPE_C_SRC_1500_THRESHOLD)
+			return TYPEC_CC_VOLT_SNK_1_5;
+		else if (CC_RP(cc_volt))
+			return TYPEC_CC_VOLT_SNK_DEF;
+		else
+			return TYPEC_CC_VOLT_OPEN;
+	} else {
+		/* If we are open, then always return 0 */
+		return 0;
+	}
+}
+
+void vpd_ct_set_pull(int pull, int rp_value)
+{
+	ct_cc_pull = pull;
+
+	switch (pull) {
+	case TYPEC_CC_RP:
+		ct_cc_rp_value = rp_value;
+		vpd_cc1_cc2_rd_l(GPO_HIGH);
+		switch (rp_value) {
+		case TYPEC_RP_USB:
+			vpd_config_cc1_rp1a5_odh(PIN_ADC, 0);
+			vpd_config_cc2_rp1a5_odh(PIN_ADC, 0);
+			vpd_config_cc1_rpusb_odh(PIN_GPO, 1);
+			vpd_config_cc2_rpusb_odh(PIN_GPO, 1);
+			break;
+		case TYPEC_RP_1A5:
+			vpd_config_cc1_rpusb_odh(PIN_ADC, 0);
+			vpd_config_cc2_rpusb_odh(PIN_ADC, 0);
+			vpd_config_cc1_rp1a5_odh(PIN_GPO, 1);
+			vpd_config_cc2_rp1a5_odh(PIN_GPO, 1);
+			break;
+		}
+		break;
+	case TYPEC_CC_RD:
+		vpd_config_cc1_rpusb_odh(PIN_ADC, 0);
+		vpd_config_cc2_rpusb_odh(PIN_ADC, 0);
+		/* FIXME: Change to PIN_ADC in next board rev. */
+		vpd_config_cc1_rp1a5_odh(PIN_GPO, 0);
+		/* FIXME: Change to PIN_ADC in next board rev. */
+		vpd_config_cc2_rp1a5_odh(PIN_GPO, 0);
+		vpd_cc1_cc2_rd_l(GPO_LOW);
+		break;
+	case TYPEC_CC_OPEN:
+		vpd_cc1_cc2_rd_l(GPO_HIGH);
+		vpd_config_cc1_rpusb_odh(PIN_ADC, 0);
+		vpd_config_cc2_rpusb_odh(PIN_ADC, 0);
+		vpd_config_cc1_rp1a5_odh(PIN_ADC, 0);
+		vpd_config_cc2_rp1a5_odh(PIN_ADC, 0);
+		break;
+	}
+}
+
+void vpd_ct_get_cc(int *cc1, int *cc2)
+{
+	int cc1_v;
+	int cc2_v;
+
+	switch (ct_cc_pull) {
+	case TYPEC_CC_RP:
+		switch (ct_cc_rp_value) {
+		case TYPEC_RP_USB:
+			cc1_v = adc_read_channel(ADC_CC1_RP1A5_ODH);
+			cc2_v = adc_read_channel(ADC_CC2_RP1A5_ODH);
+			break;
+		case TYPEC_RP_1A5:
+			cc1_v = adc_read_channel(ADC_CC1_RPUSB_ODH);
+			cc2_v = adc_read_channel(ADC_CC2_RPUSB_ODH);
+			break;
+		}
+		break;
+	case TYPEC_CC_RD:
+		cc1_v = adc_read_channel(ADC_CC1_RPUSB_ODH);
+		cc2_v = adc_read_channel(ADC_CC2_RPUSB_ODH);
+		break;
+	case TYPEC_CC_OPEN:
+		*cc1 = 0;
+		*cc2 = 0;
+		break;
+	}
+
+	*cc1 = vpd_cc_voltage_to_status(cc1_v, ct_cc_pull);
+	*cc2 = vpd_cc_voltage_to_status(cc2_v, ct_cc_pull);
+}
+
+void vpd_host_set_pull(int pull, int rp_value)
+{
+	host_cc_pull = pull;
+
+	switch (pull) {
+	case TYPEC_CC_RP:
+		host_cc_rp_value = rp_value;
+		switch (rp_value) {
+		case TYPEC_RP_USB:
+			vpd_config_cc_rp3a0_rd_l(PIN_CMP, 0);
+			vpd_cc_rp1a5_odh(GPO_HZ);
+			vpd_cc_rpusb_odh(GPO_HIGH);
+			break;
+		case TYPEC_RP_1A5:
+			vpd_config_cc_rp3a0_rd_l(PIN_CMP, 0);
+			vpd_cc_rpusb_odh(GPO_HZ);
+			vpd_cc_rp1a5_odh(GPO_HIGH);
+			break;
+		case TYPEC_RP_3A0:
+			vpd_cc_rp1a5_odh(GPO_HZ);
+			vpd_cc_rpusb_odh(GPO_HZ);
+			vpd_config_cc_rp3a0_rd_l(PIN_GPO, 1);
+			break;
+		}
+		break;
+	case TYPEC_CC_RD:
+		vpd_cc_rpusb_odh(GPO_HZ);
+		vpd_cc_rp1a5_odh(GPO_LOW);
+		vpd_config_cc_rp3a0_rd_l(PIN_GPO, 0);
+		break;
+	case TYPEC_CC_OPEN:
+		vpd_config_cc_rp3a0_rd_l(PIN_CMP, 0);
+		vpd_cc_rpusb_odh(GPO_HZ);
+		vpd_cc_rp1a5_odh(GPO_HZ);
+		break;
+	}
+}
+
+void vpd_host_get_cc(int *cc)
+{
+	vpd_config_cc_vpdmcu(PIN_ADC, 0);
+	*cc = vpd_cc_voltage_to_status(
+		adc_read_channel(ADC_CC_VPDMCU), host_cc_pull);
+	vpd_config_cc_vpdmcu(PIN_CMP, 0);
+}
+
+void vpd_rx_enable(int en)
+{
+	if (en) {
+		/* Disconnect charge-through CC1 and CC2 lines from host */
+		gpio_set_level(GPIO_CC1_SEL, 0);
+		gpio_set_level(GPIO_CC2_SEL, 0);
+
+		/* Set high to connect MCU CC to host CC line */
+		gpio_set_level(GPIO_VPDMCU_CC_EN, 1);
+	}
+
+	tcpm_set_polarity(0, 0);
+	tcpm_set_rx_enable(0, en);
+}
+
+/*
+ * PA1: Configure as ADC, CMP, or GPO
+ */
+void vpd_config_cc_vpdmcu(enum vpd_pin cfg, int en)
+{
+	if (cfg == PIN_GPO) {
+		/* Disable Analog mode and Enable GPO */
+		STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				& ~(3 << (2*1))) /* PA1 disable ADC */
+				|  (1 << (2*1)); /* Set as GPO */
+		gpio_set_level(GPIO_CC_VPDMCU, en ? 1:0);
+	}
+
+	if (cfg == PIN_ADC || cfg == PIN_CMP) {
+		/* Disable COMP1 */
+		STM32_COMP_CSR &= ~STM32_COMP_CMP1EN;
+
+		/* Enable Analog mode */
+		STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+			|  (3 << (2*1))); /* PA1 in ANALOG mode */
+
+		if (cfg == PIN_CMP) {
+			/* Enable COMP1 */
+			STM32_COMP_CSR |= STM32_COMP_CMP1EN;
+		}
+	}
+}
+
+/*
+ * PA2: Configure as COMP2_INM6 or GPO
+ */
+void vpd_config_cc_rp3a0_rd_l(enum vpd_pin cfg, int en)
+{
+	if (cfg == PIN_GPO) {
+		/* Disable Analog mode and Enable GPO */
+		STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				& ~(3 << (2*2))) /* PA2 disable ADC */
+				|  (1 << (2*2)); /* Set as GPO */
+		gpio_set_level(GPIO_CC_RP3A0_RD_L, en ? 1:0);
+	} else {
+		/* Set PA2 pin to ANALOG function */
+		STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				|  (3 << (2*2))); /* PA2 in ANALOG mode */
+
+		/* Set PA3 pin to ANALOG function */
+		STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				|  (3 << (2*3))); /* PA3 in ANALOG mode */
+
+		/* Disable Window Mode. Select PA3 */
+		STM32_COMP_CSR &= ~STM32_COMP_WNDWEN;
+
+		/* No output selection. We will use Interrupt */
+		STM32_COMP_CSR &= ~STM32_COMP_CMP2OUTSEL_NONE;
+
+		/* Not inverting */
+		STM32_COMP_CSR &= ~STM32_COMP_CMP2POL;
+
+		/* Select COMP2_INM6 (PA2) */
+		STM32_COMP_CSR |= STM32_COMP_CMP2INSEL_INM6;
+
+		/* COMP Enable */
+		STM32_COMP_CSR |= STM32_COMP_CMP2EN;
+	}
+}
+
+/*
+ * PA4: Configure as ADC, CMP, or GPO
+ */
+void vpd_config_cc1_rp1a5_odh(enum vpd_pin cfg, int en)
+{
+	if (cfg == PIN_GPO) {
+		/* Disable Analog mode and Enable GPO */
+		STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				& ~(3 << (2*4))) /* PA4 disable ADC */
+				|  (1 << (2*4)); /* Set as GPO */
+		/* Default high. Enable cc1 Rp1A5 pullup */
+		gpio_set_level(GPIO_CC1_RP1A5_ODH, en ? 1:0);
+	}
+
+	if (cfg == PIN_ADC || cfg == PIN_CMP) {
+		/* Disable COMP2 */
+		STM32_COMP_CSR &= ~STM32_COMP_CMP2EN;
+
+		/* Set PA4 pin to Analog mode */
+		STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				|  (3 << (2*4))); /* PA4 in ANALOG mode */
+
+		if (cfg == PIN_CMP) {
+			/* Set PA3 pin to ANALOG function */
+			STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				|  (3 << (2*3))); /* PA3 in ANALOG mode */
+
+			/* Disable Window Mode. Select PA3*/
+			STM32_COMP_CSR &= ~STM32_COMP_WNDWEN;
+
+			/* No output selection. We will use Interrupt */
+			STM32_COMP_CSR &= ~STM32_COMP_CMP2OUTSEL_NONE;
+
+			/* Select COMP2_INM4 (PA4) */
+			STM32_COMP_CSR |= STM32_COMP_CMP2INSEL_INM4;
+
+			/* COMP2 Enable */
+			STM32_COMP_CSR |= STM32_COMP_CMP2EN;
+		}
+	}
+}
+
+/*
+ * PA5: Configure as ADC, COMP, or GPO
+ */
+void vpd_config_cc2_rp1a5_odh(enum vpd_pin cfg, int en)
+{
+	if (cfg == PIN_GPO) {
+		/* Disable Analog mode and Enable GPO */
+		STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				& ~(3 << (2*5))) /* PA5 disable ADC */
+				|  (1 << (2*5)); /* Set as GPO */
+		gpio_set_level(GPIO_CC2_RP1A5_ODH, en ? 1:0);
+	}
+
+	if (cfg == PIN_ADC || cfg == PIN_CMP) {
+		/* Disable COMP2 */
+		STM32_COMP_CSR &= ~STM32_COMP_CMP2EN;
+
+		/* Set PA5 pin to ANALOG function */
+		STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				|  (3 << (2*5))); /* PA5 in ANALOG mode */
+
+		if (cfg == PIN_CMP) {
+			/* Set PA3 pin to ANALOG function */
+			STM32_GPIO_MODER(GPIO_A) = (STM32_GPIO_MODER(GPIO_A)
+				|  (3 << (2*3))); /* PA3 in ANALOG mode */
+
+			/* Disable Window Mode. */
+			STM32_COMP_CSR &= ~STM32_COMP_WNDWEN;
+
+			/* No output selection. We will use Interrupt */
+			STM32_COMP_CSR &= ~STM32_COMP_CMP2OUTSEL_NONE;
+
+			/* Select COMP2_INM5 (PA5) */
+			STM32_COMP_CSR |= STM32_COMP_CMP2INSEL_INM5;
+
+			/* COMP2 Enable */
+			STM32_COMP_CSR |= STM32_COMP_CMP2EN;
+		}
+	}
+}
+
+/*
+ * PB0: Configure as ADC or GPO
+ */
+void vpd_config_cc1_rpusb_odh(enum vpd_pin cfg, int en)
+{
+	if (cfg == PIN_GPO) {
+		/* Disable Analog mode and Enable GPO */
+		STM32_GPIO_MODER(GPIO_B) = (STM32_GPIO_MODER(GPIO_B)
+				& ~(3 << (2*0))) /* PB0 disable ADC */
+				|  (1 << (2*0)); /* Set as GPO */
+		gpio_set_level(GPIO_CC1_RPUSB_ODH, en ? 1:0);
+	} else {
+		/* Enable Analog mode */
+		STM32_GPIO_MODER(GPIO_B) = (STM32_GPIO_MODER(GPIO_B)
+				|  (3 << (2*0))); /* PB0 in ANALOG mode */
+	}
+}
+
+/*
+ * PB1: Configure as ADC or GPO
+ */
+void vpd_config_cc2_rpusb_odh(enum vpd_pin cfg, int en)
+{
+	if (cfg == PIN_GPO) {
+		/* Disable Analog mode and Enable GPO */
+		STM32_GPIO_MODER(GPIO_B) = (STM32_GPIO_MODER(GPIO_B)
+				& ~(3 << (2*1))) /* PB1 disable ADC */
+				|  (1 << (2*1)); /* Set as GPO */
+		gpio_set_level(GPIO_CC2_RPUSB_ODH, en ? 1:0);
+	} else {
+		/* Enable Analog mode */
+		STM32_GPIO_MODER(GPIO_B) = (STM32_GPIO_MODER(GPIO_B)
+				|  (3 << (2*1))); /* PB1 in ANALOG mode */
+	}
+}
+
+int vpd_read_cc_vpdmcu(void)
+{
+	return adc_read_channel(ADC_CC_VPDMCU);
+}
+
+int vpd_read_host_vbus(void)
+{
+	return adc_read_channel(ADC_HOST_VBUS_VSENSE);
+}
+
+int vpd_read_ct_vbus(void)
+{
+	return adc_read_channel(ADC_CHARGE_VBUS_VSENSE);
+}
+
+int vpd_read_vconn(void)
+{
+	return adc_read_channel(ADC_VCONN_VSENSE);
+}
+
+int vpd_is_host_vbus(void)
+{
+	return (vpd_read_host_vbus() >= PD_SNK_VA);
+}
+
+int vpd_is_ct_vbus(void)
+{
+	return (vpd_read_ct_vbus() >= PD_SNK_VA);
+}
+
+int vpd_is_vconn(void)
+{
+	return (vpd_read_vconn() >= PD_SNK_VA);
+}
+
+int vpd_read_rdconnect_ref(void)
+{
+	return adc_read_channel(ADC_RDCONNECT_REF);
+}
+
+int vpd_read_cc1_ct(void)
+{
+	return adc_read_channel(ADC_CC1_RP1A5_ODH);
+}
+
+int vpd_read_cc2_ct(void)
+{
+	return adc_read_channel(ADC_CC2_RP1A5_ODH);
+}
+
+void vpd_red_led(int on)
+{
+	gpio_set_level(GPIO_DEBUG_LED_R_L, (on) ? 1 : 0);
+}
+
+void vpd_green_led(int on)
+{
+	gpio_set_level(GPIO_DEBUG_LED_G_L, (on) ? 1 : 0);
+}
+
+void vpd_vbus_pass_en(int en)
+{
+	gpio_set_level(GPIO_VBUS_PASS_EN, (en) ? 1 : 0);
+}
+
+void vpd_present_billboard(int en)
+{
+	gpio_set_level(GPIO_PRESENT_BILLBOARD, (en) ? 0 : 1);
+}
+
+void vpd_mcu_cc_en(int en)
+{
+	gpio_set_level(GPIO_VPDMCU_CC_EN, (en) ? 1 : 0);
+}
+
+void vpd_cc1_sel(int en)
+{
+	gpio_set_level(GPIO_CC1_SEL, (en) ? 1 : 0);
+}
+
+void vpd_cc2_sel(int en)
+{
+	gpio_set_level(GPIO_CC2_SEL, (en) ? 1 : 0);
+}
+
+/* Set as GPO High, GPO Low, or High-Z */
+void vpd_cc_db_en_od(enum vpd_gpo val)
+{
+	if (val == GPO_HZ) {
+		gpio_set_flags(GPIO_CC_DB_EN_OD, GPIO_INPUT);
+	} else {
+		gpio_set_flags(GPIO_CC_DB_EN_OD, GPIO_OUTPUT);
+
+		if (val == GPO_HIGH)
+			gpio_set_level(GPIO_CC_DB_EN_OD, 1);
+		else
+			gpio_set_level(GPIO_CC_DB_EN_OD, 0);
+	}
+}
+
+void vpd_cc_rpusb_odh(enum vpd_gpo val)
+{
+	if (val == GPO_HZ) {
+		gpio_set_flags(GPIO_CC_RPUSB_ODH, GPIO_INPUT);
+	} else {
+		gpio_set_flags(GPIO_CC_RPUSB_ODH, GPIO_OUTPUT);
+		gpio_set_level(GPIO_CC_RPUSB_ODH, (val == GPO_HIGH) ? 1:0);
+	}
+}
+
+void vpd_cc_rp1a5_odh(enum vpd_gpo val)
+{
+	if (val == GPO_HZ) {
+		gpio_set_flags(GPIO_CC_RP1A5_ODH, GPIO_INPUT);
+	} else {
+		gpio_set_flags(GPIO_CC_RP1A5_ODH, GPIO_OUTPUT);
+		gpio_set_level(GPIO_CC_RP1A5_ODH, (val == GPO_HIGH) ? 1:0);
+	}
+}
+
+void vpd_cc1_cc2_rd_l(enum vpd_gpo val)
+{
+	if (val == GPO_HZ) {
+		gpio_set_flags(GPIO_CC1_CC2_RD_L, GPIO_INPUT);
+	} else {
+		gpio_set_flags(GPIO_CC1_CC2_RD_L, GPIO_OUTPUT);
+		gpio_set_level(GPIO_CC1_CC2_RD_L, (val == GPO_HIGH) ? 1:0);
+	}
+}
+
+void vpd_vconn_pwr_sel_odl(enum vpd_pwr en)
+{
+	gpio_set_level(GPIO_VCONN_PWR_SEL_ODL, en);
+}
--- common/usb_pd_tcpc.c
+++ common/usb_pd_tcpc.c
@@ -298,11 +298,19 @@ int prepare_message(int port, uint16_t header, uint8_t cnt,
 	int off, i;
 	/* 64-bit preamble */
 	off = pd_write_preamble(port);
+#ifdef CONFIG_USBC_CTVPD
+	/* Start Of Packet: 2x Sync-1 + 2x Sync-3 */
+	off = pd_write_sym(port, off, BMC(PD_SYNC1));
+	off = pd_write_sym(port, off, BMC(PD_SYNC1));
+	off = pd_write_sym(port, off, BMC(PD_SYNC3));
+	off = pd_write_sym(port, off, BMC(PD_SYNC3));
+#else
 	/* Start Of Packet: 3x Sync-1 + 1x Sync-2 */
 	off = pd_write_sym(port, off, BMC(PD_SYNC1));
 	off = pd_write_sym(port, off, BMC(PD_SYNC1));
 	off = pd_write_sym(port, off, BMC(PD_SYNC1));
 	off = pd_write_sym(port, off, BMC(PD_SYNC2));
+#endif
 	/* header */
 	off = encode_short(port, off, header);
 
@@ -637,17 +645,29 @@ int pd_analyze_rx(int port, uint32_t *payload)
 	while (bit > 0) {
 		bit = pd_dequeue_bits(port, bit, 20, &val);
 		if (val == PD_SOP) {
+#ifdef CONFIG_USBC_CTVPD
+			return PD_RX_ERR_UNSUPPORTED_SOP;
+#else
 			break;
+#endif
 		} else if (val == PD_SOP_PRIME) {
+#ifdef CONFIG_USBC_CTVPD
+			break;
+#else
 			CPRINTF("SOP'\n");
 			return PD_RX_ERR_UNSUPPORTED_SOP;
+#endif
 		} else if (val == PD_SOP_PRIME_PRIME) {
 			CPRINTF("SOP''\n");
 			return PD_RX_ERR_UNSUPPORTED_SOP;
 		}
 	}
 	if (bit < 0) {
+#ifdef CONFIG_USBC_CTVPD
+		msg = "SOP'";
+#else
 		msg = "SOP";
+#endif
 		goto packet_err;
 	}
 
@@ -794,6 +814,9 @@ int tcpc_run(int port, int evt)
 	if ((evt & PD_EVENT_TX) && pd[port].rx_enabled) {
 		switch (pd[port].tx_type) {
 		case TCPC_TX_SOP:
+#ifdef CONFIG_USBC_CTVPD
+		case TCPC_TX_SOP_PRIME:
+#endif
 			res = send_validate_message(port,
 					pd[port].tx_head,
 					pd[port].tx_data);
@@ -859,7 +882,7 @@ int tcpc_run(int port, int evt)
 #endif
 }
 
-#ifndef CONFIG_USB_POWER_DELIVERY
+#if !defined(CONFIG_USB_POWER_DELIVERY) && !defined(CONFIG_USBC_CTVPD)
 void pd_task(void *u)
 {
 	int port = TASK_ID_TO_PD_PORT(task_get_current());
