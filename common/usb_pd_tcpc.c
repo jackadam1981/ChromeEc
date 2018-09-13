80dd8b39944a330520013b5427fa02c90ea6c213
Sam Hurst <shurst@google.com>
2018-09-13 09:27:08 -0700

    chocodile_vpdmcu: Firmware for chocodile mcu
    Implement Chocodile Charge-Through Vconn Powered firmware for mcu
    using new Type-C/PD State machine stack.
    
    BUG=b:115626873
    BRANCH=none
    TEST=manual
    
    Signed-off-by: Sam Hurst <shurst@chromium.org>
    Change-Id: I847f1bcd2fc3ce41e66edd133a10c943d5e8c819

--- common/usb_pd_tcpc.c
+++ common/usb_pd_tcpc.c
@@ -308,11 +308,19 @@ int prepare_message(int port, uint16_t header, uint8_t cnt,
 	int off, i;
 	/* 64-bit preamble */
 	off = pd_write_preamble(port);
+#if defined(CONFIG_USB_TYPEC_VPD) || defined(CONFIG_USB_TYPEC_CTVPD)
+	/* Start Of Packet Prime: 2x Sync-1 + 2x Sync-3 */
+	off = pd_write_sym(port, off, BMC(PD_SYNC1));
+	off = pd_write_sym(port, off, BMC(PD_SYNC1));
+	off = pd_write_sym(port, off, BMC(PD_SYNC3));
+	off = pd_write_sym(port, off, BMC(PD_SYNC3));
+#else
 	/* Start Of Packet: 3x Sync-1 + 1x Sync-2 */
 	off = pd_write_sym(port, off, BMC(PD_SYNC1));
 	off = pd_write_sym(port, off, BMC(PD_SYNC1));
 	off = pd_write_sym(port, off, BMC(PD_SYNC1));
 	off = pd_write_sym(port, off, BMC(PD_SYNC2));
+#endif
 	/* header */
 	off = encode_short(port, off, header);
 
@@ -647,17 +655,30 @@ int pd_analyze_rx(int port, uint32_t *payload)
 	while (bit > 0) {
 		bit = pd_dequeue_bits(port, bit, 20, &val);
 		if (val == PD_SOP) {
+#if defined(CONFIG_USB_TYPEC_VPD) || defined(CONFIG_USB_TYPEC_CTVPD)
+			CPRINTF("SOP\n");
+			return PD_RX_ERR_UNSUPPORTED_SOP;
+#else
 			break;
+#endif
 		} else if (val == PD_SOP_PRIME) {
+#if defined(CONFIG_USB_TYPEC_VPD) || defined(CONFIG_USB_TYPEC_CTVPD)
+			break;
+#else
 			CPRINTF("SOP'\n");
 			return PD_RX_ERR_UNSUPPORTED_SOP;
+#endif
 		} else if (val == PD_SOP_PRIME_PRIME) {
 			CPRINTF("SOP''\n");
 			return PD_RX_ERR_UNSUPPORTED_SOP;
 		}
 	}
 	if (bit < 0) {
+#if defined(CONFIG_USB_TYPEC_VPD) || defined(CONFIG_USB_TYPEC_CTVPD)
+		msg = "SOP'";
+#else
 		msg = "SOP";
+#endif
 		goto packet_err;
 	}
 
@@ -803,7 +824,11 @@ int tcpc_run(int port, int evt)
 	/* outgoing packet ? */
 	if ((evt & PD_EVENT_TX) && pd[port].rx_enabled) {
 		switch (pd[port].tx_type) {
+#if defined(CONFIG_USB_TYPEC_VPD) || defined(CONFIG_USB_TYPEC_CTVPD)
+		case TCPC_TX_SOP_PRIME:
+#else
 		case TCPC_TX_SOP:
+#endif
 			res = send_validate_message(port,
 					pd[port].tx_head,
 					pd[port].tx_data);
@@ -869,7 +894,8 @@ int tcpc_run(int port, int evt)
 #endif
 }
 
-#ifndef CONFIG_USB_POWER_DELIVERY
+#if !defined(CONFIG_USB_POWER_DELIVERY) && \
+	!(defined(CONFIG_USB_TYPEC_VPD) || defined(CONFIG_USB_TYPEC_CTVPD))
 void pd_task(void *u)
 {
 	int port = TASK_ID_TO_PD_PORT(task_get_current());
