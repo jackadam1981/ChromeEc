1ca76c8866372130812621cc81ef82fc7c5f0f8c
Aseda Aboagye <aaboagye@google.com>
2018-09-17 18:25:33 -0700

    nocturne: Enable CONFIG_CHARGER_PROFILE_OVERRIDE.
    We need to stop charging the battery when the DRAM temperature exceeds
    47 C.  This commit uses charge_profile_override() to start discharging
    the pack when this happens and resume charging when the temperature
    cools down.
    
    BUG=b:115924459, b:112550414
    BRANCH=firmware-nocturne-10984.B
    TEST=Cool device down, verify that once temp sensor crosses threshold,
    charging is stopped and battery is discharging.
    TEST=Verify charging is resumed when device shuts down.
    TEST=Verify charging is resumed when device cools down.
    
    Change-Id: I00ba65daac7c8929c41ef147467caf984b9bdaa7
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- board/nocturne/battery.c
+++ board/nocturne/battery.c
@@ -7,12 +7,22 @@
 
 #include "battery.h"
 #include "battery_smart.h"
+#include "charger.h"
+#include "chipset.h"
+#include "charge_state_v2.h"
 #include "common.h"
 #include "ec_commands.h"
 #include "extpower.h"
+#include "temp_sensor.h"
 
 /* Shutdown mode parameter to write to manufacturer access register */
 #define SB_SHUTDOWN_DATA	0x0010
+
+/*
+ * We need to stop charging the battery when the DRAM temperature sensor gets
+ * over 47 C (320 K), and resume charging once it cools back down.
+ */
+#define DRAM_CRITICAL_TEMP_K	320
 
 /* Battery info */
 static const struct battery_info info = {
@@ -83,4 +93,57 @@ enum battery_disconnect_state battery_get_disconnect_state(void)
 
 	/* No safety fault, battery is disconnected */
 	return BATTERY_DISCONNECTED;
+}
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+static int should_discharge(void)
+{
+	int t_dram;
+
+	/* We can only discharge on AC, if AC is plugged in. */
+	if (!gpio_get_level(GPIO_AC_PRESENT))
+		return 0;
+
+	/*
+	 * The DRAM temperature sensor is only available when the AP is on,
+	 * therefore only inhibit charging when we can actually read a
+	 * temperature.
+	 */
+	if (chipset_in_state(CHIPSET_STATE_ON) &&
+	    !temp_sensor_read(TEMP_SENSOR_DRAM, &t_dram) &&
+	    (t_dram > DRAM_CRITICAL_TEMP_K))
+		return 1;
+	else
+		return 0;
+}
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	static uint8_t discharging_on_ac;
+	int enable_discharge;
+
+	enable_discharge = should_discharge();
+	if (enable_discharge != discharging_on_ac) {
+		if (enable_discharge) {
+			charger_discharge_on_ac(1);
+			curr->state = ST_DISCHARGE;
+			discharging_on_ac = 1;
+		} else {
+			charger_discharge_on_ac(0);
+			discharging_on_ac = 0;
+		}
+	}
+
+	return 0;
 }
