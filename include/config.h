f3844edc89ab9cc0dad6a60acdc7969d7de38778
Ben Chen <ben.chen2@quanta.corp-partner.google.com>
2018-09-18 16:39:19 +0800

    Leds : Supports GPIO-controlled for common battery/power status through a board-defined lookup table
    reference patch: 1121110
    
    BUG=b:113611642
    BRANCH=master
    TEST=None
    
    Change-Id: Id3ab88965e67d170bdc875112475565ab2dca542
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- include/config.h
+++ include/config.h
@@ -2179,6 +2179,18 @@
 #undef CONFIG_LED_PWM_COUNT
 
 /*
+ * Support GPIO-controlled LEDs for common battery/power
+ * states through a board-defined lookup table.
+ */
+#undef CONFIG_LED_ONOFF_STATES
+
+/*
+ * Supports GPIO-conntrolled LEDs states enable, if
+ * power LED is enabled, In this case should be activated.
+ */
+#undef CONFIG_LED_POWER_LED
+
+/*
  * LEDs for LED_POLICY STD may be inverted.  In this case they are active low
  * and the GPIO names will be GPIO_LED..._L.
  */
