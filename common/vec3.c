60bf34d46b30d03a516b61cdcc8866991c2fcc1d
Yilun Lin <yllin@google.com>
2018-09-19 17:19:12 +0800

    float: Remove all float* matrix/vector functions.
    All float* functions now can be used with fp_* type wrapper functions with
    CONFIG_FPU. float* functions are not used anymore. Let's remove them.
    
    TEST=make runtests -j
    TEST=make buildalltests -j
    BUG=b:113364863
    BRANCH=None
    
    Change-Id: Ibe3904b42a5938bfafbda95e6887f3eff17baadf
    Signed-off-by: Yilun Lin <yllin@google.com>

--- common/vec3.c
+++ common/vec3.c
@@ -9,29 +9,6 @@
 #include "vec3.h"
 #include "util.h"
 
-/* TODO(b:113364863): Replace all float* types with fixed-point type fp_t. */
-void floatv3_scalar_mul(floatv3_t v, float c)
-{
-	v[X] *= c;
-	v[Y] *= c;
-	v[Z] *= c;
-}
-
-float floatv3_dot(const floatv3_t v, const floatv3_t w)
-{
-	return v[X] * w[X] + v[Y] * w[Y] + v[Z] * w[Z];
-}
-
-float floatv3_norm_squared(const floatv3_t v)
-{
-	return floatv3_dot(v, v);
-}
-
-float floatv3_norm(const floatv3_t v)
-{
-	return sqrtf(floatv3_norm_squared(v));
-}
-
 void fpv3_scalar_mul(fpv3_t v, fp_t c)
 {
 	v[X] = fp_mul(v[X], c);
--- include/vec3.h
+++ include/vec3.h
@@ -12,12 +12,6 @@
 typedef float floatv3_t[3];
 typedef fp_t fpv3_t[3];
 
-/* TODO(b:113364863): Replace all float* types with fixed-point type fp_t. */
-void floatv3_scalar_mul(floatv3_t v, float c);
-float floatv3_dot(const floatv3_t v, const floatv3_t w);
-float floatv3_norm_squared(const floatv3_t v);
-float floatv3_norm(const floatv3_t v);
-
 /* fixed-point */
 void fpv3_scalar_mul(fpv3_t v, fp_t c);
 fp_t fpv3_dot(const fpv3_t v, const fpv3_t w);
