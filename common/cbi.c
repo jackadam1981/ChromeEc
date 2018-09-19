64c0b40d867c8c3554fb13ac9efdad7677177e35
Jett Rink <jettrink@chromium.org>
2018-09-19 17:43:22 -0600

    cbi: allow tags to be resized
    Since we now have a dynamically sized CBI tab (DRAM), it is more
    convenient to be able to resize tags. We accomplish this by deleting
    the old tag and adding the new tag at the end, after copying all of
    the data forward.
    
    BRANCH=none
    BUG=b:116075074
    TEST=Updated various CBI fields and watch them move to the end. tested
    updating the last field and middle fields
    
    Change-Id: Icb9b3cb36445d8e78315f9fd3a74483ff2d52ebf
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- common/cbi.c
+++ common/cbi.c
@@ -161,11 +161,29 @@ int cbi_get_board_info(enum cbi_data_tag tag, uint8_t *buf, uint8_t *size)
 	return EC_SUCCESS;
 }
 
+static void cbi_remove_tag(void *const cbi, const struct cbi_data *const d)
+{
+	struct cbi_header *h = cbi;
+	const size_t size = sizeof(*d) + d->size;
+	const uint8_t *next = (uint8_t *)d + size;
+	const size_t bytes_after =
+		h->total_size - ((uint8_t *)next - (uint8_t *)cbi);
+	memmove((void *)d, (void *)next, bytes_after);
+	h->total_size -= size;
+}
+
 int cbi_set_board_info(enum cbi_data_tag tag, const uint8_t *buf, uint8_t size)
 {
 	struct cbi_data *d;
 
 	d = cbi_find_tag(cbi, tag);
+
+	/* If we found the entry, but the size doesn't match, delete it */
+	if (d && d->size != size) {
+		cbi_remove_tag(cbi, d);
+		d = NULL;
+	}
+
 	if (!d) {
 		/* Not found. Check if new item would fit */
 		if (sizeof(cbi) < head->total_size + sizeof(*d) + size)
@@ -174,13 +192,11 @@ int cbi_set_board_info(enum cbi_data_tag tag, const uint8_t *buf, uint8_t size)
 		cbi_set_data(&cbi[head->total_size], tag, buf, size);
 		head->total_size += (sizeof(*d) + size);
 		return EC_SUCCESS;
+	} else {
+		/* Overwrite existing item */
+		memcpy(d->value, buf, d->size);
+		return EC_SUCCESS;
 	}
-	/* No expand or shrink. Items are tightly packed. */
-	if (d->size != size)
-		return EC_ERROR_INVAL;
-	/* Overwrite existing item */
-	memcpy(d->value, buf, d->size);
-	return EC_SUCCESS;
 }
 
 static int eeprom_is_write_protected(void)
