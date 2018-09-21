304c3208c519f87da26ca33949562ac214816fff
Jett Rink <jettrink@chromium.org>
2018-09-21 14:05:15 -0600

    usb-pd: set Rd before sending PS_RDY
    According to Table 7-9 "Sequence Description for Sink Requested Power
    Role Swap" in USB-PD 3.0 spec, CC termination should be set from Rp to
    Rd before sending the PD_RDY message to port partner.
    
    BRANCH=none
    BUG=b:116340006
    TEST=Verified with oscope that CC lines change before PS_RDY sent
    
    Change-Id: Ib94f55e4fd282c152091f177255dd92a7b0404b6
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -3200,16 +3200,19 @@ void pd_task(void *u)
 		case PD_STATE_SRC_SWAP_STANDBY:
 			/* Send PS_RDY to let sink know our power is off */
 			if (pd[port].last_state != pd[port].task_state) {
+				/* Switch to Rd before sending PS_RDY*/
+				tcpm_set_cc(port, TYPEC_CC_RD);
 				/* Send PS_RDY */
 				res = send_control(port, PD_CTRL_PS_RDY);
 				if (res < 0) {
+					/* Restore Rp */
+					tcpm_set_cc(port, TYPEC_CC_RP);
 					timeout = 10*MSEC;
 					set_state(port,
 						  PD_STATE_SRC_DISCONNECTED);
 					break;
 				}
-				/* Switch to Rd and swap roles to sink */
-				tcpm_set_cc(port, TYPEC_CC_RD);
+				/* Swap roles to sink */
 				pd_set_power_role(port, PD_ROLE_SINK);
 				/* Wait for PS_RDY from new source */
 				set_state_timeout(port,
