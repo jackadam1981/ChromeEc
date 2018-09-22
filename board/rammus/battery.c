16c4d7687fad4c4009c684d782b0a953a5ef793f
michael_chen <michael5_chen@pegatroncorp.com>
2018-09-22 10:45:25 +0800

    rammus: Implement the EC battery code
    Implement the EC battery code.
    
    BUG=b:111815315
    BRANCH=ToT
    TEST=Manual.
      Check DC power on system.
      Check battery can charging under AC mode.
      Check battery discharging under DC mode.
      Check battery cut off function.
      Check battery C-FET/D-FET data.
    
    Change-Id: Ia9add6518948b30fbdb136f0b3c4104abd80a735
    Signed-off-by: michael_chen <michael5_chen@pegatroncorp.com>

--- board/rammus/battery.c
+++ board/rammus/battery.c
@@ -13,6 +13,9 @@
 #include "extpower.h"
 #include "gpio.h"
 #include "util.h"
+#include "console.h"
+
+#define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
 
 static enum battery_present batt_pres_prev = BP_NOT_SURE;
 
@@ -20,32 +23,22 @@ static enum battery_present batt_pres_prev = BP_NOT_SURE;
 #define SB_SHIP_MODE_REG	SB_MANUFACTURER_ACCESS
 #define SB_SHUTDOWN_DATA	0x0010
 
-/*
- * Unlike other smart batteries, Nautilus battery uses different bit fields
- * in manufacturer access register for the conditions of the CHG/DSG FETs.
- */
-#define BATFETS_SHIFT		(14)
-#define BATFETS_MASK		(0x3)
-#define BATFETS_DISABLED	(0x2)
-
-#define CHARGING_VOLTAGE_MV_SAFE	8400
-#define CHARGING_CURRENT_MA_SAFE	1500
-
-/* TODO(b:111815315): Need to config/implement the battery related code */
+#define BATFETS_MASK		(0x6)
+#define BATFETS_DISABLED	(0x0)
 
 static const struct battery_info info = {
-	.voltage_max = 8700,
-	.voltage_normal = 7700,
-	.voltage_min = 6000,
+	.voltage_max = 13200,
+	.voltage_normal = 11550,
+	.voltage_min = 9000,
 	/* Pre-charge values. */
-	.precharge_current = 200, /* mA */
+	.precharge_current = 256, /* mA */
 
 	.start_charging_min_c = 0,
 	.start_charging_max_c = 45,
 	.charging_min_c = 0,
-	.charging_max_c = 50,
-	.discharging_min_c = -20,
-	.discharging_max_c = 70,
+	.charging_max_c = 60,
+	.discharging_min_c = 0,
+	.discharging_max_c = 60,
 };
 
 const struct battery_info *battery_get_info(void)
@@ -81,28 +74,28 @@ static int battery_init(void)
 }
 
 /*
- * Check for case where both XCHG and XDSG bits are set indicating that even
- * though the FG can be read from the battery, the battery is not able to be
- * charged or discharged. This situation might happen when power is reconnected
- * to a battery pack in sleep mode. In this transient siuation, the FG can be
- * read, but the battery is still not able to provide power to the system. The
- * calling function returns batt_pres = BP_NO, which instructs the charging
- * state machine to prevent powering up the AP on battery alone which could lead
- * to a brownout event when the battery isn't able yet to provide power to the
- * system.
+ * The function checks the charge/discharge FET status bits. If the FET status
+ * matches the disconnected value, then BATTERY_DISCONNECTED is return.
  */
 static int battery_check_disconnect(void)
 {
 	int rv;
-	int batt_mfgacc;
+	int reg;
+	uint8_t data[6];
 
 	/* Check if battery charging + discharging is disabled. */
-	rv = sb_read(SB_MANUFACTURER_ACCESS, &batt_mfgacc);
+	rv = sb_read_mfgacc(PARAM_OPERATION_STATUS,
+		SB_ALT_MANUFACTURER_ACCESS, data, sizeof(data));
+	/* Get the lowest 8bits of the OperationStatus() data */
+	reg = data[2];
+
+	CPRINTS("Battery FET: reg 0x%02x", reg);
+
 	if (rv)
 		return BATTERY_DISCONNECT_ERROR;
 
-	if (((batt_mfgacc >> BATFETS_SHIFT) & BATFETS_MASK) ==
-	    BATFETS_DISABLED)
+	reg &= BATFETS_MASK;
+	if (reg == BATFETS_DISABLED)
 		return BATTERY_DISCONNECTED;
 
 	return BATTERY_NOT_DISCONNECTED;
