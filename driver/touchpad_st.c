89d6d428af9fcd2504f05e14ba45aafb9809fb3a
Wei-Han Chen <stimim@google.com>
2018-09-25 14:59:07 +0800

    hacks for local test
    Change-Id: If2033f4563ce4c4ce97d483e9ca1e3ba03f90f41

--- driver/touchpad_st.c
+++ driver/touchpad_st.c
@@ -1055,6 +1055,7 @@ static void st_tp_panel_init_end(void)
 		tp_control &= ~(TP_CONTROL_INIT | TP_CONTROL_INIT_FULL);
 		st_tp_init();
 	} else if (ret == -EC_ERROR_BUSY) {
+		CPRINTS("Panel initialization: busy, will check again.");
 		hook_call_deferred(&st_tp_panel_init_end_data, 100 * MSEC);
 	} else {
 		CPRINTS("Panel initialization failed: %x", -ret);
@@ -1093,6 +1094,7 @@ static void st_tp_panel_init_start(int full)
 		old_cx_version = get_cx_version(
 				system_info.release_info & 0xFF);
 
+	CPRINTS("%s: stop scan", __func__);
 	st_tp_stop_scan();
 	if (st_tp_reset())
 		return;
@@ -1108,7 +1110,7 @@ static void st_tp_panel_init_start(int full)
 		full = old_cx_version != new_cx_version;
 	}
 
-	if (full) {  /* should perform full panel initialization */
+	if (full || 1) {  /* should perform full panel initialization */
 		tx_buf[2] = 0x3;
 		tp_control = TP_CONTROL_INIT_FULL;
 	} else {
@@ -1253,13 +1255,16 @@ static void touchpad_power_control(void)
 	const int enabled = !!(system_state & SYSTEM_STATE_ACTIVE_MODE);
 	int enable = touchpad_should_enable();
 
+	CPRINTS("%s: enabled=%d enable=%d", __func__, enabled, enable);
 	if (enabled == enable)
 		return;
 
 	if (enable)
 		st_tp_start_scan();
-	else
+	else {
+		CPRINTS("%s: stop scan", __func__);
 		st_tp_stop_scan();
+	}
 }
 
 static void touchpad_read_idle_count(void)
@@ -1384,6 +1389,7 @@ void touchpad_task(void *u)
 			tp_control = TP_CONTROL_SHALL_RESET;
 			st_tp_init();
 		} else if (tp_control & TP_CONTROL_SHALL_HALT) {
+			CPRINTS("shall halt");
 			tp_control = 0;
 			st_tp_stop_scan();
 		}
