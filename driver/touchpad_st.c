46b3d58ebbb9efc55d19b121f00959de7ec6e7bf
Wei-Han Chen <stimim@google.com>
2018-09-25 14:59:07 +0800

    hacks for local test
    Change-Id: If2033f4563ce4c4ce97d483e9ca1e3ba03f90f41

--- driver/touchpad_st.c
+++ driver/touchpad_st.c
@@ -992,6 +992,7 @@ static void st_tp_full_initialize_end(void)
 		tp_control &= ~TP_CONTROL_INITIALIZING;
 		st_tp_init();
 	} else if (ret == -EC_ERROR_BUSY) {
+		CPRINTS("full panel initialization: busy, will check again.");
 		hook_call_deferred(&st_tp_full_initialize_end_data, 100 * MSEC);
 	} else {
 		CPRINTS("Full Panel initialization failed: %x", -ret);
@@ -1006,6 +1007,7 @@ static void st_tp_full_initialize_start(void)
 		return;
 
 	tp_control = TP_CONTROL_INITIALIZING;
+	CPRINTS("%s: stop scan", __func__);
 	st_tp_stop_scan();
 	if (st_tp_reset())
 		return;
@@ -1148,13 +1150,16 @@ static void touchpad_power_control(void)
 	const int enabled = !!(system_state & SYSTEM_STATE_ACTIVE_MODE);
 	int enable = touchpad_should_enable();
 
+	CPRINTS("%s: enabled=%d enable=%d", __func__, enabled, enable);
 	if (enabled == enable)
 		return;
 
 	if (enable)
 		st_tp_start_scan();
-	else
+	else {
+		CPRINTS("%s: stop scan", __func__);
 		st_tp_stop_scan();
+	}
 }
 
 /*
@@ -1199,6 +1204,7 @@ static int touchpad_detect_error(void)
 			rx_buf.bytes + i + 4 * 7);
 	}
 
+#if 1
 	for (i = 0; i < ARRAY_SIZE(dump_memory); i++)
 		if (dump_memory[i] != 0xCCCCCCCC)
 			error_detected = 1;
@@ -1226,6 +1232,7 @@ static int touchpad_detect_error(void)
 		error_detected = 1;
 		break;
 	}
+#endif
 
 	if (error_detected) {
 		tp_control |= TP_CONTROL_SHALL_RESET;
@@ -1243,7 +1250,7 @@ void touchpad_task(void *u)
 
 	while (1) {
 		/* wait for at most 1 minute */
-		event = task_wait_event(60 * 1000 * 1000);
+		event = task_wait_event(10 * 1000 * 1000);
 
 		if (event & TASK_EVENT_TIMER)
 			touchpad_detect_error();
@@ -1265,6 +1272,7 @@ void touchpad_task(void *u)
 			tp_control = TP_CONTROL_SHALL_RESET;
 			st_tp_init();
 		} else if (tp_control & TP_CONTROL_SHALL_HALT) {
+			CPRINTS("shall halt");
 			tp_control = 0;
 			st_tp_stop_scan();
 		}
