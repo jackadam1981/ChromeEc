dbd60388bf7278b5dee019869f51ec5598f4da6a
Vadim Bendebury <vbendeb@chromium.org>
2018-09-25 18:15:56 -0700

    cr50: enable ITE CCD programming
    This patch adds a configuration option to support ITE EC programming,
    and enables it for Cr50, adding ITE EC sync sequence generator to the
    image. It also increases I2C RX and TX queue sizes to be able to
    accommodate messages sent during programming session.
    
    BRANCH=cr50, cr50-mp
    BUG=b:75976718
    TEST=with the rest of the patches applied verified that it is possible
         to disable and re-enable clock jitter at run time.
    
    Change-Id: I66ba619924c3d5c548ea546570fd34690e29bc4b
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- include/config.h
+++ include/config.h
@@ -3695,6 +3695,12 @@
 #error Include config.h instead of board.h!
 #endif
 
+/*
+ * Define this to enable ITE EC programming by CCD using the INA i2c
+ * interface.
+ */
+#undef CONFIG_CCD_ITE_PROGRAMMING
+
 #include "config_chip.h"
 #include "board.h"
 
