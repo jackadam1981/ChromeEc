118e816c802b4de920dda275f33d93cbc8af8d98
Vadim Bendebury <vbendeb@chromium.org>
2018-09-25 18:02:55 -0700

    g: prevent locking clock jitter when EC programming is enabled
    Programming ITE EC requires generation of two accurate phase locked
    pulse sequences, the faster one at 200 KHz, with 10% frequency
    variation tolerance.
    
    The only way to achieve this using H1 is driving the GPIOs directly in
    a tight loop, and with CPU clock jitter disabled.
    
    To make it possible, this patch makes jitter configuration locking run
    time configurable.When booting with CCD or EC programming disabled,
    jitter is enabled and jitter configuration is locked. When both CCD is
    open and EC programming is enabled, jitter is enabled but not locked.
    This allows the ITE sequences generation code to temporarily disable
    CPU clock jitter when necessary.
    
    The API is enhanced to allow other H1 boards enable clock jitter
    unconditionally.
    
    Also, had to replace 'unsigned' with 'uint32_t', as the presubmit
    check does not like bare 'unsigned' for some reason.
    
    BRANCH=cr50, cr50-mp
    BUG=b:75976718
    TEST=with the rest of the patches applied verified that it is possible
         to disable and re-enable clock jitter at run time.
    
    Change-Id: I88367b200ceb5b62613f96061d565faa56f4d75a
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -623,6 +623,8 @@ static void  check_board_id_mismatch(void)
 /* Initialize board. */
 static void board_init(void)
 {
+	int lock_jitter;
+
 #ifdef CR50_DEV
 	static enum ccd_state ccd_init_state = CCD_STATE_OPENED;
 #else
@@ -639,8 +641,6 @@ static void board_init(void)
 	init_pmu();
 	reset_wake_logic();
 	init_trng();
-	init_jittery_clock(1);
-	init_runlevel(PERMISSION_MEDIUM);
 	/* Initialize NvMem partitions */
 	nvmem_init();
 	/* Initialize the persistent storage. */
@@ -658,6 +658,12 @@ static void board_init(void)
 
 	/* Load case-closed debugging config.  Must be after initvars(). */
 	ccd_config_init(ccd_init_state);
+
+	lock_jitter = !ccd_is_cap_enabled(CCD_CAP_EC_FLASH);
+
+	/* Do not lock jitter config if EC flash is enabled. */
+	init_jittery_clock_locking_optional(1, 1, lock_jitter);
+	init_runlevel(lock_jitter ? PERMISSION_MEDIUM : PERMISSION_HIGH);
 
 	system_update_rollback_mask_with_both_imgs();
 
