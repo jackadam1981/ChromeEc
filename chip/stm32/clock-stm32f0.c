5e4925af29e42502e8da52b9ad1f6eaaa93286a7
Wei-Han Chen <stimim@google.com>
2018-09-28 16:25:20 +0800

    clock-stm32f0: fail silently if clock is not supported
    The "clock_enable_module()" is implemented by different chips, but
    called by generic codes.  It should not print anything if the clock
    module is not supported, otherwise we will have too much useless error
    messages in console.
    
    BUG=none
    BRANCH=master
    TEST=tested on whiskers
    Signed-off-by: Wei-Han Chen <stimim@chromium.org>
    
    Change-Id: I8f9e1331676a050fbe0cdca90fd7d5aea5a29da1

--- chip/stm32/clock-stm32f0.c
+++ chip/stm32/clock-stm32f0.c
@@ -400,9 +400,6 @@ void clock_enable_module(enum module_id module, int enable)
 			STM32_RCC_APB2ENR &= ~STM32_RCC_APB2ENR_ADCEN;
 		return;
 	}
-
-	CPRINTS("Module %d is not supported for clock %s\n",
-		module, enable ? "enable" : "disable");
 }
 
 void rtc_init(void)
