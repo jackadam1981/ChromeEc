4518335970f9bedd280e7be00f1f3ea8567e2376
Karthikeyan Ramasubramanian <kramasub@chromium.org>
2018-10-01 14:26:17 -0600

    driver/mag_lis2mdl: Add LIS2MDL magnetometer module
    Add support for LIS2MDL magnetometer module in cascade mode to LSM6DSM
    accelerometer module.
    
    BRANCH=none
    BUG=b:115587004
    TEST=none
    
    Change-Id: I06d11777543f14a557ebe6524f7ede15440f1f1e
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@chromium.org>

---
+++ driver/mag_lis2mdl.h
@@ -0,0 +1,17 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* LIS2MSL magnetometer module for Chrome EC */
+
+#ifndef __CROS_EC_MAG_LIS2MDL_H
+#define __CROS_EC_MAG_LIS2MDL_H
+
+#define LIS2MDL_ODR_MIN_VAL	10000
+#define LIS2MDL_ODR_MAX_VAL	100000
+
+extern const struct accelgyro_drv lis2mdl_drv;
+
+#endif /* __CROS_EC_MAG_LIS2MDL_H */
+
