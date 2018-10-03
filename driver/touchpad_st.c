b3f3390fac65887c37bb1169bc754cdd748746fe
Wei-Han Chen <stimim@google.com>
2018-10-03 20:55:04 +0800

    touchpad_st: decide if full init is required by cx version
    BRANCH=nocturne
    BUG=b:117203130
    TEST=on whiskers: downgrade to v18, upgrade to v25, full init is
         triggered
    Signed-off-by: Wei-Han Chen <stimim@chromium.org>
    
    Change-Id: I74582aba181dab31eede1193b400abfc847c1151

--- driver/touchpad_st.c
+++ driver/touchpad_st.c
@@ -1070,19 +1070,54 @@ static void st_tp_panel_init_end(void)
 	}
 }
 
+static uint8_t get_cx_version(uint8_t tp_version)
+{
+	/* CX version is tracked by ST release note. */
+
+	if (tp_version >= 20)
+		return 2;
+
+	if (tp_version >= 18)
+		return 1;
+	return 0;
+}
+
 static void st_tp_panel_init_start(int full)
 {
-	const uint8_t tx_buf[] = {
-		ST_TP_CMD_WRITE_SYSTEM_COMMAND, 0x00, full ? 0x03 : 0x02
+	uint8_t tx_buf[] = {
+		ST_TP_CMD_WRITE_SYSTEM_COMMAND, 0x00, 0x02
 	};
+	uint8_t old_cx_version;
+	uint8_t new_cx_version;
 
 	if (tp_control & (TP_CONTROL_INIT | TP_CONTROL_INIT_FULL))
 		return;
 
-	tp_control = full ? TP_CONTROL_INIT_FULL : TP_CONTROL_INIT;
+	if (full == -1) /* not specified */
+		old_cx_version = get_cx_version(
+				system_info.release_info & 0xFF);
+
 	st_tp_stop_scan();
 	if (st_tp_reset())
 		return;
+
+	if (full == -1) { /* not specified */
+		/*
+		 * On boot, ST firmware will load system info to host data
+		 * memory, So we don't need to reload it.
+		 */
+		st_tp_read_system_info(0);
+		new_cx_version = get_cx_version(
+				system_info.release_info & 0xFF);
+		full = old_cx_version != new_cx_version;
+	}
+
+	if (full) {  /* should perform full panel initialization */
+		tx_buf[2] = 0x3;
+		tp_control = TP_CONTROL_INIT_FULL;
+	} else {
+		tp_control = TP_CONTROL_INIT;
+	}
 
 	CPRINTS("Start panel initialization (full=%d)", full);
 	spi_transaction(SPI, tx_buf, sizeof(tx_buf), NULL, 0);
@@ -1125,7 +1160,7 @@ int touchpad_update_write(int offset, int size, const uint8_t *data)
 	if (offset + size == CONFIG_TOUCHPAD_VIRTUAL_SIZE) {
 		CPRINTS("%s: End update, wait for reset.", __func__);
 
-		st_tp_panel_init_start(1);
+		st_tp_panel_init_start(-1);
 	}
 
 	return EC_SUCCESS;
