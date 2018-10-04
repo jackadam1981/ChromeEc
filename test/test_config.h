b1e4d80a04815d3b061ef99ff2bec981cd5430ce
Yilun Lin <yllin@google.com>
2018-10-04 10:19:57 +0800

    mag_cal: Support fixed-point calculation.
    Modified from floating point version.  This includes changes to
    vec3, vec4, mat33, mat44, and mag_cal.
    Now fixed-point type (fp_*) functions is a function wrapper for both
    fixed-point and floating point version operations:
    
    * define CONFIG_FPU to use floating version mag_cal
    * undef CONFIG_FPU to use fixed-point version mag_cal
    
    Also, add tests for both float and fp types operations.
    
    TEST=define CONFIG_FPU; flash on reef; See ARC++ magnetmeter app moving.
    TEST=undef CONFIG_FPU; flash on reef; See ARC++ magnetmeter app moving.
    TEST=make runtests -j
    TEST=make buildalltests -j
    BUG=b:113364863
    BRANCH=None
    
    Change-Id: Ie695945acb666912babb2a603e09c602a0624d44
    Signed-off-by: Yilun Lin <yllin@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1260704
    Commit-Ready: Yilun Lin <yllin@chromium.org>
    Tested-by: Yilun Lin <yllin@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    (cherry picked from commit 315aaca9467f49bc432ef5f2de9c0e3bb56f0251)

--- test/test_config.h
+++ test/test_config.h
@@ -52,11 +52,6 @@
 #define CONFIG_MAG_CALIBRATE
 #endif
 
-#ifdef TEST_KASA
-#define CONFIG_FPU
-#define CONFIG_ONLINE_CALIB
-#endif
-
 #if defined(TEST_MOTION_LID) || defined(TEST_MOTION_ANGLE) || \
 	defined(TEST_MOTION_ANGLE_TABLET) || defined(TEST_MOTION_SENSE_FIFO)
 enum sensor_id {
