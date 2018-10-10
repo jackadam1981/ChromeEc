7af43e0c4363b66f4666fa95dafabb10bfafac3c
Namyoon Woo <namyoon@chromium.org>
2018-10-10 15:56:14 -0700

    gsctool: display board information in human readable form.
    gsctool with "-V" (verbose) mode and "-i" (get board_id)
    parses Board ID and flags, and displays the information in
    human readable form.
    
    BRANCH=gsctools
    BUG=b:109771014
    TEST=manually tested issuing 'gsctool -i -V'.
    (DUT)# gsctool -aiV
    Board ID space: 41434245:bebcbdba:0000ff7f
    	Brand : ACBE
    	Phase : prePVT
    
    Change-Id: I060ab20464914d0f116a0f2d914d50976903fc41
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -1657,6 +1657,50 @@ static void process_wp(struct transfer_descriptor *td)
 		"forced disabled");
 }
 
+#define FOUR_FF_BYTES	0xFFFFFFFF
+/*
+ * Parse and print the board information in detail.
+ */
+static void print_board_info(uint32_t bid_type, uint32_t bid_type_inv,
+			uint32_t bid_flags)
+{
+	uint32_t i;
+
+	if (bid_type == FOUR_FF_BYTES && bid_type_inv == FOUR_FF_BYTES &&
+	    bid_flags == FOUR_FF_BYTES) {
+		printf("Board ID is not programmed.\n");
+		return;
+	}
+
+	/* Display board id type in ASCII */
+	printf("\tBrand : ");
+	i = 32;
+	while (i >= 8) {
+		uint8_t ch;
+
+		i -= 8;
+		ch = (bid_type >> i) & 0xFF;
+
+		/* If not in printable ASCII range, print '?' instead. */
+		if (ch < 0x20 || ch >= 0x7F)
+			ch = '?';
+
+		printf("%c", ch);
+	}
+	printf("\n");
+
+	/* Display the build phase*/
+	printf("\tPhase : ");
+	if (bid_flags == 0x7F7F || bid_flags == 0xFF7F)
+		printf("prePVT");
+	else if (bid_flags == 0x7F80 || bid_flags == 0xFF80)
+		printf("MP");
+	else if (bid_flags == 0xFF00)
+		printf("unknown");
+	else
+		printf("invalid");
+	printf("\n");
+}
 
 void process_bid(struct transfer_descriptor *td,
 		 enum board_id_action bid_action,
@@ -1672,9 +1716,17 @@ void process_bid(struct transfer_descriptor *td,
 				    bid, &response_size);
 
 		if (response_size == sizeof(*bid)) {
+			uint32_t bid_type = be32toh(bid->type);
+			uint32_t bid_type_inv = be32toh(bid->type_inv);
+			uint32_t bid_flags = be32toh(bid->flags);
+
 			printf("Board ID space: %08x:%08x:%08x\n",
-			       be32toh(bid->type), be32toh(bid->type_inv),
-			       be32toh(bid->flags));
+			       bid_type, bid_type_inv, bid_flags);
+
+			if (verbose_mode)
+				print_board_info(bid_type, bid_type_inv,
+						bid_flags);
+
 			return;
 		}
 		fprintf(stderr, "Error reading board ID: response size %zd,"
