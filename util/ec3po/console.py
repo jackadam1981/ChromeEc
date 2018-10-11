3bfb0414fa875580a69c47fe9bae0097cdb3018b
Matthew Blecker <matthewb@chromium.org>
2018-10-11 13:42:49 -0700

    ec3po: Remove sys.exit() call from the end of StartLoop() functions.
    This removes explicit process exit() from both console and interpeter ec3po StartLoop() functions.  Use of exit() is incompatible with running these functions in threads instead of subprocesses.  This should be a no-op, since letting execution finish in Python should be equivalent to sys.exit().
    
    Since exit() in the finally: block effectively suppressed exceptions, the same behavior is now achieved in interpreter.StartLoop() by use of an except: block, same as console.StartLoop() already had.
    
    BRANCH=none
    BUG=b:79684405
    
    Signed-off-by: Matthew Blecker <matthewb@chromium.org>
    
    Change-Id: Ie92610de88fea78f6c50f5e49b9d0940f209cd92

--- util/ec3po/console.py
+++ util/ec3po/console.py
@@ -907,10 +907,14 @@ def StartLoop(console, command_active):
       while not console.oobm_queue.empty():
         console.logger.debug('OOBM queue ready for reading.')
         console.ProcessOOBMQueue()
+
   except KeyboardInterrupt:
     pass
+
+  # Is suppressing all exceptions actually necessary or desirable?
   except:
     traceback.print_exc()
+
   finally:
     # Unregister poll.
     ep.unregister(console.master_pty)
@@ -921,8 +925,6 @@ def StartLoop(console, command_active):
     os.close(console.master_pty)
     os.close(console.interface_pty)
     console.logger.debug('Exit ec3po console loop for %s', console.user_pty)
-    # Exit.
-    sys.exit(0)
 
 
 def main(argv):
