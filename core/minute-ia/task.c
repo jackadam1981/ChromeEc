f7161fe18f2bdf74dd0d67acd0315c7c1549c33b
Hyungwoo Yang <hyungwoo.yang@intel.com>
2018-10-17 20:39:32 -0700

    ISH: remove sending MNG_RESET_NOTIFY
    remove sending MNG_RESET_NOTIFY.
    sending MNG_RESET_NOTIFY should be done by IPC task.
    
    Change-Id: Iea2d2864c67763c8c8e18b520c5a776b5ce469fb

--- core/minute-ia/task.c
+++ core/minute-ia/task.c
@@ -438,14 +438,6 @@ void task_enable_all_tasks(void)
 	/* BUG: task_start() was likely already called */
 	start_called = 1;
 
-	/* The host OS driver should wait till the FW completes all hook inits.
-	 * Otherwise, FW may fail to respond to host commands or crash when
-	 * not fully initialized. This MNG (management) type IPC message sent
-	 * asynchronously from the FW indicates completion of initialization.
-	 */
-	CPUTS("*** MNG FW ready ****\n");
-	REG32(IPC_ISH2HOST_DOORBELL) = IPC_BUILD_MNG_MSG(0x8, 1);
-
 	interrupt_enable();
 	/* Reschedule the highest priority task. */
 	__schedule(0, 0);
