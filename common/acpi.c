e7ba4a12b5458854720390e42cca21ce5ba4dafa
Furquan Shaikh <furquan@google.com>
2018-10-19 16:23:50 -0700

    dptf: Add support for DDPN
    This change adds Device DPTF Profile Number(DDPN) to
    EC_ACPI_MEM_DEVICE_ORIENTATION field to indicate to host which DPTF
    profile should be loaded depending upon the device mode. This is done
    to de-couple DPTF table loading from tablet mode flag to allow
    different drivers within EC to set the profile number depending upon
    their design.
    
    In order to maintain backward compatibility, this change treats 0 as
    reserved to indicate to host that it should fall back to using tablet
    mode switch to decide which DPTF table to load. Additionally, it
    provides helper function to allow drivers to set the profile number
    that will be returned on host query for
    EC_ACPI_MEM_DEVICE_ORIENTATION.
    
    It also adds a new config option CONFIG_DPTF_MULTI_PROFILE that should
    be selected by the boards that support multiple DPTF profiles on the
    host side.
    
    BUG=b:117844490
    BRANCH=None
    TEST=make -j buildall
    
    Change-Id: Idfa0cfea48b9df346533342647258474fb63e86c
    Signed-off-by: Furquan Shaikh <furquan@google.com>

--- common/acpi.c
+++ common/acpi.c
@@ -37,6 +37,13 @@ static uint8_t __bss_slow acpi_mem_test;
 #ifdef CONFIG_DPTF
 static int __bss_slow dptf_temp_sensor_id;	/* last sensor ID written */
 static int __bss_slow dptf_temp_threshold;	/* last threshold written */
+
+/*
+ * Current DPTF profile number. This is by default initialized to 0 (which means
+ * that the profile number is invalid).
+ */
+static int current_dptf_profile = DPTF_PROFILE_DEFAULT;
+
 #endif
 
 #ifdef CONFIG_USB_PORT_POWER_DUMB
@@ -80,6 +87,38 @@ static void acpi_disable_burst_deferred(void)
 	CPUTS("ACPI missed burst disable?");
 }
 DECLARE_DEFERRED(acpi_disable_burst_deferred);
+
+#ifdef CONFIG_DPTF
+
+static int acpi_dptf_is_profile_valid(int n)
+{
+#ifdef CONFIG_DPTF_MULTI_PROFILE
+	if ((n < DPTF_PROFILE_VALID_FIRST) || (n > DPTF_PROFILE_VALID_LAST))
+		return EC_ERROR_INVAL;
+#else
+	if (n != DPTF_PROFILE_DEFAULT)
+		return EC_ERROR_INVAL;
+#endif
+
+	return EC_SUCCESS;
+}
+
+int acpi_dptf_set_profile_num(int n)
+{
+	int ret = acpi_dptf_is_profile_valid(n);
+
+	if (ret == EC_SUCCESS)
+		current_dptf_profile = n;
+
+	return ret;
+}
+
+int acpi_dptf_get_profile_num(void)
+{
+	return current_dptf_profile;
+}
+
+#endif
 
 /* Read memmapped data, returns read data or 0xff on error. */
 static int acpi_read(uint8_t addr)
@@ -178,8 +217,16 @@ int acpi_ap_to_ec(int is_cmd, uint8_t value, uint8_t *resultptr)
 #endif
 
 		case EC_ACPI_MEM_DEVICE_ORIENTATION:
-			result = tablet_get_mode();
+			result = (tablet_get_mode() & EC_ACPI_MEM_TBMD_MASK)
+				<< EC_ACPI_MEM_TBMD_SHIFT;
+
+#ifdef CONFIG_DPTF
+			result |= (acpi_dptf_get_profile_num() &
+				   EC_ACPI_MEM_DDPN_MASK)
+				<< EC_ACPI_MEM_DDPN_SHIFT;
+#endif
 			break;
+
 
 		case EC_ACPI_MEM_DEVICE_FEATURES0:
 		case EC_ACPI_MEM_DEVICE_FEATURES1:
--- include/acpi.h
+++ include/acpi.h
@@ -20,4 +20,50 @@
  */
 int acpi_ap_to_ec(int is_cmd, uint8_t value, uint8_t *result);
 
+enum acpi_dptf_profile_num {
+	/*
+	 * Value of 0 is reserved meaning the Device DPTF Profile Number in EC
+	 * shared memory is invalid and host should fallback to using tablet
+	 * mode switch to determine the DPTF table to load.
+	 */
+	DPTF_PROFILE_INVALID = 0,
+
+	/*
+	 * Default DPTF profile when multi-profile is not supported or before
+	 * any driver updates the profile number.
+	 */
+	DPTF_PROFILE_DEFAULT = 1,
+
+	/* Range of valid values for DPTF profile number. */
+	DPTF_PROFILE_VALID_FIRST = 1,
+	DPTF_PROFILE_VALID_LAST = 7,
+
+	/* Standard convertible profiles */
+	DPTF_PROFILE_CLAMSHELL = 1,
+	DPTF_PROFILE_FLIPPED_360_MODE = 2,
+
+	/* Standard detachable profiles */
+	DPTF_PROFILE_BASE_ATTACHED = 1,
+	DPTF_PROFILE_BASE_DETACHED = 2,
+};
+
+/**
+ * Set current DPTF profile in EC shared memory.
+ *
+ * @param prof_num      Profile number of current profile. Valid values are 1-7.
+ *                      See enum acpi_dptf_profile_num for standard profile
+ *                      numbers for convertibles/detachables.
+ * @return              EC_SUCCESS if operation was successful, EC_ERROR* in
+ *                      case of error.
+ */
+int acpi_dptf_set_profile_num(int n);
+
+/**
+ * Get value of current DPTF profile.
+ *
+ * @return              DPTF Profile number currently set to be shared with the
+ *                      host using EC shared memory.
+ */
+int acpi_dptf_get_profile_num(void);
+
 #endif	/* __CROS_EC_ACPI_H */
