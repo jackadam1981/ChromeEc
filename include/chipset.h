6d59004c5a974f1d3e9e4069c8da3e17a9a48d21
Nicolas Boichat <drinkcat@chromium.org>
2018-10-22 15:07:45 +0800

    power/mt8183: Implement watchdog-initiated reset
    AP watchdog line can fall in either of 2 cases:
     - AP asserts watchdog while the AP is on: this is a real
       AP-initiated reset.
     - EC asserted GPIO_AP_SYS_RST_L, so the AP is in reset and AP
       watchdog falls as well. This is _not_ a watchdog reset. We
       mask these cases by disabling the interrupt just before
       shutting down the AP, and re-enabling it before starting the
       AP.
    
    Also, take the opportunity to move warm reset code out of board
    file into generic MT8183 power code, as well as code to enable
    interrupts.
    
    BRANCH=none
    BUG=b:109900671
    TEST=apshutdown => EC understand this is an EC-initiated shutdown
    TEST=Use test-wd from bug, see that EC detects it is a watchdog.
    
    Change-Id: I02037e5be0254fef991ae2459be35e4561e0994c
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- include/chipset.h
+++ include/chipset.h
@@ -68,6 +68,8 @@ enum chipset_reset_reason {
 	CHIPSET_RESET_AP_REQ,
 	/* Reset as side-effect of startup sequence */
 	CHIPSET_RESET_INIT,
+	/* EC detected an AP watchdog event. */
+	CHIPSET_RESET_AP_WATCHDOG,
 	CHIPSET_RESET_COUNT,
 };
 
@@ -202,9 +204,17 @@ void chipset_handle_reboot(void);
 /**
  * GPIO interrupt handler of reset request from AP.
  *
- * It is used in SDM845 chipset power sequence.
+ * It is used in SDM845/MT8183 chipset power sequence.
  */
 void chipset_reset_request_interrupt(enum gpio_signal signal);
+
+/**
+ * GPIO interrupt handler of watchdog from AP.
+ *
+ * It is used in MT8183 chipset, where it must be setup to trigger on falling
+ * edge only.
+ */
+void chipset_watchdog_interrupt(enum gpio_signal signal);
 
 #ifdef CONFIG_CMD_AP_RESET_LOG
 
