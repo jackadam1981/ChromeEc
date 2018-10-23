6768de8dd3abc706ae54021d0848ca94dbf58965
Daisuke Nojiri <dnojiri@chromium.org>
2018-10-23 13:53:11 -0700

    USB-PD: Debounce when trying try.src
    Currently, if try.src is enabled, the TCPM doesn't debounce for
    SNK connection.
    
    This patch makes TCPM wait for t CCDebounce + tTryCCDebounce when
    detecting SNK connection after switching from SNK to SRC.
    
    BUG=b:115464001
    BRANCH=none
    TEST=Verify the followings on both ports of Sona:
    1. DP mode is entered and picture is shown on HP s240.
    2. DUT is charged by S240n.
    3. DUT is charged by Zinger and Apple 87W charger.
    
    Change-Id: I4f91bfca4f829f051d1c33f88f3664fad9b83e9f

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -163,6 +163,7 @@ enum pd_rx_errors {
 #define PD_T_PS_HARD_RESET     (25*MSEC) /* between 25ms and 35ms */
 #define PD_T_ERROR_RECOVERY    (25*MSEC) /* 25ms */
 #define PD_T_CC_DEBOUNCE       (100*MSEC) /* between 100ms and 200ms */
+#define PD_T_TRY_CC_DEBOUNCE   (10*MSEC)  /* between 10ms and 20ms */
 /* DRP_SNK + DRP_SRC must be between 50ms and 100ms with 30%-70% duty cycle */
 #define PD_T_DRP_SNK           (40*MSEC) /* toggle time for sink DRP */
 #define PD_T_DRP_SRC           (30*MSEC) /* toggle time for source DRP */
