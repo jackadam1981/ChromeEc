d4cfb9b1489535e164c99957017850760de83911
Bob Moragues <moragues@google.com>
2018-10-25 18:00:17 -0700

    gsctool: Add options to print out RLZ codes
    iThis RLZ code is used by cr50-verify-ro.sh.
    The RLZ code selects the verify_ro db to use.
    
    BUG=b:90495590
    BRANCH=none
    TEST=gsctool -i -M; gsctool -i -M -t
    
    Unit Test Rewults:
    
    ilocalhost ~ # gsctool -i -M
    open_device 18d1:5014
    found interface 3 endpoint 4, chunk_len 64
    READY
    -------
    BID_TYPE=4e425153
    BID_TYPE_INV=b1bdaeac
    BID_FLAGS=00007f7f
    BID_RLZ=NBQS <-- CORRECT DUT RLZ
    localhost ~ # gsctool -i -M -t
    BID_TYPE=58574a45
    BID_TYPE_INV=a7a8b5ba
    BID_FLAGS=00007f7f
    BID_RLZ=XWJE <-- CORRECT Test Device RLZ
    localhost ~ #
    
    Change-Id: I12fbca6885e3a1453544a1d379ad12ef3f6fd290
    Signed-off-by: Bob Moragues <moragues@chromium.org>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -1812,12 +1812,19 @@ void process_bid(struct transfer_descriptor *td,
 				"BID_TYPE_INV", "%08x", be32toh(bid->type_inv));
 			print_machine_output(
 				"BID_FLAGS", "%08x", be32toh(bid->flags));
-
+			print_machine_output(
+				"BID_RLZ", "%c%c%c%c",
+				((const char *)bid)[0],
+				((const char *)bid)[1],
+				((const char *)bid)[2],
+				((const char *)bid)[3]);
 		} else {
-			printf("Board ID space: %08x:%08x:%08x\n",
-			       be32toh(bid->type),
-			       be32toh(bid->type_inv),
-			       be32toh(bid->flags));
+			if (bid_action == bid_get) {
+				printf("Board ID space: %08x:%08x:%08x\n",
+				       be32toh(bid->type),
+				       be32toh(bid->type_inv),
+				       be32toh(bid->flags));
+			}
 		}
 
 		return;
