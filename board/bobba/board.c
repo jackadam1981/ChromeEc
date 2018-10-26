8f48e0467d755fc7e1ffc058f45b79648cd2696f
Jett Rink <jettrink@chromium.org>
2018-10-26 13:36:26 -0600

    octopus: update active_mask for consistency
    The base gyro and base accel are the same chip and they are on in S3 and
    S0 and off in S5. Update the active_make for all other octopus boards
    that haven't already updated it.
    
    BRANCH=none
    BUG=none
    TEST=build and sensors still work.
    
    Change-Id: I96004ba0b20dd9366848bb2fe610c250e07850aa
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- board/bobba/board.c
+++ board/bobba/board.c
@@ -183,7 +183,7 @@ struct motion_sensor_t motion_sensors[] = {
 	},
 	[BASE_GYRO] = {
 	 .name = "Base Gyro",
-	 .active_mask = SENSOR_ACTIVE_S0,
+	 .active_mask = SENSOR_ACTIVE_S0_S3,
 	 .chip = MOTIONSENSE_CHIP_BMI160,
 	 .type = MOTIONSENSE_TYPE_GYRO,
 	 .location = MOTIONSENSE_LOC_BASE,
@@ -198,15 +198,15 @@ struct motion_sensor_t motion_sensors[] = {
 	 .max_frequency = BMI160_GYRO_MAX_FREQ,
 	},
 	[VSYNC] = {
-		.name = "Camera VSYNC",
-		.active_mask = SENSOR_ACTIVE_S0,
-		.chip = MOTIONSENSE_CHIP_GPIO,
-		.type = MOTIONSENSE_TYPE_SYNC,
-		.location = MOTIONSENSE_LOC_CAMERA,
-		.drv = &sync_drv,
-		.default_range = 0,
-		.min_frequency = 0,
-		.max_frequency = 1,
+	.name = "Camera VSYNC",
+	.active_mask = SENSOR_ACTIVE_S0,
+	.chip = MOTIONSENSE_CHIP_GPIO,
+	.type = MOTIONSENSE_TYPE_SYNC,
+	.location = MOTIONSENSE_LOC_CAMERA,
+	.drv = &sync_drv,
+	.default_range = 0,
+	.min_frequency = 0,
+	.max_frequency = 1,
 	},
 };
 
