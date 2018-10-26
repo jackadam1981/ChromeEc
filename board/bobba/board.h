59d35c90a07fe4b22fa90d5dce4b4e896bc7c965
Enrico Granata <egranata@chromium.org>
2018-10-26 10:45:53 -0700

    bobba: disable verbose EC logging of host commands
    Once the sensors are enabled, the default logging mode of the EC
    is extremely verbose. Tune this down by removing the logging
    statements at every host command.
    
    BRANCH=none
    TEST=observe fewer log messages coming in
    BUG=b:118443377, crbug:896347
    
    Change-Id: Ib93f9280b8f70a8ecdc53621c4364173c77efed6
    Signed-off-by: Enrico Granata <egranata@chromium.org>

--- board/bobba/board.h
+++ board/bobba/board.h
@@ -8,6 +8,14 @@
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
 
+/*
+ * By default, enable all console messages excepted HC, ACPI and event:
+ * The sensor stack is generating a lot of activity.
+ */
+#define CC_DEFAULT     (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
+#undef CONFIG_HOSTCMD_DEBUG_MODE
+#define CONFIG_HOSTCMD_DEBUG_MODE HCDEBUG_OFF
+
 /* Select Baseboard features */
 #define VARIANT_OCTOPUS_EC_NPCX796FB
 #define VARIANT_OCTOPUS_CHARGER_ISL9238
