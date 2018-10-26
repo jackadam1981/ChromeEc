ee3995cba8276712aee52e93195c8fd29e156e85
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2018-10-26 16:55:04 +0800

    it83xx/intc:MsgID repeat
      According USB-PD spec ch.6.7.1, if transmitter sends
    the repetitive message id packet, receiver does not respond
    subsequent message(except softreset).
    
    BUG=None
    BRANCH=None
    TEST=GRL USB-PD test
    
    Change-Id: Ib257208845f1e2bbc8c4de65ad809e295e0133f0
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- driver/tcpm/it83xx_pd.h
+++ driver/tcpm/it83xx_pd.h
@@ -106,5 +106,7 @@ extern const struct usbpd_ctrl_t usbpd_ctrl_regs[];
 extern const struct tcpm_drv it83xx_tcpm_drv;
 /* Disable integrated pd module */
 void it83xx_disable_pd_module(int port);
+extern int msgid_last[USBPD_PORT_COUNT];
+extern void init_msgidlast_var(int *pmsgid_last, int port);
 
 #endif /* __CROS_EC_DRIVER_TCPM_IT83XX_H */
