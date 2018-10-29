edd71f528a764331b94641a632f1c134a904f3fe
Daisuke Nojiri <dnojiri@chromium.org>
2018-10-29 14:12:07 -0700

    chgstv2: Notify on display percentage change
    This patch makes the charger task v2 notify HOOK_BATTERY_SOC_CHANGE
    handlers when the display charge percentage changes. This allows EC
    to update LEDs synchronously to the number on the display.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b:109954565,b:80270446
    BRANCH=none
    TEST=Verify charge LED changes to white (full) on Sona synchronously
    to the display percentage.
    TEST=Verify charge LED changes to blinking white (low) on Sona
    within 30 seconds synchronously to the display percentage.
    
    Change-Id: Ib2c587ad3307fe62b268a25a93344c88c6c137f3

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -78,7 +78,7 @@ static int charge_request(int voltage, int current);
 static const struct battery_info *batt_info;
 static struct charge_state_data curr;
 static enum charge_state_v2 prev_state;
-static int prev_ac, prev_charge, prev_full;
+static int prev_ac, prev_charge, prev_full, prev_disp_charge;
 static enum battery_present prev_bp;
 static int is_full; /* battery not accepting current */
 static enum ec_charge_control_mode chg_ctl_mode;
@@ -1109,13 +1109,13 @@ static void show_charging_progress(void)
 #endif
 
 	if (rv)
-		CPRINTS("Battery %d%% / ??h:?? %s%s",
-			curr.batt.state_of_charge,
+		CPRINTS("Battery %d%% (Display %d%%) / ??h:?? %s%s",
+			curr.batt.state_of_charge, curr.batt.display_pct,
 			to_full ? "to full" : "to empty",
 			is_full ? ", not accepting current" : "");
 	else
-		CPRINTS("Battery %d%% / %dh:%d %s%s",
-			curr.batt.state_of_charge,
+		CPRINTS("Battery %d%% (Display %d%%) / %dh:%d %s%s",
+			curr.batt.state_of_charge, curr.batt.display_pct,
 			minutes / 60, minutes % 60,
 			to_full ? "to full" : "to empty",
 			is_full ? ", not accepting current" : "");
@@ -1813,9 +1813,11 @@ wait_for_it:
 		    (charge_base != prev_charge_base) ||
 #endif
 		    (is_full != prev_full) ||
-		    (curr.state != prev_state)) {
+		    (curr.state != prev_state) ||
+		    (curr.batt.display_pct != prev_disp_charge)) {
 			show_charging_progress();
 			prev_charge = curr.batt.state_of_charge;
+			prev_disp_charge = curr.batt.display_pct;
 #ifdef CONFIG_EC_EC_COMM_BATTERY_MASTER
 			prev_charge_base = charge_base;
 #endif
@@ -2116,6 +2118,11 @@ int charge_get_percent(void)
 	 * anything.
 	 */
 	return is_full ? 100 : curr.batt.state_of_charge;
+}
+
+int charge_get_display_charge(void)
+{
+	return curr.batt.display_pct;
 }
 
 int charge_get_battery_temp(int idx, int *temp_ptr)
