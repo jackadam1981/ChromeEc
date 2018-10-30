b76f5c98fb022be091b3eb97effac5c3ea8ba847
Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>
2018-10-30 15:12:45 +0800

    [TEST] sample of interrupt
      1. INT with internal pull down (custom)
      2. LOW->HIGH trigger (custom)
      3. debounce time (custom)
      4. do something in bottom half
    
    Change-Id: Ie582cfc0bce1c37dfdbd3ef1474272990aaeb447
    Signed-off-by: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>

--- board/bobba/board.c
+++ board/bobba/board.c
@@ -283,3 +283,15 @@ void lid_angle_peripheral_enable(int enable)
 		keyboard_scan_enable(enable, KB_SCAN_DISABLE_LID_ANGLE);
 }
 #endif
+static void bar_deferred(void)
+{
+	if (gpio_get_level(GPIO_BAR) == 1) {
+		/* do something */
+		;
+	}
+}
+DECLARE_DEFERRED(bar_deferred);
+void bar_interrupt(enum gpio_signal signal)
+{
+	hook_call_deferred(&bar_deferred_data, 30 * MSEC);
+}
--- board/bobba/board.h
+++ board/bobba/board.h
@@ -106,6 +106,8 @@ enum battery_type {
 	BATTERY_TYPE_COUNT,
 };
 
+void bar_interrupt(enum gpio_signal signal);
+
 #endif /* !__ASSEMBLER__ */
 
 #endif /* __CROS_EC_BOARD_H */
