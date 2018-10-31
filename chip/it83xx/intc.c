7119b199ac2faed4319e46fc5d41e2de846e2014
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2018-10-31 15:03:02 +0800

    it83xx/intc:message id of pd packet repeat
      According USB-PD spec ch.6.7.1, if transmitter sends
    the repetitive message id packet, receiver does not respond
    subsequent message(except softreset).
    
    BRANCH=None
    BUG=None
    TEST=GRL USB-PD test
    
    Change-Id: Ib257208845f1e2bbc8c4de65ad809e295e0133f0
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- chip/it83xx/intc.c
+++ chip/it83xx/intc.c
@@ -11,8 +11,55 @@
 #include "task.h"
 #include "tcpm.h"
 #include "usb_pd.h"
+#include "console.h"
 
 #ifdef CONFIG_USB_PD_TCPM_ITE83XX
+/* Store each port last message id of received packet */
+int message_id_last[USBPD_PORT_COUNT];
+
+/* Init last message id variable of received packet */
+void init_message_id_last_var(int port)
+{
+	/* Init invalid value (message id = 0 ~ 7) */
+	message_id_last[port] = 0xff;
+}
+
+int check_message_id_repeat(int port)
+{
+	int msg_header = IT83XX_USBPD_RMH(port);
+	int msg_type = PD_HEADER_TYPE(msg_header);
+	int msg_id = PD_HEADER_ID(msg_header);
+	int msg_cnt = PD_HEADER_CNT(msg_header);
+	char fsoft_acpt = (msg_id == 0 && (msg_type == 0x0D/*Softreset*/ ||
+			   msg_type == 0x03/*Accept*/) && msg_cnt == 0) ? 1 : 0;
+
+	/*
+	 * Check if repeat message id, if yes don't respond subsequent
+	 * messages, expect softreset.
+	 */
+	if (message_id_last[port] == msg_id && !fsoft_acpt) {
+		/* If clear this bit, USBPD receives next packet */
+		IT83XX_USBPD_MRSR(port) = USBPD_REG_MASK_RX_MSG_VALID;
+		ccprints("message id repeat: message_id_last[%d]= %d", port,
+			  message_id_last[port]);
+		/* message id repeat */
+		return 1;
+	}
+
+	/*
+	 * Message id not repeat, but softreset subsequent messages should
+	 * init last message id variable.
+	 */
+	if (fsoft_acpt)
+		init_message_id_last_var(port);
+	else
+		message_id_last[port] = msg_id;
+	ccprints("chk pass message_id_last[%d] = %d", port,
+		 message_id_last[port]);
+	/* message id not repeat */
+	return 0;
+}
+
 static void chip_pd_irq(enum usbpd_port port)
 {
 	task_clear_pending_irq(usbpd_ctrl_regs[port].irq);
@@ -21,11 +68,16 @@ static void chip_pd_irq(enum usbpd_port port)
 	if (USBPD_IS_HARD_RESET_DETECT(port)) {
 		/* clear interrupt */
 		IT83XX_USBPD_ISR(port) = USBPD_REG_MASK_HARD_RESET_DETECT;
+		/* Init last message id variable of received packet */
+		init_message_id_last_var(port);
+		ccprints("hard message_id_last[%d] = %d", port,
+			 message_id_last[port]);
 		task_set_event(PD_PORT_TO_TASK_ID(port),
 			PD_EVENT_TCPC_RESET, 0);
 	} else {
 		if (USBPD_IS_RX_DONE(port)) {
-			tcpm_enqueue_message(port);
+			if (!(check_message_id_repeat(port)))
+				tcpm_enqueue_message(port);
 			/* clear RX done interrupt */
 			IT83XX_USBPD_ISR(port) = USBPD_REG_MASK_MSG_RX_DONE;
 		}
--- driver/tcpm/it83xx_pd.h
+++ driver/tcpm/it83xx_pd.h
@@ -106,5 +106,9 @@ extern const struct usbpd_ctrl_t usbpd_ctrl_regs[];
 extern const struct tcpm_drv it83xx_tcpm_drv;
 /* Disable integrated pd module */
 void it83xx_disable_pd_module(int port);
+/* Store each port last message id of received packet */
+extern int message_id_last[USBPD_PORT_COUNT];
+/* Init last message id variable of received packet */
+extern void init_message_id_last_var(int port);
 
 #endif /* __CROS_EC_DRIVER_TCPM_IT83XX_H */
