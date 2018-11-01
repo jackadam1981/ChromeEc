27b71182e53c3abb21891003fc445b6e8cb2d617
Namyoon Woo <namyoon@chromium.org>
2018-10-31 17:35:09 -0700

    cr50: revoke key ladder and clear NVMEM cache on disabling TPM
    Disabling TPM will do revoke H1 key laddder and clear NVMEM
    cached data in SRAM.
    Querying TPM_MODE or enabling TPM_MODE will fail if H1 Key
    Ladder is already revoked.
    To have TPM enabled back for TPM transactions from AP firmware
    during either resume or soft-reset, CR50 do a soft reset H1
    if Key Ladder remains revoked.
    
    BUG=b:118504817
    BRANCH=cr50
    TEST=Manually tested with TPM disabling and Resume or Warm Reboot.
    
    (1) Resume
    $ trunks_send --raw 80 01 00 00 00 0c 00 00 01 45 00 01
    80010000000A00000000
    $ gsctool -a -m disable
    TPM Mode: disabled (2)
    $ echo mem > /sys/power/state
    
    (press key on chromebook either after three seconds or in a second.)
    
    (2) Warm Reboot
    $ gsctool -a -m disable
    
    (press refresh + power button or run kernel command 'reboot')
    
    Check Chrome os boot ok.
    No TPM command failures were observed (in CR50 console).
    
    Change-Id: I32fffc432a9a6068ea324a97225974c581cb9359
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- chip/g/system.c
+++ chip/g/system.c
@@ -129,12 +129,14 @@ void system_reset(int flags)
 
 	/*
 	 * On CR50 we want every reset be hard reset, causing the entire
-	 * chromebook to reboot: we don't want the TPM reset while the AP
-	 * stays up.
+	 * chromebook to reboot, except one case, SYSTEM_RESET_SOFT_H1.
+	 * SYSTEM_RESET_SOFT_H1 will reset H1 only, not EC. It is to recover
+	 * TPM function enabled back, when Key Ladder remains still disabled.
 	 */
-	GR_PMU_GLOBAL_RESET = GC_PMU_GLOBAL_RESET_KEY;
+	if ((flags & SYSTEM_RESET_SOFT_H1) == 0) {
 #else
 	if (flags & SYSTEM_RESET_HARD) {
+#endif
 		/* Reset the full microcontroller */
 		GR_PMU_GLOBAL_RESET = GC_PMU_GLOBAL_RESET_KEY;
 	} else {
@@ -166,7 +168,6 @@ void system_reset(int flags)
 			GC_PMU_LOW_POWER_DIS_START_LSB,
 			1);
 	}
-#endif  /* ^^^^^^^ CHIP_FAMILY_CR50 Not defined */
 
 	/* Wait for reboot; should never return  */
 	while (1)
