a3b609732be1ca18356715b9cb125b262ded9957
Namyoon Woo <namyoon@chromium.org>
2018-10-31 17:35:09 -0700

    cr50: revoke key ladder on disabling TPM
    Disabling TPM will do revoke H1 key laddder.
    - Querying TPM_MODE or enabling TPM_MODE will fail if H1 Key
      Ladder is already revoked.
    
    BUG=b:118504817
    BRANCH=cr50
    TEST=Manually tested with TPM disabling and Resume or Warm Reboot.
    
    (1) Resume
    $ trunks_send --raw 80 01 00 00 00 0c 00 00 01 45 00 01
    80010000000A00000000
    
    $ gsctool -a -m disable
    TPM Mode: disabled (2)
    $ echo mem > /sys/power/state
    
    (press key on chromebook either after three seconds
     or in a second.)
    
    (2) tested the case of Cold|Warm Reboot to Windows
        or resume in Windows
    
    Change-Id: I32fffc432a9a6068ea324a97225974c581cb9359
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -1380,7 +1380,6 @@ static int command_sysinfo(int argc, char **argv)
 	const struct SignedHeader *h;
 	int reset_count = GREG32(PMU, LONG_LIFE_SCRATCH0);
 	char rollback_str[15];
-	uint8_t tpm_mode;
 
 	ccprintf("Reset flags: 0x%08x (", system_get_reset_flags());
 	system_print_reset_flags();
@@ -1408,11 +1407,7 @@ static int command_sysinfo(int argc, char **argv)
 	system_get_rollback_bits(rollback_str, sizeof(rollback_str));
 	ccprintf("Rollback:    %s\n", rollback_str);
 
-	tpm_mode = get_tpm_mode();
-	ccprintf("TPM MODE:    %s (%d)\n",
-		(tpm_mode == TPM_MODE_DISABLED) ? "disabled" : "enabled",
-		tpm_mode);
-
+	print_tpm_mode();
 	return EC_SUCCESS;
 }
 DECLARE_SAFE_CONSOLE_COMMAND(sysinfo, command_sysinfo,
