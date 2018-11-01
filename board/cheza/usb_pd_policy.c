c2b007fe888c3b864472e3d046e3481d67d39f3e
Wai-Hong Tam <waihong@google.com>
2018-11-01 15:02:32 -0700

    cheza: Apply the fix to handle two or more HPD events
    Apply the following fix to Cheza:
      http://crrev.com/c/388737
    
    Some Type-C to DP adapters generate two or more HPD events while
    others generate only one HPD event. Currently hotplugging only
    works with the former adapter. Now hotplugging is triggered on
    one or more HPD events.
    
    BRANCH=none
    BUG=b:118838389
    TEST=Plugged and unplugged a USB-C hub with HDMI port to port-0.
    Plugged and unplugged a HDMI monitor cable to the above hub.
    Checked the "typec 0" command showing proper results.
    
    Change-Id: If03e3db049fb3ba4de0f52b5084fd0457e3703d5
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

--- board/cheza/usb_pd_policy.c
+++ board/cheza/usb_pd_policy.c
@@ -327,26 +327,17 @@ static int svdm_dp_config(int port, uint32_t *payload)
 static void svdm_dp_post_config(int port)
 {
 	dp_flags[port] |= DP_FLAGS_DP_ON;
-	if (!(dp_flags[port] & DP_FLAGS_HPD_HI_PENDING))
-		return;
 }
 
 static int svdm_dp_attention(int port, uint32_t *payload)
 {
 	int lvl = PD_VDO_DPSTS_HPD_LVL(payload[1]);
-	int irq = PD_VDO_DPSTS_HPD_IRQ(payload[1]);
 	int mf_pref = PD_VDO_DPSTS_MF_PREF(payload[1]);
 	const struct usb_mux *mux = &usb_muxes[port];
 
 	dp_status[port] = payload[1];
-	if (!(dp_flags[port] & DP_FLAGS_DP_ON)) {
-		if (lvl)
-			dp_flags[port] |= DP_FLAGS_HPD_HI_PENDING;
-		return 1;
-	}
 
-	if (irq)
-		mux->hpd_update(port, lvl, 1);
+	mux->hpd_update(port, lvl, 1);
 
 	if (lvl)
 		usb_mux_set(port, mf_pref ? TYPEC_MUX_DOCK : TYPEC_MUX_DP,
