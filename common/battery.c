db314d0bae60ee4706775970d6c0d9fd58c2c87c
Daisuke Nojiri <dnojiri@chromium.org>
2018-11-01 12:46:33 -0700

    Battery: Apply full factor to full capacity
    This change applies 'full factor' to the battery full capacity. It
    makes the rest of the system see consistent charge percentage behavior.
    
    More concretely, with this change we can get rid of 'full factor'
    from Powerd because it sees the current charge equal to the full
    capacity.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    CQ-DEPEND=CL:1313374
    BUG=b:109954565,b:80270446
    BRANCH=none
    TEST=Verify charge LED changes to white (full) on Sona synchronously
    to the display percentage.
    TEST=Verify charge LED changes to blinking white (low) on Sona
    within 30 seconds synchronously to the display percentage.
    
    Change-Id: Ifb27c802b0cf04195ac5b426c13f9476189feb75

--- common/battery.c
+++ common/battery.c
@@ -21,10 +21,7 @@
 #define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
 
-/*
- * See config.h for details.
- * TODO: Allow host (powerd) to update it.
- */
+/* See config.h for details */
 static int batt_full_factor = CONFIG_BATT_FULL_FACTOR;
 static int batt_host_shutdown_pct = CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE;
 
@@ -566,30 +563,30 @@ DECLARE_HOOK(HOOK_INIT, battery_init, HOOK_PRIO_DEFAULT);
 
 void battery_compensate_params(struct batt_params *batt)
 {
-	int numer, denom;
-	int remain = batt->remaining_capacity;
-	int full = batt->full_capacity;
+	int numer, denom, remain, full;
 
 	if ((batt->flags & BATT_FLAG_BAD_FULL_CAPACITY) ||
 			(batt->flags & BATT_FLAG_BAD_REMAINING_CAPACITY))
 		return;
 
-	if (remain <= 0 || full <= 0)
+	if (batt->full_capacity <= 0 || batt->remaining_capacity <= 0)
 		return;
 
-	if (remain * 100 > full * batt_full_factor) {
-		batt->remaining_capacity = full;
-		batt->display_pct = 100;
-		return;
-	}
+	batt->full_capacity = batt->full_capacity * batt_full_factor / 100;
+
+	if (batt->remaining_capacity > batt->full_capacity)
+		batt->remaining_capacity = batt->full_capacity;
+
+	remain = batt->remaining_capacity;
+	full = batt->full_capacity;
 
 	/*
 	 * Powerd uses the following equation to calculate display percentage:
-	 *   charge = remain/full;
-	 *   100 * (charge - shutdown_pct) / (full_factor - shutdown_pct);
+	 *   charge = 100 * remain/full;
+	 *   100 * (charge - shutdown_pct) / (100 - shutdown_pct);
 	 */
 	numer = (100 * remain - full * batt_host_shutdown_pct) * 100;
-	denom = full * (batt_full_factor - batt_host_shutdown_pct);
+	denom = full * (100 - batt_host_shutdown_pct);
 	/* Rounding (instead of truncating) */
 	batt->display_pct = (numer + denom / 2) / denom;
 }
