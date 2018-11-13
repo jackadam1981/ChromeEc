0f8e42d5d31eb0a8bd0580f1208d8b1cf768c5f9
Aseda Aboagye <aaboagye@google.com>
2018-11-13 14:04:32 -0800

    pd_protocol: Only suspend task when TCPC enters LPM.
    This commit fixes an issue of "suspending" the PD tasks prior to
    actually entering low power mode.  Really, the only place to suspend the
    tasks should be where we put the TCPCs into low power mode.
    
    BUG=b:119501850
    BRANCH=firmware-nocturne-10984.B,master
    TEST=flash nocturne; suspend DUT, resume DUT, verify that TCPCs enter
    low power mode after a short time of being in PD_STATE_DRP_AUTO_TOGGLE.
    
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/1334529
    Tested-by: Aseda Aboagye <aaboagye@chromium.org>
    Reviewed-by: Jett Rink <jettrink@chromium.org>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>
    Change-Id: I4361001fbbdf2d0fa9c76e926162c8573f0eef67

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -3206,10 +3206,8 @@ void pd_task(void *u)
 			 * TCPC otherwise we might wake it up.
 			 */
 			if (pd[port].flags & PD_FLAGS_LPM_REQUESTED &&
-			    !(evt & PD_EVENT_CC)) {
-				timeout = -1;
+			    !(evt & PD_EVENT_CC))
 				break;
-			}
 #endif /* CONFIG_USB_PD_TCPC_LOW_POWER */
 
 			tcpm_get_cc(port, &cc1, &cc2);
@@ -3892,10 +3890,8 @@ void pd_task(void *u)
 			 * TCPC otherwise we might wake it up.
 			 */
 			if (pd[port].flags & PD_FLAGS_LPM_REQUESTED &&
-			    !(evt & PD_EVENT_CC)) {
-				timeout = -1;
+			    !(evt & PD_EVENT_CC))
 				break;
-			}
 #endif /* CONFIG_USB_PD_TCPC_LOW_POWER */
 
 			tcpm_get_cc(port, &cc1, &cc2);
@@ -4695,7 +4691,6 @@ void pd_task(void *u)
 				 */
 				tcpm_enable_drp_toggle(port);
 				pd[port].flags |= PD_FLAGS_TCPC_DRP_TOGGLE;
-				timeout = -1;
 				set_state(port, PD_STATE_DRP_AUTO_TOGGLE);
 			}
 
