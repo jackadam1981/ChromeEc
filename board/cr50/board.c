7b045940d9b17eeb7b53e9073058447631b2fd91
Mary Ruthven <mruthven@google.com>
2018-11-16 17:23:49 -0800

    cr50: add command for forcing servo disconnect
    Early proto boards may not have the servo detection setup correctly.
    This change adds a method to work around this issue, so people can use
    the consoles even if cr50 thinks servo is connected.
    
    BUG=b:119690767
    BRANCH=cr50
    TEST=Use 'ccdblock ignore_servo enable' on cheza EVT verify ec uart
    becomes read write. Make sure uart becomes read only after 'ccdblock
    ignore_servo disable'.
    
    Change-Id: I9cf04b742bec166b1cf6f0b90d5fe41346769ea7
    Signed-off-by: Mary Ruthven <mruthven@google.com>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -120,7 +120,8 @@ const char *device_state_names[] = {
 	"connected",
 	"on",
 	"debouncing",
-	"unknown"
+	"unknown",
+	"ignored"
 };
 BUILD_ASSERT(ARRAY_SIZE(device_state_names) == DEVICE_STATE_COUNT);
 
