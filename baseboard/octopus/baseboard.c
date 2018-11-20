c2380a9acef72d69512964272d7c381c59617784
James_Chao <james_chao@asus.corp-partner.google.com>
2018-11-20 19:49:34 +0800

    Ampton: change the port0 mux to PS8751
    Due to the DP measurement eye-diagram and pre-emphasize fail, we change
    the mux to PS8751.
    
    BUG=b:118728290,b:119840612
    BRANCH=none
    TEST=Check the mux function working on EVT
    
    Change-Id: I1817686f09f6aa9a557907b279ab61c889335d9e
    Signed-off-by: James_Chao <james_chao@asus.corp-partner.google.com>

--- baseboard/octopus/baseboard.c
+++ baseboard/octopus/baseboard.c
@@ -212,7 +212,7 @@ void baseboard_tcpc_init(void)
 		mux->hpd_update(port, 0, 0);
 	}
 }
-DECLARE_HOOK(HOOK_INIT, baseboard_tcpc_init, HOOK_PRIO_INIT_I2C + 1);
+DECLARE_HOOK(HOOK_INIT, baseboard_tcpc_init, HOOK_PRIO_INIT_I2C + 2);
 
 int board_set_active_charge_port(int port)
 {
