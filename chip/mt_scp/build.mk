52e108de59236902f4eae4d3a764bd4174d835aa
Yilun Lin <yllin@google.com>
2018-11-21 14:22:42 +0800

    TESTONLY WIP ipi: Port IPI.
    This CL assumes the AP/SCP share buffer address at 0x7F800(scp->ap), and
    0x7F800+sizeof(struct ipc_share_obj)(ap->scp).
    
    Change-Id: I2a43aee13141535bf71f839cf9e6cc0460b65844
    Signed-off-by: Yilun Lin <yllin@google.com>

--- chip/mt_scp/build.mk
+++ chip/mt_scp/build.mk
@@ -15,5 +15,6 @@ chip-y=clock.o gpio.o system.o uart.o
 # Optional chip modules
 chip-$(CONFIG_COMMON_TIMER)+=hrtimer.o
 chip-$(CONFIG_I2C)+=i2c.o
+chip-$(CONFIG_IPI)+=ipi.o
 chip-$(CONFIG_SPI)+=spi.o
 chip-$(CONFIG_WATCHDOG)+=watchdog.o
