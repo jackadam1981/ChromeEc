a04c43088d5f91aa85e8d52c2a8e0f2358d64a66
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2018-11-21 11:04:29 +0800

    (TEST) :code modify for GRL-USB-PD tests
    1.board/reef_it8320:structured VDM (only local change)
    
      In VIF file we announce that pd port of UUT support
    discover ID, SVID, and mode commands. So adding those
    functions pass in GRL test item.
    
    2.it83xx/intc:message id of pd packet repeat
    
      According USB-PD spec ch.6.7.1, if transmitter sends
    the repetitive message id packet, receiver does not respond
    subsequent message(except softreset).
    
    3.common/usb_pd_protocol:fix TryWait.SNK to Unattached.SNK
    timeout value
    
      When state transits from TryWait.SNK to Unattached.SNK,
    the timeout value is 10~20ms (tPDDebounce). This define
    in USB Type-C 1.3 spec Figure 4-16.
    
    4.it83xx/intc:add plug in ISR
    
      When tcpc detect plug in/out (cc lines voltage change),
    it will interrupt fw to wake pd task, so task can react
    immediately. But except SNK plug out which is monitoring
    Vbus volt by polling. When cc has volt changes, it trigger
    plug in isr then wake up pd task frequently, this may cause
    task starvation.
    
    
    <known issue>
    1.DR swap fail
    in RO can't do that
    
    BUG=none
    BRANCH=none
    TEST=GRL-USB-PD compliance test.
    
    Change-Id: Ia0eb9c3ee987fffa055bbc65f99beb309c3c1b1b
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -2849,7 +2849,7 @@ void pd_task(void *u)
 				tcpm_set_cc(port, TYPEC_CC_RD);
 				next_role_swap = get_time().val + PD_T_DRP_SNK;
 				pd[port].try_src_marker = get_time().val
-					+ PD_T_TRY_WAIT;
+					+ PD_T_DEBOUNCE;
 
 				/* Swap states quickly */
 				timeout = 2*MSEC;
@@ -2874,7 +2874,13 @@ void pd_task(void *u)
 				new_cc_state = PD_CC_AUDIO_ACC;
 			} else {
 				/* No UFP */
+//#ifdef CONFIG_USB_PD_DUAL_ROLE
+//				pd_set_power_role(port, PD_ROLE_SINK);
+//				tcpm_set_cc(port, TYPEC_CC_RD);
+//				set_state(port, PD_STATE_SNK_DISCONNECTED);
+//#else
 				set_state(port, PD_STATE_SRC_DISCONNECTED);
+//#endif //CONFIG_USB_PD_DUAL_ROLE
 				timeout = 5*MSEC;
 				break;
 			}
@@ -4005,7 +4011,7 @@ void pd_task(void *u)
 					tcpm_set_cc(port, TYPEC_CC_RD);
 					/* Set timer for TryWait.SNK state */
 					pd[port].try_src_marker = get_time().val
-						+ PD_T_TRY_WAIT;
+						+ PD_T_DEBOUNCE;
 					/* Advance to TryWait.SNK state */
 					set_state(port,
 						  PD_STATE_SNK_DISCONNECTED);
