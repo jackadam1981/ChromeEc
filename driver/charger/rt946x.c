9b04d32378e71cd63b7df3a2004744502ff6450b
TJ Wu <tj_wu@richtek.com>
2018-11-26 14:38:15 +0800

    kukui: Add mt6370 bc12 apple/samsung TA detect charger type
    Add mt6370 bc12 apple/samsung TA detect charger type
    
    TEST=none
    BUG=b:116682788
    BRANCH=None
    
    Change-Id: Ieb684f7a3d38e3b36aab9bcf27cbc823b5a7df81
    Signed-off-by: TJ Wu <tj_wu@richtek.com>

--- driver/charger/rt946x.c
+++ driver/charger/rt946x.c
@@ -21,6 +21,7 @@
 #include "timer.h"
 #include "usb_charge.h"
 #include "util.h"
+#include "stdbool.h"
 
 /* Console output macros */
 #define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
@@ -861,9 +862,83 @@ static void rt946x_init(void)
 DECLARE_HOOK(HOOK_INIT, rt946x_init, HOOK_PRIO_INIT_I2C + 1);
 
 #ifdef HAS_TASK_USB_CHG
+static int mt6370_detect_apple_samsung_ta(int usb_stat)
+{
+	int ret = 0, reg;
+	int chg_type = (usb_stat & MT6370_MASK_USB_STATUS) >> MT6370_SHIFT_USB_STATUS;
+	int dcd_timeout = usb_stat & MT6370_MASK_DCD_TIMEOUT;
+	int dp_0_9v = false, dp_1_5v = false, dp_2_3v = false, dm_2_3v = false;
+
+	/* Only SDP/CDP/DCP could possibly be Apple/Samsung TA */
+	if (chg_type != MT6370_CHG_TYPE_SDPNSTD &&
+	    chg_type != MT6370_CHG_TYPE_CDP &&
+	    chg_type != MT6370_CHG_TYPE_DCP)
+		return chg_type;
+
+	if (chg_type == MT6370_CHG_TYPE_SDPNSTD ||
+		chg_type == MT6370_CHG_TYPE_CDP) {
+		if (!dcd_timeout) {
+			return chg_type;
+		}
+	}
+
+	/* Check D+ > 0.9V */
+	ret = rt946x_update_bits(
+		MT6370_REG_QCSTATUS2, 0x0F, 0x03);
+	if (ret < 0)
+		return ret;
+
+	ret = rt946x_read8(MT6370_REG_QCSTATUS2, &reg);
+	if (ret < 0)
+		return ret;
+	dp_0_9v = reg & (1<<4);
+
+	/* Normal port */
+	if (!dp_0_9v)
+		return chg_type;
+
+	/* Check D+ > 1.5V */
+	ret = rt946x_read8(MT6370_REG_QCSTATUS2, &reg);
+	if (ret < 0)
+		return ret;
+	dp_1_5v = reg & (1<<5);
+
+	/* Samsung charger */
+	if (!dp_1_5v)
+		return MT6370_CHG_TYPE_DCP;
+
+	/* Check DP > 2.3 V */
+	ret = rt946x_update_bits(
+		MT6370_REG_QCSTATUS2, 0x0F, 0x0B);
+	if (ret < 0)
+		return ret;
+
+	ret = rt946x_read8(MT6370_REG_QCSTATUS2, &reg);
+	if (ret < 0)
+		return ret;
+	dp_2_3v = reg & (1<<5);
+
+	/* Apple charger */
+	if (!dp_2_3v && !dm_2_3v) {
+		/* Apple 2.5W charger */
+		chg_type = MT6370_CHG_TYPE_DCP;
+	} else if (!dp_2_3v && dm_2_3v) {
+		/* Apple 5W charger */
+		chg_type = MT6370_CHG_TYPE_DCP;
+	} else if (dp_2_3v && !dm_2_3v) {
+		/* Apple 10W charger */
+		chg_type = MT6370_CHG_TYPE_DCP;
+	} else {
+		/* Apple 12W charger */
+		chg_type = MT6370_CHG_TYPE_DCP;
+	}
+
+	return chg_type;
+}
+
 static int rt946x_get_bc12_device_type(void)
 {
-	int reg;
+	int reg, result;
 
 #if defined(CONFIG_CHARGER_RT9466) || defined(CONFIG_CHARGER_RT9467)
 	if (rt946x_read8(RT946X_REG_DPDM1, &reg))
@@ -883,7 +958,9 @@ static int rt946x_get_bc12_device_type(void)
 	if (rt946x_read8(MT6370_REG_USBSTATUS1, &reg))
 		return CHARGE_SUPPLIER_NONE;
 
-	switch ((reg & MT6370_MASK_USB_STATUS) >> MT6370_SHIFT_USB_STATUS) {
+	result = mt6370_detect_apple_samsung_ta(reg);
+
+	switch (result) {
 	case MT6370_CHG_TYPE_SDP:
 	case MT6370_CHG_TYPE_SDPNSTD:
 		return CHARGE_SUPPLIER_BC12_SDP;
