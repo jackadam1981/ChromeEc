7faf935a957a16f936473db273fa849b75637838
Wai-Hong Tam <waihong@google.com>
2018-11-27 12:39:32 -0800

    cheza: Support staying and leaving S3
    In the previous power state machine, S3 was just a transitional state
    between S0 <-> S5. This change supports staying and leaving S3, by
    checking the AP_SUSPEND_L signal.
    
    BRANCH=none
    BUG=b:120036688
    TEST=Powered DUT on and off; made AP suspend and resume; checked the
    result of the console command "powerinfo".
    
    Change-Id: Ifafc67a8019674f5a557ef2438b74e34e31ad9d4
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

--- power/sdm845.c
+++ power/sdm845.c
@@ -39,6 +39,7 @@
 /* Masks for power signals */
 #define IN_POWER_GOOD		POWER_SIGNAL_MASK(SDM845_POWER_GOOD)
 #define IN_AP_RST_ASSERTED	POWER_SIGNAL_MASK(SDM845_AP_RST_ASSERTED)
+#define IN_SUSPEND_ASSERTED	POWER_SIGNAL_MASK(SDM845_SUSPEND_ASSERTED)
 
 
 /* Long power key press to force shutdown */
@@ -782,8 +783,11 @@ enum power_state power_handle_state(enum power_state state)
 			CPRINTS("power off %d", value);
 			return POWER_S3S5;
 		}
-		/* Go to S3S0 directly, as don't know if it is in suspend */
-		return POWER_S3S0;
+
+		/* Suspend signal from AP not asserted, go to S0 */
+		if (!(power_get_signals() & IN_SUSPEND_ASSERTED))
+			return POWER_S3S0;
+		break;
 
 	case POWER_S3S0:
 		hook_notify(HOOK_CHIPSET_RESUME);
@@ -791,7 +795,8 @@ enum power_state power_handle_state(enum power_state state)
 
 	case POWER_S0:
 		shutdown_from_s0 = check_for_power_off_event();
-		if (shutdown_from_s0)
+		if (shutdown_from_s0 ||
+		    power_get_signals() & IN_SUSPEND_ASSERTED)
 			return POWER_S0S3;
 		break;
 
