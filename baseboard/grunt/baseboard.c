13d611141c5e71d06530d9d95b53361737b4b8dd
Edward Hill <ecgh@chromium.org>
2018-11-27 12:40:17 -0700

    grunt: Enable PPC sink FET before hibernating
    Some versions of some boards keep the port 0 PPC powered on while
    the EC hibernates (so Closed Case Debugging keeps working).
    Turn on the sink FET, so that plugging in AC will wake the EC.
    This matches the dead-battery behavior of the powered off PPC.
    
    BUG=b:119850162,b:113654692
    BRANCH=grunt
    TEST="ectool reboot_ec hibernate", wake on port 0 AC
    
    Change-Id: I4dd7ebe5408bbb2d4c92da1a44ea8b4152dbb7da
    Signed-off-by: Edward Hill <ecgh@chromium.org>

--- baseboard/grunt/baseboard.c
+++ baseboard/grunt/baseboard.c
@@ -586,3 +586,14 @@ uint32_t board_override_feature_flags1(uint32_t flags1)
 {
 	return flags1;
 }
+
+void board_hibernate(void)
+{
+	/*
+	 * Some versions of some boards keep the port 0 PPC powered on while
+	 * the EC hibernates (so Closed Case Debugging keeps working).
+	 * Turn on the sink FET, so that plugging in AC will wake the EC.
+	 * This matches the dead-battery behavior of the powered off PPC.
+	 */
+	ppc_vbus_sink_enable(0, 1);
+}
