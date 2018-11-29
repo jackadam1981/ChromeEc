7c70e1fdf58d58e96f2af1616581e6cc2ebabc3b
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2018-11-29 15:07:10 +0800

    it83xx/intc: Add type-c plug in ISR
      When tcpc detect plug in (cc lines voltage change),
    it will interrupt fw to wake pd task, so task can react
    immediately.
    
    BRANCH=None
    BUG=None
    TEST=GRL USBPD test
    
    Change-Id: I194b2fcad1d0c62dde2d3296753abd47af8feea6
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- driver/tcpm/it83xx.c
+++ driver/tcpm/it83xx.c
@@ -341,6 +341,10 @@ static void it83xx_init(enum usbpd_port port, int role)
 	IT83XX_USBPD_PDMSR(port) = USBPD_REG_MASK_SOP_ENABLE;
 	/* W/C status */
 	IT83XX_USBPD_ISR(port) = 0xff;
+#ifndef CHIP_VARIANT_IT8320BX
+	IT83XX_USBPD_TCDCR(port) |= USBPD_REG_PLUG_IN_OUT_DETECT_DISABLE;
+	IT83XX_USBPD_TCDCR(port) |= USBPD_REG_PLUG_IN_OUT_DETECT_STAT;
+#endif //CHIP_VARIANT_IT8320BX
 	/* enable cc, select cc1 and Rd. */
 	IT83XX_USBPD_CCGCR(port) = 0xd;
 	/* change data role as the same power role */
@@ -349,9 +353,14 @@ static void it83xx_init(enum usbpd_port port, int role)
 	it83xx_set_power_role(port, role);
 	/* disable all interrupts */
 	IT83XX_USBPD_IMR(port) = 0xff;
-	/* enable tx done and reset detect interrupt */
+	/* enable tx done, reset detect and plug in interrupt */
 	IT83XX_USBPD_IMR(port) &= ~(USBPD_REG_MASK_MSG_TX_DONE |
 					USBPD_REG_MASK_HARD_RESET_DETECT);
+#ifndef CHIP_VARIANT_IT8320BX
+	IT83XX_USBPD_TCDCR(port) &= ~(USBPD_REG_MASK_TYPEC_PLUG_IN_OUT_ISR |
+					USBPD_REG_PLUG_IN_OUT_DETECT_DISABLE |
+					USBPD_REG_PLUG_IN_OUT_SELECT);
+#endif //CHIP_VARIANT_IT8320BX
 	IT83XX_USBPD_CCPSR(port) = 0xff;
 	/* cc connect */
 	IT83XX_USBPD_CCCSR(port) = 0;
@@ -570,6 +579,10 @@ static int it83xx_tcpm_get_chip_info(int port, int renew,
 static void it83xx_tcpm_sw_reset(void)
 {
 	int port = TASK_ID_TO_PD_PORT(task_get_current());
+#ifndef CHIP_VARIANT_IT8320BX
+	/* Enable detect plug in interrupt*/
+	IT83XX_USBPD_TCDCR(port) &= ~USBPD_REG_PLUG_IN_OUT_DETECT_DISABLE;
+#endif //CHIP_VARIANT_IT8320BX
 	/* exit BIST test data mode */
 	USBPD_SW_RESET(port);
 }
