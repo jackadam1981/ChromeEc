81bf06f00f097146603711383e536dc6ebffd1ea
Gwendal Grignou <gwendal@chromium.org>
2018-12-03 09:09:06 -0800

    common: Add i2c over LPC nuvoton protocol
    Add protocol defined by Nuvoton to exchange i2c messages over LPC.
    Protocol is defined at:
    
    https://goto.google.com/nuvoton-ec-i2c
    
    BUG=b:73018524
    BRANCH=eve-campfire
    TEST=Unit test. Check basic message exchange is working.
    
    Change-Id: I6688911b8f1282b1ab5644b1a0fb56010040d73d
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

---
+++ include/i2c_over_lpc.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* NPCX-specific I2C over i2c protocol */
+
+#ifndef __CROS_EC_I2C_OVER_LPC_H
+#define __CROS_EC_I2C_OVER_LPC_H
+
+#include "common.h"
+#ifndef TEST_BUILD
+#include "chip/npcx/lpc_chip.h"
+#else
+#define LPC_HOST_MEM_WINDW_SIZE 32
+#define NPCX_IOL_SEM         msg_from_host->semaphore
+#endif
+
+#define __NPCX_IOL_HOST_REQUEST 0
+#define __NPCX_IOL_HOST_ACK     1
+#define __NPCX_IOL_CORE_SEND    4
+#define __NPCX_IOL_CORE_CONT    5
+
+#define NPCX_IOL_HOST_REQUEST (1 << __NPCX_IOL_HOST_REQUEST)
+#define NPCX_IOL_HOST_ACK     (1 << __NPCX_IOL_HOST_ACK)
+#define NPCX_IOL_CORE_SEND    (1 << __NPCX_IOL_CORE_SEND)
+#define NPCX_IOL_CORE_CONT    (1 << __NPCX_IOL_CORE_CONT)
+
+#define MSG_FROM_HOST_SIZE (LPC_HOST_MEM_WINDW_SIZE - sizeof(uint8_t))
+
+/* Structure overlay on top of the host packet area */
+struct __packed npcx_iol_msg {
+#ifdef TEST_BUILD
+	uint8_t semaphore;
+#else
+	/* NPCX route the semaphore to register SHAW1_SEM */
+	uint8_t reserved;
+#endif
+	union {
+		struct __packed {
+			uint8_t address;
+			uint16_t writes_nb;
+			uint16_t reads_nb;
+			uint8_t write_buf[0];
+		} host_request;
+		struct __packed {
+			uint8_t write_buf[0];
+		} host_request_cont;
+
+		struct __packed {
+			uint8_t status;
+			uint8_t read_buf[0];
+		} core_request;
+		struct __packed {
+			uint8_t read_buf[0];
+		} core_request_cont;
+	};
+};
+
+enum npcx_iol_state {
+	IOL_IDLE,
+	IOL_HOST_REQUEST_ACK,
+	IOL_HOST_REQUEST_CONT_ACK,
+	IOL_CORE_SEND_BEGIN,
+	IOL_CORE_SEND_CONT,
+	IOL_CORE_SEND_CONT_ACK,
+	IOL_CORE_SEND_END,
+	IOL_CORE_SEND_END_ACK,
+};
+
+extern enum npcx_iol_state npcx_iol_msg_state;
+extern struct npcx_iol_msg *msg_from_host;
+
+#define TASK_EVENT_IOL_PENDING     TASK_EVENT_CUSTOM(1)
+
+int npcx_iol_host_int(int semaphore);
+
+
+/**
+ * Function to process HID message once they have been reassembled into the
+ * i2c buffer.
+ *
+ * @param len:  length of the incoming message.
+ * @param buffer: address of the incoming message.
+ * @param send_response: Function to call to send a respone.
+ *    Using same buffer, response has len bytes.
+ */
+void i2c_hid_process(int len, uint8_t *buffer,
+		void (*send_response)(int len));
+
+#endif  /*  __CROS_EC_I2C_OVER_LPC_H */
+
