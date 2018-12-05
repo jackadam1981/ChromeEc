be131fae56c326eafb2747f15e1fc1f643905657
Matthew Blecker <matthewb@chromium.org>
2018-12-05 10:30:10 -0800

    iteflash: Always disable watchdog and protect path.
    Disabling the watchdog is necessary for reading and reflashing ampton EC.
    Disabling protect path is not necessary, however it was done alongside
    watchdog disable prior to this change, so I am keeping those actions
    bundled.
    
    BRANCH=octopus
    BUG=b:79684405
    TEST=When combined with further changes to use iteflash -> i2c-dev ->
    i2c-pseudo -> servod -> servo_micro -> DUT programming path, this makes
    it possible to write to and read from ampton EC firmware without
    interruption.
    
    Change-Id: Ib8def60963d9e5b2e3ab3ca9ef71b91d5e4fb3ca
    Signed-off-by: Matthew Blecker <matthewb@chromium.org>

--- util/iteflash.c
+++ util/iteflash.c
@@ -858,26 +858,18 @@ static int send_special_waveform(struct common_hnd *chnd)
 		usleep(10 * MSEC);
 
 		if (spi_flash_follow_mode(chnd, "enter follow mode") >= 0) {
-
 			spi_flash_follow_mode_exit(chnd, "exit follow mode");
 			/*
 			 * If we can talk to chip, then we can break the retry
 			 * loop.
 			 */
 			ret = check_chipid(chnd);
-
-			/* disable watchdog before programming sequence */
-			if (!ret) {
-				dbgr_disable_watchdog(chnd);
-				dbgr_disable_protect_path(chnd);
-			}
 		} else {
 			ret = -1;
 			if (!(iterations % 10))
 				printf("!please reset EC if flashing sequence"
 					" is not starting!\n");
 		}
-
 	} while (ret && (iterations++ < 10));
 
 	if (ret)
@@ -1790,6 +1782,10 @@ int main(int argc, char **argv)
 
 	/* Register signal handler after opening the communications channel. */
 	register_sigaction();
+
+	/* disable watchdog before programming sequence */
+	dbgr_disable_watchdog(&chnd);
+	dbgr_disable_protect_path(&chnd);
 
 	/* Trigger embedded monitor detection */
 	if (chnd.conf.send_waveform) {
