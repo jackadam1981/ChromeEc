d1b208515e1bf6cb738902c4119cdf8d8b251cbd
Matthew Blecker <matthewb@chromium.org>
2018-12-06 15:58:59 -0800

    servo_micro: Increase I2C clock from 100 KHz to 400 KHz.
    This speeds up ITE EC reflashing.  Using a 128 B write blocks with an
    Ampton (512 KB flash size), representative flash_ec wall time samples go
    from 8m37.369s @ 100 KHz to 5m22.500s @ 400 KHz.
    
    Setting the servo_micro I2C clock to 1 MHz was also tested and worked fine,
    but provided no significant benefit in reflash times over 400 KHz.
    
    BRANCH=octopus
    BUG=b:79684405
    TEST=servod + flash_ec with servo_micro -> ampton
    
    Change-Id: I0e2464ee1bd4236b6d8d23bf38b137d1c4ce6a3c
    Signed-off-by: Matthew Blecker <matthewb@chromium.org>

--- board/servo_micro/board.c
+++ board/servo_micro/board.c
@@ -595,7 +595,7 @@ USB_SPI_CONFIG(usb_spi, USB_IFACE_SPI, USB_EP_SPI);
 
 /* I2C ports */
 const struct i2c_port_t i2c_ports[] = {
-	{"master", I2C_PORT_MASTER, 100,
+	{"master", I2C_PORT_MASTER, 400,
 		GPIO_MASTER_I2C_SCL, GPIO_MASTER_I2C_SDA},
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
