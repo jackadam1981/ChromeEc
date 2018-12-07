9d55678204cf99da6ef84a0209db8565bb94bbc0
Shine Liu <shine.liu@intel.com>
2018-12-06 16:25:55 -0800

    chip/ish: replace CONFIG_ISH_xx flags in chip/ish/* files with CHIP_FAMILY and CHIP_VARIANT
    Replace all CONFIG_ISH_xx flags in chip/ish/* files with CHIP_FAMILY and
    CHIP_VARIANT. Which provides more structural defines between ISH generations.
    
    BRANCH=none
    BUG=b:120295222
    TEST=Test host FW loading for main ISH FW.
    
    Change-Id: Ica92eee11034447c9f0828aa986fb1736d20cf27
    Signed-off-by: Shine Liu <shine.liu@intel.com>

--- chip/ish/hpet.h
+++ chip/ish/hpet.h
@@ -54,13 +54,13 @@
 #define HPET_TIMER_COMP(x) \
 	REG32(ISH_HPET_BASE + TIMER0_COMP_VAL_REG + (x * 0x20))
 
-#if defined CONFIG_ISH_20
+#if defined(CHIP_FAMILY_ISH2)
 #define ISH_HPET_CLK_FREQ		1000000		/* 1 MHz clock */
 
-#elif defined CONFIG_ISH_30
+#elif defined(CHIP_FAMILY_ISH3)
 #define ISH_HPET_CLK_FREQ		12000000	/* 12 MHz clock */
 
-#elif defined CONFIG_ISH_40
+#elif defined(CHIP_FAMILY_ISH4) || defined(CHIP_FAMILY_ISH5)
 #define ISH_HPET_CLK_FREQ		32768		/* 32.768 KHz clock */
 #endif
 
