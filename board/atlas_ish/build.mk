7fd9aa58908be740aa9eef5c7295541ec59b7c65
Rong Chang <rongchang@chromium.org>
2018-12-10 17:24:51 +0800

    HACK: DO NOT COMMIT: centroiding performance test
    This change imports centroiding library through build.mk and a
    centroiding symlink to experimental touch firmware repo.
    
    Touch hardware interrupt and ack test is added and can be triggered
    automatically by placing a grounding conductor surface on touchpad.
    
    2 ping-pong tasks were added to stress context switch.
    
    CQ-DEPEND=CL:*728630
    BUG=b:112750896
    BUG=b:121454497
    TEST=manual
      build and load on atlas
      type "perf" in ish console
      place ground conductor on touchpad
    
    Change-Id: I0f84b315ada6a0fd2dcd10845dd54b48be352b23
    Signed-off-by: Rong Chang <rongchang@chromium.org>

--- board/atlas_ish/build.mk
+++ board/atlas_ish/build.mk
@@ -10,4 +10,35 @@ CHIP:=ish
 CHIP_FAMILY:=ish3
 CHIP_VARIANT:=ish3p0
 
-board-y=board.o
+# Before we enable C++ in default toolchain, use SDK toolchain.
+CXX=i686-pc-linux-gnu-gcc
+
+centroiding = board/$(BOARD)/centroiding
+
+-include $(centroiding)/ec_build.mk
+
+board-y = board.o $(addprefix centroiding/, $(centroiding-y))
+
+ifeq (,$(findstring $(centroiding), $(dirs-y)))
+dirs-y += board/$(BOARD)/centroiding
+endif
+
+EXTRA_OPT_FLAGS = \
+	-fgcse-after-reload \
+	-finline-functions \
+	-fipa-cp-clone \
+	-fpredictive-commoning \
+	-funswitch-loops \
+	-ftree-vectorize
+
+ARCH_FLAGS = -march=pentium -mfpmath=387 -m32 \
+	-DVERBOSITY=0 -O2 $(EXTRA_OPT_FLAGS)
+INCLUDES = -I$(centroiding) \
+	-I$(centroiding)/embbeded
+CFLAGS += $(INCLUDES) $(ARCH_FLAGS) -fmessage-length=0 \
+	-DFIRMWARE_MCU \
+	-DNO_STDCPLUSCPLUS_LIB
+CXXFLAGS += $(INCLUDES) $(ARCH_FLAGS) -fno-rtti -fno-exceptions \
+	-DFIRMWARE_MCU \
+	-DNO_STDCPLUSCPLUS_LIB
+LDFLAGS += $(ARCH_FLAGS)
