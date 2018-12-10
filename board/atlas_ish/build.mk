58a8c6914b3ce7a7b255825bf53ae8f6e1052c20
Rong Chang <rongchang@chromium.org>
2018-12-10 17:24:51 +0800

    HACK: DO NOT COMMIT: centroiding performance test
    This change imports centroiding library through build.mk and a
    centroiding symlink to experimental touch firmware repo.
    
    CQ-DEPEND=CL:*728630
    BUG=none
    TEST=manual
      build and load on atlas
      type "perf" in ish console
    
    Change-Id: I0f84b315ada6a0fd2dcd10845dd54b48be352b23
    Signed-off-by: Rong Chang <rongchang@chromium.org>

--- board/atlas_ish/build.mk
+++ board/atlas_ish/build.mk
@@ -7,5 +7,34 @@
 #
 
 CHIP:=ish
+CXX=i686-pc-linux-gnu-gcc
 
-board-y=board.o
+centroiding = board/$(BOARD)/centroiding
+
+-include $(centroiding)/ec_build.mk
+
+board-y = board.o $(addprefix centroiding/, $(centroiding-y))
+
+ifeq (,$(findstring $(centroiding), $(dirs-y)))
+dirs-y += board/$(BOARD)/centroiding
+endif
+
+EXTRA_OPT_FLAGS = \
+	-fgcse-after-reload \
+	-finline-functions \
+	-fipa-cp-clone \
+	-fpredictive-commoning \
+	-funswitch-loops \
+	-ftree-vectorize
+
+ARCH_FLAGS = -march=i486 -mfpmath=387 -m32 \
+	-DVERBOSITY=0 -O2 $(EXTRA_OPT_FLAGS)
+INCLUDES = -I$(centroiding) \
+	-I$(centroiding)/embbeded
+CFLAGS += $(INCLUDES) $(ARCH_FLAGS) -fmessage-length=0 \
+	-DFIRMWARE_MCU \
+	-DNO_STDCPLUSCPLUS_LIB
+CXXFLAGS += $(INCLUDES) $(ARCH_FLAGS) -fno-rtti -fno-exceptions \
+	-DFIRMWARE_MCU \
+	-DNO_STDCPLUSCPLUS_LIB
+LDFLAGS += $(ARCH_FLAGS)
