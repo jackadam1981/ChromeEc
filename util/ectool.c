d23a335a386f1c5925131299c51b7201fb095ca1
Mario Tesi <mario.tesi@st.com>
2018-12-11 09:21:07 +0100

    driver: lsm6dso: Add support to LSM6DSO IMU
    Added support to LSM6DSO IMU sensor.
    Features included in this driver are:
     - Basic Sensor Read acc/gyro data
     - ODR and FS runtime configuration
     - FIFO water mark interrupt
     - Shared commons function with ST MEMs devices
    
    BUG=none
    BRANCH=master
    TEST=Tested on discovery target BOARD with
    LSM6DSO connected to EC i2c master bus and motion
    sense task running.
    
    Commands used to test LSM6DSO device are:
     - accelinit
     - accelrange
     - accelinfo
    
    All basic features tested, including:
    1) ODR change:
     - accelrate 0 [13000:208000]
     - accelrate 1 [13000:208000]
    
    2) FS Range change:
     - accelrange 0 [2:16]
     - accelrange 1 [250:2000]
    
    3) Interrupt on FIFO water mark
    
    Signed-off-by: Mario Tesi <mario.tesi@st.com>
    
    Change-Id: If2984f7d0d30b0ef475e0525aca2bc365aa4fe21
    Signed-off-by: Mario Tesi <mario.tesi@st.com>

--- util/ectool.c
+++ util/ectool.c
@@ -4341,6 +4341,9 @@ static int cmd_motionsense(int argc, char **argv)
 		case MOTIONSENSE_CHIP_LIS2MDL:
 			printf("lis2mdl\n");
 			break;
+		case MOTIONSENSE_CHIP_LSM6DSO:
+			printf("lsm6dso\n");
+			break;
 		default:
 			printf("unknown\n");
 		}
