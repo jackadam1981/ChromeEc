b4eb8491a8dc409441b8063de1205b04c9e994fd
Keith Short <keithshort@chromium.org>
2018-12-19 16:17:44 -0700

    DO NOT SUBMIT: debug for testing factory mode on proto hardware
    BRANCH=none
    BUG=none
    TEST=none
    Change-Id: I18a39c1c634918d44c66789be89e93d7f1161021
    Signed-off-by: Keith Short <keithshort@chromium.org>

--- common/gpio_commands.c
+++ common/gpio_commands.c
@@ -67,8 +67,11 @@ static enum ec_error_list set(const char *name, int value)
 	if (!gpio_is_implemented(signal))
 		return EC_ERROR_INVAL;
 
+	/* FIXME, for debug only - bypass flags check which is const data */
+#if 0
 	if (!(gpio_get_default_flags(signal) & GPIO_OUTPUT))
 		return EC_ERROR_INVAL;
+#endif
 
 	gpio_set_level(signal, value);
 
