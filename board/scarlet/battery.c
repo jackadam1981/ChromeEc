76bf20cc55fa5185dfab7973e9b5f8acfa7d7821
Philip Chen <philipchen@chromium.org>
2018-12-19 01:47:40 +0000

    Revert "scarlet: Don't disable idle mode in S3"
    This reverts commit 8746200bb7c71bdee057580447c78ffb53520fae.
    
    Reason for revert: TEST-ONLY for now
    
    Original change's description:
    > scarlet: Don't disable idle mode in S3
    > 
    > I heard we only want to disable idle mode in S5, when battery is full.
    > 
    > BUG=b:78792296
    > BRANCH=scarlet
    > TEST=manually test on scarlet, and confirm when battery is full,
    > idle mode is disabled in S5 but not in S3.
    > 
    > Change-Id: I5809da581dd3fc3d382f606168a88263740256c0
    > Signed-off-by: Philip Chen <philipchen@google.com>
    > Reviewed-on: https://chromium-review.googlesource.com/1077496
    > Commit-Queue: Philip Chen <philipchen@chromium.org>
    > Tested-by: Philip Chen <philipchen@chromium.org>
    > Reviewed-by: David Schneider <dnschneid@chromium.org>
    > Reviewed-by: Alexandru M Stan <amstan@chromium.org>
    
    Bug: b:78792296
    Change-Id: I8813608076f74e5e6eba00c20ec3eb26f02ae6b9

--- board/scarlet/battery.c
+++ board/scarlet/battery.c
@@ -257,7 +257,8 @@ int charger_profile_override(struct charge_state_data *curr)
 		 * This is a workaround for b:78792296. When AP is off and
 		 * charge termination is detected, we disable idle mode.
 		 */
-		if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		if (chipset_in_state(CHIPSET_STATE_ANY_OFF |
+				     CHIPSET_STATE_ANY_SUSPEND))
 			disable_idle();
 		else
 			enable_idle();
