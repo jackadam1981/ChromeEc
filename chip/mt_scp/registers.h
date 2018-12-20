dcd2b7fa1b566b9fd36318c7b1c1693260a01a0b
Yilun Lin <yllin@google.com>
2018-12-20 17:05:24 +0800

    kukui_scp/dma_api: Enable DMA addr translation api.
    This CL enables the addr translation between AP and SCP.
    
    TEST=None
    BRANCH=None
    BUG=b:114326670
    
    Change-Id: I29298c2e5a897d08d21390c751bdd881170adb59
    Signed-off-by: Yilun Lin <yllin@google.com>

--- chip/mt_scp/registers.h
+++ chip/mt_scp/registers.h
@@ -129,8 +129,26 @@
 #define   AUTO_DDREN			(1 << 18)
 
 /* Memory remap control */
+/*
+ * EXT_ADDR3[29:24] remap register for addr msb 31~28 equal to 0x7
+ * EXT_ADDR2[21:16] remap register for addr msb 31~28 equal to 0x6
+ * EXT_ADDR1[13:8]  remap register for addr msb 31~28 equal to 0x3
+ * EXT_ADDR0[5:0]   remap register for addr msb 31~28 equal to 0x2
+ */
 #define SCP_REMAP_CFG1			REG32(SCP_CFG_BASE + 0x120)
+/*
+ * EXT_ADDR7[29:24] remap register for addr msb 31~28 equal to 0xb
+ * EXT_ADDR6[21:16] remap register for addr msb 31~28 equal to 0xa
+ * EXT_ADDR5[13:8]  remap register for addr msb 31~28 equal to 0x9
+ * EXT_ADDR4[5:0]   remap register for addr msb 31~28 equal to 0x8
+ */
 #define SCP_REMAP_CFG2			REG32(SCP_CFG_BASE + 0x124)
+/*
+ * AUR_ADDR[31:28]  remap register for addr msb 31~28 equal to 0xd
+ * EXT_ADDR10[21:16]remap register for addr msb 31~28 equal to 0xf
+ * EXT_ADDR9[13:8]  remap register for addr msb 31~28 equal to 0xe
+ * EXT_ADDR8[5:0]   remap register for addr msb 31~28 equal to 0xc
+ */
 #define SCP_REMAP_CFG3			REG32(SCP_CFG_BASE + 0x128)
 #define SCP_L1_REMAP_CFG0		REG32(SCP_CFG_BASE + 0x130)
 #define SCP_L1_REMAP_CFG1		REG32(SCP_CFG_BASE + 0x130)
