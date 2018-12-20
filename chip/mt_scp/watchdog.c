2eb6ec6394ad83e2065678cc0c0a176ac73c30a8
Peter Shih <pihsun@chromium.org>
2018-12-20 17:40:16 +0800

    TEST ONLY: scp watchdog
    Change-Id: Ic5aeeb08ea0806fd9623e0112ae2d358142aa9a9

--- chip/mt_scp/watchdog.c
+++ chip/mt_scp/watchdog.c
@@ -6,14 +6,17 @@
 /* Watchdog driver */
 
 #include "common.h"
+#include "console.h"
 #include "hooks.h"
 #include "panic.h"
 #include "registers.h"
+#include "task.h"
 #include "watchdog.h"
 
 void watchdog_reload(void)
 {
-	SCP_WDT_RELOAD = SCP_WDT_RELOAD_VALUE;
+	/* ccprintf("watchdog reload\n"); */
+	/* SCP_WDT_RELOAD = SCP_WDT_RELOAD_VALUE; */
 }
 DECLARE_HOOK(HOOK_TICK, watchdog_reload, HOOK_PRIO_DEFAULT);
 
@@ -23,11 +26,20 @@ int watchdog_init(void)
 		SCP_WDT_PERIOD(CONFIG_WATCHDOG_PERIOD_MS);
 
 	/* Disable watchdog */
-	SCP_WDT_CFG = 0;
+	SCP_WDT_CFG = watchdog_timeout;
 	/* Enable watchdog */
 	SCP_WDT_CFG = SCP_WDT_ENABLE | watchdog_timeout;
 	/* Reload watchdog */
-	watchdog_reload();
+	/* watchdog_reload(); */
+	SCP_WDT_RELOAD = SCP_WDT_RELOAD_VALUE;
+	ccprintf("watchdog inited\n");
+	task_enable_irq(SCP_IRQ_WDT);
 
 	return EC_SUCCESS;
 }
+
+void wdt_handler(void)
+{
+	ccprintf("wdt_handler\n");
+}
+DECLARE_IRQ(SCP_IRQ_WDT, wdt_handler, 1);
