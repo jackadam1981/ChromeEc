20ebc1cb789a2d68d46160e5fd137b3737d2eb47
Devin Lu <devin.lu@quantatw.com>
2018-12-27 11:03:04 +0800

    driver: lsm6dsm: add reboot at init time
    Without this patch, the pass-through mode will get stuck after sysjump.
    Force a reboot of the lsm6dsm accelerometer side at init() to allow
    magnetometer initialization to complete.
    
    BUG=b:115587004
    BRANCH=octopus
    TEST=execute "sysjump RW" then make sure magnetometer initial passed
    and no errors as below:
    [855.944737 Base Accel: MS Done Init type:0x0 range:2]
    [855.947773 Base Accel ODR: 13000 - roundup 1 from config 1 [AP 0]]
    [855.949904 Base Gyro: MS Done Init type:0x1 range:1000]
    [855.952509 Base Gyro ODR: 0 - roundup 0 from config 0 [AP 0]]
    sensorhub_check_and_rst: Base Accel type:0x0 WAIR error ret: 1
    sensorhub_check_and_rst: Base Accel type:0x0 WAIR error ret: 1
    sensorhub_check_and_rst: Base Accel type:0x0 WAIR error ret: 1
    [855.993153 Base Mag: 3: init failed: 1]
    
    Change-Id: I7870acb674457dfcf247a3fa231e82a7123303bf
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>

--- driver/accelgyro_lsm6dsm.c
+++ driver/accelgyro_lsm6dsm.c
@@ -558,6 +558,23 @@ static int init(const struct motion_sensor_t *s)
 		if (ret != EC_SUCCESS)
 			goto err_unlock;
 
+#ifdef CONFIG_MAG_LIS2MDL
+		/*
+		 * Add reboot memory content can fix pass-through mode got
+		 * stuck, more detail see b:115587004.
+		 */
+		ret = st_raw_write8(s->port, s->addr, LSM6DSM_CTRL3_ADDR,
+				LSM6DSM_BOOT);
+		if (ret != EC_SUCCESS)
+			goto err_unlock;
+
+		/*
+		 * As lsm6dsm application note, wait 15ms for accelerometer
+		 * to doing full reboot.
+		 */
+		msleep(15);
+#endif
+
 		/*
 		 * Output data not updated until have been read.
 		 * Prefer interrupt to be active low.
