72e2b555bd49157672ccaf23f8b3e3e0a488ea69
Tony Zou <zoutao@huaqin.corp-partner.google.com>
2018-12-27 16:31:22 +0800

    flapjack/wpc: Enable wireless charge P9221-R7 for flapjack
    CQ-DEPEND=CL:1445133
    BRANCH=none
    BUG=none
    TEST=localbuild ok
    
    Change-Id: I5fbd0237cedd8095f98582c39973d432e733f2cd
    Signed-off-by: Tony Zou <zoutao@huaqin.corp-partner.google.com>

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -42,8 +42,14 @@ test_mockable const int supplier_priority[] = {
 	[CHARGE_SUPPLIER_BC12_CDP] = 4,
 	[CHARGE_SUPPLIER_BC12_SDP] = 5,
 	[CHARGE_SUPPLIER_OTHER] = 6,
-	[CHARGE_SUPPLIER_VBUS] = 7
+	[CHARGE_SUPPLIER_VBUS] = 7,
 #endif
+#ifdef CONFIG_WIRELESS_CHARGER_P9221_R7
+	[CHARGE_SUPPLIER_WPC_BPP] = 6,
+	[CHARGE_SUPPLIER_WPC_EPP] = 6,
+	[CHARGE_SUPPLIER_WPC_GPP] = 6,
+#endif
+
 };
 BUILD_ASSERT(ARRAY_SIZE(supplier_priority) == CHARGE_SUPPLIER_COUNT);
 
@@ -338,13 +344,28 @@ static void charge_manager_fill_power_info(int port,
 			r->type = USB_CHG_TYPE_VBUS;
 			break;
 #endif
+#ifdef CONFIG_WIRELESS_CHARGER_P9221_R7
+		case CHARGE_SUPPLIER_WPC_BPP:
+			r->type = USB_CHG_TYPE_PROPRIETARY;
+			break;
+		case CHARGE_SUPPLIER_WPC_EPP:
+			r->type = USB_CHG_TYPE_PROPRIETARY;
+			break;
+		case CHARGE_SUPPLIER_WPC_GPP:
+			r->type = USB_CHG_TYPE_PROPRIETARY;
+			break;
+#endif
 #if CONFIG_DEDICATED_CHARGE_PORT_COUNT > 0
 		case CHARGE_SUPPLIER_DEDICATED:
 			r->type = USB_CHG_TYPE_DEDICATED;
 			break;
 #endif
 		default:
+#ifdef CONFIG_WIRELESS_CHARGER_P9221_R7
+			r->type = USB_CHG_TYPE_VBUS;
+#else
 			r->type = USB_CHG_TYPE_OTHER;
+#endif
 		}
 		r->meas.voltage_max = available_charge[sup][port].voltage;
 
