bccd417e0bd0292471d6b3245528c557c7d2cda8
Tony Zou <zoutao@huaqin.corp-partner.google.com>
2018-12-27 16:31:22 +0800

    flapjack/wpc: Enable wireless charge P9221-R7 for flapjack
    CQ-DEPEND=CL:1445133
    BRANCH=none
    BUG=none
    TEST=localbuild ok
    
    Change-Id: I5fbd0237cedd8095f98582c39973d432e733f2cd
    Signed-off-by: Tony Zou <zoutao@huaqin.corp-partner.google.com>

--- driver/build.mk
+++ driver/build.mk
@@ -139,3 +139,6 @@ driver-$(CONFIG_USBC_PPC_NX20P3483)+=ppc/nx20p348x.o
 
 # video converters
 driver-$(CONFIG_MCDP28X0)+=mcdp28x0.o
+
+# Wireless Power Chargers
+driver-$(HAS_TASK_WPC) += wpc/p9221_r7.o
