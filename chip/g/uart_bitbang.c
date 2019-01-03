96ecdbde1a1a5839183c2c2e5a442ef829a34cee
Namyoon Woo <namyoon@chromium.org>
2019-01-02 18:32:31 -0800

    cr50: Extend CR50 Bitbang Receive buffer.
    Bitbang receive buffer in CR50 gets bigger so that 257 byte chunk
    transfer can be done without any data loss.
    
    This CL occupies 104 bytes in Flash memory.
    
    BUG=b:112163028
    BRANCH=cr50
    TEST=manually tested by writing/reading EC binary in bitbang mode.
    (a) Flash EC binary
        $ flash_ec --board=${BOARD} --bitbang_rate 57600 \
        	--image ${EC_BIN_PATH}
    
    (b) Read EC binary
        $ dut-control --port=9999 ec_ec3po_interp_connect:off
        $ dut-control --port=9999 ccd_ec_boot_mode_bitbang:on
        $ dut-control --port=9999 cold_reset:on
        $ dut-control --port=9999 cold_reset:off
        $ dut-control --port=9999 ec_uart_parity:even
        $ dut-control --port=9999 ec_uart_baudrate:57600
        $ dut-control --port=9999 ec_uart_bitbang_en:on
    
        $ stm32mon -d ${RAW_EC_UART_PTY} -c -r ${EC_BIN_READ_PATH} \
        	-b 57600
    
    Change-Id: I6634c51e1b43d8d48158c9d7a342307d8ce8b01f
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- chip/g/uart_bitbang.c
+++ chip/g/uart_bitbang.c
@@ -9,6 +9,7 @@
 #include "gpio.h"
 #include "pmu.h"
 #include "registers.h"
+#include "shared_mem.h"
 #include "task.h"
 #include "timer.h"
 #include "uart_bitbang.h"
@@ -26,6 +27,9 @@
 	 (rate == 115200))
 
 #define TIMEUS_CLK_FREQ 24 /* units: MHz */
+#define RX_BUF_SIZE	257
+#define START_BIT	0
+#define STOP_BIT	1
 
 /* Flag indicating whether bit banging is enabled or not. */
 static uint8_t bitbang_enabled;
@@ -35,6 +39,9 @@ static uint8_t bitbang_wanted;
 /* Current bitbang context */
 static uint32_t bit_period_ticks;
 static uint8_t set_parity;
+
+/* Bitbang receive buffer pointer */
+static uint8_t *rx_buffer;
 
 #if BITBANG_DEBUG
 /* debug counters and log */
@@ -206,7 +213,7 @@ static void uart_bitbang_write_char(char c)
 	next_tick = get_next_tick(bit_period_ticks);
 
 	/* Start bit. */
-	gpio_set_level(bitbang_config.tx_gpio, 0);
+	gpio_set_level(bitbang_config.tx_gpio, START_BIT);
 	wait_ticks(&next_tick);
 
 	/* 8 data bits. */
@@ -228,7 +235,7 @@ static void uart_bitbang_write_char(char c)
 	}
 
 	/* 1 stop bit. */
-	gpio_set_level(bitbang_config.tx_gpio, 1);
+	gpio_set_level(bitbang_config.tx_gpio, STOP_BIT);
 
 	/*
 	 * Re-enable interrupts early: this could be the last byte and the
@@ -299,7 +306,7 @@ static int uart_bitbang_receive_char(uint8_t *rxed_char, uint32_t *next_tick)
 
 
 	/* Check that the stop bit is valid. */
-	if (stop_bit != 1) {
+	if (stop_bit != STOP_BIT) {
 #if BITBANG_DEBUG
 		stop_bit_err_cnt++;
 		stop_bit_discard[stop_bit_discard_idx] = rx_char;
@@ -320,9 +327,11 @@ static int uart_bitbang_receive_char(uint8_t *rxed_char, uint32_t *next_tick)
 
 void uart_bitbang_irq(void)
 {
-	uint8_t rx_buffer[20];
 	size_t i = 0;
 	uint32_t next_tick;
+
+	if (!rx_buffer)
+		return;
 
 	/* Empirically chosen IRQ latency compensation. */
 	next_tick = get_next_tick(bit_period_ticks  - 40);
@@ -336,7 +345,7 @@ new_char:
 		if (rv != EC_SUCCESS)
 			break;
 
-		if (++i == sizeof(rx_buffer))
+		if (++i == RX_BUF_SIZE)
 			break;
 		/*
 		 * For the duration of one byte wait for another byte from the
@@ -344,12 +353,12 @@ new_char:
 		 */
 		max_time = GR_TIMEUS_CUR_MAJOR(0) + bit_period_ticks * 10;
 		while (GR_TIMEUS_CUR_MAJOR(0) < max_time) {
-			if (!gpio_get_level(bitbang_config.rx_gpio)) {
-				next_tick = get_next_tick(bit_period_ticks);
-				goto new_char;
-			}
-		}
+			if (gpio_get_level(bitbang_config.rx_gpio) == STOP_BIT)
+				continue;
 
+			next_tick = get_next_tick(bit_period_ticks);
+			goto new_char;
+		}
 	} while (0);
 
 	QUEUE_ADD_UNITS(bitbang_config.uart_in, rx_buffer, i);
@@ -360,17 +369,17 @@ static int write_test_pattern(int pattern_idx)
 {
 	switch (pattern_idx) {
 	case 0:
-		uart_bitbang_write_char(uart, 'a');
-		uart_bitbang_write_char(uart, 'b');
-		uart_bitbang_write_char(uart, 'c');
-		uart_bitbang_write_char(uart, '\n');
+		uart_bitbang_write_char('a');
+		uart_bitbang_write_char('b');
+		uart_bitbang_write_char('c');
+		uart_bitbang_write_char('\n');
 		ccprintf("wrote: 'abc\\n'\n");
 		break;
 
 	case 1:
-		uart_bitbang_write_char(uart, 0xAA);
-		uart_bitbang_write_char(uart, 0xCC);
-		uart_bitbang_write_char(uart, 0x55);
+		uart_bitbang_write_char(0xAA);
+		uart_bitbang_write_char(0xCC);
+		uart_bitbang_write_char(0x55);
 		ccprintf("wrote: '0xAA 0xCC 0x55'\n");
 		break;
 
@@ -394,6 +403,10 @@ static int command_bitbang(int argc, char **argv)
 			if (!strcasecmp("disable", argv[2])) {
 				bitbang_wanted = 0;
 				ccd_update_state();
+				if (rx_buffer) {
+					shared_mem_release(rx_buffer);
+					rx_buffer = NULL;
+				}
 				return EC_SUCCESS;
 			}
 			return EC_ERROR_PARAM2;
@@ -422,6 +435,10 @@ static int command_bitbang(int argc, char **argv)
 			if (servo_is_connected())
 				ccprintf("%sing superseded by servo\n",
 					feature_name);
+
+			if (shared_mem_size() < RX_BUF_SIZE)
+				return EC_ERROR_MEMORY_ALLOCATION;
+			shared_mem_acquire(RX_BUF_SIZE, (char **)&rx_buffer);
 
 			bitbang_wanted = 1;
 			ccd_update_state();
@@ -477,8 +494,13 @@ static int command_bitbang_dump_stats(int argc, char **argv)
 	ccprintf("%d chars read\n", read_char_cnt);
 	ccprintf("Contents\n");
 	ccprintf("[");
-	for (i = 0; i < RX_BUF_SIZE; i++)
-		ccprintf(" %02x ", rx_buf[i] & 0xFF);
+	if (rx_buffer) {
+		for (i = 0; i < RX_BUF_SIZE; i++) {
+			ccprintf(" %02x%s", rx_buffer[i] & 0xFF);
+			if (!((i + 1) & 0x0F))
+				ccprintf("\n ");
+		}
+	}
 	ccprintf("]\n");
 	ccprintf("Discards\nparity: ");
 	ccprintf("[");
