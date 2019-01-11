734f541ed17b65c6ab1e1544bcae419622694fa3
Scott Collyer <scollyer@google.com>
2019-01-10 18:25:36 -0800

    hatch: Add support for MST (multi stream transport) enable
    Change-Id: I1c46534bc7f32221f9e379dd9c74d5618c8f57e1
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- baseboard/hatch/baseboard.c
+++ baseboard/hatch/baseboard.c
@@ -204,6 +204,10 @@ void baseboard_tcpc_init(void)
 	gpio_enable_interrupt(GPIO_USB_C0_TCPC_INT_ODL);
 	gpio_enable_interrupt(GPIO_USB_C1_TCPC_INT_ODL);
 
+	/* Enable HPD interrupts. */
+	gpio_enable_interrupt(GPIO_TCPC_USB_C1_HPD);
+	gpio_enable_interrupt(GPIO_HDMI_CONN_HPD);
+
 }
 DECLARE_HOOK(HOOK_INIT, baseboard_tcpc_init, HOOK_PRIO_INIT_I2C + 1);
 
