302b737dd18f8df9175807b7c38e42333c2df8a8
Scott Collyer <scollyer@google.com>
2019-01-10 21:16:45 -0800

    hatch: Add support for bc12 detection
    Change-Id: Ie81e66c2921b670d8819641c922eaf38126f9641
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- baseboard/hatch/baseboard.c
+++ baseboard/hatch/baseboard.c
@@ -10,6 +10,7 @@
 #include "charge_state_v2.h"
 #include "chipset.h"
 #include "console.h"
+#include "driver/bc12/pi3usb9201.h"
 #include "driver/ppc/sn5s330.h"
 #include "driver/tcpm/anx7447.h"
 #include "driver/tcpm/ps8xxx.h"
@@ -188,6 +189,18 @@ struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_COUNT] = {
 		.driver = &tcpci_tcpm_usb_mux_driver,
 		.hpd_update = &ps8xxx_tcpc_update_hpd_status,
 	}
+};
+
+struct pi3usb2901_config_t bc12_chips[] = {
+	[USB_PD_PORT_TCPC_0] = {
+		.i2c_port = I2C_PORT_PPC0,
+		.i2c_addr = PI3USB9201_I2C_ADDR_3,
+	},
+
+	[USB_PD_PORT_TCPC_1] = {
+		.i2c_port = I2C_PORT_TCPC1,
+		.i2c_addr = PI3USB9201_I2C_ADDR_3,
+	},
 };
 
 /* GPIO to enable/disable the USB Type-A port. */
