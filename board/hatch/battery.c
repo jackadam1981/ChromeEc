e61d5318ea74025c34071d2ce07ced647860e622
Scott Collyer <scollyer@google.com>
2019-01-11 00:56:26 -0800

    hatch: Add support for SMP-SDP battery
    Change-Id: I572879fc32aaf8aad8289fc52a385262ea61cdc2
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- board/hatch/battery.c
+++ board/hatch/battery.c
@@ -33,7 +33,7 @@
  */
 const struct board_batt_params board_battery_info[] = {
 	/* SMP Dell FMXMT Battery Information */
-	[BATTERY_KEYSTONE] = {
+	[BATTERY_SMP_LIS] = {
 		.fuel_gauge = {
 			.manuf_name = "SMP-LIS3.78",
 			/* .device_name = "DELL FMXMT", */
@@ -60,7 +60,35 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c	= 60,
 		},
 	},
+
+	/* SMP Dell FMXMT Battery Information */
+	[BATTERY_SMP_SDI] = {
+		.fuel_gauge = {
+			.manuf_name = "SMP-SDI-3727",
+			.ship_mode = {
+				.reg_addr = 0x0,
+				.reg_data = { 0x10, 0x10 },
+			},
+			.fet = {
+				.reg_addr = 0x0,
+				.reg_mask = 0x2000,
+				.disconnect_val = 0x2000,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 8800,
+			.voltage_normal		= 7660, /* mV */
+			.voltage_min		= 6000, /* mV */
+			.precharge_current	= 256,	/* mA */
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 60,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= 0,
+			.discharging_max_c	= 60,
+		},
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_KEYSTONE;
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_SMP_SDI;
