80faad734a092e45ef6c47e2190c3b9b27417b7a
Scott Collyer <scollyer@google.com>
2019-01-10 21:16:16 -0800

    bc12: Add driver for pi3usb9201
    Change-Id: I1cc8fde5729221a7944e930218005e6e3d4ced3b
    Signed-off-by: Scott Collyer <scollyer@google.com>

---
+++ driver/bc12/pi3usb9201.h
@@ -0,0 +1,62 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* PI3USB9201 USB BC 1.2 Charger Detector driver definitions */
+
+/* 8-bit I2C address */
+#define PI3USB9201_I2C_ADDR_0 0xB8
+#define PI3USB9201_I2C_ADDR_1 0xBA
+#define PI3USB9201_I2C_ADDR_2 0xBC
+#define PI3USB9201_I2C_ADDR_3 0xBE
+
+#define PI3USB9201_REG_CTRL_1 0x0
+#define PI3USB9201_REG_CTRL_2 0x1
+#define PI3USB9201_REG_CLIENT_STS 0x2
+#define PI3USB9201_REG_HOST_STS 0x3
+
+
+enum pi3usb9201_mode {
+	PI3USB9201_POWER_DOWN,
+	PI3USB9201_SDP_HOST_MODE,
+	PI3USB9201_DCP_HOST_MODE,
+	PI3USB9201_CDP_HOST_MODE,
+	PI3USB9201_CLIENT_MODE,
+	PI3USB9201_RESERVED_1,
+	PI3USB9201_RESERVED_2,
+	PI3USB9201_USB_PATH_ON,
+};
+
+#define PI3USB9201_REG_CTRL_1_INT_MASK (1 << 0)
+#define PI3USB9201_REG_CTRL_1_INT_MODE (1 << 1)
+#define PI3USB9201_REG_CTRL_1_INT_MODE_BIT_SHIFT 1
+#define PI3USB9201_REG_CTRL_1_INT_MODE_MASK 0x7
+
+
+#define PI3USB9201_REG_CTRL_2_AUTO_SW (1 << 1)
+#define PI3USB9201_REG_CTRL_2_START_DET (1 << 2)
+
+
+#define PI3USB9201_REG_CLIENT_STS_CHG_OTHER (1 << 0)
+#define PI3USB9201_REG_CLIENT_STS_CHG_2_4A (1 << 1)
+#define PI3USB9201_REG_CLIENT_STS_CHG_2_0A (1 << 2)
+#define PI3USB9201_REG_CLIENT_STS_CHG_1_0A (1 << 3)
+#define PI3USB9201_REG_CLIENT_STS_CHG_CDP (1 << 5)
+#define PI3USB9201_REG_CLIENT_STS_CHG_SDP (1 << 6)
+#define PI3USB9201_REG_CLIENT_STS_CHG_DCP (1 << 7)
+
+
+#define PI3USB9201_REG_HOST_STS_BC12_DET (1 << 0)
+#define PI3USB9201_REG_HOST_STS_DEV_PLUG (1 << 1)
+#define PI3USB9201_REG_HOST_STS_DEV_UNPLUG (1 << 2)
+
+
+struct pi3usb2901_config_t {
+	const int i2c_port;
+	const int i2c_addr;
+};
+
+/* Configuration struct defined at board level */
+extern struct  pi3usb2901_config_t bc12_chips[];
+
