791567fd7b692a0624ca5297df78d4f3f9095a2a
Scott Collyer <scollyer@google.com>
2019-01-10 21:16:45 -0800

    hatch: Add support for bc12 detection
    Change-Id: Ie81e66c2921b670d8819641c922eaf38126f9641
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- board/hatch/board.c
+++ board/hatch/board.c
@@ -24,10 +24,12 @@
 #include "spi.h"
 #include "switch.h"
 #include "system.h"
+#include "task.h"
 #include "temp_sensor.h"
 #include "thermal.h"
 #include "thermistor.h"
 #include "uart.h"
+#include "usb_charge.h"
 #include "usb_pd.h"
 #include "usbc_ppc.h"
 #include "util.h"
@@ -87,6 +89,22 @@ DECLARE_DEFERRED(hpd_mst_en_ctrl_deferred);
 static void hpd_interrupt(enum gpio_signal signal)
 {
 	hook_call_deferred(&hpd_mst_en_ctrl_deferred_data, 2 * MSEC);
+}
+
+static void bc12_interrupt(enum gpio_signal signal)
+{
+	switch (signal) {
+	case GPIO_USB_C0_BC12_INT_ODL:
+		task_set_event(TASK_ID_USB_CHG_P0, USB_CHG_EVENT_BC12, 0);
+		break;
+
+	case GPIO_USB_C1_BC12_INT_ODL:
+		task_set_event(TASK_ID_USB_CHG_P1, USB_CHG_EVENT_BC12, 0);
+		break;
+
+	default:
+		break;
+	}
 }
 
 #include "gpio_list.h" /* Must come after other header files. */
