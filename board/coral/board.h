7ae27dc8c13ccfd245e191a09d28bea749850ae4
lu.zhang <lu.zhang@bitland.corp-partner.google.com>
2019-01-14 10:49:43 +0800

    coral:enable battery cutoff when shutdown_on_critical_battery
    The machine can't power on when the soc of battery is 0% with AC adapter plugging in and this
    behavior is not consistent as usual. The power consumption is very low when battery is cutoff,
    so let battery cutoff instead of EC hibernate to try to avoid battery soc dropping to 0%.
    
    BRANCH=coral
    BUG=b:120407297
    TEST=Discharge the battery to verify if the battery is cutoff.
    
    Change-Id: Ia8ce02e9e6e9b1a0eafe872664fd246667a18b00

--- board/coral/board.h
+++ board/coral/board.h
@@ -326,6 +326,7 @@ void board_tcpc_init(void);
 /* Sensors without hardware FIFO are in forced mode */
 #define CONFIG_ACCEL_FORCE_MODE_MASK (1 << LID_ACCEL)
 
+#define CONFIG_BATTERY_CRITICAL_SHUTDOWN_CUT_OFF
 #endif /* !__ASSEMBLER__ */
 
 #endif /* __CROS_EC_BOARD_H */
