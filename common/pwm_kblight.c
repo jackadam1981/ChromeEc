f454ee4c8fb68dddb152c3329d820ad50df19bf8
michael5 chen <michael5_chen@pegatroncorp.com>
2019-01-15 13:10:30 +0800

    rammus: Reduced keybaord backlight PWM
    Reduced keyboard backlight PWM to 80% by customer request.
    
    BUG=b:120813003
    BRANCH=None
    TEST=Manual
    Flash EC FW and check keyboard backlight.
    Using tool ectool pwmgetduty kb get value 52428 (80%).
    
    Change-Id: I879c4900846f5c4dc2a2a44e39396b76f167c9fa
    Signed-off-by: michael5 chen <michael5_chen@pegatroncorp.com>

--- common/pwm_kblight.c
+++ common/pwm_kblight.c
@@ -15,7 +15,12 @@ const enum pwm_channel kblight_pwm_ch = PWM_CH_KBLIGHT;
 
 static int kblight_pwm_set(int percent)
 {
+#ifdef CONFIG_PWM_KBLIGHT_RECONFIG_PERCENTAGE
+	pwm_set_duty(kblight_pwm_ch,
+			percent * CONFIG_PWM_KBLIGHT_RECONFIG_PERCENTAGE / 100);
+#else
 	pwm_set_duty(kblight_pwm_ch, percent);
+#endif
 	return EC_SUCCESS;
 }
 
