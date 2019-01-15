10ef7946d0d85ddc33a4213b2db14d42e87b1404
Patrick Georgi <pgeorgi@google.com>
2019-01-15 17:06:49 +0100

    chip/g: mark symbols used that are used from assembly code
    Otherwise LTO optimizes them away before finding out it, in fact, needs
    them - then complains.
    
    BUG=none
    BRANCH=none
    
    Change-Id: I244e5bd3da18dddec636bbe1e99c229bc3af44ec
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>

--- chip/g/uart_bitbang.c
+++ chip/g/uart_bitbang.c
@@ -318,7 +318,7 @@ static int uart_bitbang_receive_char(uint8_t *rxed_char, uint32_t *next_tick)
 	return EC_SUCCESS;
 }
 
-void uart_bitbang_irq(void)
+void __attribute__((used)) uart_bitbang_irq(void)
 {
 	uint8_t rx_buffer[20];
 	size_t i = 0;
