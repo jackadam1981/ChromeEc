b59a90248d7c2a2fe3ddb9926023ba8bb081c53d
Diana Z <dzigterman@chromium.org>
2019-01-15 15:53:19 -0700

    LSM6: Correct timestamp reporting and FIFO lengths
    When an accelerometer sends a sample to the motion_sense task with a
    timestamp, the timestamp should represent the time of the IRQ which
    caused the FIFO read.  This change caches a timestamp after the
    accelerometer data is read to use when pushing all data from that read.
    
    This also corrects the number of bits in the FIFO length field and the
    units for the FIFO watermark.
    
    BRANCH=octopus
    BUG=b:120679547
    TEST=builds, phaser360 CTS pass rate unaffected
    
    Change-Id: I220aa2e8fa23af3f7833999cdfac966e8695c831
    Signed-off-by: Diana Z <dzigterman@chromium.org>

--- driver/accelgyro_lsm6dsm.c
+++ driver/accelgyro_lsm6dsm.c
@@ -21,7 +21,7 @@
 #define CPRINTS(format, args...) cprints(CC_ACCEL, format, ## args)
 
 #ifdef CONFIG_ACCEL_FIFO
-static uint32_t last_interrupt_timestamp;
+static volatile uint32_t last_interrupt_timestamp;
 #endif
 
 /**
@@ -51,9 +51,13 @@ static int config_interrupt(const struct motion_sensor_t *s)
 		return ret;
 
 #ifdef CONFIG_ACCEL_FIFO
-	/* As soon as one sample is ready, trigger an interrupt. */
+	/*
+	 * As soon as one sample is ready, trigger an interrupt.
+	 * While the FIFO length will be read in uint16, this register is in
+	 * bytes
+	 */
 	ret = st_raw_write8(s->port, s->addr, LSM6DSM_FIFO_CTRL1_ADDR,
-			 OUT_XYZ_SIZE / sizeof(uint16_t));
+			 OUT_XYZ_SIZE);
 	if (ret != EC_SUCCESS)
 		return ret;
 	int1_ctrl_val |= LSM6DSM_INT_FIFO_TH | LSM6DSM_INT_FIFO_OVR |
@@ -198,7 +202,7 @@ static int fifo_next(struct lsm6dsm_data *private)
  * push_fifo_data - Scan data pattern and push upside
  */
 static void push_fifo_data(struct motion_sensor_t *accel, uint8_t *fifo,
-			   uint16_t flen)
+			   uint16_t flen, uint32_t int_ts)
 {
 	struct lsm6dsm_data *private = accel->drv_data;
 	/* In FIFO sensors are mapped in a different way. */
@@ -232,8 +236,7 @@ static void push_fifo_data(struct motion_sensor_t *accel, uint8_t *fifo,
 
 		vect.flags = 0;
 		vect.sensor_num = accel - motion_sensors + id;
-		motion_sense_fifo_add_data(&vect, accel + id, 3,
-					   last_interrupt_timestamp);
+		motion_sense_fifo_add_data(&vect, accel + id, 3, int_ts);
 
 		fifo += OUT_XYZ_SIZE;
 		flen -= OUT_XYZ_SIZE;
@@ -246,7 +249,7 @@ static int load_fifo(struct motion_sensor_t *s, const struct fstatus *fsts)
 	uint8_t fifo[FIFO_READ_LEN];
 
 	/*
-	 * DIFF[9:0] are number of unread uint16 in FIFO
+	 * DIFF[11:0] are number of unread uint16 in FIFO
 	 * mask DIFF and compute total byte len to read from FIFO.
 	 */
 	left = fsts->len & LSM6DSM_FIFO_DIFF_MASK;
@@ -269,7 +272,7 @@ static int load_fifo(struct motion_sensor_t *s, const struct fstatus *fsts)
 			return err;
 
 		/* Manage patterns and push data. */
-		push_fifo_data(s, fifo, length);
+		push_fifo_data(s, fifo, length, last_interrupt_timestamp);
 		left -= length;
 	} while (left > 0);
 
