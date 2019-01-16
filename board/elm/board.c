625b82e81fb1e34b5565948f0fadd3712da53b34
xiong.huang@bitland.corp-partner.google.com <xiong.huang@bitland.corp-partner.google.com>
2019-01-16 14:51:38 +0800

    hana: Disable keyboard wake in S3.
    Currently EC have no chance to disable KB function. When OS is S3 state in tablet mode,
    OS will wake up once pressing keys.
    
    BUG=b/122097163
    BRANCH=firmware-oak-8438.B
    TEST=DUT will not be resumed from S3 when pressing Keyboard under tablet mode.
    
    Change-Id: I91f1c713e4352635e6d77ee35aaf9768ed689db9

--- board/elm/board.c
+++ board/elm/board.c
@@ -500,9 +500,14 @@ static void board_chipset_resume(void)
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_chipset_resume, HOOK_PRIO_DEFAULT);
 
+void board_motion_lid_calc(void);
 /* Called on AP S0 -> S3 transition */
 static void board_chipset_suspend(void)
 {
+#ifdef CONFIG_LID_ANGLE
+	/* disable keyboard wake entirely in S3 when DUT in tent or tablet mode */
+	board_motion_lid_calc();
+#endif
 	board_spi_disable();
 #ifdef CONFIG_TEMP_SENSOR_TMP432
 	hook_call_deferred(&tmp432_set_power_deferred_data, 0);
@@ -667,5 +672,14 @@ void lid_angle_peripheral_enable(int enable)
 uint16_t tcpc_get_alert_status(void)
 {
 	return gpio_get_level(GPIO_PD_MCU_INT) ? PD_STATUS_TCPC_ALERT_0 : 0;
+}
+
+void board_motion_lid_calc(void)
+{
+	if (motion_sensors[BASE_ACCEL].state == SENSOR_INITIALIZED && \
+		motion_sensors[LID_ACCEL].state == SENSOR_INITIALIZED)
+	{
+		motion_lid_calc();
+	}
 }
 
