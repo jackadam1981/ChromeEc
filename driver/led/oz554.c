85b17ff166f55802c6931f182681fc9567c20469
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2019-01-17 13:32:20 +0800

    Karma: Add PANEL_LM_SSK1 panel settings to OZ554
    GPIOC5, GPIO01 and GPIOB1 are used for panel id.
    
    Register 0x02: setting LED current to 55(mA)
    (offset = 2, data = 0x55)
    
    BUG=b:
    BRANCH=firmware-kalista-11343.B
    TEST=make buildall PASS
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    
    Change-Id: I6668d0930d150c0a10ab70d6539f1415ea231e8a

--- driver/led/oz554.c
+++ driver/led/oz554.c
@@ -18,6 +18,13 @@
 #define I2C_ADDR_OZ554		0x62
 #define OZ554_DATA_SIZE		6
 
+enum panel_id_list {
+	PANEL_LM_SSE2 = 0,
+	PANEL_LM_SSK1,
+	PANEL_COUNT,
+	PANEL_UNKNOWN,
+};
+
 struct oz554_value {
 	uint8_t offset;
 	uint8_t data;
@@ -34,7 +41,7 @@ struct oz554_value {
  */
 
 /* This ordering is suggested by vendor. */
-static const struct oz554_value order[] = {
+static struct oz554_value order[] = {
 	/*
 	 * Reigster 0x01: Operation frequency control
 	 * Frequency selection: 300(KHz)
@@ -90,6 +97,29 @@ static void set_oz554_reg(void)
 	CPRINTS("Wrote OZ554 settings");
 }
 
+static int get_panel_id(void)
+{
+	int pin_status = 0;
+
+	if (gpio_get_level(GPIO_PANEL_ID_0))
+		pin_status |= 0x01;
+	if (gpio_get_level(GPIO_PANEL_ID_1))
+		pin_status |= 0x02;
+	if (gpio_get_level(GPIO_PANEL_ID_2))
+		pin_status |= 0x04;
+
+	CPRINTS("Panel pin_status 0x%X", pin_status);
+
+	switch (pin_status) {
+	case 0x04:
+		return PANEL_LM_SSE2;
+	case 0x05:
+		return PANEL_LM_SSK1;
+	default:
+		return PANEL_UNKNOWN;
+	}
+}
+
 static void backlight_enable_deferred(void)
 {
 	if (gpio_get_level(GPIO_PANEL_BACKLIGHT_EN))
@@ -104,6 +134,14 @@ void backlight_enable_interrupt(enum gpio_signal signal)
 
 static void init_oz554(void)
 {
+	int panel_id = get_panel_id();
+
+	CPRINTS("Panel id %d", panel_id);
+
+	if (panel_id == PANEL_LM_SSK1)
+		/* Reigster 0x02: Setting LED current: 55(mA) */
+		order[1].data = 0x55;
+
 	gpio_enable_interrupt(GPIO_PANEL_BACKLIGHT_EN);
 }
 DECLARE_HOOK(HOOK_INIT, init_oz554, HOOK_PRIO_DEFAULT);
