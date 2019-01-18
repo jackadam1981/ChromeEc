f2830021023879b08acf77047c6485afd68367f3
YongBeum Ha <ybha@samsung.com>
2019-01-18 11:18:59 +0900

    casta : hw bug fix
    Change-Id: Ia2eb006f7aa961a14278b7743f8ac8279ce161e9
    Signed-off-by: YongBeum Ha <ybha@samsung.com>

--- board/casta/board.c
+++ board/casta/board.c
@@ -88,6 +88,16 @@ const struct temp_sensor_t temp_sensors[] = {
 };
 BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 
+static void board_init(void)
+{
+	uint32_t val;
+	if (cbi_get_board_version(&val) == EC_SUCCESS && val > 0)
+		return;
+
+	gpio_set_flags(GPIO_USB_C0_MUX_INT_ODL, GPIO_INT_FALLING | GPIO_PULL_UP);
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
 /*
  * I2C callbacks to ensure bus free time for battery I2C transactions is at
  * least 5ms.
