5ccddf604fdb5ed8fb69b2afed83143b345aa038
Zhuohao Lee <zhuohao@chromium.org>
2019-01-19 17:03:10 +0800

    usb_port_power_smart: turn on/off charging mode during system suspend
    This patch adds a feature to allow the userspace program turns on/off
    the usb type-a charging mode during system suspend.
    
    BUG=b:121438672
    BRANCH=firmware-rammus-11275
    TEST=make -j buildall
         ectool usbchargemode 0 0x2 0, CDP works in S0 and S0ix
         ectool usbchargemode 0 0x2 1, CDP works in S0 but not in S0ix
    
    Change-Id: Icb8ab1b3f1beb671fbd02f441bf40284ba74e097
    Signed-off-by: Zhuohao Lee <zhuohao@chromium.org>

--- include/usb_charge.h
+++ include/usb_charge.h
@@ -33,11 +33,13 @@ enum usb_charge_mode {
 /**
  * Set USB charge mode for the port.
  *
- * @param usb_port_id	Port to set.
- * @param mode		New mode for port.
+ * @param usb_port_id		Port to set.
+ * @param mode			New mode for port.
+ * @param inhibit_charge	Inhibit charging during system suspend.
  * @return EC_SUCCESS, or non-zero if error.
  */
-int usb_charge_set_mode(int usb_port_id, enum usb_charge_mode mode);
+int usb_charge_set_mode(int usb_port_id, enum usb_charge_mode mode,
+			int inhibit_charge);
 
 #ifdef HAS_TASK_USB_CHG_P0
 #define USB_CHG_EVENT_BC12 TASK_EVENT_CUSTOM(1)
