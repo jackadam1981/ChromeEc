bae9a5db9ffd09a6a20d10da9dd9dc6146e06976
Zhuohao Lee <zhuohao@chromium.org>
2019-01-20 21:59:48 +0800

    rammus: turn the USB type-a charging off by default in suspend mode
    This patch turns the USB type-a charging off by default in suspend mode.
    If the DUT power on with connecting a USB device, the OS must send an
    event to clear the 8th-bit. Otherwise, the USB device will lost the
    power in suspend mode, and the wakeup event won't be generated if the
    device is a wakeup source (eg. USB keyboard or mouse).
    
    BUG=b:121438672
    BRANCH=firmware-rammus-11275
    CQ-DEPEND=CL:1423758
    TEST=Power on DUT without connecting USB, then suspend the DUT.
         Verfied the USB type-a charging off.
    
    Change-Id: I39a0f85d7033b798a80e80cc302a2c72cebae8d6
    Signed-off-by: Zhuohao Lee <zhuohao@chromium.org>

--- board/rammus/board.c
+++ board/rammus/board.c
@@ -465,6 +465,17 @@ static void board_init(void)
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
+static void usb_charge_mode_init(void)
+{
+	/*
+	 * By default, turn the charging off when system suspends.
+	 * If system power on with connecting a USB device,
+	 * the OS must send an event to EC to clear 8th-bit.
+	 */
+	usb_charge_set_mode(0, USB_CHARGE_MODE_CDP | 0x80);
+}
+DECLARE_HOOK(HOOK_INIT, usb_charge_mode_init, HOOK_PRIO_DEFAULT + 1);
+
 /**
  * Buffer the AC present GPIO to the PCH.
  */
