f44db52b2c85201d88dbc95143fd6be87b2ed1f5
Mary Ruthven <mruthven@google.com>
2019-01-23 16:27:42 -0800

    cr50: dbg: add support for debugging u2f
    Cr50 only records power button presses in certain cases, and there isn't
    a clear way to check if cr50 thinks the power button has been pressed
    recently. Add a print statement in record_power_button and a call to
    pop_check_presence in the powerbtn console command. Only add these to
    DBG images.
    
    BUG=b:123310652
    BRANCH=cr50
    TEST=none
    
    Change-Id: Ie104ac10d97c080812e19e65342cfe35ed1ea238
    Signed-off-by: Mary Ruthven <mruthven@google.com>

--- board/cr50/power_button.c
+++ board/cr50/power_button.c
@@ -13,6 +13,7 @@
 #include "system_chip.h"
 #include "task.h"
 #include "timer.h"
+#include "u2f_impl.h"
 
 #define CPRINTS(format, args...) cprints(CC_RBOX, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_RBOX, format, ## args)
@@ -120,7 +121,11 @@ void board_physical_presence_enable(int enable)
 static int command_powerbtn(int argc, char **argv)
 {
 	ccprintf("powerbtn: %s\n",
-		 rbox_powerbtn_is_pressed() ? "pressed\n" : "released\n");
+		 rbox_powerbtn_is_pressed() ? "pressed" : "released");
+
+#ifdef CR50_DEV
+	pop_check_presence(1);
+#endif
 	return EC_SUCCESS;
 }
 DECLARE_CONSOLE_COMMAND(powerbtn, command_powerbtn, "",
