37294e276bdc6bef56846a7f212aa64a8bd40f8d
michael5 chen <michael5_chen@pegatroncorp.com>
2019-01-25 17:51:20 +0800

    rammus: Reduce adapter charger current limit setting
    Modify adapter charger current limit to 92% of adapter current to
    reduce charge current.
    
    BUG=b:123379983
    BRANCH=None
    TEST=Manual
     Using ectool i2cread to check charger setting. (0xac0)
    
    Change-Id: I49bb8e5007df1b10fc63c9577a82f4ceb174d118
    Signed-off-by: michael5 chen <michael5_chen@pegatroncorp.com>

--- board/rammus/board.c
+++ board/rammus/board.c
@@ -529,7 +529,7 @@ void board_set_charge_limit(int port, int supplier, int charge_ma,
 	 * Limit the input current to 96% negotiated limit,
 	 * to account for the charger chip margin.
 	 */
-	charge_ma = charge_ma * 96 / 100;
+	charge_ma = charge_ma * 92 / 100;
 	charge_set_input_current_limit(
 			MAX(charge_ma, CONFIG_CHARGER_INPUT_CURRENT),
 			charge_mv);
