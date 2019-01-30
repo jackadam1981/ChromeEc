8d79a62c5e0cfb20257919ffa9179e1c39d42a9a
James_Chao <james_chao@asus.corp-partner.google.com>
2019-01-30 15:38:07 +0800

    ps8xxx: put the PS8751's power role to sink state
    BUG=b:123607435
    BRANCH=octopus
    TEST=check the external monitor can work
    
    Change-Id: Ibe334576b907774df62865817af6ba5eb3e9d96f

--- driver/tcpm/ps8xxx.c
+++ driver/tcpm/ps8xxx.c
@@ -207,6 +207,13 @@ struct i2c_stress_test_dev ps8xxx_i2c_stress_test_dev = {
 };
 #endif /* CONFIG_CMD_I2C_STRESS_TEST_TCPC */
 
+static int ps8xxx_mux_init(int port)
+{
+	tcpci_tcpm_mux_init(port);
+	return mux_write(port, TCPC_REG_ROLE_CTRL,
+		TCPC_REG_ROLE_CTRL_SET(0, 1, TYPEC_CC_RD, TYPEC_CC_RD));
+}
+
 static int ps8xxx_mux_enter_low_power_mode(int port)
 {
 	mux_write(port, TCPC_REG_ROLE_CTRL,
@@ -215,7 +222,7 @@ static int ps8xxx_mux_enter_low_power_mode(int port)
 }
 
 const struct usb_mux_driver ps8xxx_usb_mux_driver = {
-	.init = &tcpci_tcpm_mux_init,
+	.init = &ps8xxx_mux_init,
 	.set = &tcpci_tcpm_mux_set,
 	.get = &tcpci_tcpm_mux_get,
 	.enter_low_power_mode = &ps8xxx_mux_enter_low_power_mode,
