fc8313438c7b4826f466b8bd36a3f1857121a374
Jett Rink <jettrink@chromium.org>
2019-01-30 10:19:16 -0700

    lsm6ds3: update comment for clarity
    Add more comment for who am i registers since the name for st parts and
    who am i register does not seem to mix well.
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I38256c704a81984b720304d7a7499da93802d577
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- driver/accelgyro_lsm6dsm.h
+++ driver/accelgyro_lsm6dsm.h
@@ -68,7 +68,9 @@
 
 /* Who Am I */
 #define LSM6DSM_WHO_AM_I_REG		0x0f
+/* LSM6SM/LSM6SL/LSM6S3TR-C */
 #define LSM6DSM_WHO_AM_I			0x6a
+/* LSM6S3 */
 #define LSM6DS3_WHO_AM_I			0x69
 
 #define LSM6DSM_CTRL1_ADDR		0x10
