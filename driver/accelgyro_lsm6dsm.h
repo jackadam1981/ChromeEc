4b561b06da4f28a268a7e00e828f5eaf422aa49d
Jett Rink <jettrink@chromium.org>
2019-01-30 10:19:16 -0700

    lsm6ds3: Add LDM6DS3 variant to existing LSM6DSM
    The lsm6dsX where X is M/L/3 are very similar. The only differences
    appear to be the who am I register value and fifo size.
    
    BRANCH=none
    BUG=b:122281217
    TEST=Arcada with CL stack, can read sensor data via EC console using
    `accelread 0` command
    
    Change-Id: Iaa12d49616c66e574e069b43aba2618722f0dda0
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- driver/accelgyro_lsm6dsm.h
+++ driver/accelgyro_lsm6dsm.h
@@ -27,6 +27,7 @@
 /* Who Am I */
 #define LSM6DSM_WHO_AM_I_REG		0x0f
 #define LSM6DSM_WHO_AM_I		0x6a
+#define LSM6DS3_WHO_AM_I		0x69
 
 /* COMMON DEFINE FOR ACCEL-GYRO SENSORS */
 #define LSM6DSM_EN_BIT			0x01
@@ -68,10 +69,6 @@
 #define LSM6DSM_ODR_REG(_sensor) \
 	(LSM6DSM_CTRL1_ADDR + _sensor)
 #define LSM6DSM_ODR_MASK		0xf0
-
-/* Hardware FIFO size in byte */
-#define LSM6DSM_MAX_FIFO_SIZE		4096
-#define LSM6DSM_MAX_FIFO_LENGTH	(LSM6DSM_MAX_FIFO_SIZE / OUT_XYZ_SIZE)
 
 /* FIFO decimator registers and bitmask */
 #define LSM6DSM_FIFO_CTRL1_ADDR		0x06
