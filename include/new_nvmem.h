5eae2a47c9b3ee4606526f4d1ad9efe837974d04
Vadim Bendebury <vbendeb@chromium.org>
2019-02-01 10:19:51 -0800

    cr50: New NVMEM flash storage implementation
    This patch is a proposed implementation of the new TPM NVMEM flash
    layer.
    
    There is a big comment block in common/new_nvmem.c describing the
    approach taken, changes to the API and outstanding issues.
    
    This implementation follows the design document attached to
    b:69907320, with a few important omissions:
    
      - the initialization function is not checking for duplicate objects
        found in the flash
    
      - transaction processing is not yet supported
    
    With all required changes to the rest of the code the new flash
    storage scheme consumes 6752 bytes of code storage.
    
    One of the more important aspects of this implementation is that the
    (key, value) pair objects are stored in the flash only, they are not
    duplicated in the SRAM cache. The advantage of this is that there
    could be more space dedicated to these objects. Soft limit is set to
    1K as opposed to 272 bytes available with the legacy scheme. The major
    disadvantage is the need for the user not to forget to release the
    (key, value) pair retrieved from NVMEM, as it occupies space on the
    heap.
    
    BRANCH=cr50, cr50-mp
    BUG=b:69907320
    
    TEST=with the rest of the patches applied the following tests pass:
       - test cases in ./test (completely reworked for the new scheme)
    
       - TCG suite (passes on par with the existing Cr50 code with the
         reduced code footprint TPM2 library)
    
       - Chrome OS device migrates from legacy to new implementation with
         user account maintained.
    
       - Chrome OS user account is maintained over AP and H1 reboots and
         deep sleep cycles.
    
    Change-Id: I6252649597c03abd4a08e2d55d61e384fe037ef7
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

---
+++ include/new_nvmem.h
@@ -0,0 +1,141 @@
+#ifndef __TPM2_NVMEM_TEST_NEW_NVMEM_H
+#define __TPM2_NVMEM_TEST_NEW_NVMEM_H
+
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "nvmem.h"
+#include "nvmem_vars.h"
+#include "util.h"
+
+#define NVMEM_NOT_INITIALIZED ((unsigned int)-1)
+
+/*
+ * A totally arbitrary byte limit for space occupied by (key, value) pairs in
+ * the flash. This is an improvement compared to the legacy case where there
+ * were just 272 bytes dedicated to the (key, value) pairs storage.
+ */
+#define MAX_VAR_TOTAL_SPACE        1000
+
+/*
+ * Let's be reasonable: we're unlikely to have keys longer than 40 or so
+ * bytes, and leave full 255 bytes for the value. Total data space occupied by
+ * a (key, value) pair is not to exceed the value below.
+ */
+#define MAX_VAR_BODY_SPACE  300
+
+enum nn_object_type {
+	NN_OBJ_OLD_COPY = 0,
+	NN_OBJ_TUPLE = 1,
+	NN_OBJ_TPM_RESERVED = 2,
+	NN_OBJ_TPM_OBJECT = 3,
+	NN_OBJ_TRANSACTION_DEL = 4,
+	NN_OBJ_ESCAPE = 5,
+	NN_OBJ_ERASED = 7,
+};
+
+/*
+ * Structure placed at the base of each flash page used for NVMEM storage.
+ *
+ * page_number: allows to arrange pages in order they were added
+ *
+ * data_offset: the offset of the first element in the page (space above
+ *              page header and below data_offset could be taken by the
+ *              'tail' of the object stored on the previous page).
+ *
+ * page_hash:   is used to verify page header integrity
+ */
+struct nn_page_header {
+	unsigned int page_number: 21;
+	unsigned int data_offset: 11;
+	uint32_t     page_hash;
+} __packed;
+
+/*
+ * Index of the 'virtual' last reserved object. RAM index space and max
+ * counter objects stored at fixed location in the NVMEM cache are considered
+ * reserved objects by this NVMEM flash layer.
+ */
+#define NV_VIRTUAL_RESERVE_LAST (NV_RESERVE_LAST + 2)
+
+/*
+ * Container header for all blobs stored in flash.
+ *
+ * container_type: type of object stored in the container. MAKE SURE THIS
+ *                 FIELD TYPE IS THE FIRST FIELD IN THIS STRUCTURE, it is
+ *                 supposed to be in the first word of the container so that
+ *                 the type can be erased when object is deleted.
+ *
+ * container_type_copy: immutable copy of the container_type field, used to
+ *                      verify contents of deleted objects.
+ *
+ * encrypted: set to 1 if contents are encrypted.
+ *
+ * size: size of the payload, in fact could fit in 11 bits
+ *
+ * generation: a free running counter, used to compare ages of two containers
+ *
+ * container_hash: hash of the ENTIRE container, both header and body
+ *                 included. This field is set to zero before hash is calculated
+ */
+struct nn_container {
+	unsigned int container_type :3;
+	unsigned int container_type_copy :3;
+	unsigned int encrypted: 1;
+	unsigned int size: 12;
+	unsigned int generation: 2;
+	unsigned int container_hash: 19;
+} __packed;
+
+/*
+ * Helper structure to keep track of accesses to the flash storage, used to
+ * store a context of sequential accesses to the flash storage.
+ *
+ * The 'last container read' state has special significance: this is both part
+ * of the seed for encrypting/decryping container contents, and can be used to
+ * unwind reading of the container header when the end of storage is reached.
+ *
+ * list_index; index of the current page in the list of pages
+ *
+ * data_offset: offset where the next read will read from
+ *
+ * orig_data_offset; offset where the container header was read from last
+ *
+ * ph: pointer to the flash page which the next read will read from
+ *
+ * orig_ph: pointer to the flash page where the container header was read
+ *          from last.
+ */
+struct page_tracker {
+	uint8_t list_index;
+	uint16_t data_offset;
+	uint16_t orig_data_offset;
+	const struct nn_page_header *ph;
+	const struct nn_page_header *orig_ph;
+};
+
+enum ec_error_list new_nvmem_init(void);
+enum ec_error_list new_nvmem_migrate(unsigned int nvmem_act_partition);
+enum ec_error_list new_nvmem_save(void);
+
+enum ec_error_list get_next_object(struct page_tracker *pt,
+				   struct nn_container *ch,
+				   int include_deleted);
+
+#if defined(TEST_BUILD) && !defined(TEST_FUZZ)
+enum ec_error_list browse_flash_contents(int);
+enum ec_error_list compact_nvmem(void);
+extern uint16_t total_var_space;
+int is_uninitialized(const void *p, size_t size);
+extern struct page_tracker master_pt;
+#endif
+
+/*
+ * Clear tpm data from nvmem.
+ */
+int nvmem_erase_tpm_data(void);
+
+#endif  /* ! __TPM2_NVMEM_TEST_NEW_NVMEM_H */
