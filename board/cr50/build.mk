f9041e99ed89ea6bdaec872f286465569ed7b332
Daisuke Nojiri <dnojiri@chromium.org>
2019-02-05 16:05:42 -0800

    Cr50: Enable packet transfer over EC UART
    Cr50 and EC are connected by UART_EC_TX & UART_EC_RX and there are 3
    modes associated:
    
      1. Normal mode: UART_RX/TX are not used
      2. CCD mode: UART_RX/TX are used to pass-through data to/from USB.
      3. Packet mode: UART_RX/TX are used to transfer packets
    
    Normal and CCD modes are handled by rdd.c. Note that other CCD mode
    users (e.g. AP console) shouldn't be affected by which mode EC and Cr50
    are engaged in.
    
    Since current version of Cr50 doesn't support break code on UART, we
    make Cr50 watch UART_RX for 'marker' (= 0xec 0xec ...). When a marker
    is detected, Cr50 transitions to the packet mode.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b/119329144
    BRANCH=none
    TEST=Verify EC can send version string to cr50.
    
    Change-Id: I989940ea81563dbd390d8512d7c9241867cd4b44

--- board/cr50/build.mk
+++ board/cr50/build.mk
@@ -61,6 +61,7 @@ board-y += tpm2/virtual_nvmem.o
 board-y += tpm_nvmem_ops.o
 board-y += wp.o
 board-$(CONFIG_U2F) += u2f.o
+board-$(HAS_TASK_EC_COMM) += ec_comm.o
 
 ifneq ($(H1_RED_BOARD),)
 CPPFLAGS += -DH1_RED_BOARD
