85c838988472a3fb2f9660e179b42c11cf59de2c
Tom Hughes <tomhughes@chromium.org>
2019-02-12 09:49:00 -0800

    fpsensor: Build fpsensor source file with C++
    This change removes abs() since this change also makes the fingerprint
    firmware link against the toolchain's C standard library, which already
    provides an implementation. Note that abs() is not actually used by the
    FPC matching algorithm even though it links against it.
    
    BRANCH=none
    BUG=b:236025198, b:234181908
    TEST=On dragonclaw v0.2 with servo_micro and J-Trace attached:
         ./util/flash_jlink.py -b bloonchipper -i ./build/bloonchipper/ec.bin
         > fpenroll
         > fpmatch
    TEST=On dragonclaw v0.2 with servo_micro and J-Trace attached:
         ./test/run_device_tests.py --board bloonchipper
         Test "aes": PASSED
         Test "cec": PASSED
         Test "cortexm_fpu": PASSED
         Test "crc": PASSED
         Test "flash_physical": PASSED
         Test "flash_write_protect": PASSED
         Test "fpsensor_hw": PASSED
         Test "fpsensor_spi_ro": PASSED
         Test "fpsensor_spi_rw": PASSED
         Test "fpsensor_uart_ro": PASSED
         Test "fpsensor_uart_rw": PASSED
         Test "mpu_ro": PASSED
         Test "mpu_rw": PASSED
         Test "mutex": PASSED
         Test "pingpong": PASSED
         Test "printf": PASSED
         Test "queue": PASSED
         Test "rollback_region0": PASSED
         Test "rollback_region1": PASSED
         Test "rollback_entropy": PASSED
         Test "rtc": PASSED
         Test "sha256": PASSED
         Test "sha256_unrolled": PASSED
         Test "static_if": PASSED
         Test "stdlib": PASSED
         Test "system_is_locked_wp_on": PASSED
         Test "system_is_locked_wp_off": PASSED
         Test "timer_dos": PASSED
         Test "utils": PASSED
         Test "utils_str": PASSED
         Test "stm32f_rtc": PASSED
         Test "panic_data_bloonchipper_v2.0.4277": PASSED
         Test "panic_data_bloonchipper_v2.0.5938": PASSED
    TEST=On icetower v0.1 with servo_micro and J-Trace attached:
         ./util/flash_jlink.py -b dartmonkey -i ./build/dartmonkey/ec.bin
         > fpenroll
         > fpmatch
    TEST=On icetower v0.1 with servo_micro and J-Trace attached:
         ./test/run_device_tests.py --board dartmonkey
         Test "aes": PASSED
         Test "cec": PASSED
         Test "cortexm_fpu": PASSED
         Test "crc": PASSED
         Test "flash_physical": PASSED
         Test "flash_write_protect": PASSED
         Test "fpsensor_hw": PASSED
         Test "fpsensor_spi_ro": PASSED
         Test "fpsensor_spi_rw": PASSED
         Test "fpsensor_uart_ro": PASSED
         Test "fpsensor_uart_rw": PASSED
         Test "mpu_ro": PASSED
         Test "mpu_rw": PASSED
         Test "mutex": PASSED
         Test "pingpong": PASSED
         Test "printf": PASSED
         Test "queue": PASSED
         Test "rollback_region0": PASSED
         Test "rollback_region1": PASSED
         Test "rollback_entropy": PASSED
         Test "rtc": PASSED
         Test "sha256": PASSED
         Test "sha256_unrolled": PASSED
         Test "static_if": PASSED
         Test "stdlib": PASSED
         Test "system_is_locked_wp_on": PASSED
         Test "system_is_locked_wp_off": PASSED
         Test "timer_dos": PASSED
         Test "utils": PASSED
         Test "utils_str": PASSED
         Test "panic_data_dartmonkey_v2.0.2887": PASSED
         Test "panic_data_nocturne_fp_v2.2.64": PASSED
         Test "panic_data_nami_fp_v2.2.144": PASSED
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Iab0de3355cbe928c7b7132263ea6d32c1e9f3a6e

--- include/system.h
+++ include/system.h
@@ -373,7 +373,11 @@ const char *system_get_build_info(void);
  * @param flags		Reset flags; see SYSTEM_RESET_* above.
  */
 #if !(defined(TEST_FUZZ) || defined(CONFIG_ZTEST))
+#if defined(__cplusplus) && !defined(__clang__)
+[[noreturn]]
+#else
 noreturn
+#endif
 #endif
 	void
 	system_reset(int flags);
