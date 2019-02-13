c670a181fc5e0fbd5e3a93499c4e41bfbfff1f05
Aseda Aboagye <aaboagye@google.com>
2019-02-13 15:28:01 -0800

    pd: Don't Try.Src if battery is disconnected.
    If a battery is disconnected (the discharge FET is disabled) we should
    not attempt Try.Src even though the battery state of charge may be above
    CONFIG_USB_PD_TRY_SRC_MIN_BATT_SOC.  The reason is that in this case,
    the battery is not providing charge to the system, so attempting Try.Src
    will temporarily cut VBUS which may be the only power source in the
    system at this time.
    
    This commit simply checks the battery disconnect state when deciding to
    attempt Try.Src.
    
    BUG=b:123722302
    BRANCH=firmware-nocturne-10984.B,master
    TEST=Flash nocturne with battery stuck in disconnected state, verify
    that Try.Src is not attempted.
    
    Change-Id: I232dbb365fcdaf504b1957114bd1e29afd4cb2b2
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -2134,6 +2134,15 @@ static void pd_update_try_source(void)
 	 */
 	pd_try_src_enable &= (battery_is_present() == BP_YES);
 #endif
+#ifdef CONFIG_BATTERY_REVIVE_DISCONNECT
+	/*
+	 * Don't attempt Try.Src if the battery is in the disconnect state.  The
+	 * discharge FET may not be enabled and so attempting Try.Src may cut
+	 * off our only power source at the time.
+	 */
+	pd_try_src_enable &= (battery_get_disconnect_state() ==
+			      BATTERY_NOT_DISCONNECTED);
+#endif /* CONFIG_BATTERY_REVIVE_DISCONNECT */
 
 	/*
 	 * Clear this flag to cover case where a TrySrc
