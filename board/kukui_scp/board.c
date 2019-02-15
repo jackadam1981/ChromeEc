2cff0f1a1b146242b31c7999ed0a57ffc43605d2
Nicolas Boichat <drinkcat@chromium.org>
2019-02-15 12:23:16 +0800

    TEST-ONLY: kukui_scp: Test functions for cache clean/invalidate per address
    BRANCH=none
    BUG=b:123676508
    TEST=see CL that implements the feature
    
    Change-Id: I9b472140e3650f9470a9de8b1cc27af46961622c
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- board/kukui_scp/board.c
+++ board/kukui_scp/board.c
@@ -6,6 +6,7 @@
 
 #include "common.h"
 #include "console.h"
+#include "cpu.h"
 #include "ec_version.h"
 #include "gpio.h"
 #include "hooks.h"
@@ -33,3 +34,78 @@ static void board_init(void)
 	gpio_enable_interrupt(GPIO_EINT7_TP);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+static uint32_t *cached = (void *)0x10000000;
+static uint32_t *direct = (void *)0x30000000;
+static const int len = 0x40;
+
+static int command_filltest(int argc, char **argv)
+{
+	int j;
+	int c = get_time().le.lo;
+
+	for (j = 0; j < len; j++)
+		cached[j] = c << 8 | j;
+
+	return EC_SUCCESS;
+}
+
+DECLARE_SAFE_CONSOLE_COMMAND(filltest, command_filltest,
+			     NULL,
+			     "Do D-cache fill test");
+
+static int command_flushtest(int argc, char **argv)
+{
+	int j;
+	uintptr_t addr;
+	unsigned int length;
+	char *e;
+
+	if (argc >= 4) {
+		addr = (uintptr_t)strtoi(argv[1], &e, 0);
+		if (*e)
+			return EC_ERROR_PARAM1;
+		length = (uintptr_t)strtoi(argv[2], &e, 0);
+		if (*e)
+			return EC_ERROR_PARAM2;
+
+		if (argv[3][0] == 'c') {
+			ccprintf("Clean\n");
+			if (addr == 0)
+				cpu_clean_invalidate_dcache();
+			else
+				cpu_clean_invalidate_dcache_range(addr, length);
+		} else if (argv[3][0] == 'i') {
+			ccprintf("Inval %08x\n", addr);
+			if (addr == 0)
+				cpu_invalidate_dcache();
+			else
+				cpu_invalidate_dcache_range(addr, length);
+		}
+	}
+
+	ccprintf("cached:");
+	for (j = 0; j < len; j++) {
+		if ((j % 8) == 0) {
+			ccprintf("\n%08x: ", &cached[j]);
+			cflush();
+		}
+		ccprintf("%08x ", cached[j]);
+	}
+	ccprintf("\n");
+
+	ccprintf("direct:");
+	for (j = 0; j < len; j++) {
+		if ((j % 8) == 0) {
+			ccprintf("\n%08x: ", &direct[j]);
+			cflush();
+		}
+		ccprintf("%08x ", direct[j]);
+	}
+	ccprintf("\n");
+
+	return EC_SUCCESS;
+}
+DECLARE_SAFE_CONSOLE_COMMAND(flushtest, command_flushtest,
+			     NULL,
+			     "Do D-cache flush test");
