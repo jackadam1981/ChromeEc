85900c85fc244f9eb9eb5a0ecdcdb7ccfefbce4d
Jessy jiang <jiangchao5@huaqin.corp-partner.google.com>
2019-02-15 14:08:33 +0800

    flapjack: Enable backlight channel for Flapjack
    flapjack requires to enable LED Bias to control LCD backlight.
    For C18 boards, three channels are required, and each channel has a
    current of 24 mA.
    For C19 boards, four channels are required, each channel having a
    current of 23.5mA.
    
    BUG=b:124465980
    BRANCH=none
    TEST=1:)When the system boots, the backlight will light up.
         2:)Use the ectool commands to check the channel and current.
    	channle: ectool i2cread 8 0 104 0xA0
    	current: ectool i2cread 8 0 104 0xA5
    	If it's C18 board, get 0xfa in channel command and 0xc8 in
    	current command.
    	If it's C19 board, get 0xfe in channel command and 0xc4 in
    	current command.
    	If the SKU value of the board is empty or incorrect, the default
    	value is the data of the C18 board.
    
    Change-Id: I2e51feec830c4cfd46e3eaec0f9db0e1cfdd620c

--- board/flapjack/board.h
+++ board/flapjack/board.h
@@ -200,6 +200,14 @@
 		(EC_HOST_EVENT_MASK(EC_HOST_EVENT_POWER_BUTTON) |\
 		 EC_HOST_EVENT_MASK(EC_HOST_EVENT_RTC))
 
+/* Define the backlight parameters*/
+#define PANEL_SIZE_MASK  (1 << 1)
+#define BL_EN_3CHANNELS  0xFA
+#define BL_EN_4CHANNELS  0xFE
+#define BL_C18_DIM       0xC8
+#define BL_C19_DIM       0xC4
+#define BL_PWM_EN        0xAC
+
 #ifndef __ASSEMBLER__
 
 enum oem_id {
