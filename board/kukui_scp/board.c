d9f8412d3a446bbb03d4afef35051b03594bee1a
Nicolas Boichat <drinkcat@chromium.org>
2019-02-15 10:26:56 +0800

    TEST-ONLY: Test command to keep the SCP busy
    BRANCH=none
    BUG=b:124472114
    TEST=busy
    
    Change-Id: Iebbd6f731c5b40dd201b817505cf8a05a692bb2d
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- board/kukui_scp/board.c
+++ board/kukui_scp/board.c
@@ -33,3 +33,49 @@ static void board_init(void)
 	gpio_enable_interrupt(GPIO_EINT7_TP);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+#include "watchdog.h"
+
+static void busy(void) {
+	int i, j;
+	const int it = 20000;
+	const int len = 0x100;
+	char buffer[len];
+	timestamp_t start;
+	uint64_t val;
+
+	start = get_time();
+	for (j = 0; j < len; j++)
+		buffer[j] = j;
+
+	val = 0;
+	for (i = 0; i < it; i++) {
+		for (j = 0; j < len; j++)
+			val += buffer[j];
+	}
+	ccprintf("busyloop: %d us (val: %lx)\n", time_since32(start), val);
+	cflush();
+}
+
+static int command_busytest(int argc, char **argv)
+{
+	int k;
+
+	for (k = 0; k < 20; k++) {
+		busy();
+		watchdog_reload();
+	}
+
+	return EC_SUCCESS;
+}
+DECLARE_SAFE_CONSOLE_COMMAND(busytest, command_busytest,
+			     NULL,
+			     "Busy loop");
+
+static void busy_loop(void);
+DECLARE_DEFERRED(busy_loop);
+static void busy_loop(void) {
+	busy();
+	hook_call_deferred(&busy_loop_data, 1*MSEC);
+}
+DECLARE_HOOK(HOOK_INIT, busy_loop, HOOK_PRIO_LAST);
