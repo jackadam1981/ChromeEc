811580a994fe1f6e8c05ff1f0f7def627a6ba65a
Jessy jiang <jiangchao5@huaqin.corp-partner.google.com>
2019-02-15 14:08:33 +0800

    flapjack: Enable backlight channel for Flapjack
    flapjack requires to enable LED Bias to control LCD backlight.
    For C18 boards, three channels are required, and each channel has a
    current of 24 mA.
    For C19 boards, four channels are required, each channel having a
    current of 23.5mA.
    
    CQ-DEPEND=CL:1459559
    BUG=b:124465980
    BRANCH=none
    TEST=1:)When the system boots, the backlight will light up.
         2:)Use the ectool commands to check the channel and current.
    		channle: ectool i2cread 8 0 104 0xA0
    		current: ectool i2cread 8 0 104 0xA5
    		If it's C18 board, get 0xfa in channel command and 0xc8 in
    		current command.
    		If it's C19 board, get 0xFE in channel command and 0xc4 in
    		current command.
    		If the SKU value of the board is empty, the default value is the
    		data of the C19 board.
    
    Change-Id: I2e51feec830c4cfd46e3eaec0f9db0e1cfdd620c

--- board/flapjack/board.c
+++ board/flapjack/board.c
@@ -53,6 +53,52 @@ uint16_t board_version;
 uint8_t oem;
 uint32_t sku;
 
+/*  TODO: replace with actual sku id. */
+static int board_type(void)
+{
+	int i;
+	const int SKUID_C18[] = {1, 2, 3};
+	const int SKUID_C19[] = {4, 5, 6};
+
+	for(i = 0; i < ARRAY_SIZE(SKUID_C18); i++) {
+		if (sku == SKUID_C18[i])
+			return BOARD_C18;
+	}
+
+	for(i = 0; i < ARRAY_SIZE(SKUID_C19); i++) {
+		if (sku == SKUID_C19[i])
+			return BOARD_C19;
+	}
+
+	return BOARD_UNKNOW;
+}
+
+static void update_backlight(uint8_t channel,uint8_t dim)
+{
+	i2c_write8(I2C_PORT_CHARGER, RT946X_ADDR, 0xA0, channel);
+	i2c_write8(I2C_PORT_CHARGER, RT946X_ADDR, 0xA5, dim);
+	i2c_write8(I2C_PORT_CHARGER, RT946X_ADDR, 0xA2, 0xAC);
+}
+
+static void board_update_backlight_from_sku(void)
+{
+
+	switch (board_type()) {
+	case BOARD_C18:
+		/* Enable backlight channel and set dim for C18*/
+		update_backlight(0xFA, 0xC8);
+		break;
+	case BOARD_C19:
+		/* Enable backlight channel and set dim for C19*/
+		update_backlight(0xFE, 0xC4);
+		break;
+	default:
+		update_backlight(0xFE, 0xC4);
+		break;
+	}
+}
+
+
 static void cbi_init(void)
 {
 	uint32_t val;
@@ -68,6 +114,7 @@ static void cbi_init(void)
 	if (cbi_get_sku_id(&val) == EC_SUCCESS)
 		sku = val;
 	CPRINTS("SKU: 0x%08x", sku);
+	board_update_backlight_from_sku();
 }
 DECLARE_HOOK(HOOK_INIT, cbi_init, HOOK_PRIO_INIT_I2C + 1);
 
