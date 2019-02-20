b704eb53756133768564dbb29c48caf14b932741
Philip Chen <philipchen@chromium.org>
2019-02-20 14:18:20 -0800

    bq25710: Disable VDPM-triggered PROCHOT by default
    PROCHOT_VDPM is triggered in every AC detach when VBUS drops.
    Ideally we may want to check the battery voltage when
    PROCHOT_VDPM is triggered. If battery voltage is not too low,
    we clear the flag and release PROCHOT pin.
    
    However, this PROCHOT profile isn't very useful for the case of
    AC detach, because EC already knows AC detach event from
    GPIO_AC_PRESENT pin and can check battery voltage then.
    
    PROCHOT_VDPM does provide finer-grained monitoring for VBUS, but
    before we come up with a good use of this profile, we may want
    to just disable this profile to prevent PROCHOT pin from being
    asserted in every AC detach.
    
    So let's disable PROCHOT_VDPM at charger initialization.
    
    BUG=b:123931545
    BRANCH=none
    TEST=boot hatch and then unplug AC, verify bq25710 doesn't assert PROCHOT
    
    Change-Id: Icbbbb5572f26a450d0df92890d99fc216564a981
    Signed-off-by: Philip Chen <philipchen@chromium.org>

--- driver/charger/bq25710.c
+++ driver/charger/bq25710.c
@@ -97,8 +97,19 @@ static int bq25710_set_low_power_mode(int enable)
 }
 #endif
 
-/* Charger interfaces */
+/* Disbale VDPM prochot profile at initialization */
+static void bq25710_disable_prochot_vdpm(void)
+{
+	int reg;
 
+	if (!raw_read16(BQ25710_REG_PROCHOT_OPTION_1, &reg)) {
+		raw_write16(BQ25710_REG_PROCHOT_OPTION_1,
+			    (reg & ~BQ25710_PROCHOT_PROFILE_VDPM));
+	}
+}
+DECLARE_HOOK(HOOK_INIT, bq25710_disable_prochot_vdpm, HOOK_PRIO_INIT_I2C + 1);
+
+/* Charger interfaces */
 const struct charger_info *charger_get_info(void)
 {
 	return &bq25710_charger_info;
