cd105dc00b07ca348ea59f05e223b8c67b6496ff
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2019-02-20 18:12:09 +0800

    eSPI OOB: Enable eSPI OOB support
    BUG=b:124858408
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I2f839ac0ff7161d857ba6a3f8c430d0936584043
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- include/espi.h
+++ include/espi.h
@@ -91,4 +91,46 @@ const char *espi_vw_get_wire_name(enum espi_vw_signal signal);
 int espi_signal_is_vw(int signal);
 
 
+#ifdef CONFIG_HOSTCMD_ESPI_OOB
+/* OOB channel SMBus command */
+enum espi_oob_smbus_command {
+	OOB_SMBUS_GET_TEMP = 1,
+	OOB_SMBUS_GET_RTC,
+};
+
+/* eSPI cycle type field */
+#define ESPI_FLASH_READ_CYCLE_TYPE       0x00
+#define ESPI_FLASH_WRITE_CYCLE_TYPE      0x01
+#define ESPI_FLASH_ERASE_CYCLE_TYPE      0x02
+#define ESPI_OOB_CYCLE_TYPE              0x21
+
+/* eSPI tag + len[11:8] field */
+#define ESPI_TAG_LEN_FIELD(tag, len) \
+		   ((((tag) & 0xF) << 4) | (((len) >> 8) & 0xF))
+
+/* OOB channel SMBus address field */
+#define OOB_SMBUS_DEST_ADDR_PCH          0x02
+#define OOB_SMBUS_SRC_ADDR_EC            0x1F
+
+/**
+ * eSPI OOB channel data buffer size is maximum 80 bytes(ITE hw design).
+ * Actually length field can reach 4096 bytes defined in intel espi spec.
+ */
+#define ESPI_OOB_MAX_LENGTH      80
+
+/**
+ * Receive messages from eSPI OOB channel
+ * @param oob_data point to the oob data array buffer
+ * @return received OOB data length or -1 if data length over buffer size
+ */
+int espi_oob_receive(uint8_t *oob_data);
+
+/**
+ * Send messages via eSPI OOB channel
+ * @param oob_data point to the oob data array buffer
+ * @return EC_SUCCESS, or non-zero if error
+ */
+enum ec_error_list espi_oob_send(uint8_t *oob_data);
+#endif
+
 #endif  /* __CROS_EC_ESPI_H */
