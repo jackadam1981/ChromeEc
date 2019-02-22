bf73a57c451da1e637e58e1e479b76e41e038f18
Scott Collyer <scollyer@google.com>
2019-02-22 06:54:32 -0800

    bc12: pi3usb9201: Add 5V rail control for bc1.2 detection
    In cases where the pi3usb9201 is powered by a 5V rail which depends on
    the AP power state, bc1.2 detection may not function when the AP is in
    G3. This CL adds code to enable the PP5000 rail prior to setting the
    pi3usb9201 into either host or client mode.
    
    BUG=b:122265772
    BRANCH=none
    TEST=Verfied that when pi3usb9201 is accessed while AP is in G3 that
    the i2c messages no longer fail. Prior to this change, bc1.2 detection
    following EC reboot was always failing.
    
    Change-Id: Ic7113ac8086237e109b4b08bca985127c3bacdf4
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- driver/bc12/pi3usb9201.c
+++ driver/bc12/pi3usb9201.c
@@ -202,6 +202,21 @@ static void bc12_power_down(int port)
 	pi3usb9201_interrupt_mask(port, 1);
 	/* Let charge manager know there's no more charge available. */
 	charge_manager_update_charge(CHARGE_SUPPLIER_NONE, port, NULL);
+#ifdef CONFIG_POWER_PP5000_CONTROL
+	/* Turn on the 5V rail to allow the chip to be powered. */
+	power_5v_enable(task_get_current(), 0);
+#endif
+}
+
+static void bc12_power_up(int port)
+{
+#ifdef CONFIG_POWER_PP5000_CONTROL
+	/* Turn on the 5V rail to allow the chip to be powered. */
+	power_5v_enable(task_get_current(), 1);
+	/* Give the pi3usb9201 time so it's ready to receive i2c messages */
+	msleep(10);
+#endif
+	pi3usb9201_interrupt_mask(port, 1);
 }
 
 void usb_charger_task(void *u)
@@ -242,6 +257,7 @@ void usb_charger_task(void *u)
 #endif
 
 		if (evt & USB_CHG_EVENT_DR_UFP) {
+			bc12_power_up(port);
 			if (bc12_detect_start(port)) {
 				struct charge_port_info new_chg;
 
@@ -275,6 +291,7 @@ void usb_charger_task(void *u)
 			 * If the port is in DFP mode, then need to set mode to
 			 * CDP_HOST which will auto close D+/D- switches.
 			 */
+			bc12_power_up(port);
 			rv = pi3usb9201_get_mode(port, &mode);
 			if (!rv && (mode != PI3USB9201_CDP_HOST_MODE))
 				pi3usb9201_set_mode(port,
