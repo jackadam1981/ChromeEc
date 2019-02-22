0d0a2185168a5cdaa68b9dc4a4f37e090dad0acf
Shine Yin <yinchenyang@huaqin.corp-partner.google.com>
2019-02-22 17:34:07 +0800

    flapjack: apply flapjack battery model parameters
    BUG=b:128512795
    BRANCH=None
    TEST=Test with CL:1515821 and CL:1507320
    
    Change-Id: Ie790fb2f78544f91b4fb58c5da79dcfdb4a3efde

--- board/flapjack/board.h
+++ board/flapjack/board.h
@@ -136,7 +136,7 @@
 #define CONFIG_BATTERY_MAX17055_ALERT
 
 /* Battery parameters for max17055 ModelGauge m5 algorithm. */
-#define BATTERY_MAX17055_RSENSE             5     /* m-ohm */
+#define BATTERY_MAX17055_RSENSE             10    /* m-ohm */
 #define BATTERY_DESIRED_CHARGING_CURRENT    2000  /* mA */
 
 #define PD_OPERATING_POWER_MW 15000
