b473214de3a202a5eed2e9890d250dbb74a57921
Ayushee <ayushee.shah@intel.com>
2019-02-25 12:56:40 -0800

    usb_pd: Adding USBC cable attribute detection
    When a discover identity command is sent with a SOP prime
    packet, the cable plug of an emark cable responds with the
    cable attribute.
    
    Source runs the cable detection prior to sending the source
    capability messages and the sink selects the cable attributes
    offered by the source. Reference-USB_PD_R3_0 Section 4.4
    
    Added a variable cable_type that stores the cable attribute on
    connection and resetting it when the cable is disconnected.
    Also added a console command that gives the cable type of the
    connected cable.
    
    BUG=None
    BRANCH=None
    TEST=Verified on Dragonegg, able to get correct USBC cable attribute
    
    Change-Id: Id51e9fcb7299b00f9304932a2e7123992f506050
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -4772,6 +4772,7 @@ struct ec_response_usb_pd_control_v2 {
 	char state[32];
 	uint8_t cc_state; /* USBC_PD_CC_*Encoded cc state */
 	uint8_t dp_mode;  /* Current DP pin mode (MODE_DP_PIN_[A-E]) */
+	uint8_t cable_type; /* Type of cable attached */
 } __ec_align1;
 
 #define EC_CMD_USB_PD_PORTS 0x0102
