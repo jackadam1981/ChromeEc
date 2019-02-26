3892eb9f5e1ff961b6281ebaf4ad1cff402f8efb
Rong Chang <rongchang@chromium.org>
2019-02-27 00:46:40 +0800

    kukui: scp: move clock and oscillator configuration code to clock.c
    To implement ULPOSC calibration, clock and oscillator APIs are moved
    from system to its own module.
    
    BRANCH=none
    BUG=b:120176040
    TEST=manual
      make BOARD=kukui_scp -j
    
    Change-Id: I143b331f5d36cd0d0964920a9b7e7fe00afb6e3b
    Signed-off-by: Rong Chang <rongchang@chromium.org>

---
+++ chip/mt_scp/clock_chip.h
@@ -0,0 +1,16 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Clocks, PLL and power settings */
+
+#ifndef __CROS_EC_CLOCK_CHIP_H
+#define __CROS_EC_CLOCK_CHIP_H
+
+#include "registers.h"
+
+void scp_enable_clock(void);
+void scp_select_cpu_clock(enum scp_clk_sel sel);
+
+#endif /* __CROS_EC_CLOCK_CHIP_H */
