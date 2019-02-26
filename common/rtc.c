a0e2af6ee0585b323ba46625088a9d606fcbf8d5
Tim Wawrzynczak <twawrzynczak@chromium.org>
2019-02-26 12:29:34 -0700

    octopus: Print RTC timestamps at power events
    Add new host command to synchronize MTC and host RTC.
    On chipset events (shutdown, suspend, resume, startup), print the
    host synced RTC (plus current time delta) to make it easier to
    align logs between EC and AP.  Also uses a system jump tag in
    order to keep the synchronization across sysjump.
    
    BRANCH=none
    BUG=b:111675966
    TEST=make buildall -j, tested with hostcmd on phaser
    
    Change-Id: Id207b85e1c159136df71c7d7c06009658f5d0fa5
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- common/rtc.c
+++ common/rtc.c
@@ -7,9 +7,16 @@
 /* TODO(chromium:733844): Move this conversion to kernel rtc-cros-ec driver */
 
 #include "rtc.h"
+#include "console.h"
 
 static uint16_t days_since_year_start[12] = {
-0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+	0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+#define SEC_PER_HOUR 3600
+#define SEC_PER_MINUTE 60
+#define SEC_PER_DAY 86400
+#define MIN_PER_HOUR 60
+#define SEC_PER_MIN 60
 
 /* Conversion between calendar date and seconds eclapsed since 1970-01-01 */
 uint32_t date_to_sec(struct calendar_date time)
@@ -27,6 +34,11 @@ uint32_t date_to_sec(struct calendar_date time)
 		(IS_LEAP_YEAR(time.year) && time.month > 2) +
 		(time.day - 1)) * SECS_PER_DAY;
 
+	/* Add current time of day as well */
+	sec += time.hour * SEC_PER_HOUR;
+	sec += time.minute * SEC_PER_MINUTE;
+	sec += time.second;
+
 	/* add the accumulated time in seconds from 1970 to 2000 */
 	return sec + SECS_TILL_YEAR_2K;
 }
@@ -36,6 +48,7 @@ struct calendar_date sec_to_date(uint32_t sec)
 	struct calendar_date time;
 	int day_tmp; /* for intermediate calculation */
 	int i;
+	int hms;
 
 	/* RTC time must be after year 2000. */
 	sec = (sec > SECS_TILL_YEAR_2K) ? (sec - SECS_TILL_YEAR_2K) : 0;
@@ -54,14 +67,21 @@ struct calendar_date sec_to_date(uint32_t sec)
 	}
 	for (i = 1; i < 12; i++) {
 		if (days_since_year_start[i] +
-		    (IS_LEAP_YEAR(time.year) && (i >= 2)) >= day_tmp)
+			(IS_LEAP_YEAR(time.year) && (i >= 2)) >= day_tmp)
 			break;
 	}
 	time.month = i;
 
 	day_tmp -= days_since_year_start[time.month - 1] +
-		   (IS_LEAP_YEAR(time.year) && (time.month > 2));
+		(IS_LEAP_YEAR(time.year) && (time.month > 2));
 	time.day = day_tmp;
+
+	/* The residual seconds gives the time of day */
+	hms = sec % SEC_PER_DAY;
+
+	time.hour = hms / SEC_PER_HOUR;
+	time.minute = (hms % SEC_PER_HOUR) / MIN_PER_HOUR;
+	time.second = (hms % SEC_PER_HOUR) % SEC_PER_MIN;
 
 	return time;
 }
--- common/system.c
+++ common/system.c
@@ -36,6 +36,7 @@
 #include "util.h"
 #include "version.h"
 #include "watchdog.h"
+#include "rtc.h"
 
 /* Console output macros */
 #define CPUTS(outstr) cputs(CC_SYSTEM, outstr)
@@ -1632,3 +1633,112 @@ int board_write_serial(const char *serialno)
 }
 #endif
 #endif  /* CONFIG_SERIALNO_LEN */
+
+#if defined(CONFIG_HOSTCMD_RTC_SYNC)
+/*
+ * Local timestamp indicating when the host
+ * synchronized RTC time with the EC
+ */
+struct epoch_sync {
+	timestamp_t local_timestamp;
+	uint32_t host_rtc_sec;
+};
+static struct epoch_sync local_epoch;
+
+#define LOCAL_EPOCH_SYSJUMP_TAG 0x8284 /* RT */
+#define LOCAL_EPOCH_HOOK_VERSION 1
+
+/**
+ * Preserve synchronized RTC across a sysjump.
+ */
+static void synced_rtc_preserve_state(void)
+{
+	system_add_jump_tag(LOCAL_EPOCH_SYSJUMP_TAG, LOCAL_EPOCH_HOOK_VERSION,
+			sizeof(local_epoch), &local_epoch);
+}
+DECLARE_HOOK(HOOK_SYSJUMP, synced_rtc_preserve_state, HOOK_PRIO_DEFAULT);
+
+/**
+ * Restore synchronized RTC after a sysjump.
+ */
+static void synced_rtc_restore_state(void)
+{
+	const struct epoch_sync *prev_epoch;
+	int size, version;
+
+	prev_epoch = (const struct epoch_sync *)system_get_jump_tag(
+		LOCAL_EPOCH_SYSJUMP_TAG, &version, &size);
+
+	if (prev_epoch && version == LOCAL_EPOCH_HOOK_VERSION &&
+		size == sizeof(struct epoch_sync)) {
+		memcpy(&local_epoch, prev_epoch, sizeof(local_epoch));
+	}
+}
+DECLARE_HOOK(HOOK_INIT, synced_rtc_restore_state, HOOK_PRIO_DEFAULT);
+
+/* Synchronize EC's MTC and host RTC */
+void set_rtc_epoch(timestamp_t local_time, uint32_t host_rtc_sec)
+{
+	local_epoch.local_timestamp = local_time;
+	local_epoch.host_rtc_sec = host_rtc_sec;
+}
+
+/* Get the current date/time, as synchronized from the host */
+struct calendar_date get_host_synced_rtc(void)
+{
+	timestamp_t current_ts = get_time();
+
+	/*
+	 * Get the difference between the synced epoch time
+	 * and the current timestamp in seconds.  Add the difference
+	 * to the synced epoch seconds and convert to calendar_date
+	 */
+	const uint64_t us_diff = (current_ts.val -
+				local_epoch.local_timestamp.val);
+	const uint64_t sec_diff = us_diff / 1000000UL; /* us to sec */
+	const uint32_t current_rtc_sec = local_epoch.host_rtc_sec +
+		(uint32_t)sec_diff;
+
+	return sec_to_date(current_rtc_sec);
+}
+
+static void print_synced_rtc(void)
+{
+	struct calendar_date tm = get_host_synced_rtc();
+
+	if (tm.year != 0) {
+		/* struct calendar_date only stores years since 2000 */
+		const int real_year = tm.year + 2000;
+
+		CPRINTF("RTC: %02u-%02u-%04u %02u:%02u:%02u\n",
+			tm.month, tm.day, real_year,
+			tm.hour, tm.minute, tm.second);
+	}
+}
+
+
+/* Synchronize current time from the host */
+static int host_command_sync_rtc(struct host_cmd_handler_args *args)
+{
+	/* Get the EC timestamp ASAP */
+	timestamp_t tm = get_time();
+	const struct ec_params_rtc_sync *r = args->params;
+	const uint32_t host_rtc = r->host_rtc_sec;
+
+	/* Synchronize EC time with RTC time */
+	set_rtc_epoch(tm, host_rtc);
+	print_synced_rtc();
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_SYNC_RTC,
+		     host_command_sync_rtc,
+		     EC_VER_MASK(0));
+
+#if defined(HAS_TASK_CHIPSET)
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, print_synced_rtc, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, print_synced_rtc, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, print_synced_rtc, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, print_synced_rtc, HOOK_PRIO_DEFAULT);
+#endif /* defined(HAS_CHIPSET_TASK) */
+#endif /* defined(CONFIG_HOSTCMD_RTC_SYNC) */
