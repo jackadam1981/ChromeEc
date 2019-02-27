9171c808139e57a7bc117820050713dec9618256
Moritz Fischer <moritz.fischer@ettus.com>
2019-02-27 11:34:02 -0800

    chip: stm32: Fix Timers 9 through 11 for STM32F4
    The timers 9 through 11 have different enable bits in the
    STM32_RCC_APB2ENR on STM32F446/411/412 targets versus the
    default (used by STM32F4/L4/STM32L) value set.
    
    Break out the CHIP_FAMILY_STM32F4 case separately.
    
    BRANCH=none
    BUG=none
    TEST=Observe PWM output on STM32412 EVM vs none before
    
    Signed-off-by: Moritz Fischer <moritz.fischer@ettus.com>
    Change-Id: I9f00902afe58ef8ef141da39b2b912ecc592944b

--- chip/stm32/registers.h
+++ chip/stm32/registers.h
@@ -1393,9 +1393,16 @@ typedef volatile struct timer_ctlr timer_ctlr_t;
 #define STM32_RCC_CSR_LSIRDY		(1 << 1)
 
 #define STM32_RCC_HB_DMA1		(1 << 24)
+#if defined(CHIP_FAMILY_STM32F4)
+#define STM32_RCC_PB2_TIM9		(1 << 16)
+#define STM32_RCC_PB2_TIM10		(1 << 17)
+#define STM32_RCC_PB2_TIM11		(1 << 18)
+#else /* !defined(CHIP_FAMILY_STM32F4) */
 #define STM32_RCC_PB2_TIM9		(1 << 2)
 #define STM32_RCC_PB2_TIM10		(1 << 3)
 #define STM32_RCC_PB2_TIM11		(1 << 4)
+#endif
+
 #define STM32_RCC_PB1_USB		(1 << 23)
 
 #define STM32_RCC_DCKCFGR2              REG32(STM32_RCC_BASE + 0x94)
