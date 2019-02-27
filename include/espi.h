2bda795d60a7fb0e83799affe6b6b892e82bfbe1
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2019-02-27 11:39:58 +0800

    (COPY) it83xx/espi.c: eSPI OOB driver
    1.eSPI oob driver
    2.task operate
      Once upstream go, the task which calls espi_oob_send should sleep
      waiting for oob send done event. Then the task will be waked up by
      interrupt (mutex static global current task var in espi_oob_send
      so that critical section modify once a task).
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I442235cb87957a6879746b22ff43420399352704
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- include/espi.h
+++ include/espi.h
@@ -91,4 +91,67 @@ const char *espi_vw_get_wire_name(enum espi_vw_signal signal);
 int espi_signal_is_vw(int signal);
 
 
+#ifdef CONFIG_HOSTCMD_ESPI_OOB
+/* OOB channel SMBus command */
+enum espi_oob_smbus_command {
+	OOB_SMBUS_GET_TEMP = 1,
+	OOB_SMBUS_GET_RTC,
+};
+
+/* OOB channel receive length */
+#define OOB_SMBUS_REC_TEMP_LENGTH	0x05
+#define OOB_SMBUS_REC_RTC_LENTGH	0x0C
+
+/* OOB channel SMBus receive data byte count */
+#define OOB_SMBUS_REC_TEMP_BYTE_COUNT	0x02
+#define OOB_SMBUS_REC_RTC_BYTE_COUNT	0x09
+
+/* eSPI cycle type field */
+#define ESPI_FLASH_READ_CYCLE_TYPE	0x00
+#define ESPI_FLASH_WRITE_CYCLE_TYPE	0x01
+#define ESPI_FLASH_ERASE_CYCLE_TYPE	0x02
+#define ESPI_OOB_CYCLE_TYPE		0x21
+
+/* eSPI tag + len[11:8] field */
+#define ESPI_TAG_LEN_FIELD(tag, len) \
+		   ((((tag) & 0xF) << 4) | (((len) >> 8) & 0xF))
+
+/* OOB channel SMBus address field */
+
+/* PCH/SoC eSPI-MC (hardware) Request Handler Address */
+#define OOB_SMBUS_DEST_ADDR_PCH_SOC_MC		(0x01 << 1)
+
+/* PCH/SoC Intel ME FW SMBus Request Handler Address */
+#define OOB_SMBUS_DEST_ADDR_PCH_SOC_ME		(0x10 << 1)
+
+/* Intel PCH PECI SMBus MCTP Request Handler Address */
+#define OOB_SMBUS_DEST_ADDR_PCH_PECI_MCTP	(0x20 << 1)
+
+/* eSPI slave address to master */
+#define OOB_SMBUS_SRC_ADDR_EC	0x07
+
+/**
+ * eSPI OOB channel data buffer size maximum 80 bytes.
+ * Actually length field can reach 4096 bytes defined in intel espi spec.
+ */
+#define ESPI_OOB_MAX_LENGTH	80
+
+/* Minimum eSPI OOB channel data receive length */
+#define ESPI_OOB_MIN_LENGTH	5
+
+/**
+ * Receive messages from eSPI OOB channel
+ * @param oob_data point to the oob data array buffer
+ * @return EC_SUCCESS, or non-zero if error
+ */
+int espi_oob_receive(uint8_t *oob_data);
+
+/**
+ * Send messages via eSPI OOB channel
+ * @param oob_data point to the oob data array buffer
+ * @return EC_SUCCESS, or non-zero if error
+ */
+int espi_oob_send(uint8_t *oob_data);
+#endif
+
 #endif  /* __CROS_EC_ESPI_H */
