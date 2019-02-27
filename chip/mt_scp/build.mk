b1114913d3f56f7cb20850bc97044858a3cac1ac
Tzung-Bi Shih <tzungbi@chromium.org>
2019-02-27 13:11:21 +0800

    WIP: chip/mt_scp: support audio codec WoV
    BRANCH=none
    BUG=b:123268236
    TEST=make BOARD=kukui_scp \
         CONFIG_AUDIO_CODEC_DMIC=y CONFIG_AUDIO_CODEC_WOV=y -j
    
    Change-Id: I51e22947c3f535c9a504033c502ec2ff1c688e6a
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- chip/mt_scp/build.mk
+++ chip/mt_scp/build.mk
@@ -13,6 +13,7 @@ CFLAGS_CPU+=-march=armv7e-m -mcpu=cortex-m4
 chip-y=clock.o gpio.o memmap.o system.o uart.o
 
 # Optional chip modules
+chip-$(CONFIG_AUDIO_CODEC_WOV)+=audio_codec_wov.o
 chip-$(CONFIG_COMMON_TIMER)+=hrtimer.o
 chip-$(CONFIG_I2C)+=i2c.o
 chip-$(CONFIG_IPI)+=ipi.o ipi_table.o
