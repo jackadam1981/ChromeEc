0f7025d5e2b102316dd805d7fa4378146ea83741
Tzung-Bi Shih <tzungbi@chromium.org>
2019-02-27 13:04:24 +0800

    WIP: ec: add WoV abstract layer
    BRANCH=None
    BUG=b:123268236
    TEST=define CONFIG_EC_CODEC_WOV temporary and `make BOARD=kukui_scp -j`
    Change-Id: I26f7a8dbf9a6d57b1845fbb0666aa1d8285d9013
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- common/build.mk
+++ common/build.mk
@@ -148,6 +148,7 @@ common-$(HAS_TASK_KEYSCAN)+=keyboard_scan.o
 common-$(HAS_TASK_LIGHTBAR)+=lb_common.o lightbar.o
 common-$(HAS_TASK_MOTIONSENSE)+=motion_sense.o
 common-$(HAS_TASK_TPM)+=tpm_registers.o
+common-$(CONFIG_EC_CODEC_WOV)+=wov.o
 
 ifneq ($(CONFIG_COMMON_RUNTIME),)
 common-$(CONFIG_MALLOC)+=shmalloc.o
