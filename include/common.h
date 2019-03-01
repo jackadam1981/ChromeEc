61e90a6eb7c1c717c75ffb151d87a835515e04ee
Vadim Bendebury <vbendeb@chromium.org>
2019-02-28 20:11:28 -0800

    cr50: complete support of the new NVMEM structure
    This patch eliminates unnecessary legacy nvmem.c and nvmem_vars.c code
    and brings the code base to the state where the new NVMEM layout is
    fully functional.
    
    BRANCH=cr50, cr50-mp
    BUG=b:69907320
    TEST=the following tests pass:
       - test cases in ./test/nvmem.c
    
       - TCG suite (passes on par with the existing Cr50 code with the
         reduced code footprint TPM2 library)
    
       - Chrome OS device migrates from legacy to new implementation with
         user account maintained.
    
       - Chrome OS user account is maintained over AP and H1 reboots and
         deep sleep cycles.
    
    Change-Id: If4bc2dd125873a79dbe0e268eb32100a8b8b352d
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- include/common.h
+++ include/common.h
@@ -180,6 +180,9 @@ enum ec_error_list {
 	/* something wrong in a HW */
 	EC_ERROR_HW_INTERNAL = 25,
 
+	/* Sometimes operation is expected to have to be repeated. */
+	EC_ERROR_TRY_AGAIN = 26,
+
 	/* Verified boot errors */
 	EC_ERROR_VBOOT_SIGNATURE = 0x1000, /* 4096 */
 	EC_ERROR_VBOOT_SIG_MAGIC = 0x1001,
