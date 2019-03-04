7e124dac2c53410b695c6a4d1ec36001c1516731
Ayushee <ayushee.shah@intel.com>
2019-03-04 12:27:43 -0800

    usb_pd: Get current CC state
    CC state has the information of type of device attached and it is
    needed to configure the Intel virtual muxes hence returning the
    current CC state in USB_PD_CONTROL host command.
    
    BUG=None
    BRANCH=None
    TEST=Verified on Dragonegg, able to get correct CC state
    
    Change-Id: I917321b599a17381e2ffbe5e813e676df03abd47
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -4850,6 +4850,7 @@ static const enum typec_mux typec_mux_map[USB_PD_CTRL_MUX_COUNT] = {
 static int hc_usb_pd_control(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_usb_pd_control *p = args->params;
+	struct ec_response_usb_pd_control_v2 *r_v2 = args->response;
 	struct ec_response_usb_pd_control_v1 *r_v1 = args->response;
 	struct ec_response_usb_pd_control *r = args->response;
 
@@ -4919,13 +4920,19 @@ static int hc_usb_pd_control(struct host_cmd_handler_args *args)
 			strzcpy(r_v1->state,
 				pd_state_names[pd[p->port].task_state],
 				sizeof(r_v1->state));
-		args->response_size = sizeof(*r_v1);
+
+		if (args->version == 1)
+			args->response_size = sizeof(*r_v1);
+		else {
+			r_v2->flag = pd[p->port].cc_state;
+			args->response_size = sizeof(*r_v2);
+		}
 	}
 	return EC_RES_SUCCESS;
 }
 DECLARE_HOST_COMMAND(EC_CMD_USB_PD_CONTROL,
 		     hc_usb_pd_control,
-		     EC_VER_MASK(0) | EC_VER_MASK(1));
+		     EC_VER_MASK(0) | EC_VER_MASK(1) | EC_VER_MASK(2));
 
 static int hc_remote_flash(struct host_cmd_handler_args *args)
 {
