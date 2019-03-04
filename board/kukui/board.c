9669cdc654087df148a3021c637cb10eb05a75bd
Ting Shen <phoenixshen@google.com>
2019-03-04 16:31:30 +0800

    kukui: configure PI3USB9201 chip to USB Path ON Mode
    In Kukui P2, there's a BC1.2 detection chip between AP and POGO.
    The chip will be removed in later builds, but for P2, we need to
    configure it to a "USB Path ON" Mode to make the POGO pin work.
    
    BRANCH=None
    BUG=b:123268580
    TEST=Set BOARD_REV=2, build and flash ec.
         Verify that usb device connected to POGO pins appears in `lsusb`
         command.
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    
    Change-Id: Ic0ab50400a720ebd5a01551917157707acfb3fcc

--- board/kukui/board.c
+++ board/kukui/board.c
@@ -15,6 +15,7 @@
 #include "console.h"
 #include "driver/accelgyro_bmi160.h"
 #include "driver/battery/max17055.h"
+#include "driver/bc12/pi3usb9201.h"
 #include "driver/charger/rt946x.h"
 #include "driver/sync.h"
 #include "driver/tcpm/mt6370.h"
@@ -84,8 +85,15 @@ const struct i2c_port_t i2c_ports[] = {
 	{"tcpc0",     I2C_PORT_TCPC0,     400, GPIO_I2C1_SCL, GPIO_I2C1_SDA},
 	{"battery",   I2C_PORT_BATTERY,   400, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
 	{"accelgyro", I2C_PORT_ACCEL,     400, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
+#if BOARD_REV == 2
+	{"bc12",      I2C_PORT_BC12,      400, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
+#endif
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+#if BOARD_REV == 2
+#define BC12_I2C_ADDR PI3USB9201_I2C_ADDR_3
+#endif
 
 /* power signal list.  Must match order of enum power_signal. */
 const struct power_signal_info power_signal_list[] = {
@@ -227,6 +235,14 @@ static void board_init(void)
 
 	/* Enable gauge interrupt from max17055 */
 	gpio_enable_interrupt(GPIO_GAUGE_INT_ODL);
+
+#if BOARD_REV == 2
+	/* configure PI3USB9201 to USB Path ON Mode */
+	gpio_set_level(GPIO_EN_PP3300_POGO, 1);
+	i2c_write8(I2C_PORT_BC12, BC12_I2C_ADDR,
+		   PI3USB9201_REG_CTRL_1,
+		   PI3USB9201_USB_PATH_ON << PI3USB9201_REG_CTRL_1_MODE_SHIFT);
+#endif
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
