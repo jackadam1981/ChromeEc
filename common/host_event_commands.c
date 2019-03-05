6aa363ac27727fb1b3d0d7397f21df866d186389
Mathew King <mathewk@chromium.org>
2019-03-05 13:49:33 -0700

    arcada_ish: Enable accel interrupts and accel fifo
    This change will turn on the motionsense fifo but the fifo full events
    are not passed to the ap in any way.
    
    BUG=b:123634700
    TEST=Built and deployed to arcada device
    BRANCH=none
    
    Change-Id: I9b8110b0114003b19e8fc1c2f42d8a6a0475dc6f
    Signed-off-by: Mathew King <mathewk@chromium.org>

--- common/host_event_commands.c
+++ common/host_event_commands.c
@@ -211,6 +211,10 @@ void lpc_s3_resume_clear_masks(void)
 
 #endif
 
+#ifdef CONFIG_HOSTCMD_HECI
+void heci_send_host_events(host_event_t events);
+#endif
+
 /*
  * Maintain two copies of the events that are set.
  *
@@ -311,6 +315,8 @@ void host_set_events(host_event_t mask)
 
 #ifdef CONFIG_HOSTCMD_X86
 	lpc_set_host_event_state(events);
+#elif defined(CONFIG_HOSTCMD_HECI)
+	heci_send_host_events(events);
 #else
 	*(host_event_t *)host_get_memmap(EC_MEMMAP_HOST_EVENTS) = events;
 #ifdef CONFIG_MKBP_EVENT
@@ -325,6 +331,7 @@ void host_set_events(host_event_t mask)
 void host_set_single_event(enum host_event_code event)
 {
 	host_event_t ev = 0;
+	CPRINTS("host_set_single_event %d", event);
 
 	host_event_set_bit(&ev, event);
 	host_set_events(ev);
