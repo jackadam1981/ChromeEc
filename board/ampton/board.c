2a2e41ef372220a8cd0c0c05fbf4f4c80d740985
James_Chao <james_chao@asus.corp-partner.google.com>
2019-03-05 12:12:53 +0800

    ampton: tune the dp mux to improve external monitor compatible
    When the ampton connected to external monitor Viewsonic VX2880,
    it can't display normally. We need to tune the mux:
      1. modulate DP equalization setting : 12.8dB > 3.6dB
      2. modulate Vswing: 15%
    
    BUG=b:131113883
    BRANCH=octopus
    TEST=Check the monitor VX2880 can display normally.
    
    Change-Id: Id8d32b41b4fdde4799a54e0081b5ddadf3534fba

--- board/ampton/board.c
+++ board/ampton/board.c
@@ -66,6 +66,18 @@ int ppc_get_alert_status(int port)
 #define USB_PD_PORT_ITE_0      0
 #define USB_PD_PORT_ITE_1      1
 
+static int tune_mux(int port)
+{
+	int p = (port == 0) ? I2C_PORT_USBC0 : I2C_PORT_USBC1;
+
+	/* Auto EQ disabled, compensate for channel lost up to 3.6dB */
+	i2c_write8(p, 0x16, 0xd3, 0x98);
+	/* DP output swing adjustment +15% */
+	i2c_write8(p, 0x16, 0xd4, 0xc0);
+
+	return EC_SUCCESS;
+}
+
 struct usb_mux ampton_usb_muxes[CONFIG_USB_PD_PORT_COUNT] = {
 	[USB_PD_PORT_ITE_0] = {
 		/* Use PS8751 as mux only */
@@ -74,6 +86,7 @@ struct usb_mux ampton_usb_muxes[CONFIG_USB_PD_PORT_COUNT] = {
 		.flags = USB_MUX_FLAG_NOT_TCPC,
 		.driver = &ps8xxx_usb_mux_driver,
 		.hpd_update = &ps8xxx_tcpc_update_hpd_status,
+		.board_init = &tune_mux,
 	},
 	[USB_PD_PORT_ITE_1] = {
 		/* Use PS8751 as mux only */
@@ -82,6 +95,7 @@ struct usb_mux ampton_usb_muxes[CONFIG_USB_PD_PORT_COUNT] = {
 		.flags = USB_MUX_FLAG_NOT_TCPC,
 		.driver = &ps8xxx_usb_mux_driver,
 		.hpd_update = &ps8xxx_tcpc_update_hpd_status,
+		.board_init = &tune_mux,
 	}
 };
 
