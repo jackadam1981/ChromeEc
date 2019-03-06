cfec5abb6f25c9cd976088d79b90f5b0c9459151
YH Lin <yueherngl@google.com>
2019-03-06 08:09:03 -0800

    flapjack: base on the adc strapping to determine the battery type
    *** DO NOT SUBMIT, WORK IN PROGRESS ***
    
    BUG=None
    BRANCH=none
    TEST=TBD
    
    Change-Id: Ibdc9a96b7f15506deda6e87fec49270a7afe3441

--- board/flapjack/board.h
+++ board/flapjack/board.h
@@ -243,6 +243,16 @@ enum sensor_id {
 	VSYNC,
 };
 
+/* Battery type */
+enum battery_type {
+	BATTERY_ATL_8 = 0,
+	BATTERY_SUNWODA_8,
+	BATTERY_ATL_10,
+	BATTERY_SUNWODA_10,
+	BATTERY_UNKNOWN,
+	BATTERY_COUNT = BATTERY_UNKNOWN,
+};
+
 #include "gpio_signal.h"
 #include "registers.h"
 
@@ -253,6 +263,7 @@ void emmc_cmd_interrupt(enum gpio_signal signal);
 
 void board_reset_pd_mcu(void);
 int board_get_version(void);
+int board_get_battery_type(void);
 
 #endif /* !__ASSEMBLER__ */
 
