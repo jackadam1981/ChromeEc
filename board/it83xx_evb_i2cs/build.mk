2d0da571907db5fe0a8cd6eafd34a0ac44d2c483
tim <tim2.lin@ite.corp-partner.google.com>
2019-03-06 10:12:53 +0800

    (TEST) it83xx/i2c_master: i2c slave function test
    BUG=none
    BRANCH=none
    TEST=Entering the command as follows. The slave address
    is 0xa4 or 0xa8.
    
    example:
    i2cxfer w256 2 0xa4 0 ${byte_count}
    i2cxfer r256 2 0xa4 0 ${byte_count}
    
    Change-Id: I369ad805ed23ced0deb4cfd3218d1e808dae8a34
    Signed-off-by: tim <tim2.lin@ite.corp-partner.google.com>

---
+++ board/it83xx_evb_i2cs/build.mk
@@ -0,0 +1,13 @@
+# -*- makefile -*-
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+
+# the IC is ITE IT8390/IT8320
+CHIP:=it83xx
+CHIP_FAMILY:=it8320
+CHIP_VARIANT:=it8320dx
+
+board-y=board.o
