88b5dd240fd9b81df65982094b643e54f2f525de
Diana Z <dzigterman@chromium.org>
2019-03-07 11:00:17 -0700

    USB PD: Save Vconn state in BBRAM
    Currently, unlocked systems can maintain a contract over a reset or
    sysjump by referencing BBRAM flags for the data and power state.  An
    additional flag is needed to track Vconn state though, particularly in
    scenarios with hubs which prefer to swap Vconn when they source power.
    Otherwise, both port partners have a different perspective of which one
    is sourcing Vconn and this can lead to hard resets when the hub attempts
    another Vconn swap.
    
    BUG=None
    BRANCH=None
    TEST=Ran EC resets on unlocked octopus board with a hub which prefers to
    not source Vconn, ensured that there were no strange Vconn_swap
    sequences after reset
    
    Change-Id: I4ccae72bef1a2df8cda4b32fb6e0a15c0cf176b1
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1536078
    Reviewed-by: Jett Rink <jettrink@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -1218,6 +1218,20 @@ static void pd_set_data_role(int port, int role)
 	pd_update_roles(port);
 }
 
+#ifdef CONFIG_USBC_VCONN
+static void pd_set_vconn_role(int port, int role)
+{
+	if (role == PD_ROLE_VCONN_ON)
+		pd[port].flags |= PD_FLAGS_VCONN_ON;
+	else
+		pd[port].flags &= ~PD_FLAGS_VCONN_ON;
+
+#ifdef CONFIG_USB_PD_DUAL_ROLE
+	pd_update_saved_port_flags(port, PD_BBRMFLG_VCONN_ROLE, role);
+#endif
+}
+#endif /* CONFIG_USBC_VCONN */
+
 void pd_execute_hard_reset(int port)
 {
 	if (pd[port].last_state == PD_STATE_HARD_RESET_SEND)
@@ -1269,7 +1283,7 @@ void pd_execute_hard_reset(int port)
 		 */
 		if (pd[port].flags & PD_FLAGS_VCONN_ON) {
 			set_vconn(port, 0);
-			pd[port].flags &= ~PD_FLAGS_VCONN_ON;
+			pd_set_vconn_role(port, PD_ROLE_VCONN_OFF);
 		}
 #endif
 
@@ -2687,6 +2701,11 @@ void pd_task(void *u)
 			pd_set_data_role(port,
 					 (saved_flgs & PD_BBRMFLG_DATA_ROLE) ?
 					 PD_ROLE_DFP : PD_ROLE_UFP);
+#ifdef CONFIG_USBC_VCONN
+			pd_set_vconn_role(port,
+					  (saved_flgs & PD_BBRMFLG_VCONN_ROLE) ?
+					  PD_ROLE_VCONN_ON : PD_ROLE_VCONN_OFF);
+#endif /* CONFIG_USBC_VCONN */
 
 			/*
 			 * Since there is an explicit contract in place, let's
@@ -3059,7 +3078,8 @@ void pd_task(void *u)
 					 * Spec 1.3 tVconnON.
 					 */
 					set_vconn(port, 1);
-					pd[port].flags |= PD_FLAGS_VCONN_ON;
+					pd_set_vconn_role(port,
+							  PD_ROLE_VCONN_ON);
 				}
 #endif
 
@@ -3069,7 +3089,8 @@ void pd_task(void *u)
 #ifdef CONFIG_USBC_VCONN
 					/* Stop sourcing Vconn if Vbus failed */
 					set_vconn(port, 0);
-					pd[port].flags &= ~PD_FLAGS_VCONN_ON;
+					pd_set_vconn_role(port,
+							  PD_ROLE_VCONN_OFF);
 #endif /* CONFIG_USBC_VCONN */
 #ifdef CONFIG_USBC_SS_MUX
 					usb_mux_set(port, TYPEC_MUX_NONE,
@@ -3114,7 +3135,7 @@ void pd_task(void *u)
 			 * it was 0 due to a previous swap
 			 */
 			set_vconn(port, 1);
-			pd[port].flags |= PD_FLAGS_VCONN_ON;
+			pd_set_vconn_role(port, PD_ROLE_VCONN_ON);
 #endif
 
 			/* Enable VBUS */
@@ -3925,7 +3946,8 @@ void pd_task(void *u)
 			if (pd[port].last_state != pd[port].task_state) {
 				if (!(pd[port].flags & PD_FLAGS_VCONN_ON)) {
 					/* VCONN is now on, send PS_RDY */
-					pd[port].flags |= PD_FLAGS_VCONN_ON;
+					pd_set_vconn_role(port,
+							  PD_ROLE_VCONN_ON);
 					res = send_control(port,
 							   PD_CTRL_PS_RDY);
 					if (res == -1) {
@@ -3943,7 +3965,8 @@ void pd_task(void *u)
 				} else {
 					/* Turn VCONN off and wait for it */
 					set_vconn(port, 0);
-					pd[port].flags &= ~PD_FLAGS_VCONN_ON;
+					pd_set_vconn_role(port,
+							  PD_ROLE_VCONN_OFF);
 					set_state_timeout(port,
 					  get_time().val + PD_VCONN_SWAP_DELAY,
 					  READY_RETURN_STATE(port));
