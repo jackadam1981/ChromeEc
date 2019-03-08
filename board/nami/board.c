5fbd3879a4c8e36402a04005ca81c6f2c2ea853d
Daisuke Nojiri <dnojiri@chromium.org>
2019-03-08 10:47:32 -0800

    Nami: Make Vayne cut off battery on G3 timer expiration
    Currently, Vayne cut off battery when soc < 3%. This patch sets the
    threshold to the same value as Powerd's (= 4%).
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b/123727148
    BRANCH=none
    TEST=Verify Vayne cut off battery on G3 idle expiration while other
    Nami's hibernate.
    
    Change-Id: I89dd63579cae0a2f88c266c2ea913580d24eeaa4

--- board/nami/board.c
+++ board/nami/board.c
@@ -1082,3 +1082,20 @@ uint8_t board_set_battery_level_shutdown(void)
 	else
 		return BATTERY_LEVEL_SHUTDOWN;
 }
+
+enum critical_shutdown board_system_is_idle(uint64_t last_shutdown_time,
+					    uint32_t delay, uint64_t now)
+{
+	uint64_t target = last_shutdown_time + delay * SECOND;
+
+	if (oem == PROJECT_VAYNE)
+		/*
+		 * Don't do anything. Battery eventually will reach critical
+		 * charge (3%). Then, board_critical_shutdown_check will be
+		 * called, which requests cutoff.
+		 */
+		return CRITICAL_SHUTDOWN_IGNORE;
+
+	return now > target ?
+			CRITICAL_SHUTDOWN_HIBERNATE : CRITICAL_SHUTDOWN_IGNORE;
+}
