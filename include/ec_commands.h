ab8e528a9b00477af692fbaf1dcd94dc1ad4c639
Ayushee <ayushee.shah@intel.com>
2019-03-07 16:38:41 -0800

    usb_pd: Get current DP pin mode
    DP pin mode is needed to configure the Intel virtual muxes hence,
    added a board level function that returns the current DP pin mode.
    Also added a variable to return the DP pin mode in USB_PD_CONTROL
    host command.
    
    BUG=None
    BRANCH=None
    TEST=Verified on Dragonegg, able to get correct pin mode
    
    Change-Id: I997de80d0963e8ac45e97e5da0064694d5572942
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -4771,6 +4771,7 @@ struct ec_response_usb_pd_control_v2 {
 	uint8_t polarity;
 	char state[32];
 	uint8_t cc_state; /* USBC_PD_CC_*Encoded cc state */
+	uint8_t dp_mode;  /* Current DP pin mode (MODE_DP_PIN_[A-E]) */
 } __ec_align1;
 
 #define EC_CMD_USB_PD_PORTS 0x0102
