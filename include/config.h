5ac7b1cefe23363c7e2bfd62e073d31c49e934f0
Hu, Hebo <hebo.hu@intel.com>
2019-03-08 15:34:21 +0800

    ish/ish5: implement AON low power mode (D0i1-3)
    PM framework completed, including:
    1: AON task skeleton
    2: task switching between main FW and AON task via x86 HW task switching method
    3: 'idlestats' console command for D0ix statistic information
    4: D0ix entrance in idle task
    5: D0i1(TCG) and D0i2(TCG + SRAM retention) enabled
    
    TODO:
    1: final D0i3 state enabling
    2: handle D3 and Sx (reset prep) flow
    
    BUG=b:122364080
    BRANCH=none
    TEST=tested on arcada
    
    Change-Id: Iefa9e067892d5c42d9f0c795275fe88e5a36115b
    Signed-off-by: Hu, Hebo <hebo.hu@intel.com>

--- include/config.h
+++ include/config.h
@@ -4346,4 +4346,20 @@
 #error "CONFIG_DPTF_MULTI_PROFILE can be set only when CONFIG_DPTF is set."
 #endif /* CONFIG_DPTF_MULTI_PROFILE && !CONFIG_DPTF */
 
+/*****************************************************************************/
+/* ISH power management related definitions */
+#if defined(CONFIG_ISH_PM_D0I2) || \
+	defined(CONFIG_ISH_PM_D0I3) || \
+	defined(CONFIG_ISH_PM_D3) || \
+	defined(CONFIG_ISH_PM_RESET_PREP)
+
+#ifndef CONFIG_LOW_POWER_IDLE
+#error "Must define CONFIG_LOW_POWER_IDLE if enable ISH low power states"
+#endif
+
+#define CONFIG_ISH_PM_AONTASK
+
+#endif
+
+
 #endif  /* __CROS_EC_CONFIG_H */
