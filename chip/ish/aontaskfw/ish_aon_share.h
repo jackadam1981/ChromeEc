afff1cd9d7e59096592593268051a9ef897886cf
Hu, Hebo <hebo.hu@intel.com>
2019-03-08 15:34:21 +0800

    ish/ish5: implement AON low power management framework
    AON PM framework including:
    1: AON task skeleton
    2: task switching between main FW and AON task
    3: 'idlestats' console command for D0ix statistic information
    4: D0ix entrance in idle task
    
    BUG=b:122364080
    BRANCH=none
    TEST=tested on arcada
    
    Change-Id: Iefa9e067892d5c42d9f0c795275fe88e5a36115b
    Signed-off-by: Hu, Hebo <hebo.hu@intel.com>

---
+++ chip/ish/aontaskfw/ish_aon_share.h
@@ -0,0 +1,25 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_ISH_AON_SHARE_H
+#define __CROS_EC_ISH_AON_SHARE_H
+
+#include "ia_structs.h"
+
+/* shared data structure between main FW and aontask */
+struct ish_aon_share {
+	/* aontask's TSS segment entry */
+	struct tss_entry *aon_tss;
+	/* aontask's LDT start address */
+	uint32_t ldt_ptr;
+	/* aontask's LDT's limit size */
+	uint32_t ldt_size;
+	/* current power state, see chip/ish/power_mgt.h */
+	int pm_state;
+	/* for store/restore main FW's IDT */
+	struct idt_header main_fw_idt_hdr;
+} __packed;
+
+#endif /* __CROS_EC_ISH_AON_SHARE_H */
