b8d47bac693264cda6018325f5fd892cea94faee
Daisuke Nojiri <dnojiri@chromium.org>
2019-03-11 11:44:08 -0700

    Kefka: Disable hibernate on G3 idle, cutoff battery on critical charge
    Kefka currently hibernates when it's idel in G3 for 60 min. This behavior
    makes Kefka drain a battery deeply over a long idle (3+ months). This
    patch disable hibernation in G3.
    
    Kefka currently hibernates when the battery reaches critical level (2%).
    This patch makes it equal to the threshold used by Powerd (4%) and makes
    Kefka cutoff a battery instead of hibernate.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b:
    BRANCH=strago
    TEST=Make Kefka drain battery and verify it cuts off a battery when the
    soc reaches 4%. Verify Kefka doesn't hibernate after G3 idle timer
    exipres.
    
    Change-Id: Ib8cc770c1de17a1a47abf71b0ab45e732e07b267

--- board/kefka/board.c
+++ board/kefka/board.c
@@ -280,3 +280,26 @@ static void get_motion_sensors_count(void)
 		motion_sensor_count = 0;
 }
 DECLARE_HOOK(HOOK_INIT, get_motion_sensors_count, HOOK_PRIO_FIRST);
+
+
+enum critical_shutdown board_critical_shutdown_check(
+		struct charge_state_data *curr)
+{
+		return CRITICAL_SHUTDOWN_CUTOFF;
+}
+uint8_t board_set_battery_level_shutdown(void)
+{
+	/* We match the shutdown threshold with Powerd's.
+	 * 4 + 1 = 5% because Powerd uses '<=' while EC uses '<'. */
+	return 5;
+}
+enum critical_shutdown board_system_is_idle(uint64_t last_shutdown_time,
+					    uint64_t *target, uint64_t now)
+{
+	/*
+	 * Don't do anything. Battery eventually will reach critical
+	 * charge (3%). Then, board_critical_shutdown_check will be
+	 * called, which requests cutoff.
+	 */
+	return CRITICAL_SHUTDOWN_IGNORE;
+}
