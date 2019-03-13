3dff8ed76076eb382d4e6d15681e2c4ab910f588
Daisuke Nojiri <dnojiri@chromium.org>
2019-03-13 09:28:31 -0700

    Flapjack: Cut off battery when USB-C board isn't connected
    Currently, when a battery is connected, the device boots immediately.
    This makes assembly process unreliable.
    
    This patch makes EC cut off a battery at start-up if USBC_THERM reads
    about the pull-up voltage because that indicates a USB-C board isn't
    connected, which means the devices is being assembled.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b/124540674
    BRANCH=none
    TEST=Verify a battery is cut off when a USB-C board isn't connected.
    Verify a battery isn't cut off when a USB-C board is connected.
    
    Change-Id: Iadc756f8705abe083a5d56b5d8ad080dbff15b6a

--- board/flapjack/board.c
+++ board/flapjack/board.c
@@ -244,8 +244,30 @@ int pd_snk_is_vbus_provided(int port)
 	return rt946x_is_vbus_ready();
 }
 
+static int read_adc(enum adc_channel ch)
+{
+	int mv = adc_read_channel(ch);
+	if (mv == ADC_READ_ERROR)
+		mv = adc_read_channel(ch);
+	return mv;
+}
+
+/* Threshold to detect USB-C board. If the USB-C board isn't connected,
+ * USBC_THERM is floating thus the ADC pin should read the pull-up voltage. */
+#define USBC_THERM_THRESHOLD 1700
+
 static void board_init(void)
 {
+#ifdef SECTION_IS_RO
+	/* If USB-C board isn't connected, the device is being assembled.
+	 * We cut off the battery until the assembly is done for safety. */
+	int mv = read_adc(ADC_USBC_THERM);
+	CPRINTS("USBC_THERM=%d", mv);
+	if (mv > USBC_THERM_THRESHOLD) {
+		cflush();
+		board_cut_off_battery();
+	}
+#endif
 	/* Set SPI1 PB13/14/15 pins to high speed */
 	STM32_GPIO_OSPEEDR(GPIO_B) |= 0xfc000000;
 
@@ -270,7 +292,7 @@ static void board_init(void)
 	gpio_enable_interrupt(GPIO_GAUGE_INT_ODL);
 	board_setup_panel();
 }
-DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_INIT_ADC + 1);
 
 void board_config_pre_init(void)
 {
@@ -343,11 +365,7 @@ int board_get_version(void)
 	gpio_set_level(GPIO_EC_BOARD_ID_EN_L, 0);
 	/* Wait to allow cap charge */
 	msleep(10);
-	mv = adc_read_channel(ADC_BOARD_ID);
-
-	if (mv == ADC_READ_ERROR)
-		mv = adc_read_channel(ADC_BOARD_ID);
-
+	mv = read_adc(ADC_BOARD_ID);
 	gpio_set_level(GPIO_EC_BOARD_ID_EN_L, 1);
 
 	for (i = 0; i < BOARD_VERSION_COUNT; ++i) {
