ebc48ef88e03bf7d61cc37edb5cb667be97f2bc0
Vadim Bendebury <vbendeb@chromium.org>
2019-03-14 10:14:20 -0700

    common: add flash event log facility
    This patch edds implementation and test for a generic logger savinf
    log entries in the flash.
    
    The entries payload are limited to 64 bytes in size, each entry starts
    with a header, which includes
     - 8 bit type type to allow to interpret the payload
     - 6 bit size field (two top bits of the byte are left for user flags,
       not yet used)
     - 32 bit timestamp to allow to identify newer log entries (presently
       this is just a monitonously increasing number)
     - 8 bit crc field protecting the entire entry
    
    The entries are padded to make sure that they are ending on the flash
    write boundary.
    
    The location of the log is defined by the platform using it. There is
    a provision for allowing the platform to register a callback which is
    needed to be called to allow write access to the log (as is the case
    on H1).
    
    While the device is running, the log is growing until the allotted
    flash space is 90% full. If there is an attempt save another entry
    after that the log is compacted, namely the last 25% worth of flash
    space is preserved, the log space is erased and the saved contents
    written back.
    
    On restarts the log is compacted if its size exceeds 75% of the
    alloteed flash space.
    
    An API is provided to add entries to the log and to retrieve an entry
    newer than a certain timestamp value. Thus starting with timestamp
    zero will result in reading the very first log entry. To read the next
    entry, the read function needs to be called with the timestamp value
    of the current entry. This allows to browse the entire log, one entry
    at a time.
    
    A CLI command compiled in when CONFIG_CMD_FLASH_LOG is defined, allows
    to add log and retrieve log entries.
    
    BUG=b:63760920
    BRANCH=cr50, cr50-mp
    TEST=the included test case can be invoked by
    
         make run-flash_log
    
       and it passes. More tests are done when the rest of the patch stack
       is added.
    
    Change-Id: I3dcdf2704a1e08fd3101183e434ac4a4e4cf1b9a
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1020,6 +1020,7 @@
 #define CONFIG_CMD_FASTCHARGE
 #undef  CONFIG_CMD_FLASH
 #define CONFIG_CMD_FLASHINFO
+#undef  CONFIG_CMD_FLASH_LOG
 #undef  CONFIG_CMD_FLASH_TRISTATE
 #undef  CONFIG_CMD_FORCETIME
 #define CONFIG_CMD_GETTIME
@@ -1452,6 +1453,10 @@
 /* This enables chip-specific access functions */
 #define CONFIG_FLASH_PHYSICAL
 #undef CONFIG_FLASH_BANK_SIZE
+/* Provide event log stored in flash memory. */
+#undef CONFIG_FLASH_LOG
+#undef CONFIG_FLASH_LOG_BASE
+#undef CONFIG_FLASH_LOG_SPACE
 #undef CONFIG_FLASH_ERASED_VALUE32
 #undef CONFIG_FLASH_ERASE_SIZE
 #undef CONFIG_FLASH_ROW_SIZE
