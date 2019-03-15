a03d7360f252265405dbebbe52f641b6510c8f48
Kashyap Nanavati <kashyap.nanavati@intel.com>
2019-03-14 23:26:22 -0700

    hatch : Add board_hibernate_late
    Estimated battery life in Deep Sleep State on
    hatch is very less (~4 days). This CL implements
    board_hibernate_late function to fix this.
    
    BUG=none
    BRANCH=none
    TEST=Measured battery life in deep sleep state
    1. Remove AC charger and all USB pheripherals
    2. Long press power button to get DUT to Deep sleep
    3. Make sure power state is G3
       ("powerinfo on EC console")
    4. Run "battery" and note,
       'Remaining:' value as START_BATTERY
       'Cap-full:' value as MAX_CAPACITY
       note the start time
    5. Run "hibernate" on EC console
    6. Disconnect flex cable
    7. After > 4 hours, reconnect flex and power on DUT
       start servo
    8. Run "battery" on EC console once DUT is ON.
    9. Note 'Remaining:' value as END_BATTERY and note
       END_TIME
    10.Calculate battery life:
       DURATION_IN_SEC = END_TIME - START_TIME (sec)
       USAGE = START_BATTERY - END_BATTERY
       DAYS = MAX_CAPACITY / USAGE * DURATION_IN_SEC / 3600 / 24
       DAYS value should ~100 days.
    
    Change-Id: I0a3911c7a0e1b0505331b2249bb0c395b4a92362
    Signed-off-by: Kashyap Nanavati <kashyap.nanavati@intel.com>

--- board/hatch/board.c
+++ board/hatch/board.c
@@ -97,6 +97,38 @@ static void bc12_interrupt(enum gpio_signal signal)
 	}
 }
 
+void board_hibernate_late(void)
+{
+	int i;
+	const uint32_t hibernate_pins[][2] = {
+		/* Turn off LEDs in hibernate */
+		{GPIO_LED_1_L, GPIO_INPUT | GPIO_PULL_UP}, /* Yellow (hatch) */
+		{GPIO_LED_2_L, GPIO_INPUT | GPIO_PULL_UP}, /* White (hatch) */
+		{GPIO_LED_3_L, GPIO_INPUT | GPIO_PULL_UP},
+		{GPIO_LED_4_L, GPIO_INPUT | GPIO_PULL_UP},
+		{GPIO_LID_OPEN, GPIO_INT_RISING | GPIO_PULL_DOWN},
+	};
+
+	/* Change GPIOs' state in hibernate for better power consumption */
+	for (i = 0; i < ARRAY_SIZE(hibernate_pins); ++i)
+		gpio_set_flags(hibernate_pins[i][0], hibernate_pins[i][1]);
+
+	gpio_config_module(MODULE_KEYBOARD_SCAN, 0);
+
+	/*
+	 * Calling gpio_config_module sets disabled alternate function pins to
+	 * GPIO_INPUT.  But to prevent keypresses causing leakage currents
+	 * while hibernating we want to enable GPIO_PULL_UP as well.
+	 */
+	gpio_set_flags_by_mask(0x2, 0x03, GPIO_INPUT | GPIO_PULL_UP);
+	gpio_set_flags_by_mask(0x1, 0x7F, GPIO_INPUT | GPIO_PULL_UP);
+	gpio_set_flags_by_mask(0x0, 0xF0, GPIO_INPUT | GPIO_PULL_UP);
+	gpio_set_flags_by_mask(0x8, 0x04, GPIO_INPUT | GPIO_PULL_UP);
+	/* KBD_KSO2 needs to have a pull-down enabled instead of pull-up */
+	gpio_set_flags_by_mask(0x1, 0x80, GPIO_INPUT | GPIO_PULL_DOWN);
+}
+
+
 #include "gpio_list.h" /* Must come after other header files. */
 
 /******************************************************************************/
