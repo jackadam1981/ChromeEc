b2d5c3934e89c45aff3e0d46ec65d0e53d7510cf
Pi-Hsun Shih <pihsun@chromium.org>
2019-03-19 13:59:52 +0800

    kukui: scp: fix udelay hangs the system in clock enable.
    It seems that udelay doesn't work before the clock is properly
    configured and enabled, causing the SCP image not able to boot. Remove
    the udelay in scp_clock_high_enable.
    
    BUG=b:128877063
    TEST=manually, make sure SCP firmware works on boot.
    BRANCH=none
    
    Change-Id: Idc505a33a7e88d136a5b50f2e5bd52bd5213393b
    Signed-off-by: Pi-Hsun Shih <pihsun@chromium.org>

--- chip/mt_scp/clock.c
+++ chip/mt_scp/clock.c
@@ -98,20 +98,28 @@ static void scp_ulposc_config(int osc)
 
 void scp_clock_high_enable(int osc)
 {
+	volatile int i = 700000;
 	/* Enable high speed clock */
 	SCP_CLK_EN |= EN_CLK_HIGH;
 
 	switch (osc) {
 	case 0:
-		/* After 25ms, enable ULPOSC */
-		udelay(25 * MSEC);
+		/*
+		 * After 25ms, enable ULPOSC.
+		 * Note that we can't use udelay here since the clock isn't configured
+		 * correctly yet. The value 700000 is chosen so this loop takes about 25
+		 * ms.
+		 * */
+		while (i--)
+			;
 		SCP_CLK_EN |= CG_CLK_HIGH;
 		break;
 	case 1:
 		/* Turn off ULPOSC2 high-core-disable switch */
 		SCP_CLK_ON_CTRL &= ~HIGH_CORE_DIS_SUB;
 		/* After 25ms, turn on ULPOSC2 high core clock gate */
-		udelay(25 * MSEC);
+		while (i--)
+			;
 		SCP_CLK_HIGH_CORE |= CLK_HIGH_CORE_CG;
 		break;
 	default:
