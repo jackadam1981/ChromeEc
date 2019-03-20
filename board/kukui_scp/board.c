415dcfbc48534a3b587348fafb002567841bf1b9
Yilun Lin <yllin@google.com>
2019-03-20 17:29:29 +0800

    TEST-ONLY kukui_scp: Test MPU data RAM XN and code RAM RO.
    TEST=1. mpu 0 # disable MPU
         2. mpu_test # see it prints
         3. mpu 1 # enable MPU
         4. mpu_test # memory access violation, and reset.
         5. mpu_test # memory access violation, and reset again. (MPU enabled by default)
    
    TEST=1. rw 0x8 0x5566 # Write to code RAM and see memory access violation and reset.
         2. mpu 0  # disable MPU
         3. rw 0x8 0x5566 # Nothing happended
         4. rw 0x8 # Read 0x5566
         5. mpu 1  # enable MPU
         6. rw 0x8 0x5566 # memory access violation.
    
    Change-Id: I196ec77b25796febb3f09ca998c2adbbbc3b22e6
    Signed-off-by: Yilun Lin <yllin@google.com>

--- board/kukui_scp/board.c
+++ board/kukui_scp/board.c
@@ -9,10 +9,12 @@
 #include "ec_version.h"
 #include "gpio.h"
 #include "hooks.h"
+#include "mpu.h"
 #include "registers.h"
 #include "task.h"
 #include "timer.h"
 #include "util.h"
+#include "link_defs.h"
 
 /* Build GPIO tables */
 void eint_event(enum gpio_signal signal);
@@ -33,3 +35,44 @@ static void board_init(void)
 	gpio_enable_interrupt(GPIO_EINT7_TP);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+/* Put print_test in .data section to test XN (Execution Never) bit. */
+__SECTION_KEEP(data) const void print_test(void)
+{
+	ccprintf("print_test success\n");
+}
+
+static int command_mpu_test(int argc, char **argv)
+{
+	print_test();
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(mpu_test, command_mpu_test,
+	"",
+	"Test data RAM XN (Execution Never) bit.");
+
+static int command_mpu(int argc, char **argv)
+{
+	if (argc < 1 || argc > 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	if (argc != 2) {
+		ccprintf("MPU ");
+		if (MPU_CTRL & MPU_CTRL_ENABLE)
+			ccprintf("enabled\n");
+		else
+			ccprintf("disabled\n");
+		return EC_SUCCESS;
+	}
+
+	if (strtoi(argv[1], NULL, 10))
+		mpu_enable();
+	else
+		mpu_disable();
+
+	return EC_SUCCESS;
+
+}
+DECLARE_CONSOLE_COMMAND(mpu, command_mpu,
+	"[1|0]",
+	"Lock/Unlock MPU");
