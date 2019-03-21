4b9a150d84ecc8d9a0aaf136de5efa0c05094094
tim <tim2.lin@ite.corp-partner.google.com>
2019-03-21 10:43:45 +0800

    it83xx/i2cs: add enhanced i2c slave function
    The enhanced I2C channel D, E and F are used as slave,
    and these channel can support read and write 256-byte
    data by DMA mode.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I6af63721768a4949d1766dba47b2e87eaaaf8e79
    Signed-off-by: tim <tim2.lin@ite.corp-partner.google.com>

--- chip/it83xx/i2cs.c
+++ chip/it83xx/i2cs.c
@@ -5,25 +5,37 @@
 
 /* I2C module for Chrome EC */
 
-#include <stddef.h>
+#include "clock.h"
+#include "compile_time_macros.h"
+#include "console.h"
 #include "gpio.h"
 #include "hooks.h"
-#include "console.h"
-#include "registers.h"
-#include "task.h"
-#include "compile_time_macros.h"
 #include "i2cs.h"
+#include "registers.h"
+#include <stddef.h>
+#include <string.h>
+#include "task.h"
+
+/* Console output macros */
+#define CPRINTS(format, args...) cprints(CC_I2C, format, ## args)
 
 #define I2C_READ_MAXFIFO_DATA 16
 /* The size must be a power of 2 */
 #define I2C_MAX_BUFFER_SIZE 0x100
 #define I2C_SIZE_MASK (I2C_MAX_BUFFER_SIZE - 1)
 
-#define CPRINTS(format, args...) cprints(CC_I2C, format, ## args)
+/* Store master to slave data of channel D, E, F by DMA */
+static uint8_t in_data[3][I2C_MAX_BUFFER_SIZE]
+			__attribute__((section(".h2ram.pool.i2cslv")));
+/* Store slave to master data of channel D, E, F by DMA */
+static uint8_t out_data[3][I2C_MAX_BUFFER_SIZE]
+			__attribute__((section(".h2ram.pool.i2cslv")));
+/* Store read and write data of channel A by FIFO mode */
+static uint8_t pbuffer[I2C_MAX_BUFFER_SIZE];
 
 uint32_t w_index;
 uint32_t r_index;
-uint8_t pbuffer[I2C_MAX_BUFFER_SIZE];
+int wr_done;
 
 void buffer_index_reset(void)
 {
@@ -36,28 +48,29 @@ void buffer_index_reset(void)
 /* Data structure to define I2C slave control configuration. */
 struct i2c_slv_ctrl_t {
 	int irq;              /* slave irq */
-
+	int reg_shift;
+	enum clock_gate_offsets clock_gate;
+	int dma_index;
 };
 
 /* I2C slave control */
 const struct i2c_slv_ctrl_t i2c_slv_ctrl[] = {
-	[IT83XX_I2C_CH_A] = {IT83XX_IRQ_SMB_A},
-#if 0
-	[IT83XX_I2C_CH_D] = {IT83XX_IRQ_SMB_D},
-	[IT83XX_I2C_CH_E] = {IT83XX_IRQ_SMB_E},
-	[IT83XX_I2C_CH_F] = {IT83XX_IRQ_SMB_F},
-#endif
+	[IT83XX_I2C_CH_A] = {IT83XX_IRQ_SMB_A, -1, CGC_OFFSET_SMBA, -1},
+	[IT83XX_I2C_CH_D] = {IT83XX_IRQ_SMB_D, 3, CGC_OFFSET_SMBD, 0},
+	[IT83XX_I2C_CH_E] = {IT83XX_IRQ_SMB_E, 0, CGC_OFFSET_SMBE, 1},
+	[IT83XX_I2C_CH_F] = {IT83XX_IRQ_SMB_F, 1, CGC_OFFSET_SMBF, 2},
 };
-
-const unsigned int i2c_slvs_used = ARRAY_SIZE(i2c_slv_ctrl);
 
 void i2c_slave_read_write_data(int port)
 {
-	int i, count, slv_status;
+	int slv_status, i;
 
-	slv_status = IT83XX_SMB_SLSTA;
-
+	/* I2C slave channel A FIFO mode */
 	if (port < I2C_STANDARD_PORT_COUNT) {
+		int count;
+
+		slv_status = IT83XX_SMB_SLSTA;
+
 		/* bit0-4 : FIFO byte count */
 		count = IT83XX_SMB_SFFSTA & 0x1F;
 
@@ -117,6 +130,52 @@ void i2c_slave_read_write_data(int port)
 		/* Write clear the slave status */
 		IT83XX_SMB_SLSTA = slv_status;
 	}
+	/* I2C slave channel D, E, F DMA mode */
+	else {
+		int r_sh, idx;
+
+		/* Shift register */
+		r_sh = i2c_slv_ctrl[port].reg_shift;
+
+		idx = i2c_slv_ctrl[port].dma_index;
+
+		/* Interrupt pending */
+		if (IT83XX_I2C_STR(r_sh) & IT83XX_I2C_INTPEND) {
+
+			slv_status = IT83XX_I2C_IRQ_ST(r_sh);
+
+			/* Master to read data */
+			if (slv_status & IT83XX_I2C_IDR_CLR) {
+			/* TODO: Master to read data from "out_data" array */
+				/* Store test data from 0x00 to 0xFF */
+				for (i = 0; i < I2C_MAX_BUFFER_SIZE; i++)
+					out_data[idx][i] = i;
+			}
+			/* Master to write data */
+			if (slv_status & IT83XX_I2C_IDW_CLR) {
+			/* TODO: Slave to read data from "in_data" array */
+				wr_done = 1;
+			}
+
+			if (slv_status & IT83XX_I2C_SLVDATAFLG)
+				CPRINTS("Slave received data flag");
+
+			/* Slave finish */
+			if (slv_status & IT83XX_I2C_P_CLR) {
+				if (wr_done) {
+					CPRINTS("WData: %.*h\n",
+					I2C_MAX_BUFFER_SIZE, in_data[idx]);
+					wr_done = 0;
+				}
+			}
+
+			/* Write clear the slave status */
+			IT83XX_I2C_IRQ_ST(r_sh) = slv_status;
+		}
+
+		/* Hardware reset */
+		IT83XX_I2C_CTR(r_sh) |= IT83XX_I2C_HALT;
+	}
 }
 
 void i2c_slv_interrupt(int port)
@@ -130,6 +189,10 @@ void i2c_slv_interrupt(int port)
 
 void i2c_slave_enable(int port, uint8_t slv_addr1, uint8_t slv_addr2)
 {
+
+	clock_enable_peripheral(i2c_slv_ctrl[port].clock_gate, 0, 0);
+
+	/* I2C slave channel A FIFO mode */
 	if (port < I2C_STANDARD_PORT_COUNT) {
 
 		/* This field defines the SMCLK0/1/2 clock/data low timeout. */
@@ -162,27 +225,96 @@ void i2c_slave_enable(int port, uint8_t slv_addr1, uint8_t slv_addr2)
 		/* bit5 : Enable the SMBus slave device */
 		IT83XX_SMB_HOCTL2(port) |= IT83XX_SMB_SLVEN;
 	}
+	/* I2C slave channel D, E, F DMA mode */
+	else {
+		int r_sh, idx;
+
+		/* Shift register */
+		r_sh = i2c_slv_ctrl[port].reg_shift;
+
+		idx = i2c_slv_ctrl[port].dma_index;
+
+		switch (port) {
+		case IT83XX_I2C_CH_D:
+			/* Enable I2C D channel */
+			IT83XX_GPIO_GRC2 |= 0x20;
+			break;
+		case IT83XX_I2C_CH_E:
+			/* Enable I2C E channel */
+			IT83XX_GCTRL_PMER1 |= 0x01;
+			break;
+		case IT83XX_I2C_CH_F:
+			/* Enable I2C F channel */
+			IT83XX_GCTRL_PMER1 |= 0x02;
+			break;
+		}
+
+		/* Software reset */
+		IT83XX_I2C_DHTR(r_sh) |= 0x80;
+		IT83XX_I2C_DHTR(r_sh) &= 0x7F;
+
+		/* Bit stretching */
+		IT83XX_I2C_TOS(r_sh) |= IT83XX_I2C_CLK_STR;
+
+		/* Slave address(8-bit)*/
+		IT83XX_I2C_IDR(r_sh) = slv_addr1 << 1;
+
+		/* Slave address2(8-bit) */
+		if (slv_addr2)
+			IT83XX_I2C_IDR2(r_sh) = slv_addr2 << 1;
+
+		/* I2C interrupt enable and set acknowledge */
+		IT83XX_I2C_CTR(r_sh) = IT83XX_I2C_HALT |
+			IT83XX_I2C_INTEN | IT83XX_I2C_ACK;
+
+		/*
+		 * bit3 : Slave ID write flag
+		 * bit2 : Slave ID read flag
+		 * bit1 : Slave received data flag
+		 * bit0 : Slave finish
+		 */
+		IT83XX_I2C_IRQ_ST(r_sh) = 0xFF;
+
+		/* Clear read and write data buffer of DMA */
+		memset(in_data[idx], 0, I2C_MAX_BUFFER_SIZE);
+		memset(out_data[idx], 0, I2C_MAX_BUFFER_SIZE);
+
+		/* DMA write target address register */
+		IT83XX_I2C_RAMHA(r_sh) = ((uint32_t)in_data[idx] >> 8) & 0x0F;
+		IT83XX_I2C_RAMLA(r_sh) = (uint32_t)in_data[idx] & 0xFF;
+
+		/* DMA read target address register */
+		IT83XX_I2C_RAMHA2(r_sh) = ((uint32_t)out_data[idx] >> 8) & 0x0F;
+		IT83XX_I2C_RAMLA2(r_sh) = (uint32_t)out_data[idx] & 0xFF;
+
+		/* I2C module enable and command queue mode */
+		IT83XX_I2C_CTR1(r_sh) = IT83XX_I2C_COMQ_EN |
+			IT83XX_I2C_MDL_EN;
+	}
 }
 
 static void i2c_slave_init(void)
 {
-	int  i;
+	int  i, p;
+
+	/* DLM 52k~56k size select enable */
+	IT83XX_GCTRL_MCCR2 |= 0x10;
 
 	/* Enable I2C Slave function */
 	for (i = 0; i < i2c_slvs_used; i++) {
 
-		if (i2c_slv_ports[i].name != NULL) {
-			/* To enable slave ch[x] */
-			i2c_slave_enable(i, i2c_slv_ports[i].slave_adr,
-				i2c_slv_ports[i].slave_adr2);
+		/* I2c slave port mapping. */
+		p = i2c_slv_ports[i].port;
 
-			/* Clear the interrupt status */
-			task_clear_pending_irq(i2c_slv_ctrl[i].irq);
+		/* To enable slave ch[x] */
+		i2c_slave_enable(p, i2c_slv_ports[i].slave_adr,
+			i2c_slv_ports[i].slave_adr2);
 
-			/* enable i2c interrupt */
-			task_enable_irq(i2c_slv_ctrl[i].irq);
+		/* Clear the interrupt status */
+		task_clear_pending_irq(i2c_slv_ctrl[p].irq);
 
-		}
+		/* enable i2c interrupt */
+		task_enable_irq(i2c_slv_ctrl[p].irq);
 	}
 }
-DECLARE_HOOK(HOOK_INIT, i2c_slave_init, HOOK_PRIO_INIT_I2C);
+DECLARE_HOOK(HOOK_INIT, i2c_slave_init, HOOK_PRIO_INIT_I2C + 1);
