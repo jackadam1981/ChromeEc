c13a101b35cc40619d86282e606af0a99143e48e
Ting Shen <phoenixshen@google.com>
2019-03-22 20:30:49 +0800

    kukui: implement pogo charge logic
    Added a dedicated(POGO) charge port and related costomization
    functions.
    
    BUG=b:128386458
    TEST=`gpioset POGO_VBUS_PRESENT 0/1` to simulate pogo charger
    presence, and a normal usb charger for usb port to check its behavior.
    BRANCH=None
    
    Change-Id: If102c57088f1aafee437c7cc912992dc0cf8e9da
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- board/kukui/usb_pd_policy.c
+++ board/kukui/usb_pd_policy.c
@@ -61,17 +61,16 @@ int board_vbus_source_enabled(int port)
 
 int pd_set_power_supply_ready(int port)
 {
+	if (port != CHARGE_PORT_USB_C)
+		return EC_ERROR_INVAL;
 
 	pd_set_vbus_discharge(port, 0);
 	/* Provide VBUS */
 	vbus_en = 1;
 	charger_enable_otg_power(1);
 
-	if (board_get_version() >= 2) {
-		/* TODO(b:123268580): Implement POGO discharge logic. */
-		gpio_set_level(GPIO_EN_USBC_CHARGE_L, 1);
-		gpio_set_level(GPIO_EN_PP5000_USBC, 1);
-	}
+	gpio_set_level(GPIO_EN_USBC_CHARGE_L, 1);
+	gpio_set_level(GPIO_EN_PP5000_USBC, 1);
 
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
@@ -83,6 +82,9 @@ void pd_power_supply_reset(int port)
 {
 	int prev_en;
 
+	if (port != CHARGE_PORT_USB_C)
+		return;
+
 	prev_en = vbus_en;
 	/* Disable VBUS */
 	vbus_en = 0;
@@ -91,17 +93,7 @@ void pd_power_supply_reset(int port)
 	if (prev_en)
 		pd_set_vbus_discharge(port, 1);
 
-	if (board_get_version() >= 2) {
-		/*
-		 * TODO(b:123268580): Implement POGO discharge logic.
-		 *
-		 * Turn off source path and POGO path before asserting
-		 * EN_USB_CHARGE_L.
-		 */
-		gpio_set_level(GPIO_EN_PP5000_USBC, 0);
-		gpio_set_level(GPIO_EN_POGO_CHARGE_L, 1);
-		gpio_set_level(GPIO_EN_USBC_CHARGE_L, 0);
-	}
+	gpio_set_level(GPIO_EN_PP5000_USBC, 0);
 
 	/* notify host of power info change */
 	pd_send_host_event(PD_EVENT_POWER_CHANGE);
