c513b27328c3c32704e36079d36ee280b909c020
You-Cheng Syu <youcheng@chromium.org>
2019-03-22 18:23:11 +0800

    ec_commands: Make importing ec_commands.h easier for other projects.
    There are many projects (e.g., kernel, coreboot, flashrom, mosys) have
    their own copy of ec_commands.h header file. To reduce their effort, we
    should make this single file able to be directly copied and included
    without modification if possible.
    
    Currently, ec_commands.h includes compile_time_macros.h when __KERNEL__
    is not defined. However, compile_time_macros.h defines many common
    macros (e.g., ARRAY_SIZE) that other projects may already have. This
    might cause redefinition error. Also, other projects would need to
    either copy compile_time_macros.h or modify ec_commands.h to not to
    include compile_time_macros.h. This costs extra effort.
    
    This CL makes ec_commands.h not to include compile_time_macros.h when
    it's not in Chromium EC repository to solve the problem. To detect that,
    we check if macro CHROMIUM_EC is defined. CHROMIUM_EC is not defined
    when building targets under build/$BOARD/util/, so this CL add it into
    BUILD_CFLAGS and HOST_CFLAGS as well (as this macro is not used
    elsewhere).
    
    BUG=b:109900671,b:118654976
    BRANCH=none
    TEST=make -j buildall
    
    Change-Id: I38c77f0233ff5962b6f90f25ba96802d05e706d7
    Signed-off-by: You-Cheng Syu <youcheng@chromium.org>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -5,6 +5,9 @@
 
 /* Host communication command constants for Chrome EC */
 
+/* CHROMIUM_EC is expected to be defined when building images or utils in
+ * Chromium EC repository. */
+
 #ifndef __CROS_EC_EC_COMMANDS_H
 #define __CROS_EC_EC_COMMANDS_H
 
@@ -16,19 +19,16 @@
 extern "C"{
 #endif
 
+#ifdef CHROMIUM_EC
 /*
  * Include common.h for CONFIG_HOSTCMD_ALIGNED, if it's defined. This
  * generates more efficient code for accessing request/response structures on
  * ARM Cortex-M if the structures are guaranteed 32-bit aligned.
  */
-#ifdef CHROMIUM_EC
 #include "common.h"
-#endif
-
-#ifdef __KERNEL__
-#define BUILD_ASSERT(_cond)
-#else
 #include "compile_time_macros.h"
+#else
+#define BUILD_ASSERT(_cond)
 #endif
 
 /*
