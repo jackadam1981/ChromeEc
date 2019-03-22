a523c848087c4db7f114f703a41bd98568b7c090
Daisuke Nojiri <dnojiri@chromium.org>
2019-03-22 11:46:56 -0700

    chgstv2: Add callback for board to check power for booting OS
    This patch adds a callback function which allows a board to decide
    whether power is ready for the OS to boot.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    Change-Id: I32af4c2f88c9d3215edb9c622f1409dce6262b80

--- include/charge_state_v2.h
+++ include/charge_state_v2.h
@@ -126,4 +126,27 @@ enum critical_shutdown {
 enum critical_shutdown board_critical_shutdown_check(
 		struct charge_state_data *curr);
 
+/**
+ * Callback to set battery level for shutdown
+ *
+ * A board can implement this to customize shutdown battery level at runtime.
+ *
+ * @return battery level for shutdown
+ */
+uint8_t board_set_battery_level_shutdown(void);
+
+/**
+ * Check whether enough power is available for the OS to boot.
+ *
+ * @return 0: Power is good. 1: Power is short.
+ */
+int charge_state_limit_power(void);
+
+/**
+ * Callback which allows a board to decide whether power is ready for OS
+ *
+ * @return 0: Power is good. 1: Power is short.
+ */
+int board_check_os_boot_power(void);
+
 #endif /* __CROS_EC_CHARGE_STATE_V2_H */
