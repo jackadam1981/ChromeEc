ecbceacecb334c53ae17743dbc69b398f4ee36d6
Enrico Granata <egranata@chromium.org>
2019-03-22 14:14:49 -0700

    cyan: reduce sensor FIFO size to 256
    A smaller FIFO helps improve the success rate of
    CTS batching tests.
    
    BUG=b:129101597
    TEST=compile
    
    Signed-off-by: Enrico Granata <egranata@chromium.org>
    Change-Id: I9cd313455bef14661886a10f1866d7f8f1dc576b

--- board/cyan/board.h
+++ board/cyan/board.h
@@ -75,7 +75,7 @@
 #define CONFIG_CMD_ACCEL_INFO
 
 /* Depends on how fast the AP boots and typical ODRs */
-#define CONFIG_ACCEL_FIFO 512
+#define CONFIG_ACCEL_FIFO 256
 #define CONFIG_ACCEL_FIFO_THRES (CONFIG_ACCEL_FIFO / 3)
 
 #define CONFIG_LID_ANGLE
