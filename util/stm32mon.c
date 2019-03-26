063a81d5b8fd95b4d01c5b5ac8e70837e7e63c03
Namyoon Woo <namyoon@chromium.org>
2019-03-26 15:19:24 -0700

    stm32mon: retry on damaged ACK, NACK or timeout
    BUG=b:112837404
    BRANCH=None
    TEST=manually ran iteflash or flash_ec on scarlet.
    
    1. iteflash via Suzy-Q at 57600 bps on scarlet.
    $ ./util/flash_ec --board scarlet --image ${IMG} \
      --bitbang_rate 57600 --verbose
    dut-control --port=9999 servo_type
    INFO: Using ccd_cr50.
    INFO: Using ec image : /Downloads/scarlet/ec.bin
    INFO: Flashing chip stm32.
    INFO: Using serial flasher : ...
    INFO: ec UART pty : /dev/pts/9
    dut-control --port=9999 ec_ec3po_interp_connect:off
    INFO: Using CCD bitbang.
    dut-control --port=9999 ccd_ec_boot_mode_bitbang:on
    dut-control --port=9999 cold_reset:on
    dut-control --port=9999 cold_reset:off
    dut-control --port=9999 ec_uart_parity:even
    INFO: Programming at 57600 baud
    dut-control --port=9999 ec_uart_baudrate:57600
    dut-control --port=9999 ec_uart_bitbang_en:on
    INFO: Programming EC firmware image.
    stm32mon -d /dev/pts/9 -c -u -e -w ...
    Waiting for the monitor startup ...Done.
    ChipID 0x442 : STM32F09x
    Bootloader v3.1, commands : 00 01 02 11 21 31 44 63 73 82 92
    Flash write unprotected.
    Waiting for the monitor startup ...Done.
    Erasing...
    Flash erased.
    Writing 232200 bytes at 0x08000000
       232200 bytes written.
    --
    RESP_ACK        2484
    RESP_NACK       0
    RESP_BUSY       0
    RESP_ACK_ISH    1
    JUNK            0
    --
    Done.
    
    2. iteflash via Suzy-Q at 9600 bps on scarlet.
     which tends to fail for NACK or timeout.
    $ echo gpioset EC_FLASH_SELECT 1 > /dev/ttyUSB0
    $ echo bitbang 2 9600 even > /dev/ttyUSB0
    $ echo ecrst pulse > /dev/ttyUSB0
    $ sleep 2
    $ ./build/scarlet/util/stm32mon -b 9600 -u -e -w ${IMG}
    Waiting for the monitor startup ...Timeout
    NACK
    ChipID 0x442 : STM32F09x
    Monitor already started.
    ChipID 0x442 : STM32F09x
    Bootloader v3.1, commands : 00 01 02 11 21 31 44 63 73 82 92
    Flash write unprotected.
    Waiting for the monitor startup ...Done.
    Erasing...
    Flash erased.
    Writing 232200 bytes at 0x08000000
    - 39%NACK
    payload 1 ACK failed for CMD31
       232200 bytes written.
    --
    RESP_ACK        2488
    RESP_NACK       2
    RESP_BUSY       0
    RESP_ACK_ISH    0
    JUNK            0
    --
    Done.
    
    3. flash_ec via Servo Micro on scarlet
    $ ./util/flash_ec --board scarlet --image ${IMG} --verify --verbose
    INFO: Using servo_v4_with_servo_micro.
    INFO: Using ec image : /Downloads/scarlet/ec.bin
    INFO: Flashing chip stm32.
    ...
    stm32mon -d /dev/pts/17 -u -e -w ${IMG}
    ...
    Done.
    INFO: Reading EC firmware image.
    stm32mon -d /dev/pts/17 -U -r ${TMP}
    ...
    Reading 262144 bytes at 0x08000000
       262144 bytes read.
    Done.
    INFO: Verifying EC firmware image.
    ...
    
    4. flash_ec via Servo V2 on scarlet
    
    Change-Id: I3c8e8c81d9d12475f5722eaacc932dc1244625d5
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- util/stm32mon.c
+++ util/stm32mon.c
@@ -61,8 +61,9 @@
 #define CMD_RU       0x92 /* Disables the read protection */
 
 #define RESP_NACK    0x1f
-#define RESP_ACK     0x79
+#define RESP_ACK     0x79 /* 0b 0111 1001 */
 #define RESP_BUSY    0x76
+#define RESP_ACK_ISH 0xBC /* 0b 1011 1100, 1 bit shifted REST_ACK */
 
 /* SPI Start of Frame */
 #define SOF          0x5A
@@ -106,6 +107,8 @@ struct stm32_def {
 #define DEFAULT_BAUDRATE B38400
 #define PAGE_SIZE 256
 #define INVALID_I2C_ADAPTER -1
+#define MAX_ACK_RETRY_COUNT	(EXT_ERASE_TIMEOUT / DEFAULT_TIMEOUT)
+#define MAX_RETRY_COUNT		3
 
 enum interface_mode {
 	MODE_SERIAL,
@@ -129,6 +132,7 @@ const char *serial_port = "/dev/ttyUSB1";
 const char *input_filename;
 const char *output_filename;
 uint32_t offset = 0x08000000, length = 0;
+uint32_t retry_on_ackish;
 
 /* optional command flags */
 enum {
@@ -158,6 +162,27 @@ static void discard_input(int);
 
 /* On user request save all data exchange with the target in this log file. */
 static FILE *log_file;
+
+/* Statistic data structure for response kind */
+struct {
+	const char * const event_name;
+	uint32_t event_count;
+} stat_resp[] = {
+	{ "RESP_ACK",	0 },
+	{ "RESP_NACK",	0 },
+	{ "RESP_BUSY",	0 },
+	{ "RESP_ACK_ISH", 0 },
+	{ "JUNK",	0 },
+};
+
+enum {
+	RESP_ACK_IDX = 0,
+	RESP_NACK_IDX,
+	RESP_BUSY_IDX,
+	RESP_ACK_ISH_IDX,
+	JUNK_IDX,
+	MAX_EVENT_IDX
+};
 
 /*
  * Print data into the log file, in hex, 16 bytes per line, prefix the first
@@ -207,6 +232,7 @@ static ssize_t write_wrapper(int fd, const void *buf, size_t count)
 
 	return rv;
 }
+
 int open_serial(const char *port, int cr50_mode)
 {
 	int fd, res;
@@ -374,7 +400,7 @@ int wait_for_ack(int fd)
 	uint8_t resp;
 	int res;
 	time_t deadline = time(NULL) + DEFAULT_TIMEOUT;
-	uint8_t ack = RESP_ACK;
+	const uint8_t ack = RESP_ACK;
 
 	while (time(NULL) < deadline) {
 		res = read_wrapper(fd, &resp, 1);
@@ -382,27 +408,46 @@ int wait_for_ack(int fd)
 			perror("Failed to read answer");
 			return -EIO;
 		}
-		if (res == 1) {
-			if (resp == RESP_ACK) {
-				if (mode == MODE_SPI) /* Ack the ACK */
-					if (write_wrapper(fd, &ack, 1) != 1)
-						return -EIO;
-				return 0;
-			} else if (resp == RESP_NACK) {
-				fprintf(stderr, "NACK\n");
-				if (mode == MODE_SPI) /* Ack the NACK */
-					if (write_wrapper(fd, &ack, 1) != 1)
-						return -EIO;
-				discard_input(fd);
-				return -EINVAL;
-			} else if (resp == RESP_BUSY) {
-				/* I2C Boot protocol 1.1 */
-				deadline = time(NULL) + DEFAULT_TIMEOUT;
-			} else {
-				if (mode == MODE_SERIAL)
-					fprintf(stderr, "Receive junk: %02x\n",
-						resp);
-			}
+
+		if (res != 1)
+			continue;
+
+		if (resp == RESP_ACK) {
+			stat_resp[RESP_ACK_IDX].event_count++;
+			if (mode == MODE_SPI) /* Ack the ACK */
+				if (write_wrapper(fd, &ack, 1) != 1)
+					return -EIO;
+			return 0;
+		}
+
+		if (resp == RESP_ACK_ISH && retry_on_ackish) {
+			/* It is a damaged ACK. However, device is likely
+			 * to believe it sent ACK, so let's not treat it as NACK
+			 * or junk.
+			 */
+			stat_resp[RESP_ACK_ISH_IDX].event_count++;
+			fprintf(stderr, "ACK_ISH\n");
+			return -EUCLEAN;
+		}
+
+		if (resp == RESP_NACK) {
+			stat_resp[RESP_NACK_IDX].event_count++;
+			fprintf(stderr, "NACK\n");
+			if (mode == MODE_SPI) /* Ack the NACK */
+				if (write_wrapper(fd, &ack, 1) != 1)
+					return -EIO;
+			discard_input(fd);
+			return -EINVAL;
+		}
+
+		if (resp == RESP_BUSY) {
+			stat_resp[RESP_BUSY_IDX].event_count++;
+			/* I2C Boot protocol 1.1 */
+			deadline = time(NULL) + DEFAULT_TIMEOUT;
+		} else {
+			stat_resp[JUNK_IDX].event_count++;
+			if (mode == MODE_SERIAL)
+				fprintf(stderr, "Receive junk: %02x\n", resp);
 		}
 	}
 	fprintf(stderr, "Timeout\n");
@@ -418,19 +463,23 @@ int send_command(int fd, uint8_t cmd, payload_t *loads, int cnt,
 	uint8_t cmd_frame[] = { SOF, cmd, 0xff ^ cmd }; /* XOR checksum */
 	/* only the SPI mode needs the Start Of Frame byte */
 	int cmd_off = mode == MODE_SPI ? 0 : 1;
+	int ran_clean = 1;
 
 	/* Send the command index */
 	res = write_wrapper(fd, cmd_frame + cmd_off,
 			    sizeof(cmd_frame) - cmd_off);
 	if (res <= 0) {
 		perror("Failed to write command frame");
-		return -1;
+		return -EIO;
 	}
 
 	/* Wait for the ACK */
-	if (wait_for_ack(fd) < 0) {
+	res = wait_for_ack(fd);
+	if (res == -EUCLEAN)
+		ran_clean = 0;
+	else if (res < 0) {
 		fprintf(stderr, "Failed to get command 0x%02x ACK\n", cmd);
-		return -1;
+		return res;
 	}
 
 	/* Send the command payloads */
@@ -457,35 +506,35 @@ int send_command(int fd, uint8_t cmd, payload_t *loads, int cnt,
 			if (res < 0) {
 				perror("Failed to write command payload");
 				free(data);
-				return -1;
+				return -EIO;
 			}
 			size -= res;
 			data_ptr += res;
 		}
+		free(data);
 
 		/* Wait for the ACK */
 		res = wait_for_ack(fd);
-		if (res < 0) {
+		if (res == -EUCLEAN)
+			ran_clean = 0;
+		else if (res < 0) {
 			if (res != -ETIMEDOUT)
-				fprintf(stderr,
-					"payload %d ACK failed for CMD%02x\n",
-					c, cmd);
-			free(data);
+				fprintf(stderr,	"payload %d ACK failed"
+					" for CMD%02x\n", c, cmd);
 			return res;
 		}
-		free(data);
 	}
 
 	/* Read the answer payload */
 	if (resp) {
 		if (mode == MODE_SPI) /* ignore dummy byte */
 			if (read_wrapper(fd, resp, 1) < 0)
-				return -1;
+				return -EIO;
 		while ((resp_size > 0) &&
 		       (res = read_wrapper(fd, resp, resp_size))) {
 			if (res < 0) {
 				perror("Failed to read payload");
-				return -1;
+				return -EIO;
 			}
 			readcnt += res;
 			resp += res;
@@ -494,15 +543,46 @@ int send_command(int fd, uint8_t cmd, payload_t *loads, int cnt,
 
 		/* Wait for the ACK */
 		if (ack_requested) {
-			if (wait_for_ack(fd) < 0) {
+			res = wait_for_ack(fd);
+			if (res == -EUCLEAN)
+				ran_clean = 0;
+			else if (res < 0) {
 				fprintf(stderr,
-					"Failed to get response to command 0x%02x ACK\n",
-					cmd);
-				return -1;
+					"Failed to get response to command"
+					" 0x%02x ACK\n", cmd);
+				return res;
 			}
 		}
 	}
+
+	if (ran_clean == 0)
+		return -EUCLEAN;
+
 	return readcnt;
+}
+
+int send_command_retry(int fd, uint8_t cmd, payload_t *loads,
+		int cnt, uint8_t *resp, int resp_size, int ack_requested)
+{
+	int res;
+	int retries = MAX_RETRY_COUNT;
+
+	do {
+		int ack_tries = MAX_ACK_RETRY_COUNT;
+
+		res = send_command(fd, cmd, loads, cnt, resp, resp_size,
+			ack_requested);
+
+		while (res == -ETIMEDOUT && ack_tries--) {
+			if (cmd == CMD_WRITEMEM) {
+				/* send garbage byte */
+				write_wrapper(fd, loads->data, 1);
+			}
+			res = wait_for_ack(fd);
+		}
+	} while ((res == -EINVAL || res == -EUCLEAN) && retries--);
+
+	return res;
 }
 
 struct stm32_def *command_get_id(int fd)
@@ -549,23 +629,27 @@ int init_monitor(int fd)
 		res = write_wrapper(fd, &init, 1);
 		if (res <= 0) {
 			perror("Failed to write command");
-			return -1;
+			return -EIO;
 		}
 		/* Wait for the ACK */
 		res = wait_for_ack(fd);
 		if (res == 0)
 			break;
+		if (res > 0) {
+			fprintf(stderr, "Unexpected return value from"
+				" wait_for_ack:%d", res);
+			return -EIO;
+		}
 		if (res == -EINVAL) {
-			/* we got NACK'ed, the loader might be already started
-			 * let's ping it to check
+			/* we got NACK'ed, the loader might be already
+			 * started let's ping it to check
 			 */
 			if (command_get_id(fd)) {
 				printf("Monitor already started.\n");
 				return 0;
 			}
-		}
-		if (res < 0 && res != -ETIMEDOUT)
-			return -1;
+		} else if (res != -ETIMEDOUT && res != -EUCLEAN)
+			return res;
 		fflush(stdout);
 	}
 	printf("Done.\n");
@@ -631,6 +715,7 @@ static void draw_spinner(uint32_t remaining, uint32_t size)
 		printf("\r%c%3d%%", wheel[windex++], percent);
 		windex %= sizeof(wheel);
 	}
+	fflush(stdout);
 }
 
 int command_read_mem(int fd, uint32_t address, uint32_t size, uint8_t *buffer)
@@ -645,18 +730,21 @@ int command_read_mem(int fd, uint32_t address, uint32_t size, uint8_t *buffer)
 	};
 
 	while (remaining) {
-		cnt = (remaining > PAGE_SIZE) ? PAGE_SIZE - 1 : remaining - 1;
+		uint32_t bytes = MIN(remaining, PAGE_SIZE);
+
+		cnt = (uint8_t) (bytes - 1);
 		addr_be = htonl(address);
 
 		draw_spinner(remaining, size);
-		fflush(stdout);
-		res = send_command(fd, CMD_READMEM, loads, 2, buffer, cnt + 1,
-				   0);
+
+		res = send_command_retry(fd, CMD_READMEM, loads, 2, buffer,
+					 bytes, 0);
 		if (res < 0)
 			return -EIO;
-		buffer += cnt + 1;
-		address += cnt + 1;
-		remaining -= cnt + 1;
+
+		buffer += bytes;
+		address += bytes;
+		remaining -= bytes;
 	}
 
 	return size;
@@ -676,7 +764,7 @@ int command_write_mem(int fd, uint32_t address, uint32_t size, uint8_t *buffer)
 	};
 
 	while (remaining) {
-		cnt = (remaining > PAGE_SIZE) ? PAGE_SIZE : remaining;
+		cnt = MIN(remaining, PAGE_SIZE);
 		/* skip empty blocks to save time */
 		for (i = 0; i < cnt && buffer[i] == 0xff; i++)
 			;
@@ -687,9 +775,10 @@ int command_write_mem(int fd, uint32_t address, uint32_t size, uint8_t *buffer)
 			memcpy(outbuf + 1, buffer, cnt);
 
 			draw_spinner(remaining, size);
-			fflush(stdout);
-			res = send_command(fd, CMD_WRITEMEM, loads, 2,
+
+			res = send_command_retry(fd, CMD_WRITEMEM, loads, 2,
 					   NULL, 0, 1);
+
 			if (res < 0)
 				return -EIO;
 		}
@@ -707,7 +796,6 @@ int command_ext_erase(int fd, uint16_t count, uint16_t start)
 	uint16_t count_be = htons(count);
 	payload_t load = { 2, (uint8_t *)&count_be };
 	uint16_t *pages = NULL;
-	int retries = EXT_ERASE_TIMEOUT / DEFAULT_TIMEOUT;
 
 	if (count < 0xfff0) {
 		int i;
@@ -723,10 +811,7 @@ int command_ext_erase(int fd, uint16_t count, uint16_t start)
 	}
 
 	printf("Erasing...\n");
-	res = send_command(fd, CMD_EXTERASE, &load, 1, NULL, 0, 1);
-	/* Erase can take long time (e.g. 13s+ on STM32H7) */
-	while ((res == -ETIMEDOUT) && --retries)
-		res = wait_for_ack(fd);
+	res = send_command_retry(fd, CMD_EXTERASE, &load, 1, NULL, 0, 1);
 	if (res >= 0)
 		printf("Flash erased.\n");
 
@@ -764,14 +849,13 @@ int command_erase_i2c(int fd, uint16_t count, uint16_t start)
 		count_be = htons(count - 1);
 		for (i = 0; i < count; i++)
 			pages[i] = htons(start + i);
-	} else {
-		load_cnt = 1;
 	}
 
-	erase_cmd = (boot_loader_version == 0x10 ? CMD_EXTERASE :
-		     CMD_NO_STRETCH_ERASE);
-	res = send_command(fd, erase_cmd, load, load_cnt,
-			   NULL, 0, 1);
+	erase_cmd = (boot_loader_version == 0x10) ? CMD_EXTERASE :
+		     CMD_NO_STRETCH_ERASE;
+
+	printf("Erasing...\n");
+	res = send_command(fd, erase_cmd, load, load_cnt, NULL, 0, 1);
 	if (res >= 0)
 		printf("Flash erased.\n");
 
@@ -801,6 +885,7 @@ int command_erase(int fd, uint16_t count, uint16_t start)
 			pages[i+1] = start + i;
 	}
 
+	printf("Erasing...\n");
 	res = send_command(fd, CMD_ERASE, &load, 1, NULL, 0, 1);
 	if (res >= 0)
 		printf("Flash erased.\n");
@@ -813,7 +898,7 @@ int command_erase(int fd, uint16_t count, uint16_t start)
 int command_read_unprotect(int fd)
 {
 	int res;
-	int retries = EXT_ERASE_TIMEOUT / DEFAULT_TIMEOUT;
+	int retries = MAX_RETRY_COUNT;
 
 	printf("Unprotecting flash read...\n");
 
@@ -959,13 +1044,12 @@ int write_flash(int fd, struct stm32_def *chip, const char *filename,
 		return -EIO;
 	}
 	res = fread(buffer, 1, size, hnd);
+	fclose(hnd);
 	if (res <= 0) {
 		fprintf(stderr, "Cannot read %s\n", filename);
 		free(buffer);
-		fclose(hnd);
 		return -EIO;
 	}
-	fclose(hnd);
 
 	/* faster write: skip empty trailing space */
 	while (res && buffer[res - 1] == 0xff)
@@ -980,7 +1064,7 @@ int write_flash(int fd, struct stm32_def *chip, const char *filename,
 		free(buffer);
 		return -EIO;
 	}
-	printf("\rDone.\n");
+	printf("\r   %d bytes written.\n", written);
 
 	free(buffer);
 	return 0;
@@ -1144,6 +1228,19 @@ int parse_parameters(int argc, char **argv)
 	return flags;
 }
 
+static void display_stat_response(void)
+{
+	uint32_t total_events = MAX_EVENT_IDX;
+	uint32_t idx;
+
+	printf("--\n");
+	for (idx = 0; idx < total_events; ++idx) {
+		printf("%-15s %d\n", stat_resp[idx].event_name,
+				stat_resp[idx].event_count);
+	}
+	printf("--\n");
+}
+
 int main(int argc, char **argv)
 {
 	int ser;
@@ -1153,6 +1250,8 @@ int main(int argc, char **argv)
 
 	/* Parse command line options */
 	flags = parse_parameters(argc, argv);
+
+	retry_on_ackish = !!(flags & FLAG_CR50_MODE);
 
 	switch (mode) {
 	case MODE_SPI:
@@ -1226,5 +1325,14 @@ terminate:
 
 	/* Close serial port */
 	close(ser);
+
+	if (retry_on_ackish)
+		display_stat_response();
+
+	if (ret)
+		fprintf(stderr, "Failed\n");
+	else
+		printf("Done.\n");
+
 	return ret;
 }
