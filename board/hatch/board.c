85451ac93d446683bbaa1edfd037f74ec2e56ea4
Nick Vaccaro <nvaccaro@google.com>
2019-03-27 14:12:39 -0700

    FORTEST: hatch: add tcs3400 sensor connection details
     - add new RGB_ALS sensor_id
     - add RGB_ALS sensor to motion_sensors list
     - enable GPIO_BASE_TCS3400_INT_L as an interrupt
     - enable i2c port 4.1
    
    BUG=b:129419982
    BRANCH=master
    TEST=build and flash hatch ec; from ec console,
    execute 'i2cxfer w 0 0x72 0x80 0x13' and verify ec log gets
    generated showing ALS irq working and reading data.
    
    Change-Id: I80f344fb08f870b2d3f3762f77dff69af093ea56
    Signed-off-by: Nick Vaccaro <nvaccaro@google.com>
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- board/hatch/board.c
+++ board/hatch/board.c
@@ -13,6 +13,7 @@
 #include "driver/accel_bma2x2.h"
 #include "driver/accelgyro_bmi160.h"
 #include "driver/als_opt3001.h"
+#include "driver/als_tcs3400.h"
 #include "driver/bc12/pi3usb9201.h"
 #include "driver/ppc/sn5s330.h"
 #include "driver/tcpm/anx7447.h"
@@ -181,6 +182,29 @@ static struct opt3001_drv_data_t g_opt3001_data = {
 	.offset = 0,
 };
 
+static struct tcs3400_drv_data_t g_tcs3400_data = {
+	.als_cal.scale = 1,
+	.als_cal.uscale = 0,
+	.als_cal.offset = 0,
+};
+
+static struct tcs3400_rgb_drv_data_t g_tcs3400_rgb_data = {
+	.device_scale = 1,
+	.device_uscale = 0,
+	.rgb_cal[X] = {
+		.scale = TCS3400_SCALE(1),
+		.offset = 0,
+	},
+	.rgb_cal[Y] = {
+		.scale = TCS3400_SCALE(1),
+		.offset = 0,
+	},
+	.rgb_cal[Z] = {
+		.scale = TCS3400_SCALE(1),
+		.offset = 0,
+	},
+};
+
 /* Matrix to rotate accelrator into standard reference frame */
 static const mat33_fp_t base_standard_ref = {
 	{ 0, FLOAT_TO_FP(1), 0},
@@ -290,6 +314,44 @@ struct motion_sensor_t motion_sensors[] = {
 				.odr = 1000,
 			},
 		},
+	},
+
+	[CLEAR_ALS] = {
+		.name = "Clear Light",
+		.active_mask = SENSOR_ACTIVE_S0_S3,
+		.chip = MOTIONSENSE_CHIP_TCS3400,
+		.type = MOTIONSENSE_TYPE_LIGHT,
+		.location = MOTIONSENSE_LOC_LID,
+		.drv = &tcs3400_drv,
+		.drv_data = &g_tcs3400_data,
+		.port = I2C_PORT_TEST,
+		.addr = TCS3400_I2C_ADDR,
+		.rot_standard_ref = NULL,
+		.default_range = 0x10000,
+		.min_frequency = TCS3400_LIGHT_MIN_FREQ,
+		.max_frequency = TCS3400_LIGHT_MAX_FREQ,
+		.config = {
+			/* Run ALS sensor in S0 */
+			[SENSOR_CONFIG_EC_S0] = {
+				.odr = 1000,
+			},
+		},
+	},
+
+	[RGB_ALS] = {
+		.name = "RGB Light",
+		.active_mask = SENSOR_ACTIVE_S0_S3,
+		.chip = MOTIONSENSE_CHIP_TCS3400,
+		.type = MOTIONSENSE_TYPE_LIGHT_RGB,
+		.location = MOTIONSENSE_LOC_LID,
+		.drv = &tcs3400_rgb_drv,
+		.drv_data = &g_tcs3400_rgb_data,
+		.port = I2C_PORT_TEST,
+		.addr = TCS3400_I2C_ADDR,
+		.rot_standard_ref = NULL,
+		.default_range = 0x10000,
+		.min_frequency = TCS3400_LIGHT_MIN_FREQ,
+		.max_frequency = TCS3400_LIGHT_MAX_FREQ,
 	},
 };
 unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
@@ -423,6 +485,8 @@ static void board_init(void)
 	gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
 	/* Enable HDMI HPD interrupt. */
 	gpio_enable_interrupt(GPIO_HDMI_CONN_HPD);
+	/* Enable light sensor interrupt. */
+	gpio_enable_interrupt(GPIO_BASE_TCS3400_INT_L);
 	/* Select correct gpio signal for PP5000_A control */
 	board_gpio_set_pp5000();
 }
