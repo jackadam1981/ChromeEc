2c1ad011c5fc3ac5c4e8d45b82f288f4b16a09b0
Yunfei Dong <yunfei.dong@mediatek.com>
2019-03-27 11:33:22 +0800

    WIP: mtk_vcodec:Add the service for vdec h264
    Fix the service to support vdec h264 decode.
    
    BRANCH=none
    BUG=b:124804731
    TEST=build kukui_scp pass.
    
    Change-Id: I2bc424ff577ee12246ffa73e5d73cfde388fe5cf
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>

--- board/kukui_scp/vdec.c
+++ board/kukui_scp/vdec.c
@@ -35,6 +35,9 @@ void vdec_h264_service_init(void) {}
 void vdec_h264_msg_handler(void *data) {}
 #endif
 
+static vdec_msg_handler mtk_vdec_msg_handle[VDEC_MAX];
+static struct mutex vdec_lock;
+
 static void event_vdec_written(struct consumer const *consumer, size_t count)
 {
 	task_wake(TASK_ID_VDEC_SERVICE);
@@ -44,16 +47,20 @@ static void vdec_h264_ipi_handler(int id, void *data, uint32_t len)
 {
 	struct vdec_service rsv_msg;
 
+	if (!len)
+		return;
+
 	rsv_msg.type = VDEC_H264;
 	memcpy(rsv_msg.msg, data, MIN(len, sizeof(rsv_msg.msg)));
-	rsv_msg.handler[VDEC_H264] = &vdec_h264_msg_handler;
 
 	/*
 	 * If there is no other IPI handler touch this queue, we don't need to
 	 * interrupt_disable() or task_disable_irq().
 	 */
+	mutex_lock(&vdec_lock);
 	if (!queue_add_unit(&event_vdec_queue, &rsv_msg))
 		CPRINTS("Could not send vdec %d to the queue.", rsv_msg.type);
+	mutex_unlock(&vdec_lock);
 }
 DECLARE_IPI(IPI_VDEC_H264, vdec_h264_ipi_handler, 1);
 
@@ -64,6 +71,7 @@ void vdec_service_task(void *u)
 	size_t size;
 
 	vdec_h264_service_init();
+	mtk_vdec_msg_handle[VDEC_H264] = vdec_h264_msg_handler;
 
 	while (1) {
 		/*
@@ -71,12 +79,16 @@ void vdec_service_task(void *u)
 		 * Disable IRQ to prevent a clobbered queue.
 		 */
 		task_disable_irq(SCP_IRQ_IPC0);
+		mutex_lock(&vdec_lock);
 		size = queue_remove_unit(&event_vdec_queue, &rsv_msg);
+		mutex_unlock(&vdec_lock);
 		task_enable_irq(SCP_IRQ_IPC0);
 
 		if (!size)
 			task_wait_event(-1);
-		else if (rsv_msg.handler[rsv_msg.type])
-			rsv_msg.handler[rsv_msg.type](rsv_msg.msg);
+		else if (mtk_vdec_msg_handle[rsv_msg.type]) {
+			vdec_h264_msg_handler(rsv_msg.msg);
+		} else
+			CPRINTS("Handler is NULL.\n");
 	}
 }
