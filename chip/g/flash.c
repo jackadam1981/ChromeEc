e1a3e6d2f902e692400aa4d207c13044a7173d0a
Vadim Bendebury <vbendeb@chromium.org>
2019-04-02 12:42:51 -0700

    cr50: add INFO1 RW MAP erasing to CR50_SQA mode
    When building with CR50_SQA set to 1 the resulting image is supposed
    to allow transitioning the H1 chip it is running on from pre-pvt to MP
    track. This should include preserving the Board ID value, but erasing
    the board id flags, and erasing the INFO1 RW map. because older MO
    image could have a less restrictive mask. The reset of the INFO1 space
    should be preserved.
    
    This patch creates a structure describing the layout of the INFO1
    space and modifies the 'eraseflashinfo' command to behave differently
    depending on the build time configuration. In addition to erasing the
    INFO1 RW map:
    
     - when CR50_DEV is set - everything but INFO1 RO map is erased
    
     - when CR50_SQA is set - only board ID flags are erased.
    
    With these modifications the 'eraseflashinfo' command is used instead of
    'gid force_pvt', and previously erased INFO1 RO map is preserved.
    
    BRANCH=none
    BUG=none
    
    TEST=tried running 'eraseflashinfo' in both kinds of images and observed
         desired behavior.
    
    Change-Id: Icf26dc3a4a4bb6fac2fcec630749c81aa46e16ae
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- chip/g/flash.c
+++ chip/g/flash.c
@@ -39,12 +39,11 @@
  */
 
 #include "common.h"
+#include "board_space.h"
 #include "console.h"
 #include "cryptoc/util.h"
 #include "flash.h"
-#include "flash_config.h"
 #include "flash_log.h"
-#include "flash_info.h"
 #include "registers.h"
 #include "shared_mem.h"
 #include "task.h"
@@ -191,7 +190,7 @@ static int do_flash_op(enum flash_op op, int is_info_bank,
 	/* What are we doing? */
 	switch (op) {
 	case OP_ERASE_BLOCK:
-#ifndef CR50_DEV
+#ifndef CR50_RELAXED
 		if (is_info_bank)
 			/* Erasing the INFO bank from the RW section is
 			 * unsupported. */
@@ -502,32 +501,29 @@ void flash_open_ro_window(uint32_t offset, size_t size_b)
 	GWRITE_FIELD(GLOBALSEC, FLASH_REGION6_CTRL, WR_EN, 1);
 }
 
-#ifdef CR50_DEV
+#ifdef CR50_RELAXED
 
 static int command_erase_flash_info(int argc, char **argv)
 {
-	uint32_t *preserved_manufacture_state;
-	const size_t manuf_word_count = FLASH_INFO_MANUFACTURE_STATE_SIZE /
-		sizeof(uint32_t);
 	int i;
-	int rv = EC_ERROR_BUSY;
+	int rv;
+	struct info1_layout *info1;
+	uint32_t *p;
 
-	if (shared_mem_acquire(FLASH_INFO_MANUFACTURE_STATE_SIZE,
-			       (char **)&preserved_manufacture_state) !=
-	    EC_SUCCESS) {
-		ccprintf("Failed to allocate memory for manufacture state!\n");
+	rv = shared_mem_acquire(sizeof(*info1), (char **)&info1);
+	if (rv != EC_SUCCESS) {
+		ccprintf("Failed to allocate memory for info1!\n");
 		return rv;
 	}
 
 	flash_info_read_enable(0, 2048);
 	flash_info_write_enable(0, 2048);
 
-	/* Preserve manufacturing information. */
-	for (i = 0; i < manuf_word_count; i++) {
-		if (flash_physical_info_read_word
-		    (FLASH_INFO_MANUFACTURE_STATE_OFFSET +
-		     i * sizeof(uint32_t),
-		     preserved_manufacture_state + i) != EC_SUCCESS) {
+	/* Read the entire info1. */
+	p = (uint32_t *)info1;
+	for (i = 0; i < (sizeof(*info1)/sizeof(*p)); i++) {
+		if (flash_physical_info_read_word(i * sizeof(*p), p + i)
+		    != EC_SUCCESS) {
 			ccprintf("Failed to read word %d!\n", i);
 			goto exit;
 		}
@@ -544,23 +540,26 @@ static int command_erase_flash_info(int argc, char **argv)
 		goto exit;
 	}
 
-	if (flash_info_physical_write
-	    (FLASH_INFO_MANUFACTURE_STATE_OFFSET,
-	     FLASH_INFO_MANUFACTURE_STATE_SIZE,
-	     (char *)preserved_manufacture_state) != EC_SUCCESS) {
-		ccprintf("Failed to restore manufacture state!\n");
-		goto exit;
-	}
+#ifdef CR50_SQA
+	info1->board_space.bid.flags = 0;
+#else
+	memset(&info1->board_space, 0xff, sizeof(info1->board_space));
+	memset(&info1->manufacture_space, 0xff,
+	       sizeof(info1->manufacture_space));
+#endif
+	memset(info1->rw_info_map, 0xff, sizeof(info1->rw_info_map));
 
-	rv = EC_SUCCESS;
+	rv = flash_info_physical_write(0, sizeof(*info1), (char *)info1);
+	if (rv != EC_SUCCESS)
+		ccprintf("Failed write back info1 contents!\n");
+
  exit:
-	always_memset(preserved_manufacture_state, 0,
-		      FLASH_INFO_MANUFACTURE_STATE_SIZE);
-	shared_mem_release(preserved_manufacture_state);
 	flash_info_write_disable();
+	always_memset(info1, 0, sizeof(*info1));
+	shared_mem_release(info1);
 	return rv;
 }
-DECLARE_CONSOLE_COMMAND(eraseflashinfo, command_erase_flash_info,
-			"",
-			"Erase INFO1 flash space");
+DECLARE_SAFE_CONSOLE_COMMAND(eraseflashinfo, command_erase_flash_info,
+			     "",
+			     "Erase INFO1 flash space");
 #endif
