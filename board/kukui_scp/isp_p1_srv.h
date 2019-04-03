2f7f9d8ad545981c39bd49a2938af9dfa702a3f6
Jungo Lin <jungo.lin@mediatek.com>
2019-04-03 17:21:47 +0800

    WIP: mtk_isp: Add the service for isp P1 driver
    v1:
    Add new EC task to support ISP P1 driver service.
    The real isp_msg_handler is implemented in the private-mt8183 repo.
    
    v2:
    Reduce task stack size usage to 768.
    
    Task Ready Name         Events      Time (s)  StkUsed
    5          ISP_SERVICE  00000000    0.006376  744/768
    
    V3:
    Update the task stack size usage to 1024 for IT testing & AAO/AFO change
    
    BRANCH=none
    BUG=none
    TEST=build kukui_scp pass & check ISP_SERVICE is available
    
    Change-Id: I0d3927b0ee6cc9fa738a063939bcf3d03170493e
    Signed-off-by: Jungo Lin <Jungo.Lin@mediatek.com>

---
+++ board/kukui_scp/isp_p1_srv.h
@@ -0,0 +1,19 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_ISP_P1_SRV_H
+#define __CROS_EC_ISP_P1_SRV_H
+
+#include "chip/mt_scp/ipi_chip.h"
+
+struct isp_msg_service {
+	unsigned char id;
+	unsigned char msg[160];
+};
+
+/* Functions provided by private overlay. */
+void isp_msg_handler(void *data);
+
+#endif /* __CROS_EC_ISP_P1_SRV_H */
