4d9db74fd8da6e02f562300771211e98b40be9c8
Scott Collyer <scollyer@google.com>
2019-04-02 17:35:02 -0700

    tcpm: Refactor tcpc_config to include a flags field
    tcpc_config contained a field for both the alert polarity and open
    drain/push pull configuration. There is also a possible difference in TCPC
    reset polarity. Instead of adding yet another field to describe this
    configuration, it would be better to convert alert polairty, open
    drain and reset polarity into a single flags field.
    
    This CL modifies the tcpc_config struct to use a single flags field
    and adds defines for what existing flag options can be.
    
    BUG=b:130194031
    BRANCH=none
    TEST=make -j buildall
    
    Change-Id: Ifb7e7604edb7021fb2d36ee279049eb52fefc99e
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Signed-off-by: Furquan Shaikh <furquan@google.com>

--- include/usb_pd_tcpm.h
+++ include/usb_pd_tcpm.h
@@ -265,22 +265,17 @@ struct tcpm_drv {
 #endif
 };
 
-enum tcpc_alert_polarity {
-	TCPC_ALERT_ACTIVE_LOW,
-	TCPC_ALERT_ACTIVE_HIGH,
-};
-
-enum tcpc_alert_open_drain {
-	TCPC_ALERT_PUSH_PULL = 0,
-	TCPC_ALERT_OPEN_DRAIN,
-};
+/* Macros for tcpc_config_t flags field. */
+#define TCPC_FLAGS_ALERT_ACTIVE_HIGH	BIT(0)
+#define TCPC_FLAGS_ALERT_OD		BIT(1)
+#define TCPC_FLAGS_RESET_ACTIVE_HIGH	BIT(2)
 
 struct tcpc_config_t {
 	int i2c_host_port;
 	int i2c_slave_addr;
 	const struct tcpm_drv *drv;
-	enum tcpc_alert_polarity pol;
-	enum tcpc_alert_open_drain od;
+	/* See TCPC_FLAGS_* above */
+	uint32_t flags;
 };
 
 /**
