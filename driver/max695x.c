804c89c30765ad86a69d91add68351fbd4fd3747
Ayushee <ayushee.shah@intel.com>
2019-04-05 11:39:51 -0700

    driver/max695x: Add 7 segment display driver
    Port80 information and power sequencing states can be displayed on the
    7-segment so that we can observe its information without connecting a
    debugger. Adding this CL to initialize and write to the 7-segment display.
    
    For port80, 80(data) gets displayed on the seven segment, for the
    power states, EC(state) gets displayed on the seven segment.
    
    The MAX6958/MAX6959 is a compact multiplexed common - cathode display
    designed to interface microprocessors with seven - segment numerical LED
    digits or to discrete LEDs via a 2-wire serial interface compatible with
    SMBusTM and I2C. The 2-wire serial interface uses fixed logic thresholds
    of 0.8V/2.1V when the display driver is powered from a 5V supply for
    compatibility with 2.5V and 3.3V systems.
    
    BRANCH=None
    BUG=b:130738086
    TEST=Manually tested on intelrvp, able to write to the 7 segment display
    
    Change-Id: I82ba3aca40640e094463669aa1ff106ce7b99b7b
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

---
+++ driver/max695x.c
@@ -0,0 +1,103 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * MAX6958/MAX6959 7-Segment LED Display Driver
+ */
+
+#include "common.h"
+#include "console.h"
+#include "display_7seg.h"
+#include "driver/max695x.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "util.h"
+
+static inline int max695x_i2c_write8(uint8_t offset, uint8_t data)
+{
+	return i2c_write8(I2C_PORT_PORT80, PORT80_I2C_ADDR,
+			   offset, (int)data);
+}
+
+static inline int max695x_i2c_write_blk(uint8_t offset, uint8_t *data, int len)
+{
+	/*
+	 * The address pointer stored in the MAX695x increments after
+	 * each data byte is written unless the address equals 01111111
+	 */
+	return i2c_write_block(I2C_PORT_PORT80, PORT80_I2C_ADDR,
+			   offset, data, len);
+}
+
+int seven_seg_display_write(enum seven_seg_module_display module, int data)
+{
+	int i;
+	uint8_t buf[4];
+
+	/*
+	 * Append the module name for first 2 segments
+	 *
+	 * Bytes 0-1 : Module name
+	 *	0xEC : EC
+	 *	0x80 : PORT80
+	 * Bytes 2-3 : Data
+	 *
+	 * For console Command Bytes 0-4 : Data
+	 */
+	data = module << 8 | data;
+
+	/*
+	 * In hexadecimal code-decode mode, the decoder looks only at
+	 * the lower nibble of the data in the digit register (D3–D0),
+	 * disregarding bits D7–D4. Hence, preparing the hexadecimal
+	 * buffer to be sent.
+	 */
+	for (i = 0; i < 4; i++)
+		buf[3 - i] = (data >> (i << 2)) & 0xF;
+	return max695x_i2c_write_blk(MAX695X_DIGIT0_ADDR, buf, ARRAY_SIZE(buf));
+}
+
+/**
+ * Initialise MAX656x 7-segment display.
+ */
+static void max695x_init(void)
+{
+	uint8_t buf[4] = {
+		[0] = MAX695X_DECODE_MODE_HEX_DECODE,
+		[1] = MAX695X_INTENSITY_MEDIUM,
+		[2] = MAX695X_SCAN_LIMIT_4,
+		[3] = MAX695X_CONFIG_OPR_NORMAL
+	};
+	max695x_i2c_write_blk(MAX695X_REG_DECODE_MODE, buf, ARRAY_SIZE(buf));
+}
+DECLARE_HOOK(HOOK_INIT, max695x_init, HOOK_PRIO_DEFAULT);
+
+static void max695x_shutdown(void)
+{
+	max695x_i2c_write8(MAX695X_REG_CONFIG,
+			   MAX695X_CONFIG_OPR_SHUTDOWN);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, max695x_shutdown, HOOK_PRIO_DEFAULT);
+
+#ifdef CONFIG_CMD_SEVEN_SEG_DISPLAY
+static int console_command_max695x_write(int argc, char **argv)
+{
+	char *e;
+	int rv,  val;
+
+	if (argc < 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	/* Get value to be written to the seven segment display*/
+	val = strtoi(argv[1], &e, 0);
+	if (*e || val < 0 || val > 0x10000)
+		return EC_ERROR_PARAM1;
+
+	rv = seven_seg_display_write(SEVEN_SEG_CONSOLE_DISPLAY, val);
+
+	return rv;
+}
+DECLARE_CONSOLE_COMMAND(seg, console_command_max695x_write,
+			"<val>",
+			"Write to 7 segment display in hex");
+#endif
