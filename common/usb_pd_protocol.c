60ca26108441e33e54b974ec19256aa494f30fc5
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2019-04-08 15:37:53 +0800

    (TEST) common/usb_pd_protocol: power role swap
      After port partner accept power role swap, the power role field
    of next two PS_RDY packet should be the new source and sink role.
    See PD3.0 spec Figure 8-11, 12.
    
      The new Source Shall Not send the Source_Capabilities Message
    earlier than tSwapSourceStart after the last bit of the EOP of
    GoodCRC Message sent in response to the PS_RDY Message sent by
    the new Source indicating that its power supply is ready. See
    PD3.0 spec ch.6.6.8.1.
    
    reef_it8320 CP.E4: bd9995 polling Vbus = 0(actual 1), then cc
    disconnect when in PD_STATE_SNK_TRANSITION state.
    
    BUG=none
    BRANCH=none
    TEST=Ellisys USB-PD compliance test: PC.E2, CP.E4
    
    Change-Id: I9bab410206fb10efe38f67e508f8a4210918302e
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -1689,8 +1689,6 @@ static void handle_ctrl_request(int port, uint16_t head,
 		} else if (pd[port].task_state == PD_STATE_SRC_SWAP_STANDBY) {
 			/* reset message ID and swap roles */
 			pd[port].msg_id = 0;
-			pd_set_power_role(port, PD_ROLE_SINK);
-			pd_update_roles(port);
 			/*
 			 * Give the state machine time to read VBUS as high.
 			 * Note: This is empirically determined, not strictly
@@ -3422,22 +3420,30 @@ void pd_task(void *u)
 		case PD_STATE_SRC_SWAP_STANDBY:
 			/* Send PS_RDY to let sink know our power is off */
 			if (pd[port].last_state != pd[port].task_state) {
+				/*
+				 * Switch to Rd and swap roles to sink,
+				 * see Figure 8-11, 12.
+				 */
+				tcpm_set_cc(port, TYPEC_CC_RD);
+				pd_set_power_role(port, PD_ROLE_SINK);
+				pd_update_roles(port);
 				/* Send PS_RDY */
 				res = send_control(port, PD_CTRL_PS_RDY);
 				if (res < 0) {
+					tcpm_set_cc(port, TYPEC_CC_RP);
+					pd_set_power_role(port, PD_ROLE_SOURCE);
+					pd_update_roles(port); /*debnce st. do*/
 					timeout = 10*MSEC;
 					set_state(port,
 						  PD_STATE_SRC_DISCONNECTED);
 					break;
 				}
-				/* Switch to Rd and swap roles to sink */
-				tcpm_set_cc(port, TYPEC_CC_RD);
-				pd_set_power_role(port, PD_ROLE_SINK);
 				/* Wait for PS_RDY from new source */
 				set_state_timeout(port,
 						  get_time().val +
 						  PD_T_PS_SOURCE_ON,
 						  PD_STATE_SNK_DISCONNECTED);
+				/*error recover should start from unattach.src*/
 			}
 			break;
 		case PD_STATE_SUSPENDED: {
@@ -3888,14 +3894,22 @@ void pd_task(void *u)
 			}
 			break;
 		case PD_STATE_SNK_SWAP_COMPLETE:
-			/* Send PS_RDY and change to source role */
+			/*
+			 * Send PS_RDY and change to source role, see
+			 * Figure 8-11, 12.
+			 */
+			pd_set_power_role(port, PD_ROLE_SOURCE);
+			pd_update_roles(port);
 			res = send_control(port, PD_CTRL_PS_RDY);
 			if (res < 0) {
 				/* Restore Rd */
 				tcpm_set_cc(port, TYPEC_CC_RD);
+				pd_set_power_role(port, PD_ROLE_SINK);
+				pd_update_roles(port);/*debounce state will do*/
 				pd_power_supply_reset(port);
 				timeout = 10 * MSEC;
 				set_state(port, PD_STATE_SNK_DISCONNECTED);
+				/*error recover should start from unattach.src*/
 				break;
 			}
 
@@ -3903,9 +3917,10 @@ void pd_task(void *u)
 			snk_cap_count = PD_SNK_CAP_RETRIES+1;
 			caps_count = 0;
 			pd[port].msg_id = 0;
-			pd_set_power_role(port, PD_ROLE_SOURCE);
-			pd_update_roles(port);
-			set_state(port, PD_STATE_SRC_DISCOVERY);
+			/* Wait for not too early send SRC_Cap, ch.6.6.8 */
+			set_state_timeout(port, get_time().val +
+					PD_T_SWAP_SRC_START,
+					PD_STATE_SRC_DISCOVERY);
 			timeout = 10*MSEC;
 			break;
 #ifdef CONFIG_USBC_VCONN_SWAP
