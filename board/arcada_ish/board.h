fb2fb8f86d11546844a492e20e602838f2f4479f
Enrico Granata <egranata@chromium.org>
2019-04-08 13:37:25 -0700

    arcada: do not expose sensor FIFO
    This CL disables the ACCEL_FIFO feature from arcada_ish,
    in turn preventing the kernel from loading the sensor_ring
    driver and the sensor FIFO from being exposed to userspace.
    
    This is done in order to help mitigate some issue with the ISH
    stack on Arcada.
    
    BUG=b:129937881
    TEST=lsmod shows sensor_ring is not loaded
    
    Change-Id: Iac9c323e4b35b28adf18506a01259315d7e945df
    Signed-off-by: Enrico Granata <egranata@chromium.org>

--- board/arcada_ish/board.h
+++ board/arcada_ish/board.h
@@ -29,7 +29,6 @@
 #define CONFIG_ACCELGYRO_LSM6DSM /* Lid sensor: LSM6DS3 (uses LSM6DSM driver) */
 
 #define CONFIG_ACCEL_INTERRUPTS
-#define CONFIG_ACCEL_FIFO 256
 #define CONFIG_ACCEL_FIFO_THRES (CONFIG_ACCEL_FIFO / 3)
 /* Sensors without hardware FIFO are in forced mode */
 #define CONFIG_ACCEL_FORCE_MODE_MASK  (1 << BASE_ACCEL)
