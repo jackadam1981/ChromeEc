5e687766f50a27b899f841ab3c7e81f10ef33ee7
Jack Rosenthal <jrosenth@chromium.org>
2019-04-09 11:31:15 -0600

    ish: wip no cred software workaround
    This is a workaround for the no cred issue in ish. WIP and looking for
    feedback.
    
    BRANCH=none
    BUG=b:129937881,b:129159505
    TEST=start.py 1000 on arcada
    TEST=Accelerometer test app in ARC++
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I667dee38f4c81ebd48fa1998b2d26f96c01e559c

--- chip/ish/heci.c
+++ chip/ish/heci.c
@@ -285,7 +285,16 @@ int heci_send_msg(const heci_handle_t handle, uint8_t *buf,
 	}
 
 	if (!connect->flow_ctrl_creds) {
-		CPRINTF("no cred\n");
+		CPRINTS("no cred");
+
+		if (REG32(IPC_PISR) & BIT(0)) {
+			REG32(IOAPIC_EOI_REG) = ISH_IPC_VEC;
+			CPRINTS("no cred sw workaround issued");
+		} else if (REG32(IPC_BUSY_CLEAR) & BIT(0)) {
+			REG32(IOAPIC_EOI_REG) = ISH_IPC_ISH2HOST_CLR_VEC;
+			CPRINTS("no cred sw workaround issued");
+		}
+
 		ret = -HECI_ERR_NO_CRED_FROM_CLIENT_IN_HOST;
 		goto err_locked;
 	}
