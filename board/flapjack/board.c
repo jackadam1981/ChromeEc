63e5f9c3b0f04b20b99023d1b59b96b2281a41cf
YH Lin <yueherngl@google.com>
2019-04-10 09:05:54 -0700

    flapjack: read LCM_ID and put into sku_id
    Read/return LCM_ID (from P0d onward) as part of sku_id while
    preserving the compatibility of older boards.
    
    BUG=b:129569858
    TEST=LCM info should only be printed from P0d onward.
    BRANCH=none
    
    Change-Id: I053982f27fc7e6cc95c626b2d40bddff9ba37bfc
    Signed-off-by: YH Lin <yueherngl@chromium.org>

--- board/flapjack/board.c
+++ board/flapjack/board.c
@@ -50,6 +50,12 @@
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
+static const struct mv_to_id panels[] = {
+	{ PANEL_BOE_HIMAX8279D10P,	98 },
+	{ PANEL_BOE_HIMAX8279D8P,	280 },
+};
+BUILD_ASSERT(ARRAY_SIZE(panels) < PANEL_COUNT);
+
 uint16_t board_version;
 uint8_t oem;
 uint32_t sku;
@@ -75,9 +81,27 @@ static void board_setup_panel(void)
 	uint8_t channel;
 	uint8_t dim;
 	int rv = 0;
+	enum panel_id pnl_id = (sku >> PANEL_ID_BIT_POSITION) & 0xf;
 
-	channel = sku & SKU_ID_PANEL_SIZE_MASK ? 0xfe : 0xfa;
-	dim = sku & SKU_ID_PANEL_SIZE_MASK ? 0xc4 : 0xc8;
+	if (board_version >= 3) {
+		switch (pnl_id) {
+		case PANEL_BOE_HIMAX8279D8P:
+			channel = 0xfa;
+			dim = 0xc8;
+			break;
+		case PANEL_BOE_HIMAX8279D10P:
+			channel = 0xfe;
+			dim = 0xc4;
+			break;
+		default:
+			CPRINTS("Unrecognized panel %d\n", pnl_id);
+			return;
+		}
+	} else {
+		/* TODO: to be removed once the boards are deprecated. */
+		channel = sku & SKU_ID_PANEL_SIZE_MASK ? 0xfe : 0xfa;
+		dim = sku & SKU_ID_PANEL_SIZE_MASK ? 0xc4 : 0xc8;
+	}
 
 	rv |= i2c_write8(I2C_PORT_CHARGER, RT946X_ADDR, MT6370_BACKLIGHT_BLEN,
 		channel);
@@ -87,6 +111,18 @@ static void board_setup_panel(void)
 		0xac);
 	if (rv)
 		CPRINTS("Board setup panel failed\n");
+}
+
+static enum panel_id board_get_panel_id(void)
+{
+	enum panel_id pnl_id = PANEL_UNKNOWN;
+	int id = board_read_id(ADC_LCM_ID, panels, ARRAY_SIZE(panels));
+
+	if (id != ADC_READ_ERROR)
+		pnl_id = (enum panel_id)id;
+
+	CPRINTS("LCM ID: %d", pnl_id);
+	return pnl_id;
 }
 
 static void cbi_init(void)
@@ -103,6 +139,12 @@ static void cbi_init(void)
 
 	if (cbi_get_sku_id(&val) == EC_SUCCESS)
 		sku = val;
+
+	if (board_version >= 3) {
+		/* Embed LCM_ID in sku_id bit[19-16] */
+		sku |= ((board_get_panel_id() & 0xf) << PANEL_ID_BIT_POSITION);
+	}
+
 	CPRINTS("SKU: 0x%08x", sku);
 }
 DECLARE_HOOK(HOOK_INIT, cbi_init, HOOK_PRIO_INIT_I2C + 1);
@@ -122,7 +164,7 @@ static void gauge_interrupt(enum gpio_signal signal)
 /******************************************************************************/
 /* ADC channels. Must be in the exactly same order as in enum adc_channel. */
 const struct adc_t adc_channels[] = {
-	[ADC_BOARD_ID] = {"BOARD_ID", 3300, 4096, 0, STM32_AIN(10)},
+	[ADC_LCM_ID] = {"LCM_ID", 3300, 4096, 0, STM32_AIN(10)},
 	[ADC_EC_SKU_ID] = {"EC_SKU_ID", 3300, 4096, 0, STM32_AIN(8)},
 	[ADC_BATT_ID] = {"BATT_ID", 3300, 4096, 0, STM32_AIN(7)},
 	[ADC_USBC_THERM] = {"USBC_THERM", 3300, 4096, 0, STM32_AIN(14)},
