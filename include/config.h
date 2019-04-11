e6a29ae0c132e38f7de0913e3e11a314ed0775da
Diana Z <dzigterman@chromium.org>
2019-04-11 16:06:31 -0600

    USB PD: Refrain from resets if battery is low
    This change introduces a new clause to the SNK_DISCOVERY state to
    prevent reset timers from starting when a battery is reporting below a
    configured level, which indicates the point at which the battery is
    capable of withstanding the potential loss of Vbus.
    
    Once the battery has charged enough, a timer for soft reset will begin
    to trigger PD negotiation to start over if it hasn't been able to
    complete.
    
    BUG=b:128935567
    BRANCH=octopus
    TEST=verified hard reset doesn't occur on grabbiter when system is locked
    and coming back from battery cutoff without enough battery, verified
    soft followed by hard resets after the battery had charged
    
    Change-Id: Ib885d2b55acacad7ef1375572b0e6d243e639bdf
    Signed-off-by: Diana Z <dzigterman@chromium.org>

--- include/config.h
+++ include/config.h
@@ -3665,6 +3665,12 @@
 /* Set the default minimum battery percentage for Try.Src to be enabled */
 #define CONFIG_USB_PD_TRY_SRC_MIN_BATT_SOC 1
 
+/*
+ * Set the minimum battery percentage to allow a PD port to send resets as a
+ * sink (and risk a hard reset, losing Vbus)
+ */
+#undef CONFIG_USB_PD_RESET_MIN_BATT_SOC
+
 /* Alternative configuration keeping only the TX part of PHY */
 #undef CONFIG_USB_PD_TX_PHY_ONLY
 
