7375aa27835d710099a72c0eb427104bc9042e56
Sam Hurst <shurst@google.com>
2019-04-15 07:16:11 -0700

    pd: USB PD 3.0 Stack
    Implements release 1.3 of the USB Type-C Specification and revision
    3.0, version 1.2 of the USB Power Delivery Specification.
    
    Type-C Features:
    DRP with Accessory, Try.SRC, and Charge Through VCONN powered
    device support
    
    Power Delivery 3.0 Features:
    Not_supported control message - Inform a port partner that a particular
                                    message is not supported.
    Cable communication - Only the VCONN source can communicate with the
                          cable plug.
    Source-coordinated collision avoidance - New scheme to avoid collisions
                                             caused when both source and sink
                                             want to send messages.
    Battery status data message - Reporting of the battery state of charge.
    Battery capabilities extended message - Return the following information.
    
    DO NOT MERGE
    
    Change-Id: Id3318e089bec01a2e99fc518706eaa82804331d7

--- common/usb_tc_sm.c
+++ common/usb_tc_sm.c
@@ -90,6 +90,8 @@ BUILD_ASSERT(ARRAY_SIZE(tc_state_names) == TC_STATE_COUNT);
 #include "usb_tc_ctvpd_sm.h"
 #elif defined(CONFIG_USB_TYPEC_VPD)
 #include "usb_tc_vpd_sm.h"
+#elif defined(CONFIG_USB_TYPEC_DRP_ACC_TRYSRC)
+#include "usb_tc_drp_acc_trysrc_sm.h"
 #else
 #error "A USB Type-C State Machine must be defined."
 #endif
@@ -104,6 +106,11 @@ int tc_get_power_role(int port)
 int tc_get_data_role(int port)
 {
 	return tc[port].data_role;
+}
+
+void tc_set_power_role(int port, int role)
+{
+	tc[port].power_role = role;
 }
 
 void tc_set_timeout(int port, uint64_t timeout)
