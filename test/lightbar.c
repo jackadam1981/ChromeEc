d2adb5639b269d3d0c97cdad1a7daab9f73e7521
Jett Rink <jettrink@chromium.org>
2019-04-16 13:43:03 -0600

    tests: make lighbar test easier to debug
    This test was failing and it is easier to comment out tests that
    are passing to debug. I needed to add a precondition so each test
    would pass on its own. I also needed to remove the static modifier
    to allow each test to be comment out.
    
    BRANCH=none
    BUG=none
    TEST=builds and passes
    
    Change-Id: Ib2a7c0948aee363e1552835222a9700225993e46
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- test/lightbar.c
+++ test/lightbar.c
@@ -11,6 +11,8 @@
 #include "timer.h"
 #include "util.h"
 
+/* This allows tests to be easily commented out in run_test for debugging */
+#define test_static static __attribute__((unused))
 
 static int get_seq(void)
 {
@@ -53,7 +55,7 @@ static int set_seq(int s)
 	return EC_RES_SUCCESS;
 }
 
-static int test_double_oneshots(void)
+test_static int test_double_oneshots(void)
 {
 	/* Start in S0 */
 	TEST_ASSERT(set_seq(LIGHTBAR_S0) == EC_RES_SUCCESS);
@@ -125,7 +127,7 @@ static int test_double_oneshots(void)
 	return EC_SUCCESS;
 }
 
-static int test_oneshots_norm_msg(void)
+test_static int test_oneshots_norm_msg(void)
 {
 	/* Revert to the next state when interrupted with a normal message. */
 	enum lightbar_sequence seqs[] = {
@@ -154,7 +156,7 @@ static int test_oneshots_norm_msg(void)
 	return EC_SUCCESS;
 }
 
-static int test_stop_timeout(void)
+test_static int test_stop_timeout(void)
 {
 	int i;
 
@@ -188,7 +190,7 @@ static int test_stop_timeout(void)
 	return EC_SUCCESS;
 }
 
-static int test_oneshots_timeout(void)
+test_static int test_oneshots_timeout(void)
 {
 	/* These should revert to the previous state after running */
 	enum lightbar_sequence seqs[] = {
@@ -215,7 +217,7 @@ static int test_oneshots_timeout(void)
 	return EC_SUCCESS;
 }
 
-static int test_transition_states(void)
+test_static int test_transition_states(void)
 {
 	/* S5S3 */
 	TEST_ASSERT(set_seq(LIGHTBAR_S5S3) == EC_RES_SUCCESS);
@@ -240,7 +242,7 @@ static int test_transition_states(void)
 	return EC_SUCCESS;
 }
 
-static int test_stable_states(void)
+test_static int test_stable_states(void)
 {
 	int i;
 
@@ -301,7 +303,7 @@ int lid_is_open(void)
 	return 1;
 }
 
-static int test_als_lightbar(void)
+test_static int test_als_lightbar(void)
 {
 	int lux_data[] = { 500, 100, 35, 15, 30, 35, 55, 70, 55, 100 };
 	int exp_gcid[] = {   0,   0,  1,  2,  2,  2,  1,  0,  0,   0 };
@@ -322,6 +324,9 @@ static int test_als_lightbar(void)
 
 void run_test(void)
 {
+	/* Ensure tasks are started before running tests */
+	usleep(SECOND);
+
 	RUN_TEST(test_stable_states);
 	RUN_TEST(test_transition_states);
 	RUN_TEST(test_oneshots_timeout);
