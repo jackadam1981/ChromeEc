ac61d51f97f116c781dd15a308a24c31638e8692
Jett Rink <jettrink@chromium.org>
2019-04-16 12:43:58 -0600

    tasks: convert TASK_EVENT_CUSTOM macro to bit
    We should ensure that all custom task definition are non-zero and fit
    with the globally defined events. Add compile time check and change
    semantics to specify bit number (instead of making all callers use the
    BIT macro).
    
    BRANCH=none
    BUG=none
    TEST=builds
    Cq-Depend:chrome-internal:1178968,chrome-internal:1178952
    
    Change-Id: I5e1d1312382d200280c548e9128e53f4eddd3e61
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- include/task.h
+++ include/task.h
@@ -13,8 +13,8 @@
 #include "task_id.h"
 
 /* Task event bitmasks */
-/* Tasks may use the bits in TASK_EVENT_CUSTOM for their own events */
-#define TASK_EVENT_CUSTOM(x)	(x & 0x0001ffff)
+/* Tasks may use the bits in TASK_EVENT_CUSTOM_BIT for their own events */
+#define TASK_EVENT_CUSTOM_BIT(x) BUILD_CHECK_INLINE(BIT(x), BIT(x) & 0x1ffff)
 
 /* Used to signal that IPC layer is available for sending new data */
 #define TASK_EVENT_IPC_READY	BIT(17)
