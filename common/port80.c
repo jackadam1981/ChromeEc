a685f0900339a667873b25e3c628668aa3c86db1
Ayushee <ayushee.shah@intel.com>
2019-04-17 13:14:13 -0700

    7-seg display: Add config to display port80 msg and power states
    Adding this CL to display port80 message and power states of EC & SOC
    on the 7-segment display.
    
    BRANCH=None
    BUG=b:130738086
    TEST=Manually tested on intelrvp, able to verify the power states
    and port80 message displayed on the 7 segment display
    
    Change-Id: I4437cfcd60662c8637e406e425f98fad1a4ba7ed
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- common/port80.c
+++ common/port80.c
@@ -7,6 +7,7 @@
 
 #include "common.h"
 #include "console.h"
+#include "display_7seg.h"
 #include "hooks.h"
 #include "host_command.h"
 #include "port80.h"
@@ -109,6 +110,11 @@ static void port80_dump_buffer(void)
 			ccprintf(" %02x", e);
 		}
 	}
+#ifdef CONFIG_SEVEN_SEG_DISPLAY
+	/* Display last msg of port80 on 7 segment*/
+	display_7seg_write(SEVEN_SEG_PORT80_DISPLAY,
+			   history[(i-1) % ARRAY_SIZE(history)]);
+#endif
 	ccputs(" <--new\n");
 }
 
