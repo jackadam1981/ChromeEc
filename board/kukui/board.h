daf5209355c4c837854e9b76498c9602e5928fdf
Yilun Lin <yllin@google.com>
2019-04-19 13:12:01 +0800

    kukui: Enable BC12 detection by GPIO_BC12_DET_EN.
    Kukui's BC12 detection is controlled by GPIO_BC12_DET_EN.
    
    We have to
    - Assert GPIO_BC12_DET_EN on port unplugged to recognize BC12 charger
    - Deassert GPIO_BC12_DET_EN on port plugged to enumerate USB devices
    
    TEST=Test with https://crrev.com/c/1408751/1, and see it is able to
         recognize Apple 2.4A charger and 5V2A DCP charger.
    TEST=Boot w/ USB hub plugged (w/ and w/o external charger on the hub)
         and see USB devices are enumerated.
    BUG=b:122866184
    BRANCH=None
    
    Change-Id: I5b3362305361c0c950288fc83072e9bc79082c08
    Signed-off-by: Yilun Lin <yllin@google.com>

--- board/kukui/board.h
+++ board/kukui/board.h
@@ -77,6 +77,7 @@
 #define CONFIG_CHARGER_OTG
 #define CONFIG_USB_CHARGER
 #define CONFIG_USB_MUX_VIRTUAL
+#define CONFIG_BC12_DETECT_GPIO
 
 /* Increase tx buffer size, as we'd like to stream EC log to AP. */
 #undef CONFIG_UART_TX_BUF_SIZE
@@ -126,6 +127,9 @@
 #define CONFIG_USBC_VCONN
 #define CONFIG_USBC_VCONN_SWAP
 #define CONFIG_USB_PD_COMM_LOCKED
+
+/* BC12 Config */
+#define CONFIG_CHARGER_MT6370_BC12_DETECT_GPIO
 
 #define CONFIG_BATTERY_CUT_OFF
 #define CONFIG_BATTERY_PRESENT_CUSTOM
@@ -211,7 +215,6 @@ enum adc_channel {
 	ADC_EC_SKU_ID,
 	ADC_BATT_ID,
 	ADC_POGO_ADC_INT_L,
-	ADC_USBC_THERM,
 	ADC_CH_COUNT
 };
 
