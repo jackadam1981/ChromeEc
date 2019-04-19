a46503ebf598949cd689081d9cc9084d9cceea12
Ting Shen <phoenixshen@google.com>
2019-04-19 14:21:02 +0800

    DO-NO-MERGE: debug message
    Change-Id: I8bda0c6227da6654a1eb3f0516d7f8a53465cf7f

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -559,6 +559,7 @@ static void charge_manager_get_best_charge_port(int *new_port,
 
 				candidate_port_power =
 					POWER(available_charge[i][j]);
+				CPRINTS("port (%d, %d) power %d\n", j, i, candidate_port_power);
 
 				/* Select if no supplier chosen yet. */
 				if (supplier == CHARGE_SUPPLIER_NONE ||
