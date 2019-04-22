357b98d2e74896b27c09f07d6ba04f058ef2f63d
Sam Hurst <shurst@google.com>
2019-04-22 11:12:08 -0700

    vif: Update VIF generator to Revision 1.40, Version 1.0 of the spec
    Update the Vendor Information File generator so that it complies
    with Revision 1.40, Version 1.0 of the spec.
    
    BUG=b:131087690
    BRANCH=none
    TEST=manual
      Generate VIF for Atlas and checked fields.
    
    Signed-off-by: Sam Hurst <shurst@chromium.org>
    
    Change-Id: Iaa1eaf1f01f9d36ad3afd2818ebe81359b8531f6

--- include/config.h
+++ include/config.h
@@ -3694,6 +3694,46 @@
 #undef CONFIG_USB_PD_DECODE_SOP
 
 /*
+ * The following two macros are ASCII text strings that matches what appears
+ * in the USB-IF Product Registration form for this device. This macro is used
+ * during VIF generation and is to generate the product name in the
+ * USB Integrator’s List.
+ */
+#undef CONFIG_USB_PD_MODEL_PART_NUMBER
+#undef CONFIG_USB_PD_PRODUCT_REVISION
+
+/*
+ * A text string, provided by the USB-IF. This macro is used during VIF
+ * generation.
+ */
+#undef CONFIG_USB_PD_TID
+
+/*
+ * An ASCII text string that must correspond with the port label given on the
+ * device picture submitted to USB-IF by the Vendor along with the VIF. This
+ * macro is used during VIF generation.
+ */
+#undef CONFIG_USB_PD_PORT_LABEL
+
+/*
+ * Define if Get_Manufacturer_Info request PD message is supported.
+ * Used during VIF generation.
+ */
+#undef CONFIG_USB_PD_MANUFACTURE_INFO
+
+/*
+ * Define if both Security_Request and Security_Response PD messages are
+ * supported. Used during VIF generation.
+ */
+#undef CONFIG_USB_PD_SECURITY_MSGS
+
+/*
+ * The number of non-removable batteries in the device. Used duing VIF
+ * generation.
+ */
+#undef CONFIG_NUM_FIXED_BATTERIES
+
+/*
  * Track VBUS level in TCPC module. This will only be needed if we're acting
  * as an external TCPC.
  */
@@ -3853,19 +3893,6 @@
 
 /* USB Device version of product */
 #undef CONFIG_USB_BCD_DEV
-
-/*
- * Used during generation of VIF for USB Type-C Compliance Testing.
- * Indicates whether the UUT can communicate with USB 2.0 or USB 3.1 as a host
- * or as the Downstream Facing Port of a hub.
- */
-#undef CONFIG_VIF_TYPE_C_CAN_ACT_AS_HOST
-
-/*
- * Used during generation of VIF for USB Type-C Compliance Testing.
- * Indicates whether the UUT has a captive cable.
- */
-#undef CONFIG_VIF_CAPTIVE_CABLE
 
 /*****************************************************************************/
 
