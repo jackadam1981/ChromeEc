0052f5b56523e21e6e78cdf85bb6d7df6c69d6d4
Daisuke Nojiri <dnojiri@chromium.org>
2019-04-24 23:15:38 +0000

    Revert "usb-pd: Exit DP mode before shutdown"
    This reverts commit a1fa5af5da8f5209ce64428481d6916ca226a340.
    
    Reason for revert: See CL:1583221
    
    Original change's description:
    > usb-pd: Exit DP mode before shutdown
    >
    > On start-up, the tcpm tries to discover identity and enter amodes.
    > When system reboots, this makes already entered modes re-entered.
    > This causes some devices to malfunction.
    >
    > This patch makes the tcpm exit DP mode if it's already entered.
    >
    > Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    >
    > BUG=chromium:799535,b:79185392
    > BRANCH=none
    > TEST=Verify picture is displayed on external monitor after
    > suspend/resume, shutdown/power-on via D6000, Hoho, Aplle HDMI adapter,
    > StarTech DP adapter, Dingdong with Vayne.
    >
    > Change-Id: I1fd577e12eaa5a81a0ae0242d38863dc211ac8bc
    > Reviewed-on: https://chromium-review.googlesource.com/1185495
    > Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    > Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    > Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    Bug: chromium:799535, b:79185392
    Change-Id: Ibede857b5e8d0d9d1ca921d9e536ecc2e51bac0e

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -1903,20 +1903,6 @@ void pd_set_dual_role(int port, enum pd_dual_role_states state)
 			       PD_EVENT_UPDATE_DUAL_ROLE, 0);
 }
 
-static void exit_dp_mode(int port)
-{
-	int opos = pd_alt_mode(port, USB_SID_DISPLAYPORT);
-	if (opos <= 0)
-		return;
-	CPRINTS("C%d Exiting DP mode", port);
-	if (!pd_dfp_exit_mode(port, USB_SID_DISPLAYPORT, opos))
-		return;
-	pd_send_vdm(port, USB_SID_DISPLAYPORT,
-		    CMD_EXIT_MODE | VDO_OPOS(opos), NULL, 0);
-	pd_vdm_send_state_machine(port);
-	/* Have to wait for ACK */
-}
-
 void pd_update_dual_role_config(int port)
 {
 	/*
@@ -2419,8 +2405,6 @@ void pd_task(void *u)
 		evt = task_wait_event(timeout);
 
 #ifdef CONFIG_USB_PD_DUAL_ROLE
-		if (evt & PD_EVENT_DP_DISCONNECT)
-			exit_dp_mode(port);
 		if (evt & PD_EVENT_UPDATE_DUAL_ROLE)
 			pd_update_dual_role_config(port);
 #endif
@@ -3772,11 +3756,8 @@ DECLARE_HOOK(HOOK_CHIPSET_STARTUP, pd_chipset_startup, HOOK_PRIO_DEFAULT);
 static void pd_chipset_shutdown(void)
 {
 	int i;
-	for (i = 0; i < CONFIG_USB_PD_PORT_COUNT; i++) {
+	for (i = 0; i < CONFIG_USB_PD_PORT_COUNT; i++)
 		pd_set_dual_role(i, PD_DRP_FORCE_SINK);
-		task_set_event(PD_PORT_TO_TASK_ID(i),
-			       PD_EVENT_DP_DISCONNECT, 0);
-	}
 	CPRINTS("PD:S3->S5");
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, pd_chipset_shutdown, HOOK_PRIO_DEFAULT);
