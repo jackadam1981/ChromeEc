096a8ddb12f15478535448b43f945a74f9c15ffb
Scott Collyer <scollyer@google.com>
2019-04-24 11:46:04 -0700

    hatch: Account for TCPC reset polarity in ALERT handler
    Reset polarity was added to tcpc_config so that the reset function
    could be common across different boards which have different TCPC
    selections. This was applie to pd_reset_muc(), however, that CL did
    not take into account the reset polarity in the tcpc_get_alert_status
    function. This CL fixes that oversight.
    
    BUG=b:130194031
    BRANCH=none
    TEST=verified on Hatch that both ports 0/1 function correctly.
    
    Change-Id: I0d75d3655c799d4c74f4a6fb1805c06c1fe99c06
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- baseboard/hatch/baseboard.c
+++ baseboard/hatch/baseboard.c
@@ -177,19 +177,23 @@ DECLARE_HOOK(HOOK_INIT, baseboard_tcpc_init, HOOK_PRIO_INIT_I2C + 1);
 uint16_t tcpc_get_alert_status(void)
 {
 	uint16_t status = 0;
+	int level;
 
 	/*
 	 * Check which port has the ALERT line set and ignore if that TCPC has
-	 * its reset line active. Note that port 0 reset is active high and
-	 * port 1 reset is active low.
+	 * its reset line active.
 	 */
 	if (!gpio_get_level(GPIO_USB_C0_TCPC_INT_ODL)) {
-		if (!gpio_get_level(GPIO_USB_C0_TCPC_RST))
+		level = !!(tcpc_config[USB_PD_PORT_TCPC_0].flags &
+			   TCPC_FLAGS_RESET_ACTIVE_HIGH);
+		if (gpio_get_level(GPIO_USB_C0_TCPC_RST) != level)
 			status |= PD_STATUS_TCPC_ALERT_0;
 	}
 
 	if (!gpio_get_level(GPIO_USB_C1_TCPC_INT_ODL)) {
-		if (gpio_get_level(GPIO_USB_C1_TCPC_RST_ODL))
+		level = !!(tcpc_config[USB_PD_PORT_TCPC_1].flags &
+			   TCPC_FLAGS_RESET_ACTIVE_HIGH);
+		if (gpio_get_level(GPIO_USB_C1_TCPC_RST_ODL) != level)
 			status |= PD_STATUS_TCPC_ALERT_1;
 	}
 
