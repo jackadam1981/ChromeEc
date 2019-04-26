754099f32b10d6fab1d2f33ba8f4dc5872c0d8de
Bibby Hsieh <bibby.hsieh@mediatek.com>
2019-04-26 10:21:47 +0800

    WIP: mtk_isp: Add the service for p2 driver
    Change-Id: I86500068d77700e13e909b5085ca07d1170138c1
    Signed-off-by: Bibby Hsieh <bibby.hsieh@mediatek.com>

---
+++ board/kukui_scp/isp_p2_srv.h
@@ -0,0 +1,23 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_ISP_P2_SRV_H
+#define __CROS_EC_ISP_P2_SRV_H
+
+#include "chip/mt_scp/ipi_chip.h"
+
+enum dip_ipi_id {
+	DIP_CMD,
+};
+
+struct dip_msg_service {
+	unsigned char id;
+	unsigned char msg[288];
+};
+
+/* Functions provided by private overlay. */
+void dip_msg_handler(void *data);
+
+#endif /* __CROS_EC_ISP_P2_SRV_H */
