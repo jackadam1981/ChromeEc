ab3127a9c792f5f49eafec323800ae619a114114
Jack Rosenthal <jrosenth@chromium.org>
2019-05-02 09:58:48 -0600

    ish: [WIP] keep panic data across reset
    Currently not working, I think due to b:131777551
    
    BUG=b:129425206
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I5c9e458b53076eafe7fa50ba851f2c6e863f2247
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>

--- include/config.h
+++ include/config.h
@@ -2601,6 +2601,10 @@
 /* Support One Time Protection structure */
 #undef CONFIG_OTP
 
+/* Address to store persistent panic data at */
+#undef CONFIG_PANIC_DATA_BASE
+#undef CONFIG_PANIC_DATA_SIZE
+
 /* Support PECI interface to x86 processor */
 #undef CONFIG_PECI
 
@@ -4052,6 +4056,23 @@
 
 #ifndef CONFIG_RAM_BANKS
 #define CONFIG_RAM_BANKS	(CONFIG_RAM_SIZE / CONFIG_RAM_BANK_SIZE)
+#endif
+
+/******************************************************************************/
+/*
+ * Store panic data at end of memory by default, unless otherwise
+ * configured.  This is safe because we don't context switch away from
+ * the panic handler before rebooting, and stacks and data start at
+ * the beginning of RAM.
+ */
+#ifndef CONFIG_PANIC_DATA_BASE
+#define CONFIG_PANIC_DATA_BASE (CONFIG_RAM_BASE			\
+				+ CONFIG_RAM_SIZE			\
+				- sizeof(struct panic_data))
+#endif
+
+#ifndef CONFIG_PANIC_DATA_SIZE
+#define CONFIG_PANIC_DATA_SIZE	sizeof(struct panic_data)
 #endif
 
 /******************************************************************************/
