8aefc7ae9d7854977aeda3a5fb939ff034141e35
Raul E Rangel <rrangel@chromium.org>
2019-05-03 12:48:41 -0600

    ec: common: Make IS_ENABLED also handle 0 and 1
    IS_ENABLED can now handle 0 and 1 as possible values. If it gets any
    other values a compiler error will be thrown. This change requires that
    the optimizer always be enabled, otherwise errors will be thrown when a
    value is not defined.
    
    BUG=none
    BRANCH=none
    TEST=make runtests TEST_LIST_HOST="is_enabled_error is_enabled"
    
    Change-Id: I1b166311f81d07e48b3665f4bc0e9502d2ccc4c6
    Signed-off-by: Raul E Rangel <rrangel@chromium.org>

---
+++ test/is_enabled.c
@@ -0,0 +1,53 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Test the IS_ENABLED macro.
+ */
+#include "common.h"
+#include "test_util.h"
+
+#undef	CONFIG_UNDEFINED
+#define	CONFIG_BLANK
+#define	CONFIG_ZERO		0
+#define	CONFIG_ONE		1
+
+static int test_undef(void)
+{
+	TEST_ASSERT(IS_ENABLED(CONFIG_UNDEFINED) == 0);
+
+	return EC_SUCCESS;
+}
+
+static int test_blank(void)
+{
+	TEST_ASSERT(IS_ENABLED(CONFIG_BLANK) == 1);
+
+	return EC_SUCCESS;
+}
+
+static int test_zero(void)
+{
+	TEST_ASSERT(IS_ENABLED(CONFIG_ZERO) == 0);
+
+	return EC_SUCCESS;
+}
+
+static int test_one(void)
+{
+	TEST_ASSERT(IS_ENABLED(CONFIG_ONE) == 1);
+
+	return EC_SUCCESS;
+}
+
+void run_test(void)
+{
+	test_reset();
+
+	RUN_TEST(test_undef);
+	RUN_TEST(test_blank);
+	RUN_TEST(test_zero);
+	RUN_TEST(test_one);
+
+	test_print_result();
+}
---
+++ test/is_enabled_error.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+TEST_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+if BUILD_OUTPUT="$(sh "${TEST_DIR}/RO/test/is_enabled_error.o.cmd" 2>&1)"; then
+	echo "Compilation should not have succeeded"
+	echo "$BUILD_OUTPUT"
+	exit 1
+fi
+
+EXPECTED_ERROR="CONFIG_FIVE must be <blank>, 0, 1, or not defined"
+if grep -q "$EXPECTED_ERROR" <<< "$BUILD_OUTPUT"; then
+	echo "Pass!"
+else
+	echo "Expected to find: $EXPECTED_ERROR"
+	echo "Actual error:"
+	echo "$BUILD_OUTPUT"
+	exit 1
+fi
