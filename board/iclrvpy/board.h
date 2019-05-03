bd889a2eba267f95440038f3b8405b96e3b9e20b
Ayushee <ayushee.shah@intel.com>
2019-05-03 14:57:26 -0700

    Iclrvpy: Board bringup with Intelrvp baseboard
    Configuring Iclrvpy I2C ports and GPIO pins. Also using
    the Intelrvp baseboard to boot Iclrvp all the way to
    the chromeos
    
    BUG=b:132061907
    BRANCH=none
    TEST=Able to boot Iclrvp
    
    Change-Id: I00e99fa5b54ffe8e0b29f69bd0f3b1c4112bfff8
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

---
+++ board/iclrvpy/board.h
@@ -0,0 +1,63 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Intel ICLY-RVP, ITE EC board-specific configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* Chipset Icelake */
+#define CONFIG_CHIPSET_ICELAKE
+
+#include "baseboard.h"
+
+/* Charger */
+#define CONFIG_CHARGER_ISL9238
+
+/* DC Jack charge ports */
+#undef  CONFIG_DEDICATED_CHARGE_PORT_COUNT
+#define CONFIG_DEDICATED_CHARGE_PORT_COUNT 1
+
+/* USB ports */
+#define CONFIG_USB_PD_PORT_COUNT 2
+#define DEDICATED_CHARGE_PORT 2
+
+/* USB MUX */
+#define CONFIG_USB_MUX_VIRTUAL
+
+/* Config BB retimer */
+#define CONFIG_USB_PD_RETIMER_INTEL_BB
+
+/* I2C ports */
+#define CONFIG_IT83XX_SMCLK2_ON_GPC7
+
+#define I2C_PORT_CHARGER	IT83XX_I2C_CH_B
+#define I2C_PORT_BATTERY	IT83XX_I2C_CH_B
+#define I2C_PORT_PCA9555_BOARD_ID_GPIO	IT83XX_I2C_CH_B
+#define I2C_PORT_PORT80		IT83XX_I2C_CH_B
+#define I2C_PORT0_BB_RETIMER	IT83XX_I2C_CH_E
+#define I2C_PORT1_BB_RETIMER	IT83XX_I2C_CH_E
+
+#define I2C_ADDR_PCA9555_BOARD_ID_GPIO	0x44
+#define PORT80_I2C_ADDR			MAX695X_I2C_ADDR1
+#define I2C_PORT0_BB_RETIMER_ADDR	0x84
+#define I2C_PORT1_BB_RETIMER_ADDR	0x86
+
+#ifndef __ASSEMBLER__
+
+enum iclrvp_charge_ports {
+	TYPE_C_PORT_0,
+	TYPE_C_PORT_1,
+	DC_JACK_PORT_0 = DEDICATED_CHARGE_PORT,
+};
+
+/* Define max power */
+#define PD_MAX_POWER_MW        60000
+
+int board_get_version(void);
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
