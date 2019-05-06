5a94af27ab87bfd9b571dc48f5035abb40e94223
Ting Shen <phoenixshen@google.com>
2019-05-06 18:49:34 +0800

    makefile: don't build ro if EC_INCLUDE_RO not set
    BUG=b:121406695
    TEST=make
    BRANCH=None
    
    Change-Id: Ib678678330837fefffd12a7ac445038661639e8c
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- include/config.h
+++ include/config.h
@@ -1768,7 +1768,7 @@
 
 /*
  * Board Image ec.bin contains a RO firmware.  If not defined, the image will
- * only contain the RW firmware. The RO firmware comes from another board.
+ * only contain the RW firmware.
  */
 #define CONFIG_FW_INCLUDE_RO
 
