aba9b25641ab63ffe723176b5afcd0543e51f311
Nicolas Boichat <drinkcat@chromium.org>
2019-05-08 18:15:29 +0900

    common/sha1: Drop support for CONFIG_SHA1
    Nobody is using CONFIG_SHA1 anymore (it seems like some early
    zinger used it, but that was moved to SHA256 in 2014), let's
    just drop support, and not have to worry about moving that
    to third_party.
    
    BRANCH=none
    BUG=chromium:884905
    TEST=make buildall -j
    
    Change-Id: If198d63b7e388f395c8efad82763f0dfa7f1ecc7
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

--- common/build.mk
+++ common/build.mk
@@ -121,7 +121,6 @@ common-$(CONFIG_RWSIG_TYPE_RWSIG)+=vboot/vb21_lib.o
 common-$(CONFIG_MATH_UTIL)+=math_util.o
 common-$(CONFIG_ONLINE_CALIB)+=stillness_detector.o kasa.o math_util.o \
 	mat44.o vec3.o
-common-$(CONFIG_SHA1)+= sha1.o
 common-$(CONFIG_SHA256)+=sha256.o
 common-$(CONFIG_SOFTWARE_CLZ)+=clz.o
 common-$(CONFIG_SOFTWARE_CTZ)+=ctz.o
