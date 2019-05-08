945b114323b59ff0419796745a8afa8c30e46eda
David Huang <david.huang@quanta.corp-partner.google.com>
2019-05-08 11:14:54 +0800

    octopus: Move keyscan_config from baseboard to board level
    Keypad function need modify keyscan_config, move this to board level if
    some boards need support and others don't need keypad.
    Currently only Bobba need support Keypad.
    
    BRANCH=octopus
    BUG=b:131095327
    TEST=Make sure ampton/bloog/bobba/casta/fleex/meep/phaser/yorp can
    build normally.
    
    Change-Id: I2134fafa4f5c90f10bc81a096feeaa4ea2c8884d
    Signed-off-by: David Huang <David.Huang@quantatw.com>

--- board/casta/board.c
+++ board/casta/board.c
@@ -131,6 +131,27 @@ void i2c_end_xfer_notify(int port, int slave_addr)
 	battery_last_i2c_time = get_time();
 }
 
+/******************************************************************************/
+/* Keyboard scan setting */
+struct keyboard_scan_config keyscan_config = {
+	/*
+	 * F3 key scan cycle completed but scan input is not
+	 * charging to logic high when EC start scan next
+	 * column for "T" key, so we set .output_settle_us
+	 * to 80us from 50us.
+	 */
+	.output_settle_us = 80,
+	.debounce_down_us = 9 * MSEC,
+	.debounce_up_us = 30 * MSEC,
+	.scan_period_us = 3 * MSEC,
+	.min_post_scan_delay_us = 1000,
+	.poll_timeout_us = 100 * MSEC,
+	.actual_key_mask = {
+		0x14, 0xff, 0xff, 0xff, 0xff, 0xf5, 0xff,
+		0xa4, 0xff, 0xfe, 0x55, 0xfa, 0xca  /* full set */
+	},
+};
+
 /* TODO: Casta: remove this routine after rev0 is not supported */
 static void board_init(void)
 {
