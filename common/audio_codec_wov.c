80143eb164f1dc8b2119476f7148b50ff2da87aa
Tzung-Bi Shih <tzungbi@chromium.org>
2019-05-09 17:57:48 +0800

    WIP: ec: audio_codec: integrate speech-micro
    BRANCH=none
    BUG=b:122027734, b:123268236
    TEST=make BOARD=kukui_scp -j
    
    Change-Id: I8b238fd8bd9af8e70d5a747650eb8b7dbf4200cb
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- common/audio_codec_wov.c
+++ common/audio_codec_wov.c
@@ -10,6 +10,7 @@
 #include "sha256.h"
 #include "system.h"
 
+#include "hotword_dsp_api.h"
 #include "audio_codec.h"
 
 static struct {
@@ -197,7 +198,8 @@ static int wov_enable(struct host_cmd_handler_args *args)
 	if (priv.driver.enable() != EC_SUCCESS)
 		return EC_RES_ERROR;
 
-	/* TODO: call GoogleHotwordDspInit */
+	if (!GoogleHotwordDspInit((void *)priv.driver.lang_buf_addr))
+		return EC_RES_ERROR;
 
 	mutex_lock(&priv.lock);
 	priv.wov_enabled = 1;
@@ -244,7 +246,7 @@ static int wov_is_hotword_detected(struct host_cmd_handler_args *args)
 
 static int wov_reset(struct host_cmd_handler_args *args)
 {
-	/* TODO: call GoogleHotwordDspReset() */
+	GoogleHotwordDspReset();
 
 	mutex_lock(&priv.lock);
 	priv.hotword_detected = 0;
@@ -404,6 +406,7 @@ void audio_codec_wov_task(void *arg)
 {
 	uint32_t n, req;
 	uint8_t *p = (uint8_t *)priv.driver.audio_buf_addr;
+	int ret;
 
 	if (!priv.driver.read) {
 		ERR("read has not registered");
@@ -455,15 +458,17 @@ void audio_codec_wov_task(void *arg)
 			continue;
 		}
 
+		if (GoogleHotwordDspProcess(p + priv.audio_buf_wp, n, &ret)) {
+			DBG("hotword detected");
+			host_set_single_event(EC_HOST_EVENT_WOV);
+		}
+
 		mutex_lock(&priv.lock);
 		priv.audio_buf_wp += n;
 		if (priv.audio_buf_wp == priv.driver.audio_buf_len)
 			priv.audio_buf_wp = 0;
 		mutex_unlock(&priv.lock);
 		DBG("move wp to %d", priv.audio_buf_wp);
-
-		/* TODO: call GoogleHotwordDspProcess (incremental?) */
-		/* host_set_single_event(EC_HOST_EVENT_WOV); if detected */
 
 next_round:
 		usleep(1000);
