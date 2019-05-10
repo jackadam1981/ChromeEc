f38d1ba109ac5330a83b45267d7b01dfd4ea3071
Louis Collard <louiscollard@chromium.org>
2019-05-10 14:08:02 +0800

    cr50: Rename G2F salt.
    BUG=b:131807777
    TEST=build and flash to soraka locally, check U2F works
         TODO:
         - check old salt is preserved
         - delete salt, check new salt is generated
         - revert change, check new salt is not loaded, old salt is generated
         - re-flash new change, check old salt takes precedence
    BRANCH=none
    
    Signed-off-by: Louis Collard <louiscollard@chromium.org>
    Change-Id: I030588ed3441c72b6f80fbc86a0411a6faa5bb57

--- board/cr50/u2f.c
+++ board/cr50/u2f.c
@@ -72,18 +72,29 @@ static uint32_t salt[8];
 static uint32_t salt_kek[8];
 static uint32_t salt_kh[8];
 static uint8_t u2f_mode = MODE_UNSET;
-static const uint8_t k_salt = NVMEM_VAR_U2F_SALT;
+static uint8_t legacy_g2f_salt;
 
 static int load_state(void)
 {
-	const struct tuple *t_salt = getvar(&k_salt, sizeof(k_salt));
+	const struct tuple *t_salt;
+	uint8_t g2f_salt_index = NVMEM_VAR_U2F_SALT;
+
+	/* Try to load the legacy salt. */
+	t_salt = getvar(&g2f_salt_index, sizeof(g2f_salt_index));
+	legacy_g2f_salt = !!t_salt;
 
 	if (!t_salt) {
-		/* create random salt */
+		/* Legacy salt not present, look for new salt. */
+		g2f_salt_index = NVMEM_VAR_G2F_SALT;
+		t_salt = getvar(&g2f_salt_index, sizeof(g2f_salt_index));
+	}
+
+	if (!t_salt) {
+		/* Create random salt, store as new salt. */
 		if (!DCRYPTO_ladder_random(salt))
 			return 0;
-		if (setvar(&k_salt, sizeof(k_salt), (const uint8_t *)salt,
-			   sizeof(salt)))
+		if (setvar(&g2f_salt_index, sizeof(g2f_salt_index),
+			   (const uint8_t *)salt, sizeof(salt)))
 			return 0;
 	} else {
 		memcpy(salt, tuple_val(t_salt), sizeof(salt));
