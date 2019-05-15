43503fcfaf827958137c479bb89092b9555e5561
Scott Collyer <scollyer@google.com>
2019-05-15 11:17:11 -0700

    hatch: (Test-Only) Enable CONFIG_CHARGER_BQ25710_IDCHG_LIMIT_MA
    This CL enables CONFIG_CHARGER_BQ25710_IDCHG_LIMIT_MA with a 8192 mA limit.
    
    Change-Id: I60956fc0311e95cef6621287e93424a37bcfdc98
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- board/hatch/board.h
+++ board/hatch/board.h
@@ -68,6 +68,9 @@
 /* BC 1.2 */
 #define CONFIG_BC12_DETECT_PI3USB9201
 
+/* Charger features */
+#define CONFIG_CHARGER_BQ25710_IDCHG_LIMIT_MA 8192
+
 /* Volume Button feature */
 #define CONFIG_VOLUME_BUTTONS
 #define GPIO_VOLUME_UP_L GPIO_EC_VOLUP_BTN_ODL
