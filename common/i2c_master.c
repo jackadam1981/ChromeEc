8af16568c63667c6015f0961d6d2e8a9f95781f0
Daisuke Nojiri <dnojiri@chromium.org>
2019-05-16 12:12:08 -0700

    I2C: Add I2C_LOOKUP_TYPE_TCPC
    This patch adds I2C_LOOKUP_TYPE_TCPC to i2c_device_type.
    
    > ectool i2clookup 2
    Port: 0; Address: 0x16 (7-bit format)
    Port: 1; Address: 0x52 (7-bit format)
    
    > ectool i2clookup 1
    Port: 0; Address: 0x50 (7-bit format)
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=none
    BRANCH=none
    TEST=Verified ectool i2clookup 1 and 2 work on Nami
    
    Change-Id: I1a773ced65b1c5ce3656f03eff04a6eadd4bc5ff

--- common/i2c_master.c
+++ common/i2c_master.c
@@ -14,6 +14,7 @@
 #include "i2c.h"
 #include "system.h"
 #include "task.h"
+#include "usb_pd_tcpm.h"
 #include "util.h"
 #include "watchdog.h"
 #include "virtual_battery.h"
@@ -852,6 +853,7 @@ static int i2c_command_passthru(struct host_cmd_handler_args *args)
 }
 DECLARE_HOST_COMMAND(EC_CMD_I2C_PASSTHRU, i2c_command_passthru, EC_VER_MASK(0));
 
+#ifdef CONFIG_HOSTCMD_I2CLOOKUP
 static int i2c_command_lookup(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_i2c_lookup *params = args->params;
@@ -871,6 +873,19 @@ static int i2c_command_lookup(struct host_cmd_handler_args *args)
 		return EC_RES_UNAVAILABLE;
 #endif /* CONFIG_CROS_BOARD_INFO */
 		break;
+	case I2C_LOOKUP_TYPE_TCPC:
+#ifdef CONFIG_USB_PD_PORT_COUNT
+		rsize = sizeof(*res) * CONFIG_USB_PD_PORT_COUNT;
+		if (rsize > args->response_max)
+			return EC_RES_OVERFLOW;
+		for (i = 0; i < CONFIG_USB_PD_PORT_COUNT; i++) {
+			res[i].i2c_port = tcpc_config[i].i2c_host_port;
+			res[i].i2c_addr = tcpc_config[i].i2c_slave_addr;
+		}
+#else
+		return EC_RES_UNAVAILABLE;
+#endif
+		break;
 	default:
 		/* The type was unrecognized */
 		return EC_RES_INVALID_PARAM;
@@ -882,6 +897,7 @@ static int i2c_command_lookup(struct host_cmd_handler_args *args)
 DECLARE_HOST_COMMAND(EC_CMD_I2C_LOOKUP, i2c_command_lookup, EC_VER_MASK(0));
 /* If the params union expands in the future, need to bump EC_VER_MASK */
 BUILD_ASSERT(sizeof(struct ec_params_i2c_lookup) == 4);
+#endif
 
 void i2c_passthru_protect_port(uint32_t port)
 {
--- include/ec_commands.h
+++ include/ec_commands.h
@@ -5356,6 +5356,7 @@ struct ec_response_rollback_info {
 
 enum i2c_device_type {
 	I2C_LOOKUP_TYPE_CBI_EEPROM = 1,
+	I2C_LOOKUP_TYPE_TCPC = 2,
 	I2C_LOOKUP_TYPE_COUNT,
 	I2C_LOOKUP_TYPE_MAX = 0xFFFF,
 };
@@ -5371,7 +5372,8 @@ struct ec_params_i2c_lookup {
 struct ec_i2c_info {
 	uint16_t i2c_port;	/* Physical port for device */
 	uint16_t i2c_addr;	/* 7-bit (or 10-bit) address */
-} __ec_align2;
+	uint32_t reserved;
+} __ec_align4;
 
 /*****************************************************************************/
 /* The command range 0x200-0x2FF is reserved for Rotor. */
