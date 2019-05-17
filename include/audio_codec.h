662fcfc22b32e1fdd3c96993bc4f1f3b7e9746d8
Tzung-Bi Shih <tzungbi@chromium.org>
2019-05-17 11:20:46 +0800

    audio_codec: add common abstract layer
    Common utilities:
    - register shared memory (SHM)
    - console channel
    
    Common host commands:
    - get_capabilities
    - get_shm_addr
    - set_shm_addr
    
    BRANCH=none
    BUG=b:122027734, b:123268236
    TEST=1. define CONFIG_AUDIO_CODEC in board.h
    2. make BOARD=kukui_scp -j
    
    Change-Id: I90e3e112bf8fdffc13eb486e0c608c264650768b
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>

---
+++ include/audio_codec.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_AUDIO_CODEC_H
+#define __CROS_EC_AUDIO_CODEC_H
+
+#include "stdint.h"
+
+/*
+ * Common abstract layer
+ */
+
+/*
+ * Checks capability of audio codec.
+ *
+ * @cap is an integer from enum ec_codec_cap.  Note that it represents a
+ * bit field in a 32-bit integer.  The valid range is [0, 31].
+ *
+ * Returns:
+ *   1 if audio codec capabilities include cap passed as parameter.
+ *   0 if not capable.
+ */
+int audio_codec_capable(uint8_t cap);
+
+/*
+ * Registers shared memory (SHM).
+ *
+ * @shm_id is a SHM identifier from enum ec_codec_shm_id.
+ * @cap is an integer from enum ec_codec_cap.
+ * @addr is the address point to the SHM.
+ * @len is the maximum length of the SHM.
+ * @type is an integer from enum ec_codec_shm_type.
+ *
+ * Returns:
+ *   EC_SUCCESS if success.
+ *   EC_ERROR_UNKNOWN if internal errors.
+ *   EC_ERROR_INVAL if invalid shm_id.
+ *   EC_ERROR_INVAL if invalid cap.
+ *   EC_ERROR_BUSY if the shm_id has been registered.
+ */
+int audio_codec_register_shm(uint8_t shm_id, uint8_t cap,
+		uintptr_t *addr, uint32_t *len, uint8_t type);
+
+#endif
