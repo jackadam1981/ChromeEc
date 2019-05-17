83b0244ed577fa0daa7ff296c55b6502c4a43853
Tzung-Bi Shih <tzungbi@chromium.org>
2019-05-17 11:20:46 +0800

    WIP: audio_codec: add common abstract layer
    BRANCH=none
    BUG=b:122027734, b:123268236
    TEST=make BOARD=kukui_scp CONFIG_AUDIO_CODEC=y -j
    
    Change-Id: I90e3e112bf8fdffc13eb486e0c608c264650768b
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>

---
+++ include/audio_codec.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_AUDIO_CODEC_H
+#define __CROS_EC_AUDIO_CODEC_H
+
+#include "console.h"
+
+#define ERR(format, args...) \
+	cprintf(CC_AUDIO_CODEC, "[ERR] " format "\n", ##args)
+#define WARN(format, args...) \
+	cprintf(CC_AUDIO_CODEC, "[WARN] " format "\n", ##args)
+
+#ifdef DEBUG_AUDIO_CODEC
+#define DBG(format, args...) \
+do { \
+	cprintf(CC_AUDIO_CODEC, "[DBG] " format " (%s:%d)\n", ##args, \
+		__FILE__, __LINE__); \
+	cflush(); \
+} while (0)
+#else
+#define DBG(format, args...)
+#endif /* DEBUG_AUDIO_CODEC */
+
+
+struct audio_codec_driver {
+	int (*translate_addr_ec_to_ap)(uintptr_t ec_addr, uintptr_t *ap_addr);
+};
+
+/*
+ * Check capabilitiy of audio codec.
+ *
+ * Returns:
+ *   1 if capable.
+ *   0 if not capable.
+ */
+int audio_codec_capable(uint8_t cap);
+
+/*
+ * Register shared memory.
+ *
+ * Returns:
+ *   EC_SUCCESS if success.
+ *   EC_ERROR_INVAL if invalid shm_id.
+ *   EC_ERROR_INVAL if invalid cap.
+ */
+int audio_codec_register_shm(uint8_t shm_id, uint8_t cap,
+		uintptr_t addr, uint32_t len, uint8_t type);
+
+/*
+ * Register driver.
+ *
+ * Returns:
+ *   EC_SUCCESS if success.
+ */
+int audio_codec_register_driver(struct audio_codec_driver *driver);
+
+#endif
