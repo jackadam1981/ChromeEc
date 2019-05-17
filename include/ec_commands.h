7adef805ddbb184cc370c2b023dab18fff59773b
Tzung-Bi Shih <tzungbi@chromium.org>
2019-05-17 11:22:53 +0800

    audio_codec: add DMIC abstract layer
    Common DMIC host commands:
    - get_max_gain
    - get_gain_idx
    - set_gain_idx
    - get_gain_mono
    - set_gain_mono
    - get_gain_dual
    - set_gain_dual
    
    BRANCH=none
    BUG=b:122027734, b:123268236
    TEST=make BOARD=kukui_scp CONFIG_AUDIO_CODEC=y \
         CONFIG_AUDIO_CODEC_DMIC=y -j
    
    Change-Id: I7b4cc11645675f9d790947b17c3ea385dae13483
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -4617,8 +4617,13 @@ struct __ec_align4 ec_response_ec_codec_get_shm_addr {
 #define EC_CMD_EC_CODEC_DMIC 0x00BD
 
 enum ec_codec_dmic_subcmd {
-	EC_CODEC_DMIC_SET_GAIN = 0,
-	EC_CODEC_DMIC_GET_GAIN,
+	EC_CODEC_DMIC_GET_MAX_GAIN = 0,
+	EC_CODEC_DMIC_SET_GAIN_IDX,
+	EC_CODEC_DMIC_GET_GAIN_IDX,
+	EC_CODEC_DMIC_SET_GAIN_MONO,
+	EC_CODEC_DMIC_GET_GAIN_MONO,
+	EC_CODEC_DMIC_SET_GAIN_DUAL,
+	EC_CODEC_DMIC_GET_GAIN_DUAL,
 	EC_CODEC_DMIC_SUBCMD_COUNT,
 };
 
@@ -4628,14 +4633,39 @@ struct __ec_align4 ec_param_ec_codec_dmic {
 	uint8_t reserved[3];
 
 	union {
-		struct __ec_align1 ec_param_ec_codec_dmic_set_gain {
+		struct __ec_align1 ec_param_ec_codec_dmic_set_gain_idx {
+			uint8_t channel;
+			uint8_t gain;
+		} set_gain_idx_param;
+
+		struct __ec_align1 ec_param_ec_codec_dmic_get_gain_idx {
+			uint8_t channel;
+		} get_gain_idx_param;
+
+		struct __ec_align1 ec_param_ec_codec_dmic_set_gain_mono {
+			uint8_t gain;
+		} set_gain_mono_param;
+
+		struct __ec_align1 ec_param_ec_codec_dmic_set_gain_dual {
 			uint8_t left;
 			uint8_t right;
-		} set_gain_param;
+		} set_gain_dual_param;
 	};
 };
 
-struct __ec_align1 ec_response_ec_codec_dmic_get_gain {
+struct __ec_align1 ec_response_ec_codec_dmic_get_max_gain {
+	uint8_t max_gain;
+};
+
+struct __ec_align1 ec_response_ec_codec_dmic_get_gain_idx {
+	uint8_t gain;
+};
+
+struct __ec_align1 ec_response_ec_codec_dmic_get_gain_mono {
+	uint8_t gain;
+};
+
+struct __ec_align1 ec_response_ec_codec_dmic_get_gain_dual {
 	uint8_t left;
 	uint8_t right;
 };
