a4fccb2df4e54457e1dfa148eda299be0c0e0d82
Dino Li <Dino.Li@ite.com.tw>
2019-05-20 14:00:04 +0800

    core:RISC-V / chip:IT83202
    The IT83202 is an embedded controller with RISC-V core.
    It supports maximum ram size to 256KB and internal flash to 1MB.
    
    BUG=none
    BRANCH=none
    TEST=EC boots and test console commands (eg: taskinfo, version, sysjump...)
         on it83202 EVB.
    
    Change-Id: I424c0d2878beb941c816363b5c7a3f57fda9fd13
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>

--- chip/it83xx/intc.h
+++ chip/it83xx/intc.h
@@ -30,6 +30,7 @@ void espi_interrupt(void);
 void espi_vw_interrupt(void);
 void espi_enable_pad(int enable);
 void espi_init(void);
+int chip_get_intc_group(int irq);
 
 #if defined(CONFIG_HOSTCMD_X86) && defined(HAS_TASK_KEYPROTO)
 void lpc_kbc_ibf_interrupt(void);
