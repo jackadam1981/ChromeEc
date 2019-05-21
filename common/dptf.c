c11edf47fef8350b7f762125b2d48df38d251985
Evan Green <evgreen@chromium.org>
2019-05-21 14:29:39 -0700

    common: dptf: Guard against wild sensor IDs
    If developers have not set up TEMP_SENSOR_COUNT correctly, or the caller
    starts sending wild sensor_id or idx values down, then the EC will do
    arbitrary reads and writes over its own memory. In one case, the
    PD log buffer indices are next in memory, so we would see the following
    spew in the kernel (every 60 seconds, since the kernel only checks that
    often):
    
    [  138.151937] PDLOG 2019/05/17 22:46:26.913 P0 Disconnected
    [  138.158512] PDLOG 2019/05/17 22:46:04.936 P0 Disconnected
    [  138.165066] PDLOG 2019/05/17 22:46:04.935 P0 Disconnected
    [  138.171643] PDLOG 2019/05/17 22:46:04.935 P0 Disconnected
    [  138.178162] PDLOG 2019/05/17 22:46:04.935 P0 Disconnected
    ...
    
    BUG=b:132999028
    BRANCH=none
    TEST=Build and boot hatch, observe no more log spam
    
    Change-Id: If2e20972c3268e84bb4cdfa315c6b7f7cb76868f
    Signed-off-by: Evan Green <evgreen@chromium.org>

--- common/dptf.c
+++ common/dptf.c
@@ -15,6 +15,7 @@
 /* Console output macros */
 #define CPUTS(outstr) cputs(CC_DPTF, outstr)
 #define CPRINTS(format, args...) cprints(CC_DPTF, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_DPTF, format, ## args)
 
 /*****************************************************************************/
 /* DPTF temperature thresholds */
@@ -59,6 +60,11 @@ static int dptf_check_temp_threshold(int sensor_id, int temp)
 	int tripped = 0;
 	int max, i;
 
+	if (sensor_id >= TEMP_SENSOR_COUNT) {
+		CPRINTS("DPTF: Invalid sensor ID");
+		return 0;
+	}
+
 	for (i = 0; i < DPTF_THRESHOLDS_PER_SENSOR; i++) {
 
 		max = dptf_threshold[sensor_id][i].temp;
@@ -91,6 +97,12 @@ void dptf_set_temp_threshold(int sensor_id, int temp, int idx, int enable)
 {
 	CPRINTS("DPTF sensor %d, threshold %d C, index %d, %sabled",
 		sensor_id, K_TO_C(temp), idx, enable ? "en" : "dis");
+
+	if ((sensor_id >= TEMP_SENSOR_COUNT) ||
+	    (idx >= DPTF_THRESHOLDS_PER_SENSOR)) {
+		CPRINTS("DPTF: Invalid sensor ID");
+		return;
+	}
 
 	if (enable) {
 		/* Don't update threshold condition if already enabled */
