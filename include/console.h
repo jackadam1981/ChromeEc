839b33536afe4e1b22d043f7e234676185d7488b
Harry Cutts <hcutts@chromium.org>
2019-05-21 17:09:08 -0700

    console: improve long command name error message
    I ran into this today and found the "size of array 'field' is negative"
    somewhat difficult to understand. Making it "size of array
    'name_too_long'…" at least improves on it a bit. Also noted the
    restriction in the doc comment.
    
    BRANCH=none
    BUG=none
    TEST=add a command with a name longer than 15 characters and run `make`
    
    Change-Id: I965f57854a87ea4923b5d0b3b02b1f89c080b7e8
    Signed-off-by: Harry Cutts <hcutts@chromium.org>

--- include/console.h
+++ include/console.h
@@ -119,8 +119,9 @@ void console_has_input(void);
  * Register a console command handler.
  *
  * @param name          Command name; must not be the beginning of another
- *                      existing command name.  Note this is NOT in quotes
- *                      so it can be concatenated to form a struct name.
+ *                      existing command name.  Must be less than 16 characters
+ *                      long.  Note this is NOT in quotes so it can be
+ *                      concatenated to form a struct name.
  * @param routine       Command handling routine, of the form
  *                      int handler(int argc, char **argv)
  * @param argdesc       String describing arguments to command; NULL if none.
@@ -157,7 +158,9 @@ void console_has_input(void);
 #define _DCL_CON_CMD_ALL(NAME, ROUTINE, ARGDESC, HELP, FLAGS)		\
 	static const char __con_cmd_label_##NAME[] = #NAME;		\
 	struct size_check##NAME {					\
-		int field[2 * (sizeof(__con_cmd_label_##NAME) < 16) - 1]; }; \
+		int name_too_long[2 * (sizeof(__con_cmd_label_##NAME) < 16) - \
+				  1];                                         \
+	};                                                                    \
 	const struct console_command __keep __no_sanitize_address	\
 	__con_cmd_##NAME						\
 	__attribute__((section(".rodata.cmds." #NAME))) =		\
