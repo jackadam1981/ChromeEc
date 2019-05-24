0437de9f9a896e52385bbbe45471359cd84353e0
Tim Wawrzynczak <twawrzynczak@chromium.org>
2019-05-24 11:13:10 -0600

    board/helios: Add new build for Helios.
    Helios is a Hatch variant.  Added initial battery info, scrubbed GPIOs,
    and added enough infrastructure to allow BOARD=helios to build.
    
    BUG=b:133381447, b:133182138
    BRANCH=none
    TEST=make BOARD=helios
    
    Change-Id: I2c62c82e95dcc09ecec7a27fae749e3a393682a9
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- baseboard/hatch/baseboard.c
+++ baseboard/hatch/baseboard.c
@@ -309,6 +309,7 @@ void board_set_charge_limit(int port, int supplier, int charge_ma,
 
 void baseboard_mst_enable_control(enum mst_source src, int level)
 {
+#ifdef GPIO_EN_MST
 	static uint32_t mst_input_levels;
 
 	if (level)
@@ -317,6 +318,7 @@ void baseboard_mst_enable_control(enum mst_source src, int level)
 		atomic_clear(&mst_input_levels, 1 << src);
 
 	gpio_set_level(GPIO_EN_MST, mst_input_levels ? 1 : 0);
+#endif
 }
 
 /* Enable or disable input devices, based on chipset state */
