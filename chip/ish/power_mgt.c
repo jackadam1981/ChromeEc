01782b2e2610dfa7822a7c8aa0826eaa07d4d884
Hu, Hebo <hebo.hu@intel.com>
2019-06-04 15:46:33 +0800

    ish: fixed wrongly entered D0ix states in some times
    In __idle() task, when calculate the 'next_delay' value (the
    sleep time), in most time, 'next_delay' should be always positive,
    but ISH HPET timer HW has some latency for interrupt, so it's
    possible in some times when its very close to the expire
    time of the event timer, the current time could advance the
    'last_deadline' which should be updated in event timer ISR,
    in this case, 'next_delay' could be negative.
    
    We calibrated the 'last_deadline' in timer driver for
    this interrupt latency impact.
    
    So, the negative case for 'next_delay' should be not happen.
    If still happens, its doesn't matter, we can just ignore it
    , and if not want to see this case, can adjust the
    'HPET_INT_LATENCY_TICKS' for new calibration till its not
    happen anymore.
    
    BUG=b:133459192
    BRANCH=none
    TEST=tested on arcada platform, with 10ms timer loop task, D0i2/D0i3
    should not entered.
    
    Change-Id: Ie84fb630900dd7d59a41c98c08da4a71a831c030
    Signed-off-by: Hu, Hebo <hebo.hu@intel.com>

--- chip/ish/power_mgt.c
+++ chip/ish/power_mgt.c
@@ -617,7 +617,29 @@ void __idle(void)
 		t0 = get_time();
 		next_delay = __hw_clock_event_get() - t0.le.lo;
 
-		pm_process(t0, next_delay);
+		/*
+		 * In most time, 'next_delay' should be always positive, but
+		 * ISH HPET timer HW has some latency for interrupt, so it's
+		 * possible in some times when its very close to the expire
+		 * time of the event timer, the current time could advance the
+		 * 'last_deadline' which should be updated in event timer ISR,
+		 * in this case, 'next_delay' could be negative.
+		 *
+		 * We calibrated the 'last_deadline' in timer driver for
+		 * this interrupt latency impact.
+		 *
+		 * So, the negative case for 'next_delay' should be not happen.
+		 * If still happens, its doesn't matter, we can just ignore it,
+		 * and if not want to see this case, can adjust the
+		 * 'HPET_INT_LATENCY_TICKS' for new calibration till its not
+		 * happen anymore.
+		 */
+		if (next_delay >= 0)
+			pm_process(t0, next_delay);
+		else {
+			CPRINTS("Negative sleep time: %d\n", next_delay);
+			ish_mia_halt();
+		}
 	}
 }
 
