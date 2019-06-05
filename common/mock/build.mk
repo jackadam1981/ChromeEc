7c4dc3d94cb97170b42079c1459cd6295c7b9b39
Tom Hughes <tomhughes@chromium.org>
2019-06-05 14:28:00 -0700

    fpsensor: Deinit the sensor before clearing the context
    The opaque context contains sensor state that we can't nuke from
    underneath the sensor library. Calling fp_sensor_deinit will release
    this context and allow us to safely clear the context.
    
    BRANCH=none
    BUG=b:124773209
    TEST="fpenroll" followed by "fpclear" in the FP console
    TEST=On nocturne, in the UI, enroll finger, log out, and log in
         => then lock and unlock with finger
    TEST=make buildall -j
    
    Change-Id: I3e25bdf7eaaf99f3801547e11a6c524f924f4726
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1647294
    Reviewed-by: Nicolas Norvez <norvez@chromium.org>
    Commit-Queue: Yicheng Li <yichengli@chromium.org>
    Tested-by: Yicheng Li <yichengli@chromium.org>
    (cherry picked from commit 3c1215dadc3a279925a17f55bdf0cb8863c41626)

---
+++ common/mock/build.mk
@@ -0,0 +1,11 @@
+<<<<<<< HEAD   (942b9f Revert "Reland "nocturne: Add doze support"")
+=======
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# See common/mock/README.md for more information.
+
+mock-$(HAS_MOCK_FPSENSOR) += fpsensor_mock.o
+mock-$(HAS_MOCK_ROLLBACK) += rollback_mock.o
+>>>>>>> CHANGE (3c1215 fpsensor: Deinit the sensor before clearing the context)
