fbed75227db25933eb9ca1436fbc3be1463f1bb9
Daisuke Nojiri <dnojiri@chromium.org>
2019-06-06 11:28:48 -0700

    usbpd: Fix infinite init loop
    tcpci_tcpm_init can loop forever if tcpc_read or mux_read is successful
    but TCPC_REG_POWER_STATUS_UNINIT bit is set.
    
    This patch fixes it.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=chromium:971741
    BRANCH=none
    TEST=buildall
    
    Change-Id: I5eeb58fc77ee0de753a6c0b049bf332e291faa29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1647366
    Reviewed-by: Scott Collyer <scollyer@chromium.org>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    (cherry picked from commit 2e3ec3604f774d9c7bb9eaf0aaf339b4a8cbd9cd)

--- driver/tcpm/tcpci.c
+++ driver/tcpm/tcpci.c
@@ -708,13 +708,13 @@ int tcpci_tcpm_init(int port)
 		error = tcpc_read(port, TCPC_REG_POWER_STATUS, &power_status);
 		/*
 		 * If read succeeds and the uninitialized bit is clear, then
-		 * initalization is complete, clear all alert bits and write
+		 * initialization is complete, clear all alert bits and write
 		 * the initial alert mask.
 		 */
 		if (!error && !(power_status & TCPC_REG_POWER_STATUS_UNINIT))
 			break;
-		else if (error && --tries == 0)
-			return error;
+		if (--tries <= 0)
+			return error ? error : EC_ERROR_TIMEOUT;
 		msleep(10);
 	}
 
@@ -760,8 +760,8 @@ int tcpci_tcpm_mux_init(int i2c_port_addr)
 		 */
 		if (!error && !(power_status & TCPC_REG_POWER_STATUS_UNINIT))
 			break;
-		else if (error && --tries == 0)
-			return error;
+		if (--tries <= 0)
+			return error ? error : EC_ERROR_TIMEOUT;
 		msleep(10);
 	}
 
