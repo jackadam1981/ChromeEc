174eebbb4b994f9fb94943035395a34845d20859
Jerry Bradshaw <jerry.bradshaw@maximintegrated.com>
2019-06-06 08:52:13 -0500

    Basic implementation of the Maxim Integrated MAX32660 within the EC OS Includes System Clock, Timer, Uart, Watchdog
    Change-Id: I195059c87d97e70c6a134304143613b86b623e22

---
+++ board/max32660example/build.mk
@@ -0,0 +1,14 @@
+# -*- makefile -*-
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+#
+
+# MAX32660 Device
+CHIP:=max32660
+
+#board-y=board.o led.o button.o sensor.o si7021.o oled_driver.o oled.o
+board-y=board.o
+
