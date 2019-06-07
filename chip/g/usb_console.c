dee16e64706b561d0781051fc4d2e32681b5f2f6
Namyoon Woo <namyoon@chromium.org>
2019-06-07 15:21:07 -0700

    g: minor fix in putting a character into usb_console tx queue
    When __tx_char attempts to push a character into tx_queue,
    it doesn't return an error if it fails. Eventually usb_puts()
    doesn't recognize tx queue getting full if it happens, and processes
    all characters, and always return EC_SUCCESS.
    
    BUG=None
    BRANCH=None
    TEST=ran uart_usb_tester on bob.
    uart_stress_tester.sh --pty="/dev/ttyUSB0 /dev/ttyUSB2 /dev/ttyUSB1"
    --min_char 400000
    INFO    : ChromeOS UART stress test starts.
    INFO    : UART devices: /dev/ttyUSB0 /dev/ttyUSB2 /dev/ttyUSB1
    ......................................................
    INFO    : /dev/ttyUSB0: 0 lost / 483415 : 0 %
    ERROR   : /dev/ttyUSB2: 277497 lost / 415835 : 66.7 %
    ERROR   : /dev/ttyUSB1: 244459 lost / 400575 : 61.0 %
    INFO    : Test files are in /tmp/uart_stress_tester.sh_latest
    INFO    : and also in /tmp/tmp.X7jwZfxNe2.uart_stress_tester.sh.
    ERROR   : FAIL
    
    CR50 console (/dev/ttyUSB0) error rate got improved from 4~7% to
    0 %.
    
    Change-Id: I5ae0b7bd494fbc29978ce3e87531e246a9be51f8
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- chip/g/usb_console.c
+++ chip/g/usb_console.c
@@ -316,9 +316,6 @@ static int usb_wait_console(void)
 	timestamp_t deadline = get_time();
 	int wait_time_us = 1;
 
-	if (!is_enabled || !tx_fifo_is_ready_())
-		return EC_SUCCESS;
-
 	deadline.val += USB_CONSOLE_TIMEOUT_US;
 
 	/*
@@ -375,10 +372,12 @@ static int __tx_char(void *context, int c)
 
 	while (QUEUE_ADD_UNITS(state, &c, 1) != 1)
 		usleep(500);
+
+	return EC_SUCCESS;
 #else
-	QUEUE_ADD_UNITS(state, &c, 1);
+	return QUEUE_ADD_UNITS(state, &c, 1) ? EC_SUCCESS :
+					       EC_ERROR_OVERFLOW;
 #endif
-	return 0;
 }
 
 /*
@@ -405,18 +404,19 @@ int usb_puts(const char *outstr)
 		return EC_SUCCESS;
 
 	ret  = usb_wait_console();
-	if (ret)
-		return ret;
+	if (ret == EC_SUCCESS) {
+		state = tx_q;
+		while (*outstr) {
+			ret = __tx_char(&state, *outstr);
+			if (ret != EC_SUCCESS)
+				break;
+			outstr++;
+		}
 
-	state = tx_q;
-	while (*outstr)
-		if (__tx_char(&state, *outstr++))
-			break;
-
-	if (queue_count(&state))
 		handle_output();
+	}
 
-	return *outstr ? EC_ERROR_OVERFLOW : EC_SUCCESS;
+	return ret;
 }
 
 int usb_putc(int c)
@@ -437,14 +437,12 @@ int usb_vprintf(const char *format, va_list args)
 		return EC_SUCCESS;
 
 	ret = usb_wait_console();
-	if (ret)
-		return ret;
+	if (ret == EC_SUCCESS) {
+		state = tx_q;
+		ret = vfnprintf(__tx_char, &state, format, args);
 
-	state = tx_q;
-	ret = vfnprintf(__tx_char, &state, format, args);
-
-	if (queue_count(&state))
 		handle_output();
+	}
 
 	return ret;
 }
