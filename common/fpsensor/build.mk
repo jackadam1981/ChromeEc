ecd67bda5685e88d74172ebaa4e1bb90a4995a58
Yicheng Li <yichengli@chromium.org>
2019-06-06 18:27:42 -0700

    fpsensor: Move crypto-related code to fpsensor_crypto.c
    Move crypto-related code to common/fpsensor/fpsensor_crypto.c. This
    facilitates unittesting because we can control whether to link in
    crypto-related code, and also facilitates mocking the encryption
    engine.
    
    BRANCH=nocturne
    BUG=chromium:927095
    TEST=ran unittests
    TEST=tested enrollment, matching and multifinger on DUT nocturne
    
    Change-Id: I5bffc1460cbe2c9e3d6294ea5fff41f14019f0eb
    Signed-off-by: Yicheng Li <yichengli@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1648922
    Reviewed-by: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    (cherry picked from commit 35d0e17de6d76699c4fac850d39ed4c4b5488cb2)

--- common/fpsensor/build.mk
+++ common/fpsensor/build.mk
@@ -9,5 +9,10 @@ _fpsensor_dir:=$(dir $(lastword $(MAKEFILE_LIST)))
 
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_dir)fpsensor_state.o
 ifneq ($(CONFIG_SPI_FP_PORT),)
+<<<<<<< HEAD   (ae164f nocturne: Only notify MKBP via hostevent in suspend)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_dir)fpsensor.o
+=======
+all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_dir)/fpsensor.o
+all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_dir)/fpsensor_crypto.o
+>>>>>>> CHANGE (35d0e1 fpsensor: Move crypto-related code to fpsensor_crypto.c)
 endif
