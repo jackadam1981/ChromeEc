4319f7bc06fd929c36ed4f6750650beda80f18be
Yilun Lin <yllin@google.com>
2019-06-10 11:40:51 +0800

    TEST-ONLY: kukui_scp: test .dram.* sections.
    Change-Id: I747bd8ee9a267dba1f9cbddefe6e5a983c914a88
    Signed-off-by: Yilun Lin <yllin@google.com>

--- board/kukui_scp/board.c
+++ board/kukui_scp/board.c
@@ -13,16 +13,30 @@
 #include "task.h"
 #include "timer.h"
 #include "util.h"
+#include "link_defs.h"
 
 /* Build GPIO tables */
 void eint_event(enum gpio_signal signal);
 
 #include "gpio_list.h"
 
+__SECTION(dram.bss) unsigned char dram_bss_val;
+__SECTION(dram.data)
+unsigned char dram_data_val[] = {1, 2, 3, 4, 5, 6, 7, 8, 9,
+				 10, 11, 12, 13, 14, 15, 16, 17};
 
 void eint_event(enum gpio_signal signal)
 {
 	ccprintf("EINT event: %d\n", signal);
+}
+
+__SECTION(dram.text) __attribute__((noinline)) void dram_text_func(void) {
+	int i;
+
+	ccprintf("dram_bss_val = %d\n", (unsigned int)dram_bss_val);
+	for (i = 0; i < ARRAY_SIZE(dram_data_val); ++i)
+		ccprintf("dram_data_val[%d]= %d\n", i,
+			(unsigned int)(dram_data_val[i]));
 }
 
 /* Initialize board.  */
@@ -31,5 +45,7 @@ static void board_init(void)
 	gpio_enable_interrupt(GPIO_EINT5_TP);
 	gpio_enable_interrupt(GPIO_EINT6_TP);
 	gpio_enable_interrupt(GPIO_EINT7_TP);
+
+	dram_text_func();
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
