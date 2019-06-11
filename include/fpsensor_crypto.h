53ae59f98fa129ebfd6320d08baeb0715e4ac03a
Yicheng Li <yichengli@chromium.org>
2019-06-11 16:46:36 -0700

    fpsensor: Implement derivation of positive match secret.
    Implement derivation of positive match secret for a template. This
    derivation will be used when biod sends a command to ask for
    positive match secret and the secret readable bit is set. Also add
    unit tests for this derivation.
    
    BRANCH=nocturne
    BUG=chromium:927095
    TEST=ran unittests
    TEST=tested enrollment, matching and multifinger on DUT nocturne
    
    Change-Id: Ife477a98573284f69c5fb07e814d9a3d09f92127
    Signed-off-by: Yicheng Li <yichengli@chromium.org>

--- include/fpsensor_crypto.h
+++ include/fpsensor_crypto.h
@@ -41,6 +41,17 @@ int hkdf_expand(uint8_t *out_key, size_t out_key_size, const uint8_t *prk,
 int derive_encryption_key(uint8_t *out_key, const uint8_t *salt);
 
 /**
+ * Derive positive match secret from |input_encryption_salt| and
+ * SBP_Src_Key.
+ *
+ * @param output buffer to store positive match secret.
+ * @param input_encryption_salt the salt for deriving secret.
+ * @return EC_SUCCESS on success and error code otherwise.
+ */
+int derive_pos_match_secret(uint8_t *output,
+			    const uint8_t *input_encryption_salt);
+
+/**
  * Encrypt |plaintext| using AES-GCM128.
  *
  * @param key the key to use in AES.
