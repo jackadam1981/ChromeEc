6fd55a9f90d95835048fda9a3f9de2da44bff64f
Daisuke Nojiri <dnojiri@chromium.org>
2019-06-12 09:28:34 -0700

    system: Add RESET_FLAG_TIMER
    RESET_FLAG_TIMER is used for wake-up from chip hibernation.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=134973675
    BRANCH=strago
    TEST=Verify 'timer' is printed when EC wakes up from hibernation.
    
    Change-Id: I93c4df3f7bf8711661716887d4ad2061240f00dc

--- chip/mec1322/system.c
+++ chip/mec1322/system.c
@@ -114,6 +114,9 @@ void _system_reset(int flags, int wake_from_hibernate)
 	else
 		save_flags |= RESET_FLAG_SOFT;
 
+	if (MEC1322_INT_SOURCE(17) & MEC1322_INT_SOURCE_HTIMER)
+		save_flags |= RESET_FLAG_TIMER;
+
 	MEC1322_VBAT_RAM(HIBDATA_INDEX_SAVED_RESET_FLAGS) = save_flags;
 
 	/* Trigger watchdog in 1ms */
