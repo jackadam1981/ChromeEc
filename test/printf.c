f03fe2f7c3d9ed929bef1a5b0eb60ffe0ad47e76
Craig Hesling <hesling@chromium.org>
2019-06-13 18:22:27 -0700

    printf: Fix hexdump and string 0 precision
    This patch addresses a few issues with the current formatter.
    The major points are as follows:
    1. Cannot specify precision 0 (truncate all) for string or hexdump
    2. Forced safe precision for malformed strings
    3. No padding when using hexdump
    4. Bad error EC_ERROR_INVAL in vsnprintf
    5. Documentation errors
    
    For (1), no piece of code explicitly sets the precision to 0 in order to
    invoke the default behavior, which is currently no precision limit.
    You can check using the following grep line:
     grep -rI '%[\*0-9]\{0,20\}\.0\{1,20\}[a-zA-Z]'
    However, there are many cases where the precision is used to limit
    the character output (as it should be).
     grep -rI '%[\*0-9]\{0,20\}\.[\*0-9]\{1,20\}[a-zA-Z]'
    There are many more instances that use variable precision without
    checking if the precision is zero. One of which is the following:
     crrev.com/4a4e2c71a0f6aaa50e0728922f84a7d54c14380a/test/host_command_fuzz.c#116
     https://clusterfuzz.com/testcase-detail/5699023975088128
    Our current implementation will insert ERROR and stop processing, if a precision
    of zero is detected with the hexdump. This results in a badly formatter
    console line or runtime string, when the intended behavior would be to
    simply read no bytes.
    Our printf should handle explicit zero precision similar to stdlib's printf,
    which means truncating all the way to zero positions, if specified.
    
    For (2), our current implementation uses strlen to identify the length
    of the input string, regardless of the set precision.
    Since this is an embedded platform, we should use strnlen to impose safe
    limits, when a precision is specified.
    
    For (3), our implementation should support padding and adjusting of
    all formatter types, since that is a primary feature of a printf
    formatter.
    
    The remaining commented code highlights odd behavior that should be
    fixed at some point, but is not critical.
    
    BUG=chromium:974084
    TEST=Checked for any format lines that rely on a set precision of 0
         grep -rI '%[\*0-9]\{0,20\}\.[\*0-9]\{1,20\}[a-zA-Z]'
    TEST=make run-printf V=1
    BRANCH=none
    
    Change-Id: I897c53cce20a701fcbe8fb9572eb878817525cc3
    Signed-off-by: Craig Hesling <hesling@chromium.org>

--- test/printf.c
+++ test/printf.c
@@ -12,32 +12,32 @@
 #include "util.h"
 
 #define INIT_VALUE 0x5E
+#define NO_BYTES_TOUCHED NULL
 static const char err_str[] = "ERROR";
 static char output[1024];
 
-int run(const char *expect, size_t size_limit, int expect_ret,
-		const char *format, va_list args)
+int run(char *out, size_t out_size,
+		int expect_ret, const char *expect,
+		size_t size_limit, const char *format, va_list args)
 {
 	size_t expect_size = expect ? strlen(expect) + 1 : 0;
 	int rv;
 
 	ccprintf("\n");
-	ccprintf("expect='%s'\n", expect);
+	ccprintf("size_limit=%-4d | format='%s'\n", size_limit, format);
+	ccprintf("expect  ='%s'   | expect_status=%d\n", expect, expect_ret);
 
-	TEST_ASSERT(expect_size <= sizeof(output));
+	TEST_ASSERT(expect_size <= out_size);
 	TEST_ASSERT(expect_size <= size_limit);
-	memset(output, INIT_VALUE, sizeof(output));
+	memset(out, INIT_VALUE, out_size);
 
-	rv = vsnprintf(output, size_limit, format, args);
-	ccprintf("output='%s'\n", output);
+	rv = vsnprintf(out, size_limit, format, args);
+	ccprintf("received='%.*s'   | ret          =%d\n", MIN(out_size, 30), out, rv);
 
-	TEST_ASSERT_ARRAY_EQ(output, expect, expect_size);
-	TEST_ASSERT_MEMSET(&output[expect_size], INIT_VALUE,
-						sizeof(output)-expect_size);
+	TEST_ASSERT_ARRAY_EQ(out, expect, expect_size);
+	TEST_ASSERT_MEMSET(&out[expect_size], INIT_VALUE, out_size-expect_size);
 
 	if (rv >= 0) {
-		ccprintf("expect_size = %ld\n", expect_size);
-		ccprintf("rv = %d\n", rv);
 		TEST_ASSERT(rv == expect_size-1);
 		TEST_ASSERT(EC_SUCCESS == expect_ret);
 	} else {
@@ -53,20 +53,26 @@ int expect_success(const char *expect, const char *format, ...)
 	int rv;
 
 	va_start(args, format);
-	rv = run(expect, sizeof(output), EC_SUCCESS, format, args);
+	rv = run(output, sizeof(output),
+			EC_SUCCESS, expect,
+			sizeof(output), format, args);
 	va_end(args);
 
 	return rv;
 }
 
-int expect(int expect_ret, size_t size_limit, const char *expect,
-			const char *format, ...)
+
+int expect(char *out, size_t out_size,
+			int expect_ret, const char *expect,
+			size_t size_limit, const char *format, ...)
 {
 	va_list args;
 	int rv;
 
 	va_start(args, format);
-	rv = run(expect, size_limit, expect_ret, format, args);
+	rv = run(out, out_size,
+			expect_ret, expect,
+			size_limit, format, args);
 	va_end(args);
 
 	return rv;
@@ -78,8 +84,9 @@ test_static int test_vsnprintf_args(void)
 {
 	T(expect_success("",          ""));
 	T(expect_success("a",         "a"));
-	/* T(expect(NULL, 0,              EC_ERROR_INVAL,    "")); fail */
-	T(expect(EC_ERROR_OVERFLOW, 2, "a", "abc"));
+	T(expect(output, sizeof(output), EC_ERROR_INVAL, NO_BYTES_TOUCHED, 0, ""));
+	T(expect(output, sizeof(output), EC_SUCCESS, "", 1, ""));
+	T(expect(output, sizeof(output), EC_ERROR_OVERFLOW, "a", 2, "abc"));
 	return EC_SUCCESS;
 }
 
@@ -96,14 +103,17 @@ test_static int test_vsnprintf_int(void)
 	T(expect_success(" +123",     "%+5d",    123));
 	T(expect_success("00123",     "%05d",    123));
 	T(expect_success("00123",     "%005d",   123));
+	/* TODO(crbug.com/974084): This odd behavior should be fixed */
 	/* T(expect_success("+0123",     "%+05d",   123)); actual "0+123" */
 	/* T(expect_success("+0123",     "%+005d",  123)); actual "0+123" */
 
 	T(expect_success("  123",     "%*d",     5, 123));
 	T(expect_success(" +123",     "%+*d",    5, 123));
 	T(expect_success("00123",     "%0*d",    5, 123));
+	/* TODO(crbug.com/974084): This odd behavior should be fixed */
 	/* T(expect_success("00123",     "%00*d",   5, 123)); actual "ERROR" */
 	T(expect_success("0+123",     "%+0*d",   5, 123));
+	/* TODO(crbug.com/974084): This odd behavior should be fixed */
 	/* T(expect_success("0+123",     "%+00*d",  5, 123)); actual "ERROR" */
 
 	T(expect_success("123  ",     "%-5d",    123));
@@ -163,10 +173,25 @@ test_static int test_vsnprintf_strings(void)
 	T(expect_success("abc",       "%*s",     0, "abc"));
 	T(expect_success("a",         "%.1s",    "abc"));
 	T(expect_success("a",         "%.*s",    1, "abc"));
-	/* T(expect_success("",          "%.0s",    "abc"));    actual "abc" */
-	/* T(expect_success("",          "%.*s",    0, "abc")); actual "abc" */
-	T(expect_success("ab",        "%5.2s",   "abc")); /*    intentional  */
-	/* TODO(hesling): test for overruns on malformed strings */
+	T(expect_success("",          "%.0s",    "abc"));
+	T(expect_success("",          "%.*s",    0, "abc"));
+	/*
+	 * TODO(crbug.com/974084):
+	 * Ignoring the padding parameter is slightly odd behavior
+	 * and could use a review.
+	 */
+	T(expect_success("ab",        "%5.2s",   "abc"));
+	T(expect_success("abc",        "%.4s",   "abc"));
+
+	/*
+	 * Given a malformed string (address 0x1 is a good example),
+	 * if we ask for zero precision, expect no bytes to be read from
+	 * the malformed address and a blank output string.
+	 */
+	T(expect(output, sizeof(output),
+			EC_SUCCESS, "", sizeof(output),
+			"%.0s", (char *)1));
+
 	return EC_SUCCESS;
 }
 
@@ -176,16 +201,15 @@ test_static int test_vsnprintf_hexdump(void)
 
 	T(expect_success(err_str,     "%h",      bytes));
 	T(expect_success("005e",      "%.*h",    2, bytes));
-	/* T(expect_success("",          "%.*h",    0, bytes));  actual "ERROR" */
-	/* T(expect_success(" 005e",     "%5.*h",   2, bytes));  actual "005e"  */
-	/* T(expect_success("005e ",     "%-5.*h",  2, bytes));  actual "005e"  */
+	T(expect_success("",          "%.*h",    0, bytes));
+	T(expect_success(" 005e",     "%5.*h",   2, bytes));
+	T(expect_success("00",        "%0*.*h",  2, 0, bytes));
 	return EC_SUCCESS;
 }
 
 test_static int test_vsnprintf_combined(void)
 {
 	T(expect_success("abc", "%c%s", 'a', "bc"));
-	T(expect_success("12\tbc", "%d\t%s", 12, "bc"));
 	return EC_SUCCESS;
 }
 
