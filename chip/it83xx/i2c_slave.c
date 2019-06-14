5d02032e2835c3a10b7ca3da90aaabd743206dd9
tim <tim2.lin@ite.corp-partner.google.com>
2019-06-14 13:23:51 +0800

    it83xx/i2c_slave: the registers have updated in IT83202
    The DMA read and write target address registers have changed.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I9e63c3f7f402ed22c461eb86daa5eba0e6dcb58e
    Signed-off-by: tim <tim2.lin@ite.corp-partner.google.com>

--- chip/it83xx/i2c_slave.c
+++ chip/it83xx/i2c_slave.c
@@ -173,7 +173,7 @@ void i2c_slave_read_write_data(int port)
 			 * TODO(b:129360157): Handle master write
 			 * data by "in_data" array.
 			 */
-					CPRINTS("WData: %.*h\n",
+					CPRINTS("WData: %.*h",
 					I2C_MAX_BUFFER_SIZE, in_data[idx]);
 					wr_done[idx] = 0;
 				}
@@ -280,13 +280,31 @@ void i2c_slave_enable(int port, uint8_t slv_addr)
 		memset(in_data[idx], 0, I2C_MAX_BUFFER_SIZE);
 		memset(out_data[idx], 0, I2C_MAX_BUFFER_SIZE);
 
-		/* DMA write target address register */
-		IT83XX_I2C_RAMHA(ch) = ((uint32_t)in_data[idx] >> 8) & 0x0F;
-		IT83XX_I2C_RAMLA(ch) = (uint32_t)in_data[idx] & 0xFF;
+		if (IS_ENABLED(CHIP_CORE_NDS32)) {
+			/* DMA write target address register */
+			IT83XX_I2C_RAMHA(ch) =
+				((uint32_t)in_data[idx] >> 8) & 0x0F;
+			IT83XX_I2C_RAMLA(ch) = (uint32_t)in_data[idx] & 0xFF;
+			/* DMA read target address register */
+			IT83XX_I2C_RAMHA2(ch) =
+				((uint32_t)out_data[idx] >> 8) & 0x0F;
+			IT83XX_I2C_RAMLA2(ch) = (uint32_t)out_data[idx] & 0xFF;
 
-		/* DMA read target address register */
-		IT83XX_I2C_RAMHA2(ch) = ((uint32_t)out_data[idx] >> 8) & 0x0F;
-		IT83XX_I2C_RAMLA2(ch) = (uint32_t)out_data[idx] & 0xFF;
+		} else if (IS_ENABLED(CHIP_ILM_DLM_ORDER)) {
+			/* DMA write target address register */
+			IT83XX_I2C_RAMH2A(ch) =
+				((uint32_t)in_data[idx] >> 16) & 0xFF;
+			IT83XX_I2C_RAMHA(ch) =
+				((uint32_t)in_data[idx] >> 8) & 0xFF;
+			IT83XX_I2C_RAMLA(ch) = (uint32_t)in_data[idx] & 0xFF;
+			/* DMA read target address register */
+			IT83XX_I2C_CMD_ADDH2(ch) =
+				((uint32_t)out_data[idx] >> 16) & 0xFF;
+			IT83XX_I2C_CMD_ADDH(ch) =
+				((uint32_t)out_data[idx] >> 8) & 0xFF;
+			IT83XX_I2C_CMD_ADDL(ch) =
+				(uint32_t)out_data[idx] & 0xFF;
+		}
 
 		/* I2C module enable and command queue mode */
 		IT83XX_I2C_CTR1(ch) = IT83XX_I2C_COMQ_EN |
