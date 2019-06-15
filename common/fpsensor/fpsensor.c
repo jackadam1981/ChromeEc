348a7daaa0a4b46773598e7ab5662d487469f397
Yicheng Li <yichengli@chromium.org>
2019-06-14 18:21:17 -0700

    fpsensor: Implement command to read positive_match_secret.
    Add EC command to read positive_match_secret and set
    positive_match_secret_readable bit on enrollment or matching success.
    If the attempt to read is 5 seconds after the enrollment or match,
    the read is not allowed (the positive_match_secret_readable bit is
    cleared). Test that the command can read the data correctly and can
    read for each finger only once. Test that attempt to read secret
    after deadline will be rejected.
    
    BRANCH=nocturne
    BUG=chromium:927095
    TEST=make buildall
    TEST=tested enrollment, matching and multifinger on DUT nocturne
    
    Change-Id: Idc734c6392d271e2aaee1cddf7c2c5b81b727b4a
    Signed-off-by: Yicheng Li <yichengli@chromium.org>

--- common/fpsensor/fpsensor.c
+++ common/fpsensor/fpsensor.c
@@ -20,7 +20,6 @@
 #include "system.h"
 #include "task.h"
 #include "trng.h"
-#include "timer.h"
 #include "util.h"
 #include "watchdog.h"
 
@@ -133,11 +132,19 @@ static uint32_t fp_process_match(void)
 		res = fp_finger_match(fp_template[0], templ_valid, fp_buffer,
 				      &fgr, &updated);
 		CPRINTS("Match =>%d (finger %d)", res, fgr);
-		if (res < 0) {
+		if (res < 0 || fgr < 0 || fgr >= FP_MAX_FINGER_COUNT) {
 			res = EC_MKBP_FP_ERR_MATCH_NO_INTERNAL;
 			timestamps_invalid |= FPSTATS_MATCHING_INV;
 		} else {
+			timestamp_t now = get_time();
+
+			if (fp_pos_match_secret_readable)
+				CPRINTS("Error: positive match secret already "
+					"readable at match time!");
 			template_matched = (int8_t)fgr;
+			template_with_secret = (int8_t)fgr;
+			fp_pos_match_secret_readable = true;
+			read_secret_deadline.val = now.val + (5 * SECOND);
 		}
 		if (res == EC_MKBP_FP_ERR_MATCH_YES_UPDATED)
 			templ_dirty |= updated;
@@ -437,6 +444,13 @@ static int fp_command_frame(struct host_cmd_handler_args *args)
 			return EC_RES_UNAVAILABLE;
 		}
 		templ_dirty &= ~BIT(fgr);
+
+		if (fp_pos_match_secret_readable)
+			CPRINTS("Error: positive match secret already readable "
+				"when preparing new secret!");
+		template_with_secret = (int8_t)fgr;
+		fp_pos_match_secret_readable = true;
+		read_secret_deadline.val = now.val + (5 * SECOND);
 	}
 	memcpy(out, fp_enc_buffer + offset, size);
 	args->response_size = size;
@@ -514,6 +528,8 @@ static int fp_command_template(struct host_cmd_handler_args *args)
 			CPRINTS("fgr%d: Failed to derive key", idx);
 			return EC_RES_UNAVAILABLE;
 		}
+		memcpy(fp_encryption_salt[idx], enc_info->salt,
+		       sizeof(enc_info->salt));
 
 		ret = aes_gcm_decrypt(key, SBP_ENC_KEY_LEN, fp_template[idx],
 				      fp_enc_buffer + sizeof(*enc_info),
