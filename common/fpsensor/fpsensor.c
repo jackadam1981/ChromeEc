21c84a75452e6073b425c59e2f2e063053f2c14b
Yicheng Li <yichengli@chromium.org>
2019-06-14 18:21:17 -0700

    fpsensor: Implement command to read positive_match_secret.
    Add EC command to read positive_match_secret on match success.
    If the attempt to read is 5 seconds after the match, the read is not
    allowed (the readable bit for positive match secret is cleared).
    Test that the command can read the data correctly and can
    read for each finger only once. Test that attempt to read secret
    after deadline will be rejected.
    
    BRANCH=nocturne
    BUG=chromium:927095
    TEST=make buildall
    TEST=tested enrollment, matching and multifinger on DUT nocturne
    TEST=tested that if biod requests to download template and secret
    for a finger that's not currently matched, reading secret will fail.
    
    Change-Id: Idc734c6392d271e2aaee1cddf7c2c5b81b727b4a
    Signed-off-by: Yicheng Li <yichengli@chromium.org>

--- common/fpsensor/fpsensor.c
+++ common/fpsensor/fpsensor.c
@@ -20,7 +20,6 @@
 #include "system.h"
 #include "task.h"
 #include "trng.h"
-#include "timer.h"
 #include "util.h"
 #include "watchdog.h"
 
@@ -58,7 +57,6 @@ static uint32_t matching_time_us;
 static uint32_t overall_time_us;
 static timestamp_t overall_t0;
 static uint8_t timestamps_invalid;
-static int8_t template_matched;
 
 BUILD_ASSERT(sizeof(struct ec_fp_template_encryption_metadata) % 4 == 0);
 
@@ -131,20 +129,22 @@ static uint32_t fp_process_match(void)
 	timestamp_t t0 = get_time();
 	int res = -1;
 	uint32_t updated = 0;
-	int32_t fgr = -1;
+	int32_t fgr = FP_NO_SUCH_TEMPLATE;
 
 	/* match finger against current templates */
-	template_matched = -1;
+	template_matched = FP_NO_SUCH_TEMPLATE;
 	CPRINTS("Matching/%d ...", templ_valid);
 	if (templ_valid) {
 		res = fp_finger_match(fp_template[0], templ_valid, fp_buffer,
 				      &fgr, &updated);
 		CPRINTS("Match =>%d (finger %d)", res, fgr);
-		if (res < 0) {
+		if (res < 0 || fgr < 0 || fgr >= FP_MAX_FINGER_COUNT) {
 			res = EC_MKBP_FP_ERR_MATCH_NO_INTERNAL;
 			timestamps_invalid |= FPSTATS_MATCHING_INV;
 		} else {
 			template_matched = (int8_t)fgr;
+			fp_enable_positive_match_secret(fgr,
+				&positive_match_secret_state);
 		}
 		if (res == EC_MKBP_FP_ERR_MATCH_YES_UPDATED)
 			templ_dirty |= updated;
@@ -425,9 +425,6 @@ static int fp_command_frame(struct host_cmd_handler_args *args)
 		rand_bytes(enc_info->salt, FP_CONTEXT_SALT_BYTES);
 		exit_trng();
 
-		memcpy(fp_encryption_salt[fgr], enc_info->salt,
-		       sizeof(fp_encryption_salt[0]));
-
 		ret = derive_encryption_key(key, enc_info->salt);
 		if (ret != EC_SUCCESS) {
 			CPRINTS("fgr%d: Failed to derive key", fgr);
@@ -445,6 +442,8 @@ static int fp_command_frame(struct host_cmd_handler_args *args)
 			return EC_RES_UNAVAILABLE;
 		}
 		templ_dirty &= ~BIT(fgr);
+		memcpy(fp_encryption_salt[fgr], enc_info->salt,
+		       sizeof(fp_encryption_salt[0]));
 	}
 	memcpy(out, fp_enc_buffer + offset, size);
 	args->response_size = size;
@@ -536,6 +535,8 @@ static int fp_command_template(struct host_cmd_handler_args *args)
 			return EC_RES_UNAVAILABLE;
 		}
 		templ_valid++;
+		memcpy(fp_encryption_salt[idx], enc_info->salt,
+		       sizeof(enc_info->salt));
 	}
 
 	return EC_RES_SUCCESS;
