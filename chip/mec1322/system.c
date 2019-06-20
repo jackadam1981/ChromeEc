983ec88d1532b07d6d2488fd5ec23dec50e3a956
Daisuke Nojiri <dnojiri@chromium.org>
2019-06-19 19:11:04 -0700

    mec1322: Disable HTIMER IRQ and counter after wake-up
    This patch makes EC disable HTIMER IRQ and counter after it wakes up
    from hibernation.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b/134973675
    BRANCH=strago
    TEST=Verify sabin repeats hibernate and wake-up periodically.
    TEST=Verify sabin boots from hibernation when a power button is pressed.
    
    Change-Id: I3c9404e96c7f1ac5525adc8313847cf7e63af159

--- chip/mec1322/system.c
+++ chip/mec1322/system.c
@@ -116,8 +116,6 @@ void _system_reset(int flags, int wake_from_hibernate)
 
 	if (MEC1322_INT_SOURCE(17) & MEC1322_INT_SOURCE_HTIMER) {
 		save_flags |= RESET_FLAG_TIMER;
-		/* Disable HTIMER interrupt */
-		MEC1322_INT_DISABLE(17) |= MEC1322_INT_SOURCE_HTIMER;
 		/* Clear the bit */
 		MEC1322_INT_SOURCE(17) |= MEC1322_INT_SOURCE_HTIMER;
 	}
@@ -303,9 +301,16 @@ void system_hibernate(uint32_t seconds, uint32_t microseconds)
 			MEC1322_HTIMER_PRELOAD =
 				(seconds * 1000000 + microseconds) * 2 / 71;
 		}
-	}
 
-	asm("wfi");
+		asm("wfi");
+
+		/* Disable HTIMER interrupt */
+		MEC1322_INT_DISABLE(17) |= MEC1322_INT_SOURCE_HTIMER;
+		/* Disable hibernation counter */
+		MEC1322_HTIMER_PRELOAD = 0;
+	} else {
+		asm("wfi");
+	}
 
 	/* Use 48MHz clock to speed through wake-up */
 	MEC1322_PCR_PROC_CLK_CTL = 1;
