93e5d52cb0af226df5e3490b1fcfa59869f2217e
Ayushee <ayushee.shah@intel.com>
2019-06-21 02:42:32 -0700

    usb_pd: Adding support for compatible Thunderbolt mode
    Thunderbolt is a hardware interface that allows connection of external
    peripherals to a computer. This code enables thunderbolt 3 over Type-C
    interface. Thunderbolt provides concurrent support for PCIe transactions
    and DisplayPort and the thunderbolt controller provides isochronous
    communication on a single network style interface, allowing a Host
    computer to communicate at high bandwidth with multiple data/display
    devices through a single physical connection.
    
    The USB Type-C specification allows for Alternate Modes to be supported
    on the connector and cables. This CL adds a Discovery flow to determine
    if an Thunderbolt Alternate Mode is supported and a mechanism for
    switching in thunderbolt mode.
    
    TBT USB PD flow:
    -------------------Explicit contact---------------------
    
    Discover Identity(SOP') --- Check and store response
    			    (CL:1707851 and CL: 1553898)
    				|
    			Is cable super speed?
    	    -------- Yes -------|----- No ---- Disable TBT
    	    |
    Discover Identity(SOP) ---- Modal operation supported?
    				|
    	    ---------- ACK -----|---- NACK --- Disable TBT
    	    |
    Discover SVID (SOP) -------- SVID == 0x8087?
    				|
    	 ------------- Yes -----|------ No ---- Disable TBT
    	 |
    Discover SVID (SOP') -------- SVID == 0x8087?
                                    |
             ----------- yes------- | - No - Limit TBT to passive Gen 2
             |
    Discover Mode (SOP) ------ Store device response
    		---------------|
    		|
    Discover Mode (SOP') ----- Store cable response
                                    |
                            Enter Safe mode
    				|
    			  Active cable?
    	     --------- Yes -----|-------- No -- -----
    	    |                                       |
    Enter TBT mode (SOP' SOP" SOP)              Enter TBT mode SOP
    
    BUG=b:140643923
    BRANCH=none
    TEST=Tested on Tglrvp-u able to enter into Thunderbolt compatible mode
    
    Change-Id: I0629ccc81c4e7a7399d15a48475276130a698e95
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -2226,7 +2226,7 @@ static void pd_vdm_send_state_machine(int port)
 			 * If there is no ack from the cable, its a non-emark
 			 * cable and since, the pd flow should continue
 			 * irrespective of cable response, sending
-			 * discover_svid so the pd flow remains intact.
+			 * discover_identity so the pd flow remains intact.
 			 */
 			if (res < 0) {
 				header = PD_HEADER(PD_DATA_VENDOR_DEF,
@@ -2236,7 +2236,7 @@ static void pd_vdm_send_state_machine(int port)
 						   (int)pd[port].vdo_count,
 						   pd_get_rev(port), 0);
 				pd[port].vdo_data[0] =
-					VDO(USB_SID_PD, 1, CMD_DISCOVER_SVID);
+					VDO(USB_SID_PD, 1, CMD_DISCOVER_IDENT);
 				res = pd_transmit(port, TCPC_TX_SOP, header,
 						  pd[port].vdo_data);
 				reset_pd_cable(port);
@@ -2851,6 +2851,15 @@ void pd_interrupt_handler_task(void *p)
 	}
 }
 #endif /* HAS_TASK_PD_INT_C0 || HAS_TASK_PD_INT_C1 || HAS_TASK_PD_INT_C2 */
+
+#if defined(CONFIG_USBC_SS_MUX) && defined(CONFIG_USB_PD_TBT_COMPAT_MODE)
+void set_tbt_compat_mode_ready(int port)
+{
+	/* Set usb mux to thunderbolt compat mode */
+	usb_mux_set(port, TYPEC_MUX_TBT_COMPAT, USB_SWITCH_CONNECT,
+		pd[port].polarity);
+}
+#endif
 
 void pd_task(void *u)
 {
@@ -3675,6 +3684,8 @@ void pd_task(void *u)
 			if (pd[port].data_role == PD_ROLE_DFP &&
 			    (pd[port].flags & PD_FLAGS_CHECK_IDENTITY)) {
 #ifndef CONFIG_USB_PD_SIMPLE_DFP
+				if (IS_ENABLED(CONFIG_USB_PD_TBT_COMPAT_MODE))
+					enable_transmit_sop_prime(port);
 				pd_send_vdm(port, USB_SID_PD,
 					    CMD_DISCOVER_IDENT, NULL, 0);
 #endif
@@ -4266,6 +4277,8 @@ void pd_task(void *u)
 			/* If DFP, send discovery SVDMs */
 			if (pd[port].data_role == PD_ROLE_DFP &&
 			     (pd[port].flags & PD_FLAGS_CHECK_IDENTITY)) {
+				if (IS_ENABLED(CONFIG_USB_PD_TBT_COMPAT_MODE))
+					enable_transmit_sop_prime(port);
 				pd_send_vdm(port, USB_SID_PD,
 					    CMD_DISCOVER_IDENT, NULL, 0);
 				pd[port].flags &= ~PD_FLAGS_CHECK_IDENTITY;
