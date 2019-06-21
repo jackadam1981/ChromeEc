ae3bc07d263d7b699a712b73eaeafcc6cee4cc3c
Ayushee <ayushee.shah@intel.com>
2019-06-21 02:42:32 -0700

    TCPMv1: Add support for Thunderbolt-compatible mode
    Thunderbolt is a hardware interface that allows connection of external
    peripherals to a computer. This code enables thunderbolt 3 over Type-C
    interface. Thunderbolt provides concurrent support for PCIe transactions
    and DisplayPort and the thunderbolt controller provides isochronous
    communication on a single network style interface, allowing a Host
    computer to communicate at high bandwidth with multiple data/display
    devices through a single physical connection.
    
    The USB Type-C specification allows for Alternate Modes to be supported
    on the connector and cables. This CL adds a Discovery flow to determine
    if an Thunderbolt Alternate Mode is supported and a mechanism for
    switching in thunderbolt mode.
    
    TBT USB PD flow:
    -------------------Explicit contact---------------------
    
    Discover Identity(SOP) ---- Modal operation supported?
    				|
                ---------- ACK -----|---- NACK --- Disable TBT
                |
    Discover Identity(SOP') --- Check and store response
    			    (CL:1707851 and CL:1553898)
    				|
    			Is cable super speed?
    	    -------- Yes -------|----- No ---- Disable TBT
    	    |
    Discover SVID (SOP) -------- SVID == 0x8087?
    				|
    	 ------------- Yes -----|------ No ---- Disable TBT
    	 |
    Discover SVID (SOP') -------- SVID == 0x8087?
                                    |
             ----------- yes------- | - No - Limit TBT to passive Gen 2
             |
    Discover Mode (SOP) ------ Store device response
    		---------------|
    		|
    Discover Mode (SOP') ----- Store cable response
                                    |
                            Enter Safe mode
    				|
    			  Active cable?
    	     --------- Yes -----|-------- No ---------
    	    |                                         |
    Enter TBT mode (SOP' SOP" SOP)                 Enter TBT mode SOP
    (Note: This is not implemented in this CL)
    
    BUG=b:140643923
    BRANCH=none
    TEST=Tested on Tglrvp-u able to enter into Thunderbolt-compatible mode
    
    Change-Id: I65ed86e9f32d36fe8e30c3285f516b9871f3e119
    Signed-off-by: Ayushee <ayushee.shah@intel.com>
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/usb_common.c
+++ common/usb_common.c
@@ -931,6 +931,41 @@ __overridable int svdm_gfu_attention(int port, uint32_t *payload)
 	return 0;
 }
 
+__overridable int svdm_tbt_compat_enter_mode(int port, uint32_t mode_caps)
+{
+	if (IS_ENABLED(CONFIG_USB_PD_TBT_COMPAT_MODE)) {
+		/*
+		 * Before entering into alternate mode, state of the
+		 * USB-C MUX needs to be in safe mode
+		 * Ref: USB Type-C Cable and Connector Specification
+		 * Section E.2.2 Alternate Mode Electrical Requirements
+		 */
+		usb_mux_set(port, IS_ENABLED(CONFIG_USB_MUX_VIRTUAL) ?
+			TYPEC_MUX_SAFE : TYPEC_MUX_NONE, USB_SWITCH_CONNECT,
+			pd_get_polarity(port));
+	}
+	return 0;
+}
+
+__overridable void svdm_tbt_compat_exit_mode(int port)
+{
+}
+
+__overridable int svdm_tbt_compat_status(int port, uint32_t *payload)
+{
+	return 0;
+}
+
+__overridable int svdm_tbt_compat_config(int port, uint32_t *payload)
+{
+	return 0;
+}
+
+__overridable int svdm_tbt_compat_attention(int port, uint32_t *payload)
+{
+	return 0;
+}
+
 const struct svdm_amode_fx supported_modes[] = {
 	{
 		.svid = USB_SID_DISPLAYPORT,
@@ -949,7 +984,16 @@ const struct svdm_amode_fx supported_modes[] = {
 		.config = &svdm_gfu_config,
 		.attention = &svdm_gfu_attention,
 		.exit = &svdm_exit_gfu_mode,
-	}
+	},
+
+	{
+		.svid = USB_VID_INTEL,
+		.enter = &svdm_tbt_compat_enter_mode,
+		.status = &svdm_tbt_compat_status,
+		.config = &svdm_tbt_compat_config,
+		.attention = &svdm_tbt_compat_attention,
+		.exit = &svdm_tbt_compat_exit_mode,
+	},
 };
 const int supported_modes_cnt = ARRAY_SIZE(supported_modes);
 #endif /* CONFIG_USB_PD_ALT_MODE_DFP */
