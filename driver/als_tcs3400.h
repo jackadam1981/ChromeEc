7f44ac7575c5c2f79799d3ce70bdc74276c05aae
Nick Vaccaro <nvaccaro@google.com>
2019-06-25 17:38:37 -0700

    driver/tcs3400: improve efficiency of auto-adjust
    Adds an array which holds the number of atime increments needed
    at various lux levels and again settings to change delta from
    saturation by 1%.  Using this table, the anti-saturation
    algorithm can get to the 90% band in most cases in a single
    adjustment vs the 10 to 15 adjustments previously needed when
    not using this table.
    
    Enabling CONFIG_TCS_USE_LUX_TABLES takes up 280 bytes in the
    ec.obj.
    
    BUG=b:124512628
    BRANCH=master
    TEST=Flash and boot flapjack, verify that ALS and RGB sensors
    are still generating data.  I used alslog patch and enabled
    ALS logging in EC console via "alslog 2047".  Verify that under
    a constant light source, the adjustment mechanism correctly drives
    the ALS values such that they land in the sweet spot between 90
    to <100% of saturation.
    
    Change-Id: I7cd0b8242e72233f50ff2751834f7bba367c6706
    Signed-off-by: Nick Vaccaro <nvaccaro@google.com>

--- driver/als_tcs3400.h
+++ driver/als_tcs3400.h
@@ -97,6 +97,11 @@ enum tcs3400_mode {
 #define TCS_CALIBRATION_ATIME   TCS_MIN_ATIME
 #define TCS_GAIN_UPSHIFT_ATIME  TCS_MAX_ATIME
 
+/* Number of different ranges supported for atime adjustment support */
+#define TCS_MAX_ATIME_RANGES    13
+#define TCS_GAIN_TABLE_MAX_LUX  12999
+#define TCS_ATIME_GAIN_FACTOR   100     /* table values are 100x actual value */
+
 #define TCS_MIN_AGAIN           0x00            /* 1x gain */
 #define TCS_MAX_AGAIN           0x03            /* 64x gain */
 #define TCS_CALIBRATION_AGAIN   0x02            /* 16x gain */
@@ -131,6 +136,8 @@ enum tcs3400_mode {
  * method will drive towards.
  */
 #define TSC_SATURATION_LOW_BAND_PERCENT 90
+#define TSC_SATURATION_LOW_BAND_VALUE	(TCS_SATURATION_LEVEL * \
+					TSC_SATURATION_LOW_BAND_PERCENT / 100)
 
 enum crbg_index {
 	CLEAR_CRGB_IDX = 0,
