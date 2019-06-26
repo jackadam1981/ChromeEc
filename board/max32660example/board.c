0989e2003a352bb776cd05fa5950a460a9c3f22f
Jerry Bradshaw <jerry.bradshaw@maximintegrated.com>
2019-06-26 11:52:15 -0500

    made code review changes
    Change-Id: If0dacb64dbb5c03afe6166b867ded4e6d6e4c1db

--- board/max32660example/board.c
+++ board/max32660example/board.c
@@ -14,16 +14,6 @@
 #include "timer.h"
 #include "registers.h"
 #include "util.h"
-
-/* I2C ports */
-#if 0
-const struct i2c_port_t i2c_ports[] = {
-		{"sensor_oled", 1, 100},
-};
-
-const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports); 
-#endif
-
 #include "gpio_list.h"
 
 #define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
--- board/max32660example/board.h
+++ board/max32660example/board.h
@@ -10,10 +10,6 @@
 
 /* Optional features */
 #define CONFIG_SYSTEM_UNLOCKED /* Allow dangerous commands */
-#if 0
-#define CONFIG_I2C
-#define CONFIG_I2C_MASTER
-#endif
 
 #define CONFIG_FPU
 
@@ -21,15 +17,6 @@
 #undef CONFIG_LID_SWITCH
 #undef CONFIG_PECI
 #undef CONFIG_SWITCH
-
-#if 0
-#define CONFIG_CMD_HOSTCMD
-#endif
-
-#if 0
-#undef CONFIG_HOSTCMD_EVENTS
-#define CONFIG_I2C
-#endif
 
 /* #define CONFIG_I2C_SLAVE */
 /* #define CONFIG_HOSTCMD_I2C_SLAVE_ADDR (0x51 << 1) */
@@ -42,24 +29,10 @@
 
 #ifndef __ASSEMBLER__
 
-/* Slave I2C port configuration */
-#if 0
-#define I2C_PORT_SLAVE 0
-#define I2C_PORT_EC I2C_PORT_SLAVE
-
-/* I2C ports */
-#define I2C_PORT_TEMPERATURE 1
-#define I2C_PORT_OLED 1
-#endif
-
 /* Second UART port */
 #define CONFIG_UART_HOST 1
 
 #include "gpio_signal.h"
-
-#if 0
-void button_interrupt(enum gpio_signal signal);
-#endif
 
 #endif /* !__ASSEMBLER__ */
 
