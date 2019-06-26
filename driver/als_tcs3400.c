7f4fda9654fc9f05329487473dd35b51c42039da
Nick Vaccaro <nvaccaro@google.com>
2019-06-25 17:38:37 -0700

    driver/tcs3400: improve efficiency of auto-adjust
    Adds an array which holds the number of atime increments needed
    at various lux levels and again settings to change delta from
    saturation by 1%.  Using this table, the anti-saturation algorithm
    can get to the 90% band in most cases in a single adjustment vs the
    10 to 15 adjustments previously needed when not using this table.
    
    BUG=b:124512628
    BRANCH=master
    TEST=Flash and boot flapjack, verify that ALS and RGB sensors
    are still generating data.  I used alslog patch and enabled
    ALS logging in EC console via "alslog 2047".  Verify that under
    a constant light source, the adjustment mechanism correctly drives
    the ALS values such that they land in the sweet spot between 90
    to <100% of saturation.
    
    Change-Id: I7cd0b8242e72233f50ff2751834f7bba367c6706
    Signed-off-by: Nick Vaccaro <nvaccaro@google.com>

--- driver/als_tcs3400.c
+++ driver/als_tcs3400.c
@@ -21,6 +21,28 @@
 static volatile uint32_t last_interrupt_timestamp;
 #endif
 
+/*
+ * 0 = use a step constant when adjusting atime
+ * 1 = use the Lux atime table when determining how to adjust atime
+ */
+#define TCS_USE_LUX_TABLE 0
+
+#if TCS_USE_LUX_TABLE
+/*
+ * Stores the number of atime increments/decrements needed to change light value
+ * by 1% of saturation for each gain setting for each predefined LUX range.
+ *
+ * Values in array are TCS_ATIME_GAIN_FACTOR (100x) times actual value to allow
+ * for fractions using integers.
+ */
+static const uint32_t
+range_atime[TCS_MAX_AGAIN-TCS_MIN_AGAIN+1][TCS_MAX_ATIME_RANGES] = {
+{ 11300, 11300, 11300, 11300, 11300, 5650, 5650, 5650, 3766, 3766, 2825, 2825 },
+{ 11300, 11300, 3766, 2260, 1883, 1412, 1130, 1130, 869, 807, 706, 627 },
+{ 11300, 1883, 807, 565, 418, 322, 264, 256, 203, 194, 187, 300 },
+{ 3766, 418, 207, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+#endif /* TCS_USE_LUX_TABLE */
+
 static inline int tcs3400_i2c_read8(const struct motion_sensor_t *s,
 				    int reg, int *data)
 {
@@ -95,16 +117,31 @@ static int tcs3400_rgb_read(const struct motion_sensor_t *s, intv3_t v)
 	return EC_SUCCESS;
 }
 
+#if TCS_USE_LUX_TABLE
+static void
+decrement_atime(struct tcs_saturation_t *sat_p, uint16_t cur_lux, int percent)
+{
+	int atime, steps;
+	int lux = cur_lux;
+
+	if (lux > TCS_GAIN_TABLE_MAX_LUX)
+		lux = TCS_GAIN_TABLE_MAX_LUX;
+	steps = percent * range_atime[sat_p->again][lux/1000] /
+		TCS_ATIME_GAIN_FACTOR;
+	atime = sat_p->atime - steps;
+	sat_p->atime = MAX(atime, TCS_MIN_ATIME);
+}
+#else
 static void decrement_atime(struct tcs_saturation_t *sat_p)
 {
 	sat_p->atime = MAX(sat_p->atime - TCS_ATIME_DEC_STEP, TCS_MIN_ATIME);
 }
+#endif
 
 static void increment_atime(struct tcs_saturation_t *sat_p)
 {
 	sat_p->atime = MIN(sat_p->atime + TCS_ATIME_INC_STEP, TCS_MAX_ATIME);
 }
-
 /*
  * tcs3400_adjust_sensor_for_saturation() tries to keep CRGB values as
  * close to saturation as possible without saturating by implementing
@@ -129,6 +166,7 @@ tcs3400_adjust_sensor_for_saturation(struct motion_sensor_t *s,
 	uint16_t max_val =  0;
 	int ret = EC_SUCCESS;
 	int status = 0;
+	int percent_left = 0;
 
 	/* Adjust for saturation if needed */
 	ret = tcs3400_i2c_read8(s, TCS_I2C_STATUS, &status);
@@ -151,14 +189,30 @@ tcs3400_adjust_sensor_for_saturation(struct motion_sensor_t *s,
 		/* value < 90% saturation, try to increase sensitivity */
 		/* increase AGAIN if we can without saturating */
 		if (max_val <= TCS_GAIN_SAT_LEVEL) {
-			if (sat_p->again < TCS_MAX_AGAIN)
+			if (sat_p->again < TCS_MAX_AGAIN) {
 				sat_p->again++;
-			else if (sat_p->atime > TCS_MIN_ATIME)
+			} else if (sat_p->atime > TCS_MIN_ATIME) {
 				/* increase ATIME */
+				percent_left = (max_val * 100 /
+						TCS_GAIN_SAT_LEVEL);
+				percent_left = TSC_SATURATION_LOW_BAND_PERCENT -
+						percent_left;
+#if TCS_USE_LUX_TABLE
+				decrement_atime(sat_p, max_val, percent_left);
+#else
 				decrement_atime(sat_p);
+#endif
+			}
 		} else if (sat_p->atime > TCS_MIN_ATIME) {
 			/* increase ATIME */
+			percent_left = (max_val * 100 / TCS_GAIN_SAT_LEVEL);
+			percent_left = TSC_SATURATION_LOW_BAND_PERCENT -
+					percent_left;
+#if TCS_USE_LUX_TABLE
+			decrement_atime(sat_p, max_val, percent_left);
+#else
 			decrement_atime(sat_p);
+#endif
 		} else if (sat_p->again < TCS_MAX_AGAIN) {
 			/*
 			 * Although we're not at maximum gain yet, we
