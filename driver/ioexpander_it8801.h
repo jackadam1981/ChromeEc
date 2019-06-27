d8bc9c74ad338af7cd77118deda4a31b595c0e35
tim <tim2.lin@ite.corp-partner.google.com>
2019-06-27 11:08:50 +0800

    driver/ioexpander_it8801: add I/O expander driver
    IT8801 is an I/O expander with the keyboard matrix controller.
    
    BUG=none
    BRANCH=none
    TEST=Press any key on keyboard and the console command
    "ksstate on" can print keyboard scan state.
    
    Change-Id: I6903aad1eacfdfb6a7c6c04e6954a14e8c8caaac
    Signed-off-by: tim <tim2.lin@ite.corp-partner.google.com>

---
+++ driver/ioexpander_it8801.h
@@ -0,0 +1,36 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * IT8801 is an I/O expander with the keyboard matrix controller.
+ *
+ */
+
+#ifndef __CROS_EC_IO_EXPANDER_IT8801_H
+#define __CROS_EC_IO_EXPANDER_IT8801_H
+
+/* I2C slave address(8-bit) */
+#define IT8801_REG_ADDR 0x70
+
+/* Keyboard Matrix Scan control (KBS) */
+#define IT8801_REG_KSOMCR         0x40
+#define IT8801_REG_MASK_KSOSDIC   BIT(7)
+#define IT8801_REG_MASK_KSE       BIT(6)
+#define IT8801_REG_MASK_AKSOSC    BIT(5)
+#define IT8801_REG_KSIDR          0x41
+#define IT8801_REG_KSIEER         0x42
+#define IT8801_REG_KSIIER         0x43
+#define IT8801_REG_SMBCR          0xfa
+#define IT8801_REG_MASK_ARE       BIT(4)
+#define IT8801_REG_GIECR          0xfb
+#define IT8801_REG_MASK_GKSIIE    BIT(3)
+#define IT8801_REG_GPIO00         0x0a
+#define IT8801_REG_GPIO01         0x0b
+#define IT8801_REG_GPIO22         0x1c
+#define IT8801_REG_GPIO23         0x1d
+#define IT8801_REG_MASK_GPIOAFS   BIT(6)
+
+/* ISR for IT8801's SMB_INT# */
+void io_expander_interrupt(enum gpio_signal signal);
+
+#endif /* __CROS_EC_KBEXPANDER_IT8801_H */
