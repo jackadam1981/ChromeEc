1e308fe313933bb53fda67194385612fd624ba6f
Yicheng Li <yichengli@chromium.org>
2019-07-02 19:11:38 -0700

    fpsensor: Send/receive positive match salt and finger id with template.
    In EC_CMD_FP_FRAME, send positive match salt and finger id along with
    the template to the host, so that biod can compute validation value.
    In EC_CMD_FP_TEMPLATE, receive positive match salt and finger id from
    the host, and immediately re-derive positive match secret, so that
    FPMCU is in sync with biod when user deletes template.
    
    BRANCH=nocturne
    BUG=chromium:927095
    TEST=make -j buildall
    TEST=tested enrollment, matching and multifinger on DUT nocturne
    TEST=verified that the positive match salt and finger id sent through
    EC_CMD_FP_FRAME and those received through EC_CMD_FP_TEMPLATE are
    the same as the initial derived value, by printing to log (the printing
    statements are deleted before commit).
    
    Change-Id: I4bc2a878ecb1f57ba185bddd2e5669b67f435198
    Signed-off-by: Yicheng Li <yichengli@chromium.org>

--- common/fpsensor/fpsensor.c
+++ common/fpsensor/fpsensor.c
@@ -450,6 +450,12 @@ static int fp_command_frame(struct host_cmd_handler_args *args)
 			CPRINTS("fgr%d: Failed to encrypt template", fgr);
 			return EC_RES_UNAVAILABLE;
 		}
+		memcpy(fp_enc_buffer + sizeof(*enc_info)
+			+ sizeof(fp_template[0]),
+		       fp_pos_match_salt[fgr], FP_POS_MATCH_SALT_BYTES);
+		memcpy(fp_enc_buffer + sizeof(*enc_info)
+			+ sizeof(fp_template[0]) + FP_POS_MATCH_SALT_BYTES,
+		       finger_id[fgr], FP_FINGER_ID_BYTES);
 		templ_dirty &= ~BIT(fgr);
 	}
 	memcpy(out, fp_enc_buffer + offset, size);
@@ -540,6 +546,23 @@ static int fp_command_template(struct host_cmd_handler_args *args)
 			fp_clear_finger_context(idx);
 			return EC_RES_UNAVAILABLE;
 		}
+		memcpy(fp_pos_match_salt[idx],
+		       fp_enc_buffer + sizeof(*enc_info)
+			+ sizeof(fp_template[0]),
+		       FP_POS_MATCH_SALT_BYTES);
+		memcpy(finger_id[idx],
+		       fp_enc_buffer + sizeof(*enc_info)
+			+ sizeof(fp_template[0])
+			+ FP_POS_MATCH_SALT_BYTES,
+		       FP_FINGER_ID_BYTES);
+	ret = derive_pos_match_secret(fp_pos_match_secret[idx],
+				      fp_pos_match_salt[idx],
+				      finger_id[idx]);
+	if (ret != EC_RES_SUCCESS) {
+		CPRINTS("fgr%d: Failed to re-derive positive match secret",
+			idx);
+		return EC_RES_UNAVAILABLE;
+	}
 		templ_valid++;
 	}
 
