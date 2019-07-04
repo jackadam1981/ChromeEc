a361cb786b0795c898889ea81b167ec2848a5ce9
Hu, Hebo <hebo.hu@intel.com>
2019-07-04 10:32:52 +0800

    ish: fix aon task not auto rebuild issue in increment build
    aon task image build rules are lack of dependent rules of source code,
    so can't track the source code changes and trigger auto build.
    
    Refactor build rules for aon task to make sure always auto rebuild when
    aon task's source code and dependent header files update
    
    BUG=b:136691893
    BRANCH=none
    TEST= ish aon task should alwasy rebuild when it's code and dependent
    header files update
    
    Change-Id: I0d8c7c6a4a2b7e99d724b88b233e09a29b8facea
    Signed-off-by: Hu, Hebo <hebo.hu@intel.com>

--- chip/ish/build.mk
+++ chip/ish/build.mk
@@ -30,26 +30,51 @@ chip-$(CONFIG_LOW_POWER_IDLE)+=power_mgt.o
 # There is no framework for on-board tests in ISH. Do not specify any.
 test-list-y=
 
-ifeq ($(CONFIG_ISH_PM_AONTASK),y)
+# Build ish aon task fw
+ish-aon-name=ish_aontask
+ish-aon-$(CONFIG_ISH_PM_AONTASK)=aontaskfw/ish_aontask.o dma.o
+ish-aon-lds-templet-$(CONFIG_ISH_PM_AONTASK)=aontaskfw/ish_aontask.ld.in
 
-ish-aontask-fw=chip/ish/aontaskfw/ish_aontask
-ish-aontask-dma=chip/ish/dma
-ish-aontask-fw-bin=$(out)/$(ish-aontask-fw).bin
-PROJECT_EXTRA+=$(ish-aontask-fw-bin)
+# Rules for building ish aon task fw
+ish-aon-out=$(out)/aontaskfw
+ish-aon-elf-$(CONFIG_ISH_PM_AONTASK)=$(ish-aon-out)/$(ish-aon-name).elf
+ish-aon-map-$(CONFIG_ISH_PM_AONTASK)=$(ish-aon-out)/$(ish-aon-name).map
+ish-aon-bin-$(CONFIG_ISH_PM_AONTASK)=$(ish-aon-out)/$(ish-aon-name).bin
+ish-aon-lds-$(CONFIG_ISH_PM_AONTASK)=$(ish-aon-out)/$(ish-aon-name).ld
 
-_aon_size_str=$(shell stat -L -c %s $(ish-aontask-fw-bin))
+ish-aon-objs=$(call objs_from_dir,$(ish-aon-out)/chip/$(CHIP),ish-aon)
+ish-aon-deps=$(addsuffix .d, $(ish-aon-objs))
+
+PROJECT_EXTRA+=$(ish-aon-bin-y)
+deps-$(CONFIG_ISH_PM_AONTASK)+=$(ish-aon-deps)
+
+$(out)/$(PROJECT).bin: $(ish-aon-bin-y) $(out)/RW/$(PROJECT).RW.flat
+
+$(ish-aon-bin-y): $(ish-aon-lds-y) $(ish-aon-objs)
+	$(if $(V),,@echo '  EXTBIN ' $(subst $(out)/,,$@) ; )
+	-@ $(CC) $(ish-aon-objs)   $(LDFLAGS)                 \
+		-o $(ish-aon-elf-y) -Wl,-T,$(ish-aon-lds-y)    \
+		-Wl,-Map,$(ish-aon-map-y)
+	-@ $(OBJCOPY) -O binary $(ish-aon-elf-y)  $@
+
+$(ish-aon-lds-y): chip/$(CHIP)/$(ish-aon-lds-templet-y)
+	-@ mkdir -p $(@D)
+	@ $(CC) $(CFLAGS) -x assembler-with-cpp -E -P $< -o $@
+
+$(ish-aon-out)/%.o: %.c
+	-@ mkdir -p $(@D)
+	$(call quiet,c_to_o,CC     )
+
+
+# Location of the scripts used to pack image
+SCRIPTDIR:=./chip/${CHIP}/util
+
+# Calculate aon binary file size and kernel binary file size
+_aon_size_str=$(shell stat -L -c %s $(ish-aon-bin-y))
 _aon_size=$(shell echo "$$(($(_aon_size_str)))")
-
-$(out)/$(PROJECT).bin: $(ish-aontask-fw-bin) $(out)/RW/$(PROJECT).RW.flat
-
-endif
 
 _kernel_size_str=$(shell stat -L -c %s $(out)/RW/$(PROJECT).RW.flat)
 _kernel_size=$(shell echo "$$(($(_kernel_size_str)))")
-
-# location of the scripts used to pack image
-SCRIPTDIR:=./chip/${CHIP}/util
-
 
 # Commands to convert ec.RW.flat to $@.tmp - This will add the manifest header
 # needed to load the FW onto the ISH HW.
@@ -58,7 +83,7 @@ ifeq ($(CONFIG_ISH_PM_AONTASK),y)
 cmd_obj_to_bin = ${SCRIPTDIR}/pack_ec.py -o $@.tmp \
 		 -k $(out)/RW/$(PROJECT).RW.flat \
 		 --kernel-size $(_kernel_size) \
-		 -a $(ish-aontask-fw-bin)  \
+		 -a $(ish-aon-bin-y)  \
 		 --aon-size $(_aon_size);
 else
 cmd_obj_to_bin = ${SCRIPTDIR}/pack_ec.py -o $@.tmp \
