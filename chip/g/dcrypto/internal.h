0654193037967a477bbb5147572fa6ece13d5b72
Baur Krykpayev <bkrykpayev@google.com>
2019-07-04 23:40:19 -0700

    ecdsa hardening: adding a test to the existing sign function to capture the results
    Signed-off-by: mschilder@google.com
    
    BRANCH=None
    
    BUG=None
    
    TEST=something of a test itself
    
    Change-Id: I07437f6a69ba79bdcce8c92976a374733b17d339

--- chip/g/dcrypto/internal.h
+++ chip/g/dcrypto/internal.h
@@ -150,7 +150,7 @@ int hmac_drbg_generate(struct drbg_ctx *ctx,
 		       void *out, size_t out_len,
 		       const void *input, size_t input_len);
 /* Generate p256, with no additional input. */
-void hmac_drbg_generate_p256(struct drbg_ctx *ctx, p256_int *k_out);
+int hmac_drbg_generate_p256(struct drbg_ctx *ctx, p256_int *k_out);
 void drbg_exit(struct drbg_ctx *ctx);
 
 /*
@@ -171,6 +171,12 @@ int dcrypto_p256_ecdsa_verify(const p256_int *key_x, const p256_int *key_y,
 	__attribute__((warn_unused_result));
 int dcrypto_p256_is_valid_point(const p256_int *x, const p256_int *y)
 	__attribute__((warn_unused_result));
+
+/* Pick a p256 number between 1 < k < |p256| */
+int dcrypto_p256_pick(struct drbg_ctx *drbg, p256_int *output);
+
+/* Overwrite with random p256 value */
+void dcrypto_p256_rnd(p256_int *output);
 
 /*
  * Accelerator runtime.
