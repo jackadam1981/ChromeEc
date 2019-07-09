7e9b0b34228a9c1c4016fd5088671c53592a38ef
Craig Hesling <hesling@chromium.org>
2019-07-09 16:40:56 -0700

    stm32: Fix formatting of registers header file
    BRANCH=none
    BUG=none
    TEST=make buildall -j
    TEST=Grab registers-extract.bash
         from http://go/bit/hesling/6385147721023488/4 .
         chmod +x ./registers-extract.bash
         ./registers-extract.bash board-regs-new
         git checkout cros/master
         ./registers-extract.bash board-regs-original
         diff board-regs-original board-regs-new
         [ $? -eq 0 ] && echo "# Good2Go" || echo "# Bad"
    
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Change-Id: I5d5983eb1e0cf7fb46339cba2987d551ff6b16cc

--- chip/stm32/registers-stm32l4.h
+++ chip/stm32/registers-stm32l4.h
@@ -1,8 +1,11 @@
 /* Copyright 2019 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
- *
- * Register map for STM32 processor
+ */
+
+/**
+ * @file  registers-stm32l4.h
+ * @brief Register map for the STM32L4 family of chips
  */
 
 /* --- IRQ numbers --- */
