efd56627e1f5d9a28d8b2470ef7ca79a4d4e10dd
Craig Hesling <hesling@chromium.org>
2019-07-09 16:40:56 -0700

    stm32: Fix formatting of registers header file
    BRANCH=none
    BUG=none
    TEST=Grab registers-extract.bash from go/bit/hesling/6385147721023488/4 .
        	chmod +x ./registers-extract.bash
        	./registers-extract.bash board-regs-new
        	checkout cros/master
        	./registers-extract.bash board-regs-original
        	diff board-regs-original board-regs-new
        	[ $? -eq 0 ] && echo "# Good2Go" || echo "# Bad"
    
    Change-Id: I5d5983eb1e0cf7fb46339cba2987d551ff6b16cc

--- chip/stm32/registers-stm32f3.h
+++ chip/stm32/registers-stm32f3.h
@@ -2,7 +2,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  *
- * Register map for STM32 processor
+ * Register map for STM32F3 family of chips
  */
 
 /* --- IRQ numbers --- */
