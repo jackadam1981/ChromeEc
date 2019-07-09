2838f194c8efe98ece5ca5f31ba61bf8e8ea79fe
Craig Hesling <hesling@chromium.org>
2019-07-09 16:40:56 -0700

    stm32/registers.h: Formatting
    BRANCH=none
    BUG=none
    TEST=
    
    Change-Id: I5d5983eb1e0cf7fb46339cba2987d551ff6b16cc

--- chip/stm32/registers.h
+++ chip/stm32/registers.h
@@ -2,7 +2,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  *
- * Register map for STM32 processor
+ * Register map for STM32 chips
  */
 
 #ifndef __CROS_EC_REGISTERS_H
