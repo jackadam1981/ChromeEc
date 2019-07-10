0e265989325bd4ce124af780b0f4ff9812932a21
Ting Shen <phoenixshen@google.com>
2019-07-10 19:23:09 +0800

    kukui: add baseboard/kukui
    Create a baseboard to reduce the effort of forking and maintaining kukui
    derivatives.
    
    This CL is intended to just moving things around, with minimal changes
    to make compiler happy, complex logic changes will implement in subsequent
    CLs.
    
    BUG=b:137172860
    TEST=build and deploy on a Krane
    BRANCH=master
    
    Change-Id: Ifd3ad5c3b03dce101cd15cbcec304f5e3a1081ac
    Signed-off-by: Ting Shen <phoenixshen@google.com>

---
+++ baseboard/kukui/baseboard.c
@@ -0,0 +1,173 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "adc.h"
+#include "adc_chip.h"
+#include "backlight.h"
+#include "button.h"
+#include "charge_manager.h"
+#include "charge_ramp.h"
+#include "charge_state.h"
+#include "charger.h"
+#include "chipset.h"
+#include "common.h"
+#include "console.h"
+#include "driver/accelgyro_bmi160.h"
+#include "driver/als_tcs3400.h"
+#include "driver/battery/max17055.h"
+#include "driver/bc12/pi3usb9201.h"
+#include "driver/charger/rt946x.h"
+#include "driver/sync.h"
+#include "driver/tcpm/mt6370.h"
+#include "ec_commands.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "host_command.h"
+#include "i2c.h"
+#include "lid_switch.h"
+#include "power.h"
+#include "power_button.h"
+#include "pwm.h"
+#include "pwm_chip.h"
+#include "registers.h"
+#include "spi.h"
+#include "system.h"
+#include "task.h"
+#include "tcpm.h"
+#include "timer.h"
+#include "usb_charge.h"
+#include "usb_mux.h"
+#include "usb_pd_tcpm.h"
+#include "util.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+
+/******************************************************************************/
+const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
+	{
+		.bus_type = EC_BUS_TYPE_I2C,
+		.i2c_info = {
+			.port = I2C_PORT_TCPC0,
+			.addr = MT6370_TCPC_I2C_ADDR,
+		},
+		.drv = &mt6370_tcpm_drv,
+	},
+};
+
+struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_COUNT] = {
+	{
+		.driver = &virtual_usb_mux_driver,
+		.hpd_update = &virtual_hpd_update,
+	},
+};
+
+void board_reset_pd_mcu(void)
+{
+}
+
+uint16_t tcpc_get_alert_status(void)
+{
+	uint16_t status = 0;
+
+	if (!gpio_get_level(GPIO_USB_C0_PD_INT_ODL))
+		status |= PD_STATUS_TCPC_ALERT_0;
+
+	return status;
+}
+
+void board_config_pre_init(void)
+{
+	STM32_RCC_AHBENR |= STM32_RCC_HB_DMA1;
+	/*
+	 * Remap USART1 and SPI2 DMA:
+	 *
+	 * Ch4: USART1_TX / Ch5: USART1_RX (1000)
+	 * Ch6: SPI2_RX / Ch7: SPI2_TX (0011)
+	 */
+	STM32_DMA_CSELR(STM32_DMAC_CH4) = (8 << 12) | (8 << 16) |
+					  (3 << 20) | (3 << 24);
+}
+
+enum kukui_board_version {
+	BOARD_VERSION_UNKNOWN = -1,
+	BOARD_VERSION_REV0 = 0,
+	BOARD_VERSION_REV1 = 1,
+	BOARD_VERSION_REV2 = 2,
+	BOARD_VERSION_REV3 = 3,
+	BOARD_VERSION_REV4 = 4,
+	BOARD_VERSION_REV5 = 5,
+	BOARD_VERSION_REV6 = 6,
+	BOARD_VERSION_REV7 = 7,
+	BOARD_VERSION_REV8 = 8,
+	BOARD_VERSION_REV9 = 9,
+	BOARD_VERSION_REV10 = 10,
+	BOARD_VERSION_REV11 = 11,
+	BOARD_VERSION_REV12 = 12,
+	BOARD_VERSION_REV13 = 13,
+	BOARD_VERSION_REV14 = 14,
+	BOARD_VERSION_REV15 = 15,
+	BOARD_VERSION_COUNT,
+};
+
+struct {
+	enum kukui_board_version version;
+	int expect_mv;
+} const kukui_boards[] = {
+	{ BOARD_VERSION_REV0, 109 },   /* 51.1K , 2.2K(gru 3.3K) ohm */
+	{ BOARD_VERSION_REV1, 211 },   /* 51.1k , 6.8K ohm */
+	{ BOARD_VERSION_REV2, 319 },   /* 51.1K , 11K ohm */
+	{ BOARD_VERSION_REV3, 427 },   /* 56K   , 17.4K ohm */
+	{ BOARD_VERSION_REV4, 542 },   /* 51.1K , 22K ohm */
+	{ BOARD_VERSION_REV5, 666 },   /* 51.1K , 30K ohm */
+	{ BOARD_VERSION_REV6, 781 },   /* 51.1K , 39.2K ohm */
+	{ BOARD_VERSION_REV7, 900 },   /* 56K   , 56K ohm */
+	{ BOARD_VERSION_REV8, 1023 },  /* 47K   , 61.9K ohm */
+	{ BOARD_VERSION_REV9, 1137 },  /* 47K   , 80.6K ohm */
+	{ BOARD_VERSION_REV10, 1240 }, /* 56K   , 124K ohm */
+	{ BOARD_VERSION_REV11, 1343 }, /* 51.1K , 150K ohm */
+	{ BOARD_VERSION_REV12, 1457 }, /* 47K   , 200K ohm */
+	{ BOARD_VERSION_REV13, 1576 }, /* 47K   , 330K ohm */
+	{ BOARD_VERSION_REV14, 1684 }, /* 47K   , 680K ohm */
+	{ BOARD_VERSION_REV15, 1800 }, /* 56K   , NC */
+};
+BUILD_ASSERT(ARRAY_SIZE(kukui_boards) == BOARD_VERSION_COUNT);
+
+#define THRESHOLD_MV 56 /* Simply assume 1800/16/2 */
+
+int board_get_version(void)
+{
+	static int version = BOARD_VERSION_UNKNOWN;
+	int mv;
+	int i;
+
+	if (version != BOARD_VERSION_UNKNOWN)
+		return version;
+
+	gpio_set_level(GPIO_EC_BOARD_ID_EN_L, 0);
+	/* Wait to allow cap charge */
+	msleep(10);
+	mv = adc_read_channel(ADC_BOARD_ID);
+
+	if (mv == ADC_READ_ERROR)
+		mv = adc_read_channel(ADC_BOARD_ID);
+
+	gpio_set_level(GPIO_EC_BOARD_ID_EN_L, 1);
+
+	for (i = 0; i < BOARD_VERSION_COUNT; ++i) {
+		if (mv < kukui_boards[i].expect_mv + THRESHOLD_MV) {
+			version = kukui_boards[i].version;
+			break;
+		}
+	}
+
+	return version;
+}
+
+int board_allow_i2c_passthru(int port)
+{
+	return (port == I2C_PORT_VIRTUAL_BATTERY);
+}
