af7b5ad47477ebc37a955c0c0c32087f729cab04
Ting Shen <phoenixshen@google.com>
2019-07-11 15:29:13 +0800

    WIP: baseboard/kukui: support battery variants
    This CL introduces a config option VARIANT_KUKUI_BATTERY_* for
    derivatives to pick their battery option.
    
    BUG=b:137172860
    TEST=build and deploy on Krane
    BRANCH=master
    
    Change-Id: Ic5c33a233ec30c9bc71414c95603157301281041
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- board/kukui/board.h
+++ board/kukui/board.h
@@ -8,20 +8,21 @@
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
 
+#ifdef BOARD_KRANE
+#define VARIANT_KUKUI_BATTERY_MM8013
+#else
+#define VARIANT_KUKUI_BATTERY_MAX17055
+#endif
+
 #include "baseboard.h"
 
 #define CONFIG_CHARGER_MT6370
 
 #ifdef BOARD_KRANE
-#define CONFIG_BATTERY_MM8013
 #define CONFIG_CHARGER_MT6370_BACKLIGHT
-#else
-#define CONFIG_BATTERY_MAX17055
-#define CONFIG_BATTERY_MAX17055_ALERT
 #endif
 
 /* Battery parameters for max17055 ModelGauge m5 algorithm. */
-#define BATTERY_MAX17055_RSENSE             5     /* m-ohm */
 #ifdef BOARD_KRANE
 #define BATTERY_DESIRED_CHARGING_CURRENT    3500  /* mA */
 #else
