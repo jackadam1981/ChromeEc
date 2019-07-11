f261b915b159b2cb50a5096b5746e9799ce519c1
Ting Shen <phoenixshen@google.com>
2019-07-11 15:29:13 +0800

    baseboard/kukui: support battery variants
    This CL introduces a config option VARIANT_KUKUI_BATTERY_* for
    derivatives to pick their battery option, and added a smart battery
    profile copied from hatch.
    
    BUG=b:137172860
    TEST=build and deploy on Krane
    BRANCH=master
    
    Change-Id: Ic5c33a233ec30c9bc71414c95603157301281041
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- baseboard/kukui/build.mk
+++ baseboard/kukui/build.mk
@@ -10,7 +10,8 @@ baseboard-y=baseboard.o
 baseboard-$(CONFIG_USB_POWER_DELIVERY)+=usb_pd_policy.o
 baseboard-$(CONFIG_BOOTBLOCK)+=emmc.o
 
-# TODO(b:137172860) split battery.c into variant_battery_xxx.c */
-baseboard-y+=battery.o
+baseboard-$(VARIANT_KUKUI_BATTERY_MAX17055)+=battery_max17055.o
+baseboard-$(VARIANT_KUKUI_BATTERY_MM8013)+=battery_mm8013.o
+baseboard-$(VARIANT_KUKUI_BATTERY_SMART)+=battery_smart.o
 
 $(out)/RO/baseboard/$(BASEBOARD)/emmc.o: $(out)/bootblock_data.h
