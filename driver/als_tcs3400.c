756d99fe283d60adfa955e9e0a8703823f0fd06d
Yuval Peress <peress@chromium.org>
2019-07-15 14:10:11 -0600

    config: Refactor CONFIG_ACCEL_FIFO to enable use of IS_ENABLED
    This change allows us to use the IS_ENABLED condition to replace
    the various ifdef guards around the CONFIG_ACCEL_FIFO
    
    BUG=b:137758297,chromium:981990
    BRANCH=None
    TEST=buildall and CTS tests on Arcada
    
    Change-Id: I65d36bac19855e51c830a33e6f3812575e8d15d9
    Signed-off-by: Yuval Peress <peress@chromium.org>

--- driver/als_tcs3400.c
+++ driver/als_tcs3400.c
@@ -19,8 +19,11 @@
 #define CPRINTS(fmt, args...) cprints(CC_ACCEL, "%s "fmt, __func__, ## args)
 
 #ifdef CONFIG_ACCEL_FIFO
-static volatile uint32_t last_interrupt_timestamp;
+static
+#else
+extern
 #endif
+volatile uint32_t last_interrupt_timestamp;
 
 static inline int tcs3400_i2c_read8(const struct motion_sensor_t *s,
 				    int reg, int *data)
@@ -151,10 +154,10 @@ static int tcs3400_post_events(struct motion_sensor_t *s, uint32_t last_ts)
 skip_clear_vector_load:
 #endif
 
-#ifdef CONFIG_ACCEL_FIFO
-		vector.sensor_num = s - motion_sensors;
-		motion_sense_fifo_stage_data(&vector, s, 3, last_ts);
-#endif
+		if (IS_ENABLED(CONFIG_ACCEL_FIFO)) {
+			vector.sensor_num = s - motion_sensors;
+			motion_sense_fifo_stage_data(&vector, s, 3, last_ts);
+		}
 	}
 
 #ifdef CONFIG_ACCEL_SPOOF_MODE
@@ -219,9 +222,9 @@ skip_vector_load:
 
 void tcs3400_interrupt(enum gpio_signal signal)
 {
-#ifdef CONFIG_ACCEL_FIFO
-	last_interrupt_timestamp = __hw_clock_source_read();
-#endif
+	if (IS_ENABLED(CONFIG_ACCEL_FIFO))
+		last_interrupt_timestamp = __hw_clock_source_read();
+
 	task_set_event(TASK_ID_MOTIONSENSE,
 		       CONFIG_ALS_TCS3400_INT_EVENT, 0);
 }
