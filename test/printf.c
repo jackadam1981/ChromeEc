b7c9c40ce44b6aedf7e7a203668d0b5d5d7f6241
Evan Green <evgreen@chromium.org>
2019-07-19 16:20:45 -0700

    printf: Convert %T to %pT
    In order to be more compliant to standards, and ultimately turn on
    compile-time printf format validation, switch the non-standard %T
    into %pT, which takes a pointer to a 64-bit timestamp as an argument.
    For convenience, define PRINTF_TIMESTAMP_NOW, which will use the
    current time as the timestamp value, rather than forcing everyone
    to pass a pointer to get_time().val.
    
    BUG=chromium:984041
    TEST=make -j buildall
    BRANCH=None
    
    Cq-Depend:chrome-internal:1473305
    Change-Id: I83e45b55a95ea27256dc147544ae3f7e39acc5dd
    Signed-off-by: Evan Green <evgreen@chromium.org>

--- test/printf.c
+++ test/printf.c
@@ -196,7 +196,7 @@ test_static int test_vsnprintf_pointers(void)
 {
 	void *ptr = (void *)0x55005E00;
 
-	T(expect_success("55005e00",  "%p",      ptr));
+	T(expect_success("55005e00",  "%pP",     ptr));
 	T(expect_success(err_str,     "%P",      ptr));
 	/* %p by itself is invalid */
 	T(expect(EC_ERROR_INVAL, NO_BYTES_TOUCHED,
