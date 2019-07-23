cac65ab6b9bfee201f0929dd258bad4b618ba01f
Alexandru M Stan <amstan@chromium.org>
2019-07-23 12:59:51 +0800

    TEST-ONLY: cherry-pick of CL:1714436
    Change-Id: Ic4d0e3b47aa1e91576d497d574199ccbb8096a33

--- driver/accelgyro_bmi160.c
+++ driver/accelgyro_bmi160.c
@@ -1275,8 +1275,10 @@ static int init(const struct motion_sensor_t *s)
 
 	ret = raw_read8(s->port, s->i2c_spi_addr_flags,
 			BMI160_CHIP_ID, &tmp);
-	if (ret)
+	CPRINTS("bmi160 chip id ret=%d tmp=%d", ret, tmp);
+	if (ret) {
 		return EC_ERROR_UNKNOWN;
+	}
 
 	if (tmp != BMI160_CHIP_ID_MAJOR && tmp != BMI168_CHIP_ID_MAJOR) {
 		/* The device may be lock on paging mode. Try to unlock it. */
