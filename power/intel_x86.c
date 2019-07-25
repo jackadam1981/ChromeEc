659c82a3863a4ac3a097261d1e1b40c0a613ecfa
Ravi Chandra Sadineni <ravisadineni@chromium.org>
2019-07-24 17:19:22 -0700

    EC: Do not drop SCI events responsible for wake.
    EC currently clears all events(main copy of hostevents) on every resume.
    This seems to be added to clear events that are only part of wake mask
    and not part of SCI mask as they can stick and cause premature wake on
    next suspend.
    
    This patch stops clearing events that are part of SCI mask from the main
    copy as ACPI subsystem will query and clear them on resume anyway. This
    helps kernel to identify the reason for wake if it caused by events
    that are part of SCI mask.
    
    Previously coreboot used to depend on main copy to log wake reason.
    i.e on every resume coreboot used to query and log and clear the wake
    reason by reading all events from the main copy. Since this also comes
    in way of kernel in identifying the wake reason, this change also sets up
    events_copy_b for coreboot by clearing it on every suspend entery.
    
    More details can be found at http://go/hostevent-refactor.
    
    BUG=b:133262012
    BRANCH=None
    TEST=Tested suspend/resume on hach.
    
    Change-Id: I0fac250d4dac49af960b29e8b0e28841af2ef509
    Signed-off-by: Ravi Chandra Sadineni <ravisadineni@chromium.org>

--- power/intel_x86.c
+++ power/intel_x86.c
@@ -352,6 +352,7 @@ enum power_state power_chipset_init(void)
 	return POWER_G3;
 }
 
+
 enum power_state common_intel_x86_power_handle_state(enum power_state state)
 {
 	switch (state) {
@@ -671,9 +672,11 @@ power_chipset_handle_host_sleep_event(enum host_sleep_event state,
 		 */
 		s0ix_notify = S0IX_NOTIFY_RESUME;
 		task_wake(TASK_ID_CHIPSET);
-		/* clear host events */
-		while (lpc_get_next_host_event() != 0)
-			;
+		/*
+		 * Clear events that are not part of SCI mask so as to prevent
+		 * premature wakes on next suspend.
+		 */
+		host_clear_events(~backup_sci_mask);
 		lpc_s0ix_resume_restore_masks();
 		power_signal_disable_interrupt(sleep_sig[SYS_SLEEP_S0IX]);
 		s0ix_complete_resume(ctx);
