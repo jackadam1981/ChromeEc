a51f9496004c537af02df1f99c01b638fa8d278f
Daisuke Nojiri <dnojiri@chromium.org>
2019-07-25 12:22:04 -0700

    Nami: Discharge VBUS whenever Vbus is reset
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b/115464001
    
    Change-Id: I208a6e67bd210237278b3bbad705b9bc6113ff99

--- board/nami/usb_pd_policy.c
+++ board/nami/usb_pd_policy.c
@@ -102,17 +102,11 @@ int pd_set_power_supply_ready(int port)
 
 void pd_power_supply_reset(int port)
 {
-	int prev_en;
-
-	prev_en = vbus_en[port];
-
 	/* Disable VBUS */
 	vbus_en[port] = 0;
 	board_vbus_update_source_current(port);
 
-	/* Enable discharge if we were previously sourcing 5V */
-	if (prev_en)
-		pd_set_vbus_discharge(port, 1);
+	pd_set_vbus_discharge(port, 1);
 
 	/* Give back the current quota we are no longer using */
 	charge_manager_source_port(port, 0);
