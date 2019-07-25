bccdea01a9b3a6f98bbfbd5383df1e0322e0b307
Craig Hesling <hesling@chromium.org>
2019-07-25 14:58:23 -0700

    mock: Add mock structure
    BUG=none
    TEST=none
    
    Change-Id: Ib87b1a93b6d73309afaf7115276ead49218598ff
    Signed-off-by: Craig Hesling <hesling@chromium.org>

---
+++ common/mock/README.mk
@@ -0,0 +1,6 @@
+# Common Mocks
+This directory holds mock implementations for fuzzers and tests.
+
+They can be activated form within the fuzzer/test specific `*.mocklist` file.
+
+The associated header files are in `include/mock` directory.
