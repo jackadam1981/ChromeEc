285e8e76e5ec813cf199ff9d649973a5b195bde4
Ting Shen <phoenixshen@google.com>
2019-07-25 20:10:01 +0800

    jacuzzi: implement extpower
    jacuzzi has an AC_PRESENT pin, remove the custom callback and use
    EXTPOWER_GPIO to handle it.
    
    BUG=b:135895590
    TEST=AC on/off in ec console reported correctly
    BRANCH=master
    
    Change-Id: I48110191a4d36eaa1ec3851d15562643e0e465be
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- board/jacuzzi/board.c
+++ board/jacuzzi/board.c
@@ -197,18 +197,6 @@ int board_discharge_on_ac(int enable)
 	return charger_discharge_on_ac(enable);
 }
 
-int extpower_is_present(void)
-{
-	/*
-	 * The charger will indicate VBUS presence if we're sourcing 5V,
-	 * so exclude such ports.
-	 */
-	if (board_vbus_source_enabled(0))
-		return 0;
-	else
-		return tcpm_get_vbus_level(0);
-}
-
 int pd_snk_is_vbus_provided(int port)
 {
 	/* read IT8801 GPIO EN_USBC_CHARGE_L? */
