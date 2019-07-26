0133339cb76334a6e819371c2011c27ab5e34cfd
Craig Hesling <hesling@chromium.org>
2019-07-25 18:19:51 -0700

    fuzz_host_cmd: Add fuzzing of device input
    THIS IS BROKEN!
    
    BRANCH=none
    BUG=b:116065496
    TEST=none
    
    Change-Id: I3e02609d55ffb68b3aeb14ba61881d16197d6658

--- common/mock/mkbp_events_mock.c
+++ common/mock/mkbp_events_mock.c
@@ -10,12 +10,21 @@
 
 #include "mock/mkbp_events_mock.h"
 
+#include <stddef.h>
 #include <stdint.h>
 
 #include "common.h"
+#include "util.h"
 
 struct mock_ctrl_mkbp_events mock_ctrl_mkbp_events = \
 	MOCK_CTRL_DEFAULT_MKBP_EVENTS;
+
+
+size_t mock_ctrl_fill_mkbp_events(const uint8_t *data, size_t size) {
+	size_t copy_size = MIN(sizeof(mock_ctrl_mkbp_events), size);
+	memcpy(&mock_ctrl_mkbp_events, data, copy_size);
+	return copy_size;
+}
 
 test_mockable int mkbp_send_event(uint8_t event_type)
 {
