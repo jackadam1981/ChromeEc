9f666214aff75b1c159286ecfb855b60dc3209a4
Craig Hesling <hesling@chromium.org>
2019-07-26 09:32:11 -0700

    fuzz_host_cmd: Add fpsensor task + fp host cmds
    This CL enables the fpsensor task and adds the following
    remaining fingerprint host commands to the fuzzer:
    
    * EC_CMD_FP_PASSTHRU
    * EC_CMD_FP_INFO
    * EC_CMD_FP_FRAME
    * EC_CMD_FP_STATS
    * EC_CMD_FP_TEMPLATE
    
    BRANCH=none
    BUG=b:116065496
    TEST=make buildall -j
    TEST=make run-host_command_fuzz
    TEST=# Pull in TEST_COVERAGE fix
         git fetch "https://chromium.googlesource.com/chromiumos/platform/ec" \
         refs/changes/86/1725186/1 && git cherry-pick FETCH_HEAD
         make host-host_command_fuzz TEST_COVERAGE=1
         timeout 5m ./build/host/host_command_fuzz/host_command_fuzz.exe
         llvm-profdata merge -sparse default.profraw -o default.profdata
         llvm-cov show build/host/host_command_fuzz/host_command_fuzz.exe \
         --instr-profile=default.profdata --format=html --output-dir=cov
         # Inspect cov/.../common/fpsensor/fpsensor_state.c.html to verify
    
    Change-Id: Icad9493ba41cd4daa61a30246d01afd1dbe16c56
    Signed-off-by: Craig Hesling <hesling@chromium.org>

--- include/fpsensor_state.h
+++ include/fpsensor_state.h
@@ -23,6 +23,7 @@
 
 #if defined(HAVE_PRIVATE) && !defined(TEST_BUILD)
 #define HAVE_FP_PRIVATE_DRIVER
+/* Include the device specific header file */
 #define PRIV_HEADER(header) STRINGIFY(header)
 #include PRIV_HEADER(FP_SENSOR_PRIVATE)
 #else
@@ -34,6 +35,12 @@
 #define FP_MAX_FINGER_COUNT 5
 #endif
 #define FP_POSITIVE_MATCH_SECRET_BYTES 32
+
+#ifdef TEST_BUILD
+/* This represents the mock of the private */
+#define HAVE_FP_PRIVATE_DRIVER
+#endif
+
 #define SBP_ENC_KEY_LEN 16
 #define FP_ALGORITHM_ENCRYPTED_TEMPLATE_SIZE \
 	(FP_ALGORITHM_TEMPLATE_SIZE + \
