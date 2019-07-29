d40ad874f288f13c003fd91827e156dab9eb2648
Mario Tesi <mario.tesi@st.com>
2019-07-29 09:15:14 +0200

    driver: lps22hh: Add driver support
    	Added Barometer LPS22HH driver support.
    	Basic feature, no FIFO support, only data read
    	in polling mode. Output data are in Pascal.
    
    	BUG=none
    	BRANCH=master
    	TEST=Tested on discovery_stmems BOARD with LPS22HH
    	connected to EC i2c master bus.
    	All supported ODR tested using motion sense console commands
    	Enabling accelinfo to poll sensor, data output seems correct
    	(output in Pascal).
    
    Change-Id: I555548b56a3d453c405a2fbd48658948e36db76d
    Signed-off-by: Mario Tesi <mario.tesi@st.com>

--- include/config.h
+++ include/config.h
@@ -127,6 +127,7 @@
 /* Specify barometer attached */
 #undef CONFIG_BARO_BMP280
 #undef CONFIG_BARO_LPS22HB
+#undef CONFIG_BARO_LPS22HH
 
 /* When set, it indicates a secondary sensor is attached behind a BMI160. */
 #undef CONFIG_BMI160_SEC_I2C
