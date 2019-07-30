b37c92772c9501cb46a1578c250576a54b766c2a
Craig Hesling <hesling@chromium.org>
2019-07-29 17:27:02 -0700

    fuzz_host_cmd: Add fuzzing of device input
    THIS IS BROKEN!
    
    BRANCH=none
    BUG=b:116065496
    TEST=none
    
    Change-Id: I3e02609d55ffb68b3aeb14ba61881d16197d6658

--- common/mock/rollback_mock.c
+++ common/mock/rollback_mock.c
@@ -5,10 +5,12 @@
 
 #include "mock/rollback_mock.h"
 
+#include <stddef.h>
 #include <stdint.h>
 #include <string.h>
 
 #include "common.h"
+#include "util.h"
 
 struct mock_ctrl_rollback mock_ctrl_rollback = MOCK_CTRL_DEFAULT_ROLLBACK;
 
@@ -20,6 +22,14 @@ static const uint8_t fake_rollback_secret[] = {
 };
 
 BUILD_ASSERT(sizeof(fake_rollback_secret) == CONFIG_ROLLBACK_SECRET_SIZE);
+
+size_t mock_ctrl_fill_rollback(const uint8_t *data, size_t size)
+{
+	size_t copy_size = MIN(sizeof(mock_ctrl_rollback), size);
+
+	memcpy(&mock_ctrl_rollback, data, copy_size);
+	return copy_size;
+}
 
 /* Mock the rollback for unit or fuzz tests. */
 int rollback_get_secret(uint8_t *secret)
