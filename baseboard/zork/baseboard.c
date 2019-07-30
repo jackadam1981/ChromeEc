af85128dc8fc96a1368d45a6dcaf7158f52c41c3
Denis Brockus <dbrockus@chromium.org>
2019-07-30 09:30:55 -0600

    [WIP] Trembyle: Add USB-C sink BC12 driver
    Hooked in the PI3USB9201 BC12 driver
    
    BUG=b:138600672
    BRANCH=none
    TEST=make BOARD=trembyle
    
    Change-Id: I849d182063c62539add88b8a2aa73a149dc25c92
    Signed-off-by: Denis Brockus <dbrockus@chromium.org>

--- baseboard/zork/baseboard.c
+++ baseboard/zork/baseboard.c
@@ -74,6 +74,7 @@ BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
 
 /* I2C port map. */
 const struct i2c_port_t i2c_ports[] = {
+	{"ppc0",     I2C_PORT_PPC0,     100, GPIO_I2C0_SCL, GPIO_I2C0_SDA},
 	{"tcpc0",    I2C_PORT_TCPC0,    400, GPIO_I2C0_SCL, GPIO_I2C0_SDA},
 	{"tcpc1",    I2C_PORT_TCPC1,    400, GPIO_I2C1_SCL, GPIO_I2C1_SDA},
 	{"power",    I2C_PORT_BATTERY,  100, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
--- board/trembyle/board.c
+++ board/trembyle/board.c
@@ -7,12 +7,14 @@
 
 #include "button.h"
 #include "driver/accelgyro_bmi160.h"
+#include "driver/bc12/pi3usb9201.h"
 #include "extpower.h"
 #include "gpio.h"
 #include "lid_switch.h"
 #include "power.h"
 #include "power_button.h"
 #include "switch.h"
+#include "task.h"
 #include "usb_charge.h"
 
 static void ppc_interrupt(enum gpio_signal signal)
@@ -20,7 +22,37 @@ static void ppc_interrupt(enum gpio_signal signal)
 	/* TODO */
 }
 
+static void bc12_interrupt(enum gpio_signal signal)
+{
+	switch (signal) {
+	case GPIO_USB_C0_BC12_INT_ODL:
+		task_set_event(TASK_ID_USB_CHG_P0, USB_CHG_EVENT_BC12, 0);
+		break;
+
+	case GPIO_USB_C1_BC12_INT_ODL:
+		task_set_event(TASK_ID_USB_CHG_P1, USB_CHG_EVENT_BC12, 0);
+		break;
+
+	default:
+		break;
+	}
+}
+
 #include "gpio_list.h"
+
+/******************************************************************************/
+/* USB-C TPCP Configuration */
+const struct pi3usb2901_config_t pi3usb2901_bc12_chips[] = {
+	[USB_PD_PORT_TCPC_0] = {
+		.i2c_port = I2C_PORT_PPC0,
+		.i2c_addr_flags = PI3USB9201_I2C_ADDR_3_FLAGS,
+	},
+
+	[USB_PD_PORT_TCPC_1] = {
+		.i2c_port = I2C_PORT_TCPC1,
+		.i2c_addr_flags = PI3USB9201_I2C_ADDR_3_FLAGS,
+	},
+};
 
 void board_update_sensor_config_from_sku(void)
 {
@@ -45,29 +77,7 @@ uint32_t system_get_sku_id(void)
 	return 0;
 }
 
-void usb_charger_set_switches(int port, enum usb_switch setting)
-{
-	/* TODO */
-}
-
 uint16_t tcpc_get_alert_status(void)
-{
-	/* TODO */
-	return 0;
-}
-
-void usb_charger_task(void *u)
-{
-	/* TODO */
-}
-
-int usb_charger_ramp_allowed(int supplier)
-{
-	/* TODO */
-	return 0;
-}
-
-int usb_charger_ramp_max(int supplier, int sup_curr)
 {
 	/* TODO */
 	return 0;
