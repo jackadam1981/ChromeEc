2bdd60fbcb7ae3c06174385cdbe0a356275189eb
Nick Vaccaro <nvaccaro@google.com>
2019-07-30 14:53:47 -0700

    kohaku/tcs3400: add ALS cover window coefficients
    - adds cover window coeefficients for use in xyz translation
    - enables RGB to XYZ translation
    
    These values are from the "Kohaku Color Sensor Calibration"
    document.
    
    BUG=b:138679613
    BRANCH=master
    TEST=Flash and boot kohaku, verify that ALS and RGB sensors
    are still generating data.  I used alslog patch and enabled
    saturation logging in EC console via "alslog 8".
    
    Change-Id: If8abde715f9afdffdc73988227748dbf57775a56
    Signed-off-by: Nick Vaccaro <nvaccaro@google.com>

--- board/kohaku/board.h
+++ board/kohaku/board.h
@@ -54,6 +54,8 @@
 #define CONFIG_ALS_TCS3400
 #define CONFIG_ALS_TCS3400_INT_EVENT \
 	TASK_EVENT_MOTION_SENSOR_INTERRUPT(CLEAR_ALS)
+#define CONFIG_RGB_XYZ_TRANSLATE
+
 
 /* Sensors without hardware FIFO are in forced mode */
 #define CONFIG_ACCEL_FORCE_MODE_MASK \
