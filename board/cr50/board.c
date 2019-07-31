68ae90f2ab3f9624c3141278fc80880d0e99647b
Vadim Bendebury <vbendeb@chromium.org>
2019-07-31 13:25:36 -0700

    g: allow I2CS operate without hardware resets
    It is not always possible to rely on PMU for resetting the I2CS
    controller. Most of the AP firmware versions deploy the 'I2C unwedge'
    cycle when coming out of reset, but not all of them, this is why Cr50
    needs to be able to recover on its own in case there was a crash and
    the I2C bus was left mid transaction with the H1 holding down the SDA
    line.
    
    The solution requires the following steps:
    
       - reset FIFO pointers so that there is no more pending bytes to
         transmit, if any.
    
       - set the first couple of bytes of the output FIFO (I2CS
         controller's READ buffer) to 0xff so that the controller does not
         pull SDA low when going through unwedge sequence.
    
       - change the pin multiplexer settings such that SDA and SCL lines
         of the I2CS controller are connected to two dedicated GPIOs
         instead of the actual bus. Use internal 'virtual' pads for this
         instead of actual GPIO pins.
    
       - issue several clocks to make sure that the controller finishes
         pushing out the byte currently in the TX buffer
    
       - using the GPIOs clreate the bus I2C Stop condition to prepare the
         controller to the next transaction
    
       - restore pinmux settings so that the controller is connected back
         to the external bus
    
    Value read through the SDA field of the I2CS_READVAL register is used
    to determine if the bus could potentially be in the wedges state.
    
    BRANCH=cr50, cr50-mp
    BUG=b:135772657
    
    TEST=the test was ran on a Pyro device, which uses I2C for
         communication with H1 and which AP firmware does not deploy the
         'I2C unwedge' cycle.
    
         Test instrumentation involved setting a Chrome OS startup file
         such that once booted, the AP starts continuously polling TPM for
         value of an NVMEM index, creating I2C traffic. The host
         workstation sends the 'apreset cold' command to the EC within a
         few seconds of Chrome OS coming up.
    
         First run a special Cr50 image which is not resetting I2CS using
         PMU on TPM restarts, is was not trying to unwedge the stuck I2C
         bus. On five experiments, it takes on average 32 reboots for
         until I2C bus is locked up and the DUT falls into recovery.
    
         Then loaded the Cr50 image with this patch and ran the test
         again, it survived for 150 cycles without a problem.
    
    Change-Id: Iffec33f97557e3acfd1cd5fb76ba158f8c23b608
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -1690,3 +1690,46 @@ void board_start_ite_sync(void)
 	/* Let the usb reply to make it to the host. */
 	hook_call_deferred(&deferred_ite_sync_reset_data, 10 * MSEC);
 }
+
+void board_unwedge_i2cs(void)
+{
+	int i;
+
+	/*
+	 * Create connection between i2cs and the 'unwedge' GPIOs and generate
+	 * the 'i2c unwedge' sequence.
+	 *
+	 * First, disconnect the external pins from the i2cs controller.
+	 */
+	GWRITE(PINMUX, DIOA1_SEL, 0);
+	GWRITE(PINMUX, DIOA9_SEL, 0);
+
+	/* Connect the 'unwedge' GPIOs to the i2cs controller. */
+	GWRITE(PINMUX, GPIO1_GPIO5_SEL, GC_PINMUX_I2CS0_SCL_SEL);
+	GWRITE(PINMUX, GPIO1_GPIO6_SEL, GC_PINMUX_I2CS0_SDA_SEL);
+
+	gpio_set_level(GPIO_UNWEDGE_I2CS_SCL, 1);
+
+	/* 10 pulses should be more than enough in any case. */
+	for (i = 0; i < 20; i++) {
+		usleep(2);
+		gpio_set_level(GPIO_UNWEDGE_I2CS_SCL, i & 1);
+	}
+
+	/* Now create the 'stop' condition. */
+	gpio_set_level(GPIO_UNWEDGE_I2CS_SCL, 0);
+	gpio_set_level(GPIO_UNWEDGE_I2CS_SDA, 0);
+	usleep(2);
+	gpio_set_level(GPIO_UNWEDGE_I2CS_SCL, 1);
+	usleep(2);
+	gpio_set_level(GPIO_UNWEDGE_I2CS_SDA, 1);
+	usleep(2);
+
+	/* Disconnect the 'unwedge' GPIOs. */
+	GWRITE(PINMUX, GPIO1_GPIO5_SEL, 0);
+	GWRITE(PINMUX, GPIO1_GPIO6_SEL, 0);
+
+	/* Restore external pins' connection to the i2cs controller. */
+	GWRITE(PINMUX, DIOA1_SEL, GC_PINMUX_I2CS0_SDA_SEL);
+	GWRITE(PINMUX, DIOA9_SEL, GC_PINMUX_I2CS0_SCL_SEL);
+}
