6958f959258206d99e92959e5644d0685c5d6f97
Jack Rosenthal <jrosenth@chromium.org>
2019-08-01 12:49:16 -0600

    common: add STATIC_REQUIRES macro
    A common pattern to use with IS_ENABLED is like this:
    
    /*
     * This var should only be used if CONFIG_FOO. The linker errors if
     * CONFIG_FOO is not defined is intentional.
     */
     #ifdef CONFIG_FOO
     static
     #else
     extern
     #endif
     int some_var;
    
    The issue with this is that it leads to an over-verbose and
    potentially hard to read pattern, and does not have the check that
    CONFIG_FOO was only defined to blank.
    
    Suppose a macro like this existed:
    
    STATIC_REQUIRES(CONFIG_FOO) int some_var;
    
    ... which expands to "static" when CONFIG_FOO is defined to empty,
    "extern" when CONFIG_FOO is not defined, and errors when CONFIG_FOO is
    defined to non-empty.
    
    This CL implements that.
    
    BUG=chromium:989786
    BRANCH=none
    TEST=provided unit tests, buildall, and make sure that error happens
    correctly when CONFIG_FOO is defined to something non-empty
    
    Change-Id: Ib57aaba62bc184fda9aa782a780d5f13ba44ae88
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>

--- test/is_enabled.c
+++ test/is_enabled.c
@@ -1,25 +1,43 @@
 /* Copyright 2019 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
- *
- * Test the IS_ENABLED macro.
  */
+
+/* Test the IS_ENABLED and STATIC_REQUIRES macros */
 #include "common.h"
 #include "test_util.h"
 
 #undef	CONFIG_UNDEFINED
 #define	CONFIG_BLANK
 
-static int test_undef(void)
+static int test_is_enabled_undef(void)
 {
 	TEST_ASSERT(IS_ENABLED(CONFIG_UNDEFINED) == 0);
 
 	return EC_SUCCESS;
 }
 
-static int test_blank(void)
+static int test_is_enabled_blank(void)
 {
 	TEST_ASSERT(IS_ENABLED(CONFIG_BLANK) == 1);
+
+	return EC_SUCCESS;
+}
+
+STATIC_REQUIRES(CONFIG_UNDEFINED) int this_var_is_extern;
+STATIC_REQUIRES(CONFIG_BLANK) int this_var_is_static;
+
+static int test_static_requires_blank(void)
+{
+	TEST_ASSERT(this_var_is_static == 0);
+
+	return EC_SUCCESS;
+}
+
+static int test_static_requires_unused_no_fail(void)
+{
+	if (IS_ENABLED(CONFIG_UNDEFINED))
+		this_var_is_extern = 1;
 
 	return EC_SUCCESS;
 }
@@ -28,8 +46,10 @@ void run_test(void)
 {
 	test_reset();
 
-	RUN_TEST(test_undef);
-	RUN_TEST(test_blank);
+	RUN_TEST(test_is_enabled_undef);
+	RUN_TEST(test_is_enabled_blank);
+	RUN_TEST(test_static_requires_blank);
+	RUN_TEST(test_static_requires_unused_no_fail);
 
 	test_print_result();
 }
