b9375ba5dc667faf3a5b638c99991c17f1cf4447
Evan Green <evgreen@chromium.org>
2019-08-01 11:20:14 -0700

    printf: Convert %h to %ph
    In order to make printf more standard, use %ph, and continue to
    pass a pointer to the hex buffer you want to print. This gets us
    closer to being able to turn on compile-time printf format checking.
    
    BUG=chromium:984041
    TEST=make -j buildall
    BRANCH=None
    
    Cq-Depend:chrome-internal:1560880,chrome-internal:1559798,chrome-internal:1560598
    Change-Id: I9c0ca124a048314c9b62d64bd55b36be55034e0e
    Signed-off-by: Evan Green <evgreen@chromium.org>

--- chip/g/dcrypto/dcrypto_bn.c
+++ chip/g/dcrypto/dcrypto_bn.c
@@ -1484,7 +1484,7 @@ static int command_genp(int argc, char **argv)
 	result = call_on_bigger_stack(genp_core);
 
 	if (result == EC_SUCCESS) {
-		ccprintf("prime: %.*h (lsb first)\n", sizeof(prime_buf),
+		ccprintf("prime: %.*ph (lsb first)\n", sizeof(prime_buf),
 			 prime_buf);
 		ccprintf("μs   : %lu\n", genp_end.val - genp_start.val);
 	}
