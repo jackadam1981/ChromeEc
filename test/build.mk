38be20319a3dcd402b2a74f12dd25f0b58c450af
Jack Rosenthal <jrosenth@chromium.org>
2019-08-01 12:49:16 -0600

    common: add STATIC_IF and STATIC_IF_NOT macros
    A common pattern to use with IS_ENABLED is like this:
    
    /*
     * This var should only be used if CONFIG_FOO. The linker errors if
     * CONFIG_FOO is not defined is intentional.
     */
     #ifdef CONFIG_FOO
     static
     #else
     extern
     #endif
     int some_var;
    
    The issue with this is that it leads to an over-verbose and
    potentially hard to read pattern, and does not have the check that
    CONFIG_FOO was only defined to blank.
    
    Suppose a macro like this existed:
    
    STATIC_IF(CONFIG_FOO) int some_var;
    
    ... which expands to "static" when CONFIG_FOO is defined to empty,
    "extern" when CONFIG_FOO is not defined, and errors when CONFIG_FOO is
    defined to non-empty.
    
    This CL implements that, as well as the inverse (STATIC_IF_NOT).
    
    BUG=chromium:989786
    BRANCH=none
    TEST=provided unit tests, buildall
    
    Change-Id: Ib57aaba62bc184fda9aa782a780d5f13ba44ae88
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>

--- test/build.mk
+++ test/build.mk
@@ -61,6 +61,8 @@ test-list-host += sbs_charging_v2
 test-list-host += sha256
 test-list-host += sha256_unrolled
 test-list-host += shmalloc
+test-list-host += static_if
+test-list-host += static_if_error
 test-list-host += system
 test-list-host += thermal
 test-list-host += timer_dos
@@ -131,6 +133,7 @@ sbs_charging_v2-y=sbs_charging_v2.o
 sha256-y=sha256.o
 sha256_unrolled-y=sha256.o
 shmalloc-y=shmalloc.o
+static_if-y=static_if.o
 stress-y=stress.o
 system-y=system.o
 thermal-y=thermal.o
@@ -160,3 +163,6 @@ $(out)/RO/test/nvmem_tpm2_mock.o: CFLAGS += -I$(TPM2_ROOT)
 
 host-is_enabled_error: TEST_SCRIPT=is_enabled_error.sh
 is_enabled_error-y=is_enabled_error.o.cmd
+
+host-static_if_error: TEST_SCRIPT=static_if_error.sh
+static_if_error-y=static_if_error.o.cmd
