c1ba73d415ac867c5a9240307ead5e982c7b5063
Shannon Chen <shannc@google.com>
2019-08-02 17:48:58 +0800

    log: Preserve Kukui EC reset logs across every EC reboot on SRAM.
    In kukui, AP firmware bootblock is put into EC RO image due to security
    reasons. To prevent a compromised EC RW image, we have to reset EC and boot
    from EC RO whenever kukui transits from S5 to S0. EC will lose console logs
    and reset reasons after resetting EC. In order to save the console logs and
    reset reasons, we put them on SRAM.
    
    BUG=b:133795403
    TEST=1. on kukui, shutdown AP, reboot AP, and see
    	the previous logs before reboot will be kept
            on /var/log/croc_ec.log
         2. reset reasons can be viewed with ectool uptimeinfo
    BRANCH=master
    
    Change-Id: I19db49101fda1675dc2fdc047b7f14af77cdb6e6
    Signed-off-by: Shannon Chen <shannc@google.com>

--- include/config.h
+++ include/config.h
@@ -3512,6 +3512,13 @@
  */
 #undef CONFIG_UART_PAD_SWITCH
 
+/**
+ * Preserve EC reset logs and console logs on SRAM so that the logs will be
+ * preserved after EC shutting down. It will keep the contents across EC resets,
+ * so that we have more information about system states.
+ */
+#undef CONFIG_PRESERVE_LOGS
+
 /*
  * UART receive buffer size in bytes.  Must be a power of 2 for macros in
  * common/uart_buffering.c to work properly.  Must be larger than
