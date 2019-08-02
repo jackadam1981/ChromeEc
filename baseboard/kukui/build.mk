498c3c9c7997ddd088463f793e3b6b8578f35b26
xiong.huang <xiong.huang@bitland.corp-partner.google.com>
2019-08-02 17:14:52 +0800

    kodama: Support smart battery
    Provide SMP and Celxpert smart battery initial files.
    
    BUG=b:136977971, b:138826367
    BRANCH=master
    Q-DEPEND=CL:1730377
    TEST=boot ec, verify that ec console prints 'found batt:SMP' or
         'found batt:Celxpert'.
    
    Change-Id: I607fc41c51e8b27acddefda4dcd2ea02a7597a0f
    Signed-off-by: Xiong Huang <xiong.huang@bitland.corp-partner.google.com>

--- baseboard/kukui/build.mk
+++ baseboard/kukui/build.mk
@@ -14,6 +14,8 @@ baseboard-$(VARIANT_KUKUI_BATTERY_MAX17055)+=battery_max17055.o
 baseboard-$(VARIANT_KUKUI_BATTERY_MM8013)+=battery_mm8013.o
 baseboard-$(VARIANT_KUKUI_BATTERY_SMART)+=battery_smart.o
 
+ifeq ($(VARIANT_KUKUI_BATTERY_SMART),)
 baseboard-$(VARIANT_KUKUI_CHARGER_MT6370)+=charger_mt6370.o
+endif
 
 $(out)/RO/baseboard/$(BASEBOARD)/emmc.o: $(out)/bootblock_data.h
