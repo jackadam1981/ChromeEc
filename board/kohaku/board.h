c8924048f841dd17bd052de1b8bcee0c4257c178
Paul Fagerburg <pfagerburg@chromium.org>
2019-08-06 14:43:55 -0600

    kohaku: add TEMP_SENSOR_3 and TEMP_SENSOR_4 to DPTF
    The Kohaku V24 schematic adds two additional temperature sensors
    to the EC. Add these to the DPTF tables.
    Note that this requires changes in both the EC and BIOS.
    Update the thermistor equations.
    
    BRANCH=none
    BUG=b:138578073, b:124316213
    TEST=Rebuild EC and BIOS, look for new thermal sensors in kernel.
    1. Build EC
    ``cd ~/trunk/src/platform/ec``
    ``make -j BOARD=kohaku``
    2. Program EC
    ``./util/flash_ec --board=kohaku``
    3. Reboot device
    4. Rebuild BIOS
    ``cd ~/trunk/src/third_party/coreboot``
    ``FEATURES="noclean" FW_NAME=kohaku emerge-hatch chromeos-ec depthcharge
    vboot_reference libpayload coreboot-private-files intel-cmlfsp
    coreboot-private-files-hatch coreboot chromeos-bootimage``
    5. Use flashrom to program the BIOS
    6. Reboot device
    7. Log into the root console (ctrl-alt-F2 or servo)
    8. Example thermal sensor information
    ``grep . /sys/class/thermal/t*/type``
    Look for "TSR0" through "TSR3" in the output.
    
    Change-Id: Ifb0cc83418e684cf6b2a66eb046267f07bf3e7f8
    Signed-off-by: Paul Fagerburg <pfagerburg@chromium.org>

--- board/kohaku/board.h
+++ board/kohaku/board.h
@@ -111,7 +111,7 @@
 #define CONFIG_TEMP_SENSOR_POWER_GPIO GPIO_EN_A_RAILS
 #define CONFIG_THERMISTOR
 #define CONFIG_THROTTLE_AP
-#define CONFIG_STEINHART_HART_3V3_51K1_47K_4050B
+#define CONFIG_STEINHART_HART_3V3_30K9_47K_4050B
 
 /*
  * Macros for GPIO signals used in common code that don't match the
@@ -139,6 +139,8 @@ extern enum gpio_signal gpio_en_pp5000_a;
 enum adc_channel {
 	ADC_TEMP_SENSOR_1,	/* ADC0 */
 	ADC_TEMP_SENSOR_2,	/* ADC1 */
+	ADC_TEMP_SENSOR_3,	/* ADC2 */
+	ADC_TEMP_SENSOR_4,	/* ADC3 */
 	ADC_CH_COUNT
 };
 
@@ -161,6 +163,8 @@ enum pwm_channel {
 enum temp_sensor_id {
 	TEMP_SENSOR_1,
 	TEMP_SENSOR_2,
+	TEMP_SENSOR_3,
+	TEMP_SENSOR_4,
 	TEMP_SENSOR_COUNT
 };
 
