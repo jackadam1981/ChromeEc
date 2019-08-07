692567f4bfb461d0d2c9ca67a465c4cd94f5ea55
Ting Shen <phoenixshen@google.com>
2019-08-07 14:55:16 +0800

    TEST-ONLY: jacuzzi: disable adc after read
    Change-Id: I5ca69adf99bba91f86527e377a45e42b021ee310
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- baseboard/kukui/baseboard.c
+++ baseboard/kukui/baseboard.c
@@ -4,6 +4,7 @@
  */
 
 #include "adc.h"
+#include "chip/stm32/adc_chip.h"
 #include "gpio.h"
 #include "timer.h"
 
@@ -91,6 +92,7 @@ int board_get_version(void)
 		mv = adc_read_channel(ADC_BOARD_ID);
 
 	gpio_set_level(GPIO_EC_BOARD_ID_EN_L, 1);
+	adc_disable();
 
 	for (i = 0; i < BOARD_VERSION_COUNT; ++i) {
 		if (mv < kukui_boards[i].expect_mv + THRESHOLD_MV) {
