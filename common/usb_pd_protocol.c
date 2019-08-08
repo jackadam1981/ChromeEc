e8f48936443fc503cdf2ef6b836af0387c4d3e61
Jett Rink <jettrink@chromium.org>
2019-08-08 10:06:40 -0600

    usb: de-dup common code from old and new PD stack
    We still need to pull out more common code between the two stacks, but
    this is scaffolding with a few examples.
    
    BRANCH=firmware-octopus-11297.B
    BUG=b:137493121
    TEST=With other PD Policies patches, flash octopus and run faft_ec&pd
    
    Change-Id: Ibd9dda1e544e06f02aa3dde48ca7de1539700cfa
    Signed-off-by: Jett Rink <jettrink@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1744655
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -22,6 +22,7 @@
 #include "timer.h"
 #include "util.h"
 #include "usb_charge.h"
+#include "usb_common.h"
 #include "usb_mux.h"
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
@@ -2201,23 +2202,12 @@ enum pd_dual_role_states pd_get_dual_role(int port)
 	return drp_state[port];
 }
 
-static inline __maybe_unused int get_battery_soc(void)
-{
-#if defined(CONFIG_CHARGER)
-	return charge_get_percent();
-#elif defined(CONFIG_BATTERY)
-	return board_get_battery_soc();
-#else
-	return 0;
-#endif
-}
-
 #ifdef CONFIG_USB_PD_TRY_SRC
 static void pd_update_try_source(void)
 {
 	int i;
 	int try_src = 0;
-	int batt_soc = get_battery_soc();
+	int batt_soc = usb_get_battery_soc();
 
 	try_src = 0;
 	for (i = 0; i < board_get_usb_pd_port_count(); i++)
@@ -2254,7 +2244,7 @@ static void pd_update_try_source(void)
 static void pd_update_snk_reset(void)
 {
 	int i;
-	int batt_soc = get_battery_soc();
+	int batt_soc = usb_get_battery_soc();
 
 	if (batt_soc < CONFIG_USB_PD_RESET_MIN_BATT_SOC ||
 	    battery_get_disconnect_state() != BATTERY_NOT_DISCONNECTED)
@@ -2497,54 +2487,7 @@ void pd_ping_enable(int port, int enable)
 		pd[port].flags &= ~PD_FLAGS_PING_ENABLED;
 }
 
-/*
- * CC values for regular sources and Debug sources (aka DTS)
- *
- * Source type  Mode of Operation   CC1    CC2
- * ---------------------------------------------
- * Regular      Default USB Power   RpUSB  Open
- * Regular      USB-C @ 1.5 A       Rp1A5  Open
- * Regular      USB-C @ 3 A         Rp3A0  Open
- * DTS          Default USB Power   Rp3A0  Rp1A5
- * DTS          USB-C @ 1.5 A       Rp1A5  RpUSB
- * DTS          USB-C @ 3 A         Rp3A0  RpUSB
-*/
-
-/**
- * Returns the polarity of a Sink.
- */
-static inline int get_snk_polarity(int cc1, int cc2)
-{
-	/* the following assumes:
-	 * TYPEC_CC_VOLT_RP_3_0 > TYPEC_CC_VOLT_RP_1_5
-	 * TYPEC_CC_VOLT_RP_1_5 > TYPEC_CC_VOLT_RP_DEF
-	 * TYPEC_CC_VOLT_RP_DEF > TYPEC_CC_VOLT_OPEN
-	 */
-	return (cc2 > cc1);
-}
-
 #if defined(CONFIG_CHARGE_MANAGER)
-/**
- * Returns type C current limit (mA) based upon cc_voltage (mV).
- */
-static typec_current_t get_typec_current_limit(int polarity, int cc1, int cc2)
-{
-	typec_current_t charge;
-	int cc = polarity ? cc2 : cc1;
-	int cc_alt = polarity ? cc1 : cc2;
-
-	if (cc == TYPEC_CC_VOLT_RP_3_0 && cc_alt != TYPEC_CC_VOLT_RP_1_5)
-		charge = 3000;
-	else if (cc == TYPEC_CC_VOLT_RP_1_5)
-		charge = 1500;
-	else
-		charge = 0;
-
-	if (cc_is_rp(cc_alt))
-		charge |= TYPEC_CURRENT_DTS_MASK;
-
-	return charge;
-}
 
 /**
  * Signal power request to indicate a charger update that affects the port.
@@ -3766,8 +3709,8 @@ void pd_task(void *u)
 			/* initial data role for sink is UFP */
 			pd_set_data_role(port, PD_ROLE_UFP);
 #if defined(CONFIG_CHARGE_MANAGER)
-			typec_curr = get_typec_current_limit(pd[port].polarity,
-							     cc1, cc2);
+			typec_curr = usb_get_typec_current_limit(
+				pd[port].polarity, cc1, cc2);
 			typec_set_input_current_limit(
 				port, typec_curr, TYPE_C_VOLTAGE);
 #endif
@@ -3854,7 +3797,7 @@ void pd_task(void *u)
 				 * high-power chargers will stay at 15W until a
 				 * reset is sent, depending on boot timing.
 				 */
-				int batt_soc = get_battery_soc();
+				int batt_soc = usb_get_battery_soc();
 
 				if (batt_soc < CONFIG_USB_PD_RESET_MIN_BATT_SOC ||
 				    battery_get_disconnect_state() != BATTERY_NOT_DISCONNECTED)
@@ -3937,13 +3880,15 @@ void pd_task(void *u)
 
 			/* Check if CC pull-up has changed */
 			tcpm_get_cc(port, &cc1, &cc2);
-			if (typec_curr != get_typec_current_limit(
+			if (typec_curr != usb_get_typec_current_limit(
 						pd[port].polarity, cc1, cc2)) {
 				/* debounce signal by requiring two reads */
 				if (typec_curr_change) {
 					/* set new input current limit */
-					typec_curr = get_typec_current_limit(
-						pd[port].polarity, cc1, cc2);
+					typec_curr =
+						usb_get_typec_current_limit(
+							pd[port].polarity,
+							cc1, cc2);
 					typec_set_input_current_limit(
 					  port, typec_curr, TYPE_C_VOLTAGE);
 				} else {
