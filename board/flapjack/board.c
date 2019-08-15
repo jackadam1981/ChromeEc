756a0be2a99ad9adaa43c03c95bd97e4b45eeed0
Nick Vaccaro <nvaccaro@google.com>
2019-08-15 13:52:10 -0700

    FORTEST: flapjack: add calib specs for 95210JDBU0G02S
    Change-Id: I1963b84bd9b04389e74ed7ab9ff656cbb035da2d
    Signed-off-by: Nick Vaccaro <nvaccaro@google.com>

--- board/flapjack/board.c
+++ board/flapjack/board.c
@@ -459,7 +459,7 @@ static struct als_drv_data_t g_tcs3400_data = {
 	.als_cal.uscale = 0,
 	.als_cal.offset = 0,
 	.als_cal.channel_scale = {
-		.k_channel_scale = ALS_CHANNEL_SCALE(1.0),   /* kc from VPD */
+		.k_channel_scale = ALS_CHANNEL_SCALE(0.991),   /* kc from VPD */
 		.cover_scale = ALS_CHANNEL_SCALE(0.9),       /* CT */
 	},
 };
@@ -483,7 +483,7 @@ static struct tcs3400_rgb_drv_data_t g_tcs3400_rgb_data = {
 		.coeff[TCS_BLUE_COEFF_IDX] = FLOAT_TO_FP(-0.0524428),
 		.coeff[TCS_CLEAR_COEFF_IDX] = FLOAT_TO_FP(0.09092403),
 		.scale = {
-			.k_channel_scale = ALS_CHANNEL_SCALE(1.0), /* kg */
+			.k_channel_scale = ALS_CHANNEL_SCALE(0.999), /* kg */
 			.cover_scale = ALS_CHANNEL_SCALE(1.0)
 		}
 	},
@@ -494,7 +494,7 @@ static struct tcs3400_rgb_drv_data_t g_tcs3400_rgb_data = {
 		.coeff[TCS_BLUE_COEFF_IDX] = FLOAT_TO_FP(-0.01858507),
 		.coeff[TCS_CLEAR_COEFF_IDX] = FLOAT_TO_FP(-0.01793189),
 		.scale = {
-			.k_channel_scale = ALS_CHANNEL_SCALE(1.0),   /* kb */
+			.k_channel_scale = ALS_CHANNEL_SCALE(0.994),   /* kb */
 			.cover_scale = ALS_CHANNEL_SCALE(1.5)
 		}
 	},
