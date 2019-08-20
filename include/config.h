fa3e4519c6ee8d04e4cc569d06d6b72bc5db86eb
Keith Short <keithshort@chromium.org>
2019-08-20 11:06:09 -0600

    config: Merge CONFIG_CHARGER_V2 into CONFIG_CHARGER
    For all boards that defined CONFIG_CHARGER, CONFIG_CHARGER_V2 is also
    defined. Remove references to CONFIG_CHARGER_V2 from board header files.
    Replace CONFIG_CHARGER_V2 in common C modules with CONFIG_CHARGER when
    appropriate.
    
    BUG=b:139699769
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I6b54baf4ad2406bbed629b6b272dad9ea6a81280
    Signed-off-by: Keith Short <keithshort@chromium.org>

--- include/config.h
+++ include/config.h
@@ -745,9 +745,8 @@
 /*****************************************************************************/
 /* Charger config */
 
-/* Compile common charge state code. You must pick an implementation. */
+/* Compile common charge state code. */
 #undef CONFIG_CHARGER
-#undef CONFIG_CHARGER_V2
 
 /* Compile charger-specific code for these chargers (pick at most one) */
 #undef CONFIG_CHARGER_BD9995X
@@ -991,8 +990,6 @@
 /*
  * Charger should call battery_override_params() to limit/correct the voltage
  * and current requested by the battery pack before acting on the request.
- *
- * This is valid with CONFIG_CHARGER_V2 only.
  */
 #undef CONFIG_CHARGER_PROFILE_OVERRIDE
 
