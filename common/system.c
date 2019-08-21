5c3736194edb5e7d10e477b681e49e1d80e2b4e8
Daisuke Nojiri <dnojiri@chromium.org>
2019-08-21 10:48:03 -0700

    system: Make EC_REBOOT_FLAG_SWITCH_RW_SLOT reset single-slot ECs
    Currently, when a single-slot EC receives EC_REBOOT_FLAG_SWITCH_RW_SLOT
    command, it returns an error. This change makes a single-slot EC ignores
    the flag and proceeds to reset. This is for implementing EFS on single-
    slot ECs.
    
    This patch also changes #ifdef CONFIG_VBOOT_EFS to CONFIG_RW_B where
    appropriate.
    
    BUG=chromium:998135
    BRANCH=none
    TEST=Boot Nami. Verify EFS and Softsync are working.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: Id936ac136a4bf8e75b0cf753a9f22689ee6b75b1

--- common/system.c
+++ common/system.c
@@ -639,7 +639,7 @@ enum system_image_copy_t system_get_active_copy(void)
 
 enum system_image_copy_t system_get_update_copy(void)
 {
-#ifdef CONFIG_VBOOT_EFS
+#ifdef CONFIG_RW_B
 	return system_get_active_copy() == SYSTEM_IMAGE_RW_A ?
 			SYSTEM_IMAGE_RW_B : SYSTEM_IMAGE_RW_A;
 #else
@@ -658,7 +658,7 @@ int system_set_active_copy(enum system_image_copy_t copy)
  */
 uint32_t flash_get_rw_offset(enum system_image_copy_t copy)
 {
-#ifdef CONFIG_VBOOT_EFS
+#ifdef CONFIG_RW_B
 	if (copy == SYSTEM_IMAGE_RW_B)
 		return CONFIG_EC_WRITABLE_STORAGE_OFF + CONFIG_RW_B_STORAGE_OFF;
 #endif
@@ -1485,13 +1485,10 @@ int host_command_reboot(struct host_cmd_handler_args *args)
 		return EC_RES_SUCCESS;
 	}
 
-	if (p.flags & EC_REBOOT_FLAG_SWITCH_RW_SLOT) {
-#ifdef CONFIG_VBOOT_EFS
+	if (!IS_ENABLED(CONFIG_RW_B)
+			&& p.flags & EC_REBOOT_FLAG_SWITCH_RW_SLOT) {
 		if (system_set_active_copy(system_get_update_copy()))
 			CPRINTS("Failed to set active slot");
-#else
-		return EC_RES_INVALID_PARAM;
-#endif
 	}
 	if (p.flags & EC_REBOOT_FLAG_ON_AP_SHUTDOWN) {
 		/* Store request for processing at chipset shutdown */
