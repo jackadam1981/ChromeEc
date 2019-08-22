904302d7b714c6eefdc6cdae1001d4ba9884646f
Ting Shen <phoenixshen@google.com>
2019-08-22 15:04:24 +0800

    WIP: add bitbang driver
    BUG=b:138161741,b:138415463
    TEST=On a reworked jacuzzi (battery i2c connected to other gpios),
         1) `battery` shows reasonable output (this verifies i2c_readN,
         i2c_read_string)
         2) `i2cscan` works for port 3 (bitbang port)
         3) `cutoff` (verifies i2c_writeN)
    BRANCH=master
    
    Change-Id: I78020e5c51707c3d9f0fd54f2c299e2f29cabe2f
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- include/config.h
+++ include/config.h
@@ -2268,6 +2268,10 @@
  */
 #undef CONFIG_I2C_MULTI_PORT_CONTROLLER
 
+/* TODO: add doc */
+#undef CONFIG_I2C_BITBANG
+#define I2C_BITBANG_PORT_COUNT 0
+
 /*
  * Packet error checking support for SMBus.
  *
