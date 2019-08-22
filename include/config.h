f5120ef7b8802675846ae239884454521e37d147
tim <tim2.lin@ite.corp-partner.google.com>
2019-08-22 18:20:55 +0800

    driver/ioexpander_it8801: add I/O expander driver for keyboard
    IT8801 is an I/O expander with a keyboard matrix controller.
    
    BUG=b:133200075
    BRANCH=none
    TEST=Press any key on keyboard and the console command
    "ksstate on" can print keyboard scan state.
    
    Change-Id: I6903aad1eacfdfb6a7c6c04e6954a14e8c8caaac
    Signed-off-by: tim <tim2.lin@ite.corp-partner.google.com>
    Signed-off-by: Alexandru M Stan <amstan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1677976
    (cherry picked from commit 7abc7ca4abbc2f4154e177389924c258ea37ff5d)

--- include/config.h
+++ include/config.h
@@ -2338,6 +2338,9 @@
 
 /******************************************************************************/
 
+/* Support IT8801 I/O expander. */
+#undef CONFIG_IO_EXPANDER_IT8801
+
 /* Support Nuvoton NCT38xx I/O expander. */
 #undef CONFIG_IO_EXPANDER_NCT38XX
 
@@ -2501,6 +2504,8 @@
  * Enable keypad (a palm-sized keyboard section usually placed on the far right)
  */
 #undef CONFIG_KEYBOARD_KEYPAD
+/*****************************************************************************/
+
 /*****************************************************************************/
 
 /* Support common LED interface */
