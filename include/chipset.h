2a3211ffe347450c36ea7e3ac1683e057e782ff3
Tom Hughes <tomhughes@chromium.org>
2019-08-23 15:06:57 -0700

    common: Add uptime host command
    This moves the EC_CMD_GET_UPTIME_INFO command from behind the
    CONFIG_CMD_AP_RESET_LOG config in chipset.c into the generic
    common/uptime.c file, so that all boards in the codebase can use it. If
    CONFIG_CMD_AP_RESET_LOG is enabled, the "AP reset stats" will be filled.
    Otherwise, ap_reset_stats is a no-op and recent_ap_reset is filled with
    zero.
    
    BRANCH=none
    BUG=chromium:997314
    TEST=cat /sys/kernel/debug/cros_fp/uptime
    
    Change-Id: I3b6f91b2dd22d3d55b707309ec1fdfd26d42fd70
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>

--- include/chipset.h
+++ include/chipset.h
@@ -15,7 +15,9 @@
 
 #include "common.h"
 #include "compile_time_macros.h"
+#include "ec_commands.h"
 #include "gpio.h"
+#include "stddef.h"
 
 /*
  * Chipset state mask
@@ -256,9 +258,24 @@ __override_proto enum critical_shutdown board_system_is_idle(
  */
 void report_ap_reset(enum chipset_shutdown_reason reason);
 
+/**
+ * Get statistics about AP resets.
+ *
+ * @param reset_log_entries       Pointer to array of log entries.
+ * @param num_reset_log_entries   Number of items in reset_log_entries.
+ * @param resets_since_ec_boot    Number of AP resets since EC boot.
+ */
+void ap_reset_stats(struct ap_reset_log_entry *reset_log_entries,
+		    size_t num_reset_log_entries,
+		    uint32_t *resets_since_ec_boot);
+
 #else
 
 static inline void report_ap_reset(enum chipset_shutdown_reason reason) { }
+
+static inline void ap_reset_stats(struct ap_reset_log_entry *reset_log_entries,
+				  size_t num_reset_log_entries,
+				  uint32_t *resets_since_ec_boot) { }
 
 #endif /* !CONFIG_CMD_AP_RESET_LOG */
 
