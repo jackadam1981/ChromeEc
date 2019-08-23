df06fb9b42b9ce6e2799e6e77afd1464414ba588
Daisuke Nojiri <dnojiri@chromium.org>
2019-08-23 13:32:36 -0700

    Nami: Enable EFS
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=chromium:998135
    BRANCH=none
    TEST=Boot Nami. Verify EFS and Softsync are working.
    
    Change-Id: I5823799e727bee8d01f683862072fea5415348ee

--- board/nami/board.h
+++ board/nami/board.h
@@ -8,6 +8,27 @@
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
 
+/* VBOOT/EFS */
+#define CONFIG_VBOOT_EFS
+#define CONFIG_RW_A_STORAGE_OFF		CONFIG_RW_STORAGE_OFF
+#define CONFIG_RW_B_STORAGE_OFF		CONFIG_RW_STORAGE_OFF
+#define CONFIG_RW_A_SIGN_STORAGE_OFF	(CONFIG_RW_A_STORAGE_OFF + \
+					 CONFIG_RW_SIZE - CONFIG_RW_SIG_SIZE)
+#define CONFIG_RW_B_SIGN_STORAGE_OFF	(CONFIG_RW_B_STORAGE_OFF + \
+					 CONFIG_RW_SIZE - CONFIG_RW_SIG_SIZE)
+#define CONFIG_RWSIG
+#define CONFIG_RWSIG_TYPE_RWSIG
+#define CONFIG_RSA
+#ifdef SECTION_IS_RO
+#define CONFIG_RSA_OPTIMIZED
+#endif
+#define CONFIG_SHA256
+#ifdef SECTION_IS_RO
+#define CONFIG_SHA256_UNROLLED
+#endif
+#define CONFIG_RSA_KEY_SIZE 3072
+#define CONFIG_RSA_EXPONENT_3
+
 /*
  * By default, enable all console messages except ACPI and host event because
  * the sensor stack is generating a lot of activity.
