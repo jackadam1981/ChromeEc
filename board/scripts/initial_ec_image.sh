42da9ab2c0143197138e0e1a696236a212764512
Paul Fagerburg <pfagerburg@chromium.org>
2019-08-23 11:28:22 -0600

    ec: create initial EC image for a new variant
    Creating an initial EC image for a variant is just a copy of the
    base board's files. After all of the CLs to create the variant
    have landed, then we can make changes specific to the variant.
    
    BUG=b:140261109
    BRANCH=none
    TEST=``./initial_ec_image.sh hatch sushi && git show``
    Compare board/hatch/* to board/sushi/*
    
    Also run the script with an existing board name to verify that you
    can't create a variant that already exists.
    
    Change-Id: Ic54b0b5a8b41476779dcbb6af63bd0ddfaff2896
    Signed-off-by: Paul Fagerburg <pfagerburg@chromium.org>

---
+++ board/scripts/initial_ec_image.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+name=$(git config --global --get user.name)
+email=$(git config --global --get user.email)
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 base_name variant_name"
+  echo "e.g. $0 hatch kohaku"
+  echo "Creates the initial EC image as a copy of the base board's EC."
+  exit 1
+fi
+
+base="$1"
+variant="$2"
+# We will use all uppercase and all lowercase versions of these names
+# ${var,,} converts to all lowercase, ${var^^} is all uppercase
+
+# All of the necessary files are in the parent directory from this script
+pushd "${BASH_SOURCE%/*}/.." || exit
+
+# Make sure the variant doesn't already exist
+if [ -e "${variant,,}" ]; then
+  echo "${variant,,} aleady exists; have you already created this variant?"
+  popd || exit
+  exit 2
+fi
+
+# Start a branch
+repo start "${variant,,}""
+
+mkdir "${variant,,}""
+cp "${base,,}"/* "${variant,,}"
+# TODO replace the base name with the variant name in the copied files,
+# TODO except for the BASEBOARD=${base^^} line in build.mk
+git add "${variant,,}"/*
+
+# Now commit the files
+git commit -m "${variant,,}: Initial EC image
+
+The starting point for the ${variant,,} EC image
+
+BUG=b:133181366
+BRANCH=none
+TEST=make BOARD=${variant,,}
+
+Signed-off-by: ${name} <${email}>"
+
+popd || exit
+
+echo "Please check all the files (git show), make any changes you want,"
+echo "and then repo upload."
