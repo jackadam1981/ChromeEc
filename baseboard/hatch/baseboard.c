17e6807a9c6b93f4c1e8feb0a962582e9ebbd869
Scott Collyer <scollyer@google.com>
2019-08-29 14:21:34 -0700

    hatch: Change EC console log message to CPRINTS from CPRINTF
    While debugging a power role swap issue, I noticed that the console
    message "Skip enable C0/1" in funciton board_set_active_charge_port
    was missing a carriage return. This CL changes this print to use
    CPRINTS instead of CPRINTF.
    
    BUG=None
    BRANCH=None
    TEST=Verified that this message was on its own line
    
    Change-Id: I29af506cb73e6c10cb3000216c993e7e7daf3daf
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- baseboard/hatch/baseboard.c
+++ baseboard/hatch/baseboard.c
@@ -270,7 +270,7 @@ int board_set_active_charge_port(int port)
 
 	/* Check if the port is sourcing VBUS. */
 	if (ppc_is_sourcing_vbus(port)) {
-		CPRINTFUSB("Skip enable C%d", port);
+		CPRINTSUSB("Skip enable C%d", port);
 		return EC_ERROR_INVAL;
 	}
 
