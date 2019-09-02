d6e45ba59cf234a01427c5e5bd5cee3aff842a36
Patrick Georgi <pgeorgi@google.com>
2019-09-02 17:16:09 +0200

    util/ectool: Free memory before giving up
    Found by Coverity Scan #203652, #203653
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I195773c42d4d0dfa35836e3e442cca15125ac9ea
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>

--- util/ectool.c
+++ util/ectool.c
@@ -8658,6 +8658,8 @@ int cmd_tp_frame_get(int argc, char* argv[])
 
 	if (data == NULL || r == NULL) {
 		fprintf(stderr, "Couldn't allocate memory.\n");
+		free(r);
+		free(data);
 		return EC_ERROR_UNKNOWN;
 	}
 
