c37252dce59d1aeb64c4c821228fba5d689afea5
Patrick Georgi <pgeorgi@google.com>
2019-09-09 23:04:40 +0200

    board/glkrvp: Don't check for VBUS source on DC jack
    That only leads to an array overflow in tcpc_read.
    
    Found by Coverity Scan #152503
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: Iba729bed35ed456e76725cd6e02acadfef6967b7
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>

--- board/glkrvp/chg_usb_pd.c
+++ board/glkrvp/chg_usb_pd.c
@@ -202,8 +202,11 @@ int board_set_active_charge_port(int port)
 	/* charge port is a realy physical port */
 	int is_real_port = (port >= 0 &&
 			port < CHARGE_PORT_COUNT);
-	/* check if we are source vbus on that port */
-	int source = board_charger_port_is_sourcing_vbus(port);
+
+	int source = 0;
+	/* check if we are source vbus on that port if it's a PD port */
+	if (port < CONFIG_USB_PD_PORT_COUNT)
+		source = board_charger_port_is_sourcing_vbus(port);
 
 	if (is_real_port && source) {
 		CPRINTS("Skip enable p%d", port);
