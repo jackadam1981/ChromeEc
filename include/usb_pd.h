03c1c14cc1382a574c7c34b1e598515d960d7bec
Ayushee <ayushee.shah@intel.com>
2019-09-11 15:44:51 -0700

    usb_pd: Sending thunderbolt compatible host notification
    Sending the following information to the host in cros_ec_usb_pd_control
    command to configure thunderbolt compat mode:
    1. TBT flag - Contains information about:
       a. Retimer (Present/Absent)
       b. Thunderbolt compat adapter (Type-C/ Legacy TBT adapter)
       c. Thunderbolt compatible type (Optical/Non-optical)
       d. Active link training (Uni/bi-directional communication)
    2. Cable speed (USB3.1 Gen1 Cable / 10 Gbps / 10 Gbps and 20 Gbps)
    3. Cable rounded support (3rd Gen Non-Rounded TBT/
    			  3rd & 4th Gen Rounded and Non-Rounded TBT)
    
    BUG=b:140644242
    BRANCH=None
    TEST=Verfied on the tglrvp-u CPU console using "ectool usbpd" command,
         able to get the information
    
    Change-Id: I38cf0b488945d78564c96eedab50a6e2900dfb80
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -1923,7 +1923,25 @@ struct tbt_mode_resp_cable get_cable_tbt_vdo(int port);
 struct tbt_mode_resp_device get_dev_tbt_vdo(int port);
 
 /**
- * Update Mux on entering TBT mode
+ * Return Thunderbolt cable speed
+ *
+ * @param port USB-C port number
+ * @return cable speed
+ */
+enum tbt_compat_cable_speed get_tbt_cable_speed(int port);
+
+/**
+ * Return Thunderbolt rounded support
+ * Rounded support indicates if the cable can support rounding the
+ * frequency depending upon the cable generation.
+ *
+ * @param port USB-C port number
+ * @return tbt_rounded_support
+ */
+uint8_t get_tbt_rounded_support(int port);
+
+/**
+ * Update Mux on entering Thunderbolt mode
  *
  * @param port USB-C port number
  */
