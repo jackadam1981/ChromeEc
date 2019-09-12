22895b600534c381bb6df82fd82debf34592d209
Caveh Jalali <caveh@google.com>
2019-09-12 05:37:12 +0000

    Revert "npcx: ensure we don't unlock watchdog too soon"
    This reverts commit 031c5d2d62dd891622ded885756c03021e934ef2.
    
    Reason for revert: introduced compilation errors:
    
    chip/npcx/spiflashfw/npcx_monitor.c: In function 'sspi_flash_upload':
    chip/npcx/spiflashfw/npcx_monitor.c:290:2: error: implicit declaration of function 'watchdog_stop_and_unlock' [-Werror=implicit-function-declaration]
      watchdog_stop_and_unlock();
      ^~~~~~~~~~~~~~~~~~~~~~~~
    
    
    Original change's description:
    > npcx: ensure we don't unlock watchdog too soon
    > 
    > We cannot unlock the watchdog timer with 3 watch dog ticks of touching
    > it per the datasheet. This is actually around 100ms so we should protect
    > against this.
    > 
    > BRANCH=none
    > BUG=b:140207603
    > TEST=eliminates cold reset issue.
    > 
    > Change-Id: Iaef59dad9f5640d64d5d430aea87bd16c2efd30d
    > Signed-off-by: Jett Rink <jettrink@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1790302
    > Reviewed-by: Scott Collyer <scollyer@chromium.org>
    > Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    > Reviewed-by: ML Chao <mlchao@nuvoton.corp-partner.google.com>
    > Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    > Tested-by: Furquan Shaikh <furquan@chromium.org>
    > Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    
    Bug: b:140207603
    Change-Id: I540fa53c2c568cb789400d55b807a672b182302a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1799293
    Reviewed-by: Caveh Jalali <caveh@google.com>
    Commit-Queue: Caveh Jalali <caveh@google.com>
    Tested-by: Caveh Jalali <caveh@google.com>

--- chip/npcx/spiflashfw/ec_npcxflash.c
+++ chip/npcx/spiflashfw/ec_npcxflash.c
@@ -271,7 +271,9 @@ sspi_flash_upload(int spi_offset, int spi_size)
 	uint32_t sz_image = spi_size;
 
 	/* Unlock & stop watchdog */
-	watchdog_stop_and_unlock();
+	NPCX_WDSDM = 0x87;
+	NPCX_WDSDM = 0x61;
+	NPCX_WDSDM = 0x63;
 
 	/* UMA Unlock */
 	CLEAR_BIT(NPCX_UMA_ECTS, NPCX_UMA_ECTS_UMA_LOCK);
