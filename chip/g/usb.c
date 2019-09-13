2b09ed1837d9c054ecac0de0c13c0a07a9a2b29e
Barry Twycross <barry@nestlabs.com>
2019-09-13 16:32:55 -0700

    Zero data toggles on endpointis when appropriate.
    The data toggle should be reset on an endpoint in two circumstances
    when the device is configured (with a SET_CONFIGURATION), or whent
    the endpoint is "cleared" (with a CLEAR_FEATURE(endpoint_halt).
    
    The endpoint reset code is deficient, in that the data toggle is not
    reset when appropriate. The upshot of this is that if the device and
    host ever get out of sync, the transfer will be ACKed, but the data
    thrown away (which is correct behaviour per spec).
    
    This causes all sorts of head scratching. In this particular case,
    the data which is discarded is a mailbox command to a Haven (root
    of trust) chip. Thus the host hangs forever waiting for a response
    that never comes, because the device never got the command.
    
    This behaviour is seen in testing for reasons which are not clear,
    but can be provoked by sending resets to the device, while sending
    resets to the device. The device will hang as the following
    SET_CONFIGURATION is supposed to clear the toggles (but doesn't)
    After implementing this fix, the device no longer hangs after a
    reset in this test.
    
    This issue is doubly bad for the device, as the recovery should be
    to time out the bulk tranfer, and reset the endpoint, or if that
    doesn't work, reset the device. With the deficient code it is
    impossible to use standard recovery mechanisms when running across
    this issue.
    
    Note: The Synopsis data book notes under the register
    (Device Endpoint-n Control Register: DIEPCTLn/DOEPCTLn),
    DPID "The application must program the PID of the first packet to
    be received or transmitted on this endpoint, after the endpoint is
    activated." This is what this change is fixing, the code does not
    currently do this.
    
    TEST= Problem can be reproduced by sending a reset to the haven, while
    it is being access by USB EC commands. 50% of the time, the next
    transfer will hang as the toggle is now incorrect. With this fix, the
    device can be reset and no hang.
    
    TEST= A similar test is done by sending CLEAR_FEATURE(endpoint_halt)
    commands to the device while processing USB EC commands. On a USB
    analyser you can see the toggle has been reset, as the device
    continues to work, even if the PIDs before and after the CLEAR_FEATURE
    were both DATA0.
    
    BRANCH=NONE
    
    Signed-off-by: barryt@google.com
    
    Change-Id: Id621cfaa422d3589187db7be8a188ed411bb4c98

--- chip/g/usb.c
+++ chip/g/usb.c
@@ -461,6 +461,28 @@ static void stall_both_fifos(void)
 	GR_USB_DIEPCTL(0) = DXEPCTL_STALL | DXEPCTL_EPENA;
 }
 
+static void usb_reset_all_ep_pids(void)
+{
+	int i;
+
+	CPRINTS("%s, num ep %x", __func__, USB_EP_COUNT);
+
+	for (i = 1; i < USB_EP_COUNT; i++) {
+		GR_USB_DOEPCTL(i) |= DXEPCTL_SET_D0PID;
+		GR_USB_DIEPCTL(i) |= DXEPCTL_SET_D0PID;
+	}
+}
+
+static void usb_reset_ep_pid(int ep)
+{
+	CPRINTS("%s, ep %x", __func__, ep);
+
+	if (ep & 0x80)	 /* IN enpoint */
+		GR_USB_DIEPCTL(ep & 0x7f) |= DXEPCTL_SET_D0PID;
+	else
+		GR_USB_DOEPCTL(ep) |= DXEPCTL_SET_D0PID;
+}
+
 /* The next packet from the host should be a Setup packet. Get ready for it. */
 static void expect_setup_packet(void)
 {
@@ -760,7 +782,7 @@ static int handle_setup_with_no_data_stage(enum table_case tc,
 			device_state = DS_ADDRESS;
 			break;
 		case 1:	    /* Caution: Only one config descriptor TODAY */
-			/* TODO: All endpoints set to DATA0 toggle state */
+			usb_reset_all_ep_pids();
 			configuration_value = req->wValue;
 			device_state = DS_CONFIGURED;
 			break;
@@ -774,7 +796,7 @@ static int handle_setup_with_no_data_stage(enum table_case tc,
 	case USB_REQ_SET_FEATURE:
 		/* TODO: Handle DEVICE_REMOTE_WAKEUP, ENDPOINT_HALT? */
 		print_later("SET_FEATURE/CLEAR_FEATURE. Whatever...",
-			    0, 0, 0, 0, 0);
+								0, 0, 0, 0, 0);
 		break;
 
 	default:
@@ -816,6 +838,29 @@ static void handle_setup(enum table_case tc)
 		if (iface < USB_IFACE_COUNT) {
 			bytes = usb_iface_request[iface](req);
 			print_later("  iface returned %d", bytes, 0, 0, 0, 0);
+		}
+	} else if (USB_RECIP_ENDPOINT ==
+			    (req->bmRequestType & USB_RECIP_MASK)) {
+		/* Endpoint-specific requests */
+		uint8_t ep = req->wIndex & 0xff;
+
+		print_later("ep %d request (vs %d)",
+			ep, USB_EP_COUNT, 0, 0, 0);
+		if ((ep & 0x7f) < USB_EP_COUNT) {
+			/* This could call out to handle_setup_with_no_data_stage(tc, req) */
+			/* etc but only clearing the stall is implemented, and that would */
+			/* activate a bunch of other untested code paths. */
+			if ((req->bRequest == USB_REQ_CLEAR_FEATURE) &&
+				((req->bmRequestType & USB_TYPE_MASK) == USB_TYPE_STANDARD)) {
+				  /* standard endpoint request */
+					/* Case for CLEAR_FEATURE(endpoint_halt) */
+				usb_reset_ep_pid(req->wIndex);
+				bytes = 0;
+			} else {
+				report_error(-1);
+			}
+		} else {
+			report_error(-1);
 		}
 	} else {
 #ifdef CONFIG_WEBUSB_URL
