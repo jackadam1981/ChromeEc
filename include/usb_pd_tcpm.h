58dde3f7f5c73bdb73804641c367a1e22c0a6a08
Denis Brockus <dbrockus@chromium.org>
2019-09-25 11:33:53 -0600

    pd: FRS enable/disable and interrupt handling
    BUG=b:138599955
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I0c639aae18e8c2c2d1b457e2e209f8484a834f6b
    Signed-off-by: Denis Brockus <dbrockus@chromium.org>

--- include/usb_pd_tcpm.h
+++ include/usb_pd_tcpm.h
@@ -324,6 +324,17 @@ struct tcpm_drv {
 	 */
 	int (*enter_low_power_mode)(int port);
 #endif
+
+	/**
+	 * Enable/Disable TCPC FRS detection
+	 *
+	 * NOTE: It is valid to not enable/disable FRS at the TCPC level
+	 * if it is implemented in the PPC
+	 *
+	 * @param port Type-C port number
+	 * @param enable FRS enable (true) disable (false)
+	 */
+	 void (*set_frs_enable)(int port, int enable);
 };
 
 /*
@@ -417,5 +428,13 @@ int board_tcpc_post_init(int port) __attribute__((weak));
  *
  */
 void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled);
+
+/**
+ * Enable/Disable TCPC FRS detection
+ *
+ * @param port Type-C port number
+ * @param enable FRS enable (true) disable (false)
+ */
+void tcpc_set_frs_enable(int port, int enable);
 
 #endif /* __CROS_EC_USB_PD_TCPM_H */
