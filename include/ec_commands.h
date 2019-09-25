d659dfed09d2af8ad7fd29c45fbdf7fc51155569
Tim Wawrzynczak <twawrzynczak@chromium.org>
2019-09-25 16:05:47 -0600

    ec_commands: Declare new host command EC_CMD_GET_POWER_STATUS
    The new host command gives the AP the ability to dynamically change its
    power settings based on the battery status, A/C, USB-PD, and the static
    configuration of the system and its battery.
    
    BUG=b:139840435
    BRANCH=master
    TEST=buildall
    
    Change-Id: I516d838581400314e626fd679ca1c5afd5e34ff6
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -5703,6 +5703,83 @@ struct ec_response_locate_chip {
 } __ec_align2;
 
 /*****************************************************************************/
+/* Return system power status information
+ *
+ * Return values:
+ * EC_RES_INVALID: The EC does not respond to this command
+ */
+#define EC_CMD_GET_POWER_STATUS 0x0127
+
+enum system_power_source {
+	POWER_SOURCE_UNKNOWN = 0,
+	POWER_SOURCE_BATTERY = 1,
+	POWER_SOURCE_AC = 2,
+	POWER_SOURCE_AC_BATTERY = 3,
+};
+
+enum power_status_type {
+	/*
+	 * This is the "base" power information that is static and comes from
+	 * the mainboards specifications, such as VR power efficiency.
+	 */
+	POWER_STATUS_BASE = 0,
+	/* This is the dynamic power information that changes constantly. */
+	POWER_STATUS_DYNAMIC = 1,
+};
+
+struct ec_params_get_power_status {
+	/* One of enum power_status_type */
+	uint8_t power_status_type;
+} __ec_align1;
+
+struct ec_response_get_power_status {
+	/* enum system_power_source */
+	uint8_t system_power_source;
+	/* Battery state-of-charge, 0-100, 0 if not present */
+	uint8_t battery_soc;
+	/* For alignment */
+	uint16_t reserved;
+
+	union {
+		/* Dynamic section */
+		struct {
+			/* AC Adapter 100% rating, Watts */
+			uint8_t ac_adapter_100pct;
+			/* AC Adapter 10ms rating, Watts */
+			uint8_t ac_adapter_10ms;
+			/* Battery 2Cd rating, Watts */
+			uint8_t battery_2cd;
+			/* Battery 4Cd rating, Watts */
+			uint8_t battery_4cd;
+			/* Rest of Platform average, Watts */
+			uint8_t rop_avg;
+			/* Rest of Platform peak, Watts */
+			uint8_t rop_peak;
+			/* If DBPT is available */
+			/* Maximum peak power from battery (10ms), Watts */
+			uint8_t batt_dbpt_max_peak_power;
+			/* Sustained peak power from battery, Watts */
+			uint8_t batt_dbpt_sus_peak_power;
+		} dynamic;
+		/* Base section - values that don't change */
+		struct {
+			/* Nominal charger efficiency, % */
+			uint8_t nominal_charger_eff;
+			/* Rest of Platform VR Average Efficiency, % */
+			uint8_t rop_avg_eff;
+			/* Rest of Platform VR Peak Efficiency, % */
+			uint8_t rop_peak_eff;
+			/* SoC VR Efficiency at Average level, % */
+			uint8_t soc_avg_eff;
+			/* SoC VR Efficiency at Peak level, % */
+			uint8_t soc_peak_eff;
+			/* Battery's level of DBPT support: 0, 2 */
+			uint8_t batt_dbpt_support_level;
+		} base;
+	};
+} __ec_align1;
+
+/*****************************************************************************/
 /* The command range 0x200-0x2FF is reserved for Rotor. */
 
 /*****************************************************************************/
