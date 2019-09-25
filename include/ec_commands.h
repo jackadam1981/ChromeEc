987438e72c709e8cf9f0ee6160580c87e998ab03
Tim Wawrzynczak <twawrzynczak@chromium.org>
2019-09-25 16:05:47 -0600

    ec_commands: Obsolete v0 and declare v1 of host command EC_CMD_POWER_INFO
    v0 of this command is no longer used (no handler is defined), so it is
    removed.  The new host command version gives the AP the ability to
    dynamically change its power settings based on the battery status,
    A/C, USB-PD, and the static configuration of the system and its
    battery.
    
    BUG=b:139840435
    BRANCH=master
    TEST=buildall
    
    Change-Id: I516d838581400314e626fd679ca1c5afd5e34ff6
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -4071,16 +4071,59 @@ struct ec_response_ldo_get {
 
 /*
  * Get power info.
+ *
+ * Note: v0 of this command is deprecated
  */
 #define EC_CMD_POWER_INFO 0x009D
 
-struct ec_response_power_info {
-	uint32_t usb_dev_type;
-	uint16_t voltage_ac;
-	uint16_t voltage_system;
-	uint16_t current_system;
-	uint16_t usb_current_limit;
-} __ec_align4;
+/*
+ * v1 of EC_CMD_POWER_INFO
+ */
+enum system_power_source {
+	POWER_SOURCE_UNKNOWN = 0,
+	POWER_SOURCE_BATTERY = 1,
+	POWER_SOURCE_AC = 2,
+	POWER_SOURCE_AC_BATTERY = 3,
+};
+
+struct ec_response_power_info_v1 {
+	/* enum system_power_source */
+	uint8_t system_power_source;
+	/* Battery state-of-charge, 0-100, 0 if not present */
+	uint8_t battery_soc;
+	/* AC Adapter 100% rating, Watts */
+	uint8_t ac_adapter_100pct;
+	/* AC Adapter 10ms rating, Watts */
+	uint8_t ac_adapter_10ms;
+	/* Battery 1C rating, derated */
+	uint8_t battery_1cd;
+	/* Rest of Platform average, Watts */
+	uint8_t rop_avg;
+	/* Rest of Platform peak, Watts */
+	uint8_t rop_peak;
+	/* Nominal charger efficiency, % */
+	uint8_t nominal_charger_eff;
+	/* Rest of Platform VR Average Efficiency, % */
+	uint8_t rop_avg_eff;
+	/* Rest of Platform VR Peak Efficiency, % */
+	uint8_t rop_peak_eff;
+	/* SoC VR Efficiency at Average level, % */
+	uint8_t soc_avg_eff;
+	/* SoC VR Efficiency at Peak level, % */
+	uint8_t soc_peak_eff;
+	/* Battery's level of DBPT support: 0, 2 */
+	uint8_t batt_dbpt_support_level;
+	/*
+	 * Maximum peak power from battery (10ms), Watts
+	 * If DBPT is not supported, this is 0
+	 */
+	uint8_t batt_dbpt_max_peak_power;
+	/*
+	 * Sustained peak power from battery, Watts
+	 * If DBPT is not supported, this is 0
+	 */
+	uint8_t batt_dbpt_sus_peak_power;
+} __ec_align1;
 
 /*****************************************************************************/
 /* I2C passthru command */
