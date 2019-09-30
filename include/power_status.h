9485679ea467399b0e83de7500032266f887e621
Tim Wawrzynczak <twawrzynczak@chromium.org>
2019-09-30 15:36:37 -0600

    power: Add new power_status functionality
    This implements the EC_CMD_POWER_INFO host command, as well as all of
    the necessary infrastructure to support this command, including
    keeping track of the system power source, and the amount of power each
    one can provide.
    
    BUG=b:139840435, b:205928013
    BRANCH=master
    TEST=Setup DBPT parameters on helios. Use 'hostcmd' debug feature to
    print out host command results, and verify they are correct.  Also use
    the 'dumppower' console command to print system power parameters in
    different configurations (i.e., battery only, AC only, and
    battery+AC).  Disable DBPT, and verify that the non-DBPT settings are
    correct.
    
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Change-Id: Iab1dfbdbb2a7049dfed3bc7aab55186aeddbe6df

---
+++ include/power_status.h
@@ -0,0 +1,37 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_POWER_STATUS_H__
+#define __CROS_EC_POWER_STATUS_H__
+
+/* Power status related items that need to be exported */
+
+struct board_power_config {
+	/* Battery 1C rating */
+	const int batt_1c_level;
+	/* Efficiency of AC adapter power conversion */
+	const int nominal_charger_eff;
+	/* Efficiency of RoP VRs at average power */
+	const int rop_avg_eff;
+	/* Efficiency of RoP VRs at peak power */
+	const int rop_peak_eff;
+	/* Efficiency of SOC VRs at average power */
+	const int soc_avg_eff;
+	/* Efficiency of SOC VRs at peak power */
+	const int soc_peak_eff;
+	/* Average RoP power */
+	const int rop_avg;
+	/* Peak RoP power */
+	const int rop_peak;
+	/* DBPT v2 - in mOhm */
+	const int sys_resistance;
+	/* DBPT v2 - in mV */
+	const int min_sys_voltage;
+};
+
+/* Expected to be provided by the board */
+const struct board_power_config *board_get_power_config(void);
+
+#endif /* __CROS_EC_POWER_STATUS_H__ */
