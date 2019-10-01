ce010ec4a99fba49292e89053ba9fe765ffb3ac2
Yuval Peress <peress@chromium.org>
2019-10-01 10:42:28 -0600

    common: Add feature flag for online calibration
    This change adds a feature flag for online calibration.
    
    BUG=b:138303429
    BRANCH=None
    TEST=buildall since flag is not yet used
    Signed-off-by: Yuval Peress <peress@chromium.org>
    
    Change-Id: I3036371a7499bf0eaf0846ff07eec6e6ee18a391

--- common/motion_sense.c
+++ common/motion_sense.c
@@ -1075,7 +1075,16 @@ static enum ec_status host_cmd_motion_sense(struct host_cmd_handler_args *args)
 
 		out->info.location = sensor->location;
 		out->info.chip = sensor->chip;
-		if (args->version >= 3) {
+		if (args->version >= 4) {
+			out->info_4.min_frequency = sensor->min_frequency;
+			out->info_4.max_frequency = sensor->max_frequency;
+			out->info_4.fifo_max_event_count = MAX_FIFO_EVENT_COUNT;
+			if (IS_ENABLED(CONFIG_ONLINE_CALIB) &&
+			    sensor->drv->read_temp)
+				out->info_4.flags |=
+					MOTION_SENSE_CMD_INFO_FLAG_ONLINE_CALIB;
+			args->response_size = sizeof(out->info_4);
+		} else if (args->version == 3) {
 			out->info_3.min_frequency = sensor->min_frequency;
 			out->info_3.max_frequency = sensor->max_frequency;
 			out->info_3.fifo_max_event_count = MAX_FIFO_EVENT_COUNT;
@@ -1414,9 +1423,9 @@ static enum ec_status host_cmd_motion_sense(struct host_cmd_handler_args *args)
 	return EC_RES_SUCCESS;
 }
 
-DECLARE_HOST_COMMAND(EC_CMD_MOTION_SENSE_CMD,
-		     host_cmd_motion_sense,
-		     EC_VER_MASK(1) | EC_VER_MASK(2) | EC_VER_MASK(3));
+DECLARE_HOST_COMMAND(EC_CMD_MOTION_SENSE_CMD, host_cmd_motion_sense,
+		     EC_VER_MASK(1) | EC_VER_MASK(2) | EC_VER_MASK(3) |
+			     EC_VER_MASK(4));
 
 /*****************************************************************************/
 /* Console commands */
