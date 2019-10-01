cda210028c0cf36e1839c0ec3987736df5e22018
Yuval Peress <peress@chromium.org>
2019-10-01 00:51:23 -0600

    common: Add sensor stillness detector
    This change adds a stillness detector for 3d sensors.
    This will be needed to filter sensor readings when
    calibrating later.
    
    BUG=b:138303429
    BRANCH=None
    TEST=buildall with new unit tests
    Change-Id: I919ae7533fd42b0394de66aa0585e58343a662cc
    Signed-off-by: Yuval Peress <peress@chromium.org>

--- include/test_util.h
+++ include/test_util.h
@@ -40,11 +40,13 @@
 
 #define TEST_OPERATOR(a, b, op, fmt) \
 	do { \
-		if (!((a) op (b))) { \
+		__auto_type _a = (a); \
+		__auto_type _b = (b); \
+		if (!(_a op _b)) { \
 			ccprintf("%d: ASSERSION failed: %s " #op " %s\n", \
 				 __LINE__, #a, #b); \
 			ccprintf("\t\tEVAL: " fmt " " #op " " fmt "\n", \
-				 (a), (b)); \
+				 _a, _b); \
 			task_dump_trace(); \
 			return EC_ERROR_UNKNOWN; \
 		} \
