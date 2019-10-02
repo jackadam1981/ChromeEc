fbffea7254c2806488bf127e7d39f5ad442edf62
Denis Brockus <dbrockus@chromium.org>
2019-10-02 09:43:25 -0600

    trembyle: board specific fast switch enable/disable
    Added board specific tcpc FRS enable/disable to work around
    timing issues that we found with trembyle.
    
    Also added some of the bringup debug settings
    
    BUG=b:138599218
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: Ia16db22a9c2b3a47a4273ea1350a5ea58b78f5aa
    Signed-off-by: Denis Brockus <dbrockus@chromium.org>

--- baseboard/zork/baseboard.h
+++ baseboard/zork/baseboard.h
@@ -8,16 +8,6 @@
 #ifndef __CROS_EC_BASEBOARD_H
 #define __CROS_EC_BASEBOARD_H
 
-/*
- * By default, enable all console messages excepted HC, ACPI and event:
- * The sensor stack is generating a lot of activity.
- */
-#define CC_DEFAULT     (CC_ALL & ~(CC_MASK(CC_EVENTS) | CC_MASK(CC_LPC)))
-#define CONFIG_SUPPRESSED_HOST_COMMANDS \
-	EC_CMD_CONSOLE_SNAPSHOT, EC_CMD_CONSOLE_READ, EC_CMD_USB_PD_DISCOVERY,\
-	EC_CMD_USB_PD_POWER_INFO, EC_CMD_PD_GET_LOG_ENTRY, \
-	EC_CMD_MOTION_SENSE_CMD, EC_CMD_GET_NEXT_EVENT
-
 /* NPCX7 config */
 #define NPCX_UART_MODULE2 1  /* GPIO64/65 are used as UART pins. */
 #define NPCX_TACH_SEL2    0  /* No tach. */
@@ -26,7 +16,7 @@
 /* Internal SPI flash on NPCX7 */
 #define CONFIG_FLASH_SIZE (512 * 1024)
 #define CONFIG_SPI_FLASH_REGS
-#define CONFIG_SPI_FLASH_W25Q80 /* Internal SPI flash type. */
+#define CONFIG_SPI_FLASH_W25Q40 /* Internal SPI flash type. */
 
 /*
  * Enable 1 slot of secure temporary storage to support
@@ -46,8 +36,6 @@
 #define CONFIG_HOSTCMD_SKUID
 #define CONFIG_I2C
 #define CONFIG_I2C_MASTER
-#define CONFIG_LOW_POWER_IDLE
-#define CONFIG_LOW_POWER_S0
 #define CONFIG_LTO
 #define CONFIG_PWM
 #define CONFIG_PWM_KBLIGHT
@@ -80,9 +68,6 @@
 #define CONFIG_CHIPSET_STONEY
 #define CONFIG_CHIPSET_CAN_THROTTLE
 #define CONFIG_CHIPSET_RESET_HOOK
-
-/* Use external 32kHz OSC as LFCLK source */
-#define CONFIG_CLOCK_SRC_EXTERNAL
 
 #undef  CONFIG_EXTPOWER_DEBOUNCE_MS
 #define CONFIG_EXTPOWER_DEBOUNCE_MS 200
@@ -120,12 +105,12 @@
 
 /* TODO(b/142284905): Enable new PD stack */
 #if 0
-/* Enable the new USB-C PD stack */
 #define CONFIG_USB_PE_SM
 #define CONFIG_USB_PRL_SM
 #define CONFIG_USB_SM_FRAMEWORK
 #define CONFIG_USB_TYPEC_SM
 #define CONFIG_USB_TYPEC_DRP_ACC_TRYSRC
+#define CONFIG_USB_TYPEC_PD_FAST_ROLE_SWAP
 #endif
 
 #define CONFIG_CMD_PD_CONTROL
@@ -298,6 +283,10 @@ void ppc_interrupt(enum gpio_signal signal);
 
 int board_is_convertible(void);
 void board_update_sensor_config_from_sku(void);
+
+#ifdef CONFIG_USB_TYPEC_PD_FAST_ROLE_SWAP
+int board_tcpc_fast_role_swap_enable(int port, int enable);
+#endif
 
 #endif /* !__ASSEMBLER__ */
 
