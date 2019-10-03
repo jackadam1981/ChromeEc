6bd872488d12e6baf743059519bfbffba46f4cf2
Stefan Reinauer <stefan.reinauer@coreboot.org>
2019-10-02 21:17:55 -0700

    Drop LTO workaround from sweetberry
    Because it breaks external builds such as coverity builds.
    The issue needs to be fixed differently.
    
    BUG=b:132204142
    TEST=flash on sweetberry and see usb endpoints work
    BRANCH=none
    
    Change-Id: I2ee1789e12dd5240fac4bf97a2638efa85446df6

--- board/sweetberry/build.mk
+++ board/sweetberry/build.mk
@@ -5,13 +5,13 @@
 #
 # Board specific files build
 
-
-# TODO(b/132204142): Sweetberry's USB connection fails to initialize properly
-# when built using the 8.2.0 tool chain leave it with the 4.9.x.
-CROSS_COMPILE_arm:=arm-none-eabi-
-
 CHIP:=stm32
 CHIP_FAMILY:=stm32f4
 CHIP_VARIANT:=stm32f446
+
+# Use coreboot-sdk
+$(call set-option,CROSS_COMPILE_arm,\
+	$(CROSS_COMPILE_coreboot_sdk_arm),\
+	/opt/coreboot-sdk/bin/arm-eabi-)
 
 board-y=board.o
