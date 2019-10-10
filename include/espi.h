2e4969f10cf5288781874a11baa1bcdbd7f87bda
Edward Hill <ecgh@chromium.org>
2019-10-10 15:43:02 -0600

    GPIO/IOEX/eSPI: Give different IO signals unique values
    There are 3 different IO signal types used by the EC.
    Ensure they each use a unique range of values so we can tell them apart.
    1) Local GPIO => 0 to 0x0FFF
    2) IO expander GPIO => 0x1000 to 0x1FFF
    3) eSPI virtual wire signals => 0x2000 to 0x2FFF
    
    BUG=b:138600691
    BRANCH=none
    TEST=IO expander signals still work on Trembyle
    
    Change-Id: I63fadb4bb4573ed3dd121c24e3bd40a00873e29f
    Signed-off-by: Edward Hill <ecgh@chromium.org>

--- include/espi.h
+++ include/espi.h
@@ -12,7 +12,7 @@
 
 /* Signal through VW */
 enum espi_vw_signal {
-	VW_SIGNAL_BASE = GPIO_COUNT,
+	VW_SIGNAL_BASE = VW_SIGNAL_START - 1,
 	VW_SLP_S3_L,			/* index 02h (In)  */
 	VW_SLP_S4_L,
 	VW_SLP_S5_L,
@@ -37,10 +37,10 @@ enum espi_vw_signal {
 	VW_SLP_A_L,
 	VW_SLP_LAN,                     /* index 42h (In)  */
 	VW_SLP_WLAN,
-	VW_SIGNAL_BASE_END,
+	VW_SIGNAL_END,
 };
 
-#define VW_SIGNAL_COUNT (VW_SIGNAL_BASE_END - VW_SIGNAL_BASE - 1)
+#define VW_SIGNAL_COUNT (VW_SIGNAL_END - VW_SIGNAL_START)
 
 /**
  * Set eSPI Virtual-Wire signal to Host
--- include/gpio_signal.h
+++ include/gpio_signal.h
@@ -6,21 +6,36 @@
 #ifndef __CROS_EC_GPIO_SIGNAL_H
 #define __CROS_EC_GPIO_SIGNAL_H
 
+/*
+ * There are 3 different IO signal types used by the EC.
+ * Ensure they each use a unique range of values so we can tell them apart.
+ * 1) Local GPIO => 0 to 0x0FFF
+ * 2) IO expander GPIO => 0x1000 to 0x1FFF
+ * 3) eSPI virtual wire signals (defined in include/espi.h) => 0x2000 to 0x2FFF
+ */
+#define IOEX_SIGNAL_START	0x1000
+#define VW_SIGNAL_START		0x2000
+
 #define GPIO(name, pin, flags) GPIO_##name,
 #define UNIMPLEMENTED(name) GPIO_##name,
 #define GPIO_INT(name, pin, flags, signal) GPIO_##name,
 
 enum gpio_signal {
 	#include "gpio.wrap"
-	GPIO_COUNT
+	GPIO_COUNT,
+	/* Ensure that sizeof gpio_signal is large enough for ioex_signal */
+	GPIO_LIMIT = IOEX_SIGNAL_START
 };
 
 #define IOEX(name, expin, flags) IOEX_##name,
 #define IOEX_INT(name, expin, flags, signal) IOEX_##name,
 
 enum ioex_signal {
+	IOEX_SIGNAL_BASE = IOEX_SIGNAL_START - 1,
 	#include "gpio.wrap"
-	IOEX_COUNT
+	IOEX_SIGNAL_END
 };
+
+#define IOEX_COUNT (IOEX_SIGNAL_END - IOEX_SIGNAL_START)
 
 #endif /* __CROS_EC_GPIO_SIGNAL_H */
