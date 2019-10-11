53a4b259d69747b51f93050af9be8be01f4a11ea
Caveh Jalali <caveh@chromium.org>
2019-10-10 19:26:23 -0700

    system: allow reset for testing
    Change-Id: I1f65902c21ab6fc17c32388795cfef19c84d8cc8

--- common/system.c
+++ common/system.c
@@ -1596,3 +1596,12 @@ void clock_enable_module(enum module_id module, int enable)
 	 * function.
 	 */
 }
+
+#ifdef TEST_BUILD
+void system_common_reset_state(void)
+{
+	jdata = 0;
+	reset_flags = 0;
+	jumped_to_image = 0;
+}
+#endif  /* TEST_BUILD */
