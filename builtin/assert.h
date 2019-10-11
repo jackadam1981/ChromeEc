dea6fcb4f964d4327d5392f8d678c5d8e36c6a72
Caveh Jalali <caveh@chromium.org>
2019-10-10 22:21:41 -0700

    panic: disable noreturn on panic*() when fuzzing
    we need to disable attribute(noreturn) on the panic* series of
    functions when building unit tests for fuzzing.  in unit tests (i.e
    host builds), the panic*() routines don't necessarily terminate.
    "fuzz_test_noreturn" macro definition to support these cases.
    
    BRANCH=none
    BUG=b:142031466
    TEST=make buildall passes
    
    Change-Id: I7c296e9ff3cc5257230259f45a9dde751d247ca5
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- builtin/assert.h
+++ builtin/assert.h
@@ -18,9 +18,17 @@ extern "C" {
 #ifdef CONFIG_DEBUG_ASSERT
 #ifdef CONFIG_DEBUG_ASSERT_REBOOTS
 
+/**
+ * Report an assertion failure and reset
+ *
+ * @param msg		Assertion expression or other message
+ * @param func		Function name where assertion happened
+ * @param fname		File name where assertion happened
+ * @param linenum	Line number where assertion happened
+ */
 #ifdef CONFIG_DEBUG_ASSERT_BRIEF
-extern void panic_assert_fail(const char *fname, int linenum)
-	__attribute__((noreturn));
+extern void panic_assert_fail(const char *fname,
+			      int linenum) fuzz_test_noreturn;
 #define ASSERT(cond)                                           \
 	do {                                                   \
 		if (!(cond))                                   \
@@ -28,8 +36,8 @@ extern void panic_assert_fail(const char *fname, int linenum)
 	} while (0)
 #else
 extern void panic_assert_fail(const char *msg, const char *func,
-			      const char *fname, int linenum)
-	__attribute__((noreturn));
+			      const char *fname,
+			      int linenum) fuzz_test_noreturn;
 #define ASSERT(cond)                                                 \
 	do {                                                         \
 		if (!(cond))                                         \
