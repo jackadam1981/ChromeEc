fa379da4df777aed8c8288e2859c1252a735d261
Caveh Jalali <caveh@chromium.org>
2019-10-10 23:04:17 -0700

    sysjump: add tests for older panic_data
    this adds sysjump coverage for the case where the sysjump origin EC
    image uses different a different panic_data structur from the
    destination EC image.  this tests the case where we might have an
    older EC-RO image sysjumping into a newer EC-RW image.
    
    BRANCH=none
    BUG=b:142031466
    TEST=make builall passes
    
    Change-Id: Ia63f943e79d7d1aa8b4934f5370c6d8fb369d3ba
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- test/sysjump.c
+++ test/sysjump.c
@@ -23,6 +23,47 @@
  */
 
 /*
+ * original size:
+ * 1762de9d19 (Vincent Palatin   2013-12-04 17:15:12 -0800  45)
+ */
+
+struct cortex_panic_data_size1 {
+	uint32_t regs[12];        /* psp, ipsr, msp, r4-r11, lr(=exc_return).
+				   * In version 1, that was uint32_t regs[11] =
+				   * psp, ipsr, lr, r4-r11
+				   */
+	uint32_t frame[8];        /* r0-r3, r12, lr, pc, xPSR */
+
+	uint32_t mmfs;
+	uint32_t bfar;
+	uint32_t mfar;
+	uint32_t shcsr;
+	uint32_t hfsr;
+	uint32_t dfsr;
+};
+
+struct panic_data_size1 {
+	uint8_t arch;             /* Architecture (PANIC_ARCH_*) */
+	uint8_t struct_version;   /* Structure version (currently 2) */
+	uint8_t flags;            /* Flags (PANIC_DATA_FLAG_*) */
+	uint8_t reserved;         /* Reserved; set 0 */
+
+	/* core specific panic data */
+	union {
+		struct cortex_panic_data_size1 cm; /* Cortex-Mx registers */
+	};
+
+	/*
+	 * These fields go at the END of the struct so we can find it at the
+	 * end of memory.
+	 */
+	uint32_t struct_size;     /* Size of this struct */
+	uint32_t magic;           /* PANIC_SAVE_MAGIC if valid */
+};
+
+BUILD_ASSERT(sizeof(struct panic_data_size1) == 116);
+
+/*
  * size2 (144 bytes) as of:
  * 59d060ebfe (Dino Li           2019-06-10 16:26:36 +0800  86)
  */
@@ -49,7 +90,28 @@ struct panic_data_size2 {
 	uint32_t magic;           /* PANIC_SAVE_MAGIC if valid */
 };
 
+/*
+ * if one of these assertions fails, we must have changed the size of
+ * panic_data.  add the definitions here and add tests for the new
+ * size.
+ */
+BUILD_ASSERT(sizeof(struct panic_data_size2) == 144);
 BUILD_ASSERT(sizeof(struct panic_data_size2) == sizeof(struct panic_data));
+
+static int set_panic_size1(void)
+{
+	static const struct panic_data_size1 p1data = {
+		.magic = PANIC_DATA_MAGIC,
+		.struct_size = sizeof(struct panic_data_size1),
+	};
+	uintptr_t p1addr;
+
+	p1addr = CONFIG_RAM_BASE + CONFIG_RAM_SIZE -
+		sizeof(struct panic_data_size1);
+	memcpy((void *)p1addr, &p1data, sizeof(p1data));
+	TEST_ASSERT(panic_get_data());
+	return EC_SUCCESS;
+}
 
 static int set_panic_size2(void)
 {
@@ -187,10 +249,19 @@ static int test_Pn_J2(void)
 	return EC_SUCCESS;
 }
 
-static int test_P3_J2(void)
+static int test_P2_J2(void)
 {
 	setup();
 	CHECK(set_panic_size2());
+	CHECK(set_jump_v2());
+	CHECK(run_sysjump());
+	return EC_SUCCESS;
+}
+
+static int test_P1_J2(void)
+{
+	setup();
+	CHECK(set_panic_size1());
 	CHECK(set_jump_v2());
 	CHECK(run_sysjump());
 	return EC_SUCCESS;
@@ -209,10 +280,19 @@ static int test_Pn_J3(void)
 	return EC_SUCCESS;
 }
 
-static int test_P3_J3(void)
+static int test_P2_J3(void)
 {
 	setup();
 	CHECK(set_panic_size2());
+	CHECK(set_jump_v3());
+	CHECK(run_sysjump());
+	return EC_SUCCESS;
+}
+
+static int test_P1_J3(void)
+{
+	setup();
+	CHECK(set_panic_size1());
 	CHECK(set_jump_v3());
 	CHECK(run_sysjump());
 	return EC_SUCCESS;
@@ -223,10 +303,12 @@ void run_test(void)
 	test_reset();
 
 	RUN_TEST(test_Pn_J3);
-	RUN_TEST(test_P3_J3);
+	RUN_TEST(test_P2_J3);
+	RUN_TEST(test_P1_J3);
 
 	RUN_TEST(test_Pn_J2);
-	RUN_TEST(test_P3_J2);
+	RUN_TEST(test_P2_J2);
+	RUN_TEST(test_P1_J2);
 
 	test_print_result();
 }
