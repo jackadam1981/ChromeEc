059ae1af2de6d635a404a028ec5d50c3458620ac
Craig Hesling <hesling@chromium.org>
2019-10-17 17:28:41 -0700

    mem_commands: Gate md and rw commands on system_is_locked
    BRANCH=nocturne
    BRANCH=hatch
    BUG=b:142559996, b:142505927
    TEST=none yet
    
    Change-Id: I1202a118b836ff2a2acc986f6f3b0f6e07b36894
    Signed-off-by: Craig Hesling <hesling@chromium.org>

--- common/memory_commands.c
+++ common/memory_commands.c
@@ -6,6 +6,7 @@
 /* System module for Chrome EC */
 
 #include "console.h"
+#include "system.h"
 #include "timer.h"
 #include "util.h"
 #include "watchdog.h"
@@ -61,6 +62,9 @@ static int command_mem_dump(int argc, char **argv)
 	uint32_t address, i, num = 1;
 	char *e;
 	enum format fmt = FMT_WORD;
+
+	if (system_is_locked())
+		return EC_ERROR_ACCESS_DENIED;
 
 	if (argc > 1) {
 		if ((argv[1][0] == '.') && (strlen(argv[1]) == 2)) {
@@ -119,6 +123,9 @@ static int command_read_word(int argc, char **argv)
 	unsigned access_size = 4;
 	unsigned argc_offs = 0;
 	char *e;
+
+	if (system_is_locked())
+		return EC_ERROR_ACCESS_DENIED;
 
 	if (argc < 2)
 		return EC_ERROR_PARAM_COUNT;
