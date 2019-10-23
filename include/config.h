28c45e0ee755b0206e42e0a43816df574cedd0e7
Shecky Lin <sheckylin@chromium.org>
2019-10-23 15:41:48 +0800

    WIP: i2c_hid: Add I2C HID touchpad implementation
    This adds an I2C HID touchpad implementation that can be used for
    interfacing with the host as a HID touchpad.
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: Icab4b732d3f0beabf2593abe608d5d93a83e1819

--- include/config.h
+++ include/config.h
@@ -2315,6 +2315,9 @@
  */
 #undef CONFIG_ITE_FLASH_SUPPORT
 
+/* Support I2C HID touchpad interface. */
+#undef CONFIG_I2C_HID_TOUCHPAD
+
 /*****************************************************************************/
 /* IPI configuration.  Support mt_scp only for now. */
 
