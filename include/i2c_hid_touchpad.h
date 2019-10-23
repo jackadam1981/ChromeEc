60afa8d5db374bf96a8b4726a07a1442b558afa8
Shecky Lin <sheckylin@chromium.org>
2019-10-23 15:41:48 +0800

    WIP: i2c_hid: Add I2C HID touchpad implementation
    This adds an I2C HID touchpad implementation that can be used for
    interfacing with the host as a HID touchpad.
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: Icab4b732d3f0beabf2593abe608d5d93a83e1819

---
+++ include/i2c_hid_touchpad.h
@@ -0,0 +1,68 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Implementation of I2C HID for touchpads */
+#ifndef __CROS_EC_I2C_HID_TOUCHPAD_H
+#define __CROS_EC_I2C_HID_TOUCHPAD_H
+
+#include "common.h"
+#include "i2c_hid.h"
+#include "stdbool.h"
+#include "stdint.h"
+
+/* Max fingers to support */
+#define I2C_HID_TOUCHPAD_MAX_FINGERS	5
+
+/* Struct holding a touchpad event
+ *
+ * The user should parse the original touchpad report, apply necessary
+ * transformations and fill the result in this common struct. The touchpad is
+ * assumed to implement the Linux HID MT-B protocol.
+ */
+struct touchpad_event {
+	bool hover;			/* If hover is detected */
+	uint8_t button;			/* If button is clicked */
+	struct {
+		uint16_t x;		/* X & Y of the contact */
+		uint16_t y;
+		uint16_t pressure;	/* Pressure/contact area */
+		uint16_t width;		/* W & H of the contact */
+		uint16_t height;
+		uint16_t orientation;	/* Orientation of the contact ellipse */
+		bool is_palm;		/* If the touchpad believes it is a palm
+					 */
+		bool valid;		/* If this slot contains valid contact
+					 */
+	} __packed finger[I2C_HID_TOUCHPAD_MAX_FINGERS];
+} __packed;
+
+/* Initialize the I2C HID touchpad */
+void i2c_hid_touchpad_init(void);
+/**
+ * Process an I2C-HID command from host.
+ *
+ * @param len		>= 0 - Input data length in bytes
+ * @param buffer	Shared input/output buffer
+ * @param send_response	Function to send the response to host
+ * @param data		Extracted request content if there is any.
+ *
+ * @return the HID command identified.	< 0 - error/unsupported
+ *					LSB - I2C HID command as defined in
+ *					      include/i2c-hid.h
+ *					OBs - I2C HID register as defined in
+ *					      common/i2c_hid_touchpad.c
+ */
+int i2c_hid_touchpad_process(int len, uint8_t *buffer,
+			     void (*send_response)(int len), uint8_t *data);
+/**
+ * Compile an (outgoing) HID input report for an (incoming) touchpad event
+ *
+ * The compiled report would be sent next time when the host requests one.
+ *
+ * @param touchpad_event	Touchpad event data
+ */
+void i2c_hid_compile_report(struct touchpad_event *event);
+
+#endif /* __CROS_EC_I2C_HID_TOUCHPAD_H */
