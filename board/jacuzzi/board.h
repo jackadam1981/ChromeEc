e96019274994f1936b64e58493d8314140c68b57
Ting Shen <phoenixshen@google.com>
2019-10-23 20:02:29 +0800

    kodama/jacuzzi: enable i2c bitbang
    BUG=b:138161741,b:138415463
    TEST=see CL:1765110
    BRANCH=kukui
    
    Change-Id: I54aa0b5c37cd3fde497b552ea553e5542e594546
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- board/jacuzzi/board.h
+++ board/jacuzzi/board.h
@@ -30,6 +30,13 @@
 #undef CONFIG_EXTPOWER_DEBOUNCE_MS
 #define CONFIG_EXTPOWER_DEBOUNCE_MS 200
 
+#define CONFIG_I2C_BITBANG
+#undef I2C_BITBANG_PORT_COUNT
+#define I2C_BITBANG_PORT_COUNT 1
+#undef CONFIG_I2C_NACK_RETRY_COUNT
+#define CONFIG_I2C_NACK_RETRY_COUNT 3
+#define CONFIG_SMBUS_PEC
+
 #define CONFIG_USB_PD_TCPM_FUSB302
 #define CONFIG_USB_PD_DISCHARGE_GPIO
 #define CONFIG_USB_PD_VBUS_MEASURE_NOT_PRESENT
@@ -61,9 +68,9 @@
 #define I2C_PORT_BC12               0
 #define I2C_PORT_TCPC0              0
 #define I2C_PORT_USB_MUX            0
-#define I2C_PORT_BATTERY            1
 #define I2C_PORT_CHARGER            board_get_charger_i2c()
 #define I2C_PORT_IO_EXPANDER_IT8801 1
+#define I2C_PORT_BATTERY            board_get_battery_i2c()
 #define I2C_PORT_VIRTUAL_BATTERY    I2C_PORT_BATTERY
 
 /* Enable Accel over SPI */
@@ -139,8 +146,9 @@ void board_reset_pd_mcu(void);
 int board_get_version(void);
 int board_is_sourcing_vbus(int port);
 
-/* returns the i2c port number of charger */
+/* returns the i2c port number of charger/battery */
 int board_get_charger_i2c(void);
+int board_get_battery_i2c(void);
 
 #endif /* !__ASSEMBLER__ */
 
