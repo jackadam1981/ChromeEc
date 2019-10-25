fa25f54979f067f0827c6b3d3dd0dea969377cfe
Yuval Peress <peress@chromium.org>
2019-10-25 12:45:21 -0600

    common: queue: Add ability to iterate over queue
    This is done via:
    * queue_begin(q) gets an iterator to the start of the list
    * queue_next(q, &it) updates the iterator to the next element
    ** Once the iterator reaches the end, ptr will be NULL.
    ** If the queue was modified between the _begin and _next calls
       queue_next will terminate the iterator (set the ptr to NULL).
    
    BUG=None
    TEST=New unit tests
    BRANCH=None
    
    Change-Id: I643f544ed91bafac8e8b4c85545d4070f2d82610
    Signed-off-by: Yuval Peress <peress@chromium.org>

--- common/queue.c
+++ common/queue.c
@@ -248,3 +248,44 @@ size_t queue_peek_memcpy(struct queue const *q,
 
 	return transfer;
 }
+
+void queue_begin(struct queue const *q, struct queue_iterator *it)
+{
+	if (queue_is_empty(q))
+		it->ptr = NULL;
+	else
+		it->ptr = q->buffer + (q->state->head & q->buffer_units_mask) *
+			q->unit_bytes;
+	it->_state.offset = 0;
+	it->_state.head = q->state->head;
+	it->_state.tail = q->state->tail;
+}
+
+void queue_next(struct queue const *q, struct queue_iterator *it)
+{
+	uint8_t *ptr = (uint8_t *)it->ptr;
+
+	/* Check if anything changed since the iterator was created. */
+	if (it->_state.head != q->state->head ||
+	    it->_state.tail != q->state->tail) {
+		it->ptr = NULL;
+		return;
+	}
+
+	/* Check if iterator is already at end. */
+	if (ptr == NULL ||
+	    it->_state.head + it->_state.offset == it->_state.tail)
+		return;
+
+	it->_state.offset++;
+	/* Check if we've reached the end. */
+	if (it->_state.head + it->_state.offset == it->_state.tail) {
+		it->ptr = NULL;
+		return;
+	}
+
+	ptr = q->buffer +
+	      (((q->state->head + it->_state.offset) & q->buffer_units_mask) *
+	       q->unit_bytes);
+	it->ptr = (void *)ptr;
+}
