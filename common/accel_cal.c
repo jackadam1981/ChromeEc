9b67ad7e1de3ad48dc64ff74ad4e3844c23f4c36
Yuval Peress <peress@chromium.org>
2019-10-29 15:02:37 -0600

    common: Integrate online calibration
    WIP do not merge
    
    TEST=None
    BRANCH=None
    BUG=b:138303429
    
    Change-Id: Ib1fe3d2234dc2436e2bbfd4febd22196e5cdafef
    Signed-off-by: Yuval Peress <peress@chromium.org>

--- common/accel_cal.c
+++ common/accel_cal.c
@@ -4,22 +4,22 @@
  */
 
 #include "common.h"
+#include "console.h"
 #include "accel_cal.h"
 
 #ifndef CONFIG_FPU
 #error "CONFIG_FPU required for accelerometer calibration"
 #endif
 
-#define MSS_TO_G(v) (v * 0.101936799f)
-#define G_TO_MSS(v) (v * 9.81f)
+#define CPRINTS(format, args...) cprints(CC_MOTION_SENSE, format, ## args)
 
 static void accel_cal_reset(struct accel_cal *cal)
 {
 	int i;
 
 	for (i = 0; i < cal->num_temp_windows; ++i) {
-		kasa_reset(&cal->algos[i].kasa_fit);
-		newton_fit_reset(&cal->algos[i].newton_fit);
+		kasa_reset(cal->algos[i].kasa_fit);
+		newton_fit_reset(cal->algos[i].newton_fit);
 	}
 }
 
@@ -28,38 +28,36 @@ bool accel_cal_accumulate(struct accel_cal *cal, uint32_t sample_time,
 {
 	struct accel_cal_algo *algo;
 
-	x = MSS_TO_G(x);
-	y = MSS_TO_G(y);
-	z = MSS_TO_G(z);
-
 	/* Test that we're within the temperature range. */
 	if (temp >= CONFIG_ACCEL_CAL_MAX_TEMP ||
 	    temp <= CONFIG_ACCEL_CAL_MIN_TEMP)
 		return false;
 
 	/* Test that we have a still sample. */
-	if (!still_det_update(&cal->still_det, sample_time, x, y, z))
+	if (!still_det_update(cal->still_det, sample_time, x, y, z))
 		return false;
 
 	/* We have a still sample, update x, y, and z to the mean. */
-	x = cal->still_det.mean_x;
-	y = cal->still_det.mean_y;
-	z = cal->still_det.mean_z;
+	x = cal->still_det->mean_x;
+	y = cal->still_det->mean_y;
+	z = cal->still_det->mean_z;
+
+	CPRINTS("Still sample (%pf, %pf, %pf)", &x, &y, &z);
 
 	/* Compute the temp gate. */
 	algo = &cal->algos[(int) ((temp - CONFIG_ACCEL_CAL_MIN_TEMP) /
 				  (CONFIG_ACCEL_CAL_MAX_TEMP -
 				   CONFIG_ACCEL_CAL_MIN_TEMP))];
 
-	kasa_accumulate(&algo->kasa_fit, x, y, z);
-	if (newton_fit_accumulate(&algo->newton_fit, x, y, z)) {
+	kasa_accumulate(algo->kasa_fit, x, y, z);
+	if (newton_fit_accumulate(algo->newton_fit, x, y, z)) {
 		float radius;
 
-		kasa_compute(&algo->kasa_fit, cal->bias, &radius);
+		kasa_compute(algo->kasa_fit, cal->bias, &radius);
 		if (ABS(radius - 1.0f) < CONFIG_ACCEL_CAL_KASA_RADIUS_THRES)
 			goto accel_cal_accumulate_success;
 
-		newton_fit_compute(&algo->newton_fit, cal->bias, &radius);
+		newton_fit_compute(algo->newton_fit, cal->bias, &radius);
 		if (ABS(radius - 1.0f) < CONFIG_ACCEL_CAL_NEWTON_RADIUS_THRES)
 			goto accel_cal_accumulate_success;
 	}
@@ -67,9 +65,11 @@ bool accel_cal_accumulate(struct accel_cal *cal, uint32_t sample_time,
 	return false;
 
 accel_cal_accumulate_success:
-	cal->bias[X] = G_TO_MSS(cal->bias[X]);
-	cal->bias[Y] = G_TO_MSS(cal->bias[Y]);
-	cal->bias[Z] = G_TO_MSS(cal->bias[Z]);
+	cal->bias[X] = cal->bias[X];
+	cal->bias[Y] = cal->bias[Y];
+	cal->bias[Z] = cal->bias[Z];
+
+	accel_cal_reset(cal);
 
 	return true;
 }
--- common/newton_fit.c
+++ common/newton_fit.c
@@ -4,16 +4,18 @@
  */
 
 #include "common.h"
+#include "console.h"
 #include "newton_fit.h"
 #include "math.h"
 #include "math_util.h"
 #include <string.h>
-#include <stdio.h>
 
 #define FOR_ORIENTS(it, fit) \
-	for (queue_begin(&fit->orientations, &it); \
+	for (queue_begin(fit->orientations, &it); \
 	     it.ptr != NULL; \
-	     queue_next(&fit->orientations, &it))
+	     queue_next(fit->orientations, &it))
+
+#define CPRINTS(fmt, args...) cprints(CC_MOTION_SENSE, fmt, ## args)
 
 static float distance_squared(floatv3_t a, floatv3_t b)
 {
@@ -47,13 +49,18 @@ static bool is_ready_to_compute(struct newton_fit *fit, bool prune)
 	struct newton_fit_orientation *_it;
 
 	/* Not full, not ready to compute. */
-	if (!queue_is_full(&fit->orientations))
+	if (!queue_is_full(fit->orientations)) {
+		CPRINTS("queue not full (size=%d), not ready to compute",
+			(int) queue_count(fit->orientations));
 		return false;
+	}
 
 	/* Inspect all the orientations. */
 	FOR_ORIENTS(it, fit) {
 		_it = (struct newton_fit_orientation *) it.ptr;
 		/* If an orientation has too few samples, flag that. */
+		CPRINTS("    orientation %u/%u", _it->nsamples,
+			fit->min_orientation_samples);
 		if (_it->nsamples < fit->min_orientation_samples) {
 			has_min_samples = false;
 			break;
@@ -70,14 +77,14 @@ static bool is_ready_to_compute(struct newton_fit *fit, bool prune)
 	 * entry to make room for new orientations.
 	 */
 	if (prune)
-		queue_advance_head(&fit->orientations, 1);
+		queue_advance_head(fit->orientations, 1);
 
 	return false;
 }
 
 void newton_fit_reset(struct newton_fit *fit)
 {
-	queue_init(&fit->orientations);
+	queue_init(fit->orientations);
 }
 
 bool newton_fit_accumulate(struct newton_fit *fit, float x, float y, float z)
@@ -110,12 +117,12 @@ bool newton_fit_accumulate(struct newton_fit *fit, float x, float y, float z)
 	}
 
 	/* If queue isn't full. */
-	if (!queue_is_full(&fit->orientations)) {
+	if (!queue_is_full(fit->orientations)) {
 		struct newton_fit_orientation entry;
 
 		entry.nsamples = 1;
 		fpv3_init(entry.orientation, x, y, z);
-		queue_add_unit(&fit->orientations, &entry);
+		queue_add_unit(fit->orientations, &entry);
 
 		return is_ready_to_compute(fit, false);
 	}
@@ -132,10 +139,10 @@ void newton_fit_compute(struct newton_fit *fit, floatv3_t bias, float *radius)
 	uint32_t iteration = 0;
 	float inv_orient_count;
 
-	if (queue_is_empty(&fit->orientations))
+	if (queue_is_empty(fit->orientations))
 		return;
 
-	inv_orient_count = 1.0f / queue_count(&fit->orientations);
+	inv_orient_count = 1.0f / queue_count(fit->orientations);
 
 	memcpy(new_bias, bias, sizeof(floatv3_t));
 	new_error = compute_error(fit, new_bias);
