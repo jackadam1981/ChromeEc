707129dc942bbd40f298f15262f51510e1f6973b
Caveh Jalali <caveh@chromium.org>
2019-10-29 18:01:15 -0700

    usb_pd: use enum tcpc_rp_value instead of int
    this changes the declaration and definitions of
    typec_set_source_current_limit() to take an enum tcpc_rp_value instead
    of int.
    
    BRANCH=none
    BUG=none
    TEST=buildall passes
    
    Change-Id: If633641a581eeb6085b94bc727e23fb57f7cd435
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1889117
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    (cherry picked from commit 85ae91a99c04bc77b9c4fd3159e04fa4e9d0af16)

--- board/puff/usb_pd_policy.c
+++ board/puff/usb_pd_policy.c
@@ -1,9 +1,11 @@
-/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+<<<<<<< HEAD   (2740e0 main: Initialize I2C pretty early)
+=======
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-/* Shared USB-C policy for octopus boards */
+/* Shared USB-C policy for Puff boards */
 
 #include "charge_manager.h"
 #include "common.h"
@@ -12,6 +14,8 @@
 #include "ec_commands.h"
 #include "gpio.h"
 #include "system.h"
+#include "tcpci.h"
+#include "tcpm.h"
 #include "usb_mux.h"
 #include "usb_pd.h"
 #include "usbc_ppc.h"
@@ -54,23 +58,19 @@ void pd_check_dr_role(int port, int dr_role, int flags)
 {
 	/* If UFP, try to switch to DFP */
 	if ((flags & PD_FLAGS_PARTNER_DR_DATA) &&
-			dr_role == PD_ROLE_UFP)
+			dr_role == PD_ROLE_UFP &&
+			system_get_image_copy() != SYSTEM_IMAGE_RO)
 		pd_request_data_swap(port);
 }
 
 int pd_check_power_swap(int port)
 {
 	/*
-	 * Allow power swap if we are acting as a dual role device.  If we are
-	 * not acting as dual role (ex. suspended), then only allow power swap
-	 * if we are sourcing when we could be sinking.
+	 * Allow power swap as long as we are acting as a dual role device,
+	 * otherwise assume our role is fixed (not in S0 or console command
+	 * to fix our role).
 	 */
-	if (pd_get_dual_role(port) == PD_DRP_TOGGLE_ON)
-		return 1;
-	else if (pd_get_role(port) == PD_ROLE_SOURCE)
-		return 1;
-
-	return 0;
+	return pd_get_dual_role(port) == PD_DRP_TOGGLE_ON ? 1 : 0;
 }
 
 void pd_check_pr_role(int port, int pr_role, int flags)
@@ -97,12 +97,12 @@ void pd_check_pr_role(int port, int pr_role, int flags)
 int pd_check_vconn_swap(int port)
 {
 	/* Only allow vconn swap if pp5000_A rail is enabled */
-	return gpio_get_level(GPIO_EN_PP5000);
+	return gpio_get_level(GPIO_EN_PP5000_A);
 }
 
 void pd_execute_data_swap(int port, int data_role)
 {
-	/* Do nothing - functionality moved to the AP for octopus */
+
 }
 
 int pd_is_valid_input_voltage(int mv)
@@ -112,50 +112,10 @@ int pd_is_valid_input_voltage(int mv)
 
 void pd_power_supply_reset(int port)
 {
-	int prev_en;
-
-	prev_en = ppc_is_sourcing_vbus(port);
-
-	/* Disable VBUS. */
-	ppc_vbus_source_enable(port, 0);
-
-	/* Enable discharge if we were previously sourcing 5V */
-	if (prev_en)
-		pd_set_vbus_discharge(port, 1);
-
-#ifdef CONFIG_USB_PD_MAX_SINGLE_SOURCE_CURRENT
-	/* Give back the current quota we are no longer using */
-	charge_manager_source_port(port, 0);
-#endif /* defined(CONFIG_USB_PD_MAX_SINGLE_SOURCE_CURRENT) */
-
-	/* Notify host of power info change. */
-	pd_send_host_event(PD_EVENT_POWER_CHANGE);
 }
 
 int pd_set_power_supply_ready(int port)
 {
-	int rv;
-
-	/* Disable charging. */
-	rv = ppc_vbus_sink_enable(port, 0);
-	if (rv)
-		return rv;
-
-	pd_set_vbus_discharge(port, 0);
-
-	/* Provide Vbus. */
-	rv = ppc_vbus_source_enable(port, 1);
-	if (rv)
-		return rv;
-
-#ifdef CONFIG_USB_PD_MAX_SINGLE_SOURCE_CURRENT
-	/* Ensure we advertise the proper available current quota */
-	charge_manager_source_port(port, 1);
-#endif /* defined(CONFIG_USB_PD_MAX_SINGLE_SOURCE_CURRENT) */
-
-	/* Notify host of power info change. */
-	pd_send_host_event(PD_EVENT_POWER_CHANGE);
-
 	return EC_SUCCESS;
 }
 
@@ -167,7 +127,7 @@ void pd_transition_voltage(int idx)
 #ifdef CONFIG_USB_PD_VBUS_DETECT_PPC
 int pd_snk_is_vbus_provided(int port)
 {
-	return ppc_is_vbus_present(port);
+	return 0;
 }
 #endif
 
@@ -178,7 +138,7 @@ void typec_set_source_current_limit(int port, enum tcpc_rp_value rp)
 
 int board_vbus_source_enabled(int port)
 {
-	return ppc_is_sourcing_vbus(port);
+	return 0;
 }
 
 
@@ -323,6 +283,10 @@ static void svdm_dp_post_config(int port)
 	if (!(dp_flags[port] & DP_FLAGS_HPD_HI_PENDING))
 		return;
 	mux->hpd_update(port, 1, 0);
+#ifdef USB_PD_PORT_TCPC_MST
+	if (port == USB_PD_PORT_TCPC_MST)
+		baseboard_mst_enable_control(port, 1);
+#endif
 }
 
 static int svdm_dp_attention(int port, uint32_t *payload)
@@ -338,6 +302,10 @@ static int svdm_dp_attention(int port, uint32_t *payload)
 		return 1;
 	}
 	mux->hpd_update(port, lvl, irq);
+#ifdef USB_PD_PORT_TCPC_MST
+	if (port == USB_PD_PORT_TCPC_MST)
+		baseboard_mst_enable_control(port, lvl);
+#endif
 
 	/* ack */
 	return 1;
@@ -349,6 +317,10 @@ static void svdm_exit_dp_mode(int port)
 
 	svdm_safe_dp_mode(port);
 	mux->hpd_update(port, 0, 0);
+#ifdef USB_PD_PORT_TCPC_MST
+	if (port == USB_PD_PORT_TCPC_MST)
+		baseboard_mst_enable_control(port, 0);
+#endif
 }
 
 static int svdm_enter_gfu_mode(int port, uint32_t mode_caps)
@@ -402,3 +374,4 @@ const struct svdm_amode_fx supported_modes[] = {
 };
 const int supported_modes_cnt = ARRAY_SIZE(supported_modes);
 #endif /* CONFIG_USB_PD_ALT_MODE_DFP */
+>>>>>>> CHANGE (85ae91 usb_pd: use enum tcpc_rp_value instead of int)
