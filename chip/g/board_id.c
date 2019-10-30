b1763daa455e9b6635567eae1bac4c6fe5d0e55e
Mary Ruthven <mruthven@chromium.org>
2019-10-30 16:19:57 -0700

    board_id: ignore erased bid type when checking headers
    We will be able to set the board id flags without setting the type. If
    only flags are set, then check the flags. If the RLZ is set, also check
    the type.
    
    BUG=b:143649068
    BRANCH=cr50
    TEST=set flags to 0x3f80. Try to update to a ZZAF:0:0:0 image. Make
    sure it isn't rejected with RLZ mismatch. Try to update to a prepvt
    image. Make sure it's rejected.
    
    Change-Id: Ie0efdd7b1b6d76f385688f75c0765c08cab3755c
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- chip/g/board_id.c
+++ chip/g/board_id.c
@@ -14,6 +14,8 @@
 #define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ## args)
 
+#define BLANK_FIELD 0xffffffff
+
 /**
  * Return the image header for the current image copy
  */
@@ -23,9 +25,19 @@ const struct SignedHeader *get_current_image_header(void)
 			get_program_memory_addr(system_get_image_copy());
 }
 
+static int board_id_type_is_blank(const struct board_id *id)
+{
+	return (id->type & id->type_inv) == BLANK_FIELD;
+}
+
+static int board_id_flags_are_blank(const struct board_id *id)
+{
+	return id->flags == BLANK_FIELD;
+}
+
 int board_id_is_blank(const struct board_id *id)
 {
-	return ~(id->type & id->type_inv & id->flags) == 0;
+	return board_id_type_is_blank(id) && board_id_flags_are_blank(id);
 }
 
 uint32_t check_board_id_vs_header(const struct board_id *id,
@@ -40,10 +52,19 @@ uint32_t check_board_id_vs_header(const struct board_id *id,
 	if (board_id_is_blank(id))
 		return 0;
 
+	/*
+	 * All 1-bits in header Board ID flags must be present in flags from
+	 * flash
+	 */
+	header_board_id_flags = SIGNED_HEADER_PADDING ^ h->board_id_flags;
+	mismatch = ((header_board_id_flags & id->flags) !=
+		    header_board_id_flags);
+
+	if (mismatch || board_id_type_is_blank(id))
+		return mismatch;
+
 	header_board_id_type = SIGNED_HEADER_PADDING ^ h->board_id_type;
 	header_board_id_mask = SIGNED_HEADER_PADDING ^ h->board_id_type_mask;
-	header_board_id_flags = SIGNED_HEADER_PADDING ^ h->board_id_flags;
-
 	/*
 	 * Masked bits in header Board ID type must match type and inverse from
 	 * flash.
@@ -51,14 +72,6 @@ uint32_t check_board_id_vs_header(const struct board_id *id,
 	mismatch = header_board_id_type ^ id->type;
 	mismatch |= header_board_id_type ^ ~id->type_inv;
 	mismatch &= header_board_id_mask;
-
-	/*
-	 * All 1-bits in header Board ID flags must be present in flags from
-	 * flash
-	 */
-	mismatch |=
-		((header_board_id_flags & id->flags) != header_board_id_flags);
-
 	return mismatch;
 }
 
