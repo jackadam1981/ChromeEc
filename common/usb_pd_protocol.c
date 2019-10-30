07eee15113507eca69d1dd80e7bf6fe743dfba91
Yilun Lin <yllin@chromium.org>
2019-10-30 16:50:11 +0800

    WIP usb_pd: Choose preferred voltage PDO.
    TEST=todo
    BUG=b:141170279 b:141903096
    BRANCH=kukui
    
    Change-Id: I94646cb1c74c10cf8db889f19c3df6339844042c
    Signed-off-by: Yilun Lin <yllin@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -1535,6 +1535,26 @@ static int pd_send_request_msg(int port, int always_send_request)
 	set_state(port, PD_STATE_SNK_REQUESTED);
 	return EC_SUCCESS;
 }
+
+static int command_pdreq(int argc, char ** argv)
+{
+	char *e;
+	int port;
+
+	if (argc != 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	/* First argument is sensor id. */
+	port = strtoi(argv[1], &e, 0);
+	if (*e || port < 0 || port >= CONFIG_USB_PD_PORT_COUNT)
+		return EC_ERROR_PARAM1;
+
+	pd_set_new_power_request(port);
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(pdreq, command_pdreq,
+			"[port]", "Send PD request");
+
 #endif
 
 static void pd_update_pdo_flags(int port, uint32_t pdo)
