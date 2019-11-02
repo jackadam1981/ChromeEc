aeeacdc7655e61899ff06305a4851d7a27d68526
Keith Short <keithshort@chromium.org>
2019-11-02 10:22:30 -0600

    volteer: add initial battery support
    Add ODM specified 62 Wh battery.
    
    BUG=b:143477210
    BRANCH=none
    TEST=make buildall
    
    Change-Id: I7c3292bbd23405781207366981c2af03b6d4624a
    Signed-off-by: Keith Short <keithshort@chromium.org>

---
+++ baseboard/volteer/battery_presence.c
@@ -0,0 +1,60 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Common battery presence checking for Volteer family.
+ * Each board should implement board_battery_info[] to define the specific
+ * battery packs supported.
+ */
+#include "battery.h"
+#include "battery_smart.h"
+#include "gpio.h"
+
+static enum battery_present batt_pres_prev = BP_NOT_SURE;
+
+enum battery_present battery_hw_present(void)
+{
+	/* The GPIO is low when the battery is physically present */
+	return gpio_get_level(GPIO_EC_BATT_PRES_ODL) ? BP_NO : BP_YES;
+}
+
+static int battery_init(void)
+{
+	int batt_status;
+
+	return battery_status(&batt_status) ? 0 :
+		!!(batt_status & STATUS_INITIALIZED);
+}
+
+/*
+ * Physical detection of battery.
+ */
+enum battery_present battery_is_present(void)
+{
+	enum battery_present batt_pres;
+
+	/* Get the physical hardware status */
+	batt_pres = battery_hw_present();
+
+	/*
+	 * Make sure battery status is implemented, I2C transactions are
+	 * success & the battery status is Initialized to find out if it
+	 * is a working battery and it is not in the cut-off mode.
+	 *
+	 * If battery I2C fails but VBATT is high, battery is booting from
+	 * cut-off mode.
+	 *
+	 * FETs are turned off after Power Shutdown time.
+	 * The device will wake up when a voltage is applied to PACK.
+	 * Battery status will be inactive until it is initialized.
+	 */
+	if (batt_pres == BP_YES && batt_pres_prev != batt_pres &&
+	    !battery_is_cut_off() && !battery_init()) {
+		batt_pres = BP_NO;
+	}
+
+	batt_pres_prev = batt_pres;
+
+	return batt_pres;
+}
+
