41685b862bfcb18d77547805840368aa104e97ce
Yilun Lin <yllin@chromium.org>
2019-11-06 17:10:04 +0800

    baseboard/kukui: dynamic adjuct input current by thermal budget
    Limit the charger input current if we are overheat
    (junction temp > 80 celsius degree), and loose the limit
    if the thermal in control.
    
    kukui/krane/kodama are suffering from thermal issue (>100 celsius
    degree in charger junction) and the solution was limit the system
    can only sink 5V/2A in S0.  However, this cause slowing charging
    as well.  We'd like to loose the 5V/2A charging limit and controls
    the thermal by restricting the charger input current.
    
    This CL only applies the limitation when PD voltage > 5V, because
    we don't see the overheat problem when krane charged with 5V.
    
    TEST=ensure the temperature of junction is always under 84 celsius
         degree; ensure the curve of battery current at charging is
         a smooth curve when PD voltage switching from high to low.
    BUG=b:141903096
    BRANCH=kukui
    
    Change-Id: I9657f5a04897010d7018f910cb77b210147d8356
    Signed-off-by: Yilun Lin <yllin@chromium.org>

--- baseboard/kukui/charger_mt6370.c
+++ baseboard/kukui/charger_mt6370.c
@@ -4,18 +4,27 @@
  */
 
 #include "charge_manager.h"
+#include "charge_state_v2.h"
 #include "charger_mt6370.h"
 #include "console.h"
-#include "driver/tcpm/mt6370.h"
 #include "driver/charger/rt946x.h"
+#include "driver/tcpm/mt6370.h"
 #include "hooks.h"
 #include "power.h"
+#include "timer.h"
 #include "usb_pd.h"
 #include "util.h"
 
 #define BAT_LEVEL_PD_LIMIT 85
+#define JC_TEMP_TARGET 80
+#define JC_TEMP_ERR 4
 
 #define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
+
+static timestamp_t thermal_wait_until;
+static int throttled_ma = PD_MAX_CURRENT_MA;
+static int prev_charge_ma;
+static int prev_charge_mv;
 
 #ifndef CONFIG_BATTERY_SMART
 int board_cut_off_battery(void)
@@ -28,6 +37,87 @@ int board_cut_off_battery(void)
 	return EC_SUCCESS;
 }
 #endif
+
+static void board_set_charge_limit_throttle(int charge_ma, int charge_mv)
+{
+	charge_set_input_current_limit(
+		MIN(throttled_ma, MAX(charge_ma, CONFIG_CHARGER_INPUT_CURRENT)),
+		charge_mv);
+}
+
+static void battery_thermal_control(struct charge_state_data *curr)
+{
+	timestamp_t now;
+	int input_current, jc_temp;
+	static int skip_reset;
+	const int k_p = 50;
+
+	if (charge_manager_get_charger_voltage() == 5000 ||
+	    curr->state != ST_CHARGE) {
+		/* not poking charger that aggressive */
+		if (skip_reset)
+			return;
+		skip_reset = 1;
+		thermal_wait_until.val = 0;
+		throttled_ma = PD_MAX_CURRENT_MA;
+		board_set_charge_limit_throttle(prev_charge_ma, prev_charge_mv);
+		return;
+	}
+
+	skip_reset = 0;
+
+	now = get_time();
+	if (thermal_wait_until.val == 0)
+		thermal_wait_until.val = now.val + (3 * SECOND);
+
+	if (now.val < thermal_wait_until.val)
+		return;
+
+	/* If we fail to read adc, skip for this cycle. */
+	if (rt946x_get_adc(MT6370_ADC_TEMP_JC, &jc_temp))
+		return;
+
+	/* If we fail to read input curr limit, skip for this cycle. */
+	if (charger_get_input_current(&input_current))
+		return;
+
+	/*
+	 * If input current limit is maximum, and we are under thermal budget,
+	 * just skip.
+	 */
+	if (input_current == PD_MAX_CURRENT_MA &&
+	    jc_temp < JC_TEMP_TARGET + JC_TEMP_ERR)
+		return;
+
+	/* If the temp is within +- JC_TEMP_ERR, thermal is under control */
+	if (jc_temp < JC_TEMP_TARGET + JC_TEMP_ERR &&
+	    jc_temp > JC_TEMP_TARGET - JC_TEMP_ERR)
+		return;
+
+	/*
+	 * PID algorithm (https://en.wikipedia.org/wiki/PID_controller),
+	 * and operates on only P value.
+	 */
+	throttled_ma = MIN(PD_MAX_CURRENT_MA,
+			   input_current + k_p * (JC_TEMP_TARGET - jc_temp));
+	board_set_charge_limit_throttle(throttled_ma, prev_charge_mv);
+
+	thermal_wait_until.val = now.val + (3 * SECOND);
+}
+
+int command_jc(int argc, char **argv)
+{
+	static int prev_jc_temp;
+	int jc_temp;
+
+	if (rt946x_get_adc(MT6370_ADC_TEMP_JC, &jc_temp))
+		jc_temp = prev_jc_temp;
+
+	ccprintf("JC Temp: %d\n", jc_temp);
+	prev_jc_temp = jc_temp;
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(jc, command_jc, "", "mt6370 junction temp");
 
 /*
  * b/143318064: A workwround for mt6370 bad bulking efficiency.
@@ -84,6 +174,13 @@ static void battery_desired_curr_dynamic(struct charge_state_data *curr)
 		charge_reset_stable_current(delta_current * SECOND);
 		/* Rewrite the stable current to re-evalute desired watt */
 		charge_set_stable_current(prev_stable_current);
+
+		/*
+		 * do not alter current by thermal if we just raising PD
+		 * voltage
+		 */
+		thermal_wait_until.val = get_time().val + CHARGE_STABLE_WAIT_US;
+
 	} else {
 		pd_set_prefer_voltage(5000);
 	}
@@ -99,6 +196,8 @@ void mt6370_charger_profile_override(struct charge_state_data *curr)
 	int chg_limit_mv = pd_get_max_voltage();
 
 	battery_desired_curr_dynamic(curr);
+
+	battery_thermal_control(curr);
 
 	/* Limit input (=VBUS) to 5V when soc > 85% and charge current < 1A. */
 	if (!(curr->batt.flags & BATT_FLAG_BAD_CURRENT) &&
@@ -147,6 +246,7 @@ DECLARE_HOOK(HOOK_BATTERY_SOC_CHANGE,
 void board_set_charge_limit(int port, int supplier, int charge_ma,
 			    int max_ma, int charge_mv)
 {
-	charge_set_input_current_limit(
-		MAX(charge_ma, CONFIG_CHARGER_INPUT_CURRENT), charge_mv);
+	prev_charge_ma = charge_ma;
+	prev_charge_mv = charge_mv;
+	board_set_charge_limit_throttle(charge_ma, charge_mv);
 }
