816a3492774abcd825a267c27ff636f38c2f62f2
Andrew McRae <amcrae@google.com>
2019-11-08 15:47:56 +1100

    ec: Add driver for TI INA3221 voltage sensors.
    Add driver for TI INA3221 voltage sensors.
    Puff has several of these devices, and the EC has access to them.
    
    BRANCH=none
    BUG=b:144132145
    TEST=EC buildall, tests
    
    Change-Id: I37efd6ce7f154339f002c633e5daf6a18fef05aa
    Signed-off-by: Andrew McRae <amcrae@google.com>

--- include/config.h
+++ include/config.h
@@ -2311,11 +2311,13 @@
 /* Current/Power monitor */
 
 /*
- * Compile driver for INA219 or INA231. These two flags may not be both
- * defined.
+ * Compile driver for INA219 or INA231 or INA3221.
+ * Only one of these may be defined (if any).
  */
 #undef CONFIG_INA219
 #undef CONFIG_INA231
+#undef CONFIG_INA3221
+
 
 /*****************************************************************************/
 /* Inductive charging */
