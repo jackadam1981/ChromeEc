77a4b149df11c704b4488095023da52cebe65682
tim <tim2.lin@ite.corp-partner.google.com>
2019-11-11 16:12:55 +0800

    TEST-ONLY: board of elm_it83202
    BUG=none
    BRANCH=none
    TEST=none
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>
    
    Change-Id: I6a61aa3943405ca03182386835119834dd42863f

--- chip/it83xx/i2c.c
+++ chip/it83xx/i2c.c
@@ -240,7 +240,9 @@ static void i2c_reset(int p, int cause)
 		/* State reset and hardware reset */
 		IT83XX_I2C_CTR(p_ch) = E_STS_AND_HW_RST;
 	}
+#if 0
 	CPRINTS("I2C ch%d reset cause %d", p, cause);
+#endif
 }
 
 static void i2c_r_last_byte(int p)
