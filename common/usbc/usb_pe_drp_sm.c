d76a556cffdcc58471f38d0962930d85fc0642e0
Sam Hurst <shurst@google.com>
2019-11-12 09:27:24 -0800

    TCPMv2: Don't use SinkTXOK/NG in middle of partner-initiated PD3.0 AMS
    Don't send ACCEPT message (to PR_SWAP AMS initiated
    by partner) after lowering Rp to SnkTXNG.
    
    BUG=chromium:1023064
    BRANCH=none
    TEST=make -j buildall
    	manual tests:
    	Connected several docks with charger and
    	verified PR_SWAP. Also works with the CM
    	dock that triggered this bug.
    
    Change-Id: Iab5474174cf8313c24bae8a7869565005504af25
    Signed-off-by: Sam Hurst <shurst@google.com>

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -105,6 +105,8 @@
 #define PE_FLAGS_WAITING_DR_SWAP             BIT(24)
 /* FLAG to track if port partner is dualrole capable */
 #define PE_FLAGS_PORT_PARTNER_IS_DUALROLE    BIT(25)
+/* FLAG used to prevent the start of an AMS during a Sink initiated PR_SWAP */
+#define PE_FLAGS_SNK_INITIATED_PR_SWAP       BIT(26)
 
 /* 6.7.3 Hard Reset Counter */
 #define N_HARD_RESET_COUNT 2
@@ -304,9 +306,6 @@ static struct policy_engine {
 	enum pd_power_role power_role;
 	/* current port data role (DFP or UFP) */
 	enum pd_data_role data_role;
-	/* saved data and power roles while communicating with a cable plug */
-	enum pd_data_role saved_data_role;
-	enum pd_power_role saved_power_role;
 	/* state machine flags */
 	uint32_t flags;
 	/* Device Policy Manager Request */
@@ -1438,6 +1437,7 @@ static void pe_src_ready_run(int port)
 	 *   1) The DiscoverIdentityTimer times out.
 	 */
 	if (get_time().val > pe[port].discover_port_identity_timer) {
+		PE_SET_FLAG(port, PE_FLAGS_INTERRUPTIBLE_AMS);
 		pe_start_port_discovery(port);
 		return;
 	}
@@ -1481,6 +1481,7 @@ static void pe_src_ready_run(int port)
 		} else if (PE_CHK_DPM_REQUEST(port,
 					DPM_REQUEST_DISCOVER_IDENTITY)) {
 			PE_CLR_DPM_REQUEST(port, DPM_REQUEST_DISCOVER_IDENTITY);
+			PE_SET_FLAG(port, PE_FLAGS_INTERRUPTIBLE_AMS);
 
 			pe[port].partner_type = CABLE;
 			pe[port].vdm_cmd = DISCOVER_IDENTITY;
@@ -1531,6 +1532,8 @@ static void pe_src_ready_run(int port)
 			case PD_DATA_VENDOR_DEF:
 				if (PD_HEADER_TYPE(emsg[port].header) ==
 							PD_DATA_VENDOR_DEF) {
+					PE_SET_FLAG(port,
+						PE_FLAGS_INTERRUPTIBLE_AMS);
 					if (PD_VDO_SVDM(payload)) {
 						set_state_pe(port,
 							PE_VDM_RESPONSE);
@@ -1564,6 +1567,8 @@ static void pe_src_ready_run(int port)
 			case PD_CTRL_GOTO_MIN:
 				break;
 			case PD_CTRL_PR_SWAP:
+				PE_SET_FLAG(port,
+					PE_FLAGS_SNK_INITIATED_PR_SWAP);
 				set_state_pe(port,
 					PE_PRS_SRC_SNK_EVALUATE_SWAP);
 				break;
@@ -1593,10 +1598,13 @@ static void pe_src_ready_exit(int port)
 	/*
 	 * If the Source is initiating an AMS then the Policy Engine Shall
 	 * notify the Protocol Layer that the first Message in an AMS will
-	 * follow.
+	 * follow, unless this is a Sink initiated PR_SWAP.
 	 */
-	if (!PE_CHK_FLAG(port, PE_FLAGS_INTERRUPTIBLE_AMS))
+	if (!PE_CHK_FLAG(port, PE_FLAGS_INTERRUPTIBLE_AMS) &&
+			!PE_CHK_FLAG(port, PE_FLAGS_SNK_INITIATED_PR_SWAP))
 		prl_start_ams(port);
+
+	PE_CLR_FLAG(port, PE_FLAGS_SNK_INITIATED_PR_SWAP);
 }
 
 /**
@@ -2131,6 +2139,7 @@ static void pe_snk_ready_run(int port)
 	 *   1) The PortDiscoverIdentityTimer times out.
 	 */
 	if (get_time().val > pe[port].discover_port_identity_timer) {
+		PE_SET_FLAG(port, PE_FLAGS_INTERRUPTIBLE_AMS);
 		pe_start_port_discovery(port);
 		return;
 	}
@@ -2173,6 +2182,7 @@ static void pe_snk_ready_run(int port)
 			PE_CLR_DPM_REQUEST(port,
 					   DPM_REQUEST_DISCOVER_IDENTITY);
 
+			PE_SET_FLAG(port, PE_FLAGS_INTERRUPTIBLE_AMS);
 			pe[port].partner_type = CABLE;
 			pe[port].vdm_cmd = DISCOVER_IDENTITY;
 			pe[port].vdm_data[0] = VDO(
@@ -2226,6 +2236,8 @@ static void pe_snk_ready_run(int port)
 			case PD_DATA_VENDOR_DEF:
 				if (PD_HEADER_TYPE(emsg[port].header) ==
 							PD_DATA_VENDOR_DEF) {
+					PE_SET_FLAG(port,
+						PE_FLAGS_INTERRUPTIBLE_AMS);
 					if (PD_VDO_SVDM(payload))
 						set_state_pe(port,
 							PE_VDM_RESPONSE);
@@ -2962,7 +2974,7 @@ static void pe_prs_src_snk_wait_source_on_run(int port)
 	int cnt;
 	int ext;
 
-	if (pe[port].ps_source_timer != TIMER_DISABLED &&
+	if (pe[port].ps_source_timer == TIMER_DISABLED &&
 			PE_CHK_FLAG(port, PE_FLAGS_TX_COMPLETE)) {
 		PE_CLR_FLAG(port, PE_FLAGS_TX_COMPLETE);
 
@@ -2975,7 +2987,8 @@ static void pe_prs_src_snk_wait_source_on_run(int port)
 	 * Transition to PE_SNK_Startup when:
 	 *   1) An PS_RDY Message is received.
 	 */
-	if (PE_CHK_FLAG(port, PE_FLAGS_MSG_RECEIVED)) {
+	if (pe[port].ps_source_timer != TIMER_DISABLED &&
+			PE_CHK_FLAG(port, PE_FLAGS_MSG_RECEIVED)) {
 		PE_CLR_FLAG(port, PE_FLAGS_MSG_RECEIVED);
 
 		type = PD_HEADER_TYPE(emsg[port].header);
@@ -3635,15 +3648,7 @@ static void pe_vdm_request_entry(int port)
 		emsg[port].len = pe[port].vdm_cnt * 4;
 	}
 
-	if (pe[port].partner_type) {
-		/* Save power and data roles */
-		pe[port].saved_power_role = tc_get_power_role(port);
-		pe[port].saved_data_role = tc_get_data_role(port);
-
-		prl_send_data_msg(port, TCPC_TX_SOP_PRIME, PD_DATA_VENDOR_DEF);
-	} else {
-		prl_send_data_msg(port, TCPC_TX_SOP, PD_DATA_VENDOR_DEF);
-	}
+	prl_send_data_msg(port, TCPC_TX_SOP, PD_DATA_VENDOR_DEF);
 
 	pe[port].vdm_response_timer = TIMER_DISABLED;
 }
