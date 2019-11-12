18241d11f98119745ee9b10e45de31c0de16a0fa
Sam Hurst <shurst@google.com>
2019-11-12 11:32:38 -0800

    TCPMv2: Missing equivalent `pd` command
    Add pd console command
    
    BUG=chromium:1021235
    BRANCH=none
    TEST=make -j buildall
    	Manual tests:
    
    Signed-off-by: Sam Hurst <shurst@google.com>
    Change-Id: I4ed28599af4d0c566208e51208c3a6e2bfb8bf55

--- include/usb_pe_sm.h
+++ include/usb_pe_sm.h
@@ -35,6 +35,10 @@ enum pe_dpm_request {
 	DPM_REQUEST_DISCOVER_IDENTITY   = BIT(9),
 	DPM_REQUEST_EXIT_DP_MODE        = BIT(10),
 	DPM_REQUEST_SVDM                = BIT(11),
+	DPM_REQUEST_SNK_STARTUP         = BIT(12),
+	DPM_REQUEST_SRC_STARTUP         = BIT(13),
+	DPM_REQUEST_BIST_RX             = BIT(14),
+	DPM_REQUEST_BIST_TX             = BIT(15),
 };
 
 /**
