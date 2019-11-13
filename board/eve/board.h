coreboot/chrome-ec

commit 2f2a81079191ca0d5920514ae837f80c360fec4c

Add double tap and make motion sense wake up ap
Change to use CONFIG_GESTURE_DETECTION_MASK since
CONFIG_GESTURE_SENSOR_BATTERY_TAP and CONFIG_GESTURE_SENSOR_DOUBLE_TAP
both define it.

BUG=b:135575671
BRANCH=none
TEST=build pass. EC can receive double tap interrupt.

Change-Id: I6eec40ef7405ec0653ff62dbce98f975cb19e332
Signed-off-by: Heng-Ruey Hsu <henryhsu@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1710210
Reviewed-by: Jett Rink <jettrink@chromium.org>
(cherry picked from commit d8abaaa35932d7b0e7a0aa99e52a9b81b7d74584)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1906403
Reviewed-by: Hung-Te Lin <hungte@chromium.org>
Commit-Queue: Hung-Te Lin <hungte@chromium.org>
Tested-by: Hung-Te Lin <hungte@chromium.org>
Auto-Submit: Hung-Te Lin <hungte@chromium.org>

common/motion_sense.c
@@ -727,7 +727,7 @@ static void check_and_queue_gestures(uint32_t *event)
 	/* Run gesture recognition engine */
 	gesture_calc(event);
 #endif
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_DETECTION_MASK
 	if (*event & TASK_EVENT_MOTION_ACTIVITY_INTERRUPT(
 				MOTIONSENSE_ACTIVITY_DOUBLE_TAP)) {
 #ifdef CONFIG_GESTURE_HOST_DETECTION
@@ -738,7 +738,11 @@ static void check_and_queue_gestures(uint32_t *event)
 		 * AP is ignoring double tap event, do no wake up and no
 		 * automatic disable.
 		 */
+#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
 		vector.flags = 0;
+#else
+		vector.flags = MOTIONSENSE_SENSOR_FLAG_WAKEUP;
+#endif
 		vector.activity = MOTIONSENSE_ACTIVITY_DOUBLE_TAP;
 		vector.state = 1; /* triggered */
 		vector.sensor_num = MOTION_SENSE_ACTIVITY_SENSOR_ID;

driver/accelgyro_bmi160.c
@@ -772,7 +772,7 @@ int manage_activity(const struct motion_sensor_t *s,
 		break;
 	}
 #endif
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_DETECTION_MASK
 	case MOTIONSENSE_ACTIVITY_DOUBLE_TAP: {
 		int tmp;
 		/* Set double tap interrupt */
@@ -1059,7 +1059,7 @@ static int config_interrupt(const struct motion_sensor_t *s)
 	raw_write8(s->port, s->i2c_spi_addr_flags,
 		   BMI160_CMD_REG, BMI160_CMD_INT_RESET);
 
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_DETECTION_MASK
 	raw_write8(s->port, s->i2c_spi_addr_flags,
 		   BMI160_INT_TAP_0,
 		   BMI160_TAP_DUR(s, CONFIG_GESTURE_TAP_MAX_INTERSTICE_T));
@@ -1099,7 +1099,7 @@ static int config_interrupt(const struct motion_sensor_t *s)
 #ifdef CONFIG_GESTURE_SIGMO
 	tmp |= BMI160_INT_ANYMOTION;
 #endif
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_DETECTION_MASK
 	tmp |= BMI160_INT_D_TAP;
 #endif
 #ifdef CONFIG_BMI160_ORIENTATION_SENSOR
@@ -1207,7 +1207,7 @@ static int irq_handler(struct motion_sensor_t *s, uint32_t *event)
 		if (rv)
 			return rv;
 
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_DETECTION_MASK
 		if (interrupt & BMI160_D_TAP_INT)
 			*event |= TASK_EVENT_MOTION_ACTIVITY_INTERRUPT(
 					MOTIONSENSE_ACTIVITY_DOUBLE_TAP);
@@ -1311,7 +1311,7 @@ static int init(const struct motion_sensor_t *s)
 		data->disabled_activities |=
 			1 << MOTIONSENSE_ACTIVITY_SIG_MOTION;
 #endif
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_DETECTION_MASK
 		data->disabled_activities |=
 			1 << MOTIONSENSE_ACTIVITY_DOUBLE_TAP;
 #endif

include/config.h
@@ -1912,11 +1912,15 @@
 
 /* enable gesture host interface */
 #undef CONFIG_GESTURE_HOST_DETECTION
+
 /* Sensor sampling interval for gesture recognition */
 #undef CONFIG_GESTURE_SAMPLING_INTERVAL_MS
 
 /* Which sensor to look for battery tap recognition */
 #undef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+
+/* Which sensor to look for double tap recognition */
+#undef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
 
 /*
  * Double tap detection parameters

jackadam1981/ChromeEc

commit e00ae1c2f09680764a011adbbfaf2cfb7263a09d

Clean up double tap related definition
Fixes: commit 2f2a81079191ca "Add double tap and make motion sense wake up ap"
CONFIG_GESTURE_DETECTION_MASK includes significant motion in activity
list. We cannot use it for double tap.
Add more flags to distinguish it.

BUG=b:135575671
BRANCH=kukui
TEST=AP can receive mkbp event when double tap is triggered

Change-Id: I13776a01b14dc251396a615c8c97353f2d0477d4
Signed-off-by: Heng-Ruey Hsu <henryhsu@chromium.org>

board/eve/board.h
@@ -146,12 +146,12 @@
 /* Enable double tap detection */
 #define CONFIG_GESTURE_DETECTION
 #define CONFIG_GESTURE_HOST_DETECTION
-#define CONFIG_GESTURE_SENSOR_BATTERY_TAP 1
+#define CONFIG_GESTURE_SENSOR_DOUBLE_TAP 1
 #define CONFIG_GESTURE_SAMPLING_INTERVAL_MS 5
 #define CONFIG_GESTURE_TAP_THRES_MG 100
 #define CONFIG_GESTURE_TAP_MAX_INTERSTICE_T 500
 #define CONFIG_GESTURE_DETECTION_MASK \
-	 BIT(CONFIG_GESTURE_SENSOR_BATTERY_TAP)
+	 BIT(CONFIG_GESTURE_SENSOR_DOUBLE_TAP)
 
 /* USB */
 #define CONFIG_USB_CHARGER

board/kukui/board.h
@@ -154,6 +154,7 @@ int board_discharge_on_ac(int enable);
 #define CONFIG_GESTURE_DETECTION
 #define CONFIG_GESTURE_HOST_DETECTION
 #define CONFIG_GESTURE_SENSOR_DOUBLE_TAP 0
+#define CONFIG_GESTURE_SENSOR_DOUBLE_TAP_FOR_HOST
 #define CONFIG_GESTURE_SAMPLING_INTERVAL_MS 5
 #define CONFIG_GESTURE_TAP_THRES_MG 100
 #define CONFIG_GESTURE_TAP_MAX_INTERSTICE_T 500

board/samus/board.h
@@ -213,7 +213,7 @@ void jtag_interrupt(enum gpio_signal signal);
 void set_pp5000_in_g3(int mask, int enable);
 
 /* Define for sensor tasks */
-#define CONFIG_GESTURE_SENSOR_BATTERY_TAP 0
+#define CONFIG_GESTURE_SENSOR_DOUBLE_TAP 0
 #define CONFIG_GESTURE_TAP_OUTER_WINDOW_T 200
 #define CONFIG_GESTURE_TAP_INNER_WINDOW_T 30
 #define CONFIG_GESTURE_TAP_MIN_INTERSTICE_T 120

common/gesture.c
@@ -67,7 +67,7 @@ enum tap_states {
 
 /* Tap sensor to use */
 static struct motion_sensor_t *sensor =
-&motion_sensors[CONFIG_GESTURE_SENSOR_BATTERY_TAP];
+&motion_sensors[CONFIG_GESTURE_SENSOR_DOUBLE_TAP];
 
 /* Tap state information */
 static int history_z[MAX_WINDOW];  /* Changes in Z */

common/motion_sense.c
@@ -730,7 +730,7 @@ static void check_and_queue_gestures(uint32_t *event)
 	/* Run gesture recognition engine */
 	gesture_calc(event);
 #endif
-#ifdef CONFIG_GESTURE_DETECTION_MASK
+#ifdef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
 	if (*event & TASK_EVENT_MOTION_ACTIVITY_INTERRUPT(
 				MOTIONSENSE_ACTIVITY_DOUBLE_TAP)) {
 #ifdef CONFIG_GESTURE_HOST_DETECTION
@@ -741,10 +741,10 @@ static void check_and_queue_gestures(uint32_t *event)
 		 * AP is ignoring double tap event, do no wake up and no
 		 * automatic disable.
 		 */
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
-		vector.flags = 0;
-#else
+#ifdef CONFIG_GESTURE_SENSOR_DOUBLE_TAP_FOR_HOST
 		vector.flags = MOTIONSENSE_SENSOR_FLAG_WAKEUP;
+#else
+		vector.flags = 0;
 #endif
 		vector.activity = MOTIONSENSE_ACTIVITY_DOUBLE_TAP;
 		vector.state = 1; /* triggered */

driver/accel_lis2dw12.c
@@ -150,7 +150,7 @@ static int lis2dw12_config_interrupt(const struct motion_sensor_t *s)
 		return ret;
 #endif /* CONFIG_ACCEL_FIFO */
 
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
 	/*
 	 * Configure D-TAP event detection on 3 axis.
 	 * For more details please refer to AN5038.
@@ -186,7 +186,7 @@ static int lis2dw12_config_interrupt(const struct motion_sensor_t *s)
 	ret = st_write_data_with_mask(s, LIS2DW12_INT1_TAP_ADDR,
 				      LIS2DW12_INT1_DTAP_MASK,
 				      LIS2DW12_EN_BIT);
-#endif /* CONFIG_GESTURE_SENSOR_BATTERY_TAP */
+#endif /* CONFIG_GESTURE_SENSOR_DOUBLE_TAP */
 	return ret;
 }
 
@@ -221,7 +221,7 @@ static int lis2dw12_irq_handler(struct motion_sensor_t *s, uint32_t *event)
 		return EC_ERROR_NOT_HANDLED;
 	}
 
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
 	{
 		int status = 0;
 
@@ -231,7 +231,7 @@ static int lis2dw12_irq_handler(struct motion_sensor_t *s, uint32_t *event)
 		if (status & LIS2DW12_DOUBLE_TAP)
 			*event |= CONFIG_GESTURE_TAP_EVENT;
 	}
-#endif /* CONFIG_GESTURE_SENSOR_BATTERY_TAP */
+#endif /* CONFIG_GESTURE_SENSOR_DOUBLE_TAP */
 
 #ifdef CONFIG_ACCEL_FIFO
 	{

driver/accelgyro_bmi160.c
@@ -772,7 +772,7 @@ int manage_activity(const struct motion_sensor_t *s,
 		break;
 	}
 #endif
-#ifdef CONFIG_GESTURE_DETECTION_MASK
+#ifdef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
 	case MOTIONSENSE_ACTIVITY_DOUBLE_TAP: {
 		int tmp;
 		/* Set double tap interrupt */
@@ -1059,7 +1059,7 @@ static int config_interrupt(const struct motion_sensor_t *s)
 	raw_write8(s->port, s->i2c_spi_addr_flags,
 		   BMI160_CMD_REG, BMI160_CMD_INT_RESET);
 
-#ifdef CONFIG_GESTURE_DETECTION_MASK
+#ifdef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
 	raw_write8(s->port, s->i2c_spi_addr_flags,
 		   BMI160_INT_TAP_0,
 		   BMI160_TAP_DUR(s, CONFIG_GESTURE_TAP_MAX_INTERSTICE_T));
@@ -1099,7 +1099,7 @@ static int config_interrupt(const struct motion_sensor_t *s)
 #ifdef CONFIG_GESTURE_SIGMO
 	tmp |= BMI160_INT_ANYMOTION;
 #endif
-#ifdef CONFIG_GESTURE_DETECTION_MASK
+#ifdef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
 	tmp |= BMI160_INT_D_TAP;
 #endif
 #ifdef CONFIG_BMI160_ORIENTATION_SENSOR
@@ -1207,7 +1207,7 @@ static int irq_handler(struct motion_sensor_t *s, uint32_t *event)
 		if (rv)
 			return rv;
 
-#ifdef CONFIG_GESTURE_DETECTION_MASK
+#ifdef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
 		if (interrupt & BMI160_D_TAP_INT)
 			*event |= TASK_EVENT_MOTION_ACTIVITY_INTERRUPT(
 					MOTIONSENSE_ACTIVITY_DOUBLE_TAP);
@@ -1311,7 +1311,7 @@ static int init(const struct motion_sensor_t *s)
 		data->disabled_activities |=
 			1 << MOTIONSENSE_ACTIVITY_SIG_MOTION;
 #endif
-#ifdef CONFIG_GESTURE_DETECTION_MASK
+#ifdef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
 		data->disabled_activities |=
 			1 << MOTIONSENSE_ACTIVITY_DOUBLE_TAP;
 #endif

include/config.h
@@ -1933,11 +1933,11 @@
 /* Sensor sampling interval for gesture recognition */
 #undef CONFIG_GESTURE_SAMPLING_INTERVAL_MS
 
-/* Which sensor to look for battery tap recognition */
-#undef CONFIG_GESTURE_SENSOR_BATTERY_TAP
-
 /* Which sensor to look for double tap recognition */
 #undef CONFIG_GESTURE_SENSOR_DOUBLE_TAP
+
+/* Use for waking up host */
+#undef CONFIG_GESTURE_SENSOR_DOUBLE_TAP_FOR_HOST
 
 /*
  * Double tap detection parameters