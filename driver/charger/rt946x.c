6aa8b58d97bba7b6568399cae858e5edb94b4329
Allen Chiang <allen_chiang@mediatek.corp-partner.google.com>
2019-11-15 11:49:13 +0800

    rt946x: enter shipping mode command for mt6370.
    1. In order to reduce power consumption,
       we change shipping mode command on MT6370
    
    BRANCH=kukui
    BUG=b:144511629
    TEST=boot kukui, enter shipping mode.
    
    Change-Id: I4b90a7be5e70b33f54c046c740da8e38b5a4aa2e

--- driver/charger/rt946x.c
+++ driver/charger/rt946x.c
@@ -1440,12 +1440,34 @@ int rt946x_is_charge_done(void)
 
 int rt946x_cutoff_battery(void)
 {
-	int val = RT946X_MASK_SHIP_MODE;
-
 #ifdef CONFIG_CHARGER_MT6370
-	val |= RT946X_MASK_TE | RT946X_MASK_CFO_EN | RT946X_MASK_CHG_EN;
+	int rv;
+
+	rv = rt946x_write8(MT6370_REG_RSTPASCODE1, MT6370_MASK_RSTPASCODE1);
+	if (rv)
+		return rv;
+
+	rv = rt946x_write8(MT6370_REG_RSTPASCODE2, MT6370_MASK_RSTPASCODE2);
+	if (rv)
+		return rv;
+
+	/* reset all chg/fled/ldo/rgb/bl/db reg and logic */
+	rv = rt946x_write8(RT946X_REG_CORECTRL2, 0x7F);
+	if (rv)
+		return rv;
+
+	/* disable chg auto sensing */
+	mt6370_enable_hidden_mode(1);
+	rv = rt946x_clr_bit(MT6370_REG_CHGHIDDENCTRL15,
+				MT6370_MASK_ADC_TS_AUTO);
+	if (rv)
+		return rv;
+
+	mt6370_enable_hidden_mode(0);
+	msleep(50);
 #endif
-	return rt946x_set_bit(RT946X_REG_CHGCTRL2, val);
+	/* enter shipping mode */
+	return rt946x_set_bit(RT946X_REG_CHGCTRL2, RT946X_MASK_SHIP_MODE);
 }
 
 int rt946x_enable_charge_termination(int en)
