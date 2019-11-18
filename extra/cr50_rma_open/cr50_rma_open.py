07e37a7da038f0001cc8a480b719e18c993c8c7d
Evan Green <evgreen@chromium.org>
2019-11-18 10:51:04 -0800

    cr50_rma_open: Disable UART timestamps
    The log timestamps are unexpected by the script, and cause issues.
    Send a dut-control command to disable UART timestamps.
    
    The script also doesn't seem prepared to handle a colon in the board ID.
    Remove it.
    
    BUG=None
    TEST=Try cr50_rma_unlock.py
    BRANCH=None
    
    Change-Id: Ie9e54e65b86c52aef120fa3249453bfc090cf6f0
    Signed-off-by: Evan Green <evgreen@chromium.org>

--- extra/cr50_rma_open/cr50_rma_open.py
+++ extra/cr50_rma_open/cr50_rma_open.py
@@ -165,6 +165,7 @@ class RMAOpen(object):
         self.servo_port = servo_port if servo_port else '9999'
         self.print_caps = print_caps
         self.ip = ip
+        self._dut_control('cr50_uart_timestamp:off')
         if device:
             self.set_cr50_device(device)
         elif servo_port:
@@ -581,6 +582,7 @@ class RMAOpen(object):
         """Print the cr50 BID RLZ code"""
         bid_output = self.send_cmd_get_output('bid')
         bid = re.search('Board ID: (\S+),', bid_output).group(1)
+        bid = bid.replace(':', '')
         if bid == ERASED_BID:
             debug(DEBUG_ERASED_BOARD_ID)
             raise ValueError('Cannot run RMA Open when board id is erased')
