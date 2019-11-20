3eac9366d6c12fd300ff296a76d526ad9f5d01c6
Vadim Sukhomlinov <sukhomlinov@google.com>
2019-11-20 15:43:24 -0800

    cr50: add support for FIPS 140-2 initialization
    Added initial support for FIPS 140-2 initialization, including HMAC_DRBG
    to replace rand & rand_bytes functions.
    An “Approved” RNG listed in FIPS 140-2 Annex C must be used for the
    generation of random data or cryptographic keys used by an approved
    security function. Detailed information and guidance on Key Generation
    in FIPS 140-2 can be found in NIST SP 800-133 and FIPS 140-2 IG 7.8.
    
    Currently rand() and rand_bytes() use raw TRNG data, which is not
    compliant with FIPS 140-2. With this change following features are
    implemented:
      1. introduced get_entropy() and get_entropy32() functions which
         gather enough entropy from TRNG to produce full entropy via
         condensing entropy with approved external conditioning SHA256
      2. added global HMAC DRBG which is initialized on first use with
         440 bits of entropy as required by NIST SP 800 90A rev 1
      3. implemented proper reseeding of global DRBG with 256 bits of
         full entropy.
      4. fips_rand() and fips_rand_bytes() are implemented using DRBG.
    
    BUG=b:138577416
    BRANCH=cr50
    TEST=nist_entropy.sh && tpmtest.py -t1 && tpmtest.py -t2 to make sure
    that introduced changes really produce random numbers.
    
    Change-Id: Ib7b80871bde76f091e8f8e48e473f73bd77bcdad
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -12,6 +12,8 @@
 #include "ec_version.h"
 #include "endian.h"
 #include "extension.h"
+#include "fips.h"
+#include "fips_rand.h"
 #include "flash.h"
 #include "flash_config.h"
 #include "gpio.h"
@@ -180,6 +182,17 @@ int board_tpm_mode_change_allowed(void)
 {
 	return !!(board_properties & BOARD_ALLOW_CHANGE_TPM_MODE);
 }
+
+int board_fips_power_up_done(void)
+{
+	return !!(board_properties & BOARD_FIPS_POWERUP_DONE);
+}
+
+int board_fips_simulated(void)
+{
+	return !!(board_properties & BOARD_FIPS_SIMULATED);
+}
+
 
 /* Get header address of the backup RW copy. */
 const struct SignedHeader *get_other_rw_addr(void)
