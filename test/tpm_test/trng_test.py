f1e65444fa55e459832d9b7df3b314537d632ec5
Vadim Sukhomlinov <sukhomlinov@google.com>
2019-11-20 15:43:24 -0800

    cr50: implement fips_rand_bytes() using HMAC SHA256 DRBG
    An “Approved” RNG listed in FIPS 140-2 Annex C must be used for the
    generation of random data or cryptographic keys used by an approved
    security function. Detailed information and guidance on Key Generation
    in FIPS 140-2 can be found in NIST SP 800-133 and FIPS 140-2 IG 7.8.
    
    Currently rand() and rand_bytes() use raw TRNG data, which is not
    compliant with FIPS 140-2. With this change following features are
    implemented:
      1. introduced get_entropy() and get_entropy32() functions which
         gather enough entropy from TRNG to produce full entropy via
         condensing entropy with approved external conditioning SHA256
      2. added global HMAC DRBG which is initialized on first use with
         440 bits of entropy as required by NIST SP 800 90A rev 1
      3. implemented proper reseeding of global DRBG with 256 bits of
         full entropy.
      4. fips_rand() and fips_rand_bytes() are implemented using DRBG.
    
    BUG=b:138577416
    BRANCH=cr50
    TEST=nist_entropy.sh && tpmtest.py -t1 && tpmtest.py -t2 to make sure
    that introduced changes really produce random numbers.
    
    Change-Id: Ib7b80871bde76f091e8f8e48e473f73bd77bcdad
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>

--- test/tpm_test/trng_test.py
+++ test/tpm_test/trng_test.py
@@ -9,42 +9,47 @@ import struct
 import subcmd
 import utils
 
-TRNG_TEST_FMT = '>H'
+TRNG_TEST_FMT = '>HB'
 TRNG_TEST_RSP_FMT = '>H2IH'
 TRNG_TEST_CC = 0x33
 TRNG_SAMPLE_SIZE = 1000 # minimal recommended by NIST is 1000 bytes per sample
 TRNG_SAMPLE_COUNT = 1000 # NIST require at least 1000000 of 8-bit samples
 
-def get_random_command(size):
-  return struct.pack(TRNG_TEST_FMT, size)
+def get_random_command(size, op):
+  return struct.pack(TRNG_TEST_FMT, size, op)
 
 def get_random_command_rsp(size):
   return struct.pack(TRNG_TEST_RSP_FMT, 0x8001,
                      struct.calcsize(TRNG_TEST_RSP_FMT) + size, 0, TRNG_TEST_CC)
 
 
-def trng_test(tpm):
+def trng_test(tpm, trng_mode):
   """Download entropy samples from TRNG
 
     Command structure, shared out of band with the test running on the target:
 
     field     |    size  |                  note
-    ===================================================================
+    ========================================================================
     text_len  |    2     | size of the text to process, big endian
-
+    type      |    1     | 0 = TRNG, 1 = CR50 DRBG, 2 = get_entropy
   Args:
     tpm: a tpm object used to communicate with the device
+    trng_mode: type of RNG which data will be collected
 
   Raises:
     subcmd.TpmTestError: on unexpected target responses
   """
-  with open('/tmp/trng_output', 'wb') as f:
+  if trng_mode not in [0,1,2]:
+    print('%sUnknown random source: %d' % (utils.cursor_back(), trng_mode))
+    return
+  with open('/tmp/trng_output', 'ab') as f:
     for x in range(0, TRNG_SAMPLE_COUNT):
       wrapped_response = tpm.command(tpm.wrap_ext_command(TRNG_TEST_CC,
-                                     get_random_command(TRNG_SAMPLE_SIZE)))
+                                     get_random_command(TRNG_SAMPLE_SIZE,
+                                     trng_mode)))
       if wrapped_response[:12] != get_random_command_rsp(TRNG_SAMPLE_SIZE):
         raise subcmd.TpmTestError("Unexpected response to '%s': %s" %
                                  ("trng", utils.hex_dump(wrapped_response)))
       f.write(wrapped_response[12:])
       print('%s %d%%\r' %( utils.cursor_back(), (x/10)), end=""),
-  print('%sSUCCESS: %s' % (utils.cursor_back(), 'trng'))
+  print('%sSUCCESS: trng' % (utils.cursor_back()))
