907b884bbb4b0ea436455ffa7649cc919133f860
Abe Levkoy <alevkoy@chromium.org>
2019-11-20 13:22:21 -0800

    volteer: Ignore PPC1 interrupt
    Currently, there is no appropriate action to take in response to this.
    
    BUG=b:140572591
    TEST=make buildall
    BRANCH=none
    
    Change-Id: Ic6a6123fbf7673f87621ab27b98a8ae4672e0edc

--- baseboard/volteer/baseboard.c
+++ baseboard/volteer/baseboard.c
@@ -252,9 +252,6 @@ void ppc_interrupt(enum gpio_signal signal)
 	case GPIO_USB_C0_PPC_INT_ODL:
 		sn5s330_interrupt(USBC_PORT_C0);
 		break;
-	case GPIO_USB_C1_PPC_INT_ODL:
-		sn5s330_interrupt(USBC_PORT_C1);
-		break;
 	default:
 		break;
 	}
