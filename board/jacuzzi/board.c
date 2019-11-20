bfe54d3747ee3585e36f427cc34564945282810f
Ting Shen <phoenixshen@google.com>
2019-11-20 15:54:40 +0800

    jacuzzi: fix EC_SENSOR_SPI_CK power leakage
    EC_SENSOR_SPI_CK should keep low when sensor is off.
    
    BUG=b:144144075
    TEST=1) after ap boot, apshutdown + gpioget, make sure EC_SENSOR_SPI_CK
            is low
         2) verify power consumption reduced
         3) verify pp1800_ap wave form looks normal
    BRANCH=none
    
    Change-Id: Ia6928905f9d8090017863425735dac0726023a96
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- board/jacuzzi/board.c
+++ board/jacuzzi/board.c
@@ -252,6 +252,8 @@ static void board_spi_disable(void)
 	STM32_RCC_APB1ENR &= ~STM32_RCC_PB1_SPI2;
 
 	gpio_config_module(MODULE_SPI_MASTER, 0);
+	gpio_set_flags(GPIO_EC_SENSOR_SPI_CK, GPIO_OUT_LOW);
+	gpio_set_level(GPIO_EC_SENSOR_SPI_CK, 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN,
 	     board_spi_disable,
