fb2be688312e791c52f7484a4924ea1002cea19a
Vadim Sukhomlinov <sukhomlinov@google.com>
2019-11-20 09:50:40 -0800

    cr50: use NIST-compliant configuration of TRNG
    According to NIST SP 800-90B only vetted conditioning mechanism should
    be used for post-processing raw entropy.  See SP 800-90B, 3.1.5.1 Using
    Vetted Conditioning Components. Use of non-vetted algorithms is governed
    in 3.1.5.2, but assumes conservative coefficient 0.85 for entropy estimate,
    which increase number of requests to TRNG to get desirable entropy.
    More details on entropy estimate tests are in associated bug.
    
    Entropy measurements using NIST assessment tool didn't report noticeable
    change in entropy estimate. However, more changes are needed to use
    DRBG instead of raw TRNG for all purposes.
    
    Cherry-picked from https://crrev.com/c/1926384
    
    BUG=b:138577834
    TEST=test/tpm_test/nist_entropy.sh
    
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Change-Id: I5a578b90b8b7a77fae6a218eec48e87e7644ab44

--- board/cr50/tpm2/trng.c
+++ board/cr50/tpm2/trng.c
@@ -9,3 +9,51 @@ CRYPT_RESULT _cpri__StirRandom(int32_t num, uint8_t *entropy)
 {
 	return CRYPT_SUCCESS;     /* NO-OP on CR50. */
 }
+
+#ifdef CRYPTO_TEST_SETUP
+#include "trng.h"
+#include "extension.h"
+/*
+ * This extension command is similar to TPM2_GetRandom, but made
+ * available for CRYPTO_TEST = 1 which disables TPM
+ * Command structure, shared out of band with the test driver running
+ * on the host:
+ *
+ * field     |    size  |                  note
+ * =========================================================================
+ * text_len  |    2     | size of the text to process, big endian
+ * type      |    1     | 0 = TRNG, other values reserved for extensions
+ */
+static enum vendor_cmd_rc trng_test(enum vendor_cmd_cc code, void *buf,
+				    size_t input_size, size_t *response_size)
+{
+	uint16_t text_len;
+	uint8_t *cmd;
+	uint8_t op_type = 0;
+	size_t response_room = *response_size;
+
+	*response_size = 0;
+	if (input_size != sizeof(text_len) + 1)
+		return VENDOR_RC_BOGUS_ARGS;
+
+	cmd = buf;
+	text_len = *cmd++;
+	text_len = text_len * 256 + *cmd++;
+	if (text_len > response_room)
+		return VENDOR_RC_BOGUS_ARGS;
+
+	op_type = *cmd++;
+
+	switch (op_type) {
+	case 0:
+		rand_bytes(buf, text_len);
+		break;
+	default:
+		return VENDOR_RC_BOGUS_ARGS;
+	}
+	*response_size = text_len;
+	return VENDOR_RC_SUCCESS;
+}
+
+DECLARE_VENDOR_COMMAND(VENDOR_CC_TRNG_TEST, trng_test);
+#endif /* CRYPTO_TEST_SETUP */
