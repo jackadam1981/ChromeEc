1935603210a3ffcba3968d78895a8c3e99b167ef
Mary Ruthven <mruthven@chromium.org>
2019-11-20 18:40:41 -0800

    gsctool: delay RO update for old cr50 images
    If Cr50 is running older than 0.3.20, delay the RO update 1 minute after
    the RW update, so Cr50 doesn't reject the RO blocks because their
    offsets are less than the RW offsets.
    
    BUG=b:144873413
    BRANCH=none
    TEST=update board running RO 0.0.10 RW 0.3.18 to the RO 0.0.11 RW 0.3.22
    image.
    
    Change-Id: I0179cc235c692133b08cd3430d71069b2f94bf69
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -1164,12 +1164,21 @@ static void send_done(struct usb_endpoint *uep)
 	do_xfer(uep, &out, sizeof(out), &out, 1, 0, NULL);
 }
 
+/* Support for flashing RO immediately after RW was added in 0.3.20/0.4.20. */
+static int supports_reordered_section_updates(struct signed_header_version *rw)
+{
+	return (rw->epoch || rw->major > 4 ||
+		(rw->major >= 3 && rw->minor > 20));
+}
+
 /* Returns number of successfully transmitted image sections. */
 static int transfer_image(struct transfer_descriptor *td,
 			       uint8_t *data, size_t data_len)
 {
 	size_t j;
 	int num_txed_sections = 0;
+	uint32_t last_offset = 0;
+	int needs_delay = !supports_reordered_section_updates(&targ.shv[1]);
 
 	/*
 	 * In case both RO and RW updates are required, make sure the RW
@@ -1190,12 +1199,28 @@ static int transfer_image(struct transfer_descriptor *td,
 
 			if (sections[i].ustatus != needed)
 				break;
+			/*
+			 * Old Cr50 images require a 1 minute delay to update
+			 * RO after RW.
+			 * Delays more than 5 seconds cause the update to
+			 * timeout. End the update before the delay then set it
+			 * up after to recover from the timeout.
+			 */
+			if (sections[i].offset < last_offset && needs_delay) {
+				if (td->ep_type == usb_xfer)
+					send_done(&td->uep);
+				printf("Waiting 1 minute for %s update.\n",
+				       sections[i].name);
+				sleep(60);
+				setup_connection(td);
+			}
 
 			transfer_section(td,
 					 data + sections[i].offset,
 					 sections[i].offset,
 					 sections[i].size);
 			num_txed_sections++;
+			last_offset = sections[i].offset;
 		}
 	}
 
