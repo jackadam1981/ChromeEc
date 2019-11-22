a82265f18ead6b4de42e8de7f7c41aa0e0d49dc9
Ting Shen <phoenixshen@google.com>
2019-11-22 19:45:21 +0800

    kukui: refactor PD_MAX_VOLTAGE/PD_OPERATING_POWER
    1) PD_MAX_VOLTAGE_MV is determined by charger, move it into the ifdef
       VARIANT_KUKUI_CHARGER_* clause.
    2) PD_OPERATING_POWER_MW is determined by board, move it into board.h.
    3) update the two config variables above for jacuzzi family.
    
    BUG=None
    TEST=make buildall
    BRANCH=kukui
    
    Change-Id: I6f14a71a34e2c81a5030ff5809996ddf74a4ad12
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- board/kappa/board.h
+++ board/kappa/board.h
@@ -1,3 +1,5 @@
+<<<<<<< HEAD   (221567 Reland "common: Refactor motion_sense_fifo")
+=======
 /* Copyright 2018 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
@@ -30,16 +32,10 @@
 #undef CONFIG_EXTPOWER_DEBOUNCE_MS
 #define CONFIG_EXTPOWER_DEBOUNCE_MS 200
 
-#define CONFIG_I2C_BITBANG
-#undef I2C_BITBANG_PORT_COUNT
-#define I2C_BITBANG_PORT_COUNT 1
-#undef CONFIG_I2C_NACK_RETRY_COUNT
-#define CONFIG_I2C_NACK_RETRY_COUNT 3
-#define CONFIG_SMBUS_PEC
-
 #define CONFIG_USB_PD_TCPM_FUSB302
 #define CONFIG_USB_PD_DISCHARGE_GPIO
 #define CONFIG_USB_PD_VBUS_MEASURE_NOT_PRESENT
+#define CONFIG_USB_PD_TCPC_LOW_POWER
 
 #define CONFIG_USB_MUX_IT5205
 
@@ -67,10 +63,9 @@
 #define I2C_PORT_BC12               0
 #define I2C_PORT_TCPC0              0
 #define I2C_PORT_USB_MUX            0
+#define I2C_PORT_BATTERY            1
 #define I2C_PORT_CHARGER            board_get_charger_i2c()
 #define I2C_PORT_IO_EXPANDER_IT8801 1
-#define I2C_PORT_ACCEL              1
-#define I2C_PORT_BATTERY            board_get_battery_i2c()
 
 /* Enable Accel over SPI */
 #define CONFIG_SPI_ACCEL_PORT    0  /* The first SPI master port (SPI2) */
@@ -83,18 +78,8 @@
 		(EC_HOST_EVENT_MASK(EC_HOST_EVENT_LID_OPEN) |\
 		 EC_HOST_EVENT_MASK(EC_HOST_EVENT_POWER_BUTTON))
 
-<<<<<<< HEAD   (221567 Reland "common: Refactor motion_sense_fifo")
-=======
-#define CONFIG_EC_KEYBOARD
-#define CONFIG_KEYBOARD_DEBUG
-#define CONFIG_KEYBOARD_NOT_RAW
-#define CONFIG_IO_EXPANDER_IT8801
-#define CONFIG_KEYBOARD_BOARD_CONFIG
-#define CONFIG_KEYBOARD_COL2_INVERTED
-
 #define PD_OPERATING_POWER_MW 30000
 
->>>>>>> CHANGE (260857 kukui: refactor PD_MAX_VOLTAGE/PD_OPERATING_POWER)
 #ifndef __ASSEMBLER__
 
 enum adc_channel {
@@ -131,6 +116,7 @@ enum charge_port {
 
 enum battery_type {
 	BATTERY_PANASONIC_AC15A3J,
+	BATTERY_PANASONIC_AC16L5J,
 	BATTERY_TYPE_COUNT,
 };
 
@@ -147,10 +133,10 @@ void board_reset_pd_mcu(void);
 int board_get_version(void);
 int board_is_sourcing_vbus(int port);
 
-/* returns the i2c port number of charger/battery */
+/* returns the i2c port number of charger */
 int board_get_charger_i2c(void);
-int board_get_battery_i2c(void);
 
 #endif /* !__ASSEMBLER__ */
 
 #endif /* __CROS_EC_BOARD_H */
+>>>>>>> CHANGE (260857 kukui: refactor PD_MAX_VOLTAGE/PD_OPERATING_POWER)
