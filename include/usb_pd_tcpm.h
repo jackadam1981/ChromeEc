89f75017225af5cb5f05fd2da83f5bfa773bdc41
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2019-11-27 17:17:35 +0800

    driver/it83xx: add Tx PHY reset function for TCPMv2
      When cc has connected with partner,  PRL do reset Tx PHY in
    PE startup state, but actually it reset whole TCPC. This will
    cause our TCPC register values mismatch the PD stack states.
    So I add Tx PHY layer reset function for PRL.
    
    BUG=b:145259048
    BRANCH=none
    TEST=connect to dongle and adapter, check by Lecroy analyzer:
         1.We don't toggle cc again when cc has connected with
           partner.
         2.PD state to SRC_READY or SNK_READY.
    
    Change-Id: I473269a4663fd776aadef0bc41f2152f87b73ddd
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- include/usb_pd_tcpm.h
+++ include/usb_pd_tcpm.h
@@ -332,6 +332,13 @@ struct tcpm_drv {
 	 * @param enable FRS enable (true) disable (false)
 	 */
 	 void (*set_frs_enable)(int port, int enable);
+
+	/**
+	 * Reset TCPC Tx PHY layer
+	 *
+	 * @param port Type-C port number
+	 */
+	 void (*set_tx_phy_reset)(int port);
 };
 
 /*
