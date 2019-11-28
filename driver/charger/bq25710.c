2462d5a111e02ea5baa545068b1ccd5581ff6ca0
YongBeum.Ha <ybha@samsung.corp-partner.google.com>
2019-11-28 17:13:10 +0900

    bq25710: Restore VSYS_MIN after chip reset
    It appears that the BQ25710 does not re-configure the VSYS_MIN parameter after a soft reset
    (register "charge option 3"), so the old value is wiped away when the register is reset.
    
    BUG=b:142087417
    BRANCH=none
    TEST=FW_NAME=kohaku emerge-kohaku chromeos-ec
    
    Change-Id: I28f1162dc9fb6ffb97529dd91872dcde9c72b363
    Signed-off-by: YongBeum.Ha <ybha@samsung.corp-partner.google.com>

--- driver/charger/bq25710.c
+++ driver/charger/bq25710.c
@@ -186,7 +186,12 @@ static void bq25710_init(void)
 	 * MIN_SYSTEM_VOLTAGE register prior to setting the reset so that the
 	 * correct value is preserved.
 	 */
+#ifdef CONFIG_BQ25710_VSYSMIN_VALUE
+	rv = 0;
+	vsys = CONFIG_BQ25710_VSYSMIN_VALUE;
+#else
 	rv = raw_read16(BQ25710_REG_MIN_SYSTEM_VOLTAGE, &vsys);
+#endif
 	rv |= raw_read16(BQ25710_REG_CHARGE_OPTION_3, &reg);
 	if (!rv) {
 		reg |= BQ25710_CHARGE_OPTION_3_RESET_REG;
