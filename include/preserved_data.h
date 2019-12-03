cec33e0d1456fc675aca2e100f71682f28b3cf44
Craig Hesling <hesling@chromium.org>
2019-12-03 13:02:59 -0800

    preserved data
    BRANCH=nocturne,hatch
    BUG=none
    TEST=non yet
    
    Change-Id: Ie0d2ca99ae8d9f1dcfd559679cc18f54d12c4ece

---
+++ include/preserved_data.h
@@ -0,0 +1,16 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* A mechanism to preserve data across resets */
+
+#ifndef __CROS_EC_PRESERVED_DATA_H
+#define __CROS_EC_PRESERVED_DATA_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+extern char preserved_data[];
+
+#endif	/* __CROS_EC_PRESERVED_DATA_H */
