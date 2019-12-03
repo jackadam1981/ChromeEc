cfcea904c62b5ca4e7595aa20589cdf46978a1bf
Craig Hesling <hesling@chromium.org>
2019-12-03 13:02:59 -0800

    Add preserved region feature
    BRANCH=nocturne,hatch
    BUG=none
    TEST=non yet
    
    Change-Id: Ie0d2ca99ae8d9f1dcfd559679cc18f54d12c4ece

--- common/build.mk
+++ common/build.mk
@@ -164,6 +164,8 @@ common-$(HAS_TASK_LIGHTBAR)+=lb_common.o lightbar.o
 common-$(HAS_TASK_MOTIONSENSE)+=motion_sense.o
 common-$(HAS_TASK_TPM)+=tpm_registers.o
 
+common-$(CONFIG_PRESERVED_REGION)+=preserved_region.o
+
 ifneq ($(HAVE_PRIVATE_AUDIO_CODEC_WOV_LIBS),y)
 common-$(CONFIG_AUDIO_CODEC_WOV)+=hotword_dsp_api.o
 endif
