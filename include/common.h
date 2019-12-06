533d388d1dec45f457937090a273c6498a933536
Namyoon Woo <namyoon@chromium.org>
2019-12-06 13:40:18 -0800

    read EC Firmware hash from kernel secdata during board initialization
    Cr50 reads EC Firmware hash from kernel secdata. This data shall be
    used for EC-EFS (Early Firmware Selection) procedure.
    
    BUG=chromium:1020578
    BRANCH=cr50
    TEST=none
    
    Change-Id: Id8942b5b49dd5b0412d198a12ee0bf87fd59d47f
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- include/common.h
+++ include/common.h
@@ -239,6 +239,9 @@ enum ec_error_list {
 	/* Verified boot data errors */
 	EC_ERROR_VBOOT_DATA = 0x1200,
 	EC_ERROR_VBOOT_DATA_VERIFY = 0x1201,
+	EC_ERROR_VBOOT_DATA_INCOMPATIBLE = 0x1202,
+	EC_ERROR_VBOOT_DATA_UNDERSIZED = 0x1203,
+	EC_ERROR_VBOOT_DATA_OVERSIZED = 0x1204,
 
 	/* Module-internal error codes may use this range.   */
 	EC_ERROR_INTERNAL_FIRST = 0x10000,
