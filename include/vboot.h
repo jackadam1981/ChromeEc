50deb480575d2e4f561cfeb353b7fbe29db0711e
Namyoon Woo <namyoon@chromium.org>
2019-12-09 14:34:55 -0800

    EC-CR50 communication
    This patch supports EC-CR50 communication.
    EC activates EC-CR50 communication by setting high DIOB3, and send
    a command packet to CR50 through UART_EC_TX_CR50_RX. Cr50 processes
    the packet, and sends a response packet back to EC. EC deactivates
    EC-CR50 communication by putting low DIOB3.
    
    This patch supports two kinds of EC-CR50 commands:
    - CR50_COMM_CMD_SET_BOOT_MODE
    - CR50_COMM_CMD_VERIFY_HASH
    
    Cr50 stores some of EC-EFS context in a powerdown register before
    deep sleep and restores it after wakeup.
    
    This patch increases flash usage by 1456 bytes.
    
    BUG=b:141143112
    BRANCH=cr50
    TEST=Checked "ec_comm" and "ec_efs" console command on Octopus and
    reworked Helios.
    Checked uart_stress_tester.py running without character loss.
    
    Change-Id: I23e90b9f3e860a3d198dcee718d7d11080d06e40
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- include/vboot.h
+++ include/vboot.h
@@ -62,9 +62,6 @@ struct cr50_comm_packet {
 enum ec_efs_boot_mode {
 	EC_EFS_BOOT_MODE_NORMAL           = 0x00,
 	EC_EFS_BOOT_MODE_NO_BOOT          = 0x01,
-	EC_EFS_BOOT_MODE_RECOVERY         = 0x02,
-	EC_EFS_BOOT_MODE_NO_BOOT_RECOVERY = 0x03,
-	EC_EFS_BOOT_MODE_RESET            = 0xff,
 
 	/* boot_mode is uint8_t */
 	EC_EFS_BOOT_MODE_LIMIT            = 255,
