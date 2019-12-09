542164dd819b19da76935eddd3322143d3730caf
Namyoon Woo <namyoon@chromium.org>
2019-12-09 14:34:55 -0800

    EC-CR50 communication
    This patch supports EC-CR50 communication.
    EC activates EC-CR50 communication by setting high DIOB3, and send
    a command packet to CR50 through UART_EC_TX_CR50_RX. Cr50 processes
    the packet, and sends a response packet back to EC. EC deactivates
    EC-CR50 communication by putting low DIOB3.
    
    This patch supports two kinds of EC-CR50 commands:
    - CR50_COMM_CMD_SET_BOOT_MODE
    - CR50_COMM_CMD_VERIFY_HASH
    
    Cr50 stores some of EC-EFS context in a powerdown register before
    deep sleep and restores it after wakeup.
    
    This patch increases flash usage by 1456 bytes.
    
    BUG=b:141143112
    BRANCH=cr50
    TEST=Checked "ec_comm" and "ec_efs" console command on Octopus and
    reworked Helios.
    Checked uart_stress_tester.py running without character loss.
    
    Change-Id: I23e90b9f3e860a3d198dcee718d7d11080d06e40
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- chip/g/usart.c
+++ chip/g/usart.c
@@ -3,7 +3,10 @@
  * found in the LICENSE file.
  */
 
+#ifdef BOARD_CR50
 #include "ccd_config.h"
+#include "ec_comm.h"
+#endif
 #include "queue.h"
 #include "queue_policies.h"
 #ifdef CONFIG_STREAM_SIGNATURE
@@ -215,10 +218,12 @@ void send_data_to_usb(struct usart_config const *config)
 	size_t count;
 	size_t q_room;
 	size_t tail;
-	size_t mask;
+	const size_t mask = uart_in->buffer_units_mask;
 	size_t inc = 1;
 
 	q_room = queue_space(uart_in);
+	tail = uart_in->state->tail & mask;
+	count = 0;
 
 #ifdef BOARD_CR50
 	if (uart == UART_EC) {
@@ -229,23 +234,34 @@ void send_data_to_usb(struct usart_config const *config)
 		if (!usb_from_uartn_is_enabled_(uart))
 			inc = 0;
 	}
-#endif
 
-	mask = uart_in->buffer_units_mask;
-	tail = uart_in->state->tail & mask;
-	count = 0;
+	if (ec_comm_is_uart_in_packet_mode(uart)) {
+		/*
+		 * Even if UART-to-USB data queue is full (count == q_room),
+		 * It should drain UART queue, so that an EC packet
+		 * can be processed. In this case, EC console data
+		 * shall be lost anyway.
+		 */
+		while (uartn_rx_available(uart)) {
+			uint8_t ch = uartn_read_char(uart);
 
-	/*
-	 * TODO(b/119329144): Process packet data separately,
-	 * and filter console data based on ccd capability.
-	 * if (ec_comm_is_uart_in_packet_mode(uart))
-	 *	...
-	 */
+			if (ec_comm_process_packet(ch))
+				continue;
 
-	while ((count != q_room) && uartn_rx_available(uart)) {
-		uart_in->buffer[tail] = uartn_read_char(uart);
-		tail = (tail + inc) & mask;
-		count += inc;
+			if (count != q_room) {
+				uart_in->buffer[tail] = ch;
+				tail = (tail + inc) & mask;
+				count += inc;
+			}
+		}
+	} else
+#endif  /* BOARD_CR50 */
+	{
+		while ((count != q_room) && uartn_rx_available(uart)) {
+			uart_in->buffer[tail] = uartn_read_char(uart);
+			tail = (tail + inc) & mask;
+			count += inc;
+		}
 	}
 	if (count)
 		queue_advance_tail(uart_in, count);
