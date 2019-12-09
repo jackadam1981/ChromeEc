e8dd2b76661db2f87c9d3373bdff8a7819ebf010
Namyoon Woo <namyoon@chromium.org>
2019-12-09 14:34:55 -0800

    EC-CR50 communication
    This patch supports EC-CR50 communication.
    EC activates EC-CR50 communication by setting high DIOB3, and send
    a command packet to CR50 through UART_EC_TX_CR50_RX. Cr50 processes
    the packet, and sends a response packet back to EC. EC deactivates
    EC-CR50 communication by putting low DIOB3.
    
    This patch supports two kinds of EC-CR50 commands:
    - CR50_COMM_CMD_SET_BOOT_MODE
    - CR50_COMM_CMD_VERIFY_HASH
    
    Cr50 stores some of EC-EFS context in a powerdown register before
    deep sleep and restores it after wakeup.
    
    This patch increases flash usage by 1456 bytes.
    
    BUG=b:119329144
    BRANCH=cr50
    TEST=Checked "ec_comm" console command on Octopus and
    reworked Helios.
    Checked uart_stress_tester.py running without character loss.
    
    Change-Id: I23e90b9f3e860a3d198dcee718d7d11080d06e40
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- board/cr50/ec_efs.c
+++ board/cr50/ec_efs.c
@@ -147,3 +147,89 @@ void ec_efs_reset(void)
 {
 	set_boot_mode_(EC_EFS_BOOT_MODE_NORMAL);
 }
+
+/*
+ * Change the EC boot mode value.
+ *
+ * @param data Pointer to the EC-CR50 packet
+ * @param size Data (payload) size in EC-CR50 packet
+ * @return CR50_COMM_SUCCESS if the packet has been processed successfully,
+ *         CR50_COMM_ERROR_SIZE if data size is not as expected, or
+ *         0 if it does not respond to EC.
+ */
+uint16_t ec_efs_set_boot_mode(const char * const data, const uint8_t size)
+{
+	uint8_t boot_mode;
+
+	if (size != 1)
+		return CR50_COMM_ERROR_SIZE;
+
+	boot_mode = data[0];
+
+	if (boot_mode != EC_EFS_BOOT_MODE_NORMAL) {
+		board_reboot_ec_deferred(0);
+		return 0;
+	}
+
+	set_boot_mode_(boot_mode);
+	return CR50_COMM_SUCCESS;
+}
+
+/*
+ * Verify the given EC-FW hash against one in kernel secdata.
+ *
+ * @param data Pointer to the EC-CR50 packet
+ * @param size Data (payload) size in EC-CR50 packet
+ * @return CR50_COMM_SUCCESS if the packet has been processed successfully,
+ *         CR50_COMM_ERROR_SIZE if data size is not as expected, or
+ *         CR50_COMM_ERROR_BAD_PAYLOAD if the given hash and the hash in NVM
+ *                                     are not same.
+ *         0 if it deosn't have to respond to EC.
+ */
+uint16_t ec_efs_verify_hash(const char *hash_data, const uint8_t size)
+{
+	if (size != SHA256_DIGEST_SIZE)
+		return CR50_COMM_ERROR_SIZE;
+
+	if (!ec_efs_ctx.hash_is_loaded) {
+		if (ec_efs_ctx.secdata_error_code == EC_SUCCESS)
+			load_ec_hash_(&ec_efs_ctx);
+
+		if (ec_efs_ctx.secdata_error_code != EC_SUCCESS)
+			return CR50_COMM_ERROR_NVMEM;
+	}
+
+	if (safe_memcmp(hash_data, ec_efs_ctx.hash, SHA256_DIGEST_SIZE)) {
+		/* Verification failed */
+		set_boot_mode_(EC_EFS_BOOT_MODE_NO_BOOT);
+		return CR50_COMM_ERROR_BAD_PAYLOAD;
+	}
+
+	if (ec_efs_ctx.boot_mode != EC_EFS_BOOT_MODE_NORMAL) {
+		board_reboot_ec_deferred(0);
+		return 0;
+	}
+
+	return CR50_COMM_SUCCESS;
+}
+
+void ec_efs_refresh(void)
+{
+	ec_efs_ctx.hash_is_loaded = 0;
+
+	load_ec_hash_(&ec_efs_ctx);
+}
+
+void ec_efs_print_status(void)
+{
+	/* EC-EFS Context */
+	ccprintf("ec_hash is loaded  : %s\n",
+		 ec_efs_ctx.hash_is_loaded ? "YES" : "NO");
+	ccprintf("secdata_error_code : 0x%08x\n",
+		 ec_efs_ctx.secdata_error_code);
+
+#ifdef CR50_RELAXED
+	ccprintf("ec_hash_secdata    : %ph\n",
+		 HEX_BUF(ec_efs_ctx.hash, SHA256_DIGEST_SIZE));
+#endif
+}
