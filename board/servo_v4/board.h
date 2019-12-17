87faf0f64341a01bcea7c65cd2e0449fc5b79a74
Brian Nemec <bnemec@chromium.org>
2019-12-17 16:02:34 +0000

    Revert "servo_v4: remove console command on ToT only"
    This reverts commit 4fa4101b4b9114c13994326be689d74d36e50e68.
    
    Reason for revert: Link time optimization enabled to free up space on
    ToT in CL:1961149 .
    
    Original change's description:
    > servo_v4: remove console command on ToT only
    >
    > Servo v4 is under space pressure on Tot, but the actual branch is okay
    > for now. We release from the firmware branch so remove console commands
    > on ToT *only*.
    >
    > BRANCH=none
    > BUG=none
    > TEST=See ~1500K freed on RO and RW images on ToT.
    >
    > Change-Id: I8a49db7d4f00b0f5ca4ba4614fe62dccd7f928ee
    > Signed-off-by: Jett Rink <jettrink@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1940775
    > Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    > Commit-Queue: Keith Short <keithshort@chromium.org>
    
    Bug: none
    TEST: Verified ServoV4 flashes successfully, manually
    tested connection to servod and dut-control commands.
    Change-Id: If9019b320b78f1af859e1e2370b3da8cae50449f

--- board/servo_v4/board.h
+++ board/servo_v4/board.h
@@ -141,10 +141,6 @@
  */
 #define CONFIG_SYSTEM_UNLOCKED
 
-/* Console command removed on ToT only. Do not cherry pick to servo branch */
-#undef CONFIG_CMD_PD
-#undef CONFIG_CMD_USART_INFO
-
 #ifndef __ASSEMBLER__
 
 /* Timer selection */
