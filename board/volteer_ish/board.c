15b1110339e49c7eef64e82752e89706c2ca2026
li feng <li1.feng@intel.com>
2019-12-16 18:02:20 -0800

    volteer ish: enable lid accelerometer.
    Lid accelerometer is BMA253, driver accel_bma2x2.c is used.
    Reference to this CL:
    https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1965647
    BMA253 uses polling mode since it doesn't have hardware FIFO.
    
    IMU BMI260 will be enabled when driver is available.
    b:146144827 is tracking this.
    
    BUG=b:145946347
    BRANCH=none
    TEST=Verified host iio device has lid accel, can read
    name/location/x, y, z raw data, etc.
    
    Change-Id: I5581c33e7245448604ac964f9df0559f2e0327b2
    Signed-off-by: li feng <li1.feng@intel.com>

--- board/volteer_ish/board.c
+++ board/volteer_ish/board.c
@@ -28,10 +28,52 @@ const struct i2c_port_t i2c_ports[] = {
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
-/* Drivers */
-struct motion_sensor_t motion_sensors[] = {
+/* Sensor config */
+static struct mutex g_lid_mutex;
+
+/* sensor private data */
+static struct accelgyro_saved_data_t g_bma253_data;
+
+/*
+ * TODO(b/124337208): P0 boards don't have this sensor mounted so the rotation
+ * matrix can't be tested properly. This needs to be revisited after EVT to make
+ * sure the rotaiton matrix for the lid sensor is correct.
+ */
+static const mat33_fp_t lid_standard_ref = {
+	{ 0, FLOAT_TO_FP(-1), 0},
+	{ FLOAT_TO_FP(-1), 0, 0},
+	{ 0, 0, FLOAT_TO_FP(-1)}
 };
 
+/* Drivers */
+struct motion_sensor_t motion_sensors[] = {
+	[LID_ACCEL] = {
+		.name = "Lid Accel",
+		.active_mask = SENSOR_ACTIVE_S0_S3,
+		.chip = MOTIONSENSE_CHIP_BMA255,
+		.type = MOTIONSENSE_TYPE_ACCEL,
+		.location = MOTIONSENSE_LOC_LID,
+		.drv = &bma2x2_accel_drv,
+		.mutex = &g_lid_mutex,
+		.drv_data = &g_bma253_data,
+		.port = I2C_PORT_SENSOR,
+		.i2c_spi_addr_flags = BMA2x2_I2C_ADDR1_FLAGS,
+		.rot_standard_ref = &lid_standard_ref,
+		.min_frequency = BMA255_ACCEL_MIN_FREQ,
+		.max_frequency = BMA255_ACCEL_MAX_FREQ,
+		.default_range = 2, /* g, to support tablet mode */
+		.config = {
+			/* EC use accel for angle detection */
+			[SENSOR_CONFIG_EC_S0] = {
+				.odr = 10000 | ROUND_UP_FLAG,
+			},
+			/* Sensor on in S3 */
+			[SENSOR_CONFIG_EC_S3] = {
+				.odr = 10000 | ROUND_UP_FLAG,
+			},
+		},
+	},
+};
 const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 
 int chipset_in_state(int state_mask)
