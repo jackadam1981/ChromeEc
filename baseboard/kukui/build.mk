e7746fab64d92047b64bcaad263be3c7b314f3dd
Ting Shen <phoenixshen@google.com>
2019-12-18 12:19:29 +0800

    kukui: include a dummy bootblock if BOOTBLOCK variable not set
    Add a dummy bootblock to make sure size check is accurate during
    `make buildall`.
    
    The size of bootblock is picked from build 12753.0:
    $ ls -al */coreboot/bootblock.bin
    -rw-r--r-- 1 20992 Dec 13 10:20 kodama/coreboot/bootblock.bin
    -rw-r--r-- 1 20992 Dec 13 10:20 krane/coreboot/bootblock.bin
    -rw-r--r-- 1 20992 Dec 13 10:20 kukui/coreboot/bootblock.bin
    
    Jacuzzi's bootblock is 500 bytes larger, but since it still has ~6k
    bytes free space, we don't need to worry about it for now.
    
    BUG=chromium:1034518
    TEST=make buildall
    BRANCH=kukui
    
    Change-Id: I65c18f567dce0cb315e0cdc4f30ada5e83275d72
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- baseboard/kukui/build.mk
+++ baseboard/kukui/build.mk
@@ -6,6 +6,10 @@
 # Baseboard specific files build
 #
 
+ifeq ($(BOOTBLOCK),)
+BOOTBLOCK := $(out)/dummy-bootblock
+endif
+
 baseboard-y=baseboard.o
 baseboard-$(CONFIG_USB_POWER_DELIVERY)+=usb_pd_policy.o
 baseboard-$(CONFIG_BOOTBLOCK)+=emmc.o
@@ -19,3 +23,7 @@ baseboard-$(VARIANT_KUKUI_CHARGER_MT6370)+=charger_mt6370.o
 baseboard-$(VARIANT_KUKUI_POGO_KEYBOARD)+=base_detect_kukui.o
 
 $(out)/RO/baseboard/$(BASEBOARD)/emmc.o: $(out)/bootblock_data.h
+
+# Use the bootblock size of Kukui 12753.0 as our dummy file size
+$(out)/dummy-bootblock:
+	dd if=/dev/zero of=$@ bs=1 count=20992
