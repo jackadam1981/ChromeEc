0ccbe4e2822f0dcb982c93785100e8ebec35b386
Vijay Hiremath <vijay.p.hiremath@intel.com>
2019-12-22 14:00:09 -0800

    TCPMv1: refactor TBT & Cable VDO code
    Refactor TBT & Cable VDO code on TCPMv1 so that same VDO structures
    can be used for TCPMv2.
    
    BUG=none
    BRANCH=none
    TEST=Make buildall -j
         Able to detect Thunderbolt-compatible devices on TGLRVP
    
    Change-Id: I65f82e241d0cc2187050913e7d16942fdaa0ebd4
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/usb_common.c
+++ common/usb_common.c
@@ -939,14 +939,6 @@ __overridable int svdm_gfu_attention(int port, uint32_t *payload)
 #ifdef CONFIG_USB_PD_TBT_COMPAT_MODE
 __overridable int svdm_tbt_compat_enter_mode(int port, uint32_t mode_caps)
 {
-	/*
-	 * Before entering into alternate mode, state of the USB-C MUX needs to
-	 * be in safe mode Ref: USB Type-C Cable and Connector Specification
-	 * Section E.2.2 Alternate Mode Electrical Requirements
-	 */
-	usb_mux_set(port, IS_ENABLED(CONFIG_USB_MUX_VIRTUAL) ?
-		TYPEC_MUX_SAFE : TYPEC_MUX_NONE, USB_SWITCH_CONNECT,
-		pd_get_polarity(port));
 	return 0;
 }
 
@@ -998,7 +990,7 @@ const struct svdm_amode_fx supported_modes[] = {
 		.attention = &svdm_tbt_compat_attention,
 		.exit = &svdm_tbt_compat_exit_mode,
 	},
-#endif /* CONFIG_USB_PD_TBT_COMPAT_MODE */
+#endif
 };
 const int supported_modes_cnt = ARRAY_SIZE(supported_modes);
 #endif /* CONFIG_USB_PD_ALT_MODE_DFP */
