2f712bc3d51e8ae8e88f215c064041f01fcb9090
Namyoon Woo <namyoon@chromium.org>
2019-12-27 10:32:23 -0800

    make DIOB3 interruptible and wakable for EC-EFS2
    If the board supports EC-CR50 communication, Cr50 enables both
    rising/falling-edge triggered interrupt on DIOB3 pin and makes
    it wakable as well.Cr50 connects GPIO_AP_FLASH_SELECT to DIOB4.
    
    If the board does not support EC-CR50 communication, Cr50 connects
    GPIO_AP_FLASH_SELECT to DIOB3.
    
    If EC puts high on DIOB3 to activate EC-CR50 communication, CR50
    enables UART_EC RX and TX.
    
    BUG=chromium:1035706
    BRANCH=cr50
    TEST=none
    
    Change-Id: I1221a1a19219274622ab710568ce7c66ab2f1da7
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- board/cr50/rdd.c
+++ board/cr50/rdd.c
@@ -229,6 +229,7 @@ static void ccd_state_change_hook(void)
 {
 	uint32_t flags_now;
 	uint32_t flags_want = 0;
+	uint32_t flags_to_yield_to_servo;
 	uint32_t delta;
 
 	/* Check what's enabled now */
@@ -267,50 +268,68 @@ static void ccd_state_change_hook(void)
 	else
 		flags_want = 0;
 
+	flags_to_yield_to_servo = CCD_ENABLE_UART_AP_TX |
+				  CCD_ENABLE_UART_EC_TX |
+				  CCD_ENABLE_UART_EC_BITBANG | CCD_ENABLE_I2C |
+				  CCD_ENABLE_SPI;
+
+	if (ec_comm_is_uart_in_packet_mode(UART_EC) & ec_is_rx_allowed()) {
+		/* EC-CR50 comm needs UART_EC RX/TX enabled. */
+		flags_want |= (CCD_ENABLE_UART_EC | CCD_ENABLE_UART_EC_TX);
+		/*
+		 * Do not yield UART_EC TX to servo.
+		 * Note: With the board property, BOARD_EC_CR50_COMM_SUPPORT,
+		 *       H1-EC UART connection is supposed to dominate over
+		 *       servo-EC uart by HW design.
+		 */
+		flags_to_yield_to_servo &= ~CCD_ENABLE_UART_EC_TX;
+	}
+
 	/* Then disable flags we can't have */
+	if (flags_want) {
+		/* Servo takes over UART TX, I2C, and SPI. */
+		if (servo_is_connected() ||
+		    (ccd_block & CCD_BLOCK_SERVO_SHARED))
+			flags_want &= ~flags_to_yield_to_servo;
 
-	/* Servo takes over UART TX, I2C, and SPI. */
-	if (servo_is_connected() || (ccd_block & CCD_BLOCK_SERVO_SHARED))
-		flags_want &= ~(CCD_ENABLE_UART_AP_TX | CCD_ENABLE_UART_EC_TX |
-				CCD_ENABLE_UART_EC_BITBANG | CCD_ENABLE_I2C |
-				CCD_ENABLE_SPI);
+		/* Disable based on capabilities */
+		if (!ccd_is_cap_enabled(CCD_CAP_GSC_RX_AP_TX))
+			flags_want &= ~CCD_ENABLE_USB_FROM_UART_AP;
+		if (!ccd_is_cap_enabled(CCD_CAP_GSC_TX_AP_RX))
+			flags_want &= ~CCD_ENABLE_USB_TO_UART_AP;
+		if (!ccd_is_cap_enabled(CCD_CAP_GSC_RX_EC_TX))
+			flags_want &= ~CCD_ENABLE_USB_FROM_UART_EC;
+		if (!ccd_is_cap_enabled(CCD_CAP_GSC_TX_EC_RX))
+			flags_want &= ~(CCD_ENABLE_USB_TO_UART_EC |
+					CCD_ENABLE_UART_EC_BITBANG);
+		if (!ccd_is_cap_enabled(CCD_CAP_I2C))
+			flags_want &= ~CCD_ENABLE_I2C;
 
-	/* Disable based on capabilities */
-	if (!ccd_is_cap_enabled(CCD_CAP_GSC_RX_AP_TX))
-		flags_want &= ~CCD_ENABLE_USB_FROM_UART_AP;
-	if (!ccd_is_cap_enabled(CCD_CAP_GSC_TX_AP_RX))
-		flags_want &= ~CCD_ENABLE_USB_TO_UART_AP;
-	if (!ccd_is_cap_enabled(CCD_CAP_GSC_RX_EC_TX))
-		flags_want &= ~CCD_ENABLE_USB_FROM_UART_EC;
-	if (!ccd_is_cap_enabled(CCD_CAP_GSC_TX_EC_RX))
-		flags_want &= ~(CCD_ENABLE_USB_TO_UART_EC |
-				CCD_ENABLE_UART_EC_BITBANG);
-	if (!ccd_is_cap_enabled(CCD_CAP_I2C))
-		flags_want &= ~CCD_ENABLE_I2C;
+		/*
+		 * EC and AP flash block on a per-packet basis, but if we don't
+		 * have access to either one, turn off SPI.
+		 */
+		if (!ccd_is_cap_enabled(CCD_CAP_AP_FLASH) &&
+		    !ccd_is_cap_enabled(CCD_CAP_EC_FLASH))
+			flags_want &= ~CCD_ENABLE_SPI;
 
-	/*
-	 * EC and AP flash block on a per-packet basis, but if we don't have
-	 * access to either one, turn off SPI.
-	 */
-	if (!ccd_is_cap_enabled(CCD_CAP_AP_FLASH) &&
-	    !ccd_is_cap_enabled(CCD_CAP_EC_FLASH))
-		flags_want &= ~CCD_ENABLE_SPI;
+		/* EC UART TX blocked by bit-banging */
+		if (flags_want & CCD_ENABLE_UART_EC_BITBANG)
+			flags_want &= ~CCD_ENABLE_UART_EC_TX;
 
-	/* EC UART TX blocked by bit-banging */
-	if (flags_want & CCD_ENABLE_UART_EC_BITBANG)
-		flags_want &= ~CCD_ENABLE_UART_EC_TX;
+		/* UARTs can be specifically blocked by console command */
+		if (ccd_block & CCD_BLOCK_AP_UART)
+			flags_want &= ~CCD_ENABLE_UART_AP;
+		if ((ccd_block & CCD_BLOCK_EC_UART) &&
+		    !ec_comm_is_uart_in_packet_mode(UART_EC))
+			flags_want &= ~CCD_ENABLE_UART_EC;
 
-	/* UARTs can be specifically blocked by console command */
-	if (ccd_block & CCD_BLOCK_AP_UART)
-		flags_want &= ~CCD_ENABLE_UART_AP;
-	if (ccd_block & CCD_BLOCK_EC_UART)
-		flags_want &= ~CCD_ENABLE_UART_EC;
-
-	/* UARTs are either RX-only or RX+TX, so no RX implies no TX */
-	if (!(flags_want & CCD_ENABLE_UART_AP))
-		flags_want &= ~CCD_ENABLE_UART_AP_TX;
-	if (!(flags_want & CCD_ENABLE_UART_EC))
-		flags_want &= ~CCD_ENABLE_UART_EC_TX;
+		/* UARTs are either RX-only or RX+TX, so no RX implies no TX */
+		if (!(flags_want & CCD_ENABLE_UART_AP))
+			flags_want &= ~CCD_ENABLE_UART_AP_TX;
+		if (!(flags_want & CCD_ENABLE_UART_EC))
+			flags_want &= ~CCD_ENABLE_UART_EC_TX;
+	}
 
 	/* If no change, we're done */
 	if (flags_now == flags_want)
@@ -322,65 +341,81 @@ static void ccd_state_change_hook(void)
 
 	/* Handle turning things off */
 	delta = flags_now & ~flags_want;
-	if (delta & CCD_ENABLE_UART_AP)
-		uartn_disable(UART_AP);
-	if (delta & CCD_ENABLE_UART_AP_TX)
-		uartn_tx_disconnect(UART_AP);
-	if (delta & CCD_ENABLE_UART_EC)
-		uartn_disable(UART_EC);
-	if (delta & CCD_ENABLE_UART_EC_TX)
-		uartn_tx_disconnect(UART_EC);
+	if (delta) {
+		if (delta & CCD_ENABLE_UART_AP)
+			uartn_disable(UART_AP);
+		if (delta & CCD_ENABLE_UART_AP_TX)
+			uartn_tx_disconnect(UART_AP);
+		if (delta & CCD_ENABLE_UART_EC)
+			uartn_disable(UART_EC);
+		if (delta & CCD_ENABLE_UART_EC_TX)
+			uartn_tx_disconnect(UART_EC);
 #ifdef CONFIG_UART_BITBANG
-	if (delta & CCD_ENABLE_UART_EC_BITBANG)
-		uart_bitbang_disable();
+		if (delta & CCD_ENABLE_UART_EC_BITBANG)
+			uart_bitbang_disable();
 #endif
-	if (delta & CCD_ENABLE_I2C)
-		usb_i2c_board_disable();
-	if (delta & CCD_ENABLE_SPI)
-		usb_spi_enable(&ccd_usb_spi, 0);
+		if (delta & CCD_ENABLE_I2C)
+			usb_i2c_board_disable();
+		if (delta & CCD_ENABLE_SPI)
+			usb_spi_enable(&ccd_usb_spi, 0);
 
-	if (delta & CCD_ENABLE_USB_FROM_UART_AP)
-		usb_from_uartn_disable(UART_AP);
-	if (delta & CCD_ENABLE_USB_TO_UART_AP)
-		usb_to_uartn_disable(UART_AP);
-	if (delta & CCD_ENABLE_USB_FROM_UART_EC)
-		usb_from_uartn_disable(UART_EC);
-	if (delta & CCD_ENABLE_USB_TO_UART_EC)
-		usb_to_uartn_disable(UART_EC);
+		if (delta & CCD_ENABLE_USB_FROM_UART_AP)
+			usb_from_uartn_disable(UART_AP);
+		if (delta & CCD_ENABLE_USB_TO_UART_AP)
+			usb_to_uartn_disable(UART_AP);
+		if (delta & CCD_ENABLE_USB_FROM_UART_EC)
+			usb_from_uartn_disable(UART_EC);
+		if (delta & CCD_ENABLE_USB_TO_UART_EC)
+			usb_to_uartn_disable(UART_EC);
+	}
 
 	/* Handle turning things on */
 	delta = flags_want & ~flags_now;
-	if (delta & CCD_ENABLE_UART_AP)
-		uartn_enable(UART_AP);
-	if (delta & CCD_ENABLE_UART_AP_TX)
-		uartn_tx_connect(UART_AP);
-	if (delta & CCD_ENABLE_UART_EC)
-		uartn_enable(UART_EC);
-	if (delta & CCD_ENABLE_UART_EC_TX)
-		uartn_tx_connect(UART_EC);
+	if (delta) {
+		if (delta & CCD_ENABLE_UART_AP)
+			uartn_enable(UART_AP);
+		if (delta & CCD_ENABLE_UART_AP_TX)
+			uartn_tx_connect(UART_AP);
+		if (delta & CCD_ENABLE_UART_EC)
+			uartn_enable(UART_EC);
+		if (delta & CCD_ENABLE_UART_EC_TX)
+			uartn_tx_connect(UART_EC);
 #ifdef CONFIG_UART_BITBANG
-	if (delta & CCD_ENABLE_UART_EC_BITBANG) {
-		/*
-		 * Servo detect interrupt will be re-enabled by the
-		 * servo_detect() poll once bit bang mode is disabled.
-		 */
-		gpio_disable_interrupt(GPIO_DETECT_SERVO);
-		uart_bitbang_enable();
-	}
+		if (delta & CCD_ENABLE_UART_EC_BITBANG) {
+			/*
+			 * Servo detect interrupt will be re-enabled by the
+			 * servo_detect() poll once bit bang mode is disabled.
+			 */
+			gpio_disable_interrupt(GPIO_DETECT_SERVO);
+			uart_bitbang_enable();
+		}
 #endif
-	if (delta & CCD_ENABLE_I2C)
-		usb_i2c_board_enable();
-	if (delta & CCD_ENABLE_SPI)
-		usb_spi_enable(&ccd_usb_spi, 1);
+		if (delta & CCD_ENABLE_I2C)
+			usb_i2c_board_enable();
+		if (delta & CCD_ENABLE_SPI)
+			usb_spi_enable(&ccd_usb_spi, 1);
 
-	if (delta & CCD_ENABLE_USB_FROM_UART_AP)
-		usb_from_uartn_enable(UART_AP);
-	if (delta & CCD_ENABLE_USB_TO_UART_AP)
-		usb_to_uartn_enable(UART_AP);
-	if (delta & CCD_ENABLE_USB_FROM_UART_EC)
-		usb_from_uartn_enable(UART_EC);
-	if (delta & CCD_ENABLE_USB_TO_UART_EC)
-		usb_to_uartn_enable(UART_EC);
+		if (delta & CCD_ENABLE_USB_FROM_UART_AP)
+			usb_from_uartn_enable(UART_AP);
+		if (delta & CCD_ENABLE_USB_TO_UART_AP)
+			usb_to_uartn_enable(UART_AP);
+		if (delta & CCD_ENABLE_USB_FROM_UART_EC)
+			usb_from_uartn_enable(UART_EC);
+		if (delta & CCD_ENABLE_USB_TO_UART_EC)
+			usb_to_uartn_enable(UART_EC);
+	}
+
+	/*
+	 * USB->UART bridging was blocked during the EC-CR50 communication.
+	 * Let's flush any blocked console input data if any.
+	 */
+	if (board_has_ec_cr50_comm_support()) {
+		uint32_t flags_mask;
+
+		flags_mask = CCD_ENABLE_UART_EC_TX | CCD_ENABLE_USB_TO_UART_EC;
+		if ((flags_want & flags_mask) == flags_mask)
+			task_trigger_irq(GC_IRQNUM_UART2_TXINT);
+	}
 }
 DECLARE_DEFERRED(ccd_state_change_hook);
 
