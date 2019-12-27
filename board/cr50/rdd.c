803b61e118c0a109bae4b44b149d3322933fc003
Namyoon Woo <namyoon@chromium.org>
2019-12-27 10:32:23 -0800

    make DIOB3 interruptible and wakable for EC-EFS2
    If the board supports EC-CR50 communication, Cr50 enables both
    rising/falling-edge triggered interrupt on DIOB3 pin and makes
    it wakable as well.Cr50 connects GPIO_AP_FLASH_SELECT to DIOB4.
    
    If the board does not support EC-CR50 communication, Cr50 connects
    GPIO_AP_FLASH_SELECT to DIOB3.
    
    If EC puts high on DIOB3 to activate EC-CR50 communication, CR50
    enables UART_EC RX and TX.
    
    BUG=chromium:1035706
    BRANCH=cr50
    TEST=None
    
    Change-Id: I1221a1a19219274622ab710568ce7c66ab2f1da7
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- board/cr50/rdd.c
+++ board/cr50/rdd.c
@@ -100,6 +100,15 @@ static void uart_select_tx(int uart, int signal)
 void uartn_tx_connect(int uart)
 {
 	/*
+	 * If EC-CR50 communication is activated, then do not skip connecting
+	 * TX.
+	 */
+	if ((uart == UART_EC) && ec_comm_is_uart_in_packet_mode(UART_EC)) {
+		uart_select_tx(UART_EC, GC_PINMUX_UART2_TX_SEL);
+		return;
+	}
+
+	/*
 	 * Don't drive TX unless the debug cable is connected (we have
 	 * something to transmit) and servo is disconnected (we won't be
 	 * drive-fighting with servo).
@@ -299,6 +308,20 @@ static void ccd_state_change_hook(void)
 		flags_want &= ~CCD_ENABLE_UART_AP_TX;
 	if (!(flags_want & CCD_ENABLE_UART_EC))
 		flags_want &= ~CCD_ENABLE_UART_EC_TX;
+
+	/*
+	 * If the packet mode is enabled and not in bitbang mode, then enable
+	 * both UART_EC RX and TX, so that it can receive an EC packet and
+	 * respond back.
+	 *
+	 * Note: In boards supporting EC-CR50 communication, CCD is supposed to
+	 *       dominate UART channel over servo by hardware design.
+	 */
+	if (ec_comm_is_uart_in_packet_mode(UART_EC))
+		if (!(flags_want & CCD_ENABLE_UART_EC_BITBANG)) {
+			flags_want |= CCD_ENABLE_UART_EC;
+			flags_want |= CCD_ENABLE_UART_EC_TX;
+		}
 
 	/* If no change, we're done */
 	if (flags_now == flags_want)
