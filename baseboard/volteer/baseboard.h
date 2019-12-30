a66d9582a051962457968a242b7dd124b3c61c85
Vijay Hiremath <vijay.p.hiremath@intel.com>
2019-12-30 13:57:13 -0800

    volteer: Enable USB4.0 mode
    BUG=b:140819518
    BRANCH=none
    TEST=With Gatkex creek 3 device volteer can enter to USB4.0 mode
    
    Change-Id: Ib20c14db42cae413322ca060c961fd67088a1a09
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- baseboard/volteer/baseboard.h
+++ baseboard/volteer/baseboard.h
@@ -183,6 +183,9 @@
 /* Enabling Thunderbolt-compatible mode */
 #define CONFIG_USB_PD_TBT_COMPAT_MODE
 
+/* Enabling USB4 mode */
+#define CONFIG_USB_PD_USB4
+
 /* TODO: b/144165680 - measure and check these values on Volteer */
 #define PD_POWER_SUPPLY_TURN_ON_DELAY	30000 /* us */
 #define PD_POWER_SUPPLY_TURN_OFF_DELAY	30000 /* us */
