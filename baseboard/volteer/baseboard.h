5d9df93c16a0fa3bd1359c4f85a28ffed81cbf5d
Vijay Hiremath <vijay.p.hiremath@intel.com>
2019-12-30 13:57:13 -0800

    [WIP]volteer: Enable TBT & USB
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: Ib20c14db42cae413322ca060c961fd67088a1a09
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- baseboard/volteer/baseboard.h
+++ baseboard/volteer/baseboard.h
@@ -137,6 +137,15 @@
 #define CONFIG_USBC_VCONN
 #define CONFIG_USBC_VCONN_SWAP
 
+/* Enabling SOP* communication */
+#define CONFIG_USB_PD_DECODE_SOP
+
+/* Enabling Thunderbolt-compatible mode */
+#define CONFIG_USB_PD_TBT_COMPAT_MODE
+
+/* Enabling USB4 mode */
+#define CONFIG_USB_PD_USB4
+
 /* TODO: b/144165680 - measure and check these values on Volteer */
 #define PD_POWER_SUPPLY_TURN_ON_DELAY	30000 /* us */
 #define PD_POWER_SUPPLY_TURN_OFF_DELAY	30000 /* us */
