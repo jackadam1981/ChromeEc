8aec3bafefc74a59a0d3ba1045461917d75d0a97
Ruibin Chang <ruibin.chang@ite.com.tw>
2019-12-31 17:09:10 +0800

    USB-PD: Header of PS_RDY and GoodCRC be new role when power swap
      After port partner accept power role swap, the message
    header power role field of following two PS_RDY and goodcrc
    should be the new source and sink role. See PD3.0 spec v1.1
    Figure 8-11, 12.
    
      If we do not receive goodcrc of PS_RDY, we should restore
    Rp/Rd and power role before we transit to SRC/SNK_DISCONNECT
    state.
    
      The new Source Shall Not send the Source_Capabilities Message
    earlier than tSwapSourceStart after the last bit of the EOP of
    GoodCRC Message sent in response to the PS_RDY Message sent by
    the new Source indicating that its power supply is ready. See
    PD3.0 spec ch.6.6.8.1.
    
    BUG=none
    BRANCH=none
    TEST=1.Ellisys USB-PD compliance test: PC.E2, CP.E4
         2.Connect two ampton, one code is same as this CL,
           the other send hard reset in PD_STATE_SNK_SWAP_SRC_DISABLE,
           PD_STATE_SNK_SWAP_STANDBY, PD_STATE_SNK_SWAP_COMPLETE
           these states.
           Check via Lecroy analyzer and UART: after hard reset,
           both port can re-connect to ready state.
    
    
    Change-Id: Ia858804231b7b9cc14d990d22d427e538f962e50
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -1397,16 +1397,6 @@ void pd_execute_hard_reset(int port)
 	pd[port].last_state = PD_STATE_HARD_RESET_EXECUTE;
 
 #ifdef CONFIG_USB_PD_DUAL_ROLE
-	/*
-	 * If we are swapping to a source and have changed to Rp, restore back
-	 * to Rd and turn off vbus to match our power_role.
-	 */
-	if (pd[port].task_state == PD_STATE_SNK_SWAP_STANDBY ||
-	    pd[port].task_state == PD_STATE_SNK_SWAP_COMPLETE) {
-		tcpm_set_cc(port, TYPEC_CC_RD);
-		pd_power_supply_reset(port);
-	}
-
 	/* Set initial data role (matching power role) */
 	pd_set_data_role(port, pd[port].power_role);
 	if (pd[port].power_role == PD_ROLE_SINK) {
@@ -1822,8 +1812,6 @@ static void handle_ctrl_request(int port, uint16_t head,
 		} else if (pd[port].task_state == PD_STATE_SRC_SWAP_STANDBY) {
 			/* reset message ID and swap roles */
 			pd[port].msg_id = 0;
-			pd_set_power_role(port, PD_ROLE_SINK);
-			pd_update_roles(port);
 			/*
 			 * Give the state machine time to read VBUS as high.
 			 * Note: This is empirically determined, not strictly
@@ -3779,6 +3767,10 @@ void pd_task(void *u)
 				/* Send PS_RDY */
 				res = send_control(port, PD_CTRL_PS_RDY);
 				if (res < 0) {
+					/* Restore Rp and power role */
+					tcpm_set_cc(port, TYPEC_CC_RP);
+					pd_set_power_role(port, PD_ROLE_SOURCE);
+					pd_update_roles(port);
 					timeout = 10*MSEC;
 					set_state(port,
 						  PD_STATE_SRC_DISCONNECTED);
@@ -4326,19 +4318,34 @@ void pd_task(void *u)
 			break;
 		case PD_STATE_SNK_SWAP_SRC_DISABLE:
 			/* Wait for PS_RDY */
-			if (pd[port].last_state != pd[port].task_state)
+			if (pd[port].last_state != pd[port].task_state) {
+				/*
+				 * Update goodcrc message header power role
+				 * field to be SRC for repsonding PS_RDY.
+				 * But now our cc should keep asserting Rd,
+				 * later receiving PS_RDY will switch to Rp.
+				 */
+				tcpm_set_msg_header(port, PD_ROLE_SOURCE,
+						    pd[port].data_role);
 				set_state_timeout(port,
 						  get_time().val +
 						  PD_T_PS_SOURCE_OFF,
 						  PD_STATE_HARD_RESET_SEND);
+			}
 			break;
 		case PD_STATE_SNK_SWAP_STANDBY:
 			if (pd[port].last_state != pd[port].task_state) {
-				/* Switch to Rp and enable power supply. */
+				/*
+				 * Now switch to Rp for match our new SRC
+				 * power role, and enable power supply.
+				 */
+				pd_set_power_role(port, PD_ROLE_SOURCE);
 				tcpm_set_cc(port, TYPEC_CC_RP);
 				if (pd_set_power_supply_ready(port)) {
-					/* Restore Rd */
+					/* Restore Rd and power role */
 					tcpm_set_cc(port, TYPEC_CC_RD);
+					pd_set_power_role(port, PD_ROLE_SINK);
+					pd_update_roles(port);
 					timeout = 10*MSEC;
 					set_state(port,
 						  PD_STATE_SNK_DISCONNECTED);
@@ -4353,11 +4360,13 @@ void pd_task(void *u)
 			}
 			break;
 		case PD_STATE_SNK_SWAP_COMPLETE:
-			/* Send PS_RDY and change to source role */
+			/* Send PS_RDY */
 			res = send_control(port, PD_CTRL_PS_RDY);
 			if (res < 0) {
-				/* Restore Rd */
+				/* Restore Rd and power role */
 				tcpm_set_cc(port, TYPEC_CC_RD);
+				pd_set_power_role(port, PD_ROLE_SINK);
+				pd_update_roles(port);
 				pd_power_supply_reset(port);
 				timeout = 10 * MSEC;
 				set_state(port, PD_STATE_SNK_DISCONNECTED);
@@ -4368,10 +4377,8 @@ void pd_task(void *u)
 			snk_cap_count = PD_SNK_CAP_RETRIES+1;
 			caps_count = 0;
 			pd[port].msg_id = 0;
-			pd_set_power_role(port, PD_ROLE_SOURCE);
-			pd_update_roles(port);
 			set_state(port, PD_STATE_SRC_DISCOVERY);
-			timeout = 10*MSEC;
+			timeout = PD_T_SWAP_SOURCE_START;
 			break;
 #ifdef CONFIG_USBC_VCONN_SWAP
 		case PD_STATE_VCONN_SWAP_SEND:
@@ -4538,15 +4545,6 @@ void pd_task(void *u)
 			}
 			break;
 		case PD_STATE_HARD_RESET_EXECUTE:
-#ifdef CONFIG_USB_PD_DUAL_ROLE
-			/*
-			 * If hard reset while in the last stages of power
-			 * swap, then we need to restore our CC resistor.
-			 */
-			if (pd[port].last_state == PD_STATE_SNK_SWAP_STANDBY)
-				tcpm_set_cc(port, TYPEC_CC_RD);
-#endif
-
 			/* reset our own state machine */
 			pd_execute_hard_reset(port);
 			timeout = 10*MSEC;
