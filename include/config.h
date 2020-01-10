4c8796d9e035170d23916b5f8e9b337035bf8b12
Craig Hesling <hesling@chromium.org>
2020-01-10 12:10:02 -0800

    common: Factor out hardware write protect check
    This moves all of the hardware write protect gpio
    checks to a central location.
    This cleans up some parts of code and allows for
    expansion of new hardware write protect features,
    like hooks on write protect changes.
    
    BRANCH=none
    BUG=none
    TEST=buildall
    TEST=# Simple sanity check #
         # Flash nucleo-h743zi
         # Deassert WP
         flashinfo
         # No wp_gpio_asserted should be present
         # Assert write protect
         flashinfo
         # wp_gpio_asserted should be present
    
    Change-Id: Ie2a5b568707d301e184738f928ad85b6d30d63f1
    Signed-off-by: Craig Hesling <hesling@chromium.org>

--- include/config.h
+++ include/config.h
@@ -4386,6 +4386,14 @@
 /* Support Wake-on-Voice */
 #undef CONFIG_WAKE_ON_VOICE
 
+
+/*
+ * Enables the accessors for the write protect gpio signal.
+ * It uses either GPIO_WP or GPIO_WP_L based on the status of
+ * CONFIG_WP_ACTIVE_HIGH.
+ */
+#undef CONFIG_WP
+
 /*
  * Write protect signal is active-high.  If this is defined, there must be a
  * GPIO named GPIO_WP; if not defined, there must be a GPIO names GPIO_WP_L.
@@ -4856,6 +4864,15 @@
 	defined(CONFIG_FP_SENSOR_FPC1035) || \
 	defined(CONFIG_FP_SENSOR_FPC1145)
 #define CONFIG_FP_SENSOR
+#endif
+
+/*
+ * Automatically define CONFIG_WP if dependenees are enabled.
+ * Note, we really need a way to express dependencies in configs.
+ */
+#if	defined(CONFIG_FLASH) || \
+	defined(CONFIG_CROS_BOARD_INFO)
+#define CONFIG_WP
 #endif
 
 /*****************************************************************************/
