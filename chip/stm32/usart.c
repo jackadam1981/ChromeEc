a81f506e485d117068eda808d4cdd507ec1dc6d8
Jett Rink <jettrink@chromium.org>
2020-01-13 16:05:39 -0700

    UART: drop data when disabled
    When UART is disabled drop both RX and TX data. Without this, we the stm
    will crash when trying to re-enable the UART module if the USB endpoint
    has sent any data while UART is disabled.
    
    BRANCH=none
    BUG=b:147512689
    TEST=verify that RX and TX data over CR50 UART while in SPI mode on c2d2
    no longer crashes C2D2 when switching out of spi mode
    
    Change-Id: Ib30fa21603d26432290aade423e5d20e923f8507
    Signed-off-by: Jett Rink <jettrink@chromium.org>

--- chip/stm32/usart.c
+++ chip/stm32/usart.c
@@ -77,10 +77,14 @@ void usart_init(struct usart_config const *config)
 	 * succeed.
 	 */
 	STM32_USART_CR1(base) |= STM32_USART_CR1_UE;
+
+	config->state->enabled = 1;
 }
 
 void usart_shutdown(struct usart_config const *config)
 {
+	config->state->enabled = 0;
+
 	STM32_USART_CR1(config->hw->base) &= ~STM32_USART_CR1_UE;
 
 	config->hw->ops->disable(config);
