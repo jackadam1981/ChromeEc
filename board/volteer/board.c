edfe7a1b252f41eceb38ddc1b2407370147c847a
Vijay Hiremath <vijay.p.hiremath@intel.com>
2020-01-13 11:32:48 -0800

    TBT: Allow limiting the cable speed based on board design
    From the TGL PDG, MAX TBT signals routing length can be 205mm prior to
    connection to re-timer. Orthogonal routing with such length would results
    in adverse effect to channel margin, as described in Fiberweave White
    Paper. Hence, added overridable function to override the TBT cable speed
    based on the board design.
    
    BUG=b:147498371
    BRANCH=none
    TEST=Able to detect TBT3 devices on Volteer
    
    Change-Id: I4490bc507c2c12b26372ed86e485c0491f1a9f21
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- board/volteer/board.c
+++ board/volteer/board.c
@@ -22,6 +22,7 @@
 #include "task.h"
 #include "tablet_mode.h"
 #include "uart.h"
+#include "usb_pd_tbt.h"
 #include "util.h"
 
 #include "gpio_list.h" /* Must come after other header files. */
@@ -32,3 +33,11 @@ static void board_init(void)
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
+__override enum tbt_compat_cable_speed board_get_max_tbt_speed(int port)
+{
+	/* Routing length exceeds 205mm prior to connection to re-timer */
+	if (port == USBC_PORT_C1)
+		return TBT_SS_U32_GEN1_GEN2;
+
+	return TBT_SS_RES_0;
+}
