bbc9f71dc18be4f13f542cf38de259c6d5bafd38
Caveh Jalali <caveh@chromium.org>
2020-01-13 20:10:35 -0800

    volteer: add USB3 daughterboard support
    The USB3 daughterboard uses a ps8815 TCPC. If the CBI FW_CONFIG tag
    indicates that such a daughterboard is present, set up the ps8815
    as the 2nd TCPC in the system.
    
    BRANCH=none
    BUG=b:144397088
    TEST=in combination with additional patches, was able to update TCPC
    	firmware
    
    Change-Id: I50ee57f5aa2efa0b6dbc562f968587f4fe03236c
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- driver/tcpm/ps8xxx.h
+++ driver/tcpm/ps8xxx.h
@@ -18,6 +18,9 @@
 /* Minimum Delay for reset assertion */
 #define PS8XXX_RESET_DELAY_MS 1
 
+/* Delay from power on to reset de-asserted */
+#define PS8XXX_PWR_H_RST_H_DELAY_MS 20
+
 #define PS8751_BIST_TIMER_FREQ  15000000
 #define PS8751_BIST_DELAY_MS    50
 
