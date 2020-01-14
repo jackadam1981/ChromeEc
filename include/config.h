e4442bb1ffdc68529362ccd5e9f2dbc00e766688
Diana Z <dzigterman@chromium.org>
2020-01-14 14:18:08 -0700

    Charger: Create charger driver structure
    With upcoming boards which use multiple charger chips, the EC codebase
    needs to be changed to assume chargers may have different I2C ports.
    This commit creates the driver structure and wrapper functions, which
    for now are hard-coded to chip 0 for equivalent behavior with previous
    code.  A general charger config is created for all boards in charger.c
    for now, which uses the build information to fill in the structure.
    
    For drivers with non-static and non-driver exports (bd9995x, rt946x),
    the approach was to code static functions to expect a charger chip
    number, while having non-statics hard code that value to 0.
    
    BRANCH=none
    BUG=b:147672225
    TEST=builds, chargers on hatch and octopus work
    
    Change-Id: I390ede494226252e512595c48099fa1288ffe93e
    Signed-off-by: Diana Z <dzigterman@chromium.org>

--- include/config.h
+++ include/config.h
@@ -797,6 +797,9 @@
 #undef CONFIG_CHARGER_RT9467
 #undef CONFIG_CHARGER_SY21612
 
+/* Allow run-time completion of the charger driver structure */
+#undef CONFIG_CHARGER_RUNTIME_CONFIG
+
 /*
  * Enable the CHG_EN at initialization to turn-on the BGATE which allows voltage
  * to be applied to the battery PACK & wakes the battery if it is in shipmode.
