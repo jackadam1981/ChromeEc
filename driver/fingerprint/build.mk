a0c59c8cf6ca8570913541b44157de9553bfe07d
Chiehchun Yu <chiehchun.yu@elan.corp-partner.google.com>
2020-01-16 14:44:50 +0800

    elan_private: Implement elan sensor driver
    This patch provides ELAN FP APIs that used to control ELAN FP sensor
    and matching algorithm.
    
    BUG=None
    TEST=We build on nami_fp, and testing Elan sensor with libelan.a
    successfully.
    
    Change-Id: I7a58025106f8ed570860b758323bf2047cde0731
    Signed-off-by: Chiehchun Yu <chiehchun.yu@elan.corp-partner.google.com>
    Signed-off-by: herman lin <herman.lin@emc.com.tw>

--- driver/fingerprint/build.mk
+++ driver/fingerprint/build.mk
@@ -7,4 +7,4 @@
 # Note that this variable includes the trailing "/"
 _fingerprint_cur_dir:=$(dir $(lastword $(MAKEFILE_LIST)))
 
-include $(_fingerprint_cur_dir)fpc/build.mk
+include $(_fingerprint_cur_dir)elan/build.mk
--- driver/fingerprint/fpsensor.h
+++ driver/fingerprint/fpsensor.h
@@ -11,10 +11,10 @@
 #include "fpc/fpc_sensor.h"
 #else
 /* These values are used by the host (emulator) tests. */
-#define FP_SENSOR_IMAGE_SIZE 0
-#define FP_SENSOR_RES_X 0
-#define FP_SENSOR_RES_Y 0
-#define FP_ALGORITHM_TEMPLATE_SIZE 0
+#define FP_SENSOR_IMAGE_SIZE (56*56)
+#define FP_SENSOR_RES_X 56
+#define FP_SENSOR_RES_Y 56
+#define FP_ALGORITHM_TEMPLATE_SIZE 40960
 #define FP_MAX_FINGER_COUNT 5
 #endif
 
