9d5e3a4edff0720e727c1a956e317558c21aaff2
Yicheng Li <yichengli@chromium.org>
2020-01-16 17:23:50 -0800

    stm32: Enlarge console task stack for testing
    For on-device unit tests, we use the console to "runtest" and view
    results. A small console size would result in console stack overflow
    after the test finishes. Use a larger console stack if it's a test
    build.
    
    BRANCH=None
    BUG=b:146059307
    TEST=make BOARD=nucleo-h743zi tests -j
         (Flash the build/nucleo-h743zi/test-aes.bin)
         (Connect to UART console)
         runtest
         (Repeat for test-sha256.bin and test-sha256_unrolled.bin)
    
    Change-Id: I5c48ae10f5808ed2d3854fdc72275a3a416cf76d
    Signed-off-by: Yicheng Li <yichengli@chromium.org>

--- chip/stm32/config_chip.h
+++ chip/stm32/config_chip.h
@@ -105,6 +105,18 @@
 /* Even bigger */
 #define VENTI_TASK_STACK_SIZE 768
 
+/* Console stack size */
+#ifdef TEST_BUILD
+/*
+ * For test builds, the console is used to interact with the test, and
+ * insufficient stack size causes console stack overflow after running
+ * the on-device tests.
+ */
+#define CONSOLE_TASK_STACK_SIZE 4096
+#else
+#define CONSOLE_TASK_STACK_SIZE LARGER_TASK_STACK_SIZE
+#endif
+
 /* Interval between HOOK_TICK notifications */
 #define HOOK_TICK_INTERVAL_MS 500
 #define HOOK_TICK_INTERVAL    (HOOK_TICK_INTERVAL_MS * MSEC)
