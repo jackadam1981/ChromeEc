3d781946114bdefbaa1756956cd17e1c63735f55
xiong.huang <xiong.huang@bitland.corp-partner.google.com>
2020-01-21 22:50:43 +0800

    Kodama: delay to enable charger EOC function
    When the battery is activated in shutdown mode, the battery needs
    H/W Initialization and Cu Deposition Checking in 4.2 second.
    Meanwhile, if the charger EOC function is enabled, charger will
    close/open the charging path repeatly and will pull PMIC's VSYSSNS
    power.
    It's a workaround to delay 4.5 second to enable charger EOC function.
    
    BUG=b:148045048
    TEST=The adpater can boot the DUT automatically when the battery enters
         shutdown mode or ship mode.
    BRANCH=kukui
    
    Change-Id: Ie00d0f7ad506a273dc1b51f03b5d28dfa582f14f
    Signed-off-by: Xiong Huang <xiong.huang@bitland.corp-partner.google.com>

--- board/kodama/board.c
+++ board/kodama/board.c
@@ -229,6 +229,8 @@ static void board_init(void)
 		gpio_set_level(GPIO_PMIC_FORCE_RESET_ODL, 1);
 	}
 
+	rt946x_enable_charge_eoc(0);
+
 	/* Enable TCPC alert interrupts */
 	gpio_enable_interrupt(GPIO_USB_C0_PD_INT_ODL);
 
