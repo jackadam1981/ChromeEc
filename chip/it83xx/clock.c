0d28307f08518aaba9a4f21b84f4ea2bbfe5e00d
Ruibin Chang <ruibin.chang@ite.com.tw>
2020-01-22 16:52:13 +0800

    chip/it83xx: Don't turn off pd module clock if rx enabled
      Plug in type-c adapter in G3 power state. Because the
    SLEEP_MASK_USB_PD won't be set before we receive the SRC_Cap
    from SRC in SNK_Discovery state, but at waiting SRC_Cap this
    period time we will enter deep sleep mode and turns pd module
    clock off. This will cause TCPC miss the SRC_Cap message from
    SRC, then our PD_T_SINK_WAIT_CAP timer timeout and tx hard reset.
    
      So I add a flag: if one of the port is enabled TCPC Rx, chip
    will not go to deep sleep. But chip can go to doze mode that pd
    module clock won't be turned off only mcu core clock off. if all
    ports are nothing plug-in, the chip can go to deep sleep.
    
    BUG=none
    BRANCH=none
    TEST=TCPC doesn't miss any message from partner:
         -in SNK_DISCOVERY and power G3 state on board ampton.
         -in SRC_DISCOVERY and power S0 state on board it8xxx2_evb.
    
    Change-Id: I9639523e2ca180809f0f74f24321d06e7b3a04c2
    Signed-off-by: Ruibin Chang <ruibin.chang@ite.com.tw>

--- chip/it83xx/clock.c
+++ chip/it83xx/clock.c
@@ -33,6 +33,7 @@
 BUILD_ASSERT(ADC_TIMEOUT_US < SLEEP_SET_HTIMER_DELAY_USEC);
 #endif
 
+int pd_need_awake;
 static timestamp_t sleep_mode_t0;
 static timestamp_t sleep_mode_t1;
 static int idle_doze_cnt;
@@ -407,13 +408,29 @@ static void clock_htimer_enable(void)
 
 static int clock_allow_low_power_idle(void)
 {
+	/*
+	 * TCPC Rx is enabled, avoiding miss messages from partner, we don't
+	 * go to sleep mode.
+	 */
+	if (pd_need_awake)
+		return 0;
+
+	/*
+	 * Avoiding using low frequency clock run the same count as awaken in
+	 * sleep mode, so don't go to sleep mode before timer reload count.
+	 */
 	if (!(IT83XX_ETWD_ETXCTRL(EVENT_EXT_TIMER) & BIT(0)))
 		return 0;
 
+	/* If timer interrupt status is set, don't go to sleep mode. */
 	if (*et_ctrl_regs[EVENT_EXT_TIMER].isr &
 		et_ctrl_regs[EVENT_EXT_TIMER].mask)
 		return 0;
 
+	/*
+	 * If timer is less than 250us to expire, then we don't go to sleep
+	 * mode.
+	 */
 #ifdef IT83XX_EXT_OBSERVATION_REG_READ_TWO_TIMES
 	if (EVENT_TIMER_COUNT_TO_US(ext_observation_reg_read(EVENT_EXT_TIMER)) <
 #else
@@ -422,11 +439,17 @@ static int clock_allow_low_power_idle(void)
 		SLEEP_SET_HTIMER_DELAY_USEC)
 		return 0;
 
+	/*
+	 * We calculate 32bit free clock overflow counts for 64bit value,
+	 * if clock almost reach overflow, we don't go to sleep mode for
+	 * avoiding miss overflow count.
+	 */
 	sleep_mode_t0 = get_time();
 	if ((sleep_mode_t0.le.lo > (0xffffffff - SLEEP_FTIMER_SKIP_USEC)) ||
 		(sleep_mode_t0.le.lo < SLEEP_FTIMER_SKIP_USEC))
 		return 0;
 
+	/* If we are waked up by console, then keep awake at least 5s. */
 	if (sleep_mode_t0.val < console_expire_time.val)
 		return 0;
 
