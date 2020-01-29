30c7f54f9191e39069e84ec1b3a243382810987b
Abe Levkoy <alevkoy@chromium.org>
2020-01-29 12:15:43 -0700

    Revert "volteer: disable PD 3.0 stack"
    This reverts commit 436ab9db9a0f999ac3452f8f26d4fb7ad4d4802c.
    
    BUG=b:145756626
    TEST=Compare to TCPMv1 in various PD scenarios; observe no regressions
    TEST=Plug 2 chargers in; unplug charging one; switches to other one
    TEST=Charges at 60W from 65W charger
    TEST=Detects SNK devices; DUT acts as SRC
    TEST=Battery disconnected; negotiates SNK state with charger as SRC
    BRANCH=none
    
    Change-Id: I24082bcb0ab3e2332da093dec47f2ef8af3ab537
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>

--- baseboard/volteer/baseboard.h
+++ baseboard/volteer/baseboard.h
@@ -137,20 +137,11 @@
 
 /* USB Type C and USB PD defines */
 /* Enable the new USB-C PD stack */
-/* TODO: b/145756626 - re-enable once all blocking issues resolved */
-#if 0
 #define CONFIG_USB_SM_FRAMEWORK
 #define CONFIG_USB_TYPEC_SM
 #define CONFIG_USB_PRL_SM
 #define CONFIG_USB_PE_SM
 #define CONFIG_USB_TYPEC_DRP_ACC_TRYSRC
-#else
-/*
- * PD 3.0 is always enabled by the TCPMv2 stack, so it's only explicitly
- * enabled when using the TCPMv1 stack
- */
-#define CONFIG_USB_PD_REV30
-#endif
 
 #define CONFIG_USB_POWER_DELIVERY
 #define CONFIG_USB_PD_ALT_MODE
