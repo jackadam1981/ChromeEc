fd5e565b1f0e605cde55651e853fb37daceb6b7d
Vijay Hiremath <vijay.p.hiremath@intel.com>
2020-01-30 15:08:26 -0800

    TCPMv1/v2: Move dfp_consume_identity() to common file
    BUG=b:148528713
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I5b0bbd553cbe4fc76478b1c89b0f3f391f074a27
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/usb_pd_alt_mode_dfp.c
+++ common/usb_pd_alt_mode_dfp.c
@@ -231,3 +231,31 @@ void dfp_consume_attention(int port, uint32_t *payload)
 	if (modep->fx->attention)
 		modep->fx->attention(port, payload);
 }
+
+void dfp_consume_identity(int port, int cnt, uint32_t *payload)
+{
+	int ptype = PD_IDH_PTYPE(payload[VDO_I(IDH)]);
+	struct pd_policy *pe = get_pd_policy(port);
+	size_t identity_size = MIN(sizeof(pe->identity),
+				   (cnt - 1) * sizeof(uint32_t));
+	pd_dfp_pe_init(port);
+	memcpy(pe->identity, payload + 1, identity_size);
+
+	switch (ptype) {
+	case IDH_PTYPE_AMA:
+/* Leave vbus ON if the following macro is false */
+#if defined(CONFIG_USB_PD_DUAL_ROLE) && defined(CONFIG_USBC_VCONN_SWAP)
+		/* Adapter is requesting vconn, try to supply it */
+		if (PD_VDO_AMA_VCONN_REQ(payload[VDO_I(AMA)]))
+			pd_try_vconn_src(port);
+
+		/* Only disable vbus if vconn was requested */
+		if (PD_VDO_AMA_VCONN_REQ(payload[VDO_I(AMA)]) &&
+				!PD_VDO_AMA_VBUS_REQ(payload[VDO_I(AMA)]))
+			pd_power_supply_reset(port);
+#endif
+		break;
+	default:
+		break;
+	}
+}
