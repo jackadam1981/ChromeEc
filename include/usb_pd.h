89fa63a606b57a042db0de2ff93180277d0a0c29
Sam Hurst <shurst@google.com>
2020-01-30 11:52:08 -0800

    TCPMv2: Fix Power and Data Swap states
    Initialize the PE_PRS_SRC_SNK_SEND_SWAP message's sender_response_timer
    after the PR_SWAP message is sent and retry sending the PR_SWAP message
    a maximum of 5 time if a WAIT response is received from the port partner.
    
    Refactor the PE_DRS_SEND_SWAP and PE_VCS_SEND_SWAP states so that
    message resends, in response to WAIT's, are handled locally in the state.
    
    BUG=chromium:1021235
    BRANCH=none
    TEST=make -j buildall
    	Manual tests: (connected a hatch device to an apple laptop)
    	1) Verified power role swap to snk and then to source using
    	   pd console command - pd 1 swap power
    	2) Verified data role swap to ufp and then to dfp using pd
    	   console command - pd 1 swap data
    	3) Verified vconn swap with pd console
    	   command - pd 1 swap vconn
    Signed-off-by: Sam Hurst <shurst@google.com>
    
    Change-Id: Ibb61735a0a544a96db260a0aa1fcb1f48bfdda71

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -228,6 +228,9 @@ enum pd_rx_errors {
 #define PD_T_SRC_DISCONNECT        (15*MSEC) /* 15ms */
 #define PD_T_VCONN_STABLE          (50*MSEC) /* 50ms */
 #define PD_T_DISCOVER_IDENTITY     (45*MSEC) /* between 40ms and 50ms */
+#define PD_T_PR_SWAP_WAIT          (100*MSEC) /* 100ms */
+#define PD_T_DR_SWAP_WAIT          (100*MSEC) /* 100ms */
+#define PD_T_VCONN_SWAP_WAIT       (100*MSEC) /* 100ms */
 
 /* number of edges and time window to detect CC line is not idle */
 #define PD_RX_TRANSITION_COUNT  3
