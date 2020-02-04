3229d30501f7cc93520a3c473af15e4a17346d36
Craig Hesling <hesling@chromium.org>
2020-02-03 21:12:05 -0800

    icetower: Add board
    BRANCH=icetower
    BUG=b:130296790
    TEST=none
    
    Change-Id: I944ac6b04ed4a77431a3fb37fd60d9f9671261db
    Signed-off-by: Craig Hesling <hesling@chromium.org>

---
+++ board/icetower/build.mk
@@ -0,0 +1,14 @@
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+
+# the IC is STmicro STM32H7A3
+CHIP:=stm32
+CHIP_FAMILY:=stm32h7
+CHIP_VARIANT:=stm32h7a
+
+board-y=board.o fpsensor_detect.o
+
+test-list-y=aes sha256 sha256_unrolled
