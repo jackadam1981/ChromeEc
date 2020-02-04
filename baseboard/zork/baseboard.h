176031c801146823248d13079d246324110f97af
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2020-02-04 14:19:40 +0800

    morphius: add PWM CH0 for Logo LED
    This patch add PWM channel 0 for Logo LED.
    
    BUG=none
    BRANCH=none
    TEST=make buildall
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: I513856dd7c223b5ee1f6f12fa9a1846f5f616e01

--- baseboard/zork/baseboard.h
+++ baseboard/zork/baseboard.h
@@ -254,12 +254,6 @@ enum temp_sensor_id {
 	TEMP_SENSOR_COUNT
 };
 
-enum pwm_channel {
-	PWM_CH_KBLIGHT = 0,
-	PWM_CH_FAN,
-	PWM_CH_COUNT
-};
-
 enum fan_channel {
 	FAN_CH_0 = 0,
 	/* Number of FAN channels */
