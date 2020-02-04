b4397e5dd5969bca054ad3e7d8f6b64228a61788
Vijay Hiremath <vijay.p.hiremath@intel.com>
2020-02-03 18:53:13 -0800

    USB-PD: Add host command to store early boot state
    This host command synchronizes the Coreboot and Kernel TCSS (Type-C Sub
    Systems) states by saving the Coreboot's early boot TCSS state of USB-C
    ports. When the Kernel boots, it determines the next TCSS state based on
    the Coreboot's early boot state available in EC.
    
    BUG=b:145688884
    BRANCH=none
    TEST=Manually tested on Volteer.
         Able to set the Kernel's TCSS state based on initial state of
         Coreboot's TCSS state.
    
    Change-Id: I09f69af3bf48b25642d29e071e4649f66095dc78
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/usb_pd_host_cmd.c
+++ common/usb_pd_host_cmd.c
@@ -26,6 +26,8 @@ struct ec_params_usb_pd_rw_hash_entry rw_hash_table[RW_HASH_ENTRIES];
 #endif /* CONFIG_COMMON_RUNTIME */
 
 #ifdef HAS_TASK_HOSTCMD
+/* Store early boot USB-PD Type-C Sub Systems state */
+static uint8_t tcss_state[CONFIG_USB_PD_PORT_MAX_COUNT];
 
 static enum ec_status hc_pd_ports(struct host_cmd_handler_args *args)
 {
@@ -353,6 +355,7 @@ static enum ec_status hc_usb_pd_control(struct host_cmd_handler_args *args)
 		r_v2->control_flags = get_pd_control_flags(p->port);
 		r_v2->cable_speed = get_tbt_cable_speed(p->port);
 		r_v2->cable_gen = get_tbt_rounded_support(p->port);
+		r_v2->tcss_state = tcss_state[p->port];
 
 		if (args->version == 1)
 			args->response_size = sizeof(*r_v1);
@@ -368,6 +371,19 @@ static enum ec_status hc_usb_pd_control(struct host_cmd_handler_args *args)
 DECLARE_HOST_COMMAND(EC_CMD_USB_PD_CONTROL,
 		     hc_usb_pd_control,
 		     EC_VER_MASK(0) | EC_VER_MASK(1) | EC_VER_MASK(2));
+
+static enum ec_status hc_usb_pd_set_tcss_st(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_usb_pd_set_tcss_state *p = args->params;
+
+	tcss_state[p->port] = p->tcss_state;
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_USB_PD_SET_TCSS_STATE,
+		     hc_usb_pd_set_tcss_st,
+		     EC_VER_MASK(0));
+
 #endif /* CONFIG_COMMON_RUNTIME */
 
 __overridable enum ec_pd_port_location board_get_pd_port_location(int port)
