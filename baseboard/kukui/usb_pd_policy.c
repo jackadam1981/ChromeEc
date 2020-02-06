136a45e5c2e0bcd088d9b8527978adc66612cfa0
Ting Shen <phoenixshen@google.com>
2020-02-06 15:46:35 +0800

    juniper: implement otg function
    - set otg limit to 5V/3.3A according to hw requirement
    - remove unused charger_is_sourcing_otg_power function
    
    BUG=b:148627490
    TEST=verify that the USB ports (both C and A) can supply up to 3A
    BRANCH=kukui
    
    Change-Id: I61b047f1d05e5428d4eef74c9e0af3a222449779
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- baseboard/kukui/usb_pd_policy.c
+++ baseboard/kukui/usb_pd_policy.c
@@ -49,7 +49,10 @@ int pd_set_power_supply_ready(int port)
 	/* Provide VBUS */
 	vbus_en = 1;
 
-	charger_enable_otg_power(1);
+	if (IS_ENABLED(VARIANT_KUKUI_CHARGER_ISL9238))
+		charger_set_otg_current_voltage(3300, 5000);
+	else
+		charger_enable_otg_power(1);
 
 	gpio_set_level(GPIO_EN_USBC_CHARGE_L, 1);
 	gpio_set_level(GPIO_EN_PP5000_USBC, 1);
@@ -74,7 +77,11 @@ void pd_power_supply_reset(int port)
 	if (prev_en)
 		pd_set_vbus_discharge(port, 1);
 
-	charger_enable_otg_power(0);
+	if (IS_ENABLED(VARIANT_KUKUI_CHARGER_ISL9238))
+		charger_set_otg_current_voltage(0, 0);
+	else
+		charger_enable_otg_power(0);
+
 	gpio_set_level(GPIO_EN_PP5000_USBC, 0);
 
 	/* notify host of power info change */
