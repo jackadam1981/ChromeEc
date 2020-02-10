b1434e3ba40d19dfbcbfb57bf76400cc5be271e4
Ayushee <ayushee.shah@intel.com>
2020-02-10 15:41:10 -0800

    usb_pd: Move USB4 functions to common file
    BUG=b:148528713
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I80c9b7569429bc54db08d78b5a6eee16780d6fe1
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -2931,9 +2931,14 @@ void pd_interrupt_handler_task(void *p)
 
 static void pd_send_enter_usb(int port, int *timeout)
 {
-	uint32_t usb4_payload = get_enter_usb_msg_payload(port);
+	uint32_t usb4_payload;
 	uint16_t header;
 	int res;
+
+	if (!IS_ENABLED(CONFIG_USB_PD_ALT_MODE_DFP))
+		return;
+
+	usb4_payload = get_enter_usb_msg_payload(port);
 
 	/*
 	 * TODO: Enable Enter USB for cables (SOP').
@@ -3836,7 +3841,8 @@ void pd_task(void *u)
 			 * Enter_USB if port partner and cable are
 			 * USB4 compatible.
 			 */
-			if (should_enter_usb4_mode(port)) {
+			if (IS_ENABLED(CONFIG_USB_PD_ALT_MODE_DFP) &&
+			    should_enter_usb4_mode(port)) {
 				pd_send_enter_usb(port, &timeout);
 				break;
 			}
@@ -4458,7 +4464,8 @@ void pd_task(void *u)
 			 * Enter_USB if port partner and cable are
 			 * USB4 compatible.
 			 */
-			if (should_enter_usb4_mode(port)) {
+			if (IS_ENABLED(CONFIG_USB_PD_ALT_MODE_DFP) &&
+			    should_enter_usb4_mode(port)) {
 				pd_send_enter_usb(port, &timeout);
 				break;
 			}
