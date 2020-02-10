29ad161eea144308826c168a9ae0d65554725ca3
Tim Wawrzynczak <twawrzynczak@chromium.org>
2020-02-10 13:14:36 -0700

    hatch: Add check for convertible SKUs in lid_angle_peripheral_enable
    Check for a convertible SKU before turning off the keyboard in the
    switch to tablet mode. See go/hatch-skus for the list.
    
    BUG=b:125936966
    BRANCH=firmware-hatch-12672.B
    TEST=On Kohaku, artificially force SKU to both convertible and
    non-convertible SKUs, switch to tablet mode, and then check whether
    Alt-Volup-R reboots the EC.
    
    Change-Id: Id29644c4e050705203b860324f14b1b87bc4ccf4
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2047630
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    (cherry picked from commit b6adca6689753bdbe798cd1e0a56f8a4df749445)

--- board/mushu/board.c
+++ board/mushu/board.c
@@ -1,9 +1,11 @@
+<<<<<<< HEAD   (9fdc2e akemi: Enable TI BQ40Z5 series FW workaround)
+=======
 /* Copyright 2019 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-/* Hatch board-specific configuration */
+/* Mushu board-specific configuration */
 
 #include "adc.h"
 #include "adc_chip.h"
@@ -12,6 +14,7 @@
 #include "cros_board_info.h"
 #include "driver/accel_bma2x2.h"
 #include "driver/accelgyro_bmi160.h"
+#include "driver/als_tcs3400.h"
 #include "driver/bc12/pi3usb9201.h"
 #include "driver/ppc/sn5s330.h"
 #include "driver/tcpm/anx7447.h"
@@ -29,11 +32,9 @@
 #include "power_button.h"
 #include "pwm.h"
 #include "pwm_chip.h"
-#include "stdbool.h"
 #include "spi.h"
 #include "switch.h"
 #include "system.h"
-#include "tablet_mode.h"
 #include "task.h"
 #include "temp_sensor.h"
 #include "thermal.h"
@@ -47,31 +48,10 @@
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
-static void check_reboot_deferred(void);
-DECLARE_DEFERRED(check_reboot_deferred);
-
 /* GPIO to enable/disable the USB Type-A port. */
 const int usb_port_enable[CONFIG_USB_PORT_POWER_SMART_PORT_COUNT] = {
 	GPIO_EN_USB_A_5V,
 };
-
-/*
- * We have total 30 pins for keyboard connecter {-1, -1} mean
- * the N/A pin that don't consider it and reserve index 0 area
- * that we don't have pin 0.
- */
-const int keyboard_factory_scan_pins[][2] = {
-		{-1, -1}, {0, 5}, {1, 1}, {1, 0}, {0, 6},
-		{0, 7}, {-1, -1}, {-1, -1}, {1, 4}, {1, 3},
-		{-1, -1}, {1, 6}, {1, 7}, {3, 1}, {2, 0},
-		{1, 5}, {2, 6}, {2, 7}, {2, 1}, {2, 4},
-		{2, 5}, {1, 2}, {2, 3}, {2, 2}, {3, 0},
-		{-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {-1, -1},
-		{-1, -1},
-};
-
-const int keyboard_factory_scan_pins_used =
-			ARRAY_SIZE(keyboard_factory_scan_pins);
 
 static void ppc_interrupt(enum gpio_signal signal)
 {
@@ -164,7 +144,6 @@ const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 			.addr_flags = PS8751_I2C_ADDR1_FLAGS,
 		},
 		.drv = &ps8xxx_tcpm_drv,
-		.flags = 0,
 	},
 };
 
@@ -203,17 +182,60 @@ static struct bmi160_drv_data_t g_bmi160_data;
 /* BMA255 private data */
 static struct accelgyro_saved_data_t g_bma255_data;
 
+/* TCS3400 private data */
+static struct als_drv_data_t g_tcs3400_data = {
+	.als_cal.scale = 1,
+	.als_cal.uscale = 0,
+	.als_cal.offset = 0,
+	.als_cal.channel_scale = {
+		.k_channel_scale = ALS_CHANNEL_SCALE(1.0), /* kc */
+		.cover_scale = ALS_CHANNEL_SCALE(1.0),     /* CT */
+	},
+};
+
+static struct tcs3400_rgb_drv_data_t g_tcs3400_rgb_data = {
+	.calibration.rgb_cal[X] = {
+		.offset = 0,
+		.scale = {
+			.k_channel_scale = ALS_CHANNEL_SCALE(1.0), /* kr */
+			.cover_scale = ALS_CHANNEL_SCALE(1.0)
+		},
+	},
+	.calibration.rgb_cal[Y] = {
+		.offset = 0,
+		.scale = {
+			.k_channel_scale = ALS_CHANNEL_SCALE(1.0), /* kg */
+			.cover_scale = ALS_CHANNEL_SCALE(1.0)
+		},
+	},
+	.calibration.rgb_cal[Z] = {
+		.offset = 0,
+		.scale = {
+			.k_channel_scale = ALS_CHANNEL_SCALE(1.0), /* kb */
+			.cover_scale = ALS_CHANNEL_SCALE(1.0)
+		},
+	},
+	.calibration.irt = INT_TO_FP(1),
+	.saturation.again = TCS_DEFAULT_AGAIN,
+	.saturation.atime = TCS_DEFAULT_ATIME,
+};
+
 /* Matrix to rotate accelrator into standard reference frame */
 static const mat33_fp_t base_standard_ref = {
+	{ 0, FLOAT_TO_FP(1), 0},
 	{ FLOAT_TO_FP(-1), 0, 0},
-	{ 0, FLOAT_TO_FP(-1), 0},
 	{ 0, 0, FLOAT_TO_FP(1)}
 };
 
+/*
+ * TODO(b/124337208): P0 boards don't have this sensor mounted so the rotation
+ * matrix can't be tested properly. This needs to be revisited after EVT to make
+ * sure the rotation matrix for the lid sensor is correct.
+ */
 static const mat33_fp_t lid_standard_ref = {
-	{ FLOAT_TO_FP(-1), 0, 0},
 	{ 0, FLOAT_TO_FP(-1), 0},
-	{ 0, 0, FLOAT_TO_FP(1)}
+	{ FLOAT_TO_FP(-1), 0, 0},
+	{ 0, 0, FLOAT_TO_FP(-1)}
 };
 
 struct motion_sensor_t motion_sensors[] = {
@@ -231,7 +253,7 @@ struct motion_sensor_t motion_sensors[] = {
 		.rot_standard_ref = &lid_standard_ref,
 		.min_frequency = BMA255_ACCEL_MIN_FREQ,
 		.max_frequency = BMA255_ACCEL_MAX_FREQ,
-		.default_range = 2, /* g, to support lid angle calculation. */
+		.default_range = 2, /* g, to support tablet mode */
 		.config = {
 			/* EC use accel for angle detection */
 			[SENSOR_CONFIG_EC_S0] = {
@@ -258,7 +280,7 @@ struct motion_sensor_t motion_sensors[] = {
 		.rot_standard_ref = &base_standard_ref,
 		.min_frequency = BMI160_ACCEL_MIN_FREQ,
 		.max_frequency = BMI160_ACCEL_MAX_FREQ,
-		.default_range = 4,  /* g, to meet CDD 7.3.1/C-1-4 reqs */
+		.default_range = 2, /* g, to support tablet mode  */
 		.config = {
 			[SENSOR_CONFIG_EC_S0] = {
 				.odr = 10000 | ROUND_UP_FLAG,
@@ -286,8 +308,52 @@ struct motion_sensor_t motion_sensors[] = {
 		.min_frequency = BMI160_GYRO_MIN_FREQ,
 		.max_frequency = BMI160_GYRO_MAX_FREQ,
 	},
+
+	[CLEAR_ALS] = {
+		.name = "Clear Light",
+		.active_mask = SENSOR_ACTIVE_S0_S3,
+		.chip = MOTIONSENSE_CHIP_TCS3400,
+		.type = MOTIONSENSE_TYPE_LIGHT,
+		.location = MOTIONSENSE_LOC_LID,
+		.drv = &tcs3400_drv,
+		.drv_data = &g_tcs3400_data,
+		.port = I2C_PORT_ALS,
+		.i2c_spi_addr_flags = TCS3400_I2C_ADDR_FLAGS,
+		.rot_standard_ref = NULL,
+		.default_range = 0x10000, /* scale = 1x, uscale = 0 */
+		.min_frequency = TCS3400_LIGHT_MIN_FREQ,
+		.max_frequency = TCS3400_LIGHT_MAX_FREQ,
+		.config = {
+			/* Run ALS sensor in S0 */
+			[SENSOR_CONFIG_EC_S0] = {
+				.odr = 1000,
+			},
+		},
+	},
+
+	[RGB_ALS] = {
+		/*
+		 * RGB channels read by CLEAR_ALS and so the i2c port and
+		 * address do not need to be defined for RGB_ALS.
+		 */
+		.name = "RGB Light",
+		.active_mask = SENSOR_ACTIVE_S0_S3,
+		.chip = MOTIONSENSE_CHIP_TCS3400,
+		.type = MOTIONSENSE_TYPE_LIGHT_RGB,
+		.location = MOTIONSENSE_LOC_LID,
+		.drv = &tcs3400_rgb_drv,
+		.drv_data = &g_tcs3400_rgb_data,
+		.rot_standard_ref = NULL,
+		.default_range = 0x10000, /* scale = 1x, uscale = 0 */
+	},
 };
 unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+/* ALS instances when LPC mapping is needed. Each entry directs to a sensor. */
+const struct motion_sensor_t *motion_als_sensors[] = {
+	&motion_sensors[CLEAR_ALS],
+};
+BUILD_ASSERT(ARRAY_SIZE(motion_als_sensors) == ALS_COUNT);
 
 /******************************************************************************/
 /* Physical fans. These are logically separate from pwm_channels. */
@@ -301,9 +367,9 @@ const struct fan_conf fan_conf_0 = {
 
 /* Default */
 const struct fan_rpm fan_rpm_0 = {
-	.rpm_min = 3200,
-	.rpm_start = 3200,
-	.rpm_max = 6500,
+	.rpm_min = 3100,
+	.rpm_start = 3100,
+	.rpm_max = 6900,
 };
 
 const struct fan_t fans[FAN_CH_COUNT] = {
@@ -323,8 +389,6 @@ const struct adc_t adc_channels[] = {
 		"TEMP_AMB", NPCX_ADC_CH0, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
 	[ADC_TEMP_SENSOR_2] = {
 		"TEMP_CHARGER", NPCX_ADC_CH1, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
-	[ADC_TEMP_SENSOR_3] = {
-		"TEMP_WIFI", NPCX_ADC_CH3, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
 };
 BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
@@ -332,18 +396,11 @@ const struct temp_sensor_t temp_sensors[] = {
 	[TEMP_SENSOR_1] = {.name = "Temp1",
 				 .type = TEMP_SENSOR_TYPE_BOARD,
 				 .read = get_temp_3v3_30k9_47k_4050b,
-				 .idx = ADC_TEMP_SENSOR_1,
-				 .action_delay_sec = 1},
+				 .idx = ADC_TEMP_SENSOR_1},
 	[TEMP_SENSOR_2] = {.name = "Temp2",
 				 .type = TEMP_SENSOR_TYPE_BOARD,
 				 .read = get_temp_3v3_30k9_47k_4050b,
-				 .idx = ADC_TEMP_SENSOR_2,
-				 .action_delay_sec = 1},
-	[TEMP_SENSOR_3] = {.name = "Temp3",
-				 .type = TEMP_SENSOR_TYPE_BOARD,
-				 .read = get_temp_3v3_30k9_47k_4050b,
-				 .idx = ADC_TEMP_SENSOR_3,
-				 .action_delay_sec = 1},
+				 .idx = ADC_TEMP_SENSOR_2},
 };
 BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 
@@ -357,16 +414,16 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 const static struct ec_thermal_config thermal_a = {
 	.temp_host = {
 		[EC_TEMP_THRESH_WARN] = 0,
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(65),
-		[EC_TEMP_THRESH_HALT] = C_TO_K(75),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(75),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(80),
 	},
 	.temp_host_release = {
 		[EC_TEMP_THRESH_WARN] = 0,
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(55),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(65),
 		[EC_TEMP_THRESH_HALT] = 0,
 	},
 	.temp_fan_off = C_TO_K(25),
-	.temp_fan_max = C_TO_K(55),
+	.temp_fan_max = C_TO_K(50),
 };
 
 struct ec_thermal_config thermal_params[TEMP_SENSOR_COUNT];
@@ -411,48 +468,18 @@ static void board_gpio_set_pp5000(void)
 
 }
 
-bool board_is_convertible(void)
-{
-	uint8_t sku_id = get_board_sku();
-	/* SKU ID of Kled : 1, 2, 3, 4 */
-	return (sku_id >= 1) && (sku_id <= 4);
-}
-
-static void board_update_sensor_config_from_sku(void)
-{
-	/*
-	 * There are two possible sensor configurations. Clamshell device will
-	 * not have any of the motion sensors populated, while convertible
-	 * devices have the BMI160 Accel/Gryo lid acceleration sensor.
-	 * If a new SKU id is used that is not in the threshold, then the
-	 * number of motion sensors will remain as ARRAY_SIZE(motion_sensors).
-	 */
-	if (board_is_convertible()) {
-		motion_sensor_count = ARRAY_SIZE(motion_sensors);
-		/* Enable gpio interrupt for base accelgyro sensor */
-		gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
-
-		CPRINTS("Motion Sensor Count = %d", motion_sensor_count);
-	} else {
-		motion_sensor_count = 0;
-		/* Device is clamshell only */
-		tablet_disable();
-		/* Base accel is not stuffed, don't allow line to float */
-		gpio_set_flags(GPIO_BASE_SIXAXIS_INT_L,
-			       GPIO_INPUT | GPIO_PULL_DOWN);
-	}
-}
-
 static void board_init(void)
 {
 	/* Initialize Fans */
 	setup_fans();
+	/* Enable gpio interrupt for base accelgyro sensor */
+	gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
+	/* Enable interrupt for the TCS3400 color light sensor */
+	gpio_enable_interrupt(GPIO_TCS3400_INT_ODL);
 	/* Enable HDMI HPD interrupt. */
 	gpio_enable_interrupt(GPIO_HDMI_CONN_HPD);
 	/* Select correct gpio signal for PP5000_A control */
 	board_gpio_set_pp5000();
-	/* Use sku_id to set motion sensor count */
-	board_update_sensor_config_from_sku();
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
@@ -466,34 +493,10 @@ void board_overcurrent_event(int port, int is_overcurrented)
 	gpio_set_level(GPIO_USB_C_OC_ODL, !is_overcurrented);
 }
 
-bool board_has_kb_backlight(void)
+bool board_is_convertible(void)
 {
-	uint8_t sku_id = get_board_sku();
-	/* SKU ID of Kled with KB backlight: 1, 2, 3, 4 */
-	return (sku_id >= 1) && (sku_id <= 4);
-}
+	const uint8_t sku = get_board_sku();
 
-__override uint32_t board_override_feature_flags0(uint32_t flags0)
-{
-	if (board_has_kb_backlight())
-		return flags0;
-	else
-		return (flags0 & ~EC_FEATURE_MASK_0(EC_FEATURE_PWM_KEYB));
+	return (sku == 255);
 }
-
-void all_sys_pgood_check_reboot(void)
-{
-	hook_call_deferred(&check_reboot_deferred_data, 3000 * MSEC);
-}
-
-__override void board_chipset_forced_shutdown(void)
-{
-	hook_call_deferred(&check_reboot_deferred_data, -1);
-}
-DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_forced_shutdown, HOOK_PRIO_DEFAULT);
-
-static void check_reboot_deferred(void)
-{
-	if (!gpio_get_level(GPIO_PG_EC_ALL_SYS_PWRGD))
-		system_reset(SYSTEM_RESET_MANUALLY_TRIGGERED);
-}
+>>>>>>> CHANGE (b6adca hatch: Add check for convertible SKUs in lid_angle_periphera)
