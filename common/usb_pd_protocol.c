233e346d6827ce437544b92aaf7fdfaca8dc5a1a
Vijay Hiremath <vijay.p.hiremath@intel.com>
2020-02-12 13:40:06 -0800

    TCPMv1/v2: Move pd_is_vbus_present() to common file
    BUG=b:148528713
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I5fb9bda868961008db858a46b7898b5c58cdd922
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -380,15 +380,6 @@ void pd_vbus_low(int port)
 }
 #endif
 
-int pd_is_vbus_present(int port)
-{
-#ifdef CONFIG_USB_PD_VBUS_DETECT_TCPC
-	return tcpm_get_vbus_level(port);
-#else
-	return pd_snk_is_vbus_provided(port);
-#endif
-}
-
 #ifdef CONFIG_USBC_VCONN
 static void set_vconn(int port, int enable)
 {
