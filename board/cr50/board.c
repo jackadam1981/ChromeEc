0a7edb33bf0dc315cfda81bbab958759f82a83fa
Namyoon Woo <namyoon@chromium.org>
2020-02-13 10:59:06 -0800

    restructure EC-EFS module
    - add ec_efs, which tracks the system boot mode.
    - add ec_comm.h header file for EC-EFS related functions.
    - revised vboot.h header file.
    
    BUG=b:141143112
    BRANCH=cr50
    TEST=none
    
    Change-Id: Iec1bf466b832bac5ad6be8a52304c1d699a38fb2
    Signed-off-by: Namyoon Woo <namyoon@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -9,6 +9,7 @@
 #include "common.h"
 #include "console.h"
 #include "dcrypto/dcrypto.h"
+#include "ec_comm.h"
 #include "ec_version.h"
 #include "endian.h"
 #include "extension.h"
@@ -596,8 +597,11 @@ void board_configure_deep_sleep_wakepins(void)
 	 */
 	gpio_set_wakepin(GPIO_TPM_RST_L, GPIO_HIB_WAKE_HIGH);
 
-	if (board_has_ec_cr50_comm_support())
+	if (board_has_ec_cr50_comm_support()) {
+		ec_efs_ready_for_sleep();
+
 		gpio_set_wakepin(GPIO_EC_PACKET_MODE_EN, GPIO_HIB_WAKE_HIGH);
+	}
 }
 
 static void deferred_tpm_rst_isr(void);
@@ -1038,6 +1042,12 @@ void board_reboot_ec(void)
 }
 DECLARE_DEFERRED(board_reboot_ec);
 
+void board_reboot_ec_deferred(int32_t usec_delay)
+{
+	if (usec_delay >= 0)
+		hook_call_deferred(&board_reboot_ec_data, usec_delay);
+}
+
 /*
  * This interrupt handler will be called if the RBOX key combo is detected.
  */
@@ -1134,6 +1144,8 @@ void assert_ec_rst(void)
 	/* Prevent bit bang interrupt storm. */
 	if (uart_bitbang_is_enabled())
 		task_disable_irq(bitbang_config.rx_irq);
+	else if (board_has_ec_cr50_comm_support())
+		ec_efs_reset();
 
 	wait_ec_rst(1);
 
