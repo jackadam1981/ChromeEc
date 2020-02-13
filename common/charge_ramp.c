6660e7e2ef092e344949bb06ea925e023ccbf54b
Aseda Aboagye <aaboagye@google.com>
2020-02-12 17:50:36 -0800

    chgramp: Don't ramp DTS suppliers above advertisement
    We shouldn't ramp above the current limit that the DTS specifies; the
    USB Type-C spec clearly shows what the limit is.  The existing code was
    allowing us to ramp up to 2.4A when a SuzyQable was connected to a
    chromebook.  This was causing issues on various devices/workstations.
    
    If a board needs more power than SuzyQable can provide, they ought to
    use a ServoV4 with a charger connected instead.
    
    BUG=chromium:770296,b:142033952,b:144198315
    BRANCH=hatch,nami,octopus
    TEST=Build and flash kohaku, plug in SuzyQable, verify that DUT draws no
    more than 1500mA.
    TEST=Repeat above test with a ServoV4 with 60W adapter plugged in,
    verify DUT can pull ~60W.
    
    Change-Id: Ife9f8d99862d61cf7d18722fdd6e19331a47e301
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>

--- common/charge_ramp.c
+++ common/charge_ramp.c
@@ -11,8 +11,6 @@
 #include "usb_charge.h"
 #include "util.h"
 
-#define TYPEC_DTS_RAMP_MAX 2400
-
 test_mockable int chg_ramp_allowed(int supplier)
 {
 	/* Don't allow ramping in RO when write protected. */
@@ -38,11 +36,7 @@ test_mockable int chg_ramp_max(int supplier, int sup_curr)
 {
 	switch (supplier) {
 	case CHARGE_SUPPLIER_TYPEC_DTS:
-		/*
-		 * Ramp DTS suppliers to advertised current or predetermined
-		 * limit, whichever is greater.
-		 */
-		return MAX(TYPEC_DTS_RAMP_MAX, sup_curr);
+		return sup_curr;
 #ifdef CONFIG_CHARGE_RAMP_HW
 	case CHARGE_SUPPLIER_PD:
 	case CHARGE_SUPPLIER_TYPEC:
