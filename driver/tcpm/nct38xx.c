86216c79f228372f0ea0ce256c166643717fc9a7
Denis Brockus <dbrockus@chromium.org>
2020-02-13 13:35:46 -0700

    nct38xx: fix sop in header
    The code was not gathering the frame data to send
    the SOP in the message header.  Added some comments
    to the routine and fixed the header.
    
    BUG=b:149418834
    BRANCH=none
    TEST=verify sop in header is correct
    
    Change-Id: I31f2fedd7afece8303113ca1e1e74bbaf91de666
    Signed-off-by: Denis Brockus <dbrockus@chromium.org>

--- driver/tcpm/nct38xx.c
+++ driver/tcpm/nct38xx.c
@@ -127,20 +127,36 @@ static int tcpci_nct38xx_get_message_raw(int port, uint32_t *payload, int *head)
 {
 	int rv, cnt, num_obj_byte;
 
+	/* Read the number of bytes received */
 	rv = tcpc_read(port, TCPC_REG_RX_BYTE_CNT, &cnt);
 
+	/* RX_BYTE_CNT includes 3 bytes for frame type and header */
 	if (rv != EC_SUCCESS || cnt < 3) {
 		rv = EC_ERROR_UNKNOWN;
 		goto clear;
 	}
 
+	/*
+	 * Read in RX information
+	 *   [0] byte count
+	 *   [1] frame
+	 *   [2-0x1F] header and data
+	 */
 	rv = tcpc_read_block(port, TCPC_REG_RX_BYTE_CNT, rxBuf, cnt + 1);
 	if (rv != EC_SUCCESS)
 		goto clear;
 
+	/* Point to the header for the caller */
 	*head = *(int *)&rxBuf[2];
+
+	/* Determine the number of objects by the header */
 	num_obj_byte = PD_HEADER_CNT(*head) * 4;
 
+	/* Fill in SOP type in bits 31 to 28 */
+	*head &= 0x0000ffff;
+	*head |= PD_HEADER_SOP(rxBuf[1] & 7);
+
+	/* Move the data over to the payload for the caller */
 	if (num_obj_byte) {
 		uint32_t *buf_ptr;
 
