ca3d72b693259de06db526d4fd64b495c8a098ec
Craig Hesling <hesling@chromium.org>
2020-02-14 00:47:48 -0800

    nucleo-h7a3zi: Separate into baseboard
    BRANCH=none
    BUG=none
    TEST=run on-device tests
    
    Change-Id: Idf295db0fa8969a62004a7c475cef48af74b1da3
    Signed-off-by: Craig Hesling <hesling@chromium.org>

---
+++ baseboard/nucleo-h7a3zi/baseboard.c
@@ -0,0 +1,21 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Nucleo-H7A3ZI baseboard setup */
+
+#include "gpio.h"
+#include "hooks.h"
+#include "registers.h"
+
+__overridable void button_event(enum gpio_signal signal)
+{
+}
+
+/* Initialize board. */
+static void baseboard_init(void)
+{
+	STM32_DBGMCU_CR |= BIT(0)|BIT(1)|BIT(2) | BIT(7)|BIT(8);
+}
+DECLARE_HOOK(HOOK_INIT, baseboard_init, HOOK_PRIO_FIRST);
