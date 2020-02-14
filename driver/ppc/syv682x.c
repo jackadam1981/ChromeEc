9a00124f3eb145981932ac403d7b125bf86ee1fe
Eric Herrmann <eherrmann@chromium.org>
2020-02-14 09:33:56 -0800

    syv682x: Create interrupt handler
    Add interrupt handler to SYV682. The 4 interrupts we take care of here
    are:
    1. HV OCP
    2. 5V OCP
    3. Thermal Shutdown
    4. FRS detected
    
    The first 3 are handled as overcurrent events, while the last calls the
    frs event in the PD policy engine.
    
    BUG=b:149531621
    BRANCH=none
    TEST=make buildall
    TEST=On Volteer Connect a passive USB C-to-A adapter to the USB4 DB.
    Short the VBUS and ground pins; make sure the OC event is seen by the EC
    with the C1 overcurrent console message.
    
    Change-Id: I61c1a6b962e60d813adab92fcbd21fbdf838f188
    Signed-off-by: Eric Herrmann <eherrmann@chromium.org>

--- driver/ppc/syv682x.c
+++ driver/ppc/syv682x.c
@@ -7,17 +7,21 @@
 #include "common.h"
 #include "console.h"
 #include "driver/ppc/syv682x.h"
+#include "hooks.h"
 #include "i2c.h"
+#include "system.h"
 #include "timer.h"
 #include "usb_charge.h"
 #include "usb_pd_tcpm.h"
 #include "usbc_ppc.h"
+#include "usb_pd.h"
 #include "util.h"
 
 #define SYV682X_FLAGS_SOURCE_ENABLED BIT(0)
 /* 0 -> CC1, 1 -> CC2 */
 #define SYV682X_FLAGS_CC_POLARITY BIT(1)
 #define SYV682X_FLAGS_VBUS_PRESENT BIT(2)
+static uint32_t irq_pending; /* Bitmask of ports signaling an interrupt. */
 static uint8_t flags[CONFIG_USB_PD_PORT_MAX_COUNT];
 
 #define SYV682X_VBUS_DET_THRESH_MV		4000
@@ -312,6 +316,38 @@ static int syv682x_dump(int port)
 	return EC_SUCCESS;
 }
 #endif /* defined(CONFIG_CMD_PPC_DUMP) */
+
+static void syv682x_handle_interrupt(int port)
+{
+	int regval;
+
+	/* Clear on read */
+	read_reg(port,SYV682X_STATUS_REG, &regval);
+
+	/* Handle OC and thermal shutdown the same */
+	if (regval & (SYV682X_STATUS_OC_HV | SYV682X_STATUS_OC_5V | SYV682X_STATUS_TSD))
+	{
+		pd_handle_overcurrent(port);
+	}
+}
+
+static void syv682x_irq_deferred(void)
+{
+        int i;
+        uint32_t pending = atomic_read_clear(&irq_pending);
+
+        for (i = 0; i < board_get_usb_pd_port_count(); i++)
+                if (BIT(i) & pending)
+                        syv682x_handle_interrupt(i);
+}
+DECLARE_DEFERRED(syv682x_irq_deferred);
+
+void syv682x_interrupt(int port)
+{
+        atomic_or(&irq_pending, BIT(port));
+        hook_call_deferred(&syv682x_irq_deferred_data, 0);
+}
+
 
 static int syv682x_init(int port)
 {
