f9eab3c22d8723a753e1542537e245bc41218353
Leifu Zhao <leifu.zhao@intel.com>
2020-02-14 10:20:52 +0800

    ish: infrastructure changes to support ish5.4 PM
    Infrastructure related changes to support enabling power management
    for ish5.4 on tgl rvp platform.
    
    BUG=b:149238813
    BRANCH=none
    TEST=ISH can successfully enter into D0i1/D0i2/D0i3 on tgl rvp.
    
    Signed-off-by: Leifu Zhao <leifu.zhao@intel.com>
    Change-Id: I50b6f1a4fe9c14f9479af2a2a438ec7395ec27a1

--- chip/ish/dma.c
+++ chip/ish/dma.c
@@ -37,9 +37,10 @@ static int dma_poll(uint32_t addr, uint32_t expected, uint32_t mask)
 
 void ish_dma_ocp_timeout_disable(void)
 {
-	uint32_t ctrl = OCP_AGENT_CONTROL;
-
-	OCP_AGENT_CONTROL = ctrl & OCP_RESPONSE_TO_DISABLE;
+	if (!IS_ENABLED(CONFIG_ISH_NEW_PM)) {
+		uint32_t ctrl = OCP_AGENT_CONTROL;
+		OCP_AGENT_CONTROL = ctrl & OCP_RESPONSE_TO_DISABLE;
+	}
 }
 
 static inline uint32_t interrupt_lock(void)
