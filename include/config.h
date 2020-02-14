59ac69d0fed110d6ff520da41c573a02e2c5c5d3
Keith Short <keithshort@chromium.org>
2020-02-14 14:31:18 -0700

    volteer: Update USB C1 reset for next build
    Update the GPIO assignment for the USB_C1_RT_RST_ODL signal for the next
    board build.
    
    BUG=b:144933528, b:148243971
    BRANCH=none
    TEST=make buildall
    TEST=Check unassigned board ID or board ID=0 uses legacy GPIO setting.
    Otherwise new GPIO setting is used.
    
    Change-Id: I4621e039e4461a4e10ab87bc2d4e000b5dcaa885
    Signed-off-by: Keith Short <keithshort@chromium.org>

--- include/config.h
+++ include/config.h
@@ -3936,6 +3936,9 @@
 #undef CONFIG_USBC_RETIMER_PS8802
 #undef CONFIG_USBC_RETIMER_PS8818
 
+/* Allow run-time configuration the Burnside Bridge driver structure */
+#undef CONFIG_USBC_RETIMER_INTEL_BB_RUNTIME_CONFIG
+
 /*
  * Adds an EC console command to erase the ANX7447 OCM flash.
  * Note: this is intended to be a temporary option and
