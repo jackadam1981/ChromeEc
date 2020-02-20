8e668021f3943162b141181cac814dd8b9fc48d9
eric <eric_kao@wistron.corp-partner.google.com>
2020-02-20 16:56:17 +0800

    [TEST ONLY] test
    BUG=none;
    TEST=none;
    Branch=none;
    
    Change-Id: I65c1916cbf8abd0d9a30dcf79756a6939d92713b

--- baseboard/volteer/baseboard.h
+++ baseboard/volteer/baseboard.h
@@ -234,7 +234,7 @@
 #define PD_MAX_POWER_MW		60000
 #define PD_MAX_CURRENT_MA	3000
 #define PD_MAX_VOLTAGE_MV	20000
-
+#define PD_PREFER_HIGH_VOLTAGE
 
 /* I2C Bus Configuration */
 #define CONFIG_I2C
