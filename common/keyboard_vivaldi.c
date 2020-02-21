41995f7effd1167657cffdc55600f763e0ecc2a1
Rajat Jain <rajatja@google.com>
2020-02-20 18:17:21 -0800

    common/keyboard: Start sending action codes by default (aka Vivaldi)
    Vivaldi is a new keyboard that allows individual boards to have
    additional or different top row keys and/or to reorder those keys.
    As part of enabling vivaldi, another feature that needs to be enabled
    is to start sending action codes instead of function codes for the top
    row of the keyboard.
    
    go/vivaldi-prd
    go/vivaldi-design
    go/vivaldi-fw-design
    
    With this patch:
     * All board begin to send action scancodes for the top row when top row
       keys are pressed.
     * EC responds to an EC command from the coreboot, that asks for the top
       row layout for the keyboard. Coreboot uses this to expose
       corresponding keycodes to the kernel.
     * Individual boards can customize the top row layout if needed.
    
    BUG=b:146501925
    TEST=Check on Jinlon the (new) expected scancodes are output from EC.
    BRANCH=firmware-hatch-12672.B
    
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Change-Id: I1f45ce6eee138a984f8d4caef1ec76c9538dd30b

---
+++ common/keyboard_vivaldi.c
@@ -0,0 +1,147 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Vivali Keyboard code for Chrome EC */
+
+#include "keyboard_8042_sharedlib.h"
+#include "keyboard_scan.h"
+#include "ec_commands.h"
+#include <host_command.h>
+#include <util.h>
+#include <hooks.h>
+
+/*
+ * Row Column info for Top row keys T1 - T15
+ * Ref: https://drive.google.com/corp/drive/folders/17UtVQ-AixnlQuicRPTp8t46HE-sT522E
+ */
+static const struct key {
+	uint8_t row;
+	uint8_t col;
+} vivaldi[MAX_TOP_ROW_KEYS] = {
+	{.row = 0, .col = 2},	/* T1 */
+	{.row = 3, .col = 2},	/* T2 */
+	{.row = 2, .col = 2},	/* T3 */
+	{.row = 1, .col = 2},	/* T4 */
+	{.row = 3, .col = 4},	/* T5 */
+	{.row = 2, .col = 4},	/* T6 */
+	{.row = 1, .col = 4},	/* T7 */
+	{.row = 2, .col = 9},	/* T8 */
+	{.row = 1, .col = 9},	/* T9 */
+	{.row = 0, .col = 4},	/* T10 */
+	{.row = 0, .col = 1},	/* T11 */
+	{.row = 1, .col = 5},	/* T12 */
+	{.row = 3, .col = 5},	/* T13 */
+	{.row = 0, .col = 9},	/* T14 */
+	{.row = 0, .col = 11},	/* T15 */
+};
+
+/* Scancodes for top row action keys */
+static const uint16_t action_scancodes[] = {
+	[TK_BACK] = SCANCODE_BACK,
+	[TK_FORWARD] = SCANCODE_FORWARD,
+	[TK_REFRESH] = SCANCODE_REFRESH,
+	[TK_FULLSCREEN] = SCANCODE_FULLSCREEN,
+	[TK_OVERVIEW] = SCANCODE_OVERVIEW,
+	[TK_VOL_MUTE] = SCANCODE_VOLUME_MUTE,
+	[TK_VOL_DOWN] = SCANCODE_VOLUME_DOWN,
+	[TK_VOL_UP] = SCANCODE_VOLUME_UP,
+	[TK_PLAY_PAUSE] = SCANCODE_PLAY_PAUSE,
+	[TK_NEXT_TRACK] = SCANCODE_NEXT_TRACK,
+	[TK_PREV_TRACK] = SCANCODE_PREV_TRACK,
+	[TK_SNIP] = SCANCODE_SNIP,
+	[TK_BRIGHTNESS_DOWN] = SCANCODE_BRIGHTNESS_DOWN,
+	[TK_BRIGHTNESS_UP] = SCANCODE_BRIGHTNESS_UP,
+	[TK_KBD_BKLIGHT_DOWN] = SCANCODE_KBD_BKLIGHT_DOWN,
+	[TK_KBD_BKLIGHT_UP] = SCANCODE_KBD_BKLIGHT_UP,
+	[TK_PRIVACY_SCRN_TOGGLE] = SCANCODE_PRIVACY_SCRN_TOGGLE,
+};
+
+static struct top_row_layout default_top_row = {
+	/* Default Chromeos keyboard top row layout */
+	.num_top_row_keys = 10,
+	.action_keys = {
+		TK_BACK,		/* T1 */
+		TK_FORWARD,		/* T2 */
+		TK_REFRESH,		/* T3 */
+		TK_FULLSCREEN,		/* T4 */
+		TK_OVERVIEW,		/* T5 */
+		TK_BRIGHTNESS_DOWN,	/* T6 */
+		TK_BRIGHTNESS_UP,	/* T7 */
+		TK_VOL_MUTE,		/* T8 */
+		TK_VOL_DOWN,		/* T9 */
+		TK_VOL_UP,		/* T10 */
+	},
+	.can_send_function_keys = false,
+};
+
+struct top_row_layout *vivaldi_top_row;
+
+static enum ec_status get_vivaldi_top_row(struct host_cmd_handler_args *args)
+{
+	struct ec_response_top_row_layout *resp = args->response;
+	struct top_row_layout *top_row = &resp->top_row;
+
+	if (vivaldi_top_row) {
+		memcpy(top_row, vivaldi_top_row, sizeof(*top_row));
+		args->response_size = sizeof(*resp);
+		return EC_RES_SUCCESS;
+	}
+	return EC_RES_ERROR;
+}
+DECLARE_HOST_COMMAND(EC_CMD_GET_KEYBD_TOP_ROW_LAYOUT, get_vivaldi_top_row,
+		     EC_VER_MASK(0));
+
+__overridable void board_set_vivaldi_top_row(void)
+{
+	vivaldi_top_row = &default_top_row;
+}
+
+static void vivaldi_init(void)
+{
+	uint8_t row, col, *mask;
+	uint8_t i;
+	enum action_key key;
+
+	/* Allow the boards to change the top row layout */
+	board_set_vivaldi_top_row();
+
+	if (!vivaldi_top_row || !vivaldi_top_row->num_top_row_keys) {
+		cprints(CC_KEYBOARD, "VIVALDI keybd disabled on board request");
+		return;
+	}
+
+	cprints(CC_KEYBOARD, "VIVALDI: Num top row keys = %u",
+		vivaldi_top_row->num_top_row_keys);
+
+	if (vivaldi_top_row->num_top_row_keys > MAX_TOP_ROW_KEYS ||
+	    vivaldi_top_row->num_top_row_keys < MIN_TOP_ROW_KEYS) {
+		cprints(CC_KEYBOARD, "VIVALDI: Error! Bad Config, disabling");
+		return;
+	}
+
+	for (i = 0; i < MAX_TOP_ROW_KEYS; i++) {
+
+		row = vivaldi[i].row;
+		col = vivaldi[i].col;
+		key = vivaldi_top_row->action_keys[i];
+		mask = keyscan_config.actual_key_mask + col;
+
+		if (i < vivaldi_top_row->num_top_row_keys && key) {
+
+			/* Enable the mask */
+			*mask |= (1 << row);
+
+			/* Populate the scancode */
+			scancode_set2[col][row] = action_scancodes[key];
+			cprints(CC_KEYBOARD,
+				"VIVALDI key-%u (r-%u, c-%u) = scancode-%X",
+				i, row, col, action_scancodes[key]);
+		} else {
+			/* Disable the mask */
+			*mask &= ~(1 << row);
+		}
+	}
+}
+DECLARE_HOOK(HOOK_INIT, vivaldi_init, HOOK_PRIO_DEFAULT);
