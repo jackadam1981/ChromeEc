27d597a7900a21a5a4bd140f9533a480e8e116f1
Rajat Jain <rajatja@google.com>
2020-02-20 18:17:21 -0800

    common/keyboard_vivaldi: Support for new Vivaldi keyboard
    Vivaldi is a new keyboard that allows individual boards to have
    additional or different top row keys and/or to reorder those keys.
    
    go/vivaldi-prd
    go/vivaldi-design
    
    Add code to provide an API that individual boards that use the vivaldi
    keyboard, that lets them define their board keyboard top row layout.
    
    Some scan codes have been taken from
    https://wiki.osdev.org/PS/2_Keyboard#Key_Codes.2C_Key_States_and_Key_Mappings
    and ones that did not exist, have been allotted from (seemingly) free
    holes in there.
    
    BUG=b:146501925
    TEST=Check on Jinlon the (new) expected scancodes are output from EC.
    BRANCH=firmware-hatch-12672.B
    
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Change-Id: Id6b854337958d99898175e7be7a9972938e32399

--- include/config.h
+++ include/config.h
@@ -2501,6 +2501,13 @@
 #undef CONFIG_KEYBOARD_SCANCODE_MUTABLE
 
 /*
+ * Include the Vivaldi keyboard code. The vivaldi is a newer keyboard that can
+ * have upto 15 top row keys (other than Esc and Power/Lock) and also allows
+ * OEMs felxibility to use these keys for different purposes (or reordering).
+ */
+#undef CONFIG_KEYBOARD_VIVALDI
+
+/*
  * Allow board-specific 8042 keyboard callback when a key state is changed.
  */
 #undef CONFIG_KEYBOARD_SCANCODE_CALLBACK
@@ -4802,6 +4809,11 @@
 /* The Matrix Keyboard Protocol depends on MKBP events. */
 #ifdef CONFIG_KEYBOARD_PROTOCOL_MKBP
 #define CONFIG_MKBP_EVENT
+#endif
+
+/* Vivaldi keyboard code needs to be able to change the keyboard scancodes */
+#ifdef CONFIG_KEYBOARD_VIVALDI
+#define CONFIG_KEYBOARD_SCANCODE_MUTABLE
 #endif
 
 /******************************************************************************/
