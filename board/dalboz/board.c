0ddf9a382a414fe3c5cc81bba6c05c094a71af8d
Edward Hill <ecgh@chromium.org>
2020-02-25 16:55:41 -0700

    dalboz: update GPIOs
    BUG=b:147297680
    BRANCH=none
    TEST=able to boot dalboz AP
    
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Change-Id: I4a3ec2a77292e48672f5c6143990c36b9a80b6a6

--- board/dalboz/board.c
+++ board/dalboz/board.c
@@ -22,26 +22,8 @@
 
 #include "gpio_list.h"
 
-/* These GPIOs moved. Temporarily detect and support the V0 HW. */
-enum gpio_signal GPIO_PCH_PWRBTN_L = GPIO_EC_FCH_PWR_BTN_L;
-enum gpio_signal GPIO_PCH_SYS_PWROK = GPIO_EC_FCH_PWROK;
-
 void board_update_sensor_config_from_sku(void)
 {
-	int data;
-
-	/*
-	 * If the CBI EEPROM is found on the battery I2C port then we are
-	 * running on V0 HW so re-map the GPIOs that moved.
-	 */
-	if ((system_get_sku_id() == 0x7fffffff)
-	    && (i2c_read8(I2C_PORT_BATTERY, I2C_ADDR_EEPROM_FLAGS, 0, &data)
-		== EC_SUCCESS)) {
-		ccprints("V0 HW detected");
-		GPIO_PCH_PWRBTN_L = GPIO_EC_FCH_PWR_BTN_L_V0;
-		GPIO_PCH_SYS_PWROK = GPIO_EC_FCH_PWROK_V0;
-	}
-
 	/* Enable Gyro interrupts */
 	gpio_enable_interrupt(GPIO_6AXIS_INT_L);
 }
@@ -52,20 +34,5 @@ const struct pwm_t pwm_channels[] = {
 		.flags = PWM_CONFIG_DSLEEP,
 		.freq = 100,
 	},
-	[PWM_CH_FAN] = {
-		.channel = 2,
-		.flags = PWM_CONFIG_OPEN_DRAIN,
-		.freq = 25000,
-	},
 };
 BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
-
-/* MFT channels. These are logically separate from pwm_channels. */
-const struct mft_t mft_channels[] = {
-	[MFT_CH_0] = {
-		.module = NPCX_MFT_MODULE_1,
-		.clk_src = TCKC_LFCLK,
-		.pwm_id = PWM_CH_FAN,
-	},
-};
-BUILD_ASSERT(ARRAY_SIZE(mft_channels) == MFT_CH_COUNT);
