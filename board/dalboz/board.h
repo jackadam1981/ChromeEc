6b9bec306dc8eea9199376444518b3bb92f6d5eb
Edward Hill <ecgh@chromium.org>
2020-02-25 16:55:41 -0700

    dalboz: update GPIOs
    BUG=b:147297680
    BRANCH=none
    TEST=able to boot dalboz AP
    
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Change-Id: I4a3ec2a77292e48672f5c6143990c36b9a80b6a6

--- board/dalboz/board.h
+++ board/dalboz/board.h
@@ -46,9 +46,11 @@
 #define GPIO_ENABLE_BACKLIGHT_L		GPIO_EC_EDP_BL_DISABLE
 #define GPIO_ENTERING_RW		GPIO_EC_ENTERING_RW
 #define GPIO_KBD_KSO2			GPIO_EC_KSO_02_INV
+#define GPIO_PCH_PWRBTN_L		GPIO_EC_FCH_PWR_BTN_L
 #define GPIO_PCH_RSMRST_L		GPIO_EC_FCH_RSMRST_L
 #define GPIO_PCH_SLP_S3_L		GPIO_SLP_S3_L
 #define GPIO_PCH_SLP_S5_L		GPIO_SLP_S5_L
+#define GPIO_PCH_SYS_PWROK		GPIO_EC_FCH_PWROK
 #define GPIO_PCH_WAKE_L			GPIO_EC_FCH_WAKE_L
 #define GPIO_POWER_BUTTON_L		GPIO_EC_PWR_BTN_ODL
 #define GPIO_S0_PGOOD			GPIO_S0_PWROK_OD
@@ -60,10 +62,6 @@
 
 #ifndef __ASSEMBLER__
 
-/* These GPIOs moved. Temporarily detect and support the V0 HW. */
-extern enum gpio_signal GPIO_PCH_PWRBTN_L;
-extern enum gpio_signal GPIO_PCH_SYS_PWROK;
-
 enum battery_type {
 	BATTERY_SMP,
 	BATTERY_LGC,
@@ -71,15 +69,8 @@ enum battery_type {
 	BATTERY_TYPE_COUNT,
 };
 
-enum mft_channel {
-	MFT_CH_0 = 0,
-	/* Number of MFT channels */
-	MFT_CH_COUNT,
-};
-
 enum pwm_channel {
 	PWM_CH_KBLIGHT = 0,
-	PWM_CH_FAN,
 	PWM_CH_COUNT
 };
 
