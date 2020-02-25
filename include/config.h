946b1cc62dbbbc0a062f133377c81b3c657d10e8
Devin Lu <devin.lu@quantatw.com>
2020-02-25 11:56:53 +0800

    Revert "sb_fw_update: Get rid of CONFIG_SB_FIRMWARE_UPDATE"
    This reverts commit d33eb02aef288b1eb52363c68c20b5b0087ad5f6.
    
    Reason for revert: Bring it back because we have a board need it.
    
    BUG=b:149894406
    BRANCH=none
    TEST=none
    
    Change-Id: I45f072d4991c72686ee380ed2cfd75336bdc38d0
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>

--- include/config.h
+++ include/config.h
@@ -3216,6 +3216,9 @@
  */
 #undef CONFIG_SAVE_VBOOT_HASH
 
+/* Enable smart battery firmware update driver */
+#undef CONFIG_SB_FIRMWARE_UPDATE
+
 /* Allow the board to use a GPIO for the SCI# signal. */
 #undef CONFIG_SCI_GPIO
 
