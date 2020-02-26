c16ed7e52114321324d6053dce323e24f5352a69
Dino Li <Dino.Li@ite.com.tw>
2020-02-26 11:38:43 +0800

    it8xxx2: lpc/espi: mapping host I/O cycle to h2ram section
    On it8xxx2 series, host I/O cycles are mapped to the first block
    (0x80080000~0x80080fff) at default, and it is adjustable.
    We should set the correct offset depends on the base address of
    H2RAM section, so EC will be able to receive/handle commands from
    host.
    
    BUG=b:133460224
    BRANCH=none
    TEST=not yet. just check the register setting is correct (I/O cycles
         are mapped to 0x80081000~0x80081fff) after EC initialization.
    
    Change-Id: Ib9085181917712bf735b83487720f12c8cd7ca31
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>

--- chip/it83xx/config_chip_it8xxx2.h
+++ chip/it83xx/config_chip_it8xxx2.h
@@ -67,6 +67,8 @@
 #define IT83XX_GPIO_1P8V_PIN_EXTENDED
 /* All GPIOs support interrupt on rising, falling, and either edge. */
 #define IT83XX_GPIO_INT_FLEXIBLE
+/* Remap host I/O cycles to base address of H2RAM section. */
+#define IT83XX_H2RAM_REMAPPING
 /* Enable detect type-c plug in interrupt. */
 #define IT83XX_INTC_PLUG_IN_SUPPORT
 /* Chip IT83202BX actually has TCPC physical port count. */
