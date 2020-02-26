9dad190b09bc87b33bfbb4cdbafa37836dc4bf64
David Huang <david.huang@quanta.corp-partner.google.com>
2020-02-26 16:04:14 +0800

    Kukui: Return BP_NOT_SURE when DFET status is off
    Do not return BP_NO when DFET status is off.
    Battery might need more charge time to wake up from shutdown.
    Return BP_NOT_SURE to keep charge battery.
    
    BUG=b:149971271
    BRANCH=jacuzzi
    TEST=Insert UVP/shutdown battery and check battery resume normally.
    
    Change-Id: I68841e4e6e0457711fc4f43e6346b54805b7217c
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>

--- baseboard/kukui/battery_smart.c
+++ baseboard/kukui/battery_smart.c
@@ -39,22 +39,20 @@ static enum battery_present battery_check_present_status(void)
 
 	/*
 	 * Check battery disconnect status. If we are unable to read battery
-	 * disconnect status, then return BP_NOT_SURE. Battery could be in ship
-	 * mode and might require pre-charge current to wake it up. BP_NO is not
-	 * returned here because charger state machine will not provide
-	 * pre-charge current assuming that battery is not present.
+	 * disconnect status or DFET is off, then return BP_NOT_SURE. Battery
+	 * could be in ship mode and might require pre-charge current to wake
+	 * it up. BP_NO is not returned here because charger state machine
+	 * will not provide pre-charge current assuming that battery is not
+	 * present.
 	 */
-	if (battery_get_disconnect_state() == BATTERY_DISCONNECT_ERROR)
+	if (battery_get_disconnect_state() != BATTERY_NOT_DISCONNECTED)
 		return BP_NOT_SURE;
 
 	/*
 	 * Ensure that battery is:
 	 * 1. Not in cutoff
-	 * 2. Not disconnected
-	 * 3. Initialized
 	 */
-	if (battery_is_cut_off() != BATTERY_CUTOFF_STATE_NORMAL ||
-	    battery_get_disconnect_state() != BATTERY_NOT_DISCONNECTED)
+	if (battery_is_cut_off() != BATTERY_CUTOFF_STATE_NORMAL)
 		return BP_NO;
 
 	return batt_pres;
