9eceedf991b70e59fe176b10db444bb2c614ee0a
Craig Hesling <hesling@chromium.org>
2020-02-29 21:44:31 -0800

    common: Add blink example
    Sometime you really just need a sanity check that
    the system is still ticking or that a new chip works.
    
    This is "the" rudimentary blink example.
    
    BRANCH=none
    BUG=none
    TEST=# Add CONFIG_BLINK to nucleo-h743zi board.h
         make BOARD=nucleo-h743zi
         # Flash to nucleo
         # Check lights are counting in binary
         # Check that button stops/starts the counting
    
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Change-Id: Idf2d2bb245bd261806d7202f1557ced477b483c1

---
+++ common/blink.c
@@ -0,0 +1,39 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* This is a sanity check program for boards that have LEDs and buttons. */
+
+#include "common.h"
+#include "gpio.h"
+#include "hooks.h"
+
+/* Initialize board. */
+static void blink_init(void)
+{
+	gpio_enable_interrupt(GPIO_BTN1);
+}
+DECLARE_HOOK(HOOK_INIT, blink_init, HOOK_PRIO_DEFAULT);
+
+static int blink_enable = 1;
+
+static void blink(void)
+{
+	if (blink_enable) {
+		static int leds;
+
+		gpio_set_level(GPIO_LED1, BIT(0) & leds);
+		gpio_set_level(GPIO_LED2, BIT(1) & leds);
+		gpio_set_level(GPIO_LED3, BIT(2) & leds);
+		leds++;
+	}
+}
+DECLARE_HOOK(HOOK_TICK, blink, HOOK_PRIO_DEFAULT);
+
+__override
+void button_event(enum gpio_signal signal)
+{
+	if (gpio_get_level(GPIO_BTN1))
+		blink_enable = !blink_enable;
+}
