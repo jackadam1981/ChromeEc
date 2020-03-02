ceed760fad30334aaa70757c798f3cd4bd8f10e3
Wai-Hong Tam <waihong@google.com>
2020-03-02 14:36:51 -0800

    sc7180, sdm845: Clear invalid request on the power-on/off check
    The power-on, power-off, and reset request is stored in the same flag.
    
    When a user calls 'apshutdown' in S5, the request is saved. When
    another event triggers the power-on sequence, like power button, the
    flag was still carried forward and then request to shutdown.
    
    The similar problem also happens on power-off.
    
    Should clear invalid request on the check to avoid this issue.
    
    BRANCH=None
    BUG=b:150481751
    TEST=Manually tested the scenario:
    dut-control power_state:on
    dut-control power_state:off
    dut-control power_state:off
    dut-control power_state:on
    
    Change-Id: Ica6033668845a839f01900e1c98b6c92e0a12bcb
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

--- power/sdm845.c
+++ power/sdm845.c
@@ -561,11 +561,12 @@ static uint8_t check_for_power_on_event(void)
 	if (power_request == POWER_REQ_ON) {
 		power_request = POWER_REQ_NONE;
 		return POWER_ON_BY_POWER_REQ_ON;
-	}
-
-	if (power_request == POWER_REQ_RESET) {
+	} else if (power_request == POWER_REQ_RESET) {
 		power_request = POWER_REQ_NONE;
 		return POWER_ON_BY_POWER_REQ_RESET;
+	} else {
+		/* Clear invalid request */
+		power_request = POWER_REQ_NONE;
 	}
 
 	/* check if system is already ON */
@@ -628,6 +629,9 @@ static uint8_t check_for_power_off_event(void)
 		 * in check_for_power_on_event() in S5.
 		 */
 		return POWER_OFF_BY_POWER_REQ_RESET;
+	} else {
+		/* Clear invalid request */
+		power_request = POWER_REQ_NONE;
 	}
 
 	/*
