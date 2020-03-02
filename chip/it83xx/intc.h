f161beafa0d42fbb048274aaecad4df85ef657f3
Ruibin Chang <ruibin.chang@ite.com.tw>
2020-03-02 13:32:30 +0800

    it83xx/adc: add voltage comparator feature
      Add voltage comparator feature.
    The H/L threshold voltage can be adjusted by case.
    
    BUG=b:149094481
    BRANCH=none
    TEST=on board it83xx_evb,
         external input 0v and 3v to ADC7 pin,
         the INT would be triggered and ADC7 read the
         correctly voltage.
    
    Change-Id: I59510b1c6bd38004ff06e0fcbd2a671e895d59e3
    Signed-off-by: Ruibin Chang <ruibin.chang@ite.com.tw>

--- chip/it83xx/intc.h
+++ chip/it83xx/intc.h
@@ -27,6 +27,9 @@ void pm5_ibf_interrupt(void);
 void lpcrst_interrupt(enum gpio_signal signal);
 void peci_interrupt(void);
 void adc_interrupt(void);
+#ifdef CONFIG_ADC_VOLTAGE_COMPARATOR
+void volt_comp_interrupt(void);
+#endif
 void i2c_interrupt(int port);
 #ifdef CONFIG_I2C_SLAVE
 void i2c_slv_interrupt(int port);
