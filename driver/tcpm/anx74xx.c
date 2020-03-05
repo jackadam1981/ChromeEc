1a04f29dc7ee9e23c2127613111621e663891fe0
Ting Shen <phoenixshen@google.com>
2020-03-05 16:55:34 +0800

    tcpm: fix incorrect pd message enum names
    CL:2001938 renamed SOPP/SOPPP to SOP_PRIME/SOP_PRIME_PRIME, but didn't
    change some affected code. This CL fixes the incorrect names.
    
    BUG=none
    TEST=`make BOARD=kappa` with TCPMV2 enabled
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I91526f5552a188e01338b72bc47125fb12ff53b2

--- driver/tcpm/anx74xx.c
+++ driver/tcpm/anx74xx.c
@@ -1007,7 +1007,7 @@ void anx74xx_tcpc_alert(int port)
 	if (reg & ANX74XX_REG_EXT_SOP)
 		msg_sop[port] = PD_MSG_SOP;
 	else if (reg & ANX74XX_REG_EXT_SOP_PRIME)
-		msg_sop[port] = PD_MSG_SOPP;
+		msg_sop[port] = PD_MSG_SOP_PRIME;
 #endif
 
 	/* Check for Hard Reset done bit */
@@ -1022,7 +1022,7 @@ void anx74xx_tcpc_alert(int port)
 
 #ifdef CONFIG_USB_PD_DECODE_SOP
 	if (reg & ANX74XX_REG_EXT_SOP_PRIME_PRIME)
-		msg_sop[port] = PD_MSG_SOPPP;
+		msg_sop[port] = PD_MSG_SOP_PRIME_PRIME;
 #endif
 
 	if (reg & ANX74XX_REG_EXT_HARD_RST) {
