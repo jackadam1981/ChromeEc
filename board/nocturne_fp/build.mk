2a073236ce1d6fe3b7180e086a85ad10f978ea88
Craig Hesling <hesling@chromium.org>
2020-03-07 02:58:54 -0800

    dartmonkey,bloonchipper: Enable ectool_servo
    Instead of enabling the servo SPI ectool specifically for nocturne_fp,
    create a config option that enables it in both dartmonkey and bloonchipper.
    
    The ectool_servo util was introduced in crrev.com/c/864164 .
    
    BRANCH=none
    BUG=none
    TEST=rm -rf build/{hatch,bloonchipper,dartmonkey,hatch_fp,nocturne_fp}
         make -O proj-{hatch,bloonchipper,dartmonkey,hatch_fp,nocturne_fp} -j7
         ls build/*/util/ectool_servo
         # Should only show fp board have the ectool_servo
    
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Change-Id: I5b3e61ddfad3c24c0c41536b5b054b5b34031353

--- board/nocturne_fp/build.mk
+++ board/nocturne_fp/build.mk
@@ -14,6 +14,7 @@ board-y=board.o fpsensor_detect.o
 
 # Utilities
 host-util-bin-y = stm32mon
+build-util-bin-y = ectool_servo
 
 # Do not build rsa test because this board uses RSA exponent 3 and the rsa test
 # will fail on device.
