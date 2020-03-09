2a6ab23f59ef3368e1146d543ef551bd7afb0b25
Craig Hesling <hesling@chromium.org>
2020-03-08 20:29:51 -0700

    compare_builds.sh: Changed to parallel operation
    This completely changes the way compare_build.sh operates.
    
    * The test checkouts are done in /tmp directories.
      This detaches the test environment from your local
      EC workspace and allows builds for all references to
      occur at the same time.
    * Builds for all board and all references happen in parallel,
      unless disabled from command-line args.
    * New arguments:
      -k yes  Allows the tmp dir to be preserved after compare
      -j 8    Number of jobs that will run concurrently (make -j)
      -p no   Forces only one EC ref to be built at a time.
              Although -j is always enforced, this option
              limits memory usage.
    
    BRANCH=none
    BUG=none
    TEST=time ./util/compare_build.sh
    TEST=time ./util/compare_build.sh -k yes -p no -j 1
    TEST=time ./util/compare_build.sh -k yes -p no -j 4
    TEST=time ./util/compare_build.sh -k yes -p no
    
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Change-Id: I3453f58a0ce08988e86572e6979f1a75a553f9b3

--- util/compare_build.sh
+++ util/compare_build.sh
@@ -13,6 +13,12 @@ DEFINE_string 'board' "nocturne_fp" 'Board to build (\"all\" for all boards)' \
               'b'
 DEFINE_string 'ref1' "HEAD" 'Git reference (commit, branch, etc)'
 DEFINE_string 'ref2' "HEAD^" 'Git reference (commit, branch, etc)'
+DEFINE_string 'keep' "no" 'Keep the temp directory around for inspection.' 'k'
+DEFINE_string 'jobs' "" 'Number of jobs to pass to make' 'j'
+# When compiling both refs for all boards, mem usage was larger than 32GB.
+# If you don't have more than 32GB, you probably don't want to run in parallel.
+DEFINE_string 'parallel' "yes" \
+                'Make both refs at the same time (extreme mem usage)' 'p'
 
 # Process commandline flags.
 FLAGS "${@}" || exit 1
@@ -30,68 +36,41 @@ BOARDS_TO_SKIP="${BOARDS_TO_SKIP//skip_boards = /}"
 BOARDS_TO_SKIP+=" cr50"
 
 # Can specify any valid git ref (e.g., commits or branches).
-OLD_REF="$(git rev-parse --short "${FLAGS_ref1}")"
-NEW_REF="$(git rev-parse --short "${FLAGS_ref2}")"
+# We need the long sha for fetching changes
+OLD_REF="$(git rev-parse "${FLAGS_ref1}")"
+NEW_REF="$(git rev-parse "${FLAGS_ref2}")"
 
-# Save current cute branch name
-SAVED_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-# If we are in a detached HEAD state, grab the exact commit hash
-if [[ "${SAVED_BRANCH}" == "HEAD" ]]; then
-  SAVED_BRANCH="$(git rev-parse HEAD)"
+parse_bool() {
+  local value="$1"
+
+  case "${value}" in
+    y|yes|true|t|1|on)
+      echo yes
+      ;;
+    n|no|false|f|0|off)
+      echo no
+      ;;
+  esac
+}
+
+KEEP_TEMP_DIR="$(parse_bool "${FLAGS_keep}")"
+PARALLEL="$(parse_bool "${FLAGS_parallel}")"
+
+MAKE_FLAGS=( )
+# Specify -j 1 for sequential
+if [[ -n "${FLAGS_jobs}" ]]; then
+  MAKE_FLAGS+=( "-j" "${FLAGS_jobs}" )
+else
+  MAKE_FLAGS+=( "-j" )
 fi
 
-on_exit() {
-  # Intentionally not removing TMP_DIR on failure so that it can be
-  # inspected.
-  git checkout "${SAVED_BRANCH}" >/dev/null 2>&1
-}
-trap on_exit EXIT
-
-do_build() {
-  local ref="$1"
-  local result_dir="$2"
-  local board="$3"
-  git checkout "${ref}" >/dev/null 2>&1
-  echo "Testing commit: $(git rev-parse --short HEAD)"
-  rm -rf "build/${board}"
-  # STATIC_VERSION makes sure the generated ec_version.h is constant.
-  # See util/getversion.sh.
-  make STATIC_VERSION=1 V=1 BOARD="${board}" -j >./build.out 2>&1
-  rm -rf "${result_dir}"
-  mv "build/${board}" "${result_dir}"
-  mv build.out "${result_dir}"
-}
-
-compare_board() {
-  local board="$1"
-  local old_ref="$2"
-  local new_ref="$3"
-  local tmp_dir="$4"
-
-  local old_build_dir="${tmp_dir}/build.${board}_${old_ref}"
-  local new_build_dir="${tmp_dir}/build.${board}_${new_ref}"
-
-  echo "BOARD: ${board}, ${old_build_dir}, ${new_build_dir}"
-  do_build "${old_ref}" "${old_build_dir}" "${board}"
-  do_build "${new_ref}" "${new_build_dir}" "${board}"
-
-  if ! diff "${old_build_dir}/ec.bin" "${new_build_dir}/ec.bin" >/dev/null 2>&1;
-  then
-    echo "ec.bin FAILURE"
-    exit 1
-  fi
-
-  rm -rf "${old_build_dir}"
-  rm -rf "${new_build_dir}"
-}
+BOARDS=( "${BOARD}" )
 
 if [[ "${BOARD}" == "all" ]]; then
-  BOARD=""
+  BOARDS=( )
   echo "Skipping boards: ${BOARDS_TO_SKIP}"
-  for b in board/*; do
-    b=${b//board\//}
+  for b in $(make print-boards); do
     skipped=0
-    echo "b: ${b}"
     for skip in ${BOARDS_TO_SKIP}; do
       if [[ "${skip}" == "${b}" ]]; then
         skipped=1
@@ -99,19 +78,92 @@ if [[ "${BOARD}" == "all" ]]; then
       fi
     done
     if [[ ${skipped} == 0 ]]; then
-      BOARD+=" ${b}"
+      BOARDS+=( "${b}" )
     fi
   done
 fi
 
-echo "BOARD: ${BOARD}"
+echo "BOARDS: ${BOARDS[*]}"
 
-TMP_DIR="$(mktemp -d)"
+TMP_DIR="$(mktemp -d -t compare_build.XXXX)"
 
-for board in ${BOARD}; do
-  compare_board "${board}" "${OLD_REF}" "${NEW_REF}" "${TMP_DIR}"
-done
+# We want make to initiate the builds for ref1 and ref2 so that a
+# single jobserver manages the process. We could do the entire compare
+# in make, but let's keep this makefile simple.
+echo "# Preparing Makefile"
+cat > "${TMP_DIR}/Makefile" <<HEREDOC
+ORIGIN ?= $(realpath .)
+CRYPTOC_DIR ?= $(realpath ../../third_party/cryptoc)
+BOARDS ?= ${BOARDS[*]}
 
-rm -rf "${TMP_DIR}"
+.PHONY: all
+all: build-${OLD_REF} build-${NEW_REF}
 
-echo "ec.bin MATCH"
+ec-%:
+	git clone \$(ORIGIN) \$@
+	git -C \$@ fetch origin \$(@:ec-%=%)
+	git -C \$@ checkout --quiet FETCH_HEAD
+
+build-%: ec-%
+	\$(MAKE) --no-print-directory -C \$(@:build-%=ec-%)                   \\
+		STATIC_VERSION=1                                              \\
+		CRYPTOCLIB=\$(CRYPTOC_DIR)                                    \\
+		\$(addprefix proj-,\$(BOARDS))
+	@printf "  MKDIR   %s\n" "\$@"
+	@mkdir -p \$@
+	@for b in \$(BOARDS); do	                                      \\
+		printf "  CP -l   '%s' to '%s'\n"                             \\
+			"\$(@:build-%=ec-%)/build/\$\$b/ec.bin"               \\
+                        "\$@/\$\$b-ec.bin";                                   \\
+		cp -l \$(@:build-%=ec-%)/build/\$\$b/ec.bin \$@/\$\$b-ec.bin; \\
+	done
+
+# So that make doesn't try to remove them
+ec-${OLD_REF}:
+ec-${NEW_REF}:
+HEREDOC
+
+
+build() {
+  echo make --no-print-directory -C "${TMP_DIR}" "${MAKE_FLAGS[@]}"  "$@"
+  make --no-print-directory -C "${TMP_DIR}" "${MAKE_FLAGS[@]}"  "$@"
+  return $?
+}
+
+echo "# Launching build. Cover your eyes."
+result=1
+if [[ "${PARALLEL}" == "yes" ]]; then
+  build "build-${OLD_REF}" "build-${NEW_REF}"
+  result=$?
+else
+  build "build-${OLD_REF}" && build "build-${NEW_REF}"
+  result=$?
+fi
+if [[ ${result} -ne 0 ]]; then
+  echo >&2
+  echo "# Failed to make one or more of the refs." >&2
+  exit 1
+fi
+echo
+
+echo "# Comparing Files"
+echo
+if diff -s "${TMP_DIR}/build-"{"${OLD_REF}","${NEW_REF}"}; then
+  echo "# ec.bin MATCH"
+  result=0
+else
+  echo "# ec.bin FAILURE"
+  result=1
+fi
+echo
+
+# Do keep in mind that temp directory take a few GB if all boards are built.
+if [[ "${KEEP_TEMP_DIR}" == "no" ]]; then
+  echo "# Removing temp directory"
+  rm -rf "${TMP_DIR}"
+else
+  echo "# Keeping temp directory around for your inspection."
+  echo "# ${TMP_DIR}"
+fi
+
+exit "${result}"
