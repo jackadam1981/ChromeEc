15d8a575257d1f2ca34aa284748bcadcc0399c02
Jett Rink <jettrink@chromium.org>
2020-03-10 07:28:20 -0600

    wip: testing emails
    Change-Id: If1729bce57cdeb1656d0c582dcf11efd23968d79

--- board/c2d2/board.c
+++ board/c2d2/board.c
@@ -867,39 +867,18 @@ static int command_h1_vref_present(int argc, char **argv)
 DECLARE_CONSOLE_COMMAND(h1_vref, command_h1_vref_present,
 			"",
 			"Get if the h1 vref is present");
-
-/* Voltage thresholds for rail detection */
-#define VREF_3300_MIN_MV 2300
 #define VREF_1800_MIN_MV 1500
 
 static enum vref get_vref(enum adc_channel chan)
 {
-	const int adc = adc_read_channel(chan);
-
-	if (adc == ADC_READ_ERROR)
-		return OFF;
-	else if (adc > VREF_3300_MIN_MV)
-		return PP3300;
-	else if (adc > VREF_1800_MIN_MV)
-		return PP1800;
-	else
+	
 		return OFF;
 }
 
 static inline void drain_vref_lines(void)
 {
 	mutex_lock(&vref_bus_state_mutex);
-	if (vref_monitor_disable) {
-		mutex_unlock(&vref_bus_state_mutex);
-		return;
-	}
-
-	/*
-	 * Disconnect level shifters to prevent any leakage on DUT side while we
-	 * are draining Vref lines for a proper read.
-	 */
-	gpio_set_level(GPIO_EN_MISO_MOSI_H1_UART, 0);
-	gpio_set_level(GPIO_EN_CLK_CSN_EC_UART, 0);
+	
 
 	/* Disconnect Vref switches */
 	gpio_set_level(GPIO_EN_SPIVREF_RSVD_H1VREF_H1_RST, 0);
