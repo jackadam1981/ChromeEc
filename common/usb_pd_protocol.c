c63dddfd57c9b3a98bb44c57a3413b39856bcc34
Ayushee <ayushee.shah@intel.com>
2020-03-10 13:44:08 -0700

    TCPMv1/v2: Move exit_supported_alt_mode function to common file
    BUG=b:151169925
    BRANCH=None
    TEST=Tested on Volteer:
         1. On connecting a DP dongle, able to enter into DP alternate mode
    	on reboot.
         2. On connecting a Thunderbolt dock, able to enter into
    	Thunderbolt-Compatible mode on reboot.
    
    Change-Id: Ifffd1a63b8bfd44ba4d03aa6fe8c609e9b40c0dc
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -2165,7 +2165,7 @@ static void exit_tbt_mode_sop_prime(int port)
 		   pd_get_polarity(port));
 }
 
-static void pd_vdm_send_state_machine(int port)
+void pd_vdm_send_state_machine(int port)
 {
 	int res;
 	uint16_t header;
@@ -2338,38 +2338,6 @@ void pd_dev_get_rw_hash(int port, uint16_t *dev_id, uint8_t *rw_hash,
 		memcpy(rw_hash, pd[port].dev_rw_hash, PD_RW_HASH_SIZE);
 }
 
-#if defined(CONFIG_POWER_COMMON) || defined(CONFIG_USB_PD_ALT_MODE_DFP)
-static void exit_supported_alt_mode(int port)
-{
-#ifdef CONFIG_USB_PD_ALT_MODE_DFP
-	int opos;
-
-	for (int i = 0; i < supported_modes_cnt; i++) {
-		opos = pd_alt_mode(port, supported_modes[i].svid);
-		if (opos > 0) {
-			CPRINTS("C%d Exiting ALT mode with SVID = 0x%x", port,
-				supported_modes[i].svid);
-
-			if (!pd_dfp_exit_mode(port, supported_modes[i].svid,
-					      opos)) {
-				/*
-				 * In case of multiple alternate mode entry,
-				 * we need to exit from all the entered
-				 * alternate modes.
-				 */
-				continue;
-			}
-
-			pd_send_vdm(port, supported_modes[i].svid,
-				    CMD_EXIT_MODE | VDO_OPOS(opos), NULL, 0);
-			/* Wait for an ACK from port-partner */
-			pd_vdm_send_state_machine(port);
-		}
-	}
-#endif /* CONFIG_USB_PD_ALT_MODE_DFP */
-}
-#endif /* CONFIG_POWER_COMMON */
-
 #ifdef CONFIG_POWER_COMMON
 static void handle_new_power_state(int port)
 {
@@ -2379,7 +2347,8 @@ static void handle_new_power_state(int port)
 		 * The SoC will negotiate the alternate mode again when
 		 * it boots up.
 		 */
-		exit_supported_alt_mode(port);
+		if (IS_ENABLED(CONFIG_USB_PD_ALT_MODE_DFP))
+			pd_exit_supported_alt_mode(port);
 	}
 	/* Ensure mux is set properly after chipset transition */
 	set_usb_mux_with_current_data_role(port);
@@ -3117,7 +3086,7 @@ void pd_task(void *u)
 
 #if defined(CONFIG_USB_PD_ALT_MODE_DFP)
 		if (evt & PD_EVENT_SYSJUMP) {
-			exit_supported_alt_mode(port);
+			pd_exit_supported_alt_mode(port);
 			notify_sysjump_ready(&sysjump_task_waiting);
 
 		}
