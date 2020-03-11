4936d6a5754937bc4f5d2ce2fe929bc47a0ee493
Sam Hurst <shurst@google.com>
2020-03-11 10:48:18 -0700

    TCPMv2: Use a DPM_REQUEST message to trigger Port Discovery
    On reboot, while charging from a dongle, send a DPM_REQUEST
    message to trigger a Port Discover to enter an Alt Mode.
    
    BUG=b:149662829
    BRANCH=none
    TEST=make -j buildall
    	Manual tests:
    	Tested with an apple doc.
    	Should be tested on several other docs.
    Signed-off-by: Sam Hurst <shurst@google.com>
    
    Change-Id: I058c4717758792c77c02c26a74f2d9e9c4954ba0

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -889,6 +889,20 @@ static bool common_src_snk_dpm_requests(int port)
 					DPM_REQUEST_SOFT_RESET_SEND);
 		set_state_pe(port, PE_SEND_SOFT_RESET);
 		return true;
+	} else if (PE_CHK_DPM_REQUEST(port,
+					DPM_REQUEST_PORT_DISCOVERY)) {
+		PE_CLR_DPM_REQUEST(port,
+					DPM_REQUEST_PORT_DISCOVERY);
+		PE_CLR_FLAG(port, PE_FLAGS_DISCOVER_PORT_IDENTITY_DONE);
+		/*
+		 * Clear counters and reset timer to trigger a
+		 * port discovery.
+		 */
+		pe[port].dr_swap_attempt_counter = 0;
+		pe[port].discover_port_identity_counter = 0;
+		pe[port].discover_port_identity_timer = get_time().val +
+						PD_T_PORT_DISCOVERY_DELAY;
+		return true;
 	}
 
 	return false;
--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -1887,19 +1887,11 @@ static void tc_attached_snk_entry(const int port)
 static void tc_attached_snk_run(const int port)
 {
 #ifdef CONFIG_USB_PE_SM
-	/*
-	 * On device power ON, when charging from a Dock that has DP, enter any
-	 * alt modes by briefly transitioning the Unattached.SNK state. This has
-	 * no impact when charging from a regular charger.
-	 *
-	 * TODO(b/149662829): Implement a better solution for re-entering
-	 * alt modes after a reboot.
-	 */
+	/* On device power ON, enter any available alt modes after a reboot. */
 	if (TC_CHK_FLAG(port, TC_FLAGS_POWER_STATE_CHANGE) &&
 				chipset_in_state(CHIPSET_STATE_ON)) {
 		TC_CLR_FLAG(port, TC_FLAGS_POWER_STATE_CHANGE);
-		set_state_tc(port, TC_UNATTACHED_SNK);
-		return;
+		pe_dpm_request(port, DPM_REQUEST_PORT_DISCOVERY);
 	}
 
 	/*
