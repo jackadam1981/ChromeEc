8ba139511da36459190c52676cfe321f1c806a48
Shecky Lin <sheckylin@chromium.org>
2020-03-11 15:15:46 +0800

    i2c_hid_touchpad: Add basic structs and descriptors
    This is part of a chain to add a common I2C HID interface for
    touchpads.
    
    The CL layouts basic struct, defines and HID descriptors that
    would be used by an I2C HID touchpad implementation.
    
    BUG=none
    BRANCH=none
    TEST=Build and run on MCU to forward touchpad events to host
         through I2C HID.
    
    Change-Id: I89108f0cdce1a9a11bf1e26e43c6662959c7184e
    Signed-off-by: Shecky Lin <sheckylin@chromium.org>

--- include/config.h
+++ include/config.h
@@ -2240,6 +2240,9 @@
  */
 #undef CONFIG_ITE_FLASH_SUPPORT
 
+/* Support I2C HID touchpad interface. */
+#undef CONFIG_I2C_HID_TOUCHPAD
+
 /*****************************************************************************/
 /* IPI configuration.  Support mt_scp only for now. */
 
