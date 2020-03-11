0b08ad75979ac48ee8d6e76e830a9acda6b389d7
Shecky Lin <sheckylin@chromium.org>
2020-03-11 15:30:13 +0800

    i2c_hid_touchpad: Add function to prepare new input reports
    This is part of a chain to add a common I2C HID interface for
    touchpads.
    
    The CL adds a function to put a new input report in the buffer
    which would be sent next time when the host tries to read one.
    
    BUG=none
    BRANCH=none
    CQ-DEPEND=CL:2094928
    TEST=Build and run on MCU to forward touchpad events to host
         through I2C HID.
    
    Change-Id: I321054f73155da03d951a4d586dd316caf37f0fc
    Signed-off-by: Shecky Lin <sheckylin@chromium.org>

--- include/i2c_hid_touchpad.h
+++ include/i2c_hid_touchpad.h
@@ -56,5 +56,13 @@ void i2c_hid_touchpad_init(void);
  */
 int i2c_hid_touchpad_process(int len, uint8_t *buffer,
 			     void (*send_response)(int len), uint8_t *data);
+/**
+ * Compile an (outgoing) HID input report for an (incoming) touchpad event
+ *
+ * The compiled report would be sent next time when the host requests one.
+ *
+ * @param touchpad_event	Touchpad event data
+ */
+void i2c_hid_compile_report(struct touchpad_event *event);
 
 #endif /* __CROS_EC_I2C_HID_TOUCHPAD_H */
