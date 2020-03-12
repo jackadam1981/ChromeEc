3416c65d31e7bd7729b5f4161363e8588b0997b3
Pi-Hsun Shih <pihsun@chromium.org>
2020-03-12 17:29:53 +0800

    WIP: cros-ec-regulator
    BUG=b:149274957
    TEST=build
    
    Change-Id: I23bcefd64b41c36dd62f172a54f56d98243ebb04
    Signed-off-by: Pi-Hsun Shih <pihsun@chromium.org>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -6140,6 +6140,54 @@ struct ec_response_keybd_config {
 } __ec_align1;
 
 /*****************************************************************************/
+/* voltage regulator controls TODO */
+/*
+ * Enable / disable the regulator ... TODO
+ */
+#define EC_CMD_REGULATOR_ENABLE 0x012B
+
+struct ec_params_regulator_enable {
+	uint32_t index;
+	uint8_t enable;
+} __ec_align4;
+/*
+ * Query if the regulator is enabled ... TODO
+ */
+#define EC_CMD_REGULATOR_IS_ENABLED 0x012C
+
+struct ec_params_regulator_is_enabled {
+	uint32_t index;
+} __ec_align4;
+
+struct ec_response_regulator_is_enabled {
+	uint8_t enabled;
+} __ec_align1;
+
+/*
+ * Set voltages by selector ... TODO
+ */
+#define EC_CMD_REGULATOR_SET_VOLTAGE 0x012D
+
+struct ec_params_regulator_set_voltage {
+	uint32_t index;
+	uint32_t min_uV;
+	uint32_t max_uV;
+} __ec_align4;
+
+/*
+ * Get voltages by selector ... TODO
+ */
+#define EC_CMD_REGULATOR_GET_VOLTAGE 0x012E
+
+struct ec_params_regulator_get_voltage {
+	uint32_t index;
+} __ec_align4;
+
+struct ec_response_regulator_get_voltage {
+	uint32_t voltage_uV;
+} __ec_align4;
+
+/*****************************************************************************/
 /* The command range 0x200-0x2FF is reserved for Rotor. */
 
 /*****************************************************************************/
