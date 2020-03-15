f83fef632fd488c4135d8fd612805a2562ec3348
Craig Hesling <hesling@chromium.org>
2020-03-15 13:54:57 -0700

    stm32: Extract common hw types in hwtypes.h
    This brings no functional change.
    
    BRANCH=none
    BUG=b:130296790
    TEST=compare_builds -b fp
    
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Change-Id: Ic64210356f55e99c4e07bdf0ee2ea5ca6a5f26f5

---
+++ chip/stm32/hwtypes.h
@@ -0,0 +1,43 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_HWTYPES_H
+#define __CROS_EC_HWTYPES_H
+
+/*
+ * This file shall not include other headers that could circle
+ * back to common.h or config.h.
+ */
+
+enum clock_osc {
+	OSC_HSI = 0,	/* High-speed internal oscillator */
+	OSC_CSI,	/* Multi-speed internal oscillator: NOT IMPLEMENTED */
+	OSC_HSE,	/* High-speed external oscillator: NOT IMPLEMENTED */
+	OSC_PLL,	/* PLL */
+};
+
+enum voltage_scale {
+	VOLTAGE_SCALE0 = 0,
+	VOLTAGE_SCALE1,
+	VOLTAGE_SCALE2,
+	VOLTAGE_SCALE3,
+	VOLTAGE_SCALE_COUNT,
+};
+
+enum freq {
+	FREQ_1KHZ   = 1000,
+	FREQ_32KHZ  = 32  * FREQ_1KHZ,
+	FREQ_1MHZ   = 1000000,
+	FREQ_2MHZ   = 2   * FREQ_1MHZ,
+	FREQ_16MHZ  = 16  * FREQ_1MHZ,
+	FREQ_64MHZ  = 64  * FREQ_1MHZ,
+	FREQ_140MHZ = 140 * FREQ_1MHZ,
+	FREQ_200MHZ = 200 * FREQ_1MHZ,
+	FREQ_280MHZ = 280 * FREQ_1MHZ,
+	FREQ_400MHZ = 400 * FREQ_1MHZ,
+	FREQ_480MHZ = 480 * FREQ_1MHZ,
+};
+
+#endif /* __CROS_EC_HWTYPES_H */
