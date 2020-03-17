ad89a9acc2cd1d514d624d580a6d278ede237401
Vijay Hiremath <vijay.p.hiremath@intel.com>
2020-03-17 15:31:24 -0700

    USB-C: Determine port partner data role based on PD data role
    Current code uses the CC lines to find out the data role of a
    part partner, however when the PD based data role swap happens
    DR_swap command has no effect on the CC lines. Hence need to
    use PD's data role to determine port partner's data role.
    
    Reference: USB Type-C cable and connector specification, Release 2.0
    4.5.1.4.2 USB PD-based Power Role, Data Role and VCONN Swapping
    Table 4-12 USB PD Swapping Port Behavior Summary
    
    BUG=b:151653348
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I237d5c3f88096f4bd2d043ab823c98fe47686fa9
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/usb_common.c
+++ common/usb_common.c
@@ -226,19 +226,19 @@ enum pd_cc_states pd_get_cc_state(
 	return PD_CC_NONE;
 }
 
-/**
- * This function checks the current CC status of the port partner
- * and returns true if the attached partner is UFP.
+/*
+ * This function checks the host's data role, if the host is DFP then the
+ * attached port partner is UFP.
  */
 bool pd_partner_is_ufp(int port)
 {
-	enum pd_cc_states cc_state = pd_get_task_cc_state(port);
-
-	return cc_state == PD_CC_UFP_ATTACHED ||
-		cc_state == PD_CC_UFP_DEBUG_ACC ||
-		cc_state == PD_CC_UFP_AUDIO_ACC;
+	return pd_get_data_role(port) == PD_ROLE_DFP;
 }
 
+/**
+ * This function checks the current CC status of the port partner
+ * and returns true if the attached partner is debug accessory.
+ */
 bool pd_is_debug_acc(int port)
 {
 	enum pd_cc_states cc_state = pd_get_task_cc_state(port);
