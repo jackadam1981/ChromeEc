c6c6f119f8fc3856dd7951fb57585833a434a254
Ting Shen <phoenixshen@google.com>
2020-03-17 16:20:07 +0800

    WIP: asurada: setup gpio alt functions and interrupts
    BUG=b:150341271
    TEST=make
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I5ccc96c26369e75fdeb20d5a7f0e565a28605346

--- board/asurada/board.h
+++ board/asurada/board.h
@@ -13,6 +13,7 @@
 #define CONFIG_LOW_POWER_S0
 #define CONFIG_POWER_BUTTON
 #define CONFIG_PWM
+#define CONFIG_WP_ACTIVE_HIGH
 
 /* Battery */
 #define CONFIG_BATTERY_SMART
@@ -53,6 +54,9 @@
 #define CONFIG_CMD_STACKOVERFLOW
 
 /* Sensor */
+#define CONFIG_GMR_TABLET_MODE
+#define CONFIG_TABLET_MODE
+#define GMR_TABLET_MODE_GPIO_L GPIO_TABLET_MODE_L
 
 /* UART */
 #undef CONFIG_UART_TX_BUF_SIZE
@@ -73,8 +77,14 @@ enum pwm_channel {
 };
 
 enum adc_channel {
-	ADC_BOARD_ID_0,
-	ADC_BOARD_ID_1,
+	ADC_TEMP_SENSOR_SUBPMIC, /* ADC 0 */
+	ADC_BOARD_ID_0,          /* ADC 1 */
+	ADC_BOARD_ID_1,          /* ADC 2 */
+	ADC_TEMP_SENSOR_AMB,     /* ADC 3 */
+	ADC_TEMP_SENSOR_CHARGER, /* ADC 5 */
+	ADC_CHARGER_PMON,        /* ADC 6 */
+	ADC_TEMP_SENSOR_AP,      /* ADC 7 */
+
 	/* Number of ADC channels */
 	ADC_CH_COUNT,
 };
