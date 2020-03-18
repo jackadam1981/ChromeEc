c7637864042149e4fc0cc9f8cb82969adcc4cae2
Sam Hurst <shurst@google.com>
2020-03-18 13:31:55 -0700

    servo_v4.1: Initial checkin of firmware
    BRANCH=none
    BUG=b:146793000
    TEST=make -j buildall
    
    Change-Id: I68f94c5110210c134e7ff7212d6ccc0854413457
    Signed-off-by: Sam Hurst <shurst@google.com>

---
+++ board/servo_v41/ro_files.c
@@ -0,0 +1,12 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifdef SECTION_IS_RO
+#include "ccd_measure_sbu.c"
+#include "chg_control.c"
+#include "ina231s.c"
+#include "pathsel.c"
+#include "usb_pd_policy.c"
+#endif /* SECTION_IS_RO */
