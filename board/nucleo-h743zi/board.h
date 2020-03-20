73c1a13e2e766e3e4b473cd18c805d30ce411415
Craig Hesling <hesling@chromium.org>
2020-03-20 11:48:18 -0700

    nucleo-h743zi: Enable button in blink example
    BRANCH=none
    BUG=none
    TEST=make BOARD=nucleo-h743zi
         # Flash to nucleo
         # Check lights are counting in binary
         # Check that button stops/starts the counting
    
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Change-Id: I8c4b5c4439bae9ebb7f21d2d9b80890db1763982

--- board/nucleo-h743zi/board.h
+++ board/nucleo-h743zi/board.h
@@ -25,9 +25,12 @@
 #define CONFIG_RESTRICTED_CONSOLE_COMMANDS
 
 /*
- * Enable the blink example that exercises the LEDs.
+ * Enable the blink example that exercises the LEDs (and button).
  */
 #define CONFIG_BLINK
 #define CONFIG_BLINK_LEDS    GPIO_LED1, GPIO_LED2, GPIO_LED3
+#define CONFIG_BLINK_BTN     GPIO_BTN1
+#define CONFIG_BLINK_BTN_FN  button_event
+#define CONFIG_BLINK_BTN_ACT 1
 
 #endif /* __BOARD_H */
