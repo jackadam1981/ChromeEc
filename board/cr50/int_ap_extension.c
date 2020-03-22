f4bbd4a0dfef16f87064ada05f6e0e0af347fa0c
Namyoon Woo <namyoon@google.com>
2020-03-21 21:17:28 -0700

    Use a long pulse of INT_AP_L for SPS
    This patch adds a feature to extend each level of GPIO_INT_AP_L at
    least for 100 microseconds. The assertion (low GPIO_INT_AP_L)
    duration might be shorter only if AP asserts a SPS CS before
    INT_AP_L deassertion, because it means means AP recognized
    GPIO_INT_AP_L assertion already.
    
    This patch increases the flash usage by 280 bytes.
    
    BUG=b:148691139
    TEST=Tested on atlas and helios.
    1. Minimum pulse length of INT_AP_L assertion and deassertion
    observed in logic analyzer were 110 usec and 152 usec.
    
    2. measured the depthcharge exit timestamp and cr50 flash time with
    or without INT_AP pulse extended to 100 usec, on atlas and helios:
    -----------------+-------------------+------------------
                     | atlas             | helios
    -----------------+-------------------+------------------
    boot (sec)       |  1.398 ->  1.402  |  1.004 ->  1.011
    cr50 flash (sec) | 10.800 -> 14.609  | 16.024 -> 16.466
    -----------------+-------------------+------------------
    
    3. With 100 usec long INT_AP pulse, checked trunks_client
    regression_test, stress_test and ext_command_test runs good.
    Checked dmesg and found no TPM errors through all tests.
    
      $ trunks_client --regression_test
      $ trunks_client --stress_test
      $ trunks_client --ext_command_test
    
    4.checked no character loss during uart_stress_tester.
    
      $ uart_stress_tester.py -c -t 600 /dev/ttyUSB2 /dev/ttyUSB1
    
    Signed-off-by: Namyoon Woo <namyoon@google.com>
    Change-Id: Ie74b236bc5352e9fc21fe600c12946e50955160a

---
+++ board/cr50/int_ap_extension.c
@@ -0,0 +1,133 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * INT_AP_L extension
+ */
+
+#include "config.h"
+#include "console.h"
+#include "gpio.h"
+#include "registers.h"
+#include "stdbool.h"
+#include "task.h"
+#include "timer.h"
+
+/*
+ * Minimum time length (in microseconds) of INT_AP_L pulse required by CPU.
+ * It is set to be 100 microseconds, which a requirement of INTL TGL, GPIO
+ * in low power mode.
+ */
+#define PULSE_LENGTH                100ul
+
+#define USEC_TO_TIMEHS_TICKS(usec)  ((usec) * (PCLK_FREQ / SEC_UL))
+
+/* INT_AP_L pulse length in TIMEHS ticks. 0 means long pulse is disabled. */
+static uint32_t pulse_length;
+
+/* true if INT_AP_L is asserted, or false if it is deasserted. */
+static bool int_ap_asserted_;
+
+/*
+ * true if INT_AP assertion was requested but delayed for a process, or
+ * false if there is no delayed INT_AP assertion request.
+ */
+static bool assertion_delayed_;
+
+/*
+ * true if the high-speed timer for int_ap pulse (timehs0_timer1) is enabled, or
+ * false otherwise.
+ */
+static bool timer_running_;
+
+/*
+ * Toggle INT_AP_L level, and set a timer for PULSE_LENGTH
+ */
+static void toggle_int_ap_(void)
+{
+	/* Toggle INT_AP_L level. */
+	gpio_set_level(GPIO_INT_AP_L, int_ap_asserted_);
+	int_ap_asserted_ = !int_ap_asserted_;
+
+	/* Schedule to deassert INT_AP_L. */
+	GR_TIMEHS_LOAD(0, 1) = pulse_length;
+	GR_TIMEHS_CONTROL(0, 1) = GC_TIMEHS_TIMER1CONTROL_ONESHOT_MASK |
+				  GC_TIMEHS_TIMER1CONTROL_SIZE_MASK |
+				  GC_TIMEHS_TIMER1CONTROL_INTENABLE_MASK |
+				  GC_TIMEHS_TIMER1CONTROL_ENABLE_MASK;
+	timer_running_ = true;
+}
+
+/*
+ * Interrupt handler of timehs0_timint1, a timer for INT_AP_L extension.
+ */
+void timer_int_ap_irq_handler(void)
+{
+	/* Disable Timer. */
+	GR_TIMEHS_CONTROL(0, 1) = 0;
+
+	/* Clear interrupt status of TIMEHS0 TIMER1. */
+	GR_TIMEHS_INTCLR(0, 1) = 1;
+
+	timer_running_ = false;
+
+	if (!int_ap_asserted_) {
+		/*
+		 * While INT_AP_L is being deasserted, if assertion was
+		 * requested (and delayed), then toggle the signal.
+		 * Otherwise, just return without changing INT_AP_L level.
+		 */
+		if (!assertion_delayed_)
+			return;
+
+		assertion_delayed_ = false;
+	}
+
+	toggle_int_ap_();
+}
+DECLARE_IRQ(GC_IRQNUM_TIMEHS0_TIMINT1, timer_int_ap_irq_handler, 1);
+
+int assert_int_ap(void)
+{
+	/* If the long INT_AP_L pulse is not supported, then just return 0. */
+	if (!pulse_length)
+		return 0;
+
+	/*
+	 * If the timer is running, it means INT_AP_L deassertion pulse isn't
+	 * long enough yet. If so, let's delay to assert.
+	 * Otherwise, assert INT_AP_L immediately.
+	 */
+	if (timer_running_)
+		assertion_delayed_ = true;
+	else
+		toggle_int_ap_();
+
+	return 1;
+}
+
+void deassert_int_ap(void)
+{
+	/* If INT_AP_L is deasserted already, do nothing. */
+	if (!int_ap_asserted_) {
+		assertion_delayed_ = false;
+		return;
+	}
+	timer_int_ap_irq_handler();
+}
+
+void int_ap_extension_enable(void)
+{
+	/* Initialize int_ap_asserted_ based on the level of INT_AP_L. */
+	int_ap_asserted_ = !gpio_get_level(GPIO_INT_AP_L);
+	pulse_length = USEC_TO_TIMEHS_TICKS(PULSE_LENGTH);
+
+	/* Enable IRQNUM_TIMEHS0_TIMINT1 */
+	task_enable_irq(GC_IRQNUM_TIMEHS0_TIMINT1);
+}
+
+void int_ap_extension_disable(void)
+{
+	/* Enable IRQNUM_TIMEHS0_TIMINT1 */
+	task_disable_irq(GC_IRQNUM_TIMEHS0_TIMINT1);
+}
