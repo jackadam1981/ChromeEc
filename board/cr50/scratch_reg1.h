c242393fa458e79dd4f1a0019e170103c815c417
Namyoon Woo <namyoon@google.com>
2020-03-21 21:17:28 -0700

    Use a long pulse of INT_AP_L for SPS
    This patch introduces the board property, BOARD_LONG_PULSE_INT_AP,
    which keeps each level of GPIO_INT_AP_L at least for 100 microseconds.
    The assertion (low GPIO_INT_AP_L) duration might be shorter only if AP
    asserts a SPS CS before INT_AP_L deassertion, because it means means
    AP recognized GPIO_INT_AP_L assertion already.
    
    This patch increases the flash usage by 280 bytes.
    
    BUG=b:148691139
    TEST=Tested on atlas and helios.
    1. Minimum pulse length of INT_AP_L assertion and deassertion
    observed in logic analyzer were 110 usec and 152 usec.
    
    2. measured the depthcharge exit timestamp and cr50 flash time with
    or without INT_AP pulse extended to 100 usec, on atlas and helios:
    -----------------+-------------------+------------------
                     | atlas             | helios
    -----------------+-------------------+------------------
    boot (sec)       |  1.398 ->  1.402  |  1.004 ->  1.011
    cr50 flash (sec) | 10.800 -> 14.609  | 16.024 -> 16.466
    -----------------+-------------------+------------------
    
    3. With 100 usec long INT_AP pulse, checked trunks_client
    regression_test, stress_test and ext_command_test runs good.
    Checked dmesg and found no TPM errors through all tests.
    
      $ trunks_client --regression_test
      $ trunks_client --stress_test
      $ trunks_client --ext_command_test
    
    4.checked no character loss during uart_stress_tester.
    
      $ uart_stress_tester.py -c -t 600 /dev/ttyUSB2 /dev/ttyUSB1
    
    Signed-off-by: Namyoon Woo <namyoon@google.com>
    Change-Id: Ie74b236bc5352e9fc21fe600c12946e50955160a

--- board/cr50/scratch_reg1.h
+++ board/cr50/scratch_reg1.h
@@ -99,6 +99,10 @@
 #define BOARD_CCD_REC_LID_PIN_DIOA9       (2 << BOARD_CCD_REC_LID_PIN_SHIFT)
 #define BOARD_CCD_REC_LID_PIN_DIOA12      (3 << BOARD_CCD_REC_LID_PIN_SHIFT)
 
+/*
+ * The board generates a long pulse of INT_AP_L.
+ */
+#define BOARD_LONG_PULSE_INT_AP           BIT(24)
 
 /*
  * Macro to capture all properties related to board strapping pins. This must be
@@ -112,6 +116,7 @@
 	BOARD_DEEP_SLEEP_DISABLED | \
 	BOARD_DETECT_AP_WITH_UART | \
 	BOARD_EC_CR50_COMM_SUPPORT | \
+	BOARD_LONG_PULSE_INT_AP | \
 	BOARD_NEEDS_SYS_RST_PULL_UP | \
 	BOARD_NO_INA_SUPPORT | \
 	BOARD_SLAVE_CONFIG_I2C | \
