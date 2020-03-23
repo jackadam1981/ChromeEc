b274d5494a0fa949709921b7e25e5575c3b92887
Denis Brockus <dbrockus@google.com>
2020-03-23 11:47:54 -0600

    [WIP] zork: battery-less booting to AP needs to set DRP on unattached ports
    Take three.  See Patchsets 1 and 2 as well.
    
    BUG=b:152028728
    BRANCH=none
    TEST=verify trembyle will see unattached port after boot to AP
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: Ifaf0e01e4816fdf2c569d21aba7968d8538768c3

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -82,6 +82,8 @@
 #define TC_FLAGS_WAKE_FROM_LPM          BIT(22)
 /* Flag to note the TCPM supports auto toggle */
 #define TC_FLAGS_AUTO_TOGGLE_SUPPORTED  BIT(23)
+/* Flag to note TCPM was requested to DRP auto toggle */
+#define TC_FLAGS_AUTO_TOGGLE_REQUESTED  BIT(24)
 
 /*
  * Clear all flags except TC_FLAGS_AUTO_TOGGLE_SUPPORTED,
@@ -2797,13 +2799,22 @@ static void tc_drp_auto_toggle_entry(const int port)
 	atomic_clear(task_get_event_bitmap(task_get_current()),
 		PD_EXIT_LOW_POWER_EVENT_MASK);
 
-	if (drp_state[port] == PD_DRP_TOGGLE_ON)
+	/*
+	 * Enable DRP Toggle based on the current drp_state.
+	 * Keep a flag showing if DRP Toggle is enabled or not.
+	 */
+	if (drp_state[port] == PD_DRP_TOGGLE_ON) {
 		tcpm_enable_drp_toggle(port);
+		TC_SET_FLAG(port, TC_FLAGS_AUTO_TOGGLE_REQUESTED);
+	} else {
+		TC_CLR_FLAG(port, TC_FLAGS_AUTO_TOGGLE_REQUESTED);
+	}
 }
 
 static void tc_drp_auto_toggle_run(const int port)
 {
 	enum pd_drp_next_states next_state;
+	enum pd_dual_role_states curr_drp_state;
 	enum tcpc_cc_voltage_status cc1, cc2;
 
 	/*
@@ -2823,9 +2834,22 @@ static void tc_drp_auto_toggle_run(const int port)
 	/* Check for connection */
 	tcpm_get_cc(port, &cc1, &cc2);
 
+	/*
+	 * Make sure the drp_state didn't change since we went
+	 * through tc_drp_auto_toggle_entry from not toggling to
+	 * toggling. This will make getting the next state
+	 * different if we have an open connection
+	 */
+	if (!TC_CHK_FLAG(port, TC_FLAGS_AUTO_TOGGLE_REQUESTED) &&
+	    drp_state[port] == PD_DRP_TOGGLE_ON)
+		curr_drp_state = PD_DRP_TOGGLE_OFF;
+	else
+		curr_drp_state = drp_state[port];
+
+	/* Determine the next state to attempt */
 	tc[port].drp_sink_time = get_time().val;
 	next_state = drp_auto_toggle_next_state(&tc[port].drp_sink_time,
-		tc[port].power_role, drp_state[port], cc1, cc2);
+		tc[port].power_role, curr_drp_state, cc1, cc2);
 
 	/*
 	 * The next state is not determined just by what is
