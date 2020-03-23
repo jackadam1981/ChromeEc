c887b8f08837fe138f3ff29d48c1283aea93e38a
Tzung-Bi Shih <tzungbi@chromium.org>
2020-03-23 12:12:28 +0800

    core/riscv-rv32i: move interrupt details to IT83XX specific
    Getting source interrupt number is chip specific.  Moves the details to
    chip implementation.
    
    BRANCH=none
    BUG=b:151897847
    TEST=make buildall
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: Ia72acf8ec9c09cb329f8d7c92d22476512ffa669

--- core/riscv-rv32i/task.c
+++ core/riscv-rv32i/task.c
@@ -162,8 +162,6 @@ int start_called;  /* Has task swapping started */
 
 /* in interrupt context */
 static int in_interrupt;
-/* Interrupt number of EC modules */
-static volatile int ec_int;
 /* Interrupt group of EC INTC modules */
 volatile int ec_int_group;
 /* interrupt number of sw interrupt */
@@ -309,15 +307,11 @@ void __ram_code update_exc_start_time(void)
 #endif
 }
 
-#ifdef CHIP_FAMILY_IT83XX
-int __ram_code intc_get_ec_int(void)
-{
-	return ec_int;
-}
-#endif
-
 void __ram_code start_irq_handler(void)
 {
+	/* Interrupt number of EC modules */
+	int ec_int;
+
 	/* save a0, a1, and a2 for syscall */
 	asm volatile ("addi sp, sp, -4*3");
 	asm volatile ("sw a0, 0(sp)");
@@ -332,7 +326,7 @@ void __ram_code start_irq_handler(void)
 		ec_int_group = 16;
 	} else {
 		/* Determine interrupt number */
-		ec_int = IT83XX_INTC_AIVCT - 0x10;
+		ec_int = chip_get_ec_int();
 		ec_int_group = chip_get_intc_group(ec_int);
 	}
 
