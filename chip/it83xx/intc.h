ab4d60183afdd16d655f581336871de7e872beda
Tzung-Bi Shih <tzungbi@chromium.org>
2020-03-23 12:12:28 +0800

    core/riscv-rv32i: move interrupt details to IT83XX specific
    Getting source interrupt number is chip specific.  Moves the details to
    chip implementation.
    
    BRANCH=none
    BUG=b:151897847
    TEST=make buildall
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: Ia72acf8ec9c09cb329f8d7c92d22476512ffa669

--- chip/it83xx/intc.h
+++ chip/it83xx/intc.h
@@ -43,6 +43,7 @@ void espi_interrupt(void);
 void espi_vw_interrupt(void);
 void espi_enable_pad(int enable);
 void espi_init(void);
+int chip_get_ec_int(void);
 int chip_get_intc_group(int irq);
 void clock_cpu_standby(void);
 void spi_slv_int_handler(void);
