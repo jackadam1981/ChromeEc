c72e9e7e58ec4650efebdc14adbb8363207ef296
Ting Shen <phoenixshen@google.com>
2020-03-24 17:36:44 +0800

    driver/bc12: implement mt6360 driver
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I0fee8e7309178be4d933ed5c246ea107ca5931bf

---
+++ driver/bc12/mt6360.c
@@ -0,0 +1,155 @@
+#include "charger.h"
+#include "charge_manager.h"
+#include "console.h"
+#include "driver/bc12/mt6360.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "task.h"
+#include "timer.h"
+#include "usb_charge.h"
+
+/* Console output macros */
+#define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
+#define CPRINTS(format, args...) \
+	cprints(CC_CHARGER, "%s " format, "MT6360", ## args)
+
+static enum ec_error_list mt6360_read8(int reg, int *val)
+{
+	return i2c_read8(mt6360_config.i2c_port, mt6360_config.i2c_addr_flags,
+			reg, val);
+}
+
+static enum ec_error_list mt6360_write8(int reg, int val)
+{
+	return i2c_write8(mt6360_config.i2c_port, mt6360_config.i2c_addr_flags,
+			reg, val);
+}
+
+static int mt6360_update_bits(int reg, int mask, int val)
+{
+	int rv;
+	int reg_val = 0;
+
+	rv = mt6360_read8(reg, &reg_val);
+	if (rv)
+		return rv;
+	reg_val &= ~mask;
+	reg_val |= (mask & val);
+	rv = mt6360_write8(reg, reg_val);
+	return rv;
+}
+
+static inline int mt6360_set_bit(int reg, int mask)
+{
+	return mt6360_update_bits(reg, mask, mask);
+}
+
+static inline int mt6360_clr_bit(int reg, int mask)
+{
+	return mt6360_update_bits(reg, mask, 0x00);
+}
+
+
+static int mt6360_get_bc12_device_type(void)
+{
+	int reg;
+
+	if (mt6360_read8(MT6360_REG_USB_STATUS_1, &reg))
+		return CHARGE_SUPPLIER_NONE;
+
+	switch (reg & MT6360_MASK_USB_STATUS) {
+	case MT6360_MASK_SDP:
+		CPRINTS("BC12 SDP");
+		return CHARGE_SUPPLIER_BC12_SDP;
+	case MT6360_MASK_CDP:
+		CPRINTS("BC12 CDP");
+		return CHARGE_SUPPLIER_BC12_CDP;
+	case MT6360_MASK_DCP:
+		CPRINTS("BC12 DCP");
+		return CHARGE_SUPPLIER_BC12_DCP;
+	default:
+		CPRINTS("BC12 NONE");
+		return CHARGE_SUPPLIER_NONE;
+	}
+}
+
+static int mt6360_get_bc12_ilim(int charge_supplier)
+{
+	switch (charge_supplier) {
+	case CHARGE_SUPPLIER_BC12_DCP:
+	case CHARGE_SUPPLIER_BC12_CDP:
+		return USB_CHARGER_MAX_CURR_MA;
+	case CHARGE_SUPPLIER_BC12_SDP:
+	default:
+		return USB_CHARGER_MIN_CURR_MA;
+	}
+}
+
+static int mt6360_enable_bc12_detection(int en)
+{
+	int rv;
+
+	if (en) {
+#ifdef CONFIG_MT6360_BC12_DETECT_GPIO
+		gpio_set_level(CONFIG_MT6360_BC12_DETECT_GPIO, 1);
+#endif
+		return mt6360_set_bit(MT6360_REG_DEVICE_TYPE,
+				      MT6360_MASK_USBCHGEN);
+	}
+
+	rv = mt6360_clr_bit(MT6360_REG_DEVICE_TYPE, MT6360_MASK_USBCHGEN);
+#ifdef CONFIG_MT6360_BC12_DETECT_GPIO
+	gpio_set_level(CONFIG_MT6360_BC12_DETECT_GPIO, 0);
+#endif
+	return rv;
+}
+
+void mt6360_set_switches(int port, enum usb_switch setting)
+{
+}
+
+int mt6360_ramp_allowed(int port, int supplier)
+{
+	return supplier == CHARGE_SUPPLIER_BC12_DCP;
+}
+
+int mt6360_ramp_max(int port, int supplier, int sup_curr)
+{
+	return mt6360_get_bc12_ilim(supplier);
+}
+
+void mt6360_usb_charger_task(void *u)
+{
+	struct charge_port_info chg;
+
+	chg.voltage = USB_CHARGER_VOLTAGE_MV;
+	while (1) {
+		int bc12_type = CHARGE_SUPPLIER_NONE;
+
+		task_wait_event(-1);
+
+		mt6360_enable_bc12_detection(1);
+
+		/* TODO: change this to interrupt */
+		usleep(300 * MSEC);
+
+		bc12_type = mt6360_get_bc12_device_type();
+		chg.current = mt6360_get_bc12_ilim(bc12_type);
+
+		if (bc12_type == CHARGE_SUPPLIER_NONE) {
+			CPRINTS("VBUS detached");
+			charge_manager_update_charge(bc12_type, 0, NULL);
+		} else {
+			charge_manager_update_charge(bc12_type, 0, &chg);
+		}
+
+		mt6360_enable_bc12_detection(0);
+	}
+}
+
+const struct bc12_drv mt6360_drv = {
+	.usb_charger_task = mt6360_usb_charger_task,
+	.set_switches = mt6360_set_switches,
+	.ramp_allowed = mt6360_ramp_allowed,
+	.ramp_max = mt6360_ramp_max,
+};
