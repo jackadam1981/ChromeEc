2c52ad798ab8127d435c14a8c0f1542341c30545
Sam Hurst <shurst@google.com>
2020-03-24 16:46:14 -0700

    TCPMv2: Restore PD state after Sysjump
    When a PD explicit contract is negotiated in RO,
    the contract is maintained while performing a
    sysjump to RW. This is done by serializing
    the PD Power, Data, and VCONN Roles, along with
    the explicit contract flag. After jumping to RW,
    deserialization is performed by restoring the
    power roles and setting the explicit contract
    flag.
    
    BUG=b:152350558,b:152027807,b:152967274
    BRANCH=none
    TEST=make -j buildall
    	Manual tests:
    	Used total phase to verify that charging voltage
    	was maintained across sysjump from RO to RW and
    	that DP was reestablished when a dock was used.
    
    	Tested that Kohaku booted properly without a
    	battery and a charger connected to port 0. This
    	test was repeated with a charger connected to port 1.
    
    Signed-off-by: Sam Hurst <shurst@google.com>
    
    Change-Id: I349c41f2279e9af9830564d44ac73ad8435f1f80

--- include/usb_common.h
+++ include/usb_common.h
@@ -159,4 +159,39 @@ void notify_sysjump_ready(volatile const task_id_t * const
  * @param port USB-C port number
  */
 void set_usb_mux_with_current_data_role(int port);
+
+/**
+ * Get the PD flags stored in BB Ram
+ *
+ * @param port USB-C port number
+ * @param flags pointer where flags are written to
+ * @return EC_SUCCESS on success
+ */
+int pd_get_saved_port_flags(int port, uint8_t *flags);
+
+/**
+ * Update the flag in BB Ram with the give value
+ *
+ * @param port USB-C port number
+ * @param flag BB Ram flag to update
+ * @param val value written to the BB Ram flag
+ */
+void pd_update_saved_port_flags(int port, uint8_t flag, uint8_t val);
+
+/**
+ * Set the value of a given flag in BB Ram
+ *
+ * @param port USB-C port number
+ * @param flag flag to update
+ */
+void pd_set_saved_port_flag(int port, uint8_t flag);
+
+/**
+ * Clear the value of a given flag in BB Ram
+ *
+ * @param port USB-C port number
+ * @param flag flag to update
+ */
+void pd_clear_saved_port_flag(int port, uint8_t flag);
+
 #endif /* __CROS_EC_USB_COMMON_H */
