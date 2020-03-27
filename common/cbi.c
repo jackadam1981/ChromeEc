ce84c526af49d486b4e41d731a4c02fc3f59084b
elmo_lan <elmo_lan@compal.corp-partner.google.com>
2020-03-27 11:32:14 +0800

    CBI: Allow board to change the page size of rom
    Different rom parts would have different page size.
    This CL allows board to change it.
    
    BUG=b:152372438
    BRANCH=none
    TEST=make all
    
    Signed-off-by: elmo_lan <elmo_lan@compal.corp-partner.google.com>
    Change-Id: Ie2e962b21179ee7894ca844394946f8e1c475aaf

--- common/cbi.c
+++ common/cbi.c
@@ -78,7 +78,6 @@ struct cbi_data *cbi_find_tag(const void *cbi, enum cbi_data_tag tag)
 
 #define CPRINTS(format, args...) cprints(CC_SYSTEM, "CBI " format, ## args)
 
-#define EEPROM_PAGE_WRITE_SIZE	16
 #define EEPROM_PAGE_WRITE_MS	5
 #define EC_ERROR_CBI_CACHE_INVALID	EC_ERROR_INTERNAL_FIRST
 
