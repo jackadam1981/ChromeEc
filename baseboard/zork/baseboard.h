727182ca71675bd2f78a7f6442c0aed7b6ad5d6a
Lu Zhang <lu.zhang@bitland.corp-partner.google.com>
2020-03-27 11:47:21 +0800

    Dalboz: separate clamshell from tablet mode.
    Dalboz has clamshell and tablet mode and needs to distinguish them.
    
    BUG=b:150239200
    BRANCH=none
    TEST=Test ectool motionsense and keyboard enable status in
         clamshell and tablet mode.
    
    Signed-off-by: Lu Zhang <lu.zhang@bitland.corp-partner.google.com>
    Change-Id: Id63747038e0396a5083bea3e7db391f480e9b8f6

--- baseboard/zork/baseboard.h
+++ baseboard/zork/baseboard.h
@@ -348,7 +348,6 @@ void ppc_interrupt(enum gpio_signal signal);
 void hdmi_hpd_interrupt(enum ioex_signal signal);
 void mst_hpd_interrupt(enum ioex_signal signal);
 
-int board_is_convertible(void);
 void board_update_sensor_config_from_sku(void);
 
 #ifdef CONFIG_USB_TYPEC_PD_FAST_ROLE_SWAP
