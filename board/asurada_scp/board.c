8c38c751762ea15f139142c3b22220b31c682bad
Tzung-Bi Shih <tzungbi@chromium.org>
2020-03-30 09:49:25 +0800

    DO-NOT-SUBMIT: support Asurada SCP
    BRANCH=none
    BUG=b:146213943
    TEST=make buildall
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: Id6d4a75c3b593e65f9a6d35d7871dfb7025f722f

---
+++ board/asurada_scp/board.c
@@ -0,0 +1,21 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* Asurada SCP configuration */
+
+#include "registers.h"
+
+/* Build GPIO tables */
+#include "gpio_list.h"
+
+void dummy_task(void)
+{
+	int i = 0;
+
+	while (1) {
+		ccprints("%s: %d", __func__, ++i);
+		cflush();
+		busy_udelay(100000);
+	}
+}
