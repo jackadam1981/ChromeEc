285359d8ff5a7d9b7230428f25a232cd0da142af
Eric Yilun Lin <yllin@chromium.org>
2020-03-31 10:05:54 +0800

    baseboard/kukui: correct FUSB302 VBUS turn-on delay
    TCPC FUSB303 needs more time to raise VBUS to 5V. This CL
    increases the delay to ensure that in SRC_DISCOVER state,
    the VBUS is alread raised to 5V.
    
    BRANCH=kukui
    BUG=b:152544526
    TEST=Tested onn kappa,
         kappa source to servo-v4, and issue hard reset,
         and ensure they can recover from the hard reset.
    
    Change-Id: I5d656c1941c4f75410e51374ba6859d6d29776fe
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- baseboard/kukui/baseboard.h
+++ baseboard/kukui/baseboard.h
@@ -42,18 +42,31 @@
 #define CONFIG_USB_PD_TCPC_LOW_POWER
 #define CONFIG_USB_PD_DISCHARGE_TCPC
 #define CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE
+
+/* TCPC MT6370 */
+#define PD_POWER_SUPPLY_TURN_ON_DELAY  30000  /* us */
+#define PD_POWER_SUPPLY_TURN_OFF_DELAY 250000 /* us */
+#define PD_VCONN_SWAP_DELAY 5000 /* us */
+
 /*
  * The Maximum input voltage is 13.5V, need another 5% tolerance.
  * 12.85V * 1.05 = 13.5V
  */
 #define PD_MAX_VOLTAGE_MV 12850
 #define CONFIG_USB_PD_PREFER_MV
+
 #elif defined(VARIANT_KUKUI_CHARGER_ISL9238)
 #define CONFIG_CHARGER_ISL9238
 #define CONFIG_CHARGER_SENSE_RESISTOR_AC 20 /* BOARD_RS1 */
 #define CONFIG_CHARGER_SENSE_RESISTOR 10 /* BOARD_RS2 */
 #define CONFIG_CHARGER_OTG
 #define CONFIG_CHARGE_RAMP_HW
+
+/* TCPC FUSB302 */
+#define PD_POWER_SUPPLY_TURN_ON_DELAY  160000  /* us */
+#define PD_POWER_SUPPLY_TURN_OFF_DELAY 250000 /* us */
+#define PD_VCONN_SWAP_DELAY 5000 /* us */
+
 #define PD_MAX_VOLTAGE_MV 20000
 #else
 #error Must define a VARIANT_KUKUI_CHARGER
@@ -203,10 +216,6 @@
 #else
 #define PD_MAX_CURRENT_MA     3000
 #endif
-
-#define PD_POWER_SUPPLY_TURN_ON_DELAY  30000  /* us */
-#define PD_POWER_SUPPLY_TURN_OFF_DELAY 250000 /* us */
-#define PD_VCONN_SWAP_DELAY 5000 /* us */
 
 /* Timer selection */
 #define TIM_CLOCK32  2
