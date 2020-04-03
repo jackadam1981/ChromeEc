3a8d88a744b4fea68dbde2addf6d71e0fee12d7d
Rajat Jain <rajatja@google.com>
2020-04-03 00:58:13 -0700

    common: Make scancode table always mutable
    Since the vivaldi requires the scancode to be mutable, and vivaldi
    shall be enabled by default for all boards, thus make the scancode
    table to be always mutable and get rid of the config option it hides
    behind.
    
    BUG=b:146501925
    TEST=Build
    BRANCH=firmware-hatch-12672.B
    
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Change-Id: Iaedcd6d84caf31c91a61854f96414bcea38f5c2a

--- include/keyboard_8042_sharedlib.h
+++ include/keyboard_8042_sharedlib.h
@@ -18,12 +18,7 @@ struct button_8042_t {
 };
 
 /* The standard Chrome OS keyboard matrix table. */
-#if defined(CONFIG_KEYBOARD_SCANCODE_MUTABLE) || \
-	defined(CONFIG_KEYBOARD_CUSTOMIZATION)
 extern uint16_t scancode_set2[KEYBOARD_COLS_MAX][KEYBOARD_ROWS];
-#else
-extern const uint16_t scancode_set2[KEYBOARD_COLS_MAX][KEYBOARD_ROWS];
-#endif
 
 /* Translation from scan code set 2 to set 1. */
 extern const uint8_t scancode_translate_table[];
@@ -70,12 +65,7 @@ enum keycap_long_label_idx {
 };
 
 extern const char * const keycap_long_label[];
-#if defined(CONFIG_KEYBOARD_SCANCODE_MUTABLE) || \
-	defined(CONFIG_KEYBOARD_CUSTOMIZATION)
 extern char keycap_label[KEYBOARD_COLS_MAX][KEYBOARD_ROWS];
-#else
-extern const char keycap_label[KEYBOARD_COLS_MAX][KEYBOARD_ROWS];
-#endif
 #endif
 
 /* Button scancodes (Power, Volume Down, Volume Up, etc.) */
