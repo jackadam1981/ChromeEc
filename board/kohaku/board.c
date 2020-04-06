206bc5de45a42e449156367649bdf00ac78cba70
Todd Broch <tbroch@google.com>
2020-04-06 15:50:04 -0700

    kohaku: fixup names to temp sensors
    Names for charger & ambient were reversed by 03cdf27fc7 so lets fix.
    
    Note, this should have no material impact with regard to DPTF as those
    configurations in coreboot look correct.
    
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=firmware-hatch-12672.B
    BUG=b:151271332
    TEST=make -j buildall
    
    Change-Id: I50f80e2791b844e3c326d0dc02e3d8017dd5124f

--- board/kohaku/board.c
+++ board/kohaku/board.c
@@ -397,11 +397,11 @@ const struct adc_t adc_channels[] = {
 BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
 const struct temp_sensor_t temp_sensors[] = {
-	[TEMP_SENSOR_1] = {.name = "Ambient",
+	[TEMP_SENSOR_1] = {.name = "Charger",
 				 .type = TEMP_SENSOR_TYPE_BOARD,
 				 .read = get_temp_3v3_30k9_47k_4050b,
 				 .idx = ADC_TEMP_SENSOR_1},
-	[TEMP_SENSOR_2] = {.name = "Charger",
+	[TEMP_SENSOR_2] = {.name = "Ambient",
 				 .type = TEMP_SENSOR_TYPE_BOARD,
 				 .read = get_temp_3v3_30k9_47k_4050b,
 				 .idx = ADC_TEMP_SENSOR_2},
