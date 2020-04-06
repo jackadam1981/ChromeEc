3d7b9d5cfd4fa0265df114119e48be4cee9ffdc4
Todd Broch <tbroch@google.com>
2020-04-06 15:50:04 -0700

    kohaku: fixup names to temp sensors
    Names for charger & ambient and gt & ia were reversed by 03cdf27fc7.
    
    Lets fix them.
    
    Note, this should have no material impact with regard to DPTF as those
    configurations in coreboot look correct.
    
    Signed-off-by: Todd Broch <tbroch@chromium.org>
    
    BRANCH=firmware-hatch-12672.B
    BUG=b:151271332
    TEST=make -j buildall
    
    Change-Id: I50f80e2791b844e3c326d0dc02e3d8017dd5124f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2137928
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Auto-Submit: Todd Broch <tbroch@chromium.org>
    (cherry picked from commit 56117231b31c8b88433ce8ffc020c9d797f46a0f)

--- board/kohaku/board.c
+++ board/kohaku/board.c
@@ -395,33 +395,48 @@ BUILD_ASSERT(ARRAY_SIZE(motion_als_sensors) == ALS_COUNT);
 /* ADC channels */
 const struct adc_t adc_channels[] = {
 	[ADC_TEMP_SENSOR_1] = {
-		"TEMP_AMB", NPCX_ADC_CH0, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
+		"TEMP_CHARGER", NPCX_ADC_CH0, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
 	[ADC_TEMP_SENSOR_2] = {
-		"TEMP_CHARGER", NPCX_ADC_CH1, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
+		"TEMP_AMB", NPCX_ADC_CH1, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
 	[ADC_TEMP_SENSOR_3] = {
-		"TEMP_IA", NPCX_ADC_CH2, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
+		"TEMP_GT", NPCX_ADC_CH2, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
 	[ADC_TEMP_SENSOR_4] = {
-		"TEMP_GT", NPCX_ADC_CH3, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
+		"TEMP_IA", NPCX_ADC_CH3, ADC_MAX_VOLT, ADC_READ_MAX+1, 0},
 };
 BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
 const struct temp_sensor_t temp_sensors[] = {
-	[TEMP_SENSOR_1] = {.name = "Ambient",
+	[TEMP_SENSOR_1] = {.name = "Charger",
 				 .type = TEMP_SENSOR_TYPE_BOARD,
 				 .read = get_temp_3v3_30k9_47k_4050b,
+<<<<<<< HEAD   (b778c8 HACK: dratini: Prevent system can't power on)
 				 .idx = ADC_TEMP_SENSOR_1,
 				 .action_delay_sec = 1},
 	[TEMP_SENSOR_2] = {.name = "Charger",
+=======
+				 .idx = ADC_TEMP_SENSOR_1},
+	[TEMP_SENSOR_2] = {.name = "Ambient",
+>>>>>>> CHANGE (561172 kohaku: fixup names to temp sensors)
 				 .type = TEMP_SENSOR_TYPE_BOARD,
 				 .read = get_temp_3v3_30k9_47k_4050b,
+<<<<<<< HEAD   (b778c8 HACK: dratini: Prevent system can't power on)
 				 .idx = ADC_TEMP_SENSOR_2,
 				 .action_delay_sec = 1},
 	[TEMP_SENSOR_3] = {.name = "IA",
+=======
+				 .idx = ADC_TEMP_SENSOR_2},
+	[TEMP_SENSOR_3] = {.name = "GT",
+>>>>>>> CHANGE (561172 kohaku: fixup names to temp sensors)
 				 .type = TEMP_SENSOR_TYPE_BOARD,
 				 .read = get_temp_3v3_30k9_47k_4050b,
+<<<<<<< HEAD   (b778c8 HACK: dratini: Prevent system can't power on)
 				 .idx = ADC_TEMP_SENSOR_3,
 				 .action_delay_sec = 1},
 	[TEMP_SENSOR_4] = {.name = "GT",
+=======
+				 .idx = ADC_TEMP_SENSOR_3},
+	[TEMP_SENSOR_4] = {.name = "IA",
+>>>>>>> CHANGE (561172 kohaku: fixup names to temp sensors)
 				 .type = TEMP_SENSOR_TYPE_BOARD,
 				 .read = get_temp_3v3_30k9_47k_4050b,
 				 .idx = ADC_TEMP_SENSOR_4,
