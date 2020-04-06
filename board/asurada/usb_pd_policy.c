1db763f1c370cc7d53d59184e64079c652dc7d26
Eric Yilun Lin <yllin@chromium.org>
2020-04-06 16:12:24 +0800

    WIP: asurada: enable usb pd
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: Iab89fefb5f26205a5417faf28fff62df395e46ca

---
+++ board/asurada/usb_pd_policy.c
@@ -0,0 +1,18 @@
+int pd_snk_is_vbus_provided(int port)
+{
+	return 0;
+}
+
+void pd_power_supply_reset(int port)
+{
+}
+
+int pd_check_vconn_swap(int port)
+{
+	return 0;
+}
+
+int pd_set_power_supply_ready(int port)
+{
+	return 0;
+}
