7ec81b2bed68f7b81abb0ec1e4c1e705e7365298
Ching-Kang Yen <chingkang@chromium.org>
2020-04-09 13:57:20 +0800

    driver: refactor I2C_PORT_ACCEL
    Remove #ifdef I2C_PORT_ACCEL in bmi_common.c, since that we pass the
    actual port to driver data.
    The #ifdef I2C_PORT_ACCEL may sometimes cause a bug if we use other
    port instead of I2C_PORT_ACCEL and not define I2C_PORT_ACCEL.
    
    Also, remove unused I2C_PORT_ACCEL macro.
    
    BRANCH=None
    BUG=b:146065507
    TEST=make buildall -j
    
    Change-Id: I8bdaadaf94a33111b60e20fbfe197c4d539b64da
    Signed-off-by: Ching-Kang Yen <chingkang@chromium.org>

--- board/dood/board.h
+++ board/dood/board.h
@@ -13,9 +13,6 @@
 #define VARIANT_OCTOPUS_CHARGER_ISL9238
 #include "baseboard.h"
 
-/* I2C bus configuraiton */
-#define I2C_PORT_ACCEL	I2C_PORT_SENSOR
-
 /* EC console commands  */
 #define CONFIG_CMD_ACCELS
 #define CONFIG_CMD_ACCEL_INFO
--- board/elm/board.h
+++ board/elm/board.h
@@ -156,7 +156,6 @@
 
 /* 2 I2C master ports, connect to battery, charger, pd and USB switches */
 #define I2C_PORT_MASTER  0
-#define I2C_PORT_ACCEL   0
 #define I2C_PORT_BATTERY 0
 #define I2C_PORT_CHARGER 0
 #define I2C_PORT_PERICOM 0
