bce83e1b461a23905bdce257b59a19624c5c3d44
Scott Chao <scott.chao@bitland.corp-partner.google.com>
2020-04-13 12:13:33 +0800

    kakadu: remove double tap
    BUG=
    BRANCH=
    TEST=
    
    Signed-off-by: Scott Chao <scott.chao@bitland.corp-partner.google.com>
    Change-Id: I355966aa1f209da6972e0bb12d4e03370f4a0313

--- board/kakadu/board.c
+++ board/kakadu/board.c
@@ -396,15 +396,10 @@ struct motion_sensor_t motion_sensors[] = {
 		.min_frequency = LSM6DSM_ODR_MIN_VAL,
 		.max_frequency = LSM6DSM_ODR_MAX_VAL,
 		.config = {
-			/* Enable accel in S0 */
-			[SENSOR_CONFIG_EC_S0] = {
-				.odr = TAP_ODR,
-				.ec_rate = 100 * MSEC,
-			},
-			/* For double tap detection */
-			[SENSOR_CONFIG_EC_S3] = {
-				.odr = TAP_ODR,
-				.ec_rate = 100 * MSEC,
+		 /* Enable accel in S0 */
+		 [SENSOR_CONFIG_EC_S0] = {
+			 .odr = 13000 | ROUND_UP_FLAG,
+			 .ec_rate = 100 * MSEC,
 			},
 		},
 	},
