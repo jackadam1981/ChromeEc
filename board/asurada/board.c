73c54f89779e0603cba8099192744cc9e64f54d7
Ting Shen <phoenixshen@google.com>
2020-04-15 14:13:54 +0800

    WIP: asurada: enable ppc syv682x
    NOTE: not working unless ac adapter plugged when booting
    
    BUG=None
    TEST=None
    BRANCH=None
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I7d79926fbba8eb6806362772babe9b54b42772bf

--- board/asurada/board.c
+++ board/asurada/board.c
@@ -11,8 +11,9 @@
 #include "chipset.h"
 #include "common.h"
 #include "console.h"
-#include "driver/bc12/pi3usb9201.h"
+#include "driver/bc12/mt6360.h"
 #include "driver/charger/isl923x.h"
+#include "driver/ppc/syv682x.h"
 #include "driver/tcpm/it83xx_pd.h"
 #include "extpower.h"
 #include "gpio.h"
@@ -31,9 +32,12 @@
 #include "usb_charge.h"
 #include "usb_mux.h"
 #include "usb_pd_tcpm.h"
+#include "usbc_ppc.h"
 
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+
+static void ppc_interrupt(enum gpio_signal signal);
 
 #include "gpio_list.h"
 
@@ -74,6 +78,12 @@ static void board_init(void)
 {
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+static void board_tcpc_init(void)
+{
+	gpio_enable_interrupt(GPIO_USB_C0_PPC_INT_ODL);
+}
+DECLARE_HOOK(HOOK_INIT, board_tcpc_init, HOOK_PRIO_INIT_CHIPSET);
 
 /* ADC channels. Must be in the exactly same order as in enum adc_channel. */
 const struct adc_t adc_channels[] = {
@@ -144,6 +154,38 @@ const struct i2c_port_t i2c_ports[] = {
 	{"usb1",     IT83XX_I2C_CH_E, 100, GPIO_I2C_E_SCL, GPIO_I2C_E_SDA},
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+/* PPC */
+struct ppc_config_t ppc_chips[CONFIG_USB_PD_PORT_MAX_COUNT] = {
+	{
+		.i2c_port = I2C_PORT_USBC_0,
+		.i2c_addr_flags = SYV682X_ADDR0_FLAGS,
+		.drv = &syv682x_drv
+	},
+};
+unsigned int ppc_cnt = ARRAY_SIZE(ppc_chips);
+
+static void ppc_interrupt(enum gpio_signal signal)
+{
+	CPRINTS("\x1b[1;33m%s\x1b[m", __func__);
+	switch (signal) {
+	case GPIO_USB_C0_PPC_INT_ODL:
+		syv682x_interrupt(0);
+	default:
+		break;
+	}
+}
+
+int ppc_get_alert_status(int port)
+{
+	return gpio_get_level(GPIO_USB_C0_PPC_INT_ODL) == 0;
+	/* TODO: add GPIO_USB_C1_PPC_INT_ODL */
+}
+
+void board_overcurrent_event(int port, int is_overcurrented)
+{
+	/* TODO: check correct operation for Asurada */
+}
 
 /* TCPC */
 const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_MAX_COUNT] = {
