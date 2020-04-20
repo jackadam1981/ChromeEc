092838fcd6bf4d46c26fce733c8cb2ee53b9abd3
xiong.huang <xiong.huang@bitland.corp-partner.google.com>
2020-04-20 22:08:52 +0800

    malefor: move led support from baseboard to board
    Considering every OEM would have different requirements for LED
    behavior. So move led support from baseboard (volteer) to board
    (halvor, malefor and volteer), there will be happy to redefine
    LED behavior in boards.
    
    BUG=b:154447182
    BRANCH=none
    TEST=make buildall
    
    Signed-off-by: xiong.huang <xiong.huang@bitland.corp-partner.google.com>
    Change-Id: I578459d4dd75abce4eed83e1f69a14886bb6a0f4

--- board/volteer/board.h
+++ board/volteer/board.h
@@ -19,6 +19,10 @@
 #undef CONFIG_UART_TX_BUF_SIZE
 #define CONFIG_UART_TX_BUF_SIZE 4096
 
+/* LED defines */
+/* Although there are 2 LEDs, they are both controlled by the same lines. */
+#define CONFIG_LED_PWM_COUNT 1
+
 /* Keyboard features */
 
 /* Sensors */
