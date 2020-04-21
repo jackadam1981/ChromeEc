8bcb9d9006b8d2fe31562b84cfbb452234058f90
xiong.huang <xiong.huang@bitland.corp-partner.google.com>
2020-04-21 14:26:17 +0800

    malefor: define led behavior
    Malefor uses two types LED, battery LED and power led. Battery LED uses
    dual-color LED, green and red. Power LED uses monochrome LED, white.
    
    Malefor controls battery LED and power LED with GPIO method, move
    baseboard (volteer) PWM settiing to the boards (halvor, malefor, volteer),
    and remove unused malefor PWM channels.
    
    BUG=b:154447182
    BRANCH=none
    TEST=battery led and power led behavior fit the reference design.
    https://chromeos.google.com/partner/dlm/docs/reference-designs
    /volteer/volteer_clamshell.html#led-behavior
    
    Signed-off-by: xiong.huang <xiong.huang@bitland.corp-partner.google.com>
    Change-Id: I272ae8a243b72c4c37b5bb3bd8e96dc57c1ace7e

--- baseboard/volteer/baseboard.h
+++ baseboard/volteer/baseboard.h
@@ -239,16 +239,6 @@ enum adc_channel {
 	ADC_CH_COUNT
 };
 
-enum pwm_channel {
-	PWM_CH_LED1_BLUE = 0,
-	PWM_CH_LED2_GREEN,
-	PWM_CH_LED3_RED,
-	PWM_CH_LED4_SIDESEL,
-	PWM_CH_FAN,
-	PWM_CH_KBLIGHT,
-	PWM_CH_COUNT
-};
-
 enum fan_channel {
 	FAN_CH_0 = 0,
 	/* Number of FAN channels */
