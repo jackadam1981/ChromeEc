847ec3bdfbc6bb65ca2585a86f20a2addf95f860
Scott Collyer <scollyer@google.com>
2020-04-22 14:24:52 -0700

    honeybuns(WIP): Test changes for nucleo64 board
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Id0cc5077f8e2556bbff5bb86cee3b062a12d62ba

--- board/honeybuns/board.c
+++ board/honeybuns/board.c
@@ -16,8 +16,16 @@
 
 #include "gpio_list.h" /* Must come after other header files. */
 
+static int count;
 static void board_init(void)
 {
 	/* TODO */
+	count = 0;
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+static void led_second(void)
+{
+	gpio_set_level(GPIO_LED1, count++ & 1);
+}
+DECLARE_HOOK(HOOK_SECOND, led_second, HOOK_PRIO_DEFAULT);
--- board/honeybuns/board.h
+++ board/honeybuns/board.h
@@ -17,6 +17,30 @@
 #undef CONFIG_UART_TX_BUF_SIZE
 #define CONFIG_UART_TX_BUF_SIZE 4096
 
+/*
+ * Enable the blink example that exercises the LEDs.
+ */
+
+
+/* Setup UART console */
+
+#define STM32G431_EVAL_USE_LPUART_CONSOLE
+#undef CONFIG_UART_CONSOLE
+#define CONFIG_UART_TX_DMA
+#undef CONFIG_UART_TX_BUF_SIZE
+#define CONFIG_UART_TX_BUF_SIZE 2048
+
+#ifdef STM32G431_EVAL_USE_LPUART_CONSOLE
+#define CONFIG_UART_CONSOLE 9
+#define CONFIG_UART_TX_DMA_CH STM32_DMAC_LPUART_TX
+#define CONFIG_UART_TX_DMA_PH 4
+#else
+#define CONFIG_UART_CONSOLE 1
+#define CONFIG_UART_TX_DMA_CH STM32_DMAC_USART1_TX
+#define CONFIG_UART_TX_DMA_PH 2
+#endif
+
+
 /* Keyboard features */
 
 /* Sensors */
