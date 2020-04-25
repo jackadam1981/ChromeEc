5c9f0b4208904160dca71b7b12ea1b1ab293c404
Vadim Bendebury <vbendeb@chromium.org>
2020-04-24 18:43:45 -0700

    Plug in the AP RO verification implementation
    This patch enables the invocation of the AP RO verification function
    in response to the operator entering the 'magic sequence' of holding
    the power button pressed and pressing/releasing the refresh button
    three times within five seconds.
    
    The code used during the 'Open box RMA' verification process is used,
    with the physical presence confirmation phase bypassed.
    
    BUG=b:153764696
    TEST=generated AP integrity verification data using the ap_ro_hash.py
         script and then ran the verification procedure, observing the
         'hash match' message on the Cr50 console.
    
         Also verified that the Open Box RMA procedure still succeeds.
    
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Change-Id: Ic101fb892554ebb05f9ebe6d1546bfb439f74043

--- board/cr50/usb_spi.c
+++ board/cr50/usb_spi.c
@@ -469,6 +469,35 @@ static void spi_hash_pp_done(void)
 		(spi_hash_device == USB_SPI_AP ? "AP" : "EC"));
 }
 
+void enable_ap_spi_hash_shortcut(void)
+{
+	/*
+	 * This is a big hammer, invoked when the Chrome OS device is
+	 * processing the EC reset. Even if SPI bus was in use when the
+	 * operator triggered the AP RO hash verification it should be
+	 * released and re-acquired now.
+	 */
+	enum spi_bus_user_t curr_user;
+
+	curr_user = get_spi_bus_user();
+	if (curr_user != SPI_BUS_USER_NONE)
+		set_spi_bus_user(curr_user, 0);
+
+	/*
+	 * Simulate successful completion of physical presence detection
+	 * required to allow the AP flash hash check. This function is invoked
+	 * when the operator entered the appropriate sequence on the device
+	 * keyboard, so physical presence is already established.
+	 */
+	new_device = USB_SPI_AP;
+	spi_hash_pp_done();
+}
+
+void disable_ap_spi_hash_shortcut(void)
+{
+	spi_hash_disable();
+}
+
 /* Process vendor subcommand dealing with Physical presence polling. */
 static enum vendor_cmd_rc spihash_pp_poll(void *buf,
 					  size_t input_size,
