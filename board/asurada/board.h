1e026aaff613b7ac2da6c3733d74df320b0a7cd7
Eric Yilun Lin <yllin@chromium.org>
2020-04-27 14:23:25 +0800

    asurada: enable C1 USBPD on subboard
    Change-Id: Ic7dfeaee8b0f6e84e1f2e598dc93deb773bee059

--- board/asurada/board.h
+++ board/asurada/board.h
@@ -108,6 +108,14 @@
 #undef CONFIG_UART_TX_BUF_SIZE
 #define CONFIG_UART_TX_BUF_SIZE 4096
 
+/* GPIO name remapping */
+#define GPIO_EN_HDMI_PWR    GPIO_EC_X_GPIO1
+#define GPIO_USB_C1_FRS_EN  GPIO_EC_X_GPIO1
+#define GPIO_USB_C1_PPC_INT_ODL GPIO_X_EC_GPIO2
+#define GPIO_PS185_EC_DP_HPD    GPIO_X_EC_GPIO2
+#define GPIO_USB_C1_DP_IN_HPD   GPIO_EC_X_GPIO3
+#define GPIO_PS185_PWRDN_ODL    GPIO_EC_X_GPIO3
+
 #ifndef __ASSEMBLER__
 
 #include "gpio_signal.h"
@@ -140,6 +148,14 @@ enum power_signal {
 	PMIC_PWR_GOOD,
 	AP_WDT_ASSERTED,
 	POWER_SIGNAL_COUNT,
+};
+
+enum board_sub_board {
+	SUB_BOARD_NONE = -1,
+	SUB_BOARD_TYPEC,
+	SUB_BOARD_HDMI,
+
+	SUB_BOARD_COUNT,
 };
 
 void board_reset_pd_mcu(void);
