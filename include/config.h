890f677ce838b715c5eb7770b658390f9fbdfd86
Eric Yilun Lin <yllin@chromium.org>
2020-04-27 16:28:43 +0800

    dp-hpd: abstract HPD gpio pin
    The HPD GPIO active level and naming may vary across boards.
    This CL abstract the layer for board customization by adding
    config CONFIG_USB_PD_DP_HPD_GPIO_CUSTOM.
    
    BRANCH=master
    BUG=b:154565980, b:150341271
    TEST=ensure HPD works on asurada.
    
    Change-Id: I237c10722f13e5f31144e142f63e2694a23fe899
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- include/config.h
+++ include/config.h
@@ -3729,6 +3729,12 @@
 /* HPD is sent to the GPU from the EC via a GPIO */
 #undef CONFIG_USB_PD_DP_HPD_GPIO
 
+/*
+ * HPD is sent to the GPU from the EC via a GPIO, and the HPD GPIO level has
+ * to be handled separately.
+ */
+#undef CONFIG_USB_PD_DP_HPD_GPIO_CUSTOM
+
 /* Check if max voltage request is allowed before each request */
 #undef CONFIG_USB_PD_CHECK_MAX_REQUEST_ALLOWED
 
@@ -4924,6 +4930,12 @@
 	defined(CONFIG_USB_PD_DISCHARGE_TCPC) || \
 	defined(CONFIG_USB_PD_DISCHARGE_PPC)
 #define CONFIG_USB_PD_DISCHARGE
+#endif
+
+/*****************************************************************************/
+/* Define derived config options for DP HPD GPIO */
+#ifdef CONFIG_USB_PD_DP_HPD_GPIO_CUSTOM
+#define CONFIG_USB_PD_DP_HPD_GPIO
 #endif
 
 /*****************************************************************************/
