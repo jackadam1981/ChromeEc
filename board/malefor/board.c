70700948d8952c3a5ca8c47993f85089d82a3731
xiong.huang <xiong.huang@bitland.corp-partner.google.com>
2020-04-27 11:58:42 +0800

    malefor: turn off USB4 and Thunderbolt support
    Malefor board only support USB3, so turn off USB4 and Thunderbolt support.
    
    BUG=b:153996396
    BRANCH=none
    TEST=male buildall
    
    Signed-off-by: xiong.huang <xiong.huang@bitland.corp-partner.google.com>
    Change-Id: Iff87d7cae7876656ac082cfe322dd63d87a74b50

--- board/malefor/board.c
+++ board/malefor/board.c
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-/* Volteer board-specific configuration */
+/* Malefor board-specific configuration */
 
 #include "button.h"
 #include "common.h"
@@ -23,7 +23,6 @@
 #include "task.h"
 #include "tablet_mode.h"
 #include "uart.h"
-#include "usb_pd_tbt.h"
 #include "util.h"
 
 #include "gpio_list.h" /* Must come after other header files. */
@@ -37,32 +36,6 @@ static void board_init(void)
 	gpio_enable_interrupt(GPIO_EC_CAM_VSYN_SLP_S0IX);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
-
-__override enum tbt_compat_cable_speed board_get_max_tbt_speed(int port)
-{
-	/* Routing length exceeds 205mm prior to connection to re-timer */
-	if (port == USBC_PORT_C1)
-		return TBT_SS_U32_GEN1_GEN2;
-
-	/*
-	 * Thunderbolt-compatible mode not supported
-	 *
-	 * TODO (b/153996396): All the USB-C ports need to support same speed.
-	 * Need to fix once USB-C feature set is known for Volteer.
-	 */
-	return TBT_SS_RES_0;
-}
-
-__override bool board_is_tbt_usb4_port(int port)
-{
-	/*
-	 * On the Volteer reference board only port 1 supports TBT & USB4
-	 *
-	 * TODO (b/153996396): All the USB-C ports need to support same
-	 * features. Need to fix once USB-C feature set is known for Volteer.
-	 */
-	return port == USBC_PORT_C1;
-}
 
 /******************************************************************************/
 /* Sensors */
