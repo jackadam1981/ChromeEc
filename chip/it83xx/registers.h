944eb0c22b3c658bf729a0edfd962a2f0e444518
Ruibin Chang <ruibin.chang@ite.com.tw>
2020-04-29 12:01:31 +0800

    WIP it83xx: support keyboard GPIO output mode
    These pins could be used as GPIO input,
    and they can be configured as GPIO output as well.
    So we made this patch to support it.
    
    BRANCH=none
    BUG=none
    TEST=On EVB, we can set/get level properly at GPIO mode.
    
    Change-Id: I1daa8f3396c16f7be35e1dfa84efc4442aa54244
    Signed-off-by: Ruibin Chang <ruibin.chang@ite.com.tw>
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>

--- chip/it83xx/registers.h
+++ chip/it83xx/registers.h
@@ -813,8 +813,15 @@ enum {
 };
 
 struct gpio_reg_t {
-	/* GPIO port data register (bit mapping to pin) */
+	/*
+	 * GPIO port data register; KSI/KSO port data mirror register
+	 * (bit mapping to pin)
+	 */
 	uint32_t reg_gpdr;
+	/* GPIO port data register; KSI/KSO port output data register
+	 * (bit mapping to pin)
+	 */
+	uint32_t reg_gpdr_;
 	/* GPIO port output type register (bit mapping to pin) */
 	uint32_t reg_gpotr;
 	/* GPIO port control register (byte mapping to pin) */
@@ -823,29 +830,30 @@ struct gpio_reg_t {
 
 /* GPIO group index convert to GPIO data/output type/ctrl group address */
 static const struct gpio_reg_t gpio_group_to_reg[] = {
-	/*               GPDR,       GPOTR,      GPCR      */
-	[GPIO_A]     = { 0x00F01601, 0x00F01671, 0x00F01610 },
-	[GPIO_B]     = { 0x00F01602, 0x00F01672, 0x00F01618 },
-	[GPIO_C]     = { 0x00F01603, 0x00F01673, 0x00F01620 },
-	[GPIO_D]     = { 0x00F01604, 0x00F01674, 0x00F01628 },
-	[GPIO_E]     = { 0x00F01605, 0x00F01675, 0x00F01630 },
-	[GPIO_F]     = { 0x00F01606, 0x00F01676, 0x00F01638 },
-	[GPIO_G]     = { 0x00F01607, 0x00F01677, 0x00F01640 },
-	[GPIO_H]     = { 0x00F01608, 0x00F01678, 0x00F01648 },
-	[GPIO_I]     = { 0x00F01609, 0x00F01679, 0x00F01650 },
-	[GPIO_J]     = { 0x00F0160a, 0x00F0167a, 0x00F01658 },
-	[GPIO_K]     = { 0x00F0160b, 0x00F0167b, 0x00F01690 },
-	[GPIO_L]     = { 0x00F0160c, 0x00F0167c, 0x00F01698 },
-	[GPIO_M]     = { 0x00F0160d, 0x00F0167d, 0x00F016a0 },
+	/*               GPDR,       GPDR,       GPOTR,      GPCR      */
+	[GPIO_A]     = { 0x00F01601, 0x00F01601, 0x00F01671, 0x00F01610 },
+	[GPIO_B]     = { 0x00F01602, 0x00F01602, 0x00F01672, 0x00F01618 },
+	[GPIO_C]     = { 0x00F01603, 0x00F01603, 0x00F01673, 0x00F01620 },
+	[GPIO_D]     = { 0x00F01604, 0x00F01604, 0x00F01674, 0x00F01628 },
+	[GPIO_E]     = { 0x00F01605, 0x00F01605, 0x00F01675, 0x00F01630 },
+	[GPIO_F]     = { 0x00F01606, 0x00F01606, 0x00F01676, 0x00F01638 },
+	[GPIO_G]     = { 0x00F01607, 0x00F01607, 0x00F01677, 0x00F01640 },
+	[GPIO_H]     = { 0x00F01608, 0x00F01608, 0x00F01678, 0x00F01648 },
+	[GPIO_I]     = { 0x00F01609, 0x00F01609, 0x00F01679, 0x00F01650 },
+	[GPIO_J]     = { 0x00F0160A, 0x00F0160A, 0x00F0167A, 0x00F01658 },
+	[GPIO_K]     = { 0x00F0160B, 0x00F0160B, 0x00F0167B, 0x00F01690 },
+	[GPIO_L]     = { 0x00F0160C, 0x00F0160C, 0x00F0167C, 0x00F01698 },
+	[GPIO_M]     = { 0x00F0160D, 0x00F0160D, 0x00F0167D, 0x00F016A0 },
 #if defined(CHIP_FAMILY_IT8XXX1) || defined(CHIP_FAMILY_IT8XXX2)
-	[GPIO_O]     = { 0x00F03E01, 0x00F03E71, 0x00F03E10 },
-	[GPIO_P]     = { 0x00F03E02, 0x00F03E72, 0x00F03E18 },
-	[GPIO_Q]     = { 0x00F03E03, 0x00F03E73, 0x00F03E20 },
-	[GPIO_R]     = { 0x00F03E04, 0x00F03E74, 0x00F03E28 },
+	[GPIO_O]     = { 0x00F03E01, 0x00F03E01, 0x00F03E71, 0x00F03E10 },
+	[GPIO_P]     = { 0x00F03E02, 0x00F03E02, 0x00F03E72, 0x00F03E18 },
+	[GPIO_Q]     = { 0x00F03E03, 0x00F03E03, 0x00F03E73, 0x00F03E20 },
+	[GPIO_R]     = { 0x00F03E04, 0x00F03E04, 0x00F03E74, 0x00F03E28 },
 #endif
-	[GPIO_KSI]   = { 0x00F01D09,         -1,         -1 },
-	[GPIO_KSO_H] = { 0x00F01D0C,         -1,         -1 },
-	[GPIO_KSO_L] = { 0x00F01D0F,         -1,         -1 },
+	/*               KSDMR,      KSDR,       KSOTR,      KSCR      */
+	[GPIO_KSI]   = { 0x00F01D09, 0x00F01D08, 0x00F01D26,         -1 },
+	[GPIO_KSO_H] = { 0x00F01D0C, 0x00F01D01, 0x00F01D27,         -1 },
+	[GPIO_KSO_L] = { 0x00F01D0F, 0x00F01D00, 0x00F01D28,         -1 },
 };
 BUILD_ASSERT(ARRAY_SIZE(gpio_group_to_reg) == (COUNT));
 
@@ -853,6 +861,8 @@ BUILD_ASSERT(ARRAY_SIZE(gpio_group_to_reg) == (COUNT));
 
 #define IT83XX_GPIO_DATA(port)                 \
 	REG8(gpio_group_to_reg[port].reg_gpdr)
+#define IT83XX_GPIO_DATA_(port)                 \
+	REG8(gpio_group_to_reg[port].reg_gpdr_)
 #define IT83XX_GPIO_GPOT(port)                 \
 	REG8(gpio_group_to_reg[port].reg_gpotr)
 #define IT83XX_GPIO_CTRL(port, pin_offset)     \
@@ -1234,6 +1244,9 @@ REG8(IT83XX_PMC_BASE + (ch > LPC_PM2 ? 5 : 8) + (ch << 4))
 #define IT83XX_KBS_SDC2R        REG8(IT83XX_KBS_BASE+0x23)
 #define IT83XX_KBS_SDC3R        REG8(IT83XX_KBS_BASE+0x24)
 #define IT83XX_KBS_SDSR         REG8(IT83XX_KBS_BASE+0x25)
+#define IT83XX_KBS_KSIGPODR     REG8(IT83XX_KBS_BASE+0x26)
+#define IT83XX_KBS_KSOHGPODR    REG8(IT83XX_KBS_BASE+0x27)
+#define IT83XX_KBS_KSOLGPODR    REG8(IT83XX_KBS_BASE+0x28)
 
 /* Shared Memory Flash Interface Bridge (SMFI) */
 #define IT83XX_SMFI_BASE  0x00F01000
