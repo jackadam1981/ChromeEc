796d63c46087a6db62f00ee35346718c0d4873b3
Bhanu Prakash Maiya <bhanumaiya@google.com>
2020-04-30 15:26:52 -0700

    Bloonchipper: USART based transport layer for host command
    1. USART host command layer in chip/stm32
    2. Fix usart implementation in stm32
    
    BUG=b:147849609
    BRANCH=none
    TEST=1. make BOARD=bloonchipper -j
         2. usart request and response works on dragonclaw
    
    Cq-Depend: chromium:2191733
    Change-Id: Idd89d3e490f23aa528ecaf6510c13d16b405de13
    Signed-off-by: Bhanu Prakash Maiya <bhanumaiya@google.com>

--- chip/stm32/usart.h
+++ chip/stm32/usart.h
@@ -197,6 +197,13 @@ void usart_shutdown(struct usart_config const *config);
 void usart_interrupt(struct usart_config const *config);
 
 /*
+ * Trigger tx interrupt to process tx units. This can be used when tx handler
+ * should be started from other context. This function ensures tx interrupt
+ * handler is called.
+ */
+void usart_tx_start(struct usart_config const *config);
+
+/*
  * These are HW specific baud rate calculation and setting functions that the
  * peripheral variant code uses during initialization and clock frequency
  * change.  The baud rate divisor input frequency is passed in Hertz.
@@ -254,5 +261,12 @@ struct usart_configs {
 };
 
 struct usart_configs usart_get_configs(void);
+
+/*
+ * This usart_tx structure contains function pointer to interrupt
+ * handler implemented to send host response. Generic queue based
+ * interrupt handler is not used for usart host transport.
+ */
+extern struct usart_tx const usart_host_command_tx_interrupt;
 
 #endif /* __CROS_EC_USART_H */
