75af6a994ff23fe8e67d2c05e3addde709735551
Scott Chao <scott.chao@bitland.corp-partner.google.com>
2020-04-30 10:36:39 +0800

    damu: fix lid sensor rotation matrix
    BUG=b:155315888
    BRANCH=kukui
    TEST=make -j BOARD=damu
    TEST=make buildall
    TEST=boot into OS and screen show normally.
    
    Signed-off-by: Scott Chao <scott.chao@bitland.corp-partner.google.com>
    Change-Id: I65176198555e37bb5deb7f54cbf49e767fe4253f

--- board/damu/board.c
+++ board/damu/board.c
@@ -337,16 +337,17 @@ static struct mutex g_base_mutex;
 
 /* Rotation matrixes */
 static const mat33_fp_t base_standard_ref = {
-	{0, FLOAT_TO_FP(1), 0},
-	{FLOAT_TO_FP(-1), 0, 0},
-	{0, 0, FLOAT_TO_FP(1)}
+	{FLOAT_TO_FP(1), 0, 0},
+	{0, FLOAT_TO_FP(-1), 0},
+	{0, 0, FLOAT_TO_FP(-1)}
 };
 
 static const mat33_fp_t lid_standard_ref = {
-	{ 0, FLOAT_TO_FP(-1), 0},
 	{ FLOAT_TO_FP(-1), 0, 0},
-	{ 0, 0, FLOAT_TO_FP(-1)}
+	{ 0, FLOAT_TO_FP(-1), 0},
+	{ 0, 0, FLOAT_TO_FP(1)}
 };
+
 
 /* sensor private data */
 /* Lid accel private data */
