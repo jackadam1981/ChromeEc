fd29cc25735de3986ae8a40c4cefc369e696aacb
Denis Brockus <dbrockus@google.com>
2020-04-29 23:59:19 -0600

    tcpmv2: SNK HardReset cleanup - fix final comments
    After b:2172063 was submitted there were a couple of
    comments that needed to be addressed.  This CL is to
    finish off the SNK HardReset cleanup
    
    BUG=b:154073581,b:154622375,b:154937258
    BUG=b:154778807,b:155132904
    BRANCH=none
    TEST=verify charger is still detected after hardreset
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: Ieb628eec1ca8be0e1ea5abb155ab1a7926626567

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -2057,7 +2057,7 @@ static void pe_src_transition_to_default_entry(int port)
 	 * Request Device Policy Manager to set Port Data
 	 * Role to DFP and turn off VCONN
 	 */
-	tc_hard_reset(port);
+	tc_hard_reset_request(port);
 }
 
 static void pe_src_transition_to_default_run(int port)
@@ -2076,8 +2076,8 @@ static void pe_src_transition_to_default_run(int port)
 
 static void pe_src_transition_to_default_exit(int port)
 {
-	/* Inform the TC Layer the Hard Reset is complete */
-	tc_hard_reset_complete(port);
+	/* TC layer can now go unattached */
+	tc_hard_reset_allow_unattach(port);
 }
 
 /**
@@ -2235,9 +2235,9 @@ static void pe_snk_evaluate_capability_entry(int port)
 	/*
 	 * If we were in a Hard Reset condition we have to delay until
 	 * now to let the TC know we are back to a stable connection.
-	 * Inform the TC Layer the Hard Reset is complete
+	 * TC layer can now go unattached
 	 */
-	tc_hard_reset_complete(port);
+	tc_hard_reset_allow_unattach(port);
 
 	/* Set to highest revision supported by both ports. */
 	prl_set_rev(port, TCPC_TX_SOP,
@@ -2750,7 +2750,7 @@ static void pe_snk_transition_to_default_entry(int port)
 	tcpm_enable_auto_discharge_disconnect(port, 0);
 
 	/* Inform the TC Layer of Hard Reset */
-	tc_hard_reset(port);
+	tc_hard_reset_request(port);
 }
 
 static void pe_snk_transition_to_default_run(int port)
