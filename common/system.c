049bea89cf1de166b442fcafe3a0d9a0c5014b21
Daisuke Nojiri <dnojiri@chromium.org>
2020-05-07 10:51:11 -0700

    PD: Skip pd_prepare_sysjump if PD tasks haven't started
    Currently, pd_prepare_sysjump is skipped when VBOOT_EFS2 is defined.
    Even if VBOOT_EFS2 is enabled, late sysjump can happen when secdata
    kernel is missing or a communication error happens.
    
    This patch makes EC skip pd_prepare_sysjump only when PD tasks
    haven't started.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=chromium:2157728
    BRANCH=none
    TEST=Verified USB devices are enumerated on Trembyle.
    
    Change-Id: I5946fd97fb20b24b9cbcb12aca3bc293651e224f

--- common/system.c
+++ common/system.c
@@ -502,8 +502,7 @@ static void jump_to_image(uintptr_t init_addr)
 	 * PD is enabled because the device is not write protected, EFS2 jumps
 	 * to RW before PD tasks start. So, there is no states to clean up.
 	 */
-	if (!IS_ENABLED(CONFIG_VBOOT_EFS2) &&
-			IS_ENABLED(CONFIG_USB_PD_ALT_MODE_DFP))
+	if (task_start_called() && IS_ENABLED(CONFIG_USB_PD_ALT_MODE_DFP))
 		/* Note: must be before i2c module is locked down */
 		pd_prepare_sysjump();
 
