70da986f5a877d059c57b1e0935496db0b988f5c
Bhanu Prakash Maiya <bhanumaiya@google.com>
2020-05-09 01:18:42 -0700

    fpsensor: cleanup multiple fp spi enable
    BUG=b:147849609
    BRANCH=none
    TEST=1. make buildall -j
    
    Change-Id: I35146070ec20a3605588792fc19595dc1c1ea3cf
    Signed-off-by: Bhanu Prakash Maiya <bhanumaiya@google.com>

--- common/fpsensor/fpsensor.c
+++ common/fpsensor/fpsensor.c
@@ -11,7 +11,6 @@
 #include "ec_commands.h"
 #include "fpsensor.h"
 #include "fpsensor_crypto.h"
-#include "fpsensor_detect.h"
 #include "fpsensor_private.h"
 #include "fpsensor_state.h"
 #include "gpio.h"
@@ -199,15 +198,6 @@ static void fp_process_finger(void)
 void fp_task(void)
 {
 	int timeout_us = -1;
-
-	/* configure the SPI controller (also ensure that CS_N is high) */
-	gpio_config_module(MODULE_SPI_MASTER, 1);
-	spi_enable(CONFIG_SPI_FP_PORT, 1);
-
-	CPRINTS("TRANSPORT_SEL: %s",
-		fp_transport_type_to_str(get_fp_transport_type()));
-	CPRINTS("FP_SENSOR_SEL: %s",
-		fp_sensor_type_to_str(get_fp_sensor_type()));
 
 #ifdef HAVE_FP_PRIVATE_DRIVER
 	/* Reset and initialize the sensor IC */
