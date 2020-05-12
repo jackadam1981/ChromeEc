c200962e1067314ffd1c4441af41c239905fe79e
Yicheng Li <yichengli@chromium.org>
2020-05-12 11:10:11 -0700

    u2f: Add support for versioned key handles
    Support generating and signing versioned key handles in addition
    to non-versioned ones.
    
    BUG=b:144861739
    TEST=used webauthntool to verify that KH generated by old cr50 firmware
    	can be signed with this firmware
    TEST=used webauthntool to verify that non-versioned KH generated by this
    	firmware can be signed by old cr50 firmware
    	(This and the first TEST proves that non-versioned path is the
    	 same as old firmware.)
    TEST=used webauthntool to verify that non-versioned KH generated by this
    	firmware can be signed by this firmware
    TEST=used webauthntool to verify that versioned KH generated by this
    	firmware can be signed by this firmware
    
    Change-Id: Idf413a1a3e6c35a3e7e651faaa91fe2894b805db
    Signed-off-by: Yicheng Li <yichengli@chromium.org>

--- board/cr50/u2f.c
+++ board/cr50/u2f.c
@@ -177,34 +177,41 @@ int u2f_origin_key(const uint8_t *seed, p256_int *d)
 					   (const uint8_t *)tmp) == 0;
 }
 
-int u2f_origin_user_keyhandle(const uint8_t *origin,
-			      const uint8_t *user,
-			      const uint8_t *origin_seed,
+int u2f_origin_user_keyhandle(const uint8_t *origin, const uint8_t *user,
+			      const uint8_t *origin_seed, uint8_t version,
 			      uint8_t *key_handle)
 {
 	LITE_HMAC_CTX ctx;
 	struct u2f_state *state = get_state();
+	size_t hmac_offset;
 
 	if (!state)
 		return EC_ERROR_UNKNOWN;
 
 	memcpy(key_handle, origin_seed, P256_NBYTES);
 
+	if (version == 0) {
+		hmac_offset = P256_NBYTES;
+	} else {
+		key_handle[P256_NBYTES] = version;
+		hmac_offset = P256_NBYTES + U2F_KH_VERSION_SIZE;
+	}
+
 	DCRYPTO_HMAC_SHA256_init(&ctx, state->salt_kek, SHA256_DIGEST_SIZE);
 	HASH_update(&ctx.hash, origin, P256_NBYTES);
 	HASH_update(&ctx.hash, user, P256_NBYTES);
 	HASH_update(&ctx.hash, origin_seed, P256_NBYTES);
+	if (version != 0)
+		HASH_update(&ctx.hash, &version, U2F_KH_VERSION_SIZE);
 
-	memcpy(key_handle + P256_NBYTES,
-	       DCRYPTO_HMAC_final(&ctx), SHA256_DIGEST_SIZE);
+	memcpy(key_handle + hmac_offset, DCRYPTO_HMAC_final(&ctx),
+	       SHA256_DIGEST_SIZE);
 
 	return EC_SUCCESS;
 }
 
-int u2f_origin_user_keypair(const uint8_t *key_handle,
-			    p256_int *d,
-			    p256_int *pk_x,
-			    p256_int *pk_y)
+int u2f_origin_user_keypair(const uint8_t *key_handle, size_t key_handle_size,
+			    p256_int *d, p256_int *pk_x, p256_int *pk_y)
 {
 	uint32_t dev_salt[P256_NDIGITS];
 	uint8_t key_seed[P256_NBYTES];
@@ -221,9 +228,8 @@ int u2f_origin_user_keypair(const uint8_t *key_handle,
 	hmac_drbg_init(&drbg, state->salt_kh, P256_NBYTES, dev_salt,
 		       P256_NBYTES, NULL, 0);
 
-	hmac_drbg_generate(&drbg,
-			   key_seed, sizeof(key_seed),
-			   key_handle, P256_NBYTES * 2);
+	hmac_drbg_generate(&drbg, key_seed, sizeof(key_seed), key_handle,
+			   key_handle_size);
 
 	if (!DCRYPTO_p256_key_from_bytes(pk_x, pk_y, d, key_seed))
 		return EC_ERROR_TRY_AGAIN;
--- include/u2f_impl.h
+++ include/u2f_impl.h
@@ -58,13 +58,13 @@ int u2f_origin_key(const uint8_t *seed, p256_int *d);
  *
  * @param origin pointer to origin id
  * @param user pointer to user secret
- * @param pointer to origin-specific random seed
+ * @param seed pointer to origin-specific random seed
+ * @param the version byte to pack; 0 if not versioned
  *
  * @return EC_SUCCESS if a valid keypair was created.
  */
-int u2f_origin_user_keyhandle(const uint8_t *origin,
-			      const uint8_t *user,
-			      const uint8_t *seed,
+int u2f_origin_user_keyhandle(const uint8_t *origin, const uint8_t *user,
+			      const uint8_t *seed, uint8_t version,
 			      uint8_t *key_handle);
 
 /**
@@ -73,17 +73,16 @@ int u2f_origin_user_keyhandle(const uint8_t *origin,
  *
  * If pk_x and pk_y are NULL, public key generation will be skipped.
  *
- * @param key_handle pointer to the 64 byte key handle
+ * @param key_handle pointer to the key handle
+ * @param key_handle_size size of the key handle in bytes
  * @param d pointer to ECDSA private key
  * @param pk_x pointer to public key point
  * @param pk_y pointer to public key point
  *
  * @return EC_SUCCESS if a valid keypair was created.
  */
-int u2f_origin_user_keypair(const uint8_t *key_handle,
-			    p256_int *d,
-			    p256_int *pk_x,
-			    p256_int *pk_y);
+int u2f_origin_user_keypair(const uint8_t *key_handle, size_t key_handle_size,
+			    p256_int *d, p256_int *pk_x, p256_int *pk_y);
 
 /***
  * Generate a hardware derived 256b private key.
