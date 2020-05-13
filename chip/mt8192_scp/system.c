a5db906c60ef432d526ca151edd4867203b5c027
Tzung-Bi Shih <tzungbi@chromium.org>
2020-05-13 17:44:39 +0800

    chip/mt8192_scp: support clock
    BRANCH=none
    BUG=b:146213943
    BUG=b:156222893
    TEST=make BOARD=asurada_scp
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Signed-off-by: Weiyi Lu <weiyi.lu@mediatek.com>
    Change-Id: Ib9f043bb5575a6e18fc64479c7b241e11e012b5c

--- chip/mt8192_scp/system.c
+++ chip/mt8192_scp/system.c
@@ -5,6 +5,7 @@
 
 /* System : hardware specific implementation */
 
+#include "csr.h"
 #include "memmap.h"
 #include "registers.h"
 #include "system.h"
@@ -12,6 +13,15 @@
 void system_pre_init(void)
 {
 	memmap_init();
+
+	/* enable CPU and platform low power CG */
+	/* enable CPU DCM */
+	set_csr(CSR_MCTREN, CSR_MCTREN_CG);
+#if 0
+	/* enable BUS DCM */
+	VDNR_DCM_TOP_SCP_BUS_U_SCP_BUS_CTRL_0 = BIT(1);
+#endif
+
 	/* Disable jump (it has only RW) and enable MPU. */
 	/* TODO: implement MPU */
 	system_disable_jump();
