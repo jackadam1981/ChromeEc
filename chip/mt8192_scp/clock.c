6109c1f60fe5966e96c48c4bbbc86fddb41f4906
Tzung-Bi Shih <tzungbi@chromium.org>
2020-05-13 15:43:32 +0800

    chip/mt8192_scp: add dummy chip implementations
    BRANCH=none
    BUG=b:146213943
    TEST=make buildall
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: Ib5da01056c5624eae1efc14e64638c052f74c50c

---
+++ chip/mt8192_scp/clock.c
@@ -0,0 +1,16 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Clocks, PLL and power settings */
+
+#include "clock.h"
+
+void clock_init(void)
+{
+}
+
+void clock_refresh_console_in_use(void)
+{
+}
