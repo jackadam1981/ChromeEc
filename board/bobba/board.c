3409f1d269100c4bb652404e1a12667902734b50
Ben Chen <ben.chen2@quanta.corp-partner.google.com>
2020-05-13 10:02:23 +0800

    Bobba: Add ppc C1 syv682x config
    BUG=none
    BRANCH=test
    TEST=check power on, plug in AC after battery cutoff
    
    Change-Id: Ib11b5fef73482b08eb617e1df486df6f2bdfa119
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- board/bobba/board.c
+++ board/bobba/board.c
@@ -17,6 +17,8 @@
 #include "driver/accelgyro_bmi160.h"
 #include "driver/charger/bd9995x.h"
 #include "driver/ppc/nx20p348x.h"
+#include "driver/ppc/syv682x.h"
+
 #include "driver/sync.h"
 #include "driver/tcpm/anx7447.h"
 #include "driver/tcpm/ps8xxx.h"
@@ -78,7 +80,7 @@ static void ppc_interrupt(enum gpio_signal signal)
 		break;
 
 	case GPIO_USB_PD_C1_INT_ODL:
-		nx20p348x_interrupt(1);
+		syv682x_interrupt(1);
 		break;
 
 	default:
--- common/system.c
+++ common/system.c
@@ -849,6 +849,7 @@ static int handle_pending_reboot(enum ec_reboot_cmd cmd)
 	case EC_REBOOT_JUMP_RO:
 		return system_run_image_copy(EC_IMAGE_RO);
 	case EC_REBOOT_JUMP_RW:
+	return EC_SUCCESS;
 		return system_run_image_copy(system_get_active_copy());
 	case EC_REBOOT_COLD:
 #ifdef HAS_TASK_PDCMD
