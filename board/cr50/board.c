892d405f11227dd86c2a475d2aac12751f0d1295
Namyoon Woo <namyoon@google.com>
2020-05-14 12:18:42 -0700

    Introduce LONG_INT_AP_PULSE in TPM_BOARD_CFG register
    BUG=b:148691139
    TEST=tested on atlas and on grunt/careena.
    
    1. Checked the default TPM_BOARD_CFG (PWRDN_SCRATCH21) value was zero
       and the INT_AP_L assertion duration was 4~10 microseconds.
      > md 0x400000f4 1     // memory dump on GC_PMU_PWRDN_SCRATCH21
      400000F4: 0x00000000
    
    2. Attempted to change the board configuration (with a hacked UART
       command.). The register value was unchanged.
      > brdcfg 0x01
      TPM_BOARD_CFG = 0x00000000
    
      > md 0x400000f4 1
      400000F4: 0x00000000
    
    3. Forced to write the board configuration with a hacked UART command.
       The register value was changed.
      > brdcfg 0x01 force
      TPM_BOARD_CFG = 0x80000001
    
      > md 0x400000f4 1
      400000F4: 0x80000001
    
    4. Checked the INT_AP_L assertion duration extended to 110
       microseconds or longer.
    
    5. After cr50 deep sleep, checked the pulse duration was still
       extended.
      - turned AP off.
      - disconnected Suzy-Qable.
      - waited three seconds
      - connected Suzy-Qable, and checked the reset cause was 'hibernate
        rbox'.
      > md 0x400000f4 1
      400000F4: 0x8000001
    
    Signed-off-by: Namyoon Woo <namyoon@google.com>
    Change-Id: I2b9f9defb63cf05f9d91b741ccb4b49c4c6bc8e2

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -1781,6 +1781,15 @@ int board_nvmem_legacy_check_needed(void)
 	return (h->major_ <= 2) || (h->minor_ <= 18);
 }
 
+static void deferred_process_board_cfg(void)
+{
+	uint32_t tpm_board_cfg = GREG32(PMU, PWRDN_SCRATCH21);
+
+	if (tpm_board_cfg & BITMASK_LONG_INT_AP_PULSE)
+		int_ap_extension_enable();
+}
+DECLARE_DEFERRED(deferred_process_board_cfg);
+
 /*
  * TPM_BOARD_CFG write is allowed from TPM reset until TPM2_PCR_Extend command
  * is requested.
@@ -1805,6 +1814,9 @@ void board_cfg_reg_write(uint32_t value)
 
 	/* Store the tpm_board_cfg in power-down scratch. */
 	GREG32(PMU, PWRDN_SCRATCH21) = value|BITMASK_PROGRAMMED_LOCKED;
+
+	/* Process board_configuration change in a deferred function */
+	hook_call_deferred(&deferred_process_board_cfg_data, 0);
 }
 
 uint32_t board_cfg_reg_read(void)
