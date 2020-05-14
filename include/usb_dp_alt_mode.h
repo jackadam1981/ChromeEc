6dfbe81c6f5c15343e34c2753cb618e33118fc4f
Abe Levkoy <alevkoy@chromium.org>
2020-05-14 10:41:45 -0600

    TCPMv2: Separate DPM and DP from PE
    Create separate modules representing DisplayPort (which decides whether
    and how we can set up the DP alt mode) and the Device Policy Manager
    (which decides whether we should). The end goal is to have essentially
    all code with DP-specific decisions or information in the DP module and
    all code with DPM-specific decisions or information in the DPM module.
    This will allow the PE to map as directly as practical to its behaviors
    defined in the PD spec.
    
    Essentially detach PE_DO_PORT_DISCOVERY from the rest of the PE state
    machine in preparation for deleting it.
    
    This change paves the way for
    1) Allowing the AP to drive mode entry and
    2) Allowing new alt modes to be supported via largely self-contained
       modules.
    
    BUG=b:155890173
    TEST=Attach DP dongle; observe discovery and DP setup via Twinkie
    BRANCH=none
    
    Change-Id: Ie63a2e62d1ac6178722477dc53244898a04ef92f
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>

---
+++ include/usb_dp_alt_mode.h
@@ -0,0 +1,51 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/*
+ * DisplayPort alternate mode support
+ * Refer to VESA DisplayPort Alt Mode on USB Type-C Standard, version 2.0,
+ * section 5.2
+ */
+
+#ifndef __CROS_EC_USB_DP_ALT_MODE_H
+#define __CROS_EC_USB_DP_ALT_MODE_H
+
+#include "stdbool.h"
+
+/*
+ * Initialize DP state for the specified port.
+ *
+ * @param port USB-C port number
+ */
+void dp_init(int port);
+
+/*
+ * Handle received DisplayPort VDM ACKs.
+ *
+ * @param port USB-C port number
+ * @param cmd  VDM command from ACK
+ */
+void dp_vdm_cmd_acked(int port, int cmd);
+
+/*
+ * Reset the DisplayPort VDM state for the specified port, as when exiting
+ * DisplayPort mode.
+ *
+ * @param port USB-C port number
+ */
+void dp_reset_next_command(int port);
+
+/*
+ * Construct the next DisplayPort VDM that should be sent.
+ *
+ * @param port      USB-C port number
+ * @param vdo_count The number of VDOs in vdm; must be at least VDO_MAX_SIZE
+ * @param vdm       The VDM payload to be sent; output; must point to at least
+ *                  VDO_MAX_SIZE elements
+ * @return          The number of VDOs written to VDM or -1 to indicate error
+ */
+int dp_setup_next_vdm(int port, int vdo_count, uint32_t *vdm);
+
+#endif  /* __CROS_EC_USB_DP_ALT_MODE_H */
