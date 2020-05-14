e282ec6ea950930c413ddc8268dd064cebd9d74c
Namyoon Woo <namyoon@google.com>
2020-05-14 12:18:42 -0700

    Introduce LONG_INT_AP_PULSE in TPM_BOARD_CFG register
    BUG=b:148691139
    TEST=tested on atlas and on grunt/careena.
    
    1. check the default brdcfg is zero and the INT_AP_L assertion duration
       is 4~10 microseconds.
      cr50> brdcfg
      TPM_BOARD_CFG = 0x00000000
    
    2. changed the board configuration (with a test code.)
      cr50> brdcfg
      TPM_BOARD_CFG = 0x8000001
    
    3. checked the INT_AP_L assertion duration extended to 110 microseconds
       or longer.
    
    4. checked the board configuration and the pulse after cr50 deep sleep.
      - turned AP off.
      - disconnected Suzy-Qable.
      - wait three seconds
      - connect Suzy-Qable, and checked reset cause is 'hibernate rbox'.
      cr50> brdcfg
      TPM_BOARD_CFG = 0x8000001
    
    Signed-off-by: Namyoon Woo <namyoon@google.com>
    Change-Id: I2b9f9defb63cf05f9d91b741ccb4b49c4c6bc8e2

--- chip/g/i2cs.c
+++ chip/g/i2cs.c
@@ -72,6 +72,7 @@
 #include "registers.h"
 #include "system.h"
 #include "task.h"
+#include "tpm_board_cfg.h"
 
 #define REGISTER_FILE_SIZE BIT(6) /* 64 bytes. */
 #define REGISTER_FILE_MASK (REGISTER_FILE_SIZE - 1)
@@ -429,10 +430,9 @@ int i2cs_register_write_complete_handler(wr_complete_handler_f wc_handler)
 	write_complete_handler_ = wc_handler;
 	task_enable_irq(GC_IRQNUM_I2CS0_INTR_WRITE_COMPLETE_INT);
 
-	/*
-	 *TODO: call int_ap_extension_enable() if TPM_BOARD_CFG has
-	 * LONG_INT_AP_PULSE set
-	 */
+	if (board_cfg_reg_read() & BITMASK_LONG_INT_AP_PULSE)
+		int_ap_extension_enable();
+
 	/*
 	 * Start a self perpetuating polling function to check for 'hosed'
 	 * condition periodically.
