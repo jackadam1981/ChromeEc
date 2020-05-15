2f5070204b974937fc21c05c211f1e380495aad1
Vadim Bendebury <vbendeb@chromium.org>
2020-05-14 18:06:37 -0700

    close RO window after use
    The code opening the RO windows to make it possible to update or
    enable the RO images, leaves the window open, allowing subsequent
    writes into the RO space. It has been acceptable until now, because RO
    updates are usually followed up by reboots.
    
    With introduction of the AP RO hash, there is a need to close the
    window (specifically, disable write access) when not in use. This
    patch adds a function for that and uses the new function everywhere
    where flash_open_ro_window() is called.
    
    BUG=b:153764696
    TEST=verified successful Cr50 RO and AP RO hash  updates.
    
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Change-Id: Ia595e5c7ce0beb1a67ef3513117984d18655a60c

--- chip/g/upgrade_fw.c
+++ chip/g/upgrade_fw.c
@@ -115,13 +115,16 @@ static uint8_t check_update_chunk(uint32_t block_offset, size_t body_size)
 		if (block_offset == valid_sections.ro_base_offset) {
 			uint32_t base;
 			uint32_t size;
+			int rv;
 
 			base = valid_sections.ro_base_offset;
 			size = valid_sections.ro_top_offset -
 				valid_sections.ro_base_offset;
 			/* backup RO area write access needs to be enabled. */
 			flash_open_ro_window(base, size);
-			if (flash_physical_erase(base, size) != EC_SUCCESS) {
+			rv = flash_physical_erase(base, size);
+			flash_close_ro_window();
+			if (rv != EC_SUCCESS) {
 				CPRINTF("%s:%d erase failure of 0x%x..+0x%x\n",
 					__func__, __LINE__, base, size);
 				return UPGRADE_ERASE_FAILURE;
@@ -400,6 +403,7 @@ void fw_upgrade_command_handler(void *body,
 	uint8_t *error_code = body;  /* Cache the address for code clarity. */
 	size_t body_size;
 	uint32_t block_offset;
+	int rv;
 
 	*response_size = 1; /* One byte response unless this is a start PDU. */
 
@@ -507,8 +511,15 @@ void fw_upgrade_command_handler(void *body,
 	}
 
 	CPRINTF("at 0x%x\n", block_offset + CONFIG_PROGRAM_MEMORY_BASE);
-	if (flash_physical_write(block_offset, body_size, upgrade_data)
-	    != EC_SUCCESS) {
+	if (block_offset < valid_sections.ro_top_offset)
+		flash_open_ro_window(valid_sections.ro_base_offset,
+				     valid_sections.ro_top_offset -
+					     valid_sections.ro_base_offset);
+	rv = flash_physical_write(block_offset, body_size, upgrade_data);
+	if (block_offset < valid_sections.ro_top_offset)
+		flash_close_ro_window();
+
+	if (rv != EC_SUCCESS) {
 		*error_code = UPGRADE_WRITE_FAILURE;
 		CPRINTF("%s:%d upgrade write error\n",	__func__, __LINE__);
 		return;
