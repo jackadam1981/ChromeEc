3d3fda106deefd489e2e32037d8c96e0ddb4be41
Vadim Bendebury <vbendeb@chromium.org>
2020-05-15 16:26:03 -0700

    ap_ro_flash.py: add new values and clean up
    Add new error values reported by the VENDOR_CC_SEED_AP_RO_CHECK vendor
    command, and clean up the command line error processing to report all
    collected errors instead of just complaining that the ranges were not
    specified in the command line.
    
    BUG=b:153764696
    TEST=used the script and observed expected error values reported.
    
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Change-Id: I0f5e6a28776af2afc550bd2c44e6cc3a0cb80153

--- util/ap_ro_hash.py
+++ util/ap_ro_hash.py
@@ -514,6 +514,9 @@ def main(args):
         3 : 'Bad offset value',
         4 : 'Bad range size',
         5 : 'Already programmed',
+        6 : 'Flash write failed',
+        7 : 'Flash erase failed',
+        8 : 'BID programmed',
         VENDOR_RC_NO_SUCH_COMMAND_ERROR : 'Insufficient C50 version',
     }
 
@@ -545,9 +548,6 @@ def main(args):
                 continue
             ranges.append(fmap[arg])
 
-        if not ranges:
-            raise ApRoHashCmdLineError('no hashing ranges specified')
-
         error_msg = ''
         if bad_ranges:
             error_msg += 'Ranges %s not valid\n' % ' '.join(bad_ranges)
@@ -558,10 +558,15 @@ def main(args):
         # Make sure the list is sorted by the first element.
         ranges.sort(key=lambda x: x[0])
 
-        # Make sure ranges do not overlap and fall into the WP_RO section.
-        error_msg += verify_ranges(ranges, fmap['WP_RO'])
+        if ranges:
+            # Make sure ranges do not overlap and fall into the WP_RO section.
+            error_msg += verify_ranges(ranges, fmap['WP_RO'])
+
         if error_msg:
             raise ApRoHashCmdLineError(error_msg)
+
+        if not ranges:
+            raise ApRoHashCmdLineError('no hashing ranges specified')
 
         ro_check_file = os.path.join(tmpd, 'ro_check.bin')
         read_ro_ranges(ro_check_file, tmpd, ranges)
