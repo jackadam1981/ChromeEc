bb6a61ef30f0a4c5cf67c6c1d11522d82bf157ed
Gwendal Grignou <gwendal@chromium.org>
2020-05-17 16:43:02 -0700

    motion_sense: Make change in range permanent
    When AP changes range, unlike offset or ODR, it was not surviving init()
    call. If the sensor is powered off in S3, at resume the range would be
    back to the default.
    
    To make it consistent with other attributes, remember range change until
    EC powers down.
    
    - remove get_range
    - add current_range to store the range currently used.
      This is modifiable by the AP
    - when the AP shutdown, revert current_range to default_range
    - Remove const attribute for sensor structure when init and set_range is
      called.
    
    BUG=chromium:1083791
    BRANCH=none
    TEST=One eve branch, check range is preserved even after 'shutdown -h 0'
    
    Change-Id: Ia7126ac0cc9c3fef60b4464d95d6dd15e64b0fc4
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- driver/accelgyro_bmi160.c
+++ driver/accelgyro_bmi160.c
@@ -211,16 +211,15 @@ static int set_offset(const struct motion_sensor_t *s,
 	return ret;
 }
 
-static int perform_calib(const struct motion_sensor_t *s, int enable)
+static int perform_calib(struct motion_sensor_t *s, int enable)
 {
-	int ret, val, en_flag, status, rate, range;
+	int ret, val, en_flag, status, rate, range = s->current_range;
 	timestamp_t deadline, timeout;
 
 	if (!enable)
 		return EC_SUCCESS;
 
 	rate = bmi_get_data_rate(s);
-	range = bmi_get_range(s);
 	/*
 	 * Temporary set frequency to 100Hz to get enough data in a short
 	 * period of time.
@@ -573,7 +572,7 @@ static int irq_handler(struct motion_sensor_t *s, uint32_t *event)
 }
 #endif  /* CONFIG_ACCEL_INTERRUPTS */
 
-static int init(const struct motion_sensor_t *s)
+static int init(struct motion_sensor_t *s)
 {
 	int ret = 0, tmp, i;
 	struct accelgyro_saved_data_t *saved_data = BMI_GET_SAVED_DATA(s);
@@ -718,7 +717,6 @@ static int init(const struct motion_sensor_t *s)
 	 * so set data rate to 0.
 	 */
 	saved_data->odr = 0;
-	bmi_set_range(s, s->default_range, 0);
 
 	if (s->type == MOTIONSENSE_TYPE_ACCEL) {
 #ifdef CONFIG_ACCEL_INTERRUPTS
@@ -733,7 +731,6 @@ const struct accelgyro_drv bmi160_drv = {
 	.init = init,
 	.read = bmi_read,
 	.set_range = bmi_set_range,
-	.get_range = bmi_get_range,
 	.get_resolution = bmi_get_resolution,
 	.set_data_rate = set_data_rate,
 	.get_data_rate = bmi_get_data_rate,
