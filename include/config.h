afb5c3a8b5873ed515d00f1afd6b2a8ca8c922d6
Scott Collyer <scollyer@google.com>
2020-05-17 19:20:25 -0700

    tcpm: Add tpcm driver for stm32gx chips
    This CL adds tcpm driver for the STM32Gx family of chips which have a
    ucpd peripheral for its on-chip TCPC.
    
    BUG=b:148493929
    BRANCH=None
    TEST=
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Ia23eb4e346682ab3a9f8c7da2b5519c330b67936

--- include/config.h
+++ include/config.h
@@ -4109,6 +4109,10 @@
 #undef CONFIG_USB_PD_TCPM_PS8705
 #undef CONFIG_USB_PD_TCPM_PS8805
 #undef CONFIG_USB_PD_TCPM_PS8815
+#undef CONFIG_USB_PD_TCPM_MT6370
+#undef CONFIG_USB_PD_TCPM_TUSB422
+#undef CONFIG_USB_PD_TCPM_RAA489000
+#undef CONFIG_USB_PD_TCPM_STM32GX
 
 /*
  * Defined automatically by chip and depends on chip. This guards the onboard
