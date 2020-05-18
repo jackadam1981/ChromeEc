5863f21194d06be9cb8f7a2ac6555310ef09377f
Scott Collyer <scollyer@google.com>
2020-05-17 19:21:20 -0700

    quiche: Enable TCPM v2.0
    This CL adds boards specific parts required to enable TCPMv2.0
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I2a4c3bf4089fb3e167d06921b177d8c4e61a021f

--- baseboard/honeybuns/usb_pd_policy.c
+++ baseboard/honeybuns/usb_pd_policy.c
@@ -1,36 +1,36 @@
-/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-/* Shared USB-C policy for Hatch boards */
-
-#include "charge_manager.h"
 #include "common.h"
-#include "compile_time_macros.h"
 #include "console.h"
-#include "ec_commands.h"
-#include "gpio.h"
-#include "system.h"
-#include "tcpci.h"
-#include "tcpm.h"
-#include "usb_mux.h"
+#include "driver/tcpm/tcpci.h"
 #include "usb_pd.h"
 #include "usbc_ppc.h"
-#include "util.h"
 
 #define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
 
+/*
+ * TODO(b/167711550): These 4 functions need to be implemented for honeybuns
+ * and are required to build with TCPMv2 enabled. Currently, they only allow the
+ * build to work. They will be implemented in a subsequent CL.
+ */
+
 int pd_check_vconn_swap(int port)
 {
-	/* Only allow vconn swap if pp5000_A rail is enabled */
-	return gpio_get_level(GPIO_EN_PP5000_A);
+	/*TODO: Dock is the Vconn source */
+//	return EC_SUCCESS;
+	return 1;
 }
 
 void pd_power_supply_reset(int port)
 {
 	int prev_en;
+
+	if (port < 0 || port >= CONFIG_USB_PD_PORT_MAX_COUNT)
+		return;
 
 	prev_en = ppc_is_sourcing_vbus(port);
 
@@ -54,17 +54,14 @@ int pd_set_power_supply_ready(int port)
 {
 	int rv;
 
-	/* Disable charging. */
-	rv = ppc_vbus_sink_enable(port, 0);
-	if (rv)
-		return rv;
-
-	pd_set_vbus_discharge(port, 0);
-
-	/* Provide Vbus. */
+	/*
+	 * Default operation of buck-boost is 5v/3.6A.
+	 * Turn on the PPC Provide Vbus.
+	 */
 	rv = ppc_vbus_source_enable(port, 1);
 	if (rv)
 		return rv;
+
 
 #ifdef CONFIG_USB_PD_MAX_SINGLE_SOURCE_CURRENT
 	/* Ensure we advertise the proper available current quota */
@@ -77,15 +74,18 @@ int pd_set_power_supply_ready(int port)
 	return EC_SUCCESS;
 }
 
-#ifdef CONFIG_USB_PD_VBUS_DETECT_PPC
 int pd_snk_is_vbus_provided(int port)
 {
 	return ppc_is_vbus_present(port);
 }
-#endif
 
 int board_vbus_source_enabled(int port)
 {
 	return ppc_is_sourcing_vbus(port);
 }
 
+void pd_set_input_current_limit(int port, uint32_t max_ma,
+				uint32_t supply_voltage)
+{
+
+}
