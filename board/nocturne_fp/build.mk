c2ed5d3174ccf5da5c08ea05d6479234d4df6fe5
Tom Hughes <tomhughes@chromium.org>
2020-05-18 14:49:43 -0700

    test: Add test for flash write protection
    This test demonstrates the inconsistency between STM32H743 (dartmonkey)
    and STM32F412 (bloonchipper).
    
    BRANCH=none
    BUG=b:155897971
    TEST=On bloonchipper:
         * Enable HW WP: dut-control fw_wp_en:on
         * Reboot to RO: reboot ro
         * Enable flash protection: runtest 1
           => PASS
         * Reboot to RO: reboot ro
         * Try to disable flash protection: runtest 2
           => FAIL
    TEST=On dartmonkey:
         * Enable HW WP: dut-control fw_wp_en:on
         * Reboot to RO: reboot ro
         * Enable flash protection: runtest 1
           => PASS
         * Reboot to RO: reboot ro
         * Try to disable flash protection: runtest 2
           => PASS
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I6eb69257f84f79a6609984efbdad7dd37803c8f6

--- board/nocturne_fp/build.mk
+++ board/nocturne_fp/build.mk
@@ -16,6 +16,7 @@ test-list-y=\
        aes \
        compile_time_macros \
        crc32 \
+       flash_write_protect \
        mutex \
        pingpong \
        rollback \
--- board/nucleo-f412zg/build.mk
+++ board/nucleo-f412zg/build.mk
@@ -13,6 +13,7 @@ test-list-y=\
        aes \
        compile_time_macros \
        crc32 \
+       flash_write_protect \
        mutex \
        pingpong \
        rollback \
