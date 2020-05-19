2791cc7d60dd04f5b3b03284d32b31f28945e4e4
Daisuke Nojiri <dnojiri@chromium.org>
2020-05-19 10:10:48 -0700

    efs2: fix shutdown hook
    WIP
    
    Change-Id: I0aa3c60b36f5bc729af33898c932f5ffcfbf08c3

--- common/power_button_x86.c
+++ common/power_button_x86.c
@@ -246,6 +246,9 @@ static void set_initial_pwrbtn_state(void)
 		CPRINTS("PB init-off");
 		power_button_pch_release();
 		return;
+	} else if (reset_flags & EC_RESET_FLAG_AP_IDLE) {
+		CPRINTS("PB idle");
+		return;
 	}
 
 #ifdef CONFIG_BRINGUP
