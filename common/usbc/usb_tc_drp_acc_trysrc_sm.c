26a069389c278cd5a7e27802fce87fe1777871c0
Wai-Hong Tam <waihong@google.com>
2020-05-20 09:47:47 -0700

    TCPMv2: Restore the USB mux on chipset transitioning to ON
    When the chipset power state transitioning to OFF, it calls
    set_usb_mux_with_current_data_role(), which actually disconnects
    both USB and DP mux. Fixed the comment.
    
    When the chipset power state transitioning to ON, should call
    set_usb_mux_with_current_data_role() to restore the previous USB mux.
    The DP mux is restored by entering any previously exited alt modes.
    
    BRANCH=None
    BUG=b:156552979
    TEST=On Lazor, attached a servo v4 with power but not DP to port-0.
    > typec 0
    Port 0: USB=1 DP=0 POLARITY=INVERTED HPD_IRQ=0 HPD_LVL=0 SAFE=0 TBT=0...
    > power off
    > typec 0
    Port 0: USB=0 DP=0 POLARITY=NORMAL HPD_IRQ=0 HPD_LVL=0 SAFE=0 TBT=0...
            ^^^^^ USB mux turned OFF, as expected
    > power on
    > typec 0
    Port 0: USB=1 DP=0 POLARITY=INVERTED HPD_IRQ=0 HPD_LVL=0 SAFE=0 TBT=0...
            ^^^^^ USB mux restored to ON, as expected
    
    TEST=Followed the above setup, toggled servo warm_reset on -> off.
    > typec 0
    Port 0: USB=1 DP=0 POLARITY=INVERTED HPD_IRQ=0 HPD_LVL=0 SAFE=0 TBT=0...
            ^^^^^ USB mux restored to ON, as expected
    
    TEST=On Lazor, attached a Type-C dongle with DP to port-0.
    > typec 0
    Port 0: USB=1 DP=1 POLARITY=NORMAL HPD_IRQ=0 HPD_LVL=0 SAFE=0 TBT=0...
    > power off
    > typec 0
    Port 0: USB=0 DP=0 POLARITY=NORMAL HPD_IRQ=0 HPD_LVL=0 SAFE=0 TBT=0...
            ^^^^^ ^^^^ USB and DP mux turned OFF, as expected
    > power on
    > typec 0
    Port 0: USB=1 DP=1 POLARITY=NORMAL HPD_IRQ=0 HPD_LVL=0 SAFE=0 TBT=0...
            ^^^^^ ^^^^ USB and DP mux restored to ON, as expected
    
    Change-Id: I7501fb61d68077869c09345ee8012bf3822e54db
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -1378,14 +1378,16 @@ static void handle_new_power_state(int port)
 			 */
 			pe_exit_dp_mode(port);
 
-			/*
-			 * Reset mux to USB. DP mode is selected
-			 * again at boot up.
-			 */
+			/* Disconnect both USB and DP mux. */
 			set_usb_mux_with_current_data_role(port);
 		} else if (chipset_in_or_transitioning_to_state(
 					CHIPSET_STATE_ON)) {
-			/* Enter any previously exited alt modes */
+			/* Restore the USB mux */
+			set_usb_mux_with_current_data_role(port);
+			/*
+			 * Restore the DP mux by entering any previously exited
+			 * exited alt modes
+			 */
 			pe_dpm_request(port, DPM_REQUEST_PORT_DISCOVERY);
 		}
 	}
