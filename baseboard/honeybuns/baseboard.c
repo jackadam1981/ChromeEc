7b3325ea1ebb80bf7cc6fbab2f8b85ff32f05a5d
Scott Collyer <scollyer@google.com>
2020-05-22 11:46:27 -0700

    honeybuns: Enable I2C feature
    This CL adds config options and tables required to pull in i2c support
    for honeybuns.
    
    BUG=b:148493929
    BRANCH=None
    TEST=ran i2cscan on quiche
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Ib1b8b795a2a07a5b1cbe167b17c3f70a8d9703b2

--- baseboard/honeybuns/baseboard.c
+++ baseboard/honeybuns/baseboard.c
@@ -31,6 +31,15 @@ static int board_power_sequence(void)
 	return EC_SUCCESS;
 }
 
+/******************************************************************************/
+/* I2C port map configuration */
+const struct i2c_port_t i2c_ports[] = {
+	{"usbc",   I2C_PORT_USBC,   400, GPIO_EC_I2C1_SCL, GPIO_EC_I2C1_SDA},
+	{"usb_mst",  I2C_PORT_MST,  400, GPIO_EC_I2C2_SCL, GPIO_EC_I2C2_SDA},
+	{"eeprom",  I2C_PORT_EEPROM,  400, GPIO_EC_I2C3_SCL, GPIO_EC_I2C3_SDA},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
 static void baseboard_init(void)
 {
 	/* Turn on power rails */
