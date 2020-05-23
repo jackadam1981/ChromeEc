c36029fafa72ba3aa9e4e0525b7989eeeba0c330
Scott Collyer <scollyer@google.com>
2020-05-22 17:00:24 -0700

    tcpmv2.0: Add guards for config options
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Iee0764f37b401970b18efc64bf7519e209a22ea7

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -870,6 +870,7 @@ void pe_invalidate_explicit_contract(int port)
 		typec_update_cc(port);
 }
 
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 /*
  * Determine if this port may communicate with the cable plug.
  *
@@ -901,7 +902,9 @@ static bool pe_can_send_sop_prime(int port)
 		return false;
 	}
 }
+#endif
 
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 /*
  * Determine if this port may send the given VDM type
  *
@@ -935,6 +938,7 @@ static bool pe_can_send_sop_vdm(int port, int vdm_cmd)
 
 	return false;
 }
+#endif
 
 static void pe_send_soft_reset(const int port, enum tcpm_transmit_type type)
 {
@@ -1651,6 +1655,7 @@ static void pe_src_discovery_run(int port)
 		}
 	}
 
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 	/*
 	 * Note: While the DiscoverIdentityTimer is only required in an explicit
 	 * contract, we use it here to ensure we space any potential BUSY
@@ -1663,7 +1668,7 @@ static void pe_src_discovery_run(int port)
 		set_state_pe(port, PE_VDM_IDENTITY_REQUEST_CBL);
 		return;
 	}
-
+#endif
 	/*
 	 * Transition to the PE_SRC_Disabled state when:
 	 *   1) The Port Partners have not been PD Connected.
@@ -2071,13 +2076,14 @@ static void pe_src_ready_run(int port)
 		PE_CLR_FLAG(port, PE_FLAGS_FIRST_MSG);
 		pe[port].wait_and_add_jitter_timer = TIMER_DISABLED;
 
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 		/*
 		 * Attempt discovery if possible, and return if state was
 		 * changed for that discovery.
 		 */
 		if (pe_attempt_port_discovery(port))
 			return;
-
+#endif
 		/*
 		 * Handle Device Policy Manager Requests
 		 */
@@ -2810,14 +2816,14 @@ static void pe_snk_ready_run(int port)
 			set_state_pe(port, PE_SNK_SELECT_CAPABILITY);
 			return;
 		}
-
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 		/*
 		 * Attempt discovery if possible, and return if state was
 		 * changed for that discovery.
 		 */
 		if (pe_attempt_port_discovery(port))
 			return;
-
+#endif
 		/*
 		 * Handle Device Policy Manager Requests
 		 */
--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -442,12 +442,15 @@ static void pd_update_try_source(void);
 
 static void sink_stop_drawing_current(int port);
 
+
+#ifdef CONFIG_USB_PD_TRY_SRC
 static bool is_try_src_enabled(int port)
 {
 	return IS_ENABLED(CONFIG_USB_PD_TRY_SRC) &&
 		((pd_try_src_override == TRY_SRC_OVERRIDE_ON) ||
 		(pd_try_src_override == TRY_SRC_NO_OVERRIDE && pd_try_src));
 }
+#endif
 
 /*
  * Public Functions
@@ -579,6 +582,7 @@ static void tc_enable_pd(int port, int en)
 
 }
 
+#ifdef CONFIG_USB_PD_TRY_SRC
 static void tc_enable_try_src(int en)
 {
 	if (en)
@@ -586,6 +590,7 @@ static void tc_enable_try_src(int en)
 	else
 		atomic_clear(&pd_try_src, 1);
 }
+#endif
 
 static void tc_detached(int port)
 {
@@ -600,8 +605,10 @@ static inline void pd_set_dual_role_and_event(int port,
 {
 	drp_state[port] = state;
 
+#ifdef CONFIG_USB_PD_TRY_SRC
 	if (IS_ENABLED(CONFIG_USB_PD_TRY_SRC))
 		pd_update_try_source();
+#endif
 
 	if (event != 0)
 		task_set_event(PD_PORT_TO_TASK_ID(port), event, 0);
@@ -857,6 +864,7 @@ void tc_disc_ident_complete(int port)
 	TC_CLR_FLAG(port, TC_FLAGS_DISC_IDENT_IN_PROGRESS);
 }
 
+#ifdef CONFIG_USB_PD_TRY_SRC
 void tc_try_src_override(enum try_src_override_t ov)
 {
 	if (IS_ENABLED(CONFIG_USB_PD_TRY_SRC)) {
@@ -877,6 +885,7 @@ enum try_src_override_t tc_get_try_src_override(void)
 {
 	return pd_try_src_override;
 }
+#endif
 
 void tc_snk_power_off(int port)
 {
@@ -1290,8 +1299,11 @@ void tc_state_init(int port)
 		return;
 	}
 
+
+#ifdef CONFIG_USB_PD_TRY_SRC
 	/* Allow system to set try src enable */
 	tc_try_src_override(TRY_SRC_NO_OVERRIDE);
+#endif
 
 	/*
 	 * Set initial PD communication policy.
@@ -2047,9 +2059,11 @@ static void tc_attach_wait_snk_run(const int port)
 	 */
 	if (pd_is_vbus_present(port)) {
 		if (new_cc_state == PD_CC_DFP_ATTACHED) {
+#ifdef CONFIG_USB_PD_TRY_SRC
 			if (is_try_src_enabled(port))
 				set_state_tc(port, TC_TRY_SRC);
 			else
+#endif
 				set_state_tc(port, TC_ATTACHED_SNK);
 		} else {
 			/* new_cc_state is PD_CC_DFP_DEBUG_ACC */
@@ -2703,9 +2717,12 @@ static void tc_attached_src_run(const int port)
 				pd_dfp_exit_mode(port, TCPC_TX_SOP_PRIME_PRIME,
 						0, 0);
 			}
-
-		set_state_tc(port, tryWait ?
-					TC_TRY_WAIT_SNK : TC_UNATTACHED_SNK);
+#ifdef CONFIG_USB_PD_TRY_SRC
+		set_state_tc(port, IS_ENABLED(CONFIG_USB_PD_TRY_SRC) ?
+			TC_TRY_WAIT_SNK : TC_UNATTACHED_SNK);
+#else
+		set_state_tc(port, TC_UNATTACHED_SNK);
+#endif
 		return;
 	}
 
