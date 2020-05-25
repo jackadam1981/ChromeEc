c7cebb2db5a916f6d90f5af3ea3e08382a198508
Scott Collyer <scollyer@google.com>
2020-05-25 15:43:00 -0700

    quiche: (WIP) Add support for USB-PD messaging
    This CL is just a placeholder. It will get replaced by a few CLs which
    break up the changes in more logical groupings.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I772f6681c77ef3c757725e8658a1d3fa08fada08

--- driver/tcpm/stm32gx.c
+++ driver/tcpm/stm32gx.c
@@ -26,6 +26,9 @@
 #error "Unsupported config options of Stm32gx PD driver"
 #endif
 
+#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
+
 /* Wait time for vconn power switch to turn off. */
 #ifndef PD_STM32GX_VCONN_TURN_OFF_DELAY_US
 #define PD_STM32GX_VCONN_TURN_OFF_DELAY_US 500
@@ -36,8 +39,7 @@ static int cached_rp[CONFIG_USB_PD_PORT_MAX_COUNT];
 
 static int stm32gx_tcpm_get_message_raw(int port, uint32_t *buf, int *head)
 {
-
-	return EC_SUCCESS;
+	return stm32gx_ucpd_get_message_raw(port, buf, head);
 }
 
 static int stm32gx_tcpm_init(int port)
@@ -90,18 +92,17 @@ static int stm32gx_tcpm_set_polarity(int port, enum tcpc_cc_polarity polarity)
 
 static int stm32gx_tcpm_set_vconn(int port, int enable)
 {
-
 	return EC_SUCCESS;
 }
 
 static int stm32gx_tcpm_set_msg_header(int port, int power_role, int data_role)
 {
-	return EC_SUCCESS;
+	return stm32gx_ucpd_set_msg_header(port, power_role, data_role);
 }
 
 static int stm32gx_tcpm_set_rx_enable(int port, int enable)
 {
-	return EC_SUCCESS;
+	return stm32gx_ucpd_set_rx_enable(port, enable);
 }
 
 static int stm32gx_tcpm_transmit(int port,
@@ -109,13 +110,12 @@ static int stm32gx_tcpm_transmit(int port,
 			uint16_t header,
 			const uint32_t *data)
 {
-	return EC_SUCCESS;
+	return stm32gx_ucpd_transmit(port, type, header, data);
 }
 
 static int stm32gx_tcpm_get_chip_info(int port, int live,
 			struct ec_response_pd_chip_info_v1 **chip_info)
 {
-
 	return EC_SUCCESS;
 }
 
