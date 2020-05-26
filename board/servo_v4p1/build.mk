781560e2a73be2802f0a5b77e720f43655b69916
Sam Hurst <shurst@google.com>
2020-05-26 10:34:02 -0700

    servo_v4p1: Add INA231S functionality
    This functionality is only available in RO
    
    BRANCH=none
    BUG=b:146793000
    TEST=make -j buildall
    
    Signed-off-by: Sam Hurst <shurst@google.com>
    Change-Id: I37f97e8e882a8a5948aeeb669ca39cc930e32d31

--- board/servo_v4p1/build.mk
+++ board/servo_v4p1/build.mk
@@ -20,6 +20,7 @@ board-y+=ioexpanders.o
 # These files are compiled into RO only
 board-ro+=ccd_measure_sbu.o
 board-ro+=chg_control.o
+board-ro+=ina231s.o
 board-ro+=pathsel.o
 
 all_deps=$(patsubst ro,,$(def_all_deps))
