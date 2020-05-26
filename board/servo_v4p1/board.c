7bf53ae7e40b859c93a30e28e21b8d5a68070896
Sam Hurst <shurst@google.com>
2020-05-26 10:01:04 -0700

    servo_v4p1: Add CCD measurement functionality
    The CCD measurement functionality is only
    available in RO.
    
    BRANCH=none
    BUG=b:146793000
    TEST=make -j buildall
    
    Signed-off-by: Sam Hurst <shurst@google.com>
    Change-Id: I4bf9885a8a76c8d9dbb688a72673726fb0a7efa5

--- board/servo_v4p1/board.c
+++ board/servo_v4p1/board.c
@@ -6,6 +6,7 @@
 
 #include "adc.h"
 #include "adc_chip.h"
+#include "ccd_measure_sbu.h"
 #include "common.h"
 #include "console.h"
 #include "ec_version.h"
@@ -327,6 +328,7 @@ static void board_init(void)
 	gpio_enable_interrupt(GPIO_USBH_I2C_BUSY_INT);
 	gpio_enable_interrupt(GPIO_BC12_INT_ODL);
 
+	ccd_meas_sbu();
 #endif /* SECTION_IS_RO */
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
