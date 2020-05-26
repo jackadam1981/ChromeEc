296b248c95e376e97a2b75168a3fb72f662f78f6
Sam Hurst <shurst@google.com>
2020-05-26 10:34:02 -0700

    servo_v4p1: Add INA231S functionality
    This functionality is only available in RO
    
    BRANCH=none
    BUG=b:146793000
    TEST=make -j buildall
    
    Signed-off-by: Sam Hurst <shurst@google.com>
    Change-Id: I37f97e8e882a8a5948aeeb669ca39cc930e32d31

--- board/servo_v4p1/board.c
+++ board/servo_v4p1/board.c
@@ -14,6 +14,7 @@
 #include "gpio.h"
 #include "hooks.h"
 #include "i2c.h"
+#include "ina231s.h"
 #include "ioexpanders.h"
 #include "pathsel.h"
 #include "queue_policies.h"
@@ -316,6 +317,7 @@ static void board_init(void)
 
 #ifdef SECTION_IS_RO
 	init_uservo_port();
+	init_ina231s();
 	init_pathsel();
 
 	/* Enable DUT USB2.0 pair. */
