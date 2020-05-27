9b9ec40898f863aaf12a1057deb5ec846d3aeeba
Vadim Bendebury <vbendeb@chromium.org>
2020-05-27 10:26:27 -0700

    ap_ro: add handling of the corrupted hash
    This patch closes the AP RO verification loop on the Cr50 side. If the
    check is triggered, the valid AP hash is found and the RO contents is
    found to not match the hash, the Cr50 will start a periodic hook
    keeping the EC in reset which would prevent the Chromebook from
    booting.
    
    The only way to determine the state of the device would be connecting
    to the Cr50 console and checking the flash log contents. Seeing a
    recent log sequence similar to
    
    1590602806:09 01
    1590602819:09 00
    1590602820:09 00
    1590602821:09 03
    1590602823:09 06
    
    would indicate that the device is in the failed AP RO check state. The
    only way out of this state on a production device is powercycle.
    
    BUG=b:153764696
    
    TEST=verified that erasing or programming AP RO hash when board ID is
         set is impossible.
    
         Verified proper shutdown in case AP RO has is present and the AP
         RO space is corrupted.
    
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Change-Id: I1029114126a9a79f80385af7bc8d5467738e04ca

--- board/cr50/power_button.c
+++ board/cr50/power_button.c
@@ -60,6 +60,25 @@ static void power_button_press_enable_interrupt(int enable)
 #ifdef CONFIG_AP_RO_VERIFICATION
 
 /*
+ * A hook used to keep the EC in reset, no matter what keys the user presses,
+ * the only way out is the Cr50 reboot, most likely through power cycle by
+ * battery cutoff.
+ *
+ * Cr50 console over SuzyQ would still be available in case the user has the
+ * cable and wants to see what happens with the system. The easiest way to see
+ * the system is in this state to run the 'flog' command and examine the flash
+ * log.
+ */
+static void keep_ec_in_reset(void);
+DECLARE_DEFERRED(keep_ec_in_reset);
+
+static void keep_ec_in_reset(void)
+{
+	assert_ec_rst();
+	hook_call_deferred(&keep_ec_in_reset_data, 100 * MSEC);
+}
+
+/*
  * Implement sequence detecting trigger for starting AP RO verification.
  *
  * 'RCTD' is short for 'RO check trigger detection'.
@@ -169,7 +188,12 @@ static int rctd_poll_handler(void)
 
 	CPRINTS("RO Validation triggered");
 	ap_ro_add_flash_event(APROF_CHECK_TRIGGERED);
-	validate_ap_ro();
+
+	if (validate_ap_ro() == EC_ERROR_CRC) {
+		/* Validation failed, no go. */
+		keep_ec_in_reset();
+	}
+
 	return 0;
 }
 
