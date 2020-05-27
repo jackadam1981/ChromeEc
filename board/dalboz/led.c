2ada50f1484fbb4734868f405552bd38e51c32f3
hao.zhou <hao.zhou3@bitland.corp-partner.google.com>
2020-05-27 15:17:25 +0800

    morphius: add Power/Battery LED behavior
    This CL add morphius Power/Battery led behavior as below:
    Power LED:
        AC connect: 0.25s ON/0.25s OFF, repeat 3 times
        System on: ON
        System in S3: Gradual 1 s ON, Gradual 1 s OFF, 3s OFF
        System off: OFF
    
    BUG=b:146309932
    BRANCH=hao.zhou
    TEST=manual verify LEDs behavior meet spec.
    
    Signed-off-by: hao.zhou <hao.zhou3@bitland.corp-partner.google.com>
    Change-Id: I293caf24e99bd4cd688674509e9c1bd1a0baeb72

--- board/dalboz/led.c
+++ board/dalboz/led.c
@@ -11,8 +11,10 @@
 #define LED_OFF_LVL	1
 #define LED_ON_LVL	0
 
-const int led_charge_lvl_1;
-const int led_charge_lvl_2 = 100;
+const int led_charge_lvl_1 = 10;
+const int led_charge_lvl_2 = 30;
+const int led_charge_lvl_3 = 50;
+const int led_charge_lvl_4 = 100;
 
 struct led_descriptor led_bat_state_table[LED_NUM_STATES][LED_NUM_PHASES] = {
 	[STATE_CHARGING_LVL_1]       = {{EC_LED_COLOR_GREEN,  2 * LED_ONE_SEC},
