68739c11764cbb17c4a55e89a010d68ec360a12a
Tom Hughes <tomhughes@chromium.org>
2020-05-28 15:53:53 -0700

    test: Add more tests for flash_physical
    BRANCH=none
    BUG=none
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I9a74bb87bc03083877a40c8fac460b765fbefb93

--- test/flash_physical.c
+++ test/flash_physical.c
@@ -30,6 +30,117 @@ struct flash_info flash_info = {
 #error "Flash info not defined for this chip. Please add it."
 #endif
 
+test_static int test_flash_physical_write(void)
+{
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+test_static int test_flash_physical_erase(void)
+{
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+test_static int test_flash_physical_get_protect(void)
+{
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+test_static int test_flash_physical_get_protect_flags(void)
+{
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+test_static int test_flash_physical_get_valid_flags(void)
+{
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+test_static int test_flash_physical_get_writable_flags(void)
+{
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+test_static int test_flash_physical_protect_at_boot_all(void)
+{
+	int rv;
+	int i;
+
+	/* Nothing should be protected */
+	for (i = 0; i < flash_info.num_flash_banks; i++) {
+		TEST_EQ(flash_physical_get_protect(i), 0, "%d");
+	}
+
+	/* Protecting everything. */
+	rv = flash_physical_protect_at_boot(EC_FLASH_PROTECT_ALL_AT_BOOT);
+	TEST_EQ(rv, EC_SUCCESS, "%d");
+
+	for (i = 0; i < flash_info.num_flash_banks; i++) {
+		rv = flash_physical_get_protect(i);
+		TEST_EQ(rv, 1, "%d");
+	}
+
+	/* TODO: Check that RDP 1 is enabled */
+
+	return EC_SUCCESS;
+}
+
+test_static int test_flash_physical_protect_at_boot_rollback(void)
+{
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+test_static int test_flash_physical_protect_at_boot_ro(void)
+{
+	int rv;
+	int i;
+
+	/* Nothing should be protected */
+	for (i = 0; i < flash_info.num_flash_banks; i++) {
+		TEST_EQ(flash_physical_get_protect(i), 0, "%d");
+	}
+
+	/* Protecting only RO */
+	rv = flash_physical_protect_at_boot(EC_FLASH_PROTECT_RO_AT_BOOT);
+	TEST_EQ(rv, EC_SUCCESS, "%d");
+
+	for (i = 0; i < flash_info.num_flash_banks; i++) {
+		rv = flash_physical_get_protect(i);
+		if (i >= flash_info.write_protect_bank_offset &&
+			i < flash_info.write_protect_bank_offset +
+				flash_info.write_protect_bank_count) {
+			TEST_EQ(rv, 1, "%d");
+		} else {
+			TEST_EQ(rv, 0, "%d");
+		}
+	}
+
+	/* TODO: Check that RDP 1 is enabled */
+
+	return EC_SUCCESS;
+}
+
+test_static int test_flash_physical_protect_now(void)
+{
+	int rv;
+	int i;
+
+	/* Nothing should be protected */
+	for (i = 0; i < flash_info.num_flash_banks; i++) {
+		TEST_EQ(flash_physical_get_protect(i), 0, "%d");
+	}
+
+	/* Protecting only RO */
+	rv = flash_physical_protect_now(0);
+	TEST_EQ(rv, EC_SUCCESS, "%d");
+
+	/* In current implementation, option bytes are disabled.
+	 * We should check for that here. Do we expect anything else to be
+	 * done?
+	 */
+
+	return EC_SUCCESS;
+}
+
 
 test_static int test_lock_option_bytes(void)
 {
@@ -123,11 +234,17 @@ void run_test(int argc, char **argv)
 {
 	ccprintf("Running flash physical test\n");
 	RUN_TEST(test_flash_config);
+
+	// RUN_TEST(test_flash_physical_protect_at_boot_ro);
+	// RUN_TEST(test_flash_physical_protect_at_boot_all);
+	RUN_TEST(test_flash_physical_protect_now);
+
 	/*
 	 * TODO(b/157692395): These should be implemented for the STM32H743 as
 	 * well.
 	 */
-#if defined(CHIP_VARIANT_STM32F412)
+// #if defined(CHIP_VARIANT_STM32F412)
+#if 0
 	RUN_TEST(test_lock_option_bytes);
 	RUN_TEST(test_disable_option_bytes);
 	RUN_TEST(test_lock_flash_control_register);
