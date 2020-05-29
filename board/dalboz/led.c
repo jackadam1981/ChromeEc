f7066be15da5b5d178bb7d3c7f497155d71db4d1
hao.zhou <hao.zhou3@bitland.corp-partner.google.com>
2020-05-29 10:34:45 +0800

    morphius: add charge led behavior
    This CL add morphius charge led behavior as below:
    Charge LED:
        Battery Charging 0%~10%: Red 0.2 sec OFF 0.2 sec
        Battery Charging 11%~30%: Green 0.5 sec OFF 0.5 sec
        Battery Charging 31%~50%: Green 1 sec OFF 0.5 sec
        Battery Charging 51%~100%: Green 1 sec OFF 1 sec
    
    BUG=none
    BRANCH=master
    TEST=manual verify LEDs behavior meet spec.
    
    Signed-off-by: hao.zhou <hao.zhou3@bitland.corp-partner.google.com>
    Change-Id: I9d6ce34b40b1531eb966cb34dc5d8ac867b5341b

--- board/dalboz/led.c
+++ board/dalboz/led.c
@@ -11,21 +11,28 @@
 #define LED_OFF_LVL	1
 #define LED_ON_LVL	0
 
-const int led_charge_lvl_1;
-const int led_charge_lvl_2 = 100;
+const int led_charge_lvl_1 = 10;
+const int led_charge_lvl_2 = 30;
+const int led_charge_lvl_3 = 50;
+const int led_charge_lvl_4 = 100;
 
 struct led_descriptor led_bat_state_table[LED_NUM_STATES][LED_NUM_PHASES] = {
-	[STATE_CHARGING_LVL_1]       = {{EC_LED_COLOR_GREEN,  2 * LED_ONE_SEC},
-					{EC_LED_COLOR_RED, 2 * LED_ONE_SEC} },
-	[STATE_CHARGING_LVL_2]	     = {{EC_LED_COLOR_RED, LED_INDEFINITE} },
+	[STATE_CHARGING_LVL_1] = {{EC_LED_COLOR_RED,  0.2 * LED_ONE_SEC},
+					{LED_OFF, 0.2 * LED_ONE_SEC} },
+	[STATE_CHARGING_LVL_2] = {{EC_LED_COLOR_GREEN,  0.5 * LED_ONE_SEC},
+					{LED_OFF, 0.5 * LED_ONE_SEC} },
+	[STATE_CHARGING_LVL_3] = {{EC_LED_COLOR_GREEN,  1 * LED_ONE_SEC},
+					{LED_OFF, 0.5 * LED_ONE_SEC} },
+	[STATE_CHARGING_LVL_4] = {{EC_LED_COLOR_GREEN,  1 * LED_ONE_SEC},
+					{LED_OFF, 1 * LED_ONE_SEC} },
 	[STATE_CHARGING_FULL_CHARGE] = {{EC_LED_COLOR_GREEN,  LED_INDEFINITE} },
-	[STATE_DISCHARGE_S0]	     = {{EC_LED_COLOR_GREEN,  LED_INDEFINITE} },
-	[STATE_DISCHARGE_S3]	     = {{EC_LED_COLOR_RED, 1 * LED_ONE_SEC},
+	[STATE_DISCHARGE_S0]   = {{EC_LED_COLOR_GREEN,  LED_INDEFINITE} },
+	[STATE_DISCHARGE_S3]   = {{EC_LED_COLOR_RED, 1 * LED_ONE_SEC},
 					{LED_OFF,            3 * LED_ONE_SEC} },
-	[STATE_DISCHARGE_S5]         = {{LED_OFF,            LED_INDEFINITE} },
-	[STATE_BATTERY_ERROR]        = {{EC_LED_COLOR_GREEN,  2 * LED_ONE_SEC},
+	[STATE_DISCHARGE_S5]   = {{LED_OFF,            LED_INDEFINITE} },
+	[STATE_BATTERY_ERROR]  = {{EC_LED_COLOR_GREEN,  2 * LED_ONE_SEC},
 					{EC_LED_COLOR_RED, 2 * LED_ONE_SEC} },
-	[STATE_FACTORY_TEST]         = {{EC_LED_COLOR_GREEN,  LED_INDEFINITE} },
+	[STATE_FACTORY_TEST]   = {{EC_LED_COLOR_GREEN,  LED_INDEFINITE} },
 };
 
 BUILD_ASSERT(ARRAY_SIZE(led_bat_state_table) == LED_NUM_STATES);
