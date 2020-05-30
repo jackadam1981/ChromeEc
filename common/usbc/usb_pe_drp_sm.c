5607e6b44c0cbd4ad4fc876c034f7580b78c251d
Denis Brockus <dbrockus@google.com>
2020-05-29 20:23:37 -0600

    TCPMv2: Wait for Safe0V on PR Swap SNK to SRC before PS_RDY
    We were out of spec for PR Swap transitioning from
    SNK to SRC by sending PS_RDY before we were Safe0V.
    
    BUG=b:157755939
    BRANCH=none
    TEST=ThinkPad Dock G2 should attach
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: I93eb36acc64f273e8b30ca0a0bb76d6fa96b64ba

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -3464,8 +3464,9 @@ static void pe_prs_src_snk_transition_to_off_run(int port)
 	if (get_time().val < pe[port].ps_source_timer)
 		return;
 
-	/* Wait until Rd is asserted */
-	if (tc_is_attached_snk(port)) {
+	/* Wait until Rd is asserted and we are at Safe0V */
+	if (tc_is_attached_snk(port) &&
+	    tcpm_check_vbus_level(port, VBUS_SAFE0V)) {
 		/* Contract is invalid */
 		pe_invalidate_explicit_contract(port);
 		set_state_pe(port, PE_PRS_SRC_SNK_WAIT_SOURCE_ON);
