e2e27abb0e329379a22b32864863b6ca14f0b815
Denis Brockus <dbrockus@google.com>
2020-05-30 12:20:36 -0600

    TCPCI: Update RP value selection has to set the register
    The tcpci_tcpm_select_rp_value was only caching the
    value to be used in case we set other fields and had
    to recreate.  This function did not actually set the
    Rp value in the Role Control register. This made
    PD3 collision detection work incorrectly.
    
    The original designed behavior of select_rp_value
    was just to cache and set_cc would be then called
    to update the Role Control register with a combined
    value.  Charge Manager is one case that sets the
    rp value that does not call set_cc as well as
    a few others in the code.  This means we are
    "setting" the rp value but it is not reflected
    on the wire.
    
    This was seen on ThinkPad Gen2 Dock connecting to
    trembyle.  On first attach the DUT was not correctly
    reflecting Rp and the dock detected this after the
    PR_Swap was sent and performed Soft and Hard resets
    
    BUG=b:157755939
    BRANCH=none
    TEST=Verify Rp tracks SinkTxNG
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: I5e2cd1bd2c7fffa06f46e1d7a77868f49454c591

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -1247,6 +1247,7 @@ void charge_manager_source_port(int port, int enable)
 
 		typec_set_source_current_limit(p, rp);
 		tcpm_select_rp_value(p, rp);
+		tcpm_set_select_rp(p);
 		pd_update_contract(p);
 	}
 }
--- driver/tcpm/tcpm.h
+++ driver/tcpm/tcpm.h
@@ -173,6 +173,16 @@ static inline int tcpm_select_rp_value(int port, int rp)
 	return tcpc_config[port].drv->select_rp_value(port, rp);
 }
 
+static inline int tcpm_set_select_rp(int port)
+{
+	const struct tcpm_drv *tcpc = tcpc_config[port].drv;
+
+	if (tcpc->set_select_rp)
+		return tcpc->set_select_rp(port);
+
+	return EC_SUCCESS;
+}
+
 static inline int tcpm_set_cc(int port, int pull)
 {
 	return tcpc_config[port].drv->set_cc(port, pull);
