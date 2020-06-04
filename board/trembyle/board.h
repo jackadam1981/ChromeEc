af68d2005938a2be50a6306da8d4b0c5e7cf7781
Lu Zhang <lu.zhang@bitland.corp-partner.google.com>
2020-06-04 19:52:30 +0800

    zork: Move tcpc,ppc,bc12 from baseboard to board
    BUG=b:158125500
    BRANCH=none
    TEST=Build all zork boards
    
    Signed-off-by: Lu Zhang <lu.zhang@bitland.corp-partner.google.com>
    Change-Id: I86a21a049f97c42d330be6c1bfbecbda10e1ebb6

--- board/trembyle/board.h
+++ board/trembyle/board.h
@@ -143,6 +143,11 @@ enum ec_cfg_usb_db_type {
 	TREMBYLE_DB_T_OPT3_USBAC_HDMI_MSTHUB = 2,
 };
 
+enum usbc_port {
+	USBC_PORT_C0 = 0,
+	USBC_PORT_C1,
+	USBC_PORT_COUNT
+};
 
 #define HAS_USBC1_RETIMER_PS8802 \
 			(BIT(TREMBYLE_DB_T_OPT2_USBAC) | \
@@ -200,6 +205,13 @@ extern const struct usb_mux usbc0_pi3dpx1207_usb_retimer;
 extern const struct usb_mux usbc1_ps8802;
 extern const struct usb_mux usbc1_ps8818;
 extern struct usb_mux usbc1_amd_fp5_usb_mux;
+
+void board_reset_pd_mcu(void);
+
+/* Common definition for the USB PD interrupt handlers. */
+void tcpc_alert_event(enum gpio_signal signal);
+void bc12_interrupt(enum gpio_signal signal);
+void ppc_interrupt(enum gpio_signal signal);
 
 #endif /* !__ASSEMBLER__ */
 
