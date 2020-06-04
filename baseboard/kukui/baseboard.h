fc700f0242448a90cda41dbb313f873fceb13f23
Ting Shen <phoenixshen@google.com>
2020-06-04 16:31:10 +0800

    baseboard/kukui: lower pd max voltage to 15V for jacuzzi
    Charging performance is better at 15V than 20V, see the analysis
    result in issue link below.
    
    BUG=b:157116471
    TEST=make
    BRANCH=kukui
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I8113809372fc2e1e92be901025ee1ed4bd588a80

--- baseboard/kukui/baseboard.h
+++ baseboard/kukui/baseboard.h
@@ -66,7 +66,7 @@
 #define PD_POWER_SUPPLY_TURN_OFF_DELAY 250000 /* us */
 #define PD_VCONN_SWAP_DELAY 5000 /* us */
 
-#define PD_MAX_VOLTAGE_MV 20000
+#define PD_MAX_VOLTAGE_MV 15000
 #else
 #error Must define a VARIANT_KUKUI_CHARGER
 #endif /* VARIANT_KUKUI_CHARGER */
