15291f4b154fc48d76645dd01e3c108a980bfce2
Vadim Sukhomlinov <sukhomlinov@google.com>
2020-06-04 23:01:38 -0700

    test/tpm_test: update for Python3
    Due to Python3 switch tpm_test.py stop working. Updates to make it
    work with Python3.
    
    BUG=None
    TEST=tpmtest.py
         tpmtest.py -t
    
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Change-Id: I927b25ab3288274993949c53564bed73faa346e9

--- test/tpm_test/hkdf_test.py
+++ test/tpm_test/hkdf_test.py
@@ -32,18 +32,11 @@ _HKDF_OPCODES = {
 #    INFO_LEN      INFO
 #    1             MSB OKM LEN
 #    1             LSB OKM LEN
-#
-_HKDF_CMD_FORMAT = '{op:c}{sl:s}{salt}{ikml:s}{ikm}{infol:s}{info}{okml:s}'
-
-
 def _rfc_test_cmd(salt, ikm, info, okml):
-  op = _HKDF_OPCODES['TEST_RFC']
-  return _HKDF_CMD_FORMAT.format(op=op,
-                                 sl=pack('>H', len(salt)), salt=salt,
-                                 ikml=pack('>H', len(ikm)), ikm=ikm,
-                                 infol=pack('>H', len(info)), info=info,
-                                 okml=pack('>H', okml))
-
+  return _HKDF_OPCODES['TEST_RFC'].to_bytes(1, "big") + \
+         len(salt).to_bytes(2, "big") + salt + \
+         len(ikm).to_bytes(2, "big") + ikm + \
+         len(info).to_bytes(2, "big") + info + okml.to_bytes(2, "big")
 
 #
 # Test vectors for HKDF-SHA256 from RFC 5869.
--- test/tpm_test/rsa_test.py
+++ test/tpm_test/rsa_test.py
@@ -89,82 +89,50 @@ _KEYS = {
 #   0x00 LSB DIGEST LEN
 #   .... DIGEST
 #
-_RSA_CMD_FORMAT = '{o:c}{p:c}{h:c}{kl:s}{ml:s}{msg}{dl:s}{dig}'
-
-
 def _decrypt_cmd(padding, hashing, key_len, msg):
-  op = _RSA_OPCODES['DECRYPT']
-  msg_len = len(msg)
-  return _RSA_CMD_FORMAT.format(o=op, p=padding, h=hashing,
-                                kl=struct.pack('>H', key_len),
-                                ml=struct.pack('>H', msg_len), msg=msg,
-                                dl='', dig='')
-
+  return _RSA_OPCODES['DECRYPT'].to_bytes(1, "big") + \
+    padding.to_bytes(1, "big") + hashing.to_bytes(1, "big") + \
+    key_len.to_bytes(2, "big") + \
+    len(msg).to_bytes(2, "big") + msg + bytes([0, 0])
 
 def _encrypt_cmd(padding, hashing, key_len, msg):
-  op = _RSA_OPCODES['ENCRYPT']
-  msg_len = len(msg)
-  return _RSA_CMD_FORMAT.format(o=op, p=padding, h=hashing,
-                                kl=struct.pack('>H', key_len),
-                                ml=struct.pack('>H', msg_len), msg=msg,
-                                dl='', dig='')
-
+  return _RSA_OPCODES['ENCRYPT'].to_bytes(1, "big") + \
+    padding.to_bytes(1, "big") + hashing.to_bytes(1, "big") + \
+    key_len.to_bytes(2, "big") + \
+    len(msg).to_bytes(2, "big") + msg +  bytes([0, 0])
 
 def _sign_cmd(padding, hashing, key_len, digest):
-  op = _RSA_OPCODES['SIGN']
-  digest_len = len(digest)
-  return _RSA_CMD_FORMAT.format(o=op, p=padding, h=hashing,
-                                kl=struct.pack('>H', key_len),
-                                ml=struct.pack('>H', digest_len), msg=digest,
-                                dl='', dig='')
-
+  return _RSA_OPCODES['SIGN'].to_bytes(1, "big") + \
+    padding.to_bytes(1, "big") + hashing.to_bytes(1, "big") + \
+    key_len.to_bytes(2, "big") + \
+    len(digest).to_bytes(2, "big") + digest + bytes([0, 0])
 
 def _verify_cmd(padding, hashing, key_len, sig, digest):
-  op = _RSA_OPCODES['VERIFY']
-  sig_len = len(sig)
-  digest_len = len(digest)
-  return _RSA_CMD_FORMAT.format(o=op, p=padding, h=hashing,
-                                kl=struct.pack('>H', key_len),
-                                ml=struct.pack('>H', sig_len), msg=sig,
-                                dl=struct.pack('>H', digest_len), dig=digest)
-
+  return _RSA_OPCODES['VERIFY'].to_bytes(1, "big") + \
+    padding.to_bytes(1, "big") + hashing.to_bytes(1, "big") + \
+    key_len.to_bytes(2, "big") + \
+    len(sig).to_bytes(2, "big") + sig + \
+    len(digest).to_bytes(2, "big") + digest
 
 def _keytest_cmd(key_len):
-  op = _RSA_OPCODES['KEYTEST']
-  return _RSA_CMD_FORMAT.format(o=op, p=0, h=_HASH['NONE'],
-                                kl=struct.pack('>H', key_len),
-                                ml=struct.pack('>H', 0), msg='',
-                                dl='', dig='')
-
+  return _RSA_OPCODES['KEYTEST'].to_bytes(1, "big") + \
+  bytes([0, 0]) + key_len.to_bytes(2, "big") + \
+  bytes([0, 0, 0 ,0])
 
 def _keygen_cmd(key_len, e, label):
-  op = _RSA_OPCODES['KEYGEN']
-  padding = _RSA_PADDING['NONE']
-  hashing = _HASH['NONE']
-  return _RSA_CMD_FORMAT.format(o=op, p=padding, h=hashing,
-                                kl=struct.pack('>H', key_len),
-                                ml=struct.pack('>H', len(label)), msg=label,
-                                dl=struct.pack('>H', 0), dig='')
-
+  return _RSA_OPCODES['KEYGEN'].to_bytes(1, "big") + \
+  bytes([0, 0]) + key_len.to_bytes(2, "big") + \
+  len(label).to_bytes(2, "big") + label + bytes([0, 0])
 
 def _primegen_cmd(seed):
-  op = _RSA_OPCODES['PRIMEGEN']
-  padding = _RSA_PADDING['NONE']
-  hashing = _HASH['NONE']
-  return _RSA_CMD_FORMAT.format(o=op, p=padding, h=hashing,
-                                kl=struct.pack('>H', len(seed) * 8 * 2),
-                                ml=struct.pack('>H', len(seed)), msg=seed,
-                                dl=struct.pack('>H', 0), dig='')
+  return _RSA_OPCODES['PRIMEGEN'].to_bytes(1, "big") + \
+  bytes([0, 0]) + (len(seed) * 8 * 2).to_bytes(2, "big") + \
+  len(seed).to_bytes(2, "big") + seed + bytes([0, 0])
 
 def _x509_verify_cmd(key_len):
-  op = _RSA_OPCODES['X509_VERIFY']
-  padding = _RSA_PADDING['NONE']
-  hashing = _HASH['NONE']
-  return _RSA_CMD_FORMAT.format(o=op, p=padding, h=hashing,
-                                kl=struct.pack('>H', key_len),
-                                ml=struct.pack('>H', 0), msg='',
-                                dl=struct.pack('>H', 0), dig='')
-
+  return _RSA_OPCODES['X509_VERIFY'].to_bytes(1, "big") + \
+  bytes([0, 0]) + key_len.to_bytes(2, "big") + \
+  bytes([0, 0, 0, 0])
 
 _PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
            59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131,
@@ -577,13 +545,13 @@ def _prime_from_seed(seed):
     return w
 
   # Set LSB, and top two bits.
-  candidate = chr(ord(seed[0]) | 192) + seed[1:-1] + chr(ord(seed[-1]) | 1)
+  candidate = bytes([(seed[0] | 192)]) + seed[1:-1] + bytes([seed[-1] | 1])
   candidate = int(binascii.b2a_hex(candidate), 16)
   assert len(bin(candidate)[2:]) == len(seed) * 8
   w = _window(candidate, _PRIMES[:4096])
   for i, bit in enumerate(w):
     if not bit:
-      if rsa.prime.randomized_primality_testing(candidate + i, ROUNDS):
+      if rsa.prime.miller_rabin_primality_testing(candidate + i, ROUNDS):
         return candidate + i
   return None
 
@@ -631,10 +599,10 @@ _KEYTEST_INPUTS = (
 )
 
 _KEYGEN_INPUTS = (
-  (768, 65537, '', None),
-  (1024, 65537, 'rsa_test', None),
-  (2048, 65537, 'RSA key by vendor', 20811475686431332186511278472307159547870512766846593830860105577496044159545322178313772755518365593670114793803805067608811418757734989708137784444223785391864604211835387393923163468734914392307047296990698533218399115126417934050463597455237478939601236799120239663591264311485133747167378663829046579164891864068853210530642835833947569643788911200934265596274935082689832626616967124524353322373059893974744194447740045242468136414689225322177212281193879756355471091445748150740871146034049776312457888356154834233819876846764944450478069436248506560967902863015152471662817623176815923756421011384149834497587L),
-  (2048, 65537, '', None),
+  (2048, 65537, b'RSA key by vendor', 20811475686431332186511278472307159547870512766846593830860105577496044159545322178313772755518365593670114793803805067608811418757734989708137784444223785391864604211835387393923163468734914392307047296990698533218399115126417934050463597455237478939601236799120239663591264311485133747167378663829046579164891864068853210530642835833947569643788911200934265596274935082689832626616967124524353322373059893974744194447740045242468136414689225322177212281193879756355471091445748150740871146034049776312457888356154834233819876846764944450478069436248506560967902863015152471662817623176815923756421011384149834497587),
+  (768, 65537, b'', None),
+  (1024, 65537, b'rsa_test', None),
+  (2048, 65537, b'', None),
 )
 
 # 2048-bit will be done in hardware (i.e. fast), rest are in software.
@@ -650,7 +618,7 @@ _PRIMEGEN_INPUTS = (
 )
 
 def _encrypt_tests(tpm):
-  msg = 'Hello CR50!'
+  msg = b'Hello CR50!'
 
   for data in _ENCRYPT_INPUTS:
     padding, hashing, key_len = data
@@ -663,15 +631,7 @@ def _encrypt_tests(tpm):
                        key_len, ciphertext)
     wrapped_response = tpm.command(tpm.wrap_ext_command(subcmd.RSA, cmd))
     plaintext = tpm.unwrap_ext_response(subcmd.RSA, wrapped_response)
-    if padding == 'NULL':
-      # Check for leading zeros.
-      if reduce(lambda x, y: x | y,
-                map(ord, plaintext[:len(plaintext) - len(msg)])):
-        raise subcmd.TpmTestError('%s error:%s%s' % (
-          test_name, utils.hex_dump(msg), utils.hex_dump(plaintext)))
-      else:
-        plaintext = plaintext[len(plaintext) - len(msg):]
-    if msg != plaintext:
+    if msg != plaintext[-len(msg):]:
       raise subcmd.TpmTestError('%s error:%s%s' % (
           test_name, utils.hex_dump(msg), utils.hex_dump(plaintext)))
     print('%sSUCCESS: %s' % (utils.cursor_back(), test_name))
@@ -717,7 +677,7 @@ def _verify_tests(tpm):
                       key_len, signature, h.digest())
     wrapped_response = tpm.command(tpm.wrap_ext_command(subcmd.RSA, cmd))
     verified = tpm.unwrap_ext_response(subcmd.RSA, wrapped_response)
-    expected = '\x01'
+    expected = b'\x01'
     if verified != expected:
       raise subcmd.TpmTestError('%s error:%s%s' % (
           test_name, utils.hex_dump(verified), utils.hex_dump(expected)))
@@ -731,7 +691,7 @@ def _keytest_tests(tpm):
     cmd = _keytest_cmd(key_len)
     wrapped_response = tpm.command(tpm.wrap_ext_command(subcmd.RSA, cmd))
     valid = tpm.unwrap_ext_response(subcmd.RSA, wrapped_response)
-    expected = '\x01'
+    expected = b'\x01'
     if valid != expected:
       raise subcmd.TpmTestError('%s error:%s%s' % (
           test_name, utils.hex_dump(valid), utils.hex_dump(expected)))
@@ -751,12 +711,12 @@ def _keygen_tests(tpm):
       raise subcmd.TpmTestError('%s error:%s' % (
         test_name, utils.hex_dump(result)))
 
-    N = int(binascii.b2a_hex(result[0:result_len * 2 / 3]), 16)
+    N = int(binascii.b2a_hex(result[0:int(result_len * 2 / 3)]), 16)
     if expected_N and N != expected_N:
       raise subcmd.TpmTestError('%s error:%s' % (
           test_name, utils.hex_dump(result)))
-    p = int(binascii.b2a_hex(result[result_len * 2 / 3:]), 16)
-    q = N / p
+    p = int(binascii.b2a_hex(result[int(result_len * 2 / 3):]), 16)
+    q = N // p
     if not rsa.prime.is_prime(p):
       raise subcmd.TpmTestError('%s error:%s' % (
           test_name, utils.hex_dump(result)))
@@ -773,15 +733,15 @@ def _primegen_tests(tpm):
   for data in _PRIMEGEN_INPUTS:
     key_len = data
     test_name = 'RSA-PRIMEGEN:%d' % data
-    seed = rsa.randnum.read_random_bits(key_len / 2)
-    assert len(seed) == key_len / 16
+    seed = rsa.randnum.read_random_bits(key_len // 2)
+    assert len(seed) == key_len // 16
     # dcrypto interface is little-endian.
     cmd = _primegen_cmd(seed[::-1])
 
     wrapped_response = tpm.command(tpm.wrap_ext_command(subcmd.RSA, cmd))
     result = tpm.unwrap_ext_response(subcmd.RSA, wrapped_response)
     result_len = len(result)
-    if result_len != key_len / 16:
+    if result_len != key_len // 16:
       raise subcmd.TpmTestError('%s error:%s' % (
         test_name, utils.hex_dump(result)))
 
@@ -801,7 +761,7 @@ def _x509_verify_tests(tpm):
   cmd = _x509_verify_cmd(2048)
   wrapped_response = tpm.command(tpm.wrap_ext_command(subcmd.RSA, cmd))
   valid = tpm.unwrap_ext_response(subcmd.RSA, wrapped_response)
-  expected = '\x01'
+  expected = b'\x01'
   if valid != expected:
     raise subcmd.TpmTestError('%s error:%s%s' % (
       test_name, utils.hex_dump(valid), utils.hex_dump(expected)))
