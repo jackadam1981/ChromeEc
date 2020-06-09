beb5fa07c77019293ec59fdce8785d07228bd02d
Vadim Sukhomlinov <sukhomlinov@google.com>
2020-06-08 18:49:27 -0700

    test/tpm_test: fix cros lint complains
    BUG=b:158533918
    TEST=tpmtest.py
    
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Change-Id: Ia6b59c49afc7ed19507fab254cab44b2a5c1953b

--- test/tpm_test/utils.py
+++ test/tpm_test/utils.py
@@ -8,31 +8,31 @@
 import sys
 
 if hasattr(sys.stdout, 'isatty') and sys.stdout.isatty():
-  cursor_back_cmd = '\x1b[1D'  # Move one space to the left.
+    CURSOR_BACK_CMD = '\x1b[1D'  # Move one space to the left.
 else:
-  cursor_back_cmd = ''
+    CURSOR_BACK_CMD = ''
 
 
 def cursor_back():
-  """Return a string which would move cursor one space left, if available.
+    """Return a string which would move cursor one space left, if available.
 
-  This is used to remove the remaining 'spinner' character after the test
-  completes and its result is printed on the same line where the 'spinner' was
-  spinning.
+    This is used to remove the remaining 'spinner' character after the test
+    completes and its result is printed on the same line where the 'spinner' was
+    spinning.
 
-  """
-  return cursor_back_cmd
+    """
+    return CURSOR_BACK_CMD
 
 
 def hex_dump(binstr):
-  """Convert binary string into its multiline hex representation."""
+    """Convert binary string into its multiline hex representation."""
 
-  dump_lines = ['',]
-  i = 0
-  while i < len(binstr):
-    strsize = min(16, len(binstr) - i)
-    hexstr = ' '.join('%2.2x' % ord(x) for x in binstr[i:i+strsize])
-    dump_lines.append(hexstr)
-    i += strsize
-  dump_lines.append('')
-  return '\n'.join(dump_lines)
+    dump_lines = ['',]
+    i = 0
+    while i < len(binstr):
+        strsize = min(16, len(binstr) - i)
+        hexstr = ' '.join('%2.2x' % ord(x) for x in binstr[i:i+strsize])
+        dump_lines.append(hexstr)
+        i += strsize
+    dump_lines.append('')
+    return '\n'.join(dump_lines)
