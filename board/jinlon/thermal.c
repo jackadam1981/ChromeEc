1939ab5c3ce3b701003702dfce8fb74c3ce78016
Devin Lu <devin.lu@quantatw.com>
2020-06-11 14:28:19 +0800

    jinlon: Update fan table
    BUG=b:154564062
    BRANCH=firmware-hatch-12672.B
    TEST=Thermal team verified thermal policy is expected.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I6d79f2492de087af4f3576dd8faf1079100f3d12

--- board/jinlon/thermal.c
+++ board/jinlon/thermal.c
@@ -76,7 +76,7 @@ static const struct fan_step fan_table_clamshell[] = {
 	},
 	{
 		/* level 6 */
-		.on = {65, -1, 62, 40},
+		.on = {65, -1, 64, 40},
 		.off = {62, -1, 61, 39},
 		.rpm = {5400, 5300},
 	},
