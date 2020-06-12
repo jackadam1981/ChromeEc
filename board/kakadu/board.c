d9a5b2c27016d9390e5627a95937a49264be4035
Scott Chao <scott.chao@bitland.corp-partner.google.com>
2020-06-12 09:45:55 +0800

    kakadu: do not power on from cutoff when insert battery
    BUG=b:156455781
    BRANCH=kukui
    TEST=make -j BOARD=kakadu
    TEST=make buildall
    
    Signed-off-by: Scott Chao <scott.chao@bitland.corp-partner.google.com>
    Change-Id: I2ba2221bfa260a4394e7f1ba3fe1e6d75f57c93f

--- board/kakadu/board.c
+++ board/kakadu/board.c
@@ -241,7 +241,7 @@ int pd_snk_is_vbus_provided(int port)
 
 static void board_init(void)
 {
-
+	uint32_t reset_flags;
 #ifdef SECTION_IS_RW
 	int val;
 	i2c_read8(I2C_PORT_CHARGER, CHARGER_I2C_ADDR_FLAGS,
@@ -250,6 +250,19 @@ static void board_init(void)
 	i2c_write8(I2C_PORT_CHARGER, CHARGER_I2C_ADDR_FLAGS,
 		RT946X_REG_CHGCTRL1, (val | RT946X_MASK_STAT_EN));
 #endif
+
+	reset_flags = system_get_reset_flags();
+
+	if (reset_flags & EC_RESET_FLAG_POWER_ON)
+	{
+		board_cut_off_battery();
+		CPRINTF("[SC] came back from ship mode");
+	}
+	else
+	{
+		CPRINTF("[SC] normal boot");
+	}
+
 	/* If the reset cause is external, pulse PMIC force reset. */
 	if (system_get_reset_flags() == EC_RESET_FLAG_RESET_PIN) {
 		gpio_set_level(GPIO_PMIC_FORCE_RESET_ODL, 0);
