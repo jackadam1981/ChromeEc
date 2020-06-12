c938353236c8f47bd0eb5ce39acdffa0d935cf7a
Scott Chao <scott.chao@bitland.corp-partner.google.com>
2020-06-12 09:45:55 +0800

    kakadu: do not power on from cutoff when insert battery
    BUG=b:156455781
    BRANCH=kukui
    TEST=make -j BOARD=kakadu
    TEST=make buildall
    
    Signed-off-by: Scott Chao <scott.chao@bitland.corp-partner.google.com>
    Change-Id: I2ba2221bfa260a4394e7f1ba3fe1e6d75f57c93f

--- power/mt8183.c
+++ power/mt8183.c
@@ -10,6 +10,7 @@
 #include "common.h"
 #include "console.h"
 #include "ec_commands.h"
+#include "extpower.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "lid_switch.h"
@@ -359,6 +360,12 @@ enum power_state power_handle_state(enum power_state state)
 				total_sleep_ms += 10;
 				return POWER_G3S5;
 			}
+		}
+#endif
+#ifdef BOARD_KAKADU
+		if (boot_from_cutoff && gpio_get_level(GPIO_BATT_CUTOFF_INDICATOR) && !extpower_is_present()) {
+			board_cut_off_battery();
+			return POWER_G3;
 		}
 #endif
 
