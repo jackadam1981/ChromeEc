d91bc1d77d9059c15939908e8e551131212f48fa
Ayushee <ayushee.shah@intel.com>
2020-06-12 15:21:06 -0700

    usb_pd: TCPMv1: Enable VCONN swap at explicit contract
    According to Section 2.6.2 of both USB-PD Spec Revision 2.0, Version 1.3
    & USB-PD Spec Revision 3.0, Version 2.0, at explicit contract, The Sink
    can initiate or receive a request for an exchange of VCONN Source and
    The Sink when it is the VCONN Source can communicate with a Cable Plug
    using SOP’ or SOP’’ Communication.
    
    PD_FLAGS_CHECK_VCONN_STATE flag enables the DUT to request for a VCONN
    swap at explicit contract.
    
    Earlier, when UFP requested for a power role swap,
    PD_FLAGS_CHECK_VCONN_STATE flag wasn't being enabled and hence, DUT was
    not entering Thunderbolt/USB4 alternate mode.
    
    Enabling the PD_FLAGS_CHECK_VCONN_STATE flag in PD_STATE_SNK_REQUESTED
    allows DUT to request for VCONN swap at explicit contract.
    
    BUG=b:158455025
    BRANCH=None
    TEST=Able to enter into Thunderbolt/USB4 when UFP requests for a power
         role swap.
    
    Change-Id: I68f2e2de3186ea367ace497b47930e30976c9e6b
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -4268,6 +4268,16 @@ void pd_task(void *u)
 		case PD_STATE_SNK_REQUESTED:
 			/* Wait for ACCEPT or REJECT */
 			if (pd[port].last_state != pd[port].task_state) {
+				/*
+				 * Ref: Section 2.6.2 of both
+				 * USB-PD Spec Revision 2.0, Version 1.3 &
+				 * USB-PD Spec Revision 3.0, Version 2.0
+				 * During Explicit contract the Sink can
+				 * initiate or receive a request an exchange
+				 * of VCONN Source. Hence, enable Vconn swap
+				 * during explicit contract.
+				 */
+				pd[port].flags |= PD_FLAGS_CHECK_VCONN_STATE;
 				hard_reset_count = 0;
 				set_state_timeout(port,
 						  get_time().val +
@@ -4318,14 +4328,6 @@ void pd_task(void *u)
 				pd_check_pr_role(port, PD_ROLE_SINK,
 						 pd[port].flags);
 				pd[port].flags &= ~PD_FLAGS_CHECK_PR_ROLE;
-				/*
-				 * USB PD  version 1.3 section 2.6.2:
-				 * During Explicit contract the Sink can
-				 * initiate or receive a request an exchange
-				 * of VCONN Source. Hence, enable Vconn swap
-				 * during explicit contract.
-				 */
-				pd[port].flags |= PD_FLAGS_CHECK_VCONN_STATE;
 				break;
 			}
 
