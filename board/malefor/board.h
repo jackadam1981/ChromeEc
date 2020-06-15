916d61e7f35eb73e4c21e5cff83ddd029ebeb727
Keith Short <keithshort@chromium.org>
2020-06-15 10:48:51 -0600

    malefor: Move USB DB checks from baseboard
    Update Malefor USB daughterboard options to match project configuration.
    
    BUG=b:155497872
    BRANCH=none
    TEST=make BOARD=malefor
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: I4424028cd914c4dfbf13634dd1b60b1105172d28

--- board/malefor/board.h
+++ board/malefor/board.h
@@ -134,8 +134,19 @@ enum sensor_id {
 	SENSOR_COUNT,
 };
 
+/*
+ * FW_CONFIG values, must match project/volteer/malefor/config.star
+ */
+enum ec_cfg_usb_db_type {
+	USB_DB_NONE = 0,
+	USB_DB_USB3_LOW_COST = 1,
+	USB_DB_COUNT
+};
+
 /* TODO: b/143375057 - Remove this code after power on. */
 void c10_gate_change(enum gpio_signal signal);
+
+void board_reset_pd_mcu(void);
 
 #endif /* !__ASSEMBLER__ */
 
