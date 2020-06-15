1fc31834c20171ff76e0b290e3bad53619b55845
Andrew McRae <amcrae@google.com>
2020-06-15 12:35:44 +1000

    TCPMv2: Check for mode discovery complete before switching
    If port discovery is run multiple times, because of power state changes
    to the AP, DP alt modes are not detected because dpm_attempt_mode_entry
    is run after discovery is initialised, but before discovery completes,
    and so no modes are detected, and no attempt is made thereafter.
    
    Also correct an unsigned compare against 0, and fix the sending
    of CMD_EXIT_MODE.
    
    BUG=b:158813138
    TEST=Confirm that type-C monitors now work
    BRANCH=none
    
    Change-Id: Idef39ad97d6af2952474e8edcebd409f8324fa80
    Signed-off-by: Andrew McRae <amcrae@google.com>

--- common/usbc/dp_alt_mode.c
+++ common/usbc/dp_alt_mode.c
@@ -61,6 +61,9 @@ void dp_vdm_acked(int port, enum tcpm_transmit_type type, int vdo_count,
 		pd_get_amode_data(port, type, USB_SID_DISPLAYPORT);
 	const uint8_t vdm_cmd = PD_VDO_CMD(vdm[0]);
 
+	if (type == TCPC_TX_SOP && vdm_cmd == CMD_EXIT_MODE)
+		return;
+
 	if (type != TCPC_TX_SOP || next_vdm_cmd[port] != vdm_cmd) {
 		print_unexpected_response(port, type, CMDT_RSP_ACK, vdm_cmd);
 		dpm_set_mode_entry_done(port);
