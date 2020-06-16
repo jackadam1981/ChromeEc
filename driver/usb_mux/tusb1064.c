421fdac0541a0c5693e071d81c80661b718fd1a4
Scott Collyer <scollyer@google.com>
2020-06-15 20:56:25 -0700

    tusb1064: Create usb mux driver for tsub1064
    The tusb1064 is a type C demux and retimer switch. This CL adds
    mux driver support for tusb0164.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I1ee836ef0a88adab1af63901c2ec0be7cef7b232

--- driver/usb_mux/tusb1064.c
+++ driver/usb_mux/tusb1064.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* Copyright 2021 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
@@ -29,11 +30,53 @@ static int tusb1064_write(const struct usb_mux *me, uint8_t reg, uint8_t val)
 {
 	return i2c_write8(me->i2c_port, me->i2c_addr_flags,
 			  (int)reg, (int)val);
+=======
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * TI TUSB1064
+ * USB Type-C Redriving Switch for USB Host / DisplayPort.
+ */
+
+#include "common.h"
+#include "console.h"
+#include "i2c.h"
+#include "tusb1064.h"
+#include "usb_mux.h"
+#include "util.h"
+
+int tusb1064_read(const struct usb_mux *me, uint8_t reg, int *val)
+{
+	return i2c_read8(me->i2c_port, me->i2c_addr_flags,
+			 reg, val);
+}
+
+int tusb1064_write(const struct usb_mux *me, uint8_t reg, uint8_t val)
+{
+	return i2c_write8(me->i2c_port, me->i2c_addr_flags,
+			  reg, val);
+}
+
+static int tusb1064_init(const struct usb_mux *me)
+{
+	int rv;
+	int reg;
+
+	rv = tusb1064_read(me, TUSB1064_REG_MODE, &reg);
+	if (rv) {
+		ccprintf("tusb1064: mux init failed");
+		return rv;
+	}
+
+	return EC_SUCCESS;
+>>>>>>> 8b8f730b0f (tusb1064: Create usb mux driver for tsub1064)
 }
 
 /* Writes control register to set switch mode */
 static int tusb1064_set_mux(const struct usb_mux *me, mux_state_t mux_state)
 {
+<<<<<<< HEAD
 	int reg = REG_GENERAL_STATIC_BITS;
 
 	if (mux_state & USB_PD_MUX_USB_ENABLED)
@@ -44,11 +87,36 @@ static int tusb1064_set_mux(const struct usb_mux *me, mux_state_t mux_state)
 		reg |= REG_GENERAL_FLIPSEL;
 
 	return tusb1064_write(me, TUSB1064_REG_GENERAL, reg);
+=======
+	int reg;
+	int rv;
+
+	rv = tusb1064_read(me, TUSB1064_REG_MODE, &reg);
+	if (rv)
+		return rv;
+
+	/* Assume standby */
+	reg &= ~(TUSB1064_MODE_ALT_DP_EN | TUSB1064_MODE_USB_EN |
+		 TUSB1064_MODE_FLIPSEL);
+
+	if (mux_state & USB_PD_MUX_USB_ENABLED)
+		reg = TUSB1064_MODE_USB_EN;
+	if (mux_state & USB_PD_MUX_DP_ENABLED)
+		reg |= TUSB1064_MODE_ALT_DP_EN;
+	if (mux_state & USB_PD_MUX_POLARITY_INVERTED)
+		reg |= TUSB1064_MODE_FLIPSEL;
+
+	ccprintf("tusb1064: set_mux: = 0x%x\n", mux_state);
+	tusb1064_write(me, TUSB1064_REG_MODE, reg);
+
+	return EC_SUCCESS;
+>>>>>>> 8b8f730b0f (tusb1064: Create usb mux driver for tsub1064)
 }
 
 /* Reads control register and updates mux_state accordingly */
 static int tusb1064_get_mux(const struct usb_mux *me, mux_state_t *mux_state)
 {
+<<<<<<< HEAD
 	uint8_t reg;
 	int res;
 
@@ -95,12 +163,32 @@ static int tusb1064_init(const struct usb_mux *me)
 	res = tusb1064_write(me, TUSB1064_REG_AUXDPCTRL, 0);
 	if (res)
 		return res;
+=======
+	int reg;
+
+	tusb1064_read(me, TUSB1064_REG_MODE, &reg);
+
+	*mux_state = 0;
+	if (reg & TUSB1064_MODE_USB_EN)
+		*mux_state |= USB_PD_MUX_USB_ENABLED;
+	if (reg & TUSB1064_MODE_ALT_DP_EN)
+		*mux_state |= USB_PD_MUX_DP_ENABLED;
+	if (reg & TUSB1064_MODE_FLIPSEL)
+		*mux_state |= USB_PD_MUX_POLARITY_INVERTED;
+
+	ccprintf("tusb1064: get_mux: = 0x%x\n", *mux_state);
+>>>>>>> 8b8f730b0f (tusb1064: Create usb mux driver for tsub1064)
 
 	return EC_SUCCESS;
 }
 
+<<<<<<< HEAD
 const struct usb_mux_driver tusb1064_usb_mux_driver = {
 	/* CAUTION: This is an UFP/RX/SINK redriver mux */
+=======
+
+const struct usb_mux_driver tusb1064_usb_mux_driver = {
+>>>>>>> 8b8f730b0f (tusb1064: Create usb mux driver for tsub1064)
 	.init = tusb1064_init,
 	.set = tusb1064_set_mux,
 	.get = tusb1064_get_mux,
