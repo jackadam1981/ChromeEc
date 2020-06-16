7bd6dbb195522a7afef6273623d7644e09df1fa1
Scott Collyer <scollyer@google.com>
2020-06-15 20:56:25 -0700

    tusb1064: Create usb mux driver for tsub1064
    The tusb1064 is a type C demux and retimer switch. This CL adds
    mux driver support for tusb0164.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I1ee836ef0a88adab1af63901c2ec0be7cef7b232

--- driver/usb_mux/tusb1064.h
+++ driver/usb_mux/tusb1064.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* Copyright 2021 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
@@ -105,3 +106,37 @@
 #define TUSB1064_USB_EQ_UFP_11_1_DB 0xF
 
 #endif /* __CROS_EC_TUSB1064_H */
+=======
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * TI TUSB1064
+ * USB Type-C Retiming Switch for USB Device / DisplayPort Sink
+ */
+
+#ifndef __CROS_EC_PS8740_H
+#define __CROS_EC_PS8740_H
+
+#include "usb_mux.h"
+
+#define TUSB1064_I2C_ADDR0_FLAG    0x44
+#define TUSB1064_I2C_ADDR1_FLAG    0x47
+#define TUSB1064_I2C_ADDR2_FLAG    0x0C
+#define TUSB1064_I2C_ADDR3_FLAG    0x0F
+
+/* Mode register for setting mux */
+#define TUSB1064_REG_MODE         0x0A
+
+#define TUSB1064_MODE_FLIPSEL     BIT(2)
+#define TUSB1064_MODE_ALT_DP_EN   BIT(1)
+#define TUSB1064_MODE_USB_EN      BIT(0)
+
+#define TUSB1064_REG_DP_CONTROL   0x13
+#define TUSB1064_REG_DP_AUX_SNOOP_DIS   BIT(7)
+
+int tusb1064_write(const struct usb_mux *me, uint8_t reg, uint8_t val);
+int tusb1064_read(const struct usb_mux *me, uint8_t reg, int *val);
+
+#endif /* __CROS_EC_PS8740_H */
+>>>>>>> 8b8f730b0f (tusb1064: Create usb mux driver for tsub1064)
