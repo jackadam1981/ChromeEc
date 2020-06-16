5c4d755f1012b5e81c07025868be4dd1d63c7a7f
Scott Collyer <scollyer@google.com>
2020-06-15 21:19:35 -0700

    mp4245: Add support for alert handler, clean up
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I1fba62361a64c3a38ba23574e5516441610c38f4

--- baseboard/honeybuns/mp4245.h
+++ baseboard/honeybuns/mp4245.h
@@ -57,10 +57,20 @@
 #define MP4245_ILIM_STEP_MA          50
 
 
+#define MP4245_MFR_STATUS_MASK_VOUT        BIT(7)
+#define MP4245_MFR_STATUS_MASK_IOUT        BIT(6)
+#define MP4245_MFR_STATUS_MASK_INPUT       BIT(5)
+#define MP4245_MFR_STATUS_MASK_TEMP        BIT(4)
+#define MP4245_MFR_STATUS_MASK_PG_STATUS   BIT(3)
+#define MP4245_MFR_STATUS_MASK_PG_ALT_EDGE BIT(2)
+#define MP4245_MFR_STATUS_MASK_OTHER       BIT(1)
+#define MP4245_MFR_STATUS_MASK_UNKNOWN     BIT(0)
+
 int mp4245_set_voltage_out(int desired_mv);
 int mp4245_set_current_lim(int desired_ma);
 int mp4245_votlage_out_enable(int enable);
 void mp4245_dump_reg(void);
-
+void mp4245_alert_handler(void);
+int mp3245_get_vbus(int *mv, int *ma);
 
 
