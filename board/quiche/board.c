94f8894fefe247e6982381caa6fc5e0dc1251102
Scott Collyer <scollyer@google.com>
2020-06-15 21:28:27 -0700

    quiche: Add hpd support for ALT-DP mode
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I4aaee1dce987c3aef47bae9c9fa59938987907cc

--- board/quiche/board.c
+++ board/quiche/board.c
@@ -42,6 +42,11 @@ static void ppc_interrupt(enum gpio_signal signal)
 	}
 }
 
+void hpd_interrupt(enum gpio_signal signal)
+{
+	baseboard_manage_hpd_event(signal);
+}
+
 #include "gpio_list.h" /* Must come after other header files. */
 
 /*
@@ -92,7 +97,8 @@ const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 const struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	[USB_PD_PORT_HOST] = {
 		.usb_port = USB_PD_PORT_HOST,
-		.driver = & ps8822_usb_mux_driver,
+		.i2c_addr_flags = PS8822_I2C_ADDR3_FLAG,
+		.driver = &ps8822_usb_mux_driver,
 		.hpd_update = &board_hpd_update,
 	},
 };
@@ -112,6 +118,11 @@ void board_tcpc_init(void)
 {
 	/* Enable PPC interrupts. */
 	gpio_enable_interrupt(GPIO_HOST_USBC_PPC_INT_ODL);
+
+	/* Enable TCPC interrupts. */
+
+	/* Enable HPD interrupt */
+	gpio_enable_interrupt(GPIO_DDI_MST_IN_HPD);
 }
 DECLARE_HOOK(HOOK_INIT, board_tcpc_init, HOOK_PRIO_INIT_I2C + 1);
 
@@ -128,9 +139,24 @@ static void board_select_drp_mode(void)
 }
 DECLARE_DEFERRED(board_select_drp_mode);
 
+
+static void square_wave(void);
+DECLARE_DEFERRED(square_wave);
+void square_wave(void)
+{
+	/* static int count; */
+	/* int phase = count++ & 1; */
+
+	/* board_debug_gpio(TRIGGER_1, phase); */
+	/* board_debug_gpio(TRIGGER_2, !phase); */
+	hook_call_deferred(&square_wave_data, 100*MSEC);
+}
+
+
 static void board_init(void)
 {
-	hook_call_deferred(&board_select_drp_mode_data, 25 * MSEC);
+	hook_call_deferred(&board_select_drp_mode_data, 50 * MSEC);
+	hook_call_deferred(&square_wave_data, 50 * MSEC);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
@@ -149,8 +175,15 @@ void board_overcurrent_event(int port, int is_overcurrented)
 
 void board_debug_gpio(int trigger, int enable)
 {
-	enum gpio_signal signal = (trigger == TRIGGER_1) ?
-		GPIO_TRIGGER_1 : GPIO_TRIGGER_2;
-
-	gpio_set_level(signal, enable);
+	switch (trigger) {
+	case TRIGGER_1:
+		gpio_set_level(GPIO_TRIGGER_1, enable);
+		break;
+	case TRIGGER_2:
+		gpio_set_level(GPIO_TRIGGER_2, enable);
+		break;
+	default:
+		CPRINTS("bad debug gpio selection");
+		break;
+	}
 }
