ae783f0075ce8ba31edb3bae1839abbd80d90902
Scott Collyer <scollyer@google.com>
2020-06-16 13:13:35 -0700

    TCPMv2: Add guard for state exit that checks for try.src
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I941f8901717b7db2df527d240dc6f2ab45b896e0

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -1524,7 +1524,7 @@ static void handle_new_power_state(int port)
 }
 #endif /* CONFIG_POWER_COMMON */
 
-#if defined(CONFIG_USB_PD_ALT_MODE) && !defined(CONFIG_USB_PD_ALT_MODE_DFP)
+#if defined(CONFIG_USB_PD_ALT_MODE)
 void pd_send_hpd(int port, enum hpd_event hpd)
 {
 	uint32_t data[1];
