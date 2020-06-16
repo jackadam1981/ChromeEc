2f19ab8a6198ac9760229b8183de5f86689e8734
Scott Collyer <scollyer@google.com>
2020-06-15 20:56:25 -0700

    tusb1064: Create usb mux driver for tsub1064
    The tusb1064 is a type C demux and retimer switch. This CL adds
    mux driver support for tusb0164.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I1ee836ef0a88adab1af63901c2ec0be7cef7b232

--- include/config.h
+++ include/config.h
@@ -4683,6 +4683,9 @@
 /* Support the Parade PS8822 Type-C Redriving Demux Switch */
 #undef CONFIG_USB_MUX_PS8822
 
+/* Support the TI TUSB1064 Type-C Redriving Demux Switch */
+#undef CONFIG_USB_MUX_TUSB1064
+
 /* 'Virtual' USB mux under host (not EC) control */
 #undef CONFIG_USB_MUX_VIRTUAL
 
