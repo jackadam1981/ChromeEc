5e564588db92a2e91da9027c1ca429579b20fd83
Patryk Duda <pdk@semihalf.com>
2020-06-17 15:55:59 +0200

    TCPMv2: Do not initialize port which is not available
    If port doesn't exist there is no reason to initialize anything.
    Go directly to Disabled state and set flags to disable PE and PRL
    state machines too.
    
    BUG=b:154602760
    BRANCH=none
    TEST=Flash casta. EC RW should report port 1 transition to
         Disabled state
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: I4d90324842363a5b714ee2e2953edffb30c6009f

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -1160,6 +1160,14 @@ static void restart_tc_sm(int port, enum usb_tc_state start_state)
 
 void tc_state_init(int port)
 {
+	/* If port is not available, there is nothing to initialize */
+	if (port >= board_get_usb_pd_port_count()) {
+		tc_policy_pd_enable(port, 0);
+		TC_SET_FLAG(port, TC_FLAGS_SUSPEND);
+		set_state_tc(port, TC_DISABLED);
+		return;
+	}
+
 	/* Default to not jumping warm to ATTACHED_SNK */
 	TC_CLR_FLAG(port, TC_FLAGS_TC_WARM_ATTACHED_SNK);
 
