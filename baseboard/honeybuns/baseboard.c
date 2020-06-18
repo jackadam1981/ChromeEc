17748ad10e6c3f361bf658f3d90704fff29df737
Scott Collyer <scollyer@google.com>
2020-06-17 20:23:43 -0700

    honeybuns: Enable usb endpoint for fwupd
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: If535ccbaf5c404482d747f43ed577fdf286ef202

--- baseboard/honeybuns/baseboard.c
+++ baseboard/honeybuns/baseboard.c
@@ -13,10 +13,29 @@
 #include "i2c.h"
 #include "usb_pd.h"
 #include "timer.h"
+#include "usb_descriptor.h"
 #include "util.h"
 
 #define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ## args)
+
+/*
+ * Define the strings used in our USB descriptors.
+ */
+const void *const usb_strings[] = {
+	[USB_STR_DESC]         = usb_string_desc,
+	[USB_STR_VENDOR]       = USB_STRING_DESC("Google Inc."),
+	[USB_STR_PRODUCT]      = USB_STRING_DESC("Honeybuns"),
+	[USB_STR_SERIALNO]     = 0,
+	[USB_STR_VERSION]      = 0,
+	[USB_STR_I2C_NAME]     = USB_STRING_DESC("I2C"),
+	[USB_STR_UPDATE_NAME]  = USB_STRING_DESC("Firmware update"),
+#ifdef CONFIG_USB_ISOCHRONOUS
+	[USB_STR_HEATMAP_NAME] = USB_STRING_DESC("Heatmap"),
+#endif
+};
+
+BUILD_ASSERT(ARRAY_SIZE(usb_strings) == USB_STR_COUNT);
 
 struct hpd_mark {
 	int level;
