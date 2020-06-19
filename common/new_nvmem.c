36d14486e22a3e82d6e92eade14a366357659433
Vadim Bendebury <vbendeb@chromium.org>
2020-06-18 17:23:07 -0700

    WIP: selective erase of TPM objects
    what happened
    
    BUG=b:
    TEST=manual
    
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Change-Id: I3e299c8004141fa01ff20c290131b6526575c42e

--- common/new_nvmem.c
+++ common/new_nvmem.c
@@ -2995,13 +2995,7 @@ static void dump_contents(const struct nn_container *ch)
 	ccprintf("\n");
 }
 
-/*
- * Clear tpm data from nvmem. First fill up the current top page with erased
- * objects, then compact the flash storage, removing all TPM related objects.
- * This would guarantee that all pages where TPM objecs were stored would be
- * erased.
- */
-int nvmem_erase_tpm_data(void)
+int nvmem_erase_tpm_data_selective(const uint32_t *objs_to_erase)
 {
 	const uint8_t *key;
 	const uint8_t *val;
@@ -3024,6 +3018,31 @@ int nvmem_erase_tpm_data(void)
 		    (ch->container_type != NN_OBJ_TPM_EVICTABLE))
 			continue;
 
+		/* If not all TPM objects need to be erased. */
+		if (objs_to_erase) {
+			uint32_t obj_index;
+			const uint32_t *obj_to_erase;
+
+			/* Index of the current NVMEM object. */
+			memcpy(&obj_index, ch + 1, sizeof(obj_index));
+
+			/*
+			 * Iterate over indices of the subset of objects which
+			 * need to be erased.
+			 */
+			obj_to_erase = objs_to_erase;
+			do {
+				if (obj_index == *obj_to_erase)
+					break;
+			} while (*(++obj_to_erase));
+
+			/*
+			 * If current NVMEM object is not in the list, do not
+			 * erase it.
+			 */
+			if (!*obj_to_erase)
+				continue;
+		}
 		delete_object(&at, ch);
 	}
 
