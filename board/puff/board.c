496e9fc04180868a83ab770cd66acea86824b595
Andrew McRae <amcrae@google.com>
2020-06-21 13:28:53 +1000

    Puff: Only reset TCP on power-on restart
    Reset the TCPC after a power-on restart, and clear any explicit
    PD contract so that the previous state is not restored.
    
    Even though Puff does not have BBRAM, the state is required
    for restarts so that PD can determine when power needs to be
    renegotiated explicitly through sending a soft reset, or
    not since the system is starting from a fresh connection.
    
    The impact was that a system powered from a type-C charger
    would not go into recovery since a restart assumed it was
    an initial charger connection (so no soft reset was sent),
    the wait for capabilities timed out, and a hard reset was sent,
    causing the charger to cycle power, and thereby skipping the
    recovery stage.
    
    BUG=b:159281839
    TEST=Confirm that recovery can be entered using type-C charger.
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: Ibecf75e999ae5ff15580ce69c3a58a3c49930b48

--- board/puff/board.c
+++ board/puff/board.c
@@ -43,6 +43,7 @@
 #include "usb_charge.h"
 #include "usb_common.h"
 #include "usb_pd.h"
+#include "usb_pe_sm.h"
 #include "usbc_ppc.h"
 #include "util.h"
 
@@ -508,8 +509,7 @@ static void board_tcpc_init(void)
 	 * With EFSv2, it is possible to be in RW without
 	 * having reset the TCPC.
 	 */
-	if (system_get_reset_flags() &
-	    (EC_RESET_FLAG_RESET_PIN | EC_RESET_FLAG_POWER_ON))
+	if (system_get_reset_flags() & EC_RESET_FLAG_POWER_ON)
 		board_reset_pd_mcu();
 	/* Enable TCPC interrupts. */
 	gpio_enable_interrupt(GPIO_USB_C0_TCPPC_INT_ODL);
@@ -542,9 +542,9 @@ void board_reset_pd_mcu(void)
 	gpio_set_level(GPIO_USB_C0_TCPC_RST, !level);
 	if (BOARD_TCPC_C0_RESET_POST_DELAY)
 		msleep(BOARD_TCPC_C0_RESET_POST_DELAY);
-	/* Reset any saved state for previous connections. */
+	/* Since it is a power-on restart, clear any explicit contract */
 	pd_update_saved_port_flags(USB_PD_PORT_TCPC_0,
-		PD_BBRMFLG_EXPLICIT_CONTRACT, 0);
+				   PD_BBRMFLG_EXPLICIT_CONTRACT, 0);
 }
 
 int board_set_active_charge_port(int port)
