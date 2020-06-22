6ea3792e0782fb5c9c4ec411384972dc86d6f444
Scott Collyer <scollyer@google.com>
2020-06-22 15:51:42 -0700

    honeybuns: Change I2C port symbolic names
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Ia387efa61a037b8e0288e28c02eb04c1e36e81b8

--- baseboard/honeybuns/baseboard.h
+++ baseboard/honeybuns/baseboard.h
@@ -89,8 +89,8 @@ enum usb_strings {
 #define CONFIG_USB_PD_CUSTOM_PDO
 #define CONFIG_USB_PD_DUAL_ROLE
 /* #define CONFIG_USB_PD_MAX_SINGLE_SOURCE_CURRENT TYPEC_RP_3A0 */
-#define CONFIG_USB_PD_PORT_MAX_COUNT 1
 #define CONFIG_USB_PD_TCPM_MUX
+#define CONFIG_USB_PD_TCPM_PS8805
 #define CONFIG_USB_PD_TCPM_STM32GX
 #define CONFIG_USB_PD_TCPM_TCPCI
 #define CONFIG_USB_PD_DECODE_SOP
@@ -102,6 +102,7 @@ enum usb_strings {
 #define CONFIG_USBC_PPC_VCONN
 #define CONFIG_USBC_PPC_DEDICATED_INT
 #define CONFIG_CMD_PPC_DUMP
+#define CONFIG_CMD_TCPC_DUMP
 
 #define CONFIG_STM32G4_UCPD_DEBUG
 
@@ -131,13 +132,6 @@ enum usb_strings {
 /* I2C Bus Configuration */
 #define CONFIG_I2C
 #define CONFIG_I2C_CONTROLLER
-
-#define I2C_PORT_USBC		0
-#define I2C_PORT_MST		1
-#define I2C_PORT_EEPROM	2
-#define I2C_PORT_MP4245 I2C_PORT_MST
-
-#define MP4245_SLAVE_ADDR MP4245_I2C_ADDR_0_FLAGS
 
 /*
  * Macros for GPIO signals used in common code that don't match the
