9df26b3b4be383e20db08e2bbe3f866e3901dfbf
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2020-06-22 09:16:03 +0800

    common: add CONFIG_POWER_SIGNAL_RUNTIME_CONFIG
    This patch add config: CONFIG_POWER_SUGNAL_RUNTIME_CONFIG
    to allow board modified power signal gpio if needed.
    
    BUG=b:150278507, b:152841287
    BRANCH=none
    TEST=make buildall
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: I5c885f9cb3400f3354a18b6d497340dfe3ad993b

--- include/power.h
+++ include/power.h
@@ -69,6 +69,12 @@ struct power_signal_info {
  * Each board must provide its signal list and a corresponding enum
  * power_signal.
  */
+#ifdef CONFIG_POWER_SIGNAL_RUNTIME_CONFIG
+extern struct power_signal_info power_signal_list[];
+#else
+extern const struct power_signal_info power_signal_list[];
+#endif
+
 extern const struct power_signal_info power_signal_list[];
 
 /* Convert enum power_signal to a mask for signal functions */
