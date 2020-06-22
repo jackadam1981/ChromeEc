239765aad24806e5d0365a360dff51af5725a2a8
Scott Collyer <scollyer@google.com>
2020-06-22 15:46:42 -0700

    ppc: Create stub driver for asymmetric port configuration
    Chromebooks have a policy have port behavior being symmetric. However,
    other type-c devices may not follow this requirement. The stub driver
    can be used when a PPC is on at least one, but not all ports.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: If6bd96d27d1945ac4bdd6405c33a47230e19e3df

---
+++ driver/ppc/stub.h
@@ -0,0 +1,15 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* PPC Stub Type-C Power Path Controller */
+
+#ifndef __CROS_EC_PPC_STUB_H
+#define __CROS_EC_PPC_STUB_H
+
+
+struct ppc_drv;
+extern const struct ppc_drv ppc_stub_drv;
+
+#endif /* defined(__CROS_EC_PPC_STUB_H) */
