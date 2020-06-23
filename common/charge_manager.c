8e060594f206c1c1591b6fb4d794cdf57a3d8332
Madhu M <madhu.m@intel.corp-partner.google.com>
2020-06-23 14:28:46 +0530

    charge_manager: Port power swap based on presence of battery
    Port power swap has to be done only when there is a battery and
    no battery cutoff
    
    BUG=b:149460624
    BRANCH=none
    TEST=1.Remove battery, connect charger on typec port
           boot the DUT and after boot the DUT connect docking station
           on another typec port. expected result is no reboot observed
         2.Connect battery, connect charger on typec port
           boot the DUT and after boot the DUT connect docking station
           on another typec port. expected result is no reboot observed.
    
    Signed-off-by: Madhu M <madhu.m@intel.corp-partner.google.com>
    Change-Id: Iacfae4ecc2e30c6bee5a19aaed030d59efc61e36

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -650,6 +650,17 @@ static void charge_manager_get_best_charge_port(int *new_port,
 
 	}
 
+	/* if no battery presence then retain charge port and charge supplier */
+	if (supplier == CHARGE_SUPPLIER_PD &&
+	    charge_port != CHARGE_SUPPLIER_NONE &&
+	    charge_port != port &&
+	    (battery_is_present() == BP_NO ||
+	     (battery_is_present() == BP_YES &&
+	      battery_is_cut_off() != BATTERY_CUTOFF_STATE_NORMAL))) {
+		port = charge_port;
+		supplier = charge_supplier;
+	}
+
 	*new_port = port;
 	*new_supplier = supplier;
 }
