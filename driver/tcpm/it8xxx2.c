ed2fc2f51437aa08baeec70cd426846d46c53a0d
Ruibin Chang <ruibin.chang@ite.com.tw>
2020-06-23 15:39:43 +0800

    WIP chip/it8320, it81202: Implement fast role swap function
      Implement fast role swap function from initial SNK to new SRC
    for chip it8320 and it81202.
    
    BUG=none
    BRANCH=none
    TEST=When we detect FRS cc low signal, we can output properly
         GPIO signal.
    
    Change-Id: I3a7bc6a684e57fb19e50c41305751b2fca1ffe2d
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- driver/tcpm/it8xxx2.c
+++ driver/tcpm/it8xxx2.c
@@ -587,6 +587,31 @@ static int it83xx_tcpm_get_chip_info(int port, int live,
 	return EC_SUCCESS;
 }
 
+static void it83xx_tcpm_set_frs_enable(int port, int enable)
+{
+	uint8_t mask = (USBPD_REG_FAST_SWAP_REQUEST_ENABLE |
+			USBPD_REG_FAST_SWAP_DETECT_ENABLE);
+
+	if (enable) {
+		/* W/C status */
+		IT83XX_USBPD_IFS(port) = 0x33;
+		/* Enable FRS detection (cc to GND) interrupt */
+		IT83XX_USBPD_MIFS(port) &= ~(USBPD_REG_MASK_FAST_SWAP_ISR |
+					USBPD_REG_MASK_FAST_SWAP_DETECT_ISR);
+		/* Enable FRS detection (cc to GND) */
+		IT83XX_USBPD_PDFSCR(port)  = (IT83XX_USBPD_PDFSCR(port) & ~mask)
+					| USBPD_REG_FAST_SWAP_DETECT_ENABLE;
+		/* TODO: enable HW automatically output binding GPIO signal */
+	} else {
+		/* Disable FRS detection (cc to GND) interrupt */
+		IT83XX_USBPD_MIFS(port) |= (USBPD_REG_MASK_FAST_SWAP_ISR |
+					USBPD_REG_MASK_FAST_SWAP_DETECT_ISR);
+		/* Disable FRS detection and requestion */
+		IT83XX_USBPD_PDFSCR(port) &= ~mask;
+		/* TODO: disable HW automatically output binding GPIO signal */
+	}
+}
+
 static void it83xx_init(enum usbpd_port port, int role)
 {
 	uint8_t cc_config = (port == USBPD_PORT_C ?
@@ -677,4 +702,5 @@ const struct tcpm_drv it83xx_tcpm_drv = {
 	.get_message_raw	= &it83xx_tcpm_get_message_raw,
 	.transmit		= &it83xx_tcpm_transmit,
 	.get_chip_info		= &it83xx_tcpm_get_chip_info,
+	.set_frs_enable		= &it83xx_tcpm_set_frs_enable,
 };
