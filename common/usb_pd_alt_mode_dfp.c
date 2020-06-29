bdd86c03aeb4e46d9e4545d044cf073251ee2c72
Ayushee <ayushee.shah@intel.com>
2020-06-29 13:19:52 -0700

    usb_pd: Correct 'enter_tbt_compat_mode()' function
    Attempt to Enter Mode SOP'' with SVID and same object position as
    Discover Mode SOP' since, the port doesn't query Discover SOP'' to
    the cable.
    
    Also correct VDO_SVDM_VERS according to type of message to be
    transmitted for enter_tbt_compat_mode().
    
    BUG=b:158294748
        b:150611251
    BRANCH=None
    TEST=Able to enter into Thunderbolt mode for active cable.
    
    Change-Id: Ib1f58ff3bab78e1b0ace554ac63785b6e7baf192
    Signed-off-by: Ayushee <ayushee.shah@intel.com>

--- common/usb_pd_alt_mode_dfp.c
+++ common/usb_pd_alt_mode_dfp.c
@@ -837,9 +837,12 @@ int enter_tbt_compat_mode(int port, enum tcpm_transmit_type sop,
 	union tbt_mode_resp_cable cable_mode_resp;
 
 	/* Table F-12 TBT3 Cable Enter Mode Command */
-	payload[0] = pd_dfp_enter_mode(port, sop, USB_VID_INTEL, 0) |
+	payload[0] = pd_dfp_enter_mode(port,
+				       sop == TCPC_TX_SOP ? TCPC_TX_SOP :
+							    TCPC_TX_SOP_PRIME,
+				       USB_VID_INTEL, 0) |
 		     VDO_CMDT(CMDT_INIT) |
-		     VDO_SVDM_VERS(pd_get_vdo_ver(port, TCPC_TX_SOP));
+		     VDO_SVDM_VERS(pd_get_vdo_ver(port, sop));
 
 	/* For TBT3 Cable Enter Mode Command, number of Objects is 1 */
 	if ((sop == TCPC_TX_SOP_PRIME) ||
