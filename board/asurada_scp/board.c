3a14bd273edb57642242a5ba0baf86d680f068d2
Tzung-Bi Shih <tzungbi@chromium.org>
2020-06-30 14:19:34 +0800

    TEST-ONLY: test dummy IPI
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: I25c16f4c939356cb36bbd143172cb3e680fbcd4b

--- board/asurada_scp/board.c
+++ board/asurada_scp/board.c
@@ -7,3 +7,62 @@
 #include "registers.h"
 
 #include "gpio_list.h"
+
+#include "ipi_chip.h"
+#include "queue.h"
+#include "queue_policies.h"
+#include "task.h"
+#include "timer.h"
+
+struct q_msg {
+	int32_t ipi_id;
+};
+
+#define Q(N, X) \
+static struct consumer const q##N##_consumer; \
+\
+static void q##N##_written(struct consumer const *consumer, size_t count) \
+{ \
+	task_wake(TASK_ID_##N); \
+} \
+\
+static struct queue const q##N = QUEUE_DIRECT(16, struct q_msg, null_producer, q##N##_consumer); \
+static struct consumer const q##N##_consumer = { \
+	.queue = &q##N, \
+	.ops = &((struct consumer_ops const) { \
+			.written = q##N##_written, \
+		}), \
+}; \
+\
+static void handle_##N(int32_t id, void *data, uint32_t len) \
+{ \
+	struct q_msg qmsg; \
+\
+	if (!len) \
+		return; \
+\
+	qmsg.ipi_id = id; \
+\
+	if (!queue_add_unit(&q##N, &qmsg)) \
+		ccprints("failed to queue_add_unit"); \
+} \
+DECLARE_IPI(X, handle_##N, 0); \
+\
+void N##_task(void *u) \
+{ \
+	struct q_msg qmsg; \
+	size_t size; \
+\
+	while (1) { \
+		ipi_disable_irq(); \
+		size = queue_remove_unit(&q##N, &qmsg); \
+		ipi_enable_irq(); \
+\
+		if (!size) \
+			task_wait_event(-1); \
+		else \
+			ipi_send(qmsg.ipi_id, 0, 0, 0); \
+	} \
+}
+
+Q(x, 10);
