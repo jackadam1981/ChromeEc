6a01a93319488938ef81aeec4ad2826f2816c914
Devin Lu <Devin.Lu@quantatw.com>
2020-07-01 09:04:24 +0800

    drawcia: remove unused retimer
    CL:2249645 applied drawcia C1 port to ps8705. There is no need
    retimer tusb544 with ps8705.
    
    BUG=none
    BRANCH=none
    TEST=make buildall -j
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I6123cd8034a39837f90c051d6643fb6fcd9ebac0

--- board/drawcia/board.c
+++ board/drawcia/board.c
@@ -173,14 +173,6 @@ const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	},
 };
 
-/* USB Retimer */
-const struct usb_mux usbc1_retimer = {
-	.usb_port = 1,
-	.i2c_port = I2C_PORT_SUB_USB_C1,
-	.i2c_addr_flags = TUSB544_I2C_ADDR_FLAGS0,
-	.driver = &tusb544_drv,
-};
-
 /* USB Muxes */
 const struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	{
@@ -194,7 +186,6 @@ const struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 		.i2c_port = I2C_PORT_SUB_USB_C1,
 		.i2c_addr_flags = PS8751_I2C_ADDR1_FLAGS,
 		.driver = &tcpci_tcpm_usb_mux_driver,
-		.next_mux = &usbc1_retimer,
 	},
 };
 
