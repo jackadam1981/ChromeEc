3422149919a5b8f3d57a9733b363754a2ea5b7f2
Jett Rink <jettrink@chromium.org>
2020-07-01 11:41:07 -0600

    config: ensure all new board work around cr50 double reset
    All new boards, represented by EFS2, should unabled the double
    reset workaround for the CR50. This gives a better power on signal in
    the reset flags which is important for other subsystems like USB-C
    
    BRANCH=noneo
    BUG=none
    TEST=builds
    
    Signed-off-by: Jett Rink <jettrink@chromium.org>
    Change-Id: I7c0fc75138b293295ec66800e19e01e6bf8e505f

--- include/config.h
+++ include/config.h
@@ -5315,6 +5315,14 @@
 #define CONFIG_KEYBOARD_BACKLIGHT
 #endif
 
+/*
+ * Until we a no longer using CR50 with double reset issue, TCPMv2 relied
+ * on the power on flag being set correcting after the H1 reset.
+ */
+#if defined(CONFIG_VBOOT_EFS2) && !defined(CONFIG_DELAY_ON_POR)
+#error All new boards should workaroundCR50 double reset.
+#endif
+
 /*****************************************************************************/
 /* ISH power management related definitions */
 #if defined(CONFIG_ISH_PM_D0I2) || \
