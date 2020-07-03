9d06d44ec8a3ed9b5bc1c14d74c7137c317b0f59
Devin Lu <Devin.Lu@quantatw.com>
2020-07-03 09:43:56 +0800

    driver/tcpm: add support for PS8755
    The patch adds support for the Parade Tech PS8755 TCPC/SuperSpeed
    mux. It is similar chip group as PS8705/PS8805/PS8815.
    
    BUG=b:159042756, b:159082424
    BRANCH=none
    TEST=make buildall -j
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I38fa02704cc352da0e27eae8cd8bbce89a807975

--- driver/tcpm/ps8xxx.c
+++ driver/tcpm/ps8xxx.c
@@ -8,6 +8,8 @@
  *
  * Supported TCPCs:
  * - PS8751
+ * - PS8755
+ * - PS8705
  * - PS8805
  * - PS8815
  */
@@ -21,6 +23,7 @@
 #include "usb_pd.h"
 
 #if !defined(CONFIG_USB_PD_TCPM_PS8751) && \
+	!defined(CONFIG_USB_PD_TCPM_PS8755) && \
 	!defined(CONFIG_USB_PD_TCPM_PS8705) && \
 	!defined(CONFIG_USB_PD_TCPM_PS8805) && \
 	!defined(CONFIG_USB_PD_TCPM_PS8815)
@@ -285,7 +288,7 @@ static int ps8xxx_enter_low_power_mode(int port)
 #endif
 
 #if defined(CONFIG_USB_PD_TCPM_PS8751) || defined(CONFIG_USB_PD_TCPM_PS8805) \
- || defined(CONFIG_USB_PD_TCPM_PS8705)
+ || defined(CONFIG_USB_PD_TCPM_PS8705) || defined(CONFIG_USB_PD_TCPM_PS8755)
 /*
  * DCI is enabled by default and burns about 40 mW when the port is in
  * USB2 mode or when a C-to-A dongle is attached, so force it off.
@@ -308,7 +311,7 @@ static int ps8xxx_addr_dci_disable(int port, int i2c_addr, int i2c_reg)
 	}
 	return EC_SUCCESS;
 }
-#endif /* CONFIG_USB_PD_TCPM_PS8751 || CONFIG_USB_PD_TCPM_PS8[78]05 */
+#endif /* CONFIG_USB_PD_TCPM_PS875[15] || CONFIG_USB_PD_TCPM_PS8[78]05 */
 
 #ifdef CONFIG_USB_PD_TCPM_PS8815
 static int ps8xxx_dci_disable(int port)
@@ -371,6 +374,37 @@ static int ps8xxx_dci_disable(int port)
 				       PS8751_REG_MUX_USB_DCI_CFG);
 }
 #endif /* CONFIG_USB_PD_TCPM_PS8751 */
+
+#ifdef CONFIG_USB_PD_TCPM_PS8755
+static int ps8xxx_dci_disable(int port)
+{
+	int p1_addr;
+	int p3_addr;
+	int regval;
+	int rv;
+
+	/* Enable access to debug pages. */
+	p3_addr = tcpc_config[port].i2c_info.addr_flags;
+	rv = tcpc_addr_read(port, p3_addr, PS8XXX_REG_I2C_DEBUGGING_ENABLE,
+			    &regval);
+	if (rv)
+		return rv;
+
+	rv = tcpc_addr_write(port, p3_addr, PS8XXX_REG_I2C_DEBUGGING_ENABLE,
+			     PS8XXX_REG_I2C_DEBUGGING_ENABLE_ON);
+
+	/* Disable Auto DCI */
+	p1_addr = PS8751_P3_TO_P1_FLAGS(p3_addr);
+	rv = ps8xxx_addr_dci_disable(port, p1_addr,
+				     PS8755_P1_REG_MUX_USB_DCI_CFG);
+
+	/* Turn off access to debug pages. */
+	rv |= tcpc_addr_write(port, p3_addr, PS8XXX_REG_I2C_DEBUGGING_ENABLE,
+			      PS8XXX_REG_I2C_DEBUGGING_ENABLE_OFF);
+
+	return rv;
+}
+#endif /* CONFIG_USB_PD_TCPM_PS8755 */
 
 static int ps8xxx_tcpm_init(int port)
 {
