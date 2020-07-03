4592643118a24c6b1d7604e999e8088d99b60f1c
Devin Lu <Devin.Lu@quantatw.com>
2020-07-03 09:43:56 +0800

    driver/tcpm: add support for PS8755
    The patch adds support for the Parade Tech PS8755 TCPC/SuperSpeed
    mux. It is similar chip gruop as PS8705/PS8805/PS8815.
    
    BUG=b:159042756, b:159082424
    BRANCH=none
    TEST=make buildall -j
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I38fa02704cc352da0e27eae8cd8bbce89a807975

--- driver/tcpm/ps8xxx.h
+++ driver/tcpm/ps8xxx.h
@@ -80,6 +80,14 @@
 #define PS8XXX_REG_MUX_USB_C2SS_HS_THRESHOLD    0xE8
 #define PS8751_REG_MUX_USB_DCI_CFG              0xED
 
+#elif defined(CONFIG_USB_PD_TCPM_PS8755)
+/* Vendor defined registers */
+/* NOTE: The Product ID will read as 0x8803 if the firmware has malfunctioned */
+#define PS8XXX_PRODUCT_ID 0x8755
+
+#define PS8755_P1_REG_MUX_USB_DCI_CFG           0x4B
+#define FW_VER_REG                              0x82
+
 #elif defined(CONFIG_USB_PD_TCPM_PS8705)
 /* Vendor defined registers */
 /* NOTE: The Product ID will read as 0x8803 if the firmware has malfunctioned */
