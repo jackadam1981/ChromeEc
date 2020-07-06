30402bb5e390c29fd565528a310147b53d9b4654
Paul Fagerburg <pfagerburg@google.com>
2020-07-06 14:33:45 -0600

    TCPMv2: add unit tests for packet discard logic
    Add unit tests for the packet discard logic for packets while going
    into SinkTxNG.
    
    BUG=b:160622527
    BRANCH=None
    TEST=`TEST_LIST_HOST=usb_prl make runhosttests`
    
    Signed-off-by: Paul Fagerburg <pfagerburg@chromium.org>
    Change-Id: Ied7689d2fe40da93069e58b53ea32e053ab887b8

--- include/mock/usb_tc_sm_mock.h
+++ include/mock/usb_tc_sm_mock.h
@@ -11,7 +11,13 @@
 #include "usb_tc_sm.h"
 
 struct mock_tc_port_t {
+	int rev;
 	int pd_enable;
+	enum pd_power_role power_role;
+	enum pd_data_role data_role;
+	int msg_tx_id;
+	int msg_rx_id;
+	enum tcpm_transmit_type sop;
 	enum tcpc_rp_value lcl_rp;
 };
 
