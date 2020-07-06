ca2cf834b2922c960498f845ffe9c93d7941f3a4
David Huang <david.huang@quanta.corp-partner.google.com>
2020-07-06 17:02:19 +0800

    Terrador: supports bb_retimer for C0/C1 port
    add bb retimer for C0/C1 port
    
    BUG=b:151978872
    BRANCH=master
    TEST=Use console "i2cscan" get usb_0_mix/usb_0_mix and bb to read bb
    retimer register.
    
    Change-Id: I832265a69b30beca6e49840f90333abd6b297ec7
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>

--- board/terrador/board.c
+++ board/terrador/board.c
@@ -46,11 +46,35 @@ union volteer_cbi_fw_config fw_config_defaults = {
 	.usb_db = DB_USB3_PASSIVE,
 };
 
+/* USBC mux configuration - Tiger Lake includes internal mux */
+struct usb_mux usbc0_usb4_mb_retimer = {
+	.usb_port = USBC_PORT_C0,
+	.driver = &bb_usb_retimer,
+	.i2c_port = I2C_PORT_USB_0_MIX,
+	.i2c_addr_flags = USBC_PORT_C0_BB_RETIMER_I2C_ADDR,
+};
+/*****************************************************************************
+ * USB-C MUX/Retimer dynamic configuration.
+ */
+static void setup_mux(void)
+{
+	CPRINTS("C0 supports bb-retimer");
+	/* USB-C port 0 have a retimer */
+	usb_muxes[USBC_PORT_C0].next_mux = &usbc0_usb4_mb_retimer;
+}
+
 static void board_init(void)
 {
 	/* Illuminate motherboard and daughter board LEDs equally to start. */
 	pwm_enable(PWM_CH_LED4_SIDESEL, 1);
 	pwm_set_duty(PWM_CH_LED4_SIDESEL, 50);
+
+	setup_mux();
+	/* Reassign USB_C0_RT_RST_ODL */
+	bb_controls[USBC_PORT_C0].shared_nvm = false;
+	bb_controls[USBC_PORT_C0].usb_ls_en_gpio = GPIO_USB_C0_LS_EN;
+	bb_controls[USBC_PORT_C0].retimer_rst_gpio = GPIO_USB_C0_RT_RST_ODL;
+	bb_controls[USBC_PORT_C0].force_power_gpio = GPIO_USB_C0_RT_FORCE_PWR;
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
@@ -105,11 +129,18 @@ const struct i2c_port_t i2c_ports[] = {
 		.sda = GPIO_EC_I2C2_USB_C1_SDA,
 	},
 	{
+		.name = "usb_0_mix",
+		.port = I2C_PORT_USB_0_MIX,
+		.kbps = 100,
+		.scl = GPIO_EC_I2C3_USB_0_MIX_SCL,
+		.sda = GPIO_EC_I2C3_USB_0_MIX_SDA,
+	},
+	{
 		.name = "usb_1_mix",
 		.port = I2C_PORT_USB_1_MIX,
 		.kbps = 100,
-		.scl = GPIO_EC_I2C3_USB_1_MIX_SCL,
-		.sda = GPIO_EC_I2C3_USB_1_MIX_SDA,
+		.scl = GPIO_EC_I2C4_USB_1_MIX_SCL,
+		.sda = GPIO_EC_I2C4_USB_1_MIX_SDA,
 	},
 	{
 		.name = "power",
