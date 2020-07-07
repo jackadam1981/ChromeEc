23ec65ce7452b0c96e582bc504bea8a933f4f8e2
xiong.huang <xiong.huang@bitland.corp-partner.google.com>
2020-07-07 13:23:20 +0800

    boten: define led behavior
    Boten uses two types LED, battery LED and power led. Battery LED uses
    dual-color LED, green and red. Power LED uses monochrome LED, white.
    
    Adjust battery and power LED luminance with PWM method.
    
    BUG=b:160664441
    BRANCH=none
    TEST=`ectool led battery red/amber/green/off` and `ectool led power
    white/off` work well.
    
    Signed-off-by: xiong.huang <xiong.huang@bitland.corp-partner.google.com>
    Change-Id: I6fb87d841ce837ebdc256154dc4b8557f6ed0401

--- board/boten/board.c
+++ board/boten/board.c
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-/* Waddledee board-specific configuration */
+/* Boten board-specific configuration */
 
 #include "button.h"
 #include "charge_manager.h"
@@ -107,6 +107,11 @@ void board_init(void)
 	gpio_enable_interrupt(GPIO_USB_C0_CCSBU_OVP_ODL);
 	/* Enable gpio interrupt for base accelgyro sensor */
 	gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
+
+	/* Set LEDs luminance */
+	pwm_set_duty(PWM_CH_LED_RED, 70);
+	pwm_set_duty(PWM_CH_LED_GREEN, 70);
+	pwm_set_duty(PWM_CH_LED_WHITE, 70);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
@@ -219,7 +224,7 @@ const struct pwm_t pwm_channels[] = {
 		.freq_hz = 2400,
 	},
 
-	[PWM_CH_LED_BLUE] = {
+	[PWM_CH_LED_WHITE] = {
 		.channel = 3,
 		.flags = PWM_CONFIG_DSLEEP | PWM_CONFIG_ACTIVE_LOW,
 		.freq_hz = 2400,
