9622c709d50e8e1f9288c39299ec232296f3fb1d
Daisuke Nojiri <dnojiri@chromium.org>
2020-07-10 17:39:46 -0700

    (PoC) USB-PD: Set EC_RESET_FLAG_BROWNOUT before reset PD
    This patch sets EC_RESET_FLAG_BROWNOUT before resetting TCPCs or
    presenting Rp to the port partner. It'll allow TCPM to skip the
    port reset next time.
    
    This patch also uses IBBR as an indication of brownout.
    
    This patch also adds 1 sec delay for POR reset.
    
    BUG=b:151329011, chromium:1104305
    BRANCH=none
    TEST=TBD
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I2d7cf36e7b3f5a6d5b536c74366534fde93bb360

--- chip/npcx/system.c
+++ chip/npcx/system.c
@@ -88,6 +88,7 @@ static int bbram_is_byte_access(enum bbram_data_index index)
 void system_check_bbram_on_reset(void)
 {
 	if (IS_BIT_SET(NPCX_BKUP_STS, NPCX_BKUP_STS_IBBR)) {
+		system_set_reset_flags(EC_RESET_FLAG_BROWNOUT);
 		/*
 		 * If the reset cause is not power-on reset and VBAT has ever
 		 * dropped, print a warning message.
@@ -102,7 +103,7 @@ void system_check_bbram_on_reset(void)
 		 * npcx7m6fb/npcx7m6fc/npcx7m7wb/npcx7m7wc:
 		 *   Clear IBBR/VSBY_STS/VCC1_STS bit
 		 */
-		NPCX_BKUP_STS = NPCX_BKUP_STS_ALL_MASK;
+		NPCX_BKUP_STS = (NPCX_BKUP_STS_ALL_MASK & ~NPCX_BKUP_STS_IBBR);
 	}
 }
 
@@ -115,7 +116,6 @@ static int bbram_valid(enum bbram_data_index index, int bytes)
 
 	/* Check BBRAM is valid */
 	if (IS_BIT_SET(NPCX_BKUP_STS, NPCX_BKUP_STS_IBBR)) {
-		NPCX_BKUP_STS = BIT(NPCX_BKUP_STS_IBBR);
 		panic_printf("IBBR set: BBRAM corrupted!\n");
 		return 0;
 	}
@@ -157,6 +157,11 @@ static uint32_t bbram_data_read(enum bbram_data_index index)
 static int bbram_data_write(enum bbram_data_index index, uint32_t value)
 {
 	int bytes = bbram_is_byte_access(index) ? 1 : 4;
+
+	if (IS_BIT_SET(NPCX_BKUP_STS, NPCX_BKUP_STS_IBBR)) {
+		NPCX_BKUP_STS = BIT(NPCX_BKUP_STS_IBBR);
+		CPRINTS("Clear IBBR");
+	}
 
 	if (!bbram_valid(index, bytes))
 		return EC_ERROR_INVAL;
@@ -321,8 +326,7 @@ uint32_t chip_read_reset_flags(void)
 static void check_reset_cause(void)
 {
 	uint32_t hib_wake_flags = bbram_data_read(BBRM_DATA_INDEX_WAKE);
-	uint32_t chip_flags = chip_read_reset_flags();
-	uint32_t flags = chip_flags;
+	uint32_t flags = chip_read_reset_flags();
 
 	/* Clear saved reset flags in bbram */
 #ifdef CONFIG_POWER_BUTTON_INIT_IDLE
@@ -330,9 +334,9 @@ static void check_reset_cause(void)
 	 * We're not sure whether we're booting or not. AP_IDLE will be cleared
 	 * on S5->S3 transition.
 	 */
-	chip_flags &= EC_RESET_FLAG_AP_IDLE;
+	chip_save_reset_flags(flags & EC_RESET_FLAG_AP_IDLE);
 #else
-	chip_flags = 0;
+	chip_save_reset_flags(0);
 #endif
 	/* Clear saved hibernate wake flag in bbram , too */
 	bbram_data_write(BBRM_DATA_INDEX_WAKE, 0);
@@ -344,60 +348,15 @@ static void check_reset_cause(void)
 		flags |= EC_RESET_FLAG_RESET_PIN;
 #else
 		/* Check for VCC1 reset */
-		int reset = IS_BIT_SET(NPCX_RSTCTL, NPCX_RSTCTL_VCC1_RST_STS);
-
-		/*
-		 * If configured, check the saved flags to see whether
-		 * the previous restart was a power-on, in which case
-		 * treat this restart as a power-on as well.
-		 * This is to workaround the fact that the H1 will
-		 * reset the EC at power up.
-		 */
-		if (IS_ENABLED(CONFIG_BOARD_RESET_AFTER_POWER_ON)) {
-			/*
-			 * Reset pin restart rather than power-on, so check
-			 * for any flag set from a previous power-on.
-			 */
-			if (reset) {
-				if (flags & EC_RESET_FLAG_INITIAL_PWR)
-					/*
-					 * The previous restart was a power-on
-					 * so treat this restart as that, and
-					 * clear the flag so later code will
-					 * not wait for the second reset.
-					 */
-					flags =
-					  (flags & ~EC_RESET_FLAG_INITIAL_PWR)
-					   | EC_RESET_FLAG_POWER_ON;
-				else
-					/*
-					 * No previous power-on flag,
-					 * so this is a subsequent restart
-					 * i.e any restarts after the
-					 * second restart caused by the H1.
-					 */
-					flags |= EC_RESET_FLAG_RESET_PIN;
-			} else {
-				/*
-				 * Power-on restart, so set a flag and save it
-				 * for the next imminent reset. Later code
-				 * will check for this flag and wait for the
-				 * second reset.
-				 */
-				flags |= EC_RESET_FLAG_POWER_ON
-				      | EC_RESET_FLAG_INITIAL_PWR;
-				chip_flags |= EC_RESET_FLAG_INITIAL_PWR;
-			}
-		} else
-			/*
-			 * No second reset after power-on, so
-			 * set the flags according to the restart reason.
-			 */
-			flags |= reset ? EC_RESET_FLAG_RESET_PIN
-				       : EC_RESET_FLAG_POWER_ON;
+		if (IS_BIT_SET(NPCX_RSTCTL, NPCX_RSTCTL_VCC1_RST_STS)) {
+			flags |= EC_RESET_FLAG_RESET_PIN;
+		} else {
+			flags |= EC_RESET_FLAG_POWER_ON;
+			/* Wait for the 2nd reset */
+			sleep(1);
+		}
 #endif
 	}
-	chip_save_reset_flags(chip_flags);
 
 	/*
 	 * Set scratch bit to distinguish VCC1RST# is asserted again
@@ -434,7 +393,7 @@ static void check_reset_cause(void)
 		SET_BIT(NPCX_T0CSR, NPCX_T0CSR_WDRST_STS);
 	}
 
-	system_set_reset_flags(flags);
+	system_set_reset_flags(system_get_reset_flags() | flags);
 }
 
 /**
