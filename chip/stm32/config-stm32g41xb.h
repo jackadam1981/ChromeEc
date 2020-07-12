5e6479c5f2ac3617660c3aa1d7f3262dca7eefe0
Scott Collyer <scollyer@google.com>
2020-07-12 12:22:56 -0700

    stm32g4: Restore correct 128k flash size config
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I67a5a414a1a1d75ea7cb7957aa12035d837e9394

--- chip/stm32/config-stm32g41xb.h
+++ chip/stm32/config-stm32g41xb.h
@@ -16,6 +16,7 @@
  * The minimum write size for STM32G4 is 8 bytes. Cros-EC does not support
  * PSTATE in single bank memories with a write size > 4 bytes.
  */
+
 /*
  * TODO(b/167462264): This is a temporary change to allow for platform bringup
  * without being constrained by flash space issue. Currently only using RO image
