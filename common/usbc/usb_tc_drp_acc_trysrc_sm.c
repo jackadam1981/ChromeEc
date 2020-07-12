bc176e8f49bece1e55dbee81c4e6e6ae9dd8b07b
Scott Collyer <scollyer@google.com>
2020-07-12 15:46:15 -0700

    tcpmv2: Add guard for try.src config option.
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I58992d8b54d1184ef7abf6cf0725e27b65da6f2f

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -1936,12 +1936,11 @@ static void tc_error_recovery_run(const int port)
 		set_state_tc(port, TC_UNATTACHED_SNK);
 		return;
 	}
-
+#ifdef CONFIG_USB_PD_TRY_SRC
 	/*
 	 * If try src support is active (e.g. in S0). Then try to become the
 	 * SRC, otherwise we should try to be the sink.
 	 */
-#ifdef CONFIG_USB_PD_TRY_SRC
 	restart_tc_sm(port, is_try_src_enabled(port) ? TC_UNATTACHED_SRC :
 						       TC_UNATTACHED_SNK);
 #else
