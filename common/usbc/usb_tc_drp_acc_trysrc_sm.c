677535638235c18f10b2e1198e828c540a464713
Scott Collyer <scollyer@google.com>
2020-07-12 12:16:03 -0700

    tcpmv2: Fix build issue when usbc is in RW bu not RO image
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I0704f53a5e0d21a7aa0fcbe32a5ac3efc8eee060

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -1613,8 +1613,9 @@ void pd_send_hpd(int port, enum hpd_event hpd)
 		0, /* power low */
 		0x2);
 
-	pd_send_vdm(port, USB_SID_DISPLAYPORT,
-		VDO_OPOS(opos) | CMD_ATTENTION, data, 1);
+	if (IS_ENABLED(CONFIG_USB_PE_SM))
+		pd_send_vdm(port, USB_SID_DISPLAYPORT,
+			    VDO_OPOS(opos) | CMD_ATTENTION, data, 1);
 }
 #endif
 
