0774058d8c80cbe98efddcf50761e90a3b5b37c0
Ting Shen <phoenixshen@google.com>
2020-07-13 14:14:40 +0800

    mt6360: revise vbus presence detection logic
    BUG=b:159583342
    TEST=manually
    BRANCH=master
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: Ifa5366a592bc7199df85ea5489eab5010322448f

--- driver/bc12/mt6360.c
+++ driver/bc12/mt6360.c
@@ -110,16 +110,10 @@ static int mt6360_enable_bc12_detection(int en)
 	return rv;
 }
 
-static void mt6360_update_charge_manager(int port)
+static void mt6360_update_charge_manager(int port,
+				         enum charge_supplier new_bc12_type)
 {
-	static int current_bc12_type = CHARGE_SUPPLIER_NONE;
-	int reg;
-	int new_bc12_type = CHARGE_SUPPLIER_NONE;
-
-	mt6360_read8(MT6360_REG_DPDMIRQ, &reg);
-
-	if (pd_snk_is_vbus_provided(port) && (reg & MT6360_MASK_DPDMIRQ_ATTACH))
-		new_bc12_type = mt6360_get_bc12_device_type();
+	static enum charge_supplier current_bc12_type = CHARGE_SUPPLIER_NONE;
 
 	if (new_bc12_type != current_bc12_type) {
 		charge_manager_update_charge(current_bc12_type, port, NULL);
@@ -134,6 +128,23 @@ static void mt6360_update_charge_manager(int port)
 		}
 
 		current_bc12_type = new_bc12_type;
+	}
+}
+
+static void mt6360_handle_irq(int port)
+{
+	int reg;
+
+	mt6360_read8(MT6360_REG_DPDMIRQ, &reg);
+
+	if (reg & MT6360_MASK_DPDMIRQ_ATTACH) {
+		/* Check vbus again to avoid timing issue */
+		if (pd_snk_is_vbus_provided(port))
+			mt6360_update_charge_manager(
+					port, mt6360_get_bc12_device_type());
+		else
+			mt6360_update_charge_manager(
+					0, CHARGE_SUPPLIER_NONE);
 	}
 
 	/* write clear */
@@ -150,12 +161,17 @@ static void mt6360_usb_charger_task(const int port)
 		uint32_t evt = task_wait_event(-1);
 
 		/* vbus change, start bc12 detection */
-		if (evt & USB_CHG_EVENT_VBUS)
-			mt6360_enable_bc12_detection(1);
+		if (evt & USB_CHG_EVENT_VBUS) {
+			if (pd_snk_is_vbus_provided(port))
+				mt6360_enable_bc12_detection(1);
+			else
+				mt6360_update_charge_manager(
+						0, CHARGE_SUPPLIER_NONE);
+		}
 
 		/* detection done, update charge_manager and stop detection */
 		if (evt & USB_CHG_EVENT_BC12) {
-			mt6360_update_charge_manager(port);
+			mt6360_handle_irq(port);
 			mt6360_enable_bc12_detection(0);
 		}
 	}
