04b9cdfb0a1201fa1565efc37b921af0b0a2c3ff
Jason Guo <jason.guo@quanta.corp-partner.google.com>
2020-07-16 17:49:42 +0800

    [test-only]test led222
    BUG=none
    BRANCH=none
    TEST=none
    
    Signed-off-by: Jason Guo <jason.guo@quanta.corp-partner.google.com># Please enter the commit message for your changes. Lines starting
    Change-Id: I41cb2dc948c5f1f00788273cdb5de99cd63d2288

--- board/puff/led.c
+++ board/puff/led.c
@@ -248,8 +248,8 @@ DECLARE_CONSOLE_COMMAND(led, command_led,
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
 	brightness_range[EC_LED_COLOR_RED] = 100;
-	brightness_range[EC_LED_COLOR_GREEN] = 100;
-	brightness_range[EC_LED_COLOR_AMBER] = 100;
+	brightness_range[EC_LED_COLOR_GREEN] = 70;
+	brightness_range[EC_LED_COLOR_AMBER] = 170;
 }
 
 int led_set_brightness(enum ec_led_id id, const uint8_t *brightness)
