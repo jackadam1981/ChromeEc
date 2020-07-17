6459db2ff4db6a9308e3069014a46abf11e8b01c
Marco Chen <marcochen@google.com>
2020-07-17 14:50:42 +0800

    test
    Change-Id: I3a743666a4ccbcae37ecb6f0d6657122cf9c5a69

--- driver/tcpm/ps8xxx.h
+++ driver/tcpm/ps8xxx.h
@@ -67,11 +67,35 @@
 #define IN_HPD  BIT(0)
 #define HPD_IRQ BIT(1)
 
+#ifndef CONFIG_USB_PD_TCPM_MULTI_PS8XXX
 #if defined(CONFIG_USB_PD_TCPM_PS8751)
 /* Vendor defined registers */
 #define PS8XXX_PRODUCT_ID 0x8751
+#endif
 
-#define FW_VER_REG                              0x90
+#if defined(CONFIG_USB_PD_TCPM_PS8705)
+/* Vendor defined registers */
+/* NOTE: The Product ID will read as 0x8803 if the firmware has malfunctioned */
+#define PS8XXX_PRODUCT_ID 0x8705
+#endif
+
+#if defined(CONFIG_USB_PD_TCPM_PS8805)
+/* Vendor defined registers */
+/* NOTE: The Product ID will read as 0x8803 if the firmware has malfunctioned */
+#define PS8XXX_PRODUCT_ID 0x8805
+#endif
+
+#if defined(CONFIG_USB_PD_TCPM_PS8815)
+/* Vendor defined registers */
+#define PS8XXX_PRODUCT_ID 0x8815
+#endif
+
+#endif /* CONFIG_USB_PD_TCPM_MULTI_PS8XXX */
+
+#if defined(CONFIG_USB_PD_TCPM_PS8751)
+/* Vendor defined registers */
+#define PS8751_PRODUCT_ID 0x8751
+
 #define PS8XXX_REG_VENDOR_ID_L                  0x00
 #define PS8XXX_REG_VENDOR_ID_H                  0x01
 #define PS8XXX_REG_MUX_DP_EQ_CONFIGURATION      0xD3
@@ -80,28 +104,30 @@
 #define PS8XXX_REG_MUX_USB_C2SS_HS_THRESHOLD    0xE8
 #define PS8751_REG_MUX_USB_DCI_CFG              0xED
 
-#elif defined(CONFIG_USB_PD_TCPM_PS8705)
+#endif
+
+#if defined(CONFIG_USB_PD_TCPM_PS8705)
 /* Vendor defined registers */
 /* NOTE: The Product ID will read as 0x8803 if the firmware has malfunctioned */
-#define PS8XXX_PRODUCT_ID 0x8705
+#define PS8705_PRODUCT_ID 0x8705
 
 #define PS8705_P1_REG_MUX_USB_DCI_CFG           0x4B
-/* NOTE: The revision will read as 0x00 if the firmware has malfunctioned. */
-#define FW_VER_REG                              0x82
 
-#elif defined(CONFIG_USB_PD_TCPM_PS8805)
+#endif
+
+#if defined(CONFIG_USB_PD_TCPM_PS8805)
 /* Vendor defined registers */
 /* NOTE: The Product ID will read as 0x8803 if the firmware has malfunctioned */
-#define PS8XXX_PRODUCT_ID 0x8805
+#define PS8805_PRODUCT_ID 0x8805
 
 #define PS8805_P1_REG_MUX_USB_DCI_CFG           0x4B
-#define FW_VER_REG                              0x82
 
-#elif defined(CONFIG_USB_PD_TCPM_PS8815)
+#endif
+
+#if defined(CONFIG_USB_PD_TCPM_PS8815)
 /* Vendor defined registers */
-#define PS8XXX_PRODUCT_ID 0x8815
+#define PS8815_PRODUCT_ID 0x8815
 
-#define FW_VER_REG                              0x82
 #define PS8815_P1_REG_HW_REVISION		0xF0
 
 #endif
