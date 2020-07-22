ed10fc8e16f1593dd02df774dc13cd399f873852
Samsp_Liu <Samsp_Liu@compal.corp-partner.google.com>
2020-07-22 16:51:54 +0800

    Halvor: Update Current Setting
    The charging current value needs to be reduced by half.
    
    BUG=b:158257062
    BRANCH=none
    TEST=halvor test
    
    Change-Id: Iefa736ffbbe9a4d6893682c10cc65e26c9022b0f
    Signed-off-by: Samsp_Liu <Samsp_Liu@compal.corp-partner.google.com>

--- common/charger.c
+++ common/charger.c
@@ -393,7 +393,7 @@ enum ec_error_list charger_set_current(int chgnum, int current)
 		CPRINTS("%s(%d) Invalid charger!", __func__, chgnum);
 		return EC_ERROR_INVAL;
 	}
-
+	current = current >> CONFIG_CHARGE_CHANGE_TIMES;
 	if (chg_chips[chgnum].drv->set_current)
 		rv = chg_chips[chgnum].drv->set_current(chgnum, current);
 
