7f71ac52a31ef4fcd9599e3f49731e84126cef73
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2020-07-30 12:05:50 +0800

    morphius: update fan curve table
    This patch include:
    1. Set different fan curve table by fan id and lid angle.
    2. Add CPU temperature average and trigger debounce
    to avoid fan spin peak.
    
    BUG=b:162325433, b:147312313
    BRANCH=none
    TEST=verify fan follow fan curve
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: I8c5ce458f3184aa45dbcecc5c05a419b0ce33f91

--- board/morphius/board.c
+++ board/morphius/board.c
@@ -372,8 +372,8 @@ const static struct ec_thermal_config thermal_thermistor = {
 	.temp_host_release = {
 		[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
 	},
-	.temp_fan_off = C_TO_K(25),
-	.temp_fan_max = C_TO_K(58),
+	.temp_fan_off = C_TO_K(98),
+	.temp_fan_max = C_TO_K(99),
 };
 
 const static struct ec_thermal_config thermal_cpu = {
@@ -384,8 +384,8 @@ const static struct ec_thermal_config thermal_cpu = {
 	.temp_host_release = {
 		[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
 	},
-	.temp_fan_off = C_TO_K(25),
-	.temp_fan_max = C_TO_K(58),
+	.temp_fan_off = C_TO_K(98),
+	.temp_fan_max = C_TO_K(99),
 };
 
 struct ec_thermal_config thermal_params[TEMP_SENSOR_COUNT];
@@ -397,67 +397,6 @@ static void setup_fans(void)
 	thermal_params[TEMP_SENSOR_CPU] = thermal_cpu;
 }
 DECLARE_HOOK(HOOK_INIT, setup_fans, HOOK_PRIO_DEFAULT);
-
-struct fan_step {
-	int on;
-	int off;
-	int rpm;
-};
-
-static const struct fan_step fan_table0[] = {
-	{.on =  0, .off =  3, .rpm = 0},
-	{.on = 32, .off =  3, .rpm = 3000},
-	{.on = 65, .off = 53, .rpm = 3500},
-	{.on = 74, .off = 62, .rpm = 3800},
-	{.on = 82, .off = 71, .rpm = 4200},
-	{.on = 91, .off = 79, .rpm = 4500},
-	{.on = 100, .off = 88, .rpm = 4900},
-};
-/* All fan tables must have the same number of levels */
-#define NUM_FAN_LEVELS ARRAY_SIZE(fan_table0)
-
-static const struct fan_step *fan_table = fan_table0;
-
-int fan_percent_to_rpm(int fan, int pct)
-{
-	static int current_level;
-	static int previous_pct;
-	int i;
-
-	/*
-	 * Compare the pct and previous pct, we have the three paths :
-	 *  1. decreasing path. (check the off point)
-	 *  2. increasing path. (check the on point)
-	 *  3. invariant path. (return the current RPM)
-	 */
-	if (pct < previous_pct) {
-		for (i = current_level; i >= 0; i--) {
-			if (pct <= fan_table[i].off)
-				current_level = i - 1;
-			else
-				break;
-		}
-	} else if (pct > previous_pct) {
-		for (i = current_level + 1; i < NUM_FAN_LEVELS; i++) {
-			if (pct >= fan_table[i].on)
-				current_level = i;
-			else
-				break;
-		}
-	}
-
-	if (current_level < 0)
-		current_level = 0;
-
-	previous_pct = pct;
-
-	if (fan_table[current_level].rpm !=
-		fan_get_rpm_target(FAN_CH(fan)))
-		cprints(CC_THERMAL, "Setting fan RPM to %d",
-			fan_table[current_level].rpm);
-
-	return fan_table[current_level].rpm;
-}
 
 /* Battery functions */
 #define SB_OPTIONALMFG_FUNCTION2        0x26
