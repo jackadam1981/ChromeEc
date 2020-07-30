347f89e6f9dc8683d50f3241e9e41e35620c382a
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2020-07-30 11:02:18 +0800

    dirinboz: update DB setting
    This CL remove unuse DB code for dirinboz.
    
    BUG=b:159598016
    BRANCH=none
    TEST=verify DB can work.
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: Ic6b62a2f2d5428b9b88dc4bd06e099f10e083e03

--- board/dirinboz/board.c
+++ board/dirinboz/board.c
@@ -9,13 +9,11 @@
 #include "driver/accel_lis2dw12.h"
 #include "driver/accelgyro_lsm6dsm.h"
 #include "driver/bc12/pi3usb9201.h"
-#include "driver/ioexpander/pcal6408.h"
 #include "driver/ppc/aoz1380.h"
 #include "driver/ppc/nx20p348x.h"
 #include "driver/tcpm/nct38xx.h"
 #include "driver/usb_mux/amd_fp5.h"
 #include "driver/usb_mux/ps8740.h"
-#include "driver/usb_mux/ps8743.h"
 #include "extpower.h"
 #include "fan.h"
 #include "fan_chip.h"
@@ -55,21 +53,6 @@ void (*c1_ppc_config_interrupt)(enum gpio_signal signal) = ppc_interrupt;
 void c1_ppc_interrupt(enum gpio_signal signal)
 {
 	c1_ppc_config_interrupt(signal);
-}
-
-static void hdmi_hpd_handler(void)
-{
-	/* Pass HPD through from DB OPT1 HDMI connector to AP's DP1. */
-	int hpd = gpio_get_level(GPIO_USB_C1_PPC_INT_ODL);
-	gpio_set_level(GPIO_DP1_HPD, hpd);
-	ccprints("HDMI HPD %d", hpd);
-}
-DECLARE_DEFERRED(hdmi_hpd_handler);
-
-void hdmi_hpd_interrupt(enum gpio_signal signal)
-{
-	/* Debounce for 2 msec. */
-	hook_call_deferred(&hdmi_hpd_handler_data, (2 * MSEC));
 }
 
 #include "gpio_list.h"
@@ -181,41 +164,16 @@ unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 enum gpio_signal IOEX_USB_A1_RETIMER_EN = IOEX_USB_A1_RETIMER_EN_OPT1;
 enum gpio_signal IOEX_USB_A1_CHARGE_EN_DB_L = IOEX_USB_A1_CHARGE_EN_DB_L_OPT1;
 
-static void pcal6408_handler(void)
-{
-	pcal6408_ioex_event_handler(IOEX_HDMI_PCAL6408);
-}
-DECLARE_DEFERRED(pcal6408_handler);
-
-void pcal6408_interrupt(enum gpio_signal signal)
-{
-	hook_call_deferred(&pcal6408_handler_data, 0);
-}
-
-static int board_ps8743_mux_set(const struct usb_mux *me,
-				mux_state_t mux_state)
-{
-	if (mux_state & USB_PD_MUX_DP_ENABLED)
-		/* Enable IN_HPD on the DB */
-		ioex_set_level(IOEX_USB_C1_HPD_IN_DB, 1);
-	else
-		/* Disable IN_HPD on the DB */
-		ioex_set_level(IOEX_USB_C1_HPD_IN_DB, 0);
-
-	return EC_SUCCESS;
-}
-
-
 /*****************************************************************************
  * USB-C
  */
 
 /*
- * USB C0 port SBU mux use standalone FSUSB42UMX
+ * USB C0 port SBU mux use standalone PI3USB221
  * chip and it need a board specific driver.
  * Overall, it will use chained mux framework.
  */
-static int fsusb42umx_set_mux(const struct usb_mux *me, mux_state_t mux_state)
+static int pi3usb221_set_mux(const struct usb_mux *me, mux_state_t mux_state)
 {
 	if (mux_state & USB_PD_MUX_POLARITY_INVERTED)
 		ioex_set_level(IOEX_USB_C0_SBU_FLIP, 1);
@@ -232,11 +190,11 @@ static int fsusb42umx_set_mux(const struct usb_mux *me, mux_state_t mux_state)
  * properly.
  */
 const struct usb_mux_driver usbc0_sbu_mux_driver = {
-	.set = fsusb42umx_set_mux,
+	.set = pi3usb221_set_mux,
 };
 
 /*
- * Since FSUSB42UMX is not a i2c device, .i2c_port and
+ * Since PI3USB221 is not a i2c device, .i2c_port and
  * .i2c_addr_flags are not required here.
  */
 const struct usb_mux usbc0_sbu_mux = {
@@ -263,8 +221,8 @@ struct usb_mux usb_muxes[] = {
 	[USBC_PORT_C1] = {
 		.usb_port = USBC_PORT_C1,
 		.i2c_port = I2C_PORT_TCPC1,
-		.i2c_addr_flags = PS8743_I2C_ADDR1_FLAG,
-		.driver = &ps8743_usb_mux_driver,
+		.i2c_addr_flags = PS8740_I2C_ADDR0_FLAG,
+		.driver = &ps8740_usb_mux_driver,
 		.next_mux = &usbc1_amd_fp5_usb_mux,
 	}
 };
@@ -508,40 +466,14 @@ int board_pd_set_frs_enable(int port, int enable)
 
 static void setup_fw_config(void)
 {
-	uint32_t board_version = 0;
 
-	if (cbi_get_board_version(&board_version) == EC_SUCCESS
-	    && board_version >= 2) {
-		ccprints("PS8743 USB MUX");
-		usb_muxes[USBC_PORT_C1].i2c_addr_flags = PS8743_I2C_ADDR1_FLAG;
-		usb_muxes[USBC_PORT_C1].driver = &ps8743_usb_mux_driver;
-		usb_muxes[USBC_PORT_C1].board_set = &board_ps8743_mux_set;
-	} else {
-		ccprints("PS8740 USB MUX");
-		usb_muxes[USBC_PORT_C1].i2c_addr_flags = PS8740_I2C_ADDR0_FLAG;
-		usb_muxes[USBC_PORT_C1].driver = &ps8740_usb_mux_driver;
-		usb_muxes[USBC_PORT_C1].board_set = NULL;
-	}
-
-	if (ec_config_get_usb_db() == DALBOZ_DB_D_OPT2_USBA_HDMI) {
-		ccprints("DB OPT2 HDMI");
-		ioex_config[IOEX_HDMI_PCAL6408].flags = 0;
-		ioex_init(IOEX_HDMI_PCAL6408);
-		IOEX_USB_A1_RETIMER_EN = IOEX_USB_A1_RETIMER_EN_OPT2;
-		IOEX_USB_A1_CHARGE_EN_DB_L = IOEX_USB_A1_CHARGE_EN_DB_L_OPT2;
-		usb_port_enable[USBA_PORT_A1] = IOEX_EN_USB_A1_5V_DB_OPT2;
-		c1_tcpc_config_interrupt = pcal6408_interrupt;
-		c1_ppc_config_interrupt = hdmi_hpd_interrupt;
-	} else {
-		ccprints("DB OPT1 USBC");
-		ioex_config[IOEX_C1_NCT3807].flags = 0;
-		ioex_init(IOEX_C1_NCT3807);
-		IOEX_USB_A1_RETIMER_EN = IOEX_USB_A1_RETIMER_EN_OPT1;
-		IOEX_USB_A1_CHARGE_EN_DB_L = IOEX_USB_A1_CHARGE_EN_DB_L_OPT1;
-		usb_port_enable[USBA_PORT_A1] = IOEX_EN_USB_A1_5V_DB_OPT1;
-		c1_tcpc_config_interrupt = tcpc_alert_event;
-		c1_ppc_config_interrupt = ppc_interrupt;
-	}
+	ioex_config[IOEX_C1_NCT3807].flags = 0;
+	ioex_init(IOEX_C1_NCT3807);
+	IOEX_USB_A1_RETIMER_EN = IOEX_USB_A1_RETIMER_EN_OPT1;
+	IOEX_USB_A1_CHARGE_EN_DB_L = IOEX_USB_A1_CHARGE_EN_DB_L_OPT1;
+	usb_port_enable[USBA_PORT_A1] = IOEX_EN_USB_A1_5V_DB_OPT1;
+	c1_tcpc_config_interrupt = tcpc_alert_event;
+	c1_ppc_config_interrupt = ppc_interrupt;
 
 	/* Enable PPC interrupts. */
 	gpio_enable_interrupt(GPIO_USB_C0_PPC_FAULT_ODL);
@@ -591,12 +523,6 @@ struct ioexpander_config_t ioex_config[] = {
 		.i2c_host_port = I2C_PORT_TCPC1,
 		.i2c_slave_addr = NCT38XX_I2C_ADDR1_1_FLAGS,
 		.drv = &nct38xx_ioexpander_drv,
-		.flags = IOEX_FLAGS_DISABLED,
-	},
-	[IOEX_HDMI_PCAL6408] = {
-		.i2c_host_port = I2C_PORT_TCPC1,
-		.i2c_slave_addr = PCAL6408_I2C_ADDR0,
-		.drv = &pcal6408_ioexpander_drv,
 		.flags = IOEX_FLAGS_DISABLED,
 	},
 };
