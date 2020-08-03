42bf2bae264a67305b6eb326e032b782d46961e0
Eric Herrmann <eherrmann@chromium.org>
2020-08-03 15:46:58 -0700

    Volteer: Add support for RT1715
    Add support for replacing the TUSB422 with the RT1715. Since they are
    pin-to-pin compatible and currently only used with a rework, make the
    decision of which to use at runtime based on which I2C addresses are
    available. The logic is to check if the TUSB422 is present, then if the
    RT1715 is present. If neither is present, default to the TUSB422 driver.
    
    BUG=b:162617664
    TEST=make buildall
    TEST=check both TUSB422 functionality and RT1715 functionality
    BRANCH=none
    
    Change-Id: I8306f086bf030ddd7238532b1f12aa259cb72422
    Signed-off-by: Eric Herrmann <eherrmann@chromium.org>

--- board/volteer/board.c
+++ board/volteer/board.c
@@ -18,6 +18,8 @@
 #include "driver/retimer/bb_retimer.h"
 #include "driver/sync.h"
 #include "driver/tcpm/ps8xxx.h"
+#include "driver/tcpm/rt1715.h"
+#include "driver/tcpm/tusb422.h"
 #include "driver/tcpm/tcpci.h"
 #include "driver/tcpm/tusb422.h"
 #include "extpower.h"
@@ -374,6 +376,33 @@ static void config_db_usb3_passive(void)
 	usb_muxes[USBC_PORT_C1] = mux_config_p1_usb3_passive;
 }
 
+static void config_port_discrete_tcpc(int port)
+{
+	/*
+	 * 2 Pin-2-Pin compatible parts: TUSB422 and RT1715, for simplicity
+	 * allow either and decide at runtime which we are using. This checks
+	 * the I2C address, a more complete solution would also read the VID
+	 */
+
+	uint8_t tmp;
+	uint8_t i2c_port;
+
+	i2c_port = port ? I2C_PORT_USB_C1 : I2C_PORT_USB_C0;
+	i2c_lock(i2c_port, 1);
+
+	/* Ping TUSB422 and RT1715 addresses */
+	if (!i2c_xfer_unlocked(i2c_port, TUSB422_I2C_ADDR_FLAGS, NULL, 0, &tmp,
+			       1, I2C_XFER_SINGLE)) {
+		CPRINTS("C%d: TUSB422 detected", port);
+	} else if (!i2c_xfer_unlocked(i2c_port, RT1715_I2C_ADDR_FLAGS, NULL, 0,
+				      &tmp, 1, I2C_XFER_SINGLE)) {
+		CPRINTS("C%d: RT1715 detected", port);
+		tcpc_config[port].i2c_info.addr_flags = RT1715_I2C_ADDR_FLAGS;
+		tcpc_config[port].drv = &rt1715_tcpm_drv;
+	}
+	i2c_lock(i2c_port, 0);
+}
+
 static const char *db_type_prefix = "USB DB type: ";
 __override void board_cbi_init(void)
 {
@@ -389,15 +418,17 @@ __override void board_cbi_init(void)
 			GPIO_USB_C1_RT_RST_ODL_BOARDID_0;
 		ps8xxx_rst_odl = GPIO_USB_C1_RT_RST_ODL_BOARDID_0;
 	}
-
+	config_port_discrete_tcpc(0);
 	switch (usb_db) {
 	case DB_USB_ABSENT:
 		CPRINTS("%sNone", db_type_prefix);
 		break;
 	case DB_USB4_GEN2:
+		config_port_discrete_tcpc(1);
 		CPRINTS("%sUSB4 Gen1/2", db_type_prefix);
 		break;
 	case DB_USB4_GEN3:
+		config_port_discrete_tcpc(1);
 		CPRINTS("%sUSB4 Gen3", db_type_prefix);
 		break;
 	case DB_USB3_ACTIVE:
