41620ee6ab457ca46c7f74287f3e3a31bd739896
Eric Yilun Lin <yllin@chromium.org>
2020-08-04 16:22:20 +0800

    mt6360: rename LDO for regulator
    This CL simply renames LDO for regulator since that we also have buck.
    
    Also, fix the regulator name in printing.
    
    BUG=b:147789962
    TEST=make buildall
    BRANCH=none
    
    Change-Id: I6d901ac3617479af56cbc3135b7dc8274cfed46c
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- driver/bc12/mt6360.h
+++ driver/bc12/mt6360.h
@@ -52,26 +52,33 @@
 #define MT6360_MASK_LDO_SW_OP_EN BIT(7)
 #define MT6360_MASK_LDO_SW_EN BIT(6)
 
+/* This is same for LDO{1,2,3,5,6,7}_EN_CTRL2, BUCK{1,2}_EN_CTRL2 */
+#define MT6360_MASK_RGL_SW_OP_EN BIT(7)
+#define MT6360_MASK_RGL_SW_EN BIT(6)
+
 #define MT6360_LDO_VOCAL_STEP_MV 10
 #define MT6360_LDO_VOCAL_MAX_STEP 10
 
-enum mt6360_ldo_id {
+enum mt6360_regulator_id {
 	MT6360_LDO3,
 	MT6360_LDO5,
 
-	MT6360_LDO_COUNT,
+	MT6360_REGULATOR_COUNT,
 };
 
-int mt6360_ldo_get_info(enum mt6360_ldo_id ldo_id, char *name,
-			uint16_t *voltage_count, uint16_t *voltages_mv);
+int mt6360_regulator_get_info(enum mt6360_regulator_id rgl_id, char *name,
+			      uint16_t *voltage_count, uint16_t *voltages_mv);
 
-int mt6360_ldo_enable(enum mt6360_ldo_id ldo_id, uint8_t enable);
+int mt6360_regulator_enable(enum mt6360_regulator_id rgl_id, uint8_t enable);
 
-int mt6360_ldo_is_enabled(enum mt6360_ldo_id ldo_id, uint8_t *enabled);
+int mt6360_regulator_is_enabled(enum mt6360_regulator_id rgl_id,
+				uint8_t *enabled);
 
-int mt6360_ldo_set_voltage(enum mt6360_ldo_id ldo_id, int min_mv, int max_mv);
+int mt6360_regulator_set_voltage(enum mt6360_regulator_id rgl_id, int min_mv,
+				 int max_mv);
 
-int mt6360_ldo_get_voltage(enum mt6360_ldo_id ldo_id, int *voltage_mv);
+int mt6360_regulator_get_voltage(enum mt6360_regulator_id rgl_id,
+				 int *voltage_mv);
 
 enum mt6360_led_id {
 	MT6360_LED_RGB1,
