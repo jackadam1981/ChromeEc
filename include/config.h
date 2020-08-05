421875fa78d73a9696b71a9d65fb0e5dd4cb467e
Craig Hesling <hesling@chromium.org>
2020-08-05 01:20:51 -0700

    host_command: Add common name printing
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I4bb332e5d8abddfea6189d410d3a3f0e7e6efc5c

--- include/config.h
+++ include/config.h
@@ -2015,6 +2015,13 @@
 /* Config option to support 64-bit hostevents and wake-masks. */
 #define CONFIG_HOST_EVENT64
 
+/* 
+ * Print host command pretty names in the console.
+ * This option can consume much more flash space in order to hold
+ * the host command's common name.
+ */
+#undef CONFIG_HOST_COMMAND_NAMES
+
 /*
  * The host commands are sorted in the .rodata.hcmds section so use the binary
  * search algorithm to match a command to its handler
