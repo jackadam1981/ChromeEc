5a79a329487a9da4eb6202bfd99780d41de00079
Craig Hesling <hesling@chromium.org>
2020-08-05 00:03:53 +0000

    ectool: Fix "rwsig dump key_id" hex output
    The ectool command rwsig dump key_id's output was missing zeros in the
    hex string output. This is because we print each byte independently
    using the naked %x formatter, which will print the minimum hex
    characters necessary to represent the independent number provided.
    
    We need to force exactly 2 hex characters for each independent byte
    printed.
    
    BRANCH=none
    BUG=b:162588911
    TEST=# Nocturne
         make BOARD=nocturne_fp build/nocturne_fp/util/ectool
         # Copy build/nocturne_fp/util/ectool to fat32 flash drive
         mkdir /tmp/rem
         mount /dev/sda1 /tmp/rem
         ectool --name=cros_fp rwsig dump key_id | tr -d '\n' | wc -c
         # Identify there are less than 40 chars (39 in my case)
         /tmp/rem/ectool --name=cros_fp rwsig dump key_id | tr -d '\n' | wc -c
         # Identify there are exactly 40 chars
         # Rerun both commands without the byte counting to sanity
         # check the output
    
    Change-Id: I0a43833adb647aa034df87678c9657466cc8aef5

--- util/ectool.c
+++ util/ectool.c
@@ -1,4 +1,4 @@
-/* Copyright 2013 The Chromium OS Authors. All rights reserved.
+"/* Copyright 2013 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -1618,7 +1618,7 @@ static int rwsig_info(enum rwsig_info_fields fields)
 			printf("key_id: ");
 
 		for (i = 0; i < sizeof(r.key_id); i++)
-			printf("%x", r.key_id[i]);
+			printf("%02x", r.key_id[i]);
 		printf("\n");
 	}
 
