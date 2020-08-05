34546319055c6cb530ca19ce8bf648180dad012a
Vadim Sukhomlinov <sukhomlinov@google.com>
2020-08-04 23:03:33 -0700

    fips: move FIPS storage of U2F keys into AP RO Hash page
    In order to remove most of NVMEM out of scope for FIPS according
    to http://go/fips-storage-for-u2f, rewrite U2F key management to
    flash page shared with AP RO verification.
    
    Adjust loading of U2F state accordingly, move dependency on
    NVMEM functions to load existing non-FIPS state into callbacks.
    
    U2F crypto functions moved from cr50/u2f.c into fips.c to get access
    to internal state management.
    
    Removed unused functions related to FIPS policy from board.c and wp.c
    due to overall change in FIPS policy enforcment.
    
    U2F crypto modified gen as in https://crrev.com/c/2280485 to use
    approved methods for key generation.
    
    BUG=b:138577416
    TEST=make CR50_DEV=1 BOARD=cr50
    9 times repeat:
    fips load
    fips zero
    to check that new partition is created each time, previous key is
    zeroized and that AP RO page is erased on 9th call.
    
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Change-Id: I22a02850ed533fb7fd19dd281e68c420e0007907

--- board/cr50/u2f.c
+++ board/cr50/u2f.c
@@ -8,6 +8,8 @@
 #include "console.h"
 #include "dcrypto.h"
 #include "extension.h"
+#include "fips_rand.h"
+#include "new_nvmem.h"
 #include "nvmem_vars.h"
 #include "rbox.h"
 #include "registers.h"
@@ -56,38 +58,72 @@ enum touch_state pop_check_presence(int consume)
 
 /* ---- non-volatile U2F state ---- */
 
-struct u2f_state {
-	uint32_t salt[8];
-	uint32_t salt_kek[8];
-	uint32_t salt_kh[8];
-};
-
 static const uint8_t k_salt = NVMEM_VAR_G2F_SALT;
 static const uint8_t k_salt_deprecated = NVMEM_VAR_U2F_SALT;
 
-static int load_state(struct u2f_state *state)
+/* Can't include TPM2 headers, so just define constant locally. */
+#define HR_NV_INDEX (1U << 24)
+
+/* Wipe old U2F keys. */
+bool u2f_zeroize_old(void)
 {
+	const uint32_t u2fobjs[] = { TPM_HIDDEN_U2F_KEK | HR_NV_INDEX,
+				     TPM_HIDDEN_U2F_KH_SALT | HR_NV_INDEX, 0 };
+
+	/* Delete NVMEM_VAR_G2F_SALT. */
+	setvar(&k_salt, sizeof(k_salt), NULL, 0);
+	/* Remove U2F keys and wipe all deleted objects. */
+	nvmem_erase_tpm_data_selective(u2fobjs);
+	return true;
+}
+
+bool u2f_load_old_state(bool create, uint32_t **p_salt, uint32_t **p_salt_kek,
+			uint32_t **p_salt_kh)
+{
+	/* legacy u2f state */
+	static uint32_t salt[8];
+	static uint32_t salt_kek[8];
+	static uint32_t salt_kh[8];
+
 	const struct tuple *t_salt = getvar(&k_salt, sizeof(k_salt));
 
 	if (!t_salt) {
 		/* Delete the old salt if present, no-op if not. */
-		if (setvar(&k_salt_deprecated, sizeof(k_salt_deprecated),
-			   NULL, 0))
-			return 0;
+		setvar(&k_salt_deprecated, sizeof(k_salt_deprecated), NULL, 0);
 
-		/* create random salt */
-		if (!DCRYPTO_ladder_random(state->salt))
-			return 0;
-		if (setvar(&k_salt, sizeof(k_salt),
-			   (const uint8_t *)state->salt, sizeof(state->salt)))
-			return 0;
+		/* If we just check presence of old keys, stop here. */
+		if (!create)
+			return false;
+
+		/* Otherwise create U2F keys in old style. */
+		if (!fips_rand_bytes(salt, sizeof(salt)))
+			return false;
+		if (setvar(&k_salt, sizeof(k_salt), (const uint8_t *)salt,
+			   sizeof(salt)))
+			return false;
 	} else {
-		memcpy(state->salt, tuple_val(t_salt), sizeof(state->salt));
+		memcpy(salt, tuple_val(t_salt), sizeof(salt));
 		freevar(t_salt);
 	}
 
-	if (read_tpm_nvmem_hidden(TPM_HIDDEN_U2F_KEK, sizeof(state->salt_kek),
-				  state->salt_kek) == TPM_READ_NOT_FOUND) {
+	*p_salt = salt;
+
+	/**
+	 * If creation is not needed, don't spend time further, as keys will
+	 * be loaded / created on actual use.
+	 */
+	if (!create) {
+		*p_salt_kek = NULL;
+		*p_salt_kh = NULL;
+		return true;
+	}
+
+	/**
+	 * Since k_salt was present or created, we are in non-FIPS, load or
+	 * create other keys using old method.
+	 */
+	if (read_tpm_nvmem_hidden(TPM_HIDDEN_U2F_KEK, sizeof(salt_kek),
+				  salt_kek) == TPM_READ_NOT_FOUND) {
 		/*
 		 * Not found means that we have not used u2f before,
 		 * or not used it with updated fw that resets kek seed
@@ -101,211 +137,34 @@ static int load_state(struct u2f_state *state)
 			 * seed as a one-off. It will be changed on
 			 * next TPM clear.
 			 */
-			memcpy(state->salt_kek, state->salt,
-			       sizeof(state->salt_kek));
+			memcpy(salt_kek, salt, sizeof(salt_kek));
 		} else {
 			/*
 			 * We have never used u2f before - generate
 			 * new seed.
 			 */
-			if (!DCRYPTO_ladder_random(state->salt_kek))
-				return 0;
+			if (!fips_rand_bytes(salt_kek, sizeof(salt_kek)))
+				return false;
 		}
-		if (write_tpm_nvmem_hidden(TPM_HIDDEN_U2F_KEK,
-					   sizeof(state->salt_kek),
-					   state->salt_kek,
+		if (write_tpm_nvmem_hidden(TPM_HIDDEN_U2F_KEK, sizeof(salt_kek),
+					   salt_kek,
 					   1 /* commit */) != TPM_WRITE_CREATED)
-			return 0;
+			return false;
 	}
+	*p_salt_kek = salt_kek;
 
-	if (read_tpm_nvmem_hidden(TPM_HIDDEN_U2F_KH_SALT,
-				  sizeof(state->salt_kh),
-				  state->salt_kh) == TPM_READ_NOT_FOUND) {
+	if (read_tpm_nvmem_hidden(TPM_HIDDEN_U2F_KH_SALT, sizeof(salt_kh),
+				  salt_kh) == TPM_READ_NOT_FOUND) {
 		/*
-		 * We have never used u2f before - generate
-		 * new seed.
+		 * We have never used U2F before - generate
+		 * new entropy for DRBG.
 		 */
-		if (!DCRYPTO_ladder_random(state->salt_kh))
-			return 0;
-
+		fips_trng_bytes(salt_kh, sizeof(salt_kh));
 		if (write_tpm_nvmem_hidden(TPM_HIDDEN_U2F_KH_SALT,
-					   sizeof(state->salt_kh),
-					   state->salt_kh,
+					   sizeof(salt_kh), salt_kh,
 					   1 /* commit */) != TPM_WRITE_CREATED)
-			return 0;
+			return false;
 	}
-
-	return 1;
-}
-
-static struct u2f_state *get_state(void)
-{
-	static int state_loaded;
-	static struct u2f_state state;
-
-	if (!state_loaded)
-		state_loaded = load_state(&state);
-
-	return state_loaded ? &state : NULL;
-}
-
-/* ---- chip-specific U2F crypto ---- */
-
-static int _derive_key(enum dcrypto_appid appid, const uint32_t input[8],
-		       uint32_t output[8])
-{
-	struct APPKEY_CTX ctx;
-	int result;
-
-	/* Setup USR-based application key. */
-	if (!DCRYPTO_appkey_init(appid, &ctx))
-		return 0;
-	result = DCRYPTO_appkey_derive(appid, input, output);
-
-	DCRYPTO_appkey_finish(&ctx);
-	return result;
-}
-
-int u2f_origin_key(const uint8_t *seed, p256_int *d)
-{
-	uint32_t tmp[P256_NDIGITS];
-
-	memcpy(tmp, seed, sizeof(tmp));
-	if (!_derive_key(U2F_ORIGIN, tmp, tmp))
-		return EC_ERROR_UNKNOWN;
-	return DCRYPTO_p256_key_from_bytes(NULL, NULL, d,
-					   (const uint8_t *)tmp) == 0;
-}
-
-int u2f_origin_user_keyhandle(const uint8_t *origin, const uint8_t *user,
-			      const uint8_t *origin_seed,
-			      struct u2f_key_handle *key_handle)
-{
-	LITE_HMAC_CTX ctx;
-	struct u2f_state *state = get_state();
-
-	if (!state)
-		return EC_ERROR_UNKNOWN;
-
-	memcpy(key_handle->origin_seed, origin_seed, P256_NBYTES);
-
-	DCRYPTO_HMAC_SHA256_init(&ctx, state->salt_kek, SHA256_DIGEST_SIZE);
-	HASH_update(&ctx.hash, origin, P256_NBYTES);
-	HASH_update(&ctx.hash, user, P256_NBYTES);
-	HASH_update(&ctx.hash, origin_seed, P256_NBYTES);
-
-	memcpy(key_handle->hmac, DCRYPTO_HMAC_final(&ctx), SHA256_DIGEST_SIZE);
-
-	return EC_SUCCESS;
-}
-
-int u2f_origin_user_versioned_keyhandle(
-	const uint8_t *origin, const uint8_t *user, const uint8_t *origin_seed,
-	uint8_t version, struct u2f_versioned_key_handle *key_handle)
-{
-	LITE_HMAC_CTX ctx;
-	struct u2f_state *state = get_state();
-
-	if (!state)
-		return EC_ERROR_UNKNOWN;
-
-	key_handle->version = version;
-	memcpy(key_handle->origin_seed, origin_seed, P256_NBYTES);
-
-	DCRYPTO_HMAC_SHA256_init(&ctx, state->salt_kek, SHA256_DIGEST_SIZE);
-	HASH_update(&ctx.hash, origin, P256_NBYTES);
-	HASH_update(&ctx.hash, user, P256_NBYTES);
-	HASH_update(&ctx.hash, origin_seed, P256_NBYTES);
-	HASH_update(&ctx.hash, &version, sizeof(key_handle->version));
-
-	memcpy(key_handle->hmac, DCRYPTO_HMAC_final(&ctx), SHA256_DIGEST_SIZE);
-
-	return EC_SUCCESS;
-}
-
-int u2f_origin_user_keypair(const uint8_t *key_handle, size_t key_handle_size,
-			    p256_int *d, p256_int *pk_x, p256_int *pk_y)
-{
-	uint32_t dev_salt[P256_NDIGITS];
-	uint8_t key_seed[P256_NBYTES];
-
-	struct drbg_ctx drbg;
-	struct u2f_state *state = get_state();
-
-	if (!state)
-		return EC_ERROR_UNKNOWN;
-
-	if (!_derive_key(U2F_ORIGIN, state->salt_kek, dev_salt))
-		return EC_ERROR_UNKNOWN;
-
-	hmac_drbg_init(&drbg, state->salt_kh, P256_NBYTES, dev_salt,
-		       P256_NBYTES, NULL, 0);
-
-	hmac_drbg_generate(&drbg, key_seed, sizeof(key_seed), key_handle,
-			   key_handle_size);
-
-	if (!DCRYPTO_p256_key_from_bytes(pk_x, pk_y, d, key_seed))
-		return EC_ERROR_TRY_AGAIN;
-
-	return EC_SUCCESS;
-}
-
-int u2f_gen_kek(const uint8_t *origin, uint8_t *kek, size_t key_len)
-{
-	uint32_t buf[P256_NDIGITS];
-
-	struct u2f_state *state = get_state();
-
-	if (!state)
-		return EC_ERROR_UNKNOWN;
-
-	if (key_len != sizeof(buf))
-		return EC_ERROR_UNKNOWN;
-	if (!_derive_key(U2F_WRAP, state->salt_kek, buf))
-		return EC_ERROR_UNKNOWN;
-	memcpy(kek, buf, key_len);
-
-	return EC_SUCCESS;
-}
-
-int g2f_individual_keypair(p256_int *d, p256_int *pk_x, p256_int *pk_y)
-{
-	uint8_t buf[SHA256_DIGEST_SIZE];
-
-	struct u2f_state *state = get_state();
-
-	if (!state)
-		return EC_ERROR_UNKNOWN;
-
-	/* Incorporate HIK & diversification constant */
-	if (!_derive_key(U2F_ATTEST, state->salt, (uint32_t *)buf))
-		return EC_ERROR_UNKNOWN;
-
-	/* Generate unbiased private key */
-	while (!DCRYPTO_p256_key_from_bytes(pk_x, pk_y, d, buf)) {
-		HASH_CTX sha;
-
-		DCRYPTO_SHA256_init(&sha, 0);
-		HASH_update(&sha, buf, sizeof(buf));
-		memcpy(buf, HASH_final(&sha), sizeof(buf));
-	}
-
-	return EC_SUCCESS;
-}
-
-int u2f_gen_kek_seed(int commit)
-{
-	struct u2f_state *state = get_state();
-
-	if (!state)
-		return EC_ERROR_UNKNOWN;
-
-	if (!DCRYPTO_ladder_random(state->salt_kek))
-		return EC_ERROR_HW_INTERNAL;
-
-	if (write_tpm_nvmem_hidden(TPM_HIDDEN_U2F_KEK, sizeof(state->salt_kek),
-				   state->salt_kek, commit) == TPM_WRITE_FAIL)
-		return EC_ERROR_UNKNOWN;
-
-	return EC_SUCCESS;
+	*p_salt_kh = salt_kh;
+	return true;
 }
