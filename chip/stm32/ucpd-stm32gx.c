1cc2f94b5d37c56ca54dc521261a6550192d67f9
Scott Collyer <scollyer@google.com>
2020-08-05 14:10:24 -0700

    stm32g4: Guard ucpd console command
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Iceac462aedfb8f7d381d8d5f1cd7a10da77aaf27

--- chip/stm32/ucpd-stm32gx.c
+++ chip/stm32/ucpd-stm32gx.c
@@ -945,6 +945,7 @@ void stm32gx_ucpd1_irq(void)
 DECLARE_IRQ(STM32_IRQ_UCPD1, stm32gx_ucpd1_irq, 1);
 
 
+#ifdef STM32G4_UCPD_CMD
 static char ctrl_names[][10] = {
 	"rsvd",
 	"GoodCRC",
@@ -1180,3 +1181,4 @@ static int command_ucpd(int argc, char **argv)
 DECLARE_CONSOLE_COMMAND(ucpd, command_ucpd,
 			"[rst|src|bist|amber",
 			"Turn on/off LED.");
+#endif
