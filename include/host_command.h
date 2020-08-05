17683be3fcd3f8a9f1cbc2f14abe405339d87f27
Craig Hesling <hesling@chromium.org>
2020-08-05 01:20:51 -0700

    host_command: Add common name printing
    BRANCH=none
    BUG=none
    TEST=none
    
    Change-Id: I4bb332e5d8abddfea6189d410d3a3f0e7e6efc5c
    Signed-off-by: Craig Hesling <hesling@chromium.org>

--- include/host_command.h
+++ include/host_command.h
@@ -124,9 +124,19 @@ struct host_command {
 	enum ec_status (*handler)(struct host_cmd_handler_args *args);
 	/* Command code */
 	int command;
+#ifdef CONFIG_HOST_COMMAND_NAMES
+	/* Command name for console printing */
+	const char *command_name;
+#endif
 	/* Mask of supported versions */
 	int version_mask;
 };
+
+#ifdef CONFIG_HOST_COMMAND_NAMES
+#define HOST_COMMAND_FIELD_NAME(name) .command_name = name,
+#else
+#define HOST_COMMAND_FIELD_NAME(name)
+#endif
 
 #ifdef CONFIG_HOST_EVENT64
 typedef uint64_t host_event_t;
@@ -273,7 +283,9 @@ struct host_command *zephyr_find_host_command(int command);
 	const struct host_command __keep __no_sanitize_address		\
 	EXPAND(0x0000, command)						\
 	__attribute__((section(".rodata.hcmds."EXPANDSTR(0x0000, command)))) \
-		= {routine, command, version_mask}
+		= {routine, command,                                    \
+		   HOST_COMMAND_FIELD_NAME(#command)                    \
+		   version_mask}
 
 /*
  * Register a private host command handler with
@@ -285,6 +297,7 @@ struct host_command *zephyr_find_host_command(int command);
 	__attribute__((section(".rodata.hcmds."\
 	EXPANDSTR(EC_CMD_BOARD_SPECIFIC_BASE, command)))) \
 		= {routine, EC_PRIVATE_HOST_COMMAND_VALUE(command), \
+		   HOST_COMMAND_FIELD_NAME(#command) \
 		   version_mask}
 #else /* !CONFIG_ZEPHYR && !HAS_TASK_HOSTCMD */
 #define DECLARE_HOST_COMMAND(command, routine, version_mask)    \
