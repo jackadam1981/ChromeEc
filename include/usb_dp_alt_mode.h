60bf990cf6b67f3f90dc8003a702bcbc90f2c9e2
Nathan K <nkolluru@google.com>
2020-08-07 00:53:00 -0700

    HACK/WIP: Add DP Active Cable support
    Change-Id: I4b837295dda31c23f2d5fa3c9af25aea4d4c8869

--- include/usb_dp_alt_mode.h
+++ include/usb_dp_alt_mode.h
@@ -70,6 +70,7 @@ void dp_teardown(int port);
  *                  VDO_MAX_SIZE elements
  * @return          The number of VDOs written to VDM or -1 to indicate error
  */
-int dp_setup_next_vdm(int port, int vdo_count, uint32_t *vdm);
+int dp_setup_next_vdm(int port, int vdo_count, uint32_t *vdm,
+	enum tcpm_transmit_type *tx_type);
 
 #endif  /* __CROS_EC_USB_DP_ALT_MODE_H */
