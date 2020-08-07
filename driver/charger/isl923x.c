524d35d3bd4deec435966dbe315c13d3a0a7b701
Ting Shen <phoenixshen@google.com>
2020-08-07 18:56:48 +0800

    WIP: asurada: implement ULP mode
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: Idbee7914fc261d74bdc70c2858befdfbd83c40e7

--- driver/charger/isl923x.c
+++ driver/charger/isl923x.c
@@ -520,6 +520,15 @@ static void isl923x_init(int chgnum)
 			goto init_fail;
 	}
 
+	if (IS_ENABLED(CONFIG_CHAR_ISL9238C)) {
+		/*
+		 * Reset CONTROL1 as it may have been changed when entering
+		 * hibernate.
+		 */
+		if (raw_write16(chgnum, ISL923X_REG_CONTROL1, 0))
+			goto init_fail;
+	}
+
 	if (IS_ENABLED(CHARGER_ISL9238X) ||
 	    IS_ENABLED(CONFIG_CHARGER_RAA489000)) {
 		/*
@@ -678,6 +687,28 @@ void raa489000_hibernate(int chgnum)
 	cflush();
 }
 #endif /* CONFIG_CHARGER_RAA489000 */
+
+#ifdef CONFIG_CHARGER_ISL9238C
+void isl9238c_hibernate(int chgnum)
+{
+	int reg;
+
+	if (raw_read16(chgnum, ISL923X_REG_CONTROL1, &reg)) {
+		CPRINTS("%s: failed to read control1", __func__);
+		return;
+	}
+
+	/* Disable AMON/BMON */
+	reg |= ISL923X_C1_DISABLE_MON;
+	/* Disable PSYS */
+	reg &= ~ISL923X_C1_ENABLE_PSYS;
+
+	if (raw_write16(chgnum, ISL923X_REG_CONTROL1, reg)) {
+		CPRINTS("%s: failed to write control1", __func__);
+		return;
+	}
+}
+#endif /* CONFIG_CHARGER_ISL9238C */
 
 /*****************************************************************************/
 /* Hardware current ramping */
