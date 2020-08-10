ad704c92af0749fc7a803db375e0aab1102ea32c
Marco Chen <marcochen@google.com>
2020-08-10 12:19:10 +0800

    cbi: add Second Source Factory Cache (SSFC) CBI field
    SSFC field will be leveraged to record what second source is used in the
    DUT by probing components in the factory or RMA.
    
    Firmware code should refer to this field to judge what driver should be
    configured for a specific component. For example, the board code can
    arrange what sensor driver should be set into motion_sensors array if
    there are multiple sources of base or lid sensor.
    
    As the definition of FW_CONFIG, it describe which "features" the
    firmware code should enable or disable. For example, whether lid / base
    sensors should be enabled or not but not care about what second source
    is in this DUT.
    
    Conflict of Cherry-picking:
    Since FW_CONFIG and PCB_SUPPLIER are not cherry-picked, the SSFC field
    is amended from CBI_TAG_SSFC = 8 to 6.
    
    BRANCH=none
    BUG=b:163285687
    TEST=call `cbi-util` to create the cbi image with SSFC and show created
         content.
    TEST=`make buildall -j`
    TEST=`make runhosttests -j`
    
    Change-Id: Icb4aa00ae47ab025198e7fd5edd6aab96a4bf53e
    Signed-off-by: Marco Chen <marcochen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2344268
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-by: Jett Rink <jettrink@chromium.org>
    Commit-Queue: Jett Rink <jettrink@chromium.org>
    (cherry picked from commit 0212d4a3ce01452ddaba46f076f90e9a5e90e589)

--- common/cbi.c
+++ common/cbi.c
@@ -259,6 +259,14 @@ int cbi_get_model_id(uint32_t *id)
 	return cbi_get_board_info(CBI_TAG_MODEL_ID, (uint8_t *)id, &size);
 }
 
+int cbi_get_ssfc(uint32_t *ssfc)
+{
+	uint8_t size = sizeof(*ssfc);
+
+	return cbi_get_board_info(CBI_TAG_SSFC, (uint8_t *)ssfc,
+				  &size);
+}
+
 static int hc_cbi_get(struct host_cmd_handler_args *args)
 {
 	const struct __ec_align4 ec_params_get_cbi *p = args->params;
@@ -375,6 +383,7 @@ static void dump_cbi(void)
 	print_tag("OEM_ID", cbi_get_oem_id(&val), &val);
 	print_tag("MODEL_ID", cbi_get_model_id(&val), &val);
 	print_tag("SKU_ID", cbi_get_sku_id(&val), &val);
+	print_tag("SSFC", cbi_get_ssfc(&val), &val);
 }
 
 static int cc_cbi(int argc, char **argv)
