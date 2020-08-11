6dd7126d175b93d98fd5ce17937941bf1c1cc17f
AlvinCC_Hsu <alvincc_hsu@compal.corp-partner.google.com>
2020-08-11 11:04:13 +0800

    pompom: Modify standard reference of accel sensors
    This patch modify standard reference of base
    and lid accel sensors
    
    BUG=b:160750560
    BRANCH=none
    TEST=execute ectool motionsense to get values
    
    Signed-off-by: AlvinCC_Hsu <alvincc_hsu@compal.corp-partner.google.com>
    Change-Id: I227aa694061f4c77d3d5836a0a8375a682b598a9

--- board/pompom/board.c
+++ board/pompom/board.c
@@ -426,12 +426,12 @@ static struct accelgyro_saved_data_t g_bma255_data;
 const mat33_fp_t base_standard_ref = {
 	{ FLOAT_TO_FP(1), 0,  0},
 	{ 0,  FLOAT_TO_FP(-1),  0},
-	{ 0,  0, FLOAT_TO_FP(-1)}
+	{ 0,  0, FLOAT_TO_FP(1)}
 };
 
 static const mat33_fp_t lid_standard_ref = {
+	{ 0, FLOAT_TO_FP(1), 0},
 	{ FLOAT_TO_FP(-1), 0, 0},
-	{ 0, FLOAT_TO_FP(-1), 0},
 	{ 0, 0, FLOAT_TO_FP(1)}
 };
 
