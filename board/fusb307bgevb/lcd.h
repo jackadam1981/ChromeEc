ea802f8e1a0f0de42c54f214c7db3b722d7e8407
Yun-chieh, Lee <lyunjie@google.com>
2020-08-12 13:15:24 +0800

    add lcd init in board.c BUG=b:162057390 TEST=make BRANCH=master
    Signed-off-by: Yun-chieh Lee <lyunjie@google.com>
    Change-Id: I4ad1cafd91895c94cc6f73c6ed7bc4708389847a

--- board/fusb307bgevb/lcd.h
+++ board/fusb307bgevb/lcd.h
@@ -8,7 +8,7 @@
 #ifndef __CROS_EC_LCD_H
 #define __CROS_EC_LCD_H
 
-// commands
+/* commands */
 #define LCD_CLEARDISPLAY 0x01
 #define LCD_RETURNHOME 0x02
 #define LCD_ENTRYMODESET 0x04
@@ -18,13 +18,13 @@
 #define LCD_SETCGRAMADDR 0x40
 #define LCD_SETDDRAMADDR 0x80
 
-// flags for display entry mode
+/* flags for display entry mode */
 #define LCD_ENTRYRIGHT 0x00
 #define LCD_ENTRYLEFT 0x02
 #define LCD_ENTRYSHIFTINCREMENT 0x01
 #define LCD_ENTRYSHIFTDECREMENT 0x00
 
-// flags for display on/off control
+/* flags for display on/off control */
 #define LCD_DISPLAYON 0x04
 #define LCD_DISPLAYOFF 0x00
 #define LCD_CURSORON 0x02
@@ -32,13 +32,13 @@
 #define LCD_BLINKON 0x01
 #define LCD_BLINKOFF 0x00
 
-// flags for display/cursor shift
+/* flags for display/cursor shift */
 #define LCD_DISPLAYMOVE 0x08
 #define LCD_CURSORMOVE 0x00
 #define LCD_MOVERIGHT 0x04
 #define LCD_MOVELEFT 0x00
 
-// flags for function set
+/* flags for function set */
 #define LCD_8BITMODE 0x10
 #define LCD_4BITMODE 0x00
 #define LCD_2LINE 0x08
@@ -46,12 +46,14 @@
 #define LCD_5x10DOTS 0x04
 #define LCD_5x8DOTS 0x00
 
-// flags for backlight control
+/* flags for backlight control */
 #define LCD_BACKLIGHT 0x08
 #define LCD_NOBACKLIGHT 0x00
 
-#define En 0x04	// Enable bit
-#define Rw 0x02	// Read/Write bit
-#define Rs 0x01	// Register select bit
+#define En 0x04	/* Enable bit */
+#define Rw 0x02	/* Read/Write bit */
+#define Rs 0x01	/* Register select bit */
 
-#endif /*__CROS_EC_I2C_H */
+void lcd_init(uint8_t cols, uint8_t rows, uint8_t dotsize);
+
+#endif /*__CROS_EC_LCD_H */
