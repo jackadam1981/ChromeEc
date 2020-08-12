c3b45f5ce595290f961f70c30af918462a785c69
Yun-chieh, Lee <lyunjie@google.com>
2020-08-12 13:15:24 +0800

    change ec.tasklist (test)
    BUG=b:162057390
    TEST=make
    BRANCH=master
    
    Change-Id: I5ace984ea5dee49690bf6fc4905bc6327e5d87d3
    Signed-off-by: Yun-chieh, Lee <lyunjie@google.com>

--- board/fusb307bgevb/board.h
+++ board/fusb307bgevb/board.h
@@ -8,6 +8,10 @@
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
 
+#include "common.h"
+#include "host_command.h"
+#include "ec_commands.h"
+
 /* 48 MHz SYSCLK clock frequency */
 #define CPU_CLOCK 48000000
 
