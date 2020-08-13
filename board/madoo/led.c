ba58dca74070291abe6ee74d8efded8567625d7b
Leo-Tsai <leocx_tsai@compal.corp-partner.google.com>
2020-08-13 14:30:40 +0800

    madoo: add Power LED behavior
    On madoo, we have a LED indicator with following:
    Power LED:
     System S0: White.
     System S0ix: Blinking white (1 sec on, 1 sec off)
     System S5/G3: Off.
    
    Battery LED:
     DC mode:
      System S0: off.
      System S5/G3: Off.
      System battery low: Blinking white (1 sec on, 1 sec off)
     AC mode:
      Charging: Amber.
      Full charged: White.
    
      only AC without battery: Blinking Amber (0.5 sec on, 0.5 sec off).
    
    BUG=b:163935841
    BRANCH=none
    TEST=test on waddledoo reference board ，battery 、 Power LEDs are work.
    
    Signed-off-by: Leo-Tsai <leocx_tsai@compal.corp-partner.google.com>
    Change-Id: I633582dfbcf2a0050d3c30f9a70ff838f2cfcdc7

--- board/madoo/led.c
+++ board/madoo/led.c
@@ -1,70 +1,188 @@
 /* Copyright 2020 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
+ *
+ * Standard Battery LED and Power LED control for madoo
  */
 
-/* Waddledoo specific PWM LED settings. */
-
-#include "common.h"
-#include "ec_commands.h"
-#include "led_pwm.h"
-#include "pwm.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "battery.h"
+#include "charge_state.h"
+#include "chipset.h"
+#include "led_common.h"
 #include "util.h"
+#include "lid_switch.h"
+
+
+#define BAT_LED_ON 0
+#define BAT_LED_OFF 1
+
+#define POWER_LED_ON 0
+#define POWER_LED_OFF 1
+
+
+#define LED_TICKS_PER_CYCLE 10
+#define LED_ON_TICKS 5
+
 
 const enum ec_led_id supported_led_ids[] = {
-	EC_LED_ID_POWER_LED,
-};
+	EC_LED_ID_BATTERY_LED, EC_LED_ID_POWER_LED};
+
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
-/*
- * We only have a white and an amber LED, so setting any other colour results in
- * both LEDs being off.
- */
-struct pwm_led led_color_map[EC_LED_COLOR_COUNT] = {
-				/* Amber, White */
-	[EC_LED_COLOR_RED]    = {   0,   0 },
-	[EC_LED_COLOR_GREEN]  = {   0,   0 },
-	[EC_LED_COLOR_BLUE]   = {   0,   0 },
-	[EC_LED_COLOR_YELLOW] = {   0,   0 },
-	[EC_LED_COLOR_WHITE]  = {   0, 100 },
-	[EC_LED_COLOR_AMBER]  = {  100,  0 },
+
+enum led_color {
+	LED_OFF = 0,
+	LED_AMBER,
+	LED_WHITE,
+	LED_COLOR_COUNT  /* Number of colors, not a color itself */
 };
 
-/* One logical LED with amber and white channels. */
-struct pwm_led pwm_leds[CONFIG_LED_PWM_COUNT] = {
-	{
-		.ch0 = PWM_CH_LED1_AMBER,
-		.ch1 = PWM_CH_LED2_WHITE,
-		.ch2 = PWM_LED_NO_CHANNEL,
-		.enable = &pwm_enable,
-		.set_duty = &pwm_set_duty,
-	},
-};
+static int bat_led_set_color(enum led_color color)
+{
+	switch (color) {
+	case LED_OFF:
+		gpio_set_level(GPIO_CHG_LED_AMBER_L, BAT_LED_OFF);
+		gpio_set_level(GPIO_CHG_LED_WHITE_L, BAT_LED_OFF);
+		break;
+	case LED_AMBER:
+		gpio_set_level(GPIO_CHG_LED_AMBER_L, BAT_LED_ON);
+		gpio_set_level(GPIO_CHG_LED_WHITE_L, BAT_LED_OFF);
+		break;
+	case LED_WHITE:
+		gpio_set_level(GPIO_CHG_LED_WHITE_L, BAT_LED_ON);
+		gpio_set_level(GPIO_CHG_LED_AMBER_L, BAT_LED_OFF);
+		break;
+	default:
+		return EC_ERROR_UNKNOWN;
+	}
+	return EC_SUCCESS;
+}
+
+static int pwr_led_set_color(enum led_color color)
+{
+	switch (color) {
+	case LED_OFF:
+		gpio_set_level(GPIO_PWR_LED_WHITE_L, POWER_LED_OFF);
+		break;
+	case LED_WHITE:
+		gpio_set_level(GPIO_PWR_LED_WHITE_L,
+			       lid_is_open() ? POWER_LED_ON : POWER_LED_OFF);
+		break;
+	default:
+		return EC_ERROR_UNKNOWN;
+	}
+	return EC_SUCCESS;
+}
 
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
-	memset(brightness_range, '\0',
-	       sizeof(*brightness_range) * EC_LED_COLOR_COUNT);
-	brightness_range[EC_LED_COLOR_AMBER] = 100;
-	brightness_range[EC_LED_COLOR_WHITE] = 100;
+	switch (led_id) {
+	case EC_LED_ID_BATTERY_LED:
+		brightness_range[EC_LED_COLOR_AMBER] = 1;
+		brightness_range[EC_LED_COLOR_WHITE] = 1;
+		break;
+	case EC_LED_ID_POWER_LED:
+		brightness_range[EC_LED_COLOR_WHITE] = 1;
+		break;
+	default:
+		/* ignore */
+		break;
+	}
 }
 
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 {
-	enum pwm_led_id pwm_id;
-
-	/* Convert ec_led_id to pwm_led_id. */
-	if (led_id == EC_LED_ID_POWER_LED)
-		pwm_id = PWM_LED0;
-	else
+	switch (led_id) {
+	case EC_LED_ID_BATTERY_LED:
+		gpio_set_level(GPIO_CHG_LED_AMBER_L,
+			       (brightness[EC_LED_COLOR_AMBER] != 0) ?
+					BAT_LED_ON : BAT_LED_OFF);
+		gpio_set_level(GPIO_CHG_LED_WHITE_L,
+			       (brightness[EC_LED_COLOR_WHITE] != 0) ?
+					BAT_LED_ON : BAT_LED_OFF);
+		break;
+	case EC_LED_ID_POWER_LED:
+		gpio_set_level(GPIO_PWR_LED_WHITE_L,
+			       (brightness[EC_LED_COLOR_WHITE] != 0) ?
+					POWER_LED_ON : POWER_LED_OFF);
+		break;
+	default:
 		return EC_ERROR_UNKNOWN;
-
-	if (brightness[EC_LED_COLOR_WHITE])
-		set_pwm_led_color(pwm_id, EC_LED_COLOR_WHITE);
-	else if (brightness[EC_LED_COLOR_AMBER])
-		set_pwm_led_color(pwm_id, EC_LED_COLOR_AMBER);
-	else
-		/* Otherwise, the "color" is "off". */
-		set_pwm_led_color(pwm_id, -1);
-
+	}
 	return EC_SUCCESS;
 }
+
+#ifdef HAS_TASK_CHIPSET
+static void madoo_led_shutdown(void)
+{
+	pwr_led_set_color(LED_OFF);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, madoo_led_shutdown, HOOK_PRIO_DEFAULT);
+#endif
+
+static void madoo_led_set_power(void)
+{
+	static int power_second;
+
+	power_second++;
+
+	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		pwr_led_set_color(LED_OFF);
+	else if (chipset_in_state(CHIPSET_STATE_ON))
+		pwr_led_set_color(LED_WHITE);
+	else if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND))
+		pwr_led_set_color((power_second %
+					LED_TICKS_PER_CYCLE < LED_ON_TICKS)
+					? LED_WHITE : LED_OFF);
+}
+
+static void madoo_led_set_battery(void)
+{
+	static int battery_second;
+
+	battery_second++;
+
+	/* BAT LED behavior:
+	 * Same as the chromeos spec
+	 * Green/Amber for CHARGE_FLAG_FORCE_IDLE
+	 */
+	switch (charge_get_state()) {
+	case PWR_STATE_CHARGE:
+		bat_led_set_color(LED_AMBER);
+		break;
+	case PWR_STATE_DISCHARGE:
+		if (charge_get_percent() < 10)
+			bat_led_set_color((battery_second %
+					LED_TICKS_PER_CYCLE < LED_ON_TICKS)
+					? LED_WHITE : LED_OFF);
+		else
+			bat_led_set_color(LED_OFF);
+		break;
+	case PWR_STATE_ERROR:
+		/* Delay 100ms for 0.5s blink */
+		if (battery_second & 0x2)
+			msleep(100);
+
+		bat_led_set_color((battery_second & 2)
+					? LED_AMBER : LED_OFF);
+		break;
+	case PWR_STATE_CHARGE_NEAR_FULL:
+			bat_led_set_color(LED_WHITE);
+		break;
+	default:
+		/* Other states don't alter LED behavior */
+		break;
+	}
+}
+
+/* Called by hook task every 200 ms */
+static void led_tick(void)
+{
+	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
+		madoo_led_set_power();
+	if (led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED))
+		madoo_led_set_battery();
+
+}
+DECLARE_HOOK(HOOK_TICK, led_tick, HOOK_PRIO_DEFAULT);
