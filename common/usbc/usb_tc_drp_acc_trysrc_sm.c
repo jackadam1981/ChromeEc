90b1be6a27069e2c967e1c834b4f4c9208c8d0f1
Patryk Duda <pdk@semihalf.com>
2020-08-18 15:47:13 +0200

    WIP: eve: Enable TCPMv2 support
    Minimal changes to enable TCPMv2 support on eve
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: I01c02928ef4741dd65e68c0776983353dfe8ef73

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -1351,6 +1351,11 @@ void tc_state_init(int port)
 	else
 		first_state = TC_ERROR_RECOVERY;
 
+#ifdef CONFIG_USB_PD_TCPC_BOARD_INIT
+	/* Board specific TCPC init */
+	board_tcpc_init();
+#endif
+
 	/*
 	 * Start with ErrorRecovery state if we can to put us in
 	 * a clean state from any previous boots.
