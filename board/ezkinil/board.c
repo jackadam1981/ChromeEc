93ff0ff833e7b6689fd8854434998c0663703280
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2020-08-19 12:15:14 +0800

    Ezkinil: Workaround for reading unexpected value from isl9241 CONTROL1
    Sometimes CONTROL1 was read 0xFF03 for unknown reason when the state
    change from S0 to S3, but the second read will get the correct 0x0103.
    Retry CONROL1 read before update learn mode to make sure write the
    correct value.
    
    BUG=b:163076059
    BRANCH=none
    TEST=none
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: Iec56dab838b0aa362c543cce74d64615faf40bfc

--- board/ezkinil/board.c
+++ board/ezkinil/board.c
@@ -11,6 +11,7 @@
 #include "driver/accelgyro_bmi_common.h"
 #include "driver/accel_kionix.h"
 #include "driver/accel_kx022.h"
+#include "driver/charger/isl9241.h"
 #include "driver/ppc/aoz1380.h"
 #include "driver/ppc/nx20p348x.h"
 #include "driver/retimer/tusb544.h"
@@ -688,4 +689,33 @@ __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 	charge_set_input_current_limit(MAX(charge_ma,
 				CONFIG_CHARGER_INPUT_CURRENT),
 				charge_mv);
+}
+
+/*
+ * b/163076059: Sometimes CONTROL1 was read 0xFF03 for unknown reason
+ * when the state change from S0 to S3, but the second read will get
+ * the correct 0x0103. Retry CONROL1 read before update learn mode
+ * to make sure write the correct value.
+ */
+__override int isl9241_update_learn_mode(int chgnum, int enable)
+{
+	int rv;
+	int i;
+	int reg;
+
+	/* Retry CONTROL1 read of 0xff03 */
+	for (i = 0; i < 10; i++) {
+		rv = isl9241_read(chgnum, ISL9241_REG_CONTROL1, &reg);
+		if (rv)
+			return rv;
+		if (reg != 0xFF03)
+			break;
+	}
+
+	if (enable)
+		reg |= ISL9241_CONTROL1_LEARN_MODE;
+	else
+		reg &= ~ISL9241_CONTROL1_LEARN_MODE;
+
+	return isl9241_write(chgnum, ISL9241_REG_CONTROL1, reg);
 }
