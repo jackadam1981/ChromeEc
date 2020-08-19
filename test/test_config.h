jesultra/chromiumos-ec

test: body_detection: add unittest for algorithm
Add test to check if the algorithm of body detection is correct.

There are three testcases:
OnBody: always in on-body state, should not change to off-body
Off to On: change state from off-body to on-body
On to Off: change state from on-body to off-body

BRANCH=None
BUG=b:123434029
TEST=make buildall

Signed-off-by: Ching-Kang Yen <chingkang@chromium.org>
Change-Id: Ib027fa3ed659f0d2efd7416888a9ee4ad519edd6

test/test_config.h
@@ -146,7 +146,8 @@
 	defined(TEST_MOTION_ANGLE) || \
 	defined(TEST_MOTION_ANGLE_TABLET) || \
 	defined(TEST_MOTION_SENSE_FIFO) || \
-	defined(CONFIG_ONLINE_CALIB)
+	defined(CONFIG_ONLINE_CALIB) || \
+	defined(TEST_BODY_DETECTION)
 enum sensor_id {
 	BASE,
 	LID,
@@ -172,6 +173,17 @@ enum sensor_id {
 #define CONFIG_ACCEL_FORCE_MODE_MASK \
 	((1 << CONFIG_LID_ANGLE_SENSOR_BASE) | \
 	 (1 << CONFIG_LID_ANGLE_SENSOR_LID))
+#endif
+
+#if defined(TEST_BODY_DETECTION)
+#define CONFIG_BODY_DETECTION
+#define CONFIG_BODY_DETECTION_SENSOR BASE
+#define CONFIG_BODY_DETECTION_MAX_WINDOW_SIZE     250 /* max sensor odr (Hz) */
+#define CONFIG_BODY_DETECTION_VAR_THRESHOLD       550 /* (mm/s^2)^2 */
+#define CONFIG_BODY_DETECTION_CONFIDENCE_DELTA    525 /* (mm/s^2)^2 */
+#define CONFIG_BODY_DETECTION_ON_BODY_CON         50  /* % */
+#define CONFIG_BODY_DETECTION_OFF_BODY_CON        10  /* % */
+#define CONFIG_BODY_DETECTION_STATIONARY_DURATION 15  /* second */
 #endif
 
 #ifdef TEST_RMA_AUTH

jackadam1981/ChromeEc

test/test_config.h
@@ -142,11 +142,12 @@
 #define CONFIG_TEMP_CACHE_STALE_THRES (1 * SECOND)
 #endif /* CONFIG_ONLINE_CALIB && !CONFIG_TEMP_CACHE_STALE_THRES */
 
-#if defined(TEST_MOTION_LID) || \
+#if defined(CONFIG_ONLINE_CALIB) || \
+	defined(TEST_BODY_DETECTION) || \
 	defined(TEST_MOTION_ANGLE) || \
 	defined(TEST_MOTION_ANGLE_TABLET) || \
-	defined(TEST_MOTION_SENSE_FIFO) || \
-	defined(CONFIG_ONLINE_CALIB)
+	defined(TEST_MOTION_LID) || \
+	defined(TEST_MOTION_SENSE_FIFO)
 enum sensor_id {
 	BASE,
 	LID,
@@ -172,6 +173,11 @@ enum sensor_id {
 #define CONFIG_ACCEL_FORCE_MODE_MASK \
 	((1 << CONFIG_LID_ANGLE_SENSOR_BASE) | \
 	 (1 << CONFIG_LID_ANGLE_SENSOR_LID))
+#endif
+
+#if defined(TEST_BODY_DETECTION)
+#define CONFIG_BODY_DETECTION
+#define CONFIG_BODY_DETECTION_SENSOR BASE
 #endif
 
 #ifdef TEST_RMA_AUTH