ef057d2718aa78c45e47c3a7605cdc980b380a11
Edward Hill <ecgh@chromium.org>
2020-08-20 11:20:41 -0600

    TEST ONLY: Increase temp thresholds
    BUG=b:162907613
    BRANCH=none
    TEST=none
    
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Change-Id: Id8cb17f6f31a4793ac0032ca6421d23a42f539f2

--- board/trembyle/board.c
+++ board/trembyle/board.c
@@ -434,11 +434,11 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 
 const static struct ec_thermal_config thermal_thermistor = {
 	.temp_host = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
-		[EC_TEMP_THRESH_HALT] = C_TO_K(92),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(124),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(125),
 	},
 	.temp_host_release = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(123),
 	},
 	.temp_fan_off = C_TO_K(25),
 	.temp_fan_max = C_TO_K(58),
@@ -446,11 +446,11 @@ const static struct ec_thermal_config thermal_thermistor = {
 
 const static struct ec_thermal_config thermal_cpu = {
 	.temp_host = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
-		[EC_TEMP_THRESH_HALT] = C_TO_K(92),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(124),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(125),
 	},
 	.temp_host_release = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(123),
 	},
 	.temp_fan_off = C_TO_K(25),
 	.temp_fan_max = C_TO_K(58),
