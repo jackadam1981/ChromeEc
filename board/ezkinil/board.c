0c2f99e5a64ae5fae862e79a515f472457209add
Edward Hill <ecgh@chromium.org>
2020-08-20 11:20:41 -0600

    TEST ONLY: Increase temp thresholds
    BUG=b:162907613
    BRANCH=none
    TEST=none
    
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Change-Id: Id8cb17f6f31a4793ac0032ca6421d23a42f539f2

--- board/ezkinil/board.c
+++ board/ezkinil/board.c
@@ -570,11 +570,11 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 
 const static struct ec_thermal_config thermal_thermistor = {
 	.temp_host = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(85),
-		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(124),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(125),
 	},
 	.temp_host_release = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(70),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(123),
 	},
 	.temp_fan_off = 0,
 	.temp_fan_max = 0,
@@ -582,11 +582,11 @@ const static struct ec_thermal_config thermal_thermistor = {
 
 const static struct ec_thermal_config thermal_soc = {
 	.temp_host = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(75),
-		[EC_TEMP_THRESH_HALT] = C_TO_K(80),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(124),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(125),
 	},
 	.temp_host_release = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(65),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(123),
 	},
 	.temp_fan_off = 0,
 	.temp_fan_max = 0,
@@ -594,11 +594,11 @@ const static struct ec_thermal_config thermal_soc = {
 
 const static struct ec_thermal_config thermal_cpu = {
 	.temp_host = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(85),
-		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(124),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(125),
 	},
 	.temp_host_release = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(123),
 	},
 	.temp_fan_off = C_TO_K(37),
 	.temp_fan_max = C_TO_K(90),
--- board/morphius/board.c
+++ board/morphius/board.c
@@ -460,11 +460,11 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 
 const static struct ec_thermal_config thermal_cpu = {
 	.temp_host = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
-		[EC_TEMP_THRESH_HALT] = C_TO_K(92),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(124),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(125),
 	},
 	.temp_host_release = {
-		[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(123),
 	},
 	.temp_fan_off = C_TO_K(98),
 	.temp_fan_max = C_TO_K(99),
