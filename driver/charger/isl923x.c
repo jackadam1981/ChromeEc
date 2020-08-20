617f4af6d29388868ea7f2d9ee74c8bc39269dbf
Ting Shen <phoenixshen@google.com>
2020-08-20 18:06:08 +0800

    isl9238c: implement hibernate/resume
    Implemented isl9238c_hibernate/resume for ulp mode.
    And added a helper method raw_update16 for the read-update-write
    register flow.
    
    BUG=b:162790592
    TEST=make
    BRANCH=master
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I0c3d80aaf6e1290da299b27d88961806295c90e7

--- driver/charger/isl923x.c
+++ driver/charger/isl923x.c
@@ -85,6 +85,14 @@ static inline enum ec_error_list raw_write16(int chgnum, int offset, int value)
 			   offset, value);
 }
 
+static inline enum ec_error_list raw_update16(int chgnum, int offset, int mask,
+					      enum mask_update_action action)
+{
+	return i2c_update16(chg_chips[chgnum].i2c_port,
+			    chg_chips[chgnum].i2c_addr_flags,
+			    offset, mask, action);
+}
+
 static enum ec_error_list isl9237_set_current(int chgnum, uint16_t current)
 {
 	return raw_write16(chgnum, ISL923X_REG_CHG_CURRENT,
@@ -409,6 +417,48 @@ int isl923x_set_comparator_inversion(int chgnum, int invert)
 	return rv;
 }
 
+#ifdef CONFIG_CHARGER_ISL9238C
+int isl9238c_hibernate(int chgnum)
+{
+	/* Disable IMON */
+	RETURN_ERROR(raw_update16(chgnum, ISL923X_REG_CONTROL1,
+				ISL923X_C1_DISABLE_MON, MASK_SET));
+
+	/* Disable PSYS */
+	RETURN_ERROR(raw_update16(chgnum, ISL923X_REG_CONTROL1,
+				ISL923X_C1_ENABLE_PSYS, MASK_CLR));
+
+	/* Disable GP comparator */
+	RETURN_ERROR(raw_update16(chgnum, ISL923X_REG_CONTROL2,
+				ISL923X_C2_COMPARATOR, MASK_SET));
+
+	/* Force BGATE off */
+	RETURN_ERROR(raw_update16(chgnum, ISL9238_REG_CONTROL3,
+				ISL9238_C3_BGATE_OFF, MASK_SET));
+
+
+	return EC_SUCCESS;
+}
+
+static int isl9238c_resume(int chgnum)
+{
+	/* Revert everything in isl9238c_hibernate() */
+	RETURN_ERROR(raw_update16(chgnum, ISL923X_REG_CONTROL1,
+				ISL923X_C1_DISABLE_MON, MASK_CLR));
+
+	RETURN_ERROR(raw_update16(chgnum, ISL923X_REG_CONTROL1,
+				ISL923X_C1_ENABLE_PSYS, MASK_SET));
+
+	RETURN_ERROR(raw_update16(chgnum, ISL923X_REG_CONTROL2,
+				ISL923X_C2_COMPARATOR, MASK_CLR));
+
+	RETURN_ERROR(raw_update16(chgnum, ISL9238_REG_CONTROL3,
+				ISL9238_C3_BGATE_OFF, MASK_CLR));
+
+	return EC_SUCCESS;
+}
+#endif /* CONFIG_CHARGER_ISL9238C */
+
 static void isl923x_init(int chgnum)
 {
 	int reg;
@@ -519,6 +569,10 @@ static void isl923x_init(int chgnum)
 		if (raw_write16(chgnum, ISL923X_REG_CONTROL1, reg))
 			goto init_fail;
 	}
+
+	/* Revert all changes done by isl9238c_hibernate(). */
+	if (IS_ENABLED(CONFIG_CHARGER_ISL9238C) && isl9238c_resume(chgnum))
+		goto init_fail;
 
 	if (IS_ENABLED(CHARGER_ISL9238X) ||
 	    IS_ENABLED(CONFIG_CHARGER_RAA489000)) {
