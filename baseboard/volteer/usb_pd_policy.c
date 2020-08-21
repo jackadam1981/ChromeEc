5edf2b754f08ee40b9dd51c5a5c0a91afd17de44
li feng <li1.feng@intel.com>
2020-08-21 15:23:28 -0700

    TCPMv2: support TBT Alt mode as UFP
    Implemented SVDM responders for TBT Alt mode
    
    BUG=b:148528713,b:157163664
    BRANCH=none
    TEST=Boot up Volteer, run "pd trysrc 0" on EC console; then connect
    Volteer port 1 to Windows PC TBT port. TBT alt mode will set up with
    Volteer port 1 as UFP, Windows PC TBT port as DFP. From host console,
    check port partner is enumerated.
    
    Signed-off-by: li feng <li1.feng@intel.com>
    Change-Id: If4c80418677f541e9c1c7c8c84446357000aaecb

--- baseboard/volteer/usb_pd_policy.c
+++ baseboard/volteer/usb_pd_policy.c
@@ -4,9 +4,11 @@
  */
 /* Shared USB-C policy for Volteer boards */
 #include "charge_manager.h"
+#include "chipset.h"
 #include "compile_time_macros.h"
 #include "console.h"
 #include "gpio.h"
+#include "usb_common.h"
 #include "usb_mux.h"
 #include "usbc_ppc.h"
 #include "usb_pd.h"
@@ -79,3 +81,111 @@ int board_vbus_source_enabled(int port)
 {
 	return ppc_is_sourcing_vbus(port);
 }
+
+/* ----------------- Vendor Defined Messages ------------------ */
+#define OPOS_TBT 1
+
+union tbt_dev_mode_enter_cmd ufp_enter_mode[CONFIG_USB_PD_PORT_MAX_COUNT];
+
+const uint32_t vdo_tbt_modes[1] = {
+	VDO_MODE_TBT_DEV(0,      /* no vendor specific B1 supported */
+			 0,      /* no vendor specific B0 supported */
+			 1,      /* Intel specific B0 supported */
+			 0,      /* TBT3 adapter */
+			 0x0001) /* TBT mode */
+};
+
+const uint32_t vdo_idh = VDO_IDH(1, /* Data caps as USB host   */
+				 1, /* Data caps as USB device */
+				 IDH_PTYPE_HUB, /* 1: Hub */
+				 1, /* Supports alt modes */
+				 USB_VID_GOOGLE) |
+				 (((0x2) & 0x7) << 23); /* PDUSB host */
+
+const uint32_t vdo_product = VDO_PRODUCT(CONFIG_USB_PID, 0);
+
+/* TODO: add USB4 to capability once USB4 response implemented */
+const uint32_t vdo_ufp1 = VDO_UFP1(0x5, /* USB2.0, USB3.2 device capable */
+				   0x3, /* TBT3, reconfig */
+				   0x3  /* USB4 Gen3 */);
+
+
+const uint32_t vdo_dfp = VDO_DFP(7, /* USB2.0, USB3.2 and USB4 host capable */
+				 1  /* Port 1 */);
+
+static int svdm_tbt_compat_response_identity(int port, uint32_t *payload)
+{
+	payload[VDO_I(IDH)] = vdo_idh;
+	payload[VDO_I(CSTAT)] = VDO_CSTAT(0);
+	payload[VDO_I(PRODUCT)] = vdo_product;
+	payload[VDO_I(PTYPE_UFP1_VDO)] = vdo_ufp1;
+	payload[VDO_I(PTYPE_UFP2_VDO)] = 0;
+	payload[VDO_I(PTYPE_DFP_VDO)] = vdo_dfp;
+	return VDO_I(PTYPE_DFP_VDO) + 1;
+}
+
+static int svdm_tbt_compat_response_svids(int port, uint32_t *payload)
+{
+	payload[1] = VDO_SVID(USB_VID_INTEL, 0);
+	return 2;
+}
+
+static int svdm_tbt_compat_response_modes(int port, uint32_t *payload)
+{
+	if (PD_VDO_VID(payload[0]) == USB_VID_INTEL) {
+		memcpy(payload + 1, vdo_tbt_modes, sizeof(vdo_tbt_modes));
+		return ARRAY_SIZE(vdo_tbt_modes) + 1;
+	} else {
+		return 0; /* NAK */
+	}
+}
+
+static int svdm_tbt_compat_response_enter_mode(
+	int port, uint32_t *payload)
+{
+	mux_state_t mux_state = 0;
+
+	/* Do not enter mode while CPU is off. */
+	if (chipset_in_or_transitioning_to_state(CHIPSET_STATE_ANY_OFF))
+		return 0; /* NAK */
+
+	if ((PD_VDO_VID(payload[0]) == USB_VID_INTEL) &&
+		(PD_VDO_OPOS(payload[0]) == OPOS_TBT)) {
+		mux_state = usb_mux_get(port);
+
+		/*
+		 * Ref: Figure 6-21 Successful Enter Mode sequence
+		 * UFP(responder) should be in USB mode or safe mode before
+		 * sending Enter Mode Command response.
+		 */
+		if ((mux_state & USB_PD_MUX_USB_ENABLED) ||
+			(mux_state & USB_PD_MUX_SAFE_MODE)) {
+
+			/* Save TBT3 SOP VDO from enter_mode request */
+			ufp_enter_mode[port] =
+				(union tbt_dev_mode_enter_cmd)payload[1];
+
+			/*
+			 * TODO(b/166455363): Object position in Enter Mode
+			 * Command response VDM header is the offset of mode
+			 * entered, make sure it's set correctly in response
+			 * message buffer.
+			 */
+			set_tbt_compat_mode_ready(port);
+			CPRINTS("UFP Enter TBT mode");
+			return 1; /* ACK */
+		}
+	}
+
+	CPRINTS("UFP failed to enter TBT mode(mux=0x%x)", mux_state);
+	return 0;
+}
+
+const struct svdm_response svdm_rsp = {
+	.identity = &svdm_tbt_compat_response_identity,
+	.svids = &svdm_tbt_compat_response_svids,
+	.modes = &svdm_tbt_compat_response_modes,
+	.enter_mode = &svdm_tbt_compat_response_enter_mode,
+	.amode = NULL,
+	.exit_mode = NULL,
+};
