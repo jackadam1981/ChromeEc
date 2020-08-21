dcf04028f3c16fc5e130d736d08b9b14b87170d6
li feng <li1.feng@intel.com>
2020-08-21 11:01:02 -0700

    TCPMv2: set VDM response buffer and length
    Pass tx_emsg[port].buf to VDM response implementation functions
    Convert data objects count to bytes
    
    VDM response tx_payload[0] should not use USB_VID_GOOGLE as VDM header
    SVID to respond all requests. Now let VDM response implementation
    constructs VDM header; while pe_vdm_response_entry only set command type
    to ACK, NAK, or BUSY accordingly.
    
    BUG=b:148528713,b:157163664
    BRANCH=none
    TEST=Connect volteer to Gatkex DFP port, with CL:2370045, check VDM
    response messages from Volteer have correct contents.
    
    Signed-off-by: li feng <li1.feng@intel.com>
    Change-Id: I7403af1449abfa4ebf6b43ded457e3654396aadb

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -4923,7 +4923,7 @@ static void pe_vdm_request_dpm_exit(int port)
  */
 static void pe_vdm_response_entry(int port)
 {
-	int ret = 0;
+	int response_size_bytes = 0;
 	uint32_t *rx_payload;
 	uint32_t *tx_payload;
 	uint8_t vdo_cmd;
@@ -4987,47 +4987,41 @@ static void pe_vdm_response_entry(int port)
 	tx_payload = (uint32_t *)tx_emsg[port].buf;
 
 	if (func) {
-		ret = func(port, rx_payload);
-		if (ret)
+		/*
+		 * Designed in TCPMv1, svdm_response functions use same
+		 * buffer to take received data and overwrite with response
+		 * data. To work with this interface, here copy rx data to
+		 * tx buffer and pass tx_payload to func.
+		 * TODO(b/166455363): change the interface to pass both rx
+		 * and tx buffer
+		 */
+		memcpy(tx_payload, rx_payload, rx_emsg[port].len);
+		/*
+		 * Return value of func is the data objects count in payload.
+		 * return 1 means only VDM header, no VDO.
+		 */
+		response_size_bytes =
+				func(port, tx_payload) * sizeof(*tx_payload);
+		if (response_size_bytes > 0)
 			/* ACK */
-			tx_payload[0] = VDO(
-				USB_VID_GOOGLE,
-				1, /* Structured VDM */
-				VDO_SVDM_VERS(pd_get_vdo_ver(port, TCPC_TX_SOP))
-				| VDO_CMDT(CMDT_RSP_ACK) |
-				vdo_cmd);
-		else if (!ret)
+			tx_payload[0] |= VDO_CMDT(CMDT_RSP_ACK);
+		else if (response_size_bytes == 0)
 			/* NAK */
-			tx_payload[0] = VDO(
-				USB_VID_GOOGLE,
-				1, /* Structured VDM */
-				VDO_SVDM_VERS(pd_get_vdo_ver(port, TCPC_TX_SOP))
-				| VDO_CMDT(CMDT_RSP_NAK) |
-				vdo_cmd);
+			tx_payload[0] |= VDO_CMDT(CMDT_RSP_NAK);
 		else
 			/* BUSY */
-			tx_payload[0] = VDO(
-				USB_VID_GOOGLE,
-				1, /* Structured VDM */
-				VDO_SVDM_VERS(pd_get_vdo_ver(port, TCPC_TX_SOP))
-				| VDO_CMDT(CMDT_RSP_BUSY) |
-				vdo_cmd);
+			tx_payload[0] |= VDO_CMDT(CMDT_RSP_BUSY);
 
-		if (ret <= 0)
-			ret = 4;
+		if (response_size_bytes <= 0)
+			response_size_bytes = 4;
 	} else {
-		/* not supported : NACK it */
-		tx_payload[0] = VDO(
-			USB_VID_GOOGLE,
-			1, /* Structured VDM */
-			VDO_SVDM_VERS(pd_get_vdo_ver(port, TCPC_TX_SOP)) |
-			VDO_CMDT(CMDT_RSP_NAK) |
-			vdo_cmd);
-		ret = 4;
+		/* not supported : NAK it */
+		tx_payload[0] |= VDO_CMDT(CMDT_RSP_NAK);
+		response_size_bytes = 4;
 	}
 
 	/* Send ACK, NAK, or BUSY */
-	tx_emsg[port].len = ret;
+	tx_emsg[port].len = response_size_bytes;
 	send_data_msg(port, TCPC_TX_SOP, PD_DATA_VENDOR_DEF);
 }
 
