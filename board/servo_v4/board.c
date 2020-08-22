95affb44581fab8d98a0536c5872508eaba1c611
Matthew Blecker <matthewb@chromium.org>
2020-08-22 02:00:14 -0700

    servo_v4: Enable USB3 to the DUT by default.
    Since servod enables this automatically as of CL:2402441, and servod is
    always used in the labs, the main effect of this change is enabling
    USB3-to-DUT on developer desks even when they haven't started servod.
    
    BRANCH=none
    BUG=b:163418494
    TEST=Built servo_v4 firmware with this change cherry-picked onto
    f1113c92ba89880cf56cacc2439496cf5ba6171a from firmware-servo-12768.B-master
    branch, which corresponds to servo_v4_v2.4.35-f1113c92b build from
    servo-firmware-R81-12768.40.0 release.
    
    Then for each of these DUTs:
    - Atlas (USB 3, CCD) + Type-C CCD cable
    - Cave (USB 3, no CCD) + Type-A cable with A->C adapter + servo_micro
    - Krane (USB 2, CCD) + Type-C CCD cable
    
    1) Flashed the firmware to Servo v4.
    
    2) Power cycled Servo v4 by unplugging + replugging Host connection.
    
    3) All Servo v4 under test have a USB3 memory stick in the
    Servo v4 USB3 Type-A port with bootable test image for the connected DUT.
    
    4) Ran "lsusb -t" on the DUT and observed that both the Servo v4 internal
    USB NIC and the external USB memory stick had enumerated at 5 Gbps speed.
    
    5) Used scp(1) to transfer an XZ-compressed CrOS recovery image in both
    directions.  Atlas and Cave were able to approach full GbE speed (890+ Mbps
    plus SSH overhead) in both directions, which would not be possible with
    USB 2.
    
    6) Rebooted DUT from USB drive.
    
    7) $ lsusb -t  # confirmed expected USB enumeration
    
    8) $ chromeos-install  # observed faster-than-USB2 speeds for rootfs transfer on Atlas and Cave, not on Krane
    
    9) $ shutdown -r now
    
    10) $ lsusb -t  # confirmed expected USB enumeration
    
    11) Start servod on the Host, confirm that servod initialization of
    reinit_dut_usb3_en:automatic was a no-op.
    (All Servo v4 used were absent from the USB 3 exempt list.)
    
    12) [Host] $ dut-control dut_usb3_en reinit_dut_usb3_en  # confirm both report "on" value
    
    Functionality of USB3-to-DUT after servod initialization was already tested
    in CL:2361026.
    
    USB3-to-DUT has been live in the ATLabs since CL:2402441 was rolled out to
    labstations.
    
    Cq-Depend: chromium:2402441
    Change-Id: I83a33fc2dacdf1971a1b0a91588c07e5fa785a8d
    Signed-off-by: Matthew Blecker <matthewb@chromium.org>

--- board/servo_v4/board.c
+++ board/servo_v4/board.c
@@ -546,9 +546,10 @@ static void board_init(void)
 	gpio_set_flags(GPIO_DUT_HUB_USB_RESET_L, GPIO_OUT_HIGH);
 
 	/*
-	 * Disable USB3 mode in PS8742 USB/DP Mux.
+	 * Enable USB3 mode in PS8742 USB/DP Mux.
 	 */
-	i2c_write8(I2C_PORT_MASTER, PS8740_I2C_ADDR0_FLAG, PS8740_REG_MODE, 0);
+	i2c_write8(I2C_PORT_MASTER, PS8740_I2C_ADDR0_FLAG, PS8740_REG_MODE,
+		PS8740_MODE_USB_ENABLED);
 
 	/* Enable uservo USB by default. */
 	init_ioexpander();
