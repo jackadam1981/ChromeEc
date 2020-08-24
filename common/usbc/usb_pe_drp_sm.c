a266423e4a454f7793d67538c9f2d7a156d42bff
Denis Brockus <dbrockus@google.com>
2020-08-24 13:07:24 -0600

    TCPMv2: Modal Operations should not persist through HardReset
    BUG=b:165983983
    BRANCH=none
    TEST=dock specified in bug should be able to PR_Swap
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: Ifaccba2d741ed066e6bafc95c7e604b8f8dcfb17

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -3025,6 +3025,9 @@ static void pe_snk_transition_to_default_entry(int port)
 {
 	print_current_state(port);
 
+	/* Modal Operations should not persist through Hard Reset */
+	PE_CLR_FLAG(port, PE_FLAGS_MODAL_OPERATION);
+
 	/* Inform the TC Layer of Hard Reset */
 	tc_hard_reset_request(port);
 }
