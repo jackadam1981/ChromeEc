c40bb13c2528121a4d0bd3a665e2270d358f51c8
Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
2020-08-25 10:29:54 +0000

    board: nucleo-f411re: create icm426xx development platform
    Use icm426xx chip instead of bmi.
    Fix board init hook priority to be run after motionsense init.
    Increase task stack sizes.
    
    BUG=chromium:1117541
    BRANCH=None
    TEST=ectool && tast run hardware.SensorRing
    
    Cq-Depend: chromium:2317888 chromium:2374647
    Signed-off-by: Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
    Change-Id: I860b669fd3d2ee196a66e5c80cdc0835b4e5bc73

--- board/nucleo-f411re/board.c
+++ board/nucleo-f411re/board.c
@@ -8,11 +8,17 @@
 #include "adc_chip.h"
 #include "common.h"
 #include "console.h"
+<<<<<<< HEAD   (6dbd10 chgramp: Don't ramp DTS suppliers above advertisement)
 #include "driver/accelgyro_bmi160.h"
+=======
+#include "driver/accelgyro_icm_common.h"
+#include "driver/accelgyro_icm426xx.h"
+>>>>>>> CHANGE (a4b8af board: nucleo-f411re: create icm426xx development platform)
 #include "ec_version.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "i2c.h"
+#include "spi.h"
 #include "motion_sense.h"
 
 #include "gpio.h"
@@ -31,6 +37,7 @@ void user_button_evt(enum gpio_signal signal)
 static void board_init(void)
 {
 	gpio_enable_interrupt(GPIO_USER_BUTTON_L);
+	gpio_enable_interrupt(GPIO_ICM426XX_INT1_L);
 
 	/* No power control yet */
 	/* Go to S3 state */
@@ -39,7 +46,7 @@ static void board_init(void)
 	/* Go to S0 state */
 	hook_notify(HOOK_CHIPSET_RESUME);
 }
-DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_LAST);
 
 /* ADC channels */
 const struct adc_t adc_channels[] = {
@@ -53,28 +60,49 @@ BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
 
 /* I2C ports */
 const struct i2c_port_t i2c_ports[] = {
-	{"master", I2C_PORT_MASTER, 100,
+	{"master", I2C_PORT_MASTER, 400,
 	 GPIO_MASTER_I2C_SCL, GPIO_MASTER_I2C_SDA},
 };
-
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+#ifdef CONFIG_SPI_ACCEL_PORT
+
+/* SPI ports */
+enum {
+	SPI_PORT_ACCEL,
+};
+const struct spi_device_t spi_devices[] = {
+	[SPI_PORT_ACCEL] = { CONFIG_SPI_ACCEL_PORT, 1, GPIO_SPI_ACCEL_CS_L },
+};
+const unsigned int spi_devices_used = ARRAY_SIZE(spi_devices);
+
+#endif
 
 /* Base Sensor mutex */
 static struct mutex g_base_mutex;
 
+<<<<<<< HEAD   (6dbd10 chgramp: Don't ramp DTS suppliers above advertisement)
 static struct bmi160_drv_data_t g_bmi160_data;
+=======
+static struct icm_drv_data_t g_icm426xx_data;
+>>>>>>> CHANGE (a4b8af board: nucleo-f411re: create icm426xx development platform)
 
 struct motion_sensor_t motion_sensors[] = {
 	[BASE_ACCEL] = {
 	 .name = "Base Accel",
 	 .active_mask = SENSOR_ACTIVE_S0_S3,
-	 .chip = MOTIONSENSE_CHIP_BMI160,
+	 .chip = MOTIONSENSE_CHIP_ICM426XX,
 	 .type = MOTIONSENSE_TYPE_ACCEL,
 	 .location = MOTIONSENSE_LOC_BASE,
-	 .drv = &bmi160_drv,
+	 .drv = &icm426xx_drv,
 	 .mutex = &g_base_mutex,
-	 .drv_data = &g_bmi160_data,
+	 .drv_data = &g_icm426xx_data,
+#ifdef CONFIG_SPI_ACCEL_PORT
+	 .port = CONFIG_SPI_ACCEL_PORT,
+	 .i2c_spi_addr_flags = SLAVE_MK_SPI_ADDR_FLAGS(SPI_PORT_ACCEL),
+#else
 	 .port = I2C_PORT_ACCEL,
+<<<<<<< HEAD   (6dbd10 chgramp: Don't ramp DTS suppliers above advertisement)
 	 .addr = BMI160_ADDR0,
 	 .rot_standard_ref = NULL,
 	 .default_range = 2,  /* g, enough for laptop. */
@@ -90,21 +118,42 @@ struct motion_sensor_t motion_sensors[] = {
 			.ec_rate = 100 * MSEC,
 		 },
 	 },
+=======
+	 .i2c_spi_addr_flags = SLAVE_MK_I2C_ADDR_FLAGS(ICM426XX_ADDR0_FLAGS),
+#endif
+	 .default_range = 4,  /* g, to meet CDD 7.3.1/C-1-4 reqs */
+	 .min_frequency = ICM426XX_ACCEL_MIN_FREQ,
+	 .max_frequency = ICM426XX_ACCEL_MAX_FREQ,
+>>>>>>> CHANGE (a4b8af board: nucleo-f411re: create icm426xx development platform)
 	},
 
 	[BASE_GYRO] = {
 	 .name = "Base Gyro",
 	 .active_mask = SENSOR_ACTIVE_S0_S3,
-	 .chip = MOTIONSENSE_CHIP_BMI160,
+	 .chip = MOTIONSENSE_CHIP_ICM426XX,
 	 .type = MOTIONSENSE_TYPE_GYRO,
 	 .location = MOTIONSENSE_LOC_BASE,
-	 .drv = &bmi160_drv,
+	 .drv = &icm426xx_drv,
 	 .mutex = &g_base_mutex,
-	 .drv_data = &g_bmi160_data,
+	 .drv_data = &g_icm426xx_data,
+#ifdef CONFIG_SPI_ACCEL_PORT
+	 .port = CONFIG_SPI_ACCEL_PORT,
+	 .i2c_spi_addr_flags = SLAVE_MK_SPI_ADDR_FLAGS(SPI_PORT_ACCEL),
+#else
 	 .port = I2C_PORT_ACCEL,
+<<<<<<< HEAD   (6dbd10 chgramp: Don't ramp DTS suppliers above advertisement)
 	 .addr = BMI160_ADDR0,
+=======
+	 .i2c_spi_addr_flags = SLAVE_MK_I2C_ADDR_FLAGS(ICM426XX_ADDR0_FLAGS),
+#endif
+>>>>>>> CHANGE (a4b8af board: nucleo-f411re: create icm426xx development platform)
 	 .default_range = 1000, /* dps */
+<<<<<<< HEAD   (6dbd10 chgramp: Don't ramp DTS suppliers above advertisement)
 	 .rot_standard_ref = NULL,
+=======
+	 .min_frequency = ICM426XX_GYRO_MIN_FREQ,
+	 .max_frequency = ICM426XX_GYRO_MAX_FREQ,
+>>>>>>> CHANGE (a4b8af board: nucleo-f411re: create icm426xx development platform)
 	},
 };
 const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
