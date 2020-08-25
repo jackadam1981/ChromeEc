a4b8af57d15111440893759acd5d90bfc21d8c58
Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
2020-08-25 10:29:54 +0000

    board: nucleo-f411re: create icm426xx development platform
    Use icm426xx chip instead of bmi.
    Add SPI slave + interrupt to interface with AP (SPI1 + PA1).
    Add SPI master interface using SPI2 for the sensor (optional).
    Fix board init hook priority to be run after motionsense init.
    Increase task stack sizes.
    
    BUG=chromium:1117541
    BRANCH=None
    TEST=ectool && tast run hardware.SensorRing
    
    Cq-Depend: chromium:2317888 chromium:2374647
    Signed-off-by: Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
    Change-Id: I860b669fd3d2ee196a66e5c80cdc0835b4e5bc73

--- board/nucleo-f411re/board.h
+++ board/nucleo-f411re/board.h
@@ -34,7 +34,18 @@
 #define CONFIG_UART_TX_REQ_CH STM32_REQ_USART2_TX
 #define CONFIG_UART_RX_REQ_CH STM32_REQ_USART2_RX
 
-#define CONFIG_ACCELGYRO_BMI160
+#define CONFIG_ACCELGYRO_ICM426XX
+#define CONFIG_ACCEL_INTERRUPTS
+#define CONFIG_ACCELGYRO_ICM426XX_INT_EVENT \
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
+
+/* Enable sensor fifo, must also define the _SIZE and _THRES */
+#define CONFIG_ACCEL_FIFO
+/* FIFO size is in power of 2. */
+#define CONFIG_ACCEL_FIFO_SIZE 512
+/* Depends on how fast the AP boots and typical ODRs */
+#define CONFIG_ACCEL_FIFO_THRES (CONFIG_ACCEL_FIFO_SIZE / 3)
+
 #define CONFIG_CMD_ACCELS
 #define CONFIG_CMD_ACCEL_INFO
 #define CONFIG_CMD_FLASH
@@ -45,6 +56,13 @@
 #define I2C_PORT_MASTER 1
 #define I2C_PORT_SLAVE 0        /* needed for DMAC macros (ugh) */
 #define I2C_PORT_ACCEL I2C_PORT_MASTER
+
+/* SPI ports configuration */
+#define CONFIG_SPI		/* use hardcoded SPI1 slave on PA4/5/6/7 */
+#define CONFIG_MKBP_EVENT
+#define CONFIG_MKBP_USE_GPIO
+/* #define CONFIG_SPI_MASTER */
+/* #define CONFIG_SPI_ACCEL_PORT 1 */
 
 #ifndef __ASSEMBLER__
 
