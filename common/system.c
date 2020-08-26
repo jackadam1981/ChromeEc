d25704b398f62ce6f000ac5d73e0554751c9bfdd
Scott Collyer <scollyer@google.com>
2020-08-25 17:10:41 -0700

    qsi: Validate FW update via USB EP (2)
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I813da540544dbcf7c53e9d3a87a07c3f04194884

--- common/system.c
+++ common/system.c
@@ -1305,7 +1305,8 @@ static int command_reboot(int argc, char **argv)
 	if (flags & SYSTEM_RESET_WAIT_EXT)
 		ccputs("Waiting for ext reset!\n\n\n");
 	else
-		ccputs("Rebooting!\n\n\n");
+		ccputs("Rebooting! \n\n\n");
+	ccprintf("reboot: flags = 0x%08x\n", flags);
 	cflush();
 
 	system_reset(flags);
