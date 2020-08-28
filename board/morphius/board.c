f97b9b830fc48d1def4f0c4613a2db9099f6b67e
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2020-08-28 16:13:57 +0800

    morphius: fix HDMI_DATA_EN_DB on dali
    This patch fix HDMI_DATA_EN_DB not power on dali sku,
    which will cause MST hub not powered.
    
    BUG=b:150278507
    BRANCH=zork
    TEST=verify MST hub work on S0.
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: Iebd7c610d71efde9dbb3d66d7405f1334084ce97

--- board/morphius/board.c
+++ board/morphius/board.c
@@ -579,11 +579,11 @@ static void board_chipset_resume(void)
 			ioex_set_level(IOEX_HDMI_POWER_EN_DB, 1);
 			msleep(PI3HDX1204_POWER_ON_DELAY_MS);
 		}
-		ioex_set_level(IOEX_HDMI_DATA_EN_DB, 1);
 		pi3hdx1204_enable(I2C_PORT_TCPC1,
 				  PI3HDX1204_I2C_ADDR_FLAGS,
 				  1);
 	}
+	ioex_set_level(IOEX_HDMI_DATA_EN_DB, 1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_chipset_resume, HOOK_PRIO_DEFAULT);
 
@@ -598,8 +598,8 @@ static void board_chipset_suspend(void)
 				  0);
 		if (board_ver >= 3)
 			ioex_set_level(IOEX_HDMI_POWER_EN_DB, 0);
-		ioex_set_level(IOEX_HDMI_DATA_EN_DB, 0);
 	}
+	ioex_set_level(IOEX_HDMI_DATA_EN_DB, 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_chipset_suspend, HOOK_PRIO_DEFAULT);
 
