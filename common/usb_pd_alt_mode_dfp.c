1a7f9e528f7a9376818f49df35354e6a2c9890ac
Ayushee <ayushee.shah@intel.com>
2020-08-28 14:21:18 -0700

    BB retimer: Set UFP mode of BB retimer
    This CL enables getting the enter mode information received from DFP
    and sets the following BB retimer bits accordingly.
    Bit 2: RE_TIMER_DRIVER:
    Bit 18: CABLE_TYPE:
    Bit 20: TBT_ACTIVE_LINK_TRAINING
    Bit 22: ACTIVE/PASSIVE
    Bits 27-25: TBT Cable speed
    Bits 29-28: TBT_GEN_SUPPORT
    
    BUG=b:157163664
    BRANCH=None
    TEST=Tested with volteer as UFP, able to set the retimer bits
    
    Signed-off-by: Ayushee <ayushee.shah@intel.com>
    Change-Id: I661aa4630b42fbaa136ff3855c4f70e3dee61546

--- common/usb_pd_alt_mode_dfp.c
+++ common/usb_pd_alt_mode_dfp.c
@@ -869,6 +869,7 @@ __overridable enum tbt_compat_cable_speed board_get_max_tbt_speed(int port)
 
 	return cable_mode_resp.tbt_cable_speed;
 }
+
 /*
  * ############################################################################
  *
