55f97489354c76ead3f6ae40b39742d551ae6aa1
jerry2.huang <jerry2.huang@lcfc.corp-partner.google.com>
2020-09-03 13:40:15 +0800

    phaser: Add PPC syv682x config
    Supoprt PPC SYV682X on Phaser-R series(sku1~sku4)
    
    BUG=b:160940803
    BRANCH=none
    TEST=make buildall
    
    Change-Id: I7e0c66cc0626eda92985ebc40430039ba2094ae8
    Signed-off-by: jerry2.huang <jerry2.huang@lcfc.corp-partner.google.com>

--- board/phaser/board.c
+++ board/phaser/board.c
@@ -15,6 +15,7 @@
 #include "driver/accel_lis2dh.h"
 #include "driver/accelgyro_lsm6dsm.h"
 #include "driver/ppc/nx20p348x.h"
+#include "driver/ppc/syv682x.h"
 #include "driver/tcpm/anx7447.h"
 #include "extpower.h"
 #include "gpio.h"
@@ -29,6 +30,7 @@
 #include "tcpci.h"
 #include "temp_sensor.h"
 #include "thermistor.h"
+#include "usbc_ppc.h"
 #include "util.h"
 #include "battery_smart.h"
 
@@ -40,15 +42,36 @@
 
 static uint8_t sku_id;
 
+/* Check PPC ID and board version to decide which one ppc is used. */
+static bool support_syv_ppc(void)
+{
+	uint32_t board_version = 0;
+
+	if (cbi_get_board_version(&board_version) != EC_SUCCESS)
+		CPRINTSUSB("Get board version failed.");
+
+	if ((board_version >= 5) && (gpio_get_level(GPIO_PPC_ID)) &&
+		(sku_id <= 4))
+		return true;
+
+	return false;
+}
+
 static void ppc_interrupt(enum gpio_signal signal)
 {
 	switch (signal) {
 	case GPIO_USB_PD_C0_INT_ODL:
-		nx20p348x_interrupt(0);
+		if (support_syv_ppc())
+			syv682x_interrupt(0);
+		else
+			nx20p348x_interrupt(0);
 		break;
 
 	case GPIO_USB_PD_C1_INT_ODL:
-		nx20p348x_interrupt(1);
+		if (support_syv_ppc())
+			syv682x_interrupt(1);
+		else
+			nx20p348x_interrupt(1);
 		break;
 
 	default:
@@ -296,4 +319,41 @@ void board_overcurrent_event(int port, int is_overcurrented)
 
 	/* Note that the level is inverted because the pin is active low. */
 	gpio_set_level(GPIO_USB_C_OC, !is_overcurrented);
+}
+
+const struct ppc_config_t ppc_syv682x_port0 = {
+		.i2c_port = I2C_PORT_TCPC0,
+		.i2c_addr_flags = SYV682X_ADDR0_FLAGS,
+		.drv = &syv682x_drv,
+};
+
+const struct ppc_config_t ppc_syv682x_port1 = {
+		.i2c_port = I2C_PORT_TCPC1,
+		.i2c_addr_flags = SYV682X_ADDR0_FLAGS,
+		.drv = &syv682x_drv,
+};
+
+static void board_setup_ppc(void)
+{
+	if (!support_syv_ppc())
+		return;
+
+	memcpy(&ppc_chips[USB_PD_PORT_TCPC_0],
+		&ppc_syv682x_port0,
+		sizeof(struct ppc_config_t));
+	memcpy(&ppc_chips[USB_PD_PORT_TCPC_1],
+		&ppc_syv682x_port1,
+		sizeof(struct ppc_config_t));
+
+	gpio_set_flags(GPIO_USB_PD_C0_INT_ODL, GPIO_INT_BOTH);
+	gpio_set_flags(GPIO_USB_PD_C1_INT_ODL, GPIO_INT_BOTH);
+}
+DECLARE_HOOK(HOOK_INIT, board_setup_ppc, HOOK_PRIO_INIT_I2C + 2);
+
+int ppc_get_alert_status(int port)
+{
+	if (port == 0)
+		return gpio_get_level(GPIO_USB_PD_C0_INT_ODL) == 0;
+
+	return gpio_get_level(GPIO_USB_PD_C1_INT_ODL) == 0;
 }
