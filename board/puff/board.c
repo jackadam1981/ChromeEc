2083f0c3aadc4e726676a27510e1fb19183e708a
David Huang <david.huang@quanta.corp-partner.google.com>
2020-09-03 15:25:09 +0800

    Puff: Add setup_thermal for setting thermal config
    Use FW_CONFIG to set correct thermal config for different sku.
    
    BUG=b:166696500, b:167477885
    BRANCH=master
    TEST=Thermal team verified thermal policy is expected.
    
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Change-Id: I53a4998da5b01cfa4c69335062a64cbff2433752

--- board/puff/board.c
+++ board/puff/board.c
@@ -52,6 +52,8 @@
 static void power_monitor(void);
 DECLARE_DEFERRED(power_monitor);
 
+static void setup_thermal(void);
+
 static void ppc_interrupt(enum gpio_signal signal)
 {
 	if (signal == GPIO_USB_C0_TCPPC_INT_ODL)
@@ -397,6 +399,19 @@ const static struct ec_thermal_config thermal_a = {
 	.temp_fan_max = C_TO_K(55),
 };
 
+const static struct ec_thermal_config thermal_b = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(78),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(85),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(70),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+};
+
 struct ec_thermal_config thermal_params[] = {
 	[TEMP_SENSOR_CORE] = thermal_a,
 };
@@ -469,6 +484,7 @@ static void board_init(void)
 	if (board_version < 2)
 		button_disable_gpio(GPIO_EC_RECOVERY_BTN_ODL);
 
+	setup_thermal();
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
@@ -670,6 +686,24 @@ int ec_config_get_usb4_present(void)
 unsigned int ec_config_get_thermal_solution(void)
 {
 	return (fw_config & EC_CFG_THERMAL_MASK) >> EC_CFG_THERMAL_L;
+}
+
+static void setup_thermal(void)
+{
+	unsigned int table = ec_config_get_thermal_solution();
+	/* Configure Fan */
+	switch (table) {
+	/* Default and table0 use single fan */
+	case 0:
+	default:
+		thermal_params[TEMP_SENSOR_CORE] = thermal_a;
+		break;
+	/* Table1 is fanless */
+	case 1:
+		fan_set_count(0);
+		thermal_params[TEMP_SENSOR_CORE] = thermal_b;
+		break;
+	}
 }
 
 /*
