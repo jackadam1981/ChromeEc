0693e1a622f4b3f5103b4e01b8428b3e7e73e964
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2020-09-04 18:15:29 +0800

    eldrid: modify CPU halt threshold
    Set EC_TEMP_THRESH_HALT to 85 degree.
    
    BUG=b:
    TEST=
    
    Change-Id: Ifbd12193be12cb454408af0a7bf04a396e575c9a
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>

--- baseboard/volteer/baseboard.c
+++ baseboard/volteer/baseboard.c
@@ -159,7 +159,11 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 const static struct ec_thermal_config thermal_cpu = {
 	.temp_host = {
 		[EC_TEMP_THRESH_HIGH] = C_TO_K(70),
+#ifdef BOARD_ELDRID
+		[EC_TEMP_THRESH_HALT] = C_TO_K(85),
+#else
 		[EC_TEMP_THRESH_HALT] = C_TO_K(80),
+#endif
 	},
 	.temp_host_release = {
 		[EC_TEMP_THRESH_HIGH] = C_TO_K(65),
