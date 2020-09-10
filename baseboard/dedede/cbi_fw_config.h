36c5de777857affcc578761d95910e7b30fe1b85
Leo-Tsai <leocx_tsai@compal.corp-partner.google.com>
2020-09-10 10:20:05 +0800

    dedede: add FW_CONFIG field for UK2 Keyboard support
    This patch add FW_CONFIG field for UK2 Keyboard Type Support
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Signed-off-by: Leo-Tsai <leocx_tsai@compal.corp-partner.google.com>
    Change-Id: Idb9a352f40abab791e3162470efe8fe242027af5

--- baseboard/dedede/cbi_fw_config.h
+++ baseboard/dedede/cbi_fw_config.h
@@ -43,8 +43,19 @@ enum fw_config_tablet_mode_type {
 #define FW_CONFIG_TABLET_MODE_OFFSET		10
 #define FW_CONFIG_TABLET_MODE_MASK		GENMASK(10, 10)
 
+/*
+ * UK2 Keyboard (1 bit)
+ */
+enum fw_config_keyboard_type {
+	KB_UK2_ABSENT = 0,
+	KB_UK2_PRESENT = 1,
+};
+#define FW_CONFIG_KB_UK2_OFFSET		12
+#define FW_CONFIG_KB_UK2_MASK		GENMASK(12, 12)
+
 enum fw_config_db get_cbi_fw_config_db(void);
 enum fw_config_kblight_type get_cbi_fw_config_kblight(void);
 enum fw_config_tablet_mode_type get_cbi_fw_config_tablet_mode(void);
+enum fw_config_keyboard_type get_cbi_fw_config_keyboard(void);
 
 #endif /* _DEDEDE_CBI_FW_CONFIG__H_ */
