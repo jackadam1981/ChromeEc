7f3fda5aedfc59997a8f1254b3a642aa000dc030
Ben Chen <ben.chen2@quanta.corp-partner.google.com>
2020-09-14 13:53:48 +0800

    magolor: add thermal table
    Add setup_thermal for setting thermal table
    
    BUG=b:168353037
    BRANCH=master
    TEST=thermal verify policy
    
    Change-Id: I8b317855d5c15f16fe4d67656b6915269c4756fa
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- board/magolor/board.h
+++ board/magolor/board.h
@@ -11,6 +11,7 @@
 #define VARIANT_DEDEDE_EC_NPCX796FC
 #include "baseboard.h"
 
+#define CONFIG_CHIP_INIT_ROM_REGION
 /*
  * Keep the system unlocked in early development.
  * TODO(b/151264302): Make sure to remove this before production!
@@ -47,6 +48,7 @@
 
 /* Temp sensor */
 #define CONFIG_TEMP_SENSOR
+#define CONFIG_THROTTLE_AP
 #define CONFIG_THERMISTOR_NCP15WB
 #define CONFIG_STEINHART_HART_3V3_51K1_47K_4050B
 
