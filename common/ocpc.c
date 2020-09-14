ca96668f06762bcc652d56f9908c09a3e160ec6e
Aseda Aboagye <aaboagye@google.com>
2020-09-14 15:42:00 -0700

    ocpc: Add visualization graph
    When tuning an OCPC system, the OCPC debug output is very useful in
    order to examine the behaviour of the system.  However, instead of
    simply looking at a bunch of changing numbers on the terminal, it's
    easier to view a waveform of the current entering the battery.  In
    lieu of using an oscilloscope with a current probe, this commit adds a
    visualization graph which shows the current entering the battery
    relative to the target current.  Each segment on the graph represents
    5% of the target current.
    
    Usage is as follows:
    
        > ocpcdebug disable enable
    
    The first setting is for the usual OCPC debug output, the second one
    is for the visualization graph.
    
    Here is an example of the output
    
    [2665.199505 Act Chg: 1]
    [2665.702271 Battery 47% (Display 47.1 %) / ??h:?? to empty]
    [2665.703810 charge_request(13050mV, 1800mA)]
    [2665.725854 OCPC: Target VSYS: 11979mV]
    [----------#----------] (actual)251mA (desired)1880mA
    [2666.317342 OCPC: Target VSYS: 12179mV]
    [----------|---------o] (actual)879mA (desired)1880mA
    [2666.908941 OCPC: Target VSYS: 12274mV]
    [----------|-----o----] (actual)1216mA (desired)1880mA
    [----------|o---------] (actual)1631mA (desired)1880mA
    [2668.090373 OCPC: Target VSYS: 12259mV]
    [----------#----------] (actual)1797mA (desired)1880mA
    [----------#----------] (actual)1813mA (desired)1880mA
    [----------#----------] (actual)1769mA (desired)1880mA
    [----------#----------] (actual)1779mA (desired)1880mA
    
    BUG=None
    BRANCH=None
    TEST=Build and flash waddledee, enable debug command, verify that
    visualization graph is shown.
    
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Change-Id: Ie271524ce2ce90781b966a77f2a7ef27dc969075

--- common/ocpc.c
+++ common/ocpc.c
@@ -31,6 +31,11 @@
 /* Console output macros */
 #define CPUTS(outstr) cputs(CC_CHARGER, outstr)
 #define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
+#define CPRINT_VIZ(format, args...) \
+do {							\
+	if (viz_output)				\
+		cprintf(CC_CHARGER, format, ## args);	\
+} while (0)
 #define CPRINTS_DBG(format, args...) \
 do {							\
 	if (debug_output)				\
@@ -47,6 +52,7 @@ static int k_p_div = KP_DIV;
 static int k_i_div = KI_DIV;
 static int k_d_div = KD_DIV;
 static int debug_output;
+static int viz_output;
 
 enum phase {
 	PHASE_UNKNOWN = -1,
@@ -120,6 +126,7 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	static int iterations;
 	int i_step;
 	static timestamp_t delay;
+	int i, step, loc;
 
 	/*
 	 * There's nothing to do if we're not using this charger.  Should
@@ -331,6 +338,27 @@ set_vsys:
 	charger_set_voltage(CHARGER_SECONDARY, vsys_target);
 	ocpc->last_vsys = vsys_target;
 
+	/*
+	 * Print a visualization graph of the actual current vs. the target.
+	 * Each position represents 5% of the target current.
+	 */
+	if (i_ma != 0) {
+		step = 5 * i_ma / 100;
+		loc = error / step;
+		loc = CLAMP(loc, -10, 10);
+		CPRINT_VIZ("[");
+		for (i = -10; i <= 10; i++) {
+			if ((i == 0) && (loc == 0))
+				CPRINT_VIZ("#");
+			else if (i == 0)
+				CPRINT_VIZ("|");
+			else
+				CPRINT_VIZ(i == loc ? "o" : "-");
+		}
+		CPRINT_VIZ("] (actual)%dmA (desired)%dmA\n", batt.current,
+			   batt.desired_current);
+	}
+
 	return rv;
 }
 
@@ -408,11 +436,16 @@ static int command_ocpcdebug(int argc, char **argv)
 	if (!parse_bool(argv[1], &debug_output))
 		return EC_ERROR_PARAM1;
 
+	if ((argc == 3 ) && (!parse_bool(argv[2], &viz_output)))
+		return EC_ERROR_PARAM2;
+
 	return EC_SUCCESS;
 }
 DECLARE_SAFE_CONSOLE_COMMAND(ocpcdebug, command_ocpcdebug,
-			     "<enable/disable>",
-			     "enable/disable debug prints for OCPC data");
+			     "<enable/disable> <enable/disable>",
+			     "Enable/disable debug prints for OCPC data. "
+			     "1st setting is debug, 2nd is viz graph. "
+			     "Each segment is 5% of current target.");
 
 static int command_ocpcpid(int argc, char **argv)
 {
