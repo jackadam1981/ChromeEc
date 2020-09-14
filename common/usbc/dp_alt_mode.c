6d1c597d1878d52a429785e5ad26edf2bd612338
Nathan K <nkolluru@google.com>
2020-09-14 15:40:17 -0700

    HACK/WIP: Add delay between cable and UFP_D configure
    Change-Id: I6113a00c7cbe4c56834696e331a087958ff186aa

--- common/usbc/dp_alt_mode.c
+++ common/usbc/dp_alt_mode.c
@@ -728,6 +728,7 @@ int dp_setup_next_vdm(int port, int vdo_count, uint32_t *vdm,
 		dp_prints("attempt to config SOP''", port);
 		break;
 	case DP_CONFIG_SOP:
+		usleep(200000);
 		modep = pd_get_amode_data(port,
 						TCPC_TX_SOP, USB_SID_DISPLAYPORT);
 		if (!(modep && modep->opos))
