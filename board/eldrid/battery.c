af5da78ce3355b3183c15e4e5c019e317e3c5af9
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2020-09-14 17:24:41 +0800

    eldrid: fix battery cutoff
    Eldrid's battery need to apply charging voltage input>3V and aleast 2 sec to let C/D FET will turn on.
    
    So move battery_check_present_status() to board/eldrid/battery.c and try to provide pre-charge current to battery when battery have no response.
    
    BUG=b:165780074
    BRANCH=none
    TEST=make -j BOARD=eldrid
    TEST=make buildall
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I4265538b5a789b76a85d21a1459d19142a86106e

--- board/eldrid/battery.c
+++ board/eldrid/battery.c
@@ -6,8 +6,11 @@
  */
 
 #include "battery_fuel_gauge.h"
+#include "battery_smart.h"
 #include "common.h"
 #include "util.h"
+
+static enum battery_present batt_pres_prev = BP_NOT_SURE;
 
 /*
  * Battery info for all Volteer battery types. Note that the fields
@@ -92,3 +95,66 @@ const struct board_batt_params board_battery_info[] = {
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
 const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_SIMPLO_HIGHPOWER;
+
+static bool battery_init(void)
+{
+	int batt_status;
+
+	return battery_status(&batt_status) ? 0 :
+		!!(batt_status & STATUS_INITIALIZED);
+}
+
+static int battery_check_disconnect(void)
+{
+	if (!battery_init())
+		return BATTERY_DISCONNECT_ERROR;
+
+	return BATTERY_NOT_DISCONNECTED;
+}
+
+/*
+ * Physical detection of battery.
+ */
+__override enum battery_present battery_check_present_status(void)
+{
+	enum battery_present batt_pres;
+	int batt_disconnect_status;
+
+	/* Get the physical hardware status */
+	batt_pres = battery_hw_present();
+
+	/*
+	 * If the battery is not physically connected, then no need to perform
+	 * any more checks.
+	 */
+	if (batt_pres != BP_YES)
+		return batt_pres;
+
+	/*
+	 * If the battery is present now and was present last time we checked,
+	 * return early.
+	 */
+	if (batt_pres == batt_pres_prev)
+		return batt_pres;
+
+	/*
+	 * Check battery disconnect status. If we are unable to read battery
+	 * disconnect status, then return BP_NOT_SURE. Battery could be in ship
+	 * mode and might require pre-charge current to wake it up. BP_NO is not
+	 * returned here because charger state machine will not provide
+	 * pre-charge current assuming that battery is not present.
+	 */
+	batt_disconnect_status = battery_check_disconnect();
+	if (batt_disconnect_status == BATTERY_DISCONNECT_ERROR)
+		return BP_NOT_SURE;
+
+	/*
+	 * Ensure that battery is:
+	 * 1. Not in cutoff
+	 * 2. Initialized
+	 */
+	if (battery_is_cut_off() || !battery_init())
+		batt_pres = BP_NO;
+
+	return batt_pres;
+}
