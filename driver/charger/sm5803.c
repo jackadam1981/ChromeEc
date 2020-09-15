38625d891b44a4c4c5929cd47087f15109c99b3b
Divagar Mohandass <divagar.mohandass@intel.com>
2020-09-15 07:16:55 +0000

    SM5803: add suspend/resume function for S0ix state
    add function to turn on/off features in SM5803 charger
    during S0ix state to save power. we are seeing ~12mW power
    saving per charger.
    
    BRANCH=None
    BUG=b:165030094
    TEST=Check power saving in S0ix state and check charger and device
    functionality after resume.
    
    Signed-off-by: Divagar Mohandass <divagar.mohandass@intel.com>
    Change-Id: I3ea32219040263f0abef8b9dd4c52edb31289fd7

--- driver/charger/sm5803.c
+++ driver/charger/sm5803.c
@@ -702,6 +702,84 @@ void sm5803_hibernate(int chgnum)
 		CPRINTS("%s %d: Failed to set hibernate", CHARGER_NAME, chgnum);
 }
 
+void sm5803_resume(int chgnum)
+{
+	enum ec_error_list rv;
+	int reg;
+
+	rv = main_read8(chgnum, SM5803_REG_REFERENCE, &reg);
+	if (rv) {
+		CPRINTS("%s %d: Failed to read REFERENCE reg", CHARGER_NAME,
+			chgnum);
+		return;
+	}
+	/* Set a higher clock speed */
+	rv |= main_read8(chgnum, SM5803_REG_CLOCK_SEL, &reg);
+	reg &= ~SM5803_CLOCK_SEL_LOW;
+	rv |= main_write8(chgnum, SM5803_REG_CLOCK_SEL, reg);
+
+	/* Turn on GPADCs to default */
+	rv |= meas_write8(chgnum, SM5803_REG_GPADC_CONFIG1, 0xF3);
+
+	/* Enable Psys DAC */
+	rv |= meas_read8(chgnum, SM5803_REG_PSYS1, &reg);
+	reg |= SM5803_PSYS1_DAC_EN;
+	rv |= meas_write8(chgnum, SM5803_REG_PSYS1, reg);
+
+	/* Enable ADC sigma delta */
+	rv |= chg_read8(chgnum, SM5803_REG_CC_CONFIG1, &reg);
+	reg |= SM5803_CC_CONFIG1_SD_PWRUP;
+	rv |= chg_write8(chgnum, SM5803_REG_CC_CONFIG1, reg);
+
+	/* Enable PROCHOT comparators except Ibus */
+	rv |= chg_read8(chgnum, SM5803_REG_PHOT1, &reg);
+	reg |= SM5803_PHOT1_COMPARATOR_EN;
+	reg &= ~SM5803_PHOT1_IBUS_PHOT_COMP_EN;
+	rv |= chg_write8(chgnum, SM5803_REG_PHOT1, reg);
+
+	if (rv)
+		CPRINTS("%s %d: Failed to set resume", CHARGER_NAME, chgnum);
+}
+
+void sm5803_suspend(int chgnum)
+{
+	enum ec_error_list rv;
+	int reg;
+
+	rv = main_read8(chgnum, SM5803_REG_REFERENCE, &reg);
+	if (rv) {
+		CPRINTS("%s %d: Failed to read REFERENCE reg", CHARGER_NAME,
+			chgnum);
+		return;
+	}
+	/* Slow the clock speed */
+	rv |= main_read8(chgnum, SM5803_REG_CLOCK_SEL, &reg);
+	reg |= SM5803_CLOCK_SEL_LOW;
+	rv |= main_write8(chgnum, SM5803_REG_CLOCK_SEL, reg);
+
+	/* Turn off GPADCs */
+	rv |= meas_write8(chgnum, SM5803_REG_GPADC_CONFIG1, 0);
+	rv |= meas_write8(chgnum, SM5803_REG_GPADC_CONFIG2, 0);
+
+	/* Disable Psys DAC */
+	rv |= meas_read8(chgnum, SM5803_REG_PSYS1, &reg);
+	reg &= ~SM5803_PSYS1_DAC_EN;
+	rv |= meas_write8(chgnum, SM5803_REG_PSYS1, reg);
+
+	/* Disable ADC sigma delta */
+	rv |= chg_read8(chgnum, SM5803_REG_CC_CONFIG1, &reg);
+	reg &= ~SM5803_CC_CONFIG1_SD_PWRUP;
+	rv |= chg_write8(chgnum, SM5803_REG_CC_CONFIG1, reg);
+
+	/* Disable PROCHOT comparators */
+	rv |= chg_read8(chgnum, SM5803_REG_PHOT1, &reg);
+	reg &= ~SM5803_PHOT1_COMPARATOR_EN;
+	rv |= chg_write8(chgnum, SM5803_REG_PHOT1, reg);
+
+	if (rv)
+		CPRINTS("%s %d: Failed to set suspend", CHARGER_NAME, chgnum);
+}
+
 /*
  * Process interrupt registers and report any Vbus changes.  Alert the AP if the
  * charger has become too hot.
