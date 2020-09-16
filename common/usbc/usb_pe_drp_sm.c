8bee2c852b0ee92cd88c51f8abcfbe7fb83741aa
Abe Levkoy <alevkoy@chromium.org>
2020-09-16 12:41:15 -0600

    TCPMv2: Move DPM requests to public API
    It is safe (atomic bit ops) and will soon be necessary (new host
    commands) to send DPM requests from outside the PD tasks. Rename
    pe_dpm_request to pd_dpm_request and move the declarations into usb_pd.h
    to reflect this.
    
    BUG=b:168030639
    TEST=make buildall
    BRANCH=none
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: Ied43e9f6973a3172b98090cc068a607257dce21b

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -1142,7 +1142,7 @@ void pe_got_soft_reset(int port)
 	set_state_pe(port, PE_SOFT_RESET);
 }
 
-void pe_dpm_request(int port, enum pe_dpm_request req)
+void pd_dpm_request(int port, enum pd_dpm_request req)
 {
 	PE_SET_DPM_REQUEST(port, req);
 }
@@ -1285,6 +1285,10 @@ static bool common_src_snk_dpm_requests(int port)
 		pe_set_dpm_curr_request(port, DPM_REQUEST_ENTER_USB);
 		set_state_pe(port, PE_DEU_SEND_ENTER_USB);
 		return true;
+	} else if (PE_CHK_DPM_REQUEST(port, DPM_REQUEST_EXIT_MODES)) {
+		pe_set_dpm_curr_request(port, DPM_REQUEST_EXIT_MODES);
+		dpm_set_mode_exit_request(port);
+		return true;
 	}
 	return false;
 }
@@ -1417,7 +1421,7 @@ static void pe_update_pdo_flags(int port, uint32_t pdo)
 
 void pd_request_power_swap(int port)
 {
-	pe_dpm_request(port, DPM_REQUEST_PR_SWAP);
+	pd_dpm_request(port, DPM_REQUEST_PR_SWAP);
 }
 
 int pd_is_port_partner_dualrole(int port)
@@ -2085,7 +2089,7 @@ static void pe_src_transition_supply_run(int port)
 			 * PR_Swap
 			 */
 			if (pd_get_src_cap_cnt(port) == 0)
-				pe_dpm_request(port, DPM_REQUEST_GET_SRC_CAPS);
+				pd_dpm_request(port, DPM_REQUEST_GET_SRC_CAPS);
 
 			set_state_pe(port, PE_SRC_READY);
 		} else {
@@ -2771,7 +2775,7 @@ static void pe_snk_select_capability_run(int port)
 				 * request Sink Capabilities for possible FRS
 				 */
 				if (IS_ENABLED(CONFIG_USB_PD_FRS))
-					pe_dpm_request(port,
+					pd_dpm_request(port,
 						DPM_REQUEST_GET_SNK_CAPS);
 				return;
 			}
