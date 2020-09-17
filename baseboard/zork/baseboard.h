b62e42d2903d59e17404d663c41c9eda5baf6e82
Edward Hill <ecgh@chromium.org>
2020-09-16 19:51:48 -0600

    zork: Adjust AP power limits
    Adjust EC config settings that control how much battery charge and
    AC power is required to allow AP to boot, to align with how much
    power the HW actually needs.
    
    EC will boot AP to depthcharge if:
    (BAT >= 4%) || (AC >= 50W) || (BAT >= 1% && AC >= 15W)
    
    Depthcharge will boot OS if:
    (BAT >= 4%) || (AC >= 50W)
    
    BUG=b:162938720
    BRANCH=zork
    TEST=boot with various battery and AC levels
    
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Change-Id: I78aa84b8b0b18429d1301085ab878c62a4afd687

--- baseboard/zork/baseboard.h
+++ baseboard/zork/baseboard.h
@@ -211,18 +211,20 @@
 #define ZORK_AC_PROCHOT_CURRENT_MA 3328
 
 /*
- * Minimum conditions to start AP and perform swsync.  Note that when the
- * charger is connected via USB-PD analog signaling, the boot will proceed
- * regardless.
+ * EC will boot AP to depthcharge if:
+ * (BAT >= 4%) || (AC >= 50W) || (BAT >= 1% && AC >= 15W)
  */
-#define CONFIG_CHARGER_MIN_BAT_PCT_FOR_POWER_ON 3
+#define CONFIG_CHARGER_MIN_BAT_PCT_FOR_POWER_ON			4
+#define CONFIG_CHARGER_MIN_POWER_MW_FOR_POWER_ON		50000
+#define CONFIG_CHARGER_MIN_BAT_PCT_FOR_POWER_ON_WITH_AC		1
+#define CONFIG_CHARGER_MIN_POWER_MW_FOR_POWER_ON_WITH_BATT	15000
 
 /*
- * Require PD negotiation to be complete when we are in a low-battery condition
- * prior to releasing depthcharge to the kernel.
+ * Depthcharge will boot OS if:
+ * (BAT >= 4%) || (AC >= 50W)
  */
-#define CONFIG_CHARGER_LIMIT_POWER_THRESH_CHG_MW 15001
-#define CONFIG_CHARGER_LIMIT_POWER_THRESH_BAT_PCT 3
+#define CONFIG_CHARGER_LIMIT_POWER_THRESH_BAT_PCT		4
+#define CONFIG_CHARGER_LIMIT_POWER_THRESH_CHG_MW		50000
 
 /* Increase length of history buffer for port80 messages. */
 #undef CONFIG_PORT80_HISTORY_LEN
