e2e6ed0f3c0dff15d595f149cfb3500c31064084
Nathan K <nkolluru@google.com>
2020-09-17 19:23:09 -0700

    ServoV4p1: Cleanup TUSB1064 mux driver
    This commit is part of a series to clean up ServoV4p1 code.
    
    TUSB1064 driver is modified  to emulate USB_MUX driver format.
    Note that this is a UFP/RX Equalizer, thus mux functions may
    be called inappropriately.
    
    BUG=b/168621142
    BRANCH=none
    TEST=Compile multi patch series. Look for proper SSUSB mux
    function when using REV0 -> REV1 boards.
    
    Change-Id: Ie15d246ba7b6601db0d387e8ffda54f0ec36ba99

--- board/servo_v4p1/build.mk
+++ board/servo_v4p1/build.mk
@@ -17,8 +17,9 @@ test-list-y=
 board-y=board.o tca6416a.o tca6424a.o
 board-y+=ioexpanders.o
 board-y+=dacs.o
-board-y+=tusb1064.o
 board-y+=pi3usb9201.o
+# TODO: This needs be properly coded as Driver
+board-y+=tusb1064.o
 
 # These files are compiled into RO only
 board-ro+=ccd_measure_sbu.o
