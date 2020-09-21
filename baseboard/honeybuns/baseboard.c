1208d166b3a9a2accdd5794a9af3ad2a8fbf326a
Scott Collyer <scollyer@google.com>
2020-09-21 10:30:44 -0700

    quiche: changes for gpio/i2c for P1 board (disabled)
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I5f29a893b7d50666ee6b92683d051ffe399f11c7

--- baseboard/honeybuns/baseboard.c
+++ baseboard/honeybuns/baseboard.c
@@ -38,7 +38,8 @@ static void board_power_sequence(void)
 	for(i = 0; i < board_power_seq_count; i++) {
 		gpio_set_level(board_power_seq[i].signal,
 			       board_power_seq[i].level);
-		msleep(board_power_seq[i].delay_ms);
+		if (board_power_seq[i].delay_ms)
+			msleep(board_power_seq[i].delay_ms);
 	}
 }
 
@@ -46,7 +47,9 @@ static void board_power_sequence(void)
 /* I2C port map configuration */
 const struct i2c_port_t i2c_ports[] = {
 	{"i2c1",  I2C_PORT_I2C1,  400, GPIO_EC_I2C1_SCL, GPIO_EC_I2C1_SDA},
+#ifndef QUICHE_BOARD_P1
 	{"i2c2",  I2C_PORT_I2C2,  400, GPIO_EC_I2C2_SCL, GPIO_EC_I2C2_SDA},
+#endif
 	{"i2c3",  I2C_PORT_I2C3,  400, GPIO_EC_I2C3_SCL, GPIO_EC_I2C3_SDA},
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
