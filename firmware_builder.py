51d061bbda4e246fb549d56a84e575fe547912f6
Jett Rink <jettrink@chromium.org>
2020-09-22 15:03:04 -0600

    build: add build_and_test.py entry point for builder
    New ToT firmware builder will call build_and_test.py to start the build
    process for FW. For EC, all we need to do at the moment is call `make
    buildall`
    
    BRANCH=none
    BUG=b:169178847
    TEST=run build_and_test.py locally and see that it builds all EC images and
    also redirects stdout and stderror from sub command to buildall.py
    process
    
    Signed-off-by: Jett Rink <jettrink@chromium.org>
    Change-Id: Idd039e686697ee88419e0e44aa3dc96d554b997d

---
+++ firmware_builder.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Build and test all of the EC boards.
+
+This is the entry point for the custom firmware builder workflow recipe.
+"""
+
+import argparse
+import multiprocessing
+import os
+import subprocess
+import sys
+
+
+def _build(opts):
+    return subprocess.run(['make', 'buildall_only', '-j{}'.format(opts.cpus)],
+                          cwd=os.path.dirname(__file__)).returncode
+
+
+def _test(opts):
+    return subprocess.run(['make', 'runtests', '-j{}'.format(opts.cpus)],
+                          cwd=os.path.dirname(__file__)).returncode
+
+
+def _metrics(opts):
+    # TODO: Add metrics output
+    return subprocess.run(['touch', '{}'.format(opts.file)],
+                          cwd=os.path.dirname(__file__)).returncode
+
+
+def main(args):
+    """Builds all of the EC targets"""
+    opts = parse_args(args)
+
+    if opts.sub == 'build':
+        return _build(opts)
+    elif opts.sub == 'test':
+        return _test(opts)
+    elif opts.sub == 'metrics':
+        return _metrics(opts)
+    else:
+        print("Must select a valid sub command!")
+        return -1
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '--cpus',
+        default=multiprocessing.cpu_count(),
+        help='The number of cores to use.',
+    )
+
+    # Would make this required=True, but not available until 3.7
+    sub_cmds = parser.add_subparsers(dest='sub')
+
+    build_cmd = sub_cmds.add_parser('build', help='Builds all firmware')
+
+    test_cmd = sub_cmds.add_parser('test', help='Runs all firmware unit tests')
+
+    metrics_cmd = sub_cmds.add_parser(
+        'metrics', help='Write all firmware build metrics to file')
+
+    metrics_cmd.add_argument(
+        '--file',
+        dest='file',
+        required=True,
+        help='File to write a json-encoded TODO proto Message.',
+    )
+
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
