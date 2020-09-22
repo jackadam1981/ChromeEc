bdcf5a7e9b59befc70ffff271649162af731c5ad
Jett Rink <jettrink@chromium.org>
2020-09-22 15:03:04 -0600

    build: add build_and_test.py entry point for builder
    New ToT firmware builder will call build_and_test.py to start the build
    process for FW. For EC, all we need to do at the moment is call `make
    buildall`
    
    BRANCH=none
    BUG=b:169178847
    TEST=run build_and_test.py locally and see that it builds all EC images and
    also redirects stdout and stderror from sub command to buildall.py
    process
    
    Signed-off-by: Jett Rink <jettrink@chromium.org>
    Change-Id: Idd039e686697ee88419e0e44aa3dc96d554b997d

---
+++ build_and_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Build and test all of the EC boards.
+
+This is the entry point for the custom firmware builder workflow recipe.
+"""
+
+import argparse
+import multiprocessing
+import os
+import subprocess
+import sys
+
+
+def main(args):
+    """Builds all of the EC targets"""
+    opts = parse_args(args)
+
+    result = subprocess.run(['make', 'buildall', '-j{}'.format(opts.cpus)],
+                            cwd=os.path.dirname(__file__))
+    return result.returncode
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '--cpus',
+        dest='cpus',
+        default=multiprocessing.cpu_count(),
+        help='The number of cores to use.',
+    )
+
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
