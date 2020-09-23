fab8065734593497f00b951e1fd50fd862bb286e
Scott Collyer <scollyer@google.com>
2020-09-23 14:15:49 -0700

    quiche: Adding tcpmv2.0 support and SRC mode working
    This CL enables usb-pd support for quiche
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I923116f5be06021540a1db2b747a735b6789aff4

--- baseboard/honeybuns/baseboard.h
+++ baseboard/honeybuns/baseboard.h
@@ -40,7 +40,6 @@
 #define CONFIG_BOARD_VERSION_CBI
 #define CONFIG_CMD_CBI
 
-
 /* USB Type C and USB PD defines */
 #define CONFIG_USB_POWER_DELIVERY
 #define CONFIG_USB_PD_TCPMV2
@@ -48,11 +47,11 @@
 /* No AP on any honeybuns variants */
 #undef CONFIG_USB_PD_HOST_CMD
 
-/* TODO(b/167711550): Temporarily support type-c mode only */
-#undef CONFIG_USB_PRL_SM
-#undef CONFIG_USB_PE_SM
-
+#define CONFIG_USB_PD_ALT_MODE
+#define CONFIG_USB_PD_ALT_MODE_DFP
+#define CONFIG_USB_PD_CUSTOM_PDO
 #define CONFIG_USB_PD_DUAL_ROLE
+/* #define CONFIG_USB_PD_MAX_SINGLE_SOURCE_CURRENT TYPEC_RP_3A0 */
 #define CONFIG_USB_PD_PORT_MAX_COUNT 1
 #define CONFIG_USB_PD_TCPM_MUX
 #define CONFIG_USB_PD_TCPM_STM32GX
@@ -73,7 +72,6 @@
 #define CONFIG_USBC_SS_MUX
 #define CONFIG_USB_MUX_VIRTUAL
 
-
 /* Define typical operating power and max power. */
 #define PD_MAX_VOLTAGE_MV     20000
 #define PD_MAX_CURRENT_MA     3000
@@ -91,9 +89,13 @@
 /* I2C Bus Configuration */
 #define CONFIG_I2C
 #define CONFIG_I2C_CONTROLLER
+
 #define I2C_PORT_USBC		0
 #define I2C_PORT_MST		1
 #define I2C_PORT_EEPROM	2
+#define I2C_PORT_MP4245 I2C_PORT_MST
+
+#define MP4245_SLAVE_ADDR MP4245_I2C_ADDR_0_FLAGS
 
 /*
  * Macros for GPIO signals used in common code that don't match the
--- driver/mp4245.h
+++ driver/mp4245.h
@@ -50,6 +50,7 @@
 #define MP4245_VOUT_TO_MV(v)         ((v * 1000) / MP4245_VOUT_1V)
 #define MP4245_IOUT_TO_MA(i)         (((i & 0x7ff) * 1000) / BIT(6))
 #define MP4245_ILIM_STEP_MA          50
+#define MP4245_VOUT_5V_DELAY_MS      10
 
 /**
  * MP4245 set output voltage level
