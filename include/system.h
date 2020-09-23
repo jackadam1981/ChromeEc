387ae2cf39c976646b7e773b228cf5797d97f93c
Jack Rosenthal <jrosenth@chromium.org>
2020-09-23 08:16:57 -0600

    common: guard usages of atomic_* accessible by timer.c away from Zephyr
    These need guarded in Zephyr code until they are renamed.  See the bug
    for further details.
    
    BUG=b:169151160
    BRANCH=none
    TEST=compile timer.c in follow-up CL
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I49fa1afc28790ab14a91d472141a01e2370b24ac

--- include/system.h
+++ include/system.h
@@ -495,6 +495,11 @@ extern uint32_t sleep_mask;
 #endif
 #define LOW_SPEED_DEEP_SLEEP_ALLOWED (!(sleep_mask & 0xffff0000))
 
+/*
+ * TODO(b:169151160): usage of atomic_* functions in Zephyr code needs
+ * guarded away until we get the functions renamed.
+ */
+#ifndef CONFIG_ZEPHYR
 /**
  * Enable low power sleep mask. For low power sleep to take affect, all masks
  * in the sleep mask enum above must be enabled.
@@ -516,6 +521,7 @@ static inline void disable_sleep(uint32_t mask)
 {
 	atomic_or(&sleep_mask, mask);
 }
+#endif  /* CONFIG_ZEPHYR */
 
 #ifdef CONFIG_LOW_POWER_IDLE_LIMITED
 /*
---
+++ zephyr/shim/include/atomic.h
@@ -0,0 +1,11 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_ATOMIC_H
+#define __CROS_EC_ATOMIC_H
+
+/* TODO(jrosenth): how to handle atomic variables?  Zephyr APIs differ. */
+
+#endif  /* __CROS_EC_ATOMIC_H */
