f531d01ae66bbaebdec4a020030de312f0eade88
Caveh Jalali <caveh@chromium.org>
2020-09-23 14:06:53 -0700

    usb: Remove references to usb23
    The TCSS port mapping is not used by the EC and the higher layers no
    longer query the EC for this mapping. We can remove this feature and
    disable CONFIG_INTEL_VIRTUAL_MUX which was added to enable it.
    
    BRANCH=none
    BUG=b:153941950
    TEST=buildall passes
    
    Change-Id: I2d7f51212f3e64d74827d7f82654eb93534b8db4
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- board/halvor/board.c
+++ board/halvor/board.c
@@ -340,7 +340,6 @@ struct tcpc_config_t tcpc_config[] = {
 			.addr_flags = TUSB422_I2C_ADDR_FLAGS,
 		},
 		.drv = &tusb422_tcpm_drv,
-		.usb23 = USBC_PORT_0_USB2_NUM | (USBC_PORT_0_USB3_NUM << 4),
 	},
 	[USBC_PORT_C1] = {
 		.bus_type = EC_BUS_TYPE_I2C,
@@ -349,7 +348,6 @@ struct tcpc_config_t tcpc_config[] = {
 			.addr_flags = TUSB422_I2C_ADDR_FLAGS,
 		},
 		.drv = &tusb422_tcpm_drv,
-		.usb23 = USBC_PORT_1_USB2_NUM | (USBC_PORT_1_USB3_NUM << 4),
 	},
 	[USBC_PORT_C2] = {
 		.bus_type = EC_BUS_TYPE_I2C,
@@ -358,7 +356,6 @@ struct tcpc_config_t tcpc_config[] = {
 			.addr_flags = TUSB422_I2C_ADDR_FLAGS,
 		},
 		.drv = &tusb422_tcpm_drv,
-		.usb23 = USBC_PORT_2_USB2_NUM | (USBC_PORT_2_USB3_NUM << 4),
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(tcpc_config) == USBC_PORT_COUNT);
