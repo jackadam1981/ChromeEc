f61c5c8da0d1de50d6dd1d6f0c1d2bb2815ce73f
Ruibin Chang <ruibin.chang@ite.com.tw>
2020-09-23 18:01:04 +0800

    WIP driver/it83xx, it8xxx2: avoid CCs open when TCPC init
    Between we disconnect Rd_DB and set USBPD_START(port),
    CCs are open state for few micro seconds , so I change
    the flow to avoid CCs temporarily open state.
    
    BUG=none
    BRANCH=none
    TEST=on board asurada and reef_it8320, check --
    
    Signed-off-by: Ruibin Chang <ruibin.chang@ite.com.tw>
    Change-Id: I15656cf41c5c123efbb7b7c6ee56a5e74b8a21cb

--- driver/tcpm/it83xx.c
+++ driver/tcpm/it83xx.c
@@ -421,7 +421,6 @@ static void it83xx_init(enum usbpd_port port, int role)
 	 */
 	IT83XX_USBPD_TCDCR(port) = USBPD_REG_PLUG_IN_OUT_DETECT_STAT;
 #endif
-	IT83XX_USBPD_CCPSR(port) = 0xff;
 	/* cc connect */
 	IT83XX_USBPD_CCCSR(port) = 0;
 	/* disable vconn */
@@ -434,6 +433,9 @@ static void it83xx_init(enum usbpd_port port, int role)
 	task_clear_pending_irq(usbpd_ctrl_regs[port].irq);
 	task_enable_irq(usbpd_ctrl_regs[port].irq);
 	USBPD_START(port);
+	/* Disconnect CC with 5.1K DB resister to GND */
+	IT83XX_USBPD_CCPSR(port) |= (USBPD_REG_MASK_DISCONNECT_5_1K_CC2_DB |
+				     USBPD_REG_MASK_DISCONNECT_5_1K_CC1_DB);
 }
 
 static void it83xx_select_polarity(enum usbpd_port port,
