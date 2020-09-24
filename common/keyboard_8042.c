bc35fe85c06899e8214cf8307e472f67d5222b19
Josie Nordrum <JosieNordrum@google.com>
2020-09-24 17:08:41 -0600

    Keyboard: Add AP wakeup to keyboard interrupt
    Wake up AP for PS2 interrupt.
    
    BUG=b:160345665
    BRANCH=None
    TEST=None
    
    Signed-off-by: Josie Nordrum <josienordrum@google.com>
    Change-Id: Ia431525ee8f572922c8f9bfe613d44e83308d9f8

--- common/keyboard_8042.c
+++ common/keyboard_8042.c
@@ -9,6 +9,7 @@
 #include "button.h"
 #include "common.h"
 #include "console.h"
+#include "device_event.h"
 #include "hooks.h"
 #include "host_command.h"
 #include "i8042_protocol.h"
@@ -957,6 +958,10 @@ static void send_aux_data_to_host_deferred(void)
 {
 	uint8_t data;
 
+	#ifdef CONFIG_DEVICE_EVENT
+	if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND))
+		device_set_single_event(EC_DEVICE_EVENT_TRACKPAD);
+	#endif
 	while (!queue_is_empty(&aux_to_host_queue)) {
 		queue_remove_unit(&aux_to_host_queue, &data);
 		if (aux_chan_enabled && IS_ENABLED(CONFIG_8042_AUX))
