dbfe89871b0b06e93705a87c6b4b0db2b6dcd705
Ting Shen <phoenixshen@google.com>
2020-09-24 16:57:00 +0800

    crux: initial commit
    BUG=b:167884598
    TEST=make BOARD=crux
    BRANCH=master
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I0b3542bff38756c07e5073672f0d485cd6a9d0c8

--- board/hammer/variants.h
+++ board/hammer/variants.h
@@ -13,7 +13,10 @@
 #define __CROS_EC_VARIANTS_H
 
 /* USB ID */
-#ifdef BOARD_HAMMER
+#if defined(BOARD_CRUX)
+/* TODO: update PID */
+#define CONFIG_USB_PID 0x5022
+#elif defined(BOARD_HAMMER)
 #define CONFIG_USB_PID 0x5022
 #elif defined(BOARD_MAGNEMITE)
 #define CONFIG_USB_PID 0x503d
@@ -34,7 +37,16 @@
 #ifdef SECTION_IS_RW
 
 /* Touchpad interface, firmware size and physical dimension. */
-#if defined(BOARD_HAMMER) || defined(BOARD_WAND)
+#if defined(BOARD_CRUX)
+#define HAS_I2C_TOUCHPAD
+#define CONFIG_USB_HID_TOUCHPAD_LOGICAL_MAX_X 3207
+#define CONFIG_USB_HID_TOUCHPAD_LOGICAL_MAX_Y 1783
+#define CONFIG_USB_HID_TOUCHPAD_LOGICAL_MAX_PRESSURE 511
+#define CONFIG_USB_HID_TOUCHPAD_PHYSICAL_MAX_X 1018 /* tenth of mm */
+#define CONFIG_USB_HID_TOUCHPAD_PHYSICAL_MAX_Y 566 /* tenth of mm */
+#define CONFIG_TOUCHPAD_VIRTUAL_SIZE (48*1024)
+/* TODO: update correct parameters */
+#elif defined(BOARD_HAMMER) || defined(BOARD_WAND)
 #define HAS_I2C_TOUCHPAD
 #define CONFIG_USB_HID_TOUCHPAD_LOGICAL_MAX_X 3207
 #define CONFIG_USB_HID_TOUCHPAD_LOGICAL_MAX_Y 1783
