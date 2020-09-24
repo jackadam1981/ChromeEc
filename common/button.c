e106f1cb74f143db77a8814d1191c18a00c4838f
Dawid Niedzwiecki <dn@semihalf.com>
2020-09-24 09:20:41 +0200

    core: rename atomic_clear to atomic_clear_bit
    Change the name of atomic_clear to atomic_clear_bit to make to name more
    clear - the function clears only selected bits, but the name may suggest
    that it clears the whole variable.
    
    It is done as a part of porting to Zephyr, where atomic_clear_bit clears
    only selected bits and atomic_clear zeros the variable.
    
    BUG=b:169151160
    BRANCH=none
    TEST=buildall
    
    Signed-off-by: Dawid Niedzwiecki <dn@semihalf.com>
    Change-Id: I7b0b47959c6c54af40f61bca8d9baebaa0375970

--- common/button.c
+++ common/button.c
@@ -381,8 +381,9 @@ static void simulate_button_release_deferred(void)
 		/* Check state for button pressed */
 		if (sim_button_state & BIT(buttons[button_idx].type)) {
 			/* Set state of the button as released */
-			deprecated_atomic_clear(&sim_button_state,
-						BIT(buttons[button_idx].type));
+			deprecated_atomic_clear_bit(
+				&sim_button_state,
+				BIT(buttons[button_idx].type));
 
 			button_interrupt_simulate(button_idx);
 		}
