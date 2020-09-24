808474f7ae2eebd4089aea047db249512c24dd2e
Scott Collyer <scollyer@google.com>
2020-09-23 21:22:26 -0700

    honeybuns: Add function to apply Rd in RO image
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I1be541cef24c185203fdb9e5f72e0ce2d4415d3b

--- baseboard/honeybuns/baseboard.c
+++ baseboard/honeybuns/baseboard.c
@@ -53,11 +53,38 @@ const struct i2c_port_t i2c_ports[] = {
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
+#ifndef SECTION_IS_RW
+static void baseboard_set_usbc_sink_mode(void)
+{
+	uint32_t cr;
+
+	/*
+	 * Bare minimum code required to enable UCPD peripheral and apply Rd to
+	 * the CC lines. This is only applied in RO.
+	 */
+	/* Ensure that clock to UCPD is enabled */
+	STM32_RCC_APB1ENR2 |= STM32_RCC_APB1ENR2_UPCD1EN;
+	/* enable the peripheral */
+	STM32_UCPD_CFGR1(0) |= STM32_UCPD_CFGR1_UCPDEN;
+
+	/* Apply Rd to both CC lines */
+	cr = STM32_UCPD_CR(0);
+	cr |= STM32_UCPD_CR_ANAMODE | STM32_UCPD_CR_CCENABLE_MASK;
+	STM32_UCPD_CR(0) = cr;
+
+	CPRINTS("usbc: CR = 0x%x", STM32_UCPD_CR(0));
+
+}
+#endif
+
 static void baseboard_init(void)
 {
 	/* Turn on power rails */
 	board_power_sequence();
 	CPRINTS("board: Power rails enabled");
+#ifndef SECTION_IS_RW
+	baseboard_set_usbc_sink_mode();
+#endif
 }
 DECLARE_HOOK(HOOK_INIT, baseboard_init, HOOK_PRIO_DEFAULT);
 
