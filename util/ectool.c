ab36ed788678645dbc868157a3ca6e32422db88f
Diana Z <dzigterman@chromium.org>
2020-09-25 14:56:35 -0600

    Ectool: Support printing typec status on older boards
    When boards don't have the new TYPEC_STATUS host command running,
    fallback to the older USB_PD_CONTROL command to display information.
    
    BRANCH=None
    BUG=None
    TEST=on waddledoo with no TYPEC_STATUS command present, ensure that
    "ectool typecstatus <port>" prints port information
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: I78fbc0414c0c6cb73b91285b6900a34287cdf5dd

--- util/ectool.c
+++ util/ectool.c
@@ -9582,7 +9582,7 @@ int cmd_typec_status(int argc, char *argv[])
 	char *endptr;
 	int rv;
 
-	if (argc < 2) {
+	if (argc != 2) {
 		fprintf(stderr,
 			"Usage: %s <port>\n"
 			"  <port> is the type-c port to query\n", argv[0]);
@@ -9597,7 +9597,10 @@ int cmd_typec_status(int argc, char *argv[])
 
 	rv = ec_command(EC_CMD_TYPEC_STATUS, 0, &p, sizeof(p),
 			ec_inbuf, ec_max_insize);
-	if (rv < 0)
+	if (rv == -EC_RES_INVALID_COMMAND - EECRESULT)
+		/* Fall back to PD_CONTROL to support older ECs */
+		return cmd_usb_pd(argc, argv);
+	else
 		return -1;
 
 	printf("Port C%d: %s, %s  State:%s\n"
