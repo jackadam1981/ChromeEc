7b3805870fa60d11a8845ac6de63d92ff6b16bad
Scott Collyer <scollyer@google.com>
2020-09-28 22:00:37 -0700

    honeybuns: Enable usbc support
    This CL adds boards specific parts required to enable TCPMv2.0
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I2a4c3bf4089fb3e167d06921b177d8c4e61a021f

--- board/quiche/board.h
+++ board/quiche/board.h
@@ -15,6 +15,9 @@
 #define CONFIG_SYSTEM_UNLOCKED /* Allow dangerous commands while in dev. */
 
 /* USB Type C and USB PD defines */
+#define USB_PD_PORT_HOST   0
+#define USB_PD_PORT_UF   1
+#define USB_PD_PORT_DP   2
 
 /* USB Type A Features */
 
@@ -24,6 +27,9 @@
 #ifndef __ASSEMBLER__
 
 #include "registers.h"
+
+#define GPIO_TRIGGER_1 GPIO_USB3_A3_CDP_EN
+#define GPIO_TRIGGER_2 GPIO_USB3_A4_CDP_EN
 
 #endif /* !__ASSEMBLER__ */
 
