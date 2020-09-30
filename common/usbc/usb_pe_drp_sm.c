d814127ab707dff35d1a3212477e8dac11c1ddd9
Scott Collyer <scollyer@google.com>
2020-09-30 10:47:36 -0700

    TCPMv2: Attempt to split ALT-DP from ALT_DP_DFP configs
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Iee0764f37b401970b18efc64bf7519e209a22ea7

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -1034,6 +1034,7 @@ uint32_t pd_get_events(int port)
 	return pe[port].events;
 }
 
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 /*
  * Determine if this port may communicate with the cable plug.
  *
@@ -1065,7 +1066,9 @@ static bool pe_can_send_sop_prime(int port)
 		return false;
 	}
 }
+#endif
 
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 /*
  * Determine if this port may send the given VDM type
  *
@@ -1099,6 +1102,7 @@ static bool pe_can_send_sop_vdm(int port, int vdm_cmd)
 
 	return false;
 }
+#endif
 
 static void pe_send_soft_reset(const int port, enum tcpm_transmit_type type)
 {
@@ -1875,6 +1879,7 @@ static void pe_src_discovery_run(int port)
 		}
 	}
 
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 	/*
 	 * Note: While the DiscoverIdentityTimer is only required in an explicit
 	 * contract, we use it here to ensure we space any potential BUSY
@@ -1887,7 +1892,7 @@ static void pe_src_discovery_run(int port)
 		set_state_pe(port, PE_VDM_IDENTITY_REQUEST_CBL);
 		return;
 	}
-
+#endif
 	/*
 	 * Transition to the PE_SRC_Disabled state when:
 	 *   1) The Port Partners have not been PD Connected.
@@ -2378,13 +2383,14 @@ static void pe_src_ready_run(int port)
 		PE_CLR_FLAG(port, PE_FLAGS_FIRST_MSG);
 		pe[port].wait_and_add_jitter_timer = TIMER_DISABLED;
 
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 		/*
 		 * Attempt discovery if possible, and return if state was
 		 * changed for that discovery.
 		 */
 		if (pe_attempt_port_discovery(port))
 			return;
-
+#endif
 		/*
 		 * Handle Device Policy Manager Requests
 		 */
@@ -3168,14 +3174,14 @@ static void pe_snk_ready_run(int port)
 			set_state_pe(port, PE_SNK_SELECT_CAPABILITY);
 			return;
 		}
-
+#ifdef CONFIG_USB_PD_ALT_MODE_DFP
 		/*
 		 * Attempt discovery if possible, and return if state was
 		 * changed for that discovery.
 		 */
 		if (pe_attempt_port_discovery(port))
 			return;
-
+#endif
 		/*
 		 * Handle Device Policy Manager Requests
 		 */
