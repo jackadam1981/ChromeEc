a3999205a139f76d2dccb2ee30a6bd248648d6a4
Scott Collyer <scollyer@google.com>
2020-09-30 10:38:50 -0700

    TCPMv2: Config option fixes to allow type-c only support
    This CL adds config option fixes for to disconnection type-c only
    support in TCPMv2 from the try_src feature.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I0bba736b139463bfa213deaf88e7c9a4b3ffbab0

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -469,13 +469,15 @@ static void pd_update_try_source(void);
 
 static void sink_stop_drawing_current(int port);
 
+#ifdef CONFIG_USB_PD_TRY_SRC
 static bool is_try_src_enabled(int port)
 {
+
 	return IS_ENABLED(CONFIG_USB_PD_TRY_SRC) &&
 		((pd_try_src_override == TRY_SRC_OVERRIDE_ON) ||
 		(pd_try_src_override == TRY_SRC_NO_OVERRIDE && pd_try_src));
 }
-
+#endif
 /*
  * Public Functions
  *
@@ -499,15 +501,28 @@ void pd_execute_hard_reset(int port)
 	/* DO NOTHING */
 }
 
+#ifndef CONFIG_USB_PD_DISCHARGE
 void pd_set_vbus_discharge(int port, int enable)
 {
 	/* DO NOTHING */
 }
+#endif
 
+#ifdef CONFIG_USB_PE_SM
 uint16_t pd_get_identity_vid(int port)
 {
 	/* DO NOTHING */
 	return 0;
+}
+#endif
+
+void pe_invalidate_explicit_contract(int port)
+{
+
+}
+
+void pe_exit_dp_mode(int port)
+{
 }
 
 #endif /* !CONFIG_USB_PRL_SM */
@@ -626,6 +641,7 @@ static void tc_enable_pd(int port, int en)
 				     PD_DISABLED_NO_CONNECTION);
 }
 
+#ifdef CONFIG_USB_PD_TRY_SRC
 static void tc_enable_try_src(int en)
 {
 	if (en)
@@ -633,6 +649,7 @@ static void tc_enable_try_src(int en)
 	else
 		deprecated_atomic_clear(&pd_try_src, 1);
 }
+#endif
 
 static void tc_detached(int port)
 {
@@ -647,8 +664,10 @@ static inline void pd_set_dual_role_and_event(int port,
 {
 	drp_state[port] = state;
 
+#ifdef CONFIG_USB_PD_TRY_SRC
 	if (IS_ENABLED(CONFIG_USB_PD_TRY_SRC))
 		pd_update_try_source();
+#endif
 
 	if (event != 0)
 		task_set_event(PD_PORT_TO_TASK_ID(port), event, 0);
@@ -926,6 +945,7 @@ void tc_disc_ident_complete(int port)
 	TC_CLR_FLAG(port, TC_FLAGS_DISC_IDENT_IN_PROGRESS);
 }
 
+#ifdef CONFIG_USB_PD_TRY_SRC
 void tc_try_src_override(enum try_src_override_t ov)
 {
 	if (IS_ENABLED(CONFIG_USB_PD_TRY_SRC)) {
@@ -946,6 +966,7 @@ enum try_src_override_t tc_get_try_src_override(void)
 {
 	return pd_try_src_override;
 }
+#endif
 
 void tc_snk_power_off(int port)
 {
@@ -1364,8 +1385,11 @@ void tc_state_init(int port)
 		return;
 	}
 
+
+#ifdef CONFIG_USB_PD_TRY_SRC
 	/* Allow system to set try src enable */
 	tc_try_src_override(TRY_SRC_NO_OVERRIDE);
+#endif
 
 	/*
 	 * Set initial PD communication policy.
@@ -1941,8 +1965,12 @@ static void tc_error_recovery_run(const int port)
 	 * If try src support is active (e.g. in S0). Then try to become the
 	 * SRC, otherwise we should try to be the sink.
 	 */
+#ifdef CONFIG_USB_PD_TRY_SRC
 	restart_tc_sm(port, is_try_src_enabled(port) ? TC_UNATTACHED_SRC :
 						       TC_UNATTACHED_SNK);
+#else
+	restart_tc_sm(port, TC_UNATTACHED_SNK);
+#endif
 }
 
 /**
@@ -1976,11 +2004,12 @@ static void tc_unattached_snk_entry(const int port)
 
 	tc[port].data_role = PD_ROLE_DISCONNECTED;
 
+#ifdef CONFIG_USB_PE_SM
 	/*
 	 * Saved SRC_Capabilities are no longer valid on disconnect
 	 */
 	pd_set_src_caps(port, 0, NULL);
-
+#endif
 	/*
 	 * When data role set events are used to enable BC1.2, then CC
 	 * detach events are used to notify BC1.2 that it can be powered
@@ -2155,9 +2184,11 @@ static void tc_attach_wait_snk_run(const int port)
 	 */
 	if (pd_is_vbus_present(port)) {
 		if (new_cc_state == PD_CC_DFP_ATTACHED) {
+#ifdef CONFIG_USB_PD_TRY_SRC
 			if (is_try_src_enabled(port))
 				set_state_tc(port, TC_TRY_SRC);
 			else
+#endif
 				set_state_tc(port, TC_ATTACHED_SNK);
 		} else {
 			/* new_cc_state is PD_CC_DFP_DEBUG_ACC */
@@ -2459,10 +2490,12 @@ static void tc_unattached_src_entry(const int port)
 
 	tc[port].data_role = PD_ROLE_DISCONNECTED;
 
+#ifdef CONFIG_USB_PE_SM
 	/*
 	 * Saved SRC_Capabilities are no longer valid on disconnect
 	 */
 	pd_set_src_caps(port, 0, NULL);
+#endif
 
 	/*
 	 * When data role set events are used to enable BC1.2, then CC
@@ -2790,8 +2823,10 @@ static void tc_attached_src_run(const int port)
 			!TC_CHK_FLAG(port, TC_FLAGS_PR_SWAP_IN_PROGRESS) &&
 			!TC_CHK_FLAG(port, TC_FLAGS_DISC_IDENT_IN_PROGRESS)) {
 
+#ifdef CONFIG_USB_PD_TRY_SRC
 		const bool tryWait = is_try_src_enabled(port) &&
 				!TC_CHK_FLAG(port, TC_FLAGS_TS_DTS_PARTNER);
+#endif
 
 		if (IS_ENABLED(CONFIG_USB_PE_SM))
 			if (IS_ENABLED(CONFIG_USB_PD_ALT_MODE_DFP)) {
@@ -2800,9 +2835,10 @@ static void tc_attached_src_run(const int port)
 				pd_dfp_exit_mode(port, TCPC_TX_SOP_PRIME_PRIME,
 						0, 0);
 			}
-
+#ifdef CONFIG_USB_PD_TRY_SRC
 		set_state_tc(port, tryWait ?
 					TC_TRY_WAIT_SNK : TC_UNATTACHED_SNK);
+#endif
 		return;
 	}
 
