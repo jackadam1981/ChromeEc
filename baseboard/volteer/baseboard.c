77cb20c8ee9badc09d9b3e19259a6b8b78dcb5f7
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2020-10-06 17:13:34 +0800

    volteer: increase fan off and trip point
    BUG=
    BRANCH=
    TEST=
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: If64b22c766357ae38eee19dc067c15a6409c941b

--- baseboard/volteer/baseboard.c
+++ baseboard/volteer/baseboard.c
@@ -165,8 +165,8 @@ const static struct ec_thermal_config thermal_cpu = {
 	.temp_host_release = {
 		[EC_TEMP_THRESH_HIGH] = C_TO_K(65),
 	},
-	.temp_fan_off = C_TO_K(35),
-	.temp_fan_max = C_TO_K(50),
+	.temp_fan_off = C_TO_K(40),
+	.temp_fan_max = C_TO_K(55),
 };
 
 /*
