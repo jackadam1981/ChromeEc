5606ead99cf8c29e15350fa8e224a4238b2af9ff
Dawid Niedzwiecki <dn@semihalf.com>
2020-10-07 12:13:52 +0200

    tree: Use new atomic_* implementation
    It is done as a part of porting to Zephyr.
    Since the implementation of atomic functions is done for all architectures
    use atomic_* instead of deprecated_atomic_*.
    
    Sometimes there was a compilation error "discards 'volatile' qualifier"
    due to dropping "volatile" in the argument of the functions, thus
    some pointers casts need to be made. It shouldn't cause any issues,
    because we are sure about generated asm (store operation will be
    performed).
    
    BUG=b:169151160
    BRANCH=none
    TEST=buildall
    
    Signed-off-by: Dawid Niedzwiecki <dn@semihalf.com>
    Change-Id: I98f590c323c3af52035e62825e8acfa358e0805a

--- zephyr/shim/include/atomic.h
+++ zephyr/shim/include/atomic.h
@@ -9,37 +9,7 @@
 #include <sys/atomic.h>
 
 /*
- * Below EC APIs are being deprecated and replaced with the Zephyr
- * APIs.  We already get the Zephyr APIs from sys/atomic.h.  The
- * definitions here are provided so we can shim-in modules using the
- * deprecated APIs while the transition is under way.
+ * Use generic Zephyr's atomic functions
  */
-static inline void deprecated_atomic_clear_bits(uint32_t volatile *addr,
-						uint32_t bits)
-{
-	atomic_and((atomic_t *)addr, bits);
-}
-
-static inline void deprecated_atomic_or(uint32_t volatile *addr, uint32_t bits)
-{
-	atomic_or((atomic_t *)addr, bits);
-}
-
-static inline void deprecated_atomic_add(uint32_t volatile *addr,
-					 uint32_t value)
-{
-	atomic_add((atomic_t *)addr, value);
-}
-
-static inline void deprecated_atomic_sub(uint32_t volatile *addr,
-					 uint32_t value)
-{
-	atomic_sub((atomic_t *)addr, value);
-}
-
-static inline uint32_t deprecated_atomic_read_clear(uint32_t volatile *addr)
-{
-	return atomic_clear((atomic_t *)addr);
-}
 
 #endif  /* __CROS_EC_ATOMIC_H */
