6ce350408a2db8c618609ace9fa19bceffb30492
Scott Collyer <scollyer@google.com>
2020-10-09 18:37:52 -0700

    gingerbread: build fixes
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Id56ae08534fbd2c87b1f44bae4c96fdfa32837a2

--- baseboard/honeybuns/baseboard.h
+++ baseboard/honeybuns/baseboard.h
@@ -168,7 +168,7 @@ enum usb_strings {
 #define CONFIG_USBC_PPC_VCONN
 #define CONFIG_USBC_PPC_DEDICATED_INT
 /* #define CONFIG_CMD_PPC_DUMP */
-#define CONFIG_CMD_TCPC_DUMP
+/* #define CONFIG_CMD_TCPC_DUMP */
 
 #define CONFIG_STM32G4_UCPD_DEBUG
 
