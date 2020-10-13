3b4c106fc5523b6150538245037cddd3fa967bf3
Nathan K <nkolluru@google.com>
2020-10-12 21:11:01 -0700

    TCPMv2: Improve DisplayPort AltMode compliance
    This series of CLs is intended to address DP compliance issues noted in
    the VESA DisplayPort Alternate Mode over Type-C implementation in
    CrosEC.
    
    Review of VESA DP AltMode v2.0 Final document shows we are not following
    guidelines for DFP_D regarding USB mux and HPD behavior. This is exposed
    via issues like ServoV4 SSUSB muxes being null-routed on failed
    or rejected EnterMode command, certain DP devices not functioning due
    to SBU lanes being muxed prematurely, or loss of USB3 data on MF-Pref
    Docking attach.
    
    This parch series addresses the most major of these compliance issues,
    as well as improving logging to aid GFX team debugging long-tail.
    
    BRANCH=none
    BUG=b:168545785, b:170664627, b:170102805
    TEST=(1) Build and flash VOLTEER
         (2) Monitor simultaneous traces of:
             (a) AltMode packetized HPD line state
    	 (b) USB-PD configuration traffic
    	 (c) SBU line analog offset
    	 (d) Internal GFX HPD line state
    	 (e) AP and OS USB3 enumeration state
         (3) Verify USB3 lanes are not broken on DP EnterMode
         (4) Verify DP sink is not muxed into USB3 controller
         (5) Verify debug prints accurately reflect HPD state
    
    Signed-off-by: Nathan K <nkolluru@google.com>
    Change-Id: I885de28aa69be1d68ad706a9529a87571c1a19af

--- driver/usb_mux/usb_mux.c
+++ driver/usb_mux/usb_mux.c
@@ -281,6 +281,8 @@ void usb_mux_hpd_update(int port, int hpd_lvl, int hpd_irq)
 	mux_state_t mux_state;
 	const struct usb_mux *mux_ptr = &usb_muxes[port];
 
+	CPRINTS("C%d: mux HPD update lvl %d irq %d",
+			port, hpd_lvl, hpd_irq);
 	if (port >= board_get_usb_pd_port_count()) {
 		return;
 	}
@@ -290,6 +292,8 @@ void usb_mux_hpd_update(int port, int hpd_lvl, int hpd_irq)
 			mux_ptr->hpd_update(mux_ptr, hpd_lvl, hpd_irq);
 
 	if (!configure_mux(port, USB_MUX_GET_MODE, &mux_state)) {
+		CPRINTS("C%d: mux conf mux_state %d",
+			port, mux_state);
 		mux_state |= (hpd_lvl ? USB_PD_MUX_HPD_LVL : 0) |
 			     (hpd_irq ? USB_PD_MUX_HPD_IRQ : 0);
 		configure_mux(port, USB_MUX_SET_MODE, &mux_state);
