992db3245e9bd6365838bdf3e706f77009338809
Scott Collyer <scollyer@google.com>
2020-10-14 07:40:03 -0700

    honeybuns: Force usbc detach on RO -> RW jump
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I8fa65e9db0d100d341d9a488c61917999cf5f46c

--- baseboard/honeybuns/baseboard.c
+++ baseboard/honeybuns/baseboard.c
@@ -9,6 +9,7 @@
 #include "hooks.h"
 #include "i2c.h"
 #include "usb_pd.h"
+#include "system.h"
 #include "timer.h"
 #include "util.h"
 
@@ -37,10 +38,39 @@ const struct i2c_port_t i2c_ports[] = {
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
+#ifndef SECTION_IS_RW
+static void baseboard_set_usbc_sink_mode(void)
+{
+	uint32_t cr;
+
+	/*
+	 * Bare minimum code required to enable UCPD peripheral and apply Rd to
+	 * the CC lines. This is only applied in RO.
+	 */
+	/* Ensure that clock to UCPD is enabled */
+	STM32_RCC_APB1ENR2 |= STM32_RCC_APB1ENR2_UPCD1EN;
+	/* enable the peripheral */
+	STM32_UCPD_CFGR1(0) |= STM32_UCPD_CFGR1_UCPDEN;
+
+	cr = STM32_UCPD_CR(0);
+	/* Apply Rd to both CC lines */
+	cr |= STM32_UCPD_CR_ANAMODE | STM32_UCPD_CR_CCENABLE_MASK;
+	STM32_UCPD_CR(0) = cr;
+
+	CPRINTS("usbc: CR = 0x%x", STM32_UCPD_CR(0));
+}
+#endif
+
 static void baseboard_init(void)
 {
 	/* Turn on power rails */
 	board_power_sequence();
 	CPRINTS("board: Power rails enabled");
+
+#ifdef SECTION_IS_RW
+	system_clear_reset_flags(EC_RESET_FLAG_POWER_ON);
+#else
+	baseboard_set_usbc_sink_mode();
+#endif
 }
 DECLARE_HOOK(HOOK_INIT, baseboard_init, HOOK_PRIO_INIT_I2C + 1);
