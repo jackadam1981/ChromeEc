cd18647b469522824bdf1f0916d2078d09bb7c9a
tim <tim2.lin@ite.corp-partner.google.com>
2020-10-14 09:23:38 +0800

    (TEST) core/nds32: race condition test
    BUG=none
    BRANCH=none
    TEST=create two task to read adc test as follows.
    [218.651369 ====================]  TASK2: read ADC_CH1
    [218.651527 lock-mtx->waiters=4]
    [218.651949 unlock-waiters=0]
    [218.652215 unlock-mtx->waiters=0]
    [218.652484 ADC_CH1=1318mv]
    [218.652889 ====================]  TASK1: read ADC_CH0
    [218.653030 lock-mtx->waiters=2]
    [218.653445 unlock-waiters=0]
    [218.653711 unlock-mtx->waiters=0]
    [218.653991 ADC_CH0=41mv]
    [219.153173 ====================]  TASK2: read ADC_CH1
    [219.153331 lock-mtx->waiters=4]
    [219.153667 ====================]  When TASK2 enters mutex, TASK1
                                       will go to sleep waiting.
    [219.153808 lock-mtx->waiters=2]
    [219.154153 unlock-waiters=2]      The waiters will wake up TASK1.
    [219.154401 unlock-mtx->waiters=2]
    [219.154707 ADC_CH1=1277mv]
    [219.155059 Sleep waiting]         TASK1 is waiting.
    [219.155496 unlock-waiters=0]      TASK1 is woke up and cleared
                                       the waiter to zero.
    [219.155765 unlock-mtx->waiters=0]
    [219.156037 ADC_CH0=41mv]
    
    *** race condition situation ***
    [219.654114 ====================]  TASK1: read ADC_CH0
    [219.654271 lock-mtx->waiters=2]
    [219.654694 unlock-waiters=0]      The local variable of waiter is
                                       cleared to zero.
    [219.655051 ====================]  TASK2 will go to sleep waiting.
    [219.655194 lock-mtx->waiters=4]
    [219.655392 Sleep waiting]
    [219.655627 unlock-mtx->waiters=4] TASK2 is waiting and enable global
                                       variable of waiter. But local
                                       variable of waiter equal to zero,
                                       TASK2 cannot be woke up after
                                       TASK1 unlock mutex.
    [219.655872 ADC_CH0=41mv]
    [220.154578 ====================]
    [220.154736 lock-mtx->waiters=6]
    [220.155167 unlock-waiters=4]
    [220.155489 unlock-mtx->waiters=4]
    [220.155973 unlock-waiters=0]
    [220.156281 unlock-mtx->waiters=0]
    [220.156517 ADC_CH1=843mv]
    [220.156868 ADC_CH0=41mv]
    *** race condition ***
    
    [220.655070 ====================]
    [220.655227 lock-mtx->waiters=2]
    [220.655650 unlock-waiters=0]
    [220.655916 unlock-mtx->waiters=0]
    [220.656196 ADC_CH0=41mv]
    
    Change-Id: Id648030e08b8cb54ace993e6ee547c119036081b
    Signed-off-by: tim <tim2.lin@ite.corp-partner.google.com>

--- core/nds32/task.c
+++ core/nds32/task.c
@@ -608,6 +608,8 @@ void __ram_code mutex_lock(struct mutex *mtx)
 	/* critical section with interrupts off */
 	interrupt_disable();
 	mtx->waiters |= id;
+	ccprints("====================");
+	ccprints("lock-mtx->waiters=0x%x", mtx->waiters);
 	while (1) {
 		if (!mtx->lock) { /* we got it ! */
 			mtx->lock = 2;
@@ -618,6 +620,7 @@ void __ram_code mutex_lock(struct mutex *mtx)
 		} else { /* Contention on the mutex */
 			/* end of critical section : re-enable interrupts */
 			interrupt_enable();
+			ccprints("Sleep waiting");
 			/* Sleep waiting for our turn */
 			task_wait_event_mask(TASK_EVENT_MUTEX, 0);
 			/* re-enter critical section */
@@ -631,9 +634,11 @@ void __ram_code mutex_unlock(struct mutex *mtx)
 	uint32_t waiters;
 	task_ *tsk = current_task;
 
+	waiters = mtx->waiters;
+	ccprints("unlock-waiters=0x%x", waiters);
+	ccprints("unlock-mtx->waiters=0x%x", mtx->waiters);
 	/* give back the lock */
 	mtx->lock = 0;
-	waiters = mtx->waiters;
 
 	while (waiters) {
 		task_id_t id = __fls(waiters);
