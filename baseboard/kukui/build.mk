c069ab6eae11191bfe941e6353a9a4bf7c21c6a1
Dino Li <Dino.Li@ite.com.tw>
2020-10-15 15:34:59 +0800

    TEST-ONLY: Juniper with it81202bx
    Change-Id: I6ff3e36f6fd43327dcbfa52d65a14129caa6cb8d

--- baseboard/kukui/build.mk
+++ baseboard/kukui/build.mk
@@ -8,7 +8,11 @@
 
 baseboard-y=baseboard.o
 baseboard-$(CONFIG_USB_POWER_DELIVERY)+=usb_pd_policy.o
+ifeq ($(CHIP),stm32)
 baseboard-$(CONFIG_BOOTBLOCK)+=emmc.o
+else
+baseboard-$(CONFIG_BOOTBLOCK)+=emmc_ite.o
+endif
 
 baseboard-$(VARIANT_KUKUI_BATTERY_MAX17055)+=battery_max17055.o
 baseboard-$(VARIANT_KUKUI_BATTERY_MM8013)+=battery_mm8013.o
@@ -18,7 +22,11 @@ baseboard-$(VARIANT_KUKUI_CHARGER_MT6370)+=charger_mt6370.o
 
 baseboard-$(VARIANT_KUKUI_POGO_KEYBOARD)+=base_detect_kukui.o
 
+ifeq ($(CHIP),stm32)
 $(out)/RO/baseboard/$(BASEBOARD)/emmc.o: $(out)/bootblock_data.h
+else
+$(out)/RO/baseboard/$(BASEBOARD)/emmc_ite.o: $(out)/bootblock_data.h
+endif
 
 # bootblock size from 12769.0
 DEFAULT_BOOTBLOCK_SIZE:=21504
