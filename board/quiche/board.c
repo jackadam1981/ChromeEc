3289e78a98887787668ce092ce4876f3c41be067
Scott Collyer <scollyer@google.com>
2020-10-19 09:54:34 -0700

    quiche: P1 fixes power sequencing
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Ic09626eca4bc0cab2c95737d102109217d073cc6

--- board/quiche/board.c
+++ board/quiche/board.c
@@ -92,7 +92,8 @@ void hpd_interrupt(enum gpio_signal signal)
 const struct power_seq board_power_seq[] = {
 	{GPIO_EN_AC_JACK,               1, 20},
 	{GPIO_EN_PP5000_A,              1, 31},
-	{GPIO_EN_PP3300_B,              1, 100},
+	{GPIO_EN_PP3300_B,              1, 1},
+	{GPIO_MST_LP_CTL_L,             1, 100},
 	{GPIO_EN_BB,                    1, 30},
 	{GPIO_EN_PP1100_A,              1, 30},
 	{GPIO_EN_PP1050_A,              1, 30},
@@ -100,7 +101,6 @@ const struct power_seq board_power_seq[] = {
 	{GPIO_EN_PP5000_C,              1, 20},
 	{GPIO_EN_PP5000_HSPORT,         1, 31},
 	{GPIO_EN_DP_SINK,               1, 80},
-	{GPIO_MST_LP_CTL_L,             1, 41},
 	{GPIO_MST_RST_L,                1, 20},
 	{GPIO_EC_HUB2_RESET_L,          1, 41},
 	{GPIO_EC_HUB3_RESET_L,          1, 33},
