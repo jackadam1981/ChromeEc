3641d0207e8c9a2e00b44b3e0f542e1c62e725f1
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2020-10-26 09:07:39 +0800

    System: shutdown chipset before enter hibernate
    Pressing Alt + h + Volume Up cannot enter hibernate on lazor.
    The DUT would boot up immediately. To ensure the KB hibernate
    successfully, the AP needs fully shutdown before hibernating.
    
    BUG=b:171731177
    BRANCH=none
    TEST=Pressing Alt+h+VolumeUp can enter hibernate in S0, and can
         wake up by plugging AC, pressing power button
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: I3c09c0d339ebbaab33635a283fd57bac61ebf08f

--- common/system.c
+++ common/system.c
@@ -945,6 +945,24 @@ static int handle_pending_reboot(enum ec_reboot_cmd cmd)
 	}
 }
 
+void system_enter_hibernate(int seconds, int microseconds)
+{
+	/*
+	 * If chipset is already off, then call system_hibernate directly. Else,
+	 * let chipset_task bring down the power rails and transition to proper
+	 * state before system_hibernate is called.
+	 */
+	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		system_hibernate(seconds, microseconds);
+	else {
+		reboot_at_shutdown = EC_REBOOT_HIBERNATE;
+		hibernate_seconds = seconds;
+		hibernate_microseconds = microseconds;
+
+		chipset_force_shutdown(CHIPSET_SHUTDOWN_CONSOLE_CMD);
+	}
+}
+
 /*****************************************************************************/
 /* Hooks */
 
@@ -1081,20 +1099,7 @@ __maybe_unused static int command_hibernate(int argc, char **argv)
 	} else
 		ccprintf("Hibernating until wake pin asserted.\n");
 
-	/*
-	 * If chipset is already off, then call system_hibernate directly. Else,
-	 * let chipset_task bring down the power rails and transition to proper
-	 * state before system_hibernate is called.
-	 */
-	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
-		system_hibernate(seconds, microseconds);
-	else {
-		reboot_at_shutdown = EC_REBOOT_HIBERNATE;
-		hibernate_seconds = seconds;
-		hibernate_microseconds = microseconds;
-
-		chipset_force_shutdown(CHIPSET_SHUTDOWN_CONSOLE_CMD);
-	}
+	system_enter_hibernate(seconds, microseconds);
 
 	return EC_SUCCESS;
 }
