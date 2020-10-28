9101792101c17d34daf17474af28b85074b6b7ad
Divya Sasidharan <divya.s.sasidharan@intel.com>
2020-10-28 16:11:55 -0700

    virtual mux: Wait for mux config ACK from the kernel
    This CL:
    1. Add a new config CONFIG_USB_MUX_AP_ACK_REQUEST to enable request
       for ACK from AP
    1. Adds a new feature flag to inform the AP that an ACK is needed
       for boards supporting Burnside bridge retimer.
    2. Adds a new host command for the EC to wait for mux config ACK
       for entering and exiting the safe mode.
    3. Adds 12.5msec delay after configuring retimer and SoC as
       recommended.
    
    BUG=b:166300460,b:161327513
    BRANCH=none
    TEST=Verify Type-C dock is functional with multiple hotplugs
         and flipped orientation.
         Verify USB3.0 is detected and not downgraded.
         Verify no regression with TBT3 and USB4 dock.
    
    Cq-Depend: CL:2530517
    Change-Id: I5b8224648f0fc36b30e24ca3e7254d708c676149
    Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>
    Signed-off-by: Ayushee Shah <ayushee.shah@intel.com>

--- driver/usb_mux/usb_mux.c
+++ driver/usb_mux/usb_mux.c
@@ -9,6 +9,8 @@
 #include "console.h"
 #include "hooks.h"
 #include "host_command.h"
+#include "task.h"
+#include "timer.h"
 #include "usb_mux.h"
 #include "usbc_ppc.h"
 #include "util.h"
@@ -387,4 +389,19 @@ static enum ec_status hc_usb_pd_mux_info(struct host_cmd_handler_args *args)
 }
 DECLARE_HOST_COMMAND(EC_CMD_USB_PD_MUX_INFO,
 		     hc_usb_pd_mux_info,
+		     EC_VER_MASK(0));
+
+static enum ec_status hc_usb_pd_mux_ack(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_usb_pd_mux_ack *p = args->params;
+
+	if (!IS_ENABLED(CONFIG_USB_MUX_AP_ACK_REQUEST))
+		return EC_RES_INVALID_COMMAND;
+
+	task_set_event(PD_PORT_TO_TASK_ID(p->port),
+		       PD_EVENT_AP_MUX_DONE, 0);
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_USB_PD_MUX_ACK,
+		     hc_usb_pd_mux_ack,
 		     EC_VER_MASK(0));
