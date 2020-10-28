45a233c6c3c6caee18a2722f3b2ea5db2e81aadb
Scott Collyer <scollyer@google.com>
2020-10-28 10:21:38 -0700

    quiche: Add support for C2 usbc port
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Ie2583ae9271333e2ecd9561eed60fab6cdb4fda1

--- board/quiche/board.h
+++ board/quiche/board.h
@@ -17,15 +17,20 @@
 /* USB Type C and USB PD defines */
 #define USB_PD_PORT_HOST   0
 #define USB_PD_PORT_DP   1
+#define USB_PD_PORT_UF   2
 
+/*
+ * Only the host and display usbc ports are usb-pd capable. There is a 3rd usbc
+ * port, but this is type-c capable only. Only the PPC for this port needs to be
+ * controlled by FW.
+ */
+#define CONFIG_USB_PD_PORT_MAX_COUNT 3
+#define CONFIG_USB_MUX_PS8822
 
 #define CONFIG_USB_PID 0x5048
 #define CONFIG_USB_BCD_DEV 0x0001 /* v 0.01 */
 #define CONFIG_USB_PD_IDENTITY_HW_VERS 1
 #define CONFIG_USB_PD_IDENTITY_SW_VERS 1
-
-/* USB Type A Features */
-
 
 /* I2C port names */
 #define I2C_PORT_I2C1		0
