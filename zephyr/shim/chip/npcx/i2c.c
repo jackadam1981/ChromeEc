d41e0db87f98b62ad79847afded326406462dc6e
Yuval Peress <peress@chromium.org>
2020-10-28 10:20:08 -0600

    Add chip-specific shim along with i2c module
    This change accomplishes 2 things:
    1. It refactors the zephyr/shim directory to structure around
       chip specific compilation. In this example, we're focusing
       on npcx7m6fb which is used in volteer but others can be
       added easily.
    2. It shims the common/i2c_master.c by providing an alternate
       implementation of i2c_xfer_unlocked that calls down to the
       Zephyr API i2c_write_read instead of the chip specific
       i2c_xfer_no_retry or chip_i2c_xfer_with_notify.
    
       The shim layer is made possible by the addition of
       zephyr/shim/include/i2c/i2c.h which adds a functions that
       needs to be implemented per chip (npcx7 family in this case)
       and allows us to map the current port int which is defined
       in chip/${CHIP}/registers.h (chip/npcx/registers-npcx7.h in
       our case). This function (i2c_get_device_for_port) maps the
       platform/ec port int to a const struct device * which is
       needed in the Zephyr I2C APIs.
    
    BRANCH=none
    BUG=b:171302975
    TEST=clean_build.sh projects/experimental/volteer/ and make BOARD=eve
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I210f4758337bf384d0d6f103eef8b89126887d11

---
+++ zephyr/shim/chip/npcx/i2c.c
@@ -0,0 +1,117 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* #define CHIP_FAMILY_NPCX7 */
+#include "registers.h"
+#include "i2c/i2c.h"
+
+/* Define all the nodes for i2c port/bus combinations. */
+#define I2C0_0_NODE DT_NODELABEL(i2c0_0)
+#define I2C1_0_NODE DT_NODELABEL(i2c1_0)
+#define I2C2_0_NODE DT_NODELABEL(i2c2_0)
+#define I2C3_0_NODE DT_NODELABEL(i2c3_0)
+#define I2C4_0_NODE DT_NODELABEL(i2c4_0)
+#define I2C4_1_NODE DT_NODELABEL(i2c4_1)
+#define I2C5_0_NODE DT_NODELABEL(i2c5_0)
+#define I2C5_1_NODE DT_NODELABEL(i2c5_1)
+#define I2C6_0_NODE DT_NODELABEL(i2c6_0)
+#define I2C6_1_NODE DT_NODELABEL(i2c6_1)
+#define I2C7_0_NODE DT_NODELABEL(i2c7_0)
+
+/* Define all the labels for valid i2c nodes. */
+#if DT_NODE_HAS_STATUS(I2C0_0_NODE, okay)
+#define I2C0_0 DT_LABEL(I2C0_0_NODE)
+#else
+#define I2C0_0 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C1_0_NODE, okay)
+#define I2C1_0 DT_LABEL(I2C1_0_NODE)
+#else
+#define I2C1_0 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C2_0_NODE, okay)
+#define I2C2_0 DT_LABEL(I2C2_0_NODE)
+#else
+#define I2C2_0 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C3_0_NODE, okay)
+#define I2C3_0 DT_LABEL(I2C3_0_NODE)
+#else
+#define I2C3_0 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C4_0_NODE, okay)
+#define I2C4_0 DT_LABEL(I2C4_0_NODE)
+#else
+#define I2C4_0 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C4_1_NODE, okay)
+#define I2C4_1 DT_LABEL(I2C4_1_NODE)
+#else
+#define I2C4_1 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C5_0_NODE, okay)
+#define I2C5_0 DT_LABEL(I2C5_0_NODE)
+#else
+#define I2C5_0 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C5_1_NODE, okay)
+#define I2C5_1 DT_LABEL(I2C5_1_NODE)
+#else
+#define I2C5_1 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C6_0_NODE, okay)
+#define I2C6_0 DT_LABEL(I2C6_0_NODE)
+#else
+#define I2C6_0 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C6_1_NODE, okay)
+#define I2C6_1 DT_LABEL(I2C6_1_NODE)
+#else
+#define I2C6_1 ""
+#endif
+
+#if DT_NODE_HAS_STATUS(I2C7_0_NODE, okay)
+#define I2C7_0 DT_LABEL(I2C7_0_NODE)
+#else
+#define I2C7_0 ""
+#endif
+
+static const struct device *i2c_devices[NPCX_I2C_COUNT];
+
+static int init_device_bindings(const struct device *device)
+{
+	ARG_UNUSED(device);
+	i2c_devices[NPCX_I2C_PORT0_0] = device_get_binding(I2C0_0);
+	i2c_devices[NPCX_I2C_PORT1_0] = device_get_binding(I2C1_0);
+	i2c_devices[NPCX_I2C_PORT2_0] = device_get_binding(I2C2_0);
+	i2c_devices[NPCX_I2C_PORT3_0] = device_get_binding(I2C3_0);
+#ifdef CHIP_VARIANT_NPCX7M6G
+	i2c_devices[NPCX_I2C_PORT4_0] = device_get_binding(I2C4_0);
+#endif
+	i2c_devices[NPCX_I2C_PORT4_1] = device_get_binding(I2C4_1);
+	i2c_devices[NPCX_I2C_PORT5_0] = device_get_binding(I2C5_0);
+	i2c_devices[NPCX_I2C_PORT5_1] = device_get_binding(I2C5_1);
+	i2c_devices[NPCX_I2C_PORT6_0] = device_get_binding(I2C6_0);
+	i2c_devices[NPCX_I2C_PORT6_1] = device_get_binding(I2C6_1);
+	i2c_devices[NPCX_I2C_PORT7_0] = device_get_binding(I2C7_0);
+	return 0;
+}
+SYS_INIT(init_device_bindings, APPLICATION, 99);
+
+const struct device *i2c_get_device_for_port(const int port)
+{
+	if (port < 0 || port >= NPCX_I2C_COUNT)
+		return NULL;
+	return i2c_devices[port];
+}
