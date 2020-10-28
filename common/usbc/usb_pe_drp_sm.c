5b240f5390ff632ff8dd2712708fbee44bc53e19
Diana Z <dzigterman@chromium.org>
2020-10-28 16:46:08 -0600

    TCPMv2: Notify AP when discovery is NAKed
    When a partner doesn't support identity discovery, notify the AP that
    discovery is complete.  The TYPEC_DISCOVERY results will show 0 VDMs for
    the identity and 0 SVIDs found.
    
    BRANCH=None
    BUG=b:171434553
    TEST=on drawcia, plug in port partners which don't support SOP and SOP'
    identities and verify that events are set for their discovery being done
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2505778
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: I6a398552a64debf614ac95a6d46218708035d8ca

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -4990,8 +4990,12 @@ static void pe_vdm_identity_request_cbl_exit(int port)
 	}
 
 	/* Do not attempt further discovery if identity discovery failed. */
-	if (pd_get_identity_discovery(port, pe[port].tx_type) == PD_DISC_FAIL)
+	if (pd_get_identity_discovery(port, pe[port].tx_type) == PD_DISC_FAIL) {
 		pd_set_svids_discovery(port, pe[port].tx_type, PD_DISC_FAIL);
+		pe_notify_event(port, pe[port].tx_type == TCPC_TX_SOP ?
+				PD_STATUS_EVENT_SOP_DISC_DONE :
+				PD_STATUS_EVENT_SOP_PRIME_DISC_DONE);
+	}
 }
 
 /**
@@ -5049,10 +5053,6 @@ static void pe_init_port_vdm_identity_request_run(int port)
 	case VDM_RESULT_NAK:
 		/* PE_INIT_PORT_VDM_IDENTITY_NAKed embedded here */
 		pd_set_identity_discovery(port, pe[port].tx_type, PD_DISC_FAIL);
-		/*
-		 * Note: AP is only notified of discovery complete when
-		 * something was found (at least one ACK)
-		 */
 		break;
 	}
 
@@ -5077,8 +5077,12 @@ static void pe_init_port_vdm_identity_request_exit(int port)
 	}
 
 	/* Do not attempt further discovery if identity discovery failed. */
-	if (pd_get_identity_discovery(port, pe[port].tx_type) == PD_DISC_FAIL)
+	if (pd_get_identity_discovery(port, pe[port].tx_type) == PD_DISC_FAIL) {
 		pd_set_svids_discovery(port, pe[port].tx_type, PD_DISC_FAIL);
+		pe_notify_event(port, pe[port].tx_type == TCPC_TX_SOP ?
+				PD_STATUS_EVENT_SOP_DISC_DONE :
+				PD_STATUS_EVENT_SOP_PRIME_DISC_DONE);
+	}
 }
 
 /**
