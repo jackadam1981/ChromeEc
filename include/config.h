d865acbb2f6597b300e108154bd4d6b34f0137c9
Wealian Liao <whliao@nuvoton.corp-partner.google.com>
2020-10-29 01:55:56 -0700

    npcx/system: LCT compensate for MTC in PSL_hibernate
    NPCX chip uses the MTC module as the RTC counter. However, in PSL
    hibernate, MTC will stop counting. NPCX9 supports the LCT module which
    could count continuously when VCC1 power is off. The CL uses LCT to
    compensate for the MTC counter value after PSL hibernate wake-up. The
    LCT maximum counting value is 16 weeks.
    
    BRANCH=none
    BUG=b:165777478, b:171919875
    TEST=pass "make buildall"
    TEST=Check the RTC value in the following scenario:
         1. "hibernate" and wake up EC by PSL input.
         2. "hibernate 30" and wake up EC after timeout.
         3. "hibernate 30" and wake up EC by PSL input before timeout.
    
    Signed-off-by: Wealian Liao <whliao@nuvoton.corp-partner.google.com>
    Change-Id: I39e370a437f40749acbd3a35a8b37ebec70f1bf2

--- include/config.h
+++ include/config.h
@@ -2281,6 +2281,16 @@
 #undef CONFIG_HIBERNATE_PSL_VCC1_RST_WAKEUP
 
 /*
+ * Compensate the elapsed time for the RTC which couldn't work in hibernate PSL
+ * after hibernation wake-up. This option is enabled automatically when enabling
+ * all the following configs:
+ *   (1) CONFIG_HIBERNATE_PSL
+ *   (2) CONFIG_CMD_RTC or CONFIG_HOSTCMD_RTC
+ * Currently, NPCX9 supports LCT to compensate the elapsed time for the RTC.
+ */
+#undef CONFIG_HIBERNATE_PSL_COMPENSATE_RTC
+
+/*
  * Chip supports a 64-bit hardware timer and implements
  * __hw_clock_source_read64 and __hw_clock_source_set64.
  *
