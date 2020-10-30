cd31a6b51239b52e9cccbbc0105df5f454c66a85
Diana Z <dzigterman@chromium.org>
2020-10-30 14:18:56 -0600

    TCPM: Add OCP module
    Currently, the overcurrent protection is tracked in the PPC code.
    However, as different chips are able to report overcurrent move this
    code into a generic module.
    
    Logic for not sourcing Vbus or Vconn on latched ports moves into
    the TC layer, and an overridable board overcurrent function is provided
    for boards which have no special actions to take.
    
    BRANCH=None
    BUG=b:171501161
    TEST=make -j buildall; TCPMv2 tested with following drawcia patch
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2532676
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Change-Id: I6a997882dffb1fcdf3f34f2948da68f4f246015b

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -19,6 +19,7 @@
 #include "usb_prl_sm.h"
 #include "usb_sm.h"
 #include "usb_tc_sm.h"
+#include "usbc_ocp.h"
 #include "usbc_ppc.h"
 #include "vboot.h"
 
@@ -967,6 +968,15 @@ void tc_snk_power_off(int port)
 
 int tc_src_power_on(int port)
 {
+	/*
+	 * Check our OC event counter.  If we've exceeded our threshold, then
+	 * let's latch our source path off to prevent continuous cycling.  When
+	 * the PD state machine detects a disconnection on the CC lines, we will
+	 * reset our OC event counter.
+	 */
+	if (IS_ENABLED(CONFIG_USBC_OCP) && usbc_ocp_is_port_latched_off(port))
+		return EC_ERROR_ACCESS_DENIED;
+
 	if (IS_ATTACHED_SRC(port))
 		return pd_set_power_supply_ready(port);
 
@@ -981,6 +991,23 @@ void tc_src_power_off(int port)
 	if (IS_ENABLED(CONFIG_CHARGE_MANAGER))
 		charge_manager_set_ceil(port, CEIL_REQUESTOR_PD,
 					CHARGE_CEIL_NONE);
+}
+
+/* Set what role the partner is right now, for the PPC and OCP module */
+static void tc_set_partner_role(int port, enum ppc_device_role role)
+{
+	if (IS_ENABLED(CONFIG_USBC_PPC))
+		ppc_dev_is_connected(port, role);
+
+	if (IS_ENABLED(CONFIG_USBC_OCP)) {
+		usbc_ocp_snk_is_connected(port, role == PPC_DEV_SNK);
+		/*
+		 * Clear the overcurrent event counter
+		 * since we've detected a disconnect.
+		 */
+		if (role == PPC_DEV_DISCONNECTED)
+			usbc_ocp_clear_event_counter(port);
+	}
 }
 
 /*
@@ -1621,6 +1648,16 @@ static void set_vconn(int port, int enable)
 		TC_CLR_FLAG(port, TC_FLAGS_VCONN_ON);
 
 	/*
+	 * Check our OC event counter.  If we've exceeded our threshold, then
+	 * let's latch our source path off to prevent continuous cycling.  When
+	 * the PD state machine detects a disconnection on the CC lines, we will
+	 * reset our OC event counter.
+	 */
+	if (IS_ENABLED(CONFIG_USBC_OCP) &&
+	    enable && usbc_ocp_is_port_latched_off(port))
+		return;
+
+	/*
 	 * Disable PPC Vconn first then TCPC in case the voltage feeds back
 	 * to TCPC and damages.
 	 */
@@ -2005,16 +2042,7 @@ static void tc_unattached_snk_entry(const int port)
 	if (IS_ENABLED(CONFIG_CHARGE_MANAGER))
 		charge_manager_update_dualrole(port, CAP_UNKNOWN);
 
-	if (IS_ENABLED(CONFIG_USBC_PPC)) {
-		/* There is no source connected. */
-		ppc_dev_is_connected(port, PPC_DEV_DISCONNECTED);
-
-		/*
-		 * Clear the overcurrent event counter
-		 * since we've detected a disconnect.
-		 */
-		ppc_clear_oc_event_counter(port);
-	}
+	tc_set_partner_role(port, PPC_DEV_DISCONNECTED);
 
 	/*
 	 * Indicate that the port is disconnected so the board
@@ -2210,9 +2238,8 @@ static void tc_attached_snk_entry(const int port)
 	 */
 	typec_select_pull(port, TYPEC_CC_RD);
 
-	/* Inform PPC that a source is connected. */
-	if (IS_ENABLED(CONFIG_USBC_PPC))
-		ppc_dev_is_connected(port, PPC_DEV_SRC);
+	/* Inform the PPC and OCP module that a source is connected */
+	tc_set_partner_role(port, PPC_DEV_SRC);
 
 	if (IS_ENABLED(CONFIG_USB_PE_SM) &&
 	    TC_CHK_FLAG(port, TC_FLAGS_PR_SWAP_IN_PROGRESS)) {
@@ -2508,16 +2535,7 @@ static void tc_unattached_src_entry(const int port)
 	 */
 	bc12_role_change_handler(port, prev_data_role, tc[port].data_role);
 
-	if (IS_ENABLED(CONFIG_USBC_PPC)) {
-		/* There is no sink connected. */
-		ppc_dev_is_connected(port, PPC_DEV_DISCONNECTED);
-
-		/*
-		 * Clear the overcurrent event counter
-		 * since we've detected a disconnect.
-		 */
-		ppc_clear_oc_event_counter(port);
-	}
+	tc_set_partner_role(port, PPC_DEV_DISCONNECTED);
 
 	if (IS_ENABLED(CONFIG_CHARGE_MANAGER))
 		charge_manager_update_dualrole(port, CAP_UNKNOWN);
@@ -2543,12 +2561,12 @@ static void tc_unattached_src_run(const int port)
 		}
 	}
 
-	if (IS_ENABLED(CONFIG_USBC_PPC)) {
+	if (IS_ENABLED(CONFIG_USBC_OCP)) {
 		/*
 		 * If the port is latched off, just continue to
 		 * monitor for a detach.
 		 */
-		if (ppc_is_port_latched_off(port))
+		if (usbc_ocp_is_port_latched_off(port))
 			return;
 	}
 
@@ -2784,9 +2802,8 @@ static void tc_attached_src_entry(const int port)
 		typec_update_cc(port);
 	}
 
-	/* Inform PPC that a sink is connected. */
-	if (IS_ENABLED(CONFIG_USBC_PPC))
-		ppc_dev_is_connected(port, PPC_DEV_SNK);
+	/* Inform PPC and OCP module that a sink is connected. */
+	tc_set_partner_role(port, PPC_DEV_SNK);
 
 	/*
 	 * Only notify if we're not performing a power role swap.  During a
@@ -3481,16 +3498,7 @@ static void tc_cc_open_entry(const int port)
 	typec_select_pull(port, TYPEC_CC_OPEN);
 	typec_update_cc(port);
 
-	if (IS_ENABLED(CONFIG_USBC_PPC)) {
-		/* There is no device connected. */
-		ppc_dev_is_connected(port, PPC_DEV_DISCONNECTED);
-
-		/*
-		 * Clear the overcurrent event counter
-		 * since we've detected a disconnect.
-		 */
-		ppc_clear_oc_event_counter(port);
-	}
+	tc_set_partner_role(port, PPC_DEV_DISCONNECTED);
 }
 
 void tc_set_debug_level(enum debug_level debug_level)
