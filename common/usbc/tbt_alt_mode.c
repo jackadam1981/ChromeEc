676ae242910303326302990c199bd6f1f1ea22ea
Abe Levkoy <alevkoy@chromium.org>
2020-11-03 16:02:47 -0700

    TCPMv2: Allow alternate mode reentry
    When the EC is waiting for the AP to direct it to enter alternate modes,
    allow the AP to direct an Exit Mode followed by another Enter Mode.
    
    BUG=b:168030639
    TEST=Exit and reenter DP and TBT alt modes
    BRANCH=none
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: I9edf0ca0a787ed025de2d5a5403d4225a6683e3b

--- common/usbc/tbt_alt_mode.c
+++ common/usbc/tbt_alt_mode.c
@@ -131,7 +131,8 @@ bool tbt_entry_is_done(int port)
 
 static void tbt_exit_done(int port)
 {
-	tbt_state[port] = TBT_INACTIVE;
+	tbt_state[port] = IS_ENABLED(CONFIG_USB_PD_REQUIRE_AP_MODE_ENTRY)
+		? TBT_START : TBT_INACTIVE;
 	TBT_CLR_FLAG(port, TBT_FLAG_RETRY_DONE);
 
 	if (!TBT_CHK_FLAG(port, TBT_FLAG_EXIT_DONE)) {
@@ -309,7 +310,9 @@ void intel_vdm_naked(int port, enum tcpm_transmit_type type, uint8_t vdm_cmd)
 			tbt_active_cable_exit_mode(port);
 		else {
 			tbt_prints("exit mode SOP failed", port);
-			tbt_state[port] = TBT_INACTIVE;
+			tbt_state[port] =
+				IS_ENABLED(CONFIG_USB_PD_REQUIRE_AP_MODE_ENTRY)
+				? TBT_START : TBT_INACTIVE;
 			TBT_CLR_FLAG(port, TBT_FLAG_RETRY_DONE);
 		}
 		break;
