215f15091d21c5bdc8180eb906d7b762b1bdf36c
Ting Shen <phoenixshen@google.com>
2020-11-03 13:58:07 +0800

    TEST-ONLY: asurada: remove minimum input current limit
    BUG=none
    TEST=none
    BRANCH=master
    
    Change-Id: I49f69ca16ca8a17b79c82a416b84279aadb07ffc
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- board/asurada/board.c
+++ board/asurada/board.c
@@ -477,8 +477,6 @@ int board_set_active_charge_port(int port)
 void board_set_charge_limit(int port, int supplier, int charge_ma,
 			    int max_ma, int charge_mv)
 {
-	charge_set_input_current_limit(
-		MAX(charge_ma, CONFIG_CHARGER_INPUT_CURRENT), charge_mv);
 }
 
 void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled)
