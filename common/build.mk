55da264d135981c850dc78e3ed5b1d0efd434a1b
Daisuke Nojiri <dnojiri@chromium.org>
2020-11-04 10:23:14 -0800

    PCHG: Add peripheral charge manager
    Peripheral charge manager communicates with peripheral charge chips
    to charge batteries of peripheral devices.
    
    Only charging and reading battery percentage are implemented. Host
    command, error handling, pausing, for example, are not yet included.
    
    BUG=b:173235954
    BRANCH=Trogdor
    TEST=Coachz proto using listener evaluation board from demo kit.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I0c2b583e5f7736b26ec7d1fb9cd9b6c59c7e8177

--- common/build.mk
+++ common/build.mk
@@ -109,6 +109,7 @@ common-$(CONFIG_MKBP_EVENT)+=mkbp_event.o
 common-$(CONFIG_OCPC)+=ocpc.o
 common-$(CONFIG_ONEWIRE)+=onewire.o
 common-$(CONFIG_PECI_COMMON)+=peci.o
+common-$(CONFIG_PERIPHERAL_CHARGER)+=peripheral_charger.o
 common-$(CONFIG_POWER_BUTTON)+=power_button.o
 common-$(CONFIG_POWER_BUTTON_X86)+=power_button_x86.o
 common-$(CONFIG_PSTORE)+=pstore_commands.o
