889d89a3033b0976f64a77e5d0ce4922c271093a
Devin Lu <Devin.Lu@quantatw.com>
2020-11-06 10:30:53 +0800

    sm5803: Add hardware charging ramping
    This patch add support hardware charging ramp. The hw ramp is
    implemented by enabling DPM (Dynamic Power Management). Once a
    DPM Voltage loop regulation is set and the DPM loop is enabled,
    the DPM guarantees that the Vbus will not go below the threshold
    voltage limiting the power that can be delivered to the load.
    
    BUG=b:172173517
    BRANCH=none
    TEST=On drawcia, i2ctrace to make sure DPM is enabled with 5V source.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I0622da35bf57f367b21e97c5e39cb157ef911521

--- driver/charger/sm5803.c
+++ driver/charger/sm5803.c
@@ -145,12 +145,15 @@ static enum ec_error_list sm5803_flow1_update(int chgnum, const uint8_t mask,
 			return rv;
 		}
 
-		if (reg != 0x7A) {
+		/* Don't clear 0x5C[0] that set by hw ramp */
+		if ((reg & 0xFE) != 0x7A) {
 			CPRINTS("%s %d: Unexpected 0x5C reg: 0x%02x. File bug",
 				CHARGER_NAME, chgnum, reg);
 
+			reg &= 0x01;
+			reg |= 0x7A;
 			/* Fix it before enabling charging */
-			rv = chg_write8(chgnum, 0x5C, 0x7A);
+			rv = chg_write8(chgnum, 0x5C, reg);
 		}
 	}
 
@@ -602,6 +605,11 @@ static void sm5803_init(int chgnum)
 	reg |= SM5803_PHOT1_COMPARATOR_EN;
 	reg &= ~SM5803_PHOT1_IBUS_PHOT_COMP_EN;
 	rv |= chg_write8(chgnum, SM5803_REG_PHOT1, reg);
+
+	/* Set DPM Voltage to 4200 mv, see b:172173517 */
+	reg = SM5803_VOLTAGE_TO_REG(4200);
+	rv = chg_write8(chgnum, SM5803_REG_DPM_VL_SET_MSB, (reg >> 3));
+	rv |= chg_write8(chgnum, SM5803_REG_DPM_VL_SET_LSB, (reg & 0x7));
 
 	if (chgnum != CHARGER_PRIMARY) {
 		/*
@@ -1484,6 +1492,47 @@ static enum ec_error_list sm5803_set_vsys_compensation(int chgnum,
 	return EC_ERROR_UNIMPLEMENTED;
 }
 
+/* Hardware current ramping (aka DPM: Dynamic Power Management) */
+
+#ifdef CONFIG_CHARGE_RAMP_HW
+static enum ec_error_list sm5803_set_hw_ramp(int chgnum, int enable)
+{
+	enum ec_error_list rv;
+	int reg;
+
+	rv = chg_read8(chgnum, SM5803_REG_CHG_MON_REG, &reg);
+
+	if (enable)
+		reg |= SM5803_DPM_LOOP_EN;
+	else
+		reg &= ~SM5803_DPM_LOOP_EN;
+
+	rv |= chg_write8(chgnum, SM5803_REG_CHG_MON_REG, reg);
+
+	return rv;
+}
+
+static int sm5803_ramp_is_stable(int chgnum)
+{
+	return 0;
+}
+
+static int sm5803_ramp_is_detected(int chgnum)
+{
+	return 1;
+}
+
+static int sm5803_ramp_get_current_limit(int chgnum)
+{
+	int rv;
+	int input_current = 0;
+
+	rv = sm5803_get_input_current(chgnum, &input_current);
+
+	return rv ? -1 : input_current;
+}
+#endif /* CONFIG_CHARGE_RAMP_HW */
+
 #ifdef CONFIG_CMD_CHARGER_DUMP
 static int command_sm5803_dump(int argc, char **argv)
 {
@@ -1554,4 +1603,10 @@ const struct charger_drv sm5803_drv = {
 	.enable_otg_power = &sm5803_enable_otg_power,
 	.is_sourcing_otg_power = &sm5803_is_sourcing_otg_power,
 	.set_vsys_compensation = &sm5803_set_vsys_compensation,
+#ifdef CONFIG_CHARGE_RAMP_HW
+	.set_hw_ramp = &sm5803_set_hw_ramp,
+	.ramp_is_stable = &sm5803_ramp_is_stable,
+	.ramp_is_detected = &sm5803_ramp_is_detected,
+	.ramp_get_current_limit = &sm5803_ramp_get_current_limit,
+#endif
 };
