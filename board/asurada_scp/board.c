84df05e08e939d5b3ca8c0fc57bc51ea5bbe4e62
Tzung-Bi Shih <tzungbi@chromium.org>
2020-11-11 13:17:38 +0800

    asurada_scp: enable DRAM cache
    SCP view 0x1000_0000 maps to AP view 0x5000_0000.  Total size is 20MB.
    
    BRANCH=none
    BUG=b:156222459
    TEST=make BOARD=asurada_scp
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: I7357247d7621d098673890c75c26aa1b6e2d4b50

--- board/asurada_scp/board.c
+++ board/asurada_scp/board.c
@@ -13,6 +13,7 @@ struct mpu_entry mpu_entries[NR_MPU_ENTRIES] = {
 	{0x0ffc00, 0x100000, MPU_ATTR_W | MPU_ATTR_R},
 	{0x60000000, 0x70000000, MPU_ATTR_W | MPU_ATTR_R},
 	{0x70000000, 0x80000000, MPU_ATTR_W | MPU_ATTR_R},
+	{0x10000000, 0x11400000, MPU_ATTR_C | MPU_ATTR_W | MPU_ATTR_R},
 };
 
 #include "gpio_list.h"
