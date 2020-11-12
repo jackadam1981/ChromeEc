eea49f87e191a3189a512f66fd4a6ad00569d02b
Poornima Tom <poornima.tom@intel.com>
2020-11-12 12:24:21 +0530

    ADLRVP:Support for 100w typeC power adapter
    ADL SOC has a TDP of 45W and the RVP can support up to
    200W power.Hence, increased the power support to 100W.
    
    BRANCH=None
    BUG=b:173081793
    TEST=use chgsup command and list charger type negotiated values.
    Tested below on ADL:
    When Typec Charger connected to Port3:
    > chgsup
    port=3, type=0, cur=3000mA, vtg=20000mV, lsm=1
    
    when TBT dock connected to Port1:
    > chgsup
    port=1, type=0, cur=3000mA, vtg=20000mV, lsm=1
    
    when AC charger connected:
    > chgsup
    port=4, type=3, cur=3157mA, vtg=19000mV, lsm=0
    
    Signed-off-by: Poornima Tom <poornima.tom@intel.com>
    Change-Id: I0677519a68013dbc593539b692bad5016b52654f

--- baseboard/intelrvp/baseboard.h
+++ baseboard/intelrvp/baseboard.h
@@ -214,7 +214,7 @@ enum battery_type {
 
 /* Define typical operating power */
 #define PD_OPERATING_POWER_MW  15000
-#define PD_MAX_CURRENT_MA      3000
+#define PD_MAX_CURRENT_MA      ((PD_MAX_POWER_MW/PD_MAX_VOLTAGE_MV) * 1000)
 #define PD_MAX_VOLTAGE_MV      20000
 #define DC_JACK_MAX_VOLTAGE_MV 19000
 
