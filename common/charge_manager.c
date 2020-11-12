7da786e168f96cba43df2617e344853a0c2a4a30
Terry Chen <terry_chen@wistron.corp-partner.google.com>
2020-11-12 15:29:48 +0800

    DNS: Modify AC prochot and Add DC prochot
    Change-Id: I1a918326a625f294fe6cd9a2ec8e2c5b2af43569

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -778,7 +778,8 @@ static void charge_manager_refresh(void)
 					new_charge_current_uncapped,
 					new_charge_voltage);
 #endif /* HAS_TASK_CHG_RAMP */
-
+		board_set_prochot(new_port, new_supplier, new_charge_current,
+			new_charge_current_uncapped, new_charge_voltage);
 		power_changed = 1;
 
 		CPRINTS("CL: p%d s%d i%d v%d", new_port, new_supplier,
