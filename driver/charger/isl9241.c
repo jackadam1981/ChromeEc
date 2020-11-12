6a34cc8f7e5654657e02918bcc59745cd9d57d1f
Terry Chen <terry_chen@wistron.corp-partner.google.com>
2020-11-12 15:29:48 +0800

    DNS: Modify AC prochot and Add DC prochot
    Change-Id: I1a918326a625f294fe6cd9a2ec8e2c5b2af43569

--- driver/charger/isl9241.c
+++ driver/charger/isl9241.c
@@ -332,6 +332,17 @@ int isl9241_set_ac_prochot(int chgnum, int ma)
 	return rv;
 }
 
+int isl9241_set_dc_prochot(int chgnum, int ma)
+{
+	int rv;
+
+	rv = isl9241_write(chgnum, ISL9241_REG_DC_PROCHOT, ma);
+	if (rv)
+		CPRINTF("set_dc_prochot failed (%d)", rv);
+
+	return rv;
+}
+
 /*****************************************************************************/
 /* ISL-9241 initialization */
 static void isl9241_init(int chgnum)
