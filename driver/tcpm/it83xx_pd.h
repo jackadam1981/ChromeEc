1e7fb0d5c8c544e3971e3413bfda0530dc1b03b3
Ruibin Chang <ruibin.chang@ite.com.tw>
2020-11-13 16:41:53 +0800

    tcpm/it8xxx2: add cc parameter tuning function
    Add function for board to tune cc physical parameters.
    
    BUG=none
    BRANCH=none
    TEST=on it81202_pdevb, register 0x3773 and 0x3774 value
         are same as setting.
    
    Signed-off-by: Ruibin Chang <ruibin.chang@ite.com.tw>
    Change-Id: I8829f1fb4672bc98287e9526458f71026e19ca16

--- driver/tcpm/it83xx_pd.h
+++ driver/tcpm/it83xx_pd.h
@@ -202,6 +202,8 @@
 #define USBPD_REG_PLUG_IN_OUT_DETECT_DISABLE      BIT(1)
 #define USBPD_REG_PLUG_IN_OUT_DETECT_STAT         BIT(0)
 #define IT83XX_USBPD_CCPSR0(p)      REG8(IT83XX_USBPD_BASE(p)+0x70)
+#define IT83XX_USBPD_CCPSR3_RISE    REG8(IT83XX_USBPD_BASE(0)+0x73)
+#define IT83XX_USBPD_CCPSR4_FALL    REG8(IT83XX_USBPD_BASE(0)+0x74)
 #endif /* !defined(CONFIG_USB_PD_TCPM_DRIVER_IT83XX) */
 
 /*
@@ -396,8 +398,19 @@ struct usbpd_ctrl_t {
 	uint8_t irq;
 };
 
+/* Data structure for board to adjust pd port rising/falling/delay time */
+struct cc_para_t {
+	/* All ports have the same rising and falling time setting */
+	char rising_time;
+	char falling_time;
+};
+
 extern const struct usbpd_ctrl_t usbpd_ctrl_regs[];
 extern const struct tcpm_drv it83xx_tcpm_drv;
+#ifdef CONFIG_IT83XX_TUNE_CC_PHY
+/* Board level must provides this list of cc parameters */
+extern const struct cc_para_t cc_parameter_list;
+#endif
 void it83xx_Rd_5_1K_only_for_hibernate(int port);
 #ifdef CONFIG_USB_PD_TCPM_DRIVER_IT8XXX2
 void it83xx_clear_tx_error_status(enum usbpd_port port);
--- driver/tcpm/it8xxx2.c
+++ driver/tcpm/it8xxx2.c
@@ -744,6 +744,11 @@ static void it83xx_init(enum usbpd_port port, int role)
 			     IT83XX_USBPD_CC_PIN_CONFIG2 :
 			     IT83XX_USBPD_CC_PIN_CONFIG);
 
+#ifdef CONFIG_IT83XX_TUNE_CC_PHY
+	/* Tune cc physical parameters */
+	IT83XX_USBPD_CCPSR3_RISE = cc_parameter_list.rising_time;
+	IT83XX_USBPD_CCPSR4_FALL = cc_parameter_list.falling_time;
+#endif
 	/* Reset and disable HW auto generate message header */
 	IT83XX_USBPD_PDMSR(port) &= ~USBPD_REG_MASK_DISABLE_AUTO_GEN_TX_HEADER;
 	USBPD_SW_RESET(port);
