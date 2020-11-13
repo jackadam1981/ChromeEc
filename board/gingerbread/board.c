8ad3cb7b3257e529877efe8780ee9c9d00e95d48
Scott Collyer <scollyer@google.com>
2020-11-12 16:31:59 -0800

    honeybuns: account for 128 vs 256k flash.
    fixes issue with when PPC removes dead battery support
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I1988b5837e1d1185a0935a364a11db33028056fd

--- board/gingerbread/board.c
+++ board/gingerbread/board.c
@@ -42,8 +42,8 @@
 #ifdef SECTION_IS_RW
 static int pd_dual_role_init[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	PD_DRP_TOGGLE_ON,
-	PD_DRP_TOGGLE_ON,
-	//PD_DRP_FORCE_SOURCE,
+	//PD_DRP_TOGGLE_ON,
+	PD_DRP_FORCE_SOURCE,
 };
 
 static void ppc_interrupt(enum gpio_signal signal)
@@ -194,9 +194,24 @@ struct ppc_config_t ppc_chips[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 };
 unsigned int ppc_cnt = ARRAY_SIZE(ppc_chips);
 
+void board_reset_pd_mcu(void)
+{
+	cprints(CC_SYSTEM, "Resetting TCPCs...");
+	cflush();
+	gpio_set_level(GPIO_USBC_DP_PD_RST_L, 0);
+	gpio_set_level(GPIO_USBC_UF_RESET_L, 0);
+	msleep(PS8805_FW_INIT_DELAY_MS);
+	gpio_set_level(GPIO_USBC_DP_PD_RST_L, 1);
+	gpio_set_level(GPIO_USBC_UF_RESET_L, 1);
+	msleep(PS8805_FW_INIT_DELAY_MS);
+}
+
+
 /* Power Delivery and charging functions */
 void board_tcpc_init(void)
 {
+	//board_reset_pd_mcu();
+
 	/* Enable PPC interrupts. */
 	gpio_enable_interrupt(GPIO_HOST_USBC_PPC_INT_ODL);
 
@@ -206,7 +221,7 @@ void board_tcpc_init(void)
 	gpio_enable_interrupt(GPIO_DDI_MST_IN_HPD);
 
 }
-DECLARE_HOOK(HOOK_INIT, board_tcpc_init, HOOK_PRIO_INIT_I2C + 1);
+DECLARE_HOOK(HOOK_INIT, board_tcpc_init, HOOK_PRIO_INIT_I2C + 2);
 
 static void board_select_drp_mode(void)
 {
@@ -226,9 +241,9 @@ DECLARE_DEFERRED(board_select_drp_mode);
 static void board_init(void)
 {
 #ifdef SECTION_IS_RW
-	board_select_drp_mode();
+	//board_select_drp_mode();
 	/* TODO */
-	hook_call_deferred(&board_select_drp_mode_data, 25 * MSEC);
+	hook_call_deferred(&board_select_drp_mode_data, 40 * MSEC);
 #endif
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
