58e922070e3340c26bd34ea4a805e2574431f961
Matt Ziegelbaum <ziegs@chromium.org>
2020-11-13 20:00:50 -0500

    ambassador: fix LED color.
    Ambassador has red/white LEDs instead of red/green. They are open drain.
    With this change, amber is actually pink, but there's no constant for
    pink.
    
    BUG=None
    TEST=On device, control with ec console.
    BRANCH=none
    
    Signed-off-by: Matt Ziegelbaum <ziegs@chromium.org>
    Change-Id: Ib9901f3b4c3d088b0990da03411f5b785cc2e096

--- board/ambassador/board.c
+++ board/ambassador/board.c
@@ -255,12 +255,12 @@ const struct pwm_t pwm_channels[] = {
 	[PWM_CH_FAN]        = { .channel = 5,
 				.flags = PWM_CONFIG_OPEN_DRAIN,
 				.freq = 25000},
-	[PWM_CH_LED_RED]    = { .channel = 0,
-				.flags = PWM_CONFIG_ACTIVE_LOW |
+	[PWM_CH_LED_RED]  = { .channel = 0,
+				.flags = PWM_CONFIG_OPEN_DRAIN |
 					 PWM_CONFIG_DSLEEP,
 				.freq = 2000 },
-	[PWM_CH_LED_GREEN]  = { .channel = 2,
-				.flags = PWM_CONFIG_ACTIVE_LOW |
+	[PWM_CH_LED_WHITE]    = { .channel = 2,
+				.flags = PWM_CONFIG_OPEN_DRAIN |
 					 PWM_CONFIG_DSLEEP,
 				.freq = 2000 },
 };
