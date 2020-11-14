c2440bbefab9ed3fd9e555f780296a933c5471da
Denis Brockus <dbrockus@google.com>
2020-11-14 10:24:07 -0700

    genvif: USB-IF cleanup and --no-config
    A couple of the lines would not compare correctly
    because I wanted hex string values to have leading
    0x and USB-IF does not.  USB-IF also does not
    always set "YES" and "NO" for boolean tags, so I
    added an override to make this happen on my output.
    They also did not add the string value for XID_SOP
    so I removed it from mine as well.
    
    genvif has a new option of creating an output
    file based solely on the overrides and not the
    configs. This will make comparisons from other
    generators a little easier.
    
    USAGE: genvif -b|--board <board name>
                  -o|--out <out directory>
                  [-n|--no-config]
                  [-v|--over <override XML file>]
    
    BUG=b:173219559
    BRANCH=none
    TEST=verify XML output
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: I4af5662321e29ed806dea0a2420fd8f4627f8e0f

--- util/genvif.c
+++ util/genvif.c
@@ -1060,15 +1060,27 @@ static void set_override_vif_field(struct vif_field_t *vif_field,
 	}
 
 	vif_field->name = name;
-	if (str_value[0]) {
-		ptr = malloc(strlen(str_value)+1);
-		strcpy(ptr, str_value);
-		vif_field->str_value = ptr;
-	}
-	if (tag_value[0]) {
+	if (tag_value && tag_value[0]) {
 		ptr = malloc(strlen(tag_value)+1);
 		strcpy(ptr, tag_value);
 		vif_field->tag_value = ptr;
+
+		/*
+		 * If the tag_value was provided and the str_value was
+		 * not and this is a boolean value, then fill in the
+		 * str_value
+		 */
+		if (!str_value || str_value[0] == '\0') {
+			if (streq(tag_value, "true"))
+				str_value = "YES";
+			else if (streq(tag_value, "false"))
+				str_value = "NO";
+		}
+	}
+	if (str_value && str_value[0]) {
+		ptr = malloc(strlen(str_value)+1);
+		strcpy(ptr, str_value);
+		vif_field->str_value = ptr;
 	}
 }
 
@@ -1746,23 +1758,36 @@ static void override_vif_app_fields(struct vif_t *vif)
 	while (get_next_tag(name, tag_value, str_value)) {
 		int i;
 
-		if (streq(name, "/App"))
+		if (streq(name, "/VIF_App"))
 			break;
 
 		for (i = 0; i < VIF_App_Indexes; i++)
 			if (streq(name, vif_app_name[i]))
 				break;
-		if (i != VIF_App_Indexes)
-			set_override_vif_field(
-					&vif->vif_app_field[i],
-					vif_app_name[i],
-					tag_value,
-					str_value);
-		else
+		if (i == VIF_App_Indexes)
 			fprintf(stderr,
 				"VIFF/App:"
 				" Unknown tag '%s'\n", name);
 	}
+
+	/*
+	 * Don't care what they requested, I am making the file and that
+	 * means VIF/VIF_App is to be set by me.
+	 */
+	set_override_vif_field(&vif->vif_app_field[Vendor],
+		vif_app_name[Vendor],
+		NULL,
+		VIF_APP_VENDOR_VALUE);
+
+	set_override_vif_field(&vif->vif_app_field[Name],
+		vif_app_name[Name],
+		NULL,
+		VIF_APP_NAME_VALUE);
+
+	set_override_vif_field(&vif->vif_app_field[Version],
+		vif_app_name[Version],
+		NULL,
+		VIF_APP_VERSION_VALUE);
 }
 
 static void override_vif_fields(struct vif_t *vif)
@@ -1776,7 +1801,7 @@ static void override_vif_fields(struct vif_t *vif)
 		if (streq(name, "/VIF"))
 			break;
 
-		if (streq(name, "App"))
+		if (streq(name, "VIF_App"))
 			override_vif_app_fields(vif);
 		else if (streq(name, "Component"))
 			override_vif_component_fields(
@@ -2414,7 +2439,7 @@ static void init_vif_product_fields(struct vif_field_t *vif_fields)
 {
 	char hex_str[10];
 
-	sprintf(hex_str, "0x%04X", USB_VID_GOOGLE);
+	sprintf(hex_str, "%04X", USB_VID_GOOGLE);
 	set_vif_field_itss(&vif_fields[Product_VID],
 		vif_product_name[Product_VID],
 		USB_VID_GOOGLE, hex_str);
@@ -2720,19 +2745,19 @@ static void init_vif_component_general_pd_fields(
 			vif_component_name[Manufacturer_Info_Supported_Port],
 			true);
 
-		sprintf(hex_str, "0x%04X", USB_VID_GOOGLE);
+		sprintf(hex_str, "%04X", USB_VID_GOOGLE);
 		set_vif_field_itss(&vif_fields[Manufacturer_Info_VID_Port],
 			vif_component_name[Manufacturer_Info_VID_Port],
 			USB_VID_GOOGLE, hex_str);
 
 		#if defined(CONFIG_USB_PID)
-			sprintf(hex_str, "0x%04X", CONFIG_USB_PID);
+			sprintf(hex_str, "%04X", CONFIG_USB_PID);
 			set_vif_field_itss(&vif_fields[
 				Manufacturer_Info_PID_Port],
 				vif_component_name[Manufacturer_Info_PID_Port],
 				CONFIG_USB_PID, hex_str);
 		#else
-			sprintf(hex_str, "0x%04X", DEFAULT_MISSING_PID);
+			sprintf(hex_str, "%04X", DEFAULT_MISSING_PID);
 			set_vif_field_itss(&vif_fields[
 				Manufacturer_Info_PID_Port],
 				vif_component_name[Manufacturer_Info_PID_Port],
@@ -3266,15 +3291,15 @@ static void init_vif_component_sop_discovery_fields(
 	set_vif_field(&vif_fields[XID_SOP],
 		vif_component_name[XID_SOP],
 		"0",
-		"0");
+		NULL);
 
 	#if defined(CONFIG_USB_PID)
-		sprintf(hex_str, "0x%04X", CONFIG_USB_PID);
+		sprintf(hex_str, "%04X", CONFIG_USB_PID);
 		set_vif_field_itss(&vif_fields[PID_SOP],
 			vif_component_name[PID_SOP],
 			CONFIG_USB_PID, hex_str);
 	#else
-		sprintf(hex_str, "0x%04X", DEFAULT_MISSING_PID);
+		sprintf(hex_str, "%04X", DEFAULT_MISSING_PID);
 		set_vif_field_itss(&vif_fields[PID_SOP],
 			vif_component_name[PID_SOP],
 			DEFAULT_MISSING_PID, hex_str);
@@ -3328,9 +3353,7 @@ static void init_vif_component_product_power_fields(
 			"0", "Assured");
 }
 
-static int gen_vif(const char *name,
-		   const char *board,
-		   const char *vif_producer,
+static int gen_vif(const char *board,
 		   struct vif_t *vif)
 {
 	int override_value;
@@ -3453,10 +3476,7 @@ static int gen_vif(const char *name,
 			src_max_power,
 			type);
 
-	/*********************************************************************
-	 * Format the structure in XML and output it to file
-	 */
-	return vif_output_xml(name, vif);
+	return 0;
 }
 /*
  * VIF Structure Initialization from Config Functions
@@ -3468,20 +3488,19 @@ int main(int argc, char **argv)
 	int ret;
 	const char *out = NULL;
 	const char *board = NULL;
-	const char *vif_producer;
+	bool do_config_init = true;
 	DIR *vifdir;
 	char *name;
 	int name_size;
-	const char * const short_opt = "hb:o:";
+	const char * const short_opt = "hb:o:nv:";
 	const struct option long_opts[] = {
-		{ "help",  0, NULL, 'h' },
-		{ "board", 1, NULL, 'b' },
-		{ "out",   1, NULL, 'o' },
-		{ "over",  1, NULL, 'v'},
+		{ "help",      0, NULL, 'h' },
+		{ "board",     1, NULL, 'b' },
+		{ "out",       1, NULL, 'o' },
+		{ "no-config", 0, NULL, 'n' },
+		{ "over",      1, NULL, 'v' },
 		{ NULL }
 	};
-
-	vif_producer = argv[0];
 
 	/* Clear the VIF structure */
 	memset(&vif, 0, sizeof(struct vif_t));
@@ -3490,8 +3509,10 @@ int main(int argc, char **argv)
 		nopt = getopt_long(argc, argv, short_opt, long_opts, NULL);
 		switch (nopt) {
 		case 'h': /* -h or --help */
-			printf("USAGE: %s -b <board name> -o <out directory>"
-				" --over <override XML file>\n", vif_producer);
+			printf("USAGE: genvif -b|--board <board name>\n"
+			       "              -o|--out <out directory>\n"
+			       "              [-n|--no-config]\n"
+			       "              [-v|--over <override XML file>]\n");
 			return 1;
 
 		case 'b': /* -b or --board */
@@ -3502,7 +3523,11 @@ int main(int argc, char **argv)
 			out = optarg;
 			break;
 
-		case 'v': /* --over */
+		case 'n': /* -n or --no-config */
+			do_config_init = false;
+			break;
+
+		case 'v': /* -v or --over */
 			/* Handle overrides */
 			if (override_gen_vif(optarg, &vif))
 				return 1;
@@ -3529,14 +3554,21 @@ int main(int argc, char **argv)
 
 	init_src_pdos();
 
+	/* Finish CONFIG initialization file */
+	if (do_config_init) {
+		ret = gen_vif(board, &vif);
+		if (ret)
+			return 1;
+	}
+
 	name_size = asprintf(&name, "%s/%s_vif.xml", out, board);
 	if (name_size < 0) {
 		fprintf(stderr, "ERROR: Out of memory.\n");
 		return 1;
 	}
 
-	/* Finish CONFIG initialization and output the file */
-	ret = gen_vif(name, board, vif_producer, &vif);
+	/* Format the structure in XML and output it to file */
+	ret = vif_output_xml(name, &vif);
 
 	free(name);
 	return ret;
