1fbc386556073e738c0e6a4c4530038f8ae1c1eb
Aseda Aboagye <aaboagye@google.com>
2020-11-16 11:46:38 -0800

    TEST: DG SM CV
    Change-Id: I9891bccd8aa8cabcb6b6abbd906e423d608fb051

--- common/ocpc.c
+++ common/ocpc.c
@@ -54,16 +54,6 @@ static int k_d_div = KD_DIV;
 static int debug_output;
 static int viz_output;
 
-#define NUM_RESISTANCE_SAMPLES 8
-static int resistance_tbl[NUM_RESISTANCE_SAMPLES] = {
-	CONFIG_OCPC_DEF_RBATT_MOHMS
-};
-static int resistance_tbl_idx;
-static int mean_resistance;
-static int stddev_resistance;
-static int ub;
-static int lb;
-
 enum phase {
 	PHASE_UNKNOWN = -1,
 	PHASE_CC,
@@ -75,37 +65,10 @@ __overridable void board_ocpc_init(struct ocpc_data *ocpc)
 {
 }
 
-static void calc_resistance_stats(void)
-{
-	int i;
-	int sum = 0;
-
-	/* Calculate mean */
-	for (i = 0; i < NUM_RESISTANCE_SAMPLES; i++)
-		sum += resistance_tbl[i];
-
-	mean_resistance = sum / NUM_RESISTANCE_SAMPLES;
-
-	/* Calculate standard deviation */
-	sum = 0;
-	for (i = 0; i < NUM_RESISTANCE_SAMPLES; i++)
-		sum += POW2(resistance_tbl[i] - mean_resistance);
-
-	stddev_resistance = fp_sqrtf(INT_TO_FP(sum / NUM_RESISTANCE_SAMPLES));
-	stddev_resistance = FP_TO_INT(stddev_resistance);
-	CPRINTS_DBG("Rbatt+Rsys: mean: %d stddev: %d", mean_resistance,
-		    stddev_resistance);
-	lb = MAX(0, mean_resistance - (3 * stddev_resistance));
-	ub = mean_resistance + (3 * stddev_resistance);
-}
-
 enum ec_error_list ocpc_calc_resistances(struct ocpc_data *ocpc,
 					 struct batt_params *battery)
 {
 	int act_chg = ocpc->active_chg_chip;
-	static bool seeded;
-	static int initial_samples;
-	int val;
 
 	/*
 	 * In order to actually calculate the resistance, we need to make sure
@@ -130,36 +93,19 @@ enum ec_error_list ocpc_calc_resistances(struct ocpc_data *ocpc,
 		 * There's no provision to measure Isys, so we cannot separate
 		 * out Rsys from Rbatt.
 		 */
-		val = ((ocpc->vsys_aux_mv - battery->voltage) * 1000) /
-			battery->current;
+		ocpc->combined_rsys_rbatt_mo = ((ocpc->vsys_aux_mv -
+						 battery->voltage) * 1000) /
+						 battery->current;
+		CPRINTS_DBG("Rsys+Rbatt: %dmOhm", ocpc->combined_rsys_rbatt_mo);
 	} else {
 		ocpc->rsys_mo = ((ocpc->vsys_aux_mv - ocpc->vsys_mv) * 1000) /
 				 ocpc->isys_ma;
 		ocpc->rbatt_mo = ((ocpc->vsys_mv - battery->voltage) * 1000) /
 				 battery->current;
-		val = ocpc->rsys_mo + ocpc->rbatt_mo;
+		ocpc->combined_rsys_rbatt_mo = ocpc->rsys_mo + ocpc->rbatt_mo;
 		CPRINTS_DBG("Rsys: %dmOhm Rbatt: %dmOhm", ocpc->rsys_mo,
 			    ocpc->rbatt_mo);
 	}
-
-	/* Discard measurements not within a 6 std dev window. */
-	if ((!seeded) ||
-	    (seeded && ((val <= ub) && (val >= lb)))) {
-		resistance_tbl[resistance_tbl_idx] = val;
-		calc_resistance_stats();
-		resistance_tbl_idx = (resistance_tbl_idx + 1) %
-					NUM_RESISTANCE_SAMPLES;
-	}
-
-	if (seeded) {
-		ocpc->combined_rsys_rbatt_mo = MAX(mean_resistance,
-						   CONFIG_OCPC_DEF_RBATT_MOHMS);
-		CPRINTS_DBG("Rsys+Rbatt: %dmOhm", ocpc->combined_rsys_rbatt_mo);
-	} else {
-		seeded = ++initial_samples >= (2 * NUM_RESISTANCE_SAMPLES) ?
-			true : false;
-	}
-
 	return EC_SUCCESS;
 }
 
@@ -174,6 +120,7 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	int vsys_target = 0;
 	int drive = 0;
 	int i_ma = 0;
+	static int i_ma_CC_CV;
 	int min_vsys_target;
 	int error = 0;
 	int derivative = 0;
@@ -182,6 +129,7 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	int chgnum;
 	enum ec_error_list result;
 	static int iterations;
+	static int FIRST_TIME;
 	int i_step;
 	static timestamp_t delay;
 	int i, step, loc;
@@ -251,6 +199,7 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	if (ocpc->last_vsys == OCPC_UNINIT) {
 		ph = PHASE_UNKNOWN;
 		iterations = 0;
+		FIRST_TIME = 0;
 	}
 
 
@@ -269,8 +218,7 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	 * If the system is in S5/G3, we can calculate the board and battery
 	 * resistances.
 	 */
-	if (chipset_in_state(CHIPSET_STATE_ANY_OFF |
-			     CHIPSET_STATE_ANY_SUSPEND)) {
+	if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
 		/*
 		 * In the first few iterations of the loop, charging isn't
 		 * stable/correct so making the calculation then leads to some
@@ -300,6 +248,10 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 			 */
 			i_ma = batt.current;
 			ph = ph == PHASE_CC ? PHASE_CV_TRIP : PHASE_CV_COMPLETE;
+			if ((ph == PHASE_CV_TRIP)&&(FIRST_TIME == 0)){
+				i_ma_CC_CV = batt.current;
+				FIRST_TIME = 1;
+			}
 
 		}
 	}
@@ -375,11 +327,16 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	 * Once we're in the CV region, all we need to do is keep VSYS at the
 	 * desired voltage.
 	 */
-	if (ph >= PHASE_CV_TRIP)
+	if (ph == PHASE_CV_TRIP){
+		vsys_target = batt.desired_voltage +
+				(( i_ma_CC_CV *
+				  ocpc->combined_rsys_rbatt_mo) / 1000);
+		CPRINTS_DBG("i_ma_CC_CV = %d", i_ma_CC_CV);
+	}
+	if (ph == PHASE_CV_COMPLETE)
 		vsys_target = batt.desired_voltage +
 				((batt_info->precharge_current *
 				  ocpc->combined_rsys_rbatt_mo) / 1000);
-
 	/*
 	 * Ensure VSYS is no higher than the specified maximum battery voltage
 	 * plus the voltage drop across the system.
