b1920de8d1bc144f0a7e72b9f9e01165d06fe9b3
Aseda Aboagye <aaboagye@google.com>
2020-11-16 11:46:38 -0800

    TEST: DG SM CV
    Change-Id: I9891bccd8aa8cabcb6b6abbd906e423d608fb051

--- common/ocpc.c
+++ common/ocpc.c
@@ -174,6 +174,7 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	int vsys_target = 0;
 	int drive = 0;
 	int i_ma = 0;
+	static int i_ma_CC_CV;
 	int min_vsys_target;
 	int error = 0;
 	int derivative = 0;
@@ -182,6 +183,7 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	int chgnum;
 	enum ec_error_list result;
 	static int iterations;
+	static int FIRST_TIME;
 	int i_step;
 	static timestamp_t delay;
 	int i, step, loc;
@@ -251,6 +253,7 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	if (ocpc->last_vsys == OCPC_UNINIT) {
 		ph = PHASE_UNKNOWN;
 		iterations = 0;
+		FIRST_TIME = 0;
 	}
 
 
@@ -300,6 +303,10 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 			 */
 			i_ma = batt.current;
 			ph = ph == PHASE_CC ? PHASE_CV_TRIP : PHASE_CV_COMPLETE;
+			if ((ph == PHASE_CV_TRIP)&&(FIRST_TIME == 0)){
+				i_ma_CC_CV = batt.current;
+				FIRST_TIME = 1;
+			}
 
 		}
 	}
@@ -375,11 +382,16 @@ int ocpc_config_secondary_charger(int *desired_input_current,
 	 * Once we're in the CV region, all we need to do is keep VSYS at the
 	 * desired voltage.
 	 */
-	if (ph >= PHASE_CV_TRIP)
+	if (ph == PHASE_CV_TRIP){
+		vsys_target = batt.desired_voltage +
+				(( i_ma_CC_CV *
+				  ocpc->combined_rsys_rbatt_mo) / 1000);
+		CPRINTS_DBG("i_ma_CC_CV = %d", i_ma_CC_CV);
+	}
+	if (ph == PHASE_CV_COMPLETE)
 		vsys_target = batt.desired_voltage +
 				((batt_info->precharge_current *
 				  ocpc->combined_rsys_rbatt_mo) / 1000);
-
 	/*
 	 * Ensure VSYS is no higher than the specified maximum battery voltage
 	 * plus the voltage drop across the system.
