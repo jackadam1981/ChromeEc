5c70fbf61201ca5b564740c3a729085906c61174
Eric Herrmann <eherrmann@chromium.org>
2020-11-19 12:25:39 -0800

    Delbin: Remove USB4 code to save space
    Delbin doesn't support USB4 but some unused drivers were still being
    compiled for it, remove these to save space.
    
    Before: 4960 bytes RO, 4944 bytes RW
    After: 9376 bytes RO, 9344 bytes RW
    
    BUG=none
    TEST=make BOARD=delbin
    BRANCH=none
    
    Signed-off-by: Eric Herrmann <eherrmann@chromium.org>
    Change-Id: I904046f50af377c3a932d842becf896e526f9462

--- board/delbin/board.c
+++ board/delbin/board.c
@@ -4,7 +4,6 @@
  */
 
 /* Volteer board-specific configuration */
-#include "bb_retimer.h"
 #include "button.h"
 #include "common.h"
 #include "accelgyro.h"
@@ -13,7 +12,6 @@
 #include "driver/accelgyro_bmi260.h"
 #include "driver/bc12/pi3usb9201.h"
 #include "driver/ppc/syv682x.h"
-#include "driver/retimer/bb_retimer.h"
 #include "driver/tcpm/ps8xxx.h"
 #include "driver/tcpm/tcpci.h"
 #include "driver/tcpm/tusb422.h"
@@ -36,7 +34,6 @@
 #include "uart.h"
 #include "usb_mux.h"
 #include "usb_pd.h"
-#include "usb_pd_tbt.h"
 #include "usb_pd_tcpm.h"
 #include "usbc_ppc.h"
 #include "util.h"
@@ -74,47 +71,6 @@ static void board_init(void)
 {
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
-
-__override enum tbt_compat_cable_speed board_get_max_tbt_speed(int port)
-{
-	enum ec_cfg_usb_db_type usb_db = ec_cfg_usb_db_type();
-
-	if (port == USBC_PORT_C1) {
-		if (usb_db == DB_USB4_GEN2) {
-			/*
-			 * Older boards violate 205mm trace length prior
-			 * to connection to the re-timer and only support up
-			 * to GEN2 speeds.
-			 */
-			return TBT_SS_U32_GEN1_GEN2;
-		} else if (usb_db == DB_USB4_GEN3) {
-			return TBT_SS_TBT_GEN3;
-		}
-	}
-
-	/*
-	 * Thunderbolt-compatible mode not supported
-	 *
-	 * TODO (b/147726366): All the USB-C ports need to support same speed.
-	 * Need to fix once USB-C feature set is known for Volteer.
-	 */
-	return TBT_SS_RES_0;
-}
-
-__override bool board_is_tbt_usb4_port(int port)
-{
-	enum ec_cfg_usb_db_type usb_db = ec_cfg_usb_db_type();
-
-	/*
-	 * Volteer reference design only supports TBT & USB4 on port 1
-	 * if the USB4 DB is present.
-	 *
-	 * TODO (b/147732807): All the USB-C ports need to support same
-	 * features. Need to fix once USB-C feature set is known for Volteer.
-	 */
-	return ((port == USBC_PORT_C1)
-		&& ((usb_db == DB_USB4_GEN2) || (usb_db == DB_USB4_GEN3)));
-}
 
 /******************************************************************************/
 /* Physical fans. These are logically separate from pwm_channels. */
@@ -327,11 +283,6 @@ static const struct usb_mux mux_config_p1_usb3 = {
 	.next_mux = &usbc1_usb3_db_retimer,
 };
 
-static const struct bb_usb_control bb_p0_control = {
-	.usb_ls_en_gpio = GPIO_USB_C0_LS_EN,
-	.retimer_rst_gpio = GPIO_USB_C0_RT_RST_ODL,
-};
-
 /******************************************************************************/
 /* USB-A charging control */
 
@@ -389,7 +340,6 @@ __override void board_cbi_init(void)
 	/* Config MB USB3 */
 	tcpc_config[USBC_PORT_C0] = tcpc_config_p0_usb3;
 	usb_muxes[USBC_PORT_C0] = mux_config_p0_usb3;
-	bb_controls[USBC_PORT_C0] = bb_p0_control;
 
 	/* Config DB USB3 */
 	tcpc_config[USBC_PORT_C1] = tcpc_config_p1_usb3;
@@ -504,12 +454,6 @@ BUILD_ASSERT(CONFIG_USB_PD_PORT_MAX_COUNT == USBC_PORT_COUNT);
 
 /******************************************************************************/
 /* USBC mux configuration - Tiger Lake includes internal mux */
-struct usb_mux usbc1_usb4_db_retimer = {
-	.usb_port = USBC_PORT_C1,
-	.driver = &bb_usb_retimer,
-	.i2c_port = I2C_PORT_USB_1_MIX,
-	.i2c_addr_flags = USBC_PORT_C1_BB_RETIMER_I2C_ADDR,
-};
 struct usb_mux usb_muxes[] = {
 	[USBC_PORT_C0] = {
 		.usb_port = USBC_PORT_C0,
@@ -520,21 +464,9 @@ struct usb_mux usb_muxes[] = {
 		.usb_port = USBC_PORT_C1,
 		.driver = &virtual_usb_mux_driver,
 		.hpd_update = &virtual_hpd_update,
-		.next_mux = &usbc1_usb4_db_retimer,
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(usb_muxes) == USBC_PORT_COUNT);
-
-struct bb_usb_control bb_controls[] = {
-	[USBC_PORT_C0] = {
-		/* USB-C port 0 doesn't have a retimer */
-	},
-	[USBC_PORT_C1] = {
-		.usb_ls_en_gpio = GPIO_USB_C1_LS_EN,
-		.retimer_rst_gpio = GPIO_USB_C1_RT_RST_ODL,
-	},
-};
-BUILD_ASSERT(ARRAY_SIZE(bb_controls) == USBC_PORT_COUNT);
 
 static void board_tcpc_init(void)
 {
