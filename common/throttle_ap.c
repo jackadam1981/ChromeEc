51e7a4e7187e4c356db8769e806ff94ca3d535a9
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2020-11-20 11:20:12 +0800

    eldrid: ignore PROCHOT when system off
    BUG=b:173180788
    BRANCH=firmware-volteer-13521.B-master
    TEST=make buildall
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I0896dc0e9dcb45a232d7109588b95bf4ee1e6bb8

--- common/throttle_ap.c
+++ common/throttle_ap.c
@@ -97,7 +97,14 @@ static void prochot_input_deferred(void)
 
 	debounced_prochot_in = prochot_in;
 
-	if (debounced_prochot_in) {
+	/*
+	 * b/173180788 Confirmed from Intel internal that SLP_S3# asserts low
+	 * about 10us before PROCHOT# asserts low, which means that
+	 * the CPU is already in reset and therefore the PROCHOT#
+	 * asserting low is normal behavior and not a concern
+	 * for PROCHOT# event.
+	 */
+	if (debounced_prochot_in && !chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
 		CPRINTS("External PROCHOT assertion detected");
 #ifdef CONFIG_FANS
 		dptf_set_fan_duty_target(100);
