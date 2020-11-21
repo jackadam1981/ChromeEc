786dada185ce2d80bc3c91c9a66c4bd4d449536f
Nicolas Boichat <drinkcat@chromium.org>
2020-11-21 21:14:42 +0800

    WIP: mcount
    Change-Id: I6be51d2e8ecb0d423681b7c60ef23e68cb04c74b

--- core/riscv-rv32i/cpu.c
+++ core/riscv-rv32i/cpu.c
@@ -5,10 +5,26 @@
  * Set up the RISC-V core
  */
 
+#include "common.h"
 #include "cpu.h"
 
 void cpu_init(void)
 {
 	/* bit3: Global interrupt enable (M-mode) */
 	asm volatile ("csrsi mstatus, 0x8");
+}
+
+/* TODO: Move to separate file. */
+struct entry {
+	void *func;
+	int count;
+};
+
+struct entry entries[1024];
+
+void __attribute__((no_instrument_function)) _mcount(void)
+{
+	void __unused *func = __builtin_return_address(0);
+
+	// record the address in entries
 }
