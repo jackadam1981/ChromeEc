FrameworkComputer/EmbeddedController

commit d28c10498cdbf007b97b5c0a9a951373574d4eea

motion_sense: Make change in range permanent
When AP changes range, unlike offset or ODR, it was not surviving init()
call. If the sensor is powered off in S3, at resume the range would be
back to the default.

To make it consistent with other attributes, remember range change until
EC powers down.

- remove get_range
- add current_range to store the range currently used.
  This is modifiable by the AP
- when the AP shutdown, revert current_range to default_range
- Remove const attribute for sensor structure when init and set_range is
  called.

BUG=chromium:1083791
BRANCH=none
TEST=One eve branch, check range is preserved even after 'shutdown -h 0'

Change-Id: Ia7126ac0cc9c3fef60b4464d95d6dd15e64b0fc4
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2215751
Reviewed-by: Yuval Peress <peress@chromium.org>

driver/als_tcs3400.c
@@ -600,25 +600,19 @@ static int tcs3400_rgb_set_data_rate(const struct motion_sensor_t *s,
 }
 
 /* Enable/disable special factory calibration mode */
-static int tcs3400_perform_calib(const struct motion_sensor_t *s,
-				     int enable)
+static int tcs3400_perform_calib(struct motion_sensor_t *s, int enable)
 {
 	TCS3400_RGB_DRV_DATA(s+1)->calibration_mode = enable;
 	return EC_SUCCESS;
 }
 
-static int tcs3400_get_range(const struct motion_sensor_t *s)
-{
-	return (TCS3400_DRV_DATA(s)->als_cal.scale << 16) |
-			(TCS3400_DRV_DATA(s)->als_cal.uscale);
-}
-
-static int tcs3400_set_range(const struct motion_sensor_t *s,
+static int tcs3400_set_range(struct motion_sensor_t *s,
 			     int range,
 			     int rnd)
 {
 	TCS3400_DRV_DATA(s)->als_cal.scale = range >> 16;
 	TCS3400_DRV_DATA(s)->als_cal.uscale = range & 0xffff;
+	s->current_range = range;
 	return EC_SUCCESS;
 }
 
@@ -703,12 +697,12 @@ static int tcs3400_set_data_rate(const struct motion_sensor_t *s,
 /**
  * Initialise TCS3400 light sensor.
  */
-static int tcs3400_rgb_init(const struct motion_sensor_t *s)
+static int tcs3400_rgb_init(struct motion_sensor_t *s)
 {
 	return EC_SUCCESS;
 }
 
-static int tcs3400_init(const struct motion_sensor_t *s)
+static int tcs3400_init(struct motion_sensor_t *s)
 {
 	/*
 	 * These are default power-on register values with two exceptions:
@@ -761,7 +755,6 @@ const struct accelgyro_drv tcs3400_drv = {
 	.init = tcs3400_init,
 	.read = tcs3400_read,
 	.set_range = tcs3400_set_range,
-	.get_range = tcs3400_get_range,
 	.set_offset = tcs3400_set_offset,
 	.get_offset = tcs3400_get_offset,
 	.set_scale = tcs3400_set_scale,

common/body_detection.c
@@ -169,7 +169,6 @@ static void determine_threshold_scale(int range, int resolution, int rms_noise)
 void body_detect_reset(void)
 {
 	int odr = body_sensor->drv->get_data_rate(body_sensor);
-	int range = body_sensor->drv->get_range(body_sensor);
 	int resolution = body_sensor->drv->get_resolution(body_sensor);
 	int rms_noise = body_sensor->drv->get_rms_noise(body_sensor);
 
@@ -181,7 +180,8 @@ void body_detect_reset(void)
 	if (odr == 0)
 		return;
 	determine_window_size(odr);
-	determine_threshold_scale(range, resolution, rms_noise);
+	determine_threshold_scale(body_sensor->current_range,
+				  resolution, rms_noise);
 	/* initialize motion data and state */
 	memset(data, 0, sizeof(data));
 	history_idx = 0;

common/motion_lid.c
@@ -307,10 +307,8 @@ static int calculate_lid_angle(const intv3_t base, const intv3_t lid,
 	 * possible.
 	 */
 	for (i = X; i <= Z; i++) {
-		scaled_base[i] = base[i] *
-			accel_base->drv->get_range(accel_base);
-		scaled_lid[i] = lid[i] *
-			accel_lid->drv->get_range(accel_lid);
+		scaled_base[i] = base[i] * accel_base->current_range;
+		scaled_lid[i] = lid[i] * accel_lid->current_range;
 		if (ABS(scaled_base[i]) > MOTION_SCALING_AXIS_MAX ||
 		    ABS(scaled_lid[i]) > MOTION_SCALING_AXIS_MAX) {
 			reliable = 0;

common/motion_sense.c
@@ -339,19 +339,19 @@ static inline int motion_sense_init(struct motion_sensor_t *sensor)
  *
  * Called by init routine of each sensors when successful.
  */
-int sensor_init_done(const struct motion_sensor_t *s)
+int sensor_init_done(struct motion_sensor_t *s)
 {
 	int ret;
 
-	ret = s->drv->set_range(s, BASE_RANGE(s->default_range),
-				!!(s->default_range & ROUND_UP_FLAG));
+	ret = s->drv->set_range(s, BASE_RANGE(s->current_range),
+				!!(s->current_range & ROUND_UP_FLAG));
 	if (ret == EC_RES_SUCCESS) {
 		if (IS_ENABLED(CONFIG_CONSOLE_VERBOSE))
 			CPRINTS("%s: MS Done Init type:0x%X range:%d",
-				s->name, s->type, s->drv->get_range(s));
+				s->name, s->type, s->current_range);
 		else
 			CPRINTS("%c%d InitDone r:%d", s->name[0], s->type,
-				s->drv->get_range(s));
+				s->current_range);
 	}
 	return ret;
 }
@@ -369,9 +369,15 @@ static void motion_sense_switch_sensor_rate(void)
 	for (i = 0; i < motion_sensor_count; ++i) {
 		sensor = &motion_sensors[i];
 		if (SENSOR_ACTIVE(sensor)) {
-			/* Initialize or just back the odr previously set. */
+			/*
+			 * Initialize or just back the odr/range previously
+			 * set.
+			 */
 			if (sensor->state == SENSOR_INITIALIZED) {
 				motion_sense_set_data_rate(sensor);
+				sensor->drv->set_range(sensor,
+						       sensor->current_range,
+						       1);
 			} else {
 				ret = motion_sense_init(sensor);
 				if (ret != EC_SUCCESS)
@@ -408,6 +414,7 @@ static void motion_sense_shutdown(void)
 		/* Forget about changes made by the AP */
 		sensor->config[SENSOR_CONFIG_AP].odr = 0;
 		sensor->config[SENSOR_CONFIG_AP].ec_rate = 0;
+		sensor->current_range = sensor->default_range;
 	}
 	motion_sense_switch_sensor_rate();
 
@@ -1145,10 +1152,7 @@ static enum ec_status host_cmd_motion_sense(struct host_cmd_handler_args *args)
 			}
 		}
 
-		if (!sensor->drv->get_range)
-			return EC_RES_INVALID_COMMAND;
-
-		out->sensor_range.ret = sensor->drv->get_range(sensor);
+		out->sensor_range.ret = sensor->current_range;
 		args->response_size = sizeof(out->sensor_range);
 		break;
 
@@ -1485,8 +1489,7 @@ static int command_accelrange(int argc, char **argv)
 					   round) == EC_ERROR_INVAL)
 			return EC_ERROR_PARAM2;
 	} else {
-		ccprintf("Range for sensor %d: %d\n", id,
-			 sensor->drv->get_range(sensor));
+		ccprintf("Sensor %d range: %d\n", id, sensor->current_range);
 	}
 
 	return EC_SUCCESS;
@@ -1684,7 +1687,7 @@ static int command_display_accel_info(int argc, char **argv)
 		ccprintf("port: %d\n", motion_sensors[i].port);
 		ccprintf("addr: %d\n", I2C_STRIP_FLAGS(motion_sensors[i]
 						    .i2c_spi_addr_flags));
-		ccprintf("range: %d\n", motion_sensors[i].default_range);
+		ccprintf("range: %d\n", motion_sensors[i].current_range);
 		ccprintf("min_freq: %d\n", motion_sensors[i].min_frequency);
 		ccprintf("max_freq: %d\n", motion_sensors[i].max_frequency);
 		ccprintf("config:\n");

common/online_calibration.c
@@ -61,7 +61,7 @@ static void data_int16_to_fp(const struct motion_sensor_t *s,
 			     const int16_t *data, fpv3_t out)
 {
 	int i;
-	fp_t range = INT_TO_FP(s->drv->get_range(s));
+	fp_t range = INT_TO_FP(s->current_range);
 
 	for (i = 0; i < 3; ++i) {
 		fp_t v = INT_TO_FP((int32_t)data[i]);
@@ -77,7 +77,7 @@ static void data_fp_to_int16(const struct motion_sensor_t *s, const fpv3_t data,
 			     int16_t *out)
 {
 	int i;
-	fp_t range = INT_TO_FP(s->drv->get_range(s));
+	fp_t range = INT_TO_FP(s->current_range);
 
 	for (i = 0; i < 3; ++i) {
 		int32_t iv;

driver/accel_bma2x2.c
@@ -43,10 +43,9 @@ static inline int raw_write8(const int port, const uint16_t i2c_addr_flags,
 	return i2c_write8(port, i2c_addr_flags, reg, data);
 }
 
-static int set_range(const struct motion_sensor_t *s, int range, int rnd)
+static int set_range(struct motion_sensor_t *s, int range, int rnd)
 {
 	int ret,  range_val, reg_val, range_reg_val;
-	struct accelgyro_saved_data_t *data = s->drv_data;
 
 	range_val = BMA2x2_RANGE_TO_REG(range);
 	if ((BMA2x2_RANGE_TO_REG(range_val) < range) && rnd)
@@ -67,18 +66,11 @@ static int set_range(const struct motion_sensor_t *s, int range, int rnd)
 
 	/* If successfully written, then save the range. */
 	if (ret == EC_SUCCESS)
-		data->range = BMA2x2_REG_TO_RANGE(range_val);
+		s->current_range = BMA2x2_REG_TO_RANGE(range_val);
 
 	mutex_unlock(s->mutex);
 
 	return ret;
-}
-
-static int get_range(const struct motion_sensor_t *s)
-{
-	struct accelgyro_saved_data_t *data = s->drv_data;
-
-	return data->range;
 }
 
 static int get_resolution(const struct motion_sensor_t *s)
@@ -197,7 +189,7 @@ static int read(const struct motion_sensor_t *s, intv3_t v)
 	return EC_SUCCESS;
 }
 
-static int perform_calib(const struct motion_sensor_t *s, int enable)
+static int perform_calib(struct motion_sensor_t *s, int enable)
 {
 	int ret, val, status, rate, range, i;
 	timestamp_t deadline;
@@ -213,7 +205,7 @@ static int perform_calib(const struct motion_sensor_t *s, int enable)
 		return EC_ERROR_ACCESS_DENIED;
 
 	rate = get_data_rate(s);
-	range = get_range(s);
+	range = s->current_range;
 	/*
 	 * Temporary set frequency to 100Hz to get enough data in a short
 	 * period of time.
@@ -261,7 +253,7 @@ end_perform_calib:
 	return ret;
 }
 
-static int init(const struct motion_sensor_t *s)
+static int init(struct motion_sensor_t *s)
 {
 	int ret = 0, tries = 0, val, reg, reset_field;
 
@@ -319,7 +311,6 @@ const struct accelgyro_drv bma2x2_accel_drv = {
 	.init = init,
 	.read = read,
 	.set_range = set_range,
-	.get_range = get_range,
 	.get_resolution = get_resolution,
 	.set_data_rate = set_data_rate,
 	.get_data_rate = get_data_rate,

driver/accel_kionix.c
@@ -321,10 +321,9 @@ static int set_value(const struct motion_sensor_t *s, int reg, int val,
 	return ret;
 }
 
-static int set_range(const struct motion_sensor_t *s, int range, int rnd)
+static int set_range(struct motion_sensor_t *s, int range, int rnd)
 {
 	int ret, index, reg, range_field, range_val;
-	struct kionix_accel_data *data = s->drv_data;
 
 	/* Find index for interface pair matching the specified range. */
 	index = find_param_index(range, rnd, ranges[T(s)],
@@ -335,15 +334,8 @@ static int set_range(const struct motion_sensor_t *s, int range, int rnd)
 
 	ret = set_value(s, reg, range_val, range_field);
 	if (ret == EC_SUCCESS)
-		data->base.range = ranges[T(s)][index].val;
+		s->current_range = ranges[T(s)][index].val;
 	return ret;
-}
-
-static int get_range(const struct motion_sensor_t *s)
-{
-	struct kionix_accel_data *data = s->drv_data;
-
-	return data->base.range;
 }
 
 static int set_resolution(const struct motion_sensor_t *s, int res, int rnd)
@@ -490,7 +482,7 @@ static int read(const struct motion_sensor_t *s, intv3_t v)
 {
 	uint8_t acc[6];
 	uint8_t reg;
-	int ret, i, range, resolution;
+	int ret, i, resolution;
 	struct kionix_accel_data *data = s->drv_data;
 
 	/* Read 6 bytes starting at XOUT_L. */
@@ -535,14 +527,13 @@ static int read(const struct motion_sensor_t *s, intv3_t v)
 	rotate(v, *s->rot_standard_ref, v);
 
 	/* apply offset in the device coordinates */
-	range = get_range(s);
 	for (i = X; i <= Z; i++)
-		v[i] += (data->offset[i] << 5) / range;
+		v[i] += (data->offset[i] << 5) / s->current_range;
 
 	return EC_SUCCESS;
 }
 
-static int init(const struct motion_sensor_t *s)
+static int init(struct motion_sensor_t *s)
 {
 	int ret, val, reg, reset_field;
 	uint8_t timeout;
@@ -677,7 +668,6 @@ const struct accelgyro_drv kionix_accel_drv = {
 	.init = init,
 	.read = read,
 	.set_range = set_range,
-	.get_range = get_range,
 	.set_resolution = set_resolution,
 	.get_resolution = get_resolution,
 	.set_data_rate = set_data_rate,

driver/accel_lis2dh.c
@@ -30,10 +30,9 @@
  * @range: Range
  * @rnd: Round up/down flag
  */
-static int set_range(const struct motion_sensor_t *s, int range, int rnd)
+static int set_range(struct motion_sensor_t *s, int range, int rnd)
 {
 	int err, normalized_range;
-	struct stprivate_data *data = s->drv_data;
 	int val;
 
 	val = LIS2DH_FS_TO_REG(range);
@@ -63,17 +62,10 @@ static int set_range(const struct motion_sensor_t *s, int range, int rnd)
 
 	/* Save Gain in range for speed up data path */
 	if (err == EC_SUCCESS)
-		data->base.range = normalized_range;
+		s->current_range = normalized_range;
 
 	mutex_unlock(s->mutex);
 	return EC_SUCCESS;
-}
-
-static int get_range(const struct motion_sensor_t *s)
-{
-	struct stprivate_data *data = s->drv_data;
-
-	return data->base.range;
 }
 
 static int set_data_rate(const struct motion_sensor_t *s, int rate, int rnd)
@@ -168,7 +160,7 @@ static int read(const struct motion_sensor_t *s, intv3_t v)
 	return EC_SUCCESS;
 }
 
-static int init(const struct motion_sensor_t *s)
+static int init(struct motion_sensor_t *s)
 {
 	int ret = 0, tmp;
 	struct stprivate_data *data = s->drv_data;
@@ -252,7 +244,6 @@ const struct accelgyro_drv lis2dh_drv = {
 	.init = init,
 	.read = read,
 	.set_range = set_range,
-	.get_range = get_range,
 	.get_resolution = st_get_resolution,
 	.set_data_rate = set_data_rate,
 	.get_data_rate = st_get_data_rate,

jackadam1981/ChromeEc

commit 8af4d851feb0fe422c46d2f0b1f8d968182ee5c1

driver:tcs3400: Add placeholder set_range
set_range is now called all the time at init to set the current range
at resume. Set one for tcs3400 otherwise the EC crashes.

fixes: commit d28c10498cd ("motion_sense: Make change in range permanenti")
CL:2215751

BUG=chromium:1083791
BRANCH=none
TEST=check on Volteer EC is not crashing in
motion_sense_switch_sensor_rate() at resume.

Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Change-Id: I172d3289bb69c14fa757d761b0099fea312292e0

driver/als_tcs3400.c
@@ -606,6 +606,13 @@ static int tcs3400_perform_calib(struct motion_sensor_t *s, int enable)
 	return EC_SUCCESS;
 }
 
+static int tcs3400_rgb_set_range(struct motion_sensor_t *s,
+				 int range,
+				 int rnd)
+{
+	return EC_SUCCESS;
+}
+
 static int tcs3400_set_range(struct motion_sensor_t *s,
 			     int range,
 			     int rnd)
@@ -770,6 +777,7 @@ const struct accelgyro_drv tcs3400_drv = {
 const struct accelgyro_drv tcs3400_rgb_drv = {
 	.init = tcs3400_rgb_init,
 	.read = tcs3400_rgb_read,
+	.set_range = tcs3400_rgb_set_range,
 	.set_offset = tcs3400_rgb_set_offset,
 	.get_offset = tcs3400_rgb_get_offset,
 	.set_scale = tcs3400_rgb_set_scale,