550ddf0f7e2cd52068e12636361d14eb97c6149e
Ben Chen <ben.chen2@quanta.corp-partner.google.com>
2020-11-23 18:51:01 +0800

    driver: support PS8802 setting power saving mode
    Add function support setting operation to
    power saving mode(standby mode).
    
    BUG=b:172993414
    BRANCH=none
    TEST=make buildall
    
    Change-Id: I579b3b2958a930d9d7d70b913d3449397f6fa9db
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- driver/retimer/ps8802.h
+++ driver/retimer/ps8802.h
@@ -73,6 +73,7 @@
 #define PS8802_P1_ADDR			0x0A
 #define PS8802_ADDR_CFG			0xB0
 #define PS8802_I2C_SLAV_ADDR	0x50
+#define PS8802_STANDBY_MODE		0xAA
 
 extern const struct usb_mux_driver ps8802_usb_mux_driver;
 
