f92307e374bcbc7488d8a38fb16d89b6038fb529
Divya Sasidharan <divya.s.sasidharan@intel.com>
2020-11-23 16:58:28 -0800

    retimer: Print any I2c error while wrting to BB retimer
    This is a test patch for debug.
    
    Change-Id: Ife0beb5bd8402c9a0e77a2b708abe47aedf8eae6
    Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>

--- driver/retimer/bb_retimer.c
+++ driver/retimer/bb_retimer.c
@@ -339,6 +339,7 @@ static int retimer_set_state(const struct usb_mux *me, mux_state_t mux_state)
 	uint32_t set_retimer_con = 0;
 	uint8_t dp_pin_mode;
 	int port = me->usb_port;
+	int val;
 	/*
 	 * TODO(b/161327513): Remove this once we have final fix for
 	 * the Type-C MFD degradation issue.
@@ -445,8 +446,12 @@ static int retimer_set_state(const struct usb_mux *me, mux_state_t mux_state)
 		retimer_set_state_ufp(mux_state, &set_retimer_con);
 
 	/* Writing the register4 */
-	return bb_retimer_write(me, BB_RETIMER_REG_CONNECTION_STATE,
+	val = bb_retimer_write(me, BB_RETIMER_REG_CONNECTION_STATE,
 			set_retimer_con);
+	if (val)
+		CPRINTS("\nC%d: BB write I2C error %d\n", port, val);
+
+	return val;
 }
 
 static int retimer_low_power_mode(const struct usb_mux *me)
