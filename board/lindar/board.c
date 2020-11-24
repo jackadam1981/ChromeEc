ed9ead457d981bab9cb84e8ff3c40cc909ea0aef
jerry2.huang <jerry2.huang@lcfc.corp-partner.google.com>
2020-11-24 16:34:38 +0800

    lillipup: fix usb type-a bc1.2 charging
    fix usb type-a bc1.2 charging
    
    BUG=b:173093980
    BRANCH=firmware-volteer-13521.B
    TEST=make buildall, test usb type-a charging and usb function work normally
    
    Signed-off-by: jerry2.huang <jerry2.huang@lcfc.corp-partner.google.com>
    Change-Id: I60aadcac58ccbfe86d7005db506788a13d6fe4c9

--- board/lindar/board.c
+++ board/lindar/board.c
@@ -424,6 +424,11 @@ static const struct usb_mux mux_config_p1_usb3 = {
 	.next_mux = &usbc1_usb3_db_retimer,
 };
 
+/* USB-A charging control */
+const int usb_port_enable[USB_PORT_COUNT] = {
+	GPIO_EN_PP5000_USBA,
+};
+
 static void ps8815_reset(void)
 {
 	int val;
