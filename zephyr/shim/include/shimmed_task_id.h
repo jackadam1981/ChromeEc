f7dc341e034347320f43d250dea70872a0076154
Yuval Peress <peress@chromium.org>
2020-11-30 23:15:20 -0700

    zephyr: fix build
    For keyboard:
    See platform/ec/common/build.mk for reference. The use of keyboard_scan.c
    should only be allowed when HAS_TASK_KEYSCAN is defined. As such, we should
    always set HAS_TASK_KEYSCAN to 1 if CONFIG_PLATFORM_EC_KEYBOARD is defined.
    Further, CROS_EC_TASK_LIST was defined per project and it doesn't need to
    be, we can instead define it once for all boards depending on which tasks
    are set to 1 in shimmed_tasks.h
    
    For espi:
    We were casting the int to a pointer. Needed '&'.
    
    BRANCH=none
    BUG=none
    TEST=zmake buildall (this is technically still failing because
         platform/ec/zephyr/shim/chip/npcx/keyboard_raw.c is including
         "cros_kb_raw.h" which doesn't exist yet).
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: Id0ed49dd49e3c4eb2ac23184cf943c91dcd261eb

--- zephyr/shim/include/shimmed_task_id.h
+++ zephyr/shim/include/shimmed_task_id.h
@@ -12,16 +12,35 @@ typedef uint8_t task_id_t;
 /* Include the shimmed tasks for the project/board */
 #ifdef CONFIG_SHIMMED_TASKS
 #include "shimmed_tasks.h"
-#else
-#define CROS_EC_TASK_LIST
 #endif
+
+/*
+ * Highest priority on bottom -- same as in platform/ec. List of CROS_EC_TASK
+ * items. See CONFIG_TASK_LIST in platform/ec's config.h for more informaiton
+ */
+#define CROS_EC_TASK_LIST                                                      \
+	(                                                                      \
+	(COND_CODE_1(HAS_TASK_CHIPSET,                                         \
+		     (CROS_EC_TASK(CHIPSET, chipset_task, 0, 512)), ()))       \
+	(COND_CODE_1(HAS_TASK_HOSTCMD,                                         \
+		     (CROS_EC_TASK(HOSTCMD, host_command_task, 0, 512)),       \
+		     ()))                                                      \
+	(COND_CODE_1(HAS_TASK_KEYPROTO,                                        \
+		     (CROS_EC_TASK(KEYPROTO, keyboard_protocol_task, 0, 512)), \
+		     ()))                                                      \
+	(COND_CODE_1(HAS_TASK_POWERBTN,                                        \
+		     (CROS_EC_TASK(POWERBTN, power_button_task, 0, 512)),      \
+		     ()))                                                      \
+	(COND_CODE_1(HAS_TASK_KEYSCAN,                                         \
+		     (CROS_EC_TASK(KEYSCAN, keyboard_scan_task, 0, 512)),      \
+		     ()))                                                      \
+	)
 
 /* Define the task_ids globally for all shimmed platform/ec code to use */
 #define CROS_EC_TASK(name, ...) TASK_ID_##name,
 enum {
 	TASK_ID_IDLE = -1, /* We don't shim the idle task */
-	CROS_EC_TASK_LIST
-	TASK_ID_COUNT,
+	CROS_EC_TASK_LIST TASK_ID_COUNT,
 	TASK_ID_INVALID = 0xff, /* Unable to find the task */
 };
 #undef CROS_EC_TASK
