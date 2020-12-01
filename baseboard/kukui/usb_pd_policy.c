f4552574c4ac2d78b0e2ea0f12c8f301045d8746
Eric Yilun Lin <yllin@chromium.org>
2020-12-01 14:06:01 +0800

    kukui: allow Vconn swap if battery exists.
    BUG=b:174324375
    TEST=insert Apple dongle, and HDMI port works.
    BRANCH=kukui
    
    Change-Id: I6ac5c6ce172c6f395a38b0be7dffb4e92f3b9dc4

--- baseboard/kukui/usb_pd_policy.c
+++ baseboard/kukui/usb_pd_policy.c
@@ -104,11 +104,8 @@ void pd_power_supply_reset(int port)
 
 int pd_check_vconn_swap(int port)
 {
-	/*
-	 * VCONN is provided directly by the battery (PPVAR_SYS)
-	 * but use the same rules as power swap.
-	 */
-	return pd_get_dual_role(port) == PD_DRP_TOGGLE_ON ? 1 : 0;
+	/* Only allow Vconn swap if battery exists.  */
+	return battery_is_present() == BP_YES;
 }
 
 /* ----------------- Vendor Defined Messages ------------------ */
