bedd2f6a5015b707f59d23cd10b0764f40e86538
Aseda Aboagye <aaboagye@google.com>
2020-12-01 19:18:49 -0800

    charger: Add `charger_is_icl_reached()`
    Some charger ICs have the capability to report whether they have
    reached the set input current limit.  This commit simply exposes an
    API for use by the rest of the system to determine if the input
    current limit is reached.
    
    BUG=b:174167890
    BRANCH=dedede
    TEST=`make -j buildall`
    
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Change-Id: Ic0e00a54c53c985104cf400f0ce36b7a090ca5f2

--- common/charger.c
+++ common/charger.c
@@ -661,3 +661,16 @@ enum ec_error_list charger_set_vsys_compensation(int chgnum,
 	 */
 	return EC_ERROR_UNIMPLEMENTED;
 }
+
+enum ec_error_list charger_is_icl_reached(int chgnum, bool *reached)
+{
+		if ((chgnum < 0) || (chgnum >= board_get_charger_chip_count())) {
+		CPRINTS("%s(%d) Invalid charger!", __func__, chgnum);
+		return EC_ERROR_INVAL;
+	}
+
+	if (chg_chips[chgnum].drv->is_icl_reached)
+		return chg_chips[chgnum].drv->is_icl_reached(chgnum, reached);
+
+	return EC_ERROR_UNIMPLEMENTED;
+}
