f5f8c566476e6cd056e0ba0b9a6fb16f87b06c82
Keith Short <keithshort@chromium.org>
2020-12-06 13:36:49 -0700

    zephyr: Initialize the I2C bus speeds
    The NPCX I2C driver doesn't automatically set the I2C bus frequency.
    This fixes scanning of the battery on Volteer.
    
    This can be reverted once b:174951223 is fixed.
    
    BUG=b:175248556
    BRANCH=none
    TEST=build and boot zephyr on Volteer
    i2c scan I2C_5_PORT0
    See the battery appears now
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: I609d12cd130394734612e8fed7d09a0d4326b4f3

--- zephyr/shim/src/i2c.c
+++ zephyr/shim/src/i2c.c
@@ -3,8 +3,12 @@
  * found in the LICENSE file.
  */
 
+#include <drivers/i2c.h>
+
+#include "console.h"
 #include "i2c.h"
 #include "i2c/i2c.h"
+#include "i2c_map.h"
 
 /*
  * Long term we will not need these, for now they're needed to get things to
@@ -20,4 +24,33 @@ const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 int i2c_get_line_levels(int port)
 {
 	return I2C_LINE_IDLE;
+}
+
+/*
+ * TODO(b/174951223): Remove once NPCX I2C supports clock-frequency device
+ * tree property.
+ */
+int zephyr_shim_setup_i2c(void)
+{
+	uint32_t dev_config;
+	const struct device *device;
+	int i;
+	int rv = 0;
+
+	cprints(CC_I2C, "Configure I2C bus speeds\n");
+
+	for (i = 0; i < zephyr_i2c_port_assert_count; i++) {
+		dev_config = I2C_MODE_MASTER
+			| I2C_SPEED_SET(zephyr_i2c_ports[i].i2c_speed);
+		device = i2c_get_device_for_port(zephyr_i2c_ports[i].port);
+
+		rv = i2c_configure(device, dev_config);
+		if (rv) {
+			cprints(CC_I2C, "I2C configure error on bus %d\n",
+				zephyr_i2c_ports[i].port);
+			break;
+		}
+	}
+
+	return rv;
 }
