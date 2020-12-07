b8c46163261f2694d39e870f4993e7bb62f3de1d
Rob Barnes <robbarnes@google.com>
2020-12-07 15:12:43 -0700

    guybrush: Fill in gpio.inc for guybrush
    Fill in guybrush gpio.inc based on schematic.
    
    BUG=b:175056466
    TEST=Build guybrush binary
    BRANCH=None
    
    Change-Id: I2f66cebb02e174661950ec277175f63f85896c9c
    Signed-off-by: Rob Barnes <robbarnes@google.com>

--- baseboard/guybrush/baseboard.h
+++ baseboard/guybrush/baseboard.h
@@ -16,6 +16,10 @@
 #define CONFIG_UART_TX_BUF_SIZE 4096
 
 /* EC Defines */
+#define CONFIG_POWER_BUTTON
+#define CONFIG_POWER_BUTTON_X86
+#define CONFIG_EXTPOWER_GPIO
+#define CONFIG_VOLUME_BUTTONS
 
 /* Host communication */
 
@@ -30,6 +34,7 @@
 /* Common battery defines */
 
 /* USB Type C and USB PD defines */
+#define CONFIG_IO_EXPANDER_PORT_COUNT USBC_PORT_COUNT
 
 /* BC 1.2 */
 
@@ -37,6 +42,21 @@
 
 
 #ifndef __ASSEMBLER__
+
+#include "gpio_signal.h"
+#include "registers.h"
+
+/* Common definition for the USB PD interrupt handlers. */
+void tcpc_alert_event(enum gpio_signal signal);
+void bc12_interrupt(enum gpio_signal signal);
+void ppc_interrupt(enum gpio_signal signal);
+void sbu_fault_interrupt(enum ioex_signal signal);
+
+enum usbc_port {
+	USBC_PORT_C0 = 0,
+	USBC_PORT_C1,
+	USBC_PORT_COUNT
+};
 
 #endif /* !__ASSEMBLER__ */
 
