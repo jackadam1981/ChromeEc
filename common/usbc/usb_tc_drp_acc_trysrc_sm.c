cba003189af89f5c55b3508cccd45d7f555ac77f
Scott Collyer <scollyer@google.com>
2020-12-09 09:55:44 -0800

    TCPMv2: Add config option guard for prs check
    This CL fixes an implicit config option assumption where a call from
    the type C layer would expect a power role swap function to exist.
    
    BUG=b:169299049
    BRANCH=none
    TEST=verify that can build with usbc support only enabled.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I0a674beb0f68d4a767303fee5e48221e205e0887

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -3536,7 +3536,9 @@ static void pd_chipset_resume(void)
 	int i;
 
 	for (i = 0; i < CONFIG_USB_PD_PORT_MAX_COUNT; i++) {
+#ifdef CONFIG_USB_PE_SM
 		pd_resume_check_pr_swap_needed(i);
+#endif
 		pd_set_dual_role_and_event(i,
 					   PD_DRP_TOGGLE_ON,
 					   PD_EVENT_UPDATE_DUAL_ROLE
