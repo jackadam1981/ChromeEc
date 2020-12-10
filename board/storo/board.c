2512edcd91a03b83b2136cc26e789f2a087dce12
Mike Lee <mike5@huaqin.corp-partner.google.com>
2020-12-10 20:15:38 +0800

    Storo: Initial EC image
    Initial EC image for Storo
    
    BUG=b:173340493
    BRANCH=master
    TEST=make buidall pass
    boot device successfully
    
    Signed-off-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Change-Id: I1c82cc80f161cdbe8561b06606ec9f34ed094094

--- board/storo/board.c
+++ board/storo/board.c
@@ -3,27 +3,34 @@
  * found in the LICENSE file.
  */
 
-/* Waddledee board-specific configuration */
+/* Storo configuration */
 
 #include "adc_chip.h"
 #include "button.h"
+#include "cbi_fw_config.h"
 #include "charge_manager.h"
 #include "charge_state_v2.h"
 #include "charger.h"
-#include "driver/accel_kionix.h"
-#include "driver/accelgyro_lsm6dsm.h"
+#include "chipset.h"
+#include "common.h"
+#include "compile_time_macros.h"
+#include "driver/accel_bma2x2.h"
+#include "driver/accelgyro_bmi_common.h"
 #include "driver/bc12/pi3usb9201.h"
-#include "driver/charger/sm5803.h"
+#include "driver/charger/isl923x.h"
 #include "driver/retimer/tusb544.h"
 #include "driver/temp_sensor/thermistor.h"
-#include "driver/tcpm/anx7447.h"
-#include "driver/tcpm/it83xx_pd.h"
+#include "driver/tcpm/raa489000.h"
+#include "driver/tcpm/tcpci.h"
 #include "driver/usb_mux/it5205.h"
+#include "extpower.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "intc.h"
+#include "i2c.h"
 #include "keyboard_scan.h"
 #include "lid_switch.h"
+#include "motion_sense.h"
 #include "power.h"
 #include "power_button.h"
 #include "pwm.h"
@@ -40,80 +47,34 @@
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
 
-#define CPRINTUSB(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
-#define INT_RECHECK_US 5000
-
-/* C1 interrupt line swapped between board versions, track it in a variable */
-static enum gpio_signal c1_int_line;
-
-/* C0 interrupt line shared by BC 1.2 and charger */
-static void check_c0_line(void);
-DECLARE_DEFERRED(check_c0_line);
-
-static void notify_c0_chips(void)
+static void tcpc_alert_event(enum gpio_signal s)
 {
-	task_set_event(TASK_ID_USB_CHG_P0, USB_CHG_EVENT_BC12, 0);
-	sm5803_interrupt(0);
-}
+	int port = (s == GPIO_USB_C0_INT_ODL) ? 0 : 1;
 
-static void check_c0_line(void)
-{
-	/*
-	 * If line is still being held low, see if there's more to process from
-	 * one of the chips
-	 */
-	if (!gpio_get_level(GPIO_USB_C0_INT_ODL)) {
-		notify_c0_chips();
-		hook_call_deferred(&check_c0_line_data, INT_RECHECK_US);
-	}
+	schedule_deferred_pd_interrupt(port);
 }
 
 static void usb_c0_interrupt(enum gpio_signal s)
 {
-	/* Cancel any previous calls to check the interrupt line */
-	hook_call_deferred(&check_c0_line_data, -1);
-
-	/* Notify all chips using this line that an interrupt came in */
-	notify_c0_chips();
-
-	/* Check the line again in 5ms */
-	hook_call_deferred(&check_c0_line_data, INT_RECHECK_US);
+	/*
+	 * The interrupt line is shared between the TCPC and BC 1.2 detection
+	 * chip.  Therefore we'll need to check both ICs.
+	 */
+	tcpc_alert_event(s);
+	task_set_event(TASK_ID_USB_CHG_P0, USB_CHG_EVENT_BC12, 0);
 }
 
-/* C1 interrupt line shared by BC 1.2, TCPC, and charger */
-static void check_c1_line(void);
-DECLARE_DEFERRED(check_c1_line);
-
-static void notify_c1_chips(void)
-{
-	schedule_deferred_pd_interrupt(1);
-	task_set_event(TASK_ID_USB_CHG_P1, USB_CHG_EVENT_BC12, 0);
-	sm5803_interrupt(1);
-}
-
-static void check_c1_line(void)
+static void sub_usb_c1_interrupt(enum gpio_signal s)
 {
 	/*
-	 * If line is still being held low, see if there's more to process from
-	 * one of the chips.
+	 * The interrupt line is shared between the TCPC and BC 1.2 detection
+	 * chip.  Therefore we'll need to check both ICs.
 	 */
-	if (!gpio_get_level(c1_int_line)) {
-		notify_c1_chips();
-		hook_call_deferred(&check_c1_line_data, INT_RECHECK_US);
-	}
-}
-
-static void usb_c1_interrupt(enum gpio_signal s)
-{
-	/* Cancel any previous calls to check the interrupt line */
-	hook_call_deferred(&check_c1_line_data, -1);
-
-	/* Notify all chips using this line that an interrupt came in */
-	notify_c1_chips();
-
-	/* Check the line again in 5ms */
-	hook_call_deferred(&check_c1_line_data, INT_RECHECK_US);
+	tcpc_alert_event(s);
+	task_set_event(TASK_ID_USB_CHG_P1, USB_CHG_EVENT_BC12, 0);
 }
 
 static void c0_ccsbu_ovp_interrupt(enum gpio_signal s)
@@ -148,12 +109,12 @@ const struct adc_t adc_channels[] = {
 		.shift = 0,
 		.channel = CHIP_ADC_CH3
 	},
-	[ADC_SUB_ANALOG] = {
-		.name = "SUB_ANALOG",
+	[ADC_TEMP_SENSOR_3] = {
+		.name = "TEMP_SENSOR3",
 		.factor_mul = ADC_MAX_MVOLT,
 		.factor_div = ADC_READ_MAX + 1,
 		.shift = 0,
-		.channel = CHIP_ADC_CH13
+		.channel = CHIP_ADC_CH15
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
@@ -176,30 +137,36 @@ const struct pi3usb9201_config_t pi3usb9201_bc12_chips[] = {
 const struct charger_config_t chg_chips[] = {
 	[CHARGER_PRIMARY] = {
 		.i2c_port = I2C_PORT_USB_C0,
-		.i2c_addr_flags = SM5803_ADDR_CHARGER_FLAGS,
-		.drv = &sm5803_drv,
+		.i2c_addr_flags = ISL923X_ADDR_FLAGS,
+		.drv = &isl923x_drv,
 	},
 	[CHARGER_SECONDARY] = {
 		.i2c_port = I2C_PORT_SUB_USB_C1,
-		.i2c_addr_flags = SM5803_ADDR_CHARGER_FLAGS,
-		.drv = &sm5803_drv,
+		.i2c_addr_flags = ISL923X_ADDR_FLAGS,
+		.drv = &isl923x_drv,
 	},
 };
 
 /* TCPCs */
 const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	{
-		.bus_type = EC_BUS_TYPE_EMBEDDED,
-		.drv = &it83xx_tcpm_drv,
+		.bus_type = EC_BUS_TYPE_I2C,
+		.i2c_info = {
+			.port = I2C_PORT_USB_C0,
+			.addr_flags = RAA489000_TCPC0_I2C_FLAGS,
+		},
+		.flags = TCPC_FLAGS_TCPCI_REV2_0,
+		.drv = &raa489000_tcpm_drv,
 	},
+
 	{
 		.bus_type = EC_BUS_TYPE_I2C,
 		.i2c_info = {
 			.port = I2C_PORT_SUB_USB_C1,
-			.addr_flags = AN7447_TCPC0_I2C_ADDR_FLAGS,
+			.addr_flags = RAA489000_TCPC0_I2C_FLAGS,
 		},
-		.drv = &anx7447_tcpm_drv,
 		.flags = TCPC_FLAGS_TCPCI_REV2_0,
+		.drv = &raa489000_tcpm_drv,
 	},
 };
 
@@ -222,8 +189,8 @@ const struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	{
 		.usb_port = 1,
 		.i2c_port = I2C_PORT_SUB_USB_C1,
-		.i2c_addr_flags = AN7447_TCPC0_I2C_ADDR_FLAGS,
-		.driver = &anx7447_usb_mux_driver,
+		.i2c_addr_flags = IT5205_I2C_ADDR1_FLAGS,
+		.driver = &it5205_usb_mux_driver,
 		.next_mux = &usbc1_retimer,
 	},
 };
@@ -232,36 +199,12 @@ void board_init(void)
 {
 	int on;
 
-	if (system_get_board_version() <= 0) {
-		pd_set_max_voltage(5000);
-		c1_int_line = GPIO_USB_C1_INT_V0_ODL;
-	} else {
-		c1_int_line = GPIO_USB_C1_INT_V1_ODL;
-	}
-
-
 	gpio_enable_interrupt(GPIO_USB_C0_INT_ODL);
-	gpio_enable_interrupt(c1_int_line);
-
-	/*
-	 * If interrupt lines are already low, schedule them to be processed
-	 * after inits are completed.
-	 */
-	if (!gpio_get_level(GPIO_USB_C0_INT_ODL))
-		hook_call_deferred(&check_c0_line_data, 0);
-	if (!gpio_get_level(c1_int_line))
-		hook_call_deferred(&check_c1_line_data, 0);
+	gpio_enable_interrupt(GPIO_SUB_C1_INT_EN_RAILS_ODL);
 
 	gpio_enable_interrupt(GPIO_USB_C0_CCSBU_OVP_ODL);
 	/* Enable Base Accel interrupt */
 	gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
-
-	/* Charger on the MB will be outputting PROCHOT_ODL and OD CHG_DET */
-	sm5803_configure_gpio0(CHARGER_PRIMARY, GPIO0_MODE_PROCHOT, 1);
-	sm5803_configure_chg_det_od(CHARGER_PRIMARY, 1);
-
-	/* Charger on the sub-board will be a push-pull GPIO */
-	sm5803_configure_gpio0(CHARGER_SECONDARY, GPIO0_MODE_OUTPUT, 0);
 
 	/* Turn on 5V if the system is on, otherwise turn it off */
 	on = chipset_in_state(CHIPSET_STATE_ON | CHIPSET_STATE_ANY_SUSPEND |
@@ -270,37 +213,13 @@ void board_init(void)
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
-static void board_resume(void)
-{
-	sm5803_disable_low_power_mode(CHARGER_PRIMARY);
-	if (board_get_charger_chip_count() > 1)
-		sm5803_disable_low_power_mode(CHARGER_SECONDARY);
-}
-DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_resume, HOOK_PRIO_DEFAULT);
-
-static void board_suspend(void)
-{
-	sm5803_enable_low_power_mode(CHARGER_PRIMARY);
-	if (board_get_charger_chip_count() > 1)
-		sm5803_enable_low_power_mode(CHARGER_SECONDARY);
-}
-DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_suspend, HOOK_PRIO_DEFAULT);
-
 void board_hibernate(void)
 {
 	/*
 	 * Put all charger ICs present into low power mode before entering
 	 * z-state.
 	 */
-	sm5803_hibernate(CHARGER_PRIMARY);
-	if (board_get_charger_chip_count() > 1)
-		sm5803_hibernate(CHARGER_SECONDARY);
-}
-
-__override void board_ocpc_init(struct ocpc_data *ocpc)
-{
-	/* There's no provision to measure Isys */
-	ocpc->chg_flags[CHARGER_SECONDARY] |= OCPC_NO_ISYS_MEAS_CAP;
+	raa489000_hibernate(0);
 }
 
 void board_reset_pd_mcu(void)
@@ -311,16 +230,40 @@ void board_reset_pd_mcu(void)
 	 */
 }
 
+static void set_5v_gpio(int level)
+{
+	int version;
+	enum gpio_signal gpio;
+
+	/*
+	 * b/147257497: On early boards, GPIO_EN_PP5000 was swapped with
+	 * GPIO_VOLUP_BTN_ODL. Therefore, we'll actually need to set that GPIO
+	 * instead for those boards.  Note that this breaks the volume up button
+	 * functionality.
+	 */
+	version = system_get_board_version();
+
+	/*
+	 * If the CBI EEPROM wasn't formatted, assume it's a very early board.
+	 */
+	gpio = version < 0 ? GPIO_VOLUP_BTN_ODL : GPIO_EN_PP5000;
+
+	gpio_set_level(gpio, level);
+}
+
 __override void board_power_5v_enable(int enable)
 {
 	/*
-	 * Motherboard has a GPIO to turn on the 5V regulator, but the sub-board
-	 * sets it through the charger GPIO.
+	 * Port 0 simply has a GPIO to turn on the 5V regulator, however, 5V is
+	 * generated locally on the sub board and we need to set the comparator
+	 * polarity on the sub board charger IC, or send enable signal to HDMI
+	 * DB.
 	 */
-	gpio_set_level(GPIO_EN_PP5000, !!enable);
-	gpio_set_level(GPIO_EN_USB_A0_VBUS, !!enable);
-	if (sm5803_set_gpio0_level(1, !!enable))
-		CPRINTUSB("Failed to %sable sub rails!", enable ? "en" : "dis");
+	set_5v_gpio(!!enable);
+
+	if (isl923x_set_comparator_inversion(1, !!enable))
+		CPRINTS("Failed to %sable sub rails!", enable ? "en" : "dis");
+
 }
 
 uint16_t tcpc_get_alert_status(void)
@@ -333,11 +276,30 @@ uint16_t tcpc_get_alert_status(void)
 	uint16_t status = 0;
 	int regval;
 
-	/* Check whether TCPC 1 pulled the shared interrupt line */
-	if (!gpio_get_level(c1_int_line)) {
-		if (!tcpc_read16(1, TCPC_REG_ALERT, &regval)) {
+	/*
+	 * The interrupt line is shared between the TCPC and BC1.2 detector IC.
+	 * Therefore, go out and actually read the alert registers to report the
+	 * alert status.
+	 */
+	if (!gpio_get_level(GPIO_USB_C0_INT_ODL)) {
+		if (!tcpc_read16(0, TCPC_REG_ALERT, &regval)) {
+			/* The TCPCI Rev 1.0 spec says to ignore bits 14:12. */
+			if (!(tcpc_config[0].flags & TCPC_FLAGS_TCPCI_REV2_0))
+				regval &= ~((1 << 14) | (1 << 13) | (1 << 12));
+
 			if (regval)
-				status = PD_STATUS_TCPC_ALERT_1;
+				status |= PD_STATUS_TCPC_ALERT_0;
+		}
+	}
+
+	if (!gpio_get_level(GPIO_SUB_C1_INT_EN_RAILS_ODL)) {
+		if (!tcpc_read16(1, TCPC_REG_ALERT, &regval)) {
+			/* TCPCI spec Rev 1.0 says to ignore bits 14:12. */
+			if (!(tcpc_config[1].flags & TCPC_FLAGS_TCPCI_REV2_0))
+				regval &= ~((1 << 14) | (1 << 13) | (1 << 12));
+
+			if (regval)
+				status |= PD_STATUS_TCPC_ALERT_1;
 		}
 	}
 
@@ -350,59 +312,83 @@ void board_set_charge_limit(int port, int supplier, int charge_ma, int max_ma,
 	int icl = MAX(charge_ma, CONFIG_CHARGER_INPUT_CURRENT);
 
 	/*
-	 * TODO(b/151955431): Characterize the input current limit in case a
-	 * scaling needs to be applied here
+	 * b/147463641: The charger IC seems to overdraw ~4%, therefore we
+	 * reduce our target accordingly.
 	 */
+	icl = icl * 96 / 100;
 	charge_set_input_current_limit(icl, charge_mv);
+}
+
+int board_is_sourcing_vbus(int port)
+{
+	int regval;
+
+	tcpc_read(port, TCPC_REG_POWER_STATUS, &regval);
+	return !!(regval & TCPC_REG_POWER_STATUS_SOURCING_VBUS);
+
 }
 
 int board_set_active_charge_port(int port)
 {
-	int is_valid_port = (port >= 0 && port < board_get_usb_pd_port_count());
+	int is_real_port = (port >= 0 &&
+			    port < board_get_usb_pd_port_count());
+	int i;
+	int old_port;
 
-	if (!is_valid_port && port != CHARGE_PORT_NONE)
+	if (!is_real_port && port != CHARGE_PORT_NONE)
 		return EC_ERROR_INVAL;
 
+	old_port = charge_manager_get_active_charge_port();
+
+	CPRINTS("New chg p%d", port);
+
+	/* Disable all ports. */
 	if (port == CHARGE_PORT_NONE) {
-		CPRINTUSB("Disabling all charge ports");
-
-		sm5803_vbus_sink_enable(CHARGER_PRIMARY, 0);
-
-		if (board_get_charger_chip_count() > 1)
-			sm5803_vbus_sink_enable(CHARGER_SECONDARY, 0);
+		for (i = 0; i < board_get_usb_pd_port_count(); i++)
+			tcpc_write(i, TCPC_REG_COMMAND,
+				   TCPC_REG_COMMAND_SNK_CTRL_LOW);
 
 		return EC_SUCCESS;
 	}
 
-	CPRINTUSB("New chg p%d", port);
-
-	/*
-	 * Ensure other port is turned off, then enable new charge port
-	 */
-	if (port == 0) {
-		if (board_get_charger_chip_count() > 1)
-			sm5803_vbus_sink_enable(CHARGER_SECONDARY, 0);
-		sm5803_vbus_sink_enable(CHARGER_PRIMARY, 1);
-
-	} else {
-		sm5803_vbus_sink_enable(CHARGER_PRIMARY, 0);
-		sm5803_vbus_sink_enable(CHARGER_SECONDARY, 1);
+	/* Check if port is sourcing VBUS. */
+	if (board_is_sourcing_vbus(port)) {
+		CPRINTS("Skip enable p%d", port);
+		return EC_ERROR_INVAL;
 	}
 
+	/*
+	 * Turn off the other ports' sink path FETs, before enabling the
+	 * requested charge port.
+	 */
+	for (i = 0; i < board_get_usb_pd_port_count(); i++) {
+		if (i == port)
+			continue;
+
+		if (tcpc_write(i, TCPC_REG_COMMAND,
+			       TCPC_REG_COMMAND_SNK_CTRL_LOW))
+			CPRINTS("p%d: sink path disable failed.", i);
+	}
+
+	/*
+	 * Stop the charger IC from switching while changing ports.  Otherwise,
+	 * we can overcurrent the adapter we're switching to. (crbug.com/926056)
+	 */
+	if (old_port != CHARGE_PORT_NONE)
+		charger_discharge_on_ac(1);
+
+	/* Enable requested charge port. */
+	if (tcpc_write(port, TCPC_REG_COMMAND,
+		       TCPC_REG_COMMAND_SNK_CTRL_HIGH)) {
+		CPRINTS("p%d: sink path enable failed.", port);
+		charger_discharge_on_ac(0);
+		return EC_ERROR_UNKNOWN;
+	}
+
+	/* Allow the charger IC to begin/continue switching. */
+	charger_discharge_on_ac(0);
+
 	return EC_SUCCESS;
-}
-
-/* Vconn control for integrated ITE TCPC */
-void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled)
-{
-	/* Vconn control is only for port 0 */
-	if (port)
-		return;
-
-	if (cc_pin == USBPD_CC_PIN_1)
-		gpio_set_level(GPIO_EN_USB_C0_CC1_VCONN, !!enabled);
-	else
-		gpio_set_level(GPIO_EN_USB_C0_CC2_VCONN, !!enabled);
 }
 
 __override void ocpc_get_pid_constants(int *kp, int *kp_div,
@@ -431,61 +417,63 @@ __override void typec_set_source_current_limit(int port, enum tcpc_rp_value rp)
 	charger_set_otg_current_voltage(port, current, 5000);
 }
 
+int pd_snk_is_vbus_provided(int port)
+{
+	return pd_check_vbus_level(port, VBUS_PRESENT);
+}
+
 /* PWM channels. Must be in the exactly same order as in enum pwm_channel. */
 const struct pwm_t pwm_channels[] = {
-	[PWM_CH_KBLIGHT] = {
-		.channel = 0,
-		.flags = PWM_CONFIG_DSLEEP,
-		.freq_hz = 10000,
-	},
-
-	[PWM_CH_LED_RED] = {
+	[PWM_CH_LED_AMBER] = {
 		.channel = 1,
 		.flags = PWM_CONFIG_DSLEEP | PWM_CONFIG_ACTIVE_LOW,
 		.freq_hz = 2400,
 	},
 
-	[PWM_CH_LED_GREEN] = {
+	[PWM_CH_LED_WHITE] = {
 		.channel = 2,
 		.flags = PWM_CONFIG_DSLEEP | PWM_CONFIG_ACTIVE_LOW,
 		.freq_hz = 2400,
 	},
-
-	[PWM_CH_LED_BLUE] = {
-		.channel = 3,
-		.flags = PWM_CONFIG_DSLEEP | PWM_CONFIG_ACTIVE_LOW,
-		.freq_hz = 2400,
-	}
-
 };
 BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
 
-/* Sensor Mutexes */
+/* Sensors */
 static struct mutex g_lid_mutex;
 static struct mutex g_base_mutex;
 
-/* Sensor Data */
-static struct kionix_accel_data  g_kx022_data;
-static struct lsm6dsm_data lsm6dsm_data = LSM6DSM_DATA;
+/* Matrices to rotate accelerometers into the standard reference. */
+static const mat33_fp_t lid_standard_ref = {
+	{ FLOAT_TO_FP(1), 0, 0},
+	{ 0, FLOAT_TO_FP(1), 0},
+	{ 0, 0, FLOAT_TO_FP(1)}
+};
 
-/* Drivers */
+static const mat33_fp_t base_standard_ref = {
+	{ FLOAT_TO_FP(1), 0, 0},
+	{ 0, FLOAT_TO_FP(1), 0},
+	{ 0, 0, FLOAT_TO_FP(1)}
+};
+
+static struct accelgyro_saved_data_t g_bma253_data;
+static struct bmi_drv_data_t g_bmi160_data;
+
 struct motion_sensor_t motion_sensors[] = {
 	[LID_ACCEL] = {
 		.name = "Lid Accel",
 		.active_mask = SENSOR_ACTIVE_S0_S3,
-		.chip = MOTIONSENSE_CHIP_KX022,
+		.chip = MOTIONSENSE_CHIP_BMA255,
 		.type = MOTIONSENSE_TYPE_ACCEL,
 		.location = MOTIONSENSE_LOC_LID,
-		.drv = &kionix_accel_drv,
+		.drv = &bma2x2_accel_drv,
 		.mutex = &g_lid_mutex,
-		.drv_data = &g_kx022_data,
+		.drv_data = &g_bma253_data,
 		.port = I2C_PORT_SENSOR,
-		.i2c_spi_addr_flags = KX022_ADDR1_FLAGS,
-		.rot_standard_ref = NULL,
-		.default_range = 2, /* g */
-		/* We only use 2g because its resolution is only 8-bits */
-		.min_frequency = KX022_ACCEL_MIN_FREQ,
-		.max_frequency = KX022_ACCEL_MAX_FREQ,
+		.i2c_spi_addr_flags = BMA2x2_I2C_ADDR1_FLAGS,
+		.rot_standard_ref = &lid_standard_ref,
+		.default_range = 2,
+		.min_frequency = BMA255_ACCEL_MIN_FREQ,
+		.max_frequency = BMA255_ACCEL_MAX_FREQ,
 		.config = {
 			[SENSOR_CONFIG_EC_S0] = {
 				.odr = 10000 | ROUND_UP_FLAG,
@@ -498,21 +486,18 @@ struct motion_sensor_t motion_sensors[] = {
 	[BASE_ACCEL] = {
 		.name = "Base Accel",
 		.active_mask = SENSOR_ACTIVE_S0_S3,
-		.chip = MOTIONSENSE_CHIP_LSM6DSM,
+		.chip = MOTIONSENSE_CHIP_BMI160,
 		.type = MOTIONSENSE_TYPE_ACCEL,
 		.location = MOTIONSENSE_LOC_BASE,
-		.drv = &lsm6dsm_drv,
+		.drv = &bmi160_drv,
 		.mutex = &g_base_mutex,
-		.drv_data = LSM6DSM_ST_DATA(lsm6dsm_data,
-				MOTIONSENSE_TYPE_ACCEL),
-		.int_signal = GPIO_BASE_SIXAXIS_INT_L,
-		.flags = MOTIONSENSE_FLAG_INT_SIGNAL,
+		.drv_data = &g_bmi160_data,
 		.port = I2C_PORT_SENSOR,
-		.i2c_spi_addr_flags = LSM6DSM_ADDR0_FLAGS,
-		.rot_standard_ref = NULL,
-		.default_range = 4,  /* g */
-		.min_frequency = LSM6DSM_ODR_MIN_VAL,
-		.max_frequency = LSM6DSM_ODR_MAX_VAL,
+		.i2c_spi_addr_flags = BMI160_ADDR0_FLAGS,
+		.rot_standard_ref = &base_standard_ref,
+		.default_range = 4,
+		.min_frequency = BMI_ACCEL_MIN_FREQ,
+		.max_frequency = BMI_ACCEL_MAX_FREQ,
 		.config = {
 			[SENSOR_CONFIG_EC_S0] = {
 				.odr = 13000 | ROUND_UP_FLAG,
@@ -527,21 +512,18 @@ struct motion_sensor_t motion_sensors[] = {
 	[BASE_GYRO] = {
 		.name = "Base Gyro",
 		.active_mask = SENSOR_ACTIVE_S0_S3,
-		.chip = MOTIONSENSE_CHIP_LSM6DSM,
+		.chip = MOTIONSENSE_CHIP_BMI160,
 		.type = MOTIONSENSE_TYPE_GYRO,
 		.location = MOTIONSENSE_LOC_BASE,
-		.drv = &lsm6dsm_drv,
+		.drv = &bmi160_drv,
 		.mutex = &g_base_mutex,
-		.drv_data = LSM6DSM_ST_DATA(lsm6dsm_data,
-				MOTIONSENSE_TYPE_GYRO),
-		.int_signal = GPIO_BASE_SIXAXIS_INT_L,
-		.flags = MOTIONSENSE_FLAG_INT_SIGNAL,
+		.drv_data = &g_bmi160_data,
 		.port = I2C_PORT_SENSOR,
-		.i2c_spi_addr_flags = LSM6DSM_ADDR0_FLAGS,
-		.default_range = 1000 | ROUND_UP_FLAG, /* dps */
-		.rot_standard_ref = NULL,
-		.min_frequency = LSM6DSM_ODR_MIN_VAL,
-		.max_frequency = LSM6DSM_ODR_MAX_VAL,
+		.i2c_spi_addr_flags = BMI160_ADDR0_FLAGS,
+		.default_range = 1000, /* dps */
+		.rot_standard_ref = &base_standard_ref,
+		.min_frequency = BMI_GYRO_MIN_FREQ,
+		.max_frequency = BMI_GYRO_MAX_FREQ,
 	},
 };
 
@@ -557,6 +539,10 @@ const struct temp_sensor_t temp_sensors[] = {
 			   .type = TEMP_SENSOR_TYPE_BOARD,
 			   .read = get_temp_3v3_51k1_47k_4050b,
 			   .idx = ADC_TEMP_SENSOR_2},
+	[TEMP_SENSOR_3] = {.name = "Cpu",
+			   .type = TEMP_SENSOR_TYPE_BOARD,
+			   .read = get_temp_3v3_51k1_47k_4050b,
+			   .idx = ADC_TEMP_SENSOR_3},
 };
 BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 
