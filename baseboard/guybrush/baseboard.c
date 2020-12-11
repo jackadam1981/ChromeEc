c12cd8bc838b727438c5b433c5acc3ac5ff2971e
Rob Barnes <robbarnes@google.com>
2020-12-11 12:41:51 -0700

    guybrush: Temp sensor configuration
    Configuration for temp and thermal. ADC channels also configured.
    SBI-TSI CPU temp interface not enabled because it depends on I2C
    configuration.
    
    BUG=b:175408269
    TEST=Build
    BRANCH=None
    
    Change-Id: I63aae0366ccb0251163b0660cb47797fe004e68e
    Signed-off-by: Rob Barnes <robbarnes@google.com>

--- baseboard/guybrush/baseboard.c
+++ baseboard/guybrush/baseboard.c
@@ -5,8 +5,12 @@
 
 /* Guybrush family-specific configuration */
 
+#include "adc.h"
+#include "adc_chip.h"
 #include "gpio.h"
 #include "power.h"
+#include "temp_sensor.h"
+#include "thermal.h"
 
 /* Wake Sources */
 const enum gpio_signal hibernate_wake_pins[] = {
@@ -46,6 +50,95 @@ const struct power_signal_info power_signal_list[] = {
 };
 BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
 
+/* ADC Channels */
+const struct adc_t adc_channels[] = {
+	[ADC_TEMP_SENSOR_SOC] = {
+		.name = "SOC",
+		.input_ch = NPCX_ADC_CH0,
+		.factor_mul = ADC_MAX_VOLT,
+		.factor_div = ADC_READ_MAX + 1,
+		.shift = 0,
+	},
+	[ADC_TEMP_SENSOR_CHARGER] = {
+		.name = "CHARGER",
+		.input_ch = NPCX_ADC_CH1,
+		.factor_mul = ADC_MAX_VOLT,
+		.factor_div = ADC_READ_MAX + 1,
+		.shift = 0,
+	},
+	[ADC_TEMP_SENSOR_MEMORY] = {
+		.name = "MEMORY",
+		.input_ch = NPCX_ADC_CH2,
+		.factor_mul = ADC_MAX_VOLT,
+		.factor_div = ADC_READ_MAX + 1,
+		.shift = 0,
+	},
+
+};
+BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
+
+/* Temp Sensors */
+const struct temp_sensor_t temp_sensors[] = {
+	[TEMP_SENSOR_SOC] = {
+		.name = "SOC",
+		.type = TEMP_SENSOR_TYPE_BOARD,
+		.read = baseboard_get_temp,
+		.idx = TEMP_SENSOR_SOC,
+	},
+	[TEMP_SENSOR_CHARGER] = {
+		.name = "Charger",
+		.type = TEMP_SENSOR_TYPE_BOARD,
+		.read = baseboard_get_temp,
+		.idx = TEMP_SENSOR_CHARGER,
+	},
+	[TEMP_SENSOR_MEMORY] = {
+		.name = "Memory",
+		.type = TEMP_SENSOR_TYPE_BOARD,
+		.read = baseboard_get_temp,
+		.idx = TEMP_SENSOR_MEMORY,
+	},
+	[TEMP_SENSOR_CPU] = {
+		.name = "CPU",
+		.type = TEMP_SENSOR_TYPE_CPU,
+		.read = baseboard_get_temp,
+		.idx = TEMP_SENSOR_CPU,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
+
+__overridable struct ec_thermal_config thermal_params[TEMP_SENSOR_COUNT] = {
+	[TEMP_SENSOR_CHARGER] = {
+		.temp_host = {
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
+			[EC_TEMP_THRESH_HALT] = C_TO_K(92),
+		},
+		.temp_host_release = {
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
+		}
+	},
+	[TEMP_SENSOR_SOC] = {
+		.temp_host = {
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
+			[EC_TEMP_THRESH_HALT] = C_TO_K(92),
+		},
+		.temp_host_release = {
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
+		}
+	},
+	[TEMP_SENSOR_CPU] = {
+		.temp_host = {
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
+			[EC_TEMP_THRESH_HALT] = C_TO_K(92),
+		},
+		.temp_host_release = {
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(80),
+		}
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(thermal_params) == TEMP_SENSOR_COUNT);
+
+
+
 void sbu_fault_interrupt(enum ioex_signal signal)
 {
 	/* TODO */
@@ -64,4 +157,10 @@ void ppc_interrupt(enum gpio_signal signal)
 void bc12_interrupt(enum gpio_signal signal)
 {
 	/* TODO */
+}
+
+int baseboard_get_temp(int idx, int *temp_ptr)
+{
+	/* TODO */
+	return 0;
 }
