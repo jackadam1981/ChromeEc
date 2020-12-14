3120fd422c7f72d2771aa8c22246d29972d49eac
Wealian Liao <whliao@nuvoton.corp-partner.google.com>
2020-12-14 18:04:04 +0800

    [WIP]zephyr: shim partial host command function
    Signed-off-by: Wealian Liao <whliao@nuvoton.corp-partner.google.com>
    Change-Id: I519a9587eee1b1d709931600b89b0ab314af4e22

--- zephyr/shim/include/zephyr_espi_shim.h
+++ zephyr/shim/include/zephyr_espi_shim.h
@@ -6,6 +6,9 @@
 #ifndef __CROS_EC_ZEPHYR_ESPI_SHIM_H
 #define __CROS_EC_ZEPHYR_ESPI_SHIM_H
 
+#define EC_SC_SCI_EVT 5
+#define EC_SC_SMI_EVT 6
+
 /**
  * zephyr_shim_setup_espi() - initialize eSPI device
  *
