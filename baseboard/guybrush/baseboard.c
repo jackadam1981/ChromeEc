2b2b9069028920687387ee30d272fdadbbe01ccc
Rob Barnes <robbarnes@google.com>
2020-12-14 12:49:25 -0700

    guybrush: I2C port map
    Add i2c port map for guybrush.
    
    BUG=b:175593988
    TEST=Build
    BRANCH=None
    
    Signed-off-by: Rob Barnes <robbarnes@google.com>
    Change-Id: Ie78b0fa5d79c4add1776ddcb8a0ee14ec8b1618f

--- baseboard/guybrush/baseboard.c
+++ baseboard/guybrush/baseboard.c
@@ -6,6 +6,7 @@
 /* Guybrush family-specific configuration */
 
 #include "gpio.h"
+#include "i2c.h"
 
 /* Wake Sources */
 const enum gpio_signal hibernate_wake_pins[] = {
@@ -15,6 +16,65 @@ const enum gpio_signal hibernate_wake_pins[] = {
 };
 const int hibernate_wake_pins_used =  ARRAY_SIZE(hibernate_wake_pins);
 
+const struct i2c_port_t i2c_ports[] = {
+	{
+		.name = "tcpc0",
+		.port = I2C_PORT_TCPC0,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_USB_A0_C0_SCL,
+		.sda = GPIO_EC_I2C_USB_A0_C0_SDA,
+	},
+	{
+		.name = "tcpc1",
+		.port = I2C_PORT_TCPC1,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_USB_A1_C1_SCL,
+		.sda = GPIO_EC_I2C_USB_A1_C1_SDA,
+	},
+	{
+		.name = "battery",
+		.port = I2C_PORT_BATTERY,
+		.kbps = 100,
+		.scl = GPIO_EC_I2C_BATT_SCL,
+		.sda = GPIO_EC_I2C_BATT_SDA,
+	},
+	{
+		.name = "usb_mux",
+		.port = I2C_PORT_USB_MUX,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_USBC_MUX_SCL,
+		.sda = GPIO_EC_I2C_USBC_MUX_SDA,
+	},
+	{
+		.name = "charger",
+		.port = I2C_PORT_CHARGER,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_POWER_SCL,
+		.sda = GPIO_EC_I2C_POWER_SDA,
+	},
+	{
+		.name = "eeprom",
+		.port = I2C_PORT_EEPROM,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_CBI_SCL,
+		.sda = GPIO_EC_I2C_CBI_SDA,
+	},
+	{
+		.name = "sensor",
+		.port = I2C_PORT_SENSOR,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_SENSOR_SCL,
+		.sda = GPIO_EC_I2C_SENSOR_SDA,
+	},
+	{
+		.name = "ap_thermal",
+		.port = I2C_PORT_AP_THERMAL,
+		.kbps = 400,
+		.scl = GPIO_EC_I2C_SOC_SIC,
+		.sda = GPIO_EC_I2C_SOC_SID,
+	},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
 void sbu_fault_interrupt(enum ioex_signal signal)
 {
