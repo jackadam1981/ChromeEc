63c461b35761ccde07350ae29c24690636909554
Jacky Wang <jacky5_wang@pegatron.corp-partner.google.com>
2020-12-14 10:44:17 +0800

    shuboz: Modify PS8743B setting for signal quality
    Update setting value of PS8743B for USB-C1 signal quality
    The setting would will be modified to 0xB0 for offset 0x3B.
    
    BUG=b:175234108
    BRANCH=firmware-zork-13434.B
    TEST=make BOARD=shuboz
    Check PS8743B register value of 0x3B.
    
    Signed-off-by: Jacky Wang <jacky5_wang@pegatron.corp-partner.google.com>
    Change-Id: I69d88a4a224abf90db56cb49fc34f9bbc068a162

--- board/shuboz/board.c
+++ board/shuboz/board.c
@@ -164,12 +164,18 @@ DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_chipset_suspend, HOOK_PRIO_DEFAULT);
 static int board_ps8743_mux_set(const struct usb_mux *me,
 				mux_state_t mux_state)
 {
+	int res;
+
 	if (mux_state & USB_PD_MUX_DP_ENABLED)
 		/* Enable IN_HPD on the DB */
 		ioex_set_level(IOEX_USB_C1_HPD_IN_DB, 1);
 	else
 		/* Disable IN_HPD on the DB */
 		ioex_set_level(IOEX_USB_C1_HPD_IN_DB, 0);
+
+	res = ps8743_write(me, 0x3B, 0xB0);
+	if (res)
+		return res;
 
 	return EC_SUCCESS;
 }
