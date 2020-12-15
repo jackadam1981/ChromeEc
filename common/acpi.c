2a6acd2056b5a258e235c93db354a678870ef856
li feng <li1.feng@intel.com>
2020-12-14 19:58:51 -0800

    retimer: support BB retimer firmware update
    To update Burnside Bridge retimer firmware, host needs to query each
    port if it uses BB retimer and if it's in TBT/USB4 mode, then set
    retimer/tcss mux in certain sequence in order to update BB retimer
    firmware. One byte EC ram is allocated to communicate with host.
    
    During update, PD is in suspend mode and resume after update done.
    
    BUG=b:162528867
    BRANCH=none
    TEST=Use console command "bbupdate" to query port, "bbupdate [port][op]"
    to set mode; for example, "bbupdate 1 1" to set port 1 in USB mode; and
    verified mux is set by checking "typec 1".
    
    Signed-off-by: li feng <li1.feng@intel.com>
    Change-Id: Ie976e75f892d5caf48a948598a058a4f42e07eb3

--- common/acpi.c
+++ common/acpi.c
@@ -8,16 +8,17 @@
 #include "common.h"
 #include "console.h"
 #include "dptf.h"
+#include "ec_commands.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "host_command.h"
 #include "keyboard_backlight.h"
 #include "lpc.h"
-#include "ec_commands.h"
-#include "tablet_mode.h"
 #include "pwm.h"
 #include "timer.h"
+#include "tablet_mode.h"
 #include "usb_charge.h"
+#include "usb_common.h"
 #include "util.h"
 
 /* Console output macros */
@@ -281,6 +282,12 @@ int acpi_ap_to_ec(int is_cmd, uint8_t value, uint8_t *resultptr)
 			}
 #endif
 
+#ifdef CONFIG_USBC_SS_MUX
+		case EC_ACPI_MEM_BB_RETIMER_FW_UPDATE:
+			result = bb_retimer_fw_update_query_port();
+			CPRINTS("acpi retimer fw update query: 0x%X", result);
+			break;
+#endif
 		default:
 			result = acpi_read(acpi_addr);
 			break;
@@ -352,7 +359,7 @@ int acpi_ap_to_ec(int is_cmd, uint8_t value, uint8_t *resultptr)
 		case EC_ACPI_MEM_USB_PORT_POWER: {
 			int i;
 			int mode_field = data;
-			const int port_count = MIN(8, USB_PORT_COUNT);
+			const int port_count = MIN(4, USB_PORT_COUNT);
 
 			/*
 			 * Read the port power bit field (with max size 8 bits)
@@ -375,6 +382,14 @@ int acpi_ap_to_ec(int is_cmd, uint8_t value, uint8_t *resultptr)
 			}
 			break;
 			}
+#endif
+
+#ifdef CONFIG_USBC_SS_MUX
+		case EC_ACPI_MEM_BB_RETIMER_FW_UPDATE:
+			bb_retimer_fw_update_set_mode(
+				EC_ACPI_MEM_BB_RETIMER_PORT(data),
+				EC_ACPI_MEM_BB_RETIMER_MODE(data));
+			break;
 #endif
 
 		default:
---
+++ common/usbc/usb_retimer_fw_update.c
@@ -0,0 +1,149 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "atomic.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include "compile_time_macros.h"
+#include "console.h"
+#include "timer.h"
+#include "usb_common.h"
+#include "usb_mux.h"
+
+#ifdef CONFIG_COMMON_RUNTIME
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
+#else
+#define CPRINTS(format, args...)
+#define CPRINTF(format, args...)
+#endif
+
+__overridable int bb_retimer_fw_update_query_port(void)
+{
+	return 0;
+}
+
+void bb_retimer_fw_update_set_mode(int port, int mode)
+{
+	mux_state_t mux;
+
+	ASSERT(port >= 0 && port < CONFIG_USB_PD_PORT_MAX_COUNT);
+
+	CPRINTS("C%d retimer firmware update, set mode %d", port, mode);
+
+	switch (mode) {
+	/* Host starts retimer update sequence */
+	case EC_ACPI_MEM_BB_RETIMER_USB:
+		pd_set_suspend(port, 1);
+		mux = usb_mux_get(port);
+
+		/* If DP=1, disconnect then set USB mux */
+		if (mux & USB_PD_MUX_DP_ENABLED) {
+			usb_mux_set(port, USB_PD_MUX_NONE,
+				USB_SWITCH_DISCONNECT, pd_get_polarity(port));
+		} else if ((mux & USB_PD_MUX_USB_ENABLED) ||
+			(mux & USB_PD_MUX_TBT_COMPAT_ENABLED) ||
+			(mux & USB_PD_MUX_USB4_ENABLED)) {
+
+			/* Mux is USB, skip setting USB.
+			 * Mux is TBT/USB4, skip setting USB, Safe, and TBT,
+	                 * host can update retimer firmware directly without
+			 * going through USB->Safe->TBT.
+			 */
+			break;
+		}
+
+		usb_mux_set(port, USB_PD_MUX_USB_ENABLED,
+			USB_SWITCH_CONNECT, pd_get_polarity(port));
+
+		break;
+	case EC_ACPI_MEM_BB_RETIMER_SAFE:
+		if (pd_is_port_enabled(port)) {
+			CPRINTS("C%d enabled. Can't set safe mode",
+				port);
+			break;
+		}
+
+		mux = usb_mux_get(port);
+		if (mux & USB_PD_MUX_USB_ENABLED)
+			usb_mux_set_safe_mode(port);
+		else if (!(mux & USB_PD_MUX_TBT_COMPAT_ENABLED) &&
+				!(mux & USB_PD_MUX_USB4_ENABLED))
+			CPRINTS("C%d Can't set safe mode, current mux 0x%x)",
+				port, mux);
+		break;
+	case EC_ACPI_MEM_BB_RETIMER_TBT:
+		if (pd_is_port_enabled(port)) {
+			CPRINTS("C%d enabled. Can't set TBT mode", port);
+			break;
+		}
+
+		mux = usb_mux_get(port);
+		if (mux & USB_PD_MUX_SAFE_MODE) {
+			usb_mux_set(port, USB_PD_MUX_TBT_COMPAT_ENABLED,
+				USB_SWITCH_CONNECT, pd_get_polarity(port));
+		} else if (!(mux & USB_PD_MUX_TBT_COMPAT_ENABLED) &&
+				!(mux & USB_PD_MUX_USB4_ENABLED)) {
+			CPRINTS("C%d BB fw update failed", mux);
+		}
+		break;
+	case EC_ACPI_MEM_BB_RETIMER_DISCONNECT:
+		/* Disconnect */
+		usb_mux_set(port, USB_PD_MUX_NONE,
+			USB_SWITCH_DISCONNECT, pd_get_polarity(port));
+		pd_set_suspend(port, 0);
+		break;
+	default:
+		break;
+	}
+}
+
+static int command_bb_update(int argc, char **argv)
+{
+	int result;
+	int port;
+	int op;
+	char *e;
+
+	if (argc == 1) {
+		result = bb_retimer_fw_update_query_port();
+		CPRINTS("query result: 0x%x", result);
+		return EC_SUCCESS;
+	} else if (argc == 3) {
+		port = strtoi(argv[1], &e, 10);
+		if (*e || port >= board_get_usb_pd_port_count())
+			return EC_ERROR_PARAM1;
+		op = strtoi(argv[2], &e, 10);
+		if (*e || op > EC_ACPI_MEM_BB_RETIMER_TBT + 4)
+			return EC_ERROR_PARAM2;
+
+		if (op < 4)
+			bb_retimer_fw_update_set_mode(port, op);
+		else if (op == 4) {
+			CPRINTS("set usb mux");
+			usb_mux_set(port, USB_PD_MUX_USB_ENABLED,
+				USB_SWITCH_CONNECT, pd_get_polarity(port));
+		} else if (op == 5) {
+			CPRINTS("set safe mux");
+			usb_mux_set(port, USB_PD_MUX_SAFE_MODE,
+				USB_SWITCH_CONNECT, pd_get_polarity(port));
+		} else if (op == 6) {
+			CPRINTS("set TBT mux");
+			usb_mux_set(port, USB_PD_MUX_TBT_COMPAT_ENABLED,
+				USB_SWITCH_CONNECT, pd_get_polarity(port));
+		} else if (op == 7) {
+			CPRINTS("diconnect mux");
+			usb_mux_set(port, USB_PD_MUX_NONE,
+				USB_SWITCH_DISCONNECT, pd_get_polarity(port));
+		}
+		return EC_SUCCESS;
+	}
+	return EC_ERROR_PARAM_COUNT;
+}
+
+DECLARE_CONSOLE_COMMAND(bbupdate, command_bb_update,
+			"[port][op]",
+			"BB retimer fw update query/set mux");
+
