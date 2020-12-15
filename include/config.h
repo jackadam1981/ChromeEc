e7d880ed3025a272cd50810cc2cdf6767fd6a3d6
li feng <li1.feng@intel.com>
2020-12-14 19:58:51 -0800

    retimer: support retimer firmware update
    To support AP to update firmware during boot up, one byte EC ram is
    allocated for AP <-> EC communication. AP queries PD port/requests EC
    to perform operations via EC_CMD_ACPI_WRITE; and fetches result from EC
    via EC_CMD_ACPI_READ.
    
    The flow is:
    1. AP queries EC PD ports which port supports retimer firmware update.
    2. AP queries current MUX.
    3. a) If no device attached, AP requests EC to suspend PD task of the port it
    is going to update firmware. Then AP requests EC to set MUX to USB mode,
    Safe mode, then TBT mode. After firmware update, AP requests EC to
    disconnect MUX and resume PD task.
    3. b) If device attached, AP moves on as usual. Retimer firmware update is not
    supported.
    
    BUG=b:162528867
    BRANCH=none
    TEST=Tested NDA case on Voxel board, together with coreboot and kernel
    changes.
    After power up, host scans retimers, in sysfs
    localhost ~ # ls /sys/bus/thunderbolt/devices/
    0-0  0-0:1.1  0-0:3.1 domain0
    On PD port 1, manually update retimer firmware. Copy firmware to
    nvm_non_active1/nvmem, then authenticate it. Host starts flow to update
    firmware.
    After it's done, PD port 1 resumes. Hotplug DP dongle/TBT device, devices
    should function fully.
    TEST=Tested DA cases on Voxel board, together with coreboot and kernel
    changes. USB4/TBT devices function as usual.
    
    Signed-off-by: li feng <li1.feng@intel.com>
    Change-Id: Ie976e75f892d5caf48a948598a058a4f42e07eb3

--- include/config.h
+++ include/config.h
@@ -4329,6 +4329,9 @@
 #undef CONFIG_USBC_RETIMER_PS8818
 #undef CONFIG_USBC_RETIMER_TUSB544
 
+/* Support Type-C retimer firmware update */
+#undef CONFIG_USBC_RETIMER_FW_UPDATE
+
 /* Enable retimer TUSB544 tune EQ setting by register  */
 #undef CONFIG_TUSB544_EQ_BY_REGISTER
 
