4c4128a18bb6ca6d94d8be5058a17bad2310676f
wen zhang <zhangwen6@huaqin.corp-partner.google.com>
2020-12-17 13:48:31 +0800

    Kakadu: remove initial code for light sensor
    Kakadu was designed with reference to Krane that has a light sensor
    But Kakadu does not have a light sensor.
    So we should remove the initial code for the light sensor.
    
    BUG=b:175806247
    BRANCH=main
    TEST=1.make -j BOARD=kakadu
         2.ectool motionsense does not show light sensors
    
    Change-Id: Icde8f0a4e4fdbb7181badd53d03de7ae3a91a404
    Signed-off-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2596573
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    (cherry picked from commit 5fbc3bc43f163991174a6833790ccdce511fbf33)

--- board/kakadu/board.c
+++ board/kakadu/board.c
@@ -14,8 +14,12 @@
 #include "chipset.h"
 #include "common.h"
 #include "console.h"
+<<<<<<< HEAD   (02aa3a kukui/usb_pd: Store vbus enable status before sysjump)
 #include "driver/accelgyro_bmi160.h"
 #include "driver/als_tcs3400.h"
+=======
+#include "driver/accelgyro_bmi_common.h"
+>>>>>>> CHANGE (5fbc3b Kakadu: remove initial code for light sensor)
 #include "driver/charger/rt946x.h"
 #include "driver/sync.h"
 #include "driver/tcpm/mt6370.h"
@@ -296,6 +300,7 @@ static struct mutex g_lid_mutex;
 
 static struct bmi160_drv_data_t g_bmi160_data;
 
+<<<<<<< HEAD   (02aa3a kukui/usb_pd: Store vbus enable status before sysjump)
 /* TCS3400 private data */
 static struct als_drv_data_t g_tcs3400_data = {
 	.als_cal.scale = 1,
@@ -349,6 +354,8 @@ static struct tcs3400_rgb_drv_data_t g_tcs3400_rgb_data = {
 	.saturation.atime = TCS_DEFAULT_ATIME,
 };
 
+=======
+>>>>>>> CHANGE (5fbc3b Kakadu: remove initial code for light sensor)
 /* Matrix to rotate accelerometer into standard reference frame */
 static const mat33_fp_t lid_standard_ref = {
 	{FLOAT_TO_FP(-1), 0, 0},
@@ -401,41 +408,6 @@ struct motion_sensor_t motion_sensors[] = {
 	 .min_frequency = BMI160_GYRO_MIN_FREQ,
 	 .max_frequency = BMI160_GYRO_MAX_FREQ,
 	},
-	[CLEAR_ALS] = {
-	 .name = "Clear Light",
-	 .active_mask = SENSOR_ACTIVE_S0_S3,
-	 .chip = MOTIONSENSE_CHIP_TCS3400,
-	 .type = MOTIONSENSE_TYPE_LIGHT,
-	 .location = MOTIONSENSE_LOC_LID,
-	 .drv = &tcs3400_drv,
-	 .drv_data = &g_tcs3400_data,
-	 .port = I2C_PORT_ALS,
-	 .i2c_spi_addr_flags = TCS3400_I2C_ADDR_FLAGS,
-	 .rot_standard_ref = NULL,
-	 .default_range = 0x10000, /* scale = 1x, uscale = 0 */
-	 .min_frequency = TCS3400_LIGHT_MIN_FREQ,
-	 .max_frequency = TCS3400_LIGHT_MAX_FREQ,
-	 .config = {
-		 /* Run ALS sensor in S0 */
-		[SENSOR_CONFIG_EC_S0] = {
-			.odr = 1000,
-		},
-	 },
-	},
-	[RGB_ALS] = {
-	.name = "RGB Light",
-	 .active_mask = SENSOR_ACTIVE_S0_S3,
-	 .chip = MOTIONSENSE_CHIP_TCS3400,
-	 .type = MOTIONSENSE_TYPE_LIGHT_RGB,
-	 .location = MOTIONSENSE_LOC_LID,
-	 .drv = &tcs3400_rgb_drv,
-	 .drv_data = &g_tcs3400_rgb_data,
-	 /*.port = I2C_PORT_ALS,*/ /* Unused. RGB channels read by CLEAR_ALS. */
-	 .rot_standard_ref = NULL,
-	 .default_range = 0x10000, /* scale = 1x, uscale = 0 */
-	 .min_frequency = 0, /* 0 indicates we should not use sensor directly */
-	 .max_frequency = 0, /* 0 indicates we should not use sensor directly */
-	},
 	[VSYNC] = {
 	 .name = "Camera vsync",
 	 .active_mask = SENSOR_ACTIVE_S0,
@@ -449,9 +421,6 @@ struct motion_sensor_t motion_sensors[] = {
 	},
 };
 const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
-const struct motion_sensor_t *motion_als_sensors[] = {
-	&motion_sensors[CLEAR_ALS],
-};
 #endif /* VARIANT_KUKUI_NO_SENSORS */
 
 void usb_charger_set_switches(int port, enum usb_switch setting)
