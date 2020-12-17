9946e14676ba20752d43ea5fa3a6e6bc977382b4
Keith Short <keithshort@chromium.org>
2020-12-16 20:45:41 -0700

    tcpmv2: always issue SOP' soft reset
    After entering a PD contract, always issue an SOP' soft reset before
    sending any discovery VDMs to the cable.
    
    BUG=b:172364575
    BRANCH=volteer
    TEST=connect TBT loopback device. Observe SOP' soft reset is not sent
    pre-contract, but is sent post-contract.
    TEST=connect monitor with emarked cable, verify SOP' soft reset is sent
    when EC starts as SNK/DFP.
    TEST=Connect monitor with non-emark cable. Verify SOP' soft reset is
    sent once regardless of starting role.
    TEST=Connect USB4 dock, verify USB4 entry.
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: Id5026a2c8c9877b860e1356dd33763bad3e51841

--- test/usb_tcpmv2_compliance_common.c
+++ test/usb_tcpmv2_compliance_common.c
@@ -305,7 +305,16 @@ int proc_pd_e3(void)
 	 * set the value to SinkTXOK if it doesn't have anything to send in 1s,
 	 * the test fails. During this period, the Tester replies any message
 	 * sent from the UUT with a proper response.
+	 *
+	 * TODO(b/176249915): TPCMv2 compliance unit test: create function
+	 * to handle expected message sequences
 	 */
+	TEST_EQ(verify_tcpci_transmit(TCPC_TX_SOP_PRIME, PD_CTRL_SOFT_RESET, 0),
+		EC_SUCCESS, "%d");
+	mock_set_alert(TCPC_REG_ALERT_TX_SUCCESS);
+	task_wait_event(10 * MSEC);
+	partner_send_msg(PD_MSG_SOP_PRIME, PD_CTRL_NOT_SUPPORTED, 0, 0, NULL);
+
 	TEST_EQ(verify_tcpci_transmit(TCPC_TX_SOP_PRIME, 0, PD_DATA_VENDOR_DEF),
 		EC_SUCCESS, "%d");
 	mock_set_alert(TCPC_REG_ALERT_TX_SUCCESS);
--- test/usb_tcpmv2_td_pd_ll_e3.c
+++ test/usb_tcpmv2_td_pd_ll_e3.c
@@ -32,8 +32,19 @@ static int td_pd_ll_e3(enum pd_data_role data_role)
 
 	/*
 	 * Make sure we are idle. Reject everything that is pending
+	 *
+	 * TODO(b/176249915): TPCMv2 compliance unit test: create function
+	 * to handle expected message sequences
 	 */
 	if (data_role == PD_ROLE_DFP) {
+		TEST_EQ(verify_tcpci_transmit(TCPC_TX_SOP_PRIME,
+				PD_CTRL_SOFT_RESET, 0),
+			EC_SUCCESS, "%d");
+		mock_set_alert(TCPC_REG_ALERT_TX_SUCCESS);
+		task_wait_event(10 * MSEC);
+		partner_send_msg(PD_MSG_SOP_PRIME, PD_CTRL_NOT_SUPPORTED, 0, 0,
+			NULL);
+
 		TEST_EQ(verify_tcpci_transmit(TCPC_TX_SOP_PRIME, 0,
 				PD_DATA_VENDOR_DEF),
 			EC_SUCCESS, "%d");
