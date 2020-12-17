76306a23ddc724031e44e30a2b4f561c4c21bef3
Simon Glass <sjg@chromium.org>
2020-12-17 15:04:17 -0700

    zephyr: Add support for typeof()
    This is not available with gcc when -std is used, as it is in Zephyr
    builds. Define typeof() so that the EC codebase case build, e.g. with
    the GEN_NOT_SUPPORTED() macro.
    
    BUG=b:175434113
    BRANCH=none
    TEST=build zephyr for volteer
    emerge-volteer -q chromeos-ec
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: I32e4459c85a601d566a5c5b9b6897a7be739dbf2
    
    Change-Id: Ic923abeec6409857895c6b20f3c47d2d662ca1ba

--- include/compiler.h
+++ include/compiler.h
@@ -12,4 +12,7 @@
 #define GCC_VERSION \
 	(__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 
+/* The EC codebase assumes that typeof() is available but it is not in Zephyr */
+#define typeof(x)	__typeof__(x)
+
 #endif /* __CROS_EC_COMPILER_H */
