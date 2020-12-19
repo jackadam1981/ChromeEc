dfc684cb5c2513b30c840837071e76245c3d348b
Scott Worley <scott.worley@microchip.corp-partner.google.com>
2020-12-18 20:41:44 -0500

    mchp: MEC152x chip level GPIO updates
    MEC152x GPIO control includes an input pad disable
    feature enabled by default to reduce leakage. We add
    logic to enable the input pad when the pin is configured.
    Add support for the unimplemented pin macro.
    
    BRANCH=none
    BUG=b:177463787
    TEST=Booted skylake RVP to Chrome OS
    
    Signed-off-by: Scott Worley <scott.worley@microchip.corp-partner.google.com>
    Change-Id: Ie5ce58a698311328012440d76eedb1f2ce768d0e

--- chip/mchp/registers-mec1701.h
+++ chip/mchp/registers-mec1701.h
@@ -761,7 +761,7 @@
 
 /* MCHP implements 6 GPIO ports */
 #define MCHP_GPIO_MAX_PORT 6
-
+#define UNIMPLEMENTED_GPIO_BANK MCHP_GPIO_MAX_PORT
 /*
  * In MECxxxx documentation GPIO numbers are octal, each control
  * register is located on a 32-bit boundary.
