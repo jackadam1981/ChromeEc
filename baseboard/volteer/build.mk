0b3d2b6a8335698af9ba8b4002c6db034c1e5a5d
Ben Chen <ben.chen2@quanta.corp-partner.google.com>
2020-12-21 15:50:40 +0800

    Volteer: Initiate the first version of parsing SSFC of CBI
    Initiate the helper functions to parse the SSFC of CBI. The first
    component define lid / base sensors in this version.
    
    BUG=b:176039213, b:175843510
    BRANCH=main
    TEST=Make buildall PASS.
    
    Change-Id: I63254e94d4953c5046868b15f3684f7a8a40f1ab
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- baseboard/volteer/build.mk
+++ baseboard/volteer/build.mk
@@ -6,7 +6,7 @@
 # Volteer baseboard specific files build
 #
 
-baseboard-y=baseboard.o
+baseboard-y=baseboard.o cbi_ssfc.o
 baseboard-y+=battery_presence.o
 baseboard-y+=usb_pd_policy.o
 baseboard-y+=cbi_ec_fw_config.o
