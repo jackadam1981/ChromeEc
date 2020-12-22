10c97987e7bda1483a63990a8cadd29c6fab407e
Scott Worley <scott.worley@microchip.corp-partner.google.com>
2020-12-22 12:59:31 -0500

    mchp: Add MEC152x SKL RVP board
    Add a MEC152x board based upon SKL-RVP using the intelrvp
    baseboard. We use the fusb307 USB PD as a placeholder for
    an actual USB PD. Added MCHP EC module to the intel RVP
    baseboard.
    
    BRANCH=none
    BUG=b:177463787
    TEST=Booted skylake RVP to Chrome OS
    
    Signed-off-by: Scott Worley <scott.worley@microchip.corp-partner.google.com>
    Change-Id: I408767b80e6c4a8d3047833176af84c31773ac8f

---
+++ baseboard/intelrvp/mchp_ec.h
@@ -0,0 +1,11 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Intel BASEBOARD-RVP MCHP EC specific configuration */
+
+#ifndef __CROS_EC_MCHP_EC_H
+#define __CROS_EC_MCHP_EC_H
+
+#endif /* __CROS_EC_MCHP_EC_H */
