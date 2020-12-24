2a7dc4e28a64c490d9bea9cc519633389b71ea95
Eric Yilun Lin <yllin@chromium.org>
2020-12-24 10:50:04 +0800

    stm32/spi: disable DMA TX if it's not needed.
    This CL disable DMA TX when SPI NSS high, which means
    SPI client stops and won't receive any contents.
    Also, disable DMA on receiving SPI (spi_init) because there is no
    TX on SPI NSS just deasserted.
    
    BUG=b:156027159, b:173732816
    TEST=run reboot test
    BRANCH=kukui
    
    Change-Id: Ia86a8728056f651db4b481eca4717b506f84498e
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- chip/stm32/dma.c
+++ chip/stm32/dma.c
@@ -79,6 +79,8 @@ void dma_disable(enum dma_channel channel)
 {
 	stm32_dma_chan_t *chan = dma_get_channel(channel);
 
+	/* rewind TX buffer */
+	chan->cndtr = 0;
 	chan->ccr &= ~(STM32_DMA_CCR_EN | STM32_DMA_CCR_TCIE);
 }
 
@@ -88,6 +90,8 @@ void dma_disable_all(void)
 
 	for (ch = 0; ch < STM32_DMAC_COUNT; ch++) {
 		stm32_dma_chan_t *chan = dma_get_channel(ch);
+		/* rewind TX buffer */
+		chan->cndtr = 0;
 		chan->ccr &= ~(STM32_DMA_CCR_EN | STM32_DMA_CCR_TCIE);
 	}
 }
