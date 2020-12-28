f47006b8c4910da5dfcc12849dca896750ba3ddc
Paul Fagerburg <pfagerburg@google.com>
2020-12-28 12:27:16 -0700

    test: port the mutex unit test to Ztest
    BUG=b:172240633
    BRANCH=none
    TEST=
    
    Signed-off-by: Paul Fagerburg <pfagerburg@chromium.org>
    Change-Id: If724d9dbcf49d7a8e33a36fb8b80feaeeb3cad12

--- test/mutex.c
+++ test/mutex.c
@@ -13,14 +13,14 @@
 #include "timer.h"
 #include "util.h"
 
-static struct mutex mtx;
+static mutex_t mtx;
 
 /* period between 50us and 3.2ms */
 #define PERIOD_US(num) (((num % 64) + 1) * 50)
 /* one of the 3 MTX3x tasks */
 #define RANDOM_TASK(num) (TASK_ID_MTX3C + (num % 3))
 
-int mutex_random_task(void *unused)
+EC_TEST_RETURN mutex_random_task(TASK_PARAMS)
 {
 	char letter = 'A'+(TASK_ID_MTX3A - task_get_current());
 	/* wait to be activated */
@@ -40,7 +40,7 @@ int mutex_random_task(void *unused)
 	return EC_SUCCESS;
 }
 
-int mutex_second_task(void *unused)
+EC_TEST_RETURN mutex_second_task(TASK_PARAMS)
 {
 	task_id_t id = task_get_current();
 
@@ -59,7 +59,7 @@ int mutex_second_task(void *unused)
 	return EC_SUCCESS;
 }
 
-int mutex_main_task(void *unused)
+EC_TEST_RETURN mutex_main_task(TASK_PARAMS)
 {
 	task_id_t id = task_get_current();
 	uint32_t rdelay = (uint32_t)0x0bad1dea;
@@ -110,8 +110,38 @@ int mutex_main_task(void *unused)
 	return EC_SUCCESS;
 }
 
+/*
+ * Define the test cases to run. We need to do this twice, once in the format
+ * that Ztest uses, and again in the format the the EC test framework uses.
+ * If you add a test to one of them, make sure to add it to the other.
+ */
+
+#ifdef CONFIG_ZEPHYR
+static EC_TEST_RETURN test_mutex_main(void)
+{
+	task_wake(TASK_ID_MTX1);
+	task_wait_event(0);
+
+	return EC_SUCCESS;
+}
+
+void test_main(void)
+{
+	/*
+	 * When running under the EC kernel, there is a test task that calls
+	 * `run_test`, and the other tasks in the list get started as part of
+	 * the task list that includes `run_test`. For Zephyr, we are running
+	 * in the main thread and must call `start_ec_tasks` explicitly.
+	 */
+	start_ec_tasks();
+	ztest_test_suite(test_mutex_lib,
+			 ztest_unit_test(test_mutex_main));
+	ztest_run_test_suite(test_mutex_lib);
+}
+#else
 void run_test(int argc, char **argv)
 {
 	wait_for_task_started();
 	task_wake(TASK_ID_MTX1);
 }
+#endif /* CONFIG_ZEPHYR */
