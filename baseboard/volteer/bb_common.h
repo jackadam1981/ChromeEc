83ac29584fbe4ade721d8c4bb191276eeefe0ee4
Simon Glass <sjg@chromium.org>
2020-12-30 18:23:05 -0700

    wip
    Work-in-progress stuff
    
    BUG=b:175434113
    BRANCH=none
    TEST=build on volteer, which fails
    
    Change-Id: I77bf63bf00e209c94d874b7a9c734eca9c61c9b5
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ baseboard/volteer/bb_common.h
@@ -0,0 +1,8 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Volteer family-specific functions, shared with Zephyr */
+
+unsigned char get_board_id(void);
