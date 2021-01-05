a0babed080a8995b98ff66afc8210f75d4a9f689
Dino Li <Dino.Li@ite.com.tw>
2021-01-05 16:58:29 +0800

    it83xx/system: introduce GPG1 hard reset EC feature
    This CL added option of CONFIG_IT83XX_HARD_RESET_BY_GPG1.
    If we enabled the option, EC will assert GPG1 pin to reset itself
    instead of triggering an internal reset while receiving a hard reset
    request.
    
    BUG=b:173075595
    BRANCH=none
    TEST=Enable CONFIG_IT83XX_HARD_RESET_BY_GPG1 and check if GPG1 goes high
         while receiving hard reset command.
    
    Signed-off-by: Dino Li <Dino.Li@ite.com.tw>
    Change-Id: I930424c374dcbd742b8ecca7a1fa720699d42bb6

--- include/config.h
+++ include/config.h
@@ -2488,6 +2488,13 @@
 #undef CONFIG_IT83XX_FLASH_CLOCK_48MHZ
 
 /*
+ * If this option is enabled, EC will assert GPG1 pin to reset itself instead of
+ * triggering an internal reset while receiving a hard reset request.
+ * IMPORTANT: Don't enable this option if board doesn't support the mechanism.
+ */
+#undef CONFIG_IT83XX_HARD_RESET_BY_GPG1
+
+/*
  * Enable it if EC's VBAT won't go low when system's power isn't
  * presented (no battery and no AC)
  * If EC's VSTBY and VBAT(power source of BRAM) aren't connected to the same
