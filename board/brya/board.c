262fe26f40f7241ac2cc43413e986f883dd4c063
Caveh Jalali <caveh@chromium.org>
2021-01-05 23:54:58 -0800

    brya: Initial board support
    This provides basic board support for brya. The code is largely derived
    from volteer and adapted to reflect the updated AP and peripherals.
    
    - Only temp sensors are supported at this early stage.
    - AP power sequencing is implemented in a follow-up patch.
    
    BRANCH=none
    BUG=b:173575131
    TEST=buildall passes
    
    Change-Id: Ib3af777d4752fe86dd16d84f6d062c5f4877f31b
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- board/brya/board.c
+++ board/brya/board.c
@@ -3,10 +3,91 @@
  * found in the LICENSE file.
  */
 
+#include "button.h"
+#include "driver/ppc/nx20p348x.h"
+#include "driver/ppc/syv682x_public.h"
+#include "extpower.h"
+#include "power.h"
+#include "power_button.h"
 #include "registers.h"
-
-static void fake_gpio_interrupt(enum gpio_signal signal)
-{
-}
+#include "switch.h"
+#include "task.h"
+#include "throttle_ap.h"
+#include "usb_charge.h"
+#include "usb_pd.h"
 
 #include "gpio_list.h"
+
+/* Wake up pins */
+const enum gpio_signal hibernate_wake_pins[] = {
+	GPIO_ACOK_EC_OD,
+	GPIO_EC_RST_ODL,
+	GPIO_GSC_EC_PWR_BTN_ODL,
+	GPIO_LID_OPEN_OD,
+};
+const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
+
+/* USB-C section */
+
+void tcpc_alert_event(enum gpio_signal signal)
+{
+	switch (signal) {
+	case GPIO_USB_C0_C2_TCPC_INT_ODL:
+		schedule_deferred_pd_interrupt(USBC_PORT_C0);
+		schedule_deferred_pd_interrupt(USBC_PORT_C2);
+		break;
+	case GPIO_USB_C1_TCPC_INT_ODL:
+		schedule_deferred_pd_interrupt(USBC_PORT_C1);
+		break;
+	default:
+		break;
+	}
+}
+
+void bc12_interrupt(enum gpio_signal signal)
+{
+	switch (signal) {
+	case GPIO_USB_C0_BC12_INT_ODL:
+		task_set_event(TASK_ID_USB_CHG_P0, USB_CHG_EVENT_BC12);
+		break;
+	case GPIO_USB_C1_BC12_INT_ODL:
+		task_set_event(TASK_ID_USB_CHG_P1, USB_CHG_EVENT_BC12);
+		break;
+	case GPIO_USB_C2_BC12_INT_ODL:
+		task_set_event(TASK_ID_USB_CHG_P2, USB_CHG_EVENT_BC12);
+		break;
+	default:
+		break;
+	}
+}
+
+void ppc_interrupt(enum gpio_signal signal)
+{
+	switch (signal) {
+	case GPIO_USB_C0_PPC_INT_ODL:
+		syv682x_interrupt(USBC_PORT_C0);
+		break;
+	case GPIO_USB_C1_PPC_INT_ODL:
+		nx20p348x_interrupt(USBC_PORT_C1);
+		break;
+	case GPIO_USB_C2_PPC_INT_ODL:
+		syv682x_interrupt(USBC_PORT_C2);
+		break;
+	default:
+		break;
+	}
+}
+
+void retimer_interrupt(enum gpio_signal signal)
+{
+	switch (signal) {
+	case GPIO_USB_C0_RT_INT_ODL:
+		break;
+	case GPIO_USB_C1_RT_INT_ODL:
+		break;
+	case GPIO_USB_C2_RT_INT_ODL:
+		break;
+	default:
+		break;
+	}
+}
