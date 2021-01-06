c87ad9ed3fa23b0238429a0f222725f10f174650
Caveh Jalali <caveh@chromium.org>
2021-01-05 23:54:58 -0800

    brya: Initial board support
    This provides basic board support for brya. The code is largely derived
    from volteer and adapted to reflect the updated AP and peripherals.
    
    - Only temp sensors are supported at this early stage.
    
    NOTE:
     - includes a temporary hack to enable DEBUG_ESPI.
    
    BRANCH=none
    BUG=b:173575131
    TEST=buildall passes, brya boots with PD working on all 3 ports
    
    Change-Id: Ib3af777d4752fe86dd16d84f6d062c5f4877f31b
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- board/brya/board.c
+++ board/brya/board.c
@@ -3,26 +3,70 @@
  * found in the LICENSE file.
  */
 
+#include "button.h"
+#include "charge_ramp.h"
+#include "charger.h"
 #include "common.h"
-
+#include "console.h"
+#include "driver/ppc/nx20p348x.h"
+#include "driver/ppc/syv682x_public.h"
+#include "extpower.h"
+#include "power_button.h"
 #include "power.h"
+#include "registers.h"
 #include "switch.h"
 #include "throttle_ap.h"
 
 #include "gpio_list.h" /* Must come after other header files. */
 
-/*
- * remove when we enable CONFIG_POWER_BUTTON
+/* Console output macros */
+#define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
+
+/* Wake up pins */
+const enum gpio_signal hibernate_wake_pins[] = {
+	GPIO_ACOK_EC_OD,
+	GPIO_EC_RST_ODL,
+	GPIO_GSC_EC_PWR_BTN_ODL,
+	GPIO_LID_OPEN_OD,
+};
+const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
+
+#ifdef CONFIG_CHARGE_RAMP_SW
+
+#define BC12_MIN_VOLTAGE 4400
+
+/**
+ * Return true if VBUS is too low
  */
-
-void power_button_interrupt(enum gpio_signal signal)
+int board_is_vbus_too_low(int port, enum chg_ramp_vbus_state ramp_state)
 {
-}
+	int voltage;
 
-/*
- * remove when we enable CONFIG_VOLUME_BUTTONS
- */
+	if (charger_get_vbus_voltage(port, &voltage))
+		voltage = 0;
 
-void button_interrupt(enum gpio_signal signal)
-{
+	CPRINTS("%s: charger reports VBUS %d on port %d", __func__,
+		voltage, port);
+
+	if (voltage == 0) {
+		CPRINTS("%s: must be disconnected", __func__);
+		return 1;
+	}
+
+	if (voltage < BC12_MIN_VOLTAGE) {
+		CPRINTS("%s: lower than %d", __func__,
+			BC12_MIN_VOLTAGE);
+		return 1;
+	}
+
+	return 0;
+
+	/*
+	 * For legacy BC1.2 charging with CONFIG_CHARGE_RAMP_SW, ramp up input
+	 * current until voltage drops to the minimum input voltage of the
+	 * charger, 4.096V.
+	 */
+	/* return voltage < ISL9241_BC12_MIN_VOLTAGE; */
 }
+#endif /* CONFIG_CHARGE_RAMP_SW */
