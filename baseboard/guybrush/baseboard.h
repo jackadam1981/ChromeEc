95445ca7c9d23c0d8ee05029c815b4524ec6dbc4
Rob Barnes <robbarnes@google.com>
2021-01-05 17:10:31 -0700

    guybrush: Implement board_get_temp_adc
    Implement board_get_temp_adc.
    Add thermistor info.
    
    BUG=None
    BRANCH=None
    TEST=Build
    
    Signed-off-by: Rob Barnes <robbarnes@google.com>
    Change-Id: I471d840c0442a022260c7e04704b46b8dc143767

--- baseboard/guybrush/baseboard.h
+++ baseboard/guybrush/baseboard.h
@@ -48,6 +48,7 @@
 #define CONFIG_ADC
 #define CONFIG_THROTTLE_AP
 #define CONFIG_TEMP_SENSOR_SB_TSI
+#define CONFIG_THERMISTOR
 #define GPIO_CPU_PROCHOT	GPIO_PROCHOT_ODL
 
 /* Flash Config */
@@ -260,8 +261,6 @@ void tcpc_alert_event(enum gpio_signal signal);
 void bc12_interrupt(enum gpio_signal signal);
 void ppc_interrupt(enum gpio_signal signal);
 void sbu_fault_interrupt(enum ioex_signal signal);
-
-int baseboard_get_temp(int idx, int *temp_ptr);
 
 #endif /* !__ASSEMBLER__ */
 
