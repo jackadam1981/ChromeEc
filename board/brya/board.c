4117dd3293c34bcb85011bac0f738953137a1040
Caveh Jalali <caveh@chromium.org>
2021-01-05 23:54:58 -0800

    brya: Initial board support
    This provides basic board support for brya. The code is largely derived
    from volteer and adapted to reflect the updated AP and peripherals.
    
    - Only temp sensors are supported at this early stage.
    
    BRANCH=none
    BUG=b:173575131
    TEST=buildall passes, boots to CLI on EVB
    
    Change-Id: Ib3af777d4752fe86dd16d84f6d062c5f4877f31b
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- board/brya/board.c
+++ board/brya/board.c
@@ -3,41 +3,27 @@
  * found in the LICENSE file.
  */
 
+#include "button.h"
 #include "common.h"
-
+#include "driver/ppc/nx20p348x.h"
+#include "driver/ppc/syv682x_public.h"
+#include "extpower.h"
 #include "power.h"
+#include "power_button.h"
+#include "registers.h"
 #include "switch.h"
+#include "task.h"
+#include "throttle_ap.h"
+#include "usb_charge.h"
+#include "usb_pd.h"
 
 #include "gpio_list.h" /* Must come after other header files. */
 
-/*
- * remove when we enable CONFIG_POWER_BUTTON
- */
-
-void power_button_interrupt(enum gpio_signal signal)
-{
-}
-
-/*
- * remove when we enable CONFIG_THROTTLE_AP
- */
-
-void throttle_ap_prochot_input_interrupt(enum gpio_signal signal)
-{
-}
-
-/*
- * remove when we enable CONFIG_EXTPOWER_GPIO
- */
-
-void extpower_interrupt(enum gpio_signal signal)
-{
-}
-
-/*
- * remove when we enable CONFIG_VOLUME_BUTTONS
- */
-
-void button_interrupt(enum gpio_signal signal)
-{
-}
+/* Wake up pins */
+const enum gpio_signal hibernate_wake_pins[] = {
+	GPIO_ACOK_EC_OD,
+	GPIO_EC_RST_ODL,
+	GPIO_GSC_EC_PWR_BTN_ODL,
+	GPIO_LID_OPEN_OD,
+};
+const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
