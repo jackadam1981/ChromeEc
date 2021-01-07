22a26234d296f17f20f67c8ecb3fdf073ef28572
Gwendal Grignou <gwendal@chromium.org>
2021-01-07 11:44:27 -0800

    common: motion: fix races at shutdown
    Do not use collection_rate blindly after a function may have slept:
    the HOOK task could have run suspend() or suspend() call and set it to
    0.
    
    Fixes 104f5257 ("motion: Control on which task sensor setting functions are running on")
    [CL:2553347]
    
    BUG=b:176918310, b:170703322
    BRANCH=kukui
    TEST=unit test.
    
    Change-Id: I9ef13ceca195db4b48866f1e53f9408fb2bbf595
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2616137
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    (cherry picked from commit ea99e40f31445e67659c09b32cab6857cad8b83e)

--- common/motion_sense.c
+++ common/motion_sense.c
@@ -603,11 +603,8 @@ static inline void update_sense_data(uint8_t *lpc_status, int *psample_id)
 
 static int motion_sense_read(struct motion_sensor_t *sensor)
 {
-	if (sensor->state != SENSOR_INITIALIZED)
-		return EC_ERROR_UNKNOWN;
-
-	if (sensor->drv->get_data_rate(sensor) == 0)
-		return EC_ERROR_NOT_POWERED;
+	ASSERT(sensor->state == SENSOR_INITIALIZED);
+	ASSERT(sensor->drv->get_data_rate(sensor) != 0);
 
 #ifdef CONFIG_ACCEL_SPOOF_MODE
 	/*
@@ -704,8 +701,14 @@ static int motion_sense_process(struct motion_sensor_t *sensor,
 #endif
 	if (motion_sensor_in_forced_mode(sensor)) {
 		if (motion_sensor_time_to_read(ts, sensor)) {
-			ret = motion_sense_read(sensor);
+			/*
+			 * Since motion_sense_read can sleep, other task may be
+			 * scheduled. In particular if suspend is called by
+			 * HOOKS task, it may set colleciton_rate to 0 and we
+			 * would crash in increment_sensor_collection.
+			 */
 			increment_sensor_collection(sensor, ts);
+			ret = motion_sense_read(sensor);
 		} else {
 			ret = EC_ERROR_BUSY;
 		}
@@ -815,6 +818,7 @@ static void check_and_queue_gestures(uint32_t *event)
 	}
 #endif
 
+<<<<<<< HEAD   (d2736a stm32/spi: disable DMA TX if it's not needed.)
 #ifdef CONFIG_ORIENTATION_SENSOR
 	sensor = &motion_sensors[LID_ACCEL];
 	if (SENSOR_ACTIVE(sensor) && (sensor->state == SENSOR_INITIALIZED)) {
@@ -823,6 +827,16 @@ static void check_and_queue_gestures(uint32_t *event)
 			.activity = MOTIONSENSE_ACTIVITY_ORIENTATION,
 			.sensor_num = MOTION_SENSE_ACTIVITY_SENSOR_ID,
 		};
+=======
+		if (SENSOR_ACTIVE(sensor) &&
+		    (sensor->state == SENSOR_INITIALIZED)) {
+			struct ec_response_motion_sensor_data vector = {
+				.flags = 0,
+				.activity_data.activity =
+					MOTIONSENSE_ACTIVITY_ORIENTATION,
+				.sensor_num = MOTION_SENSE_ACTIVITY_SENSOR_ID,
+			};
+>>>>>>> CHANGE (ea99e4 common: motion: fix races at shutdown)
 
 		mutex_lock(sensor->mutex);
 		if (ORIENTATION_CHANGED(sensor) && (GET_ORIENTATION(sensor) !=
