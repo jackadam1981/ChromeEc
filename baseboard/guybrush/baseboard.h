285cb87a730fcbfc5be27b6a83278959df0e278b
Rob Barnes <robbarnes@google.com>
2021-01-08 10:53:31 -0700

    guybrush: AND PG_LPDDR4X_S3 and PG_GROUPC_S0 signals
    Latest schematic removes discrete AND gate and moves logic to EC.
    PG_LPDDR4X_S3 and PG_GROUPC_S0 are anded to drive EN_PWR_PCORE_S0.
    
    BUG=b:177071076
    BRANCH=None
    TEST=None
    
    Signed-off-by: Rob Barnes <robbarnes@google.com>
    Change-Id: Ic4e50015b495faad4a23e09faa5cd520eb158fe4

--- baseboard/guybrush/baseboard.h
+++ baseboard/guybrush/baseboard.h
@@ -273,6 +273,8 @@ void bc12_interrupt(enum gpio_signal signal);
 void ppc_interrupt(enum gpio_signal signal);
 void sbu_fault_interrupt(enum ioex_signal signal);
 
+void baseboard_en_pwr_pcore_s0(enum gpio_signal signal);
+
 #endif /* !__ASSEMBLER__ */
 
 #endif /* __CROS_EC_BASEBOARD_H */
