8288f7862e4b3c86359be8012e33ad2b176f7faa
Vincent Palatin <vpalatin@chromium.org>
2021-01-09 17:40:49 +0000

    servo_v4p1: W.I.P. fixes
    T.B.D. finish split/integration
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BUG=b:168621142
    BRANCH=servo
    TEST=Compile multi patch series. Look for anomalous ServoV4p1
    behavior, transitions, or CC traffic with REV0 -> REV1 boards.
    
    Change-Id: I0b9a55de05f9153cc26d8ad1cc1dd41919152ba3

--- board/servo_v4p1/pathsel.c
+++ board/servo_v4p1/pathsel.c
@@ -19,9 +19,12 @@ void init_pathsel(void)
 	/* Connect TypeA port to DUT hub */
 	usb3_a1_to_dut();
 	/* Connect data lines */
-	gpio_set_level(GPIO_USB3_A1_MUX_EN_L, 0);
+	usb3_a1_mux_en_l(0);
 	/* Enable power */
 	usb3_a1_pwr_en(1);
+
+	/* Disable USB2 lines */
+	gpio_set_level(GPIO_FASTBOOT_DUTHUB_MUX_EN_L, 1);
 }
 
 void usb3_a0_to_dut(void)
@@ -56,4 +59,9 @@ void uservo_to_host(void)
 {
 	gpio_set_level(GPIO_FASTBOOT_DUTHUB_MUX_SEL, 1);
 	uservo_fastboot_mux_sel(MUX_SEL_USERVO);
+}
+
+void usb3_a1_mux_en_l(int en)
+{
+	gpio_set_level(GPIO_USB3_A1_MUX_EN_L, !!(en));
 }
