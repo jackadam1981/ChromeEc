c53599c18941e32be5cccfb3f30f3771759cc6c0
Tzung-Bi Shih <tzungbi@chromium.org>
2021-01-12 10:55:12 +0800

    core/riscv-rv32i: correct inline assembly constraint
    The `locked' is actually read and write.  Use '+' instead of '='
    constraint.
    
    BRANCH=none
    BUG=b:176959740
    BUG=b:177287581
    TEST=make BOARD=asurada_scp
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: I3f6e5741afeaf98bf8f8e736c6d8db58daac6a17

--- core/riscv-rv32i/task.c
+++ core/riscv-rv32i/task.c
@@ -569,12 +569,12 @@ void __ram_code mutex_lock(struct mutex *mtx)
 	atomic_or(&mtx->waiters, id);
 
 	while (1) {
+		/* set lock value */
+		locked = 2;
 		asm volatile (
-			/* set lock value */
-			"li %0, 2\n\t"
 			/* attempt to acquire lock */
 			"amoswap.w.aq %0, %0, %1\n\t"
-			: "=r" (locked), "+A" (mtx->lock));
+			: "+r" (locked), "+A" (mtx->lock));
 		/* we got it ! */
 		if (!locked)
 			break;
