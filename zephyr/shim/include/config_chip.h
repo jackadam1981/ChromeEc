26d0d78e956ece575f68417ff67c33587c9ceae2
Simon Glass <sjg@chromium.org>
2021-01-14 13:30:07 -0700

    zephyr: Enable USB3.2 support
    Provide this option so that the extra facilities of the USB3.2
    standard can be used by a connected device.
    
    BUG=b:175434113
    BRANCH=none
    TEST=make BOARD=volteer -j30
    With a zephyr-chrome CL, build volteer on zephyr
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: I7459eb6769842f11f09d065e3df5a3d225457894

--- zephyr/shim/include/config_chip.h
+++ zephyr/shim/include/config_chip.h
@@ -542,4 +542,9 @@ enum battery_type {
 #define CONFIG_USBC_PPC_VCONN
 #endif
 
+#undef CONFIG_USB_PD_USB32_DRD
+#ifdef CONFIG_PLATFORM_EC_USB_PD_USB32_DRD
+#define CONFIG_USB_PD_USB32_DRD
+#endif
+
 #endif  /* __CROS_EC_CONFIG_CHIP_H */
