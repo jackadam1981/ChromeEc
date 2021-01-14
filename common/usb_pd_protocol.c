ca237981644872bc5c3e3d7615a7fb7ce63a2349
Abe Levkoy <alevkoy@chromium.org>
2021-01-13 17:00:32 -0700

    config: Provide default VCONN Swap delay
    Almost every relevant board copy-pastes 5000 us. Make that the default
    and get rid of the redundant definitions. This is the approximate result
    of this command:
    find . -type f -name *.h | xargs sed -i -E \
    '/#define CONFIG_USBC_VCONN_SWAP_DELAY_US[[:space:]]+5000[[:space:]]/d'
    
    BUG=b:144165680
    TEST=make buildall
    BRANCH=none
    
    Change-Id: Ife86f9752971abcd7ab5ad5a5e607eb2ccbde2ba
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -4446,12 +4446,14 @@ void pd_task(void *u)
 					/* Turn VCONN on and wait for it */
 					set_vconn(port, 1);
 					set_state_timeout(port,
-							get_time().val + CONFIG_USBC_VCONN_SWAP_DELAY_US,
-							PD_STATE_VCONN_SWAP_READY);
+						get_time().val +
+						CONFIG_USBC_VCONN_SWAP_DELAY_US,
+						PD_STATE_VCONN_SWAP_READY);
 				} else {
 					set_state_timeout(port,
-							get_time().val + PD_T_VCONN_SOURCE_ON,
-							READY_RETURN_STATE(port));
+						get_time().val +
+							PD_T_VCONN_SOURCE_ON,
+						READY_RETURN_STATE(port));
 				}
 			}
 			break;
@@ -4469,18 +4471,21 @@ void pd_task(void *u)
 						 * If failed to get goodCRC,
 						 * send soft reset
 						 */
-						set_state(port, PD_STATE_SOFT_RESET);
+						set_state(port,
+							PD_STATE_SOFT_RESET);
 						break;
 					}
-					set_state(port, READY_RETURN_STATE(port));
+					set_state(port,
+						READY_RETURN_STATE(port));
 				} else {
 					/* Turn VCONN off and wait for it */
 					set_vconn(port, 0);
 					pd_set_vconn_role(port,
 							PD_ROLE_VCONN_OFF);
 					set_state_timeout(port,
-							get_time().val + CONFIG_USBC_VCONN_SWAP_DELAY_US,
-							READY_RETURN_STATE(port));
+						get_time().val +
+						CONFIG_USBC_VCONN_SWAP_DELAY_US,
+						READY_RETURN_STATE(port));
 				}
 			}
 			break;
