4de4a23730bc6022346b41ccda5c7833ea22f1eb
Madhusudanarao Amara <madhusudanarao.amara@intel.corp-partner.google.com>
2021-01-15 22:21:15 +0530

    usb_mux: Send missed disconnect mode in S3/S0ix
    If the Type-C devices are connected in S0 and when DUT enters S3/S0ix,
    if the type-C devices are disconnected and re-connected, Kernel won't
    receive the disconnected state from EC once DUT boots to S0 as EC moves
    on and updates the new connected state to Kernel Mux driver.
    This leads to failure of Type-C device detection on resuming to S0
    from S3/S0iX. To overcome this scenario, adding an explicit condition
    to send previous disconnect state to Kernel Mux driver once initial mux
    request is received upon resuming from S3/S0iX.
    
    Missing Disconnect mode Patch Details:
     Set disconnect flag for the disconnect requests
     For AP to EC PD command: EC_CMD_USB_PD_CONTROL
       -Check disconnect flag if it is true set pd.enabled = 0
    
     For AP to EC mux command: EC_CMD_USB_PD_MUX_INFO
       -Check the disconnect flag if it is true then send disconnect mode
       -Reset the disconnect flag
       -Send host event EC_HOST_EVENT_USB_MUX for configuring the virtual mux with
        latest Mux configuration
    
    BUG=b:176604380
    BRANCH=None
    TEST=Type C devices in s0ix disconnect/connect or
         swapping across the ports scenarios tested
    
    Change-Id: Ic38d3632cb0fadb29393405e13ed3606a740c81e
    Signed-off-by: Madhusudanarao Amara <madhusudanarao.amara@intel.corp-partner.google.com>

--- driver/usb_mux/virtual.c
+++ driver/usb_mux/virtual.c
@@ -25,12 +25,31 @@
 			USB_PD_MUX_USB4_ENABLED)
 
 static mux_state_t virtual_mux_state[CONFIG_USB_PD_PORT_MAX_COUNT];
+static bool virtual_mux_disconnect_flag[CONFIG_USB_PD_PORT_MAX_COUNT];
+
+/*
+ * Set disconnect flag if the BB retimer is powered OFF followed by ON.
+ * This flag helps to send disconnect mode configuration to virtual mux
+ */
+static void virtual_set_disconnect_flag(int port, bool flag)
+{
+	virtual_mux_disconnect_flag[port] = flag;
+}
+
+/* Get disconnect mode flag used for sending host event */
+static bool virtual_get_disconnect_flag(int port)
+{
+	return virtual_mux_disconnect_flag[port];
+}
 
 static inline void virtual_mux_update_state(int port, mux_state_t mux_state)
 {
 	mux_state_t previous_mux_state = virtual_mux_state[port];
 
 	virtual_mux_state[port] = mux_state;
+
+	if (mux_state & USB_PD_MUX_NONE)
+		virtual_mux_disconnect_flag[port] = true;
 
 	if (!IS_ENABLED(CONFIG_HOSTCMD_EVENTS))
 		return;
@@ -65,6 +84,8 @@ static inline void virtual_mux_update_state(int port, mux_state_t mux_state)
 
 static int virtual_init(const struct usb_mux *me)
 {
+	virtual_set_disconnect_flag(me->usb_port, true);
+
 	return EC_SUCCESS;
 }
 
@@ -115,4 +136,6 @@ const struct usb_mux_driver virtual_usb_mux_driver = {
 	.init = virtual_init,
 	.set = virtual_set_mux,
 	.get = virtual_get_mux,
+	.set_disc_flag = virtual_set_disconnect_flag,
+	.get_disc_flag = virtual_get_disconnect_flag,
 };
