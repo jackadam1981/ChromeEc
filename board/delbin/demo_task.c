2abf5d015311af436c5956df713e15183b671dfa
Zhuohao Lee <zhuohao@chromium.org>
2021-01-18 15:32:10 +0800

    TEST ONLY: Do Not Merge
    This is a demo patch showing how the hook type works.
    Please try to migrate this patch to a workable platform you have.
    Step to build and apply to the device:
    
    1. Enter chroot, ie. cros_sdk
    2. Use command 'make BOARD=<board>' under ~/trunk/src/platform/ec/
       to build the ec.bin. If the build is pass, the image will be
       placed in ./build/<board>/ec.bin
       Note: use 'make BOARD=<board> clean' to make a clean build
    3. Connect servo board to the DUT
    4. Open another terminal and run the servod,
       (a). cros_sdk --no-ns-pid
       (b). sudo servod -b <reference_board>
       Note: please make sure the servod is running correctly
    5. Use 'dut-control ec_uart_pty' to query the uart console
    6. Use 'minicom -o -p <device path query in step 5> -C ec.log'
    7. In the device, enter VT2 by pressing ctrl+alt+F2, login and use
       command: '/usr/share/vboot/bin/set_gbb_flags.sh 0x239' to
       disable SOFTWARE SYNC
    8. If the build is pass in step 2, use command:
       'flash_ec --board=<board> --image=./build/<board>/ec.bin'
       to flash the ec.bin to the device.
    9. If step 7 is flashed correctly, the device should reboot.
    10.Check the ec.log, you will find the log below:
       HOOKS:----demo_hook_init_priority_default is called----
       HOOKS:----demo_hook_init_priority_last is called----
       <skip>
       DEMO_TASK:----demo_task starting----
       DEMO_TASK:----register demo_hook_deferred with 10s----
       DEMO_TASK:----waiting someone to wake me up----
       <skip>
       [3.446794 power state 7 = S3->S0, in 0x007f]
       CHIPSET:----demo_hook_resume is called----
       <skip>
       HOOKS:----demo_hook_deferred is called 1 times----
       HOOKS:----wakeup demo_task----
       DEMO_TASK:----demo_task resumed----
       DEMO_TASK:----waiting someone to wake me up----
       HOOKS:----register demo_hook_deferred with 3s----
       <skip>
       HOOKS:----demo_hook_deferred is called 2 times----
       HOOKS:----wakeup demo_task----
       DEMO_TASK:----demo_task resumed----
       DEMO_TASK:----waiting someone to wake me up----
       HOOKS:----register demo_hook_deferred with 3s----
       <skip>
    
    BUG=None
    BRANCH=None
    TEST=Ran on the device
    
    Change-Id: I61344d628f7705d20d6a395e5f870b913a135487
    Signed-off-by: Zhuohao Lee <zhuohao@chromium.org>

--- board/delbin/demo_task.c
+++ board/delbin/demo_task.c
@@ -4,7 +4,9 @@
  */
 
 #include "console.h"
+#include "hooks.h"
 #include "task.h"
+#include "timer.h"
 
 #define CPRINTS(format, args...) do {\
 	cprintf(CC_DEMO, "%s:", task_get_name(task_get_current())); \
@@ -12,12 +14,91 @@
 	cprintf(CC_DEMO, "\n"); \
 	} while (0)
 
+/*
+ * Section for adding the HOOK_INIT, the callback will be called
+ * by the hook task and prior to the demo_task(). Please observe
+ * the output log.
+ */
+
+/*
+ * Please try the other module priority like HOOK_PRIO_INIT_DMA,
+ * HOOK_PRIO_INIT_LPC and so on. The priority can be found from
+ * ./include/hooks.h
+ */
+
+static void demo_hook_init_priority_first(void)
+{
+	CPRINTS("----%s is called----", __func__);
+}
+DECLARE_HOOK(HOOK_INIT, demo_hook_init_priority_first, HOOK_PRIO_FIRST);
+
+static void demo_hook_init_priority_default(void)
+{
+	CPRINTS("----%s is called----", __func__);
+}
+DECLARE_HOOK(HOOK_INIT, demo_hook_init_priority_default, HOOK_PRIO_DEFAULT);
+
+static void demo_hook_init_priority_last(void)
+{
+	CPRINTS("----%s is called----", __func__);
+}
+DECLARE_HOOK(HOOK_INIT, demo_hook_init_priority_last, HOOK_PRIO_LAST);
+
+/*
+ * Section for adding the HOOK_CHIPSET_SUSPEND/HOOK_CHIPSET_RESUME,
+ * the callback will be called by the task which calles
+ * hook_notify(HOOK_CHIPSET_SUSPEND/HOOK_CHIPSET_RESUME). In this case,
+ * the chipset_task will call the callback function. Similar behavior
+ * for the HOOK_* type.
+ */
+static void demo_hook_suspend(void)
+{
+	CPRINTS("----%s is called----", __func__);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, demo_hook_suspend, HOOK_PRIO_DEFAULT);
+
+static void demo_hook_resume(void)
+{
+	CPRINTS("----%s is called----", __func__);
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, demo_hook_resume, HOOK_PRIO_DEFAULT);
+
+/*
+ * Section for adding the HOOK_DEFFERED
+ */
+
+static int count;
+/* Need to declare the function to avoid the build error */
+static void demo_hook_deferred(void);
+DECLARE_DEFERRED(demo_hook_deferred);
+
+static void demo_hook_deferred(void)
+{
+	CPRINTS("----%s is called %d times----", __func__, ++count);
+	CPRINTS("----wakeup demo_task----");
+	/*
+	 * Note: after waking up the demo_task, hook_task will be preempted
+	 * because the priority of demo_task is greater than hook_task.
+	 */
+	task_wake(TASK_ID_DEMO_TASK);
+	CPRINTS("----register %s with 3s----", __func__);
+	/* Register the callback function again, run it after 3s */
+	hook_call_deferred(&demo_hook_deferred_data, 3 * SECOND);
+}
+
 void demo_task(void *u)
 {
 	CPRINTS("----%s starting----", __func__);
 	while (1) {
+		if (!count) {
+			CPRINTS("----register demo_hook_deferred "
+				"with 10s----");
+			hook_call_deferred(&demo_hook_deferred_data,
+						10 * SECOND);
+		}
 		CPRINTS("----waiting someone to wake me up----");
 		task_wait_event(-1);
+		CPRINTS("----%s resumed----", __func__);
 	}
 	CPRINTS("----never go to here----");
 }
