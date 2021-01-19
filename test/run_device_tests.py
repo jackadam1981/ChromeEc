847fb2c58bf40b2b8749baf0fde88de3d7b2452e
Tom Hughes <tomhughes@chromium.org>
2021-01-19 10:48:56 -0800

    Update FPMCU console UART name
    The FPMCU console UART has been renamed to raw_fpmcu_console_uart_pty in
    https://crrev.com/c/2637968.
    
    BRANCH=none
    BUG=b:177331210
    TEST=On dragonclaw v0.2 with segger jlink:
         ./util/run_device_tests.py
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: If616421a55a6ecdfcf589b1c1b663820890b01ee

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -165,7 +165,7 @@ class AllTests:
 
 BLOONCHIPPER_CONFIG = BoardConfig(
     name=BLOONCHIPPER,
-    servo_uart_name='raw_fpmcu_uart_pty',
+    servo_uart_name='raw_fpmcu_console_uart_pty',
     servo_power_enable='spi1_vref',
     rollback_region0_regex=DATA_ACCESS_VIOLATION_8020000_REGEX,
     rollback_region1_regex=DATA_ACCESS_VIOLATION_8040000_REGEX,
@@ -173,7 +173,7 @@ BLOONCHIPPER_CONFIG = BoardConfig(
 
 DARTMONKEY_CONFIG = BoardConfig(
     name=DARTMONKEY,
-    servo_uart_name='raw_fpmcu_uart_pty',
+    servo_uart_name='raw_fpmcu_console_uart_pty',
     servo_power_enable='spi1_vref',
     rollback_region0_regex=DATA_ACCESS_VIOLATION_80C0000_REGEX,
     rollback_region1_regex=DATA_ACCESS_VIOLATION_80E0000_REGEX,
