de03b57bc6bf1f13341edd494493fde1cafef087
Keith Short <keithshort@chromium.org>
2021-01-19 08:45:21 -0700

    volteer: stub in fan for Zephyr
    Enable the PWM channel used for the cooling fan. This change forces
    the fan speed to 50% to provide active cooling until temperature sensors
    and a Zephyr fan API are available.
    
    BUG=b:174851463
    BRANCH=none
    TEST=make buildall, zmake testall
    TEST=Load zephyr on Volteer, verify fan turns on when the AP is powered.
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: Iaad82e4c83ccfcda3d4cc33be6fcd550930c4caa

--- zephyr/projects/volteer/include/gpio_map.h
+++ zephyr/projects/volteer/include/gpio_map.h
@@ -65,6 +65,9 @@
 #define GPIO_EN_PP5000_USBA        NAMED_GPIO(en_pp5000_usba)
 #define GPIO_USB_C1_RT_RST_ODL     NAMED_GPIO(usb_c1_rt_rst_odl_boardid_0)
 
+/* Fan control */
+#define GPIO_EN_PP5000_FAN         NAMED_GPIO(en_pp5000_fan)
+
 /* Don't have a load switch for retimer */
 /* TODO(b/176559881): How to do unimplemented GPIOs? */
 #define GPIO_USB_C1_LS_EN          NAMED_GPIO(unused_gpio41)
