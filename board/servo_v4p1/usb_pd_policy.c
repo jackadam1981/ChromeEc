a65ac02811695e4fdbbf87a54627015c3011ba06
Vincent Palatin <vpalatin@chromium.org>
2021-01-20 20:04:26 +0000

    [DISCARD?] servo_v4p1: disconnect the USB2 lines from the DUT
    Pilot the output enable of the USB2 mux to disconnect the USB 2.0 D+/D-
    from the DUT connector when they are not needed
    
    NOTE: I don't think this is strictly required by the type-C standard
    AFAIR either options are fine for USB safe mode.
    Type-C spec Table E-1 'USB Safe State Electrical Requirements' says
    for pins A6/A7/B6/B7 (aka USB2.0 D+/D-):
    "Applies to docking solutions/direct connect applications that redefine
    pins A6, A7, B6 and B7"
    And regardless this is safe for the servo use-cases with the captive
    cable to keep them connected.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BUG=none
    BRANCH=servo
    TEST=none
    
    Change-Id: Ib35a31a1a9f0abb6bbdd201afdd779c06861aa28

--- board/servo_v4p1/usb_pd_policy.c
+++ board/servo_v4p1/usb_pd_policy.c
@@ -1535,6 +1535,8 @@ __override void pd_execute_data_swap(int port,
 #if 0
 	if (last_role != data_role) {
 		last_role=data_role;
+			/* Disable USB2 lines */
+			gpio_set_level(GPIO_FASTBOOT_DUTHUB_MUX_EN_L, 1);
 			/* Disable USB3 lines */
 			usb_mux_set(port, USB_PD_MUX_NONE, USB_SWITCH_DISCONNECT,
 			   pd_get_polarity(port));
@@ -1544,6 +1546,9 @@ __override void pd_execute_data_swap(int port,
 
 	switch(data_role){
 	case PD_ROLE_DFP:
+		/* Enable USB2 lines */
+		gpio_set_level(GPIO_FASTBOOT_DUTHUB_MUX_EN_L, 0);
+
 		if (cc_config & CC_FASTBOOT_DFP)
 			dut_to_host();
 		else
@@ -1558,6 +1563,9 @@ __override void pd_execute_data_swap(int port,
 		   pd_get_polarity(port));
 	break;
 	case PD_ROLE_UFP:
+		/* Enable USB2 lines */
+		gpio_set_level(GPIO_FASTBOOT_DUTHUB_MUX_EN_L, 0);
+
 		if (!(cc_config & CC_FASTBOOT_DFP))
 			uservo_to_host();
 
@@ -1570,6 +1578,8 @@ __override void pd_execute_data_swap(int port,
 			   pd_get_polarity(port));
 	break;
 	case PD_ROLE_DISCONNECTED:
+		/* Disable USB2 lines */
+		gpio_set_level(GPIO_FASTBOOT_DUTHUB_MUX_EN_L, 1);
 		if (!(cc_config & CC_FASTBOOT_DFP))
 			uservo_to_host();
 		/* Disable USB3 lines */
