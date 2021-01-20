412908eb1719de7aeb056553386177947284db9d
Ting Shen <phoenixshen@google.com>
2021-01-20 14:21:35 +0800

    WIP: rt1718s bringup
    BUG=b:177391887
    TEST=none
    BRANCH=main
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I8f017e21b74c1e27ca7f257b76b0ef74fd0343f2

---
+++ driver/tcpm/rt1718s.c
@@ -0,0 +1,202 @@
+/* Copyright 2021 The Richtek Technology Corp. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * RT1718S TCPC Driver
+ */
+
+#include "console.h"
+#include "driver/tcpm/rt1718s.h"
+#include "driver/tcpm/tcpci.h"
+#include "driver/tcpm/tcpm.h"
+#include "stdint.h"
+#include "task.h"
+#include "timer.h"
+#include "usb_mux.h"
+#include "usb_pd.h"
+#include "util.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+
+#define ENABLE_FAST_ROLE_SWAP		0
+
+/* i2c_write function which won't wake TCPC from low power mode. */
+static int rt1718s_write8(int port, int reg, int val)
+{
+	if (reg & 0xF200) {
+		uint8_t out[3];
+
+		out[0] = (reg & 0xFF00) >> 8;
+		out[1] = (reg & 0x00FF);
+		out[2] = val;
+
+		return i2c_xfer(tcpc_config[port].i2c_info.port,
+			tcpc_config[port].i2c_info.addr_flags,
+			out, sizeof(out), NULL, 0);
+	} else {
+		return i2c_write8(tcpc_config[port].i2c_info.port,
+			  tcpc_config[port].i2c_info.addr_flags, reg, val);
+	}
+}
+
+static int rt1718s_read8(int port, int reg, int *val)
+{
+	if (reg & 0xF200) {
+		int rv;
+		uint8_t out[2], in;
+
+		out[0] = (reg & 0xFF00) >> 8;
+		out[1] = (reg & 0x00FF);
+
+		rv = i2c_xfer(tcpc_config[port].i2c_info.port,
+			tcpc_config[port].i2c_info.addr_flags,
+			out, sizeof(out), &in, sizeof(in));
+		*val = (int)in;
+
+		return rv;
+	} else {
+		return i2c_read8(tcpc_config[port].i2c_info.port,
+			  tcpc_config[port].i2c_info.addr_flags, reg, val);
+	}
+}
+
+static int rt1718s_update_bits8(int port, int reg, int mask, int val)
+{
+	int reg_val;
+
+	if (mask == 0xFF)
+		return rt1718s_write8(port, reg, val);
+
+	RETURN_ERROR(rt1718s_read8(port, reg, &reg_val));
+
+	reg_val &= (~mask);
+	reg_val |= (mask & val);
+	return rt1718s_write8(port, reg, reg_val);
+}
+
+static int rt1718s_sw_reset(int port)
+{
+	int rv;
+
+	rv = rt1718s_update_bits8(port, RT1718S_SYS_CTRL3,
+		RT1718S_SWRESET_MASK,RT1718S_SWRESET_MASK);
+
+	msleep(2);
+
+	return rv;
+}
+
+static int rt1718s_init(int port)
+{
+	int sys_ctrl1;
+
+	RETURN_ERROR(rt1718s_read8(port, RT1718S_SYS_CTRL1, &sys_ctrl1));
+
+	if (!(sys_ctrl1 & 0x20))
+		RETURN_ERROR(rt1718s_sw_reset(port));
+
+	/* set GPIO2 is push pull, as output, output low. */
+	RETURN_ERROR(rt1718s_update_bits8(port, RT1718S_GPIO2_CTRL, 0x0E, 0x0C));
+
+	/*
+	-- set GPIO2 as high when frs signal received.
+	-- set GPIO2 as low when RT1718S transmits frs signal.
+	-- set GPIO2 as high when enable source HV vbus by 0x23.
+	-- set GPIO2 as high when enable source vbus by 0x23.
+	-- set GPIO2 as low when disable source vbus by 0x23.
+	-- set GPIO2 as low when enable sink vbus by 0x23.
+	-- set GPIO2 as low when disable sink vbus by 0x23.
+	*/
+	RETURN_ERROR(rt1718s_update_bits8(port, RT1718S_GPIO2_VBUS_CTRL, 0x7F, 0x58));
+
+	/* set GPB floating when disable sink vbus by 0x23. */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xDE, 0xF0, 0x80));
+
+	/* set GPB low when enable sink vbus by 0x23. */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xDF, 0xF0, 0x20));
+
+	/* set GPB floating when disable source vbus by 0x23. */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xE0, 0xF0, 0x80));
+
+	/* set GPB floating when enable source vbus by 0x23. */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xE1, 0xF0, 0x80));
+
+	/* set GPB floating when enable source HV vbus by 0x23. */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xE2, 0xF0, 0x80));
+
+#if ENABLE_FAST_ROLE_SWAP
+	/* set GPB floating when RT1718S transmits frs signal (sink path off). */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xE8, 0xF0, 0x80));
+
+	/* set GPB floating when frs signal received (sink path off). */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xE9, 0xF0, 0x80));
+#endif
+
+	/* set gpio2 (source path)being controlled by 0x23. */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xEC, 0x80, 0x80));
+
+	/* set GPB (sink path) being controlled by 0x23. GPA not being controlled by 0x23. */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xEC, 0x30, 0x20));
+
+#if ENABLE_FAST_ROLE_SWAP
+	/* set GPIO2 frs action after vbus<5.5V */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xCE, 0x08, 0x08));
+
+	/* set GPB frs action after vbus<5.5V */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xCE, 0x02, 0x02));
+
+	/* set vendor defince alert unmasked */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0x13, 0x80, 0x80));
+
+	/* set vbus frs low unmasked, Rx frs unmasked */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0x91, 0xC0, 0xC0));
+#endif
+
+	/* Disable FOD function */
+	RETURN_ERROR(rt1718s_update_bits8(port, 0xCF, 0x40, 0x00));
+
+	/* tcpc connect invalid disabled. Exit shipping mode */
+	RETURN_ERROR(rt1718s_update_bits8(port, RT1718S_SYS_CTRL1, 0x60, 0x20));
+
+	RETURN_ERROR(rt1718s_write8(port, 0x1F, 0xFF));
+
+	RETURN_ERROR(rt1718s_write8(port, 0x11, 0xFF));
+
+	RETURN_ERROR(rt1718s_update_bits8(port, 0x8C, 0x02, 0x00));
+
+	return tcpci_tcpm_init(port);
+}
+
+/* RT1718S is a TCPCI compatible port controller */
+const struct tcpm_drv rt1718s_tcpm_drv = {
+	.init			= &rt1718s_init,
+	.release		= &tcpci_tcpm_release,
+	.get_cc			= &tcpci_tcpm_get_cc,
+#ifdef CONFIG_USB_PD_VBUS_DETECT_TCPC
+	.check_vbus_level	= &tcpci_tcpm_check_vbus_level,
+#endif
+	.select_rp_value	= &tcpci_tcpm_select_rp_value,
+	.set_cc			= &tcpci_tcpm_set_cc,
+	.set_polarity		= &tcpci_tcpm_set_polarity,
+	.set_vconn		= &tcpci_tcpm_set_vconn,
+	.set_msg_header		= &tcpci_tcpm_set_msg_header,
+	.set_rx_enable		= &tcpci_tcpm_set_rx_enable,
+	.get_message_raw	= &tcpci_tcpm_get_message_raw,
+	.transmit		= &tcpci_tcpm_transmit,
+	.tcpc_alert		= &tcpci_tcpc_alert,
+#ifdef CONFIG_USB_PD_DISCHARGE_TCPC
+	.tcpc_discharge_vbus	= &tcpci_tcpc_discharge_vbus,
+#endif
+#ifdef CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE
+	.drp_toggle		= &tcpci_tcpc_drp_toggle,
+#endif
+	.get_chip_info		= &tcpci_get_chip_info,
+#ifdef CONFIG_USBC_PPC
+	.set_snk_ctrl		= &tcpci_tcpm_set_snk_ctrl,
+	.set_src_ctrl		= &tcpci_tcpm_set_src_ctrl,
+#endif
+#ifdef CONFIG_USB_PD_TCPC_LOW_POWER
+	.enter_low_power_mode	= &tcpci_enter_low_power_mode,
+#endif
+};
