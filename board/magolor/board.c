26a6b603aef35dd211a0b9fac0c6fe06f76f36c5
Ben Chen <ben.chen2@quanta.corp-partner.google.com>
2021-01-21 19:57:55 +0800

    magolor: disable keyboard scan on tablet mode
    Disable keyboard function when lid angle > 180 degree,
    enable keyboard function when lig angle < 180 degree.
    
    BUG=b:173476750
    BRANCH=cros/main
    TEST=make buildall
    TEST=test on magolor, side-buttons work in clamshell/tablet mode.
    Disable keyboard function on tablet mode.
    
    Change-Id: I1b4abb23c977a971710c0ae70a012954ba0cb47e
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- board/magolor/board.c
+++ board/magolor/board.c
@@ -698,8 +698,6 @@ DECLARE_HOOK(HOOK_TICK, adc_vol_key_press_check, HOOK_PRIO_DEFAULT);
 /* This callback disables keyboard when convertibles are fully open */
 void lid_angle_peripheral_enable(int enable)
 {
-	int chipset_in_s0 = chipset_in_state(CHIPSET_STATE_ON);
-
 	/*
 	 * If the lid is in tablet position via other sensors,
 	 * ignore the lid angle, which might be faulty then
@@ -712,12 +710,9 @@ void lid_angle_peripheral_enable(int enable)
 		keyboard_scan_enable(1, KB_SCAN_DISABLE_LID_ANGLE);
 	} else {
 		/*
-		 * Ensure that the chipset is off before disabling the keyboard.
-		 * When the chipset is on, the EC keeps the keyboard enabled and
-		 * the AP decides whether to ignore input devices or not.
+		 * Disabling the keyboard scan on tablet mode.
 		 */
-		if (!chipset_in_s0)
-			keyboard_scan_enable(0, KB_SCAN_DISABLE_LID_ANGLE);
+		keyboard_scan_enable(0, KB_SCAN_DISABLE_LID_ANGLE);
 	}
 }
 #endif
