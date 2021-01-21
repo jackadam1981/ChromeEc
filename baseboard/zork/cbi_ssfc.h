2a3ec928efa7c46080f6882edddafc6e18521c4d
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2021-01-21 10:01:31 +0800

    zork: add support SSFC
    This patch add support SSFC for zork,
    add support base sensor as below:
       SSFC_SENSOR_BMI160 = 1,
       SSFC_SENSOR_ICM426XX = 2
    
    BUG=b:173753906
    BRANCH=zork
    TEST=make buildall
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: I10749d819b852861ac5a32d69f2b872527633082

---
+++ baseboard/zork/cbi_ssfc.h
@@ -0,0 +1,39 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef _VOLTEER_CBI_SSFC__H_
+#define _VOLTEER_CBI_SSFC__H_
+
+#include "stdint.h"
+
+/****************************************************************************
+ * Volteer CBI Second Source Factory Cache
+ */
+
+/*
+ * Base Sensor (Bits 0-2)
+ */
+enum ec_ssfc_base_sensor {
+	SSFC_SENSOR_BASE_DEFAULT = 0,
+	SSFC_SENSOR_BMI160 = 1,
+	SSFC_SENSOR_ICM426XX = 2
+};
+
+union zork_cbi_ssfc {
+	struct {
+		enum ec_ssfc_base_sensor	base_sensor : 3;
+		uint32_t			reserved_2 : 29;
+	};
+	uint32_t raw_value;
+};
+
+/**
+ * Get the Base sensor type from SSFC_CONFIG.
+ *
+ * @return the Base sensor board type.
+ */
+enum ec_ssfc_base_sensor get_cbi_ssfc_base_sensor(void);
+
+#endif /* _Volteer_CBI_SSFC__H_ */
