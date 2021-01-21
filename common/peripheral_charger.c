e687e64f481780752055eac7604ce1a5890ab0c7
Daisuke Nojiri <dnojiri@chromium.org>
2021-01-21 10:16:46 -0800

    pchg: Initialize pchg chips on startup
    This patch makes pchg task disable the interrupt on shutdown and
    initialize pchg chips on start-up.
    
    Tested as follows:
    
    1. Run dut-control power_state:on -> off -> on.
    2. Run dut-control power_state:reset.
    
    Do 1 and 2 with and without a stylus attached and verify pchg task
    remains under control.
    
    BUG=b:176725734, b:173235954
    BRANCH=trogdor
    TEST=CoachZ
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I0b1b5501975ad6c8a89041639d3fa90a71b4e9b5

--- common/peripheral_charger.c
+++ common/peripheral_charger.c
@@ -4,6 +4,7 @@
  */
 
 #include "atomic.h"
+#include "chipset.h"
 #include "common.h"
 #include "device_event.h"
 #include "hooks.h"
@@ -297,14 +298,12 @@ void pchg_irq(enum gpio_signal signal)
 	}
 }
 
-void pchg_task(void *u)
+static void pchg_startup(void)
 {
 	struct pchg *ctx;
 	int p;
-	int rv;
 
-	/* TODO: i2c is wedged for a while after reset. investigate. */
-	msleep(500);
+	CPRINTS("%s", __func__);
 
 	for (p = 0; p < pchg_count; p++) {
 		ctx = &pchgs[p];
@@ -313,6 +312,39 @@ void pchg_task(void *u)
 		pchg_queue_event(ctx, PCHG_EVENT_INITIALIZE);
 		gpio_enable_interrupt(ctx->cfg->irq_pin);
 	}
+
+	task_wake(TASK_ID_PCHG);
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, pchg_startup, HOOK_PRIO_DEFAULT);
+
+static void pchg_shutdown(void)
+{
+	struct pchg *ctx;
+	int p;
+
+	CPRINTS("%s", __func__);
+
+	for (p = 0; p < pchg_count; p++) {
+		ctx = &pchgs[0];
+		gpio_disable_interrupt(ctx->cfg->irq_pin);
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, pchg_shutdown, HOOK_PRIO_DEFAULT);
+
+void pchg_task(void *u)
+{
+	struct pchg *ctx;
+	int p;
+	int rv;
+
+	/* This is implicitly done if memory is cleared. */
+	for (p = 0; p < pchg_count; p++) {
+		ctx = &pchgs[p];
+		ctx->state = PCHG_STATE_RESET;
+	}
+
+	if (chipset_in_state(CHIPSET_STATE_ON))
+		pchg_startup();
 
 	while (true) {
 		/* Process pending events for all ports. */
