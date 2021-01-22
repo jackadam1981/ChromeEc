581fcdada701e6283a5cfcd6a2147d27a983f80d
Scott Collyer <scollyer@google.com>
2021-01-22 07:34:22 -0800

    tcpmv2: Fix ordering dependancy of rch and tx PRL state machines
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I7a7c193952ec5047b9d1f01dec896257274ea96b

--- common/usbc/usb_prl_sm.c
+++ common/usbc/usb_prl_sm.c
@@ -439,7 +439,7 @@ test_export_static enum usb_prl_tx_state prl_tx_get_state(const int port)
 /* Print the protocol transmit statemachine's current state. */
 static void print_current_prl_tx_state(const int port)
 {
-	if (prl_debug_level >= DEBUG_LEVEL_3)
+	if (prl_debug_level >= DEBUG_LEVEL_2)
 		CPRINTS("C%d: %s", port,
 				prl_tx_state_names[prl_tx_get_state(port)]);
 }
@@ -482,7 +482,7 @@ test_export_static enum usb_rch_state rch_get_state(const int port)
 /* Print the chunked Rx statemachine's current state. */
 static void print_current_rch_state(const int port)
 {
-	if (prl_debug_level >= DEBUG_LEVEL_3)
+	if (prl_debug_level >= DEBUG_LEVEL_2)
 		CPRINTS("C%d: %s", port,
 				rch_state_names[rch_get_state(port)]);
 }
@@ -1603,13 +1603,22 @@ static void rch_requesting_chunk_run(const int port)
 	 * Transmission Error from Protocol Layer or
 	 * Message Received From Protocol Layer
 	 */
-	else if (RCH_CHK_FLAG(port, PRL_FLAGS_MSG_RECEIVED) ||
-			PDMSG_CHK_FLAG(port, PRL_FLAGS_TX_ERROR)) {
+	else if (PDMSG_CHK_FLAG(port, PRL_FLAGS_TX_ERROR)) {
 		/*
 		 * Leave PRL_FLAGS_MSG_RECEIVED flag set. It'll be
 		 * cleared in rch_report_error state
 		 */
 		set_state_rch(port, RCH_REPORT_ERROR);
+	} else if (RCH_CHK_FLAG(port, PRL_FLAGS_MSG_RECEIVED)) {
+		/*
+		 * It is possible to have both message received and the chunk
+		 * request transmit complete before a full PRL SM run. But, the
+		 * PRL_RX state machine runs prior to RCH, but before PRL_TX, so
+		 * PRL_FLAGS_MSG_RECEIVED can be set without
+		 * PRL_FLAGS_TX_COMPLETE set at this point (though it will be
+		 * set as soon as PRL_TX is executed next.
+		 */
+		set_state_rch(port, RCH_WAITING_CHUNK);
 	}
 }
 
@@ -1630,6 +1639,9 @@ static void rch_waiting_chunk_entry(const int port)
 static void rch_waiting_chunk_run(const int port)
 {
 	if (RCH_CHK_FLAG(port, PRL_FLAGS_MSG_RECEIVED)) {
+		if (PDMSG_CHK_FLAG(port, PRL_FLAGS_TX_COMPLETE))
+			PDMSG_CLR_FLAG(port, PRL_FLAGS_TX_COMPLETE);
+
 		/*
 		 * Leave PRL_FLAGS_MSG_RECEIVED flag set just in case an error
 		 * is detected. If an error is detected, PRL_FLAGS_MSG_RECEIVED
