682acdc40f8b11f88f9dd9192094cd0996f288c3
Patrick Georgi <pgeorgi@google.com>
2021-01-25 21:51:49 +0100

    zephyr: Use Chrome EC's libgcc implementation over the compiler's
    gcc's libgcc is built with a compile time notion of what an
    architecture's ABI will look like. If that happens to be wrong, you're
    out of luck.
    
    Instead, use our own implementation which, while written in assembly
    (and as such not as flexible as it could be) is processed with the
    right set of flags.
    
    BUG=b:178363068
    BRANCH=none
    TEST=zephyr boots on kohaku when built with coreboot-sdk.
    
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Change-Id: I6d27bb48478081b6c2ff8927734492282e55e898

--- zephyr/cmake/compiler/gcc/target.cmake
+++ zephyr/cmake/compiler/gcc/target.cmake
@@ -2,6 +2,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# On ARM, we don't use libgcc: It's built against a fixed target (e.g.
+# used instruction set, ABI, ISA extensions) and doesn't adapt when
+# compiler flags change any of these assumptions. Use our own mini-libgcc
+# instead.
+if("${ARCH}" STREQUAL "arm")
+set(no_libgcc True)
+endif()
+
 # Zephyr cmake system looks into ${TOOLCHAIN_ROOT}, but we just send
 # this out to the copy in ${ZEPHYR_BASE}.
 include("${ZEPHYR_BASE}/cmake/compiler/gcc/target.cmake")
