8410c32afd0f8629e0a13b01cdf4a8a506dc0f5b
Jack Rosenthal <jrosenth@chromium.org>
2021-01-26 08:23:55 -0700

    zephyr: minimal uart_* implementations
    Added minimum required uart_* functions for panic to work, using
    printk as backend.  Probably should switch filling-out enough to
    support uart_buffering.c in the future, requiring switching to
    Zephyr's UART drivers instead of printk().
    
    BUG=b:178033156,b:178011288
    BRANCH=none
    TEST=compile with panic_output.c
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I2879cd05cd858d13241e6fc1a7f818b6649e4bd6

--- zephyr/shim/src/console.c
+++ zephyr/shim/src/console.c
@@ -11,6 +11,7 @@
 
 #include "console.h"
 #include "printf.h"
+#include "uart.h"
 
 int cputs(enum console_channel channel, const char *str)
 {
@@ -89,4 +90,22 @@ int zshim_run_ec_console_command(int (*handler)(int argc, char **argv),
 	}
 
 	return handler(argc, argv);
+}
+
+/*
+ * Minimal implementation of a few uart_* functions we need.
+ * TODO(b/178033156): probably need to swap this for something more
+ * robust in order to handle UART buffering.
+ */
+void uart_write_char(char c)
+{
+	printk_putchar(c);
+}
+
+void uart_flush_output(void)
+{
+}
+
+void uart_tx_flush(void)
+{
 }
