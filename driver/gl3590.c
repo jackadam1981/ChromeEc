daa3123ecbadd5f1619eb066193695407113ff93
Jan Dabros <jsd@semihalf.com>
2021-01-27 21:38:57 +0100

    gl3590: Add init function
    Add a way to verify whether I2C interface of the hub is ready and
    initialize it.
    
    BUG=b:178125550
    BRANCH=main
    TEST=With consecutive patches applied, verify host hub functionality on
    servo_v4p1 board. All USB ports should be usable. Check console logs for
    any possible error messages.
    
    Signed-off-by: Jan Dabros <jsd@semihalf.com>
    Change-Id: I9a4ad484d07b50e0c497f56bfaf111b47b3b0576

--- driver/gl3590.c
+++ driver/gl3590.c
@@ -85,6 +85,33 @@ int gl3590_write(int hub, uint8_t reg, uint8_t *data, int count)
 	return rv;
 }
 
+/*
+ * Basic initialization of GL3590 I2C interface.
+ *
+ * Please note, that I2C interface is online not earlier than ~50ms after
+ * RESETJ# is deasserted. Platform should check that PGREEN_A_SMD pin is
+ * asserted. This init function shouldn't be invoked until that time.
+ */
+void gl3590_init(int hub)
+{
+	uint8_t tmp;
+
+	if (gl3590_read(hub, GL3590_HUB_MODE_REG, &tmp, 1)) {
+		CPRINTF("GL3590: Cannot read HUB_MODE register");
+		return;
+	}
+	if ((tmp & GL3590_HUB_MODE_I2C_READY) == 0)
+		CPRINTF("GL3590 interface isn't ready, consider deferring "
+			"this init\n");
+
+	/* Deassert INTR# signal */
+	tmp = GL3590_INT_CLEAR;
+	if (gl3590_write(hub, GL3590_INT_REG, &tmp, 1)) {
+		CPRINTF("GL3590: Cannot write to INT register");
+		return;
+	};
+}
+
 void gl3590_irq_handler(int hub)
 {
 	uint8_t buf = 0;
