8396323d1729ccaf564863e86d7f20c07fa970b4
Scott Collyer <scollyer@google.com>
2021-01-29 09:10:50 -0800

    TCPMv2: Add high priority task timing
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I62f072093b2a7688e461975c4ab570922b67df2c

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -3114,6 +3114,7 @@ static void pe_snk_select_capability_entry(int port)
 	/* Send Request */
 	pe_send_request_msg(port);
 	pe_sender_response_msg_entry(port);
+	tc_high_priority_event(port, true);
 
 	/* We are PD Connected */
 	PE_SET_FLAG(port, PE_FLAGS_PD_CONNECTION);
@@ -3234,6 +3235,11 @@ static void pe_snk_select_capability_run(int port)
 	/* SenderResponsetimer timeout */
 	if (get_time().val > pe[port].sender_response_timer)
 		set_state_pe(port, PE_SNK_HARD_RESET);
+}
+
+static void pe_snk_select_capability_exit(int port)
+{
+	tc_high_priority_event(port, false);
 }
 
 /**
@@ -6910,6 +6916,7 @@ static __const_data const struct usb_state pe_states[] = {
 	[PE_SNK_SELECT_CAPABILITY] = {
 		.entry = pe_snk_select_capability_entry,
 		.run = pe_snk_select_capability_run,
+		.exit = pe_snk_select_capability_exit,
 	},
 	[PE_SNK_READY] = {
 		.entry = pe_snk_ready_entry,
