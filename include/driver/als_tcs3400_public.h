5024a439b4c58934d526d8c8446507c6dd81947a
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2021-01-29 15:35:52 +0800

    dooly: update als lux equation amd coefficient
    Implement als sensor TCS3400 lux equation and coefficient:
    Lux = DGFn * ((C * Ccoefn) + (R * Rcoefn) + (G * Gcoefn) +
    (B * Bcoefn)) / (Atime*Again)
    
    If (G+B)/C < 0.514, n=1
    If (G+B)/C >= 0.514 and < 0.66, n=2
    If (G+B)/C >= 0.66 and < 1.012, n=3
    Else n=4:
    
    Coeffs  n=1 (Lo)        n=2 (LM)     n=3 (MH)        n=4 (Hi)
    Ccoef   3.8             -17.436      0.08    	     -0.686
    Rcoef   3.956     	14.535       -0.89           1.224
    GCoef   -20.915   	32.07        7.096           4.043
    BCoef   3.281     	2.43         -5.603          -4.584
    DGF     993             993          993             993
    
    BUG=b:176671564
    BRANCH=puff
    TEST=make BOARD=dooly
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: Iaee45f670487d00179607192f2d97ca86415f077

--- include/driver/als_tcs3400_public.h
+++ include/driver/als_tcs3400_public.h
@@ -32,6 +32,7 @@
 #define TCS_CALIBRATION_AGAIN   0x02            /* 16x gain */
 #define TCS_DEFAULT_AGAIN       TCS_CALIBRATION_AGAIN
 
+#define TCS_MAX_INTEGRATION_TIME 2.78 /* 2.78ms */
 #define TCS_ATIME_DEC_STEP      5
 #define TCS_ATIME_INC_STEP      TCS_GAIN_UPSHIFT_ATIME
 
