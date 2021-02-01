7b2ffb4bc3b6050e37a36cf0075b15e5d2447029
Yuval Peress <peress@chromium.org>
2021-02-01 14:10:02 -0700

    zmake: allow zmake build/test to target project directories
    This change allows zmake to use the default platform/ec/build
    directory when running the build/test subcommand. Developers
    can now simply point to the actual projects without having to
    worry about where the build is being done.
    
    BRANCH=none
    BUG=none
    TEST=Added/modified unit tests
    TEST=zmake configure zephyr/test/system ; \
         zmake build zephyr/test/system ; \
         zmake test zephyr/test/system
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: Id830f8dffc30f641fff0cd5ff50d30b2efedeece

--- zephyr/zmake/tests/test_util.py
+++ zephyr/zmake/tests/test_util.py
@@ -3,22 +3,57 @@
 # found in the LICENSE file.
 
 import pathlib
+import pytest
+import tempfile
 
 import zmake.util as util
 
 
 def test_resolve_build_dir_with_build_dir():
-    build_dir = util.resolve_build_dir(
-        platform_ec_dir=pathlib.PosixPath('/x'),
-        project_dir=pathlib.PosixPath('/x/y'),
-        build_dir=pathlib.PosixPath('/x/z'))
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        temp_dir = pathlib.Path(temp_dir_name)
+        (temp_dir / 'x' / 'z' / 'project').mkdir(parents=True)
+        (temp_dir / 'x' / 'z' / 'project' / 'zmake.yaml').touch()
+        build_dir = util.resolve_build_dir(
+            platform_ec_dir=temp_dir / 'x',
+            project_dir=temp_dir / 'x' / 'y',
+            build_dir=temp_dir / 'x' / 'z')
 
-    assert build_dir == pathlib.PosixPath('/x/z')
+        assert build_dir == temp_dir / 'x' / 'z'
 
 
-def test_resolve_build_dir_default_dir():
-    build_dir = util.resolve_build_dir(
-        platform_ec_dir=pathlib.PosixPath('/x'),
-        project_dir=pathlib.PosixPath('/x/y/z'),
-        build_dir=None)
-    assert build_dir == pathlib.PosixPath('/x/build/y/z')
+def test_resolve_build_dir_invalid_project():
+    try:
+        with tempfile.TemporaryDirectory() as temp_dir_name:
+            temp_dir = pathlib.Path(temp_dir_name)
+            util.resolve_build_dir(
+                platform_ec_dir=temp_dir / 'x',
+                project_dir=temp_dir / 'x' / 'y' / 'z',
+                build_dir=None)
+            pytest.fail()
+    except Exception:
+        pass
+
+
+def test_resolve_build_dir_from_project():
+    """Since the build dir is not a configured build directory but instead a
+    project directory, it should be ignored.
+    """
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        temp_dir = pathlib.Path(temp_dir_name)
+        project_dir = temp_dir / 'x' / 'y' / 'z'
+        project_dir.mkdir(parents=True)
+        (project_dir / 'zmake.yaml').touch()
+        # Test when project_dir == build_dir.
+        build_dir = util.resolve_build_dir(
+            platform_ec_dir=temp_dir / 'x',
+            project_dir=project_dir,
+            build_dir=project_dir)
+        assert build_dir == temp_dir / 'x' / 'build' / 'y' / 'z'
+
+        # Test when build_dir is None (it should be ignored).
+        build_dir = util.resolve_build_dir(
+            platform_ec_dir=temp_dir / 'x',
+            project_dir=project_dir,
+            build_dir=None)
+        assert build_dir == temp_dir / 'x' / 'build' / 'y' / 'z'
