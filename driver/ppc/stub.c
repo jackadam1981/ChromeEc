3e9167fe5244f9b3f1480ad244887e842e62f954
Scott Collyer <scollyer@google.com>
2021-02-01 20:41:52 -0800

    ppc: stub: Add vbus enable for C1 port
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I354c3f102d8151cb738f7c18ca03d5a7ce95c296

--- driver/ppc/stub.c
+++ driver/ppc/stub.c
@@ -53,7 +53,6 @@ static int ppc_stub_is_vbus_present(int port)
 	int vbus;
 
 	vbus = tcpm_check_vbus_level(port, VBUS_PRESENT);
-	CPRINTS("ppc_stub: vbus = %d", vbus);
 
 	return vbus;
 }
@@ -61,7 +60,13 @@ static int ppc_stub_is_vbus_present(int port)
 
 static int ppc_stub_vbus_source_enable(int port, int enable)
 {
-	return EC_SUCCESS;
+	int addr = 0x1A;
+	int offset = 0x21;
+
+	/* GPIO1 of PS8805 is used to turn on/off vbus for C1 */
+
+	return i2c_write8(tcpc_config[port].i2c_info.port, addr,
+			  offset, enable ? 0x20 : 0);
 }
 
 static int ppc_stub_set_vbus_src_current_limit(int port,
