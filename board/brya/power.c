da676f13dda507bfdcb93260dea84ec24eb1dd5d
Caveh Jalali <caveh@chromium.org>
2021-02-02 17:42:51 -0800

    brya: Stub out intel_x86_dsw_pwrok_pass_thru
    Brya uses a power sequencer chip that controls dsw_pwrok and the EC does
    not need to do anything with this signal. Simply provide a no-op
    implementation for intel_x86_dsw_pwrok_pass_thru().
    
    BRANCH=none
    BUG=b:173575131
    TEST=buildall passes
    
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Change-Id: I776ceec9f6fcd1697158fd3bca8736fb145a9418

--- board/brya/power.c
+++ board/brya/power.c
@@ -6,6 +6,15 @@
 #include "common.h"
 
 #include "power/icelake.h"
+#include "power/intel_x86.h"
+
+/*
+ * Brya uses a power sequencer chip to drive DSW_PWROK to the AP.
+ */
+__override void intel_x86_dsw_pwrok_pass_thru(void)
+{
+}
+
 
 const struct intel_x86_pwrok_signal pwrok_signal_assert_list[] = {
 };
