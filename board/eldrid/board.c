26b1dee5ed86572c756fdf5fd64c7af9f09c58e8
Terry Chen <terry_chen@wistron.corp-partner.google.com>
2021-02-03 15:35:40 +0800

    eldrid: set AC PROCHOT
    Update AC PROCHOT for 45w adapter trigger prochot assert and stop fan keep max spinning
    
    BUG=b:178869017
    BRANCH=firmware-volteer-13672.B-master
    TEST=make buildall
    TEST=test pass by EE team
    
    Change-Id: I739b42453e796c3cc04657adbf84e96dc26abeb5

--- board/eldrid/board.c
+++ board/eldrid/board.c
@@ -178,9 +178,9 @@ __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 	 * Set different AC_PROCHOT value when using different wattage ADT.
 	 */
 	if (max_ma * charge_mv == PD_MAX_POWER_MW * 1000)
-		isl9241_set_ac_prochot(0, 3072);
+		isl9241_set_ac_prochot(0, 3840);
 	else
-		isl9241_set_ac_prochot(0, 2816);
+		isl9241_set_ac_prochot(0, 3328);
 
 	/*
 	 * Follow OEM request to limit the input current to
