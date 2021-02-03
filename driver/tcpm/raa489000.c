278665bfa168d1b3415d3a00b14243ba1b830a99
YongBeum.Ha <ybha@samsung.corp-partner.google.com>
2021-02-03 13:51:27 +0900

    raa489000 : Add config for Vbus Current Target
    Add CONFIG_VBUS_CURRENT_TARGET
    
    BUG=b:178064507
    BRANCH=none
    TEST=make -j BOARD=sasuke
    
    Signed-off-by: YongBeum.Ha <ybha@samsung.corp-partner.google.com>
    Change-Id: I679c89673ebad0a5a9b4bdc3caa74d4e524f81df
    Signed-off-by: YongBeum.Ha <ybha@samsung.corp-partner.google.com>

--- driver/tcpm/raa489000.c
+++ driver/tcpm/raa489000.c
@@ -44,7 +44,11 @@ int raa489000_set_output_current(int port, enum tcpc_rp_value rp)
 	int regval;
 	int selected_cur = rp == TYPEC_RP_3A0 ?
 				RAA489000_VBUS_CURRENT_TARGET_3A :
+#ifdef CONFIG_RAA489000_VBUS_CURRENT_TARGET
+				CONFIG_RAA489000_VBUS_CURRENT_TARGET;
+#else
 				RAA489000_VBUS_CURRENT_TARGET_1_5A;
+#endif
 
 	regval = AC_CURRENT_TO_REG(selected_cur) +
 				selected_cur % (DEFAULT_R_AC/R_AC);
