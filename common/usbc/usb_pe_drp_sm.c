1864169ce3718f26ef758fe605c54e77f56854ce
Abe Levkoy <alevkoy@chromium.org>
2021-02-04 12:17:20 -0700

    TCPMv2: Reset stored PD revs at appropriate times
    Stored PD revs for partners should only be reset upon Detach, Hard
    Reset, or Error Recovery. Don't do it when the PE enters a Startup
    state, e.g. after a Power Role Swap.
    
    BUG=b:173725284,b:179198412
    TEST=make buildall
    TEST=PR Swap after SOP' Discover Identity with PD 2.0 cable; subsequent
         SOP' messages still PD 2.0; subsequent Enter_USB message has
         correct cable speed
    BRANCH=none
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: I2c12287c99154fba96e4f04a76ebc48273b0bb2e

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -1991,7 +1991,7 @@ static void pe_src_startup_entry(int port)
 	pe[port].caps_counter = 0;
 
 	/* Reset the protocol layer */
-	prl_reset(port);
+	prl_reset_soft(port);
 
 	/*
 	 * Protocol layer reset clears the message IDs for all SOP types.
@@ -2873,7 +2873,7 @@ static void pe_snk_startup_entry(int port)
 	print_current_state(port);
 
 	/* Reset the protocol layer */
-	prl_reset(port);
+	prl_reset_soft(port);
 
 	/*
 	 * Protocol layer reset clears the message IDs for all SOP types.
