9894a7d7493e8a0014d178265241bbef0233cc67
Paul Fagerburg <pfagerburg@google.com>
2021-02-05 14:19:36 -0700

    firmware_builder: add --code-coverage option
    Added an option flag --code-coverage that will skip the build for EC
    hardware targets and build the tests with code coverage enabled.
    
    BUG=b:156895937
    BRANCH=None
    TEST=Run firmware_builder.py with --code-coverage option. The 'build'
    command will do nothing, and the 'test' command will build the host-
    based unit test with code coverage.
    
    Signed-off-by: Paul Fagerburg <pfagerburg@google.com>
    Change-Id: Ie3786f574e490a5ba6806e518401842bda609e5f

--- firmware_builder.py
+++ firmware_builder.py
@@ -25,6 +25,10 @@ DEFAULT_BUNDLE_METADATA_FILE = '/tmp/artifact_bundle_metadata'
 
 def build(opts):
     """Builds all EC firmware targets"""
+    # For unit test code coverage, build is a no-op.
+    if opts.code_coverage:
+        return
+
     # TODO(b/169178847): Add appropriate metric information
     metrics = firmware_pb2.FwBuildMetricList()
     with open(opts.metrics, 'w') as f:
@@ -64,8 +68,14 @@ def test(opts):
     with open(opts.metrics, 'w') as f:
         f.write(json_format.MessageToJson(metrics))
 
-    # Verify all posix-based unit tests build and pass
-    subprocess.run(['make', 'runtests', '-j{}'.format(opts.cpus)],
+    # If building for code coverage, build the 'coverage' target, which
+    # build the posix-based unit tests for code coverage and assembles
+    # the LCOV information.
+    #
+    # Otherwise build the 'runtests' target, which verifies all
+    # posix-based unit tests build and pass.
+    target = 'coverage' if opts.code_coverage else 'runtests'
+    subprocess.run(['make', target, '-j{}'.format(opts.cpus)],
                    cwd=os.path.dirname(__file__),
                    check=True)
 
@@ -123,6 +133,13 @@ def parse_args(args):
         required=False,
         help=
         'Full pathanme for the directory in which to bundle build artifacts.',
+    )
+
+    parser.add_argument(
+        '--code-coverage',
+        required=False,
+        action='store_true',
+        help='Build host-based unit tests for code coverage.',
     )
 
     # Would make this required=True, but not available until 3.7
