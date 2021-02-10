fcb520c9e8c097c35a9e7fec38245859ea5b9fcf
Yuval Peress <peress@chromium.org>
2021-02-09 23:54:48 -0700

    zephyr: fix missing CONFIG_ prefix for external storage
    BRANCH=none
    BUG=none
    TEST=none
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I8013eba02545f96406399b8f4966e92bed1e9e9a

---
+++ zephyr/include/drivers/cros_clock.h
@@ -0,0 +1,13 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_CROS_CLOCK_H
+#define ZEPHYR_INCLUDE_DRIVERS_CROS_CLOCK_H
+
+#include <device.h>
+
+typedef int cros_clock_
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_CROS_CLOCK_H */
