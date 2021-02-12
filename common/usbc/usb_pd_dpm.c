939518e5c5a599a047ecdaa925783dca26d3d6b2
Scott Collyer <scollyer@google.com>
2021-02-12 01:01:54 -0800

    DPM: Add function to request sending ATTENTION SVDM
    This CL adds a new DPM function to generate a request to the PE to
    send a DP attention SVMD message.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I9f8817cb8d40f32b878abdc09db605097a32548b

--- common/usbc/usb_pd_dpm.c
+++ common/usbc/usb_pd_dpm.c
@@ -29,8 +29,13 @@
 #define CPRINTS(format, args...)
 #endif
 
+#define DPM_ATTEN_MAX_VDO 2
+
 static struct {
 	uint32_t flags;
+	/* Max Attention length is header + 1 VDO */
+	uint32_t vdm_attention[DPM_ATTEN_MAX_VDO];
+	int vdm_cnt;
 } dpm[CONFIG_USB_PD_PORT_MAX_COUNT];
 
 #define DPM_SET_FLAG(port, flag) atomic_or(&dpm[(port)].flags, (flag))
@@ -43,6 +48,22 @@ static struct {
 #define DPM_FLAG_ENTER_DP        BIT(2)
 #define DPM_FLAG_ENTER_TBT       BIT(3)
 #define DPM_FLAG_ENTER_USB4      BIT(4)
+#define DPM_FLAG_SEND_ATTEN      BIT(5)
+
+enum ec_status pd_request_vdm_atten(int port, const uint32_t *data, int count)
+{
+	if (count > DPM_ATTEN_MAX_VDO)
+		return EC_RES_INVALID_PARAM;
+
+	/* Save contents of Attention message */
+	memcpy(dpm[port].vdm_attention, data, count << 2);
+	dpm[port].vdm_cnt = count;
+
+	/* Indicate to DPM that an ATTENTION msg needs to be sent */
+	DPM_SET_FLAG(port, DPM_FLAG_SEND_ATTEN);
+
+	return EC_SUCCESS;
+}
 
 enum ec_status pd_request_enter_mode(int port, enum typec_mode mode)
 {
@@ -339,12 +360,26 @@ static void dpm_attempt_mode_exit(int port)
 	pd_dpm_request(port, DPM_REQUEST_VDM);
 }
 
+static void dpm_send_attention_vdm(int port)
+{
+	DPM_CLR_FLAG(port, DPM_FLAG_SEND_ATTEN);
+
+	/* Set up VDM ATTEN msg that was passed in previously */
+	pd_setup_vdm_request(port, TCPC_TX_SOP, dpm[port].vdm_attention,
+			     dpm[port].vdm_cnt);
+	/* Trigger PE to start a VDM command run */
+	pd_dpm_request(port, DPM_REQUEST_VDM);
+}
+
 void dpm_run(int port)
 {
-	if (DPM_CHK_FLAG(port, DPM_FLAG_EXIT_REQUEST))
+	if (DPM_CHK_FLAG(port, DPM_FLAG_SEND_ATTEN)) {
+		dpm_send_attention_vdm(port);
+	} else if (DPM_CHK_FLAG(port, DPM_FLAG_EXIT_REQUEST)) {
 		dpm_attempt_mode_exit(port);
-	else if (!DPM_CHK_FLAG(port, DPM_FLAG_MODE_ENTRY_DONE))
+	} else if (!DPM_CHK_FLAG(port, DPM_FLAG_MODE_ENTRY_DONE)) {
 		dpm_attempt_mode_entry(port);
+	}
 }
 
 /*
