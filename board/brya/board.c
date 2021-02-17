13c934a8a6290e861ef7114cda391fe93a09adea
Caveh Jalali <caveh@chromium.org>
2021-02-17 01:23:11 -0800

    Brya: Enable charger chip
    This adds the charger chip support code.
    
    BRANCH=none
    BUG=b:173575131,b:2700316,b:180784200
    TEST=with reset of CQ-Depend patches, brya boots to AP
    
    Change-Id: I1b220376cb03197e13aad65f1e79692eaf8fb1e1
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- board/brya/board.c
+++ board/brya/board.c
@@ -6,6 +6,8 @@
 #include "common.h"
 
 #include "cbi_ec_fw_config.h"
+#include "charge_ramp.h"
+#include "charger.h"
 #include "power_button.h"
 #include "power.h"
 #include "switch.h"
@@ -43,3 +45,46 @@ __override void board_cbi_init(void)
 void button_interrupt(enum gpio_signal signal)
 {
 }
+
+#ifdef CONFIG_CHARGE_RAMP_SW
+
+/*
+ * TODO(b/181508008): tune this threshold
+ */
+
+#define BC12_MIN_VOLTAGE 4400
+
+/**
+ * Return true if VBUS is too low
+ */
+int board_is_vbus_too_low(int port, enum chg_ramp_vbus_state ramp_state)
+{
+	int voltage;
+
+	if (charger_get_vbus_voltage(port, &voltage))
+		voltage = 0;
+
+	CPRINTS("%s: charger reports VBUS %d on port %d", __func__,
+		voltage, port);
+
+	if (voltage == 0) {
+		CPRINTS("%s: must be disconnected", __func__);
+		return 1;
+	}
+
+	if (voltage < BC12_MIN_VOLTAGE) {
+		CPRINTS("%s: lower than %d", __func__,
+			BC12_MIN_VOLTAGE);
+		return 1;
+	}
+
+	return 0;
+
+	/*
+	 * For legacy BC1.2 charging with CONFIG_CHARGE_RAMP_SW, ramp up input
+	 * current until voltage drops to the minimum input voltage of the
+	 * charger, 4.096V.
+	 */
+	/* return voltage < ISL9241_BC12_MIN_VOLTAGE; */
+}
+#endif /* CONFIG_CHARGE_RAMP_SW */
