77a847d66d3edf9826a1afef62d98722e5bef1e4
Denis Brockus <dbrockus@google.com>
2021-02-18 13:13:19 -0700

    TCPMv2: PD Timers - Add PE SwapSourceStart to framework
    BUG=b:178029034
    BRANCH=none
    TEST=make runtests
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: I7d6e5e12e735029d0efb183f0ba6a1abc8f61e6d

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -624,14 +624,6 @@ static struct policy_engine {
 	 */
 	uint64_t pr_swap_wait_timer;
 
-	/*
-	 * This timer is used by the new Source, after a Power Role Swap or
-	 * Fast Role Swap, to ensure that it does not send Source_Capabilities
-	 * Message before the new Sink is ready to receive the
-	 * Source_Capabilities Message.
-	 */
-	uint64_t swap_source_start_timer;
-
 	/* Counters */
 
 	/*
@@ -2095,8 +2087,8 @@ static void pe_src_startup_entry(int port)
 		pd_dpm_request(port, DPM_REQUEST_SOP_PRIME_SOFT_RESET_SEND);
 
 		/* Start SwapSourceStartTimer */
-		pe[port].swap_source_start_timer = get_time().val +
-			PD_T_SWAP_SOURCE_START;
+		pd_timer_active(port, PE_TIMER_SWAP_SOURCE_START,
+				PD_T_SWAP_SOURCE_START);
 
 		/*
 		 * Evaluate port's sink caps for preferred current, if
@@ -2118,7 +2110,7 @@ static void pe_src_startup_entry(int port)
 		 * We can't use set_state_pe here, since we need to ensure that
 		 * the protocol layer is running again (done in run function).
 		 */
-		pe[port].swap_source_start_timer = get_time().val;
+		pd_timer_inactive(port, PE_TIMER_SWAP_SOURCE_START);
 
 		/*
 		 * Set DiscoverIdentityTimer to trigger when we enter
@@ -2146,12 +2138,24 @@ static void pe_src_startup_entry(int port)
 
 static void pe_src_startup_run(int port)
 {
+	/*
+	 * Convert active/expired to be inactive/expired, if applicable, to
+	 * not speed up timer tick in the possible case the timer expired
+	 * but we leave the state before handling it.
+	 */
+	pd_timer_expired_to_inactive(port, PE_TIMER_SWAP_SOURCE_START);
+
 	/* Wait until protocol layer is running */
 	if (!prl_is_running(port))
 		return;
 
-	if (get_time().val > pe[port].swap_source_start_timer)
+	if (pd_timer_is_expired(port, PE_TIMER_SWAP_SOURCE_START))
 		set_state_pe(port, PE_SRC_SEND_CAPABILITIES);
+}
+
+static void pe_src_startup_exit(int port)
+{
+	pd_timer_disable(port, PE_TIMER_SWAP_SOURCE_START);
 }
 
 /**
@@ -6865,6 +6869,7 @@ static __const_data const struct usb_state pe_states[] = {
 	[PE_SRC_STARTUP] = {
 		.entry = pe_src_startup_entry,
 		.run   = pe_src_startup_run,
+		.exit  = pe_src_startup_exit,
 	},
 	[PE_SRC_DISCOVERY] = {
 		.entry = pe_src_discovery_entry,
