a432e354e87916ece93282f83b2866edb0b07c14
Caveh Jalali <caveh@chromium.org>
2021-02-19 02:31:24 -0800

    brya: Add CBI and FW_CONFIG support
    This adds support for CBI and the first USB DB type field in FW_CONFIG.
    
    BUG=b:173575131,b:180434685
    BRANCH=none
    TEST=buildall succeeds
    
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Change-Id: If1d27fc100db9b814f90a9378d8dd19530a92bf4

--- baseboard/brya/baseboard.h
+++ baseboard/brya/baseboard.h
@@ -89,10 +89,13 @@
 #include "baseboard_usbc_config.h"
 #include "extpower.h"
 
-enum ec_cfg_usb_db_type {
-	DB_USB_ABSENT = 0,
-	DB_USB3_PS8815 = 1,
-};
+/**
+ * Configure run-time data structures and operation based on CBI data. This
+ * typically includes customization for changes in the BOARD_VERSION and
+ * FW_CONFIG fields in CBI. This routine is called from the baseboard after
+ * the CBI data has been initialized.
+ */
+__override_proto void board_cbi_init(void);
 
 /*
  * Check battery disconnect state.
@@ -100,6 +103,11 @@ enum ec_cfg_usb_db_type {
  * @return true - initialized. false - not.
  */
 __override_proto bool board_battery_is_initialized(void);
+
+/*
+ * Return the board revision number.
+ */
+uint8_t get_board_id(void);
 
 #endif /* !__ASSEMBLER__ */
 
--- baseboard/brya/build.mk
+++ baseboard/brya/build.mk
@@ -8,4 +8,6 @@
 
 baseboard-y=
 baseboard-y+=battery_presence.o
+baseboard-y+=cbi_ec_fw_config.o
+baseboard-y+=cbi.o
 baseboard-y+=usb_pd_policy.o
