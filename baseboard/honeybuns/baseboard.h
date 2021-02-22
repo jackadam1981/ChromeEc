fd28ac9bbf9ce91bb531b76b1c956bde3737c0ee
Scott Collyer <scollyer@google.com>
2021-02-22 08:30:06 -0800

    honeybuns: Add power button support
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I8519c072a7f10657c369344ead6149fc7d31bb36

--- baseboard/honeybuns/baseboard.h
+++ baseboard/honeybuns/baseboard.h
@@ -226,6 +226,7 @@ enum adc_channel {
 
 extern const struct power_seq board_power_seq[];
 extern const size_t board_power_seq_count;
+void baseboard_power_button_evt(int level);
 
 int baseboard_usbc_init(int port);
 int baseboard_ppc_enable_sink_path(int port);
