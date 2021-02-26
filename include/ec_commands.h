65029bebb6ed720c98777954968c74533b59d668
Wealian Liao <whliao@nuvoton.corp-partner.google.com>
2021-02-26 10:16:11 +0800

    zephyr: npcx: add reset cause check
    This CL include the following:
    1. Add check_reset_cause() which sets the system reset flag.
    2. Add chip_bbram_status_check() to clear the error status. For some
    board has CONFIG_BOARD_RESET_AFTER_POWER_ON feature, EC will be reset
    when power-up. Due to the longer init time(different initialize
    function & different code size which need more time to move code to
    ram), EC is reset before bbram_npcx_init() invoke. Add this for the
    BBRAM error handling, currently just clear status bits & show the error
    message.
    3. Add CONFIG_BOARD_RESET_AFTER_POWER_ON. EC_RESET_FLAG_INITIAL_PWR
    also couldn't be set before H1 reset EC. Add another flag to
    distinguish the first reset-pin reset.
    
    BRANCH=none
    BUG=b:178101173
    TEST=check the following reset cause
    1. power-up
    2. reset-pin reset
    3. soft by 'reboot' console command
    4. watchdog by 'waitms 2000'
    
    Signed-off-by: Wealian Liao <whliao@nuvoton.corp-partner.google.com>
    Change-Id: I515868d8cda4544fdbe782210b0108b4dda0d8cc

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -5960,6 +5960,12 @@ struct ec_params_set_cbi {
 #define EC_RESET_FLAG_EFS         BIT(20)  /* Jumped to this image by EFS */
 #define EC_RESET_FLAG_AP_IDLE     BIT(21)  /* Leave alone AP */
 #define EC_RESET_FLAG_INITIAL_PWR BIT(22)  /* EC had power, then was reset */
+#define EC_RESET_FLAG_HAD_RESET_PIN\
+				  BIT(23)  /* EC had at least 1 reset-pin reset.
+					    * This is used to check the first
+					    * reset-pin reset for
+					    * CONFIG_BOARD_RESET_AFTER_POWER_ON.
+					    */
 
 /*
  * Reason codes used by the AP after a shutdown to figure out why it was reset
