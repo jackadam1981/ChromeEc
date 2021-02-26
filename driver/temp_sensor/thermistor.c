5f308bef751a6cdd37f7c872f00b1cb1ec17fcb6
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2021-02-26 16:50:54 +0800

    lantis: thermal test
    Print temperature value of thermistor for test.
    
    BUG=None
    BRANCH=dedede
    TEST=make -j buildall
    
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Change-Id: Ieb93c8b95db2cb145da3048ac3d759bdb1a7de82

--- driver/temp_sensor/thermistor.c
+++ driver/temp_sensor/thermistor.c
@@ -86,6 +86,7 @@ static int thermistor_get_temperature(int idx_adc, int *temp_ptr,
 		return EC_ERROR_UNKNOWN;
 
 	*temp_ptr = thermistor_linear_interpolate(mv, info);
+	ccprints("Thermistor%d : %d", idx_adc, *temp_ptr);
 	*temp_ptr = C_TO_K(*temp_ptr);
 	return EC_SUCCESS;
 }
