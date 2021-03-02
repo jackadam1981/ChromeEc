67fca20c70a332de8f70ca38862ae0b99085c175
Caveh Jalali <caveh@chromium.org>
2021-03-01 19:19:15 -0800

    brya: Add wake source pins
    This adds the wake source pin definitions needed by the NPCX9 chip
    support code.
    
    BRANCH=none
    BUG=b:173575131
    TEST=with reset of CQ-Depend patches, byr can charge using PD
    
    Change-Id: If4cca6d1e20ddc3c422697e6838c9df0ddd8cb15
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- board/brya/board.c
+++ board/brya/board.c
@@ -19,15 +19,6 @@
 #define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
 
-/* Wake up pins */
-const enum gpio_signal hibernate_wake_pins[] = {
-	GPIO_ACOK_EC_OD,
-	GPIO_EC_RST_ODL,
-	GPIO_GSC_EC_PWR_BTN_ODL,
-	GPIO_LID_OPEN_OD,
-};
-const int hibernate_wake_pins_used = ARRAY_SIZE(hibernate_wake_pins);
-
 /******************************************************************************/
 /* USB-A charging control */
 
