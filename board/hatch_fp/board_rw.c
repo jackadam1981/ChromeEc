ba37dc7e2a803b9aeeccd066d3afa39e3be45758
Yicheng Li <yichengli@chromium.org>
2021-03-02 15:38:17 -0800

    fpsensor: Support building firmware that works for both sensors
    This is a refactoring to allow building FPMCU firmware that works for
    one FPC sensor and one ELAN sensor.
    
    1. When both drivers implement our common functions, e.g. fp_sensor_init(),
       rename them to fp_sensor_init_fpc() and fp_sensor_init_elan().
    2. There are a few functions implemented not in FPC driver but in FPC
       private library, e.g. fp_sensor_finger_status(). I kept this as-is for
       FPC but renamed the one in ELAN driver to fp_sensor_finger_status_elan()
    3. If building for ELAN, need to hardcode elan=1 in hatch_fp/board.c
       because the sensor type GPIO always says FPC.
    
    BRANCH=none
    BUG=b:175158241
    TEST=make run-fpsensor; make run-fpsensor_status;
         make run-fpsensor_crypto
    TEST=make -j BOARD=dartmonkey
    TEST=add CONFIG_FP_SENSOR_ELAN515 to board/hatch_fp/board.h;
         make -j BOARD=bloonchipper
         Firmware binary fully works on Dragonair (FPC) and Voema (ELAN)
    
    Cq-Depend: chrome-internal:3654297
    Change-Id: I789090dbdfe35ac6aefd6a629fa4c7bde89dc437
    Signed-off-by: Yicheng Li <yichengli@chromium.org>

--- board/hatch_fp/board_rw.c
+++ board/hatch_fp/board_rw.c
@@ -5,7 +5,10 @@
 
 #include "common.h"
 #include "console.h"
+#include "fpsensor.h"
 #include "fpsensor_detect.h"
+#include "fpsensor_elan.h"
+#include "fpsensor_fpc.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "registers.h"
@@ -76,6 +79,14 @@ static void configure_fp_sensor_spi(void)
 void board_init_rw(void)
 {
 	enum fp_transport_type ret_transport = get_fp_transport_type();
+
+	sensor_type = get_fp_sensor_type();
+	if (sensor_type == FP_SENSOR_TYPE_ELAN)
+		fp_driver = &fp_driver_elan;
+	else if (sensor_type == FP_SENSOR_TYPE_FPC)
+		fp_driver = &fp_driver_fpc;
+	else
+		ccprints("Failed to get sensor type!");
 
 	if (ret_transport == FP_TRANSPORT_TYPE_UART) {
 		/*
