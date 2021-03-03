ba39df8e279842de27cbea32d11b53f5cbabf755
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-03-03 10:17:10 +0800

    eldrid: change switching frequence
    BUG=b:
    BRANCH=firmware-volteer-13672.B-master
    TEST=make buildall
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I2bf32aeb12415655360c8e1684c8c78205a8e30e

--- board/eldrid/board.c
+++ board/eldrid/board.c
@@ -182,6 +182,12 @@ __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 	else
 		isl9241_set_ac_prochot(0, 3328);
 
+	if (max_ma * charge_mv == 45000 * 1000)
+		isl9241_set_switching_frequency(0,
+			ISL9241_CONTROL1_SWITCHING_FREQUENCY_1020kHz);
+	else if (max_ma * charge_mv >= PD_MAX_POWER_MW * 1000)
+		isl9241_set_switching_frequency(0,
+			ISL9241_CONTROL1_SWITCHING_FREQUENCY_724kHz);
 	/*
 	 * Follow OEM request to limit the input current to
 	 * 90% negotiated limit.
