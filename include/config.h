26a33cf523408fbb2b529f38c4b8ac45572c47e2
Eric Yilun Lin <yllin@chromium.org>
2021-03-05 14:01:47 +0800

    ppc/syv682x: support C version
    C version won't block I2C accessing to CONTROL4(to on/off Vconn) reg when
    smart discahrge enabled. This allows us to re-enable the smart discahrge on
    boards using SYV682C. This CL support the feature by adding:
    1. CONFIG_USBC_PPC_SYV682C
    2. CONFIG_USBC_PPC_SYV682X_SMART_DISCHARGE
    
    BUG=b:160548079
    TEST=Hayato meets tVconnOff, and tVbusDischarge
    BRANCH=main
    
    Change-Id: I89b57b8c20907249d5d97140289fb0570bd58b46
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- include/config.h
+++ include/config.h
@@ -4531,6 +4531,7 @@
 #undef CONFIG_USBC_PPC_NX20P3481
 #undef CONFIG_USBC_PPC_NX20P3483
 #undef CONFIG_USBC_PPC_SN5S330
+#undef CONFIG_USBC_PPC_SYV682C
 #undef CONFIG_USBC_PPC_SYV682X
 
 /*
@@ -4541,6 +4542,9 @@
 
 /* SYV682 does not pass through CC, instead it bypasses to the TCPC */
 #undef CONFIG_SYV682X_NO_CC
+
+/* Define to enable SYV682X VBUS smart discharge. */
+#undef CONFIG_USBC_PPC_SYV682X_SMART_DISCHARGE
 
 /* PPC is capable of gating the SBU lines. */
 #undef CONFIG_USBC_PPC_SBU
@@ -5453,6 +5457,13 @@
 #define CONFIG_USBC_PPC_POLARITY
 #define CONFIG_USBC_PPC_SBU
 #define CONFIG_USBC_PPC_VCONN
+#endif
+
+
+/*****************************************************************************/
+/* PPC SYV682C is a subset of SYV682X. */
+#if defined(CONFIG_USBC_PPC_SYV682C)
+#define CONFIG_USBC_PPC_SYV682X
 #endif
 
 /*
