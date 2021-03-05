d6e467584ca20f14faaf3b64e33338b4e05a4e67
reno.wang <reno.wang@lcfc.corp-partner.google.com>
2021-03-06 04:19:44 +0800

    Lindar: Lindar's lightbar support in S0ix/S3
    1. Lid is closed, lightbar keep off
    2. Lid is opened, its behavior follow below.
       AC+Battery < 20%, lightbar solid 2 amber led on.
       AC+Battery < 40%, lightbar solid 4 amber led on.
       AC+Battery < 60%, lightbar solid 6 amber led on.
       AC+Battery < 80%, lightbar solid 8 amber led on.
       AC+Battery < 97%, lightbar solid 10 amber led on.
       AC+Battery >= 97%, lightbar solid 10 green led on.
       Battery only >= 20%, lightbar keep off.
       Battery low < 20%, lightbar blink amber color, 1s on, 5s off.
    
    BUG=b:174133147
    BRANCH=volteer
    TEST=make buildall, test lightbar behavior in S0ix/S3.
    
    Signed-off-by: reno.wang <reno.wang@lcfc.corp-partner.google.com>
    Change-Id: Ibbdc17627f7b2d1b2abbbad23b6c06024455e66b

--- board/lindar/led.c
+++ board/lindar/led.c
@@ -9,6 +9,16 @@
 #include "led_onoff_states.h"
 #include "led_common.h"
 #include "gpio.h"
+#include "timer.h"
+#include "task.h"
+#include "stdbool.h"
+#include "i2c.h"
+#include "hooks.h"
+#include "charge_state.h"
+#include "lid_switch.h"
+#include "extpower.h"
+#define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
 
 #define LED_OFF_LVL	1
 #define LED_ON_LVL	0
@@ -109,3 +119,307 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 
 	return EC_SUCCESS;
 }
+
+static const uint16_t lightbar_i2c_addr = 0x68;
+static void controller_write(uint8_t reg, uint8_t val)
+{
+	uint8_t buf[2];
+
+	buf[0] = reg;
+	buf[1] = val;
+
+	i2c_xfer_unlocked(I2C_PORT_LIGHTBAR, lightbar_i2c_addr,
+			buf, 2, 0, 0,
+			I2C_XFER_SINGLE);
+}
+
+enum lightbar_states {
+	LB_STATE_OFF,
+	LB_STATE_LID_CLOSE,
+	LB_STATE_AC_ONLY,
+	LB_STATE_AC_BAT_LOW,
+	LB_STATE_AC_BAT_20,
+	LB_STATE_AC_BAT_40,
+	LB_STATE_AC_BAT_60,
+	LB_STATE_AC_BAT_80,
+	LB_STATE_AC_BAT_100,
+	LB_STATE_BAT_LOW,
+	LB_STATE_BAT_ONLY,
+	LB_NUM_STATES
+};
+
+/*
+ * All lightbar states should have one phase defined,
+ * and an additional phase can be defined for blinking
+ */
+enum lightbar_phase {
+	LIGHTBAR_PHASE_0 = 0,
+	LIGHTBAR_PHASE_1 = 1,
+	LIGHTBAR_NUM_PHASES
+};
+
+enum ec_lightbar_colors {
+	BAR_OFF             = 0x00,
+	BAR_COLOR_ORG_2     = 0x01,
+	BAR_COLOR_ORG_4     = 0x02,
+	BAR_COLOR_ORG_6     = 0x03,
+	BAR_COLOR_ORG_8     = 0x04,
+	BAR_COLOR_ORG_FULL  = 0x05,
+	BAR_COLOR_GREEN     = 0x06,
+	LIGHTBAR_COLOR_TOTAL
+};
+
+struct lightbar_descriptor {
+	enum ec_lightbar_colors color;
+	uint8_t time;
+};
+
+#define BAR_INFINITE      UINT8_MAX
+#define LIGHTBAR_ONE_SEC  (1000 / HOOK_TICK_INTERVAL_MS)
+const struct lightbar_descriptor
+	lb_table[LB_NUM_STATES][LIGHTBAR_NUM_PHASES] = {
+	[LB_STATE_OFF]         = {{BAR_OFF, BAR_INFINITE} },
+	[LB_STATE_LID_CLOSE]   = {{BAR_OFF, BAR_INFINITE} },
+	[LB_STATE_AC_ONLY]     = {{BAR_OFF, BAR_INFINITE} },
+	[LB_STATE_AC_BAT_LOW]  = {{BAR_COLOR_ORG_2, BAR_INFINITE} },
+	[LB_STATE_AC_BAT_20]   = {{BAR_COLOR_ORG_4, BAR_INFINITE} },
+	[LB_STATE_AC_BAT_40]   = {{BAR_COLOR_ORG_6, BAR_INFINITE} },
+	[LB_STATE_AC_BAT_60]   = {{BAR_COLOR_ORG_8, BAR_INFINITE} },
+	[LB_STATE_AC_BAT_80]   = {{BAR_COLOR_ORG_FULL, BAR_INFINITE} },
+	[LB_STATE_AC_BAT_100] = {{BAR_COLOR_GREEN, BAR_INFINITE} },
+	[LB_STATE_BAT_LOW]     = {{BAR_OFF, 5*LIGHTBAR_ONE_SEC},
+				{BAR_COLOR_ORG_FULL, LIGHTBAR_ONE_SEC} },
+	[LB_STATE_BAT_ONLY]    = {{BAR_OFF, BAR_INFINITE} },
+};
+
+static void lightbar_init(void)
+{
+		i2c_lock(I2C_PORT_LIGHTBAR, 1);
+		controller_write(0x02, 0x00);
+		controller_write(0x03, 0x00);
+		controller_write(0x04, 0x00);
+		controller_write(0x05, 0x00);
+		controller_write(0x06, 0x00);
+		controller_write(0x07, 0x00);
+		controller_write(0x08, 0x00);
+
+		i2c_lock(I2C_PORT_LIGHTBAR, 0);
+}
+
+/*	From EE's information, lindar only support two colors lightbar,
+ *  Orange (Amber) and Green. And they connect KTD20xx's red color
+ *  channel to orange color led, and green color
+ *  channel to green color led.
+ *  Blue color channel is unused.
+ *
+ *	Reg0x02: Control Configuration
+ *	         BIT7:6 is EN_MODE[1:0]
+ *	         00 = global off, 01 = Night mode,
+ *           10 = Normal mode, 11 = reset as default
+ *
+ *	Reg0x03: RED0 color's current setting
+ *	Reg0x04: GREEN0 color's current setting
+ *	Reg0x05: BLUE0 color's current setting
+ *	Reg0x06: RED1 color's current setting
+ *	Reg0x07: GREEN1 color's current setting
+ *	Reg0x08: BLUE1 color's current setting
+ *	Reg0x09: ISELA12 Selection Configuration
+ *	Reg0x0A: ISELA34 Selection Configuration
+ *	Reg0x0B: ISELB12 Selection Configuration
+ *	Reg0x0C: ISELB34 Selection Configuration
+ *	Reg0x0D: ISELC12 Selection Configuration
+ */
+static void lightbar_set_color(enum ec_lightbar_colors color)
+{
+	switch (color) {
+	case BAR_COLOR_ORG_2:
+		i2c_lock(I2C_PORT_LIGHTBAR, 1);
+		controller_write(0x03, 0x02);
+		controller_write(0x04, 0x00);
+		controller_write(0x05, 0x00);
+		controller_write(0x06, 0x00);
+		controller_write(0x07, 0x00);
+		controller_write(0x08, 0x00);
+
+		controller_write(0x09, 0x88);
+		controller_write(0x0A, 0x00);
+		controller_write(0x0B, 0x00);
+		controller_write(0x0C, 0x00);
+		controller_write(0x0D, 0x00);
+		controller_write(0x02, 0x80);
+		i2c_lock(I2C_PORT_LIGHTBAR, 0);
+		break;
+	case BAR_COLOR_ORG_4:
+		i2c_lock(I2C_PORT_LIGHTBAR, 1);
+		controller_write(0x03, 0x02);
+		controller_write(0x04, 0x00);
+		controller_write(0x05, 0x00);
+		controller_write(0x06, 0x00);
+		controller_write(0x07, 0x00);
+		controller_write(0x08, 0x00);
+
+		controller_write(0x09, 0x88);
+		controller_write(0x0A, 0x88);
+		controller_write(0x0B, 0x00);
+		controller_write(0x0C, 0x00);
+		controller_write(0x0D, 0x00);
+		controller_write(0x02, 0x80);
+		i2c_lock(I2C_PORT_LIGHTBAR, 0);
+		break;
+	case BAR_COLOR_ORG_6:
+		i2c_lock(I2C_PORT_LIGHTBAR, 1);
+		controller_write(0x03, 0x02);
+		controller_write(0x04, 0x00);
+		controller_write(0x05, 0x00);
+		controller_write(0x06, 0x00);
+		controller_write(0x07, 0x00);
+		controller_write(0x08, 0x00);
+
+		controller_write(0x09, 0x88);
+		controller_write(0x0A, 0x88);
+		controller_write(0x0B, 0x88);
+		controller_write(0x0C, 0x00);
+		controller_write(0x0D, 0x00);
+		controller_write(0x02, 0x80);
+		i2c_lock(I2C_PORT_LIGHTBAR, 0);
+		break;
+	case BAR_COLOR_ORG_8:
+		i2c_lock(I2C_PORT_LIGHTBAR, 1);
+		controller_write(0x03, 0x02);
+		controller_write(0x04, 0x00);
+		controller_write(0x05, 0x00);
+		controller_write(0x06, 0x00);
+		controller_write(0x07, 0x00);
+		controller_write(0x08, 0x00);
+
+		controller_write(0x09, 0x88);
+		controller_write(0x0A, 0x88);
+		controller_write(0x0B, 0x88);
+		controller_write(0x0C, 0x88);
+		controller_write(0x0D, 0x00);
+		controller_write(0x02, 0x80);
+		i2c_lock(I2C_PORT_LIGHTBAR, 0);
+		break;
+	case BAR_COLOR_ORG_FULL:
+		i2c_lock(I2C_PORT_LIGHTBAR, 1);
+		controller_write(0x03, 0x02);
+		controller_write(0x04, 0x00);
+		controller_write(0x05, 0x00);
+		controller_write(0x06, 0x00);
+		controller_write(0x07, 0x00);
+		controller_write(0x08, 0x00);
+
+		controller_write(0x09, 0x88);
+		controller_write(0x0A, 0x88);
+		controller_write(0x0B, 0x88);
+		controller_write(0x0C, 0x88);
+		controller_write(0x0D, 0x88);
+		controller_write(0x02, 0x80);
+		i2c_lock(I2C_PORT_LIGHTBAR, 0);
+		break;
+	case BAR_COLOR_GREEN:
+		i2c_lock(I2C_PORT_LIGHTBAR, 1);
+		controller_write(0x03, 0x00);
+		controller_write(0x04, 0x02);
+		controller_write(0x05, 0x00);
+		controller_write(0x06, 0x00);
+		controller_write(0x07, 0x00);
+		controller_write(0x08, 0x00);
+
+		controller_write(0x09, 0x88);
+		controller_write(0x0A, 0x88);
+		controller_write(0x0B, 0x88);
+		controller_write(0x0C, 0x88);
+		controller_write(0x0D, 0x88);
+		controller_write(0x02, 0x80);
+		i2c_lock(I2C_PORT_LIGHTBAR, 0);
+		break;
+	case BAR_OFF:
+	default: /* BAR_OFF and other unsupported colors */
+		i2c_lock(I2C_PORT_LIGHTBAR, 1);
+		controller_write(0x02, 0x00);
+		i2c_lock(I2C_PORT_LIGHTBAR, 0);
+		break;
+	}
+}
+
+const int lightbar_bat_low = 20;
+
+static enum lightbar_states lightbar_get_state(void)
+{
+	enum lightbar_states new_state = LB_NUM_STATES;
+
+	if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
+		if (!lid_is_open())
+			new_state = LB_STATE_LID_CLOSE;
+		else {
+			if (extpower_is_present()) {
+				if (battery_is_present()) {
+					if (charge_get_percent() < 20)
+						new_state = LB_STATE_AC_BAT_LOW;
+					else if (charge_get_percent() < 40)
+						new_state = LB_STATE_AC_BAT_20;
+					else if (charge_get_percent() < 60)
+						new_state = LB_STATE_AC_BAT_40;
+					else if (charge_get_percent() < 80)
+						new_state = LB_STATE_AC_BAT_60;
+					else if (charge_get_percent() < 97)
+						new_state = LB_STATE_AC_BAT_80;
+					else
+						new_state = LB_STATE_AC_BAT_100;
+				} else
+					new_state = LB_STATE_AC_ONLY;
+			} else {
+				if (charge_get_percent() < lightbar_bat_low)
+					new_state = LB_STATE_BAT_LOW;
+				else
+					new_state = LB_STATE_BAT_ONLY;
+			}
+		}
+	} else
+		new_state = LB_STATE_OFF;
+
+	return new_state;
+}
+
+static void lightbar_update(void)
+{
+	static uint8_t ticks, period;
+	static enum lightbar_states lb_cur_state = LB_NUM_STATES;
+	enum lightbar_states desired_state;
+	int phase;
+
+	desired_state = lightbar_get_state();
+	if (desired_state != lb_cur_state &&
+	    desired_state < LB_NUM_STATES) {
+		/* State is changing */
+		lb_cur_state = desired_state;
+		/* Reset ticks and period when state changes */
+		ticks = 0;
+
+		period = lb_table[lb_cur_state][LIGHTBAR_PHASE_0].time +
+			lb_table[lb_cur_state][LIGHTBAR_PHASE_1].time;
+	}
+
+	/* If this state is undefined, turn lightbar off */
+	if (period == 0) {
+		CPRINTS("Undefined lightbar behavior for lightbar state %d,"
+			"turning off lightbar", lb_cur_state);
+		lightbar_set_color(BAR_OFF);
+		return;
+	}
+
+	/*
+	 * Determine which phase of the state table to use. The phase is
+	 * determined if it falls within first phase time duration.
+	 */
+	phase = ticks < lb_table[lb_cur_state][LIGHTBAR_PHASE_0].time ?
+									0 : 1;
+	ticks = (ticks + 1) % period;
+
+	/* Set the color for the given state and phase */
+	lightbar_set_color(lb_table[lb_cur_state][phase].color);
+
+}
+DECLARE_HOOK(HOOK_INIT, lightbar_init, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_TICK, lightbar_update, HOOK_PRIO_DEFAULT);
