7a7322e27e2e634cfe16c0a1fec2284aea803a15
Furquan Shaikh <furquan@google.com>
2021-03-09 22:29:41 -0800

    brya: Enable switch support
    This change enables switch support for baseboard brya by defining
    CONFIG_SWITCH.
    
    BUG=b:173575131
    BRANCH=None
    TEST=make -j buildall
    
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Change-Id: Ie8400bbe174fc3662282534f4027513e8c44b220

--- board/brya/board.h
+++ board/brya/board.h
@@ -16,11 +16,7 @@
 /* Baseboard features */
 #include "baseboard.h"
 
-/*
- * Disable features enabled by default.
- */
 #undef CONFIG_HIBERNATE
-#undef CONFIG_SWITCH
 
 /* USB Type A Features */
 #define USB_PORT_COUNT			1
