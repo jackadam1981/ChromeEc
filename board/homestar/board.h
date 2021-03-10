14a2886857d83ef2376706053530b18b83ca8465
xuxinxiong <xuxinxiong@huaqin.corp-partner.google.com>
2021-03-10 19:07:29 +0800

    Homestar: Remove the DA9313 support, add the LN9310 support
    LN9310 and DA9313 are not pin compatible. The layout of
    Homestar can only choose one and it is LN9310.
    So remove the DA9313 support and add the LN9310's.
    
    BRANCH=Trogdor
    BUG=b:182358144
    TEST=Built homestar image
    
    Change-Id: I3c07c0f062a68f64a13a006aa7010c438dfe1b5e
    Signed-off-by: xuxinxiong <xuxinxiong@huaqin.corp-partner.google.com>

--- board/homestar/board.h
+++ board/homestar/board.h
@@ -32,6 +32,9 @@
 /* Internal SPI flash on NPCX7 */
 #define CONFIG_FLASH_SIZE_BYTES (512 * 1024)  /* 512KB internal spi flash */
 
+/* Switchcap */
+#define CONFIG_LN9310
+
 /* Battery */
 #define CONFIG_BATTERY_DEVICE_CHEMISTRY  "LION"
 #define CONFIG_BATTERY_REVIVE_DISCONNECT
@@ -66,6 +69,7 @@
 #define GPIO_PMIC_RESIN_L GPIO_PM845_RESIN_L
 /* TODO(Dolan): check which pin was used for tablet mode detect */
 #define GMR_TABLET_MODE_GPIO_L GPIO_LID_OPEN_EC
+#define GPIO_SWITCHCAP_PG_INT_L GPIO_LN9310_INT
 
 #ifndef __ASSEMBLER__
 
@@ -105,6 +109,7 @@ enum battery_type {
 void board_set_switchcap_power(int enable);
 int board_is_switchcap_enabled(void);
 int board_is_switchcap_power_good(void);
+enum battery_cell_type board_get_battery_cell_type(void);
 /* Custom function to indicate if sourcing VBUS */
 int board_is_sourcing_vbus(int port);
 /* Enable VBUS sink for a given port */
