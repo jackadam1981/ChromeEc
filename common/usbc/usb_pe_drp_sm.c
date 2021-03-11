ac97dcfbf1929584b1ce5f902ac74d43a14059f3
Scott Collyer <scollyer@google.com>
2021-03-10 21:43:46 -0800

    TCPMv2: Add support for disabling type-2 BIST mode
    This CL adds a TCPCI method to disable BIST mode for the case where
    the TCPC does not handle the duration automatically. This is required
    for the ucpd driver as once type-2 BIST mode is started, the ucpd
    peripheral needs to be reset to stop.
    
    This CL also reduces the timer value from 60 msec to 55 msec to ensure
    that there is enough time for BIST to be stopped and still be in spec.
    
    BUG=b:182436876
    BRANCH=None
    TEST=verifed on Quiche that when type-2 BIST is started then it stops
    when the timer in PE expires. This fixes TDA.2.1.1.1: BMC-PHY-TX-EYE
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I59ee7ac0dead09ac3a1b5783cfa3fe9db238eb7e

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -4927,6 +4927,8 @@ static void pe_bist_tx_run(int port)
 {
 	if (pd_timer_is_expired(port, PE_TIMER_BIST_CONT_MODE)) {
 
+		tcpm_reset_bist_type_2(port);
+
 		if (pe[port].power_role == PD_ROLE_SOURCE)
 			set_state_pe(port, PE_SRC_TRANSITION_TO_DEFAULT);
 		else
