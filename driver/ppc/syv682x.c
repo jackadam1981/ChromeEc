dd456e0a4299ee94b185022a9fcfa65b8637b008
Eric Yilun Lin <yllin@chromium.org>
2021-03-12 15:03:43 +0800

    TESTONLY: asurada: debug ppc
    Change-Id: I708dc1f634f996d256038bbbb6347217710d8690

--- driver/ppc/syv682x.c
+++ driver/ppc/syv682x.c
@@ -261,6 +261,7 @@ static void syv682x_handle_status_interrupt(int port, int regval)
 			atomic_or(&flags[port], SYV682X_FLAGS_SOURCE_ENABLED);
 			atomic_clear_bits(&flags[port],
 					  SYV682X_FLAGS_SINK_ENABLED);
+			CPRINTS("\033[31m%s(%d, 0x%x)\033[m", __func__, port, regval);
 			if (!IS_ENABLED(CONFIG_USB_PD_FRS_TCPC))
 				pd_got_frs_signal(port);
 		}
