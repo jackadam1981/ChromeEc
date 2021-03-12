1a03e40784e5e3bf0cbdade302b6fac7bd8cba9a
Andrey Pronin <apronin@google.com>
2021-03-12 15:43:18 -0800

    gsctool: fix error buffer size
    Before this CL possible errors were longer than the allocated buffer.
    
    BUG=b:182603755
    TEST=make all
    
    Signed-off-by: Andrey Pronin <apronin@google.com>
    Change-Id: I21890e72fc79096c9a0d67e8535673d0012dcb0a

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -1345,7 +1345,7 @@ static void process_erase_ap_ro_hash(struct transfer_descriptor *td)
 	uint32_t rv;
 	uint8_t response;
 	size_t response_size;
-	char error_details[30];
+	char error_details[70];
 
 	response_size = sizeof(response);
 	rv = send_vendor_command(td, VENDOR_CC_SEED_AP_RO_CHECK,
