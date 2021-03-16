384076c9a0b4b84455ef4fcba3bf52d4b8ad8783
Scott Collyer <scollyer@google.com>
2021-03-16 07:22:25 -0700

    honeybuns: Allow for board specific mf preference
    This CL enables a given board to have its own policy regarding the mf
    preference bit which is used by a UFP to signal its preferred pin
    configuration in the DP Status message.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Ied7cdb72d1e302aab25dd5531856b1c7c148e64f

--- baseboard/honeybuns/baseboard.h
+++ baseboard/honeybuns/baseboard.h
@@ -216,6 +216,11 @@ struct power_seq {
 	unsigned int delay_ms;   /* delay (in msec) after setting gpio_signal */
 };
 
+enum mf_preference {
+	MF_OFF = 0,
+	MF_ON,
+};
+
 /*
  * This is required as adc_channel is included in adc.h which ends up being
  * included when TCPMv2 functions are included
@@ -228,6 +233,7 @@ extern const struct power_seq board_power_seq[];
 extern const size_t board_power_seq_count;
 void baseboard_power_button_evt(int level);
 int baseboard_usbc_init(int port);
+int dock_get_mf_preferece(void);
 
 int baseboard_usbc_init(int port);
 int baseboard_ppc_enable_sink_path(int port);
