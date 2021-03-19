7dfa097642d161e8c09d97747bfc56b8659adb2e
Rob Barnes <robbarnes@google.com>
2021-03-19 11:57:30 -0600

    guybrush: Add morphious battery
    Expirement, do not merge.
    
    Change-Id: I7988815b64f5d4e147aac679f8093510ddccc414

--- baseboard/guybrush/baseboard.c
+++ baseboard/guybrush/baseboard.c
@@ -312,9 +312,36 @@ const struct board_batt_params board_battery_info[] = {
 			.discharging_max_c    = 75,
 		},
 	},
+	/* LG L19L4PG2  */
+	[BATTERY_LGC] = {
+		.fuel_gauge = {
+			.manuf_name = "LGC",
+			.ship_mode = {
+				.reg_addr = 0x34,
+				.reg_data = { 0x0000, 0x1000 },
+			},
+			.fet = {
+				.reg_addr = 0x34,
+				.reg_mask = 0x0100,
+				.disconnect_val = 0x0100,
+			}
+		},
+		.batt_info = {
+			.voltage_max		= 8800, /* mV */
+			.voltage_normal		= 7700, /* mV */
+			.voltage_min		= 6000,  /* mV */
+			.precharge_current	= 256,	 /* mA */
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 50,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 73,
+		},
+	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_AP18F4M;
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_LGC;
 
 const struct charger_config_t chg_chips[] = {
 	{
--- baseboard/guybrush/baseboard.h
+++ baseboard/guybrush/baseboard.h
@@ -307,6 +307,7 @@ enum temp_sensor_id {
 /* Battery Types */
 enum battery_type {
 	BATTERY_AP18F4M,
+	BATTERY_LGC,
 	BATTERY_TYPE_COUNT,
 };
 
