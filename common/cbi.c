20577599c2a7c3595f7abe51369566d981a75651
Scott Collyer <scollyer@google.com>
2021-03-20 16:34:12 -0700

    cbi: API for EC to update fw_config tag
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I3a4360b32feac4a04fe53b518c122940f4b340f5

--- common/cbi.c
+++ common/cbi.c
@@ -493,6 +493,42 @@ static void dump_cbi(void)
 	print_uint64_tag("REWORK_ID", cbi_get_rework_id(&lval), &lval);
 }
 
+int cbi_init(void)
+{
+	memset(cbi, 0, sizeof(cbi));
+	memcpy(head->magic, cbi_magic, sizeof(cbi_magic));
+	head->total_size = sizeof(*head);
+	cached_read_result = EC_SUCCESS;
+
+	/* Whether we're modifying existing data or creating new one,
+	 * we take over the format. */
+	head->major_version = CBI_VERSION_MAJOR;
+	head->minor_version = CBI_VERSION_MINOR;
+	head->crc = cbi_crc8(head);
+
+	/* We already checked write protect failure case. */
+	if (write_board_info())
+		return EC_RES_ERROR;
+
+	return EC_SUCCESS;
+}
+
+
+int cbi_set_fw_config(uint32_t fw_config)
+{
+	if(do_read_board_info())
+		cbi_init();
+
+	cbi_set_board_info(CBI_TAG_FW_CONFIG, (uint8_t *)&fw_config,
+			   sizeof(int));
+
+	head->crc = cbi_crc8(head);
+	if (write_board_info())
+		return EC_RES_ERROR;
+
+	return EC_SUCCESS;
+}
+
 /*
  * Space for the set command (does not include data space) plus maximum
  * possible console input
