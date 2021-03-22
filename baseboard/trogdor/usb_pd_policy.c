d903e95f635cadb888fdbc99d3d2989b8cd90440
Simon Glass <sjg@chromium.org>
2021-03-22 16:19:54 +1300

    trogdor: Allow zephyr to run without the AP for now
    We don't have full support for the AP protocol on trogdor yet. Allow
    this to be omitted for now, without causing a build error.
    
    BUG=b:183296099
    BRANCH=none
    TEST=build zephyr for lazor
    make BOARD=lazor -j4
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: I2ce0e1c40c909457686552b7207fb5c884c29614

--- baseboard/trogdor/usb_pd_policy.c
+++ baseboard/trogdor/usb_pd_policy.c
@@ -204,6 +204,11 @@ __override int svdm_dp_attention(int port, uint32_t *payload)
 			    polarity_rm_dts(pd_get_polarity(port)));
 	}
 
+/*
+ * TODO(b/183054226): Allow building this code without AP support on Zephyr,
+ * to aid bringup
+ */
+#if !defined(CONFIG_ZEPHYR) || defined(CONFIG_PLATFORM_EC_MKBP_EVENT)
 	if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND) &&
 	    (irq || lvl))
 		/*
@@ -211,6 +216,7 @@ __override int svdm_dp_attention(int port, uint32_t *payload)
 		 * present.
 		 */
 		pd_notify_dp_alt_mode_entry(port);
+#endif
 
 	/* Configure TCPC for the HPD event, for proper muxing */
 	usb_mux_hpd_update(port, lvl, irq);
