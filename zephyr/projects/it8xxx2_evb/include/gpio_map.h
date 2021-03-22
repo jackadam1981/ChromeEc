ab63990a1e3c1be788eb318159cbda4a99851ab4
tim <tim2.lin@ite.corp-partner.google.com>
2021-03-22 08:23:08 +0800

    WIP: zephyr: add SPI slave driver
    Change-Id: I25ccdd0b29bf46b6a4ad451150f63b47dc9d3b97

--- zephyr/projects/it8xxx2_evb/include/gpio_map.h
+++ zephyr/projects/it8xxx2_evb/include/gpio_map.h
@@ -12,6 +12,9 @@
 #define GPIO_ENTERING_RW	GPIO_UNIMPLEMENTED
 #define GPIO_WP_L		GPIO_UNIMPLEMENTED
 
+#define GPIO_POWER_BUTTON_L	NAMED_GPIO(power_button_l)
+#define GPIO_LID_OPEN		NAMED_GPIO(lid_open)
+
 /*
  * Set EC_CROS_GPIO_INTERRUPTS to a space-separated list of GPIO_INT items.
  *
@@ -27,6 +30,8 @@
  * #define EC_CROS_GPIO_INTERRUPTS \
  *   GPIO_INT(NAMED_GPIO(h1_ec_pwr_btn_odl), GPIO_INT_EDGE_BOTH, button_print)
  */
-#define EC_CROS_GPIO_INTERRUPTS
+#define EC_CROS_GPIO_INTERRUPTS		\
+	GPIO_INT(GPIO_POWER_BUTTON_L, GPIO_INT_EDGE_BOTH, power_button_interrupt)	\
+	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)
 
 #endif /* __ZEPHYR_GPIO_MAP_H */
