60d9b348e9593c989d7fc6db88e13b255777fbc3
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2021-03-23 15:20:18 +0800

    zephyr: add support for it8xxx2 cros_kb_row driver
    Add support for it8xxx2 cros_kb_row driver.
    
    BUG=b:187192587
    BRANCH=none
    TEST=on hayato, console cmd "ksstate" then press key:
    [115.097839 KB state: -- -- -- 02 -- -- 02 -- -- -- -- -- --]
    [116.462371 KB state: -- -- -- -- -- -- 02 -- -- -- -- -- --]
    [116.499633 KB state: -- -- -- -- -- -- -- -- -- -- -- -- --]
    
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>
    Change-Id: I067b95bf2dfe4978e5370ce27382c67db100467b

---
+++ zephyr/drivers/cros_kb_raw/cros_kb_raw_ite.c
@@ -0,0 +1,183 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#define DT_DRV_COMPAT ite_it8xxx2_cros_kb_raw
+
+#include <assert.h>
+#include <drivers/cros_kb_raw.h>
+#include <drivers/clock_control.h>
+#include <drivers/gpio.h>
+#include <kernel.h>
+#include <soc.h>
+
+#include "ec_tasks.h"
+#include "keyboard_raw.h"
+#include "task.h"
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(cros_kb_raw, LOG_LEVEL_ERR);
+
+#define IT8XXX2_IRQ_WKINTC 13
+#define KSOH_PIN_MASK (((1 << (KEYBOARD_COLS_MAX - 8)) - 1) & 0xff)
+
+/* Device config */
+struct cros_kb_raw_ite_config {
+	/* keyboard scan controller base address */
+	uintptr_t base;
+	/* Keyboard scan input (KSI) wake-up irq */
+	int irq;
+};
+
+static int kb_raw_ite_init(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	/* Clock default is on */
+	return 0;
+}
+
+/* Cros ec keyboard raw api functions */
+static int cros_kb_raw_ite_enable_interrupt(const struct device *dev,
+					    int enable)
+{
+	ARG_UNUSED(dev);
+
+	if (enable) {
+		IT8XXX2_WUC_WUESR3 = 0xFF;
+		ite_intc_isr_clear(IT8XXX2_IRQ_WKINTC);
+		irq_enable(IT8XXX2_IRQ_WKINTC);
+	} else {
+		irq_disable(IT8XXX2_IRQ_WKINTC);
+	}
+
+	return 0;
+}
+
+static int cros_kb_raw_ite_read_row(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	/* Bits are active-low, so invert returned levels */
+	return IT8XXX2_KBS_KSI ^ 0xff;
+}
+
+static int cros_kb_raw_ite_drive_column(const struct device *dev, int col)
+{
+	int mask;
+	unsigned int key;
+
+	ARG_UNUSED(dev);
+
+	/* Tri-state all outputs */
+	if (col == KEYBOARD_COLUMN_NONE)
+		mask = 0xffff;
+	/* Assert all outputs */
+	else if (col == KEYBOARD_COLUMN_ALL)
+		mask = 0;
+	/* Assert a single output */
+	else
+		mask = 0xffff ^ BIT(col);
+#ifdef CONFIG_PLATFORM_EC_KEYBOARD_COL2_INVERTED
+	/* KSO[2] is inverted. */
+	mask ^= BIT(2);
+#endif
+	IT8XXX2_KBS_KSOL = mask & 0xff;
+	/* critical section with interrupts off */
+	key = irq_lock();
+	/*
+	 * Because IT8XXX2_KBS_KSOH1 register is shared by keyboard scan
+	 * out and GPIO output mode, so we don't drive all KSOH pins
+	 * here (this depends on how many keyboard matrix output pin
+	 * we are using).
+	 */
+	IT8XXX2_KBS_KSOH1 = (IT8XXX2_KBS_KSOH1 & ~KSOH_PIN_MASK) |
+			    ((mask >> 8) & KSOH_PIN_MASK);
+	/* restore interrupts */
+	irq_unlock(key);
+
+	return 0;
+}
+
+static void cros_kb_raw_ite_ksi_isr(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	/*
+	 * We clear irq status in ite_intc_irq_handler(),
+	 * after interrupt was fired.
+	 */
+
+	IT8XXX2_WUC_WUESR3 = 0xFF;
+
+	/* Wake-up keyboard scan task */
+	task_wake(TASK_ID_KEYSCAN);
+}
+
+static int cros_kb_raw_ite_init(const struct device *dev)
+{
+	unsigned int key;
+
+	/* Ensure top-level interrupt is disabled */
+	cros_kb_raw_ite_enable_interrupt(dev, 0);
+
+	/*
+	 * bit2, Setting 1 enables the internal pull-up of the KSO[15:0] pins.
+	 * To pull up KSO[17:16], set the GPCR registers of their
+	 * corresponding GPIO ports.
+	 * bit0, Setting 1 enables the open-drain mode of the KSO[17:0] pins.
+	 */
+	IT8XXX2_KBS_KSOCTRL = 0x05;
+	/* bit2, 1 enables the internal pull-up of the KSI[7:0] pins. */
+	IT8XXX2_KBS_KSICTRL = 0x04;
+#ifdef CONFIG_PLATFORM_EC_KEYBOARD_COL2_INVERTED
+	/* KSO[2] is high, others are low. */
+	IT8XXX2_KBS_KSOL = BIT(2);
+	/* Enable KSO2's push-pull */
+	IT8XXX2_KBS_KSOLGCTRL |= BIT(2);
+	IT8XXX2_KBS_KSOLGOEN |= BIT(2);
+#else
+	/* KSO[7:0] pins low. */
+	IT8XXX2_KBS_KSOL = 0x00;
+#endif
+	/* critical section with interrupts off */
+	key = irq_lock();
+	/*
+	 * KSO[COLS_MAX:8] pins low.
+	 * NOTE: KSO[15:8] pins can part be enabled for keyboard function and
+	 *       rest be configured as GPIO output mode. In this case that we
+	 *       disable the ISR in critical section to avoid race condition.
+	 */
+	IT8XXX2_KBS_KSOH1 &= ~KSOH_PIN_MASK;
+	/* restore interrupts */
+	irq_unlock(key);
+	/* KSI[0-7] falling-edge triggered is selected */
+	IT8XXX2_WUC_WUEMR3 = 0xFF;
+	/* W/C */
+	IT8XXX2_WUC_WUESR3 = 0xFF;
+	ite_intc_isr_clear(IT8XXX2_IRQ_WKINTC);
+	/* Enable WUC for KSI[0-7] */
+	IT8XXX2_WUC_WUENR3 = 0xFF;
+
+	IRQ_CONNECT(IT8XXX2_IRQ_WKINTC, 0, cros_kb_raw_ite_ksi_isr, NULL, 0);
+
+	return 0;
+}
+
+static const struct cros_kb_raw_driver_api cros_kb_raw_ite_driver_api = {
+	.init = cros_kb_raw_ite_init,
+	.drive_colum = cros_kb_raw_ite_drive_column,
+	.read_rows = cros_kb_raw_ite_read_row,
+	.enable_interrupt = cros_kb_raw_ite_enable_interrupt,
+};
+
+static const struct cros_kb_raw_ite_config cros_kb_raw_cfg = {
+	.base = DT_INST_REG_ADDR(0),
+	.irq = DT_INST_IRQN(0),
+};
+
+DEVICE_DEFINE(cros_kb_raw_ite_0, DT_INST_LABEL(0), kb_raw_ite_init, NULL,
+	      NULL, &cros_kb_raw_cfg, PRE_KERNEL_1,
+	      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+	      &cros_kb_raw_ite_driver_api);
