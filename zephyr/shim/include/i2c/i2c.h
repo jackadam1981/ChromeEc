f6e98888f7a8b90a6c268311925dd7a7cb6aeaed
Yuval Peress <peress@chromium.org>
2021-03-23 01:24:03 -0600

    zephyr: i2c mapping using devicetree compatible strings
    Currently, every project will need to provide a custom mapping header
    for i2c as well as gpios. We'd like to move that overhead to the dts
    files, which will make the board dts a lot more reusable as well as
    clean up the bringup of new boards.
    
    This change adds to the i2c_ports enum 2 entries per port. The first
    is the compatible string which is used to identify the purpose of the
    port prefixed with I2C_. The second is the name that is expected for
    the port (this is the name that is references through the cros ec
    common code).
    
    When initializing the device we effectively reverse that mapping.
    Unfortunately, there doesn't exist a devicetree _FOREACH like method
    of iterating on these so both the common name and the compatible
    string are needed (though this is only done once when adding a new
    compatible string).
    
    In this example (to keep review simple) I've only altered volteer's
    I2C_PORT_POWER. For each new I2C_PORT_ value, we'll need to add a
    new yaml file with the compatible string, declare the enum in the
    i2c.h file, and initialize the device in the i2c.c file. Finally,
    update the .dts files which should use it.
    
    BRANCH=none
    BUG=none
    TEST=build and flashed volteer
    TEST=zmake testall
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I42cc098a9badac57a3781fa9dfaf32c0ec0c5aca

--- zephyr/shim/include/i2c/i2c.h
+++ zephyr/shim/include/i2c/i2c.h
@@ -13,8 +13,38 @@
 #if DT_NODE_EXISTS(DT_PATH(named_i2c_ports))
 #define I2C_PORT(id) DT_CAT(I2C_, id)
 #define I2C_PORT_WITH_COMMA(id) I2C_PORT(id),
+/**
+ * Add an enum value using `name` if there exists a node with the given `compat`
+ * string along with a status 'okay'.
+ *
+ * Usage:
+ *   I2C_PORT_FROM_COMPAT(test_compat_name, ENUM_NAME)
+ *
+ * This will first check to see if any nodes contain `test_compat_name` as a
+ * compatible string AND that the node has the status set to 'okay'. If that
+ * condition is met, it will resolve to `ENUM_NAME,` otherwise it will be a
+ * no-op.
+ */
+#define I2C_PORT_FROM_COMPAT(compat, name) \
+	COND_CODE_1(DT_HAS_COMPAT_STATUS_OKAY(compat), (name,), ())
+
+/**
+ * Conditionally initialize `dev_ptr` to the first device having the provided
+ * `compat` string and having the status set to 'okay'. If the condition is met,
+ * this statement will resolve to:
+ *   dev_ptr = device_get_binding(DT_LABEL(DT_INST(0, compat)))
+ *
+ * Note that the call site still need to follow with `;` to make this a valid
+ * statement.
+ */
+#define I2C_DEV_INIT_FROM_COMPAT(dev_ptr, compat)                             \
+	COND_CODE_1(                                                          \
+		DT_HAS_COMPAT_STATUS_OKAY(compat),                            \
+		(dev_ptr = device_get_binding(DT_LABEL(DT_INST(0, compat)))), \
+		())
 enum i2c_ports {
 DT_FOREACH_CHILD(DT_PATH(named_i2c_ports), I2C_PORT_WITH_COMMA)
+I2C_PORT_FROM_COMPAT(cros_i2c_port_power, I2C_PORT_POWER)
 I2C_PORT_COUNT
 };
 #define NAMED_I2C(name) I2C_PORT(DT_PATH(named_i2c_ports, name))
