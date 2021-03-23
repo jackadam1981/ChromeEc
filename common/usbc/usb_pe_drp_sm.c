b9212cfdb7b31f66ce7e8ff954fb528116d422b2
Scott Collyer <scollyer@google.com>
2021-03-22 20:50:04 -0700

    tcpmv2: vconn swap logic was commented out
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I94ed3ae2d147cdd24530a751f2957a523ac3da2e

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -2663,6 +2663,7 @@ static void pe_src_ready_run(int port)
 				set_state_pe(port, PE_DRS_EVALUATE_SWAP);
 				return;
 			case PD_CTRL_VCONN_SWAP:
+				board_debug_gpio(TRIGGER_1, 1, 1 * MSEC);
 				if (IS_ENABLED(CONFIG_USBC_VCONN))
 					set_state_pe(port,
 							PE_VCS_EVALUATE_SWAP);
@@ -3457,6 +3458,7 @@ static void pe_snk_ready_run(int port)
 							PE_DRS_EVALUATE_SWAP);
 				return;
 			case PD_CTRL_VCONN_SWAP:
+				board_debug_gpio(TRIGGER_1, 1, 1 * MSEC);
 				if (IS_ENABLED(CONFIG_USBC_VCONN))
 					set_state_pe(port,
 							PE_VCS_EVALUATE_SWAP);
