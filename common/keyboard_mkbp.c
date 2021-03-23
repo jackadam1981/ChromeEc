555df5bc2a1efeb52dd39536827ca2c47b72b344
Wai-Hong Tam <waihong@google.com>
2021-03-22 17:05:04 -0700

    keyboard_mkbp: use mutex_t which supports Zephyr
    mutex_t supports another implementation for Zephyr. Use it such
    that Zephyr and CrOS EC can share the same code.
    
    The mutex_t in Zephyr needs to be initialized using k_mutex_init()
    before use.
    
    BRANCH=None
    BUG=b:183445211
    TEST=Build the CrOS EC image for Lazor.
    
    Change-Id: Ie8ef54970774c5ec27329a8b2913e015577e8113
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

--- common/keyboard_mkbp.c
+++ common/keyboard_mkbp.c
@@ -60,12 +60,12 @@ static struct ec_response_get_next_event fifo[FIFO_DEPTH];
  * Mutex for critical sections of mkbp_fifo_add(), which is called
  * from various tasks.
  */
-static struct mutex fifo_add_mutex;
+static mutex_t fifo_add_mutex;
 /*
  * Mutex for critical sections of fifo_remove(), which is called from the
  * hostcmd task and from keyboard_clear_buffer().
  */
-static struct mutex fifo_remove_mutex;
+static mutex_t fifo_remove_mutex;
 
 /* Button and switch state. */
 static uint32_t mkbp_button_state;
@@ -316,6 +316,10 @@ static void mkbp_report_switch_on_init(void)
 {
 	/* All switches initialized, report switch state to AP */
 	mkbp_init_done = true;
+#ifdef CONFIG_ZEPHYR
+	k_mutex_init(&fifo_add_mutex);
+	k_mutex_init(&fifo_remove_mutex);
+#endif /* CONFIG_ZEPHYR */
 	mkbp_fifo_add(EC_MKBP_EVENT_SWITCH,
 		      (const uint8_t *)&mkbp_switch_state);
 }
