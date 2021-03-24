cc4bb2fb307cf62388327567071cd1c018204e6e
Scott Collyer <scollyer@google.com>
2021-03-24 13:11:04 -0700

    baklava: C1 support and full power button support
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Ia28689c9318e268787ea04b0786decf149dce98f

--- board/baklava/board.c
+++ board/baklava/board.c
@@ -60,23 +60,9 @@ void hpd_interrupt(enum gpio_signal signal)
 	usb_pd_hpd_edge_event(signal);
 }
 
-void board_uf_manage_vbus(void)
-{
-	int level = gpio_get_level(GPIO_USBC_UF_MUX_VBUS_EN);
-
-	/*
-	 * GPIO_USBC_UF_MUX_VBUS_EN is an output from the PS8803 which tracks if
-	 * C1 is attached. When it's attached, this signal will be high. Use
-	 * this level to control PPC VBUS on/off.
-	 */
-	ppc_vbus_source_enable(USB_PD_PORT_USB3, level);
-	CPRINTS("C1: State = %s", level ? "Attached.SRC " : "Unattached.SRC");
-}
-DECLARE_DEFERRED(board_uf_manage_vbus);
-
 static void board_uf_manage_vbus_interrupt(enum gpio_signal signal)
 {
-	hook_call_deferred(&board_uf_manage_vbus_data, 0);
+	baseboard_uf_check_state();
 }
 
 static void board_pwr_btn_interrupt(enum gpio_signal signal)
@@ -300,7 +286,6 @@ __overridable int dock_get_mf_preferece(void)
 static void board_init(void)
 {
 #ifdef SECTION_IS_RW
-	/* Initialize PPC and check usbc state */
 	board_config_usbc_uf_ppc();
 #endif
 }
