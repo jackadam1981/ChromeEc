abc99d4bfd09193420452955b2148be3ac9340e1
Patryk Duda <pdk@semihalf.com>
2021-03-24 14:54:55 +0000

    nightfury: Change down debounce time to 30ms
    This patch is implementation of CL:2744760 from firmware-hatch-12672.B
    for ToT. This change can't be cherry-picked because CL:2194160 removes
    keyscan_config structure from baseboard/hatch/baseboard.c.
    
    BUG=b:181307934
    BRANCH=none
    TEST=make BOARD=nightfury
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: I070390b1f4f13dcce5a93ed0c8982b82a3574d6c

--- board/nightfury/board.c
+++ board/nightfury/board.c
@@ -26,6 +26,7 @@
 #include "gpio.h"
 #include "hooks.h"
 #include "host_command.h"
+#include "keyboard_scan.h"
 #include "lid_switch.h"
 #include "power.h"
 #include "power_button.h"
@@ -303,6 +304,30 @@ const struct motion_sensor_t *motion_als_sensors[] = {
 	&motion_sensors[BASE_ALS],
 };
 BUILD_ASSERT(ARRAY_SIZE(motion_als_sensors) == ALS_COUNT);
+
+struct keyboard_scan_config keyscan_config = {
+#ifdef CONFIG_KEYBOARD_COL2_INVERTED
+	/*
+	 * CONFIG_KEYBOARD_COL2_INVERTED is defined for passing the column 2
+	 * to H1 which inverts the signal. The signal passing through H1
+	 * adds more delay. Need a larger delay value. Otherwise, pressing
+	 * Refresh key will also trigger T key, which is in the next scanning
+	 * column line. See http://b/156007029.
+	 */
+	.output_settle_us = 80,
+#else
+	.output_settle_us = 50,
+#endif /* CONFIG_KEYBOARD_COL2_INVERTED */
+	.debounce_down_us = 30 * MSEC,
+	.debounce_up_us = 30 * MSEC,
+	.scan_period_us = 3 * MSEC,
+	.min_post_scan_delay_us = 1000,
+	.poll_timeout_us = 100 * MSEC,
+	.actual_key_mask = {
+		0x1c, 0xff, 0xff, 0xff, 0xff, 0xf5, 0xff,
+		0xa4, 0xff, 0xfe, 0x55, 0xfa, 0xca  /* full set */
+	},
+};
 
 /******************************************************************************/
 /* Physical fans. These are logically separate from pwm_channels. */
