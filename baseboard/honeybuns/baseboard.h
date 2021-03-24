ae321a382682804ba10ed8356e26413db82c897b
Scott Collyer <scollyer@google.com>
2021-03-24 13:09:09 -0700

    honeybuns/quiche: C2 fixes for power button on/off
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I2f9500f7e58de674c6f7c12a95cebad45de166cc

--- baseboard/honeybuns/baseboard.h
+++ baseboard/honeybuns/baseboard.h
@@ -234,6 +234,7 @@ enum adc_channel {
 
 extern const struct power_seq board_power_seq[];
 extern const size_t board_power_seq_count;
+
 void baseboard_power_button_evt(int level);
 
 /*
@@ -245,6 +246,8 @@ void baseboard_power_button_evt(int level);
 int baseboard_usbc_init(int port);
 
 int dock_get_mf_preferece(void);
+int baseboard_config_usbc_uf_ppc(void);
+void baseboard_uf_check_state(void);
 
 /*
  * Set MST_LANE_CONTROL gpio to match the DP pin configuration selected
