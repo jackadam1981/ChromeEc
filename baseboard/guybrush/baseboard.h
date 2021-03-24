db0f61b3d8d9fab9527d03207b973007c68ec590
Matt_Wang <Matt_Wang@compal.corp-partner.google.com>
2021-03-24 15:04:33 +0800

    Guybrush: initial BMI160 sensor
    Initial base BMI160 sensor.
    
    BUG=b:178213305
    BRANCH=none
    TEST=make -j BOARD=guybrush
    TEST=can read the sensor data in ec console by i2cxfer.
    
    Signed-off-by: Matt_Wang <Matt_Wang@compal.corp-partner.google.com>
    Change-Id: Id49ff98dc921ba173b24f3c389fcfbde337ce77c

--- baseboard/guybrush/baseboard.h
+++ baseboard/guybrush/baseboard.h
@@ -110,6 +110,8 @@
 #define CONFIG_TABLET_MODE
 #define CONFIG_GMR_TABLET_MODE
 #define GMR_TABLET_MODE_GPIO_L		GPIO_TABLET_MODE
+#define CONFIG_SUPPRESSED_HOST_COMMANDS \
+	EC_CMD_MOTION_SENSE_CMD
 
 /* Battery Config */
 #define CONFIG_BATTERY_PRESENT_GPIO	GPIO_EC_BATT_PRES_ODL
@@ -307,6 +309,12 @@ enum temp_sensor_id {
 	TEMP_SENSOR_COUNT
 };
 
+enum sensor_id {
+	BASE_ACCEL = 0,
+	BASE_GYRO,
+	SENSOR_COUNT,
+};
+
 /* Battery Types */
 enum battery_type {
 	BATTERY_AP18F4M,
@@ -333,7 +341,6 @@ enum mft_channel {
 	/* Number of MFT channels */
 	MFT_CH_COUNT,
 };
-
 
 /* Common definition for the USB PD interrupt handlers. */
 void tcpc_alert_event(enum gpio_signal signal);
