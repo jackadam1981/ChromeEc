2da00d94fd5c8acc8177ff597f983f539d8474e9
Scott Collyer <scollyer@google.com>
2021-03-24 13:09:09 -0700

    honeybuns/quiche: C2 fixes for power button on/off
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I2f9500f7e58de674c6f7c12a95cebad45de166cc

--- baseboard/honeybuns/baseboard.c
+++ baseboard/honeybuns/baseboard.c
@@ -186,9 +186,14 @@ static void baseboard_init(void)
 		CPRINTS("baseboard: mf config = %d", fw_config & 0x1);
 	}
 	baseboard_set_dp_lane_control();
+#ifdef GPIO_USBC_UF_ATTACHED_SRC
+	/* Configure UF usbc ppc and check usbc state */
+	baseboard_config_usbc_uf_ppc();
+#endif
 	/* Enable power button interrupt */
 	gpio_enable_interrupt(GPIO_PWR_BTN);
 	baseboard_set_led(dock_mf);
+
 #else
 	/* Turn on power rails */
 	board_power_sequence(1);
@@ -235,6 +240,10 @@ static void baseboard_power_on(void)
 	}
 	/* Enable usbc interrupts */
 	board_enable_usbc_interrupts();
+
+#ifdef GPIO_USBC_UF_ATTACHED_SRC
+	baseboard_config_usbc_uf_ppc();
+#endif
 }
 
 static void baseboard_power_off(void)
--- baseboard/honeybuns/usbc_support.c
+++ baseboard/honeybuns/usbc_support.c
@@ -20,6 +20,8 @@
 #include "registers.h"
 #include "ucpd-stm32gx.h"
 
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ## args)
 
 static void baseboard_ucpd_apply_rd(int port)
 {
@@ -134,4 +136,45 @@ int baseboard_usbc_init(int port)
 
 	return rv;
 }
+
+#if defined(GPIO_USBC_UF_ATTACHED_SRC) && defined(SECTION_IS_RW)
+static void baseboard_uf_manage_vbus(void)
+{
+	int level = gpio_get_level(GPIO_USBC_UF_ATTACHED_SRC);
+
+	/*
+	 * GPIO_USBC_UF_MUX_VBUS_EN is an output from the PS8803 which tracks if
+	 * C2 is attached. When it's attached, this signal will be high. Use
+	 * this level to control PPC VBUS on/off.
+	 */
+	ppc_vbus_source_enable(USB_PD_PORT_UF, level);
+	CPRINTS("C2: State = %s", level ? "Attached.SRC " : "Unattached.SRC");
+}
+DECLARE_DEFERRED(baseboard_uf_manage_vbus);
+
+void baseboard_uf_check_state(void)
+{
+	hook_call_deferred(&baseboard_uf_manage_vbus_data, 0);
+}
+
+int baseboard_config_usbc_uf_ppc(void)
+{
+	int rv;
+
+	/*
+	 * This port is not usb-pd capable, but there is a ppc which must be
+	 * initialized, and keep the VBUS switch enabled.
+	 */
+	rv = ppc_init(USB_PD_PORT_UF);
+	if (rv)
+		return rv;
+
+	baseboard_uf_manage_vbus();
+
+	/* Enable VBUS control interrupt for C2 */
+	gpio_enable_interrupt(GPIO_USBC_UF_ATTACHED_SRC);
+
+	return EC_SUCCESS;
+}
+#endif
 
