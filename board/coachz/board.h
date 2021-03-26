ce67dbd3ebdcb6b0be7ae4a21c740cecf2405343
Daisuke Nojiri <dnojiri@chromium.org>
2021-03-25 17:34:22 -0700

    Coachz: Remove PCHG from RO
    Peripheral devices don't need to be charged until EC jumps to RW.
    This patch removes PCHG from the RO copy.
    
    BUG=b:173235954
    BRANCH=trogdor
    TEST=Charge stylus on Coachz.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I8156d1408bf69f4c1fccecaf3a77eaec29fa2c99

--- board/coachz/board.h
+++ board/coachz/board.h
@@ -69,9 +69,11 @@
 #define GPIO_KS_OPEN GPIO_LID_INT_N_HALL2
 
 /* WLC pins */
+#ifdef SECTION_IS_RW
 #define GPIO_PCHG_P0 GPIO_WLC_IRQ_CONN
 #define CONFIG_PERIPHERAL_CHARGER
 #define CONFIG_CTN730
+#endif
 
 #ifndef __ASSEMBLER__
 
