be9e62c854aa0c1c1c5317eecb6993ac33b27bc5
Diana Z <dzigterman@chromium.org>
2021-03-30 15:21:27 -0600

    TCPMv2: Swap priority for SOP and SOP' Discover Identity
    Test to see if increasing priority on SOP DiscoverIdentity helps port
    partners reply reliably.
    
    BRANCH=None
    BUG=b:181386287
    TEST=problematic dock works with storo
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: Ib1d10cd2c10aaceac80929d68548525489ec6a85

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -1842,17 +1842,17 @@ __maybe_unused static bool pe_attempt_port_discovery(int port)
 	 * discovery spacing or BUSY spacing runs out.
 	 */
 	if (pd_timer_is_expired(port, PE_TIMER_DISCOVER_IDENTITY)) {
-		if (pd_get_identity_discovery(port, TCPC_TX_SOP_PRIME) ==
-				PD_DISC_NEEDED) {
-			pe[port].tx_type = TCPC_TX_SOP_PRIME;
-			set_state_pe(port, PE_VDM_IDENTITY_REQUEST_CBL);
-			return true;
-		} else if (pd_get_identity_discovery(port, TCPC_TX_SOP) ==
+		if (pd_get_identity_discovery(port, TCPC_TX_SOP) ==
 				PD_DISC_NEEDED &&
 				pe_can_send_sop_vdm(port, CMD_DISCOVER_IDENT)) {
 			pe[port].tx_type = TCPC_TX_SOP;
 			set_state_pe(port,
 				     PE_INIT_PORT_VDM_IDENTITY_REQUEST);
+			return true;
+		} else if (pd_get_identity_discovery(port, TCPC_TX_SOP_PRIME) ==
+				PD_DISC_NEEDED) {
+			pe[port].tx_type = TCPC_TX_SOP_PRIME;
+			set_state_pe(port, PE_VDM_IDENTITY_REQUEST_CBL);
 			return true;
 		} else if (pd_get_svids_discovery(port, TCPC_TX_SOP) ==
 				PD_DISC_NEEDED &&
