8cc564f3746480daf2a398fcb64ab67c1d1327c9
Ting Shen <phoenixshen@google.com>
2021-03-30 17:39:49 +0800

    cherry: enable usb pd
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: Ied4516abef3d544b8b4bdf8355f0f9fc305629a3

--- driver/tcpm/rt1718s.c
+++ driver/tcpm/rt1718s.c
@@ -349,9 +349,7 @@ const struct tcpm_drv rt1718s_tcpm_drv = {
 	.init			= &rt1718s_init,
 	.release		= &tcpci_tcpm_release,
 	.get_cc			= &tcpci_tcpm_get_cc,
-#ifdef CONFIG_USB_PD_VBUS_DETECT_TCPC
 	.check_vbus_level	= &tcpci_tcpm_check_vbus_level,
-#endif
 	.select_rp_value	= &tcpci_tcpm_select_rp_value,
 	.set_cc			= &tcpci_tcpm_set_cc,
 	.set_polarity		= &tcpci_tcpm_set_polarity,
