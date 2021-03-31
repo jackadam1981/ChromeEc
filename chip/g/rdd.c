c8054fdbaa3f9c18dd153f6afce5c06f46ae96e0
Mary Ruthven <mruthven@chromium.org>
2021-04-01 07:48:53 +1100

    cr50: make rddkeepalive read-only when console is locked
    Print the Rdd state when the console is locked. Don't allow setting it.
    
    BUG=none
    TEST=manual
    	> ccd lock
    	CCD locked.
    
    	> rddkeepalive
    	Rdd:     connected
    
    	> rddkeepalive enable
    	Parameter 1 invalid
    	Usage: rddkeepalive [BOOLEAN] [atboot]
    
    	> ccd testlab open
    
    	> ccd
     	State: Opened
    	...
    
    	> rddkeepalive enable
    	Forcing Rdd detect keepalive
    	>
    
    Change-Id: Ie309e42e7ef295bb0d7e7208d4bd6c17ce799e98
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- chip/g/rdd.c
+++ chip/g/rdd.c
@@ -207,6 +207,7 @@ void init_rdd_state(void)
 	GWRITE_FIELD(RDD, INT_ENABLE, INTR_DEBUG_STATE_DETECTED, 1);
 }
 
+
 static int command_rdd_keepalive(int argc, char **argv)
 {
 	if (argc == 1) {
@@ -214,7 +215,7 @@ static int command_rdd_keepalive(int argc, char **argv)
 		return EC_SUCCESS;
 	}
 
-	if (!parse_bool(argv[1], &force_detected))
+	if (console_is_restricted() || !parse_bool(argv[1], &force_detected))
 		return EC_ERROR_PARAM1;
 
 	if (force_detected) {
@@ -228,6 +229,6 @@ static int command_rdd_keepalive(int argc, char **argv)
 
 	return EC_SUCCESS;
 }
-DECLARE_CONSOLE_COMMAND(rddkeepalive, command_rdd_keepalive,
-			"[BOOLEAN]",
-			"Get Rdd state or force keepalive");
+DECLARE_SAFE_CONSOLE_COMMAND(rddkeepalive, command_rdd_keepalive,
+			     "[BOOLEAN] [atboot]",
+			     "Get Rdd state or force keepalive");
