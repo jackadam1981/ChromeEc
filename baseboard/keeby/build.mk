0a7b17fe5efff0c92b9aeba261a24740d8c7f1a9
Aseda Aboagye <aaboagye@google.com>
2021-03-31 15:17:02 -0700

    keeby/lalala: Initial commit
    This is the initial commit for lalala, a NPCX797FC variant of keeby.
    
    BUG=b:184191507
    BRANCH=None
    TEST=`make -j BOARD=lalala`
    
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Change-Id: I0420cf7252cba5571fe82d0d88d4dccc5d866782

---
+++ baseboard/keeby/build.mk
@@ -0,0 +1,11 @@
+# -*- makefile -*-
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Baseboard specific files build
+#
+
+baseboard-y=baseboard.o cbi_fw_config.o cbi_ssfc.o
+baseboard-$(VARIANT_KEEBY_EC_NPCX797FC)+=variant_ec_npcx797fc.o
+baseboard-$(VARIANT_KEEBY_EC_IT8320)+=variant_ec_it8320.o
