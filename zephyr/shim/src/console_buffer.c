c46f0c899e1dc85fd9defe4cd16d3557be33119f
Yuval Peress <peress@chromium.org>
2021-04-01 22:28:09 -0600

    zephyr: use interrupt based RX buffering
    Poll-based RX was technically incorrect since Zephyr was setting up
    the UART to FIFO mode (instead of byte mode). This change replaces
    polling the RX UART with interrupt based callbacks. When calling
    uart_shell_stop(), the ISR will be replaced and will instead queue
    items to a ring buffer which can be read from uart_getc() (it can
    also be cleared via the uart_clear_input() function).
    
    BRANCH=none
    BUG=b:181352041
    TEST=Build volteer, run, see expected 0xec07 from the GSC.
    
    Cq-Depend: chromium:2730870, chromium:2730869
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I5d2b61e914b56f678a259b373969522da87e8df3

--- zephyr/shim/src/console_buffer.c
+++ zephyr/shim/src/console_buffer.c
@@ -3,17 +3,160 @@
  * found in the LICENSE file.
  */
 
+#include <device.h>
+#include <drivers/uart.h>
 #include <kernel.h>
+#include <shell/shell.h>
+#include <shell/shell_uart.h>
+#include <sys/ring_buffer.h>
 #include <zephyr.h>
 
 #include "common.h"
 #include "console.h"
 #include "ec_commands.h"
+#include "uart.h"
 
 static char console_buf[CONFIG_PLATFORM_EC_HOSTCMD_CONSOLE_BUF_SIZE];
 static uint32_t previous_snapshot_idx;
 static uint32_t current_snapshot_idx;
 static uint32_t tail_idx;
+
+static struct k_poll_signal shell_uninit_signal;
+static struct k_poll_signal shell_init_signal;
+RING_BUF_DECLARE(rx_buffer, CONFIG_UART_RX_BUF_SIZE);
+
+static void fork_uart_rx_handle(const struct device *dev)
+{
+	static uint8_t scratch;
+	static uint8_t *data;
+	static uint32_t len, rd_len;
+
+	do {
+		/* Get some bytes on the ring buffer */
+		len = ring_buf_put_claim(&rx_buffer, &data, rx_buffer.size);
+		if (len > 0) {
+			/* Read from the FIFO up to `len` bytes */
+			rd_len = uart_fifo_read(dev, data, len);
+
+			/* Put `rd_len` bytes on the ring buffer */
+			ring_buf_put_finish(&rx_buffer, rd_len);
+		} else {
+			/* There's no room on the ring buffer, throw away 1
+			 * byte.
+			 */
+			rd_len = uart_fifo_read(dev, &scratch, 1);
+		}
+	} while (rd_len != 0 && rd_len == len);
+}
+
+static void fork_uart_callback(const struct device *dev, void *user_data)
+{
+	uart_irq_update(dev);
+
+	if (uart_irq_rx_ready(dev))
+		fork_uart_rx_handle(dev);
+}
+
+static void shell_uninit_callback(const struct shell *shell, int res)
+{
+	const struct device *dev =
+		device_get_binding(CONFIG_UART_SHELL_ON_DEV_NAME);
+
+	if (!res) {
+		/* Set the new callback */
+		uart_irq_callback_user_data_set(dev, fork_uart_callback, NULL);
+
+		/* Disable TX interrupts. We don't actually use TX but for some
+		 * reason none of this works without this line.
+		 */
+		uart_irq_tx_disable(dev);
+
+		/* Enable RX interrupts */
+		uart_irq_rx_enable(dev);
+	}
+
+	/* Notify the uninit signal that we finished */
+	k_poll_signal_raise(&shell_uninit_signal, res);
+}
+
+int uart_shell_stop(void)
+{
+	struct k_poll_event events[1] = {
+		K_POLL_EVENT_INITIALIZER(K_POLL_TYPE_SIGNAL,
+					 K_POLL_MODE_NOTIFY_ONLY,
+					 &shell_uninit_signal),
+	};
+	const struct device *dev =
+		device_get_binding(CONFIG_UART_SHELL_ON_DEV_NAME);
+
+	/* Clear all pending input */
+	uart_clear_input();
+
+	/* Disable RX and TX interrupts */
+	uart_irq_rx_disable(dev);
+	uart_irq_tx_disable(dev);
+
+	/* Initialize the uninit signal */
+	k_poll_signal_init(&shell_uninit_signal);
+
+	/* Stop the shell */
+	shell_uninit(shell_backend_uart_get_ptr(), shell_uninit_callback);
+
+	/* Wait for the shell to be turned off, the signal will wake us */
+	k_poll(events, 1, K_FOREVER);
+
+	/* Event was signaled, return the result */
+	return events[0].signal->result;
+}
+
+static void shell_init_from_work(struct k_work *work)
+{
+	const struct device *dev =
+		device_get_binding(CONFIG_UART_SHELL_ON_DEV_NAME);
+	bool log_backend = CONFIG_SHELL_BACKEND_SERIAL_LOG_LEVEL > 0;
+	uint32_t level =
+		(CONFIG_SHELL_BACKEND_SERIAL_LOG_LEVEL > LOG_LEVEL_DBG) ?
+			CONFIG_LOG_MAX_LEVEL :
+			CONFIG_SHELL_BACKEND_SERIAL_LOG_LEVEL;
+	ARG_UNUSED(work);
+
+	/* Initialize the shell and re-enable both RX and TX */
+	shell_init(shell_backend_uart_get_ptr(), dev, false, log_backend,
+		   level);
+	uart_irq_rx_enable(dev);
+	uart_irq_tx_enable(dev);
+
+	/* Notify the init signal that initialization is complete */
+	k_poll_signal_raise(&shell_init_signal, 0);
+}
+
+void uart_shell_start(void)
+{
+	static struct k_work shell_init_work;
+	const struct device *dev =
+		device_get_binding(CONFIG_UART_SHELL_ON_DEV_NAME);
+	struct k_poll_event events[1] = {
+		K_POLL_EVENT_INITIALIZER(K_POLL_TYPE_SIGNAL,
+					 K_POLL_MODE_NOTIFY_ONLY,
+					 &shell_init_signal),
+	};
+
+	/* Disable RX and TX interrupts */
+	uart_irq_rx_disable(dev);
+	uart_irq_tx_disable(dev);
+
+	/* Initialize k_work to call shell init (this makes it thread safe) */
+	k_work_init(&shell_init_work, shell_init_from_work);
+
+	/* Initialize the init signal to make sure we're read to listen */
+	k_poll_signal_init(&shell_init_signal);
+
+	/* Submit the work to be run by the kernel */
+	k_work_submit(&shell_init_work);
+
+	/* Wait for initialization to be run, the signal will wake us */
+	k_poll(events, 1, K_FOREVER);
+}
 
 static inline uint32_t next_idx(uint32_t cur_idx)
 {
@@ -112,4 +255,21 @@ int uart_console_read_buffer(uint8_t type, char *dest, uint16_t dest_size,
 int uart_buffer_full(void)
 {
 	return false;
+}
+
+int uart_getc(void)
+{
+	uint8_t c;
+
+	if (ring_buf_get(&rx_buffer, &c, 1)) {
+		return c;
+	}
+	return -1;
+}
+
+void uart_clear_input(void)
+{
+	/* Clear any remaining shell processing. */
+	shell_process(shell_backend_uart_get_ptr());
+	ring_buf_reset(&rx_buffer);
 }
