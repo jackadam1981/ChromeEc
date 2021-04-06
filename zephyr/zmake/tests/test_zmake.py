762118d7dd7c4d688f2f3f5d440e040940e980a0
Simon Glass <sjg@chromium.org>
2021-04-07 07:09:57 +1200

    zephyr: zmake: Show devicetree errors
    These errors are currently suppressed by default which is not very
    friendly. Fix this.
    
    BUG=b:177096315
    BRANCH=none
    TEST=(cd zephyr/zmake/ && python3 -m pytest  .)
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: Icdf8e8c91003a3f89d20c704a324673e1269cb0a

--- zephyr/zmake/tests/test_zmake.py
+++ zephyr/zmake/tests/test_zmake.py
@@ -50,11 +50,13 @@ class FakeJobserver(zmake.jobserver.GNUMakeJobServer):
         return self.jobserver.popen(new_cmd, *args, **kwargs)
 
 
-def do_test_with_log_level(log_level):
+def do_test_with_log_level(log_level, samples=None):
     """Test filtering using a particular log level
 
     Args:
         log_level: Level to use
+        samples: List of sample files to use (in the 'files' subdir), None to
+            use the standard ones
 
     Returns:
         tuple:
@@ -62,7 +64,7 @@ def do_test_with_log_level(log_level):
             temp directory used for build
     """
     fnames = [os.path.join(OUR_PATH, 'files', f)
-              for f in ['sample_ro.txt', 'sample_rw.txt']]
+              for f in samples or ['sample_ro.txt', 'sample_rw.txt']]
     zmk = zm.Zmake(jobserver=FakeJobserver(fnames))
 
     with LogCapture(level=log_level) as cap:
@@ -107,3 +109,12 @@ def test_filter_debug():
     # Both versions add the first 'Building' line above, with the temp dir
     expect = 321 + 318 + 2
     assert expect == len(recs)
+
+
+def test_filter_error():
+    """Test that devicetree errors appear"""
+    recs, tmpname = do_test_with_log_level(logging.ERROR,
+                                           ['sample_err.txt'] * 2)
+    uniq = set(recs)
+    assert 1 == len(uniq)
+    assert "devicetree error: 'adc' is marked as required" in list(uniq)[0]
