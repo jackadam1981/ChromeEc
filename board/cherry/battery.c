e5d93f2dd1c3fdaa7f30452fc5a8ca057e0b9447
Ting Shen <phoenixshen@google.com>
2021-04-06 16:54:38 +0800

    cherry: add battery
    BUG=b:181838424
    TEST=1) EC console prints "found batt:LGC KT0030G020"
         2) cutoff works
    BRANCH=main
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I903d0140e75a7877fa8ae6a8f04997f44989755b

--- board/cherry/battery.c
+++ board/cherry/battery.c
@@ -14,34 +14,63 @@
 #include "usb_pd.h"
 
 const struct board_batt_params board_battery_info[] = {
-	[BATTERY_C235] = {
+	[BATTERY_LGC_AP18C8K] = {
 		.fuel_gauge = {
-			.manuf_name = "AS3GWRc3KA",
-			.device_name = "C235-41",
+			.manuf_name = "LGC KT0030G020",
 			.ship_mode = {
-				.reg_addr = 0x0,
-				.reg_data = { 0x10, 0x10 },
+				.reg_addr = 0x3A,
+				.reg_data = { 0xC574, 0xC574 },
 			},
 			.fet = {
-				.reg_addr = 0x99,
-				.reg_mask = 0x0c,
-				.disconnect_val = 0x0c,
-			}
+				.reg_addr = 0x43,
+				.reg_mask = 0x0001,
+				.disconnect_val = 0x0,
+				.cfet_mask = 0x0002,
+				.cfet_off_val = 0x0000,
+			},
 		},
 		.batt_info = {
-			.voltage_max		= 8800,
-			.voltage_normal		= 7700,
-			.voltage_min		= 6000,
-			.precharge_current	= 256,
-			.start_charging_min_c	= 0,
-			.start_charging_max_c	= 45,
-			.charging_min_c		= 0,
-			.charging_max_c		= 60,
-			.discharging_min_c	= 0,
-			.discharging_max_c	= 60,
+			.voltage_max            = 13050,
+			.voltage_normal         = 11250,
+			.voltage_min            = 9000,
+			.precharge_current      = 256,
+			.start_charging_min_c   = 0,
+			.start_charging_max_c   = 50,
+			.charging_min_c         = 0,
+			.charging_max_c         = 60,
+			.discharging_min_c      = -20,
+			.discharging_max_c      = 75,
 		},
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_C235;
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_LGC_AP18C8K;
+
+static void reduce_input_voltage_when_full(void)
+{
+	struct batt_params batt;
+	int max_pd_voltage_mv;
+	int active_chg_port;
+
+	if (!system_is_locked())
+		return;
+
+	active_chg_port = charge_manager_get_active_charge_port();
+	if (active_chg_port == CHARGE_PORT_NONE)
+		return;
+
+	battery_get_params(&batt);
+	/* Lower our input voltage to 9V when battery is full. */
+	if (!(batt.flags & BATT_FLAG_BAD_STATUS) &&
+	    (batt.status & STATUS_FULLY_CHARGED) &&
+	    chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		max_pd_voltage_mv = 9000;
+	else
+		max_pd_voltage_mv = PD_MAX_VOLTAGE_MV;
+
+	if (pd_get_max_voltage() != max_pd_voltage_mv)
+		pd_set_external_voltage_limit(active_chg_port,
+					      max_pd_voltage_mv);
+}
+DECLARE_HOOK(HOOK_SECOND, reduce_input_voltage_when_full, HOOK_PRIO_DEFAULT);
