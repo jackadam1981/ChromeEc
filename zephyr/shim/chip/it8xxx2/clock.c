d7a0db150754c876f8ce50f4b0244eb0cf0c04ee
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2021-04-07 15:56:55 +0800

    zephyr: add clock for supporting it8xxx2 cros_kb_row driver
    Add clock for supporting it8xxx2 cros_kb_row driver.
    
    BUG=b:187192587
    BRANCH=none
    TEST=on hayato, check clock frequency is same as setting.
    
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>
    Change-Id: I9fd9f6ca5c6796ad9aee22a5cf7dc23564d2a814

---
+++ zephyr/shim/chip/it8xxx2/clock.c
@@ -0,0 +1,31 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <device.h>
+#include <drivers/clock_control.h>
+#include <kernel.h>
+#include <logging/log.h>
+#include <soc.h>
+#include <zephyr.h>
+
+#include "module_id.h"
+
+LOG_MODULE_REGISTER(shim_clock, LOG_LEVEL_ERR);
+
+static const int pll_to_freq[8] = {
+	8000000,
+	16000000,
+	24000000,
+	32000000,
+	48000000,
+	64000000,
+	72000000,
+	96000000
+};
+
+int clock_get_freq(void)
+{
+	return pll_to_freq[(IT8XXX2_ECPM_PLLFREQR & 0xf)];
+}
