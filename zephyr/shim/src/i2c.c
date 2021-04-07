d6ead8cc4a2d781d417e8bd5294fa22309a3c4f1
Yuval Peress <peress@chromium.org>
2021-04-07 00:17:55 -0600

    WIP: zephyr: init i2c_ports
    This isn't working yet, but it close. For some reason devicetree isn't
    recognizing the overlay nodes in the phandle. The errors look like:
    
    /named-i2c-ports/sensor: Reference to non-existent node or label
      "ec_i2c0_sensor_scl"
    
    BRANCH=none
    BUG=none
    TEST=none
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I7f46ab8f8b4079cf6ae5287869e6844fea38a100

--- zephyr/shim/src/i2c.c
+++ zephyr/shim/src/i2c.c
@@ -16,6 +16,19 @@
 	i2c_devices[I2C_PORT(id)] = device_get_binding( \
 		DT_PROP_BY_PHANDLE(id, i2c_port, label));
 
+#define I2C_PORT_INIT(id)                                                    \
+	COND_CODE_1(DT_NODE_HAS_PROP(id, kbps),                              \
+		    (                                                        \
+			    {                                                \
+				    .name = DT_LABEL(id),                    \
+				    .port = I2C_PORT(id),                    \
+				    .kbps = DT_PROP(id, kbps),               \
+				    .scl = DT_ENUM_UPPER_TOKEN(              \
+					    DT_PHANDLE(id, scl), enum_name), \
+				    .sda = DT_ENUM_UPPER_TOKEN(              \
+					    DT_PHANDLE(id, sda), enum_name), \
+			    }, ),                                            \
+		    ())
 /*
  * Long term we will not need these, for now they're needed to get things to
  * build since these extern symbols are usually defined in
@@ -24,7 +37,11 @@
  * Since all the ports will eventually be handled by device tree. This will
  * be removed at that point.
  */
-const struct i2c_port_t i2c_ports[] = {};
+const struct i2c_port_t i2c_ports[] = {
+#if DT_NODE_EXISTS(DT_PATH(named_i2c_ports))
+	DT_FOREACH_CHILD(DT_PATH(named_i2c_ports), I2C_PORT_INIT)
+#endif
+};
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
 
 int i2c_get_line_levels(int port)
