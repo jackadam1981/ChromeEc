3bae233992c7a46e76be58cfe6d9dee6945379d4
Daisuke Nojiri <dnojiri@chromium.org>
2021-04-09 15:09:44 -0700

    crc: Use zlib for host tools and tests
    Currently, ectool uses crc32 implemented in common/crc.c. This patch
    links ectool with the zlib. This patch also makes test/crc use the
    zlib for computing reference values.
    
    BUG=None
    BRANCH=None
    TEST=buildall
    TEST=BOARD=host make utils-host
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I02ae5dc8c352b41b9cb1f0d5d16189bc528fbf23

--- test/build.mk
+++ test/build.mk
@@ -250,3 +250,5 @@ static_if_error-y=static_if_error.o.cmd
 run-genvif_test:
 	@echo "  TEST    genvif_test"
 	@test/genvif/genvif.sh
+
+$(out)/crc.exe: HOST_TEST_LDFLAGS+=$(shell $(HOST_PKG_CONFIG) --libs zlib)
