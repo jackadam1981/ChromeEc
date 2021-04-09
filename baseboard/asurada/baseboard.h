0e5667907e03adc8b4f971f579a11d64b7140909
Eric Yilun Lin <yllin@chromium.org>
2021-04-09 14:16:35 +0800

    asurada: renames ADC_VBUS to ADC_VBUS_C0
    To make the naming clearer that's pointed to C0 port.
    
    BUG=none
    TEST=make buildall
    BRANCH=asurada
    
    Change-Id: If9cb520114b893af995aa67a2281b8e7302c2771
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- baseboard/asurada/baseboard.h
+++ baseboard/asurada/baseboard.h
@@ -55,7 +55,7 @@
 /* Charger */
 #define ADC_AMON_BMON ADC_CHARGER_AMON_R /* ADC name remap */
 #define ADC_PSYS ADC_CHARGER_PMON /* ADC name remap */
-#define ADC_VBUS ADC_VBUS_C0 /* ADC name remap */
+#define ADC_VBUS_C0 ADC_VBUS /* ADC name remap */
 #define CONFIG_CHARGE_MANAGER
 #define CONFIG_CHARGER
 #define CONFIG_CHARGE_RAMP_HW
@@ -197,7 +197,7 @@
 #include "registers.h"
 
 enum adc_channel {
-	ADC_VBUS,                /* ADC 0 */
+	ADC_VBUS_C0,             /* ADC 0 */
 	ADC_BOARD_ID_0,          /* ADC 1 */
 	ADC_BOARD_ID_1,          /* ADC 2 */
 	ADC_CHARGER_AMON_R,      /* ADC 3 */
