f1d9134c1681bb4bcba50494600af66633b1d156
Caveh Jalali <caveh@chromium.org>
2021-04-14 20:36:26 -0700

    WIP: ps8815: Add FRS support
    BRANCH=none
    BUG=b:183127346
    
    Change-Id: I949958e51512b096e55d405de9fc7db848e58ceb
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- driver/tcpm/ps8xxx.c
+++ driver/tcpm/ps8xxx.c
@@ -549,6 +549,24 @@ static int ps8xxx_enter_low_power_mode(int port)
 }
 #endif
 
+#ifdef CONFIG_USB_PD_FRS_TCPC
+static int ps8xxx_tcpc_fast_role_swap_enable(int port, int enable)
+{
+	if (IS_ENABLED(CONFIG_USB_PD_TCPM_PS8815)) {
+		int status;
+
+		status = tcpc_update8(port,
+				      PS8815_P1_REG_RESERVED_F4,
+				      PS8815_P1_REG_RESERVED_F4_FRS_EN,
+				      enable ? MASK_SET : MASK_CLR);
+		if (status != EC_SUCCESS)
+			return status;
+	}
+
+	return tcpci_tcpc_fast_role_swap_enable(port, enable);
+}
+#endif
+
 static int ps8xxx_dci_disable(int port)
 {
 	for (int i = 0; i < ARRAY_SIZE(variant_map); i++) {
@@ -619,6 +637,14 @@ static int ps8xxx_tcpm_init(int port)
 	if (IS_ENABLED(CONFIG_USB_PD_TCPM_PS8815)) {
 		ps8815_transmit_buffer_workaround_check(port);
 		ps8815_disable_rp_detect_workaround_check(port);
+
+		if (IS_ENABLED(CONFIG_USB_PD_FRS_TCPC)) {
+			status = tcpc_update8(port,
+					      PS8815_P1_REG_RESERVED_D1,
+					      PS8815_P1_REG_RESERVED_D1_FRS_EN,
+					      MASK_SET);
+			/* reg 0xf4 |= BIT(6) */
+		}
 	}
 
 	status = tcpci_tcpm_init(port);
@@ -739,6 +765,9 @@ const struct tcpm_drv ps8xxx_tcpm_drv = {
 	.enter_low_power_mode	= &ps8xxx_enter_low_power_mode,
 #endif
 	.set_bist_test_mode	= &tcpci_set_bist_test_mode,
+#ifdef CONFIG_USB_PD_FRS_TCPC
+	.set_frs_enable         = &ps8xxx_tcpc_fast_role_swap_enable,
+#endif
 };
 
 #ifdef CONFIG_CMD_I2C_STRESS_TEST_TCPC
