dab3d5275ce68c26a9e665da7e8f4cebd21c43f7
Caveh Jalali <caveh@chromium.org>
2021-04-14 20:37:27 -0700

    WIP: brya: Add FRS support
    BRANCH=none
    BUG=b:183127346
    
    Change-Id: I13971ed7e538009d3f40dfee3335fe2087745572
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- board/brya/usbc_config.c
+++ board/brya/usbc_config.c
@@ -55,7 +55,8 @@ const struct tcpc_config_t tcpc_config[] = {
 		},
 		.drv = &ps8xxx_tcpm_drv,
 		.flags = TCPC_FLAGS_TCPCI_REV2_0 |
-			 TCPC_FLAGS_TCPCI_REV2_0_NO_VSAFE0V,
+			 TCPC_FLAGS_TCPCI_REV2_0_NO_VSAFE0V |
+			 TCPC_FLAGS_FRS_GPIO,
 	},
 	[USBC_PORT_C2] = {
 		.bus_type = EC_BUS_TYPE_I2C,
@@ -444,3 +445,26 @@ __override bool board_is_dts_port(int port)
 {
 	return port == USBC_PORT_C0;
 }
+
+#ifdef CONFIG_USB_PD_FRS
+__override int board_pd_set_frs_enable(int port, int enable)
+{
+	if (port != USBC_PORT_C1)
+		return EC_SUCCESS;
+
+	switch (ec_cfg_usb_db_type()) {
+	case DB_USB3_PS8815:
+		if (get_board_id() != 1) {
+			/* Only board ID 1 needs GPIO control */
+			return EC_SUCCESS;
+		}
+		break;
+	default:
+		/* Other DBs do not need GPIO control */
+		return EC_SUCCESS;
+	}
+
+	gpio_set_level(GPIO_USB_C1_FRS_EN, enable);
+	return EC_SUCCESS;
+}
+#endif /* CONFIG_USB_PD_FRS */
