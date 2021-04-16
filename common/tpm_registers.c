2baf71496ba378e77f5e979d2511552ba7868222
Mary Ruthven <mruthven@chromium.org>
2021-04-16 22:30:14 +0000

    Reland "cr50: debounce successive TPM reset pulses"
    This reverts commit 9589af4d494a070ff02b4cd6fe7c33b1b2b40623.
    
    Reason for revert: Clear reset_in_progress after wiping the TPM. Cr50
    disables the tpm after wiping it. Clear reset_in_progress, so cr50
    doesn't get stuck thinking there's always a tpm reset in progress after
    wiping the tpm.
    
    BUG=b:164130916
    TEST=use rma_auth to open ccd. Verify subsequent tpm resets reset the
    TPM and don't say reset already in progress.
    
    Original change's description:
    > Revert "cr50: debounce successive TPM reset pulses"
    >
    > This reverts commit 4805196e894a73a2a1285f1cd622d160ad248f77.
    >
    > Reason for revert: We aren't planning on landing this in the branch.
    > Reduce the difference between TOT and the branch by reverting this.
    >
    > BUG=b:164130916
    > TEST=run ccd open with TOT image
    >
    > Original change's description:
    > > cr50: debounce successive TPM reset pulses
    > >
    > > Some platforms generate more than one pulse when resetting, many Intel
    > > SOCs generate two pulses, some other chips could go even higher.
    > >
    > > TPM reset on Cr50 is processed asynchronously, repetitive pulses
    > > result in multiple reset processing cycles.In case pulses are coming
    > > too soon one after another this could cause some race conditions.
    > >
    > > Let's ignore repetitive reset pulses unless there has been an attempt
    > > by the host to read a register (which is usually the very first action
    > > of the AP when booting up).
    > >
    > > BRANCH=cr50, cr50-mp
    > > BUG=none
    > > TEST=observed that only one reset is happening on an Octopus device,
    > >      while there are two pulses present on the PLT_RST_L line.
    > >
    > >      Verified proper reboot multiple times in a row.
    > >
    > > Change-Id: Ie1b124d41be0388bd8e12d0084827782de62cfa0
    > > Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1755059
    > > Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    >
    > Bug: none
    > Change-Id: Ifbe8afea87c522c94cf9ab73cca8a1489ec3956b
    > Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2485927
    
    Bug: b:164130916
    Bug: none
    Change-Id: I62717bf3f0e9bc6d9090122e5b15d13c8f4825e5

--- common/tpm_registers.c
+++ common/tpm_registers.c
@@ -436,9 +436,6 @@ void tpm_register_put(uint32_t regaddr, const uint8_t *data, uint32_t data_size)
 {
 	uint32_t i;
 
-	if (reset_in_progress)
-		return;
-
 	CPRINTF("%s(0x%03x, %d,", __func__, regaddr, data_size);
 	for (i = 0; i < data_size && i < 4; i++)
 		CPRINTF(" %02x", data[i]);
@@ -514,6 +511,8 @@ void tpm_register_get(uint32_t regaddr, uint8_t *dest, uint32_t data_size)
 	int i;
 	static uint32_t last_sts;
 	static uint32_t checked_sts;
+
+	reset_in_progress = 0;
 
 	if (regaddr != TPM_STS) {
 		CPRINTF("%s(0x%06x, %d)\n", __func__, regaddr, data_size);
@@ -819,6 +818,9 @@ int tpm_reset_request(int wait_until_done, int wipe_nvmem_first)
 
 	evt = task_wait_event_mask(TPM_EVENT_RESET, 5 * SECOND);
 
+	/* Don't debounce TPM resets from wiping the TPM. */
+	reset_in_progress = 0;
+
 	/* We were notified of completion */
 	if (evt & TPM_EVENT_RESET)
 		return wipe_result;
@@ -898,8 +900,6 @@ static void tpm_reset_now(int wipe_first)
 	 * do not stay disabled for more than 3 seconds.
 	 */
 	hook_call_deferred(&reinstate_nvmem_commits_data, 3 * SECOND);
-
-	reset_in_progress = 0;
 
 	if_start();
 }
