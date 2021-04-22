1e7043fa2aa29ad7f3c9bd84a5fc5f98f1b425e2
Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
2021-04-22 16:27:13 +0800

    hayato: Modify HS Detector threshold of PS8743 setting value
    Modify HS detector threshold setting (0x3C) to 0x60 for USB-C C1 port
    signal quality.
    
    BUG=b:177980418
    BRANCH=asurada
    TEST=manual
    Run command "ectool i2cread 8 4 0x20 0x3c" to check register value.
    
    Signed-off-by: Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
    Change-Id: I97b6bb16e7c5298ff42e35d936e0f9e60ec3b730

--- baseboard/asurada/baseboard.c
+++ baseboard/asurada/baseboard.c
@@ -260,14 +260,20 @@ BUILD_ASSERT(ARRAY_SIZE(usb_port_enable) == USB_PORT_COUNT);
 
 /* USB Mux */
 
+#ifdef BOARD_HAYATO
 void board_usb_mux_init(void)
 {
-	if (board_get_sub_board() == SUB_BOARD_TYPEC)
+	if (board_get_sub_board() == SUB_BOARD_TYPEC) {
 		ps8743_tune_usb_eq(&usb_muxes[1],
 				   PS8743_USB_EQ_TX_12_8_DB,
 				   PS8743_USB_EQ_RX_12_8_DB);
+		ps8743_write(&usb_muxes[1],
+				   PS8743_REG_HS_DET_THRESHOLD,
+				   PS8743_USB_HS_THRESH_NEG_10);
+	}
 }
 DECLARE_HOOK(HOOK_INIT, board_usb_mux_init, HOOK_PRIO_INIT_I2C + 1);
+#endif
 
 static int board_ps8743_mux_set(const struct usb_mux *me,
 				mux_state_t mux_state)
