e98cdea86f3be5c876610d03342961db4a817e84
Scott Collyer <scollyer@google.com>
2021-04-21 17:13:12 -0700

    usb-ep: Add config option to define VID
    This value was previously hardcoded to Google's VID. However, some
    products with USB-EP support may require a different VID to correctly
    support fwupd.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I23c36507a90428ab46cd5efde7d79581207bde74

--- include/config.h
+++ include/config.h
@@ -4582,6 +4582,11 @@
 #undef CONFIG_USB_PID
 
 /*
+ * USB Vendor ID used for USB-EP in STM32 chip driver
+ */
+#define CONFIG_STM32_USB_VID USB_VID_GOOGLE
+
+/*
  * Track overcurrent events for sinking partners coming from some component on
  * the board.  Auto-enabled for drivers which contain support for this feature.
  */
