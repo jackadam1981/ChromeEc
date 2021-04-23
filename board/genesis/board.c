eb82c5ab963acebef505cadbc3d367a27efffdd4
Joe Tessler <jrt@google.com>
2021-04-23 10:11:39 +0800

    [WIP] scout: Toggle 12V power rails on S0
    BUG=b:187078663
    
    Change-Id: I41cf4a93c21cc8bd78dfba497b322a5b6c67dad0

--- board/genesis/board.c
+++ board/genesis/board.c
@@ -366,6 +366,16 @@ void board_enable_s0_rails(int enable)
 {
 	/* This output isn't connected on protos; safe to set anyway. */
 	gpio_set_level(GPIO_EN_PP5000_HDMI, enable);
+
+	/*
+	 * Toggle scaler power and its downstream USB devices.
+	 */
+	gpio_set_level(GPIO_EC_SCALER_EN, enable);
+	gpio_set_level(GPIO_PWR_CTRL, enable);
+	gpio_set_level(GPIO_EC_MX8M_ONOFF, enable);
+	gpio_set_level(GPIO_EC_CAM_V3P3_EN, enable);
+
+	gpio_set_level(GPIO_PP3300_TPU_A_EN, enable);
 }
 
 int ec_config_get_usb4_present(void)
