57f6f4a148fdfe0ec03131f95be92caa617c77ba
Daisuke Nojiri <dnojiri@chromium.org>
2021-04-23 11:37:14 -0700

    Battery: Add command to export display SoC
    Currently, CrOS EC passes the battery remaining capacity (mAh) and the
    full capacity (mAh) through ACPI to the AP so that the host can
    calculate the battery SoC.
    
    The host further manipulates the SoC to get the display SoC, which is
    used to determine user visible behaviors. To get consistent behaviors
    in all power states, this change enables the EC to send the display
    SoC to the host via EC_CMD_DISPLAY_SOC command.
    
    The Powerd's part is I5bd1371f2569d21d55df1b50a3d709b98bbf0325.
    
    BUG=b:174433637, b:181506409, b:80270446, b:109954565
    BRANCH=dedede, trogdor, nami, hatch
    TEST=Storo, CoachZ
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: Idc6992625d992a73be141987d02ed220508d3b74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2853142
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    (cherry picked from commit b9719752392ddfdc6319a79932a05c4bc794b997)

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -6769,6 +6769,7 @@ struct ec_response_pchg_update {
 	uint32_t block_size;
 } __ec_align4;
 
+<<<<<<< HEAD   (104297 star: set up vivaldi keyboard layout)
 #define EC_CMD_SET_BASE_STATE 0x0138
 
 struct ec_params_set_base_state {
@@ -6780,6 +6781,17 @@ enum ec_set_base_state_cmd {
 	EC_SET_BASE_STATE_ATTACH,
 	EC_SET_BASE_STATE_RESET,
 };
+=======
+
+#define EC_CMD_DISPLAY_SOC 0x0137
+
+struct ec_response_display_soc {
+	int16_t display_soc;  /* Display charge in 10ths of a % (1000=100.0%) */
+	int16_t full_factor;  /* Full factor in 10ths of a % (1000=100.0%) */
+	int16_t shutdown_soc; /* Shutdown SoC in 10ths of a % (1000=100.0%) */
+} __ec_align2;
+
+>>>>>>> CHANGE (b97197 Battery: Add command to export display SoC)
 
 /*****************************************************************************/
 /* The command range 0x200-0x2FF is reserved for Rotor. */
