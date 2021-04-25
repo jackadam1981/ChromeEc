7e042f28455af571f55ebca8097320bb2410d33a
Craig Hesling <hesling@chromium.org>
2021-04-25 14:42:01 -0700

    dartmonkey,bloonchipper: Rename sleep lines to be more generic
    BRANCH=none
    BUG=b:175115925, b:179946521
    TEST=make BOARD=dartmonkey
    TEST=make BOARD=bloonchipper
    TEST=./util/ide-config.sh vscode all:RW all:RO \
           | tee .vscode/c_cpp_properties.json
    
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Change-Id: Iec3bebd87fe4f806389f78d7dfd8c29cebeb807f

--- board/hatch_fp/board_rw.c
+++ board/hatch_fp/board_rw.c
@@ -28,13 +28,14 @@ static bool broken_slp_s0;
 static void ap_deferred(void)
 {
 	/*
-	 * in S3:   SLP_S3_L is 0 and SLP_S0_L is X.
-	 * in S0ix: SLP_S3_L is 1 and SLP_S0_L is 0.
-	 * in S0:   SLP_S3_L is 1 and SLP_S0_L is 1.
+	 * On Legacy Intel:
+	 * in S3:   SLP_ALT_L is 0 and SLP_L is X.
+	 * in S0ix: SLP_ALT_L is 1 and SLP_L is 0.
+	 * in S0:   SLP_ALT_L is 1 and SLP_L is 1.
 	 * in S5/G3, the FP MCU should not be running.
 	 */
-	int running = gpio_get_level(GPIO_PCH_SLP_S3_L) &&
-		      (gpio_get_level(GPIO_PCH_SLP_S0_L) || broken_slp_s0);
+	int running = (gpio_get_level(GPIO_SLP_ALT_L) || broken_slp_s0) &&
+		      gpio_get_level(GPIO_SLP_L);
 
 	if (running) { /* S0 */
 		disable_sleep(SLEEP_MASK_AP_RUN);
@@ -92,8 +93,8 @@ void board_init_rw(void)
 	configure_fp_sensor_spi();
 
 	/* Enable interrupt on PCH power signals */
-	gpio_enable_interrupt(GPIO_PCH_SLP_S3_L);
-	gpio_enable_interrupt(GPIO_PCH_SLP_S0_L);
+	gpio_enable_interrupt(GPIO_SLP_ALT_L);
+	gpio_enable_interrupt(GPIO_SLP_L);
 
 	/* enable the SPI slave interface if the PCH is up */
 	hook_call_deferred(&ap_deferred_data, 0);
