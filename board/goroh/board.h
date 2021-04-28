3ee9fda903a3c3d63eba65345cec52e6900d95ab
Eric Yilun Lin <yllin@chromium.org>
2021-04-28 15:55:42 +0800

    goroh: configure GPIO
    Change-Id: I3a8097234e12150f74736ac24a44082204d6ef21

--- board/goroh/board.h
+++ board/goroh/board.h
@@ -93,8 +93,6 @@ enum adc_channel {
 	ADC_BOARD_ID_0,          /* ADC 1 */
 	ADC_BOARD_ID_1,          /* ADC 2 */
 	ADC_CHARGER_AMON_R,      /* ADC 3 */
-	ADC_VBUS_C1,             /* ADC 5 */
-	ADC_CHARGER_PMON,        /* ADC 6 */
 
 	/* Number of ADC channels */
 	ADC_CH_COUNT,
@@ -103,7 +101,8 @@ enum adc_channel {
 enum pwm_channel {
 	PWM_CH_LED1,
 	PWM_CH_LED2,
-	PWM_CH_LED3,
+	PWM_CH_FAN,
+	PWM_CH_KB_BL,
 	PWM_CH_COUNT,
 };
 
