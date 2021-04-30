8a6e6097b7445deff336282d21647e5525e95169
Patryk Duda <pdk@semihalf.com>
2021-04-30 09:01:46 +0000

    fpsensor: Return error on some host commands when TPM seed is not set
    When TPM seed is not set, then loading/obtaining template doesn't work,
    because encryption key can't be derived. Moreover, matching or enrolling
    finger under that circumstances doesn't make sense. In that cases we
    should return error code to inform upper layers that FPMCU entered bad
    state.
    
    BUG=b:172910094
    BRANCH=none
    TEST=Reboot FPMCU using ectool and check if calling affected host
         commands returns appropriate error code.
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: I8c5da917ed90766003257ec8701a44d55b9a2fc5

--- common/fpsensor/fpsensor_state.c
+++ common/fpsensor/fpsensor_state.c
@@ -133,6 +133,12 @@ int fp_tpm_seed_is_set(void)
 	return fp_encryption_status & FP_ENC_STATUS_SEED_SET;
 }
 
+static bool mode_uses_tpm_seed(uint32_t mode)
+{
+	return mode & (FP_MODE_ENROLL_SESSION | FP_MODE_ENROLL_IMAGE |
+		       FP_MODE_MATCH);
+}
+
 static enum ec_status
 fp_command_encryption_status(struct host_cmd_handler_args *args)
 {
@@ -158,6 +164,11 @@ static int validate_fp_mode(const uint32_t mode)
 
 	if (algo_mode & ~FP_VALID_MODES)
 		return EC_ERROR_INVAL;
+
+	if (mode_uses_tpm_seed(algo_mode) && !fp_tpm_seed_is_set()) {
+		CPRINTS("TPM seed is not provided");
+		return EC_ERROR_ACCESS_DENIED;
+	}
 
 	if ((mode & FP_MODE_ENROLL_SESSION) &&
 	    templ_valid >= FP_MAX_FINGER_COUNT) {
@@ -186,6 +197,19 @@ int fp_set_sensor_mode(uint32_t mode, uint32_t *mode_output)
 
 	ret = validate_fp_mode(mode);
 	if (ret != EC_SUCCESS) {
+		if (ret == EC_ERROR_ACCESS_DENIED) {
+			CPRINTS("Access denied for mode 0x%x", mode);
+			/*
+			 * EC_RES_UNAVAILABLE in FP codebase means that we are
+			 * in invalid state (eg. because FPMCU rebooted
+			 * unexpectedly) and the only way to recover from this
+			 * situation is Chromebook reboot. That's why it is
+			 * more suitable to return EC_RES_UNAVAILABLE instead
+			 * of EC_RES_ACCESS_DENIED here.
+			 */
+			return EC_RES_UNAVAILABLE;
+		}
+
 		CPRINTS("Invalid FP mode 0x%x", mode);
 		return EC_RES_INVALID_PARAM;
 	}
