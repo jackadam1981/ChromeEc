a13bbe7d54d37fe39f899d619b1c269a6ec266c7
Simon Glass <sjg@chromium.org>
2021-04-30 15:39:47 -0600

    util: Support using kconfiglib in the CONFIG checker
    Update this to use kconfiglib so that it might be easier to maintain.
    This needs a more correct Kconfig setup in the tests. Also we need to
    provide the root Kconfig instead of just the directory.
    
    Overall this takes about 50% more time, but the total is only about
    50ms, so this is not a significant factor in build performance.
    
    We must leave it optional, since U-Boot is unlikely to want this extra
    dependency.
    
    BUG=b:181323955
    BRANCH=none
    TEST=python3 util/test_kconfig_check.py
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: I5cce2ca1fa16a7c334cef15b1f66dd7ede2644bd

--- util/kconfig_check.py
+++ util/kconfig_check.py
@@ -6,12 +6,32 @@
 
 Checks that the .config file provided does not introduce any new ad-hoc CONFIG
 options
+
+This tool is also present in U-Boot, so we should keep the two in sync.
+
+The tool supports two formats for the 'configs' file:
+
+   CONFIG_SOMETHING=xx
+
+and
+
+   #define CONFIG_SOMETHING xx
+
+Use the -d flag to select the second format.
 """
 
 import argparse
 import os
 import re
 import sys
+
+# Try to use kconfiglib if available, but fall back to a simple recursive grep
+USE_KCONFIGLIB = False
+try:
+    import kconfiglib
+    USE_KCONFIGLIB = True
+except ImportError:
+    pass
 
 
 def parse_args(argv):
@@ -32,12 +52,16 @@ a corresponding Kconfig option for Zephyr"""
                         help='File containing list of allowed ad-hoc CONFIGs')
     parser.add_argument('-c', '--configs', type=str, default='.config',
                         help='File containing CONFIG options to check')
+    parser.add_argument('-d', '--use-defines', action='store_true',
+                        help='Lines in the configs file use #define')
     parser.add_argument(
         '-D', '--debug', action='store_true',
         help='Enabling debugging (provides a full traceback on error)')
+    parser.add_argument('-I', '--search-path', type=str, action='append',
+                        help='Search paths to look for Kconfigs')
     parser.add_argument('-p', '--prefix', type=str, default='PLATFORM_EC_',
                         help='Prefix to string from Kconfig options')
-    parser.add_argument('-s', '--srctree', type=str, default='.',
+    parser.add_argument('-s', '--srctree', type=str, default='zephyr/',
                         help='Path to source tree to look for Kconfigs')
 
     subparsers = parser.add_subparsers(dest='cmd', required=True)
@@ -86,17 +110,39 @@ class KconfigCheck:
         return sorted(list(set(configs) - set(kconfigs) - set(allowed)))
 
     @classmethod
-    def read_configs(cls, configs_file):
+    def read_configs(cls, configs_file, use_defines=False):
         """Read CONFIG options from a file
 
+        The file consists of a number of lines, each containing a CONFIG
+        option
+
         Args:
-            configs_file: Filename to read from
+            configs_file: Filename to read from (e.g. u-boot.cfg)
+            use_defines: True if each line of the file starts with #define
 
         Returns:
             List of CONFIG_xxx options found in the file, with the 'CONFIG_'
-                prefixremoved
+                prefix removed
         """
         with open(configs_file, 'r') as inf:
+            configs = re.findall('%sCONFIG_([A-Za-z0-9_]*)%s' %
+                                 ((use_defines and '#define ' or ''),
+                                  (use_defines and ' ' or '')),
+                                 inf.read())
+        return configs
+
+    @classmethod
+    def read_allowed(cls, allowed_file):
+        """Read allowed CONFIG options from a file
+
+        Args:
+            allowed_file: Filename to read from
+
+        Returns:
+            List of CONFIG_xxx options found in the file, with the 'CONFIG_'
+                prefix removed
+        """
+        with open(allowed_file, 'r') as inf:
             configs = re.findall('CONFIG_([A-Za-z0-9_]*)', inf.read())
         return configs
 
@@ -121,29 +167,47 @@ class KconfigCheck:
                 dirs.remove('Kconfig')
         return kconfig_files
 
-    def scan_kconfigs(self, srcdir, prefix=''):
+    @classmethod
+    def scan_kconfigs(cls, srcdir, prefix='', search_paths=None,
+                      try_kconfiglib=True):
         """Scan a source tree for Kconfig options
 
         Args:
-            srcdir: Directory to scan
+            srcdir: Directory to scan (containing top-level Kconfig file)
             prefix: Prefix to strip from the name (e.g. 'PLATFORM_EC_')
+            search_paths: List of project paths to search for Kconfig files, in
+                addition to the current directory
+            try_kconfiglib: Use kconfiglib if available
 
         Returns:
-            List of config and menuconfig options found,
+            List of config and menuconfig options found
         """
-        kconfigs = []
+        if USE_KCONFIGLIB and try_kconfiglib:
+            os.environ['srctree'] = srcdir
+            kconf = kconfiglib.Kconfig('Kconfig', warn=False,
+                                       search_paths=search_paths,
+                                       allow_empty_macros=True)
 
-        # Remove the prefix if present
-        expr = re.compile(r'(config|menuconfig) (%s)?([A-Za-z0-9_]*)\n' %
-                          prefix)
-        for fname in self.find_kconfigs(srcdir):
-            with open(fname) as inf:
-                found = re.findall(expr, inf.read())
-                kconfigs += [name for kctype, _, name in found]
-        return kconfigs
+            # There is always a MODULES config, since kconfiglib is designed for
+            # linux, but we don't want it
+            kconfigs = filter(lambda name: name != 'MODULES', kconf.syms.keys())
+
+            if prefix:
+                re_drop_prefix = re.compile(r'^%s' % prefix)
+                kconfigs = [re_drop_prefix.sub('', name) for name in kconfigs]
+        else:
+            kconfigs = []
+            # Remove the prefix if present
+            expr = re.compile(r'\n(config|menuconfig) (%s)?([A-Za-z0-9_]*)\n' %
+                              prefix)
+            for fname in cls.find_kconfigs(srcdir):
+                with open(fname) as inf:
+                    found = re.findall(expr, inf.read())
+                    kconfigs += [name for kctype, _, name in found]
+        return sorted(kconfigs)
 
     def find_new_adhoc_configs(self, configs_file, srcdir, allowed_file,
-                               prefix=''):
+                               prefix='', use_defines=False, search_paths=None):
         """Find new ad-hoc configs in the configs_file
 
         Args:
@@ -152,14 +216,18 @@ class KconfigCheck:
             allowed_file: File containing allowed CONFIG options
             prefix: Prefix to strip from the start of each Kconfig
                 (e.g. 'PLATFORM_EC_')
+            use_defines: True if each line of the file starts with #define
+            search_paths: List of project paths to search for Kconfig files, in
+                addition to the current directory
         """
-        configs = self.read_configs(configs_file)
-        kconfigs = self.scan_kconfigs(srcdir, prefix)
-        allowed = self.read_configs(allowed_file)
+        configs = self.read_configs(configs_file, use_defines)
+        kconfigs = self.scan_kconfigs(srcdir, prefix, search_paths)
+        allowed = self.read_allowed(allowed_file)
         new_adhoc = self.find_new_adhoc(configs, kconfigs, allowed)
         return new_adhoc
 
-    def do_check(self, configs_file, srcdir, allowed_file, prefix):
+    def do_check(self, configs_file, srcdir, allowed_file, prefix, use_defines,
+                 search_paths):
         """Find new ad-hoc configs in the configs_file
 
         Args:
@@ -168,12 +236,16 @@ class KconfigCheck:
             allowed_file: File containing allowed CONFIG options
             prefix: Prefix to strip from the start of each Kconfig
                 (e.g. 'PLATFORM_EC_')
+            use_defines: True if each line of the file starts with #define
+            search_paths: List of project paths to search for Kconfig files, in
+                addition to the current directory
 
         Returns:
             Exit code: 0 if OK, 1 if a problem was found
         """
-        new_adhoc = self.find_new_adhoc_configs(configs_file, srcdir,
-                                                allowed_file, prefix)
+        new_adhoc = self.find_new_adhoc_configs(
+            configs_file, srcdir, allowed_file, prefix, use_defines,
+            search_paths)
         if new_adhoc:
             print("""Error:\tThe EC is in the process of migrating to Zephyr.
 \tZephyr uses Kconfig for configuration rather than ad-hoc #defines.
@@ -201,7 +273,7 @@ def main(argv):
     checker = KconfigCheck()
     if args.cmd == 'check':
         return checker.do_check(args.configs, args.srctree, args.allowed,
-                                args.prefix)
+                                args.prefix, args.use_defines, args.search_path)
     return 2
 
 
