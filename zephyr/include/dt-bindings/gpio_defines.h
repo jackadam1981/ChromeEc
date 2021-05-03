e3f82a2a868cf58cf7a7e3889aafa0962ffb0c7e
Denis Brockus <dbrockus@google.com>
2021-05-03 12:13:54 -0600

    zephyr: gpio: Add GPIO pin voltage overrides
    BUG=b:186778858
    BRANCH=none
    TEST=verify driver sees override bits
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: I41e8fcf8d03f283af28ba0ff5ddac157af25b8fb

--- zephyr/include/dt-bindings/gpio_defines.h
+++ zephyr/include/dt-bindings/gpio_defines.h
@@ -1,7 +1,6 @@
-/*
- * Copyright 2020 Google LLC.
- *
- * SPDX-License-Identifier: Apache-2.0
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 #ifndef DT_BINDINGS_GPIO_DEFINES_H_
 #define DT_BINDINGS_GPIO_DEFINES_H_
