a7d80618a794286d199ecda2e386166937efad7e
Daisuke Nojiri <dnojiri@chromium.org>
2021-05-05 14:02:19 -0700

    Storo: Use display SoC to control charge LED
    Currently, Storo uses the state of charge provided from the battery.
    This isn't the same as the SoC shown on the screen because Powerd
    compensates it based on the full factor and the low battery shutdown
    threshold.
    
    This change makes Storo use the display SoC for the charge LED module
    so that the charge LED and the display SoC synchronously work.
    
    BUG=b:181506409
    BRANCH=dedede
    TEST=Verify LED color is amber when soc is 96% on Storo.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I42c505c9fbd39f82de2318f0d7ff8589eeca4d8c

--- common/led_onoff_states.c
+++ common/led_onoff_states.c
@@ -15,6 +15,7 @@
 #include "led_common.h"
 #include "led_onoff_states.h"
 #include "system.h"
+#include "util.h"
 
 #define CPRINTS(format, args...) cprints(CC_GPIO, format, ## args)
 
@@ -30,10 +31,13 @@ __overridable void led_set_color_battery(enum ec_led_colors color)
 {
 }
 
-#ifndef CONFIG_CHARGER
-/* Include for the sake of compilation */
-int charge_get_percent(void);
-#endif
+static int led_get_charge_percent(void)
+{
+	if (IS_ENABLED(CONFIG_BATTERY_EXPORT_DISPLAY_SOC))
+		return DIV_ROUND_NEAREST(charge_get_display_charge(), 10);
+	else
+		return charge_get_percent();
+}
 
 static enum led_states led_get_state(void)
 {
@@ -46,7 +50,7 @@ static enum led_states led_get_state(void)
 	switch (charge_get_state()) {
 	case PWR_STATE_CHARGE:
 		/* Get percent charge */
-		charge_lvl = charge_get_percent();
+		charge_lvl = led_get_charge_percent();
 		/* Determine which charge state to use */
 		if (charge_lvl < led_charge_lvl_1)
 			new_state = STATE_CHARGING_LVL_1;
@@ -70,7 +74,7 @@ static enum led_states led_get_state(void)
 	case PWR_STATE_DISCHARGE /* and PWR_STATE_DISCHARGE_FULL */:
 		if (chipset_in_state(CHIPSET_STATE_ON)) {
 #ifdef CONFIG_LED_ONOFF_STATES_BAT_LOW
-			if (charge_get_percent() <
+			if (led_get_charge_percent() <
 				CONFIG_LED_ONOFF_STATES_BAT_LOW)
 				new_state = STATE_DISCHARGE_S0_BAT_LOW;
 			else
