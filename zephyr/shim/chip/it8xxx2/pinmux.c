dd9d1ebf5e5da8f7e9d775c99652edfe1552eeea
tim <tim2.lin@ite.corp-partner.google.com>
2021-05-05 14:49:02 +0800

    zephyr: drivers: ADC enable ADC driver
    enable ADC and test on it8xxx2_evb
    
    BUG=b:185202623
    BRANCH=none
    TEST=shell: adc
    
    Signed-off-by: tim <tim2.lin@ite.corp-partner.google.com>
    Change-Id: I426dd6aebbe69ba86b2e7c57fb8e9216a76246f5

--- zephyr/shim/chip/it8xxx2/pinmux.c
+++ zephyr/shim/chip/it8xxx2/pinmux.c
@@ -7,26 +7,54 @@
 #include <drivers/pinmux.h>
 #include <soc.h>
 
-static int it8xxx2_evb_pinmux_init(const struct device *dev)
+static int it8xxx2_pinmux_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
-	ite_write(0xf02204, 1, 0xa5);
 
-	const struct device *p = DEVICE_DT_GET(DT_NODELABEL(pinmux));
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(pinmuxb), okay)
+	const struct device *portb = DEVICE_DT_GET(DT_NODELABEL(pinmuxb));
 
-	__ASSERT_NO_MSG(device_is_ready(p));
+	__ASSERT_NO_MSG(device_is_ready(portb));
+#endif
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(uart1), okay)
-	pinmux_pin_set(p, 0, IT8XXX2_PINMUX_IOF1);
-	pinmux_pin_set(p, 56, IT8XXX2_PINMUX_IOF1);
-#endif	/* DT_NODE_HAS_STATUS(DT_NODELABEL(uart1), okay) */
+	/* SIN0 */
+	pinmux_pin_set(portb, 0, IT8XXX2_PINMUX_FUNC_3);
+	/* SOUT0 */
+	pinmux_pin_set(portb, 1, IT8XXX2_PINMUX_FUNC_3);
+#endif
 
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart2), okay)
-	pinmux_pin_set(p, 3, IT8XXX2_PINMUX_IOF1);
-	pinmux_pin_set(p, 59, IT8XXX2_PINMUX_IOF1);
-#endif	/* DT_NODE_HAS_STATUS(DT_NODELABEL(uart2), okay) */
-	ite_write(0xf02205, 1, 0xa4);
 	return 0;
 }
+SYS_INIT(it8xxx2_pinmux_init, PRE_KERNEL_1, CONFIG_PINMUX_INIT_PRIORITY);
 
-SYS_INIT(it8xxx2_evb_pinmux_init, PRE_KERNEL_1, CONFIG_PINMUX_INIT_PRIORITY);
+/*
+ * Init priority is behind CONFIG_PLATFORM_EC_GPIO_INIT_PRIORITY to overwrite
+ * GPIO_INPUT setting of i2c ports.
+ */
+static int it8xxx2_pinmux_init_latr(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+
+	const struct device *porti = DEVICE_DT_GET(DT_NODELABEL(pinmuxi));
+
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(adc0), okay)
+	/* ADC 0 */
+	pinmux_pin_set(porti, 0, IT8XXX2_PINMUX_FUNC_1);
+	/* ADC 1 */
+	pinmux_pin_set(porti, 1, IT8XXX2_PINMUX_FUNC_1);
+	/* ADC 2 */
+	pinmux_pin_set(porti, 2, IT8XXX2_PINMUX_FUNC_1);
+	/* ADC 3 */
+	pinmux_pin_set(porti, 3, IT8XXX2_PINMUX_FUNC_1);
+	/* ADC 4 */
+	pinmux_pin_set(porti, 4, IT8XXX2_PINMUX_FUNC_1);
+	/* ADC 5 */
+	pinmux_pin_set(porti, 5, IT8XXX2_PINMUX_FUNC_1);
+#endif
+
+	return 0;
+}
+SYS_INIT(it8xxx2_pinmux_init_latr, POST_KERNEL, 52);
