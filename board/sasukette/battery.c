66f3827ee13a02603828c090a4ff77dd96d730f7
Mike Lee <mike5@huaqin.corp-partner.google.com>
2021-05-10 20:16:24 +0800

    Sasukette: add battery swelling function
    According to OEM request, add battery swelling function.
    1. enable CONFIG_BATTERY_CHECK_CHARGE_TEMP_LIMITS
    2. add battery swelling function
    
    BUG=b:187441287
    BRANCH=dedede
    TEST=make -j BOARD=sasukette
    TEST=test department test swelling function pass
    
    Signed-off-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Change-Id: Ie6dedff3df071e5abbece4a00133f1c5663acbf1

--- board/sasukette/battery.c
+++ board/sasukette/battery.c
@@ -58,7 +58,7 @@ const struct board_batt_params board_battery_info[] = {
 			.start_charging_min_c	= 0,
 			.start_charging_max_c	= 45,
 			.charging_min_c		= 0,
-			.charging_max_c		= 60,
+			.charging_max_c		= 50,
 			.discharging_min_c	= -20,
 			.discharging_max_c	= 70,
 		},
@@ -68,9 +68,118 @@ BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
 const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_SDI;
 
+static int swelling_flag = -1;
+static int prev_ac = -1;
+static int chargeInterruptflag = -1;
+
+#define Swelling_trigger_5 BIT(0)
+#define Swelling_trigger_15 BIT(1)
+#define Swelling_trigger_45 BIT(2)
+#define Swelling_trigger_50 BIT(3)
+#define Swelling_recovery_10 BIT(4)
+#define Swelling_recovery_20 BIT(5)
+#define Swelling_recovery_50 BIT(6)
+
 int charger_profile_override(struct charge_state_data *curr)
 {
-	curr->requested_voltage += 100;
+	int bat_temp_c = (curr->batt.temperature - 2731) / 10;
+
+	/*
+	 *	start charge temp control
+	 *
+	 *	if bat_temp >= 45 or bat_temp <= 0 when adapter plugging in,
+	 *	stop charge
+	 *	if 0 < bat_temp < 45 when adapter plugging in, charge normal
+	 */
+	if (curr->ac != prev_ac) {
+		if (curr->ac) {
+			if ((bat_temp_c <= 0) || (bat_temp_c >= 45))
+				chargeInterruptflag = 1;
+		}
+		prev_ac = curr->ac;
+	}
+
+	if (chargeInterruptflag) {
+		curr->requested_current = 0;
+		curr->requested_voltage = 0;
+		curr->batt.flags &= ~BATT_FLAG_WANT_CHARGE;
+		if (curr->state != ST_DISCHARGE)
+			curr->state = ST_IDLE;
+	}
+
+	if ((bat_temp_c > 0) && (bat_temp_c < 45))
+		chargeInterruptflag = 0;
+
+/*
+ *	battery swelling control
+ *
+ *	trigger condition		|	recovery condition
+ *	bat_temp < 5,			|	batt_temp >= 10,
+ *	bat_cell_voltage < 4.15		|	cv = (cell CV-50mv)*series
+ *					|	   = 8700mv
+ *	cv = 4150mv*series = 8300mv	|	cc = FCC*C-rate*0.4 = 1464ma
+ *	cc = FCC*C-rate*0.4		|
+ *					|
+ *	bat_temp < 15			|	batt_temp >= 20,
+ *	bat_cell_voltage < 4.15		|	cv = (cell CV-50mv)*series
+ *					|	   = 8700mv
+ *	cv = 4150mv*series = 8300mv	|	cc = FCC*C-rate*0.9 = 3294ma
+ *	cc = FCC*C-rate*0.4= 1464ma	|
+ *					|
+ *	bat_temp >= 45			|	batt_temp < 43,
+ *	cv = 4150mv*series = 8300mv	|	cv = (cell CV-50mv)*series
+ *					|	   = 8700mv
+ *	cc = FCC*C-rate*0.45= 1647ma	|	cc = FCC*C-rate*0.9 = 3294ma
+ *					|
+ *	bat_temp >= 50			|	batt_temp < 45,
+ *	stop charge			|	recovery charge
+ */
+	if ((curr->state == ST_CHARGE) && !chargeInterruptflag) {
+		if (curr->batt.voltage < 8300) {
+			if (bat_temp_c < 5)
+				swelling_flag = Swelling_trigger_5;
+			else if (bat_temp_c < 15)
+				swelling_flag = Swelling_trigger_15;
+
+			if (bat_temp_c >= 50)
+				swelling_flag = Swelling_trigger_50;
+			else if (bat_temp_c >= 45) {
+				if (!(swelling_flag & Swelling_trigger_50))
+					swelling_flag = Swelling_trigger_45;
+			}
+		}
+
+		if (swelling_flag) {
+			if ((bat_temp_c >= 10) && (bat_temp_c < 20))
+				swelling_flag = Swelling_recovery_10;
+			else if ((bat_temp_c >= 20) && (bat_temp_c < 43))
+				swelling_flag = Swelling_recovery_20;
+			else if ((bat_temp_c >= 43) && (bat_temp_c < 45))
+				swelling_flag = Swelling_recovery_50;
+		} else
+			curr->requested_voltage += 100;
+
+		if (swelling_flag & Swelling_trigger_5) {
+			curr->requested_voltage = 4150 * 2;
+			curr->requested_current = 5230 * 0.7 * 0.4;
+		} else if (swelling_flag & Swelling_trigger_15)
+			curr->requested_current = 5230 * 0.7 * 0.4;
+		else if (swelling_flag & Swelling_trigger_45) {
+			curr->requested_voltage = 4150 * 2;
+			curr->requested_current = 5230 * 0.7 * 0.45;
+		} else if (swelling_flag & Swelling_trigger_50) {
+			curr->requested_current = 0;
+			curr->requested_voltage = 0;
+			curr->batt.flags &= ~BATT_FLAG_WANT_CHARGE;
+			if (curr->state != ST_DISCHARGE)
+				curr->state = ST_IDLE;
+		} else if (swelling_flag & Swelling_recovery_10)
+			curr->requested_current = 5230 * 0.7 * 0.4;
+		else if (swelling_flag &
+			(Swelling_recovery_20|Swelling_recovery_50))
+			curr->requested_current = 5230 * 0.7 * 0.9;
+	} else
+		swelling_flag = 0;
 
 	return 0;
 }
