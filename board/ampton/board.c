coreboot/chrome-ec

commit b5950fc5e015333ab9d7ec359cd98e8ac0a97182

ampton: change sku id 3,4 for g-sensor BMA253 in the lid
The sku id 3,4 have never been manufactured. We use those id
to identify the sensor type in the lid.

BUG=b:153934973,b:154297511
BRANCH=octopus
TEST=Check the sensor work correctly.

Change-Id: Idf0b37fb0113e97a4927810cfaf9fbd1eb7a13a9
Signed-off-by: James_Chao <james_chao@asus.corp-partner.google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2172563
Reviewed-by: Marco Chen <marcochen@chromium.org>
Reviewed-by: Li-jen Chen <lijen@google.com>
Commit-Queue: Li-jen Chen <lijen@google.com>
Commit-Queue: Henry Sun <henrysun@google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2173934
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2183569
Reviewed-by: Henry Sun <henrysun@google.com>
Tested-by: Henry Sun <henrysun@google.com>

board/ampton/board.c
@@ -11,6 +11,7 @@
 #include "charge_state.h"
 #include "common.h"
 #include "cros_board_info.h"
+#include "driver/accel_bma2x2.h"
 #include "driver/accel_kionix.h"
 #include "driver/accelgyro_bmi160.h"
 #include "driver/ppc/sn5s330.h"
@@ -176,6 +177,38 @@ const mat33_fp_t gyro_standard_ref = {
 static struct kionix_accel_data g_kx022_data;
 static struct bmi160_drv_data_t g_bmi160_data;
 
+/* BMA253 private data */
+static struct accelgyro_saved_data_t g_bma253_data;
+
+static const struct motion_sensor_t motion_sensor_bma253 = {
+	.name = "Lid Accel",
+	.active_mask = SENSOR_ACTIVE_S0_S3,
+	.chip = MOTIONSENSE_CHIP_BMA255,
+	.type = MOTIONSENSE_TYPE_ACCEL,
+	.location = MOTIONSENSE_LOC_LID,
+	.drv = &bma2x2_accel_drv,
+	.mutex = &g_lid_mutex,
+	.drv_data = &g_bma253_data,
+	.port = I2C_PORT_SENSOR,
+	.addr = BMA2x2_I2C_ADDR2,
+	.rot_standard_ref = &lid_standard_ref,
+	.min_frequency = BMA255_ACCEL_MIN_FREQ,
+	.max_frequency = BMA255_ACCEL_MAX_FREQ,
+	.default_range = 2, /* g */
+	.config = {
+		/* EC use accel for angle detection */
+		[SENSOR_CONFIG_EC_S0] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 0,
+		},
+		/* Sensor on in S3 */
+		[SENSOR_CONFIG_EC_S3] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 0,
+		},
+	},
+};
+
 /* Drivers */
 struct motion_sensor_t motion_sensors[] = {
 	[LID_ACCEL] = {
@@ -266,11 +299,11 @@ static int board_is_convertible(void)
 	/* SKU IDs of Ampton & unprovisioned: 1, 2, 3, 4, 255 */
 	return sku_id == 1 || sku_id == 2 || sku_id == 3 || sku_id == 4
 		|| sku_id == 255;
-}
+	}
 
-static int board_with_ar_cam(void)
+static int board_with_sensor_bma253(void)
 {
-	/* SKU ID of Ampton with AR Cam: 3, 4 */
+	/* SKU ID 3 and 4 of Ampton with BMA253 */
 	return sku_id == 3 || sku_id == 4;
 }
 
@@ -278,6 +311,10 @@ static void board_update_sensor_config_from_sku(void)
 {
 	if (board_is_convertible()) {
 		motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+		if (board_with_sensor_bma253())
+			motion_sensors[LID_ACCEL] = motion_sensor_bma253;
+
 		/* Enable Base Accel interrupt */
 		gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
 	} else {
@@ -287,14 +324,6 @@ static void board_update_sensor_config_from_sku(void)
 		/* Base accel is not stuffed, don't allow line to float */
 		gpio_set_flags(GPIO_BASE_SIXAXIS_INT_L,
 			       GPIO_INPUT | GPIO_PULL_DOWN);
-	}
-
-	if (board_with_ar_cam()) {
-		/* Enable interrupt from camera */
-		gpio_enable_interrupt(GPIO_WFCAM_VSYNC);
-	} else {
-		/* Camera isn't stuffed, don't allow line to float */
-		gpio_set_flags(GPIO_WFCAM_VSYNC, GPIO_INPUT | GPIO_PULL_DOWN);
 	}
 }
 

board/ampton/board.h
@@ -31,6 +31,7 @@
 #define CONFIG_STEINHART_HART_3V3_13K7_47K_4050B
 #define CONFIG_TEMP_SENSOR_POWER_GPIO GPIO_EN_PP3300
 
+#define CONFIG_ACCEL_BMA255	/* Lid accel */
 #define CONFIG_ACCEL_KX022	/* Lid accel */
 #define CONFIG_ACCELGYRO_BMI160	/* Base accel */
 #define CONFIG_SYNC		/* Camera VSYNC */

board/ampton/gpio.inc
@@ -42,7 +42,6 @@ GPIO_INT(TABLET_MODE_L,		PIN(H, 4), GPIO_INT_BOTH, hall_sensor_isr)
 GPIO_INT(EC_VOLDN_BTN_ODL,	PIN(D, 6), GPIO_INT_BOTH, button_interrupt)
 GPIO_INT(EC_VOLUP_BTN_ODL,	PIN(D, 5), GPIO_INT_BOTH, button_interrupt)
 GPIO_INT(BASE_SIXAXIS_INT_L,	PIN(J, 2), GPIO_INT_FALLING | GPIO_SEL_1P8V, bmi160_interrupt)
-GPIO_INT(WFCAM_VSYNC,		PIN(D, 4), GPIO_INT_RISING, sync_interrupt)
 GPIO(LID_ACCEL_INT_L,		PIN(J, 3), GPIO_INPUT | GPIO_SEL_1P8V)
 
 GPIO(PCH_PLTRST_L,	 PIN(E, 3), GPIO_INPUT)		/* PLT_RST_L: Platform Reset from SoC */
@@ -113,6 +112,8 @@ GPIO(BAT_LED_WHITE,  PIN(A, 3), GPIO_OUT_LOW)		/* LED_2_EC */
 
 /* MKBP event synchronization */
 GPIO(EC_INT_L, PIN(E, 6), GPIO_ODR_HIGH)
+
+GPIO(WFCAM_VSYNC,		PIN(D, 4), GPIO_INPUT | GPIO_PULL_DOWN)
 
 UNIMPLEMENTED(KB_BL_PWR_EN)
 

driver/accel_bma2x2.h
@@ -23,7 +23,7 @@ extern const struct accelgyro_drv bma2x2_accel_drv;
 * BMA222E
 */
 #define BMA2x2_I2C_ADDR1                    0x30
-#define BMA2x2_I2C_ADDR2                    0x19
+#define BMA2x2_I2C_ADDR2                    0x32
 
 /* The following definition of I2C address is used for the following sensors
 * BMC150

jackadam1981/ChromeEc

commit 4a3f98501e3f7cc8ff4ea02670b1cfd4289c7cdc

board: ampton: Fully remove VSYNC sensor
VSYNC GPIO has been removed, so we can remove the whole driver.
It will not be presented by the EC and the kernel anymore.

Fixes: b5950fc5e015 ("ampton: change sku id 3,4 for g-sensor BMA253 in the lid")
BUG=b:153934973,b:154297511
BRANCH=octopus
TEST=compile
Signed-off-by: Gwendal Grignou <gwendal@google.com>
Change-Id: I59577e82e62c7ffd69297f02bf24c3fb4d325436

board/ampton/board.c
@@ -281,17 +281,6 @@ struct motion_sensor_t motion_sensors[] = {
 	 .min_frequency = BMI_GYRO_MIN_FREQ,
 	 .max_frequency = BMI_GYRO_MAX_FREQ,
 	},
-	[VSYNC] = {
-	 .name = "Camera VSYNC",
-	 .active_mask = SENSOR_ACTIVE_S0,
-	 .chip = MOTIONSENSE_CHIP_GPIO,
-	 .type = MOTIONSENSE_TYPE_SYNC,
-	 .location = MOTIONSENSE_LOC_CAMERA,
-	 .drv = &sync_drv,
-	 .default_range = 0,
-	 .min_frequency = 0,
-	 .max_frequency = 1,
-	},
 };
 
 unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
@@ -301,7 +290,7 @@ static int board_is_convertible(void)
 	/* SKU IDs of Ampton & unprovisioned: 1, 2, 3, 4, 255 */
 	return sku_id == 1 || sku_id == 2 || sku_id == 3 || sku_id == 4
 		|| sku_id == 255;
-	}
+}
 
 static int board_with_sensor_bma253(void)
 {

board/ampton/board.h
@@ -38,7 +38,6 @@
 #define CONFIG_ACCEL_BMA255	/* Lid accel */
 #define CONFIG_ACCEL_KX022	/* Lid accel */
 #define CONFIG_ACCELGYRO_BMI160	/* Base accel */
-#define CONFIG_SYNC		/* Camera VSYNC */
 
 #define CONFIG_DYNAMIC_MOTION_SENSOR_COUNT
 /* Sensors without hardware FIFO are in forced mode */
@@ -51,8 +50,6 @@
 
 #define CONFIG_ACCELGYRO_BMI160_INT_EVENT \
 	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
-#define CONFIG_SYNC_INT_EVENT \
-	TASK_EVENT_MOTION_SENSOR_INTERRUPT(VSYNC)
 
 /* Keyboard backlight is unimplemented in hardware */
 #undef CONFIG_PWM
@@ -88,7 +85,6 @@ enum sensor_id {
 	LID_ACCEL,
 	BASE_ACCEL,
 	BASE_GYRO,
-	VSYNC,
 	SENSOR_COUNT
 };
 