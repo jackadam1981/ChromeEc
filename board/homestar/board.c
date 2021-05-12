667861501cb51041914f519b9abbf69cd12fe6d4
Wai-Hong Tam <waihong@google.com>
2021-05-12 12:17:43 -0700

    sc7x80: Generalize the filenames for SC7x80
    The existing SC7180 power sequence can be reused on its next generation.
    Generalize the filenames, sc7180.c/sc7180.h to sc7x80.c/sc7x80.h.
    
    BRANCH=None
    BUG=b:187980397
    TEST=Built all the Chromium EC images and Zephyr EC images.
    
    Signed-off-by: Wai-Hong Tam <waihong@google.com>
    Change-Id: Ie04218ef0a12a4e8aa2db353040c5c39c533ae6f
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

--- board/homestar/board.c
+++ board/homestar/board.c
@@ -25,7 +25,7 @@
 #include "peripheral_charger.h"
 #include "pi3usb9201.h"
 #include "power.h"
-#include "power/sc7180.h"
+#include "power/sc7x80.h"
 #include "power_button.h"
 #include "pwm.h"
 #include "pwm_chip.h"
