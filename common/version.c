c0f9e1a676c55fddc555641fe7e44fba350e630c
Rob Barnes <robbarnes@google.com>
2021-05-13 12:27:56 -0600

    system: Add CrOS FWID to version output
    EC version is not aligned with the AP and OS version. This causes
    confusion during development. This change augments the EC version output
    to include the CrOS FWID when available. The CrOS FWID will be missing
    when the EC is built outside of cros_sdk.
    
    BUG=b:188073399
    TEST=version
    21-05-20 16:43:18.627 Chip:	Nuvoton NPCX993F A.00160101
    21-05-20 16:43:18.631 Board:	1
    21-05-20 16:43:18.631 RO:	guybrush_v2.0.8770+f47439f75
    21-05-20 16:43:18.634 		guybrush_13983.0.2021_05_20_163
    21-05-20 16:43:18.639 RW_A:	* guybrush_v2.0.8770+f47439f75
    21-05-20 16:43:18.641 		guybrush_13983.0.2021_05_20_163
    21-05-20 16:43:18.644 RW_B:	guybrush_v2.0.8770+f47439f75
    21-05-20 16:43:18.644 		guybrush_13983.0.2021_05_20_163
    21-05-20 16:43:18.647 Build:	guybrush_v2.0.8770+f47439f75
    21-05-20 16:43:18.651 		guybrush_13983.0.2021_05_20_1631 2021-05-20
    21-05-20 16:43:18.657 		16:31:19 robbarnes@robbarnes0
    ectool version
    RO version:    guybrush_v2.0.8770+f47439f75
    RO cros fwid:  guybrush_13983.0.2021_05_20_163
    RW version:    guybrush_v2.0.8770+f47439f75
    RW cros fwid:  guybrush_13983.0.2021_05_20_163
    Firmware copy: RO
    Build info:    guybrush_v2.0.8770+f47439f75
    guybrush_13983.0.2021_05_20_1631 2021-05-20 16:31:19 robbarnes@robbarnes0
    Tool version:  1.1.9999-f47439f  @robbarnes0
    BRANCH=None
    
    Signed-off-by: Rob Barnes <robbarnes@google.com>
    Change-Id: Ief0a0c6e9d35edc72ac2d4780ee203be41d7305f

--- common/version.c
+++ common/version.c
@@ -26,11 +26,18 @@ const struct image_data __keep current_image_data
 #endif
 	.rollback_version = CONFIG_ROLLBACK_VERSION,
 	.cookie2 = CROS_EC_IMAGE_DATA_COOKIE2,
+	.cros_fwid = CROS_FWID32,
 };
 BUILD_ASSERT(sizeof(CROS_EC_VERSION32) <= 32);
+BUILD_ASSERT(sizeof(CROS_FWID32) <= 32);
 
+#ifdef CONFIG_CROS_FWID_VERSION
+const char build_info[] __keep __attribute__((section(".rodata.buildinfo"))) =
+	VERSION " " CROS_FWID " " DATE " " BUILDER;
+#else
 const char build_info[] __keep __attribute__((section(".rodata.buildinfo"))) =
 	VERSION " " DATE " " BUILDER;
+#endif
 
 static int get_num_commits(const struct image_data *data)
 {
