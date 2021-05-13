f398cd9fe43f478c5f860897ebb90467016ec395
Wai-Hong Tam <waihong@google.com>
2021-05-13 10:11:39 -0700

    qcom: Deprecate AP_RST_REQ and WARM_RESET handlings in SC7280
    Add guards to limit the following handlings only in SC7180, such that
    the future generations, like SC7280, don't have them:
     * AP_RST_REQ (just informative power signal)
     * Long warm_reset handling to program AP flash properly
    
    BRANCH=None
    BUG=b:187980397, b:148246695, b:187098628
    TEST=Built all the Chromium EC images and Zephyr EC images.
    
    Change-Id: Iad011f58522641cde2f469f71114473476c53d67
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

--- power/qcom.c
+++ power/qcom.c
@@ -54,21 +54,23 @@ const struct power_signal_info power_signal_list[] = {
 		POWER_SIGNAL_ACTIVE_HIGH,
 		"POWER_GOOD",
 	},
-	[SC7X80_WARM_RESET] = {
-		GPIO_WARM_RESET_L,
-		POWER_SIGNAL_ACTIVE_HIGH,
-		"WARM_RESET_L",
-	},
 	[SC7X80_AP_SUSPEND] = {
 		GPIO_AP_SUSPEND,
 		POWER_SIGNAL_ACTIVE_HIGH,
 		"AP_SUSPEND",
+	},
+#ifdef CONFIG_CHIPSET_SC7180
+	[SC7X80_WARM_RESET] = {
+		GPIO_WARM_RESET_L,
+		POWER_SIGNAL_ACTIVE_HIGH,
+		"WARM_RESET_L",
 	},
 	[SC7X80_DEPRECATED_AP_RST_REQ] = {
 		GPIO_DEPRECATED_AP_RST_REQ,
 		POWER_SIGNAL_ACTIVE_HIGH,
 		"DEPRECATED_AP_RST_REQ",
 	},
+#endif /* defined(CONFIG_CHIPSET_SC7180) */
 };
 BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
 
@@ -255,6 +257,7 @@ static void request_cold_reset(void)
 	task_wake(TASK_ID_CHIPSET);
 }
 
+#ifdef CONFIG_CHIPSET_SC7180
 void chipset_warm_reset_interrupt(enum gpio_signal signal)
 {
 	/*
@@ -315,6 +318,7 @@ void chipset_power_good_interrupt(enum gpio_signal signal)
 	}
 	power_signal_interrupt(signal);
 }
+#endif /* defined(CONFIG_CHIPSET_SC7180) */
 
 static void sc7x80_lid_event(void)
 {
@@ -536,8 +540,10 @@ enum power_state power_chipset_init(void)
 	uint32_t reset_flags = system_get_reset_flags();
 
 	/* Enable interrupts */
-	gpio_enable_interrupt(GPIO_WARM_RESET_L);
-	gpio_enable_interrupt(GPIO_POWER_GOOD);
+	if (IS_ENABLED(CONFIG_CHIPSET_SC7180)) {
+		gpio_enable_interrupt(GPIO_WARM_RESET_L);
+		gpio_enable_interrupt(GPIO_POWER_GOOD);
+	}
 
 	/*
 	 * Force the AP shutdown unless we are doing SYSJUMP. Otherwise,
