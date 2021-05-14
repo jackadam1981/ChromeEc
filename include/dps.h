c618b15726709d776ee428e3e383c84fffc16005
Eric Yilun Lin <yllin@chromium.org>
2021-05-14 15:36:41 +0800

    usbpd: support dynamic PDO selection
    Suppot dynamic PDO selection CONFIG_USB_PD_DPS.
    This config controls the charging voltage and power according to the
    input power and battery configuration.
    
    DPS would continuously evaluates the system load and current charging
    voltage, and decide a new one by below:
    1. If the PDO can fulfill system desired power.
    2. If the PDO is efficient for the battery configuration.
    
    To detect if the system load cannot be fulfilled by the current PDO,
    it checks:
    1. if the input current closes to the PDO current limit.
    2. if the input power closes to the PDO maximum power.
    
    To detect if the system load can be fulfilled by a more efficient PDO,
    it checks:
    - if the voltage of a new PDO is closer to the battery voltage than the
       current PDO, and the power is able fulfill the system load.
    
    BUG=b:169532537
    TEST=tested on asurada, the charging voltage is able to switch to
         different PDOs under different system loads
    BRANCH=asurada
    
    Change-Id: I7c7706b331dc0d4f8ac68569dc7ed852fc9308e3
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

---
+++ include/dps.h
@@ -0,0 +1,74 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_DPS__H
+#define __CROS_EC_DPS__H
+
+#include <stdbool.h>
+
+#include "common.h"
+
+#define DPS_FLAG_DISABLED		BIT(0)
+#define DPS_FLAG_NO_SRCCAP		BIT(1)
+#define DPS_FLAG_WAITING		BIT(2)
+#define DPS_FLAG_SAMPLED		BIT(3)
+#define DPS_FLAG_NEED_MORE_PWR		BIT(4)
+
+#define DPS_FLAG_STOP_EVENTS		(DPS_FLAG_DISABLED | \
+					 DPS_FLAG_NO_SRCCAP)
+#define DPS_FLAG_ALL			GENMASK(31, 0)
+
+/* Dynamic PDO Selection config. */
+struct dps_config_t {
+	/* (0, 100) coeff for transition to a lower power PDO*/
+	int k_less_pwr;
+	/* (0, 100) coeff for transition to a higpower PDO*/
+	int k_more_pwr;
+	/* Number for how many the same consecutive sample to transist */
+	int k_sample;
+	/* Power stablied time after a new contract in us */
+	int t_stable;
+	/* Next power evluation time interval in us */
+	int t_check;
+	/*
+	 * If the current voltage is more efficient than the previous voltage
+	 *
+	 * @param curr_mv: current PDO voltage
+	 * @param prev_mv: previous PDO voltage
+	 * @param batt_mv: battery desired voltage
+	 * @param batt_mw: current battery power
+	 * @param input_mw: current adapter input power
+	 * @return true is curr_mv is more efficient otherwise false
+	 */
+	bool (*is_more_efficient)(int curr_mv, int prev_mv, int batt_mv,
+				  int batt_mw, int input_mw);
+};
+
+/*
+ * Get adaptive voltage in the current system load
+ *
+ * @param port: the evaluated port
+ *
+ * @return a voltage(mV) that the adapter supports to charge at the given port.
+ */
+int dps_get_adaptive_voltage(int port);
+
+/*
+ * Check if DPS is enabled.
+ *
+ * @return true if enabled, false otherwise.
+ */
+bool dps_is_enabled(void);
+
+/*
+ * Update DPS stablized timeout
+ *
+ * This is called at the exit of PE_SNK_TRANSITION_SINK
+ *
+ * @param port: the port for timer reset.
+ */
+void dps_update_stabilized_time(int port);
+
+#endif /* __CROS_EC_DPS__H */
