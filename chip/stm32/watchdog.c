665ab3b4cb0256ec9d1d94582aea178ce2eeebd8
Bossen WU <bossen.wu@stmicro.corp-partner.google.com>
2021-05-18 17:21:56 +0800

    stm32: add stm32l431 ec in chip/stm32
    stm32l431 is now working with makomo board.
    
    BRANCH=main
    BUG=b:188117811
    TEST=make BOARD=makomo
    
    Signed-off-by: Bossen WU <bossen.wu@stmicro.corp-partner.google.com>
    Change-Id: Idf335005d8188f6959835aa40179a6bd771c5114

--- chip/stm32/watchdog.c
+++ chip/stm32/watchdog.c
@@ -21,12 +21,21 @@
  * value large enough that we reload before the worst-case watchdog delay
  * (fastest LSI clock).
  */
+#ifdef CHIP_FAMILY_STM32L4
+#define LSI_CLOCK 32000
+#else
 #define LSI_CLOCK 56000
+#endif
+
+#define HAL_IWDG_DEFAULT_TIMEOUT ((6UL * 256UL * 1000UL) / LSI_CLOCK)
+
 
 /*
  * Use largest prescaler divider = /256.  This gives a worst-case watchdog
  * clock of 56000/256 = 218 Hz, and a maximum timeout period of (4095/218 Hz) =
  * 18.7 sec.
+ *
+ * For STM32L4, LSI is 32000. Watchdog clock is 32000 / 256 = 125Hz
  */
 #define IWDG_PRESCALER 6
 #define IWDG_PRESCALER_DIV (4 << IWDG_PRESCALER)
@@ -44,6 +53,36 @@ DECLARE_HOOK(HOOK_TICK, watchdog_reload, HOOK_PRIO_DEFAULT);
 
 int watchdog_init(void)
 {
+#ifdef	CHIP_FAMILY_STM32L4
+	timestamp_t tickstart, ticknow;
+
+	/* Enable watchdog registers */
+	STM32_IWDG_KR = STM32_IWDG_KR_START;
+
+	/*	Enable access to IWDG	*/
+	STM32_IWDG_KR = STM32_IWDG_KR_UNLOCK;
+
+	/* Set the prescaler between the LSI clock and the watchdog counter */
+	STM32_IWDG_PR = IWDG_PRESCALER & 7;
+
+	/* Set the reload value of the watchdog counter */
+	STM32_IWDG_RLR = MIN(STM32_IWDG_RLR_MAX, CONFIG_WATCHDOG_PERIOD_MS *
+			     (LSI_CLOCK / IWDG_PRESCALER_DIV) / 1000);
+
+	tickstart = get_time();
+	/*	Wait for SR */
+	while (STM32_IWDG_SR != 0x00u)	{
+		ticknow = get_time();
+		if ((ticknow.val - tickstart.val) >
+		    HAL_IWDG_DEFAULT_TIMEOUT * 1000) {
+			return EC_ERROR_TIMEOUT;
+		}
+	}
+
+	/* Reload the watchdog */
+	  STM32_IWDG_KR = STM32_IWDG_KR_RELOAD;
+
+#else
 	/* Unlock watchdog registers */
 	STM32_IWDG_KR = STM32_IWDG_KR_UNLOCK;
 
@@ -56,7 +95,7 @@ int watchdog_init(void)
 
 	/* Start the watchdog (and re-lock registers) */
 	STM32_IWDG_KR = STM32_IWDG_KR_START;
-
+#endif
 #ifdef CONFIG_WATCHDOG_HELP
 	/* Use a harder timer to warn about an impending watchdog reset */
 	hwtimer_setup_watchdog();
