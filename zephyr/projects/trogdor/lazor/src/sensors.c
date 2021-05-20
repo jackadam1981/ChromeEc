e9a2183b67b6818c1dac6ccfb2de549ed435809a
Dawid Niedzwiecki <dn@semihalf.com>
2021-05-20 14:48:37 +0200

    WIP: zephyr: lazor: Adjust motion sensor count
    Set motion_sensor_count to 0 for Limuzeen in runtime based on SKU.
    
    BUG=b:188498050
    BRANCH=none
    TEST=Verify the motion sensor count with 'accelinfo' command
    
    Signed-off-by: Dawid Niedzwiecki <dn@semihalf.com>
    Change-Id: I70a0e6af455ed5e4af92754a53cbe351b43552b7

--- zephyr/projects/trogdor/lazor/src/sensors.c
+++ zephyr/projects/trogdor/lazor/src/sensors.c
@@ -6,6 +6,10 @@
 #include "chipset.h"
 #include "common.h"
 #include "keyboard_scan.h"
+#include "motion_sense.h"
+#include "hooks.h"
+#include "tablet_mode.h"
+#include "sku.h"
 
 /* Lazor board specific sensor implementation */
 
@@ -27,3 +31,23 @@ void lid_angle_peripheral_enable(int enable)
 	}
 }
 #endif
+
+static void board_update_sensor_config_from_sku(void)
+{
+	if (board_is_clamshell()) {
+		motion_sensor_count = 0;
+		gmr_tablet_switch_disable();
+		/* The sensors are not stuffed; don't allow lines to float */
+		gpio_set_flags(GPIO_ACCEL_GYRO_INT_L,
+			       GPIO_INPUT | GPIO_PULL_DOWN);
+		gpio_set_flags(GPIO_LID_ACCEL_INT_L,
+			       GPIO_INPUT | GPIO_PULL_DOWN);
+	} else {
+		/* TODO: This part has to be done with non-Lizmuzeen board */
+//		board_detect_motionsensor();
+//		/* Enable interrupt for the base accel sensor */
+//		gpio_enable_interrupt(GPIO_ACCEL_GYRO_INT_L);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, board_update_sensor_config_from_sku,
+	     HOOK_PRIO_INIT_I2C + 2);
