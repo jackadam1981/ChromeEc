4e88a2fb2e26b6c4867f16db0d9ab510da5166fe
Ting Shen <phoenixshen@google.com>
2021-05-20 16:21:03 +0800

    TEST-ONLY: PP5000_USB_A0_VBUS experiment
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I80536b640b4f67a4c17a3da7b193c92ab2f7b3eb

--- board/asurada/board.c
+++ board/asurada/board.c
@@ -362,3 +362,9 @@ void board_usb_mux_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_INIT, board_usb_mux_init, HOOK_PRIO_INIT_I2C + 1);
+
+void usb_a0_interrupt(enum gpio_signal signal)
+{
+	gpio_set_level(GPIO_EN_PP5000_USB_A0_VBUS,
+		       gpio_get_level(signal));
+}
