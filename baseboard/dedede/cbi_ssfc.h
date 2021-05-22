7bfd59e0a32ea835dd7977f1e1b341b7f10c4d34
Mike Lee <mike5@huaqin.corp-partner.google.com>
2021-05-22 10:57:22 +0800

    storo: Add motion sensor config for lis2dwl/icm-426xx
    Add icm-426xx config for new second source base accel/gyro,
    and lis2dwl lid accel config for new second source.
    
    BUG=b:188373185,b:188373186
    BRANCH=dedede
    TEST=Using ectool 'motionsense' verified lid angle goes
    from 0 to 360 and swtiches to tablet mode after crossing 200
    threshold on re-work lis2dwl/icm-426xx DUT.
    
    Signed-off-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Change-Id: I2080816aeb0f5f542d773b8dbe219dbe6efd4226

--- baseboard/dedede/cbi_ssfc.h
+++ baseboard/dedede/cbi_ssfc.h
@@ -28,7 +28,8 @@ enum ec_ssfc_base_sensor {
 enum ec_ssfc_lid_sensor {
 	SSFC_SENSOR_LID_DEFAULT = 0,
 	SSFC_SENSOR_BMA255 = 1,
-	SSFC_SENSOR_KX022 = 2
+	SSFC_SENSOR_KX022 = 2,
+	SSFC_SENSOR_LIS2DWL = 3
 };
 
 /*
