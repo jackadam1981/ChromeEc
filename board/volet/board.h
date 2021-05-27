c8054ce62386bef0660dec1c3c138028111cda4d
Ben Chen <ben.chen2@quanta.corp-partner.google.com>
2021-05-27 14:04:35 +0800

    volet: remmove bb control, change tcpc config
    remove bb config, and modify tcpc c0/c1 Rt1715/ps8751 driver
    
    BUG=b:189402479
    BRANCH=cros/main
    TEST=make BOARD PASS, and power on test normal.
    
    Change-Id: If55e07a0de457e1e2bdb4c55b523bfefce7c7794
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- board/volet/board.h
+++ board/volet/board.h
@@ -111,6 +111,10 @@
 #define CONFIG_CHARGER_SENSE_RESISTOR		10
 #define CONFIG_CHARGER_SENSE_RESISTOR_AC	10
 
+/* Retimer */
+#undef CONFIG_USBC_RETIMER_INTEL_BB
+#undef CONFIG_USBC_RETIMER_INTEL_BB_RUNTIME_CONFIG
+
 /*
  * Macros for GPIO signals used in common code that don't match the
  * schematic names. Signal names in gpio.inc match the schematic and are
