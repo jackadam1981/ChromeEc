3bc9c1b3fff2befacf652cc17fe85ff49b8070c1
Daisuke Nojiri <dnojiri@chromium.org>
2021-05-31 13:30:24 -0700

    chgstv2: Sustain battery SoC
    This patch makes chgstv2 capable of sustaining the battery soc
    (a.k.a. sustain soc). The host can set the lower and upper thresholds
    between which the EC tries to keep the battery state of charge.
    
    The sustain soc is off on reset. It's enabled or disabled by the host
    using EC_CMD_CHARGE_CONTROL. For example, the host can disable it
    on every resume and re-enable it on every suspend.
    
    1. Shutdown and plug AC.
    
    > chgstate
    state_of_charge = 69%
    chg_ctl_mode = 0
    manual_voltage = -1
    manual_current = -1
    sustain soc = -1% ~ -1%
    
    > chgstate sustain 70 72
    state_of_charge = 69%
    chg_ctl_mode = 0
    manual_voltage = -1
    manual_current = -1
    sustain soc = 70% ~ 72%
    
    > battfake 71
    
    > chgstate
    state_of_charge = 71%
    chg_ctl_mode = 0
    
    > battfake 73
    
    > chgstate
    state_of_charge = 73%
    chg_ctl_mode = 2
    manual_voltage = 0
    manual_current = 0
    
    2. Unplug AC and EC keeps running.
    
    > chgstate
    state_of_charge = 73%
    chg_ctl_mode = 2
    manual_voltage = 0
    manual_current = 0
    
    3. Replug AC.
    
    > chgstate
    state_of_charge = 73%
    chg_ctl_mode = 2
    manual_voltage = 0
    manual_current = 0
    
    BUG=b:188457962
    BRANCH=None
    TEST=Atlas. See the description above.
    
    Change-Id: I62b4e8bc9517900a5a32d2f35369c645fa8a60c3
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -1669,6 +1669,24 @@ static void sustain_soc_disable(void)
 	sustain_soc_set(-1, -1);
 }
 
+static bool sustain_soc_enabled(void)
+{
+	return sustain_soc.lower != -1 && sustain_soc.upper != -1;
+}
+
+static void sustain_battery_soc(void)
+{
+	/* If either AC or battery is not present, nothing to do. */
+	if (!curr.ac || curr.batt.is_present != BP_YES
+			|| !sustain_soc_enabled())
+		return;
+
+	if (curr.batt.state_of_charge < sustain_soc.lower)
+		set_chg_ctrl_mode(CHARGE_CONTROL_NORMAL);
+	else if (sustain_soc.upper < curr.batt.state_of_charge)
+		set_chg_ctrl_mode(CHARGE_CONTROL_DISCHARGE);
+}
+
 /*****************************************************************************/
 /* Hooks */
 void charger_init(void)
@@ -1684,6 +1702,8 @@ void charger_init(void)
 	 * their tasks. Make them ready first.
 	 */
 	battery_get_params(&curr.batt);
+
+	sustain_soc_disable();
 }
 DECLARE_HOOK(HOOK_INIT, charger_init, HOOK_PRIO_DEFAULT);
 
@@ -2097,6 +2117,7 @@ wait_for_it:
 		    (is_full != prev_full) ||
 		    (curr.state != prev_state) ||
 		    (curr.batt.display_charge != prev_disp_charge)) {
+			sustain_battery_soc();
 			show_charging_progress();
 			prev_charge = curr.batt.state_of_charge;
 			prev_disp_charge = curr.batt.display_charge;
@@ -2877,6 +2898,7 @@ static int command_chgstate(int argc, char **argv)
 {
 	int rv;
 	int val;
+	char *e;
 
 	if (argc > 1) {
 		if (!strcasecmp(argv[1], "idle")) {
@@ -2911,6 +2933,20 @@ static int command_chgstate(int argc, char **argv)
 				return EC_ERROR_PARAM_COUNT;
 			if (!parse_bool(argv[2], &debugging))
 				return EC_ERROR_PARAM2;
+		} else if (!strcasecmp(argv[1], "sustain")) {
+			int lower, upper;
+
+			if (argc <= 3)
+				return EC_ERROR_PARAM_COUNT;
+			lower = strtoi(argv[2], &e, 0);
+			if (*e)
+				return EC_ERROR_PARAM2;
+			upper = strtoi(argv[3], &e, 0);
+			if (*e)
+				return EC_ERROR_PARAM3;
+			rv = sustain_soc_set(lower, upper);
+			if (rv)
+				return EC_ERROR_INVAL;
 		} else {
 			return EC_ERROR_PARAM1;
 		}
@@ -2920,7 +2956,8 @@ static int command_chgstate(int argc, char **argv)
 	return EC_SUCCESS;
 }
 DECLARE_CONSOLE_COMMAND(chgstate, command_chgstate,
-			"[idle|discharge|debug on|off]",
+			"[idle|discharge|debug on|off]"
+			"\n[sustain <lower> <upper>]",
 			"Get/set charge state machine status");
 
 #ifdef CONFIG_EC_EC_COMM_BATTERY_CLIENT
