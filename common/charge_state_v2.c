22d83aa28da4369ce830a7365a49df06eb7c3757
Daisuke Nojiri <dnojiri@chromium.org>
2021-05-31 13:30:24 -0700

    chgstv2: Add unit test for battery sustainer
    This patch adds a unit test for the battery sustainer.
    
    BUG=b:188457962
    BRANCH=None
    TEST=make run-sbs_charging_v2
    
    Change-Id: Ica227cf4ee3f71a746150fb6a5f4e40ab8ca0720
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2987734
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
    (cherry picked from commit 29f7ce6dc2e941bf00d1a0ee233a9b388a7bb0d5)

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -203,6 +203,11 @@ static void problem(enum problem_type p, int v)
 	problems_exist = 1;
 }
 
+test_export_static enum ec_charge_control_mode get_chg_ctrl_mode(void)
+{
+	return chg_ctl_mode;
+}
+
 static int battery_sustainer_set(int8_t lower, int8_t upper)
 {
 	if (lower == -1 || upper == -1) {
@@ -1088,6 +1093,13 @@ static void dump_charge_state(void)
 #define DUMP(FLD, FMT) ccprintf(#FLD " = " FMT "\n", curr.FLD)
 #define DUMP_CHG(FLD, FMT) ccprintf("\t" #FLD " = " FMT "\n", curr.chg. FLD)
 #define DUMP_BATT(FLD, FMT) ccprintf("\t" #FLD " = " FMT "\n", curr.batt. FLD)
+<<<<<<< HEAD   (254df9 kukui: extends charge_task stack size again)
+=======
+#define DUMP_OCPC(FLD, FMT) ccprintf("\t" #FLD " = " FMT "\n", curr.ocpc. FLD)
+
+	enum ec_charge_control_mode cmode = get_chg_ctrl_mode();
+
+>>>>>>> CHANGE (29f7ce chgstv2: Add unit test for battery sustainer)
 	ccprintf("state = %s\n", state_list[curr.state]);
 	DUMP(ac, "%d");
 	DUMP(batt_is_charging, "%d");
@@ -1121,8 +1133,13 @@ static void dump_charge_state(void)
 	DUMP(input_voltage, "%dmV");
 #endif
 	ccprintf("chg_ctl_mode = %s (%d)\n",
+<<<<<<< HEAD   (254df9 kukui: extends charge_task stack size again)
 		 chg_ctl_mode < CHARGE_CONTROL_COUNT
 			? mode_text[chg_ctl_mode] : "UNDEF", chg_ctl_mode);
+=======
+		 cmode < CHARGE_CONTROL_COUNT ? mode_text[cmode] : "UNDEF",
+		 cmode);
+>>>>>>> CHANGE (29f7ce chgstv2: Add unit test for battery sustainer)
 	ccprintf("manual_voltage = %d\n", manual_voltage);
 	ccprintf("manual_current = %d\n", manual_current);
 	ccprintf("user_current_limit = %dmA\n", user_current_limit);
@@ -1139,7 +1156,12 @@ static void dump_charge_state(void)
 
 static void show_charging_progress(void)
 {
+<<<<<<< HEAD   (254df9 kukui: extends charge_task stack size again)
 	int rv = 0, minutes, to_full;
+=======
+	int rv = 0, minutes, to_full, chgnum = 0;
+	int dsoc;
+>>>>>>> CHANGE (29f7ce chgstv2: Add unit test for battery sustainer)
 
 #ifdef CONFIG_BATTERY_SMART
 	/*
@@ -1173,19 +1195,17 @@ static void show_charging_progress(void)
 	}
 #endif
 
+	dsoc = charge_get_display_charge();
 	if (rv)
 		CPRINTS("Battery %d%% (Display %d.%d %%) / ??h:?? %s%s",
 			curr.batt.state_of_charge,
-			curr.batt.display_charge / 10,
-			curr.batt.display_charge % 10,
+			dsoc / 10, dsoc % 10,
 			to_full ? "to full" : "to empty",
 			is_full ? ", not accepting current" : "");
 	else
 		CPRINTS("Battery %d%% (Display %d.%d %%) / %dh:%d %s%s",
 			curr.batt.state_of_charge,
-			curr.batt.display_charge / 10,
-			curr.batt.display_charge % 10,
-			minutes / 60, minutes % 60,
+			dsoc / 10, dsoc % 10, minutes / 60, minutes % 60,
 			to_full ? "to full" : "to empty",
 			is_full ? ", not accepting current" : "");
 
@@ -1556,7 +1576,7 @@ const struct batt_params *charger_current_battery_params(void)
 
 static void sustain_battery_soc(void)
 {
-	enum ec_charge_control_mode mode = chg_ctl_mode;
+	enum ec_charge_control_mode mode = get_chg_ctrl_mode();
 	int soc;
 	int rv;
 
@@ -1567,6 +1587,7 @@ static void sustain_battery_soc(void)
 
 	soc = charge_get_percent();
 
+<<<<<<< HEAD   (254df9 kukui: extends charge_task stack size again)
 	switch (chg_ctl_mode) {
 	/*
 	 * When lower < upper, the sustainer discharges using DISCHARGE. When
@@ -1574,6 +1595,9 @@ static void sustain_battery_soc(void)
 	 * switch statement handle both cases but in reality either DISCHARGE
 	 * or IDLE is used but not both.
 	 */
+=======
+	switch (mode) {
+>>>>>>> CHANGE (29f7ce chgstv2: Add unit test for battery sustainer)
 	case CHARGE_CONTROL_NORMAL:
 		/* Going up */
 		if (sustain_soc.upper < soc)
@@ -1594,7 +1618,7 @@ static void sustain_battery_soc(void)
 		return;
 	}
 
-	if (mode == chg_ctl_mode)
+	if (mode == get_chg_ctrl_mode())
 		return;
 
 	rv = set_chg_ctrl_mode(mode);
@@ -1863,7 +1887,7 @@ void charger_task(void *u)
 		/* Okay, we're on AC and we should have a battery. */
 
 		/* Used for factory tests. */
-		if (chg_ctl_mode != CHARGE_CONTROL_NORMAL) {
+		if (get_chg_ctrl_mode() != CHARGE_CONTROL_NORMAL) {
 			set_charge_state(ST_IDLE);
 			goto wait_for_it;
 		}
@@ -1955,7 +1979,7 @@ void charger_task(void *u)
 
 wait_for_it:
 #ifdef CONFIG_CHARGER_PROFILE_OVERRIDE
-		if (chg_ctl_mode == CHARGE_CONTROL_NORMAL) {
+		if (get_chg_ctrl_mode() == CHARGE_CONTROL_NORMAL) {
 			sleep_usec = charger_profile_override(&curr);
 			if (sleep_usec < 0)
 				problem(PR_CUSTOM, sleep_usec);
@@ -2007,11 +2031,21 @@ wait_for_it:
 #endif
 		    (is_full != prev_full) ||
 		    (curr.state != prev_state) ||
+<<<<<<< HEAD   (254df9 kukui: extends charge_task stack size again)
 		    (curr.batt.display_charge != prev_disp_charge)) {
+=======
+		    (charge_get_display_charge() != prev_disp_charge)) {
+			sustain_battery_soc();
+>>>>>>> CHANGE (29f7ce chgstv2: Add unit test for battery sustainer)
 			show_charging_progress();
 			prev_charge = curr.batt.state_of_charge;
+<<<<<<< HEAD   (254df9 kukui: extends charge_task stack size again)
 			prev_disp_charge = curr.batt.display_charge;
 #ifdef CONFIG_EC_EC_COMM_BATTERY_MASTER
+=======
+			prev_disp_charge = charge_get_display_charge();
+#ifdef CONFIG_EC_EC_COMM_BATTERY_CLIENT
+>>>>>>> CHANGE (29f7ce chgstv2: Add unit test for battery sustainer)
 			prev_charge_base = charge_base;
 #endif
 			hook_notify(HOOK_BATTERY_SOC_CHANGE);
@@ -2347,7 +2381,7 @@ uint32_t charge_get_flags(void)
 {
 	uint32_t flags = 0;
 
-	if (chg_ctl_mode != CHARGE_CONTROL_NORMAL)
+	if (get_chg_ctrl_mode() != CHARGE_CONTROL_NORMAL)
 		flags |= CHARGE_FLAG_FORCE_IDLE;
 	if (curr.ac)
 		flags |= CHARGE_FLAG_EXTERNAL_POWER;
@@ -2368,7 +2402,7 @@ int charge_get_percent(void)
 	return is_full ? 100 : curr.batt.state_of_charge;
 }
 
-int charge_get_display_charge(void)
+test_mockable int charge_get_display_charge(void)
 {
 	return curr.batt.display_charge;
 }
@@ -2519,7 +2553,11 @@ charge_command_charge_control(struct host_cmd_handler_args *args)
 
 	if (args->version >= 2) {
 		if (p->cmd == EC_CHARGE_CONTROL_CMD_SET) {
+<<<<<<< HEAD   (254df9 kukui: extends charge_task stack size again)
 			if (p->mode == CHARGE_CONTROL_NORMAL) {
+=======
+			if (get_chg_ctrl_mode() == CHARGE_CONTROL_NORMAL) {
+>>>>>>> CHANGE (29f7ce chgstv2: Add unit test for battery sustainer)
 				rv = battery_sustainer_set(
 						p->sustain_soc.lower,
 						p->sustain_soc.upper);
@@ -2531,7 +2569,7 @@ charge_command_charge_control(struct host_cmd_handler_args *args)
 				battery_sustainer_disable();
 			}
 		} else if (p->cmd == EC_CHARGE_CONTROL_CMD_GET) {
-			r->mode = chg_ctl_mode;
+			r->mode = get_chg_ctrl_mode();
 			r->sustain_soc.lower = sustain_soc.lower;
 			r->sustain_soc.upper = sustain_soc.upper;
 			args->response_size = sizeof(*r);
@@ -2862,7 +2900,7 @@ int charge_get_charge_state_debug(int param, uint32_t *value)
 {
 	switch (param) {
 	case CS_PARAM_DEBUG_CTL_MODE:
-		*value = chg_ctl_mode;
+		*value = get_chg_ctrl_mode();
 		break;
 	case CS_PARAM_DEBUG_MANUAL_CURRENT:
 		*value = manual_current;
