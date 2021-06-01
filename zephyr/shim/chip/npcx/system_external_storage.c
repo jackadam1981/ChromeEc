cbd5ce1b8dddd14fd9046873acebeeb0de7b85e6
Yuval Peress <peress@chromium.org>
2021-05-31 22:57:41 -0600

    zephyr: npcx9: add support for flash download api workaround
    This is still a WIP change.
    
    BRANCH=none
    BUG=b:188605676
    TEST=none
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I80ddc717165788248f8260050fc55ffb0bd4d11e

--- zephyr/shim/chip/npcx/system_external_storage.c
+++ zephyr/shim/chip/npcx/system_external_storage.c
@@ -3,6 +3,8 @@
  * found in the LICENSE file.
  */
 
+#include <sys/__assert.h>
+
 #include "clock_chip.h"
 #include "common.h"
 #include "rom_chip.h"
@@ -20,6 +22,115 @@
 /* TODO(b:179900857) Clean this up too */
 #undef IS_BIT_SET
 #define IS_BIT_SET(reg, bit)        (((reg) >> (bit)) & (0x1))
+
+#if defined(CHIP_FAMILY_NPCX5) || defined(CONFIG_WORKAROUND_FLASH_DOWNLOAD_API)
+extern unsigned int __flash_lplfw_start;
+extern unsigned int __flash_lplfw_end;
+#define LFW_OFFSET		0x160
+#define CONFIG_LPRAM_BASE	0x40001400 /* memory address of lpwr ram */
+#define CONFIG_LPRAM_SIZE	0x00000620 /* 1568B low power ram */
+#define NPCX_PMC_BASE_ADDR	0x4000D000
+#define NPCX_GDMA_BASE_ADDR	0x40011000
+
+#define NPCX_PWDWN_CTL_ADDR(offset)    (((offset) < 6) ? \
+			(NPCX_PMC_BASE_ADDR + 0x008 + (offset)) : \
+			(NPCX_PMC_BASE_ADDR + 0x024))
+#define NPCX_PWDWN_CTL(offset)	REG8(NPCX_PWDWN_CTL_ADDR(offset))
+#define NPCX_GDMA_TCNT		REG32(NPCX_GDMA_BASE_ADDR + 0x00C)
+#define NPCX_LPRAM_CTRL		REG32(0x40001044)
+#define NPCX_GDMA_CTL		REG32(NPCX_GDMA_BASE_ADDR + 0x000)
+#define NPCX_GDMA_SRCB		REG32(NPCX_GDMA_BASE_ADDR + 0x004)
+#define NPCX_GDMA_DSTB		REG32(NPCX_GDMA_BASE_ADDR + 0x008)
+#define NPCX_GDMA_CTL_TC	18
+
+enum NPCX_PMC_PWDWN_CTL_T {
+	NPCX_PMC_PWDWN_1 = 0,
+	NPCX_PMC_PWDWN_2 = 1,
+	NPCX_PMC_PWDWN_3 = 2,
+	NPCX_PMC_PWDWN_4 = 3,
+	NPCX_PMC_PWDWN_5 = 4,
+	NPCX_PMC_PWDWN_6 = 5,
+	NPCX_PMC_PWDWN_7 = 6,
+	NPCX_PMC_PWDWN_CNT,
+};
+
+/* Begin address of Suspend RAM for little FW (GDMA utilities). */
+uintptr_t __lpram_lfw_start = CONFIG_LPRAM_BASE + LFW_OFFSET;
+
+static void system_download_from_flash(uint32_t srcAddr, uint32_t dstAddr,
+		uint32_t size, uint32_t exeAddr)
+{
+	int i;
+	uint8_t chunkSize = 16; /* 4 data burst mode. ie.16 bytes */
+	/*
+	 * GDMA utility in Suspend RAM. Setting bit 0 of address to indicate
+	 * it's a thumb branch for cortex-m series CPU.
+	 */
+	void (*__start_gdma_in_lpram)(uint32_t) =
+			(void(*)(uint32_t))(__lpram_lfw_start | 0x01);
+
+	/*
+	 * Before enabling burst mode for better performance of GDMA, it's
+	 * important to make sure srcAddr, dstAddr and size of transactions
+	 * are 16 bytes aligned in case failure occurs.
+	 */
+	__ASSERT_NO_MSG((size % chunkSize) == 0 && (srcAddr % chunkSize) == 0 &&
+			(dstAddr % chunkSize) == 0);
+
+	/* Check valid address for jumpiing */
+	__ASSERT_NO_MSG(exeAddr != 0x0);
+
+	/* Enable power for the Low Power RAM */
+	CLEAR_BIT(NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_6), 6);
+
+	/* Enable Low Power RAM */
+	NPCX_LPRAM_CTRL = 1;
+
+	/*
+	 * Initialize GDMA for flash reading.
+	 * [31:21] - Reserved.
+	 * [20]    - GDMAERR   = 0  (Indicate GMDA transfer error)
+	 * [19]    - Reserved.
+	 * [18]    - TC        = 0  (Terminal Count. Indicate operation is end.)
+	 * [17]    - Reserved.
+	 * [16]    - SOFTREQ   = 0  (Don't trigger here)
+	 * [15]    - DM        = 0  (Set normal demand mode)
+	 * [14]    - Reserved.
+	 * [13:12] - TWS.      = 10 (One double-word for every GDMA transaction)
+	 * [11:10] - Reserved.
+	 * [9]     - BME       = 1  (4-data ie.16 bytes - Burst mode enable)
+	 * [8]     - SIEN      = 0  (Stop interrupt disable)
+	 * [7]     - SAFIX     = 0  (Fixed source address)
+	 * [6]     - Reserved.
+	 * [5]     - SADIR     = 0  (Source address incremented)
+	 * [4]     - DADIR     = 0  (Destination address incremented)
+	 * [3:2]   - GDMAMS    = 00 (Software mode)
+	 * [1]     - Reserved.
+	 * [0]     - ENABLE    = 0  (Don't enable yet)
+	 */
+	NPCX_GDMA_CTL = 0x00002200;
+
+	/* Set source base address */
+	NPCX_GDMA_SRCB = CONFIG_MAPPED_STORAGE_BASE + srcAddr;
+
+	/* Set destination base address */
+	NPCX_GDMA_DSTB = dstAddr;
+
+	/* Set number of transfers */
+	NPCX_GDMA_TCNT = (size / chunkSize);
+
+	/* Clear Transfer Complete event */
+	SET_BIT(NPCX_GDMA_CTL, NPCX_GDMA_CTL_TC);
+
+	/* Copy the __start_gdma_in_lpram instructions to LPRAM */
+	for (i = 0; i < &__flash_lplfw_end - &__flash_lplfw_start; i++)
+		*((uint32_t *)__lpram_lfw_start + i) =
+				*(&__flash_lplfw_start + i);
+
+	/* Start GDMA in Suspend RAM */
+	__start_gdma_in_lpram(exeAddr);
+}
+#endif /* CHIP_FAMILY_NPCX5 || CONFIG_WORKAROUND_FLASH_DOWNLOAD_API */
 
 void system_jump_to_booter(void)
 {
