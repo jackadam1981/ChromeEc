054a7bf52e910d35afb6735331bd457f471fe255
udaykiran <udaykiran@google.com>
2021-06-02 12:12:57 -0700

    DP Altmode DFP: EC console command for configuring DP pin assignments.
    The USB Type-C Altmode allows for protocols other than USB to be
    transferred over a USB connection. Chromebooks use this functionality to
    transfer DisplayPort on USB signals. This is achieved through USB-PD
    handshake through SVDMs to discover, configure, and to enter or exit Alt
    modes. When DisplayPort as an Alt Mode is enabled, allowable
    functionalities are:
    -- SS and high speed USB functionality and two-lane DP.
    -- HS USB functionality and 4-lane DP.
    Chromebooks honor Multifunction bit set in the DPStatus message sent by
    a dock. However, for development purposes we would like to have control
    to honor the MF bit or ignore it, there by achieving 2-lane DP vs 4-lane
    DP functionality. 4-lane DP functionality is required to test higher
    resolution monitors such as 4k60.
    
    BUG=b:181365633
    BRANCH=none
    TEST=make BOARD, tested on G5 dock with kindred.
    
    Signed-off-by: udaykiran <udaykiran@google.com>
    Change-Id: Icc25f339a78d1423b094d2acf9d586721ec2df46

--- common/usb_pd_alt_mode_dfp.c
+++ common/usb_pd_alt_mode_dfp.c
@@ -46,6 +46,10 @@ int dp_flags[CONFIG_USB_PD_PORT_MAX_COUNT];
 
 uint32_t dp_status[CONFIG_USB_PD_PORT_MAX_COUNT];
 
+/* Console command multi-function preference set for a PD port. */
+
+int dp_port_mfmask[CONFIG_USB_PD_PORT_MAX_COUNT] = {0};
+
 __overridable const struct svdm_response svdm_rsp = {
 	.identity = NULL,
 	.svids = NULL,
@@ -190,7 +194,7 @@ int pd_dfp_dp_get_pin_mode(int port, uint32_t status)
 	pin_caps = PD_DP_PIN_CAPS(mode_caps);
 
 	/* if don't want multi-function then ignore those pin configs */
-	if (!PD_VDO_DPSTS_MF_PREF(status))
+	if (!PD_VDO_DPSTS_MF_PREF(status) || dp_port_mfmask[port])
 		pin_caps &= ~MODE_DP_PIN_MF_MASK;
 
 	/* TODO(crosbug.com/p/39656) revisit if DFP drives USB Gen 2 signals */
@@ -1215,7 +1219,8 @@ __overridable uint8_t get_dp_pin_mode(int port)
 
 static mux_state_t svdm_dp_get_mux_mode(int port)
 {
-	int mf_pref = PD_VDO_DPSTS_MF_PREF(dp_status[port]);
+	int mf_pref = PD_VDO_DPSTS_MF_PREF(dp_status[port]) &&
+			!dp_port_mfmask[port];
 	int pin_mode = get_dp_pin_mode(port);
 	/*
 	 * Multi-function operation is only allowed if that pin config is
@@ -1230,7 +1235,8 @@ static mux_state_t svdm_dp_get_mux_mode(int port)
 __overridable int svdm_dp_config(int port, uint32_t *payload)
 {
 	int opos = pd_alt_mode(port, TCPC_TX_SOP, USB_SID_DISPLAYPORT);
-	int mf_pref = PD_VDO_DPSTS_MF_PREF(dp_status[port]);
+	int mf_pref = PD_VDO_DPSTS_MF_PREF(dp_status[port]) &&
+			!dp_port_mfmask[port];
 	uint8_t pin_mode = get_dp_pin_mode(port);
 	mux_state_t mux_mode = svdm_dp_get_mux_mode(port);
 
@@ -1469,3 +1475,28 @@ const struct svdm_amode_fx supported_modes[] = {
 #endif /* CONFIG_USB_PD_TBT_COMPAT_MODE */
 };
 const int supported_modes_cnt = ARRAY_SIZE(supported_modes);
+
+static int command_mfoverride(int argc, char **argv)
+{
+	char *e;
+	int port;
+
+	if (argc < 3)
+		return EC_ERROR_PARAM_COUNT;
+
+	port = strtoi(argv[1], &e, 10);
+	if (*e || port >= board_get_usb_pd_port_count())
+		return EC_ERROR_PARAM2;
+
+	if (!strcasecmp(argv[2], "on"))
+		dp_port_mfmask[port] = 1;
+	else if (!strcasecmp(argv[2], "off"))
+		dp_port_mfmask[port] = 0;
+	else
+		return EC_ERROR_PARAM1;
+
+	ccprintf("Port: %d multi function override is %s ", port, argv[2]);
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(mfoverride, command_mfoverride, "port[0|1] [on | off]",
+		"Controls Multifunction choice during DP Altmode.");
