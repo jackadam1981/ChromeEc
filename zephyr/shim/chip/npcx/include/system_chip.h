4bb44291dc60d4a9cdccde6a456c0dd7bd11f40c
Yuval Peress <peress@chromium.org>
2021-06-03 00:25:31 -0600

    zephyr: npcx9: add __start_gdma function to .lowpower_ram2 section
    This function is needed for the flash api workaround.
    
    BRANCH=none
    BUG=b:188605676
    TEST=build brya
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I3ddd72e7664755f07bc967695b1502a43af3e57a

--- zephyr/shim/chip/npcx/include/system_chip.h
+++ zephyr/shim/chip/npcx/include/system_chip.h
@@ -6,6 +6,13 @@
 #ifndef __CROS_EC_SYSTEM_CHIP_H_
 #define __CROS_EC_SYSTEM_CHIP_H_
 
+#define SET_BIT(reg, bit)           ((reg) |= (0x1 << (bit)))
+#define CLEAR_BIT(reg, bit)         ((reg) &= (~(0x1 << (bit))))
+
+/* TODO(b:179900857) Clean this up too */
+#undef IS_BIT_SET
+#define IS_BIT_SET(reg, bit)        (((reg) >> (bit)) & (0x1))
+
 /*****************************************************************************/
 /* Memory mapping */
 #ifdef CONFIG_PLATFORM_EC_WORKAROUND_FLASH_DOWNLOAD_API
