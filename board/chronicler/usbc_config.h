894bc7777f4e9cf32493d4a38e7482715749f62c
Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
2021-06-03 19:12:50 +0800

    Chronicler: Implement LED behavior
    Chronicler have two charging leds on left side and right side.
    Each side have two colors amber and white.
    
    The led behavior define as following:
    1. Charging led: led on with charging port active, other port is off.
    2. Charging: Amber.
    3. Discharging: Off.
    4. Battery Error: Blinking white (0.4 sec on, 0.4 sec off)
    5. Fuel < 10%: Blinking white on right side port (1 sec on, 1 sec off)
    6. Force idle for factory: Blinking amber (1 sec on, 1 sec off)
    7. S0ix without charging state:
       Blinking both side LED white (1 sec on, 1 sec off)
    
    BUG=b:190019962
    BRANCH=volteer
    TEST=make sure led behavior intended.
    make sure ectool led left white/amber/off/auto work correctly.
    make sure ectool led right white/amber/off/auto work correctly.
    Signed-off-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    
    Change-Id: I90626a233fe0ec53a3ad9ecbb57f7b47de57ce18

--- board/chronicler/usbc_config.h
+++ board/chronicler/usbc_config.h
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-/* volteer board-specific USB-C configuration */
+/* Volteer board-specific USB-C configuration */
 
 #ifndef __CROS_EC_USBC_CONFIG_H
 #define __CROS_EC_USBC_CONFIG_H
