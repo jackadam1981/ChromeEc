09d1767101c4647b47370c49dc5f58c89edb727c
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-06-04 13:22:56 +0800

    primus: configurate I2C channel
    BUG=none
    BRANCH=none
    TEST=none
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Icb1e5e74a69811321d16c13745bda4b0a1fd9bef

--- board/primus/i2c.c
+++ board/primus/i2c.c
@@ -11,16 +11,16 @@
 const struct i2c_port_t i2c_ports[] = {
 	{
 		/* I2C1 */
-		.name = "tcpc0,2",
-		.port = I2C_PORT_USB_C0_C2_TCPC,
+		.name = "tcpc0",
+		.port = I2C_PORT_USB_C0_TCPC,
 		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_TCPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_TCPC_SDA,
+		.scl = GPIO_EC_I2C_USB_C0_TCPC_SCL,
+		.sda = GPIO_EC_I2C_USB_C0_TCPC_SDA,
 	},
 	{
 		/* I2C2 */
 		.name = "ppc0,2",
-		.port = I2C_PORT_USB_C0_C2_PPC,
+		.port = I2C_PORT_USB_C0_C2_PPC_BC,
 		.kbps = 1000,
 		.scl = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SCL,
 		.sda = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SDA,
@@ -34,28 +34,12 @@ const struct i2c_port_t i2c_ports[] = {
 		.sda = GPIO_EC_I2C_USB_C0_C2_RT_SDA,
 	},
 	{
-		/* I2C4 C1 TCPC */
-		.name = "tcpc1",
-		.port = I2C_PORT_USB_C1_TCPC,
-		.kbps = 400,
-		.scl = GPIO_EC_I2C_USB_C1_TCPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_TCPC_SDA,
-	},
-	{
 		/* I2C5 */
 		.name = "battery",
 		.port = I2C_PORT_BATTERY,
 		.kbps = 100,
 		.scl = GPIO_EC_I2C_BAT_SCL,
 		.sda = GPIO_EC_I2C_BAT_SDA,
-	},
-	{
-		/* I2C6 */
-		.name = "ppc1",
-		.port = I2C_PORT_USB_C1_PPC,
-		.kbps = 400,
-		.scl = GPIO_EC_I2C_USB_C1_MIX_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_MIX_SDA,
 	},
 	{
 		/* I2C7 */
