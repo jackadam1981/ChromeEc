fe343ef7f4ab524ba9fd5e787341150176fe7ec9
wen zhang <zhangwen6@huaqin.corp-partner.google.com>
2021-06-04 16:17:19 +0800

    Kakadu: change EC from STM32 to ITE
    just for DEBUG
    
    Change-Id: I489cadebd0b0a87134b7efb7507be95180f8e66d
    Signed-off-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>

--- board/kakadu/build.mk
+++ board/kakadu/build.mk
@@ -7,9 +7,9 @@
 #
 #
 # STmicro STM32F098VC
-CHIP:=stm32
-CHIP_FAMILY:=stm32f0
-CHIP_VARIANT:=stm32f09x
+CHIP:=it83xx
+CHIP_FAMILY:=it8xxx2
+CHIP_VARIANT:=it81202bx_1024
 BASEBOARD:=kukui
 
 board-y=board.o led.o
