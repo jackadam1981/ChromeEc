ff31c73e6db56b0b623ca2ea764a6a7f10100bbc
wen zhang <zhangwen6@huaqin.corp-partner.google.com>
2021-06-04 16:17:19 +0800

    Kakadu: change EC from STM32 to ITE
    just for DEBUG
    
    Change-Id: I489cadebd0b0a87134b7efb7507be95180f8e66d

--- board/kakadu/board.h
+++ board/kakadu/board.h
@@ -13,19 +13,20 @@
 #define VARIANT_KUKUI_POGO_KEYBOARD
 
 #define VARIANT_KUKUI_CHARGER_MT6370
-#define VARIANT_KUKUI_EC_STM32F098
+#define VARIANT_KUKUI_EC_IT81202
 #define VARIANT_KUKUI_TABLET_PWRBTN
 
-#ifndef SECTION_IS_RW
-#define VARIANT_KUKUI_NO_SENSORS
-#endif /* SECTION_IS_RW */
-
 #include "baseboard.h"
+
+/* TODO: remove me once we fix IT83XX_ILM_BLOCK_SIZE out of space issue */
+#undef CONFIG_LTO
 
 #define CONFIG_USB_MUX_IT5205
 #define CONFIG_VOLUME_BUTTONS
 #define CONFIG_USB_MUX_RUNTIME_CONFIG
 
+#undef CONFIG_USB_PD_ITE_ACTIVE_PORT_COUNT
+#define CONFIG_USB_PD_ITE_ACTIVE_PORT_COUNT 0
 /* Battery */
 #define BATTERY_DESIRED_CHARGING_CURRENT    3500  /* mA */
 
@@ -33,7 +34,6 @@
 
 
 /* Motion Sensors */
-#ifdef SECTION_IS_RW
 #define CONFIG_ACCELGYRO_BMI160
 #define CONFIG_ACCEL_INTERRUPTS
 #define CONFIG_ACCELGYRO_BMI160_INT_EVENT \
@@ -47,16 +47,15 @@
 #define CONFIG_SYNC_COMMAND
 #define CONFIG_SYNC_INT_EVENT \
 	TASK_EVENT_MOTION_SENSOR_INTERRUPT(VSYNC)
-#endif /* SECTION_IS_RW */
 
 /* I2C ports */
-#define I2C_PORT_CHARGER  0
-#define I2C_PORT_TCPC0    0
-#define I2C_PORT_USB_MUX  0
-#define I2C_PORT_BATTERY  1
+#define I2C_PORT_CHARGER  IT83XX_I2C_CH_C
+#define I2C_PORT_TCPC0    IT83XX_I2C_CH_C
+#define I2C_PORT_USB_MUX  IT83XX_I2C_CH_C
+#define I2C_PORT_BATTERY  IT83XX_I2C_CH_B
 #define I2C_PORT_VIRTUAL_BATTERY I2C_PORT_BATTERY
-#define I2C_PORT_ACCEL    1
-#define I2C_PORT_BC12     1
+#define I2C_PORT_ACCEL    IT83XX_I2C_CH_B
+#define I2C_PORT_BC12     IT83XX_I2C_CH_B
 
 /* Route sbs host requests to virtual battery driver */
 #define VIRTUAL_BATTERY_ADDR_FLAGS 0x0B
@@ -112,7 +111,7 @@ enum charge_port {
 
 #ifdef SECTION_IS_RO
 /* Interrupt handler for emmc task */
-void emmc_cmd_interrupt(enum gpio_signal signal);
+void emmc_ap_jump_to_bl(enum gpio_signal signal);
 #endif
 
 void board_reset_pd_mcu(void);
