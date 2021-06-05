9aab6bffab1e491dec9434da6395468aaa4a2c37
Mike Lee <mike5@huaqin.corp-partner.google.com>
2021-06-05 18:04:59 +0800

    driver: add DA217 driver support
    Add DA217 accel driver code.
    
    BUG=b:190065382
    BRANCH=None
    TEST=ectool motionsense
    
    Signed-off-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Change-Id: I2e38a3bfd8946f99814c3d686c29608c005af410

--- common/build.mk
+++ common/build.mk
@@ -27,6 +27,7 @@ common-$(CONFIG_ACCEL_LIS2DH)+=math_util.o
 common-$(CONFIG_ACCEL_LIS2DS)+=math_util.o
 common-$(CONFIG_ACCEL_KXCJ9)+=math_util.o
 common-$(CONFIG_ACCEL_KX022)+=math_util.o
+common-$(CONFIG_ACCEL_DA217)+=math_util.o
 ifneq ($(CORE),cortex-m)
 common-$(CONFIG_AES)+=aes.o
 endif
