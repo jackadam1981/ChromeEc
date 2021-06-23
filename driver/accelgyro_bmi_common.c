ff8cc8bd31efb0506c3bb21fa766026fe7e24f00
Bhanu Prakash Maiya <bhanumaiya@google.com>
2021-06-22 21:33:33 -0700

    driver: bma4xx: Add support for BMA422 sensor
    BMA422 is one of BMA4XX series accel sensor series.
    Adding defines, driver from Bosch APIs based initial patches
    submitted by Bosch team members in crrev/c/2894333.
    Reference code can be found on
    https://github.com/BoschSensortec/BMA423-Sensor-API.
    
    BRANCH=none
    BUG=b:178400750
    TEST=Accel implementation tested on Guybrush
         EC commands:
         > accelinfo
         > acceldata
    
    Signed-off-by: Bhanu Prakash Maiya <bhanumaiya@chromium.org>
    Change-Id: I8117283e54980379989fb01f68c29b7d6c501eca

--- driver/accelgyro_bmi_common.c
+++ driver/accelgyro_bmi_common.c
@@ -23,8 +23,9 @@
 #define CPRINTF(format, args...) cprintf(CC_ACCEL, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_ACCEL, format, ## args)
 
-#if !defined(CONFIG_ACCELGYRO_BMI160) && !defined(CONFIG_ACCELGYRO_BMI260)
-#error "Must use either BMI160 or BMI260"
+#if !defined(CONFIG_ACCELGYRO_BMI160) && !defined(CONFIG_ACCELGYRO_BMI260) \
+&& !defined(CONFIG_ACCEL_BMA422)
+#error "Must use any of the following: BMI160 BMI260 BMA422"
 #endif
 
 #if defined(CONFIG_ACCELGYRO_BMI260) && !defined(CONFIG_ACCELGYRO_BMI160)
