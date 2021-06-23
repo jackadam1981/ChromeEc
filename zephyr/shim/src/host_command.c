0fb430bae8d79c6a6b76bf8ec0a953e32c3e554e
Fabio Baltieri <fabiobaltieri@google.com>
2021-06-23 16:56:29 +0000

    WIP: zephyr: shim: rework ec wrapper init functions
    Change-Id: Ia240fab90c68aad2a87c4923c340a3a52d10c346

--- zephyr/shim/src/host_command.c
+++ zephyr/shim/src/host_command.c
@@ -7,21 +7,17 @@
 
 static struct zshim_host_command_node *host_command_head;
 
-void zshim_setup_host_command(
-	int command,
-	enum ec_status (*routine)(struct host_cmd_handler_args *args),
-	int version_mask, struct zshim_host_command_node *entry)
+int zshim_setup_host_command(struct zshim_host_command_node *entry)
 {
 	struct zshim_host_command_node **loc = &host_command_head;
 
 	/* Setup the entry */
-	entry->cmd->handler = routine;
-	entry->cmd->command = command;
-	entry->cmd->version_mask = version_mask;
 	entry->next = *loc;
 
 	/* Insert the entry */
 	*loc = entry;
+
+	return 0;
 }
 
 struct host_command *zephyr_find_host_command(int command)
@@ -29,8 +25,8 @@ struct host_command *zephyr_find_host_command(int command)
 	struct zshim_host_command_node *p;
 
 	for (p = host_command_head; p != NULL; p = p->next) {
-		if (p->cmd->command == command)
-			return p->cmd;
+		if (p->cmd.command == command)
+			return &p->cmd;
 	}
 
 	return NULL;
