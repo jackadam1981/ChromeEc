128d8b9d87b6b86b09f33d132777578d09333373
Jes B. Klinke <jbk@chromium.org>
2021-06-30 12:48:56 -0700

    WIP: First crude attempt at supporting STM32L5xx and HyperDebug
    BUG=b:192262089
    TEST=None yet
    BRANCH=none
    
    Signed-off-by: Jes B. Klinke <jbk@opentitan.org>
    Change-Id: I7edeb3df0587dd0bd01ae604f65cd1b574fe8e9a

--- chip/stm32/usb-stm32l.c
+++ chip/stm32/usb-stm32l.c
@@ -14,13 +14,13 @@ void usb_connect(void)
 	/* USB is in use */
 	disable_sleep(SLEEP_MASK_USB_DEVICE);
 
-	STM32_SYSCFG_PMC |= 1;
+	/*XXX STM32_SYSCFG_PMC |= 1;*/
 }
 
 void usb_disconnect(void)
 {
 	/* disable pull-up on DP to disconnect */
-	STM32_SYSCFG_PMC &= ~1;
+	/*XXX STM32_SYSCFG_PMC &= ~1;*/
 
 	/* USB is off, so sleep whenever */
 	enable_sleep(SLEEP_MASK_USB_DEVICE);
