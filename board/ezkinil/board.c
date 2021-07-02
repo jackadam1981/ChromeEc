63a023a7712b419313e27115d0771d3f41c8ece4
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2021-07-02 10:10:19 +0800

    Ezkinil: Add USB MUX PS8818 for TYPEC C1
    Use SSFC bits 6-7 to choose which secondary MUX is used
    for the sku with HDMI on DB.
    
    BUG=b:192523667
    BRANCH=zork
    TEST=none
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: I8a66098d1e9b947acfb26b78f0cec7f835bf4c40

--- board/ezkinil/board.c
+++ board/ezkinil/board.c
@@ -6,6 +6,7 @@
 #include "adc.h"
 #include "adc_chip.h"
 #include "button.h"
+#include "cbi_ssfc.h"
 #include "charge_state_v2.h"
 #include "cros_board_info.h"
 #include "driver/accelgyro_bmi_common.h"
@@ -336,10 +337,11 @@ void motion_interrupt(enum gpio_signal signal)
  * USB-C MUX/Retimer dynamic configuration
  */
 
+int board_usbc1_retimer_inhpd = IOEX_USB_C1_HPD_IN_DB;
+
 static void setup_mux(void)
 {
-	if (ec_config_has_usbc1_retimer_tusb544()) {
-		ccprints("C1 TUSB544 detected");
+	if (ec_config_has_usbc1_retimer_on_hdmi_db()) {
 		/*
 		 * Main MUX is FP5, secondary MUX is TUSB544
 		 *
@@ -349,8 +351,16 @@ static void setup_mux(void)
 		memcpy(&usb_muxes[USBC_PORT_C1],
 		       &usbc1_amd_fp5_usb_mux,
 		       sizeof(struct usb_mux));
-		/* Set the TUSB544 as the secondary MUX */
-		usb_muxes[USBC_PORT_C1].next_mux = &usbc1_tusb544;
+
+		if (get_cbi_ssfc_c1_usb_mux() == ec_ssfc_c1_usb_mux_ps8818) {
+			ccprints("C1 PS8818 detected");
+			/* Set the PS8818 as the secondary MUX */
+			usb_muxes[USBC_PORT_C1].next_mux = &usbc1_ps8818;
+		} else {
+			ccprints("C1 TUSB544 detected");
+			/* Set the TUSB544 as the secondary MUX */
+			usb_muxes[USBC_PORT_C1].next_mux = &usbc1_tusb544;
+		}
 	} else if (ec_config_has_usbc1_retimer_ps8743()) {
 		ccprints("C1 PS8743 detected");
 		/*
--- board/ezkinil/board.h
+++ board/ezkinil/board.h
@@ -124,7 +124,7 @@ enum ec_cfg_usb_mb_type {
  * EZKINIL_DB_T_OPT1_USBC_HDMI
  *	USB-A1  none
  *	USB-C1  Speed: 5 Gbps
- *		Retimer: TUSB544
+ *		Retimer: TUSB544 or PS8818
  *		TCPC: NCT3807
  *		PPC: NX20P3483
  *		IOEX: TCPC
@@ -169,13 +169,13 @@ static inline bool ec_config_has_usbc1_retimer_ps8743(void)
 		  HAS_USBC1_RETIMER_PS8743);
 }
 
-#define HAS_USBC1_RETIMER_TUSB544 \
+#define HAS_USBC1_RETIMER_ON_HDMI_DB \
 			(BIT(EZKINIL_DB_T_OPT1_USBC_HDMI))
 
-static inline bool ec_config_has_usbc1_retimer_tusb544(void)
+static inline bool ec_config_has_usbc1_retimer_on_hdmi_db(void)
 {
 	return !!(BIT(ec_config_get_usb_db()) &
-		  HAS_USBC1_RETIMER_TUSB544);
+		  HAS_USBC1_RETIMER_ON_HDMI_DB);
 }
 
 #define HAS_HDMI_RETIMER_PI3HDX1204 \
@@ -206,6 +206,7 @@ static inline bool ec_config_has_hdmi_conn_hpd(void)
 extern const int keyboard_factory_scan_pins[][2];
 extern const int keyboard_factory_scan_pins_used;
 extern const struct usb_mux usbc1_tusb544;
+extern const struct usb_mux usbc1_ps8818;
 extern const struct usb_mux usbc1_ps8743;
 extern struct usb_mux usbc1_amd_fp5_usb_mux;
 
