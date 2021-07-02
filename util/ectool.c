75bac8fb76aece3f2c2306b96fc7e74873fd1fcd
Zhuohao Lee <zhuohao@chromium.org>
2021-07-03 00:21:11 +0800

    ectool: fixup the error for chargecontrol normal|idle|discharge
    We added the EC_CHARGE_CONTROL_CMD_SET and EC_CHARGE_CONTROL_CMD_GET
    in the http://crrev/c/2929340. However, the ectool missed to set the
    p.cmd to EC_CHARGE_CONTROL_CMD_SET before calling the host command.
    This patch adds the p.cmd to fix it.
    
    BUG=b:183679223
    BRANCH=main
    TEST=`ectool chargecontrol normal|idle|discharge` is working
    
    Change-Id: I6e07801bb9e39f67165cb18a237c459b98c6d356
    Signed-off-by: Zhuohao Lee <zhuohao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3003003
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    (cherry picked from commit e541ebdc9521450ee9ee3fed471cb764786d705c)

--- util/ectool.c
+++ util/ectool.c
@@ -7477,6 +7477,7 @@ int cmd_charge_control(int argc, char *argv[])
 		return -1;
 	}
 
+	p.cmd = EC_CHARGE_CONTROL_CMD_SET;
 	if (!strcasecmp(argv[1], "normal")) {
 		p.mode = CHARGE_CONTROL_NORMAL;
 	} else if (!strcasecmp(argv[1], "idle")) {
