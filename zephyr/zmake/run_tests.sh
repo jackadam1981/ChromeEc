5cadf1611c71b63b278b0ccb6ef5c309881d33f5
Jack Rosenthal <jrosenth@chromium.org>
2021-07-02 13:28:48 -0600

    zephyr: zmake: drop "cq" hypothesis profile
    This is not needed, as we aren't taxing the CPU heavily anymore during
    unit tests.  Drop.
    
    BUG=b:190957007
    BRANCH=none
    TEST=run_tests.sh
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I1ef81f26b93cc6203d51a1ce8c445286704130ca

--- zephyr/zmake/run_tests.sh
+++ zephyr/zmake/run_tests.sh
@@ -19,11 +19,7 @@ cd "$(dirname "$(realpath -e "${BASH_SOURCE[0]}")")"
 export PYTHONPATH="${PWD}"
 
 # Run pytest.
-# TODO(jrosenth): --hypothesis-profile=cq is very likely to be
-# unnecessary, as this was only needed when we were heavily taxing the
-# CPU by running pytest alongside all the ninjas, which no longer
-# happens.  Remove this flag.
-pytest --hypothesis-profile=cq .
+pytest .
 
 # Check import sorting.
 isort --check .
