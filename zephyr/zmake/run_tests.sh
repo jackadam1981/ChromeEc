32a99bba036ee019b2ab6e478548a12a441cab9c
Jack Rosenthal <jrosenth@chromium.org>
2021-07-02 11:26:53 -0600

    zephyr: zmake: separate zmake test execution out of zmake
    Putting execution of "pytest" inside of zmake testall itself is
    problematic:  The path this code runs relies on a lot of zmake's
    features, and if there were a bug in zmake that prevented that code
    from being reached, the code could never get executed.  Additionally,
    the log output is impossible to read as it gets mixed in with all the
    build output.
    
    Separate it into a simple shell script, which greatly simplifies the
    code, and makes it easier to add further style tests down the
    road (isort, flake8, black).
    
    BUG=b:192389533
    BRANCH=none
    TEST=run firmware_builder.py
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I8795ba0c0225183b597f3738575c790ba73610f1

---
+++ zephyr/zmake/run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Run tests for zmake itself (not including Zephyr builds).
+
+# Show commands being run.
+set -x
+
+# Exit if any command exits non-zero.
+set -e
+
+# cd to the directory containing this script.
+cd "$(dirname "$(realpath -e "${BASH_SOURCE[0]}")")"
+
+# Test the copy in-tree, instead of what setuptools or the ebuild
+# installed.
+export PYTHONPATH="${PWD}"
+
+# Run pytest.
+pytest .
