7c819f9649d849befedb09c8b3d5bde7ba2a5d7c
David Huang <david.huang@quanta.corp-partner.google.com>
2021-07-12 17:24:58 +0800

    voema: Add accel/gyro sensor icm40608
    Support accel/gyro sensor: icm40608.
    
    BUG=b:192301309
    BRANCH=volteer
    TEST=Motion sensors are detected correctly and ectool motionsense
         show correct data.
    
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Change-Id: I35cb0fca753768468f07581251234149db5a04b1

--- board/voema/board.h
+++ board/voema/board.h
@@ -44,6 +44,11 @@
 /* Sensors */
 /* BMA253 accelerometer in base */
 #define CONFIG_ACCEL_BMA255
+#ifndef BOARD_VOEMA_NPCX796FC
+#define CONFIG_ACCELGYRO_ICM426XX
+#define CONFIG_ACCELGYRO_ICM426XX_INT_EVENT \
+	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
+#endif
 #define CONFIG_ACCEL_KX022
 
 /* TCS3400 ALS */
@@ -177,6 +182,9 @@ enum sensor_id {
 	BASE_ACCEL,
 	CLEAR_ALS,
 	RGB_ALS,
+#ifndef BOARD_VOEMA_NPCX796FC
+	BASE_GYRO,
+#endif
 	SENSOR_COUNT,
 };
 
@@ -187,6 +195,7 @@ enum usbc_port {
 };
 
 void board_reset_pd_mcu(void);
+void motion_interrupt(enum gpio_signal signal);
 
 extern const int keyboard_factory_scan_pins[][2];
 extern const int keyboard_factory_scan_pins_used;
