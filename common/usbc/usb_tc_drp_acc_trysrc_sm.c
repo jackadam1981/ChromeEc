18dd3740522eade19d5da6c0417a65adb1d371f0
Michał Barnaś <mb@semihalf.com>
2021-07-13 18:29:46 +0200

    TCPMv2: Add timeout for being in tc_attached_src state
    After reverting commit e12b6eb0af54738ccecf3d8e62660dbd8b459f99 it
    is possible for port to be stuck in source attached state.
    This change adds timeout after which CC lines state is recognized
    as stable. If no port partner is detected, transition to new state
    will happen.
    For lazor board it takes about 8ms to detect correct values on CC
    lines and maximum time from specification is 20ms.
    Considering this, value of 15ms was set as timeout.
    
    BUG=b:192353324
    BRANCH=main
    TEST=Running power state FAFTs on lazor gives positive results
    
    Signed-off-by: Michał Barnaś <mb@semihalf.com>
    Change-Id: I25937fcd996d5a67b4acb1833b07c80c395e66b8

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -2964,6 +2964,10 @@ static void tc_attached_src_entry(const int port)
 			/* Apply Rp */
 			typec_update_cc(port);
 
+			pd_timer_enable(port,
+					TC_TIMER_TIMEOUT,
+					PD_T_SRC_DISCONNECT);
+
 			/*
 			 * Maintain VCONN supply state, whether ON or OFF, and
 			 * its data role / usb mux connections. Do not
@@ -3054,6 +3058,8 @@ static void tc_attached_src_entry(const int port)
 
 		/* Apply Rp */
 		typec_update_cc(port);
+
+		pd_timer_enable(port, TC_TIMER_TIMEOUT, PD_T_SRC_DISCONNECT);
 	}
 
 	/* Inform PPC and OCP module that a sink is connected. */
@@ -3102,10 +3108,13 @@ static void tc_attached_src_run(const int port)
 	 * AttachWait.SNK shall enter TryWait.SNK for a Sink detach from
 	 * Attached.SRC.
 	 */
-	if (tc[port].cc_state == PD_CC_NONE &&
-			!TC_CHK_FLAG(port, TC_FLAGS_PR_SWAP_IN_PROGRESS)) {
+	if (tc[port].cc_state == PD_CC_NONE) {
 		bool tryWait;
 		enum usb_tc_state new_tc_state = TC_UNATTACHED_SNK;
+
+		if (!pd_timer_is_expired(port, TC_TIMER_TIMEOUT) &&
+		    TC_CHK_FLAG(port, TC_FLAGS_PR_SWAP_IN_PROGRESS))
+			return;
 
 		if (IS_ENABLED(CONFIG_USB_PD_TRY_SRC))
 			tryWait = is_try_src_enabled(port) &&
