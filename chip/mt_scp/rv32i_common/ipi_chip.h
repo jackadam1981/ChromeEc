4afaac4b556d56dd2947e44ef2fc7369dc1d9f65
Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
2021-07-13 09:58:49 +0800

    chip/mt_scp: Refactor get decoder hardware capability flow
    Get decoder capability according to each platform.
    
    BRANCH=none
    BUG=b:184793035
    BUG=b:185977882
    TEST=make BOARD=cherry_scp
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.org>
    Change-Id: Ifbe2bc81df1a5278c8467f76e4072045f4f435a5

--- chip/mt_scp/rv32i_common/ipi_chip.h
+++ chip/mt_scp/rv32i_common/ipi_chip.h
@@ -15,16 +15,6 @@
 #define SCP_FW_VERSION_LEN 32
 
 /*
- * Video decoder supported capability
- */
-#define VCODEC_CAPABILITY_4K_DISABLED BIT(4)
-#define VDEC_CAP_MM21 BIT(5)
-#define VDEC_CAP_MT21C BIT(6)
-#define VDEC_CAP_H264_SLICE BIT(8)
-#define VDEC_CAP_VP8_FRAME BIT(9)
-#define VDEC_CAP_VP9_FRAME BIT(10)
-
-/*
  * Video encoder supported capability:
  * BIT(0): enable 4K
  */
