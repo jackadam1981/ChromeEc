26820dbac15bc3a572f52e2c0fc3b7dd4a4c5150
Bhanu Prakash Maiya <bhanumaiya@google.com>
2021-07-14 18:49:16 -0700

    driver: bmi3xx: Fix fifo buffer read and rotate data
    This patch does following:
    1. Fixes the fifo read level.
    2. Rotates the fifo data before staging the data.
    3. Enables fifo when rate > 0.
    BRANCH=none
    BUG=b:178398789
    TEST=Accel implementation tested on Guybrush
         EC commands:
         > accelinfo
         > acceldata
    
    Signed-off-by: Bhanu Prakash Maiya <bhanumaiya@chromium.org>
    Signed-off-by: Latchiamaran Senram <latchiamaran.senram@bosch.corp-partner.google.com>
    Change-Id: I882a8e4a487e9e58026c2e60be2aa22e4b8a18a8

--- driver/accelgyro_bmi3xx.c
+++ driver/accelgyro_bmi3xx.c
@@ -33,7 +33,8 @@ STATIC_IF(CONFIG_BMI_ORIENTATION_SENSOR) void irq_set_orientation(
 
 STATIC_IF(CONFIG_ACCEL_FIFO) volatile uint32_t last_interrupt_timestamp;
 
-static uint8_t bmi3_buffer[BMI3_FIFO_BUFFER];
+/* Allocate buffer for FIFO + 2 bytes for i2c sync data */
+static uint8_t bmi3_buffer[BMI3_FIFO_BUFFER + 2];
 
 static inline int bmi3_read_n(const struct motion_sensor_t *s, const int reg,
 			      uint8_t *data_ptr, const int len)
@@ -110,19 +111,28 @@ void bmi3xx_interrupt(enum gpio_signal signal)
 static int enable_fifo(const struct motion_sensor_t *s, int enable)
 {
 	/* Set FIFO config to enable accel gyro data */
-	uint8_t reg_data[2] = {0, 0};
+	uint8_t reg_data[4];
 	struct bmi_drv_data_t *data = BMI_GET_DATA(s);
 
-	reg_data[0] = 0;
+	RETURN_ERROR(bmi3_read_n(s, BMI3_REG_FIFO_CONF, reg_data, 4));
 
 	if (enable) {
-		reg_data[1] = BMI3_FIFO_ACC_EN | BMI3_FIFO_GYR_EN;
+		if (s->type == MOTIONSENSE_TYPE_ACCEL)
+			reg_data[3] |= BMI3_FIFO_ACC_EN;
+		else
+			reg_data[3] |=  BMI3_FIFO_GYR_EN;
+
 		data->flags |= 1 << (s->type + BMI_FIFO_FLAG_OFFSET);
 	} else {
+		if (s->type == MOTIONSENSE_TYPE_ACCEL)
+			reg_data[3] &= ~BMI3_FIFO_ACC_EN;
+		else
+			reg_data[3] &=  ~BMI3_FIFO_GYR_EN;
+
 		data->flags &= ~(1 << (s->type + BMI_FIFO_FLAG_OFFSET));
 	}
 
-	return bmi3_write_n(s, BMI3_REG_FIFO_CONF, reg_data, 2);
+	return bmi3_write_n(s, BMI3_REG_FIFO_CONF, &reg_data[2], 2);
 }
 
 static int config_interrupt(const struct motion_sensor_t *s)
@@ -160,7 +170,7 @@ static int config_interrupt(const struct motion_sensor_t *s)
 		goto err_unlock;
 
 	/* Set FIFO water-mark to read data whenever available */
-	reg_data[0] = 1;
+	reg_data[0] = 30;
 	reg_data[1] = 0;
 
 	ret = bmi3_write_n(s, BMI3_REG_FIFO_WATERMARK, reg_data, 2);
@@ -220,9 +230,7 @@ int bmi3_parse_fifo_data(struct motion_sensor_t *s, struct bmi3_fifo_frame
 	uint16_t data_lsb, data_msb;
 
 	/* Variable to store I2C sync data which will get in FIFO data */
-	uint16_t i2c_sync_data;
-
-	uint16_t fifo_size = 0;
+	uint16_t i2c_sync_data, fifo_size = 0;
 
 	struct ec_response_motion_sensor_data vect;
 
@@ -233,6 +241,8 @@ int bmi3_parse_fifo_data(struct motion_sensor_t *s, struct bmi3_fifo_frame
 	uint8_t sens_cnt = 0, reg_data[2];
 
 	struct bmi_drv_data_t *data = BMI_GET_DATA(s);
+
+	intv3_t v;
 
 	if (s->type != MOTIONSENSE_TYPE_ACCEL)
 		return EC_SUCCESS;
@@ -384,15 +394,16 @@ int bmi3_parse_fifo_data(struct motion_sensor_t *s, struct bmi3_fifo_frame
 				struct motion_sensor_t *sens_output = s +
 								sens_cnt;
 
-				/* TODO:NORMALISE */
+				v[X] = raw_data[sens_cnt].x;
+				v[Y] = raw_data[sens_cnt].y;
+				v[Z] = raw_data[sens_cnt].z;
 
-				vect.data[X] = raw_data[sens_cnt].x;
-				vect.data[Y] = raw_data[sens_cnt].y;
-				vect.data[Z] = raw_data[sens_cnt].z;
+				rotate(v, *s->rot_standard_ref, v);
 
+				vect.data[X] = v[X];
+				vect.data[Y] = v[Y];
+				vect.data[Z] = v[Z];
 				vect.flags = 0;
-
-				/* TODO:check this s-motion_sensors */
 				vect.sensor_num = sens_cnt;
 
 				motion_sense_fifo_stage_data(&vect,
@@ -456,7 +467,7 @@ static int irq_handler(struct motion_sensor_t *s,
 
 		/* Read FIFO data */
 		ret = bmi3_read_n(s, BMI3_REG_FIFO_DATA, bmi3_buffer,
-					fifo_fill_level);
+					fifo_frame.available_fifo_len);
 
 		bmi3_parse_fifo_data(s, &fifo_frame, last_interrupt_timestamp);
 		has_read_fifo = 1;
@@ -972,16 +983,19 @@ static int set_data_rate(const struct motion_sensor_t *s,
 
 	/* Set the accel/gyro configurations. */
 	ret = bmi3_write_n(s, BMI3_REG_ACC_CONF + s->type, &reg_data[2], 2);
-
-	if (ret == EC_SUCCESS) {
-		saved_data->odr = normalized_rate;
-
-		/*
-		 * FIFO start collecting events.
-		 * They will be discarded if AP does not want them.
-		 */
-		ret = enable_fifo(s, 1);
+	if (ret) {
+		mutex_unlock(s->mutex);
+		return ret;
 	}
+
+	saved_data->odr = normalized_rate;
+
+	/*
+	 * If rate is non zero, FIFO start collecting events.
+	 * They will be discarded if AP does not want them.
+	 */
+	if (rate > 0)
+		ret = enable_fifo(s, 1);
 
 	mutex_unlock(s->mutex);
 	return ret;
@@ -1127,28 +1141,34 @@ static int init(struct motion_sensor_t *s)
 	/* This driver requires a mutex */
 	ASSERT(s->mutex);
 
-	/* Reset bmi3 device */
-	reg_data[0] = (uint8_t)(BMI3_CMD_SOFT_RESET & BMI3_SET_LOW_BYTE);
-	reg_data[1] = (uint8_t)((BMI3_CMD_SOFT_RESET & BMI3_SET_HIGH_BYTE)
-				>> 8);
-
-	RETURN_ERROR(bmi3_write_n(s, BMI3_REG_CMD, reg_data, 2));
-
-	/* Delay of 2ms after soft reset*/
-	msleep(2);
-
-	/* Enable feature engine bit */
-	reg_data[0] = BMI3_ENABLE;
-	reg_data[1] = 0;
-
-	RETURN_ERROR(bmi3_write_n(s, BMI3_REG_FEATURE_ENGINE_GLOB_CTRL,
-				  reg_data, 2));
-
 	/* Read chip id */
 	RETURN_ERROR(bmi3_read_n(s, BMI3_REG_CHIP_ID, reg_data, 4));
 
 	if (reg_data[2] != BMI323_CHIP_ID)
 		return EC_ERROR_HW_INTERNAL;
+
+	if (s->type == MOTIONSENSE_TYPE_ACCEL) {
+		/* Reset bmi3 device */
+		reg_data[0] = (uint8_t)(BMI3_CMD_SOFT_RESET
+					& BMI3_SET_LOW_BYTE);
+		reg_data[1] = (uint8_t)((BMI3_CMD_SOFT_RESET
+					 & BMI3_SET_HIGH_BYTE) >> 8);
+
+		RETURN_ERROR(bmi3_write_n(s, BMI3_REG_CMD, reg_data, 2));
+
+		/* Delay of 2ms after soft reset*/
+		msleep(2);
+
+		/* Enable feature engine bit */
+		reg_data[0] = BMI3_ENABLE;
+		reg_data[1] = 0;
+
+		RETURN_ERROR(bmi3_write_n(s, BMI3_REG_FEATURE_ENGINE_GLOB_CTRL,
+					  reg_data, 2));
+
+		if (IS_ENABLED(CONFIG_ACCEL_INTERRUPTS))
+			RETURN_ERROR(config_interrupt(s));
+	}
 
 	for (i = X; i <= Z; i++)
 		saved_data->scale[i] = MOTION_SENSE_DEFAULT_SCALE;
@@ -1159,10 +1179,6 @@ static int init(struct motion_sensor_t *s)
 	/* Flags used in FIFO parsing */
 	data->flags &= ~(BMI_FLAG_SEC_I2C_ENABLED
 			| (BMI_FIFO_ALL_MASK << BMI_FIFO_FLAG_OFFSET));
-
-	if (IS_ENABLED(CONFIG_ACCEL_INTERRUPTS)
-	    && (s->type == MOTIONSENSE_TYPE_ACCEL))
-		RETURN_ERROR(config_interrupt(s));
 
 	return sensor_init_done(s);
 }
