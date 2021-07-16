ede58619bff1fe410098a72beaf8ee50c03a98fd
Bhanu Prakash Maiya <bhanumaiya@google.com>
2021-07-15 17:59:26 -0700

    driver: bmi3xx: Assert on unsupported MOTIONSENSE_TYPE
    BRANCH=none
    BUG=b:178398789
    TEST=Accel implementation tested on Guybrush
         Accel init works on Guybrush
    
    Signed-off-by: Bhanu Prakash Maiya <bhanumaiya@chromium.org>
    Signed-off-by: Latchiamaran Senram <latchiamaran.senram@bosch.corp-partner.google.com>
    Change-Id: I326152fb99d31b33c6e594ddcb9b4e5ad9e8379c

--- driver/accelgyro_bmi3xx.c
+++ driver/accelgyro_bmi3xx.c
@@ -1127,6 +1127,20 @@ static int init(struct motion_sensor_t *s)
 	/* This driver requires a mutex */
 	ASSERT(s->mutex);
 
+	/*
+	 * BMI3xx driver only supports MOTIONSENSE_TYPE_ACCEL and
+	 * MOTIONSENSE_TYPE_GYR0
+	 */
+	if (s->type != MOTIONSENSE_TYPE_ACCEL
+	    && s->type != MOTIONSENSE_TYPE_GYRO)
+		return EC_ERROR_UNIMPLEMENTED;
+
+	/* Read chip id */
+	RETURN_ERROR(bmi3_read_n(s, BMI3_REG_CHIP_ID, reg_data, 4));
+
+	if (reg_data[2] != BMI323_CHIP_ID)
+		return EC_ERROR_HW_INTERNAL;
+
 	/* Reset bmi3 device */
 	reg_data[0] = (uint8_t)(BMI3_CMD_SOFT_RESET & BMI3_SET_LOW_BYTE);
 	reg_data[1] = (uint8_t)((BMI3_CMD_SOFT_RESET & BMI3_SET_HIGH_BYTE)
@@ -1146,9 +1160,6 @@ static int init(struct motion_sensor_t *s)
 
 	/* Read chip id */
 	RETURN_ERROR(bmi3_read_n(s, BMI3_REG_CHIP_ID, reg_data, 4));
-
-	if (reg_data[2] != BMI323_CHIP_ID)
-		return EC_ERROR_HW_INTERNAL;
 
 	for (i = X; i <= Z; i++)
 		saved_data->scale[i] = MOTION_SENSE_DEFAULT_SCALE;
