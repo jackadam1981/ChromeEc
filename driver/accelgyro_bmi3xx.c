78885a16984179f589f7fd520945bc216a85dcc4
Bhanu Prakash Maiya <bhanumaiya@google.com>
2021-07-16 01:09:19 -0700

    driver: bmi3xx: Fix set data rate when odr is 0
    BRANCH=none
    BUG=b:178398789
    TEST=Accel implementation tested on Guybrush
         EC commands:
         > accelinfo
         > accelrate 0
    
    Signed-off-by: Bhanu Prakash Maiya <bhanumaiya@chromium.org>
    Signed-off-by: Latchiamaran Senram <latchiamaran.senram@bosch.corp-partner.google.com>
    Change-Id: I9b3a30eb84304ab0109b23f86686c33d9b3bdfe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3033969
    Tested-by: Bhanu Prakash Maiya <bhanumaiya@google.com>
    Auto-Submit: Bhanu Prakash Maiya <bhanumaiya@google.com>
    Commit-Queue: Bhanu Prakash Maiya <bhanumaiya@google.com>
    Commit-Queue: Diana Z <dzigterman@chromium.org>
    Reviewed-by: Diana Z <dzigterman@chromium.org>

--- driver/accelgyro_bmi3xx.c
+++ driver/accelgyro_bmi3xx.c
@@ -901,14 +901,15 @@ static int get_data_rate(const struct motion_sensor_t *s)
 static int set_data_rate(const struct motion_sensor_t *s, int rate, int rnd)
 {
 	int ret;
-	int normalized_rate;
+	int normalized_rate = 0;
 	uint8_t reg_data[4];
-	uint8_t reg_val;
+	uint8_t reg_val = 0;
 
 	struct accelgyro_saved_data_t *saved_data = BMI_GET_SAVED_DATA(s);
 
-	RETURN_ERROR(bmi_get_normalized_rate(s, rate, rnd, &normalized_rate,
-					     &reg_val));
+	if (rate > 0)
+		RETURN_ERROR(bmi_get_normalized_rate(s, rate, rnd,
+						 &normalized_rate, &reg_val));
 
 	/*
 	 * Lock accel resource to prevent another task from attempting
@@ -972,6 +973,9 @@ static int set_data_rate(const struct motion_sensor_t *s, int rate, int rnd)
 		reg_data[2] =
 			BMI3_SET_BIT_POS0(reg_data[2], BMI3_SENS_ODR, reg_val);
 	}
+
+	/* Set accelerometer ODR */
+	reg_data[2] = BMI3_SET_BIT_POS0(reg_data[2], BMI3_SENS_ODR, reg_val);
 
 	/* Set the accel/gyro configurations. */
 	ret = bmi3_write_n(s, BMI3_REG_ACC_CONF + s->type, &reg_data[2], 2);
