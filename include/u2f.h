58e38ce847a9181b5edac56feb9ecb2deedb437a
Vadim Sukhomlinov <sukhomlinov@google.com>
2021-07-16 23:51:06 -0700

    u2f: refactoring to split command processing and crypto
    Split U2F crypto from U2F command processing by moving all crypto
    code into boards/cr50 (platform hooks).
    
    U2F state management is part of common code and passed to U2F crypto
    as a parameter.
    
    BUG=b:134594373
    TEST=make BOARD=cr50 CRYPTO_TEST=1
    console: u2f_test
    
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Change-Id: I85442cddb2959bd3102f7f6e6047134ede90951b

--- include/u2f.h
+++ include/u2f.h
@@ -1,3 +1,8 @@
+/* Copyright 2017 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 // Common U2F raw message format header - Review Draft
 // 2014-10-08
 // Editor: Jakob Ehrensvard, Yubico, jakob@yubico.com
@@ -18,11 +23,12 @@ typedef unsigned long int uint64_t;
 extern "C" {
 #endif
 
+/* Load platform hooks/definitions */
+#include "tpm_vendor_cmds.h"
+#include "u2f_impl.h"
+
 /* General constants */
 
-#define U2F_EC_KEY_SIZE	      32 /* EC key size in bytes */
-#define U2F_EC_POINT_SIZE     ((U2F_EC_KEY_SIZE * 2) + 1) /* Size of EC point */
-#define U2F_MAX_KH_SIZE	      128 /* Max size of key handle */
 #define U2F_MAX_ATT_CERT_SIZE 2048 /* Max size of attestation certificate */
 #define U2F_MAX_EC_SIG_SIZE   72 /* Max size of DER coded EC signature */
 #define U2F_CTR_SIZE	      4 /* Size of counter field */
@@ -31,11 +37,13 @@ extern "C" {
 #define U2F_MAX_ATTEST_SIZE   256 /* Size of largest blob to sign */
 #define U2F_P256_SIZE	      32
 
-#define SHA256_DIGEST_SIZE    32
+#define SHA256_DIGEST_SIZE 32
 
 #define ENC_SIZE(x) ((x + 7) & 0xfff8)
 
 /* EC (uncompressed) point */
+#define U2F_EC_KEY_SIZE	  32 /* EC key size in bytes */
+#define U2F_EC_POINT_SIZE ((U2F_EC_KEY_SIZE * 2) + 1) /* Size of EC point */
 
 #define U2F_POINT_UNCOMPRESSED 0x04 /* Uncompressed point format */
 
@@ -45,6 +53,8 @@ struct u2f_ec_point {
 	uint8_t y[U2F_EC_KEY_SIZE]; /* Y-value */
 };
 
+BUILD_ASSERT(sizeof(struct u2f_ec_point) == U2F_EC_POINT_SIZE);
+
 /* Request Flags. */
 
 #define U2F_AUTH_ENFORCE    0x03 /* Enforce user presence and sign */
@@ -52,28 +62,6 @@ struct u2f_ec_point {
 #define U2F_AUTH_FLAG_TUP   0x01 /* Test of user presence set */
 /* The key handle can be used with fingerprint or PIN. */
 #define U2F_UV_ENABLED_KH 0x08
-
-#define U2F_KH_VERSION_1 0x01
-
-#define U2F_AUTHORIZATION_SALT_SIZE 16
-
-struct u2f_key_handle {
-	uint8_t origin_seed[U2F_P256_SIZE];
-	uint8_t hmac[SHA256_DIGEST_SIZE];
-};
-
-struct u2f_versioned_key_handle_header {
-	uint8_t version;
-	uint8_t origin_seed[U2F_P256_SIZE];
-	uint8_t kh_hmac[SHA256_DIGEST_SIZE];
-};
-
-struct u2f_versioned_key_handle {
-	struct u2f_versioned_key_handle_header header;
-	/* Optionally checked in u2f_sign. */
-	uint8_t authorization_salt[U2F_AUTHORIZATION_SALT_SIZE];
-	uint8_t authorization_hmac[SHA256_DIGEST_SIZE];
-};
 
 /* TODO(louiscollard): Add Descriptions. */
 
@@ -115,9 +103,13 @@ struct u2f_sign_versioned_req {
 	struct u2f_versioned_key_handle keyHandle;
 };
 
-struct u2f_sign_resp {
+struct u2f_signature {
 	uint8_t sig_r[U2F_P256_SIZE]; /* Signature */
 	uint8_t sig_s[U2F_P256_SIZE]; /* Signature */
+};
+
+struct u2f_sign_resp {
+	struct u2f_signature sig; /* Signature */
 };
 
 struct u2f_attest_req {
@@ -128,8 +120,15 @@ struct u2f_attest_req {
 };
 
 struct u2f_attest_resp {
-	uint8_t sig_r[U2F_P256_SIZE];
-	uint8_t sig_s[U2F_P256_SIZE];
+	struct u2f_signature sig; /* Signature */
+};
+
+struct g2f_register_msg {
+	uint8_t reserved;
+	uint8_t app_id[U2F_APPID_SIZE];
+	uint8_t challenge[U2F_CHAL_SIZE];
+	uint8_t key_handle[U2F_APPID_SIZE + U2F_P256_SIZE];
+	struct u2f_ec_point public_key;
 };
 
 /* Command status responses */
@@ -166,6 +165,60 @@ struct u2f_attest_resp {
 
 /* Vendor command to enable/disable the extensions */
 #define U2F_VENDOR_MODE U2F_VENDOR_LAST
+
+/* ---- Physical presence ---- */
+
+enum touch_state {
+	POP_TOUCH_NO = 0, /* waiting for a user touch */
+	POP_TOUCH_YES = 1, /* touch recorded and latched */
+};
+
+/*
+ * Check whether the user presence event was latched.
+ *
+ * @param consume reset the latched touch event and the presence LED.
+ * @return POP_TOUCH_NO or POP_TOUCH_YES.
+ */
+enum touch_state pop_check_presence(int consume);
+
+/**
+ * U2F_GENERATE command handler. Generates a key handle according to input
+ * parameters.
+ */
+enum vendor_cmd_rc u2f_generate_cmd(enum vendor_cmd_cc code, void *buf,
+				    size_t input_size, size_t *response_size);
+
+/**
+ * U2F_SIGN command handler. Verifies a key handle is owned and signs data with
+ * it.
+ */
+enum vendor_cmd_rc u2f_sign_cmd(enum vendor_cmd_cc code, void *buf,
+				size_t input_size, size_t *response_size);
+
+/***
+ * Generates and persists to nvram a new seed that will be used to
+ * derive kek in future calls to u2f_gen_kek().
+ *
+ * @param commit whether to commit nvram changes before returning.
+ * @return EC_SUCCESS if seed was successfully created
+ * (and persisted if requested).
+ */
+int u2f_gen_kek_seed(int commit);
+
+/**
+ * Gets the x509 certificate for the attestation key pair returned
+ * by g2f_individual_keypair().
+ *
+ * @param buf pointer to a buffer that must be at least
+ * G2F_ATTESTATION_CERT_MAX_LEN bytes.
+ * @return size of certificate written to buf, 0 on error.
+ */
+int g2f_attestation_cert(uint8_t *buf);
+
+/**
+ * Get the current u2f state from the board.
+ */
+struct u2f_state *u2f_get_state(void);
 
 #ifdef __cplusplus
 }
--- include/u2f_impl.h
+++ include/u2f_impl.h
@@ -14,155 +14,158 @@
 #include "board/host/dcrypto.h"
 #endif
 
-#include "cryptoc/p256.h"
-#include "tpm_vendor_cmds.h"
-#include "u2f.h"
-
-/* ---- Physical presence ---- */
-
-enum touch_state {
-	POP_TOUCH_NO   = 0,  /* waiting for a user touch */
-	POP_TOUCH_YES  = 1,  /* touch recorded and latched */
-};
-
-/*
- * Check whether the user presence event was latched.
- *
- * @param consume reset the latched touch event and the presence LED.
- * @return POP_TOUCH_NO or POP_TOUCH_YES.
- */
-enum touch_state pop_check_presence(int consume);
-
-/* ---- non-volatile U2F state ---- */
-
-struct u2f_state {
-	uint32_t salt[8];
-	uint32_t salt_kek[8];
-	uint32_t salt_kh[8];
-};
-
-/**
- * Get the current u2f state from the board.
- */
-struct u2f_state *get_state(void);
+#define SHA256_DIGEST_SIZE 32
 
 /* ---- platform cryptography hooks ---- */
 
+/* ---- non-volatile U2F state, shared with common code ---- */
+struct u2f_state {
+	uint32_t salt[8]; /* G2F DRBG entropy */
+	/* HMAC key for key handle */
+	uint32_t salt_kek[SHA256_DIGEST_SIZE / sizeof(uint32_t)];
+	uint32_t salt_kh[8]; /* DRBG entropy */
+};
+
+/* Forward declarations to reduce dependencies. */
+struct u2f_ec_point;
+struct u2f_signature;
+
+#define U2F_KH_VERSION_1	    0x01
+#define U2F_ORIGIN_SEED_SIZE	    32
+#define U2F_AUTHORIZATION_SALT_SIZE 16
+#define U2F_MAX_KH_SIZE		    128 /* Max size of key handle */
+
+struct u2f_key_handle {
+	uint8_t origin_seed[U2F_ORIGIN_SEED_SIZE];
+	uint8_t hmac[SHA256_DIGEST_SIZE];
+};
+
+struct u2f_versioned_key_handle_header {
+	uint8_t version;
+	uint8_t origin_seed[U2F_ORIGIN_SEED_SIZE];
+	uint8_t kh_hmac[SHA256_DIGEST_SIZE];
+};
+
+struct u2f_versioned_key_handle {
+	struct u2f_versioned_key_handle_header header;
+	/* Optionally checked in u2f_sign. */
+	uint8_t authorization_salt[U2F_AUTHORIZATION_SALT_SIZE];
+	uint8_t authorization_hmac[SHA256_DIGEST_SIZE];
+};
+
+union u2f_key_handle_variant {
+	struct u2f_key_handle kh;
+	struct u2f_versioned_key_handle vkh;
+};
+
+BUILD_ASSERT(sizeof(union u2f_key_handle_variant) <= U2F_MAX_KH_SIZE);
+
 /**
- * Pack the specified origin, user secret and origin-specific seed
- * into a key handle.
+ * Initialize state with newly generated keys.
  *
+ * @param state U2F state to generate
+ *
+ * @return true if successful
+ */
+bool u2f_generate_state_keys(struct u2f_state *state);
+
+/**
+ * Update HMAC key in U2F state. Used when changing ownership to
+ * cryptographically discard previously generated keys.
+ */
+bool u2f_generate_hmac_key(struct u2f_state *state);
+
+/**
+ * Create a randomized key handle for specified origin, user secret.
+ * Generate associated signing key.
+ *
+ * @param state initialized u2f state
  * @param origin pointer to origin id
  * @param user pointer to user secret
- * @param seed pointer to origin-specific random seed
- * @param key_handle buffer to hold the output key handle
+ * @param authTimeSecretHash authentication time secret
+ * @param kh output key handle header
+ * @param kh_version - key handle version to generate
+ * @param pubKey - generated public key
  *
- * @return EC_SUCCESS if a valid keypair was created.
+ * @return true if a valid key pair was created
  */
-int u2f_origin_user_keyhandle(const uint8_t *origin, const uint8_t *user,
-			      const uint8_t *seed,
-			      struct u2f_key_handle *key_handle);
+bool u2f_generate(const struct u2f_state *state, const uint8_t *user,
+		  const uint8_t *origin, const uint8_t *authTimeSecretHash,
+		  union u2f_key_handle_variant *kh, uint8_t kh_version,
+		  struct u2f_ec_point *pubKey);
 
 /**
- * Pack the specified origin, user secret, origin-specific seed and version
- * byte into a key handle.
+ * Create a randomized key handle for specified origin, user secret.
+ * Generate associated signing key.
  *
+ * @param state initialized u2f state
+ * @param kh output key handle header
+ * @param kh_version - key handle version to generate
  * @param origin pointer to origin id
  * @param user pointer to user secret
- * @param seed pointer to origin-specific random seed
- * @param version the version byte to pack; should be greater than 0.
- * @param key_handle_header buffer to hold the output key handle header
+ * @param r - generated part of signature
+ * @param s - generated part of signature
  *
- * @return EC_SUCCESS if a valid keypair was created.
+ * @return true if a valid key pair was created
  */
-int u2f_origin_user_versioned_keyhandle(
-	const uint8_t *origin, const uint8_t *user, const uint8_t *seed,
-	uint8_t version,
-	struct u2f_versioned_key_handle_header *key_handle_header);
+bool u2f_sign(const struct u2f_state *state,
+	      const union u2f_key_handle_variant *kh, uint8_t kh_version,
+	      const uint8_t *user, const uint8_t *origin, const uint8_t *hash,
+	      struct u2f_signature *sig);
 
 /**
- * Generate an origin and user-specific ECDSA keypair from the specified
- * key handle.
+ * Verify that key handle matches provided origin and user and was created
+ * on this device (signed with U2F state HMAC key).
  *
- * If pk_x and pk_y are NULL, public key generation will be skipped.
+ * @param state initialized u2f state
+ * @param kh input key handle
+ * @param kh_version - key handle version to verify
+ * @param user pointer to user secret
+ * @param origin pointer to origin id
  *
- * @param key_handle pointer to the key handle
- * @param key_handle_size size of the key handle in bytes
- * @param d pointer to ECDSA private key
- * @param pk_x pointer to public key point
- * @param pk_y pointer to public key point
- *
- * @return EC_SUCCESS if a valid keypair was created.
+ * @return true if key handle was created on
  */
-int u2f_origin_user_keypair(const uint8_t *key_handle, size_t key_handle_size,
-			    p256_int *d, p256_int *pk_x, p256_int *pk_y);
-
-/**
- * Derive an hmac from the given salt, key handle and hash. The salt is to make
- * sure the hmac is different for different key handles of one user. The key
- * handle header is encoded into the authorization hmac to protect against
- * swapping auth time secret.
- */
-int u2f_authorization_hmac(const uint8_t *authorization_salt,
-			   const struct u2f_versioned_key_handle_header *header,
-			   const uint8_t *auth_time_secret_hash, uint8_t *hmac);
-
-/***
- * Generate a hardware derived 256b private key.
- *
- * @param kek ptr to store the generated key.
- * @param key_len size of the storage buffer. Should be 32 bytes.
- * @return EC_SUCCESS if a valid key was created.
- */
-int u2f_gen_kek(const uint8_t *origin, uint8_t *kek, size_t key_len);
-
-/**
- * Generate a hardware derived ECDSA keypair for individual attestation.
- *
- * @param seed ptr to store 32-byte seed to regenerate this key on this chip
- * @param d pointer to ECDSA private key
- * @param pk_x pointer to public key point
- * @param pk_y pointer to public key point
- *
- * @return EC_SUCCESS if a valid keypair was created.
- */
-int g2f_individual_keypair(p256_int *d, p256_int *pk_x, p256_int *pk_y);
-
-/***
- * Generates and persists to nvram a new seed that will be used to
- * derive kek in future calls to u2f_gen_kek().
- *
- * @param commit whether to commit nvram changes before returning.
- * @return EC_SUCCESS if seed was successfully created
- * (and persisted if requested).
- */
-int u2f_gen_kek_seed(int commit);
+bool u2f_authorize_keyhandle(const struct u2f_state *state,
+			     const union u2f_key_handle_variant *kh,
+			     uint8_t kh_version, const uint8_t *user,
+			     const uint8_t *origin);
 
 /* Maximum size in bytes of G2F attestation certificate. */
-#define G2F_ATTESTATION_CERT_MAX_LEN	315
+#define G2F_ATTESTATION_CERT_MAX_LEN 315
 
 /**
- * Gets the x509 certificate for the attestation keypair returned
+ * Gets the x509 certificate for the attestation key pair returned
  * by g2f_individual_keypair().
  *
+ * @param state U2F state parameters
+ * @param serial Device serial number
  * @param buf pointer to a buffer that must be at least
+ *
  * G2F_ATTESTATION_CERT_MAX_LEN bytes.
  * @return size of certificate written to buf, 0 on error.
  */
-int g2f_attestation_cert(uint8_t *buf);
+int g2f_attestation_cert_serial(const struct u2f_state *state,
+				const uint8_t *serial, uint8_t *buf);
 
 /**
- * U2F_GENERATE command handler. Generates a key handle according to input
- * parameters.
+ * Verify that provided key handle and public key match.
+ * @param state U2F state parameters
+ * @param key_handle key handle
+ * @param kh_version key handle version (0 - legacy, 1 - versioned)
+ * @param user pointer to user secret
+ * @param origin pointer to origin id
+ * @param public_key pointer to public key point (big endian)
+ * @param data data to sign
+ * @param data_size data size in bytes
+ * @param r part of generated signature
+ * @param s part of generated signature
+ *
+ * @return true if public key matches key handle.
  */
-enum vendor_cmd_rc u2f_generate(enum vendor_cmd_cc code, void *buf,
-				size_t input_size, size_t *response_size);
-
-/**
- * U2F_SIGN command handler. Verifies a key handle is owned and signs data with
- * it.
- */
-enum vendor_cmd_rc u2f_sign(enum vendor_cmd_cc code, void *buf,
-			    size_t input_size, size_t *response_size);
+bool u2f_attest(const struct u2f_state *state,
+		const union u2f_key_handle_variant *kh, uint8_t kh_version,
+		const uint8_t *user, const uint8_t *origin,
+		const struct u2f_ec_point *public_key, const uint8_t *data,
+		size_t data_size, struct u2f_signature *sig);
 
 #endif /* __CROS_EC_U2F_IMPL_H */
