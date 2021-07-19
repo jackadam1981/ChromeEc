32f62b1907685b08d41c2ded9506b246e3874a28
Gwendal Grignou <gwendal@google.com>
2021-07-19 11:41:42 -0700

    motion: fix forced mode sensor oversampling
    Calculate ratio base on the ec sampling rate, not the sensor
    sampling
    rate when sensor is in forced mode.
    
    BUG=b:193093133
    BRANCH=all
    TEST=On cyan, set lid sensor at 1Hz, EC period at 100ms.
    Check samples are comming at 1Hz, instead of .87Hz before.
    
    Using R91 without iioservice (with iioservice and chrome running,
    setting sensors at 10Hz, iioservice is doing the oversampling)
    
    iio:device0 # for i in scan_elements/*_en ; do echo 1 > $i ; done
    iio:device0 # echo 1 > buffer/enable
    iio:device0 # cd ../iio:device1
    iio:device1 # echo 1 > sampling_frequency
    iio:device1 # echo .1 > buffer/hwfifo_timeout
    iio:device1 # od -Ax -t u8 -w16 /dev/iio\:device0
    Before:
    000020  4629980246928523264         930681063992
    000030  4643491045809586176         931887853815
    000040  4634483915274321920         933094186224
    000050  4634483915276419072         934300819501
    000060  4634483915275370496         935507501102
    000070  4652498382503280640         936714261084
    000080  4652498382503280640         937921157408
    000090  4634483915276419072         939127997072
    0000a0  4634483915276419072         940334736923
    0000b0  4625476647302201344         941541481677
    
    After:
    000030  4611965710983233536          49574074465
    000040  4620972978956402688          50579587644
    000050  4634483846556942336          51585163771
    000060  4625476509863247872          52590820425
    000070  4611965710982184960          53596354051
    000080  4620972978955354112          54601965748
    000090  4611965710982184960          55607478367
    0000a0  4625476578582724608          56612893013
    0000b0  4602958443010064384          57618301161
    0000c0  4602958374288490496          58623627586
    0000d0  4602958511728492544          59629021387
    
    Signed-off-by: Gwendal Grignou <gwendal@google.com>
    Change-Id: Ie227511af3f8c0ed279e61762c6a3f15dab4bfff

--- common/motion_sense.c
+++ common/motion_sense.c
@@ -184,7 +184,10 @@ int motion_sense_set_data_rate(struct motion_sensor_t *sensor)
 		 * In case the AP want to run the sensors faster than it can,
 		 * be sure we don't see the ratio to 0.
 		 */
-		sensor->oversampling_ratio = MAX(1, odr / ap_odr_mhz);
+		if (motion_sensor_in_forced_mode(sensor))
+			sensor->oversampling_ratio = MAX(1, ec_odr_mhz / ap_odr_mhz);
+		else
+			sensor->oversampling_ratio = MAX(1, odr / ap_odr_mhz);
 	else
 		sensor->oversampling_ratio = 0;
 
