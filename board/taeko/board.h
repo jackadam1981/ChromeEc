5c784542c80b236e83085cc6996fc183b0e0b5af
amber.chen <amber.chen@lcfc.corp-partner.google.com>
2021-07-20 14:38:34 +0800

    taeko: replace board name to "Taeko"
    BUG=b:193377622
    BRANCH=main
    TEST=make BOARD=taeko
    
    Signed-off-by: amber.chen <amber.chen@lcfc.corp-partner.google.com>
    Change-Id: Ic0fd57ed60ca6872adda3d626b2ca4012d840444

--- board/taeko/board.h
+++ board/taeko/board.h
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-/* Brya board configuration */
+/* Taeko board configuration */
 
 #ifndef __CROS_EC_BOARD_H
 #define __CROS_EC_BOARD_H
@@ -11,7 +11,7 @@
 #include "compile_time_macros.h"
 
 /*
- * Early brya boards are not set up for vivaldi
+ * Early Taeko boards are not set up for vivaldi
  */
 #undef CONFIG_KEYBOARD_VIVALDI
 
@@ -93,7 +93,7 @@
 #define CONFIG_USBC_PPC_SYV682X
 #define CONFIG_USBC_PPC_NX20P3483
 
-/* TODO: b/177608416 - measure and check these values on brya */
+/* TODO: measure and check these values on Taeko */
 #define PD_POWER_SUPPLY_TURN_ON_DELAY	30000 /* us */
 #define PD_POWER_SUPPLY_TURN_OFF_DELAY	30000 /* us */
 #define PD_VCONN_SWAP_DELAY		5000 /* us */
@@ -193,7 +193,7 @@
 #define CONFIG_STEINHART_HART_3V3_30K9_47K_4050B
 
 /*
- * TODO(b/181271666): no fan control loop until sensors are tuned
+ * TODO: no fan control loop until sensors are tuned
  */
 /* #define CONFIG_FANS			FAN_CH_COUNT */
 
