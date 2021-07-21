637bb39b7158850da431401b5ccf544360ae7e60
Daisuke Nojiri <dnojiri@chromium.org>
2021-07-21 13:49:49 -0700

    keyboard: Enable strict debouncer
    This CL adds CONFIG_KEYBOARD_STRICT_DEBOUNCE. It makes the keyboard
    debouncer register a key stroke after deounce is done.
    
    BUG=b:193505909
    BRANCH=Dedede
    TEST=buildall, Blipper
    
    Change-Id: Ia380657021035930afab5cafffa8cc2edd7ff475
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/config.h
+++ include/config.h
@@ -2801,6 +2801,12 @@
 #undef CONFIG_KEYBOARD_KEYPAD
 
 /*
+ * Enable strict debouncer. A strict debouncer waits until debounce is done
+ * before registering key up/down.
+ */
+#undef CONFIG_KEYBOARD_STRICT_DEBOUNCE
+
+/*
  * Enable the 8042 AUX port. This is typically used for PS/2 mouse devices.
  * You will need to implement send_aux_data_to_device and lpc_aux_put_char.
  */
