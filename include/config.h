8ccb7ec6acf32436231035b2ad6e19297e3783f9
Mary Ruthven <mruthven@chromium.org>
2021-07-20 19:18:22 -0500

    common/util: group the code needed in the fips module
    Group the code we are going to move into the fips module.
    
    BUG=none
    TEST=make buildall -j
    
    Change-Id: Idb1162dd47f00f676cb1c1ec3f77b85808ad9c30
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1358,6 +1358,8 @@
  * Use dcrypto in the board directory instead of chip/g.
  */
 #undef CONFIG_DCRYPTO_BOARD
+/* Build FIPS module separately */
+#undef CONFIG_FIPS_BOUNDARY
 /*
  * This provides struct definitions and function declarations that can be
  * implemented by unit tests for testing code that depends on dcrypto.
