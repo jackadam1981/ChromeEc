6d35415bcedb445a86acde37b5c94f484fc59f81
Daisuke Nojiri <dnojiri@chromium.org>
2021-07-21 10:05:07 -0700

    keyboard: Use __override for keyboard_scan_config
    Currently keyboard_scan_config is defined by each board using
    CONFIG_KEYBOARD_BOARD_CONFIG. This patch makes it defined as
    __override hence removes CONFIG_KEYBOARD_BOARD_CONFIG.
    
    To fix conflict, manually carry the change on:
    "baseboard/brya/baseboard.h", "baseboard/kukui/baseboard.h",
    "board/lazor/board.h", "board/marzipan/board.h",
    "board/magolor/board.c", "board/magolor/board.h",
    "board/nightfury/board.c", "board/nightfury/board.h",
    "board/pazquel/board.h", "board/pompom/board.h",
    "board/trogdor/board.h", "board/volteer/board.c",
    "board/wormdingler/board.h", "docs/configuration/keyboard.md".
    
    Note that "zephyr/shim/include/config_chip.h" is reverted to original
    version on this branch
    
    BUG=None
    BRANCH=None
    TEST=buildall
    
    Change-Id: I53a356741ba4d00e829ca59b74ee6dc704188728
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3044403
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>

--- zephyr/shim/include/config_chip.h
+++ zephyr/shim/include/config_chip.h
@@ -2,7 +2,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-
+ 
 #ifndef __CROS_EC_CONFIG_CHIP_H
 #define __CROS_EC_CONFIG_CHIP_H
 
@@ -17,7 +17,7 @@
  * Options which are for the platform/ec configuration can be found in
  * common/config.h.
  */
-
+ 
 #define CONFIG_ZEPHYR
 #define CHROMIUM_EC
 
