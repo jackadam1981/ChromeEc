ddc5f8fd21a90ed670d9c77abc78b10274ae4913
Jun Lin <CHLin56@nuvoton.com>
2021-07-26 15:14:33 +0800

    zephyr: eSPI: npcx: unify the access to eSPI event data
    Follow PR:37194 to access KBC/ACPI event data via structure instead of
    the current chip-vendor-specific macro.
    
    BUG=none
    BRANCH=none
    TEST=test on Volteer, verify that the platform can boot up to OS screen;
    make sure keyboard is functional.
    
    Cq-Depend:
    Signed-off-by: Jun Lin <CHLin56@nuvoton.com>
    Change-Id: Iff857809b3c11e734825c113cb8d9913a7a177f2

--- zephyr/shim/chip/npcx/espi.c
+++ zephyr/shim/chip/npcx/espi.c
@@ -7,34 +7,47 @@
 #include <sys/util.h>
 
 #include "soc_espi.h"
+#include "drivers/espi.h"
 #include "zephyr_espi_shim.h"
 
 bool is_acpi_command(uint32_t data)
 {
-	return (data >> NPCX_ACPI_TYPE_POS) & 0x01;
+	struct espi_evt_data_acpi *acpi = (struct espi_evt_data_acpi *)&data;
+
+	return acpi->type;
 }
 
 uint32_t get_acpi_value(uint32_t data)
 {
-	return (data >> NPCX_ACPI_DATA_POS) & 0xff;
+	struct espi_evt_data_acpi *acpi = (struct espi_evt_data_acpi *)&data;
+
+	return acpi->data;
 }
 
 bool is_8042_ibf(uint32_t data)
 {
-	return (data >> NPCX_8042_EVT_POS) & NPCX_8042_EVT_IBF;
+	struct espi_evt_data_kbc *kbc = (struct espi_evt_data_kbc *)&data;
+
+	return kbc->evt & HOST_KBC_EVT_IBF;
 }
 
 bool is_8042_obe(uint32_t data)
 {
-	return (data >> NPCX_8042_EVT_POS) & NPCX_8042_EVT_OBE;
+	struct espi_evt_data_kbc *kbc = (struct espi_evt_data_kbc *)&data;
+
+	return kbc->evt & HOST_KBC_EVT_OBE;
 }
 
 uint32_t get_8042_type(uint32_t data)
 {
-	return (data >> NPCX_8042_TYPE_POS) & 0xFF;
+	struct espi_evt_data_kbc *kbc = (struct espi_evt_data_kbc *)&data;
+
+	return kbc->type;
 }
 
 uint32_t get_8042_data(uint32_t data)
 {
-	return (data >> NPCX_8042_DATA_POS) & 0xFF;
+	struct espi_evt_data_kbc *kbc = (struct espi_evt_data_kbc *)&data;
+
+	return kbc->data;
 }
