80a8d1451bffe1acb3d8f2a3ca15c606e0901a68
johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
2021-07-27 08:53:20 +0800

    cret: Modify EC_ACOK_OTG enable/disable condition
    When the Type-C AC plug-in and got AC status from RAA489000
    then enables charger the OTP/OVP protect function.
    
    BUG=b:188567172
    BRANCH=dedede
    TEST=Test on the proj-cret the GPIOC0 when Type-C device plug-in
    is low and the Type-C AC plug-in is high
    
    Signed-off-by: johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
    Change-Id: I112ff5437c2c8195003a03437e51eb6012a42740

--- board/cret/board.c
+++ board/cret/board.c
@@ -597,8 +597,13 @@ static void fw_config_tablet_mode(void)
 
 static void board_extpower(void)
 {
-	int extpower_present = extpower_is_present();
+	int extpower_present;
+
+	if (charge_manager_get_charger_voltage() >= 5000)
+		extpower_present = extpower_is_present();
+	else
+		extpower_present = 0;
 
 	gpio_set_level(GPIO_EC_ACOK_OTG, extpower_present);
 }
-DECLARE_HOOK(HOOK_AC_CHANGE, board_extpower, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_SECOND, board_extpower, HOOK_PRIO_DEFAULT);
