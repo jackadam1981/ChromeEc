a2c500d0535f281b48f8de3d993dd69564fcecf2
Dino Li <Dino.Li@ite.com.tw>
2021-07-29 10:27:56 +0800

    TESTONLY: it83xx: i2c: don't check busy if transaction is ongoing.
    Change-Id: I97161db0bb0c54de3ad55d82512a6a188036270f

--- chip/it83xx/i2c.c
+++ chip/it83xx/i2c.c
@@ -651,10 +651,21 @@ int chip_i2c_xfer(int port, uint16_t addr_flags,
 	if (out_size == 0 && in_size == 0)
 		return EC_SUCCESS;
 
+#if 0
 	if (pd->i2ccs) {
 		if ((flags & I2C_XFER_SINGLE) == I2C_XFER_SINGLE)
 			flags &= ~I2C_XFER_START;
 	}
+#else
+	/*
+	 * Make the below i2c sequence work: eg.
+	 * I2C_XFER_START
+	 * I2C_XFER_START
+	 * I2C_XFER_STOP
+	 */
+	if (pd->i2ccs)
+		flags &= ~I2C_XFER_START;
+#endif
 
 	/* Copy data to port struct */
 	pd->out = out;
