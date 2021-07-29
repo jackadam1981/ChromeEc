6961d7c0ac1b5ec409da6d8759d70a0ead050fb4
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-07-29 16:27:10 +0800

    gimble: add mic mute
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Ide0873c32236ead87d661e36b5f9b20cfd78144b

--- include/keyboard_8042_sharedlib.h
+++ include/keyboard_8042_sharedlib.h
@@ -150,6 +150,7 @@ enum scancode_values {
 	SCANCODE_NEXT_TRACK = 0xe04d,		/* e019 in codeset 1 */
 	SCANCODE_PREV_TRACK = 0xe015,		/* e010 in codeset 1 */
 	SCANCODE_PLAY_PAUSE = 0xe054,		/* e01a in codeset 1 */
+	SCANCODE_MIC_MUTE = 0xe05b,		/* e01b in codeset 1 */
 
 	SCANCODE_UP = 0xe075,
 	SCANCODE_DOWN = 0xe072,
