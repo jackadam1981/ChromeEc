95d6e154f3a626e1da887f8fdbc7c355ad24f7a6
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-07-29 16:27:10 +0800

    common/keyboard_vivaldi: Add new top row action key
    Use codeset 1 e01b(make)/ e09b(break) for MICMUTE.
    Use codeset 1 e02b(make)/ e0ab(break) for KBBL toggle.
    
    BUG=b:194146863
    BRANCH=none
    TEST=make -j BOARD=gimble
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Ide0873c32236ead87d661e36b5f9b20cfd78144b

--- include/keyboard_8042_sharedlib.h
+++ include/keyboard_8042_sharedlib.h
@@ -150,6 +150,8 @@ enum scancode_values {
 	SCANCODE_NEXT_TRACK = 0xe04d,		/* e019 in codeset 1 */
 	SCANCODE_PREV_TRACK = 0xe015,		/* e010 in codeset 1 */
 	SCANCODE_PLAY_PAUSE = 0xe054,		/* e01a in codeset 1 */
+	SCANCODE_MICMUTE = 0xe05b,		/* e01b in codeset 1 */
+	SCANCODE_KBDILLUMTOGGLE = 0xe05d,	/* e02b in codeset 1 */
 
 	SCANCODE_UP = 0xe075,
 	SCANCODE_DOWN = 0xe072,
