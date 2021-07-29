e2d2dd3c55ed6f9076972e190bc6fdb69d384b49
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-07-29 16:27:10 +0800

    gimble: add vivaldi keyboard support
    - We have refresh key on T2.
    - Add T8 key for KBBL toggle.
    - Add T9 key for MICMUTE.
    - Add T10 for play pause key.
    
    BUG=b:194146863
    BRANCH=none
    TEST=make -j BOARD=gimble
    
    Cq-Depend: chromium:3060293
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Ide0873c32236ead87d661e36b5f9b20cfd78144b

--- common/keyboard_vivaldi.c
+++ common/keyboard_vivaldi.c
@@ -61,6 +61,7 @@ static const uint16_t action_scancodes[] = {
 	[TK_KBD_BKLIGHT_DOWN] = SCANCODE_KBD_BKLIGHT_DOWN,
 	[TK_KBD_BKLIGHT_UP] = SCANCODE_KBD_BKLIGHT_UP,
 	[TK_PRIVACY_SCRN_TOGGLE] = SCANCODE_PRIVACY_SCRN_TOGGLE,
+	[TK_MIC_MUTE] = SCANCODE_MIC_MUTE,
 };
 
 static const struct ec_response_keybd_config *vivaldi_keybd;
