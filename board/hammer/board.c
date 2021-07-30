04dba9956f1b71bd05dcf24d1229142610174ddd
Ting Shen <phoenixshen@google.com>
2021-07-30 11:44:26 +0800

    bland: initial commit
    Bland is the detachable keyboard (Hammer derivative) for MrBland project.
    
    BUG=b:194460146
    TEST=make
    BRANCH=trogdor
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I6c315cca6856afca175ce864ece025222b1a2d69

--- board/hammer/board.c
+++ board/hammer/board.c
@@ -350,7 +350,8 @@ static const struct ec_response_keybd_config zed_kb = {
 __override
 const struct ec_response_keybd_config *board_vivaldi_keybd_config(void)
 {
-	if (IS_ENABLED(BOARD_ZED) || IS_ENABLED(BOARD_STAR))
+	if (IS_ENABLED(BOARD_ZED) || IS_ENABLED(BOARD_STAR) ||
+		IS_ENABLED(BOARD_BLAND))
 		return &zed_kb;
 
 	return NULL;
