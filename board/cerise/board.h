b2269b405e1996db8c7957770018a1838c6cb4a9
Keith Short <keithshort@chromium.org>
2021-08-03 12:29:48 -0600

    COIL: Update SPI references to controller/peripheral
    Update SPI references to controller/peripheral for inclusivity.
    
    BUG=b:163885307
    BRANCH=none
    TEST=compare_build.sh
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: I19b02fb949aad9ade569c4658a904e9ce59e27c3

--- board/cerise/board.h
+++ board/cerise/board.h
@@ -83,7 +83,7 @@
 #define I2C_PORT_VIRTUAL_BATTERY    I2C_PORT_BATTERY
 
 /* Enable Accel over SPI */
-#define CONFIG_SPI_ACCEL_PORT    0  /* The first SPI master port (SPI2) */
+#define CONFIG_SPI_ACCEL_PORT    0  /* The first SPI controller port (SPI2) */
 
 #define CONFIG_KEYBOARD_PROTOCOL_MKBP
 #define CONFIG_MKBP_EVENT
--- board/hatch_fp/board_rw.c
+++ board/hatch_fp/board_rw.c
@@ -76,7 +76,9 @@ static void configure_fp_sensor_spi(void)
 	/* Configure SPI GPIOs */
 	gpio_config_module(MODULE_SPI_CONTROLLER, 1);
 
-	/* Set all SPI master signal pins to very high speed: B12/13/14/15 */
+	/* Set all SPI controller signal pins to very high speed:
+	 * B12/13/14/15
+	 */
 	STM32_GPIO_OSPEEDR(GPIO_B) |= 0xff000000;
 
 	/* Enable clocks to SPI2 module (master) */
@@ -117,7 +119,7 @@ void board_init_rw(void)
 	gpio_enable_interrupt(GPIO_SLP_L);
 
 	/*
-	 * Enable the SPI slave interface if the PCH is up.
+	 * Enable the SPI peripheral interface if the PCH is up.
 	 * Do not use hook_call_deferred(), because ap_deferred() will be
 	 * called after tasks with priority higher than HOOK task (very late).
 	 */
