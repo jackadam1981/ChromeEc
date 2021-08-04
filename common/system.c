fc3fe9dc30c605a1f94969642cf07eff2a804267
Yuval Peress <peress@chromium.org>
2021-08-04 11:19:55 -0600

    system: fix system_get_scratchpad API
    The current API for system_get_scratchpad mixes the status and the value
    being read. Update the signature to allow both.
    
    BRANCH=none
    BUG=b:195481980
    TEST=make testall && zmake testall
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Change-Id: I3a5f5ad523d507c53a5d474806f58afafb82e70c

--- common/system.c
+++ common/system.c
@@ -1174,6 +1174,7 @@ DECLARE_HOST_COMMAND(EC_CMD_SYSINFO, host_command_sysinfo,
 static int command_scratchpad(int argc, char **argv)
 {
 	int rv = EC_SUCCESS;
+	uint32_t scratchpad_value;
 
 	if (argc == 2) {
 		char *e;
@@ -1183,7 +1184,10 @@ static int command_scratchpad(int argc, char **argv)
 		rv = system_set_scratchpad(s);
 	}
 
-	ccprintf("Scratchpad: 0x%08x\n", system_get_scratchpad());
+	if (system_get_scratchpad(&scratchpad_value))
+		ccprintf("Error reading scratchpad register");
+	else
+		ccprintf("Scratchpad: 0x%08x\n", scratchpad_value);
 	return rv;
 }
 DECLARE_CONSOLE_COMMAND(scratchpad, command_scratchpad,
