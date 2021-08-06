617167d22e6337d08773523db0052f1616e7ae7d
Keith Short <keithshort@chromium.org>
2021-08-06 14:26:58 -0600

    config: rename CONFIG_HOSTCMD_SHI to CONFIG_HOST_INTERFACE_SHI
    Rename CONFIG_HOSTCMD_SHI to CONFIG_HOST_INTERFACE_SHI. This makes the
    host interface selection configs distinct from configs used to
    enable/disable specific host commands.
    
    BUG=none
    BRANCH=none
    TEST=compare_build.sh
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: I35959149554f58c8911459dcd025720b6d66eb32

--- chip/npcx/shi_chip.h
+++ chip/npcx/shi_chip.h
@@ -8,7 +8,7 @@
 #ifndef SHI_CHIP_H_
 #define SHI_CHIP_H_
 
-#ifdef CONFIG_HOSTCMD_SHI
+#ifdef CONFIG_HOST_INTERFACE_SHI
 /**
  * Called when the NSS level changes, signalling the start of a SHI
  * transaction.
