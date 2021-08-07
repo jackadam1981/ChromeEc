23cf75c6c6f1a8091962671b7f37dc7f536df691
Mary Ruthven <mruthven@chromium.org>
2021-08-06 19:21:01 -0500

    add script to inject the fips fingerprint
    Add a script to calculate the fips checksum and inject it into the
    text.fips_checksum section of the RW A and B elf files.
    
    This only affects the cr50 build.
    
    BUG=none
    TEST=manual
    
    	# Verify cr50 is the only board that creates the fips artifacts
    	make buildall -j
    	find build/ -name *fips.checksum*
    		build/cr50/RW/ec.RW.fips.checksum.dump
    		build/cr50/RW/ec.RW_B.fips.checksum.dump
    		build/cr50/RW/ec.RW.fips.checksum
    		build/cr50/RW/ec.RW_B.fips.checksum
    
    	objdump the text.fips_checksum section of ec.RW.elf and
    	ec.RW_B.elf. Make sure they match ec.RW.fips.checksum and
    	ec.RW_B.fips.checksum
    
    	# Verify cr50 can update to image signed with devid and that
    	# image loads a non-zero Stored hash that matches the computed
    	# one.
    	H1_DEVIDS="${DEVID}" make -j BOARD=cr50 CR50_DEV=1
    
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Change-Id: Iab857ec1b7e3ae0d23681a25467e26286bd68210

---
+++ util/inject_fips_fingerprint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Calculate hash of fips module and inject it into the .elf file.
+
+main() {
+  local objcopy="${1}"
+  local rw_elf="${2}"
+  local rw_flat="${3}"
+  local base="${rw_elf%.elf}"
+  local build_dir
+  local offset
+  local size
+  local sections
+  local fips_start
+  local fips_end
+  local fips_offset
+  local result
+
+  if [ ! -f "${rw_elf}" ] ; then
+    echo "  ${rw_elf} doesn't exist"
+    return 1
+  fi
+  if [ ! -f "${rw_flat}" ] ; then
+    echo "  ${rw_flat} doesn't exist"
+    return 1
+  fi
+
+  build_dir=$(dirname "${rw_elf}")
+  local checksum_section="text.fips_checksum"
+  local fips_checksum="${base}.fips.checksum"
+  local fips_checksum_dump="${fips_checksum}.dump"
+
+  offset=0x$( objdump -h "${rw_elf}" | awk '/ .text / {print $4}' )
+  sections=$( objdump -t "${rw_elf}" )
+
+  if [[ "${sections}" =~ "${checksum_section}" ]] ; then
+    echo "  get fips checksum"
+  else
+    echo "  no fips checksum"
+    return 1
+  fi
+  fips_start=0x$( echo "${sections}" | awk '/__fips_module_start/ {print $1}' )
+  fips_end=0x$( echo "${sections}" | awk '/__fips_module_end/ {print $1}' )
+  size=$((fips_end - fips_start))
+  fips_offset=$((fips_start - offset))
+
+  result=$(dd if="${rw_flat}" skip="${fips_offset}" count="${size}" bs=1 | \
+		sha256sum)
+  echo "${result%% *}" > "${fips_checksum}"
+  echo "${result%% *}" | xxd -r -p  > "${fips_checksum_dump}"
+
+  ${objcopy} --update-section "${checksum_section}"="${fips_checksum_dump}" \
+		"${rw_elf}"
+}
+
+main "$@"
