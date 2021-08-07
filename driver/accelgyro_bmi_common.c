003a989165ba0ba2c77283c204052eb36da83853
Mike Lee <mike5@huaqin.corp-partner.google.com>
2021-08-07 13:23:30 +0800

    driver: Add support for BMI220 sensor
    Based on BMI260 sensor, add support for BMI220 sensor.
    
    BUG=b:188373185,b:193945779
    BRANCH=dedede,kukui
    TEST=run factory test on storo360
    
    Signed-off-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Signed-off-by: Rong Chang <rongchang@chromium.org>
    Change-Id: Ifa88c7c58203ba405fde757b85570d53a27221a0

--- driver/accelgyro_bmi_common.c
+++ driver/accelgyro_bmi_common.c
@@ -23,20 +23,27 @@
 #define CPRINTF(format, args...) cprintf(CC_ACCEL, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_ACCEL, format, ## args)
 
-#if !defined(CONFIG_ACCELGYRO_BMI160) && !defined(CONFIG_ACCELGYRO_BMI260) \
-&& !defined(CONFIG_ACCELGYRO_BMI3XX)
-#error "Must use following sensors BMI160 BMI260 BMI3XX"
+#if !defined(CONFIG_ACCELGYRO_BMI160) && \
+    !defined(CONFIG_ACCELGYRO_BMI220) && \
+    !defined(CONFIG_ACCELGYRO_BMI260) && \
+    !defined(CONFIG_ACCELGYRO_BMI3XX)
+#error "Must use following sensors BMI160 BMI220 BMI260 BMI3XX"
 #endif
 
-#if defined(CONFIG_ACCELGYRO_BMI260) && !defined(CONFIG_ACCELGYRO_BMI160)
+#if (defined(CONFIG_ACCELGYRO_BMI260) || defined(CONFIG_ACCELGYRO_BMI220)) && \
+    !defined(CONFIG_ACCELGYRO_BMI160)
 #define V(s_) 1
-#elif defined(CONFIG_ACCELGYRO_BMI160) && !defined(CONFIG_ACCELGYRO_BMI260)
+#elif defined(CONFIG_ACCELGYRO_BMI160) && \
+      !(defined(CONFIG_ACCELGYRO_BMI260) || defined(CONFIG_ACCELGYRO_BMI220))
 #define V(s_) 0
 #else
-#define V(s_) ((s_)->chip == MOTIONSENSE_CHIP_BMI260)
+#define V(s_) ((s_)->chip == MOTIONSENSE_CHIP_BMI260 || \
+	       (s_)->chip == MOTIONSENSE_CHIP_BMI220)
 #endif
 /* Index for which table to use. */
-#if !defined(CONFIG_ACCELGYRO_BMI160) || !defined(CONFIG_ACCELGYRO_BMI260)
+#if !defined(CONFIG_ACCELGYRO_BMI160) || \
+    !defined(CONFIG_ACCELGYRO_BMI220) || \
+    !defined(CONFIG_ACCELGYRO_BMI260)
 #define T(s_) 0
 #else
 #define T(s_) V(s_)
