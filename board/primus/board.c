0ceae76f1b58e70fff7bfb0f18b7b8084d9b0c73
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-08-09 20:42:25 +0800

    primus: modify charger related setting
    BUG=b:
    BRANCH=none
    TEST=make -j BOARD=primus
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Ie5ffd07315a5cac7987095e5e87ece25396dc074

--- board/primus/board.c
+++ board/primus/board.c
@@ -6,6 +6,7 @@
 #include "battery.h"
 #include "button.h"
 #include "charge_ramp.h"
+#include "charge_state_v2.h"
 #include "charger.h"
 #include "common.h"
 #include "compile_time_macros.h"
@@ -22,6 +23,7 @@
 #include "switch.h"
 #include "throttle_ap.h"
 #include "usbc_config.h"
+#include "util.h"
 
 #include "gpio_list.h" /* Must come after other header files. */
 
@@ -105,4 +107,18 @@ enum battery_present battery_hw_present(void)
 
 	/* The GPIO is low when the battery is physically present */
 	return gpio_get_level(batt_pres) ? BP_NO : BP_YES;
+}
+
+__override void board_set_charge_limit(int port, int supplier, int charge_ma,
+			    int max_ma, int charge_mv)
+{
+	/*
+	 * Follow OEM request to limit the input current to
+	 * 97% negotiated limit.
+	 */
+	charge_ma = charge_ma * 97 / 100;
+
+	charge_set_input_current_limit(MAX(charge_ma,
+					CONFIG_CHARGER_INPUT_CURRENT),
+					charge_mv);
 }
