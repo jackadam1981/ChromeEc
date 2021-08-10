7f261b818ef72f75893e1f8366c762a1f5862f1f
Caveh Jalali <caveh@chromium.org>
2021-08-10 02:41:58 -0700

    WIP: ps8xxx: Wait for chip to wake up from LPM
    BRANCH=none
    BUG=b:195087071,b:186189039
    
    Change-Id: If047fc122d7f61ed5fc361f97b47180e5cf08970
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- driver/tcpm/ps8xxx.c
+++ driver/tcpm/ps8xxx.c
@@ -58,6 +58,8 @@
 #define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
 
+#define PS8XXX_I2C_RECOVERY_DELAY_MS	10
+
 /*
  * The product_id per ports here is expected to be set in callback function -
  * .init of tcpm_drv by calling board_get_ps8xxx_product_id().
@@ -290,7 +292,7 @@ static struct ps8xxx_variant_map variant_map[] = {
 };
 
 static int get_reg_by_product(const int port,
-				const enum ps8xxx_variant_regs reg)
+			      const enum ps8xxx_variant_regs reg)
 {
 	int i;
 
@@ -579,6 +581,43 @@ static int ps8815_make_device_id(int port, int *id)
 }
 #endif
 
+static int ps8xxx_lpm_recovery_delay(int port)
+{
+	int val;
+	int status;
+	int fw_reg;
+	timestamp_t deadline;
+
+	fw_reg = get_reg_by_product(port, REG_FW_VER);
+
+	/*
+	 * TODO(b/195087071):
+	 *   this is supposed to be 10ms, but measuring 12ms...
+	 *   "recovery from LPM"
+	 *   when running the "app" firmware, it takes 35ms...
+	 */
+
+	deadline = get_time();
+	deadline.val += PS8815_FW_INIT_DELAY_MS * 1000;
+
+	val = 0;
+	for (;;) {
+		status = tcpc_read(port, fw_reg, &val);
+		if (status != EC_SUCCESS) {
+			/* wait for chip to wake up */
+			msleep(PS8XXX_I2C_RECOVERY_DELAY_MS);
+			continue;
+		}
+		if (val != 0)
+			break;
+		msleep(1);
+		if (timestamp_expired(deadline, 0))
+			return EC_ERROR_UNKNOWN;
+	}
+
+	return EC_SUCCESS;
+}
+
 static int ps8xxx_get_chip_info(int port, int live,
 			struct ec_response_pd_chip_info_v1 *chip_info)
 {
@@ -629,6 +668,10 @@ static int ps8xxx_get_chip_info(int port, int live,
 			chip_info->device_id = val;
 		}
 #endif
+		rv = ps8xxx_lpm_recovery_delay(port);
+		if (rv != EC_SUCCESS)
+			return rv;
+
 		reg = get_reg_by_product(port, REG_FW_VER);
 		rv = tcpc_read(port, reg, &val);
 		if (rv != EC_SUCCESS)
@@ -744,6 +787,13 @@ static int ps8xxx_tcpm_init(int port)
 	int status;
 
 	product_id[port] = board_get_ps8xxx_product_id(port);
+
+	status = ps8xxx_lpm_recovery_delay(port);
+	if (status != EC_SUCCESS) {
+		CPRINTS("%s: C%d: ps8xxx_lpm_recovery_delay failed %d\n",
+			 __func__, port, status);
+		return status;
+	}
 
 	if (IS_ENABLED(CONFIG_USB_PD_TCPM_PS8815)) {
 		status = ps8815_transmit_buffer_workaround_check(port);
