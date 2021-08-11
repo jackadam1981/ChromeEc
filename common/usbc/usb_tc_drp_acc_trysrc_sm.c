06f15f70da944646d74ae872034cfebdb7c34907
Poornima Tom <poornima.tom@intel.com>
2021-08-11 11:48:54 -0700

    TCPMv2: Fix LPM for non-dual role auto toggle chips
    If the "dual role" is enabled and the TCPC chip does not support
    "Dual role auto toggle mode" then the PD state remains in
    PD_DRP_TOGGLE_ON state in active state of the AP. Hence check for CC
    line open state to decide to enter low power mode for such devices.
    
    BUG=b:195406641
    BRANCH=none
    TEST=Tested on ADL RVP, FUSB302 & IT83XX can enter LPM
    > pd 0 state
      Port C0 CC1, Disable - Role: SRC-UFP TC State: LowPowerMode,
      Flags: 0x0010 PE State: , Flags: 0x0001
    
    Change-Id: Ic70c6bfcd2ffd0721fdcaf6e61c68736971e037b
    Signed-off-by: Poornima Tom <poornima.tom@intel.com>

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -2305,7 +2305,8 @@ static void tc_unattached_snk_run(const int port)
 		set_state_tc(port, TC_UNATTACHED_SRC);
 	} else if (IS_ENABLED(CONFIG_USB_PD_TCPC_LOW_POWER) &&
 		   (drp_state[port] == PD_DRP_FORCE_SINK ||
-		    drp_state[port] == PD_DRP_TOGGLE_OFF)) {
+		    drp_state[port] == PD_DRP_TOGGLE_OFF ||
+		    cc_is_open(cc1, cc2))) {
 		set_state_tc(port, TC_LOW_POWER_MODE);
 	}
 }
@@ -2841,7 +2842,8 @@ static void tc_unattached_src_run(const int port)
 		set_state_tc(port, TC_DRP_AUTO_TOGGLE);
 	else if (IS_ENABLED(CONFIG_USB_PD_TCPC_LOW_POWER) &&
 		 (drp_state[port] == PD_DRP_FORCE_SOURCE ||
-		  drp_state[port] == PD_DRP_TOGGLE_OFF))
+		  drp_state[port] == PD_DRP_TOGGLE_OFF ||
+		  cc_is_open(cc1, cc2)))
 		set_state_tc(port, TC_LOW_POWER_MODE);
 }
 
