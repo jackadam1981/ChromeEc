efe3105001170dcaf729179379433f87e50a9f7b
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-08-11 11:17:50 +0800

    bq25710: add min system voltage configuration
    Add this function to let each board can control it.
    
    BUG=b:196184163
    BRANCH=none
    TEST=make -j BOARD=gimble
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Ic47c67e11eba1bfadef5f5942cff9fcad8b49ad2

--- driver/charger/bq25710.h
+++ driver/charger/bq25710.h
@@ -118,6 +118,20 @@
 						BQ25720_VMIN_AP_VSYS_TH2_SHIFT)
 #endif
 
+#if defined(CONFIG_CHARGER_BQ25720)
+#define BQ25710_MIN_SYSTEM_VOLTAGE_STEP_MV 100
+#define BQ25710_MIN_SYSTEM_VOLTAGE_SHIFT	8
+#endif
+
 extern const struct charger_drv bq25710_drv;
+
+/**
+ * Set VSYS_MIN
+ *
+ * @param chgnum: Index into charger chips
+ * @param mv: min system voltage in mV
+ * @return EC_SUCCESS or error
+ */
+int bq25710_set_min_system_voltage(int chgnum, int mv);
 
 #endif /* __CROS_EC_BQ25710_H */
