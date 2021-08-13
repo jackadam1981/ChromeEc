61025c991f9154bf1b722bf5874c432b0706b984
Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
2021-08-13 10:56:48 +0800

    collis: The Type-C PDO "PD_MAX_POWER_MW" change to 45w
    The Type-C PDO "PD_MAX_POWER_MW" change to 45w from 60w
    
    BUG=b:196325467
    BRANCH=firmware-volteer-13672.B
    TEST=manual
    Check PDO using command "ectool usbpdpower".
    
    Signed-off-by: Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
    Change-Id: Ie9d892eb22f9b486ceabfa2824819a6689b4046c

--- board/collis/board.h
+++ board/collis/board.h
@@ -68,10 +68,10 @@
 
 /*
  * SN5S30 PPC supports up to 24V VBUS source and sink, however passive USB-C
- * cables only support up to 60W.
+ * cables only support up to 45W.
  */
 #define PD_OPERATING_POWER_MW	15000
-#define PD_MAX_POWER_MW		60000
+#define PD_MAX_POWER_MW		45000
 #define PD_MAX_CURRENT_MA	3000
 #define PD_MAX_VOLTAGE_MV	20000
 
