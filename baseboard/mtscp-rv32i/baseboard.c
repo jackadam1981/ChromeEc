fe9d5281af55111e5d0e845c51aff96ae751a819
Tinghan Shen <tinghan.shen@mediatek.com>
2021-08-16 15:32:00 +0800

    new mpu for core1
    Change-Id: If2ad5ecd0b759075d26b24617096c7e4b18f8de7
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>

--- baseboard/mtscp-rv32i/baseboard.c
+++ baseboard/mtscp-rv32i/baseboard.c
@@ -10,6 +10,19 @@
 
 #define SCP_SRAM_END (CONFIG_IPC_SHARED_OBJ_ADDR & (~(0x400 - 1)))
 
+#ifdef CHIP_VARIANT_MT8195_CORE1
+struct mpu_entry mpu_entries[NR_MPU_ENTRIES] = {
+	/* SRAM (for most code, data) */
+	{0, SCP_SRAM_END, MPU_ATTR_C | MPU_ATTR_W | MPU_ATTR_R},
+	/* SRAM (for IPI shared buffer) */
+	{SCP_SRAM_END, SCP_FW_END, MPU_ATTR_W | MPU_ATTR_R},
+	/* For AP domain */
+	{0x60000000, 0x70000000, MPU_ATTR_W | MPU_ATTR_R | MPU_ATTR_P},
+	/* For SCP sys */
+	{0x70000000, 0x80000000, MPU_ATTR_W | MPU_ATTR_R},
+	{0x10000000, 0x11400000, MPU_ATTR_W | MPU_ATTR_R},
+};
+#else
 struct mpu_entry mpu_entries[NR_MPU_ENTRIES] = {
 	/* SRAM (for most code, data) */
 	{0, SCP_SRAM_END, MPU_ATTR_C | MPU_ATTR_W | MPU_ATTR_R},
@@ -25,5 +38,6 @@ struct mpu_entry mpu_entries[NR_MPU_ENTRIES] = {
 	{0x70000000, 0x80000000, MPU_ATTR_W | MPU_ATTR_R},
 	{0x10000000, 0x11400000, MPU_ATTR_W | MPU_ATTR_R},
 };
+#endif
 
 #include "gpio_list.h"
