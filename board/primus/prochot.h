6e72e71dfd41824c35f924dfcb69d757d3d0a3b4
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-08-16 11:00:21 +0800

    primus: control prochot
    BUG=b:198722634
    BRANCH=none
    TEST=make -j BOARD=primus
    TEST=verified by power team
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I777f803b91f0449317f0185a6ca77348de3835bc

---
+++ board/primus/prochot.h
@@ -0,0 +1,17 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Primus prochot configuration */
+
+#ifndef __CROS_EC_PROCHOT_H
+#define __CROS_EC_PROCHOT_H
+
+struct battery_para {
+	int battery_continue_discharge_wattage;
+	int battery_design_wattage;
+	int state_of_charge;
+};
+
+#endif /* __CROS_EC_PROCHOT_H */
