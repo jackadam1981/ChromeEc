2ecba41f1840f3a22382ae039cfad8070a865e3a
earlwang <earl_wang@compal.corp-partner.google.com>
2021-08-17 19:05:24 +0800

    Corori:Initial EC image GPIO
    Initial gpio for corori
    
    BUG=b:194356176
    BRANCH=keeby
    TEST=make -j BOARD=corori success
    
    Signed-off-by: earlwang <earl_wang@compal.corp-partner.google.com>
    Change-Id: I0065af9b0738775d2b4d06fea7acc60a0abb298b

--- board/corori/board.h
+++ board/corori/board.h
@@ -37,7 +37,6 @@
 /* Keyboard */
 
 #define CONFIG_KEYBOARD_KEYPAD
-#define CONFIG_PWM_KBLIGHT
 
 /* LED defines */
 #define CONFIG_LED_ONOFF_STATES
@@ -127,7 +126,6 @@
 #define CONFIG_GMR_TABLET_MODE
 
 /* Volume Button feature */
-#define CONFIG_ADC_BUTTONS
 #define CONFIG_VOLUME_BUTTONS
 #define GPIO_VOLUME_UP_L GPIO_VOLUP_BTN_ODL
 #define GPIO_VOLUME_DOWN_L GPIO_VOLDN_BTN_ODL
@@ -146,7 +144,6 @@ enum chg_id {
 enum adc_channel {
 	ADC_TEMP_SENSOR_1,     /* ADC0 */
 	ADC_TEMP_SENSOR_2,     /* ADC1 */
-	ADC_SUB_ANALOG,	       /* ADC2 */
 	ADC_VSNS_PP3300_A,     /* ADC9 */
 	ADC_CH_COUNT
 };
