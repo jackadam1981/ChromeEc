625666065da74cdf570a3ecfdf2c8353089681ee
earlwang <earl_wang@compal.corp-partner.google.com>
2021-08-17 19:05:24 +0800

    Corori:Initial EC image GPIO
    Initial gpio for corori
    
    BUG=b:194356176
    BRANCH=keeby
    TEST=make -j BOARD=corori success
    
    Signed-off-by: earlwang <earl_wang@compal.corp-partner.google.com>
    Change-Id: I0065af9b0738775d2b4d06fea7acc60a0abb298b

--- board/corori/board.c
+++ board/corori/board.c
@@ -189,6 +189,12 @@ static void sub_usb_c1_interrupt(enum gpio_signal s)
 	hook_call_deferred(&check_c1_line_data, INT_RECHECK_US);
 }
 
+static void c0_ccsbu_ovp_interrupt(enum gpio_signal s)
+{
+	cprints(CC_USBPD, "C0: CC OVP, SBU OVP, or thermal event");
+	pd_handle_cc_overvoltage(0);
+}
+
 #include "gpio_list.h"
 
 /* ADC channels */
