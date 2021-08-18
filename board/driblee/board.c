61c7457ffafbe06df9d15233bdb54f7c18b9a370
Matt_Wang <Matt_Wang@compal.corp-partner.google.com>
2021-08-18 12:52:24 +0800

    driblee: Add GPIOC0 EC_ACOK_OTG for AC OTG OVP protect enable
    When the Type-C AC plug-in enables charger the OTP/OVP protect
    function.
    
    BUG=b:196923269
    BRANCH=keeby
    TEST=make BOARD=driblee
    
    Signed-off-by: Matt_Wang <Matt_Wang@compal.corp-partner.google.com>
    Change-Id: If070a8c38cbf249b69df939778510fdf93e47f15

--- board/driblee/board.c
+++ board/driblee/board.c
@@ -851,3 +851,16 @@ void lid_angle_peripheral_enable(int enable)
 	}
 }
 #endif
+
+static void board_extpower(void)
+{
+	int extpower_present;
+
+	if (pd_is_connected(0))
+		extpower_present = extpower_is_present();
+	else
+		extpower_present = 0;
+
+	gpio_set_level(GPIO_EC_ACOK_OTG, extpower_present);
+}
+DECLARE_HOOK(HOOK_AC_CHANGE, board_extpower, HOOK_PRIO_DEFAULT);
