3bfb5dc898c86e13c3bda386ce38b283e7b77c46
Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
2021-08-18 10:06:20 +0800

    rammus: Add accelerometer 2nd source KX022.
    Add accelerometer 2nd source KX022.
    
    BUG=b:197005105
    BRANCH=rammus
    TEST=make BOARD=rammus
    Using command "watch ectool motionsense lid_angle".
    
    Signed-off-by: Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
    Change-Id: Iea7fee7766730a8c054217877abb76facb55976a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3102685
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Zhuohao Lee <zhuohao@chromium.org>
    (cherry picked from commit e7de2313be106dc09fdc2c67eda7c190be5fa3e4)

--- board/rammus/board.c
+++ board/rammus/board.c
@@ -11,6 +11,7 @@
 #include "bd99992gw.h"
 #include "board_config.h"
 #include "button.h"
+#include "cbi_ssfc.h"
 #include "charge_manager.h"
 #include "charge_state.h"
 #include "charge_ramp.h"
@@ -20,6 +21,12 @@
 #include "cros_board_info.h"
 #include "driver/accelgyro_bmi160.h"
 #include "driver/accel_bma2x2.h"
+<<<<<<< HEAD   (a6d79d Clear OWNERS for factory/firmware branch)
+=======
+#include "driver/accel_kionix.h"
+#include "driver/accel_kx022.h"
+#include "driver/charger/isl923x.h"
+>>>>>>> CHANGE (e7de23 rammus: Add accelerometer 2nd source KX022.)
 #include "driver/tcpm/ps8xxx.h"
 #include "driver/tcpm/tcpci.h"
 #include "driver/tcpm/tcpm.h"
@@ -601,8 +608,9 @@ static struct mutex g_base_mutex;
 
 static struct bmi160_drv_data_t g_bmi160_data;
 
-/* BMA255 private data */
+/* private data */
 static struct accelgyro_saved_data_t g_bma255_data;
+static struct kionix_accel_data g_kx022_data;
 
 /* Matrix to rotate accelrator into standard reference frame */
 const mat33_fp_t base_standard_ref = {
@@ -615,6 +623,35 @@ const mat33_fp_t lid_standard_ref = {
 	{ FLOAT_TO_FP(-1), 0, 0 },
 	{ 0,  FLOAT_TO_FP(1), 0 },
 	{ 0, 0, FLOAT_TO_FP(-1) }
+};
+
+struct motion_sensor_t lid_accel_kx022 = {
+	.name = "Lid Accel",
+	.active_mask = SENSOR_ACTIVE_S0_S3,
+	.chip = MOTIONSENSE_CHIP_KX022,
+	.type = MOTIONSENSE_TYPE_ACCEL,
+	.location = MOTIONSENSE_LOC_LID,
+	.drv = &kionix_accel_drv,
+	.mutex = &g_lid_mutex,
+	.drv_data = &g_kx022_data,
+	.port = I2C_PORT_ACCEL,
+	.i2c_spi_addr_flags = KX022_ADDR1_FLAGS,
+	.rot_standard_ref = &lid_standard_ref,
+	.min_frequency = KX022_ACCEL_MIN_FREQ,
+	.max_frequency = KX022_ACCEL_MAX_FREQ,
+	.default_range = 2, /* g, to support lid angle calculation. */
+	.config = {
+		/* EC use accel for angle detection */
+		[SENSOR_CONFIG_EC_S0] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 0,
+		},
+		/* Sensor on in S3 */
+		[SENSOR_CONFIG_EC_S3] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 0,
+		},
+	},
 };
 
 struct motion_sensor_t motion_sensors[] = {
@@ -692,6 +729,16 @@ struct motion_sensor_t motion_sensors[] = {
 	},
 };
 unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+static void board_detect_motionsense(void)
+{
+	if (get_cbi_ssfc_lid_sensor() == SSFC_SENSOR_LID_KX022) {
+		motion_sensors[LID_ACCEL] = lid_accel_kx022;
+		ccprints("LID_ACCEL is KX022");
+	} else
+		ccprints("LID_ACCEL is BMA253");
+}
+DECLARE_HOOK(HOOK_INIT, board_detect_motionsense, HOOK_PRIO_DEFAULT);
 
 /* Enable or disable input devices, based on chipset state and tablet mode */
 #ifndef TEST_BUILD
