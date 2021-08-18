eefa9e8e5014b02bf68ec9cd25b3cc92adaf0f93
Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
2021-08-18 10:06:20 +0800

    rammus: Add accelerometer 2nd source KX022.
    Add accelerometer 2nd source KX022.
    
    BUG=b:197005105
    BRANCH=rammus
    TEST=make BOARD=rammus
    Using command "watch ectool motionsense lid_angle".
    
    Signed-off-by: Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
    Change-Id: Iea7fee7766730a8c054217877abb76facb55976a

--- board/rammus/board.c
+++ board/rammus/board.c
@@ -20,6 +20,8 @@
 #include "cros_board_info.h"
 #include "driver/accelgyro_bmi_common.h"
 #include "driver/accel_bma2x2.h"
+#include "driver/accel_kionix.h"
+#include "driver/accel_kx022.h"
 #include "driver/charger/isl923x.h"
 #include "driver/tcpm/ps8xxx.h"
 #include "driver/tcpm/tcpci.h"
@@ -584,8 +586,9 @@ static struct mutex g_base_mutex;
 
 static struct bmi_drv_data_t g_bmi160_data;
 
-/* BMA255 private data */
+/* private data */
 static struct accelgyro_saved_data_t g_bma255_data;
+static struct kionix_accel_data g_kx022_data;
 
 /* Matrix to rotate accelrator into standard reference frame */
 const mat33_fp_t base_standard_ref = {
@@ -598,6 +601,35 @@ const mat33_fp_t lid_standard_ref = {
 	{ FLOAT_TO_FP(-1), 0, 0 },
 	{ 0,  FLOAT_TO_FP(1), 0 },
 	{ 0, 0, FLOAT_TO_FP(-1) }
+};
+
+struct motion_sensor_t lid_accel_kx022 = {
+	.name = "Lid Accel",
+	.active_mask = SENSOR_ACTIVE_S0_S3,
+	.chip = MOTIONSENSE_CHIP_KX022,
+	.type = MOTIONSENSE_TYPE_ACCEL,
+	.location = MOTIONSENSE_LOC_LID,
+	.drv = &kionix_accel_drv,
+	.mutex = &g_lid_mutex,
+	.drv_data = &g_kx022_data,
+	.port = I2C_PORT_ACCEL,
+	.i2c_spi_addr_flags = KX022_ADDR1_FLAGS,
+	.rot_standard_ref = &lid_standard_ref,
+	.min_frequency = KX022_ACCEL_MIN_FREQ,
+	.max_frequency = KX022_ACCEL_MAX_FREQ,
+	.default_range = 2, /* g, to support lid angle calculation. */
+	.config = {
+		/* EC use accel for angle detection */
+		[SENSOR_CONFIG_EC_S0] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 0,
+		},
+		/* Sensor on in S3 */
+		[SENSOR_CONFIG_EC_S3] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 0,
+		},
+	},
 };
 
 struct motion_sensor_t motion_sensors[] = {
@@ -675,6 +707,39 @@ struct motion_sensor_t motion_sensors[] = {
 	},
 };
 const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+enum lid_accel_type {
+	LID_ACCEL_NONE = 0,
+	LID_ACCEL_BMA253 = 1,
+	LID_ACCEL_KX022 = 2,
+};
+
+static enum lid_accel_type lid_accel_config;
+
+static void board_detect_motionsense(void)
+{
+	int val;
+	int rv;
+
+	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		return;
+	if (lid_accel_config != LID_ACCEL_NONE)
+		return;
+
+	rv = i2c_read8(I2C_PORT_ACCEL, KX022_ADDR1_FLAGS, KX022_WHOAMI, &val);
+
+	if (rv == EC_SUCCESS && val == KX022_WHO_AM_I_VAL) {
+		motion_sensors[LID_ACCEL] = lid_accel_kx022;
+		lid_accel_config = LID_ACCEL_KX022;
+		ccprints("Lid Accel: KX022");
+	} else {
+		lid_accel_config = LID_ACCEL_BMA253;
+		ccprints("Lid Accel: BMA253");
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_detect_motionsense,
+	     HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, board_detect_motionsense, HOOK_PRIO_DEFAULT);
 
 /* Enable or disable input devices, based on chipset state and tablet mode */
 __override void lid_angle_peripheral_enable(int enable)
