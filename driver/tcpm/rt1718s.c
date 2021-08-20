ea6219c41a6171a0fc3e97f26b3a5b23d841e6d2
Ting Shen <phoenixshen@google.com>
2021-08-20 16:23:30 +0800

    rt1718s: enable FRS
    BUG=b:190348051
    TEST=Combined with other CLs in the chain, verify FRS workable on Tomato
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I52a020b1288928eb9a0f3ada1364776cd8e78337

--- driver/tcpm/rt1718s.c
+++ driver/tcpm/rt1718s.c
@@ -224,13 +224,10 @@ static int rt1718s_init(int port)
 		need_sw_reset = false;
 	}
 
-	if (IS_ENABLED(CONFIG_USB_PD_FRS_TCPC))
-		/* Set vbus frs low unmasked, Rx frs unmasked */
-		RETURN_ERROR(rt1718s_update_bits8(port, RT1718S_RT_MASK1,
-					RT1718S_RT_MASK1_M_VBUS_FRS_LOW |
-					RT1718S_RT_MASK1_M_RX_FRS,
-					0xFF));
-
+	/* Set vbus frs low unmasked, Rx frs unmasked */
+	RETURN_ERROR(rt1718s_update_bits8(port, RT1718S_RT_MASK1,
+				RT1718S_RT_MASK1_M_RX_FRS,
+				0xFF));
 
 	RETURN_ERROR(rt1718s_bc12_init(port));
 
@@ -357,6 +354,26 @@ void rt1718s_vendor_defined_alert(int port)
 {
 	int rv, value;
 
+	if (IS_ENABLED(CONFIG_USB_PD_FRS)) {
+		int int1;
+
+		rv = rt1718s_read8(port, RT1718S_RT_INT1, &int1);
+		if (rv)
+			return;
+		rv = rt1718s_write8(port, RT1718S_RT_INT1, int1);
+		if (rv)
+			return;
+
+		if ((int1 & BIT(6))) {
+			pd_got_frs_signal(port);
+
+			/* ignore other interrupts for faster processing */
+			tcpc_write16(port, TCPC_REG_ALERT,
+					TCPC_REG_ALERT_VENDOR_DEF);
+			return;
+		}
+	}
+
 	/* Process BC12 alert */
 	rv = rt1718s_read8(port, RT1718S_RT_INT6, &value);
 	if (rv)
@@ -393,7 +410,9 @@ static void rt1718s_alert(int port)
 	tcpc_read16(port, TCPC_REG_ALERT, &alert);
 	if (alert & TCPC_REG_ALERT_VENDOR_DEF)
 		rt1718s_vendor_defined_alert(port);
-	tcpci_tcpc_alert(port);
+
+	if (alert & ~TCPC_REG_ALERT_VENDOR_DEF)
+		tcpci_tcpc_alert(port);
 }
 
 #ifdef CONFIG_USB_PD_TCPC_LOW_POWER
