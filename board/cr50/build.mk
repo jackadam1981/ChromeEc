019a07a92b80517bd68ce7ee70af407c743d9ec2
Vadim Sukhomlinov <sukhomlinov@google.com>
2021-08-23 16:55:35 -0700

    cr50: add support for hardware HMAC and one-shot SHA
    1. Hardware HMAC implementation is added for key lengths <= 32 bytes and
       more than 64 bytes. Keys between 32 and 64 bytes use hybrid approach.
    2. HMAC DRBG performance increased even more from 520us to 320us per
       32  bytes.
    3. Added support for one-shot SHA operation which is a bit faster than
       livestream mode when message length is known beforehand.
    4. Image size impact - +216 bytes.
    5. Added opportunities to enable keyladder code to use some common
       primitives like dcrypto_fifo_load() instead of it's own versions.
    6. Added new console command hmac activated with CRYPTO_TEST=1 to
       test all paths (hw, sw, hybrid for HMAC) for SHA256, SHA1 and
       HMAC SHA256. Due to size of test vectors, you should choose one at
       a time to test. Also, since HMAC is used by DRBG, DRBG tests are also
       relevant.
    
    BUG=b:195092622
    TEST=make CRYPTO_TEST=1; 'hmac' command in console, tests/tpmtest.py
    
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Change-Id: Icb3d8a9d0f3bd0509eb72993d5835584bc14640b

--- board/cr50/build.mk
+++ board/cr50/build.mk
@@ -19,7 +19,7 @@ ifeq ($(BOARD_MK_INCLUDED_ONCE),)
 # List of variables which can be defined in the environment or set in the make
 # command line.
 ENV_VARS := CR50_DEV CRYPTO_TEST H1_RED_BOARD U2F_TEST RND_TEST DRBG_TEST\
-	    ECDSA_TEST DCRYPTO_TEST
+	    ECDSA_TEST DCRYPTO_TEST SHA1_TEST SHA256_TEST HMAC_SHA256_TEST
 
 ifneq ($(CRYPTO_TEST),)
 CPPFLAGS += -DCRYPTO_TEST_SETUP
@@ -42,6 +42,18 @@ endif
 
 ifneq ($(DCRYPTO_TEST),)
 CPPFLAGS += -DCRYPTO_TEST_CMD_DCRYPTO_TEST=1
+endif
+
+ifneq ($(SHA1_TEST),)
+CPPFLAGS += -DSHA1_TEST=1
+endif
+
+ifneq ($(SHA256_TEST),)
+CPPFLAGS += -DSHA256_TEST=1
+endif
+
+ifneq ($(HMAC_SHA256_TEST),)
+CPPFLAGS += -DHMAC_SHA256_TEST=1
 endif
 
 endif
