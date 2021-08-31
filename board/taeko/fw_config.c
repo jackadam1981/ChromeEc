65f8612b5bb389b699fdf5be4d6cc9fa5e54d913
reno.wang <reno.wang@lcfc.corp-partner.google.com>
2021-09-01 00:45:10 +0800

    taeko: System cannot auto power on w/o TCPC1 DB
    1. Using board default for early board.
    2. Add DB detection policy for board w/o plugging DB. It can make sure
       system can power on and won't be blocked by TCPC1.
    3. Add draft PS8815 reset timing. The others should check b/194618663.
    4. Follow CL:3095438, mux HPD interface change, to update
    
    BUG=b:197585292
    BRANCH=None
    TEST=make -j BOARD=taeko w/ and w/o CONFIG_SYSTEM_UNLOCKED; test dut can
    power on w/o TCPC1 DB
    
    Signed-off-by: reno.wang <reno.wang@lcfc.corp-partner.google.com>
    Change-Id: I7fd115baa95d455b282e4ee5bfdf8ee25524e140

--- board/taeko/fw_config.c
+++ board/taeko/fw_config.c
@@ -7,7 +7,10 @@
 #include "compile_time_macros.h"
 #include "console.h"
 #include "cros_board_info.h"
+#include "driver/tcpm/ps8xxx_public.h"
 #include "fw_config.h"
+#include "gpio.h"
+#include "timer.h"
 
 #define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ## args)
 
@@ -23,6 +26,33 @@ static const union taeko_cbi_fw_config fw_config_defaults = {
 	.kb_bl = KEYBOARD_BACKLIGHT_ENABLED,
 };
 
+#ifdef CONFIG_SYSTEM_UNLOCKED
+/* DB should be plugged as default */
+static bool db_is_plugged = 1;
+
+/*
+ * b/197585292
+ * If DB isn't plugged into dut, it may cause TCPC1 initialization abnormal.
+ * This is used to detect if DB is plugged or not.
+ */
+static bool board_detect_db_is_plugged(void)
+{
+	int val, rv;
+
+	gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 1);
+	msleep(GENERIC_MAX(PS8XXX_RESET_DELAY_MS,
+			PS8815_PWR_H_RST_H_DELAY_MS));
+
+	rv = i2c_read8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_FLAGS, 0x00, &val);
+
+	gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 0);
+	msleep(PS8815_FW_INIT_DELAY_MS);
+
+	return (rv == EC_SUCCESS);
+}
+#endif /* CONFIG_SYSTEM_UNLOCKED */
+
 /****************************************************************************
  * Taeko FW_CONFIG access
  */
@@ -35,17 +65,16 @@ void board_init_fw_config(void)
 
 	if (get_board_id() == 0) {
 		/*
-		 * Early boards have a zero'd out FW_CONFIG, so replace
-		 * it with a sensible default value. If DB_USB_ABSENT2
-		 * was used as an alternate encoding of DB_USB_ABSENT to
-		 * avoid the zero check, then fix it.
+		 * Early boards doesn't have correct FW_CONFIG, so replace
+		 * it with a sensible default value.
 		 */
-		if (fw_config.raw_value == 0) {
-			CPRINTS("CBI: FW_CONFIG is zero, using board defaults");
-			fw_config = fw_config_defaults;
-		} else if (fw_config.usb_db == DB_USB_ABSENT2) {
-			fw_config.usb_db = DB_USB_ABSENT;
-		}
+		CPRINTS("CBI: Using board defaults for early board");
+		fw_config = fw_config_defaults;
+
+	#ifdef CONFIG_SYSTEM_UNLOCKED
+		db_is_plugged = board_detect_db_is_plugged();
+	#endif /* CONFIG_SYSTEM_UNLOCKED */
+
 	}
 }
 
@@ -56,6 +85,10 @@ union taeko_cbi_fw_config get_fw_config(void)
 
 enum ec_cfg_usb_db_type ec_cfg_usb_db_type(void)
 {
+#ifdef CONFIG_SYSTEM_UNLOCKED
+	if (!db_is_plugged)
+		return DB_USB_ABSENT;
+#endif /* CONFIG_SYSTEM_UNLOCKED */
 	return fw_config.usb_db;
 }
 
--- board/taeko/usbc_config.c
+++ board/taeko/usbc_config.c
@@ -151,6 +151,25 @@ void config_usb_db_type(void)
 	CPRINTS("Configured USB DB type number is %d", db_type);
 }
 
+static void ps8815_reset(void)
+{
+	int val;
+
+	CPRINTS("%s: patching ps8815 registers", __func__);
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		      PS8751_I2C_ADDR1_P2_FLAGS, 0x0f, &val) == EC_SUCCESS)
+		CPRINTS("ps8815: reg 0x0f was %02x", val);
+
+	if (i2c_write8(I2C_PORT_USB_C1_TCPC,
+		       PS8751_I2C_ADDR1_P2_FLAGS, 0x0f, 0x31) == EC_SUCCESS)
+		CPRINTS("ps8815: reg 0x0f set to 0x31");
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		      PS8751_I2C_ADDR1_P2_FLAGS, 0x0f, &val) == EC_SUCCESS)
+		CPRINTS("ps8815: reg 0x0f now %02x", val);
+}
+
 void board_reset_pd_mcu(void)
 {
 	/*
@@ -158,20 +177,26 @@ void board_reset_pd_mcu(void)
 	 */
 
 	gpio_set_level(GPIO_USB_C0_TCPC_RST_ODL, 0);
-	gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 0);
+	if (ec_cfg_usb_db_type() != DB_USB_ABSENT)
+		gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 0);
 
 	/*
 	 * delay for power-on to reset-off and min. assertion time
 	 */
-
-	msleep(20);
+	msleep(GENERIC_MAX(PS8XXX_RESET_DELAY_MS,
+			   PS8815_PWR_H_RST_H_DELAY_MS));
 
 	gpio_set_level(GPIO_USB_C0_TCPC_RST_ODL, 1);
-	gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 1);
+	if (ec_cfg_usb_db_type() != DB_USB_ABSENT)
+		gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 1);
 
 	/* wait for chips to come up */
-
-	msleep(50);
+	msleep(PS8815_FW_INIT_DELAY_MS);
+	if (ec_cfg_usb_db_type() != DB_USB_ABSENT) {
+		ps8815_reset();
+		usb_mux_hpd_update(USBC_PORT_C1, USB_PD_MUX_HPD_LVL_DEASSERTED |
+				   USB_PD_MUX_HPD_IRQ_DEASSERTED);
+	}
 }
 
 
@@ -193,15 +218,18 @@ static void board_tcpc_init(void)
 
 	/* Enable PPC interrupts. */
 	gpio_enable_interrupt(GPIO_USB_C0_PPC_INT_ODL);
-	gpio_enable_interrupt(GPIO_USB_C1_PPC_INT_ODL);
+	if (ec_cfg_usb_db_type() != DB_USB_ABSENT)
+		gpio_enable_interrupt(GPIO_USB_C1_PPC_INT_ODL);
 
 	/* Enable TCPC interrupts. */
 	gpio_enable_interrupt(GPIO_USB_C0_TCPC_INT_ODL);
-	gpio_enable_interrupt(GPIO_USB_C1_TCPC_INT_ODL);
+	if (ec_cfg_usb_db_type() != DB_USB_ABSENT)
+		gpio_enable_interrupt(GPIO_USB_C1_TCPC_INT_ODL);
 
 	/* Enable BC1.2 interrupts. */
 	gpio_enable_interrupt(GPIO_USB_C0_BC12_INT_ODL);
-	gpio_enable_interrupt(GPIO_USB_C1_BC12_INT_ODL);
+	if (ec_cfg_usb_db_type() != DB_USB_ABSENT)
+		gpio_enable_interrupt(GPIO_USB_C1_BC12_INT_ODL);
 }
 DECLARE_HOOK(HOOK_INIT, board_tcpc_init, HOOK_PRIO_INIT_CHIPSET);
 
@@ -210,7 +238,7 @@ uint16_t tcpc_get_alert_status(void)
 	uint16_t status = 0;
 
 	if (gpio_get_level(GPIO_USB_C0_TCPC_INT_ODL) == 0)
-		status |= PD_STATUS_TCPC_ALERT_0 | PD_STATUS_TCPC_ALERT_2;
+		status |= PD_STATUS_TCPC_ALERT_0;
 
 	if (gpio_get_level(GPIO_USB_C1_TCPC_INT_ODL) == 0)
 		status |= PD_STATUS_TCPC_ALERT_1;
@@ -264,7 +292,6 @@ void ppc_interrupt(enum gpio_signal signal)
 	case GPIO_USB_C1_PPC_INT_ODL:
 		nx20p348x_interrupt(USBC_PORT_C1);
 		break;
-
 	default:
 		break;
 	}
@@ -273,4 +300,11 @@ void ppc_interrupt(enum gpio_signal signal)
 __override bool board_is_dts_port(int port)
 {
 	return port == USBC_PORT_C0;
+}
+__override uint8_t board_get_usb_pd_port_count(void)
+{
+	if (ec_cfg_usb_db_type() == DB_USB_ABSENT)
+		return CONFIG_USB_PD_PORT_MAX_COUNT - 1;
+	else
+		return CONFIG_USB_PD_PORT_MAX_COUNT;
 }
