dd5b05ab6ae2a94a4d4fad922fc24e445b1f4898
Aaron Massey <aaronmassey@google.com>
2021-09-01 20:25:13 +0000

    zephyr: --wip compiling new files with function ptr added. Tests fail successfully.
    Change-Id: If1be69152fdae3b275291ce8e10d943fa1a658e6

---
+++ zephyr/shim/src/zthermistor.c
@@ -0,0 +1,15 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+
+
+#include "temp_sensor/zthermistor.h"
+
+// I shall not compile
+
+int get_temp(int idx_adc, int *temp_ptr)
+{
+	return 0;
+}
