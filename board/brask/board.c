9a60f9def1dd1bede6e7cabc45d85e6b5ddcb695
David Huang <david.huang@quanta.corp-partner.google.com>
2021-09-02 09:49:12 +0800

    brask: Add cbi_init function
    Add cbi_init function
    
    BUG=none
    BRANCH=none
    TEST=make buildall -j succeeded.
    
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Change-Id: I89ad170ef418b6f771056baa130165c5592ef636

--- board/brask/board.c
+++ board/brask/board.c
@@ -6,8 +6,10 @@
 #include "common.h"
 #include "compile_time_macros.h"
 #include "console.h"
+#include "cros_board_info.h"
 #include "gpio.h"
 #include "gpio_signal.h"
+#include "hooks.h"
 #include "power_button.h"
 #include "power.h"
 #include "switch.h"
@@ -39,3 +41,28 @@ void board_set_charge_limit(int port, int supplier, int charge_ma,
 				int max_ma, int charge_mv)
 {
 }
+
+static uint16_t board_version;
+static uint32_t sku_id;
+static uint32_t fw_config;
+
+static void cbi_init(void)
+{
+	/*
+	 * Load board info from CBI to control per-device configuration.
+	 *
+	 * If unset it's safe to treat the board as a proto, just C10 gating
+	 * won't be enabled.
+	 */
+	uint32_t val;
+
+	if (cbi_get_board_version(&val) == EC_SUCCESS && val <= UINT16_MAX)
+		board_version = val;
+	if (cbi_get_sku_id(&val) == EC_SUCCESS)
+		sku_id = val;
+	if (cbi_get_fw_config(&val) == EC_SUCCESS)
+		fw_config = val;
+	CPRINTS("Board Version: %d, SKU ID: 0x%08x, F/W config: 0x%08x",
+		board_version, sku_id, fw_config);
+}
+DECLARE_HOOK(HOOK_INIT, cbi_init, HOOK_PRIO_INIT_I2C + 1);
