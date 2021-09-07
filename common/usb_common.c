bd3ff0baf1561145b71a90bb2a416e363b9771a7
Vijay Hiremath <vijay.p.hiremath@intel.com>
2021-09-07 12:07:46 -0700

    Type-C: add support for both PPC & non-PPC ports
    BUG=none
    BRANCH=none
    TEST=none
    
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>
    Change-Id: I5289f41664c9c5bf6c4966c9dcf79274cebe8c2b

--- common/usb_common.c
+++ common/usb_common.c
@@ -527,7 +527,7 @@ void usb_mux_set_safe_mode(int port)
 	}
 
 	/* Isolate the SBU lines. */
-	if (IS_ENABLED(CONFIG_USBC_PPC_SBU))
+	if (IS_ENABLED(CONFIG_USBC_PPC_SBU) && board_is_port_ppc(port))
 		ppc_set_sbu(port, 0);
 }
 
@@ -538,7 +538,7 @@ void usb_mux_set_safe_mode_exit(int port)
 			    polarity_rm_dts(pd_get_polarity(port)));
 
 	/* Isolate the SBU lines. */
-	if (IS_ENABLED(CONFIG_USBC_PPC_SBU))
+	if (IS_ENABLED(CONFIG_USBC_PPC_SBU) && board_is_port_ppc(port))
 		ppc_set_sbu(port, 0);
 }
 
@@ -672,7 +672,7 @@ __overridable void pd_transition_voltage(int idx)
 
 __overridable void typec_set_source_current_limit(int p, enum tcpc_rp_value rp)
 {
-	if (IS_ENABLED(CONFIG_USBC_PPC))
+	if (IS_ENABLED(CONFIG_USBC_PPC) && board_is_port_ppc(p))
 		ppc_set_vbus_source_current_limit(p, rp);
 }
 
