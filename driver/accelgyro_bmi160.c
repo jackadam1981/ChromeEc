3250890213ecd70bff7812063391e96f5b7baa32
Gwendal Grignou <gwendal@chromium.org>
2021-09-08 10:20:58 -0700

    driver: bmi: Merge list_activities in common code
    Put list_activities in common code, as it is used by both BMI160 and
    BMI260 driver.
    
    Fixes cb3771973de ("Coachz: MotionSensor: Avoid redefine gesture function list_activites")
    Fixes fc298a0ada0 ("driver: bmi260: integrate body detection")
    
    BUG=b:195908820
    BRANCH=trogdor
    TEST=buildall
    
    Conflicts:
    	driver/accelgyro_bmi_common.c: 851daf728d: motion orientation interface
    
    Change-Id: I05591954c825802502328c842b03e8934c497f08
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3150056
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    (cherry picked from commit 8b37b17df0b8946763b1da984c64c51418a6b381)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3213986
    Tested-by: Rong Chang <rongchang@chromium.org>
    Auto-Submit: Rong Chang <rongchang@chromium.org>
    Commit-Queue: Rong Chang <rongchang@chromium.org>

--- driver/accelgyro_bmi160.c
+++ driver/accelgyro_bmi160.c
@@ -344,18 +344,6 @@ int manage_activity(const struct motion_sensor_t *s,
 	return ret;
 }
 
-#ifdef CONFIG_GESTURE_HOST_DETECTION
-int list_activities(const struct motion_sensor_t *s,
-		    uint32_t *enabled,
-		    uint32_t *disabled)
-{
-	struct bmi_drv_data_t *data = BMI_GET_DATA(s);
-	*enabled = data->enabled_activities;
-	*disabled = data->disabled_activities;
-	return EC_RES_SUCCESS;
-}
-#endif
-
 #ifdef CONFIG_ACCEL_INTERRUPTS
 /**
  * bmi160_interrupt - called when the sensor activates the interrupt line.
@@ -721,7 +709,7 @@ const struct accelgyro_drv bmi160_drv = {
 #endif
 #ifdef CONFIG_GESTURE_HOST_DETECTION
 	.manage_activity = manage_activity,
-	.list_activities = list_activities,
+	.list_activities = bmi_list_activities,
 #endif
 };
 
