0660fd160b8bb823cc416e874e63917ff7a59b71
YongBeum.Ha <ybha@samsung.corp-partner.google.com>
2021-09-08 15:55:42 +0900

    bugzzy : set VSN/VSP/VBST
    change voltage from 5V to 5.5V
    
    BUG=b:198072674
    
    BRANCH=None
    TEST=make -j BOARD=bugzzy
    
    Signed-off-by: YongBeum.Ha <ybha@samsung.corp-partner.google.com>
    Change-Id: I430f40272ed9ade025f58c60b90d255bb3940374

--- board/bugzzy/board.c
+++ board/bugzzy/board.c
@@ -731,6 +731,15 @@ static void panel_power_change_deferred(void)
 	if (signal == 0 && board_id >= 3)
 		return;
 
+	if (board_id >= 3) {
+		i2c_write8(I2C_PORT_LCD, I2C_ADDR_ISL98607_FLAGS,
+				ISL98607_REG_VBST_OUT, ISL98607_VBST_OUT_5P5);
+		i2c_write8(I2C_PORT_LCD, I2C_ADDR_ISL98607_FLAGS,
+				ISL98607_REG_VN_OUT, ISL98607_VN_OUT_5P5);
+		i2c_write8(I2C_PORT_LCD, I2C_ADDR_ISL98607_FLAGS,
+				ISL98607_REG_VP_OUT, ISL98607_VP_OUT_5P5);
+	}
+
 	gpio_set_level(GPIO_EN_LCD_ENP, signal);
 	msleep(1);
 	gpio_set_level(GPIO_EN_LCD_ENN, signal);
