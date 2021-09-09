796260e3878b7e9a157f6a610ec189baf09f872c
Tang Qijun <qijun.tang@ecs.corp-partner.google.com>
2021-09-09 01:22:28 -0400

    pazquel: Implement battery protection for deep charge state
    Deep charge inspection is a battery protection mechanism: If
    battery voltage is lower than 6V, deep charge for more than 30 minutes
    The battery voltage is still lower than 6V, the system will stop charging
    
    BRANCH=trogdor
    BUG=b:198226241
    TEST=Keep the battery voltage below 6V and charge it for more than 30
      minutes. The system stops the charging function and the charging
      current is zero.
    
    Signed-off-by: Tang Qijun <qijun.tang@ecs.corp-partner.google.com>
    Change-Id: I89adf7809992b9e71b232ad1129af1407acf05a2

--- include/config.h
+++ include/config.h
@@ -609,6 +609,21 @@
 #undef CONFIG_BATTERY_REVIVE_DISCONNECT
 
 /*
+ * Deep charge inspection is a battery protection mechanism:
+ * If battery voltage is lower than voltage_min, deep charge for more
+ * than precharge time The battery voltage is still lower than voltage_min,
+ * the system will stop charging
+ */
+#undef CONFIG_BATTERY_DEEP_CHARGE
+
+/*
+ * If battery voltage is lower than voltage_min, deep charge timer no more
+ * than 30 minutes , if need change the value , redefine
+ * CONFIG_BATTERY_DEEP_CHARGE_TIMEOUT_US
+ */
+#define CONFIG_BATTERY_DEEP_CHARGE_TIMEOUT_US  (30*60*1000000)
+
+/*
  * Specify the battery percentage at which the host is told it is full.
  * If this value is not specified the default is 97% set in battery.h.
  */
