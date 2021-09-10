8f2ff8a510e323dd70de8c09c8c20b3bd0789067
Ting Shen <phoenixshen@google.com>
2021-09-10 17:39:07 +0800

    cherry: adjust charger input voltage limit
    See b:198707662 comment#9 for more detail.
    
    BUG=b:198707662
    TEST=check the register value of 0x4B
    BRANCH=main
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I148e8943b295a73d27a9bc381309d074b053fb85

--- baseboard/cherry/baseboard.c
+++ baseboard/cherry/baseboard.c
@@ -78,6 +78,17 @@ const struct charger_config_t chg_chips[] = {
 	},
 };
 
+/* Override default setting, called after charger_chips_init */
+static void baseboard_charger_init(void)
+{
+	int reg = (4096 / ISL9238_INPUT_VOLTAGE_REF_STEP)
+			<< ISL9238_INPUT_VOLTAGE_REF_SHIFT;
+
+	i2c_write16(I2C_PORT_CHARGER, ISL923X_ADDR_FLAGS,
+			ISL9238_REG_INPUT_VOLTAGE, reg);
+}
+DECLARE_HOOK(HOOK_INIT, baseboard_charger_init, HOOK_PRIO_DEFAULT + 2);
+
 __override void board_hibernate_late(void)
 {
 	/*
