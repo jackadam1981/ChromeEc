bdf81a2cc4f62add2b57c2414a96cded9109054f
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2021-09-14 10:30:40 +0800

    TEST-ONLY: power number of it81202
    Change-Id: I8cb9e102f2d158a66a0c2a412f5b2933bc1f078f

--- chip/it83xx/spi_master.c
+++ chip/it83xx/spi_master.c
@@ -168,6 +168,6 @@ static void sspi_init(void)
 
 	for (i = 0; i < spi_devices_used; i++)
 		/* Disabling spi module */
-		spi_enable(&spi_devices[i], 0);
+		spi_enable(&spi_devices[i], 1);
 }
 DECLARE_HOOK(HOOK_INIT, sspi_init, HOOK_PRIO_INIT_SPI);
