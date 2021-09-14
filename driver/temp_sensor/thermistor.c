99ea71db84254a288721736ac8be6b64917eb013
Aaron Massey <aaronmassey@google.com>
2021-09-14 12:00:47 -0600

    zephyr: WIP thermistors dt node
    Working on changing the dt and thermistor.c tests now.
    
    Lot of cleanup to do.
    
    Cq-Depend: chromium:3161332
    
    Change-Id: Ic5330cd5c33e79e192428ca857651de9a225856e

--- driver/temp_sensor/thermistor.c
+++ driver/temp_sensor/thermistor.c
@@ -71,7 +71,7 @@ int thermistor_linear_interpolate(uint16_t mv,
 	defined(CONFIG_STEINHART_HART_6V0_51K1_47K_4050B) || \
 	defined(CONFIG_STEINHART_HART_3V0_22K6_47K_4050B) || \
 	defined(CONFIG_STEINHART_HART_3V3_30K9_47K_4050B)
-static int thermistor_get_temperature(int idx_adc, int *temp_ptr,
+int thermistor_get_temperature(int idx_adc, int *temp_ptr,
 		const struct thermistor_info *info)
 {
 	int mv;
