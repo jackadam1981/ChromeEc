10d1449d6e11b74db190aa6b92b350c08fce8600
Diana Z <dzigterman@chromium.org>
2021-09-15 21:45:08 -0600

    BB Retimer: Add new HPD callback to boards
    Now that the BB retimer has its own HPD interface, add this interface to
    every board using the bb_usb_retimer driver.
    
    BRANCH=None
    BUG=b:195773400
    TEST=on voxel, pass tast typec.Mode*.manual
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: Ia70d139431739e8f2c0577359cb3aaa7fb906d0a

--- board/felwinter/usbc_config.c
+++ board/felwinter/usbc_config.c
@@ -127,6 +127,7 @@ struct usb_mux usb_muxes[] = {
 		.driver = &bb_usb_retimer,
 		.i2c_port = I2C_PORT_USB_C2_MUX,
 		.i2c_addr_flags = USBC_PORT_BB_RETIMER_I2C_ADDR,
+		.hpd_update = bb_retimer_hpd_update,
 		.next_mux = &usbc2_tcss_usb_mux,
 	},
 	[USBC_PORT_C1] = {
@@ -142,6 +143,7 @@ BUILD_ASSERT(ARRAY_SIZE(usb_muxes) == USBC_PORT_COUNT);
 struct usb_mux usb_muxes_c1 = {
 		.usb_port = USBC_PORT_C1,
 		.driver = &bb_usb_retimer,
+		.hpd_update = bb_retimer_hpd_update,
 		.i2c_port = I2C_PORT_USB_C1_MUX,
 		.i2c_addr_flags = USBC_PORT_BB_RETIMER_I2C_ADDR,
 		.next_mux = &usbc1_tcss_usb_mux,
