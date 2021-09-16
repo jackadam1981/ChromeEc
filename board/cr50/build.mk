eac4b139c6b6b7e872ee56a4c244fc247a049e01
Howard Yang <hcyang@google.com>
2021-09-16 15:51:56 +0800

    cr50: improve toolchain
    Adjust Makefile toolchain setting so that fuzzers can generate coverage
    mapping correctly and generate debug symbols for source-based coverage
    report. Also, update gitignore to ignore local vscode settings.
    
    BUG=none
    TEST=make buildall -j
    
    Change-Id: I6d5c720895cbb9119c9266df998aa5cc308c1e61
    Signed-off-by: Howard Yang <hcyang@google.com>

--- board/cr50/build.mk
+++ board/cr50/build.mk
@@ -11,7 +11,7 @@ CHIP_FAMILY:=cr50
 CHIP_VARIANT ?= cr50_fpga
 
 # Additional / overriding warnings for common rules and chip
-CFLAGS_BOARD :=-Warray-parameter -Wstringop-overread
+CFLAGS_BOARD :=-Wno-array-parameter -Wno-stringop-overread
 
 # This file is included twice by the Makefile, once to determine the CHIP info
 # and then again after defining all the CONFIG_ and HAS_TASK variables. We use
