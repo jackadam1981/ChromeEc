6da7bc80a13a3d1a3000d7e820c7c9cb93cbb439
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2021-09-17 17:26:04 +0800

    (TEST) test Vconn switch
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>
    Change-Id: I006452b5e0795ab49e2eac1846427e2c70ecf4b4

--- board/haboki/board.c
+++ board/haboki/board.c
@@ -598,10 +598,10 @@ void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled)
 	if (port)
 		return;
 
-	if (cc_pin == USBPD_CC_PIN_1)
-		gpio_set_level(GPIO_EN_USB_C0_CC1_VCONN, !!enabled);
-	else
-		gpio_set_level(GPIO_EN_USB_C0_CC2_VCONN, !!enabled);
+	//if (cc_pin == USBPD_CC_PIN_1)
+	//	gpio_set_level(GPIO_EN_USB_C0_CC1_VCONN, !!enabled);
+	//else
+	//	gpio_set_level(GPIO_EN_USB_C0_CC2_VCONN, !!enabled);
 }
 
 __override void typec_set_source_current_limit(int port, enum tcpc_rp_value rp)
