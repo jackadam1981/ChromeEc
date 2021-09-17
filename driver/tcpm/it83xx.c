a5922d47acfe72d25387340366a73c7b00c5dd92
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2021-09-17 17:26:04 +0800

    (TEST) test Vconn switch
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>
    Change-Id: I006452b5e0795ab49e2eac1846427e2c70ecf4b4

--- driver/tcpm/it83xx.c
+++ driver/tcpm/it83xx.c
@@ -284,6 +284,9 @@ static void it83xx_enable_vconn(enum usbpd_port port, int enabled)
 	else
 		cc_pin = USBPD_CC_PIN_2;
 
+	if (port == 0)
+		ccprints("p%d cc%d Vconn%d en/dis%d", port, cc_pin, !cc_pin, enabled);
+
 	if (enabled) {
 		/* Disable unused CC to become VCONN */
 		if (cc_pin == USBPD_CC_PIN_1) {
