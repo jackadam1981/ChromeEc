306ef5eb332fbd52f9cd5a0d7c4d9b30383af53e
Parth Malkan <parthmalkan@google.com>
2021-09-20 21:09:32 +0000

    SSFC: Support two charger sources on pirika
    Pirika has a requirement to support more than one charger sources. This
    code adds a capability to enable RUNTIME flags and set specific flags
    based on the charger source selected. New charger_type field is added in
    cbi_ssfc to select between multiple charger sources.
    
    BRANCH=None
    BUG=b:200598507
    TEST=make -j buildall
    
    Signed-off-by: Parth Malkan <parthmalkan@google.com>
    Change-Id: I03239828a2e43cc2b6374eb49871dbab56c929c1

--- board/pirika/cbi_ssfc.h
+++ board/pirika/cbi_ssfc.h
@@ -33,11 +33,20 @@ enum ec_ssfc_lid_sensor {
 	SSFC_SENSOR_LIS2DWL = 3
 };
 
+/*
+ * Charger Type (Bit 6-7)
+ */
+enum ec_ssfc_charger_type {
+	SSFC_CHARGER_RAA489000 = 0,
+	SSFC_CHARGER_SM5803 = 1
+};
+
 union dedede_cbi_ssfc {
 	struct {
 		uint32_t base_sensor : 3;
 		uint32_t lid_sensor : 3;
-		uint32_t reserved_2 : 26;
+		uint32_t charger_type : 2;
+		uint32_t reserved_2 : 24;
 	};
 	uint32_t raw_value;
 };
@@ -56,5 +65,11 @@ enum ec_ssfc_base_sensor get_cbi_ssfc_base_sensor(void);
  */
 enum ec_ssfc_lid_sensor get_cbi_ssfc_lid_sensor(void);
 
+/**
+ * Get the charger type from SSFC_CONFIG.
+ *
+ * @return the charger type.
+ */
+enum ec_ssfc_charger_type get_cbi_ssfc_charger_type(void);
 
 #endif /* _DEDEDE_CBI_SSFC__H_ */
