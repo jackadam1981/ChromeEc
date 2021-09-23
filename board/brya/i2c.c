aa20efc8cd9eb1073c4e7a6ec9ad49638e0e5721
Caveh Jalali <caveh@chromium.org>
2021-09-23 13:56:33 -0700

    brya: Run TCPC1/PPC1 I2C at 1 MHz on newer boards
    This updates the TCPC1 and PPC1 (ps8815 daughterboard) I2C buses to
    operate at 1 MHz instead of 400 kHz. Boards before board ID 2 were not
    tuned for 1 MHz operation, so they will continue to operate at 400 kHz.
    
    BRANCH=none
    BUG=b:186189039,b:187764571
    TEST=updated TCPC1 firmware using window scheme and FIFO scheme.
    TEST=verified I2C bus 4, 7 speed is 1000 on board ID 2 and at 400 when
    	BOARD_ID_FAST_PLUS_CAPABLE is set to 9.
    
    Change-Id: I182cb6bc0d1be24d512ef14d16e2cb4347b7eb02
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- board/brya/i2c.c
+++ board/brya/i2c.c
@@ -5,8 +5,10 @@
 
 #include "common.h"
 #include "compile_time_macros.h"
+#include "hooks.h"
+#include "i2c_private.h"
 
-#include "i2c.h"
+#define BOARD_ID_FAST_PLUS_CAPABLE	2
 
 /* I2C port map configuration */
 const struct i2c_port_t i2c_ports[] = {
@@ -46,7 +48,7 @@ const struct i2c_port_t i2c_ports[] = {
 		/* I2C4 C1 TCPC */
 		.name = "tcpc1",
 		.port = I2C_PORT_USB_C1_TCPC,
-		.kbps = 400,
+		.kbps = 1000,
 		.scl = GPIO_EC_I2C_USB_C1_TCPC_SCL,
 		.sda = GPIO_EC_I2C_USB_C1_TCPC_SDA,
 		.flags = I2C_PORT_FLAG_DYNAMIC_SPEED,
@@ -63,7 +65,7 @@ const struct i2c_port_t i2c_ports[] = {
 		/* I2C6 */
 		.name = "ppc1",
 		.port = I2C_PORT_USB_C1_PPC,
-		.kbps = 400,
+		.kbps = 1000,
 		.scl = GPIO_EC_I2C_USB_C1_MIX_SCL,
 		.sda = GPIO_EC_I2C_USB_C1_MIX_SDA,
 	},
@@ -77,3 +79,26 @@ const struct i2c_port_t i2c_ports[] = {
 	},
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+static void i2c_set_fast_only(int chip_i2c_port)
+{
+	i2c_lock(chip_i2c_port, 1);
+	chip_i2c_set_freq(chip_i2c_port, I2C_FREQ_400KHZ);
+	i2c_lock(chip_i2c_port, 0);
+}
+
+/*
+ * I2C controllers are initialized in main.c. This sets the speed much
+ * later, but before I2C peripherals are initialized.
+ */
+static void set_board_legacy_i2c_speeds(void)
+{
+	if (get_board_id() >= BOARD_ID_FAST_PLUS_CAPABLE)
+		return;
+
+	ccprints("setting USB DB I2C buses to 400 kHz\n");
+
+	i2c_set_fast_only(I2C_PORT_USB_C1_TCPC);
+	i2c_set_fast_only(I2C_PORT_USB_C1_PPC);
+}
+DECLARE_HOOK(HOOK_INIT, set_board_legacy_i2c_speeds, HOOK_PRIO_INIT_I2C - 1);
