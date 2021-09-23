aeb34f737f5d076e3ab203af231a36dffe6d8c4c
Ting Shen <phoenixshen@google.com>
2021-09-23 18:07:47 +0800

    WIP: rt9490 driver
    BUG=b:194967754
    TEST=work on hayato
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: If334aacafe8f23fdab4558b44fe6ac94a3c7a0be

--- zephyr/projects/corsola/src/krabby/usbc_config.c
+++ zephyr/projects/corsola/src/krabby/usbc_config.c
@@ -11,6 +11,7 @@
 #include "charge_manager.h"
 #include "charger.h"
 #include "console.h"
+#include "driver/charger/rt9490.h"
 #include "driver/tcpm/it83xx_pd.h"
 #include "driver/usb_mux/ps8743.h"
 #include "hooks.h"
@@ -28,8 +29,8 @@
 const struct charger_config_t chg_chips[] = {
 	{
 		.i2c_port = I2C_PORT_CHARGER,
-		.i2c_addr_flags = 0,
-		.drv = NULL,
+		.i2c_addr_flags = RT9490_ADDR_FLAGS,
+		.drv = &rt9490_drv,
 	},
 };
 
