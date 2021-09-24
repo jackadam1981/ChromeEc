6e9e61fe4e6ae20ed4bb5c73561353f52102bebc
Parth Malkan <parthmalkan@google.com>
2021-09-23 18:12:35 -0700

    Zephyr: Implement a generic battery led SM
    Implement a generic battery led state machine for gpio led using device
    tree. Currently this is implemented for lazor device as a first step.
    It would be eventually converted to a standalone driver. This will be
    applicable across all boards removing the need to have led.c
    implementation for each board.
    
    BRANCH=none
    BUG=b:201007290
    TEST=compared logs to verify the color value in each state
    
    Signed-off-by: Parth Malkan <parthmalkan@google.com>
    Change-Id: I571937457f9eb41d5949ffb8d1382d89ee118ba8

--- zephyr/projects/trogdor/lazor/src/led.c
+++ zephyr/projects/trogdor/lazor/src/led.c
@@ -17,10 +17,15 @@
 #include "system.h"
 #include "util.h"
 
+#include <devicetree.h>
+
 #define LED_ONE_SEC (1000 / HOOK_TICK_INTERVAL_MS)
 
 #define BAT_LED_ON 1
 #define BAT_LED_OFF 0
+
+#define BATT_LED_NODE    DT_PATH(gpio_led, battery_led_colors)
+#define LED_CONTROL      DT_NODELABEL(led_control)
 
 const enum ec_led_id supported_led_ids[] = {
 	EC_LED_ID_BATTERY_LED,
@@ -43,10 +48,13 @@ static void led_set_color(enum led_color color)
 		(color == LED_BLUE) ? BAT_LED_ON : BAT_LED_OFF);
 }
 
+static const uint8_t dt_brigthness_range[EC_LED_COLOR_COUNT] =
+	DT_PROP(DT_PATH(gpio_led, brightness_range), brightness_range_battery);
+
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
-	brightness_range[EC_LED_COLOR_AMBER] = 1;
-	brightness_range[EC_LED_COLOR_BLUE] = 1;
+	memcpy(brightness_range, dt_brigthness_range,
+		sizeof(dt_brigthness_range));
 }
 
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
@@ -61,7 +69,107 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 	return EC_SUCCESS;
 }
 
-static void board_led_set_battery(void)
+enum chipset {
+	S0 = 0,
+	S3,
+	S5,
+	ANY,
+	CHIPSET_STATE_COUNT
+};
+
+struct values {
+	int color_1;
+	int color_2;
+	int color_3;
+	int period;
+	int on_sec;
+};
+
+struct policy {
+	bool chipset_state_enabled;
+	bool chflags_enabled;
+};
+
+struct policy led_policy[PWR_STATE_COUNT];
+struct values led_values[PWR_STATE_COUNT][CHIPSET_STATE_COUNT];
+
+/* Lazor example
+ * Battery LED color in PWR_STATE_DISCHARGE also depends on the chipset_state
+ * Eg. for chipset_state S0 color is LED_BLUE, but for S3 it is blinking. So,
+ * led_policy[PWR_STATE_DISCHARGE].chipset_state_enabled will be set to true.
+ * However, for the rest of the states it is set to false. Similarly, chflags
+ * are only used in PWR_STATE_IDLE. So
+ * led_policy[PWR_STATE_IDLE].chflags_enabled is set to true and for rest of
+ * the states it is set to false
+ *
+ * led_values describe what color to set for the combination of pwr_state and
+ * chipset_state. It also describes if the leds need to blink and if so
+ * what is the period (total led ticks) and color_1 and color_2. Eg.
+ * led_values[PWR_STATE_DISCHARGE][S3].color_1 = LED_AMBER;
+ * led_values[PWR_STATE_DISCHARGE][S3].color_2 = LED_OFF;
+ * led_values[PWR_STATE_DISCHARGE][S3].period = 4;
+ * led_values[PWR_STATE_DISCHARGE][S3].on_sec = 1;
+ * So 1 sec amber, 3 sec off.
+ * For pwr_state where led isn't blinking and also doesn't depend on chipset
+ * state:
+ * led_values[PWR_STATE_CHARGE][ANY].color_1 = LED_AMBER;
+ * led_values[PWR_STATE_CHARGE][ANY].period = 0;
+ * led_values[PWR_STATE_CHARGE][ANY].on_sec = 0;
+ */
+
+#define CHARGE_STATE(id)  DT_STRING_UPPER_TOKEN(id, charge_state)
+#define CHIPSET_STATE(id) DT_STRING_UPPER_TOKEN(id, chipset_state)
+#define COLOR(id, color)  DT_STRING_UPPER_TOKEN(DT_CHILD(id, color), led_color)
+
+#define SET_LED_POLICY_AND_VALUES(id)					       \
+	do {								       \
+		if (DT_PROP(id, chflags))				       \
+			led_policy[CHARGE_STATE(id)].chflags_enabled = true;   \
+		else							       \
+			led_policy[CHARGE_STATE(id)].chflags_enabled = false;  \
+		if (CHIPSET_STATE(id) != ANY)				       \
+			led_policy[CHARGE_STATE(id)].chipset_state_enabled     \
+								   = true;     \
+		else							       \
+			led_policy[CHARGE_STATE(id)].chipset_state_enabled     \
+								   = false;    \
+		led_values[CHARGE_STATE(id)][CHIPSET_STATE(id)].period =       \
+			DT_PROP(id, period);				       \
+		led_values[CHARGE_STATE(id)][CHIPSET_STATE(id)].on_sec =       \
+			DT_PROP(id, on_sec);				       \
+		led_values[CHARGE_STATE(id)][CHIPSET_STATE(id)].color_1 =      \
+			COLOR(id, color_1);				       \
+		IF_ENABLED(DT_NODE_EXISTS(DT_CHILD(id, color_2)),	       \
+			(led_values[CHARGE_STATE(id)][CHIPSET_STATE(id)].      \
+			 color_2 = COLOR(id, color_2);))		       \
+		IF_ENABLED(DT_NODE_EXISTS(DT_CHILD(id, color_3)),	       \
+			(led_values[CHARGE_STATE(id)][CHIPSET_STATE(id)].      \
+			 color_3 = COLOR(id, color_3);))		       \
+	} while (0);
+
+static void led_init(void)
+{
+#if DT_NODE_EXISTS(BATT_LED_NODE)
+	DT_FOREACH_CHILD(BATT_LED_NODE, SET_LED_POLICY_AND_VALUES)
+#endif
+}
+DECLARE_HOOK(HOOK_INIT, led_init, HOOK_PRIO_DEFAULT);
+
+static int get_led_color(int battery_ticks, enum charge_state pwr_state,
+						enum chipset chipset_state)
+{
+	int color = LED_OFF;
+	int on_sec = led_values[pwr_state][chipset_state].on_sec;
+
+	if (battery_ticks < on_sec)
+		color = led_values[pwr_state][chipset_state].color_1;
+	else
+		color = led_values[pwr_state][chipset_state].color_2;
+
+	return color;
+}
+
+static int board_led_set_battery(void)
 {
 	static int battery_ticks;
 	int color = LED_OFF;
@@ -70,59 +178,47 @@ static void board_led_set_battery(void)
 
 	battery_ticks++;
 
-	switch (charge_get_state()) {
-	case PWR_STATE_CHARGE:
-		/* Always indicate amber on when charging. */
-		color = LED_AMBER;
-		break;
-	case PWR_STATE_DISCHARGE:
-		if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
-			/* Discharging in S3: Amber 1 sec, off 3 sec */
-			period = (1 + 3) * LED_ONE_SEC;
-			battery_ticks = battery_ticks % period;
-			if (battery_ticks < 1 * LED_ONE_SEC)
-				color = LED_AMBER;
-			else
-				color = LED_OFF;
-		} else if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
-			/* Discharging in S5: off */
-			color = LED_OFF;
-		} else if (chipset_in_state(CHIPSET_STATE_ON)) {
-			/* Discharging in S0: Blue on */
-			color = LED_BLUE;
-		}
-		break;
-	case PWR_STATE_ERROR:
-		/* Battery error: Amber 1 sec, off 1 sec */
-		period = (1 + 1) * LED_ONE_SEC;
-		battery_ticks = battery_ticks % period;
-		if (battery_ticks < 1 * LED_ONE_SEC)
-			color = LED_AMBER;
-		else
-			color = LED_OFF;
-		break;
-	case PWR_STATE_CHARGE_NEAR_FULL:
-		/* Full Charged: Blue on */
-		color = LED_BLUE;
-		break;
-	case PWR_STATE_IDLE: /* External power connected in IDLE */
-		if (chflags & CHARGE_FLAG_FORCE_IDLE) {
-			/* Factory mode: Blue 2 sec, Amber 2 sec */
-			period = (2 + 2) * LED_ONE_SEC;
-			battery_ticks = battery_ticks % period;
-			if (battery_ticks < 2 * LED_ONE_SEC)
-				color = LED_BLUE;
-			else
-				color = LED_AMBER;
-		} else
-			color = LED_BLUE;
-		break;
-	default:
-		/* Other states don't alter LED behavior */
-		break;
-	}
+	enum charge_state pwr_state = charge_get_state();
 
-	led_set_color(color);
+	/* Only PWR_STATE_IDLE depends on chflags */
+	if (led_policy[pwr_state].chflags_enabled) {
+		if (chflags & CHARGE_FLAG_FORCE_IDLE) {
+			period = led_values[pwr_state][ANY].period;
+			color = get_led_color((battery_ticks % period),
+							pwr_state, ANY);
+		} else {
+			color = led_values[pwr_state][ANY].color_3;
+		}
+	/* LED Color depends on chipset_state */
+	} else if (led_policy[pwr_state].chipset_state_enabled) {
+		enum chipset chipset_state;
+
+		if (chipset_in_state(CHIPSET_STATE_ON))
+			/* S0 */
+			chipset_state = S0;
+		else if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND))
+			/* S3 */
+			chipset_state = S3;
+		else if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+			/* S5 */
+			chipset_state = S5;
+
+		period = led_values[pwr_state][chipset_state].period;
+
+		if (period != 0)
+			color = get_led_color((battery_ticks % period),
+						pwr_state, chipset_state);
+		else
+			color = led_values[pwr_state][chipset_state].color_1;
+	/* LED is blinking in this state */
+	} else if (led_values[pwr_state][ANY].period != 0) {
+		period = led_values[pwr_state][ANY].period;
+		color = get_led_color((battery_ticks % period),
+						pwr_state, ANY);
+	} else
+		color = led_values[pwr_state][ANY].color_1;
+
+	return color;
 }
 
 /* Called by hook task every TICK */
@@ -147,7 +243,7 @@ void led_control(enum ec_led_id led_id, enum ec_led_state state)
 		return;
 	}
 
-	color = state ? LED_BLUE : LED_OFF;
+	color = state ? DT_STRING_UPPER_TOKEN(LED_CONTROL, led_color) : LED_OFF;
 
 	led_auto_control(EC_LED_ID_BATTERY_LED, 0);
 
