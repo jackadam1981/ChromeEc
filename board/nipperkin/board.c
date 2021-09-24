962f5dd9c769ed905919bd2cad44e12df591077c
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2021-09-24 17:30:02 +0800

    nipperkin: support vivaldi keyboard
    BUG=b:197494540
    BRANCH=none
    TEST=verify keyboard top row key work as intended.
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: I7a0b1573b323f09f8e2ec8b0188dee5cb498af81

--- board/nipperkin/board.c
+++ board/nipperkin/board.c
@@ -321,3 +321,96 @@ static int board_get_temp(int idx, int *temp_k)
 		return EC_ERROR_NOT_POWERED;
 	return get_temp_3v3_30k9_47k_4050b(idx, temp_k);
 }
+
+static const struct ec_response_keybd_config keybd1 = {
+	.num_top_row_keys = 13,
+	.action_keys = {
+		TK_BACK,				/* T1 */
+		TK_REFRESH,				/* T2 */
+		TK_FULLSCREEN,			/* T3 */
+		TK_OVERVIEW,			/* T4 */
+		TK_SNAPSHOT,			/* T5 */
+		TK_BRIGHTNESS_DOWN,		/* T6 */
+		TK_BRIGHTNESS_UP,		/* T7 */
+		TK_KBD_BKLIGHT_TOGGLE,	/* T8 */
+		TK_PLAY_PAUSE,			/* T9 */
+		TK_MICMUTE,				/* T10 */
+		TK_VOL_MUTE,			/* T11 */
+		TK_VOL_DOWN,			/* T12 */
+		TK_VOL_UP,				/* T13 */
+	},
+	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
+};
+
+static const struct ec_response_keybd_config keybd2 = {
+	.num_top_row_keys = 13,
+	.action_keys = {
+		TK_BACK,				/* T1 */
+		TK_REFRESH,				/* T2 */
+		TK_FULLSCREEN,			/* T3 */
+		TK_OVERVIEW,			/* T4 */
+		TK_SNAPSHOT,			/* T5 */
+		TK_BRIGHTNESS_DOWN,		/* T6 */
+		TK_BRIGHTNESS_UP,		/* T7 */
+		TK_PRIVACY_SCRN_TOGGLE,	/* T8 */
+		TK_KBD_BKLIGHT_TOGGLE,	/* T9 */
+		TK_MICMUTE,				/* T10 */
+		TK_VOL_MUTE,			/* T11 */
+		TK_VOL_DOWN,			/* T12 */
+		TK_VOL_UP,				/* T13 */
+	},
+	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
+};
+
+static const struct ec_response_keybd_config keybd3 = {
+	.num_top_row_keys = 13,
+	.action_keys = {
+		TK_BACK,				/* T1 */
+		TK_REFRESH,				/* T2 */
+		TK_FULLSCREEN,			/* T3 */
+		TK_OVERVIEW,			/* T4 */
+		TK_SNAPSHOT,			/* T5 */
+		TK_BRIGHTNESS_DOWN,		/* T6 */
+		TK_BRIGHTNESS_UP,		/* T7 */
+		TK_PRIVACY_SCRN_TOGGLE,	/* T8 */
+		TK_PLAY_PAUSE,			/* T9 */
+		TK_MICMUTE,				/* T10 */
+		TK_VOL_MUTE,			/* T11 */
+		TK_VOL_DOWN,			/* T12 */
+		TK_VOL_UP,				/* T13 */
+	},
+	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
+};
+
+static const struct ec_response_keybd_config keybd4 = {
+	.num_top_row_keys = 13,
+	.action_keys = {
+		TK_BACK,				/* T1 */
+		TK_REFRESH,				/* T2 */
+		TK_FULLSCREEN,			/* T3 */
+		TK_OVERVIEW,			/* T4 */
+		TK_SNAPSHOT,			/* T5 */
+		TK_BRIGHTNESS_DOWN,		/* T6 */
+		TK_BRIGHTNESS_UP,		/* T7 */
+		TK_PREV_TRACK,			/* T8 */
+		TK_PLAY_PAUSE,			/* T9 */
+		TK_MICMUTE,				/* T10 */
+		TK_VOL_MUTE,			/* T11 */
+		TK_VOL_DOWN,			/* T12 */
+		TK_VOL_UP,				/* T13 */
+	},
+	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
+};
+
+__override const struct ec_response_keybd_config *
+board_vivaldi_keybd_config(void)
+{
+	if (board_has_privacy_panel() && board_has_kblight())
+		return &keybd2;
+	else if (!board_has_privacy_panel() && board_has_kblight())
+		return &keybd1;
+	else if (board_has_privacy_panel() && !board_has_kblight())
+		return &keybd3;
+	else
+		return &keybd4;
+}
