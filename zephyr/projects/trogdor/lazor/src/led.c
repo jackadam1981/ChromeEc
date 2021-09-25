bd203647ca19c36c54343a41fc749d797ae2675a
Parth Malkan <parthmalkan@google.com>
2021-09-24 18:16:42 -0700

    Zephyr: Add a generic power led SM
    Add a generic power LED SM and power led node in led.dts file. This
    makes this led.c file usable for all the boards that either support
    gpio battery_led or both battery_led and power_led
    
    BRANCH=none
    BUG=b:201007290
    TEST=Built zephyr code for lazor project successfully
    
    Signed-off-by: Parth Malkan <parthmalkan@google.com>
    Change-Id: Ie930c9411f6466985475f84f2a662395a1c626de

--- zephyr/projects/trogdor/lazor/src/led.c
+++ zephyr/projects/trogdor/lazor/src/led.c
@@ -21,47 +21,86 @@
 
 #define LED_ONE_SEC (1000 / HOOK_TICK_INTERVAL_MS)
 
-#define BAT_LED_ON 1
-#define BAT_LED_OFF 0
+#define LED_ON_LVL  1
+#define LED_OFF_LVL 0
 
-#define BATT_LED_COLORS  DT_PATH(gpio_led, battery_led_colors)
-#define CHARGING         DT_NODELABEL(charging)
-#define DISCHARGE_S0     DT_NODELABEL(discharge_s0)
-#define DISCHARGE_S3     DT_NODELABEL(discharge_s3)
-#define DISCHARGE_S5     DT_NODELABEL(discharge_s5)
-#define ERROR            DT_NODELABEL(error)
-#define NEAR_FULL        DT_NODELABEL(near_full)
-#define IDLE             DT_NODELABEL(idle)
-#define LED_CONTROL      DT_NODELABEL(led_control)
+#define BRIGHTNESS_RANGE DT_NODELABEL(brightness_range)
+
+/* Battery LED SM Defines */
+#define CHARGING        DT_NODELABEL(charging)
+#define DISCHARGE_S0    DT_NODELABEL(discharge_s0)
+#define DISCHARGE_S3    DT_NODELABEL(discharge_s3)
+#define DISCHARGE_S5    DT_NODELABEL(discharge_s5)
+#define ERROR           DT_NODELABEL(error)
+#define IDLE            DT_NODELABEL(idle)
+#define LED_CONTROL     DT_NODELABEL(led_control)
+#define LED_ID_BATT     DT_PATH(gpio_led, battery_led_colors)
+#define NEAR_FULL       DT_NODELABEL(near_full)
+
+/* Power LED SM Defines */
+#define LED_ID_POWER    DT_PATH(gpio_led, power_led_colors)
+#define POWER_LED_NODE  DT_NODE_EXISTS(LED_ID_POWER)
+#define POWER_STATE_S0  DT_NODELABEL(power_state_s0)
+#define POWER_STATE_S3  DT_NODELABEL(power_state_s3)
+#define POWER_STATE_S5  DT_NODELABEL(power_state_s5)
+
+#define LED_ENUM(id) DT_ENUM_UPPER_TOKEN(id, enum_name)
+#define LED_ENUM_WITH_COMMA(id) \
+	COND_CODE_1(DT_NODE_HAS_PROP(id, enum_name), (LED_ENUM(id), ), ())
 
 const enum ec_led_id supported_led_ids[] = {
-	EC_LED_ID_BATTERY_LED,
+#if DT_NODE_EXISTS(DT_PATH(gpio_led, led_id))
+	DT_FOREACH_CHILD(DT_PATH(gpio_led, led_id), LED_ENUM_WITH_COMMA)
+#endif
 };
 
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 
 enum led_color {
 	LED_OFF = 0,
-	LED_AMBER,
+	LED_RED,
+	LED_GREEN,
 	LED_BLUE,
+	LED_YELLOW,
+	LED_WHITE,
+	LED_AMBER,
 	LED_COLOR_COUNT  /* Number of colors, not a color itself */
 };
 
 static void led_set_color(enum led_color color)
 {
 	gpio_set_level(GPIO_EC_CHG_LED_Y_C1,
-		(color == LED_AMBER) ? BAT_LED_ON : BAT_LED_OFF);
+		(color == LED_AMBER) ? LED_ON_LVL : LED_OFF_LVL);
 	gpio_set_level(GPIO_EC_CHG_LED_B_C1,
-		(color == LED_BLUE) ? BAT_LED_ON : BAT_LED_OFF);
+		(color == LED_BLUE) ? LED_ON_LVL : LED_OFF_LVL);
 }
 
-static const uint8_t dt_brigthness_range[EC_LED_COLOR_COUNT] =
-	DT_PROP(DT_PATH(gpio_led, brightness_range), brightness_range_battery);
+#if POWER_LED_NODE
+static void led_set_color_power(enum led_color color)
+{
+	gpio_set_level(GPIO_EC_PWR_LED_W,
+		(color == LED_WHITE) ? LED_ON_LVL : LED_OFF_LVL);
+}
+#endif
 
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
-	memcpy(brightness_range, dt_brigthness_range,
-		sizeof(dt_brigthness_range));
+	if (led_id == EC_LED_ID_BATTERY_LED) {
+		const uint8_t dt_brigthness_range[EC_LED_COLOR_COUNT] =
+			DT_PROP(BRIGHTNESS_RANGE, brightness_range_battery);
+
+		memcpy(brightness_range, dt_brigthness_range,
+			sizeof(dt_brigthness_range));
+	}
+#if POWER_LED_NODE
+	else if (led_id == EC_LED_ID_POWER_LED) {
+		const uint8_t dt_brigthness_range[EC_LED_COLOR_COUNT] =
+			DT_PROP(BRIGHTNESS_RANGE, brightness_range_power);
+
+		memcpy(brightness_range, dt_brigthness_range,
+			sizeof(dt_brigthness_range));
+	}
+#endif
 }
 
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
@@ -91,26 +130,27 @@ static void board_led_set_battery(void)
 		break;
 	case PWR_STATE_DISCHARGE:
 		/* TODO: Need additional work to make it work for Coachz */
-		if (DT_PROP(BATT_LED_COLORS, chipset_state)) {
-			if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
-				/* Discharging in S3 */
-				period = DT_PROP(DISCHARGE_S3, period);
-				battery_ticks = battery_ticks % period;
-				if (battery_ticks < 1 * LED_ONE_SEC)
-					color = DT_ENUM_TOKEN(
-						  DISCHARGE_S3, led_color_1);
-				else
-					color = DT_ENUM_TOKEN(
-						  DISCHARGE_S3, led_color_2);
-			} else if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
-				/* Discharging in S5 */
-				color = DT_ENUM_TOKEN(DISCHARGE_S5, led_color);
-			} else if (chipset_in_state(CHIPSET_STATE_ON)) {
-				/* Discharging in S0 */
-				color = DT_ENUM_TOKEN(DISCHARGE_S0, led_color);
-			}
-		} else
-			color = LED_OFF;
+#if DT_PROP(LED_ID_BATT, chipset_state)
+		if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
+			/* Discharging in S3 */
+			period = DT_PROP(DISCHARGE_S3, period);
+			battery_ticks = battery_ticks % period;
+			if (battery_ticks < 1 * LED_ONE_SEC)
+				color = DT_ENUM_TOKEN(
+						DISCHARGE_S3, led_color_1);
+			else
+				color = DT_ENUM_TOKEN(
+						DISCHARGE_S3, led_color_2);
+		} else if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
+			/* Discharging in S5 */
+			color = DT_ENUM_TOKEN(DISCHARGE_S5, led_color);
+		} else if (chipset_in_state(CHIPSET_STATE_ON)) {
+			/* Discharging in S0 */
+			color = DT_ENUM_TOKEN(DISCHARGE_S0, led_color);
+		}
+#else
+		color = LED_OFF;
+#endif
 		break;
 	case PWR_STATE_ERROR:
 		/* Battery error */
@@ -146,11 +186,44 @@ static void board_led_set_battery(void)
 	led_set_color(color);
 }
 
+#if POWER_LED_NODE
+static void board_led_set_power(void)
+{
+	static int power_ticks;
+	int color = LED_OFF;
+	int period = 0;
+
+	power_ticks++;
+
+	if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
+		/* S3 */
+		period = DT_PROP(POWER_STATE_S3, period);
+		power_ticks = power_ticks % period;
+		if (power_ticks < 1)
+			color = DT_ENUM_TOKEN(POWER_STATE_S3, led_color_1);
+		else
+			color = DT_ENUM_TOKEN(POWER_STATE_S3, led_color_2);
+	} else if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
+		/* S5: LED off */
+		color = DT_ENUM_TOKEN(POWER_STATE_S5, led_color);
+	} else if (chipset_in_state(CHIPSET_STATE_ON)) {
+		/* S0: LED on */
+		color = DT_ENUM_TOKEN(POWER_STATE_S0, led_color);
+	}
+
+	led_set_color_power(color);
+}
+#endif
+
 /* Called by hook task every TICK */
 static void led_tick(void)
 {
 	if (led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED))
 		board_led_set_battery();
+#if POWER_LED_NODE
+	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
+		board_led_set_power();
+#endif
 }
 DECLARE_HOOK(HOOK_TICK, led_tick, HOOK_PRIO_DEFAULT);
 
