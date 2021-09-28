5c91606a42c7d71f22a62f14f1edc7f3635195bf
Eric Yilun Lin <yllin@chromium.org>
2021-09-28 11:48:17 +0800

    asurada: enable battery sleep mode when in ULP
    It should be enabled in ULP mode to lower the power
    consumption.
    
    BUG=b/201389829
    TEST=ensure the current is dropped after entering sleep mode.
    BRANCH=asurada
    
    Change-Id: If75be19da11ebe35c36020ccde569fa9a5c15a00
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- board/spherion/battery.c
+++ board/spherion/battery.c
@@ -21,6 +21,11 @@ const struct board_batt_params board_battery_info[] = {
 				.reg_addr = 0x0,
 				.reg_data = { 0x10, 0x10 },
 			},
+			.sleep_mode = {
+				.sleep_supported = true,
+				.reg_addr = 0x00,
+				.reg_data = 0x0011,
+			},
 			.fet = {
 				.reg_addr = 0x99,
 				.reg_mask = 0x0c,
@@ -48,6 +53,9 @@ const struct board_batt_params board_battery_info[] = {
 			.ship_mode = {
 				.reg_addr = 0x3A,
 				.reg_data = { 0xC574, 0xC574 },
+			},
+			.sleep_mode = {
+				/* sleep mode is enabled automatically. */
 			},
 			.fet = {
 				.reg_addr = 0x0,
