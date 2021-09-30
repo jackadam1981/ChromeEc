aafcbede5fcc727c4b21312f05a4e91db20e518d
Tom Hughes <tomhughes@chromium.org>
2021-09-30 23:19:40 +0000

    core/cortex-m[0]: Enable "-Oz" when using clang
    "-Oz" is like "-Os" (and thus "-O2"), but reduces code size further.
    
    dartmonkey, clang, -Os:
    RO: 733500 flash remaining
    RW: 979488 flash remaining
    
    dartmonkey, clang, -Oz:
    RO: 737980 flash remaining
    RW: 984776 flash remaining
    
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Ia787845ffdeff26134162007a621b902bbc0d051

--- core/cortex-m0/build.mk
+++ core/cortex-m0/build.mk
@@ -12,11 +12,13 @@ $(call set-option,CROSS_COMPILE,\
 	/opt/coreboot-sdk/bin/arm-eabi-)
 
 # CPU specific compilation flags
-CFLAGS_CPU+=-mthumb -Os
+CFLAGS_CPU+=-mthumb
 ifeq ($(cc-name),clang)
+CFLAGS_CPU+=-Oz		# Like -Os (and thus -O2), but reduces code size further.
 # Link compiler-rt when using clang, so clang finds the builtins it provides.
 LDFLAGS_EXTRA+=-lclang_rt.builtins-arm
 else
+CFLAGS_CPU+=-Os
 CFLAGS_CPU+=-mno-sched-prolog
 endif
 CFLAGS_CPU+=-mno-unaligned-access
