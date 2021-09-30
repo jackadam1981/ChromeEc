4a39f9ffa15388194d2c20c9fe749b6ef980b876
Ting Shen <phoenixshen@google.com>
2021-09-30 15:49:09 +0800

    cherry: Enable VBUS source capability for USB-C after AP XHCI done
    WIP
    
    BUG=b:200772411
    TEST=WIP
    BRANCH=main
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I674391622698b6e241b0a97063b3abbc8550e365

--- baseboard/cherry/baseboard.c
+++ baseboard/cherry/baseboard.c
@@ -48,6 +48,7 @@
 #include "usb_mux.h"
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
+#include "usb_tc_sm.h"
 
 static void bc12_interrupt(enum gpio_signal signal);
 static void ppc_interrupt(enum gpio_signal signal);
@@ -251,6 +252,17 @@ __maybe_unused void usb_a0_interrupt(enum gpio_signal signal)
 
 	for (int i = 0; i < USB_PORT_COUNT; i++)
 		usb_charge_set_mode(i, mode, USB_ALLOW_SUSPEND_CHARGE);
+
+	/*
+	 * Trigger hard reset to cycle Vbus on Type-C ports, recommended by
+	 * USB 3.2 spec 10.3.1.1.
+	 */
+	if (gpio_get_level(signal)) {
+		for (int i = 0; i < CONFIG_USB_PD_PORT_MAX_COUNT; i++) {
+			if (tc_is_attached_src(i))
+				pd_dpm_request(i, DPM_REQUEST_HARD_RESET_SEND);
+		}
+	}
 }
 
 /* USB Mux */
