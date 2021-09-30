cd8e3abd911352919e188038910bba5709ca7b0f
Rajesh Kumar <rajesh3.kumar@intel.com>
2021-09-30 15:47:10 -0700

    Enabling LED on brya using PWM design on zephyr shim BUG=b:200974848 BRANCH=main TEST=buildall -j
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I6144b25098c1c5330c958b2578a3fe79f5c09adb

--- board/brya/led.c
+++ board/brya/led.c
@@ -25,6 +25,7 @@ const enum ec_led_id supported_led_ids[] = {
 
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 
+#ifndef CONFIG_ZEPHYR
 /*
  * We only have a white and an amber LED, so setting any other color results in
  * both LEDs being off.
@@ -64,6 +65,7 @@ void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 	brightness_range[EC_LED_COLOR_AMBER] = 100;
 	brightness_range[EC_LED_COLOR_WHITE] = 100;
 }
+#endif
 
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 {
