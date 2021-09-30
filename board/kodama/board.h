abdd5b99eed332250b399394b89cebf62c9afb99
Vijay Hiremath <vijay.p.hiremath@intel.com>
2021-09-30 10:03:53 -0700

    kodama: Disable 'sysinfo' console command to save space
    Just preserving the build at ToT. Do not cherry pick.
    
    BRANCH=none
    BUG=none
    TEST=make BOARD=kodama -j
    before:
     236 bytes in flash and 11212 bytes in RAM still available on kodama RO
     13696 bytes in flash and 8688 bytes in RAM still available on kodama RW
    After:
     876 bytes in flash and 11212 bytes in RAM still available on kodama RO
     13696 bytes in flash and 8688 bytes in RAM still available on kodama RW
    
    Change-Id: I42ffbf532d2a42b8d325692f20ca5f0c061e8969
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- board/kodama/board.h
+++ board/kodama/board.h
@@ -15,6 +15,10 @@
 #define VARIANT_KUKUI_TABLET_PWRBTN
 #undef CONFIG_CMD_MFALLOW
 
+/* save space at RO image */
+#ifdef SECTION_IS_RO
+#undef CONFIG_CMD_SYSINFO
+#endif
 
 #ifndef SECTION_IS_RW
 #define VARIANT_KUKUI_NO_SENSORS
