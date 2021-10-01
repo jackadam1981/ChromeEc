355688c8e19df4b65a5c6eec680060d0590359e1
Vijay Hiremath <vijay.p.hiremath@intel.com>
2021-10-01 13:45:27 -0700

    [WIp] ioex: cleanup: Use IOEX_FLAGS_INITIALIZED to get the IOEX status
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I98ff9643223c02388a2fa679b4631e3389fd466e
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- board/gimble/usbc_config.c
+++ board/gimble/usbc_config.c
@@ -132,7 +132,7 @@ struct ioexpander_config_t ioex_config[] = {
 		.i2c_host_port = I2C_PORT_USB_C0_TCPC,
 		.i2c_addr_flags = NCT38XX_I2C_ADDR1_1_FLAGS,
 		.drv = &nct38xx_ioexpander_drv,
-		.flags = IOEX_FLAGS_DISABLED,
+		.flags = IOEX_FLAGS_DEFAULT_INIT_DISABLED,
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(ioex_config) == CONFIG_IO_EXPANDER_PORT_COUNT);
@@ -195,7 +195,6 @@ static void board_tcpc_init(void)
 		board_reset_pd_mcu();
 
 		for (i = 0; i < CONFIG_IO_EXPANDER_PORT_COUNT; ++i) {
-			ioex_config[i].flags &= ~IOEX_FLAGS_DISABLED;
 			ioex_init(i);
 		}
 	}
