FrameworkComputer/EmbeddedController

commit 26900d2e8c76f9b04d560def5aa459fd42742341

nightfury: Enable base light sensor
nightfury uses opt3001 for base ALS sensor.
So we need to update board files for nightfury.

BUG=b:149226871
BRANCH=firmware-hatch-12672.B
TEST=emerge-hatch chromeos-ec
     flash ec and check ectool motionsense

Signed-off-by: Jongpil Jung <jongpil19.jung@samsung.corp-partner.google.com>
Change-Id: I86ee37140d1a74d85baec53e0dd8792bd6b77362
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2100563
Reviewed-by: Bob Moragues <moragues@chromium.org>
Reviewed-by: Scott Collyer <scollyer@chromium.org>
Commit-Queue: Bob Moragues <moragues@chromium.org>

board/nightfury/board.h
@@ -46,7 +46,8 @@
 #define CONFIG_ALS
 #define ALS_COUNT 1
 #define I2C_PORT_ALS      I2C_PORT_SENSOR
-#define CONFIG_ALS_BH1730
+#define CONFIG_ALS_OPT3001
+#define OPT3001_I2C_ADDR_FLAGS OPT3001_I2C_ADDR1_FLAGS
 
 /* Sensors without hardware FIFO are in forced mode */
 #define CONFIG_ACCEL_FORCE_MODE_MASK \

board/nightfury/board.c
@@ -12,7 +12,7 @@
 #include "cros_board_info.h"
 #include "driver/accel_bma2x2.h"
 #include "driver/accelgyro_bmi160.h"
-#include "driver/als_bh1730.h"
+#include "driver/als_opt3001.h"
 #include "driver/als_tcs3400.h"
 #include "driver/ppc/sn5s330.h"
 #include "driver/bc12/pi3usb9201.h"
@@ -164,11 +164,15 @@ static struct mutex g_lid_mutex;
 /* Base accel private data */
 static struct bmi160_drv_data_t g_bmi160_data;
 
+/* Base light sensor private data */
+static struct opt3001_drv_data_t g_opt3001_data = {
+	.scale = 1,
+	.uscale = 0,
+	.offset = 0,
+};
+
 /* BMA255 private data */
 static struct accelgyro_saved_data_t g_bma255_data;
-
-/* BH1730 private data */
-struct bh1730_drv_data_t g_bh1730_data;
 
 /* Matrix to rotate accelrator into standard reference frame */
 static const mat33_fp_t base_standard_ref = {
@@ -262,20 +266,20 @@ struct motion_sensor_t motion_sensors[] = {
 	[BASE_ALS] = {
 		.name = "Light",
 		.active_mask = SENSOR_ACTIVE_S0,
-		.chip = MOTIONSENSE_CHIP_BH1730,
+		.chip = MOTIONSENSE_CHIP_OPT3001,
 		.type = MOTIONSENSE_TYPE_LIGHT,
 		.location = MOTIONSENSE_LOC_BASE,
-		.drv = &bh1730_drv,
-		.drv_data = &g_bh1730_data,
+		.drv = &opt3001_drv,
+		.drv_data = &g_opt3001_data,
 		.port = I2C_PORT_ACCEL,
-		.i2c_spi_addr_flags = BH1730_I2C_ADDR_FLAGS,
+		.i2c_spi_addr_flags = OPT3001_I2C_ADDR_FLAGS,
 		.rot_standard_ref = NULL,
-		.default_range = 65535,
-		.min_frequency = 10,
-		.max_frequency = 10,
+		.default_range = 0x10000, /* scale = 1; uscale = 0 */
+		.min_frequency = OPT3001_LIGHT_MIN_FREQ,
+		.max_frequency = OPT3001_LIGHT_MAX_FREQ,
 		.config = {
 			[SENSOR_CONFIG_EC_S0] = {
-				.odr = 100000,
+				.odr = 1000,
 				.ec_rate = 0,
 			},
 		},

jackadam1981/ChromeEc

commit f3855b07ef14d5cac29c22288ec2bcbcc8d8512c

board: nightfury: Remove BH1730 defines
Light sensor BH1730 is not used on nightfury. We use OPT3001 instead.
Remove unnecessary defines.

Fixes: 26900d2e8c ("nightfury: Enable base light sensor")

BUG=none
TEST=compile
BRANCH=hatch

Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Change-Id: I6108db8424ca2608fe5d881600cb7463f38ca77d

board/nightfury/board.h
@@ -43,7 +43,7 @@
 #define CONFIG_LID_ANGLE_SENSOR_BASE BASE_ACCEL
 #define CONFIG_LID_ANGLE_SENSOR_LID LID_ACCEL
 #define CONFIG_LID_ANGLE_UPDATE
-/* BH1730 and TCS3400 ALS */
+/* OPT3001 and TCS3400 ALS */
 #define CONFIG_ALS
 #define ALS_COUNT 1
 #define I2C_PORT_ALS      I2C_PORT_SENSOR
@@ -52,34 +52,6 @@
 
 /* Sensors without hardware FIFO are in forced mode */
 #define CONFIG_ACCEL_FORCE_MODE_MASK (BIT(BASE_ALS))
-
-/* Parameter to calculate LUX on Nightfury */
-#define CONFIG_ALS_BH1730_LUXTH_PARAMS
-/*
- * Calculation formula depends on characteristic of optical window.
- * In case of Nightfury, we can select two different formula
- * as characteristic of optical window.
- * BH1730_LUXTH1_1K is charateristic of optical window.
- * 1. d1_1K/d0_1K * 1000 <  BH1730_LUXTH1_1K
- * 2. d1_1K/d0_1K * 1000 >= BH1730_LUXTH1_1K
- * d0 and d1 are unsigned 16 bit. So, d1/d0 max is 65535
- * To meet 2nd condition, make BH1730_LUXTH2_1K to (max+1)*1000
- * Nightfury will not use both BH1730_LUXTH3_1K condition
- * and BH1730_LUXTH4_1K condition.
- */
-#define BH1730_LUXTH1_1K                270
-#define BH1730_LUXTH1_D0_1K             19200
-#define BH1730_LUXTH1_D1_1K             30528
-#define BH1730_LUXTH2_1K                655360000
-#define BH1730_LUXTH2_D0_1K             11008
-#define BH1730_LUXTH2_D1_1K             10752
-#define BH1730_LUXTH3_1K                1030
-#define BH1730_LUXTH3_D0_1K             11008
-#define BH1730_LUXTH3_D1_1K             10752
-#define BH1730_LUXTH4_1K                3670
-#define BH1730_LUXTH4_D0_1K             11008
-#define BH1730_LUXTH4_D1_1K             10752
-
 /* USB Type C and USB PD defines */
 #define CONFIG_USB_MUX_RUNTIME_CONFIG
 #define CONFIG_USB_PD_COMM_LOCKED