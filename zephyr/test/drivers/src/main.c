dd18cb363dc2836a430b65d2dca7c69218c62ce8
Abe Levkoy <alevkoy@chromium.org>
2021-10-01 13:10:44 -0600

    DNS: Only PPC tests
    For development
    
    BUG=none
    TEST=make buildall
    BRANCH=none
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: I7e596ee8d87a699bb9b40a83660f90e839a906ef

--- zephyr/test/drivers/src/main.c
+++ zephyr/test/drivers/src/main.c
@@ -34,6 +34,7 @@ void test_main(void)
 	ec_app_main();
 
 	/* Test suites to run after ec_app_main.*/
+#if 0
 	test_suite_battery();
 	test_suite_cbi();
 	test_suite_smart_battery();
@@ -41,7 +42,9 @@ void test_main(void)
 	test_suite_temp_sensor();
 	test_suite_bma2x2();
 	test_suite_bc12();
+#endif
 	test_suite_ppc();
+#if 0
 	test_suite_bmi260();
 	test_suite_bmi160();
 	test_suite_tcs3400();
@@ -52,5 +55,6 @@ void test_main(void)
 	test_suite_stm_mems_common();
 	test_suite_isl923x();
 	test_suite_usb_mux();
+#endif
 	test_suite_ppc_syv682c();
 }
