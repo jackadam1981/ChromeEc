1ca0f57eafecad50d4b36ac69aba28e633259136
Tom Hughes <tomhughes@chromium.org>
2021-10-04 21:37:15 +0000

    tree: Move DECLARE_IRQ before IRQ definition
    DECLARE_IRQ creates a declaration of the function with the "used"
    attribute. The declaration with this attribute must come before the
    definition of the function or the compiler can ignore it. If it ignores
    the "used" attribute, the linker can optimize it out when doing LTO:
    
    ld.lld: error: undefined symbol: adc_interrupt
    >>> referenced by adc.c:357 (platform/ec/chip/npcx/adc.c:357)
    >>>               lto.tmp:(irq_10_handler)
    
    ld.lld: error: undefined symbol: __gpio_rtc_interrupt
    >>> referenced by gpio-npcx7.c:171 (platform/ec/chip/npcx/gpio-npcx7.c:171)
    >>>               lto.tmp:(irq_7_handler)
    
    BRANCH=none
    BUG=b:172020503
    TEST=make CC=arm-none-eabi-clang BOARD=aleena
    TEST=make buildall -j
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Ie184158c69333309232299eb1ed54a18d25c4352

--- chip/mchp/adc.c
+++ chip/mchp/adc.c
@@ -139,6 +139,7 @@ static void adc_init(void)
 }
 DECLARE_HOOK(HOOK_INIT, adc_init, HOOK_PRIO_INIT_ADC);
 
+DECLARE_IRQ(MCHP_IRQ_ADC_SNGL, adc_interrupt, 2);
 static void adc_interrupt(void)
 {
 	MCHP_INT_DISABLE(MCHP_ADC_GIRQ) = MCHP_ADC_GIRQ_SINGLE_BIT;
@@ -154,4 +155,3 @@ static void adc_interrupt(void)
 	if (task_waiting != TASK_ID_INVALID)
 		task_wake(task_waiting);
 }
-DECLARE_IRQ(MCHP_IRQ_ADC_SNGL, adc_interrupt, 2);
