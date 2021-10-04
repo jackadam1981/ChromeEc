25e4b1de9f711d6c4ae27e63225db6c041a12a5d
Aaron Massey <aaronmassey@google.com>
2021-10-04 13:35:16 -0600

    zephyr: drivers ln9310 software_enable race condition fix test
    Tests fix for race condition in ln9310_software_enable
    mentioned by change I976d2f35b1e392bc584cbd7e13ab34421846f679
    BRANCH=none
    BUG=b:184856083
    TEST=zmake configure --test zephyr/test/drivers
    
    Signed-off-by: Aaron Massey <aaronmassey@chromium.org>
    
    Cq-Depend: chromium:3200519
    Change-Id: I5c5ddcc46342203298655342cc8b1365263d4f3b

--- zephyr/test/drivers/src/ln9310.c
+++ zephyr/test/drivers/src/ln9310.c
@@ -117,6 +117,58 @@ static void test_ln9310_3s_software_enable(void)
 	zassert_false(ln9310_power_good(), NULL);
 }
 
+static void test_ln9310_2s_software_enable_fixed_chip(void)
+{
+	const struct emul *emulator =
+		emul_get_binding(DT_LABEL(DT_NODELABEL(ln9310)));
+
+	zassert_not_null(emulator, NULL);
+
+	ln9310_emul_set_context(emulator);
+	ln9310_emul_reset(emulator);
+	ln9310_emul_set_battery_cell_type(emulator, BATTERY_CELL_TYPE_2S);
+	ln9310_emul_set_version(emulator, LN9310_BC_STS_C_CHIP_REV_FIXED);
+
+	zassert_ok(ln9310_init(), NULL);
+	zassert_true(ln9310_emul_is_init(emulator), NULL);
+
+	ln9310_software_enable(1);
+	k_msleep(TEST_DELAY_MS);
+
+	zassert_true(ln9310_power_good(), NULL);
+
+	ln9310_software_enable(0);
+	k_msleep(TEST_DELAY_MS);
+
+	zassert_false(ln9310_power_good(), NULL);
+}
+
+static void test_ln9310_3s_software_enable_fixed_chip(void)
+{
+	const struct emul *emulator =
+		emul_get_binding(DT_LABEL(DT_NODELABEL(ln9310)));
+
+	zassert_not_null(emulator, NULL);
+
+	ln9310_emul_set_context(emulator);
+	ln9310_emul_reset(emulator);
+	ln9310_emul_set_battery_cell_type(emulator, BATTERY_CELL_TYPE_3S);
+	ln9310_emul_set_version(emulator, LN9310_BC_STS_C_CHIP_REV_FIXED);
+
+	zassert_ok(ln9310_init(), NULL);
+	zassert_true(ln9310_emul_is_init(emulator), NULL);
+
+	ln9310_software_enable(1);
+	k_msleep(TEST_DELAY_MS);
+
+	zassert_true(ln9310_power_good(), NULL);
+
+	ln9310_software_enable(0);
+	k_msleep(TEST_DELAY_MS);
+
+	zassert_false(ln9310_power_good(), NULL);
+}
+
 static void reset_ln9310_state(void)
 {
 	ln9310_reset_to_initial_state();
@@ -137,6 +189,13 @@ void test_suite_ln9310(void)
 					       reset_ln9310_state),
 		ztest_unit_test_setup_teardown(test_ln9310_3s_software_enable,
 					       reset_ln9310_state,
-					       reset_ln9310_state));
+					       reset_ln9310_state),
+		ztest_unit_test_setup_teardown(
+			test_ln9310_2s_software_enable_fixed_chip,
+			reset_ln9310_state, reset_ln9310_state),
+		ztest_unit_test_setup_teardown(
+			test_ln9310_3s_software_enable_fixed_chip,
+			reset_ln9310_state, reset_ln9310_state));
+
 	ztest_run_test_suite(ln9310);
 }
