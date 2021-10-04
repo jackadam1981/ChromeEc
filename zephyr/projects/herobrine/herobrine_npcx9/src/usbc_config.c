ec0841c88dc7b121b9ac0802d5b614a5d76bfe24
Sam Hurst <shurst@google.com>
2021-10-04 10:23:03 -0700

    zephyr: add support for bc1.2 in device tree
    This commit adds support for bc1.2 definition in device tree.
    It will allow to remove board specific files which implemented
    the bc1.2 functionalities. This will unify the logic between
    different board in zephyr.
    
    BRANCH=main
    BUG=b:194432779
    TEST=HOST MODE: Plug non-pd device into TypeC port and verified that the
                    device received power and the following was printed to
    		the EC console: pi3usb9201[p0]: CDP_HOST mode]
         CLIENT MODE: Plug an non-pd TypeC charger into the DUT and verified
                      that the device was charging with the "chgsup"
    		  command. The EC console displayed the following:
    		  port=0, type=1, cur=3000mA, vtg=5000mV, lsm=1
    		  And the following debug output was displayed:
    		  pi3usb9201[p0]: sts = 0x80, lim = 1500 mA, supplier = 3]
    
                      Using Suzy-Q, EC console displayed the follow:
    		  pi3usb9201[p0]: sts = 0x40, lim = 500 mA, supplier = 5]
    		  and "chgsup"
    		  port=0, type=2, cur=1500mA, vtg=5000mV, lsm=1
    
    Signed-off-by: Sam Hurst <shurst@google.org>
    Change-Id: Ia8e182997c48aa00f92c06e0fef6758473df29bb

--- zephyr/projects/herobrine/herobrine_npcx9/src/usbc_config.c
+++ zephyr/projects/herobrine/herobrine_npcx9/src/usbc_config.c
@@ -5,7 +5,6 @@
 
 /* Herobrine board-specific USB-C configuration */
 
-#include "bc12/pi3usb9201_public.h"
 #include "charger.h"
 #include "charger/isl923x_public.h"
 #include "charge_manager.h"
@@ -185,18 +184,6 @@ const struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 
 const int usb_port_enable[USB_PORT_COUNT] = {
 	GPIO_EN_USB_A_5V,
-};
-
-/* BC1.2 */
-const struct pi3usb9201_config_t pi3usb9201_bc12_chips[] = {
-	{
-		.i2c_port = I2C_PORT_POWER,
-		.i2c_addr_flags = PI3USB9201_I2C_ADDR_3_FLAGS,
-	},
-	{
-		.i2c_port = I2C_PORT_EEPROM,
-		.i2c_addr_flags = PI3USB9201_I2C_ADDR_3_FLAGS,
-	},
 };
 
 /* Initialize board USC-C things */
