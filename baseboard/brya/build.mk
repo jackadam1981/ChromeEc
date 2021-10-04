f42017f1e9c7ce84f9e908305c7b6597b14be9ae
Will Tsai <will_tsai@wistron.corp-partner.google.com>
2021-10-04 18:59:13 +0800

    gimble: control prochot
    BUG=b:198689488
    BRANCH=none
    TEST=make -j BOARD=gimble
    
    Signed-off-by: Will Tsai <will_tsai@wistron.corp-partner.google.com>
    Change-Id: I4be2dc8505baffaa8b7d0c9b6949711aca370778

--- baseboard/brya/build.mk
+++ baseboard/brya/build.mk
@@ -10,4 +10,7 @@ baseboard-y=
 baseboard-y+=baseboard.o
 baseboard-y+=battery_presence.o
 baseboard-y+=cbi.o
+ifeq ($(CONFIG_CONTROL_PROCHOT),y)
+baseboard-y+=prochot.o
+endif
 baseboard-y+=usb_pd_policy.o
