ec1e68baa04f2c5c5c1345e42a5f82afe02d714e
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2021-10-06 13:23:36 +0800

    dooly: update gpio setting prevent power leakage
    We found a power leakage in G3 cause by EC_EDID_WP_DISABLE_L,
    this patch update gpio status to prevent power leakage.
    
    BUG=b:200710471
    BRANCH=puff
    TEST=verify EC_EDID_WP_DISABLE_L status in G3 and S0.
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: Ic1a01306a6ee43360dbd5041a9ead95802efae58

--- board/dooly/board.c
+++ board/dooly/board.c
@@ -728,6 +728,9 @@ static void board_chipset_startup(void)
 		ppc_vbus_source_enable(USB_PD_PORT_TCPC_0, 1);
 	if (ppc_is_sourcing_vbus(USB_PD_PORT_TCPC_1))
 		ppc_vbus_source_enable(USB_PD_PORT_TCPC_1, 1);
+
+	/* set high to high to enable EDID rom WP */
+	gpio_set_level(GPIO_EC_EDID_WP_DISABLE_L, 1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup,
 	     HOOK_PRIO_DEFAULT);
@@ -1185,3 +1188,11 @@ __override void oz554_board_init(void)
 		break;
 	}
 }
+
+static void board_chipset_shutdown(void)
+{
+	/* set low to prevent power leakage */
+	gpio_set_level(GPIO_EC_EDID_WP_DISABLE_L, 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown,
+	     HOOK_PRIO_DEFAULT);
