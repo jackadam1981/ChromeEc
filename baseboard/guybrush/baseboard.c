57edd8ed6cc65b58c500379fb484ba41f06a2ffa
Diana Z <dzigterman@chromium.org>
2021-10-07 16:48:21 -0600

    Guybrush: Add EN_PWR_S0_R to PCORE enable
    Add in the signal for EN_PWR_S0_R to the AND we perform to produce
    EN_PWR_PCORE_S0_R.  This will ensure that if leakage has caused unusual
    power sequencing, we still turn off the PCORE enable.
    
    Additionally, warn if PG_CROUPC_S0_OD is still high after EN_PWR_S0_R is
    taken low.  This still needs tuning, as its current spot prints in
    suspend.
    
    BRANCH=None
    BUG=b:202417065
    TEST=on guybrush, confirm normal boot/suspend/shutdown.  On faulty
    board, confirm the warning shows and PCORE is disabled
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: I2046474f0fa321f0d3d273f12a2bc5ad20d1b0d3

--- baseboard/guybrush/baseboard.c
+++ baseboard/guybrush/baseboard.c
@@ -43,6 +43,8 @@
 #define CPRINTSUSB(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTFUSB(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
+#define CPRINTSCHIP(format, args...) cprints(CC_CHIPSET, format ## args)
+
 static void reset_nct38xx_port(int port);
 
 /* Wake Sources */
@@ -872,13 +874,21 @@ void board_overcurrent_event(int port, int is_overcurrented)
 	}
 }
 
-void baseboard_en_pwr_pcore_s0(enum gpio_signal signal)
+static void baseboard_set_en_pwr_pcore(void)
 {
-
-	/* EC must AND signals PG_LPDDR4X_S3_OD and PG_GROUPC_S0_OD */
+	/*
+	 * EC must AND signals PG_LPDDR4X_S3_OD, PG_GROUPC_S0_OD, and
+	 * EN_PWR_S0_R.
+	 */
 	gpio_set_level(GPIO_EN_PWR_PCORE_S0_R,
 					gpio_get_level(GPIO_PG_LPDDR4X_S3_OD) &&
-					gpio_get_level(GPIO_PG_GROUPC_S0_OD));
+					gpio_get_level(GPIO_PG_GROUPC_S0_OD) &&
+					gpio_get_level(GPIO_EN_PWR_S0_R));
+}
+
+void baseboard_en_pwr_pcore_signal(enum gpio_signal signal)
+{
+	baseboard_set_en_pwr_pcore();
 }
 
 void baseboard_en_pwr_s0(enum gpio_signal signal)
@@ -888,6 +898,14 @@ void baseboard_en_pwr_s0(enum gpio_signal signal)
 	gpio_set_level(GPIO_EN_PWR_S0_R,
 					gpio_get_level(GPIO_SLP_S3_L) &&
 					gpio_get_level(GPIO_PG_PWR_S5));
+
+	/* Change EN_PWR_PCORE_S0_R if needed */
+	baseboard_set_en_pwr_pcore();
+
+	/* Warn if we see unexpected sequencing here */
+	if (!gpio_get_level(GPIO_EN_PWR_S0_R) &&
+					gpio_get_level(GPIO_PG_GROUPC_S0_OD))
+		CPRINTSCHIP("WARN: PG_GROUPC_S0_OD set while EN_PWR_S0_R low");
 
 	/* Now chain off to the normal power signal interrupt handler. */
 	power_signal_interrupt(signal);
