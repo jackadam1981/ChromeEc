0a829447c8acf9ec4b15567666f540d4bfce766f
Tomasz Michalec <tm@semihalf.com>
2021-10-07 20:01:51 +0200

    zephyr: drivers: add TCPCI vbus discharge test
    Function tcpci_tcpc_discharge_vbus() is guarded by #ifdef
    CONFIG_USB_PD_DISCHARGE_TCPC in tcpci.h. This patch removes ifdef
    guarda and test of tcpci_tcpc_discharge_vbus() in tcpci test suite.
    
    BUG=b:184857030
    BRANCH=none
    TEST=make configure --test zephyr/test/drivers
    TEST=make runhosttests
    TEST=make buildall
    
    Signed-off-by: Tomasz Michalec <tm@semihalf.com>
    Change-Id: Ib0e2c3e36a4b56c67fb6b13e40ee11226a5dae47

--- zephyr/test/drivers/src/tcpci.c
+++ zephyr/test/drivers/src/tcpci.c
@@ -1011,6 +1011,27 @@ static void test_tcpci_set_bist_mode(void)
 	check_tcpci_reg(emul, exp_mask, TCPC_REG_ALERT_MASK);
 }
 
+/** Test TCPCI discharge vbus */
+static void test_tcpci_discharge_vbus(void)
+{
+	const struct emul *emul = emul_get_binding(DT_LABEL(EMUL_LABEL));
+	uint8_t exp_ctrl;
+
+	/* Only bit 2 should be changed */
+	exp_ctrl = 0x42;
+	tcpci_emul_set_reg(emul, TCPC_REG_POWER_CTRL, exp_ctrl);
+
+	/* Test discharge enable */
+	exp_ctrl |= 0x4;
+	tcpci_tcpc_discharge_vbus(USBC_PORT_C0, 1);
+	check_tcpci_reg(emul, exp_ctrl, TCPC_REG_POWER_CTRL);
+
+	/* Test discharge disable */
+	exp_ctrl &= ~0x4;
+	tcpci_tcpc_discharge_vbus(USBC_PORT_C0, 0);
+	check_tcpci_reg(emul, exp_ctrl, TCPC_REG_POWER_CTRL);
+}
+
 void test_suite_tcpci(void)
 {
 	ztest_test_suite(tcpci,
@@ -1034,6 +1055,7 @@ void test_suite_tcpci(void)
 			 ztest_user_unit_test(test_tcpci_drp_toggle),
 			 ztest_user_unit_test(test_tcpci_get_chip_info),
 			 ztest_user_unit_test(test_tcpci_low_power_mode),
-			 ztest_user_unit_test(test_tcpci_set_bist_mode));
+			 ztest_user_unit_test(test_tcpci_set_bist_mode),
+			 ztest_user_unit_test(test_tcpci_discharge_vbus));
 	ztest_run_test_suite(tcpci);
 }
