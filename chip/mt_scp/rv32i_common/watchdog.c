c64a29b790b9c737a2afe02e66cdbbece86f323d
Tinghan Shen <tinghan.shen@mediatek.com>
2021-10-08 13:13:44 +0800

    chip/mt_scp: dump panic information on console channel
    The panic information currently dumps by debug_print API which implemented on
    SCP UART. This implies that the panic information can only be observed
    on SCP UART. We cannot have any information about crashed SCP on
    non-development environment.
    
    This change dumps panic information on DRAM and print it at next reboot on console
    channel. The panic information is available on /var/log/cros_scp.log at
    kernel side.
    
    BRANCH=None
    BUG=b:199444513
    BUG=b:189356151
    TEST=see exception log at /var/log/cros_scp.log
    
    Change-Id: I2167d16a709fa2814f3b0a208411ae5e7f51f70b
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>

--- chip/mt_scp/rv32i_common/watchdog.c
+++ chip/mt_scp/rv32i_common/watchdog.c
@@ -6,8 +6,11 @@
 /* Watchdog driver */
 
 #include "common.h"
+#include "console.h"
 #include "hooks.h"
+#include "panic.h"
 #include "registers.h"
+#include "util.h"
 #include "watchdog.h"
 
 void watchdog_reload(void)
@@ -28,6 +31,26 @@ int watchdog_init(void)
 	SCP_CORE0_WDT_CFG = WDT_EN | timeout;
 	/* reload watchdog */
 	watchdog_reload();
+
+#ifdef CONFIG_PANIC_CONSOLE_OUTPUT
+	{
+		struct panic_data * panic = panic_get_data();
+
+		if (panic == NULL && SCP_CORE0_MON_PC_LATCH == 0)
+			return EC_SUCCESS;
+
+		ccprintf("[Previous Panic]\n");
+		if (panic) {
+			panic_data_ccprint(panic);
+		} else {
+			ccprintf("No panic data\n");
+		}
+		ccprintf("Latch PC:%x LR:%x SP:%x\n",
+			SCP_CORE0_MON_PC_LATCH,
+			SCP_CORE0_MON_LR_LATCH,
+			SCP_CORE0_MON_SP_LATCH);
+	}
+#endif
 
 	return EC_SUCCESS;
 }
