0c0c61e6bf8ce9135ef54bcd413576ec02ea5e8b
Rajesh Kumar <rajesh3.kumar@intel.com>
2021-10-11 12:22:31 -0700

    zephyr: Add support for battery/charger on zephyr
    BUG=b:202701454
    BRANCH=none
    TEST=none
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I0cbc4d74de0f86b18b1e9a881c6a0076aba8118e

--- driver/tcpm/nct38xx.h
+++ driver/tcpm/nct38xx.h
@@ -145,5 +145,4 @@ enum nct38xx_boot_type nct38xx_get_boot_type(int port);
 void nct38xx_reset_notify(int port);
 
 extern const struct ioexpander_drv nct38xx_ioexpander_drv;
-
 #endif /* defined(__CROS_EC_USB_PD_TCPM_NCT38XX_H) */
