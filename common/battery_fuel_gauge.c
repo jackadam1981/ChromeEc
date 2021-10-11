06a63e08b2c3876c4a76db15e0082417ecef293d
poornima tom <poornima.tom@intel.com>
2021-10-12 02:24:58 +0530

    config: Add CONFIG_BATTERY_NO_AUTO_DETECT option
    There are some battery types which cannot be identified
    by normal manufacturing name or device name. For devices,
    which uses this type of batteries can be configured
    with CONFIG_BATTERY_NO_AUTO_DETECT to indicate the same.
    Based on this config, the idea is to point to correct
    battery parameter info and then use the manufacturing or
    device name to identify the battery later. The motivation
    of this change is to have a single binary for multiple
    platforms that have different battery configuration.
    
    BUG=b:194163586
    BRANCH=none
    TEST=make buildall -j
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: I488ca3d72250311630724381a8cfeb52d8dad657

--- common/battery_fuel_gauge.c
+++ common/battery_fuel_gauge.c
@@ -14,11 +14,49 @@
 
 #define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
 
+/*
+ * Authenticate the battery connected.
+ *
+ * Compare the manufacturer name read from the fuel gauge to the
+ * manufacturer names defined in the board_battery_info table. If
+ * a device name has been specified in the board_battery_info table,
+ * then both the manufacturer and device name must match.
+ */
+static int authenticate_battery_type(int index, char *manuf_name)
+{
+	char device_name[32];
+	const struct fuel_gauge_info * const fuel_gauge =
+		&board_battery_info[index].fuel_gauge;
+	int len = 0;
+
+	/* manufacturer name mismatch */
+	if (strcasecmp(manuf_name, fuel_gauge->manuf_name))
+		return 0;
+
+	/* device name is specified in table */
+	if (fuel_gauge->device_name != NULL) {
+
+		/* Get the device name */
+		if (battery_device_name(device_name,
+					sizeof(device_name)))
+			return 0;
+
+		len = strlen(fuel_gauge->device_name);
+
+		/* device name mismatch */
+		if (strncasecmp(device_name, fuel_gauge->device_name,
+					len))
+			return 0;
+	}
+
+	CPRINTS("found batt:%s", fuel_gauge->manuf_name);
+	return 1;
+}
 
 /* Get type of the battery connected on the board */
 static int get_battery_type(void)
 {
-	char manuf_name[32], device_name[32];
+	char manuf_name[32];
 	int i;
 	static enum battery_type battery_type = BATTERY_TYPE_COUNT;
 
@@ -33,36 +71,18 @@ static int get_battery_type(void)
 	if (battery_manufacturer_name(manuf_name, sizeof(manuf_name)))
 		return battery_type;
 
-	/*
-	 * Compare the manufacturer name read from the fuel gauge to the
-	 * manufacturer names defined in the board_battery_info table. If
-	 * a device name has been specified in the board_battery_info table,
-	 * then both the manufacturer and device name must match.
-	 */
-	for (i = 0; i < BATTERY_TYPE_COUNT; i++) {
-		const struct fuel_gauge_info * const fuel_gauge =
-			&board_battery_info[i].fuel_gauge;
-		int len = 0;
-
-		if (strcasecmp(manuf_name, fuel_gauge->manuf_name))
-			continue;
-
-		if (fuel_gauge->device_name != NULL) {
-
-			if (battery_device_name(device_name,
-						sizeof(device_name)))
-				continue;
-
-			len = strlen(fuel_gauge->device_name);
-			if (strncasecmp(device_name, fuel_gauge->device_name,
-						len))
-				continue;
-		}
-
-		CPRINTS("found batt:%s", fuel_gauge->manuf_name);
+#if defined(CONFIG_BATTERY_TYPE_NO_AUTO_DETECT)
+	i = board_get_default_battery_type();
+	if (authenticate_battery_type(i, manuf_name))
 		battery_type = i;
-		break;
+#else
+	for (i = 0; i < BATTERY_TYPE_COUNT; i++) {
+		if (authenticate_battery_type(i, manuf_name)) {
+			battery_type = i;
+			break;
+		}
 	}
+#endif
 
 	return battery_type;
 }
