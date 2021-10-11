c97f93d1e8f397e9e90123333c5e6853a4af99ab
Rajesh Kumar <rajesh3.kumar@intel.com>
2021-10-11 12:22:31 -0700

    zephyr: brya: Enable battery, charger and type-c charging
    This enables the configuration for battery, charger
    and their dependencies like power delivery and usb typec.
    
    BUG=b:202701454
    BRANCH=none
    TEST=Tested feature by using Console command:
         1. battery
         2. charger
         3. chgstate
         4. chgsup
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I0cbc4d74de0f86b18b1e9a881c6a0076aba8118e

--- zephyr/shim/src/ioex.c
+++ zephyr/shim/src/ioex.c
@@ -264,6 +264,7 @@ static void ioex_isr(const struct device *port,
 
 int ioex_init(int ioex)
 {
+#ifdef CONFIG_PLATFORM_EC_IOEX_CROS_DRV
 	const struct ioexpander_drv *drv = ioex_config[ioex].drv;
 	int rv;
 
@@ -277,6 +278,7 @@ int ioex_init(int ioex)
 	}
 
 	ioex_config[ioex].flags |= IOEX_FLAGS_INITIALIZED;
+#endif /* CONFIG_PLATFORM_EC_IOEX_CROS_DRV */
 
 	return EC_SUCCESS;
 }
