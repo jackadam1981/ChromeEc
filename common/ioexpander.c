84a1a1b5fff3854552993c7bb4b3e7caf5e1858a
poornima tom <poornima.tom@intel.com>
2021-10-12 04:02:01 +0530

    ioex:Add generic function to count ioexpander
    Add generic overridable functions to detect
    num of ioexpanders used at run time.
    Replace CONFIG_IO_EXPANDER_PORT_COUNT config with
    relevant function.
    In ADL M/N, there are only 2 ioexpanders used.
    Configure the same at run time.
    
    BUG=none
    BRANCH=none
    TEST=make build all
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: I6cb328b4eafd59a5e485dd852d42e0dc9ff9a28f

--- common/ioexpander.c
+++ common/ioexpander.c
@@ -204,7 +204,7 @@ static void ioex_init_default(void)
 {
 	int i;
 
-	for (i = 0; i < CONFIG_IO_EXPANDER_PORT_COUNT; i++) {
+	for (i = 0; i < board_get_ioex_port_count(); i++) {
 		/* IO Expander has been initialized, skip re-initializing */
 		if (ioex_config[i].flags & IOEX_FLAGS_INITIALIZED)
 			continue;
@@ -342,3 +342,8 @@ static int command_ioex_get(int argc, char **argv)
 DECLARE_SAFE_CONSOLE_COMMAND(ioexget, command_ioex_get,
 			     "[name]",
 			     "Read level of IO expander pin(s)");
+
+__overridable uint8_t board_get_ioex_port_count(void)
+{
+	return CONFIG_IO_EXPANDER_PORT_COUNT;
+}
