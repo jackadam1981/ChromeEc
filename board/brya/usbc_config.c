6e02dfd5ab7a77bbe8139335b54cd3156059d9dd
Rajesh Kumar <rajesh3.kumar@intel.com>
2021-10-11 12:22:31 -0700

    zephyr: Add support for battery/charger on zephyr
    BUG=b:202701454
    BRANCH=none
    TEST=none
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I0cbc4d74de0f86b18b1e9a881c6a0076aba8118e

--- board/brya/usbc_config.c
+++ board/brya/usbc_config.c
@@ -7,6 +7,7 @@
 #include <stdbool.h>
 
 #include "common.h"
+#include "cbi.h"
 #include "compile_time_macros.h"
 #include "console.h"
 #include "driver/bc12/pi3usb9201_public.h"
@@ -70,6 +71,17 @@ const struct tcpc_config_t tcpc_config[] = {
 };
 BUILD_ASSERT(ARRAY_SIZE(tcpc_config) == USBC_PORT_COUNT);
 BUILD_ASSERT(CONFIG_USB_PD_PORT_MAX_COUNT == USBC_PORT_COUNT);
+
+
+/******************************************************************************/
+/* USB-A charging control */
+
+const int usb_port_enable[USB_PORT_COUNT] = {
+	GPIO_EN_PP5000_USBA_R,
+};
+BUILD_ASSERT(ARRAY_SIZE(usb_port_enable) == USB_PORT_COUNT);
+
+/******************************************************************************/
 
 /* USBC PPC configuration */
 struct ppc_config_t ppc_chips[] = {
