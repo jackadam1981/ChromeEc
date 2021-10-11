91b37c3c21a986719e2c1e51a71eab78ff824e58
Rajesh Kumar <rajesh3.kumar@intel.com>
2021-10-11 12:22:31 -0700

    zephyr: brya: Add support for battery and charger
    This change add support for battery and charger.
    
    BUG=b:202701454
    BRANCH=none
    TEST=Console command 'charger','battery' and 'chgsup'
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I0cbc4d74de0f86b18b1e9a881c6a0076aba8118e

--- zephyr/projects/brya/brya/BUILD.py
+++ zephyr/projects/brya/brya/BUILD.py
@@ -5,5 +5,10 @@
 register_npcx_project(
     project_name="brya",
     zephyr_board="brya",
-    dts_overlays=["gpio.dts"],
+    dts_overlays=[
+        "gpio.dts",
+        "cbi_eeprom.dts",
+        "usbc.dts",
+        "bb_retimer.dts",
+    ],
 )
