4a8ef55a04fcd4b6a8877f6b8e13d3d4096c6b8c
Daisuke Nojiri <dnojiri@chromium.org>
2021-10-11 12:50:25 -0700

    npcx/timer: Unroll udelay
    This patch flattens udelay by unrolling __hw_clock_source_read.
    This increases the chance that we record LR of the instruction
    near which an inifinite loop happened.
    
    BUG=b:200593658
    BRANCH=
    TEST=Sona. Run crash assert. Hack battery command to trigger WD reset.
         Verify LR points to the root causes.
    
    Change-Id: Ibd6cbcf18ab6d58c06ddfd19021058268289bf00
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- chip/npcx/hwtimer.c
+++ chip/npcx/hwtimer.c
@@ -16,6 +16,7 @@
 #include "console.h"
 #include "task.h"
 #include "timer.h"
+#include "registers.h"
 #include "util.h"
 
 /* Depth of event timer */
@@ -338,4 +339,25 @@ int __hw_clock_source_init(uint32_t start_t)
 	task_enable_irq(ITIM_INT(ITIM_SYSTEM_NO));
 
 	return ITIM_INT(ITIM_SYSTEM_NO);
+}
+
+/*
+ * Unrolled udelay. It preserves LR, which points to the root cause of WD crash.
+ */
+__override void udelay(unsigned us)
+{
+	uint32_t cnt, cnt2;
+	unsigned t0;
+
+	cnt = NPCX_ITCNT_SYSTEM;
+	while ((cnt2 = NPCX_ITCNT_SYSTEM) != cnt)
+		cnt = cnt2;
+
+	t0 = TICK_ITIM32_MAX_CNT - cnt;
+
+	do {
+		cnt = NPCX_ITCNT_SYSTEM;
+		while ((cnt2 = NPCX_ITCNT_SYSTEM) != cnt)
+			cnt = cnt2;
+	} while (TICK_ITIM32_MAX_CNT - cnt - t0 <= us);
 }
