8da20eceb67a44b5907e18e728f3aaf8509258f6
Rajesh Kumar <rajesh3.kumar@intel.com>
2021-10-11 12:22:31 -0700

    zephyr: brya: Enable battery, charger and type-c charging
    This enables the configuration for battery, charger
    and their dependencies like power delivery and usb typec.
    
    BUG=b:202701454
    BRANCH=none
    TEST=Tested feature by using Console command:
         1. battery
         2. charger
         3. chgstate
         4. chgsup
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I0cbc4d74de0f86b18b1e9a881c6a0076aba8118e

--- zephyr/projects/brya/brya/BUILD.py
+++ zephyr/projects/brya/brya/BUILD.py
@@ -6,10 +6,12 @@ register_npcx_project(
     project_name="brya",
     zephyr_board="brya",
     dts_overlays=[
+        "bb_retimer.dts",
         "cbi_eeprom.dts",
         "fan.dts",
         "gpio.dts",
         "motionsense.dts",
         "pwm.dts",
+        "usbc.dts",
     ],
 )
