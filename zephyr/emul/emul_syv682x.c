cc3e62be12fbd5142c8efa53ef9847b6db005899
Abe Levkoy <alevkoy@chromium.org>
2021-10-12 16:56:07 -0600

    zephyr test: SYV682 VCONN_OC interrupt
    Simulate VCONN OC and verify that the driver handles it as expected.
    Increase syv682x.c line coverage 65.4% -> 68.8%.
    
    BUG=b:190519131
    TEST=zmake configure --test zephyr/test/drivers
    BRANCH=none
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: Ifa9ff4129ef0f158e2f59314dbf62ee53b6180e8

--- zephyr/emul/emul_syv682x.c
+++ zephyr/emul/emul_syv682x.c
@@ -82,6 +82,18 @@ void syv682x_emul_set_status(struct i2c_emul *emul, uint8_t val)
 	 */
 }
 
+void syv682x_emul_set_control_4(struct i2c_emul *emul, uint8_t val)
+{
+	struct syv682x_emul_data *data;
+	uint8_t val_interrupt = val & SYV682X_CONTROL_4_INT_MASK;
+
+	data = CONTAINER_OF(emul, struct syv682x_emul_data, emul);
+	data->control_4_cond = val_interrupt;
+	/* Only update the interrupting bits. */
+	data->reg[SYV682X_CONTROL_4_REG] &= ~SYV682X_CONTROL_4_INT_MASK;
+	data->reg[SYV682X_CONTROL_4_REG] |= val_interrupt;
+}
+
 int syv682x_emul_get_reg(struct i2c_emul *emul, int reg, uint8_t *val)
 {
 	struct syv682x_emul_data *data;
@@ -142,6 +154,10 @@ static int syv682x_emul_transfer(struct i2c_emul *emul, struct i2c_msg *msgs,
 						SYV682X_STATUS_OVP))
 				val |= SYV682X_CONTROL_1_PWR_ENB;
 			break;
+		case SYV682X_CONTROL_4_REG:
+			/* Interrupt bits are read-only. */
+			val &= ~SYV682X_CONTROL_4_INT_MASK;
+			break;
 		default:
 			break;
 		}
@@ -167,14 +183,16 @@ static int syv682x_emul_transfer(struct i2c_emul *emul, struct i2c_msg *msgs,
 
 		switch (reg) {
 		/*
-		 * These registers are clear-on-read (if the underlying
-		 * condition has cleared).
+		 * STATUS and the interrupt bits of CONTROL_4 are clear-on-read
+		 * (if the underlying condition has cleared).
 		 */
 		case SYV682X_STATUS_REG:
 			syv682x_emul_set_reg(emul, reg, data->status_cond);
 			break;
 		case SYV682X_CONTROL_4_REG:
-			syv682x_emul_set_reg(emul, reg, data->control_4_cond);
+			syv682x_emul_set_reg(emul, reg,
+					(*buf & ~SYV682X_CONTROL_4_INT_MASK) |
+					data->control_4_cond);
 			break;
 		default:
 			break;
--- zephyr/test/drivers/src/ppc.c
+++ zephyr/test/drivers/src/ppc.c
@@ -121,6 +121,38 @@ static void test_ppc_syv682x_interrupt(void)
 			SYV682X_CONTROL_1_PWR_ENB,
 			"Power path enabled after HV_OC handled 3 times");
 	syv682x_emul_set_status(emul, 0);
+
+	/*
+	 * A VCONN OC event less than 100 ms should not cause VCONN to turn off.
+	 */
+	ppc_set_vconn(syv682x_port, true);
+	syv682x_emul_set_control_4(emul, SYV682X_CONTROL_4_VCONN_OCP);
+	syv682x_interrupt(syv682x_port);
+	msleep(1);
+	zassert_ok(syv682x_emul_get_reg(emul, SYV682X_CONTROL_4_REG, &reg),
+			"Reading CONTROL_4 failed");
+	zassert_true(reg &
+			(SYV682X_CONTROL_4_VCONN1 | SYV682X_CONTROL_4_VCONN2),
+			"VCONN disabled after initial VCONN OC");
+	msleep(50);
+	syv682x_emul_set_control_4(emul, SYV682X_CONTROL_4_VCONN_OCP);
+	syv682x_interrupt(syv682x_port);
+	msleep(1);
+	zassert_ok(syv682x_emul_get_reg(emul, SYV682X_CONTROL_4_REG, &reg),
+			"Reading CONTROL_4 failed");
+	zassert_true(reg &
+			(SYV682X_CONTROL_4_VCONN1 | SYV682X_CONTROL_4_VCONN2),
+			"VCONN disabled after short VCONN OC");
+	/* But one greater than 100 ms should. */
+	msleep(60);
+	syv682x_interrupt(syv682x_port);
+	msleep(1);
+	zassert_ok(syv682x_emul_get_reg(emul, SYV682X_CONTROL_4_REG, &reg),
+			"Reading CONTROL_4 failed");
+	zassert_false(reg &
+			(SYV682X_CONTROL_4_VCONN1 | SYV682X_CONTROL_4_VCONN2),
+			"VCONN enabled after long VCONN OC");
+	syv682x_emul_set_control_4(emul, 0x0);
 }
 
 static void test_ppc_syv682x(void)
