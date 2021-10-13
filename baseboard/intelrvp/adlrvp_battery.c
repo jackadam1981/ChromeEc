43dae27bad8aac2607683e040d3d5ace757e045c
poornima tom <poornima.tom@intel.com>
2021-10-14 05:20:24 +0530

    adlrvp:Reconfigure battery to 2s based at runtime
    Default battery configuration for ADL-P based sku is
    3S based. Reconfiguration to 2s based battery configuration
    at runtime for ADL M/N based RVPs can help in enabling single
    binary for all adl based skus, irrespective of it's
    differences.
    
    BUG=b:194163586
    BRANCH=none
    TEST=verified on adl
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: I94eb45dbae7ad8c67e2aa6f0142e166a22ad1a0b

--- baseboard/intelrvp/adlrvp_battery.c
+++ baseboard/intelrvp/adlrvp_battery.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  *
@@ -10,7 +10,9 @@
 #include "common.h"
 #include "util.h"
 
-const struct board_batt_params board_battery_info[] = {
+#ifdef CONFIG_BATTERY_RUNTIME_CONFIG
+/* Default 3S battery configuration */
+struct board_batt_params board_battery_info[] = {
 	/*
 	 * Getac Battery (Getac SMP-HHP-408) Information
 	 * Fuel gauge: BQ40Z50-R3
@@ -22,6 +24,7 @@ const struct board_batt_params board_battery_info[] = {
 				.reg_addr = 0x00,
 				.reg_data = { 0x0010, 0x0010 },
 			},
+
 			.fet = {
 				.reg_addr = 0x0,
 				.reg_mask = 0x6000,
@@ -44,4 +47,40 @@ const struct board_batt_params board_battery_info[] = {
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
+/* 2S battery configuration */
+struct board_batt_params board_battery2S_info[] = {
+	/*
+	 * Getac Battery (Getac SMP-HHP-408) Information
+	 * Fuel gauge: BQ40Z50-R3
+	 */
+	[BATTERY_GETAC_SMP_HHP_408] = {
+		.fuel_gauge = {
+			.manuf_name = "Getac",
+			.ship_mode = {
+				.reg_addr = 0x00,
+				.reg_data = { 0x0010, 0x0010 },
+			},
+			.fet = {
+				.reg_addr = 0x0,
+				.reg_mask = 0x6000,
+				.disconnect_val = 0x6000,
+			}
+		},
+		.batt_info = {
+			.voltage_max = 8800,        /* mV */
+			.voltage_normal = 7700,
+			.voltage_min = 6000,
+			.precharge_current = 256,   /* mA */
+			.start_charging_min_c = 0,
+			.start_charging_max_c = 45,
+			.charging_min_c = 0,
+			.charging_max_c = 45,
+			.discharging_min_c = 0,
+			.discharging_max_c = 60,
+		},
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(board_battery2S_info) == BATTERY_TYPE_COUNT);
+
 const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_GETAC_SMP_HHP_408;
+#endif /* CONFIG_BATTERY_RUNTIME_CONFIG */
