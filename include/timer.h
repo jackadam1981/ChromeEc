05927613421009b6c45d0d6f2567c511854b00fc
Daisuke Nojiri <dnojiri@chromium.org>
2021-10-13 16:57:12 -0700

    timer: Make udelay overridable
    This patch defines udelay as overridable so that individual projects
    can implement customized versions. CONFIG_HW_SPECIFIC_UDELAY is no
    longer needed.
    
    BUG=None
    BRANCH=None
    TEST=buildall; zmake testall
    
    Change-Id: I291151755c3d714db6863749d66579007eb73ecd
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/timer.h
+++ include/timer.h
@@ -72,7 +72,7 @@ int timestamp_expired(timestamp_t deadline, const timestamp_t *now);
  *
  * @param us		Number of microseconds to delay.
  */
-void udelay(unsigned us);
+__override_proto void udelay(unsigned us);
 
 /**
  * Sleep.
