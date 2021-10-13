d79445c88be8ac27cf8203d7e054659a664f6247
Daisuke Nojiri <dnojiri@chromium.org>
2021-10-13 16:57:12 -0700

    timer: Remove CONFIG_HW_SPECIFIC_UDELAY
    CONFIG_HW_SPECIFIC_UDELAY is no longer used.
    
    BUG=None
    BRANCH=None
    TEST=buildall
    
    Change-Id: I291151755c3d714db6863749d66579007eb73ecd
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/config.h
+++ include/config.h
@@ -2427,9 +2427,6 @@
  */
 #undef CONFIG_HWTIMER_64BIT
 
-/* Use a hardware specific udelay(). */
-#undef CONFIG_HW_SPECIFIC_UDELAY
-
 /*****************************************************************************/
 /* I2C configuration */
 
