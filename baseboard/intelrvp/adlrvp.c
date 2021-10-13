c4670cbdf033b4fdcef97cf7d8319d10de0aa558
poornima tom <poornima.tom@intel.com>
2021-10-14 05:20:24 +0530

    adlrvp: Reconfigure battery to 2s based at runtime
    Default battery configuration for ADL-P based sku is
    3S based. Reconfiguration to 2s based battery configuration
    at runtime for ADL M/N based RVPs can help in enabling single
    binary for all ADL based skus, irrespective of it's
    differences.
    
    BUG=b:194163586
    BRANCH=none
    TEST=verified on ADL-P,M&N to get correct battery info
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: I94eb45dbae7ad8c67e2aa6f0142e166a22ad1a0b

--- baseboard/intelrvp/adlrvp.c
+++ baseboard/intelrvp/adlrvp.c
@@ -5,6 +5,7 @@
 
 /* Intel ADLRVP board-specific common configuration */
 
+#include "battery_fuel_gauge.h"
 #include "charger.h"
 #include "bq25710.h"
 #include "common.h"
@@ -24,6 +25,9 @@
 
 #define CPRINTS(format, args...) cprints(CC_COMMAND, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_COMMAND, format, ## args)
+
+/* battery 2S config */
+extern struct board_batt_params board_battery2S_info[];
 
 /* TCPC AIC GPIO Configuration */
 const struct tcpc_aic_gpio_config_t tcpc_aic_gpios[] = {
@@ -388,6 +392,22 @@ static void configure_retimer_usbmux(void)
 	}
 }
 
+static void configure_battery(void)
+{
+	switch (ADL_RVP_BOARD_ID(board_get_version())) {
+	case ADLM_LP4_RVP1_SKU_BOARD_ID:
+	case ADLM_LP5_RVP2_SKU_BOARD_ID:
+	case ADLM_LP5_RVP3_SKU_BOARD_ID:
+	case ADLN_LP5_ERB_SKU_BOARD_ID:
+	case ADLN_LP5_RVP_SKU_BOARD_ID:
+		/* Reconfigure Battery to 2S based */
+		board_battery_info[BATTERY_GETAC_SMP_HHP_408].batt_info =
+		board_battery2S_info[BATTERY_GETAC_SMP_HHP_408].batt_info;
+		break;
+	default:
+		break;
+	}
+}
 /******************************************************************************/
 /* PWROK signal configuration */
 /*
@@ -483,4 +503,7 @@ __override void board_pre_task_i2c_peripheral_init(void)
 
 	/* Configure board specific retimer & mux */
 	configure_retimer_usbmux();
+
+	/* Configure battery config */
+	configure_battery();
 }
