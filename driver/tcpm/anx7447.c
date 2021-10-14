0675354145a09d817e2b34538285d529c9dd7075
Tom Hughes <tomhughes@chromium.org>
2021-10-14 22:56:34 +0000

    tree: Move DECLARE_CONSOLE_COMMAND before command definition
    The DECLARE_CONSOLE_COMMAND macro adds __attribute__((unused)) to the
    handler declaration in the !CONFIG_ZEPHYR && !HAS_TASK_CONSOLE case.
    The declaration with this attribute must come before the definition of
    the function or the compiler can ignore it.
    
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Id4ff3dacb5ee614c738e00fdfb614b6752b42852

--- driver/tcpm/anx7447.c
+++ driver/tcpm/anx7447.c
@@ -252,6 +252,9 @@ int anx7447_flash_erase(int port)
 					    0 /* suppress console if empty */);
 }
 
+DECLARE_CONSOLE_COMMAND(anx_ocm, command_anx_ocm,
+			"port [erase]",
+			"Print OCM status or erases OCM for a given port.");
 /* Add console command to erase OCM flash if needed. */
 static int command_anx_ocm(int argc, char **argv)
 {
@@ -282,9 +285,6 @@ static int command_anx_ocm(int argc, char **argv)
 
 	return EC_SUCCESS;
 }
-DECLARE_CONSOLE_COMMAND(anx_ocm, command_anx_ocm,
-			"port [erase]",
-			"Print OCM status or erases OCM for a given port.");
 #endif
 
 static int anx7447_init(int port)
