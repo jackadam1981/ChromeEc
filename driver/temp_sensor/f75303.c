aed54999df86a7a44ea81f4030af96dcf1f0aa00
Tom Hughes <tomhughes@chromium.org>
2021-10-14 22:56:34 +0000

    tree: Move DECLARE_CONSOLE_COMMAND before command definition
    The DECLARE_CONSOLE_COMMAND macro adds __attribute__((unused)) to the
    handler declaration in the !CONFIG_ZEPHYR && !HAS_TASK_CONSOLE case.
    The declaration with this attribute must come before the definition of
    the function or the compiler can ignore it.
    
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Id4ff3dacb5ee614c738e00fdfb614b6752b42852

--- driver/temp_sensor/f75303.c
+++ driver/temp_sensor/f75303.c
@@ -59,6 +59,9 @@ static void f75303_sensor_poll(void)
 }
 DECLARE_HOOK(HOOK_SECOND, f75303_sensor_poll, HOOK_PRIO_TEMP_SENSOR);
 
+DECLARE_CONSOLE_COMMAND(f75303, f75303_set_fake_temp,
+		"<index> <value>|off",
+		"Set fake temperature of sensor f75303.");
 static int f75303_set_fake_temp(int argc, char **argv)
 {
 	int index;
@@ -88,6 +91,3 @@ static int f75303_set_fake_temp(int argc, char **argv)
 
 	return EC_SUCCESS;
 }
-DECLARE_CONSOLE_COMMAND(f75303, f75303_set_fake_temp,
-		"<index> <value>|off",
-		"Set fake temperature of sensor f75303.");
