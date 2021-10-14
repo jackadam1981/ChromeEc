1a64c95d81f84617b4afa20f7ed3bfe538abdc03
Tom Hughes <tomhughes@chromium.org>
2021-10-14 22:56:34 +0000

    tree: Move DECLARE_CONSOLE_COMMAND before command definition
    The DECLARE_CONSOLE_COMMAND macro adds __attribute__((unused)) to the
    handler declaration in the !CONFIG_ZEPHYR && !HAS_TASK_CONSOLE case.
    The declaration with this attribute must come before the definition of
    the function or the compiler can ignore it.
    
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Id4ff3dacb5ee614c738e00fdfb614b6752b42852

--- driver/battery/bq27621_g1.c
+++ driver/battery/bq27621_g1.c
@@ -562,6 +562,9 @@ int battery_wait_for_stable(void)
 
 #ifdef CONFIG_CMD_BATDEBUG
 
+DECLARE_CONSOLE_COMMAND(fgunseal, command_fgunseal,
+			"",
+			"Unseal the fg");
 static int command_fgunseal(int argc, char **argv)
 {
 	int rv = EC_SUCCESS;
@@ -574,10 +577,9 @@ static int command_fgunseal(int argc, char **argv)
 	return rv;
 }
 
-DECLARE_CONSOLE_COMMAND(fgunseal, command_fgunseal,
+DECLARE_CONSOLE_COMMAND(fgseal, command_fgseal,
 			"",
-			"Unseal the fg");
-
+			"Seal the fg");
 static int command_fgseal(int argc, char **argv)
 {
 	int rv = EC_SUCCESS;
@@ -590,10 +592,10 @@ static int command_fgseal(int argc, char **argv)
 	return rv;
 }
 
-DECLARE_CONSOLE_COMMAND(fgseal, command_fgseal,
-			"",
-			"Seal the fg");
 
+DECLARE_CONSOLE_COMMAND(fginit, command_fginit,
+			"[force]",
+			"Initialize the fg");
 static int command_fginit(int argc, char **argv)
 {
 	int rv = EC_SUCCESS;
@@ -625,10 +627,10 @@ static int command_fginit(int argc, char **argv)
 	return rv;
 }
 
-DECLARE_CONSOLE_COMMAND(fginit, command_fginit,
-			"[force]",
-			"Initialize the fg");
 
+DECLARE_CONSOLE_COMMAND(fgprobe, command_fgprobe,
+			"",
+			"Probe the fg");
 static int command_fgprobe(int argc, char **argv)
 {
 	int rv = EC_SUCCESS;
@@ -641,10 +643,10 @@ static int command_fgprobe(int argc, char **argv)
 	return rv;
 }
 
-DECLARE_CONSOLE_COMMAND(fgprobe, command_fgprobe,
-			"",
-			"Probe the fg");
 
+DECLARE_CONSOLE_COMMAND(fgrd, command_fgrd,
+			"cmd len",
+			"Read _len_ words from the fg");
 static int command_fgrd(int argc, char **argv)
 {
 	int cmd, len;
@@ -675,10 +677,10 @@ static int command_fgrd(int argc, char **argv)
 	return rv;
 }
 
-DECLARE_CONSOLE_COMMAND(fgrd, command_fgrd,
-			"cmd len",
-			"Read _len_ words from the fg");
 
+DECLARE_CONSOLE_COMMAND(fgcmd, command_fgcmd,
+			"cmd data [byte]",
+			"Send a cmd to the fg");
 static int command_fgcmd(int argc, char **argv)
 {
 	int cmd, data, byte = 0;
@@ -711,10 +713,10 @@ static int command_fgcmd(int argc, char **argv)
 
 }
 
-DECLARE_CONSOLE_COMMAND(fgcmd, command_fgcmd,
-			"cmd data [byte]",
-			"Send a cmd to the fg");
 
+DECLARE_CONSOLE_COMMAND(fgcmdrd, command_fgcmdrd,
+			"cmd data",
+			"Send a 2-byte cmd to the fg, read back the 2-byte result");
 static int command_fgcmdrd(int argc, char **argv)
 {
 	int cmd, data, val;
@@ -739,8 +741,5 @@ static int command_fgcmdrd(int argc, char **argv)
 	return rv;
 }
 
-DECLARE_CONSOLE_COMMAND(fgcmdrd, command_fgcmdrd,
-			"cmd data",
-			"Send a 2-byte cmd to the fg, read back the 2-byte result");
 
 #endif /* CONFIG_CMD_BATDEBUG */
