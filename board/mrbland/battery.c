39a0bd5d0b881db672d75eefdb73884b0f0dab82
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2021-10-14 09:15:15 +0800

    battery: Move battery get/set vendor param to common/battery.c
    Since the functions are copied by many boards, move them to common.
    The specific param regs are defined in each specific boards.
    
    BUG=none
    BRANCH=none
    TEST=make BOARD=burnet/coachz/eve/homestar/kappa/mrbland/quackingstick/
    wormdingler.
    
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Change-Id: I77c070663ad3e800ec484bd21865c5d911a2c48b

--- board/mrbland/battery.c
+++ board/mrbland/battery.c
@@ -124,3 +124,6 @@ const struct board_batt_params board_battery_info[] = {
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
 const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_L21D2PG1;
+
+/* Starting byte of specific battery vendor parameter */
+uint32_t battery_vendor_param_start = 0x70;
