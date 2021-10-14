417a9af2f15da1f77726cad5b73b937f3a3cabc8
Tom Hughes <tomhughes@chromium.org>
2021-10-14 20:04:24 +0000

    tree: Move DECLARE_HOST_COMMAND before command
    The DECLARE_HOST_COMMAND macro adds __attribute__((unused)) to the
    handler declaration in the !CONFIG_ZEPHYR && !HAS_TASK_HOSTCMD case.
    The declaration with this attribute must come before the definition of
    the function or the compiler can ignore it.
    
    We see this behavior when compiling with clang:
    
    CC=clang make BOARD=gingerbread
    
    chip/stm32/host_command_common.c:20:23: error: unused function
    'host_command_protocol_info' [-Werror,-Wunused-function]
    static enum ec_status host_command_protocol_info(struct
                                                     host_cmd_handler_args
    
    BRANCH=none
    BUG=b:172020503
    TEST=make buildall -j
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I1be95643c9e0908ee2b21b7c42c81266539b7520

--- common/panic_output.c
+++ common/panic_output.c
@@ -416,6 +416,9 @@ DECLARE_CONSOLE_COMMAND(panicinfo, command_panicinfo,
 /*****************************************************************************/
 /* Host commands */
 
+DECLARE_HOST_COMMAND(EC_CMD_GET_PANIC_INFO,
+		     host_command_panic_info,
+		     EC_VER_MASK(0));
 static enum ec_status
 host_command_panic_info(struct host_cmd_handler_args *args)
 {
@@ -437,6 +440,3 @@ host_command_panic_info(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_GET_PANIC_INFO,
-		     host_command_panic_info,
-		     EC_VER_MASK(0));
