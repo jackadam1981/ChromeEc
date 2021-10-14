9603faeedbc4bcd60e74396246f97264ce1d4393
poornima tom <poornima.tom@intel.com>
2021-10-14 05:57:03 +0530

    adlrvp: Configure typec ports at runtime
    Default board ADL-P has 4 typec ports , while
    ADL-M/N has only 2 typec ports. Configure the
    number of typec ports based on board id and use
    an overridable function to return this number
    at run time.
    
    BUG=b:194163586
    BRANCH=none
    TEST=usbpdmuxinfo must list only 2 typec ports for M/N
    
    Also, typec 2/3 must be listed as invalid ports in M/P.
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: I7daf1f728e0c630745b8532236abcd0c27f89a4e

--- baseboard/intelrvp/adlrvp.c
+++ baseboard/intelrvp/adlrvp.c
@@ -28,6 +28,9 @@
 
 static enum battery_type bat_cell_type;
 
+/* Variable to hold the number of typec usb ports based on board type */
+static uint8_t adlrvp_usbc_ports;
+
 /* TCPC AIC GPIO Configuration */
 const struct tcpc_aic_gpio_config_t tcpc_aic_gpios[] = {
 	[TYPE_C_PORT_0] = {
@@ -408,6 +411,22 @@ static void configure_battery_cell_type(void)
 		break;
 	}
 }
+
+static void configure_usbc_ports(void)
+{
+	switch (ADL_RVP_BOARD_ID(board_get_version())) {
+	case ADLM_LP4_RVP1_SKU_BOARD_ID:
+	case ADLM_LP5_RVP2_SKU_BOARD_ID:
+	case ADLM_LP5_RVP3_SKU_BOARD_ID:
+	case ADLN_LP5_ERB_SKU_BOARD_ID:
+	case ADLN_LP5_RVP_SKU_BOARD_ID:
+		adlrvp_usbc_ports = ADL_N_M_USB_PD_PORT_MAX_COUNT;
+		break;
+	default:
+		adlrvp_usbc_ports = CONFIG_USB_PD_PORT_MAX_COUNT;
+		break;
+	}
+}
 /******************************************************************************/
 /* PWROK signal configuration */
 /*
@@ -506,6 +525,9 @@ __override void board_pre_task_i2c_peripheral_init(void)
 
 	/* Configure battery cell type */
 	configure_battery_cell_type();
+
+	/* Configure the number of typec usb ports */
+	configure_usbc_ports();
 }
 
 __override int board_get_default_battery_type(void)
@@ -514,4 +536,9 @@ __override int board_get_default_battery_type(void)
 		return DEFAULT_BATTERY_TYPE;
 	else
 		return BATTERY_GETAC_SMP_HHP_408_2S;
+}
+
+__override uint8_t board_get_usb_pd_port_count(void)
+{
+	return adlrvp_usbc_ports;
 }
