424e77c227b7d5cae71f868e6b025841ff02b1a5
Tom Hughes <tomhughes@chromium.org>
2021-10-14 20:04:24 +0000

    tree: Move DECLARE_HOST_COMMAND before command
    The DECLARE_HOST_COMMAND macro adds __attribute__((unused)) to the
    handler declaration in the !CONFIG_ZEPHYR && !HAS_TASK_HOSTCMD case.
    The declaration with this attribute must come before the definition of
    the function or the compiler can ignore it.
    
    We see this behavior when compiling with clang:
    
    CC=clang make BOARD=gingerbread
    
    chip/stm32/host_command_common.c:20:23: error: unused function
    'host_command_protocol_info' [-Werror,-Wunused-function]
    static enum ec_status host_command_protocol_info(struct
                                                     host_cmd_handler_args
    
    BRANCH=none
    BUG=b:172020503
    TEST=make buildall -j
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I1be95643c9e0908ee2b21b7c42c81266539b7520

--- common/pstore_commands.c
+++ common/pstore_commands.c
@@ -10,6 +10,9 @@
 #include "host_command.h"
 #include "util.h"
 
+DECLARE_HOST_COMMAND(EC_CMD_PSTORE_INFO,
+		     pstore_command_get_info,
+		     EC_VER_MASK(0));
 static enum ec_status
 pstore_command_get_info(struct host_cmd_handler_args *args)
 {
@@ -23,9 +26,10 @@ pstore_command_get_info(struct host_cmd_handler_args *args)
 	args->response_size = sizeof(*r);
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_PSTORE_INFO, pstore_command_get_info,
-		     EC_VER_MASK(0));
 
+DECLARE_HOST_COMMAND(EC_CMD_PSTORE_READ,
+		     pstore_command_read,
+		     EC_VER_MASK(0));
 static enum ec_status pstore_command_read(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_pstore_read *p = args->params;
@@ -59,8 +63,10 @@ static enum ec_status pstore_command_read(struct host_cmd_handler_args *args)
 	args->response_size = p->size;
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_PSTORE_READ, pstore_command_read, EC_VER_MASK(0));
 
+DECLARE_HOST_COMMAND(EC_CMD_PSTORE_WRITE,
+		     pstore_command_write,
+		     EC_VER_MASK(0));
 static enum ec_status pstore_command_write(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_pstore_write *p = args->params;
@@ -94,4 +100,3 @@ static enum ec_status pstore_command_write(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_PSTORE_WRITE, pstore_command_write, EC_VER_MASK(0));
