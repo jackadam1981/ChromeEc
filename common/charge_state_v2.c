98f9a598a4962702e8e7a51c2b3dffa8472bd0f4
Tom Hughes <tomhughes@chromium.org>
2021-10-14 20:04:24 +0000

    tree: Move DECLARE_HOST_COMMAND before command
    The DECLARE_HOST_COMMAND macro adds __attribute__((unused)) to the
    handler declaration in the !CONFIG_ZEPHYR && !HAS_TASK_HOSTCMD case.
    The declaration with this attribute must come before the definition of
    the function or the compiler can ignore it.
    
    We see this behavior when compiling with clang:
    
    CC=clang make BOARD=gingerbread
    
    chip/stm32/host_command_common.c:20:23: error: unused function
    'host_command_protocol_info' [-Werror,-Wunused-function]
    static enum ec_status host_command_protocol_info(struct
                                                     host_cmd_handler_args
    
    BRANCH=none
    BUG=b:172020503
    TEST=make buildall -j
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I1be95643c9e0908ee2b21b7c42c81266539b7520

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -2457,6 +2457,8 @@ void trigger_ocpc_reset(void)
 /*****************************************************************************/
 /* Host commands */
 
+DECLARE_HOST_COMMAND(EC_CMD_CHARGE_CONTROL, charge_command_charge_control,
+		     EC_VER_MASK(1) | EC_VER_MASK(2));
 static enum ec_status
 charge_command_charge_control(struct host_cmd_handler_args *args)
 {
@@ -2494,8 +2496,6 @@ charge_command_charge_control(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_CHARGE_CONTROL, charge_command_charge_control,
-		     EC_VER_MASK(1) | EC_VER_MASK(2));
 
 static void reset_current_limit(void)
 {
@@ -2504,6 +2504,8 @@ static void reset_current_limit(void)
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, reset_current_limit, HOOK_PRIO_DEFAULT);
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, reset_current_limit, HOOK_PRIO_DEFAULT);
 
+DECLARE_HOST_COMMAND(EC_CMD_CHARGE_CURRENT_LIMIT, charge_command_current_limit,
+		     EC_VER_MASK(0));
 static enum ec_status
 charge_command_current_limit(struct host_cmd_handler_args *args)
 {
@@ -2513,8 +2515,6 @@ charge_command_current_limit(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_CHARGE_CURRENT_LIMIT, charge_command_current_limit,
-		     EC_VER_MASK(0));
 
 /*
  * Expose charge/battery related state
@@ -2552,6 +2552,8 @@ static int charge_get_charge_state_debug(int param, uint32_t *value)
 	return EC_SUCCESS;
 }
 
+DECLARE_HOST_COMMAND(EC_CMD_CHARGE_STATE, charge_command_charge_state,
+		     EC_VER_MASK(0) | EC_VER_MASK(1));
 static enum ec_status
 charge_command_charge_state(struct host_cmd_handler_args *args)
 {
@@ -2682,8 +2684,6 @@ charge_command_charge_state(struct host_cmd_handler_args *args)
 	return rv;
 }
 
-DECLARE_HOST_COMMAND(EC_CMD_CHARGE_STATE, charge_command_charge_state,
-		     EC_VER_MASK(0) | EC_VER_MASK(1));
 
 /*****************************************************************************/
 /* Console commands */
--- common/system.c
+++ common/system.c
@@ -1173,6 +1173,8 @@ DECLARE_SAFE_CONSOLE_COMMAND(sysinfo, command_sysinfo,
 			     NULL,
 			     "Print system info");
 
+DECLARE_HOST_COMMAND(EC_CMD_SYSINFO, host_command_sysinfo,
+		     EC_VER_MASK(EC_VER_SYSINFO));
 static enum ec_status host_command_sysinfo(struct host_cmd_handler_args *args)
 {
 	struct ec_response_sysinfo *r = args->response;
@@ -1185,8 +1187,6 @@ static enum ec_status host_command_sysinfo(struct host_cmd_handler_args *args)
 	return EC_RES_SUCCESS;
 }
 
-DECLARE_HOST_COMMAND(EC_CMD_SYSINFO, host_command_sysinfo,
-		     EC_VER_MASK(EC_VER_SYSINFO));
 #endif
 
 #ifdef CONFIG_CMD_SCRATCHPAD
@@ -1564,6 +1564,9 @@ DECLARE_CONSOLE_COMMAND(rflags, command_rflags,
 /*****************************************************************************/
 /* Host commands */
 
+DECLARE_HOST_COMMAND(EC_CMD_GET_VERSION,
+		     host_command_get_version,
+		     EC_VER_MASK(0) | EC_VER_MASK(1));
 static enum ec_status
 host_command_get_version(struct host_cmd_handler_args *args)
 {
@@ -1615,11 +1618,11 @@ host_command_get_version(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_GET_VERSION,
-		     host_command_get_version,
-		     EC_VER_MASK(0) | EC_VER_MASK(1));
 
 #ifdef CONFIG_HOSTCMD_SKUID
+DECLARE_HOST_COMMAND(EC_CMD_GET_SKU_ID,
+		     host_command_get_sku_id,
+		     EC_VER_MASK(0));
 static enum ec_status
 host_command_get_sku_id(struct host_cmd_handler_args *args)
 {
@@ -1630,12 +1633,12 @@ host_command_get_sku_id(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_GET_SKU_ID,
-		     host_command_get_sku_id,
-		     EC_VER_MASK(0));
 #endif
 
 #ifdef CONFIG_HOSTCMD_AP_SET_SKUID
+DECLARE_HOST_COMMAND(EC_CMD_SET_SKU_ID,
+		     host_command_set_sku_id,
+		     EC_VER_MASK(0));
 static enum ec_status
 host_command_set_sku_id(struct host_cmd_handler_args *args)
 {
@@ -1645,12 +1648,12 @@ host_command_set_sku_id(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_SET_SKU_ID,
-		     host_command_set_sku_id,
-		     EC_VER_MASK(0));
 #endif
 
 #ifdef CONFIG_KEYBOARD_LANGUAGE_ID
+DECLARE_HOST_COMMAND(EC_CMD_GET_KEYBOARD_ID,
+		     host_command_get_keyboard_id,
+		     EC_VER_MASK(0));
 static enum ec_status
 host_command_get_keyboard_id(struct host_cmd_handler_args *args)
 {
@@ -1661,11 +1664,11 @@ host_command_get_keyboard_id(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_GET_KEYBOARD_ID,
-		     host_command_get_keyboard_id,
-		     EC_VER_MASK(0));
 #endif
 
+DECLARE_HOST_COMMAND(EC_CMD_GET_BUILD_INFO,
+		     host_command_build_info,
+		     EC_VER_MASK(0));
 static enum ec_status
 host_command_build_info(struct host_cmd_handler_args *args)
 {
@@ -1674,10 +1677,10 @@ host_command_build_info(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_GET_BUILD_INFO,
-		     host_command_build_info,
-		     EC_VER_MASK(0));
 
+DECLARE_HOST_COMMAND(EC_CMD_GET_CHIP_INFO,
+		     host_command_get_chip_info,
+		     EC_VER_MASK(0));
 static enum ec_status
 host_command_get_chip_info(struct host_cmd_handler_args *args)
 {
@@ -1691,10 +1694,10 @@ host_command_get_chip_info(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_GET_CHIP_INFO,
-		     host_command_get_chip_info,
-		     EC_VER_MASK(0));
 
+DECLARE_HOST_COMMAND(EC_CMD_GET_BOARD_VERSION,
+		     host_command_get_board_version,
+		     EC_VER_MASK(0));
 static enum ec_status
 host_command_get_board_version(struct host_cmd_handler_args *args)
 {
@@ -1712,10 +1715,10 @@ host_command_get_board_version(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_GET_BOARD_VERSION,
-		     host_command_get_board_version,
-		     EC_VER_MASK(0));
 
+DECLARE_HOST_COMMAND(EC_CMD_REBOOT_EC,
+		     host_command_reboot,
+		     EC_VER_MASK(0));
 static enum ec_status host_command_reboot(struct host_cmd_handler_args *args)
 {
 	struct ec_params_reboot_ec p;
@@ -1770,9 +1773,6 @@ static enum ec_status host_command_reboot(struct host_cmd_handler_args *args)
 		return EC_RES_ERROR;
 	}
 }
-DECLARE_HOST_COMMAND(EC_CMD_REBOOT_EC,
-		     host_command_reboot,
-		     EC_VER_MASK(0));
 
 int system_can_boot_ap(void)
 {
