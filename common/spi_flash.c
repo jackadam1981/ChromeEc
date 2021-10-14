a2b3059c3157f67f52f2db1625b459c1ab787170
Tom Hughes <tomhughes@chromium.org>
2021-10-14 20:04:24 +0000

    tree: Move DECLARE_HOST_COMMAND before command
    The DECLARE_HOST_COMMAND macro adds __attribute__((unused)) to the
    handler declaration in the !CONFIG_ZEPHYR && !HAS_TASK_HOSTCMD case.
    The declaration with this attribute must come before the definition of
    the function or the compiler can ignore it.
    
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I1be95643c9e0908ee2b21b7c42c81266539b7520

--- common/spi_flash.c
+++ common/spi_flash.c
@@ -511,6 +511,9 @@ DECLARE_CONSOLE_COMMAND(spi_flashinfo, command_spi_flashinfo,
 	"Print SPI flash info");
 
 #ifdef CONFIG_HOSTCMD_FLASH_SPI_INFO
+DECLARE_HOST_COMMAND(EC_CMD_FLASH_SPI_INFO,
+		     flash_command_spi_info,
+		     EC_VER_MASK(0));
 static enum ec_status flash_command_spi_info(struct host_cmd_handler_args *args)
 {
 	struct ec_response_flash_spi_info *r = args->response;
@@ -524,9 +527,6 @@ static enum ec_status flash_command_spi_info(struct host_cmd_handler_args *args)
 	args->response_size = sizeof(*r);
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_FLASH_SPI_INFO,
-		     flash_command_spi_info,
-		     EC_VER_MASK(0));
 #endif  /* CONFIG_HOSTCMD_FLASH_SPI_INFO */
 
 #ifdef CONFIG_CMD_SPI_FLASH
