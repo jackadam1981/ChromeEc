3127a8b318ff371825f219fa05192f74cd34cc02
Tom Hughes <tomhughes@chromium.org>
2021-10-14 20:04:24 +0000

    tree: Move DECLARE_HOST_COMMAND before command
    The DECLARE_HOST_COMMAND macro adds __attribute__((unused)) to the
    handler declaration in the !CONFIG_ZEPHYR && !HAS_TASK_HOSTCMD case.
    The declaration with this attribute must come before the definition of
    the function or the compiler can ignore it.
    
    We see this behavior when compiling with clang:
    
    CC=clang make BOARD=gingerbread
    
    chip/stm32/host_command_common.c:20:23: error: unused function
    'host_command_protocol_info' [-Werror,-Wunused-function]
    static enum ec_status host_command_protocol_info(struct
                                                     host_cmd_handler_args
    
    BRANCH=none
    BUG=b:172020503
    TEST=make buildall -j
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I1be95643c9e0908ee2b21b7c42c81266539b7520

--- common/button.c
+++ common/button.c
@@ -471,6 +471,7 @@ DECLARE_CONSOLE_COMMAND(button, console_command_button,
 #endif /* CONFIG_CMD_BUTTON */
 
 #ifdef CONFIG_HOSTCMD_BUTTON
+DECLARE_HOST_COMMAND(EC_CMD_BUTTON, host_command_button, EC_VER_MASK(0));
 static enum ec_status host_command_button(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_button *p = args->params;
@@ -490,7 +491,6 @@ static enum ec_status host_command_button(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_BUTTON, host_command_button, EC_VER_MASK(0));
 
 #endif /* CONFIG_HOSTCMD_BUTTON */
 
