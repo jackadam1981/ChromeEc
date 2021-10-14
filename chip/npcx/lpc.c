987319a3ca9a1e1650ad5f51d1415ac5a8157307
Tom Hughes <tomhughes@chromium.org>
2021-10-14 20:04:24 +0000

    tree: Move DECLARE_HOST_COMMAND before command
    The DECLARE_HOST_COMMAND macro adds __attribute__((unused)) to the
    handler declaration in the !CONFIG_ZEPHYR && !HAS_TASK_HOSTCMD case.
    The declaration with this attribute must come before the definition of
    the function or the compiler can ignore it.
    
    We see this behavior when compiling with clang:
    
    CC=clang make BOARD=gingerbread
    
    chip/stm32/host_command_common.c:20:23: error: unused function
    'host_command_protocol_info' [-Werror,-Wunused-function]
    static enum ec_status host_command_protocol_info(struct
                                                     host_cmd_handler_args
    
    BRANCH=none
    BUG=b:172020503
    TEST=make buildall -j
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I1be95643c9e0908ee2b21b7c42c81266539b7520

--- chip/npcx/lpc.c
+++ chip/npcx/lpc.c
@@ -958,6 +958,9 @@ static void lpc_init(void)
  */
 DECLARE_HOOK(HOOK_INIT, lpc_init, HOOK_PRIO_INIT_LPC);
 
+DECLARE_HOST_COMMAND(EC_CMD_GET_PROTOCOL_INFO,
+		lpc_get_protocol_info,
+		EC_VER_MASK(0));
 /* Get protocol information */
 static enum ec_status lpc_get_protocol_info(struct host_cmd_handler_args *args)
 {
@@ -973,9 +976,6 @@ static enum ec_status lpc_get_protocol_info(struct host_cmd_handler_args *args)
 
 	return EC_RES_SUCCESS;
 }
-DECLARE_HOST_COMMAND(EC_CMD_GET_PROTOCOL_INFO,
-		lpc_get_protocol_info,
-		EC_VER_MASK(0));
 
 #if DEBUG_LPC
 static int command_lpc(int argc, char **argv)
