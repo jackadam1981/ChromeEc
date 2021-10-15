efe6214e1fd7eda0c6f2c12baa0161b1d77f7a95
Vijay Hiremath <vijay.p.hiremath@intel.com>
2021-10-15 15:21:58 -0700

    tcpm: Support PD chips with SBU protection
    Some PD chips have integrated port protection for SBU lines and
    the switches to enable the SBU lines coming out of the PD chip
    are controlled by vendor specific registers. Hence, added the
    code in TCPM & driver skeleton in TCPCI to support those chips.
    
    By checking if the SBU enable function exists in TCPM driver
    before trying to enable the SBU from discrete PPC, this code
    supports co-existence of both PD based SBU protection and
    discrete PPC chip SBU protection in a same platform.
    
    BUG=none
    BRANCH=none
    TEST=make buildall -j, zmake testall
    
    Change-Id: I3d3a5eec9077df95c0eb91cf1a709cb79f36ca60
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- common/usbc/usb_mode.c
+++ common/usbc/usb_mode.c
@@ -243,7 +243,10 @@ void enter_usb_accepted(int port, enum tcpci_msg_type type)
 		break;
 	case USB4_ENTER_SOP:
 		/* Connect the SBU and USB lines to the connector */
-		if (IS_ENABLED(CONFIG_USBC_PPC_SBU))
+		if (IS_ENABLED(CONFIG_USB_PD_SBU) &&
+			tcpc_config[port].drv->set_sbu)
+			tcpc_set_sbu(port, 1);
+		else if (IS_ENABLED(CONFIG_USBC_PPC_SBU))
 			ppc_set_sbu(port, 1);
 
 		usb4_state[port] = USB4_ACTIVE;
