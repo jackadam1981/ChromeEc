41ba252db14f26dbd33a21f2ccf5a9e4a3e502e3
Devin Lu <Devin.Lu@quantatw.com>
2021-10-19 18:33:26 +0800

    vell: Configure USBC ports
    Vell has 4 USBC ports:
    C0: TCPC NCT3808 port 1 + SYV682B + BBR + BC1.2
    C1: TCPC NCT3808 port 2 + SYV682B + BBR + BC1.2
    C2: TCPC NCT3808 port 1 + SYV682B + BBR + BC1.2
    C3: TCPC NCT3808 port 2 + SYV682B + BBR + BC1.2
    
    BUG=b:186907625
    BRANCH=none
    TEST=make BOARD=vell
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: Ideb5fcb438d6d68d568ae6776c3fdab0c16f2103

--- board/vell/i2c.c
+++ board/vell/i2c.c
@@ -20,36 +20,35 @@ const struct i2c_port_t i2c_ports[] = {
 	},
 	{
 		/* I2C1 */
-		.name = "tcpc0,2",
-		.port = I2C_PORT_USB_C0_C2_TCPC,
+		.name = "tcpc0,1",
+		.port = I2C_PORT_USB_C0_C1_TCPC,
 		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_TCPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_TCPC_SDA,
+		.scl = GPIO_EC_I2C_USB_C0_C1_TCPC_SCL,
+		.sda = GPIO_EC_I2C_USB_C0_C1_TCPC_SDA,
 	},
 	{
 		/* I2C2 */
-		.name = "ppc0,2",
-		.port = I2C_PORT_USB_C0_C2_PPC,
+		.name = "ppc0,1",
+		.port = I2C_PORT_USB_C0_C1_PPC,
 		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SDA,
+		.scl = GPIO_EC_I2C_USB_C0_C1_PPC_BC_SCL,
+		.sda = GPIO_EC_I2C_USB_C0_C1_PPC_BC_SDA,
 	},
 	{
 		/* I2C3 */
-		.name = "retimer0,2",
-		.port = I2C_PORT_USB_C0_C2_MUX,
+		.name = "retimer0,1",
+		.port = I2C_PORT_USB_C0_C1_MUX,
 		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_RT_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_RT_SDA,
+		.scl = GPIO_EC_I2C_USB_C0_C1_RT_SCL,
+		.sda = GPIO_EC_I2C_USB_C0_C1_RT_SDA,
 	},
 	{
-		/* I2C4 C1 TCPC */
-		.name = "tcpc1",
-		.port = I2C_PORT_USB_C1_TCPC,
+		/* I2C4 */
+		.name = "tcpc2,3",
+		.port = I2C_PORT_USB_C2_C3_TCPC,
 		.kbps = 400,
-		.scl = GPIO_EC_I2C_USB_C1_TCPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_TCPC_SDA,
-		.flags = I2C_PORT_FLAG_DYNAMIC_SPEED,
+		.scl = GPIO_EC_I2C_USB_C2_C3_TCPC_SCL,
+		.sda = GPIO_EC_I2C_USB_C2_C3_TCPC_SDA,
 	},
 	{
 		/* I2C5 */
@@ -60,12 +59,20 @@ const struct i2c_port_t i2c_ports[] = {
 		.sda = GPIO_EC_I2C_BAT_SDA,
 	},
 	{
+		/* I2C5_1 */
+		.name = "retimer2,3",
+		.port = I2C_PORT_USB_C2_C3_MUX,
+		.kbps = 1000,
+		.scl = GPIO_EC_I2C_USB_C2_C3_RT_SCL,
+		.sda = GPIO_EC_I2C_USB_C2_C3_RT_SDA,
+	},
+	{
 		/* I2C6 */
-		.name = "ppc1",
-		.port = I2C_PORT_USB_C1_PPC,
+		.name = "ppc2,3",
+		.port = I2C_PORT_USB_C2_C3_PPC,
 		.kbps = 400,
-		.scl = GPIO_EC_I2C_USB_C1_MIX_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_MIX_SDA,
+		.scl = GPIO_EC_I2C_USB_C2_C3_PPC_BC_SCL,
+		.sda = GPIO_EC_I2C_USB_C2_C3_PPC_BC_SDA,
 	},
 	{
 		/* I2C7 */
