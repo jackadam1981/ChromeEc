ebe4108f50d81a74b288a75a97bfd2e3520cfa73
Denis Brockus <dbrockus@google.com>
2021-10-26 16:50:44 -0600

    WIP: power-signal: gather power-signal information from DT
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: I151c451f0725449459744b1ebda042c0c3aa6ad8

---
+++ zephyr/shim/src/power.c
@@ -0,0 +1,37 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <sys/util.h>
+
+#include "console.h"
+#include "power.h"
+#include "power/power.h"
+
+/* power signal list.  Must match order of enum power_signal. */
+
+#if (SYSTEM_DT_NODE_POWER_SIGNAL_CONFIG)
+
+const struct power_signal_info power_signal_list[] = {
+	DT_FOREACH_STATUS_OKAY(named_gpios, GEN_POWER_SIGNAL_STRUCT_PARENT)
+};
+BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
+
+#elif defined(CONFIG_AP_ARM_MTK_MT8192)
+
+/*
+ * TODO: Convert all of the profiles that define
+ * CONFIG_AP_ARM_MTK_MT8192 and replace this default
+ * enum with the following warning
+ *
+ * #warning "enum power_signal should use DT"
+ */
+const struct power_signal_info power_signal_list[] = {
+	{GPIO_PMIC_EC_PWRGD, POWER_SIGNAL_ACTIVE_HIGH, "PMIC_PWR_GOOD"},
+	{GPIO_AP_IN_SLEEP_L, POWER_SIGNAL_ACTIVE_LOW, "AP_IN_S3_L"},
+	{GPIO_AP_EC_WATCHDOG_L, POWER_SIGNAL_ACTIVE_LOW, "AP_WDT_ASSERTED"},
+};
+BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
+
+#endif
