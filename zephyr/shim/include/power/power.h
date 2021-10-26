bd3ee101a4a1aec0268685138e05ee2a46b1a969
Denis Brockus <dbrockus@google.com>
2021-10-26 16:50:44 -0600

    zephyr: mt8192: gather power-signal information from DT
    This currently only uses DT power-signal for
    platforms that define CONFIG_AP_ARM_MTK_MT8192
    
    BUG=b:188674805
    BRANCH=none
    TEST=zmake testall
    
    Signed-off-by: Denis Brockus <dbrockus@google.com>
    Change-Id: I151c451f0725449459744b1ebda042c0c3aa6ad8

---
+++ zephyr/shim/include/power/power.h
@@ -0,0 +1,105 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_CHROME_POWER_POWER_H
+#define ZEPHYR_CHROME_POWER_POWER_H
+
+#include <devicetree.h>
+
+#define POWER_SIGNAL_LIST_NODE                                                \
+	DT_NODELABEL(power_signal_list)
+
+#define SYSTEM_DT_POWER_SIGNAL_CONFIG                                         \
+	DT_NODE_EXISTS(POWER_SIGNAL_LIST_NODE)
+
+#if (SYSTEM_DT_POWER_SIGNAL_CONFIG)
+
+/*
+ * I want to use the flags to convert to POWER_SIGNAL_ACTIVE_* but it is
+ * not working for me... I added a hack to verify I could boot and the
+ * fields are the same in the old static method and the from DT method.
+ * The ,flags section is the last thing I need to figure out.
+ */
+#define GEN_POWER_SIGNAL_STRUCT_ENTRY_GPIO(cid)                               \
+	DT_STRING_UPPER_TOKEN(                                                \
+		DT_PROP(                                                      \
+			cid,                                                  \
+			gpio                                                  \
+		),                                                            \
+		enum_name                                                     \
+	)
+#define GEN_POWER_SIGNAL_STRUCT_ENTRY_FLAGS(cid)                              \
+(                                                                             \
+	DT_GPIO_FLAGS(                                                        \
+		DT_PROP(                                                      \
+			cid,                                                  \
+			gpio                                                  \
+		),                                                            \
+		gpios                                                         \
+	) & GPIO_ACTIVE_LOW                                                   \
+		? POWER_SIGNAL_ACTIVE_LOW                                     \
+		: POWER_SIGNAL_ACTIVE_HIGH                                    \
+)
+#define GEN_POWER_SIGNAL_STRUCT_ENTRY_NAME(cid)                               \
+	DT_PROP(                                                              \
+		cid,                                                          \
+		power_enum_name                                               \
+	)
+
+#define GEN_POWER_SIGNAL_STRUCT_ENTRY(cid)                                    \
+{                                                                             \
+	.gpio = GEN_POWER_SIGNAL_STRUCT_ENTRY_GPIO(cid),                      \
+	.flags = GEN_POWER_SIGNAL_STRUCT_ENTRY_FLAGS(cid),                    \
+	.name = GEN_POWER_SIGNAL_STRUCT_ENTRY_NAME(cid)                       \
+}
+#define GEN_POWER_SIGNAL_STRUCT(cid)                                          \
+	[GEN_POWER_SIGNAL_ENUM_ENTRY(cid)] =                                  \
+		GEN_POWER_SIGNAL_STRUCT_ENTRY(cid),
+#define GEN_POWER_SIGNAL_STRUCT_PARENT(pid)                                   \
+	DT_FOREACH_CHILD(                                                     \
+		pid,                                                          \
+		GEN_POWER_SIGNAL_STRUCT                                       \
+	)
+#define INIT_POWER_SIGNAL_STRUCT                                              \
+	GEN_POWER_SIGNAL_STRUCT_PARENT(                                       \
+		POWER_SIGNAL_LIST_NODE                                        \
+	)
+
+
+#define GEN_POWER_SIGNAL_ENUM_ENTRY(cid)                                      \
+	DT_STRING_UPPER_TOKEN(                                                \
+		cid,                                                          \
+		power_enum_name                                               \
+	)
+#define GEN_POWER_SIGNAL_ENUM(cid)                                            \
+	GEN_POWER_SIGNAL_ENUM_ENTRY(cid),
+#define GEN_POWER_SIGNAL_ENUM_PARENT(pid)                                     \
+	DT_FOREACH_CHILD(                                                     \
+		pid,                                                          \
+		GEN_POWER_SIGNAL_ENUM                                         \
+	)
+#define INIT_POWER_SIGNAL_ENUM                                                \
+	GEN_POWER_SIGNAL_ENUM_PARENT(                                         \
+		POWER_SIGNAL_LIST_NODE                                        \
+	)
+
+enum power_signal {
+	INIT_POWER_SIGNAL_ENUM
+	POWER_SIGNAL_COUNT
+};
+
+/*
+ * Verify the number of required power-signals are specified in
+ * the DeviceTree
+ */
+#define POWER_SIGNALS_REQUIRED                                                \
+	DT_PROP(                                                              \
+		POWER_SIGNAL_LIST_NODE,                                       \
+		power_signal_count                                            \
+	)
+BUILD_ASSERT(POWER_SIGNALS_REQUIRED == POWER_SIGNAL_COUNT);
+
+#endif /* SYSTEM_DT_POWER_SIGNAL_CONFIG */
+#endif /* ZEPHYR_CHROME_POWER_POWER_H */
