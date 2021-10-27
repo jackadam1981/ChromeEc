ca228c15c4a7b9247ed57d41a4fbfaf70aaa0e45
Peter Marheine <pmarheine@chromium.org>
2021-10-27 14:15:37 +1100

    volteer: configure BC1.2 on type-C ports with DT
    This adopts the recently-added support for PI3USB9201 in Zephyr.
    
    BUG=b:202397628
    TEST=builds
    BRANCH=None
    
    Change-Id: I87c898a4d1296090c723c2802df7cf3a27991aee
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>

--- board/volteer/usbc_config.c
+++ board/volteer/usbc_config.c
@@ -339,26 +339,8 @@ static void board_tcpc_init(void)
 	/* Enable TCPC interrupts. */
 	gpio_enable_interrupt(GPIO_USB_C0_TCPC_INT_ODL);
 	gpio_enable_interrupt(GPIO_USB_C1_TCPC_INT_ODL);
-
-	/* Enable BC1.2 interrupts. */
-	gpio_enable_interrupt(GPIO_USB_C0_BC12_INT_ODL);
-	gpio_enable_interrupt(GPIO_USB_C1_BC12_INT_ODL);
 }
 DECLARE_HOOK(HOOK_INIT, board_tcpc_init, HOOK_PRIO_INIT_CHIPSET);
-
-/******************************************************************************/
-/* BC1.2 charger detect configuration */
-const struct pi3usb9201_config_t pi3usb9201_bc12_chips[] = {
-	[USBC_PORT_C0] = {
-		.i2c_port = I2C_PORT_USB_C0,
-		.i2c_addr_flags = PI3USB9201_I2C_ADDR_3_FLAGS,
-	},
-	[USBC_PORT_C1] = {
-		.i2c_port = I2C_PORT_USB_C1,
-		.i2c_addr_flags = PI3USB9201_I2C_ADDR_3_FLAGS,
-	},
-};
-BUILD_ASSERT(ARRAY_SIZE(pi3usb9201_bc12_chips) == USBC_PORT_COUNT);
 
 /******************************************************************************/
 /* TCPC support routines */
