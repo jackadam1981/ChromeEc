f66b5fdaf9e1f1f6e85d8c946a2d5f68b0268329
Tinghan Shen <tinghan.shen@mediatek.com>
2021-10-28 17:23:03 +0800

    core/riscv-rv32i: dump panic info on console channel
    The panic information currently dumps by debug_print API which
    implemented on SCP UART. This implies that the panic information
    can only be observed on SCP UART. We cannot have any information
    about crashed SCP on non-development environment.
    
    Add a new panic API to dump information on console channel, so that
    panic information can be transmitted to kernel side via host command.
    
    BRANCH=None
    BUG=b:199444513
    BUG=b:189356151
    TEST=build pass
    
    Change-Id: I2167d16a709fa2814f3b0a208411ae5e7f51f70b
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>

--- include/config.h
+++ include/config.h
@@ -1526,6 +1526,9 @@
 /* Don't save General Purpose Registers during panic */
 #undef CONFIG_PANIC_STRIP_GPR
 
+/* Provide another output method of panic information by console channel */
+#undef CONFIG_PANIC_CONSOLE_OUTPUT
+
 /*
  * Provide the default GPIO abstraction layer.
  * You want this unless you are doing a really tiny firmware.
