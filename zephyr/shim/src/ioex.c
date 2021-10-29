5d60fb7a74ce103b1fda8a9f20085e4a2129c7c5
Michał Barnaś <mb@semihalf.com>
2021-10-29 17:27:32 +0200

    zephyr: add shim layer for ioex
    Add support for io expanders layer in zephyr.
    This layer allows to use zephyr gpios drivers as ioex pins.
    
    BRANCH=main
    BUG=b:202701452
    TEST=enable ioex and shell commands
         define some ioex pins using zephyr drivers
         on board without ioexes, errors should be seen on console
    
    Change-Id: I18110f442bfb9419af249ade6ab2c8988f5cb4c8
    Signed-off-by: Michał Barnaś <mb@semihalf.com>

---
+++ zephyr/shim/src/ioex.c
@@ -0,0 +1,355 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <device.h>
+#include <devicetree.h>
+#include <init.h>
+#include <kernel.h>
+#include <logging/log.h>
+#include "gpio.h"
+#include "gpio/gpio.h"
+#include "i2c.h"
+#include "ioexpander.h"
+#include "system.h"
+#include "util.h"
+
+LOG_MODULE_REGISTER(ioex_shim, LOG_LEVEL_ERR);
+
+struct ioex_gpio_config {
+	/* GPIO net name */
+	const char *name;
+	/* Set at build time for lookup */
+	const struct device *dev;
+	/* Bit number of pin within device */
+	gpio_pin_t pin;
+	/* From DTS, excludes interrupts flags */
+	gpio_flags_t init_flags;
+	/* Index of IO expander chip */
+	int ioex;
+	/* Port of IO expander */
+	int port;
+};
+
+struct ioex_int_config {
+	const enum ioex_signal signal;
+	const gpio_flags_t flags;
+
+	void (*const handler)(enum gpio_signal);
+	struct gpio_callback callback;
+};
+
+/* Declare handlers */
+#ifdef EC_CROS_IOEX_INTERRUPTS
+#define IOEX_INT(arg_signal, arg_flags, arg_handler) \
+	void arg_handler(enum gpio_signal);
+EC_CROS_IOEX_INTERRUPTS
+#undef IOEX_INT
+#endif /* EC_CROS_IOEX_INTERRUPTS */
+
+#define IOEX_INT(arg_signal, arg_flags, arg_handler) \
+{ \
+	.signal = arg_signal, \
+	.flags = arg_flags, \
+	.handler = arg_handler, \
+},
+
+struct ioex_int_config ioex_int_configs[] = {
+#ifdef EC_CROS_IOEX_INTERRUPTS
+	EC_CROS_IOEX_INTERRUPTS
+#endif
+};
+#undef IOEX_INT
+
+#define CHIP_FROM_GPIO(id) DT_PARENT(DT_GPIO_CTLR(id, gpios))
+
+#define IOEX_GPIO_CONFIG(id)                                                \
+	{                                                                   \
+		.name = DT_LABEL(id),                                       \
+		.dev = DEVICE_DT_GET(DT_PHANDLE(id, gpios)),                \
+		.pin = DT_GPIO_PIN(id, gpios),                              \
+		.init_flags = DT_GPIO_FLAGS(id, gpios),                     \
+		.ioex = COND_CODE_1( DT_NODE_HAS_COMPAT(CHIP_FROM_GPIO(id), \
+				cros_ioex_chip),                            \
+			(IOEXPANDER_ID(CHIP_FROM_GPIO(id)), ),              \
+			(-1,))                                              \
+		.port = DT_REG_ADDR(DT_GPIO_CTLR(id, gpios))                \
+	},
+
+#define IOEX_INIT_FLAGS(id) 0,
+
+static const struct ioex_gpio_config ioex_configs[] = {
+#if DT_NODE_EXISTS(DT_PATH(named_ioexes))
+	DT_FOREACH_CHILD(DT_PATH(named_ioexes), IOEX_GPIO_CONFIG)
+#endif
+};
+
+static gpio_flags_t ioexes_flags[] = {
+#if DT_NODE_EXISTS(DT_PATH(named_ioexes))
+	DT_FOREACH_CHILD(DT_PATH(named_ioexes), IOEX_INIT_FLAGS)
+#endif
+};
+
+#define CTLR_FROM_IOEX(id) DT_STRING_TOKEN(DT_GPIO_CTLR(id, gpios), port_named)
+#define NAME_OF_I2C(id) NAMED_I2C(CTLR_FROM_IOEX(id))
+
+#define IOEX_I2C_PORT(id) \
+	COND_CODE_1(DT_NODE_HAS_PROP(DT_GPIO_CTLR(id, gpios), port_named), \
+	(NAME_OF_I2C(id),), \
+	(-1,))
+
+int signal_is_ioex(int signal)
+{
+	return ((signal >= IOEX_SIGNAL_START) && (signal < IOEX_SIGNAL_END));
+}
+
+static struct ioex_int_config *get_interrupt_from_signal(
+	enum ioex_signal signal)
+{
+	for (size_t i = 0; i < ARRAY_SIZE(ioex_int_configs); i++) {
+		if (ioex_int_configs[i].signal == signal)
+			return &ioex_int_configs[i];
+	}
+
+	LOG_ERR("No interrupt defined for GPIO %s",
+		ioex_configs[signal - IOEX_SIGNAL_START].name);
+
+	return NULL;
+}
+
+static const struct ioex_gpio_config *ioex_get_signal_info(
+	enum ioex_signal signal)
+{
+	const struct ioex_gpio_config *g;
+
+	ASSERT(signal_is_ioex(signal));
+
+	g = ioex_configs + signal - IOEX_SIGNAL_START;
+
+	if (g->ioex >= 0 &&
+	    !(ioex_config[g->ioex].flags & IOEX_FLAGS_INITIALIZED)) {
+		LOG_ERR("ioex %s disabled", g->name);
+		return NULL;
+	}
+
+	return g;
+}
+
+int ioex_enable_interrupt(enum ioex_signal signal)
+{
+	struct ioex_int_config *cfg = get_interrupt_from_signal(signal);
+	int offset = (signal - IOEX_SIGNAL_START);
+	int res;
+
+	if (!cfg)
+		return EC_ERROR_PARAM1;
+
+	res = gpio_pin_interrupt_configure(ioex_configs[offset].dev,
+					   ioex_configs[offset].pin,
+					   (cfg->flags | GPIO_INT_ENABLE)
+						& ~GPIO_INT_DISABLE);
+
+	return res;
+}
+
+int ioex_disable_interrupt(enum ioex_signal signal)
+{
+	struct ioex_int_config *cfg = get_interrupt_from_signal(signal);
+	int offset = (signal - IOEX_SIGNAL_START);
+	int res;
+
+	if (!cfg)
+		return EC_ERROR_PARAM1;
+
+	res = gpio_pin_interrupt_configure(ioex_configs[offset].dev,
+					   ioex_configs[offset].pin,
+					   GPIO_INT_DISABLE);
+
+	return res;
+}
+
+int ioex_get_flags(enum ioex_signal signal, int *flags)
+{
+	if (signal >= IOEX_SIGNAL_START && signal < IOEX_SIGNAL_END) {
+		*flags = convert_from_zephyr_flags(
+			ioexes_flags[signal - IOEX_SIGNAL_START]);
+		return EC_SUCCESS;
+	}
+
+	return EC_ERROR_INVAL;
+}
+
+int ioex_set_flags(enum ioex_signal signal, int flags)
+{
+	const struct ioex_gpio_config *g = ioex_get_signal_info(signal);
+
+	if (g == NULL)
+		return EC_ERROR_INVAL;
+
+	if (gpio_pin_configure(g->dev,
+			      g->pin,
+			      convert_to_zephyr_flags(flags)) < 0) {
+		return EC_ERROR_UNKNOWN;
+	}
+
+	ioexes_flags[signal - IOEX_SIGNAL_START] =
+						convert_to_zephyr_flags(flags);
+
+	return EC_SUCCESS;
+}
+
+int ioex_get_level(enum ioex_signal signal, int *val)
+{
+	const struct ioex_gpio_config *g = ioex_get_signal_info(signal);
+	gpio_port_value_t portval;
+	int res;
+
+	if (g == NULL)
+		return EC_ERROR_INVAL;
+
+	res = gpio_port_get_raw(g->dev, &portval);
+	if (res)
+		return EC_ERROR_UNKNOWN;
+
+	*val = !!(portval & (1 << g->pin));
+
+	return EC_SUCCESS;
+}
+
+int ioex_set_level(enum ioex_signal signal, int value)
+{
+	const struct ioex_gpio_config *g = ioex_get_signal_info(signal);
+	int res;
+
+	if (g == NULL)
+		return EC_ERROR_INVAL;
+
+	res = gpio_pin_set_raw(g->dev, g->pin, value);
+	if (res < 0)
+		return EC_ERROR_UNKNOWN;
+
+	return EC_SUCCESS;
+}
+
+int ioex_get_port(int ioex, int port, int *val)
+{
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
+static void ioex_isr(const struct device *port,
+		     struct gpio_callback *cb,
+		     gpio_port_pins_t pins)
+{
+	struct ioex_int_config *cfg =
+			CONTAINER_OF(cb, struct ioex_int_config, callback);
+
+	cfg->handler(cfg->signal);
+}
+
+int ioex_init(int ioex)
+{
+	const struct ioexpander_drv *drv = ioex_config[ioex].drv;
+	int rv;
+
+	if (ioex_config[ioex].flags & IOEX_FLAGS_INITIALIZED)
+		return EC_SUCCESS;
+
+	if (drv->init != NULL) {
+		rv = drv->init(ioex);
+		if (rv != EC_SUCCESS)
+			return rv;
+	}
+
+	ioex_config[ioex].flags |= IOEX_FLAGS_INITIALIZED;
+
+	return EC_SUCCESS;
+}
+
+static int ioex_init_default(const struct device *unused)
+{
+	int i;
+
+	ARG_UNUSED(unused);
+
+	for (i = 0; i < CONFIG_IO_EXPANDER_PORT_COUNT; i++) {
+		/* IO Expander has been initialized, skip re-initializing */
+		if (ioex_config[i].flags & (IOEX_FLAGS_INITIALIZED |
+					IOEX_FLAGS_DEFAULT_INIT_DISABLED))
+			continue;
+
+		ioex_init(i);
+	}
+
+	/*
+	 * Set all IO expander GPIOs to default flags according to the setting
+	 * in device tree
+	 */
+	for (i = 0; i < IOEX_COUNT; i++) {
+		const struct ioex_gpio_config *g =
+				ioex_get_signal_info(IOEX_SIGNAL_START + i);
+		const struct ioexpander_drv *drv;
+		int flags;
+
+		if (!g || g->ioex < 0)
+			continue;
+
+		flags = convert_from_zephyr_flags(g->init_flags);
+		drv = ioex_config[g->ioex].drv;
+
+		if (!(flags & GPIO_DEFAULT)) {
+			/* Late-sysJump should not set the output levels */
+			if (system_jumped_late())
+				flags &= ~(GPIO_LOW | GPIO_HIGH);
+
+			drv->set_flags_by_mask(g->ioex, g->port,
+					(1 << g->pin), flags);
+		}
+
+		ioexes_flags[i] = g->init_flags;
+	}
+
+	/* Init interrupts */
+	for (i = 0; i < ARRAY_SIZE(ioex_int_configs); i++) {
+		int offset = ioex_int_configs[i].signal - IOEX_SIGNAL_START;
+
+		gpio_init_callback(&ioex_int_configs[i].callback,
+				   ioex_isr,
+				   (1 << ioex_configs[offset].pin));
+		gpio_add_callback(ioex_configs[offset].dev,
+				  &ioex_int_configs[i].callback);
+
+		ioex_enable_interrupt(ioex_int_configs[i].signal);
+	}
+
+	return 0;
+}
+SYS_INIT(ioex_init_default, POST_KERNEL, CONFIG_PLATFORM_EC_IOEX_INIT_PRIORITY);
+
+const char *ioex_get_name(enum ioex_signal signal)
+{
+	const struct ioex_gpio_config *g = ioex_get_signal_info(signal);
+
+	if (g == NULL)
+		return NULL;
+
+	return g->name;
+}
+
+int ioex_get_ioex_flags(enum ioex_signal signal, int *val)
+{
+	const struct ioex_gpio_config *g = ioex_get_signal_info(signal);
+
+	if (g == NULL)
+		return EC_ERROR_INVAL;
+
+	if (g->ioex < 0) {
+		/* Zephyr gpio drivers are initialized by internal subsystem */
+		*val = IOEX_FLAGS_INITIALIZED;
+		return EC_SUCCESS;
+	}
+
+	*val = ioex_config[g->ioex].flags;
+
+	return EC_SUCCESS;
+}
