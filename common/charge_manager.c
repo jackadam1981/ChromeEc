20eedde95799690db2bb6ca91ee23d1cdcae0487
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2021-10-29 14:46:14 +0800

    sm5803: Enable DPM when ACOK
    For sm5803, DPM should be enabled and kept on when ACOK is asserted.
    
    BUG=b:196928096
    BRANCH=none
    TEST=make sure that charging C1 port by connecting C2C cable to another
    DUT works.
    
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Change-Id: I98c8ca3a189ed1794c06a789a5e685b5d8a8aa12

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -852,8 +852,11 @@ static void charge_manager_refresh(void)
 				registration_time[new_port],
 				new_charge_voltage);
 #else
-#ifdef CONFIG_CHARGE_RAMP_HW
-		/* Enable or disable charge ramp */
+#if defined(CONFIG_CHARGE_RAMP_HW) && !defined(CONFIG_CHARGER_SM5803)
+		/*
+		 * Enable or disable charge ramp.
+		 * For sm5803, enable or disable charge ramp in sm5803.c.
+		 */
 		charger_set_hw_ramp(chg_ramp_allowed(new_port, new_supplier));
 #endif
 		board_set_charge_limit(new_port, new_supplier,
