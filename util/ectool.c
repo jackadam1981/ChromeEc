4fa11ad7e2f38b539fd37b05d03cffd9bb287ad6
B R, Harsha <harsha.b.r@intel.com>
2021-10-29 08:02:18 +0530

    ectool: Change "ectool temps all" output similar to EC console command
    Retrieve sensor name and thermal fan percentage. To the print
    statement add additional parameters - sensor_name, temperature
    converted from kelvin to Celsius and thermal_fan_percent in
    addition to temperature reading in Kelvin.
    
    BRANCH=none
    TEST=make buildall -j, zmake testall
    
    Signed-off-by: B R, Harsha <harsha.b.r@intel.com>
    Change-Id: Id86652d426fdbf30a1b27db8c2d2e7ce1b9a1d4c

--- util/ectool.c
+++ util/ectool.c
@@ -3018,10 +3018,49 @@ int read_mapped_temperature(int id)
 	return rv;
 }
 
+static int get_thermal_fan_percent(int temp)
+{
+	struct ec_params_thermal_get_threshold_v1 p;
+	struct ec_thermal_config r;
+	int rv = 0;
+
+	if (ec_cmd_version_supported(EC_CMD_THERMAL_GET_THRESHOLD, 1))
+		rv = ec_command(EC_CMD_THERMAL_GET_THRESHOLD, 1, &p,
+				sizeof(p), &r, sizeof(r));
+
+	if (rv <= 0 || r.temp_fan_max == r.temp_fan_off)
+		return -1;
+	if (temp < r.temp_fan_off)
+		return 0;
+	if (temp > r.temp_fan_max)
+		return 100;
+	return 100 * (temp - r.temp_fan_off) /
+		     (r.temp_fan_max - r.temp_fan_off);
+}
+
+static int cmd_temperature_print(int id, int mtemp)
+{
+	struct ec_response_temp_sensor_get_info r;
+	struct ec_params_temp_sensor_get_info p;
+	int rc;
+
+	p.id = id;
+	rc = ec_command(EC_CMD_TEMP_SENSOR_GET_INFO, 0, &p, sizeof(p),
+			&r, sizeof(r));
+	if (rc < 0)
+		return rc;
+
+	printf("%-20s: %d K = %d C  %d%%\n", r.sensor_name,
+	       mtemp + EC_TEMP_SENSOR_OFFSET,
+	       K_TO_C(mtemp + EC_TEMP_SENSOR_OFFSET),
+	       get_thermal_fan_percent(mtemp + EC_TEMP_SENSOR_OFFSET));
+
+	return 0;
+}
 
 int cmd_temperature(int argc, char *argv[])
 {
-	int rv;
+	int mtemp;
 	int id;
 	char *e;
 
@@ -3034,8 +3073,8 @@ int cmd_temperature(int argc, char *argv[])
 		for (id = 0;
 		     id < EC_TEMP_SENSOR_ENTRIES + EC_TEMP_SENSOR_B_ENTRIES;
 		     id++) {
-			rv = read_mapped_temperature(id);
-			switch (rv) {
+			mtemp = read_mapped_temperature(id);
+			switch (mtemp) {
 			case EC_TEMP_SENSOR_NOT_PRESENT:
 				break;
 			case EC_TEMP_SENSOR_ERROR:
@@ -3049,8 +3088,7 @@ int cmd_temperature(int argc, char *argv[])
 					id);
 				break;
 			default:
-				printf("%d: %d K\n", id,
-				       rv + EC_TEMP_SENSOR_OFFSET);
+				cmd_temperature_print(id, mtemp);
 			}
 		}
 		return 0;
@@ -3069,9 +3107,9 @@ int cmd_temperature(int argc, char *argv[])
 	}
 
 	printf("Reading temperature...");
-	rv = read_mapped_temperature(id);
+	mtemp = read_mapped_temperature(id);
 
-	switch (rv) {
+	switch (mtemp) {
 	case EC_TEMP_SENSOR_NOT_PRESENT:
 		printf("Sensor not present\n");
 		return -1;
@@ -3085,8 +3123,7 @@ int cmd_temperature(int argc, char *argv[])
 		fprintf(stderr, "Sensor not calibrated\n");
 		return -1;
 	default:
-		printf("%d K\n", rv + EC_TEMP_SENSOR_OFFSET);
-		return 0;
+		return cmd_temperature_print(id, mtemp);
 	}
 }
 
