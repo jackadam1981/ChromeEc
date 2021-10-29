b32118874a956e50e68c90229c625460555117f5
Will Tsai <will_tsai@wistron.corp-partner.google.com>
2021-10-29 17:42:04 +0800

    gimble: tune PS8815 parameter for RX
    The parameters were tuned depending on type-c RX test
    
    BRANCH=none
    BUG=b:204719059
    TEST=make -j BOARD=gimble
    
    Signed-off-by: Will Tsai <will_tsai@wistron.corp-partner.google.com>
    Change-Id: Iddda0a56317bee39ad7038dbab9b67dc0ecea7dc

--- board/gimble/board.c
+++ board/gimble/board.c
@@ -25,6 +25,7 @@
 #include "lid_switch.h"
 #include "power_button.h"
 #include "power.h"
+#include "ps8xxx.h"
 #include "registers.h"
 #include "switch.h"
 #include "tablet_mode.h"
@@ -135,6 +136,47 @@ static void board_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_SECOND, board_init, HOOK_PRIO_DEFAULT);
+
+__overridable void board_ps8xxx_tcpc_init(int port)
+{
+	int val;
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_APTX_EQ_AT_10G, &val))
+		CPRINTS("ps8815: fail to read reg 0x20");
+
+	/* APTX2 EQ 23dB, APTX1 EQ 23dB */
+	if (i2c_write8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_APTX_EQ_AT_10G, 0x99))
+		CPRINTS("ps8815: fail to write reg 0x20");
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_RX_EQ_AT_10G, &val))
+		CPRINTS("ps8815: fail to read reg 0x22");
+
+	/* RX2 EQ 18dB, RX1 EQ 16dB */
+	if (i2c_write8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_RX_EQ_AT_10G, 0x64))
+		CPRINTS("ps8815: fail to write reg 0x22");
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_APTX_EQ_AT_5G, &val))
+		CPRINTS("ps8815: fail to read reg 0x24");
+
+	/* APTX2 EQ 16dB, APTX1 EQ 16dB */
+	if (i2c_write8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_APTX_EQ_AT_5G, 0x44))
+		CPRINTS("ps8815: fail to write reg 0x24");
+
+	if (i2c_read8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_RX_EQ_AT_5G, &val))
+		CPRINTS("ps8815: fail to read reg 0x26");
+
+	/* RX2 EQ 16dB, RX1 EQ 16dB */
+	if (i2c_write8(I2C_PORT_USB_C1_TCPC,
+		PS8751_I2C_ADDR1_P1_FLAGS, PS8815_REG_RX_EQ_AT_5G, 0x44))
+		CPRINTS("ps8815: fail to write reg 0x26");
+}
 
 __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 			    int max_ma, int charge_mv)
