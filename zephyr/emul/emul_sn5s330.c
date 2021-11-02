4732d284256e9d35cc853cc6ec9e602ed8dce005
Aaron Massey <aaronmassey@google.com>
2021-11-02 15:53:25 -0600

    zephyr: emul: sn5s330 add int trip fall registers
    Emulate int trip fall registers used by driver during initialization.
    
    BRANCH=none
    BUG=b:203364783
    TEST=zmake configure --test zephyr/test/drivers (check coverage change)
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I67f51c457f243c3255081c736459a940e440a033

--- zephyr/emul/emul_sn5s330.c
+++ zephyr/emul/emul_sn5s330.c
@@ -78,6 +78,12 @@ struct sn5s330_emul_data {
 	uint8_t int_trip_rise_reg2;
 	/** Emulated INT_TRIP_RISE_REG3 register */
 	uint8_t int_trip_rise_reg3;
+	/** Emulated INT_TRIP_FALL_REG1 register */
+	uint8_t int_trip_fall_reg1;
+	/** Emulated INT_TRIP_FALL_REG2 register */
+	uint8_t int_trip_fall_reg2;
+	/** Emulated INT_TRIP_FALL_REG3 register */
+	uint8_t int_trip_fall_reg3;
 };
 
 struct sn5s330_emul_cfg {
@@ -212,6 +218,18 @@ static int sn5s330_emul_read_byte(struct i2c_emul *emul, int reg, uint8_t *val,
 		__ASSERT_NO_MSG(bytes == 0);
 		*val = data->int_trip_rise_reg3;
 		break;
+	case SN5S330_INT_TRIP_FALL_REG1:
+		__ASSERT_NO_MSG(bytes == 0);
+		*val = data->int_trip_fall_reg1;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG2:
+		__ASSERT_NO_MSG(bytes == 0);
+		*val = data->int_trip_fall_reg2;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG3:
+		__ASSERT_NO_MSG(bytes == 0);
+		*val = data->int_trip_fall_reg3;
+		break;
 	default:
 		return -EINVAL;
 	}
@@ -321,6 +339,18 @@ static int sn5s330_emul_write_byte(struct i2c_emul *emul, int reg, uint8_t val,
 	case SN5S330_INT_TRIP_RISE_REG3:
 		__ASSERT_NO_MSG(bytes == 1);
 		data->int_trip_rise_reg3 = val;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG1:
+		__ASSERT_NO_MSG(bytes == 1);
+		data->int_trip_fall_reg1 = val;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG2:
+		__ASSERT_NO_MSG(bytes == 1);
+		data->int_trip_fall_reg2 = val;
+		break;
+	case SN5S330_INT_TRIP_FALL_REG3:
+		__ASSERT_NO_MSG(bytes == 1);
+		data->int_trip_fall_reg3 = val;
 		break;
 	default:
 		return -EINVAL;
