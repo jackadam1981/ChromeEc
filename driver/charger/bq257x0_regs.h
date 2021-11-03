17f833a661c33d7fa83d06cf3126c0dac34d6eb1
Caveh Jalali <caveh@chromium.org>
2021-11-02 20:22:26 -0700

    bq25710: Allow CMP_REF to be configured
    Change-Id: Ie296c5f7a0fac62aca826cb951cdc932f9ca14c7
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- driver/charger/bq257x0_regs.h
+++ driver/charger/bq257x0_regs.h
@@ -10,22 +10,28 @@
 
 #include "bq25710.h"
 
+/* ChargeOption1 Register (0x30) */
+#define BQ257X0_CHARGE_OPTION_1_CMP_REF_SHIFT	7
+#define BQ257X0_CHARGE_OPTION_1_CMP_REF_BITS	1
+#define BQ257X0_CHARGE_OPTION_1_CMP_REF__2P3	0
+#define BQ257X0_CHARGE_OPTION_1_CMP_REF__1P2	1
 
 /* ChargeCurrent Register */
 #define BQ257X0_CHARGE_CURRENT_CHARGE_CURRENT_SHIFT	6
 #define BQ257X0_CHARGE_CURRENT_CHARGE_CURRENT_BITS	7
 
-#define BQ257X0_CHARGE_CURRENT_MASK	BQ257X0_MASK(BQ257X0, \
+#define BQ257X0_CHARGE_CURRENT_MASK	BQ257X0_MASK_(BQ257X0, \
 						CHARGE_CURRENT, \
 						CHARGE_CURRENT)
 
-#define BQ257X0_MASK(_chip, _reg, _field)				\
+#define BQ257X0_MASK_(_chip, _reg, _field)				\
 	GENMASK(							\
 		(_chip##_##_reg##_##_field##_SHIFT +			\
 		 _chip##_##_reg##_##_field##_BITS - 1),			\
 		_chip##_##_reg##_##_field##_SHIFT)
 
-#define BQ25720_MASK(_reg, _field)	BQ257X0_MASK(BQ25720, _reg, _field)
+#define BQ257X0_MASK(_reg, _field)	BQ257X0_MASK_(BQ257X0, _reg, _field)
+#define BQ25720_MASK(_reg, _field)	BQ257X0_MASK_(BQ25720, _reg, _field)
 
 #define GET_BQ25720(_reg, _field, _x)				\
 	(((_x) >> BQ25720_##_reg##_##_field##_SHIFT) &		\
@@ -37,12 +43,20 @@
 	   GENMASK(BQ25720_##_reg##_##_field##_BITS - 1, 0)) <<		\
 	  BQ25720_##_reg##_##_field##_SHIFT))
 
+#define SET_BQ257X0_BY_NAME(_reg, _field, _e, _x)			\
+	(((_x) & ~BQ257X0_MASK(_reg, _field)) |				\
+	 ((BQ257X0_##_reg##_##_field##__##_e &				\
+	   GENMASK(BQ257X0_##_reg##_##_field##_BITS - 1, 0)) <<		\
+	  BQ257X0_##_reg##_##_field##_SHIFT))
+
 #define SET_BQ25720_BY_NAME(_reg, _field, _e, _x)			\
 	(((_x) & ~BQ25720_MASK(_reg, _field)) |				\
 	 ((BQ25720_##_reg##_##_field##__##_e &				\
 	   GENMASK(BQ25720_##_reg##_##_field##_BITS - 1, 0)) <<		\
 	  BQ25720_##_reg##_##_field##_SHIFT))
 
+#define SET_CO1_BY_NAME(_f, _e, _x)	SET_BQ257X0_BY_NAME(CHARGE_OPTION_1, \
+							    _f, _e, (_x))
 
 #define SET_CO4(_f, _v, _x)		SET_BQ25720(CHARGE_OPTION_4, _f, _v, (_x))
 #define SET_CO4_BY_NAME(_f, _e, _x)	SET_BQ25720_BY_NAME(CHARGE_OPTION_4, _f, _e, (_x))
