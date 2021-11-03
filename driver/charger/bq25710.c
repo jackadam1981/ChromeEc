a0d2e1c293ca16a119a12804028bb2ed569ad6e8
Caveh Jalali <caveh@chromium.org>
2021-11-02 20:58:14 -0700

    bq25710: enable ACOC
    Change-Id: If7295b75d7b163d31139aee12d3f8d1fd2086a5e
    Signed-off-by: Caveh Jalali <caveh@chromium.org>

--- driver/charger/bq25710.c
+++ driver/charger/bq25710.c
@@ -378,13 +378,17 @@ static void bq25710_init(int chgnum)
 		raw_write16(chgnum, BQ25710_REG_PROCHOT_OPTION_0, reg);
 	}
 
-	/*
-	 * Reduce peak power mode overload and relax cycle time from default 20
-	 * msec to the minimum of 5 msec.
-	 */
 	if (!raw_read16(chgnum, BQ25710_REG_CHARGE_OPTION_2, &reg)) {
-		//reg &= ~BQ25710_CHARGE_OPTION_2_TMAX_MASK;
+		/*
+		 * Reduce peak power mode overload and relax cycle time
+		 * from default 20 msec to the minimum of 5 msec on the
+		 * bq25710 or 20 msec on the bq25720.
+		 */
 		reg &= ~BQ_FIELD_MASK(BQ257X0, CHARGE_OPTION_2, PKPWR_TMAX);
+		/*
+		 * Enable AC input over-current protection.
+		 */
+		reg = SET_CO2_BY_NAME(EN_ACOC, ENABLE, reg);
 		raw_write16(chgnum, BQ25710_REG_CHARGE_OPTION_2, reg);
 	}
 }
