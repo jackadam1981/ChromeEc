0c4397eaa33d361d4decea98edd3d069c28875f3
Caveh Jalali <caveh@chromium.org>
2021-11-04 00:44:24 -0700

    bq25710: [CO3] Set IL_AVG to 10A
    Change-Id: I40ec0b39f83ad4693133561968df891639310ad4

--- driver/charger/bq25710.c
+++ driver/charger/bq25710.c
@@ -391,6 +391,16 @@ static void bq25710_init(int chgnum)
 	}
 
 	bq257x0_init_charge_option_2(chgnum);
+
+	if (IS_ENABLED(CONFIG_CHARGER_BQ25720) &&
+	    !raw_read16(chgnum, BQ25710_REG_CHARGE_OPTION_3, &reg)) {
+		/*
+		 * The bq25720 defaults to 15 A while the bq25710
+		 * defaults to 10A. Set the bq25720 to 10A as well.
+		 */
+		reg = SET_CO3(IL_AVG, BQ25720_CHARGE_OPTION_3_IL_AVG__10A, reg);
+		raw_write16(chgnum, BQ25710_REG_CHARGE_OPTION_3, reg);
+	}
 }
 
 /* Charger interfaces */
