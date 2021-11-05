9826cbd4e8e2d5adc258140682dc973bb2231d48
Peter Marheine <pmarheine@chromium.org>
2021-11-05 16:30:29 +1100

    zephyr: support shared PD interrupts
    Some boards share an interrupt line between multiple PD ports by running
    pd_shared_alert_task in the PD_INT_C0 task. Add support for that by adding
    one config option to enable the shared alert task rather than the regular
    pd_interrupt_handler_task, and one option per-port to handle a port's
    interrupts in the shared handler.
    
    BUG=b:204860054
    TEST=builds
    BRANCH=none
    
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Change-Id: Ice67a6a74b0a2f42982de6e19aa1a478c0a4e068

--- zephyr/shim/include/shimmed_task_id.h
+++ zephyr/shim/include/shimmed_task_id.h
@@ -18,67 +18,103 @@ typedef uint8_t task_id_t;
  * CONFIG_SHIMMED_TASKS.
  */
 #ifdef CONFIG_SHIMMED_TASKS
-#define CROS_EC_TASK_LIST                                                 \
-	COND_CODE_1(HAS_TASK_HOOKS,                                       \
-		     (CROS_EC_TASK(HOOKS, hook_task, 0,                   \
-				   CONFIG_TASK_HOOKS_STACK_SIZE)), ())    \
-	COND_CODE_1(HAS_TASK_CHG_RAMP,                                    \
-		     (CROS_EC_TASK(CHG_RAMP, chg_ramp_task, 0,            \
-				   CONFIG_TASK_CHG_RAMP_STACK_SIZE)), ()) \
-	COND_CODE_1(HAS_TASK_USB_CHG_P0,                                  \
-		     (CROS_EC_TASK(USB_CHG_P0, usb_charger_task, 0,       \
-				   CONFIG_TASK_USB_CHG_STACK_SIZE)), ())  \
-	COND_CODE_1(HAS_TASK_USB_CHG_P1,                                  \
-		     (CROS_EC_TASK(USB_CHG_P1, usb_charger_task, 0,       \
-				   CONFIG_TASK_USB_CHG_STACK_SIZE)), ())  \
-	COND_CODE_1(HAS_TASK_USB_CHG_P2,                                  \
-		     (CROS_EC_TASK(USB_CHG_P2, usb_charger_task, 0,       \
-				   CONFIG_TASK_USB_CHG_STACK_SIZE)), ())  \
-	COND_CODE_1(HAS_TASK_CHARGER,                                     \
-		     (CROS_EC_TASK(CHARGER, charger_task, 0,              \
-				   CONFIG_TASK_CHARGER_STACK_SIZE)), ())  \
-	COND_CODE_1(HAS_TASK_CHIPSET,                                     \
-		     (CROS_EC_TASK(CHIPSET, chipset_task, 0,              \
-				   CONFIG_TASK_CHIPSET_STACK_SIZE)), ())  \
-	COND_CODE_1(HAS_TASK_MOTIONSENSE,                                     \
-		     (CROS_EC_TASK(MOTIONSENSE, motion_sense_task, 0,         \
-				   CONFIG_TASK_MOTIONSENSE_STACK_SIZE)), ())  \
-	COND_CODE_1(HAS_TASK_HOSTCMD,                                     \
-		     (CROS_EC_TASK(HOSTCMD, host_command_task, 0,         \
-				   CONFIG_TASK_HOSTCMD_STACK_SIZE)), ())  \
-	COND_CODE_1(HAS_TASK_KEYPROTO,                                    \
-		     (CROS_EC_TASK(KEYPROTO, keyboard_protocol_task, 0,   \
-				   CONFIG_TASK_KEYPROTO_STACK_SIZE)), ()) \
-	COND_CODE_1(HAS_TASK_POWERBTN,                                    \
-		     (CROS_EC_TASK(POWERBTN, power_button_task, 0,        \
-				   CONFIG_TASK_POWERBTN_STACK_SIZE)), ()) \
-	COND_CODE_1(HAS_TASK_KEYSCAN,                                     \
-		     (CROS_EC_TASK(KEYSCAN, keyboard_scan_task, 0,        \
-				   CONFIG_TASK_KEYSCAN_STACK_SIZE)), ())  \
-	COND_CODE_1(HAS_TASK_PD_C0,                                       \
-		     (CROS_EC_TASK(PD_C0, pd_task, 0,                     \
-				   CONFIG_TASK_PD_STACK_SIZE)), ())       \
-	COND_CODE_1(HAS_TASK_PD_C1,                                       \
-		     (CROS_EC_TASK(PD_C1, pd_task, 0,                     \
-				   CONFIG_TASK_PD_STACK_SIZE)), ())       \
-	COND_CODE_1(HAS_TASK_PD_C2,                                       \
-		     (CROS_EC_TASK(PD_C2, pd_task, 0,                     \
-				   CONFIG_TASK_PD_STACK_SIZE)), ())       \
-	COND_CODE_1(HAS_TASK_PD_C3,                                       \
-		     (CROS_EC_TASK(PD_C3, pd_task, 0,                     \
-				   CONFIG_TASK_PD_STACK_SIZE)), ())       \
-	COND_CODE_1(HAS_TASK_PD_INT_C0,                                   \
-		     (CROS_EC_TASK(PD_INT_C0, pd_interrupt_handler_task, 0, \
-				   CONFIG_TASK_PD_INT_STACK_SIZE)), ())   \
-	COND_CODE_1(HAS_TASK_PD_INT_C1,                                   \
-		     (CROS_EC_TASK(PD_INT_C1, pd_interrupt_handler_task, 1, \
-				   CONFIG_TASK_PD_INT_STACK_SIZE)), ())   \
-	COND_CODE_1(HAS_TASK_PD_INT_C2,                                   \
-		     (CROS_EC_TASK(PD_INT_C2, pd_interrupt_handler_task, 2, \
-				   CONFIG_TASK_PD_INT_STACK_SIZE)), ())   \
-	COND_CODE_1(HAS_TASK_PD_INT_C3,                                   \
-		     (CROS_EC_TASK(PD_INT_C3, pd_interrupt_handler_task, 3, \
-				   CONFIG_TASK_PD_INT_STACK_SIZE)), ())
+#define CROS_EC_TASK_LIST                                                       \
+	COND_CODE_1(HAS_TASK_HOOKS,                                             \
+		    (CROS_EC_TASK(HOOKS, hook_task, 0,                          \
+				  CONFIG_TASK_HOOKS_STACK_SIZE)),               \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_CHG_RAMP,                                          \
+		    (CROS_EC_TASK(CHG_RAMP, chg_ramp_task, 0,                   \
+				  CONFIG_TASK_CHG_RAMP_STACK_SIZE)),            \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_USB_CHG_P0,                                        \
+		    (CROS_EC_TASK(USB_CHG_P0, usb_charger_task, 0,              \
+				  CONFIG_TASK_USB_CHG_STACK_SIZE)),             \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_USB_CHG_P1,                                        \
+		    (CROS_EC_TASK(USB_CHG_P1, usb_charger_task, 0,              \
+				  CONFIG_TASK_USB_CHG_STACK_SIZE)),             \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_USB_CHG_P2,                                        \
+		    (CROS_EC_TASK(USB_CHG_P2, usb_charger_task, 0,              \
+				  CONFIG_TASK_USB_CHG_STACK_SIZE)),             \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_CHARGER,                                           \
+		    (CROS_EC_TASK(CHARGER, charger_task, 0,                     \
+				  CONFIG_TASK_CHARGER_STACK_SIZE)),             \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_CHIPSET,                                           \
+		    (CROS_EC_TASK(CHIPSET, chipset_task, 0,                     \
+				  CONFIG_TASK_CHIPSET_STACK_SIZE)),             \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_MOTIONSENSE,                                       \
+		    (CROS_EC_TASK(MOTIONSENSE, motion_sense_task, 0,            \
+				  CONFIG_TASK_MOTIONSENSE_STACK_SIZE)),         \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_HOSTCMD,                                           \
+		    (CROS_EC_TASK(HOSTCMD, host_command_task, 0,                \
+				  CONFIG_TASK_HOSTCMD_STACK_SIZE)),             \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_KEYPROTO,                                          \
+		    (CROS_EC_TASK(KEYPROTO, keyboard_protocol_task, 0,          \
+				  CONFIG_TASK_KEYPROTO_STACK_SIZE)),            \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_POWERBTN,                                          \
+		    (CROS_EC_TASK(POWERBTN, power_button_task, 0,               \
+				  CONFIG_TASK_POWERBTN_STACK_SIZE)),            \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_KEYSCAN,                                           \
+		    (CROS_EC_TASK(KEYSCAN, keyboard_scan_task, 0,               \
+				  CONFIG_TASK_KEYSCAN_STACK_SIZE)),             \
+		    ())                                                         \
+	COND_CODE_1(                                                            \
+		HAS_TASK_PD_C0,                                                 \
+		(CROS_EC_TASK(PD_C0, pd_task, 0, CONFIG_TASK_PD_STACK_SIZE)),   \
+		())                                                             \
+	COND_CODE_1(                                                            \
+		HAS_TASK_PD_C1,                                                 \
+		(CROS_EC_TASK(PD_C1, pd_task, 0, CONFIG_TASK_PD_STACK_SIZE)),   \
+		())                                                             \
+	COND_CODE_1(                                                            \
+		HAS_TASK_PD_C2,                                                 \
+		(CROS_EC_TASK(PD_C2, pd_task, 0, CONFIG_TASK_PD_STACK_SIZE)),   \
+		())                                                             \
+	COND_CODE_1(                                                            \
+		HAS_TASK_PD_C3,                                                 \
+		(CROS_EC_TASK(PD_C3, pd_task, 0, CONFIG_TASK_PD_STACK_SIZE)),   \
+		())                                                             \
+	IF_ENABLED(                                                             \
+		HAS_TASK_PD_INT_C0,                                             \
+		(COND_CODE_1(                                                   \
+			CONFIG_TASK_PD_INT_C0_SHARED, /* shared alert task      \
+						       * always handles port    \
+						       * 0, and may also        \
+						       * handle any other       \
+						       * ports */               \
+			(CROS_EC_TASK(                                          \
+				PD_INT_C0, pd_shared_alert_task,                \
+				(BIT(0) IF_ENABLED(TASK_PD_INT_C1_SHARED,       \
+						   (| BIT(1)))                  \
+					 IF_ENABLED(TASK_PD_INT_C2_SHARED,      \
+						    (| BIT(2)))                 \
+						 IF_ENABLED(                    \
+							 TASK_PD_INT_C3_SHARED, \
+							 (| BIT(3)))),          \
+				CONFIG_TASK_PD_INT_STACK_SIZE)),                \
+			(CROS_EC_TASK(PD_INT_C0, pd_interrupt_handler_task, 0,  \
+				      CONFIG_TASK_PD_INT_STACK_SIZE)))))        \
+	COND_CODE_1(HAS_TASK_PD_INT_C1,                                         \
+		    (CROS_EC_TASK(PD_INT_C1, pd_interrupt_handler_task, 1,      \
+				  CONFIG_TASK_PD_INT_STACK_SIZE)),              \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_PD_INT_C2,                                         \
+		    (CROS_EC_TASK(PD_INT_C2, pd_interrupt_handler_task, 2,      \
+				  CONFIG_TASK_PD_INT_STACK_SIZE)),              \
+		    ())                                                         \
+	COND_CODE_1(HAS_TASK_PD_INT_C3,                                         \
+		    (CROS_EC_TASK(PD_INT_C3, pd_interrupt_handler_task, 3,      \
+				  CONFIG_TASK_PD_INT_STACK_SIZE)),              \
+		    ())
 #elif defined(CONFIG_HAS_TEST_TASKS)
 #include "shimmed_test_tasks.h"
 /*
