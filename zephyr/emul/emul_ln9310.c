1b66adbcedac47b7d540379e053e24a3974dd4a6
Aaron Massey <aaronmassey@google.com>
2021-11-09 15:56:08 -0700

    zephyr: emul: ln9310 i2c errors to asserts
    Returning -EINVAL on i2c is inaccurate, do assertion fail instead.
    
    BRANCH=none
    BUG=b:205579152
    TEST=zmake configure --test test-drivers
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: If795fdb2d2c75ef3823f3d6ab45514c7dfbee260

--- zephyr/emul/emul_ln9310.c
+++ zephyr/emul/emul_ln9310.c
@@ -24,6 +24,10 @@ LOG_MODULE_REGISTER(ln9310_emul, CONFIG_LN9310_EMUL_LOG_LEVEL);
 	CONTAINER_OF(CONTAINER_OF(_emul, struct i2c_common_emul_data, emul), \
 		     struct ln9310_emul_data, common)
 
+#ifndef CONFIG_ASSERT
+#error CONFIG_ASSERT must be enabled.
+#endif /* CONFIG_ASSRT */
+
 enum functional_mode {
 	/* TODO shutdown_mode, */
 	/* TODO bypass, */
@@ -279,8 +283,11 @@ static int ln9310_emul_write_byte(struct i2c_emul *emul, int reg, uint8_t val,
 		data->bc_sts_b_reg = val;
 		break;
 	case LN9310_REG_BC_STS_C:
-		LOG_ERR("Can't write to BC STS C register");
-		return -EINVAL;
+		__ASSERT(false,
+			 "Write to an unverified as safe "
+			 "read-only register on 0x%x",
+			 reg);
+		break;
 	case LN9310_REG_CFG_0:
 		__ASSERT_NO_MSG(bytes == 1);
 		data->cfg_0_reg = val;
@@ -350,7 +357,8 @@ static int ln9310_emul_write_byte(struct i2c_emul *emul, int reg, uint8_t val,
 		data->test_mode_ctrl_reg = val;
 		break;
 	default:
-		return -EINVAL;
+		__ASSERT(false, "Unimplemented Register Access Error on 0x%x",
+			 reg);
 	}
 	mode_change(data);
 	return 0;
