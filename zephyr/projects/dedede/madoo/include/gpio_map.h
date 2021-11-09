52b03eca07d4bfd9cbb894862587c5cfa3c85038
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2021-11-09 14:21:37 -0800

    [WIP]: Zephyr: madoo: Enable power sequence
    BUG=b:203184102
    BRANCH=None
    TEST=None
    
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Change-Id: Id89b7b811ad80fa4e80e03ae6cb53a0651b1e448

--- zephyr/projects/dedede/madoo/include/gpio_map.h
+++ zephyr/projects/dedede/madoo/include/gpio_map.h
@@ -9,9 +9,8 @@
 #include <devicetree.h>
 #include <gpio_signal.h>
 
-#define GPIO_ENTERING_RW	GPIO_UNIMPLEMENTED
-#define GPIO_WP_L		GPIO_UNIMPLEMENTED
-
+#define POWER_SIGNAL_INT(gpio, edge)                                      \
+	GPIO_INT(gpio, edge, power_signal_interrupt)
 /*
  * Set EC_CROS_GPIO_INTERRUPTS to a space-separated list of GPIO_INT items.
  *
@@ -27,10 +26,22 @@
  * #define EC_CROS_GPIO_INTERRUPTS \
  *   GPIO_INT(NAMED_GPIO(h1_ec_pwr_btn_odl), GPIO_INT_EDGE_BOTH, button_print)
  */
-#define EC_CROS_GPIO_INTERRUPTS                                           \
-	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)        \
-	GPIO_INT(GPIO_POWER_BUTTON_L, GPIO_INT_EDGE_BOTH,                 \
-		 power_button_interrupt)                                  \
-	GPIO_INT(GPIO_WP_L, GPIO_INT_EDGE_BOTH, switch_interrupt)
 
+#define EC_CROS_GPIO_INTERRUPTS                                           \
+	POWER_SIGNAL_INT(GPIO_PCH_SLP_S0_L, GPIO_INT_EDGE_BOTH)           \
+	GPIO_INT(GPIO_PCH_SLP_S3_L, GPIO_INT_EDGE_BOTH,                   \
+		baseboard_all_sys_pgood_interrupt)                        \
+	POWER_SIGNAL_INT(GPIO_PCH_SLP_S4_L, GPIO_INT_EDGE_BOTH)           \
+	POWER_SIGNAL_INT(GPIO_SLP_SUS_L, GPIO_INT_EDGE_BOTH)              \
+	POWER_SIGNAL_INT(GPIO_PG_EC_RSMRST_ODL, GPIO_INT_EDGE_BOTH)       \
+	GPIO_INT(GPIO_PG_VCCIO_EXT_OD, GPIO_INT_EDGE_BOTH,                \
+		baseboard_all_sys_pgood_interrupt)                        \
+	GPIO_INT(GPIO_LID_OPEN, GPIO_INT_EDGE_BOTH, lid_interrupt)        \
+	POWER_SIGNAL_INT(GPIO_PP5000_A_PG_OD, GPIO_INT_EDGE_BOTH)         \
+	GPIO_INT(GPIO_PG_DRAM_OD, GPIO_INT_EDGE_BOTH,                     \
+		baseboard_all_sys_pgood_interrupt)                        \
+	POWER_SIGNAL_INT(GPIO_PG_PP1050_ST_S_OD, GPIO_INT_EDGE_BOTH)      \
+	GPIO_INT(GPIO_POWER_BUTTON_L, GPIO_INT_EDGE_BOTH,                 \
+		power_button_interrupt)                                   \
+	GPIO_INT(GPIO_WP_L, GPIO_INT_EDGE_BOTH, switch_interrupt)
 #endif /* __ZEPHYR_GPIO_MAP_H */
