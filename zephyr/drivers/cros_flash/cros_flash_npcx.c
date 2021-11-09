040ee638d88da590556aac396dd2af5f1692a66d
Jun Lin <CHLin56@nuvoton.com>
2021-11-09 11:34:16 +0800

    zephyr: flash: npcx: Fix physical_get_protect_flags()
    Protecting status register by /WP works when SRP0 is enabled. The
    getting protect flags function should check SRP0 & QE for
    EC_FLASH_PROTECT_ERROR_INCONSISTENT flag.
    
    This CL adds the following:
    1. SRP0 & QE bits check for npcx flash.
    2. EC_FLASH_PROTECT_ERROR_UNKNOWN flag
    
    This change alignes with the change in the CL:3246882 of ECOS.
    
    BUG=none
    BRANCH=none
    TEST=Protect a range & don't set the SRP0. Check flag by `flashinfo`.
    
    Signed-off-by: Jun Lin <CHLin56@nuvoton.com>
    Change-Id: I1e53222c25b7d74681961c6e78fbf05a590665eb

--- zephyr/drivers/cros_flash/cros_flash_npcx.c
+++ zephyr/drivers/cros_flash/cros_flash_npcx.c
@@ -23,7 +23,6 @@ LOG_MODULE_REGISTER(cros_flash, LOG_LEVEL_ERR);
 static int all_protected; /* Has all-flash protection been requested? */
 static int addr_prot_start;
 static int addr_prot_length;
-static uint8_t flag_prot_inconsistent;
 static uint8_t saved_sr1;
 static uint8_t saved_sr2;
 
@@ -507,17 +506,27 @@ static int cros_flash_npcx_get_protect(const struct device *dev, int bank)
 static uint32_t cros_flash_npcx_get_protect_flags(const struct device *dev)
 {
 	uint32_t flags = 0;
+	int rv;
+	uint8_t sr1, sr2;
+	unsigned int start, len;
 
 	/* Check if WP region is protected in status register */
-	if (flash_check_prot_reg(dev, WP_BANK_OFFSET * CONFIG_FLASH_BANK_SIZE,
-				 WP_BANK_COUNT * CONFIG_FLASH_BANK_SIZE))
+	rv = flash_check_prot_reg(dev, WP_BANK_OFFSET * CONFIG_FLASH_BANK_SIZE,
+				 WP_BANK_COUNT * CONFIG_FLASH_BANK_SIZE);
+	if (rv == EC_ERROR_ACCESS_DENIED)
 		flags |= EC_FLASH_PROTECT_RO_AT_BOOT;
+	else if (rv)
+		return EC_FLASH_PROTECT_ERROR_UNKNOWN;
 
 	/*
-	 * TODO: If status register protects a range, but SRP0 is not set,
-	 * flags should indicate EC_FLASH_PROTECT_ERROR_INCONSISTENT.
+	 * If the status register protects a range, but SRP0 is not set, or QE
+	 * is set, flags should indicate EC_FLASH_PROTECT_ERROR_INCONSISTENT.
 	 */
-	if (flag_prot_inconsistent)
+	flash_get_status(dev, &sr1, &sr2);
+	rv = spi_flash_reg_to_protect(sr1, sr2, &start, &len);
+	if (rv)
+		return EC_FLASH_PROTECT_ERROR_UNKNOWN;
+	if (len && (!(sr1 & SPI_FLASH_SR1_SRP0) || (sr2 & SPI_FLASH_SR2_QE)))
 		flags |= EC_FLASH_PROTECT_ERROR_INCONSISTENT;
 
 	/* Read all-protected state from our shadow copy */
