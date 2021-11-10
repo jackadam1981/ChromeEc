eb399d1cca3280c3c1b0373f091133e8ffe5de80
Mary Ruthven <mruthven@chromium.org>
2021-11-09 19:51:29 -0600

    Revert "timer: Add note about `forcetime` command being unsafe"
    This reverts commit 9663bdca5cae0d2d67d6e9cf422a6b6119a24926.
    
    BUG=b:200823466
    TEST=make buildall -j
    
    Change-Id: I66650db8199ee3dd074c853d9f124eca7f29c78a
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- common/timer.c
+++ common/timer.c
@@ -275,11 +275,6 @@ DECLARE_CONSOLE_COMMAND(waitms, command_wait,
 #endif
 
 #ifdef CONFIG_CMD_FORCETIME
-/*
- * Force the hwtimer to a given time. This may have undesired consequences,
- * especially when going "backward" in time, because task deadlines are
- * left un-adjusted.
- */
 static int command_force_time(int argc, char **argv)
 {
 	char *e;
