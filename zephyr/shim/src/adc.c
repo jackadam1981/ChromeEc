082ecfbcb31974aed3ba0efebba966f54f1da189
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2021-11-09 22:55:07 -0800

    zephyr: adc: Add support to ADC threshold interruption
    These changes use ADC API's to configure and enable threshold
    interruptions.
    
    BRANCH=None
    BUG=None
    TEST=None
    
    Change-Id: Ib4f3ca621767fbebb0c78ca3b06b78e50ccbe4ee
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Depends-On: https://github.com/zephyrproject-rtos/zephyr/pull/40904

--- zephyr/shim/src/adc.c
+++ zephyr/shim/src/adc.c
@@ -5,6 +5,7 @@
 
 #include <drivers/adc.h>
 #include <logging/log.h>
+#include "system.h"
 #include "adc.h"
 #include "zephyr_adc.h"
 
@@ -78,3 +79,22 @@ int adc_read_channel(enum adc_channel ch)
 	ret = (ret * adc_channels[ch].factor_mul) / adc_channels[ch].factor_div;
 	return ret;
 }
+
+#ifdef CONFIG_ADC_THRESHOLD_IRQ
+int adc_config_threshold_interrupt(struct adc_threshold_cfg *threshold_cfg)
+{
+	return adc_config_threshold_irq(adc_dev, threshold_cfg);
+}
+
+int adc_enable_threshold_interrupt(const int threshold_id)
+{
+	disable_sleep(SLEEP_MASK_ADC);
+	return adc_enable_threshold_irq(adc_dev, threshold_id);
+}
+
+int adc_disable_threshold_interrupt(const int threshold_id)
+{
+	enable_sleep(SLEEP_MASK_ADC);
+	return adc_disable_threshold_irq(adc_dev, threshold_id);
+}
+#endif /* CONFIG_ADC_THRESHOLD_IRQ */
