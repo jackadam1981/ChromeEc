42b72c2374a8a84107a076790435bd82ac10c894
Kevin Shelton <kmshelton@chromium.org>
2021-11-10 23:10:26 -0800

    fingerprint: validate the expected hwid
    BUG=b:187134801
    BRANCH=none
    TEST=make buildall -j,
         Using dragonclaw v0.2 and servo_micro:
         ./test/run_device_tests.py -t fpsensor_hw
         --flasher=servo_micro,
         not passed yet: Using icetower and servo_micro:
         ./test/run_device_tests.py -t fpsensor_wh
         --flasher=servo_micro --board dartmonkey;
    
    Change-Id: I7a3e99eccfcd828a72b12d8570c1cf7ed4fdd7fc
    Signed-off-by: Kevin Shelton <kmshelton@chromium.org>

--- test/fpsensor_hw.c
+++ test/fpsensor_hw.c
@@ -6,15 +6,16 @@
 #include "common.h"
 #include "test_util.h"
 #include "fpc_private.h"
+#include "board.h"
+#include "fpc/fpc_sensor.h"
 
 /* Hardware-dependent smoke test that makes a SPI transaction with the
  * fingerprint sensor.
  */
 test_static int test_fp_check_hwid(void)
 {
-	/* TODO(b/187134801): modify fpc_check_hwid() to return the hwid. */
 	if (IS_ENABLED(SECTION_IS_RW))
-		TEST_EQ(fpc_check_hwid(), EC_SUCCESS, "%d");
+		TEST_EQ(fpc_check_hwid(), FP_SENSOR_HWID, "%d");
 
 	return EC_SUCCESS;
 }
