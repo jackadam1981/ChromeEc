3054ae3d07c4a7ed5c10bd747c23c544cabc81fb
Kevin Shelton <kmshelton@chromium.org>
2021-11-10 23:10:26 -0800

    fingerprint: validate the expected hwid
    BUG=b:187134801
    BRANCH=none
    TEST=Passed using dragonclaw v0.2 and servo_micro:
         ./test/run_device_tests.py -t fpsensor_hw
         --flasher=servo_micro,
         ./test/run_device_tests.py
         --flasher=servo_micro;
         Passed using icetower v0.2 and servo_micro:
         ./test/run_device_tests.py -t fpsensor_hw
         --flasher=servo_micro --board dartmonkey;
    
    Change-Id: I7a3e99eccfcd828a72b12d8570c1cf7ed4fdd7fc
    Signed-off-by: Kevin Shelton <kmshelton@chromium.org>

--- driver/fingerprint/fpc/bep/fpc_private.h
+++ driver/fingerprint/fpc/bep/fpc_private.h
@@ -34,7 +34,12 @@ typedef struct {
 int fp_sensor_maintenance(uint8_t *image_data,
 			  fp_sensor_info_t *fp_sensor_info);
 
-/* Read the HWID from the sensor. */
-int fpc_check_hwid(void);
+/**
+ * Get the HWID of the sensor.
+ *
+ * @param id Pointer to where to store the HWID value.
+ * @return EC_SUCCESS, FP_ERROR_SPI_COMM, FP_ERROR_BAD_HWID, or EC_ERROR_INVAL
+ */
+int fpc_get_hwid(uint16_t *id);
 
 #endif  /* __CROS_EC_FPC_PRIVATE_H */
--- driver/fingerprint/fpc/libfp/fpc_private.h
+++ driver/fingerprint/fpc/libfp/fpc_private.h
@@ -125,7 +125,12 @@ typedef struct {
 int fp_sensor_maintenance(uint8_t *image_data,
 			  fp_sensor_info_t *fp_sensor_info);
 
-/* Read the HWID from the sensor. */
-int fpc_check_hwid(void);
+/**
+ * Get the HWID of the sensor.
+ *
+ * @param id Pointer to where to store the HWID value.
+ * @return EC_SUCCESS, EC_ERROR_HW_INTERNAL, or EC_ERROR_INVAL
+ */
+int fpc_get_hwid(uint16_t *id);
 
 #endif /* __CROS_EC_FPC_PRIVATE_H */
