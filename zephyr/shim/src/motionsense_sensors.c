80fc9e894273744949f5e5454b382ad846235de5
Dawid Niedzwiecki <dn@semihalf.com>
2021-11-15 13:46:53 +0100

    zephyr: improve handling CBI SSFC
    The improvements include:
    -Use better names for SSFC related things.
    -Do not call checking SSFC and replacing alternate sensors automatically.
    Let the board-code call the replacement, since it is very specific
    operation.
    
    BUG=b:202451919
    TEST=zmake testall
    BRANCH=main
    
    Signed-off-by: Dawid Niedzwiecki <dn@semihalf.com>
    Change-Id: I77a720d5b2ce09ea85902b681c5266d44eb6d144

--- zephyr/shim/src/motionsense_sensors.c
+++ zephyr/shim/src/motionsense_sensors.c
@@ -392,22 +392,25 @@ DECLARE_HOOK(HOOK_INIT, sensor_enable_irqs, HOOK_PRIO_DEFAULT);
 /* Handle the alternative motion sensors */
 #define REPLACE_ALT_MOTION_SENSOR(new_id, old_id) \
 	motion_sensors[SENSOR_ID(old_id)] =       \
-		motion_sensors_alt[SENSOR_ID(new_id)];
+		motion_sensors_alt[SENSOR_ID(new_id)]
 
-#define CHECK_AND_REPLACE_ALT_MOTION_SENSOR(id)                        \
-	do {                                                           \
-		if (cros_cbi_ssfc_check_match(                         \
-			    dev, CBI_SSFC_VALUE_ID(DT_PHANDLE(         \
-					 id, alternate_indicator)))) { \
-			REPLACE_ALT_MOTION_SENSOR(                     \
-				id, DT_PHANDLE(id, alternate_for))     \
-		}                                                      \
-	} while (0);
+#define CHECK_SSFC_AND_REPLACE_ALT_SENSOR(id, cbi_dev)                        \
+	do {                                                                  \
+		if (cros_cbi_ssfc_check_match(cbi_dev, CBI_SSFC_VALUE_ID(     \
+				DT_PHANDLE(id, alternate_ssfc_indicator)))) { \
+			LOG_INF("Replacing \"%s\" for \"%s\" based on SSFC",  \
+				motion_sensors[SENSOR_ID(DT_PHANDLE(id,       \
+					alternate_for))].name,                \
+				motion_sensors_alt[SENSOR_ID(id)].name);      \
+			REPLACE_ALT_MOTION_SENSOR(                            \
+				id, DT_PHANDLE(id, alternate_for));           \
+		}                                                             \
+	} while (0)
 
-#define ALT_MOTION_SENSOR_INIT_ID(id)                                    \
-	COND_CODE_1(UTIL_AND(DT_NODE_HAS_PROP(id, alternate_for),        \
-			     DT_NODE_HAS_PROP(id, alternate_indicator)), \
-		    (CHECK_AND_REPLACE_ALT_MOTION_SENSOR(id)), ())
+#define ALT_SENSOR_CHECK_SSFC_ID(id, cbi_dev)                                 \
+	COND_CODE_1(UTIL_AND(DT_NODE_HAS_PROP(id, alternate_for),             \
+			     DT_NODE_HAS_PROP(id, alternate_ssfc_indicator)), \
+		    (CHECK_SSFC_AND_REPLACE_ALT_SENSOR(id, cbi_dev);), ())
 
 #if DT_NODE_EXISTS(SENSOR_ALT_NODE)
 
@@ -451,15 +454,15 @@ int motion_sense_probe(enum sensor_alt_id alt_idx)
 	return res;
 }
 
-static void motion_sensors_init_alt(void)
+void motion_sensors_check_ssfc(void)
 {
 	const struct device *dev = device_get_binding("cros_cbi");
 
 	if (dev != NULL) {
-		DT_FOREACH_CHILD(SENSOR_ALT_NODE, ALT_MOTION_SENSOR_INIT_ID)
+		DT_FOREACH_CHILD_VARGS(SENSOR_ALT_NODE,
+				       ALT_SENSOR_CHECK_SSFC_ID, dev)
 	}
 }
-DECLARE_HOOK(HOOK_INIT, motion_sensors_init_alt, HOOK_PRIO_INIT_I2C + 1);
 #endif /* DT_NODE_EXISTS(SENSOR_ALT_NODE) */
 
 #define DEF_MOTION_ISR_NAME_ENUM(id) \
