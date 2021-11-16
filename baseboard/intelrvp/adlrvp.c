4f3f71b4732f18464b90fea2f5fae69f54bebc9b
Dino Li <Dino.Li@ite.com.tw>
2021-11-16 14:05:19 +0800

    TEST-ONLY: adlrvpp ite it81302
    Change-Id: I1b2e9dcb3233fb5861cd053123639805bfccf2c3

--- baseboard/intelrvp/adlrvp.c
+++ baseboard/intelrvp/adlrvp.c
@@ -388,27 +388,6 @@ static void configure_retimer_usbmux(void)
 	}
 }
 
-/******************************************************************************/
-/* PWROK signal configuration */
-/*
- * On ADLRVP, SYS_PWROK_EC is an output controlled by EC and uses ALL_SYS_PWRGD
- * as input.
- */
-const struct intel_x86_pwrok_signal pwrok_signal_assert_list[] = {
-	{
-		.gpio = GPIO_SYS_PWROK_EC,
-		.delay_ms = 3,
-	},
-};
-const int pwrok_signal_assert_count = ARRAY_SIZE(pwrok_signal_assert_list);
-
-const struct intel_x86_pwrok_signal pwrok_signal_deassert_list[] = {
-	{
-		.gpio = GPIO_SYS_PWROK_EC,
-	},
-};
-const int pwrok_signal_deassert_count = ARRAY_SIZE(pwrok_signal_assert_list);
-
 /*
  * Returns board information (board id[7:0] and Fab id[15:8]) on success
  * -1 on error.
