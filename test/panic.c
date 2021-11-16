165a96929e29f4482dc55c06311a45cead7dcaff
Tom Hughes <tomhughes@chromium.org>
2021-11-16 09:25:35 -0800

    test: Add panic test
    Add a test for ARM that verifies that exception_panic() correctly
    preserves the registers we expect it to.
    
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I592483c922652df8a4fb2b0a0a5fb57b1480b8b6

---
+++ test/panic.c
@@ -0,0 +1,121 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "panic.h"
+#include "system.h"
+#include "task.h"
+#include "test_util.h"
+
+struct reg_vals {
+	int index;
+	uint32_t val;
+};
+
+static const struct reg_vals expected[] = {
+	{ .index = CORTEX_PANIC_REGISTER_R4, .val = 4 },
+	{ .index = CORTEX_PANIC_REGISTER_R5, .val = 5 },
+	{ .index = CORTEX_PANIC_REGISTER_R6, .val = 6 },
+	{ .index = CORTEX_PANIC_REGISTER_R7, .val = 7 },
+	{ .index = CORTEX_PANIC_REGISTER_R8, .val = 8 },
+	{ .index = CORTEX_PANIC_REGISTER_R9, .val = 9 },
+	{ .index = CORTEX_PANIC_REGISTER_R10, .val = 10 },
+	{ .index = CORTEX_PANIC_REGISTER_R11, .val = 11 },
+	{ .index = CORTEX_PANIC_REGISTER_LR, .val = 14 },
+};
+
+test_static int test_exception_panic_registers(void)
+{
+	/* Cortex-M */
+	asm volatile("movs r0, #0\n"
+		     "movs r1, #1\n"
+		     "movs r2, #2\n"
+		     "movs r3, #3\n"
+		     "movs r4, #4\n"
+		     "movs r5, #5\n"
+		     "movs r6, #6\n"
+		     "movs r7, #7\n"
+		     "movs r8, #8\n"
+		     "movs r9, #9\n"
+		     "movs r10, #10\n"
+		     "movs r11, #11\n"
+		     "movs r14, #14\n"
+		     /*
+		      * Using branch (b) instead of branch with link (bl) so
+		      * that we preserve the link register (r14).
+		      */
+		     "b exception_panic\n");
+
+#if 0
+	/* Cortex-M0 */
+	asm volatile(
+		     "movs r1, #1\n"
+		     "movs r2, #2\n"
+		     "movs r3, #3\n"
+		     "movs r4, #4\n"
+		     "movs r5, #5\n"
+		     "movs r6, #6\n"
+		     "movs r7, #7\n"
+		     "movs r0, #8\n"
+		     "mov r8, r0\n"
+		     "movs r0, #9\n"
+		     "mov r9, r0\n"
+		     "movs r0, #10\n"
+		     "mov r10, r0\n"
+		     "movs r0, #11\n"
+		     "mov r11, r0\n"
+		     "movs r0, #14\n"
+		     "mov r14, r0\n"
+		     "movs r0, #0\n"
+		     "bl exception_panic\n");
+#endif
+
+	__builtin_unreachable();
+}
+
+test_static void run_test_step1(void)
+{
+	ccprintf("Step 1: Panic\n");
+	system_set_scratchpad(TEST_STATE_MASK(TEST_STATE_STEP_2));
+	RUN_TEST(test_exception_panic_registers);
+}
+
+test_static int run_test_step2(void)
+{
+	struct panic_data *data;
+	int i;
+
+	ccprintf("Step 2: Read panic data\n");
+	data = panic_get_data();
+	for (i = 0; i < ARRAY_SIZE(expected); i++) {
+		TEST_EQ(expected[i].val, data->cm.regs[expected[i].index],
+			"%d");
+		cflush();
+	}
+	return EC_SUCCESS;
+}
+
+void test_run_step(uint32_t state)
+{
+	if (state & TEST_STATE_MASK(TEST_STATE_STEP_1))
+		run_test_step1();
+	else if (state & TEST_STATE_MASK(TEST_STATE_STEP_2)) {
+		if (run_test_step2() == EC_SUCCESS)
+			test_reboot_to_next_step(TEST_STATE_PASSED);
+		else
+			test_reboot_to_next_step(TEST_STATE_FAILED);
+	}
+}
+
+int task_test(void *unused)
+{
+	test_run_multistep();
+	return EC_SUCCESS;
+}
+
+void run_test(int argc, char **argv)
+{
+	msleep(30); /* Wait for TASK_ID_TEST to initialize */
+	task_wake(TASK_ID_TEST);
+}
