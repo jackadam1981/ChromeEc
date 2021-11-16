6e27f7709d87e5b7fa8c9aa25cd351f86a76d624
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-11-16 14:20:56 +0800

    ps8xxx: disable low power mode for PS8815
    Don't send TCPC_REG_COMMAND_I2CIDLE to PS8815 since it will enter sleep
    mode by itself.
    
    BUG=b:206553720
    BRANCH=none
    TEST=make -j BOARD=gimble
    TEST=zmake configure --test zephyr/test/drivers
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: If49079e30137bc08125c8b58c0a535efd4e9931f

--- zephyr/test/drivers/src/ps8xxx.c
+++ zephyr/test/drivers/src/ps8xxx.c
@@ -911,7 +911,8 @@ static void test_ps8xxx_tcpci_low_power_mode(void)
 {
 	const struct emul *ps8xxx_emul = emul_get_binding(PS8XXX_EMUL_LABEL);
 	const struct emul *tcpci_emul = ps8xxx_emul_get_tcpci(ps8xxx_emul);
-
+	if(board_get_ps8xxx_product_id(USBC_PORT_C1) == PS8815_PRODUCT_ID)
+		return;
 	test_tcpci_low_power_mode(tcpci_emul, USBC_PORT_C1);
 }
 
