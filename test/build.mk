c50e92c95685080cab9fcf9ab88cca5582d2f7f7
Tom Hughes <tomhughes@chromium.org>
2021-11-18 12:04:02 -0800

    test: Add panic_data_print
    BRANCH=none
    BUG=none
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Ia1398e33caeb00b06d183cbd177321221233acc4

--- test/build.mk
+++ test/build.mk
@@ -243,6 +243,7 @@ mutex-y=mutex.o
 newton_fit-y=newton_fit.o
 panic-y=panic.o
 panic_data-y=panic_data.o
+panic_data_print-y=panic_data_print.o
 pingpong-y=pingpong.o
 power_button-y=power_button.o
 powerdemo-y=powerdemo.o
