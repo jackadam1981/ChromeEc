9674bdb170ab190590ab1af7a159c4184ff017c1
Dawid Niedzwiecki <dn@semihalf.com>
2021-11-18 11:37:46 +0100

    zephyr: move probing of alt sensors to a function
    Remove a function to probe all alternate motion sensors, but prepare a
    function to probe a specific sensor. It introduces more flexibility.
    
    Also, add a function to replace a default sensor when a new one is
    detected.
    
    BUG=b:202451919
    TEST=zmake testall
    BRANCH=main
    
    Signed-off-by: Dawid Niedzwiecki <dn@semihalf.com>
    Change-Id: Ibfab10cbff39e17c8a86aad0c6d7c5897285a0a4

--- zephyr/shim/include/motionsense_sensors.h
+++ zephyr/shim/include/motionsense_sensors.h
@@ -101,10 +101,21 @@ enum sensor_alt_id {
 #endif
 
 /*
- * Performs probing of alternative sensors marked with "runtime-probe" property
- * in device tree. If an alternative sensor is detected, the function replaces
- * a default one in the motion_sensors array.
+ * Performs probing of an alternate sensor.
+ * @param alt_idx Index in motion_sensors_alt of the sensor to be probed.
+ *		  It should be gained wiht SENSOR_ID,
+ *		  e.g. with SENSOR_ID(DT_NODELABEL(label)).
+ * @return EC_SUCCESS if the probe was successful, non-zero otherwise.
  */
-void motion_sense_probe_sensors(void);
+int motion_sense_probe(enum sensor_alt_id alt_idx);
+
+/*
+ * Replaces a default motion sensor with an alternate one pointed by alt_idx.
+ * @param alt_idx Index in motion_sensors_alt of the sensor to be used.
+ *		  It should be gained wiht SENSOR_ID,
+ *		  e.g. with SENSOR_ID(DT_NODELABEL(label)).
+ * @return EC_SUCCESS if the replacement was successful, non-zero otherwise.
+ */
+int motion_sense_replace_alt(enum sensor_alt_id alt_idx);
 
 #endif /* __CROS_EC_MOTIONSENSE_SENSORS_H */
