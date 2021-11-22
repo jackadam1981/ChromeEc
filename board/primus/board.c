4a22abaa21f58a675aba84a54240636ea89cefd2
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2021-11-22 14:17:30 +0800

    DNS: primus:set input current
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I0f6410bf31b6fb4d34669bd8da1af0eba7969df9

--- board/primus/board.c
+++ board/primus/board.c
@@ -126,7 +126,12 @@ __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 	 * Follow OEM request to limit the input current to
 	 * 97% negotiated limit.
 	 */
-	charge_ma = charge_ma * 97 / 100;
+	if ((max_ma == 3250) && chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		charge_ma = 3150;
+	else if (!chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		charge_ma = 3000;
+	else
+		charge_ma = charge_ma * 90 / 100;
 
 	charge_set_input_current_limit(MAX(charge_ma,
 					CONFIG_CHARGER_INPUT_CURRENT),
