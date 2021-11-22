66a82e953683c013b287ef0931a1e2c9e21d4ee4
Divya Sasidharan <divya.s.sasidharan@intel.com>
2021-11-22 11:58:15 -0800

    CHROMIUM: zephyr: Add AP power sequencing for x86 SoC
    Add AP power sequencing
    Skeleton derived from below:
    https://github.com/zephyrproject-rtos/zephyr/pull/29245
    
    TODO:
       1. Espi callbacks for sleep state seed into state machine
       2. Add wait signal list for each chipset
       3. Add changes for non-power sequencer chips
       4. State machine entry and exit routines.
       5. Add power button task
       6. Enable required console commands like powerinfo,
          powerindebug.
       7. Enable hook notifications.
       8. Implement input event handling when lid state,
          power button changes or ap power control console
          commands.
    
    BUG=None
    BRANCH=None
    TEST=Testing with brya, AP moves to S0 at bootup.
    
    Change-Id: I9fb3eef1a7f948c995c0517aa580829e6bd9383d
    Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>

---
+++ zephyr/subsys/ap_pwrseq/include/x86_non_dsx_espi.h
@@ -0,0 +1,26 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * @file
+ * @brief ESPI sleep signal callbacks for power sequencing
+ */
+
+#ifndef __NDSX_ESPI_H__
+#define __NDSX_ESPI_H__
+
+/**
+ * @brief Configure eSPI channels for Non Deep Sleep well platforms
+ */
+void ndsx_espi_configure(void);
+
+/**
+ * @brief Return Virtual wire value
+
+ * @retval Actual Virtual Wire value or 0 on failure
+ */
+uint8_t vw_get_level(enum espi_vwire_signal signal);
+
+#endif /* __NDSX_ESPI_H__ */
