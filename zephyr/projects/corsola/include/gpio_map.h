6eca02558defe60c56c01230a815c10ff4453d2e
Eric Yilun Lin <yllin@chromium.org>
2021-11-26 14:43:06 +0800

    DO-NOT-SUBMIT: use npcx9m6f
    Change-Id: I719a51910af15a2962352f42bba07ea59a1c3284

--- zephyr/projects/corsola/include/gpio_map.h
+++ zephyr/projects/corsola/include/gpio_map.h
@@ -45,7 +45,7 @@
 	#define AP_SPI_INT()        GPIO_INT(GPIO_SPI0_CS,                     \
 					     GPIO_INT_EDGE_BOTH,               \
 					     spi_event)
-#elif defined(CONFIG_SOC_NPCX9M3F)
+#elif defined(CONFIG_SOC_NPCX9M3F) || defined(CONFIG_SOC_NPCX9M6F)
 	/* TODO(b:203619750): enable SPI interrupt */
 	#define AP_SPI_INT()
 #endif
