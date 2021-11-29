dd2966e2dfa956fed1bca6db1d5d2f9646dfc6e2
li feng <li1.feng@intel.com>
2021-11-29 13:35:34 -0800

    WIP: zephyr/ap_pwrseq: add power signal
    GPIO, VW
    
    Signed-off-by: li feng <li1.feng@intel.com>
    Change-Id: Idffb5470cd341ec5d072ee62ed87371272b8db60

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_espi.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_espi.c
@@ -24,7 +24,9 @@ static void espi_vw_handler(struct espi_event *event)
 	switch (event->evt_details) {
 	case ESPI_VWIRE_SIGNAL_SLP_S3:
 	case ESPI_VWIRE_SIGNAL_SLP_S4:
-		  // k_thread_resume(&pwrseq_thread_id);
+	case ESPI_VWIRE_SIGNAL_SLP_S5:
+		power_update_signals();
+		// k_thread_resume(&pwrseq_thread_id);
 		break;
 	default:
 		break;
