d004a1cdb35bf5c733fbc49cf2d95ed0921bc827
Peter Marheine <pmarheine@chromium.org>
2021-12-01 09:49:03 +1100

    dooly: fix incorrect I2C data pin mappings
    The data lines defined for the I2C buses associated with USB-C port 1 seem
    to have been copied from port 0 and while the clock line pins were updated,
    the data lines were not. This change corrects the data lines to refer to
    the correct pins.
    
    BUG=b:208485991
    TEST=builds
    BRANCH=none
    
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Change-Id: Ic093e9a4c305cef5e8b5661e8443adf90d1faa7b

--- board/dooly/board.c
+++ board/dooly/board.c
@@ -534,9 +534,9 @@ const struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 const struct i2c_port_t i2c_ports[] = {
 	{"ina",     I2C_PORT_INA,     400, GPIO_I2C0_SCL, GPIO_I2C0_SDA},
 	{"ppc0",    I2C_PORT_PPC0,    400, GPIO_I2C1_SCL, GPIO_I2C1_SDA},
-	{"ppc1",    I2C_PORT_PPC1,    400, GPIO_I2C2_SCL, GPIO_I2C1_SDA},
+	{"ppc1",    I2C_PORT_PPC1,    400, GPIO_I2C2_SCL, GPIO_I2C2_SDA},
 	{"tcpc0",   I2C_PORT_TCPC0,   400, GPIO_I2C3_SCL, GPIO_I2C3_SDA},
-	{"tcpc1",   I2C_PORT_TCPC1,   400, GPIO_I2C4_SCL, GPIO_I2C3_SDA},
+	{"tcpc1",   I2C_PORT_TCPC1,   400, GPIO_I2C4_SCL, GPIO_I2C4_SDA},
 	{"power",   I2C_PORT_POWER,   400, GPIO_I2C5_SCL, GPIO_I2C5_SDA},
 	{"eeprom",  I2C_PORT_EEPROM,  400, GPIO_I2C7_SCL, GPIO_I2C7_SDA},
 };
