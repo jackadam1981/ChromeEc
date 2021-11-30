0cd3281c7333e1cb2b59b3d1f6f126fbc0733fed
reno.wang <reno.wang@lcfc.corp-partner.google.com>
2021-11-30 22:02:01 +0800

    taeko: Correct base sensor's y/z axis
    In some angle, dut should keep clashell mode, but change to tablet mode
    and lock keyboard because of wrong y/z axis setting. This CL is used to
    correct it.
    
    BRANCH=none
    BUG=b:201504044
    TEST=make -j BOARD=taeko, verify base sensor's x/y/z via checking
    ectool motionsense.
    
    Signed-off-by: reno.wang <reno.wang@lcfc.corp-partner.google.com>
    Change-Id: Id3d48b184b7e20744961f4219bbae1be06136ddf

--- board/taeko/sensors.c
+++ board/taeko/sensors.c
@@ -76,8 +76,8 @@ static const mat33_fp_t lid_standard_ref = {
 
 static const mat33_fp_t base_standard_ref = {
 	{ FLOAT_TO_FP(-1), 0, 0},
-	{ 0, 0, FLOAT_TO_FP(-1)},
-	{ 0, FLOAT_TO_FP(-1), 0}
+	{ 0, FLOAT_TO_FP(1), 0},
+	{ 0, 0, FLOAT_TO_FP(-1)}
 };
 
 struct motion_sensor_t bma422_lid_accel = {
