a445ab4ec3d0e111f73b3fb4e0380acfef2ff6fb
Tristan Honscheid <honscheid@google.com>
2021-12-01 16:16:00 -0700

    zephyr: lis2dw12: Test set_data_rate()
    Unit test for the LIS2DW12 set_data_rate function. Covers turning the
    device off by setting a rate of 0, and exercises the rounding and
    clamping functionality.
    
    BRANCH=None
    BUG=b:200046770
    TEST=zmake -D configure --test test-drivers; make runhosttests
    
    Signed-off-by: Tristan Honscheid <honscheid@google.com>
    Change-Id: I1ba28a7b192b4921593085b922269adc172ba6ad

--- zephyr/test/drivers/src/lis2dw12.c
+++ zephyr/test/drivers/src/lis2dw12.c
@@ -153,6 +153,109 @@ static void test_lis2dw12_set_range(void)
 		      EC_ERROR_INVAL, rv);
 }
 
+void test_lis2dw12_set_rate(void)
+{
+	const struct emul *emul = emul_get_binding(EMUL_LABEL);
+	struct i2c_emul *i2c_emul = lis2dw12_emul_to_i2c_emul(emul);
+	struct motion_sensor_t *ms = &motion_sensors[LIS2DW12_SENSOR_ID];
+	struct stprivate_data *drv_data = (struct stprivate_data *)ms->drv_data;
+	int rv;
+
+	/* Part 1: Turn off sensor with rate=0 */
+	rv = ms->drv->set_data_rate(ms, 0, 0);
+
+	zassert_equal(lis2dw12_emul_peek_odr(i2c_emul),
+		      LIS2DW12_ODR_POWER_OFF_VAL,
+		      "Output data rate should be %d but got %d",
+		      LIS2DW12_ODR_POWER_OFF_VAL,
+		      lis2dw12_emul_peek_odr(i2c_emul));
+	zassert_equal(drv_data->base.odr, LIS2DW12_ODR_POWER_OFF_VAL,
+		      "Output data rate should be %d but got %d",
+		      LIS2DW12_ODR_POWER_OFF_VAL, drv_data->base.odr);
+	zassert_equal(rv, EC_SUCCESS, "Returned %d but expected %d", rv,
+		      EC_SUCCESS);
+
+	/* Part 2: Set some output data rates. We will request a certain rate
+	 * and make sure the closest supported rate is used.
+	 */
+
+	struct {
+		int requested_rate; /* millihertz */
+		int round; /* 0=round down, 1=round up */
+		int expected_norm_rate; /* millihertz */
+		uint8_t expected_reg_val;
+	} test_params[] = {
+		{ 1000, 0, LIS2DW12_ODR_MIN_VAL, LIS2DW12_ODR_12HZ_VAL },
+		{ 12501, 0, 12500, LIS2DW12_ODR_12HZ_VAL },
+		{ 25001, 0, 25000, LIS2DW12_ODR_25HZ_VAL },
+		{ 50001, 0, 50000, LIS2DW12_ODR_50HZ_VAL },
+		{ 100001, 0, 100000, LIS2DW12_ODR_100HZ_VAL },
+		{ 200001, 0, 200000, LIS2DW12_ODR_200HZ_VAL },
+		{ 400001, 0, 400000, LIS2DW12_ODR_400HZ_VAL },
+		{ 800001, 0, 800000, LIS2DW12_ODR_800HZ_VAL },
+		{ 1600001, 0, 1600000, LIS2DW12_ODR_1_6kHZ_VAL },
+		{ 3200001, 0, LIS2DW12_ODR_MAX_VAL, LIS2DW12_ODR_1_6kHZ_VAL },
+
+		{ 1000, 1, LIS2DW12_ODR_MIN_VAL, LIS2DW12_ODR_12HZ_VAL },
+		{ 12501, 1, 25000, LIS2DW12_ODR_25HZ_VAL },
+		{ 25001, 1, 50000, LIS2DW12_ODR_50HZ_VAL },
+		{ 50001, 1, 100000, LIS2DW12_ODR_100HZ_VAL },
+		{ 100001, 1, 200000, LIS2DW12_ODR_200HZ_VAL },
+		{ 200001, 1, 400000, LIS2DW12_ODR_400HZ_VAL },
+		{ 400001, 1, 800000, LIS2DW12_ODR_800HZ_VAL },
+		{ 800001, 1, 1600000, LIS2DW12_ODR_1_6kHZ_VAL },
+		{ 1600001, 1, LIS2DW12_ODR_MAX_VAL, LIS2DW12_ODR_1_6kHZ_VAL },
+	};
+	for (size_t i = 0; i < ARRAY_SIZE(test_params); i++) {
+		/* For each test vector in the above array */
+		drv_data->base.odr = -1;
+		rv = ms->drv->set_data_rate(ms, test_params[i].requested_rate,
+					    test_params[i].round);
+
+		/* Check the normalized rate the driver chose */
+		zassert_equal(
+			drv_data->base.odr, test_params[i].expected_norm_rate,
+			"For requested rate %d, output data rate should be %d but got %d",
+			test_params[i].requested_rate,
+			test_params[i].expected_norm_rate, drv_data->base.odr);
+
+		/* Read ODR and mode bits back from CTRL1 register */
+		uint8_t odr_bits = lis2dw12_emul_peek_odr(i2c_emul);
+
+		zassert_equal(
+			odr_bits, test_params[i].expected_reg_val,
+			"For requested rate %d, ODR bits should be 0x%x but got 0x%x - %d",
+			test_params[i].requested_rate,
+			test_params[i].expected_reg_val, odr_bits,
+			LIS2DW12_ODR_MAX_VAL);
+
+		/* Check if high performance mode was enabled if rate >
+		 * 200,000mHz
+		 */
+
+		uint8_t mode_bits = lis2dw12_emul_peek_mode(i2c_emul);
+		uint8_t lpmode_bits = lis2dw12_emul_peek_lpmode(i2c_emul);
+
+		if (odr_bits > LIS2DW12_ODR_200HZ_VAL) {
+			/* High performance mode, LP mode immaterial */
+			zassert_equal(mode_bits, LIS2DW12_HIGH_PERF,
+				      "MODE[1:0] should be 0x%x, but got 0x%x",
+				      LIS2DW12_HIGH_PERF, mode_bits);
+
+		} else {
+			/* Low power mode, LP mode 2 */
+			zassert_equal(mode_bits, LIS2DW12_LOW_POWER,
+				      "MODE[1:0] should be 0x%x, but got 0x%x",
+				      LIS2DW12_LOW_POWER, mode_bits);
+
+			zassert_equal(
+				lpmode_bits, LIS2DW12_LOW_POWER_MODE_2,
+				"LPMODE[1:0] should be 0x%x, but got 0x%x",
+				LIS2DW12_LOW_POWER_MODE_2, lpmode_bits);
+		}
+	}
+}
+
 void test_suite_lis2dw12(void)
 {
 	ztest_test_suite(lis2dw12,
@@ -176,6 +279,9 @@ void test_suite_lis2dw12(void)
 				 lis2dw12_setup, lis2dw12_setup),
 			 ztest_unit_test_setup_teardown(
 				 test_lis2dw12_set_range,
+				 lis2dw12_setup, lis2dw12_setup),
+			 ztest_unit_test_setup_teardown(
+				 test_lis2dw12_set_rate,
 				 lis2dw12_setup, lis2dw12_setup)
 			 );
 	ztest_run_test_suite(lis2dw12);
