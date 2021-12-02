1c5f70ef6c7e71253d5ba25ae8b720c1d62636cc
Peter Marheine <pmarheine@chromium.org>
2021-12-02 15:31:46 +1100

    zephyr: use a chosen device for bbram shim
    Rather than assume the bbram device is named 'bbram', get the device from
    a chosen cros-ec,bbram node instead.
    
    To accommodate this in testing, this also converts the system test that
    exercises bbram shimming to use a Zephyr emulated bbram device rather
    than rolling its own that replaces the one in the shim only. An unused
    include file is also removed.
    
    BUG=b:205615358
    TEST=zmake testall
    BRANCH=none
    
    Change-Id: I68c31b7be6b63c065ada65aba3b49ec0e6df7448
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>

--- zephyr/test/system/test_system.c
+++ zephyr/test/system/test_system.c
@@ -8,7 +8,6 @@
 #include <logging/log.h>
 #include <ztest.h>
 
-#include "bbram.h"
 #include "system.h"
 
 LOG_MODULE_REGISTER(test);
@@ -21,32 +20,16 @@ LOG_MODULE_REGISTER(test);
 static char mock_data[64] =
 	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
 
-static int mock_bbram_read(const struct device *unused, size_t offset,
-			   size_t size, uint8_t *data)
-{
-	if (offset < 0 || offset + size >= ARRAY_SIZE(mock_data))
-		return -1;
-	memcpy(data, mock_data + offset, size);
-	return EC_SUCCESS;
-}
-
-static const struct bbram_driver_api bbram_api = {
-	.read = mock_bbram_read,
-};
-
-static const struct device bbram_dev_instance = {
-	.name = "TEST_BBRAM_DEV",
-	.config = NULL,
-	.api = &bbram_api,
-	.data = NULL,
-};
-
-const struct device *bbram_dev = &bbram_dev_instance;
-
 static void test_bbram_get(void)
 {
+	const struct device *const bbram_dev =
+		DEVICE_DT_GET(DT_CHOSEN(cros_ec_bbram));
 	uint8_t output[10];
 	int rc;
+
+	/* Write expected data to read back */
+	rc = bbram_write(bbram_dev, 0, ARRAY_SIZE(mock_data), mock_data);
+	zassert_equal(rc, 0, NULL);
 
 	rc = system_get_bbram(SYSTEM_BBRAM_IDX_PD0, output);
 	zassert_equal(rc, 0, NULL);
