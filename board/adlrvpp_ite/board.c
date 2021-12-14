5a667a551a2e03c42ddac464f273ad8f8f5f3955
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2021-12-14 15:01:10 +0530

    adlrvpn: configure charger vsys min register
    ADL-N uses the samilar charger chip as Brya i.e BQ25720
    Referring to the b:196184163 charger system voltage register
    should be set based on the AC or AC+battery conditions.
    New API set_charger_system_voltage() is implemented for this.
    API is called when AC/battery is connected/disconnected.
    
    Changes helped to solve the charger chip asserting PROCHOT signal
    when battery disconnected from DUT.
    
    BRANCH=none
    BUG=none
    TEST=verified change on adlrvp-n board.PROCHOT is not
    asserted when battery is disconnected.
    
    Change-Id: Ic21d8008530e08f2b7d26994665ca8cc37cffdfe
    Signed-off-by: Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>

--- board/adlrvpp_ite/board.c
+++ board/adlrvpp_ite/board.c
@@ -8,6 +8,7 @@
 #include "fan.h"
 #include "fusb302.h"
 #include "gpio.h"
+#include "hooks.h"
 #include "i2c.h"
 #include "i2c_bitbang.h"
 #include "it83xx_pd.h"
@@ -137,3 +138,23 @@ const struct tcpc_config_t tcpc_config[] = {
 #endif
 };
 BUILD_ASSERT(ARRAY_SIZE(tcpc_config) == CONFIG_USB_PD_PORT_MAX_COUNT);
+
+static void enable_irq(void)
+{
+	gpio_enable_interrupt(CONFIG_BATTERY_PRESENT_GPIO);
+}
+DECLARE_HOOK(HOOK_INIT, enable_irq, HOOK_PRIO_LAST);
+
+static void handle_batt_interrupt(void)
+{
+	/* configure the PPVAR_SYS depend on AC or AC+battery */
+	set_charger_system_voltage();
+}
+
+DECLARE_DEFERRED(handle_batt_interrupt);
+
+void battery_detect_interrupt(enum gpio_signal signal)
+{
+	/* Trigger deferred notification of battery interrupt */
+	hook_call_deferred(&handle_batt_interrupt_data, 0);
+}
