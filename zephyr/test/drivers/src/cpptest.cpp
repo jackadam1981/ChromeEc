06bb34e6ea3374b5723c15b71c2497c2d120450e
Tristan Honscheid <honscheid@google.com>
2021-12-15 16:30:04 -0700

    CPP testing
    BUG=None
    BRANCH=None
    TEST=None
    Signed-off-by: Tristan Honscheid <honscheid@google.com>
    
    Change-Id: I87e15fb3d78224aef3a588dc36afebd5f7bfb108

---
+++ zephyr/test/drivers/src/cpptest.cpp
@@ -0,0 +1,12 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+extern "C" {
+    #include <stdio.h>
+}
+
+void cpptest(void) {
+    printf("\n\n******* Hi from CPP ********\n\n");
+}
