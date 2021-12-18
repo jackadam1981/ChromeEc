fb1cfa2fbaba5a46521b86d9f0941d768b405404
Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
2021-12-18 11:43:58 +0800

    vdec: add to support mt8185 decoder
    1: fix vdec ipi service interface
    2: fix supported format
    3: fix ectasklist
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Change-Id: I9d458a8c92ee27366ae03c3386d5682f62f5693b

--- board/corsola_scp/vdec.h
+++ board/corsola_scp/vdec.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -7,27 +7,22 @@
 #define __CROS_EC_SCP_VDEC_H
 
 #include "compile_time_macros.h"
-#include "queue.h"
-#include "registers.h"
 
 enum vdec_type {
-	VDEC_H264,
-	VDEC_VP8,
-	VDEC_VP9,
+	VDEC_LAT,
+	VDEC_CORE,
 	VDEC_MAX,
 };
-
-typedef void (*vdec_msg_handler)(void *msg);
 
 struct vdec_msg {
 	enum vdec_type type;
 	unsigned char msg[48];
 };
-
-BUILD_ASSERT(member_size(struct vdec_msg, msg) <= CONFIG_IPC_SHARED_OBJ_BUF_SIZE);
+BUILD_ASSERT(member_size(struct vdec_msg, msg) <=
+		CONFIG_IPC_SHARED_OBJ_BUF_SIZE);
 
 /* Functions provided by private overlay. */
-void vdec_h264_service_init(void);
-void vdec_h264_msg_handler(void *data);
+void vdec_core_msg_handler(void *msg);
+void vdec_msg_handler(void *msg);
 
 #endif /* __CROS_EC_SCP_VDEC_H */
