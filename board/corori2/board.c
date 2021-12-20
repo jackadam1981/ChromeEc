d5306969450ae0bd8b2d2100c49f783f3bbfcf49
Owen_Ou <Owen_Ou@compal.corp-partner.google.com>
2021-12-20 10:20:33 +0800

    Corori2: Initialize the vivaldi keyboard
    Initialize vivaldi keyboard.
    
    BUG=b:201340797
    BRANCH=none
    TEST=make -j BOARD=corori2
    
    Signed-off-by: Owen_Ou <Owen_Ou@compal.corp-partner.google.com>
    Change-Id: Id59881e23bc4bb15f09f7a3a6d5c4d113a8bac51

--- board/corori2/board.c
+++ board/corori2/board.c
@@ -28,6 +28,7 @@
 #include "gpio.h"
 #include "hooks.h"
 #include "i2c.h"
+#include "keyboard_8042.h"
 #include "keyboard_scan.h"
 #include "lid_switch.h"
 #include "motion_sense.h"
@@ -48,6 +49,31 @@
 #define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
 #define INT_RECHECK_US 5000
+
+static const struct ec_response_keybd_config corori_keybd = {
+	/* Default Chromeos keyboard config */
+	.num_top_row_keys = 10,
+	.action_keys = {
+		TK_BACK,		/* T1 */
+		TK_FORWARD,		/* T2 */
+		TK_REFRESH,		/* T3 */
+		TK_FULLSCREEN,		/* T4 */
+		TK_OVERVIEW,		/* T5 */
+		TK_BRIGHTNESS_DOWN,	/* T6 */
+		TK_BRIGHTNESS_UP,	/* T7 */
+		TK_VOL_MUTE,		/* T8 */
+		TK_VOL_DOWN,		/* T9 */
+		TK_VOL_UP,		/* T10 */
+	},
+	/* No function keys, no numeric keypad, no screenlock key */
+
+};
+
+__override const struct ec_response_keybd_config
+*board_vivaldi_keybd_config(void)
+{
+	return &corori_keybd;
+}
 
 /* C0 interrupt line shared by BC 1.2 and charger */
 static void check_c0_line(void);
