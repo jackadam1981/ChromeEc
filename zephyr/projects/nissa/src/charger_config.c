dc787f7db5667a8056ede75edf9cbb8739ac66e4
Peter Marheine <pmarheine@chromium.org>
2021-12-23 14:33:41 +1100

    nissa/nivviks: enable USB-PD and charger support
    All of these are enabled at once because the USB-PD implementation
    requires the others.
    
    Board-specific functions are largely taken from lalala's implementation,
    since that board also uses dual RAA489000s.
    
    TODO: split it up some, fix GPIO data so C1 interrupt line is generated.
    
    BUG=b:211693800
    TEST=builds
    BRANCH=none
    
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Change-Id: Ib84324d29d4435ed1a4dfeb4d48c3a79afda5516

---
+++ zephyr/projects/nissa/src/charger_config.c
@@ -0,0 +1,20 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "battery.h"
+#include "charger.h"
+#include "charger/isl923x_public.h"
+
+const struct charger_config_t chg_chips[] = {
+	{
+		.i2c_port = I2C_PORT_USB_C0_TCPC,
+		.i2c_addr_flags = ISL923X_ADDR_FLAGS,
+		.drv = &isl923x_drv,
+	},
+	/* TODO(b:211693800) port 1 is present on sub-boards 1 and 2 with same
+	 * configuration as port 0 but on I2C_PORT_USB_C1_TCPC.
+	 */
+};
+const unsigned int chg_cnt = ARRAY_SIZE(chg_chips);
