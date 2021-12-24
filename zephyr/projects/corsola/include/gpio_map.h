21e6be604edb01fa8fd7824adeb3f4cd8feefb1b
Ting Shen <phoenixshen@google.com>
2021-12-24 16:04:11 +0800

    krabby: enable usb-c
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I2b608044882e39d2ef35dbbf5562112fca7fc7d4

--- zephyr/projects/corsola/include/gpio_map.h
+++ zephyr/projects/corsola/include/gpio_map.h
@@ -159,6 +159,15 @@
 #define GPIO_EN_USB_C1_FRS          RT1718S_GPIO3
 #endif
 
+#define RT9490_INT() IF_ENABLED(CONFIG_PLATFORM_EC_CHARGER_RT9490,             \
+				(GPIO_INT(GPIO_USB_C1_BC12_CHARGER_INT_ODL,    \
+					  GPIO_INT_EDGE_FALLING,               \
+					  bc12_interrupt)))
+#define RT1739_INT() IF_ENABLED(CONFIG_PLATFORM_EC_USBC_PPC_RT1739,            \
+				(GPIO_INT(GPIO_USB_C0_PPC_BC12_INT_ODL,        \
+					  GPIO_INT_EDGE_FALLING,               \
+					  bc12_interrupt)))
+
 /*
  * Set EC_CROS_GPIO_INTERRUPTS to a space-separated list of GPIO_INT items.
  *
@@ -194,6 +203,8 @@
 	TCPC_C1_INT()							\
 	PPC_C0_INT()							\
 	BC12_C0_INT()							\
-	X_EC_GPIO2_INT()
+	X_EC_GPIO2_INT()						\
+	RT9490_INT()							\
+	RT1739_INT()
 
 #endif /* __ZEPHYR_GPIO_MAP_H */
