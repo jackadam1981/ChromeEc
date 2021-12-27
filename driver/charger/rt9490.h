0ec1f4c2d047e8a9f55eb42aa74c23f34bafa501
Ting Shen <phoenixshen@google.com>
2021-12-27 15:27:03 +0800

    WIP: rt9490: implement bc12 driver
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I2e72f45eb954f9a3da58b360d3cbaa9662821846

--- driver/charger/rt9490.h
+++ driver/charger/rt9490.h
@@ -162,6 +162,9 @@ struct rt9490_init_setting {
 #define RT9490_JEITA_WARM_MASK			BIT(1)
 #define RT9490_JEITA_HOT_MASK			BIT(0)
 
+/* CHG_IRQ_FLAG1 */
+#define RT9490_BC12_DONE_FLAG			BIT(0)
+
 /* SAFETY TMR CTRL */
 #define RT9490_EN_TRICHG_TMR			BIT(5)
 #define RT9490_EN_PRECHG_TMR			BIT(4)
@@ -189,6 +192,10 @@ struct rt9490_init_setting {
 #define RT9490_VBUS_STAT_SHIFT			1
 #define RT9490_BC12_DONE_STAT			BIT(0)
 
+#define RT9490_SDP				0x1
+#define RT9490_CDP				0x2
+#define RT9490_DCP				0x3
+
 /* FAULT STATUS0 */
 #define RT9490_VBAT_OVP_STAT			BIT(5)
 
@@ -204,5 +211,8 @@ struct rt9490_init_setting {
 #define RT9490_JEITA_COLD_HOT			BIT(0)
 
 extern const struct charger_drv rt9490_drv;
+extern const struct bc12_drv rt9490_bc12_drv;
+
+void rt9490_interrupt(int port);
 
 #endif /* __CROS_EC_RT9490_H */
