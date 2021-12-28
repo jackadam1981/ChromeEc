298c760c3bc05520320a5b1fe273767b61920c81
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2021-12-28 20:50:13 +0800

    charge_ramp: Allow HW ramping when vbus reaching vSafe5V
    Allow HW ramping when vbus reaching vSafe5V to avoid voltage dropping.
    
    BUG=b:196928096
    BRANCH=none
    TEST=make sure that 4V/5V adapter and charging through C2C cable by
    another DUT work.
    
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Change-Id: Iadeb4842bdca9671cb2811964a0bf9f5a38c0de9

--- common/charge_ramp.c
+++ common/charge_ramp.c
@@ -9,6 +9,7 @@
 #include "common.h"
 #include "system.h"
 #include "usb_charge.h"
+#include "usb_pd.h"
 #include "util.h"
 
 test_mockable int chg_ramp_allowed(int port, int supplier)
@@ -26,21 +27,24 @@ test_mockable int chg_ramp_allowed(int port, int supplier)
 	case CHARGE_SUPPLIER_TYPEC_DTS:
 		return 1;
 	/*
-	 * Don't regulate the input voltage for USB-C chargers. It is
-	 * unnecessary as the USB-C compliant adapters should never trigger it
-	 * active.
-	 *
 	 * The USB-C spec defines their load curves should not be below
 	 * 4.75V @0A and 4V @3A. We can't define the voltage regulation value
-	 * higher than 4V since it limits the current reaching its max 3A. If
-	 * we define the voltage regulation value lower than 4V, their load
-	 * curves will never be below the voltage regulation line.
+	 * higher than 4V when vbus is less than 4.75V since it limits the
+	 * current reaching its max 3A. If we define the voltage regulation
+	 * value lower than 4V when vbus is less than 4.75V, their load curves
+	 * will never be below the voltage regulation line. When vbus is above
+	 * 4.75V, we have to enable HW ramping to avoid vbus voltage dropping
+	 * below 4.75V (aka "vSafe5V").
 	 *
 	 * Check go/charge_ramp_typec for detail.
 	 */
 	case CHARGE_SUPPLIER_PD:
 	case CHARGE_SUPPLIER_TYPEC:
-		return 0;
+	/* Allow HW ramping for USB-C chargers when vbus reaching vSafe5V. */
+		if (pd_check_vbus_level(port, VBUS_PRESENT))
+			return IS_ENABLED(CONFIG_CHARGE_RAMP_HW);
+		else
+			return 0;
 	/* default: fall through */
 	}
 
