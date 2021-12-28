7a3445180f6056a1ab01396afa4a40e508dfd01e
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2021-12-28 11:13:33 +0800

    draco: config USBC port and remove db
    This patch update USBC setting and
    remove daughter board to fit draco HW design.
    
    BUG=none
    BRANCH=none
    TEST=amke BOARD=draco
    
    Change-Id: Ibdc1d2e4dd86a3b8383ac33622d8ca038862b721
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>

--- board/draco/i2c.c
+++ board/draco/i2c.c
@@ -22,35 +22,27 @@ const struct i2c_port_t i2c_ports[] = {
 	},
 	{
 		/* I2C1 */
-		.name = "tcpc0,2",
-		.port = I2C_PORT_USB_C0_C2_TCPC,
+		.name = "tcpc0",
+		.port = I2C_PORT_USB_C0_TCPC,
 		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_TCPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_TCPC_SDA,
+		.scl = GPIO_EC_I2C_USB_C0_SCL,
+		.sda = GPIO_EC_I2C_USB_C0_SDA,
 	},
 	{
 		/* I2C2 */
-		.name = "ppc0,2",
-		.port = I2C_PORT_USB_C0_C2_PPC,
+		.name = "ppc0",
+		.port = I2C_PORT_USB_C0_PPC,
 		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SDA,
-	},
-	{
-		/* I2C3 */
-		.name = "retimer0,2",
-		.port = I2C_PORT_USB_C0_C2_MUX,
-		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_RT_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_RT_SDA,
+		.scl = GPIO_EC_I2C_USB_C0_SCL,
+		.sda = GPIO_EC_I2C_USB_C0_SDA,
 	},
 	{
 		/* I2C4 C1 TCPC */
 		.name = "tcpc1",
 		.port = I2C_PORT_USB_C1_TCPC,
 		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C1_TCPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_TCPC_SDA,
+		.scl = GPIO_EC_I2C_USB_C1_SCL,
+		.sda = GPIO_EC_I2C_USB_C1_SDA,
 		.flags = I2C_PORT_FLAG_DYNAMIC_SPEED,
 	},
 	{
@@ -60,15 +52,6 @@ const struct i2c_port_t i2c_ports[] = {
 		.kbps = 100,
 		.scl = GPIO_EC_I2C_BAT_SCL,
 		.sda = GPIO_EC_I2C_BAT_SDA,
-	},
-	{
-		/* I2C6 */
-		.name = "ppc1",
-		.port = I2C_PORT_USB_C1_PPC,
-		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C1_MIX_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_MIX_SDA,
-		.flags = I2C_PORT_FLAG_DYNAMIC_SPEED,
 	},
 	{
 		/* I2C7 */
@@ -90,7 +73,7 @@ static void set_board_legacy_i2c_speeds(void)
 	if (get_board_id() >= BOARD_ID_FAST_PLUS_CAPABLE)
 		return;
 
-	ccprints("setting USB DB I2C buses to 400 kHz\n");
+	ccprints("setting USB C1 I2C buses to 400 kHz\n");
 
 	i2c_set_freq(I2C_PORT_USB_C1_TCPC, I2C_FREQ_400KHZ);
 	i2c_set_freq(I2C_PORT_USB_C1_PPC, I2C_FREQ_400KHZ);
