4a1a38f23a8930fe15e144a6b5df6a22e4ff975e
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2021-12-30 11:48:31 +0800

    baseboard/guybrush: move fan setting to board level
    Move fans[], fan_rpm and fan_conf to board level.
    
    BUG=none
    BRANCH=none
    TEST="make BAORD=$BOARD" pass for guybrush family.
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: I83c476b3569f24722ff375a19e4dfabbd7bede07

--- board/dewatt/board.c
+++ board/dewatt/board.c
@@ -19,6 +19,7 @@
 #include "driver/temp_sensor/sb_tsi.h"
 #include "driver/temp_sensor/pct2075.h"
 #include "extpower.h"
+#include "fan.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "keyboard_8042.h"
@@ -414,6 +415,25 @@ const struct temp_sensor_t temp_sensors[] = {
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
+
+const struct fan_conf fan_conf_0 = {
+	.flags = FAN_USE_RPM_MODE,
+	.ch = MFT_CH_0,	/* Use MFT id to control fan */
+	.pgood_gpio = GPIO_S0_PGOOD,
+	.enable_gpio = -1,
+};
+const struct fan_rpm fan_rpm_0 = {
+	.rpm_min = 1000,
+	.rpm_start = 1000,
+	.rpm_max = 6500,
+};
+const struct fan_t fans[] = {
+	[FAN_CH_0] = {
+		.conf = &fan_conf_0,
+		.rpm = &fan_rpm_0,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(fans) == FAN_CH_COUNT);
 
 struct ec_thermal_config thermal_params[TEMP_SENSOR_COUNT] = {
 	[TEMP_SENSOR_SOC] = {
--- board/nipperkin/board.c
+++ board/nipperkin/board.c
@@ -18,6 +18,7 @@
 #include "driver/temp_sensor/sb_tsi.h"
 #include "driver/temp_sensor/pct2075.h"
 #include "extpower.h"
+#include "fan.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "keyboard_scan.h"
@@ -222,6 +223,25 @@ const struct pct2075_sensor_t pct2075_sensors[] = {
 	{ I2C_PORT_SENSOR, PCT2075_I2C_ADDR_FLAGS7 },
 };
 BUILD_ASSERT(ARRAY_SIZE(pct2075_sensors) == PCT2075_COUNT);
+
+const struct fan_conf fan_conf_0 = {
+	.flags = FAN_USE_RPM_MODE,
+	.ch = MFT_CH_0,	/* Use MFT id to control fan */
+	.pgood_gpio = GPIO_S0_PGOOD,
+	.enable_gpio = -1,
+};
+const struct fan_rpm fan_rpm_0 = {
+	.rpm_min = 1000,
+	.rpm_start = 1000,
+	.rpm_max = 6500,
+};
+const struct fan_t fans[] = {
+	[FAN_CH_0] = {
+		.conf = &fan_conf_0,
+		.rpm = &fan_rpm_0,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(fans) == FAN_CH_COUNT);
 
 const struct temp_sensor_t temp_sensors[] = {
 	[TEMP_SENSOR_SOC] = {
