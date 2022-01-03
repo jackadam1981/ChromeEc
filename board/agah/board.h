830b1fadf893e9b9658062ae33f8ce39cc42a1a0
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2022-01-03 16:22:16 +0800

    agah: support barrel jack adapter
    BUG=b:210970640
    BRANCH=none
    TEST=make BOARD=agah
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: I53ded7ae43b306790fe9f396b69c296d2bd4d5f5

--- board/agah/board.h
+++ board/agah/board.h
@@ -75,6 +75,11 @@
 #define PD_MAX_CURRENT_MA	3000
 #define PD_MAX_VOLTAGE_MV	20000
 
+/* Dedicated barreljack charger port */
+#undef  CONFIG_DEDICATED_CHARGE_PORT_COUNT
+#define CONFIG_DEDICATED_CHARGE_PORT_COUNT 1
+#define DEDICATED_CHARGE_PORT 3
+
 /*
  * Macros for GPIO signals used in common code that don't match the
  * schematic names. Signal names in gpio.inc match the schematic and are
@@ -198,6 +203,14 @@ enum mft_channel {
 	MFT_CH_0 = 0,
 	MFT_CH_COUNT
 };
+
+enum charge_port {
+	CHARGE_PORT_TYPEC0,
+	CHARGE_PORT_TYPEC1,
+	CHARGE_PORT_BARRELJACK,
+};
+
+extern void adp_connect_interrupt(enum gpio_signal signal);
 
 #endif /* !__ASSEMBLER__ */
 
