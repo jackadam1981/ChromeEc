fbb4bdfa293e564287d837006b51fb15539ed427
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2022-01-03 16:22:16 +0800

    agah: support barrel jack adapter
    Support barrel jack adapter.
    
    BUG=b:210970640
    BRANCH=none
    TEST=make BOARD=agah
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: I53ded7ae43b306790fe9f396b69c296d2bd4d5f5

--- board/agah/charger_isl9241.c
+++ board/agah/charger_isl9241.c
@@ -11,13 +11,14 @@
 #include "compile_time_macros.h"
 #include "console.h"
 #include "driver/charger/isl9241.h"
+#include "hooks.h"
 #include "usbc_ppc.h"
 #include "usb_pd.h"
 #include "util.h"
 
 
-#define CPRINTSUSB(format, args...) cprints(CC_USBCHARGE, format, ## args)
-#define CPRINTFUSB(format, args...) cprintf(CC_USBCHARGE, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
 /* Charger Chip Configuration */
 const struct charger_config_t chg_chips[] = {
@@ -29,13 +30,22 @@ const struct charger_config_t chg_chips[] = {
 };
 BUILD_ASSERT(ARRAY_SIZE(chg_chips) == CHARGER_NUM);
 
+int board_agah_vbus_source_enabled(int port)
+{
+	/* Ignore non-PD ports (the barrel jack). */
+	if (port >= CONFIG_USB_PD_PORT_MAX_COUNT)
+		return 0;
+	return ppc_is_sourcing_vbus(port);
+}
+
 int board_set_active_charge_port(int port)
 {
-	int is_valid_port = board_is_usb_pd_port_present(port);
 	int i;
 
+	CPRINTS("Requested charge port change to %d", port);
+
 	if (port == CHARGE_PORT_NONE) {
-		CPRINTSUSB("Disabling all charger ports");
+		CPRINTS("Disabling all charger ports");
 
 		/* Disable all ports. */
 		for (i = 0; i < ppc_cnt; i++) {
@@ -44,21 +54,30 @@ int board_set_active_charge_port(int port)
 			 * get into a boot loop assertion failure.
 			 */
 			if (ppc_vbus_sink_enable(i, 0))
-				CPRINTSUSB("Disabling C%d as sink failed.", i);
+				CPRINTS("Disabling C%d as sink failed.", i);
 		}
-
 		return EC_SUCCESS;
-	} else if (!is_valid_port) {
-		return EC_ERROR_INVAL;
 	}
+
+	if (port < 0 || CHARGE_PORT_COUNT <= port)
+		return EC_ERROR_INVAL;
+
+	if (port == charge_manager_get_active_charge_port())
+		return EC_SUCCESS;
+
+	/* Don't charge from a source port */
+	if (board_agah_vbus_source_enabled(port))
+		return EC_ERROR_INVAL;
 
 	/* Check if the port is sourcing VBUS. */
-	if (ppc_is_sourcing_vbus(port)) {
-		CPRINTFUSB("Skip enable C%d", port);
+	if ((port == CHARGE_PORT_TYPEC0 ||
+		port == CHARGE_PORT_TYPEC1) &&
+		ppc_is_sourcing_vbus(port)) {
+		CPRINTS("Skip enable C%d", port);
 		return EC_ERROR_INVAL;
 	}
 
-	CPRINTSUSB("New charge port: C%d", port);
+	CPRINTS("New charger p%d", port);
 
 	/*
 	 * Turn off the other ports' sink path FETs, before enabling the
@@ -69,13 +88,31 @@ int board_set_active_charge_port(int port)
 			continue;
 
 		if (ppc_vbus_sink_enable(i, 0))
-			CPRINTSUSB("C%d: sink path disable failed.", i);
+			CPRINTS("C%d: sink path disable failed.", i);
 	}
 
-	/* Enable requested charge port. */
-	if (ppc_vbus_sink_enable(port, 1)) {
-		CPRINTSUSB("C%d: sink path enable failed.", port);
+	/* Enable requested USBC charge port. */
+	if ((port == CHARGE_PORT_TYPEC0 ||
+		port == CHARGE_PORT_TYPEC1) &&
+		ppc_vbus_sink_enable(port, 1)) {
+		CPRINTS("C%d: sink path enable failed.", port);
 		return EC_ERROR_UNKNOWN;
+	}
+
+	switch (port) {
+	case CHARGE_PORT_TYPEC0:
+	case CHARGE_PORT_TYPEC1:
+		gpio_set_level(GPIO_EN_PPVAR_BJ_ADP, 0);
+		break;
+	case CHARGE_PORT_BARRELJACK:
+		/* Make sure BJ adapter is sourcing power */
+		if (gpio_get_level(GPIO_BJ_ADP_PRESENT_L))
+			return EC_ERROR_INVAL;
+
+		gpio_set_level(GPIO_EN_PPVAR_BJ_ADP, 1);
+		break;
+	default:
+		return EC_ERROR_INVAL;
 	}
 
 	return EC_SUCCESS;
@@ -88,3 +125,46 @@ __overridable void board_set_charge_limit(int port, int supplier, int charge_ma,
 					   CONFIG_CHARGER_INPUT_CURRENT),
 				       charge_mv);
 }
+
+#define ADP_DEBOUNCE_MS		1000  /* Debounce time for BJ plug/unplug */
+/* Debounced connection state of the barrel jack */
+static int8_t adp_connected = -1;
+static void adp_connect_deferred(void)
+{
+	struct charge_port_info pi = { 0 };
+	int connected = !gpio_get_level(GPIO_BJ_ADP_PRESENT_L);
+
+	/* Debounce */
+	if (connected == adp_connected)
+		return;
+	if (connected) {
+		pi.voltage = 19500;
+		pi.current = 7700;
+	}
+	charge_manager_update_charge(CHARGE_SUPPLIER_DEDICATED,
+				     DEDICATED_CHARGE_PORT, &pi);
+	adp_connected = connected;
+}
+DECLARE_DEFERRED(adp_connect_deferred);
+
+/* IRQ for BJ plug/unplug. It shouldn't be called if BJ is the power source. */
+void adp_connect_interrupt(enum gpio_signal signal)
+{
+	hook_call_deferred(&adp_connect_deferred_data, ADP_DEBOUNCE_MS * MSEC);
+}
+
+static void adp_state_init(void)
+{
+	/*
+	 * Initialize all charge suppliers to 0. The charge manager waits until
+	 * all ports have reported in before doing anything.
+	 */
+	for (int i = 0; i < CHARGE_PORT_COUNT; i++) {
+		for (int j = 0; j < CHARGE_SUPPLIER_COUNT; j++)
+			charge_manager_update_charge(j, i, NULL);
+	}
+
+	/* Report charge state from the barrel jack. */
+	adp_connect_deferred();
+}
+DECLARE_HOOK(HOOK_INIT, adp_state_init, HOOK_PRIO_CHARGE_MANAGER_INIT + 1);
