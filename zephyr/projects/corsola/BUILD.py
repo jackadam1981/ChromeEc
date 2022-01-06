862648d5c49e8de6942e9ccb857d335f98afb255
Eric Yilun Lin <yllin@chromium.org>
2022-01-06 10:52:00 +0800

    zephyr: kingler: enable BMI323 base gyro and accel sensor
    BUG=b:213280143
    TEST=accelread 0,1
    BRANCH=none
    
    Change-Id: I65fd2dfe3ef0bf9463992740aa825ba2456e37d8
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/projects/corsola/BUILD.py
+++ zephyr/projects/corsola/BUILD.py
@@ -45,6 +45,7 @@ register_corsola_project(
         here / "i2c_kingler.dts",
         here / "cbi_eeprom_kingler.dts",
         here / "gpio_kingler.dts",
+        here / "motionsense_kingler.dts",
     ],
     extra_kconfig_files=[here / "prj_kingler.conf"],
 )
