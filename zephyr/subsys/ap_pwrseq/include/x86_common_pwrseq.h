ab1ca92b87229b50f72a77a47af65114948d4bef
Divya Sasidharan <divya.s.sasidharan@intel.com>
2022-01-07 10:37:51 -0800

    zephyr: subsys/ap_pwrseq: Add inbuilt power sequencing support
    Add inbuilt AP power sequencing support.
    This patch adds power up sequence for AP.
    
    Built on top of below reference:
    https://github.com/zephyrproject-rtos/zephyr/pull/29245
    
    BUG=b:203446068
    BRANCH=None
    TEST=zmake testall; make buildall; brya boots up to S0
         and display is up.
    
    Change-Id: I84ce803d4e1b4594dc3f61bd2d8bcd1f94fd2be8
    Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>

---
+++ zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
@@ -0,0 +1,85 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __X86_COMMON_H__
+#define __X86_COMMON_H__
+
+#include <drivers/espi.h>
+#include <drivers/gpio.h>
+
+/* Power signal GPIO configuration */
+struct gpio_config {
+	/* GPIO net name */
+	const char *net_name;
+	/* GPIO pin port name */
+	const char *port_name;
+	/* GPIO pin index */
+	const gpio_pin_t pin;
+	/* GPIO configuration flags */
+	const gpio_flags_t flags;
+	/* Device structure for the driver instance */
+	const struct device *port;
+};
+
+/**
+ * @brief System power states for Non Deep Sleep Well
+ * EC is an always on device in a Non Deep Sx system except when EC
+ * is hibernated or all the VRs are turned off.
+ */
+enum power_states_ndsx {
+	/*
+	 * Actual power states
+	 */
+	/* AP is off & EC is on */
+	SYS_POWER_STATE_G3,
+	/* AP is in soft off state */
+	SYS_POWER_STATE_S5,
+	/* AP is suspended to Non-volatile disk */
+	SYS_POWER_STATE_S4,
+	/* AP is suspended to RAM */
+	SYS_POWER_STATE_S3,
+	/* AP is in active state */
+	SYS_POWER_STATE_S0,
+
+	/*
+	 * Intermediate power up states
+	 */
+	/* Determine if the AP's power rails are turned on */
+	SYS_POWER_STATE_G3S5,
+	/* Determine if AP is suspended from sleep */
+	SYS_POWER_STATE_S5S4,
+	/* Determine if Suspend to Disk is de-asserted */
+	SYS_POWER_STATE_S4S3,
+	/* Determine if Suspend to RAM is de-asserted */
+	SYS_POWER_STATE_S3S0,
+
+	/*
+	 * Intermediate power down states
+	 */
+	/* Determine if the AP's power rails are turned off */
+	SYS_POWER_STATE_S5G3,
+	/* Determine if AP is suspended to sleep */
+	SYS_POWER_STATE_S4S5,
+	/* Determine if Suspend to Disk is asserted */
+	SYS_POWER_STATE_S3S4,
+	/* Determine if Suspend to RAM is asserted */
+	SYS_POWER_STATE_S0S3,
+};
+
+/* Common device tree configurable attributes */
+struct common_pwrseq_config {
+	int pch_dsw_pwrok_delay_ms;
+	int pch_pm_pwrbtn_delay_ms;
+	int pch_rsmrst_delay_ms;
+	int vr_en_vccin_delay_ms;
+};
+
+/* This encapsulates the attributes of the state machine */
+struct power_seq_context {
+	/* On power-on start boot up sequence */
+	enum power_states_ndsx power_state;
+};
+
+#endif /* __X86_COMMON_H__ */
