6d6f4e67ab14ccd8b5ce326981c47d41893c468d
wen zhang <zhangwen6@huaqin.corp-partner.google.com>
2022-01-07 11:00:43 +0800

    zephyr: kingler: enable LED
    Enable battery LEDs for Kingler
    the LEDs are drived by PWM modules
    
    BUG=b:203739613
    BRANCH=none
    TEST=1.ectool led battery query/red/green/amber works well
         2.ectool pwmsetduty/pwmgetduty works well
    
    Change-Id: I8af1c4bc7849626a2c5e8de8e6bf3fad56830f94
    Signed-off-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>

--- zephyr/projects/corsola/BUILD.py
+++ zephyr/projects/corsola/BUILD.py
@@ -46,7 +46,9 @@ register_corsola_project(
         here / "i2c_kingler.dts",
         here / "cbi_eeprom.dts",
         here / "gpio_kingler.dts",
+        here / "led_kingler.dts",
         here / "motionsense_kingler.dts",
+        here / "pwm_kingler.dts",
     ],
     extra_kconfig_files=[here / "prj_kingler.conf"],
 )
