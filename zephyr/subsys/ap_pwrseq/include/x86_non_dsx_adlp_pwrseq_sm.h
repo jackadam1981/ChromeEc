22e8748c643c8c1785316774d9f8e57c7810767e
Divya Sasidharan <divya.s.sasidharan@intel.com>
2022-01-07 10:37:51 -0800

    zephyr: subsys/ap_pwrseq: Add inbuilt power sequencing support
    Add inbuilt AP power sequencing support.
    This patch adds power up sequence for AP.
    
    Built on top of below reference:
    https://github.com/zephyrproject-rtos/zephyr/pull/29245
    
    BUG=b:203446068
    BRANCH=None
    TEST=zmake testall; make buildall; brya boots up to S0
         and display is up.
    
    Change-Id: I84ce803d4e1b4594dc3f61bd2d8bcd1f94fd2be8
    Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>

---
+++ zephyr/subsys/ap_pwrseq/include/x86_non_dsx_adlp_pwrseq_sm.h
@@ -0,0 +1,26 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __X86_NON_DSX_ADLP_H__
+#define __X86_NON_DSX_ADLP_H__
+
+#include <logging/log.h>
+#include <x86_common_pwrseq.h>
+#include <x86_non_dsx_common_pwrseq_sm_handler.h>
+
+extern struct power_seq_context pwrseq_ctx;
+
+extern void gpio_set_lvl(const char *net_name, int val);
+extern int gpio_get_lvl(const char *net_name);
+extern struct common_pwrseq_config com_cfg;
+
+struct chipset_pwrseq_config {
+	int pch_pwrok_delay_ms;
+	int sys_pwrok_delay_ms;
+	int sys_reset_delay_ms;
+	int vccst_pwrgd_delay_ms;
+	int vrrdy_timeout_ms;
+};
+#endif /* __X86_NON_DSX_ADLP_H__ */
