f686bbff76fe5f0278a8658dc1cae4b3d43bf53b
Andrew McRae <amcrae@google.com>
2022-01-08 15:03:35 +1100

    nissa: Add FW_CONFIG fields for sub-board selection
    Add FW_CONFIG fields for sub-board selection.
    
    BUG=b:212660338
    TEST=zmake configure -b {nivviks,nereid}
    BRANCH=none
    
    Change-Id: I12f3872ab2d05ca0a1246b12ea3f842260f511d3
    Signed-off-by: Andrew McRae <amcrae@google.com>

---
+++ zephyr/projects/nissa/src/subboard_config.c
@@ -0,0 +1,57 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Nissa sub-board configuration */
+
+#include <drivers/cros_cbi.h>
+#include "console.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "sub_board.h"
+
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
+#define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ## args)
+
+enum nissa_sub_board_type nissa_get_sb_type(void)
+{
+	static enum nissa_sub_board_type sb = NISSA_SB_UNKNOWN;
+	uint32_t val;
+	const struct device *dev;
+
+	if (sb != NISSA_SB_UNKNOWN)
+		return sb;
+
+	sb = NISSA_SB_NONE;	/* Defaults to none */
+	dev = device_get_binding(CROS_CBI_LABEL);
+	if (dev == NULL) {
+		CPRINTS("No %s device", CROS_CBI_LABEL);
+	} else {
+		switch (val) {
+		default:
+			CPRINTS("No sub-board defined");
+			break;
+		case FW_SUB_BOARD_1:
+			sb = NISSA_SB_C_A;
+			CPRINTS("SB1: USB type C, USB type A");
+			break;
+
+		case FW_SUB_BOARD_2:
+			sb = NISSA_SB_C_LTE;
+			CPRINTS("SB2: USB type C, WWAN LTE");
+			break;
+
+		case FW_SUB_BOARD_3:
+			sb = NISSA_SB_HDMI_A;
+			CPRINTS("SB3: HDMI, USB type A");
+			break;
+		}
+	}
+	return sb;
+}
+
+static void nissa_subboard_init(void)
+{
+}
+DECLARE_HOOK(HOOK_INIT, nissa_subboard_init, HOOK_PRIO_INIT_I2C - 1);
