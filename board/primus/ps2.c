c83e80dceaa35eeebecbf38c1a0508fc896a68b4
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-01-10 16:12:46 +0800

    primus: delay PS2 signal
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I49869d26262948f7f7242ebe494eef86e459a42c

--- board/primus/ps2.c
+++ board/primus/ps2.c
@@ -14,6 +14,16 @@ void send_aux_data_to_device(uint8_t data)
 
 static void board_init(void)
 {
-	ps2_enable_channel(NPCX_PS2_CH1, 1, send_aux_data_to_host_interrupt);
+	if (gpio_get_level(GPIO_EC_PCH_SYS_PWROK)) {
+		gpio_set_alternate_function(GPIO_PORT_6, BIT(2), GPIO_ALT_FUNC_1);
+		gpio_set_alternate_function(GPIO_PORT_6, BIT(3), GPIO_ALT_FUNC_1);
+		ps2_enable_channel(NPCX_PS2_CH1, 1, send_aux_data_to_host_interrupt);
+	} else {
+		gpio_set_flags(GPIO_EC_PS2_SCL_TPAD, GPIO_ODR_LOW);
+		gpio_set_flags(GPIO_EC_PS2_SDA_TPAD, GPIO_ODR_LOW);
+		gpio_set_alternate_function(GPIO_PORT_6, BIT(2), GPIO_ALT_FUNC_NONE);
+		gpio_set_alternate_function(GPIO_PORT_6, BIT(3), GPIO_ALT_FUNC_NONE);
+		ps2_enable_channel(NPCX_PS2_CH1, 0, send_aux_data_to_host_interrupt);
+	}
 }
-DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_SECOND, board_init, HOOK_PRIO_DEFAULT);
