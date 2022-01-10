d871f23f8d41711f77920b106ce701fa6f0dd94d
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-01-10 13:38:00 +0800

    DNS: zephyr on gimble
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I4d7fa004470a1e3959969015747b757c7ef091c6

---
+++ zephyr/projects/brya/gimble/BUILD.py
@@ -0,0 +1,16 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+register_npcx_project(
+    project_name="gimble",
+    zephyr_board="brya",
+    dts_overlays=[
+        "cbi_eeprom.dts",
+        "gpio.dts",
+        "motionsense.dts",
+        "thermal.dts",
+        "sensors.dts",
+        "pwm.dts",
+    ],
+)
