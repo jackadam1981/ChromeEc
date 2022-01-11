34ef72adb005f7f72dc3b9c2b6e5c52526bf8550
li feng <li1.feng@intel.com>
2022-01-10 20:14:09 -0800

    zephyr: subsys/ap_pwrseq: enable power button task
    BUG=none
    BRANCH=none
    TEST=none
    
    Signed-off-by: li feng <li1.feng@intel.com>
    Change-Id: I20dc03a9f7cb9b86c31d88bf98416720111b2e6e
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>

--- zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
+++ zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
@@ -151,6 +151,7 @@ struct common_pwrseq_config {
 struct power_seq_context {
 	/* On power-on start boot up sequence */
 	enum power_states_ndsx power_state;
+	int want_g3_exit;
 };
 
 #endif /* __X86_COMMON_H__ */
