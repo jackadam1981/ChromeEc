28e62a1bb148019dde8f6625382dabf7fa9d0826
Andrew McRae <amcrae@google.com>
2022-01-11 22:07:11 +1100

    nissa: Add some initial sub-board setup for nivviks
    Sub-board initial setup for nivviks.
    
    BUG=b:212490923
    TEST=zmake configure -b nivviks; flash & run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I1a41db88053514f8b0b8b5e41ede4f049d6db35b

--- zephyr/projects/nissa/src/nivviks/board_config.c
+++ zephyr/projects/nissa/src/nivviks/board_config.c
@@ -5,7 +5,11 @@
 
 /* Nivviks sub-board hardware configuration */
 
-#include "gpio.h"
+#include <init.h>
+#include <kernel.h>
+#include <sys/printk.h>
+
+#include "gpios.h"
 #include "hooks.h"
 #include "sub_board.h"
 
@@ -13,17 +17,37 @@ static void nivviks_subboard_init(void)
 {
 	enum nissa_sub_board_type sb = nissa_get_sb_type();
 
+	printk("Setting up for sub-board %d\n", sb);
+
+	gpio_pin_configure_dt(&gpio_usb_c0_int_odl,
+			      GPIO_INPUT | GPIO_PULL_UP);
 	if (sb != NISSA_SB_C_A && sb != NISSA_SB_HDMI_A) {
 		/* Turn off unused USB A1 GPIOs */
+		gpio_pin_configure_dt(&gpio_sub_usb_a1_ilimit_sdp,
+				      GPIO_DISCONNECTED);
+		gpio_pin_configure_dt(&gpio_en_sub_usb_a1_vbus,
+				      GPIO_DISCONNECTED);
 	}
-	if (sb == NISSA_SB_C_A || sb == NISSA_SB_C_LTE) {
+	if (sb == NISSA_SB_C_A || sb == NISSA_SB_C_LTE)
 		/* Enable type-C port 1 */
-	}
+		gpio_pin_configure_dt(&gpio_usb_c1_int_odl,
+				      GPIO_INPUT | GPIO_PULL_UP);
 	if (sb == NISSA_SB_HDMI_A) {
+		/* Disable I2C_PORT_USB_C1_TCPC */
+		/* TODO(b:212490923): Use pinctrl to switch from I2C */
 		/* Enable HDMI GPIOs */
+		gpio_pin_configure_dt(&gpio_en_sub_rails_odl,
+				      GPIO_OUTPUT |
+				      GPIO_OPEN_DRAIN |
+				      GPIO_OUTPUT_INIT_HIGH);
+		gpio_pin_configure_dt(&gpio_hdmi_en_sub_odl,
+				      GPIO_OUTPUT |
+				      GPIO_OPEN_DRAIN |
+				      GPIO_OUTPUT_INIT_HIGH);
+		/* Configure the interrupt separately */
+		gpio_pin_configure_dt(&gpio_hpd_sub_odl,
+				      GPIO_INPUT |
+				      GPIO_PULL_UP);
 	}
 }
-/*
- * Make sure setup is done after EEPROM is readable.
- */
-DECLARE_HOOK(HOOK_INIT, nivviks_subboard_init, HOOK_PRIO_INIT_I2C + 1);
+DECLARE_HOOK(HOOK_INIT, nivviks_subboard_init, HOOK_PRIO_FIRST+1);
