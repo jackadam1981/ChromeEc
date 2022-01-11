53a201c2760c53a5bb77604029ffad9b9b1be160
Parth Malkan <parthmalkan@google.com>
2022-01-10 17:56:47 -0800

    Taniks: Implement antighost keyboard scanner
    This change adds the code that avoids detecting ghost key
    presses thus supporting anti-ghost feature for gaming kb.
    It includes configuring KSI pins as gpios to identify key
    press interrupt and then switching them to adc channels
    to read voltage level to identify actual key presses.
    
    BUG=b:208773873
    TEST=make run-kb_scan
    BRANCH=none
    
    Signed-off-by: Parth Malkan <parthmalkan@google.com>
    Change-Id: I8f6d66aa51167a6a769f11c999a2c8aacbd637f8

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -5,6 +5,7 @@
 
 /* Keyboard scanner module for Chrome EC */
 
+#include "adc.h"
 #include "chipset.h"
 #include "clock.h"
 #include "common.h"
@@ -194,6 +195,80 @@ static void ensure_keyboard_scanned(int old_polls)
 		usleep(keyscan_config.scan_period_us);
 }
 
+#ifdef CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC
+/**
+ * Configure KSI pins in alternate mode as adc channels.
+ *
+ * After we get interrupted on GPIO KSI pins, we need to set the pins in
+ * alternate ADC mode to read the input
+ */
+/*  
+static void keyboard_config_ksi_as_adc(void)
+{
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_00, 1);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_01, 1);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_02, 1);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_03, 1);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_04, 1);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_05, 1);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_06, 1);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_07, 1);
+}
+ */
+
+/**
+ * Configure KSI pins for gpio mode.
+ *
+ * This sets the KSI pins for gpio mode and configures them as inputs
+ */
+/*  
+static void keyboard_config_ksi_as_gpio(void)
+{
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_00, 0);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_01, 0);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_02, 0);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_03, 0);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_04, 0);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_05, 0);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_06, 0);
+	gpio_config_pin(MODULE_ADC, GPIO_KSI_07, 0);
+*/
+	/* Set flags */
+/*
+	gpio_set_flags(GPIO_KSI_00, GPIO_INPUT);
+	gpio_set_flags(GPIO_KSI_01, GPIO_INPUT);
+	gpio_set_flags(GPIO_KSI_02, GPIO_INPUT);
+	gpio_set_flags(GPIO_KSI_03, GPIO_INPUT);
+	gpio_set_flags(GPIO_KSI_04, GPIO_INPUT);
+	gpio_set_flags(GPIO_KSI_05, GPIO_INPUT);
+	gpio_set_flags(GPIO_KSI_06, GPIO_INPUT);
+	gpio_set_flags(GPIO_KSI_07, GPIO_INPUT);
+}
+*/
+
+/**
+ * Read KSI adc rows
+ *
+ * Read each adc channel and look for voltage crossing threshold level
+ */
+static int keyboard_read_adc_rows(void)
+{
+	uint8_t kb_row = 0;
+
+	/* Read each adc channel to build row byte */
+	for (int i = 0; i < KEYBOARD_ROWS; i++) {
+		CPRINTF("*** Reading row %d ***\n", i);
+		if (adc_read_channel(ADC_KSI_00 + i) > TH_VT) {
+			CPRINTF("*** Key pressed for row %x ***\n\n", i);
+			kb_row |= (1 << i);
+		}
+	}
+
+	CPRINTF("*** KB_ROW = 0x%X ***\n\n", kb_row);
+	return kb_row;
+}
+#endif
+
 /**
  * Simulate a keypress.
  *
@@ -247,6 +322,12 @@ static int read_matrix(uint8_t *state)
 	int c;
 	int pressed = 0;
 
+
+#ifdef CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC
+	/* Change KSI gpio pins to adc */
+/*  	keyboard_config_ksi_as_adc(); */
+#endif
+
 	/* 1. Read input pins */
 	for (c = 0; c < keyboard_cols; c++) {
 		/*
@@ -266,13 +347,21 @@ static int read_matrix(uint8_t *state)
 		udelay(keyscan_config.output_settle_us);
 
 		/* Read the row state */
+		CPRINTF("*** Column = 0x%X ***\n\n", c);
+#ifdef CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC
+		state[c] = keyboard_read_adc_rows();
+		CPRINTF("\n\n");
+		cflush();
+#else
 		state[c] = keyboard_raw_read_rows();
+#endif
 
 		/* Use simulated keyscan sequence instead if testing active */
 		if (IS_ENABLED(CONFIG_KEYBOARD_TEST))
 			state[c] = keyscan_seq_get_scan(c, state[c]);
 	}
 
+#if !defined(CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC)
 	/* 2. Detect transitional ghost */
 	for (c = 0; c < keyboard_cols; c++) {
 		int c2;
@@ -296,6 +385,7 @@ static int read_matrix(uint8_t *state)
 			}
 		}
 	}
+#endif
 
 	/* 3. Fix result */
 	for (c = 0; c < keyboard_cols; c++) {
@@ -429,6 +519,7 @@ static int check_runtime_keys(const uint8_t *state)
  *
  * @return 1 if ghosting detected, else 0.
  */
+#if !defined(CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC)
 static int has_ghosting(const uint8_t *state)
 {
 	int c, c2;
@@ -454,6 +545,7 @@ static int has_ghosting(const uint8_t *state)
 
 	return 0;
 }
+#endif
 
 /* Inform keyboard module if scanning is enabled */
 static void key_state_changed(int row, int col, uint8_t state)
@@ -488,9 +580,11 @@ static int check_keys_changed(uint8_t *state)
 	/* Read the raw key state */
 	any_pressed = read_matrix(new_state);
 
+#if !defined(CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC)
 	/* Ignore if so many keys are pressed that we're ghosting. */
 	if (has_ghosting(new_state))
 		return any_pressed;
+#endif
 
 	/* Check for changes between previous scan and this one */
 	for (c = 0; c < keyboard_cols; c++) {
@@ -716,13 +810,17 @@ void keyboard_scan_init(void)
 		board_keyboard_row_refresh());
 
 	/* Configure GPIO */
+	CPRINTS("*** Starting KB Raw init ***");
 	keyboard_raw_init();
+	CPRINTS("*** KB Raw init done ***");
 
 	/* Tri-state the columns */
 	keyboard_raw_drive_column(KEYBOARD_COLUMN_NONE);
 
+#if !defined(CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC)
 	/* Initialize raw state */
 	read_matrix(debounced_state);
+#endif
 
 #ifdef CONFIG_KEYBOARD_LANGUAGE_ID
 	/* Check keyboard ID state */
@@ -767,8 +865,18 @@ void keyboard_scan_task(void *u)
 	int wait_time;
 	uint32_t local_disable_scanning = 0;
 
+#ifdef CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC
+	/* Tri-state the columns */
+	keyboard_raw_drive_column(KEYBOARD_COLUMN_NONE);
+
+	CPRINTS("*** KB read debounced state matrix ***");
+	read_matrix(debounced_state);
+#endif
+
 	print_state(debounced_state, "init state");
 
+	CPRINTS("*** KB raw task start ***");
+	cflush();
 	keyboard_raw_task_start();
 
 	/* Set initial clock frequency-based minimum delay between scans */
@@ -778,6 +886,12 @@ void keyboard_scan_task(void *u)
 		/* Enable all outputs */
 		CPRINTS5("KB wait");
 
+#ifdef CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC
+/*  		CPRINTS("*** Config KSI AS GPIO ***");
+		keyboard_config_ksi_as_gpio(); */
+#endif
+
+		CPRINTS("*** KB raw Enable interrupt ***");
 		keyboard_raw_enable_interrupt(1);
 
 		/* Wait for scanning enabled and key pressed. */
@@ -817,11 +931,19 @@ void keyboard_scan_task(void *u)
 			 * user pressing a key and enable_interrupt()
 			 * starting to pay attention to edges.
 			 */
+#if !defined(CONFIG_KEYBOARD_SCAN_ANTIGHOST_ADC)
 			if (!local_disable_scanning &&
 			    (keyboard_raw_read_rows() || force_poll))
 				break;
-			else
+#else
+			if (!local_disable_scanning &&
+			    (keyboard_read_adc_rows() || force_poll))
+				break;
+#endif
+			else {
+				CPRINTS("** KB Task Sleep **");
 				task_wait_event(-1);
+			}
 		}
 
 		/* We're about to poll, so any existing forces are fulfilled */
