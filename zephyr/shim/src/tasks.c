f5df8d491e6064bf6154b2d8538448d98e593cf2
Keith Short <keithshort@chromium.org>
2022-01-12 10:13:59 -0700

    zephyr: fix the console/shell task priority
    The EC application was setting the shell thread priority to the highest
    priority, but this can cause PD protocol timeouts.
    
    Set the shell priority to be higher than the HOSTCMD thread, matching
    the legacy cros-ec implementation.
    
    This change also makes sure the shell priority is persistent through
    vboot operations.
    
    BUG=b:213526824
    BRANCH=none
    TEST=zmake testall
    TEST=On volteer, validate shell priority is higher than HOSTCMD, but
    lower than the PD threads.
    
    Change-Id: Ia74a8ca0c8a785b57a9069d19742d405711a35c1
    Signed-off-by: Keith Short <keithshort@chromium.org>

--- zephyr/shim/src/tasks.c
+++ zephyr/shim/src/tasks.c
@@ -9,6 +9,7 @@
 #include <shell/shell.h>
 
 #include "common.h"
+#include "console.h"
 #include "timer.h"
 #include "task.h"
 
@@ -285,6 +286,8 @@ void set_test_runner_tid(void)
 
 void start_ec_tasks(void)
 {
+	int priority;
+
 	/* Initialize all EC tasks, which does not include the sysworkq entry */
 	for (size_t i = 0; i < TASK_ID_COUNT; ++i) {
 		struct task_ctx_dyn *const ctx_dyn = &shimmed_tasks_dyn[i];
@@ -293,6 +296,8 @@ void start_ec_tasks(void)
 
 		k_timer_init(&ctx_dyn->timer, timer_expire, NULL);
 
+		priority = K_PRIO_PREEMPT(TASK_ID_COUNT - i - 1);
+
 #ifdef TEST_BUILD
 		/* Do not create thread for test runner; it will be set later */
 		if (i == TASK_ID_TEST_RUNNER) {
@@ -300,6 +305,20 @@ void start_ec_tasks(void)
 			continue;
 		}
 #endif
+
+#if defined CONFIG_SHELL && !defined TEST_BUILD
+		/*
+		 * The console is run on the built-in Zephyr shell thread.
+		 * We need to set the priority to be below the PD tasks
+		 * but higher than most threads so that the FAFT tests work
+		 * correctly.
+		 */
+		if (i == TASK_ID_CONSOLE) {
+			uart_shell_set_priority(priority);
+			continue;
+		}
+#endif
+
 		/*
 		 * TODO(b/172361873): Add K_FP_REGS for FPU tasks. See
 		 * comment in config.h for CONFIG_TASK_LIST for existing flags
@@ -313,7 +332,7 @@ void start_ec_tasks(void)
 			(void *)ctx_static,
 			ctx_dyn,
 			NULL,
-			K_PRIO_PREEMPT(TASK_ID_COUNT - i - 1),
+			priority,
 			0,
 			K_NO_WAIT);
 	}
