45af8643806ba04cf08c636fffbda2b999b90003
Li Feng <li1.feng@intel.com>
2022-01-11 16:13:49 -0800

    zephyr: subsys/ap_pwrseq:  Add chipset state interface APIs
    BUG=b:203446068 b:214509787
    BRANCH=none
    TEST=zmake -l DEBUG build ~/tmp/brya
    
    Signed-off-by: Li Feng <li1.feng@intel.com>
    Change-Id: I40f6d134f1f73283b4eb568f00dacc1e0baf3f08

---
+++ zephyr/shim/src/chipset_api.c
@@ -0,0 +1,29 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Chipset interface APIs from subsys/ap_pwrseq */
+
+#include "common.h"
+#include "zephyr/subsys/ap_pwrseq/include/ap_pwrseq_chipset.h"
+
+int chipset_in_state(int state_mask)
+{
+	return ap_pwrseq_chipset_in_state(state_mask);
+}
+
+int chipset_in_or_transitioning_to_state(int state_mask)
+{
+	return ap_pwrseq_chipset_in_or_transitioning_to_state(state_mask);
+}
+
+void chipset_exit_hard_off(void)
+{
+	ap_pwrseq_chipset_exit_hardoff();
+}
+
+/* TODO: b/214509787
+ * To be added later when this functionality is implemented in ap_pwrseq.
+ */
+void chipset_throttle_cpu(int throttle) { }
--- zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_sm_handler.h
+++ zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_sm_handler.h
@@ -50,6 +50,7 @@ void chipset_force_shutdown(enum pwrseq_chipset_shutdown_reason reason,
 void chipset_reset(enum pwrseq_chipset_shutdown_reason reason);
 void common_rsmrst_pass_thru_handler(void);
 void init_chipset_pwr_seq_state(void);
+void chipset_request_exit_hardoff(bool should_exit);
 enum power_states_ndsx pwr_sm_get_state(void);
 void apshutdown(void);
 
