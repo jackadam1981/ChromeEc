4b17e17106863a0725b02ef3e3369f17b9c88bf1
Dino Li <Dino.Li@ite.com.tw>
2022-01-12 11:24:08 +0800

    TEST-ONLY: adlrvpp ite it81302
    Change-Id: I1b2e9dcb3233fb5861cd053123639805bfccf2c3

--- baseboard/intelrvp/build.mk
+++ baseboard/intelrvp/build.mk
@@ -8,7 +8,7 @@
 
 #Intel RVP common files
 baseboard-y=baseboard.o
-baseboard-$(CONFIG_LED_COMMON)+=led.o led_states.o
+baseboard-$(CONFIG_LED_COMMON)+=led.o led_states.o power.o
 
 ifneq ($(CONFIG_USB_POWER_DELIVERY),)
 baseboard-$(CONFIG_USB_POWER_DELIVERY)+=chg_usb_pd.o
@@ -24,7 +24,7 @@ baseboard-$(VARIANT_INTELRVP_EC_NPCX)+=npcx_ec.o
 #Common board specific files
 ifneq ($(filter y,$(BOARD_ADLRVPP_ITE) $(BOARD_ADLRVPM_ITE) \
 		$(BOARD_ADLRVPP_MCHP1521) $(BOARD_ADLRVPP_NPCX) \
-		$(BOARD_ADLRVPP_MCHP1727)),)
+		$(BOARD_ADLRVPP_MCHP1727) $(BOARD_ADLRVPP_ITE_IT81302)),)
 baseboard-y+=adlrvp.o
 baseboard-$(CONFIG_BATTERY_SMART)+=adlrvp_battery.o
 endif
