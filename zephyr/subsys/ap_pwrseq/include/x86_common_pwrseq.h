c95f14df5dd4a957842c83205860e420cffd64a7
Li Feng <li1.feng@intel.com>
2022-01-11 16:13:49 -0800

    zephyr: subsys/ap_pwrseq: Add chipset state interface
    S0ix is not included.
    
    BUG=b:203446068
    BRANCH=none
    TEST=zmake -l DEBUG build ~/tmp/brya
    
    Signed-off-by: Li Feng <li1.feng@intel.com>
    Change-Id: I40f6d134f1f73283b4eb568f00dacc1e0baf3f08

--- zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
+++ zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
@@ -77,4 +77,20 @@ struct pwrseq_context {
 	/* On power-on start boot up sequence */
 	enum power_states_ndsx power_state;
 };
+
+/* Chipset state mask */
+enum chipset_state_mask {
+	CHIPSET_STATE_HARD_OFF = 0x01,   /* Hard off (G3) */
+	CHIPSET_STATE_SOFT_OFF = 0x02,   /* Soft off (S5) */
+	CHIPSET_STATE_SUSPEND  = 0x04,   /* Suspend (S3) */
+	CHIPSET_STATE_ON       = 0x08,   /* On (S0) */
+	CHIPSET_STATE_STANDBY  = 0x10,   /* Standby (S0ix) */
+	/* Common combinations */
+	CHIPSET_STATE_ANY_OFF = (CHIPSET_STATE_HARD_OFF |
+				 CHIPSET_STATE_SOFT_OFF),  /* Any off state */
+	/* This combination covers any kind of suspend i.e. S3 or S0ix. */
+	CHIPSET_STATE_ANY_SUSPEND = (CHIPSET_STATE_SUSPEND |
+				     CHIPSET_STATE_STANDBY),
+};
+
 #endif /* __X86_COMMON_H__ */
