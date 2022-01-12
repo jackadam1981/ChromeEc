37e0b3f6d0778feb4cd7e20fa8d1254b25c93a75
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-01-12 12:40:41 -0800

    nivviks: Add signals required for power sequence
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Change-Id: If32297a8bfb6b5ba1a5d84b38eba9d5d5087137a

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_adlp_pwrseq_sm.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_adlp_pwrseq_sm.c
@@ -23,9 +23,16 @@ struct gpio_config power_seq_gpios[] = {
 	{
 		POWER_SEQ_GPIO(VR_PG_EC_RSMRST_ODL),
 	},
+#if POWER_SEQ_GPIO_PRESENT(VR_EC_ALL_SYS_PWRGD)
 	{
 		POWER_SEQ_GPIO(VR_EC_ALL_SYS_PWRGD),
 	},
+#endif
+#if POWER_SEQ_GPIO_PRESENT(EC_OUT_ALL_SYS_PWRGD)
+	{
+		POWER_SEQ_GPIO(EC_OUT_ALL_SYS_PWRGD),
+	},
+#endif
 #if POWER_SEQ_GPIO_PRESENT(VR_EC_DSW_PWROK)
 	{
 		POWER_SEQ_GPIO(VR_EC_DSW_PWROK),
@@ -96,10 +103,12 @@ struct gpio_interrupt_config power_seq_intr_gpios[] = {
 		POWER_SEQ_INTR_GPIO(PCH_EC_SLP_S3_L),
 		.disable_at_boot = false,
 	},
+#if POWER_SEQ_GPIO_PRESENT(VR_EC_ALL_SYS_PWRGD)
 	{
 		POWER_SEQ_INTR_GPIO(VR_EC_ALL_SYS_PWRGD),
 		.disable_at_boot = false,
 	},
+#endif
 };
 
 const int power_seq_intr_gpios_count = ARRAY_SIZE(power_seq_intr_gpios);
@@ -136,12 +145,14 @@ const struct power_signal_gpio_info power_signal_gpio_list[] = {
 		.flags = POWER_SIGNAL_ACTIVE_HIGH,
 		.name = "SLP_S3_DEASSERTED",
 	},
+#if POWER_SEQ_GPIO_PRESENT(VR_EC_ALL_SYS_PWRGD)
 	{
 		.net_name = GPIO_NET_NAME(VR_EC_ALL_SYS_PWRGD),
 		.power_sig = X86_ALL_SYS_PGOOD,
 		.flags = POWER_SIGNAL_ACTIVE_HIGH,
 		.name = "ALL_SYS_PWRGD",
 	},
+#endif
 };
 
 const int power_signal_gpio_count = ARRAY_SIZE(power_signal_gpio_list);
@@ -174,7 +185,11 @@ void ap_off(void)
 /* This should be overridden if there is no power sequencer chip */
 __attribute__((weak)) int intel_x86_get_pg_ec_all_sys_pwrgd(void)
 {
+#if POWER_SEQ_GPIO_PRESENT(VR_EC_ALL_SYS_PWRGD)
 	return gpio_get_lvl(GPIO_NET_NAME(VR_EC_ALL_SYS_PWRGD));
+#else
+	return 0;
+#endif
 }
 
 /* Handle ALL_SYS_PWRGD signal
