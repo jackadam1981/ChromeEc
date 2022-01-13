309f91036abe4d7f26e0a95fad318144c2730f94
Eric Yilun Lin <yllin@chromium.org>
2022-01-13 13:37:52 +0800

    zephyr: kingler: enable bc12
    Enable C0 RT1718S and C1 PI3USB9201 BC12 driver.
    
    BUG=b:211363424
    TEST=Test C0 and C1 that DCP/CDP/SDP can be detected respectively
    BRANCH=none
    
    Change-Id: Iaf6afe6c4b59ae1598bfbccbfd3a15ee17f73926
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/projects/corsola/include/gpio_map.h
+++ zephyr/projects/corsola/include/gpio_map.h
@@ -44,6 +44,7 @@
 	#define TCPC_C0_INT()
 	#define TCPC_C1_INT()
 	#define PPC_C0_INT()
+	#define BC12_C0_INT()
 #elif defined(CONFIG_SOC_NPCX9M3F)
 	/* The interrupt is configured by dts */
 	#define AP_SPI_INT()
@@ -56,6 +57,9 @@
 	#define PPC_C0_INT()        GPIO_INT(GPIO_USB_C0_PPC_INT_ODL,          \
 					     GPIO_INT_EDGE_FALLING,            \
 					     ppc_interrupt)
+	#define BC12_C0_INT()       GPIO_INT(GPIO_USB_C0_BC12_INT_ODL,         \
+					     GPIO_INT_EDGE_FALLING,            \
+					     bc12_interrupt)
 #endif
 
 #ifdef CONFIG_PLATFORM_EC_TABLET_MODE
@@ -187,6 +191,7 @@ static inline void motion_interrupt(enum gpio_signal signal)
 	TCPC_C0_INT()							\
 	TCPC_C1_INT()							\
 	PPC_C0_INT()							\
+	BC12_C0_INT()							\
 	X_EC_GPIO2_INT()
 
 #endif /* __ZEPHYR_GPIO_MAP_H */
