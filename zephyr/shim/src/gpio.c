ba6ac421c2ad20215eb34fd6c0ef297dac27278f
Andrew McRae <amcrae@google.com>
2022-01-13 19:04:55 +1100

    zephyr: Initialise non-disabled GPIOs
    Allow GPIOs to be configured as 'inactive', and
    do not initialise them at the start.
    
    BUG=b:214333364
    TEST=zmake configure -b nivviks; flash & run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I98979f9c025bf4050b384d7e2c84271ff4e30022

--- zephyr/shim/src/gpio.c
+++ zephyr/shim/src/gpio.c
@@ -28,6 +28,8 @@ struct gpio_config {
 	gpio_pin_t pin;
 	/* From DTS, excludes interrupts flags */
 	gpio_flags_t init_flags;
+	/* From DTS, skips initialisation */
+	bool no_auto_init;
 };
 
 #define GPIO_CONFIG(id)                                                      \
@@ -39,6 +41,7 @@ struct gpio_config {
 				.dev = DEVICE_DT_GET(DT_PHANDLE(id, gpios)), \
 				.pin = DT_GPIO_PIN(id, gpios),               \
 				.init_flags = DT_GPIO_FLAGS(id, gpios),      \
+				.no_auto_init = DT_PROP(id, no_auto_init),   \
 			}, ),                                                \
 		())
 static const struct gpio_config configs[] = {
@@ -331,35 +334,35 @@ int gpio_get_default_flags(enum gpio_signal signal)
 static int init_gpios(const struct device *unused)
 {
 	gpio_flags_t flags;
-	struct jump_data *jdata;
-	bool is_sys_jumped;
+	struct jump_data *jdata = get_jump_data();
+	bool is_sys_jumped = (jdata && jdata->magic == JUMP_DATA_MAGIC);
 
 	ARG_UNUSED(unused);
 
-	jdata = get_jump_data();
+	/* Loop through all GPIOs with 'no-auto-init' not set to set
+	 * initial configuration.
+	 */
 
-	if (jdata && jdata->magic == JUMP_DATA_MAGIC)
-		is_sys_jumped = true;
-	else
-		is_sys_jumped = false;
-
-	/* Loop through all GPIOs in device tree to set initial configuration */
 	for (size_t i = 0; i < ARRAY_SIZE(configs); ++i) {
 		int rv;
+
+		if (configs[i].no_auto_init)
+			continue;
 
 		if (!device_is_ready(configs[i].dev))
 			LOG_ERR("Not found (%s)", configs[i].name);
 
 		/*
-		 * The configs[i].init_flags variable is read-only, so the
-		 * following assignment is needed because the flags need
-		 * adjusting on a warm reboot.
+		 * The configs[i].init_flags variable is read-only,
+		 * so the following assignment is needed because the
+		 * flags need adjusting on a warm reboot.
 		 */
 		flags = configs[i].init_flags;
 
 		if (is_sys_jumped) {
 			flags &=
-				~(GPIO_OUTPUT_INIT_LOW | GPIO_OUTPUT_INIT_HIGH);
+				~(GPIO_OUTPUT_INIT_LOW |
+				GPIO_OUTPUT_INIT_HIGH);
 		}
 
 		rv = gpio_pin_configure(configs[i].dev, configs[i].pin, flags);
