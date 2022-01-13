40a7a84a452e41897560cf5fa1a294c0b6d726d7
Andrew McRae <amcrae@google.com>
2022-01-13 14:44:29 +1100

    zephyr: Instantiate all GPIOs in gpio_config list
    Instantiate all GPIOs in the common gpio_config list.
    For GPIOs with no enum-name property, create an internal
    enum name from the ordinal.
    
    Test by checking that all GPIOs are seen via gpioget
    
    BUG=b:214316663
    TEST=zmake configure -b nivviks; flash and run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: If6193e25123d6e3cc85c89a2349e4fd384d27080

--- zephyr/shim/src/gpio.c
+++ zephyr/shim/src/gpio.c
@@ -30,17 +30,13 @@ struct gpio_config {
 	gpio_flags_t init_flags;
 };
 
-#define GPIO_CONFIG(id)                                                      \
-	COND_CODE_1(                                                         \
-		DT_NODE_HAS_PROP(id, enum_name),                             \
-		(                                                            \
-			{                                                    \
-				.name = DT_NODE_FULL_NAME(id),               \
-				.dev = DEVICE_DT_GET(DT_PHANDLE(id, gpios)), \
-				.pin = DT_GPIO_PIN(id, gpios),               \
-				.init_flags = DT_GPIO_FLAGS(id, gpios),      \
-			}, ),                                                \
-		())
+#define GPIO_CONFIG(id)                                              \
+	{                                                    \
+		.name = DT_NODE_FULL_NAME(id),               \
+		.dev = DEVICE_DT_GET(DT_PHANDLE(id, gpios)), \
+		.pin = DT_GPIO_PIN(id, gpios),               \
+		.init_flags = DT_GPIO_FLAGS(id, gpios),      \
+	},
 static const struct gpio_config configs[] = {
 #if DT_NODE_EXISTS(DT_PATH(named_gpios))
 	DT_FOREACH_CHILD(DT_PATH(named_gpios), GPIO_CONFIG)
