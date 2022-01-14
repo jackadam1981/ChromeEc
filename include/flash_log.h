dd66efcb641ddb0dbe4bdef4ec0c1088d5ceb0c9
Mary Ruthven <mruthven@chromium.org>
2022-01-14 09:30:20 -0600

    brdprop: log invalid and ambiguous events
    Log brdprop errors in flog, so the team can track brdprop errors from
    the AP without grepping through cr50 logs.
    
    BUG=b:214550629
    TEST=flash on red board. Verify invalid strap events are logged.
         enable closed-loop-reset on the red board. Verify "ambiguous" strap
         logs are ignored.
    
    Change-Id: Ibea73fb19119fa81ed3652c5d68e430cdbae9fa5
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- include/flash_log.h
+++ include/flash_log.h
@@ -25,6 +25,7 @@ enum flash_event_type {
 	FE_LOG_FIPS_FAILURE = 10, /* Error during continuous and/or known-answer
 				   * tests for FIPS 140-2/3
 				   */
+	FE_LOG_BRDPROP = 11, /* Detected invalid board properties */
 	/*
 	 * Fixed padding value makes it easier to parse log space
 	 * snapshots.
@@ -105,6 +106,27 @@ enum ap_ro_verification_ev {
 struct ap_ro_entry_payload {
 	enum ap_ro_verification_ev event : 8;
 } __packed;
+
+/*****************************************************************************/
+/* Brdprop Events */
+/* Each event can only be logged once per boot. */
+enum brdprop_ev {
+	BRDPROP_INVALID = 0,
+	BRDPROP_AMBIGUOUS = 1,
+	BRDPROP_NO_ENTRY = 2,
+
+	BRDPROP_COUNT = 3,
+};
+
+#define BRDPROP_EVENT         0
+#define BRDPROP_RESET_FLAGS   1
+#define BRDPROP_CONFIG_OFFSET 2
+
+struct brdprop_payload {
+	uint32_t events;
+	uint32_t reset_flags;
+	uint8_t configs[BRDPROP_COUNT];
+};
 
 /* Returned in the "type" field, when there is no entry available */
 #define FLASH_LOG_NO_ENTRY 0xff
