c0b259163f1ac6818a860d43c81e7b739aa82edf
Jeremy Bettis <jbettis@google.com>
2022-01-14 13:18:35 -0700

    zmake: Run lcov after test
    Change lcov & test to run sequentally, not in parallel. Also cleaned up
    function that is only called in one place.
    
    zmake coverage was broken by crrev/c/3385775
    
    BRANCH=None
    BUG=b:214323409
    TEST=zmake coverage build/zcoverage
    zmake configure --coverage zephyr/test/drivers && \
      zmake test --coverage build/zephyr/test-drivers
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: Ic0789b377f4be43afce50bab67bee7e17f79cd8f

--- zephyr/zmake/zmake/__main__.py
+++ zephyr/zmake/zmake/__main__.py
@@ -265,6 +265,13 @@ def get_argparser():
         type=pathlib.Path,
         help="The build directory used during configuration",
     )
+    test.add_argument(
+        "-c",
+        "--coverage",
+        action="store_true",
+        dest="coverage",
+        help="Run lcov after running test to generate coverage info file.",
+    )
 
     testall = sub.add_parser(
         "testall",
@@ -285,6 +292,12 @@ def get_argparser():
         "build_dir",
         type=pathlib.Path,
         help="The build directory used during configuration",
+    )
+    coverage.add_argument(
+        "--clobber",
+        action="store_true",
+        dest="clobber",
+        help="Delete existing build directories, even if configuration is unchanged",
     )
 
     generate_readme = sub.add_parser(
