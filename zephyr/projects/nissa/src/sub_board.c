c2318953e83869faf61b08e9d60e7b7d1ab24019
Andrew McRae <amcrae@google.com>
2022-01-15 15:39:17 +1100

    nissa: Add initial test code for handling interrupts in zephyr
    Add some initial code to use the device tree
    zephyr configuration and handling of interrupts.
    
    BUG=b:214608987
    TEST=zmake configure -b nivviks; flash and run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: Id63aaa2cbf7864697686c945304b919dcf82c143

--- zephyr/projects/nissa/src/sub_board.c
+++ zephyr/projects/nissa/src/sub_board.c
@@ -5,8 +5,14 @@
 
 /* Nissa sub-board selection */
 
+#include <device.h>
 #include <drivers/cros_cbi.h>
+#include <drivers/gpio.h>
+
 #include "console.h"
+#include "hooks.h"
+#include "gpio/gpio.h"
+#include "power_button.h"
 #include "sub_board.h"
 
 #define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
@@ -57,4 +63,77 @@ enum nissa_sub_board_type nissa_get_sb_type(void)
 		}
 	}
 	return sb;
+}
+__override struct gpio_int_config *
+	board_map_gpio_signal_to_interrupt(enum gpio_signal signal)
+{
+	switch (signal) {
+	default:
+		return 0;
+
+	case GPIO_WP_L:
+		return GPIO_INTERRUPT(int_wp_l);
+
+	case GPIO_POWER_BUTTON_L:
+		return GPIO_INTERRUPT(int_power_button);
+
+	case GPIO_LID_OPEN:
+		return GPIO_INTERRUPT(int_lid_open);
+
+	case GPIO_EC_IMU_INT_L:
+		return GPIO_INTERRUPT(int_imu);
+
+	case GPIO_VOLUME_DOWN_L:
+		return GPIO_INTERRUPT(int_vol_down);
+
+	case GPIO_VOLUME_UP_L:
+		return GPIO_INTERRUPT(int_vol_up);
+
+	case GPIO_CPU_PROCHOT:
+		return GPIO_INTERRUPT(int_prochot);
+	}
+}
+
+/*
+ * Temporary interrupt shims for testing.
+ */
+void switch_interrupt(enum gpio_signal sig);
+void lid_interrupt(enum gpio_signal sig);
+void lsm6dso_interrupt(enum gpio_signal sig);
+void button_interrupt(enum gpio_signal sig);
+void throttle_ap_prochot_input_interrupt(enum gpio_signal sig);
+
+void shim_interrupt_wp(void)
+{
+	switch_interrupt(GPIO_WP_L);
+}
+
+void shim_interrupt_lid(void)
+{
+	lid_interrupt(GPIO_LID_OPEN);
+}
+
+void shim_interrupt_lsm6dso(void)
+{
+	lsm6dso_interrupt(GPIO_EC_IMU_INT_L);
+}
+
+void shim_interrupt_vol_down(void)
+{
+	button_interrupt(GPIO_VOLUME_DOWN_L);
+}
+
+void shim_interrupt_vol_up(void)
+{
+	button_interrupt(GPIO_VOLUME_UP_L);
+}
+
+void shim_interrupt_prochot(void)
+{
+	throttle_ap_prochot_input_interrupt(GPIO_CPU_PROCHOT);
+}
+
+void shim_interrupt_power_button(void)
+{
+	power_button_interrupt(GPIO_POWER_BUTTON_L);
 }
