5a6fb2c932cf20e2c81555fa7cd7dcab165c9c04
Vijay Hiremath <vijay.p.hiremath@intel.com>
2022-01-14 19:35:29 -0800

    ccgxxf: SBU enable
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I06af101f4efa228163dc0a673887bdc368dbd504
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- driver/tcpm/ccgxxf.h
+++ driver/tcpm/ccgxxf.h
@@ -13,6 +13,18 @@
 #define CCGXXF_I2C_ADDR1_FLAGS	0x0B
 #define CCGXXF_I2C_ADDR2_FLAGS	0x1B
 
+#define CCGXXF_REG_SBU_MUX_CTL	0xBB
+
+/*
+ * Enable SBU lines.
+ *
+ * @param port Type-C port number
+ * @enable true for enable, false for disable
+ *
+ * @return EC_SUCCESS or error
+ */
+int ccgxxf_tcpc_set_sbu(int port, bool enable);
+
 /* CCGXXF built in I/O expander definitions */
 #ifdef CONFIG_IO_EXPANDER_CCGXXF
 
