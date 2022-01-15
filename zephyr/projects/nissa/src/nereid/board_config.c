046d8e816cd2df7784070a95dcee057f3361e95f
Andrew McRae <amcrae@google.com>
2022-01-15 15:39:17 +1100

    nissa: Add initial test code for handling interrupts in zephyr
    Add some initial code to use the device tree
    zephyr configuration and handling of interrupts.
    
    BUG=b:214608987
    TEST=zmake configure -b nivviks; flash and run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: Id63aaa2cbf7864697686c945304b919dcf82c143

--- zephyr/projects/nissa/src/nereid/board_config.c
+++ zephyr/projects/nissa/src/nereid/board_config.c
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-/* Nivviks sub-board hardware configuration */
+/* Nereid sub-board hardware configuration */
 
 #include <init.h>
 #include <kernel.h>
@@ -27,7 +27,7 @@ void board_interrupt_prochot(void)
 	throttle_ap_prochot_input_interrupt(GPIO_CPU_PROCHOT);
 }
 
-static void nivviks_subboard_init(void)
+static void nereid_subboard_init(void)
 {
 	enum nissa_sub_board_type sb = nissa_get_sb_type();
 
@@ -49,9 +49,6 @@ static void nivviks_subboard_init(void)
 		gpio_pin_configure_dt(&gpio_usb_c1_int_odl,
 				      GPIO_INPUT |
 				      gpio_usb_c1_int_odl.dt_flags);
-	} else {
-		/* Disable the port 1 charger task */
-		task_disable_task(TASK_ID_USB_CHG_P1);
 	}
 	if (sb == NISSA_SB_HDMI_A) {
 		/* Disable I2C_PORT_USB_C1_TCPC */
@@ -70,4 +67,4 @@ static void nivviks_subboard_init(void)
 				      GPIO_INPUT | gpio_hpd_sub_odl.dt_flags);
 	}
 }
-DECLARE_HOOK(HOOK_INIT, nivviks_subboard_init, HOOK_PRIO_FIRST+1);
+DECLARE_HOOK(HOOK_INIT, nereid_subboard_init, HOOK_PRIO_FIRST+1);
