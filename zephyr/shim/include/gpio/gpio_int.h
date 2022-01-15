102129f9a7a25f0bc24c028c31d89f121e6fe456
Andrew McRae <amcrae@google.com>
2022-01-15 11:40:11 +1100

    zephyr: Use DTS to configure gpio interrupts
    Use device tree to define and configure GPIO interrupts.
    
    Add an API to allow code to individually enable and disable interrupts.
    
    BUG=b:214608987
    TEST=zmake configure -b nivviks; flash and run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I3f448743aebb09565359d34ca655000c1e9c7474

---
+++ zephyr/shim/include/gpio/gpio_int.h
@@ -0,0 +1,62 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_SHIM_INCLUDE_GPIO_GPIO_INT_H_
+#define ZEPHYR_SHIM_INCLUDE_GPIO_GPIO_INT_H_
+
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+
+/*
+ * Zephyr based interrupt handling.
+ * Uses device tree to configure the interrupt handling.
+ */
+
+/*
+ * Creates an internal name for the interrupt config block.
+ */
+#define GPIO_NODE_TO_INTERRUPT(id) DT_CAT(gpio_interrupt_, id)
+
+/*
+ * Maps nodelabel of interrupt node to internal configuration block.
+ */
+#define GPIO_INTERRUPT(lbl)	(&GPIO_NODE_TO_INTERRUPT(DT_NODELABEL(lbl)))
+
+/*
+ * Forward reference to avoiding exposing internal structure
+ * defined in gpio_int.c
+ */
+struct gpio_int_config;
+/*
+ * Enable the interrupt.
+ *
+ * Interrupts are not automatically enabled, so
+ * each interrupt will need a call to activate the interrupt e.g
+ *
+ *   ... // set up device
+ *   gpio_interrupt_enable(GPIO_INTERRUPT(my_interrupt_node));
+ */
+void gpio_interrupt_enable(struct gpio_int_config *zc);
+
+/*
+ * Disable the interrupt.
+ */
+void gpio_interrupt_disable(struct gpio_int_config *zc);
+
+/*
+ * Declare interrupt configuration data structures.
+ */
+#define GPIO_INT_DECLARE(id)	\
+extern struct gpio_int_config GPIO_NODE_TO_INTERRUPT(id);
+
+#if DT_NODE_EXISTS(DT_PATH(gpio_interrupts))
+DT_FOREACH_CHILD(DT_PATH(gpio_interrupts), GPIO_INT_DECLARE)
+#endif
+
+#undef GPIO_INT_DECLARE
+#undef GPIO_INT_DECLARE_NODE
+
+#endif /* ZEPHYR_SHIM_INCLUDE_GPIO_GPIO_INT_H_ */
