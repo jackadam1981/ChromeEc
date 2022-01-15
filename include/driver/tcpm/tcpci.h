cdaa85e8e9ca8ff57661c54784f43de89d07cddc
Vijay Hiremath <vijay.p.hiremath@intel.com>
2022-01-14 19:35:29 -0800

    CCGXXF: Support enabling SBU FETs
    CCGXXF chip has SBU line port protection built inside the PD chip
    hence added code to enable/disable SBU FETs.
    To support the this vendor specific feature separated the CCGXXF
    driver structure from standard TCPCI driver.
    
    BUG=none
    BRANCH=none
    TEST=USB, DP, USB, TBT works on ADL-RVP
    
    Change-Id: I06af101f4efa228163dc0a673887bdc368dbd504
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- include/driver/tcpm/tcpci.h
+++ include/driver/tcpm/tcpci.h
@@ -320,7 +320,7 @@ int tcpci_tcpm_mux_get(const struct usb_mux *me, mux_state_t *mux_state);
 int tcpci_tcpm_mux_enter_low_power(const struct usb_mux *me);
 int tcpci_get_chip_info(int port, int live,
 			struct ec_response_pd_chip_info_v1 *chip_info);
-#ifdef CONFIG_USBC_PPC
+#ifdef CONFIG_USB_PD_PPC
 bool tcpci_tcpm_get_snk_ctrl(int port);
 int tcpci_tcpm_set_snk_ctrl(int port, int enable);
 bool tcpci_tcpm_get_src_ctrl(int port);
