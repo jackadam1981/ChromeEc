3f2d557bc36cef5ae845d3c142f166034d5b51bc
Andrew McRae <amcrae@google.com>
2022-01-15 15:39:17 +1100

    nissa: Migrate nissa to use the new interrupt framework
    Migrate nissa to use the new interrupt framework.
    
    BUG=b:214608987
    TEST=zmake configure -b nivviks; flash and run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: Id63aaa2cbf7864697686c945304b919dcf82c143

--- zephyr/projects/nissa/src/nereid/board_config.c
+++ zephyr/projects/nissa/src/nereid/board_config.c
@@ -3,21 +3,19 @@
  * found in the LICENSE file.
  */
 
-/* Nivviks sub-board hardware configuration */
+/* Nereid sub-board hardware configuration */
 
 #include <init.h>
 #include <kernel.h>
 #include <sys/printk.h>
 
-#include "gpio/gpio_int.h"
 #include "hooks.h"
-#include "usb_pd.h"
 #include "task.h"
 
 #include "gpios.h"
 #include "sub_board.h"
 
-static void nivviks_subboard_init(void)
+static void nereid_subboard_init(void)
 {
 	enum nissa_sub_board_type sb = nissa_get_sb_type();
 
@@ -39,9 +37,6 @@ static void nivviks_subboard_init(void)
 		gpio_pin_configure_dt(&gpio_usb_c1_int_odl,
 				      GPIO_INPUT |
 				      gpio_usb_c1_int_odl.dt_flags);
-	} else {
-		/* Disable the port 1 charger task */
-		task_disable_task(TASK_ID_USB_CHG_P1);
 	}
 	if (sb == NISSA_SB_HDMI_A) {
 		/* Disable I2C_PORT_USB_C1_TCPC */
@@ -60,22 +55,4 @@ static void nivviks_subboard_init(void)
 				      GPIO_INPUT | gpio_hpd_sub_odl.dt_flags);
 	}
 }
-DECLARE_HOOK(HOOK_INIT, nivviks_subboard_init, HOOK_PRIO_FIRST+1);
-
-/*
- * Enable interrupts
- */
-static void board_init(void)
-{
-	/*
-	 * Enable sensor interrupts
-	 */
-	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
-	/*
-	 * Enable USB-C interrupts.
-	 */
-	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0));
-	if (board_get_usb_pd_port_count() == 2)
-		gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c1));
-}
-DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, nereid_subboard_init, HOOK_PRIO_FIRST+1);
