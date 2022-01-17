c896ecb3965875269ce1f90f668a959bc6fe5992
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2022-01-17 16:07:02 +0800

    zephyr: it8xxx2: pinmux: read pinctrl-0 property instead of hardcoding
    Such as CL:3346769, to modify other I2C buses.
    
    BUG=none
    BRANCH=none
    TEST=zmake configure -b zephyr/projects/it8xxx2_evb/
    TEST=From the memory dump 0xf01600, check whether GPIO are set to
    I2C alternate function.
    
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>
    Change-Id: I71a77e2464bb1ccb1c418c39bb596b44091acb99

--- zephyr/shim/chip/it8xxx2/pinmux.c
+++ zephyr/shim/chip/it8xxx2/pinmux.c
@@ -43,81 +43,101 @@ static int it8xxx2_pinmux_init_latr(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c0), okay) && \
-	DT_NODE_HAS_STATUS(DT_NODELABEL(pinmuxb), okay)
-	{
-		const struct device *portb =
-				DEVICE_DT_GET(DT_NODELABEL(pinmuxb));
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c0), okay)
+	/* Pinmux control group */
+	const struct device *clk0_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c0, SCL);
+	const struct device *dat0_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c0, SDA);
+	/* GPIO pin */
+	uint8_t clk0_pin = IT8XXX2_I2C_DEV_PIN(i2c0, SCL);
+	uint8_t dat0_pin = IT8XXX2_I2C_DEV_PIN(i2c0, SDA);
+	/* Alternate function */
+	uint8_t clk0_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c0, SCL);
+	uint8_t dat0_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c0, SDA);
 
-		/* I2C0 CLK */
-		pinmux_pin_set(portb, 3, IT8XXX2_PINMUX_FUNC_1);
-		/* I2C0 DAT */
-		pinmux_pin_set(portb, 4, IT8XXX2_PINMUX_FUNC_1);
-	}
+	/* I2C0 CLK */
+	pinmux_pin_set(clk0_pinctrls, clk0_pin, clk0_alt);
+	/* I2C0 DAT */
+	pinmux_pin_set(dat0_pinctrls, dat0_pin, dat0_alt);
 #endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c1), okay) && \
-	DT_NODE_HAS_STATUS(DT_NODELABEL(pinmuxc), okay)
-	{
-		const struct device *portc =
-				DEVICE_DT_GET(DT_NODELABEL(pinmuxc));
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c1), okay)
+	/* Pinmux control group */
+	const struct device *clk1_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c1, SCL);
+	const struct device *dat1_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c1, SDA);
+	/* GPIO pin */
+	uint8_t clk1_pin = IT8XXX2_I2C_DEV_PIN(i2c1, SCL);
+	uint8_t dat1_pin = IT8XXX2_I2C_DEV_PIN(i2c1, SDA);
+	/* Alternate function */
+	uint8_t clk1_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c1, SCL);
+	uint8_t dat1_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c1, SDA);
 
-		/* I2C1 CLK */
-		pinmux_pin_set(portc, 1, IT8XXX2_PINMUX_FUNC_1);
-		/* I2C1 DAT */
-		pinmux_pin_set(portc, 2, IT8XXX2_PINMUX_FUNC_1);
-	}
+	/* I2C1 CLK */
+	pinmux_pin_set(clk1_pinctrls, clk1_pin, clk1_alt);
+	/* I2C1 DAT */
+	pinmux_pin_set(dat1_pinctrls, dat1_pin, dat1_alt);
 #endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c2), okay) && \
-	DT_NODE_HAS_STATUS(DT_NODELABEL(pinmuxf), okay)
-	{
-		const struct device *portf =
-				DEVICE_DT_GET(DT_NODELABEL(pinmuxf));
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c2), okay)
+	/* Pinmux control group */
+	const struct device *clk2_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c2, SCL);
+	const struct device *dat2_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c2, SDA);
+	/* GPIO pin */
+	uint8_t clk2_pin = IT8XXX2_I2C_DEV_PIN(i2c2, SCL);
+	uint8_t dat2_pin = IT8XXX2_I2C_DEV_PIN(i2c2, SDA);
+	/* Alternate function */
+	uint8_t clk2_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c2, SCL);
+	uint8_t dat2_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c2, SDA);
 
-		/* I2C2 CLK */
-		pinmux_pin_set(portf, 6, IT8XXX2_PINMUX_FUNC_1);
-		/* I2C2 DAT */
-		pinmux_pin_set(portf, 7, IT8XXX2_PINMUX_FUNC_1);
-	}
+	/* I2C2 CLK */
+	pinmux_pin_set(clk2_pinctrls, clk2_pin, clk2_alt);
+	/* I2C2 DAT */
+	pinmux_pin_set(dat2_pinctrls, dat2_pin, dat2_alt);
 #endif
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c3), okay)
 	/* Pinmux control group */
-	const struct device *clk_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c3, SCL);
-	const struct device *dat_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c3, SDA);
+	const struct device *clk3_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c3, SCL);
+	const struct device *dat3_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c3, SDA);
 	/* GPIO pin */
-	uint8_t clk_pin = IT8XXX2_I2C_DEV_PIN(i2c3, SCL);
-	uint8_t dat_pin = IT8XXX2_I2C_DEV_PIN(i2c3, SDA);
+	uint8_t clk3_pin = IT8XXX2_I2C_DEV_PIN(i2c3, SCL);
+	uint8_t dat3_pin = IT8XXX2_I2C_DEV_PIN(i2c3, SDA);
 	/* Alternate function */
-	uint8_t clk_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c3, SCL);
-	uint8_t dat_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c3, SDA);
+	uint8_t clk3_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c3, SCL);
+	uint8_t dat3_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c3, SDA);
 
 	/* I2C3 CLK */
-	pinmux_pin_set(clk_pinctrls, clk_pin, clk_alt);
+	pinmux_pin_set(clk3_pinctrls, clk3_pin, clk3_alt);
 	/* I2C3 DAT */
-	pinmux_pin_set(dat_pinctrls, dat_pin, dat_alt);
+	pinmux_pin_set(dat3_pinctrls, dat3_pin, dat3_alt);
 #endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c4), okay) && \
-	DT_NODE_HAS_STATUS(DT_NODELABEL(pinmuxe), okay)
-	{
-		const struct device *porte =
-				DEVICE_DT_GET(DT_NODELABEL(pinmuxe));
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c4), okay)
+	/* Pinmux control group */
+	const struct device *clk4_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c4, SCL);
+	const struct device *dat4_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c4, SDA);
+	/* GPIO pin */
+	uint8_t clk4_pin = IT8XXX2_I2C_DEV_PIN(i2c4, SCL);
+	uint8_t dat4_pin = IT8XXX2_I2C_DEV_PIN(i2c4, SDA);
+	/* Alternate function */
+	uint8_t clk4_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c4, SCL);
+	uint8_t dat4_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c4, SDA);
 
-		/* I2C4 CLK */
-		pinmux_pin_set(porte, 0, IT8XXX2_PINMUX_FUNC_3);
-		/* I2C4 DAT */
-		pinmux_pin_set(porte, 7, IT8XXX2_PINMUX_FUNC_3);
-	}
+	/* I2C4 CLK */
+	pinmux_pin_set(clk4_pinctrls, clk4_pin, clk4_alt);
+	/* I2C4 DAT */
+	pinmux_pin_set(dat4_pinctrls, dat4_pin, dat4_alt);
 #endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c5), okay) && \
-	DT_NODE_HAS_STATUS(DT_NODELABEL(pinmuxa), okay)
-	{
-		const struct device *porta =
-				DEVICE_DT_GET(DT_NODELABEL(pinmuxa));
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c5), okay)
+	/* Pinmux control group */
+	const struct device *clk5_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c5, SCL);
+	const struct device *dat5_pinctrls = IT8XXX2_I2C_DEV_PINMUX(i2c5, SDA);
+	/* GPIO pin */
+	uint8_t clk5_pin = IT8XXX2_I2C_DEV_PIN(i2c5, SCL);
+	uint8_t dat5_pin = IT8XXX2_I2C_DEV_PIN(i2c5, SDA);
+	/* Alternate function */
+	uint8_t clk5_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c5, SCL);
+	uint8_t dat5_alt = IT8XXX2_I2C_DEV_ALT_FUNC(i2c5, SDA);
 
-		/* I2C5 CLK */
-		pinmux_pin_set(porta, 4, IT8XXX2_PINMUX_FUNC_3);
-		/* I2C5 DAT */
-		pinmux_pin_set(porta, 5, IT8XXX2_PINMUX_FUNC_3);
-	}
+	/* I2C5 CLK */
+	pinmux_pin_set(clk5_pinctrls, clk5_pin, clk5_alt);
+	/* I2C5 DAT */
+	pinmux_pin_set(dat5_pinctrls, dat5_pin, dat5_alt);
 #endif
 
 	return 0;
