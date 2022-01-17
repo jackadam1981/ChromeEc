dd9beec04785b8a139b6bfaa5ed7da2c6db46caa
Andrew McRae <amcrae@google.com>
2022-01-17 21:44:05 +1100

    nissa: Convert GPIOs to use Zephyr API
    Convert the GPIO access to use the generated Zephyr API.
    
    BUG=none
    TEST=zmake configure -b nivviks; flash and run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: Iaadebe54e84aab431f289c2b897d85153af4820f

--- zephyr/projects/nissa/src/nivviks/board_config.c
+++ zephyr/projects/nissa/src/nivviks/board_config.c
@@ -9,7 +9,7 @@
 #include <kernel.h>
 #include <sys/printk.h>
 
-#include "gpios.h"
+#include "gpio.h"
 #include "hooks.h"
 #include "task.h"
 
@@ -25,20 +25,16 @@ static void nivviks_subboard_init(void)
 	 * Remove once common code initialises all GPIOs, not just
 	 * the ones with enum-names.
 	 */
-	gpio_pin_configure_dt(&gpio_usb_c0_int_odl,
-			      GPIO_INPUT | gpio_usb_c0_int_odl.dt_flags);
 	if (sb != NISSA_SB_C_A && sb != NISSA_SB_HDMI_A) {
 		/* Turn off unused USB A1 GPIOs */
-		gpio_pin_configure_dt(&gpio_sub_usb_a1_ilimit_sdp,
+		gpio_pin_configure_dt(DT_GPIO_SUB_USB_A1_ILIMIT_SDP,
 				      GPIO_DISCONNECTED);
-		gpio_pin_configure_dt(&gpio_en_sub_usb_a1_vbus,
+		gpio_pin_configure_dt(DT_GPIO_EN_SUB_USB_A1_VBUS,
 				      GPIO_DISCONNECTED);
 	}
 	if (sb == NISSA_SB_C_A || sb == NISSA_SB_C_LTE) {
 		/* Enable type-C port 1 */
-		gpio_pin_configure_dt(&gpio_usb_c1_int_odl,
-				      GPIO_INPUT |
-				      gpio_usb_c1_int_odl.dt_flags);
+		gpio_reset(GPIO_SIGNAL(DT_NODELABEL(gpio_usb_c1_int_odl)));
 	} else {
 		/* Disable the port 1 charger task */
 		task_disable_task(TASK_ID_USB_CHG_P1);
@@ -47,17 +43,10 @@ static void nivviks_subboard_init(void)
 		/* Disable I2C_PORT_USB_C1_TCPC */
 		/* TODO(b:212490923): Use pinctrl to switch from I2C */
 		/* Enable HDMI GPIOs */
-		gpio_pin_configure_dt(&gpio_en_sub_rails_odl,
-				      GPIO_OUTPUT |
-				      GPIO_OUTPUT_INIT_HIGH |
-				      gpio_en_sub_rails_odl.dt_flags);
-		gpio_pin_configure_dt(&gpio_hdmi_en_sub_odl,
-				      GPIO_OUTPUT |
-				      GPIO_OUTPUT_INIT_HIGH |
-				      gpio_hdmi_en_sub_odl.dt_flags);
+		gpio_reset(GPIO_SIGNAL(DT_NODELABEL(gpio_en_sub_rails_odl)));
+		gpio_reset(GPIO_SIGNAL(DT_NODELABEL(gpio_hdmi_en_sub_odl)));
 		/* Configure the interrupt separately */
-		gpio_pin_configure_dt(&gpio_hpd_sub_odl,
-				      GPIO_INPUT | gpio_hpd_sub_odl.dt_flags);
+		gpio_reset(GPIO_SIGNAL(DT_NODELABEL(gpio_hpd_sub_odl)));
 	}
 }
 DECLARE_HOOK(HOOK_INIT, nivviks_subboard_init, HOOK_PRIO_FIRST+1);
