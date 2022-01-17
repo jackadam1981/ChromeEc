bf1e23fc0aa2854a9e06a36622021c0e2b6f765e
Daisuke Nojiri <dnojiri@chromium.org>
2022-01-16 20:04:10 -0800

    Taniks: Enable RGB keyboard
    This patch enables the RGB keyboard module for Taniks.
    
    BUG=b:199995751
    BRANCH=None
    TEST=make BOARD=taniks
    
    Change-Id: I2d95bb2f56a5f8e6505cb902aa67ee05e053acf3
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/taniks/keyboard.c
+++ board/taniks/keyboard.c
@@ -6,6 +6,7 @@
 #include "common.h"
 #include "ec_commands.h"
 #include "keyboard_scan.h"
+#include "rgb_keyboard.h"
 #include "timer.h"
 
 /* Keyboard scan setting */
@@ -40,6 +41,26 @@ static const struct ec_response_keybd_config taniks_kb = {
 	},
 	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
 };
+
+extern struct rgbkbd_drv aw20198_drv;
+
+static struct rgb_s grid0[RGB_GRID0_COL * RGB_GRID0_ROW];
+
+struct rgbkbd rgbkbds[] = {
+	[0] = {
+		.cfg = &(const struct rgbkbd_cfg) {
+			.drv = &aw20198_drv,
+			.i2c = I2C_PORT_KBMCU,
+			.col_len = RGB_GRID0_COL,
+			.row_len = RGB_GRID0_ROW,
+		},
+		.buf = grid0,
+	},
+};
+const uint8_t rgbkbd_count = ARRAY_SIZE(rgbkbds);
+
+const uint8_t rgbkbd_hsize = RGB_GRID0_COL;
+const uint8_t rgbkbd_vsize = RGB_GRID0_ROW;
 
 __override const struct ec_response_keybd_config
 *board_vivaldi_keybd_config(void)
