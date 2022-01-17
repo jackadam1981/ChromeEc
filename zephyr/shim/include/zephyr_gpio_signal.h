02fde504973b1c35ff3920cad39ccab98a613fb1
Andrew McRae <amcrae@google.com>
2022-01-17 16:46:59 +1100

    zephyr: Refactor gpio_config to include gpio_dt_spec
    Add gpio_config to include gpio_dt_spec so that
    Zephyr based can directly use the initialised spec
    to access the GPIO API.
    
    BUG=b:214879219
    TEST=zmake testall
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I496669b5ed9caf1ab23c7a241353144dc5137048

--- zephyr/shim/include/zephyr_gpio_signal.h
+++ zephyr/shim/include/zephyr_gpio_signal.h
@@ -71,6 +71,42 @@ BUILD_ASSERT(GPIO_COUNT < GPIO_LIMIT);
 #define NAMED_GPIO_NODELABEL(label, prop) \
 	GPIO_SIGNAL(DT_PHANDLE(DT_NODELABEL(label), prop))
 
+/** @brief Converts a signal to a gpio_dt_spec pointer name.
+ *
+ * Prepend "DT_" to the the gpio_signal name to create a name that
+ * can be used as a pointer to gpio_dt_spec.
+ *
+ * For example, given the DTS node under "named-gpios":
+ *
+ * gpio_ec_wp_l: ec_wp_l {
+ * 	gpios = <&gpioe 5 GPIO_INPUT>;
+ *	enum-name = "GPIO_WP_L";
+ * };
+ *
+ * The following methods can all be used to access the GPIO:
+ *
+ * inp = gpio_get_level(GPIO_WP_L); // Legacy access
+ * inp = gpio_pin_get_dt(DT_GPIO_LID_OPEN); // Zephyr API
+ * inp = gpio_pin_get_dt(GPIO_DT_LABEL(gpio_ec_wp_l)); // Zephyr API
+ * enum gpio_signal sig = GPIO_WP_L;
+ * inp = gpio_pin_get_dt(gpio_get_dt_spec(sig)); // Zephyr API
+ */
+#define GPIO_DT_NAME(signal) DT_CAT(DT_, signal)
+
+#define GPIO_DT_LABEL(label) GPIO_DT_NAME(GPIO_SIGNAL(DT_NODELABEL(label)))
+/*
+ * Declare the pointers that refer to the gpio_dt_spec entries
+ * for each GPIO.
+ */
+struct gpio_dt_spec;
+
+#define GPIO_DT_PTR_DECL(id) extern const struct gpio_dt_spec * const \
+	GPIO_DT_NAME(GPIO_SIGNAL(id));
+
+DT_FOREACH_CHILD(DT_PATH(named_gpios), GPIO_DT_PTR_DECL)
+
+#undef GPIO_DT_PTR_DECL
+
 /*
  * Define enums for IO expanders and signals
  */
