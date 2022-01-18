96853cb35562fef1b2eeca341e61db7c512c343d
Al Semjonovs <asemjonovs@google.com>
2022-01-18 10:15:46 -0700

    zephyr: drivers: Improve code coverage of watchdog.c
    Add test cases to improve code coverage of watchdog.c
    
    BRANCH=none
    BUG=b:210148660
    TEST=zmake -D configure --coverage --test zephyr/test/drivers/
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: I90b0c0268fb75769180918e69f822d25007f14aa

---
+++ zephyr/test/drivers/src/watchdog.c
@@ -0,0 +1,45 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <device.h>
+#include <drivers/watchdog.h>
+#include <logging/log.h>
+#include <zephyr.h>
+#include <ztest.h>
+
+#include "config.h"
+#include "hooks.h"
+#include "watchdog.h"
+
+/** Setup watchdog  */
+static void setup_watchdog(void)
+{
+	set_test_runner_tid();
+}
+
+/** Restore watchdog */
+static void restore_watchdog(void)
+{
+	/* TODO */
+}
+
+/** Test watchdog init */
+static void test_watchdog_init(void)
+{
+	int retval = EC_SUCCESS;
+
+	/* Test successful initialization */
+	retval = watchdog_init();
+	zassert_equal(EC_SUCCESS, retval,
+				"Expected EC_SUCCESS, returned %u", retval);
+}
+
+void test_suite_watchdog(void)
+{
+	ztest_test_suite(watchdog,
+			 ztest_unit_test(test_watchdog_init));
+
+	ztest_run_test_suite(watchdog);
+}
