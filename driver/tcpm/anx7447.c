34c2f213224467e04fd5cde33eead6b2da7c3bea
Xin Ji <xji@analogixsemi.com>
2022-01-19 15:01:26 +0800

    tcpm: anx7447: Set vbus & vconn ADC threshold
    To avoid report VBUS_VOLTAGE_ALARM_HI while no device attached, set VBUS
    max threshold to around 25V. Set vconn maximum voltage threshold to 6V.
    
    BUG=b:153989733, b:214893572
    BRANCH=none
    TEST=Tested on STM32 EVB board
    
    Change-Id: Ieea3789cbb89a3c02a881cc6f1eda892072e273d
    Signed-off-by: Xin Ji <xji@analogixsemi.com>

--- driver/tcpm/anx7447.c
+++ driver/tcpm/anx7447.c
@@ -346,15 +346,14 @@ static int anx7447_init(int port)
 	if (rv)
 		return rv;
 
-	/*
-	 * Specifically disable voltage alarms, as VBUS_VOLTAGE_ALARM_HI may
-	 * trigger repeatedly despite being masked (b/153989733)
-	 */
-	rv = tcpc_update16(port, TCPC_REG_POWER_CTRL,
-			   TCPC_REG_POWER_CTRL_VBUS_VOL_MONITOR_DIS, MASK_SET);
+	/* Set VBUS_VOLTAGE_ALARM_HI threshold */
+	rv = tcpc_write16(port, TCPC_REG_VBUS_VOLTAGE_ALARM_HI_CFG, 0x3FF);
 	if (rv)
 		return rv;
-
+	/* Set VCONN_VOLTAGE_ALARM_HI threshold to 6V */
+	rv = tcpc_write16(port, VCONN_VOLTAGE_ALARM_HI_CFG, 0xF0);
+	if (rv)
+		return rv;
 	/* ADC enable, use to monitor VBUS voltage */
 	rv = tcpc_read(port, ANX7447_REG_ADC_CTRL_1, &reg);
 	if (rv)
