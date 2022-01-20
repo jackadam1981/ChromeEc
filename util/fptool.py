efdfd3d188c0a9539ff65d976d2bfa21fe486aa1
Ofer Fried <oferfried@google.com>
2022-01-20 15:02:52 -0800

    util/fptool.py: Convert flash_fp_mcu to fptool.py
    fptool.py becomes the main tool for FP communication
    This is the initial commit for transferring the functionality from the
    shell script flash_fp_mcu to Python.
    
    usage: fptool.py flash [-h] [-r | --noread]
                           [-U | --noremove_flash_read_protect]
                           [-u | --noremove_flash_write_protect]
    		       [-R RETRIES]
                           [-B BAUDRATE]
    		       [-H | --nohello]
    		       [-s | --noservices]
                           [--log_level {DEBUG,INFO,WARNING,ERROR,
    			CRITICAL}]
                           [binary]
    
           positional arguments:
             binary                Flash binary [ec.bin]
    
           optional arguments:
             -h, --help        show this help message and exit
             -r, --read
             --noread          Read instead of write (Default: False)
             -U, --remove_flash_read_protect
             --noremove_flash_read_protect
                               Remove flash read protection while
    			   performing command (Default: True)
             -u, --remove_flash_write_protect
             --noremove_flash_write_protect
                               Remove flash read protection while
    			   performing command (Default: True)
             -R RETRIES, --retries RETRIES
                               Specify number of retries (default: 4)
             -B BAUDRATE, --baudrate BAUDRATE
                               Specify UART baudrate (default: 115200)
             -H, --hello
             --nohello         Only ping the bootloader (Default: False)
             -s, --services
             --noservices      Stop and restart conflicting fingerprint
    	                   services (Default: True)
             -l, --log_level {DEBUG,INFO,WARNING,ERROR,CRITICAL}
    
    BUG=b:172020576
    TEST=None
    
    BRANCH=None
    Signed-off-by: Ofer Fried <oferfried@chromium.org>
    Change-Id: Ic1e435fdb4eca13fdd065e3188c024982bba1cad

--- util/fptool.py
+++ util/fptool.py
@@ -6,50 +6,75 @@
 """A tool to manage the fingerprint system on Chrome OS."""
 
 import argparse
-import os
-import shutil
-import subprocess
 import sys
 
 
-def cmd_flash(args: argparse.Namespace) -> int:
-    """Flash the entire firmware FPMCU using the native bootloader.
+def cmd_flash(args: argparse.Namespace):
+    """Flash the entire firmware FPMCU using the built-in bootloader.
 
     This requires the Chromebook to be in dev mode with hardware write protect
     disabled.
     """
 
-    if not shutil.which('flash_fp_mcu'):
-        print('Error - The flash_fp_mcu utility does not exist.')
-        return 1
 
-    cmd = ['flash_fp_mcu']
-    if args.image:
-        if not os.path.isfile(args.image):
-            print(f'Error - image {args.image} is not a file.')
-            return 1
-        cmd.append(args.image)
+def flash_init(parser):
+    flash_parser = parser.add_parser('flash', help=cmd_flash.__doc__)
+    group = flash_parser.add_mutually_exclusive_group()
+    group.add_argument('-r', '--read', action='store_true')
+    group.add_argument('--noread', dest='read', action='store_false',
+                       default=False,
+                       help='Read instead of write (Default: False)')
+    group = flash_parser.add_mutually_exclusive_group()
+    group.add_argument('-U', '--remove_flash_read_protect', action='store_true',
+                       default=True)
+    group.add_argument('--noremove_flash_read_protect',
+                       dest='remove_flash_read_protect', action='store_false',
+                       help='Remove flash read protection while performing '
+                       'command (Default: True)')
+    group = flash_parser.add_mutually_exclusive_group()
+    group.add_argument('-u', '--remove_flash_write_protect',
+                       action='store_true', default=True)
+    group.add_argument('--noremove_flash_write_protect',
+                       dest='remove_flash_write_protect', action='store_false',
+                       help='Remove flash read protection while performing '
+                       'command (Default: True)')
+    flash_parser.add_argument('-R', '--retries', type=int, default=4,
+                              help='Specify number of retries (default: '
+                              '%(default)s)')
+    flash_parser.add_argument('-B', '--baudrate', type=int, default=115200,
+                              help='Specify UART baudrate (default: '
+                              '%(default)s)')
+    group = flash_parser.add_mutually_exclusive_group()
+    group.add_argument('-H', '--hello', action='store_true')
+    group.add_argument('--nohello', dest='hello', action='store_false',
+                       default=False,
+                       help='Only ping the bootloader (Default: %(default)s)')
+    group = flash_parser.add_mutually_exclusive_group()
+    group.add_argument('-s', '--services', default=True, action='store_true')
+    group.add_argument('--noservices', dest='services', action='store_false',
+                       default=False,
+                       help='Stop and restart conflicting fingerprint services '
+                       '(Default: True)')
+    flash_parser.add_argument('binary', type=str, nargs='?',
+                              help='Flash binary [ec.bin]')
+    flash_parser.set_defaults(func=cmd_flash, connect_retries=6)
+    log_level_choices = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+    flash_parser.add_argument('--log_level', '-l', choices=log_level_choices,
+                              default='INFO')
 
-    print(f'Running {" ".join(cmd)}.')
-    sys.stdout.flush()
-    p = subprocess.run(cmd)  # pylint: disable=subprocess-run-check
-    return p.returncode
 
-
-def main(argv: list) -> int:
+def main() -> int:
     parser = argparse.ArgumentParser(description=__doc__)
     subparsers = parser.add_subparsers(dest='subcommand', title='subcommands')
     # This method of setting required is more compatible with older python.
     subparsers.required = True
 
-    # Parser for "flash" subcommand.
-    parser_decrypt = subparsers.add_parser('flash', help=cmd_flash.__doc__)
-    parser_decrypt.add_argument(
-        'image', nargs='?', help='Path to the firmware image')
-    parser_decrypt.set_defaults(func=cmd_flash)
-    opts = parser.parse_args(argv)
+    flash_init(subparsers)
+
+    opts = parser.parse_args()
+
     return opts.func(opts)
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main())
