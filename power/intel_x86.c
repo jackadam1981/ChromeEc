d1411b813ae185a50d4f0971030f76468500c8d4
Jon Murphy <jpmurphy@google.com>
2022-01-19 22:48:00 -0700

    power: Consolidate x86 power source
    Consolidate power_{chipset/board}_handle_host_sleep_event
    into common_x86
    
    BUG=b:179294969
    TEST=make -j100 buildall; zmake -j100 testall
    BRANCH=none
    
    Cq-Depend: chromium:3410532
    Signed-off-by: Jon Murphy <jpmurphy@google.com>
    Change-Id: I6dbbaca7988a20a1f8fc53b5425d93bb2bbfac31

--- power/intel_x86.c
+++ power/intel_x86.c
@@ -15,16 +15,7 @@
 #define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ##args)
 #define CPRINTF(format, args...) cprintf(CC_CHIPSET, format, ##args)
 
-enum sys_sleep_state {
-	SYS_SLEEP_S3,
-	SYS_SLEEP_S4,
-	SYS_SLEEP_S5,
-#ifdef CONFIG_POWER_S0IX
-	SYS_SLEEP_S0IX,
-#endif
-};
-
-static const int sleep_sig[] = {
+const int sleep_sig[] = {
 	[SYS_SLEEP_S3] = SLP_S3_SIGNAL_L,
 	[SYS_SLEEP_S4] = SLP_S4_SIGNAL_L,
 	[SYS_SLEEP_S5] = SLP_S5_SIGNAL_L,
@@ -479,57 +470,6 @@ void common_intel_x86_handle_rsmrst(enum power_state state)
 
 	board_after_rsmrst(rsmrst_in);
 }
-
-#ifdef CONFIG_POWER_TRACK_HOST_SLEEP_STATE
-
-__overridable void power_board_handle_host_sleep_event(
-		enum host_sleep_event state)
-{
-	/* Default weak implementation -- no action required. */
-}
-
-__override void power_chipset_handle_host_sleep_event(
-		enum host_sleep_event state,
-		struct host_sleep_event_context *ctx)
-{
-	power_board_handle_host_sleep_event(state);
-
-#ifdef CONFIG_POWER_S0IX
-	if (state == HOST_SLEEP_EVENT_S0IX_SUSPEND) {
-		/*
-		 * Indicate to power state machine that a new host event for
-		 * s0ix/s3 suspend has been received and so chipset suspend
-		 * notification needs to be sent to listeners.
-		 */
-		sleep_set_notify(SLEEP_NOTIFY_SUSPEND);
-
-		sleep_start_suspend(ctx, lpc_s0ix_hang_detected);
-		power_signal_enable_interrupt(sleep_sig[SYS_SLEEP_S0IX]);
-	} else if (state == HOST_SLEEP_EVENT_S0IX_RESUME) {
-		/*
-		 * Wake up chipset task and indicate to power state machine that
-		 * listeners need to be notified of chipset resume.
-		 */
-		sleep_set_notify(SLEEP_NOTIFY_RESUME);
-		task_wake(TASK_ID_CHIPSET);
-		lpc_s0ix_resume_restore_masks();
-		power_signal_disable_interrupt(sleep_sig[SYS_SLEEP_S0IX]);
-		sleep_complete_resume(ctx);
-		/*
-		 * If the sleep signal timed out and never transitioned, then
-		 * the wake mask was modified to its suspend state (S0ix), so
-		 * that the event wakes the system. Explicitly restore the wake
-		 * mask to its S0 state now.
-		 */
-		power_update_wake_mask();
-	} else if (state == HOST_SLEEP_EVENT_DEFAULT_RESET) {
-		power_signal_disable_interrupt(sleep_sig[SYS_SLEEP_S0IX]);
-	}
-#endif
-
-}
-
-#endif
 
 enum ec_error_list intel_x86_wait_power_up_ok(void)
 {
