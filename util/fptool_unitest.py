68449920a9b73c9af85c4d784b2699d869ae600d
Ofer Fried <oferfried@google.com>
2022-01-20 15:29:12 -0800

    util/fptool.py: Add device utils and unitest
    Add device utilities to get the FP device name for UART and SPI
    devices and an API to assert that the device is not write protected.
    
    BUG=b:172020576
    TEST=None
    
    BRANCH=None
    Signed-off-by: Ofer Fried <oferfried@chromium.org>
    Change-Id: Ifc6f281bfbf50d0a1b4ad151109f85d16f2255b6

---
+++ util/fptool_unitest.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unittest for fptool."""
+
+import unittest
+from unittest import mock
+
+import fptool
+
+fptool.logging = mock.Mock()
+
+_SPI_MODALIAS_GLOB = ['/sys/bus/spi/devices/spi-PRP0001:01/modalias',
+                      '/sys/bus/spi/devices/spi-PRP0001:02/modalias'
+                      ]
+
+_SPI_MODALIAS = ['of:Ns001TCgoogle,cr50',
+                 'of:NcrfpTCgoogle,cros-ec-spi'
+                 ]
+
+_UART_MODALIAS_GLOB = ['/sys/bus/serial/devices/serial0-0/modalias']
+
+_UART_MODALIAS = ['of:NcrfpTCgoogle,cros-ec-uart']
+
+_STRONGBAD_MODALIAS_GLOB = ['/sys/bus/spi/devices/spi0.0/modalias',
+                            '/sys/bus/spi/devices/spi10.0/modalias',
+                            '/sys/bus/spi/devices/spi12.0/modalias',
+                            '/sys/bus/spi/devices/spi6.0/modalias'
+                            ]
+
+_STRONGBAD_MODALIAS = ['spi:cr50',
+                       'spi:cros-ec-spi',
+                       'spi:spi-nor',
+                       'spi:cros-ec-spi'
+                       ]
+
+_DEV_NAME_GLOB_INPUT = '/sys/bus/platform/drivers/dw-apb-uart/*/*/serial0-0/'
+
+_DEV_NAME_GLOB_SINGLE = ['/sys/bus/platform/drivers/dw-apb-uart/AMD0020:01/'
+                         'serial0/serial0-0/'
+                         ]
+
+_DEV_NAME_GLOB_DOUBLE = ['/sys/bus/platform/drivers/dw-apb-uart/AMD0020:01/'
+                         'serial0/serial0-0/',
+                         '/sys/bus/platform/drivers/dw-apb-uart/AMD0020:02/'
+                         'serial0/serial0-0/'
+                         ]
+
+
+class AssertWpIsDisabledTest(unittest.TestCase):
+    """Test the assert_wp_is_disabled functionality"""
+
+    def test_assert_wp_is_disabled_crossystem_error(self):
+        with mock.patch('fptool.run_system_cmd') as mock_sys_cmd:
+            mock_sys_cmd.return_value = [1, None, None]
+            with self.assertRaises(SystemExit) as exit_trap:
+                fptool.assert_wp_is_disabled()
+            self.assertEqual(exit_trap.exception.code,
+                             fptool.ExitCode.EXIT_PRECONDITION)
+
+    def test_assert_wp_is_disabled_wp_enabled(self):
+        with mock.patch('fptool.run_system_cmd') as mock_sys_cmd:
+            mock_sys_cmd.return_value = [0, '1', None]
+            with self.assertRaises(SystemExit) as exit_trap:
+                fptool.assert_wp_is_disabled()
+            self.assertEqual(exit_trap.exception.code,
+                             fptool.ExitCode.EXIT_PRECONDITION)
+
+    def test_assert_wp_is_disabled_wp_disabled(self):
+        with mock.patch('fptool.run_system_cmd') as mock_sys_cmd:
+            mock_sys_cmd.return_value = [0, '0', None]
+            fptool.assert_wp_is_disabled()
+
+
+class ReadModaliasTest(unittest.TestCase):
+    """Test the correctness of reading the transport device ID"""
+
+    def test_spi_get_devid_no_modalias(self):
+        with mock.patch('glob.glob') as mock_glob:
+            mock_glob.return_value = []
+            ret = fptool.get_spiid()
+            mock_glob.assert_called_once_with('/sys/bus/spi/devices/*/modalias')
+            self.assertEqual(ret, '')
+
+    def test_get_uart_devid_no_modalias(self):
+        with mock.patch('glob.glob') as mock_glob:
+            mock_glob.return_value = []
+            ret = fptool.get_uartid()
+            mock_glob.assert_called_once_with('/sys/bus/serial/devices/*/'
+                                              'modalias')
+            self.assertEqual(ret, '')
+
+    def test_get_spi_devid(self):
+        with mock.patch('glob.glob') as mock_glob:
+            with mock.patch('fptool.readline') as mock_readline:
+                mock_glob.return_value = _SPI_MODALIAS_GLOB
+                mock_readline.side_effect = _SPI_MODALIAS
+                ret = fptool.get_spiid()
+                mock_glob.assert_called_once()
+                self.assertEqual(mock_readline.call_count, 2)
+                self.assertEqual(ret, 'spi-PRP0001:02')
+
+    def test_get_uart_devid(self):
+        with mock.patch('glob.glob') as mock_glob:
+            with mock.patch('fptool.readline') as mock_readline:
+                mock_glob.return_value = _UART_MODALIAS_GLOB
+                mock_readline.side_effect = _UART_MODALIAS
+                ret = fptool.get_uartid()
+                mock_glob.assert_called_once()
+                mock_readline.assert_called_once()
+                self.assertEqual(ret, 'serial0-0')
+
+    def test_get_strongbad_devid(self):
+        with mock.patch('glob.glob') as mock_glob:
+            with mock.patch('fptool.readline') as mock_readline:
+                mock_glob.return_value = _STRONGBAD_MODALIAS_GLOB
+                mock_readline.side_effect = _STRONGBAD_MODALIAS
+                ret = fptool.get_spiid()
+                mock_glob.assert_called_once()
+                self.assertEqual(mock_readline.call_count, 4)
+                self.assertEqual(ret, 'spi10.0')
+
+
+class GetUartDevNameTest(unittest.TestCase):
+    """Test the correctness of reading the UART device name"""
+
+    def test_get_uart_dev_name_no_device(self):
+        with mock.patch('glob.glob') as mock_glob:
+            mock_glob.return_value = []
+            dev_name = fptool.get_uart_dev_name('serial0-0')
+            mock_glob.assert_called_once_with(_DEV_NAME_GLOB_INPUT)
+            self.assertEqual(dev_name, '')
+
+    def test_get_uart_dev_name_two_devices(self):
+        with mock.patch('glob.glob') as mock_glob:
+            mock_glob.return_value = _DEV_NAME_GLOB_DOUBLE
+            dev_name = fptool.get_uart_dev_name('serial0-0')
+            mock_glob.assert_called_once_with(_DEV_NAME_GLOB_INPUT)
+            self.assertEqual(dev_name, '')
+
+    def test_get_uart_dev_name(self):
+        with mock.patch('glob.glob') as mock_glob:
+            mock_glob.return_value = _DEV_NAME_GLOB_SINGLE
+            dev_name = fptool.get_uart_dev_name('serial0-0')
+            mock_glob.assert_called_once_with(_DEV_NAME_GLOB_INPUT)
+            self.assertEqual(dev_name, 'AMD0020:01')
+
+
+if __name__ == '__main__':
+    unittest.main()
