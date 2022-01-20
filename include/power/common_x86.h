069a078f3567681d617b7c3497e94569d50adc83
Jon Murphy <jpmurphy@google.com>
2022-01-19 22:48:00 -0700

    power: Consolidate x86 power - consolidate host_sleep_event
    Consolidate power_{chipset/board}_handle_host_sleep_event
    into common_x86
    
    BUG=b:179294969
    TEST=make -j100 buildall; zmake -j100 testall
    BRANCH=none
    
    Signed-off-by: Jon Murphy <jpmurphy@google.com>
    Change-Id: I6dbbaca7988a20a1f8fc53b5425d93bb2bbfac31

--- include/power/common_x86.h
+++ include/power/common_x86.h
@@ -24,6 +24,40 @@
 #include "vboot.h"
 #include "wireless.h"
 
+/* GPIO for power signal */
+#ifdef CONFIG_HOSTCMD_ESPI_VW_SLP_S3
+#define SLP_S3_SIGNAL_L VW_SLP_S3_L
+#else
+#define SLP_S3_SIGNAL_L GPIO_PCH_SLP_S3_L
+#endif
+#ifdef CONFIG_HOSTCMD_ESPI_VW_SLP_S4
+#define SLP_S4_SIGNAL_L VW_SLP_S4_L
+#else
+#define SLP_S4_SIGNAL_L GPIO_PCH_SLP_S4_L
+#endif
+/*
+ * The SLP_S5 signal has not traditionally been connected to the EC. If virtual
+ * wire support is enabled, then SLP_S5 will be available that way. Otherwise,
+ * use SLP_S4's GPIO as a proxy for SLP_S5. This matches old behavior and
+ * effectively prevents S4 residency.
+ */
+#ifdef CONFIG_HOSTCMD_ESPI_VW_SLP_S5
+#define SLP_S5_SIGNAL_L VW_SLP_S5_L
+#else
+#define SLP_S5_SIGNAL_L SLP_S4_SIGNAL_L
+#endif
+
+enum sys_sleep_state {
+	SYS_SLEEP_S3,
+	SYS_SLEEP_S4,
+	SYS_SLEEP_S5,
+#ifdef CONFIG_POWER_S0IX
+	SYS_SLEEP_S0IX,
+#endif
+};
+
+extern const int sleep_sig[];
+
 #ifdef CONFIG_POWER_S0IX
 /*
  * Restore host event masks for SMI and SCI when host exits S0ix. This is done
