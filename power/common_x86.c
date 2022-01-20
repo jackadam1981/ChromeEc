fb65d459e124d03fa2656e9ddcecde55b814f0cf
Jon Murphy <jpmurphy@google.com>
2022-01-20 09:15:43 -0700

    power: Consolidate x86 power source
    Consolidate chipset_throttle_cpu into common_x86
    selected more robust macro implementation
    
    BUG=b:179294969
    TEST=make BOARD=guybrush;make BOARD=brya;make BOARD=trogdor
    BRANCH=none
    
    Signed-off-by: Jon Murphy <jpmurphy@google.com>
    Change-Id: I193983a0ea527499a2ce6ba1845f66eba441c868

--- power/common_x86.c
+++ power/common_x86.c
@@ -6,6 +6,7 @@
 /* X86 chipset power control module for Chrome EC */
 
 #include "chipset.h"
+#include "common.h"
 #include "console.h"
 #include "ec_commands.h"
 #include "gpio.h"
@@ -25,6 +26,17 @@
 #define CPUTS(outstr) cputs(CC_CHIPSET, outstr)
 #define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ##args)
 #define CPRINTF(format, args...) cprintf(CC_CHIPSET, format, ##args)
+
+void chipset_throttle_cpu(int throttle)
+{
+	CPRINTS("%s(%d)", __func__, throttle);
+
+	if (IS_ENABLED(CONFIG_CPU_PROCHOT_ACTIVE_LOW))
+		throttle = !throttle;
+
+	if (chipset_in_state(CHIPSET_STATE_ON))
+		gpio_set_level(GPIO_CPU_PROCHOT, throttle);
+}
 
 __overridable void x86_sys_reset_delay(void)
 {
@@ -155,7 +167,7 @@ void power_reset_host_sleep_state(void)
 	power_set_host_sleep_state(HOST_SLEEP_EVENT_DEFAULT_RESET);
 	sleep_reset_tracking();
 	power_chipset_handle_host_sleep_event(HOST_SLEEP_EVENT_DEFAULT_RESET,
-					      NULL);
+						  NULL);
 }
 
 #endif /* CONFIG_POWER_S0IX */
