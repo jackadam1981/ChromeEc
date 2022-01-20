b75364c015e477d74fcb67fe781314e842fbb9d4
Ofer Fried <oferfried@google.com>
2022-01-20 15:30:35 -0800

    util/fptool.py: Add platform and fingerprint utils
    Add utils to retrieve the platform's name the FP firmware and
    find if the FP is in use before re-flashing.
    
    BUG=b:172020576
    TEST=None
    
    BRANCH=None
    Signed-off-by: Ofer Fried <oferfried@chromium.org>
    Change-Id: Icca9d0209e426948d53b9d980a7aacfb8761a52c

--- util/fptool_unitest.py
+++ util/fptool_unitest.py
@@ -49,6 +49,78 @@ _DEV_NAME_GLOB_DOUBLE = ['/sys/bus/platform/drivers/dw-apb-uart/AMD0020:01/'
                          'serial0/serial0-0/'
                          ]
 
+_LSBVAL = ['CHROMEOS_RELEASE_APPID={0BE68F68-A2F2-46B7-A7B4-B51B63F64FBA}',
+           '# Normal line.',
+           'SOME_KEY=value',
+           '# Key and value with leading/trailing whitespace.',
+           '        WS_KEY  =    value       ',
+           '# Value with whitespace in the middle.',
+           'WS_VALUE = v a l u e ',
+           '# Value with quotes don\'t get removed.',
+           'DOUBLE_QUOTES = \"double\"',
+           'SINGLE_QUOTES = \'sin gle\'',
+           'RANDOM_QUOTES = \'\"',
+           'CHROMEOS_BOARD_APPID={0BE68F68-A2F2-46B7-A7B4-B51B63F64FBA}',
+           'CHROMEOS_CANARY_APPID={90F229CE-83E2-4FAF-8479-E368A34938B1}',
+           'DEVICETYPE=CHROMEBOOK', 'CHROMEOS_RELEASE_NAME=Chrome OS',
+           'CHROMEOS_AUSERVER=https://tools.google.com/service/update2',
+           'CHROMEOS_DEVSERVER=',
+           'CHROMEOS_ARC_VERSION=7906507',
+           'CHROMEOS_ARC_ANDROID_SDK_VERSION=28',
+           'CHROMEOS_RELEASE_BUILDER_PATH=zork-release/R98-14346.0.0',
+           'CHROMEOS_RELEASE_KEYSET=devkeys',
+           'CHROMEOS_RELEASE_TRACK=testimage-channel',
+           'CHROMEOS_RELEASE_BUILD_TYPE=Official Build',
+           'CHROMEOS_RELEASE_DESCRIPTION=14346.0.0 (Official Build) '
+           'dev-channel zork test',
+           'CHROMEOS_RELEASE_BOARD=zork',
+           'CHROMEOS_RELEASE_BRANCH_NUMBER=0',
+           'CHROMEOS_RELEASE_BUILD_NUMBER=14346',
+           'CHROMEOS_RELEASE_CHROME_MILESTONE=98',
+           'CHROMEOS_RELEASE_PATCH_NUMBER=0',
+           'CHROMEOS_RELEASE_VERSION=14346.0.0',
+           'GOOGLE_RELEASE=14346.0.0',
+           'CHROMEOS_RELEASE_UNIBUILD=1'
+           ]
+
+_LSBVAL_NO_PLATFORM_NAME = ['CHROMEOS_ARC_VERSION=7906507',
+                            'CHROMEOS_ARC_ANDROID_SDK_VERSION=28',
+                            'CHROMEOS_RELEASE_KEYSET=devkeys',
+                            'CHROMEOS_RELEASE_TRACK=testimage-channel',
+                            ]
+
+_LSBVAL_NORMAL = ['# Normal line.',
+                  'SOME_KEY=value'
+                  ]
+
+_LSBVAL_LEADING_SPACES = ['# Key and value with leading/trailing whitespace.',
+                          '        WS_KEY  =    value       ',
+                          ]
+
+_LSBVAL_SPACED_VALUE = ['# Value with whitespace in the middle.',
+                        'WS_VALUE = v a l u e ',
+                        ]
+
+_LSBVAL_QUOTED_VALUES = ['# Value with quotes don\'t get removed.',
+                         'DOUBLE_QUOTES = \"double\"',
+                         'SINGLE_QUOTES = \'sin gle\'',
+                         'RANDOM_QUOTES = \'\"',
+                         ]
+
+_DEFAULT_FIRMWARE = ['/opt/google/biod/fw/bloonchipper_v2.0.5938-197506c1-RO_v2'
+                     '.0.10551-87ebfe5-RW.bin'
+                     ]
+
+_TWO_DEFAULT_FIRMWARES = ['/opt/google/biod/fw/bloonchipper_v1.0.5938-197506c1-'
+                          'RO_v1.0.10551-87ebfe5-RW.bin',
+                          '/opt/google/biod/fw/bloonchipper_v2.0.5938-197506c1-'
+                          'RO_v2.0.10551-87ebfe5-RW.bin'
+                          ]
+
+_FIRMWARE_GLOB_INPUT_NO_BOARD = '/opt/google/biod/fw/*.bin'
+
+_FIRMWARE_GLOB_INPUT_BLOONCHIPPER = '/opt/google/biod/fw/bloonchipper*.bin'
+
 
 class AssertWpIsDisabledTest(unittest.TestCase):
     """Test the assert_wp_is_disabled functionality"""
@@ -149,6 +221,127 @@ class GetUartDevNameTest(unittest.TestCase):
             dev_name = fptool.get_uart_dev_name('serial0-0')
             mock_glob.assert_called_once_with(_DEV_NAME_GLOB_INPUT)
             self.assertEqual(dev_name, 'AMD0020:01')
+
+
+class lsbvalTest(unittest.TestCase):
+    """Test the correctness of the lsbval parser"""
+
+    def test_leading_spaces(self):
+        with mock.patch('fptool.read_lsbval') as mock_lsbval:
+            mock_lsbval.return_value = _LSBVAL_LEADING_SPACES
+            value = fptool.lsbval('WS_KEY')
+            self.assertEqual(value, 'value')
+
+    def test_spaced_value(self):
+        with mock.patch('fptool.read_lsbval') as mock_lsbval:
+            mock_lsbval.return_value = _LSBVAL_SPACED_VALUE
+            value = fptool.lsbval('WS_VALUE')
+            self.assertEqual(value, 'v a l u e')
+
+    def test_double_quoted_value(self):
+        with mock.patch('fptool.read_lsbval') as mock_lsbval:
+            mock_lsbval.return_value = _LSBVAL_QUOTED_VALUES
+            value = fptool.lsbval('DOUBLE_QUOTES')
+            self.assertEqual(value, '\"double\"')
+
+    def test_single_quoted_value(self):
+        with mock.patch('fptool.read_lsbval') as mock_lsbval:
+            mock_lsbval.return_value = _LSBVAL_QUOTED_VALUES
+            value = fptool.lsbval('SINGLE_QUOTES')
+            self.assertEqual(value, '\'sin gle\'')
+
+    def test_random_quotes(self):
+        with mock.patch('fptool.read_lsbval') as mock_lsbval:
+            mock_lsbval.return_value = _LSBVAL_QUOTED_VALUES
+            value = fptool.lsbval('RANDOM_QUOTES')
+            self.assertEqual(value, '\'\"')
+
+    def test_lsbval(self):
+        with mock.patch('fptool.read_lsbval') as mock_lsbval:
+            mock_lsbval.return_value = _LSBVAL
+            value = fptool.lsbval('CHROMEOS_RELEASE_BUILD_NUMBER')
+            self.assertEqual(value, '14346')
+
+
+class platformNameTest(unittest.TestCase):
+    """Test the lsbval parser for fetching the platform's name"""
+
+    def test_no_platform_name(self):
+        with mock.patch('fptool.read_lsbval') as mock_lsbval:
+            mock_lsbval.return_value = _LSBVAL_NO_PLATFORM_NAME
+            platform_name = fptool.get_platform_name()
+            self.assertEqual(platform_name, '')
+
+    def test_get_platform_name(self):
+        with mock.patch('fptool.read_lsbval') as mock_lsbval:
+            mock_lsbval.return_value = _LSBVAL
+            platform_name = fptool.get_platform_name()
+            self.assertEqual(platform_name, 'zork')
+
+
+class PlatformBaseNameTest(unittest.TestCase):
+    """Test the correctness of the platform base name parser"""
+
+    def test_get_platform_base_name(self):
+        base_name = fptool.get_platform_base_name('hatch')
+        self.assertEqual(base_name, 'hatch')
+        base_name = fptool.get_platform_base_name('hatch-kernel')
+        self.assertEqual(base_name, 'hatch')
+        base_name = fptool.get_platform_base_name('hatch-arc-c')
+        self.assertEqual(base_name, 'hatch')
+
+
+class GetDefaultFirmwareTest(unittest.TestCase):
+    """Test the get_default_firmware function"""
+
+    def test_cros_config_error(self):
+        with mock.patch('fptool.run_system_cmd') as mock_sys_cmd:
+            mock_sys_cmd.return_value = [1, None, None]
+            with mock.patch('glob.glob') as mock_glob:
+                mock_glob.return_value = []
+                with self.assertRaises(SystemExit) as exit_trap:
+                    fptool.get_default_firmware()
+                assert isinstance(exit_trap.exception, SystemExit)
+                self.assertEqual(exit_trap.exception.code,
+                                 fptool.ExitCode.EXIT_CONFIG)
+                mock_glob.assert_called_once_with(_FIRMWARE_GLOB_INPUT_NO_BOARD)
+
+    def test_multiple_fingerprint_firmwares(self):
+        with mock.patch('fptool.run_system_cmd') as mock_sys_cmd:
+            mock_sys_cmd.return_value = [1, None, None]
+            with mock.patch('glob.glob') as mock_glob:
+                mock_glob.return_value = _TWO_DEFAULT_FIRMWARES
+                with self.assertRaises(SystemExit) as exit_trap:
+                    fptool.get_default_firmware()
+                assert isinstance(exit_trap.exception, SystemExit)
+                self.assertEqual(exit_trap.exception.code,
+                                 fptool.ExitCode.EXIT_CONFIG)
+                mock_glob.assert_called_once_with(_FIRMWARE_GLOB_INPUT_NO_BOARD)
+
+    def test_no_fingerprint_firmwares(self):
+        with mock.patch('fptool.run_system_cmd') as mock_sys_cmd:
+            mock_sys_cmd.return_value = [0, None, None]
+            with mock.patch('glob.glob') as mock_glob:
+                mock_glob.return_value = []
+                with self.assertRaises(SystemExit) as exit_trap:
+                    fptool.get_default_firmware()
+                assert isinstance(exit_trap.exception, SystemExit)
+                self.assertEqual(exit_trap.exception.code,
+                                 fptool.ExitCode.EXIT_CONFIG)
+                mock_glob.assert_called_once_with(_FIRMWARE_GLOB_INPUT_NO_BOARD)
+
+    def test_get_default_firmware(self):
+        with mock.patch('fptool.run_system_cmd') as mock_sys_cmd:
+            mock_sys_cmd.return_value = [0, 'bloonchipper', None]
+            with mock.patch('glob.glob') as mock_glob:
+                mock_glob.return_value = []
+                try:
+                    with self.assertRaises(SystemExit):
+                        default_firmware = fptool.get_default_firmware()
+                except AssertionError:
+                    mock_glob.assert_called_once_with(
+                        _FIRMWARE_GLOB_INPUT_BLOONCHIPPER)
+                    self.assertEqual(default_firmware, _DEFAULT_FIRMWARE)
 
 
 if __name__ == '__main__':
