4ad5d8fadf54fb32ab6864d45e952a3b3d436141
Ofer Fried <oferfried@google.com>
2022-01-20 15:30:35 -0800

    biod/fptool.py: Add more device utils
    Device utils:
            proc_open_files
            get_default_fw
            get_platform_base_name
            get_platform_name
    
    BUG=b:172020576
    TEST=None
    
    BRANCH=None
    Signed-off-by: Bhanu Prakash Maiya <bhanumaiya@chromium.org>
    Change-Id: Icca9d0209e426948d53b9d980a7aacfb8761a52c

--- util/fptool.py
+++ util/fptool.py
@@ -95,7 +95,7 @@ def get_devid(dev_type, dev_str) -> str:
 def get_spiid() -> str:
     """Get the spiid for the fingerprint sensor based on the modalias string.
 
-       see: https://crbug.com/955117
+    see: https://crbug.com/955117
     """
     return get_devid('spi', 'cros-ec-spi')
 
@@ -108,11 +108,11 @@ def get_uartid() -> str:
 def get_uart_dev_name(device_id: str) -> str:
     """Find the UART device associated with the device ID.
 
-       e.g. Zork
-         Device ID: serial0-0
-         Device association:
-           /sys/bus/platform/drivers/dw-apb-uart/AMD0020:01/serial0/serial0-0/
-         Device Name: AMD0020:01
+    e.g. Zork
+        Device ID: serial0-0
+        Device association:
+            /sys/bus/platform/drivers/dw-apb-uart/AMD0020:01/serial0/serial0-0/
+        Device Name: AMD0020:01
     """
     path = '/sys/bus/platform/drivers/dw-apb-uart/'
     dirs = glob.glob(f'{path}*/*/{device_id}/')
@@ -124,6 +124,111 @@ def get_uart_dev_name(device_id: str) -> str:
         return ''
 
     return os.path.basename(os.path.dirname(os.path.dirname(dirs[0][:-1])))
+
+
+def get_platform_name() -> str:
+    """Get the reference design name.
+
+    Get the underlying board (reference design) that we're running on
+    (not the FPMCU or sensor).
+    This may be an extended platform name, like nami-kernelnext, hatch-arc-r,
+    or hatch-borealis.
+    """
+    # We used to use "cros_config /identity platform-name", but that is specific
+    # to mosys and does not actually provide the board name in all cases.
+    # cros_config intentionally does not provide a way to get the board
+    # name: b/156650654.
+
+    # If there was a way to get the board name from cros_config, it's possible
+    # that it could fail in the following cases:
+    #
+    # 1) We're running on a non-unibuild device (the only one with FP is
+    #    nocturne)
+    # 2) We're running on a proto device during bringup and the cros_config
+    #    settings haven't yet been setup.
+    #
+    # In all cases we can fall back to /etc/lsb-release. It's not recommended
+    # to do this, but we don't have any other options in this case.
+
+    # lsbval should not be used by anything except get_platform_name.
+    # See https://crbug.com/98462.
+    def lsbval(key) -> str:
+        try:
+            with open('/etc/lsb-release', 'r') as fp:
+                for line in fp.readlines():
+                    line = line.rstrip()
+                    keyval = line.split('=')
+                    if key == keyval[0]:
+                        return keyval[1]
+                logging.warning('Failed to find %s in /etc/lsb-release', key)
+        except OSError:
+            logging.warning('--------------- Error reading /etc/lsb-release')
+            logging.warning('\t\tOSError: %s', sys.exc_info()[1].strerror)
+
+    logging.info('Getting platform name from /etc/lsb-release.')
+    return lsbval('CHROMEOS_RELEASE_BOARD')
+
+
+def get_platform_base_name(platform_name: str) -> str:
+    """Given a full platform name, extract the base platform.
+
+    Tests are also run on modified images, like hatch-arc-r, hatch-borealis,
+    or hatch-kernelnext. These devices still have fingerprint and are
+    expected to pass tests. The full platform name reflects these
+    modifications and might be needed to apply an alternative configuration
+    (kernelnext). Other modified tests (arc-r) just need to default to the
+    base platform config, which is identified by this function.
+    See b/186697064.
+
+    Examples:
+        * platform_base_name "hatch-kernelnext" --> "hatch"
+        * platform_base_name "hatch-arc-r"      --> "hatch"
+        * platform_base_name "hatch-borealis"   --> "hatch"
+        * platform_base_name "hatch"            --> "hatch"
+    """
+    return platform_name.split('-')[0]
+
+
+def get_default_fw() -> str:
+    rc, board, _stderr = run_system_cmd('cros_config /fingerprint board')
+    if rc != 0:
+        logging.warning('Failed to identify fingerprint board name')
+    # If cros_config returns "", that is okay assuming there is only
+    # one firmware file on disk.
+    if not board:
+        board = ''
+    fws = glob.glob('/opt/google/biod/fw/' + board + '*.bin')
+    if len(fws) == 0:
+        logging.error('Failed to identify the default fingerprint fw name')
+        sys.exit(ExitCode.EXIT_CONFIG)
+    if len(fws) != 1:
+        logging.error('Multiple fingerprint fw names')
+        sys.exit(ExitCode.EXIT_CONFIG)
+    return fws[0]
+
+
+def proc_open_files(*args) -> list:
+    """Find processes that have the named file, active or deleted, open.
+
+    Deleted files are important because unbinding/rebinding cros-ec
+    with biod/timberslide running will result in the processes holding open
+    a deleted version of the files. Issues can arise if the process continue
+    to interact with the deleted files (e.g. kernel panic) while the raw
+    driver is being used in flash_fp_mcu. The lsof and fuser tools can't
+    seem to identify usages of the deleted named file directly, without
+    listing all files. This takes a large amount of time on Chromebooks,
+    thus we need this custom search routine.
+    """
+    pids = []
+    procs = glob.glob('/proc/*/fd/*')
+    for proc in procs:
+        if os.access(proc, os.F_OK):
+            link = os.readlink(proc)
+            for pattern in args:
+                if re.search(pattern, link):
+                    pids.append('PID {} -> {}'.format(proc.split('/')[2], link))
+                    break
+    return pids
 
 
 def cmd_flash(args: argparse.Namespace):
