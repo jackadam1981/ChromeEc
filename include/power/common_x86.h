b45ba9e4d82cc2356f70bcb49ba833478b7e2f9a
Jon Murphy <jpmurphy@google.com>
2022-01-20 17:32:37 -0700

    power: Consolidate x86 power source
    Consolidate power up functions when CONFIG_CHARGER is defined
    
    BUG=b:179294969
    TEST=make BOARD=guybrush;make BOARD=brya;make BOARD=trogdor
    BRANCH=none
    
    Signed-off-by: Jon Murphy <jpmurphy@google.com>
    Change-Id: Id853ff476e83bea37b34689a356edd2bd0c45f3c

--- include/power/common_x86.h
+++ include/power/common_x86.h
@@ -59,6 +59,21 @@ enum sys_sleep_state {
 #endif
 };
 
+void power_up_inhibited_cb(void);
+
+/**
+ * Wait for power-up to be allowed based on available power.
+ *
+ * This delays G3->S5 until there is enough power to boot the AP, waiting
+ * first until the charger (if any) is ready, then for there to be sufficient
+ * power.
+ *
+ * In case of error, the caller should not allow power-up past G3.
+ *
+ * @return EC_SUCCESS if OK.
+ */
+enum ec_error_list x86_wait_power_up_ok(void);
+
 /**
  * Provides custom logic for passthrough signals
  *
