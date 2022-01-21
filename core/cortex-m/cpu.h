817d2d4763a32915fede500231e3826c85ecfa7b
Patryk Duda <pdk@semihalf.com>
2022-01-21 12:14:00 +0100

    cortex-m: Introduce FPU interrupt handler
    When performing some illegal operation or when result can't be
    represented using floats, the FPU will assert an interrupt which
    should be handled.
    
    After this change, the EC will inform about type of FPU exception
    and address where it ocurred. To reduce overhead, the FPU handler will
    only copy necessary information, schedule fpu_warn() function and clear
    FPU flags. Message is printed from fpu_warn() which is deferred function
    (it's called from HOOK task context).
    
    Please note that:
    - FPU interrupt is not asserted immediately after problem occurred, but
      with noticeable delay, so PC and LR might not be correct.
    - FPU interrupt will be never triggered on STM32H7 (see errata ES0392
      Rev 8, 2.1.2 Cortex-M7 FPU interrupt not present on NVIC line 81).
    
    BUG=b:215606535
    BRANCH=none
    TEST=./test/run_device_tests.py --board bloonchipper --tests cortexm_fpu
    TEST=./test/run_device_tests.py --board dartmonkey --tests cortexm_fpu
    TEST=To test that FPU interrupts are reported correctly, it's necessary
         to add the following function:
         static float divf(float a, float b) {
            float result;
            asm volatile(
                    "fdivs %0, %1, %2"
                    : "=w"(result)
                    : "w"(a),  "w"(b)
            );
    
            return result;
         }
    
         Then add following console command:
         static int command_test(int argc, char **argv)
         {
            divf(1.40130e-45f, 2.0f);
            usleep(1000000);
    
            divf(3.40282e38f, 0.5f);
            usleep(1000000);
    
            divf(1.0f, 0.0f);
            usleep(1000000);
    
            sqrtf(-1.0f);
            usleep(1000000);
    
            return EC_SUCCESS;
         }
         DECLARE_CONSOLE_COMMAND(test, command_test, "", "");
    
         Compile and flash firmware on device with FPU enabled
         (eg. bloonchipper). Call "test" command from console.
         Following result should appear:
    
         ### FPU exception at PC=08061aee LR=0806918b (task 4) ###
         ### FPSCR=00000018 => Underflow Inexact ###
    
         ### FPU exception at PC=08069184 LR=08068243 (task 4) ###
         ### FPSCR=00000014 => Overflow Inexact ###
    
         ### FPU exception at PC=08069186 LR=08068255 (task 4) ###
         ### FPSCR=00000012 => Division By Zero Inexact ###
    
         ### FPU exception at PC=08069186 LR=08068263 (task 4) ###
         ### FPSCR=00000011 => Invalid Operation Inexact ###
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: Ib6f6c974082affc35302a822f0beea176e204206

--- core/cortex-m/cpu.h
+++ core/cortex-m/cpu.h
@@ -70,6 +70,22 @@ enum {
 #define CPU_SCB_DCISW          CPUREG(0xe000ef60)
 #define CPU_SCB_DCCISW         CPUREG(0xe000ef74)
 
+/* Floating Point Context Address Register */
+#define CPU_FPU_FPCAR          CPUREG(0xe000ef38)
+
+/*
+ * As defined by Armv7-M Reference Manual B1.5.7 "Context state stacking on
+ * exception entry with the FP extension" the structure of the FPU state is:
+ * s0, s1, ..., s14, s15, fpscr.
+ */
+#define FPU_IDX_REG_FPSCR 16
+#define FPU_FPSCR_IOC (1u << 0) /* Invalid operation */
+#define FPU_FPSCR_DZC (1u << 1) /* Division by zero */
+#define FPU_FPSCR_OFC (1u << 2) /* Overflow */
+#define FPU_FPSCR_UFC (1u << 3) /* Underflow */
+#define FPU_FPSCR_IXC (1u << 4) /* Inexact */
+#define FPU_FPSCR_IDC (1u << 7) /* Input denormal */
+
 /* Bitfield values for EXC_RETURN. */
 #define EXC_RETURN_ES_MASK	BIT(0)
 #define EXC_RETURN_ES_NON_SECURE 0
---
+++ test/cortexm_fpu.c
@@ -0,0 +1,136 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "test_util.h"
+
+#include "cpu.h"
+#include "math.h"
+#include "math_util.h"
+#include "time.h"
+
+#ifdef CONFIG_FPU
+
+volatile uint32_t fpscr;
+
+/* Override default FPU interrupt handler */
+void __keep fpu_irq(uint32_t excep_lr, uint32_t excep_sp)
+{
+	/*
+	 * Get address of exception FPU exception frame. FPCAR register points
+	 * to the beginning of allocated FPU exception frame on the stack.
+	 */
+	uint32_t *fpu_state = (uint32_t *)CPU_FPU_FPCAR;
+
+	fpscr = fpu_state[FPU_IDX_REG_FPSCR];
+
+	/* Clear FPSCR on stack */
+	fpu_state[FPU_IDX_REG_FPSCR] &= ~0x8f;
+	asm volatile("dmb");
+
+}
+
+/* Performs division without casting to double */
+static float divf(float a, float b)
+{
+	float result;
+
+	asm volatile(
+		"fdivs %0, %1, %2"
+		: "=w"(result)
+		: "w"(a), "w"(b)
+	);
+
+	return result;
+}
+
+/*
+ * Expect underflow when dividing the smallest number that can be represented
+ * using floats.
+ */
+test_static int test_cortexm_fpu_underflow(void)
+{
+	float result = divf(1.40130e-45f, 2.0f);
+
+	TEST_ASSERT(result == 0.0f);
+	/*
+	 * Don't check FPSCR value on STM32H7, because FPU interrupt is not
+	 * triggered (see errata ES0392 Rev 8, 2.1.2 Cortex-M7 FPU interrupt
+	 * not present on NVIC line 81).
+	 */
+	if (!IS_ENABLED(CHIP_FAMILY_STM32H7))
+		TEST_ASSERT(fpscr & FPU_FPSCR_UFC);
+
+	return EC_SUCCESS;
+}
+
+/*
+ * Expect overflow when dividing the highest number that can be represented
+ * using floats by number smaller than < 1.0f.
+ */
+test_static int test_cortexm_fpu_overflow(void)
+{
+	float result = divf(3.40282e38f, 0.5f);
+
+	TEST_ASSERT(isinf(result));
+	/*
+	 * Don't check FPSCR value on STM32H7, because FPU interrupt is not
+	 * triggered (see errata ES0392 Rev 8, 2.1.2 Cortex-M7 FPU interrupt
+	 * not present on NVIC line 81).
+	 */
+	if (!IS_ENABLED(CHIP_FAMILY_STM32H7))
+		TEST_ASSERT(fpscr & FPU_FPSCR_OFC);
+
+	return EC_SUCCESS;
+}
+
+/* Expect Division By Zero exception when 1.0f/0.0f */
+test_static int test_cortexm_fpu_division_by_zero(void)
+{
+	float result = divf(1.0f, 0.0f);
+
+	TEST_ASSERT(isinf(result));
+	/*
+	 * Don't check FPSCR value on STM32H7, because FPU interrupt is not
+	 * triggered (see errata ES0392 Rev 8, 2.1.2 Cortex-M7 FPU interrupt
+	 * not present on NVIC line 81).
+	 */
+	if (!IS_ENABLED(CHIP_FAMILY_STM32H7))
+		TEST_ASSERT(fpscr & FPU_FPSCR_DZC);
+
+	return EC_SUCCESS;
+}
+
+/* Expect Invalid Operation when trying to get square root of -1.0f */
+test_static int test_cortexm_fpu_invalid_operation(void)
+{
+	float result = sqrtf(-1.0f);
+
+	TEST_ASSERT(isnan(result));
+	/*
+	 * Don't check FPSCR value on STM32H7, because FPU interrupt is not
+	 * triggered (see errata ES0392 Rev 8, 2.1.2 Cortex-M7 FPU interrupt
+	 * not present on NVIC line 81).
+	 */
+	if (!IS_ENABLED(CHIP_FAMILY_STM32H7))
+		TEST_ASSERT(fpscr & FPU_FPSCR_IOC);
+
+	return EC_SUCCESS;
+}
+
+#endif /* CONFIG_FPU */
+
+void run_test(int argc, char **argv)
+{
+	test_reset();
+
+	if (IS_ENABLED(CONFIG_FPU)) {
+		RUN_TEST(test_cortexm_fpu_underflow);
+		RUN_TEST(test_cortexm_fpu_overflow);
+		RUN_TEST(test_cortexm_fpu_division_by_zero);
+		RUN_TEST(test_cortexm_fpu_invalid_operation);
+	}
+
+	test_print_result();
+}
