f9995214663d8191b5084e6f31cf26c6225b38a5
Dawid Niedzwiecki <dn@semihalf.com>
2022-01-21 14:49:40 +0100

    zephyr: eeprom: use Zephry EEPROM driver
    Start using the Zephyr EEPROM driver. ECs use M24C02 EEPROM that can be
    handled with AT24 driver.
    
    Remove wp-gpios property from EEPROM nodes because it points to GPIO
    that is used to set WP, not read. The WP status is set by other
    board components.
    
    The change increases usage of flash by 692 bytes and RAM by 32 bytes.
    
    BUG=b:211779766
    TEST=verify CBI can be read and set with 'cbi' console commands e.g.
    'cbi' to read all CBI and 'cbi set 2 10 4' to set SKU to 10
    BRANCH=main
    
    Signed-off-by: Dawid Niedzwiecki <dn@semihalf.com>
    Change-Id: Iddaf28fbcfb4c0e047c82b10e819b2ca2702b68a

---
+++ zephyr/shim/src/cbi_eeprom.c
@@ -0,0 +1,45 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "cros_board_info.h"
+#include <drivers/eeprom.h>
+#include "gpio.h"
+
+#if DT_NODE_EXISTS(DT_NODELABEL(cbi_eeprom))
+#define CBI_EEPROM_DEV DEVICE_DT_GET(DT_NODELABEL(cbi_eeprom))
+
+static int eeprom_load(uint8_t offset, uint8_t *data, int len)
+{
+	return eeprom_read(CBI_EEPROM_DEV, offset, data, len);
+}
+
+static int eeprom_is_write_protected(void)
+{
+	if (IS_ENABLED(CONFIG_PLATFORM_EC_BYPASS_CBI_EEPROM_WP_CHECK))
+		return 0;
+#ifdef CONFIG_WP_ACTIVE_HIGH
+	return gpio_get_level(GPIO_WP);
+#else
+	return !gpio_get_level(GPIO_WP_L);
+#endif
+}
+
+static int eeprom_store(uint8_t *cbi)
+{
+	return eeprom_write(CBI_EEPROM_DEV, 0, cbi,
+			    ((struct cbi_header *)cbi)->total_size);
+}
+
+static const struct cbi_storage_driver eeprom_drv = {
+	.store = eeprom_store,
+	.load = eeprom_load,
+	.is_protected = eeprom_is_write_protected,
+};
+
+const struct cbi_storage_config_t cbi_config = {
+	.storage_type = CBI_STORAGE_TYPE_EEPROM,
+	.drv = &eeprom_drv,
+};
+#endif
