4da1af0f230ca321eee7be29b2ed41750f1227c0
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2022-01-22 10:28:16 +0800

    agah: support USB retimer
    This patch apply support of
    USBC retimer ps8818 on USBC2,
    and USBA retimer PS8811 on USBA2,
    will upload another CL after EQ setting confirmed.
    
    BUG=b:218400524, b:233552228
    BRANCH=none
    TEST=make BOARD=agah
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: I3f6490dd8809b37cf974f2663374a472399c5861

--- board/agah/board.h
+++ board/agah/board.h
@@ -47,10 +47,12 @@
 /* USB Type A Features */
 #define USB_PORT_COUNT			1
 #define CONFIG_USB_PORT_POWER_DUMB
+#define CONFIG_USBC_RETIMER_PS8811
 
 /* USB Type C and USB PD defines */
 #define CONFIG_USB_PD_TCPM_RT1715
 #undef CONFIG_USB_PD_TCPM_NCT38XX
+#define CONFIG_USBC_RETIMER_PS8818
 
 /* I2C speed console command */
 #define CONFIG_CMD_I2C_SPEED
@@ -195,6 +197,13 @@ enum mft_channel {
 	MFT_CH_0 = 0,
 	MFT_CH_1,
 	MFT_CH_COUNT
+};
+
+/* USB-A ports */
+enum usba_port {
+	USBA_PORT_A0 = 0,
+	USBA_PORT_A1,
+	USBA_PORT_COUNT
 };
 
 /**
