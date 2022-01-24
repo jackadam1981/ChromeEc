02796d246ea7c40cf1254a7902da2dfd28012a27
Andrew McRae <amcrae@google.com>
2022-01-24 15:53:55 +1100

    nissa: Add USB-C config to Nereid
    Add type C USB config to Nereid
    
    BUG=b:201000844
    TEST=zmake configure -b nereid
    BRANCH=none
    
    Cq-Depend: chromium:3411679
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I75b53549a284a984067b894c421bd840bd46d372

---
+++ zephyr/projects/nissa/src/nereid/usbc.c
@@ -0,0 +1,147 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "charge_state_v2.h"
+#include "chipset.h"
+#include "hooks.h"
+#include "usb_mux.h"
+#include "system.h"
+#include "driver/charger/sm5803.h"
+#include "driver/tcpm/it83xx_pd.h"
+
+#include "gpios.h"
+#include "sub_board.h"
+
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
+
+struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_MAX_COUNT] = {
+	{
+		.bus_type = EC_BUS_TYPE_EMBEDDED,
+		/* TCPC is embedded within EC so no i2c config needed */
+		.drv = &it8xxx2_tcpm_drv,
+		/* Alert is active-low, push-pull */
+		.flags = 0,
+	},
+	{
+		.bus_type = EC_BUS_TYPE_EMBEDDED,
+		/* TCPC is embedded within EC so no i2c config needed */
+		.drv = &it8xxx2_tcpm_drv,
+		/* Alert is active-low, push-pull */
+		.flags = 0,
+	},
+};
+
+/*
+ * TODO(b/197480501): Move common code into common file.
+ */
+
+struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
+	{
+		.usb_port = 0,
+		.driver = &virtual_usb_mux_driver,
+		.hpd_update = &virtual_hpd_update,
+	},
+	{ /* sub-board */
+		.usb_port = 1,
+		.driver = &virtual_usb_mux_driver,
+		.hpd_update = &virtual_hpd_update,
+	},
+};
+
+static uint8_t cached_usb_pd_port_count;
+
+__override uint8_t board_get_usb_pd_port_count(void)
+{
+	if (cached_usb_pd_port_count == 0)
+		CPRINTS("USB PD Port count not initialized!");
+	return cached_usb_pd_port_count;
+}
+
+/*
+ * Initialise the USB PD port count, which
+ * depends on which sub-board is attached.
+ */
+static void init_usb_pd_port_count(void)
+{
+	switch (nissa_get_sb_type()) {
+	default:
+		cached_usb_pd_port_count = 1;
+		break;
+
+	case NISSA_SB_C_A:
+	case NISSA_SB_C_LTE:
+		cached_usb_pd_port_count = 2;
+		break;
+	}
+}
+/*
+ * Make sure setup is done after EEPROM is readable.
+ */
+DECLARE_HOOK(HOOK_INIT, init_usb_pd_port_count, HOOK_PRIO_INIT_I2C + 1);
+
+void board_set_charge_limit(int port, int supplier, int charge_ma,
+			    int max_ma, int charge_mv)
+{
+	int icl = MAX(charge_ma, CONFIG_CHARGER_INPUT_CURRENT);
+
+	/*
+	 * Assume charger overdraws by about 4%, keeping the actual draw
+	 * within spec. This adjustment can be changed with characterization
+	 * of actual hardware.
+	 */
+	icl = icl * 96 / 100;
+	charge_set_input_current_limit(icl, charge_mv);
+}
+
+/* Vconn control for integrated ITE TCPC */
+void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled)
+{
+	/* Vconn control is only for port 0 */
+	if (port)
+		return;
+
+	if (cc_pin == USBPD_CC_PIN_1)
+		gpio_pin_set_dt(&gpio_en_usb_c0_cc1_vconn, !!enabled);
+	else
+		gpio_pin_set_dt(&gpio_en_usb_c0_cc2_vconn, !!enabled);
+}
+
+/*
+ * TODO(b/201000844): Fill in missing functions.
+ */
+
+int board_is_sourcing_vbus(int port)
+{
+	return 0;
+}
+
+int board_set_active_charge_port(int port)
+{
+	return EC_SUCCESS;
+}
+
+uint16_t tcpc_get_alert_status(void)
+{
+	return 0;
+}
+
+int pd_check_vconn_swap(int port)
+{
+	/* Allow VCONN swaps if the AP is on. */
+	return chipset_in_state(CHIPSET_STATE_ANY_SUSPEND | CHIPSET_STATE_ON);
+}
+
+void pd_power_supply_reset(int port)
+{
+}
+
+int pd_set_power_supply_ready(int port)
+{
+	return EC_SUCCESS;
+}
+
+void board_reset_pd_mcu(void)
+{
+}
