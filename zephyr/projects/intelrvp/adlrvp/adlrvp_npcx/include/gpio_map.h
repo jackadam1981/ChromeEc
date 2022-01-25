8e9e385c1d2d109cbc9be04e33615e4044d22297
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-01-24 17:43:19 -0800

    zephyr: adlrvp: Power sequencing using shim
    This enables power sequencing on adlrvp npcx using shim
    
    BUG=none
    BRANCH=none
    TEST=zmake configure -B ~/tmp/adlrvp_npcx/ adlrvp_npcx -b
         EC boots up and stays in S0 power state
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I37d4b9c536db61f00a451019bd0b143a55230f08

--- zephyr/projects/intelrvp/adlrvp/adlrvp_npcx/include/gpio_map.h
+++ zephyr/projects/intelrvp/adlrvp/adlrvp_npcx/include/gpio_map.h
@@ -6,4 +6,9 @@
 #ifndef __ZEPHYR_GPIO_MAP_H
 #define __ZEPHYR_GPIO_MAP_H
 
+#include "intelrvp.h"
+
+#define GPIO_SYS_RESET_L		GPIO_SYS_RST_ODL
+#define GPIO_SYS_PWROK_EC		GPIO_EC_PCH_SYS_PWROK
+
 #endif /* __ZEPHYR_GPIO_MAP_H */
