9420e63f20880b22d6c2de91006cfc410230cb65
Aaron Massey <aaronmassey@google.com>
2022-01-25 17:01:40 -0700

    WIP: zephyr: test: verify src/snk attached
    WIP
    
    BRANCH=none
    BUG=b:209907615
    TEST=zmake configure --test test-drivers
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: Iba8a29f39a365cfdae405849823c4b6e193eac68

--- zephyr/test/drivers/src/main.c
+++ zephyr/test/drivers/src/main.c
@@ -43,30 +43,32 @@ void test_main(void)
 	ec_app_main();
 
 	/* Test suites to run after ec_app_main.*/
-	test_suite_battery();
-	test_suite_cbi();
-	test_suite_smart_battery();
-	test_suite_thermistor();
-	test_suite_temp_sensor();
-	test_suite_bma2x2();
-	test_suite_bc12();
-	test_suite_bmi260();
-	test_suite_bmi160();
-	test_suite_tcs3400();
-	test_suite_espi();
-	test_suite_bb_retimer();
-	test_suite_ln9310();
-	test_suite_lis2dw12();
-	test_suite_stm_mems_common();
-	test_suite_isl923x();
-	test_suite_usb_mux();
-	test_suite_ppc_sn5s330();
-	test_suite_ppc_syv682c();
-	test_suite_cros_cbi();
-	test_suite_tcpci();
-	test_suite_ps8xxx();
+	/* test_suite_battery(); */
+	/* test_suite_cbi(); */
+	/* test_suite_smart_battery(); */
+	/* test_suite_thermistor(); */
+	/* test_suite_temp_sensor(); */
+	/* test_suite_bma2x2(); */
+	/* test_suite_bc12(); */
+	/* test_suite_bmi260(); */
+	/* test_suite_bmi160(); */
+	/* test_suite_tcs3400(); */
+	/* test_suite_espi(); */
+	/* test_suite_bb_retimer(); */
+	/* test_suite_ln9310(); */
+	/* test_suite_lis2dw12(); */
+	/* test_suite_stm_mems_common(); */
+	/* test_suite_isl923x(); */
+	/* test_suite_usb_mux(); */
+	/* test_suite_ppc_sn5s330(); */
+	/* test_suite_ppc_syv682c(); */
+	/* test_suite_cros_cbi(); */
+	/* test_suite_tcpci(); */
+	/* test_suite_ps8xxx(); */
+
 	test_suite_integration_usb();
-	test_suite_power_common();
-	test_suite_watchdog();
-	test_suite_usb_pd_host_cmd();
+
+	/* test_suite_power_common(); */
+	/* test_suite_watchdog(); */
+	/* test_suite_usb_pd_host_cmd(); */
 }
