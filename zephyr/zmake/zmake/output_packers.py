fd678bac90d2e191f4c5447fea51dfbe720a8527
Wealian Liao <whliao@nuvoton.corp-partner.google.com>
2022-01-27 11:12:17 +0800

    zephyr: npcx: Fix code RAM size check
    On zephyr upstream, NPCX series map code RAM to flash0. Zephyr uses
    CONFIG_FLASH_SIZE for the code RAM size. However, this is overridden by
    Kconfig. This removes CONFIG_FLASH_SIZE in the cros repo. For the
    zmake, change to use CONFIG_FLASH_SIZE_BYTES for flash size check.
    
    BUG=b:216385443
    BRANCH=none
    TEST=linker show the error for the code RAM size out of range
    
    Signed-off-by: Wealian Liao <whliao@nuvoton.corp-partner.google.com>
    Change-Id: I0942f18bd3b145c4d35eb15a44169f217496a6a6

--- zephyr/zmake/zmake/output_packers.py
+++ zephyr/zmake/zmake/output_packers.py
@@ -192,12 +192,14 @@ class NpcxPacker(BinmanPacker):
     # code?
     def _get_max_image_bytes(self, ro, rw):
         ro_size = util.read_kconfig_autoconf_value(
-            ro / "zephyr" / "include" / "generated", "CONFIG_FLASH_SIZE"
+            ro / "zephyr" / "include" / "generated",
+            "CONFIG_PLATFORM_EC_FLASH_SIZE_BYTES",
         )
         rw_size = util.read_kconfig_autoconf_value(
-            ro / "zephyr" / "include" / "generated", "CONFIG_FLASH_SIZE"
+            ro / "zephyr" / "include" / "generated",
+            "CONFIG_PLATFORM_EC_FLASH_SIZE_BYTES",
         )
-        return max(int(ro_size, 0), int(rw_size, 0)) * 1024
+        return max(int(ro_size, 0), int(rw_size, 0))
 
     # This can probably be removed too and just rely on binman to
     # check the sizes... see the comment above.
