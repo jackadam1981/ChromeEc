a6b33223e49e1454615d2d98e90fc304cb419c5e
Aaron Massey <aaronmassey@google.com>
2022-01-27 12:49:44 -0700

    zephyr: test: battery present in integration usb
    The usb integration tests require the EC to know that a battery is
    present to charge.
    
    Now this happens without a test order dependency on the bc12 test
    suite.
    
    BRANCH=none
    BUG=b:209907615
    TEST=zmake configure --test test-drivers with bc12 tests
    TEST=zmake configure --test test-drivers without bc12 tests
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I7415cd9430fa708a2b40eff1d4ba2d31906b01cb

--- zephyr/test/drivers/src/integration_usb.c
+++ zephyr/test/drivers/src/integration_usb.c
@@ -29,6 +29,9 @@
 #define GPIO_AC_OK_PATH DT_PATH(named_gpios, acok_od)
 #define GPIO_AC_OK_PIN DT_GPIO_PIN(GPIO_AC_OK_PATH, gpios)
 
+#define GPIO_BATT_PRES_ODL_PATH DT_PATH(named_gpios, ec_batt_pres_odl)
+#define GPIO_BATT_PRES_ODL_PORT DT_GPIO_PIN(GPIO_BATT_PRES_ODL_PATH, gpios)
+
 static void integration_usb_before(void *state)
 {
 	const struct emul *tcpci_emul =
@@ -37,6 +40,10 @@ static void integration_usb_before(void *state)
 	struct sbat_emul_bat_data *bat;
 	const struct device *gpio_dev =
 		DEVICE_DT_GET(DT_GPIO_CTLR(GPIO_AC_OK_PATH, gpios));
+	const struct device *batt_pres_dev =
+		DEVICE_DT_GET(DT_GPIO_CTLR(GPIO_BATT_PRES_ODL_PATH, gpios));
+	zassert_ok(gpio_emul_input_set(batt_pres_dev,
+				       GPIO_BATT_PRES_ODL_PORT, 0), NULL);
 
 	ARG_UNUSED(state);
 	set_test_runner_tid();
