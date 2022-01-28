24f83223da97faf5e030b6d44e7c24b65f9e493c
Patryk Duda <pdk@semihalf.com>
2022-01-28 12:19:57 +0100

    math: Introduce isnan and isinf functions
    This patch adds functions responsible for checking if floating point
    operation result is NaN or infinity.
    Minute-ia specific functions were removed, because generic version
    should be used.
    
    To test added functions, 'fp' unit test was extended.
    
    BUG=b:215606535
    BRANCH=none
    TEST=make buildall
    TEST=make runhosttests
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: I68f46e91491b4a221beccde4275fd27a5424a048

--- builtin/math.h
+++ builtin/math.h
@@ -6,6 +6,29 @@
 #ifndef __CROS_EC_MATH_H__
 #define __CROS_EC_MATH_H__
 
+#include <stdbool.h>
 #include "fpu.h"
+
+static inline bool isnan(float a)
+{
+	uint32_t x = *(uint32_t *)&a;
+
+	/*
+	 * If value is NaN then the standard guarantees that a != a is true.
+	 * This could be used as a convenient way to check if value is NaN, but
+	 * we don't have guarantee that it won't trigger FPU exception.
+	 */
+
+	/* Exponent must have all bits set and fraction must be non-zero */
+	return ((x & 0x7f800000) == 0x7f800000) && ((x & 0x7fffff) != 0);
+}
+
+static inline bool isinf(float a)
+{
+	uint32_t x = *(uint32_t *)&a;
+
+	/* Exponent must have all bits set and fraction must be zero */
+	return (x & 0x7fffffff) == 0x7f800000;
+}
 
 #endif /* __CROS_EC_MATH_H__ */
