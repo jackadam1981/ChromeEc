b9d7d2b3f2d37a297a729eca781702748f919ee7
Keith Short <keithshort@chromium.org>
2022-01-28 10:01:57 -0700

    hooks: add HOOK_PRIO_POST_CHIPSET
    Add the HOOK_PRIO_POST_CHIPSET alias for
    (HOOK_PRIO_INIT_CHARGE_CHIPSET+1). This more clearly identifies the intent
    of this priority level.
    
    BUG=none
    BRANCH=none
    TEST=compare_build.sh
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: I82d5e6782606e9b1ce54c2fe251c86a290cb3622

--- zephyr/shim/chip/npcx/shi.c
+++ zephyr/shim/chip/npcx/shi.c
@@ -47,7 +47,7 @@ static void shi_reenable_on_sysjump(void)
 	}
 }
 /* Call hook after chipset sets initial power state */
-DECLARE_HOOK(HOOK_INIT, shi_reenable_on_sysjump, HOOK_PRIO_INIT_CHIPSET + 1);
+DECLARE_HOOK(HOOK_INIT, shi_reenable_on_sysjump, HOOK_PRIO_POST_CHIPSET);
 
 static void shi_disable(void)
 {
