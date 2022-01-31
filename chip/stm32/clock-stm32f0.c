4246cae9d8f9a6a2c7c215dfb410033aa8d0093b
Patryk Duda <pdk@semihalf.com>
2022-01-31 15:36:23 +0100

    clock-stm32f0: Fix IDLE task behaviour in deep sleep
    Like STM32F4, implementation of IDLE task for STM32F0 is also prone to
    issues with timer overflow when interrupts are disabled (CL:3347659).
    It also calculates timestamp after deep sleep imprecisely (CL:3359428).
    
    This patch brings these fixes to boards based on STM32F0 family.
    
    Description of timer overflow issue in CL:3347659:
    
    STM32F4 microcontroller has 32 bit timer which is used by EC to measure
    system uptime (in microseconds) and wake tasks if necessary. In EC
    timestamp is defined as two 32 bit values, which combined together give
    full 64 bit value. Lower 32 bits are read directly from timer. Higher
    32 bits are kept in clksrc_high variable. The variable is updated in
    process_timers() function which is called in interrupt context (it's not
    an interrupt handler, but it's called by the driver). The function also
    sets event timestamp which is supposed to generate interrupt if timer
    register matches event timestamp.
    
    When EC switches to IDLE task, interrupts are disabled in order
    to prevent task preemption in uncontrolled manner. It is possible that
    after interrupts are disabled timer overflow could occur and clksrc_high
    variable remains old. Other problem is that event timestamp which is
    read using __hw_clock_event_get() and points to timestamp in previous
    "epoch". Its value is about 2^32 - HOOK_TICK_INTERVAL (500 ms).
    
    After overflow, t0.le.lo is very low and t0.le.hi remains unincremented.
    As a result next_delay variable (which tells for how long RTC alarm is
    set) is very high (even above 1 hour). Another important fact is that t0
    timestamp is wrong and it is used to calculate value which will be
    loaded into the timer after wake. This can potentially lead to very long
    wait before running tasks.
    
    This fix checks if timestamp overflow occurs between disabling interrupt
    and getting "t0" value. If it happens, interrupts are enabled (to handle
    timer overflow) and IDLE enter procedure is repeated.
    
    Description of timestamp calculation improvements in CL:3359428
    
    Before this change, the timestamp (t0) and current time from the RTC
    (rtc0) were not called close together, so some time could have elapsed
    between them. This would result in the calculation of rtc_diff be a
    little smaller than it should be:
    
    rtc_diff = get_rtc_diff(&rtc0, &rtc1);
    
    This patch fixes the problem by obtaining the time from RTC close to
    obtaining t0 timestamp and using it to calculate the difference after
    wake up.
    
    BUG=b:200828093
    BRANCH=none
    TEST=make -j buildall
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: Id9144228fecb432372c7e0fd9b1ad0c1fea8324a

--- chip/stm32/clock-stm32f0.c
+++ chip/stm32/clock-stm32f0.c
@@ -35,6 +35,7 @@
 static int idle_sleep_cnt;
 static int idle_dsleep_cnt;
 static uint64_t idle_dsleep_time_us;
+static int idle_sleep_prevented_cnt;
 static int dsleep_recovery_margin_us = 1000000;
 
 /*
@@ -290,23 +291,65 @@ void clock_refresh_console_in_use(void)
 {
 }
 
+static bool timer_interrupt_pending(void)
+{
+	return task_is_irq_pending(IRQ_TIM(TIM_CLOCK32));
+}
+
 void __idle(void)
 {
 	timestamp_t t0;
 	uint32_t rtc_diff;
 	int next_delay, margin_us;
-	struct rtc_time_reg rtc0, rtc1;
+	struct rtc_time_reg rtc0, rtc1, rtc_sleep;
 
 	while (1) {
+		if (IS_ENABLED(CONFIG_LOW_POWER_IDLE_LIMITED) &&
+		    idle_is_disabled())
+			continue;
+
 		interrupt_disable();
 
+		/*
+		 * Get timestamp with interrupts disabled.
+		 * This value is used as a base to calculate timestamp after
+		 * wake from deep sleep. In combination with next_delay it gives
+		 * information how long the CPU can sleep. The timestamp can
+		 * point to the previous "epoch" when timer overflowed after
+		 * interrupts were disabled, since clksrc_high (which keeps
+		 * higher 32 bits of the timestamp) will not be updated.
+		 */
+		rtc_read(&rtc0);
 		t0 = get_time();
+
+		/*
+		 * Get time to next event.
+		 * After disabling interrupts, event timestamp
+		 * (__hw_clock_event_get()) is frozen, because
+		 * process_timers(), responsible for updating the
+		 * next event value with __hw_clock_event_set(),
+		 * can't be called. There is a risk that timer overflow
+		 * occurred after interrupts were disabled and obtained
+		 * event timestamp points to previous "epoch". We will
+		 * check that later.
+		 */
 		next_delay = __hw_clock_event_get() - t0.le.lo;
 
-#ifdef CONFIG_LOW_POWER_IDLE_LIMITED
-		if (idle_is_disabled())
-			goto en_int;
-#endif
+		/*
+		 * Repeat idle enter procedure when timer interrupt is pending
+		 * (eg. overflow occurred after disabling interrupts). To work
+		 * properly, this code assumes that timer interrupt is enabled
+		 * in NVIC and interrupt is generated on timer overflow.
+		 */
+		if (timer_interrupt_pending()) {
+			idle_sleep_prevented_cnt++;
+
+			/* Enable interrupts to handle detected overflow. */
+			interrupt_enable();
+
+			/* Repeat idle enter procedure. */
+			continue;
+		}
 
 		if (DEEP_SLEEP_ALLOWED &&
 #ifdef CONFIG_HOSTCMD_RTC
@@ -332,7 +375,7 @@ void __idle(void)
 			CPU_SCB_SYSCTRL |= 0x4;
 
 			set_rtc_alarm(0, next_delay - STOP_MODE_LATENCY,
-				      &rtc0, 0);
+				      &rtc_sleep, 0);
 			asm("wfi");
 
 			CPU_SCB_SYSCTRL &= ~0x4;
@@ -356,7 +399,7 @@ void __idle(void)
 				&restore_host_wake_alarm_deferred_data, 0);
 #endif
 			/* Record time spent in deep sleep. */
-			idle_dsleep_time_us += rtc_diff;
+			idle_dsleep_time_us += get_rtc_diff(&rtc_sleep, &rtc1);
 
 			/* Calculate how close we were to missing deadline */
 			margin_us = next_delay - rtc_diff;
@@ -373,9 +416,6 @@ void __idle(void)
 			/* Normal idle : only CPU clock stopped */
 			asm("wfi");
 		}
-#ifdef CONFIG_LOW_POWER_IDLE_LIMITED
-en_int:
-#endif
 		interrupt_enable();
 	}
 }
@@ -490,6 +530,8 @@ static int command_idle_stats(int argc, char **argv)
 	ccprintf("Num idle calls that deep-sleep:      %d\n", idle_dsleep_cnt);
 	ccprintf("Time spent in deep-sleep:            %.6llds\n",
 			idle_dsleep_time_us);
+	ccprintf("Num of prevented sleep:              %d\n",
+			idle_sleep_prevented_cnt);
 	ccprintf("Total time on:                       %.6llds\n", ts.val);
 	ccprintf("Deep-sleep closest to wake deadline: %dus\n",
 			dsleep_recovery_margin_us);
