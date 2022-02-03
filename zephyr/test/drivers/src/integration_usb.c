c26b5b0d11e757fc2c09b629401649f6058818ea
Tomasz Michalec <tm@semihalf.com>
2022-02-03 17:04:19 +0100

    zephyr: emul: Set FW rev on PS8xxx init
    Set some arbitrary FW revision value on PS8xxx emulator initialization.
    FW revision equals to 0 indicate that device is not working correctly,
    so value other than zero allows PD task to initialize this TCPC.
    
    BUG=none
    BRANCH=none
    TEST=make configure --test zephyr/test/drivers
    
    Signed-off-by: Tomasz Michalec <tm@semihalf.com>
    Change-Id: I8458ad973233a44edbaf3f0b5c3a15fd0939877b

--- zephyr/test/drivers/src/integration_usb.c
+++ zephyr/test/drivers/src/integration_usb.c
@@ -54,11 +54,6 @@ static void integration_usb_before(void *state)
 
 	ARG_UNUSED(state);
 	zassert_ok(tcpc_config[0].drv->init(0), NULL);
-	/*
-	 * Arbitrary FW ver. The emulator should really be setting this
-	 * during its init.
-	 */
-	tcpci_emul_set_reg(tcpci_emul2, PS8XXX_REG_FW_REV, 0x31);
 	zassert_ok(tcpc_config[1].drv->init(1), NULL);
 	tcpci_emul_set_rev(tcpci_emul, TCPCI_EMUL_REV1_0_VER1_0);
 	pd_set_suspend(0, 0);
