487d63131d665fefaacef505680f2df92e42aa58
Peter Marheine <pmarheine@chromium.org>
2022-02-04 16:33:26 +1100

    nissa/nereid: configure sensors
    Nereid uses a BMI323 IMU in the base, and a BMA422 accelerometer
    in the lid.
    
    BUG=none
    TEST=sensors appear to initialize and read as expected when power is
         forced on via GPIO (can't test normally since they are enabled only
         in S0 and S3, but we don't currently have an AP)
    BRANCH=none
    
    Change-Id: I96da8531bad49fc05152a6b0ccfe400dc14dbff5
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>

--- zephyr/projects/nissa/src/nereid/board_config.c
+++ zephyr/projects/nissa/src/nereid/board_config.c
@@ -66,6 +66,10 @@ DECLARE_HOOK(HOOK_INIT, nereid_subboard_init, HOOK_PRIO_FIRST+1);
 static void board_init(void)
 {
 	/*
+	 * Enable sensor interrupts
+	 */
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+	/*
 	 * Enable USB-C interrupts.
 	 */
 	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0));
