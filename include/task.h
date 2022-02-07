66ca2f46c54d4f5b3055b0822dae7fc52ad9e7f7
Brian J. Nemec <bnemec@google.com>
2022-02-07 09:24:37 -0800

    stm32: Add the DFU Boot Manager
    Added the DFU Boot Manager to control the switch between the DFU
    and main application region.
    
    The DFU Bootmanager performs self checks when the application boots.
    If any of the following conditions are true it will jump into DFU:
    
    * The system backup registers contain a 'Jump to DFU' request.
    * The start of the RW region is erased indicating the prior
        firmware update has failed.
    * An optional check can be enabled to record the number of
        consecutive unexpected reboots which will be useful for
        developer test images to avoid locked systems.
    
    Testing requires enabling multiple configuration settings set in the
    follower CL which enables it on Servo_v4. dfu-util version 0.9 lacks
    the 'leave' parameter required to automatically jump out, systems using
    this would need to be power cycled to leave DFU.
    
    BRANCH=servo
    BUG=b:217955677
    TEST=Enabled the DFU configuration on a Servo_v4. Verified that the
        runtime identifier is correctly recognized by dfu-util and pydfu.py
        implementations of the DFU host protocol. Verified firmware updates
        via the DFU protocol function correctly.
    TEST=sudo dfu-util -a 0 -s 0x08000000:leave -e -D ec.bin
        Verified that dfu-util can program the application region.
    
    Signed-off-by: Brian Nemec <bnemec@chromium.org>
    Change-Id: Ie535efc91318244574949542c03efa0b5973cbe5

--- include/task.h
+++ include/task.h
@@ -463,7 +463,9 @@ struct irq_def {
 
 /* Include ec.irqlist here for compilation dependency */
 #define ENABLE_IRQ(x)
+#if !defined(CONFIG_DFU_BOOTMANAGER) || defined(SECTION_IS_RW)
 #include "ec.irqlist"
+#endif /* !defined(CONFIG_DFU_BOOTMANAGER) || defined(SECTION_IS_RW) */
 #endif /* CONFIG_COMMON_RUNTIME */
 #endif /* !CONFIG_ZEPHYR */
 
