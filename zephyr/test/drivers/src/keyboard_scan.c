61846071ae5104dc04b859b38abd28a81479d197
Jeremy Bettis <jbettis@google.com>
2022-02-07 10:35:40 -0700

    zephyr: Check that the tasks run at non-idle
    If there are too many EC tasks, some of them will run at idle or
    sub-idle priorities.  Check for that and fail to build.
    
    BRANCH=None
    BUG=None
    TEST=None
    
    Change-Id: I2ea3a834f52f558bc09408f0ee11882b2735d928
    Signed-off-by: Jeremy Bettis <jbettis@google.com>

--- zephyr/test/drivers/src/keyboard_scan.c
+++ zephyr/test/drivers/src/keyboard_scan.c
@@ -7,11 +7,26 @@
 #include <drivers/emul.h>
 #include <drivers/gpio.h>
 #include <drivers/gpio/gpio_emul.h>
+#include <emul/emul_kb_raw.h>
 
 #include "test_state.h"
 
-ZTEST(keyboard_scan, test_lid_open)
+int emulate_keystate(int row, int col, int pressed)
 {
+	const struct device *dev =
+		DEVICE_DT_GET(DT_NODELABEL(cros_kb_raw));
+
+	return emul_kb_raw_set_kbstate(dev, row, col, pressed);
+}
+
+ZTEST(keyboard_scan, test_press_enter)
+{
+	zassert_ok(emulate_keystate(4, 11, true), NULL);
+	k_sleep(K_MSEC(100));
+
+	/* TODO(jbettis): Check espi_emul to verify the AP was notified. */
+	zassert_ok(emulate_keystate(4, 11, false), NULL);
+	k_sleep(K_MSEC(100));
 }
 
 ZTEST_SUITE(keyboard_scan, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
