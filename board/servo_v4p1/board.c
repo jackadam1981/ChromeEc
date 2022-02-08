9111bb62244d8c7da7a27ec9a59615241da6a4ce
Brian J. Nemec <bnemec@google.com>
2022-02-08 11:54:24 -0800

    WIP: Enable the DFU and builds on Servo_v4 and Servo_v4p1
    DO NOT SUBMIT:
    
    Added the DFU and DFU Migration builds to Servo_v4.
    
    BRANCH=servo
    BUG=b:217955677
    TEST=`make BOARD=servo_v4` and `make BOARD=servo_v4 MIGRATION=1`:
        Verified the partitions are resized.
    TEST=Flashed a Servo_v4 with the EC Update to the Migration build:
        Verified EC update remains functional and DFU update can be entered.
    TEST=Performed a DFU update from the Servo_v4 Migration build to a DFU
        to the non-Migration flow. Verified this update path works.
    
    Signed-off-by: Brian Nemec <bnemec@chromium.org>
    Change-Id: I583b97c6ba5a8ba8dbf0e890292a2bbdba03c0bd

--- board/servo_v4p1/board.c
+++ board/servo_v4p1/board.c
@@ -40,16 +40,9 @@
 #include "usb-stream.h"
 #include "util.h"
 
-#ifdef SECTION_IS_RO
-#define CROS_EC_SECTION "RO"
-#else
-#define CROS_EC_SECTION "RW"
-#endif
-
 /******************************************************************************
  * GPIO interrupt handlers.
  */
-#ifdef SECTION_IS_RO
 static void vbus0_evt(enum gpio_signal signal)
 {
 	task_wake(TASK_ID_PD_C0);
@@ -227,7 +220,6 @@ void ext_hpd_detection_enable(int enable)
 		gpio_disable_interrupt(GPIO_DP_HPD);
 	}
 }
-#endif /* SECTION_IS_RO */
 
 #include "gpio_list.h"
 
@@ -423,7 +415,6 @@ int board_get_version(void)
 	return board_id_det();
 }
 
-#ifdef SECTION_IS_RO
 /* Forward declaration */
 static void evaluate_input_power_def(void);
 DECLARE_DEFERRED(evaluate_input_power_def);
@@ -451,7 +442,6 @@ static void evaluate_input_power_def(void)
 	init_uservo_port();
 	init_pathsel();
 }
-#endif
 
 static void board_init(void)
 {
@@ -474,7 +464,6 @@ static void board_init(void)
 	system_set_bbram(SYSTEM_BBRAM_IDX_PD0, 0);
 	system_set_bbram(SYSTEM_BBRAM_IDX_PD1, 0);
 
-#ifdef SECTION_IS_RO
 	init_ioexpanders();
 	CPRINTS("Board ID is %d", board_id_det());
 
@@ -518,13 +507,10 @@ static void board_init(void)
 
 	/* Start SuzyQ detection */
 	start_ccd_meas_sbu_cycle();
-#else /* SECTION_IS_RO */
 	CPRINTS("Board ID is %d", board_id_det());
-#endif /* SECTION_IS_RO */
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
-#ifdef SECTION_IS_RO
 void tick_event(void)
 {
 	static int i = 0;
@@ -560,5 +546,3 @@ struct ioexpander_config_t ioex_config[] = {
 		.flags = IOEX_FLAGS_TCA64XXA_FLAG_VER_TCA6424A
 	}
 };
-
-#endif /* SECTION_IS_RO */
