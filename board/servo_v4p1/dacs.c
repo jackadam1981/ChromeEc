72e6f43d0c100243f80d2dcc24bda496ecbf6fae
Brian J. Nemec <bnemec@google.com>
2022-02-08 11:54:24 -0800

    WIP: Enable the DFU and builds on Servo_v4 and Servo_v4p1
    DO NOT SUBMIT:
    
    Added the DFU and DFU Migration builds to Servo_v4.
    
    BRANCH=servo
    BUG=b:217955677
    TEST=`make BOARD=servo_v4` and `make BOARD=servo_v4 MIGRATION=1`:
        Verified the partitions are resized.
    TEST=Flashed a Servo_v4 with the EC Update to the Migration build:
        Verified EC update remains functional and DFU update can be entered.
    TEST=Performed a DFU update from the Servo_v4 Migration build to a DFU
        to the non-Migration flow. Verified this update path works.
    
    Signed-off-by: Brian Nemec <bnemec@chromium.org>
    Change-Id: I583b97c6ba5a8ba8dbf0e890292a2bbdba03c0bd

--- board/servo_v4p1/dacs.c
+++ board/servo_v4p1/dacs.c
@@ -101,7 +101,6 @@ int write_dac(enum dac_t dac, uint16_t value)
 	return EC_SUCCESS;
 }
 
-#ifdef SECTION_IS_RO
 static int cmd_cc_dac(int argc, char *argv[])
 {
 	uint8_t dac;
@@ -137,4 +136,3 @@ static int cmd_cc_dac(int argc, char *argv[])
 DECLARE_CONSOLE_COMMAND(cc_dac, cmd_cc_dac,
 			"dac <\"on\"|\"off\"|mv>",
 			"Set Servo v4.1 CC dacs");
-#endif
