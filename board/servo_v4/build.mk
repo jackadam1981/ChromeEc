13dfeb0545322358612b890f104af13529fac809
Brian J. Nemec <bnemec@google.com>
2022-02-08 11:54:24 -0800

    servo_v4: Enable the DFU and Migration builds on Servo_v4
    DO NOT SUBMIT:
    
    Added the DFU and DFU Migration builds to Servo_v4.
    
    With these changes the DFU build has the following space:
    2400 bytes in flash and 15168 bytes in RAM RO
    68668 bytes in flash and 2888 bytes in RAM RW
    
    BRANCH=servo
    BUG=b:217955677
    TEST=`make BOARD=servo_v4` and `make BOARD=servo_v4 MIGRATION=1`:
        Verified the partitions are resized.
    TEST=Flashed a Servo_v4 with the EC Update to the Migration build:
        Verified EC update remains functional and DFU update can be entered.
    TEST=Performed a DFU update from the Servo_v4 Migration build to a DFU
        to the non-Migration flow. Verified this update path works.
    
    Signed-off-by: Brian Nemec <bnemec@chromium.org>
    Change-Id: I583b97c6ba5a8ba8dbf0e890292a2bbdba03c0bd

--- board/servo_v4/build.mk
+++ board/servo_v4/build.mk
@@ -13,7 +13,15 @@ CHIP_VARIANT:=stm32f07x
 # Not enough SRAM: Disable all tests
 test-list-y=
 
-board-y=board.o
+chip-y=bkpdata.o
+
+board-rw=board.o
 board-$(CONFIG_USB_POWER_DELIVERY)+=usb_pd_policy.o usb_pd_pdo.o
 
 all_deps=$(patsubst ro,,$(def_all_deps))
+
+# Passing the parameter MIGRATION to the make file will generate
+# the migration image instead.
+ifdef MIGRATION
+CPPFLAGS+=-DMIGRATION=1
+endif
