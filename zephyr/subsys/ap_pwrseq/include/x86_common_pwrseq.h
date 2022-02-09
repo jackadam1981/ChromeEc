56447a1fa6ba414808393b586456cf1c07704c8c
Li Feng <li1.feng@intel.com>
2022-02-08 18:18:50 -0800

    zephyr: subsys/ap_pwrseq: handle power signals
    Input power signals change results power state transition.
    Signals are from either GPIO interrupts or ESPI via virtual wires.
    Add power signals handling in power state machine.
    
    1. Config power signals GPIO interrupts
    2. Add power signals list
    3. Check power signals for power state transition
    4. Add S5 inactive timer
    
    BUG=b:203446068
    BRANCH=none
    TEST=On Brya platform, tested boot up to S0; AP shutdown, power state
    enter S5 then G3.
    
    Signed-off-by: Li Feng <li1.feng@intel.com>
    Change-Id: I34535fa8f8d1e5ba20fca578a638bf1bd7da250f

--- zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
+++ zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
@@ -77,5 +77,118 @@ enum pwrseq_chipset_shutdown_reason {
 struct pwrseq_context {
 	/* On power-on start boot up sequence */
 	enum power_states_ndsx power_state;
+
+	/*
+	 * Current input power signal states. Each bit represents an input
+	 * power signal that is defined by enum power_signal in same order.
+	 * 1 - signal state is asserted.
+	 * 0 - signal state is de-asserted.
+	 */
+	uint32_t in_signals;
+	/* Input signal state we're waiting for */
+	uint32_t in_want;
+	/* Signal values which print debug output */
+	uint32_t in_debug;
 };
-#endif /* __X86_COMMON_H__ */
+
+struct pwrseq_gpio_int_config {
+	const struct device *port;     /* GPIO device */
+	const char *port_name;
+	gpio_pin_t pin;                /* GPIO pin */
+	struct gpio_callback intr_cb;  /* GPIO callback */
+	const gpio_flags_t intr_flags; /* GPIO interrupt flags */
+};
+
+/* Extract power signals information from devicetree */
+#define POWER_SIGNALS_LIST_NODE                                \
+	DT_NODELABEL(pwrseq_signals_list)
+
+#if (DT_NODE_EXISTS(POWER_SIGNALS_LIST_NODE))
+#define GEN_POWER_SIGNAL_ENUM(id)                              \
+	DT_STRING_UPPER_TOKEN(id, pwrseq_signal_enum)
+
+/*
+ * Pass id of gpio-interrupts child node to GPIO_INT_CREATE
+ * For example, the node int_slp_s0.
+ * int_slp_s0: slp_s0 {
+ *     irq-pin = <&gpio_slp_s0_l>;
+ *     flags = <GPIO_INT_EDGE_BOTH>;
+ *     handler = "power_signal_interrupt";
+ * };
+ */
+
+#define GPIO_INT_CREATE(id, irq_pin)                           \
+{                                                              \
+	.intr_flags = DT_PROP(id, flags),                      \
+	.port = DEVICE_DT_GET(DT_GPIO_CTLR(irq_pin, gpios)),   \
+	.port_name = DT_GPIO_LABEL(irq_pin, gpios),            \
+	.pin = DT_GPIO_PIN(irq_pin, gpios),                    \
+}
+
+#define CONFIG_FROM_GPIO_INT(id)\
+	GPIO_INT_CREATE(id, DT_PHANDLE(id, irq_pin))
+
+#define GEN_POWER_INT_CONFIG(id) \
+	CONFIG_FROM_GPIO_INT(DT_PROP(id, pwrseq_gpio_int_node))
+
+#define GEN_GPIO_POWER_SIGNAL_ENTRY_COMMA(id)                  \
+{                                                              \
+	.int_config = GEN_POWER_INT_CONFIG(id),                \
+	.power_sig = GEN_POWER_SIGNAL_ENUM(id),                \
+	.flags = DT_PROP(id, flags),                           \
+	.name = DT_PROP(id, dbg_label),                        \
+},
+
+#define GEN_GPIO_POWER_SIGNAL_ENTRY(id)                        \
+	COND_CODE_1(DT_NODE_HAS_PROP(id, pwrseq_gpio_int_node),\
+		(GEN_GPIO_POWER_SIGNAL_ENTRY_COMMA(id)), ())
+
+#define GEN_VW_POWER_SIGNAL_ENTRY_VW_ENUM(id)                  \
+	DT_STRING_UPPER_TOKEN(id, pwrseq_vw_enum)
+
+#define GEN_VW_POWER_SIGNAL_ENTRY_COMMA(id)                    \
+{                                                              \
+	.vw_signal = GEN_VW_POWER_SIGNAL_ENTRY_VW_ENUM(id),    \
+	.power_sig = GEN_POWER_SIGNAL_ENUM(id),                \
+	.flags = DT_PROP(id, flags),                           \
+	.name = DT_PROP(id, dbg_label),                        \
+},
+
+#define GEN_VW_POWER_SIGNAL_ENTRY(id)                          \
+	COND_CODE_1(DT_NODE_HAS_PROP(id, pwrseq_vw_enum),      \
+		(GEN_VW_POWER_SIGNAL_ENTRY_COMMA(id)), ())
+
+#define GEN_POWER_SIGNAL_ENUM_COMMA(id)                        \
+	GEN_POWER_SIGNAL_ENUM(id),
+
+enum power_signal {
+	DT_FOREACH_CHILD(
+		POWER_SIGNALS_LIST_NODE,
+		GEN_POWER_SIGNAL_ENUM_COMMA)
+	POWER_SIGNAL_COUNT
+};
+/*
+ * Verify the number of required power signals are specified in
+ * the devicetree
+ */
+BUILD_ASSERT(POWER_SIGNAL_COUNT ==
+	DT_PROP(POWER_SIGNALS_LIST_NODE, pwrseq_signals_required));
+#endif /* (DT_NODE_EXISTS(POWER_SIGNALS_LIST_NODE)) */
+
+/* Information of a GPIO power signal */
+struct power_signal_gpio_info {
+	enum power_signal power_sig;        /* Power signal*/
+	struct pwrseq_gpio_int_config int_config;
+	uint32_t flags;        /* See POWER_SIGNAL_* macros */
+	const char *name;
+};
+
+/* Information of a virtual wire power signal */
+struct power_signal_vw_info {
+	enum espi_vwire_signal vw_signal; /* ESPI VW signal */
+	enum power_signal power_sig;        /* Power signal */
+	uint32_t flags;	       /* See POWER_SIGNAL_* macros */
+	const char *name;
+};
+
+#endif /* __X86_COMMON_PWRSEQ_H__ */
--- zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_sm_handler.h
+++ zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_sm_handler.h
@@ -29,6 +29,8 @@ struct common_pwrseq_config {
 	int pch_dsw_pwrok_delay_ms;
 	int pch_pm_pwrbtn_delay_ms;
 	int pch_rsmrst_delay_ms;
+	int wait_signal_timeout_ms;
+	int s5_timeout_s;
 #if PWRSEQ_GPIO_PRESENT(en_pp5000_s5_gpios)
 	const struct gpio_dt_spec enable_pp5000_a;
 #endif
@@ -48,6 +50,38 @@ struct common_pwrseq_config {
 	const struct gpio_dt_spec slp_sus_l;
 };
 
+/* Power signal GPIO pin flags */
+#define POWER_SIGNAL_ACTIVE_STATE BIT(0)
+#define POWER_SIGNAL_DISABLE_INT_ON_BOOT BIT(1)
+#define POWER_SIGNAL_ACTIVE_LOW   0
+#define POWER_SIGNAL_ACTIVE_HIGH  BIT(0)
+/* Convert enum power_signal to a mask for signal functions */
+#define POWER_SIGNAL_MASK(signal) (1 << (signal))
+/* Input state flags. */
+#define IN_PCH_SLP_S0_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S0_DEASSERTED)
+#define IN_PCH_SLP_S3_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S3_DEASSERTED)
+#define IN_PCH_SLP_S4_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S4_DEASSERTED)
+#define IN_PCH_SLP_S5_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S5_DEASSERTED)
+#define IN_PCH_SLP_SUS_DEASSERTED POWER_SIGNAL_MASK(X86_SLP_SUS_DEASSERTED)
+#define IN_ALL_PM_SLP_DEASSERTED (IN_PCH_SLP_S3 | \
+				  IN_PCH_SLP_S4 | \
+				  IN_PCH_SLP_SUS)
+#define IN_PGOOD_ALL_CORE POWER_SIGNAL_MASK(X86_DSW_PWROK)
+#define IN_ALL_S0 (IN_PGOOD_ALL_CORE | IN_ALL_PM_SLP_DEASSERTED)
+#define CHIPSET_G3S5_POWERUP_SIGNAL IN_PCH_SLP_SUS_DEASSERTED
+
+/* The wait time is ~150 msec, allow for safety margin. */
+#define IN_PCH_SLP_SUS_WAIT_TIME_MS 250
+/*
+ * Each board must provide its signal list and a corresponding enum
+ * power_signal.
+ */
+extern struct power_signal_gpio_info power_signal_gpio_list[];
+extern struct power_signal_vw_info power_signal_vw_list[];
+extern const int power_signal_gpio_count;
+extern const int power_signal_vw_count;
+
+void power_update_signals(void);
 enum power_states_ndsx chipset_pwr_sm_run(
 				enum power_states_ndsx curr_state,
 				const struct common_pwrseq_config *com_cfg);
@@ -58,8 +92,11 @@ void chipset_reset(enum pwrseq_chipset_shutdown_reason reason);
 void common_rsmrst_pass_thru_handler(void);
 void init_chipset_pwr_seq_state(void);
 enum power_states_ndsx pwr_sm_get_state(void);
+uint32_t pwrseq_get_input_signals(void);
+void pwrseq_set_debug_signals(uint32_t signals);
+uint32_t pwrseq_get_debug_signals(void);
 void apshutdown(void);
 
 extern const char pwrsm_dbg[][25];
 
-#endif /* __X86_NON_DSX_COMMON_H__ */
+#endif /* __X86_NON_DSX_COMMON_PWRSEQ_SM_HANDLER_H__ */
