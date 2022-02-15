538ed2f8920058a92c97c6a39b473812ff4e9085
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-02-14 17:17:04 -0800

    zephyr: adlrvp: Add support to get Board ID
    Add support to get Board ID for ADL RVP
    
    BUG=b:218684235
    BRANCH=none
    TEST=zmake configure -B ~/tmp/adlrvp_npcx/ adlrvp_npcx -b
             EC Boot up logs: BID:0x13, FID:0x1, BOM:0x4
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I181e8c6eb5235547b36adf76519b3155affac572

---
+++ zephyr/projects/intelrvp/include/intelrvp.h
@@ -0,0 +1,12 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#ifndef __INTELRVP_BOARD_H
+#define __INTELRVP_BOARD_H
+
+/* RVP ID read retry count */
+#define RVP_VERSION_READ_RETRY_CNT      2
+
+int ioexpander_read_intelrvp_version(int *port0, int *port1);
+#endif /* __INTELRVP_BOARD_H */
--- zephyr/projects/intelrvp/src/intelrvp.c
+++ zephyr/projects/intelrvp/src/intelrvp.c
@@ -6,6 +6,12 @@
 /* TODO: b/218904113: Convert to using Zephyr GPIOs */
 #include "gpio.h"
 #include "hooks.h"
+#include "intelrvp.h"
+#include "pca9555.h"
+
+/* Board ID */
+#define I2C_PORT_PCA9555_BOARD_ID_GPIO  I2C_PORT_BATTERY
+#define I2C_ADDR_PCA9555_BOARD_ID_GPIO  0x22
 
 static void board_init(void)
 {
@@ -13,3 +19,24 @@ static void board_init(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(ec_spi_oe_mecc), 1);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_LAST);
+
+int ioexpander_read_intelrvp_version(int *port0, int *port1)
+{
+	int i, rv;
+
+	for (i = 0; i < RVP_VERSION_READ_RETRY_CNT; i++) {
+		rv = pca9555_read(I2C_PORT_PCA9555_BOARD_ID_GPIO,
+			I2C_ADDR_PCA9555_BOARD_ID_GPIO,
+			PCA9555_CMD_INPUT_PORT_0, port0);
+
+		if (!rv && !pca9555_read(I2C_PORT_PCA9555_BOARD_ID_GPIO,
+			I2C_ADDR_PCA9555_BOARD_ID_GPIO,
+			PCA9555_CMD_INPUT_PORT_1, port1))
+			return 0;
+
+		msleep(1);
+	}
+
+	/* pca9555 read failed */
+	return -1;
+}
