FrameworkComputer/EmbeddedController

commit ae22b703601122b14005dd1afadd327939b5dfab

driver: add an option to emulate irq event in TCS3400
Krane does not reserve an interrupt pin for ALS, so we need another way
to trigger the irq handler. Add a new config option
CONFIG_ALS_TCS3400_EMULATED_IRQ_EVENT to support this use case.

BUG=b:129419982
BRANCH=None
TEST=verify that `accelread 3` outputs reasonable data on krane.

Change-Id: I960df249d29c0ac21810057e25f14d4bac3e14f5
Signed-off-by: Ting Shen <phoenixshen@google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1621449
Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
Commit-Queue: Ting Shen <phoenixshen@chromium.org>
Tested-by: Ting Shen <phoenixshen@chromium.org>

driver/als_tcs3400.c
@@ -9,6 +9,7 @@
 #include "common.h"
 #include "console.h"
 #include "driver/als_tcs3400.h"
+#include "hooks.h"
 #include "hwtimer.h"
 #include "i2c.h"
 #include "math_util.h"
@@ -32,12 +33,37 @@ static inline int tcs3400_i2c_write8(const struct motion_sensor_t *s,
 	return i2c_write8(s->port, s->addr, reg, data);
 }
 
+static void tcs3400_read_deferred(void)
+{
+	task_set_event(TASK_ID_MOTIONSENSE, CONFIG_ALS_TCS3400_INT_EVENT, 0);
+}
+DECLARE_DEFERRED(tcs3400_read_deferred);
+
+/* convert ATIME register to integration time, in microseconds */
+static int tcs3400_get_integration_time(int atime)
+{
+	return 2780 * (256 - atime);
+}
+
 static int tcs3400_read(const struct motion_sensor_t *s, intv3_t v)
 {
 	int ret;
 
 	/* Enable power, ADC, and interrupt to start cycle */
 	ret = tcs3400_i2c_write8(s, TCS_I2C_ENABLE, TCS3400_MODE_COLLECTING);
+	if (ret)
+		return ret;
+
+	if (IS_ENABLED(CONFIG_ALS_TCS3400_EMULATED_IRQ_EVENT)) {
+		int atime;
+
+		ret = tcs3400_i2c_read8(s, TCS_I2C_ATIME, &atime);
+		if (ret)
+			return ret;
+
+		hook_call_deferred(&tcs3400_read_deferred_data,
+				tcs3400_get_integration_time(atime));
+	}
 
 	/*
 	 * If write succeeded, we've started the read process, but can't
@@ -45,10 +71,7 @@ static int tcs3400_read(const struct motion_sensor_t *s, intv3_t v)
 	 * to inform upper level that read data process is under way and data
 	 * will be delivered when available.
 	 */
-	if (ret == EC_SUCCESS)
-		ret = EC_RES_IN_PROGRESS;
-
-	return ret;
+	return EC_RES_IN_PROGRESS;
 }
 
 static int tcs3400_rgb_read(const struct motion_sensor_t *s, intv3_t v)
@@ -223,8 +246,9 @@ static int tcs3400_irq_handler(struct motion_sensor_t *s, uint32_t *event)
 		return ret;
 
 	if ((status & TCS_I2C_STATUS_RGBC_VALID) ||
-		((status & TCS_I2C_STATUS_ALS_IRQ) &&
-		(status & TCS_I2C_STATUS_ALS_VALID))) {
+			((status & TCS_I2C_STATUS_ALS_IRQ) &&
+			(status & TCS_I2C_STATUS_ALS_VALID)) ||
+			IS_ENABLED(CONFIG_ALS_TCS3400_EMULATED_IRQ_EVENT)) {
 		ret = tcs3400_post_events(s, last_interrupt_timestamp);
 		if (ret)
 			return ret;

include/config.h
@@ -286,6 +286,14 @@
  */
 #undef CONFIG_ALS_SI114X_POLLING
 
+/*
+ * Enable tcs3400 to operate without interrupt pin. This config is used in
+ * conjunction with CONFIG_ALS_TCS3400_INT_EVENT. When this option is enabled,
+ * the read is initiated in the same manner as when interrupts are used, but the
+ * event which triggers the irq_handler is generated by deferred call.
+ */
+#undef CONFIG_ALS_TCS3400_EMULATED_IRQ_EVENT
+
 /* Define which ALS sensor is used for dimming the lightbar when dark */
 #undef CONFIG_ALS_LIGHTBAR_DIMMING
 

jackadam1981/ChromeEc

commit b74ac5d7bae36b2f742a64e638b94ff9041b9276

driver/tcs3400: Set timestamp when emulating interrupt
Use the same interrupt handler for wire or emulated interrupt,
so we are sure to timestamp the samples.

BUG=b:218890983,b:129419982
TEST=make -j buildall
Check on kukui/krane that tast test hardware.SensorIioserviceHard passes.
It would fail previously with error like:
"error during validation: too much delta between samples for
 lid cros-ec-gyro at index 174(1m52.310435376s):
 got 14.894431ms; want <= 7.5ms"

BRANCH=kukui,asurada

Fixes: commit ae22b703601 ("driver: add an option to emulate irq event in TCS3400")
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Change-Id: I9f4deaa214abe6e8ac7a2aa6b6071a02565f8825

driver/als_tcs3400.c
@@ -97,11 +97,20 @@ static inline int tcs3400_i2c_write8(const struct motion_sensor_t *s,
 	return i2c_write8(s->port, s->i2c_spi_addr_flags, reg, data);
 }
 
+#ifdef CONFIG_ALS_TCS3400_EMULATED_IRQ_EVENT
 static void tcs3400_read_deferred(void)
+#else
+void tcs3400_interrupt(enum gpio_signal signal)
+#endif
 {
+	if (IS_ENABLED(CONFIG_ACCEL_FIFO))
+		last_interrupt_timestamp = __hw_clock_source_read();
+
 	task_set_event(TASK_ID_MOTIONSENSE, CONFIG_ALS_TCS3400_INT_EVENT);
 }
+#ifdef CONFIG_ALS_TCS3400_EMULATED_IRQ_EVENT
 DECLARE_DEFERRED(tcs3400_read_deferred);
+#endif
 
 /* convert ATIME register to integration time, in microseconds */
 int tcs3400_get_integration_time(int atime)
@@ -134,6 +143,7 @@ static int tcs3400_read(const struct motion_sensor_t *s, intv3_t v)
 	if (ret)
 		return ret;
 
+#ifdef CONFIG_ALS_TCS3400_EMULATED_IRQ_EVENT
 	if (IS_ENABLED(CONFIG_ALS_TCS3400_EMULATED_IRQ_EVENT)) {
 		int atime;
 
@@ -142,8 +152,9 @@ static int tcs3400_read(const struct motion_sensor_t *s, intv3_t v)
 			return ret;
 
 		hook_call_deferred(&tcs3400_read_deferred_data,
-				tcs3400_get_integration_time(atime));
+				   tcs3400_get_integration_time(atime));
 	}
+#endif
 
 	/*
 	 * If write succeeded, we've started the read process, but can't
@@ -514,14 +525,6 @@ static int tcs3400_post_events(struct motion_sensor_t *s,
 							   raw_data, status);
 
 	return ret;
-}
-
-void tcs3400_interrupt(enum gpio_signal signal)
-{
-	if (IS_ENABLED(CONFIG_ACCEL_FIFO))
-		last_interrupt_timestamp = __hw_clock_source_read();
-
-	task_set_event(TASK_ID_MOTIONSENSE, CONFIG_ALS_TCS3400_INT_EVENT);
 }
 
 /*