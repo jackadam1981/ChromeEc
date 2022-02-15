be5fad9d691a89075c27fbe149d99b2d8a5a766c
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-02-14 17:17:04 -0800

    zephyr: adlrvp: Add support to get Board ID
    Add support to get Board ID for ADL RVP
    
    BUG=b:218684235
    BRANCH=none
    TEST=zmake configure -B ~/tmp/adlrvp_npcx/ adlrvp_npcx -b
             EC Boot up logs: BID:0x13, FID:0x1, BOM:0x4
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I181e8c6eb5235547b36adf76519b3155affac572

---
+++ zephyr/projects/intelrvp/adlrvp/include/adlrvp_zephyr.h
@@ -0,0 +1,16 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Intel ADL-RVP specific configuration */
+
+#ifndef __ADLRVP_BOARD_H
+#define __ADLRVP_BOARD_H
+
+#include "gpio_signal.h"
+#include "intelrvp.h"
+
+#define GPIO_SYS_PWROK_EC GPIO_PCH_SYS_PWROK
+
+#endif /* __ADLRVP_BOARD_H */
