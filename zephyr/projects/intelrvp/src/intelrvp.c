6ed77e587d3fbecb8de78cb7b95a9b762dd03211
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-02-14 17:17:04 -0800

    zephyr: adlrvp: Add support to get Board ID
    Add support to get Board ID for ADL RVP
    
    BUG=b:218684235
    BRANCH=none
    TEST=zmake configure -B ~/tmp/adlrvp_npcx/ adlrvp_npcx -b
             EC Boot up logs: BID:0x13, FID:0x1, BOM:0x4
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I181e8c6eb5235547b36adf76519b3155affac572

--- zephyr/projects/intelrvp/src/intelrvp.c
+++ zephyr/projects/intelrvp/src/intelrvp.c
@@ -5,6 +5,11 @@
 
 #include "gpio.h"
 #include "hooks.h"
+#include "pca9555.h"
+
+/* Board ID */
+#define I2C_PORT_PCA9555_BOARD_ID_GPIO  I2C_PORT_BATTERY
+#define I2C_ADDR_PCA9555_BOARD_ID_GPIO  0x22
 
 static void board_init(void)
 {
@@ -12,3 +17,15 @@ static void board_init(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(ec_spi_oe_mecc), 1);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_LAST);
+
+int ioexpander_read_intelrvp_version(int *port0, int *port1)
+{
+	if (pca9555_read(I2C_PORT_PCA9555_BOARD_ID_GPIO,
+				I2C_ADDR_PCA9555_BOARD_ID_GPIO,
+				PCA9555_CMD_INPUT_PORT_0, port0))
+		return -1;
+
+	return pca9555_read(I2C_PORT_PCA9555_BOARD_ID_GPIO,
+			I2C_ADDR_PCA9555_BOARD_ID_GPIO,
+			PCA9555_CMD_INPUT_PORT_1, port1);
+}
