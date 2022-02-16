433d6211a346ff2dda063b8dba750d6aa842a61b
Divya Sasidharan <divya.s.sasidharan@intel.com>
2022-02-15 17:00:54 -0800

    zephyr: subsys/ap_pwrseq: Add chipset hook notification
    Several tasks register hooks to get notified at specific
    power sequence levels. This patch is adding the notifications
    using shimmed hooks until replaced with inbuilt zephyr API.
    
    BUG=b:203446068
    BRANCH=None
    TEST=zmake testall; boot up nivviks and test hooks called.
    
    Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>
    Change-Id: I37c7d141235ebf9b0ef3140834a1cb30aec02605

--- zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
+++ zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
@@ -10,6 +10,7 @@
 #include <drivers/gpio.h>
 #include <logging/log.h>
 #include <x86_power_signals.h>
+#include <zephyr_hooks_common.h>
 
 /**
  * @brief System power states for Non Deep Sleep Well
@@ -92,4 +93,7 @@ struct pwrseq_context {
 	/* Signal values which print debug output */
 	uint32_t in_debug;
 };
+
+extern void hook_notify(enum hook_type type);
+
 #endif /* __X86_COMMON_PWRSEQ_H__ */
