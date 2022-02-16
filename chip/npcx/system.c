e158a44173278df7c2b755d63a3d0def6c13093c
Jun Lin <CHLin56@nuvoton.com>
2022-02-16 14:15:41 +0800

    npcx: power down unused modules
    This CL sets the power down bit for unused module SDP and I3C to
    get better power efficiency.
    
    BUG=b:219388463
    BRANCH=none
    TEST=pass "make buiilall"
    TEST=observe registers by "rw .b 0x4000d008" and "rw .b 0x4000d025"
    berfore/after this CL.
    
    Signed-off-by: Jun Lin <CHLin56@nuvoton.com>
    Change-Id: Idc399af40588a650e9031c6eadffc70c058d4ac4

--- chip/npcx/system.c
+++ chip/npcx/system.c
@@ -852,15 +852,20 @@ void system_pre_init(void)
 	 * EC should be initialized in Booter
 	 */
 
-	/* Power-down the modules we don't need */
-	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_1) = 0xF9; /* Skip SDP_PD FIU_PD */
+	/* Power down KBS, SDP, PS2, UART1, and MFT1-3 */
+	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_1) = 0xFB;
+	/* Power down PWM0-7 */
 	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_2) = 0xFF;
 #if defined(CHIP_FAMILY_NPCX5)
-	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_3) = 0x0F; /* Skip GDMA */
+	/* Power down SMB0-3 */
+	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_3) = 0x0F;
 #elif NPCX_FAMILY_VERSION >= NPCX_FAMILY_NPCX7
-	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_3) = 0x3F; /* Skip GDMA */
+	/* Power down SMB0-4 */
+	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_3) = 0x3F;
 #endif
-	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_4) = 0xF4; /* Skip ITIM2/1_PD */
+	/* Power down ITIM3, ADC, PECI, SPIP */
+	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_4) = 0xF4;
+	/* Power down C2HACC, SHM_REG, SHM, Port80, and MSWC */
 	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_5) = 0xF8;
 
 	pwdwn6 = 0x70 |
@@ -871,7 +876,7 @@ void system_pre_init(void)
 		 */
 		BIT(NPCX_PWDWN_CTL6_ITIM6_PD) |
 #endif
-		BIT(NPCX_PWDWN_CTL6_ITIM4_PD); /* Skip ITIM5_PD */
+		BIT(NPCX_PWDWN_CTL6_ITIM4_PD);
 #if !defined(CONFIG_HOST_INTERFACE_ESPI)
 	pwdwn6 |= 1 << NPCX_PWDWN_CTL6_ESPI_PD;
 #endif
@@ -881,13 +886,18 @@ void system_pre_init(void)
 #if defined(CHIP_VARIANT_NPCX7M6FB) || defined(CHIP_VARIANT_NPCX7M6FC) || \
 	defined(CHIP_VARIANT_NPCX7M7FC) || defined(CHIP_VARIANT_NPCX7M7WB) || \
 	defined(CHIP_VARIANT_NPCX7M7WC)
+	/* Power down UART2, SMB5-7, ITIM64, and WoV */
 	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_7) = 0xE7;
 #else
+	/* Power down SMB5-7 */
 	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_7) = 0x07;
 #endif
 #endif
 #if NPCX_FAMILY_VERSION >= NPCX_FAMILY_NPCX9
+	/* Power down UART2-4, SMB5-7, and ITIM64 */
 	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_7) = 0xFF;
+	/* Power down I3C */
+	NPCX_PWDWN_CTL(NPCX_PMC_PWDWN_8) = 0x31;
 #endif
 
 	/* Following modules can be powered down automatically in npcx7 */
