b3f17475a1216786f9d470d38a7c07ed126de857
Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
2022-02-17 17:28:41 +0800

    moli: configure EC GPIO/ I2C channel
    BUG=
    BRANCH=none
    TEST=make -j BOARD=moli
    
    Signed-off-by: Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
    Change-Id: I268eff2672cb2b98a29a395e3bf6370993f9e1a1

--- board/moli/i2c.c
+++ board/moli/i2c.c
@@ -12,14 +12,6 @@
 const struct i2c_port_t i2c_ports[] = {
 	{
 		/* I2C0 */
-		.name = "dp_redriver",
-		.port = I2C_PORT_DP_REDRIVER,
-		.kbps = 400,
-		.scl = GPIO_EC_I2C_DP_SCL,
-		.sda = GPIO_EC_I2C_DP_SDA,
-	},
-	{
-		/* I2C1 */
 		.name = "tcpc0,2",
 		.port = I2C_PORT_USB_C0_C2_TCPC,
 		.kbps = 1000,
@@ -27,15 +19,15 @@ const struct i2c_port_t i2c_ports[] = {
 		.sda = GPIO_EC_I2C_USB_C0_C2_TCPC_SDA,
 	},
 	{
-		/* I2C2 */
+		/* I2C1 */
 		.name = "ppc0,2",
 		.port = I2C_PORT_USB_C0_C2_PPC,
 		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C0_C2_PPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C0_C2_PPC_SDA,
+		.scl = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SCL,
+		.sda = GPIO_EC_I2C_USB_C0_C2_PPC_BC_SDA,
 	},
 	{
-		/* I2C3 */
+		/* I2C2 */
 		.name = "retimer0,2",
 		.port = I2C_PORT_USB_C0_C2_MUX,
 		.kbps = 1000,
@@ -43,15 +35,15 @@ const struct i2c_port_t i2c_ports[] = {
 		.sda = GPIO_EC_I2C_USB_C0_C2_RT_SDA,
 	},
 	{
-		/* I2C4 C1 TCPC */
+		/* I2C3 C1 TCPC */
 		.name = "tcpc1",
 		.port = I2C_PORT_USB_C1_TCPC,
 		.kbps = 400,
-		.scl = GPIO_EC_I2C_USB_C1_TCPC_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_TCPC_SDA,
+		.scl = GPIO_EC_I2C_USB_C1_SCL,
+		.sda = GPIO_EC_I2C_USB_C1_SDA,
 	},
 	{
-		/* I2C5 */
+		/* I2C4 */
 		.name = "wireless_charger",
 		.port = I2C_PORT_QI,
 		.kbps = 400,
@@ -59,20 +51,12 @@ const struct i2c_port_t i2c_ports[] = {
 		.sda = GPIO_EC_I2C_QI_SDA,
 	},
 	{
-		/* I2C6 */
-		.name = "ppc1",
-		.port = I2C_PORT_USB_C1_PPC,
-		.kbps = 1000,
-		.scl = GPIO_EC_I2C_USB_C1_MIX_SCL,
-		.sda = GPIO_EC_I2C_USB_C1_MIX_SDA,
-	},
-	{
-		/* I2C7 */
+		/* I2C5 */
 		.name = "eeprom",
 		.port = I2C_PORT_EEPROM,
 		.kbps = 400,
-		.scl = GPIO_EC_I2C_MISC_SCL_R,
-		.sda = GPIO_EC_I2C_MISC_SDA_R,
+		.scl = GPIO_EC_I2C_MISC_SCL,
+		.sda = GPIO_EC_I2C_MISC_SDA,
 	},
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
