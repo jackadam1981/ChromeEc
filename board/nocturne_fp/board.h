a02a112a79e296d58b5372a2d4fde73a07ce0c29
Patryk Duda <pdk@semihalf.com>
2022-02-21 14:02:49 +0000

    board/dartmonkey: Disable support for saving extended reset flags
    FPMCU RO for nocturne and nami don't support extended reset flags in
    backup RAM. CL:1295890 adds BKPDATA_INDEX_SAVED_RESET_FLAGS_2 in the
    middle of bkpdata_index enum, so the order of data has changed causing
    mismatch between RO and RW. RO interprets the register as saved panic
    reason, so it tries to restore panic data when value in the register is
    different than 0. Another problem is that panic data saved in backup RAM
    on hard reset is restored incorrectly.
    
    Fix needs to be applied only for nami_fp and nocturne_fp firmware.
    
    BUG=b:119131962
    BRANCH=none
    TEST=Compile FPMCU firmware for nami (nami_fp) with proper RO.
         Crash RW using 'crash assert' command. After FPMCU jumps to RW
         issue 'reboot hard' command. After jump to RW check panic data
         using 'panicinfo' command. R4 and R5 registers should have correct
         values.
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: I090c653218cac4d986e61948e25937d12f190b86

--- board/nocturne_fp/board.h
+++ board/nocturne_fp/board.h
@@ -152,6 +152,16 @@
  * RV32I panic data structure.
  */
 #define CONFIG_DO_NOT_INCLUDE_RV32I_PANIC_DATA
+
+/*
+ * FPMCU RO for nocturne and nami don't support extended reset flags in backup
+ * RAM. RO interprets the register as saved panic reason, so it tries to restore
+ * panic data when value in the register is different than 0. Another problem is
+ * that panic data saved in backup RAM on hard reset is restored incorrectly.
+ *
+ * CL:1295890 is the change which introduced extended reset flags.
+ */
+#define CONFIG_STM32_RO_DOES_NOT_SUPPORT_EXTENDED_RESET_FLAGS
 #endif
 
 #else /* SECTION_IS_RO */
