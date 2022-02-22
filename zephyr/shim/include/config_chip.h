516ca5bc7de2817211da6807fcbe8d112487a6a3
Will Tsai <will_tsai@wistron.corp-partner.google.com>
2022-02-22 14:20:37 +0800

    nereid: adjust initial value of SM5803 registers change SM5803 register 0x24 to 1f, which sets input current limit to 3100 mA. change SM5803 register 0x25 bit7 to 1, this setting will make the value of register 0x24 multiplied by 1.56.
    Signed-off-by: Will Tsai <will_tsai@wistron.corp-partner.google.com>
    Change-Id: I014a42e72bc124117f6ac4c92cd8db7798a1847b

--- zephyr/shim/include/config_chip.h
+++ zephyr/shim/include/config_chip.h
@@ -896,8 +896,8 @@
  * cables only support up to 60W.
  */
 #define PD_OPERATING_POWER_MW	15000
-#define PD_MAX_POWER_MW		60000
-#define PD_MAX_CURRENT_MA	3000
+#define PD_MAX_POWER_MW		62000
+#define PD_MAX_CURRENT_MA	3100
 #define PD_MAX_VOLTAGE_MV	20000
 
 /* TODO: b/144165680 - measure and check these values on Volteer */
