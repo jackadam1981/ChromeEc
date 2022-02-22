8c73acdda4c1ccc1458abe2488ec1253c8771030
Jeremy Bettis <jbettis@google.com>
2022-02-22 10:03:25 -0700

    presubmit: Add warning on edit of non-zephyr code
    Add presubmit to repo upload that will check that all C files in the cl
    are actually used in the Zephyr EC. If any of the edited files are not
    actually part of the Zephyr builds, print a warning message for the
    user.
    
    This is to satisfy the goal to prevent people from accidentally making
    changes in a legacy EC driver when we've already migrated that code to
    Zephyr.
    
    I excluded the baseboard and board dirs, since they are clearly not
    zephyr code. chip/stm32 since we don't have any boards in zephyr that
    use stm32. Excluded test and util since those aren't ec code. And
    exclude the zephyr dir, since obviously that is zephyr code.
    
    BRANCH=None
    BUG=b:205759844
    TEST=Edited several included and not-included files and made sure the
    warning only applied to the non-included files.
    
    Change-Id: Ida4f6702a1408004e3a98a057c6c472247feff25
    Signed-off-by: Jeremy Bettis <jbettis@google.com>

---
+++ util/migrated_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+mapfile -d '' cmakes < <(find zephyr \( -path zephyr/test -prune \) -o \
+  -name CMakeLists.txt -print0)
+
+exit_code=0
+
+for file in "$@"; do
+  ec_file="${file##**/platform/ec/}"
+  case "${ec_file}" in
+    baseboard/*|board/*|chip/stm32/*|test/*|util/*|zephyr/*) ;;
+    **.c)
+      if ! grep -q -F "\${PLATFORM_EC}/${ec_file}" "${cmakes[@]}" ; then
+        echo -n "WARNING: ${ec_file} is not used in Zephyr EC. Unless you are "
+        echo "fixing legacy code only, you should not be editing this file."
+        exit_code=1
+      fi
+      ;;
+  esac
+done
+
+exit "${exit_code}"
