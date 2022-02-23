20cf310550fd5c9d4f4ead39b1bbda29152f2c0b
Andrew McRae <amcrae@google.com>
2022-02-23 23:11:27 +1100

    zephyr: AP Power signal API layer.
    Separates out the power signal access from the core
    power sequence code.
    
    Signals can be obtained from GPIOs, eSPI virtual wire, or
    from board specific functions.
    
    The common code deals purely with an enum power_signal.
    
    Access handlers are added for GPIOs, GPIO interrupts, eSPI VW
    and board specific functions.
    
    Devicetree is used to configure how the signals are mapped
    to the access layer.
    
    BUG=b:201000950
    TEST=zmake configure -b brya
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I30bf903b7d95165ad48200f15aae2cf0dbf24b61

---
+++ zephyr/subsys/ap_pwrseq/include/x86_common_signals.h
@@ -0,0 +1,30 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Define power signals from device tree */
+
+#ifndef __X86_COMMON_SIGNALS_H__
+#define __X86_COMMON_SIGNALS_H__
+
+#if defined(CONFIG_AP_X86_INTEL_ADL)
+
+/* Input state flags */
+#define IN_PCH_SLP_S0_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S0_DEASSERTED)
+#define IN_PCH_SLP_S3_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S3_DEASSERTED)
+#define IN_PCH_SLP_S4_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S4_DEASSERTED)
+#define IN_PCH_SLP_S5_DEASSERTED  POWER_SIGNAL_MASK(X86_SLP_S5_DEASSERTED)
+#define IN_PCH_SLP_SUS_DEASSERTED POWER_SIGNAL_MASK(X86_SLP_SUS_DEASSERTED)
+#define IN_ALL_PM_SLP_DEASSERTED (IN_PCH_SLP_S3 | \
+				  IN_PCH_SLP_S4 | \
+				  IN_PCH_SLP_SUS)
+#define IN_PGOOD_ALL_CORE POWER_SIGNAL_MASK(X86_DSW_PWROK)
+#define IN_ALL_S0 (IN_PGOOD_ALL_CORE | IN_ALL_PM_SLP_DEASSERTED)
+#define CHIPSET_G3S5_POWERUP_SIGNAL IN_PCH_SLP_SUS_DEASSERTED
+
+#else
+#warning("Input power signals state flags not defined");
+#endif
+
+#endif /* __X86_COMMON_SIGNALS_H__ */
