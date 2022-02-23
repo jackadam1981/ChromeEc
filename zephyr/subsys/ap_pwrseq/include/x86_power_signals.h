f726b5e4762f228ab1437f62f86084fb00353d97
Andrew McRae <amcrae@google.com>
2022-02-23 23:11:27 +1100

    zephyr: AP Power signal API layer.
    Separates out the power signal access from the core
    power sequence code.
    
    Signals can be obtained from GPIOs, eSPI virtual wire, or
    from board specific functions.
    
    The common code deals purely with an enum power_signal.
    
    Access handlers are added for GPIOs, GPIO interrupts, eSPI VW
    and board specific functions.
    
    Devicetree is used to configure how the signals are mapped
    to the access layer.
    
    BUG=b:201000950
    TEST=zmake configure -b brya
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I30bf903b7d95165ad48200f15aae2cf0dbf24b61

--- zephyr/subsys/ap_pwrseq/include/x86_power_signals.h
+++ zephyr/subsys/ap_pwrseq/include/x86_power_signals.h
@@ -8,64 +8,6 @@
 #ifndef __X86_POWER_SIGNALS_H__
 #define __X86_POWER_SIGNALS_H__
 
-#include <drivers/espi.h>
-#include <drivers/gpio.h>
-
-#if DT_HAS_COMPAT_STATUS_OKAY(intel_ap_pwrseq_signal_list)
-BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(intel_ap_pwrseq_signal_list) == 1,
-	"Only one node for intel_ap_pwrseq_signal_list is allowed");
-#endif
-
-#define POWER_SIGNALS_LIST_NODE                                \
-	DT_COMPAT_GET_ANY_STATUS_OKAY(intel_ap_pwrseq_signal_list)
-
-#define POWER_SIGNAL_ENUM(id) DT_STRING_UPPER_TOKEN(id, pwrseq_signal_enum)
-#define POWER_SIGNAL_ENUM_COMMA(id) POWER_SIGNAL_ENUM(id),
-
-enum power_signal {
-	DT_FOREACH_CHILD(POWER_SIGNALS_LIST_NODE, POWER_SIGNAL_ENUM_COMMA)
-	POWER_SIGNAL_COUNT
-};
-
-enum power_source {
-	SOURCE_GPIO,
-	SOURCE_VW,
-	SOURCE_ADC,
-	SOURCE_BOARD,
-};
-
-#define GPIO_PWR_ENUM(id, enum_name) DT_CAT(GPIO, enum_name)
-#define GPIO_PWR_ENUM_COMMA(id, enum_name) GPIO_PWR_ENUM(id, enum_name),
-#define GEN_GPIO_ENUM(id)                                      \
-	COND_CODE_1(DT_NODE_HAS_PROP(id, pwrseq_int_gpios),    \
-		(GPIO_PWR_ENUM_COMMA(id, POWER_SIGNAL_ENUM(id))), ())
-
-enum gpio_power_signal {
-	DT_FOREACH_CHILD(POWER_SIGNALS_LIST_NODE, GEN_GPIO_ENUM)
-	POWER_SIGNAL_GPIO_COUNT
-};
-
-/* GPIO power signal configuration */
-struct gpio_power_signal_config {
-	enum power_signal power_signal;
-	const struct gpio_dt_spec spec;
-	gpio_flags_t intr_flags; /* GPIO interrupt flags */
-	bool enable_on_boot;     /* Enable interrupt at boot up */
-};
-
-/* Power signal common configuration */
-struct power_signal_config {
-	const char *debug_name;
-	enum power_source source;
-	uint8_t source_id;
-	uint8_t flags;
-};
-
-/* Power signal flags */
-#define POWER_SIGNAL_ACTIVE_STATE BIT(0)
-/* Convert enum power_signal to a mask for signal functions */
-#define POWER_SIGNAL_MASK(signal) (1 << (signal))
-
 #if defined(CONFIG_AP_X86_INTEL_ADL)
 
 /* Input state flags */
