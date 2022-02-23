e2050f615c98ef9544e1a9e6e70be550174aef3a
Andrew McRae <amcrae@google.com>
2022-02-23 23:11:27 +1100

    zephyr: AP Power signal API layer.
    Separates out the power signal access from the core
    power sequence code.
    
    Signals can be obtained from GPIOs, eSPI virtual wire, or
    from board specific functions.
    
    The common code deals purely with an enum power_signal.
    
    Access handlers are added for GPIOs, GPIO interrupts, eSPI VW
    and board specific functions.
    
    Devicetree is used to configure how the signals are mapped
    to the access layer.
    
    BUG=none
    TEST=none
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I30bf903b7d95165ad48200f15aae2cf0dbf24b61

---
+++ zephyr/subsys/ap_pwrseq/include/signal_gpio.h
@@ -0,0 +1,56 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __AP_PWRSEQ_SIGNAL_GPIO_H__
+#define __AP_PWRSEQ_SIGNAL_GPIO_H__
+
+#include <signal_interface.h>
+
+#if DT_HAS_COMPAT_STATUS_OKAY(COMPAT_IN) || \
+	DT_HAS_COMPAT_STATUS_OKAY(COMPAT_OUT)
+/**
+ * @brief Get the value of the GPIO power signal.
+ *
+ * @param signal The power_signal_gpios value to get.
+ * @return the current value of the power signal.
+ */
+int power_signal_gpio_get(enum power_signal_gpios gpio);
+
+/**
+ * @brief Set the output of this GPIO power signal.
+ *
+ * @param signal The GPIO to set.
+ * @param value The output value to set it to.
+ * @return 0 is successful
+ * @return negative If output cannot be set.
+ */
+int power_signal_gpio_set(enum power_signal_gpios gpio, int value);
+
+/**
+ * @brief Enable the GPIO interrupt
+ *
+ * @param signal The power_signal_gpios to enable.
+ * @return 0 if successful
+ * @return -error if failed
+ */
+int power_signal_gpio_enable_int(enum power_signal_gpios gpio);
+
+/**
+ * @brief Disable the GPIO interrupt
+ *
+ * @param signal The power_signal_gpios to disable.
+ * @return 0 if successful
+ * @return -error if failed
+ */
+int power_signal_gpio_disable_int(enum power_signal_gpios gpio);
+
+/**
+ * @brief Initialize the GPIOs for the power signals.
+ */
+void power_signal_gpio_init(void);
+
+#endif
+
+#endif /* __AP_PWRSEQ_SIGNAL_GPIO_H__ */
