7ea8df14f6a2262ee71ea4b9b4355052c98e8883
Andrew McRae <amcrae@google.com>
2022-02-23 23:11:27 +1100

    zephyr: AP Power signal API layer.
    Separates out the power signal access from the core
    power sequence code.
    
    Signals can be obtained from GPIOs, eSPI virtual wire, or
    from board specific functions.
    
    The common code deals purely with an enum power_signal.
    
    Access handlers are added for GPIOs, GPIO interrupts, eSPI VW
    and board specific functions.
    
    Devicetree is used to configure how the signals are mapped
    to the access layer.
    
    BUG=none
    TEST=none
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I30bf903b7d95165ad48200f15aae2cf0dbf24b61

--- zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_sm_handler.h
+++ zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_sm_handler.h
@@ -7,38 +7,17 @@
 #define __X86_NON_DSX_COMMON_PWRSEQ_SM_HANDLER_H__
 
 #include <x86_common_pwrseq.h>
-#include <x86_non_dsx_espi.h>
+#include <kernel.h>
+#include <init.h>
 #include <zephyr/types.h>
 
-#define DT_DRV_COMPAT intel_ap_pwrseq
-#define INTEL_COM_POWER_NODE	DT_INST(0, intel_ap_pwrseq)
-
-/* Check if the gpio is needed for the power sequence */
-#define PWRSEQ_GPIO_PRESENT(pha) \
-	DT_PHA_HAS_CELL(INTEL_COM_POWER_NODE, pha, flags)
+#define DT_DRV_COMPAT	intel_ap_pwrseq
 
 /* Common device tree configurable attributes */
 struct common_pwrseq_config {
 	int pch_dsw_pwrok_delay_ms;
 	int pch_pm_pwrbtn_delay_ms;
 	int pch_rsmrst_delay_ms;
-#if PWRSEQ_GPIO_PRESENT(en_pp5000_s5_gpios)
-	const struct gpio_dt_spec enable_pp5000_a;
-#endif
-#if PWRSEQ_GPIO_PRESENT(en_pp3300_s5_gpios)
-	const struct gpio_dt_spec enable_pp3300_a;
-#endif
-	const struct gpio_dt_spec pg_ec_rsmrst_odl;
-	const struct gpio_dt_spec ec_pch_rsmrst_odl;
-#if PWRSEQ_GPIO_PRESENT(pg_ec_dsw_pwrok_gpios)
-	const struct gpio_dt_spec pg_ec_dsw_pwrok;
-#endif
-#if PWRSEQ_GPIO_PRESENT(ec_soc_dsw_pwrok_gpios)
-	const struct gpio_dt_spec ec_soc_dsw_pwrok;
-#endif
-	const struct gpio_dt_spec slp_s3_l;
-	const struct gpio_dt_spec all_sys_pwrgd;
-	const struct gpio_dt_spec slp_sus_l;
 };
 
 enum power_states_ndsx chipset_pwr_sm_run(
@@ -55,4 +34,4 @@ void apshutdown(void);
 
 extern const char pwrsm_dbg[][25];
 
-#endif /* __X86_NON_DSX_COMMON_H__ */
+#endif /* __X86_NON_DSX_COMMON_PWRSEQ_SM_HANDLER_H__ */
