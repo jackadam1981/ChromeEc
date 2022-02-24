c2189ceff6e2240c06cf44e23a05afc65bfb4b9a
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-02-23 17:05:21 -0800

    zephyr: adlrvp: Enable Type-C Charger
    This Enables Type-C charger configuration for ADP RVP
    NPCX platform.
    
    BUG=b:218684235
    BRANCH=none
    TEST=zmake -l DEBUG configure -B ~/tmp/adlrvp_npcx/ adlrvp_npcx -b
         EC command charger, chgstate and chgsup
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I241201df7fc7f0cdde9b09f1ffe17bbbf54f6c85

--- zephyr/projects/intelrvp/adlrvp/include/adlrvp_zephyr.h
+++ zephyr/projects/intelrvp/adlrvp/include/adlrvp_zephyr.h
@@ -13,4 +13,65 @@
 
 #define GPIO_SYS_PWROK_EC GPIO_PCH_SYS_PWROK
 
+#define I2C_ADDR_FUSB302_TCPC_AIC       0x22
+#define I2C_ADDR_SN5S330_TCPC_AIC_PPC   0x40
+
+#define I2C_ADDR_PCA9675_TCPC_AIC_IOEX  0x21
+
+/* Connector side BB retimers */
+#define I2C_PORT0_BB_RETIMER_ADDR       0x56
+#if defined(HAS_TASK_PD_C1)
+#define I2C_PORT1_BB_RETIMER_ADDR       0x57
+#endif
+#if defined(HAS_TASK_PD_C2)
+#define I2C_PORT2_BB_RETIMER_ADDR       0x58
+#endif
+#if defined(HAS_TASK_PD_C3)
+#define I2C_PORT3_BB_RETIMER_ADDR       0x59
+#endif
+
+/* SOC side BB retimers (dual retimer config) */
+#define I2C_PORT0_BB_RETIMER_SOC_ADDR   0x54
+#if defined(HAS_TASK_PD_C1)
+#define I2C_PORT1_BB_RETIMER_SOC_ADDR   0x55
+#endif
+
+#define ADLM_LP4_RVP1_SKU_BOARD_ID      0x01
+#define ADLM_LP5_RVP2_SKU_BOARD_ID      0x02
+#define ADLM_LP5_RVP3_SKU_BOARD_ID      0x03
+#define ADLN_LP5_ERB_SKU_BOARD_ID       0x06
+#define ADLN_LP5_RVP_SKU_BOARD_ID       0x07
+#define ADLP_DDR5_RVP_SKU_BOARD_ID      0x12
+#define ADLP_LP5_T4_RVP_SKU_BOARD_ID    0x13
+#define ADL_RVP_BOARD_ID(id)            ((id) & 0x3F)
+
+#define CONFIG_BATTERY_TYPE_NO_AUTO_DETECT
+
+#undef CONFIG_DEDICATED_CHARGE_PORT_COUNT
+#define CONFIG_DEDICATED_CHARGE_PORT_COUNT 1
+#define DEDICATED_CHARGE_PORT CONFIG_USB_PD_PORT_MAX_COUNT
+
+enum adlrvp_charge_ports {
+	TYPE_C_PORT_0,
+#if defined(HAS_TASK_PD_C1)
+	TYPE_C_PORT_1,
+#endif
+#if defined(HAS_TASK_PD_C2)
+	TYPE_C_PORT_2,
+#endif
+#if defined(HAS_TASK_PD_C3)
+	TYPE_C_PORT_3,
+#endif
+};
+
+enum ioex_port {
+	IOEX_C0_PCA9675,
+	IOEX_C1_PCA9675,
+#if defined(HAS_TASK_PD_C2)
+	IOEX_C2_PCA9675,
+	IOEX_C3_PCA9675,
+#endif
+	IOEX_PORT_COUNT
+};
+
 #endif /* __ADLRVP_BOARD_H */
