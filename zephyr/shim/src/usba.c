6147482cf069c4ff3cfe0a0eccdfed4a511bb269
Diana Z <dzigterman@chromium.org>
2022-02-24 16:19:30 -0700

    Zephyr: Fill in USB-A enable pins
    Fill in the USB-A enable pins based on the enum-names of the elements in
    the enable_pins field.
    
    BRANCH=None
    BUG=b:208515128
    TEST=zmake testall
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: I00da940a574e76a6595b9824cf4449894f0ebf05

--- zephyr/shim/src/usba.c
+++ zephyr/shim/src/usba.c
@@ -13,8 +13,19 @@
 BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) > 0,
 		"No compatible USBA Port Enable instance found");
 
+#define SIGNAL_PHANDLE(id, prop, idx) DT_PHANDLE_BY_IDX(id, prop, idx)
+
+#define SIGNAL_NAME(id, prop, idx) \
+	T_STRING_UPPER_TOKEN(SIGNAL_PHANDLE(id, prop, idx), enum_name)
+#define SIGNAL_NAME_WITH_COMMA(id, prop, idx) \
+	SIGNAL_NAME(id, prop, idx),
+
 const int usb_port_enable[] = {
-	DT_FOREACH_CHILD(DT_PATH(usba_port_enable_list), GPIO_SIGNAL_WITH_COMMA)
+	DT_FOREACH_PROP_ELEM(DT_DRV_INST(0),
+			     enable_pins,
+			     SIGNAL_NAME_WITH_COMMA)
 };
+
+#undef GPIO_SIGNAL_WITH_COMMA
 
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
