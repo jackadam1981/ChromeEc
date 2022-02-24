599d570e7d23dd29af274154be7835311e4a477f
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-02-23 17:05:21 -0800

    zephyr: adlrvp: Enable Type-C Charger
    This Enables Type-C charger configuration for ADP RVP
    NPCX platform.
    
    BUG=b:218684235
    BRANCH=none
    TEST=zmake -l DEBUG configure -B ~/tmp/adlrvp_npcx/ adlrvp_npcx -b
         EC command charger, chgstate and chgsup
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I241201df7fc7f0cdde9b09f1ffe17bbbf54f6c85

--- zephyr/projects/intelrvp/include/intelrvp.h
+++ zephyr/projects/intelrvp/include/intelrvp.h
@@ -6,6 +6,9 @@
 #define __INTELRVP_BOARD_H
 
 #include <devicetree.h>
+#include "compiler.h"
+#include "gpio_signal.h"
+#include "stdbool.h"
 
 /* RVP ID read retry count */
 #define RVP_VERSION_READ_RETRY_CNT	2
@@ -17,4 +20,22 @@
 	(DT_PROP(DT_NODELABEL(intelrvp_board_id), i2c_addr))
 #endif
 
+FORWARD_DECLARE_ENUM(tcpc_rp_value);
+
+struct tcpc_aic_gpio_config_t {
+	/* TCPC interrupt */
+	enum gpio_signal tcpc_alert;
+	/* PPC interrupt */
+	enum gpio_signal ppc_alert;
+	/* PPC interrupt handler */
+	void (*ppc_intr_handler)(int port);
+};
+extern const struct tcpc_aic_gpio_config_t tcpc_aic_gpios[];
+
+void board_charging_enable(int port, int enable);
+void board_vbus_enable(int port, int enable);
+void board_set_vbus_source_current_limit(int port, enum tcpc_rp_value rp);
+void board_dc_jack_interrupt(enum gpio_signal signal);
+void tcpc_alert_event(enum gpio_signal signal);
+bool is_typec_port(int port);
 #endif /* __INTELRVP_BOARD_H */
