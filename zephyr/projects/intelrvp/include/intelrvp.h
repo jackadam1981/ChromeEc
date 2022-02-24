ce086e482489d1ba1ab4077ec31fcd6c1cc6185f
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-02-23 17:05:21 -0800

    zephyr: adlrvp: Enable Type-C Charger
    This Enables Type-C charger configuration for ADP RVP
    NPCX platform.
    
    BUG=b:218684235
    BRANCH=none
    TEST=zmake -l DEBUG configure -B ~/tmp/adlrvp_npcx/ adlrvp_npcx -b
         EC command charger, chgstate and chgsup
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I241201df7fc7f0cdde9b09f1ffe17bbbf54f6c85

---
+++ zephyr/projects/intelrvp/include/intelrvp.h
@@ -0,0 +1,60 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#ifndef __INTELRVP_BOARD_H
+#define __INTELRVP_BOARD_H
+
+#include "compiler.h"
+#include "config.h"
+#include "stdbool.h"
+
+FORWARD_DECLARE_ENUM(tcpc_rp_value);
+
+/* TCPC gpios */
+struct tcpc_gpio_t {
+	enum gpio_signal pin;
+	uint8_t pin_pol;
+};
+
+/* VCONN gpios */
+struct vconn_gpio_t {
+	enum gpio_signal cc1_pin;
+	enum gpio_signal cc2_pin;
+	uint8_t pin_pol;
+};
+
+struct tcpc_gpio_config_t {
+	/* VBUS interrput */
+	struct tcpc_gpio_t vbus;
+	/* Source enable */
+	struct tcpc_gpio_t src;
+	/* Sink enable */
+	struct tcpc_gpio_t snk;
+#if defined(CONFIG_USBC_VCONN) && defined(CHIP_FAMILY_IT83XX)
+	/* Enable VCONN */
+	struct vconn_gpio_t vconn;
+#endif
+	/* Enable source ILIM */
+	struct tcpc_gpio_t src_ilim;
+};
+extern const struct tcpc_gpio_config_t tcpc_gpios[];
+
+struct tcpc_aic_gpio_config_t {
+	/* TCPC interrupt */
+	enum gpio_signal tcpc_alert;
+	/* PPC interrupt */
+	enum gpio_signal ppc_alert;
+	/* PPC interrupt handler */
+	void (*ppc_intr_handler)(int port);
+};
+extern const struct tcpc_aic_gpio_config_t tcpc_aic_gpios[];
+
+void board_charging_enable(int port, int enable);
+void board_vbus_enable(int port, int enable);
+void board_set_vbus_source_current_limit(int port, enum tcpc_rp_value rp);
+int ioexpander_read_intelrvp_version(int *port0, int *port1);
+void board_dc_jack_interrupt(enum gpio_signal signal);
+void tcpc_alert_event(enum gpio_signal signal);
+bool is_typec_port(int port);
+#endif /* __INTELRVP_BOARD_H */
