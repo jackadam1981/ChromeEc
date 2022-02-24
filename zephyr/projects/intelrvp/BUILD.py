34a10fc287c7338a4d128523f15e7a396f80a61c
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-02-23 17:05:21 -0800

    zephyr: adlrvp: Enable Type-C Charger
    This Enables Type-C charger configuration for ADP RVP
    NPCX platform.
    
    BUG=b:218684235
    BRANCH=none
    TEST=zmake -l DEBUG configure -B ~/tmp/adlrvp_npcx/ adlrvp_npcx -b
         EC command charger, chgstate and chgsup
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I241201df7fc7f0cdde9b09f1ffe17bbbf54f6c85

--- zephyr/projects/intelrvp/BUILD.py
+++ zephyr/projects/intelrvp/BUILD.py
@@ -37,6 +37,7 @@ register_intelrvp_project(
     chip="npcx9",
     extra_dts_overlays=[
         here / "adlrvp/adlrvp_npcx/adlrvp_npcx.dts",
+        here / "adlrvp/adlrvp_npcx/bb_retimer.dts",
         here / "adlrvp/adlrvp_npcx/gpio.dts",
         here / "adlrvp/adlrvp_npcx/interrupts.dts",
         here / "adlrvp/adlrvp_npcx/keyboard.dts",
