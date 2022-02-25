8fc31466a29b44c9005a814735273c78a653018e
Ting Shen <phoenixshen@google.com>
2022-02-25 18:20:31 +0800

    rt1739/rt9490: fix bc12 detection logic
    <WIP>
    
    BUG=none
    TEST=none
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: Ie84fcc8fa5e594b451657ec245c48ec376931d74

--- driver/charger/rt9490.c
+++ driver/charger/rt9490.c
@@ -657,7 +657,7 @@ static void rt9490_usb_charger_task(const int port)
 			 * trigger bc1.2 detection for other cases.
 			 */
 			bool is_non_pd_sink = !pd_capable(port) &&
-				pd_get_power_role(port) == PD_ROLE_SINK &&
+				!board_vbus_source_enabled(port) &&
 				pd_snk_is_vbus_provided(port);
 
 			if (is_non_pd_sink)
