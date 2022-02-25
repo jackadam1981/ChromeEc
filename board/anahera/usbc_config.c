3e47966ff7fc1b4b74f0a250f0c431dc9f17e40d
Devin Lu <Devin.Lu@quantatw.com>
2022-02-25 16:56:32 +0800

    anahera: Tune USBA retimer EQ
    Tune USBA retimer EQ for USB A0, USB A1 port.
    
    BUG=b:203837657
    BRANCH=none
    TEST=i2cxfer to read setting is correct.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: Ia81d8a2559a3a9ac6c6b220fbd4bb31a83482a4d

--- board/anahera/usbc_config.c
+++ board/anahera/usbc_config.c
@@ -336,6 +336,57 @@ static int usba_retimer_init(int port)
 	}
 	CPRINTS("A%d: PS8811 retimer detected !", port);
 
+	switch (port) {
+	case USBA_PORT_A0:
+		/* Set channel A output swing */
+		rv = ps8811_i2c_field_update(
+			me, PS8811_REG_PAGE1, PS8811_REG1_USB_CHAN_A_SWING,
+			PS8811_CHAN_A_SWING_MASK,
+			0x2 << PS8811_CHAN_A_SWING_SHIFT);
+		break;
+	case USBA_PORT_A1:
+		if (ec_cfg_has_lte()) {
+			/* Set channel A output swing */
+			rv = ps8811_i2c_field_update(
+				me, PS8811_REG_PAGE1,
+				PS8811_REG1_USB_CHAN_A_SWING,
+				PS8811_CHAN_A_SWING_MASK,
+				0x2 << PS8811_CHAN_A_SWING_SHIFT);
+
+			/* Set channel A EQ setting */
+			rv |= ps8811_i2c_write(me, PS8811_REG_PAGE1,
+				PS8811_REG1_USB_AEQ_LEVEL,
+				(PS8811_AEQ_I2C_LEVEL_UP_10P5DB <<
+				PS8811_AEQ_I2C_LEVEL_UP_SHIFT) |
+				(PS8811_AEQ_PIN_LEVEL_UP_18DB <<
+				PS8811_AEQ_PIN_LEVEL_UP_SHIFT));
+
+			/* Set ADE pin setting */
+			rv |= ps8811_i2c_write(me, PS8811_REG_PAGE1,
+				PS8811_REG1_USB_ADE_CONFIG,
+				(PS8811_ADE_PIN_MID_LEVEL_3DB <<
+				PS8811_ADE_PIN_MID_LEVEL_SHIFT) |
+				PS8811_AEQ_CONFIG_REG_ENABLE |
+				PS8811_AEQ_ADAPTIVE_REG_ENABLE);
+
+			/* Set channel B EQ setting */
+			rv |= ps8811_i2c_write(me, PS8811_REG_PAGE1,
+				PS8811_REG1_USB_BEQ_LEVEL,
+				(PS8811_BEQ_I2C_LEVEL_UP_10P5DB <<
+				PS8811_BEQ_I2C_LEVEL_UP_SHIFT) |
+				PS8811_BEQ_PIN_LEVEL_UP_18DB <<
+				PS8811_BEQ_PIN_LEVEL_UP_SHIFT);
+
+			/* Set BDE pin setting */
+			rv |= ps8811_i2c_write(me, PS8811_REG_PAGE1,
+				PS8811_REG1_USB_BDE_CONFIG,
+				(PS8811_BDE_PIN_MID_LEVEL_3DB <<
+				PS8811_BDE_PIN_MID_LEVEL_SHIFT) |
+				PS8811_AEQ_CONFIG_REG_ENABLE);
+		}
+		break;
+	}
+
 	return rv;
 }
 
