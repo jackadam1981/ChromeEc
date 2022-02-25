899897733b90bdf62caed1b8c2044beea1a6f389
Ting Shen <phoenixshen@google.com>
2022-02-25 16:17:09 +0800

    zephyr: fix usba-port-enable-pins driver
    The phandles array `enable-pins` is not a child in the schema.
    Use the FOREEACH_PROP macro instead to generate the correct array
    content.
    
    BUG=b:220814055
    TEST=1) en_pp5000_usb_a0_vbus toggles when AP boot/shutdown
         2) on krabby, assert usb_port_enable[0] == en_pp5000_usb_a0_vbus
            and ARRAY_SIZE(usb_port_enable) == 1
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I0bd9e767ac85172820c0c4f0cad95c377f50c7a8

--- zephyr/shim/src/usba.c
+++ zephyr/shim/src/usba.c
@@ -6,15 +6,18 @@
 #define DT_DRV_COMPAT cros_ec_usba_port_enable_pins
 
 #include <devicetree.h>
-#include "hooks.h"
+#include "gpio_signal.h"
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
-BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) > 0,
+BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) == 1,
 		"No compatible USBA Port Enable instance found");
 
+#define GPIO_SIGNAL_WITH_COMMA(node_id, prop, idx) \
+	GPIO_SIGNAL(DT_PHANDLE_BY_IDX(node_id, prop, idx)),
+
 const int usb_port_enable[] = {
-	DT_FOREACH_CHILD(DT_PATH(usba_port_enable_list), GPIO_SIGNAL_WITH_COMMA)
+	DT_INST_FOREACH_PROP_ELEM(0, enable_pins, GPIO_SIGNAL_WITH_COMMA)
 };
 
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
