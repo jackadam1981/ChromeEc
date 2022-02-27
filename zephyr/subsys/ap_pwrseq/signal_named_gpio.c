6ab1a032863da7ff650c021061ee996d79250dfa
Andrew McRae <amcrae@google.com>
2022-02-27 11:36:09 +1100

    zephr: Add extra access for power signals
    Add access APIs for 'named-gpios' and 'gpio-interrupts' to
    the power signal access API.
    
    This allows the power signal DTS config to reference the
    cros-ec 'named-gpios' nodes and the 'gpio-interrupts' node.
    
    This avoids having to duplicate the GPIO config (and to mark
    named-gpios as 'no-auto-init').
    
    The other advantage is that the named-gpios GPIOs are handled specially
    during a RW sysjump (output values are maintained).
    
    BUG=b:201000950
    TEST=zmake configure -b brya
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I49ba32ce122ecee3a2a95b56cbed78108e2d9c75

---
+++ zephyr/subsys/ap_pwrseq/signal_named_gpio.c
@@ -0,0 +1,58 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <drivers/gpio.h>
+
+#include <power_signals.h>
+#include <signal_named_gpio.h>
+
+#include "gpio_signal.h"
+
+#define MY_COMPAT	intel_ap_pwrseq_named_gpio
+
+#if HAS_NAMED_GPIO_SIGNALS
+
+/*
+ * Configuration for named GPIOs
+ */
+struct named_gpio_config {
+	uint8_t output;
+	uint8_t gpio_enum;
+};
+
+#define INIT_CONFIG(id)						\
+	{							\
+		.output = DT_PROP(id, output),			\
+		.gpio_enum = GPIO_SIGNAL(DT_PHANDLE(id, pin)),	\
+	 },
+
+const static struct named_gpio_config config[] = {
+DT_FOREACH_STATUS_OKAY(MY_COMPAT, INIT_CONFIG)
+};
+
+int power_signal_named_gpio_get(enum pwr_sig_named_gpio index)
+{
+	if (index < 0 || index >= ARRAY_SIZE(config)) {
+		return -EINVAL;
+	}
+	return gpio_pin_get_dt(gpio_get_dt_spec(config[index].gpio_enum));
+}
+
+int power_signal_named_gpio_set(enum pwr_sig_named_gpio index, int value)
+{
+	const struct named_gpio_config *cp;
+
+	if (index < 0 || index >= ARRAY_SIZE(config)) {
+		return -EINVAL;
+	}
+	cp = &config[index];
+
+	if (!cp->output) {
+		return -EINVAL;
+	}
+	return gpio_pin_set_dt(gpio_get_dt_spec(cp->gpio_enum), value);
+}
+
+#endif /*  HAS_NAMED_GPIO_SIGNALS */
