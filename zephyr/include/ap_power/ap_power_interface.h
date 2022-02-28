97b710f6e3ed24621dc9c4135d8f6097127884c1
Li Feng <li1.feng@intel.com>
2022-02-27 18:07:52 -0800

    zephyr: subsys/ap_pwrseq: support S0ix
    BUG=b:203446068 b:203446865
    BRANCH=none
    TEST=Verified on Nivviks, system enter S0ix following:
    echo 1 > /var/lib/power_manager/suspend_to_idle
    restart powerd
    powerd_dbus_suspend
    
    Signed-off-by: Li Feng <li1.feng@intel.com>
    Change-Id: I317e62a81fd6012aa3b41df5553545be974753be

--- zephyr/include/ap_power/ap_power_interface.h
+++ zephyr/include/ap_power/ap_power_interface.h
@@ -69,6 +69,15 @@ enum power_states_ndsx {
 	SYS_POWER_STATE_S3S4,
 	/* Determine if Suspend to RAM is asserted */
 	SYS_POWER_STATE_S0S3,
+
+	/* S0ix */
+#if defined(CONFIG_PLATFORM_EC_POWERSEQ_S0IX)
+	/* AP is in standby; cache is flushed to RAM */
+	SYS_POWER_STATE_S0ix,
+	/* Intermediate states */
+	SYS_POWER_STATE_S0ixS0, /* Power up   */
+	SYS_POWER_STATE_S0S0ix, /* Power down */
+#endif
 };
 
 /**
