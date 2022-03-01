c319ac2cec100d67b9da73bcb75383a9330f2602
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-02-28 19:28:43 -0800

    [Workaround] nivviks: Enable Type A USB Port 1
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Change-Id: I77ee4f084b39443ed9dfaf407cee4e8787c78d8d

--- zephyr/projects/nissa/src/nivviks/board_config.c
+++ zephyr/projects/nissa/src/nivviks/board_config.c
@@ -49,7 +49,7 @@ static void nivviks_subboard_init(void)
 		/* Configure type-A port 1 VBUS, initialise it as low */
 		gpio_pin_configure_dt(
 			GPIO_DT_FROM_ALIAS(gpio_en_usb_a1_vbus),
-			GPIO_OUTPUT_LOW);
+			GPIO_OUTPUT_HIGH);
 	} else {
 		/* Disable the port 1 charger task */
 		task_disable_task(TASK_ID_USB_CHG_P1);
