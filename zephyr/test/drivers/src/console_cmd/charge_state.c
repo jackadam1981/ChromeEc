c5e520210b5fc7a93b434332b5ec890892e3245c
Yuval Peress <peress@google.com>
2022-03-01 23:38:57 -0700

    zephyr: test: Add tests for chgstate idle command
    Add tests to verify both on/off cases of the idle command
    
    BRANCH=none
    BUG=b:221471404
    TEST=zmake configure --clobber --test test-drivers
    
    Signed-off-by: Yuval Peress <peress@google.com>
    Change-Id: I02ae8a70e8b1d8eddaf4c06961ad2189a1fb4cfb

--- zephyr/test/drivers/src/console_cmd/charge_state.c
+++ zephyr/test/drivers/src/console_cmd/charge_state.c
@@ -7,7 +7,11 @@
 #include <ztest.h>
 
 #include "console.h"
+#include "ec_commands.h"
 #include "test_state.h"
+#include "utils.h"
+
+enum ec_charge_control_mode get_chg_ctrl_mode(void);
 
 ZTEST_USER(console_cmd_charge_state, test_idle_too_few_args)
 {
@@ -73,5 +77,70 @@ ZTEST_USER(console_cmd_charge_state, test_sustain_too_few_args__3_args)
 		      EC_ERROR_PARAM_COUNT, rv);
 }
 
-ZTEST_SUITE(console_cmd_charge_state, drivers_predicate_post_main, NULL, NULL,
-	    NULL, NULL);
+struct console_cmd_charge_state_fixture {
+	struct tcpci_src_emul source_5v_3a;
+	const struct emul *tcpci_emul;
+	const struct emul *charger_emul;
+};
+
+static void *console_cmd_charge_state_setup(void)
+{
+	static struct console_cmd_charge_state_fixture fixture;
+
+	/* Get references for the emulators */
+	fixture.tcpci_emul =
+		emul_get_binding(DT_LABEL(DT_NODELABEL(tcpci_emul)));
+	fixture.charger_emul =
+		emul_get_binding(DT_LABEL(DT_NODELABEL(isl923x_emul)));
+
+	/* Initialized the source to supply 5V and 3A */
+	tcpci_src_emul_init(&fixture.source_5v_3a);
+	fixture.source_5v_3a.data.pdo[1] =
+		PDO_FIXED(5000, 3000, PDO_FIXED_UNCONSTRAINED);
+
+	return &fixture;
+}
+
+static void console_cmd_charge_state_after(void *data)
+{
+	struct console_cmd_charge_state_fixture *fixture = data;
+
+	disconnect_source_from_port(fixture->tcpci_emul, fixture->charger_emul);
+}
+
+ZTEST_SUITE(console_cmd_charge_state, drivers_predicate_post_main,
+	    console_cmd_charge_state_setup, NULL,
+	    console_cmd_charge_state_after, NULL);
+
+ZTEST_USER_F(console_cmd_charge_state, test_idle_on_from_normal)
+{
+	/* Connect a source so we start charging */
+	connect_source_to_port(&this->source_5v_3a, 1, this->tcpci_emul,
+			       this->charger_emul);
+
+	/* Verify that we're in "normal" mode */
+	zassume_equal(CHARGE_CONTROL_NORMAL, get_chg_ctrl_mode(), NULL);
+
+	/* Move to idle */
+	zassert_ok(shell_execute_cmd(get_ec_shell(), "chgstate idle on"), NULL);
+	zassert_equal(CHARGE_CONTROL_IDLE, get_chg_ctrl_mode(), NULL);
+}
+
+ZTEST_USER_F(console_cmd_charge_state, test_normal_from_idle)
+{
+	/* Connect a source so we start charging */
+	connect_source_to_port(&this->source_5v_3a, 1, this->tcpci_emul,
+			       this->charger_emul);
+
+	/* Verify that we're in "normal" mode */
+	zassume_equal(CHARGE_CONTROL_NORMAL, get_chg_ctrl_mode(), NULL);
+
+	/* Move to idle */
+	zassume_ok(shell_execute_cmd(get_ec_shell(), "chgstate idle on"), NULL);
+	zassume_equal(CHARGE_CONTROL_IDLE, get_chg_ctrl_mode(), NULL);
+
+	/* Move back to normal */
+	zassert_ok(shell_execute_cmd(get_ec_shell(), "chgstate idle off"),
+		   NULL);
+	zassert_equal(CHARGE_CONTROL_NORMAL, get_chg_ctrl_mode(), NULL);
+}
