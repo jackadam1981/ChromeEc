0ece674f59d7448971d76875315dbd9dbca67731
Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
2022-03-02 17:16:37 +0800

    vdec: add to support mt8186 decoder
    1: fix vdec ipi service interface
    2: fix supported format
    3: fix ectasklist
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Change-Id: I9d458a8c92ee27366ae03c3386d5682f62f5693b

--- board/kukui_scp/vdec.c
+++ board/kukui_scp/vdec.c
@@ -1,25 +1,25 @@
-/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
 #include "console.h"
-#include "hooks.h"
 #include "ipi_chip.h"
-#include "queue.h"
 #include "queue_policies.h"
 #include "registers.h"
 #include "task.h"
 #include "util.h"
+#include "link_defs.h"
 #include "vdec.h"
 
 #define CPRINTF(format, args...) cprintf(CC_IPI, format, ##args)
 #define CPRINTS(format, args...) cprints(CC_IPI, format, ##args)
 
-/* Forwad declaration. */
+static void event_vdec_written(struct consumer const *consumer, size_t count)
+{
+	task_wake(TASK_ID_VDEC_SERVICE);
+}
 static struct consumer const event_vdec_consumer;
-static void event_vdec_written(struct consumer const *consumer, size_t count);
-
 static struct queue const event_vdec_queue = QUEUE_DIRECT(8,
 	struct vdec_msg, null_producer, event_vdec_consumer);
 static struct consumer const event_vdec_consumer = {
@@ -29,27 +29,37 @@ static struct consumer const event_vdec_consumer = {
 	}),
 };
 
-/* Stub functions only provided by private overlays. */
-#ifndef HAVE_PRIVATE_MT8183
-void vdec_h264_service_init(void) {}
-void vdec_h264_msg_handler(void *data) {}
-#endif
-
-static vdec_msg_handler mtk_vdec_msg_handle[VDEC_MAX];
-
-static void event_vdec_written(struct consumer const *consumer, size_t count)
+static void event_vdec_core_written(struct consumer const *consumer,
+				    size_t count)
 {
-	task_wake(TASK_ID_VDEC_SERVICE);
+	task_wake(TASK_ID_VDEC_CORE_SERVICE);
 }
+static struct consumer const event_vdec_core_consumer;
+static struct queue const event_vdec_core_queue = QUEUE_DIRECT(8,
+	struct vdec_msg, null_producer, event_vdec_core_consumer);
+static struct consumer const event_vdec_core_consumer = {
+	.queue = &event_vdec_core_queue,
+	.ops = &((struct consumer_ops const) {
+		.written = event_vdec_core_written,
+	}),
+};
+
+/* Stub functions only provided by private overlays. */
+#ifndef HAVE_PRIVATE_MT_SCP
+void vdec_msg_handler(void *data) {}
+void vdec_core_msg_handler(void *data) {}
+#endif
 
 static void vdec_h264_ipi_handler(int id, void *data, uint32_t len)
 {
 	struct vdec_msg rsv_msg;
 
-	if (!len)
+	if (!len) {
+		CPRINTS("len is zero.");
 		return;
+	}
 
-	rsv_msg.type = VDEC_H264;
+	rsv_msg.type = VDEC_LAT;
 	memcpy(rsv_msg.msg, data, MIN(len, sizeof(rsv_msg.msg)));
 
 	/*
@@ -59,16 +69,33 @@ static void vdec_h264_ipi_handler(int id, void *data, uint32_t len)
 	if (!queue_add_unit(&event_vdec_queue, &rsv_msg))
 		CPRINTS("Could not send vdec %d to the queue.", rsv_msg.type);
 }
-DECLARE_IPI(IPI_VDEC_H264, vdec_h264_ipi_handler, 0);
+DECLARE_IPI(SCP_IPI_VDEC_LAT, vdec_h264_ipi_handler, 0);
 
-/* This function renames from vdec_service_entry. */
+static void vdec_h264_ipi_core_handler(int id, void *data, uint32_t len)
+{
+	struct vdec_msg rsv_msg;
+
+	if (!len) {
+		CPRINTS("len is zero.");
+		return;
+	}
+
+	rsv_msg.type = VDEC_CORE;
+	memcpy(rsv_msg.msg, data, MIN(len, sizeof(rsv_msg.msg)));
+
+	/*
+	 * If there is no other IPI handler touch this queue, we don't need to
+	 * interrupt_disable() or task_disable_irq().
+	 */
+	if (!queue_add_unit(&event_vdec_core_queue, &rsv_msg))
+		CPRINTS("Could not send vdec %d to core queue.", rsv_msg.type);
+}
+DECLARE_IPI(SCP_IPI_VDEC_CORE, vdec_h264_ipi_core_handler, 0);
+
 void vdec_service_task(void *u)
 {
 	struct vdec_msg rsv_msg;
 	size_t size;
-
-	vdec_h264_service_init();
-	mtk_vdec_msg_handle[VDEC_H264] = vdec_h264_msg_handler;
 
 	while (1) {
 		/*
@@ -81,9 +108,28 @@ void vdec_service_task(void *u)
 
 		if (!size)
 			task_wait_event(-1);
-		else if (mtk_vdec_msg_handle[rsv_msg.type])
-			vdec_h264_msg_handler(rsv_msg.msg);
 		else
-			CPRINTS("vdec handler %d not exists.", rsv_msg.type);
+			vdec_msg_handler(rsv_msg.msg);
+	}
+}
+
+void vdec_core_service_task(void *u)
+{
+	struct vdec_msg rsv_msg;
+	size_t size;
+
+	while (1) {
+		/*
+		 * Queue unit is added in IPI handler, which is in ISR context.
+		 * Disable IRQ to prevent a clobbered queue.
+		 */
+		ipi_disable_irq(SCP_IRQ_IPC0);
+		size = queue_remove_unit(&event_vdec_core_queue, &rsv_msg);
+		ipi_enable_irq(SCP_IRQ_IPC0);
+
+		if (!size)
+			task_wait_event(-1);
+		else
+			vdec_core_msg_handler(rsv_msg.msg);
 	}
 }
