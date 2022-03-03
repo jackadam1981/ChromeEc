d824bd87bd159efa38ab82ebe263d67f61bf980d
Yuval Peress <peress@google.com>
2022-03-02 22:43:54 -0700

    zephyr: test enabling charge state debugging
    Verify that the charging progress was printed. This is not 100% ideal,
    since it relies on an additional variable being set in order to expose
    whether or not the function has been called. Ideally, we'd be watching
    the logs in the test and verifying that the correct information was
    printed, but we do not yet have that capability.
    
    BRANCH=none
    BUG=b:221471404
    TEST=zmake configure --clobber --test test-drivers
    
    Signed-off-by: Yuval Peress <peress@google.com>
    Change-Id: Iec1dd156ce7711f7550b142c528bd2374ed99258

--- include/charge_state_v2.h
+++ include/charge_state_v2.h
@@ -213,4 +213,8 @@ struct charge_state_data *charge_get_status(void);
 
 __test_only enum ec_charge_control_mode get_chg_ctrl_mode(void);
 
+__test_only void reset_prev_disp_charge(void);
+
+__test_only extern bool charging_progress_displayed;
+
 #endif /* __CROS_EC_CHARGE_STATE_V2_H */
