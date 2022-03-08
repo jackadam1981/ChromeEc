59a9120c2a02f0d8d1de415af3a70beaf1ae13ba
Andrew McRae <amcrae@google.com>
2022-03-08 16:04:10 +1100

    zephyr: Add initial unit tests for AP power sequence
    Add initial unit tests for AP power sequence code.
    Covers the power_signal access API.
    
    BUG=b:222934152
    TEST=zmake configure --test test-ap_power
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I681ccc244cd37a397428c5889af12046a6664863

---
+++ zephyr/test/ap_power/src/stubs.c
@@ -0,0 +1,16 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "chipset.h"
+#include "stubs.h"
+
+/*
+ * There are some collisions between the old chipset API and
+ * the new API.
+ */
+void chipset_force_shutdown(enum chipset_shutdown_reason reason)
+{
+	new_chipset_force_shutdown();
+}
