cf5444ed365e4694af93d56737074a900cb2ca55
Robert Chen <robert.chen@quanta.corp-partner.google.com>
2022-03-08 15:10:34 +0800

    lantis: Add SSFC field for 2nd source Touchscreen driver
    Due to landrid's ELAN touch screen use different driver but the same i2c slave address 0x10 as lantis.
    This patch adds SSFC field to support 2nd source touchscreen driver.
    
    BUG=b:222976965
    TEST= emerge-dedede chromeos-ec and make sure works normally on lantis.
    
    Change-Id: I44fe6a5c0320fee9c7220bd27ae6af9c1798576e
    Signed-off-by: Robert Chen <robert.chen@quanta.corp-partner.google.com>

--- board/lantis/cbi_ssfc.h
+++ board/lantis/cbi_ssfc.h
@@ -42,13 +42,23 @@ enum ec_ssfc_audio_codec_source {
         SSFC_ADUIO_CODEC_VS = 2,
 };
 
+/*
+ * Touchscreen Driver Source(Bit 11-13)
+ */
+enum ec_ssfc_ts_driver_source {
+        SSFC_TS_DRIVER_DEFAULT = 0,
+        SSFC_TS_DRIVER_GENERIC = 1,
+        SSFC_TS_DRIVER_HID = 2,
+};
+
 union dedede_cbi_ssfc {
 	struct {
 		uint32_t base_sensor : 3;
 		uint32_t lid_sensor : 3;
 		uint32_t reserved : 2;
 		uint32_t audio_codec_source : 3;
-		uint32_t reserved_2 : 21;
+		uint32_t ts_driver_source: 3;
+		uint32_t reserved_2 : 18;
 	};
 	uint32_t raw_value;
 };
@@ -73,5 +83,12 @@ enum ec_ssfc_lid_sensor get_cbi_ssfc_lid_sensor(void);
  * @return the Audio codec source type.
  */
 enum ec_ssfc_audio_codec_source get_audio_codec_source(void);
+
+/**
+ * Get the Touchscreen driver source for ELAN from SSFC_CONFIG.
+ *
+ * @return the Touchscreen driver source type.
+ */
+enum ec_ts_driver_source get_cbi_ts_driver_source(void);
 
 #endif /* _DEDEDE_CBI_SSFC__H_ */
