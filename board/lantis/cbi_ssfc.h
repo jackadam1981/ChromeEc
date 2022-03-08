3a33e041d0855c194c93080e3bc2074a9d3450eb
Robert Chen <robert.chen@quanta.corp-partner.google.com>
2022-03-08 14:34:59 +0800

    lantis: Add SSFC field for 2nd source Audio codec
    This patch adds SSFC field to support 2nd source audio codec.
    
    BUG=b:222976965
    TEST= emerge-dedede chromeos-ec and make sure works normally on lantis.
    
    Change-Id: Ibd596b46cf2470f07cb8cfd051e86f5c91f683f1
    Signed-off-by: Robert Chen <robert.chen@quanta.corp-partner.google.com>

--- board/lantis/cbi_ssfc.h
+++ board/lantis/cbi_ssfc.h
@@ -33,11 +33,22 @@ enum ec_ssfc_lid_sensor {
 	SSFC_SENSOR_LIS2DWL = 3
 };
 
+/*
+ * Audio Codec Source(Bit 8-10)
+ */
+enum ec_ssfc_audio_codec_source {
+        SSFC_AUDIO_CODEC_DEFAULT = 0,
+        SSFC_AUDIO_CODEC_VD = 1,
+        SSFC_ADUIO_CODEC_VS = 2,
+};
+
 union dedede_cbi_ssfc {
 	struct {
 		uint32_t base_sensor : 3;
 		uint32_t lid_sensor : 3;
-		uint32_t reserved_2 : 26;
+		uint32_t reserved : 2;
+		uint32_t audio_codec_source : 3;
+		uint32_t reserved_2 : 21;
 	};
 	uint32_t raw_value;
 };
@@ -56,5 +67,11 @@ enum ec_ssfc_base_sensor get_cbi_ssfc_base_sensor(void);
  */
 enum ec_ssfc_lid_sensor get_cbi_ssfc_lid_sensor(void);
 
+/**
+ * Get the Audio codec source from SSFC_CONFIG.
+ *
+ * @return the Audio codec source type.
+ */
+enum ec_ssfc_audio_codec_source get_audio_codec_source(void);
 
 #endif /* _DEDEDE_CBI_SSFC__H_ */
