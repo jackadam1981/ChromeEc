05061759795b96356d608cdd610236efe8f362dd
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-03-09 18:15:49 +0800

    DNS: gimble: check if 0x1d is 0x10 and disable BIST mode
    When running TDA 2.1.2.2 BMC PHY RX INT REJ, GRL C2 tester will set
    "Ra_asserted" and wait for TCPC to response. But in this case TCPC was
    set into BIST mode(0x19 bit1 = 1), it can not response. At this moment,
    there is no hard reset or deatach event. So EC need to check if
    0x1d=0x10 and disable BIST mode.
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Ie37c100f897bde1045c92bab8173eafe0ee7eaf8

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -5142,8 +5142,14 @@ static void pe_bist_tx_run(int port)
 		 * GoodCRC Messages in response to received Messages will
 		 * be sent.
 		 */
+		int regval;
 		if (PE_CHK_FLAG(port, PE_FLAGS_MSG_RECEIVED))
 			PE_CLR_FLAG(port, PE_FLAGS_MSG_RECEIVED);
+
+		tcpc_read(port, 0x1d, &regval);
+		if (regval == 0x10) {
+			tcpc_write(port, 0x19, 0x41);
+		}
 	}
 }
 
