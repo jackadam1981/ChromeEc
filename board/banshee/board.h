9df2198f8cf4884417631558b0c5c8b93f865eb5
Leo-Tsai <leocx_tsai@compal.corp-partner.google.com>
2022-03-09 14:08:10 +0800

    Banshee: Remove useless function
    This patch removes useless function as following:
    1. Remove Accelerometers.
    2. Remove tablet mode.
    
    BUG=b:223490888
    BRANCH=brya
    TEST=make BOARD=banshee
    
    Signed-off-by: Leo-Tsai <leocx_tsai@compal.corp-partner.google.com>
    Change-Id: Id24a45e80e276175594d7fe35ac39e642da07811

--- board/banshee/board.h
+++ board/banshee/board.h
@@ -11,13 +11,15 @@
 #include "compile_time_macros.h"
 
 /*
- * Early brya boards are not set up for vivaldi
+ * Early banshee boards are not set up for vivaldi
  */
 #undef CONFIG_KEYBOARD_VIVALDI
 
 /* Baseboard features */
 #include "baseboard.h"
 
+#undef CONFIG_TABLET_MODE
+#undef CONFIG_TABLET_MODE_SWITCH
 #undef CONFIG_GMR_TABLET_MODE
 #undef CONFIG_VOLUME_BUTTONS
 /*
@@ -53,24 +55,9 @@
 #define CONFIG_ACCEL_FIFO_THRES (CONFIG_ACCEL_FIFO_SIZE / 3)
 
 /* Sensors without hardware FIFO are in forced mode */
-#define CONFIG_ACCEL_FORCE_MODE_MASK \
-	(BIT(LID_ACCEL) | BIT(CLEAR_ALS))
-
-/* Lid accel */
-#define CONFIG_LID_ANGLE
-#define CONFIG_LID_ANGLE_UPDATE
-#define CONFIG_LID_ANGLE_SENSOR_BASE	BASE_ACCEL
-#define CONFIG_LID_ANGLE_SENSOR_LID	LID_ACCEL
-#define CONFIG_ACCEL_LIS2DWL
-#define CONFIG_ACCEL_LIS2DW_AS_BASE
-#define CONFIG_ACCEL_LIS2DW12_INT_EVENT \
-	TASK_EVENT_MOTION_SENSOR_INTERRUPT(LID_ACCEL)
+#define CONFIG_ACCEL_FORCE_MODE_MASK BIT(CLEAR_ALS)
 
 #define CONFIG_ACCEL_INTERRUPTS
-
-/* Sensor console commands */
-#define CONFIG_CMD_ACCELS
-#define CONFIG_CMD_ACCEL_INFO
 
 /* USB Type C and USB PD defines */
 #define CONFIG_USB_PD_REQUIRE_AP_MODE_ENTRY
@@ -90,7 +77,6 @@
 #define CONFIG_USBC_PPC_SYV682X
 #define CONFIG_USBC_PPC_NX20P3483
 
-/* TODO: b/177608416 - measure and check these values on brya */
 #define PD_POWER_SUPPLY_TURN_ON_DELAY	30000 /* us */
 #define PD_POWER_SUPPLY_TURN_OFF_DELAY	30000 /* us */
 #define PD_VCONN_SWAP_DELAY		5000 /* us */
@@ -223,10 +209,7 @@ enum temp_sensor_id {
 };
 
 enum sensor_id {
-	LID_ACCEL = 0,
-	BASE_ACCEL,
-	BASE_GYRO,
-	CLEAR_ALS,
+	CLEAR_ALS = 0,
 	SENSOR_COUNT
 };
 
