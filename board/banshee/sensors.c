a6de79590780ddd9806e516a005fdb409b70bf6e
Ridden_Liu <ridden_liu@compal.corp-partner.google.com>
2022-03-09 09:48:32 +0800

    Banshee: Implement fan table
    Modify the fan rpm min/max/start value.
    And modify the fan temperature setting.
    
    BUG=none
    BRANCH=brya
    TEST=make -j BOARD=banshee
    
    Signed-off-by: Ridden_Liu <ridden_liu@compal.corp-partner.google.com>
    Change-Id: Ia723da837df6fc3a7b5383007174ad23a0298edb

--- board/banshee/sensors.c
+++ board/banshee/sensors.c
@@ -245,14 +245,17 @@ __maybe_unused static const struct ec_thermal_config thermal_cpu = THERMAL_CPU;
 #define THERMAL_AMBIENT \
 	{ \
 		.temp_host = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(85), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(90), \
+			[EC_TEMP_THRESH_WARN] = C_TO_K(85), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(95), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(105), \
 		}, \
 		.temp_host_release = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(80), \
+			[EC_TEMP_THRESH_WARN] = C_TO_K(70), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(86), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(96), \
 		}, \
-		.temp_fan_off = C_TO_K(35), \
-		.temp_fan_max = C_TO_K(60), \
+		.temp_fan_off = C_TO_K(40), \
+		.temp_fan_max = C_TO_K(58), \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_ambient =
 	THERMAL_AMBIENT;
@@ -274,14 +277,17 @@ __maybe_unused static const struct ec_thermal_config thermal_ambient =
 #define THERMAL_CHARGER \
 	{ \
 		.temp_host = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(105), \
-			[EC_TEMP_THRESH_HALT] = C_TO_K(120), \
+			[EC_TEMP_THRESH_WARN] = C_TO_K(90), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(100), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(110), \
 		}, \
 		.temp_host_release = { \
-			[EC_TEMP_THRESH_HIGH] = C_TO_K(90), \
+			[EC_TEMP_THRESH_WARN] = C_TO_K(85), \
+			[EC_TEMP_THRESH_HIGH] = C_TO_K(91), \
+			[EC_TEMP_THRESH_HALT] = C_TO_K(101), \
 		}, \
-		.temp_fan_off = C_TO_K(35), \
-		.temp_fan_max = C_TO_K(65), \
+		.temp_fan_off = C_TO_K(40), \
+		.temp_fan_max = C_TO_K(58), \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_charger =
 	THERMAL_CHARGER;
