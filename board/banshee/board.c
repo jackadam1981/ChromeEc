438decef9b25c69658b4aae9fdaf33c10f9278e3
Matt Wang <matt_wang@compal.corp-partner.google.com>
2022-03-09 14:09:33 +0800

    banshee: modify ISL9241 current limit setting
    This modify is prevent the power lose when ac only power on the system.
    Set the 0x4C bit5 to 1 when AC only then set to 0 when battery present.
    
    ISL9241 0x4C Control3
    bit5 Input Current Limit Loop
    Disables the input current limit loop.
    0 = Enable input current limit loop (default)
    1 = Disable input current limit loop
    
    BUG=b:222038282
    BRANCH=brya
    TEST=AC only can power on the system to OS success. AC+DC boot to
    OS then unplug the battery the system power not lose.
    
    Signed-off-by: Matt Wang <matt_wang@compal.corp-partner.google.com>
    Change-Id: Ibadef0f523eaca532ec20cbff8a17ec14b6a4d19

--- board/banshee/board.c
+++ board/banshee/board.c
@@ -16,6 +16,7 @@
 #include "driver/accel_lis2dw12.h"
 #include "driver/accelgyro_lsm6dso.h"
 #include "driver/als_tcs3400.h"
+#include "driver/charger/isl9241.h"
 #include "fw_config.h"
 #include "hooks.h"
 #include "lid_switch.h"
@@ -44,3 +45,44 @@ static void board_chipset_suspend(void)
 {
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_chipset_suspend, HOOK_PRIO_DEFAULT);
+
+void board_set_charger_current_limit_deferred(void)
+{
+	int chgnum = 0;
+	int ctl3_val;
+	int rv;
+
+	rv = i2c_read16(chg_chips[chgnum].i2c_port,
+				chg_chips[chgnum].i2c_addr_flags,
+				ISL9241_REG_CONTROL3, &ctl3_val);
+
+	if (extpower_is_present() &&
+		(battery_get_disconnect_state() != BATTERY_NOT_DISCONNECTED))
+		/* AC only or AC+DC but battery is disconnect */
+		ctl3_val |= ISL9241_CONTROL3_INPUT_CURRENT_LIMIT;
+	else
+		ctl3_val &= ~ISL9241_CONTROL3_INPUT_CURRENT_LIMIT;
+
+	rv = i2c_write16(chg_chips[chgnum].i2c_port,
+				chg_chips[chgnum].i2c_addr_flags,
+				ISL9241_REG_CONTROL3, ctl3_val);
+
+	if (rv)
+		CPRINTF("Could not set charger input current limit! Error: %d\n"
+		, rv);
+}
+DECLARE_DEFERRED(board_set_charger_current_limit_deferred);
+DECLARE_HOOK(HOOK_SECOND, board_set_charger_current_limit_deferred,
+	HOOK_PRIO_DEFAULT);
+
+void battery_present_interrupt(enum gpio_signal signal)
+{
+	hook_call_deferred(&board_set_charger_current_limit_deferred_data, 0);
+}
+
+void board_init(void)
+{
+	gpio_enable_interrupt(GPIO_EC_BATT_PRES_ODL);
+	hook_call_deferred(&board_set_charger_current_limit_deferred_data, 0);
+}
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
