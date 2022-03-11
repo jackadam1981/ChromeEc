8508529c2035c2ae6ecad440ca1248cec848a423
Andrew McRae <amcrae@google.com>
2022-03-11 13:50:27 +1100

    zephyr: Add AP pwrseq optional external functions
    Allow board specific functions for selected functions,
    selected by CONFIG entries.
    
    BUG=b:223923728
    TEST=zmake build nivviks
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: Ie4cb4683e6ad4358af7535861cebfd966ef2a456

--- zephyr/test/ap_power/src/board.c
+++ zephyr/test/ap_power/src/board.c
@@ -6,6 +6,7 @@
 #include <zephyr.h>
 #include <ztest.h>
 
+#include <ap_power/ap_power_interface.h>
 #include <power_signals.h>
 
 static bool signal_PWR_ALL_SYS_PWRGD;
@@ -49,4 +50,17 @@ int board_power_signal_get(enum power_signal signal)
 	case PWR_PG_PP1P05:
 		return signal_PWR_PG_PP1P05;
 	}
+}
+
+void board_ap_power_force_shutdown(void)
+{
+}
+
+int board_ap_power_assert_pch_power_ok(int delay)
+{
+	return 0;
+}
+
+void board_ap_power_action_g3_s5(int delay, int signal_timeout)
+{
 }
