1143ab97ee4aac44dac38a3cee0e53847237bfd7
Ting Shen <phoenixshen@google.com>
2022-03-14 17:49:53 +0800

    WIP: zephyr: implement common tcpc shim
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I4e646e90fc6529f1d80cb94db54503cea50ba8fd

--- zephyr/shim/include/usbc/tcpc_ps8xxx.h
+++ zephyr/shim/include/usbc/tcpc_ps8xxx.h
@@ -3,34 +3,19 @@
  * found in the LICENSE file.
  */
 
-#define DT_DRV_COMPAT parade_ps8xxx
-
 #include <devicetree.h>
-#include "hooks.h"
 #include "driver/tcpm/ps8xxx_public.h"
-#include "usb_pd_tcpm.h"
-#include "usb_pd.h"
 
+#define PS8XXX_COMPAT parade_ps8xxx
 
-#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
-
-BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) > 0,
-		"No compatible TCPC instance found");
-
-#define USBC_PORT_TCPC(inst)                                                  \
+#define TCPC_CONFIG_PS8XXX(id)                                                \
 	{                                                                     \
 		.bus_type = EC_BUS_TYPE_I2C,                                  \
 		.i2c_info = {                                                 \
-			.port = I2C_PORT(DT_PHANDLE(                          \
-					DT_DRV_INST(inst), port)),            \
+			.port = I2C_PORT(DT_PHANDLE(id, port)),               \
 			.addr_flags = DT_STRING_UPPER_TOKEN(                  \
-					DT_DRV_INST(inst), i2c_addr_flags),   \
+					id, i2c_addr_flags),                  \
 		},                                                            \
 		.drv = &ps8xxx_tcpm_drv,                                      \
 	},                                                                    \
 
-const struct tcpc_config_t tcpc_config[] = {
-	DT_INST_FOREACH_STATUS_OKAY(USBC_PORT_TCPC)
-};
-
-#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
