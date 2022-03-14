39de5a27594c3aee5ef9918850eb3f93d5c0c039
Matt Wang <matt_wang@compal.corp-partner.google.com>
2022-03-14 19:52:30 +0800

    kinox: implement OBP function
    Add the two ADC pin for detect the ADP_ID and the PWR_IN_IMON.
    ADP_ID can identify the Barrel power device and the PWR_IN_IMON can
    detect the adptor input voltage.
    
    Use those ADC value to set the OBP pointer then trigger PROCHOT.
    
    BUG=b:211806236;b=213955278
    BRANCH=brask
    TEST=make BOARD=kinox pass
    
    Signed-off-by: Matt Wang <matt_wang@compal.corp-partner.google.com>
    Change-Id: I84455d1028b2cfc4297e10410092454f3dd61bc2

--- board/kinox/board.c
+++ board/kinox/board.c
@@ -4,6 +4,7 @@
  */
 
 #include "assert.h"
+#include "adc.h"
 #include "button.h"
 #include "charge_manager.h"
 #include "charge_state_v2.h"
@@ -19,6 +20,7 @@
 #include "switch.h"
 #include "throttle_ap.h"
 #include "usbc_config.h"
+#include "util.h"
 
 #include "gpio_list.h" /* Must come after other header files. */
 
@@ -35,6 +37,190 @@ const int usb_port_enable[USB_PORT_COUNT] = {
 BUILD_ASSERT(ARRAY_SIZE(usb_port_enable) == USB_PORT_COUNT);
 
 /******************************************************************************/
+/* ADP_ID control */
+struct adpater_id_params tio1_power[] = {
+	{
+	.min_voltage = 0x3FF,
+	.max_voltage = 0x3FF,
+	.watt = 120000,
+	.obp95 = 0x2D3,
+	.obp85 = 0x286
+	},
+};
+
+struct adpater_id_params tio2_power[] = {
+	{
+	.min_voltage = 0,
+	.max_voltage = 0x19,
+	.watt = 170000,
+	.obp95 = 0x400,
+	.obp85 = 0x394
+	},
+	{
+	.min_voltage = 0x19,
+	.max_voltage = 0x34,
+	.watt = 45000,
+	.obp95 = 0x10F,
+	.obp85 = 0x0F2
+	},
+	{
+	.min_voltage = 0x49,
+	.max_voltage = 0x69,
+	.watt = 65000,
+	.obp95 = 0x187,
+	.obp85 = 0x15E
+	},
+	{
+	.min_voltage = 0xC1,
+	.max_voltage = 0xDD,
+	.watt = 120000,
+	.obp95 = 0x2D3,
+	.obp85 = 0x286
+	},
+	{
+	.min_voltage = 0x8C,
+	.max_voltage = 0xAE,
+	.watt = 150000,
+	.obp95 = 0x387,
+	.obp85 = 0x328
+	},
+	{
+	.min_voltage = 0x182,
+	.max_voltage = 0x199,
+	.watt = 170000,
+	.obp95 = 0x400,
+	.obp85 = 0x394
+	},
+};
+
+struct adpater_id_params tiny_power[] = {
+	{
+	.min_voltage = 0x19,
+	.max_voltage = 0x34,
+	.watt = 45000,
+	.obp95 = 0x10F,
+	.obp85 = 0x0F2
+	},
+	{
+	.min_voltage = 0x49,
+	.max_voltage = 0x69,
+	.watt = 65000,
+	.obp95 = 0x187,
+	.obp85 = 0x15E
+	},
+	{
+	.min_voltage = 0x8C,
+	.max_voltage = 0xAE,
+	.watt = 90000,
+	.obp95 = 0x21E,
+	.obp85 = 0x1E5
+	},
+	{
+	.min_voltage = 0xC1,
+	.max_voltage = 0xDD,
+	.watt = 120000,
+	.obp95 = 0x2D3,
+	.obp85 = 0x286
+	},
+	{
+	.min_voltage = 0xED,
+	.max_voltage = 0x11C,
+	.watt = 135000,
+	.obp95 = 0x32D,
+	.obp85 = 0x2D7
+	},
+	{
+	.min_voltage = 0x135,
+	.max_voltage = 0x16A,
+	.watt = 150000,
+	.obp95 = 0x387,
+	.obp85 = 0x328
+	},
+	{
+	.min_voltage = 0x182,
+	.max_voltage = 0x1BD,
+	.watt = 170000,
+	.obp95 = 0x400,
+	.obp85 = 0x394
+	},
+	{
+	.min_voltage = 0x27B,
+	.max_voltage = 0x2CB,
+	.watt = 230000,
+	.obp95 = 0x569,
+	.obp85 = 0x4D7
+	},
+};
+
+struct adpater_id_params *power_type[8];
+
+static int adp_id_value;
+static int adp_id_value_debounce;
+static int adp_type;
+static int adp_finial_adc_value;
+static int adp_obp95;
+static int adp_obp85;
+
+DECLARE_DEFERRED(adp_id_deferred);
+DECLARE_DEFERRED(board_obp_deferred);
+
+void adp_id_deferred(void)
+{
+	int i = 0;
+
+	adp_id_value = adc_read_channel(ADC_ADP_ID);
+
+	if (!adp_id_value_debounce) {
+		adp_id_value_debounce = adp_id_value;
+		/* for delay the 400ms to get the next APD_ID value */
+		hook_call_deferred(&adp_id_deferred_data, 400 * MSEC);
+	} else if (adp_id_value_debounce == 0x3FF && adp_id_value == 0x3FF) {
+		adp_finial_adc_value = adp_id_value;
+		adp_type = TIO1;
+	} else if (adp_id_value_debounce < 0x3FF && adp_id_value == 0x3FF) {
+		adp_finial_adc_value = adp_id_value_debounce;
+		adp_type = TIO2;
+	} else if (adp_id_value_debounce < 0x3FF && adp_id_value < 0x3FF) {
+		adp_finial_adc_value = adp_id_value;
+		adp_type = TINY;
+	}
+
+	switch (adp_type) {
+	case TIO1:
+		memcpy(power_type, &tio1_power, sizeof(tio1_power));
+		break;
+	case TIO2:
+		memcpy(power_type, &tio2_power, sizeof(tio2_power));
+		break;
+	case TINY:
+		memcpy(power_type, &tiny_power, sizeof(tiny_power));
+		break;
+	}
+
+	for (i = 0; i < sizeof(power_type) && adp_type; i++) {
+		if (adp_finial_adc_value <= power_type[i]->max_voltage) {
+			adp_obp95 = power_type[i]->obp95;
+			adp_obp85 = power_type[i]->obp85;
+			break;
+		}
+	}
+}
+
+void board_obp_deferred(void)
+{
+	int imon_value = adc_read_channel(ADC_PWR_IN_IMON);
+	int active = 1;
+
+	if (imon_value > adp_obp95)
+		/* Trigger Prochot */
+		active = 0;
+	else if (imon_value < adp_obp85)
+		/* Release Prochot */
+		active = 1;
+
+	gpio_set_level(GPIO_EC_PROCHOT_ODL, active);
+}
+DECLARE_HOOK(HOOK_TICK, board_obp_deferred, HOOK_PRIO_DEFAULT);
 
 int board_set_active_charge_port(int port)
 {
@@ -195,5 +381,6 @@ DECLARE_HOOK(HOOK_INIT, adp_state_init, HOOK_PRIO_CHARGE_MANAGER_INIT + 1);
 static void board_init(void)
 {
 	gpio_enable_interrupt(GPIO_BJ_ADP_PRESENT_ODL);
+	hook_call_deferred(&adp_id_deferred_data, 200 * MSEC);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
