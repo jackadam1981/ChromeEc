03c8540ff25ae8226473a0c5ffde7745f138a8a8
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-03-16 16:30:04 -0700

    zephyr: Enhance tcpc shim to support multiple drivers
    This CL enhances existing shim code to support multiple tcpc drivers.
    Supported Drivers: PS8705, PS8751, PS8755, PS8805, PS8815 and FUSB302.
    Support for more drivers can be added easily if required.
    
    BUG=b:224751369
    BRANCH=none
    TEST=zmake testall
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: Ib03610998407a192b8050371105fd83c2a6ad5a8

--- zephyr/shim/src/tcpc.c
+++ zephyr/shim/src/tcpc.c
@@ -3,13 +3,24 @@
  * found in the LICENSE file.
  */
 
-#define DT_DRV_COMPAT parade_ps8xxx
+#define DT_DRV_COMPAT cros_tcpc
 
 #include <devicetree.h>
 #include "hooks.h"
-#include "driver/tcpm/ps8xxx_public.h"
 #include "usb_pd_tcpm.h"
 #include "usb_pd.h"
+
+/* Include drivers if enabled */
+#if defined(CONFIG_PLATFORM_EC_USB_PD_TCPM_PS8705)     || \
+	defined(CONFIG_PLATFORM_EC_USB_PD_TCPM_PS8751) || \
+	defined(CONFIG_PLATFORM_EC_USB_PD_TCPM_PS8755) || \
+	defined(CONFIG_PLATFORM_EC_USB_PD_TCPM_PS8805) || \
+	defined(CONFIG_PLATFORM_EC_USB_PD_TCPM_PS8815)
+#include "driver/tcpm/ps8xxx_public.h"
+#endif
+#ifdef CONFIG_PLATFORM_EC_USB_PD_TCPM_FUSB302
+#include "fusb302.h"
+#endif
 
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
@@ -26,7 +37,8 @@ BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) > 0,
 			.addr_flags = DT_STRING_UPPER_TOKEN(                  \
 					DT_DRV_INST(inst), i2c_addr_flags),   \
 		},                                                            \
-		.drv = &ps8xxx_tcpm_drv,                                      \
+		.drv = &DT_STRING_TOKEN(DT_DRV_INST(inst), drv),              \
+		.flags = DT_PROP(DT_DRV_INST(inst), flags),                   \
 	},                                                                    \
 
 const struct tcpc_config_t tcpc_config[] = {
