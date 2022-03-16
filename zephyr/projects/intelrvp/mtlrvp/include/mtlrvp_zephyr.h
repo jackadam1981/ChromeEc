616faac86555f4cea64d49293bb34e70081780bc
Brandon Breitenstein <brandon.breitenstein@intel.com>
2022-03-16 13:56:30 -0700

    zephyr-mtlrvp: Add support to get BOARD ID
    Add support to get board ID for MTLRVP
    
    BUG=none
    BRANCH=none
    TEST= zmake mtlrvp and verify BOARD ID and system booting
    
    Cq-Depend: chromium:3462922
    Signed-off-by: Brandon Breitenstein <brandon.breitenstein@intel.com>
    Change-Id: I63167500eb485413e30a8872b530c3add544df3b

---
+++ zephyr/projects/intelrvp/mtlrvp/include/mtlrvp_zephyr.h
@@ -0,0 +1,14 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Intel MTL-RVP specific configuration */
+
+#ifndef __MTLRVP_BOARD_H
+#define __MTLRVP_BOARD_H
+
+#define GPIO_SYS_PWROK_EC	GPIO_PCH_SYS_PWROK
+#define GPIO_ENTERING_RW	GPIO_UNIMPLEMENTED
+
+#endif /* __MTLRVP_BOARD_H */
