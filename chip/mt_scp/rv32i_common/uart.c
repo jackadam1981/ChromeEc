092ff1fc4e06ae24ba1b2f9c4a9344f7263b085b
Tinghan Shen <tinghan.shen@mediatek.com>
2022-03-21 16:55:39 +0800

    TEST-ONLY: core uart config
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: Ic6adcc5b8711c33dbf0c1739caedb2143bd7b56f

--- chip/mt_scp/rv32i_common/uart.c
+++ chip/mt_scp/rv32i_common/uart.c
@@ -154,7 +154,7 @@ DECLARE_IRQ(UART_INTC_GROUP, uart_irq_handler, 0);
 #error "APUART task hasn't defined in ec.tasklist."
 #endif
 
-void uart_task(void)
+void uart_task(void *u)
 {
 	while (1) {
 		if (uart_rx_available() || tx_started)
