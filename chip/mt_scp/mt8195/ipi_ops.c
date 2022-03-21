ed089a9afa9537e6fbad60cde1740a52fd7d4f1f
Tinghan Shen <tinghan.shen@mediatek.com>
2022-03-21 15:47:21 +0800

    chip/mt_scp: support mt8195 core 1 ipi
    Add IPI control operations for mt8195 SCP core 1
    
    BRANCH=none
    BUG=b:193814857
    TEST=make BOARD=cherry_scp_core1
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: Idbfdad0c870c945b6211d60ff4ebfeca52588c41

---
+++ chip/mt_scp/mt8195/ipi_ops.c
@@ -0,0 +1,33 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "registers.h"
+#include "ipi_chip.h"
+
+#ifdef CHIP_VARIANT_MT8195_CORE1
+void ipi_op_wake_ap(void)
+{
+}
+
+int ipi_op_scp2ap_is_irq_set(void)
+{
+	return SCP_SSHUB2APMCU_IPC_SET & IPC_SCP2HOST;
+}
+
+void ipi_op_scp2ap_irq_set(void)
+{
+	SCP_SSHUB2APMCU_IPC_SET = IPC_SCP2HOST;
+}
+
+void ipi_op_ap2scp_irq_clr(void)
+{
+	SCP_GIPC_IN_CLR = GIPC_IN(4);
+}
+
+int ipi_op_ap2scp_is_irq_set(void)
+{
+	return SCP_GIPC_IN_SET & GIPC_IN(4);
+}
+#endif
