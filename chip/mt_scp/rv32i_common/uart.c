860110670941c009c2f9d8d255b9bb2b5760a88f
Tinghan Shen <tinghan.shen@mediatek.com>
2022-03-21 16:55:39 +0800

    HACK: core 0 with uart write only
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I6f33d792eb3ec3625609aa9b118e043dcda0d9c0

--- chip/mt_scp/rv32i_common/uart.c
+++ chip/mt_scp/rv32i_common/uart.c
@@ -55,8 +55,10 @@ void uart_init(void)
 	UART_IER(UARTN) |= UART_IER_RDI;
 
 #if (UARTN < SCP_UART_COUNT)
+#ifdef CHIP_VARIANT_MT8195_CORE1
 	task_enable_irq(UART_TX_IRQ(UARTN));
 	task_enable_irq(UART_RX_IRQ(UARTN));
+#endif
 #endif
 
 	init_done = 1;
@@ -125,11 +127,14 @@ void uart_tx_stop(void)
 
 static void uart_process(void)
 {
+#ifdef CHIP_VARIANT_MT8195_CORE1
 	uart_process_input();
+#endif
 	uart_process_output();
 }
 
 #if (UARTN < SCP_UART_COUNT)
+#ifdef CHIP_VARIANT_MT8195_CORE1
 static void uart_irq_handler(void)
 {
 	extern volatile int ec_int;
@@ -148,6 +153,17 @@ static void uart_irq_handler(void)
 	}
 }
 DECLARE_IRQ(UART_INTC_GROUP, uart_irq_handler, 0);
+#else
+void uart_task(void *u)
+{
+	while (1) {
+		if (tx_started)
+			uart_process();
+		else
+			task_wait_event(UART_IDLE_WAIT_US);
+	}
+}
+#endif
 #else
 
 #ifndef HAS_TASK_APUART
