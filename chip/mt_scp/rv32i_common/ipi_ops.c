31fffd3c07e2252bc34cf15132266a501c426797
Tinghan Shen <tinghan.shen@mediatek.com>
2022-03-21 15:46:38 +0800

    chip/mt_scp: extract core dependent ipi operations
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: Ib943a398c293b77a19283eef8bd9d9f061d7fb8f

---
+++ chip/mt_scp/rv32i_common/ipi_ops.c
@@ -0,0 +1,32 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "registers.h"
+#include "ipi_chip.h"
+
+__overridable  void ipi_op_wake_ap(void)
+{
+	SCP_SCP2SPM_IPC_SET = IPC_SCP2HOST;
+}
+
+__overridable int ipi_op_scp2ap_is_irq_set(void)
+{
+	return SCP_SCP2APMCU_IPC_SET & IPC_SCP2HOST;
+}
+
+__overridable void ipi_op_scp2ap_irq_set(void)
+{
+	SCP_SCP2APMCU_IPC_SET = IPC_SCP2HOST;
+}
+
+__overridable void ipi_op_ap2scp_irq_clr(void)
+{
+	SCP_GIPC_IN_CLR = GIPC_IN(0);
+}
+
+__overridable int ipi_op_ap2scp_is_irq_set(void)
+{
+	return SCP_GIPC_IN_SET & GIPC_IN(0);
+}
