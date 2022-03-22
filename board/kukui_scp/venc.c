96ba1ac1d28453d28d7eb26012fa386a51701aa8
Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
2022-03-22 17:18:18 +0800

    TEST-ONLY: ec: fix venc build error
    fix venc build error
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Change-Id: I7641a44ecde376b8a72b7ee71edf2bb187c9b2b1

--- board/kukui_scp/venc.c
+++ board/kukui_scp/venc.c
@@ -32,7 +32,7 @@ static struct consumer const event_venc_consumer = {
 static venc_msg_handler mtk_venc_msg_handle[VENC_MAX];
 
 /* Stub functions only provided by private overlays. */
-#ifndef HAVE_PRIVATE_MT8183
+#ifdef HAVE_PRIVATE_MT8183
 void venc_h264_msg_handler(void *data) {}
 #endif
 
