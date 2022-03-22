ae727e71e06e6186270bcb24de38a51b1d19eb09
Andrew McRae <amcrae@google.com>
2022-03-23 10:31:36 +1100

    zephyr: Add alternative LED handling
    Add alternative LED handling.
    Provides a Zephyr based LED module that splits
    policy from LED behaviour and actions.
    Supports both multi or single PWM and GPIO based LEDs.
    
    v2: Use range instead of value, allow alt-colors for flash.
    v3: Use common dts and add LED PWM config to nereid.
    v4: Reworked to allow multiple color sets cycling through.
        Added charger support.
    v5: Added skeleton of GPIO support. Added common LED API.
    v6: Split from Nissa config
    
    BUG=b:225323731
    TEST=zmake build nivviks; flash and look at LEDs
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: Ied03970b6c4a50fe78f3bf7763672239a76a64b9

---
+++ zephyr/drivers/cros_led/led_gpio.h
@@ -0,0 +1,35 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_LED_GPIO_H__
+#define __CROS_EC_LED_GPIO_H__
+
+#define COMPAT_GPIO	cros_ec_gpio_leds
+
+#if DT_HAS_COMPAT_STATUS_OKAY(COMPAT_GPIO)
+
+/*
+ * Enum representing the GPIO LED handlers.
+ * One is created for each instance of the GPIO LED
+ * handlers.
+ */
+enum led_gpio_hand {
+	DT_FOREACH_STATUS_OKAY(COMPAT_GPIO, GEN_TYPE_INDEX_ENUM)
+};
+
+DT_FOREACH_STATUS_OKAY(COMPAT_GPIO, GEN_ACTION_ENUM_LIST)
+
+/*
+ * GPIO LED functions.
+ */
+void gpio_led_init(void);
+void gpio_get_led_brightness(enum led_gpio_hand h, uint8_t *br);
+void gpio_set_led_brightness(enum led_gpio_hand h, const uint8_t *br);
+void gpio_set_led_action(enum led_gpio_hand h, int action);
+void gpio_led_shutdown(enum led_gpio_hand h);
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(COMPAT_GPIO) */
+
+#endif /* __CROS_EC_LED_GPIO_H__ */
