f01186a5a11a144ffe3d5eb385a93f4729b82e73
Tinghan Shen <tinghan.shen@mediatek.com>
2022-03-22 16:33:04 +0800

    add GPR register
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I2020366b0198f89a5c5c75952f258195db8d44a7

--- chip/mt_scp/rv32i_common/registers.h
+++ chip/mt_scp/rv32i_common/registers.h
@@ -150,6 +150,12 @@
 #define SCP_CORE0_MON_LR_LATCH		REG32(SCP_REG_BASE + 0x300D4)
 #define SCP_CORE0_MON_SP_LATCH		REG32(SCP_REG_BASE + 0x300D8)
 
+/* General purpose register */
+#define SCP_CORE0_GPR_COUNT		8
+#define SCP_CORE0_GPR(x)		REG32_ADDR(SCP_REG_BASE + 0x30040)[x]
+#define   SCP_CORE0_INIT_DONE		BIT(0)
+#define   SCP_CORE1_INIT_DONE		BIT(1)
+
 /* INTC */
 #define SCP_INTC_WORD(irq)		((irq) >> 5) /* word length = 2^5 */
 #define SCP_INTC_BIT(irq)		((irq) & 0x1F) /* bit shift =LSB[0:4] */
