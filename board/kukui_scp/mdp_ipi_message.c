d2d314940758694c3c8a9f9605423adadfb7f6f2
Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
2022-03-23 16:07:30 +0800

    mt_scp: fix venc and mdp build error
    fix venc and mdp build error when add mt8186 driver
    
    BRANCH=none
    BUG=b:218771968
    TEST=build corsola_scp pass
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Change-Id: I41b973fe3e7810efa231ea531b9a0942f000504f

--- board/kukui_scp/mdp_ipi_message.c
+++ board/kukui_scp/mdp_ipi_message.c
@@ -29,12 +29,10 @@ static struct consumer const event_mdp_consumer = {
 
 /* Stub functions only provided by private overlays. */
 #ifndef HAVE_PRIVATE_MT8183
-void mdp_common_init(void)
-{
-}
-void mdp_ipi_task_handler(void *pvParameters)
-{
-}
+#ifndef HAVE_PRIVATE_MT8186
+void mdp_common_init(void) {}
+void mdp_ipi_task_handler(void *pvParameters) {}
+#endif
 #endif
 
 static void event_mdp_written(struct consumer const *consumer, size_t count)
