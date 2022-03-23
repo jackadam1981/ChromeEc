cfa92f0171ddeb2c88e9d5c8e6a899a6379bf2a9
poornima tom <poornima.tom@intel.com>
2022-03-23 23:59:37 +0530

    power: Add PSRC support for DTT
    For DTT Power Participant, PSRC is one of the object.
    PSRC is allocated in EC memory with 8 bit size. Whenever
    there is a power source change, the lower nibble of this
    memory must be updated with the source information and
    AP must be notified about it by an sci event.
    Also, for AC removal or low battery level condition,
    a PROCHOT must be asserted and the upper nibble
    indicating the sequence number must be incremented.
    Once prochot is asserted, if there is no PBOK ACK
    from AP within 2sec, PROCHOT is deasserted.
    Document - #626707 has more details. This CL consists
    of PSRC updation, PROCHOT assertion/deassertion in the
    absence of PBOK and sci event generation.
    CL also has zephyr support for the newly added config.
    
    BUG=b:205928013
    BRANCH=None
    TEST=make buildall
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: Id98a42a3d4c5418b89253da3da62ab91f6e83ebe

--- include/config.h
+++ include/config.h
@@ -1969,6 +1969,9 @@
 /* Enable fake shared memory buffer, which is used by emulators. */
 #undef CONFIG_FAKE_SHMEM
 
+/* Enable Power Participant Support for DTT(Dynamic Tuning Technology) */
+#undef CONFIG_DTT_POWER_STATUS
+
 /*****************************************************************************/
 /* Number of cooling fans. Undef if none. */
 #undef CONFIG_FANS
