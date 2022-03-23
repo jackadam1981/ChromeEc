2f17005982c7dec5203523d66bd19a43c4e5fc7a
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-03-23 14:44:58 +0800

    primus: send suspend mode to trackpoint
    BUG=b:226215983
    BRANCH=none
    TEST=make -j BOARD=primus
    TEST=verify by EE
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I139c8a628f9c01ce299b7ec650839f67ca99a12e

---
+++ board/primus/ps2.h
@@ -0,0 +1,22 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Primus board-specific PS2 configuration */
+
+#define TP_READ_ID		0xE1	/* Sent for device identification */
+#define TP_COMMAND		0xE2	/* Commands start with this */
+
+/*
+ * Toggling Flag bits
+ */
+#define TP_TOGGLE		0x47	/* Toggle command */
+
+/*
+ * Valid first byte responses to the "Read Secondary ID" (0xE1) command.
+ * 0x01 was the original IBM trackpoint, others implement very limited
+ * subset of trackpoint features.
+ */
+#define TP_VARIANT_ELAN			0x03
+#define TP_VARIANT_SYNAPTICS		0x06
