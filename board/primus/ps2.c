6eeacf9c61267bd693a50214c2c8aa1aac0bc585
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-03-23 14:46:34 +0800

    primus: send suspend mode to synaptics trackpoint
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Id180c15f8ef2e5d7cd3dd80d6ac2bf60e1c7da18

--- board/primus/ps2.c
+++ board/primus/ps2.c
@@ -7,6 +7,7 @@
 #include "hooks.h"
 #include "keyboard_8042.h"
 #include "ps2_chip.h"
+#include "time.h"
 
 void send_aux_data_to_device(uint8_t data)
 {
@@ -18,3 +19,17 @@ static void board_init(void)
 	ps2_enable_channel(NPCX_PS2_CH1, 1, send_aux_data_to_host_interrupt);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+
+/* Called on AP S0 -> S3 transition */
+static void ps2_suspend(void)
+{
+	ps2_transmit_byte(NPCX_PS2_CH1, 0xe2);
+	msleep(10);
+	ps2_transmit_byte(NPCX_PS2_CH1, 0x47);
+	msleep(10);
+	ps2_transmit_byte(NPCX_PS2_CH1, 0x20);
+	msleep(10);
+	ps2_transmit_byte(NPCX_PS2_CH1, 0x10);
+	msleep(10);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, ps2_suspend, HOOK_PRIO_DEFAULT);
