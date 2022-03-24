6fe6ad0a8ebeb71b0d0ba01f7047210c487e7404
Keith Short <keithshort@chromium.org>
2022-03-24 13:04:49 -0600

    nissa: convert HOOK_INIT to SYS_INIT
    Convert all HOOK_INIT calls to the equivalent SYS_INIT.
    
    BUG=b:226434387
    BRANCH=none
    TEST=zmake testall
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: I9fde383e7ee7ebf5e939e39ac4145b1d585b4a67

--- zephyr/projects/nissa/src/nereid/board_config.c
+++ zephyr/projects/nissa/src/nereid/board_config.c
@@ -20,8 +20,9 @@
 
 LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
 
-static void nereid_subboard_init(void)
+static int nereid_subboard_init(const struct device *unused)
 {
+	ARG_UNUSED(unused);
 	enum nissa_sub_board_type sb = nissa_get_sb_type();
 
 	/*
@@ -66,22 +67,27 @@ static void nereid_subboard_init(void)
 			GPIO_DT_FROM_ALIAS(gpio_hpd_odl),
 			GPIO_INPUT);
 	}
+
+	return 0;
 }
-DECLARE_HOOK(HOOK_INIT, nereid_subboard_init, HOOK_PRIO_POST_FIRST);
+SYS_INIT(nereid_subboard_init, APPLICATION, HOOK_PRIO_POST_FIRST);
 
 /*
  * Enable interrupts
  */
-static void board_init(void)
+static int board_init(const struct device *unused)
 {
+	ARG_UNUSED(unused);
 	/*
 	 * Enable USB-C interrupts.
 	 */
 	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0));
 	if (board_get_usb_pd_port_count() == 2)
 		gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c1));
+
+	return 0;
 }
-DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
+SYS_INIT(board_init, APPLICATION, HOOK_PRIO_DEFAULT);
 
 __override void board_hibernate(void)
 {
