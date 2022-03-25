b5e0804b60ff7bcfea19a39284db8f07d1540389
felixtwchang <felixtw_chang@compal.corp-partner.google.com>
2022-03-25 16:14:09 +0800

    Banshee: modify fan frequency
    Modify the Fan frequency to 25KHz, to meet fan spec.
    
    BUG=b:223492897
    BRANCH=none
    TEST=Make board passed
    
    Signed-off-by: felixtwchang <felixtw_chang@compal.corp-partner.google.com>
    Change-Id: I96845d9b61dd8f0b2295a10f8e305555448a4742

--- board/banshee/pwm.c
+++ board/banshee/pwm.c
@@ -40,7 +40,7 @@ const struct pwm_t pwm_channels[] = {
 	[PWM_CH_FAN] = {
 		.channel = 5,
 		.flags = PWM_CONFIG_OPEN_DRAIN | PWM_CONFIG_DSLEEP,
-		.freq = 1000
+		.freq = 25000
 	},
 	[PWM_CH_POWER_LED_W] = {
 		.channel = 7,
