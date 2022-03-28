db4f97a41e20a81ba7ce20aeb558634941f39f78
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-03-28 15:39:08 +0800

    DNS: moli: add second CEC
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I07c64c52ad8a5e24c4717220360ab7eee0f506ae

--- chip/npcx/cec.c
+++ chip/npcx/cec.c
@@ -994,6 +994,10 @@ DECLARE_EVENT_SOURCE(EC_MKBP_EVENT_CEC_MESSAGE, cec_get_next_msg);
 static void cec_init(void)
 {
 	int mdl = NPCX_MFT_MODULE_1;
+#ifdef CONFIG_TWO_CEC
+	/* We use GPIOA7/ TB2 as CEC2 */
+	int mdl2 = NPCX_MFT_MODULE_2;
+#endif
 
 	/* APB1 is the clock we base the timers on */
 	apb1_freq_div_10k = clock_get_apb1_freq()/10000;
@@ -1012,6 +1016,28 @@ static void cec_init(void)
 
 	/* Ensure the CEC bus is not pulled low by default on startup. */
 	gpio_set_level(CEC_GPIO_OUT, 1);
+
+#ifdef CONFIG_TWO_CEC
+
+	/* APB1 is the clock we base the timers on */
+	apb1_freq_div_10k = clock_get_apb1_freq()/10000;
+
+	/* Ensure Multi-Function timer is powered up. */
+	CLEAR_BIT(NPCX_PWDWN_CTL(mdl2), NPCX_PWDWN_CTL1_MFT2_PD);
+
+	/* Mode 2 - Dual-input capture */
+	SET_FIELD(NPCX_TMCTRL(mdl2), NPCX_TMCTRL_MDSEL_FIELD, NPCX_MFT_MDSEL_2);
+
+	/* Enable capture TCNT1 into TCRA and preset TCNT1. */
+	SET_BIT(NPCX_TMCTRL(mdl2), NPCX_TMCTRL_TBEN);  //WIP
+
+	/* If RO doesn't set it, RW needs to set it explicitly. */
+	gpio_set_level(CEC2_GPIO_PULL_UP, 1);
+
+	/* Ensure the CEC bus is not pulled low by default on startup. */
+	gpio_set_level(CEC2_GPIO_OUT, 1);
+
+#endif
 
 	CPRINTS("CEC initialized");
 }
