094289626ef76d86f0e170c84c66155db1d4387c
Tinghan Shen <tinghan.shen@mediatek.com>
2022-03-28 13:56:32 +0800

    chip/mt_scp: add mt8195 core 1 clock
    BRANCH=none
    BUG=b:193814857
    TEST=none
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I0ab64f2c239ca2d032b0e49fa541a1ded625954a

---
+++ chip/mt_scp/mt8195/clock_mt8195_core1.c
@@ -0,0 +1,14 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Clocks, PLL and power settings */
+
+#include "clock.h"
+
+void clock_init(void)
+{
+	/* clock is controlled by core 0 */
+	return;
+}
