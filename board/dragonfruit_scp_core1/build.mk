5911499612cc8dfc55cf82119ffc29c97a4a6ed7
Tinghan Shen <tinghan.shen@mediatek.com>
2022-03-28 14:15:21 +0800

    board/dragonfruit_scp_core1: add dragonfruit SCP core 1 board
    Add dragonfruit SCP core 1 board and modify SRAM configurations to
    use the spare 64KB SRAM.
    
    BRANCH=none
    BUG=b:193814857
    TEST=make BOARD=dragonfruit_scp_core1
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I23cfcb7dd24f67d27b0ee8adefe4b58c0b67d059

---
+++ board/dragonfruit_scp_core1/build.mk
@@ -0,0 +1,11 @@
+# -*- makefile -*-
+# Copyright 2022 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Board specific files build
+#
+
+CHIP:=mt_scp
+CHIP_VARIANT:=mt8195_core1
+BASEBOARD:=mtscp-rv32i
