9fc3e3fdec4ab153c844d5360a70d50f4dabe9d8
BJ Chen <bj.chen@ite.com.tw>
2022-03-30 18:38:30 +0800

    TESTONLY: testcode trials
    BUG=none
    TEST=none
    BRANCH=none
    
    Signed-off-by: BJ Chen <bj.chen@ite.com.tw>
    Change-Id: Iee1be7fb6a1faefb38c05a602d3147be92391dc4

--- common/console_output.c
+++ common/console_output.c
@@ -9,7 +9,17 @@
 #include "uart.h"
 #include "usb_console.h"
 #include "util.h"
-
+/***********************************************************/
+/***********************************************************/
+/***********************************************************/
+#include <errno.h>
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <device.h>
+#include <drivers/spi.h>
+/***********************************************************/
+/***********************************************************/
+/***********************************************************/
 #ifdef CONFIG_CONSOLE_CHANNEL
 /* Default to all channels active */
 #ifndef CC_DEFAULT
@@ -35,7 +45,390 @@ static const char * const channel_names[] = {
 BUILD_ASSERT(ARRAY_SIZE(channel_names) == CC_CHANNEL_COUNT);
 /* ensure that we are not silently masking additional channels */
 BUILD_ASSERT(CC_CHANNEL_COUNT <= 8*sizeof(uint32_t));
+/***********************************************************/
+/***********************************************************/
+/***********************************************************/
 
+
+/* Console commands & test codes of IT8XXX2 SPI */
+static const struct device *spi_dev;
+static const struct device *spi_dev0;
+static const struct device *spi_dev1;
+uint8_t spi_channel_status=0;
+
+#define	SPI_CH0_RDY	1
+#define	SPI_CH1_RDY	2
+#define	SPI_ALL_RDY	(SPI_CH0_RDY | SPI_CH1_RDY)
+
+static const struct spi_config spi_cfg = {
+	.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB |
+		     SPI_MODE_CPOL | SPI_MODE_CPHA,
+	.frequency = 4000000,
+	.slave = 0,
+};
+
+void spi_we(uint8_t spi_ch)
+{
+	int err;
+	static uint8_t tx_buffer[1];
+	uint8_t cnt;
+
+	const struct spi_buf tx_buf = {
+		.buf = tx_buffer,
+		.len = sizeof(tx_buffer)
+	};
+	const struct spi_buf_set tx = {
+		.buffers = &tx_buf,
+		.count = 1
+	};
+
+	struct spi_buf rx_buf = {
+		.buf = NULL,
+		.len = 0
+	};
+	const struct spi_buf_set rx = {
+		.buffers = &rx_buf,
+		.count = 0
+	};
+
+	tx_buffer[0] = 0x06;
+
+	printk("[user] %s\r\n", __func__);
+	printk("[user] len txbuf=%d\r\n", tx_buf.len);
+	printk("[user] len tx_buffer=%d\r\n", sizeof(tx_buffer));
+
+	for(cnt=0 ; cnt < tx_buf.len ; cnt++) {
+		printk("[user] tx_buffer[%d]=0x%X\r\n", cnt, tx_buffer[cnt]);
+	}
+	spi_dev = spi_ch ? (spi_dev1):(spi_dev0);
+	err = spi_transceive(spi_dev, &spi_cfg, &tx, &rx);
+
+	if (err)
+		printk("SPI error: %d\n", err);
+
+}
+
+
+void spi_a3(uint8_t spi_ch)
+{
+	int err;
+	static uint8_t tx_buffer[1];
+	uint8_t cnt;
+
+	const struct spi_buf tx_buf = {
+		.buf = tx_buffer,
+		.len = sizeof(tx_buffer)
+	};
+	const struct spi_buf_set tx = {
+		.buffers = &tx_buf,
+		.count = 1
+	};
+
+	struct spi_buf rx_buf = {
+		.buf = NULL,
+		.len = 0
+	};
+	const struct spi_buf_set rx = {
+		.buffers = &rx_buf,
+		.count = 0
+	};
+
+	tx_buffer[0] = 0xA3;
+
+	printk("[user] %s\r\n", __func__);
+	printk("[user] len txbuf=%d\r\n", tx_buf.len);
+	printk("[user] len tx_buffer=%d\r\n", sizeof(tx_buffer));
+
+	for(cnt=0 ; cnt < tx_buf.len ; cnt++) {
+		printk("[user] tx_buffer[%d]=0x%X\r\n", cnt, tx_buffer[cnt]);
+	}
+	spi_dev = spi_ch ? (spi_dev1):(spi_dev0);
+	err = spi_transceive(spi_dev, &spi_cfg, &tx, &rx);
+
+	if (err)
+		printk("SPI error: %d\n", err);
+
+}
+
+
+void spi_9f(uint8_t spi_ch)
+{
+	int err;
+	static uint8_t tx_buffer[1];
+	static uint8_t rx_buffer[3];
+	uint8_t cnt;
+
+	const struct spi_buf tx_buf = {
+		.buf = tx_buffer,
+		.len = sizeof(tx_buffer)
+	};
+	const struct spi_buf_set tx = {
+		.buffers = &tx_buf,
+		.count = 1
+	};
+
+	struct spi_buf rx_buf = {
+		.buf = NULL,
+		.len = 0
+	};
+	const struct spi_buf_set rx = {
+		.buffers = &rx_buf,
+		.count = 0
+	};
+
+	printk("[user] %s\r\n", __func__);
+	tx_buffer[0] = 0x9F;
+
+	printk("[user] len txbuf=%d\r\n", tx_buf.len);
+	printk("[user] len tx_buffer=%d\r\n", sizeof(tx_buffer));
+
+	for(cnt=0 ; cnt < tx_buf.len ; cnt++) {
+		printk("[user] tx_buffer[%d]=0x%X\r\n", cnt, tx_buffer[cnt]);
+	}
+
+	spi_dev = spi_ch ? (spi_dev1):(spi_dev0);
+
+	err = spi_transceive(spi_dev, &spi_cfg, &tx, &rx);
+
+	if (err) {
+		printk("SPI error: %d\n", err);
+	} else {
+		/* Connect MISO to MOSI for loopback */
+		printk("TX sent: %x\n", tx_buffer[0]);
+		printk("RX recv: %x\n", rx_buffer[0]);
+		printk("RX recv: %x\n", rx_buffer[1]);
+		printk("RX recv: %x\n", rx_buffer[2]);
+		tx_buffer[0]++;
+	}
+
+
+}
+
+
+void spi_read_id(uint8_t spi_ch)
+{
+	int err;
+	static uint8_t tx_buffer[1];
+	static uint8_t rx_buffer[3];
+	uint8_t cnt;
+
+	const struct spi_buf tx_buf = {
+		.buf = tx_buffer,
+		.len = sizeof(tx_buffer)
+	};
+	const struct spi_buf_set tx = {
+		.buffers = &tx_buf,
+		.count = 1
+	};
+
+	struct spi_buf rx_buf = {
+		.buf = rx_buffer,
+		.len = sizeof(rx_buffer),
+	};
+	const struct spi_buf_set rx = {
+		.buffers = &rx_buf,
+		.count = 1
+	};
+
+	printk("[user] %s\r\n", __func__);
+	tx_buffer[0] = 0x9F;
+
+	printk("[user] len txbuf=%d\r\n", tx_buf.len);
+	printk("[user] len tx_buffer=%d\r\n", sizeof(tx_buffer));
+
+	for(cnt=0 ; cnt < tx_buf.len ; cnt++) {
+		printk("[user] tx_buffer[%d]=0x%X\r\n", cnt, tx_buffer[cnt]);
+	}
+
+	spi_dev = spi_ch ? (spi_dev1):(spi_dev0);
+	printk("SPI Read ID for Channel %d, Name=%s\r\n", spi_ch, spi_dev->name);
+	err = spi_transceive(spi_dev, &spi_cfg, &tx, &rx);
+
+	if (err) {
+		printk("SPI error: %d\n", err);
+	} else {
+		/* Connect MISO to MOSI for loopback */
+		printk("TX sent: %x\n", tx_buffer[0]);
+		printk("RX recv: %x\n", rx_buffer[0]);
+		printk("RX recv: %x\n", rx_buffer[1]);
+		printk("RX recv: %x\n", rx_buffer[2]);
+		tx_buffer[0]++;
+	}
+}
+
+int spi_cmd_we(uint8_t spi_ch)
+{
+	printk("SPI WE\n");
+	spi_we(spi_ch);
+
+	return 0;
+}
+
+int spi_cmd_a3(uint8_t spi_ch)
+{
+	printk("SPI A3h\n");
+	spi_a3(spi_ch);
+
+	return 0;
+}
+
+int spi_cmd_9f(uint8_t spi_ch)
+{
+	printk("SPI A3h\n");
+	spi_9f(spi_ch);
+
+	return 0;
+}
+
+int spi_cmd_read_id(uint8_t spi_ch)
+{
+	printk("SPI Read ID for Channel %d\r\n", spi_ch);
+	spi_read_id(spi_ch);
+
+	return 0;
+}
+
+static void spi_it8xxx2_init0(void)
+{
+	printk("%s\r\n", __func__);
+	spi_dev0 = DEVICE_DT_GET(DT_NODELABEL(spi0));
+
+	if (!device_is_ready(spi_dev0)) {
+		printk("[user] Err: SPI device is not ready\r\n");
+	}
+	else {
+		printk("[user] It seems that the %s is ready.\r\n", spi_dev0->name);
+		spi_channel_status |= SPI_CH0_RDY;
+	}
+}
+
+static void spi_it8xxx2_init1(void)
+{
+	printk("%s\r\n", __func__);
+
+	spi_dev1 = DEVICE_DT_GET(DT_NODELABEL(spi1));
+
+	if (!device_is_ready(spi_dev1)) {
+		printk("[user] Err: SPI device is not ready\r\n");
+	} else {
+		printk("[user] It seems that the %s is ready.\r\n", spi_dev1->name);
+		spi_channel_status |= SPI_CH1_RDY;
+	}
+}
+
+static int command_spi_cmd0(int argc, char **argv)
+{
+	int ret=0;
+	char *e;
+	int opt;
+
+	printk("\r\n");
+	printk("SPI0 Command Tests\r\n");
+	printk("%s\r\n", __func__);
+
+	spi_it8xxx2_init0();
+
+	if (argc > 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	if ((spi_channel_status & SPI_CH0_RDY) != SPI_CH0_RDY) {
+		printk("[user][spi_cmd0] The SPI CH0 isn't ready.\r\n");
+		return -EIO;
+	}
+
+	if (argc == 2) {
+
+		opt = strtoi(argv[1], &e, 0);
+
+		switch(opt) {
+
+			case 0:
+					ret = spi_cmd_we(0);
+					break;
+			case 1:
+					ret = spi_cmd_a3(0);
+					break;
+			case 2:
+					ret = spi_cmd_9f(0);
+					break;
+			case 3:
+					ret = spi_cmd_read_id(0);
+					break;
+			default:
+					ret = spi_cmd_a3(0);
+					break;
+		}
+	}
+	else {
+		ret = spi_cmd_a3(0);
+	}
+
+	return ret;
+
+}
+DECLARE_CONSOLE_COMMAND(spi_test0, command_spi_cmd0,
+			NULL,
+			"SPI Test");
+
+
+static int command_spi_cmd1(int argc, char **argv)
+{
+	int ret=0;
+	char *e;
+	int opt;
+
+	spi_it8xxx2_init1();
+
+	printk("\r\n");
+	printk("SPI1 Command Tests\r\n");
+	printk("%s\r\n", __func__);
+
+	if (argc > 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	if ((spi_channel_status & SPI_CH1_RDY) != SPI_CH1_RDY) {
+		printk("[user][spi_cmd1] The SPI CH1 isn't ready.\r\n");
+		return -EIO;
+	}
+
+	if (argc == 2) {
+
+		opt = strtoi(argv[1], &e, 0);
+
+		switch(opt) {
+
+			case 0:
+					ret = spi_cmd_we(1);
+					break;
+			case 1:
+					ret = spi_cmd_a3(1);
+					break;
+			case 2:
+					ret = spi_cmd_9f(1);
+					break;
+			case 3:
+					ret = spi_cmd_read_id(1);
+					break;
+			default:
+					ret = spi_cmd_a3(1);
+					break;
+		}
+	}
+	else {
+		ret = spi_cmd_a3(1);
+	}
+
+	return ret;
+
+}
+DECLARE_CONSOLE_COMMAND(spi_test1, command_spi_cmd1,
+			NULL,
+			"SPI Test");
+/* SPI Commands End Up Here */
+
+/***********************************************************/
+/***********************************************************/
+/***********************************************************/
 static int console_channel_name_to_index(const char *name)
 {
 	int i;
