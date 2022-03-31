48ea470a5cfd11534ef53724cb11c3b27edff60d
Ting Shen <phoenixshen@google.com>
2022-03-31 17:51:06 +0800

    rt9490: add dt support for irq handler
    Adds an `irq` property and auto generates the irq handler for
    rt9490 driver.
    
    BUG=b:220814055
    TEST=C1 BC1.2 works on krabby
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I5603880378f2ddc67b296adaaabb4948c8cba355

--- zephyr/projects/corsola/src/krabby/usbc_config.c
+++ zephyr/projects/corsola/src/krabby/usbc_config.c
@@ -42,19 +42,10 @@ void c0_bc12_interrupt(enum gpio_signal signal)
 	rt1739_interrupt(0);
 }
 
-void c1_bc12_interrupt(enum gpio_signal signal)
-{
-	rt9490_interrupt(1);
-}
-
-
 static int board_sub_bc12_init(const struct device *unused)
 {
 	ARG_UNUSED(unused);
-	if (corsola_get_db_type() == CORSOLA_DB_TYPEC)
-		gpio_enable_dt_interrupt(
-			GPIO_INT_FROM_NODELABEL(int_usb_c1_bc12_charger));
-	else
+	if (corsola_get_db_type() == CORSOLA_DB_HDMI)
 		/* If this is not a Type-C subboard, disable the task. */
 		task_disable_task(TASK_ID_USB_CHG_P1);
 
