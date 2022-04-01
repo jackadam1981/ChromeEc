93b84b8705b66ddf4ad170aa38161e3db92d6f7d
Tom Hughes <tomhughes@chromium.org>
2022-04-01 13:14:08 -0700

    Add __keep to remaining interrupt handlers
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I4d0435f9d91d7bade10a846222b4b06a7a79c0b0

--- chip/stm32/usart-stm32f0.c
+++ chip/stm32/usart-stm32f0.c
@@ -106,7 +106,7 @@ struct usart_hw_config const usart1_hw = {
 	.ops            = &usart_variant_hw_ops,
 };
 
-static void usart1_interrupt(void)
+static void __keep usart1_interrupt(void)
 {
 	usart_interrupt(configs[0]);
 }
@@ -124,7 +124,7 @@ struct usart_hw_config const usart2_hw = {
 	.ops            = &usart_variant_hw_ops,
 };
 
-static void usart2_interrupt(void)
+static void __keep usart2_interrupt(void)
 {
 	usart_interrupt(configs[1]);
 }
@@ -155,7 +155,7 @@ struct usart_hw_config const usart4_hw = {
 #endif
 
 #if defined(CONFIG_STREAM_USART3) || defined(CONFIG_STREAM_USART4)
-static void usart3_4_interrupt(void)
+static void __keep usart3_4_interrupt(void)
 {
 	/*
 	 * This interrupt handler could be called with one of these configs
