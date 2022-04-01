0030ccd2a76cf72cdbb53e7ec267766596ab79a7
Tom Hughes <tomhughes@chromium.org>
2022-04-01 13:14:08 -0700

    Add __keep to remaining interrupt handlers
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I4d0435f9d91d7bade10a846222b4b06a7a79c0b0

--- chip/stm32/uart.c
+++ chip/stm32/uart.c
@@ -161,7 +161,7 @@ int uart_read_char(void)
 }
 
 /* Interrupt handler for console USART */
-static void uart_interrupt(void)
+static void __keep uart_interrupt(void)
 {
 #ifndef CONFIG_UART_TX_DMA
 	/*
