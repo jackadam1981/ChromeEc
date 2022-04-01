218435ebd4a2bf884b883e6cdf61226d1c38338d
Tom Hughes <tomhughes@chromium.org>
2022-04-01 13:14:08 -0700

    Add __keep to remaining interrupt handlers
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I4d0435f9d91d7bade10a846222b4b06a7a79c0b0

--- chip/stm32/usb.c
+++ chip/stm32/usb.c
@@ -684,7 +684,7 @@ static void usb_interrupt_handle_wake(uint16_t status)
 }
 #endif /* CONFIG_USB_SUSPEND && CONFIG_USB_REMOTE_WAKEUP */
 
-static void usb_interrupt(void)
+static void __keep usb_interrupt(void)
 {
 	uint16_t status = STM32_USB_ISTR;
 
