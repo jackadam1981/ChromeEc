f92a4ed99756511cc0334a44fc83a2eac7f27ca0
Tom Hughes <tomhughes@chromium.org>
2022-04-01 13:14:08 -0700

    Add __keep to remaining interrupt handlers
    BRANCH=none
    BUG=b:172020503
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I4d0435f9d91d7bade10a846222b4b06a7a79c0b0

--- chip/stm32/i2c-stm32f0.c
+++ chip/stm32/i2c-stm32f0.c
@@ -436,10 +436,7 @@ static void i2c_event_handler(int port)
 		}
 	}
 }
-static void i2c2_event_interrupt(void)
-{
-	i2c_event_handler(I2C_PORT_EC);
-}
+static void __keep i2c2_event_interrupt(void) { i2c_event_handler(I2C_PORT_EC); }
 DECLARE_IRQ(IRQ_PERIPHERAL, i2c2_event_interrupt, 2);
 #endif
 
