228025cc9a22cd718f918773bb93b744a1a071bb
Jeremy Bettis <jbettis@google.com>
2022-04-05 15:58:00 -0600

    ec: Delete /dev/shm/EC_persist_* before tests
    Some tests (specifically flash) leave /dev/shm/EC_persist_* files on the
    disk and the behavior of the test changes when they exist.  To get a
    consistent test, remove the corresponding files before running a host
    test.
    
    BRANCH=None
    BUG=b:213374060
    TEST=Ran flash test 50 times
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: I27a92adee1b181fdedf0b75bdd19a39e27bfa6af

--- test/build.mk
+++ test/build.mk
@@ -132,7 +132,7 @@ cov-dont-test += version
 cov-dont-test += interrupt
 # Flaky tests. The number of covered lines changes from run to run
 # b/213374060
-cov-dont-test += flash accel_cal rsa float entropy kb_mkbp
+cov-dont-test += accel_cal rsa float entropy kb_mkbp
 
 cov-test-list-host = $(filter-out $(cov-dont-test), $(test-list-host))
 
