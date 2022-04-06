46f51e4be15dd50f45931ac6bf9a5f035a2a59cf
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-04-06 17:14:18 +0800

    CEC2: modify MKBP event to support second cec
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I3419ed6f3b2a2b1ff43c98039f360e613a310759

--- chip/npcx/cec2.c
+++ chip/npcx/cec2.c
@@ -73,7 +73,7 @@ static uint32_t apb1_freq_div_10k;
 static void send_mkbp_event(uint32_t event)
 {
 	atomic_or(&cec_events, event);
-	mkbp_send_event(EC_MKBP_EVENT_CEC_EVENT);
+	mkbp_send_event(EC_MKBP_EVENT_CEC2_EVENT);
 }
 
 static int cec2_get_next_event(uint8_t *out)
