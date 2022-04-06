512d93cbc40fbbcbf8785325f71739b7b328d613
Brandon Breitenstein <brandon.breitenstein@intel.com>
2022-04-06 16:30:06 -0700

    zephyr: Enable USB Type C on Nuvoton Ports for MTL
    This patch will enable the Type C functionality on ports 0 and 1 for
    Meteorlake RVP.
    
    BUG=none
    BRANCH=none
    TEST=verified tcpcs are coming up on board
    
    Signed-off-by: Brandon Breitenstein <brandon.breitenstein@intel.com>
    Change-Id: I11e7fe1b8aeb7ae4a7ddf0b55dcddd2ac37c9100

---
+++ zephyr/projects/intelrvp/src/usb_pd_policy_mecc_1_1.c
@@ -0,0 +1,81 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "console.h"
+#include "gpio.h"
+#include "usb_mux.h"
+#include "usbc_ppc.h"
+#include "intelrvp.h"
+
+#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
+
+int pd_set_power_supply_ready(int port)
+{
+	int rv;
+
+	/* Disable charging. */
+	rv = tcpc_aic_gpios[port].ppc_intr_handler ?
+		ppc_vbus_sink_enable(port, 0) :
+		tcpc_config[port].drv->set_snk_ctrl(port, 0);
+	if (rv)
+		return rv;
+
+	pd_set_vbus_discharge(port, 0);
+
+	/* Provide Vbus. */
+	rv = tcpc_aic_gpios[port].ppc_intr_handler ?
+		ppc_vbus_source_enable(port, 1) :
+		tcpc_config[port].drv->set_src_ctrl(port, 1);
+	if (rv)
+		return rv;
+
+	/* Notify host of power info change. */
+	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+
+	return EC_SUCCESS;
+}
+
+void pd_power_supply_reset(int port)
+{
+	int prev_en;
+
+	prev_en = board_vbus_source_enabled(port);
+
+	/* Disable VBUS. */
+	if (tcpc_aic_gpios[port].ppc_intr_handler)
+		ppc_vbus_source_enable(port, 0);
+	else
+		tcpc_config[port].drv->set_src_ctrl(port, 0);
+
+	/* Enable discharge if we were previously sourcing 5V */
+	if (prev_en)
+		pd_set_vbus_discharge(port, 1);
+
+	/* Notify host of power info change. */
+	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+}
+
+int pd_check_vconn_swap(int port)
+{
+	/* Only allow vconn swap if PP3300 rail is enabled */
+	return gpio_get_level(GPIO_EN_PP3300_A);
+}
+
+int pd_snk_is_vbus_provided(int port)
+{
+	return tcpc_aic_gpios[port].ppc_intr_handler ?
+		ppc_is_vbus_present(port) :
+		tcpc_config[port].drv->check_vbus_level(port, VBUS_PRESENT);
+}
+
+int board_vbus_source_enabled(int port)
+{
+	if (is_typec_port(port))
+		return tcpc_aic_gpios[port].ppc_intr_handler ?
+			ppc_is_sourcing_vbus(port) :
+			tcpc_config[port].drv->get_src_ctrl(port);
+	return 0;
+}
