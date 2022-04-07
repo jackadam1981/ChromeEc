241b54c6b561e39c76fe59936f49f9eaec66d1e4
Tinghan Shen <tinghan.shen@mediatek.com>
2022-04-07 16:54:31 +0800

    TEST-ONLY: use ap uart2
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I8d22e0cd5ad843099b6ad5db45ea6ace20f1ae9a
    
    uart 2
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I11f6e176831b6164e772f6f74c5048c8a623d55e

--- chip/mt_scp/rv32i_common/registers.h
+++ chip/mt_scp/rv32i_common/registers.h
@@ -20,6 +20,8 @@
 #define CORE_SN 0
 #endif
 
+/* external address: AP */
+#define AP_REG_BASE			0x60000000 /* 0x10000000 remap to 0x6 */
 #define SCP_REG_BASE			0x70000000
 
 /* clock control */
@@ -140,6 +142,7 @@
 #define UART_RX_IRQ(n)			CONCAT3(SCP_IRQ_UART, n, _RX)
 #define SCP_UART0_BASE			(SCP_REG_BASE + 0x26000)
 #define SCP_UART1_BASE			(SCP_REG_BASE + 0x27000)
+#define SCP_UART2_BASE			(AP_REG_BASE + 0x1001300)
 #define SCP_UART_BASE(n)		CONCAT3(SCP_UART, n, _BASE)
 #define UART_REG(n, offset)		REG32_ADDR(SCP_UART_BASE(n))[offset]
 
@@ -264,8 +267,6 @@
 #define SCP_R_REMAP_0X89AB		REG32(SCP_REG_BASE + 0xA5068)
 #define SCP_R_REMAP_0XCDEF		REG32(SCP_REG_BASE + 0xA506C)
 
-/* external address: AP */
-#define AP_REG_BASE			0x60000000 /* 0x10000000 remap to 0x6 */
 /* AP GPIO */
 #define AP_GPIO_BASE			(AP_REG_BASE + 0x5000)
 #define AP_GPIO_MODE11_SET		REG32(AP_GPIO_BASE + 0x03B4)
