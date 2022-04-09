d4a7685f45768b0537c92b033124ca9387c8c483
Craig Hesling <hesling@chromium.org>
2022-04-09 23:12:07 +0000

    common: Make IS_ENABLED work at low optimization
    This allows for the use of "-Og", which reduces compiler optimization
    to a debugger friendly level.
    https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html
    
    The previous mechanism relies on compiler optimization
    to not check the IS_ENABLED_BAD_ARGS() call when (!__undefined)
    if always false, thus not emitting the error associated with
    the function.
    
    When you turn off optimzation, all references (and branches)
    are checked, thus the error is always emitted.
    
    BRANCH=none
    BUG=b:172212718
    TEST=make BOARD=$BOARD -j V=1 CFLAGS_DEBUG_CHIP="-ggdb -Og" \
             build/$BOARD/ec.bin
    TEST=compare_builds.sh -b all
         # Use objdump and meld to ensure asm differences
         # are functionally equivalent.
    
    Change-Id: I8884b80e05ead10e78f0c8be05158590618bf3a8
    Signed-off-by: Craig Hesling <hesling@chromium.org>

--- include/common.h
+++ include/common.h
@@ -439,11 +439,8 @@ enum ec_error_list {
 #define __config_enabled(cfg, value)					      \
 	__cfg_select(							      \
 		value, 1, ({						      \
-			int __undefined = __builtin_strcmp(cfg, #value) == 0; \
-			extern int IS_ENABLED_BAD_ARGS(void) __error(	      \
+			BUILD_ASSERT(__builtin_strcmp(cfg, #value) == 0,      \
 				cfg " must be <blank>, or not defined.");     \
-			if (!__undefined)				      \
-				IS_ENABLED_BAD_ARGS();			      \
 			0;						      \
 		}))
 
