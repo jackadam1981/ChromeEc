bdd2dbf707cea192ef30d0b9d61293061829b082
Eric Yilun Lin <yllin@chromium.org>
2022-04-11 10:52:22 +0800

    corsola: enable DRP until XHCI inited.
    Previously we issued a hard reset signal to cycle the VBUS after
    XHCI inited. The hard reset was happened when AP at coreboot stage,
    but somehow the outdated hard reset event was carried into kernel
    causing a wrong type-c state.
    
    To prevent that, we force the port at SINK state until the XHCI
    inited, and then enable DRP toggling so we don't need to cycle the
    VBUS and issue hard reset.
    
    BUG=b:224471170
    TEST=Plug external monitor and boot device, the MUX state is correct
         at kernel.
    BRANCH=none
    
    Change-Id: I848e8e086a50b1eea8b70befdfb24599346c5743
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/projects/corsola/src/usbc_config.c
+++ zephyr/projects/corsola/src/usbc_config.c
@@ -74,15 +74,23 @@ void usb_a0_interrupt(enum gpio_signal signal)
 		usb_charge_set_mode(i, mode, USB_ALLOW_SUSPEND_CHARGE);
 
 	/*
-	 * Trigger hard reset to cycle Vbus on Type-C ports, recommended by
+	 * Enable DRP toggle after XHCI inited. This is used to follow
 	 * USB 3.2 spec 10.3.1.1.
 	 */
 	if (gpio_get_level(signal)) {
 		for (int i = 0; i < CONFIG_USB_PD_PORT_MAX_COUNT; i++) {
-			if (tc_is_attached_src(i))
-				pd_dpm_request(i, DPM_REQUEST_HARD_RESET_SEND);
+			pd_set_dual_role(i, PD_DRP_TOGGLE_ON);
 		}
 	}
+}
+
+__override enum pd_dual_role_states pd_get_drp_state_in_on(void)
+{
+	if (gpio_pin_get_dt(
+		GPIO_DT_FROM_NODELABEL(gpio_ap_xhci_init_done)))
+		return PD_DRP_TOGGLE_ON;
+	else
+		return PD_DRP_FORCE_SINK;
 }
 
 void board_set_charge_limit(int port, int supplier, int charge_ma,
