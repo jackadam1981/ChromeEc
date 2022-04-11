aefb16d9d864476a4cc20226e4f3fb2e206aea78
alvishsu <alvishsu@google.com>
2022-04-11 15:21:35 +0800

    TEST-ONLY: Force RT9490 switching frequence in 1MHz
    Reduce switching loss to get higher current rating
    
    BUG=b:215294785
    TEST=N/A
    
    Change-Id: Icf2a07e7dc7e438745f4fda3f55247176a47d375

--- driver/charger/rt9490.c
+++ driver/charger/rt9490.c
@@ -338,6 +338,10 @@ static int rt9490_init_setting(int chgnum)
 	RETURN_ERROR(rt9490_set_bit(chgnum, RT9490_REG_CHG_IRQ_MASK5,
 				    RT9490_CHG_IRQ_MASK5_ALL));
 
+	/* Reduce switching frequency to 1MHz for higher current rating b/215294785 */
+	RETURN_ERROR(rt9490_set_bit(chgnum, RT9490_REG_ADD_CTRL1,
+				    RT9490_EN_1MHZ));
+
 	return EC_SUCCESS;
 }
 
