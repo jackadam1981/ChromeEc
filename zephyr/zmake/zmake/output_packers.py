6997a3e0abe2daf87842d2b981d1718f73637082
martin yan <martin.yan@microchip.corp-partner.google.com>
2022-04-12 16:50:47 -0400

    zephyr: mchp: Add MchpPacker to generate SPI image
    Add MchpPacker to support SPI image generation
    
    BUG=none
    BRANCH=main
    TEST=zmake testall and make buildall -j$(nproc)
    	verify "zmake build adlrvp_mchp" project locally
    	-- able to generate 512KB SPI image
    	-- image with expected header, RO/RW and FMAP information
    	-- RO can be loaded by EC Rom loader as POR or reboot
    
    Signed-off-by: martin yan <martin.yan@microchip.corp-partner.google.com>
    Change-Id: I55f188aa1ddf96ebc0f4b37990de01ff3f5a3bfb

--- zephyr/zmake/zmake/output_packers.py
+++ zephyr/zmake/zmake/output_packers.py
@@ -220,10 +220,23 @@ class NpcxPacker(BinmanPacker):
         yield ro / self.npcx_monitor, self.npcx_monitor
 
 
+# MCHP all we do is set binman's ro file to zephyr.mchp.bin
+class MchpPacker(BinmanPacker):
+    """Packer for RO/RW image to generate a .bin build using FMAP.
+
+    This expects that the build is setup to generate a
+    zephyr.npcx.bin for the RO image, which should be packed using
+    Microchip's loader format.
+    """
+
+    ro_file = "zephyr.mchp.bin"
+
+
 # A dictionary mapping packer config names to classes.
 packer_registry = {
     "binman": BinmanPacker,
     "elf": ElfPacker,
     "npcx": NpcxPacker,
     "raw": RawBinPacker,
+    "mchp": MchpPacker,
 }
