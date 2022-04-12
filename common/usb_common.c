a5d1e54a21548d02a57a99d8fbda9b99cd58fcfc
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-04-12 14:44:30 +0800

    primus: reset dpm
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: Ibef64136f52a29c4530e2237c956d2c26c0f4e16

--- common/usb_common.c
+++ common/usb_common.c
@@ -351,6 +351,9 @@ int pd_check_requested_voltage(uint32_t rdo, const int port)
 	if (max_ma > pdo_ma && !(rdo & RDO_CAP_MISMATCH))
 		return EC_ERROR_INVAL; /* too much max current */
 
+	if (op_ma != pdo_ma)
+		dpm_remove_sink(port);
+
 	CPRINTF("Requested %d mV %d mA (for %d/%d mA)\n",
 		 ((pdo >> 10) & 0x3ff) * 50, (pdo & 0x3ff) * 10,
 		 op_ma * 10, max_ma * 10);
