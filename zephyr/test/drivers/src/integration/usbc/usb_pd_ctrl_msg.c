28adbe4b51bb41285cb2403462f404543aba9e27
Al Semjonovs <asemjonovs@google.com>
2022-04-12 22:17:37 +0000

    zephyr:test: Rename/refactor partner connect functions.
    Clean up the naming of the partner connect/disconnect
    functions.
    
    BUG=b:223449451
    BRANCH=none
    TEST=zmake test test-drivers
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: I575e30ee98206160e6fefa5369f1512daf8749a8

--- zephyr/test/drivers/src/integration/usbc/usb_pd_ctrl_msg.c
+++ zephyr/test/drivers/src/integration/usbc/usb_pd_ctrl_msg.c
@@ -16,7 +16,7 @@
 #include "test/drivers/utils.h"
 #include "usb_pd.h"
 
-#define SNK_PORT USBC_PORT_C0
+#define TEST_USB_PORT USBC_PORT_C0
 
 #define TCPCI_EMUL_LABEL DT_NODELABEL(tcpci_emul)
 
@@ -35,35 +35,32 @@ struct usb_pd_ctrl_msg_test_source_fixture {
 	struct usb_pd_ctrl_msg_test_fixture fixture;
 };
 
-static void connect_sink_to_port(struct usb_pd_ctrl_msg_test_fixture *fixture)
+static void tcpci_drp_emul_connect_partner(struct tcpci_drp_emul *partner_emul,
+					   const struct emul *tcpci_emul,
+					   const struct emul *charger_emul)
 {
 	/*
 	 * TODO(b/221439302) Updating the TCPCI emulator registers, updating the
 	 *   vbus, as well as alerting should all be a part of the connect
 	 *   function.
 	 */
-	isl923x_emul_set_adc_vbus(fixture->charger_emul, 0);
-	zassume_ok(tcpci_emul_set_reg(fixture->tcpci_emul,
-				      TCPC_REG_POWER_STATUS,
-				      TCPC_REG_POWER_STATUS_VBUS_DET),
-		   NULL);
-	zassume_ok(tcpci_emul_set_reg(fixture->tcpci_emul, TCPC_REG_EXT_STATUS,
-				      TCPC_REG_EXT_STATUS_SAFE0V),
-		   NULL);
+	isl923x_emul_set_adc_vbus(charger_emul, 0);
+	tcpci_emul_set_reg(tcpci_emul, TCPC_REG_POWER_STATUS,
+			   TCPC_REG_POWER_STATUS_VBUS_DET);
 
-	tcpci_tcpc_alert(USBC_PORT_C0);
+	tcpci_emul_set_reg(tcpci_emul, TCPC_REG_EXT_STATUS,
+			   TCPC_REG_EXT_STATUS_SAFE0V);
+
+	tcpci_tcpc_alert(TEST_USB_PORT);
 
 	zassume_ok(tcpci_drp_emul_connect_to_tcpci(
-			   &fixture->partner_emul.data,
-			   &fixture->partner_emul.src_data,
-			   &fixture->partner_emul.snk_data,
-			   &fixture->partner_emul.common_data,
-			   &fixture->partner_emul.ops, fixture->tcpci_emul),
+			   &partner_emul->data, &partner_emul->src_data,
+			   &partner_emul->snk_data, &partner_emul->common_data,
+			   &partner_emul->ops, tcpci_emul),
 		   NULL);
 }
 
-static void
-disconnect_sink_from_port(struct usb_pd_ctrl_msg_test_fixture *fixture)
+static void disconnect_partner(struct usb_pd_ctrl_msg_test_fixture *fixture)
 {
 	zassume_ok(tcpci_emul_disconnect_partner(fixture->tcpci_emul), NULL);
 	k_sleep(K_SECONDS(1));
@@ -108,7 +105,6 @@ static void usb_pd_ctrl_msg_before(void *data)
 	/* TODO(b/214401892): Check why need to give time TCPM to spin */
 	k_sleep(K_SECONDS(1));
 
-	/* Initialized the sink to request 5V and 3A */
 	tcpci_drp_emul_init(&fixture->partner_emul);
 
 	fixture->partner_emul.data.sink = fixture->is_sink;
@@ -116,12 +112,14 @@ static void usb_pd_ctrl_msg_before(void *data)
 	tcpci_emul_set_rev(fixture->tcpci_emul, TCPCI_EMUL_REV2_0_VER1_1);
 
 	/* Turn TCPCI rev 2 ON */
-	tcpc_config[SNK_PORT].flags |= TCPC_FLAGS_TCPCI_REV2_0;
+	tcpc_config[TEST_USB_PORT].flags |= TCPC_FLAGS_TCPCI_REV2_0;
 
 	/* Reset to disconnected state */
-	disconnect_sink_from_port(fixture);
+	disconnect_partner(fixture);
 
-	connect_sink_to_port(fixture);
+	tcpci_drp_emul_connect_partner(&fixture->partner_emul,
+				       fixture->tcpci_emul,
+				       fixture->charger_emul);
 
 	k_sleep(K_SECONDS(10));
 }
@@ -130,7 +128,7 @@ static void usb_pd_ctrl_msg_after(void *data)
 {
 	struct usb_pd_ctrl_msg_test_fixture *fixture = data;
 
-	disconnect_sink_from_port(fixture);
+	disconnect_partner(fixture);
 }
 
 ZTEST_SUITE(usb_pd_ctrl_msg_test_sink, drivers_predicate_post_main,
@@ -150,7 +148,7 @@ ZTEST_F(usb_pd_ctrl_msg_test_sink, verify_vconn_swap)
 	 */
 	usb_pd_ctrl_msg_before(fixture);
 
-	snk_resp = host_cmd_typec_status(SNK_PORT);
+	snk_resp = host_cmd_typec_status(TEST_USB_PORT);
 	zassert_equal(PD_ROLE_VCONN_SRC, snk_resp.vconn_role,
 		      "SNK Returned vconn_role=%u", snk_resp.vconn_role);
 
@@ -161,7 +159,7 @@ ZTEST_F(usb_pd_ctrl_msg_test_sink, verify_vconn_swap)
 
 	k_sleep(K_SECONDS(1));
 
-	snk_resp = host_cmd_typec_status(SNK_PORT);
+	snk_resp = host_cmd_typec_status(TEST_USB_PORT);
 	zassert_equal(PD_ROLE_VCONN_OFF, snk_resp.vconn_role,
 		      "SNK Returned vconn_role=%u", snk_resp.vconn_role);
 }
@@ -177,7 +175,7 @@ ZTEST_F(usb_pd_ctrl_msg_test_sink, verify_pr_swap)
 	 */
 	usb_pd_ctrl_msg_before(fixture);
 
-	snk_resp = host_cmd_typec_status(SNK_PORT);
+	snk_resp = host_cmd_typec_status(TEST_USB_PORT);
 	zassert_equal(PD_ROLE_SINK, snk_resp.power_role,
 		      "SNK Returned power_role=%u", snk_resp.power_role);
 
@@ -199,7 +197,7 @@ ZTEST_F(usb_pd_ctrl_msg_test_sink, verify_pr_swap)
 
 	k_sleep(K_MSEC(20));
 
-	snk_resp = host_cmd_typec_status(SNK_PORT);
+	snk_resp = host_cmd_typec_status(TEST_USB_PORT);
 	zassert_equal(PD_ROLE_SOURCE, snk_resp.power_role,
 		      "SNK Returned power_role=%u", snk_resp.power_role);
 }
@@ -214,7 +212,7 @@ ZTEST_F(usb_pd_ctrl_msg_test_sink, verify_dr_swap)
 	/* Give TCPM time to trigger DR Swap request */
 	k_sleep(K_MSEC(20));
 
-	typec_status = host_cmd_typec_status(SNK_PORT);
+	typec_status = host_cmd_typec_status(TEST_USB_PORT);
 	zassert_equal(PD_ROLE_DFP, typec_status.data_role,
 		      "Returned data_role=%u", typec_status.data_role);
 }
@@ -230,7 +228,7 @@ ZTEST_F(usb_pd_ctrl_msg_test_source, verify_dr_swap)
 	 */
 	usb_pd_ctrl_msg_before(fixture);
 
-	typec_status = host_cmd_typec_status(SNK_PORT);
+	typec_status = host_cmd_typec_status(TEST_USB_PORT);
 	zassert_equal(PD_ROLE_DFP, typec_status.data_role,
 		      "Returned data_role=%u", typec_status.data_role);
 
@@ -242,7 +240,7 @@ ZTEST_F(usb_pd_ctrl_msg_test_source, verify_dr_swap)
 	k_sleep(K_MSEC(20));
 
 	/* Verify DR_Swap request is REJECTED */
-	typec_status = host_cmd_typec_status(SNK_PORT);
+	typec_status = host_cmd_typec_status(TEST_USB_PORT);
 	zassert_equal(PD_ROLE_DFP, typec_status.data_role,
 		      "Returned data_role=%u", typec_status.data_role);
 }
