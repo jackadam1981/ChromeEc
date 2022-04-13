3cf163c02ea7ef92a218f89802326df43637775c
Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
2022-04-13 13:25:54 +0800

    virtual_battery: Return manufacturer data
    Add battery command manufacturer data
    
    BUG=b:228360450
    BRANCH=kukui
    TEST=make buildall
    Run command "ectool i2cxfer 2 0x0b 0x05 0x23" on damu
    
    Signed-off-by: Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
    Change-Id: Iab93801e28b0d6f32cb257c19573e6dfbdc3c3bb

--- common/virtual_battery.c
+++ common/virtual_battery.c
@@ -187,6 +187,9 @@ int virtual_battery_operation(const uint8_t *batt_cmd_head,
 {
 	int val;
 	int year, month, day;
+#ifdef CONFIG_BATTERY_SMART
+	char str[32];
+#endif
 	/*
 	 * We cache battery operational mode locally for both read and write
 	 * commands. If MODE_CAPACITY bit is set, battery capacity will be
@@ -382,6 +385,14 @@ int virtual_battery_operation(const uint8_t *batt_cmd_head,
 		}
 		memcpy(dest, &val, bounded_read_len);
 		break;
+#ifdef CONFIG_BATTERY_SMART
+	case SB_MANUFACTURER_DATA:
+		/* This may cause an i2c transaction */
+		if (battery_manufacturer_data(str, read_len))
+			return EC_ERROR_INVAL;
+		memcpy(dest, &str, read_len);
+		break;
+#endif
 	case SB_MANUFACTURER_ACCESS:
 		/* No manuf. access reg access allowed over VB interface */
 		return EC_ERROR_INVAL;
