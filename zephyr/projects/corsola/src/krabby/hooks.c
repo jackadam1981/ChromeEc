0d49eb78027ff4ae1a07ce4f1ab2768f3c65ff30
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2022-04-13 18:02:06 +0800

    WIP: zephyr: krabby: Use pinctrl control to instead of hook method
    If I2C3 is using GPIOF group, we must set SMB3PSEL=0 in PMER3
    register before power state entering suspend mode.
    
    
    BUG=b:226296649
    BRANCH=none
    TEST= TODO: krabby
    
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>
    Change-Id: I37f41010f429a64a73653a2478006d0cb3777d7e

--- zephyr/projects/corsola/src/krabby/hooks.c
+++ zephyr/projects/corsola/src/krabby/hooks.c
@@ -13,38 +13,6 @@
 #include "gpio.h"
 #include "hooks.h"
 
-static void board_i2c3_ctrl(bool enable)
-{
-	if (DEVICE_DT_GET(DT_GPIO_CTLR_BY_IDX(DT_NODELABEL(i2c3),
-	    scl_gpios, 0)) == DEVICE_DT_GET(DT_NODELABEL(gpiof))) {
-		/*
-		 * TODO(b/226296649):
-		 * Use pinctrl APIs to enable/disable an interface.
-		 */
-		struct gctrl_it8xxx2_regs *const gctrl_base =
-			(struct gctrl_it8xxx2_regs *)
-				DT_REG_ADDR(DT_NODELABEL(gctrl));
-
-		if (enable) {
-			gctrl_base->GCTRL_PMER3 |= IT8XXX2_GCTRL_SMB3PSEL;
-		} else {
-			gctrl_base->GCTRL_PMER3 &= ~IT8XXX2_GCTRL_SMB3PSEL;
-		}
-	}
-}
-
-static void board_enable_i2c3(void)
-{
-	board_i2c3_ctrl(1);
-}
-DECLARE_HOOK(HOOK_CHIPSET_PRE_INIT, board_enable_i2c3, HOOK_PRIO_FIRST);
-
-static void board_disable_i2c3(void)
-{
-	board_i2c3_ctrl(0);
-}
-DECLARE_HOOK(HOOK_CHIPSET_HARD_OFF, board_disable_i2c3, HOOK_PRIO_LAST);
-
 static void board_suspend_handler(struct ap_power_ev_callback *cb,
 				  struct ap_power_ev_data data)
 {
