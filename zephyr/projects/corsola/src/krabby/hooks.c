b019aafaa65296c6225855cb80b67d428eff85ba
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2022-04-13 18:02:06 +0800

    WIP: zephyr: krabby: Use pinctrl control to instead of hook method
    If I2C3 is using GPIOF group, we must set SMB3PSEL=0 in PMER3
    register before power state entering suspend mode.
    
    
    BUG=b:226296649
    BRANCH=none
    TEST= TODO: krabby
    
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>
    Change-Id: I37f41010f429a64a73653a2478006d0cb3777d7e

--- zephyr/projects/corsola/src/krabby/hooks.c
+++ zephyr/projects/corsola/src/krabby/hooks.c
@@ -5,6 +5,9 @@
 
 #include <zephyr/init.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pinctrl.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(board_hook, LOG_LEVEL_ERR);
 
 #include <ap_power/ap_power.h>
 #include "charger.h"
@@ -13,22 +16,33 @@
 #include "gpio.h"
 #include "hooks.h"
 
+#define I2C_NODE DT_NODELABEL(i2c3)
+
+PINCTRL_DT_DEFINE(I2C_NODE);
+
 static void board_i2c3_ctrl(bool enable)
 {
 	if (DEVICE_DT_GET(DT_GPIO_CTLR_BY_IDX(DT_NODELABEL(i2c3),
 	    scl_gpios, 0)) == DEVICE_DT_GET(DT_NODELABEL(gpiof))) {
-		/*
-		 * TODO(b/226296649):
-		 * Use pinctrl APIs to enable/disable an interface.
-		 */
-		struct gctrl_it8xxx2_regs *const gctrl_base =
-			(struct gctrl_it8xxx2_regs *)
-				DT_REG_ADDR(DT_NODELABEL(gctrl));
+
+		const struct pinctrl_dev_config *pcfg =
+			PINCTRL_DT_DEV_CONFIG_GET(I2C_NODE);
+		int ret;
 
 		if (enable) {
-			gctrl_base->GCTRL_PMER3 |= IT8XXX2_GCTRL_SMB3PSEL;
+			ret = pinctrl_apply_state(pcfg, PINCTRL_STATE_DEFAULT);
+			if (ret < 0) {
+				LOG_ERR("Failed to configure I2C pins (%d).",
+					ret);
+				return;
+			}
 		} else {
-			gctrl_base->GCTRL_PMER3 &= ~IT8XXX2_GCTRL_SMB3PSEL;
+			ret = pinctrl_apply_state(pcfg, PINCTRL_STATE_SLEEP);
+			if (ret < 0) {
+				LOG_ERR("Failed to configureto default (%d).",
+					ret);
+				return;
+			}
 		}
 	}
 }
