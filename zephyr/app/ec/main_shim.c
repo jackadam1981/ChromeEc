5d94d5b879dac24bb6968815276bcafb4e8a742c
Fabio Baltieri <fabiobaltieri@google.com>
2022-04-14 13:12:36 +0000

    wip: run HOSTCMD in main thread
    Change-Id: Ib2b3ba3d8a6fb883876ecdd432068ca3300e5344

--- zephyr/app/ec/main_shim.c
+++ zephyr/app/ec/main_shim.c
@@ -5,18 +5,28 @@
 
 #include <kernel.h>
 #include "ec_app_main.h"
+#include "task.h"
+
+void host_command_task(void *u);
+
+extern struct k_thread z_main_thread;
 
 /** A stub main to call the real ec app main function. LCOV_EXCL_START */
 void main(void)
 {
 	ec_app_main();
 
-#ifdef CONFIG_THREAD_MONITOR
-	/*
-	 * Avoid returning so that the main stack is displayed by the
-	 * "kernel stacks" shell command.
-	 */
-	k_sleep(K_FOREVER);
-#endif
+	if (IS_ENABLED(CONFIG_TASK_HOSTCMD_THREAD_MAIN)) {
+		k_thread_priority_set(&z_main_thread,
+				      EC_TASK_PRIORITY(EC_TASK_HOSTCMD_PRIO));
+		k_thread_name_set(&z_main_thread, "HOSTCMD*");
+		host_command_task(NULL);
+	} else if (IS_ENABLED(CONFIG_THREAD_MONITOR)) {
+		/*
+		 * Avoid returning so that the main stack is displayed by the
+		 * "kernel stacks" shell command.
+		 */
+		k_sleep(K_FOREVER);
+	}
 }
 /* LCOV_EXCL_STOP */
