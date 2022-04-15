cb257c1d6058f6bce237724e3bd283149281b22a
Tomasz Michalec <tm@semihalf.com>
2022-04-15 13:09:21 +0200

    zephyr: Add TCPCI USB-C mux DTS configuration
    Add necessary changes to make it possible to configure TCPCI USB-C mux
    using devicetree. TCPCI USB-C mux has hpd-update property which allows
    to select hpd_update callback. Right now it allows to select NULL (by
    not selecting any value) or ps8xxx_tcpc_update_hpd_status.
    
    BUG=b:227757117
    TEST=zmake testall
    BRANCH=none
    
    Signed-off-by: Tomasz Michalec <tm@semihalf.com>
    Change-Id: I2b5a98de58aa9fe8c47da69dff9151fd34f2a62b

---
+++ zephyr/shim/include/usbc/tcpci_usb_mux.h
@@ -0,0 +1,22 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_SHIM_TCPCI_USB_MUX_H
+#define __ZEPHYR_SHIM_TCPCI_USB_MUX_H
+
+#include "tcpm/ps8xxx_public.h"
+#include "tcpm/tcpci.h"
+
+#define TCPCI_TCPM_USB_MUX_COMPAT	cros_ec_usbc_mux_tcpci
+
+#define USB_MUX_CONFIG_TCPCI_TCPM(mux_id, port_id, idx)			\
+	{								\
+		USB_MUX_COMMON_FIELDS(mux_id, port_id, idx),		\
+		.driver = &tcpci_tcpm_usb_mux_driver,			\
+		.hpd_update = USB_MUX_CALLBACK_OR_NULL(mux_id,		\
+						       hpd_update),	\
+	}
+
+#endif /* __ZEPHYR_SHIM_TCPCI_USB_MUX_H */
