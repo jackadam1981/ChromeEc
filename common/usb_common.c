9108e35f72e4f12e6876eaf1fd319520266f872b
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-04-19 12:52:29 +0800

    common/usbc: restore Rp to default when SNK CAP and request mismatch
    If port partner return SNK CAP can support 3A, but request 1.5A.
    Remove sink_max_pdo_requested and supply 1.5A for it.
    
    BUG=b:229683022
    BRANCH=none
    TEST=make -j BOARD=primus
    TEST=verified by GRL
    
    Change-Id: Icc4904ec1267b7a34e64c01b626f607999072410
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>

--- common/usb_common.c
+++ common/usb_common.c
@@ -351,6 +351,14 @@ int pd_check_requested_voltage(uint32_t rdo, const int port)
 	if (max_ma > pdo_ma && !(rdo & RDO_CAP_MISMATCH))
 		return EC_ERROR_INVAL; /* too much max current */
 
+	/*
+	 * If port partner return SNK CAP can support 3A, but
+	 * request 1.5A. Remove sink_max_pdo_requested and supply
+	 * 1.5A for it.
+	 */
+	if (op_ma != pdo_ma)
+		dpm_remove_sink(port);
+
 	CPRINTF("Requested %d mV %d mA (for %d/%d mA)\n",
 		 ((pdo >> 10) & 0x3ff) * 50, (pdo & 0x3ff) * 10,
 		 op_ma * 10, max_ma * 10);
