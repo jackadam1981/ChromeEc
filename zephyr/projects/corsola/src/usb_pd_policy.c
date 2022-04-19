f0816ed5c4332fbb673e371f2ec10d6ac22015d8
Eric Yilun Lin <yllin@chromium.org>
2022-04-19 14:14:28 +0800

    corsola: re-config usb mux at DP post-config
    If we plug a dongle connected with an external monitor, the
    HPD level will be sending high at the first DP STATUS command.
    In this case, the USB MUX then will be put into safe mode before
    post-config and the previously configured mux mode in DP STATUS will be
    forfeited. So, we should re-configure it again (and this is also in
    the common code) if it's the selected aux-port.
    
    BUG=b:228289594
    TEST=plug a dongle with an external monitor connected, and is able to
         output.
    TEST=plug C0 DP dongle, then C1 dongle, and plug an monitor to C0
         dongle, and it has output. (test aux path switching).
    TEST=plug C0 DP dongle, then C1 dongle, and plug an monitor to C1
         dongle, and it has output. (test aux path switching).
    BRANCH=none
    
    Change-Id: I3ebfeee5d4a89475721b2cb2ca6050d808fc44ac
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/projects/corsola/src/usb_pd_policy.c
+++ zephyr/projects/corsola/src/usb_pd_policy.c
@@ -110,6 +110,20 @@ __override int svdm_dp_config(int port, uint32_t *payload)
 
 __override void svdm_dp_post_config(int port)
 {
+	mux_state_t mux_mode = svdm_dp_get_mux_mode(port);
+
+	/*
+	 * Prior to post-config, the mux will be reset to safe mode, and this
+	 * will break mux config and aux path config we did in the first DP
+	 * status command. Only enable this if the port is the current aux-port.
+	 */
+	if (port == active_aux_port) {
+		usb_mux_set(port, mux_mode, USB_SWITCH_CONNECT,
+			polarity_rm_dts(pd_get_polarity(port)));
+		usb_mux_hpd_update(port, USB_PD_MUX_HPD_LVL |
+					 USB_PD_MUX_HPD_IRQ_DEASSERTED);
+	}
+
 	dp_flags[port] |= DP_FLAGS_DP_ON;
 }
 
@@ -166,14 +180,6 @@ __override int svdm_dp_attention(int port, uint32_t *payload)
 		if (IS_ENABLED(CONFIG_MKBP_EVENT)) {
 			pd_notify_dp_alt_mode_entry(port);
 		}
-	}
-
-	/* Its initial DP status message prior to config */
-	if (!(dp_flags[port] & DP_FLAGS_DP_ON)) {
-		if (lvl) {
-			dp_flags[port] |= DP_FLAGS_HPD_HI_PENDING;
-		}
-		return 1;
 	}
 
 #ifdef CONFIG_USB_PD_DP_HPD_GPIO
