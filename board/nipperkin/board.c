9ba0d4d5b27f4985066f97a608366abf30a0db9e
Jon Murphy <jpmurphy@google.com>
2022-04-21 15:01:54 -0600

    nipperkin: Set DC prochot current thresh at init
    Nipperkin devices are spiking current frequently exceeding the 4 A
    threshold for over current prochot assertion on the ISL.  Testing
    shows that setting the current threshold limit to ~6 A will minimize
    the impact to the user.
    
    BUG=b:216489130
    TEST=Boot, verify that the ISL dc prochot threshold register is set to
    5.888 A
    BRANCH=guybrush
    
    Signed-off-by: Jon Murphy <jpmurphy@google.com>
    Change-Id: I7ed388ab535dad228c40fbe5bcb108679d985418

--- board/nipperkin/board.c
+++ board/nipperkin/board.c
@@ -14,6 +14,7 @@
 #include "chipset.h"
 #include "common.h"
 #include "cros_board_info.h"
+#include "driver/charger/isl9241.h"
 #include "driver/retimer/pi3hdx1204.h"
 #include "driver/retimer/ps8811.h"
 #include "driver/retimer/ps8818.h"
@@ -532,3 +533,10 @@ void board_set_current_limit(void)
 }
 DECLARE_HOOK(HOOK_BATTERY_SOC_CHANGE, board_set_current_limit,
 	     HOOK_PRIO_INIT_EXTPOWER);
+
+/* Set the DCPROCHOT base on battery over discharging current 5.888A */
+static void set_dc_prochot(void)
+{
+	isl9241_set_dc_prochot(CHARGER_SOLO, 5888);
+}
+DECLARE_HOOK(HOOK_INIT, set_dc_prochot, HOOK_PRIO_DEFAULT);
