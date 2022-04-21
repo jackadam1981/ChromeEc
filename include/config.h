69f849b1b871d29517bbc4a360b1e8b2d6945b35
Vijay Hiremath <vijay.p.hiremath@intel.com>
2022-04-21 13:52:47 -0700

    cleanup: it8801: Standardize I2C names to support Zephyr SHIM
    Standardized IT8801 I2C related names like I2C_PORT, I2C_ADDRESS and
    Interrupt GPIO to support discrete keyboard in Zephyr SHIM.
    
    BUG=b:230008245
    BRANCH=none
    TEST=make buildall
    
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>
    Change-Id: I9be3ff570681f88e1fc3ac0650f9919ad272b847

--- include/config.h
+++ include/config.h
@@ -2784,8 +2784,8 @@
 /*
  * Support IT8801 I/O expander.
  *
- * I2C address IT8801_KEYBOARD_PWM_I2C_ADDR_FLAGS and I2C port
- * IT8801_KEYBOARD_PWM_I2C_PORT must be defined as well.
+ * I2C address KB_NOT_RAW_I2C_ADDR_FLAGS and I2C port
+ * I2C_PORT_KB_NOT_RAW must be defined as well.
  * Note: these values are only used when accessing the keyboard and PWM
  * function of the IT8801 chip.  I/O expander functions are accessed using
  * the ioex_config[] array.
