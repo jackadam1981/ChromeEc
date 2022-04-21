ca767030a12e759d1c8b4f8f65540f0d9baa6ba2
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2022-04-21 13:52:28 +0800

    dojo: Set up board FW_CONFIG parsing
    Set up basic file to use for parsing FW_CONFIG. Currently, this just
    has a function to return the KB BL type.
    
    BUG=b:221315327
    BRANCH=cherry
    TEST=make sure FW_CONFIG prints current value correctly.
    
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Change-Id: I1b220faf39868ee1b4716a54bb165fd693fba4de

---
+++ board/dojo/cbi_fw_config.h
@@ -0,0 +1,23 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef _DOJO_CBI_FW_CONFIG__H_
+#define _DOJO_CBI_FW_CONFIG__H_
+
+/****************************************************************************
+ * Dojo CBI FW Configuration
+ */
+
+/*
+ * Keyboard backlight (1 bit)
+ */
+enum fw_config_kblight_type {
+	KB_BL_ABSENT = 0,
+	KB_BL_PRESENT = 1,
+};
+#define FW_CONFIG_KB_BL_OFFSET			0
+#define FW_CONFIG_KB_BL_MASK			GENMASK(0, 0)
+
+#endif /* _DOJO_CBI_FW_CONFIG__H_ */
