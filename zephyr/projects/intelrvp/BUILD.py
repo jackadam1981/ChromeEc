04d63f19b223b57daa2638b54c1b0ef7672652ec
Brandon Breitenstein <brandon.breitenstein@intel.com>
2022-04-25 22:58:11 -0700

    zephyr: Enable USB Type C on Nuvoton Ports for MTL
    This patch will enable the Type C functionality on ports 0 and 1 for
    Meteorlake RVP.
    
    BUG=none
    BRANCH=none
    TEST=zmake mtlrvpp_npcx flashed and tested USB, DP, TBT, and USB4
    
    Signed-off-by: Brandon Breitenstein <brandon.breitenstein@intel.com>
    Change-Id: I11e7fe1b8aeb7ae4a7ddf0b55dcddd2ac37c9100

--- zephyr/projects/intelrvp/BUILD.py
+++ zephyr/projects/intelrvp/BUILD.py
@@ -69,6 +69,7 @@ register_intelrvp_project(
         here / "mtlrvp/ioex.dts",
         here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx.dts",
         here / "adlrvp/adlrvp_npcx/temp_sensor.dts",
+        here / "mtlrvp/usbc.dts",
     ],
     extra_kconfig_files=[
         here / "legacy_ec_pwrseq.conf",
