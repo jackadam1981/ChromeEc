0ebb56db97b2d921500f782dd38fc1dc77db7050
ben chen <ben.chen2@quanta.corp-partner.google.com>
2022-04-26 15:57:14 +0800

    ocpc: Init VSYS with battery present
    init VSYS wiht battery pack is present
    
    BUG=b:229941346
    BRANCH=dedede
    TEST=magke buildall pass
    
    Change-Id: Ibee99b28c7e56ff130004b0ef92dce2e4ff4adfd
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- common/ocpc.c
+++ common/ocpc.c
@@ -666,7 +666,8 @@ void ocpc_reset(struct ocpc_data *ocpc)
 	 * Initialize the VSYS target on the aux chargers to the current battery
 	 * voltage to avoid a large spike.
 	 */
-	if (ocpc->active_chg_chip > CHARGER_PRIMARY && batt.voltage > 0) {
+	if (ocpc->active_chg_chip > CHARGER_PRIMARY && batt.voltage > 0
+	    && battery_is_present() == BP_YES) {
 		CPRINTS("OCPC: C%d Init VSYS to %dmV", ocpc->active_chg_chip,
 			batt.voltage);
 		charger_set_voltage(ocpc->active_chg_chip, batt.voltage);
