21d089980a6d9905f9fbfb24e2a279d986f0b0b4
Brandon Breitenstein <brandon.breitenstein@intel.com>
2022-04-25 22:58:11 -0700

    zephyr: Enable USB Type C on Nuvoton Ports for MTL
    This patch will enable the Type C functionality on ports 0 and 1 for
    Meteorlake RVP.
    
    BUG=none
    BRANCH=none
    TEST=verified tcpcs are coming up on board
    
    Signed-off-by: Brandon Breitenstein <brandon.breitenstein@intel.com>
    Change-Id: I11e7fe1b8aeb7ae4a7ddf0b55dcddd2ac37c9100

---
+++ zephyr/projects/intelrvp/src/chg_usb_pd_mecc_1_1.c
@@ -0,0 +1,103 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Intel-RVP family-specific configuration */
+
+#include "console.h"
+#include "include/gpio.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "intelrvp.h"
+#include "ioexpander.h"
+#include "tcpm/tcpci.h"
+#include "system.h"
+#include "usbc_ppc.h"
+
+#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ## args)
+#define CPRINTS(format, args...) cprints(CC_USBPD, format, ## args)
+
+static void baseboard_tcpc_init(void)
+{
+
+	/* Only reset TCPC if not sysjump */
+	if (!system_jumped_late())
+		board_reset_pd_mcu();
+
+	 /* Enable PPC interrupts. */
+        gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0_ppc));
+        gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c1_ppc));
+
+        /* Enable TCPC interrupts. */
+        gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0_c1_tcpc));
+
+	/* Enable CCD Mode interrupt */
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_ccd_mode));
+
+}
+DECLARE_HOOK(HOOK_INIT, baseboard_tcpc_init, HOOK_PRIO_POST_I2C);
+
+
+void tcpc_alert_event(enum gpio_signal signal)
+{
+	int i;
+
+	for (i = 0; i < CONFIG_USB_PD_PORT_MAX_COUNT; i++) {
+		/* No alerts for embedded TCPC */
+		if (tcpc_config[i].bus_type == EC_BUS_TYPE_EMBEDDED)
+			continue;
+
+		if (signal == tcpc_aic_gpios[i].tcpc_alert) {
+			schedule_deferred_pd_interrupt(i);
+			break;
+		}
+	}
+}
+
+uint16_t tcpc_get_alert_status(void)
+{
+	uint16_t status = 0;
+	int i;
+
+	/* Check which port has the ALERT line set */
+	for (i = 0; i < CONFIG_USB_PD_PORT_MAX_COUNT; i++) {
+		/* No alerts for embdeded TCPC */
+		if (tcpc_config[i].bus_type == EC_BUS_TYPE_EMBEDDED)
+			continue;
+
+		if (!gpio_get_level(tcpc_aic_gpios[i].tcpc_alert))
+			status |= PD_STATUS_TCPC_ALERT_0 << i;
+	}
+
+	return status;
+}
+
+int ppc_get_alert_status(int port)
+{
+	return tcpc_aic_gpios[port].ppc_intr_handler &&
+		!gpio_get_level(tcpc_aic_gpios[port].ppc_alert);
+}
+
+/* PPC support routines */
+void ppc_interrupt(enum gpio_signal signal)
+{
+	int i;
+
+	for (i = 0; i < CONFIG_USB_PD_PORT_MAX_COUNT; i++) {
+		if (tcpc_aic_gpios[i].ppc_intr_handler &&
+			signal == tcpc_aic_gpios[i].ppc_alert) {
+			tcpc_aic_gpios[i].ppc_intr_handler(i);
+			break;
+		}
+	}
+}
+
+void board_charging_enable(int port, int enable)
+{
+	if (tcpc_aic_gpios[port].ppc_intr_handler ?
+		ppc_vbus_sink_enable(port, enable) :
+		tcpc_config[port].drv->set_snk_ctrl(port, enable))
+		CPRINTS("C%d: sink path %s failed",
+				port, enable ? "en" : "dis");
+}
