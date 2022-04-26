f2d42881b3e3cddc6072f7459692d378303bdc3b
Eric Yilun Lin <yllin@chromium.org>
2022-04-26 14:28:26 +0800

    usbpd: add flag for BIST test mode
    BUG=b:229812911
    TEST=Pass BIST test mode with ANX3447 on kingler
    BRANCH=none
    
    Change-Id: I4f9b853454d747d2efb968310e4c89ab0845f56b

--- common/usbc/usb_pe_private.h
+++ common/usbc/usb_pe_private.h
@@ -80,6 +80,8 @@ enum {
 	PE_FLAGS_SNK_WAITING_BATT_FN,
 /* FLAG to note that a data reset is complete */
 	PE_FLAGS_DATA_RESET_COMPLETE_FN,
+/* FLAG to note that BIST test data mode is enabled  */
+	PE_FLAGS_BIST_TEST_DATA_MODE_FN,
 /* Last element */
 	PE_FLAGS_COUNT
 };
