98dbdbada0c12001b956a9c4106042bbd3d9cf58
Ting Shen <phoenixshen@google.com>
2022-04-28 14:50:41 +0800

    usb_mux: add TUSB564 driver
    Reuse the code from existing TUSB1064/TUSB1044 driver and add TUSB546
    specific logic to it.
    
    Currently the only change is that we will enable IN_HPD when mux enters
    dock mode, instead of using hpd_update.
    
    BUG=b:228289594
    TEST=make
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I2182eef1a4c5d8cc830f837c544379e854ac331c

--- include/config.h
+++ include/config.h
@@ -5322,6 +5322,12 @@
 /* Support the Texas Instrument TUSB1064 Type-C Redriving Switch (UFP) */
 #undef CONFIG_USB_MUX_TUSB1064
 
+/*
+ * Support yjr TUSB546 USB Type-C DP ALT Mode Linear Redriver Crosspoint
+ * Switch
+ */
+#undef CONFIG_USB_MUX_TUSB546
+
 /* Support the Parade PS8822 Type-C Redriving Demux Switch */
 #undef CONFIG_USB_MUX_PS8822
 
