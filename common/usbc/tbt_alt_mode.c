f94660ac2662f68dbfed25e1333fc181a5486ea8
Abe Levkoy <alevkoy@chromium.org>
2022-04-29 19:19:22 -0600

    DNS: Break TBT mode entry
    Work around b/230921757.
    
    BUG=b:230921757
    TEST=make buildall
    BRANCH=none
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: I1386496de8e0c2c787e4ed06c2766373cac9a981

--- common/usbc/tbt_alt_mode.c
+++ common/usbc/tbt_alt_mode.c
@@ -140,13 +140,14 @@ bool tbt_cable_entry_is_done(int port)
 
 static void tbt_exit_done(int port)
 {
-    /*
-     * If the EC exits an alt mode autonomously, don't try to enter it again. If
-     * the AP commands the EC to exit DP mode, it might command the EC to enter
-     * again later, so leave the state machine ready for that possibility.
-     */
-	tbt_state[port] = IS_ENABLED(CONFIG_USB_PD_REQUIRE_AP_MODE_ENTRY)
-		? TBT_START : TBT_INACTIVE;
+	/*
+	 * If the EC exits an alt mode autonomously, don't try to enter it
+	 * again. If the AP commands the EC to exit TBT mode, it might command
+	 * the EC to enter again later, so leave the state machine ready for
+	 * that possibility.
+	 */
+	tbt_state[port] = /*IS_ENABLED(CONFIG_USB_PD_REQUIRE_AP_MODE_ENTRY)
+		? TBT_START :*/ TBT_INACTIVE;
 	TBT_CLR_FLAG(port, TBT_FLAG_RETRY_DONE);
 	TBT_CLR_FLAG(port, TBT_FLAG_CABLE_ENTRY_DONE);
 
