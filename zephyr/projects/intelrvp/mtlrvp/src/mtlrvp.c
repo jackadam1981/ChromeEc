b383e948a9851e8737379da19d48f8f59570fd22
Brandon Breitenstein <brandon.breitenstein@intel.com>
2022-05-02 13:43:54 -0700

    [WIP] Temporary Workarounds for MTL
    This is a temporary patch for Meteorlake do not merge
    
    Signed-off-by: Brandon Breitenstein <brandon.breitenstein@intel.com>
    Change-Id: I15d05f78eff8245cd43d0be7b5b810e4813f0ca1

--- zephyr/projects/intelrvp/mtlrvp/src/mtlrvp.c
+++ zephyr/projects/intelrvp/mtlrvp/src/mtlrvp.c
@@ -211,6 +211,12 @@ static void board_connect_c0_sbu_deferred(void)
 {
 	enum pd_power_role prole;
 
+	ioex_set_level(IOEX_USB_C0_MUX_SBU_SEL_1, 0);
+        ioex_set_level(IOEX_USB_C0_MUX_SBU_SEL_0, 0);
+		ppc_set_sbu(0, 1);
+	return;
+
+
 	if (gpio_get_level(GPIO_CCD_MODE_ODL)) {
 		CPRINTS("Default AUX line connected");
 		/* Default set the SBU lines to AUX mode */
