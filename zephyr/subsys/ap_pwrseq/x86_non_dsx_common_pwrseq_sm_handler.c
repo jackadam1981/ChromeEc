d6e1cf4253a3581e93cebc3d32f3b4ad4dc87b49
Li Feng <li1.feng@intel.com>
2022-05-06 23:36:09 -0700

    zephyr: check power rails is platform specific
    BUG=none
    BRANCH=none
    TEST=zmake build nivviks, flash, boot up.
    
    Signed-off-by: Li Feng <li1.feng@intel.com>
    Change-Id: I991569d50728e0c07292bc134dffa433e36a66a4

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
@@ -76,14 +76,9 @@ static inline bool signals_valid_and_off(power_signal_mask_t signals)
 	return signals_valid(signals) && power_signals_off(signals);
 }
 
-static int check_power_rails_enabled(void)
+static bool check_power_rails_enabled(void)
 {
-	int out = 1;
-
-	out &= power_signal_get(PWR_EN_PP3300_A);
-	out &= power_signal_get(PWR_EN_PP5000_A);
-	out &= power_signal_get(PWR_EC_SOC_DSW_PWROK);
-	return out;
+	return board_ap_power_check_power_rails_enabled();
 }
 
 enum power_states_ndsx pwr_sm_get_state(void)
