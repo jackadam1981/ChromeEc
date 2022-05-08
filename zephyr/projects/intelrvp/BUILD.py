145da38134080dbda7ca61ad2df6c35774bab20e
Li Feng <li1.feng@intel.com>
2022-05-07 22:56:25 -0700

    zephyr: enable build for mtlrvpp_npcx_shim
    Provide build target mtlrvpp_npcx_shim to build with legacy EC power
    sequencing.
    We can revert this CL if not needed later.
    
    BUG=none
    BRANCH=none
    TEST=zmake mtlrvpp_npcx_shim; zmake mtlrvpp_npcx;.
    
    Signed-off-by: Li Feng <li1.feng@intel.com>
    Change-Id: I143a82fd4347e7ee3b28941e985fef563a16179b

--- zephyr/projects/intelrvp/BUILD.py
+++ zephyr/projects/intelrvp/BUILD.py
@@ -76,3 +76,21 @@ register_intelrvp_project(
         here / "mtlrvp/mtlrvpp_npcx/prj.conf",
     ],
 )
+
+register_intelrvp_project(
+    project_name="mtlrvpp_npcx_shim",
+    chip="npcx9",
+    extra_dts_overlays=[
+        here / "adlrvp/adlrvp_npcx/cbi_eeprom.dts",
+        here / "mtlrvp/mtlrvpp_npcx/fan.dts",
+        here / "mtlrvp/mtlrvpp_npcx/gpio.dts",
+        here / "mtlrvp/mtlrvpp_npcx/interrupts_shim.dts",
+        here / "mtlrvp/ioex.dts",
+        here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx.dts",
+        here / "adlrvp/adlrvp_npcx/temp_sensor.dts",
+    ],
+    extra_kconfig_files=[
+        here / "legacy_ec_pwrseq.conf",
+        here / "mtlrvp/mtlrvpp_npcx/prj_shim.conf",
+    ],
+)
