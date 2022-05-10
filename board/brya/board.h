4003cce34436f786e1ae55b571bdf8f05c3a0750
poornima tom <poornima.tom@intel.com>
2022-05-10 23:42:56 +0530

    brya: Add support for DBPT
    To support DBPT in brya, corresponding config
    must be enabled in board file. Also, board specific
    value for system resistance and minimum system voltage
    that must be configured in fuel guage for battery must
    be initialised.
    
    BUG=b:205928013
    BRANCH=None
    TEST=make buildall
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: Ia4b62fad30efd1d9a66d586a6103798181fcf890

--- board/brya/board.h
+++ board/brya/board.h
@@ -18,6 +18,9 @@
 /* Add Power participant support in DTT */
 #define CONFIG_DTT_POWER_STATUS
 
+/* Add support for DBPT */
+#define CONFIG_BATTERY_DBPT_V2PLUS
+
 /* Baseboard features */
 #include "baseboard.h"
 
