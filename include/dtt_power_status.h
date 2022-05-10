30d1a0931371fb48235748716b55dc4103d15a74
poornima tom <poornima.tom@intel.com>
2022-05-10 23:25:27 +0530

    power: initialization of DBPT
    As part of DBPT initialization, it is required
    to initialize the system resistance and minimum
    system voltage ideal for the board. This CL
    takes care of it.
    
    BUG=b:205928013
    BRANCH=None
    TEST=make buildall
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: I1ddcefc64f150d3c31e82c57b1ff110809c1cd59

---
+++ include/dtt_power_status.h
@@ -0,0 +1,17 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ */
+#ifndef __CROS_EC_DTT_POWER_STATUS_H
+#define __CROS_EC_DTT_POWER_STATUS_H
+
+struct dbpt_batt_params {
+        uint16_t sys_resistance;
+	uint16_t min_sys_volt;
+};
+
+/* Forward declare board specific data to be used by common code */
+extern const struct dbpt_batt_params batt_param;
+
+#endif /* __CROS_EC_DTT_POWER_STATUS_H */
