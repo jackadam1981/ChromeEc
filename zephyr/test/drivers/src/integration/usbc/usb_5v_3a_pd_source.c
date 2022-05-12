b9a6702a4d0f04d40d5dc4120ca28942c1fc35af
Tristan Honscheid <honscheid@google.com>
2022-05-12 11:34:05 -0600

    tests: Set TCPCI revision in suite usb_attach_5v_3a_pd_source
    The test suite usb_attach_5v_3a_pd_source contains two flaky tests
    (test_power_info and test_typec_status) that fail when the suite is run
    in isolation or out-of-order.
    
    Abe noticed that the test doesn't set the TCPCI revision, which we need
    to be higher than the default of 1.0. These tests were previously
    relying on other test suites to make this update.
    
    This change sets the TCPCI version in the board config and emulator
    (both are required and must agree) in the suite's setup function. It
    may be required in other suites as well, but those will get handled on
    a case-by-case basis as test shuffling reveals more flaky tests.
    
    BRANCH=None
    BUG=b:232431388
    TEST=zmake test test-drivers
    
    Signed-off-by: Tristan Honscheid <honscheid@google.com>
    Change-Id: I55f1ad46c3dfb370f43a6f80cd93913a95335f9a

--- zephyr/test/drivers/src/integration/usbc/usb_5v_3a_pd_source.c
+++ zephyr/test/drivers/src/integration/usbc/usb_5v_3a_pd_source.c
@@ -36,6 +36,10 @@ static void *usb_attach_5v_3a_pd_source_setup(void)
 	test_fixture.source_5v_3a.data.pdo[1] =
 		PDO_FIXED(5000, 3000, PDO_FIXED_UNCONSTRAINED);
 
+	/* Configure TCPCI revision in board config and emulator */
+	tcpc_config[0].flags |= TCPC_FLAGS_TCPCI_REV2_0;
+	tcpci_emul_set_rev(test_fixture.tcpci_emul, TCPCI_EMUL_REV2_0_VER1_1);
+
 	return &test_fixture;
 }
 
