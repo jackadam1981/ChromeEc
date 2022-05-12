e153849fdce8369ec6af96a26dce1dd2b65243a6
james2 lee <james2_lee@pegatron.corp-partner.google.com>
2022-05-12 13:57:26 +0800

    pirika: bring-up test
    Change charger code from RAA489000 to SM5803
    
    BUG=none
    BRANCH=none
    TEST=Manual
    
    manual build code pass.
    
    Signed-off-by: james2 lee <james2_lee@pegatron.corp-partner.google.com>
    Change-Id: I6dab7ea38fbdc201aacf0c907d09e10e1831b4ac

--- board/pirika/board.c
+++ board/pirika/board.c
@@ -40,6 +40,9 @@
 #include "usb_mux.h"
 #include "usb_pd.h"
 #include "usb_pd_tcpm.h"
+#include "driver/charger/sm5803.h"
+#include "driver/tcpm/anx7447.h"
+#include "driver/tcpm/it83xx_pd.h"
 
 #define CPRINTUSB(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
@@ -115,8 +118,8 @@ __override const struct ec_response_keybd_config
 
 static void notify_c0_chips(void)
 {
-	schedule_deferred_pd_interrupt(0);
 	task_set_event(TASK_ID_USB_CHG_P0, USB_CHG_EVENT_BC12);
+	sm5803_interrupt(0);
 }
 
 static void check_c0_line(void)
@@ -151,6 +154,7 @@ static void notify_c1_chips(void)
 {
 	schedule_deferred_pd_interrupt(1);
 	task_set_event(TASK_ID_USB_CHG_P1, USB_CHG_EVENT_BC12);
+	sm5803_interrupt(1);
 }
 
 static void check_c1_line(void)
@@ -235,154 +239,47 @@ const struct pi3usb9201_config_t pi3usb9201_bc12_chips[] = {
 
 int pd_snk_is_vbus_provided(int port)
 {
-	return pd_check_vbus_level(port, VBUS_PRESENT);
+	return sm5803_is_vbus_present(port);
 }
 
 /* Charger chips */
 const struct charger_config_t chg_chips[] = {
 	[CHARGER_PRIMARY] = {
 		.i2c_port = I2C_PORT_USB_C0,
-		.i2c_addr_flags = ISL923X_ADDR_FLAGS,
-		.drv = &isl923x_drv,
+		.i2c_addr_flags = SM5803_ADDR_CHARGER_FLAGS,
+		.drv = &sm5803_drv,
 	},
 	[CHARGER_SECONDARY] = {
 		.i2c_port = I2C_PORT_SUB_USB_C1,
-		.i2c_addr_flags = ISL923X_ADDR_FLAGS,
-		.drv = &isl923x_drv,
+		.i2c_addr_flags = SM5803_ADDR_CHARGER_FLAGS,
+		.drv = &sm5803_drv,
 	},
 };
 
 /* TCPCs */
 const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	{
-		.bus_type = EC_BUS_TYPE_I2C,
-		.i2c_info = {
-			.port = I2C_PORT_USB_C0,
-			.addr_flags = RAA489000_TCPC0_I2C_FLAGS,
-		},
-		.flags = TCPC_FLAGS_TCPCI_REV2_0,
-		.drv = &raa489000_tcpm_drv,
+		.bus_type = EC_BUS_TYPE_EMBEDDED,
+		.drv = &it83xx_tcpm_drv,
 	},
 	{
 		.bus_type = EC_BUS_TYPE_I2C,
 		.i2c_info = {
 			.port = I2C_PORT_SUB_USB_C1,
-			.addr_flags = RAA489000_TCPC0_I2C_FLAGS,
+			.addr_flags = AN7447_TCPC0_I2C_ADDR_FLAGS,
 		},
+		.drv = &anx7447_tcpm_drv,
 		.flags = TCPC_FLAGS_TCPCI_REV2_0,
-		.drv = &raa489000_tcpm_drv,
 	},
 };
 
 /* USB Retimer */
-enum tusb544_conf {
-	USB_DP = 0,
-	USB_DP_INV,
-	USB,
-	USB_INV,
-	DP,
-	DP_INV
-};
-
-static int board_tusb544_set(const struct usb_mux *me,
-		mux_state_t mux_state)
-{
-	int  rv = EC_SUCCESS;
-	enum tusb544_conf usb_mode = 0;
-	/* USB */
-	if (mux_state & USB_PD_MUX_USB_ENABLED) {
-		/* USB with DP */
-		if (mux_state & USB_PD_MUX_DP_ENABLED) {
-			usb_mode = (mux_state & USB_PD_MUX_POLARITY_INVERTED)
-					? USB_DP_INV
-					: USB_DP;
-		}
-		/* USB without DP */
-		else {
-			usb_mode = (mux_state & USB_PD_MUX_POLARITY_INVERTED)
-					? USB_INV
-					: USB;
-		}
-	}
-	/* DP without USB */
-	else if (mux_state & USB_PD_MUX_DP_ENABLED) {
-		usb_mode = (mux_state & USB_PD_MUX_POLARITY_INVERTED)
-				? DP_INV
-				: DP;
-	}
-	/* Nothing enabled */
-	else
-		return EC_SUCCESS;
-	/* Write the retimer config byte */
-	if (usb_mode == USB_INV) {
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_GENERAL4, 0x15);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_1, 0x33);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_2, 0x33);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_USB3_1_1, 0x22);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_USB3_1_2, 0x22);
-	} else if (usb_mode == USB) {
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_GENERAL4, 0x11);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_1, 0x33);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_2, 0x33);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_USB3_1_1, 0x22);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_USB3_1_2, 0x22);
-	} else if (usb_mode == USB_DP_INV) {
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_GENERAL4, 0x1F);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_1, 0x33);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_2, 0x99);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_USB3_1_1, 0x22);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_USB3_1_2, 0x22);
-	} else if (usb_mode == USB_DP) {
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_GENERAL4, 0x1B);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_1, 0x99);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_2, 0x33);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_USB3_1_1, 0x22);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_USB3_1_2, 0x22);
-	} else if (usb_mode == DP_INV) {
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_GENERAL4, 0x1E);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_1, 0x99);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_2, 0x99);
-	} else if (usb_mode == DP) {
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_GENERAL4, 0x1A);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_1, 0x99);
-		rv = i2c_write8(me->i2c_port, me->i2c_addr_flags,
-				TUSB544_REG_DISPLAYPORT_2, 0x99);
-	}
-
-	return rv;
-}
 
 const struct usb_mux usbc1_retimer = {
 	.usb_port = 1,
 	.i2c_port = I2C_PORT_SUB_USB_C1,
 	.i2c_addr_flags = TUSB544_I2C_ADDR_FLAGS0,
 	.driver = &tusb544_drv,
-	.board_set = &board_tusb544_set,
 };
 
 /* USB Muxes */
@@ -396,8 +293,8 @@ const struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	{
 		.usb_port = 1,
 		.i2c_port = I2C_PORT_SUB_USB_C1,
-		.i2c_addr_flags = IT5205_I2C_ADDR1_FLAGS,
-		.driver = &it5205_usb_mux_driver,
+		.i2c_addr_flags = AN7447_TCPC0_I2C_ADDR_FLAGS,
+		.driver = &anx7447_usb_mux_driver,
 		.next_mux = &usbc1_retimer,
 	},
 };
@@ -413,14 +310,19 @@ void board_init(void)
 	 * If interrupt lines are already low, schedule them to be processed
 	 * after inits are completed.
 	 */
-	if (!gpio_get_level(GPIO_USB_C0_INT_ODL))
-		hook_call_deferred(&check_c0_line_data, 0);
-	if (!gpio_get_level(GPIO_USB_C1_INT_ODL))
-		hook_call_deferred(&check_c1_line_data, 0);
+	check_c0_line();
+	check_c1_line();
 
 	gpio_enable_interrupt(GPIO_USB_C0_CCSBU_OVP_ODL);
 	/* Enable Base Accel interrupt */
 	gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
+
+	/* Charger on the MB will be outputting PROCHOT_ODL and OD CHG_DET */
+	sm5803_configure_gpio0(CHARGER_PRIMARY, GPIO0_MODE_PROCHOT, 1);
+	sm5803_configure_chg_det_od(CHARGER_PRIMARY, 1);
+
+	/* Charger on the sub-board will be a push-pull GPIO */
+	sm5803_configure_gpio0(CHARGER_SECONDARY, GPIO0_MODE_OUTPUT, 0);
 
 	/* Turn on 5V if the system is on, otherwise turn it off */
 	on = chipset_in_state(CHIPSET_STATE_ON | CHIPSET_STATE_ANY_SUSPEND |
@@ -439,15 +341,31 @@ void board_init(void)
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
+static void board_resume(void)
+{
+	sm5803_disable_low_power_mode(CHARGER_PRIMARY);
+	if (board_get_charger_chip_count() > 1)
+		sm5803_disable_low_power_mode(CHARGER_SECONDARY);
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_resume, HOOK_PRIO_DEFAULT);
+
+static void board_suspend(void)
+{
+	sm5803_enable_low_power_mode(CHARGER_PRIMARY);
+	if (board_get_charger_chip_count() > 1)
+		sm5803_enable_low_power_mode(CHARGER_SECONDARY);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_suspend, HOOK_PRIO_DEFAULT);
+
 void board_hibernate(void)
 {
 	/*
 	 * Put all charger ICs present into low power mode before entering
 	 * z-state.
 	 */
-	raa489000_hibernate(CHARGER_PRIMARY, true);
+	sm5803_hibernate(CHARGER_PRIMARY);
 	if (board_get_charger_chip_count() > 1)
-		raa489000_hibernate(CHARGER_SECONDARY, true);
+		sm5803_hibernate(CHARGER_SECONDARY);
 }
 
 __override void board_ocpc_init(struct ocpc_data *ocpc)
@@ -472,21 +390,14 @@ __override void board_power_5v_enable(int enable)
 	 */
 	gpio_set_level(GPIO_EN_PP5000, !!enable);
 	gpio_set_level(GPIO_EN_USB_A0_VBUS, !!enable);
-	if (isl923x_set_comparator_inversion(1, !!enable))
-		CPRINTS("Failed to %sable sub rails!", enable ? "en" : "dis");
+	if (sm5803_set_gpio0_level(1, !!enable))
+		CPRINTUSB("Failed to %sable sub rails!", enable ? "en" : "dis");
 }
 
 uint16_t tcpc_get_alert_status(void)
 {
 	uint16_t status = 0;
 	int regval;
-
-	if (!gpio_get_level(GPIO_USB_C0_INT_ODL)) {
-		if (!tcpc_read16(0, TCPC_REG_ALERT, &regval)) {
-			if (regval)
-				status = PD_STATUS_TCPC_ALERT_0;
-		}
-	}
 
 	/* Check whether TCPC 1 pulled the shared interrupt line */
 	if (!gpio_get_level(GPIO_USB_C1_INT_ODL)) {
@@ -511,100 +422,83 @@ void board_set_charge_limit(int port, int supplier, int charge_ma, int max_ma,
 	charge_set_input_current_limit(icl, charge_mv);
 }
 
-int board_is_sourcing_vbus(int port)
-{
-	int regval;
-
-	tcpc_read(port, TCPC_REG_POWER_STATUS, &regval);
-	return !!(regval & TCPC_REG_POWER_STATUS_SOURCING_VBUS);
-}
-
 int board_set_active_charge_port(int port)
 {
 		int is_real_port = (port >= 0 &&
 			    port < board_get_usb_pd_port_count());
-	int i;
-	int old_port;
 
 	if (!is_real_port && port != CHARGE_PORT_NONE)
 		return EC_ERROR_INVAL;
 
-	old_port = charge_manager_get_active_charge_port();
-
-	CPRINTS("New chg p%d", port);
-
 	/* Disable all ports. */
 	if (port == CHARGE_PORT_NONE) {
-		for (i = 0; i < board_get_usb_pd_port_count(); i++) {
-			tcpc_write(i, TCPC_REG_COMMAND,
-				   TCPC_REG_COMMAND_SNK_CTRL_LOW);
-			raa489000_enable_asgate(i, false);
-		}
+
+		CPRINTUSB("Disabling all charge ports");
+
+		sm5803_vbus_sink_enable(CHARGER_PRIMARY, 0);
+
+		if (board_get_charger_chip_count() > 1)
+			sm5803_vbus_sink_enable(CHARGER_SECONDARY, 0);
 
 		return EC_SUCCESS;
 	}
 
-	/* Check if port is sourcing VBUS. */
-	if (board_is_sourcing_vbus(port)) {
-		CPRINTS("Skip enable p%d", port);
-		return EC_ERROR_INVAL;
-	}
+	CPRINTUSB("New chg p%d", port);
 
 	/*
-	 * Turn off the other ports sink path FETs, before enabling the
-	 * requested charge port.
+	 * Ensure other port is turned off, then enable new charge port
 	 */
-	for (i = 0; i < board_get_usb_pd_port_count(); i++) {
-		if (port == i)
-			continue;
+	if (port == 0) {
+		if (board_get_charger_chip_count() > 1)
+			sm5803_vbus_sink_enable(CHARGER_SECONDARY, 0);
+		sm5803_vbus_sink_enable(CHARGER_PRIMARY, 1);
 
-		if (tcpc_write(i, TCPC_REG_COMMAND,
-			       TCPC_REG_COMMAND_SNK_CTRL_LOW))
-			CPRINTS("p%d: sink path disable failed.", i);
-		raa489000_enable_asgate(i, false);
+	} else {
+		sm5803_vbus_sink_enable(CHARGER_PRIMARY, 0);
+		sm5803_vbus_sink_enable(CHARGER_SECONDARY, 1);
 	}
-
-	/*
-	 * Stop the charger IC from switching while changing ports.  Otherwise,
-	 * we can overcurrent the adapter we're switching to. (crbug.com/926056)
-	 */
-	if (old_port != CHARGE_PORT_NONE)
-		charger_discharge_on_ac(1);
-
-	/* Enable requested charge port. */
-	if (raa489000_enable_asgate(port, true) ||
-	    tcpc_write(port, TCPC_REG_COMMAND,
-		       TCPC_REG_COMMAND_SNK_CTRL_HIGH)) {
-		CPRINTS("p%d: sink path enable failed.", port);
-		charger_discharge_on_ac(0);
-		return EC_ERROR_UNKNOWN;
-	}
-
-	/* Allow the charger IC to begin/continue switching. */
-	charger_discharge_on_ac(0);
 
 	return EC_SUCCESS;
 
+}
+
+/* Vconn control for integrated ITE TCPC */
+void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled)
+{
+	/* Vconn control is only for port 0 */
+	if (port)
+		return;
+
+	if (cc_pin == USBPD_CC_PIN_1)
+		gpio_set_level(GPIO_EN_USB_C0_CC1_VCONN, !!enabled);
+	else
+		gpio_set_level(GPIO_EN_USB_C0_CC2_VCONN, !!enabled);
 }
 
 __override void ocpc_get_pid_constants(int *kp, int *kp_div,
 				       int *ki, int *ki_div,
 				       int *kd, int *kd_div)
 {
-	*kp = 1;
-	*kp_div = 20;
-	*ki = 1;
-	*ki_div = 250;
-	*kd = 0;
-	*kd_div = 1;
+	*kp = 3;
+	*kp_div = 14;
+
+	*ki = 3;
+	*ki_div = 500;
+
+	*kd = 4;
+	*kd_div = 40;
 }
 
 __override void typec_set_source_current_limit(int port, enum tcpc_rp_value rp)
 {
-	if (port < 0 || port > board_get_usb_pd_port_count())
+	int current;
+
+	if (port < 0 || port > CONFIG_USB_PD_PORT_MAX_COUNT)
 		return;
 
-	raa489000_set_output_current(port, rp);
+	current = (rp == TYPEC_RP_3A0) ? 3000 : 1500;
+
+	charger_set_otg_current_voltage(port, current, 5000);
 }
 
 /* PWM channels. Must be in the exactly same order as in enum pwm_channel. */
