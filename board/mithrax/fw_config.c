9b64340d22499fbf193dcd2c1a44d31e43fd0ca1
johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
2022-05-13 10:05:09 +0800

    Mithrax: modify FW_config for RGB type
    Add fw config settings for RGB type to switch RGB backlight support
    or not.
    
    BUG=b:229186412
    BRANCH=none
    TEST=make BOARD=mithrax
    
    Signed-off-by: johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
    Change-Id: I5b21ac4f3efca9f22fa019954c93320bad8e8121

--- board/mithrax/fw_config.c
+++ board/mithrax/fw_config.c
@@ -53,3 +53,8 @@ enum ec_cfg_stylus_type ec_cfg_stylus(void)
 {
 	return fw_config.stylus;
 }
+
+enum ec_cfg_rgb_type ec_cfg_rgb(void)
+{
+	return fw_config.rgb;
+}
--- board/mithrax/fw_config.h
+++ board/mithrax/fw_config.h
@@ -34,10 +34,16 @@ enum ec_cfg_stylus_type {
 	STYLUS_PRSENT = 1
 };
 
+enum ec_cfg_rgb_type {
+	NORMAL_BACKLIGHT = 0,
+	RGB_BACKLIGHT = 1
+};
+
 union mithrax_cbi_fw_config {
 	struct {
 		enum ec_cfg_usb_db_type			usb_db : 3;
-		uint32_t				wifi : 2;
+		uint32_t				wifi : 1;
+		enum ec_cfg_rgb_type			rgb : 1;
 		enum ec_cfg_stylus_type			stylus : 1;
 		enum ec_cfg_keyboard_backlight_type	kb_bl : 1;
 		uint32_t				audio : 3;
@@ -78,3 +84,10 @@ enum ec_cfg_usb_mb_type ec_cfg_usb_mb_type(void);
  * @return the stylus type.
  */
 enum ec_cfg_stylus_type ec_cfg_stylus(void);
+
+/**
+ * Get the rgb type from FW_CONFIG.
+ *
+ * @return the rgb type.
+ */
+enum ec_cfg_rgb_type ec_cfg_rgb(void);
