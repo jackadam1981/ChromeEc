e2b55489f567e8405050d7cfa1fad1d7bee94892
Dino Li <Dino.Li@ite.com.tw>
2022-05-17 16:06:14 +0800

    zephyr: boards: support chip variants of it81202bx and it81302bx
    We divide baseboard into it81202bx and it81302bx, which is convenient
    for maintenance.
    Ex: SOC_IT8XXX2_GPIO_GROUP_K_L_DEFAULT_PULL_DOWN does not be enabled
    on it81302 chip.
    
    BUG=none
    BRANCH=none
    TEST=zmake build it8xxx2_evb --clobber
         zmake build krabby --clobber
         zmake build nereid --clobber
    TEST=Make sure gpiok(0xf01690-0xf01697) and gpiol(0xf01698-0xf0169f)
         group are set as input and pull-down in krabby project and do
         not be set in nereid project.
    
    Change-Id: I5a12e9f97aef2ef14193491cfad46a6614969c8a
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

--- zephyr/projects/nissa/BUILD.py
+++ zephyr/projects/nissa/BUILD.py
@@ -9,7 +9,7 @@
 
 def register_nissa_project(
     project_name,
-    chip="it8xxx2",
+    chip="it81302bx",
     extra_dts_overlays=(),
     extra_kconfig_files=(),
 ):
@@ -42,7 +42,7 @@ register_nissa_project(
 
 register_nissa_project(
     project_name="nereid",
-    chip="it8xxx2",
+    chip="it81302bx",
     extra_dts_overlays=[
         here / "nereid_generated.dts",
         here / "nereid_overlay.dts",
