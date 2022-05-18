a18f9d3ae5c66890cacf8d0041b5277e01b9aa35
Tim Wawrzynczak <twawrzynczak@chromium.org>
2022-05-18 13:15:39 -0600

    chip/npcx: Poll VW DIRTY bit around SCI# and SMI# pulses
    This patch changes the way SCI# and SMI# are signalled over their
    virtual wires; before and after generating the low pulse, the code
    will ensure that the VWEVSM DIRTY bit is not set, which should
    correspond to the host acknowledging the change in VWIRE state.
    Because the code now verifies that the host has recognized the change
    in signal level, the extension of the deasserted state of SCI# and
    SMI# is removed, as we already should know that the host has captured
    both the asserted and then deasserted states of the respective VWs.
    
    BUG=b:230366077
    BRANCH=none
    TEST=attempt SCI-loss reproduction scenario from
    b:227367177, and verify that no SCIs were noticed as missing.
    
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Change-Id: I8cb72a92064ad98f2c5c49caa3c87fdfe7afa4d5

--- chip/npcx/lpc.c
+++ chip/npcx/lpc.c
@@ -10,6 +10,7 @@
 #include "clock.h"
 #include "common.h"
 #include "console.h"
+#include "espi.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "host_command.h"
@@ -145,11 +146,12 @@ static void lpc_generate_smi(void)
 	 * from SMIB/SCIB doesn't really reflect the SMI/SCI status. SMI/SCI
 	 * status should be read from bit 1/0 in eSPI VMEVSM(2) register.
 	 */
-	NPCX_HIPMIC(PMC_ACPI) = NPCX_VW_SMI(1);
-	udelay(CONFIG_ESPI_DEFAULT_VW_WIDTH_US);
 	/* Generate a falling edge */
+	espi_wait_vw_not_dirty(VW_SMI_L, CONFIG_ESPI_DEFAULT_VW_WIDTH_US);
 	NPCX_HIPMIC(PMC_ACPI) = NPCX_VW_SMI(0);
 	udelay(CONFIG_ESPI_DEFAULT_VW_WIDTH_US);
+	espi_wait_vw_not_dirty(VW_SMI_L, CONFIG_ESPI_DEFAULT_VW_WIDTH_US);
+
 	/* Set signal high */
 	NPCX_HIPMIC(PMC_ACPI) = NPCX_VW_SMI(1);
 #else
@@ -191,11 +193,12 @@ static void lpc_generate_sci(void)
 	 * from SMIB/SCIB doesn't really reflect the SMI/SCI status. SMI/SCI
 	 * status should be read from bit 1/0 in eSPI VMEVSM(2) register.
 	 */
-	NPCX_HIPMIC(PMC_ACPI) = NPCX_VW_SCI(1);
-	udelay(CONFIG_ESPI_DEFAULT_VW_WIDTH_US);
 	/* Generate a falling edge */
+	espi_wait_vw_not_dirty(VW_SCI_L, CONFIG_ESPI_DEFAULT_VW_WIDTH_US);
 	NPCX_HIPMIC(PMC_ACPI) = NPCX_VW_SCI(0);
 	udelay(CONFIG_ESPI_DEFAULT_VW_WIDTH_US);
+	espi_wait_vw_not_dirty(VW_SCI_L, CONFIG_ESPI_DEFAULT_VW_WIDTH_US);
+
 	/* Set signal high */
 	NPCX_HIPMIC(PMC_ACPI) = NPCX_VW_SCI(1);
 #else
