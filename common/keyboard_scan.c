93d3eee0cad63b233355c2df4afeee2f51de8076
Daisuke Nojiri <dnojiri@chromium.org>
2022-05-20 20:01:34 +0000

    keyboard: Scan boot keys by scancode
    WIP
    
    This patch makes check_key_list scan boot keys by scancode instead of
    column and row.
    
    BUG=b:232737379
    BRANCH=None
    TEST=None
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: Ie339d974ac390817be014230ef6125d22eeb6b01

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -85,10 +85,15 @@ struct boot_key_entry {
 };
 
 #ifdef CONFIG_KEYBOARD_BOOT_KEYS
+
+#define KEYBOARD_SCANCODE_ESC		0x0076
+#define KEYBOARD_SCANCODE_DOWN		0xe072
+#define KEYBOARD_SCANCODE_LEFT_SHIFT	0x0012
+
 static const struct boot_key_entry boot_key_list[] = {
-	{KEYBOARD_COL_ESC, KEYBOARD_ROW_ESC},   /* Esc */
-	{KEYBOARD_COL_DOWN, KEYBOARD_ROW_DOWN}, /* Down-arrow */
-	{KEYBOARD_COL_LEFT_SHIFT, KEYBOARD_ROW_LEFT_SHIFT}, /* Left-Shift */
+	KEYBOARD_SCANCODE_ESC,
+	KEYBOARD_SCANCODE_DOWN,
+	KEYBOARD_SCANCODE_LEFT_SHIFT,
 };
 static uint32_t boot_key_value = BOOT_KEY_NONE;
 #endif
@@ -660,7 +665,6 @@ static uint32_t check_key_list(const uint8_t *state)
 	uint8_t curr_state[KEYBOARD_COLS_MAX];
 	int c;
 	uint32_t boot_key_mask = BOOT_KEY_NONE;
-	const struct boot_key_entry *k;
 
 	/* Make copy of current debounced state. */
 	memcpy(curr_state, state, sizeof(curr_state));
@@ -683,11 +687,21 @@ static uint32_t check_key_list(const uint8_t *state)
 	curr_state[KEYBOARD_COL_REFRESH] &= ~keyboard_mask_refresh;
 
 	/* Update mask with all boot keys that were pressed. */
-	k = boot_key_list;
-	for (c = 0; c < ARRAY_SIZE(boot_key_list); c++, k++) {
-		if (curr_state[k->col] & BIT(k->row)) {
-			boot_key_mask |= BIT(c);
-			curr_state[k->col] &= ~BIT(k->row);
+	for (c = 0; c < keyboard_cols; c++) {
+		uint8_t s = curr_state[c];
+
+		for (int r = 0; r < KEYBOARD_ROWS; r++) {
+			uint16_t code;
+
+			if (!(s & BIT(r)))
+				continue;
+
+			code = get_scancode_set2(r, c);
+
+			for (int i = 0; i < ARRAY_SIZE(boot_key_list); i++) {
+				if (boot_key_list[i] == code)
+					boot_key_mask |= BIT(i);
+			}
 		}
 	}
 
