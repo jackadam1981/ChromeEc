66d9b8a1ccee906e1ec0769ff8ddb096fa3890af
Jett Rink <jettrink@google.com>
2022-05-20 16:36:00 -0600

    gsctool: block ti50 upgrade without going through 0.0.15
    We need to ensure that versions before 0.0.15 upgrade to 0.0.15 before
    going to 0.0.16 or later. Otherwise, the first page of the new RW is
    erased when upgrading RO and we need to rescue.
    
    BUG=none
    TEST=none :( This is hard to test since I don't have these versions
    
    Change-Id: I79b41fb86c22aa37163264815faef9558370d702
    Signed-off-by: Jett Rink <jettrink@google.com>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -1120,8 +1120,8 @@ static void fetch_header_versions(const void *image)
 
 
 /* Compare to signer headers and determine which one is newer. */
-static int a_newer_than_b(struct signed_header_version *a,
-			  struct signed_header_version *b)
+static int a_newer_than_b(const struct signed_header_version *a,
+			  const struct signed_header_version *b)
 {
 	uint32_t fields[][3] = {
 		{a->epoch, a->major, a->minor},
@@ -1152,6 +1152,38 @@ static int a_newer_than_b(struct signed_header_version *a,
 
 	return 0;	/* All else being equal A is no newer than B. */
 }
+
+/*
+ * Determine if the current RW version can be upgrade to the potential RW
+ * version.
+ */
+static int supports_rw_upgrade(const struct signed_header_version *current_rw,
+			       const struct signed_header_version *to_rw)
+{
+	/*
+	 * Disallow upgrade to 0.0.16+ without going through 0.0.15
+	 * first. This check won't be needed after 2023-01-01
+	 */
+	const struct signed_header_version ver15 = { .epoch = 0,
+						     .major = 0,
+						     .minor = 15 };
+	const int current_less_than_15 = a_newer_than_b(&ver15, current_rw);
+	const int to_greater_than_15 = a_newer_than_b(to_rw, &ver15);
+
+	switch (image_magic) {
+	case MAGIC_DAUNTLESS:
+		if (current_less_than_15 && to_greater_than_15) {
+			printf("Must upgrade to 0.0.15 first!\n");
+			return false;
+		} else {
+			return true;
+		}
+	default:
+		/* No restrictions */
+		return true;
+	}
+}
+
 /*
  * Pick sections to transfer based on information retrieved from the target,
  * the new image, and the protocol version the target is running.
@@ -1180,8 +1212,12 @@ static void pick_sections(struct transfer_descriptor *td)
 			 */
 
 			if (a_newer_than_b(&sections[i].shv, &targ.shv[1]) ||
-			    !td->upstart_mode)
-				sections[i].ustatus = needed;
+			    !td->upstart_mode) {
+				if (supports_rw_upgrade(&targ.shv[1],
+							&sections[i].shv)) {
+					sections[i].ustatus = needed;
+				}
+			}
 			/* Rest of loop is RO */
 			continue;
 		}
