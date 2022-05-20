4840c0a6439462e2699689c0145851094f520bc7
Tinghan Shen <tinghan.shen@mediatek.com>
2022-05-20 18:29:55 +0800

    ec_features: add new feature for scp core 1
    Add EC_FEATURE_SCP_C1.
    
    BRANCH=none
    BUG=b:193814857
    TEST=Run 'feat' on SCP core 1 uart
    feat
     0-31: 0x00006000
    32-63: 0x00002013
                 ^
                 The bit 45 is asserted.
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: Iba8a91d2bba914142a6698185fec8c805cdcbdca

--- common/ec_features.c
+++ common/ec_features.c
@@ -133,7 +133,11 @@ uint32_t get_feature_flags1(void)
 		| EC_FEATURE_MASK_1(EC_FEATURE_EFS2)
 #endif
 #ifdef CONFIG_IPI
+#ifdef CHIP_VARIANT_MT8195_CORE1
+		| EC_FEATURE_MASK_1(EC_FEATURE_SCP_C1)
+#else
 		| EC_FEATURE_MASK_1(EC_FEATURE_SCP)
+#endif
 #endif
 #ifdef CHIP_ISH
 		| EC_FEATURE_MASK_1(EC_FEATURE_ISH)
