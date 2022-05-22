8d996ef53948df7fcf7e3dfe2ab02f870b914d13
poornima tom <poornima.tom@intel.com>
2022-05-23 02:28:35 +0530

    power: Add support for RBHF in DBPT
    For DTT Battery participant, RBHF indicates the high
    frequency impedance. This value must be read from
    Fuel guage register and the ec memmap region must be
    updated every 1 sec. Also, if there is any change
    of 5mohm in RBHF, CB must be notified with an sci
    event to notify the change. This CL takes care of
    this changes.
    
    BUG=b:205928013
    BRANCH=None
    TEST=make buildall
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: Ib5e02f4830f0ebafb53dfe5050059f2006a93c93

--- power/power_status.c
+++ power/power_status.c
@@ -200,8 +200,11 @@ void update_dbpt(void)
 {
 	static int count;
 	uint16_t batt_max_peak_power, batt_sus_peak_power;
+	uint16_t batt_high_freq_impedance;
 	uint16_t prev_batt_max_peak_power, prev_batt_sus_peak_power;
+	uint16_t prev_batt_high_freq_impedance;
 	uint16_t threshold_max_power_change, threshold_sus_peak_power_change;
+	uint16_t threshold_high_freq_impedance_change;
 
 	/* return if fuel guage is not initialized */
 	if (!dbpt_initialized)
@@ -210,11 +213,14 @@ void update_dbpt(void)
 	/* Read from fuel guage */
 	batt_max_peak_power = battery_maximum_power();
 	batt_sus_peak_power = battery_sustained_power();
+	batt_high_freq_impedance = battery_high_frequency_impedance();
 
 	prev_batt_max_peak_power =
 			*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_PMAX));
 	prev_batt_sus_peak_power =
 			*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_PBSS));
+	prev_batt_high_freq_impedance =
+			*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_RBHF));
 
 	/* Check for any threshold level change */
 	if (prev_batt_max_peak_power < batt_max_peak_power) {
@@ -233,29 +239,46 @@ void update_dbpt(void)
 							batt_sus_peak_power);
 	}
 
+	if (prev_batt_high_freq_impedance < batt_high_freq_impedance) {
+			threshold_high_freq_impedance_change =
+					(batt_high_freq_impedance -
+					 prev_batt_high_freq_impedance);
+	} else {
+			threshold_high_freq_impedance_change =
+					(prev_batt_high_freq_impedance -
+					 batt_high_freq_impedance);
+	}
+
 	/* update ec_memmap regions */
 	*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_PMAX)) =
                                                         batt_max_peak_power;
 	*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_PBSS)) =
 							batt_sus_peak_power;
+	*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_RBHF)) =
+						batt_high_freq_impedance;
 
 	/* Send sci event for any threshold level cross */
 	if ((threshold_max_power_change >= 250) ||
-			       (threshold_sus_peak_power_change >= 100)) {
+			       (threshold_sus_peak_power_change >= 100) ||
+			       (threshold_high_freq_impedance_change >= 5)) {
 		CPRINTS("PTOM:BATTERY_sci event-23 on threshold above 250mv/100mv");
 		CPRINTS("PTOM- thresh_max_pwr_chnge-%d,thresh_sus_pk_pwr_chnge-%d",
 		threshold_max_power_change, threshold_sus_peak_power_change);
+		CPRINTS("thresh_high_freq_impedance_change - %d",
+				threshold_high_freq_impedance_change);
 		host_set_single_event(EC_HOST_EVENT_BATTERY_STATUS);
 	}
 
 
 	// For testing purpose
 	if (count<5) {
-		CPRINTS("PTOM: PMAX-%d, PBSS-%d",
-			batt_max_peak_power, batt_sus_peak_power);
-		CPRINTS("PTOM_in memap: PMAX-%d, PBSS-%d",
+		CPRINTS("PTOM: PMAX-%d, PBSS-%d, RBFH-%d",
+			batt_max_peak_power, batt_sus_peak_power,
+			batt_high_freq_impedance);
+		CPRINTS("PTOM_in memap: PMAX-%d, PBSS-%d, RBFH-%d",
 			*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_PMAX)),
-			*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_PBSS)));
+			*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_PBSS)),
+			*((uint16_t *)host_get_memmap(EC_MEMMAP_BATT_RBHF)));
 		count++;
 	}
 	else
@@ -294,12 +317,14 @@ static void power_status_init(void)
 	uint16_t *memmap_artg = (uint16_t *)host_get_memmap(EC_MEMMAP_PWR_ARTG);
 	uint16_t *memmap_pmax = (uint16_t *)host_get_memmap(EC_MEMMAP_BATT_PMAX);
 	uint16_t *memmap_pbss = (uint16_t *)host_get_memmap(EC_MEMMAP_BATT_PBSS);
+	uint16_t *memmap_rbhf = (uint16_t *)host_get_memmap(EC_MEMMAP_BATT_RBHF);
 
 	/* Initial Value */
 	*memmap_psrc = 0;
 	*memmap_artg = 0;
 	*memmap_pmax = 0;
 	*memmap_pbss = 0;
+	*memmap_rbhf = 0;
 
 	/* Update the initial information on power source */
 	update_power_source();
