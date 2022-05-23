1c109c7a43cb5542ba9144f9bfedf577ba6c80c5
Tinghan Shen <tinghan.shen@mediatek.com>
2022-05-23 10:26:13 +0800

    chip/mt_scp: mt8195: support suspend/resume of SCP core 1
    The suspend/resume of SCP core 1 is achieved by exchange information
    with SCP core 0 which controlls the main SCP clock.
    
    When AP notifies SCP core 0(c0) to enter S3 mode, c0 snoops the 26M
    clock and requests SCP core 1(c1) to disable watchdog and IRQ when 26M is
    off. c0 will wait for ready signal from c1. If received the c1 ready
    signal or timeout, c0 changes the main clock to 26M to stop most activities
    of SCP. Once 26M is backed, c0 informs c1 to enable watchdog and IRQ.
    
    when AP enters S3 mode, the 26M clock source is blocked, so c0 snoops it to
    determine the time of disabling watchdog and IRQ.
    
    BRANCH=none
    BUG=b:193814857
    TEST=suspend_stress_test --suspend_min 15 --suspend_max 18 --count 3000
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I9db19e2804a472b001f7f59390e40cbd9603d3cc

--- chip/mt_scp/mt8195/clock_s3.c
+++ chip/mt_scp/mt8195/clock_s3.c
@@ -4,24 +4,42 @@
  */
 #include "clock_s3.h"
 #include "common.h"
+#include "registers.h"
 #include "scp_timer.h"
 #include "scp_watchdog.h"
 #include "task.h"
 #include "timer.h"
 
 #define CHECK_26M_PERIOD_US 50000
+#define CHECK_CORE1_MAX_RETRY 3
+#define NO_CORE1 0xFF
 
 enum scp_sr_state {
 	SR_S0,
 	SR_S02S3,
+	SR_S02S3_WAIT_C1,
 	SR_S3,
 };
+
+static void irq_group11_handler(void)
+{
+	extern volatile int ec_int;
+
+	task_set_event(TASK_ID_SR, TASK_EVENT_C1_READY);
+	SCP_GIPC_IN_CLR = GIPC_IN(S3_IPI_READY);
+	asm volatile ("fence.i" ::: "memory");
+	task_clear_pending_irq(ec_int);
+}
+DECLARE_IRQ(11, irq_group11_handler, 0);
 
 void sr_task(void *u)
 {
 	enum scp_sr_state state = SR_S0;
 	uint32_t event;
-	uint32_t prev, now;
+	uint32_t prev = 0, now = 0;
+	uint32_t c1_retry = 0;
+
+	task_enable_irq(SCP_IRQ_GIPC_IN2);
 
 	while(1) {
 		switch (state) {
@@ -34,6 +52,7 @@ void sr_task(void *u)
 			}
 			break;
 		case SR_S02S3:
+			/* wait 26M off */
 			event = task_wait_event(CHECK_26M_PERIOD_US);
 			if (event & TASK_EVENT_RESUME) {
 				/* suspend is aborted */
@@ -46,8 +65,34 @@ void sr_task(void *u)
 					prev = now;
 				} else {
 					/* 26M is off */
-					state = SR_S3;
+					if (SCP_CORE1_RSTN_CLR & SCP_CORE1_RUN) {
+						/* alert core 1 to enter S3 */
+						state = SR_S02S3_WAIT_C1;
+						c1_retry = 0;
+						SCP_GIPC_IN_SET = GIPC_IN(S3_IPI_SUSPEND);
+					} else {
+						state = SR_S3;
+						c1_retry = NO_CORE1;
+					}
 				}
+			}
+			break;
+		case SR_S02S3_WAIT_C1:
+			/* wait core 1 ready */
+			event = task_wait_event(CHECK_26M_PERIOD_US);
+			if (event & TASK_EVENT_RESUME) {
+				/* suspend is aborted */
+				timer_disable(TIMER_SR);
+				state = SR_S0;
+				/* alert core 1 that core 0 resumed */
+				SCP_GIPC_IN_SET = GIPC_IN(S3_IPI_RESUME);
+			} else if (event & TASK_EVENT_C1_READY) {
+				/* core 1 is ready */
+				state = SR_S3;
+			} else if (event & TASK_EVENT_TIMER) {
+				c1_retry += 1;
+				if (c1_retry >= CHECK_CORE1_MAX_RETRY)
+					state = SR_S3;
 			}
 			break;
 		case SR_S3:
@@ -64,6 +109,11 @@ void sr_task(void *u)
 			interrupt_enable();
 			timer_disable(TIMER_SR);
 			state = SR_S0;
+
+			if (c1_retry < NO_CORE1) {
+				/* alert core 1 that core 0 resumed */
+				SCP_GIPC_IN_SET = GIPC_IN(S3_IPI_RESUME);
+			}
 			break;
 		}
 	}
