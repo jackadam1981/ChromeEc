14ed509288c292dfa295eb8d3a28410ec7de3ef9
Tinghan Shen <tinghan.shen@mediatek.com>
2022-05-23 10:26:13 +0800

    chip/mt_scp: mt8195: support suspend/resume of SCP core 1
    The suspend/resume of SCP core 1 is achieved by exchange information
    with SCP core 0 which controlls the main SCP clock.
    
    When AP notifies SCP core 0(c0) to enter S3 mode, c0 snoops the 26M
    clock and requests SCP core 1(c1) to disable watchdog and IRQ when 26M is
    off. c0 will wait for ready signal from c1. If received the c1 ready
    signal or timeout, c0 changes the main clock to 26M to stop most activities
    of SCP. Once 26M is backed, c0 informs c1 to enable watchdog and IRQ.
    
    when AP enters S3 mode, the 26M clock source is blocked, so c0 snoops it to
    determine the time of disabling watchdog and IRQ.
    
    BRANCH=none
    BUG=b:193814857
    TEST=suspend_stress_test --suspend_min 15 --suspend_max 18 --count 3000
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I9db19e2804a472b001f7f59390e40cbd9603d3cc

---
+++ chip/mt_scp/mt8195/clock_s3_mt8195_core1.c
@@ -0,0 +1,55 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include "clock_s3.h"
+#include "common.h"
+#include "console.h"
+#include "registers.h"
+#include "scp_watchdog.h"
+#include "task.h"
+
+#define CPRINTS(format, args...) cprints(CC_CLOCK, format, ##args)
+
+static void irq_group11_handler(void)
+{
+	extern volatile int ec_int;
+	uint32_t sr_st;
+
+	sr_st = SCP_GIPC_IN_SET;
+	if (sr_st & GIPC_IN(S3_IPI_SUSPEND)) {
+		CPRINTS("AP suspend");
+		task_set_event(TASK_ID_SR, TASK_EVENT_SUSPEND);
+		SCP_GIPC_IN_CLR = GIPC_IN(S3_IPI_SUSPEND);
+	} else if (sr_st & GIPC_IN(S3_IPI_RESUME)) {
+		CPRINTS("AP resume");
+		SCP_GIPC_IN_CLR = GIPC_IN(S3_IPI_RESUME);
+	}
+	asm volatile ("fence.i" ::: "memory");
+	task_clear_pending_irq(ec_int);
+}
+DECLARE_IRQ(11, irq_group11_handler, 0);
+
+void sr_task(void *u)
+{
+	uint32_t event;
+
+	task_enable_irq(SCP_IRQ_GIPC_IN3);
+
+	while(1) {
+		event = task_wait_event(-1);
+		if (event & TASK_EVENT_SUSPEND) {
+			interrupt_disable();
+			watchdog_disable();
+
+			/* alert core1 that core2 is ready */
+			SCP_GIPC_IN_SET = GIPC_IN(S3_IPI_READY);
+
+			/* wait core 1 resume */
+			while ((SCP_GIPC_IN_SET & GIPC_IN(S3_IPI_RESUME)) == 0);
+
+			watchdog_enable();
+			interrupt_enable();
+		}
+	}
+}
