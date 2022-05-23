8996e7886022224e283c9ed9063efe86cdb38b5a
Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
2022-05-23 14:55:38 +0800

    led
    Signed-off-by: Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
    Change-Id: Ic838483c43e201c1ee1d8e619759be027cc99133

--- board/moli/led.c
+++ board/moli/led.c
@@ -84,9 +84,9 @@ static int set_color(enum ec_led_id id, enum led_color color, int duty)
 	}
 }
 
-#define LED_PULSE_US		(2 * SECOND)
-/* 40 msec for nice and smooth transition. */
-#define LED_PULSE_TICK_US	(40 * MSEC)
+#define LED_PULSE_US		(500 * MSEC)
+#define LED_PULSE_TICK_US	(10 * MSEC)
+#define LED_PERIOD		(4 * SECOND)
 
 /*
  * When pulsing is enabled, brightness is incremented by <duty_inc> every
@@ -98,17 +98,21 @@ static struct {
 	int duty_inc;
 	enum led_color color;
 	int duty;
+	uint32_t time;
 } led_pulse;
 
-#define CONFIG_TICK(interval, color) \
-	config_tick((interval), 100 / (LED_PULSE_US / (interval)), (color))
+#define CONFIG_TICK(interval, time, color) \
+	config_tick((interval), 100 / (LED_PULSE_US / (interval)), \
+			LED_PERIOD - 2 * LED_PULSE_US, (color))
 
-static void config_tick(uint32_t interval, int duty_inc, enum led_color color)
+static void config_tick(uint32_t interval, int duty_inc,
+					int time, enum led_color color)
 {
 	led_pulse.interval = interval;
 	led_pulse.duty_inc = duty_inc;
 	led_pulse.color = color;
 	led_pulse.duty = 0;
+	led_pulse.time = time;
 }
 
 static void pulse_power_led(enum led_color color)
@@ -133,12 +137,13 @@ static void led_tick(void)
 		pulse_power_led(led_pulse.color);
 	elapsed = get_time().le.lo - start;
 	next = led_pulse.interval > elapsed ? led_pulse.interval - elapsed : 0;
+	next = (led_pulse.duty - led_pulse.duty_inc) ? next : next + led_pulse.time;
 	hook_call_deferred(&led_tick_data, next);
 }
 
 static void led_suspend(void)
 {
-	CONFIG_TICK(LED_PULSE_TICK_US, LED_AMBER);
+	CONFIG_TICK(LED_PULSE_TICK_US, LED_PERIOD, LED_BLUE);
 	led_tick();
 }
 DECLARE_DEFERRED(led_suspend);
@@ -177,8 +182,11 @@ static void led_resume(void)
 	 */
 	hook_call_deferred(&led_suspend_data, -1);
 	hook_call_deferred(&led_shutdown_data, -1);
-	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
-		set_color(EC_LED_ID_POWER_LED, LED_AMBER, 100);
+	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED)) {
+		set_color(EC_LED_ID_POWER_LED, LED_BLUE, 100);
+		//CONFIG_TICK(LED_PULSE_TICK_US, LED_PERIOD, LED_AMBER);
+		//led_tick();
+	}
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, led_resume, HOOK_PRIO_DEFAULT);
 
@@ -186,7 +194,7 @@ void led_alert(int enable)
 {
 	if (enable) {
 		/* Overwrite the current signal */
-		config_tick(1 * SECOND, 100, LED_BLUE);
+		config_tick(1 * SECOND, 100, 0, LED_AMBER);
 		led_tick();
 	} else {
 		/* Restore the previous signal */
@@ -203,7 +211,7 @@ void show_critical_error(void)
 {
 	hook_call_deferred(&led_tick_data, -1);
 	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
-		set_color(EC_LED_ID_POWER_LED, LED_BLUE, 100);
+		set_color(EC_LED_ID_POWER_LED, LED_AMBER, 100);
 }
 
 static int command_led(int argc, char **argv)
