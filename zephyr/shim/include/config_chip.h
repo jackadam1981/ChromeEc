45bce56c5a6931d914ea9a7bdf6ac75b9ad89e76
Fabio Baltieri <fabiobaltieri@google.com>
2022-05-23 15:56:08 +0000

    zephyr: shim: add single charge task support
    Add an option to run the charger code in a single task.
    
    BRANCH=none
    BUG=b:226411332
    TEST=zmake testall
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>
    Change-Id: I75a63f7b0a9545e6c824114de7f81b71924e0789

--- zephyr/shim/include/config_chip.h
+++ zephyr/shim/include/config_chip.h
@@ -1442,6 +1442,7 @@ extern struct jump_data mock_jump_data;
 #endif /* CONFIG_PLATFORM_EC_USB_POWER_DELIVERY */
 
 #ifdef CONFIG_PLATFORM_EC_USB_CHARGER
+#ifndef CONFIG_PLATFORM_EC_USB_CHARGER_SINGLE_TASK
 #define HAS_TASK_USB_CHG_P0 1
 
 #if CONFIG_USB_PD_PORT_MAX_COUNT > 1
@@ -1456,6 +1457,7 @@ extern struct jump_data mock_jump_data;
 #define HAS_TASK_USB_CHG_P3 1
 #endif /* CONFIG_USB_PD_PORT_MAX_COUNT > 3 */
 
+#endif /* PLATFORM_EC_USB_CHARGER_SINGLE_TASK */
 #endif /* CONFIG_PLATFORM_EC_USB_CHARGER */
 
 #undef CONFIG_USB_PD_ITE_ACTIVE_PORT_COUNT
