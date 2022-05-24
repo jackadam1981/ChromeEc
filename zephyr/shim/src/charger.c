4ccc2e6d8f06ebda354a9bc2c220636888ba104e
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-05-23 19:18:50 -0700

    zephyr: ap_pwrseq: Handle low battery charge
    This will prevent AP power up when battery charge is too low and power
    up AP automatically as soon as battery charge level becomes acceptable.
    
    BUG=b:234049176
    BRANCH=None
    TEST=Build, flash, boot and follow steps:
         1. Discharge battery to ~2%
         2. Press power button
              Board should not boot
         3. Connect AC adapter
              Board should boot automatically
    
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Change-Id: Ida4f6557c998e3e6059d68a66672e35890b13046

--- zephyr/shim/src/charger.c
+++ zephyr/shim/src/charger.c
@@ -4,12 +4,14 @@
  */
 
 #include "charger.h"
+#include "charge_state.h"
 #include <zephyr/devicetree.h>
 #include "charger/chg_bq25710.h"
 #include "charger/chg_isl923x.h"
 #include "charger/chg_isl9241.h"
 #include "charger/chg_rt9490.h"
 #include "charger/chg_sm5803.h"
+#include "hooks.h"
 
 #define CHG_CHIP(id, fn) [DT_REG_ADDR(DT_PARENT(id))] = fn(id)
 
@@ -39,4 +41,34 @@ BUILD_ASSERT(ARRAY_SIZE(chg_chips) == 1,
 BUILD_ASSERT(ARRAY_SIZE(chg_chips) == CONFIG_USB_PD_PORT_MAX_COUNT,
 	"For the OCPC config, the number of defined charger chips must equal "
 	"the number of USB-C ports.");
+#endif
+
+#if defined(CONFIG_AP_PWRSEQ)
+LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
+
+static bool charger_ready;
+
+bool ap_power_is_ok_to_power_up(void)
+{
+	return charger_ready =
+		!charge_prevent_power_on(false) && !charge_want_shutdown();
+}
+
+static void hook_battery_soc_change(void)
+{
+	if (charger_ready) {
+		/* Do not poll charger state if we know it is ready */
+		return;
+	}
+
+	if (ap_power_is_ok_to_power_up()) {
+		/* Charger is Ready, power up the AP */
+		chipset_exit_hard_off();
+	}
+
+	LOG_INF("Battery is %s to boot AP!",
+		 charger_ready ? "READY" : "NOT READY");
+}
+DECLARE_HOOK(HOOK_BATTERY_SOC_CHANGE, hook_battery_soc_change,
+	     HOOK_PRIO_DEFAULT);
 #endif
