4fa7864554e14e621fe5bbe0792c31522992267b
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-05-23 19:18:50 -0700

    zephyr: ap_pwrseq: Handle low battery charge
    This will prevent AP power up when battery charge is too low and power
    up AP automatically as soon as battery charge level becomes acceptable.
    
    BUG=b:234049176
    BRANCH=None
    TEST=Build, flash, boot and follow steps:
         1. Discharge battery to ~2%
         2. Press power button
              Board should not boot
         3. Connect AC adapter
              Board should boot automatically
    
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Change-Id: Ida4f6557c998e3e6059d68a66672e35890b13046

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
@@ -222,6 +222,11 @@ static int common_pwr_sm_run(int state)
 		break;
 
 	case SYS_POWER_STATE_G3S5:
+		if (!ap_power_is_ok_to_power_up()) {
+			ap_power_force_shutdown(
+				CHIPSET_SHUTDOWN_BATTERY_INHIBIT);
+			return SYS_POWER_STATE_G3;
+		}
 		if ((power_get_signals() & PWRSEQ_G3S5_UP_SIGNAL) ==
 				PWRSEQ_G3S5_UP_VALUE)
 			return SYS_POWER_STATE_S5;
