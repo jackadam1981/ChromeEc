138f6e4be0b268f4cbd9f4d861575b26fb6ec5a1
Tinghan Shen <tinghan.shen@mediatek.com>
2022-05-25 13:13:15 +0800

    board/cherry_scp: reserve SRAM for SCP core 1
    Reserve a SRAM section for new board of cherry SCP core 1.
    
    BRANCH=none
    BUG=b:193814857
    TEST=make BOARD=cherry_scp
    
    Change-Id: Ia9429d6aa91e009f0d08a93bc506f62f26fd47df
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>

--- board/cherry_scp/board.h
+++ board/cherry_scp/board.h
@@ -10,12 +10,19 @@
 
 #include "baseboard.h"
 
+#define SCP_CORE1_RAM_SIZE 0x10000 /* 64K */
+#define SCP_CORE1_RAM_PADDING 0xc00 /* for 4K-alignment */
+
 /*
  * RW only, no flash
  * +-------------------- 0x0
  * | ROM vectortable, .text, .rodata, .data LMA
  * +-------------------- 0x68000
  * | RAM .bss, .data
+ * +-------------------- 0xaf000 (4k-alignment)
+ * | SCP core1 boot code
+ * +-------------------- 0xbf000 (4k-alignment)
+ * | Reserved (padding for 4k-alignment)
  * +-------------------- 0xbfc00
  * | Reserved (padding for 1k-alignment)
  * +-------------------- 0xbfdb0
@@ -26,7 +33,9 @@
 #define CONFIG_RAM_BASE 0x68000
 #define CONFIG_ROM_SIZE (CONFIG_RAM_BASE - CONFIG_ROM_BASE)
 #define CONFIG_RAM_SIZE ((CONFIG_IPC_SHARED_OBJ_ADDR & (~(0x400 - 1))) - \
-			 CONFIG_RAM_BASE)
+			 CONFIG_RAM_BASE - \
+			 SCP_CORE1_RAM_SIZE - \
+			 SCP_CORE1_RAM_PADDING)
 
 #define SCP_FW_END 0xc0000
 
