8a89d60de97ba44204c518d740b1e5ee7edfbaea
Sam Hurst <shurst@google.com>
2022-05-25 10:24:54 -0700

    ec: Add EC_SPI_OLD_READY_MSB_LOW host command
    Some EC SPI interface configurations places the MSB
    of a data byte on SDO when CS goes low.
    The EC_SPI_OLD_READY_MSB_LOW macro should be used
    if the SDO line should not go high when CS goes low.
    
    BUG=b:216831450
    BRANCH=none
    TEST=zmake testall
         Verified on scope that SHI_SDO does not pulse high
         during AP Shutdown
    
    Signed-off-by: Sam Hurst <shurst@google.com>
    Change-Id: I851633855e1c2bcb91f98ca81612cbdea8330ee0

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -864,6 +864,15 @@ struct ec_lpc_host_args {
  */
 #define EC_SPI_OLD_READY      0xfd
 
+/*
+ * Same as EC_SPI_OLD_READY.
+ *
+ * NOTE: Some SPI configurations place the Most Significant Bit on SDO when
+ *	 CS goes low. This macro has the Most Significant Bit set to zero,
+ *	 so SDO will not be driven high when CS goes low.
+ */
+#define EC_SPI_OLD_READY_MSB_LOW 0x7d
+
 /*****************************************************************************/
 
 /*
