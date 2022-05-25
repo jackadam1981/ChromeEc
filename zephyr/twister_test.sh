542d57b663f35b8188e6f2ad3b6021f11bb89799
Tristan Honscheid <honscheid@google.com>
2022-05-24 18:36:23 -0600

    Sample Twister-based Test
    Convert the math test to run in twister. Run the twister_test.sh script.
    
    This commit is just an FYI for developers
    
    BRANCH=None
    BUG=None
    TEST=zephyr/twister_test.sh
    
    Signed-off-by: Tristan Honscheid <honscheid@google.com>
    Change-Id: Id6bb214430d5ee5fe7fbe83c600395bae1013b79

---
+++ zephyr/twister_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export EC_DIR=/mnt/host/source/src/platform/ec
+
+export TOOLCHAIN_ROOT=${EC_DIR}/zephyr
+
+export ZEPHYR_DIR=/mnt/host/source/src/third_party/zephyr
+export ZEPHYR_BASE=${ZEPHYR_DIR}/main
+export ZEPHYR_TOOLCHAIN_VARIANT=llvm
+
+${ZEPHYR_DIR}/main/scripts/twister \
+        -T ${EC_DIR}/zephyr/test/math/ \
+        -x=ZEPHYR_MODULES="${ZEPHYR_DIR}/cmsis;${ZEPHYR_DIR}/nanopb;${ZEPHYR_DIR}/hal_stm32;${EC_DIR}" \
+        -x=CMAKE_EXPORT_COMPILE_COMMANDS="ON" \
+        -x=DTS_ROOT="${EC_DIR}/zephyr" \
+        -x=SYSCALL_INCLUDE_DIRS="${EC_DIR}/zephyr/include/drivers" \
+        --ninja --clobber \
+        -p native_posix
