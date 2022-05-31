8e88824da4ba1e0badb76f30630265ccbfb14abc
lschyi <lschyi@google.com>
2022-05-31 16:59:13 +0800

    kingler: Set up USB C1 SBU protection with TCPM SBU
    The USB C1 SBU line in kingler is provided by TCPM module, enable it in
    the configuration and remove setting it from tcpc init function.
    
    BUG=b:208372205
    BRANCH=none
    TEST=manually test with inserting a type c display, and check the SBU
         register is set to enabled.
    
    Change-Id: I69189e19d3151181655b9290eed7d19e1c318e4f
    Signed-off-by: lschyi <lschyi@google.com>

--- zephyr/projects/corsola/src/kingler/usbc_config.c
+++ zephyr/projects/corsola/src/kingler/usbc_config.c
@@ -194,7 +194,6 @@ __override int board_rt1718s_init(int port)
 
 	/* Turn on SBU switch */
 	RETURN_ERROR(rt1718s_update_bits8(port, RT1718S_RT2_SBU_CTRL_01,
-				RT1718S_RT2_SBU_CTRL_01_SBU_VIEN |
 				RT1718S_RT2_SBU_CTRL_01_SBU2_SWEN |
 				RT1718S_RT2_SBU_CTRL_01_SBU1_SWEN,
 				0xFF));
