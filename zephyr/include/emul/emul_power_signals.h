70166f641083aa23ae2275786578e34343040fa1
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-06-01 19:14:29 -0700

    zephyr: ap_pwrseq: Add Power Signals Emulator
    Power Signal Emulator provides an engine in which a given set of defined
    power signals connection is instantiated to simulate platform behavior.
    
    This emulator is created to supports executing AP Power Sequence test by
    configuring power signals behavior using devicetree.
    
    BUG=b:222933615
    TEST=zmake test test-ap_power
    BRANCH=None
    
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Change-Id: If1b1af2d287e23fe73e77986356bd3c50d56f148

---
+++ zephyr/include/emul/emul_power_signals.h
@@ -0,0 +1,52 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef EMUL_POWER_SIGNALS_H_
+#define EMUL_POWER_SIGNALS_H_
+
+#define AP_PWR_TEST_PLATFORM_ENUM_WITH_COMMA(inst) \
+	DT_STRING_TOKEN(inst, name_id),
+
+/**
+ * @brief Test platform definition,
+ *        This structure contains all power signal nodes associated to one
+ *        test.
+ */
+struct power_signal_emul_test_platform {
+	char *name_id;
+	int nodes_count;
+	struct power_signal_emul_node **nodes;
+};
+
+#define EMUL_POWER_SIGNAL_TEST_PLATFORM_DECL(inst) \
+	extern const struct power_signal_emul_test_platform inst;
+
+#define EMUL_POWER_SIGNAL_TEST_PLATFORM(inst) (&DT_CAT(DT_N_S_, inst))
+
+DT_FOREACH_STATUS_OKAY(ap_pwr_test_platform,
+		       EMUL_POWER_SIGNAL_TEST_PLATFORM_DECL)
+/**
+ * @brief Load test platform.
+ *
+ * This initialize each of the test platform nodes.
+ *
+ * @param test_id Test platform enumeration ID.
+ *
+ * @return 0 indicating success.
+ * @return -EINVAL `test_id` parameter is invalid.
+ * @return -EBUSY `test_id` One test platform is currently loaded.
+ */
+int power_signal_emul_load(
+	const struct power_signal_emul_test_platform *test_platform);
+
+/**
+ * @brief Unload test platform.
+ *
+ * @return 0 indicating success.
+ * @return -EINVAL no test platform has been loaded.
+ */
+int power_signal_emul_unload(void);
+
+#endif /* EMUL_POWER_SIGNALS_H_ */
