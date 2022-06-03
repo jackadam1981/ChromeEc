437e5958ab0484122e8cf2e2cd86ce4372deb948
Aaron Massey <aaronmassey@google.com>
2022-06-03 17:15:22 -0600

    WIP displayport test
    Motivation
    
    Action
    
    BRANCH=none
    BUG=b:1337
    TEST=CQ
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I933ad770239cae1d2b8de716504347a8701c6667

--- zephyr/test/drivers/src/integration/usbc/usb_alt_mode.c
+++ zephyr/test/drivers/src/integration/usbc/usb_alt_mode.c
@@ -1,3 +1,7 @@
+
+
+#pragma clang optimize off
+
 /* Copyright 2022 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
@@ -80,12 +84,17 @@ static void *usbc_alt_mode_setup(void)
 	partner->identity_vdm[VDO_INDEX_IDH] = VDO_IDH(
 		/* USB host */ false, /* USB device */ false, IDH_PTYPE_AMA,
 		/* modal operation */ true, USB_VID_GOOGLE);
-	partner->identity_vdm[VDO_INDEX_CSTAT] = 0xabcdabcd;
-	partner->identity_vdm[VDO_INDEX_PRODUCT] = VDO_PRODUCT(0x1234, 0x5678);
+	partner->identity_vdm[VDO_INDEX_CSTAT] = 0xabcdabcd; // dummy data
+	partner->identity_vdm[VDO_INDEX_PRODUCT] = VDO_PRODUCT(0x1234, 0x5678); // dummy data
+
 	/* Hardware version 1, firmware version 2 */
 	partner->identity_vdm[VDO_INDEX_AMA] = 0x12000000;
 	partner->identity_vdos = VDO_INDEX_AMA + 1;
-
+	/*
+	 * TODO(b/aaronmassey): Make the array index a lot simpler. This is
+	 * harder than it needs to be.
+	 */
+	/* headers are vdos */
 	/* Support DisplayPort VID. */
 	partner->svids_vdm[VDO_INDEX_HDR] =
 		VDO(USB_SID_PD, /* structured VDM */ true,
@@ -101,6 +110,8 @@ static void *usbc_alt_mode_setup(void)
 	partner->modes_vdm[VDO_INDEX_HDR + 1] = VDO_MODE_DP(
 		0, MODE_DP_PIN_C, 1, CABLE_PLUG, MODE_DP_V13, MODE_DP_SNK);
 	partner->modes_vdos = VDO_INDEX_HDR + 2;
+
+	/* These are all supporting sub parts of the discovery command */
 
 	/* Sink 5V 3A. */
 	snk_ext->pdo[1] = PDO_FIXED(5000, 3000, PDO_FIXED_UNCONSTRAINED);
@@ -154,6 +165,31 @@ ZTEST_F(usbc_alt_mode, verify_discovery)
 	zassert_equal(discovery->svids[0].mode_vdo[0],
 		      this->partner.modes_vdm[1],
 		      "DP mode VDOs did not match");
+}
+
+ZTEST_USER(espi, test_host_command_usb_pd_get_amode)
+{
+	/* Verify host command when VDOs are present. */
+	struct ec_params_usb_pd_get_mode_request params = {
+		.port = TEST_PORT,
+		.svid_idx = 0,
+	};
+	struct ec_params_usb_pd_get_mode_response response;
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND(
+		EC_CMD_USB_PD_GET_AMODE, 0, response, params);
+
+	zassert_ok(host_command_process(&args), NULL);
+	zassert_ok(args.result, NULL);
+
+	zassert_equal(args.response_size, sizeof(response), NULL);
+}
+
+ZTEST_F(usbc_alt_mode, verify_dp_mode_entry)
+{
+	struct ec_params_usb_pd_get_mode_response response;
+	host_cmd_usb_pd_get_amode(TEST_PORT, 0, &response);
+
+	zassert_equal(0, response.svid, NULL);
 }
 
 ZTEST_SUITE(usbc_alt_mode, drivers_predicate_post_main, usbc_alt_mode_setup,
