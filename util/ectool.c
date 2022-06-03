970a8076819f7ad951824f2b334a55a6b9c96e17
Daisuke Nojiri <dnojiri@chromium.org>
2022-06-03 19:01:51 +0000

    RGBKBD: Set scale per key
    Currently, rgbkbd_set_scale can only set a scale of all keys. This patch
    makes it set a scale of an individual key. rgbkbd_reset_scale will
    replace the previous rgbkbd_set_scale.
    
    BUG=b:233986174
    BRANCH=None
    TEST=Run ectool rgbkbd scale on Vell.
    TEST=Run ectool rgbkbd scale on Taniks.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I34e6b3e41c40510eca148fcf879e8caaf57317be

--- util/ectool.c
+++ util/ectool.c
@@ -1300,10 +1300,13 @@ static void cmd_rgbkbd_help(char *cmd)
 	"  Usage2: %s clear <RGB>\n"
 	"          Set the color of all keys to <RGB>.\n"
 	"\n"
-	"  Usage2: %s demo <num>\n"
+	"  Usage3: %s demo <num>\n"
 	"          Run demo-<num>. 0: Off, 1: Flow, 2: Dot.\n"
+	"\n"
+	"  Usage4: %s scale <key> <val>\n"
+	"          Set the scale parameter of key_<key> to <val>.\n"
 	"\n",
-	cmd, cmd, cmd);
+	cmd, cmd, cmd, cmd);
 }
 
 static int cmd_rgbkbd_parse_rgb_text(const char *text, struct rgb_s *color)
@@ -1388,12 +1391,31 @@ static int cmd_rgbkbd(int argc, char *argv[])
 		struct ec_params_rgbkbd p;
 
 		val = strtol(argv[2], &e, 0);
-		if ((e && *e) || val >= EC_RGBKBD_SUBCMD_COUNT) {
+		if ((e && *e) || val >= EC_RGBKBD_DEMO_COUNT) {
 			fprintf(stderr, "Invalid demo id: %s\n", argv[2]);
 			return -1;
 		}
 		p.subcmd = EC_RGBKBD_SUBCMD_DEMO;
 		p.demo = val;
+		rv = ec_command(EC_CMD_RGBKBD, 0, &p, sizeof(p), NULL, 0);
+	} else if (argc == 4 && !strcasecmp(argv[1], "scale")) {
+		/* Usage 4 */
+		struct ec_params_rgbkbd p;
+
+		val = strtol(argv[2], &e, 0);
+		if ((e && *e) || val > EC_RGBKBD_MAX_KEY_COUNT) {
+			fprintf(stderr, "Invalid key number: %s\n", argv[2]);
+			return -1;
+		}
+		p.set_scale.key = val;
+
+		val = strtol(argv[3], &e, 0);
+		if ((e && *e) || val > EC_RGBKBD_MAX_SCALE) {
+			fprintf(stderr, "Invalid scale value: %s\n", argv[3]);
+			return -1;
+		}
+		p.set_scale.scale = val;
+		p.subcmd = EC_RGBKBD_SUBCMD_SET_SCALE;
 		rv = ec_command(EC_CMD_RGBKBD, 0, &p, sizeof(p), NULL, 0);
 	} else {
 		/* Usage 1 */
