81ea44c63a333bcd844ec16dfa80d95cb8e2337e
Hank Xie <hank.xie@quanta.corp-partner.google.com>
2022-06-06 08:26:30 -0400

    shotzo: Remove cbi_fw_config setting
    Remove cbi_fw_config setting copied from drawcia.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Signed-off-by: Hank Xie <hank.xie@quanta.corp-partner.google.com>
    Change-Id: I17d631f1a61d1152d18560641370d156ba8662f6

--- board/shotzo/board.c
+++ board/shotzo/board.c
@@ -137,33 +137,6 @@ static void usb_c1_interrupt(enum gpio_signal s)
 	hook_call_deferred(&check_c1_line_data, INT_RECHECK_US);
 }
 
-static void board_enable_hdmi_hpd(int enable)
-{
-	enum fw_config_db db = get_cbi_fw_config_db();
-	int hdmi_hpd = gpio_get_level(GPIO_VOLUP_BTN_ODL_HDMI_HPD);
-
-	if (db == DB_1A_HDMI || db == DB_LTE_HDMI || db == DB_1A_HDMI_LTE) {
-		/* Check if we can report HDMI_HPD signal to CPU  */
-		if (enable)
-			gpio_set_level(GPIO_EC_AP_USB_C1_HDMI_HPD, hdmi_hpd);
-		else
-			gpio_set_level(GPIO_EC_AP_USB_C1_HDMI_HPD, 0);
-	}
-}
-
-static void button_sub_hdmi_hpd_interrupt(enum gpio_signal s)
-{
-	enum fw_config_db db = get_cbi_fw_config_db();
-	int hdmi_hpd = gpio_get_level(GPIO_VOLUP_BTN_ODL_HDMI_HPD);
-
-	if (db == DB_1A_HDMI || db == DB_LTE_HDMI || db == DB_1A_HDMI_LTE) {
-		/* Do not report HDMI_HPD signal to CPU when system off. */
-		if (!chipset_in_state(CHIPSET_STATE_ANY_OFF))
-			gpio_set_level(GPIO_EC_AP_USB_C1_HDMI_HPD, hdmi_hpd);
-	} else
-		button_interrupt(s);
-}
-
 static void c0_ccsbu_ovp_interrupt(enum gpio_signal s)
 {
 	cprints(CC_USBPD, "C0: CC OVP, SBU OVP, or thermal event");
@@ -413,24 +386,9 @@ struct motion_sensor_t bma422_lid_accel = {
 	},
 };
 
-static void board_update_motion_sensor_config(void)
-{
-	if (get_cbi_ssfc_lid_sensor() == SSFC_SENSOR_BMA422)
-		motion_sensors[LID_ACCEL] = bma422_lid_accel;
-}
-
 void board_init(void)
 {
 	int on;
-	enum fw_config_db db = get_cbi_fw_config_db();
-
-	if (db == DB_1A_HDMI || db == DB_LTE_HDMI || db == DB_1A_HDMI_LTE) {
-		/* Select HDMI option */
-		gpio_set_level(GPIO_HDMI_SEL_L, 0);
-	} else {
-		/* Select AUX option */
-		gpio_set_level(GPIO_HDMI_SEL_L, 1);
-	}
 
 	gpio_enable_interrupt(GPIO_USB_C0_INT_ODL);
 	gpio_enable_interrupt(GPIO_USB_C1_INT_ODL);
@@ -449,37 +407,16 @@ void board_init(void)
 
 	gpio_enable_interrupt(GPIO_USB_C0_CCSBU_OVP_ODL);
 
-	if (get_cbi_fw_config_tablet_mode() == TABLET_MODE_PRESENT) {
-		motion_sensor_count = ARRAY_SIZE(motion_sensors);
-		/* Enable Base Accel interrupt */
-		gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
-
-		board_update_motion_sensor_config();
-	} else {
-		motion_sensor_count = 0;
-		gmr_tablet_switch_disable();
-		/* Base accel is not stuffed, don't allow line to float */
-		gpio_set_flags(GPIO_BASE_SIXAXIS_INT_L,
-			       GPIO_INPUT | GPIO_PULL_DOWN);
-	}
-
 	gpio_enable_interrupt(GPIO_PEN_DET_ODL);
 
 	/* Make sure pen detection is triggered or not at sysjump */
 	if (!gpio_get_level(GPIO_PEN_DET_ODL))
 		gpio_set_level(GPIO_EN_PP5000_PEN, 1);
 
-	/* Make sure HDMI_HPD signal can be reported to CPU at sysjump */
-	board_enable_hdmi_hpd(1);
-
 	/* Charger on the MB will be outputting PROCHOT_ODL and OD CHG_DET */
 	sm5803_configure_gpio0(CHARGER_PRIMARY, GPIO0_MODE_PROCHOT, 1);
 	sm5803_configure_chg_det_od(CHARGER_PRIMARY, 1);
 
-	if (board_get_charger_chip_count() > 1) {
-		/* Charger on the sub-board will be a push-pull GPIO */
-		sm5803_configure_gpio0(CHARGER_SECONDARY, GPIO0_MODE_OUTPUT, 0);
-	}
 
 	/* Turn on 5V if the system is on, otherwise turn it off */
 	on = chipset_in_state(CHIPSET_STATE_ON | CHIPSET_STATE_ANY_SUSPEND |
@@ -491,26 +428,17 @@ DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 static void board_resume(void)
 {
 	sm5803_disable_low_power_mode(CHARGER_PRIMARY);
-	if (board_get_charger_chip_count() > 1)
-		sm5803_disable_low_power_mode(CHARGER_SECONDARY);
-
-	/* Enable reporting HDMI_HPD to CPU when system resume */
-	board_enable_hdmi_hpd(1);
 }
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_resume, HOOK_PRIO_DEFAULT);
 
 static void board_suspend(void)
 {
 	sm5803_enable_low_power_mode(CHARGER_PRIMARY);
-	if (board_get_charger_chip_count() > 1)
-		sm5803_enable_low_power_mode(CHARGER_SECONDARY);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_suspend, HOOK_PRIO_DEFAULT);
 
 static void board_shutdown(void)
 {
-	/* Disable reporting HDMI_HPD to CPU at shutdown */
-	board_enable_hdmi_hpd(0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_shutdown, HOOK_PRIO_DEFAULT);
 
@@ -521,8 +449,6 @@ void board_hibernate(void)
 	 * z-state.
 	 */
 	sm5803_hibernate(CHARGER_PRIMARY);
-	if (board_get_charger_chip_count() > 1)
-		sm5803_hibernate(CHARGER_SECONDARY);
 }
 
 __override void board_ocpc_init(struct ocpc_data *ocpc)
@@ -571,32 +497,12 @@ __override void board_power_5v_enable(int enable)
 
 __override uint8_t board_get_usb_pd_port_count(void)
 {
-	enum fw_config_db db = get_cbi_fw_config_db();
-
-	if (db == DB_1A_HDMI || db == DB_NONE || db == DB_LTE_HDMI
-			|| db == DB_1A_HDMI_LTE)
-		return CONFIG_USB_PD_PORT_MAX_COUNT - 1;
-	else if (db == DB_1C || db == DB_1C_LTE || db == DB_1C_1A
-			|| db == DB_1C_1A_LTE)
-		return CONFIG_USB_PD_PORT_MAX_COUNT;
-
-	ccprints("Unhandled DB configuration: %d", db);
-	return 0;
+	return CONFIG_USB_PD_PORT_MAX_COUNT;
 }
 
 __override uint8_t board_get_charger_chip_count(void)
 {
-	enum fw_config_db db = get_cbi_fw_config_db();
-
-	if (db == DB_1A_HDMI || db == DB_NONE || db == DB_LTE_HDMI
-			|| db == DB_1A_HDMI_LTE)
-		return CHARGER_NUM - 1;
-	else if (db == DB_1C || db == DB_1C_LTE || db == DB_1C_1A
-			|| db == DB_1C_1A_LTE)
-		return CHARGER_NUM;
-
-	ccprints("Unhandled DB configuration: %d", db);
-	return 0;
+	return CHARGER_NUM;
 }
 
 uint16_t tcpc_get_alert_status(void)
