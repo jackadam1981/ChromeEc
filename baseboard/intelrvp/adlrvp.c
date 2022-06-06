2a13a8a5b834a11e827df3b4a9ebb5061c8692c1
poornima tom <poornima.tom@intel.com>
2022-06-06 23:55:57 +0530

    adlrvp[rpl]: Disable dual role toggle in S0
    The FUSB TCPC chip does not support dual role auto toggle by default.
    Hence, the PD state of a non-attached port remains in PD_DRP_TOGGLE_ON
    in active state. Thus the TCPC ports do not enter Low power mode. To
    fix the issue, added board specific code to disable the dual role toggle
    in S0.
    Note: Originally CL:3088161 was merged to fix this issue. But, it
    was reverted due to b:197298349(CL:3109275).
    
    BUG=b:195406641
    BRANCH=none
    TEST=Tested on ADL RVP on ITE & FUSB based TCPC ports
    
    In NDA case and exit of LPM when a device is attached:
    >pd 0 state
    Port C0 CC1, Disable - Role: SNK-UFP TC State: LowPowerMode,
    Flags: 0x0010 PE State: , Flags: 0x8400000
    
    >pd 1 state
    Port C1 CC1, Disable - Role: SNK-UFP TC State: LowPowerMode,
    Flags: 0x0010 PE State: , Flags: 0x8400000
    
    Any attachment of any TypeC based device exits the port
    from low power mode.
    Example of attaching TBT:
    C0: Exit Low Power Mode]
    --
    >pd 0 state
    Port C0 CC2, Enable - Role: SNK-DFP-VC TC State: Attached.SNK,
    Flags: 0x9001 PE State: PE_SNK_Ready, Flags: 0x8211
    >typec 0
    Port 0: USB=0 DP=0 POLARITY=INVERTED HPD_IRQ=0 HPD_LVL=0 SAFE=0
    TBT=1 USB4=0
    
    Signed-off-by: poornima tom <poornima.tom@intel.com>
    Change-Id: Ia47a0b252fd3ce9da27832dbcba1298897c6406f

--- baseboard/intelrvp/adlrvp.c
+++ baseboard/intelrvp/adlrvp.c
@@ -540,3 +540,15 @@ __override void board_pre_task_i2c_peripheral_init(void)
 	/* Configure board specific retimer & mux */
 	configure_retimer_usbmux();
 }
+
+/*
+ * The FUSB TCPC chip does not support dual role auto toggle by
+ * default. Hence, the PD state of a non-attached port remains in
+ * PD_DRP_TOGGLE_ON in active state. Thus the TCPC ports do not
+ * enter Low power mode. To fix the issue, added board specific
+ * code to disable the dual role toggle in S0.
+ **/
+__override enum pd_dual_role_states pd_get_drp_state_in_s0(void)
+{
+	return PD_DRP_TOGGLE_OFF;
+}
