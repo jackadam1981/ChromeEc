68c7ad09d4a975166e88f5ef7eb8e14083bf7381
arthur.lin <arthur.lin@lcfc.corp-partner.google.com>
2022-06-06 11:34:40 +0800

    taniks: remove unused define and code for clamshell system
    Remove unused define and code about tablet mode, because taniks is clamshell only.
    
    BUG=b:231930177
    BRANCH=none
    TEST=make buildall -j
    TEST=on taniks, run test_that -b brya "IP" power_WakeSources can get
    pass result.
    
    Signed-off-by: arthur.lin <arthur.lin@lcfc.corp-partner.google.com>
    Change-Id: Iaed80bb621993eb3b054cfce6e054630439becb8

--- board/taniks/board.h
+++ board/taniks/board.h
@@ -25,6 +25,9 @@
 
 #undef CONFIG_CMD_POWERINDEBUG
 #undef CONFIG_MKBP_INPUT_DEVICES
+#undef CONFIG_TABLET_MODE
+#undef CONFIG_GMR_TABLET_MODE
+#undef CONFIG_TABLET_MODE_SWITCH
 
 /*
  * This will happen automatically on NPCX9 ES2 and later. Do not remove
