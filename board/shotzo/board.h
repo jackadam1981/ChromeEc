69a936399635c0cb88dad4db97a1c5485ffeaa86
Hank Xie <hank.xie@quanta.corp-partner.google.com>
2022-06-05 23:10:51 -0400

    shotzo: Configure barrel jack adapter
    There is a barrel jack adapter on shotzo
    
    BUG=none
    BRANCH=none
    TEST=insert barrel jack and check shotzo works fine.
    
    Signed-off-by: Hank Xie <hank.xie@quanta.corp-partner.google.com>
    Change-Id: I8a84b852215996de8d805c8f6a7029ad770c1c90

--- board/shotzo/board.h
+++ board/shotzo/board.h
@@ -89,6 +89,11 @@
 #define USB_PORT_COUNT 1
 #define CONFIG_USB_PORT_POWER_DUMB
 
+/* Dedicated barreljack charger port */
+#undef  CONFIG_DEDICATED_CHARGE_PORT_COUNT
+#define CONFIG_DEDICATED_CHARGE_PORT_COUNT 1
+#define DEDICATED_CHARGE_PORT 2
+
 #ifndef __ASSEMBLER__
 
 #include "gpio_signal.h"
@@ -146,6 +151,15 @@ enum battery_type {
 	BATTERY_ATL,
 	BATTERY_TYPE_COUNT,
 };
+
+enum charge_port {
+	CHARGE_PORT_TYPEC0,
+	CHARGE_PORT_TYPEC1,
+	CHARGE_PORT_BARRELJACK,
+};
+
+/* Pin renaming */
+#define GPIO_AC_PRESENT		GPIO_BJ_ADP_PRESENT_L
 
 #endif /* !__ASSEMBLER__ */
 
