6d62cdcf2743dfc196c5c289acd3d9cc4a6a36f9
Tom Hughes <tomhughes@chromium.org>
2022-06-07 09:05:12 -0700

    fpsensor: Test std::optional and std::array
    BRANCH=none
    BUG=b:234181908
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I7266a903b6ee167a8683377003b005b4b1c53ffd

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -4,6 +4,8 @@
  */
 
 #include "compile_time_macros.h"
+#include <array>
+#include <optional>
 
 extern "C" {
 #include "atomic.h"
@@ -238,6 +240,18 @@ static void fp_process_finger(void)
 extern "C" void fp_task(void)
 {
 	int timeout_us = -1;
+
+	std::array<int, 3> a = { 1, 2, 3 };
+	CPRINTS("std::array a[0]: %d", a[0]);
+	a[0] = 5;
+	CPRINTS("std::array a[0]: %d", a[0]);
+
+	std::optional<int> foo;
+
+	if (!foo)
+		CPRINTS("foo has no value");
+	foo = 10;
+	CPRINTS("foo: %d", *foo);
 
 	CPRINTS("FP_SENSOR_SEL: %s",
 		fp_sensor_type_to_str(get_fp_sensor_type()));
