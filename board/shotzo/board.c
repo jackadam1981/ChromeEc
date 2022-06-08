d6881557b71352065a3ecb7cf9db7f76307a1324
Hank Xie <hank.xie@quanta.corp-partner.google.com>
2022-06-08 03:44:34 -0400

    shotzo: Configure LCD backlight driver OZ554
    Porting LCD backlight driver OZ554 for shotzo.
    
    BUG=none
    BRANCH=none
    TEST=check LCD panel works fine on shotzo
    
    Signed-off-by: Hank Xie <hank.xie@quanta.corp-partner.google.com>
    Change-Id: I9307acbdc0987325792f3ec518634481e7dda900

--- board/shotzo/board.c
+++ board/shotzo/board.c
@@ -15,6 +15,7 @@
 #include "cros_board_info.h"
 #include "driver/bc12/pi3usb9201.h"
 #include "driver/charger/sm5803.h"
+#include "driver/led/oz554.h"
 #include "driver/temp_sensor/thermistor.h"
 #include "driver/tcpm/it83xx_pd.h"
 #include "driver/tcpm/ps8xxx.h"
@@ -319,6 +320,34 @@ const struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	},
 };
 
+void oz554_board_init(void)
+{
+	int panel_id = 0;
+
+	panel_id |= gpio_get_level(GPIO_PANEL_ID0) << 0;
+	panel_id |= gpio_get_level(GPIO_PANEL_ID1) << 1;
+	panel_id |= gpio_get_level(GPIO_PANEL_ID2) << 2;
+	panel_id |= gpio_get_level(GPIO_PANEL_ID3) << 3;
+
+	switch (panel_id) {
+	case 0x00:
+		CPRINTUSB("PANEL_HAN01.10A");
+		oz554_set_config(0, 0xF3);
+		oz554_set_config(2, 0x4C);
+		oz554_set_config(5, 0xB7);
+		break;
+	case 0x02:
+		CPRINTUSB("PANEL_WF9_SSA2");
+		oz554_set_config(0, 0xF3);
+		oz554_set_config(2, 0x55);
+		oz554_set_config(5, 0x87);
+		break;
+	default:
+		CPRINTUSB("PANEL UNKNOWN");
+		break;
+	}
+}
+
 void board_init(void)
 {
 	int on;
@@ -341,6 +370,9 @@ void board_init(void)
 		hook_call_deferred(&check_c1_line_data, 0);
 
 	gpio_enable_interrupt(GPIO_USB_C0_CCSBU_OVP_ODL);
+
+	oz554_board_init();
+	gpio_enable_interrupt(GPIO_PANEL_BACKLIGHT_EN);
 
 	/* Charger on the MB will be outputting PROCHOT_ODL and OD CHG_DET */
 	sm5803_configure_gpio0(CHARGER_PRIMARY, GPIO0_MODE_PROCHOT, 1);
