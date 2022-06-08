b378ee771bddfff48f1c2b1b570e0c7ce77b0255
Hank Xie <hank.xie@quanta.corp-partner.google.com>
2022-06-08 03:44:34 -0400

    shotzo: Configure LCD backlight driver OZ554
    Porting LCD backlight driver OZ554 for shotzo.
    Add __attribute__((weak)) to i2c_ports[] in dedede
    to override it in shotzo' board.c.
    Rename i2c channel b to edid.
    Rename i2c channel c to backlight.
    
    BUG=none
    BRANCH=none
    TEST=check LCD panel works fine on shotzo
    
    Signed-off-by: Hank Xie <hank.xie@quanta.corp-partner.google.com>
    Change-Id: I9307acbdc0987325792f3ec518634481e7dda900

--- baseboard/dedede/variant_ec_it8320.c
+++ baseboard/dedede/variant_ec_it8320.c
@@ -74,48 +74,38 @@ BUILD_ASSERT(ARRAY_SIZE(vcmp_list) <= CHIP_VCMP_COUNT);
 BUILD_ASSERT(ARRAY_SIZE(vcmp_list) == VCMP_COUNT);
 
 /* I2C Ports */
-const struct i2c_port_t i2c_ports[] = {
-	{
-		.name = "eeprom",
-		.port = I2C_PORT_EEPROM,
-		.kbps = 400,
-		.scl  = GPIO_EC_I2C_EEPROM_SCL,
-		.sda  = GPIO_EC_I2C_EEPROM_SDA
-	},
+__attribute__((weak)) const struct i2c_port_t i2c_ports[] = {
+	{ .name = "eeprom",
+	  .port = I2C_PORT_EEPROM,
+	  .kbps = 400,
+	  .scl = GPIO_EC_I2C_EEPROM_SCL,
+	  .sda = GPIO_EC_I2C_EEPROM_SDA },
 
-	{
-		.name = "battery",
-		.port = I2C_PORT_BATTERY,
-		.kbps = 100,
-		.scl  = GPIO_EC_I2C_BATTERY_SCL,
-		.sda  = GPIO_EC_I2C_BATTERY_SDA
-	},
+	{ .name = "battery",
+	  .port = I2C_PORT_BATTERY,
+	  .kbps = 100,
+	  .scl = GPIO_EC_I2C_BATTERY_SCL,
+	  .sda = GPIO_EC_I2C_BATTERY_SDA },
 #ifdef HAS_TASK_MOTIONSENSE
-	{
-		.name = "sensor",
-		.port = I2C_PORT_SENSOR,
-		.kbps = 400,
-		.scl  = GPIO_EC_I2C_SENSOR_SCL,
-		.sda  = GPIO_EC_I2C_SENSOR_SDA
-	},
+	{ .name = "sensor",
+	  .port = I2C_PORT_SENSOR,
+	  .kbps = 400,
+	  .scl = GPIO_EC_I2C_SENSOR_SCL,
+	  .sda = GPIO_EC_I2C_SENSOR_SDA },
 #endif
 
 #if CONFIG_USB_PD_PORT_MAX_COUNT > 1
-	{
-		.name = "sub_usbc1",
-		.port = I2C_PORT_SUB_USB_C1,
-		.kbps = 1000,
-		.scl  = GPIO_EC_I2C_SUB_USB_C1_SCL,
-		.sda  = GPIO_EC_I2C_SUB_USB_C1_SDA
-	},
+	{ .name = "sub_usbc1",
+	  .port = I2C_PORT_SUB_USB_C1,
+	  .kbps = 1000,
+	  .scl = GPIO_EC_I2C_SUB_USB_C1_SCL,
+	  .sda = GPIO_EC_I2C_SUB_USB_C1_SDA },
 #endif
 
-	{
-		.name = "usbc0",
-		.port = I2C_PORT_USB_C0,
-		.kbps = 1000,
-		.scl  = GPIO_EC_I2C_USB_C0_SCL,
-		.sda  = GPIO_EC_I2C_USB_C0_SDA
-	},
+	{ .name = "usbc0",
+	  .port = I2C_PORT_USB_C0,
+	  .kbps = 1000,
+	  .scl = GPIO_EC_I2C_USB_C0_SCL,
+	  .sda = GPIO_EC_I2C_USB_C0_SDA },
 };
 const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
