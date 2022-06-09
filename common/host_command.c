342a3d5045144889f4d1fb7264919da6b1dbeb81
Michał Barnaś <mb@semihalf.com>
2022-06-09 15:43:44 +0200

    hostcmd: prepare support for zephyr ec_host_cmd API
    This commit prepares the movement from shimmed CrOS EC host commands
    to upstreamed API in Zephyr.
    At the moment, the shim must still exist due to the initialization
    process which fills the header in shared memory with supported
    version and adds the HOST_CMD channel to console.
    
    BRANCH=main
    BUG=b:218332582
    TEST=build all boards for zephyr and cros ec
    
    Change-Id: I04049cf50f7558564801b0a7aff3aa871b4f2128
    Signed-off-by: Michał Barnaś <mb@semihalf.com>

--- common/host_command.c
+++ common/host_command.c
@@ -371,6 +371,7 @@ host_packet_bad:
  * @param command	Command number to find
  * @return The command structure, or NULL if no match found.
  */
+#ifndef CONFIG_EC_HOST_CMD
 static const struct host_command *find_host_command(int command)
 {
 	if (IS_ENABLED(CONFIG_ZEPHYR)) {
@@ -408,6 +409,7 @@ static const struct host_command *find_host_command(int command)
 		return NULL;
 	}
 }
+#endif
 
 static void host_command_init(void)
 {
@@ -429,6 +431,7 @@ static void host_command_init(void)
 
 void host_command_task(void *u)
 {
+#ifndef CONFIG_EC_HOST_CMD
 	timestamp_t t0, t1, t_recess;
 	t_recess.val = 0;
 	t1.val = 0;
@@ -460,6 +463,9 @@ void host_command_task(void *u)
 			/* Short recess */
 			usleep(CONFIG_HOSTCMD_RATE_LIMITING_RECESS);
 	}
+#else
+	host_command_init();
+#endif
 }
 
 /*****************************************************************************/
@@ -545,6 +551,7 @@ DECLARE_HOST_COMMAND(EC_CMD_READ_MEMMAP, host_command_read_memmap,
 		     EC_VER_MASK(0));
 #endif
 
+#ifndef CONFIG_EC_HOST_CMD
 static enum ec_status
 host_command_get_cmd_versions(struct host_cmd_handler_args *args)
 {
@@ -581,6 +588,7 @@ static int host_command_is_suppressed(uint16_t cmd)
 #endif
 	return 0;
 }
+#endif
 
 /*
  * Print & reset suppressed command counters. It should be called periodically
@@ -619,6 +627,7 @@ DECLARE_HOOK(HOOK_SYSJUMP, dump_host_command_suppressed_, HOOK_PRIO_DEFAULT);
 }
 #endif /* CONFIG_SUPPRESSED_HOST_COMMANDS */
 
+#ifndef CONFIG_EC_HOST_CMD
 /**
  * Print debug output for the host command request, before it's processed.
  *
@@ -730,6 +739,7 @@ uint16_t host_command_process(struct host_cmd_handler_args *args)
 
 	return rv;
 }
+#endif
 
 #ifdef CONFIG_HOST_COMMAND_STATUS
 /* Returns current command status (busy or not) */
