2c1766d3602a2e80c291b080ebbaabf067fbfff2
Hank Xie <hank.xie@quanta.corp-partner.google.com>
2022-06-13 02:56:02 -0400

    shotzo: Porting recovery botton
    Shotzo doesn't have keyboard,
    so there is physical recovery button on shotzo.
    
    BUG=none
    BRANCH=none
    TEST=check recovery button works fine on shotzo
    
    Signed-off-by: Hank Xie <hank.xie@quanta.corp-partner.google.com>
    Change-Id: Ib65e20f57bcb03f93647200e37e0c0919c548667

--- board/shotzo/board.h
+++ board/shotzo/board.h
@@ -60,6 +60,10 @@
  */
 #define CONFIG_LED_DRIVER_OZ554
 
+/* EC Defines */
+#define CONFIG_DEDICATED_RECOVERY_BUTTON
+#define CONFIG_EMULATED_SYSRQ
+
 /* Unused Features */
 #define CONFIG_POWER_BUTTON_IGNORE_LID
 #undef CONFIG_BACKLIGHT_LID
@@ -131,6 +135,7 @@ enum charge_port {
 
 /* Pin renaming */
 #define GPIO_AC_PRESENT		GPIO_BJ_ADP_PRESENT_L
+#define GPIO_RECOVERY_L         GPIO_EC_RECOVERY_BTN_ODL
 
 #endif /* !__ASSEMBLER__ */
 
