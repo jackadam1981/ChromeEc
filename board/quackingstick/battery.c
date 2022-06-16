d5d027bbbc1d490f571798b0ecfe6af0608c2f12
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2022-06-16 14:48:36 +0800

    Quackingstick: modify battery D-FET status reading command
    Using ManufacturerAccess 0x0054 OperationStatus bit1 as D-FET status.
    
    BUG=b:231911921
    BRANCH=trogdor
    TEST=check battery status is normal after resume from cut off.
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: I9c3bfdf88389623567c3d0d7c15b75dc05c0084a

--- board/quackingstick/battery.c
+++ board/quackingstick/battery.c
@@ -43,8 +43,9 @@ const struct board_batt_params board_battery_info[] = {
 				.reg_data = { 0xC574, 0xC574 },
 			},
 			.fet = {
+				.mfgacc_support = 1,
 				.reg_addr = 0x0,
-				.reg_mask = 0x8000,
+				.reg_mask = 0x0002,
 				.disconnect_val = 0x0,
 			},
 		},
@@ -72,8 +73,9 @@ const struct board_batt_params board_battery_info[] = {
 				.reg_data = { 0xC574, 0xC574 },
 			},
 			.fet = {
+				.mfgacc_support = 1,
 				.reg_addr = 0x0,
-				.reg_mask = 0x8000,
+				.reg_mask = 0x0002,
 				.disconnect_val = 0x0,
 			}
 		},
