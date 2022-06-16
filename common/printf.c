c1c91069f8d836f5b0f656a76707386a771c55ff
Bobby Casey <bobbycasey@google.com>
2022-06-16 17:32:06 -0400

    common: Conditionally support printf %l modifier
    The libfp library prints some values with PRIx32 or PRId32 format
    specifiers which, in their compilation environment, output "%lx" and
    "%ld".
    
    Unfortunately, support for printing longs in EC code was deprecated in
    crbug.com/984041 after changing it from it being treated as a hard-coded
    64-bit length. There was concern that long running branches without an
    updated printf may attempt to use %l for 64-bit values rather than the
    expected 32-bit values.
    
    Printing 32-bit values with "%l" is safe as long as we can guarantee no
    legacy code will attempt to print using "%l" with a 64-bit value. The
    logic here is protected by a config flag that is only enabled for FPMCU
    and FPMCU doesn't use long running release branches.
    
    BRANCH=none
    BUG=b:234781655
    BUG=b:234143158
    TEST=./test/run_device_tests.py -b dartmonkey -t printf
    TEST=./test/run_device_tests.py -b bloonchipper -t printf
    
    Signed-off-by: Bobby Casey <bobbycasey@google.com>
    Change-Id: If432f507a31cc12a4c5c4bdcd07c6141407bd70d

--- common/printf.c
+++ common/printf.c
@@ -225,7 +225,7 @@ int vfnprintf(int (*addchar)(void *context, int c), void *context,
 
 			/*
 			 * Handle length:
-			 * %l - DEPRECATED (see below)
+			 * %l - requires CONFIG_USE_32BIT_LONG, see below
 			 * %ll - long long
 			 * %z - size_t
 			 */
@@ -240,6 +240,9 @@ int vfnprintf(int (*addchar)(void *context, int c), void *context,
 				}
 
 				/*
+				 * The CONFIG_USE_32BIT_LONG flag is required to
+				 * enable the %l flag on systems where it
+				 * would signify a 32-bit value. Otherwise,
 				 * %l on 32-bit systems is deliberately
 				 * deprecated. It was originally used as
 				 * shorthand for 64-bit values. When
@@ -251,7 +254,8 @@ int vfnprintf(int (*addchar)(void *context, int c), void *context,
 				 * cherry-picked into an old firmware branch.
 				 * See crbug.com/984041 for more context.
 				 */
-				if (!(flags & PF_64BIT)) {
+				if (!IS_ENABLED(CONFIG_USE_32BIT_LONG) &&
+				    !(flags & PF_64BIT)) {
 					format = error_str;
 					continue;
 				}
