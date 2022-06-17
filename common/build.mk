85a306e75ec8a718c2fb9c0f0233627c0e2327b9
Tom Hughes <tomhughes@chromium.org>
2022-06-17 13:44:44 -0700

    WIP: Compile with the C standard library
    BRANCH=none
    BUG=none
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I63285569205b9c560655dfead6a260c270a92494

--- common/build.mk
+++ common/build.mk
@@ -9,7 +9,10 @@
 # Note that this variable includes the trailing "/"
 _common_dir:=$(dir $(lastword $(MAKEFILE_LIST)))
 
-common-y=util.o util_stdlib.o
+common-y=util.o
+ifeq ($(USE_STDLIB), 0)
+common-y+=util_stdlib.o
+endif
 common-y+=version.o printf.o queue.o queue_policies.o irq_locking.o
 
 common-$(CONFIG_ACCELGYRO_BMI160)+=math_util.o
