851fd937b267746600aaac5805639c007fc580dc
Tom Hughes <tomhughes@chromium.org>
2022-06-17 15:47:18 -0700

    tree: Move printf declarations to stdio.h
    The "builtin" directory is EC's copy of the C standard library headers.
    Move the declarations for printf functions that are provided by the
    standard library to the "builtin" directory. This change makes it easier
    for future changes to optionally build with the C standard library
    instead of the standalone EC subset.
    
    BRANCH=none
    BUG=b:172020503, b:234181908
    TEST=make buildall
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I4a5b4f8b98b972e86c4cca65d4910b5aa07ec524

--- test/printf.c
+++ test/printf.c
@@ -12,6 +12,8 @@
 #include "test_util.h"
 #include "util.h"
 
+#include <builtin/stdio.h>
+
 #define INIT_VALUE 0x5E
 #define NO_BYTES_TOUCHED NULL
 
@@ -33,7 +35,8 @@ int run(int expect_ret, const char *expect, bool output_null, size_t size_limit,
 	TEST_ASSERT(expect_size <= size_limit);
 	memset(output, INIT_VALUE, sizeof(output));
 
-	rv = vsnprintf(output_null ? NULL : output, size_limit, format, args);
+	rv = crec_vsnprintf(output_null ? NULL : output, size_limit, format,
+			    args);
 	ccprintf("received='%.*s'   | ret          =%d\n", 30, output, rv);
 
 	TEST_ASSERT_ARRAY_EQ(output, expect, expect_size);
