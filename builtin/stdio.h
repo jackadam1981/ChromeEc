025ec62250edfcbed9d32e8d705f0e90bede921d
Tom Hughes <tomhughes@chromium.org>
2022-06-17 15:47:18 -0700

    tree: Move printf declarations to stdio.h
    The "builtin" directory is EC's copy of the C standard library headers.
    Move the declarations for printf functions that are provided by the
    standard library to the "builtin" directory. This change makes it easier
    for future changes to optionally build with the C standard library
    instead of the standalone EC subset.
    
    BRANCH=none
    BUG=b:172020503, b:234181908
    TEST=make buildall
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I4a5b4f8b98b972e86c4cca65d4910b5aa07ec524

---
+++ builtin/stdio.h
@@ -0,0 +1,48 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_STDIO_H__
+#define __CROS_EC_STDIO_H__
+
+#include <stddef.h>
+#include <stdarg.h>
+
+#if defined(CONFIG_ZEPHYR) || defined(TEST_BUILD)
+#define EC_SNPRINTF crec_snprintf
+#define EC_VSNPRINTF crec_vsnprintf
+#else
+#define EC_SNPRINTF snprintf
+#define EC_VSNPRINTF vsnprintf
+#endif
+
+/**
+ * Print formatted output to a string.
+ *
+ * Guarantees null-termination if size!=0.
+ *
+ * @param str		Destination string
+ * @param size		Size of destination in bytes
+ * @param format	Format string
+ * @return EC_SUCCESS, or EC_ERROR_OVERFLOW if the output was truncated.
+ */
+__attribute__((__format__(__printf__, 3, 4)))
+__attribute__((warn_unused_result)) int
+EC_SNPRINTF(char *str, size_t size, const char *format, ...);
+
+/**
+ * Print formatted output to a string.
+ *
+ * Guarantees null-termination if size!=0.
+ *
+ * @param str		Destination string
+ * @param size		Size of destination in bytes
+ * @param format	Format string
+ * @param args		Parameters
+ * @return The string length written to str, or a negative value on error.
+ *         The negative values can be -EC_ERROR_INVAL or -EC_ERROR_OVERFLOW.
+ */
+int EC_VSNPRINTF(char *str, size_t size, const char *format, va_list args);
+
+#endif /* __CROS_EC_STDIO_H__ */
