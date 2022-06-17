1c8afad9eb7e44d2c424e0e55e475052c6ef8bb4
Daisuke Nojiri <dnojiri@chromium.org>
2022-06-17 21:50:49 +0000

    gpio: Wrap gpio_get_default_flags by gpio_get_flags
    Currently, gpio_get_flags returns the runtime GPIO flags while
    gpio_get_default_flags return the compile time flags. This patch makes
    gpio_get_flags call gpio_get_default_flags if CONFIG_GPIO_GET_EXTENDED
    isn't defined so that callers will get the runtime flags by default and
    fall back to the compile time flags only if runtime flags aren't
    available.
    
    This patch itself introduces no behavior change (since currently
    gpio_get_flags is defined only if CONFIG_GPIO_GET_EXTENDED is defined).
    Callers should individually switch to gpio_get_flags if it's calling
    gpio_get_default_flags by mistake.
    
    BUG=None
    BRANCH=None
    TEST=buildall
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I714bf8ea135d3c91a8cefcc3a1ba577f5650200d

--- common/gpio.c
+++ common/gpio.c
@@ -109,14 +109,16 @@ void gpio_set_flags(enum gpio_signal signal, int flags)
 	gpio_set_flags_by_mask(g->port, g->mask, flags);
 }
 
-#ifdef CONFIG_GPIO_GET_EXTENDED
 int gpio_get_flags(enum gpio_signal signal)
 {
+#ifdef CONFIG_GPIO_GET_EXTENDED
 	const struct gpio_info *g = gpio_list + signal;
 
 	return gpio_get_flags_by_mask(g->port, g->mask);
-}
+#else
+	return gpio_get_default_flags(signal);
 #endif
+}
 
 int gpio_get_default_flags(enum gpio_signal signal)
 {
