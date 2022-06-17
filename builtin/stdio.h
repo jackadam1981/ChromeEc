3793304d069086fd088b945a65f7f503efcd64f2
Tom Hughes <tomhughes@chromium.org>
2022-06-17 15:47:18 -0700

    WIP: Move printf declarations to stdio.h
    BRANCH=none
    BUG=none
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I4a5b4f8b98b972e86c4cca65d4910b5aa07ec524

---
+++ builtin/stdio.h
@@ -0,0 +1,39 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_STDIO_H__
+#define __CROS_EC_STDIO_H__
+
+#include <stddef.h>
+#include <stdarg.h>
+
+/**
+ * Print formatted output to a string.
+ *
+ * Guarantees null-termination if size!=0.
+ *
+ * @param str		Destination string
+ * @param size		Size of destination in bytes
+ * @param format	Format string
+ * @return EC_SUCCESS, or EC_ERROR_OVERFLOW if the output was truncated.
+ */
+__attribute__((__format__(__printf__, 3, 4))) int
+snprintf(char *str, size_t size, const char *format, ...);
+
+/**
+ * Print formatted output to a string.
+ *
+ * Guarantees null-termination if size!=0.
+ *
+ * @param str		Destination string
+ * @param size		Size of destination in bytes
+ * @param format	Format string
+ * @param args		Parameters
+ * @return The string length written to str, or a negative value on error.
+ *         The negative values can be -EC_ERROR_INVAL or -EC_ERROR_OVERFLOW.
+ */
+int vsnprintf(char *str, size_t size, const char *format, va_list args);
+
+#endif /* __CROS_EC_STDIO_H__ */
