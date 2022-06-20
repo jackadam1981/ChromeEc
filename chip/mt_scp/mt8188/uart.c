d450f95e4db8ed07311992f3d86301b165106525
Tinghan Shen <tinghan.shen@mediatek.com>
2022-06-20 19:21:31 +0800

    chip/mt_scp: add chip dependent driver for mt8188
    Add chip dependent driver for mt8188.
    
    BRANCH=none
    BUG=b:236331462
    TEST=boot to scp console
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: If1503ad7fa6ee434f25bde50afd81202cd9b169c

---
+++ chip/mt_scp/mt8188/uart.c
@@ -0,0 +1,27 @@
+/* Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* SCP UART module for MT8195 specific */
+
+#include "uart_regs.h"
+
+/*
+ * UARTN == 0, SCP UART0
+ * UARTN == 1, SCP UART1
+ * UARTN == 2, AP UART1
+ */
+#define UARTN CONFIG_UART_CONSOLE
+
+void uart_init_pinmux(void)
+{
+#if UARTN == 0
+	SCP_UART_CK_SEL |= UART0_CK_SEL_VAL(UART_CK_SEL_ULPOSC);
+	SCP_SET_CLK_CG |= CG_UART0_MCLK | CG_UART0_BCLK | CG_UART0_RST;
+
+	/* set AP GPIO33 and GPIO34 to alt func 4 */
+	AP_GPIO_MODE4_CLR = 0x00000770;
+	AP_GPIO_MODE4_SET = 0x00000440;
+#endif
+}
