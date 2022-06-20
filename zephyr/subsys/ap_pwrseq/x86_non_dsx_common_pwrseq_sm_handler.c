df01abfa3b6f56776006a0ecc6460c9db38a7dbe
Andrew McRae <amcrae@google.com>
2022-06-20 17:41:16 +1000

    ap_pwrseq: Refactor exit-hardoff handling
    Redo the exit-hardoff handling so that the S5 inactivity timer
    is extended to give the AP more time to handle a button press,
    and also to clear the request flag when transitioning out of S5 to S4.
    
    This is avoid the situation when the request-exit-hardoff flag
    is set in S5, but the AP does not enter G3 because it is
    restarting (e.g via a cool reboot).
    
    BUG=b:234572032,b:235429065
    TEST=Run firmware_ECPowerButton test
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I8039bf9a1c28ec24dfd313c52bbc0e2aeb7e6d59

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
@@ -14,6 +14,7 @@ static struct pwrseq_context pwrseq_ctx;
 static bool s5_inactive_tmr_running;
 /* S5 inactive timer*/
 K_TIMER_DEFINE(s5_inactive_timer, NULL, NULL);
+static bool request_g3_exit;
 
 LOG_MODULE_REGISTER(ap_pwrseq, CONFIG_AP_PWRSEQ_LOG_LEVEL);
 
@@ -96,14 +97,20 @@ void pwr_sm_set_state(enum power_states_ndsx new_state)
 	pwrseq_ctx.power_state = new_state;
 }
 
-void request_exit_hardoff(bool should_exit)
+void request_exit_hardoff(void)
 {
-	pwrseq_ctx.want_g3_exit = should_exit;
-}
-
-static bool chipset_is_exit_hardoff(void)
-{
-	return pwrseq_ctx.want_g3_exit;
+	LOG_DBG("Request exit hard-off");
+	request_g3_exit = true;
+	/*
+	 * If in S5, restart the timer to give the CPU more time
+	 * to respond to a power button press (which is presumably
+	 * why we are being called). This avoids having the S5
+	 * inactivity timer expiring before the AP can process
+	 * the power button press and start up.
+	 */
+	if (pwr_sm_get_state() == SYS_POWER_STATE_S5) {
+		s5_inactive_tmr_running = false;
+	}
 }
 
 void ap_power_force_shutdown(enum ap_power_shutdown_reason reason)
@@ -191,8 +198,9 @@ static int common_pwr_sm_run(int state)
 {
 	switch (state) {
 	case SYS_POWER_STATE_G3:
-		if (chipset_is_exit_hardoff()) {
-			request_exit_hardoff(false);
+		if (request_g3_exit) {
+			LOG_DBG("Request to exit G3");
+			request_g3_exit = false;
 			/*
 			 * G3->S0 transition should happen only after the
 			 * user specified delay. Hence, wait until the
@@ -231,7 +239,10 @@ static int common_pwr_sm_run(int state)
 			return SYS_POWER_STATE_S5G3;
 		else if (AP_PWRSEQ_DT_VALUE(s5_inactivity_timeout) > 0) {
 			if (!s5_inactive_tmr_running) {
-				/* Timer is not started */
+				/*
+				 * Timer is not started, or needs
+				 * restarting.
+				 */
 				k_timer_start(&s5_inactive_timer,
 					K_SECONDS(AP_PWRSEQ_DT_VALUE(
 						s5_inactivity_timeout)),
@@ -249,6 +260,8 @@ static int common_pwr_sm_run(int state)
 		shutdown_and_notify(AP_POWER_SHUTDOWN_G3);
 		/* Notify power event before we enter G3 */
 		ap_power_ev_send_callbacks(AP_POWER_HARD_OFF);
+		/* Clear any request to exit hard-off */
+		request_g3_exit = false;
 		return SYS_POWER_STATE_G3;
 
 	case SYS_POWER_STATE_S5S4:
@@ -539,7 +552,7 @@ void ap_pwrseq_task_start(void)
 
 static void init_pwr_seq_state(void)
 {
-	request_exit_hardoff(false);
+	request_g3_exit = false;
 	/*
 	 * The state of the CPU needs to be determined now
 	 * so that init routines can check the state of
