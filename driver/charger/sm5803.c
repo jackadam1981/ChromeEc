71ee3f271702a127a98e7943c2f0eaf1de27e3f1
Peter Marheine <pmarheine@chromium.org>
2022-06-20 13:28:37 +1000

    sm5803: ensure clocks are not in LPM when enabling
    If the charger's internal clocks are running at their reduced
    frequency when switching is enabled, it is possible for its internal
    control loops to respond slower than is intended and cause incorrect
    behavior or possibly even damage to the charger. This change ensures
    that the clocks are running at full speed before enabling either
    sink or source mode on the charger, providing strong runtime assurance
    that the charger should not be damaged.
    
    BUG=b:230712704
    TEST=Bus-powered devices continue to work normally when connected to
         Nereid, and the battery can be charged.
    BRANCH=none
    
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Change-Id: I656605921648b72c1da7394012dc7a9a32397895

--- driver/charger/sm5803.c
+++ driver/charger/sm5803.c
@@ -142,6 +142,35 @@ static inline enum ec_error_list test_update8(int chgnum, const int offset,
 			   SM5803_ADDR_TEST_FLAGS, offset, mask, action);
 }
 
+/*
+ * Ensure the charger clocks are at normal operating speed, setting them to
+ * that speed if not already.
+ *
+ * The SM5803 runs multiple digital control loops that are important to correct
+ * operation. The CLOCK_SEL_LOW register reduced their speed by about 10x, which
+ * is dangerous when either sinking or sourcing is to be enabled because the
+ * control loops will respond much more slowly. Leaving clocks at low speed can
+ * cause incorrect operation or even hardware damage.
+ *
+ * This function is used by the functions that enable sinking or sourcing to
+ * ensure the control loops are running at full speed before enabling switching
+ * on the charger.
+ */
+static int sm5803_ensure_main_clock_speed(int chgnum)
+{
+	int rv, val;
+
+	rv = main_read8(chgnum, SM5803_REG_CLOCK_SEL, &val);
+	if (rv) {
+		return rv;
+	}
+	if (val & SM5803_CLOCK_SEL_LOW) {
+		rv = main_write8(chgnum, SM5803_REG_CLOCK_SEL,
+				 val & ~SM5803_CLOCK_SEL_LOW);
+	}
+	return rv;
+}
+
 static enum ec_error_list sm5803_flow1_update(int chgnum, const uint8_t mask,
 					const enum mask_update_action action)
 {
@@ -292,6 +321,13 @@ enum ec_error_list sm5803_vbus_sink_enable(int chgnum, int enable)
 		return rv;
 
 	if (enable) {
+		rv = sm5803_ensure_main_clock_speed(chgnum);
+		if (rv) {
+			CPRINTS("%s %d: unable to ensure clocks are out of LPM",
+				CHARGER_NAME, chgnum);
+			return rv;
+		}
+
 		if (chgnum == CHARGER_PRIMARY) {
 			/* Magic for new silicon */
 			if (dev_id >= 3) {
@@ -1696,6 +1732,13 @@ static enum ec_error_list sm5803_enable_otg_power(int chgnum, int enabled)
 
 	if (enabled) {
 		int selected_current;
+
+		rv = sm5803_ensure_main_clock_speed(chgnum);
+		if (rv) {
+			CPRINTS("%s %d: unable to ensure clocks are out of LPM",
+				CHARGER_NAME, chgnum);
+			return rv;
+		}
 
 		rv = chg_read8(chgnum, SM5803_REG_ANA_EN1, &reg);
 		if (rv)
