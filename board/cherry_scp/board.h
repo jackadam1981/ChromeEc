f83f84c47ab9e30428fa52eae6db1666cbdf2d59
Tinghan Shen <tinghan.shen@mediatek.com>
2022-06-23 15:08:26 +0800

    WIP: cherry_scp: extend scp core 1 sram 64K->124K
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I841f0318a8ba0aa1732c1b19205eebbd94093e5d

--- board/cherry_scp/board.h
+++ board/cherry_scp/board.h
@@ -10,22 +10,22 @@
 
 #include "baseboard.h"
 
-#define SCP_CORE1_RAM_SIZE 0x10000 /* 64K */
+#define SCP_CORE1_RAM_SIZE 0x1F000 /* 124K */
 #define SCP_CORE1_RAM_PADDING 0xc00 /* for 4K-alignment */
 
 #ifdef CHIP_VARIANT_MT8195_CORE1
 
 /*
  * RW only, no flash
- * +-------------------- 0xaf000 + 0
+ * +-------------------- 0xa0000 + 0
  * | ROM vectortable, .text, .rodata, .data LMA
- * +-------------------- 0xaf000 + 0xb000 = 0xba000
+ * +-------------------- 0xa0000 + 0x14000 = 0xbb000
  * | RAM .bss, .data
- * +-------------------- 0xaf000 + 0xfc00 = 0xbec00
+ * +-------------------- 0xa0000 + 0xfc00 = 0xbec00
  * | Reserved (padding for 1k-alignment)
- * +-------------------- 0xaf000 + 0xfdb0 = 0xbedb0
+ * +-------------------- 0xa0000 + 0xfdb0 = 0xbedb0
  * | IPI shared buffer with AP (288 + 8) * 2
- * +-------------------- 0xaf000 + 0x10000 = 0xbf000
+ * +-------------------- 0xa0000 + 0x10000 = 0xbf000
  *
  * [Memory remap]
  * SCP core 1 has registers to remap core view addresses by SCP bus. This is
@@ -35,7 +35,7 @@
  * The core address 0x0~0x10000 are translated to 0xaf000~0xbf000.
  */
 #define CONFIG_ROM_BASE 0x0
-#define CONFIG_RAM_BASE 0xb000
+#define CONFIG_RAM_BASE 0x14000
 #define CONFIG_ROM_SIZE (CONFIG_RAM_BASE - CONFIG_ROM_BASE)
 #define CONFIG_RAM_SIZE ((CONFIG_IPC_SHARED_OBJ_ADDR & (~(0x400 - 1))) - \
 			 CONFIG_RAM_BASE)
