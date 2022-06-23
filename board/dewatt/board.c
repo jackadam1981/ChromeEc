78fdd35b8a910fe5ce0325b16701299dc975d5cd
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2022-06-23 16:33:59 +0800

    Dewatt: Override current limit when battery can't discharge
    When the battery is bad, override charger current limit to be
    6A to prevent brownouts during boot.
    
    BUG=b:237336324
    BRANCH=guybrush
    TEST=there's no power loss with bad battery during boot.
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: Ifb19321a14ba5acb898d1baf7451cc2ddcecc076

--- board/dewatt/board.c
+++ board/dewatt/board.c
@@ -447,10 +447,12 @@ void board_set_current_limit(void)
 {
 	const int no_battery_current_limit_override_ma = 6000;
 	/*
-	 * When there is no battery, override charger current limit to
-	 * prevent brownout during boot.
+	 * When there is no battery or battery is bad, override charger
+	 * current limit to prevent brownout during boot.
 	 */
-	if (battery_is_present() == BP_NO) {
+	if (battery_is_present() == BP_NO ||
+		battery_get_disconnect_state() == BATTERY_DISCONNECT_ERROR ||
+		battery_get_disconnect_state() == BATTERY_DISCONNECTED) {
 		ccprints("No Battery Found - Override Current Limit to %dmA",
 			 no_battery_current_limit_override_ma);
 		charger_set_input_current_limit(
@@ -459,3 +461,5 @@ void board_set_current_limit(void)
 }
 DECLARE_HOOK(HOOK_BATTERY_SOC_CHANGE, board_set_current_limit,
 	     HOOK_PRIO_INIT_EXTPOWER);
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_set_current_limit,
+	     HOOK_PRIO_DEFAULT);
