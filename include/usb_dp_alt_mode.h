b31459fb8e4d3ca121e2d4ff160c15f0a26fc1a0
Abe Levkoy <alevkoy@chromium.org>
2022-06-24 17:21:00 -0600

    TCPMv2: Synchronize DP and DPM states
    Leave DisplayPort alt mode state machine in inactive state after failed
    entry. Reset DP state machine to start state after DP entry request from
    AP. Avoid repeatedly cycling DP state machine, independently of DPM,
    after failed DP mode entry sequence.
    
    BUG=b:235984702
    TEST=zmake testall
    BRANCH=none
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: I2e1a5ab36a38cba51e19f39e1f9ea9a8944e1849

--- include/usb_dp_alt_mode.h
+++ include/usb_dp_alt_mode.h
@@ -34,6 +34,16 @@ void dp_init(int port);
 bool dp_is_active(int port);
 
 /*
+ * Returns True if DisplayPort mode entry has not started, or mode exit has
+ * already finished.
+ *
+ * @param port      USB-C port number
+ * @return          True if DisplayPort mode is in inactive state
+ *                  False otherwise.
+ */
+bool dp_is_inactive(int port);
+
+/*
  * Checks whether the mode entry sequence for DisplayPort alternate mode is done
  * for a port.
  *
