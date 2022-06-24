92ee78271652d5e396d851a3785bcff059abb415
Al Semjonovs <asemjonovs@google.com>
2022-06-24 07:48:45 -0600

    zephyr: generic power button driver
    Generic power button driver for zephyr
    
    BUG=b:234728861
    BRANCH=none
    TEST=zmake testall
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: I98e48cc030c19f0b4463df9658d58f2af56570cd

---
+++ zephyr/test/drivers/default/src/button_config.c
@@ -0,0 +1,52 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * @file
+ * @brief Unit Tests for watchdog.
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/watchdog.h>
+
+#include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
+#include <zephyr/ztest.h>
+
+#include "button.h"
+#include "button_config.h"
+#include "common.h"
+#include "ec_tasks.h"
+#include "test/drivers/stubs.h"
+#include "test/drivers/test_state.h"
+
+/**
+ * @brief TestPurpose: Verify button_config initialization.
+ *
+ */
+ZTEST(button_config, test_button_config)
+{
+	const struct button_config *button;
+
+	for (int i = 0; i < BUTTON_CFG_COUNT; i++) {
+		button = get_button_cfg(i);
+
+		printf("button[%d]= {%s, %d, %d, %d, %d}\n", i, button->name,
+		       button->type, button->gpio, button->debounce_us,
+		       button->flags);
+	}
+
+	button = get_button_cfg(BUTTON_CFG_POWER_BUTTON);
+
+	zassert_equal(button->type, 0, NULL);
+	zassert_equal(button->gpio, GPIO_POWER_BUTTON_L, NULL);
+	zassert_equal(button->debounce_us, 30000, NULL);
+	zassert_equal(button->flags, 0, NULL);
+}
+
+/**
+ * @brief Test Suite: Verifies button_config functionality.
+ */
+ZTEST_SUITE(button_config, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
