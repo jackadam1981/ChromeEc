61f54263eba8eb943c92c4e98a1403184d292891
Eric Yilun Lin <yllin@chromium.org>
2022-06-24 16:56:26 +0800

    rt1718s: support device binding for TCPC and BC1.2
    support RichTek RT1718S BC1.2 and TCPC device binding
    
    BUG=b:227359727
    TEST=zmake
    BRANCH=none
    
    Change-Id: I3c13781e9bc53fa269295d900903d38ac91b1f0d
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/shim/src/bc12.c
+++ zephyr/shim/src/bc12.c
@@ -5,12 +5,14 @@
 
 #include <zephyr/devicetree.h>
 #include "usbc/bc12_pi3usb9201.h"
+#include "usbc/bc12_rt1718s.h"
 #include "usbc/bc12_rt1739.h"
 #include "usbc/bc12_rt9490.h"
 #include "usbc/utils.h"
 #include "usb_charge.h"
 
-#if DT_HAS_COMPAT_STATUS_OKAY(RT1739_BC12_COMPAT) || \
+#if DT_HAS_COMPAT_STATUS_OKAY(RT1718S_BC12_COMPAT) || \
+	DT_HAS_COMPAT_STATUS_OKAY(RT1739_BC12_COMPAT) || \
 	DT_HAS_COMPAT_STATUS_OKAY(RT9490_BC12_COMPAT) || \
 	DT_HAS_COMPAT_STATUS_OKAY(PI3USB9201_COMPAT)
 
@@ -18,6 +20,8 @@
 
 /* Power Path Controller */
 struct bc12_config bc12_ports[CHARGE_PORT_COUNT] = {
+	DT_FOREACH_STATUS_OKAY_VARGS(RT1718S_BC12_COMPAT, BC12_CHIP,
+				     BC12_CHIP_RT1718S)
 	DT_FOREACH_STATUS_OKAY_VARGS(RT1739_BC12_COMPAT, BC12_CHIP,
 				     BC12_CHIP_RT1739)
 	DT_FOREACH_STATUS_OKAY_VARGS(RT9490_BC12_COMPAT, BC12_CHIP,
