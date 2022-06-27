52c9470c077e6ed511a03a0ad55fed6766f72012
Eric Yilun Lin <yllin@chromium.org>
2022-06-27 15:08:32 +0800

    nx20p348x: add device binding for PPC
    add NXP NX20P348X PPC device binding
    
    BUG=b:227359727
    TEST=enable on kingler
    BRANCH=none
    
    Change-Id: Id050a0cb5fccf3dd0b868f374bf2849087ec6505
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/shim/include/usbc/ppc.h
+++ zephyr/shim/include/usbc/ppc.h
@@ -9,6 +9,7 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include "usbc/ppc_rt1739.h"
+#include "usbc/ppc_nx20p348x.h"
 #include "usbc/ppc_sn5s330.h"
 #include "usbc/ppc_syv682x.h"
 #include "usbc/utils.h"
@@ -21,7 +22,8 @@
 	COND_CODE_1(DT_NODE_HAS_PROP(id, alternate_for), (PPC_ID(id)), (EMPTY))
 
 enum ppc_chips_alt_id {
-	LIST_DROP_EMPTY(DT_FOREACH_STATUS_OKAY(RT1739_PPC_COMPAT, PPC_ALT_ENUM),
+	LIST_DROP_EMPTY(DT_FOREACH_STATUS_OKAY(NX20P348X_COMPAT, PPC_ALT_ENUM),
+			DT_FOREACH_STATUS_OKAY(RT1739_PPC_COMPAT, PPC_ALT_ENUM),
 			DT_FOREACH_STATUS_OKAY(SN5S330_COMPAT, PPC_ALT_ENUM),
 			DT_FOREACH_STATUS_OKAY(SYV682X_COMPAT, PPC_ALT_ENUM),
 			PPC_CHIP_ALT_COUNT)
