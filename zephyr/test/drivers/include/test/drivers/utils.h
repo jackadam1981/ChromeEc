6d7324e6b6b66794b327898e00b71ffd901f9b03
Jack Rosenthal <jrosenth@chromium.org>
2022-06-27 15:40:28 -0600

    zephyr/test/drivers/include/test/drivers/utils.h: Format with clang-format
    BUG=b:236386294
    BRANCH=none
    TEST=none
    
    Change-Id: I0df69832f37afcaa254ca18138aa40e1dfa43940
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>

--- zephyr/test/drivers/include/test/drivers/utils.h
+++ zephyr/test/drivers/include/test/drivers/utils.h
@@ -452,7 +452,7 @@ void host_cmd_typec_discovery(int port, enum typec_partner_type partner_type,
 			      void *response, size_t response_size);
 
 #define GPIO_ACOK_OD_NODE DT_NODELABEL(gpio_acok_od)
-#define GPIO_ACOK_OD_PIN  DT_GPIO_PIN(GPIO_ACOK_OD_NODE, gpios)
+#define GPIO_ACOK_OD_PIN DT_GPIO_PIN(GPIO_ACOK_OD_NODE, gpios)
 
 /**
  * Set whether or not AC is enabled.
