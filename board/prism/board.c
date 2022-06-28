25f27ab235668eb525f415ec7932b59b2a60a0aa
Parth Malkan <parthmalkan@google.com>
2022-06-28 13:54:35 -0700

    prism: rgbkbd: Add support_mode field
    Support mode describes number of zones in LEDs supported on the rgbkbd
    
    BRANCH=none
    BUG=b:232134905
    TEST=make -j buildall
    
    Cq-Depend: chromium:3732802
    Signed-off-by: Parth Malkan <parthmalkan@google.com>
    Change-Id: Idf4b9081e6510dcdbd4301d9d855aba2fe7e7499

--- board/prism/board.c
+++ board/prism/board.c
@@ -103,6 +103,8 @@ const uint8_t rgbkbd_count = ARRAY_SIZE(rgbkbds);
 const uint8_t rgbkbd_hsize = RGB_GRID0_COL + RGB_GRID1_COL;
 const uint8_t rgbkbd_vsize = RGB_GRID0_ROW;
 
+const enum ec_rgbkbd_support_mode support_mode = PER_KEY;
+
 #define LED(x, y)	RGBKBD_COORD((x), (y))
 #define DELM		RGBKBD_DELM
 const uint8_t rgbkbd_map[] = {
