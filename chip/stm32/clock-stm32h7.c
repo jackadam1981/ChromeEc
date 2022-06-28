e87a5e3a8b81322c56cf6f47c611c463ca2069cd
Bobby Casey <bobbycasey@google.com>
2022-06-28 11:22:21 -0400

    cortex-m: Add debugger protection to stm32f4 and stm32f7 idle tasks
    Allowing the processor to sleep causes the debugger to stop working.
    
    BRANCH=none
    BUG=b:180144572
    TEST=manual testing via debugger
    
    Signed-off-by: Bobby Casey <bobbycasey@google.com>
    Change-Id: Iabbb0a5271c8af36ad55142f79140784dbfb05ff

--- chip/stm32/clock-stm32h7.c
+++ chip/stm32/clock-stm32h7.c
@@ -21,6 +21,7 @@
 #include "common.h"
 #include "console.h"
 #include "cpu.h"
+#include "debug.h"
 #include "hooks.h"
 #include "hwtimer.h"
 #include "registers.h"
@@ -492,7 +493,8 @@ void __idle(void)
 			/* ensure outstanding memory transactions complete */
 			asm volatile("dsb");
 
-			asm("wfi");
+			if (!debugger_is_connected())
+				asm("wfi");
 
 			CPU_SCB_SYSCTRL &= ~0x4;
 
@@ -527,7 +529,8 @@ void __idle(void)
 			idle_sleep_cnt++;
 
 			/* normal idle : only CPU clock stopped */
-			asm("wfi");
+			if (!debugger_is_connected())
+				asm("wfi");
 		}
 		asm volatile("cpsie i");
 	}
