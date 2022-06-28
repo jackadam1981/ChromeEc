421e586feee39ab13681576b049702994252926e
Yuval Peress <peress@google.com>
2022-06-27 20:49:48 -0600

    test: Reconfigure drivers BUILD.py to use .variant
    Following up on chromium:3724970. Updating the build file to use the
    supported variant functionality of zmake to make the tests more
    conformant.
    
    BRANCH=none
    BUG=none
    TEST=zmake test test-drivers-isl923x
    TEST=zmake test test-drivers-led_driver
    
    Signed-off-by: Yuval Peress <peress@google.com>
    Change-Id: I04912e56c180566bb4bac4b08d2aca828d85a3b0

--- zephyr/test/drivers/BUILD.py
+++ zephyr/test/drivers/BUILD.py
@@ -5,49 +5,37 @@
 """Construct the drivers test binaries"""
 
 
-default_config = {
-    "dts_overlays": [
-        here / "overlay.dts",
-    ],
-    "kconfig_files": [
-        here / "prj.conf",
-    ],
-    "test_args": ["-flash={test_temp_dir}/flash.bin"],
-}
+def register_drivers_variant(test_name, extra_dts_overlays=(), extra_kconfig_files=()):
+    """Register a variant of the drivers test"""
+    return register_host_test(
+        test_name=test_name,
+        dts_overlays=[
+            here / "overlay.dts",
+            # Project-specific DTS customization.
+            *extra_dts_overlays,
+        ],
+        kconfig_files=[
+            # Common to all projects.
+            here / "prj.conf",
+            # Project-specific KConfig customization.
+            *extra_kconfig_files,
+        ],
+    )
 
 
-def merge_dictionary(dict_1, dict_2):
-    """Merge dict_1 and dict_2 and return the result"""
-    dict_3 = {**dict_1, **dict_2}
-    for key, value in dict_3.items():
-        if key in dict_1 and key in dict_2:
-            if isinstance(value, list) or isinstance(dict_1[key], list):
-                dict_3[key] = value + dict_1[key]
-            else:
-                dict_3[key] = [value, dict_1[key]]
-    return dict_3
-
-
-register_host_test(
-    "drivers",
-    **default_config,
+drivers = register_drivers_variant(
+    test_name="drivers",
 )
 
-register_host_test(
-    "drivers-isl923x",
-    **default_config,
+isl923x = register_drivers_variant(
+    test_name="drivers-isl923x",
 )
 
-register_host_test(
-    "drivers-led_driver",
-    **merge_dictionary(
-        default_config,
-        {
-            "dts_overlays": [
-                here / "led_driver" / "led_pins.dts",
-                here / "led_driver" / "led_policy.dts",
-            ],
-            "kconfig_files": [here / "led_driver" / "prj.conf"],
-        },
-    ),
+led_driver = register_drivers_variant(
+    test_name="drivers-led_driver",
+    extra_dts_overlays=[
+        here / "led_driver" / "led_pins.dts",
+        here / "led_driver" / "led_policy.dts",
+    ],
+    extra_kconfig_files=[here / "led_driver" / "prj.conf"],
 )
