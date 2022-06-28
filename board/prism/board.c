44a31a577d27232f4344fc37f35a5d07f55ea713
Parth Malkan <parthmalkan@google.com>
2022-06-28 13:54:35 -0700

    prism: rgbkbd: Add rgb keyboard type field
    rgbkbd_type describes number of zones and LEDs supported.
    
    BRANCH=none
    BUG=b:232134905
    TEST=make -j buildall
    
    Cq-Depend: chromium:3732802
    Signed-off-by: Parth Malkan <parthmalkan@google.com>
    Change-Id: Idf4b9081e6510dcdbd4301d9d855aba2fe7e7499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3732805
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    (cherry picked from commit fa3766d2e4a13c228caf88c122eb6dfc30bd1818)

--- board/prism/board.c
+++ board/prism/board.c
@@ -102,6 +102,14 @@ const uint8_t rgbkbd_count = ARRAY_SIZE(rgbkbds);
 const uint8_t rgbkbd_hsize = RGB_GRID0_COL + RGB_GRID1_COL;
 const uint8_t rgbkbd_vsize = RGB_GRID0_ROW;
 
+<<<<<<< HEAD   (4413e9 ectool: Bypass GEC_LOCK for COMM_USB interfaces)
+=======
+const enum ec_rgbkbd_type rgbkbd_type = EC_RGBKBD_TYPE_PER_KEY;
+
+#define LED(x, y) RGBKBD_COORD((x), (y))
+#define DELM RGBKBD_DELM
+
+>>>>>>> CHANGE (fa3766 prism: rgbkbd: Add rgb keyboard type field)
 const uint8_t rgbkbd_map[] = {
 	RGBKBD_DELM,
 	RGBKBD_COORD( 0, 0), RGBKBD_DELM,	/* `~ */
