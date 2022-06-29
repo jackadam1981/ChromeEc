95b513f534b4154576d7ab3d96a4cf0e68b5fd0a
Parth Malkan <parthmalkan@google.com>
2022-06-29 14:39:49 -0700

    ectool: rgbkbd: Add getconfig support
    Usage - ectool rgbkbd getconfig
    Returns HW capability
    
    BRANCH=NONE
    
    Change-Id: Ia58c648865e8bca2f073770b54a3b0f529f35abf

--- util/ectool.c
+++ util/ectool.c
@@ -1308,8 +1308,11 @@ static void cmd_rgbkbd_help(char *cmd)
 	"          Set the scale parameter of key_<key> to <val>.\n"
 	"          <val> is a 24-bit integer where scale values are encoded\n"
 	"          as R=23:16, G=15:8, B=7:0.\n"
+	"\n"
+	"  Usage5: %s getconfig\n"
+	"          Get the HW config supported.\n"
 	"\n",
-	cmd, cmd, cmd, cmd);
+	cmd, cmd, cmd, cmd, cmd);
 }
 
 static int cmd_rgbkbd_parse_rgb_text(const char *text, struct rgb_s *color)
@@ -1375,7 +1378,7 @@ static int cmd_rgbkbd(int argc, char *argv[])
 	char *e;
 	int rv = -1;;
 
-	if (argc < 3) {
+	if (argc < 2) {
 		cmd_rgbkbd_help(argv[0]);
 		return -1;
 	}
@@ -1417,6 +1420,17 @@ static int cmd_rgbkbd(int argc, char *argv[])
 		}
 		p.subcmd = EC_RGBKBD_SUBCMD_SET_SCALE;
 		rv = ec_command(EC_CMD_RGBKBD, 0, &p, sizeof(p), NULL, 0);
+	} else if (argc == 2 && !strcasecmp(argv[1], "getconfig")) {
+		/* Usage 5 */
+		struct ec_params_rgbkbd p;
+		struct ec_response_rgbkbd r;
+
+		p.subcmd = EC_RGBKBD_SUBCMD_GET_CONFIG;
+		rv = ec_command(EC_CMD_RGBKBD, 0, &p, sizeof(p), &r, sizeof(r));
+
+		if (rv < 0)
+			return rv;
+		printf("GetConfig support %d\n", r.support_mode);
 	} else {
 		/* Usage 1 */
 		rv = cmd_rgbkbd_set_color(argc, argv);
