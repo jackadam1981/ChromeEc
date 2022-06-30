2e2c691bcb943c1177aa6ab5ad641df8f991619c
Mulin Chao <mlchao@nuvoton.com>
2022-06-29 18:56:36 -0700

    zephyr: gpio: npcx: Add `gpiodbg` command for power investigation
    This CL adds a `gpiodbg` console command for investigating power
    consumption. This command is used to turn on/off IO pad's input buffer
    and this pad won't consume power anymore.
    
    Here is sn example to list all IO pads used on npcx9 evb and turn
    `spi_cs_l` input buffer off
    
    ```
    ec:~$ gpiodbg list
    NO |ON| GPIO |  Name
    ---+--+------+----------
    00 |* | io03 | recovery_l
    01 |* | io93 | wp_l
    02 |* | iod2 | ac_present
    03 |* | io00 | power_button_l
    04 |* | io01 | lid_open
    05 |* | io36 | entering_rw
    06 |* | io50 | pch_wake_l
    07 |* | ioc7 | pgood_fan
    08 |* | ioa5 | spi_cs_l
    09 |* | io64 | board_version1
    10 |* | io65 | board_version2
    11 |* | io66 | board_version3
    12 |* | io52 | unused pin
    13 |* | io54 | unused pin
    14 |* | io76 | unused pin
    15 |* | iod1 | unused pin
    
    ec:~$ gpiodbg 8 off
    ```
    
    BUG=b:228222441
    BRANCH=none
    TEST=zmake build npcx9 --clobber.
    
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>
    Change-Id: Ia61deaf6bd3acf9faa5c2bce14615f92a809ddc1

--- zephyr/shim/chip/npcx/gpio.c
+++ zephyr/shim/chip/npcx/gpio.c
@@ -9,7 +9,10 @@
 
 #include <zephyr/logging/log.h>
 
+#include "console.h"
 #include "gpio/gpio.h"
+#include "soc_gpio.h"
+#include "util.h"
 
 LOG_MODULE_REGISTER(shim_cros_gpio, LOG_LEVEL_ERR);
 
@@ -53,3 +56,105 @@ int gpio_config_unused_pins(void)
 
 	return 0;
 }
+
+#ifdef CONFIG_PLATFORM_EC_CONSOLE_CMD_GPIODBG
+/*
+ * IO information about each GPIO that is configured in the `named_gpios` and
+ *` unused_pins` device tree nodes.
+ */
+struct npcx_io_info {
+	/* A npcx gpio port device */
+	const struct device *dev;
+	/* A npcx gpio port number */
+	int port;
+	/* Bit number of pin within a npcx gpio port */
+	gpio_pin_t pin;
+	/* Config flags of npcx gpio pin */
+	gpio_flags_t flags;
+	/* GPIO net name */
+	const char *name;
+	/* Enable flag of npcx gpio input buffer */
+	bool enable;
+};
+
+#define NAMED_GPIO_INFO(node)						\
+	{								\
+		.dev = DEVICE_DT_GET(DT_GPIO_CTLR(node, gpios)),	\
+		.port = DT_PROP(DT_GPIO_CTLR(node, gpios), index),	\
+		.pin = DT_GPIO_PIN(node, gpios),			\
+		.flags = DT_GPIO_FLAGS(node, gpios),			\
+		.name = DT_NODE_FULL_NAME(node),			\
+		.enable = true,						\
+	},
+
+#define UNUSED_GPIO_INFO(node, prop, idx)					\
+	{									\
+		.dev = DEVICE_DT_GET(DT_GPIO_CTLR_BY_IDX(node, prop, idx)),	\
+		.port = DT_PROP(DT_GPIO_CTLR_BY_IDX(node, prop, idx), index),	\
+		.pin = DT_GPIO_PIN_BY_IDX(node, prop, idx),			\
+		.flags =  DT_GPIO_FLAGS_BY_IDX(node, prop, idx),		\
+		.name = "unused pin",						\
+		.enable = true,							\
+	},
+
+static struct npcx_io_info gpio_info[] = {
+#if DT_NODE_EXISTS(DT_PATH(named_gpios))
+	DT_FOREACH_CHILD(DT_PATH(named_gpios), NAMED_GPIO_INFO)
+#endif
+#if DT_NODE_EXISTS(DT_PATH(unused_pins))
+	DT_FOREACH_PROP_ELEM(DT_PATH(unused_pins), unused_gpios, UNUSED_GPIO_INFO)
+#endif
+};
+
+/*
+ * Command used to turn on/off input buffer of gpios to investigate power
+ * consumption
+ */
+static int command_gpio_debug(int argc, char **argv)
+{
+	if (argc == 2) {
+		/* List all GPIOs in 'named-gpios' node */
+		if (!strcasecmp(argv[1], "list")) {
+			ccprintf("NO |ON| GPIO |  Name\n");
+			ccprintf("---+--+------+----------\n");
+
+			for (int i = 0; i < ARRAY_SIZE(gpio_info); i++) {
+				ccprintf("%02d |%s |", i,
+					gpio_info[i].enable ? "*" : " ");
+				ccprintf(" io%x%x | ",
+					gpio_info[i].port, gpio_info[i].pin);
+				ccprintf("%s\n",  gpio_info[i].name);
+			}
+			return EC_SUCCESS;
+		}
+	}
+	else if (argc == 3) {
+		int enable;
+		char *e;
+		int num = strtoi(argv[1], &e, 0);
+
+		if (*e || num < 0 || num >=  ARRAY_SIZE(gpio_info))
+			return EC_ERROR_PARAM1;
+
+		if (parse_bool(argv[2], &enable)) {
+			if (enable) {
+				gpio_info[num].enable = true;
+				npcx_gpio_enable_io_pads(gpio_info[num].dev,
+							gpio_info[num].pin);
+			} else {
+				gpio_info[num].enable = false;
+				npcx_gpio_disable_io_pads(gpio_info[num].dev,
+							gpio_info[num].pin);
+			}
+		}
+		else
+			return EC_ERROR_PARAM2;
+
+		return EC_SUCCESS;
+	}
+	return EC_ERROR_INVAL;
+}
+DECLARE_CONSOLE_COMMAND(gpiodbg, command_gpio_debug,
+		"list/<num> on|off",
+		"Turn on/off GPIO buffer to investigate power consumption");
+#endif /* CONFIG_PLATFORM_EC_CONSOLE_CMD_GPIODBG */
