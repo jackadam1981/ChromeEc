fb0c494565f3851e8f020ed6fc0b4f80c05ab07b
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2022-06-30 16:07:46 +0800

    power/mt8192: Do not exit hard off when ap idle
    To prevent system power on from POWER_G3 when ap idle (such as power
    off the system in recovery mode), set exit_hard_off to 0 for this
    condition.
    
    BUG=b:237230874
    BRANCH=cherry
    TEST=make sure that system will not power on when power off in recovery
    mode.
    
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Change-Id: I27697a030f3fdb128ea87b559add0a4a05399c56

--- power/mt8192.c
+++ power/mt8192.c
@@ -204,10 +204,14 @@ DECLARE_HOOK(HOOK_CHIPSET_RESET, handle_chipset_reset, HOOK_PRIO_FIRST);
 enum power_state power_chipset_init(void)
 {
 	int exit_hard_off = 1;
+	uint32_t reset_flags = system_get_reset_flags();
 
 	/* Enable reboot / sleep control inputs from AP */
 	gpio_enable_interrupt(GPIO_AP_EC_WARM_RST_REQ);
 	gpio_enable_interrupt(GPIO_AP_IN_SLEEP_L);
+
+	if (reset_flags & EC_RESET_FLAG_AP_IDLE)
+		exit_hard_off = 0;
 
 	if (system_jumped_late()) {
 		if ((power_get_signals() & IN_ALL_S0) == IN_ALL_S0) {
@@ -216,9 +220,9 @@ enum power_state power_chipset_init(void)
 			CPRINTS("already in S0");
 			return POWER_S0;
 		}
-	} else if (system_get_reset_flags() & EC_RESET_FLAG_AP_OFF) {
+	} else if (reset_flags & EC_RESET_FLAG_AP_OFF) {
 		exit_hard_off = 0;
-	} else if ((system_get_reset_flags() & EC_RESET_FLAG_HIBERNATE) &&
+	} else if ((reset_flags & EC_RESET_FLAG_HIBERNATE) &&
 			gpio_get_level(GPIO_AC_PRESENT)) {
 		/*
 		 * If AC present, assume this is a wake-up by AC insert.
