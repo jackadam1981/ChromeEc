36c8688f6becd8fbad8929b047dc828a87db0c2c
martin yan <martin.yan@microchip.corp-partner.google.com>
2022-06-30 13:56:43 -0400

    [WIP] zephyr: mchp: adlrvp_mchp - Support CBI
    Repo sync on 06/01
    Support CBI:
    	CONFIG_EEPROM=y
    	CONFIG_EEPROM_AT24=y
    	CONFIG_PLATFORM_EC_CBI_EEPROM=y
    
    BUG=none
    BRANCH=none
    TEST=test on adlrvp_mchp, EC console command "cbi"
    
    Signed-off-by: martin yan <martin.yan@microchip.corp-partner.google.com>
    Change-Id: I908b315d04d93802529d62788683eb5fd818bcdc

--- zephyr/projects/intelrvp/BUILD.py
+++ zephyr/projects/intelrvp/BUILD.py
@@ -46,6 +46,7 @@ register_intelrvp_project(
     extra_dts_overlays=[
         here / "adlrvp/adlrvp_mchp/adlrvp_mchp.dts",
         here / "adlrvp/adlrvp_mchp/bb_retimer.dts",
+        here / "adlrvp/adlrvp_mchp/cbi_eeprom.dts",    
         here / "adlrvp/adlrvp_mchp/gpio.dts",
         here / "adlrvp/adlrvp_mchp/interrupts.dts",
         here / "adlrvp/adlrvp_mchp/keyboard.dts",
