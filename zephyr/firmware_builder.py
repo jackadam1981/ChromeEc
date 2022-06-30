d4aa35fc7551f0bb818997fa8d626310b2224d69
Jeremy Bettis <jbettis@google.com>
2022-06-30 15:10:11 -0600

    cq: Try to use goma in the zephyr cq builder
    Attempt to start goma, and if it succeeds, run zmake with the --goma
    option.
    
    BRANCH=None
    BUG=b:214323409
    TEST=Dry-run in CQ
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: I2f1d0fe765558cd21f1665f9d6e7d9c5ae3630f5

--- zephyr/firmware_builder.py
+++ zephyr/firmware_builder.py
@@ -10,6 +10,7 @@ This is the entry point for the custom firmware builder workflow recipe.
 
 import argparse
 import multiprocessing
+import os
 import pathlib
 import re
 import subprocess
@@ -29,7 +30,15 @@ def build(opts):
     """Builds all Zephyr firmware targets"""
     metric_list = firmware_pb2.FwBuildMetricList()
 
-    cmd = ['zmake', '-D', 'build', '-a']
+    subprocess.run(['ls', '/mnt/host/depot_tools/.cipd_bin/gomacc', '/home/goma', os.environ['HOME'] + '/goma'], check=False)
+    subprocess.run(['which', 'gomacc'], check=False)
+    goma_running = subprocess.run(['goma_ctl', 'ensure_start'], check=False).returncode == 0
+
+    cmd = ['zmake', '-D']
+    if goma_running:
+        cmd.append('--goma')
+    cmd.append('build')
+    cmd.append('-a')
     if opts.code_coverage:
         cmd.append('--coverage')
     subprocess.run(cmd, cwd=pathlib.Path(__file__).parent, check=True)
@@ -180,7 +189,14 @@ def test(opts):
     config_files = zephyr_dir.rglob('**/BUILD.py')
     subprocess.run(['black', '--diff', '--check', *config_files], check=True)
 
-    cmd = ['zmake', '-D', 'test', '-a', '--no-rebuild']
+    goma_running = subprocess.run(['goma_ctl', 'ensure_start'], check=False).returncode == 0
+
+    cmd = ['zmake', '-D']
+    if goma_running:
+        cmd.append('--goma')
+    cmd.append('test')
+    cmd.append('-a')
+    cmd.append('--no-rebuild')
     if opts.code_coverage:
         cmd.append('--coverage')
     ret = subprocess.run(cmd, check=True).returncode
