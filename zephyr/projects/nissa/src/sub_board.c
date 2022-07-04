7ce6e233db4f212b0e8ced9910b9c85e4009c4fd
Dino Li <Dino.Li@ite.com.tw>
2022-07-04 12:03:38 +0800

    WIP: Nereid: gating unused modules' clock
    Change-Id: I0b021499503a3678ffe015e02e4b9480438b89f1

--- zephyr/projects/nissa/src/sub_board.c
+++ zephyr/projects/nissa/src/sub_board.c
@@ -109,6 +109,31 @@ static void soc_it8xxx2_disable_i2c4_alt(void)
 	pinctrl_apply_state(pcfg, PINCTRL_STATE_SLEEP);
 }
 #endif
+
+static void board_system_suspend_hooks(void)
+{
+	/* CPU clock is 24MHz */
+	IT8XXX2_ECPM_SCDCR0 = 1;
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, board_system_suspend_hooks, HOOK_PRIO_LAST);
+
+static void board_system_resume_hooks(void)
+{
+	/* CPU clock is 48MHz */
+	IT8XXX2_ECPM_SCDCR0 = 0;
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, board_system_resume_hooks, HOOK_PRIO_FIRST);
+
+static void board_clock_init(void)
+{
+	/* Disable unused modules' clock */
+	IT8XXX2_ECPM_CGCTRL2R = 0x10;
+	IT8XXX2_ECPM_CGCTRL3R = 0x5b;
+	IT8XXX2_ECPM_CGCTRL5R = 0x7a;
+	IT8XXX2_ECPM_CGCTRL6R = 0xe;
+}
+DECLARE_HOOK(HOOK_INIT, board_clock_init, HOOK_PRIO_DEFAULT);
+
 #endif /* CONFIG_SOC_IT8XXX2 */
 
 /**
