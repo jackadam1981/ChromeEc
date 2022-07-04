ebc6647ebb5f4348ffe714c3c75c640469415c49
Hank.Xie <hank.xie@quanta.corp-partner.google.com>
2022-07-04 07:38:01 -0400

    shotzo: Add fake battery info
    Shotzo doesn't have battery,
    but needs charger to convert type-c power to Vsys.
    
    Add a fake battery info to set charger voltage.
    
    BUG=none
    BRANCH=none
    TEST=plug type-c adapter, check it works fine on shotzo
    
    Signed-off-by: Hank.Xie <hank.xie@quanta.corp-partner.google.com>
    Change-Id: I44baaa9e90052cf963dd66623bcb8db0a2830bbb

--- board/shotzo/board.h
+++ board/shotzo/board.h
@@ -12,9 +12,6 @@
 #define VARIANT_DEDEDE_EC_IT8320
 #include "baseboard.h"
 
-/* Battery */
-#define CONFIG_BATTERY_FUEL_GAUGE
-
 /* Charger */
 #define CONFIG_CHARGER_SM5803 /* C0 and C1: Charger */
 #define PD_MAX_VOLTAGE_MV 15000
@@ -101,21 +98,6 @@ enum temp_sensor_id {
 	TEMP_SENSOR_3,
 	TEMP_SENSOR_4,
 	TEMP_SENSOR_COUNT
-};
-
-/* List of possible batteries */
-enum battery_type {
-	BATTERY_DYNAPACK_COS,
-	BATTERY_DYNAPACK_ATL,
-	BATTERY_DYNAPACK_HIGHPOWER,
-	BATTERY_DYNAPACK_BYD,
-	BATTERY_SAMSUNG_SDI,
-	BATTERY_SIMPLO_COS,
-	BATTERY_SIMPLO_HIGHPOWER,
-	BATTERY_COS,
-	BATTERY_COS_2,
-	BATTERY_ATL,
-	BATTERY_TYPE_COUNT,
 };
 
 enum charge_port {
