7086b663991ecf79a5fac8cfdd0210f5791d07ae
Ting Shen <phoenixshen@google.com>
2022-07-05 16:59:00 +0800

    charger/rt9490: fix incorrect unit in ADC measurement
    The units returned from charger is mV/mA, no need to multiply 1000.
    
    BUG=none
    TEST=manually checked datasheet
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: Icdb56294b6508f684a3d0eebeeadf786f5ef2086

--- driver/charger/rt9490.c
+++ driver/charger/rt9490.c
@@ -411,7 +411,7 @@ static enum ec_error_list rt9490_get_actual_current(int chgnum, int *current)
 	uint16_t reg_val;
 
 	RETURN_ERROR(rt9490_read16(chgnum, RT9490_REG_IBAT_ADC, &reg_val));
-	*current = (int)reg_val * 1000;
+	*current = (int)reg_val;
 	return EC_SUCCESS;
 }
 
@@ -420,7 +420,7 @@ static enum ec_error_list rt9490_get_actual_voltage(int chgnum, int *voltage)
 	uint16_t reg_val;
 
 	RETURN_ERROR(rt9490_read16(chgnum, RT9490_REG_VBAT_ADC, &reg_val));
-	*voltage = (int)reg_val * 1000;
+	*voltage = (int)reg_val;
 	return EC_SUCCESS;
 }
 
@@ -435,7 +435,7 @@ static enum ec_error_list rt9490_get_vbus_voltage(int chgnum, int port,
 	uint16_t reg_val;
 
 	RETURN_ERROR(rt9490_read16(chgnum, RT9490_REG_VBUS_ADC, &reg_val));
-	*voltage = (int)reg_val * 1000;
+	*voltage = (int)reg_val;
 	return EC_SUCCESS;
 }
 
