b6c674a168608f97c637ad188ee475829f7ae957
Tomasz Michalec <tm@semihalf.com>
2022-07-05 16:12:16 +0200

    zephyr: Support alternative usb_mux_chain config in DTS
    Add support for alternative usb_mux configuration from the devicetree.
    Alternate USB mux chain is created by adding "cros-ec,usb-mux-chain"
    in DTS with alternative-chain property.
    
    To enable alternate configuration, USB_MUX_ENABLE_ALTERNATE_NODE macro
    has to be used at runtime.
    
    BUG=b:234482311
    TEST=zmake testall
    BRANCH=none
    
    Signed-off-by: Tomasz Michalec <tm@semihalf.com>
    Change-Id: I0ae8889b225800d8741f01f8ea70c5f4f5547065

--- zephyr/shim/src/usb_muxes.c
+++ zephyr/shim/src/usb_muxes.c
@@ -42,7 +42,15 @@
 USB_MUX_FOREACH_CHAIN(USB_MUX_CHECK_ALL_PORTS_ARE_SAME)
 
 /**
- * Define usb_mux_chain structures e.g.
+ * Declare all usb_mux_chain structures e.g.
+ * MAYBE_CONST struct usb_mux_chain
+ * USB_MUX_chain_port_<port_id>_mux_<position_id>;
+ */
+USB_MUX_FOREACH_CHAIN_VARGS(USB_MUX_FOREACH_NO_FIRST_MUX,
+			    USB_MUX_CHAIN_STRUCT_DECLARE_OP)
+
+/**
+ * Define usb_mux_chain structures for main chain e.g.
  *
  * MAYBE_CONST struct usb_mux_chain
  * USB_MUX_chain_port_<port_id>_mux_<position_id> = {
@@ -50,7 +58,8 @@ USB_MUX_FOREACH_CHAIN(USB_MUX_CHECK_ALL_PORTS_ARE_SAME)
  *         .next = &USB_MUX_chain_port_0_mux_1,
  * }
  */
-USB_MUX_FOREACH_CHAIN_VARGS(USB_MUX_FOREACH_NO_FIRST_MUX,
+USB_MUX_FOREACH_CHAIN_VARGS(USB_MUX_FOR_MAIN_CHAIN,
+			    USB_MUX_FOREACH_NO_FIRST_MUX,
 			    USB_MUX_CHAIN_STRUCT_DEFINE_OP)
 
 /**
@@ -69,8 +78,8 @@ USB_MUX_FOREACH_MUX(USB_MUX_CB_BOARD_SET_DECLARE_IF_EXISTS)
  * },
  * [1] = { ... },
  */
-MAYBE_CONST struct usb_mux_chain usb_muxes[] = { USB_MUX_FOREACH_CHAIN(
-	USB_MUX_DEFINE_FIRST_MUX) };
+MAYBE_CONST struct usb_mux_chain usb_muxes[] = { USB_MUX_FOREACH_CHAIN_VARGS(
+	USB_MUX_FOR_MAIN_CHAIN, USB_MUX_DEFINE_FIRST_MUX) };
 BUILD_ASSERT(ARRAY_SIZE(usb_muxes) == CONFIG_USB_PD_PORT_MAX_COUNT);
 
 /**
