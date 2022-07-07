8ee43363c6712d907844e83e381857b892b9dac5
Hank.Xie <hank.xie@quanta.corp-partner.google.com>
2022-07-07 01:14:38 -0400

    shotzo: Remove unused feature 2
    Remove unused features as below.
    Gpio:
    EC_BATTERY_PRES_ODL
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Signed-off-by: Hank.Xie <hank.xie@quanta.corp-partner.google.com>
    Change-Id: I95a5d510c3c2584a87788357ec91dd95b302fd54

--- board/shotzo/board.c
+++ board/shotzo/board.c
@@ -672,3 +672,11 @@ const struct temp_sensor_t temp_sensors[] = {
 			   .idx = ADC_TEMP_SENSOR_4},
 };
 BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
+
+#ifndef CONFIG_BATTERY_PRESENT_GPIO
+enum battery_present battery_is_present(void)
+{
+	/* Shotzo has no battery, so always return no battery present. */
+	return BP_NO;
+}
+#endif
