8ef8f90be2f7451128ee4f62471b671f66839188
Peter Marheine <pmarheine@chromium.org>
2022-07-07 12:45:51 +1000

    sm5803: slow clocks last when enabling LPM
    Silicon Mitus say that slowing clocks before disabling GPADCs can cause
    spurious readings, which may cause unexpected behavior. We don't
    currently disable GPADCs, but in the same spirit change the order of
    operations to slow the clocks last when going into low-power mode.
    
    BUG=b:168591511,b:237697900
    TEST=sinking and sourcing still work on Nereid and ports enter runtime
         LPM as expected.
    BRANCH=none
    
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Change-Id: Ideac273cf39b6d1b13cc8336701774595e49df41

--- driver/charger/sm5803.c
+++ driver/charger/sm5803.c
@@ -961,10 +961,6 @@ static void sm5803_enable_runtime_low_power_mode(void)
 			chgnum);
 		return;
 	}
-	/* Slow the clock speed */
-	rv |= main_read8(chgnum, SM5803_REG_CLOCK_SEL, &reg);
-	reg |= SM5803_CLOCK_SEL_LOW;
-	rv |= main_write8(chgnum, SM5803_REG_CLOCK_SEL, reg);
 
 	/* Disable ADC sigma delta */
 	rv |= chg_read8(chgnum, SM5803_REG_CC_CONFIG1, &reg);
@@ -978,6 +974,11 @@ static void sm5803_enable_runtime_low_power_mode(void)
 		reg &= ~SM5803_PHOT1_COMPARATOR_EN;
 		rv |= chg_write8(chgnum, SM5803_REG_PHOT1, reg);
 	}
+
+	/* Slow the clock speed */
+	rv |= main_read8(chgnum, SM5803_REG_CLOCK_SEL, &reg);
+	reg |= SM5803_CLOCK_SEL_LOW;
+	rv |= main_write8(chgnum, SM5803_REG_CLOCK_SEL, reg);
 
 	if (rv)
 		CPRINTS("%s %d: Failed to set in enable runtime LPM",
