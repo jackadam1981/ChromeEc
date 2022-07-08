c7e1c8deb5ce7739e04a77ae45302d68be79831d
madhurimaparuchuri <mparuchuri@google.com>
2022-07-08 16:13:11 +0530

    zephyr: shim: fan: Make fan driver use 'period' from PWM spec inplace of 'pwm-frequency'
    Update fan driver to use 'period' from PWM spec inplace of
    'pwm-frequency' and remove 'pwm-frequency' from device tree
    
    BUG=b:230093078
    BRANCH=none
    TEST=zmake testall
    
    Signed-off-by: Madhurima Paruchuri <mparuchuri@google.com>
    Change-Id: Icc56287133002731df8af4d72b455d4db2401a08

--- zephyr/shim/src/fan.c
+++ zephyr/shim/src/fan.c
@@ -56,7 +56,7 @@ BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) == 1,
 		.pwm = DEVICE_DT_GET(DT_PWMS_CTLR(node_id)),                   \
 		.channel = DT_PWMS_CHANNEL(node_id),                           \
 		.flags = DT_PWMS_FLAGS(node_id),                               \
-		.period_ns = (NSEC_PER_SEC / DT_PROP(node_id, pwm_frequency)), \
+		.period_ns = (NSEC_PER_SEC / DT_PWMS_PERIOD(node_id)), \
 		.tach = DEVICE_DT_GET(DT_PHANDLE(node_id, tach)),              \
 	},
 
