8378350839e3b0e1fe9f7d38e3dca3d629989705
Al Semjonovs <asemjonovs@google.com>
2022-07-11 10:12:53 -0600

    WIP: Add GPIO DT spec to button config struct
    BUG=b:234728861
    BRANCH=none
    TEST=zmake test test-drivers
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: I90999a00cb26d42a31f26d260ea3ddd52459838a

--- zephyr/shim/include/button_config.h
+++ zephyr/shim/include/button_config.h
@@ -7,6 +7,9 @@
 #define __BUTTON_CONFIG_H
 
 #include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+
+#include "include/ec_commands.h"
 
 #define BUTTON_CFG_COMPAT cros_ec_button_cfg
 #define DT_BUTTON_CFG_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(BUTTON_CFG_COMPAT)
@@ -22,6 +25,72 @@ enum button_cfg_type {
 		BUTTON_CFG_ENUM(COUNT),
 };
 
-const struct button_config *get_button_cfg(enum button_cfg_type type);
+#define BUTTON_FLAG_ACTIVE_HIGH BIT(0)
+#define BUTTON_FLAG_DISABLED BIT(1) /* Button disabled */
+
+struct button_config_v2 {
+	const char *name;
+	enum keyboard_button_type type;
+	uint32_t debounce_us;
+	uint8_t button_flags;
+	enum gpio_signal gpio;
+	const struct gpio_dt_spec spec;
+	void (*gpio_int_handler)(enum gpio_signal);
+	gpio_flags_t gpio_int_flags;
+};
+
+/**
+ * @brief Get the button cfg
+ *
+ * @param type
+ * @return const struct button_config_v2*
+ */
+const struct button_config_v2 *button_cfg_get(enum button_cfg_type type);
+
+/**
+ * @brief Get button name
+ *
+ * @param type
+ * @return const char*
+ */
+const char *button_get_name(enum button_cfg_type type);
+
+/**
+ * @brief Get button debounce time in microseconds
+ *
+ * @param type
+ * @return int
+ */
+int button_get_debounce_us(enum button_cfg_type type);
+
+/**
+ * @brief Enable interrupt for button
+ *
+ * @param type
+ */
+int button_enable_interrupt(enum button_cfg_type type);
+
+/**
+ * @brief Disable interrupt for button
+ *
+ * @param type
+ */
+int button_disable_interrupt(enum button_cfg_type type);
+
+/**
+ * @brief Get the logical level of button press
+ *
+ * @param type
+ * @return int
+ */
+int button_is_pressed(enum button_cfg_type type);
+
+/**
+ * @brief Get the physical level of button press
+ *
+ * @param type
+ * @return int
+ */
+int button_is_pressed_raw(enum button_cfg_type type);
 
 #endif /* __BUTTON_CONFIG_H */
--- zephyr/shim/src/power_button.c
+++ zephyr/shim/src/power_button.c
@@ -7,16 +7,15 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#include "button_config.h"
+#include "hooks.h"
+#include "host_command.h"
 #include "include/button.h"
-#include "gpio.h"
-#include "gpio/gpio.h"
+#include "keyboard_scan.h"
+#include "lid_switch.h"
+#include "task.h"
 
-static const struct button_config power_button = {
-	.name = "power button",
-	.gpio = GPIO_POWER_BUTTON_L,
-	.debounce_us = BUTTON_DEBOUNCE_US,
-	.flags = CONFIG_POWER_BUTTON_FLAGS,
-};
+LOG_MODULE_REGISTER(power_button, LOG_LEVEL_ERR);
 
 static int debounced_power_pressed; /* Debounced power button state */
 static int simulate_power_pressed;
@@ -24,11 +23,66 @@ static volatile int power_button_is_stable = 1;
 
 int power_button_signal_asserted(void)
 {
-	/* TODO
-	 * return !!(gpio_get_level(power_button.gpio)
-	 *	== (power_button.flags & BUTTON_FLAG_ACTIVE_HIGH) ? 1 : 0);
+	const struct button_config_v2 *cfg =
+		button_cfg_get(BUTTON_CFG_POWER_BUTTON);
+	const int is_active_high =
+		((cfg->button_flags & BUTTON_FLAG_ACTIVE_HIGH) ? 1 : 0);
+
+	return button_is_pressed_raw(BUTTON_CFG_POWER_BUTTON) == is_active_high;
+}
+
+static int raw_power_button_pressed(void)
+{
+	if (simulate_power_pressed)
+		return 1;
+
+#ifndef CONFIG_POWER_BUTTON_IGNORE_LID
+	/*
+	 * Always indicate power button released if the lid is closed.
+	 * This prevents waking the system if the device is squashed enough to
+	 * press the power button through the closed lid.
 	 */
-	return 0;
+	if (!lid_is_open())
+		return 0;
+#endif
+
+	return power_button_signal_asserted();
+}
+
+int power_button_is_pressed(void)
+{
+	return debounced_power_pressed;
+}
+
+int power_button_wait_for_release(int timeout_us)
+{
+	timestamp_t deadline;
+	timestamp_t now = get_time();
+
+	deadline.val = now.val + timeout_us;
+
+	while (!power_button_is_stable || power_button_is_pressed()) {
+		now = get_time();
+		if (timeout_us >= 0 && timestamp_expired(deadline, &now)) {
+			LOG_INF("%s not released in time",
+				button_get_name(BUTTON_CFG_POWER_BUTTON));
+			return EC_ERROR_TIMEOUT;
+		}
+		/*
+		 * We use task_wait_event() instead of usleep() here. It will
+		 * be woken up immediately if the power button is debouned and
+		 * changed. However, it is not guaranteed, like the cases that
+		 * the power button is debounced but not changed, or the power
+		 * button has not been debounced.
+		 */
+		task_wait_event(
+			MIN(button_get_debounce_us(BUTTON_CFG_POWER_BUTTON),
+			    deadline.val - now.val));
+	}
+
+	LOG_INF("%s released in time",
+		button_get_name(BUTTON_CFG_POWER_BUTTON));
+	return EC_SUCCESS;
 }
 
 /**
@@ -36,11 +90,98 @@ int power_button_signal_asserted(void)
  */
 static void power_button_init(void)
 {
-	/* TODO
-	 *	if (raw_power_button_pressed())
-	 *	debounced_power_pressed = 1;
-	 *
-	 *	Enable interrupts, now that we've initialized
-	 * gpio_enable_interrupt(power_button.gpio);
-	 * /
+	if (raw_power_button_pressed())
+		debounced_power_pressed = 1;
+
+	/* Enable interrupts, now that we've initialized */
+	button_enable_interrupt(BUTTON_CFG_POWER_BUTTON);
 }
+
+DECLARE_HOOK(HOOK_INIT, power_button_init, HOOK_PRIO_INIT_POWER_BUTTON);
+
+/**
+ * Handle debounced power button changing state.
+ */
+static void power_button_change_deferred(void)
+{
+	const int new_pressed = raw_power_button_pressed();
+
+	/* Re-enable keyboard scanning if power button is no longer pressed */
+	if (!new_pressed)
+		keyboard_scan_enable(1, KB_SCAN_DISABLE_POWER_BUTTON);
+
+	/* If power button hasn't changed state, nothing to do */
+	if (new_pressed == debounced_power_pressed) {
+		power_button_is_stable = 1;
+		return;
+	}
+
+	debounced_power_pressed = new_pressed;
+	power_button_is_stable = 1;
+
+	LOG_INF("%s %s", button_get_name(BUTTON_CFG_POWER_BUTTON),
+		new_pressed ? "pressed" : "released");
+
+	/* Call hooks */
+	hook_notify(HOOK_POWER_BUTTON_CHANGE);
+
+	/* Notify host if power button has been pressed */
+	if (new_pressed)
+		host_set_single_event(EC_HOST_EVENT_POWER_BUTTON);
+}
+DECLARE_DEFERRED(power_button_change_deferred);
+
+void power_button_interrupt(enum gpio_signal signal)
+{
+	ARG_UNUSED(signal);
+	/*
+	 * If power button is pressed, disable the matrix scan as soon as
+	 * possible to reduce the risk of false-reboot triggered by those keys
+	 * on the same column with refresh key.
+	 */
+	if (raw_power_button_pressed())
+		keyboard_scan_enable(0, KB_SCAN_DISABLE_POWER_BUTTON);
+
+	/* Reset power button debounce time */
+	power_button_is_stable = 0;
+	hook_call_deferred(&power_button_change_deferred_data,
+			   button_get_debounce_us(BUTTON_CFG_POWER_BUTTON));
+}
+
+void power_button_simulate_press(unsigned int duration)
+{
+	LOG_INF("Simulating %d ms %s press.\n", duration,
+		button_get_name(BUTTON_CFG_POWER_BUTTON));
+	simulate_power_pressed = 1;
+	power_button_is_stable = 0;
+	hook_call_deferred(&power_button_change_deferred_data, 0);
+
+	if (duration > 0)
+		msleep(duration);
+
+	LOG_INF("Simulating %s release.\n",
+		button_get_name(BUTTON_CFG_POWER_BUTTON));
+	simulate_power_pressed = 0;
+	power_button_is_stable = 0;
+	hook_call_deferred(&power_button_change_deferred_data, 0);
+}
+
+/*****************************************************************************/
+/* Console commands */
+
+static int command_powerbtn(int argc, const char **argv)
+{
+	int ms = 200; /* Press duration in ms */
+	char *e;
+
+	if (argc > 1) {
+		ms = strtoi(argv[1], &e, 0);
+		if (*e || ms < 0)
+			return EC_ERROR_PARAM1;
+	}
+
+	power_button_simulate_press(ms);
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(powerbtn, command_powerbtn, "[msec]",
+			"Simulate power button press");
