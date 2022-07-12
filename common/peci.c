4000ad45d5125ad5e869b4b1830883f7a1d77483
Tom Hughes <tomhughes@chromium.org>
2022-07-12 10:01:23 -0700

    tree: Remove non-standard "%ph" printf format
    The non-standard "%ph" format is replaced with snprintf_hex_buffer and
    then using "%s" to print the resulting buffer.
    
    Using standard format specifiers makes it easier to switch between the
    "builtin" EC standard library and the C standard library provided by the
    toolchain (or Zephyr).
    
    BRANCH=none
    BUG=b:238433667, b:234181908
    TEST=Enable CONFIG_CMD_RAND in nocturne_fp/board.h
         On icetower v0.1 with servo_micro and J-Trace:
    
         Before change:
         > rand
         rand 8ab8b15090ca5ae83bdad671c906d51a5f2b98a359a4106054ee6b54a4087190
    
         After change:
         > rand
         rand 2a8645235a31936a28b8d1b9c4948f46d39662e7fcb10a185ddb14c6a998e2eb
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I3bff928d32579440d7cdb27a75899e45159accfb

--- common/peci.c
+++ common/peci.c
@@ -140,7 +140,6 @@ static int peci_cmd(int argc, char **argv)
 		ccprintf("PECI transaction error\n");
 		return EC_ERROR_UNKNOWN;
 	}
-	ccprintf("PECI read data: %ph\n", HEX_BUF(r_buf, peci.r_len));
 	return EC_SUCCESS;
 }
 DECLARE_CONSOLE_COMMAND(peci, peci_cmd, "addr wlen rlen cmd timeout(us)",
