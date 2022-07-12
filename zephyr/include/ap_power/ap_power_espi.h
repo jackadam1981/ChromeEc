f41ea0711a148b7d8c2f148cb070d46f1a9e72f7
Andrew McRae <amcrae@google.com>
2022-07-12 12:44:16 +1000

    ap_pwrseq: Use common ESPI init and callback
    Use common ESPI init and callback for power signals ESPI callback
    so that signal changes are guaranteed to be received by
    the power signal VW handler.
    
    BUG=b:237449433
    TEST=zmake build nivviks; flash & run
    BRANCH=none
    
    Signed-off-by: Andrew McRae <amcrae@google.com>
    Change-Id: I1dfbc45bd1944ab124bec42897617463190b6853

---
+++ zephyr/include/ap_power/ap_power_espi.h
@@ -0,0 +1,32 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * @file
+ * @brief API for power signal ESPI callback.
+ */
+
+#ifndef __AP_POWER_AP_POWER_ESPI_H__
+#define __AP_POWER_AP_POWER_ESPI_H__
+
+#include <zephyr/drivers/espi.h>
+
+/**
+ * @brief ESPI callback for power signal handling.
+ *
+ * @param dev ESPI device
+ * @param cb Callback structure
+ * @param event ESPI event data
+ */
+void power_signal_espi_cb(const struct device *dev, struct espi_callback *cb,
+			  struct espi_event event);
+
+/*
+ * The ESPI bus events required for the power signal ESPI callback.
+ */
+#define POWER_SIGNAL_ESPI_BUS_EVENTS \
+	(ESPI_BUS_EVENT_CHANNEL_READY | ESPI_BUS_EVENT_VWIRE_RECEIVED)
+
+#endif /* __AP_POWER_AP_POWER_ESPI_H__ */
