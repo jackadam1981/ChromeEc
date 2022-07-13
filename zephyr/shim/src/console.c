e7432bda9f01701b97722abbaa02ed110023323b
Tom Hughes <tomhughes@chromium.org>
2022-07-13 11:55:16 -0700

    zephyr: console: Switch to standard vsnprintf and snprintf
    The custom formats ("%pT", "%pb", "%ph", and "%pP") have been removed
    from the EC code, so we can use Zephyr's vsnprintf instead of the EC
    version.
    
    BRANCH=none
    BUG=b:238433667, b:234181908
    TEST=zmake build -a
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Ife8188ec1156e88c87d4a456c1e8549d47df443f

--- zephyr/shim/src/console.c
+++ zephyr/shim/src/console.c
@@ -10,6 +10,7 @@
 #include <zephyr/shell/shell_dummy.h> /* nocheck */
 #endif
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 
 #include <zephyr/kernel.h>
@@ -18,12 +19,6 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/sys/ring_buffer.h>
 
-/*
- * TODO(b/238433667): Include EC printf functions
- * (crec_vsnprintf/crec_snprintf) until we switch to the standard
- * vsnprintf/snprintf.
- */
-#include "builtin/stdio.h"
 #include "console.h"
 #include "printf.h"
 #include "task.h"
@@ -432,7 +427,7 @@ int cprintf(enum console_channel channel, const char *format, ...)
 		return EC_SUCCESS;
 
 	va_start(args, format);
-	rv = crec_vsnprintf(buff, CONFIG_SHELL_PRINTF_BUFF_SIZE, format, args);
+	rv = vsnprintf(buff, CONFIG_SHELL_PRINTF_BUFF_SIZE, format, args);
 	va_end(args);
 	handle_sprintf_rv(rv, &len);
 
@@ -463,13 +458,12 @@ int cprints(enum console_channel channel, const char *format, ...)
 	handle_sprintf_rv(rv, &len);
 
 	va_start(args, format);
-	rv = crec_vsnprintf(buff + len, CONFIG_SHELL_PRINTF_BUFF_SIZE - len,
-			    format, args);
+	rv = vsnprintf(buff + len, CONFIG_SHELL_PRINTF_BUFF_SIZE - len, format,
+		       args);
 	va_end(args);
 	handle_sprintf_rv(rv, &len);
 
-	rv = crec_snprintf(buff + len, CONFIG_SHELL_PRINTF_BUFF_SIZE - len,
-			   "]\n");
+	rv = snprintf(buff + len, CONFIG_SHELL_PRINTF_BUFF_SIZE - len, "]\n");
 	handle_sprintf_rv(rv, &len);
 
 	zephyr_print(buff, len);
