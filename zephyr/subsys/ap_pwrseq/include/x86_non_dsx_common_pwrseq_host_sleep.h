5f1be45a248a1ea17577aefaf5846149fc26a16e
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-07-13 12:17:14 -0700

    zephyr: ap_pwrseq: Add S0IX error recovery support
    This CL supports s0ix error recovery to ensure the AP doesn't get itself
    stuck in a state where it's no longer in a sleep state (S0ix/S3), but
    from the Linux perspective is still suspended.
    
    BUG=none
    BRANCH=none
    TEST=zmake build nivviks
         Tested using 'powerd_dbus_suspend' on AP and observed timeout and
         hang detected through EC logs.
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: Iaf37b2afc7e1bdec251b9c21f6a3f131acab6a50

---
+++ zephyr/subsys/ap_pwrseq/include/x86_non_dsx_common_pwrseq_host_sleep.h
@@ -0,0 +1,27 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __X86_NON_DSX_COMMON_PWRSEQ_HOST_SLEEP_H__
+#define __X86_NON_DSX_COMMON_PWRSEQ_HOST_SLEEP_H__
+
+#include <ap_power_host_sleep.h>
+
+#ifdef CONFIG_AP_PWRSEQ_S0IX_ERROR_RECOVERY
+
+/**
+ * Handles if sleep hang is detected.
+ *
+ * It is called in sleep_transition_timeout(struct k_work *work), once timeout
+ * happens after sleep hang is detected.
+ *
+ *
+ * @param hang_type- type of hang detected.
+ */
+void sleep_start_suspend(struct host_sleep_event_context *ctx);
+
+void power_chipset_handle_sleep_hang(enum sleep_hang_type hang_type);
+#endif /* !CONFIG_AP_PWRSEQ_S0IX_ERROR_RECOVERY */
+
+#endif /* __X86_NON_DSX_COMMON_PWRSEQ_HOST_SLEEP_H__ */
--- zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_host_command.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_host_command.c
@@ -5,6 +5,9 @@
 
 #include <x86_non_dsx_common_pwrseq_sm_handler.h>
 
+/* Track last reported sleep event */
+static enum host_sleep_event host_sleep_state;
+
 /* Host commands */
 static enum ec_status
 host_command_reboot_ap_on_g3(struct host_cmd_handler_args *args)
@@ -29,5 +32,64 @@ host_command_reboot_ap_on_g3(struct host_cmd_handler_args *args)
 }
 DECLARE_HOST_COMMAND(EC_CMD_REBOOT_AP_ON_G3, host_command_reboot_ap_on_g3,
 		     EC_VER_MASK(0) | EC_VER_MASK(1));
+
+static enum ec_status
+host_command_host_sleep_event(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_host_sleep_event_v1 *p = args->params;
+	struct ec_response_host_sleep_event_v1 *r = args->response;
+	struct host_sleep_event_context ctx;
+	enum host_sleep_event state = p->sleep_event;
+
+	host_sleep_state = state;
+	ctx.sleep_transitions = 0;
+	switch (state) {
+	case HOST_SLEEP_EVENT_S0IX_SUSPEND:
+	case HOST_SLEEP_EVENT_S3_SUSPEND:
+	case HOST_SLEEP_EVENT_S3_WAKEABLE_SUSPEND:
+		ctx.sleep_timeout_ms = EC_HOST_SLEEP_TIMEOUT_DEFAULT;
+
+		/* The original version contained only state. */
+		if (args->version >= 1)
+			ctx.sleep_timeout_ms =
+				p->suspend_params.sleep_timeout_ms;
+
+		break;
+
+	default:
+		break;
+	}
+
+	ap_power_chipset_handle_host_sleep_event(host_sleep_state, &ctx);
+	switch (state) {
+	case HOST_SLEEP_EVENT_S0IX_RESUME:
+	case HOST_SLEEP_EVENT_S3_RESUME:
+		if (args->version >= 1) {
+			r->resume_response.sleep_transitions =
+				ctx.sleep_transitions;
+
+			args->response_size = sizeof(*r);
+		}
+
+		break;
+
+	default:
+		break;
+	}
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_HOST_SLEEP_EVENT, host_command_host_sleep_event,
+		     EC_VER_MASK(0) | EC_VER_MASK(1));
+
+enum host_sleep_event power_get_host_sleep_state(void)
+{
+	return host_sleep_state;
+}
+
+void power_set_host_sleep_state(enum host_sleep_event state)
+{
+	host_sleep_state = state;
+}
 
 /* End of host commands */
