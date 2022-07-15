cee0b8a534e5b7f042fa15246c610f81a9ce20d7
Andrea Grandi <agrandi@google.com>
2022-07-15 13:11:03 -0700

    run_device_tests.py: Update disabled linter options
    The latest version of Pylint introduced new checkers that are flagging
    errors in lines of code where Pylint errors had already been disabled.
    Update pylint disable directives to suppress the new errors.
    
    BUG=b:239100048
    TEST=cros lint test/run_device_tests.py
    BRANCH=none
    
    Signed-off-by: Andrea Grandi <agrandi@google.com>
    Change-Id: I6d2dd2f1660cb34f2bff0a1ed073825b80f5da6a

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -298,7 +298,7 @@ class AllTests:
             if not have_private:
                 return []
             sys.path.append(private_dir)
-            import private_tests  # pylint: disable=import-error
+            import private_tests  # pylint: disable=import-error,import-outside-toplevel
 
             for test_args in private_tests.tests:
                 tests.append(TestConfig(**test_args))
@@ -356,9 +356,10 @@ def read_file_gsutil(path: str) -> bytes:
     cmd = ["gsutil", "cat", path]
 
     logging.debug('Running command: "%s"', " ".join(cmd))
+    # pylint: disable=subprocess-run-check
     gsutil = subprocess.run(
         cmd, stdout=subprocess.PIPE
-    )  # pylint: disable=subprocess-run-check
+    )
     gsutil.check_returncode()
 
     return gsutil.stdout
