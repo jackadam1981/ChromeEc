ce42879e2c997c381529e272e6acae0db0076504
Andrea Grandi <agrandi@google.com>
2022-07-15 12:52:18 -0700

    run_device_tests.py: Add missing docstrings
    Pylint reports several missing docstring errors:
    C0116: Missing function or method docstring (missing-function-docstring)
    
    Add documentation where required.
    
    BUG=b:239100048
    TEST=cros lint test/run_device_tests.py
    BRANCH=none
    
    Signed-off-by: Andrea Grandi <agrandi@google.com>
    Change-Id: I006cd21faf3e149b8fb76a49e644afd16db316e2

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -176,6 +176,7 @@ class AllTests:
 
     @staticmethod
     def get(board_config: BoardConfig) -> List[TestConfig]:
+        """Return public and private test configs for the specified board"""
         public_tests = AllTests.get_public_tests(board_config)
         private_tests = AllTests.get_private_tests()
 
@@ -183,6 +184,7 @@ class AllTests:
 
     @staticmethod
     def get_public_tests(board_config: BoardConfig) -> List[TestConfig]:
+        """Return public test configs for the specified board"""
         tests = [
             TestConfig(test_name="aes"),
             TestConfig(test_name="cec"),
@@ -287,7 +289,7 @@ class AllTests:
 
     @staticmethod
     def get_private_tests() -> List[TestConfig]:
-        # Return all private tests, if the folder exists
+        """Return private test configs for the specified board, if available"""
         tests = []
         try:
             current_dir = os.path.dirname(__file__)
@@ -547,6 +549,7 @@ def readlines_until_timeout(executor, f: BinaryIO, timeout_secs: int) -> List[by
 
 
 def process_console_output_line(line: bytes, test: TestConfig):
+    """Parse console output line and update test pass/fail counters"""
     try:
         line_str = line.decode()
 
@@ -638,6 +641,7 @@ def get_test_list(config: BoardConfig, test_args) -> List[TestConfig]:
 
 
 def parse_remote_arg(remote: str) -> str:
+    """Convert the 'remote' input argument to IP address, if available"""
     if not remote:
         return ""
 
@@ -650,6 +654,7 @@ def parse_remote_arg(remote: str) -> str:
 
 
 def main():
+    """Run unit tests on device and displays the results"""
     parser = argparse.ArgumentParser()
 
     default_board = "bloonchipper"
