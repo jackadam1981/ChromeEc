5ab06940d46d9dae6e4bc9a4edaed3cc57ecbbd9
Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
2022-07-15 15:05:13 +0800

    moli: add power on/wakeup by HDMI monitor
    After EC received interrupt from monitor on GPIOB4/ GPIOB5/ GPIO10. EC
    will power on the system.
    
    If EC received interrupt during s0ix, EC will trigger powr button to
    wake AP.
    
    BUG=b:239634608
    BRANCH=none
    TEST=make BOARD=moli
    
    Signed-off-by: Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
    Change-Id: Ia407468afc103595d33b6ce526d8e05bc66c76a2

--- board/moli/board.c
+++ board/moli/board.c
@@ -8,6 +8,7 @@
 #include "button.h"
 #include "charge_manager.h"
 #include "charge_state_v2.h"
+#include "chipset.h"
 #include "common.h"
 #include "compile_time_macros.h"
 #include "console.h"
@@ -198,6 +199,9 @@ static void board_init(void)
 	gpio_enable_interrupt(GPIO_USB_A2_OC_ODL);
 	gpio_enable_interrupt(GPIO_USB_A3_OC_ODL);
 	gpio_enable_interrupt(GPIO_USB_A4_OC_ODL);
+	gpio_enable_interrupt(GPIO_HDMI1_MONITOR_ON);
+	gpio_enable_interrupt(GPIO_HDMI2_MONITOR_ON);
+	gpio_enable_interrupt(GPIO_OPTION_MONITOR_ON);
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
@@ -436,6 +440,14 @@ DECLARE_HOOK(HOOK_INIT, power_monitor, HOOK_PRIO_INIT_ADC + 1);
 /******************************************************************************/
 /*
  * System power on and wake up by monitor power button.
+ *
+ * After pressing power button of monitor for power on, monitor will send power
+ * on signal with 3.3V / 200ms to DT. If DT detect that pulse, there are three
+ * DT behavior:
+ *
+ *  - Do nothing in state S0.
+ *  - Wake up from state S0ix.
+ *  - Power on from state S5 and G3.
  */
 uint8_t ec_config_power_on_by_monitor(void)
 {
@@ -445,3 +457,77 @@ uint8_t ec_config_power_on_by_monitor(void)
 
 	return (fw_config & BIT(EC_CFG_POWER_ON_BY_MONITOR)) ? true : false;
 };
+
+/* Debounce time for HDMI power button press */
+#define MONITOR_DEBOUNCE_MS 100
+
+static void monitor_irq_deferred(void);
+DECLARE_DEFERRED(monitor_irq_deferred);
+
+struct monitor_config {
+	enum gpio_signal gpio;
+	uint8_t state;
+};
+
+static struct monitor_config monitors[MONITOR_COUNT] = {
+	[HDMI1_MONITOR] = {
+		.gpio = GPIO_HDMI1_MONITOR_ON,
+		.state = MONITOR_OFF,
+	},
+
+	[HDMI2_MONITOR] = {
+		.gpio = GPIO_HDMI2_MONITOR_ON,
+		.state = MONITOR_OFF,
+	},
+
+	[OPTION_MONITOR] = {
+		.gpio = GPIO_OPTION_MONITOR_ON,
+		.state = MONITOR_OFF,
+	},
+};
+
+static void monitor_irq_deferred(void)
+{
+	int i;
+
+	for (i = 0; i < MONITOR_COUNT; i++) {
+		if (monitors[i].state && gpio_get_level(monitors[i].gpio)) {
+			/*
+			 * System power on from state S5 and G3.
+			 */
+			if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+				chipset_power_on();
+			/*
+			 * System wake up from state S0ix.
+			 */
+			else if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND)) {
+				power_button_pch_press();
+				msleep(200);
+				power_button_pch_release();
+			}
+		}
+		monitors[i].state = MONITOR_OFF;
+	}
+}
+
+/* Power on by HDMI monitor. */
+void monitor_interrupt(enum gpio_signal signal)
+{
+	if (ec_config_power_on_by_monitor() == POWER_ON_BY_MONITOR_ENABLE) {
+		switch (signal) {
+		case GPIO_HDMI1_MONITOR_ON:
+			monitors[HDMI1_MONITOR].state = MONITOR_ON;
+			break;
+		case GPIO_HDMI2_MONITOR_ON:
+			monitors[HDMI2_MONITOR].state = MONITOR_ON;
+			break;
+		case GPIO_OPTION_MONITOR_ON:
+			monitors[OPTION_MONITOR].state = MONITOR_ON;
+			break;
+		default:
+			break;
+		}
+		hook_call_deferred(&monitor_irq_deferred_data,
+				   MONITOR_DEBOUNCE_MS * MSEC);
+	}
+}
