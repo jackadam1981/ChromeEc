37b12ac82882670e804adf386c0b5f04deb5ffd7
Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
2022-07-15 15:05:13 +0800

    moli: add power on/wakeup by HDMI monitor
    After EC received interrupt from monitor on GPIOB4/ GPIOB5/ GPIO10. EC
    will power on the system.
    
    If EC received interrupt during s0ix, EC will trigger powr button to
    wake AP.
    
    BUG=b:239634608
    BRANCH=none
    TEST=make BOARD=moli
    
    Signed-off-by: Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
    Change-Id: Ia407468afc103595d33b6ce526d8e05bc66c76a2

--- board/moli/board.h
+++ board/moli/board.h
@@ -181,6 +181,19 @@ enum fan_channel { FAN_CH_0 = 0, FAN_CH_COUNT };
 
 enum mft_channel { MFT_CH_0 = 0, MFT_CH_COUNT };
 
+enum monitor_port {
+	HDMI1_MONITOR,
+	HDMI2_MONITOR,
+	OPTION_MONITOR,
+	MONITOR_COUNT
+};
+
+enum monitor_state { MONITOR_OFF, MONITOR_ON };
+
+enum ec_config_power_on_hdmi {
+	POWER_ON_HDMI_ENABLE = 0,
+	POWER_ON_HDMI_DISABLE = 1
+};
 uint8_t ec_config_power_on_hdmi(void);
 
 uint8_t ec_config_power_on_typec(void);
@@ -200,6 +213,7 @@ uint8_t ec_config_power_on_typec(void);
 #define EC_CFG_POWER_ON_TYPEC 8
 
 extern void adp_connect_interrupt(enum gpio_signal signal);
+extern void monitor_interrupt(enum gpio_signal signal);
 
 #endif /* !__ASSEMBLER__ */
 
