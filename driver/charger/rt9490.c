7dad43b0ec44d09a590ebd1124b48a2ed0c7907a
Tom Hughes <tomhughes@chromium.org>
2022-07-18 16:04:13 -0700

    tree: Remove "builtin" prefix
    If using the toolchain's standard library, we want to include the
    toolchain's standard library headers, not the EC "builtin" version. The
    "builtin" directory is automatically to the include path when compiling
    with the EC "builtin" standard library, so this format works for both
    cases.
    
    BRANCH=none
    BUG=b:234181908
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Icdaf7427e2aa76254cba48c9ff2e0dd3054e561a

--- driver/charger/rt9490.c
+++ driver/charger/rt9490.c
@@ -5,10 +5,11 @@
  * Richtek 5A 1-4 cell buck-boost switching battery charger driver.
  */
 
+#include <endian.h>
+
 #include "battery.h"
 #include "battery_smart.h"
 #include "builtin/assert.h"
-#include "builtin/endian.h"
 #include "charger.h"
 #include "charge_manager.h"
 #include "common.h"
