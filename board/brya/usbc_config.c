a2cf835cdbabdb7a1d22e994f6300e7dd1c46430
Boris Mittelberg <bmbm@google.com>
2022-07-19 16:56:09 -0700

    brya/skolas: don't reset nct38xx on reboot
    Fix TCPC state on dead/absent battery startup by removing extra reset
    
    BRANCH=none
    BUG=b:221532258
    TEST=manual test on brya and skolas:
     - exit shipping mode on C0/C2
     - boot without battery
     - check Role Control register on boot
    
    Signed-off-by: Boris Mittelberg <bmbm@google.com>
    Change-Id: Iac695294b2cd76b9a791db41b33d7ad2778edc82

--- board/brya/usbc_config.c
+++ board/brya/usbc_config.c
@@ -309,19 +309,10 @@ __override int bb_retimer_power_enable(const struct usb_mux *me, bool enable)
 
 void board_reset_pd_mcu(void)
 {
-	enum gpio_signal tcpc_rst;
+	/* Reset the NCT38xx boot state (not resetting the chip) */
+	nct38xx_reset_notify(USBC_PORT_C0);
+	nct38xx_reset_notify(USBC_PORT_C2);
 
-#ifndef CONFIG_ZEPHYR
-	tcpc_rst = GPIO_USB_C0_C2_TCPC_RST_ODL;
-#else
-	tcpc_rst = GPIO_UNIMPLEMENTED;
-#endif /* !CONFIG_ZEPHYR */
-
-	/*
-	 * TODO(b/179648104): figure out correct timing
-	 */
-
-	gpio_set_level(tcpc_rst, 0);
 	if (ec_cfg_usb_db_type() != DB_USB_ABSENT) {
 		gpio_set_level(GPIO_USB_C1_RST_ODL, 0);
 		gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 0);
@@ -333,7 +324,6 @@ void board_reset_pd_mcu(void)
 
 	msleep(20);
 
-	gpio_set_level(tcpc_rst, 1);
 	if (ec_cfg_usb_db_type() != DB_USB_ABSENT) {
 		gpio_set_level(GPIO_USB_C1_RST_ODL, 1);
 		gpio_set_level(GPIO_USB_C1_RT_RST_R_ODL, 1);
