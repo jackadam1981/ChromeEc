378d76fb251d4f80c73dc764226cc00032e2a87b
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-07-19 13:52:28 +0800

    moli: support power on by customized type-c monitor power button
    BUG=b:239767485, b:241207167
    BRANCH=none
    TEST=power on by press type-c monitor power button
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I38d1ae3ef9d0a35fde25543aab2592c66d6054d8

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -6187,6 +6187,9 @@ static void pe_vdm_response_entry(int port)
 	uint32_t *tx_payload;
 	uint8_t vdo_cmd;
 	svdm_rsp_func func = NULL;
+#ifdef USB_VID_USBC_MONITOR
+	int is_customized_vdm;
+#endif
 
 	print_current_state(port);
 
@@ -6273,6 +6276,14 @@ static void pe_vdm_response_entry(int port)
 		return;
 #endif
 	default:
+#ifdef USB_VID_USBC_MONITOR
+		is_customized_vdm =
+			board_customized_vdm_response(port, rx_payload);
+		if (is_customized_vdm) {
+			pe_set_ready_state(port);
+			return;
+		}
+#endif
 		CPRINTF("VDO ERR:CMD:%d\n", vdo_cmd);
 	}
 
@@ -7768,6 +7779,10 @@ void pd_dfp_mode_init(int port)
 	/* Reset the DPM and DP modules to enable alternate mode entry. */
 	dpm_mode_exit_complete(port);
 	dp_init(port);
+
+#ifdef USB_VID_USBC_MONITOR
+	docking_monitor_mode_init(port);
+#endif
 
 	if (IS_ENABLED(CONFIG_USB_PD_TBT_COMPAT_MODE))
 		tbt_init(port);
