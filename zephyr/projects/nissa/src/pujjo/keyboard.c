e07bf26c915b1a7293db030900407cc8a9b6aa55
jimmy.wu <jimmy.wu@lcfc.corp-partner.google.com>
2022-07-19 17:11:54 +0800

    pujjo: Correct hotkey function define
    Correct hotkey function define.
    
    BUG=b:239511837
    TEST=zmake build pujjo
    BRANCH=none
    
    Signed-off-by: jimmy.wu <jimmy.wu@lcfc.corp-partner.google.com>
    Change-Id: I727a86145ceec1a2d74e8aa520f4b280d0ad3b24

--- zephyr/projects/nissa/src/pujjo/keyboard.c
+++ zephyr/projects/nissa/src/pujjo/keyboard.c
@@ -8,16 +8,16 @@
 static const struct ec_response_keybd_config pujjo_kb = {
 	.num_top_row_keys = 10,
 	.action_keys = {
-		TK_BACK,		/* T1 */
-		TK_REFRESH,		/* T2 */
+		TK_BACK,		    /* T1 */
+		TK_REFRESH,		    /* T2 */
 		TK_FULLSCREEN,		/* T3 */
 		TK_OVERVIEW,		/* T4 */
-		TK_SNAPSHOT,		/* T5 */
-		TK_BRIGHTNESS_DOWN,	/* T6 */
-		TK_BRIGHTNESS_UP,	/* T7 */
+		TK_BRIGHTNESS_DOWN, /* T5 */
+		TK_BRIGHTNESS_UP,	/* T6 */
+		TK_MICMUTE,	        /* T7 */
 		TK_VOL_MUTE,		/* T8 */
 		TK_VOL_DOWN,		/* T9 */
-		TK_VOL_UP,		/* T10 */
+		TK_VOL_UP,		    /* T10 */
 	},
 	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
 };
