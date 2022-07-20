0000c975c6191c41658d18b75376910aeb52a5d3
Tomasz Michalec <tm@semihalf.com>
2022-07-20 11:54:06 +0200

    keeby: usb_mux: Remove usb_port
    Update keeby boards to not use usb_port.
    
    BUG=None
    TEST=make buildall
    BRANCH=None
    
    Signed-off-by: Tomasz Michalec <tm@semihalf.com>
    Change-Id: Idaac7124e08d9c39e96127206e00465a85f69870

--- board/cappy2/board.c
+++ board/cappy2/board.c
@@ -288,7 +288,6 @@ const struct usb_mux_chain usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	{
 		.mux =
 			&(const struct usb_mux){
-				.usb_port = 0,
 				.i2c_port = I2C_PORT_USB_C0,
 				.i2c_addr_flags = PS8743_I2C_ADDR0_FLAG,
 				.driver = &ps8743_usb_mux_driver,
