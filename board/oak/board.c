c5405a85e320c6df2ee4b3c8c1c47a2e9f075905
Tomasz Michalec <tm@semihalf.com>
2022-07-20 12:08:34 +0200

    oak: usb_mux: Remove usb_port
    Update oak boards to not use usb_port.
    
    BUG=None
    TEST=make buildall
    BRANCH=None
    
    Signed-off-by: Tomasz Michalec <tm@semihalf.com>
    Change-Id: Ief0ed6584387ea5001dc68e2f9f378620e4dea77

--- board/oak/board.c
+++ board/oak/board.c
@@ -199,7 +199,6 @@ const struct usb_mux_chain usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	{
 		.mux =
 			&(const struct usb_mux){
-				.usb_port = 0,
 				.i2c_port = I2C_PORT_USB_MUX,
 				.i2c_addr_flags = PI3USB3X532_I2C_ADDR0,
 				.driver = &pi3usb3x532_usb_mux_driver,
@@ -208,7 +207,6 @@ const struct usb_mux_chain usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	{
 		.mux =
 			&(const struct usb_mux){
-				.usb_port = 1,
 				.i2c_port = I2C_PORT_USB_MUX,
 #if (BOARD_REV <= OAK_REV4)
 				.i2c_addr_flags = PI3USB3X532_I2C_ADDR1,
