bc611908951a32eb4505ca49541d1d910bfbc3a2
Tomasz Michalec <tm@semihalf.com>
2022-07-20 11:55:48 +0200

    cherry: usb_mux: Split struct usb_mux in cherry board
    Update cherry boards to use new struct usb_mux_chain.
    
    BUG=b:236274003
    TEST=make buildall
    BRANCH=None
    
    Cq-Depend: chromium:3748785
    Signed-off-by: Tomasz Michalec <tm@semihalf.com>
    Change-Id: Icb5054d97ef12ba659cf405b99760a340565ee97

--- board/cherry/board.c
+++ board/cherry/board.c
@@ -197,7 +197,8 @@ BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
 
 /* USB Mux */
 
-static int board_ps8762_mux_set(const struct usb_mux *me, mux_state_t mux_state)
+static int board_ps8762_mux_set(const struct usb_mux *me, int port,
+				mux_state_t mux_state)
 {
 	/* Make sure the PS8802 is awake */
 	RETURN_ERROR(ps8802_i2c_wake(me));
@@ -222,7 +223,7 @@ static int board_ps8762_mux_set(const struct usb_mux *me, mux_state_t mux_state)
 	return EC_SUCCESS;
 }
 
-static int board_ps8762_mux_init(const struct usb_mux *me)
+static int board_ps8762_mux_init(const struct usb_mux *me, int port)
 {
 	return ps8802_i2c_field_update8(me, PS8802_REG_PAGE1, PS8802_REG_DCIRX,
 					PS8802_AUTO_DCI_MODE_DISABLE |
@@ -230,7 +231,7 @@ static int board_ps8762_mux_init(const struct usb_mux *me)
 					PS8802_AUTO_DCI_MODE_DISABLE);
 }
 
-static int board_anx3443_mux_set(const struct usb_mux *me,
+static int board_anx3443_mux_set(const struct usb_mux *me, int port,
 				 mux_state_t mux_state)
 {
 	gpio_set_level(GPIO_USB_C1_DP_IN_HPD,
@@ -238,21 +239,25 @@ static int board_anx3443_mux_set(const struct usb_mux *me,
 	return EC_SUCCESS;
 }
 
-const struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
+const struct usb_mux_chain usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	{
-		.usb_port = 0,
-		.i2c_port = I2C_PORT_USB_MUX0,
-		.i2c_addr_flags = PS8802_I2C_ADDR_FLAGS,
-		.driver = &ps8802_usb_mux_driver,
-		.board_init = &board_ps8762_mux_init,
-		.board_set = &board_ps8762_mux_set,
+		.mux =
+			&(const struct usb_mux){
+				.i2c_port = I2C_PORT_USB_MUX0,
+				.i2c_addr_flags = PS8802_I2C_ADDR_FLAGS,
+				.driver = &ps8802_usb_mux_driver,
+				.board_init = &board_ps8762_mux_init,
+				.board_set = &board_ps8762_mux_set,
+			},
 	},
 	{
-		.usb_port = 1,
-		.i2c_port = I2C_PORT_USB_MUX1,
-		.i2c_addr_flags = ANX3443_I2C_ADDR0_FLAGS,
-		.driver = &anx3443_usb_mux_driver,
-		.board_set = &board_anx3443_mux_set,
+		.mux =
+			&(const struct usb_mux){
+				.i2c_port = I2C_PORT_USB_MUX1,
+				.i2c_addr_flags = ANX3443_I2C_ADDR0_FLAGS,
+				.driver = &anx3443_usb_mux_driver,
+				.board_set = &board_anx3443_mux_set,
+			},
 	},
 };
 
