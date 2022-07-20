f1146d40fb9591ceaf33488dfd35d46612fee985
jimmy.wu <jimmy.wu@lcfc.corp-partner.google.com>
2022-07-20 16:49:51 +0800

    pujjo: Remove second type-c port/charger code.
    Disable GPIO I2c function for hdmi pin.
    Remove type-c port1, second charger code.
    
    BUG=b:239653512
    TEST=zmake build pujjo
    BRANCH=none
    
    Signed-off-by: jimmy.wu <jimmy.wu@lcfc.corp-partner.google.com>
    Change-Id: I87ddb8d8df18d70363349f06bf986da8f14c65cc

--- zephyr/projects/nissa/src/sub_board.c
+++ zephyr/projects/nissa/src/sub_board.c
@@ -151,6 +151,7 @@ static void nereid_subboard_config(void)
 	 * USB-C port: the default configuration has I2C on the I2C pins,
 	 * but the interrupt line needs to be configured.
 	 */
+#if CONFIG_USB_PD_PORT_MAX_COUNT > 1
 	if (sb == NISSA_SB_C_A || sb == NISSA_SB_C_LTE) {
 		/* Configure interrupt input */
 		gpio_pin_configure_dt(GPIO_DT_FROM_ALIAS(gpio_usb_c1_int_odl),
@@ -160,6 +161,7 @@ static void nereid_subboard_config(void)
 		task_disable_task(TASK_ID_USB_CHG_P1);
 		usb_muxes[1].next_mux = NULL;
 	}
+#endif
 
 	switch (sb) {
 #if BOARD_HAS_HDMI_SUPPORT
