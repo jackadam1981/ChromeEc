7894dda451d06e71cd873593ba280967cdfb169f
Tomasz Michalec <tm@semihalf.com>
2022-07-20 14:01:48 +0200

    zephyr: intelrvp: usb_mux: Split struct usb_mux in intelrvp boards
    Update intelrvp boards to use new struct usb_mux_chain.
    
    BUG=b:236274003
    TEST=zmake build -a
    TEST=./twister -T zephyr/test
    BRANCH=None
    
    Cq-Depend: chromium:3779618
    Signed-off-by: Tomasz Michalec <tm@semihalf.com>
    Change-Id: I6f69cc3d4ce99837a0db6bb12d55b71551a0d577

--- zephyr/projects/intelrvp/adlrvp/src/adlrvp.c
+++ zephyr/projects/intelrvp/adlrvp/src/adlrvp.c
@@ -96,23 +96,27 @@ BUILD_ASSERT(ARRAY_SIZE(ppc_chips) == CONFIG_USB_PD_PORT_MAX_COUNT);
 unsigned int ppc_cnt = ARRAY_SIZE(ppc_chips);
 
 /* USB Mux Configuration for Soc side BB-Retimers for Dual retimer config */
-static struct usb_mux soc_side_bb_retimer0_usb_mux = {
-	.usb_port = TYPE_C_PORT_0,
-	.next_mux = USB_MUX_NEXT_POINTER(DT_NODELABEL(usbc_port0), 0),
-	.driver = &bb_usb_retimer,
-	.hpd_update = bb_retimer_hpd_update,
-	.i2c_port = I2C_PORT_TYPEC_0,
-	.i2c_addr_flags = I2C_PORT0_BB_RETIMER_SOC_ADDR,
+struct usb_mux_chain soc_side_bb_retimer0_usb_mux = {
+	.mux =
+		&(const struct usb_mux){
+			.driver = &bb_usb_retimer,
+			.hpd_update = bb_retimer_hpd_update,
+			.i2c_port = I2C_PORT_TYPEC_0,
+			.i2c_addr_flags = I2C_PORT0_BB_RETIMER_SOC_ADDR,
+		},
+	.next = &USB_MUX_CHAIN_STRUCT_NAME(1, 0),
 };
 
 #if defined(HAS_TASK_PD_C1)
-static struct usb_mux soc_side_bb_retimer1_usb_mux = {
-	.usb_port = TYPE_C_PORT_1,
-	.next_mux = USB_MUX_NEXT_POINTER(DT_NODELABEL(usbc_port1), 0),
-	.driver = &bb_usb_retimer,
-	.hpd_update = bb_retimer_hpd_update,
-	.i2c_port = I2C_PORT_TYPEC_1,
-	.i2c_addr_flags = I2C_PORT1_BB_RETIMER_SOC_ADDR,
+struct usb_mux_chain soc_side_bb_retimer1_usb_mux = {
+	.mux =
+		&(const struct usb_mux){
+			.driver = &bb_usb_retimer,
+			.hpd_update = bb_retimer_hpd_update,
+			.i2c_port = I2C_PORT_TYPEC_1,
+			.i2c_addr_flags = I2C_PORT1_BB_RETIMER_SOC_ADDR,
+		},
+	.next = &USB_MUX_CHAIN_STRUCT_NAME(1, 1),
 };
 #endif
 
@@ -133,7 +137,8 @@ void board_overcurrent_event(int port, int is_overcurrented)
 	ioex_set_level(oc_signal, is_overcurrented ? 0 : 1);
 }
 
-__override int bb_retimer_power_enable(const struct usb_mux *me, bool enable)
+__override int bb_retimer_power_enable(const struct usb_mux *me, int port,
+				       bool enable)
 {
 	/*
 	 * ADL-P-DDR5 RVP SKU has cascaded retimer topology.
@@ -141,14 +146,14 @@ __override int bb_retimer_power_enable(const struct usb_mux *me, bool enable)
 	 * hence no need to set the power state again if the 1st retimer's power
 	 * status has already changed.
 	 */
-	if (cache_bb_enable[me->usb_port] == enable)
+	if (cache_bb_enable[port] == enable)
 		return EC_SUCCESS;
 
-	cache_bb_enable[me->usb_port] = enable;
+	cache_bb_enable[port] = enable;
 
 	/* Handle retimer's power domain.*/
 	if (enable) {
-		ioex_set_level(bb_controls[me->usb_port].usb_ls_en_gpio, 1);
+		ioex_set_level(bb_controls[port].usb_ls_en_gpio, 1);
 
 		/*
 		 * minimum time from VCC to RESET_N de-assertion is 100us
@@ -157,7 +162,7 @@ __override int bb_retimer_power_enable(const struct usb_mux *me, bool enable)
 		 * this function.
 		 */
 		msleep(1);
-		ioex_set_level(bb_controls[me->usb_port].retimer_rst_gpio, 1);
+		ioex_set_level(bb_controls[port].retimer_rst_gpio, 1);
 
 		/*
 		 * Allow 1ms time for the retimer to power up lc_domain
@@ -166,9 +171,9 @@ __override int bb_retimer_power_enable(const struct usb_mux *me, bool enable)
 		msleep(1);
 
 	} else {
-		ioex_set_level(bb_controls[me->usb_port].retimer_rst_gpio, 0);
+		ioex_set_level(bb_controls[port].retimer_rst_gpio, 0);
 		msleep(1);
-		ioex_set_level(bb_controls[me->usb_port].usb_ls_en_gpio, 0);
+		ioex_set_level(bb_controls[port].usb_ls_en_gpio, 0);
 	}
 	return EC_SUCCESS;
 }
@@ -244,25 +249,29 @@ static void configure_charger(void)
 
 static void configure_retimer_usbmux(void)
 {
+	struct usb_mux *mux;
+
 	switch (ADL_RVP_BOARD_ID(board_get_version())) {
 	case ADLN_LP5_ERB_SKU_BOARD_ID:
 	case ADLN_LP5_RVP_SKU_BOARD_ID:
 		/* enable TUSB1044RNQR redriver on Port0  */
-		usb_muxes[TYPE_C_PORT_0].i2c_addr_flags =
-			TUSB1064_I2C_ADDR14_FLAGS;
-		usb_muxes[TYPE_C_PORT_0].driver = &tusb1064_usb_mux_driver;
-		usb_muxes[TYPE_C_PORT_0].hpd_update = tusb1044_hpd_update;
+		mux = USB_MUX_POINTER(DT_NODELABEL(usb_mux_chain_0), 0);
+		mux->i2c_addr_flags = TUSB1064_I2C_ADDR14_FLAGS;
+		mux->driver = &tusb1064_usb_mux_driver;
+		mux->hpd_update = tusb1044_hpd_update;
 
 #if defined(HAS_TASK_PD_C1)
-		usb_muxes[TYPE_C_PORT_1].driver = NULL;
-		usb_muxes[TYPE_C_PORT_1].hpd_update = NULL;
+		mux = USB_MUX_POINTER(DT_NODELABEL(usb_mux_chain_1), 0);
+		mux->driver = NULL;
+		mux->hpd_update = NULL;
 #endif
 		break;
 
 	case ADLP_LP5_T4_RVP_SKU_BOARD_ID:
 		/* No retimer on Port-2 */
 #if defined(HAS_TASK_PD_C2)
-		usb_muxes[TYPE_C_PORT_2].driver = NULL;
+		mux = USB_MUX_POINTER(DT_NODELABEL(usb_mux_chain_2), 0);
+		mux->driver = NULL;
 #endif
 		break;
 
@@ -272,11 +281,9 @@ static void configure_retimer_usbmux(void)
 		 * Change the default usb mux config on runtime to support
 		 * dual retimer topology.
 		 */
-		usb_muxes[TYPE_C_PORT_0].next_mux =
-			&soc_side_bb_retimer0_usb_mux;
+		usb_muxes[TYPE_C_PORT_0].next = &soc_side_bb_retimer0_usb_mux;
 #if defined(HAS_TASK_PD_C1)
-		usb_muxes[TYPE_C_PORT_1].next_mux =
-			&soc_side_bb_retimer1_usb_mux;
+		usb_muxes[TYPE_C_PORT_1].next = &soc_side_bb_retimer1_usb_mux;
 #endif
 		break;
 
