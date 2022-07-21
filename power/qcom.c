25a2547a320f2dcce033bef647291538519cb19e
Al Semjonovs <asemjonovs@google.com>
2022-07-21 10:24:11 -0600

    zephyr: Mock power sequence driver
    Mock power sequence handling
    
    BUG=None
    BRANCH=NONE
    TEST=./twister -T zephyr/test
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: I8ec3198abfb069501bed2c5aa661e31f780e2c34

--- power/qcom.c
+++ power/qcom.c
@@ -814,7 +814,7 @@ static inline void cancel_power_button_timer(void)
 /*****************************************************************************/
 /* Chipset interface */
 
-void chipset_force_shutdown(enum chipset_shutdown_reason reason)
+__overridable void chipset_force_shutdown(enum chipset_shutdown_reason reason)
 {
 	CPRINTS("%s(%d)", __func__, reason);
 	report_ap_reset(reason);
@@ -824,7 +824,7 @@ void chipset_force_shutdown(enum chipset_shutdown_reason reason)
 	task_wake(TASK_ID_CHIPSET);
 }
 
-void chipset_power_on(void)
+__overridable void chipset_power_on(void)
 {
 	if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
 		power_request = POWER_REQ_ON;
@@ -999,7 +999,7 @@ power_chipset_handle_host_sleep_event(enum host_sleep_event state,
  * @param state		Current power state
  * @return Updated power state
  */
-enum power_state power_handle_state(enum power_state state)
+__overridable enum power_state power_handle_state(enum power_state state)
 {
 	static uint8_t boot_from_off, shutdown_from_on;
 
@@ -1204,7 +1204,7 @@ static const char *const state_name[] = {
 	"on",
 };
 
-static int command_power(int argc, char **argv)
+__overridable int command_power(int argc, char **argv)
 {
 	int v;
 
@@ -1230,4 +1230,11 @@ static int command_power(int argc, char **argv)
 
 	return EC_SUCCESS;
 }
-DECLARE_CONSOLE_COMMAND(power, command_power, "on/off", "Turn AP power on/off");
+
+static int qcom_command_power(int argc, char **argv)
+{
+	return command_power(argc, argv);
+}
+
+DECLARE_CONSOLE_COMMAND(power, qcom_command_power, "on/off",
+			"Turn AP power on/off");
