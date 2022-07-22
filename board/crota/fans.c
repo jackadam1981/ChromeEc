e664f52300d782bf5d774445297002329a4eefbc
Peter Chi <peter_chi@wistron.corp-partner.google.com>
2022-07-22 14:41:42 +0800

    crota: modify fan rpm range
    BUG=b:
    BRANCH=brya
    TEST=make -j BOARD=crota
    
    Signed-off-by: Peter Chi <peter_chi@wistron.corp-partner.google.com>
    Change-Id: I7ea9c53f168b2ab3f050b87006adb772c9f08ba4

--- board/crota/fans.c
+++ board/crota/fans.c
@@ -15,6 +15,10 @@
 #include "thermal.h"
 #include "util.h"
 
+#define SENSOR_SOC_FAN_OFF 35
+#define SENSOR_SOC_FAN_MID 45
+#define SENSOR_SOC_FAN_MAX 51
+
 /* MFT channels. These are logically separate from pwm_channels. */
 const struct mft_t mft_channels[] = {
 	[MFT_CH_0] = {
@@ -32,74 +36,43 @@ static const struct fan_conf fan_conf_0 = {
 	.enable_gpio = GPIO_EN_PP5000_FAN,
 };
 
-static const struct fan_rpm fan_rpm_0 = {
-	.rpm_min = 3500,
-	.rpm_start = 3500,
-	.rpm_max = 4300,
+static const struct fan_rpm fan_rpm_cpu_0 = {
+	.rpm_min = 2200,
+	.rpm_start = 2200,
+	.rpm_max = 3700,
 };
 
-static const struct fan_rpm fan_rpm_1 = {
-	.rpm_min = 4300,
-	.rpm_start = 4300,
+static const struct fan_rpm fan_rpm_cpu_1 = {
+	.rpm_min = 3700,
+	.rpm_start = 3700,
+	.rpm_max = 4000,
+};
+
+static const struct fan_rpm fan_rpm_charger = {
+	.rpm_min = 4000,
+	.rpm_start = 4000,
 	.rpm_max = 4700,
 };
 
 struct fan_t fans[FAN_CH_COUNT] = {
 	[FAN_CH_0] = {
 		.conf = &fan_conf_0,
-		.rpm = &fan_rpm_0,
+		.rpm = &fan_rpm_cpu_0,
 	},
 };
 
-#ifndef CONFIG_FANS
-
-/*
- * TODO(b/181271666): use static fan speeds until fan and sensors are
- * tuned. for now, use:
- *
- *   AP off:  33%
- *   AP  on: 100%
- */
-
-static void fan_slow(void)
-{
-	const int duty_pct = 33;
-
-	ccprints("%s: speed %d%%", __func__, duty_pct);
-
-	pwm_enable(PWM_CH_FAN, 1);
-	pwm_set_duty(PWM_CH_FAN, duty_pct);
-}
-
-static void fan_max(void)
-{
-	const int duty_pct = 100;
-
-	ccprints("%s: speed %d%%", __func__, duty_pct);
-
-	pwm_enable(PWM_CH_FAN, 1);
-	pwm_set_duty(PWM_CH_FAN, duty_pct);
-}
-
-DECLARE_HOOK(HOOK_INIT, fan_slow, HOOK_PRIO_DEFAULT);
-DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, fan_slow, HOOK_PRIO_DEFAULT);
-DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, fan_slow, HOOK_PRIO_DEFAULT);
-DECLARE_HOOK(HOOK_CHIPSET_RESET, fan_max, HOOK_PRIO_FIRST);
-DECLARE_HOOK(HOOK_CHIPSET_RESUME, fan_max, HOOK_PRIO_DEFAULT);
-
-#endif /* CONFIG_FANS */
-
-static void fan_set_percent(int fan, int pct, bool fan_triggered)
+static void fan_set_percent(int fan, int pct, int soc_temp, bool fan_triggered)
 {
 	int new_rpm;
 
 	if (fan_triggered)
-		fans[fan].rpm = &fan_rpm_1;
+		fans[fan].rpm = &fan_rpm_charger;
+	else if (soc_temp > SENSOR_SOC_FAN_MID)
+		fans[fan].rpm = &fan_rpm_cpu_1;
 	else
-		fans[fan].rpm = &fan_rpm_0;
+		fans[fan].rpm = &fan_rpm_cpu_0;
 
 	new_rpm = fan_percent_to_rpm(fan, pct);
-
 	fan_set_rpm_target(FAN_CH(fan), new_rpm);
 }
 
@@ -109,41 +82,53 @@ void board_override_fan_control(int fan, int *tmp)
 	 * Crota's fan speed is control by three sensors.
 	 *
 	 * Sensor SOC control high loading's speed.
-	 * Sensor ambient control low loading's speed.
 	 * Sensor charger control the speed when system's temperature
 	 * is too high.
 	 *
 	 * When sensor charger is not triggered, the fan is control
-	 * and choose the smaller speed between SOC and ambient.
+	 * by sensor SOC. Sensor SOC has two slopes for fan speed.
 	 *
 	 * When sensor charger is triggered, the fan speed is only
 	 * control by sensor charger, avoid heat damage to system.
 	 */
-
+	int soc_temp_c;
+	int charger_temp_c;
+	int sensor_soc_fan_off;
+	int sensor_soc_fan_mid;
+	int sensor_soc_fan_max;
 	int pct;
 	int sensor_soc;
-	int sensor_ambient;
 	int sensor_charger;
 	bool fan_triggered;
 
+	soc_temp_c = tmp[TEMP_SENSOR_1_SOC];
+	charger_temp_c = tmp[TEMP_SENSOR_3_CHARGER];
+	sensor_soc_fan_off = C_TO_K(SENSOR_SOC_FAN_OFF);
+	sensor_soc_fan_mid = C_TO_K(SENSOR_SOC_FAN_MID);
+	sensor_soc_fan_max = C_TO_K(SENSOR_SOC_FAN_MAX);
+
+	if (soc_temp_c > SENSOR_SOC_FAN_MID) {
+		thermal_params[0].temp_fan_off = sensor_soc_fan_mid;
+		thermal_params[0].temp_fan_max = sensor_soc_fan_max;
+	} else {
+		thermal_params[0].temp_fan_off = sensor_soc_fan_off;
+		thermal_params[0].temp_fan_max = sensor_soc_fan_mid;
+	}
+
 	sensor_soc = thermal_fan_percent(thermal_params[0].temp_fan_off,
 					 thermal_params[0].temp_fan_max,
-					 C_TO_K(tmp[0]));
-	sensor_ambient = thermal_fan_percent(thermal_params[3].temp_fan_off,
-					     thermal_params[3].temp_fan_max,
-					     C_TO_K(tmp[3]));
+					 C_TO_K(soc_temp_c));
 	sensor_charger = thermal_fan_percent(thermal_params[2].temp_fan_off,
 					     thermal_params[2].temp_fan_max,
-					     C_TO_K(tmp[2]));
+					     C_TO_K(charger_temp_c));
 
 	if (sensor_charger) {
 		fan_triggered = true;
 		pct = sensor_charger;
 	} else {
 		fan_triggered = false;
-		pct = MIN(sensor_soc, sensor_ambient);
+		pct = sensor_soc;
 	}
-
 	/* transfer percent to rpm */
-	fan_set_percent(fan, pct, fan_triggered);
+	fan_set_percent(fan, pct, soc_temp_c, fan_triggered);
 }
--- board/crota/sensors.c
+++ board/crota/sensors.c
@@ -187,28 +187,29 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 		.temp_host_release = { \
 			[EC_TEMP_THRESH_HIGH] = C_TO_K(77), \
 		}, \
-		.temp_fan_off = C_TO_K(39), \
-		.temp_fan_max = C_TO_K(52), \
+		.temp_fan_off = 35, \
+		.temp_fan_max = 45, \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_cpu = THERMAL_CPU;
 
 #define THERMAL_CHARGER                                                 \
 	{                                                               \
-		.temp_fan_off = C_TO_K(59), .temp_fan_max = C_TO_K(65), \
+		.temp_fan_off = C_TO_K(67), .temp_fan_max = C_TO_K(70), \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_charger =
 	THERMAL_CHARGER;
 
-#define THERMAL_AMBIENT                                                 \
-	{                                                               \
-		.temp_fan_off = C_TO_K(26), .temp_fan_max = C_TO_K(31), \
+#define THERMAL_NO_USE \
+	{              \
 	}
-__maybe_unused static const struct ec_thermal_config thermal_ambient =
-	THERMAL_AMBIENT;
+__maybe_unused static const struct ec_thermal_config thermal_no_use =
+	THERMAL_NO_USE;
 
 struct ec_thermal_config thermal_params[] = {
 	[TEMP_SENSOR_1_SOC] = THERMAL_CPU,
+	[TEMP_SENSOR_2_DDR] = THERMAL_NO_USE,
 	[TEMP_SENSOR_3_CHARGER] = THERMAL_CHARGER,
-	[TEMP_SENSOR_4_AMBIENT] = THERMAL_AMBIENT,
+	[TEMP_SENSOR_4_AMBIENT] = THERMAL_NO_USE,
 };
+
 BUILD_ASSERT(ARRAY_SIZE(thermal_params) == TEMP_SENSOR_COUNT);
