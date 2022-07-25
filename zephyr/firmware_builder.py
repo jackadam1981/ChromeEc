d57af092a2c60bb0d9ebc7bc8437f84b920818b7
Al Semjonovs <asemjonovs@google.com>
2022-07-25 11:59:18 -0600

    zephyr: Upload test results to ResultDB
    Parse results of twister and upload them to ResultDB
    
    BUG=None
    BRANCH=None
    TEST=./twister
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: Idf3a6c67f8676736c73b5da3b388b4eb925e15ff

--- zephyr/firmware_builder.py
+++ zephyr/firmware_builder.py
@@ -196,6 +196,23 @@ def bundle_firmware(opts):
     write_metadata(opts, info)
 
 
+def upload_results(platform_ec):
+    """Uploads Zephyr Test results to ResultDB"""
+    json_path = platform_ec / "twister-out" / "twister.json"
+    cmd = [
+        "rdb",
+        "stream",
+        "--",
+        str(platform_ec / "util/zephyr_to_result_db.py"),
+        "--result=" + str(json_path),
+        "--upload=True",
+    ]
+
+    ret = subprocess.run(cmd, check=True).returncode
+
+    return ret
+
+
 def test(opts):
     """Runs all of the unit tests for Zephyr firmware"""
     metrics = firmware_pb2.FwTestMetricList()
@@ -216,6 +233,7 @@ def test(opts):
     # Twister-based tests
     platform_ec = zephyr_dir.parent
     run_twister(platform_ec, opts.code_coverage, ["--test-only"])
+    upload_results(platform_ec)
 
     if opts.code_coverage:
         build_dir = platform_ec / "build" / "zephyr"
