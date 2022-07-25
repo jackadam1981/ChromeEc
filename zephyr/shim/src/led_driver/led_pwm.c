5febe270000e609801b6e0dee6635cd7c00515ab
Ting Shen <phoenixshen@google.com>
2022-07-25 18:03:53 +0800

    zephyr: led_pwm: check led_id in LED apis
    LED get/set brightness functions should check if led id matches the input
    argument.
    
    BUG=b:240090610
    TEST=on tentacruel (two leds with different supported colors)
         verify `ectool led <power|battery> query` returns expected numbers.
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: Ic4c463bfdec090d3d05354db98af08b9294b67a8

--- zephyr/shim/src/led_driver/led_pwm.c
+++ zephyr/shim/src/led_driver/led_pwm.c
@@ -8,12 +8,15 @@
 #include "ec_commands.h"
 #include "led.h"
 #include "util.h"
+#include "console.h"
 
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/logging/log.h>
 
 #if DT_HAS_COMPAT_STATUS_OKAY(COMPAT_PWM_LED)
+
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
 
 LOG_MODULE_REGISTER(pwm_led, LOG_LEVEL_ERR);
 
@@ -98,8 +101,13 @@ void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 	for (int i = 0; i < ARRAY_SIZE(pins_node); i++) {
 		int br_color = pins_node[i]->br_color;
 
-		if (br_color != -1)
+		if (pins_node[i]->led_id != led_id) {
+			continue;
+		}
+
+		if (br_color != -1) {
 			brightness_range[br_color] = 100;
+		}
 	}
 }
 
@@ -109,6 +117,10 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 
 	for (int i = 0; i < ARRAY_SIZE(pins_node); i++) {
 		int br_color = pins_node[i]->br_color;
+
+		if (pins_node[i]->led_id != led_id) {
+			continue;
+		}
 
 		if ((br_color != -1) && (brightness[br_color] != 0)) {
 			color_set = true;
