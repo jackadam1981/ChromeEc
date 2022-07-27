7ce3cef4331d545cb565d305a1794c14a90b756a
Tomasz Michalec <tm@semihalf.com>
2022-07-27 16:48:17 +0200

    zephyr: test: isl923x: Fix finish i2c write
    Side effects of I2C write messages should be applied only after actual
    write message. Selecting register before an I2C read message isn't
    considered as a write message.
    
    BUG=None
    TEST=zmake -D test test-drivers
    BRANCH=None
    
    Signed-off-by: Tomasz Michalec <tm@semihalf.com>
    Change-Id: I95871d1b518c7211ff1e56b74598e7ba35358d41

--- zephyr/emul/emul_isl923x.c
+++ zephyr/emul/emul_isl923x.c
@@ -382,6 +382,11 @@ static int isl923x_emul_finish_write(struct i2c_emul *emul, int reg, int bytes)
 	struct sbat_emul_bat_data *bat;
 	int16_t current;
 
+	/* This write only selected register for I2C read message */
+	if (bytes < 2) {
+		return 0;
+	}
+
 	switch (reg) {
 	case ISL923X_REG_CHG_CURRENT:
 		/* Write current to battery. */
