5cccd7fbf995d07a2cf986d8f9b8b8763fd3b978
Peter Marheine <pmarheine@chromium.org>
2022-07-27 16:28:37 +1000

    sm5803: verify CHG_MON and stop on unexpected state
    It seems possible that unexpected charger resets cause incorrect
    configuration, so add a check for safe CHG_MON configuration whenever
    the charger gets enabled that will stop any charger operations but keep
    the system alive for debugging.
    
    Also watch the fault input from the USB-C0 protection device, to note if
    it ever seems to trigger.
    
    BUG=b:230712704
    
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Change-Id: I627984e7331688e63dd62b6280f385f027ace53b

--- zephyr/projects/nissa/nereid/src/charger.c
+++ zephyr/projects/nissa/nereid/src/charger.c
@@ -54,3 +54,29 @@ __override void board_hibernate(void)
 	LOG_INF("Charger(s) hibernated");
 	cflush();
 }
+
+
+#define CC_OVP_GPIO GPIO_DT_FROM_NODELABEL(gpio_usb_c0_prot_fault_odl)
+
+static void cc_ovp_interrupt(const struct device *device, struct gpio_callback *callback, gpio_port_pins_t pins)
+{
+	int state = gpio_pin_get_dt(CC_OVP_GPIO);
+	LOG_ERR("CC OVP fired!!!1!1! (= %d)", state);
+}
+
+static int init_cc_ovp_interrupt(const struct device *unused)
+{
+	static struct gpio_callback cc_ovp_cb;
+	int rv;
+
+	gpio_pin_configure_dt(CC_OVP_GPIO, GPIO_INPUT | GPIO_PULL_UP);
+	gpio_init_callback(&cc_ovp_cb, cc_ovp_interrupt, BIT(CC_OVP_GPIO->pin));
+	rv = gpio_pin_interrupt_configure_dt(CC_OVP_GPIO, GPIO_INT_EDGE_BOTH);
+
+	if (rv != 0) {
+		LOG_ERR("failed to configure CC OVP interrupt");
+		k_oops();
+	}
+	return 0;
+}
+SYS_INIT(init_cc_ovp_interrupt, APPLICATION, 0);
