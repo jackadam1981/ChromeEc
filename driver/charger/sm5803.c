495eec8dad27e41b27ccbfd33169167407e80703
Peter Marheine <pmarheine@chromium.org>
2022-07-27 16:28:37 +1000

    sm5803: verify CHG_MON and stop on unexpected state
    It seems possible that unexpected charger resets cause incorrect
    configuration, so add a check for safe CHG_MON configuration whenever
    the charger gets enabled that will stop any charger operations but keep
    the system alive for debugging.
    
    BUG=b:230712704
    
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Change-Id: I627984e7331688e63dd62b6280f385f027ace53b

--- driver/charger/sm5803.c
+++ driver/charger/sm5803.c
@@ -181,6 +181,19 @@ static int sm5803_set_active_safe(int chgnum)
 	/* Enable default GPADCs */
 	rv = meas_write8(chgnum, SM5803_REG_GPADC_CONFIG1,
 			 SM5803_GPADCC1_DEFAULT_ENABLE);
+	if (rv) {
+		goto out;
+	}
+
+	rv = chg_read8(chgnum, 0x5C, &val);
+	if (rv) {
+		goto out;
+	}
+	if (val != 0x7A) {
+		CPRINTS("Charger %d in unexpected state: 0x5C = 0x%02x", chgnum, val);
+		CPRINTS("Killing the charger task to permit debug!");
+		k_thread_abort(k_current_get());
+	}
 
 out:
 	if (rv) {
