dd73526e3fe37fded78bc90232925b9d6548c2b9
Yuval Peress <peress@google.com>
2022-07-28 11:26:54 -0600

    zephyr: support Zephyr unittest test types
    Add support for unittest test types under twister. Since unit testing
    in Zephyr doesn't include Kconfig, some configs need to be declared
    manually. The common ones were added to a new cmake package along
    with some copy code that brings some of the shared logic needed to
    build pretty much anything in the CrosEC. A stub common.h is also
    provided in order to keek the test as small and simple as possible.
    
    BRANCH=none
    BUG=b:240574048
    TEST=./twister -T zephyr/test/math
    
    Signed-off-by: Yuval Peress <peress@google.com>
    Change-Id: Ied6cbf031fdf3b55542a34067e70a756e6c2fa26

---
+++ zephyr/share/cros-package/cmake/stub_common.h
@@ -0,0 +1,9 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_SHARE_CROS_PACKAGE_CMAKE_STUB_COMMON_H_
+#define ZEPHYR_SHARE_CROS_PACKAGE_CMAKE_STUB_COMMON_H_
+
+#endif /* ZEPHYR_SHARE_CROS_PACKAGE_CMAKE_STUB_COMMON_H_ */
