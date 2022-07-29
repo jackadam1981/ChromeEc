9f3caa63a67575aeef5556d3325eb81edbefd93e
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-07-29 15:19:37 -0700

    ec: Support logging system boot time info
    This CL add support logging system boot time and hostcommand to get
    boot time info.
    EC logs elapsed time (in micro second) info since boot for different
    signal events,that can be used to calculate system boot time in cold
    boot and warm boot scenarios.
    
    Below are the events when EC logs time info
    
     ARAIL		EC turns on A-RAIL
     RSMRST		EC releases RSMRST#
     ESPIRST	ESPIRST received
     PLTRST_LOW	PLTRST# asserted
     PLTRST_HIGH	PLTRST# deasserted
     EC_CUR_TIME	EC current time
     cnt		increments on every warm reboot and reset to 0 on
     		cold reboot
    
    BUG=b:249163956
    BRANCH=none
    TEST='ectool boottime' AP console command is used to fetch data
         'reboot' on AP console to trigger warm reboot
         'apshutdown' on EC console to trigger cold reboot
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I15fedbde252e0452270ef0e3fd4391bf624169f1

--- chip/npcx/espi.c
+++ chip/npcx/espi.c
@@ -7,6 +7,7 @@
 
 #include "registers.h"
 #include "system.h"
+#include "system_boot_time.h"
 #include "task.h"
 #include "chipset.h"
 #include "console.h"
@@ -459,11 +460,14 @@ void espi_vw_evt_pltrst(void)
 		/* Enable eSPI peripheral channel */
 		SET_BIT(NPCX_ESPICFG, NPCX_ESPICFG_PCHANEN);
 #endif
+		update_ap_boot_time(PLTRST_HIGH);
+
 	} else {
 		/* PLTRST# asserted */
 #ifdef CONFIG_CHIPSET_RESET_HOOK
 		hook_call_deferred(&espi_chipset_reset_data, MSEC);
 #endif
+		update_ap_boot_time(PLTRST_LOW);
 	}
 }
 
@@ -523,6 +527,8 @@ void espi_espirst_handler(void)
 {
 	/* Clear pending bit of WUI */
 	SET_BIT(NPCX_WKPCL(MIWU_TABLE_0, MIWU_GROUP_5), 5);
+
+	update_ap_boot_time(ESPIRST);
 
 	CPRINTS("eSPI RST issued!");
 }
