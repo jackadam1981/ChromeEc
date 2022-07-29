a38b41dca4ecce48a10029949585a9ab20bb5b4a
Rajesh Kumar <rajesh3.kumar@intel.com>
2022-07-29 15:19:37 -0700

    ec: Support logging system boot time info
    This CL add support logging system boot time and hostcommand to get
    boot time info.
    EC logs elapsed time (in micro second) info since boot for different
    signal events,that can be used to calculate system boot time in cold
    boot and warm boot scenarios.
    
    Below are the events when EC logs time info
    
     arail		EC turns on A-RAIL
     rsmrst		EC releases RSMRST#
     espirst	ESPIRST received
     pltrst_low	PLTRST# asserted
     pltrst_high	PLTRST# deasserted
     cnt		increments on every warm reboot and reset to 0 on
     		cold reboot
     ec_cur_time	EC current time
    
    BUG=none
    BRANCH=none
    TEST='ectool boottime' AP console command is used to fetch data
         'reboot' on AP console to trigger warm reboot
         'apshutdown' on EC console to trigger cold reboot
    
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>
    Change-Id: I15fedbde252e0452270ef0e3fd4391bf624169f1

--- chip/it83xx/espi.c
+++ chip/it83xx/espi.c
@@ -273,6 +273,9 @@ static void espi_vw_idx7_isr(uint8_t flag_changed, uint8_t vw_evt)
 static void espi_chipset_reset(void)
 {
 	hook_notify(HOOK_CHIPSET_RESET);
+#ifdef CONFIG_SYSTEM_BOOT_TIME_LOGGING
+	update_ap_boot_time(ESPIRST);
+#endif
 }
 DECLARE_DEFERRED(espi_chipset_reset);
 #endif
@@ -284,12 +287,18 @@ static void espi_vw_idx3_isr(uint8_t flag_changed, uint8_t vw_evt)
 
 		if (pltrst) {
 			espi_vw_host_startup();
+#ifdef CONFIG_SYSTEM_BOOT_TIME_LOGGING
+			update_ap_boot_time(PLTRST_HIGH);
+#endif
 		} else {
 #ifdef CONFIG_CHIPSET_RESET_HOOK
 			hook_call_deferred(&espi_chipset_reset_data, MSEC);
 #endif
 			/* Store port 80 reset event */
 			port_80_write(PORT_80_EVENT_RESET);
+#ifdef CONFIG_SYSTEM_BOOT_TIME_LOGGING
+			update_ap_boot_time(PLTRST_LOW);
+#endif
 		}
 
 		CPRINTS("VW PLTRST_L %sasserted", pltrst ? "de" : "");
--- power/alderlake_slg4bd44540.c
+++ power/alderlake_slg4bd44540.c
@@ -16,6 +16,7 @@
 #include "power.h"
 #include "power/alderlake_slg4bd44540.h"
 #include "power/intel_x86.h"
+#include "system.h"
 #include "timer.h"
 
 /*
@@ -251,6 +252,10 @@ enum power_state power_handle_state(enum power_state state)
 	switch (state) {
 	case POWER_G3S5:
 		GPIO_SET_LEVEL(GPIO_EN_S5_RAILS, 1);
+
+#ifdef CONFIG_SYSTEM_BOOT_TIME_LOGGING
+		update_ap_boot_time(ARAIL);
+#endif
 
 		if (power_wait_signals(IN_PGOOD_ALL_CORE))
 			break;
