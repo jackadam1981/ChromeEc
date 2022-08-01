078c8f68f0d5dc92bf3b5bcf2469ef842e171a8f
Peter Marheine <pmarheine@chromium.org>
2022-08-01 13:45:50 +1000

    sm5803/nereid: trace runtime LPM on GPIO
    This makes the GPL2 GPIO reflect the current power mode for the secondary
    charger: it goes high when initialized and goes low when in LPM (as done in
    response to a USB-PD disconnect), going high again on LPM exit.
    
    GPIO timing is set such that it goes low when LPM entry begins, and goes
    high when LPM exit completes.
    
    BUG=b:230712704
    
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Change-Id: Iac06fa086f97ab29d598cfa3858ed96c1242b29a

--- driver/charger/sm5803.c
+++ driver/charger/sm5803.c
@@ -474,6 +474,11 @@ static void init_mutexes(void)
 DECLARE_HOOK(HOOK_INIT, init_mutexes, HOOK_PRIO_FIRST);
 #endif
 
+static const struct gpio_dt_spec trace_gpio = {
+	.port = DEVICE_DT_GET(DT_NODELABEL(gpiol)),
+	.pin = 2,
+};
+
 static void sm5803_init(int chgnum)
 {
 	enum ec_error_list rv;
@@ -482,6 +487,8 @@ static void sm5803_init(int chgnum)
 	const struct battery_info *batt_info;
 	int pre_term;
 	int cells;
+
+	gpio_pin_configure_dt(&trace_gpio, GPIO_OUTPUT_HIGH);
 
 	/*
 	 * If a charger is not currently present, disable switching per OCPC
@@ -899,6 +906,10 @@ static void sm5803_disable_runtime_low_power_mode(void)
 	if (rv)
 		CPRINTS("%s %d: Failed to set in disable runtime LPM",
 			CHARGER_NAME, chgnum);
+
+	if (chgnum == CHARGER_SECONDARY) {
+		gpio_pin_set_dt(&trace_gpio, 1);
+	}
 }
 DECLARE_HOOK(HOOK_USB_PD_CONNECT, sm5803_disable_runtime_low_power_mode,
 	     HOOK_PRIO_FIRST);
@@ -964,6 +975,10 @@ static void sm5803_enable_runtime_low_power_mode(void)
 		CPRINTS("%s %d: Failed to read REFERENCE reg", CHARGER_NAME,
 			chgnum);
 		return;
+	}
+
+	if (chgnum == CHARGER_SECONDARY) {
+		gpio_pin_set_dt(&trace_gpio, 0);
 	}
 
 	/*
