e2a5886e71a7204613d9481d38a39516dd648f11
Vijay Hiremath <vijay.p.hiremath@intel.com>
2022-08-03 12:06:52 -0700

    cleanup: Make board_set_charge_limit() overridable
    As board_set_charge_limit() is common to many projects moved it
    to common file and made it overridable so that the board specific
    function can override it.
    
    BUG=none
    BRANCH=none
    TEST=make buildall -j, zmake testall
    
    Change-Id: I307a844d6865857caac28082aca0afab6c8e773e
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- board/felwinter/charger_isl9241.c
+++ board/felwinter/charger_isl9241.c
@@ -80,8 +80,8 @@ int board_set_active_charge_port(int port)
 	return EC_SUCCESS;
 }
 
-__overridable void board_set_charge_limit(int port, int supplier, int charge_ma,
-					  int max_ma, int charge_mv)
+__override void board_set_charge_limit(int port, int supplier, int charge_ma,
+				       int max_ma, int charge_mv)
 {
 	charge_ma = (charge_ma * 90) / 100;
 	charge_set_input_current_limit(
--- board/vell/charger.c
+++ board/vell/charger.c
@@ -81,8 +81,8 @@ int board_set_active_charge_port(int port)
 	return EC_SUCCESS;
 }
 
-void board_set_charge_limit(int port, int supplier, int charge_ma, int max_ma,
-			    int charge_mv)
+__override void board_set_charge_limit(int port, int supplier, int charge_ma,
+				       int max_ma, int charge_mv)
 {
 	/*
 	 * Limit the input current to 96% negotiated limit,
