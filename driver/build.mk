18fd80125df59505924697e991217710b43cdc0c
Gwendal Grignou <gwendal@chromium.org>
2022-08-04 14:09:37 -0700

    driver/tcs3410: Build ams fft code
    Move code around to build the Fast Fourrier Transform code needed for
    AMS TCS3410 flicker detection.
    
    BUG=b:232473311
    TEST=compile
    BRANCH=none
    
    Change-Id: I4595615b1dd946e35d1888fb1fa0c06592aa4ba8

--- driver/build.mk
+++ driver/build.mk
@@ -48,6 +48,9 @@ driver-$(CONFIG_ALS_SI114X)+=als_si114x.o
 driver-$(CONFIG_ALS_BH1730)+=als_bh1730.o
 driver-$(CONFIG_ALS_TCS3400)+=als_tcs3400.o
 driver-$(CONFIG_ALS_CM32183)+=als_cm32183.o
+# TCS3410
+include $(_driver_cur_dir)/tcs3410/build.mk
+
 
 # Barometers
 driver-$(CONFIG_BARO_BMP280)+=baro_bmp280.o
