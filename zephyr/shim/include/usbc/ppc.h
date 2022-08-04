341d67f84ad98e4e51ba95a625ab2da22eab836a
Adam Mills <adamjmills@google.com>
2022-08-04 15:09:43 +1000

    zephyr: Move ppc nodes under i2c bus
    Move all ppc nodes under the i2c bus nodes instead of named-usbc-port
    nodes. The change should simplify the upstream process of the ppc chip
    drivers.
    
    BUG=b:240617931
    TEST=zmake testall; zephyr/zmake/run_tests.sh
    BRANCH=main
    
    Signed-off-by: Adam Mills <adamjmills@google.com>
    Change-Id: I4fcf95cd200fc82b3fedd4b3bc166e8aa8243c79

--- zephyr/shim/include/usbc/ppc.h
+++ zephyr/shim/include/usbc/ppc.h
@@ -15,32 +15,26 @@
 #include "usbc/utils.h"
 #include "usbc_ppc.h"
 
-#define PPC_ID(id) DT_CAT(PPC_, id)
-#define PPC_ID_WITH_COMMA(id) PPC_ID(id),
-#define PPC_ALT_FOR(alt_id) USBC_PORT(DT_PHANDLE(alt_id, alternate_for))
-
-#define PPC_ALT_ENUM(id)                                 \
-	COND_CODE_1(DT_NODE_HAS_PROP(id, alternate_for), \
-		    (PPC_ID_WITH_COMMA(id)), ())
-
-enum ppc_chips_alt_id {
-	DT_FOREACH_STATUS_OKAY(NX20P348X_COMPAT, PPC_ALT_ENUM)
-		DT_FOREACH_STATUS_OKAY(RT1739_PPC_COMPAT, PPC_ALT_ENUM)
-			DT_FOREACH_STATUS_OKAY(SN5S330_COMPAT, PPC_ALT_ENUM)
-				DT_FOREACH_STATUS_OKAY(SYV682X_COMPAT,
-						       PPC_ALT_ENUM)
-					PPC_CHIP_ALT_COUNT
-};
-
 extern struct ppc_config_t ppc_chips_alt[];
 
-#define PPC_ENABLE_ALTERNATE(nodelabel)                                  \
-	do {                                                             \
-		BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(nodelabel)),    \
-			     "PPC alternate node does not exist");       \
-		memcpy(&ppc_chips[PPC_ALT_FOR(DT_NODELABEL(nodelabel))], \
-		       &ppc_chips_alt[PPC_ID(DT_NODELABEL(nodelabel))],  \
-		       sizeof(struct ppc_config_t));                     \
+#define ALT_PPC_CHIP_CHK(usbc_id) || DT_NODE_HAS_PROP(usbc_id, ppc_alt)
+
+#define ALT_PPC_CHIP_CPY(usbc_id)                                            \
+	COND_CODE_1(                                                         \
+		DT_NODE_HAS_PROP(usbc_id, ppc),                              \
+		(COND_CODE_1(DT_NODE_HAS_PROP(usbc_id, ppc_alt),             \
+			     (memcpy(&ppc_chips[USBC_PORT_NEW(usbc_id)],     \
+				     &ppc_chips_alt[USBC_PORT_NEW(usbc_id)], \
+				     sizeof(struct ppc_config_t));),         \
+			     ())),                                           \
+		())
+
+#define PPC_ENABLE_ALTERNATE                                            \
+	do {                                                                \
+		BUILD_ASSERT((0 DT_FOREACH_STATUS_OKAY(named_usbc_port,     \
+						       ALT_PPC_CHIP_CHK)),  \
+			     "No USB node specifies a PPC alternate chip"); \
+		DT_FOREACH_STATUS_OKAY(named_usbc_port, ALT_PPC_CHIP_CPY)   \
 	} while (0)
 
 #endif /* ZEPHYR_CHROME_USBC_PPC_H */
