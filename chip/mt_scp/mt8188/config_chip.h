10abd56c888c85fd06ad0c6d70f8399542d49f2d
Tinghan Shen <tinghan.shen@mediatek.com>
2022-08-12 13:05:33 +0800

    WIP: 8188 enable dramnc section
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I1fa754992081e07333a43da5d7e520235c869643

--- chip/mt_scp/mt8188/config_chip.h
+++ chip/mt_scp/mt8188/config_chip.h
@@ -6,6 +6,7 @@
 #ifndef __CROS_EC_CONFIG_CHIP_MT8188_H
 #define __CROS_EC_CONFIG_CHIP_MT8188_H
 
+#define CONFIG_CHIP_MEMORY_REGIONS
 #define CONFIG_PANIC_CONSOLE_OUTPUT
 
 /* Add some space (0x100) before panic for jump data */
