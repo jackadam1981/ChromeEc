3147c18ee9d16a720b6311463391ffecf2f711c9
Raymond Chung <raymondchung@ami.corp-partner.google.com>
2022-08-15 18:03:18 +0800

    [TEST_ONLY, DO_NOT_MERGE] gaelin: Create functions
    BUG=None
    BRANCH=brask
    TEST=None
    
    Signed-off-by: Raymond Chung <raymondchung@ami.corp-partner.google.com>
    Change-Id: I319d94f8295e5c844d2b94d270724bbec0300a9b

--- board/gaelin/pwm.c
+++ board/gaelin/pwm.c
@@ -4,17 +4,12 @@
  */
 
 #include "common.h"
-
 #include "compile_time_macros.h"
 #include "hooks.h"
 #include "pwm.h"
 #include "pwm_chip.h"
 
 const struct pwm_t pwm_channels[] = {
-	[PWM_CH_LED_GREEN] = { .channel = 0,
-			       .flags = PWM_CONFIG_ACTIVE_LOW |
-					PWM_CONFIG_DSLEEP,
-			       .freq = 2000 },
 	[PWM_CH_FAN] = { .channel = 5,
 			 .flags = PWM_CONFIG_OPEN_DRAIN,
 			 .freq = 25000 },
@@ -35,6 +30,5 @@ static void board_pwm_init(void)
 	pwm_set_duty(PWM_CH_FAN, 100);
 
 	pwm_enable(PWM_CH_LED_RED, 1);
-	pwm_enable(PWM_CH_LED_GREEN, 1);
 }
 DECLARE_HOOK(HOOK_INIT, board_pwm_init, HOOK_PRIO_DEFAULT);
