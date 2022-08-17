cf907818751bd12f379e1b7bf217f8ac3a590229
Michał Barnaś <mb@semihalf.com>
2022-08-17 19:09:38 +0200

    zephyr: remove the msg NULL parameter in ztest macros
    This commit removes the last NULL parameter that was required by
    the zassert_* and zassume_* macros.
    
    BRANCH=main
    BUG=b:242982518
    TEST=run twister
    
    Change-Id: I8970ee0a755e103367c5edcdf0fd469337c66721
    Signed-off-by: Michał Barnaś <mb@semihalf.com>

--- zephyr/test/drivers/default/src/usb_pd_host_cmd.c
+++ zephyr/test/drivers/default/src/usb_pd_host_cmd.c
@@ -16,10 +16,10 @@ ZTEST_USER(usb_pd_host_cmd, test_host_command_hc_pd_ports)
 	struct host_cmd_handler_args args =
 		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_USB_PD_PORTS, 0, response);
 
-	zassert_ok(host_command_process(&args), NULL);
-	zassert_ok(args.result, NULL);
-	zassert_equal(args.response_size, sizeof(response), NULL);
-	zassert_equal(response.num_ports, CONFIG_USB_PD_PORT_MAX_COUNT, NULL);
+	zassert_ok(host_command_process(&args));
+	zassert_ok(args.result);
+	zassert_equal(args.response_size, sizeof(response));
+	zassert_equal(response.num_ports, CONFIG_USB_PD_PORT_MAX_COUNT);
 }
 
 ZTEST_SUITE(usb_pd_host_cmd, drivers_predicate_post_main, NULL, NULL, NULL,
