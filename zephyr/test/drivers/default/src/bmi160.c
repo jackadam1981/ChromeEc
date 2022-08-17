38aa812bc0ef13db03e6800c35c000027bf70435
Jeremy Bettis <jbettis@google.com>
2022-08-17 09:29:30 -0600

    zephyr: Add bmi160 test case of interrupting task
    In order to cover common/motion_sense.c:734 consistently, add a test
    that triggers an interrupt and verifies it was handled.
    
    BRANCH=None
    BUG=b:242765844
    TEST=./twister --coverage
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: I880bd9fbc8fd853120c87c20c12010c0e9364d98

--- zephyr/test/drivers/default/src/bmi160.c
+++ zephyr/test/drivers/default/src/bmi160.c
@@ -2159,3 +2159,26 @@ static void bmi160_after(void *state)
 
 ZTEST_SUITE(bmi160, drivers_predicate_pre_main, NULL, bmi160_before,
 	    bmi160_after, NULL);
+
+/** Cause an interrupt and verify the motion_sense task handled it. */
+ZTEST_USER(bmi160_tasks, test_irq_handling)
+{
+	struct bmi_emul_frame f[3];
+	const struct emul *emul = EMUL_DT_GET(BMI_NODE);
+
+	f[0].type = BMI_EMUL_FRAME_ACC;
+	f[0].acc_x = BMI_EMUL_1G / 10;
+	f[0].acc_y = BMI_EMUL_1G / 20;
+	f[0].acc_z = -(int)BMI_EMUL_1G / 30;
+	f[0].next = NULL;
+	bmi_emul_append_frame(emul, f);
+
+	bmi160_interrupt(0);
+	k_sleep(K_SECONDS(10));
+
+	/* Verify that the motion_sense_task read it. */
+	/* TODO(jbettis): How do I do this? */
+}
+
+ZTEST_SUITE(bmi160_tasks, drivers_predicate_post_main, NULL, bmi160_before,
+	    bmi160_after, NULL);
