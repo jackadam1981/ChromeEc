cd8f46ec9a530ea05e811d5a62ab0a9a7aa25652
Tinghan Shen <tinghan.shen@mediatek.com>
2022-08-18 15:20:06 +0800

    chip/mt_scp: add mt8188 core 1 IRQ group
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: Ic1d517f8e3097c46746ff12f8eff54baaec0165e

--- chip/mt_scp/mt8188/intc_group.c
+++ chip/mt_scp/mt8188/intc_group.c
@@ -7,8 +7,13 @@
 static struct intc_irq_group
 irqs[SCP_INTC_IRQ_COUNT] = {
 	/* 0 */
+#ifdef BOARD_GERALT_SCP_CORE1
+	[SCP_IRQ_GIPC_IN0]		= { INTC_GRP_0 },
+	[SCP_IRQ_GIPC_IN1]		= { INTC_GRP_7 },
+#else
 	[SCP_IRQ_GIPC_IN0]		= { INTC_GRP_7 },
 	[SCP_IRQ_GIPC_IN1]		= { INTC_GRP_0 },
+#endif
 	[SCP_IRQ_GIPC_IN2]		= { INTC_GRP_0 },
 	[SCP_IRQ_GIPC_IN3]		= { INTC_GRP_0 },
 	/* 4 */
