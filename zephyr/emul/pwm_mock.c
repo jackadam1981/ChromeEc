23fb62a7b2e5934f163c72895b7b6d038d1274f6
Ting Shen <phoenixshen@google.com>
2022-08-18 19:07:16 +0800

    zephyr: test: merge pwm_led_driver test into default binary
    BUG=none
    TEST=./twister -T zephyr/test/drivers
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I2b4c892b0c9e48d4871b808d865856ff8b374068

--- zephyr/emul/pwm_mock.c
+++ zephyr/emul/pwm_mock.c
@@ -10,6 +10,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/pwm.h>
 
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
 #include "pwm_mock.h"
 
 #define CYCLES_PER_SEC 1000000
@@ -67,3 +69,5 @@ static const struct pwm_driver_api pwm_mock_api = {
 			      CONFIG_KERNEL_INIT_PRIORITY_DEVICE,       \
 			      &pwm_mock_api);
 DT_INST_FOREACH_STATUS_OKAY(INIT_PWM_MOCK)
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
