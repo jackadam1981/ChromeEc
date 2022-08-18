d931d7fc2f10006cf6f9ecc16715c7cabe803aaa
Tinghan Shen <tinghan.shen@mediatek.com>
2022-08-18 15:24:20 +0800

    chip/mt_scp: add mt8188 core 1 chip config
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I9c0b23df315e241864209fe37664e436e8974fde

--- chip/mt_scp/config_chip.h
+++ chip/mt_scp/config_chip.h
@@ -8,7 +8,7 @@
 #endif
 
 #if defined(CHIP_VARIANT_MT8192) || defined(CHIP_VARIANT_MT8195) || defined(CHIP_VARIANT_MT8188) || \
-    defined(CHIP_VARIANT_MT8195_CORE1)
+    defined(CHIP_VARIANT_MT8195_CORE1) || defined(CHIP_VARIANT_MT8188_CORE1)
 #include "rv32i_common/config_chip.h"
 #endif
 
@@ -16,6 +16,6 @@
 #include "mt8195/config_chip.h"
 #endif
 
-#if defined(CHIP_VARIANT_MT8188)
+#if defined(CHIP_VARIANT_MT8188) || defined(CHIP_VARIANT_MT8188_CORE1)
 #include "mt8188/config_chip.h"
 #endif
