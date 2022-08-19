da4c64cc9f58c3217ca82561ea340b660c1b280a
Hank.Xie <hank.xie@quanta.corp-partner.google.com>
2022-08-19 05:30:52 -0400

    shotzo: Remove battery related configuration
    Shotzo has a charger but has no battery.
    Modify the code as below:
    1.Disbale CONFIG_BATTERY_SMART, CONFIG_BATTERY_FUEL_GAUGE,
    CONFIG_BATTERY_CUT_OFF, CONFIG_BATTERY_REQUESTS_NIL_WHEN_DEAD and
    CONFIG_BATTERY_REVIVE_DISCONNECT.
    2.Add fake battery subfunctions for charger functional requirements.
    3.Add a fake battery info for charger to set Vsys.
    4.Remove EC_BATTERY_PRES_ODL to avoid it action incorrectly.
    
    BUG=b:242949656, b:240356595
    BRANCH=none
    TEST=plug type-c adapter, check it works fine on shotzo
    
    Signed-off-by: Hank.Xie <hank.xie@quanta.corp-partner.google.com>
    Change-Id: I44baaa9e90052cf963dd66623bcb8db0a2830bbb

--- board/shotzo/board.h
+++ board/shotzo/board.h
@@ -12,9 +12,6 @@
 #define VARIANT_DEDEDE_EC_IT8320
 #include "baseboard.h"
 
-/* Battery */
-#define CONFIG_BATTERY_FUEL_GAUGE
-
 /* Charger */
 #define CONFIG_CHARGER_SM5803 /* C0 and C1: Charger */
 #define PD_MAX_VOLTAGE_MV 15000
@@ -59,8 +56,16 @@
 /* LED backlight controller */
 #define CONFIG_LED_DRIVER_OZ554
 
+/* Include math_util for round_divide used in charge_state_v2 */
+#define CONFIG_MATH_UTIL
+
 /* Unused Features */
 #undef CONFIG_BACKLIGHT_LID
+#undef CONFIG_BATTERY_CUT_OFF
+#undef CONFIG_BATTERY_PRESENT_GPIO
+#undef CONFIG_BATTERY_REQUESTS_NIL_WHEN_DEAD
+#undef CONFIG_BATTERY_REVIVE_DISCONNECT
+#undef CONFIG_BATTERY_SMART
 #undef CONFIG_CMD_KEYBOARD
 #undef CONFIG_HIBERNATE
 #undef CONFIG_KEYBOARD_BOOT_KEYS
@@ -100,21 +105,6 @@ enum temp_sensor_id {
 	TEMP_SENSOR_3,
 	TEMP_SENSOR_4,
 	TEMP_SENSOR_COUNT
-};
-
-/* List of possible batteries */
-enum battery_type {
-	BATTERY_DYNAPACK_COS,
-	BATTERY_DYNAPACK_ATL,
-	BATTERY_DYNAPACK_HIGHPOWER,
-	BATTERY_DYNAPACK_BYD,
-	BATTERY_SAMSUNG_SDI,
-	BATTERY_SIMPLO_COS,
-	BATTERY_SIMPLO_HIGHPOWER,
-	BATTERY_COS,
-	BATTERY_COS_2,
-	BATTERY_ATL,
-	BATTERY_TYPE_COUNT,
 };
 
 enum charge_port {
