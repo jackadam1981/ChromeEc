a9340dc27959a2aad133694e6d5c7083e23eadc5
Jeremy Bettis <jbettis@google.com>
2022-08-22 11:21:05 -0600

    zephyr: Add test for fprintf flushing
    Coverage is intermittent for
    third_party/zephyr/main/subsys/shell/shell_fprintf.c:27. Add test that
    forces a fprintf buffer flush, and read back from the console to make
    sure all the text was written.
    
    Turn off VT100 commands in the console, it just makes it harder to look
    at the shell output.
    
    BRANCH=None
    BUG=b:242764426
    TEST=./twister --coverage
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: I4bae1880ccfeaf8f6586d335d386def614281e8d

--- zephyr/test/drivers/default/src/console.c
+++ zephyr/test/drivers/default/src/console.c
@@ -5,7 +5,9 @@
 
 #include <zephyr/zephyr.h>
 #include <zephyr/ztest.h>
+#include <zephyr/shell/shell_dummy.h> /* nocheck */
 
+#include "console.h"
 #include "builtin/stdio.h"
 #include "test/drivers/test_state.h"
 
@@ -19,6 +21,28 @@ ZTEST_USER(console, printf_overflow)
 	zassert_equal(-EC_ERROR_OVERFLOW,
 		      crec_snprintf(buffer, 4, "%%%%%%%%%%"), NULL);
 	zassert_equal(0, strcmp(buffer, "%%%"), "got '%s'", buffer);
+}
+
+ZTEST_USER(console, shell_fprintf_full)
+{
+	const struct shell *shell_zephyr = get_ec_shell();
+	const char *buffer =
+		"This is a very long string, it will cause a buffer flush at "
+		"some point while printing to the shell. Long long text. Blah "
+		"blah. Long long text. Blah blah. Long long text. Blah blah.";
+	const char *outbuffer;
+	size_t buffer_size;
+
+	zassert_true(strlen(buffer) >= shell_zephyr->fprintf_ctx->buffer_size,
+		     "buffer is too short, fix test.");
+
+	shell_backend_dummy_clear_output(shell_zephyr); /* nocheck */
+	shell_fprintf(shell_zephyr, SHELL_NORMAL, "%s", buffer);
+
+	outbuffer = shell_backend_dummy_get_output(shell_zephyr, /* nocheck */
+						   &buffer_size);
+	zassert_true(strncmp(outbuffer, buffer, strlen(buffer)) == 0,
+		     "Invalid console output %s", outbuffer);
 }
 
 ZTEST_SUITE(console, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
