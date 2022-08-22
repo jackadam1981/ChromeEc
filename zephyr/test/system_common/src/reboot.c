4d0eaf0167a3bb8e65cbcc717ba2dfa1ed661886
Fabio Baltieri <fabiobaltieri@google.com>
2022-08-22 11:32:25 +0000

    zephyr: test: add tests for EC_CMD_GET_BUILD_INFO host command
    Add a couple tests for EC_CMD_GET_BUILD_INFO host command, not much
    going on here, just returning the expected build string.
    
    BRANCH=none
    BUG=b:236075091
    TEST=./twister -v -T zephyr/test/system_common
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>
    Change-Id: I1d1ffe88072d46534b5a123a52e19f8f18da6767

--- zephyr/test/system_common/src/reboot.c
+++ zephyr/test/system_common/src/reboot.c
@@ -4,11 +4,14 @@
  */
 
 #include <zephyr/device.h>
+#include <zephyr/fff.h>
 #include <zephyr/ztest_assert.h>
 #include <zephyr/ztest_test_new.h>
 
 #include "host_command.h"
 #include "system.h"
+
+DEFINE_FFF_GLOBALS;
 
 /* This has to be declared here so it's visible in system_reset(), so no point
  * passing it through the fixture parameter of the ztest APIs.
@@ -274,4 +277,48 @@ ZTEST(console_cmd_hibernate, test_hibernate_args)
 	zassert_equal(reboot_fixture.hibernate_microseconds, 456,
 		      "Unexpected hibernate_secondst: %d",
 		      reboot_fixture.hibernate_microseconds);
+}
+
+ZTEST_SUITE(host_cmd_get_build_info, NULL, NULL, NULL, NULL, NULL);
+
+FAKE_VALUE_FUNC(const char *, system_get_build_info);
+
+ZTEST(host_cmd_get_build_info, test_get_build_info)
+{
+	int ret;
+	char resp[1024];
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_GET_BUILD_INFO, 0, resp);
+
+	RESET_FAKE(system_get_build_info);
+	system_get_build_info_fake.return_val = "i-am-a-version";
+
+	ret = host_command_process(&args);
+
+	zassert_equal(ret, EC_SUCCESS, "Unexpected return value: %d", ret);
+	zassert_equal(strcmp(resp, "i-am-a-version"), 0,
+		      "Unexpected response: %s", resp);
+	zassert_equal(system_get_build_info_fake.call_count, 1,
+		      "Unexpected call count: %d",
+		      system_get_build_info_fake.call_count);
+}
+
+ZTEST(host_cmd_get_build_info, test_get_build_info_truncated)
+{
+	int ret;
+	char resp[8];
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_GET_BUILD_INFO, 0, resp);
+
+	RESET_FAKE(system_get_build_info);
+	system_get_build_info_fake.return_val = "i-am-a-long-version";
+
+	ret = host_command_process(&args);
+
+	zassert_equal(ret, EC_SUCCESS, "Unexpected return value: %d", ret);
+	zassert_equal(strcmp(resp, "i-am-a-"), 0, "Unexpected response: %s",
+		      resp);
+	zassert_equal(system_get_build_info_fake.call_count, 1,
+		      "Unexpected call count: %d",
+		      system_get_build_info_fake.call_count);
 }
