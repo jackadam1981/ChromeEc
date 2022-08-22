8f36d3963db1b19eaf92f11da9399eb96a8b1274
Tinghan Shen <tinghan.shen@mediatek.com>
2022-08-22 11:53:25 +0800

    board/cherry_scp_core1: add cherry SCP core 1 board
    Add cherry SCP core 1 board and modify SRAM configurations to
    use the spare 64KB SRAM.
    
    BRANCH=none
    BUG=b:193814857
    TEST=make BOARD=cherry_scp
    TEST=make BOARD=cherry_scp_core1
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: I23cfcb7dd24f67d27b0ee8adefe4b58c0b67d059
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>

--- chip/mt_scp/mt8195/build.mk
+++ chip/mt_scp/mt8195/build.mk
@@ -7,6 +7,8 @@
 chip-y+=$(CHIP_VARIANT)/uart.o
 chip-y+=$(CHIP_VARIANT)/clock.o
 chip-y+=$(CHIP_VARIANT)/clock_s3.o
-chip-y+=$(CHIP_VARIANT)/video.o
 chip-y+=$(CHIP_VARIANT)/intc_group.o
 
+ifeq ($(BOARD),cherry_scp)
+chip-y+=$(CHIP_VARIANT)/video.o
+endif
