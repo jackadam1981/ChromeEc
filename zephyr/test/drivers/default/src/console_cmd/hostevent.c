9e139b7418b359e3811acc625ad7be8643ea32e2
Keith Short <keithshort@chromium.org>
2022-08-22 11:48:49 -0600

    zephyr test: test hostevent console command
    Test the "hostevent" command with no arguments.
    
    BUG=b:236074810
    BRANCH=none
    TEST=./twister -T zephyr/test/drivers
    
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: Ie8f3e67c4039a78d697ce326f17b3212da477855

---
+++ zephyr/test/drivers/default/src/console_cmd/hostevent.c
@@ -0,0 +1,60 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/shell/shell.h>
+#include <zephyr/ztest.h>
+
+#include "console.h"
+#include "ec_commands.h"
+#include "include/lpc.h"
+#include "test/drivers/test_state.h"
+#include "test/drivers/utils.h"
+
+struct console_cmd_hostevent {
+	host_event_t lpc_host_events;
+	host_event_t lpc_host_event_mask[LPC_HOST_EVENT_COUNT];
+};
+
+static void *console_cmd_hostevent_setup(void)
+{
+	static struct console_cmd_hostevent fixture = { 0 };
+
+	return &fixture;
+}
+
+static void console_cmd_hostevent_before(void *fixture)
+{
+	struct console_cmd_hostevent *f = fixture;
+
+	/* Save all current events and masks */
+	f->lpc_host_events = lpc_get_host_events();
+
+	for (int i = 0; i < LPC_HOST_EVENT_COUNT; i++) {
+		f->lpc_host_event_mask[i] = lpc_get_host_events_by_type(i);
+	}
+}
+
+static void console_cmd_hostevent_after(void *fixture)
+{
+	struct console_cmd_hostevent *f = fixture;
+
+	/* Restore all current events and masks */
+	lpc_set_host_event_state(f->lpc_host_events);
+
+	for (int i = 0; i < LPC_HOST_EVENT_COUNT; i++) {
+		lpc_set_host_event_mask(i, f->lpc_host_event_mask[i]);
+	}
+}
+
+/* hostevent with no arguments */
+ZTEST_USER(console_cmd_hostevent, test_hostevent)
+{
+	zassert_ok(shell_execute_cmd(get_ec_shell(), "hostevent"),
+		   "Failed default print");
+}
+
+ZTEST_SUITE(console_cmd_hostevent, drivers_predicate_post_main,
+	    console_cmd_hostevent_setup, console_cmd_hostevent_before,
+	    console_cmd_hostevent_after, NULL);
