d360bbc7abe58fe97f0ca091b92e7758cdfb17be
Tom Hughes <tomhughes@chromium.org>
2022-08-23 10:18:26 -0700

    fpsensor: Compile more fingerprint files with C++
    BRANCH=none
    BUG=b:234181908
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I3f8d1a95f0f1e844f0bf1e56e5514dda521979b4

--- include/fpsensor.h
+++ include/fpsensor.h
@@ -13,6 +13,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #ifndef SPI_FP_DEVICE
 #define SPI_FP_DEVICE (&spi_devices[0])
 #endif
@@ -209,5 +213,9 @@ int fp_finger_enroll(uint8_t *image, int *completion);
  * @return EC_SUCCESS on success
  */
 int fp_maintenance(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __CROS_EC_FPSENSOR_H */
