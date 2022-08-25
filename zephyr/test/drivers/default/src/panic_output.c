4d957c630a4fd72cc6643a4e0a67b67ea0476338
Yuval Peress <peress@google.com>
2022-08-24 23:50:52 -0600

    test: Add basic tests exercising the panic output API
    Exercise some of the panic output API. Some of the basic printing API
    cannot yet be fully tested. But a fix upstream is coming to allow us
    to use the 'console' harness features to parse the output log and
    pass/fail the test based on output.
    
    BRANCH=none
    BUG=none
    TEST=twister -s zephyr/test/drivers/drivers.default
    
    Signed-off-by: Yuval Peress <peress@google.com>
    Change-Id: I8e56e8bcdaa0e095891c38a26e56ced8f3a8d4d0

---
+++ zephyr/test/drivers/default/src/panic_output.c
@@ -0,0 +1,41 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/ztest.h>
+
+#include "panic.h"
+#include "test/drivers/test_state.h"
+
+ZTEST_SUITE(panic_output, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
+
+ZTEST(panic_output, test_panic_printf)
+{
+	panic_printf("test output string from %s\n", __FUNCTION__ );
+}
+
+ZTEST(panic_output, test_panic_puts)
+{
+	panic_puts("test output string\n");
+}
+
+ZTEST(panic_output, test_panic_sw_reason_is_valid)
+{
+	zassert_false(panic_sw_reason_is_valid(PANIC_SW_BASE - 1), NULL);
+	/* PANIC_SW_DIV_ZERO */
+	zassert_true(panic_sw_reason_is_valid(PANIC_SW_BASE), NULL);
+	/* PANIC_SW_STACK_OVERFLOW */
+	zassert_true(panic_sw_reason_is_valid(PANIC_SW_BASE + 1), NULL);
+	/* PANIC_SW_PD_CRASH */
+	zassert_true(panic_sw_reason_is_valid(PANIC_SW_BASE + 2), NULL);
+	/* PANIC_SW_ASSERT */
+	zassert_true(panic_sw_reason_is_valid(PANIC_SW_BASE + 3), NULL);
+	/* PANIC_SW_WATCHDOG */
+	zassert_true(panic_sw_reason_is_valid(PANIC_SW_BASE + 4), NULL);
+	/* PANIC_SW_RNG */
+	zassert_true(panic_sw_reason_is_valid(PANIC_SW_BASE + 5), NULL);
+	/* PANIC_SW_PMIC_FAULT */
+	zassert_true(panic_sw_reason_is_valid(PANIC_SW_BASE + 6), NULL);
+	zassert_false(panic_sw_reason_is_valid(PANIC_SW_BASE + 7), NULL);
+}
