9ac4a8db4425efea16d5c2f2ecebedc2abbdb146
B R, Harsha <harsha.b.r@intel.com>
2022-08-26 15:27:12 +0530

    usbc: soix fails due to SOP_DISC_DONE notify events to kernel
    In USB PD 2.0, mark identity discovery as fail and notify kernel
    of SOP_DISC_DONE event only when there is DR Swap rejection.
    If DR Swap rejection is not considered then SOP_DISC_DONE event
    will be sent to kernel every time port discovery happens resulting
    in s0ix failure.
    
    BUG=b:238340215
    TEST=Hotplug PD 2.0 charger to DUT and check if kernel receives
    get the corresponding notification. Execute s0ix test.
    BRANCH=none
    
    Signed-off-by: B R, Harsha <harsha.b.r@intel.com>
    Change-Id: I8cdd814e2c6bad4e63141bd77e2a8cd224cc688f

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -1945,21 +1945,6 @@ __maybe_unused static bool pe_attempt_port_discovery(int port)
 		return true;
 	}
 
-	/*
-	 * An edge case of DR Swap fail (port still UFP) and partner in PD 2.0.
-	 * PD 2.0 allows only DFP to initiate Discover Identity, but partner may
-	 * reject a DR Swap.
-	 */
-	if (pe[port].data_role == PD_ROLE_UFP &&
-	    prl_get_rev(port, TCPCI_MSG_SOP) == PD_REV20) {
-		pd_set_identity_discovery(port, TCPCI_MSG_SOP, PD_DISC_FAIL);
-		pd_set_identity_discovery(port, TCPCI_MSG_SOP_PRIME,
-					  PD_DISC_FAIL);
-		pd_notify_event(port, PD_STATUS_EVENT_SOP_DISC_DONE);
-		pd_notify_event(port, PD_STATUS_EVENT_SOP_PRIME_DISC_DONE);
-		return false;
-	}
-
 	/* Apply Port Discovery VCONN Swap Policy */
 	if (IS_ENABLED(CONFIG_USBC_VCONN) &&
 	    port_discovery_vconn_swap_policy(
@@ -4582,6 +4567,25 @@ static void pe_drs_send_swap_run(int port)
 			} else if ((type == PD_CTRL_REJECT) ||
 				   (type == PD_CTRL_WAIT) ||
 				   (type == PD_CTRL_NOT_SUPPORTED)) {
+				/*
+				 * An edge case of DR Swap fail (port still
+				 * UFP) and partner in PD 2.0. PD 2.0 allows
+				 * only DFP to initiate Discover Identity,
+				 * but partner may reject a DR Swap.
+				 */
+				if (pe[port].data_role == PD_ROLE_UFP &&
+				    prl_get_rev(port,
+					  TCPCI_MSG_SOP) == PD_REV20 &&
+					  type == PD_CTRL_REJECT) {
+					pd_set_identity_discovery(port,
+					  TCPCI_MSG_SOP, PD_DISC_FAIL);
+					pd_set_identity_discovery(port,
+					  TCPCI_MSG_SOP_PRIME, PD_DISC_FAIL);
+					pd_notify_event(port,
+					  PD_STATUS_EVENT_SOP_DISC_DONE);
+					pd_notify_event(port,
+					  PD_STATUS_EVENT_SOP_PRIME_DISC_DONE);
+				}
 				pe_set_ready_state(port);
 				return;
 			}
