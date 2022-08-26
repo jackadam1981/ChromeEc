f0764df378d43e7aa02e682c3bba6574776e550f
Tom Hughes <tomhughes@chromium.org>
2022-08-26 15:30:03 -0700

    test/fpsensor_crypto: Convert to C++
    BRANCH=none
    BUG=b:234181908
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I26e7b7bcc8f2a7ee7d1ba19cb8cd8c0f7e22b2cf

--- test/fpsensor_crypto.cc
+++ test/fpsensor_crypto.cc
@@ -5,11 +5,15 @@
 
 #include <stdbool.h>
 
+#include "compile_time_macros.h"
+
+#include "fpsensor_crypto.h"
+#include "fpsensor_state.h"
+
+extern "C" {
 #include "builtin/assert.h"
 #include "common.h"
 #include "ec_commands.h"
-#include "fpsensor_crypto.h"
-#include "fpsensor_state.h"
 #include "mock/fpsensor_crypto_mock.h"
 #include "mock/fpsensor_state_mock.h"
 #include "mock/rollback_mock.h"
@@ -18,6 +22,8 @@
 #include "util.h"
 
 extern int get_ikm(uint8_t *ikm);
+}
+
 
 static const uint8_t fake_positive_match_salt[] = {
 	0x04, 0x1f, 0x5a, 0xac, 0x5f, 0x79, 0x10, 0xaf,
