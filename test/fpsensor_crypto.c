663f31acfce2ca89fd1c02fc3becaa63b752e3a7
Firas Sammoura <fsammoura@google.com>
2022-08-29 19:36:26 +0000

    test: Add test for get_ikm succeed
    Add test for get_ikm when the tpm seed is set and rollback get secret
    succeeds.
    
    BRANCH=None
    BUG=b:242720240
    TEST=make run-fpsensor_crypto
    TEST=make runhosttests
    
    Signed-off-by: Firas Sammoura <fsammoura@google.com>
    Change-Id: I14d0887aa2ecafe4565141a89ad5fb43317cc1a7

--- test/fpsensor_crypto.c
+++ test/fpsensor_crypto.c
@@ -125,6 +125,12 @@ test_static int test_get_ikm_tpm_seed_set(void)
 	/* GIVEN that reading the rollback secret will succeed. */
 	mock_ctrl_rollback.get_secret_fail = false;
 
+	/* GIVEN that the TPM seed has been set. */
+	TEST_ASSERT(fp_tpm_seed_is_set());
+
+	/* THEN get_ikm will succeed. */
+	TEST_ASSERT(get_ikm(ikm) == EC_SUCCESS);
+
 	return EC_SUCCESS;
 }
 
