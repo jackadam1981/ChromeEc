a90de9cf44bdb72f30471fa1c1cbb70f87630daa
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2022-08-29 16:21:35 +0800

    zephyr: implement keyboard_raw_is_input_low() function
    Implement keyboard_raw_is_input_low() function for factory
    testing KSIand KSO pins as GPIO.
    
    BRANCH=none
    BUG=none
    TEST=zmake -D build krabby --clobber
         zmake -D build nereid --clobber
    
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>
    Change-Id: Ic354df730e85cb655f6d1572dc64d0c0fd9345d9

--- zephyr/shim/chip/it8xxx2/keyboard_raw.c
+++ zephyr/shim/chip/it8xxx2/keyboard_raw.c
@@ -9,19 +9,20 @@
 #include "keyboard_raw.h"
 
 #include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/gpio/gpio_kscan_it8xxx2.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 #include <soc.h>
 
 /**
- * Return true if the current value of the given input GPIO port is zero
+ * Return true if the current value of the given gpioksi/gpioksoh/gpioksol
+ * port is zero
  */
 int keyboard_raw_is_input_low(int port, int id)
 {
-	/*
-	 * TODO: implement for factory testing KSI and KSO pin as GPIO
-	 *       function.
-	 */
-	return 0;
+	const struct device *io_dev = it8xxx2_get_gpio_kscan_dev(port);
+
+	return gpio_pin_get_raw(io_dev, id) == 0;
 }
