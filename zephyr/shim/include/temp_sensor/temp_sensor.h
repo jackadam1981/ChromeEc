ce343ed15047bd85942a33e79ff8e620f95f4b72
Fabio Baltieri <fabiobaltieri@google.com>
2022-08-30 15:04:56 +0000

    zephyr: shim: use DT_FOREACH_CHILD_STATUS_OKAY_SEP where applicable
    Use DT_FOREACH_CHILD_STATUS_OKAY_SEP for _COMMA lists where applicable.
    This allows dropping some macros that were only being used to add a
    comma to an existing macro.
    
    BRANCH=none
    BUG=none
    TEST=zmake buildall
    
    Cq-Depend: chromium:todo
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>
    Change-Id: Ie58ae12c01ef6afc193e0fb3c4b6cbd1a224518e

--- zephyr/shim/include/temp_sensor/temp_sensor.h
+++ zephyr/shim/include/temp_sensor/temp_sensor.h
@@ -12,7 +12,6 @@
 #ifdef CONFIG_PLATFORM_EC_TEMP_SENSOR
 
 #define ZSHIM_TEMP_SENSOR_ID(node_id) DT_STRING_UPPER_TOKEN(node_id, enum_name)
-#define TEMP_SENSOR_ID_WITH_COMMA(node_id) ZSHIM_TEMP_SENSOR_ID(node_id),
 
 #define HAS_POWER_GOOD_PIN(node_id) DT_NODE_HAS_PROP(node_id, power_good_pin) ||
 #define ANY_INST_HAS_POWER_GOOD_PIN \
@@ -20,27 +19,23 @@
 
 enum temp_sensor_id {
 #if DT_NODE_EXISTS(DT_PATH(named_temp_sensors))
-	DT_FOREACH_CHILD(DT_PATH(named_temp_sensors), TEMP_SENSOR_ID_WITH_COMMA)
+	DT_FOREACH_CHILD_SEP(DT_PATH(named_temp_sensors), ZSHIM_TEMP_SENSOR_ID,
+			     (, )),
 #endif /* named_temp_sensors */
-		TEMP_SENSOR_COUNT
+	TEMP_SENSOR_COUNT
 };
-
-#undef TEMP_SENSOR_ID_WITH_COMMA
 
 /* PCT2075 access array */
 #define ZSHIM_PCT2075_SENSOR_ID(node_id) \
 	DT_STRING_UPPER_TOKEN(node_id, pct2075_name)
-#define PCT2075_SENSOR_ID_WITH_COMMA(node_id) ZSHIM_PCT2075_SENSOR_ID(node_id),
 
 enum pct2075_sensor {
 #if DT_HAS_COMPAT_STATUS_OKAY(cros_ec_temp_sensor_pct2075)
-	DT_FOREACH_STATUS_OKAY(cros_ec_temp_sensor_pct2075,
-			       PCT2075_SENSOR_ID_WITH_COMMA)
+	DT_FOREACH_STATUS_OKAY_SEP(cros_ec_temp_sensor_pct2075,
+				   ZSHIM_PCT2075_SENSOR_ID, (, ))
 #endif
 		PCT2075_COUNT,
 };
-
-#undef PCT2075_SENSOR_ID_WITH_COMMA
 
 /* TMP112 access array */
 #define ZSHIM_TMP112_SENSOR_ID(node_id) \
