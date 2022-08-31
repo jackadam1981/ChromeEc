fbb1dcffb54723e27cfedf07067858e6b09fc324
Adam Mills <adamjmills@google.com>
2022-08-31 13:00:24 +1000

    zephyr: Fix PPC chips on the i2c nodes for herobrine variants.
    Two of the herobrine variants (hoglin and villager) use different PPC
    chips to the other herobrine boards. To facilitate this difference
    between the boards, variant specific i2c DTS files needed to be created.
    
    BUG=b:243759491
    TEST=zmake build -a; ./twister -T zephyr/test/
    BRANCH=main
    
    Signed-off-by: Adam Mills <adamjmills@google.com>
    Change-Id: I00748d71d458e847e141dbbfa3a1ffcf7fa63379

--- zephyr/projects/herobrine/BUILD.py
+++ zephyr/projects/herobrine/BUILD.py
@@ -38,6 +38,7 @@ register_variant(
         here / "display.dts",
         here / "battery_evoker.dts",
         here / "gpio_evoker.dts",
+        here / "i2c_evoker.dts",
         here / "led_pins_evoker.dts",
         here / "led_policy_evoker.dts",
         here / "motionsense.dts",
@@ -53,6 +54,7 @@ register_variant(
         here / "display.dts",
         here / "battery_herobrine.dts",
         here / "gpio.dts",
+        here / "i2c_herobrine.dts",
         here / "led_pins_herobrine.dts",
         here / "led_policy_herobrine.dts",
         here / "motionsense.dts",
@@ -67,6 +69,7 @@ register_variant(
     extra_dts_overlays=[
         here / "battery_hoglin.dts",
         here / "gpio_hoglin.dts",
+        here / "i2c_hoglin.dts",
         here / "led_pins_hoglin.dts",
         here / "led_policy_hoglin.dts",
         here / "motionsense_hoglin.dts",
@@ -81,6 +84,7 @@ register_variant(
     extra_dts_overlays=[
         here / "battery_villager.dts",
         here / "gpio_villager.dts",
+        here / "i2c_villager.dts",
         here / "led_pins_villager.dts",
         here / "led_policy_villager.dts",
         here / "motionsense_villager.dts",
@@ -95,6 +99,7 @@ register_variant(
     extra_dts_overlays=[
         here / "battery_hoglin.dts",
         here / "gpio_hoglin.dts",
+        here / "i2c_hoglin.dts",
         here / "led_pins_hoglin.dts",
         here / "led_policy_hoglin.dts",
         here / "motionsense_hoglin.dts",
