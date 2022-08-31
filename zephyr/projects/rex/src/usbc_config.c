0d7c6ce4815e3ec23bfb43c3b194bd59ae898885
Vijay Hiremath <vijay.p.hiremath@intel.com>
2022-08-30 17:40:02 -0700

    mtlrvp: rex: Send VW based overcurrent to AP
    BUG=b:243120083
    BRANCH=none
    TEST=Observd 'usb usb3-portX: over-current condition' on MTLRVP
         Kernel console
    
    Change-Id: Ia4ac4b71622e91377410458efd57b1fa75c985a7
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

--- zephyr/projects/rex/src/usbc_config.c
+++ zephyr/projects/rex/src/usbc_config.c
@@ -54,10 +54,8 @@ DECLARE_HOOK(HOOK_INIT, usbc_interrupt_init, HOOK_PRIO_POST_I2C);
 
 void board_overcurrent_event(int port, int is_overcurrented)
 {
-	/*
-	 * TODO: Meteorlake PCH does not use Physical GPIO for over current
-	 * error, hence Send 'Over Current Virtual Wire' eSPI signal.
-	 */
+	espi_vw_set_wire(port + ESPI_VWIRE_SIGNAL_SLV_GPIO_0,
+			 !is_overcurrented);
 }
 
 void sbu_fault_interrupt(enum gpio_signal signal)
