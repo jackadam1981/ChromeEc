00fad4ea17c2eee9530fff2511927bc34bc0f68c
Scott Collyer <scollyer@google.com>
2022-08-31 19:09:15 -0700

    rex: Fix the pin type for usb_c0_rt_3p3_sx_en
    This GPIO was being set as an open drain output, but it should be a
    push-pull. This CL modifies its entry in generated.dts. There is a
    TODO to review the EC GPIO input spreadsheet so that generated.dts
    will not require edits. This change is being added now to support
    power on activies.
    
    BRANCH=none
    BUG=b:244441996
    TEST=zmake build rex. Verified that HB is powered up when
    usb_c0_rt_3p3_sx_en is driven high.
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: Ia378a5772a41214c0f49de053d53d5af82c104e8

--- driver/retimer/bb_retimer.c
+++ driver/retimer/bb_retimer.c
@@ -149,7 +149,7 @@ __overridable int bb_retimer_power_enable(const struct usb_mux *me, bool enable)
 		 * Allow 1ms time for the retimer to power up lc_domain
 		 * which powers I2C controller within retimer
 		 */
-		msleep(1);
+		msleep(50);
 	} else {
 		gpio_set_level(control->retimer_rst_gpio, 0);
 		msleep(1);
