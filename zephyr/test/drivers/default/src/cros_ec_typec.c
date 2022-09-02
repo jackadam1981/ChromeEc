e9ee1a50dbd72301465e1af66633af1bb1ff74eb
Eric Yilun Lin <yllin@chromium.org>
2022-09-02 17:17:34 +0800

    usb_pe: clear past hard reset events when boot AP
    Hard reset events happened in chipset off state should not
    inform the cros-ec-typec when boot up.
    Clears the hard reset events to prevent cros-ec-typec dropping
    the type-c config.
    
    BUG=b:239386916
    TEST=hard reset events happened before boot won't sent to AP
    TEST=./twister -T zephyr/test
    BRANCH=none
    
    Change-Id: Idcf201da3395cabf2be8969cd37c6d92ca8c1bfc
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

---
+++ zephyr/test/drivers/default/src/cros_ec_typec.c
@@ -0,0 +1,51 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Tests for the intercations for EC and kernel cros_ec_typec module
+ */
+
+#include <zephyr/ztest.h>
+
+#include "test/drivers/test_state.h"
+#include "test/drivers/utils.h"
+#include "usb_pd.h"
+
+#define TEST_PORT 0
+BUILD_ASSERT(TEST_PORT == 0);
+
+static void cros_ec_typec_before(void *data)
+{
+	ARG_UNUSED(data);
+
+	zassume_true(chipset_in_state(CHIPSET_STATE_ON), NULL);
+}
+
+static void cros_ec_typec_after(void *data)
+{
+	ARG_UNUSED(data);
+
+	test_set_chipset_to_s0();
+}
+
+ZTEST(cros_ec_typec, verify_hard_reset_event_cleared_at_startup)
+{
+	test_set_chipset_to_g3();
+
+	zassert_false(pd_get_events(TEST_PORT) & PD_STATUS_EVENT_HARD_RESET,
+		      NULL);
+
+	pd_notify_event(TEST_PORT, PD_STATUS_EVENT_HARD_RESET);
+
+	zassert_true(pd_get_events(TEST_PORT) & PD_STATUS_EVENT_HARD_RESET,
+		     NULL);
+
+	test_set_chipset_to_power_level(POWER_S3);
+	k_sleep(K_SECONDS(2));
+
+	zassert_false(pd_get_events(TEST_PORT) & PD_STATUS_EVENT_HARD_RESET,
+		      NULL);
+}
+
+ZTEST_SUITE(cros_ec_typec, drivers_predicate_post_main, NULL,
+	    cros_ec_typec_before, cros_ec_typec_after, NULL);
