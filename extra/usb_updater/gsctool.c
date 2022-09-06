911da24d1834c703405bb75ca7ce0099c39414e6
Vadim Bendebury <vbendeb@google.com>
2022-09-06 10:26:20 -0700

    gsctool: fix compilation problems
    It turns out that due to a bug in ./Makefile gsctool is always
    compiled with -O0. Fixing the make file bug highlighted a bug in
    gsctool code where a return value of getline() was left unchecked.
    
    This patch fixes the gsctool compilation problem.
    
    BUG=b:245028043
    TEST=Verified that gsctool now builds successfully with -O3. Verified
         that attempts to set password are properly failed when entered
         passwords are of different length and succeed otherwise.
    
    Change-Id: I8a3f337f3d40ff14a23162fab796b5b02f6cc664
    Signed-off-by: Vadim Bendebury <vbendeb@google.com>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -1983,18 +1983,20 @@ static uint32_t common_process_password(struct transfer_descriptor *td,
 	printf("Enter password:");
 	len = getline(&password, &len, stdin);
 	printf("Re-enter password:");
-	getline(&password_copy, &copy_len, stdin);
+	copy_len = getline(&password_copy, &copy_len, stdin);
 
 	/* Restore command line echo. */
 	tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
 
 	/* Empty password will still have the newline. */
-	if ((len <= 1) || !password_copy) {
+	if ((len <= 1) || !password_copy || (copy_len != len)) {
+		fprintf(stderr, "Error reading password\n");
 		if (password)
 			free(password);
 		if (password_copy)
 			free(password_copy);
-		fprintf(stderr, "Error reading password\n");
+		if (copy_len != len)
+			fprintf(stderr, "Password length mismatch\n");
 		exit(update_error);
 	}
 
