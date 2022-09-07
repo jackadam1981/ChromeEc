040da8c7060bfc7cfc1815c635218d70cb2d8745
Will Tsai <will_tsai@wistron.corp-partner.google.com>
2022-09-07 19:40:00 +0800

    dps: limit the minimum operating power when AC only
    We found that the system will fail to boot without battery if PDO 5V/3A
    is selected. To prevent boot failed, limit the minimum operating power
    to the larger PDO when the system is booting up without battery.
    
    BUG=b:240533589
    TEST=On Joxer, confirm the selected power is equal to 9V when system
    booting, and the system boot up successfully.
    TEST=./twister -v -i --coverage -p native_posix -p unit_testing
    BRANCH=none
    
    Signed-off-by: Will Tsai <will_tsai@wistron.corp-partner.google.com>
    Change-Id: I5d9655b09eead6791c5a3bf161e574f26f6beb02

--- zephyr/test/drivers/default/src/integration/usbc/usb_5v_3a_pd_source.c
+++ zephyr/test/drivers/default/src/integration/usbc/usb_5v_3a_pd_source.c
@@ -7,16 +7,22 @@
 #include <zephyr/sys/slist.h>
 #include <zephyr/ztest.h>
 
+#include "battery.h"
 #include "battery_smart.h"
+#include "dps.h"
 #include "emul/emul_isl923x.h"
 #include "emul/emul_smart_battery.h"
 #include "emul/tcpc/emul_tcpci_partner_src.h"
 #include "system.h"
+#include "task.h"
 #include "test/drivers/test_state.h"
 #include "test/drivers/utils.h"
 #include "usb_pd.h"
 
 #define BATTERY_NODE DT_NODELABEL(battery)
+
+#define GPIO_BATT_PRES_ODL_PATH DT_PATH(named_gpios, ec_batt_pres_odl)
+#define GPIO_BATT_PRES_ODL_PORT DT_GPIO_PIN(GPIO_BATT_PRES_ODL_PATH, gpios)
 
 struct usb_attach_5v_3a_pd_source_fixture {
 	struct tcpci_partner_data source_5v_3a;
@@ -56,6 +62,16 @@ static void usb_attach_5v_3a_pd_source_after(void *data)
 	struct usb_attach_5v_3a_pd_source_fixture *fixture = data;
 
 	disconnect_source_from_port(fixture->tcpci_emul, fixture->charger_emul);
+}
+
+static void control_battery_present(bool present)
+{
+	const struct device *dev =
+		DEVICE_DT_GET(DT_GPIO_CTLR(GPIO_BATT_PRES_ODL_PATH, gpios));
+
+	/* 0 means battery present */
+	zassume_ok(gpio_emul_input_set(dev, GPIO_BATT_PRES_ODL_PORT, !present),
+		   NULL);
 }
 
 ZTEST_SUITE(usb_attach_5v_3a_pd_source, drivers_predicate_post_main,
@@ -281,4 +297,28 @@ ZTEST_F(usb_attach_5v_3a_pd_source, test_uvdm_ignored)
 
 	zassert_false(tcpm_response,
 		      "Sent unstructured VDM to TCPM; TCPM did not ignore");
+}
+
+ZTEST_F(usb_attach_5v_3a_pd_source, verify_dps_battery_absent)
+{
+	control_battery_present(false);
+	zassert_false(battery_is_present(), "dps battery is present");
+	task_wake(TASK_ID_DPS);
+	/* wait dps_config.t_check*/
+	k_sleep(K_MSEC(5000));
+	zassert_true(dps_get_flag() & DPS_FLAG_NO_BATTERY, "DPS_FLAG_NO_BATTERY is set");
+	control_battery_present(true);
+	zassert_true(battery_is_present(), "dps battery is not present");
+}
+
+ZTEST_F(usb_attach_5v_3a_pd_source, verify_dps_enable)
+{
+	dps_enable(false);
+	zassert_false(dps_is_enabled(), NULL);
+	task_wake(TASK_ID_DPS);
+	/* wait dps_config.t_check*/
+	k_sleep(K_MSEC(5000));
+	zassert_true(dps_get_flag() & DPS_FLAG_DISABLED, "DPS_FLAG_DISABLED is set");
+	dps_enable(true);
+	zassert_true(dps_is_enabled(), NULL);
 }
