f463990866b3f630bccfc8a1a2360392d1a71ff1
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-09-08 08:38:03 +0800

    ectool: support bist share mode enable/disable command
    Add bist share mode enable/ disable command.
    
    BUG=b:9999
    BRANCH=none
    TEST=pass TDA.2.3.1.1
    
    Change-Id: I307596b9b59d60b72ff1e594919fadfb644dcc53
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>

--- common/usbc/usb_pd_dpm.c
+++ common/usbc/usb_pd_dpm.c
@@ -1159,3 +1159,16 @@ int dpm_get_status_msg(int port, uint8_t *msg, uint32_t *len)
 	memcpy(msg, &sdb, *len);
 	return EC_SUCCESS;
 }
+
+void pd_set_bist_share_mode(uint8_t enable)
+{
+	if (enable)
+		bist_shared_mode_enabled = true;
+	else
+		bist_shared_mode_enabled = false;
+}
+
+uint8_t pd_get_bist_share_mode(void)
+{
+	return bist_shared_mode_enabled;
+}
