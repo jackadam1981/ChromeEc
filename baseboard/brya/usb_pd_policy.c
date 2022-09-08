c79ac33c223e02831e65d92eecb220bc350e545c
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-09-08 16:25:23 +0800

    brya: add svdm TBT exit mode support
    TEST=verify pass on TEST.PD.VDM.SNK.1
    
    Change-Id: I2c91ba72ec282bb1f740d40823570f26ff76fab9
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>

--- baseboard/brya/usb_pd_policy.c
+++ baseboard/brya/usb_pd_policy.c
@@ -16,6 +16,7 @@
 #include "ec_commands.h"
 #include "gpio.h"
 #include "timer.h"
+#include "typec_control.h"
 #include "usbc_ppc.h"
 #include "usb_mux.h"
 #include "usb_pd.h"
@@ -96,14 +97,14 @@ static const union tbt_mode_resp_device vdo_tbt_modes[1] = { {
 } };
 
 static const uint32_t vdo_idh = VDO_IDH(1, /* Data caps as USB host     */
-					0, /* Not a USB device   */
+					1, /* is a USB device   */
 					IDH_PTYPE_PERIPH, 1, /* Supports alt
 								modes */
 					USB_VID_GOOGLE);
 
 static const uint32_t vdo_idh_rev30 =
 	VDO_IDH_REV30(1, /* Data caps as USB host     */
-		      0, /* Not a USB device   */
+		      1, /* is a USB device   */
 		      IDH_PTYPE_PERIPH, 1, /* Supports alt modes */
 		      IDH_PTYPE_DFP_HOST, USB_TYPEC_RECEPTACLE, USB_VID_GOOGLE);
 
@@ -197,15 +198,15 @@ static int svdm_tbt_compat_response_enter_mode(int port, uint32_t *payload)
 	/* Do not enter mode while CPU is off. */
 	if (chipset_in_or_transitioning_to_state(CHIPSET_STATE_ANY_OFF))
 		return 0; /* NAK */
-
+	ccprints("[SC] aaa");
 	/* Do not enter mode while policy disallows it */
 	if (!tbt_ufp_ack_allowed[port])
 		return 0; /* NAK */
-
+	ccprints("[SC] bbb");
 	if ((PD_VDO_VID(payload[0]) != USB_VID_INTEL) ||
 	    (PD_VDO_OPOS(payload[0]) != OPOS_TBT))
 		return 0; /* NAK */
-
+	ccprints("[SC] ccc");
 	mux_state = usb_mux_get(port);
 	/*
 	 * Ref: USB PD 3.0 Spec figure 6-21 Successful Enter Mode sequence
@@ -234,12 +235,29 @@ static int svdm_tbt_compat_response_enter_mode(int port, uint32_t *payload)
 	return 0;
 }
 
+static int svdm_tbt_compat_response_exit_mode(int port, uint32_t *payload)
+{
+
+	if ((PD_VDO_VID(payload[0]) != USB_VID_INTEL) ||
+	    (PD_VDO_OPOS(payload[0]) != OPOS_TBT))
+		return 0; /* NAK */
+	ccprints("[SC] svdm_tbt_compat_response_exit_mode");
+
+	pd_ufp_set_exit_mode(port, payload);
+	/* Isolate the SBU lines. */
+	typec_set_sbu(port, false);
+	ccprints("[SC] 11111111111");
+	usb_mux_set(port, USB_PD_MUX_NONE, USB_SWITCH_CONNECT,
+		    polarity_rm_dts(pd_get_polarity(port)));
+	return 1; /* ACK */
+}
+
 const struct svdm_response svdm_rsp = {
 	.identity = &svdm_tbt_compat_response_identity,
 	.svids = &svdm_tbt_compat_response_svids,
 	.modes = &svdm_tbt_compat_response_modes,
 	.enter_mode = &svdm_tbt_compat_response_enter_mode,
 	.amode = NULL,
-	.exit_mode = NULL,
+	.exit_mode = &svdm_tbt_compat_response_exit_mode,
 };
 #endif /* CONFIG_USB_PD_TBT_COMPAT_MODE */
