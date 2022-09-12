6e3d673ecf48181922f65aa0216fca2715b43db2
Ting Shen <phoenixshen@google.com>
2022-09-12 13:11:38 +0800

    TEST-ONLY: rt1739: disable over temperature protection
    BUG=b:244151949
    TEST=none
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I553c725813acd83eec7333a99bb10ba536574c71

--- driver/ppc/rt1739.c
+++ driver/ppc/rt1739.c
@@ -247,7 +247,7 @@ static int rt1739_init(int port)
 	RETURN_ERROR(write_reg(port, RT1739_REG_SW_RESET, RT1739_SW_RESET));
 	usleep(1 * MSEC);
 	RETURN_ERROR(write_reg(port, RT1739_REG_SYS_CTRL,
-			       RT1739_OT_EN | RT1739_SHUTDOWN_OFF));
+			       RT1739_SHUTDOWN_OFF));
 
 	RETURN_ERROR(rt1739_workaround(port));
 	RETURN_ERROR(rt1739_set_frs_enable(port, false));
