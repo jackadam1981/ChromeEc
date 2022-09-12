67f6dac5789a94ba21a765ee09c67ada1ec1f017
Li Feng <li1.feng@intel.com>
2022-09-12 00:01:25 -0700

    adl_ish_lite: Lite version of ISH firmware for ADL
    ISH and UFS are in same PCI slot function 0. To enable UFS, ISH has to
    be enabled too.
    This ISH firmware provides power management.
    If no activities, ISH will be in deep sleep all the time.
    
    BUG=b:234136500
    BRANCH=none
    TEST=make BOARD=adl_ish_lite
    TEST=on ADLP RVP with ish enabled in coreboot(ISH BUP and loader are
    stitched to CB), copy ec.bin to DUT's file system: /lib/firmware/intel,
    rename it as adlrvp_ish.bin and "sync". Reboot system, from host dmesg
    can see ISH main firmware is loaded.
    And ISH console shows ISH is in deep sleep.
    
    Signed-off-by: Li Feng <li1.feng@intel.com>
    Signed-off-by: Guangfu Hu <guangfu.hu@intel.com>
    Change-Id: I6bfac3e1fd47cfa7cb62caf52bfc4b3b4c74aecd

---
+++ board/adl_ish_lite/board.c
@@ -0,0 +1,42 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* TGL RVP ISH board-specific configuration */
+
+#include "accelgyro_lsm6dsm.h"
+#include "console.h"
+#include "gpio.h"
+#include "hooks.h"
+#include "host_command.h"
+#include "i2c.h"
+#include "motion_sense.h"
+#include "power.h"
+#include "task.h"
+
+#include "gpio_list.h" /* has to be included last */
+
+/* I2C port map */
+const struct i2c_port_t i2c_ports[] ={ };
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+int chipset_in_state(int state_mask)
+{
+	return state_mask & CHIPSET_STATE_ON;
+}
+
+int chipset_in_or_transitioning_to_state(int state_mask)
+{
+	return state_mask & CHIPSET_STATE_ON;
+}
+
+void chipset_force_shutdown(enum chipset_shutdown_reason reason)
+{
+}
+
+int board_idle_task(void *unused)
+{
+	while (1)
+		task_wait_event(-1);
+}
---
+++ board/adl_ish_lite/board.h
@@ -0,0 +1,72 @@
+/* Copyright 2019 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* ADL ISH board configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/* ISH specific */
+#undef  CONFIG_DEBUG_ASSERT
+#define CONFIG_CLOCK_CRYSTAL
+#define CONFIG_ISH_UART_0
+
+/* EC */
+#define CONFIG_FLASH_SIZE_BYTES 0x80000
+#define CONFIG_FPU
+#define CONFIG_I2C
+#define CONFIG_I2C_CONTROLLER
+
+/* EC Console Commands */
+#define CONFIG_CMD_TIMERINFO
+
+/* Undefined features */
+#undef CONFIG_CMD_HASH
+#undef CONFIG_CMD_I2C_SCAN
+#undef CONFIG_CMD_KEYBOARD
+#undef CONFIG_CMD_POWER_AP
+#undef CONFIG_CMD_POWERINDEBUG
+#undef CONFIG_CMD_SHMEM
+#undef CONFIG_EXTPOWER
+#undef CONFIG_KEYBOARD_KSO_BASE
+#undef CONFIG_FLASH_CROS
+#undef CONFIG_FMAP
+#undef CONFIG_LID_SWITCH
+#undef CONFIG_SWITCH
+#undef CONFIG_WATCHDOG
+
+/* Modules we want to exclude */
+#undef CONFIG_CMD_HASH
+#undef CONFIG_CMD_TEMP_SENSOR
+#undef CONFIG_ADC
+#undef CONFIG_SHA256
+
+/* DMA paging between SRAM and DRAM */
+#define CONFIG_DMA_PAGING
+
+/* power management definitions */
+#define CONFIG_LOW_POWER_IDLE
+
+#define CONFIG_ISH_PM_D0I1
+#define CONFIG_ISH_PM_D0I2
+#define CONFIG_ISH_PM_D0I3
+#define CONFIG_ISH_PM_D3
+#define CONFIG_ISH_PM_RESET_PREP
+
+#define CONFIG_ISH_IPAPG
+
+#define CONFIG_ISH_D0I2_MIN_USEC	(15*MSEC)
+#define CONFIG_ISH_D0I3_MIN_USEC	(50*MSEC)
+
+#define CONFIG_ISH_NEW_PM
+
+#ifndef __ASSEMBLER__
+
+#include "gpio_signal.h"
+#include "registers.h"
+
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
