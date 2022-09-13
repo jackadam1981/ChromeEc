7d3410e7fad4f836cfe10804a7c89bc54f78d1e2
Peter Chi <peter_chi@wistron.corp-partner.google.com>
2022-09-13 14:56:18 +0800

    crota: change fan table and add tablet fan control
    BUG=b:
    BRANCH=none
    TEST=make -j BOARD=crota
    
    Signed-off-by: Peter Chi <peter_chi@wistron.corp-partner.google.com>
    Change-Id: I4d34da9663fe392992fa5436e543c259c8a7b27a

--- board/crota/sensors.c
+++ board/crota/sensors.c
@@ -261,7 +261,7 @@ BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
 			[EC_TEMP_THRESH_HIGH] = C_TO_K(77), \
 		}, \
 		.temp_fan_off = C_TO_K(30), \
-		.temp_fan_max = C_TO_K(47), \
+		.temp_fan_max = C_TO_K(52), \
 	}
 __maybe_unused static const struct ec_thermal_config thermal_cpu = THERMAL_CPU;
 
