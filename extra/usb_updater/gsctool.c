752fbe6419381c0a9d750ddbe8b2b63b9807099c
Mary Ruthven <mruthven@chromium.org>
2022-09-13 13:45:09 -0700

    gsctoo: print ccd info version
    Print the ccd info version so it's obvious which config gsctool ends up
    using. This is also an easy way to tell the difference between ti50 and
    cr50.
    
    BUG=none
    TEST=run `gsctool -aI`
    
    Change-Id: Ieab6be191218c84466c7ddbd1d74769da7d8bbc1
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -2303,6 +2303,7 @@ static void print_ccd_info(void *response, size_t response_size)
 	}
 
 	/* Now report CCD state on the console. */
+	printf("Version: %d\n", ccd_info_version);
 	printf("State: %s\n", ccd_info.ccd_state > ARRAY_SIZE(state_names) ?
 	       "Error" : state_names[ccd_info.ccd_state]);
 	printf("Password: %s\n", (ccd_info.ccd_indicator_bitmap &
