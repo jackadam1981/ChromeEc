f06a0a7dc7e9ab624b07d5ebdbf253551d24feee
Peter Chi <peter_chi@wistron.corp-partner.google.com>
2022-09-13 14:56:18 +0800

    crota: change fan table and add tablet fan control
    Add different fan speed on clamshell and tablet mode,
    also verified by Thermal team(thermal policy table V17)
    
    BUG=b:248457426, b:246214508
    BRANCH=none
    TEST=make -j BOARD=crota
    
    Signed-off-by: Peter Chi <peter_chi@wistron.corp-partner.google.com>
    Change-Id: I4d34da9663fe392992fa5436e543c259c8a7b27a

--- board/crota/board.h
+++ board/crota/board.h
@@ -260,8 +260,8 @@ enum pwm_channel {
 enum fan_channel { FAN_CH_0 = 0, FAN_CH_COUNT };
 
 enum fan_rpm_table {
-	RPM_TABLE_CPU0,
-	RPM_TABLE_CPU1,
+	RPM_TABLE_CPU,
+	RPM_TABLE_CPU_TABLET,
 	RPM_TABLE_DDR,
 	RPM_TABLE_CHARGER,
 	RPM_TABLE_AMBIENT,
@@ -269,6 +269,19 @@ enum fan_rpm_table {
 };
 
 enum mft_channel { MFT_CH_0 = 0, MFT_CH_COUNT };
+
+struct thermal_policy_config {
+	int fan_off_slop1;
+	int fan_max_slop1;
+	int fan_off_slop2;
+	int fan_max_slop2;
+	int fan_slop_threshold;
+	int ddr_fan_turn_on;
+	int ddr_fan_turn_off;
+	int rpm_table_cpu;
+};
+
+enum thermal_cfg_table { LAPTOP_MODE, TABLET_MODE, THERMAL_CFG_TABLE_COUNT };
 
 void motion_interrupt(enum gpio_signal signal);
 
