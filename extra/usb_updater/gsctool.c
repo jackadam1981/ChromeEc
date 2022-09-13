0484bd9dfbaaa25bff1530fdcaa72c5840a879b9
Mary Ruthven <mruthven@chromium.org>
2022-09-13 14:56:40 -0700

    gsctool: print ccd info version
    BUG=none
    TEST=gsctool -I
    
    Change-Id: I9a84052c8772a1f2b821edda10e75a6911a401a5
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -2305,6 +2305,7 @@ static void print_ccd_info(void *response, size_t response_size)
 	}
 
 	/* Now report CCD state on the console. */
+	printf("Format: %d\n", ccd_info_version);
 	printf("State: %s\n", ccd_info.ccd_state > ARRAY_SIZE(state_names) ?
 	       "Error" : state_names[ccd_info.ccd_state]);
 	printf("Password: %s\n", (ccd_info.ccd_indicator_bitmap &
