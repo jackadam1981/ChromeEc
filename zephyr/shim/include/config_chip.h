0fba4853d9d007075901a95477919cba200584a9
Rob Barnes <robbarnes@google.com>
2022-09-14 20:47:00 +0000

    zephyr/linker: Ensure space for panic and jump data
    Ensure space is available for end of ram data. End of ram data must be
    located at the very end of noinit ram. This currently includes
    panic_data followed by jump_data.
    
    This is being enforced with linker asserts and build asserts rather
    than allocating the space directly so RAM utiliztion reports are still
    relevant.
    
    Introduce PLATFORM_EC_END_OF_RAM_DATA_SIZE config option and default it
    to 1KB. This can be adjusted on boards that are constrained.
    
    BUG=b:246778588,b:246798928
    BRANCH=None
    TEST=Increase PLATFORM_EC_END_OF_RAM_DATA_SIZE until assert triggers.
    
    Change-Id: I444bbe3a583396b4f9b104bb6999e78ae3ff6f2f
    Signed-off-by: Rob Barnes <robbarnes@google.com>

--- zephyr/shim/include/config_chip.h
+++ zephyr/shim/include/config_chip.h
@@ -478,6 +478,12 @@ extern char mock_jump_data[sizeof(struct jump_data) + 256];
 #error "A zephyr,sram device must be chosen in the device tree"
 #endif
 
+#undef CONFIG_PRESERVED_END_OF_RAM_SIZE
+#ifdef CONFIG_PLATFORM_EC_PRESERVED_END_OF_RAM_SIZE
+#define CONFIG_PRESERVED_END_OF_RAM_SIZE \
+	CONFIG_PLATFORM_EC_PRESERVED_END_OF_RAM_SIZE
+#endif
+
 #define CONFIG_RO_MEM_OFF CONFIG_CROS_EC_RO_MEM_OFF
 #define CONFIG_RO_MEM_SIZE CONFIG_CROS_EC_RO_MEM_SIZE
 #define CONFIG_RW_MEM_OFF CONFIG_CROS_EC_RW_MEM_OFF
