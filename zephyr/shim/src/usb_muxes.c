fc6290dcc347ab64f3b563e36ca2ff38c307cfd2
Keith Short <keithshort@chromium.org>
2022-09-14 16:13:18 -0600

    Revert "zephyr: Support alternative usb_mux_chain config in DTS"
    This reverts commit a062b533a068069af8fdea3ea394e99703d7561a.
    
    Reason for revert: b:246828811, servo_v4p1 FW broken at HEAD
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Change-Id: I8769c0c8a4532e6f07dc373653b9401aa63e670c

--- zephyr/shim/src/usb_muxes.c
+++ zephyr/shim/src/usb_muxes.c
@@ -43,15 +43,7 @@
 USB_MUX_FOREACH_MUX(USB_MUX_CHECK_ALL_PORTS_ARE_SAME)
 
 /**
- * Declare all usb_mux_chain structures e.g.
- * MAYBE_CONST struct usb_mux_chain
- * USB_MUX_chain_port_<port_id>_mux_<position_id>;
- */
-USB_MUX_FOREACH_CHAIN_VARGS(USB_MUX_FOREACH_NO_ROOT_MUX,
-			    USB_MUX_CHAIN_STRUCT_DECLARE_OP)
-
-/**
- * Define usb_mux_chain structures for main chain e.g.
+ * Define usb_mux_chain structures e.g.
  *
  * MAYBE_CONST struct usb_mux_chain
  * USB_MUX_chain_port_<port_id>_mux_<position_id> = {
@@ -59,7 +51,7 @@ USB_MUX_FOREACH_CHAIN_VARGS(USB_MUX_FOREACH_NO_ROOT_MUX,
  *         .next = &USB_MUX_chain_port_0_mux_1,
  * }
  */
-USB_MUX_FOREACH_CHAIN_VARGS(USB_MUX_FOR_MAIN_CHAIN, USB_MUX_FOREACH_NO_ROOT_MUX,
+USB_MUX_FOREACH_CHAIN_VARGS(USB_MUX_FOREACH_NO_ROOT_MUX,
 			    USB_MUX_CHAIN_STRUCT_DEFINE_OP)
 
 /**
@@ -78,8 +70,8 @@ USB_MUX_FOREACH_MUX(USB_MUX_CB_BOARD_SET_DECLARE_IF_EXISTS)
  * },
  * [1] = { ... },
  */
-MAYBE_CONST struct usb_mux_chain usb_muxes[] = { USB_MUX_FOREACH_CHAIN_VARGS(
-	USB_MUX_FOR_MAIN_CHAIN, USB_MUX_DEFINE_ROOT_MUX) };
+MAYBE_CONST struct usb_mux_chain usb_muxes[] = { USB_MUX_FOREACH_CHAIN(
+	USB_MUX_DEFINE_ROOT_MUX) };
 BUILD_ASSERT(ARRAY_SIZE(usb_muxes) == CONFIG_USB_PD_PORT_MAX_COUNT);
 
 /**
