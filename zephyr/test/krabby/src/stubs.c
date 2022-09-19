5e6a4c75dc2d903b69692ed74413d5a13c0ea9ba
Ting Shen <phoenixshen@google.com>
2022-09-19 17:53:22 +0800

    krabby usbc_config test
    This test verifies the sink path switching logic on Krabby.
    
    BUG=b:243841599
    TEST=twister -T zephyr/test/krabby/
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I0128f9b1b200397afe30bfdd5e99903a35ac0260

--- zephyr/test/krabby/src/stubs.c
+++ zephyr/test/krabby/src/stubs.c
@@ -6,11 +6,6 @@
 #include "charge_ramp.h"
 #include "charge_state.h"
 
-int board_set_active_charge_port(int port)
-{
-	return 0;
-}
-
 int board_is_vbus_too_low(int port, enum chg_ramp_vbus_state ramp_state)
 {
 	return 0;
@@ -21,9 +16,11 @@ void board_set_charge_limit(int port, int supplier, int charge_ma, int max_ma,
 {
 }
 
-const struct batt_params *charger_current_battery_params(void)
+int pd_check_vconn_swap(int port)
 {
-	static const struct batt_params params = {};
+	return 0;
+}
 
-	return &params;
+void pd_set_vbus_discharge(int port, int enable)
+{
 }
