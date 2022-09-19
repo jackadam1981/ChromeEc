3fe58846acd5cf759269d2223aed5a98a4c26172
Yi Chou <yich@google.com>
2022-09-19 19:07:40 +0800

    fpsensor: Add EC_CMD_FP_ESTABLISH_PAIRING_KEY_KEYGEN
    These commands generate the FPMCU-side ECDH secret for pairing key.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    
    Change-Id: I00f1cb86638f451c0938590d5a7da80d79e47cc0

--- common/fpsensor/fpsensor_auth_commands.cc
+++ common/fpsensor/fpsensor_auth_commands.cc
@@ -225,3 +225,36 @@ enum ec_error_list generate_ecdh_shared_secret(const EC_KEY &private_key,
 
 	return EC_SUCCESS;
 }
+
+static enum ec_status
+fp_command_establish_pairing_key_keygen(struct host_cmd_handler_args *args)
+{
+	auto *r = static_cast<ec_response_fp_establish_pairing_key_keygen *>(
+		args->response);
+
+	ScopedFastCpu fast_cpu;
+
+	bssl::UniquePtr<EC_KEY> ecdh_key = generate_elliptic_curve_key();
+	if (ecdh_key == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	enum ec_error_list res = fill_encrypted_private_key(
+		*ecdh_key, FP_PAIRING_KEY_ENC_METADATA_VERSION,
+		r->encrypted_private_key);
+	if (res != EC_SUCCESS) {
+		CPRINTS("pairing_keygen: Failed to fill response encrypted private key");
+		return EC_RES_UNAVAILABLE;
+	}
+
+	res = fill_pubkey(*ecdh_key, r->pubkey);
+	if (res != EC_SUCCESS) {
+		CPRINTS("pairing_keygen: Failed to fill response pubkey");
+		return EC_RES_UNAVAILABLE;
+	}
+
+	args->response_size = sizeof(*r);
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_FP_ESTABLISH_PAIRING_KEY_KEYGEN,
+		     fp_command_establish_pairing_key_keygen, EC_VER_MASK(0));
