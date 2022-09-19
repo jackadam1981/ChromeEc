ff1d473bc6a33ae9288fbb35bfa7cc822b4ddc1b
Diana Z <dzigterman@chromium.org>
2022-09-19 14:55:33 -0600

    Skyrim: Move PPC control to board code
    Not all boards are using the AOZ PPC, so move its control function into
    a specific PPC board file. Also move the PPC interrupt function to board
    code.
    
    LOW_COVERAGE_REASON=Skyrim board tests not created yet: b/247151116
    BRANCH=None
    BUG=b:244457125
    TEST=zmake build skyrim and morthal
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: I93d24ddeaa61706c8d02e7e8ad099f4d904ae90c

---
+++ zephyr/projects/skyrim/src/winterhold/ppc_config.c
@@ -0,0 +1,28 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Winterhold board-specific PPC code */
+
+#include <zephyr/drivers/gpio.h>
+
+#include "driver/ppc/nx20p348x.h"
+#include "usbc_ppc.h"
+
+void ppc_interrupt(enum gpio_signal signal)
+{
+	switch (signal) {
+	case GPIO_USB_C0_PPC_INT_ODL:
+		nx20p348x_interrupt(USBC_PORT_C0);
+		break;
+
+	case GPIO_USB_C1_PPC_INT_ODL:
+		nx20p348x_interrupt(USBC_PORT_C1);
+		break;
+
+	default:
+		break;
+	}
+}
+
