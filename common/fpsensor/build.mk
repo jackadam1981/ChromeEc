5424bb55e85c8ed4c047ee426286bae8a8523614
Yi Chou <yich@google.com>
2022-09-19 19:07:40 +0800

    fpsensor: Add EC_CMD_FP_ESTABLISH_PAIRING_KEY_KEYGEN
    These commands generate the FPMCU-side ECDH secret for pairing key.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    
    Change-Id: I00f1cb86638f451c0938590d5a7da80d79e47cc0

--- common/fpsensor/build.mk
+++ common/fpsensor/build.mk
@@ -17,15 +17,18 @@ _fpsensor_state_obj:=$(_fpsensor_dir)fpsensor_state.o
 _fpsensor_crypto_obj:=$(_fpsensor_dir)fpsensor_crypto.o
 _fpsensor_obj:=$(_fpsensor_dir)fpsensor.o
 _fpsensor_detect_strings_obj:=$(_fpsensor_dir)fpsensor_detect_strings.o
+_fpsensor_auth_commands_obj:=$(_fpsensor_dir)fpsensor_auth_commands.o
 
 $(out)/RW/$(_fpsensor_state_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_crypto_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_detect_strings_obj): CFLAGS+=$(fpsensor_CFLAGS)
+$(out)/RW/$(_fpsensor_auth_commands_obj): CFLAGS+=$(fpsensor_CFLAGS)
 
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_state_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_crypto_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_obj)
 all-obj-$(HAS_TASK_CONSOLE)+=$(_fpsensor_detect_strings_obj)
+all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_auth_commands_obj)
 
 endif # CONFIG_FINGERPRINT_MCU or TEST_BUILD
