e7467dd64e12c37bbb8a2c52c773c1e5c0168215
Ting Shen <phoenixshen@google.com>
2022-09-19 17:53:22 +0800

    tentacruel: add ppc test
    This test verifies the ppc chip selecting logic on tentacruel
    
    BUG=b:243841599
    TEST=twister -T zephyr/test/krabby/
    BRANCH=none
    
    Signed-off-by: Ting Shen <phoenixshen@google.com>
    Change-Id: I9c3921cdf3b612e5c575b3771caed076b7c860b6

--- zephyr/test/krabby/src/stubs.c
+++ zephyr/test/krabby/src/stubs.c
@@ -5,6 +5,7 @@
 
 #include "charge_ramp.h"
 #include "charge_state.h"
+#include "usb_charge.h"
 
 int board_is_vbus_too_low(int port, enum chg_ramp_vbus_state ramp_state)
 {
@@ -28,4 +29,10 @@ void pd_power_supply_reset(int port)
 int pd_set_power_supply_ready(int port)
 {
 	return 1;
+}
+
+__attribute__((weak)) struct bc12_config bc12_ports[0];
+
+__attribute__((weak)) void usb_charger_vbus_change(int port, int vbus_level)
+{
 }
