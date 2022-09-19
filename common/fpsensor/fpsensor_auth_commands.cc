0f71441a9e8095f4a1c5707533b2ebfc5f70c7d1
Yi Chou <yich@google.com>
2022-09-19 19:07:40 +0800

    fpsensor: Add EC_CMD_FP_ESTABLISH_PAIRING_KEY_KEYGEN
    These commands generate the FPMCU-side ECDH secret for pairing key.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    
    Change-Id: I00f1cb86638f451c0938590d5a7da80d79e47cc0

--- common/fpsensor/fpsensor_auth_commands.cc
+++ common/fpsensor/fpsensor_auth_commands.cc
@@ -3,11 +3,17 @@
  * found in the LICENSE file.
  */
 
+#include "crypto/elliptic_curve_key.h"
+#include "ec_commands.h"
 #include "fpsensor.h"
+#include "fpsensor_auth_crypto.h"
 #include "fpsensor_crypto.h"
 #include "fpsensor_state.h"
 #include "fpsensor_utils.h"
 #include "openssl/mem.h"
+#include "scoped_fast_cpu.h"
+
+#include <utility>
 
 /**
  * Clear all fingerprint templates associated with the current user id.
@@ -22,3 +28,39 @@ void fp_clear_context(void)
 	for (uint16_t idx = 0; idx < FP_MAX_FINGER_COUNT; idx++)
 		fp_clear_finger_context(idx);
 }
+
+static enum ec_status
+fp_command_establish_pairing_key_keygen(struct host_cmd_handler_args *args)
+{
+	auto *r = static_cast<ec_response_fp_establish_pairing_key_keygen *>(
+		args->response);
+
+	ScopedFastCpu fast_cpu;
+
+	bssl::UniquePtr<EC_KEY> ecdh_key = generate_elliptic_curve_key();
+	if (ecdh_key == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	enum ec_error_list res = fill_encrypted_private_key(
+		*ecdh_key, FP_AES_KEY_ENC_METADATA_VERSION,
+		r->encrypted_private_key);
+	if (res != EC_SUCCESS) {
+		CPRINTS("pairing_keygen: Failed to fill response encrypted private key");
+		return EC_RES_UNAVAILABLE;
+	}
+
+	std::optional<fp_elliptic_curve_public_key> pubkey =
+		create_pubkey_from_ec_key(*ecdh_key);
+	if (!pubkey.has_value()) {
+		CPRINTS("pairing_keygen: Failed to create response pubkey");
+		return EC_RES_UNAVAILABLE;
+	}
+
+	r->pubkey = std::move(pubkey).value();
+
+	args->response_size = sizeof(*r);
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_FP_ESTABLISH_PAIRING_KEY_KEYGEN,
+		     fp_command_establish_pairing_key_keygen, EC_VER_MASK(0));
