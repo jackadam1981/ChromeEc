cb5fdf7fea3a1fe3e9ddef6e4a7f4e24b0ae6fbe
Yi Chou <yich@google.com>
2022-09-23 11:26:26 +0800

    fpsensor: Add EC_CMD_FP_LOAD_PAIRING_KEY
    This command would load encrypted GSC pairing key from biod.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    
    Change-Id: Ia5672eb39d283cf86064366f3a26453630af3adc

--- test/fpsensor_auth_commands.cc
+++ test/fpsensor_auth_commands.cc
@@ -134,12 +134,14 @@ test_fp_command_establish_pairing_key_keygen(void)
 	return EC_SUCCESS;
 }
 
-test_static int test_fp_command_establish_pairing_key(void)
+test_static enum ec_error_list
+test_fp_command_establish_and_load_pairing_key(void)
 {
 	enum ec_status rv;
 	struct ec_response_fp_establish_pairing_key_keygen keygen_response;
 	struct ec_params_fp_establish_pairing_key_wrap wrap_params;
 	struct ec_response_fp_establish_pairing_key_wrap wrap_response;
+	struct ec_params_fp_load_pairing_key load_params;
 
 	uint8_t privkey[FP_PAIRING_KEY_EC_PRIVATE_KEY_LEN];
 
@@ -164,57 +166,41 @@ test_static int test_fp_command_establish_pairing_key(void)
 	bssl::UniquePtr<EC_GROUP> group(
 		EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
 
-	if (group == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(group.get(), nullptr, "%p");
 
 	bssl::UniquePtr<BIGNUM> n(
 		BN_bin2bn(privkey, FP_PAIRING_KEY_EC_PRIVATE_KEY_LEN, nullptr));
 
-	if (n == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(n.get(), nullptr, "%p");
 
 	bssl::UniquePtr<EC_POINT> public_point(EC_POINT_new(group.get()));
 
-	if (public_point == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(public_point.get(), nullptr, "%p");
 
-	if (EC_POINT_mul(group.get(), public_point.get(), n.get(), nullptr,
-			 nullptr, nullptr) != 1) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_EQ(EC_POINT_mul(group.get(), public_point.get(), n.get(), nullptr,
+			     nullptr, nullptr),
+		1, "%d");
 
 	bssl::UniquePtr<BIGNUM> x_bn(BN_new());
 
-	if (x_bn == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(x_bn.get(), nullptr, "%p");
 
 	bssl::UniquePtr<BIGNUM> y_bn(BN_new());
 
-	if (y_bn == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(y_bn.get(), nullptr, "%p");
 
-	if (EC_POINT_get_affine_coordinates_GFp(group.get(), public_point.get(),
-						x_bn.get(), y_bn.get(),
-						nullptr) != 1) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_EQ(EC_POINT_get_affine_coordinates_GFp(
+			group.get(), public_point.get(), x_bn.get(), y_bn.get(),
+			nullptr),
+		1, "%d");
 
-	if (BN_bn2binpad(x_bn.get(), wrap_params.peers_pubkey.x,
-			 FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN) !=
-	    FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_EQ(BN_bn2binpad(x_bn.get(), wrap_params.peers_pubkey.x,
+			     FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN),
+		FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN, "%d");
 
-	if (BN_bn2binpad(y_bn.get(), wrap_params.peers_pubkey.y,
-			 FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN) !=
-	    FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_EQ(BN_bn2binpad(y_bn.get(), wrap_params.peers_pubkey.y,
+			     FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN),
+		FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN, "%d");
 
 	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PAIRING_KEY_WRAP, 0,
 				    &wrap_params, sizeof(wrap_params),
@@ -222,12 +208,25 @@ test_static int test_fp_command_establish_pairing_key(void)
 
 	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
 
+	memcpy(&load_params.encrypted_pairing_key.info,
+	       &wrap_response.encrypted_pairing_key.info,
+	       sizeof(wrap_response.encrypted_pairing_key.info));
+
+	memcpy(load_params.encrypted_pairing_key.data,
+	       wrap_response.encrypted_pairing_key.data,
+	       sizeof(wrap_response.encrypted_pairing_key.data));
+
+	rv = test_send_host_command(EC_CMD_FP_LOAD_PAIRING_KEY, 0, &load_params,
+				    sizeof(load_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
 	return EC_SUCCESS;
 }
 
-test_static int test_fp_command_establish_pairing_key_fail(void)
+test_static enum ec_error_list test_fp_command_establish_pairing_key_fail(void)
 {
-	int rv;
+	enum ec_status rv;
 	struct ec_response_fp_establish_pairing_key_keygen keygen_response;
 	struct ec_params_fp_establish_pairing_key_wrap wrap_params;
 	struct ec_response_fp_establish_pairing_key_wrap wrap_response;
@@ -255,61 +254,134 @@ test_static int test_fp_command_establish_pairing_key_fail(void)
 	bssl::UniquePtr<EC_GROUP> group(
 		EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
 
-	if (group == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(group.get(), nullptr, "%p");
 
 	bssl::UniquePtr<BIGNUM> n(
 		BN_bin2bn(privkey, FP_PAIRING_KEY_EC_PRIVATE_KEY_LEN, nullptr));
 
-	if (n == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(n.get(), nullptr, "%p");
 
 	bssl::UniquePtr<EC_POINT> public_point(EC_POINT_new(group.get()));
 
-	if (public_point == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(public_point.get(), nullptr, "%p");
 
-	if (EC_POINT_mul(group.get(), public_point.get(), n.get(), nullptr,
-			 nullptr, nullptr) != 1) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_EQ(EC_POINT_mul(group.get(), public_point.get(), n.get(), nullptr,
+			     nullptr, nullptr),
+		1, "%d");
 
 	bssl::UniquePtr<BIGNUM> x_bn(BN_new());
 
-	if (x_bn == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(x_bn.get(), nullptr, "%p");
 
 	bssl::UniquePtr<BIGNUM> y_bn(BN_new());
 
-	if (y_bn == nullptr) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_NE(y_bn.get(), nullptr, "%p");
 
-	if (EC_POINT_get_affine_coordinates_GFp(group.get(), public_point.get(),
-						x_bn.get(), y_bn.get(),
-						nullptr) != 1) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_EQ(EC_POINT_get_affine_coordinates_GFp(
+			group.get(), public_point.get(), x_bn.get(), y_bn.get(),
+			nullptr),
+		1, "%d");
 
-	if (BN_bn2binpad(x_bn.get(), wrap_params.peers_pubkey.x,
-			 FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN) !=
-	    FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_EQ(BN_bn2binpad(x_bn.get(), wrap_params.peers_pubkey.x,
+			     FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN),
+		FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN, "%d");
 
-	if (BN_bn2binpad(y_bn.get(), wrap_params.peers_pubkey.y,
-			 FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN) !=
-	    FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN) {
-		return EC_RES_UNAVAILABLE;
-	}
+	TEST_EQ(BN_bn2binpad(y_bn.get(), wrap_params.peers_pubkey.y,
+			     FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN),
+		FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN, "%d");
 
 	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PAIRING_KEY_WRAP, 0,
 				    &wrap_params, sizeof(wrap_params),
 				    &wrap_response, sizeof(wrap_response));
+
+	TEST_NE(rv, EC_RES_SUCCESS, "%d");
+
+	return EC_SUCCESS;
+}
+
+test_static enum ec_error_list test_fp_command_load_pairing_key_fail(void)
+{
+	enum ec_status rv;
+	struct ec_response_fp_establish_pairing_key_keygen keygen_response;
+	struct ec_params_fp_establish_pairing_key_wrap wrap_params;
+	struct ec_response_fp_establish_pairing_key_wrap wrap_response;
+	struct ec_params_fp_load_pairing_key load_params;
+
+	uint8_t privkey[FP_PAIRING_KEY_EC_PRIVATE_KEY_LEN];
+
+	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PAIRING_KEY_KEYGEN, 0,
+				    NULL, 0, &keygen_response,
+				    sizeof(keygen_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	memcpy(&wrap_params.encrypted_private_key.info,
+	       &keygen_response.encrypted_private_key.info,
+	       sizeof(keygen_response.encrypted_private_key.info));
+
+	memcpy(wrap_params.encrypted_private_key.data,
+	       keygen_response.encrypted_private_key.data,
+	       sizeof(keygen_response.encrypted_private_key.data));
+
+	trng_init();
+	trng_rand_bytes(privkey, FP_PAIRING_KEY_EC_PRIVATE_KEY_LEN);
+	trng_exit();
+
+	bssl::UniquePtr<EC_GROUP> group(
+		EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
+
+	TEST_NE(group.get(), nullptr, "%p");
+
+	bssl::UniquePtr<BIGNUM> n(
+		BN_bin2bn(privkey, FP_PAIRING_KEY_EC_PRIVATE_KEY_LEN, nullptr));
+
+	TEST_NE(n.get(), nullptr, "%p");
+
+	bssl::UniquePtr<EC_POINT> public_point(EC_POINT_new(group.get()));
+
+	TEST_NE(public_point.get(), nullptr, "%p");
+
+	TEST_EQ(EC_POINT_mul(group.get(), public_point.get(), n.get(), nullptr,
+			     nullptr, nullptr),
+		1, "%d");
+
+	bssl::UniquePtr<BIGNUM> x_bn(BN_new());
+
+	TEST_NE(x_bn.get(), nullptr, "%p");
+
+	bssl::UniquePtr<BIGNUM> y_bn(BN_new());
+
+	TEST_NE(y_bn.get(), nullptr, "%p");
+
+	TEST_EQ(EC_POINT_get_affine_coordinates_GFp(
+			group.get(), public_point.get(), x_bn.get(), y_bn.get(),
+			nullptr),
+		1, "%d");
+
+	TEST_EQ(BN_bn2binpad(x_bn.get(), wrap_params.peers_pubkey.x,
+			     FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN),
+		FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN, "%d");
+
+	TEST_EQ(BN_bn2binpad(y_bn.get(), wrap_params.peers_pubkey.y,
+			     FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN),
+		FP_PAIRING_KEY_EC_PUBLIC_KEY_POINT_LEN, "%d");
+
+	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PAIRING_KEY_WRAP, 0,
+				    &wrap_params, sizeof(wrap_params),
+				    &wrap_response, sizeof(wrap_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	/* No encryption info. */
+	memset(&load_params.encrypted_pairing_key.info, 0,
+	       sizeof(load_params.encrypted_pairing_key.info));
+
+	memcpy(load_params.encrypted_pairing_key.data,
+	       wrap_response.encrypted_pairing_key.data,
+	       sizeof(wrap_response.encrypted_pairing_key.data));
+
+	rv = test_send_host_command(EC_CMD_FP_LOAD_PAIRING_KEY, 0, &load_params,
+				    sizeof(load_params), NULL, 0);
 
 	TEST_NE(rv, EC_RES_SUCCESS, "%d");
 
@@ -323,7 +395,8 @@ extern "C" void run_test(int argc, const char **argv)
 	RUN_TEST(test_fp_command_establish_pairing_key_without_seed);
 	RUN_TEST(test_set_fp_tpm_seed);
 	RUN_TEST(test_fp_command_establish_pairing_key_keygen);
-	RUN_TEST(test_fp_command_establish_pairing_key);
 	RUN_TEST(test_fp_command_establish_pairing_key_fail);
+	RUN_TEST(test_fp_command_establish_and_load_pairing_key);
+	RUN_TEST(test_fp_command_load_pairing_key_fail);
 	test_print_result();
 }
