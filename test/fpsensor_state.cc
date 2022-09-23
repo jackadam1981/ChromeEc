911a5b26103a202999e482a9c858c371703e46fe
Yi Chou <yich@google.com>
2022-09-23 15:48:36 +0800

    fpsensor: Add EC_CMD_FP_READ_MATCH_SECRET_WITH_PUBKEY
    This command would prevent the passive attacker reading the match
    secret without knowning the private key in cryptohome.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I77f80fe819482234f4679c49922e6139c4cee5eb

--- test/fpsensor_state.cc
+++ test/fpsensor_state.cc
@@ -7,14 +7,20 @@
 #include "ec_commands.h"
 #include "fpsensor_state.h"
 #include "mock/fpsensor_state_mock.h"
+#include "openssl/aes.h"
 #include "openssl/ec.h"
 #include "openssl/obj_mac.h"
 #include "test_util.h"
 #include "util.h"
 
 extern "C" {
+#include "sha256.h"
 #include "trng.h"
 }
+
+/* These must be included after the "openssl/aes.h" */
+#include "crypto/fipsmodule/aes/internal.h"
+#include "crypto/fipsmodule/modes/internal.h"
 
 #include <stdbool.h>
 
@@ -829,6 +835,175 @@ test_static int test_fp_command_nonce_context_limit(void)
 	return EC_SUCCESS;
 }
 
+test_static int test_fp_command_read_match_secret_with_pubkey_succeed(void)
+{
+	struct ec_response_fp_read_match_secret_with_pubkey response = {};
+	/* Create valid param with 0 <= fgr < 5 */
+	uint16_t matched_fgr = 1;
+	struct ec_params_fp_read_match_secret_with_pubkey params = {
+		.fgr = matched_fgr,
+	};
+
+	/* Expected positive_match_secret same as  in test/fpsensor_crypto.c*/
+	static const uint8_t
+		expected_positive_match_secret_for_empty_user_id[] = {
+			0x8d, 0xc4, 0x5b, 0xdf, 0x55, 0x1e, 0xa8, 0x72,
+			0xd6, 0xdd, 0xa1, 0x4c, 0xb8, 0xa1, 0x76, 0x2b,
+			0xde, 0x38, 0xd5, 0x03, 0xce, 0xe4, 0x74, 0x51,
+			0x63, 0x6c, 0x6a, 0x26, 0xa9, 0xb7, 0xfa, 0x68,
+		};
+
+	/* Create positive secret match state with valid deadline value,
+	 * readable state, and correct template matched
+	 */
+	struct positive_match_secret_state test_state_1 = {
+		.template_matched = static_cast<int8_t>(matched_fgr),
+		.readable = true,
+		.deadline =
+			timestamp_t{
+				.val = 5000000,
+			},
+	};
+
+	uint8_t privkey[FP_PK_EC_PRIVATE_KEY_LEN];
+	uint8_t share_secret[FP_POSITIVE_MATCH_SECRET_BYTES];
+	struct sha256_ctx ctx;
+	AES_KEY aes_key;
+	uint8_t aes_iv[FP_CONTEXT_USERID_IV_LEN];
+	uint8_t ecount_buf[16];
+	unsigned int block_num = 0;
+
+	trng_init();
+	trng_rand_bytes(privkey, FP_PK_EC_PRIVATE_KEY_LEN);
+	trng_exit();
+
+	ScopedEC_GROUP group(EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
+
+	if (group.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM n(BN_bin2bn(privkey, FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (n.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedEC_POINT public_point(EC_POINT_new(group.get()));
+
+	if (public_point.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_mul(group.get(), public_point.get(), n.get(), nullptr,
+			 nullptr, nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM x_bn(BN_new());
+
+	if (x_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM y_bn(BN_new());
+
+	if (y_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_get_affine_coordinates_GFp(group.get(), public_point.get(),
+						x_bn.get(), y_bn.get(),
+						nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(x_bn.get(), params.pubkey_x,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(y_bn.get(), params.pubkey_y,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	positive_match_secret_state = test_state_1;
+	/* Set fp_positive_match_salt to the trivial value */
+	memcpy(fp_positive_match_salt, default_fake_fp_positive_match_salt,
+	       sizeof(default_fake_fp_positive_match_salt));
+
+	TEST_ASSERT_ARRAY_EQ(
+		(uint8_t const *)fp_positive_match_salt,
+		(uint8_t const *)default_fake_fp_positive_match_salt,
+		static_cast<int>(sizeof(default_fake_fp_positive_match_salt)));
+
+	/* Initialize an empty user_id to compare positive_match_secret */
+	memset(user_id, 0, sizeof(user_id));
+
+	TEST_ASSERT(fp_tpm_seed_is_set());
+	/* Test with the correct matched finger state and the default fake
+	 * fp_positive_match_salt
+	 */
+	TEST_ASSERT(
+		test_send_host_command(EC_CMD_FP_READ_MATCH_SECRET_WITH_PUBKEY,
+				       0, &params, sizeof(params), &response,
+				       sizeof(response)) == EC_SUCCESS);
+
+	x_bn = ScopedBIGNUM(
+		BN_bin2bn(response.pubkey_x, FP_PK_EC_PUBLIC_KEY_LEN, nullptr));
+	if (x_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	y_bn = ScopedBIGNUM(
+		BN_bin2bn(response.pubkey_y, FP_PK_EC_PUBLIC_KEY_LEN, nullptr));
+	if (y_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_set_affine_coordinates_GFp(group.get(), public_point.get(),
+						x_bn.get(), y_bn.get(),
+						nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_mul(group.get(), public_point.get(), nullptr,
+			 public_point.get(), n.get(), nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_get_affine_coordinates_GFp(group.get(), public_point.get(),
+						x_bn.get(), y_bn.get(),
+						nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(x_bn.get(), share_secret,
+			 FP_POSITIVE_MATCH_SECRET_BYTES) != FP_PK_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	SHA256_init(&ctx);
+	SHA256_update(&ctx, share_secret, FP_POSITIVE_MATCH_SECRET_BYTES);
+
+	TEST_EQ(AES_set_encrypt_key(SHA256_final(&ctx), 256, &aes_key), 0,
+		"%d");
+
+	memcpy(aes_iv, response.iv, FP_EC_PUBLIC_KEY_IV_LEN);
+
+	CRYPTO_ctr128_encrypt(response.enc_secret, response.enc_secret,
+			      FP_CONTEXT_USERID_LEN, &aes_key, aes_iv,
+			      ecount_buf, &block_num, (block128_f)AES_encrypt);
+	TEST_ASSERT_ARRAY_EQ(
+		response.enc_secret,
+		expected_positive_match_secret_for_empty_user_id,
+		static_cast<int>(sizeof(
+			expected_positive_match_secret_for_empty_user_id)));
+
+	return EC_SUCCESS;
+}
+
 void run_test(int argc, const char **argv)
 {
 	RUN_TEST(test_fp_command_establish_pk_without_seed);
@@ -852,5 +1027,6 @@ void run_test(int argc, const char **argv)
 	RUN_TEST(test_fp_command_nonce_context);
 	RUN_TEST(test_fp_command_nonce_context_clear);
 	RUN_TEST(test_fp_command_nonce_context_limit);
+	RUN_TEST(test_fp_command_read_match_secret_with_pubkey_succeed);
 	test_print_result();
 }
