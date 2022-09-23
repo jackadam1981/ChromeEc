f7d64043607353fe4495664260e2595c0e89ff53
Yi Chou <yich@google.com>
2022-09-23 11:26:26 +0800

    fpsensor: Add EC_CMD_FP_LOAD_PAIRING_KEY
    This command would load encrypted GSC pairing key from biod.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    
    Change-Id: Ia5672eb39d283cf86064366f3a26453630af3adc

--- common/fpsensor/fpsensor_auth_commands.cc
+++ common/fpsensor/fpsensor_auth_commands.cc
@@ -13,7 +13,12 @@
 #include "openssl/mem.h"
 #include "scoped_fast_cpu.h"
 
+#include <algorithm>
+#include <array>
 #include <utility>
+
+/* The GSC pairing key. */
+static std::array<uint8_t, FP_PAIRING_KEY_LEN> pairing_key;
 
 /**
  * Clear all fingerprint templates associated with the current user id.
@@ -112,3 +117,29 @@ fp_command_establish_pairing_key_wrap(struct host_cmd_handler_args *args)
 }
 DECLARE_HOST_COMMAND(EC_CMD_FP_ESTABLISH_PAIRING_KEY_WRAP,
 		     fp_command_establish_pairing_key_wrap, EC_VER_MASK(0));
+
+static enum ec_status
+fp_command_load_pairing_key(struct host_cmd_handler_args *args)
+{
+	const auto *params = static_cast<const ec_params_fp_load_pairing_key *>(
+		args->params);
+
+	ScopedFastCpu fast_cpu;
+
+	/* Clear the context to prevent leaking the existing template. */
+	fp_clear_context();
+
+	enum ec_error_list ret =
+		decrypt_data(params->encrypted_pairing_key.info,
+			     params->encrypted_pairing_key.data,
+			     sizeof(params->encrypted_pairing_key.data),
+			     pairing_key.data(), pairing_key.size());
+	if (ret != EC_SUCCESS) {
+		CPRINTS("load_pairing_key: Failed to decrypt pairing key");
+		return EC_RES_UNAVAILABLE;
+	}
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_FP_LOAD_PAIRING_KEY, fp_command_load_pairing_key,
+		     EC_VER_MASK(0));
