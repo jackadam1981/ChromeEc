903304698dc205b4e3ef1ba274ba1a6ef3c66fa8
Yi Chou <yich@google.com>
2022-09-23 15:48:36 +0800

    fpsensor: Add EC_CMD_FP_READ_MATCH_SECRET_WITH_PUBKEY
    This command would prevent the passive attacker reading the match
    secret without knowning the private key in cryptohome.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I77f80fe819482234f4679c49922e6139c4cee5eb

--- test/fpsensor_state.cc
+++ test/fpsensor_state.cc
@@ -377,10 +377,443 @@ test_static int test_fp_command_read_match_secret_derive_succeed(void)
 	return EC_SUCCESS;
 }
 
-<<<<<<< HEAD
-=======
+test_static int test_fp_command_establish_pk_without_seed(void)
+{
+	int rv;
+	struct ec_response_fp_establish_pk_keygen keygen_response;
 
->>>>>>> f7c27d3810 (fpsensor: Add limits to nonce context)
+	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PK_KEYGEN, 0, NULL, 0,
+				    &keygen_response, sizeof(keygen_response));
+
+	TEST_NE(rv, EC_RES_SUCCESS, "%d");
+
+	return EC_SUCCESS;
+}
+
+test_static int test_fp_command_establish_and_load_pk(void)
+{
+	int rv;
+	struct ec_response_fp_establish_pk_keygen keygen_response;
+	struct ec_params_fp_establish_pk_wrap wrap_params;
+	struct ec_response_fp_establish_pk_wrap wrap_response;
+	struct ec_params_fp_load_pk load_params;
+
+	uint8_t privkey[FP_PK_EC_PRIVATE_KEY_LEN];
+
+	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PK_KEYGEN, 0, NULL, 0,
+				    &keygen_response, sizeof(keygen_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	memcpy(&wrap_params.enc_privkey_info, &keygen_response.enc_privkey_info,
+	       sizeof(keygen_response.enc_privkey_info));
+
+	memcpy(wrap_params.enc_privkey, keygen_response.enc_privkey,
+	       sizeof(keygen_response.enc_privkey));
+
+	trng_init();
+	trng_rand_bytes(privkey, FP_PK_EC_PRIVATE_KEY_LEN);
+	trng_exit();
+
+	bssl::UniquePtr<EC_GROUP> group(
+		EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
+
+	if (group.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<BIGNUM> n(
+		BN_bin2bn(privkey, FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (n.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<EC_POINT> public_point(EC_POINT_new(group.get()));
+
+	if (public_point.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_mul(group.get(), public_point.get(), n.get(), nullptr,
+			 nullptr, nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<BIGNUM> x_bn(BN_new());
+
+	if (x_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<BIGNUM> y_bn(BN_new());
+
+	if (y_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_get_affine_coordinates_GFp(group.get(), public_point.get(),
+						x_bn.get(), y_bn.get(),
+						nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(x_bn.get(), wrap_params.peers_pubkey_x,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(y_bn.get(), wrap_params.peers_pubkey_y,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PK_WRAP, 0,
+				    &wrap_params, sizeof(wrap_params),
+				    &wrap_response, sizeof(wrap_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	memcpy(&load_params.enc_pk_info, &wrap_response.enc_pk_info,
+	       sizeof(wrap_response.enc_pk_info));
+
+	memcpy(load_params.enc_pk, wrap_response.enc_pk,
+	       sizeof(wrap_response.enc_pk));
+
+	rv = test_send_host_command(EC_CMD_FP_LOAD_PK, 0, &load_params,
+				    sizeof(load_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	return EC_SUCCESS;
+}
+
+test_static int test_fp_command_establish_pk_fail(void)
+{
+	int rv;
+	struct ec_response_fp_establish_pk_keygen keygen_response;
+	struct ec_params_fp_establish_pk_wrap wrap_params;
+	struct ec_response_fp_establish_pk_wrap wrap_response;
+
+	uint8_t privkey[FP_PK_EC_PRIVATE_KEY_LEN];
+
+	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PK_KEYGEN, 0, NULL, 0,
+				    &keygen_response, sizeof(keygen_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	/* No encryption info. */
+	memset(&wrap_params.enc_privkey_info, 0,
+	       sizeof(wrap_params.enc_privkey_info));
+
+	memcpy(wrap_params.enc_privkey, keygen_response.enc_privkey,
+	       sizeof(keygen_response.enc_privkey));
+
+	trng_init();
+	trng_rand_bytes(privkey, FP_PK_EC_PRIVATE_KEY_LEN);
+	trng_exit();
+
+	bssl::UniquePtr<EC_GROUP> group(
+		EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
+
+	if (group.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<BIGNUM> n(
+		BN_bin2bn(privkey, FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (n.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<EC_POINT> public_point(EC_POINT_new(group.get()));
+
+	if (public_point.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_mul(group.get(), public_point.get(), n.get(), nullptr,
+			 nullptr, nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<BIGNUM> x_bn(BN_new());
+
+	if (x_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<BIGNUM> y_bn(BN_new());
+
+	if (y_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_get_affine_coordinates_GFp(group.get(), public_point.get(),
+						x_bn.get(), y_bn.get(),
+						nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(x_bn.get(), wrap_params.peers_pubkey_x,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(y_bn.get(), wrap_params.peers_pubkey_y,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PK_WRAP, 0,
+				    &wrap_params, sizeof(wrap_params),
+				    &wrap_response, sizeof(wrap_response));
+
+	TEST_NE(rv, EC_RES_SUCCESS, "%d");
+
+	return EC_SUCCESS;
+}
+
+test_static int test_fp_command_load_pk_fail(void)
+{
+	int rv;
+	struct ec_response_fp_establish_pk_keygen keygen_response;
+	struct ec_params_fp_establish_pk_wrap wrap_params;
+	struct ec_response_fp_establish_pk_wrap wrap_response;
+	struct ec_params_fp_load_pk load_params;
+
+	uint8_t privkey[FP_PK_EC_PRIVATE_KEY_LEN];
+
+	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PK_KEYGEN, 0, NULL, 0,
+				    &keygen_response, sizeof(keygen_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	memcpy(&wrap_params.enc_privkey_info, &keygen_response.enc_privkey_info,
+	       sizeof(keygen_response.enc_privkey_info));
+
+	memcpy(wrap_params.enc_privkey, keygen_response.enc_privkey,
+	       sizeof(keygen_response.enc_privkey));
+
+	trng_init();
+	trng_rand_bytes(privkey, FP_PK_EC_PRIVATE_KEY_LEN);
+	trng_exit();
+
+	bssl::UniquePtr<EC_GROUP> group(
+		EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
+
+	if (group.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<BIGNUM> n(
+		BN_bin2bn(privkey, FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (n.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<EC_POINT> public_point(EC_POINT_new(group.get()));
+
+	if (public_point.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_mul(group.get(), public_point.get(), n.get(), nullptr,
+			 nullptr, nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<BIGNUM> x_bn(BN_new());
+
+	if (x_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	bssl::UniquePtr<BIGNUM> y_bn(BN_new());
+
+	if (y_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_get_affine_coordinates_GFp(group.get(), public_point.get(),
+						x_bn.get(), y_bn.get(),
+						nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(x_bn.get(), wrap_params.peers_pubkey_x,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(y_bn.get(), wrap_params.peers_pubkey_y,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	rv = test_send_host_command(EC_CMD_FP_ESTABLISH_PK_WRAP, 0,
+				    &wrap_params, sizeof(wrap_params),
+				    &wrap_response, sizeof(wrap_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	/* No encryption info. */
+	memset(&load_params.enc_pk_info, 0, sizeof(load_params.enc_pk_info));
+
+	memcpy(load_params.enc_pk, wrap_response.enc_pk,
+	       sizeof(wrap_response.enc_pk));
+
+	rv = test_send_host_command(EC_CMD_FP_LOAD_PK, 0, &load_params,
+				    sizeof(load_params), NULL, 0);
+
+	TEST_NE(rv, EC_RES_SUCCESS, "%d");
+
+	return EC_SUCCESS;
+}
+
+test_static int test_fp_command_generate_nonce(void)
+{
+	int rv;
+	struct ec_response_fp_generate_nonce nonce_response;
+
+	rv = test_send_host_command(EC_CMD_FP_GENERATE_NONCE, 0, NULL, 0,
+				    &nonce_response, sizeof(nonce_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	return EC_SUCCESS;
+}
+
+test_static int test_fp_command_nonce_context(void)
+{
+	int rv;
+	struct ec_response_fp_generate_nonce nonce_response;
+	struct ec_params_fp_nonce_context nonce_params = {};
+
+	templ_valid = 1;
+
+	rv = test_send_host_command(EC_CMD_FP_GENERATE_NONCE, 0, NULL, 0,
+				    &nonce_response, sizeof(nonce_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	rv = test_send_host_command(EC_CMD_FP_NONCE_CONTEXT, 0, &nonce_params,
+				    sizeof(nonce_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	TEST_EQ(templ_valid, 1u, "%d");
+
+	return EC_SUCCESS;
+}
+
+test_static int test_fp_command_nonce_context_clear(void)
+{
+	int rv;
+	struct ec_response_fp_generate_nonce nonce_response;
+	struct ec_params_fp_nonce_context nonce_params = {
+		.clear_context = 1,
+	};
+
+	templ_valid = 1;
+
+	rv = test_send_host_command(EC_CMD_FP_GENERATE_NONCE, 0, NULL, 0,
+				    &nonce_response, sizeof(nonce_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	rv = test_send_host_command(EC_CMD_FP_NONCE_CONTEXT, 0, &nonce_params,
+				    sizeof(nonce_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	TEST_EQ(templ_valid, 0u, "%d");
+
+	return EC_SUCCESS;
+}
+
+test_static int test_fp_command_nonce_context_limit(void)
+{
+	int rv;
+	struct ec_params_fp_context_v1 ctx_params = {
+		.action = FP_CONTEXT_GET_RESULT,
+	};
+	struct ec_response_fp_generate_nonce nonce_response;
+	struct ec_params_fp_nonce_context nonce_params = {
+		.clear_context = 1,
+	};
+
+	rv = test_send_host_command(EC_CMD_FP_CONTEXT, 1, &ctx_params,
+				    sizeof(ctx_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	/* Call nonce context without generated nonce should fail. */
+	rv = test_send_host_command(EC_CMD_FP_NONCE_CONTEXT, 0, &nonce_params,
+				    sizeof(nonce_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_ACCESS_DENIED, "%d");
+
+	rv = test_send_host_command(EC_CMD_FP_GENERATE_NONCE, 0, NULL, 0,
+				    &nonce_response, sizeof(nonce_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	/* Normal context should not clear the generated nonce. */
+	/* This will be used for the migration path. */
+	rv = test_send_host_command(EC_CMD_FP_CONTEXT, 1, &ctx_params,
+				    sizeof(ctx_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	/* Call nonce context with generated nonce should success. */
+	rv = test_send_host_command(EC_CMD_FP_NONCE_CONTEXT, 0, &nonce_params,
+				    sizeof(nonce_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	rv = test_send_host_command(EC_CMD_FP_GENERATE_NONCE, 0, NULL, 0,
+				    &nonce_response, sizeof(nonce_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	rv = test_send_host_command(EC_CMD_FP_NONCE_CONTEXT, 0, &nonce_params,
+				    sizeof(nonce_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	/* Call nonce context twice should fail. */
+	rv = test_send_host_command(EC_CMD_FP_NONCE_CONTEXT, 0, &nonce_params,
+				    sizeof(nonce_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_ACCESS_DENIED, "%d");
+
+	rv = test_send_host_command(EC_CMD_FP_GENERATE_NONCE, 0, NULL, 0,
+				    &nonce_response, sizeof(nonce_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	rv = test_send_host_command(EC_CMD_FP_GENERATE_NONCE, 0, NULL, 0,
+				    &nonce_response, sizeof(nonce_response));
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	rv = test_send_host_command(EC_CMD_FP_NONCE_CONTEXT, 0, &nonce_params,
+				    sizeof(nonce_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_SUCCESS, "%d");
+
+	/* Call nonce context twice should fail even we generated two nonces. */
+	rv = test_send_host_command(EC_CMD_FP_NONCE_CONTEXT, 0, &nonce_params,
+				    sizeof(nonce_params), NULL, 0);
+
+	TEST_EQ(rv, EC_RES_ACCESS_DENIED, "%d");
+
+	return EC_SUCCESS;
+}
+
 extern "C" void run_test(int argc, const char **argv)
 {
 	RUN_TEST(test_fp_enc_status_valid_flags);
