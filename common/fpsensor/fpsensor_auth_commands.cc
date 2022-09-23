e5fe541c0dadb4d82593489037683ad772aa6314
Yi Chou <yich@google.com>
2022-09-23 11:26:26 +0800

    fpsensor: Add EC_CMD_FP_LOAD_PAIRING_KEY
    This command would load encrypted GSC pairing key from biod.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    
    Change-Id: Ia5672eb39d283cf86064366f3a26453630af3adc

--- common/fpsensor/fpsensor_auth_commands.cc
+++ common/fpsensor/fpsensor_auth_commands.cc
@@ -14,6 +14,8 @@
 
 #include <assert.h>
 
+#include <utility>
+
 extern "C" {
 #include "common.h"
 #include "ec_commands.h"
@@ -32,6 +34,8 @@ extern "C" {
 #include "fpsensor_utils.h"
 #include "scoped_fast_cpu.h"
 
+#include <algorithm>
+#include <array>
 #include <iterator>
 #include <type_traits>
 
@@ -52,6 +56,9 @@ void fp_clear_context(void)
 	for (uint16_t idx = 0; idx < FP_MAX_FINGER_COUNT; idx++)
 		fp_clear_finger_context(idx);
 }
+
+/* The GSC paring key. */
+std::array<uint8_t, FP_PAIRING_KEY_LEN> pairing_key;
 
 enum ec_error_list fill_pubkey(const EC_KEY &key,
 			       struct ec_fp_ec_public_key &pubkey)
@@ -297,3 +304,29 @@ fp_command_establish_pairing_key_wrap(struct host_cmd_handler_args *args)
 }
 DECLARE_HOST_COMMAND(EC_CMD_FP_ESTABLISH_PAIRING_KEY_WRAP,
 		     fp_command_establish_pairing_key_wrap, EC_VER_MASK(0));
+
+static enum ec_status
+fp_command_load_pairing_key(struct host_cmd_handler_args *args)
+{
+	const auto *params = static_cast<const ec_params_fp_load_pairing_key *>(
+		args->params);
+
+	ScopedFastCpu fast_cpu;
+
+	/* Clear the context to prevent leaking the existing template. */
+	fp_clear_context();
+
+	enum ec_error_list ret =
+		decrypt_data(params->encrypted_pairing_key.info,
+			     params->encrypted_pairing_key.data,
+			     sizeof(params->encrypted_pairing_key.data),
+			     pairing_key.data(), pairing_key.size());
+	if (ret != EC_SUCCESS) {
+		CPRINTS("load_pairing_key: Failed to decrypt pairing key");
+		return EC_RES_UNAVAILABLE;
+	}
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_FP_LOAD_PAIRING_KEY, fp_command_load_pairing_key,
+		     EC_VER_MASK(0));
