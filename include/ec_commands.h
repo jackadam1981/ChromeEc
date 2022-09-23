1eff3e9aaece35f136f9e3cf21f135974083e22e
Yi Chou <yich@google.com>
2022-09-23 15:48:36 +0800

    fpsensor: Add EC_CMD_FP_READ_MATCH_SECRET_WITH_PUBKEY
    This command would prevent the passive attacker reading the match
    secret without knowning the private key in cryptohome.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I77f80fe819482234f4679c49922e6139c4cee5eb

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -7782,6 +7782,23 @@ struct ec_params_fp_nonce_context {
 	uint8_t clear_context;
 } __ec_align4;
 
+#define EC_CMD_FP_READ_MATCH_SECRET_WITH_PUBKEY 0x0415
+
+#define FP_EC_PUBLIC_KEY_LEN 32
+#define FP_EC_PUBLIC_KEY_IV_LEN 16
+struct ec_params_fp_read_match_secret_with_pubkey {
+	uint16_t fgr;
+	uint8_t pubkey_x[FP_EC_PUBLIC_KEY_LEN];
+	uint8_t pubkey_y[FP_EC_PUBLIC_KEY_LEN];
+} __ec_align4;
+
+struct ec_response_fp_read_match_secret_with_pubkey {
+	uint8_t pubkey_x[FP_EC_PUBLIC_KEY_LEN];
+	uint8_t pubkey_y[FP_EC_PUBLIC_KEY_LEN];
+	uint8_t iv[FP_EC_PUBLIC_KEY_IV_LEN];
+	uint8_t enc_secret[FP_POSITIVE_MATCH_SECRET_BYTES];
+} __ec_align4;
+
 /*****************************************************************************/
 /* Touchpad MCU commands: range 0x0500-0x05FF */
 
