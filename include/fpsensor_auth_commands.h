93e674342a1eb3fdc88c71f594952e45b2d04138
Yi Chou <yich@google.com>
2022-09-23 11:26:26 +0800

    fpsensor: Add EC_CMD_FP_LOAD_PK
    This command would load encryped GSC pairing key from biod.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: Ia5672eb39d283cf86064366f3a26453630af3adc

---
+++ include/fpsensor_auth_commands.h
@@ -0,0 +1,26 @@
+/* Copyright 2019 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Fingerprint sensor interface */
+
+#ifndef __CROS_EC_FPSENSOR_AUTH_COMMANDS_H
+#define __CROS_EC_FPSENSOR_AUTH_COMMANDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @warning |fp_buffer| contains data used by the matching algorithm that must
+ * be released by calling fp_sensor_deinit() first. Call
+ * fp_reset_and_clear_context instead of calling this directly.
+ */
+void fp_clear_context(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CROS_EC_FPSENSOR_AUTH_COMMANDS_H */
