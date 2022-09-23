310dd909a137a180ef0817119186541783bbc40f
Yi Chou <yich@google.com>
2022-09-23 14:58:39 +0800

    fpsensor: Add limits to nonce context
    To prevent downgrade & replay attack, we would harden the nonce
    context with the following limits:
    1. Only do FP match once in a nonce context.
    2. EC_CMD_FP_CONTEXT would clear the context if we already in a nonce
       context.
    3. EC_CMD_FP_GENERATE_NONCE would clear the context if we already in a
       nonce context.
    4. EC_CMD_FP_NONCE_CONTEXT can only be used when we have valid auth
       nonce.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: Ia6b8a094da147f9cd37625ad429e6c85fce5b9b4

--- common/fpsensor/fpsensor_state.c
+++ common/fpsensor/fpsensor_state.c
@@ -61,6 +61,8 @@ uint8_t tpm_seed[FP_CONTEXT_TPM_BYTES];
 uint8_t pairing_key[FP_PK_LEN];
 /* The auth nonce for CK. */
 uint8_t auth_nonce[FP_CK_AUTH_NONCE_LEN];
+/* Status of the FP context. */
+uint32_t fp_context_status;
 /* Status of the FP encryption engine. */
 static uint32_t fp_encryption_status;
 
@@ -94,6 +96,7 @@ static void _fp_clear_context(void)
 
 	templ_valid = 0;
 	templ_dirty = 0;
+	fp_context_status = 0;
 	always_memset(fp_buffer, 0, sizeof(fp_buffer));
 	always_memset(fp_enc_buffer, 0, sizeof(fp_enc_buffer));
 	always_memset(user_id, 0, sizeof(user_id));
@@ -249,7 +252,13 @@ static enum ec_status fp_command_context(struct host_cmd_handler_args *args)
 		if (sensor_mode & FP_MODE_RESET_SENSOR)
 			return EC_RES_BUSY;
 
+		if (fp_context_status & FP_CONTEXT_STATUS_NONCE_CONTEXT) {
+			/* Clear the context to prevent downgrade attack. */
+			_fp_clear_context();
+		}
+
 		memcpy(user_id, p->userid, sizeof(user_id));
+
 		return EC_RES_SUCCESS;
 	}
 
@@ -500,11 +509,20 @@ fp_command_generate_nonce(struct host_cmd_handler_args *args)
 {
 	struct ec_response_fp_generate_nonce *r = args->response;
 
+	if (fp_context_status & FP_CONTEXT_STATUS_NONCE_CONTEXT) {
+		/* Clear the context to prevent leaking the data from previous
+		 * nonce context.
+		 */
+		_fp_clear_context();
+	}
+
 	trng_init();
 	trng_rand_bytes(auth_nonce, FP_CK_AUTH_NONCE_LEN);
 	trng_exit();
 
 	memcpy(r->nonce, auth_nonce, FP_CK_AUTH_NONCE_LEN);
+
+	fp_context_status |= FP_CONTEXT_AUTH_NONCE_SET;
 
 	args->response_size = sizeof(*r);
 	return EC_RES_SUCCESS;
@@ -518,6 +536,11 @@ static enum ec_status
 fp_command_nonce_context(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_fp_nonce_context *p = args->params;
+
+	if (!(fp_context_status & FP_CONTEXT_AUTH_NONCE_SET)) {
+		CPRINTS("No existing auth nonce");
+		return EC_RES_ACCESS_DENIED;
+	}
 
 	struct sha256_ctx ctx;
 
@@ -559,6 +582,7 @@ fp_command_nonce_context(struct host_cmd_handler_args *args)
 	/* Set the user_id. */
 	memcpy(user_id, raw_user_id, FP_CONTEXT_USERID_LEN);
 
+	fp_context_status = FP_CONTEXT_STATUS_NONCE_CONTEXT;
 	return EC_RES_SUCCESS;
 }
 DECLARE_HOST_COMMAND(EC_CMD_FP_NONCE_CONTEXT, fp_command_nonce_context,
