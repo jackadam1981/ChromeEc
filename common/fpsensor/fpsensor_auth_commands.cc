d8421032458235ac71e47702862218bd34048dc9
Yi Chou <yich@google.com>
2022-09-23 14:58:39 +0800

    fpsensor: Add limits to nonce context
    To prevent downgrade & replay attack, we would harden the nonce
    context with the following limits:
    1. Only do FP match once in a nonce context.
    2. EC_CMD_FP_CONTEXT would clear the context if we already in a nonce
       context.
    3. EC_CMD_FP_GENERATE_NONCE would clear the context if we already in a
       nonce context.
    4. EC_CMD_FP_NONCE_CONTEXT can only be used when we have valid auth
       nonce.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: Ia6b8a094da147f9cd37625ad429e6c85fce5b9b4

--- common/fpsensor/fpsensor_auth_commands.cc
+++ common/fpsensor/fpsensor_auth_commands.cc
@@ -58,6 +58,7 @@ void fp_clear_context(void)
 {
 	templ_valid = 0;
 	templ_dirty = 0;
+	fp_encryption_status &= FP_ENC_STATUS_SEED_SET;
 	OPENSSL_cleanse(fp_buffer, sizeof(fp_buffer));
 	OPENSSL_cleanse(fp_enc_buffer, sizeof(fp_enc_buffer));
 	OPENSSL_cleanse(user_id, sizeof(user_id));
@@ -335,11 +336,20 @@ fp_command_generate_nonce(struct host_cmd_handler_args *args)
 
 	ScopedFastCpu fast_cpu;
 
+	if (fp_encryption_status & FP_CONTEXT_STATUS_NONCE_CONTEXT_SET) {
+		/* Clear the context to prevent leaking the data from previous
+		 * nonce context.
+		 */
+		fp_clear_context();
+	}
+
 	trng_init();
 	trng_rand_bytes(auth_nonce.data(), auth_nonce.size());
 	trng_exit();
 
 	std::copy(auth_nonce.begin(), auth_nonce.end(), r->nonce);
+
+	fp_encryption_status |= FP_CONTEXT_AUTH_NONCE_SET;
 
 	args->response_size = sizeof(*r);
 	return EC_RES_SUCCESS;
@@ -352,6 +362,11 @@ fp_command_nonce_context(struct host_cmd_handler_args *args)
 {
 	const auto *p =
 		static_cast<const ec_params_fp_nonce_context *>(args->params);
+
+	if (!(fp_encryption_status & FP_CONTEXT_AUTH_NONCE_SET)) {
+		CPRINTS("No existing auth nonce");
+		return EC_RES_ACCESS_DENIED;
+	}
 
 	ScopedFastCpu fast_cpu;
 
@@ -394,6 +409,8 @@ fp_command_nonce_context(struct host_cmd_handler_args *args)
 	/* Set the user_id. */
 	memcpy(user_id, raw_user_id, FP_CONTEXT_USERID_LEN);
 
+	fp_encryption_status &= FP_ENC_STATUS_SEED_SET;
+	fp_encryption_status |= FP_CONTEXT_STATUS_NONCE_CONTEXT_SET;
 	return EC_RES_SUCCESS;
 }
 DECLARE_HOST_COMMAND(EC_CMD_FP_NONCE_CONTEXT, fp_command_nonce_context,
