f271cba6efe8acf0ac023eb2ba8da67ab77f4d89
Yi Chou <yich@google.com>
2022-09-23 11:54:27 +0800

    fpsensor: Add EC_CMD_FP_GENERATE_NONCE
    This command would generate a nonce for the nonce context.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I3e029ec02965a0978b9db0d784cbcde8a6744b8e

--- common/fpsensor/fpsensor_auth_commands.cc
+++ common/fpsensor/fpsensor_auth_commands.cc
@@ -38,6 +38,8 @@ BUILD_ASSERT(FP_PAIRING_KEY_EC_PRIVATE_KEY_LEN == 32);
 
 /* The GSC paring key. */
 static std::array<uint8_t, FP_PAIRING_KEY_LEN> pairing_key;
+/* The auth nonce for CK. */
+static std::array<uint8_t, FP_CK_AUTH_NONCE_LEN> auth_nonce;
 
 /**
  * @warning |fp_buffer| contains data used by the matching algorithm that must
@@ -51,6 +53,7 @@ void fp_clear_context(void)
 	OPENSSL_cleanse(fp_buffer, sizeof(fp_buffer));
 	OPENSSL_cleanse(fp_enc_buffer, sizeof(fp_enc_buffer));
 	OPENSSL_cleanse(user_id, sizeof(user_id));
+	OPENSSL_cleanse(auth_nonce.data(), auth_nonce.size());
 	fp_disable_positive_match_secret(&positive_match_secret_state);
 	for (uint16_t idx = 0; idx < FP_MAX_FINGER_COUNT; idx++)
 		fp_clear_finger_context(idx);
@@ -339,4 +342,23 @@ fp_command_load_pairing_key(struct host_cmd_handler_args *args)
 	return EC_RES_SUCCESS;
 }
 DECLARE_HOST_COMMAND(EC_CMD_FP_LOAD_PAIRING_KEY, fp_command_load_pairing_key,
+		     EC_VER_MASK(0));
+
+static enum ec_status
+fp_command_generate_nonce(struct host_cmd_handler_args *args)
+{
+	auto *r = static_cast<ec_response_fp_generate_nonce *>(args->response);
+
+	ScopedFastCpu fast_cpu;
+
+	trng_init();
+	trng_rand_bytes(auth_nonce.data(), auth_nonce.size());
+	trng_exit();
+
+	std::copy(auth_nonce.begin(), auth_nonce.end(), r->nonce);
+
+	args->response_size = sizeof(*r);
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_FP_GENERATE_NONCE, fp_command_generate_nonce,
 		     EC_VER_MASK(0));
