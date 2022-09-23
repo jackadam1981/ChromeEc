42fe8f71603f00477c1dbe4b85404d98aa1d6f33
Harsha B R <harsha.b.r@intel.com>
2022-09-23 15:31:07 +0530

    print_temps: Modify the help description of temps cmd
    Fan speed of temps command is the calculated ratio of
    Fan_off and fan_max with respect to corresponding sensor
    temperature. Update the description accordingly so that
    the output of temps command is not misinterpreted.
    
    temps command description from EC console,
    help temps
    Usage: temps
    Print temp sensors and temp ratio.
    
    BUG=b:246929159
    TEST=make -j BOARD=gimble, zmake build nivviks,
    check temps description
    BRANCH=none
    
    Signed-off-by: Harsha B R <harsha.b.r@intel.com>
    Change-Id: Iabdd81876985b40da56d2dc669e14bd87eb7ca1a

--- common/temp_sensor.c
+++ common/temp_sensor.c
@@ -147,7 +147,7 @@ static int command_temps(int argc, const char **argv)
 	return print_temps();
 }
 DECLARE_CONSOLE_COMMAND(temps, command_temps, NULL,
-			"Print temp sensors and fan speed");
+			"Print temp sensors and temp ratio.");
 #endif
 
 /*****************************************************************************/
