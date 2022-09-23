019d163bcaadfa560dc51e44dc0e783ce5cf21a5
Yi Chou <yich@google.com>
2022-09-23 11:54:27 +0800

    fpsensor: Add EC_CMD_FP_GENERATE_NONCE
    This command would generate a nonce for the nonce context.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I3e029ec02965a0978b9db0d784cbcde8a6744b8e

--- common/fpsensor/fpsensor_state.c
+++ common/fpsensor/fpsensor_state.c
@@ -58,6 +58,8 @@ uint32_t user_id[FP_CONTEXT_USERID_WORDS];
 uint8_t tpm_seed[FP_CONTEXT_TPM_BYTES];
 /* The GSC paring key. */
 uint8_t pairing_key[FP_PK_LEN];
+/* The auth nonce for CK. */
+uint8_t auth_nonce[FP_CK_AUTH_NONCE_LEN];
 /* Status of the FP encryption engine. */
 static uint32_t fp_encryption_status;
 
@@ -94,6 +96,7 @@ static void _fp_clear_context(void)
 	always_memset(fp_buffer, 0, sizeof(fp_buffer));
 	always_memset(fp_enc_buffer, 0, sizeof(fp_enc_buffer));
 	always_memset(user_id, 0, sizeof(user_id));
+	always_memset(auth_nonce, 0, sizeof(auth_nonce));
 	fp_disable_positive_match_secret(&positive_match_secret_state);
 	for (idx = 0; idx < FP_MAX_FINGER_COUNT; idx++)
 		fp_clear_finger_context(idx);
@@ -479,3 +482,20 @@ static enum ec_status fp_command_load_pk(struct host_cmd_handler_args *args)
 	return EC_RES_SUCCESS;
 }
 DECLARE_HOST_COMMAND(EC_CMD_FP_LOAD_PK, fp_command_load_pk, EC_VER_MASK(0));
+
+static enum ec_status
+fp_command_generate_nonce(struct host_cmd_handler_args *args)
+{
+	struct ec_response_fp_generate_nonce *r = args->response;
+
+	trng_init();
+	trng_rand_bytes(auth_nonce, FP_CK_AUTH_NONCE_LEN);
+	trng_exit();
+
+	memcpy(r->nonce, auth_nonce, FP_CK_AUTH_NONCE_LEN);
+
+	args->response_size = sizeof(*r);
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_FP_GENERATE_NONCE, fp_command_generate_nonce,
+		     EC_VER_MASK(0));
