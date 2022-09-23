af6c8862666ca0177046780bd606521b29e3fccd
Yi Chou <yich@google.com>
2022-09-23 14:58:39 +0800

    fpsensor: Only do FP match once in a nonce context
    BUG=None
    TEST=None
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: Ia6b8a094da147f9cd37625ad429e6c85fce5b9b4

--- include/fpsensor_state.h
+++ include/fpsensor_state.h
@@ -68,6 +68,8 @@ extern uint8_t tpm_seed[FP_CONTEXT_TPM_BYTES];
 extern uint8_t pairing_key[FP_PK_LEN];
 /* The auth nonce for CK. */
 extern uint8_t auth_nonce[FP_CK_AUTH_NONCE_LEN];
+/* Status of the FP context. */
+extern uint32_t fp_context_status;
 
 extern atomic_t fp_events;
 
