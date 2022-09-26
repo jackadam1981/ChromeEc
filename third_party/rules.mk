20f0563fe3a1eaa994e2e376a446612c59888915
Yi Chou <yich@google.com>
2022-09-26 16:23:00 +0800

    Use third_party boringssl for AES
    The original boringssl copy is not maintainable in the long term.
    We should build it from the upstream version.
    
    BUG=b:248508087
    TEST=make runhosttests -j
    TEST=make buildall -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I3ead1244c3c839590281e20e5c4828242933d1f2

--- third_party/rules.mk
+++ third_party/rules.mk
@@ -15,6 +15,10 @@ ifeq ($(CONFIG_LIBCRYPTOC),y)
 # $ make CRYPTOC_DIR=~/src/cryptoc BOARD=bloonchipper
 CRYPTOC_DIR ?= ../../third_party/cryptoc
 
+# The boringssl path can be overridden on invocation, as in the following
+# example: $ make BORINGSSL_DIR=~/src/boringssl BOARD=bloonchipper
+BORINGSSL_DIR ?= ../../third_party/boringssl
+
 # SUPPORT_UNALIGNED indicates to libcryptoc that provided data buffers
 # may be unaligned and please handle them safely.
 cmd_libcryptoc = $(MAKE) -C $(CRYPTOC_DIR) \
@@ -24,6 +28,41 @@ cmd_libcryptoc_clean = $(cmd_libcryptoc) -q && echo clean
 
 CPPFLAGS += -I$(CRYPTOC_DIR)/include
 CRYPTOC_LDFLAGS := -L$(out)/cryptoc -lcryptoc
+
+ifeq ($(CROSS_COMPILE), armv7m-cros-eabi-)
+CMAKE_SYSTEM_PROCESSOR ?= armv7
+OPENSSL_NO_ASM ?= 0
+else ifeq ($(CROSS_COMPILE), x86_64-pc-linux-gnu-)
+CMAKE_SYSTEM_PROCESSOR ?= x86_64
+OPENSSL_NO_ASM ?= 1
+else ifeq ($(CONFIG_BORINGSSL_CRYPTO), n)
+$(error ERROR: Unknown compiler: $(CROSS_COMPILE))
+endif
+
+ifeq ($(CONFIG_BORINGSSL_CRYPTO), y)
+BORINGSSL_OUTDIR := $(out)/third_party/boringssl/crypto
+TOOLCHAIN_FILE := $(shell pwd)/third_party/boringssl/cros-ec-toolchain.cmake
+CPPFLAGS += -I$(BORINGSSL_DIR) -I$(BORINGSSL_DIR)/include
+CPPFLAGS += -DOPENSSL_NO_THREADS_CORRUPT_MEMORY_AND_LEAK_SECRETS_IF_THREADED
+$(BORINGSSL_OUTDIR)/libcrypto.a:
+	mkdir -p $(out)/third_party/boringssl/
+	cmake \
+		-DCC_NAME=$(cc-name) \
+		-DCXX_NAME=$(cxx-name) \
+		-DCROSS_COMPILE=$(CROSS_COMPILE) \
+		-DCMAKE_SYSTEM_PROCESSOR=$(CMAKE_SYSTEM_PROCESSOR) \
+		-DCMAKE_SYSROOT=$(SYSROOT) \
+		-DOPENSSL_NO_ASM=$(OPENSSL_NO_ASM) \
+		-DCMAKE_TOOLCHAIN_FILE=$(TOOLCHAIN_FILE) \
+		-B $(out)/third_party/boringssl/ \
+		-S $(BORINGSSL_DIR) \
+		-GNinja
+	ninja -C $(out)/third_party/boringssl/ crypto
+BORINGSSL_LDFLAGS := -L$(BORINGSSL_OUTDIR) -lcrypto
+endif
+
+# set(CMAKE_SYSTEM_PROCESSOR "armv7")
+# set(CROSS_COMPILE "armv7m-cros-eabi-")
 
 # Conditionally force the rebuilding of libcryptoc.a only if it would be
 # changed.
@@ -45,13 +84,29 @@ $(out)/RO/ec.RO.elf $(out)/RO/ec.RO_B.elf: LDFLAGS_EXTRA += $(CRYPTOC_LDFLAGS)
 $(out)/RO/ec.RO.elf $(out)/RO/ec.RO_B.elf: $(out)/cryptoc/libcryptoc.a
 $(out)/RW/ec.RW.elf $(out)/RW/ec.RW_B.elf: LDFLAGS_EXTRA += $(CRYPTOC_LDFLAGS)
 $(out)/RW/ec.RW.elf $(out)/RW/ec.RW_B.elf: $(out)/cryptoc/libcryptoc.a
+ifeq ($(CONFIG_BORINGSSL_CRYPTO), y)
+$(out)/RO/ec.RO.elf $(out)/RO/ec.RO_B.elf: LDFLAGS_EXTRA += $(BORINGSSL_LDFLAGS)
+$(out)/RO/ec.RO.elf $(out)/RO/ec.RO_B.elf: $(BORINGSSL_OUTDIR)/libcrypto.a
+$(out)/RW/ec.RW.elf $(out)/RW/ec.RW_B.elf: LDFLAGS_EXTRA += $(BORINGSSL_LDFLAGS)
+$(out)/RW/ec.RW.elf $(out)/RW/ec.RW_B.elf: $(BORINGSSL_OUTDIR)/libcrypto.a
+endif
+
 # Host test executables (including fuzz tests).
 $(out)/$(PROJECT).exe: LDFLAGS_EXTRA += $(CRYPTOC_LDFLAGS)
 $(out)/$(PROJECT).exe: $(out)/cryptoc/libcryptoc.a
+ifeq ($(CONFIG_BORINGSSL_CRYPTO), y)
+$(out)/$(PROJECT).exe: LDFLAGS_EXTRA += $(BORINGSSL_LDFLAGS)
+$(out)/$(PROJECT).exe: $(BORINGSSL_OUTDIR)/libcrypto.a
+endif
+
 # On-device tests.
 third-party-test-targets=$(foreach test,$(test-list-y),\
 	$(out)/RW/$(test).RW.elf $(out)/RO/$(test).RO.elf)
 $(third-party-test-targets): LDFLAGS_EXTRA += $(CRYPTOC_LDFLAGS)
 $(third-party-test-targets): $(out)/cryptoc/libcryptoc.a
+ifeq ($(CONFIG_BORINGSSL_CRYPTO), y)
+$(third-party-test-targets): LDFLAGS_EXTRA += $(BORINGSSL_LDFLAGS)
+$(third-party-test-targets): $(BORINGSSL_OUTDIR)/libcrypto.a
+endif
 
 endif # CONFIG_LIBCRYPTOC
