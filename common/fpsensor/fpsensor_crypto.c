7d2fab6697a5f3f44a33b2286926d0110c5db851
Yi Chou <yich@google.com>
2022-09-26 16:28:59 +0800

    fpsensor: Enable AES CTR config
    BUG=b:248508087
    TEST=make buildall -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I13b10230f7fa62e05f53b3e92709bd39e3303de9

--- common/fpsensor/fpsensor_crypto.c
+++ common/fpsensor/fpsensor_crypto.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 
 #include "aes.h"
+#include "aes-ctr.h"
 #include "aes-gcm.h"
 #include "cryptoc/util.h"
 #include "fpsensor_crypto.h"
@@ -12,10 +13,11 @@
 #include "fpsensor_state.h"
 #include "rollback.h"
 
-#if !defined(CONFIG_AES) || !defined(CONFIG_AES_GCM) || \
-	!defined(CONFIG_ROLLBACK_SECRET_SIZE)
-#error "fpsensor requires AES, AES_GCM and ROLLBACK_SECRET_SIZE"
+#if !defined(CONFIG_AES) || !defined(CONFIG_AES_CTR) || \
+	!defined(CONFIG_AES_GCM) || !defined(CONFIG_ROLLBACK_SECRET_SIZE)
+#error "fpsensor requires AES, AES_CTR, AES_GCM and ROLLBACK_SECRET_SIZE"
 #endif
+
 
 test_export_static int get_ikm(uint8_t *ikm)
 {
