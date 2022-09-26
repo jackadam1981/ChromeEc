b99350a2fc3f88afa97fcd29494970fd139b5d7c
Yi Chou <yich@google.com>
2022-09-26 16:23:00 +0800

    Use third_party boringssl for AES
    The original boringssl copy is not maintainable in the long term.
    We should build it from the upstream version.
    
    BUG=b:248508087
    TEST=make runhosttests -j
    TEST=make buildall -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I3ead1244c3c839590281e20e5c4828242933d1f2

--- test/aes.cc
+++ test/aes.cc
@@ -19,14 +19,19 @@
 #include "test_util.h"
 
 extern "C" {
-#include "aes.h"
-#include "aes-gcm.h"
 #include "builtin/assert.h"
 #include "console.h"
 #include "timer.h"
 #include "util.h"
 #include "watchdog.h"
 }
+
+#include "aes_gcm_helpers.h"
+#include "openssl/aes.h"
+
+/* These must be included after the "openssl/aes.h" */
+#include "crypto/fipsmodule/aes/internal.h"
+#include "crypto/fipsmodule/modes/internal.h"
 
 /* Temporary buffer, to avoid using too much stack space. */
 static uint8_t tmp[512];
@@ -449,6 +454,7 @@ static void test_aes_gcm_speed(void)
 
 	benchmark.run("AES-GCM encrypt", [&]() {
 		AES_set_encrypt_key(key, 8 * key_size, &aes_key);
+
 		CRYPTO_gcm128_init(&ctx, &aes_key, (block128_f)AES_encrypt, 0);
 		CRYPTO_gcm128_setiv(&ctx, &aes_key, nonce, nonce_size);
 		CRYPTO_gcm128_encrypt(&ctx, &aes_key, plaintext, encrypted_data,
@@ -458,6 +464,7 @@ static void test_aes_gcm_speed(void)
 
 	benchmark.run("AES-GCM decrypt", [&]() {
 		AES_set_encrypt_key(key, 8 * key_size, &aes_key);
+
 		CRYPTO_gcm128_init(&ctx, &aes_key, (block128_f)AES_encrypt, 0);
 		CRYPTO_gcm128_setiv(&ctx, &aes_key, nonce, nonce_size);
 		auto decrypt_res =
