07882029f5e97208cc11491655355f9c51086916
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2022-09-26 16:49:38 +0800

    Craask: Create interrupts.dts and adc.dts
    Move interrupts and adc to two independent dts files.
    
    BUG=none
    BRANCH=none
    TEST=zmake build craask
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: I9b8f7c7fabb5b078c9d9e26e99435d7f900bb622

--- zephyr/projects/nissa/BUILD.py
+++ zephyr/projects/nissa/BUILD.py
@@ -64,6 +64,8 @@ craask = register_nissa_project(
     chip="npcx9m3f",
     extra_dts_overlays=[
         "generated.dts",
+        "interrupts.dts",
+        "adc.dts",
         "cbi.dts",
         "overlay.dts",
         "motionsense.dts",
