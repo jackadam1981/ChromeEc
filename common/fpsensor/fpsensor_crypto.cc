e4544c4597c3396e81c85b38222c55fb0be1a4ad
Yi Chou <yich@google.com>
2022-09-26 16:23:00 +0800

    Use third_party boringssl for AES
    The original boringssl copy is not maintainable in the long term.
    We should build it from the upstream version.
    
    BUG=b:248508087, b:273639386
    TEST=make runhosttests -j
    TEST=make buildall -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I3ead1244c3c839590281e20e5c4828242933d1f2

--- common/fpsensor/fpsensor_crypto.cc
+++ common/fpsensor/fpsensor_crypto.cc
@@ -3,16 +3,21 @@
  * found in the LICENSE file.
  */
 
+#include "aes_gcm_helpers.h"
 #include "fpsensor_crypto.h"
 #include "fpsensor_state.h"
 #include "fpsensor_utils.h"
+#include "openssl/aes.h"
+
+/* These must be included after the "openssl/aes.h" */
+#include "crypto/fipsmodule/aes/internal.h"
+#include "crypto/fipsmodule/modes/internal.h"
 
 extern "C" {
-#include "aes-gcm.h"
-#include "aes.h"
 #include "cryptoc/util.h"
 #include "rollback.h"
 #include "sha256.h"
+#include "util.h"
 
 test_export_static int get_ikm(uint8_t *ikm);
 test_mockable void compute_hmac_sha256(uint8_t *output, const uint8_t *key,
@@ -22,9 +27,9 @@ test_mockable void compute_hmac_sha256(uint8_t *output, const uint8_t *key,
 }
 
 #include <stdbool.h>
-#if !defined(CONFIG_AES) || !defined(CONFIG_AES_GCM) || \
-	!defined(CONFIG_ROLLBACK_SECRET_SIZE)
-#error "fpsensor requires AES, AES_GCM and ROLLBACK_SECRET_SIZE"
+
+#if !defined(CONFIG_BORINGSSL_CRYPTO) || !defined(CONFIG_ROLLBACK_SECRET_SIZE)
+#error "fpsensor requires CONFIG_BORINGSSL_CRYPTO and ROLLBACK_SECRET_SIZE"
 #endif
 
 test_export_static int get_ikm(uint8_t *ikm)
@@ -252,6 +257,7 @@ int aes_gcm_encrypt(const uint8_t *key, int key_size, const uint8_t *plaintext,
 		CPRINTS("Failed to set encryption key: %d", res);
 		return EC_ERROR_UNKNOWN;
 	}
+
 	CRYPTO_gcm128_init(&ctx, &aes_key, (block128_f)AES_encrypt, 0);
 	CRYPTO_gcm128_setiv(&ctx, &aes_key, nonce, nonce_size);
 	/* CRYPTO functions return 1 on success, 0 on error. */
@@ -284,6 +290,7 @@ int aes_gcm_decrypt(const uint8_t *key, int key_size, uint8_t *plaintext,
 		CPRINTS("Failed to set decryption key: %d", res);
 		return EC_ERROR_UNKNOWN;
 	}
+
 	CRYPTO_gcm128_init(&ctx, &aes_key, (block128_f)AES_encrypt, 0);
 	CRYPTO_gcm128_setiv(&ctx, &aes_key, nonce, nonce_size);
 	/* CRYPTO functions return 1 on success, 0 on error. */
