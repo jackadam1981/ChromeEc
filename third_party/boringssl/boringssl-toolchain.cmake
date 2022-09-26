274282b10649b6d94ef87c7a5eab50ac64b3015b
Yi Chou <yich@google.com>
2022-09-26 16:23:00 +0800

    Use third_party boringssl for AES
    The original boringssl copy is not maintainable in the long term.
    We should build it from the upstream version.
    
    BUG=b:248508087
    TEST=make runhosttests -j
    TEST=make buildall -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I3ead1244c3c839590281e20e5c4828242933d1f2

---
+++ third_party/boringssl/boringssl-toolchain.cmake
@@ -0,0 +1,47 @@
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_BUILD_TYPE Release)
+
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER   "${CROSS_COMPILE}${CC_NAME}")
+set(CMAKE_CXX_COMPILER "${CROSS_COMPILE}${CXX_NAME}")
+set(CMAKE_LINKER       "${CROSS_COMPILE}ld.lld")
+set(CMAKE_OBJCOPY      "${CROSS_COMPILE}objcopy")
+set(CMAKE_OBJDUMP      "${CROSS_COMPILE}objdump")
+set(CMAKE_READELF      "${CROSS_COMPILE}readelf")
+
+if ("${CC_NAME}" STREQUAL gcc)
+set(CMAKE_AR           "${CROSS_COMPILE}gcc-ar")
+set(CMAKE_NM           "${CROSS_COMPILE}gcc-nm")
+set(CMAKE_RANLIB       "${CROSS_COMPILE}gcc-ranlib")
+else()
+set(CMAKE_AR           "${CROSS_COMPILE}ar")
+set(CMAKE_NM           "${CROSS_COMPILE}nm")
+set(CMAKE_RANLIB       "${CROSS_COMPILE}ranlib")
+endif()
+
+# Pretend as "Trusty", an embedded platform.
+add_definitions(-D__TRUSTY__)
+
+# See https://www.chromium.org/chromium-os/build/c-exception-support
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables")
+
+# Somehow the system fail to set the -march flag for assembly files.
+# Without this flag, the compiler believes that it's building for ARMv5.
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=${CMAKE_SYSTEM_PROCESSOR}")
+
+# Enable the Link Time Optimization.
+add_compile_options(-flto)
+add_link_options(-flto)
+
+set(CMAKE_POSITION_INDEPENDENT_CODE OFF)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
