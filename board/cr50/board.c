6961eceed038a40b4d3ca393935bdab2bc99419d
Mary Ruthven <mruthven@chromium.org>
2022-09-27 13:54:40 -0500

    TEST-ONLY: open iteflash test image
    Allow rollback and open ccd.
    
    BUG=none
    TEST=none
    
    Change-Id: Id75afe706ea80b1e5b162d1bc85527c185eedba3
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -909,11 +909,7 @@ DECLARE_DEFERRED(process_board_cfg);
 /* Initialize board. */
 static void board_init(void)
 {
-#if defined(CR50_DEV) || defined(CRYPTO_TEST_SETUP)
 	static enum ccd_state ccd_init_state = CCD_STATE_OPENED;
-#else
-	static enum ccd_state ccd_init_state = CCD_STATE_LOCKED;
-#endif
 	static const struct fips_vtable fips_module_vtable = {
 		.shared_mem_acquire = shared_mem_acquire,
 		.shared_mem_release = shared_mem_release,
@@ -1908,7 +1904,6 @@ static int command_board_properties(int argc, char **argv)
 DECLARE_SAFE_CONSOLE_COMMAND(brdprop, command_board_properties,
 			     NULL, "Display board properties");
 
-#ifdef CONFIG_CMD_ROLLBACK
 static int command_rollback(int argc, char **argv)
 {
 	system_ensure_rollback();
@@ -1920,7 +1915,6 @@ static int command_rollback(int argc, char **argv)
 }
 DECLARE_SAFE_CONSOLE_COMMAND(rollback, command_rollback,
 	"", "Force rollback to escape DEV image.");
-#endif
 
 /*
  * Set long life register bit requesting generating of the ITE SYNC sequence
