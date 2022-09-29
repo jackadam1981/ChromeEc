9a5d82378dec299b512d8f99b543bd3e002df660
Tinghan Shen <tinghan.shen@mediatek.com>
2022-09-29 18:11:39 +0800

    seperate scp c1 c0 sram
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Change-Id: Id4a56062dba6718b56c3343bc560f848e7429fb1

--- board/cherry_scp/board.h
+++ board/cherry_scp/board.h
@@ -11,8 +11,7 @@
 #include "baseboard.h"
 
 #define SCP_CORE1_RAM_START 0xa0000
-#define SCP_CORE1_RAM_SIZE 0x1f000 /* 124K */
-#define SCP_CORE1_RAM_PADDING 0xc00 /* for 4K-alignment */
+#define SCP_CORE1_RAM_SIZE 0x20000 /* 128K */
 
 #ifdef BOARD_CHERRY_SCP_CORE1
 
@@ -22,11 +21,11 @@
  * | ROM vectortable, .text, .rodata, .data LMA
  * +-------------------- 0xa0000 + 0x14000 = 0xb4000
  * | RAM .bss, .data
- * +-------------------- 0xa0000 + 0x1ec00 = 0xbec00
+ * +-------------------- 0xa0000 + 0x1fc00 = 0xbfc00
  * | Reserved (padding for 1k-alignment)
- * +-------------------- 0xa0000 + 0x1edb0 = 0xbedb0
+ * +-------------------- 0xa0000 + 0x1fdb0 = 0xbfdb0
  * | IPI shared buffer with AP (288 + 8) * 2
- * +-------------------- 0xa0000 + 0x1f000 = 0xbf000
+ * +-------------------- 0xa0000 + 0x20000 = 0xc0000
  *
  * [Memory remap]
  * SCP core 1 has registers to remap core view addresses by SCP bus. This is
@@ -37,8 +36,7 @@
 #define CONFIG_ROM_BASE 0x0
 #define CONFIG_RAM_BASE 0x14000
 #define CONFIG_ROM_SIZE (CONFIG_RAM_BASE - CONFIG_ROM_BASE)
-#define CONFIG_RAM_SIZE ((CONFIG_IPC_SHARED_OBJ_ADDR & (~(0x400 - 1))) - \
-			 CONFIG_RAM_BASE)
+#define CONFIG_RAM_SIZE ((CONFIG_IPC_SHARED_OBJ_ADDR & (~(0x400 - 1))) - CONFIG_RAM_BASE)
 
 /*
  * SCP_FW_END is used to calc the base of IPI buffer for AP.
@@ -53,24 +51,18 @@
  * | ROM vectortable, .text, .rodata, .data LMA
  * +-------------------- 0x68000
  * | RAM .bss, .data
- * +-------------------- 0xaf000 (4k-alignment)
- * | SCP core1 boot code
- * +-------------------- 0xbf000 (4k-alignment)
- * | Reserved (padding for 4k-alignment)
- * +-------------------- 0xbfc00
+ * +-------------------- 0x9f000 (4k-alignment)
  * | Reserved (padding for 1k-alignment)
- * +-------------------- 0xbfdb0
+ * +-------------------- 0x9fdb0
  * | IPI shared buffer with AP (288 + 8) * 2
- * +-------------------- 0xc0000
+ * +-------------------- 0xa0000
  */
 #define CONFIG_ROM_BASE 0x0
 #define CONFIG_RAM_BASE 0x68000
 #define CONFIG_ROM_SIZE (CONFIG_RAM_BASE - CONFIG_ROM_BASE)
-#define CONFIG_RAM_SIZE \
-	((CONFIG_IPC_SHARED_OBJ_ADDR & (~(0x400 - 1))) - CONFIG_RAM_BASE - \
-	 SCP_CORE1_RAM_SIZE - SCP_CORE1_RAM_PADDING)
+#define CONFIG_RAM_SIZE ((CONFIG_IPC_SHARED_OBJ_ADDR & (~(0x400 - 1))) - CONFIG_RAM_BASE)
 
-#define SCP_FW_END 0xc0000
+#define SCP_FW_END 0xa0000
 
 #endif /* BOARD_CHERRY_SCP_CORE1 */
 
