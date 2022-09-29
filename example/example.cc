9f47ff39fbfa122d1ecc90e7e8e825f06213d26d
Tom Hughes <tomhughes@chromium.org>
2022-09-29 11:44:56 -0700

    WIP: Add cmake example
    BRANCH=none
    BUG=b:248508087
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I6fedc5ac8c118507e18707e3ed0b2a0ce305d422

---
+++ example/example.cc
@@ -0,0 +1,12 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "example.h"
+
+int example_func(int a)
+{
+	int b = 10;
+	return a + b;
+}
