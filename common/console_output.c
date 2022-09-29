c41471bf1909c7f7026b241ae538c144a4f5dd69
Jeremy Bettis <jbettis@google.com>
2022-09-29 17:09:03 -0600

    ec: Delete unused console channel functions
    There are no users of the enable/disable by name functions.
    
    BRANCH=None
    BUG=None
    TEST=CQ
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: Ia9254252440555c78d175b1cc2328d9c168871de

--- common/console_output.c
+++ common/console_output.c
@@ -37,34 +37,6 @@ BUILD_ASSERT(ARRAY_SIZE(channel_names) == CC_CHANNEL_COUNT);
 /* ensure that we are not silently masking additional channels */
 BUILD_ASSERT(CC_CHANNEL_COUNT <= 8 * sizeof(uint32_t));
 
-static int console_channel_name_to_index(const char *name)
-{
-	int i;
-
-	for (i = 0; i < CC_CHANNEL_COUNT; i++) {
-		if (!strncasecmp(name, channel_names[i], strlen(name)))
-			return i;
-	}
-
-	/* Not found */
-	return -1;
-}
-
-void console_channel_enable(const char *name)
-{
-	int index = console_channel_name_to_index(name);
-
-	if (index >= 0 && index != CC_COMMAND)
-		channel_mask |= CC_MASK(index);
-}
-void console_channel_disable(const char *name)
-{
-	int index = console_channel_name_to_index(name);
-
-	if (index >= 0 && index != CC_COMMAND)
-		channel_mask &= ~CC_MASK(index);
-}
-
 bool console_channel_is_disabled(enum console_channel channel)
 {
 	if (!(CC_MASK(channel) & channel_mask))
