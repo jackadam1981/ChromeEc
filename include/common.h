330eb84808066d3096ffdee24b129f9e8c67e60d
Tom Hughes <tomhughes@chromium.org>
2022-10-03 15:08:44 -0700

    include/common: Use noreturn attribute in test and non-test build
    In the test build, the "test_mockable_noreturn" helper adds the
    "__attribute__((weak))" attribute to allow overriding functions. It
    should still keep the "noreturn" attribute to be consistent with the
    non-test build. Otherwise, a function declared with "noreturn" that
    calls one of the functions that uses "test_mockable_noreturn" will fail
    to compile with:
    
    error: function declared 'noreturn' should not return
    [-Werror,-Winvalid-noreturn]
    
    BRANCH=none
    BUG=b:234181908
    TEST=./util/compare_build.sh -b all
         => MATCH
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I6ebfc78b7b07e19f9305a22fbfeccf62b4000667

--- include/common.h
+++ include/common.h
@@ -252,8 +252,13 @@
 #define test_mockable __attribute__((weak))
 #define test_mockable_static __attribute__((weak))
 #define test_mockable_static_inline __attribute__((weak))
+#ifdef CONFIG_ZTEST
 #define test_mockable_noreturn __attribute__((weak))
 #define test_mockable_static_noreturn __attribute__((weak))
+#else
+#define test_mockable_noreturn noreturn __attribute__((weak))
+#define test_mockable_static_noreturn noreturn __attribute__((weak))
+#endif
 #define test_export_static
 #else
 #define test_mockable
