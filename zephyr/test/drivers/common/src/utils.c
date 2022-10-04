0f1a4055e0720738421b0e78151330ac71b22353
Aaron Massey <aaronmassey@google.com>
2022-10-03 18:23:26 -0600

    test: Harden check_console_cmd against race
    There exists a race-condition where clearing the fake backend shell may
    start delete output while the uart is being written to.
    
    Harden against this race-condition by inserting a delay between the
    clearing and writing to the shell in the Zephyr test utility code.
    
    BRANCH=none
    BUG=b:250719984
    TEST=none
    TEST=twister --clobber -i -s zephyr/test/drivers/drivers.default
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: Iad9a298a6eb5b1e38df46600128d35474e041af5

--- zephyr/test/drivers/common/src/utils.c
+++ zephyr/test/drivers/common/src/utils.c
@@ -612,6 +612,13 @@ void check_console_cmd(const char *cmd, const char *expected_output,
 	int rv;
 
 	shell_backend_dummy_clear_output(get_ec_shell());
+	/*
+	 * Harden against race-condition where clearing output can happen at the
+	 * same time as writing to console
+	 */
+	if (expected_output != NULL) {
+		k_sleep(K_SECONDS(1));
+	}
 	rv = shell_execute_cmd(get_ec_shell(), cmd);
 
 	zassert_equal(expected_rv, rv,
