5547387e6510ce81707902b5b27bf6fdfed01d2a
Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
2022-10-04 17:23:46 +0800

    crota: modify fan actual rpm log
    BUG=
    BRANCH=none
    TEST=make BOARD=crota
    
    Signed-off-by: Elsie Shih <elsie_shih@wistron.corp-partner.google.com>
    Change-Id: Id3332a705bcd0a274848a86d36848163c3eefc6e

--- board/crota/fans.c
+++ board/crota/fans.c
@@ -106,8 +106,9 @@ static void fan_get_rpm(int fan)
 
 	/* Record actual RPM every 2 minutes. */
 	if (timestamp_expired(deadline, NULL)) {
+		while (timestamp_expired(deadline, NULL))
+			deadline.val += RECORD_TIME;
 		ccprints("fan actual rpm: %d", fan_get_rpm_actual(FAN_CH(fan)));
-		deadline.val += RECORD_TIME;
 	}
 }
 
