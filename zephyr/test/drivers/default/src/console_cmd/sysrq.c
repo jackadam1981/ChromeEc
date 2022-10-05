c596fa8f85ac66ef8ec40567f0711f0e5880f8da
Aaron Massey <aaronmassey@google.com>
2022-10-05 17:41:58 -0600

    test: system.c sysrq console cmd
    Verify the sysrq console command allows a key for be virtually send to
    the host by checking the mkbp_fifo queue.
    
    BRANCH=none
    BUG=b:248106876
    TEST=twister --clobber -i -s zephyr/test/drivers/drivers.default
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: If003850696e3310f2e5e0f98ed8a1b20e88a19f6

---
+++ zephyr/test/drivers/default/src/console_cmd/sysrq.c
@@ -0,0 +1,32 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/ztest.h>
+
+#include "console.h"
+#include "mkbp_fifo.h"
+#include "test/drivers/test_state.h"
+#include "system.h"
+
+ZTEST_USER(console_cmd_sysrq, test_no_args)
+{
+	const struct shell *shell_zephyr = get_ec_shell();
+	/* Size of SYSRQ event */
+	uint32_t out = 0;
+
+	mkbp_clear_fifo();
+
+	/* Send arbitrary key 'q' */
+	zassert_ok(shell_execute_cmd(shell_zephyr, "sysrq q"), NULL);
+
+	int event_size =
+		mkbp_fifo_get_next_event((uint8_t *)&out, EC_MKBP_EVENT_SYSRQ);
+
+	zassert_equal(event_size, sizeof(uint32_t));
+	zassert_equal(out, 'q');
+}
+
+ZTEST_SUITE(console_cmd_sysrq, drivers_predicate_post_main, NULL, NULL, NULL,
+	    NULL);
