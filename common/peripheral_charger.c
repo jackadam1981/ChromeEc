f625f4ce7b1294ac29469405b406dfb3b158370f
Daisuke Nojiri <dnojiri@chromium.org>
2022-10-06 15:27:38 -0700

    PCHG: Add pass-through mode
    In pass-through (passthru in short) mode, EC stops handling PCHG events
    but reports IRQs to the host. This mode allows the host to directly
    communicate with a chip using I2C pass-through, for example.
    
    A host sends EC_CMD_PCHG_UPDATE with EC_PCHG_UPDATE_CMD_ENABLE_PASSTHRU
    sub-command or EC_PCHG_UPDATE_CMD_RESET_TO_NORMAL sub-command to enter
    or exit the mode.
    
    BUG=b:245764044
    BRANCH=None
    TEST=Redrix
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I16fe3f6ccd4a78bf3417fe3c0e86b5428a954acd

--- common/peripheral_charger.c
+++ common/peripheral_charger.c
@@ -62,6 +62,21 @@ struct pchg_policy_t pchg_policy_suspend = {
 	.err_mask = 0,
 };
 
+static const char *_text_mode(enum pchg_mode mode)
+{
+	static const char * const mode_names[] = {
+		[PCHG_MODE_NORMAL] = "NORMAL",
+		[PCHG_MODE_DOWNLOAD] = "DOWNLOAD",
+		[PCHG_MODE_PASSTHRU] = "PASSTHRU",
+	};
+	BUILD_ASSERT(ARRAY_SIZE(mode_names) == PCHG_MODE_COUNT);
+
+	if (mode >= PCHG_MODE_COUNT)
+		return "UNDEF";
+
+	return mode_names[mode];
+}
+
 static const char *_text_event(enum pchg_event event)
 {
 	/* TODO: Use "S%d" for normal build. */
@@ -192,7 +207,10 @@ static enum pchg_state pchg_reset(struct pchg *ctx)
 	 */
 	_clear_port(ctx);
 
-	if (ctx->mode == PCHG_MODE_NORMAL) {
+	if (ctx->mode == PCHG_MODE_DOWNLOAD) {
+		state = PCHG_STATE_DOWNLOAD;
+		pchg_queue_event(ctx, PCHG_EVENT_UPDATE_OPEN);
+	} else if (ctx->mode == PCHG_MODE_NORMAL) {
 		rv = ctx->cfg->drv->init(ctx);
 		if (rv == EC_SUCCESS) {
 			state = PCHG_STATE_INITIALIZED;
@@ -202,9 +220,6 @@ static enum pchg_state pchg_reset(struct pchg *ctx)
 			ctx->error |= PCHG_ERROR_MASK(PCHG_ERROR_COMMUNICATION);
 			CPRINTS("ERR: Failed to reset to normal mode");
 		}
-	} else {
-		state = PCHG_STATE_DOWNLOAD;
-		pchg_queue_event(ctx, PCHG_EVENT_UPDATE_OPEN);
 	}
 
 	return state;
@@ -218,7 +233,8 @@ static void pchg_state_reset(struct pchg *ctx)
 		break;
 	case PCHG_EVENT_IN_NORMAL:
 		ctx->state = PCHG_STATE_INITIALIZED;
-		pchg_queue_event(ctx, PCHG_EVENT_ENABLE);
+		if (ctx->mode == PCHG_MODE_NORMAL)
+			pchg_queue_event(ctx, PCHG_EVENT_ENABLE);
 		break;
 	default:
 		break;
@@ -522,11 +538,17 @@ static int pchg_run(struct pchg *ctx)
 	CPRINTS("P%d Run in STATE_%s for EVENT_%s", port,
 		_text_state(ctx->state), _text_event(ctx->event));
 
+	/*
+	 * IRQ event is further translated to an actual event unless we're
+	 * in passthru mode, where IRQ events will be passed to the host.
+	 */
 	if (ctx->event == PCHG_EVENT_IRQ) {
-		rv = ctx->cfg->drv->get_event(ctx);
-		if (rv) {
-			CPRINTS("ERR: Failed to get event (%d)", rv);
-			return 0;
+		if (ctx->mode != PCHG_MODE_PASSTHRU) {
+			rv = ctx->cfg->drv->get_event(ctx);
+			if (rv) {
+				CPRINTS("ERR: Failed to get event (%d)", rv);
+				return 0;
+			}
 		}
 		CPRINTS("  EVENT_%s", _text_event(ctx->event));
 	}
@@ -713,6 +735,8 @@ static enum ec_status hc_pchg(struct host_cmd_handler_args *args)
 	ctx = &pchgs[port];
 	mutex_lock(&ctx->mtx);
 
+	mutex_lock(&ctx->mtx);
+
 	if (ctx->state == PCHG_STATE_CONNECTED &&
 	    ctx->battery_percent >= ctx->cfg->full_percent)
 		r->state = PCHG_STATE_FULL;
@@ -825,6 +849,23 @@ static enum ec_status hc_pchg_update(struct host_cmd_handler_args *args)
 		ctx->update.crc32 = p->crc32;
 		pchg_queue_event(ctx, PCHG_EVENT_UPDATE_CLOSE);
 		break;
+
+	case EC_PCHG_UPDATE_CMD_RESET:
+		HCPRINTS("Resetting");
+
+		gpio_disable_interrupt(ctx->cfg->irq_pin);
+		_clear_port(ctx);
+		ctx->cfg->drv->reset(ctx);
+		gpio_enable_interrupt(ctx->cfg->irq_pin);
+		break;
+
+	case EC_PCHG_UPDATE_CMD_ENABLE_PASSTHRU:
+		HCPRINTS("Enabling passthru mode");
+		mutex_lock(&ctx->mtx);
+		ctx->mode = PCHG_MODE_PASSTHRU;
+		mutex_unlock(&ctx->mtx);
+		break;
+
 	default:
 		return EC_RES_INVALID_PARAM;
 	}
@@ -853,6 +894,7 @@ static int cc_pchg(int argc, const char **argv)
 		ccprintf("P%d STATE_%s EVENT_%s SOC=%d%%\n", port,
 			 _text_state(ctx->state), _text_event(ctx->event),
 			 ctx->battery_percent);
+		ccprintf("mode=%s\n", _text_mode(ctx->mode));
 		ccprintf("error=0x%x dropped=%u fw_version=0x%x\n", ctx->error,
 			 ctx->dropped_event_count, ctx->fw_version);
 		return EC_SUCCESS;
