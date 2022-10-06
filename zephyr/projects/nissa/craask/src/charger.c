2af0f725c51d8fc49d1e75bf3e209f0052d560d7
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2022-10-06 11:44:08 +0800

    Craask: support thermal charge policy
    The charging limitation:
    While the DUT is enter OS, sample temperature as charger task
    Level |  low T'C    |   high T'C  |  current
          | (Tsens > T) | (Tsens < T) |   (mA)
    ------------------------------------------------
       0  |     0       |     52      |   3000     (Normal charge)
       1  |    48       |     54      |   1500
       2  |    52       |     56      |    800
       3  |    55       |    100      |    800
    
    BUG=b:196997371
    BRANCH=trogdor
    TEST=use console cmd chgstate to check requested_current is as
         expected.
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: I617aa910c227ff653c08db7f6b69cdab72f1164a

--- zephyr/projects/nissa/craask/src/charger.c
+++ zephyr/projects/nissa/craask/src/charger.c
@@ -6,11 +6,15 @@
 #include <zephyr/logging/log.h>
 
 #include "battery.h"
+#include "charge_state_v2.h"
 #include "charger.h"
 #include "charger/isl923x_public.h"
+#include "chipset.h"
 #include "console.h"
 #include "extpower.h"
+#include "temp_sensor/temp_sensor.h"
 #include "usb_pd.h"
+#include "util.h"
 #include "nissa_common.h"
 
 LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
@@ -53,4 +57,63 @@ __override void board_hibernate(void)
 	raa489000_hibernate(CHARGER_PRIMARY, true);
 	LOG_INF("Charger(s) hibernated");
 	cflush();
+}
+
+struct temp_chg_step {
+	int low; /* temp thershold ('C) to lower level */
+	int high; /* temp thershold ('C) to higher level */
+	int current; /* charging limitation (mA) */
+};
+
+static const struct temp_chg_step temp_chg_table[] = {
+	{ .low = 0, .high = 52, .current = 3000 }, /* Lv0: normal charge */
+	{ .low = 48, .high = 54, .current = 1500 },
+	{ .low = 52, .high = 56, .current = 800 },
+	{ .low = 55, .high = 100, .current = 0 },
+};
+#define NUM_TEMP_CHG_LEVELS ARRAY_SIZE(temp_chg_table)
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	static int current_level;
+	int charger_temp, charger_temp_c;
+
+	if (curr->state != ST_CHARGE)
+		return 0;
+
+	/* Charge current control depends on temp if the system is on */
+	if (chipset_in_state(CHIPSET_STATE_ON)) {
+		temp_sensor_read(TEMP_SENSOR_ID(DT_NODELABEL(charger)),
+				 &charger_temp);
+		charger_temp_c = K_TO_C(charger_temp);
+
+		if (charger_temp_c <= temp_chg_table[current_level].low)
+			current_level--;
+		else if (charger_temp_c >= temp_chg_table[current_level].high)
+			current_level++;
+
+		if (current_level < 0)
+			current_level = 0;
+
+		if (current_level >= NUM_TEMP_CHG_LEVELS)
+			current_level = NUM_TEMP_CHG_LEVELS - 1;
+
+		curr->requested_current =
+			MIN(curr->requested_current,
+			    temp_chg_table[current_level].current);
+	}
+
+	return 0;
+}
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
 }
