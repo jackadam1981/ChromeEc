de61956e80f56a440304a12ca323c591e2a0f10c
Yi Chou <yich@google.com>
2022-10-06 17:30:08 +0800

    fpsensor: Reset template_newly_enrolled to none in clear context
    BUG=b:248508087, b:251370748
    TEST=make V=1 BOARD=bloonchipper -j
    
    Change-Id: Ifa05fa66d54930e7040d57eb93957b1c8426a41c

--- common/fpsensor/fpsensor_auth_commands.cc
+++ common/fpsensor/fpsensor_auth_commands.cc
@@ -31,6 +31,7 @@ void fp_clear_context(void)
 {
 	templ_valid = 0;
 	templ_dirty = 0;
+	template_newly_enrolled = FP_NO_SUCH_TEMPLATE;
 	OPENSSL_cleanse(fp_enc_buffer, sizeof(fp_enc_buffer));
 	OPENSSL_cleanse(user_id, sizeof(user_id));
 	OPENSSL_cleanse(auth_nonce.data(), auth_nonce.size());
