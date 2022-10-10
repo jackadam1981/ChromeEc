1ef6b2d99e13449154a874cb1acaaa920a5f7dc5
Patryk Duda <pdk@semihalf.com>
2022-10-10 11:23:29 +0200

    zephyr/program: Introduce bloonchipper project
    Bloonchipper is a Fingerprint MCU project that works with FPC1025
    fingerprint sensors. It runs on a dragonclaw board (STM32F412CG MCU).
    
    This patch contains minimal configuration needed to run the firmware on
    a development board.
    
    BUG=b:239712345
    TEST=zmake build bloonchipper
    LOW_COVERAGE_REASON=Function prepare_for_sysjump_to_ec() contains such
                        things as timer and uart peripheral reset, MPU
    		    and system clock disabling. Function is called on
    		    HOOK_SYSJUMP. It would be really hard to test it if
    		    not impossible.
    
    Change-Id: I8962108d4114268ce7a248c7e0ea7dc6f32d62ad

---
+++ zephyr/program/fpmcu/BUILD.py
@@ -0,0 +1,38 @@
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Define zmake projects for FPMCUs."""
+
+
+def register_fpmcu_variant(
+    project_name,
+    zephyr_board,
+    variant_modules=(),
+    variant_dts_overlays=(),
+    variant_kconfig_files=(),
+):
+    """Register FPMCU project"""
+    return register_binman_project(
+        project_name=project_name,
+        zephyr_board=zephyr_board,
+        modules=["ec", *variant_modules],
+        supported_toolchains=["coreboot-sdk"],
+        dts_overlays=[*variant_dts_overlays],
+        kconfig_files=[here / "prj.conf", *variant_kconfig_files],
+    )
+
+
+bloonchipper = register_fpmcu_variant(
+    project_name="bloonchipper",
+    zephyr_board="google_dragonclaw",
+    variant_modules=["hal_stm32", "cmsis"],
+    variant_dts_overlays=[
+        here / "bloonchipper" / "bloonchipper.dts",
+        here / "bloonchipper" / "rdp1_quirk.dts",
+    ],
+    variant_kconfig_files=[
+        here / "bloonchipper" / "prj.conf",
+        here / "bloonchipper" / "ec_quirks.conf",
+    ],
+)
