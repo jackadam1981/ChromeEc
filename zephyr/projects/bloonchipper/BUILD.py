1714de76e9c5421f0ade1cca67be3fefc11da5a9
Patryk Duda <pdk@semihalf.com>
2022-10-10 11:23:29 +0200

    Zephyr bloonchipper
    Change-Id: I8962108d4114268ce7a248c7e0ea7dc6f32d62ad

---
+++ zephyr/projects/bloonchipper/BUILD.py
@@ -0,0 +1,16 @@
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Define zmake projects for bloonchipper."""
+
+register_binman_project(
+    project_name="bloonchipper",
+    zephyr_board="google_dragonclaw",
+    supported_toolchains=["coreboot-sdk"],
+    dts_overlays=[
+        "binman.dts",
+        "gpio.dts",
+        "soc.dts",
+    ],
+)
