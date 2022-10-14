48b9e10552f3d559507dee49edc11783ce47ac37
jimmy.wu <jimmy.wu@lcfc.corp-partner.google.com>
2022-10-14 18:09:25 +0800

    RAA48900 : Set trickle charge current to maximum 256mA
    Change requested by power team.
    Set trickle charge current to maximum 256mA.
    
    BUG=b:253154031
    BRANCH=none
    TEST=Power team verified get possitive result.
    
    Signed-off-by: jimmy.wu <jimmy.wu@lcfc.corp-partner.google.com>
    Change-Id: I5a3b4aa8e42d8516cfb16e25863eb9d5df2c65f4

--- include/config.h
+++ include/config.h
@@ -1332,6 +1332,9 @@
 /* Enable trickle charging */
 #undef CONFIG_TRICKLE_CHARGING
 
+/* Set trickle charge current to 256mA */
+#undef CONFIG_CHARGER_TRICKLE_256mA
+
 /* Wireless chargers */
 #undef CONFIG_CPS8100
 
