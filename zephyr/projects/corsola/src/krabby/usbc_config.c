2eba9205a4cb4f41f3e607157ab9ef6a17e4e330
lschyi <lschyi@google.com>
2022-10-17 14:26:19 +0800

    zephyr/test: add krabby usb mux board init test
    Add testing for checking the krabby project usb mux init. Enable USBC
    and other related functionality in krabby test config, and update the
    it8xxx2 emul configuration.
    
    BUG=b:229340646
    TEST=./twister -p native_posix -p unit_testing
    BRANCH=none
    
    Signed-off-by: lschyi <lschyi@google.com>
    Change-Id: I981e89eeecd58b78e6a256fe154611f2a2a50ead

--- zephyr/projects/corsola/src/krabby/usbc_config.c
+++ zephyr/projects/corsola/src/krabby/usbc_config.c
@@ -5,7 +5,9 @@
 
 /* Krabby board-specific USB-C configuration */
 
+#ifdef CONFIG_USB_PD_VBUS_MEASURE_ADC_EACH_PORT
 #include "adc.h"
+#endif /* CONFIG_USB_PD_VBUS_MEASURE_ADC_EACH_PORT */
 #include "baseboard_usbc_config.h"
 #include "charge_manager.h"
 #include "console.h"
@@ -36,6 +38,7 @@ int tusb1064_mux_1_board_init(const struct usb_mux *me)
 			   MASK_SET);
 }
 
+#ifdef CONFIG_USB_PD_TCPM_ITE_ON_CHIP
 const struct cc_para_t *board_get_cc_tuning_parameter(enum usbpd_port port)
 {
 	const static struct cc_para_t
@@ -56,6 +59,7 @@ const struct cc_para_t *board_get_cc_tuning_parameter(enum usbpd_port port)
 
 	return &cc_parameter[port];
 }
+#endif
 
 void board_reset_pd_mcu(void)
 {
@@ -65,6 +69,7 @@ void board_reset_pd_mcu(void)
 	 */
 }
 
+#ifndef CONFIG_TEST
 int board_set_active_charge_port(int port)
 {
 	int i;
@@ -121,6 +126,7 @@ int board_set_active_charge_port(int port)
 
 	return EC_SUCCESS;
 }
+#endif
 
 #ifdef CONFIG_USB_PD_VBUS_MEASURE_ADC_EACH_PORT
 enum adc_channel board_get_vbus_adc(int port)
