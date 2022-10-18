c1bd5bb32256ecdf39f9ef689eee712845b0c92b
Daisuke Nojiri <dnojiri@chromium.org>
2022-10-18 16:42:38 -0700

    PCGH: Print error name
    This patch makes PCHG print error names when they are reported.
    
    BUG=b:245764044
    BRANCH=None
    TEST=Redrix
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I077161168e068bb62684008e3b613b241229efa0

--- driver/nfc/ctn730.c
+++ driver/nfc/ctn730.c
@@ -446,15 +446,6 @@ static int _process_payload_event(struct pchg *ctx, struct ctn730_msg *res)
 				return EC_ERROR_INVAL;
 			ctx->event = PCHG_EVENT_DEVICE_LOST;
 			break;
-<<<<<<< Updated upstream
-=======
-		case WLC_CHG_CTRL_DEVICE_STATE_DEVICE_FO_PRESENT:
-			if (len != WLC_CHG_CTRL_DEVICE_STATE_EVT_SIZE)
-				return EC_ERROR_INVAL;
-			ctx->event = PCHG_EVENT_ERROR;
-			ctx->error |= PCHG_ERROR_MASK(PCHG_ERROR_FOREIGN_OBJECT);
-			break;
->>>>>>> Stashed changes
 		default:
 			return EC_ERROR_INVAL;
 		}
