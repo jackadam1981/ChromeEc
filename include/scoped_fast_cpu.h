002b9bddfc4e3ce176aaed65c5ebbd937b9703bf
Yi Chou <yich@google.com>
2022-10-19 12:23:34 +0800

    fpsensor: Boost the CPU when needed
    We should boost the CPU for faster crypto operations.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I533f8c487a5f7485c0f775b8228073623d563d4e

---
+++ include/scoped_fast_cpu.h
@@ -0,0 +1,30 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* A class to scope the range of boosting CPU. */
+
+#ifndef __CROS_EC_SCOPED_FAST_CPU_H
+#define __CROS_EC_SCOPED_FAST_CPU_H
+
+#include "clock.h"
+
+struct ScopedFastCpu {
+	ScopedFastCpu()
+		: previous_state_(current_state_)
+	{
+		clock_enable_module(MODULE_FAST_CPU, 1);
+		current_state_ = 1;
+	}
+	~ScopedFastCpu()
+	{
+		clock_enable_module(MODULE_FAST_CPU, previous_state_);
+		current_state_ = previous_state_;
+	}
+
+	int previous_state_;
+	static inline int current_state_;
+};
+
+#endif /* __CROS_EC_SCOPED_FAST_CPU_H */
