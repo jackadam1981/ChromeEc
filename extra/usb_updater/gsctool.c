4486294b494f7faf63533579a0cb856d6cc3384b
Matt Vertescher <mvertescher@google.com>
2022-10-19 11:24:45 -0700

    gsctool: Add AP RO verify SPI config mode command
    This patch adds the ability to get and set the AP RO verification
    SPI mode configuration setting from `gsctool` by adding a new `-C`
    command flag with an optional argument.
    
    BUG=b:250972056
    TEST=`gsctool -D -C`
    
    Change-Id: I2ad4af65615310cf1477adfda7df1bfdf0e0a914

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -324,6 +324,29 @@ static void sha_final_into_block_digest(union sha_ctx *ctx, void *block_digest,
 					size_t size);
 
 /*
+ * Current AP RO verification config setting version
+ */
+#define AP_RO_VERIFY_CONFIG_SETTING_CURRENT_VERSION 0x01
+
+/*
+ * AP RO verification config setting command choices
+ */
+enum ap_ro_verify_config_setting_command_e {
+	ap_ro_verify_config_setting_command_write_protect_descriptors = 0,
+	ap_ro_verify_config_setting_command_spi_addressing_mode = 1,
+};
+
+/*
+ * AP RO verification SPI mode configuration choices
+ */
+enum ap_ro_verify_config_spi_mode_choice_e {
+	ap_ro_verify_config_spi_mode_choice_none = 0,
+	ap_ro_verify_config_spi_mode_choice_get = 1,
+	ap_ro_verify_config_spi_mode_choice_set_3byte = 2,
+	ap_ro_verify_config_spi_mode_choice_set_4byte = 3,
+};
+
+/*
  * This by far exceeds the largest vendor command response size we ever
  * expect.
  */
@@ -382,6 +405,8 @@ static const struct option_container cmd_line_options[] = {
 	{{"binvers", no_argument, NULL, 'b'},
 	 "Report versions of Cr50 image's "
 	 "RW and RO headers, do not update"},
+	{{"apro_config_spi_mode", optional_argument, NULL, 'C'},
+	 "Get/set the ap ro verify spi mode either to `3byte` or `4byte`"},
 	{{"corrupt", no_argument, NULL, 'c'},
 	 "Corrupt the inactive rw"},
 	{{"dauntless", no_argument, NULL, 'D'},
@@ -2596,6 +2621,72 @@ static int process_get_apro_boot_status(struct transfer_descriptor *td)
 	return 0;
 }
 
+static int process_ap_ro_config_spi_mode(struct transfer_descriptor *td, int ap_ro_verify_config_spi_mode)
+{
+	enum ap_ro_config_spi_mode_e {
+		ap_ro_spi_config_3byte = 0,
+		ap_ro_spi_config_4byte = 1,
+	};
+
+	struct __attribute__((__packed__)) ap_ro_config_spi_mode_msg {
+		uint8_t version;
+		uint8_t command;
+		uint8_t mode;
+	};
+
+	struct ap_ro_config_spi_mode_msg msg = { 
+		.version = AP_RO_VERIFY_CONFIG_SETTING_CURRENT_VERSION,
+		.command = ap_ro_verify_config_setting_command_spi_addressing_mode,
+		.mode = ap_ro_spi_config_4byte
+	};
+	size_t response_size = sizeof(msg);
+	int rv = 0;
+
+	switch (ap_ro_verify_config_spi_mode) {
+		case ap_ro_verify_config_spi_mode_choice_get:
+			rv = send_vendor_command(td, VENDOR_CC_GET_AP_RO_VERIFY_SETTING,
+				&msg, sizeof(msg), &msg, &response_size);
+			if (rv != VENDOR_RC_SUCCESS) {
+				fprintf(stderr, "Error %d getting ap ro spi addr mode\n", rv);
+				return update_error;
+			}
+
+			if (response_size != 3) {
+				fprintf(stderr, "Error getting ap ro spi addr mode response\n");
+				return update_error;
+			}
+
+			switch (msg.mode) {
+				case ap_ro_spi_config_3byte:
+					fprintf(stderr, "3byte\n");
+					break;
+				case ap_ro_spi_config_4byte:
+					fprintf(stderr, "4byte\n");
+					break;
+				default:
+					fprintf(stderr, "unknown spi mode\n");
+					return update_error;
+			}
+
+			break;
+		case ap_ro_verify_config_spi_mode_choice_set_3byte:
+			msg.mode = ap_ro_spi_config_3byte;
+			// Fallthrough
+		case ap_ro_verify_config_spi_mode_choice_set_4byte:
+			rv = send_vendor_command(td, VENDOR_CC_SET_AP_RO_VERIFY_SETTING,
+				&msg, sizeof(msg), &msg, &response_size);
+			if (rv != VENDOR_RC_SUCCESS) {
+				fprintf(stderr, "Error %d setting ap ro spi addr mode\n", rv);
+				return update_error;
+			}
+			break;
+		default:
+			return update_error;
+	}
+
+	return 0;
+}
+
 static int process_get_boot_mode(struct transfer_descriptor *td)
 {
 	size_t response_size;
@@ -3365,6 +3456,8 @@ int main(int argc, char *argv[])
 	bool show_machine_output = false;
 	int tstamp = 0;
 	const char *tstamp_arg = NULL;
+	enum ap_ro_verify_config_spi_mode_choice_e ap_ro_verify_config_spi_mode =
+		ap_ro_verify_config_spi_mode_choice_none;
 
 	const char *exclusive_opt_error =
 		"Options -a, -s and -t are mutually exclusive\n";
@@ -3446,6 +3539,14 @@ int main(int argc, char *argv[])
 				start_apro_verify = 1;
 			else
 				get_apro_boot_status = 1;
+			break;
+		case 'C':
+			if (optarg && !strcmp(optarg, "3byte"))
+				ap_ro_verify_config_spi_mode = ap_ro_verify_config_spi_mode_choice_set_3byte;
+			else if (optarg && !strcmp(optarg, "4byte"))
+				ap_ro_verify_config_spi_mode = ap_ro_verify_config_spi_mode_choice_set_4byte;
+			else
+				ap_ro_verify_config_spi_mode = ap_ro_verify_config_spi_mode_choice_get;
 			break;
 		case 'd':
 			if (!parse_vidpid(optarg, &vid, &pid)) {
@@ -3588,6 +3689,7 @@ int main(int argc, char *argv[])
 	image_magic = is_dauntless ? MAGIC_DAUNTLESS : MAGIC_HAVEN;
 
 	if ((bid_action == bid_none) &&
+	    !ap_ro_verify_config_spi_mode &&
 	    !ccd_info &&
 	    !ccd_lock &&
 	    !ccd_open &&
@@ -3731,6 +3833,9 @@ int main(int argc, char *argv[])
 
 	if (erase_ap_ro_hash)
 		process_erase_ap_ro_hash(&td);
+
+	if (ap_ro_verify_config_spi_mode)
+		exit(process_ap_ro_config_spi_mode(&td, ap_ro_verify_config_spi_mode));
 
 	if (data || show_fw_ver) {
 
