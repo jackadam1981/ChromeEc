4c5f24dc96abad489e85f32341e889aea12605dc
Diana Z <dzigterman@chromium.org>
2022-10-20 15:57:21 -0600

    Test: Add test for VDM REQ command
    Add a test to exercise the parameter checking and message send from the
    VDM REQ command.
    
    BRANCH=None
    BUG=b:208884535
    TEST=./twister -T ./zephyr/test
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: I1cb1d2dbc8d04b7826b4981e8120b44f11b697f6

--- zephyr/test/drivers/ap_vdm_control/src/ap_vdm_control.c
+++ zephyr/test/drivers/ap_vdm_control/src/ap_vdm_control.c
@@ -3,6 +3,9 @@
  * found in the LICENSE file.
  */
 
+#include <stdint.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/byteorder.h>
 #include <zephyr/ztest.h>
 
 #include "ec_commands.h"
@@ -11,20 +14,168 @@
 #include "test/drivers/test_state.h"
 #include "test/drivers/utils.h"
 
-static void ap_vdm_control_before(void *data)
+#define TEST_PORT USBC_PORT_C0
+
+struct ap_vdm_control_fixture {
+	const struct emul *tcpci_emul;
+	const struct emul *charger_emul;
+	struct tcpci_partner_data partner;
+	struct tcpci_src_emul_data src_ext;
+};
+
+static void verify_vdm_req(struct ap_vdm_control_fixture *fixture,
+			   struct typec_vdm_req *req)
 {
-	/* Set chipset on so the "AP" is on to give us commands */
-	test_set_chipset_to_s0();
+	struct tcpci_partner_log_msg *msg;
+	bool message_seen = 0;
+
+	SYS_SLIST_FOR_EACH_CONTAINER(&fixture->partner.msg_log, msg, node)
+	{
+		uint16_t header = sys_get_le16(msg->buf);
+
+		/* Ignore messages from ourselves */
+		if (msg->sender == TCPCI_PARTNER_SENDER_PARTNER) {
+			continue;
+		}
+
+		/*
+		 * Control messages, non-VDMs, and extended messages are not of
+		 * interest
+		 */
+		if ((PD_HEADER_CNT(header) == 0) ||
+		    (PD_HEADER_TYPE(header) != PD_DATA_VENDOR_DEF) ||
+		    (PD_HEADER_EXT(header) != 0)) {
+			continue;
+		}
+
+		/* We have a VDM, check entry we're interested in */
+		if (memcmp(msg->buf, req->vdm_message,
+			   req->vdm_message_count * sizeof(uint32_t))) {
+			message_seen = true;
+			break;
+		}
+	}
+
+	zassert_true(message_seen, "Expected message not found");
 }
 
-ZTEST_SUITE(ap_vdm_control, drivers_predicate_post_main, NULL,
-	    ap_vdm_control_before, NULL, NULL);
+static void *ap_vdm_control_setup(void)
+{
+	static struct ap_vdm_control_fixture fixture;
+	struct tcpci_partner_data *partner = &fixture.partner;
+	struct tcpci_src_emul_data *src_ext = &fixture.src_ext;
 
-ZTEST(ap_vdm_control, test_feature_present)
+	tcpci_partner_init(partner, PD_REV30);
+	partner->extensions = tcpci_src_emul_init(src_ext, partner, NULL);
+
+	/* Get references for the emulators */
+	fixture.tcpci_emul = EMUL_DT_GET(DT_NODELABEL(tcpci_emul));
+	fixture.charger_emul = EMUL_DT_GET(DT_NODELABEL(isl923x_emul));
+
+	return &fixture;
+}
+
+static void ap_vdm_control_before(void *data)
+{
+	struct ap_vdm_control_fixture *fix = data;
+
+	/* Set chipset on so the "AP" is on to give us commands */
+	test_set_chipset_to_s0();
+
+	/* Connect our port partner */
+	connect_source_to_port(&fix->partner, &fix->src_ext, 0, fix->tcpci_emul,
+			       fix->charger_emul);
+}
+
+static void ap_vdm_control_after(void *data)
+{
+	struct ap_vdm_control_fixture *fix = data;
+
+	disconnect_source_from_port(fix->tcpci_emul, fix->charger_emul);
+	tcpci_partner_common_clear_logged_msgs(&fix->partner);
+}
+
+ZTEST_SUITE(ap_vdm_control, drivers_predicate_post_main, ap_vdm_control_setup,
+	    ap_vdm_control_before, ap_vdm_control_after, NULL);
+
+ZTEST_F(ap_vdm_control, test_feature_present)
 {
 	struct ec_response_get_features feat = host_cmd_get_features();
 
 	zassert_true(feat.flags[1] &
 			     EC_FEATURE_MASK_1(EC_FEATURE_TYPEC_AP_VDM_SEND),
 		     "Failed to see feature present");
+}
+
+ZTEST_F(ap_vdm_control, test_send_vdm_req_bad_port)
+{
+	struct ec_params_typec_control params = {
+		.port = 85,
+		.command = TYPEC_CONTROL_COMMAND_SEND_VDM_REQ,
+		.vdm_req_params = {
+			.vdm_message = { 0 },
+			.vdm_message_count = 1,
+			.partner_type = TYPEC_PARTNER_SOP,
+		},
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_TYPEC_CONTROL, 0, params);
+
+	zassert_equal(host_command_process(&args), EC_RES_INVALID_PARAM,
+		      "Failed to see port error");
+}
+
+ZTEST_F(ap_vdm_control, test_send_vdm_req_bad_type)
+{
+	struct ec_params_typec_control params = {
+		.port = TEST_PORT,
+		.command = TYPEC_CONTROL_COMMAND_SEND_VDM_REQ,
+		.vdm_req_params = {
+			.vdm_message = { 0 },
+			.vdm_message_count = 1,
+			.partner_type = TYPEC_PARTNER_SOP_PRIME_PRIME + 1,
+		},
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_TYPEC_CONTROL, 0, params);
+
+	zassert_equal(host_command_process(&args), EC_RES_INVALID_PARAM,
+		      "Failed to see port error");
+}
+
+ZTEST_F(ap_vdm_control, test_send_vdm_req_bad_count)
+{
+	struct ec_params_typec_control params = {
+		.port = TEST_PORT,
+		.command = TYPEC_CONTROL_COMMAND_SEND_VDM_REQ,
+		.vdm_req_params = {
+			.vdm_message = { 0 },
+			.vdm_message_count = 0,
+			.partner_type = TYPEC_PARTNER_SOP,
+		},
+	};
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_PARAMS(EC_CMD_TYPEC_CONTROL, 0, params);
+
+	zassert_equal(host_command_process(&args), EC_RES_INVALID_PARAM,
+		      "Failed to see port error");
+}
+
+ZTEST_F(ap_vdm_control, test_send_vdm_req_valid)
+{
+	uint32_t vdm_req_header = VDO(USB_SID_DISPLAYPORT, 1, CMD_ENTER_MODE);
+	struct typec_vdm_req req = {
+		.vdm_message = { vdm_req_header },
+		.vdm_message_count = 1,
+		.partner_type = TYPEC_PARTNER_SOP,
+	};
+
+	tcpci_partner_common_enable_pd_logging(&fixture->partner, true);
+	host_cmd_typec_control_vdm_req(TEST_PORT, req);
+	k_sleep(K_SECONDS(1));
+
+	tcpci_partner_common_enable_pd_logging(&fixture->partner, false);
+
+	/* Look for our REQ */
+	verify_vdm_req(fixture, &req);
 }
