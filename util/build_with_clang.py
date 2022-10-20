e0a0933bfb548a2cfc1bd5425a40e4f791b04b7e
Tom Hughes <tomhughes@chromium.org>
2022-10-20 15:42:44 -0700

    include/atomic_bit: Provide __atomic_load_4 when missing from builtins
    clang does not have __atomic_load_4 for ARMv6-M, so provide an
    implementation until it is fixed:
    https://github.com/llvm/llvm-project/issues/58603.
    
    BRANCH=none
    BUG=b:254710459
    TEST=./util/compare_build.sh -b all -j 120
         => MATCH
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I841c582efff1bf040d086ac4339c68d7f09695b6

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -49,6 +49,7 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "burnet",
     "c2d2",
     "cerise",
+	"chocodile_vpdmcu",
     "coffeecake",
     "damu",
     "dingdong",
@@ -301,8 +302,6 @@ RISCV_BOARDS = [
 ]
 
 BOARDS_THAT_FAIL_WITH_CLANG = [
-    # Boards that use CHIP:=stm32 *and* CHIP_FAMILY:=stm32f0
-    "chocodile_vpdmcu",  # compilation error: b/254710459
     # Boards that use CHIP:=npcx
     "bobba",  # overflows flash
     "coachz",  # overflows flash
