addf5d75e8162656bde7028166f631430278a061
Tom Hughes <tomhughes@chromium.org>
2022-10-20 15:42:44 -0700

    DO NOT SUBMIT: Work around clang bug
    This is NOT a valid implementation; just a test to get past the
    compilation error to see if there are others.
    
    BRANCH=none
    BUG=b:254710459
    TEST=none
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I841c582efff1bf040d086ac4339c68d7f09695b6

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -44,6 +44,7 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     # git grep  --name-only 'CHIP:=stm32' | xargs grep -L 'CHIP_FAMILY:=stm32f0' | sed 's#board/\(.*\)/build.mk#"\1",#'
     "bland",
     "c2d2",
+    "chocodile_vpdmcu",
     "coffeecake",
     "dingdong",
     "discovery-stm32f072",
