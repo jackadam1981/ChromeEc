dcf5e20c1308636ff1d912cf02ff84cc83811a32
Tom Hughes <tomhughes@chromium.org>
2022-10-20 15:42:44 -0700

    include/atomic_bit: Provide __atomic_load_4 when missing from builtins
    clang does not have __atomic_load_4 for ARMv6-M, so provide an
    implementation until it is fixed:
    https://github.com/llvm/llvm-project/issues/58603.
    
    BRANCH=none
    BUG=b:254710459
    TEST=./util/compare_build.sh -b all -j 120
         => MATCH
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I841c582efff1bf040d086ac4339c68d7f09695b6

--- include/atomic_bit.h
+++ include/atomic_bit.h
@@ -8,11 +8,39 @@
 
 #ifndef CONFIG_ZEPHYR
 #include "atomic.h"
+#include "compiler.h"
 
+/*
+ * TODO(b/254710459): clang does not have __atomic_load_4 for ARMv6-M, so
+ * provide an implementation until it is fixed:
+ * https://github.com/llvm/llvm-project/issues/58603.
+ */
+#if !__has_builtin(__atomic_load_4)
+static atomic_val_t __atomic_load_4(const atomic_t *target, int memorder)
+{
+	atomic_val_t ret;
+
+	asm volatile("dmb ish\n"
+		     "ldr  %0, [%1]\n"
+		     "dmb ish\n"
+		     : "=r"(ret)
+		     : "r"(target));
+
+	return ret;
+}
+
+static inline atomic_val_t atomic_get(const atomic_t *target)
+{
+	DISABLE_CLANG_WARNING("-Watomic-alignment");
+	return __atomic_load_n(target, __ATOMIC_SEQ_CST);
+	ENABLE_CLANG_WARNING("-Watomic-alignment");
+}
+#else
 static inline atomic_val_t atomic_get(const atomic_t *target)
 {
 	return __atomic_load_n(target, __ATOMIC_SEQ_CST);
 }
+#endif
 
 #include "third_party/zephyr/atomic.h"
 
