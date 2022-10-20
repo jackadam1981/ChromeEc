bba651e53af20466a2022f6d710ab0fb3f1463e8
Abe Levkoy <alevkoy@chromium.org>
2022-10-20 12:31:54 -0600

    TCPM: Add augmented PDO constructor
    Add a macro to construct Augmented PDOs, analogous to the existing
    macros for fixed, battery, and variable PDOs.
    
    BUG=b:236074634
    TEST=make buidall
    BRANCH=none
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: I7c8428035ee97c2899d5e9a2ef6b0e4142bf06a7

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -124,6 +124,10 @@ enum pd_rx_errors {
 	(PDO_BATT_MIN_VOLT(min_mv) | PDO_BATT_MAX_VOLT(max_mv) | \
 	 PDO_BATT_OP_POWER(op_mw) | PDO_TYPE_BATTERY)
 
+#define PDO_AUG(min_mv, max_mv, max_ma)                              \
+	(PDO_AUG_MIN_VOLTAGE(min_mv) | PDO_AUG_MAX_VOLTAGE(max_mv) | \
+	 PDO_AUG_MAX_CURRENT(max_ma) | PDO_TYPE_AUGMENTED)
+
 /* RDO : Request Data Object */
 #define RDO_OBJ_POS(n) (((n)&0x7) << 28)
 #define RDO_POS(rdo) (((rdo) >> 28) & 0x7)
