23e21404842b8181ad918e8a94f9db90382e7b35
Devin Lu <Devin.Lu@quantatw.com>
2022-10-20 19:13:19 +0800

    vell: Change SEQ_EC_ALL_SYS_PG for Board ID 4
    Change GPIO_SEQ_EC_ALL_SYS_PG from GPIOD7 to GPIO97 for board ID 4
    
    BUG=b:253957352
    BRANCH=none
    TEST=On re-work Vell. System boots.
    Shorting the SEQ_EC_ALL_SYS_PG to ground is able to see the system
    shutting down.
    
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
    Change-Id: I44a0c58efad532f0a09c3d16ed20788c7d5895ea

--- board/vell/board.c
+++ board/vell/board.c
@@ -20,6 +20,8 @@
 #include "panic.h"
 #include "power_button.h"
 #include "power.h"
+#include "power/alderlake_slg4bd44540.h"
+#include "power/intel_x86.h"
 #include "registers.h"
 #include "switch.h"
 #include "throttle_ap.h"
@@ -30,6 +32,51 @@
 /* Console output macros */
 #define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ##args)
 #define CPRINTS(format, args...) cprints(CC_CHARGER, format, ##args)
+
+struct power_signal_info power_signal_list[] = {
+	[X86_SLP_S0_DEASSERTED] = {
+		.gpio = GPIO_PCH_SLP_S0_L,
+		.flags = POWER_SIGNAL_ACTIVE_HIGH |
+			POWER_SIGNAL_DISABLE_AT_BOOT,
+		.name = "SLP_S0_DEASSERTED",
+	},
+	[X86_SLP_S3_DEASSERTED] = {
+		.gpio = SLP_S3_SIGNAL_L,
+		.flags = POWER_SIGNAL_ACTIVE_HIGH,
+		.name = "SLP_S3_DEASSERTED",
+	},
+	[X86_SLP_S4_DEASSERTED] = {
+		.gpio = (enum gpio_signal)SLP_S4_SIGNAL_L,
+		.flags = POWER_SIGNAL_ACTIVE_HIGH,
+		.name = "SLP_S4_DEASSERTED",
+	},
+	[X86_SLP_S5_DEASSERTED] = {
+		.gpio = (enum gpio_signal)SLP_S5_SIGNAL_L,
+		.flags = POWER_SIGNAL_ACTIVE_HIGH,
+		.name = "SLP_S5_DEASSERTED",
+	},
+	[X86_SLP_SUS_DEASSERTED] = {
+		.gpio = GPIO_SLP_SUS_L,
+		.flags = POWER_SIGNAL_ACTIVE_HIGH,
+		.name = "SLP_SUS_DEASSERTED",
+	},
+	[X86_RSMRST_L_PGOOD] = {
+		.gpio = GPIO_PG_EC_RSMRST_ODL,
+		.flags = POWER_SIGNAL_ACTIVE_HIGH,
+		.name = "RSMRST_L_PGOOD",
+	},
+	[X86_DSW_DPWROK] = {
+		.gpio = GPIO_PG_EC_DSW_PWROK,
+		.flags = POWER_SIGNAL_ACTIVE_HIGH,
+		.name = "DSW_DPWROK",
+	},
+	[X86_ALL_SYS_PGOOD] = {
+		.gpio = GPIO_SEQ_EC_ALL_SYS_PG_NEW,
+		.flags = POWER_SIGNAL_ACTIVE_HIGH,
+		.name = "ALL_SYS_PWRGD",
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
 
 static void board_chipset_startup(void)
 {
@@ -46,3 +93,20 @@ static void board_chipset_shutdown(void)
 	gpio_set_level(GPIO_EC_KB_BL_EN, 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown, HOOK_PRIO_DEFAULT);
+
+static void set_board_id_4_gpios(void)
+{
+	if (get_board_id() > 3) {
+		power_signal_list[X86_ALL_SYS_PGOOD].gpio = GPIO_SEQ_EC_ALL_SYS_PG_NEW;
+		gpio_enable_interrupt(GPIO_SEQ_EC_ALL_SYS_PG_NEW);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, set_board_id_4_gpios, HOOK_PRIO_FIRST);
+
+__override int intel_x86_get_pg_ec_all_sys_pwrgd(void)
+{
+	if (get_board_id() > 3)
+		return gpio_get_level(GPIO_SEQ_EC_ALL_SYS_PG_NEW);
+	else
+		return gpio_get_level(GPIO_SEQ_EC_ALL_SYS_PG_OLD);
+}
