feb9455e839d36b8e684223ddb6acca4f23e72dc
Eric Yilun Lin <yllin@chromium.org>
2022-10-20 11:21:51 +0800

    corsola: set HDMI HPD in interrupt handler
    HPD pulse has to toggle with 0.5ms ~ 1ms. Previously,
    this was toggled in the hook, which will be hard to
    compliant to the requirement. This CL moves the HPD
    set function from hook to the interrupt handler to
    reduce the delay.
    
    Also, fixed the bugs when HDMI is plugged on boot and
    sometimes won't be detected. This is due to the usb_mux_set
    function will be skipped before we intentionally makes the
    USB_PD_PORT_COUNT to 2 after task inited. We fix this by
    re-configure mux setting after tasks inited.
    
    BUG=b:252999628
    TEST=1. it6505 is able to capture the HPD interrupt when unplugged
         2. HDMI port is able to output when plugged on boot
    BRANCH=none
    
    Change-Id: I6347033e2a7996a8f3a972e9f6b3feb328d4f8c7
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/projects/corsola/src/usbc_config.c
+++ zephyr/projects/corsola/src/usbc_config.c
@@ -131,28 +131,19 @@ void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled)
 /**
  * Handle PS185 HPD changing state.
  */
-int debounced_hpd;
-
-static void ps185_hdmi_hpd_deferred(void)
+static void ps185_hdmi_hpd_set(void)
 {
-	const int new_hpd =
+	const int hpd =
 		gpio_pin_get_dt(GPIO_DT_FROM_ALIAS(gpio_ps185_ec_dp_hpd));
 
-	/* HPD status not changed, probably a glitch, just return. */
-	if (debounced_hpd == new_hpd) {
-		return;
-	}
-
-	debounced_hpd = new_hpd;
-
 	if (!corsola_is_dp_muxable(USBC_PORT_C1)) {
-		if (debounced_hpd) {
+		if (hpd) {
 			CPRINTS("C0 port is already muxed.");
 		}
 		return;
 	}
 
-	if (debounced_hpd) {
+	if (hpd && !(usb_mux_get(USBC_PORT_C1) & USB_PD_MUX_DP_ENABLED)) {
 		dp_status[USBC_PORT_C1] =
 			VDO_DP_STATUS(0, /* HPD IRQ  ... not applicable */
 				      0, /* HPD level ... not applicable */
@@ -167,21 +158,18 @@ static void ps185_hdmi_hpd_deferred(void)
 			    USB_SWITCH_DISCONNECT,
 			    0 /* polarity, don't care */);
 
-		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(dp_aux_path_sel),
-				debounced_hpd);
-		CPRINTS("Set DP_AUX_PATH_SEL: %d", 1);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(dp_aux_path_sel), hpd);
+		CPRINTS("HDMI plug");
 	}
-	svdm_set_hpd_gpio(USBC_PORT_C1, debounced_hpd);
-	CPRINTS(debounced_hpd ? "HDMI plug" : "HDMI unplug");
+	svdm_set_hpd_gpio(USBC_PORT_C1, hpd);
 }
-DECLARE_DEFERRED(ps185_hdmi_hpd_deferred);
 
 static void ps185_hdmi_hpd_disconnect_deferred(void)
 {
-	const int new_hpd =
+	const int hpd =
 		gpio_pin_get_dt(GPIO_DT_FROM_ALIAS(gpio_ps185_ec_dp_hpd));
 
-	if (debounced_hpd == new_hpd && !new_hpd) {
+	if (!hpd && (usb_mux_get(USBC_PORT_C1) & USB_PD_MUX_DP_ENABLED)) {
 		dp_status[USBC_PORT_C1] =
 			VDO_DP_STATUS(0, /* HPD IRQ  ... not applicable */
 				      0, /* HPD level ... not applicable */
@@ -194,16 +182,16 @@ static void ps185_hdmi_hpd_disconnect_deferred(void)
 		usb_mux_set(USBC_PORT_C1, USB_PD_MUX_NONE,
 			    USB_SWITCH_DISCONNECT,
 			    0 /* polarity, don't care */);
+		CPRINTS("HDMI unplug");
 	}
 }
 DECLARE_DEFERRED(ps185_hdmi_hpd_disconnect_deferred);
 
-#define PS185_HPD_DEBOUCE 250
 #define HPD_SINK_ABSENCE_DEBOUNCE (2 * MSEC)
 
 static void hdmi_hpd_interrupt(enum gpio_signal signal)
 {
-	hook_call_deferred(&ps185_hdmi_hpd_deferred_data, PS185_HPD_DEBOUCE);
+	ps185_hdmi_hpd_set();
 
 	if (!gpio_pin_get_dt(GPIO_DT_FROM_ALIAS(gpio_ps185_ec_dp_hpd))) {
 		hook_call_deferred(&ps185_hdmi_hpd_disconnect_deferred_data,
@@ -253,6 +241,13 @@ static void board_hdmi_handler(struct ap_power_ev_callback *cb,
 static void tasks_init_deferred(void)
 {
 	tasks_inited = true;
+	if (corsola_get_db_type() == CORSOLA_DB_HDMI) {
+		/* If the HDMI port is plugged on-boot, and the usb_mux won't
+		 * be configured before the task inited.  Re-invoke the
+		 * HPD configuration after task inited.
+		 */
+		ps185_hdmi_hpd_set();
+	}
 }
 DECLARE_DEFERRED(tasks_init_deferred);
 
