fdb871ae11b41559396eec9026860dfdeeaa91ce
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2022-10-21 15:46:39 +0800

    keyboard_raw: add function keyboard_raw_config_alt
    Using keyboard_raw_config_alt to set KSI/KSO alternative function.
    Add all the keyboard gpio pins in pinctrl for setting sleep state
    in cros_kb_raw.
    
    BUG=b:252950988
    BRANCH=none
    TEST=none
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: I45d6de5b1f950f7e6c8d8b67ec03ba1e219c635b

--- include/keyboard_raw.h
+++ include/keyboard_raw.h
@@ -62,6 +62,14 @@ int keyboard_raw_read_rows(void);
  */
 void keyboard_raw_enable_interrupt(int enable);
 
+/**
+ * Enable or disable keyboard alternative function.
+ *
+ * @param enable 1 enable KSI/KSO alternative function,
+ *               0 set all KSI/KSO pins to normal GPIO.
+ */
+void keybaord_raw_config_alt(bool enable);
+
 #ifdef HAS_TASK_KEYSCAN
 
 /**
