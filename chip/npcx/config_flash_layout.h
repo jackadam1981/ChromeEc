2c7ab7f7ce086e840916e90dbceb4ceb9a5d3b7f
Jun Lin <CHLin56@nuvoton.com>
2022-10-26 16:23:31 +0800

    npcx: flash: FLASH_BANK_SIZE shouldn't depend on FLASH_ERASE_SIZE
    The flash bank defination is used to check the protection block. There
    is no reason to define it following the flash erase size setting.
    This CL fixed the flash bank size to 64KB which is the max flash erase
    block in the SPI flash.
    
    BRANCH=none
    BUG=b:254877499
    TEST="BOARD=npcx9_evb make", use console command flashwp/flashinfo to
    check the flash protection.
    
    Signed-off-by: Jun Lin <CHLin56@nuvoton.com>
    Change-Id: I9dfdc4083e2aa0a198ff882ecaa7794bb6ac5ad5

--- chip/npcx/config_flash_layout.h
+++ chip/npcx/config_flash_layout.h
@@ -120,7 +120,8 @@
 	"size or add support for CONFIG_FLASH_MULTIPLE_REGION."
 #endif
 
-#define CONFIG_FLASH_BANK_SIZE CONFIG_FLASH_ERASE_SIZE
+/* Use max flash erase size (64KB) as the protection bank size */
+#define CONFIG_FLASH_BANK_SIZE 0x20000
 #define CONFIG_FLASH_WRITE_SIZE 0x1 /* minimum write size */
 #define CONFIG_FLASH_WRITE_IDEAL_SIZE 256 /* one page size for write */
 
--- zephyr/shim/chip/npcx/include/flash_chip.h
+++ zephyr/shim/chip/npcx/include/flash_chip.h
@@ -11,7 +11,8 @@
 #define CONFIG_FLASH_WRITE_SIZE 0x1 /* minimum write size */
 #define CONFIG_FLASH_WRITE_IDEAL_SIZE 256 /* one page size for write */
 #define CONFIG_FLASH_ERASE_SIZE 0x10000
-#define CONFIG_FLASH_BANK_SIZE CONFIG_FLASH_ERASE_SIZE
+/* Use max flash erase size (64KB) as the protection bank size */
+#define CONFIG_FLASH_BANK_SIZE 0x20000
 
 /* RO image resides at start of protected region, right after header */
 #define CONFIG_RO_STORAGE_OFF CONFIG_RO_HDR_SIZE
