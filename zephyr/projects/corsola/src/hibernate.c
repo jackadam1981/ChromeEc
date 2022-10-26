3307260f2584bb2310f442f92f679ad7be9a769b
Eric Yilun Lin <yllin@chromium.org>
2022-10-26 11:35:31 +0800

    corsola: forcing shutdown before hibernate
    BUG=
    TEST=
    BRANCH=corsola
    
    Change-Id: Ia8ae0a4e6fb571414542477a11d19e59911d1966
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/projects/corsola/src/hibernate.c
+++ zephyr/projects/corsola/src/hibernate.c
@@ -5,12 +5,20 @@
 #include <zephyr/drivers/gpio.h>
 
 #include "charger.h"
+#include "chipset.h"
 #include "driver/charger/isl923x_public.h"
 #include "system.h"
+
+#define MT8186_SHUTDOWN_DELAY (10 * SECOND)
 
 /* Corsola board specific hibernate implementation */
 __override void board_hibernate(void)
 {
+	if (!chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
+		chipset_force_shutdown(CHIPSET_SHUTDOWN_BOARD_CUSTOM);
+		/* forcing shutdown needs */
+		sleep(MT8186_SHUTDOWN_DELAY);
+	}
 #ifdef CONFIG_CHARGER_ISL9238C
 	isl9238c_hibernate(CHARGER_SOLO);
 #endif
