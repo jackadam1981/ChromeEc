2a1dc4f270e15b5a21a6843e2c721f2f0db6b361
Abe Levkoy <alevkoy@chromium.org>
2022-10-27 16:30:35 -0600

    port80: Fix coverage error
    Add a #line directive to fix incorrect coverage reports.
    
    BUG=b:256008706
    TEST=twister -T zephyr/test --coverage
    BRANCH=none
    
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>
    Change-Id: Icf197de72a6aea2fa2b38bc2ccb4b14de46d6443

--- common/port80.c
+++ common/port80.c
@@ -38,6 +38,7 @@ static int print_in_int = CONFIG_PORT80_PRINT_IN_INT;
 static void port80_dump_buffer(void);
 DECLARE_DEFERRED(port80_dump_buffer);
 
+#line 42 /* This avoids a coverage off-by-one error of unknown origin. */
 void port_80_write(int data)
 {
 	char ts_str[PRINTF_TIMESTAMP_BUF_SIZE];
