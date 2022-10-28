af6c5798ba67108bce81c499b0cfc2b119e0dea3
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-10-28 13:13:38 +0800

    TCPMv2: enable enter custom mode and handle custom vdm
    This will allow EC to send enter mode with custom SVID and
    also handle vdm request and response.
    
    BUG=b:239767485, b:241207167
    BRANCH=none
    TEST=DUT can send enter mode and handle custom vdm
    
    Change-Id: I660772bb1498792f767fab1fe99f11c7e51c28e2
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>

--- common/usbc/usb_pd_dpm.c
+++ common/usbc/usb_pd_dpm.c
@@ -257,6 +257,11 @@ void dpm_vdm_acked(int port, enum tcpci_msg_type type, int vdo_count,
 #else
 		__fallthrough;
 #endif
+#ifdef USB_VID_USBC_MONITOR
+	case USB_VID_USBC_MONITOR:
+		board_docking_monitor_vdm_acked(port, type, vdo_count, vdm);
+		break;
+#endif
 	default:
 		CPRINTS("C%d: Received unexpected VDM ACK for SVID %d", port,
 			svid);
@@ -300,7 +305,11 @@ static void dpm_attempt_mode_entry(int port)
 	bool enter_mode_requested =
 		IS_ENABLED(CONFIG_USB_PD_REQUIRE_AP_MODE_ENTRY) ? false : true;
 	enum dpm_msg_setup_status status = MSG_SETUP_UNSUPPORTED;
+#ifdef USB_VID_USBC_MONITOR
+	struct rmdo partner_rmdo;
 
+	partner_rmdo = pe_get_partner_rmdo(port);
+#endif
 	if (pd_get_data_role(port) != PD_ROLE_DFP) {
 		if (DPM_CHK_FLAG(port, DPM_FLAG_ENTER_DP | DPM_FLAG_ENTER_TBT |
 					       DPM_FLAG_ENTER_USB4))
@@ -322,8 +331,10 @@ static void dpm_attempt_mode_entry(int port)
 	 * of the modes can get out of sync, causing the attempt to
 	 * enter the mode to fail prematurely.
 	 */
+#ifndef USB_VID_USBC_MONITOR
 	if (!chipset_in_state(CHIPSET_STATE_ANY_SUSPEND | CHIPSET_STATE_ON))
 		return;
+#endif
 #endif
 	/*
 	 * If discovery has not occurred for modes, do not attempt to switch
@@ -333,7 +344,17 @@ static void dpm_attempt_mode_entry(int port)
 	    pd_get_modes_discovery(port, TCPCI_MSG_SOP) != PD_DISC_COMPLETE)
 		return;
 
-	if (dp_entry_is_done(port) ||
+	/*
+	 * If support USB_VID_USBC_MONITOR but plug in normal DP dongle,
+	 * need to check if corresponding svid is supported.
+	 */
+	if ((dp_entry_is_done(port)
+#ifdef USB_VID_USBC_MONITOR
+	     && (board_docking_monitor_mode_entry_is_done(port) ||
+		 !pd_is_mode_discovered_for_svid(port, TCPCI_MSG_SOP,
+						 USB_VID_USBC_MONITOR))
+#endif
+		     ) ||
 	    (IS_ENABLED(CONFIG_USB_PD_TBT_COMPAT_MODE) &&
 	     tbt_entry_is_done(port)) ||
 	    (IS_ENABLED(CONFIG_USB_PD_USB4) && enter_usb_entry_is_done(port))) {
@@ -361,6 +382,9 @@ static void dpm_attempt_mode_entry(int port)
 
 	if (IS_ENABLED(CONFIG_USB_PD_REQUIRE_AP_MODE_ENTRY) &&
 	    IS_ENABLED(CONFIG_USB_PD_DATA_RESET_MSG) &&
+#ifdef USB_VID_USBC_MONITOR
+	    board_docking_monitor_mode_entry_is_done(port) &&
+#endif
 	    !DPM_CHK_FLAG(port, DPM_FLAG_DATA_RESET_DONE)) {
 		return;
 	}
@@ -404,6 +428,20 @@ static void dpm_attempt_mode_entry(int port)
 		enter_mode_requested = true;
 		vdo_count = ARRAY_SIZE(vdm);
 		status = dp_setup_next_vdm(port, &vdo_count, vdm);
+#ifdef USB_VID_USBC_MONITOR
+	} else if (!((partner_rmdo.major_rev == 3 &&
+		      partner_rmdo.minor_rev >= 1) ||
+		     partner_rmdo.major_rev > 3) &&
+		   pd_is_mode_discovered_for_svid(port, TCPCI_MSG_SOP,
+						  USB_VID_USBC_MONITOR)) {
+		/*
+		 * If the device support USB3.1 or greater,
+		 * do not enter docking monitor mode.
+		 */
+		vdo_count = ARRAY_SIZE(vdm);
+		status = board_docking_monitor_setup_next_vdm(port, &vdo_count,
+							      vdm);
+#endif
 	}
 
 	/* Not ready to send a VDM, check again next cycle */
@@ -646,7 +684,11 @@ void dpm_run(int port)
 		/* Run DFP related DPM requests */
 		if (DPM_CHK_FLAG(port, DPM_FLAG_EXIT_REQUEST))
 			dpm_attempt_mode_exit(port);
-		else if (!DPM_CHK_FLAG(port, DPM_FLAG_MODE_ENTRY_DONE))
+		else if (!DPM_CHK_FLAG(port, DPM_FLAG_MODE_ENTRY_DONE)
+#ifdef USB_VID_USBC_MONITOR
+			 || !board_docking_monitor_mode_entry_is_done(port)
+#endif
+		)
 			dpm_attempt_mode_entry(port);
 
 		/* Run USB PD Power button state machine */
