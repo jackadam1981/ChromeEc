4b4fc0a98f91ad2a38256015e7c4527e32040b4b
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-10-28 15:16:50 -0700

    ap_pwrseq: Adapt power sequence handlers for x86 non DSX
    This will make power sequence for x86 non DSX state handlers
    suitable for AP power sequence driver, following its state machine
    framework.
    
    TEST=zmake build
    BRANCH=none
    BUG=b:217952699
    
    Change-Id: Ia9438ea88b04304c5b3a8d8c44652e0ba91f8aa3
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>

--- zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
+++ zephyr/subsys/ap_pwrseq/include/x86_common_pwrseq.h
@@ -13,10 +13,4 @@
 #include <power_signals.h>
 #include <x86_power_signals.h>
 
-/* This encapsulates the attributes of the state machine */
-struct pwrseq_context {
-	/* On power-on start boot up sequence */
-	enum power_states_ndsx power_state;
-};
-
 #endif /* __X86_COMMON_PWRSEQ_H__ */
