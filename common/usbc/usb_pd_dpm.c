c9985bd2d42c9e783efdfc406f6d175522ffd32e
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-10-28 13:13:38 +0800

    TCPMv2: enable enter custom mode and handle custom vdm
    This will allow EC to send enter mode with custom SVID and
    also handle vdm request and response.
    
    BUG=b:239767485, b:241207167
    BRANCH=none
    TEST=DUT can send enter mode and handle custom vdm
    
    Change-Id: I660772bb1498792f767fab1fe99f11c7e51c28e2
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>

--- common/usbc/usb_pd_dpm.c
+++ common/usbc/usb_pd_dpm.c
@@ -335,6 +335,11 @@ void dpm_vdm_acked(int port, enum tcpci_msg_type type, int vdo_count,
 #else
 		__fallthrough;
 #endif /* CONFIG_USB_PD_TBT_COMPAT_MODE */
+#ifdef USB_VID_ACER_USBC
+	case USB_VID_ACER_USBC:
+		board_docking_monitor_vdm_acked(port, type, vdo_count, vdm);
+		break;
+#endif
 	default:
 		CPRINTS("C%d: Received unexpected VDM ACK for SVID %d", port,
 			svid);
@@ -1094,6 +1099,11 @@ static bool dpm_dfp_enter_mode_msg(int port)
 	bool enter_mode_requested =
 		IS_ENABLED(CONFIG_USB_PD_REQUIRE_AP_MODE_ENTRY) ? false : true;
 	enum dpm_msg_setup_status status = MSG_SETUP_UNSUPPORTED;
+#ifdef USB_VID_ACER_USBC
+	struct rmdo partner_rmdo;
+
+	partner_rmdo = pe_get_partner_rmdo(port);
+#endif
 
 #ifdef CONFIG_AP_POWER_CONTROL
 	/*
@@ -1103,8 +1113,10 @@ static bool dpm_dfp_enter_mode_msg(int port)
 	 * of the modes can get out of sync, causing the attempt to
 	 * enter the mode to fail prematurely.
 	 */
+#ifndef USB_VID_ACER_USBC
 	if (!chipset_in_state(CHIPSET_STATE_ANY_SUSPEND | CHIPSET_STATE_ON))
 		return false;
+#endif
 #endif
 	/*
 	 * If discovery has not occurred for modes, do not attempt to switch
@@ -1114,7 +1126,13 @@ static bool dpm_dfp_enter_mode_msg(int port)
 	    pd_get_modes_discovery(port, TCPCI_MSG_SOP) != PD_DISC_COMPLETE)
 		return false;
 
-	if (dp_entry_is_done(port) ||
+	if ((dp_entry_is_done(port)
+#ifdef USB_VID_ACER_USBC
+	     && (board_docking_monitor_mode_check_entry_is_done(port) ||
+		 !pd_is_mode_discovered_for_svid(port, TCPCI_MSG_SOP,
+						 USB_VID_ACER_USBC))
+#endif
+		     ) ||
 	    (IS_ENABLED(CONFIG_USB_PD_TBT_COMPAT_MODE) &&
 	     tbt_entry_is_done(port)) ||
 	    (IS_ENABLED(CONFIG_USB_PD_USB4) && enter_usb_entry_is_done(port))) {
@@ -1173,6 +1191,20 @@ static bool dpm_dfp_enter_mode_msg(int port)
 		enter_mode_requested = true;
 		vdo_count = ARRAY_SIZE(vdm);
 		status = dp_setup_next_vdm(port, &vdo_count, vdm);
+#ifdef USB_VID_ACER_USBC
+	} else if (!((partner_rmdo.major_rev == 3 &&
+		      partner_rmdo.minor_rev >= 1) ||
+		     partner_rmdo.major_rev > 3) &&
+		   pd_is_mode_discovered_for_svid(port, TCPCI_MSG_SOP,
+						  USB_VID_ACER_USBC)) {
+		/*
+		 * If the device support USB3.1 or greater,
+		 * do not enter docking monitor mode.
+		 */
+		vdo_count = ARRAY_SIZE(vdm);
+		status = board_docking_monitor_setup_next_vdm(port, &vdo_count,
+							      vdm);
+#endif
 	}
 
 	/* Not ready to send a VDM, check again next cycle */
@@ -1197,6 +1229,9 @@ static bool dpm_dfp_enter_mode_msg(int port)
 		 * future, but the DPM is done trying for now.
 		 */
 		dpm_set_mode_entry_done(port);
+#ifdef USB_VID_ACER_USBC
+		board_docking_monitor_set_mode_done(port);
+#endif
 		return false;
 	}
 
@@ -1365,7 +1400,11 @@ static void dpm_dfp_ready_run(const int port)
 	if (DPM_CHK_FLAG(port, DPM_FLAG_EXIT_REQUEST)) {
 		if (dpm_dfp_exit_mode_msg(port))
 			return;
-	} else if (!DPM_CHK_FLAG(port, DPM_FLAG_MODE_ENTRY_DONE)) {
+	} else if (!DPM_CHK_FLAG(port, DPM_FLAG_MODE_ENTRY_DONE)
+#ifdef USB_VID_ACER_USBC
+		   || !board_docking_monitor_mode_check_entry_is_done(port)
+#endif
+	) {
 		if (dpm_dfp_enter_mode_msg(port))
 			return;
 	}
--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -6312,6 +6312,12 @@ static void pe_vdm_response_entry(int port)
 		return;
 #endif
 	default:
+#ifdef USB_VID_ACER_USBC
+		if (board_customized_vdm_response(port, rx_payload)) {
+			pe_set_ready_state(port);
+			return;
+		}
+#endif
 		CPRINTF("VDO ERR:CMD:%d\n", vdo_cmd);
 	}
 
@@ -7806,6 +7812,10 @@ void pd_dfp_mode_init(int port)
 	/* Reset the DPM and DP modules to enable alternate mode entry. */
 	dpm_mode_exit_complete(port);
 	dp_init(port);
+
+#ifdef USB_VID_ACER_USBC
+	board_docking_monitor_mode_init(port);
+#endif
 
 	if (IS_ENABLED(CONFIG_USB_PD_TBT_COMPAT_MODE))
 		tbt_init(port);
