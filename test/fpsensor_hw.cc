d4213bfd2e733947916d93d0a5dffeb7a3925b85
Tom Hughes <tomhughes@chromium.org>
2022-10-28 15:00:08 -0700

    WIP: fpsensor_hw test compiles and runs with gtest
    libcxx needs a fix applied before this will compile:
    https://crrev.com/c/3994436.
    
    googletest must be built manually with:
    
    rm -rf build && mkdir build && pushd build && cmake -GNinja \
    -Dgtest_disable_pthreads=ON \
    -DCMAKE_TOOLCHAIN_FILE=~/chromiumos/src/platform/ec/cmake/toolchain.cmake \
    -DCMAKE_INSTALL_PREFIX=~/chromiumos/src/third_party/tmp .. \
    ninja
    ninja install
    
    BRANCH=none
    BUG=b:254530679
    TEST=make BOARD=dartmonkey test-fpsensor_hw -j
    
    Cq-Depend: chromium:3994436
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I1908ea496cd69c7d0e0cd84a841bd4c1692a1bf2

--- test/fpsensor_hw.cc
+++ test/fpsensor_hw.cc
@@ -4,9 +4,15 @@
  */
 
 #include "common.h"
+#include "compiler.h"
 #include "test_util.h"
 #include "fpc_private.h"
 #include "board.h"
+
+// TODO(https://github.com/google/googletest/issues/240)
+DISABLE_COMPILER_WARNING("-Wundef")
+#include <gtest/gtest.h>
+ENABLE_COMPILER_WARNING("-Wundef")
 
 #ifdef SECTION_IS_RW
 #include "fpc/fpc_sensor.h"
@@ -18,22 +24,48 @@ static const uint32_t fp_sensor_hwid = UINT32_MAX;
 /* Hardware-dependent smoke test that makes a SPI transaction with the
  * fingerprint sensor.
  */
-test_static int test_fp_check_hwid(void)
+TEST(FpSensor, CheckHardwareID)
 {
 	uint16_t id = 0;
 
 	if (IS_ENABLED(SECTION_IS_RW)) {
-		TEST_EQ(fpc_get_hwid(&id), EC_SUCCESS, "%d");
+		EXPECT_EQ(fpc_get_hwid(&id), EC_SUCCESS);
+
 		/* The lower 4-bits of the sensor hardware id are a
 		 * manufacturing ID that is ok to vary.
 		 */
-		TEST_EQ(fp_sensor_hwid, id >> 4, "%d");
+		EXPECT_EQ(fp_sensor_hwid, id >> 4);
+
+		// Uncomment to test failure.
+		// EXPECT_TRUE(false);
 	};
-	return EC_SUCCESS;
 }
 
-extern "C" void run_test(int argc, const char **argv)
+// TODO: This shouldn't be necessary since tests should be registered during
+// construction of static/global variables:
+// https://github.com/google/googletest/blob/3026483ae575e2de942db5e760cf95e973308dd5/googletest/include/gtest/internal/gtest-internal.h#L1557-L1567
+void RegisterMyTests()
 {
-	RUN_TEST(test_fp_check_hwid);
-	test_print_result();
+	::testing::RegisterTest(
+		"FpSensor", "CheckHardwareID", nullptr, nullptr, __FILE__,
+		__LINE__, [=]() -> ::testing::Test * {
+			return new FpSensor_CheckHardwareID_Test();
+		});
+}
+
+extern "C" void run_test(int, const char **)
+{
+	// TODO: This should not be necessary:
+	// https://github.com/google/googletest/blob/main/docs/advanced.md#running-a-subset-of-the-tests
+	const char *_argv[] = { "ignored", "--gtest_filter=*",
+				"--gtest_repeat=1" };
+	int _argc = ARRAY_SIZE(_argv);
+
+	testing::InitGoogleTest(&_argc, (char **)_argv);
+	RegisterMyTests();
+	int ret = RUN_ALL_TESTS();
+	if (ret == 0)
+		printf("Pass!\n");
+	else
+		printf("Fail!\n");
 }
