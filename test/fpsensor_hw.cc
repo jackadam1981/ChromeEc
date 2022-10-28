81243c51e5155502611f7e41d687daad0fa91c6d
Tom Hughes <tomhughes@chromium.org>
2022-10-28 15:00:08 -0700

    test/fpsensor_hw: fpsensor_hw test compiles and runs with gtest
    BRANCH=none
    BUG=b:254530679
    TEST=make BOARD=bloonchipper test-fpsensor_hw -j
    TEST=make BOARD=dartmonkey test-fpsensor_hw -j
    
    Cq-Depend: chromium:3994436
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I1908ea496cd69c7d0e0cd84a841bd4c1692a1bf2

--- test/fpsensor_hw.cc
+++ test/fpsensor_hw.cc
@@ -5,8 +5,14 @@
 
 #include "common.h"
 #include "config.h"
+#include "compiler.h"
 #include "fpc_private.h"
 #include "test_util.h"
+
+// TODO(https://github.com/google/googletest/issues/240)
+DISABLE_COMPILER_WARNING("-Wundef")
+#include <gtest/gtest.h>
+ENABLE_COMPILER_WARNING("-Wundef")
 
 #ifdef SECTION_IS_RW
 #include "fpc/fpc_sensor.h"
@@ -18,22 +24,29 @@ static const uint32_t fp_sensor_hwid = UINT32_MAX;
 /* Hardware-dependent smoke test that makes a SPI transaction with the
  * fingerprint sensor.
  */
-test_static int test_fp_check_hwid(void)
+TEST(FpSensor, CheckHardwareID)
 {
 	uint16_t id = 0;
 
 	if (IS_ENABLED(SECTION_IS_RW)) {
-		TEST_EQ(fpc_get_hwid(&id), EC_SUCCESS, "%d");
+		EXPECT_EQ(fpc_get_hwid(&id), EC_SUCCESS);
+
 		/* The lower 4-bits of the sensor hardware id are a
 		 * manufacturing ID that is ok to vary.
 		 */
-		TEST_EQ(fp_sensor_hwid, id >> 4, "%d");
+		EXPECT_EQ(fp_sensor_hwid, id >> 4);
+
+		// Uncomment to test failure.
+		// EXPECT_TRUE(false);
 	};
-	return EC_SUCCESS;
 }
 
-extern "C" void run_test(int argc, const char **argv)
+extern "C" void run_test(int, const char **)
 {
-	RUN_TEST(test_fp_check_hwid);
-	test_print_result();
+	testing::InitGoogleTest();
+	int ret = RUN_ALL_TESTS();
+	if (ret == 0)
+		printf("Pass!\n");
+	else
+		printf("Fail!\n");
 }
