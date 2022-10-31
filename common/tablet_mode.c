3c2dca6cd0461ea1ee447febeaab52fa69012c2b
Tim Van Patten <timvp@google.com>
2022-10-31 15:19:53 -0600

    Add EC_CMD_SET_TABLET_MODE handler
    Add the EC_CMD_SET_TABLET_MODE host command handler. This will update
    the tablet mode status in the EC, based on the parameters of the
    EC_CMD_SET_TABLET_MODE command.
    
    BRANCH=none
    BUG=b:256015402
    TEST=CQ
    
    Change-Id: Ia7d8ad44a284742bb347004ba355a8419d047faa
    Signed-off-by: Tim Van Patten <timvp@google.com>

--- common/tablet_mode.c
+++ common/tablet_mode.c
@@ -28,6 +28,12 @@ static uint32_t tablet_mode;
  */
 static bool tablet_mode_forced;
 
+/*
+ * Console command can force the value of tablet_mode. If tablet_mode_force is
+ * false, use stored tablet mode value before it was (possibly) overridden.
+ */
+static uint32_t tablet_mode_store;
+
 /* True if GMR sensor is reporting 360 degrees. */
 static bool gmr_sensor_at_360;
 
@@ -173,11 +179,26 @@ void gmr_tablet_switch_disable(void)
 }
 #endif
 
+static enum ec_status tablet_mode_command(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_set_tablet_mode *p = args->params;
+
+	if (tablet_mode_forced == false)
+		tablet_mode_store = tablet_mode;
+
+	tablet_mode = p->tablet_mode;
+	tablet_mode_forced = p->tablet_mode_forced;
+
+	notify_tablet_mode_change();
+
+	return EC_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_SET_TABLET_MODE, tablet_mode_command,
+		     EC_VER_MASK(0) | EC_VER_MASK(1));
+
 #ifdef CONFIG_TABLET_MODE
 static int command_settabletmode(int argc, const char **argv)
 {
-	static uint32_t tablet_mode_store;
-
 	if (argc == 1) {
 		print_tablet_mode();
 		return EC_SUCCESS;
