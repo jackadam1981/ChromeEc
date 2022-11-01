89817734678a07580649979e6a185c9bdd2fa28f
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-11-01 12:08:49 -0700

    ap_pwrseq: Adapt MTL chipset power sequence handlers
    This CL makes MTL chipset code to follow new AP Power Sequence driver
    state handlers framework.
    
    TEST=zmake build
    BRANCH=none
    BUG=b:217952699
    
    Change-Id: I444ce775bc78fdc75288de2934448cbf79240c1a

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_mtl_pwrseq_sm.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_mtl_pwrseq_sm.c
@@ -5,55 +5,53 @@
 
 #include <x86_non_dsx_common_pwrseq_sm_handler.h>
 
+#include "ap_power/ap_pwrseq_sm.h"
+
 LOG_MODULE_DECLARE(ap_pwrseq, CONFIG_AP_PWRSEQ_LOG_LEVEL);
 
-static void ap_off(void)
+static int x86_non_dsx_mtl_s0_exit(void *data)
 {
+	enum ap_pwrseq_state state;
+
+	ap_pwrseq_get_current_state(data, &state);
+	if (state == AP_POWER_STATE_S0IX) {
+		return 0;
+	}
+
 	power_signal_set(PWR_PCH_PWROK, 0);
 	power_signal_set(PWR_EC_PCH_SYS_PWROK, 0);
+
+	return 0;
 }
 
-/* Generate SYS_PWROK->SOC if needed by system */
-static void generate_pwrok_handler(void)
+static int x86_non_dsx_mtl_s0_entry(void *data)
 {
-	int all_sys_pwrgd_in;
-
-	if (power_signal_get(PWR_EC_PCH_SYS_PWROK) == 0) {
+	if (AP_PWRSEQ_DT_VALUE(sys_pwrok_delay)) {
 		k_msleep(AP_PWRSEQ_DT_VALUE(sys_pwrok_delay));
 	}
 
-	all_sys_pwrgd_in = power_signal_get(PWR_ALL_SYS_PWRGD);
+	return 0;
+}
+
+/* Generate SYS_PWROK->SOC if needed by system */
+static int x86_non_dsx_mtl_s0_run(void *data)
+{
 	/* Loop through all PWROK signals defined by the board */
-	if (all_sys_pwrgd_in == 0) {
+	if (power_signal_get(PWR_ALL_SYS_PWRGD) == 0) {
 		LOG_DBG("PG_EC_ALL_SYS_PWRGD deasserted, "
 			"shutting AP off!");
-		ap_off();
-		return;
+		return ap_pwrseq_sm_set_state(data, AP_POWER_STATE_S3);
 	}
 
-	power_signal_set(PWR_EC_PCH_SYS_PWROK, all_sys_pwrgd_in);
+	power_signal_set(PWR_EC_PCH_SYS_PWROK, 1);
 	/* PCH_PWROK is set to combined result of ALL_SYS_PWRGD and SLP_S3 */
 	power_signal_set(PWR_PCH_PWROK,
-			 all_sys_pwrgd_in && !power_signal_get(PWR_SLP_S3));
+			 !power_signal_get(PWR_SLP_S3));
+
+	return 0;
 }
 
-/* Chipset specific power state machine handler */
-enum power_states_ndsx chipset_pwr_sm_run(enum power_states_ndsx curr_state)
-{
-	/* Add chipset specific state handling if any */
-	switch (curr_state) {
-	case SYS_POWER_STATE_G3S5:
-		board_ap_power_action_g3_s5();
-		break;
-	case SYS_POWER_STATE_S0S3:
-		ap_off();
-		break;
-	case SYS_POWER_STATE_S0:
-		/* Send SYS_PWROK->SoC if conditions met */
-		generate_pwrok_handler();
-		break;
-	default:
-		break;
-	}
-	return curr_state;
-}
+AP_POWER_CHIPSET_STATE_DEFINE(AP_POWER_STATE_S0,
+			      x86_non_dsx_mtl_s0_entry,
+			      x86_non_dsx_mtl_s0_run,
+			      x86_non_dsx_mtl_s0_exit)
