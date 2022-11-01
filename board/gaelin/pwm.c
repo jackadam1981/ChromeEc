85e0e879e182ba438ba4daff0460868edba20e1a
Raymond Chung <raymondchung@ami.corp-partner.google.com>
2022-11-01 14:45:42 +0800

    gaelin: Enable the fan control
    Fan control via custom fan table.
    
    BUG=b:249000573
    BRANCH=firmware-brya-14505.B
    TEST=Thermal team verified thermal policy is expected.
    
    Change-Id: Ifbd3d8ffbc8035e5b3075bf840beccd158e28e18
    Signed-off-by: Raymond Chung <raymondchung@ami.corp-partner.google.com>

--- board/gaelin/pwm.c
+++ board/gaelin/pwm.c
@@ -17,7 +17,7 @@ const struct pwm_t pwm_channels[] = {
 			       .freq = 2000 },
 	[PWM_CH_FAN] = { .channel = 5,
 			 .flags = PWM_CONFIG_OPEN_DRAIN | PWM_CONFIG_DSLEEP,
-			 .freq = 1000 },
+			 .freq = 25000 },
 	[PWM_CH_LED_RED] = { .channel = 2,
 			     .flags = PWM_CONFIG_ACTIVE_LOW | PWM_CONFIG_DSLEEP,
 			     .freq = 2000 },
