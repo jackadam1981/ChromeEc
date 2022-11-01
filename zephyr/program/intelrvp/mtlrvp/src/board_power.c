d05fe84c42d577377aa97844ae2a0116c44c4f17
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-11-01 12:09:16 -0700

    ap_pwrseq: Adapt MTL RVP power sequence handlers
    This CL makes MTL RVP spefific power sequence code to follow new AP
    Power Sequence driver state handlers framework.
    
    TEST=zmake build
    BRANCH=none
    BUG=b:217952699
    
    Change-Id: I6e0d99e37e866f5453bf1b19b8ffe96ac844c9c8

--- zephyr/program/intelrvp/mtlrvp/src/board_power.c
+++ zephyr/program/intelrvp/mtlrvp/src/board_power.c
@@ -3,59 +3,93 @@
  * found in the LICENSE file.
  */
 
-#include "gpio/gpio.h"
-#include "gpio_signal.h"
-
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/atomic.h>
 
 #include <ap_power/ap_power.h>
 #include <ap_power/ap_power_events.h>
-#include <ap_power/ap_power_interface.h>
 #include <ap_power_override_functions.h>
 #include <power_signals.h>
 #include <x86_power_signals.h>
+
+#include "ap_power/ap_pwrseq_sm.h"
 
 LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
 
 #if CONFIG_X86_NON_DSX_PWRSEQ_MTL
 #define X86_NON_DSX_MTL_FORCE_SHUTDOWN_TO_MS 50
 
-void board_ap_power_force_shutdown(void)
-{
-	int timeout_ms = X86_NON_DSX_MTL_FORCE_SHUTDOWN_TO_MS;
+static void ap_board_timer_handler(struct k_timer *timer);
+/* S5 inactive timer*/
+K_TIMER_DEFINE(ap_board_timer, ap_board_timer_handler, NULL);
 
+int board_ap_power_action_g3_entry(void *data)
+{
 	/* Turn off PCH_RMSRST to meet tPCH12 */
 	power_signal_set(PWR_EC_PCH_RSMRST, 0);
 
 	/* Turn off PRIM load switch. */
 	power_signal_set(PWR_EN_PP3300_A, 0);
-
-	/* Wait RSMRST to be off. */
-	while (power_signal_get(PWR_RSMRST) && (timeout_ms > 0)) {
-		k_msleep(1);
-		timeout_ms--;
-	};
-
-	if (power_signal_get(PWR_RSMRST))
-		LOG_WRN("RSMRST_ODL didn't go low!  Assuming G3.");
-}
-
-void board_ap_power_action_g3_s5(void)
-{
-	/* Turn on the PP3300_PRIM rail. */
-	power_signal_set(PWR_EN_PP3300_A, 1);
-
-	if (!power_wait_signals_timeout(
-		    IN_PGOOD_ALL_CORE,
-		    AP_PWRSEQ_DT_VALUE(wait_signal_timeout))) {
-		ap_power_ev_send_callbacks(AP_POWER_PRE_INIT);
+	/* This timer is for checking signal state */
+	if (power_signal_get(PWR_RSMRST)) {
+		k_timer_start(&ap_board_timer,
+			K_MSEC(X86_NON_DSX_MTL_FORCE_SHUTDOWN_TO_MS),
+			K_NO_WAIT);
 	}
+	return 0;
 }
 
-bool board_ap_power_check_power_rails_enabled(void)
+static int board_ap_power_action_g3_run(void *data)
 {
-	return power_signal_get(PWR_EN_PP3300_A);
+	if (IS_EVENT_SET(data, AP_PWRSEQ_EVENT_POWER_STARTUP)) {
+		/* Turn on the PP3300_PRIM rail. */
+		power_signal_set(PWR_EN_PP3300_A, 1);
+
+		k_timer_start(&ap_board_timer,
+			K_MSEC(AP_PWRSEQ_DT_VALUE(wait_signal_timeout)),
+			K_NO_WAIT);
+	}
+
+	if (power_signals_on(IN_PGOOD_ALL_CORE)) {
+		k_timer_stop(&ap_board_timer);
+		return 0;
+	}
+
+	if  (IS_EVENT_SET(data, AP_PWRSEQ_EVENT_POWER_TIMEOUT)) {
+		power_signal_set(PWR_EN_PP3300_A, 0);
+	}
+	return 1;
+}
+
+static int board_ap_power_action_g3_exit(void *data)
+{
+	k_timer_stop(&ap_board_timer);
+	ap_power_ev_send_callbacks(AP_POWER_PRE_INIT);
+	return 0;
+}
+
+AP_POWER_APP_STATE_DEFINE(AP_POWER_STATE_G3,
+			  board_ap_power_action_g3_entry,
+			  board_ap_power_action_g3_run,
+			  board_ap_power_action_g3_exit)
+
+static void ap_board_timer_handler(struct k_timer *timer)
+{
+	const struct device *dev = ap_pwrseq_get_instance();
+	enum ap_pwrseq_state state;
+
+	ap_pwrseq_get_current_state(dev, &state);
+	switch(state) {
+	case AP_POWER_STATE_G3:
+		if (power_signal_get(PWR_EN_PP3300_A)) {
+			ap_pwrseq_post_event(dev,
+					     AP_PWRSEQ_EVENT_POWER_TIMEOUT);
+		} else if (power_signal_get(PWR_RSMRST)) {
+			LOG_WRN("RSMRST_ODL didn't go low!  Assuming G3.");
+		}
+		break;
+	default:
+		break;
+	}
 }
 #endif /* CONFIG_X86_NON_DSX_PWRSEQ_MTL */
