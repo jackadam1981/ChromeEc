a9b0ccfd77d69e37fa72d8d954df6bfe3a06f183
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-11-01 12:09:16 -0700

    ap_pwrseq: Adapt MTL RVP power sequence handlers
    This CL makes MTL RVP spefific power sequence code to follow new AP
    Power Sequence driver state handlers framework.
    
    TEST=zmake build
    BRANCH=none
    BUG=b:217952699
    
    Change-Id: I6e0d99e37e866f5453bf1b19b8ffe96ac844c9c8

--- zephyr/program/intelrvp/mtlrvp/src/board_power.c
+++ zephyr/program/intelrvp/mtlrvp/src/board_power.c
@@ -3,19 +3,14 @@
  * found in the LICENSE file.
  */
 
-#include "gpio/gpio.h"
-#include "gpio_signal.h"
-
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/atomic.h>
 
-#include <ap_power/ap_power.h>
-#include <ap_power/ap_power_events.h>
-#include <ap_power/ap_power_interface.h>
 #include <ap_power_override_functions.h>
 #include <power_signals.h>
 #include <x86_power_signals.h>
+
+#include "ap_power/ap_pwrseq_sm.h"
 
 LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
 
@@ -24,6 +19,12 @@ LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
 
 void board_ap_power_force_shutdown(void)
 {
+	power_signal_set(PWR_EC_PCH_RSMRST, 0);
+	power_signal_set(PWR_EN_PP3300_A, 0);
+}
+
+int board_ap_power_action_g3_entry(void *data)
+{
 	int timeout_ms = X86_NON_DSX_MTL_FORCE_SHUTDOWN_TO_MS;
 
 	/* Turn off PCH_RMSRST to meet tPCH12 */
@@ -31,31 +32,55 @@ void board_ap_power_force_shutdown(void)
 
 	/* Turn off PRIM load switch. */
 	power_signal_set(PWR_EN_PP3300_A, 0);
-
-	/* Wait RSMRST to be off. */
+	/* This timer is for checking signal state */
 	while (power_signal_get(PWR_RSMRST) && (timeout_ms > 0)) {
 		k_msleep(1);
 		timeout_ms--;
 	};
 
-	if (power_signal_get(PWR_RSMRST))
-		LOG_WRN("RSMRST_ODL didn't go low!  Assuming G3.");
+	return 0;
 }
 
-void board_ap_power_action_g3_s5(void)
+static int board_ap_power_action_g3_run(void *data)
 {
-	/* Turn on the PP3300_PRIM rail. */
-	power_signal_set(PWR_EN_PP3300_A, 1);
-
-	if (!power_wait_signals_timeout(
-		    IN_PGOOD_ALL_CORE,
-		    AP_PWRSEQ_DT_VALUE(wait_signal_timeout))) {
-		ap_power_ev_send_callbacks(AP_POWER_PRE_INIT);
+	if (IS_EVENT_SET(data, AP_PWRSEQ_EVENT_POWER_STARTUP)) {
+		/* Turn on the PP3300_PRIM rail. */
+		power_signal_set(PWR_EN_PP3300_A, 1);
 	}
+
+	return 0;
 }
 
-bool board_ap_power_check_power_rails_enabled(void)
+AP_POWER_APP_STATE_DEFINE(AP_POWER_STATE_G3,
+			  board_ap_power_action_g3_entry,
+			  board_ap_power_action_g3_run,
+			  NULL)
+
+static int board_ap_power_action_s3_run(void *data)
 {
-	return power_signal_get(PWR_EN_PP3300_A);
+	int all_sys_pwrgd_in;
+
+	if (power_signal_get(PWR_EC_PCH_SYS_PWROK) == 0) {
+		k_msleep(AP_PWRSEQ_DT_VALUE(sys_pwrok_delay));
+	}
+
+	all_sys_pwrgd_in = power_signal_get(PWR_ALL_SYS_PWRGD);
+	/* Loop through all PWROK signals defined by the board */
+	if (all_sys_pwrgd_in == 0) {
+		LOG_ERR("PG_EC_ALL_SYS_PWRGD deasserted, "
+			"shutting AP off!");
+		return 1;
+	}
+
+	power_signal_set(PWR_EC_PCH_SYS_PWROK, all_sys_pwrgd_in);
+	/* PCH_PWROK is set to combined result of ALL_SYS_PWRGD and SLP_S3 */
+	power_signal_set(PWR_PCH_PWROK,
+			 all_sys_pwrgd_in && !power_signal_get(PWR_SLP_S3));
+	return 0;
 }
+
+AP_POWER_APP_STATE_DEFINE(AP_POWER_STATE_S3,
+			  NULL,
+			  board_ap_power_action_s3_run,
+			  NULL)
 #endif /* CONFIG_X86_NON_DSX_PWRSEQ_MTL */
