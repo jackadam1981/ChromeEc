782dd4e91fdfe6f4bfd489b597688cae98ba18af
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-11-01 12:09:16 -0700

    ap_pwrseq: Adapt MTL RVP power sequence handlers
    This CL makes MTL RVP spefific power sequence code to follow new AP
    Power Sequence driver state handlers framework. Include G3 state
    action handler.
    
    TEST=zmake build
    BRANCH=none
    BUG=b:217952699
    
    Change-Id: I6e0d99e37e866f5453bf1b19b8ffe96ac844c9c8

--- zephyr/program/intelrvp/mtlrvp/src/board_power.c
+++ zephyr/program/intelrvp/mtlrvp/src/board_power.c
@@ -18,11 +18,16 @@
 #include <power_signals.h>
 #include <x86_power_signals.h>
 
+#ifdef CONFIG_AP_PWRSEQ_DRIVER
+#include "ap_power/ap_pwrseq_sm.h"
+#endif
+
 LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
 
 #if CONFIG_X86_NON_DSX_PWRSEQ_MTL
 #define X86_NON_DSX_MTL_FORCE_SHUTDOWN_TO_MS 50
 
+#ifndef CONFIG_AP_PWRSEQ_DRIVER
 void board_ap_power_force_shutdown(void)
 {
 	int timeout_ms = X86_NON_DSX_MTL_FORCE_SHUTDOWN_TO_MS;
@@ -60,4 +65,44 @@ bool board_ap_power_check_power_rails_enabled(void)
 {
 	return power_signal_get(PWR_EN_PP3300_A);
 }
+#else
+void board_ap_power_force_shutdown(void)
+{
+	power_signal_set(PWR_EC_PCH_RSMRST, 0);
+	power_signal_set(PWR_EN_PP3300_A, 0);
+}
+
+int board_ap_power_action_g3_entry(void *data)
+{
+	int timeout_ms = X86_NON_DSX_MTL_FORCE_SHUTDOWN_TO_MS;
+
+	/* Turn off PCH_RMSRST to meet tPCH12 */
+	power_signal_set(PWR_EC_PCH_RSMRST, 0);
+
+	/* Turn off PRIM load switch. */
+	power_signal_set(PWR_EN_PP3300_A, 0);
+	/* This timer is for checking signal state */
+	while (power_signal_get(PWR_RSMRST) && (timeout_ms > 0)) {
+		k_msleep(1);
+		timeout_ms--;
+	};
+
+	return 0;
+}
+
+static int board_ap_power_action_g3_run(void *data)
+{
+	if (IS_EVENT_SET(data, AP_PWRSEQ_EVENT_POWER_STARTUP)) {
+		/* Turn on the PP3300_PRIM rail. */
+		power_signal_set(PWR_EN_PP3300_A, 1);
+	}
+
+	return 0;
+}
+
+AP_POWER_APP_STATE_DEFINE(AP_POWER_STATE_G3,
+			  board_ap_power_action_g3_entry,
+			  board_ap_power_action_g3_run,
+			  NULL)
+#endif // CONFIG_AP_PWRSEQ_DRIVER
 #endif /* CONFIG_X86_NON_DSX_PWRSEQ_MTL */
