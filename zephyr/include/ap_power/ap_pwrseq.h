9f15b5d01905803609cdfb69f50bff8aabec0a1b
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-11-01 18:56:04 -0700

    ap_pwrseq: Adapt power interface components to AP Power Sequence driver
    This CL make other components to follow AP Power Sequence driver.
    
    BUG=b:217952699
    BRANCH=none
    TEST=zmake build
    
    Change-Id: I9d528fadd3074bfa9a2e9d1f36cfdeaf45015579
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>

--- zephyr/include/ap_power/ap_pwrseq.h
+++ zephyr/include/ap_power/ap_pwrseq.h
@@ -8,10 +8,9 @@
 #include <zephyr/device.h>
 #include <zephyr/kernel.h>
 
-/** Starts the AP power sequence thread */
-void ap_pwrseq_task_start(void);
-
+#ifndef CONFIG_AP_PWRSEQ_DRIVER
 void ap_pwrseq_wake(void);
+#else
 
 #ifdef __cplusplus
 extern "C" {
@@ -201,4 +200,5 @@ int ap_pwrseq_register_state_exit_callback(
 #ifdef __cplusplus
 }
 #endif
+#endif /* CONFIG_AP_PWRSEQ_DRIVER */
 #endif /* _AP_PWRSEQ_H_ */
