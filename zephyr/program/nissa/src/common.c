67d2e0eabd93445fc9ac21ca9f8304c168e76bbe
Zick Wei <zick.wei@quanta.corp-partner.google.com>
2022-11-08 18:46:38 +0800

    yaviks: update sub board setting
    Currently sub board work when cbi fw_config exist,
    in some condition fw_config may not set which cause
    sub board not functional.
    yaviks only has one sub board,
    this CL ignore fw_config, let sub board always functional.
    
    BUG=b:260780060
    BRANCH=none
    TEST=make sure USBC1 port work without fw_config.
    
    Signed-off-by: Zick Wei <zick.wei@quanta.corp-partner.google.com>
    Change-Id: I3f33829525344d7867031e68a24d4e8b065c8ba2

--- zephyr/program/nissa/src/common.c
+++ zephyr/program/nissa/src/common.c
@@ -19,67 +19,6 @@
 #include <ap_power/ap_power.h>
 LOG_MODULE_REGISTER(nissa, CONFIG_NISSA_LOG_LEVEL);
 
-static uint8_t cached_usb_pd_port_count;
-
-__override uint8_t board_get_usb_pd_port_count(void)
-{
-	__ASSERT(cached_usb_pd_port_count != 0,
-		 "sub-board detection did not run before a port count request");
-	if (cached_usb_pd_port_count == 0)
-		LOG_WRN("USB PD Port count not initialized!");
-	return cached_usb_pd_port_count;
-}
-
-static void board_power_change(struct ap_power_ev_callback *cb,
-			       struct ap_power_ev_data data)
-{
-	/*
-	 * Enable power to pen garage when system is active (safe even if no
-	 * pen is present).
-	 */
-	const struct gpio_dt_spec *const pen_power_gpio =
-		GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_pen_x);
-
-	switch (data.event) {
-	case AP_POWER_STARTUP:
-		gpio_pin_set_dt(pen_power_gpio, 1);
-		break;
-	case AP_POWER_SHUTDOWN:
-		gpio_pin_set_dt(pen_power_gpio, 0);
-		break;
-	default:
-		break;
-	}
-}
-
-/*
- * Initialise the USB PD port count, which
- * depends on which sub-board is attached.
- */
-static void board_setup_init(void)
-{
-	static struct ap_power_ev_callback cb;
-
-	ap_power_ev_init_callback(&cb, board_power_change,
-				  AP_POWER_STARTUP | AP_POWER_SHUTDOWN);
-	ap_power_ev_add_callback(&cb);
-
-	switch (nissa_get_sb_type()) {
-	default:
-		cached_usb_pd_port_count = 1;
-		break;
-
-	case NISSA_SB_C_A:
-	case NISSA_SB_C_LTE:
-		cached_usb_pd_port_count = 2;
-		break;
-	}
-}
-/*
- * Make sure setup is done after EEPROM is readable.
- */
-DECLARE_HOOK(HOOK_INIT, board_setup_init, HOOK_PRIO_INIT_I2C);
-
 int pd_check_vconn_swap(int port)
 {
 	/* Allow VCONN swaps if the AP is on. */
