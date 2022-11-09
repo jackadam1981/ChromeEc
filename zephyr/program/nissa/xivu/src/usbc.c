bb042b3a4ba6a263f8303c36742d19bb62a4183d
Peter Marheine <pmarheine@chromium.org>
2022-11-09 16:51:36 +1100

    Add CONFIG_CHARGER_INPUT_CURRENT_DERATE_PCT
    Many boards derate their charge current limits by some fixed ratio,
    which results in duplicate code appearing in many different boards.
    Since nearly all of these are in implementations of
    board_set_charge_limit() and themselves call
    charge_set_input_current_limit(), make that derating a config option
    and move it into charge_set_input_current_limit(). This makes most
    boards' implementations uniform and ripe for further simplification
    in later changes.
    
    For those boards that do more complex adjustments, those can be
    retained by keeping the existing logic in board_set_charge_limit().
    
    Several boards also uselessly defined multiple versions of
    board_set_charge_limit(): the redundant ones are removed.
    
    BUG=b:163093572
    TEST=make buildall; zmake build -a
    BRANCH=none
    
    Change-Id: I0a7162e72538a91ad06ba85b91a10b93eb6af96b
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>

--- zephyr/program/nissa/xivu/src/usbc.c
+++ zephyr/program/nissa/xivu/src/usbc.c
@@ -279,7 +279,6 @@ void usb_interrupt(enum gpio_signal signal)
 __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 				       int max_ma, int charge_mv)
 {
-	charge_ma = (charge_ma * 90) / 100;
 	charge_set_input_current_limit(
 		MAX(charge_ma, CONFIG_CHARGER_INPUT_CURRENT), charge_mv);
 }
