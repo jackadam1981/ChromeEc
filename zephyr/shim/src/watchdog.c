6858bb8d2af1685f7ec5f3a9549b2c1dc76877b3
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2022-11-09 11:21:09 +0800

    (TEST) wdt pre-warning msg
    Task is enabled by config,
    ex.CONFIG_HAS_TASK_CHARGER (HAS_TASK_CHARGER)...
    
    Change-Id: I017b2718453fe555981a8b35fc2d6a42a694ca7a
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- zephyr/shim/src/watchdog.c
+++ zephyr/shim/src/watchdog.c
@@ -20,13 +20,14 @@ LOG_MODULE_REGISTER(watchdog_shim, LOG_LEVEL_ERR);
 extern bool wdt_warning_triggered;
 #endif /* TEST_BUILD */
 
+uint32_t int_ra_reg;
 static void wdt_warning_handler(const struct device *wdt_dev, int channel_id)
 {
 	const char *thread_name = k_thread_name_get(k_current_get());
 
 #ifdef CONFIG_RISCV
-	printk("WDT pre-warning MEPC:%p THREAD_NAME:%s\n",
-	       (void *)csr_read(mepc), thread_name);
+	printk("WDT pre-warning MEPC:%p int_ra=0x%08x THREAD_NAME:%s\n",
+	       (void *)csr_read(mepc), int_ra_reg, thread_name);
 #else
 	/* TODO(b/176523207): watchdog warning message */
 	printk("Watchdog deadline is close! THREAD_NAME:%s\n", thread_name);
