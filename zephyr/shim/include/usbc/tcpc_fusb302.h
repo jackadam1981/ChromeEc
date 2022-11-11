9022d82157fc47b77a0a6e5bec9eebe2122dc7d1
Jason Yuan <jasonyuan@google.com>
2022-11-10 17:05:57 -0800

    zephyr: move usbc interrupt handler to shim
    various board specific TCPC interrupt handler have been moved into
    tcpc.c in shim. ports are now automatically configured from the i2c dts
    instead of hardcoded.
    
    this does not remove the handler binding from interrupts.dts or
    initialize the interrupt from the driver, which will be added in a
    followup CL.
    
    stub test handler in test/.../stubs.c has been removed and existing
    tests have been modified to use new tcpc driver.
    
    BUG=b:256996124
    TEST=twister
    BRANCH=none
    
    Signed-off-by: Jason Yuan <jasonyuan@google.com>
    Change-Id: Iac05907f3c286f26329dac7f99730396d23b1166

--- zephyr/shim/include/usbc/tcpc_fusb302.h
+++ zephyr/shim/include/usbc/tcpc_fusb302.h
@@ -10,11 +10,12 @@
 #define FUSB302_TCPC_COMPAT fairchild_fusb302
 
 #define TCPC_CONFIG_FUSB302(id) \
-	{                                              \
-		.bus_type = EC_BUS_TYPE_I2C,           \
-		.i2c_info = {                          \
-			.port = I2C_PORT_BY_DEV(id),   \
-			.addr_flags = DT_REG_ADDR(id), \
-		},                                     \
-		.drv = &fusb302_tcpm_drv,              \
+	{                                                             \
+		.bus_type = EC_BUS_TYPE_I2C,                          \
+		.i2c_info = {                                         \
+			.port = I2C_PORT_BY_DEV(id),                  \
+			.addr_flags = DT_REG_ADDR(id),                \
+		},                                                    \
+		.drv = &fusb302_tcpm_drv,                             \
+		.alert_signal = GPIO_SIGNAL(DT_PHANDLE(id, int_pin)), \
 	},
