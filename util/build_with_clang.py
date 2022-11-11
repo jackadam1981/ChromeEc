a00d47ab5d4dd9bc77518c30e2c5465761ecc72e
Tom Hughes <tomhughes@chromium.org>
2022-11-11 10:39:33 -0800

    board/garg: Free up more flash
    BRANCH=none
    BUG=b:172020503
    TEST=CC=clang make BOARD=garg
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I68f8396fad4025901b85235c1281942db312bae8

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -145,6 +145,7 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "fleex",
     "foob",
     "gaelin",
+    "garg",
     "gelarshie",
     "genesis",
     "gimble",
@@ -295,7 +296,6 @@ BOARDS_THAT_FAIL_WITH_CLANG = [
     "bobba",  # overflows flash
     "coachz",  # overflows flash
     "corori2",  # overflows flash
-    "garg",  # overflows flash
     "mushu",  # overflows flash
     "terrador",  # overflows flash
     "volteer",  # overflows flash
