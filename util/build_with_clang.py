84daf93dd5656838f2a3359052913ca144595eb2
Tom Hughes <tomhughes@chromium.org>
2022-11-11 13:07:19 -0800

    board/volteer: Free flash space
    BRANCH=none
    BUG=b:172020503
    TEST=CC=clang make BOARD=volteer
    
    Cq-Depend:
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: Iefec35f2dccf4444d0e97050b53eee28ec476176

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -228,6 +228,7 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "voema",
     "volet",
     "volmar",
+    "volteer",
     "volteer_npcx797fc",
     "voxel",
     "voxel_ecmodeentry",
@@ -296,8 +297,6 @@ RISCV_BOARDS = [
 BOARDS_THAT_FAIL_WITH_CLANG = [
     # Boards that use CHIP:=stm32 *and* CHIP_FAMILY:=stm32f0
     "kodama",  # overflows flash
-    # Boards that use CHIP:=npcx
-    "volteer",  # overflows flash
 ]
 
 # TODO(b/201311714): NDS32 is not supported by LLVM.
