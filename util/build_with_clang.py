9daf4634861808c13e1cfab488e40da3a1de0900
Tom Hughes <tomhughes@chromium.org>
2022-11-11 12:01:26 -0800

    board/terrador: Free up more flash space
    BRANCH=none
    BUG=b:172020503
    TEST=CC=clang make BOARD=terrador
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I3f360891e9f005120b3f7d7a0f84f81467630327

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -219,6 +219,7 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "stryke",
     "taeko",
     "taniks",
+    "terrador",
     "treeya",
     "trembyle",
     "trogdor",
@@ -296,7 +297,6 @@ BOARDS_THAT_FAIL_WITH_CLANG = [
     # Boards that use CHIP:=stm32 *and* CHIP_FAMILY:=stm32f0
     "kodama",  # overflows flash
     # Boards that use CHIP:=npcx
-    "terrador",  # overflows flash
     "volteer",  # overflows flash
 ]
 
