aab4669262b5f502f60f147169494e75fab4aa83
Richard Yeh <rcy@google.com>
2022-11-12 13:55:41 +0000

    fan: Refactor and test the most common custom fan_percent_to_rpm.
    This is in preparation for reusing this implementation for
    {ambassador, genesis, moonbuggy, scout}.
    
    The below-named boards are all the boards that have defined
    CONFIG_FAN_RPM_CUSTOM.
    
    {kalista, berknip, chronicler, dewatt, endeavour, ezkinil, fizz} have
    similar implementations of a path-dependent fan_percent_to_rpm
    function. The path-dependence enables hysteresis, typically to avoid
    oscillations in temperature and fan speed.  (fizz includes excelsior)
    
    {chronicler} performs boxcar smoothing of the input.
    
    {osiris} has two fans, and other boards have multiple sensors, and we
    have left their refactoring for another time.
    
    BRANCH=refactor-fan-percent-to-rpm
    BUG=b:252966838,b:191187610,chromium:1383859
    TEST=make -j buildall && make -j runhosttests
    
    Signed-off-by: Richard Yeh <rcy@google.com>
    Change-Id: I50ad4d78ac1145f92573a417646c1f57b8945463

--- board/fizz/board.c
+++ board/fizz/board.c
@@ -599,16 +599,12 @@ const struct pwm_t pwm_channels[] = {
 };
 BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
 
-struct fan_step {
-	int on;
-	int off;
-	int rpm;
-};
+typedef struct fan_step_1_1 fan_step;
 
-static const struct fan_step *fan_table;
+static const fan_step *fan_table;
 
 /* Note: Do not make the fan on/off point equal to 0 or 100 */
-static const struct fan_step fan_table0[] = {
+static const fan_step fan_table0[] = {
 	{ .on = 0, .off = 1, .rpm = 0 },
 	{ .on = 36, .off = 1, .rpm = 2800 },
 	{ .on = 58, .off = 58, .rpm = 3200 },
@@ -618,7 +614,7 @@ static const struct fan_step fan_table0[] = {
 	{ .on = 88, .off = 83, .rpm = 5200 },
 	{ .on = 98, .off = 91, .rpm = 5600 },
 };
-static const struct fan_step fan_table1[] = {
+static const fan_step fan_table1[] = {
 	{ .on = 0, .off = 1, .rpm = 0 },
 	{ .on = 36, .off = 1, .rpm = 2800 },
 	{ .on = 62, .off = 58, .rpm = 3200 },
@@ -628,7 +624,7 @@ static const struct fan_step fan_table1[] = {
 	{ .on = 88, .off = 83, .rpm = 5200 },
 	{ .on = 98, .off = 91, .rpm = 5600 },
 };
-static const struct fan_step fan_table2[] = {
+static const fan_step fan_table2[] = {
 	{ .on = 0, .off = 1, .rpm = 0 },
 	{ .on = 36, .off = 1, .rpm = 2200 },
 	{ .on = 63, .off = 56, .rpm = 2900 },
@@ -638,7 +634,7 @@ static const struct fan_step fan_table2[] = {
 	{ .on = 87, .off = 81, .rpm = 3900 },
 	{ .on = 98, .off = 91, .rpm = 5000 },
 };
-static const struct fan_step fan_table3[] = {
+static const fan_step fan_table3[] = {
 	{ .on = 0, .off = 1, .rpm = 0 },
 	{ .on = 36, .off = 22, .rpm = 2500 },
 	{ .on = 54, .off = 49, .rpm = 3200 },
@@ -814,40 +810,6 @@ DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
 int fan_percent_to_rpm(int fan, int pct)
 {
-	static int current_level;
-	static int previous_pct;
-	int i;
-
-	/*
-	 * Compare the pct and previous pct, we have the three paths :
-	 *  1. decreasing path. (check the off point)
-	 *  2. increasing path. (check the on point)
-	 *  3. invariant path. (return the current RPM)
-	 */
-	if (pct < previous_pct) {
-		for (i = current_level; i >= 0; i--) {
-			if (pct <= fan_table[i].off)
-				current_level = i - 1;
-			else
-				break;
-		}
-	} else if (pct > previous_pct) {
-		for (i = current_level + 1; i < NUM_FAN_LEVELS; i++) {
-			if (pct >= fan_table[i].on)
-				current_level = i;
-			else
-				break;
-		}
-	}
-
-	if (current_level < 0)
-		current_level = 0;
-
-	previous_pct = pct;
-
-	if (fan_table[current_level].rpm != fan_get_rpm_target(FAN_CH(fan)))
-		cprints(CC_THERMAL, "Setting fan RPM to %d",
-			fan_table[current_level].rpm);
-
-	return fan_table[current_level].rpm;
+	return fan_percent_to_rpm_path_dependent(fan_table, NUM_FAN_LEVELS, fan,
+						 pct, NULL);
 }
