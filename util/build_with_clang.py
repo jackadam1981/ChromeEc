2cc21a122ad69b11e0f7f124e28f97a740e51b36
Tom Hughes <tomhughes@chromium.org>
2022-11-11 16:46:27 -0800

    board/kodama: Free up flash space
    BRANCH=none
    BUG=b:172020503
    TEST=CC=clang make BOARD=kodama
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>
    Change-Id: I6bd5b6d3304e0bf87140cc092c37d0a32984e9d6

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -66,6 +66,7 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "kakadu",
     "kappa",
     "katsu",
+    "kodama",
     "krane",
     "kukui",
     "magnemite",
@@ -287,11 +288,9 @@ RISCV_BOARDS = [
 ]
 
 BOARDS_THAT_FAIL_WITH_CLANG = [
-    # Boards that use CHIP:=stm32 *and* CHIP_FAMILY:=stm32f0
     "fennel",  # overflows flash
     "jacuzzi",  # overflows flash
     "juniper",  # overflows flash
-    "kodama",  # overflows flash
     "makomo",  # overflows flash
     "oak",  # overflows flash
     "bobba",  # overflows flash
