56e7b9d5aed93046312b820d4093e4a434e275a2
Richard Yeh <rcy@google.com>
2022-11-12 13:55:41 +0000

    Refactor and test the most common custom fan_percent_to_rpm impl.
    This is in preparation for reusing this implementation for
    {ambassador, genesis, moonbuggy, scout}.
    
    The below-named boards are all the boards that have defined
    CONFIG_FAN_RPM_CUSTOM.
    
    {kalista, berknip, chronicler, dewatt, endeavour, ezkinil, fizz} have
    similar implementations of a path-dependent fan_percent_to_rpm
    function. The path-dependence enables hysteresis, typically to avoid
    oscillations in temperature and fan speed.  (fizz includes excelsior)
    
    {chronicler} performs boxcar smoothing of the input.
    
    {osiris} has two fans, and other boards have multiple sensors, and we
    have left their refactoring for another time.
    
    BRANCH=refactor-fan-percent-to-rpm
    BUG=b:252966838,b:191187610,chromium:1383859
    TEST=make -j buildall && make -j runhosttests
    
    Signed-off-by: Richard Yeh <rcy@google.com>
    Change-Id: I50ad4d78ac1145f92573a417646c1f57b8945463

--- board/ezkinil/board.c
+++ board/ezkinil/board.c
@@ -796,14 +796,10 @@ __maybe_unused static const struct ec_thermal_config thermal_soc = THERMAL_SOC;
 
 struct ec_thermal_config thermal_params[TEMP_SENSOR_COUNT];
 
-struct fan_step {
-	int on;
-	int off;
-	int rpm;
-};
+typedef struct fan_step_1_1 fan_step;
 
 /* Note: Do not make the fan on/off point equal to 0 or 100 */
-static const struct fan_step fan_table0[] = {
+static const fan_step fan_table0[] = {
 	{ .on = 0, .off = 1, .rpm = 0 },
 	{ .on = 9, .off = 1, .rpm = 3200 },
 	{ .on = 21, .off = 7, .rpm = 3500 },
@@ -816,7 +812,7 @@ static const struct fan_step fan_table0[] = {
 /* All fan tables must have the same number of levels */
 #define NUM_FAN_LEVELS ARRAY_SIZE(fan_table0)
 
-static const struct fan_step *fan_table = fan_table0;
+static const fan_step *fan_table = fan_table0;
 
 static void setup_fans(void)
 {
@@ -827,43 +823,8 @@ DECLARE_HOOK(HOOK_INIT, setup_fans, HOOK_PRIO_DEFAULT);
 
 int fan_percent_to_rpm(int fan, int pct)
 {
-	static int current_level;
-	static int previous_pct;
-	int i;
-	/*
-	 * Compare the pct and previous pct, we have the three paths :
-	 *  1. decreasing path. (check the off point)
-	 *  2. increasing path. (check the on point)
-	 *  3. invariant path. (return the current RPM)
-	 */
-	if (pct < previous_pct) {
-		for (i = current_level; i >= 0; i--) {
-			if (pct <= fan_table[i].off)
-				current_level = i - 1;
-			else
-				break;
-		}
-	} else if (pct > previous_pct) {
-		for (i = current_level + 1; i < NUM_FAN_LEVELS; i++) {
-			if (pct >= fan_table[i].on)
-				current_level = i;
-			else
-				break;
-		}
-	}
-
-	if (current_level < 0)
-		current_level = 0;
-
-	previous_pct = pct;
-
-	if (fan_table[current_level].rpm != fan_get_rpm_target(FAN_CH(fan))) {
-		cprints(CC_THERMAL, "Setting fan RPM to %d",
-			fan_table[current_level].rpm);
-		board_print_temps();
-	}
-
-	return fan_table[current_level].rpm;
+	return fan_percent_to_rpm_path_dependent(fan_table, NUM_FAN_LEVELS, fan,
+						 pct, board_print_temps);
 }
 
 __override void board_set_charge_limit(int port, int supplier, int charge_ma,
