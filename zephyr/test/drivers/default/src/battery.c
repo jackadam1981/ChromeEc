245c332b6a44072efd04a09008adee6134afd72a
Yuval Peress <peress@google.com>
2022-11-14 10:36:44 -0700

    test: add tests for battery_fuel_gauge
    Add error handling tests for fuel gauge.
    
    BRANCH=none
    BUG=none
    TEST=twister
    
    Signed-off-by: Yuval Peress <peress@google.com>
    Change-Id: I87552b936184c6152c4be4a64bd42278ce5b5ca2

--- zephyr/test/drivers/default/src/battery.c
+++ zephyr/test/drivers/default/src/battery.c
@@ -9,10 +9,13 @@
 #include <zephyr/drivers/gpio/gpio_emul.h>
 
 #include "battery.h"
+#include "battery_fuel_gauge.h"
 #include "test/drivers/test_state.h"
 
 #define GPIO_BATT_PRES_ODL_PATH DT_PATH(named_gpios, ec_batt_pres_odl)
 #define GPIO_BATT_PRES_ODL_PORT DT_GPIO_PIN(GPIO_BATT_PRES_ODL_PATH, gpios)
+
+bool authenticate_battery_type(int index, const char *manuf_name);
 
 static void battery_after(void *data)
 {
@@ -35,6 +38,25 @@ ZTEST_USER(battery, test_battery_is_present_gpio)
 	/* ec_batt_pres_odl = 1 means battery missing. */
 	zassert_ok(gpio_emul_input_set(dev, GPIO_BATT_PRES_ODL_PORT, 1));
 	zassert_equal(BP_NO, battery_is_present());
+}
+
+ZTEST(battery, test_authenticate_battery_type)
+{
+	/* Invalid index */
+	zassert_false(authenticate_battery_type(BATTERY_TYPE_COUNT, NULL));
+	/* Use fuel-gauge 1's manufacturer name for index 0 */
+	zassert_false(authenticate_battery_type(
+		0, board_battery_info[1].fuel_gauge.manuf_name));
+	/* Use the correct manufacturer name, but wrong device name (because the
+	 * index is 1 and not 0)
+	 */
+	zassert_false(authenticate_battery_type(
+		1, board_battery_info[1].fuel_gauge.manuf_name));
+}
+
+ZTEST(battery, test_board_get_default_battery_type)
+{
+	zassert_equal(DEFAULT_BATTERY_TYPE, board_get_default_battery_type());
 }
 
 ZTEST_SUITE(battery, drivers_predicate_post_main, NULL, NULL, battery_after,
