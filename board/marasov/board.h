b9e5e3ee7aa4a322671483504afd570f79a18411
Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
2022-11-14 09:13:55 +0800

    marasov: Config the EC battery setting.
    Config the EC battery setting depend on battery spec.
    
    BUG=none
    BRANCH=firmware-brya-14505.B
    TEST=make BOARD=marasov
    
    Signed-off-by: Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
    Change-Id: I04a6e3d96780cbffd44b83f6453d40bb577564eb

--- board/marasov/board.h
+++ board/marasov/board.h
@@ -90,6 +90,13 @@
 #define CONFIG_USB_PD_TCPM_PS8815_FORCE_DID
 #define CONFIG_USBC_RETIMER_INTEL_BB
 
+/* Battery Configuration */
+#define CONFIG_SMBUS_PEC
+#undef CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE
+#define CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE 3
+#undef CONFIG_BATT_HOST_FULL_FACTOR
+#define CONFIG_BATT_HOST_FULL_FACTOR 99
+
 /* I2C speed console command */
 #define CONFIG_CMD_I2C_SPEED
 
@@ -246,7 +253,7 @@ enum sensor_id {
 
 enum ioex_port { IOEX_C0_NCT38XX = 0, IOEX_C2_NCT38XX, IOEX_PORT_COUNT };
 
-enum battery_type { BATTERY_POWER_TECH, BATTERY_LGC011, BATTERY_TYPE_COUNT };
+enum battery_type { BATTERY_C490, BATTERY_TYPE_COUNT };
 
 enum pwm_channel {
 	PWM_CH_LED2 = 0, /* PWM0 (white charger) */
