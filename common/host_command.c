1e964346e895a98ca92036c647ccf39300536276
Rob Barnes <robbarnes@google.com>
2022-11-15 01:46:49 +0000

    system: Implement system safe mode
    Basic implementation of system safe mode recovery.
    
    System safe mode is a recovery mode that may be started after
    a fault/panic. It allows the AP to collect info about the fault
    and system state before the system resets
    
    This CL only includes support for legacy CrOS EC
    
    BUG=b:249128225
    BRANCH=None
    TEST=Manually tested on octopus
    
    Orig-Change-Id: I15139bb082011485b54e4ca7813839940bf5401a
    Orig-Signed-off-by: Rob Barnes <robbarnes@google.com>
    Orig-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4029604
    Orig-Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I8abd563b82b611dafbc9fe1fda05ac6ade2b7c91
    Signed-off-by: Rob Barnes <robbarnes@google.com>

--- common/host_command.c
+++ common/host_command.c
@@ -14,6 +14,7 @@
 #include "lpc.h"
 #include "shared_mem.h"
 #include "system.h"
+#include "system_safe_mode.h"
 #include "task.h"
 #include "timer.h"
 #include "util.h"
@@ -378,6 +379,10 @@ host_packet_bad:
  */
 static const struct host_command *find_host_command(int command)
 {
+	if (IS_ENABLED(CONFIG_SYSTEM_SAFE_MODE) && system_is_in_safe_mode()) {
+		if (!command_is_allowed_in_safe_mode(command))
+			return NULL;
+	}
 #ifdef CONFIG_HOSTCMD_SECTION_SORTED
 	const struct host_command *l, *r, *m;
 	uint32_t num;
