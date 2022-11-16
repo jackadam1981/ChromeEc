fc866aaf0a1ed7e66e04614617447da2cb66475d
Al Semjonovs <asemjonovs@google.com>
2022-11-16 14:12:58 -0700

    twister: Add config to tags for tests
    Parse autoconf.h to add testsuite configuration as
    tags for the test
    
    BUG=b:239952573
    BRANCH=NONE
    TEST=./twister -T zephyr/test
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: I964c0e7e1f5c50435075e5c76fb29426eb54d184

--- util/zephyr_to_resultdb.py
+++ util/zephyr_to_resultdb.py
@@ -14,6 +14,7 @@ import base64
 import datetime
 import json
 import os
+import re
 
 import requests  # pylint: disable=import-error
 
@@ -83,8 +84,10 @@ def testcase_artifact(testcase):
     return base64.b64encode(artifact.encode())
 
 
-def testcase_to_result(testsuite, testcase, base_tags):
-    """Translates ZTEST testcase to ResultDB format"""
+def testcase_to_result(testsuite, testcase, base_tags, config_tags):
+    """Translates ZTEST testcase to ResultDB format
+    https://crsrc.org/i/go/src/go.chromium.org/luci/resultdb/sink/proto/v1/test_result.proto;l=31
+    """
     result = {
         "testId": testcase["identifier"],
         "status": translate_status(testcase["status"]),
@@ -95,9 +98,7 @@ def testcase_to_result(testsuite, testcase, base_tags):
                 "contents": testcase_artifact(testcase),
             }
         },
-        # TODO(b/239952573) Add all test configs as tags
         "tags": [
-            {"key": "category", "value": "ChromeOS/EC"},
             {"key": "platform", "value": testsuite["platform"]},
             {"key": "suite", "value": testsuite["name"]},
         ],
@@ -108,7 +109,26 @@ def testcase_to_result(testsuite, testcase, base_tags):
     for (key, value) in base_tags:
         result["tags"].append({"key": key, "value": value})
 
+    for (key, value) in config_tags:
+        result["tags"].append({"key": key.lower(), "value": value})
+
     return result
+
+
+def get_testsuite_config_tags(twister_dir, testsuite):
+    """Creates config tags from the testsuite"""
+    config_tags = []
+    suite_path = f"{twister_dir}/{testsuite['platform']}/{testsuite['name']}"
+    autoconf_h = f"{suite_path}/zephyr/include/generated/autoconf.h"
+
+    with open(autoconf_h) as file:
+        lines = file.readlines()
+
+        for line in lines:
+            result = re.search(r"#define\s*(\w+)\s*(.+$)", line)
+            config_tags.append((result.group(1), result.group(2)))
+
+    return config_tags
 
 
 def create_base_tags(data):
@@ -137,10 +157,15 @@ def json_to_resultdb(result_file):
         base_tags = create_base_tags(data)
 
         for testsuite in data["testsuites"]:
+            config_tags = get_testsuite_config_tags(
+                os.path.dirname(result_file), testsuite
+            )
             for testcase in testsuite["testcases"]:
                 if testcase["status"]:
                     results.append(
-                        testcase_to_result(testsuite, testcase, base_tags)
+                        testcase_to_result(
+                            testsuite, testcase, base_tags, config_tags
+                        )
                     )
 
         file.close()
