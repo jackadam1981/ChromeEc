cc21c69c8e4cc63b9d89126fc9897036024cdfd5
Matt Wang <matt_wang@compal.corp-partner.google.com>
2022-11-16 09:39:14 +0800

    xivu: add 2nd source for motionsensors
    Follow the FW_CONFIG to setup the motionsensor chip.
    
    FW_BASE_GYRO [7:6]
    0: NONE
    1: LSM6DSO
    2: BMI323
    
    FW_LID_G [5:4]
    0: NONE
    1: LIS2DW12
    2: BMA422
    
    BUG=b:257174577
    BRANCH=none
    TEST=zmake build xivu
    
    Signed-off-by: Matt Wang <matt_wang@compal.corp-partner.google.com>
    Change-Id: Ib7f42441625232424e15c0d0a82de1a315ea5ede

---
+++ zephyr/program/nissa/xivu/src/form_factor.c
@@ -0,0 +1,89 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
+
+#include "accelgyro.h"
+#include "button.h"
+#include "cros_board_info.h"
+#include "cros_cbi.h"
+#include "driver/accel_bma4xx.h"
+#include "driver/accel_lis2dw12_public.h"
+#include "driver/accelgyro_bmi323.h"
+#include "driver/accelgyro_lsm6dso.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "motionsense_sensors.h"
+#include "motion_sense.h"
+#include "tablet_mode.h"
+
+#include "nissa_common.h"
+
+LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
+
+/*
+ * Mainboard orientation support.
+ */
+
+#define LIS_ALT_MAT SENSOR_ROT_STD_REF_NAME(DT_NODELABEL(lid_rot_bma422))
+#define BMA_ALT_MAT SENSOR_ROT_STD_REF_NAME(DT_NODELABEL(lid_rot_ref))
+#define ALT_MAT SENSOR_ROT_STD_REF_NAME(DT_NODELABEL(base_rot_ver1))
+#define LID_SENSOR SENSOR_ID(DT_NODELABEL(lid_accel))
+#define BASE_SENSOR SENSOR_ID(DT_NODELABEL(base_accel))
+#define BASE_GYRO SENSOR_ID(DT_NODELABEL(base_gyro))
+#define ALT_LID_S SENSOR_ID(DT_NODELABEL(alt_lid_accel))
+
+static bool use_alt_sensor;
+static bool use_alt_lid_accel;
+
+void motion_interrupt(enum gpio_signal signal)
+{
+	if (use_alt_sensor)
+		bmi3xx_interrupt(signal);
+	else
+		lsm6dso_interrupt(signal);
+}
+
+void lid_accel_interrupt(enum gpio_signal signal)
+{
+	if (use_alt_lid_accel)
+		bma4xx_interrupt(signal);
+	else
+		lis2dw12_interrupt(signal);
+}
+
+static void form_factor_init(void)
+{
+	int ret;
+	uint32_t val, sb;
+
+	cros_cbi_get_fw_config(FW_SUB_BOARD, &sb);
+
+	ret = cbi_get_board_version(&val);
+	if (ret != EC_SUCCESS) {
+		LOG_ERR("Error retrieving CBI BOARD_VER.");
+		return;
+	}
+
+	/*
+	 * If the board version is 1
+	 * use ver1 rotation matrix.
+	 */
+	if (val == 1) {
+		LOG_INF("Switching to ver1 base");
+		motion_sensors[BASE_SENSOR].rot_standard_ref = &ALT_MAT;
+		motion_sensors[BASE_GYRO].rot_standard_ref = &ALT_MAT;
+	}
+
+	/* check which motion sensors are used */
+	use_alt_sensor = cros_cbi_ssfc_check_match(
+		CBI_SSFC_VALUE_ID(DT_NODELABEL(base_sensor_1)));
+	use_alt_lid_accel = cros_cbi_ssfc_check_match(
+		CBI_SSFC_VALUE_ID(DT_NODELABEL(lid_sensor_1)));
+
+	motion_sensors_check_ssfc();
+}
+DECLARE_HOOK(HOOK_INIT, form_factor_init, HOOK_PRIO_POST_I2C);
