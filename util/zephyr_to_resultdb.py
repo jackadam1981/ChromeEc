a9cfb3c5d942931c4aa9b34eeba89d615a42b9a5
Al Semjonovs <asemjonovs@google.com>
2022-11-16 14:09:55 -0700

    twister: Add tags to enable Testhaus
    Add queued_time and board tags to test results
    
    BUG=b:247857971
    BRANCH=None
    TEST=./twister -T zephyr/test
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>
    Change-Id: Ia252aa72fa433e13ec45e4d9470d96370888ad00

--- util/zephyr_to_resultdb.py
+++ util/zephyr_to_resultdb.py
@@ -11,6 +11,7 @@
 
 import argparse
 import base64
+import datetime
 import json
 import os
 
@@ -82,7 +83,7 @@ def testcase_artifact(testcase):
     return base64.b64encode(artifact.encode())
 
 
-def testcase_to_result(testsuite, testcase):
+def testcase_to_result(testsuite, testcase, base_tags):
     """Translates ZTEST testcase to ResultDB format"""
     result = {
         "testId": testcase["identifier"],
@@ -98,12 +99,34 @@ def testcase_to_result(testsuite, testcase):
         "tags": [
             {"key": "category", "value": "ChromeOS/EC"},
             {"key": "platform", "value": testsuite["platform"]},
+            {"key": "suite", "value": testsuite["name"]},
         ],
         "duration": translate_duration(testcase),
         "testMetadata": {"name": testcase["identifier"]},
     }
 
+    for (key, value) in base_tags:
+        result["tags"].append({"key": key, "value": value})
+
     return result
+
+
+def create_base_tags(data):
+    """Creates base tags needed for TestHaus"""
+    base_tags = []
+
+    queued_time = datetime.datetime.fromisoformat(
+        data["environment"]["run_date"]
+    )
+    base_tags.append(
+        ("queued_time", queued_time.strftime("%Y-%m-%d %H:%M:%S.%f UTC"))
+    )
+
+    base_tags.append(("zephyr_version", data["environment"]["zephyr_version"]))
+    base_tags.append(("board", data["environment"]["os"]))
+    base_tags.append(("toolchain", data["environment"]["toolchain"]))
+
+    return base_tags
 
 
 def json_to_resultdb(result_file):
@@ -111,11 +134,14 @@ def json_to_resultdb(result_file):
     with open(result_file) as file:
         data = json.load(file)
         results = []
+        base_tags = create_base_tags(data)
 
         for testsuite in data["testsuites"]:
             for testcase in testsuite["testcases"]:
                 if testcase["status"]:
-                    results.append(testcase_to_result(testsuite, testcase))
+                    results.append(
+                        testcase_to_result(testsuite, testcase, base_tags)
+                    )
 
         file.close()
 
