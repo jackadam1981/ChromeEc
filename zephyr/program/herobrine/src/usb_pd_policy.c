c90f521ac4113cbbbda4581ea263b26efb105409
Wai-Hong Tam <waihong@google.com>
2022-11-18 14:42:24 -0800

    zephyr: herobrine: Pass through AUX termination when DP-Config is acked
    The hardware block diagram is like:
    
              AUX termination
                     |
                +----+--- TCPC MUX --- PPC SW --- Type-C connector (port-0)
    DP PHY -- DP MUX
                +----+--- TCPC MUX --- PPC SW --- Type-C connector (port-1)
                     |
              AUX termination
    
    The current implementation muxes the path end-to-end on a HPD high event.
    
    The dependencies are:
     * Chromebook muxing the AUX channel depends on a HPD high event,
     * DP sink raising HPD depends on a cable connection detected (auto-probe),
     * Successful probe of a cable connection depends on AUX termination,
    which cause a dead-lock.
    
    This change breaks the dead-lock. During the DP-Config phase, when the
    DP-Config VDM is acked, connect the PPC SW and TCPC MUX, but leave the DP
    MUX disconnected. So the DP sink can detect the correct AUX termination.
    
    BRANCH=None
    BUG=b:247005613
    TEST=Connect the device through a Type-C dongle to a monitor and verify
    the screen. Try the combinations:
     * Plug C0 monitor -> plug C0 dongle -> screen on C0 monitor
     * Plug C0 dongle -> plug C0 monitor -> screen on C0 monitor
       -> unplug C0 monitor -> plug C0 monitor -> screen on C0 monitor
     * Plug C0 dongle -> plug C0 monitor -> screen on C0 monitor
       -> plug C1 dongle -> -> plug C1 monitor -> no screen on C1 monitor
       -> unplug C0 monitor -> replug C1 monitor -> screen on C0 monitor
     * Plug C0 dongle -> plug C1 dongle -> plug C1 monitor
       -> screen on C1 monitor -> unplug C1 dongle -> plug C0 monitor
       -> screen on C0 monitor
    
    Change-Id: I5a5e08dc26ebaa22ff7a3cfc0828ed2e01df3995
    Signed-off-by: Wai-Hong Tam <waihong@google.com>

--- zephyr/program/herobrine/src/usb_pd_policy.c
+++ zephyr/program/herobrine/src/usb_pd_policy.c
@@ -105,6 +105,23 @@ __override int svdm_dp_config(int port, uint32_t *payload)
 __override void svdm_dp_post_config(int port)
 {
 	dp_flags[port] |= DP_FLAGS_DP_ON;
+
+	/*
+	 * Connect the SBU lines in PPC chip such that the AUX termination
+	 * can be passed through.
+	 */
+	if (IS_ENABLED(CONFIG_USBC_PPC_SBU))
+		ppc_set_sbu(port, 1);
+
+	/*
+	 * Connect the USB SS/DP lines in TCPC chip.
+	 *
+	 * When mf_pref not true, still use the dock muxing
+	 * because of the board USB-C topology (limited to 2
+	 * lanes DP).
+	 */
+	usb_mux_set(port, USB_PD_MUX_DOCK, USB_SWITCH_CONNECT,
+		    polarity_rm_dts(pd_get_polarity(port)));
 }
 
 /**
@@ -165,31 +182,12 @@ __override int svdm_dp_attention(int port, uint32_t *payload)
 				port == 1);
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_dp_mux_oe_l), 0);
 
-		/* Connect the SBU lines in PPC chip. */
-		if (IS_ENABLED(CONFIG_USBC_PPC_SBU))
-			ppc_set_sbu(port, 1);
 
-		/*
-		 * Connect the USB SS/DP lines in TCPC chip.
-		 *
-		 * When mf_pref not true, still use the dock muxing
-		 * because of the board USB-C topology (limited to 2
-		 * lanes DP).
-		 */
-		usb_mux_set(port, USB_PD_MUX_DOCK, USB_SWITCH_CONNECT,
-			    polarity_rm_dts(pd_get_polarity(port)));
 	} else {
 		/* Disconnect the DP port selection mux. */
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_dp_mux_oe_l), 1);
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_dp_mux_sel), 0);
 
-		/* Disconnect the SBU lines in PPC chip. */
-		if (IS_ENABLED(CONFIG_USBC_PPC_SBU))
-			ppc_set_sbu(port, 0);
-
-		/* Disconnect the DP but keep the USB SS lines in TCPC chip. */
-		usb_mux_set(port, USB_PD_MUX_USB_ENABLED, USB_SWITCH_CONNECT,
-			    polarity_rm_dts(pd_get_polarity(port)));
 	}
 
 	if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND) && (irq || lvl))
