96c06d79767a71371da48631a37666b1021d91ec
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2022-11-22 13:30:42 +0800

    WIP: zephyr/program/geralt_kb: Reference build for it82xx2 chip
    3/16: add cros_kb_raw pinctrl
    3/20: add fakes
    3/21: UART2 test
    
    TEST=zmake build geralt_kb --clobber
    TEST=test items as follows:
         UART: Console command can read and write.
         GPIO: The setting of gpio.dtsi are correct.
         I2C:  console cmd: $i2c write I2C_SENSOR 0x52 0x11 0x22
               I2C0:test OK!; I2C2:test OK!(need to set bit7@GCR7=0)
         ADC:  console cmd: $adc
               ec_id = 205 mV, test OK!
         PWM:  console cmd: $kblight 30
               connect LA and measure duty clcle. test OK!
    
    
    Change-Id: I3551d1fe20900e1abade3904481efddf467d4ff3
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

---
+++ zephyr/program/geralt_kb/src/fakes.c
@@ -0,0 +1,18 @@
+#include "gpio_signal.h"
+
+void touchpad_interrupt(enum gpio_signal signal)
+{
+}
+
+void keyboard_clear_buffer(void)
+{
+	/* release all keys? */
+}
+void clear_typematic_key(void)
+{
+}
+
+void keyboard_state_changed(int row, int col, int is_pressed)
+{
+
+}
