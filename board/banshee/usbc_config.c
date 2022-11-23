52c1ea64235369ace423fd648d580c857db72c6f
johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
2022-11-23 09:58:24 +0800

    Banshee: Add condition to set 'DP CONNECTION' bit
    Set 'DP CONNECTION' bit when mux_state gets HPD event only on
    the ports with HDMI/DP card
    
    It will increase BBR power consumption, so set 'DP CONNECTION'
    bit only when mux_state gets HPD event only on the ports with
    HDMI/DP card.
    
    BUG=b:233975818
    BRANCH=None
    TEST=make BOARD=banshee
    
    Signed-off-by: johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
    Change-Id: I87b2a4379e66e08830c556d703390a7fb96d3258

--- board/banshee/usbc_config.c
+++ board/banshee/usbc_config.c
@@ -16,6 +16,7 @@
 #include "driver/ppc/nx20p348x.h"
 #include "driver/ppc/syv682x_public.h"
 #include "driver/retimer/bb_retimer_public.h"
+#include "driver/retimer/bb_retimer.h"
 #include "driver/tcpm/nct38xx.h"
 #include "driver/tcpm/ps8xxx_public.h"
 #include "driver/tcpm/tcpci.h"
@@ -112,6 +113,75 @@ BUILD_ASSERT(ARRAY_SIZE(ppc_chips) == USBC_PORT_COUNT);
 
 unsigned int ppc_cnt = ARRAY_SIZE(ppc_chips);
 
+static mux_state_t bb_mux_state[CONFIG_USB_PD_PORT_MAX_COUNT];
+
+void board_bb_retimer_hpd_update(const struct usb_mux *me,
+				 mux_state_t hpd_state, bool *ack_required)
+{
+	uint32_t retimer_con_reg = 0;
+	int port = me->usb_port;
+	uint16_t USB_VID = pd_get_identity_vid(port);
+	uint16_t USB_PID = pd_get_identity_pid(port);
+
+	/* This driver does not use host command ACKs */
+	*ack_required = false;
+
+	bb_mux_state[port] = (bb_mux_state[port] & ~MUX_STATE_HPD_UPDATE_MASK) |
+			     (hpd_state & MUX_STATE_HPD_UPDATE_MASK);
+
+	if (bb_retimer_read(me, BB_RETIMER_REG_CONNECTION_STATE,
+			    &retimer_con_reg) != EC_SUCCESS) {
+		return;
+	}
+	/*
+	 * Bit 14: IRQ_HPD (ignored if BIT8 = 0)
+	 * 0 - No IRQ_HPD
+	 * 1 - IRQ_HPD received
+	 */
+	if (hpd_state & USB_PD_MUX_HPD_IRQ)
+		retimer_con_reg |= BB_RETIMER_IRQ_HPD;
+	else
+		retimer_con_reg &= ~BB_RETIMER_IRQ_HPD;
+
+	/*
+	 * Bit 15: HPD_LVL (ignored if BIT8 = 0)
+	 * 0 - HPD_State Low
+	 * 1 - HPD_State High
+	 */
+
+	if (hpd_state & USB_PD_MUX_HPD_LVL) {
+		retimer_con_reg |= BB_RETIMER_HPD_LVL;
+		if (((USB_PID == USB_PID_FRAMEWORK_HDMI_CARD) ||
+		     (USB_PID == USB_PID_FRAMEWORK_DP_CARD)) &&
+		    (USB_VID == USB_VID_FRAMEWORK))
+			retimer_con_reg |= BB_RETIMER_DP_CONNECTION;
+	} else
+		retimer_con_reg &=
+			~(BB_RETIMER_HPD_LVL | BB_RETIMER_DP_CONNECTION);
+
+	/* Writing the register4 */
+	bb_retimer_write(me, BB_RETIMER_REG_CONNECTION_STATE, retimer_con_reg);
+}
+
+int board_virtual_mux_set(const struct usb_mux *me, mux_state_t mux_state)
+{
+	int rv;
+	uint32_t retimer_con_reg = 0;
+
+	rv = bb_retimer_read(me, BB_RETIMER_REG_CONNECTION_STATE,
+			     &retimer_con_reg);
+	if (rv != EC_SUCCESS) {
+		return rv;
+	}
+
+	retimer_con_reg &= ~BB_RETIMER_DP_CONNECTION;
+
+	/* Writing the register4 */
+	rv = bb_retimer_write(me, BB_RETIMER_REG_CONNECTION_STATE,
+			      retimer_con_reg);
+	return rv;
+}
+
 /* USBC mux configuration - Alder Lake includes internal mux */
 static const struct usb_mux_chain usbc0_tcss_usb_mux = {
 	.mux =
@@ -159,9 +229,10 @@ const struct usb_mux_chain usb_muxes[] = {
 			 */
 			.flags = USB_MUX_FLAG_CAN_IDLE,
 			.driver = &bb_usb_retimer,
-			.hpd_update = bb_retimer_hpd_update,
+			.hpd_update = board_bb_retimer_hpd_update,
 			.i2c_port = I2C_PORT_USB_C0_C1_MUX,
 			.i2c_addr_flags = USBC_PORT_C0_BB_RETIMER_I2C_ADDR,
+			.board_set = &board_virtual_mux_set,
 		},
 		.next = &usbc0_tcss_usb_mux,
 	},
@@ -170,9 +241,10 @@ const struct usb_mux_chain usb_muxes[] = {
 			.usb_port = USBC_PORT_C1,
 			.flags = USB_MUX_FLAG_CAN_IDLE,
 			.driver = &bb_usb_retimer,
-			.hpd_update = bb_retimer_hpd_update,
+			.hpd_update = board_bb_retimer_hpd_update,
 			.i2c_port = I2C_PORT_USB_C0_C1_MUX,
 			.i2c_addr_flags = USBC_PORT_C1_BB_RETIMER_I2C_ADDR,
+			.board_set = &board_virtual_mux_set,
 		},
 		.next = &usbc1_tcss_usb_mux,
 	},
@@ -181,9 +253,10 @@ const struct usb_mux_chain usb_muxes[] = {
 			.usb_port = USBC_PORT_C2,
 			.flags = USB_MUX_FLAG_CAN_IDLE,
 			.driver = &bb_usb_retimer,
-			.hpd_update = bb_retimer_hpd_update,
+			.hpd_update = board_bb_retimer_hpd_update,
 			.i2c_port = I2C_PORT_USB_C2_C3_MUX,
 			.i2c_addr_flags = USBC_PORT_C2_BB_RETIMER_I2C_ADDR,
+			.board_set = &board_virtual_mux_set,
 		},
 		.next = &usbc2_tcss_usb_mux,
 	},
@@ -192,9 +265,10 @@ const struct usb_mux_chain usb_muxes[] = {
 			.usb_port = USBC_PORT_C3,
 			.flags = USB_MUX_FLAG_CAN_IDLE,
 			.driver = &bb_usb_retimer,
-			.hpd_update = bb_retimer_hpd_update,
+			.hpd_update = board_bb_retimer_hpd_update,
 			.i2c_port = I2C_PORT_USB_C2_C3_MUX,
 			.i2c_addr_flags = USBC_PORT_C3_BB_RETIMER_I2C_ADDR,
+			.board_set = &board_virtual_mux_set,
 		},
 		.next = &usbc3_tcss_usb_mux,
 	},
