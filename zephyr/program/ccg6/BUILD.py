faff9ad16d2b5073b247cb20f3367d0bb3b61b0f
Vijay Hiremath <vijay.p.hiremath@intel.com>
2022-11-22 19:32:26 -0800

    ccg8 base code
    Change-Id: I669fc918041c2978669af59c3c66694e8bbe6928

---
+++ zephyr/program/ccg6/BUILD.py
@@ -0,0 +1,11 @@
+# Copyright 2022 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""CCG6 example project."""
+
+register_npcx_project(
+    project_name="ccg6",
+    zephyr_board="npcx9m3f",
+    dts_overlays=[here / "gpio.dts"],
+)
