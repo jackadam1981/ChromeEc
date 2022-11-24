336fb10300c44db77700b02cbce5737678a2675c
Leila Lin <leilacy_lin@compal.corp-partner.google.com>
2022-11-25 01:09:20 +0800

    winterhold: Notify host when temperature change
    Send "EC_HOST_EVENT_THERMAL_THRESHOLD" event to host when
    temp_sensor_amb's value changes +/-1 deg C.
    
    LOW_COVERAGE_REASON=no unit test for skyrim board yet: b/247151116
    BRANCH=none
    BUG=b:232946420
    TEST=zmake build winterhold and confirm event
    "EC_HOST_EVENT_THERMAL_THRESHOLD" is send when temp sensor's value
    changes.
    
    Change-Id: I97c64cc4a0e75566823556c0b957a474de15e5d2
    Signed-off-by: Leila Lin <leilacy_lin@compal.corp-partner.google.com>

---
+++ zephyr/program/skyrim/winterhold/src/thermal.c
@@ -0,0 +1,30 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "hooks.h"
+#include "temp_sensor/temp_sensor.h"
+#include "host_command.h"
+
+#define CPRINTS(format, args...) cprints(CC_THERMAL, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_THERMAL, format, ##args)
+
+#define TEMP_AMB TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_amb))
+
+static int temp = -1;
+
+/* Set SCI event to host for temperature change */
+static void detect_temp_change(void)
+{
+	int t, rv;
+
+	rv = temp_sensor_read(TEMP_AMB, &t);
+	if (rv == EC_SUCCESS) {
+		if (temp != t) {
+			temp = t;
+			host_set_single_event(EC_HOST_EVENT_THERMAL_THRESHOLD);
+		}
+	}
+}
+DECLARE_HOOK(HOOK_SECOND, detect_temp_change, HOOK_PRIO_DEFAULT);
