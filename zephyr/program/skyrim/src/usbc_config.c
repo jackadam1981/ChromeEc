98cf1be5bb97e28e429a6795e501a262bcb26f11
Matt Wang <matt_wang@compal.corp-partner.google.com>
2022-11-25 14:10:31 +0800

    skyrim: add isl9238 charger prochot initial function
    This patch allow the Skyrim board's project if using the isl9238 charger
    can change the charger prochot threshold.
    
    BUG=b:260378925
    BRANCH=none
    TEST=zmake build skyrim success
    
    Signed-off-by: Matt Wang <matt_wang@compal.corp-partner.google.com>
    Change-Id: I69a786e934518eeab7ec1f940bf73d46f76dfd01

--- zephyr/program/skyrim/src/usbc_config.c
+++ zephyr/program/skyrim/src/usbc_config.c
@@ -14,6 +14,7 @@
 #include "cros_board_info.h"
 #include "cros_cbi.h"
 #include "driver/bc12/pi3usb9201.h"
+#include "driver/charger/isl923x_public.h"
 #include "driver/charger/isl9241.h"
 #include "driver/ppc/nx20p348x.h"
 #include "driver/retimer/anx7483_public.h"
@@ -228,6 +229,15 @@ static void set_ac_prochot(void)
 }
 DECLARE_HOOK(HOOK_INIT, set_ac_prochot, HOOK_PRIO_DEFAULT);
 #endif /* CONFIG_CHARGER_ISL9241 */
+
+#ifdef CONFIG_CHARGER_ISL9238
+static void charger_prochot_init(void)
+{
+	isl923x_set_ac_prochot(CHARGER_SOLO, CONFIG_AC_PROCHOT_CURRENT_MA);
+	isl923x_set_dc_prochot(CHARGER_SOLO, CONFIG_DC_PROCHOT_CURRENT_MA);
+}
+DECLARE_HOOK(HOOK_INIT, charger_prochot_init, HOOK_PRIO_DEFAULT);
+#endif /* CONFIG_CHARGER_ISL9238 */
 
 void tcpc_alert_event(enum gpio_signal signal)
 {
