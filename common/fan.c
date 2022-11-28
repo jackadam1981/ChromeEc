6b40e76d605bf778a7a693951014bba03d18908e
Richard Yeh <rcy@google.com>
2022-11-28 02:02:16 +0000

    fan: Rewrite and test the most common custom fan_percent_to_rpm.
    temp_ratio_to_rpm_hysteresis uses a sorted fan_table containing a
    mapping of temperatures (in Kelvin) to fan rpm. This clarifies the
    direct relationship of temperature to fan speed, while reducing the
    number of static variables. The existing temp_ratio expressing the
    percentage of cooling needed (temperature from temp_fan_off to
    temp_fan_max) suggests the possibility that a
    proportional-integral-derivative (PID) feedback controller might have
    been considered; but the default implementation is purely linear
    (mapping to fan speed from min to max) and this stepwise one just
    enables hysteresis --- different speeds when the board is warming up
    than when cooling down.
    
    This refactoring is in preparation for reusing this implementation for
    {ambassador, genesis, moonbuggy, scout}.
    
    {kalista, berknip, chronicler, dewatt, endeavour, ezkinil, fizz} define
    CONFIG_FAN_RPM_CUSTOM and use this or nearly this implementation of a
    fan_percent_to_rpm with hysteresis.  The hysteresis attempts to avoid
    oscillations in temperature and fan speed.
    
    {chronicler} performs boxcar smoothing of the input.
    
    {osiris}, which has two fans, and {jinlon, redrix, and other boards
    with multiple sensors}, use a similar but not exactly the same
    implementation, and would need additional abstraction.  See:
    https://source.chromium.org/search?q=f:ec%2Fb%20fan_step%20-f:board.c
    
    BRANCH=refactor-fan-percent-to-rpm
    BUG=b:252966838,b:191187610,chromium:1383859
    TEST=make -j run-fan && make -j buildall && make -j runhosttests
    
    Signed-off-by: Richard Yeh <rcy@google.com>
    Change-Id: I50ad4d78ac1145f92573a417646c1f57b8945463

--- common/fan.c
+++ common/fan.c
@@ -57,21 +57,84 @@ void fan_set_count(int count)
  * the way down to zero because most fans won't turn that slowly, so
  * we'll map [1,100] => [FAN_MIN,FAN_MAX], and [0] => "off".
  */
-int fan_percent_to_rpm(int fan, int pct)
+int fan_percent_to_rpm(int fan_index, int temp_ratio)
 {
 	int rpm, max, min;
 
-	if (!pct) {
+	if (temp_ratio <= 0) {
 		rpm = 0;
 	} else {
-		min = fans[fan].rpm->rpm_min;
-		max = fans[fan].rpm->rpm_max;
-		rpm = ((pct - 1) * max + (100 - pct) * min) / 99;
+		min = fans[fan_index].rpm->rpm_min;
+		max = fans[fan_index].rpm->rpm_max;
+		rpm = ((temp_ratio - 1) * max + (100 - temp_ratio) * min) / 99;
 	}
 
 	return rpm;
 }
 #endif /* CONFIG_FAN_RPM_CUSTOM */
+
+int temp_ratio_to_rpm_hysteresis(const struct fan_step_1_1 *fan_table,
+				 int num_fan_levels, int temp_fan_off,
+				 int temp_fan_max, int fan_index,
+				 int temp_ratio, void (*on_change)(void))
+{
+	static int previous_temperature;
+	const int temperature =
+		temp_fan_off + (temp_fan_max - temp_fan_off) * temp_ratio / 100;
+	const int previous_rpm = fan_get_rpm_target(FAN_CH(fan_index));
+	int rpm;
+
+	if (temperature <= fan_table[0].decreasing_temp_threshold) {
+		rpm = 0;
+	} else if (previous_rpm == 0 &&
+		   temperature < fan_table[0].increasing_temp_threshold) {
+		rpm = 0;
+	} else {
+		/*
+		 * Comparing temperature and previous temperature, trichotomy:
+		 *  1. decreasing path. (check the decreasing threshold)
+		 *  2. increasing path. (check the increasing threshold)
+		 *  3. invariant path. (return the current RPM)
+		 */
+		int fan_table_index;
+
+		if (temperature < previous_temperature) {
+			fan_table_index = num_fan_levels - 1;
+			while (fan_table_index > 0 &&
+			       temperature <=
+				       fan_table[fan_table_index]
+					       .decreasing_temp_threshold) {
+				fan_table_index--;
+			}
+			rpm = fan_table[fan_table_index].rpm;
+		} else if (temperature > previous_temperature) {
+			fan_table_index = 0;
+			while (fan_table_index < num_fan_levels &&
+			       temperature >=
+				       fan_table[fan_table_index]
+					       .increasing_temp_threshold) {
+				fan_table_index++;
+			}
+			if (fan_table_index > 0) {
+				fan_table_index--;
+			}
+			rpm = fan_table[fan_table_index].rpm;
+		} else {
+			rpm = previous_rpm;
+		}
+	}
+
+	previous_temperature = temperature;
+
+	if (rpm != previous_rpm) {
+		cprints(CC_THERMAL, "Setting fan %d RPM to %d", fan_index, rpm);
+		if (on_change) {
+			on_change();
+		}
+	}
+
+	return rpm;
+}
 
 /* The thermal task will only call this function with pct in [0,100]. */
 test_mockable void fan_set_percent_needed(int fan, int pct)
--- test/fan.c
+++ test/fan.c
@@ -104,9 +104,122 @@ static int test_fan(void)
 	return EC_SUCCESS;
 }
 
+/* Provide a test driver to make test easier to read. */
+int temp_to_rpm(int temperature_c)
+{
+	const struct fan_step_1_1 fan_table[] = {
+		{ .decreasing_temp_threshold = C_TO_K(35),
+		  .increasing_temp_threshold = C_TO_K(41),
+		  .rpm = 2500 },
+		{ .decreasing_temp_threshold = C_TO_K(37),
+		  .increasing_temp_threshold = C_TO_K(43),
+		  .rpm = 3200 },
+		{ .decreasing_temp_threshold = C_TO_K(42),
+		  .increasing_temp_threshold = C_TO_K(45),
+		  .rpm = 3500 },
+		{ .decreasing_temp_threshold = C_TO_K(44),
+		  .increasing_temp_threshold = C_TO_K(47),
+		  .rpm = 3900 },
+		{ .decreasing_temp_threshold = C_TO_K(46),
+		  .increasing_temp_threshold = C_TO_K(49),
+		  .rpm = 4500 },
+		{ .decreasing_temp_threshold = C_TO_K(48),
+		  .increasing_temp_threshold = C_TO_K(52),
+		  .rpm = 5100 },
+		{ .decreasing_temp_threshold = C_TO_K(51),
+		  .increasing_temp_threshold = C_TO_K(55),
+		  .rpm = 5400 },
+	};
+	const int num_fan_levels = ARRAY_SIZE(fan_table);
+	const int temp_fan_off = C_TO_K(35);
+	const int temp_fan_max = C_TO_K(55);
+	int rpm;
+	int temp_ratio = 100 * (C_TO_K(temperature_c) - temp_fan_off) /
+			 (temp_fan_max - temp_fan_off);
+	temp_ratio = temp_ratio < 0 ? 0 : (temp_ratio > 100 ? 100 : temp_ratio);
+	rpm = temp_ratio_to_rpm_hysteresis(fan_table, num_fan_levels,
+					   temp_fan_off, temp_fan_max, 0,
+					   temp_ratio, NULL);
+	fan_set_rpm_target(FAN_CH(0), rpm);
+	return rpm;
+}
+
+static int test_temp_ratio_to_rpm_hysteresis(void)
+{
+	/* set initial value to be different so that a log message appears */
+	fan_set_rpm_target(FAN_CH(0), 5400);
+	/* initial turn-on behavior, ramp up. @ represents fan speed; + temp */
+	TEST_ASSERT(temp_to_rpm(30) == 0);    /* @+. .   40    .   50    .60 */
+	TEST_ASSERT(temp_to_rpm(30) == 0);    /* @+. .    .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(35) == 0);    /* @   +    .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(37) == 0);    /* @   . +  .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(39) == 0);    /* @   .   +.    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(40) == 0);    /* @   .    +    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(41) == 2500); /*    @.    .+   .    .    .   */
+	TEST_ASSERT(temp_to_rpm(36) == 2500); /*    @.+   .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(42) == 2500); /*    @.    . +  .    .    .   */
+	TEST_ASSERT(temp_to_rpm(43) == 3200); /*     @    .  + .    .    .   */
+	TEST_ASSERT(temp_to_rpm(38) == 3200); /*     @  + .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(44) == 3200); /*     @    .   +.    .    .   */
+	TEST_ASSERT(temp_to_rpm(45) == 3500); /*     .@   .    +    .    .   */
+	TEST_ASSERT(temp_to_rpm(43) == 3500); /*     .@   .  + .    .    .   */
+	TEST_ASSERT(temp_to_rpm(46) == 3500); /*     .@   .    .+   .    .   */
+	TEST_ASSERT(temp_to_rpm(47) == 3900); /*     . @  .    . +  .    .   */
+	TEST_ASSERT(temp_to_rpm(45) == 3900); /*     . @  .    +    .    .   */
+	TEST_ASSERT(temp_to_rpm(48) == 3900); /*     . @  .    .  + .    .   */
+	TEST_ASSERT(temp_to_rpm(49) == 4500); /*     .  @ .    .   +.    .   */
+	TEST_ASSERT(temp_to_rpm(47) == 4500); /*     .  @ .    . +  .    .   */
+	TEST_ASSERT(temp_to_rpm(51) == 4500); /*     .  @ .    .    .+   .   */
+	TEST_ASSERT(temp_to_rpm(52) == 5100); /*     .   @.    .    . +  .   */
+	TEST_ASSERT(temp_to_rpm(49) == 5100); /*     .   @.    .   +.    .   */
+	TEST_ASSERT(temp_to_rpm(54) == 5100); /*     .   @.    .    .   +.   */
+	TEST_ASSERT(temp_to_rpm(55) == 5400); /*     .    @    .    .    +   */
+	TEST_ASSERT(temp_to_rpm(52) == 5400); /*     .    @    .    . +  .   */
+	TEST_ASSERT(temp_to_rpm(60) == 5400); /*     .    @    .   50    ..+ */
+	/* cool-down */
+	TEST_ASSERT(temp_to_rpm(55) == 5400); /*     .    @    .    .    +   */
+	TEST_ASSERT(temp_to_rpm(52) == 5400); /*     .    @    .    . +  .   */
+	TEST_ASSERT(temp_to_rpm(51) == 5100); /*     .   @.    .    .+   .   */
+	TEST_ASSERT(temp_to_rpm(54) == 5100); /*     .   @.    .    .   +.   */
+	TEST_ASSERT(temp_to_rpm(49) == 5100); /*     .   @.    .   +.    .   */
+	TEST_ASSERT(temp_to_rpm(48) == 4500); /*     .  @ .    .  + .    .   */
+	TEST_ASSERT(temp_to_rpm(51) == 4500); /*     .  @ .    .    .+   .   */
+	TEST_ASSERT(temp_to_rpm(47) == 4500); /*     .  @ .    . +  .    .   */
+	TEST_ASSERT(temp_to_rpm(46) == 3900); /*     . @  .    .+   .    .   */
+	TEST_ASSERT(temp_to_rpm(48) == 3900); /*     . @  .    .  + .    .   */
+	TEST_ASSERT(temp_to_rpm(45) == 3900); /*     . @  .    +    .    .   */
+	TEST_ASSERT(temp_to_rpm(44) == 3500); /*     .@   .   +.    .    .   */
+	TEST_ASSERT(temp_to_rpm(46) == 3500); /*     .@   .    .+   .    .   */
+	TEST_ASSERT(temp_to_rpm(43) == 3500); /*     .@   .  + .    .    .   */
+	TEST_ASSERT(temp_to_rpm(42) == 3200); /*     @    . +  .    .    .   */
+	TEST_ASSERT(temp_to_rpm(44) == 3200); /*     @    .   +.    .    .   */
+	TEST_ASSERT(temp_to_rpm(38) == 3200); /*     @  + .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(37) == 2500); /*    @. +  .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(42) == 2500); /*    @.    . +  .    .    .   */
+	TEST_ASSERT(temp_to_rpm(36) == 2500); /*    @.+   .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(35) == 0);    /* @   +   40    .   50    .   */
+	/* warm up again */
+	TEST_ASSERT(temp_to_rpm(38) == 0);    /* @   .  + .    .    .    .   */
+	/* jumping */
+	TEST_ASSERT(temp_to_rpm(46) == 3500); /*     .@   .    .+   .    .   */
+	TEST_ASSERT(temp_to_rpm(36) == 2500); /*    @.+   .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(35) == 0);    /* @   +    .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(37) == 0);    /* @   . +  .    .    .    .   */
+	TEST_ASSERT(temp_to_rpm(46) == 3500); /*     .@   .    .+   .    .   */
+	TEST_ASSERT(temp_to_rpm(54) == 5100); /*     .   @.    .    .   +.   */
+	TEST_ASSERT(temp_to_rpm(55) == 5400); /*     .    @    .    .    +   */
+	TEST_ASSERT(temp_to_rpm(60) == 5400); /*     .    @    .    .    ..+ */
+	TEST_ASSERT(temp_to_rpm(53) == 5400); /*     .    @    .    .  + .   */
+	TEST_ASSERT(temp_to_rpm(46) == 3900); /*     . @  .    .+   .    .   */
+	TEST_ASSERT(temp_to_rpm(30) == 0);    /* @+. .   40    .   50    .   */
+
+	return EC_SUCCESS;
+}
+
 void run_test(int argc, const char **argv)
 {
 	RUN_TEST(test_fan);
+	RUN_TEST(test_temp_ratio_to_rpm_hysteresis);
 
 	test_print_result();
 }
