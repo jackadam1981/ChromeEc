8998f44405a3e0d0e5429b44235b0fa1ba647097
Rob Barnes <robbarnes@google.com>
2022-11-29 20:54:29 +0000

    panic: Publish EC_HOST_EVENT_PANIC on panic
    Publish EC_HOST_EVENT_PANIC when a panic occurs.
    
    The kernel may use this event to clean up before the system is reset
    (e.g. sync the drive).
    
    This will be a no-op if the kernel doesn't handle it.
    
    BUG=b:258195448
    BRANCH=None
    TEST=Observe event in kernel
    
    Change-Id: I34b9847778bf17dd113e81158bbbdf999ad2ca33
    Signed-off-by: Rob Barnes <robbarnes@google.com>

--- core/cortex-m/panic.c
+++ core/cortex-m/panic.c
@@ -364,6 +364,8 @@ void __keep report_panic(void)
 	if (IS_ENABLED(CONFIG_ARMV7M_CACHE))
 		cpu_clean_invalidate_dcache();
 
+	host_set_single_event(EC_HOST_EVENT_PANIC);
+
 	/* Start safe mode if possible */
 	if (IS_ENABLED(CONFIG_SYSTEM_SAFE_MODE)) {
 		/* TODO: check for nested exceptions */
