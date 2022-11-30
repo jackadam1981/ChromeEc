2bfad1f231a28c54a35e8bf1320c8e0f8f540458
ben chen <ben.chen2@quanta.corp-partner.google.com>
2022-11-30 13:22:59 +0800

    Craask: add Fw_config structure for keyboard numeric pad control
    Craask need to implement FW_KB_NUMERIC_PAD config in
    bit 16 of cbi fw_config.Enable/Disable keypad control
    by FW_KB_NUMERIC_PAD flag.
    
    BUG=b:257387315,259498529
    BRANCH=none
    TEST=check keypad workable by FW_KB_NUMERIC_PAD flags.
    
    Change-Id: I94b153d065db20e8619cd13f21877a11e76ea430
    Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

--- zephyr/program/nissa/craask/src/keyboard.c
+++ zephyr/program/nissa/craask/src/keyboard.c
@@ -3,7 +3,11 @@
  * found in the LICENSE file.
  */
 
+#include "cros_cbi.h"
 #include "ec_commands.h"
+#include "hooks.h"
+#include "keyboard_raw.h"
+#include "keyboard_scan.h"
 
 static const struct ec_response_keybd_config craask_kb = {
 	.num_top_row_keys = 10,
@@ -22,11 +26,63 @@ static const struct ec_response_keybd_config craask_kb = {
 	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
 };
 
+static const struct ec_response_keybd_config craask_kb_w_kb_numpad = {
+	.num_top_row_keys = 10,
+	.action_keys = {
+		TK_BACK,		/* T1 */
+		TK_BRIGHTNESS_DOWN,		/* T2 */
+		TK_FULLSCREEN,		/* T3 */
+		TK_OVERVIEW,		/* T4 */
+		TK_SNAPSHOT,		/* T5 */
+		TK_BRIGHTNESS_DOWN,	/* T6 */
+		TK_BRIGHTNESS_UP,	/* T7 */
+		TK_VOL_MUTE,		/* T8 */
+		TK_VOL_DOWN,		/* T9 */
+		TK_VOL_UP,		/* T10 */
+	},
+	.capabilities = KEYBD_CAP_SCRNLOCK_KEY | KEYBD_CAP_NUMERIC_KEYPAD,
+};
+
 __override const struct ec_response_keybd_config *
 board_vivaldi_keybd_config(void)
 {
-	return &craask_kb;
+	uint32_t val;
+
+	cros_cbi_get_fw_config(FW_KB_NUMERIC_PAD, &val);
+
+	if (val == FW_KB_NUMERIC_PAD_ABSENT)
+		return &craask_kb;
+	else
+		return &craask_kb_w_kb_numpad;
 }
+
+static void board_update_no_keypad_by_fwconfig(void)
+{
+	uint32_t val;
+
+	cros_cbi_get_fw_config(FW_KB_NUMERIC_PAD, &val);
+
+	if (val == FW_KB_NUMERIC_PAD_ABSENT) {
+		/* Disable scanning KSO13 & 14 if keypad isn't present. */
+		keyboard_raw_set_cols(KEYBOARD_COLS_NO_KEYPAD);
+	} else {
+		/* Setting scan mask KSO11, KSO12, KSO13 and KSO14 */
+		keyscan_config.actual_key_mask[11] = 0xfe;
+		keyscan_config.actual_key_mask[12] = 0xff;
+		keyscan_config.actual_key_mask[13] = 0xff;
+		keyscan_config.actual_key_mask[14] = 0xff;
+	}
+}
+
+/*
+ * Keyboard function decided by FW config.
+ */
+static void kb_init(void)
+{
+	/* Support Keyboard Pad */
+	board_update_no_keypad_by_fwconfig();
+}
+DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_FIRST);
 
 /*
  * We have total 30 pins for keyboard connecter {-1, -1} mean
