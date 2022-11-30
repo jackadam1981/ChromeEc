9f96042b6e08d3ceaa3dbf26bb44177e3d1aea6a
Leila Lin <leilacy_lin@compal.corp-partner.google.com>
2022-11-30 18:44:13 +0800

    winterhold: Dynamic switch thermal table
    There are 3 modes: DESKTOP_LID_OPEN_MODE、DESKTOP_LID_CLOSE_MODE
    and LAPTOP_MODE. The mode will base on the condition of lid and
    body detection, when the hook notification is received, the mode
    will change and different modes will have corresponding thermal
    settings.
    
    LOW_COVERAGE_REASON=no unit test for skyrim board yet: b/247151116
    BRANCH=none
    BUG=b:232946420, b:255732503
    TEST=zmake build winterhold and confirm thermal config change when
    mode change.
    
    Change-Id: I5b4eaba2dbf3b4a833589e45588cf7f1001e22ea
    Signed-off-by: Leila Lin <leilacy_lin@compal.corp-partner.google.com>

--- zephyr/program/skyrim/winterhold/src/thermal.c
+++ zephyr/program/skyrim/winterhold/src/thermal.c
@@ -6,6 +6,9 @@
 #include "hooks.h"
 #include "temp_sensor/temp_sensor.h"
 #include "host_command.h"
+#include "thermal.h"
+#include "lid_switch.h"
+#include "body_detection.h"
 
 #define CPRINTS(format, args...) cprints(CC_THERMAL, format, ##args)
 #define CPRINTF(format, args...) cprintf(CC_THERMAL, format, ##args)
@@ -13,6 +16,62 @@
 #define TEMP_AMB TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_amb))
 
 static int last_amb_temp = -1;
+
+enum dynamic_thermal_switch_mode {
+	DESKTOP_LID_OPEN_MODE,
+	DESKTOP_LID_CLOSE_MODE,
+	LAPTOP_MODE
+};
+
+struct thermal_setting {
+	int warn;
+	int release_warn;
+	int fan_off;
+	int fan_max;
+};
+
+/* Thermal table for DESKTOP_LID_OPEN_MODE, DESKTOP_LID_CLOSE_MODE, LAPTOP_MODE
+ */
+static const struct thermal_setting thermal_table[] = {
+	{ .warn = 50, .release_warn = 45, .fan_off = 31, .fan_max = 38 },
+	{ .warn = 55, .release_warn = 50, .fan_off = 32, .fan_max = 39 },
+	{ .warn = 45, .release_warn = 40, .fan_off = 30, .fan_max = 38 },
+};
+
+/* Change temp sensor amb-pct2075's config setting */
+static void thermal_threshold_set(int mode)
+{
+	thermal_params[TEMP_AMB].temp_host[EC_TEMP_THRESH_WARN] =
+		C_TO_K(thermal_table[mode].warn);
+	thermal_params[TEMP_AMB].temp_host_release[EC_TEMP_THRESH_WARN] =
+		C_TO_K(thermal_table[mode].release_warn);
+	thermal_params[TEMP_AMB].temp_fan_off =
+		C_TO_K(thermal_table[mode].fan_off);
+	thermal_params[TEMP_AMB].temp_fan_max =
+		C_TO_K(thermal_table[mode].fan_max);
+}
+
+/* Switch thermal table when mode change */
+static void thermal_table_switch(void)
+{
+	enum body_detect_states body_state = body_detect_get_state();
+
+	if (body_state == BODY_DETECTION_OFF_BODY) {
+		if (lid_is_open()) {
+			thermal_threshold_set(DESKTOP_LID_OPEN_MODE);
+			CPRINTS("Thermal: Desktop lid open mode");
+		} else {
+			thermal_threshold_set(DESKTOP_LID_CLOSE_MODE);
+			CPRINTS("Thermal: Desktop lid close mode");
+		}
+	} else {
+		thermal_threshold_set(LAPTOP_MODE);
+		CPRINTS("Thermal: Laptop mode");
+	}
+}
+DECLARE_HOOK(HOOK_INIT, thermal_table_switch, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_LID_CHANGE, thermal_table_switch, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_BODY_DETECT_CHANGE, thermal_table_switch, HOOK_PRIO_DEFAULT);
 
 /* Set SCI event to host for temperature change */
 static void detect_temp_change(void)
