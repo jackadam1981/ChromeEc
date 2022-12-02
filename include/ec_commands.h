dfe544766487a05ae1327e9759437e15fcb0e22c
Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
2022-12-02 15:48:15 +0530

    [WIP] Add fan rpm support for dptf control
    Add fan rpm support for dptf based thermal control. This is based on
    document #572349 for _FST ACPI implementation for Active2 policy.
    Coreboot has corresponding implementation
    https://review.coreboot.org/c/coreboot/+/62789
    
    BUG=b:224457192
    BRANCH=None
    TEST=Built and flash on brya system.
         Verified"/sys/bus/acpi/devices/INTC1048:00/fan_speed_rpm" sysfs
         value matching with "ectool pwmgetfanrpm"
    
    Signed-off-by: Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
    Change-Id: Iad68909f0b09368932f63f3640ba629825bd5f6a

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -145,6 +145,7 @@ extern "C" {
 /* The offset address of each type of data in mapped memory. */
 #define EC_MEMMAP_TEMP_SENSOR 0x00 /* Temp sensors 0x00 - 0x0f */
 #define EC_MEMMAP_FAN 0x10 /* Fan speeds 0x10 - 0x17 */
+#define EC_MEMMAP_FAN_RPM 0x16 /* Current Fan Speed in RPM */
 #define EC_MEMMAP_TEMP_SENSOR_B 0x18 /* More temp sensors 0x18 - 0x1f */
 #define EC_MEMMAP_ID 0x20 /* 0x20 == 'E', 0x21 == 'C' */
 #define EC_MEMMAP_ID_VERSION 0x22 /* Version of data in 0x20 - 0x2f */
