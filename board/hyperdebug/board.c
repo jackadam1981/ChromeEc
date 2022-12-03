b0079a52d58f947edcd907f88b935e59ec8c4b3d
Jes B. Klinke <jbk@chromium.org>
2022-12-02 20:19:14 -0800

    board/hyperdebug: Enumerate and configure SPI ports via USB
    Add console commands: spiget and spisetspeed
    
    The first can list information on all SPI port or a particular port.
    
    The other, spisetspeed, can be used to modify the clock divisor between
    SPI transactions.
    
    BUG=b:192262089
    TEST=Observed different clock speeds on analyzer
    
    Change-Id: I113b8bdef48f58d5941212c8a8598f3293b5da91

--- board/hyperdebug/board.c
+++ board/hyperdebug/board.c
@@ -131,7 +131,7 @@ USB_STREAM_CONFIG(usart5_usb, USB_IFACE_USART5_STREAM,
 #define SPI_CLOCK (16000000UL)
 
 /* SPI devices, default to 250 kb/s for all. */
-const struct spi_device_t spi_devices[] = {
+struct spi_device_t spi_devices[] = {
 	{ .port = 1 /* SPI2 */,
 	  .div = 5,
 	  .gpio_cs = GPIO_CN9_25,
@@ -628,3 +628,127 @@ static int command_gpio_pull_mode(int argc, const char **argv)
 DECLARE_CONSOLE_COMMAND_FLAGS(gpiopullmode, command_gpio_pull_mode,
 			      "name <none | up | down>",
 			      "Set a GPIO weak pull mode", CMD_FLAG_RESTRICTED);
+
+/*
+ * Find spi device by name or by number.  Returns an index into spi_devices[],
+ * or on error a negative value.
+ */
+static int find_spi_by_name(const char *name)
+{
+	int i;
+	char *e;
+	i = strtoi(name, &e, 0);
+
+	if (!*e && i < spi_devices_used)
+		return i;
+
+	for (i = 0; i < spi_devices_used; i++) {
+		if (!strcasecmp(name, spi_device_names[i]))
+			return i;
+	}
+
+	/* SPI device not found */
+	return -1;
+}
+
+static void print_spi_info(int index)
+{
+	uint32_t bits_per_second;
+
+	if (spi_devices[index].port == 0xFF) {
+		// OCTOSPI as 8 bit prescaler, dividing clock by 1..256.
+		bits_per_second = OCTOSPI_CLOCK / (spi_devices[index].div + 1);
+	} else {
+		// Other SPIs have prescaler by power of two 2, 4, 8, ..., 256.
+		bits_per_second = SPI_CLOCK / (2 << spi_devices[index].div);
+	}
+
+	ccprintf("  %d %s %d\n", index, spi_device_names[index],
+		 bits_per_second);
+
+	/* Flush console to avoid truncating output */
+	cflush();
+}
+
+/*
+ * Get information about one or all SPI ports.
+ */
+static int command_spi_get(int argc, const char **argv)
+{
+	int i;
+
+	/* If a SPI target is specified, print only that one */
+	if (argc == 2) {
+		int index = find_spi_by_name(argv[1]);
+		if (index < 0) {
+			/* Not found */
+			return EC_ERROR_PARAM1;
+		}
+
+		print_spi_info(index);
+		return EC_SUCCESS;
+	}
+
+	/* Otherwise print them all */
+	for (i = 0; i < spi_devices_used; i++) {
+		print_spi_info(i);
+	}
+
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND_FLAGS(spiget, command_spi_get, "[name]",
+			      "Get SPI configuration", CMD_FLAG_RESTRICTED);
+
+static int command_spi_set_speed(int argc, const char **argv)
+{
+	int index;
+	uint32_t desired_speed;
+	char *e;
+	if (argc < 3)
+		return EC_ERROR_PARAM_COUNT;
+
+	index = find_spi_by_name(argv[1]);
+	if (index < 0)
+		return EC_ERROR_PARAM1;
+
+	desired_speed = strtoi(argv[2], &e, 0);
+	if (*e)
+		return EC_ERROR_PARAM2;
+
+	if (spi_devices[index].port == 0xFF) {
+		/*
+		 * Find prescaler value by division, rounding up in order to get
+		 * slightly slower speed than requested, if it cannot be matched
+		 * exactly.
+		 */
+		spi_devices[index].div =
+			(OCTOSPI_CLOCK + desired_speed - 1) / desired_speed - 1;
+		STM32_OCTOSPI_DCR2 = spi_devices[index].div;
+	} else {
+		int divisor = 7;
+		/*
+		 * Find the smallest divisor that result in a speed not faster
+		 * than what was requested.
+		 */
+		while (divisor > 0) {
+			if (SPI_CLOCK / (2 << (divisor - 1)) > desired_speed) {
+				/* One step further would make the clock too
+				 * fast, stop here. */
+				break;
+			}
+			divisor--;
+		}
+
+		/*
+		 * Re-initialize spi controller to apply the new clock divisor.
+		 */
+		spi_enable(&spi_devices[index], 0);
+		spi_devices[index].div = divisor;
+		spi_enable(&spi_devices[index], 1);
+	}
+
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND_FLAGS(spisetspeed, command_spi_set_speed,
+			      "name bits_per_second", "Get SPI configuration",
+			      CMD_FLAG_RESTRICTED);
