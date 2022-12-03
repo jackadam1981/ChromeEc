8c218cbc179704af719ac5d3cdcd0752cdcf65fd
Li-Yu Yu <aaronyu@google.com>
2022-12-03 17:09:27 +0800

    Use fixed-width printf directives for fixed width integers
    CL:3794612 introduced a compile error for 32-bit boards.
    
    Also fix -Wclang-format-violations.
    
    BUG=b:249163956
    BUG=b:261258378
    BRANCH=none
    TEST=emerge-cherry ec-utils
    TEST=emerge-cherry64 ec-utils
    TEST=emerge-volteer ec-utils
    
    Change-Id: I00878b3047d9d448770e652839d26ab73ce3d253
    Signed-off-by: Li-Yu Yu <aaronyu@google.com>

--- common/system_boot_time.c
+++ common/system_boot_time.c
@@ -24,7 +24,7 @@ void update_ap_boot_time(enum boot_time_param param)
 	}
 	if (param < RESET_CNT) {
 		ap_boot_time.timestamp[param] = get_time().val;
-		ccprintf("Boot Time: %d, %lld\n", param,
+		ccprintf("Boot Time: %d, %" PRIu64 "\n", param,
 			 ap_boot_time.timestamp[param]);
 	}
 
