863ec48b9264e69e0736ddec0a4bdd58e5f26efa
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2022-12-05 10:46:22 +0800

    TEST: Debug log for vbus_pres and charge supplier
    Print out charge supplier changed info and vbus_pres status
    for issue b/259211184
    
    BUG=b:259211184
    BRANCH=none
    TEST=Try to reproduce the issue and capture ec log
    
    Change-Id: I489c0f5070c107023dab1242ed8a747398e2ab7f
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -1223,6 +1223,8 @@ void charge_manager_update_charge(int supplier, int port,
 	struct charge_port_info zero = { 0 };
 	if (!charge)
 		charge = &zero;
+	CPRINTS("%s supplier=%d %dmA/%dmV", __func__, supplier, charge->current,
+		charge->voltage);
 	charge_manager_make_change(CHANGE_CHARGE, supplier, port, charge);
 }
 
