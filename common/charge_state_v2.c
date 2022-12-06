8b141b81762987c8273038396ce0623215f7c3bb
wen zhang <zhangwen6@huaqin.corp-partner.google.com>
2022-12-06 16:06:26 +0800

    zephyr: test charge_get_state function
    Just for debug
    
    Change-Id: Idbf03b6e96c7502353240c60b39f2f1548ea1d73

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -1085,7 +1085,7 @@ void chgstate_set_manual_voltage(int volt_mv)
 }
 
 /* Force charging off before the battery is full. */
-static int set_chg_ctrl_mode(enum ec_charge_control_mode mode)
+test_export_static int set_chg_ctrl_mode(enum ec_charge_control_mode mode)
 {
 	bool discharge_on_ac = false;
 	int current, voltage;
@@ -1306,7 +1306,7 @@ static void notify_host_of_low_battery_charge(void)
 #endif
 }
 
-static void set_charge_state(enum charge_state_v2 state)
+test_export_static void set_charge_state(enum charge_state_v2 state)
 {
 	prev_state = curr.state;
 	curr.state = state;
