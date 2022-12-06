fdc42ee1855c967ca34878534de233271958d023
Kyle Lin <kylelinck@google.com>
2022-12-06 11:03:30 +0800

    [test]Pujjoteen: Remove BC 12 function
    Set CONFIG_PLATFORM_EC_USB_CHARGER=n and fix dpendency
    
    BUG=b:4037123
    TEST=zmake confiure pujjo && zmake build pujjo
    BRANCH=None
    
    Change-Id: I4156da55d10e9e1fef78e4e4a3e9e7ca5109dca3
    Signed-off-by: Kyle Lin <kylelinck@google.com>

--- zephyr/program/nissa/pujjo/src/usbc.c
+++ zephyr/program/nissa/pujjo/src/usbc.c
@@ -188,7 +188,6 @@ DECLARE_DEFERRED(poll_c0_int);
 static void usbc_interrupt_trigger(int port)
 {
 	schedule_deferred_pd_interrupt(port);
-	usb_charger_task_set_event(port, USB_CHG_EVENT_BC12);
 }
 
 static inline void poll_usb_gpio(int port, const struct gpio_dt_spec *gpio,
