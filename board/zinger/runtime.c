0c660ba43cf46531235e35b3adb286a20657d97c
Andrea Grandi <agrandi@google.com>
2022-12-07 15:31:34 -0800

    host: Add return value to sleep() and usleep()
    This solves the following build error on host:
    
    core/host/timer.c:20:6: error: conflicting types for ‘usleep’
       20 | void usleep(unsigned us)
          |      ^~~~~~
    In file included from include/timer.h:23,
                     from core/host/timer.c:11:
    /usr/include/unistd.h:480:12: note: previous declaration of ‘usleep’ was
      here
      480 | extern int usleep (__useconds_t __useconds);
          |            ^~~~~~
    
    BUG=b:260723169
    TEST=test/run_device_tests.py -b bloonchipper -t timer
    TEST=test/run_device_tests.py -b dartmonkey -t timer
    TEST=make run-timer
    BRANCH=none
    
    Change-Id: I7dbaebb9eec870fa1bf03b4ac21f5728d4f9a624
    Signed-off-by: Andrea Grandi <agrandi@google.com>

--- board/zinger/runtime.c
+++ board/zinger/runtime.c
@@ -289,6 +289,7 @@ void __hw_timer_enable_clock(int n, int enable)
 { /* Done in hardware init */
 }
 
-void usleep(unsigned us)
+int usleep(unsigned us)
 { /* Used only as a workaround */
+	return 0;
 }
