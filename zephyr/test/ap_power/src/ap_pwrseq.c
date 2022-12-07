037375e7667bc25d7842daabf9067fda8ec9f333
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-12-07 09:53:42 -0800

    zephyr: ap_pwrseq: Adapt test to driver model
    This CL makes board test file to follow AP power sequence driver
    framewor by using macro `AP_POWER_APP_STATE_DEFINE` to define state
    action handlers, and makes test to use driver API to post events.
    
    TEST=zmake build
    BRANCH=none
    BUG=b:217952699
    
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Change-Id: I860683fb93ea1d44386e002a6a4d99076551535c

--- zephyr/test/ap_power/src/ap_pwrseq.c
+++ zephyr/test/ap_power/src/ap_pwrseq.c
@@ -65,6 +65,7 @@ ZTEST(ap_pwrseq, test_ap_pwrseq_0)
 			      EMUL_POWER_SIGNAL_TEST_PLATFORM(tp_sys_g3_to_s0)),
 		      "Unable to load test platfform `tp_sys_g3_to_s0`");
 
+	chipset_exit_hard_off();
 	k_msleep(500);
 
 	zassert_equal(1, power_start_up_count,
@@ -79,12 +80,8 @@ ZTEST(ap_pwrseq, test_ap_pwrseq_1)
 		      power_signal_emul_load(EMUL_POWER_SIGNAL_TEST_PLATFORM(
 			      tp_sys_s0_power_fail)),
 		      "Unable to load test platfform `tp_sys_s0_power_fail`");
-
-	/*
-	 * Once emulated power signals are loaded, we need to wake AP power
-	 * Sequence thread up to start executing new set of power signals
-	 */
-	ap_pwrseq_wake();
+	ap_pwrseq_post_event(ap_pwrseq_get_instance(),
+			     AP_PWRSEQ_EVENT_POWER_STARTUP);
 	k_msleep(500);
 	zassert_equal(1, power_shutdown_count,
 		      "AP_POWER_SHUTDOWN event not generated");
@@ -102,11 +99,12 @@ ZTEST(ap_pwrseq, test_ap_pwrseq_2)
 			tp_sys_g3_to_s0_power_down)),
 		"Unable to load test platfform `tp_sys_g3_to_s0_power_down`");
 
-	ap_power_exit_hardoff();
+	chipset_exit_hard_off();
 	k_msleep(2000);
-	zassert_equal(3, power_shutdown_count,
+
+	zassert_equal(2, power_shutdown_count,
 		      "AP_POWER_SHUTDOWN event not generated");
-	zassert_equal(3, power_shutdown_complete_count,
+	zassert_equal(2, power_shutdown_complete_count,
 		      "AP_POWER_SHUTDOWN_COMPLETE event not generated");
 	zassert_equal(1, power_suspend_count,
 		      "AP_POWER_SUSPEND event generated");
