cbf484c805ba0838f62b4939df5b456fb4caba11
Andrea Grandi <agrandi@google.com>
2022-12-07 15:17:48 -0800

    builtin: Add usleep() and sleep() to builtin
    Add sleep functions declarations to unistd.h inside the builtin folder.
    Exclude implementation from the host build.
    
    BUG=b:260723169
    TEST=test/run_device_tests.py -b bloonchipper -t timer
    TEST=test/run_device_tests.py -b dartmonkey -t timer
    TEST=make run-timer
    TEST=./util/compare_build.sh -b all -j 120
    BRANCH=none
    
    Change-Id: Ibd32837f177098ddb589e8d69d348e0bbdd5bc04
    Signed-off-by: Andrea Grandi <agrandi@google.com>

---
+++ builtin/unistd.h
@@ -0,0 +1,37 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_UNITSTD_H__
+#define __CROS_EC_UNITSTD_H__
+
+/* Time units in microseconds */
+#define SECOND 1000000
+
+/**
+ * Sleep.
+ *
+ * The current task will be de-scheduled for at least the specified delay (and
+ * perhaps longer, if a higher-priority task is running when the delay
+ * expires).
+ *
+ * This may only be called from a task function, with interrupts enabled.
+ *
+ * @param us		Number of microseconds to sleep.
+ */
+void usleep(unsigned us);
+
+/**
+ * Sleep for seconds
+ *
+ * Otherwise the same as usleep().
+ *
+ * @param sec		Number of seconds to sleep.
+ */
+static inline void sleep(unsigned sec)
+{
+	usleep(sec * SECOND);
+}
+
+#endif /* __CROS_EC_UNISTD_H__ */
