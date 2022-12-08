a489053b97e5dd165a539e58115f08120df84a47
Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>
2022-12-08 13:39:22 +0800

    marasov: Implement keyboard and keyboard backlight function
    1. Depend on keyboard matrix, implement keyboard function.
    2. Modify keyboard backlight config depend on fw_config.
    
    BUG=none
    BRANCH=none
    TEST=manual
     1. Check all key by evtest.
     2. Check Function key behavior.
    
    Change-Id: I7fd561e0475c59ac5221be78a1f76b49cb2d974d
    Signed-off-by: Michael5 Chen1 <michael5_chen1@pegatron.corp-partner.google.com>

--- board/marasov/fw_config.c
+++ board/marasov/fw_config.c
@@ -12,14 +12,14 @@
 
 #define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
 
-static union brya_cbi_fw_config fw_config;
+static union marasov_cbi_fw_config fw_config;
 BUILD_ASSERT(sizeof(fw_config) == sizeof(uint32_t));
 
 /*
  * FW_CONFIG defaults for brya if the CBI.FW_CONFIG data is not
  * initialized.
  */
-static const union brya_cbi_fw_config fw_config_defaults = {
+static const union marasov_cbi_fw_config fw_config_defaults = {
 	.usb_db = DB_USB3_PS8815,
 	.kb_bl = KEYBOARD_BACKLIGHT_ENABLED,
 };
@@ -35,7 +35,7 @@ void board_init_fw_config(void)
 	}
 }
 
-union brya_cbi_fw_config get_fw_config(void)
+union marasov_cbi_fw_config get_fw_config(void)
 {
 	return fw_config;
 }
@@ -43,4 +43,9 @@ union brya_cbi_fw_config get_fw_config(void)
 enum ec_cfg_usb_db_type ec_cfg_usb_db_type(void)
 {
 	return fw_config.usb_db;
+}
+
+enum ec_cfg_keyboard_backlight_type ec_cfg_kb_bl_type(void)
+{
+	return fw_config.kb_bl;
 }
