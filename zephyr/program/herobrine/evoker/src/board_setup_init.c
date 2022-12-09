418d355370f91d6470fea4285a592cf76422de52
Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
2022-12-09 13:13:35 +0800

    [TEST_ONLY] evoker: test
    Change-Id: I3ae030a90cdefd63cb2ca5592b66fa8cc7ccb9ef

---
+++ zephyr/program/herobrine/evoker/src/board_setup_init.c
@@ -0,0 +1,63 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "charger.h"
+#include "charge_manager.h"
+#include "chipset.h"
+#include "console.h"
+#include "extpower.h"
+#include "hooks.h"
+#include "usbc_ppc.h"
+#include "util.h"
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(board_init, LOG_LEVEL_ERR);
+
+#define INT_RECHECK_MS 10
+#define INT_RECHECK_TIMES 500
+
+/* minimum request for turn on S5 power */
+#define MIN_POWER_MW_FOR_TURNON_S5_POWER 15000
+
+
+static void check_s5_power(void);
+DECLARE_DEFERRED(check_s5_power);
+
+static void check_s5_power(void)
+{
+	int port, pd_power;
+	static int count;
+
+	pd_power = charge_manager_get_power_limit_uw() / 1000;
+
+	if (pd_power >= MIN_POWER_MW_FOR_TURNON_S5_POWER) {
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_s5), 1);
+		LOG_ERR("%s: Turn on S5 power (%d)",
+					__func__, pd_power);
+		msleep(20);
+		for (port = 0; port < CONFIG_USB_PD_PORT_MAX_COUNT; port++) {
+			ppc_init(port);
+		}
+		return;
+	}
+
+	if (count++ >= INT_RECHECK_TIMES) {
+		LOG_ERR("Failed to turn on S5 power, back to G3");
+		chipset_force_shutdown(CHIPSET_SHUTDOWN_INIT);
+		return;
+	} else
+		hook_call_deferred(&check_s5_power_data, INT_RECHECK_MS * MSEC);
+
+	LOG_ERR("(%d)", pd_power);
+}
+
+/*
+ * check board has enough power to turn on the GPIO_EN_PP5000
+ */
+static void board_s5_power_init(void)
+{
+	hook_call_deferred(&check_s5_power_data, INT_RECHECK_MS * MSEC);
+}
+DECLARE_HOOK(HOOK_INIT, board_s5_power_init, HOOK_PRIO_POST_I2C);
