99c56f381ed3a971ad0cc212f5d8575edbed91d1
arthur.lin <arthur.lin@lcfc.corp-partner.google.com>
2022-12-12 17:04:48 +0800

    battery: Avoid read battery when cut off process
    Avoid read battery data when starting battery cut off process.
    
    BUG=b:260684835
    BRANCH=none
    TEST=zmake build -a
    
    Change-Id: I739e6c46f79a1455a8dbfdfb2c63a146296c6372
    Signed-off-by: arthur.lin <arthur.lin@lcfc.corp-partner.google.com>

--- include/battery.h
+++ include/battery.h
@@ -78,6 +78,7 @@ FORWARD_DECLARE_ENUM(battery_present){
 
 enum battery_cutoff_states {
 	BATTERY_CUTOFF_STATE_NORMAL = 0,
+	BATTERY_CUTOFF_STATE_IN_PROGRESS,
 	BATTERY_CUTOFF_STATE_CUT_OFF,
 	BATTERY_CUTOFF_STATE_PENDING,
 };
@@ -437,6 +438,11 @@ int battery_imbalance_mv(void);
  * @return EC_RES_INVALID_COMMAND if the battery doesn't support.
  */
 int board_cut_off_battery(void);
+
+/**
+ * Return if the battery start cut off.
+ */
+int battery_cutoff_in_progress(void);
 
 /**
  * Return if the battery has been cut off.
