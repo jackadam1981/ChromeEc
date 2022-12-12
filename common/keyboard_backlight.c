4d5ca2f724c875d96fadc755c3d292d8cc8f06b6
Peter Marheine <pmarheine@chromium.org>
2022-12-13 10:35:19 +1100

    keyboard_backlight: correct system EC detect condition
    HAS_TASK_CHIPSET is only enabled when using the legacy power sequencing
    code, not the new Zephyr implementation. Use CONFIG_AP_POWER_CONTROL
    instead, which covers both implementations.
    
    BUG=b:257325101
    TEST=Keyboard backlight remains off on Yaviks following a warm reboot when
         system is in suspend and a charger is disconnected.
    BRANCH=nissa
    
    Change-Id: Ie0f4355313e4f50e0bbbe54af0e37412baa29319
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>

--- common/keyboard_backlight.c
+++ common/keyboard_backlight.c
@@ -117,9 +117,10 @@ static void keyboard_backlight_init(void)
 	/* Don't leave kblight enable state undetermined */
 	kblight_enable(0);
 }
-#ifdef HAS_TASK_CHIPSET
+#ifdef CONFIG_AP_POWER_CONTROL
 /* We're running on a system EC. Initialize kblight once per AP start-up. */
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, keyboard_backlight_init, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_RESET, keyboard_backlight_init, HOOK_PRIO_DEFAULT);
 #else
 /* We're running on a KBMCU thus powered when AP starts. Do init on reset. */
 DECLARE_HOOK(HOOK_INIT, keyboard_backlight_init, HOOK_PRIO_DEFAULT);
