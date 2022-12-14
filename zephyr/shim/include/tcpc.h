e59189cabc06ab7d6d3ac5b29f895b21cc57a015
Jason Yuan <jasonyuan@google.com>
2022-12-14 15:23:05 -0800

    zephyr: move usbc interrupt handler to shim
    TCPC interrupt no longer uses gpio-int. Instead the shim tcpc conpletely
    handles its own interrupt.
    
    BUG=b:254148652
    TEST=usbc charging on villager and lazor
    BRANCH=none
    
    Change-Id: Ieeb4fb61ca6cf8f44df212a6cf520d265cff5147
    Signed-off-by: Jason Yuan <jasonyuan@google.com>

---
+++ zephyr/shim/include/tcpc.h
@@ -0,0 +1,16 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_SHIM_INCLUDE_GPIO_TCPC_H_
+#define ZEPHYR_SHIM_INCLUDE_GPIO_TCPC_H_
+
+/*
+ * Enable the interrupt.
+ *
+ * All tcpc interrupts are automatically retrieved and enabled from devicetree
+ */
+void tcpc_enable_interrupt(void);
+
+#endif
