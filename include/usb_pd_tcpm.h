19655fc5623571d9222e1ec601a26ca5ff308198
Jason Yuan <jasonyuan@google.com>
2022-12-14 15:23:05 -0800

    zephyr: move usbc interrupt handler to shim
    TCPC interrupt no longer uses gpio-int. Instead the shim tcpc conpletely
    handles its own interrupt.
    
    The programs nissa and intelrvp are excluded from this CL. They will be
    refactored to use the shim tcpc driver in a follow-up CL.
    
    BUG=b:254148652
    TEST=twister, usbc charging on villager and lazor
    BRANCH=none
    
    Change-Id: Ieeb4fb61ca6cf8f44df212a6cf520d265cff5147
    Signed-off-by: Jason Yuan <jasonyuan@google.com>

--- include/usb_pd_tcpm.h
+++ include/usb_pd_tcpm.h
@@ -578,7 +578,11 @@ struct tcpc_config_t {
 	const struct tcpm_drv *drv;
 	/* See TCPC_FLAGS_* above */
 	uint32_t flags;
+#ifdef CONFIG_ZEPHYR
+	struct gpio_dt_spec int_cfg;
+#else
 	enum gpio_signal alert_signal;
+#endif
 };
 
 #ifndef CONFIG_USB_PD_TCPC_RUNTIME_CONFIG
