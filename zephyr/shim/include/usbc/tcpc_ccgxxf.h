9a3a782afd4f572bafec830800d49fdfa88c55f7
Jason Yuan <jasonyuan@google.com>
2022-12-14 15:23:05 -0800

    zephyr: move usbc interrupt handler to shim
    TCPC interrupt no longer uses gpio-int. Instead the shim tcpc conpletely
    handles its own interrupt.
    
    BUG=b:254148652
    TEST=usbc charging on villager and lazor
    BRANCH=none
    
    Change-Id: Ieeb4fb61ca6cf8f44df212a6cf520d265cff5147
    Signed-off-by: Jason Yuan <jasonyuan@google.com>

--- zephyr/shim/include/usbc/tcpc_ccgxxf.h
+++ zephyr/shim/include/usbc/tcpc_ccgxxf.h
@@ -9,6 +9,14 @@
 
 #define CCGXXF_TCPC_COMPAT cypress_ccgxxf
 
+#define INT_PIN_CONFIG_CCGXXF(id)                                            \
+	COND_CODE_1(DT_NODE_HAS_PROP(id, int_pin),                           \
+		    (.gpio_port = DEVICE_DT_GET(                             \
+			     DT_GPIO_CTLR(DT_PHANDLE(id, int_pin), gpios)),  \
+		     .interrupt_pin =                                        \
+			     DT_GPIO_PIN(DT_PHANDLE(id, int_pin), gpios), ), \
+		    ())
+
 #define TCPC_CONFIG_CCGXXF(id) \
 	{                                                                      \
 		.bus_type = EC_BUS_TYPE_I2C,                                   \
@@ -18,7 +26,5 @@
 		},                                                             \
 		.drv = &ccgxxf_tcpm_drv,                                       \
 		.flags = TCPC_FLAGS_TCPCI_REV2_0,                              \
-		.alert_signal = COND_CODE_1(DT_NODE_HAS_PROP(id, int_pin),     \
-			(GPIO_SIGNAL(DT_PHANDLE(id, int_pin))),                \
-			(GPIO_LIMIT)),                                         \
+		INT_PIN_CONFIG_CCGXXF(id)                                      \
 	},
