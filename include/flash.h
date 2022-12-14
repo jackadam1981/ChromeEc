7e99c937cf953079daae2aebc190c9f3e19a3ead
Patryk Duda <pdk@semihalf.com>
2022-12-14 15:34:22 +0100

    flash: Introduce function which provides number of flash sectors
    In CrosEC, PHYSICAL_BANKS contains information how many sectors the
    flash has. If all sectors have the same size the value is a result of
    dividing flash size by sector size. If sectors have different sizes,
    we define it as CONFIG_FLASH_MULTIPLE_REGION alias.
    
    When running Zephyr, these information are available through flash pages
    API. In CrosEC, newly introduced crec_flash_total_banks() function
    returns PHYSICAL_BANKS. Next commit will introduce the function
    counterpart in Zephyr EC build.
    
    BUG=b:239712345
    BRANCH=none
    TEST=make buildall -j
    TEST=./test/run_device_tests.py --board bloonchipper \
    	--tests flash_physical
    TEST=Compile bloonchipper firmware and flash it on dragonclaw board.
         Run 'flashinfo' command. Make sure that there are 12 sectors under
         'Protected now:' line.
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: I797b7ca60047c3c1549c95e8515888f56b0059e8

--- include/flash.h
+++ include/flash.h
@@ -52,6 +52,8 @@ int crec_flash_bank_erase_size(int bank);
 
 void crec_flash_print_region_info(void);
 
+int crec_flash_total_banks(void);
+
 /* Number of physical flash banks */
 #define PHYSICAL_BANKS CONFIG_FLASH_MULTIPLE_REGION
 
