b721a2d3d25e8536c884895d1c974137361d2cbe
Derek Huang <derekhuang@google.com>
2022-12-14 09:48:16 +0000

    PCHG: Support firmware update for CPS8200 wireless charger
    This patch allows the host to update firmware of cps8200 via I2C
    without the help of IRQ.
    
    An updater (e.g. ectool) can issue EC_PCHG_UPDATE_CMD_OPEN,
    multiple EC_PCHG_UPDATE_CMD_WRITE, then EC_PCHG_UPDATE_CLOSE to
    update the firmware of CPS8200.
    
    BUG=b:195708351
    BRANCH=none
    TEST=ectool pchg 0 update 0x3 0x0 ./CPS8200_A0_40_V3_CRCBEA6.bin
    
    Change-Id: Ifd73ae2b28baccaad8d2b4233d1036317b40045d
    Signed-off-by: Derek Huang <derekhuang@google.com>

--- include/peripheral_charger.h
+++ include/peripheral_charger.h
@@ -219,6 +219,14 @@ enum pchg_chipset_state {
 	PCHG_CHIPSET_STATE_COUNT
 };
 
+enum pchg_cfg {
+	/*
+	 * IRQ is not required for firmware update, update_open, update_write
+	 * and update_close may block and must return after completion.
+	 */
+	PCHG_CFG_FW_UPDATE_SYNC = BIT(0),
+};
+
 /**
  * Data struct describing the configuration of a peripheral charging port.
  */
@@ -235,6 +243,13 @@ struct pchg_config {
 	uint32_t block_size;
 	/* RF charge duration in msec. Set it to 0 to disable RF charge. */
 	uint16_t rf_charge_msec;
+	enum pchg_cfg flags;
+};
+
+struct cps8x00_update {
+	uint8_t cmd_id;
+	uint16_t crc;
+	uint32_t firmware_len;
 };
 
 struct pchg_update {
@@ -250,6 +265,11 @@ struct pchg_update {
 	uint8_t data_ready;
 	/* Partial data of new firmware */
 	uint8_t data[128];
+	/* Driver data for firmware update */
+	union {
+		struct cps8x00_update cps8200_update;
+		/* other driver data follows in the future */
+	} driver_data;
 };
 
 struct pchg_policy_t {
