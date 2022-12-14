92fcd9e019381af82b20faf98620d22d8db632b9
Jason Yuan <jasonyuan@google.com>
2022-12-14 15:23:05 -0800

    zephyr: move usbc interrupt handler to shim
    TCPC interrupt no longer uses gpio-int. Instead the shim tcpc conpletely
    handles its own interrupt.
    
    The programs nissa and intelrvp are excluded from this CL. They will be
    refactored to use the shim tcpc driver in a follow-up CL.
    
    BUG=b:254148652
    TEST=twister, usbc charging on villager and lazor
    BRANCH=none
    
    Change-Id: Ieeb4fb61ca6cf8f44df212a6cf520d265cff5147
    Signed-off-by: Jason Yuan <jasonyuan@google.com>

--- zephyr/shim/include/usbc/tcpc_rt1718s.h
+++ zephyr/shim/include/usbc/tcpc_rt1718s.h
@@ -18,7 +18,12 @@
 		},                                                             \
 		.drv = &rt1718s_tcpm_drv,                                      \
 		.flags = DT_PROP(id, tcpc_flags),                              \
-		.alert_signal = COND_CODE_1(DT_NODE_HAS_PROP(id, int_pin),     \
-			(GPIO_SIGNAL(DT_PHANDLE(id, int_pin))),                \
-			(GPIO_LIMIT)),                                         \
+		.int_cfg = GPIO_DT_SPEC_GET_OR(id, irq_gpios, {}),             \
 	},
+
+#define RT17118S_CHECK_FLAGS(id)                          \
+	BUILD_ASSERT((DT_PROP(id, tcpc_flags) &           \
+		      TCPC_FLAGS_ALERT_ACTIVE_HIGH) == 0, \
+		     "incorrect tcpc interrupt configuration for RT1718S");
+
+DT_FOREACH_STATUS_OKAY(RT1718S_TCPC_COMPAT, RT17118S_CHECK_FLAGS)
