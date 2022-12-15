ed9d654e0ba7640c305d399b0b4fe6a8a3fd53b3
Jes B. Klinke <jbk@chromium.org>
2022-12-14 21:36:38 -0800

    Introduce CONFIG_SPI_MUTABLE_DEVICE_LIST
    Previously, one of the effects of CONFIG_FINGERPRINT_MCU was that
    spi_devices[] would not be declared 'const', enabling run-time
    reconfiguration of clock divisor or other fields of the struct.
    
    This CL separates the const-ness into a new configuration option.
    
    BUG=b:192262089
    TEST=build hyperdebug
    
    Change-Id: I895a4ec04759706be14bf544fb9b9d66c3b46131

--- board/nucleo-dartmonkey/board.h
+++ board/nucleo-dartmonkey/board.h
@@ -34,6 +34,7 @@
 /* SPI configuration for the fingerprint sensor */
 #define CONFIG_SPI_CONTROLLER
 #define CONFIG_SPI_FP_PORT 2 /* SPI4: third master config */
+#define CONFIG_SPI_MUTABLE_DEVICE_LIST
 
 #define CONFIG_FINGERPRINT_MCU
 
