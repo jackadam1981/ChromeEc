2c2bfc78b706ecf5d6981f822b0145baf5fc3da2
Eric Yilun Lin <yllin@chromium.org>
2022-12-17 14:34:37 +0800

    corsola: bring forward the fake type-c port count init timing
    Corsola emulates HDMI MUX over the current type-c MUX stack.
    The EC has to fake a type-c port to pass the MUX info.
    However, tcpc/pd tasks are not needed for the HDMI port.
    We want the tasks are not initiated on starting up, and increase the
    type-c port counts after the tasks are launched.
    
    However, on the normal image, the kernel uptime is much faster than on
    the test image. There is a chance that the cros-ec-typec getting
    the actual type-c port counts from board_get_usb_pd_port_count(),
    before we are able to fake it, and this will block the HDMI mux
    function.
    
    BUG=b:262898170
    TEST=reboot EC for 1900 times and no HDMI output errors
    BRANCH=none
    
    Change-Id: I8628c456c55ede796973ecbd6c720e2b2ac7d175
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/program/corsola/src/usbc.c
+++ zephyr/program/corsola/src/usbc.c
@@ -169,6 +169,7 @@ static void tasks_init_deferred(void)
 		ps185_hdmi_hpd_mux_set();
 	}
 }
+DECLARE_HOOK(HOOK_CHIPSET_PRE_INIT, tasks_init_deferred, HOOK_PRIO_PRE_DEFAULT);
 DECLARE_DEFERRED(tasks_init_deferred);
 
 static void baseboard_x_ec_gpio2_init(void)
@@ -215,8 +216,11 @@ static void baseboard_x_ec_gpio2_init(void)
 	 * After C1 port tasks finished, we intentionally increase the port
 	 * count by 1 for usb_mux to access the C1 virtual mux for notifying
 	 * mainlink direction.
+	 * The current delay is set to 1 second. cros-ec-typec module will
+	 * not be getting the fake type-c port count if the deferred call is
+	 * later than the kernel upstarts.
 	 */
-	hook_call_deferred(&tasks_init_deferred_data, 2 * SECOND);
+	hook_call_deferred(&tasks_init_deferred_data, SECOND);
 }
 DECLARE_HOOK(HOOK_INIT, baseboard_x_ec_gpio2_init, HOOK_PRIO_DEFAULT);
 
