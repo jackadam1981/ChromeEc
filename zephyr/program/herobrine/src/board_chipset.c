0f95927bde280495f79f6881d3c16407fb1d26d4
Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
2022-12-19 10:21:42 +0800

    herobrine: Rerun the ppc_init after 5V rail ready
    Due to the delay of the 5V rail enabling until 5V@3A is ready, the
    ppc_init may run when the PPC is not powered on. This CL reruns the
    ppc_init function after the 5V rail enables to prevent Type-C port
    no function.
    
    BUG=b:260192201
    BRANCH=none
    TEST=check boot log on evoker
    
    Change-Id: Ifd17e46e936d15fdfd7f93673606f51c9c8252d6
    Signed-off-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>

--- zephyr/program/herobrine/src/board_chipset.c
+++ zephyr/program/herobrine/src/board_chipset.c
@@ -14,6 +14,7 @@
 #include "hooks.h"
 #include "timer.h"
 #include "usb_pd.h"
+#include "usbc_ppc.h"
 
 #define CPRINTS(format, args...) cprints(CC_HOOK, format, ##args)
 #define CPRINTF(format, args...) cprintf(CC_HOOK, format, ##args)
@@ -70,6 +71,7 @@ static void wait_pd_ready(void)
 /* Called on AP S5 -> S3 transition */
 static void board_chipset_pre_init(void)
 {
+	int port;
 	if (!pp5000_inited) {
 		if (pd_ready_timeout.val) {
 			wait_pd_ready();
@@ -77,6 +79,9 @@ static void board_chipset_pre_init(void)
 		CPRINTS("Enable 5V rail");
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_s5), 1);
 		pp5000_inited = true;
+		msleep(10);
+		for (port = 0; port < CONFIG_USB_PD_PORT_MAX_COUNT; port++)
+			ppc_init(port);
 	}
 }
 DECLARE_HOOK(HOOK_CHIPSET_PRE_INIT, board_chipset_pre_init, HOOK_PRIO_DEFAULT);
