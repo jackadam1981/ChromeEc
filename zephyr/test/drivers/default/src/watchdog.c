00a1f19fe436490cfdcaf274fa49197158453679
Patryk Duda <pdk@semihalf.com>
2022-12-19 15:47:54 +0100

    shim/src/watchdog: Add support for watchdog helper
    Watchdog helper is used to print a warning along with useful debugging
    information before hardware watchdog will reset SOC. Zephyr provides
    counter based watchdog which is very similar to watchdog helper from
    EC.
    
    This patch adds support for using multiple watchdogs simultaneously.
    One of the watchdogs will be counter based, the other will be hardware
    based (e.g. IWDG on STM32).
    
    Watchdog tests was modified to address the problem with allocating new
    counter channel every time watchdog_init() is called while there is no
    way to free them.
    
    BUG=b:239712345
    BRANCH=none
    TEST=Add 'zephyr,counter-watchdog' node to DTS. Use 'waitms' command to
         check if a warning from watchdog based counter is triggered.
    TEST=./twister -i -T zephyr/test/drivers \
         --test external/platform/ec/zephyr/test/drivers/drivers.default
    
    Signed-off-by: Patryk Duda <pdk@semihalf.com>
    Change-Id: I66b5474d7e65978791450c2fd62dd3d345cd474a

--- zephyr/test/drivers/default/src/watchdog.c
+++ zephyr/test/drivers/default/src/watchdog.c
@@ -34,6 +34,7 @@
  * @brief Boolean to indicate watchdog alert triggered
  */
 bool wdt_warning_triggered;
+bool wdt_initialized;
 
 /**
  * @brief timer to used to validate watchdog expiries.
@@ -52,37 +53,10 @@ static void watchdog_before(void *state)
 	/* When shuffling need watchdog initialized and running
 	 * for other tests.
 	 */
-	(void)watchdog_init();
-	(void)wdt_setup(wdt, 0);
-}
-
-/**
- * @brief Watchdog test teardown handler.
- */
-static void watchdog_after(void *state)
-{
-	ARG_UNUSED(state);
-	wdt_warning_triggered = false;
-}
-
-/**
- * @brief TestPurpose: Verify watchdog initialization.
- *
- * @details
- * Validate watchdog initialization.
- *
- * Expected Results
- *  - Successful on first init.
- *  - Failure on second init.
- */
-ZTEST(watchdog, test_watchdog_init)
-{
-	int retval = EC_SUCCESS;
-
-	/* Test already initialized (initialized in watchdog_before) */
-	retval = watchdog_init();
-	zassert_equal(-ENOMEM, retval, "Expected -ENOMEM, returned %d.",
-		      retval);
+	if (!wdt_initialized) {
+		(void)watchdog_init();
+		wdt_initialized = true;
+	}
 }
 
 /**
@@ -100,6 +74,7 @@ ZTEST(watchdog, test_watchdog_reload)
 	int safe_wait_ms = DEFAULT_WDT_EXPIRY_MS / 2;
 
 	zassert_false(wdt_warning_triggered, "Watchdog timer expired early.");
+	watchdog_reload();
 	for (i = 0; i < 10; i++) {
 		k_timer_start(&ktimer, K_MSEC(safe_wait_ms), K_NO_WAIT);
 		k_busy_wait(safe_wait_ms * 1000);
@@ -137,5 +112,5 @@ ZTEST(watchdog, test_wdt_warning_handler)
 /**
  * @brief Test Suite: Verifies watchdog functionality.
  */
-ZTEST_SUITE(watchdog, drivers_predicate_post_main, NULL, watchdog_before,
-	    watchdog_after, NULL);
+ZTEST_SUITE(watchdog, drivers_predicate_post_main, NULL, watchdog_before, NULL,
+	    NULL);
