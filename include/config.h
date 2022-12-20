c42d8dc9bffc09c0bfd2b99ca5441193f5ab7a6e
Rob Barnes <robbarnes@google.com>
2022-12-20 15:36:24 +0000

    system: Introduce Memory Dump Host Commands
    Add memory dump host commands and memory dump backend.
    These APIs differ from the Zephyr Core Dump because the Zephyr Core Dump
    module did not fit the use case needed for system safe mode.
    
    BUG=b:263250131
    BRANCH=None
    TEST=Passing memory_dump_hostcmds unit test
    
    Change-Id: Iebddc2c1a65d741d3e7e5936bd6156647f193dc6
    Signed-off-by: Rob Barnes <robbarnes@google.com>

--- include/config.h
+++ include/config.h
@@ -2591,6 +2591,9 @@
 /* Include host command to control I2C busses (get, set speed, etc.) */
 #undef CONFIG_HOSTCMD_I2C_CONTROL
 
+/* Host commands for dumping memory after a panic */
+#undef CONFIG_HOSTCMD_MEMORY_DUMP
+
 /*
  * List of host commands whose debug output will be suppressed
  * By default remove periodic commands and commands called often (SENSE).
