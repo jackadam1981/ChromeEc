c1a68a5b5f57e5c6421a97b358ed65dbbbed39dd
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2022-12-19 16:55:50 -0800

    util: Add battery temperature to EC_CMD_BATTERY_GET_DYNAMIC
    BRANCH=None
    BUG=None
    TEST=Issue command EC_CMD_BATTERY_GET_DYNAMIC
         It should include battery tempreature.
    
    Change-Id: I1f9c86ecb71e15785a2945011924a042afaef937
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>

--- util/ectool.cc
+++ util/ectool.cc
@@ -8072,6 +8072,7 @@ int get_battery_command(int index)
 		goto cmd_error;
 	printf("  Desired current         %u mA\n", dynamic_r.desired_current);
 
+	printf("  Temperature             %.1f °C\n", (double)(dynamic_r.temperature - 2731) / 10);
 	print_battery_flags(dynamic_r.flags);
 	return 0;
 
