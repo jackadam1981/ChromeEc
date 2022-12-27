ee874d574e6db4a8e718a27f120fce53f573ed9e
Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
2022-12-27 10:46:42 +0800

    herobrine: Delay enabling 5V if battery is disconnected
    Wait negotiated VBUS transition before enabling 5V rail when
    battery is disconnected
    
    BUG=b:260192201
    BRANCH=none
    TEST=zmake build evoker
    
    Change-Id: Ieebd10ea32b26e9f1f3f3e438351ba519ae1b337
    Signed-off-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>

--- zephyr/program/herobrine/src/board_chipset.c
+++ zephyr/program/herobrine/src/board_chipset.c
@@ -71,6 +71,12 @@ static void wait_pd_ready(void)
 static void board_chipset_pre_init(void)
 {
 	if (!pp5000_inited) {
+		if (battery_get_disconnect_state() !=
+		    BATTERY_NOT_DISCONNECTED) {
+			pd_ready_timeout = get_time();
+			pd_ready_timeout.val += PD_READY_TIMEOUT;
+			CPRINTS("Delay 5V due to battery disconnect");
+		}
 		if (pd_ready_timeout.val) {
 			wait_pd_ready();
 		}
