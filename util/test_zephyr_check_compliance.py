02d79832bc2d94be32aef09e31dad4e413c75371
Fabio Baltieri <fabiobaltieri@google.com>
2022-12-28 16:51:42 +0000

    PRESUBMIT: integrate upstream bindings and yaml check
    Add a wrapper script to use the upstream Zephyr check_compliance.py
    script in the EC code base.
    
    That script has various presubmit type of checks that are used upstream,
    and we can reuse some of those internally as is. The wrapper handles the
    vpython dependencies, skips if there's no Zephyr files touched and runs
    the YAMLLint and DevicetreeBindings.
    
    BRANCH=none
    BUG=b:264661333
    TEST=create some test patches to touch the non compliant files, ran the
    script on those
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>
    Change-Id: Ibe77e92092013780befa54e6c02ab17101b014fb

---
+++ util/test_zephyr_check_compliance.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unit tests for check_zephyr_project_config.py"""
+
+import sys
+import unittest
+
+import mock  # pylint:disable=import-error
+import zephyr_check_compliance
+
+# pylint:disable=protected-access
+
+
+class TestZephyrCheckCompliance(unittest.TestCase):
+    """Tests for zephyr_check_compliance."""
+
+    @mock.patch("check_compliance.get_files")
+    def test_changed_files_prefix(self, get_files_mock):
+        """Test _changed_files_prefix."""
+        get_files_mock.return_value = [
+            "a/file",
+            "b/file",
+            "c/file",
+        ]
+
+        out = zephyr_check_compliance._changed_files_prefix("x/", "ref")
+        self.assertFalse(out)
+        out = zephyr_check_compliance._changed_files_prefix("b/", "ref")
+        self.assertTrue(out)
+
+    @mock.patch("zephyr_check_compliance._changed_files_prefix")
+    @mock.patch("check_compliance.main")
+    def test_main(self, main_mock, changed_files_prefix_mock):
+        """Tests the main function."""
+        changed_files_prefix_mock.return_value = True
+        sys.argv = [""]
+
+        zephyr_check_compliance.main(["ref"])
+
+        changed_files_prefix_mock.assert_called_with("zephyr/", "ref~1..ref")
+        main_mock.assert_called_with()
+        self.assertEqual(
+            sys.argv,
+            [
+                "<internal>",
+                "-m",
+                "YAMLLint",
+                "-m",
+                "DevicetreeBindings",
+                "-c",
+                "ref~1..ref",
+            ],
+        )
+
+    @mock.patch("zephyr_check_compliance._changed_files_prefix")
+    @mock.patch("check_compliance.main")
+    def test_main_skip(self, main_mock, changed_files_prefix_mock):
+        """Tests the main function."""
+        changed_files_prefix_mock.return_value = False
+
+        zephyr_check_compliance.main(["ref"])
+
+        changed_files_prefix_mock.assert_called_with("zephyr/", "ref~1..ref")
+        self.assertEqual(main_mock.call_count, 0)
