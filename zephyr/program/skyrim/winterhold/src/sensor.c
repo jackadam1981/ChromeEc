265ce11734d17e002e35d68f4c0aabb39f6028c4
johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>
2023-01-05 14:52:43 +0800

    Winterhold:  Add fw_config to support two base sensors
    Use fw_config to separate LIS2DW12 and BMI323.
    
    LOW_COVERAGE_REASON=Skyrim board tests not created yet: b/247151116
    BRANCH=None
    BUG=b:262321157
    TEST=zmake build winterhold
    
    Change-Id: I71d78f232867a54c46a003308590521fd687d94b
    Signed-off-by: johnwc_yeh <johnwc_yeh@compal.corp-partner.google.com>

--- zephyr/program/skyrim/winterhold/src/sensor.c
+++ zephyr/program/skyrim/winterhold/src/sensor.c
@@ -5,6 +5,7 @@
 
 #include "common.h"
 #include "cros_board_info.h"
+#include "cros_cbi.h"
 #include "driver/accel_lis2dw12.h"
 #include "driver/accelgyro_bmi3xx.h"
 #include "hooks.h"
@@ -12,26 +13,26 @@
 
 void base_accel_interrupt(enum gpio_signal signal)
 {
-	int ret;
 	uint32_t val;
 
-	ret = cbi_get_board_version(&val);
+	cros_cbi_get_fw_config(FW_BASE_SENSOR, &val);
 
-	if (ret == EC_SUCCESS && val < 1)
+	if (val == FW_BASE_BMI323)
 		bmi3xx_interrupt(signal);
-	else
+	else if (val == FW_BASE_LIS2DW12)
 		lis2dw12_interrupt(signal);
 }
 
 static void motionsense_init(void)
 {
-	int ret;
 	uint32_t val;
 
-	ret = cbi_get_board_version(&val);
-
-	if (ret == EC_SUCCESS && val < 1) {
+	cros_cbi_get_fw_config(FW_BASE_SENSOR, &val);
+	if (val == FW_BASE_BMI323) {
 		MOTIONSENSE_ENABLE_ALTERNATE(alt_base_accel);
+		ccprints("BASE ACCEL is BMI323");
+	} else if (val == FW_BASE_LIS2DW12) {
+		ccprints("BASE ACCEL IS LIS2DW12");
 	}
 }
 DECLARE_HOOK(HOOK_INIT, motionsense_init, HOOK_PRIO_DEFAULT);
