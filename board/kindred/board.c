ecfa9172ba077fed335c4f787093b824d6816856
Boris Mittelberg <bmbm@google.com>
2023-01-11 15:30:13 -0800

    kindred: fix set TCPC_AUX_SWITCH to 0xC on Port 0
    Updating the mux via a callback instead of interrupt
    
    BUG=b:183123775
    BRANCH=main,firmware-hatch-12672.B
    TEST=make BOARD=kindred
    
    Change-Id: Ic90202937bda5b8c8028b9f9affae021095521b3

--- board/kindred/board.c
+++ board/kindred/board.c
@@ -174,10 +174,46 @@ const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_MAX_COUNT] = {
 	},
 };
 
+<<<<<<< PATCH SET (f32ec7 kindred: fix set TCPC_AUX_SWITCH to 0xC on Port 0)
+static int board_anx7447_mux_set_c0(const struct usb_mux *me,
+				    mux_state_t mux_state)
+{
+	int port = me->usb_port;
+	int rv = EC_SUCCESS;
+
+	if (port != USB_PD_PORT_TCPC_0)
+		return rv;
+
+	if (gpio_get_level(GPIO_CCD_MODE_ODL))
+		return rv;
+
+	/*
+	 * Expect to set AUX_SWITCH to 0, but 0xc isolates the DP_AUX
+	 * signal from SBU.
+	 */
+	CPRINTS("C%d: AUX_SW_SEL=0x%x", port, 0xc);
+	if (tcpc_write(port, ANX7447_REG_TCPC_AUX_SWITCH, 0xc))
+		CPRINTS("C%d: Setting AUX_SW_SEL failed", port);
+
+	return rv;
+}
+
+const struct usb_mux_chain usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
+=======
 struct usb_mux usb_muxes[CONFIG_USB_PD_PORT_MAX_COUNT] = {
+>>>>>>> BASE      (f7a5b6 dratini: Set TCPC_AUX_SWITCH to 0xC on Port 0 on mux set)
 	[USB_PD_PORT_TCPC_0] = {
+<<<<<<< PATCH SET (f32ec7 kindred: fix set TCPC_AUX_SWITCH to 0xC on Port 0)
+		.mux = &(const struct usb_mux) {
+			.usb_port = USB_PD_PORT_TCPC_0,
+			.driver = &anx7447_usb_mux_driver,
+			.board_set = &board_anx7447_mux_set_c0,
+			.hpd_update = &anx7447_tcpc_update_hpd_status,
+		},
+=======
 		.driver = &anx7447_usb_mux_driver,
 		.hpd_update = &anx7447_tcpc_update_hpd_status,
+>>>>>>> BASE      (f7a5b6 dratini: Set TCPC_AUX_SWITCH to 0xC on Port 0 on mux set)
 	},
 	[USB_PD_PORT_TCPC_1] = {
 		.driver = &tcpci_tcpm_usb_mux_driver,
@@ -530,39 +566,12 @@ static void board_update_sensor_config_from_sku(void)
 	}
 }
 
-static void anx7447_set_aux_switch(void)
-{
-	const int port = USB_PD_PORT_TCPC_0;
-
-	/* Debounce */
-	if (gpio_get_level(GPIO_CCD_MODE_ODL))
-		return;
-
-	/*
-	 * Expect to set AUX_SWITCH to 0, but 0xc isolates the DP_AUX
-	 * signal from SBU.
-	 */
-	CPRINTS("C%d: AUX_SW_SEL=0x%x", port, 0xc);
-	if (tcpc_write(port, ANX7447_REG_TCPC_AUX_SWITCH, 0xc))
-		CPRINTS("C%d: Setting AUX_SW_SEL failed", port);
-}
-DECLARE_DEFERRED(anx7447_set_aux_switch);
-
-void ccd_mode_isr(enum gpio_signal signal)
-{
-	/* Wait 2 seconds until all mux setting is done by PD task */
-	hook_call_deferred(&anx7447_set_aux_switch_data, 2 * SECOND);
-}
-
 static void board_init(void)
 {
 	/* Initialize Fans */
 	setup_fans();
 	/* Enable HDMI HPD interrupt. */
 	gpio_enable_interrupt(GPIO_HDMI_CONN_HPD);
-	/* Trigger once to set mux in case CCD cable is already connected. */
-	ccd_mode_isr(GPIO_CCD_MODE_ODL);
-	gpio_enable_interrupt(GPIO_CCD_MODE_ODL);
 	/* Select correct gpio signal for PP5000_A control */
 	board_gpio_set_pp5000();
 	/* Use sku_id to set motion sensor count */
