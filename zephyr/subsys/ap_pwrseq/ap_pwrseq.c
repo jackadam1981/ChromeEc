744383a4ec77d116fef6370c28cb04cd3dbb8f32
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2023-01-12 18:12:47 -0800

    ap_pwrseq: Enable driver to use state machine API
    This CL includes state machine API calls in AP power sequence driver
    implementation.
    
    BUG=b:217952699
    BRANCH=none
    TEST=zmake build
    
    Change-Id: Ibe48608f1e09879a636a5e9ce7ac0e5a25214fe0
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>

--- zephyr/subsys/ap_pwrseq/ap_pwrseq.c
+++ zephyr/subsys/ap_pwrseq/ap_pwrseq.c
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-#include "ap_power/ap_pwrseq.h"
+#include "ap_power/ap_pwrseq_sm.h"
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -28,11 +28,36 @@ struct ap_pwrseq_data {
 	struct ap_pwrseq_cb_list exit_list;
 };
 
+/* Resolve into substate name string */
+#define AP_PWRSEQ_SUB_STATE_STR_DEFINE_WITH_COMA(state) state,
+
+#define AP_PWRSEQ_EACH_SUB_STATE_STR_DEFINE(node_id, prop, idx) \
+	AP_PWRSEQ_SUB_STATE_STR_DEFINE_WITH_COMA(               \
+		DT_CAT5(node_id, _P_, prop, _IDX_, idx))
+
+#define AP_PWRSEQ_EACH_SUB_STATE_STR_DEF_NODE_CHILD_DEFINE(node_id)          \
+	COND_CODE_1(                                                         \
+		DT_NODE_HAS_PROP(node_id, application),                      \
+		(DT_FOREACH_PROP_ELEM(node_id, application,                  \
+				      AP_PWRSEQ_EACH_SUB_STATE_STR_DEFINE)), \
+		(COND_CODE_1(DT_NODE_HAS_PROP(node_id, chipset),             \
+			     (DT_FOREACH_PROP_ELEM(                          \
+				     node_id, chipset,                       \
+				     AP_PWRSEQ_EACH_SUB_STATE_STR_DEFINE)),  \
+			     ())))
+
 static const char *const ap_pwrseq_state_str[AP_POWER_STATE_COUNT] = {
-	"AP_POWER_STATE_UNINIT", "AP_POWER_STATE_G3", "AP_POWER_STATE_S5",
-	"AP_POWER_STATE_S4",	 "AP_POWER_STATE_S3", "AP_POWER_STATE_S2",
-	"AP_POWER_STATE_S1",	 "AP_POWER_STATE_S0",
-	/* TODO: Add substate name strings */
+	"AP_POWER_STATE_UNINIT",
+	"AP_POWER_STATE_G3",
+	"AP_POWER_STATE_S5",
+	"AP_POWER_STATE_S4",
+	"AP_POWER_STATE_S3",
+	"AP_POWER_STATE_S2",
+	"AP_POWER_STATE_S1",
+	"AP_POWER_STATE_S0",
+	DT_FOREACH_STATUS_OKAY(
+		ap_pwrseq_sub_states,
+		AP_PWRSEQ_EACH_SUB_STATE_STR_DEF_NODE_CHILD_DEFINE)
 };
 BUILD_ASSERT(ARRAY_SIZE(ap_pwrseq_state_str) == AP_POWER_STATE_COUNT);
 
@@ -120,24 +145,42 @@ static uint32_t ap_pwrseq_wait_event(const struct device *dev)
 static void ap_pwrseq_thread(void *arg, void *unused1, void *unused2)
 {
 	struct device *const dev = (struct device *)arg;
+	struct ap_pwrseq_data *const data = dev->data;
+	enum ap_pwrseq_state cur_state, new_state;
+	uint32_t events;
 
 	LOG_INF("Power Sequence thread start");
 	while (true) {
-		uint32_t events = ap_pwrseq_wait_event(dev);
-
-		if (events) {
-			LOG_DBG("Events posted: 0x%0#x", events);
+		events = ap_pwrseq_wait_event(dev);
+		if (!events) {
+			continue;
 		}
+		LOG_DBG("Event posted: 0x%0#x", events);
 
 		/**
-		 * TODO: Process state machine until the new state is not equal
-		 * to previous state and generate callbacks.
+		 * Process generated events and keep looping while state
+		 * transitions are occurring.
 		 **/
-		ap_pwrseq_send_entry_callback(dev, AP_POWER_STATE_UNDEF,
-					      AP_POWER_STATE_UNDEF);
+		while (true) {
+			cur_state = ap_pwrseq_sm_get_cur_state(data->sm_data);
+			if (ap_pwrseq_sm_run_state(data->sm_data, events)) {
+				/* Was this terminated? */
+				return;
+			}
 
-		ap_pwrseq_send_exit_callback(dev, AP_POWER_STATE_UNDEF,
-					     AP_POWER_STATE_UNDEF);
+			/* Check if state transition took place */
+			new_state = ap_pwrseq_sm_get_cur_state(data->sm_data);
+			if (cur_state == new_state) {
+				break;
+			}
+			LOG_INF("%s -> %s", ap_pwrseq_get_state_str(cur_state),
+				ap_pwrseq_get_state_str(new_state));
+
+			ap_pwrseq_send_entry_callback(dev, cur_state,
+						      new_state);
+
+			ap_pwrseq_send_exit_callback(dev, cur_state, new_state);
+		}
 	}
 }
 
@@ -154,9 +197,15 @@ K_THREAD_DEFINE(ap_pwrseq_tid, CONFIG_AP_PWRSEQ_STACK_SIZE, ap_pwrseq_thread,
 static int ap_pwrseq_driver_init(const struct device *dev)
 {
 	struct ap_pwrseq_data *const data = dev->data;
+	int ret;
 
-	/* TODO: Obtain state machine data reference. */
+	data->sm_data = ap_pwrseq_sm_get_instance();
 	k_event_init(&data->evt);
+
+	ret = ap_pwrseq_sm_init(data->sm_data, ap_pwrseq_tid);
+	if (ret) {
+		return ret;
+	}
 
 	if (IS_ENABLED(CONFIG_AP_PWRSEQ_AUTOSTART)) {
 		ap_pwrseq_thread_start(dev);
@@ -192,9 +241,7 @@ enum ap_pwrseq_state ap_pwrseq_get_current_state(const struct device *dev)
 {
 	struct ap_pwrseq_data *const data = dev->data;
 
-	/* TODO: Call function to get current state from state machine */
-	(void)data;
-	return AP_POWER_STATE_UNDEF;
+	return ap_pwrseq_sm_get_cur_state(data->sm_data);
 }
 
 const char *const ap_pwrseq_get_state_str(enum ap_pwrseq_state state)
