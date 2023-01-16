3d12473948d545c387857d81cdad340c5322dfd4
Diana Z <dzigterman@chromium.org>
2023-01-16 16:13:58 -0700

    TCPMv2: Queue Attention messages for consumption
    Set up a queue within the DPM to hold VDM:Attention messages which the
    AP will be able to deplete at its leisure.
    
    BRANCH=None
    BUG=b:208884535
    TEST=builds with and without AP VDM control
    
    Change-Id: Ia73ac3c6164251ab021776fdca252033646797dd
    Signed-off-by: Diana Z <dzigterman@chromium.org>

--- include/usb_pd_dpm_sm.h
+++ include/usb_pd_dpm_sm.h
@@ -98,6 +98,29 @@ void dpm_vdm_naked(int port, enum tcpci_msg_type type, uint16_t svid,
  */
 enum ec_status dpm_copy_vdm_reply(int port, uint8_t *type, uint8_t *size,
 				  uint32_t *buf);
+
+/*
+ * Informs the DPM of a received Attention message.  Note: all Attention
+ * messages are assumed to be SOP since cables are disallowed from sending
+ * this type of VDM.
+ *
+ * @param[in] port		USB-C port number
+ * @param[in] vdo_objects	Number of objects filled in
+ * @param[in] buf		Buffer containing received VDM (header and VDO)
+ */
+void dpm_consume_attention(int port, uint8_t vdo_objects, uint32_t *buf);
+
+/*
+ * Copy and pop the last VDM:Attention from the DPM queue
+ *
+ * @param[in] port		USB-C port number
+ * @param[out] length		Number of 32-bit objects filled in
+ * @param[out] buf		Buffer to copy VDM header and VDO
+ * @param[out] items_left	Number of Attention messages left in the queue
+ */
+void dpm_vdm_attention_pop(int port, int *length, uint32_t *buf,
+			   int *items_left);
+
 /*
  * Determines the current allocation for the connection, past the basic
  * CONFIG_USB_PD_PULLUP value set by the TC (generally 1.5 A)
