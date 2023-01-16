8a7f84a75dfdf958bf745d16fd03fa1a53071d52
Ting Shen <phoenixshen@google.com>
2023-01-16 15:36:07 +0800

    flash: allow zero num_banks_desc in flash_info request
    Some external tools (e.g. flashrom) sends EC_CMD_FLASH_INFO with
    num_banks_desc=0 to get the total number of banks. EC should allow this
    type of request.
    
    Also add a test to cover this use case.
    
    BUG=b:265472751
    TEST=`flashrom -p ec -w <ec.bin>` on tentacruel
    BRANCH=none
    
    Change-Id: Iab97ac754bac87067f949cf9435156be91b07ebc
    Signed-off-by: Ting Shen <phoenixshen@google.com>

--- common/flash.c
+++ common/flash.c
@@ -227,9 +227,6 @@ int crec_flash_response_fill_banks(struct ec_response_flash_info_2 *r,
 	const struct ec_flash_bank *banks = flash_bank_array;
 	int banks_to_copy = MIN(ARRAY_SIZE(flash_bank_array), num_banks);
 
-	if (num_banks < 1)
-		return EC_RES_INVALID_PARAM;
-
 	memcpy(r->banks, banks, banks_to_copy * sizeof(struct ec_flash_bank));
 	r->num_banks_desc = banks_to_copy;
 	r->num_banks_total = ARRAY_SIZE(flash_bank_array);
@@ -243,8 +240,12 @@ int crec_flash_response_fill_banks(struct ec_response_flash_info_2 *r,
 int crec_flash_response_fill_banks(struct ec_response_flash_info_2 *r,
 				   int num_banks)
 {
-	if (num_banks < 1)
-		return EC_RES_INVALID_PARAM;
+	if (num_banks < 1) {
+		r->num_banks_desc = 0;
+		r->num_banks_total = 1;
+
+		return EC_RES_SUCCESS;
+	}
 
 	r->banks[0].count = crec_flash_total_banks();
 	r->banks[0].size_exp = __fls(CONFIG_FLASH_BANK_SIZE);
