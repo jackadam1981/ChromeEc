747c8a3ab8ec1c8704c9df58849dc2309d6f2e35
Jes B. Klinke <jbk@chromium.org>
2023-01-17 10:06:06 -0800

    board/hyperdebug: Add logic analyzer support
    This CL adds special purpose command to initiage monitoring (edge
    detection) on a given set of pins, and record the precise time of each
    edge, allowing in to be streamed to the host machine (currently in a
    text-based format on the console.)
    
    Change-Id: I899010e258cd5420a176645e9bba634229cb41be

--- board/hyperdebug/gpio.c
+++ board/hyperdebug/gpio.c
@@ -4,10 +4,174 @@
  */
 /* HyperDebug GPIO logic and console commands */
 
+#include "atomic.h"
 #include "common.h"
 #include "console.h"
 #include "gpio.h"
+#include "hooks.h"
+#include "registers.h"
+#include "shared_mem.h"
+#include "task.h"
+#include "timer.h"
 #include "util.h"
+
+/*
+ * A cyclic buffer is used to record events (edges) of one or more GPIO
+ * signals.  Data is stored in the memory range immediately following this
+ * struct.  Each event records the time since the previous event, and the
+ * signal that changed (the direction of change is not explicitly recorded).
+ */
+struct cyclic_buffer_header_t {
+	/* Number of signals being monitored in this buffer. */
+	uint8_t num_signals;
+	/* The number of bits required to represent 0..num_signals-1. */
+	uint8_t signal_bits;
+	/* Sticky bit recording if overflow occurred. */
+	volatile uint8_t overflow;
+	/* Time of the most recent event, updated from interrupt context. */
+	volatile timestamp_t head_time;
+	/* Time base that the oldest event is relative to. */
+	timestamp_t tail_time;
+	/* Index at which new records are placed, updated from interrupt. */
+	volatile uint32_t head;
+	/* Index af oldest record. */
+	uint32_t tail;
+	/* Size of cyclic byte buffer, head and tail wrap to zero here. */
+	uint32_t size;
+};
+
+/*
+ * The STM32L5 has 16 edge detection circuits.  Each pin can only be used with
+ * one of them.  That is, detector 0 can take its input from one of pins A0,
+ * B0, C0, ..., while detector 1 can choose between A1, B1, etc.
+ *
+ * Information about the current use of each detection circuit is stored in 16
+ * "slots" below.
+ */
+struct monitoring_slot_t {
+	/* Link to buffer recording edges of this signal. */
+	struct cyclic_buffer_header_t *buffer;
+	/* EC enum id of the signal used by this detection slot. */
+	int gpio_signal;
+	/* The index of the signal as used in the recording buffer. */
+	uint8_t signal_no;
+	/* Most recently recorded level of the signal. */
+	uint8_t head_level;
+	/* Level as of the current oldest end (tail) of the recording. */
+	uint8_t tail_level;
+};
+struct monitoring_slot_t monitoring_slots[16];
+
+/*
+ * Counts unacknowledged buffer overflows.  Whenever non-zero, the red LED
+ * will flash.
+ */
+atomic_t num_cur_error_conditions;
+
+/*
+ * Counts the number of cyclic buffers currently in existence, the green LED
+ * will flash whenever this is non-zero, indicating the monitoring activity.
+ */
+int num_cur_monitoring = 0;
+
+static __attribute__((noinline)) void overflow(struct monitoring_slot_t *slot)
+{
+	struct cyclic_buffer_header_t *buffer_header = slot->buffer;
+	gpio_disable_interrupt(slot->gpio_signal);
+	buffer_header->overflow = 1;
+	atomic_add(&num_cur_error_conditions, 1);
+}
+
+void gpio_edge(enum gpio_signal signal)
+{
+	/*
+	 * Hardware has detected one or more edges since last time.  We
+	 * process by looking at the current level of the signal.  If opposite
+	 * the most recent level, we record one edge, if the same as most
+	 * recent level, we record two edges, that is, a zero-width pulse.
+	 * This is useful for tests trying to verify e.g. that there are no
+	 * glitches on a paricular signal, and want to know about any pulses,
+	 * however narrow.
+	 */
+	struct monitoring_slot_t *slot =
+		monitoring_slots + GPIO_MASK_TO_NUM(gpio_list[signal].mask);
+	int current_level = gpio_get_level(signal);
+	timestamp_t now = get_time();
+	struct cyclic_buffer_header_t *buffer_header = slot->buffer;
+	uint8_t *buffer_data = (uint8_t *)(buffer_header + 1);
+	uint32_t tail = buffer_header->tail, head = buffer_header->head,
+		 size = buffer_header->size;
+	uint64_t diff = now.val - buffer_header->head_time.val;
+	uint8_t signal_bits = buffer_header->signal_bits;
+
+	/*
+	 * Insert an entry recording the time since last event, and which
+	 * signal changed (the direction of the edge is not explicitly
+	 * recorded, as it can be derived from the initial level
+	 */
+	diff <<= signal_bits;
+	diff |= slot->signal_no;
+	do {
+		buffer_data[head++] = ((diff >= 0x80) ? 0x80 : 0x00) |
+				      (diff & 0x7F);
+		diff >>= 7;
+		if (head == size)
+			head = 0;
+		if (head == tail)
+			return overflow(slot);
+	} while (diff);
+
+	/*
+	 * If current level equals the previous level, then record an
+	 * addtional edge 0ms after the previous.
+	 */
+	if (!!current_level == !!slot->head_level) {
+		buffer_data[head++] = slot->signal_no;
+		if (head == size)
+			head = 0;
+		if (head == tail)
+			return overflow(slot);
+	} else {
+		slot->head_level = current_level;
+	}
+	buffer_header->head = head;
+	buffer_header->head_time = now;
+}
+
+static void board_gpio_init(void)
+{
+	/* Mark every slot as unused. */
+	for (int i = 0; i < ARRAY_SIZE(monitoring_slots); i++)
+		monitoring_slots[i].gpio_signal = GPIO_COUNT;
+}
+DECLARE_HOOK(HOOK_INIT, board_gpio_init, HOOK_PRIO_DEFAULT);
+
+static void stop_all_gpio_monitoring(void)
+{
+	struct monitoring_slot_t *slot;
+	struct cyclic_buffer_header_t *buffer_header;
+	for (int i = 0; i < ARRAY_SIZE(monitoring_slots); i++) {
+		slot = monitoring_slots + i;
+		if (!slot->buffer)
+			continue;
+
+		/* Disable interrupts for all signals feeding into the same
+		 * cyclic buffer. */
+		buffer_header = slot->buffer;
+		for (int j = i; j < ARRAY_SIZE(monitoring_slots); j++) {
+			slot = monitoring_slots + i;
+			if (slot->buffer != buffer_header)
+				continue;
+			gpio_disable_interrupt(slot->gpio_signal);
+			slot->gpio_signal = GPIO_COUNT;
+		}
+		/* Deallocate this one cyclic buffer. */
+		num_cur_monitoring--;
+		if (buffer_header->overflow)
+			atomic_sub(&num_cur_error_conditions, 1);
+		shared_mem_release((char *)buffer_header);
+	}
+}
 
 /**
  * Find a GPIO signal by name.
@@ -102,3 +266,280 @@ static int command_gpio_pull_mode(int argc, const char **argv)
 DECLARE_CONSOLE_COMMAND_FLAGS(gpiopullmode, command_gpio_pull_mode,
 			      "name <none | up | down>",
 			      "Set a GPIO weak pull mode", CMD_FLAG_RESTRICTED);
+
+static int command_gpio_monitoring_start(int argc, const char **argv)
+{
+	int gpios[16];
+	int gpio_num = argc - 3;
+	int i;
+	timestamp_t now;
+	int rv;
+	struct cyclic_buffer_header_t *buf;
+	struct monitoring_slot_t *slot;
+
+	if (gpio_num <= 0 || gpio_num > 16)
+		return EC_ERROR_PARAM_COUNT;
+
+	for (i = 0; i < gpio_num; i++) {
+		gpios[i] = find_signal_by_name(argv[3 + i]);
+		if (gpios[i] == GPIO_COUNT)
+			return EC_ERROR_PARAM3 + i; /* May overflow */
+		slot = monitoring_slots +
+		       GPIO_MASK_TO_NUM(gpio_list[gpios[i]].mask);
+		if (slot->gpio_signal != GPIO_COUNT) {
+			ccprintf("Error: Monitoring of %s conflicts with %s\n",
+				 argv[3 + i],
+				 gpio_list[slot->gpio_signal].name);
+			rv = EC_ERROR_PARAM3 + i;
+			goto out_partial_cleanup;
+		}
+		slot->gpio_signal = gpios[i];
+	}
+
+	/*
+	 * All the requested signals were available for monitoring, and their
+	 * slots have been marked as reserved for the respective signal.
+	 */
+	rv = shared_mem_acquire(sizeof(struct cyclic_buffer_header_t) + 256,
+				(char **)&buf);
+	if (rv != EC_SUCCESS)
+		goto out_cleanup;
+
+	buf->head = buf->tail = 0;
+	buf->size = 256;
+	buf->overflow = 0;
+	buf->num_signals = gpio_num;
+	buf->signal_bits = 0;
+	/* Compute how many bits are required to represent 0..gpio_num-1. */
+	while (gpio_num >> buf->signal_bits)
+		buf->signal_bits++;
+
+	for (i = 0; i < gpio_num; i++) {
+		slot = monitoring_slots +
+		       GPIO_MASK_TO_NUM(gpio_list[gpios[i]].mask);
+		slot->buffer = buf;
+		slot->signal_no = i;
+	}
+
+	/* Time critical block */
+	interrupt_disable();
+	now = get_time();
+	buf->head_time = now;
+	for (i = 0; i < gpio_num; i++) {
+		slot = monitoring_slots +
+		       GPIO_MASK_TO_NUM(gpio_list[gpios[i]].mask);
+		slot->head_level = gpio_get_level(gpios[i]) ? 1 : 0;
+		gpio_enable_interrupt(gpios[i]);
+	}
+	interrupt_enable();
+
+	buf->tail_time = buf->head_time;
+	for (i = 0; i < gpio_num; i++) {
+		slot = monitoring_slots +
+		       GPIO_MASK_TO_NUM(gpio_list[gpios[i]].mask);
+		slot->tail_level = slot->head_level;
+	}
+
+	num_cur_monitoring++;
+	return EC_SUCCESS;
+
+out_cleanup:
+	i = gpio_num;
+out_partial_cleanup:
+	while (i-- > 0) {
+		monitoring_slots[GPIO_MASK_TO_NUM(gpio_list[gpios[i]].mask)]
+			.gpio_signal = GPIO_COUNT;
+	}
+	return rv;
+}
+
+static int command_gpio_monitoring_read(int argc, const char **argv)
+{
+	int gpios[16];
+	int gpio_num = argc - 3;
+	int i;
+	struct cyclic_buffer_header_t *buf = NULL;
+	struct monitoring_slot_t *slot;
+	int gpio_signals_by_no[16];
+	uint8_t signal_bits;
+	uint32_t tail;
+	timestamp_t tail_time;
+
+	if (gpio_num <= 0 || gpio_num > 16)
+		return EC_ERROR_PARAM_COUNT;
+
+	for (i = 0; i < gpio_num; i++) {
+		gpios[i] = find_signal_by_name(argv[3 + i]);
+		if (gpios[i] == GPIO_COUNT)
+			return EC_ERROR_PARAM3 + i; /* May overflow */
+		slot = monitoring_slots +
+		       GPIO_MASK_TO_NUM(gpio_list[gpios[i]].mask);
+		if (slot->gpio_signal != gpios[i]) {
+			ccprintf("Error: Not monitoring %s\n",
+				 gpio_list[gpios[i]].name);
+			return EC_ERROR_PARAM3 + i;
+		}
+		if (buf == NULL) {
+			buf = slot->buffer;
+		} else if (buf != slot->buffer) {
+			ccprintf(
+				"Error: Not monitoring %s as part of same groups as %s\n",
+				gpio_list[gpios[i]].name,
+				gpio_list[gpios[0]].name);
+			return EC_ERROR_PARAM3 + i;
+		}
+		gpio_signals_by_no[slot->signal_no] = gpios[i];
+	}
+	if (gpio_num != buf->num_signals) {
+		ccprintf("Error: Not full set of signals monitored\n");
+		return EC_ERROR_INVAL;
+	}
+
+	signal_bits = buf->signal_bits;
+	tail = buf->tail;
+	tail_time = buf->tail_time;
+	while (tail != buf->head) {
+		uint8_t *buffer = (uint8_t *)(buf + 1);
+		timestamp_t diff;
+		uint8_t byte;
+		uint8_t signal_no;
+		int shift = 0;
+		struct monitoring_slot_t *slot;
+		diff.val = 0;
+		do {
+			byte = buffer[tail++];
+			if (tail == buf->size)
+				tail = 0;
+			diff.val |= (byte & 0x7F) << shift;
+			shift += 7;
+		} while (byte & 0x80);
+		signal_no = diff.val & (0xFF >> (8 - signal_bits));
+		diff.val >>= signal_bits;
+		tail_time.val += diff.val;
+		slot = monitoring_slots +
+		       GPIO_MASK_TO_NUM(
+			       gpio_list[gpio_signals_by_no[signal_no]].mask);
+		ccprintf("  %d %lld %s\n", signal_no, tail_time.val,
+			 slot->tail_level ? "F" : "R");
+		slot->tail_level = !slot->tail_level;
+	}
+	buf->tail = tail;
+	buf->tail_time = tail_time;
+	if (buf->overflow) {
+		ccprintf("Error: Buffer overflow\n");
+	}
+
+	return EC_SUCCESS;
+}
+
+static int command_gpio_monitoring_stop(int argc, const char **argv)
+{
+	int gpios[16];
+	int gpio_num = argc - 3;
+	int i;
+	struct cyclic_buffer_header_t *buf = NULL;
+	struct monitoring_slot_t *slot;
+
+	if (gpio_num <= 0 || gpio_num > 16)
+		return EC_ERROR_PARAM_COUNT;
+
+	for (i = 0; i < gpio_num; i++) {
+		gpios[i] = find_signal_by_name(argv[3 + i]);
+		if (gpios[i] == GPIO_COUNT)
+			return EC_ERROR_PARAM3 + i; /* May overflow */
+		slot = monitoring_slots +
+		       GPIO_MASK_TO_NUM(gpio_list[gpios[i]].mask);
+		if (slot->gpio_signal != gpios[i]) {
+			ccprintf("Error: Not monitoring %s\n",
+				 gpio_list[gpios[i]].name);
+			return EC_ERROR_PARAM3 + i;
+		}
+		if (buf == NULL) {
+			buf = slot->buffer;
+		} else if (buf != slot->buffer) {
+			ccprintf(
+				"Error: Not monitoring %s as part of same groups as %s\n",
+				gpio_list[gpios[i]].name,
+				gpio_list[gpios[0]].name);
+			return EC_ERROR_PARAM3 + i;
+		}
+	}
+	if (gpio_num != buf->num_signals) {
+		ccprintf("Error: Not full set of signals monitored\n");
+		return EC_ERROR_INVAL;
+	}
+
+	for (i = 0; i < gpio_num; i++) {
+		gpio_disable_interrupt(gpios[i]);
+	}
+
+	/*
+	 * With no more interrupts modifying the buffer, it can be deallocated.
+	 */
+	* / num_cur_monitoring--;
+	for (i = 0; i < gpio_num; i++) {
+		slot = monitoring_slots +
+		       GPIO_MASK_TO_NUM(gpio_list[gpios[i]].mask);
+		slot->gpio_signal = GPIO_COUNT;
+		slot->buffer = NULL;
+	}
+
+	if (buf->overflow)
+		atomic_sub(&num_cur_error_conditions, 1);
+
+	shared_mem_release((char *)buf);
+	return EC_SUCCESS;
+}
+
+static int command_gpio_monitoring(int argc, const char **argv)
+{
+	if (argc < 3)
+		return EC_ERROR_PARAM_COUNT;
+	if (!strcasecmp(argv[2], "start"))
+		return command_gpio_monitoring_start(argc, argv);
+	if (!strcasecmp(argv[2], "read"))
+		return command_gpio_monitoring_read(argc, argv);
+	if (!strcasecmp(argv[2], "stop"))
+		return command_gpio_monitoring_stop(argc, argv);
+	return EC_ERROR_PARAM2;
+}
+
+static int command_gpio(int argc, const char **argv)
+{
+	if (argc < 2)
+		return EC_ERROR_PARAM_COUNT;
+	if (!strcasecmp(argv[1], "monitoring"))
+		return command_gpio_monitoring(argc, argv);
+	return EC_ERROR_PARAM1;
+}
+DECLARE_CONSOLE_COMMAND_FLAGS(gpio, command_gpio,
+			      "monitoring start PIN"
+			      "\nmonitoring read PIN"
+			      "\nmonitoring stop PIN",
+			      "GPIO manipulation", CMD_FLAG_RESTRICTED);
+
+static int command_reinit(int argc, const char **argv)
+{
+	stop_all_gpio_monitoring();
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND_FLAGS(reinit, command_reinit, "",
+			      "Stop any ongoing operation",
+			      CMD_FLAG_RESTRICTED);
+
+static void led_tick(void)
+{
+	/* Indicate ongoing GPIO monitoring by flashing the green LED. */
+	if (num_cur_monitoring)
+		gpio_set_level(GPIO_NUCLEO_LED1,
+			       !gpio_get_level(GPIO_NUCLEO_LED1));
+	else
+		gpio_set_level(GPIO_NUCLEO_LED1, 1);
+	/* Indicate error conditions by flashing red LED. */
+	if (atomic_add(&num_cur_error_conditions, 0))
+		gpio_set_level(GPIO_NUCLEO_LED3,
+			       !gpio_get_level(GPIO_NUCLEO_LED3));
+	else
+		gpio_set_level(GPIO_NUCLEO_LED3, 0);
+}
+DECLARE_HOOK(HOOK_TICK, led_tick, HOOK_PRIO_DEFAULT);
