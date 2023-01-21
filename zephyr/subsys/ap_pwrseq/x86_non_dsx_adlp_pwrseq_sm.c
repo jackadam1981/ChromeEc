2c564d340ee31dd174965f9f66bf6000b3f9d838
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2023-01-20 21:19:48 -0800

    ap_pwrseq: Add S0 state action handler for MTL chipset
    Following AP power sequence driver framework, this CL will add S0 state
    action handlers.
    
    BUG=b:217952699
    BRANCH=none
    TEST=zmake build
    
    Change-Id: Iefa512319f9b793f031d9c92215af13243d8deb5
    Signed-off-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_adlp_pwrseq_sm.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_adlp_pwrseq_sm.c
@@ -229,4 +229,16 @@ static int x86_non_dsx_adlp_s3_run(void *data)
 
 AP_POWER_CHIPSET_STATE_DEFINE(AP_POWER_STATE_S3, x86_non_dsx_adlp_s3_entry,
 			      x86_non_dsx_adlp_s3_run, NULL);
+
+static int x86_non_dsx_adlp_s0_run(void *data)
+{
+	if (power_signal_get(PWR_SLP_SUS)) {
+		return ap_pwrseq_sm_set_state(data, AP_POWER_STATE_G3);
+	}
+
+	return 0;
+}
+
+AP_POWER_CHIPSET_STATE_DEFINE(AP_POWER_STATE_S0, NULL, x86_non_dsx_adlp_s0_run,
+			      NULL);
 #endif /* CONFIG_AP_PWRSEQ_DRIVER */
