2c37eecd0bd91f23f03946966ca4f0d35bd8ef23
Rajesh Kumar <rajesh3.kumar@intel.com>
2023-01-20 17:26:13 -0800

    [WIP] ec: DP2.1 cable detection
    This implements the DP2.1 Cable detection
    
    BUG=none
    TEST=none
    BRANCH=none
    
    Change-Id: Id1f9a4bc0ee02d2014c5a37adeb9f8afbad57ae9
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>

--- common/usb_pd_alt_mode_dfp.c
+++ common/usb_pd_alt_mode_dfp.c
@@ -18,6 +18,7 @@
 #include "usb_dp_alt_mode.h"
 #include "usb_mux.h"
 #include "usb_pd.h"
+#include "usb_pd_dp.h"
 #include "usb_pd_tcpm.h"
 #include "usb_tbt_alt_mode.h"
 #include "usbc_ppc.h"
@@ -469,6 +470,9 @@ __overridable int svdm_dp_config(int port, uint32_t *payload)
 {
 	int opos = pd_alt_mode(port, TCPCI_MSG_SOP, USB_SID_DISPLAYPORT);
 	uint8_t pin_mode = get_dp_pin_mode(port);
+	union dp_mode_resp_cable cable_dp_mode_resp;
+	uint8_t bit_rate;
+
 	mux_state_t mux_mode = svdm_dp_get_mux_mode(port);
 	/* Default dp_port_mf_allow is true */
 	int mf_pref;
@@ -484,11 +488,32 @@ __overridable int svdm_dp_config(int port, uint32_t *payload)
 
 	CPRINTS("pin_mode: %x, mf: %d, mux: %d", pin_mode, mf_pref, mux_mode);
 
-	payload[0] =
-		VDO(USB_SID_DISPLAYPORT, 1, CMD_DP_CONFIG | VDO_OPOS(opos));
-	payload[1] = VDO_DP_CFG(pin_mode, /* pin mode */
-				1, /* DPv1.3 signaling */
-				2); /* UFP connected */
+	if (IS_ENABLED(CONFIG_USB_PD_DP21_SUPPORT) &&
+	    resolve_dpam_version(port, TCPCI_MSG_SOP) == DPAM_VERSION_21) {
+		cable_dp_mode_resp.raw_value =
+			pd_get_dp_mode_vdo(port, TCPCI_MSG_SOP_PRIME);
+		bit_rate = get_dp_cable_bit_rate(port);
+
+		payload[0] = VDO(USB_SID_DISPLAYPORT, 1,
+				 VDO_SVDM_VERS_MAJOR(
+					 pd_get_vdo_ver(port, TCPCI_MSG_SOP)) |
+					 VDO_SVDM_VERS_MINOR(1) |
+					 CMD_DP_CONFIG | VDO_OPOS(opos));
+		payload[1] = VDO_DP2_1_CFG(1, /* DPAM Version */
+					   /* DP Cable Type */
+					   cable_dp_mode_resp.cable_type,
+					   /* uhbr13.5 support */
+					   cable_dp_mode_resp.uhbr13_5_support,
+					   pin_mode, /* pin mode */
+					   bit_rate, /* DP bitrate */
+					   2); /* UFP connected */
+	} else {
+		payload[0] = VDO(USB_SID_DISPLAYPORT, 1,
+				 CMD_DP_CONFIG | VDO_OPOS(opos));
+		payload[1] = VDO_DP_CFG(pin_mode, /* pin mode */
+					1, /* DPv1.3 signaling */
+					2); /* UFP connected */
+	}
 	return 2;
 };
 
