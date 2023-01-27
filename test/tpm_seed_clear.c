f40df03727c00b8efbd287b42e41bbda32c98136
Firas Sammoura <fsammoura@google.com>
2023-01-27 02:05:40 +0000

    test: Check TPM_SEED is reset after reboot
    Add a unit test to check that TPM_SEED is reset after the FPMCU is
    rebooted.
    
    BRANCH=None
    BUG=b:143471027
    TEST=make BOARD=bloonchipper test-tpm_seed_clear
    TEST=make BOARD=dartmonkey test-tpm_seed_clear
    
    Change-Id: I478c8ffcfc75c8188438d56958f0bf16f752efef
    Signed-off-by: Firas Sammoura <fsammoura@google.com>

---
+++ test/tpm_seed_clear.c
@@ -0,0 +1,53 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "ec_commands.h"
+#include "test_util.h"
+
+#ifdef SECTION_IS_RW
+#include "fpsensor_state.h"
+#endif
+
+#include <stdint.h>
+#include <string.h>
+
+#ifdef SECTION_IS_RW
+static const uint8_t default_fake_tpm_seed[] = {
+	0xd9, 0x71, 0xaf, 0xc4, 0xcd, 0x36, 0xe3, 0x60, 0xf8, 0x5a, 0xa0,
+	0xa6, 0x2c, 0xb3, 0xf5, 0xe2, 0xeb, 0xb9, 0xd8, 0x2f, 0xb5, 0x78,
+	0x5c, 0x79, 0x82, 0xce, 0x06, 0x3f, 0xcc, 0x23, 0xb9, 0xe7,
+};
+
+static const uint8_t zero_fake_tpm_seed[] = {
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+#endif
+
+test_static int test_try(void)
+{
+#ifdef SECTION_IS_RW
+
+	TEST_ASSERT_ARRAY_EQ(tpm_seed, zero_fake_tpm_seed, sizeof(tpm_seed));
+
+	memcpy(tpm_seed, default_fake_tpm_seed, FP_CONTEXT_TPM_BYTES);
+
+	TEST_ASSERT_ARRAY_EQ(tpm_seed, default_fake_tpm_seed, sizeof(tpm_seed));
+
+	ccprints("The size of tpm_seed %d\n", (int32_t)sizeof(tpm_seed));
+	for (int i = 0; i < sizeof(tpm_seed); ++i) {
+		ccprints("tpm_seed[%d]: (0x%x)", i, tpm_seed[i]);
+	}
+#endif
+
+	return EC_SUCCESS;
+}
+void run_test(int argc, const char **argv)
+{
+	RUN_TEST(test_try);
+	test_print_result();
+}
