d5117241c2155cf9bb7691d40dbb239075bd5797
Logan_Liao <Logan_Liao@compal.corp-partner.google.com>
2023-01-31 10:01:52 +0800

    Zephyr : Add PS8811 to build.
    This patch add PS8811 retimer zephyr driver code.
    
    BUG=b:263458745
    BRANCH=none
    TEST=zmake build frostflow success.
    
    Change-Id: I74faa235bb0a84b4e5d140af2e7a0c45e02f91a7
    Signed-off-by: Logan_Liao <Logan_Liao@compal.corp-partner.google.com>

---
+++ zephyr/shim/include/usbc/ps8811_usb_mux.h
@@ -0,0 +1,13 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_SHIM_PS8811_USB_MUX_H
+#define __ZEPHYR_SHIM_PS8811_USB_MUX_H
+
+#include "driver/retimer/ps8811_public.h"
+
+#define PS8818_USB_MUX_COMPAT parade_ps8811
+
+#endif /* __ZEPHYR_SHIM_PS8818_USB_MUX_H */
