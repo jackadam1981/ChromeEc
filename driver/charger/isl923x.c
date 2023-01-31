48aa3dba80f1ef5c3ee648dcb63172b0d5934866
arthur.lin <arthur.lin@lcfc.corp-partner.google.com>
2023-01-31 16:54:16 +0800

    raa489000: fix acok state when OTG mode
    Use comparator bit to be acok state instead of acok bit when
    state machine is OTG mode.
    
    BUG=b:262663436
    BRANCH=none
    TEST=zmake build -a
    
    Change-Id: I083c19d6bf875f780662b7a2ea4627630352d3d0
    Signed-off-by: arthur.lin <arthur.lin@lcfc.corp-partner.google.com>

--- driver/charger/isl923x.c
+++ driver/charger/isl923x.c
@@ -887,7 +887,11 @@ enum ec_error_list raa489000_is_acok(int chgnum, bool *acok)
 	rv = raw_read16(chgnum, ISL9238_REG_INFO2, &regval);
 	if (rv != EC_SUCCESS)
 		return rv;
-	*acok = (regval & RAA489000_INFO2_ACOK);
+	if (((regval >> RAA489000_INFO2_STATE_SHIFT) &
+	     RAA489000_INFO2_STATE_MASK) == RAA489000_INFO2_STATE_OTG)
+		*acok = (regval & RAA489000_INFO2_COMPARATOR);
+	else
+		*acok = (regval & RAA489000_INFO2_ACOK);
 
 	return EC_SUCCESS;
 }
