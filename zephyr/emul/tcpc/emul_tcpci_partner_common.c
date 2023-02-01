6e1ddbc19a196038687de0ebf7e16397a2f444d4
Abe Levkoy <alevkoy@chromium.org>
2023-02-01 15:03:06 -0700

    partner emul: Handle Accept, Reject consistently
    When the partner emulator receives a Reject, consistently call Reject
    handlers. When the partner receives an Accept, consistently call Accept
    handlers. Do not combine Accept and Reject logic. After the partner
    sends DR_Swap, avoid sending Soft_Reset in response to Accept.
    
    Verify that the TCPM will Accept a DR_Swap when the TCPM is UFP and that
    the TCPM will actually switch data roles without a disconnection.
    
    BUG=b:262345042
    TEST=twister -s zephyr/test/drivers/drivers.default
    BRANCH=none
    
    Change-Id: Iab61d912a1acd7300bedf251f4856e4c3d86abf6
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>

--- zephyr/emul/tcpc/emul_tcpci_partner_common.c
+++ zephyr/emul/tcpc/emul_tcpci_partner_common.c
@@ -1011,10 +1011,10 @@ static enum tcpci_partner_handler_res
 tcpi_partner_common_handle_accept(struct tcpci_partner_data *data)
 {
 	switch (data->cur_ams_ctrl_req) {
+	case PD_CTRL_DR_SWAP:
+		return tcpci_partner_common_accept_dr_swap_handler(data);
 	case PD_CTRL_VCONN_SWAP:
-		data->cur_ams_ctrl_req = PD_CTRL_INVALID;
-		return TCPCI_PARTNER_COMMON_MSG_HANDLED;
-
+		return tcpci_partner_common_accept_vconn_swap_handler(data);
 	default:
 		LOG_ERR("Unhandled current_req=%u in ACCEPT",
 			data->cur_ams_ctrl_req);
@@ -1025,12 +1025,16 @@ tcpi_partner_common_handle_accept(struct tcpci_partner_data *data)
 static enum tcpci_partner_handler_res
 tcpci_partner_common_handle_reject(struct tcpci_partner_data *data)
 {
+	/* Typically, if the TCPM Rejects an initial message, there isn't much
+	 * for the partner to do.
+	 */
 	switch (data->cur_ams_ctrl_req) {
 	case PD_CTRL_DR_SWAP:
-		return tcpci_partner_common_accept_dr_swap_handler(data);
+		tcpci_partner_common_clear_ams_ctrl_msg(data);
+		return TCPCI_PARTNER_COMMON_MSG_HANDLED;
 	case PD_CTRL_VCONN_SWAP:
-		return tcpci_partner_common_accept_vconn_swap_handler(data);
-
+		tcpci_partner_common_clear_ams_ctrl_msg(data);
+		return TCPCI_PARTNER_COMMON_MSG_HANDLED;
 	default:
 		LOG_ERR("Unhandled current_req=%u in ACCEPT",
 			data->cur_ams_ctrl_req);
@@ -1152,7 +1156,16 @@ tcpci_partner_common_sop_msg_handler(struct tcpci_partner_data *data,
 
 		tcpci_partner_common_clear_ams_ctrl_msg(data);
 
-		__fallthrough;
+		if (data->wait_for_response) {
+			/* Reject is expected message, and emulator code should
+			 * handle it.
+			 */
+			return TCPCI_PARTNER_COMMON_MSG_NOT_HANDLED;
+		}
+		/* Unexpected message - trigger soft reset. */
+		tcpci_partner_common_send_soft_reset(data);
+		return TCPCI_PARTNER_COMMON_MSG_HANDLED;
+
 	case PD_CTRL_ACCEPT:
 		if (data->wait_for_response) {
 			if (data->in_soft_reset) {
