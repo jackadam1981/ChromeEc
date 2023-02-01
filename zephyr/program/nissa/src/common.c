b2130eb07fe711183fdfeaf754f0bc9e6acf029f
Adam Mills <adamjmills@google.com>
2023-02-02 00:56:05 +1100

    nissa: Add ocpc init function to Nissa boards
    Add ocpc init function to all relevant boards to prevent ocpc Vsys
    overshoot.
    
    BUG=b:267404783
    TEST=zmake build -a; Charged a Craask and a Yaviks device from 0% to
    100% battery.
    BRANCH=main
    LOW_COVERAGE_REASON=Nissa specific changes.
    
    Change-Id: I733581054c57e093ce1ea4b5efe8f14afb5cf328
    Signed-off-by: Adam Mills <adamjmills@google.com>

--- zephyr/program/nissa/src/common.c
+++ zephyr/program/nissa/src/common.c
@@ -131,3 +131,11 @@ __override void board_hibernate_late(void)
 	 * a small delay, so return.
 	 */
 }
+
+#ifdef CONFIG_OCPC
+__override void board_ocpc_init(struct ocpc_data *ocpc)
+{
+	/* There's no provision to measure Isys */
+	ocpc->chg_flags[CHARGER_SECONDARY] |= OCPC_NO_ISYS_MEAS_CAP;
+}
+#endif
