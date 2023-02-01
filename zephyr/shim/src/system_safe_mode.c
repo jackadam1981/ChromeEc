da87e395b557c22fbc195d93fccaee985b2f90d1
Rob Barnes <robbarnes@google.com>
2023-02-01 08:45:25 -0700

    system_safe_mode: Use common method for identifying critical tasks
    Legacy EC uses tasks, Zephyr uses threads, so separate methods were used
    to identify critical tasks/threads. Also, to iterate thorugh threads,
    CONFIG_THREAD_MONITOR must be enabled, which costs ~1.5KB of flash.
    
    This change switches Zephyr EC to use task ids to identify critical
    tasks. This allows some duplicate code to be removed and avoids the
    CONFIG_THREAD_MONITOR dependency. This relies on the tasks shim for
    mapping between task ids and thread ids.
    
    BUG=b:266696987
    BRANCH=None
    TEST=Observe threads being disabled when safe mode runs
    
    Change-Id: I87bcb7de87b0575cfc5d90bd12b21314ddeea093
    Signed-off-by: Rob Barnes <robbarnes@google.com>

--- zephyr/shim/src/system_safe_mode.c
+++ zephyr/shim/src/system_safe_mode.c
@@ -13,46 +13,25 @@
 #include <zephyr/kernel.h>
 #include <zephyr/kernel/thread.h>
 
-static bool thread_is_safe_mode_critical(const struct k_thread *thread)
-{
-	/* List of safe mode critical tasks */
-	static const char *safe_mode_critical_threads[] = {
-		"main",
-		"sysworkq",
-		"idle",
-		"HOSTCMD",
-	};
-	for (int i = 0; i < ARRAY_SIZE(safe_mode_critical_threads); i++)
-		if (strcmp(thread->name, safe_mode_critical_threads[i]) == 0)
-			return true;
-	return false;
-}
-
-__override bool current_task_is_safe_mode_critical(void)
-{
-	return thread_is_safe_mode_critical(k_current_get());
-}
-
-static void abort_non_critical_threads_cb(const struct k_thread *thread,
-					  void *user_data)
-{
-	ARG_UNUSED(user_data);
-
-	/*
-	 * Don't abort if thread is critical or current thread.
-	 * Current thread will be canceled automatically after returning from
-	 * exception handler.
-	 */
-	if (thread_is_safe_mode_critical(thread) || k_current_get() == thread)
-		return;
-
-	printk("Aborting thread %s\n", thread->name);
-	k_thread_abort((struct k_thread *)thread);
-}
-
 __override int disable_non_safe_mode_critical_tasks(void)
 {
-	k_thread_foreach(abort_non_critical_threads_cb, NULL);
+	for (task_id_t task_id = 0; task_id < TASK_ID_COUNT + EXTRA_TASK_COUNT;
+	     task_id++) {
+		k_tid_t thread_id = task_id_to_thread_id(task_id);
+
+		if (thread_id == NULL)
+			/* Failed to look up thread from task id, skip it */
+			continue;
+		if (thread_id == k_current_get())
+			/* Don't abort current thread, it will be aborted
+			 * automatically.
+			 */
+			continue;
+		if (is_task_safe_mode_critical(task_id))
+			/* Don't abort critical threads */
+			continue;
+		k_thread_abort(thread_id);
+	}
 	return EC_SUCCESS;
 }
 
