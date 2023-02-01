dc63f77f16508664615e085b3c93e9a94c3b48b7
Rob Barnes <robbarnes@google.com>
2023-02-01 10:59:47 -0700

    tasks: Add extra SHELL task id
    Add TASK_ID_SHELL to extra non-shimmed tasks list. get_shell_thread
    method also added to allow mapping between shell task id and shell
    thread.
    
    BUG=b:267470086
    BRANCH=None
    TEST=Unit tests
    
    Change-Id: Id49f983e2ae8b9e7359d5e5887f128985ea87786
    Signed-off-by: Rob Barnes <robbarnes@google.com>

--- zephyr/shim/include/shimmed_task_id.h
+++ zephyr/shim/include/shimmed_task_id.h
@@ -229,7 +229,9 @@ enum {
  */
 #define CROS_EC_EXTRA_TASKS(fn)                                         \
 	COND_CODE_1(CONFIG_TASK_HOSTCMD_THREAD_MAIN, (fn(HOSTCMD)), ()) \
-	fn(SYSWORKQ) fn(IDLE)
+	fn(SYSWORKQ) fn(IDLE) COND_CODE_1(                              \
+		CONFIG_SHELL_BACKEND_SERIAL, (fn(SHELL)),               \
+		(COND_CODE_1(CONFIG_SHELL_BACKEND_DUMMY, (fn(SHELL)), ())))
 
 #define EXTRA_TASK_INTERNAL_ID(name) EXTRA_TASK_##name,
 enum {
