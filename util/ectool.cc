10be5c453e64e2409aa8e36d2fad4586b1e9c859
Grzegorz Bernacki <bernacki@google.com>
2023-02-02 11:32:42 +0000

    ectool: Add command to get/set s0ix counter
    BUG=b:261869264
    TEST=Tested on skyrim and nissa
    BRANCH=none
    
    Change-Id: Id6d7eb138f44559fb0c8c601935f6ab94c367b44
    Signed-off-by: Grzegorz Bernacki <bernacki@google.com>

--- util/ectool.cc
+++ util/ectool.cc
@@ -11130,6 +11130,53 @@ int cmd_cec(int argc, char *argv[])
 	return -1;
 }
 
+static void cmd_s0ix_counter_help(char *cmd)
+{
+	fprintf(stderr,
+		"  Usage: %s get \n"
+		"         %s set <value>\n",
+		cmd, cmd);
+}
+
+static int cmd_s0ix_counter(int argc, char *argv[])
+{
+	struct ec_params_s0ix_cnt p;
+	struct ec_response_s0ix_cnt r;
+	int rv;
+
+	if (argc < 2) {
+		fprintf(stderr, "Invalid number of params\n");
+		cmd_s0ix_counter_help(argv[0]);
+		return -1;
+	}
+
+	if (!strcasecmp(argv[1], "get")) {
+		p.flags = 0;
+		p.s0ix_counter = 0;
+	} else if (!strcasecmp(argv[1], "set")) {
+		if (argc != 3) {
+			fprintf(stderr, "Invalid number of params\n");
+			cmd_s0ix_counter_help(argv[0]);
+			return -1;
+		}
+		p.flags = EC_S0IX_COUNTER_SET;
+		p.s0ix_counter = strtoull(argv[2], NULL, 0);
+	} else {
+		fprintf(stderr, "Bad subcommand: %s\n", argv[1]);
+		return -1;
+	}
+
+	rv = ec_command(EC_CMD_GET_S0IX_COUNTER, 0, &p, sizeof(p), &r,
+			sizeof(r));
+	if (rv < 0) {
+		return rv;
+	}
+
+	printf("s0ix_counter: %u\n", r.s0ix_counter);
+
+	return 0;
+}
+
 /* NULL-terminated list of commands */
 const struct command commands[] = {
 	{ "adcread", cmd_adc_read },
@@ -11247,6 +11294,7 @@ const struct command commands[] = {
 	{ "rwsigaction", cmd_rwsig_action_legacy },
 	{ "rwsigstatus", cmd_rwsig_status },
 	{ "sertest", cmd_serial_test },
+	{ "s0ix_counter", cmd_s0ix_counter },
 	{ "smartdischarge", cmd_smart_discharge },
 	{ "stress", cmd_stress_test },
 	{ "sysinfo", cmd_sysinfo },
