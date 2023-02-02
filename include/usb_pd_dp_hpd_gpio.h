85dac25b563282900d6b89f849d0cbfc5f01f0fb
Diana Z <dzigterman@chromium.org>
2023-02-02 11:01:40 -0700

    TCPMv2: Create a HPD GPIO module
    Create a module to track the HPD transmission to the AP through a GPIO.
    This way, any implementations which use the HPD GPIO but otherwise do
    not use the DP module can still have access to this bit setting.
    
    BRANCH=None
    BUG=b:266714542
    TEST=./twister -T ./zephyr/test
    
    Change-Id: Ifcc3ec11485143e3c70ae02f04a6d0240187554f
    Signed-off-by: Diana Z <dzigterman@chromium.org>

---
+++ include/usb_pd_dp_hpd_gpio.h
@@ -0,0 +1,31 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/*
+ * HPD GPIO control module
+ * Note: Stubs of APIs are implemented for linking if feature is not enabled
+ */
+
+#ifndef __CROS_EC_USB_DP_HPD_GPIO_H
+#define __CROS_EC_USB_DP_HPD_GPIO_H
+
+#ifdef CONFIG_USB_PD_DP_HPD_GPIO
+/*
+ * Informs the AP of HPD activity through at GPIO, including toggling on IRQ if
+ * necessary.
+ *
+ * @param[in] port		USB-C port number
+ * @param[in] level		Level of the HPD line
+ * @param[in] irq		IRQ pulse to the AP needed
+ */
+void dp_hpd_gpio_set(int port, bool level, bool irq);
+
+#else /* else for CONFIG_USB_PD_DP_HPD_GPIO */
+static inline void dp_hpd_gpio_set(int port, bool level, bool irq)
+{
+}
+
+#endif /* CONFIG_USB_PD_DP_HPD_GPIO */
+#endif /* __CROS_EC_USB_DP_HPD_GPIO_H */
