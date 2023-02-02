0a5b2eb761f8ace858fbe9ebb5be667295a0f7ab
Robert Zieba <robertzieba@google.com>
2023-02-02 17:28:32 +0000

    power/amd_x86: Add support for STB dumping
    Skyrim supports dumping the contents of the SoCs smart-trace buffer
    when triggered externally. The contents of the STB can be useful in
    debugging S0i3 suspend failures. Add support for triggering an STB dump
    when the SoC hangs during S0i3 entry/exit.
    
    BRANCH=none
    BUG=b:246770811
    TEST=Ran on skyrim DUT
    
    Change-Id: Ie18c6072a77f6dd306a46d7f76bd345b4f9e75a7
    Signed-off-by: Robert Zieba <robertzieba@google.com>

---
+++ zephyr/include/drivers/amd_stb.h
@@ -0,0 +1,16 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include <stddef.h>
+
+#include <zephyr/drivers/gpio.h>
+
+#include <gpio_signal.h>
+
+void amd_stb_dump_finish(void);
+void amd_stb_dump_init(const struct gpio_dt_spec *int_out,
+		       const struct gpio_dt_spec *int_in);
+bool amd_stb_dump_in_progress(void);
+void amd_stb_dump_interrupt(enum gpio_signal signal);
+void amd_stb_dump_trigger(void);
