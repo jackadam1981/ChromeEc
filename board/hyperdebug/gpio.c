414237cd19ddc49d5ec47010a351b520f1763695
Jes B. Klinke <jbk@chromium.org>
2023-02-03 14:56:35 -0800

    board/hyperdebug: Optimize gpio edge interrupt handler
    Skip `task_resched_if_needed()` by setting the interrupt vector to point
    directly at `gpio_interrupt()`, sidestepping the scheduling wrapper of
    DECLARE_IRQ().  This is safe because these interrupts do not cause any
    task to become runnable.
    
    Also, increase gpio interrupt priority to the highest.
    
    Move interrupt routines and vector table into SRAM, giving slightly
    faster access times, (and to allow the sidestepping described above.)
    
    BUG=b:266832220
    
    Change-Id: I6043eb87bab3668080dd0ed8d340d50fdc8e0e8b

--- board/hyperdebug/gpio.c
+++ board/hyperdebug/gpio.c
@@ -10,6 +10,7 @@
 #include "console.h"
 #include "cpu.h"
 #include "gpio.h"
+#include "gpio_chip.h"
 #include "hooks.h"
 #include "registers.h"
 #include "shared_mem.h"
@@ -209,11 +210,53 @@ void gpio_edge(enum gpio_signal signal)
 	buffer_header->head_time = now;
 }
 
+#define GPIO_IRQ_HIGHEST_PRIORITY(no)                                     \
+	const struct irq_priority __keep IRQ_PRIORITY(STM32_IRQ_EXTI##no) \
+		__attribute__((section(                                   \
+			".rodata.irqprio"))) = { STM32_IRQ_EXTI##no, 0 }
+
+GPIO_IRQ_HIGHEST_PRIORITY(0);
+GPIO_IRQ_HIGHEST_PRIORITY(1);
+GPIO_IRQ_HIGHEST_PRIORITY(2);
+GPIO_IRQ_HIGHEST_PRIORITY(3);
+GPIO_IRQ_HIGHEST_PRIORITY(4);
+GPIO_IRQ_HIGHEST_PRIORITY(5);
+GPIO_IRQ_HIGHEST_PRIORITY(6);
+GPIO_IRQ_HIGHEST_PRIORITY(7);
+GPIO_IRQ_HIGHEST_PRIORITY(8);
+GPIO_IRQ_HIGHEST_PRIORITY(9);
+GPIO_IRQ_HIGHEST_PRIORITY(10);
+GPIO_IRQ_HIGHEST_PRIORITY(11);
+GPIO_IRQ_HIGHEST_PRIORITY(12);
+GPIO_IRQ_HIGHEST_PRIORITY(13);
+GPIO_IRQ_HIGHEST_PRIORITY(14);
+GPIO_IRQ_HIGHEST_PRIORITY(15);
+
+/* Usual vector table in flash memory. */
+extern void (*vectors[125])(void);
+
+/* Our copy of the vector table in a specially aligned SRAM section. */
+__attribute((section(".bss.vector_table"))) void (*sram_vectors[125])(void);
+
+#define CORTEX_VTABLE REG32(0xE000ED08)
+
 static void board_gpio_init(void)
 {
 	/* Mark every slot as unused. */
 	for (int i = 0; i < ARRAY_SIZE(monitoring_slots); i++)
 		monitoring_slots[i].gpio_signal = GPIO_COUNT;
+
+	/* Copy vector table from flash to SRAM. */
+	memcpy(sram_vectors, vectors, sizeof(sram_vectors));
+	for (int i = 0; i < 16; i++) {
+		/*
+		 * Update GPIO edge interrupt vector to point directly at
+		 * gpio_interrupt(), thereby bypassing the scheduling wrapper of
+		 * DECLARE_IRQ().
+		 */
+		sram_vectors[16 + STM32_IRQ_EXTI0 + i] = gpio_interrupt;
+	}
+	CORTEX_VTABLE = (uint32_t)(sram_vectors);
 }
 DECLARE_HOOK(HOOK_INIT, board_gpio_init, HOOK_PRIO_DEFAULT);
 
