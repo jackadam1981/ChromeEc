9dcae6aa9c295ec3988679b96ab559f92ad2897d
Jack Rosenthal <jrosenth@chromium.org>
2023-02-07 15:46:15 -0700

    zephyr: zmake: Limit modules by default
    Most builds only require the EC module, and for ARM boards, they
    require the CMSIS module as well.
    
    Default to just the EC module, and turn on CMSIS for nuvoton/mchp.
    
    BUG=b:268050548
    BRANCH=none
    TEST=zmake build -a
              => version strings have less modules than usual
    
    Change-Id: Iddf24c159edf1a9c13671eaa478ea30f402d52c7
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>

--- zephyr/zmake/zmake/project.py
+++ zephyr/zmake/zmake/project.py
@@ -43,8 +43,8 @@ class ProjectConfig:
     zephyr_board: str
     supported_toolchains: "list[str]"
     output_packer: type
-    modules: "dict[str, typing.Any]" = dataclasses.field(
-        default_factory=lambda: zmake.modules.known_modules,
+    modules: typing.Iterable[str] = dataclasses.field(
+        default_factory=lambda: ["ec"],
     )
     is_test: bool = dataclasses.field(default=False)
     test_args: typing.List[str] = dataclasses.field(default_factory=list)
