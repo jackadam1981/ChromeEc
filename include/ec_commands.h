2faa0da53673ba024540e21279ba67fba1f399b3
Tim Van Patten <timvp@google.com>
2023-02-08 11:51:18 -0700

    Remove CONFIG_HOST_EVENT64
    The config option CONFIG_HOST_EVENT64 has been unconditionally enabled
    since 2017. This CL removes the option entirely, to clarify that 64 bit
    host events are enabled by default.
    
    BRANCH=none
    BUG=b:261141172
    TEST=CQ
    
    Change-Id: I806c12b8e69955dd19d32ad96587050fd189bea4
    Signed-off-by: Tim Van Patten <timvp@google.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -777,6 +777,9 @@ enum host_event_code {
 	 * not initialized on the EC, or improperly configured on the host.
 	 */
 	EC_HOST_EVENT_INVALID = 32,
+
+	/* The maximum number of support host events */
+	EC_HOST_EVENT_MAX_EVENTS = 65
 };
 /* Host event mask */
 #define EC_HOST_EVENT_MASK(event_code) BIT_ULL((event_code)-1)
