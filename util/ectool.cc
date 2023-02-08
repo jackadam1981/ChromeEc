056db38d9a83571650cb8ab48a6072a67e5f074e
Tim Van Patten <timvp@google.com>
2023-02-08 11:51:18 -0700

    Remove CONFIG_HOST_EVENT64
    The config option CONFIG_HOST_EVENT64 has been unconditionally enabled
    since 2017. This CL removes the option entirely, to clarify that 64 bit
    host events are enabled by default, expanding the number of support
    host events to 64.
    
    BRANCH=none
    BUG=b:261141172
    TEST=Manually build and flash, verify device boots
    TEST=CQ
    
    Change-Id: I806c12b8e69955dd19d32ad96587050fd189bea4
    Signed-off-by: Tim Van Patten <timvp@google.com>

--- util/ectool.cc
+++ util/ectool.cc
@@ -10832,7 +10832,7 @@ int cmd_wait_event(int argc, char *argv[])
 	char *e;
 
 	BUILD_ASSERT(ARRAY_SIZE(mkbp_event_text) == EC_MKBP_EVENT_COUNT);
-	BUILD_ASSERT(ARRAY_SIZE(host_event_text) == 33); /* events start at 1 */
+	BUILD_ASSERT(ARRAY_SIZE(host_event_text) < EC_HOST_EVENT_MAX_EVENTS);
 
 	if (!ec_pollevent) {
 		fprintf(stderr, "Polling for MKBP event not supported\n");
@@ -10880,7 +10880,7 @@ int cmd_wait_event(int argc, char *argv[])
 	switch (event_type) {
 	case EC_MKBP_EVENT_HOST_EVENT:
 		printf("Host events:");
-		for (int evt = 1; evt <= 32; evt++) {
+		for (int evt = 1; evt < EC_HOST_EVENT_MAX_EVENTS; evt++) {
 			if (buffer.data.host_event & EC_HOST_EVENT_MASK(evt)) {
 				const char *name = host_event_text[evt];
 
