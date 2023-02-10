4e1753e090dd201d0fafccda78dff2d88e1297b9
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2023-02-10 11:29:12 +0800

    CPS8100: create header file
    BUG=b:
    BRANCH=none
    TEST=
    
    Change-Id: I08df700f4aff4c5cbe5ccd1345652263feab36f3
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>

---
+++ driver/wpc/cps8100.h
@@ -0,0 +1,13 @@
+/* Copyright 2022 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* cps8100 for Chrome EC */
+
+#ifndef __CROS_EC_WPC_CPS8100_H
+#define __CROS_EC_WPC_CPS8100_H
+
+extern struct pchg_drv cps8100_drv;
+
+#endif /* __CROS_EC_WPC_CPS8100_H */
