420a9a7f1fc911965f19228c8cb2c23337008238
Rob Barnes <robbarnes@google.com>
2023-02-13 14:38:21 -0700

    shim/panic: Fix CODE_UNREACHABLE when running test
    CODE_UNREACHABLE inisde k_sys_fatal_error_handler is disabled during
    tests. This was disabled because system_safe_mode needs to test
    returning from k_sys_fatal_error_handler. However this change will mask
    asserts in some cases. Switch to __ASSERT_UNREACHABLE which can be
    disabled on a per test basis.
    
    BUG=None
    BRANCH=None
    TEST=Unit tests
    
    Change-Id: I32e20a5358d85d059985c0cdf4a65ce37e454a80
    Signed-off-by: Rob Barnes <robbarnes@google.com>

--- zephyr/shim/src/panic.c
+++ zephyr/shim/src/panic.c
@@ -12,6 +12,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/logging/log_ctrl.h>
+#include <zephyr/sys/__assert.h>
 
 /*
  * Arch-specific configuration
@@ -169,9 +170,7 @@ void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *esf)
 	 * the watchdog will overwrite this panic.
 	 */
 	panic_reboot();
-#ifndef TEST_BUILD
-	CODE_UNREACHABLE;
-#endif
+	__ASSERT_UNREACHABLE;
 }
 
 void panic_set_reason(uint32_t reason, uint32_t info, uint8_t exception)
