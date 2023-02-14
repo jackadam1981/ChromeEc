1c468102bc887574a8f2d8843b2e8bec8f1f9679
Kenny Pan <kennypan@google.com>
2023-02-14 08:52:38 +0000

    herobrine: Remove CONFIG_SYSTEM_UNLOCKED
    Disable CONFIG_SYSTEM_UNLOCKED for herobrine board.
    Developers who need unlocked EC please manually turn on the option.
    
    TEST=emerge-herobrine chromeos-ec
    BUG=b:249401128
    
    Change-Id: Ide090dafe68def0f205b5f3e8f0c3af08f5b00e6

--- board/herobrine/board.h
+++ board/herobrine/board.h
@@ -12,7 +12,7 @@
 
 /* TODO(waihong): Remove the following bringup features */
 #define CONFIG_BRINGUP
-#define CONFIG_SYSTEM_UNLOCKED /* Allow dangerous commands. */
+#undef CONFIG_SYSTEM_UNLOCKED /* Disable dangerous commands. */
 #define CONFIG_USB_PD_DEBUG_LEVEL 3
 #define CONFIG_CMD_GPIO_EXTENDED
 #define CONFIG_CMD_POWERINDEBUG
