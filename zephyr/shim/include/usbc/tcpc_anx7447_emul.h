bdaba201e7741d94680cc1fd4f7da588e3fa3864
Jason Yuan <jasonyuan@google.com>
2023-02-15 15:56:56 -0800

    zephyr: move tcpc_get_alert_status to shim
    tcpc_get_alert_status is similar between multiple different projects.
    They have been now merged into common code.
    
    BUG=b:254148652
    TEST=twister, usbc charging on villager, lazor
    BRANCH=none
    LOW_COVERAGE_REASON=Some untested code have been simplified, which does
                        not actually reduce code coverage even though it is
    		    technically uncovered.
    
    Change-Id: I3136bd5b600dbb385547e06490dcbf8815b85b85
    Signed-off-by: Jason Yuan <jasonyuan@google.com>

--- zephyr/shim/include/usbc/tcpc_anx7447_emul.h
+++ zephyr/shim/include/usbc/tcpc_anx7447_emul.h
@@ -18,4 +18,5 @@
 		},                                                             \
 		.drv = &anx7447_tcpm_drv,                                      \
 		.irq_gpio = GPIO_DT_SPEC_GET_OR(id, irq_gpios, {}),            \
+		.rst_gpio = GPIO_DT_SPEC_GET_OR(id, rst_gpios, {}),            \
 	},
