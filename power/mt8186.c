0e66103632f7129506fa7c6afd1ed14a8f622c36
Eric Yilun Lin <yllin@chromium.org>
2023-02-20 14:31:13 +0800

    mt8186,mt8188: disable IN_AP_SUSPEND interrupt in G3/S5
    GPIO_AP_IN_SLEEP_L will be floating when the AP off, so only enable
    the interrupt when AP is on.
    
    BUG=none
    TEST=AP suspend, and AP_IN_SLEEP_L is captured by the powerindebug
    BRANCH=none
    LOW_COVERAGE_REASON=initial bringup
    
    Change-Id: I496c2a4a0b7d7e2f18d8c14945f67d91878ac045
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- power/mt8186.c
+++ power/mt8186.c
@@ -200,6 +200,20 @@ void chipset_reset(enum chipset_shutdown_reason reason)
 	GPIO_SET_LEVEL(GPIO_SYS_RST_ODL, 1);
 }
 
+static void mt8186_enable_interrupts(void)
+{
+	power_signal_enable_interrupt(GPIO_AP_IN_SLEEP_L);
+	power_signal_enable_interrupt(GPIO_AP_EC_WDTRST_L);
+	power_signal_enable_interrupt(GPIO_AP_EC_WARM_RST_REQ);
+}
+
+static void mt8186_disable_interrupts(void)
+{
+	power_signal_disable_interrupt(GPIO_AP_IN_SLEEP_L);
+	power_signal_disable_interrupt(GPIO_AP_EC_WDTRST_L);
+	power_signal_disable_interrupt(GPIO_AP_EC_WARM_RST_REQ);
+}
+
 #ifdef CONFIG_POWER_TRACK_HOST_SLEEP_STATE
 static void power_reset_host_sleep_state(void)
 {
@@ -254,13 +268,11 @@ enum power_state power_chipset_init(void)
 	enum power_state init_state = power_get_signal_state();
 
 	/* Enable reboot / sleep control inputs from AP */
-	gpio_enable_interrupt(GPIO_AP_IN_SLEEP_L);
-	gpio_enable_interrupt(GPIO_AP_EC_SYSRST_ODL);
+	power_signal_enable_interrupt(GPIO_AP_EC_SYSRST_ODL);
 
 	if (system_jumped_late()) {
 		if (init_state == POWER_S0) {
-			gpio_enable_interrupt(GPIO_AP_EC_WDTRST_L);
-			gpio_enable_interrupt(GPIO_AP_EC_WARM_RST_REQ);
+			mt8186_enable_interrupts();
 			disable_sleep(SLEEP_MASK_AP_RUN);
 			CPRINTS("already in S0");
 		}
@@ -278,8 +290,7 @@ enum power_state power_chipset_init(void)
 		exit_hard_off = 0;
 	} else if (system_get_reset_flags() & EC_RESET_FLAG_AP_IDLE) {
 		if (init_state == POWER_S0) {
-			gpio_enable_interrupt(GPIO_AP_EC_WDTRST_L);
-			gpio_enable_interrupt(GPIO_AP_EC_WARM_RST_REQ);
+			mt8186_enable_interrupts();
 			disable_sleep(SLEEP_MASK_AP_RUN);
 		}
 
@@ -353,8 +364,7 @@ enum power_state power_handle_state(enum power_state state)
 		is_exiting_off = false;
 		hook_notify(HOOK_CHIPSET_PRE_INIT);
 
-		gpio_enable_interrupt(GPIO_AP_EC_WARM_RST_REQ);
-		gpio_enable_interrupt(GPIO_AP_EC_WDTRST_L);
+		mt8186_enable_interrupts();
 
 #if DT_NODE_EXISTS(DT_NODELABEL(en_pp4200_s5))
 		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(en_pp4200_s5), 1);
@@ -445,8 +455,7 @@ enum power_state power_handle_state(enum power_state state)
 		return POWER_S3;
 
 	case POWER_S3S5:
-		gpio_disable_interrupt(GPIO_AP_EC_WDTRST_L);
-		gpio_disable_interrupt(GPIO_AP_EC_WARM_RST_REQ);
+		mt8186_disable_interrupts();
 
 		/* Call hooks before we remove power rails */
 		hook_notify(HOOK_CHIPSET_SHUTDOWN);
