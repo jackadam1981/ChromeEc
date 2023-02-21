65d07b896046602fa76c6c056bacb06dd00d372b
Madhurima Paruchuri <mparuchuri@google.com>
2023-02-21 13:03:39 +0000

    rex: zephyr: test: Add unit tests for board_power
    BUG=b:247796816
    BRANCH=None
    TEST=./twister  -s rex/rex.board_power -c
    
    Change-Id: Ic0c93914ff978ca69215f196cec2f33dbd080524
    Signed-off-by: Madhurima Paruchuri <mparuchuri@google.com>

---
+++ zephyr/mock/power_signals.c
@@ -0,0 +1,12 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "power_signals.h"
+#include "mock/power_signals.h"
+
+#include <zephyr/logging/log.h>
+#include <zephyr/ztest.h>
+
+LOG_MODULE_REGISTER(mock_power_signals);
