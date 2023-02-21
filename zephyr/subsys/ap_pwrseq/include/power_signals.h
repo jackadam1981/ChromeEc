bcbfa5d68f1c34d9f0fa11baa8ca0ecbcfaa4fbb
Madhurima Paruchuri <mparuchuri@google.com>
2023-02-21 13:03:39 +0000

    rex: zephyr: test: Add unit tests for board_power
    BUG=b:247796816
    BRANCH=None
    TEST=./twister -T zephyr/test/rex --clobber
    
    Change-Id: Ic0c93914ff978ca69215f196cec2f33dbd080524
    Signed-off-by: Madhurima Paruchuri <mparuchuri@google.com>

--- zephyr/subsys/ap_pwrseq/include/power_signals.h
+++ zephyr/subsys/ap_pwrseq/include/power_signals.h
@@ -56,6 +56,17 @@
 	DT_INST_STRING_UPPER_TOKEN(inst, enum_name)
 
 #define PWR_SIGNAL_ENUM_COMMA(id) PWR_SIGNAL_ENUM(id),
+
+#ifdef CONFIG_POWER_SIGNALS_MOCK
+enum power_signal {
+	PWR_EC_PCH_RSMRST,
+	PWR_EN_PP3300_A,
+	PWR_RSMRST,
+
+	/* Number of signals */
+	POWER_SIGNAL_COUNT,
+};
+#else
 /**
  * @brief Enum of all power signals.
  *
@@ -76,6 +87,7 @@ enum power_signal {
 						       PWR_SIGNAL_ENUM_COMMA)
 					POWER_SIGNAL_COUNT,
 };
+#endif
 
 #undef PWR_SIGNAL_ENUM_COMMA
 
