ae09c2ebd6c0e1fd5d41804f490e71b78bf5a105
Al Semjonovs <asemjonovs@google.com>
2023-02-22 09:23:58 -0700

    WIP: Get PW tokenize working
    Change-Id: I93b7944ac05b9d0ac828f876ef09e7f7894f39f7
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>

---
+++ zephyr/app/ec/include/zephyr_custom_log.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022 Google, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __ZEPHYR_CUSTOM_LOG_H__
+#define __ZEPHYR_CUSTOM_LOG_H__
+
+#ifndef static_assert
+#define static_assert(...)
+#endif
+
+#include <pw_tokenizer/tokenize.h>
+#include <pw_tokenizer/tokenize_to_global_handler.h>
+
+#undef LOG_DBG
+#undef LOG_INF
+#undef LOG_WRN
+#undef LOG_ERR
+
+void SetMessage(const uint8_t* message, size_t size);
+
+#define LOG_DBG(format, ...)
+#define LOG_INF(format, ...) PW_TOKENIZE_TO_CALLBACK(SetMessage, format, __VA_ARGS__)
+#define LOG_WRN(format, ...) PW_TOKENIZE_TO_CALLBACK(SetMessage, format, __VA_ARGS__)
+#define LOG_ERR(format, ...) PW_TOKENIZE_TO_CALLBACK(SetMessage, format, __VA_ARGS__)
+
+#endif /* __ZEPHYR_CUSTOM_LOG_H__ */
