60121b6abb810684fcf2a7a8954cf7ddf3cf38d4
Bobby Casey <bobbycasey@google.com>
2023-02-22 14:34:50 -0500

    fpsensor: WIP support multiple algorithm types
    BRANCH=none
    BUG=b:264457390
    TEST=none
    
    Change-Id: If12cf9b7a9207c38707f6f6f8e0bdf16930981a8
    Signed-off-by: Bobby Casey <bobbycasey@google.com>

--- board/hatch_fp/board_rw.c
+++ board/hatch_fp/board_rw.c
@@ -6,6 +6,7 @@
 #include "common.h"
 #include "console.h"
 #include "fpsensor_detect.h"
+#include "fpsensor_driver.h"
 #include "gpio.h"
 #include "registers.h"
 #include "spi.h"
@@ -56,6 +57,11 @@ void board_init_rw(void)
 	 * Explicitly reset FP_RST_ODL pin to default value.
 	 */
 	gpio_reset(GPIO_FP_RST_ODL);
+
+	fp_driver = get_fp_sensor_driver();
+	if (fp_driver == NULL) {
+		ccprints("Failed to get sensor type!");
+	}
 
 	/* Configure and enable SPI as master for FP sensor */
 	configure_fp_sensor_spi();
