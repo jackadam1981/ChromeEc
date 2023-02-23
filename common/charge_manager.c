affb4fdc9e8f4560232fe9961d81d02fdb3bd9ed
Eric Yilun Lin <yllin@chromium.org>
2023-02-23 15:40:20 +0800

    charge_manager: fix race condition with PE_SNK_TRANSITION
    BUG=b:266025412 b:269061612
    TEST=reboot the system for 100 times, and the input power is not capped
         at 2.5W:
         ec:~$ charger
           Name:   rt9490
           Option: (0x0000)
           Man id: (unsupported)
           Dev id: 0x0060
           V_batt:  8800 (3000 - 18800,  10)
           I_batt:   150 ( 150 -  5000,  10)
           I_in:    2910 ( 100 -  3300,  10)
           I_dptf: disabled
    BRANCH=none
    
    Change-Id: I31ec1b463bef2e458ac3dcd5dc0ad0b790aed034
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -878,7 +878,7 @@ static void charge_manager_refresh(void)
 
 	/* Change the charge limit + charge port/supplier if modified. */
 	if (new_port != charge_port || new_charge_current != charge_current ||
-	    new_supplier != charge_supplier) {
+	    new_supplier != charge_supplier || pd_ceil_enforced) {
 #ifdef HAS_TASK_CHG_RAMP
 		chg_ramp_charge_supplier_change(new_port, new_supplier,
 						new_charge_current,
@@ -896,6 +896,7 @@ static void charge_manager_refresh(void)
 #endif /* HAS_TASK_CHG_RAMP */
 
 		power_changed = 1;
+		pd_ceil_enforced = 0;
 
 		CPRINTS("CL: p%d s%d i%d v%d", new_port, new_supplier,
 			new_charge_current, new_charge_voltage);
@@ -1285,9 +1286,11 @@ void charge_manager_force_ceil(int port, int ceil)
 	 * Force our input current to ceil if we're exceeding it, without
 	 * waiting for our deferred task to run.
 	 */
-	if (left_safe_mode && port == charge_port && ceil < charge_current)
+	if (left_safe_mode && port == charge_port && ceil < charge_current) {
+		pd_ceil_enforced = true;
 		board_set_charge_limit(port, CHARGE_SUPPLIER_PD, ceil,
 				       charge_current_uncapped, charge_voltage);
+	}
 
 	/*
 	 * Now inform charge_manager so it stays in sync with the state of
