7703a72b3ec4f26409ffaa0551893fb5a7aea10d
David Huang <david.huang@quanta.corp-partner.google.com>
2023-02-23 14:25:50 +0800

    constitution: add pse function
    Add pse function
    
    BUG=b:267539938
    BRANCH=None
    TEST=make BOARD=constitution
    
    Change-Id: Iec7434ed25c860823461a19ce24f140f3757342d
    Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>

---
+++ board/constitution/pse.c
@@ -0,0 +1,21 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/*
+ * The LTC4291 is a power over ethernet (PoE) power sourcing equipment (PSE)
+ * controller.
+ */
+#include "pse_ltc4291.h"
+
+/*
+ * Port 1: 100W
+ * Port 2-4: 15W
+ */
+int pse_port_hpmd[4] = {
+	LTC4291_HPMD_MAX,
+	LTC4291_HPMD_MIN,
+	LTC4291_HPMD_MIN,
+	LTC4291_HPMD_MIN,
+};
