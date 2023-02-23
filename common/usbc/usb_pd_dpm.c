348915d3c3ab0c1695a0adf2ac1fa01affcc8248
Daisuke Nojiri <dnojiri@chromium.org>
2023-02-23 14:41:03 -0800

    USB-PD: Print DPM state only if debug_level >= 2
    Printing DPM state names is not much useful unless you're debugging
    timing issues). Normally, it's mostly either in 'waiting' or 'ready'.
    
    This patch introduces dpm_debug_level and sets it to DEBUG_LEVEL_1 by
    default. It can be configured by CONFIG_USB_PD_DEBUG_LEVEL.
    
    This prevents the EC console from being flooded by DPM state names when
    a policy engine sends an EPR keep alive message.
    
    BUG=b:257320026
    BRANCH=None
    TEST=Agah.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I7da0c2fb775cd0978e08464c8923757094c12ca7

--- common/usbc/usb_pd_dpm.c
+++ common/usbc/usb_pd_dpm.c
@@ -38,6 +38,13 @@
 #include "temp_sensor/temp_sensor.h"
 #endif
 
+static const enum debug_level dpm_debug_level =
+#ifdef CONFIG_USB_PD_DEBUG_LEVEL
+	CONFIG_USB_PD_DEBUG_LEVEL;
+#else
+	DEBUG_LEVEL_1;
+#endif
+
 #ifdef CONFIG_COMMON_RUNTIME
 #define CPRINTF(format, args...) cprintf(CC_USBPD, format, ##args)
 #define CPRINTS(format, args...) cprints(CC_USBPD, format, ##args)
@@ -1510,7 +1517,9 @@ void dpm_run(int port, int evt, int en)
 static void dpm_waiting_entry(const int port)
 {
 	DPM_CLR_FLAG(port, DPM_FLAG_PE_READY);
-	print_current_state(port);
+	if (dpm_debug_level >= DEBUG_LEVEL_2) {
+		print_current_state(port);
+	}
 }
 
 static void dpm_waiting_run(const int port)
@@ -1531,7 +1540,9 @@ static void dpm_waiting_run(const int port)
  */
 static void dpm_dfp_ready_entry(const int port)
 {
-	print_current_state(port);
+	if (dpm_debug_level >= DEBUG_LEVEL_2) {
+		print_current_state(port);
+	}
 }
 
 static void dpm_dfp_ready_run(const int port)
@@ -1574,7 +1585,9 @@ static void dpm_dfp_ready_run(const int port)
  */
 static void dpm_ufp_ready_entry(const int port)
 {
-	print_current_state(port);
+	if (dpm_debug_level >= DEBUG_LEVEL_2) {
+		print_current_state(port);
+	}
 }
 
 static void dpm_ufp_ready_run(const int port)
