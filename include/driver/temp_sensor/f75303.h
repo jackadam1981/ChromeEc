3af49b96bf40f7a5319660ef788a3a407d97d593
Logan_Liao <Logan_Liao@compal.corp-partner.google.com>
2023-02-24 11:19:23 +0800

    Zephyr: Correct Temp sensor F75303 read methond.
    This patch correct F75303 read fail on zephyr.
    
    BUG=b:269786658
    BRANCH=none
    TEST=test markarth console that temps show right value.
    
    Change-Id: I315d80e2166d0b5f4d69f4e671846abe52fccbba
    Signed-off-by: Logan_Liao <Logan_Liao@compal.corp-partner.google.com>

--- include/driver/temp_sensor/f75303.h
+++ include/driver/temp_sensor/f75303.h
@@ -1,12 +1,12 @@
-/* Copyright 2018 The ChromiumOS Authors
+/* Copyright 2023 The ChromiumOS Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-/* F75303 temperature sensor module for Chrome EC */
-
 #ifndef __CROS_EC_F75303_H
 #define __CROS_EC_F75303_H
+
+#include "i2c.h"
 
 #ifdef BOARD_MUSHU
 #define F75303_I2C_ADDR_FLAGS 0x4D
@@ -28,10 +28,27 @@ struct f75303_sensor_t {
 
 extern const struct f75303_sensor_t f75303_sensors[];
 
-/* F75303 register */
-#define F75303_TEMP_LOCAL 0x00
-#define F75303_TEMP_REMOTE1 0x01
-#define F75303_TEMP_REMOTE2 0x23
+#ifdef CONFIG_ZEPHYR
+/* F75303 register
+ * Note, these must be macros for compatibility with the Zephyr temperature
+ * sensor shim.
+ */
+enum f75303_index {
+	F75303_IDX_LOCAL,
+	F75303_IDX_REMOTE1,
+	F75303_IDX_REMOTE2,
+	F75303_IDX_COUNT,
+};
+#endif /* CONFIG_ZEPHYR */
+
+#define F75303_TEMP_LOCAL_REGISTER 0x00
+#define F75303_TEMP_REMOTE1_REGISTER 0x01
+#define F75303_TEMP_REMOTE2_REGISTER 0x23
+
+/*
+ * The F75303 driver only supports a single device instamce, that contains 3
+ * separate temperature sensors.
+ */
 
 /**
  * Get the last polled value of a sensor.
