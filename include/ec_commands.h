eb868480bedaf4bf10a88f1b1830f453266ddef5
Daisuke Nojiri <dnojiri@chromium.org>
2023-02-23 16:19:55 -0800

    USB-PD: Use VDO_MAX_OBJECTS to define max VDO objects
    Currently, VDO buffers are defined literally or by PDO_MAX_OBJECTS. This
    isn't applicable for EPR since it extends PDO_MAX_OBJECTS to 11.
    
    This CL redefines VDO buffers using VDO_MAX_OBJECTS, which is set to 6.
    This allows VOD buffers and PDO buffers to be defined in different
    sizes.
    
    BUG=b:257320026
    BRANCH=None
    TEST=buildall
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I760c10eb604733f2f8cc1a643d3ba77e036255c6

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -6785,11 +6785,14 @@ struct svid_mode_info {
 	uint32_t mode_vdo[6]; /* Max VDOs allowed after VDM header is 6 */
 };
 
+/* Max number of VDM data objects without VDM header */
+#define VDO_MAX_OBJECTS 6
+
 struct ec_response_typec_discovery {
 	uint8_t identity_count; /* Number of identity VDOs partner sent */
 	uint8_t svid_count; /* Number of SVIDs partner sent */
 	uint16_t reserved;
-	uint32_t discovery_vdo[6]; /* Max VDOs allowed after VDM header is 6 */
+	uint32_t discovery_vdo[VDO_MAX_OBJECTS];
 	struct svid_mode_info svids[0];
 } __ec_align1;
 
--- include/usb_pd.h
+++ include/usb_pd.h
@@ -417,10 +417,10 @@ union disc_ident_ack {
 		uint32_t product_t3;
 	};
 
-	uint32_t raw_value[PDO_MAX_OBJECTS - 1];
+	uint32_t raw_value[VDO_MAX_OBJECTS];
 };
 BUILD_ASSERT(sizeof(union disc_ident_ack) ==
-	     sizeof(uint32_t) * (PDO_MAX_OBJECTS - 1));
+	     sizeof(uint32_t) * (VDO_MAX_OBJECTS));
 
 /* Discover Identity data - ACK plus discovery state */
 struct identity_data {
