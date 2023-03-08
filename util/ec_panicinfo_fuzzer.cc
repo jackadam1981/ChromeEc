95c2d2ee654ab2db5d51c843ad4949bc6b859ab9
Hsin-Yi Wang <hsinyi@chromium.org>
2023-03-08 21:16:57 +0800

    util: Remove ec_panicinfo
    ec_panicinfo is moved to libec.
    
    BUG=b:172222556
    BRANCH=None
    TEST=`BOARD=host make utils-host`
    
    Cq-Depend: chromium:4318210
    Change-Id: Ib9770932f4555600bd3487757809bfb046f0bad8
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>

--- util/ec_panicinfo_fuzzer.cc
+++ util/ec_panicinfo_fuzzer.cc
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-#include "ec_panicinfo.h"
+#include <libec/ec_panicinfo.h>
 
 /* Fuzzing Build command:
  * $ clang++ ec_panicinfo_fuzzer.cc ec_panicinfo.cc -g -fsanitize=address,fuzzer
@@ -13,10 +13,8 @@
  * Run Fuzzing:
  *  $ ./ec_panicinfo_fuzzer -runs=5000
  */
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, unsigned int size)
 {
 	parse_panic_info((const char *)data, size);
-
 	return 0;
 }
