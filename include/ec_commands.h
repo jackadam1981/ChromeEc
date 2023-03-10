5bcd99287013d7c3915efabf7c4edd6c27fa1156
Rajesh Kumar <rajesh3.kumar@intel.com>
2023-03-10 14:58:00 -0800

    ec: Add host command for DP2.1 cable discovery
    This implements a host command for DP2.1 cable discovery.
    
    BUG=b:266823500
    BRANCH=none
    TEST=EC is responding with DP2.1 cable information
    
    Change-Id: I564e8214935cf602b776968ca87ad9e107b4a584
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -5698,6 +5698,21 @@ struct ec_response_host_event_status {
 	uint32_t status; /* PD MCU host event status */
 } __ec_align4;
 
+/* DP2.1 Cable Discovery Command */
+#define EC_CMD_USB_PD_DP21_DISCOVERY 0x0107
+
+struct ec_params_usb_pd_dp21 {
+	uint8_t port;
+} __ec_align1;
+
+/* DP2.1 Cable Discovery Command */
+struct ec_response_usb_dp21_discovery {
+	uint8_t dpam_version;
+	uint8_t cable_speed;
+	uint8_t uhbr_13_5_supported;
+	uint8_t cable_type;
+} __ec_align1;
+
 /*
  * Set USB type-C port role and muxes
  *
