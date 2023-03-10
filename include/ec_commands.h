dbd7bdd01da39727b88b3558e959eef2c7429b32
Rajesh Kumar <rajesh3.kumar@intel.com>
2023-03-10 15:05:48 -0800

    ec: Report DP2.1 feature flag in host command
    If CONFIG_USB_PD_DP21_MODE is enabled, report this in
    EC_CMD_GET_FEATURES host command.
    
    BUG=b:266823500
    BRANCH=none
    TEST=Feature flag bit is set in EC_CMD_GET_FEATURES host command
    
    Change-Id: Icbc01897aa51753ca772d01f48803af400391ae4
    Signed-off-by: Rajesh Kumar <rajesh3.kumar@intel.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -1591,6 +1591,10 @@ enum ec_feature_code {
 	 * The EC supports the AP composing VDMs for us to send.
 	 */
 	EC_FEATURE_TYPEC_AP_VDM_SEND = 46,
+	/*
+	 * The EC supports DP2.1 capability
+	 */
+	EC_FEATURE_TYPEC_DP2_1 = 47,
 };
 
 #define EC_FEATURE_MASK_0(event_code) BIT(event_code % 32)
