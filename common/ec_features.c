c39a4bce423faf6cb786ee06aba08ac9d5084639
Rajesh Kumar <rajesh3.kumar@intel.com>
2023-03-10 15:05:48 -0800

    ec: Report DP2.1 feature flag in host command
    If CONFIG_USB_PD_DP21_MODE is enabled, report this in
    EC_CMD_GET_FEATURES host command.
    
    BUG=b:266823500
    TEST=Feature flag bit is set in EC_CMD_GET_FEATURES host command
    
    Change-Id: Icbc01897aa51753ca772d01f48803af400391ae4

--- common/ec_features.c
+++ common/ec_features.c
@@ -171,6 +171,9 @@ uint32_t get_feature_flags1(void)
 #ifdef CONFIG_PIGWEED_LOG_TOKENIZED
 		| EC_FEATURE_MASK_1(EC_FEATURE_TOKENIZED_LOGGING)
 #endif
+#ifdef CONFIG_USB_PD_DP21_MODE
+		| EC_FEATURE_MASK_1(EC_FEATURE_TYPEC_DP2_1)
+#endif
 		;
 	return board_override_feature_flags1(result);
 }
