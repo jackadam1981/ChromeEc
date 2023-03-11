c600efd5154def666165e11a09ae0cb4995e21b8
Abe Levkoy <alevkoy@chromium.org>
2023-03-10 17:17:00 -0700

    TCPMv2: Only notify failed discovery once
    When the PE can't perform discovery because it is UFP under PD 2.0, set
    a flag to avoid resending discovery-complete notifications. Avoid
    tripping watchdog timer while processing repeated host commands.
    
    BUG=b:271613370
    TEST=whiterun plugs into charger without issue
    BRANCH=none
    
    Change-Id: Iff66653cce3e8f4addc3946b8d85b343a7120d67
    Signed-off-by: Abe Levkoy <alevkoy@chromium.org>

--- common/usbc/usb_pe_private.h
+++ common/usbc/usb_pe_private.h
@@ -84,6 +84,8 @@ enum {
 	PE_FLAGS_DATA_RESET_COMPLETE_FN,
 	/* Waiting for SRC to SNK settle time */
 	PE_FLAGS_SRC_SNK_SETTLE_FN,
+	/* Discovery disabled due to UFP/PD 2.0 constraint. */
+	PE_FLAGS_DISCOVERY_DISABLED_FN,
 	/* Last element */
 	PE_FLAGS_COUNT
 };
