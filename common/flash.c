c0f5c7b33dacb68f240bab9582c6279ff383886d
Firas Sammoura <fsammoura@google.com>
2023-03-13 21:20:36 +0000

    ec: Add async FLASH_PROTECT
    Change-Id: I0605cc600841ec2ec3c1dd2d3d3598b28d20ffcb
    Signed-off-by: Firas Sammoura <fsammoura@google.com>

--- common/flash.c
+++ common/flash.c
@@ -9,6 +9,7 @@
 #include "common.h"
 #include "console.h"
 #include "cros_board_info.h"
+#include "ec_commands.h"
 #include "flash.h"
 #include "gpio.h"
 #include "hooks.h"
@@ -1547,6 +1548,56 @@ static enum ec_status flash_command_protect(struct host_cmd_handler_args *args)
  */
 DECLARE_HOST_COMMAND(EC_CMD_FLASH_PROTECT, flash_command_protect,
 		     EC_VER_MASK(0) | EC_VER_MASK(1));
+
+static enum ec_status
+flash_command_protect_v2(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_flash_protect_v2 *p = args->params;
+	struct ec_response_flash_protect *r = args->response;
+
+	/*
+	 * Handle requesting new flags.  Note that we ignore the return code
+	 * from flash_set_protect(), since errors will be visible to the caller
+	 * via the flags in the response.  (If we returned error, the caller
+	 * wouldn't get the response.)
+	 */
+
+	switch (p->action) {
+	case FLASH_PROTECT_ASYNC: {
+		if (p->mask)
+			if (crec_flash_set_protect(p->mask, p->flags) !=
+			    EC_SUCCESS)
+				return EC_RES_BUSY;
+		return EC_RES_SUCCESS;
+	}
+
+	/*
+	 * Retrieve the current flags.  The caller can use this to
+	 * determine which of the requested flags could be set.  This is
+	 * cleaner than simply returning error, because it provides
+	 * information to the caller about the actual result.
+	 */
+	case FLASH_ERASE_GET_RESULT: {
+		r->flags = crec_flash_get_protect();
+
+		/* Indicate which flags are valid on this platform */
+		r->valid_flags = EC_FLASH_PROTECT_GPIO_ASSERTED |
+				 EC_FLASH_PROTECT_ERROR_STUCK |
+				 EC_FLASH_PROTECT_ERROR_INCONSISTENT |
+				 EC_FLASH_PROTECT_ERROR_UNKNOWN |
+				 crec_flash_physical_get_valid_flags();
+		r->writable_flags =
+			crec_flash_physical_get_writable_flags(r->flags);
+
+		args->response_size = sizeof(*r);
+
+		return EC_RES_SUCCESS;
+	}
+	}
+	return EC_RES_INVALID_PARAM;
+}
+DECLARE_HOST_COMMAND(EC_CMD_FLASH_PROTECT, flash_command_protect_v2,
+		     EC_VER_MASK(1));
 
 static enum ec_status
 flash_command_region_info(struct host_cmd_handler_args *args)
