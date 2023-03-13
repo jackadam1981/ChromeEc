1b302176d210f2de97fc20bb5f1d1524a907b9ae
Yuval Peress <peress@google.com>
2023-03-13 11:58:26 -0600

    rex: split variant based on sensor inclusion
    Add a new variant to the Rex BUILD.py that allows for skipping the
    motion_sense task and all sensor related features. This requires:
    1. Conditionally including the motionsense.dts file at the top level
       build.
    2. Adding a new Kconfig overlay that disables the sensor related
       features for the sans-sensors build.
    3. Factoring out the sensor related interrupts to only be included
       as an overlay when building the motionsense task.
    
    These couldn't be done via a single Kconfig since Kconfig and DTS
    processing are done at the same time (as far as we're concerned) in
    CMake. This means we can't conditionally include the DTS files at
    configuration time and instead require a new overlay.
    
    BRANCH=none
    BUG=b:272529568
    TEST=zmake build rex rex-sans-sensors
    
    Change-Id: I23df21f41f8a9cd4336187d04034e7e9de10110d
    Signed-off-by: Yuval Peress <peress@google.com>

--- zephyr/program/rex/BUILD.py
+++ zephyr/program/rex/BUILD.py
@@ -7,23 +7,40 @@
 
 def register_variant(
     project_name,
+    kconfig_files=None,
+    dts_files=None,
 ):
     """Register a variant of Rex."""
+    if kconfig_files is None:
+        kconfig_files = []
+    if dts_files is None:
+        dts_files = []
+
+    # Add DTS files for the project (if found)
+    if (here / project_name / "project.overlay").is_file():
+        dts_files.insert(0, here / project_name / "project.overlay")
+
+    # Project-specific KConfig customization (if found)
+    if (here / project_name / "project.conf").is_file():
+        kconfig_files.insert(0, here / project_name / "project.conf")
+
+    # Common to all projects.
+    kconfig_files.insert(0, here / "program.conf")
     register_npcx_project(
         project_name=project_name,
         zephyr_board="npcx9m7f",
-        dts_overlays=[
-            here / project_name / "project.overlay",
-        ],
-        kconfig_files=[
-            # Common to all projects.
-            here / "program.conf",
-            # Project-specific KConfig customization.
-            here / project_name / "project.conf",
-        ],
+        dts_overlays=dts_files,
+        kconfig_files=kconfig_files,
     )
 
 
 register_variant(
     project_name="rex",
+    dts_files=[here / "sensor_interrupts.dts", here / "motionsense.dts"],
+)
+
+register_variant(
+    project_name="rex-sans-sensors",
+    kconfig_files=[here / "rex" / "project.conf", here / "sans-sensors.conf"],
+    dts_files=[here / "rex" / "project.overlay"],
 )
