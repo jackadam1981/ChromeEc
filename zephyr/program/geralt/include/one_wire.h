91295f16ff067c52a4d82ae275b46d1828e9a773
Ting Shen <phoenixshen@google.com>
2023-03-13 18:09:57 +0800

    Copy of CL:4363147
    geralt: enable uart
    
    Change-Id: I88764d84d8e7848cb679d4a8ee817f2afe061f42
    Signed-off-by: Ting Shen <phoenixshen@google.com>

---
+++ zephyr/program/geralt/include/one_wire.h
@@ -0,0 +1,46 @@
+/* Copyright 2023 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include <stdint.h>
+
+enum OneWireRole {
+	MODE_UNKNOWN = 0,
+	MODE_BASE,
+	MODE_LID
+};
+
+enum RoachCommand {
+	ROACH_CMD_ACK,
+	ROACH_CMD_KEYBOARD_MATRIX,
+	ROACH_CMD_TOUCHPAD_REPORT,
+	ROACH_CMD_SUSPEND,
+	ROACH_CMD_RESUME,
+};
+
+struct RoachMsgHeader {
+	uint8_t magic;
+	uint8_t payload_len;
+	uint8_t checksum;
+	uint8_t cmd: 6;
+	uint8_t sender: 2;
+	uint8_t msg_id;
+} __packed;
+
+#define HEADER_SIZE sizeof(struct RoachMsgHeader)
+_Static_assert(HEADER_SIZE == 5);
+
+struct RoachMessage {
+	struct RoachMsgHeader header;
+	uint8_t payload[64 - HEADER_SIZE];
+} __packed __aligned(4);
+
+_Static_assert(sizeof(struct RoachMessage) == 64);
+
+extern enum OneWireRole mode;
+
+void board_uart_tx(enum RoachCommand cmd, const uint8_t* payload, int size);
+
+void board_process_packet(const struct RoachMessage *msg);
+
+void uart_handler(const struct device *dev, void *user_data);
