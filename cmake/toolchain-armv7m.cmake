d1f814bc1e133d111174edaa81e94f881f78fc88
Yi Chou <yich@google.com>
2023-03-14 11:40:21 +0800

    cmake: Separate the common parts of the toolchain file
    Separate the common parts of the toolchain file, so the other toolchain
    files can reuse the same configs.
    
    BUG=b:248508087
    TEST=make runhosttests -j
    TEST=make buildall -j
    BRANCH=none
    
    Change-Id: I1a77ad3bd027c3d16640f99de3c0dfb9f009b48e
    Signed-off-by: Yi Chou <yich@google.com>

--- cmake/toolchain-armv7m.cmake
+++ cmake/toolchain-armv7m.cmake
@@ -5,30 +5,14 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(PREFIX armv7m-cros-eabi-)
+set(CROSS_COMPILE armv7m-cros-eabi-)
 set(CMAKE_SYSROOT /usr/armv7m-cros-eabi/)
 
-set(CMAKE_C_COMPILER ${PREFIX}clang)
-set(CMAKE_CXX_COMPILER ${PREFIX}clang++)
-set(CMAKE_LINKER ${PREFIX}ld.lld)
-set(CMAKE_AR ${PREFIX}ar)
-set(CMAKE_NM ${PREFIX}nm)
-set(CMAKE_OBJCOPY ${PREFIX}objcopy)
-set(CMAKE_OBJDUMP ${PREFIX}objdump)
-set(CMAKE_RANLIB ${PREFIX}ranlib)
-set(CMAKE_READELF ${PREFIX}readelf)
+set(CC_NAME clang)
+set(CXX_NAME clang++)
 
-add_compile_options(-Oz)
-
-add_compile_options(-flto)
-add_link_options(-flto)
+set(CMAKE_TRY_COMPILE_PLATFORM_VARIABLES CROS_EC_REPO CROSS_COMPILE CC_NAME CXX_NAME)
+include("${CROS_EC_REPO}/cmake/toolchain-common.cmake")
 
 add_link_options(-lclang_rt.builtins-armv7m)
 add_link_options(-lnosys)
-
-set(CMAKE_POSITION_INDEPENDENT_CODE OFF)
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
