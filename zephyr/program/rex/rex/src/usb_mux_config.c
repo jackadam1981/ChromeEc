eba9bbf2ea0c5ba46bcdcfb160cf48dc3a9c7e1a
Madhurima Paruchuri <mparuchuri@google.com>
2023-03-14 18:43:47 +0000

    rex: Add FW_IO_DB CBI lookup
    TEST= ./twister -T zephyr/test/rex -c
    
    Change-Id: I2b0651f8aadfa4050344357859afae961ffc7499
    Signed-off-by: Madhurima Paruchuri <mparuchuri@google.com>

---
+++ zephyr/program/rex/rex/src/usb_mux_config.c
@@ -0,0 +1,52 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Rex board-specific USB-C mux configuration */
+
+#include "console.h"
+#include "cros_board_info.h"
+#include "cros_cbi.h"
+#include "hooks.h"
+#include "ioexpander.h"
+#include "usb_mux.h"
+#include "usbc/usb_muxes.h"
+
+#include <zephyr/drivers/gpio.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(rex, CONFIG_REX_LOG_LEVEL);
+
+static void setup_mux(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FW_IO_DB, &val);
+	if (ret != 0) {
+		LOG_ERR("Error finding FW_DB_IO in CBI FW_CONFIG, ret: %d", ret);
+		return;
+	}
+
+	if (val == FW_IO_DB_NOT_CONNECTED) {
+		LOG_INF("DB USB not connected");
+	}
+	if (val == FW_IO_DB_USB3) {
+		LOG_INF("C1: Setting USB3 mux");
+	}
+//	if (val == FW_IO_DB_USB4_KB8010) {
+//		LOG_INF("C1: Setting KB8010 mux");
+//		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_kb8010_port1);
+//	}
+//	if (val == FW_IO_DB_USB4_ANX7452) {
+//		LOG_INF("C1: Setting ANX7452 mux");
+//		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_anx7452_port1);
+//	}
+//	if (val == FW_IO_DB_USB4_HB) {
+//		LOG_INF("C1: Setting HB mux");
+//		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_hb_port1);
+//	}
+}
+DECLARE_HOOK(HOOK_INIT, setup_mux, HOOK_PRIO_INIT_I2C);
