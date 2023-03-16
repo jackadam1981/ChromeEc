e5ed6ce20d606c1ec61251cee414ae26c79340d5
Tom Hughes <tomhughes@chromium.org>
2023-03-16 14:17:14 -0700

    third_party: Add option to build and link googletest
    When CONFIG_GOOGLETEST is enabled, the googletest library will be built
    and linked into the test images.
    
    BRANCH=none
    BUG=b:254530679
    TEST=make BOARD=bloonchipper test-fpsensor_hw -j
    
    Cq-Depend: chrome-internal:5548068
    Change-Id: I969599ed441415fd809b08396aefcfa92275fbcd
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>

--- include/config.h
+++ include/config.h
@@ -5845,6 +5845,11 @@
  */
 #define CONFIG_HOST_INTERFACE_ESPI_DEFAULT_VW_WIDTH_US 65
 
+/*
+ * Build and link *test* images with googletest.
+ */
+#undef CONFIG_GOOGLETEST
+
 /*****************************************************************************/
 /*
  * Include board and core configs, since those hold the CONFIG_ constants for a
