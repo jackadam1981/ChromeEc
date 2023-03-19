8070afc4ae1367567a9d20feea4f415f91ea6941
Li Feng <li1.feng@intel.com>
2023-03-18 23:17:27 -0700

    ish: create baseboard for ISH
    ISH 5 and later share common configurations, create baseboard to reuse
    common code.
    
    BUG=b:274211471
    BRANCH=none
    TEST=Verified by test done in CL:4351355.
    
    Change-Id: I5537ea861569c86f394e108e7fe0f2ec6a825477
    Signed-off-by: Li Feng <li1.feng@intel.com>

---
+++ baseboard/ish/build.mk
@@ -0,0 +1,9 @@
+# -*- makefile -*-
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# ISH Baseboard specific files build
+#
+
+baseboard-y=baseboard.o
