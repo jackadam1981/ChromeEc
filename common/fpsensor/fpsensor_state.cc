b8b757b98f722ca450aac03da796870c30db2a8b
Yi Chou <yich@google.com>
2023-03-20 17:00:51 +0800

    [TEST] Use boringssl EC
    BUG=None
    TEST=None
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I5cba11207a040d91d9811ecfe821e7595c138981

--- common/fpsensor/fpsensor_state.cc
+++ common/fpsensor/fpsensor_state.cc
@@ -6,10 +6,11 @@
 #include "compile_time_macros.h"
 #include "fpsensor_utils.h"
 
+#include <cstddef>
+
 extern "C" {
 #include "atomic.h"
 #include "common.h"
-#include "cryptoc/p256.h"
 #include "cryptoc/util.h"
 #include "ec_commands.h"
 #include "host_command.h"
@@ -24,7 +25,11 @@ extern "C" {
 #include "fpsensor.h"
 #include "fpsensor_crypto.h"
 #include "fpsensor_state.h"
+#include "openssl/ec.h"
+#include "openssl/obj_mac.h"
 #include "scoped_fast_cpu.h"
+
+#include "scoped_openssl_types.hpp"
 
 /* These must be included after the "openssl/aes.h" */
 #include "crypto/fipsmodule/aes/internal.h"
@@ -372,8 +377,8 @@ DECLARE_HOST_COMMAND(EC_CMD_FP_READ_MATCH_SECRET, fp_command_read_match_secret,
 		     EC_VER_MASK(0));
 
 BUILD_ASSERT(FP_PK_LEN == SHA256_DIGEST_SIZE);
-BUILD_ASSERT(FP_PK_EC_PUBLIC_KEY_LEN == P256_BITSPERDIGIT);
-BUILD_ASSERT(FP_PK_EC_PRIVATE_KEY_LEN == P256_BITSPERDIGIT);
+BUILD_ASSERT(FP_PK_EC_PUBLIC_KEY_LEN == 32);
+BUILD_ASSERT(FP_PK_EC_PRIVATE_KEY_LEN == 32);
 
 static enum ec_status
 fp_command_establish_pk_keygen(struct host_cmd_handler_args *args)
@@ -392,15 +397,57 @@ fp_command_establish_pk_keygen(struct host_cmd_handler_args *args)
 			FP_PK_ENCRYPTION_SALT_BYTES);
 	trng_exit();
 
-	p256_int p256_n, p256_x, p256_y;
+	ScopedEC_GROUP group(EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
 
-	p256_from_bin(r->enc_privkey, &p256_n);
-	p256_base_point_mul(&p256_n, &p256_x, &p256_y);
-	p256_to_bin(&p256_x, r->pubkey_x);
-	p256_to_bin(&p256_y, r->pubkey_y);
+	if (group.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
 
-	/* Clear the private key. */
-	always_memset(&p256_n, 0, sizeof(p256_n));
+	ScopedBIGNUM secret(
+		BN_bin2bn(r->enc_privkey, FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (secret.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedEC_POINT public_point(EC_POINT_new(group.get()));
+
+	if (public_point.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_mul(group.get(), public_point.get(), secret.get(), nullptr,
+			 nullptr, nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM x_bn(BN_new());
+
+	if (x_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM y_bn(BN_new());
+
+	if (y_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_get_affine_coordinates_GFp(group.get(), public_point.get(),
+						x_bn.get(), y_bn.get(),
+						nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(x_bn.get(), r->pubkey_x, FP_PK_EC_PUBLIC_KEY_LEN) !=
+	    FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(y_bn.get(), r->pubkey_y, FP_PK_EC_PUBLIC_KEY_LEN) !=
+	    FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
 
 	uint8_t key[SBP_ENC_KEY_LEN];
 	int ret =
@@ -461,20 +508,60 @@ fp_command_establish_pk_wrap(struct host_cmd_handler_args *args)
 		return EC_RES_UNAVAILABLE;
 	}
 
-	p256_int p256_n, p256_x, p256_y;
+	ScopedEC_GROUP group(EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
 
-	p256_from_bin(privkey, &p256_n);
-	p256_from_bin(params->peers_pubkey_x, &p256_x);
-	p256_from_bin(params->peers_pubkey_y, &p256_y);
-	/* Doing the calculation in-place. */
-	p256_point_mul(&p256_n, &p256_x, &p256_y, &p256_x, &p256_y);
-	p256_to_bin(&p256_x, r->enc_pk);
+	if (group.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
 
-	/* Clear the private key and pk material. */
-	always_memset(privkey, 0, sizeof(privkey));
-	always_memset(&p256_n, 0, sizeof(p256_n));
-	always_memset(&p256_x, 0, sizeof(p256_x));
-	always_memset(&p256_y, 0, sizeof(p256_y));
+	ScopedBIGNUM private_key(
+		BN_bin2bn(privkey, FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (private_key.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM public_key_x(BN_bin2bn(params->peers_pubkey_x,
+					    FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (public_key_x.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM public_key_y(BN_bin2bn(params->peers_pubkey_y,
+					    FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (public_key_y.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedEC_POINT public_point(EC_POINT_new(group.get()));
+
+	if (public_point.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_set_affine_coordinates_GFp(
+		    group.get(), public_point.get(), public_key_x.get(),
+		    public_key_y.get(), nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_mul(group.get(), public_point.get(), nullptr,
+			 public_point.get(), private_key.get(), nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_get_affine_coordinates_GFp(
+		    group.get(), public_point.get(), public_key_x.get(),
+		    public_key_y.get(), nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(public_key_x.get(), r->enc_pk, FP_PK_LEN) !=
+	    FP_PK_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
 
 	struct sha256_ctx ctx;
 
@@ -664,28 +751,95 @@ fp_command_read_match_secret_with_pubkey(struct host_cmd_handler_args *args)
 	trng_rand_bytes(response->iv, FP_EC_PUBLIC_KEY_IV_LEN);
 	trng_exit();
 
-	p256_int p256_n, p256_x, p256_y;
+	ScopedEC_GROUP group(EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1));
+	if (group.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
 
-	p256_from_bin(privkey, &p256_n);
+	ScopedBIGNUM private_key(
+		BN_bin2bn(privkey, FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
 
-	p256_base_point_mul(&p256_n, &p256_x, &p256_y);
+	if (private_key.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
 
-	p256_to_bin(&p256_x, response->pubkey_x);
-	p256_to_bin(&p256_y, response->pubkey_y);
+	ScopedEC_POINT public_point(EC_POINT_new(group.get()));
 
-	p256_from_bin(params->pubkey_x, &p256_x);
-	p256_from_bin(params->pubkey_y, &p256_y);
-	p256_point_mul(&p256_n, &p256_x, &p256_y, &p256_x, &p256_y);
+	if (public_point.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_mul(group.get(), public_point.get(), private_key.get(),
+			 nullptr, nullptr, nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM x_bn(BN_new());
+
+	if (x_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM y_bn(BN_new());
+
+	if (y_bn.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_get_affine_coordinates_GFp(group.get(), public_point.get(),
+						x_bn.get(), y_bn.get(),
+						nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(x_bn.get(), response->pubkey_x,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (BN_bn2binpad(y_bn.get(), response->pubkey_y,
+			 FP_PK_EC_PUBLIC_KEY_LEN) != FP_PK_EC_PUBLIC_KEY_LEN) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM public_key_x(
+		BN_bin2bn(params->pubkey_x, FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (public_key_x.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	ScopedBIGNUM public_key_y(
+		BN_bin2bn(params->pubkey_y, FP_PK_EC_PRIVATE_KEY_LEN, nullptr));
+
+	if (public_key_y.get() == nullptr) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_set_affine_coordinates_GFp(
+		    group.get(), public_point.get(), public_key_x.get(),
+		    public_key_y.get(), nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_mul(group.get(), public_point.get(), nullptr,
+			 public_point.get(), private_key.get(), nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
+
+	if (EC_POINT_get_affine_coordinates_GFp(
+		    group.get(), public_point.get(), public_key_x.get(),
+		    public_key_y.get(), nullptr) != 1) {
+		return EC_RES_UNAVAILABLE;
+	}
 
 	uint8_t share_secret[FP_POSITIVE_MATCH_SECRET_BYTES];
 
-	p256_to_bin(&p256_x, share_secret);
-
-	/* Clear the private key and share_secret material. */
-	always_memset(privkey, 0, sizeof(privkey));
-	always_memset(&p256_n, 0, sizeof(p256_n));
-	always_memset(&p256_x, 0, sizeof(p256_x));
-	always_memset(&p256_y, 0, sizeof(p256_y));
+	if (BN_bn2binpad(public_key_x.get(), share_secret,
+			 FP_POSITIVE_MATCH_SECRET_BYTES) !=
+	    FP_POSITIVE_MATCH_SECRET_BYTES) {
+		return EC_RES_UNAVAILABLE;
+	}
 
 	/* Sha256 the share_secret. */
 	struct sha256_ctx ctx;
