cf818dbd45ee0799d57d3cb67654618bb8b503eb
amber.chen <amber.chen@lcfc.corp-partner.google.com>
2023-03-20 11:44:52 +0800

    tabor: add keyboard setting
    add keyboard setting for tabor
    
    BUG=b:273888144
    BRANCH=none
    TEST=make buildall -j48
    
    Change-Id: I104bdb287699d2676c0f365f62c68e688e2d5113
    Signed-off-by: amber.chen <amber.chen@lcfc.corp-partner.google.com>

--- board/taeko/keyboard.c
+++ board/taeko/keyboard.c
@@ -4,6 +4,7 @@
  */
 
 #include "common.h"
+#include "cros_board_info.h"
 #include "ec_commands.h"
 #include "fw_config.h"
 #include "keyboard_scan.h"
@@ -60,6 +61,23 @@ static const struct ec_response_keybd_config tarlo_kb = {
 	.capabilities = KEYBD_CAP_SCRNLOCK_KEY | KEYBD_CAP_NUMERIC_KEYPAD,
 };
 
+static const struct ec_response_keybd_config tabor_kb = {
+	.num_top_row_keys = 10,
+	.action_keys = {
+		TK_BACK,		/* T1 */
+		TK_REFRESH,		/* T2 */
+		TK_FULLSCREEN,		/* T3 */
+		TK_OVERVIEW,		/* T4 */
+		TK_BRIGHTNESS_DOWN,	/* T5 */
+		TK_BRIGHTNESS_UP,	/* T6 */
+		TK_MICMUTE,             /* T7 */
+		TK_VOL_MUTE,		/* T8 */
+		TK_VOL_DOWN,		/* T9 */
+		TK_VOL_UP,		/* T10 */
+	},
+	.capabilities = KEYBD_CAP_SCRNLOCK_KEY,
+};
+
 /*
  * Row Column info for Top row keys T1 - T15.
  * Since tarlo keyboard top row keys have some issue when press with search
@@ -95,15 +113,31 @@ BUILD_ASSERT(ARRAY_SIZE(vivaldi_keys) == MAX_TOP_ROW_KEYS);
 __override const struct ec_response_keybd_config *
 board_vivaldi_keybd_config(void)
 {
+	uint32_t val;
+	uint8_t sku_id = 0;
+
+	if (cbi_get_sku_id(&val) == EC_SUCCESS) {
+		sku_id = (val >> 9) & 0x1;
+	}
 	if (ec_cfg_has_keyboard_number_pad())
 		return &tarlo_kb;
 	else {
-		vivaldi_keys[7].row = 2; /* T8 */
-		vivaldi_keys[7].col = 9;
-		vivaldi_keys[8].row = 1; /* T9 */
-		vivaldi_keys[8].col = 9;
-		vivaldi_keys[9].row = 0; /* T10 */
-		vivaldi_keys[9].col = 4;
+		if (sku_id == 0x00) {
+			vivaldi_keys[7].row = 2; /* T8 */
+			vivaldi_keys[7].col = 9;
+			vivaldi_keys[8].row = 1; /* T9 */
+			vivaldi_keys[8].col = 9;
+			vivaldi_keys[9].row = 0; /* T10 */
+			vivaldi_keys[9].col = 4;
+		} else {
+			vivaldi_keys[7].row = 2; /* T8 */
+			vivaldi_keys[7].col = 9;
+			vivaldi_keys[8].row = 1; /* T9 */
+			vivaldi_keys[8].col = 9;
+			vivaldi_keys[9].row = 0; /* T10 */
+			vivaldi_keys[9].col = 4;
+			return &tabor_kb;
+		}
 		return &taeko_kb;
 	}
 }
