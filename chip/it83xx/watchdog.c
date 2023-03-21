f54b566f2bbf1d10af8343dcd052a5090f34278f
Aaron Massey <aaronmassey@google.com>
2023-03-21 14:36:57 -0600

    ite83xx: Remove false-alarm EC panic reports
    The pre-watchdog warning interrupt handler currently invokes
    get_panic_data_write() to retrieve a pointer to panic data. This has a
    side-effect of initializing the panic data with PANIC_MAGIC and a panic
    data size without other pertient fields set. As a result, the EC reports
    malformed panic to the AP causing an unparsable EC crash to be reported.
    
    Revert the ITE watchdog warning interrupt usage of
    get_panic_data_write() to using the global PANIC_DATA_PTR pointer to
    panic data.
    
    BRANCH=none
    BUG=b:247100203
    TEST=On drawcia, dead battery boot, invoke waitms 1100 (pre-wdt parning),
    invoke ectool panicinfo to check for well-formed panic data
    TEST=On drawcia, invoke crash watchdog, invoke ectool panicinfo to check
    for well-formed panic data.
    
    Change-Id: I5a75bec0efa79d00c358edc0645a0a0d39a879d2
    Signed-off-by: Aaron Massey <aaronmassey@google.com>

--- chip/it83xx/watchdog.c
+++ chip/it83xx/watchdog.c
@@ -30,6 +30,8 @@ static int wdt_warning_fired;
 /* The interval to print warning message at critical period. */
 #define ITE83XX_WATCHDOG_CRITICAL_MS 30
 
+static struct panic_data *const pdata_ptr = PANIC_DATA_PTR;
+
 /* set warning timer */
 static void watchdog_set_warning_timer(int32_t ms, int init)
 {
@@ -38,8 +40,6 @@ static void watchdog_set_warning_timer(int32_t ms, int init)
 
 void watchdog_warning_irq(void)
 {
-	struct panic_data *const pdata_ptr = get_panic_data_write();
-
 #if defined(CHIP_CORE_NDS32)
 	pdata_ptr->nds_n8.ipc = get_ipc();
 #elif defined(CHIP_CORE_RISCV)
