ea998a2545c1a01729bb4c6e276e361e5605aac1
Ridden_Liu <ridden_liu@compal.corp-partner.google.com>
2023-03-22 11:07:07 +0800

    Omnigul: Modify FW_CONFIG
    This CL is modify the FW_CONFIG bit field.
    
    BUG=b:272845126
    TEST=make BOARD=omnigul
    BRANCH=omnigul
    
    Change-Id: I31014ba5ba47194bd2913352b2ac5f258e1e2dc9
    Signed-off-by: Ridden_Liu <ridden_liu@compal.corp-partner.google.com>

--- board/omnigul/fw_config.h
+++ board/omnigul/fw_config.h
@@ -14,25 +14,31 @@
  * Source of truth is the project/brya/brya/config.star configuration file.
  */
 
-enum ec_cfg_usb_db_type {
-	DB_USB_ABSENT = 0,
-	DB_USB3_PS8815 = 1,
-	DB_USB_ABSENT2 = 15
-};
-
 enum ec_cfg_keyboard_backlight_type {
 	KEYBOARD_BACKLIGHT_DISABLED = 0,
 	KEYBOARD_BACKLIGHT_ENABLED = 1
 };
 
-union brya_cbi_fw_config {
+enum ec_cfg_tabletmode_type { TABLETMODE_DISABLED = 0, TABLETMODE_ENABLED = 1 };
+
+enum ec_cfg_storage_type {
+	STORAGE_UNPROVISION = 0,
+	STORAGE_UFS = 1,
+	STORAGE_NVMe = 2
+};
+
+enum ec_cfg_audio_type {
+	AUDIO_ALC5682I_ALC1019 = 0,
+	AUDIO_ALC5682I_ALC1019_3MIC = 1
+};
+
+union omnigul_cbi_fw_config {
 	struct {
-		enum ec_cfg_usb_db_type usb_db : 4;
-		uint32_t sd_db : 2;
-		uint32_t lte_db : 1;
 		enum ec_cfg_keyboard_backlight_type kb_bl : 1;
-		uint32_t audio : 3;
-		uint32_t reserved_1 : 21;
+		enum ec_cfg_tabletmode_type tab_mode : 1;
+		enum ec_cfg_storage_type stg : 2;
+		enum ec_cfg_audio_type aud : 1;
+		uint32_t reserved_1 : 26;
 	};
 	uint32_t raw_value;
 };
@@ -42,13 +48,6 @@ union brya_cbi_fw_config {
  *
  * @return the FW_CONFIG for the board.
  */
-union brya_cbi_fw_config get_fw_config(void);
-
-/**
- * Get the USB daughter board type from FW_CONFIG.
- *
- * @return the USB daughter board type.
- */
-enum ec_cfg_usb_db_type ec_cfg_usb_db_type(void);
+union omnigul_cbi_fw_config get_fw_config(void);
 
 #endif /* __BOARD_BRYA_FW_CONFIG_H_ */
