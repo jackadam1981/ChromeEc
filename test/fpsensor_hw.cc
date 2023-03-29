b0a87ffe40c0f49b358bac424acfe1132c6f13fc
Bobby Casey <bobbycasey@google.com>
2023-03-28 22:39:34 -0400

    test: Remove RO logic from fpsensor_hw test
    The build file (test/build.mk) was updated in
    https://crrev.com/c/4379778 to remove fpsensor_hw from RO tests. Remove
    the detritus special-case RO logic from the test itself.
    
    BUG=none
    TEST=test/run_device_tests.py -b bloonchipper -t fpsensor_hw
    TEST=test/run_device_tests.py -b dartmonkey -t fpsensor_hw
    
    Change-Id: I86d11567574a75fa72d0b836c016a1ebfa9d55f5

--- test/fpsensor_hw.cc
+++ test/fpsensor_hw.cc
@@ -7,13 +7,9 @@
 #include "config.h"
 #include "ec_gtest.h"
 #include "fpc_private.h"
+#include "fpc_sensor.h"
 
-#ifdef SECTION_IS_RW
-#include "fpc/fpc_sensor.h"
 static const uint32_t fp_sensor_hwid = FP_SENSOR_HWID_FPC;
-#else
-static const uint32_t fp_sensor_hwid = UINT32_MAX;
-#endif
 
 /* Hardware-dependent smoke test that makes a SPI transaction with the
  * fingerprint sensor.
@@ -22,12 +18,10 @@ TEST(FpSensor, CheckHardwareID)
 {
 	uint16_t id = 0;
 
-	if (IS_ENABLED(SECTION_IS_RW)) {
-		EXPECT_EQ(fpc_get_hwid(&id), EC_SUCCESS);
+	EXPECT_EQ(fpc_get_hwid(&id), EC_SUCCESS);
 
-		/* The lower 4-bits of the sensor hardware id are a
-		 * manufacturing ID that is ok to vary.
-		 */
-		EXPECT_EQ(fp_sensor_hwid, id >> 4);
-	};
+	/* The lower 4-bits of the sensor hardware id are a
+	 * manufacturing ID that is ok to vary.
+	 */
+	EXPECT_EQ(fp_sensor_hwid, id >> 4);
 }
