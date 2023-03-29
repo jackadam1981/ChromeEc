c6f0ff49b00600b2ae27e61af3eaa8c7df00afdf
Patryk Duda <pdk@semihalf.com>
2023-03-29 16:54:47 +0200

    rollback: Add support for entropy from RNG for rollbackaddent
    If 'rollbackaddent' is called without any arguments we can add entropy
    from RNG. This is useful for testing and debugging purposes.
    
    BUG=b:277029648
    BRANCH=none
    TEST=Compile firmware or bloonchipper. Use 'rollbackaddent' command
         without any arguments to add randomness to rollback. Check if
         the secret was changed using 'rollbackinfo' command.
    TEST=./test/run_device_tests.py -b bloonchipper -t rollback.*
         Test "rollback_region0": PASSED
         Test "rollback_region1": PASSED
         Test "rollback_entropy": PASSED
    
    Change-Id: I0da2df8825d915eaf92364ec07f596b7638a64fd
    Signed-off-by: Patryk Duda <pdk@semihalf.com>

--- common/rollback.c
+++ common/rollback.c
@@ -375,16 +375,29 @@ DECLARE_CONSOLE_COMMAND(rollbackupdate, command_rollback_update, "min_version",
 #ifdef CONFIG_ROLLBACK_SECRET_SIZE
 static int command_rollback_add_entropy(int argc, const char **argv)
 {
+	const uint8_t *data;
 	int len;
 
-	if (argc < 2)
-		return EC_ERROR_PARAM_COUNT;
+	if (argc < 2) {
+		uint8_t rand[CONFIG_ROLLBACK_SECRET_SIZE];
 
-	len = strlen(argv[1]);
+		if (!IS_ENABLED(CONFIG_RNG))
+			return EC_ERROR_PARAM_COUNT;
 
-	return rollback_add_entropy(argv[1], len);
+		trng_init();
+		trng_rand_bytes(rand, sizeof(rand));
+		trng_exit();
+
+		data = rand;
+		len = sizeof(rand);
+	} else {
+		data = argv[1];
+		len = strlen(argv[1]);
+	}
+
+	return rollback_add_entropy(data, len);
 }
-DECLARE_CONSOLE_COMMAND(rollbackaddent, command_rollback_add_entropy, "data",
+DECLARE_CONSOLE_COMMAND(rollbackaddent, command_rollback_add_entropy, "[data]",
 			"Add entropy to rollback block");
 
 #ifdef CONFIG_RNG
