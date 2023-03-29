b0169c1231ade32edcfb730adf26fcb280db7bab
Bobby Casey <bobbycasey@google.com>
2023-03-28 22:39:34 -0400

    test: Only run fpsensor_hw in RW
    The RO doesn't connect to FP sensor HW so it doesn't need to build/run
    this test. This prevents future updates from unnecessarily breaking RO
    build tests.
    
    BRANCH=none
    BUG=b:256674283
    TEST=test/run_device_tests.py -b bloonchipper -t fpsensor_hw
    TEST=test/run_device_tests.py -b dartmonkey -t fpsensor_hw
    
    Change-Id: I86d11567574a75fa72d0b836c016a1ebfa9d55f5
    Signed-off-by: Bobby Casey <bobbycasey@google.com>

--- test/fpsensor_hw.cc
+++ test/fpsensor_hw.cc
@@ -8,12 +8,7 @@
 #include "fpc_private.h"
 #include "test_util.h"
 
-#ifdef SECTION_IS_RW
-#include "fpc/fpc_sensor.h"
 static const uint32_t fp_sensor_hwid = FP_SENSOR_HWID_FPC;
-#else
-static const uint32_t fp_sensor_hwid = UINT32_MAX;
-#endif
 
 /* Hardware-dependent smoke test that makes a SPI transaction with the
  * fingerprint sensor.
@@ -22,13 +17,11 @@ test_static int test_fp_check_hwid(void)
 {
 	uint16_t id = 0;
 
-	if (IS_ENABLED(SECTION_IS_RW)) {
-		TEST_EQ(fpc_get_hwid(&id), EC_SUCCESS, "%d");
-		/* The lower 4-bits of the sensor hardware id are a
-		 * manufacturing ID that is ok to vary.
-		 */
-		TEST_EQ(fp_sensor_hwid, id >> 4, "%d");
-	};
+	TEST_EQ(fpc_get_hwid(&id), EC_SUCCESS, "%d");
+	/* The lower 4-bits of the sensor hardware id are a
+	 * manufacturing ID that is ok to vary.
+	 */
+	TEST_EQ(fp_sensor_hwid, id >> 4, "%d");
 	return EC_SUCCESS;
 }
 
