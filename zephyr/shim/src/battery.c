72f69b84f5847d1cd698dde91771d651a10a3f40
Aaron Massey <aaronmassey@google.com>
2023-03-30 17:17:54 -0600

    test: Initial partial test of upstream fuel gauge
    We want to iteratively validate upstream fuel gauge apis and drivers
    downstream before making the switch to using the upstream code.
    
    Get a small initial test build that includes usage of the upstream SBS
    API and driver.
    
    BUG=b:271889974
    TEST=./twister -c -iv -s drivers/drivers.upstream_fuel_gauge
    TEST=compare builds
    
    Cq-Depend: chromium:4637997
    Change-Id: I410acc8af69168c3ca8b875423d9a1c6bef47fa2

--- zephyr/shim/src/battery.c
+++ zephyr/shim/src/battery.c
@@ -7,6 +7,42 @@
 
 #include <zephyr/devicetree.h>
 
+#ifdef CONFIG_FUEL_GAUGE
+#include <zephyr/drivers/fuel_gauge.h>
+#include "battery.h"
+
+/* Using upstream fuel gauge driver */
+
+void battery_get_params(struct batt_params *batt)
+{
+	/* TODO rename upstream_battery to default_battery */
+	const struct device *dev =
+		DEVICE_DT_GET(DT_NODELABEL(upstream_battery));
+
+	struct fuel_gauge_get_property props[] = {
+		{
+			.property_type = FUEL_GAUGE_VOLTAGE,
+		},
+		{
+			.property_type = FUEL_GAUGE_CURRENT,
+		},
+	};
+
+	int ret = fuel_gauge_get_prop(dev, props, ARRAY_SIZE(props));
+
+	/* We explicitly expect some properties to fail */
+	ARG_UNUSED(ret);
+
+	batt->voltage = props[0].value.voltage / 1000;
+	batt->current = props[1].value.current / 1000;
+
+	/*
+	 * TODO(b/271889974): Percolate failed properties to client.
+	 */
+}
+
+#endif /* CONFIG_FUEL_GAUGE */
+
 #define NODE_FUEL_GAUGE(node) \
 	{ \
 	.manuf_name = DT_PROP(node, manuf_name), \
