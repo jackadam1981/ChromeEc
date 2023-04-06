3667344861f574fa2093898f9d6d24289dce4ebe
Eric Yilun Lin <yllin@chromium.org>
2023-04-06 12:55:36 +0800

    mt8186,mt8188: check the holder of AP_RST_ODL
    To distinguish the AP shutdown, and the AP reset held by GSC/Servo,
    we use the SYS_RST_ODL as the reference.
    
    If AP_EC_SYSRST_ODL is asserted, but SYS_RST_ODL is not, this is a
    normal shutdown.
    If AP_EC_SYSRST_ODL is asserted, and so is SYS_RST_ODL, the AP reset
    is held by GSC or Servo. In this case, we should let the power state
    stay at S0 to prevent from an unexpected shutdown.
    
    BUG=b:276229973
    TEST=pass firmware_CorruptMinios
    TEST=dut-control warm_reset:on sleep:1 warm:reset:off, it stays at S0
    BRANCH=none
    
    Change-Id: I9b69caa0d15e6e58e7f11ad1079536bc1954b5ce
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- power/mt8186.c
+++ power/mt8186.c
@@ -80,6 +80,8 @@
 
 /* indicate MT8186 is processing a chipset reset. */
 static bool is_resetting;
+/* indicate MT8186 is AP reset is held by servo or GSC. */
+static bool is_held;
 /* indicate MT8186 is processing a AP forcing shutdown. */
 static bool is_shutdown;
 /* indicate MT8186 has been dropped to S5G3 from the last IN_AP_RST state . */
@@ -123,6 +125,22 @@ static void set_pmic_pwroff(void)
 	};
 
 	GPIO_SET_LEVEL(GPIO_EC_PMIC_EN_ODL, 1);
+}
+
+void chipset_warm_reset_interrupt(enum gpio_signal signal)
+{
+	int sys_rst_ext = gpio_get_level(GPIO_SYS_RST_ODL);
+
+	/* If this is not a chipset_reset, the ap_rst must be held by gsc or
+	 * servo.
+	 */
+	if (!is_resetting && sys_rst_ext == 0 &&
+	    gpio_get_level(signal) == sys_rst_ext)
+		is_held = true;
+	else
+		is_held = false;
+
+	power_signal_interrupt(signal);
 }
 
 static void reset_request_interrupt_deferred(void)
@@ -237,7 +255,8 @@ static void power_reset_host_sleep_state(void)
  *  G3 |         1 |                   x |               1|
  *
  * S5 is a temp stage, which will be put into G3 after s5_inactivity_timeout.
- * is_resetting flag indicate it's resetting chipset, and it's always S0.
+ * is_resetting flag indicates it's resetting chipset, and always S0.
+ * is_held flag indicates the AP reset is held by servo or GSC, and always S0.
  * is_shutdown flag indicates it's shutting down the AP, it goes for S5.
  * is_s5g3_passed flag indicates it has shutdown from S5 to G3 since last
  * shutdown.
@@ -254,6 +273,11 @@ static enum power_state power_get_signal_state(void)
 		return POWER_S0;
 	if (is_shutdown)
 		return POWER_S5;
+	/* GSC or Servo is holding the SYS_RST, in this case, we should stay at
+	 * S0.
+	 */
+	if (is_held)
+		return POWER_S0;
 	if (power_get_signals() & IN_AP_RST) {
 		/* If it has been put to G3 from S5 idle, then stay at G3.*/
 		if (is_s5g3_passed)
@@ -470,6 +494,10 @@ enum power_state power_handle_state(enum power_state state)
 		hook_notify(HOOK_CHIPSET_SHUTDOWN_COMPLETE);
 
 		is_shutdown = false;
+		/* AP down and PMIC off, the servo and GSC is unable to hold the
+		 * AP for S0.
+		 */
+		is_held = false;
 		return POWER_S5;
 
 	case POWER_S5G3:
