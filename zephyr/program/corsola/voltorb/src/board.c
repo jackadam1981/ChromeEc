4fbbafbcad0b2d794dd5b7468813907259938539
mike <mike5@huaqin.corp-partner.google.com>
2023-04-07 19:46:24 +0800

    voltorb: Disable dps function when battery charge full.
    voltorb needs set PDO=15V  when the battery is fully charged
    to prevent battery low current leakage.
    
    BUG=b:269431836
    BRANCH=corsola
    TEST=1. zmake build voltorb
    
    Change-Id: I68e62f739bc1e058131e7a2d918fd6bdf0f62f29
    Signed-off-by: mike <mike5@huaqin.corp-partner.google.com>

---
+++ zephyr/program/corsola/voltorb/src/board.c
@@ -0,0 +1,43 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "charge_state.h"
+#include "common.h"
+#include "dps.h"
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(board_init, LOG_LEVEL_ERR);
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	if (curr->state != ST_CHARGE)
+		return 0;
+
+	/* Lower the max requested voltage to 5V when battery is full. */
+	if (!(curr->batt.flags & BATT_FLAG_BAD_STATUS) &&
+	    !(curr->batt.flags & BATT_FLAG_WANT_CHARGE) &&
+	    (curr->batt.status & STATUS_FULLY_CHARGED)) {
+		if (dps_is_enabled())
+			dps_enable(false);
+	} else {
+		if (!dps_is_enabled())
+			dps_enable(true);
+	}
+
+	return 0;
+}
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
