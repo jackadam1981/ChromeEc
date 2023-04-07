c38b12900838384204c80a50f42376171f7cca6c
Daisuke Nojiri <dnojiri@chromium.org>
2023-04-07 15:28:13 -0700

    ISL9241: Fix ACProchot reg <-> mA conversion
    This CL fixes expressions used to convert ACProchot register values
    to/from the current.
    
    There is no functionality change because the register happens to use
    identical conversion. That is, 128 mA <-> 0x80, for example.
    
    BUG=b:277280318
    BRANCH=None
    TEST=None
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I7c2d6c8b1819c5031306de4ea421193828359290

--- driver/charger/isl9241.c
+++ driver/charger/isl9241.c
@@ -491,10 +491,10 @@ int isl9241_set_ac_prochot(int chgnum, int ma)
 	 * This routine should ensure these bits are not set
 	 * before writing the register.
 	 */
-	if (ma > AC_REG_TO_CURRENT(ISL9241_AC_PROCHOT_CURRENT_MAX))
-		reg = ISL9241_AC_PROCHOT_CURRENT_MAX;
-	else if (ma < AC_REG_TO_CURRENT(ISL9241_AC_PROCHOT_CURRENT_MIN))
-		reg = ISL9241_AC_PROCHOT_CURRENT_MIN;
+	if (ma > ISL9241_AC_PROCHOT_CURRENT_MAX)
+		reg = AC_CURRENT_TO_REG(ISL9241_AC_PROCHOT_CURRENT_MAX);
+	else if (ma < ISL9241_AC_PROCHOT_CURRENT_MIN)
+		reg = AC_CURRENT_TO_REG(ISL9241_AC_PROCHOT_CURRENT_MIN);
 	else
 		reg = AC_CURRENT_TO_REG(ma);
 
