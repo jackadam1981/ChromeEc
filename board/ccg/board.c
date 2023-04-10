c9c9a42d99f93ef015c169c2178a5338fb177903
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-04-10 12:47:29 -0700

    [Do not merge] dummy code to mimic CCG8
    gpioset EC_DS3 1
    gpioset DSW_PWROK_EC 1
    gpioset PM_RSMRST_EC 1
    
    BUG=none
    TEST=none
    BRANCH=none
    
    Change-Id: I0343612c8a40e59a5f1b6743cc258456067f1bd3
    Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

---
+++ board/ccg/board.c
@@ -0,0 +1,13 @@
+/* Copyright 2020 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Intel ADLRVP-ITE board-specific configuration */
+
+#include "i2c.h"
+#include "intc.h"
+#include "registers.h"
+
+#include "gpio_list.h"
+/******************************************************************************/
---
+++ board/ccg/board.h
@@ -0,0 +1,32 @@
+/* Copyright 2020 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Intel ADL-P-RVP-ITE board-specific configuration */
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+#undef CONFIG_ADC
+#undef CONFIG_LID_SWITCH
+#undef CONFIG_HIBERNATE
+#undef CONFIG_SPI_FLASH
+#undef CONFIG_SWITCH
+
+/* eSPI */
+#define CONFIG_HOST_INTERFACE_ESPI
+
+/* Optional feature - used by ITE */
+#define CONFIG_IT83XX_FLASH_CLOCK_48MHZ
+#define CONFIG_IT83XX_VCC_1P8V
+
+#ifndef __ASSEMBLER__
+#include "gpio_signal.h"
+
+enum adc_channel {
+	TEST,
+};
+#endif /* !__ASSEMBLER__ */
+
+#endif /* __CROS_EC_BOARD_H */
