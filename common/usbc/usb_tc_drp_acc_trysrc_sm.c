953b22953f522290721310e01cd81ddf9aef824e
Diana Z <dzigterman@chromium.org>
2023-04-11 13:38:35 -0600

    TCPMv2: Disable PE during ErrorRecovery
    If ErrorRecovery was requested from the TC layer, ensure that the TC
    layer also disables the PE layer.  Otherwise, the PE states may continue
    to progress while we're holding Open on the CC lines.
    
    BRANCH=None
    BUG=b:276837557
    TEST=on skyrim, run some dead battery boots and observe we don't have
    any instances of sending a Request while in ErrorRecovery
    
    Change-Id: I8bd23ff8824b32e3895fe6e71f314aa6f0bc4127
    Signed-off-by: Diana Z <dzigterman@chromium.org>

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -731,6 +731,7 @@ static void tc_detached(int port)
 {
 	TC_CLR_FLAG(port, TC_FLAGS_TS_DTS_PARTNER);
 	hook_notify(HOOK_USB_PD_DISCONNECT);
+	tc_enable_pd(port, 0);
 	tc_pd_connection(port, 0);
 	tcpm_debug_accessory(port, 0);
 	set_ccd_mode(port, 0);
