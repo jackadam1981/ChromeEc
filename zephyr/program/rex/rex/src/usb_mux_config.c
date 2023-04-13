bc6ebb71a73f0fe3651dbe259ea23653291fe301
Madhurima Paruchuri <mparuchuri@google.com>
2023-04-13 04:53:56 +0000

    anx7452: Hardware debugging fixes
    Change-Id: Iee9bfb3b7094abb153c8a2186ce752f4503664e2
    Signed-off-by: Madhurima Paruchuri <mparuchuri@google.com>

--- zephyr/program/rex/rex/src/usb_mux_config.c
+++ zephyr/program/rex/rex/src/usb_mux_config.c
@@ -50,9 +50,24 @@ static void setup_mux(void)
 	}
 	if (usb_db_type == FW_USB_DB_USB3) {
 		LOG_INF("USB DB: Setting USB3 mux");
+		ret = gpio_pin_configure_dt(
+			GPIO_DT_FROM_NODELABEL(gpio_usb_c1_rt_rst_r_odl), 0);
+		if (ret) {
+			LOG_INF("USB DB: USB3: gpio_usb_c1_rt_rst_r_odl configuration failed: %d",
+				ret);
+		}
 	}
 	if (usb_db_type == FW_USB_DB_USB4_ANX7452) {
 		LOG_INF("USB DB: Setting ANX7452 mux");
+		const struct gpio_dt_spec *spec =
+			GPIO_DT_FROM_NODELABEL(gpio_usb_c1_rt_rst_r_odl);
+
+		ret = gpio_pin_configure(spec->port, spec->pin,
+					 GPIO_OUTPUT_LOW);
+		if (ret) {
+			LOG_INF("USB DB: ANX7452: gpio_usb_c1_rt_rst_r_odl configuration failed: %d",
+				ret);
+		}
 		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_anx7452_port1);
 		TCPC_ENABLE_ALTERNATE_BY_NODELABEL(1, rt1716_tcpc_port1);
 		PPC_ENABLE_ALTERNATE_BY_NODELABEL(1, ppc_port1_syv);
