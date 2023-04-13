be76a312ded5ba6c196b704b7a75c541dc2549ae
Brian Granaghan <granaghan@google.com>
2023-04-13 18:15:46 +0000

    cr50: Add get/set factory config command codes.
    BUG=b:275356839
    TEST=build
    
    Change-Id: I6de6de6a43cfe45860bbe9b4aa7cce211da0166e
    Signed-off-by: Brian Granaghan <granaghan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4421422
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>

--- include/tpm_vendor_cmds.h
+++ include/tpm_vendor_cmds.h
@@ -170,6 +170,9 @@ enum vendor_cmd_cc {
 	/* POP_LOG_ENTRY with a 64 bit previous timestamp in ms */
 	VENDOR_CC_POP_LOG_ENTRY_MS = 61,
 
+	VENDOR_CC_GET_FACTORY_CONFIG = 68,
+	VENDOR_CC_SET_FACTORY_CONFIG = 69,
+
 	LAST_VENDOR_COMMAND = 65535,
 };
 
