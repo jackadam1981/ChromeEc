d68b546a0fde25794363dc84bcce97799702bdab
Madhurima Paruchuri <mparuchuri@google.com>
2023-04-13 04:53:56 +0000

    anx7452: Hardware debugging fixes
    Change-Id: Iee9bfb3b7094abb153c8a2186ce752f4503664e2
    Signed-off-by: Madhurima Paruchuri <mparuchuri@google.com>

--- zephyr/test/rex/src/usb_mux_config.c
+++ zephyr/test/rex/src/usb_mux_config.c
@@ -4,6 +4,7 @@
  */
 
 #include "cros_cbi.h"
+#include "gpio.h"
 #include "hooks.h"
 #include "usb_mux.h"
 #include "usbc/usb_muxes.h"
@@ -42,48 +43,95 @@ int mock_cros_cbi_get_fw_config_error(enum cbi_fw_config_field_id field_id,
 	return -1;
 }
 
+void gpio_pin_config_reset(const struct gpio_dt_spec *spec, int flags)
+{
+	gpio_pin_configure(spec->port, spec->pin, flags);
+}
+
 static void usb_mux_config_before(void *fixture)
 {
 	ARG_UNUSED(fixture);
 
 	RESET_FAKE(cros_cbi_get_fw_config);
+	gpio_pin_config_reset(GPIO_DT_FROM_NODELABEL(gpio_usb_c1_rt_rst_r_odl),
+			      0);
 }
 
 ZTEST_USER(usb_mux_config, test_setup_mux)
 {
+	const struct gpio_dt_spec *spec =
+		GPIO_DT_FROM_NODELABEL(gpio_usb_c1_rt_rst_r_odl);
+	gpio_flags_t flags;
+
 	cros_cbi_get_fw_config_fake.custom_fake = mock_cros_cbi_get_fw_config;
 
+	gpio_pin_get_config_dt(spec, &flags);
+	zassert_equal(0, flags);
+
 	hook_notify(HOOK_INIT);
+
+	gpio_pin_get_config_dt(spec, &flags);
+	zassert_equal(GPIO_ACTIVE_LOW, flags);
 
 	zassert_equal(1, cros_cbi_get_fw_config_fake.call_count);
 }
 
 ZTEST_USER(usb_mux_config, test_setup_mux_anx7452)
 {
+	const struct gpio_dt_spec *spec =
+		GPIO_DT_FROM_NODELABEL(gpio_usb_c1_rt_rst_r_odl);
+	gpio_flags_t flags;
+
 	cros_cbi_get_fw_config_fake.custom_fake =
 		mock_cros_cbi_get_fw_config_anx7452;
 
+	gpio_pin_get_config_dt(spec, &flags);
+	zassert_equal(0, flags);
+
 	hook_notify(HOOK_INIT);
+
+	gpio_pin_get_config_dt(spec, &flags);
+	zassert_equal(GPIO_OUTPUT_LOW, flags);
 
 	zassert_equal(1, cros_cbi_get_fw_config_fake.call_count);
 }
 
 ZTEST_USER(usb_mux_config, test_setup_mux_no_usb_db)
 {
+	const struct gpio_dt_spec *spec =
+		GPIO_DT_FROM_NODELABEL(gpio_usb_c1_rt_rst_r_odl);
+	gpio_flags_t flags;
+
 	cros_cbi_get_fw_config_fake.custom_fake =
 		mock_cros_cbi_get_fw_config_no_usb_db;
 
+	gpio_pin_get_config_dt(spec, &flags);
+	zassert_equal(0, flags);
+
 	hook_notify(HOOK_INIT);
+
+	gpio_pin_get_config_dt(spec, &flags);
+	zassert_equal(0, flags);
 
 	zassert_equal(1, cros_cbi_get_fw_config_fake.call_count);
 }
 
 ZTEST_USER(usb_mux_config, test_setup_mux_error_reading_cbi)
 {
+	const struct gpio_dt_spec *spec =
+		GPIO_DT_FROM_NODELABEL(gpio_usb_c1_rt_rst_r_odl);
+	gpio_flags_t flags;
+
 	cros_cbi_get_fw_config_fake.custom_fake =
 		mock_cros_cbi_get_fw_config_error;
 
+	gpio_pin_get_config_dt(spec, &flags);
+	zassert_equal(0, flags);
+
 	hook_notify(HOOK_INIT);
+
+	gpio_pin_get_config_dt(spec, &flags);
+	zassert_equal(0, flags);
 
 	zassert_equal(1, cros_cbi_get_fw_config_fake.call_count);
 }
