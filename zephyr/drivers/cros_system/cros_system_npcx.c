3d8b9ec497b2fbd3eca19da081e108263350e446
Yuval Peress <peress@google.com>
2023-04-13 21:50:39 -0600

    zephyr: Remove deprecated calls to ceiling_fraction()
    The ceiling_fraction() macro was deprecated, use DIV_ROUND_UP()
    instead.
    
    BRANCH=none
    BUG=none
    TEST=zmake build --all
    
    Change-Id: I1dede993fbaebb0fb50f8082412196a0ffdb9c75
    Signed-off-by: Yuval Peress <peress@google.com>

--- zephyr/drivers/cros_system/cros_system_npcx.c
+++ zephyr/drivers/cros_system/cros_system_npcx.c
@@ -280,9 +280,9 @@ system_npcx_hibernate_by_lfw_in_last_ram(const struct device *dev,
 static inline int system_npcx_get_ram_blk_by_lfw_addr(char *address)
 {
 	return NPCX_RAM_BLOCK_COUNT -
-	       ceiling_fraction((uint32_t)address -
-					CONFIG_CROS_EC_PROGRAM_MEMORY_BASE,
-				NPCX_RAM_BLOCK_SIZE);
+	       DIV_ROUND_UP((uint32_t)address -
+				    CONFIG_CROS_EC_PROGRAM_MEMORY_BASE,
+			    NPCX_RAM_BLOCK_SIZE);
 }
 
 static void system_npcx_hibernate_by_disable_ram(const struct device *dev,
