7c5cac2679ef3837e6a9ad20b8770916b39c9993
Daisuke Nojiri <dnojiri@chromium.org>
2023-04-14 13:42:36 -0700

    Hades: Enable ANX7406 for USB-C1
    This CL enables ANX7406 for USB-C port 1.
    
    BUG=b:272821227
    BRANCH=None
    TEST=make BOARD=Hades
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: Ic6c9a869b17670c247376cae95848b5446bbe57a

--- board/hades/i2c.c
+++ board/hades/i2c.c
@@ -22,8 +22,8 @@ const struct i2c_port_t i2c_ports[] = {
 	},
 	{
 		/* I2C2 */
-		.name = "tcpc2",
-		.port = I2C_PORT_USB_C2_TCPC,
+		.name = "tcpc1",
+		.port = I2C_PORT_USB_C1_TCPC,
 		.kbps = 400,
 		.scl = GPIO_EC_I2C_USB_C2_SCL,
 		.sda = GPIO_EC_I2C_USB_C2_SDA,
