3b032932edbaf1259505f8aab078d406d0d9342b
Tristan Honscheid <honscheid@google.com>
2023-04-14 14:32:20 -0600

    presubmit: Validate testcase.yaml files
    Add a helper script that examines testcase.yaml files and call it during
    the presubmit hook. Currently the script scans for extra_args fields in
    the testcase.yaml files that reference the deprecated CONF_FILE,
    OVERLAY_CONFIG, or DTC_OVERLAY_FILE vars and prints a message to use the
    corresponding extra_conf_files, extra_overlay_confs, or
    extra_dtc_overlay_files YAML field.
    
    BUG=None
    BRANCH=None
    TEST=Tried submitting some invalid testcase.yaml files
    
    Change-Id: I0a08c2bec156eb4e43acca255377c7563757c53b
    Signed-off-by: Tristan Honscheid <honscheid@google.com>

--- util/twister_launcher.py
+++ util/twister_launcher.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env vpython3
-
 # Copyright 2022 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -92,6 +91,19 @@ import subprocess
 import sys
 import tempfile
 import time
+
+
+# Paths under the EC base dir that contain tests. This is used to define
+# Twister's search scope.
+EC_TEST_PATHS = [
+    Path("common"),
+    Path("zephyr/test"),
+]
+
+# Paths under ZEPHYR_BASE that we also wish to search for test cases.
+ZEPHYR_TEST_PATHS = [
+    Path("tests/subsys/shell"),
+]
 
 
 def find_checkout() -> Path:
@@ -332,9 +344,12 @@ def main():
         # should encompass all current Zephyr EC tests. The paths are meant to
         # be as specific as possible to limit Twister's search scope. This saves
         # significant time when starting Twister.
-        twister_cli.extend(["-T", str(ec_base / "common")])
-        twister_cli.extend(["-T", str(ec_base / "zephyr/test")])
-        twister_cli.extend(["-T", str(zephyr_base / "tests/subsys/shell")])
+        for path in EC_TEST_PATHS:
+            twister_cli.extend(["-T", str(ec_base / path)])
+
+        # Upstream tests we also wish to run:
+        for path in ZEPHYR_TEST_PATHS:
+            twister_cli.extend(["-T", str(zephyr_base / path)])
 
     if intercepted_args.platform:
         # Pass user-provided -p args when present.
