48fc295aa55196c71e4c1836d31ac89646b3bfc5
Daisuke Nojiri <dnojiri@chromium.org>
2023-04-14 13:37:13 -0700

    ANX7406: Add API to control GPIO0
    This CL adds an API which controls GPIO0 of ANX7406.
    
    BUG=b:269790564
    BRANCH=None
    TEST=make BOARD=hades
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I1e2ebda63fa49aec88d703f31f7bb70730939b63

--- driver/tcpm/anx7406.h
+++ driver/tcpm/anx7406.h
@@ -68,6 +68,10 @@ struct anx7406_i2c_addr {
 #define ANX7406_REG_HPD_OEN BIT(6)
 #define HPD_DEGLITCH_TIME 0x0D
 
+#define ANX7406_REG_GPIO0 0xCA
+#define GPIO0_OUTPUT_HIGH (BIT(0) | BIT(1))
+#define GPIO0_OUTPUT_LOW BIT(0)
+
 #define EXT_I2C_OP_DELAY 1000
 /* Internal I2C0 master */
 /* External I2C0 address & offset */
@@ -131,5 +135,15 @@ int anx7406_set_aux(int port, int flip);
 int anx7406_hpd_reset(const int port);
 void anx7406_update_hpd_status(const struct usb_mux *mux,
 			       mux_state_t mux_state);
+
+/**
+ * Set/clear GPIO
+ *
+ * @param port  USB-C port
+ * @param gpio  GPIO number
+ * @param value  1:high 0:low
+ * @return  enum ec_error_list
+ */
+int anx7406_set_gpio(int port, int gpio, int value);
 
 #endif /* __CROS_EC_USB_PD_TCPM_ANX7406_H */
