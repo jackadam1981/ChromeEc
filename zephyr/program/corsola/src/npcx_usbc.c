22910df1dd293dd56d9ba2fd4bb5b23799e1e939
Eric Yilun Lin <yllin@chromium.org>
2023-04-18 14:46:10 +0800

    kingler: only reset TCPC with power-on reset flag
    Reset the TCPC after a power-on restart, and clear any explicit
    PD contract so that the previous state is not restored. The TCPC
    reset is not needed on starting up with EFS2 enabled since the
    contract will be established after entering RW.
    
    This fixes that while the EC soft-reset is issued, the TCPC
    will be reset on the starting up, and this will cause a brown-out
    if no battery connected.
    
    BUG=b:276661970
    TEST=steelix with AC only, no battery, issue reboot and system boots.
    BRANCH=none
    
    Change-Id: I17f9f3122a51a179996e1a950a18887cb2a5c8be
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>

--- zephyr/program/corsola/src/npcx_usbc.c
+++ zephyr/program/corsola/src/npcx_usbc.c
@@ -53,8 +53,11 @@ DECLARE_HOOK(HOOK_INIT, board_usb_mux_init, HOOK_PRIO_INIT_I2C + 1);
 
 void board_tcpc_init(void)
 {
-	/* Only reset TCPC if not sysjump */
-	if (!system_jumped_late()) {
+	/* Reset TCPC if we have had a system reset.
+	 * With EFSv2, it is possible to be in RW without having reset the TCPC.
+	 */
+	if (!system_jumped_late() &&
+	    (system_get_reset_flags() & EC_RESET_FLAG_POWER_ON)) {
 		/* TODO(crosbug.com/p/61098): How long do we need to wait? */
 		board_reset_pd_mcu();
 	}
