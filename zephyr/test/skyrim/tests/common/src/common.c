336d65055503ed2dc10d59ba681af72f5ed37e94
Diana Z <dzigterman@chromium.org>
2023-04-21 16:03:46 -0600

    Skyrim: Set up BMA422 interrupts
    The BMA422 sensor provides interrupts.  Enable these for all skyrim
    variants using this chip for more accurate readings when the ODR is
    changing frequently.
    
    BRANCH=skyrim
    BUG=b:278156424
    TEST=on frostflow, ensure factory tablet test passes
    
    Change-Id: Iafabd8ccf04cd4df970d6f0c1f77195ba340f76a
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4460851
    Reviewed-by: SamSP Liu <samsp_liu2@compal.corp-partner.google.com>
    Reviewed-by: Chao Gui <chaogui@google.com>
    Commit-Queue: Chao Gui <chaogui@google.com>
    (cherry picked from commit ce23f23d1a43782c6dacbfc443ab3b3d2ef4f738)

--- zephyr/test/skyrim/tests/common/src/common.c
+++ zephyr/test/skyrim/tests/common/src/common.c
@@ -5,3 +5,50 @@
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(skyrim, CONFIG_SKYRIM_LOG_LEVEL);
+<<<<<<< HEAD   (e09fc4 frostflow: Add CONFIG_USB_PD_STARTUP_DELAY_MS)
+=======
+
+/*
+ * Provide weak definitons for interrupt handlers. This minimizes the number
+ * of test-specific device tree overrides needed.
+ */
+
+test_mockable void baseboard_soc_pcore_ocp(enum gpio_signal signal)
+{
+}
+
+test_mockable void baseboard_soc_thermtrip(enum gpio_signal signal)
+{
+}
+
+test_mockable int bmi3xx_interrupt(enum gpio_signal signal)
+{
+	return -EINVAL;
+}
+
+test_mockable int bma4xx_interrupt(enum gpio_signal signal)
+{
+	return -EINVAL;
+}
+
+test_mockable int board_anx7483_c0_mux_set(const struct usb_mux *me,
+					   mux_state_t mux_state)
+{
+	return -EINVAL;
+}
+
+test_mockable int board_anx7483_c1_mux_set(const struct usb_mux *me,
+					   mux_state_t mux_state)
+{
+	return -EINVAL;
+}
+
+test_mockable int power_interrupt_handler(const struct gpio_dt_spec *dt)
+{
+	return -EINVAL;
+}
+
+test_mockable void throttle_ap_prochot_input_interrupt(enum gpio_signal signal)
+{
+}
+>>>>>>> CHANGE (ce23f2 Skyrim: Set up BMA422 interrupts)
