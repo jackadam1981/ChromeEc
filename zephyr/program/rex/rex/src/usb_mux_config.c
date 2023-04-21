3a33cb4dc0ec1ff669563d142946c31125b2ad25
Scott Collyer <scollyer@google.com>
2023-04-20 20:31:31 -0700

    rex: Enable KB8010 DB support
    Change-Id: I1cfa7a4a990d14f851ecce751a8500b4eebaad1c
    Signed-off-by: Scott Collyer <scollyer@google.com>

--- zephyr/program/rex/rex/src/usb_mux_config.c
+++ zephyr/program/rex/rex/src/usb_mux_config.c
@@ -62,6 +62,7 @@ static void setup_usb_db(void)
 		break;
 	case FW_USB_DB_USB4_KB8010:
 		LOG_INF("USB DB: Setting KB8010 mux");
+		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_kb8010_port1);
 		TCPC_ENABLE_ALTERNATE_BY_NODELABEL(1, tcpc_rt1716_port1);
 		PPC_ENABLE_ALTERNATE_BY_NODELABEL(1, ppc_ktu1125_port1);
 		break;
