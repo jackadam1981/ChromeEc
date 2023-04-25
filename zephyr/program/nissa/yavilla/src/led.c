5ac31eb7dd2c24edb39b7c9f4eb6d3d2cb750c26
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2023-04-25 15:31:47 +0800

    yavilla: Update power led behavior
    Yavilla has convertible sku with power led, and the behavior of power
    led is described as following:
    Power state in S0: White on
    Power state in suspend: Blink white (1 sec. on, 1 sec. off)
    Power state in G3/S5: Off
    
    BUG=b:273791621
    BRANCH=none
    TEST=make sure power led works correctly.
    
    Change-Id: I433def8dcfddafbd5e5b3601105621d3aa041e07
    Signed-off-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>

--- zephyr/program/nissa/yavilla/src/led.c
+++ zephyr/program/nissa/yavilla/src/led.c
@@ -18,6 +18,9 @@
 #define BAT_LED_ON 0
 #define BAT_LED_OFF 1
 
+#define PWR_LED_ON 0
+#define PWR_LED_OFF 1
+
 #define BATT_LOW_BCT 10
 
 #define LED_TICKS_PER_CYCLE 4
@@ -26,7 +29,8 @@
 #define POWER_LED_ON_S3_TICKS 2
 
 const enum ec_led_id supported_led_ids[] = { EC_LED_ID_LEFT_LED,
-					     EC_LED_ID_RIGHT_LED };
+					     EC_LED_ID_RIGHT_LED,
+					     EC_LED_ID_POWER_LED };
 
 const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 
@@ -69,6 +73,20 @@ static void led_set_color_battery(int port, enum led_color color)
 	}
 }
 
+static void led_set_color_power(enum led_color color)
+{
+	switch (color) {
+	case LED_OFF:
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_power_led_white_l), PWR_LED_OFF);
+		break;
+	case LED_WHITE:
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_power_led_white_l), PWR_LED_ON);
+		break;
+	default:
+		break;
+	}
+}
+
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
 	switch (led_id) {
@@ -79,6 +97,9 @@ void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 	case EC_LED_ID_RIGHT_LED:
 		brightness_range[EC_LED_COLOR_WHITE] = 1;
 		brightness_range[EC_LED_COLOR_AMBER] = 1;
+		break;
+	case EC_LED_ID_POWER_LED:
+		brightness_range[EC_LED_COLOR_WHITE] = 1;
 		break;
 	default:
 		break;
@@ -103,6 +124,12 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 			led_set_color_battery(RIGHT_PORT, LED_AMBER);
 		else
 			led_set_color_battery(RIGHT_PORT, LED_OFF);
+		break;
+	case EC_LED_ID_POWER_LED:
+		if (brightness[EC_LED_COLOR_WHITE] != 0)
+			led_set_color_power(LED_WHITE);
+		else
+			led_set_color_power(LED_OFF);
 		break;
 	default:
 		return EC_ERROR_PARAM1;
@@ -223,9 +250,28 @@ static void led_set_battery(void)
 	}
 }
 
+static void led_set_power(void)
+{
+	static int power_ticks;
+
+	power_ticks++;
+
+	if (chipset_in_state(CHIPSET_STATE_ON))
+		led_set_color_power(LED_WHITE);
+	else if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND))
+		led_set_color_power((power_ticks % LED_TICKS_PER_CYCLE_S3 <
+				             POWER_LED_ON_S3_TICKS) ?
+				             LED_WHITE : LED_OFF);
+	else
+		led_set_color_power(LED_OFF);
+}
+
 /* Called by hook task every TICK(IT83xx 500ms) */
 static void led_tick(void)
 {
 	led_set_battery();
+
+	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
+		led_set_power();
 }
 DECLARE_HOOK(HOOK_TICK, led_tick, HOOK_PRIO_DEFAULT);
