1d0d65598ef2d1197292927060c91207b3b15635
Keith Short <keithshort@chromium.org>
2023-04-25 17:12:08 -0600

    zephyr: clang: Use no_position_independent variable
    Zephyr upstream added the "no_position_independent" property to track
    the compiler and linker specific flags used to disable position
    independent code and disable position independent executable.
    
    Set the "no_position_independent" property with the correct clang
    compiler and linker flags.
    
    BUG=none
    BRANCH=none
    TEST=./twister -ivc --toolchain llvm
    
    Change-Id: I853f10561df8731db541942f8fa6223b18095127
    Signed-off-by: Keith Short <keithshort@chromium.org>

--- zephyr/cmake/linker/lld/target.cmake
+++ zephyr/cmake/linker/lld/target.cmake
@@ -18,10 +18,9 @@ macro(toolchain_ld_base)
   set_property(GLOBAL PROPERTY PROPERTY_LINKER_SCRIPT_DEFINES
     -D__GCC_LINKER_CMD__)
 
-  # Default flags
+    # Default flags
   zephyr_ld_options(
     ${TOOLCHAIN_LD_FLAGS}
-    -no-pie
     -Wl,--gc-sections
     --build-id=none)
 endmacro()
