eb0c41883e92cab14db6cc06ca15ee6ecb6a8ac4
Rob Barnes <robbarnes@google.com>
2023-04-26 16:20:07 -0600

    octopus: Increase watchdog timeout period to 2100ms
    Increase the watchdog period on NPCX based octopus boards to 2100ms. The
    assumption is that the watchdog may be triggering on false positive
    hangs. Increasing the watchdog has negligible impact on real hangs.
    
    BUG=b:255368431
    BRANCH=octopus
    TEST=Build
    
    Change-Id: Ibcdaf6ff8c1e28dabd914ff0401988d52bdc2372
    Signed-off-by: Rob Barnes <robbarnes@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4482242
    Reviewed-by: Keith Short <keithshort@chromium.org>
    (cherry picked from commit e4c1e440a127c9410884a001ee827cfc3ef5f5ce)

--- baseboard/octopus/baseboard.h
+++ baseboard/octopus/baseboard.h
@@ -55,8 +55,21 @@
 	/* EC variant determines USB-C variant */
 	#define VARIANT_OCTOPUS_USBC_STANDALONE_TCPCS
 
+<<<<<<< HEAD   (39bfb5 octopus: Undef CONFIG_CMD_MMAPINFO)
 	/* Allow the EC to enter deep sleep in S0 */
 	#define CONFIG_LOW_POWER_S0
+=======
+/* Allow the EC to enter deep sleep in S0 */
+#define CONFIG_LOW_POWER_S0
+
+/*
+ * Increase period to prevent false positive hangs (b/255368431).
+ * TODO(b/281584278): Reevaluate period when more data is available.
+ */
+#undef CONFIG_WATCHDOG_PERIOD_MS
+#define CONFIG_WATCHDOG_PERIOD_MS 2100
+
+>>>>>>> CHANGE (e4c1e4 octopus: Increase watchdog timeout period to 2100ms)
 #elif defined(VARIANT_OCTOPUS_EC_ITE8320)
 	 /* Flash clock must be > (50Mhz / 2) */
 	#define CONFIG_IT83XX_FLASH_CLOCK_48MHZ
