013298353ebbda4669f96895c82b218a54bd8d57
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2023-04-26 09:56:17 -0700

    USB-PD: Add PPS sink capability to TCPMv2
    BUG=b:264409462
    TEST=build
    
    Change-Id: If7062968cbe42d1a16e6b85ee788ab5baed20537

--- common/usbc/usb_pe_private.h
+++ common/usbc/usb_pe_private.h
@@ -99,6 +99,8 @@ enum {
 	PE_FLAGS_EPR_EXPLICIT_EXIT_FN,
 	/* Discovery disabled due to UFP/PD 2.0 constraint. */
 	PE_FLAGS_DISCOVERY_DISABLED_FN,
+	/* FLAG to note that PDO request has changed */
+	PE_FLAGS_PPS_ACTIVE_FN,
 	/* Last element */
 	PE_FLAGS_COUNT
 };
