aa54a9fa206aba705dbbb90ceeb1c35c0f8e26ec
wen zhang <zhangwen6@huaqin.corp-partner.google.com>
2023-04-26 11:28:05 +0800

    chgstv2: Change battery near full depends on display SOC
    The battery_near_full is used to set PWR_STATE_DISCHARGE_FULL and
    PWR_STATE_CHARGE_NEAR_FULL and these two states are used to set LED
    behaviors. We should change battery_near_full to depend on display
    SOC because LED bahaviors are already depends on display SOC.
    
    There is an exception, isl9241.c also checks
    charge_get_state() == PWR_STATE_CHARGE_NEAR_FULL in function
    isl9241_restart_charge_voltage_when_full. With this change,
    this feature will depend on the battery display SOC rather than RSOC.
    The display SOC is bigger than RSOC ~3% when the battery is
    BATTERY_LEVEL_NEAR_FULL(default 97%) so this feature may be 3% ahead
    of schedule. The 3% difference should have no harm for this feature.
    
    BUG=b:278512388
    BRANCH=none
    TEST=1.verify LED behavior on voltorb
         2.no "E: Node with matching prop not found" on voltorb
    
    Change-Id: I87f555571e22cbcdd0e8e0a201f76e3de8204ebe
    Signed-off-by: wen zhang <zhangwen6@huaqin.corp-partner.google.com>

--- common/charge_state_v2.c
+++ common/charge_state_v2.c
@@ -2230,7 +2230,8 @@ bool charge_prevent_power_on(bool power_button_pressed)
 
 static int battery_near_full(void)
 {
-	if (charge_get_percent() < BATTERY_LEVEL_NEAR_FULL)
+	if (DIV_ROUND_NEAREST(charge_get_display_charge(), 10) <
+	    BATTERY_LEVEL_NEAR_FULL)
 		return 0;
 
 #ifdef CONFIG_EC_EC_COMM_BATTERY_CLIENT
