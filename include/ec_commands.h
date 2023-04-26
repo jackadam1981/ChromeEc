9ed464d2b42e2a18b5059d863b4600eded2f48d3
Yi Chou <yich@google.com>
2023-04-26 16:04:21 +0800

    fpsensor: Add EC_CMD_FP_ESTABLISH_PK_WRAP
    These commands would generate the final pairing key, and wrap it with
    the encryption key.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Change-Id: I034f5846fa2b5ac00e26b4523b278b25872b2b1b
    Signed-off-by: Yi Chou <yich@google.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -7779,6 +7779,26 @@ struct ec_response_fp_establish_pairing_key_keygen {
 	} encrypted_private_key;
 } __ec_align4;
 
+#define EC_CMD_FP_ESTABLISH_PAIRING_KEY_WRAP 0x0411
+
+struct ec_params_fp_establish_pairing_key_wrap {
+	struct {
+		uint8_t x[FP_PAIRING_KEY_EC_PUBLIC_KEY_LEN];
+		uint8_t y[FP_PAIRING_KEY_EC_PUBLIC_KEY_LEN];
+	} peers_pubkey;
+	struct {
+		struct ec_fp_pairing_key_encryption_metadata info;
+		uint8_t data[FP_PAIRING_KEY_EC_PRIVATE_KEY_LEN];
+	} encrypted_private_key;
+} __ec_align4;
+
+struct ec_response_fp_establish_pairing_key_wrap {
+	struct {
+		struct ec_fp_pairing_key_encryption_metadata info;
+		uint8_t data[FP_PAIRING_KEY_LEN];
+	} encrypted_pairing_key;
+} __ec_align4;
+
 /*****************************************************************************/
 /* Touchpad MCU commands: range 0x0500-0x05FF */
 
