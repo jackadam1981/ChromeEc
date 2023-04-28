1fa3775d6701fd5f9d9a304241324fd4f4d9b828
Kornel Dulęba <korneld@google.com>
2023-04-28 15:30:38 +0000

    WIP: POC: winterhold: PID algorithm PoC
    This is only a POC, it's not meant to be merged, or used in production.
    The idea here is to implement a generic solution based on this.
    
    Leverage the board_override_fan_control_duty function to implement a PID
    PoC on winterhold.
    Currently only PI part is used, i.e. the derivative coefficient is set
    to 0.
    I've found that this configuration yields acceptable results - no fan
    oscillation related to fan duty changes was observed.
    If the fan RPM changes with this config it's likely due to nature of the
    "linear" thermal function approach.
    
    In order to adjust the parameters quickly a new EC command was added -
    "setpid". It takes integers as input and then shifts it by 4 decimal
    places to get the coefficients.
    
    Change-Id: I6f5f4053551539602f9579a1d54872ab16dcfe9f

--- zephyr/program/skyrim/winterhold/src/fan.c
+++ zephyr/program/skyrim/winterhold/src/fan.c
@@ -7,84 +7,113 @@
 #include "fan.h"
 #include "math_util.h"
 #include "thermal.h"
+#include "util.h"
 
 #include <zephyr/kernel.h>
 
 #define CPRINTS(format, args...) cprints(CC_THERMAL, format, ##args)
 #define CPRINTF(format, args...) cprintf(CC_THERMAL, format, ##args)
 
-K_TIMER_DEFINE(grace_period_timer, NULL, NULL);
+struct pid_state {
+	float kp;
+	float ki;
+	float kd;
+
+	/*
+	 * Ideally we'd use double here.
+	 * The problem is that some chips support single-precision floating
+	 * point only. Using doubles on them results in:
+	 * 'undefined reference to `__aeabi_f2d'' linker error.
+	 */
+	float integral;
+	float prev_error;
+};
+
+struct pid_state state = {
+	.kp = 0.0400,
+	.ki = 0.0070,
+	.kd = 0.0005,
+
+	.integral = 0,
+	.prev_error = 0
+};
+
+float pid(struct pid_state *state, float error)
+{
+	float output;
+
+	state->integral += error;
+
+	output = error * state->kp;		/* P */
+	output += state->integral * state->ki;	/* I */
+	output += state->prev_error * state->kd;/* D */
+
+	state->prev_error = error;
+
+	return output;
+}
 
 enum fan_status board_override_fan_control_duty(int ch)
 {
-	int duty, rpm_diff, deviation, duty_step;
 	struct fan_data *data = &fan_data[ch];
 	int rpm_actual = data->rpm_actual;
 	int rpm_target = data->rpm_target;
+	int old_duty;
+	float new_duty;
 
-	/* This works with one fan only. */
-	if (ch != 0) {
-		CPRINTS("Only FAN0 is supported!");
-		return FAN_STATUS_FRUSTRATED;
-	}
-
-	/* Wait for fan RPM to catch up after its duty has been changed. */
-	if (k_timer_remaining_ticks(&grace_period_timer) != 0)
-		return FAN_STATUS_LOCKED;
-
-	duty = fan_get_duty(ch);
-	if (duty == 0 && rpm_target == 0)
+	old_duty = fan_get_duty(ch);
+	if (old_duty == 0 && rpm_target == 0) {
+		state.integral = 0;
+		state.prev_error = 0;
 		return FAN_STATUS_STOPPED;
-
-	/*
-	 * If the current RPM is close enough to the target just leave it.
-	 * It's always going to fluctuate a bit anyway.
-	 */
-	deviation = fans[ch].rpm->rpm_deviation * rpm_target / 100;
-	rpm_diff = rpm_target - rpm_actual;
-	if (rpm_diff > deviation) {
-		/* Can't set duty higher than 100%... */
-		if (duty == 100)
-			return FAN_STATUS_FRUSTRATED;
-	} else if (rpm_diff < -deviation) {
-		/* Can't set duty lower than 1%... */
-		if (duty == 1 && rpm_target != 0)
-			return FAN_STATUS_FRUSTRATED;
-	} else {
-		return FAN_STATUS_LOCKED;
 	}
 
-	/*
-	 * The rpm_diff -> duty_step conversion is specific to a specific
-	 * whiterun fan.
-	 * It has been determined empirically.
-	 */
-	if (ABS(rpm_diff) >= 2500) {
-		duty_step = 35;
-		k_timer_start(&grace_period_timer, K_MSEC(800), K_NO_WAIT);
-	} else if (ABS(rpm_diff) >= 2000) {
-		duty_step = 28;
-		k_timer_start(&grace_period_timer, K_MSEC(800), K_NO_WAIT);
-	} else if (ABS(rpm_diff) >= 1000) {
-		duty_step = 14;
-		k_timer_start(&grace_period_timer, K_MSEC(800), K_NO_WAIT);
-	} else if (ABS(rpm_diff) >= 500) {
-		duty_step = 6;
-		k_timer_start(&grace_period_timer, K_MSEC(800), K_NO_WAIT);
-	} else if (ABS(rpm_diff) >= 250) {
-		duty_step = 3;
-		k_timer_start(&grace_period_timer, K_MSEC(600), K_NO_WAIT);
-	} else {
-		duty_step = 1;
-		k_timer_start(&grace_period_timer, K_MSEC(600), K_NO_WAIT);
-	}
+	new_duty = pid(&state, rpm_target - rpm_actual);
 
-	if (rpm_diff > 0)
-		duty = MIN(duty + duty_step, 100);
+	if (new_duty > 0.0f)
+		new_duty = MIN(new_duty, 100.0f);
 	else
-		duty = MAX(duty - duty_step, 1);
+		new_duty = 0.0f;
 
-	fan_set_duty(ch, duty);
+	if ((int)new_duty == old_duty)
+		return FAN_STATUS_LOCKED;
+
+	fan_set_duty(ch, (int)new_duty);
+
+	CPRINTS("rpm_target: %d, rpm_actual: %d, rpm_diff: %d, old_duty %d: new_duty: %d",
+		rpm_target, rpm_actual, rpm_target - rpm_actual, old_duty, (int)new_duty);
 
 	return FAN_STATUS_CHANGING;
 }
+
+static int command_setpid(int argc, const char **argv)
+{
+	float val;
+
+	if (argc == 1) {
+		CPRINTS("kp: %d, ki: %d, kd: %d",
+			(int)(state.kp * 1000000),
+			(int)(state.ki * 1000000),
+			(int)(state.kd * 1000000));
+		return EC_SUCCESS;
+	}
+
+	if (argc != 4)
+		return EC_ERROR_PARAM_COUNT;
+
+	val = strtoi(argv[1], NULL, 0);
+	state.kp = val / 1000000;
+
+	val = strtoi(argv[2], NULL, 0);
+	state.ki = val / 1000000;
+
+	val = strtoi(argv[3], NULL, 0);
+	state.kd = val / 1000000;
+
+	return EC_SUCCESS;
+}
+
+DECLARE_CONSOLE_COMMAND(
+	setpid, command_setpid, "kp, kd, ki",
+	"Set the PID parameters."
+	"Three integers are expected, each is shifted by 6 deciaml places.");
