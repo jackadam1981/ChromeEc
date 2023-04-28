07f5b15bb7fb03ecb5c43883c54adf035277b786
BJ Chen <bj.chen@ite.com.tw>
2023-04-28 16:26:15 +0800

    TESTONLY: cros usbdc api & threading hookup experiments
    TEST=none
    BRANCH=none
    BUG=none
    
    Change-Id: Idf6e522b7a32101d5585b9626d415e8e301eb512
    Signed-off-by: BJ Chen <bj.chen@ite.com.tw>

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -549,7 +549,11 @@ test_mockable_static void key_state_changed(int row, int col, uint8_t state)
 		return;
 
 	/* No-op for protocols that require full keyboard matrix (e.g. MKBP). */
+	#ifdef CONFIG_USB_DC_TRANS
+	usb_keyboard_state_changed(row, col, !!(state & BIT(row)));
+	#else
 	keyboard_state_changed(row, col, !!(state & BIT(row)));
+	#endif /* #ifdef CONFIG_USB_DC_TRANS */
 }
 
 /* TODO: move this to common header */
