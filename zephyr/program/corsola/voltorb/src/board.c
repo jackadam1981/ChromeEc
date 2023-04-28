11db7df79294e679bb593a682bfc4a5db1d393ed
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2023-04-28 16:02:11 +0800

    [TEST-ONLY] voltorb: check battery wake up time from cut off
    Change-Id: I7c5b4c9a74024846b6611ed49bf4823cf877469c

--- zephyr/program/corsola/voltorb/src/board.c
+++ zephyr/program/corsola/voltorb/src/board.c
@@ -12,6 +12,16 @@
 
 #include <dt-bindings/battery.h>
 
+#include "battery.h"
+#include "battery_smart.h"
+#include "console.h"
+#include "timer.h"
+
+#define CPRINTS(format, args...) cprints(CC_USB, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_USB, format, ##args)
+
+#define BATTERY_NO_RESPONSE_TIMEOUT (2000 * MSEC)
+
 LOG_MODULE_REGISTER(board_init, LOG_LEVEL_ERR);
 
 bool voltorb_is_more_efficient(int curr_mv, int prev_mv, int batt_mv,
@@ -39,3 +49,27 @@ __override struct dps_config_t dps_config = {
 	.t_check = 5 * SECOND,
 	.is_more_efficient = &voltorb_is_more_efficient,
 };
+
+int board_battery_initialized(void)
+{
+	int status;
+	uint64_t wait_timeout = get_time().val + BATTERY_NO_RESPONSE_TIMEOUT;
+
+	while (get_time().val < wait_timeout) {
+		/* Starting pinging battery */
+		battery_status(&status);
+
+		if ((status & STATUS_REMAINING_CAPACITY_ALARM) || 
+			(status & STATUS_TERMINATE_CHARGE_ALARM)) {
+			msleep(25); /* clock stretching could hold 25ms */
+			continue;
+		}
+
+		CPRINTS("battery initialized status %x", status);
+		return EC_SUCCESS;
+	}
+
+	CPRINTS("battery wait stable timeout");
+
+	return EC_ERROR_TIMEOUT;
+}
