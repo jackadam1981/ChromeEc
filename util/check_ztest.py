7228c41ea427382952be47b3a4bbbb4edfc1d26a
Jeremy Bettis <jbettis@google.com>
2023-05-01 21:48:32 -0600

    ec: Replace presubmit_check.sh
    Replace presubmit_check.sh and zephyr_check_testcase_yaml.py with
    python scripts which handle PRESUBMIT_COMMIT, PRESUBMIT_FILES, and also
    work outside of the chroot.
    
    BUG=None
    TEST=on various paths
    
    Change-Id: If7ff5e5d238a5846c19894bb288e96052f99aae2

---
+++ util/check_ztest.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env vpython3
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Checks files for invalid ZTEST decls."""
+
+import argparse
+import contextlib
+import pathlib
+import re
+import subprocess
+import sys
+from typing import Generator, IO, List, Optional
+
+
+PRE_SUBMIT = "pre-submit"
+ZTEST_RE = re.compile(
+    r"(ZTEST|ZTEST_F|ZTEST_USER|ZTEST_USER_F)\(\w+,\s*($|\w+)"
+)
+
+
+@contextlib.contextmanager
+def cat_file(args, filename) -> Generator[IO[str], None, None]:
+    """Read a file either from disk, or from a git commit."""
+    if args.commit == PRE_SUBMIT:
+        with open(filename, encoding="utf-8") as infile:
+            yield infile
+    else:
+        with subprocess.Popen(
+            ["git", "show", f"{args.commit}:{filename}"],
+            universal_newlines=True,
+            stdout=subprocess.PIPE,
+        ) as cmd:
+            assert cmd.stdout
+            yield cmd.stdout
+            if cmd.wait():
+                raise subprocess.CalledProcessError(cmd.returncode, cmd.args)
+
+
+def main(argv: Optional[List[str]] = None) -> Optional[int]:
+    """Look at all files passed in on commandline for ZTEST decls where the
+    test is not named test_.
+    """
+    return_code = 0
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--commit", default=PRE_SUBMIT)
+    parser.add_argument("filename", nargs="+", type=pathlib.Path)
+
+    args = parser.parse_args(argv)
+    for filename in args.filename:
+        with cat_file(args, filename) as infile:
+            linenum = 0
+            for line in infile:
+                line = line.rstrip("\n")
+                linenum += 1
+                match = ZTEST_RE.search(line)
+                if match and match.group(2) == "":
+                    linenum += 1
+                    line2 = infile.readline()
+                    line += line2.strip("\n")
+                    match = ZTEST_RE.search(line)
+                if match and not match.group(2).startswith("test_"):
+                    print(
+                        "error: 'test_' prefix missing from test function name"
+                        f"\n{filename}:{linenum}: {match.group(2)}",
+                        file=sys.stderr,
+                    )
+                    return_code = 1
+
+    return return_code
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
