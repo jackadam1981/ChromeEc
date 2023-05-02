3991669fe0677dcc1075c4cff8cf60435618ff8b
Yi Chou <yich@google.com>
2023-05-02 15:18:58 +0800

    boringssl: Implement sysrand from TRNG
    The boringssl doesn't include the TRNG implementation, we need our own
    CRYPTO_sysrand.
    
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    BRANCH=none
    
    Signed-off-by: Yi Chou <yich@google.com>
    Change-Id: I3136bcc96e09f674278bf338297d7bc0576e174c

--- common/build.mk
+++ common/build.mk
@@ -51,6 +51,7 @@ common-$(CONFIG_BATTERY_FUEL_GAUGE)+=battery_fuel_gauge.o
 common-$(CONFIG_BLUETOOTH_LE)+=bluetooth_le.o
 common-$(CONFIG_BLUETOOTH_LE_STACK)+=btle_hci_controller.o btle_ll.o
 common-$(CONFIG_BODY_DETECTION)+=body_detection.o
+common-$(CONFIG_BORINGSSL_CRYPTO)+=sysrand.o
 common-$(CONFIG_CAPSENSE)+=capsense.o
 common-$(CONFIG_CEC)+=cec.o
 common-$(CONFIG_CBI_EEPROM)+=cbi.o cbi_eeprom.o
