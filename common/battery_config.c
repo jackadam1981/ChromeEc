4eb2b891cfe8e4d574fec9b5f57ca1d0bf192af6
Daisuke Nojiri <dnojiri@chromium.org>
2023-05-05 15:50:42 -0700

    Battery: Rename battery info to battery config
    'Battery info' is ambiguous and overused. It can mean struct
    battery_info, struct battery_static_info, or battery-info-in-cbi.
    
    This CL renames battery-info-in-cbi to battery-config-in-cbi. It'll
    remove at least one source of confusion.
    
    BUG=b:283131235
    TEST=Use bcfg command on Agah.
    TEST=make run-battery_config
    
    Change-Id: I732503b9ef38173773a71e255ceeb23373435e35

--- common/battery_config.c
+++ common/battery_config.c
@@ -2,18 +2,19 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  *
- * Library for read/write battery info in CBI
+ * Module for reading battery config
  *
- * Default battery info (i.e. default_battery_info) will be updated as a value
- * is read from CBI. If a read fails, the failed field will be ignored and
- * search will continue. This allows CBI to store only the differences between
- * the default and the target battery info, which saves us the boot time and the
- * CBI space.
+ * Default battery config (i.e. default_battery_conf) will be updated as a value
+ * is read from a source storage (e.g. CBI). If a read fails, the failed field
+ * will be ignored and search will continue. This allows a source storage to
+ * store only the differences between the default and the target battery config,
+ * which saves us the boot time and the storage space.
  *
- * If CBI is corrupted, this may result in mix of two pieces of battery info.
- * Partially updating the info (or using as much as discovered) is most likely
- * better (and safer) than entirely falling back to the default battery info
- * especially when only one field is missing or corrupted.
+ * If data in a storage is corrupted, this may result in mix of two pieces of
+ * battery config. Partially updating the config (or using as much as
+ * discovered) is most likely better (and safer) than entirely falling back to
+ * the default battery config especially when only one field is missing or
+ * corrupted.
  */
 
 #include "battery_fuel_gauge.h"
@@ -22,11 +23,11 @@
 #include "hooks.h"
 #include "util.h"
 
-#define CPRINTF(format, args...) cprintf(CC_SYSTEM, "CBI " format, ##args)
-#define CPRINTS(format, args...) cprints(CC_SYSTEM, "CBI " format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_SYSTEM, "BCNF " format, ##args)
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, "BCNF " format, ##args)
 
-test_export_static int batt_cbi_read(enum cbi_data_tag tag, uint8_t *data,
-				     uint8_t size)
+test_export_static int batt_conf_read(enum cbi_data_tag tag, uint8_t *data,
+				      uint8_t size)
 {
 	int rv;
 
@@ -45,59 +46,59 @@ test_export_static int batt_cbi_read(enum cbi_data_tag tag, uint8_t *data,
 	return EC_SUCCESS;
 }
 
-test_export_static int batt_cbi_read_ship_mode(struct board_batt_params *info)
+test_export_static int batt_conf_read_ship_mode(struct board_batt_params *info)
 {
 	struct ship_mode_info *ship = &info->fuel_gauge.ship_mode;
 	uint8_t d8;
 
-	batt_cbi_read(CBI_TAG_BATT_SHIP_MODE_REG_ADDR, &ship->reg_addr,
+	batt_conf_read(CBI_TAG_BATT_SHIP_MODE_REG_ADDR, &ship->reg_addr,
 		      sizeof(ship->reg_addr));
 
-	batt_cbi_read(CBI_TAG_BATT_SHIP_MODE_REG_DATA,
+	batt_conf_read(CBI_TAG_BATT_SHIP_MODE_REG_DATA,
 		      (uint8_t *)&ship->reg_data, sizeof(ship->reg_data));
 
-	if (batt_cbi_read(CBI_TAG_BATT_SHIP_MODE_FLAGS, &d8, sizeof(d8)) ==
+	if (batt_conf_read(CBI_TAG_BATT_SHIP_MODE_FLAGS, &d8, sizeof(d8)) ==
 	    EC_SUCCESS)
 		ship->wb_support = d8 & BIT(0) ? 1 : 0;
 
 	return EC_SUCCESS;
 }
 
-test_export_static int batt_cbi_read_sleep_mode(struct board_batt_params *info)
+test_export_static int batt_conf_read_sleep_mode(struct board_batt_params *info)
 {
 	struct sleep_mode_info *sleep = &info->fuel_gauge.sleep_mode;
 	uint8_t d8;
 
-	batt_cbi_read(CBI_TAG_BATT_SLEEP_MODE_REG_ADDR, &sleep->reg_addr,
+	batt_conf_read(CBI_TAG_BATT_SLEEP_MODE_REG_ADDR, &sleep->reg_addr,
 		      sizeof(sleep->reg_addr));
 
-	batt_cbi_read(CBI_TAG_BATT_SLEEP_MODE_REG_DATA,
+	batt_conf_read(CBI_TAG_BATT_SLEEP_MODE_REG_DATA,
 		      (uint8_t *)&sleep->reg_data, sizeof(sleep->reg_data));
 
-	if (batt_cbi_read(CBI_TAG_BATT_SLEEP_MODE_FLAGS, &d8, sizeof(d8)) ==
+	if (batt_conf_read(CBI_TAG_BATT_SLEEP_MODE_FLAGS, &d8, sizeof(d8)) ==
 	    EC_SUCCESS)
 		sleep->sleep_supported = d8 & BIT(0) ? 1 : 0;
 
 	return EC_SUCCESS;
 }
 
-test_export_static int batt_cbi_read_fet_info(struct board_batt_params *info)
+test_export_static int batt_conf_read_fet_info(struct board_batt_params *info)
 {
 	struct fet_info *fet = &info->fuel_gauge.fet;
 	uint8_t d8;
 
-	batt_cbi_read(CBI_TAG_BATT_FET_REG_ADDR, &fet->reg_addr,
+	batt_conf_read(CBI_TAG_BATT_FET_REG_ADDR, &fet->reg_addr,
 		      sizeof(fet->reg_addr));
-	batt_cbi_read(CBI_TAG_BATT_FET_REG_MASK, (uint8_t *)&fet->reg_mask,
+	batt_conf_read(CBI_TAG_BATT_FET_REG_MASK, (uint8_t *)&fet->reg_mask,
 		      sizeof(fet->reg_mask));
-	batt_cbi_read(CBI_TAG_BATT_FET_DISCONNECT_VAL,
+	batt_conf_read(CBI_TAG_BATT_FET_DISCONNECT_VAL,
 		      (uint8_t *)&fet->disconnect_val,
 		      sizeof(fet->disconnect_val));
-	batt_cbi_read(CBI_TAG_BATT_FET_CFET_MASK, (uint8_t *)&fet->cfet_mask,
+	batt_conf_read(CBI_TAG_BATT_FET_CFET_MASK, (uint8_t *)&fet->cfet_mask,
 		      sizeof(fet->cfet_mask));
-	batt_cbi_read(CBI_TAG_BATT_FET_CFET_OFF_VAL,
+	batt_conf_read(CBI_TAG_BATT_FET_CFET_OFF_VAL,
 		      (uint8_t *)&fet->cfet_off_val, sizeof(fet->cfet_off_val));
-	if (batt_cbi_read(CBI_TAG_BATT_FET_FLAGS, &d8, sizeof(d8)) ==
+	if (batt_conf_read(CBI_TAG_BATT_FET_FLAGS, &d8, sizeof(d8)) ==
 	    EC_SUCCESS)
 		fet->mfgacc_support = d8 & BIT(0) ? 1 : 0;
 
@@ -105,79 +106,79 @@ test_export_static int batt_cbi_read_fet_info(struct board_batt_params *info)
 }
 
 test_export_static int
-batt_cbi_read_fuel_gauge_info(struct board_batt_params *info)
+batt_conf_read_fuel_gauge_info(struct board_batt_params *info)
 {
 	struct fuel_gauge_info *fg = &info->fuel_gauge;
 	uint8_t d8;
 
-	batt_cbi_read(CBI_TAG_FUEL_GAUGE_MANUF_NAME, (uint8_t *)&fg->manuf_name,
+	batt_conf_read(CBI_TAG_FUEL_GAUGE_MANUF_NAME, (uint8_t *)&fg->manuf_name,
 		      sizeof(fg->manuf_name));
-	batt_cbi_read(CBI_TAG_FUEL_GAUGE_DEVICE_NAME,
+	batt_conf_read(CBI_TAG_FUEL_GAUGE_DEVICE_NAME,
 		      (uint8_t *)&fg->device_name, sizeof(fg->device_name));
-	if (batt_cbi_read(CBI_TAG_FUEL_GAUGE_FLAGS, &d8, sizeof(d8)) ==
+	if (batt_conf_read(CBI_TAG_FUEL_GAUGE_FLAGS, &d8, sizeof(d8)) ==
 	    EC_SUCCESS)
 		fg->override_nil = d8 & BIT(0) ? 1 : 0;
 
-	batt_cbi_read_ship_mode(info);
-	batt_cbi_read_sleep_mode(info);
-	batt_cbi_read_fet_info(info);
+	batt_conf_read_ship_mode(info);
+	batt_conf_read_sleep_mode(info);
+	batt_conf_read_fet_info(info);
 
 	return EC_SUCCESS;
 }
 
 test_export_static int
-batt_cbi_read_battery_info(struct board_batt_params *info)
+batt_conf_read_battery_info(struct board_batt_params *info)
 {
 	const struct battery_info *batt = &info->batt_info;
 
-	batt_cbi_read(CBI_TAG_BATT_VOLTAGE_MAX, (uint8_t *)&batt->voltage_max,
+	batt_conf_read(CBI_TAG_BATT_VOLTAGE_MAX, (uint8_t *)&batt->voltage_max,
 		      sizeof(batt->voltage_max));
-	batt_cbi_read(CBI_TAG_BATT_VOLTAGE_NORMAL,
+	batt_conf_read(CBI_TAG_BATT_VOLTAGE_NORMAL,
 		      (uint8_t *)&batt->voltage_normal,
 		      sizeof(batt->voltage_normal));
-	batt_cbi_read(CBI_TAG_BATT_VOLTAGE_MIN, (uint8_t *)&batt->voltage_min,
+	batt_conf_read(CBI_TAG_BATT_VOLTAGE_MIN, (uint8_t *)&batt->voltage_min,
 		      sizeof(batt->voltage_min));
-	batt_cbi_read(CBI_TAG_BATT_PRECHARGE_VOLTAGE,
+	batt_conf_read(CBI_TAG_BATT_PRECHARGE_VOLTAGE,
 		      (uint8_t *)&batt->precharge_voltage,
 		      sizeof(batt->precharge_voltage));
-	batt_cbi_read(CBI_TAG_BATT_PRECHARGE_CURRENT,
+	batt_conf_read(CBI_TAG_BATT_PRECHARGE_CURRENT,
 		      (uint8_t *)&batt->precharge_current,
 		      sizeof(batt->precharge_current));
-	batt_cbi_read(CBI_TAG_BATT_START_CHARGING_MIN_C,
+	batt_conf_read(CBI_TAG_BATT_START_CHARGING_MIN_C,
 		      (uint8_t *)&batt->start_charging_min_c,
 		      sizeof(batt->start_charging_min_c));
-	batt_cbi_read(CBI_TAG_BATT_START_CHARGING_MAX_C,
+	batt_conf_read(CBI_TAG_BATT_START_CHARGING_MAX_C,
 		      (uint8_t *)&batt->start_charging_max_c,
 		      sizeof(batt->start_charging_max_c));
-	batt_cbi_read(CBI_TAG_BATT_CHARGING_MIN_C,
+	batt_conf_read(CBI_TAG_BATT_CHARGING_MIN_C,
 		      (uint8_t *)&batt->charging_min_c,
 		      sizeof(batt->charging_min_c));
-	batt_cbi_read(CBI_TAG_BATT_CHARGING_MAX_C,
+	batt_conf_read(CBI_TAG_BATT_CHARGING_MAX_C,
 		      (uint8_t *)&batt->charging_max_c,
 		      sizeof(batt->charging_max_c));
-	batt_cbi_read(CBI_TAG_BATT_DISCHARGING_MIN_C,
+	batt_conf_read(CBI_TAG_BATT_DISCHARGING_MIN_C,
 		      (uint8_t *)&batt->discharging_min_c,
 		      sizeof(batt->discharging_min_c));
-	batt_cbi_read(CBI_TAG_BATT_DISCHARGING_MAX_C,
+	batt_conf_read(CBI_TAG_BATT_DISCHARGING_MAX_C,
 		      (uint8_t *)&batt->discharging_max_c,
 		      sizeof(batt->discharging_max_c));
 
 	return EC_SUCCESS;
 }
 
-test_export_static void batt_cbi_main(void)
+test_export_static void batt_conf_main(void)
 {
 	CPRINTS("%s", __func__);
-	batt_cbi_read_fuel_gauge_info(&default_battery_info);
-	batt_cbi_read_battery_info(&default_battery_info);
+	batt_conf_read_fuel_gauge_info(&default_battery_conf);
+	batt_conf_read_battery_info(&default_battery_conf);
 	CPRINTS("%s done", __func__);
 }
-DECLARE_HOOK(HOOK_INIT, batt_cbi_main, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, batt_conf_main, HOOK_PRIO_DEFAULT);
 
-#ifdef CONFIG_CMD_BATTERY_INFO_IN_CBI
-static struct board_batt_params scratch_battery_info;
+#ifdef CONFIG_CMD_BATTERY_CONFIG
+static struct board_batt_params scratch_battery_conf;
 
-static void batt_cbi_dump(const struct board_batt_params *info)
+static void batt_conf_dump(const struct board_batt_params *info)
 {
 	const struct fuel_gauge_info *fg = &info->fuel_gauge;
 	const struct ship_mode_info *ship = &info->fuel_gauge.ship_mode;
@@ -257,20 +258,20 @@ static void batt_cbi_dump(const struct board_batt_params *info)
 	ccprintf("   },\n"); /* end of batt_info */
 }
 
-static int cc_batt_cbi(int argc, const char *argv[])
+static int cc_batt_conf(int argc, const char *argv[])
 {
 	if (argc == 2 && strcasecmp(argv[1], "read") == 0) {
-		batt_cbi_read_fuel_gauge_info(&scratch_battery_info);
-		batt_cbi_read_battery_info(&scratch_battery_info);
+		batt_conf_read_fuel_gauge_info(&scratch_battery_conf);
+		batt_conf_read_battery_info(&scratch_battery_conf);
 	} else if (argc == 2 && strcasecmp(argv[1], "dump") == 0) {
-		batt_cbi_dump(&scratch_battery_info);
+		batt_conf_dump(&scratch_battery_conf);
 	} else if (argc == 2 && strcasecmp(argv[1], "reset") == 0) {
-		memcpy(&scratch_battery_info, &default_battery_info,
-		       sizeof(scratch_battery_info));
+		memcpy(&scratch_battery_conf, &default_battery_conf,
+		       sizeof(scratch_battery_conf));
 	} else {
 		return EC_ERROR_PARAM_COUNT;
 	}
 	return EC_SUCCESS;
 }
-DECLARE_CONSOLE_COMMAND(biic, cc_batt_cbi, "read | dump | reset", NULL);
-#endif /* CONFIG_CMD_BATTERY_INFO_IN_CBI */
+DECLARE_CONSOLE_COMMAND(bcfg, cc_batt_conf, "read | dump | reset", NULL);
+#endif /* CONFIG_CMD_BATTERY_CONFIG */
