71f233bdacf3f0e27a809f97253c947e08ff0233
Yi Chou <yich@google.com>
2023-05-05 16:23:26 +0800

    TEST: Custom allocator
    BUG=None
    TEST=None
    
    Change-Id: I3ef9fbfb8fdf4c367a077524fb48e93a4e640d01

--- third_party/boringssl/common/elliptic_curve_key_helpers.cc
+++ third_party/boringssl/common/elliptic_curve_key_helpers.cc
@@ -8,58 +8,97 @@
 #include "openssl/mem.h"
 #include "openssl/obj_mac.h"
 
+#include <bitset>
+#include <cstddef>
+#include <cstdint>
+
+template <size_t Size, size_t BlockSize> struct memory_block {
+	static constexpr size_t size = Size;
+	static constexpr size_t block_size = BlockSize;
+	std::bitset<Size> used;
+	uint8_t block[Size][BlockSize];
+};
+
+static memory_block<128, 64> small_bucket;
+static memory_block<16, 512> medium_bucket;
+static memory_block<8, 1024> big_bucket;
+
+template <typename Bucket> void *bucket_alloc(Bucket &bucket, size_t size)
+{
+	if (size > Bucket::block_size) {
+		return nullptr;
+	}
+	for (size_t i = 0; i < Bucket::size; i++) {
+		if (!bucket.used[i]) {
+			bucket.used[i] = true;
+			return bucket.block[i];
+		}
+	}
+	return nullptr;
+}
+
+template <typename Bucket> void bucket_free(Bucket &bucket, void *ptr)
+{
+	uint8_t *address = static_cast<uint8_t *>(ptr);
+	uint8_t *bucket_begin = &bucket.block[0][0];
+	if (address < bucket_begin ||
+	    address >= bucket_begin + sizeof(bucket.block)) {
+		return;
+	}
+
+	int i = (address - bucket_begin) / Bucket::block_size;
+	bucket.used[i] = false;
+}
+
+template <typename Bucket> bool is_this_bucket(Bucket &bucket, void *ptr)
+{
+	uint8_t *address = static_cast<uint8_t *>(ptr);
+	uint8_t *bucket_begin = &bucket.block[0][0];
+	if (address < bucket_begin ||
+	    address >= bucket_begin + sizeof(bucket.block)) {
+		return true;
+	}
+	return false;
+}
+
 extern "C" void *OPENSSL_memory_alloc(size_t size)
 {
-	ccprints("OPENSSL_memory_alloc %d\n", size);
-	uint8_t *result = (uint8_t *)malloc(size + 8);
-	if (result == nullptr)
-		return nullptr;
-	ccprints("%p\n", result + 8);
-	*((int *)((void *)result)) = size;
-	return result + 8;
+	void *result = nullptr;
+	result = bucket_alloc(small_bucket, size);
+	if (result)
+		return result;
+	result = bucket_alloc(medium_bucket, size);
+	if (result)
+		return result;
+	result = bucket_alloc(big_bucket, size);
+	if (result)
+		return result;
+	return result;
 }
 
 extern "C" void OPENSSL_memory_free(void *ptr)
 {
-	ccprints("OPENSSL_memory_free %p\n", ptr);
-	if (ptr == nullptr)
-		return;
-	uint8_t *tmp = (uint8_t *)ptr;
-	tmp -= 8;
-	return free(tmp);
+	bucket_free(small_bucket, ptr);
+	bucket_free(medium_bucket, ptr);
+	bucket_free(big_bucket, ptr);
 }
+
 extern "C" size_t OPENSSL_memory_get_size(void *ptr)
 {
-	ccprints("OPENSSL_memory_get_size %p\n", ptr);
-	if (ptr == nullptr)
-		return 0;
-	uint8_t *tmp = (uint8_t *)ptr;
-	tmp -= 8;
-	int value = *((int *)((void *)tmp));
-	ccprints("%d\n", value);
-	return value;
+	if (is_this_bucket(small_bucket, ptr))
+		return decltype(small_bucket)::block_size;
+	if (is_this_bucket(medium_bucket, ptr))
+		return decltype(medium_bucket)::block_size;
+	if (is_this_bucket(big_bucket, ptr))
+		return decltype(big_bucket)::block_size;
+	return 0;
 }
 
 bssl::UniquePtr<EC_KEY> generate_elliptic_curve_key()
 {
-	using VoidPtr = void *;
-	VoidPtr arr[11] = {
-		OPENSSL_malloc(32), OPENSSL_malloc(24),	 OPENSSL_malloc(20),
-		OPENSSL_malloc(32), OPENSSL_malloc(20),	 OPENSSL_malloc(32),
-		OPENSSL_malloc(20), OPENSSL_malloc(32),	 OPENSSL_malloc(20),
-		OPENSSL_malloc(32), OPENSSL_malloc(344),
-	};
-	for (int i = 0; i < 11; i++) {
-		ccprints("arr[%d] = %p\n", i, arr[i]);
-		OPENSSL_free(arr[i]);
-		if (arr[i] == nullptr) {
-			break;
-		}
-	}
 	bssl::UniquePtr<EC_KEY> key(
 		EC_KEY_new_by_curve_name(NID_X9_62_prime256v1));
 	if (key == nullptr) {
-		ccprints("EC_KEY OOM\n");
 		return nullptr;
 	}
 
