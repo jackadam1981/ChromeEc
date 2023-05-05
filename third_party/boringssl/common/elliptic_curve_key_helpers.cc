e69d7948a761841317cbc5c68c332c27a9aa5c1d
Yi Chou <yich@google.com>
2023-05-05 15:25:59 +0800

    WHY OOM????
    BUG=None
    TEST=None
    
    Change-Id: I464ef347a11f862278bd309a70e0908ae2e75327

--- third_party/boringssl/common/elliptic_curve_key_helpers.cc
+++ third_party/boringssl/common/elliptic_curve_key_helpers.cc
@@ -3,15 +3,63 @@
  * found in the LICENSE file.
  */
 
+#include "console.h"
 #include "openssl/ec_key.h"
 #include "openssl/mem.h"
 #include "openssl/obj_mac.h"
 
+extern "C" void *OPENSSL_memory_alloc(size_t size)
+{
+	ccprints("OPENSSL_memory_alloc %d\n", size);
+	uint8_t *result = (uint8_t *)malloc(size + 8);
+	if (result == nullptr)
+		return nullptr;
+	ccprints("%p\n", result + 8);
+	*((int *)((void *)result)) = size;
+	return result + 8;
+}
+
+extern "C" void OPENSSL_memory_free(void *ptr)
+{
+	ccprints("OPENSSL_memory_free %p\n", ptr);
+	if (ptr == nullptr)
+		return;
+	uint8_t *tmp = (uint8_t *)ptr;
+	tmp -= 8;
+	return free(tmp);
+}
+extern "C" size_t OPENSSL_memory_get_size(void *ptr)
+{
+	ccprints("OPENSSL_memory_get_size %p\n", ptr);
+	if (ptr == nullptr)
+		return 0;
+	uint8_t *tmp = (uint8_t *)ptr;
+	tmp -= 8;
+	int value = *((int *)((void *)tmp));
+	ccprints("%d\n", value);
+	return value;
+}
+
 bssl::UniquePtr<EC_KEY> generate_elliptic_curve_key()
 {
+	using VoidPtr = void *;
+	VoidPtr arr[11] = {
+		OPENSSL_malloc(32), OPENSSL_malloc(24),	 OPENSSL_malloc(20),
+		OPENSSL_malloc(32), OPENSSL_malloc(20),	 OPENSSL_malloc(32),
+		OPENSSL_malloc(20), OPENSSL_malloc(32),	 OPENSSL_malloc(20),
+		OPENSSL_malloc(32), OPENSSL_malloc(344),
+	};
+	for (int i = 0; i < 11; i++) {
+		ccprints("arr[%d] = %p\n", i, arr[i]);
+		OPENSSL_free(arr[i]);
+		if (arr[i] == nullptr) {
+			break;
+		}
+	}
 	bssl::UniquePtr<EC_KEY> key(
 		EC_KEY_new_by_curve_name(NID_X9_62_prime256v1));
 	if (key == nullptr) {
+		ccprints("EC_KEY OOM\n");
 		return nullptr;
 	}
 
