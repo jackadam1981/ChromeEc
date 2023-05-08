560b190528ecb103ed35e7210f8af950f61908bb
Dino Li <Dino.Li@ite.com.tw>
2023-05-08 14:36:24 +0800

    TEST: Roach: HID Keyboard & TouchPad
    Change-Id: I114f35c58a3a47de9f2786efd9cc34c46067c2b3
    Signed-off-by: BJ Chen <bj.chen@ite.com.tw>

---
+++ zephyr/subsys/usb_dc/usb_dc.h
@@ -0,0 +1,16 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * USB DC Shimming Definitions.
+ */
+
+#ifndef __USB_DC_H
+#define __USB_DC_H
+
+#include "common.h"
+
+bool check_usb_is_suspended(void);
+void request_usb_wake(void);
+
+#endif
