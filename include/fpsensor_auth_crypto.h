566bc6e8d6d1bf14363dac66f2a3f3b3dab30367
Yi Chou <yich@google.com>
2023-05-08 12:53:46 +0800

    fpsensor: Add supporting functions for decryption
    BUG=b:248508087
    TEST=make run-fpsensor_auth_commands -j
    
    Change-Id: I60a2e9e36b50a1c755ab023f38712c323de7dcf7

--- include/fpsensor_auth_crypto.h
+++ include/fpsensor_auth_crypto.h
@@ -38,6 +38,17 @@ bssl::UniquePtr<EC_KEY>
 create_ec_key_from_pubkey(const struct ec_fp_ec_public_key &pubkey);
 
 /**
+ * Create a boringssl @EC_KEY from a private key.
+ *
+ * @param[in] privkey private key
+ *
+ * @return @p EC_KEY on success
+ * @return nullptr on error
+ */
+bssl::UniquePtr<EC_KEY> create_ec_key_from_privkey(const uint8_t *privkey,
+						   size_t privkey_size);
+
+/**
  * Encrypt the data in place with a specific version of encryption method and
  * output the metadata and encrypted data.
  *
@@ -74,5 +85,45 @@ encrypt_data_in_place(uint16_t version,
 enum ec_error_list
 fill_encrypted_private_key(const EC_KEY &key, uint16_t version,
 			   struct ec_fp_encrypted_private_key &enc_key);
+
+/**
+ * Decrypt the encrypted data.
+ *
+ * version 1 is 128 bit AES-GCM, and the encryption key is bound to the TPM
+ * seed, rollback secret and user_id.
+ *
+ * @param[in] info the metadata of the encryption output
+ * @param[in] enc_data the encrypted data
+ * @param[in] enc_data_size the size of encrypted data
+ * @param[in] version the version of the encryption method
+ * @param[out] data the decrypted data
+ * @param[in] data_size the size of decrypted data
+ *
+ * @return EC_SUCCESS on success
+ * @return EC_ERROR_INVAL on error
+ */
+enum ec_error_list
+decrypt_data(const struct ec_fp_auth_command_encryption_metadata &info,
+	     const uint8_t *enc_data, size_t enc_data_size, uint8_t *data,
+	     size_t data_size);
+
+/**
+ * Decrypt the encrypted private key.
+ *
+ * version 1 is 128 bit AES-GCM, and the encryption key is bound to the TPM
+ * seed, rollback secret and user_id.
+ *
+ * @param[in] info the metadata of the encryption output
+ * @param[in] enc_data the encrypted data
+ * @param[in] enc_data_size the size of encrypted data
+ * @param[in] version the version of the encryption method
+ * @param[out] data the decrypted data
+ * @param[in] data_size the size of decrypted data
+ *
+ * @return EC_SUCCESS on success
+ * @return EC_ERROR_INVAL on error
+ */
+bssl::UniquePtr<EC_KEY> decrypt_private_key(
+	const struct ec_fp_encrypted_private_key &encrypted_private_key);
 
 #endif /* __CROS_EC_FPSENSOR_AUTH_CRYPTO_H */
