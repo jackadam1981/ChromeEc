1806b90ee1949cfe6d478c7714a511b221d5083d
Dino Li <Dino.Li@ite.com.tw>
2023-05-08 14:36:24 +0800

    TEST: Roach: enable hid keyboard
    Change-Id: I4d87dd8c7bf35ed703c76db0f1f635fcf0cc4c09

---
+++ zephyr/subsys/usb_dc/usb_hid_kb.c
@@ -0,0 +1,266 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "keyboard_config.h"
+
+#include <errno.h>
+
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/class/usb_hid.h>
+
+LOG_MODULE_DECLARE(usb_hid_kb, LOG_LEVEL_INF);
+
+#define HID_KEYBOARD_MODIFIER_LOW 0xe0
+#define HID_KEYBOARD_MODIFIER_HIGH 0xe7
+#define HID_KEYBOARD_ASSISTANT_KEY 0xf0
+
+#define CONFIG_USB_HID_KB_NUM_TOP_ROW_KEYS 10
+
+#define KEYBOARD_BASE_DESC                                                 \
+	0x05, 0x01, /* Usage Page (Generic Desktop) */                     \
+		0x09, 0x06, /* Usage (Keyboard) */                         \
+		0xA1, 0x01, /* Collection (Application) */                 \
+                                                                           \
+		/* Modifiers */                                            \
+		0x05, 0x07, /* Usage Page (Key Codes) */                   \
+		0x19, HID_KEYBOARD_MODIFIER_LOW, /* Usage Minimum */       \
+		0x29, HID_KEYBOARD_MODIFIER_HIGH, /* Usage Maximum */      \
+		0x15, 0x00, /* Logical Minimum (0) */                      \
+		0x25, 0x01, /* Logical Maximum (1) */                      \
+		0x75, 0x01, /* Report Size (1) */                          \
+		0x95, 0x08, /* Report Count (8) */                         \
+		0x81, 0x02, /* Input (Data, Variable, Absolute), ;Modifier \
+			       byte */                                     \
+                                                                           \
+		0x95, 0x01, /* Report Count (1) */                         \
+		0x75, 0x08, /* Report Size (8) */                          \
+		0x81, 0x01, /* Input (Constant), ;Reserved byte */         \
+                                                                           \
+		/* Normal keys */                                          \
+		0x95, 0x06, /* Report Count (6) */                         \
+		0x75, 0x08, /* Report Size (8) */                          \
+		0x15, 0x00, /* Logical Minimum (0) */                      \
+		0x25, 0xa4, /* Logical Maximum (164) */                    \
+		0x05, 0x07, /* Usage Page (Key Codes) */                   \
+		0x19, 0x00, /* Usage Minimum (0) */                        \
+		0x29, 0xa4, /* Usage Maximum (164) */                      \
+		0x81, 0x00, /* Input (Data, Array), ;Key arrays (6 bytes) */
+
+#define KEYBOARD_TOP_ROW_DESC                                                 \
+	/* Modifiers */                                                       \
+	0x05, 0x0C, /* Consumer Page */                                       \
+		0x0A, 0x24, 0x02, /* AC Back (0x224) */                       \
+		0x0A, 0x25, 0x02, /* AC Forward (0x225) */                    \
+		0x0A, 0x27, 0x02, /* AC Refresh (0x227) */                    \
+		0x0A, 0x32, 0x02, /* AC View Toggle (0x232) */                \
+		0x0A, 0x9F, 0x02, /* AC Desktop Show All windows (0x29F) */   \
+		0x09, 0x70, /* Display Brightness Decrement (0x70) */         \
+		0x09, 0x6F, /* Display Brightness Increment (0x6F) */         \
+		0x09, 0xE2, /* Mute (0xE2) */                                 \
+		0x09, 0xEA, /* Volume Decrement (0xEA) */                     \
+		0x09, 0xE9, /* Volume Increment (0xE9) */                     \
+		0x0B, 0x46, 0x00, 0x07, 0x00, /* PrintScreen (Page 0x7, Usage \
+						 0x46) */                     \
+		0x0A, 0xD0, 0x02, /* Privacy Screen Toggle (0x2D0) */         \
+		0x09, 0x7A, /* Keyboard Brightness Decrement (0x7A) */        \
+		0x09, 0x79, /* Keyboard Brightness Increment (0x79)*/         \
+		0x09, 0xCD, /* Play / Pause (0xCD) */                         \
+		0x09, 0xB5, /* Scan Next Track (0xB5) */                      \
+		0x09, 0xB6, /* Scan Previous Track (0xB6) */                  \
+		0x09, 0x7C, /* Keyboard Backlight OOC (0x7C) */               \
+		0x0B, 0x2F, 0x00, 0x0B, 0x00, /* Phone Mute (Page 0xB, Usage  \
+						 0x2F) */                     \
+		0x09, 0x32, /* Sleep (0x32) */                                \
+		0x15, 0x00, /* Logical Minimum (0) */                         \
+		0x25, 0x01, /* Logical Maximum (1) */                         \
+		0x75, 0x01, /* Report Size (1) */                             \
+		0x95, 0x14, /* Report Count (20) */                           \
+		0x81, 0x02, /* Input (Data, Variable, Absolute), ;Modifier    \
+			       byte */                                        \
+                                                                              \
+		/* 12-bit padding */                                          \
+		0x95, 0x0C, /* Report Count (12) */                           \
+		0x75, 0x01, /* Report Size (1) */                             \
+		0x81, 0x01, /* Input (Constant), ;1-bit padding */
+
+#define KEYBOARD_TOP_ROW_FEATURE_DESC                                         \
+	0x06, 0xd1, 0xff, /* Usage Page (Google) */                           \
+		0x09, 0x01, /* Usage (Top Row List) */                        \
+		0xa1, 0x02, /* Collection (Logical) */                        \
+		0x05, 0x0a, /*   Usage Page (Ordinal) */                      \
+		0x19, 0x01, /*   Usage Minimum (1) */                         \
+		0x29, CONFIG_USB_HID_KB_NUM_TOP_ROW_KEYS, /* Usage Maximum */ \
+		0x95, CONFIG_USB_HID_KB_NUM_TOP_ROW_KEYS, /* Report Count */  \
+		0x75, 0x20, /*   Report Size (32) */                          \
+		0xb1, 0x03, /*   Feature (Cnst,Var,Abs) */                    \
+		0xc0, /* End Collection */
+
+/*
+ * Vendor-defined Usage Page 0xffd1:
+ *  - 0x18: Assistant key
+ *  - 0x19: Tablet mode switch
+ */
+#ifdef HID_KEYBOARD_EXTRA_FIELD
+#ifdef CONFIG_KEYBOARD_ASSISTANT_KEY
+#define KEYBOARD_ASSISTANT_KEY_DESC                                        \
+	0x19, 0x18, /* Usage Minimum */                                    \
+		0x29, 0x18, /* Usage Maximum */                            \
+		0x15, 0x00, /* Logical Minimum (0) */                      \
+		0x25, 0x01, /* Logical Maximum (1) */                      \
+		0x75, 0x01, /* Report Size (1) */                          \
+		0x95, 0x01, /* Report Count (1) */                         \
+		0x81, 0x02, /* Input (Data, Variable, Absolute), ;Modifier \
+			       byte */
+#else
+/* No assistant key: just pad 1 bit. */
+#define KEYBOARD_ASSISTANT_KEY_DESC               \
+	0x95, 0x01, /* Report Count (1) */        \
+		0x75, 0x01, /* Report Size (1) */ \
+		0x81, 0x01, /* Input (Constant), ;1-bit padding */
+#endif /* !CONFIG_KEYBOARD_ASSISTANT_KEY */
+
+#ifdef CONFIG_KEYBOARD_TABLET_MODE_SWITCH
+#define KEYBOARD_TABLET_MODE_SWITCH_DESC                                   \
+	0x19, 0x19, /* Usage Minimum */                                    \
+		0x29, 0x19, /* Usage Maximum */                            \
+		0x15, 0x00, /* Logical Minimum (0) */                      \
+		0x25, 0x01, /* Logical Maximum (1) */                      \
+		0x75, 0x01, /* Report Size (1) */                          \
+		0x95, 0x01, /* Report Count (1) */                         \
+		0x81, 0x02, /* Input (Data, Variable, Absolute), ;Modifier \
+			       byte */
+#else
+/* No tablet mode swtch: just pad 1 bit. */
+#define KEYBOARD_TABLET_MODE_SWITCH_DESC          \
+	0x95, 0x01, /* Report Count (1) */        \
+		0x75, 0x01, /* Report Size (1) */ \
+		0x81, 0x01, /* Input (Constant), ;1-bit padding */
+#endif /* CONFIG_KEYBOARD_TABLET_MODE_SWITCH */
+
+#define KEYBOARD_VENDOR_DESC                                                 \
+	0x06, 0xd1, 0xff, /* Usage Page (Vendor-defined 0xffd1) */           \
+                                                                             \
+		KEYBOARD_ASSISTANT_KEY_DESC KEYBOARD_TABLET_MODE_SWITCH_DESC \
+                                                                             \
+		0x95,                                                        \
+		0x01, /* Report Count (1) */                                 \
+		0x75, 0x06, /* Report Size (6) */                            \
+		0x81, 0x01, /* Input (Constant), ;6-bit padding */
+#endif /* HID_KEYBOARD_EXTRA_FIELD */
+
+#define KEYBOARD_BACKLIGHT_DESC                                       \
+	0xA1, 0x02, /* Collection (Logical) */                        \
+		0x05, 0x14, /*   Usage Page (Alphanumeric Display) */ \
+		0x09, 0x46, /*   Usage (Display Brightness) */        \
+		0x95, 0x01, /*   Report Count (1) */                  \
+		0x75, 0x08, /*   Report Size (8) */                   \
+		0x15, 0x00, /*   Logical Minimum (0) */               \
+		0x25, 0x64, /*   Logical Maximum (100) */             \
+		0x91, 0x02, /*   Output (Data, Variable, Absolute) */ \
+		0xC0, /* End Collection */
+
+/* HID : Report Descriptor */
+static const uint8_t hid_report_desc[] = {
+	KEYBOARD_BASE_DESC
+
+#ifdef KEYBOARD_VENDOR_DESC
+	KEYBOARD_VENDOR_DESC
+#endif
+
+#ifdef CONFIG_USB_HID_KEYBOARD_VIVALDI
+	KEYBOARD_TOP_ROW_DESC KEYBOARD_TOP_ROW_FEATURE_DESC
+#endif
+	0xC0 /* End Collection */
+};
+
+/* The standard Chrome OS keyboard matrix table. See HUT 1.12v2 Table 12 and
+ * https://www.w3.org/TR/DOM-Level-3-Events-code .
+ *
+ * Assistant key is mapped as 0xf0, but this key code is never actually send.
+ */
+const uint8_t keycodes[KEYBOARD_COLS_MAX][KEYBOARD_ROWS] = {
+	{ 0x00, 0x00, 0xe0, 0xe3, 0xe4, HID_KEYBOARD_ASSISTANT_KEY, 0x00,
+	  0x00 },
+	{ 0xe3, 0x29, 0x2b, 0x35, 0x04, 0x1d, 0x1e, 0x14 },
+	{ 0x3a, 0x3d, 0x3c, 0x3b, 0x07, 0x06, 0x20, 0x08 },
+	{ 0x05, 0x0a, 0x17, 0x22, 0x09, 0x19, 0x21, 0x15 },
+	{ 0x43, 0x40, 0x3f, 0x3e, 0x16, 0x1b, 0x1f, 0x1a },
+	{ 0x87, 0x00, 0x30, 0x00, 0x0e, 0x36, 0x25, 0x0c },
+	{ 0x11, 0x0b, 0x1c, 0x23, 0x0d, 0x10, 0x24, 0x18 },
+	{ 0x00, 0x00, 0x64, 0x00, 0x00, 0xe1, 0x00, 0xe5 },
+	{ 0x2e, 0x34, 0x2F, 0x2d, 0x33, 0x38, 0x27, 0x13 },
+	{ 0x00, 0x42, 0x41, 0x68, 0x0f, 0x37, 0x26, 0x12 },
+	{ 0xe6, 0x00, 0x89, 0x00, 0x31, 0x00, 0xe2, 0x00 },
+	{ 0x00, 0x2a, 0x00, 0x31, 0x28, 0x2c, 0x51, 0x52 },
+	{ 0x00, 0x8a, 0x00, 0x8b, 0x00, 0x00, 0x4f, 0x50 },
+};
+
+static enum usb_dc_status_code usb_status;
+
+void keyboard_state_changed(int row, int col, int is_pressed)
+{
+	int ret;
+	uint8_t report[8];
+	const struct device *hid_dev;
+
+	hid_dev = device_get_binding("HID_0");
+
+	uint8_t keycode = keycodes[col][row];
+
+	if (!keycode) {
+		LOG_INF("Unknown key at %d/%d\n", row, col);
+		return;
+	}
+
+	LOG_INF("Key pressed keycode:%x pressed:%d\n", keycode, is_pressed);
+
+	memset(&report, 0, sizeof(report));
+	if (is_pressed) {
+		report[2] = keycode;
+	}
+
+	ret = hid_int_ep_write(hid_dev, report, sizeof(report), NULL);
+	if (ret) {
+		LOG_INF("HID write error, %d", ret);
+	}
+
+	//queue_keycode_event(keycode, is_pressed);
+}
+
+static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
+{
+	usb_status = status;
+}
+
+static int usb_hid_kb_init(void)
+{
+	int ret;
+	const struct device *hid_dev;
+
+	hid_dev = device_get_binding("HID_0");
+	if (hid_dev == NULL) {
+		LOG_ERR("Cannot get USB HID Device");
+		return 1;
+	}
+
+	usb_hid_register_device(hid_dev,
+				hid_report_desc, sizeof(hid_report_desc),
+				NULL);
+
+	usb_hid_init(hid_dev);
+
+	ret = usb_enable(status_cb);
+	if (ret != 0) {
+		LOG_ERR("Failed to enable USB");
+		return 1;
+	}
+
+	return 0;
+}
+SYS_INIT(usb_hid_kb_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
