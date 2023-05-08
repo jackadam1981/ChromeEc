0400c086c365077d0a462fd292b72c75a4df4145
Dino Li <Dino.Li@ite.com.tw>
2023-05-08 14:36:24 +0800

    TEST: Roach: enable hid keyboard
    Change-Id: I4d87dd8c7bf35ed703c76db0f1f635fcf0cc4c09

--- zephyr/program/roach/src/one_wire.c
+++ zephyr/program/roach/src/one_wire.c
@@ -46,10 +46,6 @@ static int ec_ec_comm_init(void)
 }
 SYS_INIT(ec_ec_comm_init, APPLICATION, 50);
 
-void keyboard_state_changed(int row, int col, int is_pressed)
-{
-}
-
 void detachable_keyboard_add(const uint8_t *state)
 {
 	board_uart_tx(ROACH_CMD_KEYBOARD_MATRIX, state, KEYBOARD_COLS_MAX);
