9e973774e550fac91348c96594f85e122a775302
Yi Chou <yich@google.com>
2023-05-08 12:35:54 +0800

    fpsensor: Add supporting functions for public key
    BUG=b:248508087
    TEST=make run-fpsensor_auth_commands -j
    
    Change-Id: Ic8ff370d17cd5da56070ba3a1778f24b429a8274

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -7753,6 +7753,43 @@ struct ec_response_fp_read_match_secret {
 	uint8_t positive_match_secret[FP_POSITIVE_MATCH_SECRET_BYTES];
 } __ec_align4;
 
+/* Constants for pairing key establishment */
+#define FP_PAIRING_KEY_ENC_METADATA_VERSION 1
+#define FP_PAIRING_KEY_LEN 32
+#define FP_PAIRING_KEY_NONCE_BYTES 12
+#define FP_PAIRING_KEY_ENCRYPTION_SALT_BYTES 16
+#define FP_PAIRING_KEY_TAG_BYTES 16
+
+#define FP_EC_PUBLIC_KEY_POINT_LEN 32
+#define FP_EC_PRIVATE_KEY_LEN 32
+
+struct ec_fp_ec_public_key {
+	uint8_t x[FP_EC_PUBLIC_KEY_POINT_LEN];
+	uint8_t y[FP_EC_PUBLIC_KEY_POINT_LEN];
+} __ec_align4;
+
+struct ec_fp_auth_command_encryption_metadata {
+	/*
+	 * Version of the structure format
+	 * (FP_PAIRING_KEY_ENC_METADATA_VERSION).
+	 */
+	uint16_t struct_version;
+	/* Reserved bytes, set to 0. */
+	uint16_t reserved;
+	/*
+	 * The salt is *only* ever used for key derivation. The nonce is unique,
+	 * a different one is used for every message.
+	 */
+	uint8_t nonce[FP_PAIRING_KEY_NONCE_BYTES];
+	uint8_t encryption_salt[FP_PAIRING_KEY_ENCRYPTION_SALT_BYTES];
+	uint8_t tag[FP_PAIRING_KEY_TAG_BYTES];
+} __ec_align4;
+
+struct ec_fp_encrypted_private_key {
+	struct ec_fp_auth_command_encryption_metadata info;
+	uint8_t data[FP_EC_PRIVATE_KEY_LEN];
+} __ec_align4;
+
 /*****************************************************************************/
 /* Touchpad MCU commands: range 0x0500-0x05FF */
 
