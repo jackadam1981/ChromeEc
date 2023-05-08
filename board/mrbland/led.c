717d7d0a7e1a5ed61c46b09caea130f62c6c9b8a
Simon Glass <sjg@chromium.org>
2023-05-08 15:33:31 -0600

    charger: Move charger_state_v2.h into charge_state.h
    We don't have a v1 anymore, so the name makes no sense. Move it into
    the existing file.
    
    Include charge_state.h instead of v2, doing this in the same change to
    avoid build errors.
    
    This makes no functional change.
    
    BUG=b:218332694
    TEST=make buildall
    
    Change-Id: Ic3e3adc45e4d002c2cd5ba8aa65e24686e01d628

--- board/mrbland/led.c
+++ board/mrbland/led.c
@@ -100,7 +100,7 @@ static void board_led_set_battery(void)
 
 	battery_ticks++;
 
-	switch (led_pwr_state()) {
+	switch (pwr_get_state()) {
 	case PWR_STATE_CHARGE:
 	case PWR_STATE_CHARGE_NEAR_FULL:
 		if (chipset_in_state(CHIPSET_STATE_ON |
@@ -159,7 +159,7 @@ static void board_led_set_power(void)
 
 	power_ticks++;
 
-	switch (led_pwr_state()) {
+	switch (pwr_get_state()) {
 	case PWR_STATE_CHARGE:
 	case PWR_STATE_CHARGE_NEAR_FULL:
 	case PWR_STATE_DISCHARGE:
