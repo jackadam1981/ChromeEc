0af8ac2e36f0d3c902356a747a7c70a9644c7f0f
Dino Li <Dino.Li@ite.com.tw>
2023-05-08 14:36:24 +0800

    TEST: Roach: enable HID Keyboard & TouchPad
    Change-Id: I114f35c58a3a47de9f2786efd9cc34c46067c2b3
    Signed-off-by: BJ Chen <bj.chen@ite.com.tw>

---
+++ zephyr/subsys/usb_dc/usb_dc.c
@@ -0,0 +1,55 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include "common.h"
+#include "util.h"
+#include "usb_dc.h"
+
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/usb/usb_dc.h>
+#include <zephyr/usb/class/usb_hid.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(usb_dc, LOG_LEVEL_INF);
+
+static enum usb_dc_status_code usb_status;
+
+static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
+{
+	usb_status = status;
+}
+
+bool check_usb_is_suspended(void)
+{
+	/* TODO */
+	return false;
+}
+
+void request_usb_wake(void)
+{
+	if (IS_ENABLED(CONFIG_USB_DEVICE_REMOTE_WAKEUP)) {
+		if (usb_status == USB_DC_SUSPEND) {
+			usb_wakeup_request();
+			return;
+		}
+	}
+}
+
+static int usb_dc_init(void)
+{
+	int ret = usb_enable(status_cb);
+
+	if (ret != 0) {
+		LOG_ERR("Failed to enable USB");
+		return ret;
+	}
+
+	return 0;
+
+}
+
+SYS_INIT(usb_dc_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
