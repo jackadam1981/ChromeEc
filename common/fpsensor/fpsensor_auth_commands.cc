3553314a87382dad63a683ab8c4187eea43eb86c
Yi Chou <yich@google.com>
2023-05-08 12:35:54 +0800

    fpsensor_auth_commands: Add public key functions
    BUG=b:248508087
    TEST=make run-fpsensor_auth_commands -j
    
    Change-Id: Ic8ff370d17cd5da56070ba3a1778f24b429a8274

--- common/fpsensor/fpsensor_auth_commands.cc
+++ common/fpsensor/fpsensor_auth_commands.cc
@@ -3,11 +3,33 @@
  * found in the LICENSE file.
  */
 
+#include "compile_time_macros.h"
+
+/* Boringssl headers need to be included before extern "C" section. */
+#include "crypto/elliptic_curve_key.h"
+#include "openssl/bn.h"
+#include "openssl/ec.h"
+#include "openssl/mem.h"
+#include "openssl/obj_mac.h"
+
+#include <assert.h>
+
+extern "C" {
+#include "common.h"
+#include "ec_commands.h"
+#include "host_command.h"
+#include "sha256.h"
+#include "system.h"
+#include "task.h"
+#include "trng.h"
+#include "util.h"
+}
+
 #include "fpsensor.h"
 #include "fpsensor_crypto.h"
 #include "fpsensor_state.h"
 #include "fpsensor_utils.h"
-#include "openssl/mem.h"
+#include "scoped_fast_cpu.h"
 
 /**
  * Clear all fingerprint templates associated with the current user id.
@@ -21,4 +43,95 @@ void fp_clear_context(void)
 	fp_disable_positive_match_secret(&positive_match_secret_state);
 	for (uint16_t idx = 0; idx < FP_MAX_FINGER_COUNT; idx++)
 		fp_clear_finger_context(idx);
+}
+
+enum ec_error_list fill_pubkey(const EC_KEY &key,
+			       struct ec_fp_ec_public_key &pubkey)
+{
+	static_assert(sizeof(pubkey) == sizeof(pubkey.x) + sizeof(pubkey.y));
+
+	/* POINT_CONVERSION_UNCOMPRESSED indicates that the point is encoded as
+	 * z||x||y, where z is the octet 0x04. */
+	uint8_t *pubkey_ptr = nullptr;
+	if (EC_KEY_key2buf(&key, POINT_CONVERSION_UNCOMPRESSED, &pubkey_ptr,
+			   nullptr) !=
+	    sizeof(pubkey.x) + sizeof(pubkey.y) + 1) {
+		return EC_ERROR_INVAL;
+	}
+
+	bssl::UniquePtr<uint8_t> pubkey_data(pubkey_ptr);
+	memcpy(&pubkey, pubkey_data.get() + 1,
+	       sizeof(pubkey.x) + sizeof(pubkey.y));
+
+	return EC_SUCCESS;
+}
+
+enum ec_error_list
+encrypt_data_in_place(uint16_t version,
+		      struct ec_fp_auth_command_encryption_metadata &info,
+		      uint8_t *data, size_t data_size)
+{
+	info.struct_version = version;
+	trng_init();
+	trng_rand_bytes(info.nonce, sizeof(info.nonce));
+	trng_rand_bytes(info.encryption_salt, sizeof(info.encryption_salt));
+	trng_exit();
+
+	uint8_t enc_key[SBP_ENC_KEY_LEN];
+	enum ec_error_list ret =
+		derive_encryption_key(enc_key, info.encryption_salt);
+	if (ret != EC_SUCCESS) {
+		return EC_ERROR_INVAL;
+	}
+
+	/* Encrypt the secret blob in-place. */
+	ret = aes_gcm_encrypt(enc_key, SBP_ENC_KEY_LEN, data, data, data_size,
+			      info.nonce, sizeof(info.nonce), info.tag,
+			      sizeof(info.tag));
+	OPENSSL_cleanse(enc_key, sizeof(enc_key));
+	if (ret != EC_SUCCESS) {
+		return EC_ERROR_INVAL;
+	}
+
+	return EC_SUCCESS;
+}
+
+enum ec_error_list
+fill_encrypted_private_key(const EC_KEY &key, uint16_t version,
+			   struct ec_fp_encrypted_private_key &enc_key)
+{
+	if (EC_KEY_priv2oct(&key, enc_key.data, sizeof(enc_key.data)) !=
+	    sizeof(enc_key.data)) {
+		return EC_ERROR_INVAL;
+	}
+
+	return encrypt_data_in_place(version, enc_key.info, enc_key.data,
+				     sizeof(enc_key.data));
+}
+
+bssl::UniquePtr<EC_KEY>
+create_ec_key_from_pubkey(const struct ec_fp_ec_public_key &pubkey)
+{
+	bssl::UniquePtr<BIGNUM> x_bn(
+		BN_bin2bn(pubkey.x, sizeof(pubkey.x), nullptr));
+	if (x_bn == nullptr) {
+		return nullptr;
+	}
+
+	bssl::UniquePtr<BIGNUM> y_bn(
+		BN_bin2bn(pubkey.y, sizeof(pubkey.y), nullptr));
+	if (y_bn == nullptr) {
+		return nullptr;
+	}
+
+	static_assert(sizeof(pubkey.x) == 32);
+	bssl::UniquePtr<EC_KEY> key(
+		EC_KEY_new_by_curve_name(NID_X9_62_prime256v1));
+
+	if (EC_KEY_set_public_key_affine_coordinates(key.get(), x_bn.get(),
+						     y_bn.get()) != 1) {
+		return nullptr;
+	}
+
+	return key;
 }
