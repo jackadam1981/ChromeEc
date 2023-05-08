cca5c0d5385dea213f627b1dcd293ab135cf3b6b
Yi Chou <yich@google.com>
2023-05-08 12:35:54 +0800

    fpsensor: Add supporting functions for public key
    BUG=b:248508087
    TEST=make run-fpsensor_auth_commands -j
    
    Change-Id: Ic8ff370d17cd5da56070ba3a1778f24b429a8274

--- include/fpsensor_auth_commands.h
+++ include/fpsensor_auth_commands.h
@@ -8,9 +8,21 @@
 #ifndef __CROS_EC_FPSENSOR_AUTH_COMMANDS_H
 #define __CROS_EC_FPSENSOR_AUTH_COMMANDS_H
 
+#include "openssl/ec.h"
+
+extern "C" {
+#include "ec_commands.h"
+}
+
 /**
  * Clear all fingerprint templates associated with the current user id.
  */
 void fp_clear_context(void);
+
+enum ec_error_list fill_pubkey(const EC_KEY &key,
+			       struct ec_fp_ec_public_key &pubkey);
+
+bssl::UniquePtr<EC_KEY>
+create_ec_key_from_pubkey(const struct ec_fp_ec_public_key &pubkey);
 
 #endif /* __CROS_EC_FPSENSOR_AUTH_COMMANDS_H */
