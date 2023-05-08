b2d6857c96c425cc5c2f566701204267261accbb
Yi Chou <yich@google.com>
2023-05-08 12:35:54 +0800

    fpsensor: Add supporting functions for public key
    BUG=b:248508087
    TEST=make run-fpsensor_auth_commands -j
    
    Change-Id: Ic8ff370d17cd5da56070ba3a1778f24b429a8274

--- common/fpsensor/build.mk
+++ common/fpsensor/build.mk
@@ -19,6 +19,7 @@ _fpsensor_obj:=$(_fpsensor_dir)fpsensor.o
 _fpsensor_detect_strings_obj:=$(_fpsensor_dir)fpsensor_detect_strings.o
 _fpsensor_debug_obj:=$(_fpsensor_dir)fpsensor_debug.o
 _fpsensor_auth_commands_obj:=$(_fpsensor_dir)fpsensor_auth_commands.o
+_fpsensor_auth_crypto_obj:=$(_fpsensor_dir)fpsensor_auth_crypto.o
 
 $(out)/RW/$(_fpsensor_state_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_crypto_obj): CFLAGS+=$(fpsensor_CFLAGS)
@@ -26,6 +27,7 @@ $(out)/RW/$(_fpsensor_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_detect_strings_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_debug_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_auth_commands_obj): CFLAGS+=$(fpsensor_CFLAGS)
+$(out)/RW/$(_fpsensor_auth_crypto_obj): CFLAGS+=$(fpsensor_CFLAGS)
 
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_state_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_crypto_obj)
@@ -33,5 +35,6 @@ all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_obj)
 all-obj-$(HAS_TASK_CONSOLE)+=$(_fpsensor_detect_strings_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_debug_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_auth_commands_obj)
+all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_auth_crypto_obj)
 
 endif # CONFIG_FINGERPRINT_MCU or TEST_BUILD
