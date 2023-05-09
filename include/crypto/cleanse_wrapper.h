38aaed3ed317c30f2964f75baa70199f9cbe5e2f
Yi Chou <yich@google.com>
2023-05-09 16:53:29 +0800

    boringssl: Add cleanse_wrapper
    A Drop-in replacement wrapper for OPENSSL_cleanse.
    
    TEST=make V=1 BOARD=bloonchipper -j
    TEST=make run-boringssl_crypto -j
    TEST=./test/run_device_tests.py --board bloonchipper -t boringssl_crypto
         => PASS
    TEST=./test/run_device_tests.py --board dartmonkey -t boringssl_crypto
         => PASS
    
    Change-Id: I041105cfd3580536c45e2c2afcdef074b7214ba7

---
+++ include/crypto/cleanse_wrapper.h
@@ -0,0 +1,38 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* A Drop-in replacement wrapper for OPENSSL_cleanse. */
+
+#ifndef __CROS_EC_CRYPTO_CLEANSE_WRAPPER_H
+#define __CROS_EC_CRYPTO_CLEANSE_WRAPPER_H
+
+#include "openssl/mem.h"
+
+#include <type_traits>
+#include <utility>
+
+/* This only works for standard layout type. */
+template <typename T, typename = std::enable_if_t<std::is_standard_layout_v<T> > >
+class CleanseWrapper : public T {
+    public:
+	using T::T;
+	using T::operator=;
+
+	constexpr CleanseWrapper(T &&t)
+		: T(std::move(t))
+	{
+	}
+	constexpr CleanseWrapper(const T &t)
+		: T(t)
+	{
+	}
+
+	~CleanseWrapper()
+	{
+		OPENSSL_cleanse(this, sizeof(*this));
+	}
+};
+
+#endif /* __CROS_EC_CRYPTO_CLEANSE_WRAPPER_H */
--- test/boringssl_crypto.cc
+++ test/boringssl_crypto.cc
@@ -4,6 +4,7 @@
  */
 
 #include "common.h"
+#include "crypto/cleanse_wrapper.h"
 #include "crypto/elliptic_curve_key.h"
 #include "openssl/bn.h"
 #include "openssl/ec.h"
@@ -12,6 +13,13 @@
 #include "openssl/rand.h"
 #include "test_util.h"
 #include "util.h"
+
+extern "C" {
+#include "sha256.h"
+}
+
+#include <array>
+#include <memory>
 
 test_static enum ec_error_list test_rand(void)
 {
@@ -58,9 +66,158 @@ test_static enum ec_error_list test_ecc_keygen(void)
 	return EC_SUCCESS;
 }
 
+test_static enum ec_error_list test_cleanse_wrapper_std_array(void)
+{
+	using TypeToCheck = std::array<uint8_t, 6>;
+	using Wrapped = CleanseWrapper<TypeToCheck>;
+
+	/* Preserve a space for it. */
+	uint8_t *buffer = static_cast<uint8_t *>(malloc(sizeof(Wrapped)));
+	Wrapped *data = reinterpret_cast<Wrapped *>(buffer);
+
+	/* Call the constructor. */
+	new (data) Wrapped({ 1, 1, 1, 1, 1, 1 });
+
+	TEST_ASSERT_MEMSET(data->data(), 1, data->size());
+
+	/* Call the destructor. */
+	data->~Wrapped();
+
+	TEST_ASSERT_MEMSET(buffer, 0, sizeof(Wrapped));
+
+	/* Free the space. */
+	free(buffer);
+
+	return EC_SUCCESS;
+}
+
+test_static enum ec_error_list test_cleanse_wrapper_sha256(void)
+{
+	using TypeToCheck = struct sha256_ctx;
+	using Wrapped = CleanseWrapper<TypeToCheck>;
+
+	/* Preserve a space for it. */
+	uint8_t *buffer = static_cast<uint8_t *>(malloc(sizeof(Wrapped)));
+	Wrapped *data = reinterpret_cast<Wrapped *>(buffer);
+
+	/* Call the constructor. */
+	new (data) Wrapped();
+
+	std::array<uint8_t, 5> data_to_sha = { 1, 2, 3, 4, 5 };
+	SHA256_init(data);
+	SHA256_update(data, data_to_sha.data(), data_to_sha.size());
+	SHA256_final(data);
+	uint8_t *result = SHA256_final(data);
+
+	std::array<uint8_t, 32> expected_result = {
+		0x10, 0x5C, 0xC1, 0x32, 0x30, 0x80, 0xE3, 0x73,
+		0xB9, 0xFB, 0xA6, 0x88, 0x38, 0x4F, 0x40, 0xAE,
+		0x45, 0x09, 0x89, 0xFB, 0xC9, 0x3A, 0xD4, 0xFD,
+		0x29, 0xCA, 0x57, 0xC6, 0x29, 0xCD, 0x4A, 0x60,
+	};
+
+	TEST_ASSERT_ARRAY_EQ(result, expected_result, expected_result.size());
+
+	/* Call the destructor. */
+	data->~Wrapped();
+
+	TEST_ASSERT_MEMSET(buffer, 0, sizeof(Wrapped));
+
+	/* Free the space. */
+	free(buffer);
+
+	return EC_SUCCESS;
+}
+
+test_static enum ec_error_list test_cleanse_wrapper_custom_struct(void)
+{
+	struct TestingStruct {
+		bool used;
+		std::array<uint32_t, 4> data;
+	};
+
+	using TypeToCheck = TestingStruct;
+	using Wrapped = CleanseWrapper<TypeToCheck>;
+
+	/* Preserve a space for it. */
+	uint8_t *buffer = static_cast<uint8_t *>(malloc(sizeof(Wrapped)));
+	Wrapped *data = reinterpret_cast<Wrapped *>(buffer);
+
+	/* Call the constructor. */
+	new (data) Wrapped({
+		.used = true,
+		.data = { 0x7fffffffu, 0x12345678u, 0x0u, 0x42u },
+	});
+
+	TEST_EQ(data->used, true, "%d");
+	TEST_EQ(data->data[0], 0x7fffffffu, "%d");
+	TEST_EQ(data->data[1], 0x12345678u, "%d");
+	TEST_EQ(data->data[2], 0x0u, "%d");
+	TEST_EQ(data->data[3], 0x42u, "%d");
+
+	/* Call the destructor. */
+	data->~Wrapped();
+
+	TEST_ASSERT_MEMSET(buffer, 0, sizeof(Wrapped));
+
+	/* Free the space. */
+	free(buffer);
+
+	return EC_SUCCESS;
+}
+
+test_static enum ec_error_list test_cleanse_wrapper_normal_usage(void)
+{
+	struct TestingStruct {
+		bool used;
+		std::array<uint32_t, 4> data;
+	};
+
+	CleanseWrapper<std::array<uint8_t, 6> > array({ 1, 1, 1, 1, 1, 1 });
+
+	TEST_ASSERT_MEMSET(array.data(), 1, array.size());
+
+	CleanseWrapper<TestingStruct> data({
+		.used = true,
+		.data = { 0x7fffffffu, 0x12345678u, 0x0u, 0x42u },
+	});
+
+	TEST_EQ(data.used, true, "%d");
+	TEST_EQ(data.data[0], 0x7fffffffu, "%d");
+	TEST_EQ(data.data[1], 0x12345678u, "%d");
+	TEST_EQ(data.data[2], 0x0u, "%d");
+	TEST_EQ(data.data[3], 0x42u, "%d");
+
+	CleanseWrapper<struct sha256_ctx> ctx;
+
+	std::array<uint8_t, 5> data_to_sha = { 1, 2, 3, 4, 5 };
+	SHA256_init(&ctx);
+	SHA256_update(&ctx, data_to_sha.data(), data_to_sha.size());
+	SHA256_final(&ctx);
+	uint8_t *result = SHA256_final(&ctx);
+
+	std::array<uint8_t, 32> expected_result = {
+		0x10, 0x5C, 0xC1, 0x32, 0x30, 0x80, 0xE3, 0x73,
+		0xB9, 0xFB, 0xA6, 0x88, 0x38, 0x4F, 0x40, 0xAE,
+		0x45, 0x09, 0x89, 0xFB, 0xC9, 0x3A, 0xD4, 0xFD,
+		0x29, 0xCA, 0x57, 0xC6, 0x29, 0xCD, 0x4A, 0x60,
+	};
+
+	TEST_ASSERT_ARRAY_EQ(result, expected_result, expected_result.size());
+
+	/* There is no way to check the contest is cleared without undefined
+	 * behavior. */
+
+	return EC_SUCCESS;
+}
+
 extern "C" void run_test(int argc, const char **argv)
 {
 	RUN_TEST(test_rand);
 	RUN_TEST(test_ecc_keygen);
+	RUN_TEST(test_cleanse_wrapper_std_array);
+	RUN_TEST(test_cleanse_wrapper_sha256);
+	RUN_TEST(test_cleanse_wrapper_custom_struct);
+	RUN_TEST(test_cleanse_wrapper_normal_usage);
 	test_print_result();
 }
