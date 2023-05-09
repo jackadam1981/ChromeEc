70402623830928764bf13dc0f00d9e04d1d73993
wen zhang <zhangwen6@huaqin.corp-partner.google.com>
2023-05-09 21:36:12 +0800

    zephyr: Add tests for screebo usb_config
    Add test for screebo usb_config
    
    BUG=b:278160232
    TEST=./twister --coverage -v -i -s rex/rex.screebo.usb_config
    
    Change-Id: I36e5777b8dd6d44651f980e9b691b11cf51b7624

--- zephyr/program/rex/screebo/src/usb_config.c
+++ zephyr/program/rex/screebo/src/usb_config.c
@@ -23,6 +23,19 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
+#ifdef CONFIG_ZTEST
+
+#undef USB_MUX_ENABLE_ALTERNATIVE
+#define USB_MUX_ENABLE_ALTERNATIVE(x)
+
+#undef TCPC_ENABLE_ALTERNATE_BY_NODELABEL
+#define TCPC_ENABLE_ALTERNATE_BY_NODELABEL(x, y)
+
+#undef PPC_ENABLE_ALTERNATE_BY_NODELABEL
+#define PPC_ENABLE_ALTERNATE_BY_NODELABEL(x, y)
+
+#endif /* CONFIG_ZTEST */
+
 LOG_MODULE_REGISTER(screebo, LOG_LEVEL_INF);
 
 uint32_t usb_db_type;
