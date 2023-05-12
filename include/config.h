9580299147844b9bd92eda98d69465108d06c666
David Cross <davidmcross@google.com>
2023-05-12 15:29:51 -0700

    rwsig: Support memory mapped signature addresses
    Some targets, such as the npcx9, require reading the signature from a
    memory mapped address but CONFIG_RW_SIG_ADDR must be set to the
    physical address because it is also used by the linker scripts to
    place the signature. To resolve this issue, utilize a separate
    CONFIG_RWSIG_READ_ADDR address that defaults to CONFIG_RW_SIG_ADDR.
    
    BUG=b:273569976
    TEST=compare_build.sh -b all => MATCH
    
    Change-Id: Ia65a1784a79596ee09c52e0921fa7c8ddd13eacc

--- include/config.h
+++ include/config.h
@@ -3944,6 +3944,7 @@
 #undef CONFIG_RO_PUBKEY_SIZE
 #undef CONFIG_RW_SIG_ADDR
 #undef CONFIG_RW_SIG_SIZE
+#undef CONFIG_RWSIG_READ_ADDR
 
 /* Size of the serial number if needed */
 #undef CONFIG_SERIALNO_LEN
--- include/rwsig.h
+++ include/rwsig.h
@@ -95,6 +95,11 @@ void rwsig_jump_now(void);
 #endif
 #endif /* CONFIG_RO_PUBKEY_ADDR */
 
+/* Some chips require reading signature from a memory mapped address */
+#ifndef CONFIG_RWSIG_READ_ADDR
+#define CONFIG_RWSIG_READ_ADDR CONFIG_RW_SIG_ADDR
+#endif
+
 #ifndef CONFIG_RW_SIG_SIZE
 #ifdef CONFIG_RWSIG_TYPE_RWSIG
 /*
