75857657ea1821754875c37a91605ce7b2c3d160
wen zhang <zhangwen6@huaqin.corp-partner.google.com>
2023-05-15 20:00:56 +0800

    screebo: squash CLs for bring up
    this CL contains DB/MB/LED CL and fix C1 HBR i2c addr and
    add CONFIG_LOG too.
    
    BUG=b:9999
    TEST=build pass
    
    Change-Id: I8d478a7c8d3de53cb2be6dc9b2c8a0471d13f450

--- zephyr/program/rex/screebo/src/usb_config.c
+++ zephyr/program/rex/screebo/src/usb_config.c
@@ -5,13 +5,41 @@
 
 /* Screebo board-specific USB-C configuration */
 
+#include "console.h"
+#include "cros_board_info.h"
+#include "cros_cbi.h"
+#include "driver/ppc/nx20p348x.h"
 #include "driver/tcpm/ps8xxx_public.h"
+#include "hooks.h"
 #include "ppc/syv682x_public.h"
 #include "system.h"
+#include "usb_config.h"
+#include "usbc/ppc.h"
+#include "usbc/tcpci.h"
+#include "usbc/usb_muxes.h"
 #include "usbc_config.h"
 #include "usbc_ppc.h"
 
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+#ifdef CONFIG_ZTEST
+
+#undef USB_MUX_ENABLE_ALTERNATIVE
+#define USB_MUX_ENABLE_ALTERNATIVE(x)
+
+#undef TCPC_ENABLE_ALTERNATE_BY_NODELABEL
+#define TCPC_ENABLE_ALTERNATE_BY_NODELABEL(x, y)
+
+#undef PPC_ENABLE_ALTERNATE_BY_NODELABEL
+#define PPC_ENABLE_ALTERNATE_BY_NODELABEL(x, y)
+
+#endif /* CONFIG_ZTEST */
+
+LOG_MODULE_REGISTER(screebo, LOG_LEVEL_INF);
+
+uint32_t usb_db_type;
+uint32_t usb_mb_type;
 
 void screebo_ppc_interrupt(enum gpio_signal signal)
 {
@@ -20,7 +48,11 @@ void screebo_ppc_interrupt(enum gpio_signal signal)
 		syv682x_interrupt(USBC_PORT_C0);
 		break;
 	case GPIO_USB_C1_PPC_INT_ODL:
-		syv682x_interrupt(USBC_PORT_C1);
+		if (usb_db_type == FW_USB_DB_USB3) {
+			nx20p348x_interrupt(USBC_PORT_C1);
+		} else {
+			syv682x_interrupt(USBC_PORT_C1);
+		}
 		break;
 	default:
 		break;
@@ -33,10 +65,61 @@ void board_reset_pd_mcu(void)
 	reset_nct38xx_port(USBC_PORT_C0);
 
 	/* Reset TCPC1 */
-	if (tcpc_config[1].rst_gpio.port) {
-		gpio_pin_set_dt(&tcpc_config[1].rst_gpio, 1);
-		msleep(PS8XXX_RESET_DELAY_MS);
-		gpio_pin_set_dt(&tcpc_config[1].rst_gpio, 0);
-		msleep(PS8815_FW_INIT_DELAY_MS);
+	if (usb_db_type == FW_USB_DB_USB3) {
+		if (tcpc_config[1].rst_gpio.port) {
+			gpio_pin_set_dt(&tcpc_config[1].rst_gpio, 1);
+			msleep(PS8XXX_RESET_DELAY_MS);
+			gpio_pin_set_dt(&tcpc_config[1].rst_gpio, 0);
+			msleep(PS8815_FW_INIT_DELAY_MS);
+		}
+	} else {
+		reset_nct38xx_port(USBC_PORT_C1);
 	}
+}
+
+static void setup_alt_db(void)
+{
+	int ret;
+
+	ret = cros_cbi_get_fw_config(FW_USB_DB, &usb_db_type);
+	if (ret != 0) {
+		LOG_ERR("USB DB: Failed to get FW_USB_DB from CBI");
+		usb_db_type = -1;
+		return;
+	}
+
+	if (usb_db_type == FW_USB_DB_USB3) {
+		LOG_INF("USB DB: USB3 DB connected");
+		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_ps8xxx_port1);
+		TCPC_ENABLE_ALTERNATE_BY_NODELABEL(1, tcpc_ps8xxx_port1);
+		PPC_ENABLE_ALTERNATE_BY_NODELABEL(1, ppc_nxp_port1);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, setup_alt_db, HOOK_PRIO_POST_I2C);
+
+static void setup_mb_usb(void)
+{
+	int ret;
+
+	ret = cros_cbi_get_fw_config(FW_USB_MB, &usb_mb_type);
+	if (ret != 0) {
+		LOG_ERR("USB MB: Failed to get FW_USB_MB from CBI");
+		usb_mb_type = -1;
+		return;
+	}
+
+	if (usb_db_type == FW_USB_MB_USB3) {
+		LOG_INF("USB MB: C0 port is USB3");
+		USB_MUX_ENABLE_ALTERNATIVE(usb_mux_chain_usb3_port0);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, setup_mb_usb, HOOK_PRIO_POST_I2C);
+
+__override bool board_is_tbt_usb4_port(int port)
+{
+	/* Both C0 and C1 are USB4 port */
+	if (usb_mb_type == FW_USB_MB_USB4_HB)
+		return true;
+
+	return false;
 }
