6acf3f0012e0990801de1d3bfc9559182139d06a
Daisuke Nojiri <dnojiri@chromium.org>
2023-05-16 13:38:50 -0700

    fuel gauge: Refactor battery detection
    Currently fuel gauge module saves a detected battery as an index to
    struct board_batt_params array. This CL makes the module save a battery
    as a pointer.
    
    This allows us to use the same code for battery-info-in-cbi module,
    which doesn't use an array of battery info.
    
    BUG=b:283131235
    TEST=Agah
    
    Change-Id: I4cdf3adfedb989303bfa040a2ea54312f0f45556

--- common/battery_fuel_gauge.c
+++ common/battery_fuel_gauge.c
@@ -15,6 +15,45 @@
 
 #define CPRINTS(format, args...) cprints(CC_CHARGER, format, ##args)
 
+struct board_batt_params *batt_params;
+
+
+#ifdef CONFIG_BATTERY_TYPE_NO_AUTO_DETECT
+
+/* Variable to decide the battery type */
+static int fixed_battery_type = BATTERY_TYPE_UNINITIALIZED;
+
+/*
+ * Function to get the fixed battery type.
+ */
+static int battery_get_fixed_battery_type(void)
+{
+	if (fixed_battery_type == BATTERY_TYPE_UNINITIALIZED) {
+		CPRINTS("Warning: Battery type is not Initialized! "
+			"Setting to default battery type.\n");
+		fixed_battery_type = DEFAULT_BATTERY_TYPE;
+	}
+
+	return fixed_battery_type;
+}
+
+/*
+ * Function to set the battery type, when auto-detection cannot be used.
+ */
+void battery_set_fixed_battery_type(int type)
+{
+	if (type < BATTERY_TYPE_COUNT)
+		fixed_battery_type = type;
+}
+#endif /* CONFIG_BATTERY_TYPE_NO_AUTO_DETECT */
+
+#ifndef CONFIG_BATTERY_INFO_IN_CBI
+/**
+ * Allows us to override the battery in order to select the battery which has
+ * the right configuration for the test.
+ */
+test_export_static int battery_fuel_gauge_type_override = -1;
+
 /*
  * Authenticate the battery connected.
  *
@@ -56,51 +95,17 @@ test_export_static bool authenticate_battery_type(int index,
 	CPRINTS("found batt:%s", fuel_gauge->manuf_name);
 	return true;
 }
-
-#ifdef CONFIG_BATTERY_TYPE_NO_AUTO_DETECT
-
-/* Variable to decide the battery type */
-static int fixed_battery_type = BATTERY_TYPE_UNINITIALIZED;
-
-/*
- * Function to get the fixed battery type.
- */
-static int battery_get_fixed_battery_type(void)
-{
-	if (fixed_battery_type == BATTERY_TYPE_UNINITIALIZED) {
-		CPRINTS("Warning: Battery type is not Initialized! "
-			"Setting to default battery type.\n");
-		fixed_battery_type = DEFAULT_BATTERY_TYPE;
-	}
-
-	return fixed_battery_type;
-}
-
-/*
- * Function to set the battery type, when auto-detection cannot be used.
- */
-void battery_set_fixed_battery_type(int type)
-{
-	if (type < BATTERY_TYPE_COUNT)
-		fixed_battery_type = type;
-}
-#endif /* CONFIG_BATTERY_TYPE_NO_AUTO_DETECT */
-
-/**
- * Allows us to override the battery in order to select the battery which has
- * the right configuration for the test.
- */
-test_export_static int battery_fuel_gauge_type_override = -1;
-
 /* Get type of the battery connected on the board */
-static int get_battery_type(void)
+static void detect_battery(void)
 {
 	char manuf_name[32];
 	int i;
 	static enum battery_type battery_type = BATTERY_TYPE_COUNT;
 
 	if (IS_ENABLED(TEST_BUILD) && battery_fuel_gauge_type_override >= 0) {
-		return battery_fuel_gauge_type_override;
+		batt_params =
+			&board_battery_info[battery_fuel_gauge_type_override];
+		return;
 	}
 
 	/*
@@ -108,11 +113,13 @@ static int get_battery_type(void)
 	 * as there is no need to query the fuel gauge.
 	 */
 	if (battery_type != BATTERY_TYPE_COUNT)
-		return battery_type;
+		return;
 
 	/* Get the manufacturer name. If can't read then just exit */
-	if (battery_manufacturer_name(manuf_name, sizeof(manuf_name)))
-		return battery_type;
+	if (battery_manufacturer_name(manuf_name, sizeof(manuf_name))) {
+		CPRINTS("Battery not found (no manuf name)");
+		return;
+	}
 
 #if defined(CONFIG_BATTERY_TYPE_NO_AUTO_DETECT)
 	i = battery_get_fixed_battery_type();
@@ -127,14 +134,16 @@ static int get_battery_type(void)
 	}
 #endif
 
-	return battery_type;
+	if (battery_type == BATTERY_TYPE_COUNT)
+		battery_type = board_get_default_battery_type();
+
+	batt_params = &board_battery_info[battery_type];
 }
 
 __overridable int board_get_default_battery_type(void)
 {
 	return DEFAULT_BATTERY_TYPE;
 }
-
 /*
  * Initialize the battery type for the board.
  *
@@ -143,19 +152,22 @@ __overridable int board_get_default_battery_type(void)
  */
 static void init_battery_type(void)
 {
-	if (get_battery_type() == BATTERY_TYPE_COUNT)
-		CPRINTS("battery not found");
+	detect_battery();
 }
 DECLARE_HOOK(HOOK_INIT, init_battery_type, HOOK_PRIO_POST_I2C);
 
 static inline const struct board_batt_params *get_batt_params(void)
 {
-	int type = get_battery_type();
+	detect_battery();
 
-	return &board_battery_info[type == BATTERY_TYPE_COUNT ?
-					   board_get_default_battery_type() :
-					   type];
+	return batt_params;
 }
+#else
+static inline struct board_batt_params *get_batt_params(void)
+{
+	return &default_battery_info;
+}
+#endif
 
 const struct battery_info *battery_get_info(void)
 {
@@ -215,33 +227,31 @@ static int cut_off_battery_sb_write(const struct ship_mode_info *ship_mode)
 
 int board_cut_off_battery(void)
 {
+	struct board_batt_params *params = get_batt_params();
 	int rv;
-	int type = get_battery_type();
 
-	/* If battery type is unknown can't send ship mode command */
-	if (type == BATTERY_TYPE_COUNT)
+	/* If battery is unknown can't send ship mode command */
+	if (!params)
 		return EC_RES_ERROR;
 
-	if (board_battery_info[type].fuel_gauge.ship_mode.wb_support)
-		rv = cut_off_battery_block_write(
-			&board_battery_info[type].fuel_gauge.ship_mode);
+	if (params->fuel_gauge.ship_mode.wb_support)
+		rv = cut_off_battery_block_write(&params->fuel_gauge.ship_mode);
 	else
-		rv = cut_off_battery_sb_write(
-			&board_battery_info[type].fuel_gauge.ship_mode);
+		rv = cut_off_battery_sb_write(&params->fuel_gauge.ship_mode);
 
 	return rv ? EC_RES_ERROR : EC_RES_SUCCESS;
 }
 
 enum ec_error_list battery_sleep_fuel_gauge(void)
 {
+	struct board_batt_params *params = get_batt_params();
 	const struct sleep_mode_info *sleep_command;
-	int type = get_battery_type();
 
 	/* Sleep entry command must be supplied as it will vary by gauge */
-	if (type == BATTERY_TYPE_COUNT)
+	if (!params)
 		return EC_ERROR_UNKNOWN;
 
-	sleep_command = &board_battery_info[type].fuel_gauge.sleep_mode;
+	sleep_command = &params->fuel_gauge.sleep_mode;
 
 	if (!sleep_command->sleep_supported)
 		return EC_ERROR_UNIMPLEMENTED;
@@ -249,17 +259,17 @@ enum ec_error_list battery_sleep_fuel_gauge(void)
 	return sb_write(sleep_command->reg_addr, sleep_command->reg_data);
 }
 
-static enum ec_error_list battery_get_fet_status_regval(int type, int *regval)
+static enum ec_error_list battery_get_fet_status_regval(int *regval)
 {
+	struct board_batt_params *params = get_batt_params();
 	int rv;
 	uint8_t data[6];
 
-	ASSERT(type < BATTERY_TYPE_COUNT);
+	ASSERT(params);
 
 	/* Read the status of charge/discharge FETs */
-	if (board_battery_info[type].fuel_gauge.fet.mfgacc_support == 1) {
-		if (board_battery_info[type].fuel_gauge.fet.mfgacc_smb_block ==
-		    1)
+	if (params->fuel_gauge.fet.mfgacc_support == 1) {
+		if (params->fuel_gauge.fet.mfgacc_smb_block == 1)
 			rv = sb_read_mfgacc_block(PARAM_OPERATION_STATUS,
 						  SB_ALT_MANUFACTURER_ACCESS,
 						  data, sizeof(data));
@@ -270,20 +280,19 @@ static enum ec_error_list battery_get_fet_status_regval(int type, int *regval)
 		/* Get the lowest 16bits of the OperationStatus() data */
 		*regval = data[2] | data[3] << 8;
 	} else
-		rv = sb_read(board_battery_info[type].fuel_gauge.fet.reg_addr,
-			     regval);
+		rv = sb_read(params->fuel_gauge.fet.reg_addr, regval);
 
 	return rv;
 }
 
 test_mockable int battery_is_charge_fet_disabled(void)
 {
+	struct board_batt_params *params = get_batt_params();
 	int rv;
 	int reg;
-	int type = get_battery_type();
 
 	/* If battery type is not known, can't check CHG/DCHG FETs */
-	if (type >= BATTERY_TYPE_COUNT) {
+	if (!params) {
 		/* Still don't know, so return here */
 		return -1;
 	}
@@ -291,15 +300,15 @@ test_mockable int battery_is_charge_fet_disabled(void)
 	/*
 	 * If the CFET mask hasn't been defined, assume that it's not disabled.
 	 */
-	if (!board_battery_info[type].fuel_gauge.fet.cfet_mask)
+	if (!params->fuel_gauge.fet.cfet_mask)
 		return 0;
 
-	rv = battery_get_fet_status_regval(type, &reg);
+	rv = battery_get_fet_status_regval(&reg);
 	if (rv)
 		return -1;
 
-	return (reg & board_battery_info[type].fuel_gauge.fet.cfet_mask) ==
-	       board_battery_info[type].fuel_gauge.fet.cfet_off_val;
+	return (reg & params->fuel_gauge.fet.cfet_mask) ==
+			params->fuel_gauge.fet.cfet_off_val;
 }
 
 /*
@@ -316,23 +325,23 @@ test_mockable int battery_is_charge_fet_disabled(void)
  */
 enum battery_disconnect_state battery_get_disconnect_state(void)
 {
+	struct board_batt_params *params = get_batt_params();
 	int reg;
-	int type = get_battery_type();
 
 	/* If battery type is not known, can't check CHG/DCHG FETs */
-	if (type >= BATTERY_TYPE_COUNT) {
+	if (!params) {
 		/* Still don't know, so return here */
 		return BATTERY_DISCONNECT_ERROR;
 	}
 
-	if (battery_get_fet_status_regval(type, &reg))
+	if (battery_get_fet_status_regval(&reg))
 		return BATTERY_DISCONNECT_ERROR;
 
-	if ((reg & board_battery_info[type].fuel_gauge.fet.reg_mask) ==
-	    board_battery_info[type].fuel_gauge.fet.disconnect_val) {
+	if ((reg & params->fuel_gauge.fet.reg_mask) ==
+	    params->fuel_gauge.fet.disconnect_val) {
 		CPRINTS("Batt disconnected: reg 0x%04x mask 0x%04x disc 0x%04x",
-			reg, board_battery_info[type].fuel_gauge.fet.reg_mask,
-			board_battery_info[type].fuel_gauge.fet.disconnect_val);
+			reg, params->fuel_gauge.fet.reg_mask,
+			params->fuel_gauge.fet.disconnect_val);
 		return BATTERY_DISCONNECTED;
 	}
 
@@ -342,15 +351,15 @@ enum battery_disconnect_state battery_get_disconnect_state(void)
 #ifdef CONFIG_BATTERY_MEASURE_IMBALANCE
 int battery_imbalance_mv(void)
 {
-	int type = get_battery_type();
+	struct board_batt_params *params = get_batt_params();
 
 	/*
 	 * If battery type is unknown, we cannot safely access non-standard
 	 * registers.
 	 */
-	return (type == BATTERY_TYPE_COUNT) ?
+	return (!params) ?
 		       0 :
-		       board_battery_info[type].fuel_gauge.imbalance_mv();
+		       batt_params->fuel_gauge.imbalance_mv();
 }
 
 int battery_default_imbalance_mv(void)
