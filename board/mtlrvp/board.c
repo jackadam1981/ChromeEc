7045cba22771b9060299c8624a388b171cb76299
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-05-16 16:58:52 -0700

    [Do not merge] mtlrvp: Add power seq code
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I7ccb56f7c968fe3d37a3c7d641a30fed5cccda78

--- board/mtlrvp/board.c
+++ board/mtlrvp/board.c
@@ -6,7 +6,35 @@
 /* Intel MTL-P-RVP board-specific configuration */
 
 #include "console.h"
+#include "gpio.h"
+#include "power/meteorlake.h"
+#include "power.h"
 #include "registers.h"
+
+/* PWROK signal configuration */
+/*
+ * On MTLRVP, SYS_PWROK_EC is an output controlled by EC and uses ALL_SYS_PWRGD
+ * as input.
+ */
+const struct intel_x86_pwrok_signal pwrok_signal_assert_list[] = {
+	{
+		.gpio = GPIO_SYS_PWROK_EC,
+		.delay_ms = 3,
+	},
+};
+const int pwrok_signal_assert_count = ARRAY_SIZE(pwrok_signal_assert_list);
+
+const struct intel_x86_pwrok_signal pwrok_signal_deassert_list[] = {
+	{
+		.gpio = GPIO_SYS_PWROK_EC,
+	},
+};
+const int pwrok_signal_deassert_count = ARRAY_SIZE(pwrok_signal_deassert_list);
+
+int extpower_is_present(void)
+{
+	return gpio_get_level(GPIO_BC_ACOK_EC);
+}
 
 static void fake_interrupt(enum gpio_signal signal)
 {
