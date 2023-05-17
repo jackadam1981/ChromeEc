107fec7fca748fba6ad323a8f89d1a024d7e7093
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2023-05-17 16:30:19 +0800

    starmie: turn off 5V before hibernated
    I add a Kconfig(CORSOLA_HIBERNATE_PRE_OFF_5V) to disable
    the 5V power and a short delay, then perform the common
    corsola hibernate.
    
    BUG=b:283037861
    TEST=1.not waking up immediately after hibernated
    2.zmake -D compare-builds kingler pass
    3.zmake -D compare-builds krabby  pass
    
    Change-Id: Ic56a77f930a404197c743dc16a643e448778be25

--- zephyr/program/corsola/src/hibernate.c
+++ zephyr/program/corsola/src/hibernate.c
@@ -18,5 +18,17 @@ __override void board_hibernate(void)
 
 __override void board_hibernate_late(void)
 {
+#ifdef CONFIG_CORSOLA_HIBERNATE_PRE_OFF_5V
+	/*
+	 * b:283037861
+	 * This CONFIG_CORSOLA_HIBERNATE_PRE_OFF_5V will
+	 * change hibernate behavior. Specifically, it will
+	 * disable pp5000_z2 power line and a short delay,
+	 * then perform the common corsola hibernate.
+	 */
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(en_pp5000_z2), 1);
+	/* It takes around 30ms to release the PP5000 capacitance. */
+	udelay(30 * MSEC);
+#endif
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_ulp), 1);
 }
