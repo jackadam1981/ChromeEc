04408dde3ced2988d11bf95dce60f3d82b4f32f7
Yi Chou <yich@google.com>
2023-05-17 14:47:34 +0800

    fpsensor: Separate the driver related state
    BUG=b:248508087
    TEST=make V=1 BOARD=bloonchipper -j
    TEST=./test/run_device_tests.py --board bloonchipper -t fpsensor_auth_crypto
         => PASS
    TEST=./test/run_device_tests.py --board dartmonkey -t fpsensor_auth_crypto
         => PASS
    
    Change-Id: Iec2a4a29cf1cb57148bb9afce869cf005a0d41fc

--- common/fpsensor/build.mk
+++ common/fpsensor/build.mk
@@ -19,6 +19,7 @@ _fpsensor_obj:=$(_fpsensor_dir)fpsensor.o
 _fpsensor_detect_strings_obj:=$(_fpsensor_dir)fpsensor_detect_strings.o
 _fpsensor_debug_obj:=$(_fpsensor_dir)fpsensor_debug.o
 _fpsensor_auth_crypto_obj:=$(_fpsensor_dir)fpsensor_auth_crypto.o
+_fpsensor_state_without_driver_info_obj:=$(_fpsensor_dir)fpsensor_state_without_driver_info.o
 
 $(out)/RW/$(_fpsensor_state_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_crypto_obj): CFLAGS+=$(fpsensor_CFLAGS)
@@ -26,9 +27,9 @@ $(out)/RW/$(_fpsensor_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_detect_strings_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_debug_obj): CFLAGS+=$(fpsensor_CFLAGS)
 $(out)/RW/$(_fpsensor_auth_crypto_obj): CFLAGS+=$(fpsensor_CFLAGS)
+$(out)/RW/$(_fpsensor_state_without_driver_info_obj): CFLAGS+=$(fpsensor_CFLAGS)
 
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_state_obj)
-all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_crypto_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_obj)
 all-obj-$(HAS_TASK_CONSOLE)+=$(_fpsensor_detect_strings_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_debug_obj)
@@ -36,9 +37,13 @@ all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_debug_obj)
 # If CONFIG_FINGERPRINT_MCU or HAS_TASK_FPSENSOR is "y".
 ifneq (,$(filter y,$(CONFIG_FINGERPRINT_MCU) $(HAS_TASK_FPSENSOR)))
 all-obj-y+=$(_fpsensor_auth_crypto_obj)
+all-obj-y+=$(_fpsensor_crypto_obj)
+all-obj-y+=$(_fpsensor_state_without_driver_info_obj)
 # Or we are building fpsensor related projects.
 else ifneq (,$(findstring fpsensor,$(PROJECT)))
 all-obj-y+=$(_fpsensor_auth_crypto_obj)
+all-obj-y+=$(_fpsensor_crypto_obj)
+all-obj-y+=$(_fpsensor_state_without_driver_info_obj)
 endif # CONFIG_FINGERPRINT_MCU or HAS_TASK_FPSENSOR or fpsensor projects.
 
 endif # CONFIG_FINGERPRINT_MCU or TEST_BUILD
