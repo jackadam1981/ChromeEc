daae1b9feb55b98775dec4b476781cedeccd6451
Caveh Jalali <caveh@chromium.org>
2023-05-17 19:11:51 -0700

    rex: Declare TCPC, PPC on USB3 DB is-alt
    Rex has multiple USB daughterboard options. It doesn't really make sense
    to have a default TCPC or PPC for one and use "is-alt" for the
    others. So, set up all TCPCs and PPCs as "is-alt" and select the
    appropriate one at runtime.
    
    BUG=b:267589042
    TEST=boots on rex
    
    Change-Id: I7bb524c66825c0dd1c36ae8137b260e5b78234ea

--- zephyr/program/rex/rex/src/usb_mux_config.c
+++ zephyr/program/rex/rex/src/usb_mux_config.c
@@ -53,6 +53,10 @@ static void setup_usb_db(void)
 		break;
 	case FW_USB_DB_USB3:
 		LOG_INF("USB DB: Setting USB3 mux");
+		TCPC_ENABLE_ALTERNATE_BY_NODELABEL(USBC_PORT_C1,
+						   tcpc_ps8815_port1);
+		PPC_ENABLE_ALTERNATE_BY_NODELABEL(USBC_PORT_C1,
+						  ppc_nx20p_port1);
 		break;
 	case FW_USB_DB_USB4_ANX7452:
 		LOG_INF("USB DB: Setting ANX7452 mux");
