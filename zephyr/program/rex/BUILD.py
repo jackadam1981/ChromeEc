4db8663a6804d0269a80127b0c551d943782e195
Jay Patel <jay2.patel@intel.com>
2023-05-22 13:54:00 -0700

    program/rex: enable ebuild for ISH variant of EC with USE flag
    This commit modifies BUILD.py script to enable ebuild using
    USE="ish_fw". "ISH_ENABLE" is the new flag introduced in program Kconfig
    file which can be used to disable the EC sensing capability and i2c.
    This approach eliminates the need to create a new variant,
    "rex-sans-sensors" here. Therefore, if EC is built with this flag,
    the EC image in /build/rex/firmware/<project> will not have any sensor
    functions.
    
    BUG=b'280329972
    TEST=ebuild tested using the USE='ish_fw' flag. The EC does not have
         sensor capabilities and does not support sensor commands.
    
    Change-Id: Ib688ae607e82ba48b12db872f5d4f72a387747b6
    Signed-off-by: Jay Patel <jay2.patel@intel.com>

--- zephyr/program/rex/BUILD.py
+++ zephyr/program/rex/BUILD.py
@@ -8,6 +8,7 @@
 def register_rex_project(
     project_name,
     kconfig_files=None,
+    dts_overlays=None,
 ):
     """Register a variant of Rex."""
     if kconfig_files is None:
@@ -18,31 +19,30 @@ def register_rex_project(
             here / project_name / "project.conf",
         ]
 
+    if dts_overlays is None:
+        dts_overlays=[
+            here / project_name / "project.overlay",
+        ]
     register_npcx_project(
         project_name=project_name,
         zephyr_board="npcx9m7f",
-        dts_overlays=[
-            here / project_name / "project.overlay",
-        ],
+        dts_overlays=dts_overlays,
         kconfig_files=kconfig_files,
         inherited_from=["rex"],
     )
 
-
 register_rex_project(
     project_name="rex",
-)
-
-register_rex_project(
-    project_name="rex-sans-sensors",
     kconfig_files=[
         # Common to all projects.
         here / "program.conf",
         # Parent project's config
         here / "rex" / "project.conf",
-        # Project-specific KConfig customization.
-        here / "rex-sans-sensors" / "project.conf",
-    ],
+    ] + [ here / "rex-sans-sensors" / "project.conf" ] if \
+	"CONFIG_ISH_ENABLE=y" in open(here / "program.conf").read() \
+	else None, dts_overlays=[ here / "rex-sans-sensors" / \
+	"project.overlay" ] if "CONFIG_ISH_ENABLE=y" in \
+	open(here / "program.conf").read() else None,
 )
 
 register_rex_project(
