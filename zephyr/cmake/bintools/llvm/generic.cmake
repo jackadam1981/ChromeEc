7f01f192b9c825e0c45af48a62f3327dd5fe0fd7
Patryk Duda <pdk@semihalf.com>
2023-05-23 16:43:45 +0200

    bintools/llvm: Set path to bintools manually
    CMake is looking for bintools by adding a suffix to compiler binary
    e.g for AR it would be armv7m-cros-eabi-clang-ar, which doesn't exist.
    
    This patch adds definitions of the CMAKE_C_COMPILER_tool-name
    
    BUG=b:239712345
    TEST=zmake build bloonchipper (with 'llvm' toolchain)
    
    Change-Id: I2769eef4ec120322a3d551cbe95cdb0a4116127b

--- zephyr/cmake/bintools/llvm/generic.cmake
+++ zephyr/cmake/bintools/llvm/generic.cmake
@@ -8,3 +8,13 @@ set(CMAKE_OBJCOPY    "/usr/bin/llvm-objcopy")
 set(CMAKE_OBJDUMP    "/usr/bin/llvm-objdump")
 set(CMAKE_RANLIB     "/usr/bin/llvm-ranlib")
 set(CMAKE_READELF    "/usr/bin/llvm-readelf")
+
+# CMake is looking for bintools by adding a suffix to compiler binary
+# e.g for AR it would be armv7m-cros-eabi-clang-ar, which doesn't exist.
+# Set bintools locations manually
+set(CMAKE_C_COMPILER_AR         "/usr/bin/llvm-ar")
+set(CMAKE_C_COMPILER_NM         "/usr/bin/llvm-nm")
+set(CMAKE_C_COMPILER_OBJCOPY    "/usr/bin/llvm-objcopy")
+set(CMAKE_C_COMPILER_OBJDUMP    "/usr/bin/llvm-objdump")
+set(CMAKE_C_COMPILER_RANLIB     "/usr/bin/llvm-ranlib")
+set(CMAKE_C_COMPILER_READELF    "/usr/bin/llvm-readelf")
