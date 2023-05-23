da2e282aa19c0e9e32acd61c94c73435e943b1f2
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-05-22 20:13:02 -0700

    [Do not merge]mtlrvp: Add I2C support
    BUG=none
    TEST=none
    
    Change-Id: Ided0cc893c5586fcbdeba0a62df1a6a14abcb834

--- board/mtlrvp/board.c
+++ board/mtlrvp/board.c
@@ -14,11 +14,62 @@
 #include "registers.h"
 #include "usb_mux.h"
 #include "ec_commands.h"
+#include "ioexpander.h"
+#include "driver/ioexpander/it8801.h"
 
 enum mtlrvp_typec_ports {
 	TYPE_C_PORT_0,
 	TYPE_C_PORT_1,
 };
+
+enum mtlrvp_i2c {
+#ifdef CONFIG_IO_EXPANDER
+	I2C_CHAN_IOEX,
+#endif
+	I2C_CHAN_TYPEC_AIC1,
+	I2C_CHAN_TYPEC_AIC2,
+	I2C_CHAN_COUNT
+};
+
+const struct i2c_port_t i2c_ports[] = {
+#ifdef CONFIG_IO_EXPANDER
+	[I2C_CHAN_IOEX] = {
+		.name = "ioex",
+		.port = I2C_PORT_KB_DISCRETE,
+		.kbps = 100,
+		.scl = GPIO_SMB_BS_CLK,
+		.sda = GPIO_SMB_BS_DATA,
+	},
+#endif
+	[I2C_CHAN_TYPEC_AIC1] = {
+		.name = "typec_aic1",
+		.port = I2C_PORT_TYPEC_AIC1,
+		.kbps = 400,
+		.scl = GPIO_USBC_TCPC_I2C_CLK_AIC1,
+		.sda = GPIO_USBC_TCPC_I2C_DATA_AIC1,
+	},
+	[I2C_CHAN_TYPEC_AIC2] = {
+		.name = "typec_aic2",
+		.port = I2C_PORT_TYPEC_AIC2,
+		.kbps = 400,
+		.scl = GPIO_USBC_TCPC_I2C_CLK_AIC2,
+		.sda = GPIO_USBC_TCPC_I2C_DATA_AIC2,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(i2c_ports) == I2C_CHAN_COUNT);
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+#ifdef CONFIG_IO_EXPANDER
+/* IO expander config */
+struct ioexpander_config_t ioex_config[] = {
+	[IOEX_KBD_GPIO] = {
+		.i2c_host_port = I2C_PORT_KB_DISCRETE,
+		.i2c_addr_flags = IT8801_I2C_ADDR2,
+		.drv = &it8801_ioexpander_drv,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(ioex_config) == CONFIG_IO_EXPANDER_PORT_COUNT);
+#endif
 
 /* PWROK signal configuration */
 /*
