4c7fca1d13175d58fb599981c118e13726178ef8
Bobby Casey <bobbycasey@google.com>
2023-05-23 21:05:03 -0400

    fpsensor: Create variables for common sizeof operations
    Simplify future commits by replacing various sizeof calls with variables.
    
    BUG=none
    TEST=./util/compare_build.sh -b fp => MATCH
    
    Change-Id: I6b31cff89e62b2eb53cfa7c049bfb63f211dc2c6

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -41,6 +41,10 @@ extern "C" {
 #error "fpsensor code should not be in RO image."
 #endif
 
+constexpr int fp_buffer_size = sizeof(fp_buffer);
+constexpr int fp_enc_buffer_size = sizeof(fp_enc_buffer);
+constexpr int fp_template_size = sizeof(fp_template[0]);
+
 /* Ready to encrypt a template. */
 static timestamp_t encryption_deadline;
 
@@ -426,7 +430,7 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		if (!is_raw_capture(sensor_mode))
 			offset += FP_SENSOR_IMAGE_OFFSET;
 
-		ret = validate_fp_buffer_offset(sizeof(fp_buffer), offset,
+		ret = validate_fp_buffer_offset(fp_buffer_size, offset,
 						size);
 		if (ret != EC_SUCCESS)
 			return EC_RES_INVALID_PARAM;
@@ -445,7 +449,7 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		return EC_RES_INVALID_PARAM;
 	if (fgr >= templ_valid)
 		return EC_RES_UNAVAILABLE;
-	ret = validate_fp_buffer_offset(sizeof(fp_enc_buffer), offset, size);
+	ret = validate_fp_buffer_offset(fp_enc_buffer_size, offset, size);
 	if (ret != EC_SUCCESS)
 		return EC_RES_INVALID_PARAM;
 
@@ -458,8 +462,8 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		uint8_t *encrypted_template = fp_enc_buffer + sizeof(*enc_info);
 		/* Positive match salt is after the template. */
 		uint8_t *positive_match_salt =
-			encrypted_template + sizeof(fp_template[0]);
-		size_t encrypted_blob_size = sizeof(fp_template[0]) +
+			encrypted_template + fp_template_size;
+		size_t encrypted_blob_size = fp_template_size +
 					     sizeof(fp_positive_match_salt[0]);
 
 		/* b/114160734: Not more than 1 encrypted message per second. */
@@ -467,7 +471,7 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 			return EC_RES_BUSY;
 		encryption_deadline.val = now.val + (1 * SECOND);
 
-		memset(fp_enc_buffer, 0, sizeof(fp_enc_buffer));
+		memset(fp_enc_buffer, 0, fp_enc_buffer_size);
 		/*
 		 * The beginning of the buffer contains nonce, encryption_salt
 		 * and tag.
@@ -506,7 +510,7 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		 * encrypted in-place.
 		 */
 		memcpy(encrypted_template, fp_template[fgr],
-		       sizeof(fp_template[0]));
+		       fp_template_size);
 		memcpy(positive_match_salt, fp_positive_match_salt[fgr],
 		       sizeof(fp_positive_match_salt[0]));
 
@@ -589,7 +593,7 @@ static enum ec_status fp_command_template(struct host_cmd_handler_args *args)
 	    size + offsetof(struct ec_params_fp_template, data))
 		return EC_RES_INVALID_PARAM;
 	enum ec_error_list ret =
-		validate_fp_buffer_offset(sizeof(fp_enc_buffer), offset, size);
+		validate_fp_buffer_offset(fp_enc_buffer_size, offset, size);
 	if (ret != EC_SUCCESS)
 		return EC_RES_INVALID_PARAM;
 
@@ -602,7 +606,7 @@ static enum ec_status fp_command_template(struct host_cmd_handler_args *args)
 		uint8_t *encrypted_template = fp_enc_buffer + sizeof(*enc_info);
 		/* Positive match salt is after the template. */
 		uint8_t *positive_match_salt =
-			encrypted_template + sizeof(fp_template[0]);
+			encrypted_template + fp_template_size;
 		size_t encrypted_blob_size;
 
 		/*
@@ -623,9 +627,9 @@ static enum ec_status fp_command_template(struct host_cmd_handler_args *args)
 		}
 
 		if (enc_info->struct_version <= 3) {
-			encrypted_blob_size = sizeof(fp_template[0]);
+			encrypted_blob_size = fp_template_size;
 		} else {
-			encrypted_blob_size = sizeof(fp_template[0]) +
+			encrypted_blob_size = fp_template_size +
 					      sizeof(fp_positive_match_salt[0]);
 		}
 
@@ -648,7 +652,7 @@ static enum ec_status fp_command_template(struct host_cmd_handler_args *args)
 			return EC_RES_UNAVAILABLE;
 		}
 		memcpy(fp_template[idx], encrypted_template,
-		       sizeof(fp_template[0]));
+		       fp_template_size);
 		if (template_needs_validation_value(enc_info)) {
 			CPRINTS("fgr%d: Generating positive match salt.", idx);
 			trng_init();
@@ -660,7 +664,7 @@ static enum ec_status fp_command_template(struct host_cmd_handler_args *args)
 				      sizeof(fp_positive_match_salt[0]))) {
 			CPRINTS("fgr%d: Trivial positive match salt.", idx);
 			OPENSSL_cleanse(fp_template[idx],
-					sizeof(fp_template[0]));
+					fp_template_size);
 			return EC_RES_INVALID_PARAM;
 		}
 		memcpy(fp_positive_match_salt[idx], positive_match_salt,
