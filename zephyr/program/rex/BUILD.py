60449578c90812129a945b6ed463cc22d9d1de46
Yuval Peress <peress@google.com>
2023-05-24 15:33:38 -0600

    rex: rename rex-sans-sensors to rex-ec-ish
    In order to align better with the rex variant, we need to rename the EC
    build.
    
    BUG=b:284198384
    TEST=zmake rex-ec-ish
    
    Change-Id: Ifb4ba990418c10c5d7c689bce7bb27cef1ebfab7

--- zephyr/program/rex/BUILD.py
+++ zephyr/program/rex/BUILD.py
@@ -34,14 +34,14 @@ register_rex_project(
 )
 
 register_rex_project(
-    project_name="rex-sans-sensors",
+    project_name="rex-ec-ish",
     kconfig_files=[
         # Common to all projects.
         here / "program.conf",
         # Parent project's config
         here / "rex" / "project.conf",
         # Project-specific KConfig customization.
-        here / "rex-sans-sensors" / "project.conf",
+        here / "rex-ec-ish" / "project.conf",
     ],
 )
 
