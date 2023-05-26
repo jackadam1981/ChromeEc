e822edad184da297f3b31d175823d111b7a56af1
Daisuke Nojiri <dnojiri@chromium.org>
2023-05-26 09:18:11 -0700

    BCIC: Handle legacy battery configuration
    This CL makes BCIC fall back to the legacy battery configuration method
    if a board chooses to do so. This allows new units of a released board
    to configure (new) batteries by BCIC while existing units will continue
    to configure batteries by the legacy method.
    
    BUG=b:283131235
    TEST=make run-battery_config
    
    Change-Id: Icced6023987f7a68ff4cf4f5993d9c0258427c77

--- include/battery_fuel_gauge.h
+++ include/battery_fuel_gauge.h
@@ -47,13 +47,13 @@ struct fet_info {
 };
 
 struct fuel_gauge_info {
-#ifdef CONFIG_BATTERY_CONFIG_IN_CBI
-	char manuf_name[32];
-	char device_name[32];
-#else
+	/*
+	 * These are used only for battery detection, thus, unused in BCIC.
+	 * Host can read them from CBI if needed.
+	 */
 	const char *manuf_name;
 	const char *device_name;
-#endif
+
 	uint8_t override_nil;
 	struct ship_mode_info ship_mode;
 	struct sleep_mode_info sleep_mode;
@@ -71,11 +71,8 @@ struct board_batt_params {
 };
 
 /* Forward declare board specific data used by common code */
-#ifdef CONFIG_BATTERY_CONFIG_IN_CBI
 extern struct board_batt_params default_battery_conf;
-#else
 extern const struct board_batt_params board_battery_info[];
-#endif
 extern const enum battery_type DEFAULT_BATTERY_TYPE;
 
 #ifdef CONFIG_BATTERY_MEASURE_IMBALANCE
@@ -108,6 +105,16 @@ void battery_set_fixed_battery_type(int type);
 __override_proto int board_get_default_battery_type(void);
 
 /**
+ * Detect a battery model.
+ */
+void init_battery_type(void);
+
+/**
+ * Return struct board_batt_params of the battery.
+ */
+const struct board_batt_params *get_batt_params(void);
+
+/**
  * Return 1 if CFET is disabled, 0 if enabled. -1 if an error was encountered.
  * If the CFET mask is not defined, it will return 0.
  */
@@ -120,14 +127,25 @@ int battery_is_charge_fet_disabled(void);
  */
 enum ec_error_list battery_sleep_fuel_gauge(void);
 
-#ifdef CONFIG_BATTERY_CONFIG_IN_CBI
 /**
- * Return struct board_batt_params of default battery.
+ * Return whether a board supports BCIC or not.
+ *
+ * If a board needs to support units without battery config in CBI and units
+ * with battery config in CBI, it needs to implement this callback so that BCIC
+ * can distinguish the two groups. This is needed because without this callback,
+ * BCIC can't tell battery config is missing because it's an old unit or because
+ * the default config is applicable.
+ *
+ *   bool board_batt_conf_supported(void)
+ *   {
+ *       if (BOARD_VERSION > 0)
+ *           return true;
+ *       else
+ *           return false;
+ *   }
+ *
+ * @return true if board supports BCIC or false otherwise.
  */
-inline struct board_batt_params *get_batt_params(void)
-{
-	return &default_battery_conf;
-}
-#endif
+__override_proto bool board_batt_conf_supported(void);
 
 #endif /* __CROS_EC_BATTERY_FUEL_GAUGE_H */
