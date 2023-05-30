60afa3812224e025ce4354a2eb84de9fe47e9204
Mary Ruthven <mruthven@chromium.org>
2023-05-30 14:41:23 -0700

    aprv: keep unsupported status for at least 10 seconds
    Keep UNSUPPORTED_TRIGGERED for at least 10 seconds. Even if the AP
    resets multiple times don't clear the status until at least 10 seconds
    has passed.
    
    BUG=b:260307815
    TEST=trigger verification on dedede. Check that it keeps the
    UNSUPPORTED_TRIGGERED status the first boot.
    
    Change-Id: If4b102b71012bca62a7d1040bca6380dc278feb5

--- common/ap_ro_integrity_check.c
+++ common/ap_ro_integrity_check.c
@@ -29,6 +29,8 @@
 
 /* Prevent releasing EC_RST_L for 60 seconds after AP RO verification fails */
 #define DELAY_EC_RST_RELEASE (60 * SECOND)
+/* Don't clear the UNSUPPORTED_TRIGGERED status for 10 seconds */
+#define UNSUPPORTED_GRACE_PERIOD_US (10 * SECOND)
 /* FMAP must be aligned at 4K or larger power of 2 boundary. */
 #define LOWEST_FMAP_ALIGNMENT  (4 * 1024)
 #define FMAP_SIGNATURE	       "__FMAP__"
@@ -262,6 +264,15 @@ static const struct ap_ro_check *p_chk =
  */
 static enum ap_ro_status apro_result = AP_RO_NOT_RUN;
 static uint8_t apro_fail_status_cleared;
+static timestamp_t grace_period;
+
+void update_unsupported_grace_period(void)
+{
+	timestamp_t tmp = get_time();
+
+	tmp.val += UNSUPPORTED_GRACE_PERIOD_US;
+	grace_period = tmp;
+}
 
 /* Clear validate_ap_ro_boot state. */
 void ap_ro_device_reset(void)
@@ -269,6 +280,10 @@ void ap_ro_device_reset(void)
 	if (apro_result == AP_RO_NOT_RUN || apro_result == AP_RO_IN_PROGRESS ||
 	    ec_rst_override())
 		return;
+	if (grace_period.val && !timestamp_expired(grace_period, 0)) {
+		CPRINTS("%s: ignored", __func__);
+		return;
+	}
 	CPRINTS("%s: clear apro result", __func__);
 	apro_fail_status_cleared = 0;
 	apro_result = AP_RO_NOT_RUN;
@@ -1148,6 +1163,7 @@ static uint8_t do_ap_ro_check(void)
 
 	apro_result = AP_RO_IN_PROGRESS;
 	apro_fail_status_cleared = 0;
+	grace_period.val = 0;
 
 	switch (ap_ro_check_unsupported(true)) {
 	case ARCVE_OK:
@@ -1156,6 +1172,7 @@ static uint8_t do_ap_ro_check(void)
 	case ARCVE_NOT_PROGRAMMED:
 	case ARCVE_BOARD_ID_BLOCKED:
 		CPRINTS("%s: unsupported", __func__);
+		update_unsupported_grace_period();
 		apro_result = AP_RO_UNSUPPORTED_TRIGGERED;
 		ap_ro_add_flash_event(APROF_CHECK_UNSUPPORTED);
 		return EC_ERROR_UNIMPLEMENTED;
