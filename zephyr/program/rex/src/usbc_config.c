c0d670263f25679a3ac954de70b0a9f98548c258
David Huang <david.huang@quanta.corp-partner.google.com>
2023-05-31 09:50:12 +0800

    Rex: PS8811 tuning
    Tuning PS8811- for compliance test.
    
    BUG=b:280027382
    TEST=Check PS8811 reg value correct.
    
    Change-Id: I89b543b99ceef4d7baf74b7d4f30523ece925629

--- zephyr/program/rex/src/usbc_config.c
+++ zephyr/program/rex/src/usbc_config.c
@@ -11,11 +11,13 @@
 #include "driver/charger/isl9241.h"
 #include "driver/ppc/nx20p348x.h"
 #include "driver/retimer/bb_retimer_public.h"
+#include "driver/retimer/ps8811.h"
 #include "driver/tcpm/nct38xx.h"
 #include "driver/tcpm/tcpci.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
 #include "i2c.h"
+#include "i2c/i2c.h"
 #include "ioexpander.h"
 #include "ppc/syv682x_public.h"
 #include "system.h"
@@ -23,6 +25,8 @@
 #include "usb_mux.h"
 #include "usbc_config.h"
 #include "usbc_ppc.h"
+
+#include <ap_power/ap_power.h>
 
 #include <stdbool.h>
 
@@ -216,3 +220,115 @@ int board_set_active_charge_port(int port)
 
 	return EC_SUCCESS;
 }
+
+struct ps8811_reg_val {
+	uint8_t reg;
+	uint16_t val;
+};
+
+const static struct ps8811_reg_val equalizer_table[] = {
+	{
+		/* Set channel B EQ setting */
+		.reg = PS8811_REG1_USB_BEQ_LEVEL,
+		.val = (PS8811_BEQ_I2C_LEVEL_UP_13DB
+			<< PS8811_BEQ_I2C_LEVEL_UP_SHIFT) |
+		       (PS8811_BEQ_PIN_LEVEL_UP_18DB
+			<< PS8811_BEQ_PIN_LEVEL_UP_SHIFT),
+	},
+	{
+		/* Set BDE pin setting */
+		.reg = PS8811_REG1_USB_BDE_CONFIG,
+		.val = (PS8811_BDE_PIN_MID_LEVEL_3DB
+			<< PS8811_BDE_PIN_MID_LEVEL_SHIFT) |
+		       PS8811_BEQ_CONFIG_REG_ENABLE |
+		       PS8811_BEQ_ADAPTIVE_REG_ENABLE,
+	},
+};
+
+#define NUM_EQ_WWAN_ARRAY ARRAY_SIZE(equalizer_table)
+
+/* USB-A ports */
+enum usba_port { USBA_PORT_A1, USBA_PORT_COUNT };
+const struct usb_mux usba_ps8811[] = {
+	[USBA_PORT_A1] = {
+		.usb_port = USBA_PORT_A1,
+		.i2c_port = I2C_PORT_NODELABEL(i2c6_1),
+		.i2c_addr_flags = PS8811_I2C_ADDR_FLAGS0,
+	},
+};
+
+static int usba_retimer_init(int port)
+{
+	int rv;
+	int val;
+	int i;
+	const struct usb_mux *me = &usba_ps8811[port];
+
+	rv = ps8811_i2c_read(me, PS8811_REG_PAGE1, PS8811_REG1_USB_BEQ_LEVEL,
+			     &val);
+
+	if (rv) {
+		CPRINTSUSB("A1: PS8811 retimer response fail! : %d", rv);
+	}
+	CPRINTSUSB("A1: PS8811 retimer detected");
+
+	if (chipset_in_state(CHIPSET_STATE_ON)) {
+		for (i = 0; i < NUM_EQ_WWAN_ARRAY; i++) {
+			rv |= ps8811_i2c_write(me, PS8811_REG_PAGE1,
+					       equalizer_table[i].reg,
+					       equalizer_table[i].val);
+		}
+
+		/* Set channel A output swing */
+		rv |= ps8811_i2c_field_update(me, PS8811_REG_PAGE1,
+					     PS8811_REG1_USB_CHAN_A_SWING,
+					     PS8811_CHAN_A_SWING_MASK,
+					     0x2 << PS8811_CHAN_A_SWING_SHIFT);
+	}
+	return rv;
+}
+
+extern void baseboard_a1_retimer_resetup(void);
+
+void baseboard_a1_retimer_setup(void)
+{
+	int i, rv;
+
+	for (i = 0; i < USBA_PORT_COUNT; ++i)
+		rv = usba_retimer_init(i);
+	if (rv != EC_SUCCESS)
+		baseboard_a1_retimer_resetup();
+}
+DECLARE_DEFERRED(baseboard_a1_retimer_setup);
+
+void baseboard_a1_retimer_resetup(void)
+{
+	hook_call_deferred(&baseboard_a1_retimer_setup_data, 3000 * MSEC);
+}
+
+static void board_resume_change(struct ap_power_ev_callback *cb,
+					    struct ap_power_ev_data data)
+{
+	switch (data.event) {
+	default:
+		return;
+
+	case AP_POWER_RESUME:
+		/* Any retimer tuning can be done after the retimer turns on */
+		hook_call_deferred(&baseboard_a1_retimer_setup_data,
+						30000 * MSEC);
+		break;
+	}
+}
+
+
+static int board_resume_change_init(void)
+{
+	static struct ap_power_ev_callback cb = {
+		.handler = board_resume_change,
+		.events = AP_POWER_RESUME,
+	};
+	ap_power_ev_add_callback(&cb);
+	return 0;
+}
+SYS_INIT(board_resume_change_init, APPLICATION, 0);
