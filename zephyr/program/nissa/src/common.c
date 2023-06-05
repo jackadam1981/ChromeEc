9e8487bce15db1715dd6429727b80c289f2bcbd5
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2023-06-05 14:41:10 +0800

    nissa: Overridable board_power_change
    Set board_power_change to overridable to let board to override it.
    
    BUG=none
    TEST=build
    
    Change-Id: Idb7843f567887a8a4f5a6dd3137294bee11c3db2

--- zephyr/program/nissa/src/common.c
+++ zephyr/program/nissa/src/common.c
@@ -8,6 +8,7 @@
 #include "charger.h"
 #include "chipset.h"
 #include "hooks.h"
+#include "nissa_common.h"
 #include "system.h"
 #include "usb_mux.h"
 
@@ -17,7 +18,7 @@
 #include <ap_power/ap_power.h>
 LOG_MODULE_REGISTER(nissa, CONFIG_NISSA_LOG_LEVEL);
 
-static void board_power_change(struct ap_power_ev_callback *cb,
+__overridable void board_power_change(struct ap_power_ev_callback *cb,
 			       struct ap_power_ev_data data)
 {
 	/*
