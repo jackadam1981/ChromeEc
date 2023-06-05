bf9a3e12ffc20bc5b8787f9f0aa50a1d7c3681d1
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2023-06-05 14:41:10 +0800

    nissa: Move board power change to board level
    BUG=none
    TEST=build
    
    Change-Id: Idb7843f567887a8a4f5a6dd3137294bee11c3db2

--- zephyr/program/nissa/pujjo/src/form_factor.c
+++ zephyr/program/nissa/pujjo/src/form_factor.c
@@ -20,6 +20,8 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/logging/log.h>
 
+#include <ap_power/ap_power.h>
+
 LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
 
 static bool use_alt_sensor;
@@ -75,3 +77,35 @@ static void sensor_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_INIT, sensor_init, HOOK_PRIO_POST_I2C);
+
+static void board_power_change(struct ap_power_ev_callback *cb,
+			       struct ap_power_ev_data data)
+{
+	/*
+	 * Enable power to pen garage when system is active (safe even if no
+	 * pen is present).
+	 */
+	const struct gpio_dt_spec *const pen_power_gpio =
+		GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_pen_x);
+
+	switch (data.event) {
+	case AP_POWER_STARTUP:
+		gpio_pin_set_dt(pen_power_gpio, 1);
+		break;
+	case AP_POWER_SHUTDOWN:
+		gpio_pin_set_dt(pen_power_gpio, 0);
+		break;
+	default:
+		break;
+	}
+}
+
+static void board_setup_init(void)
+{
+	static struct ap_power_ev_callback cb;
+
+	ap_power_ev_init_callback(&cb, board_power_change,
+				  AP_POWER_STARTUP | AP_POWER_SHUTDOWN);
+	ap_power_ev_add_callback(&cb);
+}
+DECLARE_HOOK(HOOK_INIT, board_setup_init, HOOK_PRIO_INIT_I2C);
