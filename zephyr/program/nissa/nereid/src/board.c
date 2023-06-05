70f97c7e61e4ec60094dc519a01aaa764ebb48a7
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2023-06-05 14:41:10 +0800

    nissa: Move board power change to board level
    BUG=none
    TEST=build
    
    Change-Id: Idb7843f567887a8a4f5a6dd3137294bee11c3db2

--- zephyr/program/nissa/nereid/src/board.c
+++ zephyr/program/nissa/nereid/src/board.c
@@ -1,38 +1,13 @@
-/* Copyright 2022 The ChromiumOS Authors
+/* Copyright 2023 The ChromiumOS Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-#include "gpio/gpio_int.h"
 #include "hooks.h"
-#include "task.h"
 
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/pinctrl.h>
-#include <zephyr/init.h>
-#include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+#include <zephyr/devicetree.h>
 
 #include <ap_power/ap_power.h>
-
-LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
-
-__override uint8_t board_get_usb_pd_port_count(void)
-{
-	return 2;
-}
-/*
- * Enable interrupts
- */
-static void board_init(void)
-{
-	/*
-	 * Enable USB-C interrupts.
-	 */
-	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0));
-	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c1));
-}
-DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
 
 static void board_power_change(struct ap_power_ev_callback *cb,
 			       struct ap_power_ev_data data)
