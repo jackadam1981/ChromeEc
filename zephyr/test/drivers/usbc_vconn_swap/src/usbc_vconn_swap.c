873366f03f1312d423782e969f1574fdc42dfadb
Jeremy Bettis <jbettis@google.com>
2023-06-07 15:08:39 -0600

    zephyr: Add testcase for a vconn swap that is late
    To cover the lines of code in pe_vdm_send_request_entry that get
    intermittently covered, add a new test case that disables vconn swap
    initially, but then allows it on the 2nd call.
    
    BUG=b:283475516
    TEST=./twister -v -i -c -C -s drivers.usbc_vconn_swap
    TEST=Coverage before 48102 / 93116
    
    Change-Id: Id05c280cf5fe9c9ee657609148dc6b5298ee3afb

--- zephyr/test/drivers/usbc_vconn_swap/src/usbc_vconn_swap.c
+++ zephyr/test/drivers/usbc_vconn_swap/src/usbc_vconn_swap.c
@@ -16,14 +16,35 @@
 #include "test/drivers/stubs.h"
 #include "test/drivers/test_state.h"
 #include "test/drivers/utils.h"
+#include "usb_tc_sm.h"
 
 #include <stdint.h>
 
 #include <zephyr/drivers/gpio/gpio_emul.h>
+#include <zephyr/fff.h>
 #include <zephyr/kernel.h>
 #include <zephyr/ztest.h>
 
 #define TEST_PORT 0
+
+FAKE_VALUE_FUNC(bool, port_discovery_vconn_swap_policy, int, bool);
+
+static bool port_discovery_vconn_swap_policy_custom(int port,
+						    bool vconn_swap_flag)
+{
+	if (IS_ENABLED(CONFIG_USBC_VCONN) && vconn_swap_flag &&
+	    !tc_is_vconn_src(port) && tc_check_vconn_swap(port))
+		return true;
+
+	/* Do not perform a VCONN swap */
+	return false;
+}
+
+static bool port_discovery_vconn_swap_policy_nope(int port,
+						  bool vconn_swap_flag)
+{
+	return false;
+}
 
 struct common_fixture {
 	const struct emul *tcpci_emul;
@@ -35,6 +56,10 @@ struct common_fixture {
 };
 
 struct usbc_vconn_swap_fixture {
+	struct common_fixture common;
+};
+
+struct usbc_vconn_swap_late_fixture {
 	struct common_fixture common;
 };
 
@@ -123,12 +148,33 @@ static void usbc_vconn_swap_before(void *data)
 {
 	struct usbc_vconn_swap_fixture *outer = data;
 
+	RESET_FAKE(port_discovery_vconn_swap_policy);
+	port_discovery_vconn_swap_policy_fake.custom_fake =
+		port_discovery_vconn_swap_policy_custom;
+
+	common_before(&outer->common);
+}
+
+static void usbc_vconn_swap_late_before(void *data)
+{
+	struct usbc_vconn_swap_fixture *outer = data;
+
+	RESET_FAKE(port_discovery_vconn_swap_policy);
+	static bool (*custom_fakes[])(
+		int, bool) = { port_discovery_vconn_swap_policy_nope,
+			       port_discovery_vconn_swap_policy_custom };
+	SET_CUSTOM_FAKE_SEQ(port_discovery_vconn_swap_policy, custom_fakes, 3);
+
 	common_before(&outer->common);
 }
 
 static void usbc_vconn_swap_not_supported_before(void *data)
 {
 	struct usbc_vconn_swap_not_supported_fixture *outer = data;
+
+	RESET_FAKE(port_discovery_vconn_swap_policy);
+	port_discovery_vconn_swap_policy_fake.custom_fake =
+		port_discovery_vconn_swap_policy_custom;
 
 	common_before(&outer->common);
 }
@@ -194,3 +240,21 @@ ZTEST_SUITE(usbc_vconn_swap_not_supported, drivers_predicate_post_main,
 	    usbc_vconn_swap_not_supported_setup,
 	    usbc_vconn_swap_not_supported_before,
 	    usbc_vconn_swap_not_supported_after, NULL);
+
+/** Tests if the board's port_discovery_vconn_swap_policy doesn't allow a
+ *  vconn immediately, that it will still work eventually. See fake
+ *  setup in usbc_vconn_swap_late_before().
+ */
+ZTEST_F(usbc_vconn_swap_late, test_vconn_swap_before_discovery)
+{
+	struct ec_response_typec_status status;
+
+	status = host_cmd_typec_status(TEST_PORT);
+
+	zassert_equal(status.vconn_role, PD_ROLE_VCONN_SRC,
+		      "TCPM did not initiate VCONN Swap after attach");
+}
+
+ZTEST_SUITE(usbc_vconn_swap_late, drivers_predicate_post_main,
+	    usbc_vconn_swap_setup, usbc_vconn_swap_late_before,
+	    usbc_vconn_swap_after, NULL);
