12e76c028db89decab30a49d52bf9ede86ca5bc7
Brian Granaghan <granaghan@google.com>
2023-06-08 20:29:36 +0000

    gsctool: Add command to get ti50 metrics.
    BUG=b:282781925
    TEST=localhost ~ # /usr/sbin/gsctool -aWM
    00000003000014E200000000100000000000000100000002
    localhost ~ # /usr/sbin/gsctool -aW
    version:               1
    fs_init_time:          3
    fs_usage:              5346
    aprov_time:            0
    expanded_aprov_status: 10000000
    rdd_keepalive:         2
    ccd_mode_l:            0
    
    Change-Id: I986f118a2ba87f849d60849883d45913d7c5c475

--- include/tpm_vendor_cmds.h
+++ include/tpm_vendor_cmds.h
@@ -339,6 +339,32 @@ struct vendor_cc_spi_hash_request {
 	uint32_t size;		/* Size in bytes to hash/read */
 } __packed;
 
+struct ti50_stats {
+	/* filesystem initialization time in ms */
+	uint32_t fs_init_time;
+	/* filesustem usage in bytes */
+	uint32_t fs_usage;
+	/* AP RO verification time in ms */
+	uint32_t aprov_time;
+	/* combination of AP RO verification result and failure reason, used by
+	 * UMA
+	 */
+	uint32_t expanded_aprov_status;
+	/* struct version number */
+	uint32_t version;
+	/* [31:27] - Version
+	 * [27: 3] - unused
+	 * [ 2: 2] - CCD_MODE_L
+	 * [ 1: 0] - rdd keep alive state
+	 */
+	uint32_t misc_status;
+};
+
+#define VERSION_SHIFT      27
+#define RDD_KEEP_ALIVE_MASK 3
+#define CCD_MODE_L_MASK     4
+#define CCD_MODE_L_SHIFT    2
+
 /* Maximum size of a response = SHA-256 hash or 1-32 bytes of data */
 #define SPI_HASH_MAX_RESPONSE_BYTES 32
 
