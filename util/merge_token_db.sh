cac2681c107e86c85d2afe4588e5fc16bf2ba9bf
Al Semjonovs <asemjonovs@google.com>
2023-06-14 11:31:26 -0600

    zephyr: Merge individual board token databases
    Add script to merge all token databases into single database.
    
    BUG=b:287264860
    
    Change-Id: I1ab38e07181a1e46ad8ab5a39c0bf77280359d75

---
+++ util/merge_token_db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+find build -name "database.bin" -print0 | xargs -0 \
+  python3 ../../third_party/pigweed/pw_tokenizer/py/pw_tokenizer/database.py \
+  create --type binary --force --database build/tokens.bin
