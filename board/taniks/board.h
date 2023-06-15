ed364ab00953e81e820fd646a38fcdadec0f8290
Gwendal Grignou <gwendal@chromium.org>
2023-06-15 16:27:02 -0700

    taniks: remove motion_sense subsytem
    Since taniks is a clamshell without MEMS sensors (accel, gyro), remove
    the motion_sense tasks and all the related sensors.
    
    BUG=b:231930177
    TEST=compile
    
    Fixes: commit 8f6fc6ce71fa9 ("taniks: remove unused define and code for clamshell system")
    Change-Id: I0b1211123764bd890c8f04e16a42c9139291ff80

--- board/taniks/board.h
+++ board/taniks/board.h
@@ -43,43 +43,6 @@
 /* LED */
 #define CONFIG_LED_ONOFF_STATES
 
-/* Sensors */
-#define CONFIG_DYNAMIC_MOTION_SENSOR_COUNT
-
-/* Change Request (b/211078551)
- * GYRO sensor change from ST LSM6DSOETR3TR to ST LSM6DS3TR-C
- *	LSM6DSOETR3TR base accel/gyro if board id = 0
- *	LSM6DS3TR-C Base accel/gyro if board id > 0
- */
-#define CONFIG_ACCELGYRO_LSM6DSO /* Base accel */
-#define CONFIG_ACCEL_LSM6DSO_INT_EVENT \
-	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
-#define CONFIG_ACCELGYRO_LSM6DSM
-#define CONFIG_ACCEL_LSM6DSM_INT_EVENT \
-	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
-
-/* Enable sensor fifo, must also define the _SIZE and _THRES */
-#define CONFIG_ACCEL_FIFO
-/* FIFO size is in power of 2. */
-#define CONFIG_ACCEL_FIFO_SIZE 256
-/* Depends on how fast the AP boots and typical ODRs */
-#define CONFIG_ACCEL_FIFO_THRES (CONFIG_ACCEL_FIFO_SIZE / 3)
-
-/* Sensors without hardware FIFO are in forced mode */
-#define CONFIG_ACCEL_FORCE_MODE_MASK (BIT(LID_ACCEL))
-
-/* Lid accel */
-#define CONFIG_LID_ANGLE
-#define CONFIG_LID_ANGLE_UPDATE
-#define CONFIG_LID_ANGLE_SENSOR_BASE BASE_ACCEL
-#define CONFIG_LID_ANGLE_SENSOR_LID LID_ACCEL
-#define CONFIG_ACCEL_BMA4XX
-#define CONFIG_ACCEL_LIS2DWL
-
-/* Sensor console commands */
-#define CONFIG_CMD_ACCELS
-#define CONFIG_CMD_ACCEL_INFO
-
 /* USB Type A Features */
 #define USB_PORT_COUNT 1
 #define CONFIG_USB_PORT_POWER_DUMB
