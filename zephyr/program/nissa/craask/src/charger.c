2e5f5d70130632e5f5330a6812ca3c4bd90d3636
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2023-06-16 14:04:05 +0800

    Craask: override board_can_leave_safe_mode
    For cosmx battery need about 2s to get stable, craask needs add
    board_can_leave_safe_mode to check if charge_manager could leave
    safe mode.
    
    Use DEFERRED function to count 2 seconds after
    gpio_ec_battery_pres_odl is low to make sure the battery is stable.
    
    BUG=b:287563811
    TEST=check leave safe mode timing of both cosmx and lgc battery.
    
    Change-Id: I68f0b5b025961083a41f50cf0808353bbaf46f5f

--- zephyr/program/nissa/craask/src/charger.c
+++ zephyr/program/nissa/craask/src/charger.c
@@ -4,10 +4,13 @@
  */
 
 #include "battery.h"
+#include "battery_fuel_gauge.h"
+#include "charge_manager.h"
 #include "charger.h"
 #include "charger/isl923x_public.h"
 #include "console.h"
 #include "extpower.h"
+#include "hooks.h"
 #include "usb_mux.h"
 #include "usb_pd.h"
 
@@ -53,4 +56,44 @@ __override void board_hibernate(void)
 	raa489000_hibernate(CHARGER_PRIMARY, true);
 	LOG_INF("Charger(s) hibernated");
 	cflush();
+}
+
+static bool board_batt_stable;
+
+static void board_batt_stable_deferred(void)
+{
+	board_batt_stable = true;
+	LOG_INF("batt is stable.");
+}
+DECLARE_DEFERRED(board_batt_stable_deferred);
+
+static void batt_stable_count_init(void)
+{
+	/*
+	 * It needs 2s for cosmx batt get stable after
+	 * gpio_ec_battery_pres_odl is low
+	 */
+	if (battery_is_present() == BP_YES) {
+		LOG_INF("wait batt statble count 2s");
+		hook_call_deferred(&board_batt_stable_deferred_data,
+				   (2 * SECOND));
+	}
+}
+DECLARE_HOOK(HOOK_INIT, batt_stable_count_init, HOOK_PRIO_INIT_CHARGE_MANAGER);
+
+__override bool board_can_leave_safe_mode(void)
+{
+	const struct fuel_gauge_info *const fuel_gauge =
+		&get_batt_params()->fuel_gauge;
+
+	/* If it's not COSMX battery, there's no need more delay time. */
+	if (strcasecmp(fuel_gauge->manuf_name, "COSMX KT0030B002") &&
+	    strcasecmp(fuel_gauge->manuf_name, "COSMX KT0030B004"))
+		return true;
+
+	/* cosmx battery might need about 2s to get stable. */
+	if (board_batt_stable)
+		return true;
+
+	return false;
 }
