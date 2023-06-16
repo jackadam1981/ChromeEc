a1319c6fbffd6f6d18bda35bfbb85a24d1375071
Vadim Sukhomlinov <sukhomlinov@google.com>
2023-06-16 13:50:49 -0700

    cr50: fix retrieve_nvmem_contents() total_var_size, cleanup
    1. Fix calculation of total_var_size in case of retries. It could be
       triple the size than expected.
    2. For testing added clean-up of fake nvmem content to `make clean`
    
    BUG=b:287674488
    TEST=`make run-nvmem`; verify that nvmem content is loaded
    
    Change-Id: I8a4a730a06cb30cbe5c66e1d23a0f9db4ec368c9
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>

--- common/new_nvmem.c
+++ common/new_nvmem.c
@@ -2265,7 +2265,7 @@ static enum ec_error_list verify_delimiter(struct nn_container *nc)
  */
 static enum ec_error_list retrieve_nvmem_contents(void)
 {
-	int rv;
+	enum ec_error_list rv;
 	int tries;
 	struct max_var_container *vc;
 	struct nn_container *nc;
@@ -2283,6 +2283,7 @@ static enum ec_error_list retrieve_nvmem_contents(void)
 		memset(nvmem_cache_base(NVMEM_TPM), 0,
 		       nvmem_user_sizes[NVMEM_TPM]);
 		memset(res_bitmap, 0, sizeof(res_bitmap));
+		total_var_space = 0;
 		next_evict_obj_base = 0;
 
 		while ((rv = get_next_object(&controller_at, nc, 0)) ==
