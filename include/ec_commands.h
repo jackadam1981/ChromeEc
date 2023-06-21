97cf55913efccebc6082578b29a021a35a58bc82
Reka Norman <rekanorman@google.com>
2023-06-21 16:44:47 +1000

    cec: Support multiple ports in cec_events
    Use the top four bits of cec_events to store the port.
    
    BUG=b:270507438
    TEST=`ectool cec write` still works on dibbi
    
    Change-Id: If236b7bf41f0f9964c894a67f4243cc454feacae

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -5128,6 +5128,16 @@ struct ec_response_i2c_passthru_protect {
 #define MAX_CEC_MSG_LEN 16
 
 /*
+ * Helper macros for packing/unpacking cec_events.
+ * bits[27:0] : bitmask of events from enum mkbp_cec_event
+ * bits[31:28]: port number
+ */
+#define EC_MKBP_EVENT_CEC_PACK(events, port) \
+	(((events)&GENMASK(27, 0)) | (((port)&0xf) << 28))
+#define EC_MKBP_EVENT_CEC_GET_EVENTS(event) ((event)&GENMASK(27, 0))
+#define EC_MKBP_EVENT_CEC_GET_PORT(event) (((event) >> 28) & 0xf)
+
+/*
  * Helper macros for packing/unpacking the cmd_port field of
  * ec_params_cec_set/get.
  */
