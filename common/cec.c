2e595ae6dcaa9540117caef30e073aac0d8e1dbd
Reka Norman <rekanorman@google.com>
2023-06-22 12:42:34 +1000

    cec: Support multiple ports in init function
    This completes support for multiple ports in the EC.
    
    BUG=b:270507438
    TEST=Use CEC to turn a TV on and off on dibbi
    
    Change-Id: I30b7632b7cfbfab7273c5451460b23e9527845d0

--- common/cec.c
+++ common/cec.c
@@ -23,10 +23,6 @@
 #define CPRINTS(...)
 #endif
 
-/* Only one port is supported for now */
-#define CEC_PORT 0
-BUILD_ASSERT(CEC_PORT_COUNT == 1);
-
 /*
  * Mutex for the read-offset of the rx queue. Needed since the
  * queue is read and flushed from different contexts
@@ -420,9 +416,11 @@ DECLARE_EVENT_SOURCE(EC_MKBP_EVENT_CEC_MESSAGE, cec_get_next_msg);
 
 static void cec_init(void)
 {
-	int port = CEC_PORT;
+	int port;
 
-	cec_config[port].drv->init(port);
+	for (port = 0; port < CEC_PORT_COUNT; port++) {
+		cec_config[port].drv->init(port);
+	}
 
 	CPRINTS("CEC initialized");
 }
