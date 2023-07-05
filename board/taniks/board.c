FrameworkComputer/EmbeddedController

commit 8f6fc6ce71fa98ef593c41c359f1d97d4183ea75

taniks: remove unused define and code for clamshell system
Remove unused define and code about tablet mode, because taniks is clamshell only.

BUG=b:231930177
BRANCH=none
TEST=make buildall -j
TEST=on taniks, run test_that -b brya "IP" power_WakeSources can get
pass result.

Signed-off-by: arthur.lin <arthur.lin@lcfc.corp-partner.google.com>
Change-Id: Iaed80bb621993eb3b054cfce6e054630439becb8
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3690035
Reviewed-by: Parth Malkan <parthmalkan@google.com>
Reviewed-by: Boris Mittelberg <bmbm@google.com>

board/taniks/board.c
@@ -60,17 +60,6 @@ __override void board_cbi_init(void)
 
 void board_init(void)
 {
-
-	if (ec_cfg_has_tabletmode()) {
-
-	} else {
-		/* only clamshell todo */
-		gpio_set_flags(GPIO_EC_VOLUP_BTN_ODL, GPIO_INPUT | GPIO_PULL_DOWN);
-		gpio_set_flags(GPIO_EC_VOLDN_BTN_ODL, GPIO_INPUT | GPIO_PULL_DOWN);
-		button_disable_gpio(BUTTON_VOLUME_UP);
-		button_disable_gpio(BUTTON_VOLUME_DOWN);
-	}
-
 #ifdef SECTION_IS_RW
 	rgbkbd_register_init_setting(&rgbkbd_init_taniks);
 #endif

board/taniks/board.h
@@ -25,6 +25,10 @@
 
 #undef CONFIG_CMD_POWERINDEBUG
 #undef CONFIG_MKBP_INPUT_DEVICES
+#undef CONFIG_TABLET_MODE
+#undef CONFIG_GMR_TABLET_MODE
+#undef CONFIG_TABLET_MODE_SWITCH
+#undef CONFIG_VOLUME_BUTTONS
 
 /*
  * This will happen automatically on NPCX9 ES2 and later. Do not remove
@@ -166,8 +170,6 @@
 #define GPIO_PG_EC_RSMRST_ODL		GPIO_SEQ_EC_RSMRST_ODL
 #define GPIO_POWER_BUTTON_L		GPIO_GSC_EC_PWR_BTN_ODL
 #define GPIO_SYS_RESET_L		GPIO_SYS_RST_ODL
-#define GPIO_VOLUME_DOWN_L		GPIO_EC_VOLDN_BTN_ODL
-#define GPIO_VOLUME_UP_L		GPIO_EC_VOLUP_BTN_ODL
 #define GPIO_WP_L			GPIO_EC_WP_ODL
 
 /* System has back-lit keyboard */

board/taniks/gpio.inc
@@ -9,10 +9,7 @@
 
 /* INTERRUPT GPIOs: */
 GPIO_INT(ACOK_OD,                        PIN(0, 0), GPIO_INT_BOTH | GPIO_HIB_WAKE_HIGH, extpower_interrupt)
-GPIO_INT(EC_IMU_INT_R_L,                 PIN(5, 6), GPIO_SEL_1P8V | GPIO_INT_FALLING, lsm6dso_interrupt)
 GPIO_INT(EC_PROCHOT_IN_L,                PIN(2, 4), GPIO_INT_BOTH, throttle_ap_prochot_input_interrupt)
-GPIO_INT(EC_VOLDN_BTN_ODL,               PIN(9, 3), GPIO_INT_BOTH | GPIO_PULL_UP, button_interrupt)
-GPIO_INT(EC_VOLUP_BTN_ODL,               PIN(9, 7), GPIO_INT_BOTH | GPIO_PULL_UP, button_interrupt)
 GPIO_INT(EC_WP_ODL,                      PIN(A, 1), GPIO_INT_BOTH, switch_interrupt)
 GPIO_INT(GSC_EC_PWR_BTN_ODL,             PIN(0, 1), GPIO_INT_BOTH | GPIO_HIB_WAKE_LOW, power_button_interrupt)
 GPIO_INT(LID_OPEN,                       PIN(D, 2), GPIO_INT_BOTH | GPIO_HIB_WAKE_HIGH, lid_interrupt)
@@ -22,7 +19,6 @@ GPIO_INT(SEQ_EC_RSMRST_ODL,              PIN(E, 2), GPIO_INT_BOTH, power_signal_
 GPIO_INT(SLP_S3_L,                       PIN(A, 5), GPIO_INT_BOTH, power_signal_interrupt)
 GPIO_INT(SLP_SUS_L,                      PIN(2, 5), GPIO_INT_BOTH, power_signal_interrupt)
 GPIO_INT(SYS_SLP_S0IX_L,                 PIN(D, 5), GPIO_INT_BOTH, power_signal_interrupt)
-GPIO_INT(TABLET_MODE_L,                  PIN(9, 5), GPIO_INT_BOTH, gmr_tablet_switch_isr)
 GPIO_INT(USB_C0_BC12_INT_ODL,            PIN(C, 6), GPIO_INT_FALLING, bc12_interrupt)
 GPIO_INT(USB_C0_TCPC_INT_ODL,            PIN(2, 3), GPIO_INT_FALLING, tcpc_alert_event)
 GPIO_INT(USB_C0_PPC_INT_ODL,             PIN(6, 2), GPIO_INT_FALLING, ppc_interrupt)
@@ -144,6 +140,10 @@ UNUSED(PIN(3, 2))       /* GPO32/TRIS_L */
 UNUSED(PIN(3, 5))       /* GPO35/CR_SOUT4/TEST_L */
 UNUSED(PIN(6, 6))       /* GPIO66 */
 UNUSED(PIN(5, 7))       /* GPIO57/SER_IRQ/ESPI_ALERT_L */
+UNUSED(PIN(9, 5))       /* GPIO95/SPIP_MISO */
+UNUSED(PIN(5, 6))       /* GPIO56/CLKRUN# */
+UNUSED(PIN(9, 3))       /* GPIO93/TA1/F_DIO2 */
+UNUSED(PIN(9, 7))       /* GPIO97 */
 
 /* Pre-configured PSL balls:  J8  K6 */
 

board/taniks/sensors.c
@@ -337,38 +337,8 @@ DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_detect_motionsensor,
 static void baseboard_sensors_init(void)
 {
 	CPRINTS("baseboard_sensors_init");
-	/* b/194765820
-	 * Dynamic motion sensor count
-	 * All board supports tablet mode if board id > 0
-	 */
-	if (ec_cfg_has_tabletmode()) {
-		/*
-		 * GPIO_EC_ACCEL_INT_R_L
-		 * The interrupt of lid accel is disabled by default.
-		 * We'll enable it later if lid accel is LIS2DW12.
-		 */
 
-		/* Change Request (b/199529373)
-		 * GYRO sensor change from ST LSM6DSOETR3TR to ST LSM6DS3TR-C
-		 *	LSM6DSOETR3TR base accel/gyro if board id = 0
-		 *	LSM6DS3TR-C Base accel/gyro if board id > 0
-		 */
-		if (get_board_id() > 0) {
-			motion_sensors[BASE_ACCEL] = lsm6dsm_base_accel;
-			motion_sensors[BASE_GYRO] = lsm6dsm_base_gyro;
-		}
-
-		/* Enable gpio interrupt for base accelgyro sensor */
-		gpio_enable_interrupt(GPIO_EC_IMU_INT_R_L);
-	} else {
-		CPRINTS("Clamshell");
-		motion_sensor_count = 0;
-		gmr_tablet_switch_disable();
-		gpio_set_flags(GPIO_TABLET_MODE_L, GPIO_INPUT | GPIO_PULL_DOWN);
-		/* Gyro is not present, don't allow line to float */
-		gpio_set_flags(GPIO_EC_IMU_INT_R_L, GPIO_INPUT |
-				GPIO_PULL_DOWN);
-	}
+	CPRINTS("Clamshell");
 }
 DECLARE_HOOK(HOOK_INIT, baseboard_sensors_init, HOOK_PRIO_INIT_I2C + 1);
 

jackadam1981/ChromeEc

commit fa6180cb0bc016cbf9f4c9706a2bebc8794ef413

taniks: remove motion_sense subsystem
Since taniks is a clamshell without MEMS sensors (accel, gyro), remove
the motion_sense tasks and all the related sensors.

BUG=b:231930177
TEST=Install on Taniks, check /var/log/cros_ec.log for errors related
to i2c or (non-temperature) sensors.

Fixes: commit 8f6fc6ce71fa9 ("taniks: remove unused define and code for clamshell system")
Change-Id: I0b1211123764bd890c8f04e16a42c9139291ff80

board/taniks/board.c
@@ -12,9 +12,6 @@
 #include "common.h"
 #include "compile_time_macros.h"
 #include "console.h"
-#include "driver/accel_lis2dw12.h"
-#include "driver/accelgyro_lsm6dso.h"
-#include "driver/als_tcs3400.h"
 #include "fw_config.h"
 #include "gpio.h"
 #include "gpio_signal.h"
@@ -26,9 +23,9 @@
 #include "registers.h"
 #include "rgb_keyboard.h"
 #include "switch.h"
-#include "tablet_mode.h"
 #include "throttle_ap.h"
 #include "usbc_config.h"
+#include "util.h"
 
 /* Must come after other header files and interrupt handler declarations */
 #include "gpio_list.h"

board/taniks/board.h
@@ -43,43 +43,6 @@
 /* LED */
 #define CONFIG_LED_ONOFF_STATES
 
-/* Sensors */
-#define CONFIG_DYNAMIC_MOTION_SENSOR_COUNT
-
-/* Change Request (b/211078551)
- * GYRO sensor change from ST LSM6DSOETR3TR to ST LSM6DS3TR-C
- *	LSM6DSOETR3TR base accel/gyro if board id = 0
- *	LSM6DS3TR-C Base accel/gyro if board id > 0
- */
-#define CONFIG_ACCELGYRO_LSM6DSO /* Base accel */
-#define CONFIG_ACCEL_LSM6DSO_INT_EVENT \
-	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
-#define CONFIG_ACCELGYRO_LSM6DSM
-#define CONFIG_ACCEL_LSM6DSM_INT_EVENT \
-	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
-
-/* Enable sensor fifo, must also define the _SIZE and _THRES */
-#define CONFIG_ACCEL_FIFO
-/* FIFO size is in power of 2. */
-#define CONFIG_ACCEL_FIFO_SIZE 256
-/* Depends on how fast the AP boots and typical ODRs */
-#define CONFIG_ACCEL_FIFO_THRES (CONFIG_ACCEL_FIFO_SIZE / 3)
-
-/* Sensors without hardware FIFO are in forced mode */
-#define CONFIG_ACCEL_FORCE_MODE_MASK (BIT(LID_ACCEL))
-
-/* Lid accel */
-#define CONFIG_LID_ANGLE
-#define CONFIG_LID_ANGLE_UPDATE
-#define CONFIG_LID_ANGLE_SENSOR_BASE BASE_ACCEL
-#define CONFIG_LID_ANGLE_SENSOR_LID LID_ACCEL
-#define CONFIG_ACCEL_BMA4XX
-#define CONFIG_ACCEL_LIS2DWL
-
-/* Sensor console commands */
-#define CONFIG_CMD_ACCELS
-#define CONFIG_CMD_ACCEL_INFO
-
 /* USB Type A Features */
 #define USB_PORT_COUNT 1
 #define CONFIG_USB_PORT_POWER_DUMB
@@ -179,7 +142,6 @@
 #define CONFIG_KEYBOARD_BACKLIGHT
 
 /* I2C Bus Configuration */
-#define I2C_PORT_SENSOR NPCX_I2C_PORT0_0
 #define I2C_PORT_USB_C0_TCPC NPCX_I2C_PORT1_0
 #define I2C_PORT_USB_C1_TCPC NPCX_I2C_PORT4_1
 #define I2C_PORT_USB_C0_PPC NPCX_I2C_PORT2_0
@@ -275,8 +237,6 @@ enum temp_sensor_id {
 	TEMP_SENSOR_4_CPUCHOKE,
 	TEMP_SENSOR_COUNT
 };
-
-enum sensor_id { LID_ACCEL = 0, BASE_ACCEL, BASE_GYRO, SENSOR_COUNT };
 
 enum ioex_port { IOEX_C0_NCT38XX = 0, IOEX_PORT_COUNT };
 

board/taniks/ec.tasklist
@@ -17,7 +17,6 @@
 	TASK_ALWAYS(USB_CHG_P0, usb_charger_task, 0, TASK_STACK_SIZE) \
 	TASK_ALWAYS(USB_CHG_P1, usb_charger_task, 0, TASK_STACK_SIZE) \
 	TASK_ALWAYS(CHARGER, charger_task, NULL, BASEBOARD_CHARGER_TASK_STACK_SIZE) \
-	TASK_ALWAYS(MOTIONSENSE, motion_sense_task, NULL, VENTI_TASK_STACK_SIZE) \
 	TASK_NOTEST(KEYPROTO, keyboard_protocol_task, NULL, LARGER_TASK_STACK_SIZE) \
 	TASK_NOTEST(CHIPSET, chipset_task, NULL, BASEBOARD_CHIPSET_TASK_STACK_SIZE) \
 	TASK_ALWAYS(USB_MUX, usb_mux_task, NULL, VENTI_TASK_STACK_SIZE) \

board/taniks/gpio.inc
@@ -52,8 +52,6 @@ GPIO(EC_I2C_BAT_SCL,                 PIN(3, 3), GPIO_INPUT)
 GPIO(EC_I2C_BAT_SDA,                 PIN(3, 6), GPIO_INPUT)
 GPIO(EC_I2C_MISC_SCL_R,              PIN(B, 3), GPIO_INPUT)
 GPIO(EC_I2C_MISC_SDA_R,              PIN(B, 2), GPIO_INPUT)
-GPIO(EC_I2C_SENSOR_SCL,              PIN(B, 5), GPIO_INPUT | GPIO_SEL_1P8V)
-GPIO(EC_I2C_SENSOR_SDA,              PIN(B, 4), GPIO_INPUT | GPIO_SEL_1P8V)
 GPIO(EC_I2C_USB_C0_PPC_BC_SCL,       PIN(9, 2), GPIO_INPUT)
 GPIO(EC_I2C_USB_C0_PPC_BC_SDA,       PIN(9, 1), GPIO_INPUT)
 GPIO(EC_I2C_KBMCU_SCL,               PIN(D, 1), GPIO_INPUT)
@@ -113,7 +111,6 @@ ALTERNATE(PIN_MASK(3, 0x48), 0, MODULE_I2C, 0)               /* GPIO33/I2C5_SCL0
 ALTERNATE(PIN_MASK(8, 0x80), 0, MODULE_I2C, 0)               /* GPIO87/I2C1_SDA0 */
 ALTERNATE(PIN_MASK(9, 0x07), 0, MODULE_I2C, 0)               /* GPIO92/I2C2_SCL0, GPIO91/I2C2_SDA0, GPIO90/I2C1_SCL0 */
 ALTERNATE(PIN_MASK(B, 0x0c), 0, MODULE_I2C, 0)               /* GPIOB3/I2C7_SCL0/DCD_L, GPIOB2/I2C7_SDA0/DSR_L */
-ALTERNATE(PIN_MASK(B, 0x30), 0, MODULE_I2C, GPIO_SEL_1P8V)   /* GPIOB5/I2C0_SCL0, GPIOB4/I2C0_SDA0 */
 ALTERNATE(PIN_MASK(D, 0x03), 0, MODULE_I2C, 0)               /* GPIOD1/I2C3_SCL0, GPIOD0/I2C3_SDA0 */
 ALTERNATE(PIN_MASK(E, 0x18), 0, MODULE_I2C, 0)               /* GPIOE4/I2C6_SCL1/I3C_SCL, GPIOE3/I2C6_SDA1/I3C_SDA */
 ALTERNATE(PIN_MASK(F, 0x0c), 0, MODULE_I2C, 0)               /* GPIOF3/I2C4_SCL1, GPIOF2/I2C4_SDA1 */
@@ -135,6 +132,8 @@ ALTERNATE(PIN_MASK(D, 0x04), 0, MODULE_PMU, GPIO_INT_BOTH | GPIO_HIB_WAKE_HIGH) 
 
 /* Unused Pins */
 UNUSED(PIN(B, 1))       /* KSO17/GPIOB1/CR_SIN4 */
+UNUSED(PIN(B, 4))       /* GPIOB4/I2C0_SDA0 */
+UNUSED(PIN(B, 5))       /* GPIOB5/I2C0_SCL0 */
 UNUSED(PIN(D, 6))       /* GPOD6/CR_SOUT3/SHDF_ESPI_L */
 UNUSED(PIN(3, 2))       /* GPO32/TRIS_L */
 UNUSED(PIN(3, 5))       /* GPO35/CR_SOUT4/TEST_L */

board/taniks/i2c.c
@@ -10,14 +10,6 @@
 /* I2C port map configuration */
 const struct i2c_port_t i2c_ports[] = {
 	{
-		/* I2C0 */
-		.name = "sensor",
-		.port = I2C_PORT_SENSOR,
-		.kbps = 400,
-		.scl = GPIO_EC_I2C_SENSOR_SCL,
-		.sda = GPIO_EC_I2C_SENSOR_SDA,
-	},
-	{
 		/* I2C1 */
 		.name = "tcpc0",
 		.port = I2C_PORT_USB_C0_TCPC,

board/taniks/sensors.c
@@ -3,20 +3,12 @@
  * found in the LICENSE file.
  */
 
-#include "accelgyro.h"
 #include "adc_chip.h"
 #include "common.h"
-#include "driver/accel_bma422.h"
-#include "driver/accel_bma4xx.h"
-#include "driver/accel_lis2dw12.h"
-#include "driver/accelgyro_lsm6dsm.h"
-#include "driver/accelgyro_lsm6dso.h"
 #include "fw_config.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "i2c.h"
-#include "motion_sense.h"
-#include "tablet_mode.h"
 #include "temp_sensor.h"
 #include "temp_sensor/thermistor.h"
 #include "thermal.h"
@@ -110,232 +102,6 @@ const struct adc_t adc_channels[] = {
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
-
-K_MUTEX_DEFINE(g_lid_accel_mutex);
-K_MUTEX_DEFINE(g_base_accel_mutex);
-static struct stprivate_data g_lis2dw12_data;
-static struct accelgyro_saved_data_t g_bma422_data;
-static struct lsm6dso_data lsm6dso_data;
-static struct lsm6dsm_data lsm6dsm_data = LSM6DSM_DATA;
-
-/* Matrix to rotate lid and base sensor into standard reference frame */
-static const mat33_fp_t lid_standard_ref = { { 0, FLOAT_TO_FP(1), 0 },
-					     { FLOAT_TO_FP(-1), 0, 0 },
-					     { 0, 0, FLOAT_TO_FP(1) } };
-
-static const mat33_fp_t base_standard_ref = { { FLOAT_TO_FP(-1), 0, 0 },
-					      { 0, FLOAT_TO_FP(1), 0 },
-					      { 0, 0, FLOAT_TO_FP(-1) } };
-
-struct motion_sensor_t bma422_lid_accel = {
-	.name = "Lid Accel - BMA",
-	.active_mask = SENSOR_ACTIVE_S0_S3,
-	.chip = MOTIONSENSE_CHIP_BMA422,
-	.type = MOTIONSENSE_TYPE_ACCEL,
-	.location = MOTIONSENSE_LOC_LID,
-	.drv = &bma4_accel_drv,
-	.mutex = &g_lid_accel_mutex,
-	.drv_data = &g_bma422_data,
-	.port = I2C_PORT_SENSOR,
-	.i2c_spi_addr_flags = BMA4_I2C_ADDR_PRIMARY, /* 0x18 */
-	.rot_standard_ref = &lid_standard_ref, /* identity matrix */
-	.default_range = 2, /* g, enough for laptop. */
-	.min_frequency = BMA4_ACCEL_MIN_FREQ,
-	.max_frequency = BMA4_ACCEL_MAX_FREQ,
-	.config = {
-		/* EC use accel for angle detection */
-		[SENSOR_CONFIG_EC_S0] = {
-			.odr = 12500 | ROUND_UP_FLAG,
-			.ec_rate = 100 * MSEC,
-		},
-		/* Sensor on in S3 */
-		[SENSOR_CONFIG_EC_S3] = {
-			.odr = 12500 | ROUND_UP_FLAG,
-			.ec_rate = 0,
-		},
-	},
-};
-
-struct motion_sensor_t lsm6dsm_base_accel = {
-	.name = "Base Accel",
-	.active_mask = SENSOR_ACTIVE_S0_S3,
-	.chip = MOTIONSENSE_CHIP_LSM6DSM,
-	.type = MOTIONSENSE_TYPE_ACCEL,
-	.location = MOTIONSENSE_LOC_BASE,
-	.drv = &lsm6dsm_drv,
-	.mutex = &g_base_accel_mutex,
-	.drv_data = LSM6DSM_ST_DATA(lsm6dsm_data,
-			MOTIONSENSE_TYPE_ACCEL),
-	.port = I2C_PORT_SENSOR,
-	.i2c_spi_addr_flags = LSM6DSM_ADDR0_FLAGS,
-	.rot_standard_ref = &base_standard_ref,
-	.default_range = 4,  /* g, to meet CDD 7.3.1/C-1-4 reqs */
-	.min_frequency = LSM6DSM_ODR_MIN_VAL,
-	.max_frequency = LSM6DSM_ODR_MAX_VAL,
-	.config = {
-		/* EC use accel for angle detection */
-		[SENSOR_CONFIG_EC_S0] = {
-			.odr = 13000 | ROUND_UP_FLAG,
-			.ec_rate = 100 * MSEC,
-		},
-		/* Sensor on for angle detection */
-		[SENSOR_CONFIG_EC_S3] = {
-			.odr = 10000 | ROUND_UP_FLAG,
-			.ec_rate = 100 * MSEC,
-		},
-	},
-};
-
-struct motion_sensor_t lsm6dsm_base_gyro = {
-	.name = "Base Gyro",
-	.active_mask = SENSOR_ACTIVE_S0_S3,
-	.chip = MOTIONSENSE_CHIP_LSM6DSM,
-	.type = MOTIONSENSE_TYPE_GYRO,
-	.location = MOTIONSENSE_LOC_BASE,
-	.drv = &lsm6dsm_drv,
-	.mutex = &g_base_accel_mutex,
-	.drv_data = LSM6DSM_ST_DATA(lsm6dsm_data, MOTIONSENSE_TYPE_GYRO),
-	.port = I2C_PORT_SENSOR,
-	.i2c_spi_addr_flags = LSM6DSM_ADDR0_FLAGS,
-	.default_range = 1000 | ROUND_UP_FLAG, /* dps */
-	.rot_standard_ref = &base_standard_ref,
-	.min_frequency = LSM6DSM_ODR_MIN_VAL,
-	.max_frequency = LSM6DSM_ODR_MAX_VAL,
-};
-
-struct motion_sensor_t motion_sensors[] = {
-	[LID_ACCEL] = {
-		.name = "Lid Accel - ST",
-		.active_mask = SENSOR_ACTIVE_S0_S3,
-		.chip = MOTIONSENSE_CHIP_LIS2DW12,
-		.type = MOTIONSENSE_TYPE_ACCEL,
-		.location = MOTIONSENSE_LOC_LID,
-		.drv = &lis2dw12_drv,
-		.mutex = &g_lid_accel_mutex,
-		.drv_data = &g_lis2dw12_data,
-		.port = I2C_PORT_SENSOR,
-		.i2c_spi_addr_flags = LIS2DW12_ADDR1, /* 0x19 */
-		.rot_standard_ref = &lid_standard_ref, /* identity matrix */
-		.default_range = 2, /* g */
-		.min_frequency = LIS2DW12_ODR_MIN_VAL,
-		.max_frequency = LIS2DW12_ODR_MAX_VAL,
-		.config = {
-			/* EC use accel for angle detection */
-			[SENSOR_CONFIG_EC_S0] = {
-				.odr = 12500 | ROUND_UP_FLAG,
-			},
-			/* Sensor on for lid angle detection */
-			[SENSOR_CONFIG_EC_S3] = {
-				.odr = 10000 | ROUND_UP_FLAG,
-			},
-		},
-	},
-
-	[BASE_ACCEL] = {
-		.name = "Base Accel",
-		.active_mask = SENSOR_ACTIVE_S0_S3,
-		.chip = MOTIONSENSE_CHIP_LSM6DSO,
-		.type = MOTIONSENSE_TYPE_ACCEL,
-		.location = MOTIONSENSE_LOC_BASE,
-		.drv = &lsm6dso_drv,
-		.mutex = &g_base_accel_mutex,
-		.drv_data = &lsm6dso_data,
-		.port = I2C_PORT_SENSOR,
-		.i2c_spi_addr_flags = LSM6DSO_ADDR0_FLAGS,
-		.rot_standard_ref = &base_standard_ref,
-		.default_range = 4,  /* g */
-		.min_frequency = LSM6DSO_ODR_MIN_VAL,
-		.max_frequency = LSM6DSO_ODR_MAX_VAL,
-		.config = {
-			[SENSOR_CONFIG_EC_S0] = {
-				.odr = 13000 | ROUND_UP_FLAG,
-				.ec_rate = 100 * MSEC,
-			},
-			[SENSOR_CONFIG_EC_S3] = {
-				.odr = 10000 | ROUND_UP_FLAG,
-				.ec_rate = 100 * MSEC,
-			},
-		},
-	},
-
-	[BASE_GYRO] = {
-		.name = "Base Gyro",
-		.active_mask = SENSOR_ACTIVE_S0_S3,
-		.chip = MOTIONSENSE_CHIP_LSM6DSO,
-		.type = MOTIONSENSE_TYPE_GYRO,
-		.location = MOTIONSENSE_LOC_BASE,
-		.drv = &lsm6dso_drv,
-		.mutex = &g_base_accel_mutex,
-		.drv_data = &lsm6dso_data,
-		.port = I2C_PORT_SENSOR,
-		.i2c_spi_addr_flags = LSM6DSO_ADDR0_FLAGS,
-		.default_range = 1000 | ROUND_UP_FLAG, /* dps */
-		.rot_standard_ref = &base_standard_ref,
-		.min_frequency = LSM6DSO_ODR_MIN_VAL,
-		.max_frequency = LSM6DSO_ODR_MAX_VAL,
-	},
-
-};
-
-#ifdef CONFIG_DYNAMIC_MOTION_SENSOR_COUNT
-unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
-#else
-const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
-#endif
-
-static void board_detect_motionsensor(void)
-{
-	int ret;
-	int val;
-
-	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
-		return;
-
-	/*
-	 * b/194765820 - Dynamic motion sensor count
-	 * All board supports tablet mode if board id > 0
-	 */
-	if (get_board_id() == 0 && !ec_cfg_has_tabletmode())
-		return;
-
-	/* Check lid accel chip */
-	ret = i2c_read8(I2C_PORT_SENSOR, LIS2DW12_ADDR1, LIS2DW12_WHO_AM_I_REG,
-			&val);
-	if (ret == 0 && val == LIS2DW12_WHO_AM_I) {
-		CPRINTS("LID_ACCEL is LIS2DW12");
-		return;
-	}
-
-	ret = i2c_read8(I2C_PORT_SENSOR, BMA4_I2C_ADDR_PRIMARY,
-			BMA4_CHIP_ID_ADDR, &val);
-	if (ret == 0 && val == BMA422_CHIP_ID) {
-		CPRINTS("LID_ACCEL is BMA422");
-		motion_sensors[LID_ACCEL] = bma422_lid_accel;
-		/*
-		 * The driver for BMA422 doesn't have code to support
-		 * INT1. So, it doesn't need to enable interrupt.
-		 * Vendor recommend to configure EC gpio as high-z if
-		 * we don't use INT1. Keep this pin as input w/o enable
-		 * interrupt.
-		 */
-		return;
-	}
-
-	/* Lid accel is not stuffed, don't allow line to float */
-	gpio_disable_interrupt(GPIO_EC_ACCEL_INT_R_L);
-	gpio_set_flags(GPIO_EC_ACCEL_INT_R_L, GPIO_INPUT | GPIO_PULL_DOWN);
-	CPRINTS("No LID_ACCEL are detected");
-}
-DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_detect_motionsensor,
-	     HOOK_PRIO_DEFAULT);
-
-static void baseboard_sensors_init(void)
-{
-	CPRINTS("baseboard_sensors_init");
-
-	CPRINTS("Clamshell");
-}
-DECLARE_HOOK(HOOK_INIT, baseboard_sensors_init, HOOK_PRIO_INIT_I2C + 1);
 
 /* Temperature sensor configuration */
 const struct temp_sensor_t temp_sensors[] = {