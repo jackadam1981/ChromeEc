8902297dd0437835ae961288ad8a65668688cb8b
Diana Z <dzigterman@chromium.org>
2023-07-05 10:22:40 -0600

    Zephyr test: Expand temp_sensor cases
    Add test cases for the console command and host command which are a part
    of common/temp_sensor.c
    
    BUG=None
    TEST=CQ+1
    
    Change-Id: I12996a426afcce8d43d783a01842e96f32a3bcb7

--- zephyr/test/drivers/default/src/temp_sensor.c
+++ zephyr/test/drivers/default/src/temp_sensor.c
@@ -4,6 +4,7 @@
  */
 
 #include "common.h"
+#include "console.h"
 #include "driver/temp_sensor/pct2075.h"
 #include "emul/emul_pct2075.h"
 #include "math_util.h"
@@ -20,6 +21,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/gpio/gpio_emul.h>
 #include <zephyr/kernel.h>
+#include <zephyr/shell/shell_dummy.h>
 #include <zephyr/ztest.h>
 
 #define GPIO_PG_EC_DSW_PWROK_PATH NAMED_GPIOS_GPIO_NODE(pg_ec_dsw_pwrok)
@@ -265,6 +267,90 @@ ZTEST_USER(temp_sensor, test_temp_sensor_pct2075_fail)
 	zassert_equal(MILLI_KELVIN_TO_KELVIN(mk2), temp);
 }
 
+/*
+ * Test we see reasonable prints from temperature sensors on the console
+ */
+ZTEST_USER(temp_sensor, test_temps_print_good)
+{
+	const char *buffer;
+	size_t buffer_size;
+
+	shell_backend_dummy_clear_output(get_ec_shell());
+
+	zassert_ok(shell_execute_cmd(get_ec_shell(), "temps"));
+
+	buffer = shell_backend_dummy_get_output(get_ec_shell(), &buffer_size);
+	/* Verify some small portion of expected output */
+	zassert_true(strstr(buffer, "K (= ") != NULL);
+}
+
+/*
+ * Test we see error returns for an unpowered sensor
+ */
+ZTEST_USER(temp_sensor, test_temps_print_unpowered)
+{
+	const char *buffer;
+	size_t buffer_size;
+	const struct device *gpio_dev =
+		DEVICE_DT_GET(DT_GPIO_CTLR(GPIO_EC_PG_PIN_TEMP_PATH, gpios));
+
+	zassert_not_null(gpio_dev, "Cannot get GPIO device");
+
+	/* ec_pg_pin_temp = 0 means temperature sensors are not powered. */
+	zassert_ok(gpio_emul_input_set(gpio_dev, GPIO_EC_PG_PIN_TEMP_PORT, 0),
+		   NULL);
+
+	shell_backend_dummy_clear_output(get_ec_shell());
+
+	zassert_equal(shell_execute_cmd(get_ec_shell(), "temps"),
+		      EC_ERROR_NOT_POWERED);
+
+	buffer = shell_backend_dummy_get_output(get_ec_shell(), &buffer_size);
+	zassert_true(strstr(buffer, "Not powered") != NULL);
+}
+
+/* Test that we report temp sensor info up to the AP if asked */
+ZTEST_USER(temp_sensor, test_temp_get_info_good)
+{
+	struct ec_params_temp_sensor_get_info params = {
+		.id = 0,
+	};
+	struct ec_response_temp_sensor_get_info response;
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND(
+		EC_CMD_TEMP_SENSOR_GET_INFO, 0, response, params);
+
+	zassert_ok(ec_cmd_temp_sensor_get_info(&args, &params, &response));
+	zassert_equal(response.sensor_type, TEMP_SENSOR_TYPE_BOARD);
+}
+
+ZTEST_USER(temp_sensor, test_temp_get_info_failure)
+{
+	struct ec_params_temp_sensor_get_info params = {
+		.id = TEMP_SENSOR_COUNT,
+	};
+	struct ec_response_temp_sensor_get_info response;
+	struct host_cmd_handler_args args = BUILD_HOST_COMMAND(
+		EC_CMD_TEMP_SENSOR_GET_INFO, 0, response, params);
+
+	zassert_equal(ec_cmd_temp_sensor_get_info(&args, &params, &response),
+		      EC_RES_ERROR);
+}
+
+static void temp_sensor_after(void *fixture)
+{
+	/* Clean up any PGOOD pin changes */
+	const struct device *dev =
+		DEVICE_DT_GET(DT_GPIO_CTLR(GPIO_PG_EC_DSW_PWROK_PATH, gpios));
+	const struct device *dev_pin =
+		DEVICE_DT_GET(DT_GPIO_CTLR(GPIO_EC_PG_PIN_TEMP_PATH, gpios));
+
+	zassert_not_null(dev, NULL);
+	zassert_ok(gpio_emul_input_set(dev, GPIO_PG_EC_DSW_PWROK_PORT, 1),
+		   NULL);
+	zassert_ok(gpio_emul_input_set(dev_pin, GPIO_EC_PG_PIN_TEMP_PORT, 1),
+		   NULL);
+}
+
 static void *temp_sensor_setup(void)
 {
 	const struct device *dev =
@@ -290,4 +376,4 @@ static void *temp_sensor_setup(void)
 }
 
 ZTEST_SUITE(temp_sensor, drivers_predicate_post_main, temp_sensor_setup, NULL,
-	    NULL, NULL);
+	    temp_sensor_after, NULL);
