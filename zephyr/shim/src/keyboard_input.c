2760be5d1225d040a67d691355c05ce69c4617eb
Fabio Baltieri <fabiobaltieri@google.com>
2023-07-07 15:05:49 +0000

    zephyr: add an input based keyboard driver
    Add a keyboard shim to use the internal EC protocols with the uptsream
    keyboard drivers.
    
    This is still very experimental, it lacks most config options available
    with the internal keyboard scan driver, but it works enough to unblock
    further work on the upstream driver.
    
    BUG=b:274443029
    TEST=zmake build rex -DCONFIG_INPUT=y \
         -DCONFIG_CROS_EC_KEYBOARD_INPUT=y \
         -DCONFIG_PLATFORM_EC_KEYBOARD_VIVALDI=n \
         -DCONFIG_INPUT_EVENT_DUMP=y
    
    Change-Id: I6cd8331d3cc3ef6edbbe52bf07ea406aa925debf

---
+++ zephyr/shim/src/keyboard_input.c
@@ -0,0 +1,43 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "keyboard_protocol.h"
+
+#include <stdio.h>
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/input/input.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(kbd_input, CONFIG_INPUT_LOG_LEVEL);
+
+#define CROS_EC_KEYBOARD_NODE DT_CHOSEN(cros_ec_keyboard)
+
+static void keyboard_input_cb(struct input_event *evt)
+{
+	static int row;
+	static int col;
+	static bool pressed;
+
+	switch (evt->code) {
+	case INPUT_ABS_X:
+		col = evt->value;
+		break;
+	case INPUT_ABS_Y:
+		row = evt->value;
+		break;
+	case INPUT_BTN_TOUCH:
+		pressed = evt->value;
+		break;
+	}
+
+	if (evt->sync) {
+		LOG_DBG("keyboard_state_changed %d %d %d", row, col, pressed);
+		keyboard_state_changed(row, col, pressed);
+	}
+}
+INPUT_LISTENER_CB_DEFINE(DEVICE_DT_GET(CROS_EC_KEYBOARD_NODE),
+			 keyboard_input_cb);
