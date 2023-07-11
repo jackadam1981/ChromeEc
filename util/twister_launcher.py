2daa52a02870abaa04420583d6e053abf9babc12
Aaron Massey <aaronmassey@google.com>
2023-07-11 15:55:53 -0600

    twister_launcher: Symlink to bazel gen twister-out
    Twister users are used to seeing a twister-out directory after running
    `./twister`.
    
    Have the twister_launcher bazel portion create a twister-out symlink to
    the bazel generated twister-out.
    
    BUG=b:268051194,b:268051194
    TEST=twister -T zephyr/test/system_shim/
    
    Change-Id: Ic473071b98349b35006fa1f59da1e079e96bcb5c

--- util/twister_launcher.py
+++ util/twister_launcher.py
@@ -357,6 +357,29 @@ twister_test_binary(
         run_dir.mkdir(parents=True)
         (run_dir / "BUILD.bazel").write_text(gen_starlark, encoding="utf-8")
 
+    # Twister users are used to seeing `twister-out`; symlink it to bazel twister-out
+    bazel_bin = subprocess.run(
+        ["bazel", "info", "bazel-bin"],
+        cwd=Path(__file__).parent,
+        check=True,
+        stdout=subprocess.PIPE,
+        encoding="utf-8",
+    ).stdout.strip()
+
+    ec_twister_out = Path("twister-out")
+    bazel_twister_out = (
+        bazel_bin
+        / pathlib.Path("platform/ec/build/twister-bzl")
+        / run_hash
+        / ec_twister_out
+    )
+    print(bazel_twister_out)
+    try:
+        os.remove(ec_twister_out)
+    except FileNotFoundError:
+        pass
+    ec_twister_out.symlink_to(bazel_twister_out)
+
     bazel_cmd = ["bazel", "build", ":run_twister"]
     if sandbox_debug:
         bazel_cmd.append("--sandbox_debug")
