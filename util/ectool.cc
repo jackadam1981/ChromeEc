6280f514aa555d497585c1f98ca63a015c20f1dc
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2023-07-13 09:56:17 +0800

    driver: add VCNL4200 Proximity Sensor chip driver
    add VCNL4200 Proximity Sensor chip driver
    
    BUG=none
    TEST=none
    
    Change-Id: Iacfede68164aae6e006a6cd95a9e6a87102da251

--- util/ectool.cc
+++ util/ectool.cc
@@ -5602,6 +5602,9 @@ static int cmd_motionsense(int argc, char **argv)
 			case MOTIONSENSE_CHIP_BMI220:
 				printf("bmi220\n");
 				break;
+			case MOTIONSENSE_CHIP_VCNL4200:
+				printf("vcnl4200\n");
+				break;
 			default:
 				printf("unknown\n");
 			}
