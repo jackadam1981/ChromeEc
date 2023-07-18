ac42c4db4a6c57a6a59bacfe00cf84dfd4caaab2
Jason Yuan <jasonyuan@google.com>
2023-07-18 13:22:06 -0700

    zephyr: move PPC interrupt handler to shim
    PPC interrupt is moved from project specific code to shim. PPC interrupt
    is hook initialized from devicetree instead of hook initialized.
    
    The program corsola is not supported by this driver because the PPC
    interrupt is shared with hdmi.
    
    BUG=b:254148652
    TEST=twister, tested PPC interrupt with loaded tester on villager
    
    Change-Id: I86d52b67a8e92cf41db4ef3a71492431a3c3bed6

--- zephyr/program/skyrim/src/usbc_config.c
+++ zephyr/program/skyrim/src/usbc_config.c
@@ -46,10 +46,6 @@ static void reset_nct38xx_port(int port);
 
 static void usbc_interrupt_init(void)
 {
-	/* Enable PPC interrupts. */
-	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0_ppc));
-	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c1_ppc));
-
 #ifdef CONFIG_PLATFORM_EC_USB_CHARGER
 	/* Enable BC 1.2 interrupts */
 	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0_bc12));
