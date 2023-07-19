502c9015aa02e485760dc0d6959d09a4b5aed358
matt_wang <matt_wang@compal.corp-partner.google.com>
2023-07-19 16:49:42 +0800

    anx7483: Add terminal register setting for AA1 chip
    Re-config AA1 chip terminal register for USB, DP, DOCK mode
    
    BUG=None
    TEST=zmake build uldren and zmake build craask success
    
    Change-Id: I76a331db7163c49b986f83a7c99c4203a44f9651

--- driver/retimer/anx7483.h
+++ driver/retimer/anx7483.h
@@ -16,6 +16,12 @@ struct anx7483_tuning_set {
 	uint8_t value;
 };
 
+#define ANX7483_AUX_CFG_1 0x01
+#define ANX7483_AUX_CFG_1_REPLY 0
+
+#define ANX7483_AUX_CFG_0 0x7B
+#define ANX7483_AUX_CFG_0_REPLY 0x28
+
 /*
  * LFPS_TIMER register
  *
@@ -147,6 +153,8 @@ struct anx7483_tuning_set {
 #define ANX7483_CFG3_90Ohm_IN 0x7A
 #define ANX7483_CFG3_90Ohm_OUT 0x7E
 
+#define ANX7483_BA_CFG3_90Ohm_IN 0x3A
+#define ANX7483_BA_CFG3_90Ohm_OUT 0x5E
 /*
  * AUX_Snooping_CTRL register
  *
@@ -156,6 +164,8 @@ struct anx7483_tuning_set {
  */
 #define ANX7483_AUX_SNOOPING_CTRL_REG 0x13
 
+#define ANX7483_VID 0x14
+#define ANX7483_BA 0xBA
 /*
  * Default value: Enable snooping with 90mVppd
  * (register ignored outside DP mode and does not need to be cleared)
