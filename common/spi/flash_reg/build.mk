2a3758fbb30d0923ff3dfb79d8468dbf66764549
Yuval Peress <peress@google.com>
2023-07-19 11:55:45 -0600

    build: Filter private directories from include path
    The union of testing in the spi/flash_reg caused an issue since
    all paths under common/ were automatically included. This was hiding
    a bug where common/spi/flash_reg/build.mk wasn't including the
    right directory, and the private directory was being included.
    
    BUG=b:291150225
    TEST=make BOARD=hatch print-includes
    
    Change-Id: I7ef03bb884b94a403902975b5b4e6297c12dc663

--- common/spi/flash_reg/build.mk
+++ common/spi/flash_reg/build.mk
@@ -6,6 +6,6 @@
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 REL_PATH := $(shell realpath --relative-to $(_common_dir) $(SELF_DIR))
 
-includes+=$(REL_PATH)public
+includes+=$(REL_PATH)/public
 common-$(CONFIG_SPI_FLASH)+=$(REL_PATH)/src/spi_flash_reg.o
 common-$(CONFIG_SPI_FLASH_REGS)+=$(REL_PATH)/src/spi_flash_reg.o
