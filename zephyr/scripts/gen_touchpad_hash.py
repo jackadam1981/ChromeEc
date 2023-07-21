5d998450250415442674fd1226db9b2bae19efd6
Ting Shen <phoenixshen@google.com>
2023-07-21 18:29:08 +0800

    zephyr: support touchpad firmware hash calculation
    This CL adds touchpad fw update related configs to the build system,
    and a python port of util/gen_touchpad_hash.c to generate touchpad
    firmware hash at build time.
    
    BUG=b:291869306
    TEST=Build roach with following configs:
           CONFIG_PLATFORM_EC_TOUCHPAD_HASH_FW=y
           CONFIG_PLATFORM_EC_TOUCHPAD_VIRTUAL_OFF=0
           CONFIG_PLATFORM_EC_TOUCHPAD_VIRTUAL_SIZE=57344
           CONFIG_PLATFORM_EC_UPDATE_PDU_SIZE=4096
         run `zmake build -DTOUCHPAD_FW=/path/to/fw roach`,
         manually check the generated touchpad_fw_hash.h
    TEST=Use the same config, run
         `zmake build -DTOUCHPAD_FW= roach` and
         `zmake build roach`
         should generate empty hash.
    TEST=`zmake compare-builds --all`
         (no change to other boards)
    
    Change-Id: Ie9e0fbd6644bba12e965e40e27494b8766a7e980

---
+++ zephyr/scripts/gen_touchpad_hash.py
@@ -0,0 +1,125 @@
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Touchpad firmware hash generator script."""
+
+import argparse
+import hashlib
+import os.path
+import pathlib
+import sys
+from typing import BinaryIO, TextIO
+
+
+def print_hash(out: TextIO, digest: [int], end: str = ",") -> None:
+    """Convert the digest blob to a C array literal."""
+
+    print("{ ", file=out, end="")
+    for val in digest:
+        print(f"{val:#04x}, ", file=out, end="")
+    print(f"}}{end}", file=out)
+
+
+def gen_header(
+    fw_file: BinaryIO,
+    out: TextIO,
+    touchpad_virtual_size: int,
+    update_pdu_size: int,
+):
+    """Generate the touchpad hash header using given parameters."""
+
+    digest_size = hashlib.sha256().digest_size
+    num_blocks = touchpad_virtual_size // update_pdu_size
+
+    print("#include <stdint.h>\n", file=out)
+
+    print(
+        f"const uint8_t touchpad_fw_hashes[{num_blocks}][{digest_size}] = {{",
+        file=out,
+    )
+
+    for _ in range(num_blocks):
+        if fw_file is not None:
+            block = fw_file.read(update_pdu_size)
+            print_hash(out, hashlib.sha256(block).digest())
+        else:
+            print_hash(out, [0] * digest_size)
+
+    print("};", file=out)
+    print(
+        f"const uint8_t touchpad_fw_full_hash[{digest_size}] =\n\t",
+        file=out,
+        end="",
+    )
+    if fw_file is not None:
+        fw_file.seek(0, 0)
+        print_hash(out, hashlib.sha256(fw_file.read()).digest(), ";")
+    else:
+        print_hash(out, [0] * digest_size, ";")
+
+
+def main() -> int:
+    """The main function."""
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-t",
+        "--touchpad_virtual_size",
+        type=int,
+        required=True,
+        help="Expected size of the touchpad firmware",
+    )
+    parser.add_argument(
+        "-u",
+        "--update_pdu_size",
+        type=int,
+        required=True,
+        help="PDU size for firmware update",
+    )
+    parser.add_argument(
+        "-f",
+        type=str,
+        dest="fw_path",
+        help="Firmware file. Will output blank hashes if not provided.",
+    )
+    parser.add_argument(
+        "-o",
+        type=str,
+        required=True,
+        dest="output_path",
+        help="Output file",
+    )
+
+    args = parser.parse_args()
+
+    pathlib.Path(args.output_path).parent.mkdir(parents=True, exist_ok=True)
+
+    with open(args.output_path, "w", encoding="ascii") as output:
+        if args.fw_path:
+            fw_size = os.path.getsize(args.fw_path)
+            expected_size = args.touchpad_virtual_size
+            if fw_size != expected_size:
+                print(
+                    f"Incorrect TP FW size ({fw_size} vs {expected_size})",
+                    file=sys.stderr,
+                )
+                return 1
+            with open(args.fw_path, "rb") as fw_file:
+                gen_header(
+                    fw_file,
+                    output,
+                    args.touchpad_virtual_size,
+                    args.update_pdu_size,
+                )
+        else:
+            gen_header(
+                None, output, args.touchpad_virtual_size, args.update_pdu_size
+            )
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
