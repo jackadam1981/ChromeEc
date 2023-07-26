24361cdb60e3e19d4c7acdea0430199be5449b2d
Li Feng <li1.feng@intel.com>
2023-07-26 14:50:11 -0700

    ish: pm: add system state handling
    BUG=b:281038970
    TEST=zmake build rex-ish, flash and run, system state messages sent from
    host are processed.
    
    Change-Id: I998d208099d23cde4d897461739e829a8ae07197

---
+++ zephyr/subsys/host_service/heci/heci_system_state.h
@@ -0,0 +1,7 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "heci_internal.h"
+void heci_handle_system_state_msg(uint8_t *msg, const size_t length);
