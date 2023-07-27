39651af2dd5d2f4fc9f37ff50216cb157e64b0e6
Kornel Dulęba <korneld@google.com>
2023-07-27 09:58:17 +0000

    WIP: PoC changes to whiterun for using Zephyr for thermal managment
    Change-Id: I65f6dd9b1b6c568d787a1380b991fa7bb380b161

---
+++ zephyr/shim/src/fan_zephyr.c
@@ -0,0 +1,72 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "console.h"
+#include "util.h"
+
+#include <zephyr/drivers/fan.h>
+#include <zephyr/logging/log.h>
+
+#define CPRINTS(format, args...) cprints(CC_THERMAL, format, ##args)
+
+struct fan {
+	const struct device *dev;
+};
+
+#define APPEND_FAN(i)	\
+	{ .dev = DEVICE_DT_GET(i) }, \
+
+static struct fan fans[]= {
+	DT_FOREACH_STATUS_OKAY(zephyr_fan_pid, APPEND_FAN)
+};
+
+int fan_get_count()
+{
+	return ARRAY_SIZE(fans);
+}
+
+int dptf_get_fan_duty_target()
+{
+	enum fan_mode mode;
+	int ret, duty;
+
+	/* TODO(crosbug.com/p/23803) */
+
+	if(fan_get_count() == 0)
+		return -1;
+
+	ret = fan_get_mode(fans[0].dev, &mode);
+	if (ret || mode != FAN_MODE_PWM)
+		return -1;
+
+	ret = fan_get_speed(fans[0].dev, FAN_SPEED_TYPE_PWM, &duty);
+	if (ret != 0)
+		return ret;
+
+	return duty;
+}
+
+void dptf_set_fan_duty_target(int duty)
+{
+	int ret, i;
+
+	/* TODO: Enable thermal control if duty is outside [0-100] */
+	if (duty < 0 || duty > 100)
+		return;
+
+	for (i = 0; i < fan_get_count(); i++) {
+		ret = fan_set_mode(fans[i].dev, FAN_MODE_PWM);
+		if (ret != 0)
+			return;
+
+		(void)fan_set_target(fans[i].dev, duty);
+	}
+}
+
+/* TODO */
+int fan_set_percent_needed(int ch, int pct)
+{
+	return 0;
+}
