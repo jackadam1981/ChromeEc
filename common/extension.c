218e12199b38e735e413be87b68fd1ce23fa32c7
Mary Ruthven <mruthven@chromium.org>
2023-07-28 13:56:10 -0500

    cr50: enable the FACTORY_CONFIG vendor commands over usb
    BUG=none
    TEST=none
    
    Change-Id: Idc93523d437849d55a2e2f1fc6f27d8c98a5fcfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4728912
    Auto-Submit: Mary Ruthven <mruthven@chromium.org>
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Commit-Queue: Andrey Pronin <apronin@chromium.org>
    (cherry picked from commit db59f86f9be9ec6310e1f13453daeaa7b8248116)

--- common/extension.c
+++ common/extension.c
@@ -39,8 +39,12 @@ uint32_t extension_route_command(struct vendor_cmd_params *p)
 		case VENDOR_CC_DS_DIS_TEMP:
 		case VENDOR_CC_USER_PRES:
 		case VENDOR_CC_WP:
+<<<<<<< HEAD   (a25ae3 cr50: prepare to release 0.{6,5}.190)
 		case VENDOR_CC_GET_FACTORY_CONFIG:
 		case VENDOR_CC_SET_FACTORY_CONFIG:
+=======
+		case VENDOR_CC_GET_CR50_METRICS:
+>>>>>>> CHANGE (db59f8 cr50: enable the FACTORY_CONFIG vendor commands over usb)
 #endif /* defined(CR50_DEV) */
 		case EXTENSION_POST_RESET: /* Always need to reset. */
 		case VENDOR_CC_CCD:
@@ -48,7 +52,9 @@ uint32_t extension_route_command(struct vendor_cmd_params *p)
 		case VENDOR_CC_GET_AP_RO_STATUS:
 		case VENDOR_CC_GET_BOARD_ID:
 		case VENDOR_CC_GET_BOOT_MODE:
+		case VENDOR_CC_GET_FACTORY_CONFIG:
 		case VENDOR_CC_RMA_CHALLENGE_RESPONSE:
+		case VENDOR_CC_SET_FACTORY_CONFIG:
 		case VENDOR_CC_SPI_HASH:   /* Requires physical presence. */
 		case VENDOR_CC_TURN_UPDATE_ON:
 			break;
