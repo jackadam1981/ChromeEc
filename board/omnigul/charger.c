b88fd4875ab96f9b3fd3162567b15c9866a973a2
Logan_Liao <Logan_Liao@compal.corp-partner.google.com>
2023-07-28 15:52:13 +0800

    nx20p348x: Workround solution for PPC damage.
    Base on Silicon vendor report, upload this patch for PPC damage
    workround.
    
    BUG=b:289989297
    TEST=make BOARD success
    
    Change-Id: I006166b5dea0e98cc04f28403d132dd53736dff3

--- board/omnigul/charger.c
+++ board/omnigul/charger.c
@@ -2,7 +2,6 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-
 #include "charge_manager.h"
 #include "charge_state.h"
 #include "charger.h"
@@ -10,12 +9,19 @@
 #include "compile_time_macros.h"
 #include "console.h"
 #include "driver/charger/isl9241.h"
+#include "driver/ppc/nx20p348x.h"
+#include "tcpm/tcpm.h"
 #include "usb_pd.h"
 #include "usbc_ppc.h"
 #include "util.h"
 
 #define CPRINTSUSB(format, args...) cprints(CC_USBCHARGE, format, ##args)
 #define CPRINTFUSB(format, args...) cprintf(CC_USBCHARGE, format, ##args)
+
+/* SM5360A Software workaround switch vchg*/
+#define NX20P348X_SWITCH_CONTROL_HIDDEN_REG 0x80
+/*SM5360A Software workaround control hidden register enable*/
+#define NX20P348X_VCHG_SWITCH_HIDDEN_REG 0x88
 
 #ifndef CONFIG_ZEPHYR
 /* Charger Chip Configuration */
@@ -29,10 +35,66 @@ const struct charger_config_t chg_chips[] = {
 BUILD_ASSERT(ARRAY_SIZE(chg_chips) == CHARGER_NUM);
 #endif
 
+static int write_reg(uint8_t port, int reg, int regval)
+{
+	return i2c_write8(ppc_chips[port].i2c_port,
+			  ppc_chips[port].i2c_addr_flags, reg, regval);
+}
+
+int board_set_ppc_vchg(int port, bool enable)
+{
+	int rv;
+	int en = enable ? 1 : 0;
+
+	if (enable) {
+		rv = tcpm_set_src_ctrl(port, en);
+		if (rv)
+			return rv;
+		msleep(1);
+		rv = write_reg(1, NX20P348X_SWITCH_CONTROL_REG, 0x00);
+		if (rv)
+			return rv;
+		msleep(1);
+		rv = write_reg(1, NX20P348X_VCHG_SWITCH_HIDDEN_REG, 0x00);
+		if (rv)
+			return rv;
+		msleep(1);
+		rv = write_reg(1, NX20P348X_SWITCH_CONTROL_HIDDEN_REG, 0x00);
+		if (rv)
+			return rv;
+	} else {
+		/*
+		 * SM5360A Software workaround solution.
+		 * Applied before Main Switch sink mode
+		 */
+		rv = write_reg(1, NX20P348X_SWITCH_CONTROL_REG, 0x80);
+		if (rv)
+			return rv;
+		msleep(1);
+		rv = tcpm_set_src_ctrl(1, 1);
+		if (rv)
+			return rv;
+		msleep(1);
+		rv = write_reg(1, NX20P348X_SWITCH_CONTROL_HIDDEN_REG, 0xEA);
+		if (rv)
+			return rv;
+		msleep(1);
+		rv = write_reg(1, NX20P348X_SWITCH_CONTROL_HIDDEN_REG, 0xAF);
+		if (rv)
+			return rv;
+		msleep(1);
+		rv = write_reg(1, NX20P348X_VCHG_SWITCH_HIDDEN_REG, 0x98);
+		if (rv)
+			return rv;
+	}
+	return EC_SUCCESS;
+}
+
 int board_set_active_charge_port(int port)
 {
 	int is_valid_port = board_is_usb_pd_port_present(port);
 	int i;
+	int rv;
 
 	if (port == CHARGE_PORT_NONE) {
 		CPRINTSUSB("Disabling all charger ports");
@@ -45,6 +107,12 @@ int board_set_active_charge_port(int port)
 			 */
 			if (ppc_vbus_sink_enable(i, 0))
 				CPRINTSUSB("Disabling C%d as sink failed.", i);
+			if (port == USBC_PORT_C0) {
+				rv = board_set_ppc_vchg(USBC_PORT_C1, 1);
+				if (rv)
+					CPRINTSUSB("set C%d ppc vchg failed.",
+						   i);
+			}
 		}
 
 		return EC_SUCCESS;
@@ -70,8 +138,21 @@ int board_set_active_charge_port(int port)
 
 		if (ppc_vbus_sink_enable(i, 0))
 			CPRINTSUSB("C%d: sink path disable failed.", i);
+		if (port == USBC_PORT_C0) {
+			rv = board_set_ppc_vchg(USBC_PORT_C1, 1);
+			if (rv)
+				CPRINTSUSB("set C%d ppc vchg failed.", i);
+		}
 	}
-
+	/*
+	 * SM5360A Software workaround solution.
+	 * Applied before Main Switch sink mode
+	 */
+	if (port == USBC_PORT_C0) {
+		rv = board_set_ppc_vchg(USBC_PORT_C1, 0);
+		if (rv)
+			CPRINTSUSB("set C%d ppc vchg failed.", i);
+	}
 	/* Enable requested charge port. */
 	if (ppc_vbus_sink_enable(port, 1)) {
 		CPRINTSUSB("C%d: sink path enable failed.", port);
