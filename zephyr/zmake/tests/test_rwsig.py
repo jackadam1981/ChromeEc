74879ad573af86289a856347e3d5083d1a372b76
Ting Shen <phoenixshen@google.com>
2023-07-31 19:15:29 +0800

    WIP: zmake: Move RWSIG signing step to output_packers
    Moves the signing step from makefile to the end of packer phase. RWSIG now
    signs the whole EC_RW instead of only the ZEPHYR_RW section.
    
    This change allows us to put arbitrary data (e.g. FWID or touchpad hash)
    in RW, and protect these data using RWSIG.
    
    BUG=b:291869306
    TEST=1) run_tests.sh
         2) zmake build roach
            futility verify build/zephyr/roach/output/ec.bin
    
    Change-Id: I2f463a0d9a1df5b122969d1239fe9fbf347b67d7

---
+++ zephyr/zmake/tests/test_rwsig.py
@@ -0,0 +1,44 @@
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Test rwsig signing."""
+
+import logging
+import pathlib
+import subprocess
+import tempfile
+import unittest.mock as mock
+
+import pytest  # pylint:disable=import-error
+import zmake.jobserver as jobserver
+import zmake.multiproc as multiproc
+import zmake.rwsig as rwsig
+
+
+FILES_DIR = pathlib.Path(__file__).parent / "files"
+
+
+def test_sign_firmware():
+    """Test the signing function."""
+
+    jobclient = jobserver.JobClient()
+    logger = mock.Mock(spec=logging.Logger)
+    multiproc.LogWriter.reset()
+
+    dts_path = FILES_DIR / "rwsig.dts"
+
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        temp_dir = pathlib.Path(temp_dir_name)
+
+        # Create a fake EC image and a RSA key.
+        subprocess.run(["binman", "build", "--dt", dts_path, "--outdir", temp_dir], check=True)
+        key_path = temp_dir / "key.pem"
+        subprocess.run(["openssl", "genrsa", "-3", "-out", key_path, "3072"],
+                       check=True)
+
+        # Sign the firmware.
+        signed_fw = rwsig.sign_fw(temp_dir / "ec.bin", key_path, temp_dir, jobclient, logger)
+
+        # Verify signature.
+        subprocess.run(["futility", "verify", signed_fw], check=True)
---
+++ zephyr/zmake/zmake/rwsig.py
@@ -0,0 +1,90 @@
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Classes """
+import logging
+import os.path
+from pathlib import Path
+import subprocess
+from typing import Dict, List, Union
+
+import zmake.build_config as build_config
+import zmake.jobserver
+import zmake.multiproc
+import zmake.util as util
+
+
+def sign_fw(
+        bin_file: Path,
+        key_path: Path,
+        work_dir: Path,
+        jobclient: zmake.jobserver.JobClient,
+        logger: logging.Logger,
+    ) -> Path:
+    """Sign a firmware image using rwsig algorithm.
+
+    Args:
+        bin_file: Path to the unsigned EC binary.
+        work_dir: A directory to write outputs and temporary files into.
+        jobclient: A JobClient object to use.
+        logger: The Logger object to log to.
+        key_path: Path to the PEM encoded key.
+
+    Returns:
+        Path to the signed firmware.
+    """
+    ec_rw = work_dir / "ec_rw"
+    pub_key = work_dir / "key.vbpubk2"
+    pri_key = work_dir / "key.vbprik2"
+    sig_file = work_dir / "ec.sig"
+    signed_bin = work_dir / "ec-signed.bin"
+
+    _run_futility(["dump_fmap", "-x", bin_file, f"EC_RW:{ec_rw}",
+                   f"SIG_RW:{sig_file}"],
+                  work_dir, jobclient, logger)
+    data_size = os.path.getsize(ec_rw) - os.path.getsize(sig_file)
+
+    _run_futility(["create", key_path, work_dir / "key"],
+                  work_dir, jobclient, logger)
+
+    _run_futility(["sign", "--type", "rwsig", "--data_size", str(data_size),
+                   "--prikey", pri_key, ec_rw, sig_file],
+                  work_dir, jobclient, logger)
+
+    _run_futility(["load_fmap", "-o", signed_bin, bin_file,
+                   f"KEY_RO:{pub_key}", f"SIG_RW:{sig_file}"],
+                  work_dir, jobclient, logger)
+
+    return signed_bin
+
+def _run_futility(args: List[Union[str, Path]],
+                  work_dir: Path,
+                  jobclient: zmake.jobserver.JobClient,
+                  logger: logging.Logger) -> None:
+    """Helper to execute futility command.
+
+    Args:
+        args:
+        work_dir: A directory to write outputs and temporary files into.
+        jobclient: A JobClient object to use.
+        logger: The Logger object to log to.
+    """
+    proc = jobclient.popen(
+        [util.get_tool_path("futility"), *args],
+        cwd=work_dir,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    zmake.multiproc.LogWriter.log_output(
+        logger, logging.DEBUG, proc.stdout
+    )
+    zmake.multiproc.LogWriter.log_output(
+        logger, logging.ERROR, proc.stderr
+    )
+    proc.wait(timeout=60)
+    if proc.returncode:
+        raise subprocess.CalledProcessError(
+                returncode=proc.returncode,
+                cmd=['futility', *args])
