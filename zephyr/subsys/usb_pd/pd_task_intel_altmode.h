62aa51229f7976172a3738b2177534b54c14628e
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-08-01 14:31:02 -0700

    PD: Add skeleton code to support PD Alt modes on Intel SOC
    Added skeleton PD task code to configure USB-C Alternate modes on
    Intel SoC. In this architecture, EC communicates with the PD and
    gather's necessary information to configure the virtual muxes and
    sends an eSPI event to Kernel to handle the TCSS flow.
    
    BUG=b:295917619
    TEST=Able to negotiate USB using CCG8-PD & MTL-RVP
    LOW_COVERAGE_REASON=Likely PMC to PD communication is POR, b:295924125
    
    Change-Id: Idcd8da94dcde9a57d02e56c20c1a045bd575a51a

---
+++ zephyr/subsys/usb_pd/pd_task_intel_altmode.h
@@ -0,0 +1,50 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/*
+ * Header file for PD task to configure USB-C Alternate modes on Intel SoC.
+ * Elaborate details can be found in respective SoC's "Platform Power
+ * Delivery Controller Interface for SoC and Retimer" document.
+ */
+
+#ifndef __CROS_EC_PD_TASK_INTEL_ALTMODE_H
+#define __CROS_EC_PD_TASK_INTEL_ALTMODE_H
+
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/kernel.h>
+
+#include <ap_power/ap_power.h>
+
+#define INTEL_ALTMODE_EVENT_MASK GENMASK(INTEL_ALTMODE_EVENT_COUNT - 1, 0)
+
+enum intel_altmode_event {
+	INTEL_ALTMODE_EVENT_FORCE,
+	INTEL_ALTMODE_EVENT_INTERRUPT,
+	INTEL_ALTMODE_EVENT_COUNT
+};
+
+struct pd_config_t {
+	/* I2C config */
+	struct i2c_dt_spec i2c;
+	/*
+	 * PD interrupt to wake the task to configure alternate modes. There
+	 * can be individual Interrupt pin for each PD port or all the PD
+	 * interrupts can be muxed to single GPIO. This helps to keep common
+	 * code for single port / dual port PD solutions offered by different
+	 * PD vendors.
+	 */
+	struct gpio_dt_spec irq_gpio;
+};
+
+struct intel_altmode_data {
+	/* Driver event object to receive events posted. */
+	struct k_event evt;
+	/* Callback for the AP power events */
+	struct ap_power_ev_callback cb;
+	/* PD data path I2C */
+	struct pd_config_t *pd_conf;
+};
+
+#endif /* __CROS_EC_PD_TASK_INTEL_ALTMODE_H */
