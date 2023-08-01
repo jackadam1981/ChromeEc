59731076f59c281a9d53abd232b69c0e78610df4
Al Semjonovs <asemjonovs@google.com>
2023-08-01 11:46:17 -0600

    zephyr: Picolibc integration with coreboot
    Enable picolibc to work with coreboot SDK
    
    BUG=b:287264443
    TEST=Enable
         CONFIG_PICOLIBC=y
         CONFIG_PICOLIBC_USE_MODULE=y
         Verified EC runs on skyrim
    
    Change-Id: I5c041c0000b4ae38bf8364904ae9d24660f98939

--- zephyr/zmake/zmake/configlib.py
+++ zephyr/zmake/zmake/configlib.py
@@ -47,14 +47,14 @@ def register_binman_project(**kwargs):
 def register_npcx_project(**kwargs):
     """Register a project that uses NpcxPacker."""
     kwargs.setdefault("output_packer", zmake.output_packers.NpcxPacker)
-    kwargs.setdefault("modules", ["cmsis", "ec"])
+    kwargs.setdefault("modules", ["cmsis", "picolibc", "ec"])
     return register_binman_project(**kwargs)
 
 
 def register_mchp_project(**kwargs):
     """Register a project that uses MchpPacker."""
     kwargs.setdefault("output_packer", zmake.output_packers.MchpPacker)
-    kwargs.setdefault("modules", ["cmsis", "ec"])
+    kwargs.setdefault("modules", ["cmsis", "picolibc", "ec"])
     return register_binman_project(**kwargs)
 
 
