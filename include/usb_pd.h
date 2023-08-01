60f1d2cdb2e9701509980f025ab84c8c5cb8ef16
Brandon Breitenstein <brandon.breitenstein@intel.com>
2023-08-01 10:54:12 -0700

    ec: Update SVDM VERS to Major
    With incoming DP 2.1 changes SVDM will need major and minor versions
    updated the existing macros to VDO_SVDM_VERS_MAJOR.
    
    BUG=b:266823500
    TEST=zmake build rex, twister rex
    LOW_COVERAGE_REASON=this is a no-op name change; see b/266823500
    
    Change-Id: I977a78f5cf0670ccb7843908bdd40e7ef7a1a5cc

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -521,7 +521,8 @@ struct partner_active_modes {
 	(((vid) << 16) | ((type) << 15) | ((custom)&0x7FFF))
 
 #define VDO_SVDM_TYPE BIT(15)
-#define VDO_SVDM_VERS(x) (x << 13)
+#define VDO_SVDM_VERS_MAJOR(x) (x << 13)
+#define VDO_SVDM_VERS_MINOR(x) (x << 11)
 #define VDO_OPOS(x) (x << 8)
 #define VDO_CMDT(x) (x << 6)
 #define VDO_OPOS_MASK VDO_OPOS(0x7)
@@ -566,6 +567,8 @@ struct partner_active_modes {
 #define PD_VDO_OPOS(vdo) (((vdo) >> 8) & 0x7)
 #define PD_VDO_CMD(vdo) ((vdo)&0x1f)
 #define PD_VDO_CMDT(vdo) (((vdo) >> 6) & 0x3)
+#define PD_VDO_SVDM_VERS_MAJOR(vdo) (((vdo) >> 13) & 0x3)
+#define PD_VDO_SVDM_VERS_MINOR(vdo) (((vdo) >> 11) & 0x3)
 
 /*
  * SVDM Identity request -> response
