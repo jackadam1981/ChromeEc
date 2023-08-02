58411f3dfd4495dcd90e0c3105ebe5e8db3c9bd3
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-08-02 11:37:14 -0700

    intel altmode: Add code to support the PD read/write
    Added code to support read / write to the PD's data status and
    data control registers.
    
    BUG=none
    TEST=none
    
    Change-Id: I279c651b3f95f4023f2552fa46f4cce06d8f138f

--- common/pd/pd_task_intel_altmode.c
+++ common/pd/pd_task_intel_altmode.c
@@ -20,8 +20,67 @@
 #define CPRINTF(format, args...) cprintf(CC_USBPD, format, ##args)
 #define CPRINTS(format, args...) cprints(CC_USBPD, format, ##args)
 
+/* Store current data of the DATA STATUS register */
+static union data_status_reg data_status[CONFIG_USB_PD_PORT_MAX_COUNT];
+
+static int pd_read(int port, union data_status_reg *data)
+{
+	int rv;
+	uint8_t buf[DATA_STATUS_REG_LEN + 1];
+	const uint8_t reg = REG_DATA_STATUS;
+
+	/*
+	 * Read sequence
+	 * DEV_ADDR - REG_ID - DEV_ADDR - READ_LEN - DATA0 .. DATAn
+	 */
+	rv = i2c_xfer(pd_config[port].i2c_info.port,
+		      pd_config[port].i2c_info.addr_flags, &reg, 1, buf,
+		      DATA_STATUS_REG_LEN + 1);
+	if (rv)
+		return rv;
+	if (buf[0] != DATA_STATUS_REG_LEN)
+		return EC_ERROR_UNKNOWN;
+
+	memcpy(data, &buf[1], DATA_STATUS_REG_LEN);
+	return EC_SUCCESS;
+}
+
+static int pd_write(int port, union data_control_reg *data)
+{
+	uint8_t buf[DATA_CONTROL_REG_LEN + 2];
+
+	buf[0] = REG_DATA_CONTROL;
+	buf[1] = DATA_CONTROL_REG_LEN;
+	memcpy(&buf[2], data->raw_value, DATA_CONTROL_REG_LEN);
+
+	/*
+	 * Write sequence
+	 * DEV_ADDR - REG_ID - DATA_LEN - DATA0 .. DATAn
+	 */
+	return i2c_xfer(pd_config[port].i2c_info.port,
+			pd_config[port].i2c_info.addr_flags, buf,
+			DATA_CONTROL_REG_LEN + 2, NULL, 0);
+}
+
 static void process_altmode_pd_data(int port)
 {
+	union data_status_reg status;
+	union data_control_reg control = { 0 };
+
+	/* Clear the interrupt */
+	control.i2c_int_ack = 1;
+	pd_write(port, &control);
+
+	/* Nothing to do if the data in the status register has not changed */
+	if (pd_read(port, &status) ||
+	    memcmp(&status.raw_value[0], &data_status[port].raw_value[0],
+		   sizeof(union data_status_reg)))
+		return;
+
+	/* Update the new data */
+	memcpy(&data_status[port], &status, sizeof(union data_status_reg));
+
+	/* Process MUX events */
 }
 
 /* Enable interrupt when AP is on */
