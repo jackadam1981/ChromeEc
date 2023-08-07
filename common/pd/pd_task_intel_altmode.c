f66115ab6aacb9740e04f866bfb49e0c7109b8bf
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-08-07 12:18:22 -0700

    intel altmode: Add support for DP mode mux
    Added code to support MUX for DP mode to configure the MUXes
    on Intel SOC.
    
    BUG=b:295917619
    TEST=Able to negotiate DP using CCG8-PD & MTL-RVP
    LOW_COVERAGE_REASON=Likely PMC to PD communication is POR, b:295924125
    
    Change-Id: Id467badf3f34c72c84924629d636ed48856ba28d

--- common/pd/pd_task_intel_altmode.c
+++ common/pd/pd_task_intel_altmode.c
@@ -69,6 +69,9 @@ static void process_altmode_pd_data(int port)
 	union data_status_reg status;
 	union data_control_reg control = { .i2c_int_ack = 1 };
 	mux_state_t mux = USB_PD_MUX_NONE;
+#ifdef CONFIG_PLATFORM_EC_USB_PD_DP_MODE
+	mux_state_t prv_hpd_lvl;
+#endif
 
 	/* Clear the interrupt */
 	rv = pd_write(port, &control);
@@ -89,6 +92,11 @@ static void process_altmode_pd_data(int port)
 		    sizeof(union data_status_reg)))
 		return;
 
+#ifdef CONFIG_PLATFORM_EC_USB_PD_DP_MODE
+	/* Store previous HPD tatus */
+	prv_hpd_lvl = data_status[port].hpd_lvl;
+
+#endif
 	/* Update the new data */
 	memcpy(&data_status[port], &status, sizeof(union data_status_reg));
 
@@ -102,12 +110,30 @@ static void process_altmode_pd_data(int port)
 	if (status.usb2 || status.usb3_2)
 		mux |= USB_PD_MUX_USB_ENABLED;
 
+#ifdef CONFIG_PLATFORM_EC_USB_PD_DP_MODE
+	/* DP status */
+	if (status.dp)
+		mux |= USB_PD_MUX_DP_ENABLED;
+
+	if (status.hpd_lvl)
+		mux |= USB_PD_MUX_HPD_LVL;
+
+	if (status.dp_irq)
+		mux |= USB_PD_MUX_HPD_IRQ;
+#endif
+
 	CPRINTS("Set P%d mux=0x%x", port, mux);
 
 	usb_mux_set(port, mux,
 		    mux == USB_PD_MUX_NONE ? USB_SWITCH_DISCONNECT :
 					     USB_SWITCH_CONNECT,
 		    polarity_rm_dts(status.conn_ori));
+
+#ifdef CONFIG_PLATFORM_EC_USB_PD_DP_MODE
+	/* Update the change in HPD level */
+	if (prv_hpd_lvl != status.hpd_lvl)
+		usb_mux_hpd_update(port, USB_PD_MUX_HPD_LVL);
+#endif
 }
 
 /* Enable interrupt when AP is on */
@@ -228,6 +254,13 @@ int pd_is_connected(int port)
 {
 	return data_status[port].data_conn;
 }
+
+#ifdef CONFIG_PLATFORM_EC_USB_PD_DP_MODE
+__override uint8_t get_dp_pin_mode(int port)
+{
+	return data_status[port].dp_pin << 2;
+}
+#endif
 
 /*
  * Add functions for which the data can be obtained from PD to EC interface.
