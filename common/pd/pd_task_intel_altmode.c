6e646d6970f48297b57c28ff31abef99bf034213
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-08-07 14:38:23 -0700

    mtlrvp: test PD code
    BUG=none
    TEST=none
    
    Change-Id: I1e85ff885152e87406fc168c49cb2e87ec1c11c4

--- common/pd/pd_task_intel_altmode.c
+++ common/pd/pd_task_intel_altmode.c
@@ -34,14 +34,19 @@ static int pd_read(int port, union data_status_reg *data)
 	 * Read sequence
 	 * DEV_ADDR - REG_ID - DEV_ADDR - READ_LEN - DATA0 .. DATAn
 	 */
+	ccprintf("i2c=%d add=0x%x, rg=0x%x\n", pd_config[port].i2c_info.port,
+		 pd_config[port].i2c_info.addr_flags, reg);
 	rv = i2c_xfer(pd_config[port].i2c_info.port,
 		      pd_config[port].i2c_info.addr_flags, &reg, 1, buf,
 		      DATA_STATUS_REG_LEN + 1);
+	ccprintf("p%d @%d rv=%d******\n", port, __LINE__, rv);
 	if (rv)
 		return rv;
+	ccprintf("p%d @%d ******\n", port, __LINE__);
 	if (buf[0] != DATA_STATUS_REG_LEN)
 		return EC_ERROR_UNKNOWN;
 
+	ccprintf("p%d @%d ******\n", port, __LINE__);
 	memcpy(data, &buf[1], DATA_STATUS_REG_LEN);
 	return EC_SUCCESS;
 }
@@ -74,12 +79,20 @@ static void process_altmode_pd_data(int port)
 	control.i2c_int_ack = 1;
 	pd_write(port, &control);
 
+	ccprintf("p%d @%d ******\n", port, __LINE__);
 	/* Nothing to do if the data in the status register has not changed */
-	if (pd_read(port, &status) ||
-	    memcmp(&status.raw_value[0], &data_status[port].raw_value[0],
-		   sizeof(union data_status_reg)))
+	if (pd_read(port, &status)) {
+		ccprintf("p%d @%d ******\n", port, __LINE__);
 		return;
+	}
 
+	if (!memcmp(&status.raw_value[0], &data_status[port].raw_value[0],
+		    sizeof(union data_status_reg))) {
+		ccprintf("p%d @%d ******\n", port, __LINE__);
+		return;
+	}
+
+	ccprintf("p%d @%d ******\n", port, __LINE__);
 	/* Store previous HPD tatus */
 	prv_hpd_lvl = data_status[port].hpd_lvl;
 
@@ -147,7 +160,7 @@ void pd_task_intel_altmode(void *u)
 
 	while (1) {
 		task_wait_event(-1);
-
+		ccprintf("wake event ******\n");
 		for (i = 0; i < CONFIG_USB_PD_PORT_MAX_COUNT; i++) {
 			/* Process data of interrupted port */
 			if (!gpio_get_level(pd_config[i].alert_signal))
@@ -155,3 +168,105 @@ void pd_task_intel_altmode(void *u)
 		}
 	}
 }
+
+static int console_command_intel_altmode(int argc, const char **argv)
+{
+	int port, rv, i;
+	char rw, *e;
+	uint16_t val1;
+	uint32_t val2 = 0;
+	union data_status_reg data;
+	union data_control_reg control;
+
+	/* Get PD port number */
+	port = strtoi(argv[1], &e, 0);
+	if (*e || port > board_get_usb_pd_port_count())
+		return EC_ERROR_PARAM1;
+
+	/* Validate r/w selection */
+	rw = argv[2][0];
+	if (rw != 'w' && rw != 'r')
+		return EC_ERROR_PARAM2;
+
+	if (rw == 'r') {
+		rv = pd_read(port, &data);
+		ccprintf("RAW_VAL[LB->HB]: ");
+		for (i = 0; i < DATA_STATUS_REG_LEN; i++)
+			ccprintf("[%d<-%d]0x%x ", 7 + 8 * i, 8 * i,
+				 data.raw_value[i]);
+		ccprintf("\n");
+	} else {
+		val1 = strtoull(argv[3], &e, 0);
+		if (*e)
+			return EC_ERROR_PARAM3;
+
+		if (argc > 4) {
+			val2 = strtoull(argv[4], &e, 0);
+			if (*e)
+				return EC_ERROR_PARAM4;
+		}
+
+		memcpy(&control.raw_value[0], &val1, 2);
+		memcpy(&control.raw_value[2], &val2, 4);
+
+		rv = pd_write(port, &control);
+		ccprintf("RAW_VAL[LB->HB]: ");
+		for (i = 0; i < DATA_CONTROL_REG_LEN; i++)
+			ccprintf("[%d<-%d]0x%x ", 7 + 8 * i, 8 * i,
+				 control.raw_value[i]);
+		ccprintf("\n");
+	}
+
+	return rv;
+}
+DECLARE_CONSOLE_COMMAND(pd, console_command_intel_altmode,
+			"<port> r\n"
+			"<port> w <val1> | <val2>",
+			"Read or write to PD reg");
+
+/****************************************************************************/
+enum tcpc_cc_polarity pd_get_polarity(int port)
+{
+	return POLARITY_CC1;
+}
+
+static int dummy_init(const struct usb_mux *me)
+{
+	return EC_SUCCESS;
+}
+
+static int dummy_set_state(const struct usb_mux *me, mux_state_t mux_state,
+			   bool *ack_required)
+{
+	*ack_required = false;
+	return EC_SUCCESS;
+}
+
+static int dummy_idle_mode(const struct usb_mux *me, bool idle)
+{
+	return EC_SUCCESS;
+}
+
+static int dummy_low_power_mode(const struct usb_mux *me)
+{
+	return EC_SUCCESS;
+}
+
+static bool dummy_fw_update_capable(void)
+{
+	return false;
+}
+
+void dummy_hpd_update(const struct usb_mux *me, mux_state_t hpd_state,
+		      bool *ack_required)
+{
+	*ack_required = false;
+}
+
+const struct usb_mux_driver dummy_mtl_pd = {
+	.init = dummy_init,
+	.set = dummy_set_state,
+	.set_idle_mode = dummy_idle_mode,
+	.enter_low_power_mode = dummy_low_power_mode,
+	.is_retimer_fw_update_capable = dummy_fw_update_capable,
+};
