0d952469cbe102bc0714940ef28ed18ae4d95091
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-08-07 11:57:20 -0700

    intel altmode: Add support for USB mode mux
    Added code to support MUX for USB mode to configure the MUXes
    on Intel SOC.
    
    BUG=b:295917619
    TEST=Able to negotiate USB using CCG8-PD & MTL-RVP
    LOW_COVERAGE_REASON=Likely PMC to PD communication is POR, b:295924125
    
    Change-Id: I753e27e5d52d7879e0cef6e3d2d1ec77ee05021f

--- common/pd/pd_task_intel_altmode.c
+++ common/pd/pd_task_intel_altmode.c
@@ -68,6 +68,7 @@ static void process_altmode_pd_data(int port)
 	int rv;
 	union data_status_reg status;
 	union data_control_reg control = { .i2c_int_ack = 1 };
+	mux_state_t mux = USB_PD_MUX_NONE;
 
 	/* Clear the interrupt */
 	rv = pd_write(port, &control);
@@ -92,6 +93,21 @@ static void process_altmode_pd_data(int port)
 	memcpy(&data_status[port], &status, sizeof(union data_status_reg));
 
 	/* Process MUX events */
+
+	/* Orientation */
+	if (status.conn_ori)
+		mux |= USB_PD_MUX_POLARITY_INVERTED;
+
+	/* USB status */
+	if (status.usb2 || status.usb3_2)
+		mux |= USB_PD_MUX_USB_ENABLED;
+
+	CPRINTS("Set P%d mux=0x%x", port, mux);
+
+	usb_mux_set(port, mux,
+		    mux == USB_PD_MUX_NONE ? USB_SWITCH_DISCONNECT :
+					     USB_SWITCH_CONNECT,
+		    polarity_rm_dts(status.conn_ori));
 }
 
 /* Enable interrupt when AP is on */
