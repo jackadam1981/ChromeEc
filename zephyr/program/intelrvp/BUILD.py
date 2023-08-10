147b712a62558423931e2d877519da6b91301d4e
Jay Patel <jay2.patel@intel.com>
2023-08-09 17:17:24 -0700

    mtlrvp: Modify dts files for mtlrvpp_pd variant support
    Modify device tree files to use common .dts files for mtlrvpp_pd and
    mtlrvpp_npcx. Typec related devices excluded in mtlrvpp_pd using
    BUILD.py file.
    
    Change-Id: I84d427c565d6532f4067b92d7808a36a25b336b2
    Signed-off-by: Jay Patel <jay2.patel@intel.com>

--- zephyr/program/intelrvp/BUILD.py
+++ zephyr/program/intelrvp/BUILD.py
@@ -95,6 +95,7 @@ register_intelrvp_project(
     extra_kconfig_files=[
         here / "zephyr_ap_pwrseq.conf",
         here / "mtlrvp/mtlrvpp_mchp/prj.conf",
+        here / "mtlrvp/typec.conf",
     ],
 )
 
@@ -109,6 +110,7 @@ register_intelrvp_project(
         here / "mtlrvp/mtlrvpp_npcx/interrupts.dts",
         here / "mtlrvp/ioex.dts",
         here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx.dts",
+        here / "mtlrvp/mtlrvpp_npcx/typec.dts",
         here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx_power_signals.dts",
         here / "adlrvp/adlrvp_npcx/temp_sensor.dts",
         here / "mtlrvp/usbc.dts",
@@ -116,5 +118,27 @@ register_intelrvp_project(
     extra_kconfig_files=[
         here / "zephyr_ap_pwrseq.conf",
         here / "mtlrvp/mtlrvpp_npcx/prj.conf",
+        here / "mtlrvp/typec.conf",
+    ],
+)
+
+register_intelrvp_project(
+    project_name="mtlrvpp_pd",
+    chip="npcx9m3f",
+    extra_dts_overlays=[
+        here / "mtlrvp/mtlrvpp_npcx/fan.dts",
+        here / "mtlrvp/mtlrvpp_npcx/gpio.dts",
+        here / "mtlrvp/mtlrvpp_npcx/keyboard.dts",
+        here / "mtlrvp/mtlrvpp_npcx/interrupts.dts",
+        here / "mtlrvp/ioex.dts",
+        here / "mtlrvp/mtlrvpp_npcx/mtlrvp_ncpx.dts",
+        here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx_power_signals.dts",
+        here / "adlrvp/adlrvp_npcx/temp_sensor.dts",
+        here / "mtlrvp/usbc.dts",
+    ],
+    extra_kconfig_files=[
+        here / "zephyr_ap_pwrseq.conf",
+        here / "mtlrvp/mtlrvpp_npcx/prj.conf",
+        here / "mtlrvp/typec.conf",
     ],
 )
