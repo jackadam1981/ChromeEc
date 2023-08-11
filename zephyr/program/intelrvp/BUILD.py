e15725bb76437cc247f59e6e66af4e492c2efbe2
Jay Patel <jay2.patel@intel.com>
2023-08-10 18:04:10 -0700

    mtlrvp: Separate typec related device nodes
    Change-Id: Iac01d98917f014cb61c0daa929e047abcc53ab3b
    Signed-off-by: Jay Patel <jay2.patel@intel.com>

--- zephyr/program/intelrvp/BUILD.py
+++ zephyr/program/intelrvp/BUILD.py
@@ -106,6 +106,7 @@ register_intelrvp_project(
     extra_dts_overlays=[
         here / "mtlrvp/mtlrvpp_npcx/fan.dts",
         here / "mtlrvp/mtlrvpp_npcx/gpio.dts",
+        here / "mtlrvp/mtlrvpp_npcx/typec_gpio.dts",
         here / "mtlrvp/mtlrvpp_npcx/keyboard.dts",
         here / "mtlrvp/mtlrvpp_npcx/interrupts.dts",
         here / "mtlrvp/ioex.dts",
@@ -134,7 +135,6 @@ register_intelrvp_project(
         here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx.dts",
         here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx_power_signals.dts",
         here / "adlrvp/adlrvp_npcx/temp_sensor.dts",
-        here / "mtlrvp/usbc.dts",
     ],
     extra_kconfig_files=[
         here / "zephyr_ap_pwrseq.conf",
