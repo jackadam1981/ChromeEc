332b79f5788fd657d281a241d4c6ff7a162d46a9
Jay Patel <jay2.patel@intel.com>
2023-08-14 23:18:17 -0700

    intelrvp: Separate usb functions
    Modify CMakeLists, Kconfig and src files to separate USB functions to
    build according to the project. This would enable PD solution project
    mtlrvpp_pd to use other common functions.
    
    BUG=b:296055690
    TEST=zmake build mtlrvpp_mchp
         zmake build mtlrvpp_npcx
    
    Change-Id: Ie70508aef5249771fc0e5d408cfe33f4798783af
    Signed-off-by: Jay Patel <jay2.patel@intel.com>

--- zephyr/program/intelrvp/include/intelrvp.h
+++ zephyr/program/intelrvp/include/intelrvp.h
@@ -26,6 +26,7 @@ struct tcpc_aic_gpio_config_t {
 };
 extern const struct tcpc_aic_gpio_config_t tcpc_aic_gpios[];
 
+int board_dc_jack_present(void);
 void board_charging_enable(int port, int enable);
 void board_vbus_enable(int port, int enable);
 void board_set_vbus_source_current_limit(int port, enum tcpc_rp_value rp);
