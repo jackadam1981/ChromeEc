a53c037972ee0c691d91eba3d3cae7774eaf38c4
Brandon Breitenstein <brandon.breitenstein@intel.com>
2023-08-16 09:09:24 -0700

    retimer: Add DP2.1 cable support in retimer
    This change add support for DP2.1 Alternate Mode support in reimer.
    
    BUG=b:266823500
    TEST=EC console command retimer 0 r 4
    
    Change-Id: I00a57350018e34a46c951106e39a0e21b3f84d44

--- driver/retimer/bb_retimer.c
+++ driver/retimer/bb_retimer.c
@@ -14,6 +14,7 @@
 #include "i2c.h"
 #include "task.h"
 #include "timer.h"
+#include "usb_dp_alt_mode.h"
 #include "usb_pd.h"
 #include "util.h"
 
@@ -175,6 +176,12 @@ static void retimer_set_state_dfp(int port, mux_state_t mux_state,
 	};
 	union tbt_mode_resp_device dev_resp;
 	enum idh_ptype cable_type = get_usb_pd_cable_type(port);
+	union dp_mode_resp_cable cable_dp_mode_resp = {
+		.raw_value =
+			IS_ENABLED(CONFIG_USB_PD_DP21_MODE) ?
+				dp_get_mode_vdo(port, TCPCI_MSG_SOP_PRIME) :
+				0
+	};
 
 	if (mux_state & BB_RETIMER_MUX_USB_MODE) {
 		/*
@@ -290,8 +297,68 @@ static void retimer_set_state_dfp(int port, mux_state_t mux_state,
 		*set_retimer_con |=
 			BB_RETIMER_TBT_CABLE_GENERATION(cable_resp.tbt_rounded);
 	}
+	if (IS_ENABLED(CONFIG_USB_PD_DP21_MODE) &&
+	    (mux_state & USB_PD_MUX_DP_ENABLED)) {
+		enum dpam_version dpam_ver =
+			dp_resolve_dpam_version(port, TCPCI_MSG_SOP_PRIME);
 
-	if (mux_state & USB_PD_MUX_DP_ENABLED) {
+		/*
+		 * Bit 2: RE_TIMER_DRIVER
+		 * 0 - Re-driver
+		 * 1 - Re-timer
+		 *
+		 * If Alternate mode is DP2.0 RE_TIMER_DRIVER is
+		 * set according to SOP' VDO2 response Bit 9.
+		 *
+		 * If Alternate mode is DP2.1 RE_TIMER_DRIVER is
+		 * set according to DP Discover mode SOP' response Bit 29:28.
+		 */
+		if ((dpam_ver == DPAM_VERSION_20 &&
+		     is_active_cable_element_retimer(port)) ||
+		    (dpam_ver == DPAM_VERSION_21 &&
+		     cable_dp_mode_resp.active_comp ==
+			     DP21_ACTIVE_RETIMER_CABLE))
+			*set_retimer_con |= BB_RETIMER_RE_TIMER_DRIVER;
+
+		/*
+		 * Bit 18: CABLE_TYPE
+		 * 0 - Electrical cable
+		 * 1 - Optical cable
+		 */
+		if (cable_dp_mode_resp.active_comp == DP21_OPTICAL_CABLE &&
+		    dpam_ver == DPAM_VERSION_21)
+			*set_retimer_con |= BB_RETIMER_TBT_CABLE_TYPE;
+
+		/*
+		 * Bit 22: ACTIVE/PASSIVE
+		 * 0 - Passive cable
+		 * 1 - Active cable
+		 *
+		 * If the mode is DP2.1, ACTIVE/PASIVE is set according to
+		 * DP Discover mode SOP' response B29:28
+		 * If the mode is DP2.0, ACTIVE/PASIVE is set according to
+		 * Discover ID SOP' response B29:27.
+		 */
+		if (((dpam_ver == DPAM_VERSION_20) &&
+		     (cable_type == IDH_PTYPE_ACABLE)) ||
+		    ((dpam_ver == DPAM_VERSION_21) &&
+		     (cable_dp_mode_resp.active_comp != DP21_PASSIVE_CABLE)))
+			*set_retimer_con |= BB_RETIMER_ACTIVE_PASSIVE;
+
+		/*
+		 * Bit 27-25: DP Cable speed for DP2.1
+		 * 000b - No functionality
+		 * 001b - HBR3
+		 * 010b - UHBR10
+		 * 100b - UHBR20
+		 */
+		*set_retimer_con |= BB_RETIMER_USB4_TBT_CABLE_SPEED_SUPPORT(
+			dpam_ver == DPAM_VERSION_21 ?
+				dp_get_cable_bit_rate(port) :
+				get_usb4_cable_speed(port));
+
+	} else if (!IS_ENABLED(CONFIG_USB_PD_DP21_MODE) &&
+		   (mux_state & USB_PD_MUX_DP_ENABLED)) {
 		/*
 		 * Bit 2: RE_TIMER_DRIVER
 		 * 0 - Re-driver
