39e2313256691b6b03487f29850bc762e380bf1b
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-08-16 18:07:36 -0700

    Zephyr: Add PD_CHIP_INFO & RWHASHPD host command Kconfigs
    Added CONFIG_HOSTCMD_RWHASHPD & CONFIG_HOSTCMD_RWHASHPD host commands
    to Zephyr Kconfig so that the configs can be disabled if unused.
    
    BUG=b:295917619
    TEST=zmake build -a && make buildall -j
    
    Change-Id: I9f55d73e158acad972730fb2222d510431356194

--- include/config.h
+++ include/config.h
@@ -2568,7 +2568,7 @@
 #undef CONFIG_HOSTCMD_PD
 
 /* EC supports EC_CMD_PD_CHIP_INFO */
-#define CONFIG_EC_CMD_PD_CHIP_INFO
+#define CONFIG_HOSTCMD_PD_CHIP_INFO
 
 /*
  * Use if PD MCU controls charging (selecting charging port and input
