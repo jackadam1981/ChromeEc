a6cf8ac07eaa2bcdb86740d387375a444bbf7bdd
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2023-08-17 15:30:36 +0800

    SM5803: enable PROCHOT control and related configuration
    - Enable prochot duration setting
    - Enable VBUS monitor selection
    - Enable VSYS monitor selection
    - Enable IBAT prochot selection
    
    BUG=b:296487447
    TEST=zmake build joxer
    
    Change-Id: If4ef885d1bc867422eaa8a53109cb6b46da8500a

--- driver/charger/sm5803.c
+++ driver/charger/sm5803.c
@@ -484,6 +484,86 @@ static void init_mutexes(void)
 DECLARE_HOOK(HOOK_INIT, init_mutexes, HOOK_PRIO_FIRST);
 #endif
 
+#ifdef CONFIG_CHARGER_SM5803_PROCHOT_DURATION
+__maybe_unused static enum ec_error_list
+sm5803_set_phot_duration(int chgnum, enum sm5803_phot1_duration duration)
+{
+	enum ec_error_list rv = EC_SUCCESS;
+	int reg;
+
+	/* Set PHOT_DURATION */
+	rv |= chg_read8(chgnum, SM5803_REG_PHOT1, &reg);
+	reg &= ~SM5803_PHOT1_DURATION;
+	reg |= duration << SM5803_PHOT1_DURATION_SHIFT;
+	rv |= chg_write8(chgnum, SM5803_REG_PHOT1, reg);
+
+	if (rv)
+		return rv;
+
+	return EC_SUCCESS;
+}
+#endif /* CONFIG_CHARGER_SM5803_PROCHOT_DURATION */
+
+#ifdef CONFIG_CHARGER_SM5803_VBUS_MON_SEL
+__maybe_unused static enum ec_error_list
+sm5803_set_vbus_monitor_sel(int chgnum, enum sm5803_phot2_vbus_sel vbus_sel)
+{
+	enum ec_error_list rv = EC_SUCCESS;
+	int reg;
+
+	/* Set VBUS_MONITOR_SEL */
+	rv |= chg_read8(chgnum, SM5803_REG_PHOT2, &reg);
+	reg &= ~SM5803_PHOT2_VBUS_SEL;
+	reg |= vbus_sel;
+	rv |= chg_write8(chgnum, SM5803_REG_PHOT2, reg);
+
+	if (rv)
+		return rv;
+
+	return EC_SUCCESS;
+}
+#endif /* CONFIG_CHARGER_SM5803_VBUS_MON_SEL */
+
+#ifdef CONFIG_CHARGER_SM5803_VSYS_MON_SEL
+__maybe_unused static enum ec_error_list
+sm5803_set_vsys_monitor_sel(int chgnum, enum sm5803_phot3_vbus_sel vsys_sel)
+{
+	enum ec_error_list rv = EC_SUCCESS;
+	int reg;
+
+	/* Set VSYS_MONITOR_SEL */
+	rv |= chg_read8(chgnum, SM5803_REG_PHOT3, &reg);
+	reg &= ~SM5803_PHOT3_VSYS_SEL;
+	reg |= vsys_sel;
+	rv |= chg_write8(chgnum, SM5803_REG_PHOT3, reg);
+
+	if (rv)
+		return rv;
+
+	return EC_SUCCESS;
+}
+#endif /* CONFIG_CHARGER_SM5803_VSYS_MON_SEL */
+
+#ifdef CONFIG_CHARGER_SM5803_IBAT_PHOT_SEL
+__maybe_unused static enum ec_error_list sm5803_set_ibat_phot_sel(int chgnum,
+								  int ibat_sel)
+{
+	enum ec_error_list rv = EC_SUCCESS;
+	int reg;
+
+	/* Set IBAT_PHOT_SEL */
+	rv |= chg_read8(chgnum, SM5803_REG_PHOT4, &reg);
+	reg &= ~SM5803_PHOT4_IBAT_SEL;
+	reg |= SM5803_IBAT_PROCHOT_MA_TO_REG(ibat_sel);
+	rv |= chg_write8(chgnum, SM5803_REG_PHOT4, reg);
+
+	if (rv)
+		return rv;
+
+	return EC_SUCCESS;
+}
+#endif /* CONFIG_CHARGER_SM5803_IBAT_PHOT_SEL */
+
 static void sm5803_init(int chgnum)
 {
 	enum ec_error_list rv;
@@ -695,6 +775,25 @@ static void sm5803_init(int chgnum)
 		rv |= test_write8(chgnum, 0x47, 0x10);
 		rv |= test_write8(chgnum, 0x48, 0x04);
 		rv |= main_write8(chgnum, 0x1F, 0x0);
+/*
+ * Check the config is hardware default and do nothing if it is.
+ */
+#if CONFIG_CHARGER_SM5803_PROCHOT_DURATION != 2
+		rv |= sm5803_set_phot_duration(
+			chgnum, CONFIG_CHARGER_SM5803_PROCHOT_DURATION);
+#endif
+#if CONFIG_CHARGER_SM5803_VBUS_MON_SEL != 2
+		rv |= sm5803_set_vbus_monitor_sel(
+			chgnum, CONFIG_CHARGER_SM5803_VBUS_MON_SEL);
+#endif
+#if CONFIG_CHARGER_SM5803_VSYS_MON_SEL != 10
+		rv |= sm5803_set_vsys_monitor_sel(
+			chgnum, CONFIG_CHARGER_SM5803_VSYS_MON_SEL);
+#endif
+#if CONFIG_CHARGER_SM5803_IBAT_PHOT_SEL != 37800
+		rv |= sm5803_set_ibat_phot_sel(
+			chgnum, CONFIG_CHARGER_SM5803_IBAT_PHOT_SEL);
+#endif
 	}
 
 	/* Enable LDO bits */
