2a887e287c51e5760b5aa4e230e9f9c510e2b8fd
Reka Norman <rekanorman@google.com>
2023-08-21 09:59:47 +1000

    cec: Enable most logs by default
    Enable most CEC logs by default when CEC is enabled.
    
    For very verbose logs, e.g. those logged for every message sent or
    received, add new DEBUG_CPRINTx macros which are default off.
    
    BUG=b:270507438
    TEST=Logs are as expected with and without CONFIG_CEC_DEBUG.
    
    Change-Id: I24e5228fdba8171c166374c993a58bdf467ab16e

--- driver/cec/bitbang.c
+++ driver/cec/bitbang.c
@@ -12,12 +12,15 @@
 #include "task.h"
 #include "util.h"
 
-#ifdef CONFIG_CEC_DEBUG
 #define CPRINTF(format, args...) cprintf(CC_CEC, format, ##args)
 #define CPRINTS(format, args...) cprints(CC_CEC, format, ##args)
+
+#ifdef CONFIG_CEC_DEBUG
+#define DEBUG_CPRINTF(format, args...) cprintf(CC_CEC, format, ##args)
+#define DEBUG_CPRINTS(format, args...) cprints(CC_CEC, format, ##args)
 #else
-#define CPRINTF(...)
-#define CPRINTS(...)
+#define DEBUG_CPRINTF(...)
+#define DEBUG_CPRINTS(...)
 #endif
 
 /*
@@ -383,7 +386,8 @@ static void enter_state(int port, enum cec_state new_state)
 				 * recent one.
 				 */
 				if (cec_rx.received_message_available)
-					CPRINTS("CEC%d: received message not "
+					DEBUG_CPRINTS(
+						"CEC%d: received message not "
 						"read out, discarding",
 						port);
 
@@ -760,7 +764,7 @@ static int bitbang_cec_send(int port, const uint8_t *msg, uint8_t len)
 	cec_tx.len = len;
 
 	snprintf_hex_buffer(str_buf, sizeof(str_buf), HEX_BUF(msg, len));
-	CPRINTS("CEC%d send: 0x%s", port, str_buf);
+	DEBUG_CPRINTS("CEC%d send: 0x%s", port, str_buf);
 
 	memcpy(cec_tx.transfer.buf, msg, len);
 
