6e1050445f942a5b17d3fd377a45f43403288f41
Eric Yilun Lin <yllin@chromium.org>
2023-08-23 10:29:23 -0700

    rt1739: mask SRCP and OCPS when FRS enabled
    This CL fixes RT1739 sometimes output 2V only while FRS happends.
    
    BUG=b:296988176
    TEST=FRS success, no output 2V issue
    
    Change-Id: I1fa80145624294e790c5f3a1a8e7c69a2368ae46

--- driver/ppc/rt1739.c
+++ driver/ppc/rt1739.c
@@ -244,6 +244,9 @@ static int rt1739_set_frs_enable(int port, int enable)
 	/* Enable FRS RX detect */
 	RETURN_ERROR(update_reg(port, RT1739_REG_CC_FRS_CTRL1, RT1739_FRS_RX_EN,
 				enable ? MASK_SET : MASK_CLR));
+	RETURN_ERROR(update_reg(port, RT1739_REG_VBUS_DEG_TIME,
+				RT1739_FRS_SRCP_MASK | RT1739_FRS_OSCS_MASK,
+				enable ? MASK_SET : MASK_CLR));
 
 	/*
 	 * To enable FRS, turn on FRS_RX interrupt and disable
