224f01179566e1e88fd034b938e71adeca72e9fa
Yi Chou <yich@google.com>
2023-08-24 14:40:00 +0800

    fpsensor: Add state to template
    BUG=None
    TEST=None
    
    Change-Id: I4648c7cc6110629b32458ad7a6c26467bbcf9e2f

--- common/fpsensor/fpsensor_state.cc
+++ common/fpsensor/fpsensor_state.cc
@@ -6,6 +6,7 @@
 #include "compile_time_macros.h"
 
 #include <array>
+#include <variant>
 
 /* Boringssl headers need to be included before extern "C" section. */
 #include "openssl/mem.h"
@@ -24,6 +25,7 @@ extern "C" {
 #include "fpsensor_auth_commands.h"
 #include "fpsensor_crypto.h"
 #include "fpsensor_state.h"
+#include "fpsensor_template_state.h"
 #include "fpsensor_utils.h"
 
 /* Last acquired frame (aligned as it is used by arbitrary binary libraries) */
@@ -42,6 +44,8 @@ uint8_t fp_enc_buffer[FP_ALGORITHM_ENCRYPTED_TEMPLATE_SIZE] FP_TEMPLATE_SECTION;
 uint8_t fp_positive_match_salt[FP_MAX_FINGER_COUNT]
 			      [FP_POSITIVE_MATCH_SALT_BYTES];
 
+std::array<fp_template_state, FP_MAX_FINGER_COUNT> template_states;
+
 /* LCOV_EXCL_START */
 __test_only void fp_task_simulate(void)
 {
@@ -57,6 +61,8 @@ void fp_clear_finger_context(uint16_t idx)
 	OPENSSL_cleanse(fp_template[idx], sizeof(fp_template[0]));
 	OPENSSL_cleanse(fp_positive_match_salt[idx],
 			sizeof(fp_positive_match_salt[0]));
+	template_states[idx].is_locked = false;
+	template_states[idx].state = std::monostate();
 }
 
 /**
@@ -75,8 +81,6 @@ static void _fp_clear_context(void)
 	OPENSSL_cleanse(user_id, sizeof(user_id));
 	OPENSSL_cleanse(auth_nonce.data(), auth_nonce.size());
 	fp_disable_positive_match_secret(&positive_match_secret_state);
-	for (uint16_t idx = 0; idx < FP_MAX_FINGER_COUNT; idx++)
-		fp_clear_finger_context(idx);
 }
 
 void fp_reset_and_clear_context(void)
