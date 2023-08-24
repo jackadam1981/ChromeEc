d349cef7ff4fc0ec4131a77f8f3760a7b7f8e4d0
Yi Chou <yich@google.com>
2023-08-24 14:40:00 +0800

    fpsensor: Add state to template
    BUG=None
    TEST=None
    
    Change-Id: I4648c7cc6110629b32458ad7a6c26467bbcf9e2f

---
+++ include/fpsensor_template_state.h
@@ -0,0 +1,30 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Fingerprint sensor interface */
+
+#ifndef __CROS_EC_FPSENSOR_TEMPLATE_STATE_H
+#define __CROS_EC_FPSENSOR_TEMPLATE_STATE_H
+
+#include "ec_commands.h"
+
+#include <stdbool.h>
+
+#include <array>
+#include <optional>
+
+extern "C" {
+#include "fpsensor_driver.h"
+}
+
+struct fp_template_state {
+	ec_fp_template_encryption_metadata enc_info;
+	std::optional<std::array<uint32_t, FP_CONTEXT_USERID_WORDS> > user_id;
+	bool is_locked;
+};
+
+extern std::array<fp_template_state, FP_MAX_FINGER_COUNT> template_states;
+
+#endif /* __CROS_EC_FPSENSOR_TEMPLATE_STATE_H */
