f20c017ea9ef46f9a5c374c5cebffb5be75a8ffb
Dawid Niedzwiecki <dawidn@google.com>
2023-08-25 09:42:05 +0200

    zephyr: bloonchipper: add support for host commands
    Bloonchipper uses can use two interfaces to communicate with host - UART
    and SPI. It is detected in runtime which interface should be used, based
    on the GPIO state.
    
    Add initialization of a proper host command backend in runtime. Also add
    handler for the EC_CMD_GET_PROTOCOL_INFO, which also returns values
    appropriate for a present interface.
    
    BUG=b:259667397
    TEST=zmake build bloonchipper && enable commands and run 'ectool
    --name=cros_fp stress'
    
    Change-Id: I93713e893b60228e73d2cd35ba7d894412faf13f

--- zephyr/test/fpmcu/tests/bloonchipper/src/transport.c
+++ zephyr/test/fpmcu/tests/bloonchipper/src/transport.c
@@ -10,10 +10,33 @@
 #include "zephyr/kernel.h"
 
 #include <zephyr/drivers/gpio/gpio_emul.h>
+#include <zephyr/fff.h>
+#include <zephyr/mgmt/ec_host_cmd/backend.h>
+#include <zephyr/mgmt/ec_host_cmd/ec_host_cmd.h>
 #include <zephyr/ztest.h>
+
+#define UART_MAX_REQUEST_SIZE 0x220
+#define SPI_MAX_REQUEST_SIZE 0x220
+
+#define UART_MAX_RESPONSE_SIZE 0x100
+#define SPI_MAX_RESPONSE_SIZE 0x220
+
+FAKE_VALUE_FUNC(struct ec_host_cmd_backend *, ec_host_cmd_backend_get_uart,
+		const struct device *);
+FAKE_VALUE_FUNC(struct ec_host_cmd_backend *, ec_host_cmd_backend_get_spi,
+		struct gpio_dt_spec *);
+FAKE_VALUE_FUNC(int, ec_host_cmd_init, struct ec_host_cmd_backend *);
+
+enum ec_host_cmd_status
+host_command_protocol_info(struct ec_host_cmd_handler_args *args);
+int fp_transport_init(void);
 
 static void *transport_setup(void)
 {
+	RESET_FAKE(ec_host_cmd_backend_get_uart);
+	RESET_FAKE(ec_host_cmd_backend_get_spi);
+	RESET_FAKE(ec_host_cmd_init);
+
 	return NULL;
 }
 
@@ -44,4 +67,69 @@ ZTEST(transport, test_transport_type)
 		      "Incorrecty transport type");
 	zassert_equal(gpio_emul_output_get(div_highside_gpio, div_highside_pin),
 		      0);
+}
+
+ZTEST(transport, test_protocol_info)
+{
+	const struct device *transport_sel_gpio =
+		DEVICE_DT_GET(DT_GPIO_CTLR(DT_NODELABEL(transport_sel), gpios));
+	const gpio_port_pins_t transport_sel_pin =
+		DT_GPIO_PIN(DT_NODELABEL(transport_sel), gpios);
+	struct ec_response_get_protocol_info protocol_info;
+	struct ec_host_cmd_handler_args args;
+
+	memset(&args, 0, sizeof(args));
+	memset(&protocol_info, 0, sizeof(protocol_info));
+	args.output_buf = &protocol_info;
+
+	/* UART */
+	gpio_emul_input_set(transport_sel_gpio, transport_sel_pin, 0);
+	host_command_protocol_info(&args);
+	zassert_equal(protocol_info.flags,
+		      EC_PROTOCOL_INFO_IN_PROGRESS_SUPPORTED);
+	zassert_equal(protocol_info.max_request_packet_size,
+		      UART_MAX_REQUEST_SIZE);
+	zassert_equal(protocol_info.max_response_packet_size,
+		      UART_MAX_RESPONSE_SIZE);
+	zassert_equal(protocol_info.protocol_versions, BIT(3));
+	zassert_equal(args.output_buf_size, sizeof(protocol_info));
+
+	/* SPI */
+	gpio_emul_input_set(transport_sel_gpio, transport_sel_pin, 1);
+	host_command_protocol_info(&args);
+	zassert_equal(protocol_info.flags,
+		      EC_PROTOCOL_INFO_IN_PROGRESS_SUPPORTED);
+	zassert_equal(protocol_info.max_request_packet_size,
+		      SPI_MAX_REQUEST_SIZE);
+	zassert_equal(protocol_info.max_response_packet_size,
+		      SPI_MAX_RESPONSE_SIZE);
+	zassert_equal(protocol_info.protocol_versions, BIT(3));
+	zassert_equal(args.output_buf_size, sizeof(protocol_info));
+}
+
+ZTEST(transport, test_hc_init)
+{
+	const struct device *transport_sel_gpio =
+		DEVICE_DT_GET(DT_GPIO_CTLR(DT_NODELABEL(transport_sel), gpios));
+	const gpio_port_pins_t transport_sel_pin =
+		DT_GPIO_PIN(DT_NODELABEL(transport_sel), gpios);
+	const struct device *const dev_uart =
+		DEVICE_DT_GET(DT_CHOSEN(zephyr_host_cmd_uart_backend));
+	struct ec_host_cmd_backend *backend;
+
+	/* UART */
+	gpio_emul_input_set(transport_sel_gpio, transport_sel_pin, 0);
+	backend = (struct ec_host_cmd_backend *)0xABCD;
+	SET_RETURN_SEQ(ec_host_cmd_backend_get_uart, &backend, 1);
+	zassert_equal(fp_transport_init(), 0);
+	zassert_equal(ec_host_cmd_backend_get_uart_fake.arg0_history[0],
+		      dev_uart);
+	zassert_equal(ec_host_cmd_init_fake.arg0_history[0], backend);
+
+	/* SPI */
+	gpio_emul_input_set(transport_sel_gpio, transport_sel_pin, 1);
+	backend = (struct ec_host_cmd_backend *)0xDCBA;
+	SET_RETURN_SEQ(ec_host_cmd_backend_get_spi, &backend, 1);
+	zassert_equal(fp_transport_init(), 0);
+	zassert_equal(ec_host_cmd_init_fake.arg0_history[1], backend);
 }
