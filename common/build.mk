e109e9adf652e2feded14421bfe08cad807b3066
Craig Hesling <hesling@chromium.org>
2023-08-24 17:51:48 -0700

    common: Use new vars_from_dir to pull in usbc objects
    This doesn't change the objects selection for common/usbc,
    it just cleans up and simplifies the usbc build.mk.
    
    BUG=b:176500425, b:295089839
    TEST=make buildall
    TEST=./util/compare_build.sh -b all
    
    Change-Id: I27be25cca30a5a93842c6b22c4f3c2059782d06b

--- common/build.mk
+++ common/build.mk
@@ -337,4 +337,6 @@ include $(_common_dir)usbc/build.mk
 include $(_common_dir)spi/build.mk
 
 include $(_common_dir)mock/build.mk
+
+$(eval $(call vars_from_dir,common,usbc,common-usbc))
 $(eval $(call vars_from_dir,common,mock,mock))
