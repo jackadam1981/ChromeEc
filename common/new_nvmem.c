a3e9b182828d5f255cb466c017fd4a84783ac23c
Vadim Sukhomlinov <sukhomlinov@google.com>
2023-08-28 16:49:00 -0700

    cr50: refactor nvmem error logging
    Move common logging functionality into dedicated function.
    
    BUG=None
    TEST=make CR50_DEV=1, make sure that events are logged on prod->dev
    transition.
    
    Change-Id: I8a40d57659cf83a18bba72c722e677abbd431d90
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>

--- common/new_nvmem.c
+++ common/new_nvmem.c
@@ -366,6 +366,16 @@ test_export_static enum ec_error_list browse_flash_contents(int print);
 static enum ec_error_list save_container(struct nn_container *nc);
 static void invalidate_nvmem_flash(void);
 
+/* Log NVMEM problem into flash log. */
+static void log_no_payload_failure(enum nvmem_failure_type type)
+{
+	struct nvmem_failure_payload fp;
+
+	fp.failure_type = type;
+	flash_log_add_event(FE_LOG_NVMEM,
+			    offsetof(struct nvmem_failure_payload, size), &fp);
+}
+
 /* Log NVMEM problem as per passed in payload and size, and reboot. */
 static void report_failure(struct nvmem_failure_payload *payload,
 			   size_t payload_union_size)
@@ -387,15 +397,8 @@ static void report_failure(struct nvmem_failure_payload *payload,
 	ccprintf("Logging failure %d, will %sreinit\n", payload->failure_type,
 		 init_in_progress ? "" : "not ");
 
-	if (init_in_progress) {
-		struct nvmem_failure_payload fp;
-
-		fp.failure_type = NVMEMF_NVMEM_WIPE;
-
-		flash_log_add_event(
-			FE_LOG_NVMEM,
-			offsetof(struct nvmem_failure_payload, size), &fp);
-	}
+	if (init_in_progress)
+		log_no_payload_failure(NVMEMF_NVMEM_WIPE);
 
 	cflush();
 
@@ -807,8 +810,6 @@ test_export_static enum ec_error_list get_next_object(struct access_tracker *at,
 
 		/* And calculate hash. */
 		if (!container_is_valid(ch)) {
-			struct nvmem_failure_payload fp;
-
 			if (!init_in_progress)
 				report_no_payload_failure(
 					NVMEMF_CONTAINER_HASH_MISMATCH);
@@ -816,11 +817,7 @@ test_export_static enum ec_error_list get_next_object(struct access_tracker *at,
 			 * During init there might be a way to deal with
 			 * this, let's just log this and continue.
 			 */
-			fp.failure_type = NVMEMF_CONTAINER_HASH_MISMATCH;
-			flash_log_add_event(
-				FE_LOG_NVMEM,
-				offsetof(struct nvmem_failure_payload, size),
-				&fp);
+			log_no_payload_failure(NVMEMF_CONTAINER_HASH_MISMATCH);
 
 			return EC_ERROR_INVAL;
 		}
