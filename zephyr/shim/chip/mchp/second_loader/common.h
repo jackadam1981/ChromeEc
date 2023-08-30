fc5d54eb9a495b2a2311be751bb87fb5674e1a64
Manimaran A <manimaran.a@microchip.corp-partner.google.com>
2023-08-30 20:54:49 +0530

    mchp: Second loader source to program internal SPI Flash
    This is the source code to generate "second_loader_fw.bin".
    Which will be used during the MCHP Crisis recovery by "flash_ec" script.
    To build the source, go to "~/chromiumos/src/platform/ec/zephyr/" folder
    and invoke the command "zmake build adlrvp_mchp".
    
    BUG=None
    TEST=zmake build adlrvp_mchp
    TEST=zmake build mtlrvpp_mchp
    TEST=Test on MTL + MEC1727 MECC Version B board
    
    Change-Id: Iab26144d4f95f7f73fcb576888b8fdd8bc770acf

---
+++ zephyr/shim/chip/mchp/second_loader/common.h
@@ -0,0 +1,13 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#ifndef _COMMON_h_
+#define _COMMON_h_
+
+/* UART0 for HOST to EC interface */
+#define HOST_IF_UART (UART0_INST)
+/* UART1 for debug strings */
+#define DEBUG_UART (UART1_INST)
+
+#endif
