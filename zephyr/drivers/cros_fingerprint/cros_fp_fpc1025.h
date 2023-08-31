a1893f988b0112f8edb5f1945456c039848e024b
Patryk Duda <pdk@semihalf.com>
2023-08-31 19:43:07 +0200

    cros_fingerprint: Introduce driver for FPC1025 fingerprint sensor
    BUG=b:299924239
    TEST=zmake build bloonchipper
    
    Change-Id: Ic471f273ab64491a1a4daba18246d1dafa97c28d

---
+++ zephyr/drivers/cros_fingerprint/cros_fp_fpc1025.h
@@ -0,0 +1,26 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_FP_FPC1025_H__
+#define __CROS_FP_FPC1025_H__
+
+#include <drivers/cros_fingerprint.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+
+struct fpc1025_cfg {
+	struct spi_dt_spec spi;
+	struct gpio_dt_spec interrupt;
+	struct gpio_dt_spec reset_pin;
+	struct fingerprint_info info;
+};
+
+struct fpc1025_data {
+	fingerprint_callback_t callback;
+	struct gpio_callback irq_cb;
+	uint16_t errors;
+};
+
+#endif /* __CROS_FP_FPC1025_H__ */
