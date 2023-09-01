661190e26407225f54e1077e2019e0177e0c9d2a
Bobby Casey <bobbycasey@google.com>
2023-09-01 11:24:46 -0400

    run_device_tests: Fix bug mapping build_board to board_config
    A bug was introduced in https://crrev.com/c/4775559 when trying to
    obtain the build_config from build_board when using a variant
    build_board (nocturne_fp or nami_fp). Resolve this by search variants
    for the correct build_board if needed.
    
    BUG=none
    TEST=run_device_tests.py --board bloonchipper \
                   -t unaligned_access_nami_fp_v2.2.144
    TEST=run_device_tests.py --board bloonchipper \
                   -t unaligned_access_nocturne_fp_v2.2.64
    
    Change-Id: I74c9a98dcb4fad938d6d93f9fedaad8d8dd27cfe

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -486,6 +486,29 @@ BOARD_CONFIGS = {
 }
 
 
+def get_board_config(build_board: str) -> BoardConfig:
+    """
+    Returns the BOARD_CONFIG entry for the given build_board. If build_board is
+    one of the standard boards then just return the config from BOARD_CONFIGS.
+    Otherwise search through the build variants for a matching `build_board`.
+    """
+
+    try:
+        return BOARD_CONFIGS[build_board]
+    except KeyError as board_config_key_error:
+        logging.debug(
+            "build_board %s not found in BOARD_CONFIGS, checking variants",
+            board_config_key_error,
+        )
+
+    for board_config in BOARD_CONFIGS.values():
+        variants_info = board_config.variants.values()
+        if any(v.get("build_board") == build_board for v in variants_info):
+            return board_config
+
+    return None
+
+
 def read_file_gsutil(path: str) -> bytes:
     """Get data from bucket, using gsutil tool"""
     cmd = ["gsutil", "cat", path]
@@ -730,7 +753,13 @@ def run_test(
     """Run specified test."""
     start = time.time()
 
-    board_config = BOARD_CONFIGS[build_board]
+    board_config = get_board_config(build_board)
+    logging.debug(
+        "Mapped build board %s to BoardConfig %s",
+        build_board,
+        board_config.name,
+    )
+
     logging.debug("Calling pre-test callback")
     if not test.pre_test_callback(board_config):
         logging.error("pre-test callback failed, aborting")
