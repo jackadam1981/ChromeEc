62fd0d578def36adcf7489d83858352dd6289c00
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2023-09-01 10:56:27 +0800

    Nissa/tests: Add specific test for Craask form_factor and fan
    Setting bmi3xx_interrupt, lsm6dso_interrupt, bma4xx_intererupt
    and lis2dw12_interrupt test mockable lets the int_imu interrupt
    test easily in clamshell/convertible and alt sensor tests.
    
    Move clamshell/convertible checking code to clamshell_init.
    The hook priority of clamshell_init is HOOK_PRIO_POST_DEFAULT
    that it will be called after sensor_enable_irqs which enabled
    the interrupt int_imu.
    
    Move alt sensor checking code to alt_sensor_init to make tests
    simplier.
    
    test_base_orientation/test_lid_orientation: test the orientation
    of base/lid sensor.
    
    test_clamshell/test_convertible: test the tablet mode,
    tablet mode interrupt, and imu interrupt should disable/enable
    for clamshell/convertible sku.
    
    test_alt_sensor_base_bmi323/lsm6dso and test_alt_sensor_lid_bma422/
    lis2dw12: test the alternative sensor by checking which interrupt
    function called.
    
    test_fan_present/absent: test fan_set_count and gpio_fan_enable
    configuration correctly when fan is present/absent.
    
    BUG=b:297981597
    TEST=./twister -T zephyr/test/nissa/ -v -i -s nissa.craask
         ./twister --coverage -v -i -s nissa/nissa.craask
    
    Change-Id: Ie43334809420261c2ee7efd32f3cbf0f69946105

--- zephyr/program/nissa/craask/src/form_factor.c
+++ zephyr/program/nissa/craask/src/form_factor.c
@@ -12,7 +12,6 @@
 #include "driver/accelgyro_lsm6dso.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
-#include "motion_sense.h"
 #include "motionsense_sensors.h"
 #include "tablet_mode.h"
 
@@ -52,7 +51,7 @@ void lid_accel_interrupt(enum gpio_signal signal)
 		lis2dw12_interrupt(signal);
 }
 
-static void form_factor_init(void)
+test_export_static void form_factor_init(void)
 {
 	int ret;
 	uint32_t val;
@@ -62,7 +61,6 @@ static void form_factor_init(void)
 		LOG_ERR("Error retrieving CBI BOARD_VER.");
 		return;
 	}
-
 	/*
 	 * If the board version is 1
 	 * use ver1 rotation matrix.
@@ -89,7 +87,11 @@ static void form_factor_init(void)
 		motion_sensors[LID_SENSOR].rot_standard_ref = &LIS_ALT_MAT;
 		motion_sensors_alt[ALT_LID_S].rot_standard_ref = &BMA_ALT_MAT;
 	}
+}
+DECLARE_HOOK(HOOK_INIT, form_factor_init, HOOK_PRIO_POST_I2C);
 
+test_export_static void alt_sensor_init(void)
+{
 	/* check which motion sensors are used */
 	use_alt_sensor = cros_cbi_ssfc_check_match(
 		CBI_SSFC_VALUE_ID(DT_NODELABEL(base_sensor_1)));
@@ -97,6 +99,13 @@ static void form_factor_init(void)
 		CBI_SSFC_VALUE_ID(DT_NODELABEL(lid_sensor_1)));
 
 	motion_sensors_check_ssfc();
+}
+DECLARE_HOOK(HOOK_INIT, alt_sensor_init, HOOK_PRIO_POST_I2C);
+
+test_export_static void clamshell_init(void)
+{
+	int ret;
+	uint32_t val;
 
 	/* Check if it's clamshell or convertible */
 	ret = cros_cbi_get_fw_config(FORM_FACTOR, &val);
@@ -110,7 +119,7 @@ static void form_factor_init(void)
 		gmr_tablet_switch_disable();
 		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
 		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
-				      GPIO_DISCONNECTED);
+				      GPIO_INPUT | GPIO_PULL_UP);
 	}
 }
-DECLARE_HOOK(HOOK_INIT, form_factor_init, HOOK_PRIO_POST_I2C);
+DECLARE_HOOK(HOOK_INIT, clamshell_init, HOOK_PRIO_POST_DEFAULT);
