7ff438f94706be404cdcd23705bbc87689b8ecb2
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-09-05 11:25:28 -0700

    intelrvp: Handle ports without PPC
    MTL-RVP uses combination of ports with integrated source FET and
    external PPC. To support the shimmed interrupt handler introduced by
    CL:4697836, check if the port is PPC port before calling the common
    code.
    
    BUG=b:300427558
    TEST=PD charger, USB, DP, TBT are detected on MTLRVP
    LOW_COVERAGE_REASON=Board level change, b:300427558
    
    Change-Id: I071b9d4a92faa518f8f7ef40992a3941423007d4

--- zephyr/program/intelrvp/include/intelrvp.h
+++ zephyr/program/intelrvp/include/intelrvp.h
@@ -19,10 +19,6 @@ FORWARD_DECLARE_ENUM(tcpc_rp_value);
 struct tcpc_aic_gpio_config_t {
 	/* TCPC interrupt */
 	enum gpio_signal tcpc_alert;
-	/* PPC interrupt */
-	enum gpio_signal ppc_alert;
-	/* PPC interrupt handler */
-	void (*ppc_intr_handler)(int port);
 };
 extern const struct tcpc_aic_gpio_config_t tcpc_aic_gpios[];
 
