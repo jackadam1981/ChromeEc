1da9eba22b01413d43d6d0667791c31f1558b8a1
Wai-Hong Tam <waihong@google.com>
2023-09-06 14:25:26 -0700

    ectool: Support 32-bit I2C data read and write
    Currently, only 8-bit and 16-bit data r/w are supported.
    32-bit data r/w are common too and should be supported.
    
    BUG=b:299223702
    TEST=Run the following commands on Lazor
    lazor-rev3 ~ # ectool i2cread 32 1 0x10 0
    Read from I2C port 1 at 0x10 offset 0x0 = 0x200000d5
    lazor-rev3 ~ # ectool i2cwrite 32 1 0x10 0 0x200000d4
    Wrote 0x200000d4 to I2C port 1 at 0x10 offset 0x0.
    lazor-rev3 ~ # ectool i2cread 32 1 0x10 0
    Read from I2C port 1 at 0x10 offset 0x0 = 0x200000d4
    lazor-rev3 ~ # ectool i2cxfer 1 0x8 4 0
    Read bytes: 0xd4 00 00 0x20
    
    Change-Id: I2a26f95dee1b8213ce06f4f9145e2174f26e4bea

--- util/ectool_i2c.cc
+++ util/ectool_i2c.cc
@@ -121,9 +121,9 @@ static int do_i2c_xfer(unsigned int port, unsigned int addr, uint8_t *write_buf,
 static void cmd_i2c_help(void)
 {
 	fprintf(stderr,
-		"  Usage: i2cread <8 | 16> <port> <addr8> <offset>\n"
+		"  Usage: i2cread <8|16|32> <port> <addr8> <offset>\n"
 		"  Usage: i2cspeed <port> [speed in kHz]\n"
-		"  Usage: i2cwrite <8 | 16> <port> <addr8> <offset> <data>\n"
+		"  Usage: i2cwrite <8|16|32> <port> <addr8> <offset> <data>\n"
 		"  Usage: i2cxfer <port> <addr7> <read_count> [bytes...]\n"
 		"    <port> i2c port number\n"
 		"    <addr8> 8-bit i2c address\n"
@@ -149,7 +149,7 @@ int cmd_i2c_read(int argc, char *argv[])
 	}
 
 	read_len = strtol(argv[1], &e, 0);
-	if ((e && *e) || (read_len != 8 && read_len != 16)) {
+	if ((e && *e) || (read_len != 8 && read_len != 16 && read_len != 32)) {
 		fprintf(stderr, "Bad read size.\n");
 		return -1;
 	}
@@ -182,7 +182,7 @@ int cmd_i2c_read(int argc, char *argv[])
 		return rv;
 
 	printf("Read from I2C port %d at 0x%x offset 0x%x = 0x%x\n", port,
-	       addr8, write_buf[0], *(uint16_t *)read_buf);
+	       addr8, write_buf[0], *(uint32_t *)read_buf);
 	return 0;
 }
 
@@ -190,7 +190,7 @@ int cmd_i2c_write(int argc, char *argv[])
 {
 	unsigned int port, addr8, addr7;
 	int write_len;
-	uint8_t write_buf[3];
+	uint8_t write_buf[5];
 	char *e;
 	int rv;
 
@@ -200,7 +200,8 @@ int cmd_i2c_write(int argc, char *argv[])
 	}
 
 	write_len = strtol(argv[1], &e, 0);
-	if ((e && *e) || (write_len != 8 && write_len != 16)) {
+	if ((e && *e) ||
+	    (write_len != 8 && write_len != 16 && write_len != 32)) {
 		fprintf(stderr, "Bad write size.\n");
 		return -1;
 	}
@@ -226,7 +227,7 @@ int cmd_i2c_write(int argc, char *argv[])
 		return -1;
 	}
 
-	*((uint16_t *)&write_buf[1]) = strtol(argv[5], &e, 0);
+	*((uint32_t *)&write_buf[1]) = strtol(argv[5], &e, 0);
 	if (e && *e) {
 		fprintf(stderr, "Bad data.\n");
 		return -1;
@@ -238,7 +239,7 @@ int cmd_i2c_write(int argc, char *argv[])
 		return rv;
 
 	printf("Wrote 0x%x to I2C port %d at 0x%x offset 0x%x.\n",
-	       *((uint16_t *)&write_buf[1]), port, addr8, write_buf[0]);
+	       *((uint32_t *)&write_buf[1]), port, addr8, write_buf[0]);
 	return 0;
 }
 
