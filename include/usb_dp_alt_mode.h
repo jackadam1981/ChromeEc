6e4c5e03f7eceb7dcdd28f70305ff25c2233884f
Brandon Breitenstein <brandon.breitenstein@intel.com>
2023-09-07 14:56:42 -0700

    ec: DP Active cable support
    This implements the DP active cable support
    
    BUG=b:266823500
    TEST=verified SOP and SOP' messages through PD analyzer
    
    Change-Id: Id1471f02c943f2d1db2ee3bc846125e555e2d30f

--- include/usb_dp_alt_mode.h
+++ include/usb_dp_alt_mode.h
@@ -261,7 +261,8 @@ void dp_vdm_naked(int port, enum tcpci_msg_type type, uint8_t vdm_cmd);
  * @return		    enum dpm_msg_setup_status
  */
 enum dpm_msg_setup_status dp_setup_next_vdm(int port, int *vdo_count,
-					    uint32_t *vdm);
+					    uint32_t *vdm,
+					    enum tcpci_msg_type *tx_type);
 /*
  * Construct the vdo cfg message for the dp port
  *
@@ -302,7 +303,8 @@ static inline void dp_vdm_naked(int port, enum tcpci_msg_type type,
 }
 
 static inline enum dpm_msg_setup_status
-dp_setup_next_vdm(int port, int *vdo_count, uint32_t *vdm)
+dp_setup_next_vdm(int port, int *vdo_count, uint32_t *vdm,
+		  enum tcpci_msg_type *tx_type)
 {
 	return MSG_SETUP_ERROR;
 }
