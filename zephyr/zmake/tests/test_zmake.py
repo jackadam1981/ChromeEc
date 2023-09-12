a2efe87a3936838b3e08f951dcc28f51c680e349
Jeremy Bettis <jbettis@google.com>
2023-09-12 13:00:57 -0600

    zmake: Look in private/zephyr for projects also
    Change the project search code to look in both platform/ec/zephyr and
    platform/ec/private/zephyr. This will be helpful for projects that
    depend on private repos for some dependency.
    
    BUG=b:294235668
    TEST=zmake unit tests, zmake build -a
    
    Change-Id: Id1c6eabb1df1d7b950b5e73c47642dd80f6df500

--- zephyr/zmake/tests/test_zmake.py
+++ zephyr/zmake/tests/test_zmake.py
@@ -307,7 +307,7 @@ def test_list_projects(
         for name in project_names
     }
 
-    zmk = zmake_factory_from_dir(projects_dir=search_dir)
+    zmk = zmake_factory_from_dir(projects_dirs=[search_dir])
     with unittest.mock.patch(
         "zmake.project.find_projects",
         autospec=True,
