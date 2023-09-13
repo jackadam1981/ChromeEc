923c592b70ed32f3bfe6b3a896b76e6b4b6deede
Jeremy Bettis <jbettis@google.com>
2023-09-13 17:30:16 -0600

    zmake: Add ish/programs to projects_dirs
    Since there are now 3 dirs in the projects_dirs path, move the logic to
    a common function zmake.modules.default_projects_dirs().
    
    BUG=b:294235668
    TEST=zmake compare-builds -a
    
    Change-Id: I7cc07638d6a27e99354460cc1b5b9d78f8927d61

--- zephyr/zmake/zmake/modules.py
+++ zephyr/zmake/zmake/modules.py
@@ -100,3 +100,19 @@ def setup_module_symlinks(output_dir, modules):
             cmake_defs={"ZEPHYR_MODULES": ";".join(map(str, module_links))}
         )
     return build_config.BuildConfig()
+
+
+def default_projects_dirs(modules):
+    """Get the default search path for projects.
+
+    Args:
+        modules: A dictionary of module names mapping to paths.
+
+    Returns:
+        A list of paths that should be searched for projects, if they exist.
+    """
+    return [
+        modules["ec"] / "zephyr" / "program",
+        modules["ec"] / "private" / "zephyr" / "program",
+        modules["ish"] / "program",
+    ]
