ebd2c305d4ab833e7fb93af70a1e3dcbfe445c45
Daisuke Nojiri <dnojiri@chromium.org>
2023-09-13 11:14:11 -0700

    Keyboard: Make ksstate print boot keys
    This CL makes ksstate print boot keys.
    
    BUG=b:299186434
    TEST=Run ksstate command.
    
    Change-Id: Ida05a34490c49cbe1c311a29dc8f0468d39bcaa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4864200
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -1038,6 +1038,9 @@ static int command_ksstate(int argc, char **argv)
 	ccprintf("Keyboard scan disable mask: 0x%08x\n", disable_scanning_mask);
 	ccprintf("Keyboard scan state printing %s\n",
 		 print_state_changes ? "on" : "off");
+#ifdef CONFIG_KEYBOARD_BOOT_KEYS
+	ccprintf("boot keys: 0x%08x\n", boot_key_value);
+#endif
 	return EC_SUCCESS;
 }
 DECLARE_CONSOLE_COMMAND(ksstate, command_ksstate, "ksstate [on | off | force]",
