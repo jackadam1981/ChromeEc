3c9e876e84b954dff3655b5e262d0e9be6cd10da
Brandon Breitenstein <brandon.breitenstein@intel.com>
2023-09-14 17:24:30 +0000

    Revert "keyboard: Remove workaround for stuck KSI2"
    This reverts commit 0e5d8cab99326c45ebf85dcb7780a9fd20c5cb81.
    
    Reason for revert: This patch is breaking Recovery mode on Rex
    refer to b:300418915
    
    Original change's description:
    > keyboard: Remove workaround for stuck KSI2
    >
    > H1 used to assert KSI2 when a power button is held down. This bug has
    > been fixed. We no longer need to workaround this issue on the EC.
    >
    > BUG=b:35575102, b:299186434
    > TEST=Verified KSI2 isn't asserted on EC reset on Redrix.
    >
    > Change-Id: I9d027bedb36a4c493d127b9dc3b4b3162c8bbf28
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4846112
    > Reviewed-by: Keith Short <keithshort@chromium.org>
    > Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    > Auto-Submit: Daisuke Nojiri <dnojiri@chromium.org>
    > Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    > Commit-Queue: Keith Short <keithshort@chromium.org>
    > Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=b:35575102, b:299186434
    
    Change-Id: Ic1c63ee8e0149ac96d599b24ed8fdcab711d46f4

--- zephyr/test/drivers/keyboard_scan/src/keyboard_scan.c
+++ zephyr/test/drivers/keyboard_scan/src/keyboard_scan.c
@@ -28,7 +28,11 @@
 ZTEST(keyboard_scan, test_boot_key)
 {
 	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(cros_kb_raw));
+<<<<<<< HEAD   (06f010 Merge remote-tracking branch cros/main into firmware-nissa-1)
 	static const struct device *gpio_dev = GPIO_DEVICE;
+=======
+	const int kb_cols = DT_PROP(DT_NODELABEL(cros_kb_raw), cols);
+>>>>>>> CHANGE (13f7ad Revert "keyboard: Remove workaround for stuck KSI2")
 
 	emul_kb_raw_reset(dev);
 	zassert_equal(keyboard_scan_get_boot_keys(), BOOT_KEY_NONE, NULL);
@@ -44,7 +48,23 @@ ZTEST(keyboard_scan, test_boot_key)
 	keyboard_scan_init();
 	zassert_equal(keyboard_scan_get_boot_keys(), BIT(BOOT_KEY_ESC));
 
+<<<<<<< HEAD   (06f010 Merge remote-tracking branch cros/main into firmware-nissa-1)
 	/* Case 2: esc only -> BOOT_KEY_ESC */
+=======
+	/*
+	 * Case 1.5:
+	 * GSC may hold ksi2 when power button is pressed, simulate this
+	 * behavior and verify boot key detection again.
+	 */
+	zassert_true(IS_ENABLED(CONFIG_KEYBOARD_PWRBTN_ASSERTS_KSI2), NULL);
+	for (int i = 0; i < kb_cols; i++) {
+		zassert_ok(emulate_keystate(KEYBOARD_ROW_REFRESH, i, true));
+	}
+	keyboard_scan_init();
+	zassert_equal(keyboard_scan_get_boot_keys(), BOOT_KEY_ESC);
+
+	/* Case 2: esc only -> BOOT_KEY_NONE */
+>>>>>>> CHANGE (13f7ad Revert "keyboard: Remove workaround for stuck KSI2")
 	emul_kb_raw_reset(dev);
 	zassert_ok(emulate_keystate(KEYBOARD_ROW_ESC, KEYBOARD_COL_ESC, true));
 	keyboard_scan_init();
