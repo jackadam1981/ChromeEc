4aaf5b9af730c519a5e04792e34baa6599f461e4
Patryk Duda <pdk@semihalf.com>
2023-09-18 15:48:22 +0200

    zephyr: shim: Introduce shim layer for 'fpsensor' task
    BUG=b:299924239
    TEST=zmake build bloonchipper
    
    Change-Id: Ia6ac84f761a58ed6af39f1f75764a0ecaeb4d145

---
+++ zephyr/shim/src/fpsensor.c
@@ -0,0 +1,122 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <drivers/cros_fingerprint.h>
+#include <fingerprint/fingerprint_alg.h>
+#include <fpsensor.h>
+#include <fpsensor_detect.h>
+
+#if !DT_HAS_CHOSEN(cros_fp_fingerprint_sensor)
+#error "cros-fp,fingerprint-sensor device must be chosen"
+#else
+#define fp_sensor_dev DEVICE_DT_GET(DT_CHOSEN(cros_fp_fingerprint_sensor))
+#endif
+
+enum fp_sensor_type fpsensor_detect_get_type(void)
+{
+	enum fp_sensor_type ret;
+
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(div_highside), 1);
+	k_usleep(1);
+	switch (gpio_pin_get_dt(
+		GPIO_DT_FROM_NODELABEL(fp_sensor_sel))) {
+	case 0:
+		ret = FP_SENSOR_TYPE_ELAN;
+		break;
+	case 1:
+		ret = FP_SENSOR_TYPE_FPC;
+		break;
+	default:
+		ret = FP_SENSOR_TYPE_UNKNOWN;
+		break;
+	}
+	/* We leave GPIO_DIVIDER_HIGHSIDE enabled, since the dragonclaw
+	 * development board use it to enable the AND gate (U10) to CS.
+	 * Production boards could disable this to save power since it's
+	 * only needed for initial detection on those boards.
+	 */
+	return ret;
+}
+
+int fp_sensor_init(void)
+{
+	int rc;
+
+	rc = fingerprint_init(fp_sensor_dev);
+	if (rc) {
+		return rc;
+	}
+
+	rc = fingerprint_algorithm_init();
+	if (rc) {
+		return rc;
+	}
+
+	return 0;
+}
+
+int fp_sensor_deinit(void)
+{
+	int rc;
+
+	rc = fingerprint_algorithm_exit();
+	if (rc) {
+		return rc;
+	}
+
+	rc = fingerprint_deinit(fp_sensor_dev);
+	if (rc) {
+		return rc;
+	}
+
+	return 0;
+}
+
+int fp_sensor_get_info(struct ec_response_fp_info *resp)
+{
+	struct fingerprint_info info;
+	int rc;
+
+	rc = fingerprint_get_info(fp_sensor_dev, &info);
+	if (rc) {
+		return rc;
+	}
+
+	resp->vendor_id = info.vendor_id;
+	resp->product_id = info.product_id;
+	resp->model_id = info.model_id;
+	resp->version = info.version;
+	resp->frame_size = info.frame_size;
+	resp->pixel_format = info.pixel_format;
+	resp->width = info.width;
+	resp->height = info.height;
+	resp->bpp = info.bpp;
+	resp->errors = info.errors;
+
+	return 0;
+}
+
+int fp_enrollment_begin(void)
+{
+	return fingerprint_enroll_start();
+}
+
+int fp_enrollment_finish(void *templ)
+{
+	return fingerprint_enroll_finish(templ);
+}
+
+void fp_sensor_low_power(void)
+{
+	fingerprint_enter_low_power(fp_sensor_dev);
+}
+
+int fp_maintenance(void)
+{
+	uint8_t buffer[CONFIG_FP_SENSOR_IMAGE_SIZE];
+
+	return fingerprint_maintenance(fp_sensor_dev, buffer,
+		CONFIG_FP_SENSOR_IMAGE_SIZE);
+}
