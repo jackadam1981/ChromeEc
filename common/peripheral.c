132aec3aaa816152c4baf5f7ea1435aa82b218d2
Sam Hurst <shurst@google.com>
2023-09-20 13:16:22 -0700

    WIP: init for TCPMv3
    Change-Id: I43e0f37aa53523083208dde20c1f274de8983b71

--- common/peripheral.c
+++ common/peripheral.c
@@ -30,7 +30,7 @@ static enum ec_status hc_locate_chip(struct host_cmd_handler_args *args)
 #endif /* CONFIG_CBI_EEPROM */
 		break;
 	case EC_CHIP_TYPE_TCPC:
-#if defined(CONFIG_USB_POWER_DELIVERY) && \
+#if !defined(CONFIG_USB_PD_TCPMV3) && defined(CONFIG_USB_POWER_DELIVERY) && \
 	defined(CONFIG_USB_PD_PORT_MAX_COUNT) && !defined(CONFIG_USB_PD_TCPC)
 		if (params->index >= board_get_usb_pd_port_count())
 			return EC_RES_OVERFLOW;
