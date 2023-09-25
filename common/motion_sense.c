736e24cfce93cce92585e3a97d8dede5539ca276
Gwendal Grignou <gwendal@chromium.org>
2023-09-25 14:13:02 -0700

    motion_sense: Do not use stale data when checking forced mode
    When the EC transitions from RO to RW, the sensors are reset, an ODR
    request is sent, but otherwise, existing data are still present.
    
    In particular, the |collection_rate| in increment_sensor_collection() is
    still used, but during the sensor init() call, its odr field has been
    reset to 0.
    
    So we will assert in motion_sense_read() because ODR is 0 but
    |collection_rate| contains the stale value set by RO firmware.
    
    We would crash with:
    [2.896482 Lid Accel: MS Done Init type:0x0 range:2]
    [2.900263 SENSOR accelgyro_lsm6dsm WHO_AM_I value: 0x6a]
    [2.901431 Lid Accel Missed 12 data collections at 1938850 - rate: 80000]
    ASSERTION FAILURE
    'sensor->drv->get_data_rate(sensor) != 0' in motion_sense_read() at
    common/motion_sense.c:553
    
    The message "rate: 80000" is the stale collection_rate in us.
    The message "Missed 12 data collections" indicates we could not collect
    while transition to RW.
    
    Make sure we set the data needed for forced collection up to date before
    using it.
    
    BUG=b:295773669
    BRANCH=all
    TEST=compile.
    
    Change-Id: I8037206a4b6befd4e9d57cda1cc77b6e5a402caf

--- common/motion_sense.c
+++ common/motion_sense.c
@@ -646,6 +646,15 @@ static int motion_sense_process(struct motion_sensor_t *sensor, uint32_t *event,
 		if (ret == EC_SUCCESS)
 			has_data_read = 1;
 	}
+
+	/*
+	 * ODR change was requested: update the collection data rate,
+	 * we may miss a sample, but we won't use stale collection_rate.
+	 */
+	if (is_odr_pending) {
+		motion_sense_set_data_rate(sensor);
+	}
+
 	if (motion_sensor_in_forced_mode(sensor)) {
 		if (motion_sensor_time_to_read(ts, sensor)) {
 			/*
@@ -675,9 +684,8 @@ static int motion_sense_process(struct motion_sensor_t *sensor, uint32_t *event,
 		}
 	}
 
-	/* ODR change was requested. */
+	/* ODR change was requested, confirm change to AP, after flush.*/
 	if (is_odr_pending) {
-		motion_sense_set_data_rate(sensor);
 		if (IS_ENABLED(CONFIG_ACCEL_FIFO))
 			motion_sense_fifo_insert_async_event(sensor,
 							     ASYNC_EVENT_ODR);
