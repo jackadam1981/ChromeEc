01341c0547848d36691adbdea283e21191b2e845
Mark Hasemeyer <markhas@chromium.org>
2023-09-25 14:16:00 -0600

    npcx: Provide option to invert keyboard interrupts
    Due to limitations already baked into RO AP firmware, Guybrush based
    boards require an active edge/low interrupt to register key presses.
    See https://review.coreboot.org/c/coreboot/+/54924 for more information.
    
    Provide an option to invert the interrupts on NPCX based ECs.
    
    BUG=b:218874489
    TEST=CQ
    
    Change-Id: I7f622edf57d2f290661b91abdc0329fbab98b9f2

--- chip/npcx/lpc.c
+++ chip/npcx/lpc.c
@@ -872,8 +872,13 @@ static void lpc_init(void)
 	 * enable IBF core interrupt
 	 */
 	NPCX_HIPMCTL(PMC_ACPI) |= 0x81;
-	/* Normally Polarity IRQ1,12 type (level + high) setting */
+#ifdef CONFIG_NCPX_KBC_IRQ_ACTIVE_LOW
+	/* Inverted polarity on IRQ1 and IRQ12 (level + low) */
+	NPCX_HIIRQC = 0x40;
+#else
+	/* Normal polarity on IRQ1 and IRQ12 (level + high) */
 	NPCX_HIIRQC = 0x00;
+#endif
 
 	/*
 	 * Init PORT80
