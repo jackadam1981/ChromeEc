45925ec0e9d66701754932df4279ffdeda444aad
Li Feng <li1.feng@intel.com>
2023-09-25 23:10:58 -0700

    ish: use upstreamed ISH BSP
    ALL ISH BSP are upstreamed. Now ISH project doesn't need private repo.
    1. remove ish private repo from zmake
    2. made rex-ish config changes to pass build.
    
    BUG=b:281038970
    TEST=zmake build rex-ish; flash to Rex proto 2, ISH is running.
    
    Change-Id: I5db8f4858f37813f3cd76a1846d87eedea2e4544

--- zephyr/zmake/zmake/configlib.py
+++ zephyr/zmake/zmake/configlib.py
@@ -56,5 +56,5 @@ def register_ish_project(**kwargs):
     """Register a project that uses IshBinPacker."""
     kwargs.setdefault("supported_toolchains", ["coreboot-sdk", "zephyr"])
     kwargs.setdefault("output_packer", zmake.output_packers.IshBinPacker)
-    kwargs.setdefault("modules", ["ec", "ish", "cmsis", "hal_intel_public"])
+    kwargs.setdefault("modules", ["ec", "cmsis", "hal_intel_public"])
     return _register_project(**kwargs)
