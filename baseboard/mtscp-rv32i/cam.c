15b50aa1c033258a8fa63d89b1038131c66acae0
Harry Lai <harry.lai@mediatek.corp-partner.google.com>
2023-09-28 10:43:18 +0800

    scp: p2 scp migration
    test
    
    Change-Id: I9086c901ca9f19b10e88e9c4ccbfbf4dd1f22d31

--- baseboard/mtscp-rv32i/cam.c
+++ baseboard/mtscp-rv32i/cam.c
@@ -16,10 +16,14 @@
 
 /* Forwad declaration. */
 static struct consumer const event_cam_consumer;
+static struct consumer const event_img_consumer;
 static void event_cam_written(struct consumer const *consumer, size_t count);
+static void event_img_written(struct consumer const *consumer, size_t count);
 
 static struct queue const event_cam_queue = QUEUE_DIRECT(8,
 	struct cam_msg, null_producer, event_cam_consumer);
+static struct queue const event_img_queue = QUEUE_DIRECT(8,
+	struct cam_msg, null_producer, event_img_consumer);
 
 static struct consumer const event_cam_consumer = {
 	.queue = &event_cam_queue,
@@ -27,10 +31,20 @@ static struct consumer const event_cam_consumer = {
 		.written = event_cam_written,
 	}),
 };
+static struct consumer const event_img_consumer = {
+	.queue = &event_img_queue,
+	.ops = &((struct consumer_ops const) {
+		.written = event_img_written,
+	}),
+};
 
 /* Stub functions only provided by private overlays. */
 #ifndef HAVE_PRIVATE_MT_SCP_CORE1
 void ipi_cam_handler(void *data) {}
+void ipi_img_handler(void *data) {}
+int32_t startRED(void) {}
+void img_task_handler(void) {}
+bool img_task_working = false;
 #endif
 
 static void event_cam_written(struct consumer const *consumer, size_t count)
@@ -38,9 +52,15 @@ static void event_cam_written(struct consumer const *consumer, size_t count)
 	task_wake(TASK_ID_CAM_SERVICE);
 }
 
+static void event_img_written(struct consumer const *consumer, size_t count)
+{
+	task_wake(TASK_ID_IMG_SERVICE);
+}
+
 static void cam_ipi_handler(int id, void *data, uint32_t len)
 {
 	struct cam_msg rsv_msg;
+	int ret = 0;
 
 	if (!len)
 		return;
@@ -52,11 +72,37 @@ static void cam_ipi_handler(int id, void *data, uint32_t len)
 	 * If there is no other IPI handler touch this queue, we don't need to
 	 * interrupt_disable() or task_disable_irq().
 	 */
-	if (!queue_add_unit(&event_cam_queue, &rsv_msg))
-		CPRINTS("Could not send cam %d to the queue", id);
+	if (id == SCP_IPI_ISP_CMD || id == SCP_IPI_ISP_FRAME)
+		ret = queue_add_unit(&event_cam_queue, &rsv_msg);
+	else
+		ret = queue_add_unit(&event_img_queue, &rsv_msg);
+
+	if (!ret)
+		CPRINTS("Could not send ipi cmd %d to the queue", id);
 }
+
+/*static void img_ipi_handler(int id, void *data, uint32_t len)
+{
+	struct cam_msg rsv_msg;
+
+	if (!len)
+		return;
+
+	rsv_msg.id = id;
+	memcpy(rsv_msg.msg, data, MIN(len, sizeof(rsv_msg.msg)));
+
+	*
+	 * If there is no other IPI handler touch this queue, we don't need to
+	 * interrupt_disable() or task_disable_irq().
+	 *
+	if (!queue_add_unit(&event_img_queue, &rsv_msg))
+		CPRINTS("Could not send img %d to the queue", id);
+	CPRINTS("send img %d to the queue", id);
+}*/
+
 DECLARE_IPI(SCP_IPI_ISP_CMD, cam_ipi_handler, 0);
 DECLARE_IPI(SCP_IPI_ISP_FRAME, cam_ipi_handler, 0);
+DECLARE_IPI(SCP_IPI_ISP_IMG_CMD, cam_ipi_handler, 0);
 
 /* This function renames from cam_service_entry. */
 void cam_service_task(void *u)
@@ -78,4 +124,36 @@ void cam_service_task(void *u)
 		else
 			ipi_cam_handler(&rsv_msg);
 	}
+}
+
+void img_service_task(void *u)
+{
+	struct cam_msg rsv_msg;
+	size_t size;
+
+	while (1) {
+		/*
+		 * Queue unit is added in IPI handler, which is in ISR context.
+		 * Disable IRQ to prevent a clobbered queue.
+		 */
+		ipi_disable_irq();
+		size = queue_remove_unit(&event_img_queue, &rsv_msg);
+		ipi_enable_irq();
+
+		if (!size) {
+			if (img_task_working)
+				task_wake(TASK_ID_IMG_HANDLER);
+			else
+				task_wait_event(-1);
+
+		} else
+			ipi_img_handler(&rsv_msg);
+	}
+}
+
+void img_handler_task(void *u)
+{
+	CPRINTS("img_handler_task");
+	startRED();
+	img_task_handler();
 }
