2ae2e75eb768a7317388cd1ae71d43065dc7596a
Reka Norman <rekanorman@google.com>
2023-09-28 10:56:44 +1000

    [TESTONLY] improve cec bitbang timing on it83xx
    Change-Id: Ia434cda46c6501b4761b420f1f9313ed99e4dc92

--- chip/it83xx/cec_bitbang.c
+++ chip/it83xx/cec_bitbang.c
@@ -33,6 +33,9 @@ static timestamp_t prev_interrupt_time;
 /* Flag set when a transfer is initiated from the AP */
 static bool transfer_initiated;
 
+/* The capture edge we're waiting for */
+static enum cec_cap_edge expected_cap_edge;
+
 static int port_from_timer(enum ext_timer_sel ext_timer)
 {
 	int port;
@@ -86,27 +89,15 @@ void cec_tmr_cap_start(int port, enum cec_cap_edge edge, int timeout)
 	const struct bitbang_cec_config *drv_config =
 		cec_config[port].drv_config;
 
-	switch (edge) {
-	case CEC_CAP_EDGE_NONE:
-		gpio_disable_interrupt(drv_config->gpio_in);
-		break;
-	case CEC_CAP_EDGE_FALLING:
-		gpio_set_flags(drv_config->gpio_in, GPIO_INT_FALLING);
-		gpio_enable_interrupt(drv_config->gpio_in);
-		break;
-	case CEC_CAP_EDGE_RISING:
-		gpio_set_flags(drv_config->gpio_in, GPIO_INT_RISING);
-		gpio_enable_interrupt(drv_config->gpio_in);
-		break;
-	}
+	expected_cap_edge = edge;
 
 	if (timeout > 0) {
 		/*
 		 * Take into account the delay from when the interrupt occurs to
 		 * when we actually get here.
 		 */
-		int delay =
-			CEC_US_TO_TICKS(get_time().val - interrupt_time.val);
+		int delay = CEC_US_TO_TICKS(get_time().val -
+					    interrupt_time.val + 100);
 		int timer_count = timeout - delay;
 
 		/*
@@ -165,6 +156,12 @@ void cec_gpio_interrupt(enum gpio_signal signal)
 	int port = port_from_gpio_in(signal);
 
 	cec_update_interrupt_time(port);
+
+	int level = gpio_get_level(signal);
+	if (!((expected_cap_edge == CEC_CAP_EDGE_FALLING && level == 0) ||
+	      (expected_cap_edge == CEC_CAP_EDGE_RISING && level == 1)))
+		return;
+
 	cec_event_cap(port);
 }
 
@@ -180,10 +177,14 @@ void cec_trigger_send(int port)
 
 void cec_enable_timer(int port)
 {
+	const struct bitbang_cec_config *drv_config =
+		cec_config[port].drv_config;
+
 	/*
-	 * Nothing to do. Interrupts will be enabled as needed by
+	 * Enable gpio interrupts. Timer interrupts will be enabled as needed by
 	 * cec_tmr_cap_start().
 	 */
+	gpio_enable_interrupt(drv_config->gpio_in);
 }
 
 void cec_disable_timer(int port)
