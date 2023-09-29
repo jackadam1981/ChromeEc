3c0a148276845bb391afd55f93bad834b92ac680
Vijay Hiremath <vijay.p.hiremath@intel.com>
2023-09-29 13:37:44 -0700

    test i2c
    Change-Id: I6713d11b0959a92645ca98f1fe1d7cb035ed7265

--- zephyr/program/intelrvp/mtlrvp/src/pd.c
+++ zephyr/program/intelrvp/mtlrvp/src/pd.c
@@ -4,7 +4,26 @@
  */
 
 #include "common.h"
+#include "console.h"
+#include "i2c.h"
 
 void board_dc_jack_interrupt(enum gpio_signal signal)
 {
 }
+
+
+static int test_i2c(int argc, const char **argv)
+{
+	uint8_t w_buf[] = {0x1c, 0x10};
+	uint8_t r_buf[10] = { 0 };
+	int rv;
+
+	rv = i2c_xfer(0, 0x08, w_buf, 2, r_buf, 4);
+	cflush();
+	ccprintf("rv=%d, %x %x %x %x %x\n", rv, r_buf[0], r_buf[1], r_buf[2], r_buf[3], r_buf[4]);
+	cflush();
+
+	return 0;
+}
+DECLARE_CONSOLE_COMMAND(test, test_i2c, "<repeat_count> <sleep_ms>",
+			"Print battery info");
