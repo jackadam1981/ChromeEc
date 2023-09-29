939da2afb8b20949e945dbf1768c44ba77b45ba3
Patryk Duda <pdk@semihalf.com>
2023-09-29 11:55:20 +0200

    fpsensor: Change #include order in fpsensor_auth_crypto_stateful.cc
    This patch fixes following issue when compiling this file in Zephyr
    environment:
    
    In file included from /mnt/host/source/src/platform/ec/build/zephyr/bloonchipper/modules/ec/common/fpsensor/fpsensor_auth_crypto_stateful.cc:30:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/array:545:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/algorithm:1925:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/chrono:766:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/__chrono/convert_to_tm.h:20:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/__chrono/statically_widen.h:17:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/__format/concepts.h:17:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/__format/format_parse_context.h:16:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/string_view:1050:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/functional:527:
    In file included from /usr/armv7m-cros-eabi/usr/include/c++/v1/__functional/boyer_moore_searcher.h:26:
    /usr/armv7m-cros-eabi/usr/include/c++/v1/vector:855:3: error: expected ']'
      _LIBCPP_NORETURN _LIBCPP_HIDE_FROM_ABI
      ^
    /usr/armv7m-cros-eabi/usr/include/c++/v1/__config:436:32: note: expanded from macro '_LIBCPP_NORETURN'
        #define _LIBCPP_NORETURN [[noreturn]]
                                   ^
    /usr/lib64/clang/17/include/stdnoreturn.h:13:18: note: expanded from macro 'noreturn'
     #define noreturn _Noreturn
                     ^
    /usr/armv7m-cros-eabi/include/sys/cdefs.h:300:20: note: expanded from macro '_Noreturn'
     #define _Noreturn               [[noreturn]]
    
    The problem here is that 'noreturn' is defined in stdnoreturn.h which is
    included by some shim headers. Finally, after preprocessing [[noreturn]]
    becomes [[[[noreturn]]]] which is wrong.
    
    The problem was fixed by changing order of includes, so C++ headers
    don't see 'noreturn' definition. When porting FPMCU to Zephyr is
    completed, we will migrate to use Zephyr headers only.
    
    BUG=b:299924239
    TEST=make BOARD=bloonchipper
    
    Change-Id: Iac6941ffd4654629c7bdb235791417b54ba9d436

--- common/fpsensor/fpsensor_auth_crypto_stateful.cc
+++ common/fpsensor/fpsensor_auth_crypto_stateful.cc
@@ -22,12 +22,12 @@ extern "C" {
 #include "sha256.h"
 }
 
+#include <array>
+
 #include "fpsensor_auth_crypto.h"
 #include "fpsensor_crypto.h"
 #include "fpsensor_state_without_driver_info.h"
 #include "fpsensor_utils.h"
-
-#include <array>
 
 enum ec_error_list
 encrypt_data_in_place(uint16_t version,
