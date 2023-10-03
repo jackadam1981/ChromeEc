30ca3e8facf6c8c85b74d65b03838a6fe82f9dae
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2023-10-02 17:29:17 -0700

    emul: intel_pd_controller: Introduce device emulator
    This emulator is used to test features that require intel
    PD controller device. It reponds to I2C transations for
    accessing status and control regsiters. Interruptions are
    generated by GPIO.
    
    BUG=b:295924125
    TEST=./twister
    
    Change-Id: I1c362dbdcd94b89ceb796f444eacd3ff5702fb61

---
+++ zephyr/emul/include/emul_intel_pd_controller.h
@@ -0,0 +1,35 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __EMUL_INTEL_PD_CONTROLLER_H
+#define __EMUL_INTEL_PD_CONTROLLER_H
+
+#include <zephyr/drivers/emul.h>
+
+#include <drivers/intel_altmode.h>
+
+/**
+ * @brief Modify the value status register of emulated Intel PD Controller.
+ *
+ * @param emul The device emulator instance.
+ * @param status The value of status register.
+ *
+ * @return 0 on success
+ * @return -EINVAL if an invalid argument is provided
+ */
+int intel_pd_controller_emul_set_status(const struct emul *emul,
+					const union data_status_reg status);
+
+/**
+ * @brief Trigger emulated Intel PD Controller interruption.
+ *
+ * @param emul The device emulator instance.
+ *
+ * @return 0 on success
+ * @return -EINVAL if an invalid argument is provided
+ */
+int intel_pd_controller_emul_trigger_irq(const struct emul *emul);
+
+#endif /*__EMUL_INTEL_PD_CONTROLLER_H */
