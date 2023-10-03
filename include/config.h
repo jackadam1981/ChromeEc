20b5898116b12ab13a038c4fc51932f13366d6db
Usha P <usha.p@intel.com>
2023-10-03 15:31:27 +0530

    Revert "Reland "intelrvp: Remove TGLRVP & JSLRVP boards and MECC0.9""
    This reverts commit 8ccd9ad21a9b5b34b64cc23cf7eecca296fe663a.
    
    Change-Id: I43a26e05cef355512431f50db66f792ff1dd716c

--- include/config.h
+++ include/config.h
@@ -5357,6 +5357,7 @@
  * Intel Reference Validation Platform's (RVP) Modular Embedded Control
  * Card (MECC) versions
  */
+#undef CONFIG_INTEL_RVP_MECC_VERSION_0_9
 #undef CONFIG_INTEL_RVP_MECC_VERSION_1_0
 #undef CONFIG_INTEL_RVP_MECC_VERSION_1_1
 
