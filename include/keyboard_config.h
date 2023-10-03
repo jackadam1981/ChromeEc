b8635e047e1ac23cdd1fd3f2ae727902466c9282
Daisuke Nojiri <dnojiri@chromium.org>
2023-10-03 13:27:59 -0700

    Keyboard: Cancel pass-through rows while power button is pressed
    While the power button is pressed, Ti50 drives KSO2 to read a refresh
    key state but it does not mask KSI2 (or KSI3) forwarded to the EC.
    This makes the EC see KSI2 asserted on any column it scans.
    
    With this CL, the keyboard scanner will clear the KSI2 bit if it sees
    the PB is pressed. If the power button state changes before and after
    reading a column, it re-scans the column. This is expected to work as a
    debouncer.
    
    This is a workaround. When Ti50 with crrev/i/4896707 reverted is
    released, we should revert this workaround as well.
    
    BUG=b:300418915,b:35575102
    TEST=On Rex, type keyboard normally.
    TEST=On Rex, press refresh+power to register refresh key as a boot key.
    TEST=On Rex, press refresh+power+esc to enter recovery mode.
    LOW_COVERAGE_REASON=False positives. See comments on Gerrit.
    
    Change-Id: I547079adc99dcb7e43d2652ff20dd7d38ead1d9d

--- include/keyboard_config.h
+++ include/keyboard_config.h
@@ -90,4 +90,15 @@ extern uint8_t keyboard_cols;
 #define KEYBOARD_MASK_LEFT_SHIFT KEYBOARD_ROW_TO_MASK(KEYBOARD_ROW_LEFT_SHIFT)
 
 #endif /* CONFIG_KEYBOARD_CUSTOMIZATION */
+
+/*
+ * GSC may not mask pass-through rows even when the power button is pressed.
+ * Keyscan will clear the corresponding rows.
+ */
+#ifdef CONFIG_KSI0_NOT_MASKED_BY_GSC
+#define KEYBOARD_POWERBTN_MASK (KEYBOARD_MASK_REFRESH | KEYBOARD_ROW_TO_MASK(0))
+#else
+#define KEYBOARD_POWERBTN_MASK KEYBOARD_MASK_REFRESH
+#endif
+
 #endif /* __CROS_EC_KEYBOARD_CONFIG_H */
