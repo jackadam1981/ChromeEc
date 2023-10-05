86daedf9771e222078e4912b69a19f400076e4fd
Ting Shen <phoenixshen@google.com>
2023-10-05 18:28:51 +0800

    usb_updater: make usb updater compatible with zephyr toolchain
    Minor change to make the usb updater module compiles on Zephyr:
    
    1) Use builtin/endian.h instead of byteorder.h, which is not implemented
       in Zephyr's minimal libc
    
    2) Add explicit type casts to some expressions that references to
       `CONFIG_PROGRAM_MEMORY_BASE`, due to the type of integer literal
       generated by device tree macros may different than expected.
    
       This is only for suppressing the compile warnings, should be no
       functional change except the CPRINT line.
    
       --
    
       Dirty details below about the type of this config value:
    
       When `CONFIG_PROGRAM_MEMORY_BASE` greater or equal to 2^31, the device
       tree macro generates a decimal literal (e.g. 2147483648), which has
       type `long long` according to the C language spec.
       Ref: https://en.cppreference.com/w/c/language/integer_constant
    
       In legacy EC boards, we usually write the same number as a hex literal
       0x80000000, which has type `unsigned int` under the same conversion
       rule. The different type causes compile warning.
    
    BUG=b:269989432
    TEST=1) With CL:4609653, verifies the code compiles on a Zephyr board.
         2) make buildall
    
    Change-Id: If73956b57b4813b5fb8eaba61cb9f7baaca4066a

--- common/update_fw.c
+++ common/update_fw.c
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-#include "byteorder.h"
+#include "builtin/endian.h"
 #include "console.h"
 #include "flash.h"
 #include "hooks.h"
@@ -293,7 +293,8 @@ void fw_update_command_handler(void *body, size_t cmd_size,
 	}
 #endif
 
-	CPRINTF("update: 0x%x\n", block_offset + CONFIG_PROGRAM_MEMORY_BASE);
+	CPRINTF("update: 0x%llx\n",
+		(uint64_t)(block_offset + CONFIG_PROGRAM_MEMORY_BASE));
 	if (crec_flash_physical_write(block_offset, body_size, update_data) !=
 	    EC_SUCCESS) {
 		*error_code = UPDATE_WRITE_FAILURE;
@@ -305,7 +306,8 @@ void fw_update_command_handler(void *body, size_t cmd_size,
 
 	/* Verify that data was written properly. */
 	if (memcmp(update_data,
-		   (void *)(block_offset + CONFIG_PROGRAM_MEMORY_BASE),
+		   (void *)(uintptr_t)(block_offset +
+				       CONFIG_PROGRAM_MEMORY_BASE),
 		   body_size)) {
 		*error_code = UPDATE_VERIFY_ERROR;
 		CPRINTF("%s:%d update verification error\n", __func__,
--- common/usb_update.c
+++ common/usb_update.c
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-#include "byteorder.h"
+#include "builtin/endian.h"
 #include "common.h"
 #include "console.h"
 #include "consumer.h"
@@ -49,7 +49,7 @@ struct usb_stream_config const usb_update;
 
 static struct queue const update_to_usb =
 	QUEUE_DIRECT(64, uint8_t, null_producer, usb_update.consumer);
-static struct queue const usb_to_update =
+struct queue const usb_to_update =
 	QUEUE_DIRECT(64, uint8_t, usb_update.producer, update_consumer);
 
 USB_STREAM_CONFIG_FULL(usb_update, USB_IFACE_UPDATE, USB_CLASS_VENDOR_SPEC,
