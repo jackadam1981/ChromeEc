f29a65fdd8b0e386d509aac53c32b34fbea68097
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2023-10-06 13:26:26 +0530

    intelrvp: handle flash access during syjump
    Firmware of EC & AP shares the shared external SPI flash.
    EC and AP SPI lines are separated by SPI isolation FET's
    on RVP. They are controlled by EC using gpio "ec_spi_oe_mecc".
    
    EC access SPI flash before "sysjump" and releases post sysjump.
    
    BUG=b:289783489
    BRANCH=none
    TEST=Build i.e zmake build mtlrvpp_mchp successful.
    LOW_COVERAGE_REASON=EC boot from external SPI flash is not POR.
    
    Change-Id: I210aab2fb9f7fde9c9e131d16ee267e890b9e66d
    Signed-off-by: Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>

--- zephyr/program/intelrvp/src/intelrvp.c
+++ zephyr/program/intelrvp/src/intelrvp.c
@@ -7,12 +7,33 @@
 #include "gpio.h"
 #include "hooks.h"
 
+#define DT_DRV_COMPAT cros_ec_shared_spi_flash
+
+BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) <= 1,
+	     "Unsupported External SPI GPIO");
+
+#define SHARED_SPI_NODE DT_PATH(shared_spi)
+
+const struct gpio_dt_spec ec_spi_oe_mecc =
+			GPIO_DT_SPEC_GET(SHARED_SPI_NODE, spi_oe_gpios);
+
+#ifdef CONFIG_PLATFORM_EC_SHARED_SPI_FLASH
+static void ec_get_external_spi_access(void)
+{
+	/* EC to get access to SPI flash  */
+	gpio_pin_set_dt(&ec_spi_oe_mecc, 0);
+	/* delay before EC access the external SPI flash */
+	k_msleep(10);
+}
+DECLARE_HOOK(HOOK_SYSJUMP, ec_get_external_spi_access, HOOK_PRIO_FIRST);
+#endif
+
 static void board_init(void)
 {
 	/* Enable SOC SPI */
-	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(ec_spi_oe_mecc), 1);
+	gpio_pin_set_dt(&ec_spi_oe_mecc, 1);
 }
-DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_LAST);
+DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_FIRST);
 
 __override void intel_x86_sys_reset_delay(void)
 {
