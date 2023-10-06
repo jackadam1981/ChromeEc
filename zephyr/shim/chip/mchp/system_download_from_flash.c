6ff4f6550f23718e1e4ed725391ceeadbf7994e0
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2023-10-06 14:04:40 +0530

    MCHP: changes to support sysjump
    After poweron, changes supports download of EC RW or RO image from
    external SPI flash chip to EC internal SRAM.
    EC FW (512 kb) is placed at an offset of 0x4000 in external SPI flash.
    
    changes include additional configurations in existing API
    "system_download_from_flash" to support download of the EC RW/RO image
    from extrrnal flash.
    
    BUG=b:289783489
    BRANCH=main
    TEST=Build i.e zmake build mtlrvpp_mchp, successful.
    LOW_COVERAGE_REASON=EC boot from external SPI is not POR.
    
    Change-Id: I444b9d713ceb8822ba1534888388d52020ee8e32
    Signed-off-by: Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>

--- zephyr/shim/chip/mchp/system_download_from_flash.c
+++ zephyr/shim/chip/mchp/system_download_from_flash.c
@@ -3,6 +3,7 @@
  * found in the LICENSE file.
  */
 #include "common.h"
+#include "flash.h"
 #include "soc.h"
 #include "system_chip.h"
 
@@ -90,11 +91,22 @@ void system_download_from_flash(uint32_t srcAddr, uint32_t dstAddr,
 
 	/* Check valid address for jumpiing */
 	__ASSERT_NO_MSG(exeAddr != 0x0);
+
+#ifdef CONFIG_FLASH_EX_OP_ENABLED
+	/* flash registers reset before starting DMA */
+	crec_flash_reset();
+#endif
+
 	/* Configure QMSPI controller */
 	qspi->MODE = MCHP_QMSPI_M_SRST;
 	fdiv = 2;
 	if (pcr->TURBO_CLK & MCHP_PCR_TURBO_CLK_96M)
 		fdiv *= 2;
+
+#ifdef CONFIG_PLATFORM_EC_SHARED_SPI_FLASH
+	/* value depends on the spi length from ec to flashchip */
+	fdiv = 6;
+#endif
 
 	qspi->MODE = (fdiv << MCHP_QMSPI_M_FDIV_POS) & MCHP_QMSPI_M_FDIV_MASK;
 	qspi->MODE |= (MCHP_QMSPI_M_ACTIVATE | MCHP_QMSPI_M_LDMA_RX_EN);
