da615bb0d04169f219000b4cca32377ed0c099fb
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2023-10-06 14:37:52 +0530

    zephyr/shim: reset external shared SPI flash
    EC, AP shares the same external SPI flash.
    To support download of RO/RW copy from external SPI to the EC
    internal SRAM, EC sends spi flash register reset command to the
    flash chip. Flash device will return to its default power on
    state.
    
    BUG=b:289783489
    BRANCH=none
    TEST=zmake build mtlrvpp_mchp, successful.
    LOW_COVERAGE_REASON=EC boot from external SPI flash is not POR.
    
    Change-Id: Ia7d59add1ca302bd9a6c423460f59794f145ed54
    Signed-off-by: Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>

--- include/flash.h
+++ include/flash.h
@@ -421,6 +421,14 @@ const char *crec_flash_read_pstate_mac_addr(void);
  */
 int crec_flash_write_pstate_mac_addr(const char *mac_addr);
 
+#ifdef CONFIG_PLATFORM_EC_USE_ZEPHYR_FLASH_RESET_REGISTERS
+/**
+ * Flash device register's reset.
+ *
+ */
+void crec_flash_register_reset(void);
+#endif
+
 /**
  * Lock or unlock HW necessary for mapped storage read.
  *
