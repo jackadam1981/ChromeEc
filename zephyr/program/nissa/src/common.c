2934d244b265f107f5745f458af302e92ca85ae7
Ridden_Liu <ridden_liu@compal.corp-partner.google.com>
2023-10-06 16:25:18 +0800

    Craaskov:Modify GPIO setting
    Modify GPIO for new setting based
    on JH2GN_Craaskov_ADN_15w_GPIO_Table_0923
    
    BUG=b:289962132
    TEST=zmake build craaskov
    
    Change-Id: I8196e12e281abeaf237c02e9fbd2015006389067

--- zephyr/program/nissa/src/common.c
+++ zephyr/program/nissa/src/common.c
@@ -21,6 +21,7 @@ LOG_MODULE_REGISTER(nissa, CONFIG_NISSA_LOG_LEVEL);
 __overridable void board_power_change(struct ap_power_ev_callback *cb,
 				      struct ap_power_ev_data data)
 {
+#if CONFIG_NISSA_BOARD_HAS_PEN
 	/*
 	 * Enable power to pen garage when system is active (safe even if no
 	 * pen is present).
@@ -38,6 +39,7 @@ __overridable void board_power_change(struct ap_power_ev_callback *cb,
 	default:
 		break;
 	}
+#endif
 }
 
 static void board_setup_init(void)
