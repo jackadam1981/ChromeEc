226997e6b4ac716863a8d85893053c6994841f4f
David Cross <davidmcross@google.com>
2023-10-09 16:28:47 -0700

    helipilot: update mpu unit test, associated board config
    BUG=b:304369464,b:286232426
    TEST=run_device_tests.py -b helipilot -t mpu_ro/mpu_rw
    --flasher=servo_micro
    
    Change-Id: Ib451905703488118ffc70a5a4edf0c51ef559016

--- board/helipilot/board.h
+++ board/helipilot/board.h
@@ -78,7 +78,7 @@
 /*
  * Define Data RAM size  = 160KB - 4KB (Reserved for booter).
  */
-#define CONFIG_DATA_RAM_SIZE ((96 + 64) * 1024 - 0x1000)
+#define CONFIG_DATA_RAM_SIZE ((96 + 64) * 1024)
 /*-------------------------------------------------------------------------*/
 
 #define CONFIG_SHAREDLIB_SIZE 0
@@ -88,6 +88,9 @@
 /* Need to account for the 64 (0x40) byte long firmware header */
 #define CONFIG_RO_STORAGE_OFF 64
 #define CONFIG_RO_SIZE (128 * 1024 - 0x1000)
+
+#undef CONFIG_CODE_RAM_SIZE
+#define CONFIG_CODE_RAM_SIZE (128 * 1024)
 
 #define CONFIG_RO_PUBKEY_READ_ADDR                                      \
 	(CONFIG_MAPPED_STORAGE_BASE + CONFIG_EC_PROTECTED_STORAGE_OFF + \
