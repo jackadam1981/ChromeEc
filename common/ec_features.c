f910ec4911537195609e1fc358a1832c437cde9e
Patryk Duda <pdk@semihalf.com>
2023-10-11 13:11:24 +0000

    common: Expose fingerprint feature when FPSENSOR task is present
    The fingerprint feature was controller by CONFIG_SPI_FP_PORT which is
    not available in Zephyr builds. We should expose fingerprint
    functionality when HAS_TASK_FPSENSOR is defined. It will work for both
    EC and Zephyr.
    
    BUG=b:299924239
    TEST=Build bloonchipper EC firmware. Flash it on DUT. Make sure cros_fp
         device is created.
    
    Change-Id: I795f2cbe6c188d8bef1ee88e7518c09d4f8712e4

--- common/ec_features.c
+++ common/ec_features.c
@@ -97,7 +97,7 @@ uint32_t get_feature_flags0(void)
 #ifdef CONFIG_HOSTCMD_RTC
 			  | EC_FEATURE_MASK_0(EC_FEATURE_RTC)
 #endif
-#ifdef CONFIG_SPI_FP_PORT
+#ifdef HAS_TASK_FPSENSOR
 			  | EC_FEATURE_MASK_0(EC_FEATURE_FINGERPRINT)
 #endif
 #ifdef HAS_TASK_CENTROIDING
