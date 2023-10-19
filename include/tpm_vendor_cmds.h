dc27099fac1daf1549ac07f6f8f8097b103928cd
Brian Granaghan <granaghan@google.com>
2023-10-19 22:46:39 +0000

    gsctool: Add Ti50 scheduling metrics.
    Adds additional metrics for how many times drivers are returned busy,
    how many times processes have exceeded their timeslice, and measures the
    crypto initialization time.
    
    BUG=b:286279214
    TEST=granaghan@granaghan-glaptop:~/ti50_images$ ./gsctool -DW
    finding_device 18d1:504a
    Found device.
    found interface 3 endpoint 4, chunk_len 64
    READY
    -------
    fs_init_time:          23
    fs_usage:              6264
    aprov_time:            62
    expanded_aprov_status: E000000
    rdd_keepalive:         1
    rdd_keepalive_at_boot: 0
    ccd_mode:              1
    filesystem_busy_count: 7
    crypto_busy_count:     0
    dispatcher_busy_count: 0
    timeslices_expired:    0
    crypto_init_time:      0
    
    Change-Id: Idbb5ff9e8c94f25fab1fbe28301de92b9d7c2e43

--- include/tpm_vendor_cmds.h
+++ include/tpm_vendor_cmds.h
@@ -370,6 +370,21 @@ struct ti50_stats_v1 {
 	uint32_t misc_status;
 };
 
+/*
+ * Keep in sync with
+ * ti50/common/applications/sys_mgr/src/tpm_vendor/metrics.rs
+ */
+struct ti50_stats_v2 {
+	struct ti50_stats_v0 v0;
+	struct ti50_stats_v1 v1;
+	uint32_t version;
+	uint32_t filesystem_busy_count;
+	uint32_t crypto_busy_count;
+	uint32_t dispatcher_busy_count;
+	uint32_t timeslices_expired;
+	uint32_t crypto_init_time;
+};
+
 #define METRICSV_BITS_USED_SHIFT	      27
 #define METRICSV_RDD_KEEP_ALIVE_MASK	      3
 #define METRICSV_RDD_KEEP_ALIVE_AT_BOOT_SHIFT 2
