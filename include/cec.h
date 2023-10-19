525e38088f7c8cd80a00604914d0a93f6aaee8cf
Reka Norman <rekanorman@google.com>
2023-10-19 15:05:15 +1100

    cec: Move some bit timing definitions to cec.h
    BUG=b:287558802
    TEST=None
    
    Change-Id: I8e9ea5455b1dba9c6d68f7636112e879dbf6e5ff

--- include/cec.h
+++ include/cec.h
@@ -48,6 +48,12 @@
 
 /* Bit timing */
 #define CEC_NOMINAL_BIT_PERIOD_US 2400
+#define CEC_START_BIT_LOW_US 3700
+#define CEC_START_BIT_HIGH_US 800
+#define CEC_DATA_ZERO_LOW_US 1500
+#define CEC_DATA_ZERO_HIGH_US 900
+#define CEC_DATA_ONE_LOW_US 600
+#define CEC_DATA_ONE_HIGH_US 1800
 
 /* Free time timing */
 /* Resend */
