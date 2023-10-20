44cb1a5b1d0c9e828be535f45873f8b681c961fe
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2023-10-20 11:38:05 -0700

    [WIP] Add Power Path handling for debugging
    Change-Id: I2c9fe9b67521cfbbec9bee17819a0ec6186f15f0

--- zephyr/drivers/usbc/intel_altmode.c
+++ zephyr/drivers/usbc/intel_altmode.c
@@ -16,6 +16,8 @@
 #include <zephyr/logging/log.h>
 
 #include <drivers/intel_altmode.h>
+#include <ap_power/ap_power.h>
+#include "i2c.h"
 
 #define DT_DRV_COMPAT intel_pd_altmode
 
@@ -136,6 +138,150 @@ static void pd_altmode_isr_work(struct k_work *item)
 	}
 }
 
+typedef uint8_t* rt_smbus_cmd;
+
+#define RT_SMBUS_CMD_RETRIES           10
+#define RT_SMBUS_CMD_LEN(cmd)          cmd[1]
+#define RT_SMBUS_CMD_RESULT(ping)      (ping & 3)
+#define RT_SMBUS_CMD_RESULT_LEN(ping)  ((ping >> 2) & 0x3F)
+
+static int rt_smbus_send_cmd(rt_smbus_cmd cmd, uint8_t *resp_len)
+{
+	uint8_t ping;
+	int ping_retries;
+	int rv;
+
+	i2c_lock(I2C_PORT_TYPEC_AIC_1, 1);
+	rv = i2c_xfer_unlocked(I2C_PORT_TYPEC_AIC_1, 0x69, cmd,
+				RT_SMBUS_CMD_LEN(cmd) + 2, NULL, 0,
+				I2C_XFER_SINGLE);
+	i2c_lock(I2C_PORT_TYPEC_AIC_1, 0);
+	if (rv) {
+		return rv;
+	}
+
+	ping_retries = 0;
+	k_msleep(3);
+	do {
+		k_msleep(3);
+		i2c_lock(I2C_PORT_TYPEC_AIC_1, 1);
+		rv = i2c_xfer_unlocked(I2C_PORT_TYPEC_AIC_1, 0x69, NULL, 0, &ping, 1,
+				I2C_XFER_SINGLE);
+		i2c_lock(I2C_PORT_TYPEC_AIC_1, 0);
+		if (rv) {
+			return rv;
+		}
+	} while ((RT_SMBUS_CMD_RESULT(ping)  == 0) && ping_retries++ < RT_SMBUS_CMD_RETRIES);
+
+	if (RT_SMBUS_CMD_RESULT(ping)  != 1) {
+		return -EPROTO;
+	}
+
+	if (resp_len) {
+		*resp_len = RT_SMBUS_CMD_RESULT_LEN(ping);
+	}
+
+	return 0;
+}
+
+int rt_smbus_get_resp(uint8_t *resp, uint8_t len)
+{
+	int rv;
+	uint8_t cmd = 0x80;
+
+	i2c_lock(I2C_PORT_TYPEC_AIC_1, 1);
+	rv = i2c_xfer_unlocked(I2C_PORT_TYPEC_AIC_1, 0x69, &cmd, 1, resp,
+			len + 1,
+			I2C_XFER_SINGLE);
+	i2c_lock(I2C_PORT_TYPEC_AIC_1, 0);
+
+	return rv;
+}
+
+#define RT_SMBUS_CMD_ENABLE_VENDOR     {0x01, 0x03, 0xda, 0x0b, 0x01}
+#define RT_SMBUS_CMD_GET_TPC_OP_MODE   {0x08, 0x02, 0x9d, 0x00}
+#define RT_SMBUS_CMD_SET_TPC_OP_MODE   {0x08, 0x03, 0x1d, 0x00, 0x00}
+#define RT_SMBUS_CMD_SET_VOLTAGE       {0x1e, 0x03, 0x00, 0x00, 0x00}
+#define RT_SMBUS_CMD_SET_NOTIF_ENABLE  {0x08, 0x06, 0x01, 0x00, 0x00, 0x00, 0x00}
+
+static void enable_vendor(void)
+{
+	uint8_t cmd[] = RT_SMBUS_CMD_ENABLE_VENDOR;
+
+	rt_smbus_send_cmd(cmd, NULL);
+}
+
+static void set_drm(bool enable)
+{
+	uint8_t cmd[] = RT_SMBUS_CMD_SET_TPC_OP_MODE;
+
+	cmd[4] = enable? 0x05: 0x04;
+
+	rt_smbus_send_cmd(cmd, NULL);
+}
+
+static void set_voltage(void)
+{
+	uint8_t cmd[] =  RT_SMBUS_CMD_SET_VOLTAGE;
+	int rv;
+
+	rv = rt_smbus_send_cmd(cmd, NULL);
+	if (rv) {
+		LOG_ERR("ERROR Gettin CSD OP Mode");
+	}
+}
+
+void get_tcp_op_mode(void)
+{
+	uint8_t resp_len;
+	uint8_t resp[16];
+	uint8_t cmd[] =  RT_SMBUS_CMD_GET_TPC_OP_MODE;
+	int rv;
+
+	rv = rt_smbus_send_cmd(cmd, &resp_len);
+	if (rv || resp_len == 0) {
+		LOG_ERR("ERROR Gettin CSD OP Mode");
+	}
+
+	rt_smbus_get_resp(resp, resp_len);
+}
+
+static void init_drp(void)
+{
+
+	enable_vendor();
+
+	set_drm(false);
+
+	/* Enable this code to get default TPC Operation mode */
+	//get_tcp_op_mode();
+}
+
+void pd_driver_hook(struct ap_power_ev_callback *cb,
+					  struct ap_power_ev_data data)
+{
+	switch (data.event) {
+	case AP_POWER_RESUME:
+		set_drm(true);
+		break;
+	case AP_POWER_PRE_INIT:
+		set_voltage();
+		break;
+
+	default:
+	}
+}
+
+static void intel_altmode_init_cbs(void)
+{
+	static struct ap_power_ev_callback cb;
+
+	ap_power_ev_init_callback(&cb,
+				  pd_driver_hook,
+				  AP_POWER_RESUME | AP_POWER_PRE_INIT);
+	ap_power_ev_add_callback(&cb);
+}
+
 static int intel_altmode_init(const struct device *dev)
 {
 	const struct pd_altmode_config *cfg = dev->config;
@@ -181,6 +327,16 @@ static int intel_altmode_init(const struct device *dev)
 		}
 	}
 
+	static bool init = false;
+	if (init) {
+		return 0;
+	}
+	/* This needs to be only once, currently only testing with Port 0 */
+	intel_altmode_init_cbs();
+
+	init_drp();
+
+	init = true;
 	return 0;
 }
 
