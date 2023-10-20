b331d8a7d5d9a47a806735411abe74c8a3da1a29
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2023-10-20 11:36:45 -0700

    [WIP] Change Control and Status register length in PD Data path
    Change-Id: I42178883531500a4ae0850f502554e736076073a

--- zephyr/include/drivers/intel_altmode.h
+++ zephyr/include/drivers/intel_altmode.h
@@ -120,7 +120,7 @@ extern "C" {
  */
 
 #define INTEL_ALTMODE_REG_DATA_STATUS 0x5F
-#define INTEL_ALTMODE_DATA_STATUS_REG_LEN 5
+#define INTEL_ALTMODE_DATA_STATUS_REG_LEN 4
 
 union data_status_reg {
 	struct {
@@ -163,7 +163,7 @@ union data_status_reg {
 		uint8_t res2 : 1;
 
 		/* Bits 32 to 39 */
-		uint8_t res3;
+		//uint8_t res3;
 	};
 	uint8_t raw_value[INTEL_ALTMODE_DATA_STATUS_REG_LEN];
 };
@@ -191,7 +191,7 @@ union data_status_reg {
  */
 
 #define INTEL_ALTMODE_REG_DATA_CONTROL 0x50
-#define INTEL_ALTMODE_DATA_CONTROL_REG_LEN 6
+#define INTEL_ALTMODE_DATA_CONTROL_REG_LEN 4
 
 union data_control_reg {
 	struct {
@@ -207,7 +207,7 @@ union data_control_reg {
 		uint8_t res3 : 2;
 
 		/* Bits 16 to 47 */
-		uint32_t ret_dbg_mode;
+		uint16_t ret_dbg_mode;
 	};
 	uint8_t raw_value[INTEL_ALTMODE_DATA_CONTROL_REG_LEN];
 };
