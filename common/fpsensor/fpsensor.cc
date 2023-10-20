84446c86e024d0f242e336635084a9e62f836ec0
Patryk Duda <patrykd@google.com>
2023-10-20 15:15:11 +0000

    fpsensor: Use fp_idle() function
    The function is used to clear any previous states on TASK_EVENT_TIMER
    and TASK_EVENT_UPDATE_CONFIG.
    
    BUG=b:299924239
    TEST=zmake build bloonchipper
    
    Change-Id: If2d369dfc33beea2914c085df4dc45e3c2137ce2

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -259,7 +259,15 @@ extern "C" void fp_task(void)
 		if (evt & TASK_EVENT_UPDATE_CONFIG) {
 			uint32_t mode = sensor_mode;
 
+#ifdef CONFIG_ZEPHYR
+			/*
+			 * We are about to change sensor mode, so exit any
+			 * previous states.
+			 */
+			fp_idle();
+#else
 			gpio_disable_interrupt(GPIO_FPS_INT);
+#endif
 			if ((mode ^ enroll_session) & FP_MODE_ENROLL_SESSION) {
 				if (mode & FP_MODE_ENROLL_SESSION) {
 					if (fp_enrollment_begin())
@@ -304,7 +312,13 @@ extern "C" void fp_task(void)
 		} else if (evt & (TASK_EVENT_SENSOR_IRQ | TASK_EVENT_TIMER)) {
 			overall_t0 = get_time();
 			timestamps_invalid = 0;
+#ifdef CONFIG_ZEPHYR
+			/* On timeout, put sensor into idle state. */
+			if (evt & TASK_EVENT_TIMER)
+				fp_idle();
+#else
 			gpio_disable_interrupt(GPIO_FPS_INT);
+#endif
 			if (sensor_mode & FP_MODE_ANY_DETECT_FINGER) {
 				st = fp_finger_status();
 				if (st == FINGER_PRESENT &&
