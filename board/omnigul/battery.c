537d718d481ec4ff670e38a7c2dc4e1d5aa89a7f
Logan_Liao <Logan_Liao@compal.corp-partner.google.com>
2023-10-24 18:43:29 +0800

    omnigul: Correct battery parameter D-FET C-FET
    Base on b/290149136 comment#2, Correct battery parameter D-FET C-FET
    setting.
    
    BUG=b:290149136
    TEST=cutoff success, EC reset success.
    
    Change-Id: I2aa8b8902242567a006222a9efe4d51b3a5117a2

--- board/omnigul/battery.c
+++ board/omnigul/battery.c
@@ -77,7 +77,7 @@ const struct board_batt_params board_battery_info[] = {
 			},
 			.fet = {
 				.reg_addr = 0x0,
-				.reg_mask = 0x0006,	/* D-FET C-FET */
+				.reg_mask = 0xC000,	/* D-FET C-FET */
 				.disconnect_val = 0x0000,
 			},
 			.flags = FUEL_GAUGE_FLAG_MFGACC,
@@ -108,7 +108,7 @@ const struct board_batt_params board_battery_info[] = {
 			},
 			.fet = {
 				.reg_addr = 0x0,
-				.reg_mask = 0x0006,	/* D-FET C-FET */
+				.reg_mask = 0xC000,	/* D-FET C-FET */
 				.disconnect_val = 0x0000,
 			},
 			.flags = FUEL_GAUGE_FLAG_MFGACC,
@@ -140,7 +140,7 @@ const struct board_batt_params board_battery_info[] = {
 			},
 			.fet = {
 				.reg_addr = 0x0,
-				.reg_mask = 0x0006,	/* D-FET C-FET */
+				.reg_mask = 0xC000,	/* D-FET C-FET */
 				.disconnect_val = 0x0000,
 			},
 			.flags = FUEL_GAUGE_FLAG_MFGACC,
