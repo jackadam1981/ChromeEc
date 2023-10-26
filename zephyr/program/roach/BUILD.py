c0e82223737d2c15da8114ef63f31152761dc32d
Ting Shen <phoenixshen@google.com>
2023-10-26 18:57:59 +0800

    roach: add unittest
    Add test for Axii board.
    
    Note that there's no code in Roach board at this moment. So test Axii
    only.
    
    BUG=b:287012522
    TEST=twister
    
    Change-Id: I7042533b549afd0015b44fb14a50b28c78df2a86

--- zephyr/program/roach/BUILD.py
+++ zephyr/program/roach/BUILD.py
@@ -11,7 +11,7 @@ def register_variant(project_name, rwsig_sign=True):
     signer_kwarg = {}
     if rwsig_sign:
         # pylint: disable=undefined-variable
-        signer_kwarg['signer'] = signers.RwsigSigner(here / "dev_key.pem")
+        signer_kwarg["signer"] = signers.RwsigSigner(here / "dev_key.pem")
 
     register_binman_project(
         project_name=project_name,
