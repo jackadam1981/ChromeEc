a03d8a5f9a3cdc59279ad0c582d4cbf9f134bc17
Abe Levkoy <alevkoy@chromium.org>
2023-10-30 14:22:22 -0600

    usb_tc_sm_mock: Exclude trivial mocks from coverage
    Exclude mock functions with no internal logic from coverage
    calculations. Avoid low-coverage warnings for code that only exists to
    allow unrelated tests to link.
    
    BUG=b:188578923
    TEST=make buildall
    
    Change-Id: I62dc221b6d7e3145c554e09819428b0b640bce04

--- common/mock/usb_tc_sm_mock.c
+++ common/mock/usb_tc_sm_mock.c
@@ -56,10 +56,6 @@ void typec_select_src_collision_rp(int port, enum tcpc_rp_value rp)
 	mock_tc_port[port].lcl_rp = rp;
 }
 
-void typec_select_src_current_limit_rp(int port, enum tcpc_rp_value rp)
-{
-}
-
 int tc_is_attached_src(int port)
 {
 	return mock_tc_port[port].attached_src;
@@ -82,6 +78,21 @@ void tc_prs_src_snk_assert_rd(int port)
 	mock_tc_port[port].attached_src = 0;
 }
 
+int tc_is_vconn_src(int port)
+{
+	return mock_tc_port[port].vconn_src;
+}
+
+void tc_hard_reset_request(int port)
+{
+	mock_tc_port_reset();
+}
+
+/* LCOV_EXCL_START These functions only serve to allow tests to link. */
+void typec_select_src_current_limit_rp(int port, enum tcpc_rp_value rp)
+{
+}
+
 int typec_update_cc(int port)
 {
 	return EC_SUCCESS;
@@ -95,17 +106,6 @@ int tc_check_vconn_swap(int port)
 void tc_ctvpd_detected(int port)
 {
 }
-
-int tc_is_vconn_src(int port)
-{
-	return mock_tc_port[port].vconn_src;
-}
-
-void tc_hard_reset_request(int port)
-{
-	mock_tc_port_reset();
-}
-
 void tc_partner_dr_data(int port, int en)
 {
 }
@@ -161,6 +161,7 @@ enum pd_power_role pd_get_power_role(int port)
 	return mock_tc_port[port].power_role;
 }
 
+/* LCOV_EXCL_START These functions only serve to allow tests to link. */
 enum pd_cc_states pd_get_task_cc_state(int port)
 {
 	return PD_CC_NONE;
@@ -220,3 +221,4 @@ bool pd_alt_mode_capable(int port)
 {
 	return false;
 }
+/* LCOV_EXCL_STOP */
