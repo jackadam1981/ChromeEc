f9f0f57fc9bb2829f788c61a28d80d412328364b
Craig Hesling <hesling@chromium.org>
2023-11-02 16:15:38 -0700

    DNS: Hack in DRUID
    BUG=b:295249061
    TEST=make buildall
    
    Change-Id: I5cc8f4f03944fd349a50196d6361451a2dd2fba3

--- driver/fingerprint/fpsensor_algorithm.h
+++ driver/fingerprint/fpsensor_algorithm.h
@@ -1,40 +1,33 @@
-/* Copyright 2019 The ChromiumOS Authors
+/* Copyright 2023 The ChromiumOS Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-#ifndef __CROS_EC_DRIVER_FINGERPRINT_FPSENSOR_DRIVER_H_
-#define __CROS_EC_DRIVER_FINGERPRINT_FPSENSOR_DRIVER_H_
+#ifndef __CROS_EC_DRIVER_FINGERPRINT_FPSENSOR_ALGORITHM_H_
+#define __CROS_EC_DRIVER_FINGERPRINT_FPSENSOR_ALGORITHM_H_
 
 #include <stdint.h>
 
 #if defined(HAVE_PRIVATE) && !defined(EMU_BUILD)
-#define HAVE_FP_PRIVATE_DRIVER
 
 #if defined(CONFIG_FP_SENSOR_ELAN80) || defined(CONFIG_FP_SENSOR_ELAN515)
 #include "elan/elan_sensor.h"
-#define FP_SENSOR_IMAGE_OFFSET (FP_SENSOR_IMAGE_OFFSET_ELAN)
-#define FP_SENSOR_IMAGE_SIZE (FP_SENSOR_IMAGE_SIZE_ELAN)
-#define FP_SENSOR_RES_X (FP_SENSOR_RES_X_ELAN)
-#define FP_SENSOR_RES_Y (FP_SENSOR_RES_Y_ELAN)
+#define FP_ALGORITHM_TEMPLATE_SIZE (FP_ALGORITHM_TEMPLATE_SIZE_ELAN)
+#define FP_MAX_FINGER_COUNT (FP_MAX_FINGER_COUNT_ELAN)
 #endif /* CONFIG_FP_SENSOR_ELAN80 || CONFIG_FP_SENSOR_ELAN515 */
 
 #if defined(CONFIG_FP_SENSOR_FPC1025) || defined(CONFIG_FP_SENSOR_FPC1145)
 #include "fpc/fpc_sensor.h"
-#define FP_SENSOR_IMAGE_OFFSET (FP_SENSOR_IMAGE_OFFSET_FPC)
-#define FP_SENSOR_IMAGE_SIZE (FP_SENSOR_IMAGE_SIZE_FPC)
-#define FP_SENSOR_RES_X (FP_SENSOR_RES_X_FPC)
-#define FP_SENSOR_RES_Y (FP_SENSOR_RES_Y_FPC)
+#define FP_ALGORITHM_TEMPLATE_SIZE (FP_ALGORITHM_TEMPLATE_SIZE_FPC)
+#define FP_MAX_FINGER_COUNT (FP_MAX_FINGER_COUNT_FPC)
 #endif /* CONFIG_FP_SENSOR_FPC1025 || CONFIG_FP_SENSOR_FPC1145 */
 
 #endif /* HAVE_PRIVATE && !EMU_BUILD */
 
 /* These values are used for public or host (emulator) tests. */
 #if !defined(HAVE_PRIVATE) || defined(EMU_BUILD)
-#define FP_SENSOR_IMAGE_OFFSET (0)
-#define FP_SENSOR_IMAGE_SIZE 0
-#define FP_SENSOR_RES_X 0
-#define FP_SENSOR_RES_Y 0
+#define FP_ALGORITHM_TEMPLATE_SIZE 0
+#define FP_MAX_FINGER_COUNT 5
 #endif /* !HAVE_PRIVATE || EMU_BUILD */
 
 #if defined(HAVE_PRIVATE) && defined(TEST_BUILD)
@@ -49,4 +42,20 @@
 #define FP_SENSOR_IMAGE_OFFSET (0)
 #endif
 
-#endif /* __CROS_EC_DRIVER_FINGERPRINT_FPSENSOR_DRIVER_H_ */
+#ifdef CONFIG_LIB_DRUID
+#undef noreturn
+#include "mcu/primitives/templates.h"
+// #warning APPLYING TEMPLATE SIZE OVERRIDE
+#undef FP_ALGORITHM_TEMPLATE_SIZE
+#define FP_ALGORITHM_TEMPLATE_SIZE sizeof(fingerprintauth::FingerTemplate)
+#undef FP_MAX_FINGER_COUNT
+#define FP_MAX_FINGER_COUNT 2
+#ifndef __cplusplus
+#error This header requires C++ support.
+#endif
+typedef fingerprintauth::FingerTemplate fp_template_t;
+#else
+typedef uint8_t fp_template_t[FP_ALGORITHM_TEMPLATE_SIZE];
+#endif /* CONFIG_LIB_DRUID */
+
+#endif /* __CROS_EC_DRIVER_FINGERPRINT_FPSENSOR_ALGORITHM_H_ */
