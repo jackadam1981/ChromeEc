3b398f21687477fb9b6a9b9e2e90886c55b40950
Craig Hesling <hesling@chromium.org>
2023-11-02 18:17:47 -0700

    fingerprint: Disable jtag/swd on boot
    There is a bit of a catch'22. We would like to disable JTAG, if the
    flash protect is enabled, which means that we need to init flash.
    We would like JTAG to be disabled before we initialize the protection
    flags.
    
    The alternative method considered was to use the EC gpio module
    functionality to enable/disable the debug interface. This was
    shown to work in PS8, but it requires that all users declare the
    JTAG/SWD pins with the correct default pulls/config in the board
    gpio.inc file. It also means that the "disablement" method
    may change when another usage of GPIO_ALT_FUNC_NONE comes along.
    Furthermore, this method would be less portable to Zephyr.
    
    BUG=b:142556193
    TEST=make proj-dartmonkey proj-bloonchipper
    TEST=make BOARD=bloonchipper -j test-debug
    
    Change-Id: Ia51d7b53734dfe36c9e4b9c116f16d94f6980496

--- common/debug.c
+++ common/debug.c
@@ -3,8 +3,10 @@
  * found in the LICENSE file.
  */
 
+#include "assert.h"
 #include "common.h"
 #include "debug.h"
+#include "panic.h"
 #include "stdbool.h"
 
 __overridable bool debugger_is_connected(void)
@@ -15,4 +17,28 @@ __overridable bool debugger_is_connected(void)
 __overridable bool debugger_was_connected(void)
 {
 	return false;
+}
+
+/*
+ * This should never be called on a platform that doesn't implement it,
+ * considering this is a security critical function.
+ */
+__overridable void debugger_disable(void)
+{
+	// software_panic(PANIC_SW_ASSERT, task_get_current());
+	// panic_printf("PANIC: Called unimplemented security function %s.",
+	// 	     __func__);
+	// panic_assert_fail();
+	const int CALLED_SECURITY_FUNCTION_THAT_IS_NOT_IMPLEMENTED = 0;
+	assert(CALLED_SECURITY_FUNCTION_THAT_IS_NOT_IMPLEMENTED);
+	__builtin_unreachable();
+}
+
+__overridable void debugger_enable(void)
+{
+}
+
+__overridable void debugger_disable_on_boot(void)
+{
+	/* By default, we do not disable the debugger, unless overridden. */
 }
