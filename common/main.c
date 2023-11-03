d7b864c6773c63207d91bb48e3c5151306cd1a90
Craig Hesling <hesling@chromium.org>
2023-11-02 18:17:47 -0700

    fingerprint: Disable jtag/swd on boot
    There is a bit of a catch'22. We would like to disable JTAG, if the
    flash protect is enabled, which means that we need to init flash.
    We would like JTAG to be disabled before we initialize the protection
    flags.
    
    The alternative method considered was to use the EC gpio module
    functionality to enable/disable the debug interface. This was
    shown to work in PS8, but it requires that all users declare the
    JTAG/SWD pins with the correct default pulls/config in the board
    gpio.inc file. It also means that the "disablement" method
    may change when another usage of GPIO_ALT_FUNC_NONE comes along.
    Furthermore, this method would be less portable to Zephyr.
    
    BUG=b:142556193
    TEST=make proj-dartmonkey proj-bloonchipper
    TEST=make BOARD=bloonchipper -j test-debug
    
    Change-Id: Ia51d7b53734dfe36c9e4b9c116f16d94f6980496

--- common/main.c
+++ common/main.c
@@ -16,6 +16,7 @@
 #include "console.h"
 #include "cpu.h"
 #include "cros_board_info.h"
+#include "debug.h"
 #include "dma.h"
 #include "eeprom.h"
 #include "flash.h"
@@ -46,6 +47,9 @@
 
 test_mockable __keep int main(void)
 {
+	/* Disabling the debugger must remain the first item of main. */
+	debugger_disable_on_boot();
+
 	int mpu_pre_init_rv = EC_SUCCESS;
 
 	if (IS_ENABLED(CONFIG_PRESERVE_LOGS)) {
