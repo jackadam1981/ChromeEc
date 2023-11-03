3f2fd8a3a58782d56ea129aa65b5e64fdd7e9b26
Craig Hesling <hesling@chromium.org>
2023-11-02 18:17:47 -0700

    fingerprint: Disable jtag/swd on boot
    There is a bit of a catch'22. We would like to disable JTAG, if the
    flash protect is enabled, which means that we need to init flash.
    We would like JTAG to be disabled before we initialize the protection
    flags.
    
    The alternative method considered was to use the EC gpio module
    functionality to enable/disable the debug interface. This was
    shown to work in PS8, but it requires that all users declare the
    JTAG/SWD pins with the correct default pulls/config in the board
    gpio.inc file. It also means that the "disablement" method
    may change when another usage of GPIO_ALT_FUNC_NONE comes along.
    Furthermore, this method would be less portable to Zephyr.
    
    BUG=b:142556193
    TEST=make proj-dartmonkey proj-bloonchipper
    TEST=make BOARD=bloonchipper -j test-debug
    
    Change-Id: Ia51d7b53734dfe36c9e4b9c116f16d94f6980496

--- chip/stm32/debug.c
+++ chip/stm32/debug.c
@@ -3,9 +3,16 @@
  * found in the LICENSE file.
  */
 
+#include "assert.h"
 #include "common.h"
+#include "compile_time_macros.h"
 #include "debug.h"
+#include "gpio.h"
+#include "panic.h"
 #include "registers.h"
+
+/* Debugger internal-only panic routine for debugger_disable. */
+void debugger_internal_panic_unimplemented(void);
 
 /*
  * This function looks for signs that a debugger was attached. If we
@@ -37,4 +44,72 @@ __override bool debugger_was_connected(void)
 	 * Only power-on-reset / power-cycle.
 	 */
 	return STM32_DBGMCU_CR & STM32_DBGMCU_CR_LOW_PWR_FRIENDLY;
+}
+
+__override void debugger_disable(void)
+{
+	if (IS_ENABLED(CHIP_FAMILY_STM32F4) ||
+	    IS_ENABLED(CHIP_FAMILY_STM32H7)) {
+		/*
+		 * There are two important gpio settings that must be set
+		 * correctly, for the JTAG/SWD pins, in order for an attached
+		 * debugger to function correctly.
+		 * - The first is that the gpio mode must be set to use the
+		 * alternative function.
+		 * - The second is that the alternative function selector must
+		 * be set to system/default/0.
+		 *
+		 * Changing either of these will effectively block communication
+		 * to the debug module. We alter both as an extra precaution.
+		 * See debugger_enable for information about each debug pin.
+		 *
+		 * Note that we configure the pins as analog input, instead of
+		 * digital input, to save power. See ST's AN4365 section 1.2.6
+		 * for more detail.
+		 */
+		gpio_set_alternate_function(STM32_GPIOA_BASE, GENMASK(15, 13),
+					    GPIO_ALT_FUNC_1);
+		gpio_set_alternate_function(STM32_GPIOB_BASE, GENMASK(4, 3),
+					    GPIO_ALT_FUNC_1);
+		gpio_set_flags_by_mask(STM32_GPIOA_BASE, GENMASK(15, 13),
+				       GPIO_ANALOG);
+		gpio_set_flags_by_mask(STM32_GPIOB_BASE, GENMASK(4, 3),
+				       GPIO_ANALOG);
+
+	} else {
+		debugger_internal_panic_unimplemented();
+		__builtin_unreachable();
+	}
+}
+
+__override void debugger_enable(void)
+{
+	if (IS_ENABLED(CHIP_FAMILY_STM32F4) ||
+	    IS_ENABLED(CHIP_FAMILY_STM32H7)) {
+		/*
+		 * STM32F412 and STM32H743 Debug Port Default Configuration.
+		 *
+		 * These pins and pulls are listed in the STM32F412 Reference
+		 * Manual (RM0402 Rev 5) section 7.3.1 AND in the STM32H743
+		 * Reference Manual (RM0433 Rev 6) section 10.3.1 as the
+		 * following:
+		 *
+		 * - PA15: JTDI in pull-up
+		 * - PA14: JTCK/SWCLK in pull-down
+		 * - PA13: JTMS/SWDAT in pull-up
+		 * - PB4:  NJTRST in pull-up
+		 * - PB3:  JTDO in floating state
+		 */
+		gpio_set_flags_by_mask(STM32_GPIOA_BASE, BIT(15) | BIT(13),
+				       GPIO_PULL_UP);
+		gpio_set_flags_by_mask(STM32_GPIOA_BASE, BIT(14),
+				       GPIO_PULL_DOWN);
+		gpio_set_flags_by_mask(STM32_GPIOB_BASE, BIT(4), GPIO_PULL_UP);
+		gpio_set_flags_by_mask(STM32_GPIOB_BASE, BIT(3),
+				       GPIO_FLAG_NONE);
+		gpio_set_alternate_function(STM32_GPIOA_BASE, GENMASK(15, 13),
+					    GPIO_ALT_FUNC_DEFAULT);
+		gpio_set_alternate_function(STM32_GPIOB_BASE, GENMASK(4, 3),
+					    GPIO_ALT_FUNC_DEFAULT);
+	}
 }
