0d2cabdb0e6b303053c63e90f19a1d2b29341be9
Abe Levkoy <alevkoy@chromium.org>
2023-11-06 16:56:58 -0700

    TCPMv2: Handle SOP' Discover ID response in DPM
    Record the response (or non-response) to SOP' Discover Identity in the
    PE by calling into the DPM, which in turn records the response properly.
    This is a first step in moving discovery policy into the DPM, including
    the timing of initiating discovery messages. The bulk of this commit
    creates the discovery state structure in the DPM.
    
    This commit does not change the observable discovery policy of the TCPM,
    but it is a step towards making it easier to do so.
    
    BUG=b:272827504
    TEST=Attach to dock; observe initial PD traffic, including discovery
    
    Change-Id: I76b140cc1ff9382898b5e4bee00ca878d1fc83fa

--- common/mock/usb_pd_dpm_mock.c
+++ common/mock/usb_pd_dpm_mock.c
@@ -57,6 +57,9 @@ void dpm_vdm_acked(int port, enum tcpci_msg_type type, int vdo_count,
 void dpm_vdm_naked(int port, enum tcpci_msg_type type, uint16_t svid,
 		   uint8_t vdm_cmd, uint32_t vdm_header)
 {
+	if (type == TCPCI_MSG_SOP_PRIME && svid == USB_SID_PD &&
+	    vdm_cmd == CMD_DISCOVER_IDENT)
+		pd_set_identity_discovery(port, type, PD_DISC_FAIL);
 }
 
 void dpm_notify_attention(int port, size_t vdo_objects, uint32_t *buf)
