b484aea0d44c5d61165f01bc58e9fefb5bfbdae6
Wai-Hong Tam <waihong@google.com>
2023-11-09 10:43:14 -0800

    zmake: Update version string, test, and README
    Update the version string as described in go/cros-fwid-deprecation.
    
    The CL comes from the patches Mark Hasemeyer posted and has the
    following modifications:
     * rebased and fixed the merge conflicts,
     * formatted by `cros format`,
     * fixed lint errors,
     * added missing docstrings,
     * modified CME for the new arguments of `get_version_string()`,
     * merged `static` and `official-build` arguments.
    
    BUG=b:198475757
    TEST=./zephyr/zmake/run_tests.sh
    Make some dummmy commit and run `zmake compare-builds rex` on passed
    and failed cases.
    
    Cq-Depend: chromium:5021684
    Change-Id: Ia8acecaf0b9fe3c8eb2281fe6071a93e63a9eedc

--- zephyr/zmake/zmake/zmake.py
+++ zephyr/zmake/zmake/zmake.py
@@ -13,6 +13,7 @@ import logging
 import os
 import pathlib
 import re
+import shlex
 import shutil
 import subprocess
 import sys
@@ -298,6 +299,7 @@ class Zmake:
         all_projects=False,
         extra_cflags=None,
         delete_intermediates=False,
+        version=None,
         static_version=False,
         save_temps=False,
         wait_for_executor=True,
@@ -330,6 +332,7 @@ class Zmake:
                     allow_warnings=allow_warnings,
                     extra_cflags=extra_cflags,
                     delete_intermediates=delete_intermediates,
+                    version=version,
                     static_version=static_version,
                     save_temps=save_temps,
                 )
@@ -371,6 +374,7 @@ class Zmake:
         all_projects=False,
         extra_cflags=None,
         delete_intermediates=False,
+        version=None,
         static_version=False,
         save_temps=False,
     ):
@@ -389,6 +393,7 @@ class Zmake:
             extra_cflags=extra_cflags,
             build_after_configure=True,
             delete_intermediates=delete_intermediates,
+            version=version,
             static_version=static_version,
             save_temps=save_temps,
         )
@@ -508,6 +513,7 @@ class Zmake:
         allow_warnings=False,
         extra_cflags=None,
         delete_intermediates=False,
+        version=None,
         static_version=False,
         save_temps=False,
     ):
@@ -523,6 +529,14 @@ class Zmake:
                     shutil.rmtree(build_dir)
 
                 generated_include_dir = (build_dir / "include").resolve()
+
+                ec_version_flags = []
+                if version:
+                    quoted_version = shlex.quote(version)
+                    ec_version_flags.append(f"--version {quoted_version}")
+                if static_version:
+                    ec_version_flags.append("--static")
+
                 base_config = zmake.build_config.BuildConfig(
                     cmake_defs={
                         "CMAKE_EXPORT_COMPILE_COMMANDS": "ON",
@@ -543,8 +557,12 @@ class Zmake:
                         "ZMAKE_INCLUDE_DIR": str(generated_include_dir),
                         "Python3_EXECUTABLE": sys.executable,
                         **(
-                            {"EXTRA_EC_VERSION_FLAGS": "--static"}
-                            if static_version
+                            {
+                                "EXTRA_EC_VERSION_FLAGS": " ".join(
+                                    ec_version_flags
+                                )
+                            }
+                            if ec_version_flags
                             else {}
                         ),
                     },
@@ -651,6 +669,7 @@ class Zmake:
                     build_dir=build_dir,
                     project=project,
                     coverage=coverage,
+                    version=version,
                     static_version=static_version,
                     delete_intermediates=delete_intermediates,
                 )
@@ -735,6 +754,7 @@ class Zmake:
         build_dir,
         project: zmake.project.Project,
         coverage=False,
+        version=None,
         static_version=False,
         delete_intermediates=False,
     ):
@@ -748,8 +768,7 @@ class Zmake:
             # Compute the version string.
             version_string = zmake.version.get_version_string(
                 project.config.project_name,
-                build_dir / "zephyr_base",
-                zmake.modules.locate_from_directory(build_dir / "modules"),
+                version,
                 static=static_version,
             )
 
