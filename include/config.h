ccf5bf78ae9b2cd4543e661dc53067d1545934ff
Ting Shen <phoenixshen@google.com>
2023-11-14 18:08:25 +0800

    WIP: enable usb pairing
    Change-Id: I52387cf1b44738204f240422dfb592b0945c4ef7

--- include/config.h
+++ include/config.h
@@ -5820,6 +5820,7 @@
 
 /* Add support for pairing over the USB update interface. */
 #undef CONFIG_USB_PAIRING
+#define CONFIG_USB_PAIRING
 
 /* Add support for reading UART buffer from USB update interface. */
 #undef CONFIG_USB_CONSOLE_READ
