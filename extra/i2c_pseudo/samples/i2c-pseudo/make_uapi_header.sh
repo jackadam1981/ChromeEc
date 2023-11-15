1714560687eafe91c46634e3b77f2a80e83bb06d
Matthew Blecker <matthewb@chromium.org>
2023-11-14 17:22:12 -0800

    extra: i2c_pseudo: samples: standalone out-of-tree Makefile
    This replaces the samples/i2c-pseudo/Makefile for building
    i2c-adapter-example with a Makefile written for standalone use
    outside the kernel tree.
    
    The prior samples/i2c-pseudo/Makefile revision was written for
    integration into the upstream kernel build. I will use that version
    when creating the upstream patch. This version is more useful here
    and now.
    
    BUG=b:275615681
    TEST=$ cd samples/i2c-pseudo
    $ git status  # no modified files
    $ ls
    i2c-adapter-example.c
    Makefile
    make_modified_include.sh
    make_uapi_header.sh
    $ make clean  # no errors
    $ git status  # no modified files
    $ ls
    i2c-adapter-example.c
    Makefile
    make_modified_include.sh
    make_uapi_header.sh
    $ make  # no errors
    $ git status  # no modified files
    $ ls
    i2c-adapter-example
    i2c-adapter-example.c
    i2c-adapter-example-modified-include.c
    i2c-pseudo.h
    Makefile
    make_modified_include.sh
    make_uapi_header.sh
    $ ./i2c-adapter-example < /dev/urandom  # works, no errors
    $ make clean  # no errors
    $ ls
    i2c-adapter-example.c
    Makefile
    make_modified_include.sh
    make_uapi_header.sh
    $
    
    Change-Id: Ie14e63ddaf426bf273720db3d5f44ad070d39e8a

---
+++ extra/i2c_pseudo/samples/i2c-pseudo/make_uapi_header.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# TODO: upstream patch: remove this file
+
+set -ex
+
+grep -v -E '^\s*#include\s+<linux/compiler\.h>\s*$' | sed 's/ __user / /g'
