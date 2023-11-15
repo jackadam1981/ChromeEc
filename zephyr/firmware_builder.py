f448b6d960f67d273182d411ceeb957d708f91cf
Al Semjonovs <asemjonovs@google.com>
2023-11-15 12:42:35 -0700

    zephyr: Add universal database to artifact bundle
    Package universal token database in artifact bundle
    
    BUG=b:287268093
    TEST=./firmware_builder.py bundle
         Validate token database present in /tmp/artifact_bundle_metadata
    
    Cq-Depend: chromium:5032339
    Change-Id: I6915b13f1739da958270524bf1079789179be51f

--- zephyr/firmware_builder.py
+++ zephyr/firmware_builder.py
@@ -279,6 +279,12 @@ def bundle_firmware(opts):
         # TODO(kmshelton): Populate the rest of metadata contents as it
         # gets defined in infra/proto/src/chromite/api/firmware.proto.
 
+    # TODO(b/287268093 asemjonovs): Add universal token database artifact
+    meta = info.objects.add()
+    meta.file_name = str(platform_ec / "build" / "database.bin")
+    meta.token_info.type = (
+        firmware_pb2.FirmwareArtifactInfo.TokenDatabaseInfo.TokenDatabaseType.EC  # pylint: disable=no-member
+    )
     write_metadata(opts, info)
 
 
