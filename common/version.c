c55dc11cd66f3322af188946242c43467e2d69d7
Robert Zieba <robertzieba@google.com>
2023-11-16 16:38:25 -0700

    common/version: Deprecate CrOS FWID
    This version information string is not used with zephyr. Note the
    deprecation and set this string to empty when building with zephyr.
    
    BUG=b:198475757
    TEST=./zephyr/zmake/run_tests.sh, ran on skyrim and guybrush
    
    Change-Id: I517bd33e53cf9d698541eef867a7e29875e4b62e

--- common/version.c
+++ common/version.c
@@ -15,6 +15,12 @@
 
 #include <stdint.h>
 
+#ifdef CONFIG_ZEPHYR
+/* Deprecated and unused with zephyr. */
+#undef CROS_FWID32
+#define CROS_FWID32 ""
+#endif
+
 BUILD_ASSERT(CONFIG_ROLLBACK_VERSION >= 0);
 BUILD_ASSERT(CONFIG_ROLLBACK_VERSION <= INT32_MAX);
 
