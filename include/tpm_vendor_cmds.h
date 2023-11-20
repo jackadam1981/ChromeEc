a9c96ef16ddb73a40f988a47d3d2f3a5f83ac448
Mary Ruthven <mruthven@chromium.org>
2023-11-20 11:41:44 -0600

    tpm_vendor_cmds: reorder the file to separate ti50 and cr50 code
    Reorder the file, to separate the ti50 and cr50 specific code. We
    shouldn't need ti50 code in the cr50 repo. We still need to pick shared
    enum changes to the cr50 repo.
    
    BUG=none
    TEST=none
    
    Change-Id: I60ff881602940c96dfa4a2511995f3565ca570da

--- include/tpm_vendor_cmds.h
+++ include/tpm_vendor_cmds.h
@@ -276,6 +276,16 @@ enum wp_options {
 	WP_FOLLOW,
 };
 
+/*
+ * Subcommand code, used to set write protect.
+ */
+#define WPV_UPDATE	     BIT(0)
+#define WPV_ENABLE	     BIT(1)
+#define WPV_FORCE	     BIT(2)
+#define WPV_ATBOOT_SET	     BIT(3)
+#define WPV_ATBOOT_ENABLE    BIT(4)
+#define WPV_FWMP_FORCE_WP_EN BIT(5)
+
 /* VENDOR_CC_USER_PRES options. */
 enum user_pres_options {
 	USER_PRES_ENABLE = BIT(0),
@@ -299,28 +309,6 @@ struct user_pres_response {
 #define VENDOR_CC_MASK	       0x0000ffff
 /* Our vendor-specific command codes go here */
 #define TPM_CC_VENDOR_CR50 0x0000
-
-/*** Structures and constants for VENDOR_CC_SPI_HASH ***/
-
-enum vendor_cc_spi_hash_request_subcmd {
-	/* Relinquish the bus */
-	SPI_HASH_SUBCMD_DISABLE = 0,
-	/* Acquire the bus for AP SPI */
-	SPI_HASH_SUBCMD_AP = 1,
-	/* Acquire the bus for EC SPI */
-	SPI_HASH_SUBCMD_EC = 2,
-	/* Hash SPI data */
-	SPI_HASH_SUBCMD_SHA256 = 4,
-	/* Read SPI data */
-	SPI_HASH_SUBCMD_DUMP = 5,
-	/* Poll spi hash PP state. */
-	SPI_HASH_PP_POLL = 6,
-};
-
-enum vendor_cc_spi_hash_request_flags {
-	/* EC uses gang programmer mode */
-	SPI_HASH_FLAG_EC_GANG = BIT(0),
-};
 
 /*
  * Errors recognized and returned by the VENDOR_CC_SEED_AP_RO_CHECK vendor
@@ -351,6 +339,8 @@ struct vendor_cc_spi_hash_request {
 	uint32_t size; /* Size in bytes to hash/read */
 } __packed;
 
+/*****************************************************************************/
+/* Ti50 Specific Structs */
 struct ti50_stats_v0 {
 	/* filesystem initialization time in ms */
 	uint32_t fs_init_time;
@@ -397,6 +387,9 @@ struct ti50_stats_v2 {
 #define METRICSV_CCD_MODE_SHIFT 3
 #define METRICSV_CCD_MODE_MASK	(1 << METRICSV_CCD_MODE_SHIFT)
 
+/* End Ti50 Specific Structs */
+/*****************************************************************************/
+/* Cr50 Specific Structs */
 #define CR50_METRICSV_RDD_IS_DETECTED_SHIFT		0
 #define CR50_METRICSV_RDD_KEEPALIVE_EN_SHIFT		1
 #define CR50_METRICSV_CCD_MODE_EN_SHIFT			2
@@ -427,17 +420,29 @@ struct cr50_stats_response {
 	uint32_t cold_reset_time_s;
 };
 
+/*** Structures and constants for VENDOR_CC_SPI_HASH ***/
 /* Maximum size of a response = SHA-256 hash or 1-32 bytes of data */
 #define SPI_HASH_MAX_RESPONSE_BYTES 32
 
-/*
- * Subcommand code, used to set write protect.
- */
-#define WPV_UPDATE	     BIT(0)
-#define WPV_ENABLE	     BIT(1)
-#define WPV_FORCE	     BIT(2)
-#define WPV_ATBOOT_SET	     BIT(3)
-#define WPV_ATBOOT_ENABLE    BIT(4)
-#define WPV_FWMP_FORCE_WP_EN BIT(5)
+enum vendor_cc_spi_hash_request_subcmd {
+	/* Relinquish the bus */
+	SPI_HASH_SUBCMD_DISABLE = 0,
+	/* Acquire the bus for AP SPI */
+	SPI_HASH_SUBCMD_AP = 1,
+	/* Acquire the bus for EC SPI */
+	SPI_HASH_SUBCMD_EC = 2,
+	/* Hash SPI data */
+	SPI_HASH_SUBCMD_SHA256 = 4,
+	/* Read SPI data */
+	SPI_HASH_SUBCMD_DUMP = 5,
+	/* Poll spi hash PP state. */
+	SPI_HASH_PP_POLL = 6,
+};
+
+enum vendor_cc_spi_hash_request_flags {
+	/* EC uses gang programmer mode */
+	SPI_HASH_FLAG_EC_GANG = BIT(0),
+};
+/* End Cr50 Specific Structs */
 
 #endif /* __INCLUDE_TPM_VENDOR_CMDS_H */
