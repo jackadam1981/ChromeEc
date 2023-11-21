e551aeb39e96c0966b7fe221ef484046230afdae
Jason Yuan <jasonyuan@google.com>
2023-11-20 18:08:31 -0800

    zephyr: CME: fix EC version in manifest
    fixes the version string after changes to zmake
    
    BUG=b:308497935
    TEST=zmake build --all
    
    Change-Id: I5c5c5ee2b158f336ba8fb450bb9a4083168beebc

--- zephyr/scripts/cme.py
+++ zephyr/scripts/cme.py
@@ -10,7 +10,6 @@ from pathlib import Path
 import sys
 from typing import List, Optional
 
-import zmake.modules
 import zmake.version
 
 from scripts import util
@@ -30,6 +29,18 @@ def parse_args(argv: Optional[List[str]] = None):
         required=True,
     )
 
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="Base version string to use in build",
+    )
+
+    parser.add_argument(
+        "--static",
+        action="store_true",
+        dest="static_version",
+        help="Generate static version information for reproducible builds and official builds",
+    )
     return parser.parse_args(argv)
 
 
@@ -255,8 +266,8 @@ def main(argv: Optional[List[str]] = None) -> Optional[int]:
         # `version` and `static` arguments that CME doesn't know.
         ec_version_string = zmake.version.get_version_string(
             build_dir.name,
-            version=None,
-            static=False,
+            version=args.version,
+            static=args.static_version,
         )
     manifest = Manifest(ec_version_string)
 
