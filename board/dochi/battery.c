300bbe99e5e5d5320c9fc2d73d1946218d3a35c5
ben chen <ben.chen2@quanta.corp-partner.google.com>
2023-11-21 16:43:58 +0800

    dochi: add COSMX battery AP20CBL
    Add new manufacturer name KT.0030B.04 COSMX battery
    
    BUG=b:307878517
    TEST=Check battery charge/discharge/cutoff working
    
    Change-Id: I00fc81c174814eb9199079749d160f18138548c0

--- board/dochi/battery.c
+++ board/dochi/battery.c
@@ -11,7 +11,7 @@
 #include "compile_time_macros.h"
 #include "gpio.h"
 /*
- * Battery info for all Brya battery types. Note that the fields
+ * Battery info for all Dochi battery types. Note that the fields
  * start_charging_min/max and charging_min/max are not used for the charger.
  * The effective temperature limits are given by discharging_min/max_c.
  *
@@ -33,81 +33,46 @@
  * address, mask, and disconnect value need to be provided.
  */
 const struct batt_conf_embed board_battery_info[] = {
-	/* POW-TECH GQA05 Battery Information */
-	[BATTERY_POWER_TECH] = {
-			/* BQ40Z50 Fuel Gauge */
-		.manuf_name = "POW-TECH",
-		.device_name = "BATGQA05L22",
+	/* COSMX AP20CBL Battery Information (new firmware ver) */
+	[BATTERY_COSMX_AP20CBL_004] = {
+		.manuf_name = "COSMX KT0030B004",
+		.device_name = "AP20CBL",
 		.config = {
 			.fuel_gauge = {
 				.ship_mode = {
-					.reg_addr = 0x00,
-					.reg_data = { 0x0010, 0x0010 },
+					.reg_addr = 0x3A,
+					.reg_data = { 0xC574, 0xC574 },
 				},
 				.fet = {
-					.reg_addr = 0x00,
-					.reg_mask = 0x2000, /* XDSG */
+					.reg_addr = 0x0,
+					.reg_mask = 0x2000,
 					.disconnect_val = 0x2000,
+					.cfet_mask = 0x4000,
+					.cfet_off_val = 0x4000,
 				},
 				.flags = FUEL_GAUGE_FLAG_MFGACC,
 			},
 			.batt_info = {
-				.voltage_max = TARGET_WITH_MARGIN(13050, 5),
-				.voltage_normal		= 11400, /* mV */
-				.voltage_min		= 9000, /* mV */
-				.precharge_current	= 280,	/* mA */
-				.start_charging_min_c	= 0,
-				.start_charging_max_c	= 45,
-				.charging_min_c		= 0,
-				.charging_max_c		= 45,
-				.discharging_min_c	= -10,
-				.discharging_max_c	= 60,
-			},
-		},
-	},
-	/* LGC L17L3PB0 Battery Information */
-	/*
-	 * Battery info provided by ODM on b/143477210, comment #11
-	 */
-	[BATTERY_LGC011] = {
-		.manuf_name = "LGC",
-		.config = {
-			.fuel_gauge = {
-				.ship_mode = {
-					.reg_addr = 0x00,
-					.reg_data = { 0x0010, 0x0010 },
-				},
-				.fet = {
-					.reg_addr = 0x0,
-					.reg_mask = 0x6000,
-					.disconnect_val = 0x6000,
-				},
-			},
-			.batt_info = {
-				.voltage_max = TARGET_WITH_MARGIN(13200, 5),
-				.voltage_normal		= 11550, /* mV */
-				.voltage_min		= 9000, /* mV */
-				.precharge_current	= 256,	/* mA */
-				.start_charging_min_c	= 0,
-				.start_charging_max_c	= 45,
-				.charging_min_c		= 0,
-				.charging_max_c		= 60,
-				.discharging_min_c	= 0,
-				.discharging_max_c	= 75,
+				.voltage_max            = 13200,
+				.voltage_normal         = 11550,
+				.voltage_min            = 9000,
+				.precharge_current      = 256,
+				.start_charging_min_c   = 0,
+				.start_charging_max_c   = 50,
+				.charging_min_c         = 0,
+				.charging_max_c         = 60,
+				.discharging_min_c      = -20,
+				.discharging_max_c      = 75,
 			},
 		},
 	},
 };
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
-const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_POWER_TECH;
+const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_COSMX_AP20CBL_004;
 
 enum battery_present battery_hw_present(void)
 {
-	enum gpio_signal batt_pres;
-
-	batt_pres = GPIO_EC_BATT_PRES_ODL;
-
 	/* The GPIO is low when the battery is physically present */
-	return gpio_get_level(batt_pres) ? BP_NO : BP_YES;
+	return gpio_get_level(GPIO_EC_BATT_PRES_ODL) ? BP_NO : BP_YES;
 }
