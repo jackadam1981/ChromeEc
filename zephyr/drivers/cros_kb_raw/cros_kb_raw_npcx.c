a3eabfd97921e261205c81fd4756c58cf5c752ff
Fabio Baltieri <fabiobaltieri@google.com>
2023-11-21 17:14:46 +0000

    zephyr: keyboard: use the correct kso02 logical level
    The line is normally active low to select the column, but being inverted
    make it active high (i.e. the column is selected when the signal is at
    the high level), so the right way of declaring it is flagging as
    GPIO_ACTIVE_HIGH.
    
    Change all current board to flag the kso02 column as GPIO_ACTIVE_HIGH
    and change the npcx and xec driver to use active and inactive correctly.
    
    The ITE driver is not affected as that uses a special Kconfig option.
    
    BUG=b:274443029
    TEST=zmake buildall
    TEST=test on rex
    LOW_COVERAGE_REASON=b:256669239 these seems to be out of scope, working on replacing them with the upstream drivers anyway
    
    Change-Id: Id9a6162f2881a20766727dde4652e885257a15bb

--- zephyr/drivers/cros_kb_raw/cros_kb_raw_npcx.c
+++ zephyr/drivers/cros_kb_raw/cros_kb_raw_npcx.c
@@ -139,23 +139,19 @@ static int cros_kb_raw_npcx_drive_column(const struct device *dev, int col)
 	/* Drive all lines to high. ie. Key detection is disabled. */
 	if (col == KEYBOARD_COLUMN_NONE) {
 		mask = ~0;
-		/* Set logical level high on COL2 */
-		cros_kb_raw_set_col2(1);
+		cros_kb_raw_set_col2(0);
 	}
 	/* Drive all lines to low for detection any key press */
 	else if (col == KEYBOARD_COLUMN_ALL) {
 		mask = ~(BIT(keyboard_cols) - 1);
-		/* Set logical level low on COL2 */
-		cros_kb_raw_set_col2(0);
+		cros_kb_raw_set_col2(1);
 	}
 	/* Drive one line to low for determining which key's state changed. */
 	else {
 		if (col == 2) {
-			/* Set logical level low on COL2 */
-			cros_kb_raw_set_col2(0);
-		} else {
-			/* Set logical level high on COL2 */
 			cros_kb_raw_set_col2(1);
+		} else {
+			cros_kb_raw_set_col2(0);
 		}
 		mask = ~BIT(col_out);
 	}
