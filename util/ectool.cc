c94ede4eb82d7c2dc6a37f6371aee906d421c172
Tzu-Min Sun <jimmysun@google.com>
2023-11-22 16:21:12 +0800

    ectool: add battery_config_hex parse bc to hex string
    This CL:
    (1) Adds a cmd to parse struct board_batt_params to hex string
    (2) Adds a cmd battery_config_hex to read json and parse it to hex
        string
    
    BUG=b:285378416
    TEST=(1) cros workon and build ectool binary
         (2) scp the ectool binary to /usr/sbin/
         (3) `ectool battery_config_hex tmp_file.json identity` gets
             the expected stdout hex string
         (4) `ectool cbi set 12 <stdout in (3)>`
         (5) `ectool cbi get 12` get expected outcome
    
    Change-Id: I2fe132e580248456ba45eb9a4d60457af6d3ca81

--- util/ectool.cc
+++ util/ectool.cc
@@ -34,12 +34,16 @@
 #include <time.h>
 
 #include <getopt.h>
+#include <iomanip>
+#include <iostream>
+#include <libchrome/base/json/json_reader.h>
 #include <libec/add_entropy_command.h>
 #include <libec/ec_panicinfo.h>
 #include <libec/fingerprint/fp_encryption_status_command.h>
 #include <libec/flash_protect_command.h>
 #include <libec/rand_num_command.h>
 #include <libec/versions_command.h>
+#include <string>
 #include <unistd.h>
 #include <vector>
 
@@ -8711,6 +8715,146 @@ static int cmd_cbi_is_binary_field(enum cbi_data_tag tag)
 	return tag == CBI_TAG_BATTERY_CONFIG;
 }
 
+static std::string cmd_convert_to_hex(struct board_batt_params &battery_config)
+{
+	unsigned char *p = reinterpret_cast<unsigned char *>(&battery_config);
+	std::vector<unsigned char> bytes(p, p + sizeof(board_batt_params));
+	std::stringstream ss;
+	for (unsigned char i : bytes) {
+		ss << std::hex << std::setw(2) << std::setfill('0') << (int)i;
+	}
+	std::string hex_string = ss.str();
+	return hex_string;
+}
+
+static int cmd_battery_config_hex(int argc, char *argv[])
+{
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <file_path> <battery_identifier>\n",
+			argv[0]);
+		return -1;
+	}
+	FILE *fp;
+	fp = fopen(argv[1], "rb");
+	if (!fp) {
+		fprintf(stderr, "Can't open %s\n", argv[1]);
+		return 1;
+	}
+	char buffer[1024];
+	int len = fread(buffer, 1, sizeof(buffer), fp);
+	fclose(fp);
+	if (len <= 0) {
+		fprintf(stderr, "Failed to read %s\n", argv[1]);
+		return 1;
+	}
+
+	struct board_batt_params battery_config;
+
+	absl::optional<base::Value> root = base::JSONReader::Read(buffer);
+	base::Value::Dict *root_dict = root->GetIfDict();
+	base::Value::Dict *root_dict_identifier = root_dict->FindDict(argv[2]);
+	base::Value::Dict *fuel_gauge =
+		root_dict_identifier->FindDict("fuel_gauge");
+
+	absl::optional<int> flags = fuel_gauge->FindInt("flags");
+	absl::optional<int> board_flags = fuel_gauge->FindInt("board_flags");
+	battery_config.fuel_gauge.flags = flags.value();
+	battery_config.fuel_gauge.board_flags = board_flags.value();
+
+	base::Value::Dict *ship_mode = fuel_gauge->FindDict("ship_mode");
+	absl::optional<int> ship_mode_reg_addr = ship_mode->FindInt("reg_addr");
+	absl::optional<int> ship_mode_reserved = ship_mode->FindInt("reserved");
+	battery_config.fuel_gauge.ship_mode.reg_addr =
+		ship_mode_reg_addr.value();
+	battery_config.fuel_gauge.ship_mode.reserved =
+		ship_mode_reserved.value();
+	base::Value::List *ship_mode_reg_data = ship_mode->FindList("reg_data");
+	for (size_t i = 0;
+	     i < ship_mode_reg_data->size() && i < SHIP_MODE_WRITES; ++i) {
+		absl::optional<int> value = (*ship_mode_reg_data)[i].GetIfInt();
+		battery_config.fuel_gauge.ship_mode.reg_data[i] =
+			static_cast<uint16_t>(value.value());
+	};
+
+	base::Value::Dict *sleep_mode = fuel_gauge->FindDict("sleep_mode");
+	absl::optional<int> sleep_mode_reg_addr =
+		sleep_mode->FindInt("reg_addr");
+	absl::optional<int> sleep_mode_reserved =
+		sleep_mode->FindInt("reserved");
+	absl::optional<int> sleep_mode_reg_data =
+		sleep_mode->FindInt("reg_data");
+	battery_config.fuel_gauge.sleep_mode.reg_addr =
+		sleep_mode_reg_addr.value();
+	battery_config.fuel_gauge.sleep_mode.reserved =
+		sleep_mode_reserved.value();
+	battery_config.fuel_gauge.sleep_mode.reg_data =
+		sleep_mode_reg_data.value();
+
+	base::Value::Dict *fet_info = fuel_gauge->FindDict("fet_info");
+	absl::optional<int> fet_info_reg_addr = fet_info->FindInt("reg_addr");
+	absl::optional<int> fet_info_reserved = fet_info->FindInt("reserved");
+	absl::optional<int> fet_info_reg_mask = fet_info->FindInt("reg_mask");
+	absl::optional<int> fet_info_disconnect_val =
+		fet_info->FindInt("disconnect_val");
+	absl::optional<int> fet_info_cfet_mask = fet_info->FindInt("cfet_mask");
+	absl::optional<int> fet_info_cfet_off_val =
+		fet_info->FindInt("cfet_off_val");
+	battery_config.fuel_gauge.fet.reg_addr = fet_info_reg_addr.value();
+	battery_config.fuel_gauge.fet.reserved = fet_info_reserved.value();
+	battery_config.fuel_gauge.fet.reg_mask = fet_info_reg_mask.value();
+	battery_config.fuel_gauge.fet.disconnect_val =
+		fet_info_disconnect_val.value();
+	battery_config.fuel_gauge.fet.cfet_mask = fet_info_cfet_mask.value();
+	battery_config.fuel_gauge.fet.cfet_off_val =
+		fet_info_cfet_off_val.value();
+
+	base::Value::Dict *batt_info =
+		root_dict_identifiery->FindDict("batt_info");
+
+	absl::optional<int> voltage_max = batt_info->FindInt("voltage_max");
+	absl::optional<int> voltage_normal =
+		batt_info->FindInt("voltage_normal");
+	absl::optional<int> voltage_min = batt_info->FindInt("voltage_min");
+	absl::optional<int> precharge_voltage =
+		batt_info->FindInt("precharge_voltage");
+	absl::optional<int> precharge_current =
+		batt_info->FindInt("precharge_current");
+	absl::optional<int> start_charging_min_c =
+		batt_info->FindInt("start_charging_min_c");
+	absl::optional<int> start_charging_max_c =
+		batt_info->FindInt("start_charging_max_c");
+	absl::optional<int> charging_min_c =
+		batt_info->FindInt("charging_min_c");
+	absl::optional<int> charging_max_c =
+		batt_info->FindInt("charging_max_c");
+	absl::optional<int> discharging_min_c =
+		batt_info->FindInt("discharging_min_c");
+	absl::optional<int> discharging_max_c =
+		batt_info->FindInt("discharging_max_c");
+	absl::optional<int> vendor_param_start =
+		batt_info->FindInt("vendor_param_start");
+	absl::optional<int> batt_info_reserved = batt_info->FindInt("reserved");
+	battery_config.batt_info.voltage_max = voltage_max.value();
+	battery_config.batt_info.voltage_normal = voltage_normal.value();
+	battery_config.batt_info.voltage_min = voltage_min.value();
+	battery_config.batt_info.precharge_voltage = precharge_voltage.value();
+	battery_config.batt_info.precharge_current = precharge_current.value();
+	battery_config.batt_info.start_charging_min_c =
+		start_charging_min_c.value();
+	battery_config.batt_info.start_charging_max_c =
+		start_charging_max_c.value();
+	battery_config.batt_info.charging_min_c = charging_min_c.value();
+	battery_config.batt_info.charging_max_c = charging_max_c.value();
+	battery_config.batt_info.discharging_min_c = discharging_min_c.value();
+	battery_config.batt_info.discharging_max_c = discharging_max_c.value();
+	battery_config.batt_info.vendor_param_start =
+		vendor_param_start.value();
+	battery_config.batt_info.reserved = batt_info_reserved.value();
+
+	printf(cmd_convert_to_hex(battery_config).c_str());
+	return 0;
+}
+
 /*
  * Write value to CBI
  *
@@ -11822,6 +11966,7 @@ const struct command commands[] = {
 	{ "boardversion", cmd_board_version },
 	{ "boottime", cmd_boottime },
 	{ "button", cmd_button },
+	{ "battery_config_hex", cmd_battery_config_hex },
 	{ "cbi", cmd_cbi },
 	{ "chargecurrentlimit", cmd_charge_current_limit },
 	{ "chargecontrol", cmd_charge_control },
