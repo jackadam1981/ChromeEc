f19b501c131239a6d2fde7dac16d48c59c109ab2
Jiahong Wei <weijiahong@huaqin.corp-partner.google.com>
2023-11-24 23:35:20 +0800

    chinchou: Customize to modify the keyboard matrix value
    Customize to modify the keyboard matrix value in order to realize
    the key combination function of fn+f1~f12.
    
    BUG=313149578
    test=Press the key combination fn+f1~f12
    
    Change-Id: Ibe6691ea32c2757d7ef4f27e3c3699eab09c80db

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -300,6 +300,55 @@ static bool power_button_raw_pressed(void)
 	else
 		return false;
 }
+#define BIN(nr) (1U << (nr))
+#define Fn_key 0x4a
+static uint16_t keyboard_fnmap[][3] = {
+	{0x4a,0x02,0x0d},
+	{0x4a,0x32,0x1d},
+	{0x4a,0x22,0x2d},
+	{0x4a,0x12,0x3d},
+	{0x4a,0x34,0x4d},
+	{0x4a,0x24,0x5d},
+	{0x4a,0x14,0x7d},
+	{0x4a,0x29,0x0e},
+	{0x4a,0x19,0x2e},
+	{0x4a,0x04,0x4e},
+	{0x4a,0x01,0x5e},
+	{0x4a,0x15,0x6e},
+};
+
+static void enable_fn_matrix(uint8_t *state)
+{
+	uint8_t *temp = state;
+	uint8_t fn_key_state = 0;
+	uint16_t i,j;
+	if(temp[Fn_key&0xf] == BIN((Fn_key&0xf0)>>4))
+	{
+		for(i = 0; i < keyboard_cols; i++)
+		{
+			if(temp[i] == 0)
+				continue;
+			for(j = 0; j <12 ; j++) {
+				if((temp[i] == BIN((keyboard_fnmap[j][1]&0xf0)>>4))
+					&& (i == (keyboard_fnmap[j][1]&0xf)))
+					{
+						fn_key_state = 1;
+						break;
+					}
+			}
+			if(fn_key_state == 1)
+				break;
+		}
+	}
+
+	if(fn_key_state == 1)
+	{
+		CPRINTS("%d++++++++%d",j,i);
+		state[Fn_key&0xf] = 0;
+		state[i] = 0;
+		state[keyboard_fnmap[j][2]&0xf] = BIN((keyboard_fnmap[j][2]&0xf0)>>4);
+	}
+}
 
 /**
  * Read the raw keyboard matrix state.
@@ -410,7 +459,7 @@ static int read_matrix(uint8_t *state, bool at_boot)
 	}
 
 	keyboard_raw_drive_column(KEYBOARD_COLUMN_NONE);
-
+	enable_fn_matrix(state);
 	return pressed ? 1 : 0;
 }
 
