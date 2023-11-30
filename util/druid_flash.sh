ddd624c98ae50519caaabff2a5e676a6d488cc5b
Craig Hesling <hesling@chromium.org>
2023-11-30 15:09:24 -0800

    DNS: Hack to enable
    Change-Id: Ib43801f3652dea05e7e226d441b056ee84e356df

---
+++ util/druid_flash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DUT="${1:-dut1}"
+
+msg-run() {
+	echo "> $*"
+	"$@"
+}
+
+ssh-run() {
+	msg-run ssh "${DUT}" -- "$@"
+}
+
+ssh-run touch /mnt/stateful_partition/.disable_fp_updater
+msg-run scp build/bloonchipper/ec.bin "${DUT}":/tmp/druid.bin
+ssh-run flash_fp_mcu /tmp/druid.bin
+ssh-run ectool --name=cros_fp version
+ssh-run bio_wash --factory_init
+ssh-run ectool --name=cros_fp rollbackinfo
