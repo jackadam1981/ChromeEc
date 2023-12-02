3c94cbc15f3205c4aa152700ef949ab6244901f3
Li Feng <li1.feng@intel.com>
2023-12-02 00:10:42 -0800

    WIP: rex-ish-ec: handle notebook mode gpio
    gpio_soc_ec_ish_nb_mode_l is an active low pin; from ISH to EC.
    This pin is used to notify EC about current mode, notebook(clamshell)
    mode or tablet mode.
    
    In EC this pin is configured as interrupt. This will trigger tablet mode
    changes in EC.
    
    BUG=b:281038970
    TEST=Flip lid to greater than 180,
    EC log shows:
    tablet mode
    mkbp switches: 3
    Flip lid back to less than 180,
    clamshell mode
    mkbp switches: 1
    TEST=./twister -T zephyr/test/rex -c
    
    Change-Id: Ia50c827f80619454ca9da7db387a33df2f342ed9

---
+++ zephyr/program/rex/rex-ish-ec/include/board.h
@@ -0,0 +1,11 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_NB_MODE_H
+#define __ZEPHYR_NB_MODE_H
+
+void nb_mode_interrupt(enum gpio_signal signal);
+
+#endif /* __ZEPHYR_NB_MODE_H */
