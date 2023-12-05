3ac1e8c244b138060be884fbd514428fd0e83e69
Jay Patel <jay2.patel@intel.com>
2023-12-04 16:40:01 -0800

    retimer: USBC retimer f/w update using PD chips
    Implement functions to enter and exit retimer firmware update mode in
    PD chips.
    
    BUG=b:303206789
    TEST=When TBT device not connected, run following on CPU console
         'echo 1 > /sys/devices/pci0000\:00/0000\:00\:0d.2/domain0/0-0/usb4_port1/offline'
         to enter retimer update mode. Exit the mode using following
         'echo 0 > /sys/devices/pci0000\:00/0000\:00\:0d.2/domain0/0-0/usb4_port1/offline'.
         On entry, the both port 0 and port 1 should go to TBT altmode and
         on exit, they go to NONE altmode.
    
    Change-Id: I0ffeaa0ec558c37b640ed2807faa07e5a7597672

--- zephyr/include/drivers/pdc_ccg8.h
+++ zephyr/include/drivers/pdc_ccg8.h
@@ -4,13 +4,14 @@
  */
 
 /* Header file for Power Delivery chip CCG8 */
+
+#ifndef __ZEPHYR_INCLUDE_DRIVERS_PDC_CCG8_H
+#define __ZEPHYR_INCLUDE_DRIVERS_PDC_CCG8_H
+
 /*
  * Retimer firmware update register: Send 0x01 command to go to Firmware update
  * mode for all the retimers(single/dual port PD) controlled by a CCG8 PD.
  */
-
-#ifndef __ZEPHYR_INCLUDE_DRIVERS_PDC_CCG8_H
-#define __ZEPHYR_INCLUDE_DRIVERS_PDC_CCG8_H
 
 #define PD_ICL_CTRL_REG 0x0040
 #define PD_ICL_CTRL_REG_LEN 1
