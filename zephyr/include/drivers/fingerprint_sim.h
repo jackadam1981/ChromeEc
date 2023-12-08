2d3228cc8ff34cab3e24dee14635956e3e655080
Patryk Duda <patrykd@google.com>
2023-12-08 14:33:55 +0100

    drivers: Introduce fingerprint sensor simulator
    BUG=b:299924239
    TEST=
    
    Change-Id: If4466eaa6dc5a1e6b42cdd378f91226d16cd5716

---
+++ zephyr/include/drivers/fingerprint_sim.h
@@ -0,0 +1,53 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * @file
+ * @brief API for fingerprint sensor simulator
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_FINGERPRINT_SIM_H_
+#define ZEPHYR_INCLUDE_DRIVERS_FINGERPRINT_SIM_H_
+
+#include <drivers/fingerprint.h>
+
+struct fingerprint_sensor_state {
+	uint16_t bad_pixels;
+	bool maintenance_ran;
+	bool detect_mode;
+	bool low_power_mode;
+	enum fingerprint_finger_state finger_state;
+};
+
+/**
+ * @brief Set fingerprint sensor state
+ *
+ * @param dev Pointer to the device structure of the fingerprint sensor
+ *	      simulator.
+ * @param state New fingerprint sensor state.
+ */
+__syscall void fingerprint_set_state(const struct device *dev,
+				     struct fingerprint_sensor_state *state);
+
+/**
+ * @brief Get fingerprint sensor state
+ *
+ * @param dev Pointer to the device structure of the fingerprint sensor
+ *	      simulator.
+ * @param state Fingerprint sensor state.
+ */
+__syscall void fingerprint_get_state(const struct device *dev,
+				     struct fingerprint_sensor_state *state);
+
+/**
+ * @brief Run callback passed to fingerprint sensor driver
+ *
+ * @param dev Pointer to the device structure of the fingerprint sensor
+ *	      simulator.
+ */
+__syscall void fingerprint_run_callback(const struct device *dev);
+
+#include <syscalls/fingerprint_sim.h>
+#endif /* ZEPHYR_INCLUDE_DRIVERS_FINGERPRINT_SIM_H_ */
