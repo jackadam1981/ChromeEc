52b7967e82e18b95ddf9f83ce8c0f886c15b29b8
Daisuke Nojiri <dnojiri@chromium.org>
2023-12-08 10:27:35 -0800

    ectool/bcfg: Don't print message for missing optional parameter
    This CL makes 'ectool bcfg set' command print warning messages for
    missing optional parameters only if the verbose option is set.
    
    BUG=b:285378416
    TEST=Vell. ectool bcfg set /tmp/bcfg.json foo bar
    TEST=Vell. ectool -v bcfg set /tmp/bcfg.json foo bar
    
    Change-Id: I471548f75bdc58b6c582793e7ff70653398682cc

--- util/ectool.cc
+++ util/ectool.cc
@@ -8651,7 +8651,8 @@ static int read_u32_from_json(base::Value::Dict *dict, const char *key,
 	char *e;
 
 	if (str == nullptr) {
-		printf("Key '%s' not found\n", key);
+		if (verbose)
+			printf("Key '%s' not found. Ignored.\n", key);
 		return 0;
 	}
 
