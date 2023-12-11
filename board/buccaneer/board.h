db5408344bf7adf45d6474553788d46c3def538a
Craig Hesling <hesling@chromium.org>
2023-12-11 14:59:28 -0800

    buccaneer: Initial board offering
    Copied from helipilot.
    
    Run ./bazel/gen_bazel_targets.py to update the bazel
    ./bazel/gen_bazel_targets.py.
    
    BUG=b:315858518
    TEST=Doesn't compile
    
    Change-Id: I6ed93d523ebe165e49263a96e34595d4f491bcc9

---
+++ board/buccaneer/board.h
@@ -0,0 +1,22 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Buccaneer board configuration */
+
+#include "baseboard.h"
+
+#ifndef __CROS_EC_BOARD_H
+#define __CROS_EC_BOARD_H
+
+/*-------------------------------------------------------------------------*
+ * Fingerprint Specific
+ *-------------------------------------------------------------------------*
+ */
+
+#ifdef SECTION_IS_RW
+#define CONFIG_FP_SENSOR_FPC1025
+#endif /* SECTION_IS_RW */
+
+#endif /* __CROS_EC_BOARD_H */
