d1a87bcc4a8a9bd96b420aaa9b46d1b9afa5117b
Jay Patel <jay2.patel@intel.com>
2023-12-11 16:11:55 -0800

    ccg8: Add driver implementation
    Add driver APIs for read/write to CCG8 PD. Add CCG8 power path node to
    mtlrvpp_pd device tree. Add console commands to read/write from PD.
    
    BUG=none
    TEST=zmake build mtlrvpp_pd
    TEST=Run 'ccg write 0x1800 <byte0> <byte1> .. <byte3>' and
         run 'ccg read 0x1800 <1-4>'. This will write and read back values.
    
    Change-Id: I764561df7a3c74dd94707d1f03d51147167e36d4

---
+++ zephyr/include/drivers/pdc_ccg8.h
@@ -0,0 +1,20 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Header file for Power Delivery chip CCG8 */
+/*
+ * Retimer firmware update register: Send 0x01 command to go to Firmware update
+ * mode for all the retimers(single/dual port PD) controlled by a CCG8 PD.
+ */
+
+#ifndef __ZEPHYR_INCLUDE_DRIVERS_PDC_CCG8_H
+#define __ZEPHYR_INCLUDE_DRIVERS_PDC_CCG8_H
+
+#define PD_ICL_CTRL_REG 0x0040
+#define PD_ICL_CTRL_REG_LEN 1
+
+#define PD_MAX_READ_WRITE_SIZE 4
+
+#endif /* __ZEPHYR_INCLUDE_DRIVERS_PDC_CCG8_H */
