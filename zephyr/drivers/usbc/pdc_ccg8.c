6ccb324f834816c6177137011dab48a4b1e4bb79
Jay Patel <jay2.patel@intel.com>
2023-12-11 16:11:55 -0800

    ccg8: Add driver implementation
    Add driver APIs for read/write to CCG8 PD. Add CCG8 power path node to
    mtlrvpp_pd device tree. Add console commands to read/write from PD.
    
    BUG=none
    TEST=zmake build mtlrvpp_pd
    TEST=Run 'ccg write 0x1800 <byte0> <byte1> .. <byte3>' and
         run 'ccg read 0x1800 <1-4>'. This will write and read back values.
    
    Change-Id: I764561df7a3c74dd94707d1f03d51147167e36d4

---
+++ zephyr/drivers/usbc/pdc_ccg8.c
@@ -0,0 +1,196 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Source file for PD CCG8 driver */
+
+#include "console.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#include <drivers/pd_chip.h>
+#include <usbc/utils.h>
+
+#define DT_DRV_COMPAT infineon_ccg8
+
+#define CCG8_DEV(inst) DEVICE_DT_GET(DT_DRV_INST(inst)),
+
+/* Generate device object array for available PDs */
+const struct device *pd_pow_config_array[] = { DT_INST_FOREACH_STATUS_OKAY(
+	CCG8_DEV) };
+
+#define PD_MAX_WRITE_SIZE 4
+
+LOG_MODULE_REGISTER(CCG8, LOG_LEVEL_ERR);
+
+struct ccg8_config {
+	/* I2C config */
+	struct i2c_dt_spec i2c;
+	/* Interrupt pin to trigger power event handlers */
+	struct gpio_dt_spec int_gpio;
+	/* Shared interrupt pin in dual port solution */
+	bool shared_irq;
+};
+
+static int pd_write_ccg(const struct device *dev, uint16_t reg, uint8_t len,
+			void *data)
+{
+	const struct ccg8_config *cfg = dev->config;
+	uint8_t i2c_buf[PD_MAX_WRITE_SIZE + 2];
+	struct i2c_msg msg;
+
+	/*
+	 * Write sequence
+	 * DEV_ADDR - REG_ID_0 - REG_ID_1 - DATA0 .. DATAn
+	 */
+	i2c_buf[0] = reg & 0x00ff;
+	i2c_buf[1] = (reg & 0xff00) >> 8;
+	memcpy(&i2c_buf[2], data, len);
+	msg.buf = (uint8_t *)&i2c_buf;
+	msg.len = len + 2;
+	msg.flags = I2C_MSG_WRITE | I2C_MSG_STOP;
+
+	return i2c_transfer_dt(&cfg->i2c, &msg, 1);
+}
+
+static int pd_read_ccg(const struct device *dev, uint16_t reg, uint8_t len,
+		       void *buf)
+{
+	const struct ccg8_config *cfg = dev->config;
+
+	return i2c_write_read_dt(&cfg->i2c, (uint8_t *)&reg, 2, buf, len);
+}
+
+static int pd_ccg8_init(const struct device *dev)
+{
+	const struct ccg8_config *cfg = dev->config;
+
+	if (!i2c_is_ready_dt(&cfg->i2c)) {
+		LOG_ERR("I2C is not ready");
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
+static int pd_intel_retimer_fw_update_ccg(const struct device *dev, bool enable)
+{
+	return pd_write_ccg(dev, PD_ICL_CTRL_REG, PD_ICL_CTRL_REG_LEN,
+			    (uint8_t *)&enable);
+}
+
+static const struct ccg8_driver_api ccg8_api = {
+	.pd_intel_retimer_fw_update = pd_intel_retimer_fw_update_ccg,
+};
+
+/*
+ * TODO : Move console command to application and make it generic for
+ * for all PD chips
+ */
+#ifdef CONFIG_CONSOLE_CMD_PD_CCG8
+static int get_int_val(char *arg, void *reg, size_t val_size)
+{
+	char *e;
+
+	switch (val_size) {
+	case 1:
+		*(uint8_t *)reg = strtol(arg, &e, 0);
+		break;
+	case 2:
+		*(uint16_t *)reg = strtol(arg, &e, 0);
+		break;
+	default:
+		return -EINVAL;
+		break;
+	}
+	if (e != (arg + strlen(arg)))
+		return -EINVAL;
+
+	return 0;
+}
+
+static int cmd_read_register(const struct shell *sh, size_t argc, char **argv)
+{
+	uint16_t reg;
+	uint16_t len;
+	uint8_t red_buf[4] = { 0 };
+	int rv = get_int_val(argv[1], &reg, sizeof(reg));
+
+	if (rv) {
+		shell_print(sh, "Invalid register");
+		return rv;
+	}
+	rv = get_int_val(argv[2], &len, sizeof(len));
+	if (rv) {
+		shell_print(sh, "Invalid length");
+		return rv;
+	}
+	rv = pd_read_ccg(pd_pow_config_array[0], reg, len, red_buf);
+	if (rv) {
+		shell_print(sh, "Read Failed, rv = %d", rv);
+		return rv;
+	}
+	for (int i = 0; i < len; i++)
+		shell_print(sh, "[%d] = %x\n", i, red_buf[i]);
+
+	return 0;
+}
+
+static int cmd_write_register(const struct shell *sh, size_t argc, char **argv)
+{
+	uint16_t reg;
+	uint16_t len = argc - 2 < PD_MAX_WRITE_SIZE ? argc - 2 :
+						      PD_MAX_WRITE_SIZE;
+	uint8_t buf[PD_MAX_WRITE_SIZE];
+	int rv = get_int_val(argv[1], &reg, sizeof(reg));
+
+	if (rv) {
+		shell_print(sh, "Invalid register");
+		return rv;
+	}
+	for (int i = 0; i < len; i++) {
+		rv = get_int_val(argv[2 + i], &buf[i], sizeof(buf[i]));
+		if (rv) {
+			shell_print(sh, "Invalid data");
+			return rv;
+		}
+	}
+	rv = pd_write_ccg(pd_pow_config_array[0], reg, len, buf);
+	if (rv) {
+		shell_print(sh, "Write failed, rv = %d", rv);
+		return rv;
+	}
+
+	return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(ccg_sub_cmds,
+       SHELL_CMD_ARG(read, NULL, "read from ccg PD\n",
+		     cmd_read_register, 3, 0),
+       SHELL_CMD_ARG(write, NULL, "write to ccg PD\n",
+		     cmd_write_register, 3, 3),
+       SHELL_SUBCMD_SET_END);
+
+SHELL_CMD_REGISTER(ccg, &ccg_sub_cmds, "CCG commands\n", NULL);
+#endif
+
+#define CCG8_DEFINE(inst)                                           \
+	static const struct ccg8_config ccg8_config##inst = {       \
+		.i2c = I2C_DT_SPEC_INST_GET(inst),                  \
+		.int_gpio = GPIO_DT_SPEC_INST_GET(inst, irq_gpios), \
+		.shared_irq = DT_INST_PROP(inst, irq_shared),       \
+	};                                                          \
+								    \
+	DEVICE_DT_INST_DEFINE(inst, pd_ccg8_init, NULL, NULL,       \
+			      &ccg8_config##inst, POST_KERNEL,      \
+			      CONFIG_APPLICATION_INIT_PRIORITY, &ccg8_api);
+
+DT_INST_FOREACH_STATUS_OKAY(CCG8_DEFINE)
---
+++ zephyr/include/drivers/pd_chip.h
@@ -0,0 +1,54 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __ZEPHYR_INCLUDE_DRIVERS_PD_CHIP_H
+#define __ZEPHYR_INCLUDE_DRIVERS_PD_CHIP_H
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PD_ICL_CTRL_REG 0x0040
+#define PD_ICL_CTRL_REG_LEN 1
+
+extern const struct device *pd_pow_config_array[];
+
+struct ccg8_driver_api {
+	int (*pd_intel_retimer_fw_update)(const struct device *dev,
+					  bool enable);
+};
+
+/**
+ * @brief Command CCG8 PD chip to enter retimer firmware update mode.
+ *
+ * @param enable True->entry, False->exit retimer firmware update mode.
+ *
+ * @retval 0 if success or I2C error.`
+ * @retval -EIO if input/output error.
+ */
+__syscall int pd_intel_retimer_fw_update(const struct device *dev, bool enable);
+
+static inline int z_impl_pd_intel_retimer_fw_update(const struct device *dev,
+						    bool enable)
+{
+	const struct ccg8_driver_api *api =
+		(const struct ccg8_driver_api *)dev->api;
+
+	return api->pd_intel_retimer_fw_update(dev, enable);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+#include <syscalls/pd_chip.h>
+
+#endif /* __ZEPHYR_INCLUDE_DRIVERS_PD_CHIP_H */
