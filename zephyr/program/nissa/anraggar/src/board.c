03cc034ed743697e298d96a7afa47e519bed258e
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2023-12-12 19:33:32 +0800

    anraggar: Disable motion sense interrupt respond in S0ix
    For reduce EC power consumption, disable motion sense interrupt
    respond in S0ix.
    
    BUG=b:328718619
    TEST=1. zmake build anraggar
         2. 'ectool motionsense' in S0ix
    LOW_COVERAGE_REASON=Tests will be added in b:328718619
    
    Change-Id: Ie4921228ce68a950a6f2fecd7fae13aadcab7c1d

--- zephyr/program/nissa/anraggar/src/board.c
+++ zephyr/program/nissa/anraggar/src/board.c
@@ -10,8 +10,10 @@
  */
 #include "accelgyro.h"
 #include "battery.h"
-#include "common.h"
+#include "console.h"
 #include "cros_cbi.h"
+#include "driver/accel_bma4xx.h"
+#include "driver/accel_lis2dw12_public.h"
 #include "driver/accelgyro_bmi323.h"
 #include "driver/accelgyro_lsm6dsm.h"
 #include "gpio/gpio_int.h"
@@ -22,6 +24,9 @@
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
+
+#define CPRINTS(format, args...) cprints(CC_MOTION_SENSE, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_MOTION_SENSE, format, ##args)
 
 LOG_MODULE_REGISTER(board_init, LOG_LEVEL_ERR);
 
@@ -44,11 +49,15 @@ static void board_setup_init(void)
 		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
 		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
 				      GPIO_INPUT | GPIO_PULL_UP);
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
+				      GPIO_INPUT | GPIO_PULL_UP);
 	}
 }
 DECLARE_HOOK(HOOK_INIT, board_setup_init, HOOK_PRIO_PRE_DEFAULT);
 
 static bool base_use_alt_sensor;
+static bool lid_use_alt_sensor;
 
 void motion_interrupt(enum gpio_signal signal)
 {
@@ -59,14 +68,83 @@ void motion_interrupt(enum gpio_signal signal)
 	}
 }
 
+void lid_accel_interrupt(enum gpio_signal signal)
+{
+	if (lid_use_alt_sensor) {
+		lis2dw12_interrupt(signal);
+	} else {
+		bma4xx_interrupt(signal);
+	}
+}
+
 static void alt_sensor_init(void)
 {
 	base_use_alt_sensor = cros_cbi_ssfc_check_match(
 		CBI_SSFC_VALUE_ID(DT_NODELABEL(base_sensor_lsm6dsm)));
+	lid_use_alt_sensor = cros_cbi_ssfc_check_match(
+		CBI_SSFC_VALUE_ID(DT_NODELABEL(lid_sensor_lis2dw12)));
 
 	motion_sensors_check_ssfc();
 }
 DECLARE_HOOK(HOOK_INIT, alt_sensor_init, HOOK_PRIO_POST_I2C);
+
+static bool detect_flag;
+
+static void motionsense_detect_deferred(void);
+DECLARE_DEFERRED(motionsense_detect_deferred);
+static void motionsense_detect_deferred(void)
+{
+	static bool detect_en;
+
+	if (detect_flag) {
+		detect_en = !detect_en;
+
+		if (detect_en) {
+			CPRINTS("----enable %d", detect_en);
+			gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+			gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+			motion_interrupt(0);
+			hook_call_deferred(&motionsense_detect_deferred_data,
+					1 * SECOND);
+		} else {
+			CPRINTS("----disable %d", detect_en);
+			gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+			gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+			hook_call_deferred(&motionsense_detect_deferred_data,
+					3 * SECOND);
+		}
+	} else {
+		hook_call_deferred(&motionsense_detect_deferred_data, -1);
+	}
+}
+
+static void motionsense_suspend(void)
+{
+	if (!board_is_clamshell) {
+		CPRINTS("--suspend dis");
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		detect_flag = true;
+		hook_call_deferred(&motionsense_detect_deferred_data,
+				   1);
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, motionsense_suspend, HOOK_PRIO_DEFAULT);
+
+static void motionsense_resume(void)
+{
+	if (!board_is_clamshell) {
+		CPRINTS("--resume en");
+		detect_flag = false;
+		gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+		gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		motion_interrupt(0);
+		hook_call_deferred(&motionsense_detect_deferred_data, 0);
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, motionsense_resume, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, motionsense_resume,
+	     MOTION_SENSE_HOOK_PRIO - 1);
 
 enum battery_present battery_hw_present(void)
 {
