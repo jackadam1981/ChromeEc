f68676e4adaa7ed8fd0b5a3e4960cba333316066
Daisuke Nojiri <dnojiri@chromium.org>
2023-12-14 16:44:25 -0800

    SBS: Check error code on every sb_read
    Smart battery spec uses the BatteryStatus register to return an error
    code. With this CL, sb_read will check the error code after every
    (successful) I2C transaction.
    
    BUG=b:316213327
    TEST=Vell
    
    Change-Id: Ic1ba3e8a25752f04f3db2b51dc260dc8dad745f7

--- driver/battery/smart.c
+++ driver/battery/smart.c
@@ -20,6 +20,15 @@
 
 #define BATTERY_NO_RESPONSE_TIMEOUT (1000 * MSEC)
 
+/*
+ * sb_read calls BatteryStatus (0x16) to check an error code of the requested
+ * function. It's not clear whether BatteryStatus reports an error through
+ * itself or not. Define this macro if you suspect a battery pack does so
+ * (instead of failing I2C). It'll make sb_read check the error code of
+ * BatteryStatus by reading BatteryStatus again.
+ */
+#undef SBS_DOUBLE_CHECK_STATUS_READ
+
 static int fake_state_of_charge = -1;
 static int fake_temperature = -1;
 
@@ -79,12 +88,48 @@ static bool sb_cutoff_or_in_progress(void)
 test_mockable int sb_read(int cmd, int *param)
 {
 	uint16_t addr_flags = BATTERY_ADDR_FLAGS;
+	const uint8_t func_status = SB_BATTERY_STATUS;
+	int status;
+	uint8_t error;
+	int rv;
 
 	if (sb_cutoff_or_in_progress())
 		return EC_ERROR_ACCESS_DENIED;
 
 	ADDR_FLAGS_FOR_PEC(&addr_flags);
-	return i2c_read16(I2C_PORT_BATTERY, addr_flags, cmd, param);
+	rv = i2c_read16(I2C_PORT_BATTERY, addr_flags, cmd, param);
+	if (rv) {
+		CPRINTS("Failed to call func 0x%02x (%d)", cmd, rv);
+		return rv;
+	}
+
+	/* Check the error code. */
+	rv = i2c_read16(I2C_PORT_BATTERY, addr_flags, func_status, &status);
+	if (rv) {
+		CPRINTS("Failed to read error for func 0x%02x (%d)", cmd, rv);
+		return rv;
+	}
+	error = status & STATUS_ERR_CODE_MASK;
+	if (error) {
+		CPRINTS("Func 0x%02x returned error 0x%x", cmd, error);
+		return EC_ERROR_UNKNOWN;
+	}
+
+#ifdef SBS_DOUBLE_CHECK_STATUS_READ
+	/* Check the error code for SB_BATTERY_STATUS. */
+	rv = i2c_read16(I2C_PORT_BATTERY, addr_flags, func_status, &status);
+	if (rv) {
+		CPRINTS("Failed to call func 0x%02x (%d)", func_status, rv);
+		return rv;
+	}
+	error = status & STATUS_ERR_CODE_MASK;
+	if (error) {
+		CPRINTS("Func 0x%02x returned error 0x%x", func_status, error);
+		return EC_ERROR_UNKNOWN;
+	}
+#endif
+
+	return EC_SUCCESS;
 }
 
 test_mockable int sb_write(int cmd, int param)
