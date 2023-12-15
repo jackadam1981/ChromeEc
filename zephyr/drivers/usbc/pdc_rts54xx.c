087ea2c88b891cb243e78aaff188e1031085511d
Sam Hurst <shurst@google.com>
2023-12-15 06:34:57 -0800

    PDC_Driver: Properly read VBUS from register
    Read VBUS returned in the read buffer from bytes
    1 and 2, instead of 0 and 1. Byte 0 contains the
    number of following bytes.
    
    BUG=b:305014156
    TEST=Verified that VBUS is read correctly by plugging
    in a charger.
    
    Change-Id: Ia7ab7b1accfff9f7de0580aba357fffd36b8e6cb

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -691,8 +691,14 @@ static void st_read_run(void *o)
 			(data->pdc_pd_version & 0xffff));
 		break;
 	case CMD_GET_VBUS_VOLTAGE:
+		/*
+		 * Realtek Voltage reading is on Byte16 and Byte17, but
+		 * the READ_RTK_STATUS command was issued with reading
+		 * 2-bytes from offset 16, so the data is read from
+		 * rd_buf at Byte1 and Byte2.
+		 */
 		*(uint16_t *)data->user_buf =
-			(data->rd_buf[1] << 8 | data->rd_buf[0]) * 50;
+			((data->rd_buf[2] << 8) | data->rd_buf[1]) * 50;
 		break;
 	case CMD_GET_CONNECTOR_STATUS:
 		/* Map Realtek GET_RTK_STATUS bits to UCSI GET_CONNECTOR_STATUS
