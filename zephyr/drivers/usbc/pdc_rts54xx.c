2c3c78f8e92bb2adfe182ded93c80208d343ac92
Sam Hurst <shurst@google.com>
2023-12-18 07:12:01 -0800

    PDC_Driver: Assertions for proper state transition
    Add assertions to guarantee proper state transitions
    IDLE and INIT ---> WRITE
    WRITE         ---> PING_STATUS
    PING_STATUS   ---> READ or (IDLE or INIT)
    READ          ---> IDLE or INIT
    
    BUG=b:305014156
    TEST=Verified assertion on incorrect state transition
    
    Change-Id: I246d382ed774b39a3645fdeff85e4948f55a4a53

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -7,6 +7,7 @@
  * Realtek RTS545x Power Delivery Controller Driver
  */
 
+#include <assert.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
@@ -498,6 +499,9 @@ static void st_write_entry(void *o)
 
 	print_current_state(data);
 
+	/* This state can only be entered from the Init and Idle states */
+	assert(data->last_state == ST_INIT || data->last_state == ST_IDLE);
+
 	/* Clear I2C transaction retry counter */
 	data->i2c_transaction_retry_counter = 0;
 	/* Clear the Error Status */
@@ -552,6 +556,10 @@ static void st_ping_status_entry(void *o)
 
 	print_current_state(data);
 
+	/* This state can only be entered from the Write Status state */
+	assert(data->last_state == ST_WRITE);
+
+	/* Clear the Error Status */
 	/* Clear I2c Transaction Retry Counter */
 	data->i2c_transaction_retry_counter = 0;
 	/* Clear Ping Rety Counter */
@@ -709,6 +717,9 @@ static void st_read_entry(void *o)
 	const struct pdc_config_t *cfg = data->dev->config;
 
 	print_current_state(data);
+
+	/* This state can only be entered from the Ping Status state */
+	assert(data->last_state == ST_PING_STATUS);
 
 	/* Clear the Error Status */
 	data->error_status.raw_value = 0;
