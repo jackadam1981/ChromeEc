90d32edf757489b9885e6081ea25554977eda86f
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2023-12-19 13:36:54 +0800

    [TEST_ONLY] isl9214: disable way overcurrent detection
    Change-Id: Ifa33221436eb36a0480274a35105396df652456a

--- driver/charger/isl9241.c
+++ driver/charger/isl9241.c
@@ -1128,6 +1128,14 @@ static void isl9241_init(int chgnum)
 			   ISL9241_CONTROL4_SLEW_RATE_CTRL, MASK_SET))
 		goto init_fail;
 
+	/*
+	 * Set control4 register to
+	 * [9]: Disable way overcurrent detection
+	 */
+	if (isl9241_update(chgnum, ISL9241_REG_CONTROL4,
+			   ISL9241_CONTROL4_WOCP, MASK_SET))
+		goto init_fail;
+
 #ifndef CONFIG_CHARGE_RAMP_HW
 	if (isl9241_update(chgnum, ISL9241_REG_CONTROL0,
 			   ISL9241_CONTROL0_INPUT_VTG_REGULATION, MASK_SET))
