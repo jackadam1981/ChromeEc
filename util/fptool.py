dd8e0a076c7460901d3d7fa0e271867537136cbb
Craig Hesling <hesling@chromium.org>
2023-12-19 00:00:40 -0500

    fptool.py: Clean up flash subcommand
    BUG=b:176500425
    
    Change-Id: Ieafac65fa79aa19db88f07152324bb2629565d10

--- util/fptool.py
+++ util/fptool.py
@@ -8,24 +8,25 @@
 import argparse
 import os
 import pathlib
+import shlex
 import shutil
 import subprocess
 import sys
-from typing import List
+from typing import List, Optional
 
 
-DISABLE_UPDATER_FILE = pathlib.Path(
+UPDATER_DISABLE_FILE = pathlib.Path(
     "/mnt/stateful_partition/.disable_fp_updater"
 )
 
 
 def run(cmd: List[str]) -> int:
     """Run a command locally with the command echoed first."""
-    print(f'> {" ".join(cmd)}')
+    print(f"> {shlex.join(cmd)}")
     sys.stdout.flush()
-    p = subprocess.run(cmd)  # pylint: disable=subprocess-run-check
+    p = subprocess.run(cmd, check=False)
     if p.returncode:
-        print(f"Returned error status {p.returncode}.")
+        print(f"Returned error status {p.returncode}.", file=sys.stderr)
     return p.returncode
 
 
@@ -35,32 +36,57 @@ def remote_run(host: str, port: int, remote_cmd: List[str]) -> int:
     return run(cmd)
 
 
-def cmd_flash(args: argparse.Namespace) -> int:
+def scp_file(host: str, port: int, src: pathlib.Path, dst: pathlib.Path) -> int:
+    """Copy one file to remote host over scp."""
+    scp_cmd = ["scp", f"-P{port}", src, f"{host}:{dst}"]
+    return run(scp_cmd)
+
+
+def cmd_flash(opts: argparse.Namespace) -> int:
     """Flash the entire firmware FPMCU using the native bootloader.
 
     This requires the Chromebook to be in dev mode with hardware write protect
     disabled.
+
+    Args:
+        opts:
+            The argparse.Namespace that includes the optional `image` parameter,
+            which is the path to the firmware image file.
+
+    Returns:
+        0 on success, non-zero upon error.
     """
 
+    image: Optional[pathlib.Path] = opts.image
+
     if not shutil.which("flash_fp_mcu"):
-        print("Error - The flash_fp_mcu utility does not exist.")
+        print(
+            "Error - The flash_fp_mcu utility does not exist.", file=sys.stderr
+        )
         return 1
 
     cmd = ["flash_fp_mcu"]
-    if args.image:
-        if not os.path.isfile(args.image):
-            print(f"Error - image {args.image} is not a file.")
+    if image:
+        if not image.is_file():
+            print(f"Error - image {image} is not a file.", file=sys.stderr)
             return 1
-        cmd.append(args.image)
+        cmd.append(str(image))
 
-    print(f'Running {" ".join(cmd)}.')
-    sys.stdout.flush()
-    p = subprocess.run(cmd)  # pylint: disable=subprocess-run-check
-    return p.returncode
+    return run(cmd)
 
 
 def updater_disable(disable: bool) -> int:
-    """Disable or enable the fingerprint updater."""
+    """Disable or re-enable the fingerprint updater.
+
+    Args:
+        disable:
+            When True, disable auto updates.
+            When False, resume auto updates.
+
+    Returns:
+        0 on success, non-zero upon error.
+    """
+
     if disable:
         print("# Disabling fingerprint updater.")
         action = "touch"
@@ -68,25 +94,25 @@ def updater_disable(disable: bool) -> int:
         print("# Enabling fingerprint updater.")
         action = "rm"
 
-    ret = run([action, str(DISABLE_UPDATER_FILE)])
+    ret = run([action, str(UPDATER_DISABLE_FILE)])
     if ret:
-        print(f"Failed to {action} {DISABLE_UPDATER_FILE}.")
+        print(f"Failed to {action} {UPDATER_DISABLE_FILE}.", file=sys.stderr)
         return ret
 
     ret = run(["sync"])
     if ret:
-        print(f"Failed to sync {DISABLE_UPDATER_FILE}.")
+        print(f"Failed to sync {UPDATER_DISABLE_FILE}.", file=sys.stderr)
         return ret
 
     return 0
 
 
-def cmd_updater_enable(args: argparse.Namespace) -> int:
+def cmd_updater_enable(opts: argparse.Namespace) -> int:
     """Enable fingerprint firmware updater."""
     return updater_disable(False)
 
 
-def cmd_updater_disable(args: argparse.Namespace) -> int:
+def cmd_updater_disable(opts: argparse.Namespace) -> int:
     """Disable fingerprint firmware updater."""
     return updater_disable(True)
 
@@ -102,56 +128,55 @@ def remote_updater_disable(host: str, port: int, disable: bool) -> int:
         action = "rm"
 
     ret = remote_run(
-        host, port, [action, str(DISABLE_UPDATER_FILE), ";", "sync"]
+        host, port, [action, str(UPDATER_DISABLE_FILE), ";", "sync"]
     )
     if ret:
-        print(f"Failed to {action} {DISABLE_UPDATER_FILE}.")
+        print(f"Failed to {action} {UPDATER_DISABLE_FILE}.", file=sys.stderr)
         return ret
 
     return 0
 
 
-def cmd_remote_updater_enable(args: argparse.Namespace) -> int:
+def cmd_remote_updater_enable(opts: argparse.Namespace) -> int:
     """Enable fingerprint firmware updater on remote DUT."""
-    return remote_updater_disable(args.host, args.port, False)
+    return remote_updater_disable(opts.host, opts.port, False)
 
 
-def cmd_remote_updater_disable(args: argparse.Namespace) -> int:
+def cmd_remote_updater_disable(opts: argparse.Namespace) -> int:
     """Disable fingerprint firmware updater on remote DUT."""
-    return remote_updater_disable(args.host, args.port, True)
+    return remote_updater_disable(opts.host, opts.port, True)
 
 
-def cmd_remote_flash(args: argparse.Namespace) -> int:
+def cmd_remote_flash(opts: argparse.Namespace) -> int:
     """Remotely flash the entire firmware FPMCU using the native bootloader.
 
     This requires the Chromebook to be in dev mode with hardware write protect
     disabled.
     """
 
-    REMOTE_TMP_IMG = "/tmp/fpmcu-fw.bin"
+    REMOTE_TMP_IMG = pathlib.Path("/tmp/fpmcu-fw.bin")
 
-    host = args.host
-    port = args.port
-    image = args.image
+    host: str = opts.host
+    port: int = opts.port
+    image: Optional[pathlib.Path] = opts.image
 
     image_name = image if image else "default rootfs FW"
     print(f"Flashing {image_name} to {host}:{port}.")
 
     if image:
         print(f"# Copying image {image} to {host}.")
-        scp_cmd = ["scp", f"-P{port}", str(image), f"{host}:{REMOTE_TMP_IMG}"]
-        ret = run(scp_cmd)
+        ret = scp_file(host, port, image, REMOTE_TMP_IMG)
         if ret:
-            print("Failed to copy image to host.")
+            print("Failed to copy image to host.", file=sys.stderr)
             return ret
 
     print("# Flashing copied image.")
     remote_flash_cmd = ["fptool", "flash"]
     if image:
-        remote_flash_cmd += [REMOTE_TMP_IMG]
+        remote_flash_cmd += [str(REMOTE_TMP_IMG)]
     ret = remote_run(host, port, remote_flash_cmd)
     if ret:
-        print("Failed to flash FPMCU.")
+        print("Failed to flash FPMCU.", file=sys.stderr)
         return ret
 
     return 0
@@ -166,7 +191,7 @@ def main(argv: list) -> int:
     # Parser for "flash" subcommand.
     parser_flash = subparsers.add_parser("flash", help=cmd_flash.__doc__)
     parser_flash.add_argument(
-        "image", nargs="?", help="Path to the firmware image"
+        "image", nargs="?", type=pathlib.Path, help="Path to the firmware image"
     )
     parser_flash.set_defaults(func=cmd_flash)
 
@@ -183,7 +208,8 @@ def main(argv: list) -> int:
     # Parser with subcommands for "remote [options] <host> <subcommand>".
     parser_remote = subparsers.add_parser(
         "remote",
-        help="Commands for working with the fingerprint subsystem on a remote DUT.",
+        help="Commands for working with the fingerprint subsystem on a remote "
+        "DUT.",
     )
     parser_remote.add_argument(
         "--port", default=22, type=int, help="Target SSH port number."
