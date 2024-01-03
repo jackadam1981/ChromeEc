7320afe8b8c21ea6fc744af43c1b61bb516e5b88
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2024-01-03 13:23:23 -0800

    subsys/ap_pwrseq: Make RSMRST to support both edges
    Logic around RSMRST input and output power signals assumes that rising edge
    is supported only. This CL make RSMRST power signals assertion to be
    handled regardless of its edge configuation.
    
    BUG=b:317810253
    TEST=build and flash EC
         Platform should boot
    
    Change-Id: I122a8eca7c0264a3ed6f4dd6bacd335eef7810f7

--- zephyr/program/rex/src/board_power.c
+++ zephyr/program/rex/src/board_power.c
@@ -38,12 +38,12 @@ void board_ap_power_force_shutdown(void)
 	power_signal_set(PWR_EN_PP3300_A, 0);
 
 	/* Wait RSMRST to be off. */
-	while (power_signal_get(PWR_RSMRST) && (timeout_ms > 0)) {
+	while (!power_signal_get(PWR_RSMRST) && (timeout_ms > 0)) {
 		k_msleep(1);
 		timeout_ms--;
 	};
 
-	if (power_signal_get(PWR_RSMRST)) {
+	if (!power_signal_get(PWR_RSMRST)) {
 		LOG_WRN("RSMRST_ODL didn't go low!  Assuming G3.");
 	}
 }
@@ -56,7 +56,7 @@ void board_ap_power_action_g3_s5(void)
 
 	update_ap_boot_time(ARAIL);
 
-	if (!power_wait_signals_timeout(
+	if (!power_wait_signals_off_timeout(
 		    IN_PGOOD_ALL_CORE,
 		    AP_PWRSEQ_DT_VALUE(wait_signal_timeout))) {
 		ap_power_ev_send_callbacks(AP_POWER_PRE_INIT);
--- zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
@@ -318,7 +318,7 @@ void ap_power_reset(enum ap_power_shutdown_reason reason)
 int rsmrst_power_is_good(void)
 {
 	/* TODO: Check if this is still intact */
-	return power_signal_get(PWR_RSMRST);
+	return !power_signal_get(PWR_RSMRST);
 }
 
 /* Handling RSMRST signal is mostly common across x86 chipsets */
@@ -330,7 +330,7 @@ void rsmrst_pass_thru_handler(void)
 	int out_sig_val = power_signal_get(PWR_EC_PCH_RSMRST);
 
 	if (in_sig_val != out_sig_val) {
-		if (in_sig_val)
+		if (!in_sig_val)
 			k_msleep(AP_PWRSEQ_DT_VALUE(rsmrst_delay));
 		LOG_DBG("Setting PWR_EC_PCH_RSMRST to %d", in_sig_val);
 		power_signal_set(PWR_EC_PCH_RSMRST, in_sig_val);
@@ -842,7 +842,7 @@ static int x86_non_dsx_s5_run(void *data)
 	 * have already checked that required power rails are OK.
 	 */
 	rsmrst_pass_thru_handler();
-	if (power_signal_get(PWR_EC_PCH_RSMRST)) {
+	if (!power_signal_get(PWR_EC_PCH_RSMRST)) {
 		if (signals_valid_and_off(IN_PCH_SLP_S5)) {
 			return ap_pwrseq_sm_set_state(data, AP_POWER_STATE_S4);
 		}
@@ -873,7 +873,7 @@ AP_POWER_ARCH_STATE_DEFINE(AP_POWER_STATE_S5, x86_non_dsx_s5_entry,
 
 static int x86_non_dsx_s4_run(void *data)
 {
-	if (power_signal_get(PWR_RSMRST) == 0 ||
+	if (power_signal_get(PWR_RSMRST) ||
 	    signals_valid_and_on(IN_PCH_SLP_S5)) {
 		return ap_pwrseq_sm_set_state(data, AP_POWER_STATE_S5);
 	}
@@ -896,7 +896,7 @@ AP_POWER_ARCH_STATE_DEFINE(AP_POWER_STATE_S4, NULL, x86_non_dsx_s4_run, NULL);
 
 static int x86_non_dsx_s3_run(void *data)
 {
-	if (power_signal_get(PWR_RSMRST) == 0 ||
+	if (power_signal_get(PWR_RSMRST) ||
 	    signals_valid_and_on(IN_PCH_SLP_S4)) {
 		return ap_pwrseq_sm_set_state(data, AP_POWER_STATE_S4);
 	}
