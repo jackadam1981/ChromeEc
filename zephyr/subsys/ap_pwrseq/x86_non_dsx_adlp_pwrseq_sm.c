a8c12b2efa1e7eaf3be085b4393006e418dca729
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2024-01-03 15:54:55 -0800

    subsys/ap_pwrseq: Add chipset funcions to determine power good
    Power signals required to determine AP power good can be active low or may
    require further logic. This CL will make corresponding chipset to provide
    its implementation of functions `chipset_is_all_power_good` and
    `chipset_is_vw_power_good`.
    
    BUG=b:317810253
    TEST=Build and flash EC
         Platform should boot
    
    Change-Id: Iaf8bd215787a15d2ee893c240a11a6f3f3519765

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_adlp_pwrseq_sm.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_adlp_pwrseq_sm.c
@@ -10,6 +10,19 @@
 
 LOG_MODULE_DECLARE(ap_pwrseq, CONFIG_AP_PWRSEQ_LOG_LEVEL);
 
+bool chipset_is_all_power_good(void)
+{
+	return !power_signal_get(PWR_SLP_SUS) &&
+	       power_signal_get(PWR_DSW_PWROK);
+}
+
+bool chipset_is_vw_power_good(void)
+{
+	return power_signal_get(PWR_RSMRST_PWRGD) &&
+	       power_signal_get(PWR_DSW_PWROK) &&
+	       !power_signal_get(PWR_SLP_SUS);
+}
+
 /* The wait time is ~150 msec, allow for safety margin. */
 #define IN_PCH_SLP_SUS_WAIT_TIME_MS 250
 
