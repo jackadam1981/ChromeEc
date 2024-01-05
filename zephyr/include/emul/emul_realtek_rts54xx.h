961039c3ae26165efd8f046798e60b3e0567bc0f
Al Semjonovs <asemjonovs@google.com>
2024-01-05 14:52:43 -0700

    pdc: Add RTS54 command table and handlers
    Initial support for RTS54 commands and handling.
    
    BUG=b:317065172
    TEST=./twister -c  -p native_posix -T zephyr/test/pdc/
    
    Change-Id: I82c3eb14ceadc4dc52e9f9f7f25c19ee315a8519

--- zephyr/include/emul/emul_realtek_rts54xx.h
+++ zephyr/include/emul/emul_realtek_rts54xx.h
@@ -19,9 +19,63 @@
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/drivers/i2c_emul.h>
 
+struct rts54_command {
+	uint8_t command_code;
+	uint8_t byte_count;
+	uint8_t data[32];
+};
+
+struct rts54_ic_status {
+	uint8_t byte_count;
+	uint8_t is_flash_code;
+	uint8_t reserved0[2];
+	uint8_t fw_main_version;
+	uint8_t fw_sub_version[2];
+	uint8_t reserved1[2];
+	uint8_t pd_ready : 1;
+	uint8_t reserved2 : 2;
+	uint8_t typec_connected : 1;
+	uint8_t reserved3 : 4;
+	uint8_t vid[2];
+	uint8_t pid[2];
+	uint8_t reserved4;
+	uint8_t running_flash_bank_offset;
+	uint8_t reserved5[7];
+	uint8_t pd_revision[2];
+	uint8_t pd_version[2];
+	uint8_t reserved6[6];
+};
+
+enum cmd_sts_t {
+	/** Command has not been started */
+	CMD_BUSY = 0,
+	/** Command has completed */
+	CMD_COMPLETE = 1,
+	/** Command has been started but has not completed */
+	CMD_DEFERRED = 2,
+	/** Command completed with error. Send GET_ERROR_STATUS for details */
+	CMD_ERROR = 3,
+};
+
+struct ping_status {
+	/** Command status */
+	uint8_t cmd_sts : 2;
+	/** Length of data read to read */
+	uint8_t data_len : 6;
+};
+
 /** @brief Emulated properties */
 struct rts5453p_emul_pdc_data {
-	uint8_t tbd; /* TBD fill with emulated PD controller data */
+	uint8_t vnd_command;
+	struct rts54_ic_status ic_status;
+	struct rts54_command request;
+
+	bool read_ping;
+	union {
+		struct ping_status ping_status;
+		uint8_t ping_raw_value;
+	};
+	uint8_t response[32];
 };
 
 /**
