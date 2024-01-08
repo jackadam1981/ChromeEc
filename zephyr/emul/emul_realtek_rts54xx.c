ea063657d0e40f71fc2f33ec877c9680a9241aeb
Al Semjonovs <asemjonovs@google.com>
2024-01-08 13:58:47 -0700

    pdc: Add connector reset test
    Add PDC connector reset test
    
    BUG=b:317065172
    TEST=./twister -c  -p native_posix -T zephyr/test/pdc/
    LOW_COVERAGE_REASON=Incremental change for coverage b:317065172
    
    Change-Id: I9656a41a630b714203c7e1051bbae1171128da03

--- zephyr/emul/emul_realtek_rts54xx.c
+++ zephyr/emul/emul_realtek_rts54xx.c
@@ -122,11 +122,23 @@ static int ppm_reset(struct rts5453p_emul_pdc_data *data,
 	return 0;
 }
 
+static int connector_reset(struct rts5453p_emul_pdc_data *data,
+			   const union rts54_request *req)
+{
+	LOG_INF("CONNECTOR_RESET port=%d, hard_reset=%d",
+		req->connector_reset.port_num, req->connector_reset.hard_reset);
+
+	memset(&data->response, 0, sizeof(union rts54_response));
+	send_response(data);
+
+	return 0;
+}
+
 static int tcpm_reset(struct rts5453p_emul_pdc_data *data,
 		      const union rts54_request *req)
 {
-	LOG_INF("%s", __func__);
-
+	LOG_INF("TCPM_RESET port=%d, reset_type=0x%X", req->tcpm_reset.port_num,
+		req->tcpm_reset.reset_type);
 	memset(&data->response, 0, sizeof(union rts54_response));
 	send_response(data);
 
@@ -222,7 +234,7 @@ const struct commands sub_cmd_x08[] = {
 
 const struct commands sub_cmd_x0E[] = {
 	{ .code = 0x01, HANDLER_DEF(ppm_reset) },
-	{ .code = 0x03, HANDLER_DEF(unsupported) },
+	{ .code = 0x03, HANDLER_DEF(connector_reset) },
 	{ .code = 0x06, HANDLER_DEF(unsupported) },
 	{ .code = 0x07, HANDLER_DEF(unsupported) },
 	{ .code = 0x09, HANDLER_DEF(unsupported) },
@@ -479,6 +491,7 @@ static int emul_realtek_rts54xx_set_response_delay(const struct emul *target,
 	struct rts5453p_emul_pdc_data *data =
 		rts5453p_emul_get_pdc_data(target);
 
+	LOG_INF("set_response_delay delay_ms=%d", delay_ms);
 	data->delay_ms = delay_ms;
 
 	return 0;
