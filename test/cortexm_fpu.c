ae4f5ab94dfe9438a29dd9a3b982b234ae7b82a4
Firas Sammoura <fsammoura@google.com>
2024-01-09 10:43:13 -0800

    mcutest: Add _read_fpscr function to read fpscr register
    BUG=b:298697667
    TEST=run_device_tests.py -b helipilot -f servo_micro
    TEST=run_device_tests.py -b dartmonkey -f servo_micro
    TEST=run_device_tests.py -b bloonchipper -f servo_micro
    
    Change-Id: Ieca65642ae52b97f41fb8f0860d0c00b0b275fde

--- test/cortexm_fpu.c
+++ test/cortexm_fpu.c
@@ -13,6 +13,24 @@
 static volatile uint32_t fpscr;
 static volatile bool fpu_irq_handled;
 
+<<<<<<< HEAD
+=======
+static uint32_t _read_fpscr(void)
+{
+	uint32_t val;
+
+	asm volatile("vmrs %0, fpscr" : "=r"(val));
+	return val;
+}
+
+static void clear_fpscr(void)
+{
+	uint32_t val = 0;
+
+	asm volatile("vmsr fpscr, %0" : : "r"(val));
+}
+
+>>>>>>> c0703fc082 (mcutest: Add _read_fpscr function to read fpscr register)
 /* Override default FPU interrupt handler. */
 void __keep fpu_irq(uint32_t excep_lr, uint32_t excep_sp)
 {
