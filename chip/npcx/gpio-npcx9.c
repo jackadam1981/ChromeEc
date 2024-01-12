052a49dc596dff617f9891adcaddc2f5217efafa
Firas Sammoura <fsammoura@google.com>
2024-01-11 16:01:07 -0800

    test: rtc test for helipilot
    BUG=
    TEST=
    
    Change-Id: If205166a6460d152385514f6c12f252995797686
    Signed-off-by: Firas Sammoura <fsammoura@google.com>

--- chip/npcx/gpio-npcx9.c
+++ chip/npcx/gpio-npcx9.c
@@ -99,8 +99,14 @@ static void set_rtc_host_event(void)
 DECLARE_DEFERRED(set_rtc_host_event);
 #endif
 
+test_mockable_static void rtc_interrupt_handler(void)
+{
+}
+
 static void __gpio_rtc_interrupt(void)
 {
+	rtc_interrupt_handler();
+
 	/* Check pending bit 7 */
 #ifdef CONFIG_HOSTCMD_RTC
 	if (NPCX_WKPND(MIWU_TABLE_0, MIWU_GROUP_4) & 0x80) {
