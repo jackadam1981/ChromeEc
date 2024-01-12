1d9647a96e37d7a7057c1853cba2c9435d77b8cc
Firas Sammoura <fsammoura@google.com>
2024-01-11 16:01:07 -0800

    test: rtc test for helipilot
    BUG=
    TEST=
    
    Change-Id: If205166a6460d152385514f6c12f252995797686
    Signed-off-by: Firas Sammoura <fsammoura@google.com>

--- test/stm32f_rtc.c
+++ test/stm32f_rtc.c
@@ -1,77 +1,74 @@
-/* Copyright 2020 The ChromiumOS Authors
+/* Copyright 2024 The ChromiumOS Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
+#include "atomic.h"
 #include "clock_chip.h"
+#include "system.h"
 #include "test_util.h"
+#include "timer.h"
 
+static const int rtc_delay_s = 2;
+static uint32_t interrupt_counter = 0;
 static uint32_t rtc_fired;
-static struct rtc_time_reg rtc_irq;
-static const int rtc_delay_ms = 500;
 
-/*
- * We will be testing that the RTC interrupt timestamp occurs
- * within +/- delay_tol_us (tolerance) of the above rtc_delay_ms.
- */
-static const int delay_tol_us = MSEC / 2;
-
-/* Override default RTC interrupt handler */
-void rtc_alarm_irq(void)
+void rtc_interrupt_handler(void)
 {
-	atomic_add(&rtc_fired, 1);
-	reset_rtc_alarm(&rtc_irq);
+	interrupt_counter++;
 }
 
-test_static int test_rtc_alarm(void)
+test_static int test_rtc_alarm_fired(void)
 {
-	struct rtc_time_reg rtc;
-	uint32_t rtc_diff_us;
-	const int delay_us = rtc_delay_ms * MSEC;
+	uint32_t interrupt_counter_0 = interrupt_counter;
+	system_set_rtc_alarm(rtc_delay_s, 0);
 
-	set_rtc_alarm(0, delay_us, &rtc, 0);
+	sleep(2 * rtc_delay_s);
 
-	msleep(2 * rtc_delay_ms);
+	uint32_t interrupt_counter_1 = interrupt_counter;
 
-	/* Make sure the interrupt fired exactly once. */
-	TEST_EQ(1, atomic_clear(&rtc_fired), "%d");
+	rtc_fired = interrupt_counter_1 - interrupt_counter_0;
 
-	rtc_diff_us = get_rtc_diff(&rtc, &rtc_irq);
+	TEST_EQ(1, rtc_fired, "%d");
 
-	ccprintf("Target delay was %dus\n", delay_us);
-	ccprintf("Actual delay was %dus\n", rtc_diff_us);
-	ccprintf("The delays are expected to be within +/- %dus\n", MSEC / 2);
-
-	/* Assume we'll always fire within 500us. May need to be adjusted if
-	 * this doesn't hold.
-	 *
-	 * delay_us-delay_tol_us < rtc_diff_us < delay_us+delay_tol_us
-	 */
-	TEST_LT(delay_us - delay_tol_us, rtc_diff_us, "%dus");
-	TEST_LT(rtc_diff_us, delay_us + delay_tol_us, "%dus");
+	TEST_EQ(0, system_get_rtc_alarm(), "%d");
 
 	return EC_SUCCESS;
 }
 
-static const int rtc_match_delay_iterations = 5000;
-
-test_static int test_rtc_match_delay(void)
+test_static int test_rtc_alarm_not_fired(void)
 {
-	struct rtc_time_reg rtc;
-	int i;
+	uint32_t interrupt_counter_0 = interrupt_counter;
+	system_set_rtc_alarm(rtc_delay_s, 0);
 
-	atomic_clear(&rtc_fired);
-	for (i = 0; i < rtc_match_delay_iterations; i++) {
-		set_rtc_alarm(0, SET_RTC_MATCH_DELAY, &rtc, 0);
-		usleep(2 * SET_RTC_MATCH_DELAY);
+	sleep(0.5 * rtc_delay_s);
+
+	uint32_t interrupt_counter_1 = interrupt_counter;
+
+	rtc_fired = interrupt_counter_1 - interrupt_counter_0;
+
+	TEST_EQ(0, rtc_fired, "%d");
+
+	TEST_EQ(1, system_get_rtc_alarm(), "%d");
+
+	return EC_SUCCESS;
+}
+
+static const int rtc_alarm_iterations = 3;
+
+test_static int test_rtc_series_alarm_fired(void)
+{
+	uint32_t interrupt_counter_0 = interrupt_counter;
+
+	for (int i = 0; i < rtc_alarm_iterations; ++i) {
+		system_set_rtc_alarm(rtc_delay_s, 0);
+		sleep(2 * rtc_delay_s);
 	}
+	uint32_t interrupt_counter_1 = interrupt_counter;
 
-	ccprintf("Expected number of RTC alarm interrupts %d\n",
-		 rtc_match_delay_iterations);
-	ccprintf("Actual number of RTC alarm interrupts %d\n", rtc_fired);
+	rtc_fired = interrupt_counter_1 - interrupt_counter_0;
 
-	/* Make sure each set_rtc_alarm() generated the interrupt. */
-	TEST_EQ(rtc_match_delay_iterations, atomic_clear(&rtc_fired), "%d");
+	TEST_EQ(rtc_alarm_iterations, rtc_fired, "%d");
 
 	return EC_SUCCESS;
 }
@@ -80,8 +77,9 @@ void run_test(int argc, const char **argv)
 {
 	test_reset();
 
-	RUN_TEST(test_rtc_alarm);
-	RUN_TEST(test_rtc_match_delay);
+	RUN_TEST(test_rtc_alarm_fired);
+	RUN_TEST(test_rtc_alarm_not_fired);
+	RUN_TEST(test_rtc_series_alarm_fired);
 
 	test_print_result();
 }
