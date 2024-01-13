d3439fff75a7781be74b395ffd8bf4160e538117
Jay Patel <jay2.patel@intel.com>
2024-01-12 16:52:07 -0800

    mtlrvpp_pmc_altmode: Create mtlrvpp_pmc_altmode project
    Adding mtlrvpp_pmc_altmode project which supports Power Delivery chips.
    Disable Type-C altmode configuration. Type-C altmode configuration is
    handled by intel Power Management Controller. Create device tree files
    and configs for the project.
    
    Change-Id: I488d340753dee849f840ad45e16ee0d64942d497

--- zephyr/program/intelrvp/BUILD.py
+++ zephyr/program/intelrvp/BUILD.py
@@ -178,3 +178,27 @@ register_intelrvp_project(
         here / "mtlrvp/pd.conf",
     ],
 )
+
+register_intelrvp_project(
+    project_name="mtlrvpp_pmc_altmode",
+    chip="npcx9m3f",
+    extra_dts_overlays=[
+        here / "mtlrvp/mtlrvpp_npcx/fan.dts",
+        here / "mtlrvp/mtlrvpp_npcx/gpio.dts",
+        here / "mtlrvp/mtlrvpp_pmc_altmode/gpio.dts",
+        here / "mtlrvp/mtlrvpp_npcx/keyboard.dts",
+        here / "mtlrvp/mtlrvpp_npcx/interrupts.dts",
+        here / "mtlrvp/ioex.dts",
+        here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx.dts",
+        here / "mtlrvp/mtlrvpp_pmc_altmode/pd_i2c.dts",
+        here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx_power_signals.dts",
+        here / "adlrvp/adlrvp_npcx/temp_sensor.dts",
+        here / "mtlrvp/mtlrvpp_pmc_altmode/pd.dts",
+    ],
+    extra_kconfig_files=[
+        here / "zephyr_ap_pwrseq.conf",
+        here / "mtlrvp/mtlrvpp_pd/prj.conf",
+        here / "mtlrvp/mtlrvpp_npcx/board_npcx.conf",
+        here / "mtlrvp/mtlrvpp_pmc_altmode/pmc_altmode.conf",
+    ],
+)
