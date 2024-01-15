039c56e76416b2ae27312763abae02621d59ef3f
ben chen <ben.chen2@quanta.corp-partner.google.com>
2024-01-15 16:37:57 +0800

    dochi: add motionsense lis2dh
    motionse function
    
    BUG=b:315386114
    TEST=boot board, ectool motionsense lid_angle
    
    Change-Id: I635c5f6ad9912ffc9512f5e7c75bde36ba07a38c

--- board/dochi/board.h
+++ board/dochi/board.h
@@ -26,11 +26,10 @@
 
 /* Sensors */
 
-#define CONFIG_ACCEL_BMA4XX
-#define CONFIG_ACCEL_BMA4XX_INT_EVENT \
-	TASK_EVENT_MOTION_SENSOR_INTERRUPT(BASE_ACCEL)
+#define CONFIG_ACCEL_LIS2DE /* Lid accel */
+
 /* Sensors without hardware FIFO are in forced mode */
-#define CONFIG_ACCEL_FORCE_MODE_MASK BIT(LID_ACCEL)
+#define CONFIG_ACCEL_FORCE_MODE_MASK (BIT(BASE_ACCEL) | BIT(LID_ACCEL))
 
 /* Enable sensor fifo, must also define the _SIZE and _THRES */
 #define CONFIG_ACCEL_FIFO
