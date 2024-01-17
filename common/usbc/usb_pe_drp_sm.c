4f824292e40f2ae371c39351954816c71c51a975
Abe Levkoy <alevkoy@chromium.org>
2024-01-16 17:23:49 -0700

    TCPMv2: Handle SOP Discover Identity response in DPM
    Add SOP Discover Identity state to discovery state machine. Parse the
    response to SOP Discover Identity within that state. For now, still
    initiate the request from the PE.
    
    BUG=b:272827504
    TEST=twister -s zephyr/test/drivers/drivers.usbc_alt_mode
    
    Change-Id: Iaa7dd41dbae1b701edba64bea3dd21ef42586543

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -6079,6 +6079,11 @@ static void pe_init_port_vdm_identity_request_entry(int port)
 
 static void pe_init_port_vdm_identity_request_run(int port)
 {
+	/* Retrieve the message information. */
+	uint32_t *payload = (uint32_t *)rx_emsg[port].buf;
+	int sop = PD_HEADER_GET_SOP(rx_emsg[port].header);
+	uint8_t cnt = PD_HEADER_CNT(rx_emsg[port].header);
+
 	switch (parse_vdm_response_common(port)) {
 	case VDM_RESULT_WAITING:
 		/* If common code didn't parse a message, continue waiting. */
@@ -6091,19 +6096,14 @@ static void pe_init_port_vdm_identity_request_run(int port)
 		 */
 		break;
 	case VDM_RESULT_ACK: {
-		/* Retrieve the message information. */
-		uint32_t *payload = (uint32_t *)rx_emsg[port].buf;
-		int sop = PD_HEADER_GET_SOP(rx_emsg[port].header);
-		uint8_t cnt = PD_HEADER_CNT(rx_emsg[port].header);
-
 		/* PE_INIT_PORT_VDM_Identity_ACKed embedded here */
-		dfp_consume_identity(port, sop, cnt, payload);
-
+		dpm_vdm_acked(port, sop, cnt, payload);
 		break;
 	}
 	case VDM_RESULT_NAK:
 		/* PE_INIT_PORT_VDM_IDENTITY_NAKed embedded here */
-		pd_set_identity_discovery(port, pe[port].tx_type, PD_DISC_FAIL);
+		dpm_vdm_naked(port, pe[port].tx_type, USB_SID_PD,
+			      CMD_DISCOVER_IDENT, payload[0]);
 		break;
 	}
 
@@ -6124,7 +6124,8 @@ static void pe_init_port_vdm_identity_request_exit(int port)
 		 * If Structured VDMs are not supported, a Structured VDM
 		 * Command received by a DFP or UFP Shall be Ignored.
 		 */
-		pd_set_identity_discovery(port, pe[port].tx_type, PD_DISC_FAIL);
+		dpm_vdm_naked(port, TCPCI_MSG_SOP, USB_SID_PD,
+			      CMD_DISCOVER_IDENT, 0);
 	}
 
 	/* Do not attempt further discovery if identity discovery failed. */
