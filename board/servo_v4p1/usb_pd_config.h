76f5f5c9558a817f18e74e5a98fd435eed4e4a73
Łukasz Hajec <hajec@google.com>
2024-01-18 17:39:16 +0100

    servo_v4p1: Add poweron config feature
    This feature allows to set up servo ports on start up as user needs.
    Implementation is based on similar features already used in servo FW:
    serialname and MAC address in non volatile flash.
    New configs are added to config_allowed list, as this feature is limited
    to servo and there is no plans of migrating servo to Zephyr.
    
    BUG=b:316402626 b:248386313
    TEST=manually check if all paths works as expected, use Omnigul DUT
    
    Change-Id: Ic525d4f3ed7565d48666977c29afc9e6e54af9e6

--- board/servo_v4p1/usb_pd_config.h
+++ board/servo_v4p1/usb_pd_config.h
@@ -291,4 +291,13 @@ static inline void pd_config_init(int port, uint8_t power_role)
 
 int pd_adc_read(int port, int cc);
 
+/**
+ * External function to allow setting resetting specific flags in cc_config.
+ * Allows similar functionality as cc console command.
+ *
+ * @param flag        cc_config flag to set/reset
+ * @param set         true to set, false to reset flag
+ */
+void set_reset_cc_flag(int flag, bool set);
+
 #endif /* __CROS_EC_USB_PD_CONFIG_H */
