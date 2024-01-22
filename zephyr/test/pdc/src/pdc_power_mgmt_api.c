6459b98b399edafb51279af9a73d675876961598
Al Semjonovs <asemjonovs@google.com>
2024-01-22 12:00:21 -0700

    pdc: Add pdc power management tests
    Adding the following tests:
     - get_usb_pd_port_count
     - is_connected
    
    Fixing compiler error for missing types in
    pdc_power_mgmt.h
    
    BUG=b:317065172
    TEST=./twister -c  -p native_posix -T zephyr/test/pdc/
    LOW_COVERAGE_REASON=Incremental change for coverage b:317065172
    
    Change-Id: I2f0e4db37738f9958333e61fdb6dd16ac31b62f6

---
+++ zephyr/test/pdc/src/pdc_power_mgmt_api.c
@@ -0,0 +1,57 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "drivers/ucsi_v3.h"
+#include "emul/emul_pdc.h"
+#include "usbc/pdc_power_mgmt.h"
+
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/emul.h>
+#include <zephyr/ztest.h>
+
+#define RTS5453P_NODE DT_NODELABEL(rts5453p_emul)
+#define EMUL_DATA rts5453p_emul_get_i2c_common_data(EMUL)
+
+static const struct emul *emul = EMUL_DT_GET(RTS5453P_NODE);
+#define TEST_PORT 0
+
+void pdc_power_mgmt_setup(void)
+{
+	zassume(TEST_PORT < CONFIG_USB_PD_PORT_MAX_COUNT,
+		"TEST_PORT is invalid");
+
+	emul_pdc_set_response_delay(emul, 0);
+}
+
+ZTEST_SUITE(pdc_power_mgmt_api, NULL, pdc_power_mgmt_setup, NULL, NULL, NULL);
+
+ZTEST_USER(pdc_power_mgmt_api, test_get_usb_pd_port_count)
+{
+	zassert_equal(CONFIG_USB_PD_PORT_MAX_COUNT,
+		      pdc_power_mgmt_get_usb_pd_port_count());
+}
+
+ZTEST_USER(pdc_power_mgmt_api, test_is_connected)
+{
+	zassert_false(
+		pdc_power_mgmt_is_connected(CONFIG_USB_PD_PORT_MAX_COUNT));
+
+	zassert_false(pdc_power_mgmt_is_connected(TEST_PORT));
+
+/* TODO(b/321749548) - Read connector status after its read from I2C bus
+ * not after reading PING status.
+ */
+#ifdef TODO_B_321749548
+	struct connector_status_t connector_status;
+
+	connector_status.connect_status = 1;
+	connector_status.power_operation_mode = PD_OPERATION;
+	connector_status.power_direction = 1;
+	emul_pdc_set_connector_status(emul, &connector_status);
+	emul_pdc_pulse_irq(emul);
+	k_sleep(K_MSEC(1000));
+	zassert_true(pdc_power_mgmt_is_connected(TEST_PORT));
+#endif /* TODO_B_321749548 */
+}
