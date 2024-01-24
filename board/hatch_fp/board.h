c0a7af3b61d2cde2d139a48a716a873b9571ade1
Craig Hesling <hesling@chromium.org>
2024-01-24 13:45:14 -0800

    DNS: Enable DRUID
    * bloonchipper
    * dartmonkey
    
    BUG=b:295249061
    TEST=Flash and run fpenroll/fpmatch manually
    
    Change-Id: I4ff5d9588697534d98dd02bfbf430c1b4a5d3b20

--- board/hatch_fp/board.h
+++ board/hatch_fp/board.h
@@ -40,6 +40,13 @@
  *
  *-------------------------------------------------------------------------*/
 
+#ifdef SECTION_IS_RW
+#define CONFIG_LIB_DRUID
+#define CONFIG_LIB_DRUID_WRAPPER
+#define CONFIG_LIB_DRUID_TEMPLATE_UPDATE
+#define CONFIG_LIB_DRUID_ARMV7
+#endif
+
 #undef _IMAGE_SIZE
 #undef CONFIG_ROLLBACK_OFF
 #undef CONFIG_ROLLBACK_SIZE
