6474c2aeba6dca5c3152c146bfcca7e9f3fcd5a9
Firas Sammoura <fsammoura@google.com>
2024-01-25 15:22:24 -0800

    test: trial
    Change-Id: Iaa08cab94247b6aff09d65dbc1519b3dcdd3ca45
    Signed-off-by: Firas Sammoura <fsammoura@google.com>

--- test/rtc_stm32f4.c
+++ test/rtc_stm32f4.c
@@ -6,82 +6,56 @@
 #include "clock_chip.h"
 #include "test_util.h"
 
-static uint32_t rtc_fired;
-static struct rtc_time_reg rtc_irq;
-static const int rtc_delay_ms = 500;
+extern int handle_command(char *input);
 
-/*
- * We will be testing that the RTC interrupt timestamp occurs
- * within +/- delay_tol_us (tolerance) of the above rtc_delay_ms.
- */
-static const int delay_tol_us = MSEC / 2;
-
-/* Override default RTC interrupt handler */
-void rtc_alarm_irq(void)
-{
-	atomic_add(&rtc_fired, 1);
-	reset_rtc_alarm(&rtc_irq);
-}
 
 test_static int test_rtc_alarm(void)
 {
-	struct rtc_time_reg rtc;
-	uint32_t rtc_diff_us;
-	const int delay_us = rtc_delay_ms * MSEC;
+	struct ec_params_flash_protect params;
+	struct ec_response_flash_protect resp;
+	int res;
 
-	set_rtc_alarm(0, delay_us, &rtc, 0);
+	char input1[]="md 0x2000cfd8 2";
+	char input2[]="md 0x2000cf67 2";
+	char input3[]="rw 0x2000cf67 0x38";
+	char input4[]="md 0x2000cf67";
 
-	msleep(2 * rtc_delay_ms);
 
-	/* Make sure the interrupt fired exactly once. */
-	TEST_EQ(1, atomic_clear(&rtc_fired), "%d");
+	res = handle_command(input1);
+	cflush();
+	ccprints ("after md command is: %d\n", res);
 
-	rtc_diff_us = get_rtc_diff(&rtc, &rtc_irq);
+	params.flags = 0; //EC_FLASH_PROTECT_ALL_NOW;
+	params.mask = 0; // EC_FLASH_PROTECT_ALL_NOW;
 
-	ccprintf("Target delay was %dus\n", delay_us);
-	ccprintf("Actual delay was %dus\n", rtc_diff_us);
-	ccprintf("The delays are expected to be within +/- %dus\n", MSEC / 2);
+	res = test_send_host_command(EC_CMD_FLASH_PROTECT, 1, &params, sizeof(params), &resp, sizeof(resp));
+	cflush();
+	ccprints ("after flash protect command is: %d\n", res);
 
-	/* Assume we'll always fire within 500us. May need to be adjusted if
-	 * this doesn't hold.
-	 *
-	 * delay_us-delay_tol_us < rtc_diff_us < delay_us+delay_tol_us
-	 */
-	TEST_LT(delay_us - delay_tol_us, rtc_diff_us, "%dus");
-	TEST_LT(rtc_diff_us, delay_us + delay_tol_us, "%dus");
 
-	return EC_SUCCESS;
-}
+	res = handle_command(input2);
+	cflush();
+	ccprints ("after md command is: %d\n", res);
 
-static const int rtc_match_delay_iterations = 5000;
 
-test_static int test_rtc_match_delay(void)
-{
-	struct rtc_time_reg rtc;
-	int i;
+	res = handle_command(input3);
+	cflush();
+	ccprints ("after rw command is: %d\n", res);
 
-	atomic_clear(&rtc_fired);
-	for (i = 0; i < rtc_match_delay_iterations; i++) {
-		set_rtc_alarm(0, SET_RTC_MATCH_DELAY, &rtc, 0);
-		usleep(2 * SET_RTC_MATCH_DELAY);
-	}
-
-	ccprintf("Expected number of RTC alarm interrupts %d\n",
-		 rtc_match_delay_iterations);
-	ccprintf("Actual number of RTC alarm interrupts %d\n", rtc_fired);
-
-	/* Make sure each set_rtc_alarm() generated the interrupt. */
-	TEST_EQ(rtc_match_delay_iterations, atomic_clear(&rtc_fired), "%d");
+	res = handle_command(input4);
+	cflush();
+	ccprints ("after md command is: %d\n", res);
 
 	return EC_SUCCESS;
 }
+
 
 void run_test(int argc, const char **argv)
 {
 	test_reset();
 
 	RUN_TEST(test_rtc_alarm);
-	RUN_TEST(test_rtc_match_delay);
+	//RUN_TEST(test_rtc_match_delay);
 
 	test_print_result();
 }
