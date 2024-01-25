2e42ff919c0ec3ec603dda54195d976d108fda6a
Firas Sammoura <fsammoura@google.com>
2024-01-25 15:45:28 -0800

    test: add unit test for restricted console command_mem_dump
    BUG=b:142944002
    TEST=run_device_tests.py -b bloonchipper -t restricted_console
    
    Change-Id: Iacdfb1540eee47019cf1f1ce204152883d5084a3

---
+++ test/restricted_console.c
@@ -0,0 +1,39 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "test_util.h"
+
+static int is_locked;
+
+int system_is_locked(void)
+{
+	return is_locked;
+}
+
+test_static int test_command_mem_dump(void)
+{
+	enum ec_error_list res;
+	char input1[] = "md 0x2000cfd8 2";
+	char input2[] = "md 0x2000cf67 2";
+
+	is_locked = 0;
+	res = test_send_console_command(input1);
+	TEST_EQ(res, EC_SUCCESS, "%d");
+
+	is_locked = 1;
+	res = test_send_console_command(input2);
+	TEST_EQ(res, EC_ERROR_ACCESS_DENIED, "%d");
+
+	return EC_SUCCESS;
+}
+
+void run_test(int argc, const char **argv)
+{
+	test_reset();
+
+	RUN_TEST(test_command_mem_dump);
+
+	test_print_result();
+}
