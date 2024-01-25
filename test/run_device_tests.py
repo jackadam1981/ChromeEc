619b8b80d69e204f65a17efce0f5ecd5e8880944
Firas Sammoura <fsammoura@google.com>
2024-01-25 15:45:28 -0800

    test: add unit test for restricted console command_mem_dump
    BUG=b:142944002
    TEST=run_device_tests.py -b bloonchipper -t restricted_console
    
    Change-Id: Iacdfb1540eee47019cf1f1ce204152883d5084a3

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -339,6 +339,9 @@ class AllTests:
             TestConfig(test_name="pingpong"),
             TestConfig(test_name="printf"),
             TestConfig(test_name="queue"),
+            TestConfig(
+                test_name="restricted_console", exclude_boards=[HELIPILOT]
+            ),
             TestConfig(test_name="rng_benchmark"),
             TestConfig(
                 config_name="rollback_region0",
