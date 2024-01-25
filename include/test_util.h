731939a10dac663ef954e11890c987919ee21989
Firas Sammoura <fsammoura@google.com>
2024-01-25 15:34:02 -0800

    common: Add test_send_console_command function
    BUG=b:142944002
    TEST=make buildall
    
    Change-Id: Id8be6d9b30df06792934dc41f4714c73633268a3

--- include/test_util.h
+++ include/test_util.h
@@ -210,6 +210,9 @@ enum ec_status test_send_host_command(int command, int version,
 				      const void *params, int params_size,
 				      void *resp, int resp_size);
 
+/* Simulates console command sent from the host send_con*/
+enum ec_error_list test_send_console_command(const char *input);
+
 /* Optionally defined interrupt generator entry point */
 void interrupt_generator(void);
 
