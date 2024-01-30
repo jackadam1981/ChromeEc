29400e67f5e6c094072ac123353e6552697588de
Li Feng <li1.feng@intel.com>
2024-01-30 00:11:59 -0800

    ish:
    BUG=none
    TEST=zmake build rex-ish
    
    Change-Id: I685303c42401df306e6340de6f061a6e395506ed

--- zephyr/subsys/host_service/heci/heci_system_state.c
+++ zephyr/subsys/host_service/heci/heci_system_state.c
@@ -4,6 +4,7 @@
  */
 
 #include "heci_system_state.h"
+#include "sedi_driver_pm.h"
 
 #define SYSTEM_STATE_SUBSCRIBE 0x1
 #define SYSTEM_STATE_STATUS 0x2
@@ -66,12 +67,14 @@ void heci_handle_system_state_msg(uint8_t *msg, const size_t length)
 		break;
 	case SYSTEM_STATE_STATUS:
 		status = (struct ss_status *)msg;
+#ifdef CONFIG_PM
 		if (status->supported_states & SUSPEND_STATE_BIT) {
 			if (status->states_status & SUSPEND_STATE_BIT)
 				sedi_pm_host_suspend(1);
 			else
 				sedi_pm_host_suspend(0);
 		}
+#endif
 
 		break;
 	}
