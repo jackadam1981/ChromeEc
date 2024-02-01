811868217fbfabb202bce0a7b2d4a8b98a3821f3
michael5_chen1 <michael5_chen1@pegatron.corp-partner.google.com>
2024-02-01 12:15:19 +0800

    glassway: Initial zephyr config for glassway project
    Initial EC Zephyr config for glassway
    Reuse most of nivviks sources.
    
    BUG=b:319071869
    TEST=zmake build glassway
    LOW_COVERAGE_REASON=Initial glassway project, board test under
    developement(b/319071869)
    
    Change-Id: I7fc30cffe372a5ae2c9018a99b9c3880a4046047
    Signed-off-by: michael5_chen1 <michael5_chen1@pegatron.corp-partner.google.com>

--- zephyr/program/nissa/BUILD.py
+++ zephyr/program/nissa/BUILD.py
@@ -142,7 +142,10 @@ anraggar = register_nissa_project(
     project_name="anraggar",
     chip="it81302bx",
 )
-
+glassway = register_nissa_project(
+    project_name="glassway",
+    chip="npcx9m3f",
+)
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="anraggar", addr=0xBFFE0)
