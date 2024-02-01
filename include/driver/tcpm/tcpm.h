4691ee06d6aab8356fd1acfe70650f12295c704b
Sam Hurst <shurst@google.com>
2024-02-01 08:01:37 -0800

    PD_CONTROLLER: Add get PDC chip info API
    Add get PDC chip info API used by host command
    
    BUG=b:305014156
    TEST=ectool pdchipinfo 1
       vendor_id: 0x18d1
       product_id: 0x5065
       device_id: 0x0
       fw_version: 0x401
       min_req_fw_version: 0x0
    
    LOW_COVERAGE_REASON=Coverage to be handled in b/317065172
    
    Change-Id: I55c51aab30db3b31fe05e37dd153ba40b098da93

--- include/driver/tcpm/tcpm.h
+++ include/driver/tcpm/tcpm.h
@@ -408,6 +408,10 @@ static inline int tcpc_i2c_write(const int port, const uint16_t addr_flags,
 }
 #endif
 
+#ifdef CONFIG_USB_PD_CONTROLLER
+int tcpm_get_chip_info(int port, int live,
+		       struct ec_response_pd_chip_info_v1 *info);
+#else
 static inline int tcpm_get_chip_info(int port, int live,
 				     struct ec_response_pd_chip_info_v1 *info)
 {
@@ -415,6 +419,7 @@ static inline int tcpm_get_chip_info(int port, int live,
 		return tcpc_config[port].drv->get_chip_info(port, live, info);
 	return EC_ERROR_UNIMPLEMENTED;
 }
+#endif
 
 static inline int tcpm_hard_reset_reinit(int port)
 {
