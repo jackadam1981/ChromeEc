fccf70493b94f8ccd5715e4665444e2ea0f49119
Craig Hesling <hesling@chromium.org>
2024-02-01 18:31:43 -0800

    helipliot: Make baseboard helipilot
    This allows for the usage of helipilot configuration for buccaneer.
    The creation of this baseboard is effectively a cut/paste from
    board/helipilot with minimal corrections to paths.
    
    Run ./bazel/gen_bazel_targets.py to update the bazel
    ./bazel/gen_bazel_targets.py.
    
    BUG=b:323424273, b:315858518
    TEST=./util/compare_build.sh -b helipilot
    TEST=./util/compare_build.sh -b helipilot --noprivate
    
    Change-Id: I2d52888b6e92a723a9fb67890aa93dbc8be7e7c5

---
+++ baseboard/helipilot/build.mk
@@ -0,0 +1,18 @@
+# -*- makefile -*-
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Helipilot baseboard specific files build
+#
+
+baseboard-y += base_board.o
+baseboard-rw += base_board_rw.o
+
+# If we're mocking the sensor detection for testing (so we can test
+# sensor/transport permutations in the unit tests), don't build the real sensor
+# detection.
+ifeq ($(HAS_MOCK_FPSENSOR_DETECT),)
+	baseboard-y += fpsensor_detect.o
+	baseboard-rw += fpsensor_detect_rw.o
+endif
