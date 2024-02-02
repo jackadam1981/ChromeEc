b32e7c6966efc065dd743d412bef9ca1c57d2737
Keith Short <keithshort@chromium.org>
2024-02-01 17:48:09 -0700

    DNM: brox: test code to pulse a GPIO at boot
    This drives GPIOA 2 high for 10 EC cycles and then low.
    
    Change-Id: I91ca3bbeb02a567392e247067b75a52cbb115112

--- zephyr/program/brox/src/charger.c
+++ zephyr/program/brox/src/charger.c
@@ -18,3 +18,55 @@ int extpower_is_present(void)
 
 	return 0;
 }
+
+#include <soc.h>
+#define LED_PIN 2
+#define RTCRST_PIN 5
+
+static int brox_sign_of_life(void)
+{
+	uintptr_t a_gpdr = 0x00f01601;
+	uintptr_t a_gpcr = 0x00f01610 + LED_PIN;
+	uintptr_t a_gpotr = 0x00f01671;
+	uintptr_t h_gpdr = 0x00f01608;
+	uintptr_t h_gpcr = 0x00f01648 + RTCRST_PIN;
+	uintptr_t h_gpotr = 0x00f01678;
+	uint8_t temp;
+
+	/* LED_1 */
+	/* Push-pull */
+	ECREG(a_gpotr) &= ~BIT(LED_PIN);
+	/* Set high to start */
+	ECREG(a_gpdr) |= BIT(LED_PIN);
+	/* Set output */
+	ECREG(a_gpcr) = (ECREG(a_gpcr) | GPCR_PORT_PIN_MODE_OUTPUT) &
+			~GPCR_PORT_PIN_MODE_INPUT;
+
+	/* RTCRST */
+	/* Push-pull */
+	ECREG(h_gpotr) &= ~BIT(RTCRST_PIN);
+	/* Set high to start */
+	ECREG(h_gpdr) |= BIT(RTCRST_PIN);
+	/* Set output */
+	ECREG(h_gpcr) = (ECREG(h_gpcr) | GPCR_PORT_PIN_MODE_OUTPUT) &
+			~GPCR_PORT_PIN_MODE_INPUT;
+
+	/* No-ops to add a little delay */
+	temp = ECREG(a_gpdr);
+	temp = ECREG(a_gpdr);
+	temp = ECREG(a_gpdr);
+	temp = ECREG(a_gpdr);
+	temp = ECREG(a_gpdr);
+	temp = ECREG(a_gpdr);
+	temp = ECREG(a_gpdr);
+	temp = ECREG(a_gpdr);
+	temp = ECREG(a_gpdr);
+	temp = ECREG(a_gpdr);
+
+	/* Set low to generate a pulse */
+	ECREG(a_gpdr) &= ~BIT(LED_PIN);
+	ECREG(h_gpdr) &= ~BIT(RTCRST_PIN);
+
+	return 0;
+}
+SYS_INIT(brox_sign_of_life, EARLY, 0);
