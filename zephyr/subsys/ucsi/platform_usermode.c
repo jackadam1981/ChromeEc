ed28ab4bdba6901fed002090b26799c201f1d881
Daisuke Nojiri <dnojiri@chromium.org>
2024-02-08 19:00:53 -0800

    PPM: Add UCSI Platform Policy Manager
    This CL adds a UCSI PPM. It receives commands from the OPM and sends
    them to a PDC through a PDC driver (e.g. pdc_rts54xx.c).
    
    The PDM (i.e. pdc_power_mgmt.c) will continue to work like before but it
    will get an error (i.e. -EBUSY) if a PDC driver is being used by the PPM
    for the same port.
    
    BUG=b:319124515
    TEST=Run ectool ppm on Brox SKU1.
    
    Change-Id: I7b01e2604f6e33125bd784854fc17ada581f6869

---
+++ zephyr/subsys/ucsi/platform_usermode.c
@@ -0,0 +1,207 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/platform.h"
+#include "ppm_common.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <zephyr/kernel/thread_stack.h>
+
+#include <pthread.h>
+#include <unistd.h>
+
+bool debug_enabled;
+
+void *platform_malloc(size_t size)
+{
+	return NULL;
+}
+
+void *platform_calloc(size_t nmemb, size_t size)
+{
+	return NULL;
+}
+
+void platform_free(void *ptr)
+{
+}
+
+void platform_memcpy(void *dest, const void *src, size_t length)
+{
+	memcpy(dest, src, length);
+}
+
+void platform_memset(void *dest, uint8_t data, size_t length)
+{
+	memset(dest, data, length);
+}
+
+void platform_set_debug(bool enable)
+{
+	debug_enabled = enable;
+}
+
+bool platform_debug_enabled(void)
+{
+	return debug_enabled;
+}
+
+void platform_printf(const char *format, ...)
+{
+	va_list arglist;
+
+	va_start(arglist, format);
+	vprintf(format, arglist);
+	va_end(arglist);
+}
+
+void platform_eprintf(const char *format, ...)
+{
+	va_list arglist;
+
+	va_start(arglist, format);
+	vfprintf(stderr, format, arglist);
+	va_end(arglist);
+}
+
+void platform_usleep(uint32_t usec)
+{
+	usleep(usec);
+}
+
+void platform_hexdump(const uint8_t *data, size_t len)
+{
+	for (int _i = 0; _i < len; ++_i) {
+		DLOG_LOOP("0x%02x", data[_i]);
+		if (_i != len - 1)
+			DLOG_LOOP(", ");
+	}
+}
+
+struct task_handle {
+	pthread_t thread;
+};
+
+#define STACK_SIZE (1024)
+K_THREAD_STACK_DEFINE(stack, STACK_SIZE);
+
+int platform_task_init(void *start_fn, void *arg, struct task_handle **handle)
+{
+	pthread_attr_t attr;
+	int res;
+
+	if (!*handle) {
+		ELOG("Handle=NULL not supported");
+		return -1;
+	}
+
+	/* Zephyr requires a non-NULL attribute for pthread_create */
+	res = pthread_attr_init(&attr);
+	if (res != 0) {
+		errno = res;
+		perror("pthread_attr_init");
+		return -1;
+	}
+
+	res = pthread_attr_setstack(&attr, &stack, STACK_SIZE);
+	if (res != 0) {
+		errno = res;
+		perror("pthread_attr_setstack");
+		return -1;
+	}
+	res = pthread_create(&(*handle)->thread, &attr, start_fn, arg);
+	if (res != 0) {
+		ELOG("Failed to start thread with error %d for start_fn %p",
+		     res, start_fn);
+		return -1;
+	}
+
+	return 0;
+}
+
+void platform_task_exit(void)
+{
+	pthread_exit(NULL);
+}
+
+int platform_task_complete(struct task_handle *handle)
+{
+	return pthread_join(handle->thread, NULL);
+}
+
+struct platform_mutex {
+	pthread_mutex_t lock;
+};
+
+int platform_mutex_init(struct platform_mutex **mutex)
+{
+	if (!*mutex) {
+		return -1;
+	}
+
+	if (pthread_mutex_init(&(*mutex)->lock, NULL)) {
+		return -1;
+	}
+
+	return 0;
+}
+
+void platform_mutex_lock(struct platform_mutex *mutex)
+{
+	pthread_mutex_lock(&mutex->lock);
+}
+void platform_mutex_unlock(struct platform_mutex *mutex)
+{
+	pthread_mutex_unlock(&mutex->lock);
+}
+
+struct platform_condvar {
+	pthread_cond_t var;
+};
+
+int platform_condvar_init(struct platform_condvar **cond)
+{
+	if (!*cond) {
+		return -1;
+	}
+
+	if (pthread_cond_init(&(*cond)->var, NULL)) {
+		return -1;
+	}
+
+	return 0;
+}
+
+void platform_condvar_wait(struct platform_condvar *condvar,
+			   struct platform_mutex *mutex)
+{
+	pthread_cond_wait(&condvar->var, &mutex->lock);
+}
+
+void platform_condvar_signal(struct platform_condvar *condvar)
+{
+	pthread_cond_signal(&condvar->var);
+}
+
+struct ucsi_ppm_driver *platform_init_ppm(void)
+{
+	/* These are all set to 0 by default. */
+	static struct ppm_common_device dev;
+	static struct ucsi_ppm_driver drv;
+	static struct platform_condvar ppm_condvar;
+	static struct platform_mutex ppm_lock;
+	static struct task_handle ppm_task_handle;
+
+	drv.dev = (struct ucsi_ppm_device *)&dev;
+	dev.ppm_condvar = &ppm_condvar;
+	dev.ppm_lock = &ppm_lock;
+	dev.ppm_task_handle = &ppm_task_handle;
+
+	return &drv;
+}
