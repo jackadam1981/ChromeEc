745385b0739c729bf8f3c9f5805b03f125276799
Daisuke Nojiri <dnojiri@chromium.org>
2024-02-08 19:00:53 -0800

    PPM: Add UCSI Platform Policy Manager
    This CL adds a UCSI PPM. It receives commands from the OPM and sends
    them to a PDC through a PDC driver (e.g. pdc_rts54xx.c).
    
    The PDM (i.e. pdc_power_mgmt.c) will continue to work like before. It
    will get -EBUSY if a PDC driver is serving for another request.  There
    is no change in this behavior but contention will be more frequent
    because of the PPM.
    
    Tested:
    UCSI_CMD_PPM_RESET
    UCSI_CMD_SET_NOTIFICATION_ENABLE
    UCSI_CMD_GET_CAPABILITY
    UCSI_CMD_GET_CONNECTOR_CAPABILITY
    UCSI_CMD_GET_PDOS
    UCSI_CMD_GET_ERROR_STATUS
    UCSI_CMD_GET_ALTERNATE_MODES
    
    BUG=b:319124515
    TEST=Boot modified kernel on Brox. Observed UCSI commands from OPM were
         successfully processed.
    LOW_COVERAGE_REASON=b:335261254
    
    Change-Id: I7b01e2604f6e33125bd784854fc17ada581f6869

---
+++ zephyr/subsys/ucsi/lpm_driver.c
@@ -0,0 +1,192 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* UCSI LPM Driver */
+
+#include "include/pd_driver.h"
+#include "include/ppm.h"
+#include "ppm_common.h"
+#include "usb_pd.h"
+#include "util.h"
+
+#include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
+
+#include <drivers/pdc.h>
+
+LOG_MODULE_REGISTER(lpm, LOG_LEVEL_INF);
+
+#define DT_DRV_COMPAT ucsi_lpm_driver
+#define UCSI_7BIT_PORTMASK(p) ((p) & 0x7F)
+#define DT_LPM_DRV DT_INST(0, ucsi_lpm_driver)
+#define NUM_PORTS DT_PROP_LEN(DT_LPM_DRV, pdc)
+
+struct rts5453_ucsi_commands {
+	uint8_t command;
+	uint8_t command_copy_length;
+};
+
+#define UCSI_CMD_ENTRY(cmd, length)                            \
+	{                                                      \
+		.command = cmd, .command_copy_length = length, \
+	}
+
+struct rts5453_ucsi_commands ucsi_commands[UCSI_CMD_VENDOR_CMD + 1] = {
+	UCSI_CMD_ENTRY(UCSI_CMD_RESERVED, 0),
+	UCSI_CMD_ENTRY(UCSI_CMD_PPM_RESET, 0),
+	UCSI_CMD_ENTRY(UCSI_CMD_CANCEL, 0),
+	UCSI_CMD_ENTRY(UCSI_CMD_CONNECTOR_RESET, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_ACK_CC_CI, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_SET_NOTIFICATION_ENABLE, 3),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_CAPABILITY, 0),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_CONNECTOR_CAPABILITY, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_SET_CCOM, 2),
+	UCSI_CMD_ENTRY(UCSI_CMD_SET_UOR, 2),
+	UCSI_CMD_ENTRY(obsolete_UCSI_CMD_SET_PDM, 0),
+	UCSI_CMD_ENTRY(UCSI_CMD_SET_PDR, 2),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_ALTERNATE_MODES, 4),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_CAM_SUPPORTED, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_CURRENT_CAM, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_SET_NEW_CAM, 6),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_PDOS, 3),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_CABLE_PROPERTY, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_CONNECTOR_STATUS, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_ERROR_STATUS, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_SET_POWER_LEVEL, 6),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_PD_MESSAGE, 4),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_ATTENTION_VDO, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_reserved_0x17, 0),
+	UCSI_CMD_ENTRY(UCSI_CMD_GET_CAM_CS, 2),
+	UCSI_CMD_ENTRY(UCSI_CMD_LPM_FW_UPDATE_REQUEST, 4),
+	UCSI_CMD_ENTRY(UCSI_CMD_SECURITY_REQUEST, 5),
+	UCSI_CMD_ENTRY(UCSI_CMD_SET_RETIMER_MODE, 5),
+	UCSI_CMD_ENTRY(UCSI_CMD_SET_SINK_PATH, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_SET_PDOS, 3),
+	UCSI_CMD_ENTRY(UCSI_CMD_READ_POWER_LEVEL, 3),
+	UCSI_CMD_ENTRY(UCSI_CMD_CHUNKING_SUPPORT, 1),
+	UCSI_CMD_ENTRY(UCSI_CMD_VENDOR_CMD, 6),
+};
+
+struct lpm_device {
+	/* PPM driver (common implementation). */
+	struct ucsi_ppm_driver *ppm;
+
+	/* Number of active ports from |GET_CAPABILITIES|. */
+	uint8_t active_port_count;
+
+	const struct device *pdc[NUM_PORTS];
+};
+
+static int ucsi_init_ppm(struct ucsi_pd_device *device)
+{
+	struct lpm_device *dev = (struct lpm_device *)device;
+
+	return dev->ppm->init_and_wait(dev->ppm->dev, ARRAY_SIZE(dev->pdc));
+}
+
+static struct ucsi_ppm_driver *ucsi_get_ppm(struct ucsi_pd_device *device)
+{
+	struct lpm_device *dev = (struct lpm_device *)device;
+
+	return dev->ppm;
+}
+
+static int ucsi_execute_cmd(struct ucsi_pd_device *device,
+			    struct ucsi_control *control, uint8_t *lpm_data_out)
+{
+	struct lpm_device *lpm = (struct lpm_device *)device;
+	struct ppm_common_device *dev =
+		(struct ppm_common_device *)lpm->ppm->dev;
+	uint8_t ucsi_command = control->command;
+	uint8_t conn; /* 1:port=0, 2:port=1, ... */
+	uint8_t data_size;
+
+	if (control->command == 0 || control->command > UCSI_CMD_VENDOR_CMD) {
+		LOG_ERR("Invalid command 0x%x", control->command);
+		return -1;
+	}
+
+	switch (ucsi_command) {
+	case UCSI_CMD_PPM_RESET:
+	case UCSI_CMD_SET_NOTIFICATION_ENABLE:
+		return -ENOTSUP;
+	case UCSI_CMD_CONNECTOR_RESET:
+	case UCSI_CMD_GET_CONNECTOR_CAPABILITY:
+	case UCSI_CMD_GET_CAM_SUPPORTED:
+	case UCSI_CMD_GET_CURRENT_CAM:
+	case UCSI_CMD_SET_NEW_CAM:
+	case UCSI_CMD_GET_PDOS:
+	case UCSI_CMD_GET_CABLE_PROPERTY:
+	case UCSI_CMD_GET_CONNECTOR_STATUS:
+	case UCSI_CMD_GET_ERROR_STATUS:
+	case UCSI_CMD_GET_PD_MESSAGE:
+	case UCSI_CMD_GET_ATTENTION_VDO:
+	case UCSI_CMD_GET_CAM_CS:
+		conn = UCSI_7BIT_PORTMASK(control->command_specific[0]);
+		/*
+		 * The OPS can set conn=0 for ERROR_STATUS if the error happened
+		 * in PPM. The OPS will know it was an error specific to a
+		 * connector when it sees -EINVAL.
+		 */
+		if (conn == 0 || conn > dev->num_ports) {
+			LOG_ERR("Invalid conn=%d", conn);
+			return -EINVAL;
+		}
+		break;
+	case UCSI_CMD_GET_ALTERNATE_MODES:
+		conn = UCSI_7BIT_PORTMASK(control->command_specific[1]);
+		if (conn == 0 || conn > dev->num_ports)
+			return -EINVAL;
+		break;
+	default:
+		conn = 1;
+	}
+
+	data_size = ucsi_commands[ucsi_command].command_copy_length;
+	LOG_INF("%s: Executing conn=%u cmd=0x%02x data_size=%d", __func__, conn,
+		ucsi_command, data_size);
+	return pdc_execute_command_sync(lpm->pdc[conn - 1], ucsi_command,
+					data_size, control->command_specific,
+					lpm_data_out);
+}
+
+#define PHANDLE_TO_DEV(node_id, prop, idx) \
+	[idx] = DEVICE_DT_GET(DT_PHANDLE_BY_IDX(node_id, prop, idx)),
+
+static struct lpm_device lpm_dev = {
+	.pdc = { DT_FOREACH_PROP_ELEM(DT_LPM_DRV, pdc, PHANDLE_TO_DEV) },
+};
+
+static struct ucsi_pd_driver lpm_drv = {
+	.init_ppm = ucsi_init_ppm,
+	.get_ppm = ucsi_get_ppm,
+	.execute_cmd = ucsi_execute_cmd,
+	.dev = (struct ucsi_pd_device *)&lpm_dev,
+};
+
+static struct ucsiv3_get_connector_status_data
+	port_status[NUM_PORTS] __aligned(4);
+
+static int lpm_init(const struct device *device)
+{
+	const struct ucsi_pd_driver *drv = device->api;
+	struct ppm_common_device *ppm_dev;
+	struct lpm_device *dev = device->data;
+
+	/* Initialize the PPM. */
+	dev->ppm = ppm_open(drv);
+	if (!dev->ppm) {
+		LOG_ERR("Failed to open PPM");
+		return -ENODEV;
+	}
+
+	ppm_dev = (struct ppm_common_device *)dev->ppm->dev;
+	ppm_dev->num_ports = ARRAY_SIZE(port_status);
+	ppm_dev->per_port_status = port_status;
+
+	return 0;
+}
+DEVICE_DT_INST_DEFINE(0, &lpm_init, NULL, &lpm_dev, NULL, POST_KERNEL,
+		      CONFIG_PDC_POWER_MGMT_INIT_PRIORITY, &lpm_drv);
