b2b5e7ff27aaaaa21f55f84288a6b24b70074d2d
Daisuke Nojiri <dnojiri@chromium.org>
2024-02-08 19:00:53 -0800

    UCSI/PPM: Add UCSI Platform Policy Manager
    SMBUS driver
    - Receive IRQ from PDC.
    - Provide SMBUS APIs (read_byte, read_block, write_block, etc.).
    
    UCSI Host Command Handler
    - Handle read/write request from OPM.
    
    RTS5453 driver
    
    BUG=b:319124515
    TEST=None
    
    Change-Id: I8475077634b9089392842b674818f4bec9d48ef7

---
+++ zephyr/subsys/ucsi/smbus.c
@@ -0,0 +1,254 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "include/platform.h"
+#include "include/smbus.h"
+#include "smbus_usermode.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <fcntl.h>
+#include "i2c.h"
+#include <pthread.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define MAX_PATH_SIZE 32
+
+// 10ms timeout for gpiod wakeup
+#define GPIOD_WAIT_TIMEOUT_NS (10 * 1000 * 1000)
+
+/**
+ * Internal structure for smbus implementation.
+ */
+struct smbus_device {
+	/* i2c port */
+	uint8_t port;
+	/* i2c address of the chip */
+	uint8_t addr;
+
+	volatile bool cleaning_up;
+	struct platform_condvar *pdc_condvar;
+	struct platform_mutex *gpio_lock;
+};
+
+static struct smbus_device smbus_dev;
+
+#define CAST_FROM(v) (struct smbus_device *)(v)
+
+static int smbus_read_byte_nolock(int port, uint8_t addr, uint8_t *buf)
+{
+	return i2c_xfer_unlocked(port, addr, NULL, 0, buf, 1, I2C_XFER_SINGLE);
+}
+
+static int __smbus_read_byte(struct smbus_device *device, uint8_t chip_address)
+{
+	struct smbus_device *dev = CAST_FROM(device);
+	uint8_t buf;
+	int rv;
+
+	i2c_lock(dev->port, 1);
+	rv = smbus_read_byte_nolock(dev->port, dev->addr, &buf);
+	i2c_lock(dev->port, 0);
+
+	if (rv)
+		return -1;
+
+	return buf;
+}
+
+static int __smbus_read_block(struct smbus_device *device, uint8_t addr,
+			    uint8_t cmd, void *data, size_t length)
+{
+	struct smbus_device *dev = CAST_FROM(device);
+	uint8_t read_len;
+	int rv;
+
+	// Block read will read at most 32 bytes.
+	if (length > SMBUS_MAX_BLOCK_SIZE) {
+		ELOG("length (%d) > max (%d) for block read", length,
+		     SMBUS_MAX_BLOCK_SIZE);
+		return -1;
+	}
+
+	i2c_lock(dev->port, 1);
+
+	/* First read the size. */
+	rv = i2c_xfer_unlocked(dev->port, addr, &cmd, 1, &read_len, 1,
+			       I2C_XFER_START);
+	if (rv)
+		goto smbus_read_block_done;
+
+	if (read_len > SMBUS_MAX_BLOCK_SIZE) {
+		DLOG("Got size (%d) > max (%d) for block read", read_len,
+		     SMBUS_MAX_BLOCK_SIZE);
+		goto smbus_read_block_done;
+	}
+	if (read_len > length) {
+		DLOG("Got size %d > expected size (%d) for block read",
+		     read_len, length);
+		goto smbus_read_block_done;
+	}
+
+	DLOG("Reading block at 0x%02x (%d bytes) from addr=0x02x port=%d", cmd,
+	     read_len, addr, dev->port);
+
+	rv = i2c_xfer_unlocked(dev->port, addr, NULL, 0, data, read_len,
+			       I2C_XFER_STOP);
+
+smbus_read_block_done:
+	i2c_lock(dev->port, 0);
+
+	return rv;
+}
+
+static int __smbus_write_block(struct smbus_device *device, uint8_t addr,
+			     uint8_t cmd, void *buf, size_t length)
+{
+	struct smbus_device *dev = CAST_FROM(device);
+	int rv;
+
+	DLOG_START("[0x%02x]: Sending data to %02x [", addr, cmd);
+	for (int i = 0; i < length; ++i) {
+		DLOG_LOOP("%02x, ", ((uint8_t *)buf)[i]);
+	}
+	DLOG_END("]");
+
+	i2c_lock(dev->port, 1);
+	rv = i2c_xfer_unlocked(dev->port, addr, &cmd, 1, NULL, 0,
+			       I2C_XFER_START);
+	if (rv)
+		goto smbus_write_block_done;
+	rv = i2c_xfer_unlocked(dev->port, addr, buf, length, NULL, 0,
+			       I2C_XFER_STOP);
+
+smbus_write_block_done:
+	i2c_lock(dev->port, 0);
+
+	return rv;
+}
+
+static int __smbus_read_ara(struct smbus_device *device, uint8_t ara_address)
+{
+	struct smbus_device *dev = CAST_FROM(device);
+	uint8_t ara_byte_result;
+	int rv;
+
+	i2c_lock(dev->port, 1);
+
+	/* Read ARA (0xC). */
+	rv = smbus_read_byte_nolock(dev->port, ara_address, &ara_byte_result);
+	if (rv)
+		rv = -1;
+	else
+		/* ARA will have 8 bits with top 7 bits of chip address. */
+		rv = ara_byte_result >> 1;
+
+	i2c_lock(dev->port, 0);
+
+	return rv;
+}
+
+void pdc_interrupt(enum gpio_signal signal)
+{
+	platform_condvar_signal(smbus_dev.pdc_condvar);
+}
+
+static int __smbus_block_for_interrupt(struct smbus_device *device)
+{
+	struct smbus_device *dev = CAST_FROM(device);
+	int ret = 0;
+	bool cleaning_up = false;
+	struct timespec ts;
+
+	if (dev->cleaning_up) {
+		return -1;
+	}
+
+	ts.tv_sec = 0;
+	ts.tv_nsec = GPIOD_WAIT_TIMEOUT_NS;
+
+	DLOG("Polling for smbus interrupt.");
+
+	do {
+		platform_mutex_lock(dev->gpio_lock);
+
+		platform_condvar_wait(dev->pdc_condvar, dev->gpio_lock);
+		cleaning_up = dev->cleaning_up;
+
+		platform_mutex_unlock(dev->gpio_lock);
+
+		// If we're cleaning up, exit out with an error.
+		if (cleaning_up) {
+			ret = -1;
+			break;
+		}
+
+		// Either error or result will break here. Otherwise, continue.
+		if (ret == 1 || ret == -1) {
+			break;
+		}
+	} while (true);
+
+	// Got an event. Clear the event before forwarding interrupt.
+	if (ret == 1) {
+		DLOG("Got SMBUS interrupt!");
+	} else {
+		DLOG("Smbus polling resulted in ret %d", ret);
+	}
+
+	return ret == 1 ? 0 : -1;
+}
+
+static void __smbus_cleanup(struct smbus_driver *driver)
+{
+	if (driver->dev) {
+		struct smbus_device *dev = CAST_FROM(driver->dev);
+
+		dev->cleaning_up = true;
+	}
+}
+
+static int init_interrupt(struct smbus_device *dev, int gpio_line)
+{
+	if (platform_mutex_init(dev->gpio_lock, NULL) != 0) {
+		ELOG("Failed to init gpio mutex");
+		return -1;
+	}
+	return 0;
+}
+
+struct smbus_driver *smbus_open(int bus_num, uint8_t chip_address,
+				int gpio_line)
+{
+	static struct smbus_driver drv;
+
+	smbus_dev.addr = chip_address;
+
+	// Initialize the gpio lines
+	if (init_interrupt(&smbus_dev, gpio_line) == -1) {
+		ELOG("Failed to initialize gpio for interrupt.");
+		return NULL;
+	}
+
+	drv.dev = &smbus_dev;
+	drv.read_byte = __smbus_read_byte;
+	drv.read_block = __smbus_read_block;
+	drv.write_block = __smbus_write_block;
+	drv.read_ara = __smbus_read_ara;
+	drv.block_for_interrupt = __smbus_block_for_interrupt;
+	drv.cleanup = __smbus_cleanup;
+
+	// Make sure chip address is valid before returning.
+	if (__smbus_read_byte(&smbus_dev, chip_address) < 0) {
+		ELOG("Could not read byte at given chip address.");
+		return NULL;
+	}
+
+	return &drv;
+}
--- zephyr/subsys/ucsi/ucsi_host_cmd.c
+++ zephyr/subsys/ucsi/ucsi_host_cmd.c
@@ -6,9 +6,47 @@
 /* UCSI host command */
 
 #include "ec_commands.h"
+#include "hooks.h"
 #include "host_command.h"
+#include "include/platform.h"
+#include "include/ppm.h"
+#include "ppm_common.h"
+#include "rts5453.h"
+#include "task.h"
 
-#if 1
+#define PDC_I2C_PORT 0
+#define PDC_I2C_ADDRESS 0
+#define GPIO_LPM_INTERRUPT 0
+
+#define DEV_CAST_FROM(v) (struct ppm_common_device *)(v)
+
+static struct ucsi_ppm_driver *ec_ppm_drv;
+
+struct smbus_driver *smbus_open(int bus_num, uint8_t chip_address,
+				int gpio_line);
+
+/* Sort of main */
+static void ucsi_init(void)
+{
+	struct smbus_driver *smbus;
+	struct ucsi_pd_driver *drv;
+
+	smbus = smbus_open(PDC_I2C_PORT, PDC_I2C_ADDRESS, GPIO_LPM_INTERRUPT);
+
+	drv = rts5453_open(smbus, NULL);
+	if (!drv)
+		return;
+
+	ec_ppm_drv = drv->get_ppm(drv->dev);
+
+	/* Start a LPM task. */
+	drv->configure_lpm_irq(drv->dev);
+
+	/* Start a PPM task. */
+	drv->init_ppm(drv->dev);
+}
+DECLARE_HOOK(HOOK_INIT, ucsi_init, HOOK_PRIO_DEFAULT);
+
 static enum ec_status hc_ucsi_ppm_set(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_ucsi_ppm_set *p = args->params;
@@ -19,6 +57,9 @@ static enum ec_status hc_ucsi_ppm_set(struct host_cmd_handler_args *args)
 	if (ec_ppm_drv->write(ec_ppm_drv->dev, p->offset, p->data,
 			      args->params_size - sizeof(p->offset)))
 		return EC_RES_ERROR;
+
+	/* Wake up PPM task. */
+	//task_wake(TASK_ID_PPM);
 
 	return EC_RES_SUCCESS;
 }
@@ -42,32 +83,3 @@ static enum ec_status hc_ucsi_ppm_get(struct host_cmd_handler_args *args)
 	return EC_RES_SUCCESS;
 }
 DECLARE_HOST_COMMAND(EC_CMD_UCSI_PPM_GET, hc_ucsi_ppm_get, EC_VER_MASK(0));
-
-#else
-
-static enum ec_status hc_ucsi_ppm(struct host_cmd_handler_args *args)
-{
-	struct ppm_common_device* dev = DEV_CAST_FROM(ec_ppm_drv->dev);
-	struct ec_params_ucsi_ppm *p = args->params;
-	/* Payload size */
-	uint16_t length = args->params_size - sizeof(p->subcmd);
-	enum ec_status rv = EC_RES_SUCCESS;
-	const void *buf = p->data;
-
-	switch (p->subcmd) {
-	case EC_UCSI_SUBCMD_SET_CONTROL:
-		rv = ppm_common_handle_control_message(dev, buf, length);
-		break;
-	case EC_UCSI_SUBCMD_SET_MESSAGE:
-		memcpy(dev->ucsi_data.message_out, buf, length);
-		break;
-	default:
-		ELOG("Invalid command received: 0x%02x", p->subcmd);
-		rv = EC_RES_INVALID_COMMAND;
-	}
-
-	return rv;
-}
-DECLARE_HOST_COMMAND(EC_CMD_UCSI_PPM, hc_ucsi_ppm, EC_VER_MASK(0));
-
-#endif
