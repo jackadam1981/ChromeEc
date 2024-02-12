c155b12799f210f20e94bac0550b7271144f8e2a
David Cross <davidmcross@google.com>
2024-02-12 15:55:16 -0800

    helipilot: add OTP Key Provisioning
    BUG=b:323094297
    TEST=rollbackaddent
    TEST=./test/run_device_tests.py -b helipilot -t otp_key
    
    Change-Id: Ia53055ab45b3d879e6aa697d4f5e05d2614a9ad2

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -337,6 +337,7 @@ class AllTests:
                 finish_regexes=[board_config.mpu_regex],
             ),
             TestConfig(test_name="mutex"),
+            TestConfig(test_name="otp_key"),
             TestConfig(test_name="panic"),
             TestConfig(test_name="pingpong"),
             TestConfig(test_name="printf"),
