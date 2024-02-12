002dede1225957a9107ac99bcdcc1fe3be754793
David Cross <davidmcross@google.com>
2024-02-12 15:55:16 -0800

    helipilot: add OTP Key Provisioning
    BUG=b:323094297
    TEST=rollbackaddent
    TEST=./test/run_device_tests.py -b helipilot -t otp_key
    
    Change-Id: Ia53055ab45b3d879e6aa697d4f5e05d2614a9ad2

--- baseboard/helipilot/base_board.c
+++ baseboard/helipilot/base_board.c
@@ -11,6 +11,7 @@
 #include "fpsensor/fpsensor_detect.h"
 #include "gpio.h"
 #include "hooks.h"
+#include "otp_key.h"
 #include "registers.h"
 #include "shi_chip.h"
 #include "spi.h"
@@ -88,5 +89,8 @@ static void board_init(void)
 	 * avoid incurring that cost when generating random numbers
 	 */
 	npcx_trng_hw_init();
+
+	/* Power on OTP Memory */
+	otp_key_init();
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
