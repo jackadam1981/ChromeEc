f7d2ab1e34c7700c9fa1d1e3af1016af25406870
Daisuke Nojiri <dnojiri@chromium.org>
2024-02-13 10:13:49 -0800

    USBPD: Add reset sub-command
    This CL moves 'hard' and 'soft' commands under 'reset'.
    
    BUG=b:315177051
    TEST=Run 'pd 0 reset hard' on Vell.
    
    Change-Id: I3d6fecd7aa720da6c818d888effb3b6cdd1ac0bb

--- common/usbc/usb_pd_console.c
+++ common/usbc/usb_pd_console.c
@@ -110,10 +110,18 @@ static
 			pd_comm_enable(port, 1);
 			ccprintf("Port C%d enabled\n", port);
 			return EC_SUCCESS;
-		} else if (!strcasecmp(argv[2], "hard")) {
-			pd_dpm_request(port, DPM_REQUEST_HARD_RESET_SEND);
-		} else if (!strcasecmp(argv[2], "soft")) {
-			pd_dpm_request(port, DPM_REQUEST_SOFT_RESET_SEND);
+		} else if (!strcasecmp(argv[2], "reset")) {
+			if (argc < 4)
+				return EC_ERROR_PARAM_COUNT;
+			if (!strcasecmp(argv[3], "hard")) {
+				pd_dpm_request(port,
+					       DPM_REQUEST_HARD_RESET_SEND);
+			} else if (!strcasecmp(argv[3], "soft")) {
+				pd_dpm_request(port,
+					       DPM_REQUEST_SOFT_RESET_SEND);
+			} else {
+				return EC_ERROR_PARAM3;
+			}
 		} else if (!strcasecmp(argv[2], "swap")) {
 			if (argc < 4)
 				return EC_ERROR_PARAM_COUNT;
@@ -259,9 +267,10 @@ DECLARE_CONSOLE_COMMAND(pd, command_pd,
 #endif /* CONFIG_CMD_PD_TIMER */
 #ifdef CONFIG_USB_PD_DUAL_ROLE
 			"|tx|charger|dev"
-			"\n\t<port> disable|enable|soft|hard"
+			"\n\t<port> disable|enable"
 			"\n\t<port> suspend|resume"
 			"\n\t<port> dualrole [on|off|freeze|sink|source]"
+			"\n\t<port> reset [soft|hard]"
 			"\n\t<port> swap [power|data|vconn]"
 #ifdef CONFIG_USB_PD_EPR
 			"\n\t<port> epr [enter|exit]"
