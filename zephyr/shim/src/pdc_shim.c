74451a905491618ce6725e81df60a16fbd633f6c
Sam Hurst <shurst@google.com>
2024-02-13 12:30:41 -0800

    PD_CONTROLLER: Add PD shim functions
    Add PD shim functions. Implementation will come in
    following CLs.
    
    BUG=b:305014156
    TEST=code built
    LOW_COVERAGE_REASON=Coverage to be handled in b/317065172
    
    Change-Id: Ib002c3a51c006b0b2945579aabb02aa82e9480ae

--- zephyr/shim/src/pdc_shim.c
+++ zephyr/shim/src/pdc_shim.c
@@ -92,6 +92,7 @@ void pd_set_dual_role(int port, enum pd_dual_role_states state)
 
 void pd_set_new_power_request(int port)
 {
+	/* TODO */
 }
 
 __override uint8_t board_get_usb_pd_port_count(void)
@@ -101,6 +102,8 @@ __override uint8_t board_get_usb_pd_port_count(void)
 
 __override int board_set_active_charge_port(int charge_port)
 {
+	/* TODO */
+
 	return EC_SUCCESS;
 }
 
@@ -112,4 +115,48 @@ const uint32_t *const pd_get_src_caps(int port)
 uint8_t pd_get_src_cap_cnt(int port)
 {
 	return pdc_power_mgmt_get_src_cap_cnt(port);
+}
+
+const uint32_t *const pd_get_snk_caps(int port)
+{
+	/* TODO */
+
+	return NULL;
+}
+
+uint8_t pd_get_snk_cap_cnt(int port)
+{
+	/* TODO */
+
+	return 0;
+}
+
+uint32_t pd_get_events(int port)
+{
+	/* TODO */
+
+	return 0;
+}
+
+struct rmdo pd_get_partner_rmdo(int port)
+{
+	struct rmdo value = {0};
+
+	/* TODO */
+
+	return value;
+}
+
+enum pd_discovery_state pd_get_identity_discovery(int port, enum tcpci_msg_type type)
+{
+	/* TODO */
+
+	return 0;
+}
+
+int pd_get_rev(int port, enum tcpci_msg_type type)
+{
+	/* TODO */
+
+	return PD_REV30;
 }
