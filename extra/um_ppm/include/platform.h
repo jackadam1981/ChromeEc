e3a6742abc28034eea6515af2dc8e3e71cdcf2a8
Daisuke Nojiri <dnojiri@chromium.org>
2024-02-14 14:27:55 -0800

    UCSI/PPM: Use fixed memory allocation
    BUG=b:319124515
    TEST=Run ectool ppm reset on Brox. Run make local_tests.
    
    Change-Id: I7b733bc5c3562a29645be63c1836073f50599b11

--- extra/um_ppm/include/platform.h
+++ extra/um_ppm/include/platform.h
@@ -55,8 +55,16 @@ void platform_usleep(uint32_t usec);
 /* Opaque task id type.*/
 struct task_handle;
 
-/* Initialize a task (code that can be independently scheduled). */
-struct task_handle *platform_task_init(void *start_fn, void *arg);
+/**
+ * Initialize a task (code that can be independently scheduled).
+ *
+ * @param start_fn Start routine
+ * @param arg Argument passed to start_fn
+ * @param handle Task id type. If *handle is NULL, memory will be allocated.
+ * @return 0 Success
+ * @return -1 Fail
+ */
+int platform_task_init(void *start_fn, void *arg, struct task_handle **handle);
 
 /* Called from within the task to complete / exit. */
 void platform_task_exit();
@@ -67,8 +75,14 @@ int platform_task_complete(struct task_handle *handle);
 /* Opaque mutex struct. */
 struct platform_mutex;
 
-/* Allocate and initialize a platform mutex. */
-struct platform_mutex *platform_mutex_init();
+/**
+ * @brief Allocate and initialize a platform mutex.
+ *
+ * @param mutex POSIX mutex. If *mutex is NULL, memory will be allocated.
+ * @retval 0 Success
+ * @retval -1 Fail
+ */
+int platform_mutex_init(struct platform_mutex **mutex);
 
 void platform_mutex_lock(struct platform_mutex *mutex);
 void platform_mutex_unlock(struct platform_mutex *mutex);
@@ -76,8 +90,14 @@ void platform_mutex_unlock(struct platform_mutex *mutex);
 /* Opaque notifier struct. */
 struct platform_condvar;
 
-/* Allocate and initialize a platform condvar. */
-struct platform_condvar *platform_condvar_init();
+/**
+ * @brief Allocate and initialize a platform condvar.
+ *
+ * @param cond POSIX condvar. If *cond is NULL, memory will be allocated.
+ * @retval 0 Success
+ * @retval -1 Fail
+ */
+int platform_condvar_init(struct platform_condvar **cond);
 
 void platform_condvar_wait(struct platform_condvar *condvar,
 			   struct platform_mutex *mutex);
