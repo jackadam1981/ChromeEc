eb9cb983d178005e11dbd29c8ed60efa356468eb
Dawid Niedzwiecki <dawidn@google.com>
2024-02-16 14:35:22 +0100

    zephyr: panic: store Zephyr panic reason
    Store a fatal error reason provided by Zephyr if no reason has been set
    by the EC sources. There are reason defines provided by kernel and arch
    specific.
    
    Add a magic number to the panic data info to distinguish reasons provided
    by CrosEC and Zephyr.
    
    BUG=b:324574796
    TEST=Call the abort function and make sure K_ERR_KERNEL_PANIC is set as
    reason and PANIC_INFO_ZEPHYR_MAGIC as info in the panic data
    
    Change-Id: I05f7f520318b0c7751253fa11620c7da0fb356ec

---
+++ zephyr/test/drivers/panic_reason/src/panic_reason.c
@@ -0,0 +1,48 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "panic.h"
+#include "test/drivers/test_state.h"
+
+#include <zephyr/ztest.h>
+#include <zephyr/ztest_assert.h>
+
+#include <sys/types.h>
+
+ZTEST(panic_reason, test_panic_reason_zephyr)
+{
+	uint32_t reason;
+	uint32_t info;
+	uint8_t exception;
+
+	panic_set_reason(0, 0, 0);
+
+	k_sys_fatal_error_handler(K_ERR_KERNEL_PANIC, NULL);
+
+	/* No panic reason set by EC, make sure the one from Zephyr i stored */
+	panic_get_reason(&reason, &info, &exception);
+	zassert_equal(K_ERR_KERNEL_PANIC, reason);
+	zassert_equal(PANIC_INFO_ZEPHYR_MAGIC, info);
+	zassert_equal(0, exception);
+}
+
+ZTEST(panic_reason, test_panic_reason_ec)
+{
+	uint32_t reason;
+	uint32_t info;
+	uint8_t exception;
+
+	panic_set_reason(PANIC_SW_EXIT, 0x64, 0xFA);
+
+	k_sys_fatal_error_handler(K_ERR_KERNEL_PANIC, NULL);
+
+	/* Make sure, that the panic reason set by EC is in place */
+	panic_get_reason(&reason, &info, &exception);
+	zassert_equal(PANIC_SW_EXIT, reason);
+	zassert_equal(0x64, info);
+	zassert_equal(0xFA, exception);
+}
+
+ZTEST_SUITE(panic_reason, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
