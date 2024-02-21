7b9c449baa1e16b27359db4488412c0598564997
Dawid Niedzwiecki <dawidn@google.com>
2024-02-21 14:43:23 +0100

    zephyr: fpmcu: add always_memset test to testcase.yaml
    Add the always_memset test as to the testcase.yaml file as nope for
    compatibility. Crytoc is not supported with Zephyr.
    
    BUG=b:324574796
    TEST=connect Bloonchipper via servo_micro and run
    './test/run_device_tests.py --board=bloonchipper --flasher=servo_micro
    --tests always_memset --zephyr'
    
    Change-Id: I1c8496c15edc35d3b933165934c5a5b18e91d86f

---
+++ zephyr/test/hwtest/src/always_memset.c
@@ -0,0 +1,15 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "zephyr/kernel.h"
+
+#include <zephyr/ztest.h>
+
+ZTEST_SUITE(always_memset, NULL, NULL, NULL, NULL, NULL);
+
+ZTEST(always_memset, test_always_memset)
+{
+	zassert_equal(1, 1);
+}
