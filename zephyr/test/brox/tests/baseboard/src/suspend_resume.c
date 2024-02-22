39121190c0f66e2e37b63cd5f83fdf174ed971ca
Keith Short <keithshort@chromium.org>
2024-02-22 12:23:38 -0700

    brox: Fix suspend/resume test logic
    The suspend/resume test and changes to the brox suspend/resume logic
    landed simultaneously that now causes the brox suspend/resume test to
    fail.
    
    Update the test logic to match the brox suspend/resume logic.
    
    BUG=none
    TEST=./twister -ivc -T zephyr/test/brox
    
    Change-Id: I331ab37a6c77823b160c346b707582a65e139d9a

--- zephyr/test/brox/tests/baseboard/src/suspend_resume.c
+++ zephyr/test/brox/tests/baseboard/src/suspend_resume.c
@@ -20,12 +20,12 @@ ZTEST(suspend_resume, test_suspend_resume_handler)
 		GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp3300_wlan);
 
 	gpio_pin_set_dt(gpio_ec_en_pp3300_wlan, 1);
-	ap_power_ev_send_callbacks(AP_POWER_SUSPEND);
+	ap_power_ev_send_callbacks(AP_POWER_SHUTDOWN);
 	zassert_false(gpio_emul_output_get(gpio_ec_en_pp3300_wlan->port,
 					   gpio_ec_en_pp3300_wlan->pin));
 
 	gpio_pin_set_dt(gpio_ec_en_pp3300_wlan, 0);
-	ap_power_ev_send_callbacks(AP_POWER_RESUME);
+	ap_power_ev_send_callbacks(AP_POWER_PRE_INIT);
 	zassert_true(gpio_emul_output_get(gpio_ec_en_pp3300_wlan->port,
 					  gpio_ec_en_pp3300_wlan->pin));
 }
