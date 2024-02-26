08a0eb6cfba7f9145fd1e493533ae27d18f1121a
Firas Sammoura <fsammoura@google.com>
2024-02-26 15:11:35 -0800

    common: Enable SRAM MPU protection on helipilot
    BUG=b:324677118
    TEST=run_device_tests.py -b helipilot -t sram_mpu_protection
    
    Change-Id: Ic2a70b04f88e9adb37afd6b9d22d8929fcc9b26a

--- common/system.c
+++ common/system.c
@@ -188,6 +188,20 @@ static uint32_t __attribute__((unused)) get_size(enum ec_image copy)
 	}
 }
 
+#if defined(CONFIG_EXTERNAL_STORAGE) || !defined(CONFIG_FLASH_PHYSICAL)
+void mpu_protect_code_ram_script(void)
+{
+	int ret;
+
+	ret = mpu_protect_code_ram();
+	if (ret == EC_SUCCESS) {
+		CPRINTS("code RAM locked.");
+	} else {
+		CPRINTS("Failed to lock code RAM (%d)", ret);
+	}
+}
+#endif
+
 test_mockable int system_is_locked(void)
 {
 	static int is_locked = -1;
