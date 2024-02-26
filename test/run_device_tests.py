3ad5f2ef82c5d103604d3220156f8768f18cbfe3
Firas Sammoura <fsammoura@google.com>
2024-02-26 15:11:35 -0800

    common: Enable SRAM MPU protection on helipilot
    BUG=b:324677118
    TEST=run_device_tests.py -b helipilot -t sram_mpu_protection
    
    Change-Id: Ic2a70b04f88e9adb37afd6b9d22d8929fcc9b26a

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -368,6 +368,7 @@ class AllTests:
             TestConfig(test_name="sbrk", imagetype_to_use=ImageType.RO),
             TestConfig(test_name="sha256"),
             TestConfig(test_name="sha256_unrolled"),
+            TestConfig(test_name="sram_mpu_protection"),
             TestConfig(test_name="static_if"),
             TestConfig(test_name="stdlib"),
             TestConfig(test_name="std_vector"),
@@ -701,7 +702,6 @@ def fp_sensor_sel(
     # power cycle after setting sensor type to ensure detection
     power_cycle(board_config)
     return True
-
 
 def hw_write_protect(enable: bool) -> None:
     """Enable/disable hardware write protect."""
