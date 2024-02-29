a5d4b76b41eba7d6e3671f4f338a2279fc35424c
Patryk Duda <patrykd@google.com>
2024-02-29 18:53:37 +0100

    libc: Implement locking interface for Newlib
    Newlib needs mutex and recursive mutex to run properly on multithreaded
    systems. EC already provides mutexes, but recursive mutexes must be
    implemented from scratch.
    
    BUG=b:326310636
    TEST=Build and flash bloonchipper firmware. Make sure that firmware
         works properly.
    
    Change-Id: Ib21fd1e26eacf4547d9e2f79bf11e20061e0a448

--- libc/syscalls.c
+++ libc/syscalls.c
@@ -11,6 +11,7 @@
  * https://sourceware.org/git/?p=newlib-cygwin.git;a=tree;f=libgloss/libnosys.
  */
 
+#include "builtin/assert.h"
 #include "gettimeofday.h"
 #include "link_defs.h"
 #include "panic.h"
@@ -21,6 +22,7 @@
 #include "uart.h"
 
 #include <errno.h>
+#include <stdlib.h>
 
 #include <sys/stat.h>
 
@@ -122,4 +124,185 @@ void *_sbrk(intptr_t incr)
 	heap_end += incr;
 
 	return (void *)prev_heap_end;
+}
+
+/*
+ * Newlib Retargetable Locking Interface Implementation for EC.
+ */
+
+/* Make sure that Newlib was compilet with retargetable locking support. */
+#ifndef _RETARGETABLE_LOCKING
+#error "Newlib must be compiled with retargetable locking support"
+#endif
+
+/* Non-recursive mutex. */
+
+/*
+ * Static locks
+ *
+ * We don't use K_MUTEX_DEFINE macro, because macro uses 'static' keyword which
+ * makes mutexes unvisible outside of the file.
+ */
+struct mutex __lock___at_quick_exit_mutex;
+struct mutex __lock___tz_mutex;
+struct mutex __lock___dd_hash_mutex;
+struct mutex __lock___arc4random_mutex;
+
+void __retarget_lock_init(_LOCK_T *lock)
+{
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	*lock = malloc(sizeof(struct mutex));
+	ASSERT(*lock != NULL);
+
+	memset(*lock, 0, sizeof(struct mutex));
+}
+
+void __retarget_lock_close(_LOCK_T lock)
+{
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	free(lock);
+}
+
+void __retarget_lock_acquire(_LOCK_T lock)
+{
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	mutex_lock((struct mutex *)lock);
+}
+
+int __retarget_lock_try_acquire(_LOCK_T lock)
+{
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	return mutex_try_lock((struct mutex *)lock);
+}
+
+void __retarget_lock_release(_LOCK_T lock)
+{
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	mutex_unlock((struct mutex *)lock);
+}
+
+/* Recursive mutex. */
+
+struct mutex_r {
+	atomic_t owner;
+	atomic_t waiters;
+	int count;
+};
+
+#define K_MUTEX_R_DEFINE(name)  \
+	struct mutex_r name = { \
+		.owner = -1,    \
+		.waiters = 0,   \
+		.count = 0,     \
+	}
+
+/* Static locks */
+K_MUTEX_R_DEFINE(__lock___sinit_recursive_mutex);
+K_MUTEX_R_DEFINE(__lock___sfp_recursive_mutex);
+K_MUTEX_R_DEFINE(__lock___atexit_recursive_mutex);
+K_MUTEX_R_DEFINE(__lock___malloc_recursive_mutex);
+K_MUTEX_R_DEFINE(__lock___env_recursive_mutex);
+
+void __retarget_lock_init_recursive(_LOCK_T *lock)
+{
+	struct mutex_r *mutex_r;
+
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	*lock = malloc(sizeof(struct mutex_r));
+	ASSERT(*lock != NULL);
+
+	mutex_r = (struct mutex_r *)*lock;
+	mutex_r->owner = -1;
+	mutex_r->count = 0;
+	mutex_r->waiters = 0;
+}
+
+void __retarget_lock_close_recursive(_LOCK_T lock)
+{
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	free(lock);
+}
+
+void __retarget_lock_acquire_recursive(_LOCK_T lock)
+{
+	struct mutex_r *mutex_r = (struct mutex_r *)lock;
+	atomic_val_t owner, current;
+
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	owner = -1;
+	current = task_get_current();
+
+	atomic_or(&mutex_r->waiters, 1 << current);
+	while (!atomic_compare_exchange(&mutex_r->owner, &owner, current) &&
+	       owner != current) {
+		owner = -1;
+		task_wait_event_mask(TASK_EVENT_MUTEX, 0);
+	}
+
+	/* Only owner can escape while loop above. */
+
+	/* We are not waiting for this mutex, so remove from waiters. */
+	atomic_clear_bits(&mutex_r->waiters, 1 << current);
+	mutex_r->count++;
+}
+
+int __retarget_lock_try_acquire_recursive(_LOCK_T lock)
+{
+	struct mutex_r *mutex_r = (struct mutex_r *)lock;
+	atomic_val_t owner, current;
+
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	owner = -1;
+	current = task_get_current();
+
+	if (!atomic_compare_exchange(&mutex_r->owner, &owner, current) &&
+	    owner != current) {
+		return 0;
+	}
+
+	/* Only owner can escape if above. */
+
+	mutex_r->count++;
+
+	return 1;
+}
+
+void __retarget_lock_release_recursive(_LOCK_T lock)
+{
+	struct mutex_r *mutex_r = (struct mutex_r *)lock;
+	uint32_t waiters;
+
+	ASSERT(lock != NULL);
+	ASSERT(!in_interrupt_context());
+
+	/* Panic if not called by owner */
+	ASSERT(mutex_r->owner == task_get_current());
+	ASSERT(mutex_r->count > 0);
+
+	mutex_r->count--;
+	if (mutex_r->count == 0) {
+		mutex_r->owner = -1;
+		waiters = atomic_load(&mutex_r->waiters);
+		if (waiters) {
+			task_set_event(__fls(waiters), TASK_EVENT_MUTEX);
+		}
+	}
 }
