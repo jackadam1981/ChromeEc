b6ae13c1eccf4ae9946ed30f107e5a7e845934a6
Scott Collyer <scollyer@google.com>
2024-03-02 14:16:29 -0800

    pdc: Add console support for connector_reset
    This CL adds support in the PDC API for the PDC command
    CONNECTOR_RESET which can be used to trigger sending either
    a hard or data reset to a port partner. In addition, a new PDC console
    command `pdc conn_reset <port> [hard|data]' is added to support PD
    TAST testing.
    
    BUG=b:321266756
    TEST=Verified with USB-PD analyzer that hard reset message is sent to
    the port partner and the PD contract is re-established. Testing data
    reset requires a USB-4 connection which is not yet functional.
    LOW_COVERAGE_REASON=Coverage to be handled in b/317065172
    
    Change-Id: Ia507d2aee3f4d59cdf31ab194521d7d9dabe6bd6

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -94,6 +94,8 @@ enum pdc_cmd_t {
 	CMD_PDC_GET_CONNECTOR_STATUS,
 	/** CMD_PDC_GET_VDO */
 	CMD_PDC_GET_VDO,
+	/** CMD_PDC_CONNECTOR_RESET */
+	CMD_PDC_CONNECTOR_RESET,
 
 	/** CMD_PDC_COUNT */
 	CMD_PDC_COUNT
@@ -249,7 +251,8 @@ static const char *const pdc_cmd_names[] = {
 	[CMD_PDC_SET_UOR] = "PDC_SET_UOR",
 	[CMD_PDC_SET_PDR] = "PDC_SET_PDR",
 	[CMD_PDC_GET_CONNECTOR_STATUS] = "PDC_GET_CONNECTOR_STATUS",
-	[CMD_PDC_GET_VDO] = "PDC_GET_VDO"
+	[CMD_PDC_GET_VDO] = "PDC_GET_VDO",
+	[CMD_PDC_CONNECTOR_RESET] = "PDC_CONNECTOR_RESET",
 };
 
 /**
@@ -459,6 +462,8 @@ struct pdc_port_t {
 	uint8_t vdo_type[VDO_NUM];
 	/** Array used to store VDOs returned from the GET_VDO command */
 	uint32_t vdo[VDO_NUM];
+	/** CONNECTOR_RESET temp variable used with CMD_PDC_CONNECTOR_RESET */
+	union connector_reset_t connector_reset;
 };
 
 /**
@@ -1119,6 +1124,9 @@ static int send_pdc_cmd(struct pdc_port_t *port)
 	case CMD_PDC_GET_VDO:
 		rv = pdc_get_vdo(port->pdc, port->vdo_req, port->vdo_type,
 				 port->vdo);
+		break;
+	case CMD_PDC_CONNECTOR_RESET:
+		rv = pdc_connector_reset(port->pdc, port->connector_reset);
 		break;
 	default:
 		LOG_ERR("Invalid command: %d", port->cmd->cmd);
@@ -2139,6 +2147,25 @@ int pdc_power_mgmt_get_bus_info(int port, struct pdc_bus_info_t *pdc_bus_info)
 	 */
 
 	return pdc_get_bus_info(pdc_data[port]->port.pdc, pdc_bus_info);
+}
+
+int pdc_power_mgmt_connector_reset(int port, enum connector_reset reset_type)
+{
+	/* Make sure port is in range and that an output buffer is provided */
+	if (!is_pdc_port_valid(port)) {
+		return -ERANGE;
+	}
+
+	/* Make sure port is connected */
+	if (!pdc_power_mgmt_is_connected(port)) {
+		return EC_SUCCESS;
+	}
+
+	pdc_data[port]->port.connector_reset.raw_value = 0;
+	pdc_data[port]->port.connector_reset.reset_type = reset_type;
+
+	/* Block until command completes */
+	return public_api_block(port, CMD_PDC_CONNECTOR_RESET);
 }
 
 static int pdc_run_get_discovery(int port)
