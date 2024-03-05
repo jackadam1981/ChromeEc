ad37c887a22bc2be9610ce3c5829a43c748924b6
Scott Collyer <scollyer@google.com>
2024-03-04 16:30:17 -0800

    pdc: Add support for GET_VDO for discovery identity
    This CL implements the RTS545 GET_VDO command and modifies both the
    src/snk.attached flow to call this method when a new connection is
    detected. These VDOs then are used to extract the VID/PID and Product
    Type
    
    BUG=b:319390850,b:328819306
    TEST=Verifed that the VID, PID, and PTYPE are retrieved and matches
    what is shown in the discovery identity message decoded in the total
    phase trace.
    LOW_COVERAGE_REASON=Coverage to be handled in b/317065172
    
    Change-Id: I69ae334f34782407a78c238b4e52ea2220538232

--- zephyr/include/usbc/pdc_power_mgmt.h
+++ zephyr/include/usbc/pdc_power_mgmt.h
@@ -295,4 +295,31 @@ int pdc_power_mgmt_get_bus_info(int port, struct pdc_bus_info_t *pdc_bus_info);
  */
 int pdc_power_mgmt_get_rev(int port, enum tcpci_msg_type type);
 
+/**
+ * @brief Get port partner VID
+ *
+ * @param port USB-C port number
+ *
+ * @retval VID if available, 0 otherwise
+ */
+uint16_t pdc_power_mgmt_get_identity_vid(int port);
+
+/**
+ * @brief Get port partner PID
+ *
+ * @param port USB-C port number
+ *
+ * @retval PID if available, 0 otherwise
+ */
+uint16_t pdc_power_mgmt_get_identity_pid(int port);
+
+/**
+ * @brief Get port partner prodcut type
+ *
+ * @param port USB-C port number
+ *
+ * @retval product type if available, 0 otherwise
+ */
+uint8_t pdc_power_mgmt_get_product_type(int port);
+
 #endif /* __CROS_EC_PDC_POWER_MGMT_H */
