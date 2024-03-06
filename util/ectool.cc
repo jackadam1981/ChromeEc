34937fbdfcfb036bc29ab133d0616941ac7d7cd1
Daisuke Nojiri <dnojiri@chromium.org>
2024-03-05 18:28:27 -0800

    ectoo/ppm: Add ack, notify, cap sub-commands
    BUG=b:319124515
    TEST=Brox SKU1
    
    Change-Id: Iac0c2d73b79424c072d9cc051813b4fed62a3b6f

--- util/ectool.cc
+++ util/ectool.cc
@@ -20,6 +20,7 @@
 #include "panic.h"
 #include "tablet_mode.h"
 #include "usb_pd.h"
+#include "extra/um_ppm/include/ppm.h"
 
 #include <assert.h>
 #include <ctype.h>
@@ -806,29 +807,61 @@ static const char *reset_cause_to_str(uint16_t cause)
 	return "(shutdown unknown)";
 }
 
-int cmd_ppm(int argc, char *argv[])
+static int ppm_set_control(const struct ucsi_control *ctrl)
 {
+	struct ec_params_ucsi_ppm_set *p =
+			(struct ec_params_ucsi_ppm_set *)ec_outbuf;
 	int rv;
 
-	if (argc == 2 && !strcmp(argv[1], "reset")) {
-		struct ec_params_ucsi_ppm_set *p =
-				(struct ec_params_ucsi_ppm_set *)ec_outbuf;
+	memset(ec_outbuf, 0, ec_max_outsize);
 
-		p->offset = 8;
-		p->data[0] = 0x01; /* PPM_RESET */
-		rv = ec_command(EC_CMD_UCSI_PPM_SET, 0, p,
-				sizeof(p->offset) + 1, NULL, 0);
-		if (rv < 0) {
-			fprintf(stderr,
-				"ERROR: EC_CMD_UCSI_PPM_SET failed: %d\n", rv);
-			return rv;
-		}
+	p->offset = UCSI_CONTROL_OFFSET;
+	memcpy(p->data, ctrl, sizeof(*ctrl));
+
+	rv = ec_command(EC_CMD_UCSI_PPM_SET, 0, p,
+			sizeof(p->offset) + sizeof(*ctrl), NULL, 0);
+	if (rv < 0)
+		fprintf(stderr, "ERROR: EC_CMD_UCSI_PPM_GET failed: %d\n", rv);
+	return rv;
+}
+
+static int ppm_get_message_in(uint8_t *data, uint8_t len)
+{
+	struct ec_params_ucsi_ppm_get p = {
+		.offset = UCSI_MESSAGE_IN_OFFSET,
+		.size = len,
+	};
+	int rv;
+
+	if (len > MESSAGE_IN_SIZE)
+		return -1;
+
+	memset(data, 0, len);
+	rv = ec_command(EC_CMD_UCSI_PPM_GET, 0, &p, sizeof(p), data, len);
+	if (rv < 0)
+		fprintf(stderr, "ERROR: EC_CMD_UCSI_PPM_GET failed: %d\n", rv);
+
+	return rv;
+}
+
+int cmd_ppm(int argc, char *argv[])
+{
+	if (argc == 2 && !strcmp(argv[1], "reset")) {
+		struct ucsi_control ctrl = {
+			.command = UCSI_CMD_PPM_RESET,
+			.data_length = 0,
+			.command_specific = {},
+		};
+		if (ppm_set_control(&ctrl) < 0)
+			return -1;
+		printf("Successfully executed PPM_RESET.\n");
 	} else if (argc == 2 && !strcmp(argv[1], "cci")) {
 		struct ec_params_ucsi_ppm_get p = {
 			.offset = 4,
 			.size = 4,
 		};
 		uint32_t cci;
+		int rv;
 
 		rv = ec_command(EC_CMD_UCSI_PPM_GET, 0, &p, sizeof(p), &cci,
 				sizeof(cci));
@@ -838,13 +871,63 @@ int cmd_ppm(int argc, char *argv[])
 			return rv;
 		}
 		printf("CCI=0x%08x\n", cci);
-	} else {
-		if (argc < 2) {
-			fprintf(stderr, "ERROR: Invalid number of args.\n");
+	} else if (argc == 3 && !strcmp(argv[1], "notify")) {
+		uint32_t notify;
+		char *e;
+
+		notify = strtoul(argv[2], &e, 0);
+		if (e && *e) {
+			fprintf(stderr, "ERROR: Invalid arg: '%s'\n", argv[2]);
 			return -1;
 		}
 
-		fprintf(stderr, "Sub-command '%s' is unknown.\n", argv[1]);
+		struct ucsi_control ctrl = {
+			.command = UCSI_CMD_SET_NOTIFICATION_ENABLE,
+			.data_length = 0,
+			.command_specific = {},
+		};
+		memcpy(ctrl.command_specific, &notify, sizeof(notify));
+		if (ppm_set_control(&ctrl) < 0)
+			return -1;
+		printf("Successfully executed SET_NOTIFICATION_ENABLE.\n");
+	} else if (argc == 3 && !strcmp(argv[1], "ack")) {
+		uint8_t ack;
+		char *e;
+
+		ack = strtoul(argv[2], &e, 0);
+		if ((e && *e) || ack > BIT(0) + BIT(1)) {
+			fprintf(stderr, "ERROR: Invalid arg: '%s'\n", argv[2]);
+			return -1;
+		}
+
+		struct ucsi_control ctrl = {
+			.command = UCSI_CMD_ACK_CC_CI,
+			.data_length = 0,
+			.command_specific = {},
+		};
+		memcpy(ctrl.command_specific, &ack, sizeof(ack));
+		if (ppm_set_control(&ctrl) < 0)
+			return -1;
+		printf("Successfully executed ACK_CC_CI. Acknowledged:%s%s.\n",
+		       ack & BIT(0) ? " CI" : "", ack & BIT(1) ? " CC" : "");
+	} else if (argc == 2 && !strcmp(argv[1], "cap")) {
+		struct ucsi_control ctrl = {
+			.command = UCSI_CMD_GET_CAPABILITY,
+			.data_length = 0,
+			.command_specific = {},
+		};
+		if (ppm_set_control(&ctrl) < 0)
+			return -1;
+		uint8_t cap[128];
+
+		if (ppm_get_message_in(cap, sizeof(cap)) < 0)
+			return -1;
+		printf("Successfully executed GET_CAPABILITY.\n");
+		hexdump_canonical(cap, sizeof(cap), 0);
+	} else {
+		fprintf(stderr,
+			"Sub-command '%s' is unknown or number of args for '%s' is invalid.\n",
+			argv[1], argv[1]);
 		return -1;
 	}
 
