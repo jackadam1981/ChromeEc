9ce3ef1b6019af5e58a3be8ac7dafb0238d2cc62
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2024-03-07 08:57:10 +0530

    pd: Enable retimer f/w update flow with PD->PMC communication
    BUG=none
    TEST=none
    
    Change-Id: I34c3af04d9046e8bc4d1238797fe389be65d2768

--- include/config.h
+++ include/config.h
@@ -6200,10 +6200,9 @@
  * CONFIG_USBC_SS_MUX both are defined. USBC retimer firmware update feature
  * requires both.
  */
-#if (defined(CONFIG_USBC_RETIMER_FW_UPDATE) &&             \
-     (!((defined(CONFIG_USB_PD_TCPMV2) ||                  \
-	 defined(CONFIG_PLATFORM_EC_USB_PD_CONTROLLER)) && \
-	defined(CONFIG_USBC_SS_MUX))))
+#if (defined(CONFIG_USBC_RETIMER_FW_UPDATE) &&                            \
+     (!((defined(CONFIG_USB_PD_TCPMV2) && defined(CONFIG_USBC_SS_MUX)) || \
+	defined(CONFIG_PLATFORM_EC_USB_PD_CONTROLLER))))
 #error "Retimer firmware update requires TCPMv2 or USB PD controller, and" \
 	"USBC_SS_MUX."
 #endif
