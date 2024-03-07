58b55ceb4b3b243a36c4db8f9ce293894753a7cd
Daisuke Nojiri <dnojiri@chromium.org>
2024-03-06 16:52:36 -0800

    util: Fix hexdump_canonical
    The current version of hexdump_canonical prints data beyond the boundary
    because it adds index i and j (i.e. data[i + j]) while j starts from i
    (i.e. j = i; j < i + 16 ...). This CL fixes it.
    
    BUG=None
    TEST=ectool ppm cap
    
    Change-Id: Iec7116c46795363a6fd4eb4ae5ba87c2f478aa88

--- util/misc_util.cc
+++ util/misc_util.cc
@@ -201,11 +201,12 @@ void hexdump_canonical(const uint8_t *data, size_t len, uint32_t offset_start)
 				printf("   ");
 		}
 		printf(" |");
-		for (j = i; j < i + 16 && j < len; j++)
-			if (isprint(data[i + j]))
-				printf("%c", data[i + j]);
-			else
-				printf(".");
+		for (j = i; j < i + 16; j++) {
+			int c = ' ';
+			if (j < len)
+				c = isprint(data[j]) ? data[j] : '.';
+			printf("%c", c);
+		}
 		printf("|\n");
 	}
 	printf("%08x\n", i + offset_start);
