744c0c2a18fee9fc5700eb3b1f99c575dd5ad298
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2024-03-07 08:57:10 +0530

    Brox: enable PMC-PD communication
    PD ALT task disabled in EC as PMC is expected to handle.
    
    BUG=b:327622474
    TEST=USB storage device enumerated on Brox.
    Retimer FW update flow is functional.
    
    Change-Id: I288208193e36f326558003dd734c3bde779c38ad

--- zephyr/subsys/pd_controller/pdc_host_cmd.c
+++ zephyr/subsys/pd_controller/pdc_host_cmd.c
@@ -71,4 +71,16 @@ uint8_t get_pd_control_flags(int port)
 	/* To support EC_CMD_USB_PD_CONTROL  */
 	return 0;
 }
+void resume_pd_intel_altmode_task(void)
+{
+	return;
+}
+void suspend_pd_intel_altmode_task(void)
+{
+	return;
+}
+bool is_pd_intel_altmode_task_suspended(void)
+{
+	return true;
+}
 #endif
