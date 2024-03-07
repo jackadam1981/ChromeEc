2c2954906affc8a9b1ce267dfe4c4d2a8663be2b
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2024-03-07 08:57:10 +0530

    Brox: enable retimer firmware update flow
    For PMC-PD flow, PD ALT mode task is disabled in EC.
    Hence provide stub api's for suspend and resume of the
    PD ALT mode task.
    
    BUG=b:327622474
    TEST=zephyr build brox;
    
    Change-Id: I288208193e36f326558003dd734c3bde779c38ad

--- zephyr/subsys/pd_controller/retimer_fw_update.c
+++ zephyr/subsys/pd_controller/retimer_fw_update.c
@@ -332,6 +332,16 @@ void usb_retimer_fw_update_process_op(int port, int op)
 			k_work_init(&enter_workq_info.retimer_update_workq,
 				    enter_retimer_fw_update);
 			k_work_submit(&enter_workq_info.retimer_update_workq);
+			/*
+			 * PMC handles the PD I2C DATA.
+			 * Hence force the mux mode to TBT mode as retimer
+			 */
+			if (!IS_ENABLED(CONFIG_USBC_INTEL_ALTMODE) &&
+			    IS_ENABLED(CONFIG_PLATFORM_EC_USB_PD_CONTROLLER)) {
+				usb_mux_set(port, USB_PD_MUX_TBT_COMPAT_ENABLED,
+					    USB_SWITCH_CONNECT,
+					    pd_get_polarity(port));
+			}
 		} else {
 			atomic_set_bit(&fw_update_status,
 				       USB_PD_RETIMER_FW_UPDATE_ERROR);
