d1400c16ae4789e46c58b3e6786cbb32317cb067
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2024-03-08 08:55:36 +0800

    sm5803: reset flag in set mode when discharge
    The sm5803 did not enter idle mode when we set discharge mode first.
    This CL will make sm5803 stop charging battery when we set discharge
    mode first and set idle mode.
    
    BUG=b:327137021
    TEST=ectool chargecontrol discharge/ ectool chargecontrol idle
    
    Change-Id: I4af5905ec430d3c68acad8da362215e3c4858a26

--- driver/charger/sm5803.c
+++ driver/charger/sm5803.c
@@ -1563,7 +1563,13 @@ static enum ec_error_list sm5803_set_mode(int chgnum, int mode)
 					  MASK_SET);
 		rv |= sm5803_flow1_update(chgnum, CHARGER_MODE_SINK, MASK_SET);
 		charge_idle_enabled = 0;
-	}
+	} else if ((get_chg_ctrl_mode() == CHARGE_CONTROL_DISCHARGE) &&
+		   charge_idle_enabled)
+		/*
+		 * Discharge is controlled by discharge_on_ac, so only need to
+		 * reset charge_idle_enabled.
+		 */
+		charge_idle_enabled = 0;
 #endif
 
 	return rv;
