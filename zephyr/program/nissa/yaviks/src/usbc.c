9f2a1187d343f38b747383ac885be0c28a3fb7de
Devin Lu <Devin.Lu@quantatw.com>
2024-03-09 14:36:51 +0800

    Yaviks: Override board_usb_charger_support according to whether BC1.2 exists or not.
    Yaviks has a combination of the BC1.2 or without BC1.2 chip. Query
    the BC1.2 chip with I2C to know if the BC1.2 chip exists or not.
    Then override board_usb_charger_support to disable usb charger task.
    
    BUG=none
    TEST=1. On Yahiko. Attached BC1.2 charger/charger while S5 to G3.
    Make sure the battery is charging.
    2. On Yavkis. Attached BC1.2 charger/charger while S5 to G3.
    Make sure the battery is charging.
    3. On Yaviks. Attach USB devices. Make sure the device is workable.
    4. On the EC console. Don’t see any BC1.2 fault.
    
    Change-Id: Ifb313e65dfc8eeb6338758cfb5708397efc6ca25

--- zephyr/program/nissa/yaviks/src/usbc.c
+++ zephyr/program/nissa/yaviks/src/usbc.c
@@ -5,12 +5,14 @@
 
 #include "charge_state.h"
 #include "chipset.h"
+#include "driver/bc12/pi3usb9201.h"
 #include "driver/charger/sm5803.h"
 #include "driver/tcpm/it83xx_pd.h"
 #include "driver/tcpm/ps8xxx_public.h"
 #include "driver/tcpm/tcpci.h"
 #include "hooks.h"
 #include "system.h"
+#include "usb_charge.h"
 #include "usb_mux.h"
 #include "watchdog.h"
 
@@ -308,6 +310,24 @@ void usb_c1_interrupt(enum gpio_signal s)
 	schedule_deferred_pd_interrupt(1);
 }
 
+static bool pi3usb9201_is_exist;
+
+static void board_has_bc12_detected(void)
+{
+	int rv, value;
+
+	rv = i2c_read8(I2C_PORT_USB_C0_TCPC, PI3USB9201_I2C_ADDR_3_FLAGS,
+		       PI3USB9201_REG_CLIENT_STS, &value);
+
+	/* Enable BC1.2 data role detection if PI3USB9201 ACKed */
+	if (rv == EC_SUCCESS)
+		pi3usb9201_is_exist = true;
+	else
+		pi3usb9201_is_exist = false;
+
+	LOG_INF("PI3USB9201 is exist: %d", pi3usb9201_is_exist);
+}
+
 /*
  * Check state of IRQ lines at startup, ensuring an IRQ that happened before
  * the EC started up won't get lost (leaving the IRQ line asserted and blocking
@@ -318,6 +338,8 @@ void usb_c1_interrupt(enum gpio_signal s)
  */
 void board_handle_initial_typec_irq(void)
 {
+	board_has_bc12_detected();
+
 	check_c0_line();
 	/*
 	 * C1 port IRQ already handled by board_process_pd_alert(), we don't
@@ -377,4 +399,9 @@ int pd_snk_is_vbus_provided(int port)
 	sm5803_get_chg_det(port, &chg_det);
 
 	return chg_det;
+}
+
+__override bool board_usb_charger_support(void)
+{
+	return pi3usb9201_is_exist;
 }
