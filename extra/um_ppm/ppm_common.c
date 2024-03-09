42816c3ae7aea0f6658f7221751cac6accea3d78
Daisuke Nojiri <dnojiri@chromium.org>
2024-03-08 16:56:06 -0800

    PPM: Run PPM & PDM in parallel
    This CL allows the PPM to receive a notification from a PDC driver
    through a callback for asynchronous events (i.e. CI). The PPM filters
    out events by the mask set by the SET_NOTIFICATION_ENABLE command before
    notifying the OPM.
    
    BUG=b:319124515,b:339703938
    TEST=Boot Brox SKU1 with OPM.
    
    Change-Id: Ie8fd0807824ab5e3871b8fb28eb9005532cacfff

--- extra/um_ppm/ppm_common.c
+++ extra/um_ppm/ppm_common.c
@@ -216,7 +216,11 @@ static void ppm_common_handle_async_event(struct ppm_common_device *dev)
 			 * OPM.
 			 */
 			if (port < dev->num_ports) {
-				alert_port = true;
+				/* Let through only enabled notifications. */
+				port_status = &dev->per_port_status[port];
+				if (dev->notif_mask.raw_value &
+				    port_status->connector_status_change)
+					alert_port = true;
 			} else {
 				DLOG("No more ports needing OPM alerting");
 			}
@@ -310,6 +314,12 @@ static int ppm_common_execute_pending_cmd(struct ppm_common_device *dev)
 		break;
 	case UCSI_CMD_PPM_RESET:
 		ppm_common_reset_data(dev);
+		ret = 0;
+		goto success;
+	case UCSI_CMD_SET_NOTIFICATION_ENABLE:
+		/* Save the notification mask. */
+		platform_memcpy(&dev->notif_mask, control->command_specific,
+				sizeof(dev->notif_mask));
 		ret = 0;
 		goto success;
 	default:
