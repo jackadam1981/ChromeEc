adf391ad3574c4b953bf723fd7ade615e08036c3
Daisuke Nojiri <dnojiri@chromium.org>
2024-03-08 16:56:06 -0800

    PPM: Run PPM & PDM in parallel
    This CL allows the PPM to receive a notification from a PDC driver
    through a callback for asynchronous events (i.e. CI). The PPM filters
    out events by the mask set by the SET_NOTIFICATION_ENABLE command before
    notifying the OPM.
    
    BUG=b:319124515,b:339703938
    TEST=Boot Brox SKU1 with OPM.
    
    Change-Id: Ie8fd0807824ab5e3871b8fb28eb9005532cacfff

--- zephyr/subsys/ucsi/ppm_driver.c
+++ zephyr/subsys/ucsi/ppm_driver.c
@@ -175,6 +175,10 @@ static int ucsi_get_active_port_count(const struct device *dev)
 	return NUM_PORTS;
 }
 
+void ppm_cci_cb(union cci_event_t cci_event, void *cb_data);
+int rts54_set_handler_cb_ex(const struct device *dev,
+			    pdc_cci_handler_cb_t cci_cb, void *cb_data);
+
 static struct ucsi_pd_driver ppm_drv = {
 	.init_ppm = ucsi_ppm_init,
 	.get_ppm = ucsi_ppm_get,
@@ -184,6 +188,8 @@ static struct ucsi_pd_driver ppm_drv = {
 
 static int ppm_init(const struct device *device)
 {
+	const struct ppm_config *cfg =
+		(const struct ppm_config *)device->config;
 	struct ppm_data *dat = (struct ppm_data *)device->data;
 	const struct ucsi_pd_driver *drv = device->api;
 	union ec_common_control ctrl;
@@ -198,6 +204,10 @@ static int ppm_init(const struct device *device)
 	if (!dat->ppm) {
 		LOG_ERR("Failed to open PPM");
 		return -ENODEV;
+	}
+
+	for (int i = 0; i < cfg->active_port_count; i++) {
+		rts54_set_handler_cb_ex(cfg->lpm[i], ppm_cci_cb, NULL);
 	}
 
 	return 0;
