7ed9316812f18865da1941f0f9391c90802e2661
Daisuke Nojiri <dnojiri@chromium.org>
2024-03-08 16:56:06 -0800

    PPM: Run PPM & PDM in parallel
    This CL allows the PPM to receive a notification from a PDC driver
    through a callback for asynchronous events (i.e. CI). The PPM filters
    out events by the mask set by the SET_NOTIFICATION_ENABLE command before
    notifying the OPM.
    
    BUG=b:319124515
    TEST=Boot Brox SKU1 with OPM.
    
    Change-Id: Ie8fd0807824ab5e3871b8fb28eb9005532cacfff

--- zephyr/subsys/ucsi/ucsi_host_cmd.c
+++ zephyr/subsys/ucsi/ucsi_host_cmd.c
@@ -23,6 +23,30 @@ LOG_MODULE_REGISTER(ucsi, LOG_LEVEL_INF);
 
 static struct ucsi_ppm_driver *ppm_drv;
 
+void ppm_cci_cb(union cci_event_t cci, void *cb_data)
+{
+	struct ppm_common_device *dev = DEV_CAST_FROM(ppm_drv->dev);
+
+	LOG_INF("%s %s%s(0x%08x)", __func__, cci.connector_change ? "CI " : "",
+		cci.command_completed ? "CC " : "", cci.raw_value);
+
+	if (dev->ppm_state == PPM_STATE_IDLE ||
+	    dev->ppm_state == PPM_STATE_NOT_READY) {
+		LOG_INF("%s: Not ready to handle CCI", __func__);
+		return;
+	}
+
+	memcpy(&dev->ucsi_data.cci, &cci, sizeof(cci));
+
+	if (cci.connector_change) {
+		dev->pending.async_event = 1;
+		dev->last_connector_alerted = cci.connector_change;
+	}
+
+	LOG_INF("%s: Waking up PPM", __func__);
+	platform_condvar_signal(dev->ppm_condvar);
+}
+
 static void opm_notify(void *context)
 {
 	LOG_INF("Notifying OPM");
