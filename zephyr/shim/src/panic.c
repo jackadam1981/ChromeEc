524c21bc701c9450c76d19d97d575152c350cbd9
Dawid Niedzwiecki <dawidn@google.com>
2024-03-11 12:08:22 +0100

    zephyr: panic: print missing registers that contains panic data
    PANIC_REG_LIST contains a list of registers to print and copy from esf
    structure provided by Zephyr. For some reasons, it doesn't contain
    all registers that are store panic data.
    
    Update the list to print all registers that may contain panic data -
    reason/info/exception.
    
    BUG=b:324574796
    TEST=Run CC 'crash watchdog', then 'panicinfo' and make sure all panic
    data are printed.
    
    Change-Id: I636a371ace2b41eb063472119ec577bf7b5c2984

--- zephyr/shim/src/panic.c
+++ zephyr/shim/src/panic.c
@@ -120,6 +120,10 @@ void panic_data_print(const struct panic_data *pdata)
 {
 	PANIC_REG_LIST(PANIC_PRINT_REGS, PANIC_PRINT_REGS);
 	PANIC_REG_LIST(PANIC_PRINT_REGS, PANIC_PRINT_REGS);
+#if defined(CONFIG_RISCV) && !defined(CONFIG_64BIT)
+	PANIC_PRINT_REGS(NULL, riscv.regs[10], S1);
+	PANIC_PRINT_REGS(NULL, riscv.regs[11], S0);
+#endif
 }
 
 static void copy_esf_to_panic_data(const z_arch_esf_t *esf,
