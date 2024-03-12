7f614b1bf8479d1008742d6e4a577db708aa8082
Tristan Honscheid <honscheid@google.com>
2024-03-12 13:09:53 -0600

    pdc_power_mgmt: return to init state when resetting PDC
    In pdc_power_mgmt_reset():
    
     1. Check the provided port parameter is valid.
     2. After executing the CMD_PDC_RESET command, force the PDC Power Mgmt
        state machine back to its init state so both layers (driver and PDC
        Power Mgmt) get reset.
    
    BUG=None
    TEST=Run `pdc reset {0|1}` and PD stack recovers
    LOW_COVERAGE_REASON=Coverage to be handled in b/317065172
    
    Change-Id: I19960d4add7501b04ae526bdf704500571fa1462

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -1826,8 +1826,22 @@ uint32_t pdc_power_mgmt_get_vbus_voltage(int port)
 
 int pdc_power_mgmt_reset(int port)
 {
+	int rv;
+
+	if (!is_pdc_port_valid(port)) {
+		return -ERANGE;
+	}
+
 	/* Block until command completes */
-	return public_api_block(port, CMD_PDC_RESET);
+	rv = public_api_block(port, CMD_PDC_RESET);
+	if (rv) {
+		return rv;
+	}
+
+	/* Revert back to init state */
+	set_pdc_state(&pdc_data[port]->port, PDC_INIT);
+
+	return 0;
 }
 
 uint8_t pdc_power_mgmt_get_src_cap_cnt(int port)
