c20329ecd01b67422ff5c603df6b11508c3d4d52
Jiahong Wei <weijiahong@huaqin.corp-partner.google.com>
2024-03-14 12:32:32 +0800

    Wugtrio: Initial zephyr config for wugtrio
    Initial EC Zephyr config for Staryu/wugtrio.
    
    BUG=N/A
    TEST=zmake build wugtrio
    LOW_COVERAGE_REASON=Tests will be added in N/A
    
    Change-Id: I362caedfe6d7c6339996848d709cc34b9d29ec9a

---
+++ zephyr/program/corsola/wugtrio/src/ppc.c
@@ -0,0 +1,31 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "baseboard_usbc_config.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "system.h"
+#include "usb_mux.h"
+#include "usbc/ppc.h"
+
+static void board_usbc_init(void)
+{
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_usb_c0_ppc_bc12));
+}
+DECLARE_HOOK(HOOK_INIT, board_usbc_init, HOOK_PRIO_POST_DEFAULT);
+
+void ppc_interrupt(enum gpio_signal signal)
+{
+	switch (signal) {
+	case GPIO_SIGNAL(DT_NODELABEL(usb_c0_ppc_bc12_int_odl)):
+		ppc_chips[0].drv->interrupt(0);
+		break;
+	case GPIO_SIGNAL(DT_ALIAS(gpio_usb_c1_ppc_int_odl)):
+		ppc_chips[1].drv->interrupt(1);
+		break;
+	default:
+		break;
+	}
+}
