d782048802a845a0f3b3b72f6cdb65c8b5b3817d
Caveh Jalali <caveh@chromium.org>
2024-03-19 13:45:18 -0700

    zephyr/drivers/pdc_rts54xx: GET_IC_STATUS does not have a sub-command
    The GET_IC_STATUS command does not have a sub-command, so don't use that
    field.
    
    BUG=none
    TEST='./twister -T zephyr/test/pdc' passes
    
    Change-Id: Ibce499ebd194e922c6d8b2618fb7321aa190e1c5

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -149,7 +149,7 @@ const struct smbus_cmd_t SET_UOR = { 0x0E, 0x04, 0x09 };
 const struct smbus_cmd_t SET_PDR = { 0x0E, 0x04, 0x0B };
 const struct smbus_cmd_t UCSI_GET_ERROR_STATUS = { 0x0E, 0x03, 0x13 };
 const struct smbus_cmd_t UCSI_READ_POWER_LEVEL = { 0x0E, 0x05, 0x1E };
-const struct smbus_cmd_t GET_IC_STATUS = { 0x3A, 0x03, 0x00 };
+const struct smbus_cmd_t GET_IC_STATUS = { 0x3A, 0x03 };
 const struct smbus_cmd_t SET_RETIMER_FW_UPDATE_MODE = { 0x20, 0x03, 0x00 };
 const struct smbus_cmd_t GET_CABLE_PROPERTY = { 0x0E, 0x02, 0x11 };
 
@@ -1860,7 +1860,7 @@ static int rts54_get_info(const struct device *dev, struct pdc_info_t *info)
 	uint8_t payload[] = {
 		GET_IC_STATUS.cmd,
 		GET_IC_STATUS.len,
-		GET_IC_STATUS.sub,
+		0,
 		0x00,
 		26,
 	};
