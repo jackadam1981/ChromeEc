18d392548bfec5ae5d56802831a23a0e10e6e242
Caveh Jalali <caveh@chromium.org>
2024-03-19 13:46:07 -0700

    zephyr/drivers/pdc_rts54xx: Fix GET_CABLE_PROPERTY write length
    The GET_CABLE_PROPERTY command WR_DATA_LEN is 3.
    
    BUG=none
    TEST='./twister -T zephyr/test/pdc' passes
    
    Change-Id: I02a635a5646d02ae72b880d97b877482ebbb8350

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -151,7 +151,7 @@ const struct smbus_cmd_t UCSI_GET_ERROR_STATUS = { 0x0E, 0x03, 0x13 };
 const struct smbus_cmd_t UCSI_READ_POWER_LEVEL = { 0x0E, 0x05, 0x1E };
 const struct smbus_cmd_t GET_IC_STATUS = { 0x3A, 0x03 };
 const struct smbus_cmd_t SET_RETIMER_FW_UPDATE_MODE = { 0x20, 0x03, 0x00 };
-const struct smbus_cmd_t GET_CABLE_PROPERTY = { 0x0E, 0x02, 0x11 };
+const struct smbus_cmd_t GET_CABLE_PROPERTY = { 0x0E, 0x03, 0x11 };
 
 /**
  * @brief PDC Command states
