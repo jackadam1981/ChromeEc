ae2a64a52365e1262f8a24609bf07e04de75f9c0
Scott Collyer <scollyer@google.com>
2024-03-23 14:04:43 -0600

    pdc: Fix issue with RTS54 SET_TPC_RP cmd format
    This CL modifies the SET_TPC_RP driver function to set both the TPC Rp
    and PD Rp fields. The documentation doesn't state this, but
    setting the TPC Rp field only results in an error from the TPC.
    
    BUG=:
    TEST=Verified that sending SET_TPC_RP no longers results in an error
    ping status result. In addition, verified with total_phase that CC
    voltage level does change as expected when the Rp value is changed.
    
    Change-Id: I12c4a2623d4a574459fb3041518ad2326004cfbd

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -1605,6 +1605,7 @@ static int rts54_set_power_level(const struct device *dev,
 {
 	struct pdc_data_t *data = dev->data;
 	uint8_t byte = 0;
+	int i;
 
 	if (get_state(data) != ST_IDLE) {
 		return -EBUSY;
@@ -1626,9 +1627,17 @@ static int rts54_set_power_level(const struct device *dev,
 		break;
 	}
 
+	byte |= (byte << 2);
+
 	uint8_t payload[] = {
 		SET_TPC_RP.cmd, SET_TPC_RP.len, SET_TPC_RP.sub, 0x00, byte,
 	};
+
+	printk("SET_TPC_RP: ");
+	for (i=0; i < ARRAY_SIZE(payload); i++) {
+		printk("%02x ", payload[i]);
+	}
+	printk("\n");
 
 	return rts54_post_command(dev, CMD_SET_TPC_RP, payload,
 				  ARRAY_SIZE(payload), NULL);
