df7febad30c0a77ae922889ba8dd3af310ef6b39
Scott Collyer <scollyer@google.com>
2024-03-23 14:17:52 -0600

    pdc: Add usbc port source current sharing policy
    This CL adds device policy manager code for usbc source current
    sharing policy for the PDC. The algorithm is very similar to and
    adapted from the same function in TCPMv2. The PDC does not have the
    same level of granularity as TCPMv2, but the same ideas still apply.
    
    By default PDC ports will offer 1.5A per port. If there is at least
    one 3A port available, then the source current sharing policy will
    allocate 3A to the first port partner that requires it. This is
    determined from checkign the sink cap message for USB-PD
    connections. Type-c only port partners don't support this so 3A is
    allocated to any type-c only port partner if 3A is still available.
    
    BUG=b:319643480
    TEST=Tested on Brox and verified that 3A is allocated to the first
    USB-PD sink port partner that requires it. Verfied that type-c only
    port partners are allocated 3A, but get reduced to 1.5A when another
    sink port partner is attached.
    LOW_COVERAGE_REASON=Coverage to be handled in b/317065172
    
    Change-Id: Ie0b68eb97becb14f7f8ecc82436824009fed671d

--- zephyr/include/usbc/pdc_power_mgmt.h
+++ zephyr/include/usbc/pdc_power_mgmt.h
@@ -456,4 +456,45 @@ unsigned int pdc_power_mgmt_get_max_voltage(void);
  */
 void pdc_power_mgmt_request_source_voltage(int port, int mv);
 
+/**
+ * @brief Sets the SRC CAPs sent by the PDC to its port partner when attached in
+ * a source power role
+ *
+ * @param port USB-C port number
+ * @param src_pdo Pointer to array of PDOs
+ * @param pdc_count Number of PDOs to write
+ *
+ * @retval 0 if successful or error code
+ */
+int pdc_power_mgmt_set_src_pdo(int port, const uint32_t *src_pdo,
+			       uint8_t pdo_count);
+
+/**
+ * @brief Set current limit for USB-C port acting in a source power role
+ *
+ * @param port USB-C port number
+ * @param tcc Desired source current limit (1.5 or 3.0A)
+ *
+ * @retval 0 if successful or error code
+ */
+int pdc_power_mgmt_set_current_limit(int port, enum usb_typec_current_t tcc);
+
+/**
+ * @brief Get the default source current limit for a USB-C port
+ *
+ * @param port USB-C port number
+ *
+ * @retval USB-C current limit enum value
+ */
+enum usb_typec_current_t pdc_power_mgmt_get_default_current_limit_rp(int port);
+
+/**
+ * @brief Enable/Disable FRS for a given port
+ *
+ * @param port USB-C port number
+ *
+ * @retval 0 if successful or error code
+ */
+int pdc_power_mgmt_frs_enable(int port_num, bool enable);
+
 #endif /* __CROS_EC_PDC_POWER_MGMT_H */
