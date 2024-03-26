FrameworkComputer/EmbeddedController

commit 0fff3a0bd4e3e82165eebac88c809cac1417e600

omnigul: Add condition use Accel and Gyro by FWCONFIG.
This patch add FW config to deceide enable Accel and Gyro or not.

BUG=b:280001153
TEST=ectool motionsense show
sensor 0: 0  0  0
sensor 1: 0  0  0
sensor 2: 0  0  0

Change-Id: Id040713b20e415f52f2feb1f9320a3affa94711b
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4487062
Tested-by: Logan Liao <logan_liao@compal.corp-partner.google.com>
Reviewed-by: Logan Liao <logan_liao@compal.corp-partner.google.com>
Commit-Queue: Kyle Lin <kylelinck@google.com>
Code-Coverage: Kyle Lin <kylelinck@google.com>
Reviewed-by: Kyle Lin <kylelinck@google.com>
Reviewed-by: Josh Tsai <josh_tsai@compal.corp-partner.google.com>

board/omnigul/sensors.c
@@ -8,9 +8,11 @@
 #include "common.h"
 #include "driver/accel_lis2dw12.h"
 #include "driver/accelgyro_lsm6dso.h"
+#include "fw_config.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "motion_sense.h"
+#include "tablet_mode.h"
 #include "temp_sensor.h"
 #include "temp_sensor/thermistor.h"
 #include "thermal.h"
@@ -134,10 +136,19 @@ const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 
 static void baseboard_sensors_init(void)
 {
-	/* Enable gpio interrupt for lid accel sensor */
-	gpio_enable_interrupt(GPIO_EC_ACCEL_INT_R_L);
-	/* Enable gpio interrupt for base accelgyro sensor */
-	gpio_enable_interrupt(GPIO_EC_IMU_INT_R_L);
+	if (ec_cfg_has_tabletmode()) {
+		/* Enable gpio interrupt for lid accel sensor */
+		gpio_enable_interrupt(GPIO_EC_ACCEL_INT_R_L);
+		/* Enable gpio interrupt for base accelgyro sensor */
+		gpio_enable_interrupt(GPIO_EC_IMU_INT_R_L);
+	} else {
+		gmr_tablet_switch_disable();
+		/* Base accel is not stuffed, don't allow line to float */
+		gpio_set_flags(GPIO_EC_ACCEL_INT_R_L,
+			       GPIO_INPUT | GPIO_PULL_DOWN);
+		gpio_set_flags(GPIO_EC_IMU_INT_R_L,
+			       GPIO_INPUT | GPIO_PULL_DOWN);
+	}
 }
 DECLARE_HOOK(HOOK_INIT, baseboard_sensors_init, HOOK_PRIO_INIT_I2C + 1);
 

board/omnigul/fw_config.c
@@ -41,3 +41,8 @@ union omnigul_cbi_fw_config get_fw_config(void)
 {
 	return fw_config;
 }
+
+bool ec_cfg_has_tabletmode(void)
+{
+	return (fw_config.tab_mode == TABLETMODE_ENABLED);
+}

board/omnigul/fw_config.h
@@ -53,4 +53,6 @@ union omnigul_cbi_fw_config {
  */
 union omnigul_cbi_fw_config get_fw_config(void);
 
+bool ec_cfg_has_tabletmode(void);
+
 #endif /* __BOARD_OMNIGUL_FW_CONFIG_H_ */

jackadam1981/ChromeEc

commit f1799ef163bfea6b3187a19a278a29f2380239b1

omnigul: Fix dynamic sensor configuration
Set the number of MEMS sensors to 0 for clamshell.
It avoids error in the kernel:
cros-ec-sensorhub cros-ec-sensorhub.2.auto: no info for EC sensor 0 : -22/3
It speeds up ec boot since we don't have to fail sensor init.

BUG=b:280001153
TEST=Check "ectool motionsense" shows not sensor.

Fixes: commit 0fff3a0bd4e3e821 ("omnigul: Add condition use Accel and Gyro by FWCONFIG.")
Change-Id: Id5dd157cade1ca112d4d74e0bf9090185e629dfb

board/omnigul/board.h
@@ -54,6 +54,7 @@
 /* Sensor console commands */
 #define CONFIG_CMD_ACCELS
 #define CONFIG_CMD_ACCEL_INFO
+#define CONFIG_DYNAMIC_MOTION_SENSOR_COUNT
 
 /* USB Type A Features */
 #define USB_PORT_COUNT 1

board/omnigul/sensors.c
@@ -132,7 +132,7 @@ struct motion_sensor_t motion_sensors[] = {
 		.max_frequency = LSM6DSO_ODR_MAX_VAL,
 	},
 };
-const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 
 static void baseboard_sensors_init(void)
 {
@@ -142,6 +142,7 @@ static void baseboard_sensors_init(void)
 		/* Enable gpio interrupt for base accelgyro sensor */
 		gpio_enable_interrupt(GPIO_EC_IMU_INT_R_L);
 	} else {
+		motion_sensor_count = 0;
 		gmr_tablet_switch_disable();
 		/* Base accel is not stuffed, don't allow line to float */
 		gpio_set_flags(GPIO_EC_ACCEL_INT_R_L,