5804554471f965699552f096943701f87cf6428c
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2024-03-27 17:00:56 +0800

    ppc/syv682x: In the FRS process, add the judgment of whether CC is RP
    In the FRS process, add the judgment of whether CC is RP.
    
    BUG=b:331572419
    TEST==1. zmake build anraggar
    2.FRS functions normally
    LOW_COVERAGE_REASON=Tests will be added in b:331572419
    
    Change-Id: Idb267cc7ebe11d0cad30cbee37fee5ef24c9fc50

--- driver/ppc/syv682x.c
+++ driver/ppc/syv682x.c
@@ -261,6 +261,27 @@ static bool syv682x_interrupt_filter(int port, int regval, int regmask,
 	return false;
 }
 
+#ifdef CONFIG_USB_PD_FRS_PPC
+#define CC_RP_DEBOUNCE 500
+
+static int check_cc_rp_timeout(int port, int timeout)
+{
+	enum tcpc_cc_voltage_status cc1, cc2;
+
+	tcpm_get_cc(port, &cc1, &cc2);
+
+	while (((cc_is_rp(cc1)) || (cc_is_rp(cc2))) != true) {
+		if (task_wait_event(timeout) == TASK_EVENT_TIMER) {
+			ppc_init(port);
+			cprints(0, "PPC_init done!!!!!");
+			break;
+		}
+		tcpm_get_cc(port, &cc1, &cc2);
+	}
+	return EC_SUCCESS;
+}
+#endif
+
 /*
  * Two status registers can trigger the ALERT_L pin, STATUS and CONTROL_4
  * These registers are clear on read if the condition has been cleared.
@@ -282,6 +303,21 @@ static void syv682x_handle_status_interrupt(int port, int regval)
 	if (IS_ENABLED(CONFIG_USB_PD_FRS_PPC)) {
 		if (syv682x_interrupt_filter(port, regval, SYV682X_STATUS_FRS,
 					     SYV682X_FLAGS_FRS)) {
+			/*
+			 * According to the syv682 manual, the FRS process of
+			 * SYV682 only determines Rd pull down. FRS may be
+			 * triggered by unplugging the device. base on USB
+			 * PD 3.2 spec, version ,sections 8.3.2.9. Its Source
+			 * Port drives CC to ground no larger than
+			 * tFRSwapTx(MAX). In order to avoid FRS errors in
+			 * syv682. Add the check CC state judgment after FRS is
+			 * triggered.
+			 */
+			if (check_cc_rp_timeout(port, CC_RP_DEBOUNCE)) {
+				syv682x_init(port);
+				return;
+			}
+
 			atomic_or(&flags[port], SYV682X_FLAGS_SOURCE_ENABLED);
 			atomic_clear_bits(&flags[port],
 					  SYV682X_FLAGS_SINK_ENABLED);
