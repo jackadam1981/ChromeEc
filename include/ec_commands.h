1d0cd3fb89a02ae4a5570568f80c6dd9d65aeab7
Daisuke Nojiri <dnojiri@chromium.org>
2024-04-01 16:18:59 -0700

    features: Add feature flag for Finch
    BUG=b:365865009
    TEST=Run feat command on EC console
    
    Change-Id: I0cb6da3821eb2d3fe1a9e9eaece5fca8d27fd87c

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -1760,6 +1760,10 @@ enum ec_feature_code {
 	 * The EC supports UCSI PPM.
 	 */
 	EC_FEATURE_UCSI_PPM = 54,
+	/*
+	 * The EC supports feature finch.
+	 */
+	EC_FEATURE_FINCH = 55,
 };
 
 #define EC_FEATURE_MASK_0(event_code) BIT(event_code % 32)
