3eed6e6697015f6b911897d0ab993392a87d07e9
Sam Hurst <shurst@google.com>
2024-04-02 08:31:30 -0700

    PD_CONTROLLER: Add get_vconn_state API
    Add get_vconn_state API
    
    BUG=b:326466765
    TEST=ectool usbpd; With sink device, verified that VCONN was sourced,
    and with source device, verified that VCONN was not sourced
    LOW_COVERAGE_REASON=Coverage to be handled in b/317065172
    
    Change-Id: I978323f213a166606218a3eff6df8d0d45357ec3

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -110,7 +110,8 @@ enum pdc_cmd_t {
 	CMD_PDC_CONNECTOR_RESET,
 	/** CMD_PDC_GET_IDENTITY_DISCOVERY */
 	CMD_PDC_GET_IDENTITY_DISCOVERY,
-
+	/** CMD_PDC_IS_SOURCING_VCONN */
+	CMD_PDC_IS_VCONN_SOURCING,
 	/** CMD_PDC_COUNT */
 	CMD_PDC_COUNT
 };
@@ -511,6 +512,8 @@ struct pdc_port_t {
 	enum pdo_type_t pdo_type;
 	/** Charge current while in TypeC Sink state */
 	uint32_t typec_current_ma;
+	/** True if port is sourcing VCONN */
+	bool vconn_sourcing;
 };
 
 /**
@@ -1290,6 +1293,9 @@ static int send_pdc_cmd(struct pdc_port_t *port)
 		rv = pdc_get_identity_discovery(port->pdc,
 						&port->discovery_state);
 		break;
+	case CMD_PDC_IS_VCONN_SOURCING:
+		rv = pdc_is_vconn_sourcing(port->pdc, &port->vconn_sourcing);
+		break;
 	default:
 		LOG_ERR("Invalid command: %d", port->cmd->cmd);
 		return -EIO;
@@ -1868,13 +1874,18 @@ int pdc_power_mgmt_comm_is_enabled(int port)
 
 bool pdc_power_mgmt_get_vconn_state(int port)
 {
-	/* Make sure port is connected */
-	if (!pdc_power_mgmt_is_connected(port)) {
+	/* Make sure port is source connected */
+	if (!pdc_power_mgmt_is_source_connected(port)) {
 		return false;
 	}
 
-	/* TODO: Add driver support for this */
-	return true;
+	/* Block until command completes */
+	if (public_api_block(port, CMD_PDC_IS_VCONN_SOURCING)) {
+		/* something went wrong */
+		return false;
+	}
+
+	return pdc_data[port]->port.vconn_sourcing;
 }
 
 bool pdc_power_mgmt_get_partner_usb_comm_capable(int port)
