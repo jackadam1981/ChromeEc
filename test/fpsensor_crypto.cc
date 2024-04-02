68d4d3cbe58e868c7a2622e6f3124281ee5d2966
Tom Hughes <tomhughes@chromium.org>
2024-04-02 15:24:26 -0700

    Use std::span for key argument in aes_gcm_encrypt
    BUG=none
    TEST=none
    
    Change-Id: I40209cc118ed41aa0fbb2fd394f7b6e1b0995695

--- test/fpsensor_crypto.cc
+++ test/fpsensor_crypto.cc
@@ -782,9 +782,8 @@ test_static ec_error_list test_aes_128_gcm_encrypt_in_place()
 	};
 
 	ec_error_list ret = aes_128_gcm_encrypt(
-		key.data(), key.size(), plaintext.data(), plaintext.data(),
-		plaintext.size(), nonce.data(), nonce.size(), tag.data(),
-		tag.size());
+		key, plaintext.data(), plaintext.data(), plaintext.size(),
+		nonce.data(), nonce.size(), tag.data(), tag.size());
 	TEST_EQ(ret, EC_SUCCESS, "%d");
 	TEST_ASSERT_ARRAY_EQ(plaintext.data(), expected_ciphertext.data(),
 			     plaintext.size());
