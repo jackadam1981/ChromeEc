ceffd5659da164d52635643e89369aa487e46104
Craig Hesling <hesling@chromium.org>
2024-04-03 15:33:44 -0700

    util: Add druid to version string
    BUG=b:332760097
    TEST=BOARD=bloonchipper-druid ./util/getversion.sh
    TEST=BOARD=bloonchipper ./util/getversion.sh
    
    Cq-Depend: chromium:7135458
    Change-Id: I343aa5b77f336e43d3507a9b5a9219bfef52303f

--- util/getversion.sh
+++ util/getversion.sh
@@ -124,6 +124,10 @@ main() {
   if [[ -d ./private ]]; then
     fp_common_dir_list+=( ./private )
   fi
+  # Example: bloonchipper-druid
+  if [[ "${BOARD}" =~ -druid$ ]]; then
+    fp_common_dir_list+=( ./private/fingerprint/druid )
+  fi
 
   case "${BOARD}" in
     cr50)
