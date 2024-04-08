f6f071bdf6b34627820c0a5c92e704f953e6b44d
Eric Yilun Lin <yllin@chromium.org>
2024-04-08 11:18:03 -0700

    rauru: support hibernate mode
    BUG=b:333344533
    TEST=hibernate EC, and EN_ULP is asserted
    
    Change-Id: I6036ae93f6befc90b077387031481005950e3f1f

---
+++ zephyr/program/rauru/src/hibernate.c
@@ -0,0 +1,19 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include "charger.h"
+#include "driver/charger/isl923x_public.h"
+#include "system.h"
+
+#include <zephyr/drivers/gpio.h>
+
+__override void board_hibernate_late(void)
+{
+	/* b/283037861: Pre-off the 5V power line for hibernate. */
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(en_pp5000_z1), 1);
+	/* It takes around 30ms to release the PP5000 capacitance. */
+	udelay(30 * MSEC);
+
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_ulp), 1);
+}
