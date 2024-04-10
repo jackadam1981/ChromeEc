87d078582dfd56f30919b981f6fa2604323a9711
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2024-04-10 15:32:25 +0530

    PDC_Driver: add support for CMD_GET_PCH_DATA_STATUS
    This implementation provides interface to send
    CMD_GET_PCH_DATA_STATUS command to the Realtek PDC
    firmware. This is needed to populate debug data
    as part of pd debug commands.
    
    BUG=b:330475942
    TEST=zmake build brox
    LOW_COVERAGE_REASON=Coverage to be handled in b/337721162
    
    Change-Id: I1c4301f2d3e13c60a480653928c90ea75e129cc9

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -155,6 +155,7 @@ const struct smbus_cmd_t UCSI_SET_CCOM = { 0x0E, 0x04, 0x08 };
 const struct smbus_cmd_t GET_IC_STATUS = { 0x3A, 0x03 };
 const struct smbus_cmd_t SET_RETIMER_FW_UPDATE_MODE = { 0x20, 0x03, 0x00 };
 const struct smbus_cmd_t GET_CABLE_PROPERTY = { 0x0E, 0x03, 0x11 };
+const struct smbus_cmd_t GET_PCH_DATA_STATUS = { 0x08, 0x02, 0xE0 };
 
 /**
  * @brief PDC Command states
@@ -285,6 +286,8 @@ enum cmd_t {
 	CMD_GET_IDENTITY_DISCOVERY,
 	/** CMD_GET_IS_VCONN_SOURCING */
 	CMD_GET_IS_VCONN_SOURCING,
+	/** Get PDC ALT MODE Status Register value */
+	CMD_GET_PCH_DATA_STATUS,
 };
 
 /**
@@ -399,6 +402,7 @@ static const char *const cmd_names[] = {
 	[CMD_GET_VDO] = "GET VDO",
 	[CMD_GET_IDENTITY_DISCOVERY] = "CMD_GET_IDENTITY_DISCOVERY",
 	[CMD_GET_IS_VCONN_SOURCING] = "CMD_GET_IS_VCONN_SOURCING",
+	[CMD_GET_PCH_DATA_STATUS] = "CMD_GET_PCH_DATA_STATUS",
 };
 
 /**
@@ -2072,6 +2076,31 @@ static int rts54_is_vconn_sourcing(const struct device *dev,
 				    (uint8_t *)vconn_sourcing);
 }
 
+static int rts54_get_pch_data_status(const struct device *dev, uint8_t port_num,
+				     uint8_t *status_reg)
+{
+	struct pdc_data_t *data = dev->data;
+
+	if (get_state(data) != ST_IDLE) {
+		return -EBUSY;
+	}
+
+	if (status_reg == NULL) {
+		return -EINVAL;
+	}
+
+	uint8_t payload[] = {
+		GET_PCH_DATA_STATUS.cmd,
+		GET_PCH_DATA_STATUS.len,
+		GET_PCH_DATA_STATUS.sub,
+		port_num,
+	};
+
+	rts54_post_command(dev, CMD_GET_PCH_DATA_STATUS, payload,
+			   ARRAY_SIZE(payload), status_reg);
+	return 0;
+}
+
 static bool rts54_is_init_done(const struct device *dev)
 {
 	struct pdc_data_t *data = dev->data;
@@ -2184,6 +2213,7 @@ static const struct pdc_driver_api_t pdc_driver_api = {
 	.get_identity_discovery = rts54_get_identity_discovery,
 	.set_comms_state = rts54_set_comms_state,
 	.is_vconn_sourcing = rts54_is_vconn_sourcing,
+	.get_pch_data_status = rts54_get_pch_data_status,
 };
 
 static void pdc_interrupt_callback(const struct device *dev,
