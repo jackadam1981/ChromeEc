36a01f3adf501fae517718e421b8e38de647754c
Dawid Niedzwiecki <dawidn@google.com>
2024-04-10 10:51:19 +0200

    zephyr: fpmcu: add stdlib test
    Add the stdlib test. It verifies builtin stdlib function used by Zephyr.
    
    This code was migrated from the following EC test:
    https://chromium.googlesource.com/chromiumos/platform/ec/+/5aa9baf19b6b8e1c49276c8c37418d0a2d96f91a/test/stdlib.c
    
    BUG=b:324574796
    TEST=connect Bloonchipper via servo_micro and run
    './test/run_device_tests.py --board=bloonchipper --flasher=servo_micro
    --tests stdlib --zephyr'
    
    Change-Id: Ibb93cc788937357f0ce19f88716f1c52e4e009b0

---
+++ zephyr/test/lib/src/stdlib.c
@@ -0,0 +1,18 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/ztest.h>
+
+#include <strings.h>
+
+/* Test only builtin stdlib functions used in Zephyr. */
+ZTEST_SUITE(stdlib, NULL, NULL, NULL, NULL, NULL);
+
+ZTEST(stdlib, test_strcasecmp)
+{
+	zassert_true(strcasecmp("test string", "TEST strIng") == 0);
+	zassert_true(strcasecmp("test123!@#", "TesT123!@#") == 0);
+	zassert_true(strcasecmp("lower", "UPPER") != 0);
+}
