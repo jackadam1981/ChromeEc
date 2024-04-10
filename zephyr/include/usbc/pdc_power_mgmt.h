652897f0a77e23c9e9b6869504ab49cf27c0efb7
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2024-04-10 15:44:16 +0530

    brox: enable 'altmode' and 'ectool usbpdmuxinfo'
    To troubleshoot PDC-PMC issues, this patch enables the debug
    commands 'altmode' and 'ectool usbpdmuxinfo'.
    This is implemented by sending VDM command GET_PCH_DATA_STATUS
    to PDC and retrieving the required information.
    The information received is populated in the debug commands.
    
    BUG=b:330475942
    TEST=zmake build brox
    
    Change-Id: I19b2657e5c90f9cf0006553a3c438ee371ce5173

--- zephyr/include/usbc/pdc_power_mgmt.h
+++ zephyr/include/usbc/pdc_power_mgmt.h
@@ -417,4 +417,14 @@ int pdc_power_mgmt_set_comms_state(bool run);
 int pdc_power_mgmt_get_connector_status(
 	int port, union connector_status_t *connector_status);
 
+/**
+ * @brief Return PCH DATA STATUS register for PMC Debug
+ *
+ * @param port USB-C port number
+ * @param PCH data status Output variable to store register value
+ *
+ * @retval 0 if successful or error code
+ */
+int pdc_power_mgmt_get_pch_data_status(int port, uint8_t *status);
+
 #endif /* __CROS_EC_PDC_POWER_MGMT_H */
