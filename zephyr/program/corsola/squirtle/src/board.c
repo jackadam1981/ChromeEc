143ea459b7de48716b8c4f42d964ab71bed5221c
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2024-04-11 21:36:34 +0800

    squirtle: Initial zephyr config for squirtle
    BUG=None
    TEST=zmake build squirtle
    
    Change-Id: I0de6065c50a62532d98508595dcce3858e2d42bd

---
+++ zephyr/program/corsola/squirtle/src/board.c
@@ -0,0 +1,70 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "charge_state.h"
+#include "common.h"
+#include "cros_cbi.h"
+#include "dps.h"
+#include "driver/accel_lis2dw12.h"
+#include "driver/accelgyro_bmi3xx.h"
+#include "gpio.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "math_util.h"
+#include "motion_sense.h"
+#include "motionsense_sensors.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+#include <dt-bindings/battery.h>
+
+LOG_MODULE_REGISTER(board_init, LOG_LEVEL_ERR);
+
+bool voltorb_is_more_efficient(int curr_mv, int prev_mv, int batt_mv,
+			       int batt_mw, int input_mw)
+{
+	int batt_state;
+
+	battery_status(&batt_state);
+
+	/* Choose 15V PDO or higher when battery is full. */
+	if ((batt_state & SB_STATUS_FULLY_CHARGED) && (curr_mv >= 15000) &&
+	    (prev_mv < 15000 || curr_mv <= prev_mv)) {
+		return true;
+	} else {
+		return ABS(curr_mv - batt_mv) < ABS(prev_mv - batt_mv);
+	}
+}
+
+static bool base_use_alt_sensor;
+
+void motion_interrupt(enum gpio_signal signal)
+{
+	if (base_use_alt_sensor) {
+		lis2dw12_interrupt(signal);
+	} else {
+		bmi3xx_interrupt(signal);
+	}
+}
+
+static void alt_sensor_init(void)
+{
+	base_use_alt_sensor = cros_cbi_ssfc_check_match(
+		CBI_SSFC_VALUE_ID(DT_NODELABEL(base_sensor_lis2dw12)));
+
+	motion_sensors_check_ssfc();
+}
+DECLARE_HOOK(HOOK_INIT, alt_sensor_init, HOOK_PRIO_POST_I2C);
+
+__override struct dps_config_t dps_config = {
+	.k_less_pwr = 93,
+	.k_more_pwr = 96,
+	.k_sample = 1,
+	.k_window = 3,
+	.t_stable = 10 * SECOND,
+	.t_check = 5 * SECOND,
+	.is_more_efficient = &voltorb_is_more_efficient,
+};
