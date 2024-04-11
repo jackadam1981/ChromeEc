2786ba93b4e8b99d4a07d28b937a2fb1501c5031
Daisuke Nojiri <dnojiri@chromium.org>
2024-04-11 09:23:48 -0700

    RTS54XX: Cache connector status
    With this CL, pdc_rts54xx will cache the data returned from
    GET_CONNECTOR_STATUS command. The cache is renewed on every IRQ for the
    port specified by the ARA.
    
    BUG=b:319124515
    TEST=Plug an AC adapter. Verify chgsup command prints it as a PD and a
         type-c charger.
    
    Change-Id: I579c4f5702c29faa27ba393e343967b24096e1c9

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -367,6 +367,10 @@ struct pdc_data_t {
 	uint16_t error_recovery_counter;
 	/** Error Status used during initialization */
 	union error_status_t es;
+	/** Connector Status */
+	union connector_status_t conn_status;
+	/** Connector Status Cache State */
+	bool conn_status_cached;
 };
 
 /**
@@ -850,6 +854,7 @@ static void handle_irqs(struct pdc_data_t *data)
 				/* Set the interrupt event */
 				pdc_int_data->cci_event
 					.vendor_defined_indicator = 1;
+				pdc_int_data->conn_status_cached = false;
 				/* Notify system of status change */
 				call_cci_event_cb(pdc_int_data);
 				/* done with this port */
@@ -1258,6 +1263,12 @@ static void st_read_run(void *o)
 		*vconn_sourcing = (data->rd_buf[11] & 0x20);
 		break;
 	}
+	case CMD_GET_CONNECTOR_STATUS:
+		memcpy(data->user_buf, data->rd_buf + offset, len);
+		/* Save connector status in cache. */
+		memcpy(&data->conn_status, data->user_buf, len);
+		data->conn_status_cached = true;
+		break;
 	default:
 		/* No preprocessing needed for the user data */
 		memcpy(data->user_buf, data->rd_buf + offset, len);
@@ -1758,6 +1769,15 @@ static int rts54_get_connector_status(const struct device *dev,
 				      union connector_status_t *cs)
 {
 	struct pdc_data_t *data = dev->data;
+
+	if (data->conn_status_cached) {
+		LOG_INF("%s: Copy conn status from cache", __func__);
+		memcpy(cs, &data->conn_status, sizeof(*cs));
+		data->cci_event.data_len = sizeof(*cs);
+		data->cci_event.command_completed = 1;
+		call_cci_event_cb(data);
+		return 0;
+	}
 
 	if (get_state(data) != ST_IDLE) {
 		return -EBUSY;
