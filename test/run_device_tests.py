a39c012f1ec99e5e8a01bfb971ef846b9da5d22c
Dawid Niedzwiecki <dawidn@google.com>
2024-04-15 14:44:02 +0200

    test: run_device_tests.py: return value for pre_test_callback
    fp_sensor_sel function is used as pre_test_callback. It requires
    returning True/False if it goes well or not. Add it to the fp_sensor_sel
    function so the return value can be checked correctly.
    
    BUG=b:324574796
    TEST=connect Bloonchipper via servo_micro and run
    './test/run_device_tests.py --board=bloonchipper --flasher=servo_micro
    --tests fpsensor_hw'
    
    Change-Id: I3224f274c361e8259ad1df19c1f1035eefa5c44b

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -707,10 +707,14 @@ def fp_sensor_sel(
     ]
 
     logging.debug('Running command: "%s"', " ".join(cmd))
-    subprocess.run(cmd, check=False).check_returncode()
+    proc = subprocess.run(cmd, check=False)
 
-    # power cycle after setting sensor type to ensure detection
-    power_cycle(board_config)
+    if proc.returncode == 0:
+        # power cycle after setting sensor type to ensure detection
+        power_cycle(board_config)
+        return True
+
+    return False
 
 
 def hw_write_protect(enable: bool) -> None:
