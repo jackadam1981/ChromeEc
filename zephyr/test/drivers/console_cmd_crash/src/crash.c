29107f46cbc7d6a2abbb534444229bbdfcb5c91f
Rob Barnes <robbarnes@google.com>
2024-04-15 16:12:34 -0600

    panic: Add nesting support to crash console command
    Add support for nesting to `crash` console command.
    Calling the crash console command with multiple crash arguments
    will result in nested crashes in the order specified. This is useful for
    testing complex panic scenarios.
    
    BUG=b:333916658
    TEST=Nested crashes tested on volteer, zork and skyrim.
    TEST=Twister test drivers/drivers.panic_output
    TEST=Twister test drivers/drivers.console_cmd_crash
    
    Change-Id: I65ff48c0a977f3451cbde6c1787bde74d11d7a83

--- zephyr/test/drivers/console_cmd_crash/src/crash.c
+++ zephyr/test/drivers/console_cmd_crash/src/crash.c
@@ -14,12 +14,27 @@
 ZTEST_SUITE(console_cmd_crash, drivers_predicate_post_main, NULL, NULL, NULL,
 	    NULL);
 
-ZTEST_USER(console_cmd_crash, test_wrong_num_args)
+ZTEST_USER(console_cmd_crash, test_no_args)
 {
 	int rv = shell_execute_cmd(get_ec_shell(), "crash");
 
 	zassert_equal(EC_ERROR_PARAM1, rv, "Expected %d, but got %d",
 		      EC_ERROR_PARAM1, rv);
+}
+
+ZTEST_USER(console_cmd_crash, test_too_many_args)
+{
+	int rv;
+
+	/* CONFIG_PLATFORM_EC_CONSOLE_CMD_CRASH_NESTED should be disabled for
+	 * this test.
+	 */
+	zassert_false(IS_ENABLED(CONFIG_PLATFORM_EC_CONSOLE_CMD_CRASH_NESTED));
+
+	rv = shell_execute_cmd(get_ec_shell(), "crash assert asset");
+
+	zassert_equal(EC_ERROR_PARAM_COUNT, rv, "Expected %d, but got %d",
+		      EC_ERROR_PARAM_COUNT, rv);
 }
 
 ZTEST_USER(console_cmd_crash, test_assert)
