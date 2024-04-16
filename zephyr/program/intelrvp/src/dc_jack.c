4b222061cbbc6376b5c067144e05fab01738696a
Jay Patel <jay2.patel@intel.com>
2024-04-16 12:27:09 -0700

    intelrvp: Enable charger for intelrvp
    Enable charger code for intelrvp. Separate APIs provided by pdc_shim
    code for PD variants to use them.
    
    BUG=b:333693462
    TEST=mtlrvpp_pd booting and charger working.
         Checked using chgsup.
    
    Change-Id: I359e7bd6af131ae91dbdb5e3fb1d8956d0823318

--- zephyr/program/intelrvp/src/dc_jack.c
+++ zephyr/program/intelrvp/src/dc_jack.c
@@ -1,9 +1,9 @@
-/* Copyright 2022 The ChromiumOS Authors
+/* Copyright 2024 The ChromiumOS Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
-/* Common USB PD charge configuration */
+/* DC Jack configuration */
 
 #include "charge_manager.h"
 #include "charge_state.h"
@@ -11,9 +11,6 @@
 #include "hooks.h"
 #include "intelrvp.h"
 #include "tcpm/tcpci.h"
-
-#define CPRINTF(format, args...) cprintf(CC_USBPD, format, ##args)
-#define CPRINTS(format, args...) cprints(CC_USBPD, format, ##args)
 
 bool is_typec_port(int port)
 {
@@ -24,7 +21,7 @@ bool is_typec_port(int port)
 #endif /* CONFIG_DEDICATED_CHARGE_PORT_COUNT > 0 */
 }
 
-static inline int board_dc_jack_present(void)
+int board_dc_jack_present(void)
 {
 #if CONFIG_DEDICATED_CHARGE_PORT_COUNT > 0
 	return gpio_get_level(GPIO_DC_JACK_PRESENT);
@@ -78,45 +75,3 @@ static void board_charge_init(void)
 	board_dc_jack_handle();
 }
 DECLARE_HOOK(HOOK_INIT, board_charge_init, HOOK_PRIO_DEFAULT);
-
-int board_set_active_charge_port(int port)
-{
-	int i;
-	/* charge port is a realy physical port */
-	int is_real_port = (port >= 0 && port < CHARGE_PORT_COUNT);
-	/* check if we are source vbus on that port */
-	int source = board_vbus_source_enabled(port);
-
-	if (is_real_port && source) {
-		CPRINTS("Skip enable p%d", port);
-		return EC_ERROR_INVAL;
-	}
-
-#if CONFIG_DEDICATED_CHARGE_PORT_COUNT > 0
-	/*
-	 * Do not enable Type-C port if the DC Jack is present.
-	 * When the Type-C is active port, hardware circuit will
-	 * block DC jack from enabling +VADP_OUT.
-	 */
-	if (port != DEDICATED_CHARGE_PORT && board_dc_jack_present()) {
-		CPRINTS("DC Jack present, Skip enable p%d", port);
-		return EC_ERROR_INVAL;
-	}
-#endif /* CONFIG_DEDICATED_CHARGE_PORT_COUNT */
-
-	/* Make sure non-charging ports are disabled */
-	for (i = 0; i < CONFIG_USB_PD_PORT_MAX_COUNT; i++) {
-		if (i != port) {
-			board_charging_enable(i, 0);
-		}
-	}
-
-	/* Enable charging port */
-	if (is_typec_port(port)) {
-		board_charging_enable(port, 1);
-	}
-
-	CPRINTS("New chg p%d", port);
-
-	return EC_SUCCESS;
-}
