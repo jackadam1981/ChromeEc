566fb9b2891c051b8b1daa71d7c255d98da13096
David <david1.huang@lcfc.corp-partner.google.com>
2024-04-16 10:53:57 +0800

    nissa: Initial zephyr config for pujjoga
    Initial EC Zephyr config for Nissa/Pujjoga duplicated from Nokris
    
    BUG=b:335312714 b:333664294
    TEST=zmkae build pujjoga succes and /twister -T zephyr/test/nissa pass
    
    Change-Id: I6eb8b6491c6f7ebb40c7901a5e9d71c2de2e4ffb

---
+++ zephyr/program/nissa/pujjoga/src/hdmi.c
@@ -0,0 +1,13 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "nissa_hdmi.h"
+
+#include <cros_board_info.h>
+
+__override void nissa_configure_hdmi_power_gpios(void)
+{
+	nissa_configure_hdmi_vcc();
+}
