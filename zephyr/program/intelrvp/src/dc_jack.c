cd6b65d4d46f44c73bef5baa87d02de9a6810611
Jay Patel <jay2.patel@intel.com>
2024-04-16 12:27:09 -0700

    intelrvp: Enable charger for intelrvp
    Enable charger code for intelrvp. Separate APIs provided by pdc_shim
    code for PD variants to use them.
    
    BUG=b:333693462
    TEST=mtlrvpp_pd booting and charger working.
         Checked using chgsup.
    LOW_COVERAGE_REASON=Board specific code b:336751447
    
    Change-Id: I359e7bd6af131ae91dbdb5e3fb1d8956d0823318

---
+++ zephyr/program/intelrvp/src/dc_jack.c
@@ -0,0 +1,67 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* DC Jack configuration */
+
+#include "gpio.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "intelrvp.h"
+#include "tcpm/tcpci.h"
+
+bool is_typec_port(int port)
+{
+	return !(port == DEDICATED_CHARGE_PORT || port == CHARGE_PORT_NONE);
+}
+
+int board_dc_jack_present(void)
+{
+	return gpio_get_level(GPIO_DC_JACK_PRESENT);
+}
+
+static void board_dc_jack_handle(void)
+{
+	struct charge_port_info charge_dc_jack;
+
+	/* System is booted from DC Jack */
+	if (board_dc_jack_present()) {
+		charge_dc_jack.current =
+			(PD_MAX_POWER_MW * 1000) / DC_JACK_MAX_VOLTAGE_MV;
+		charge_dc_jack.voltage = DC_JACK_MAX_VOLTAGE_MV;
+	} else {
+		charge_dc_jack.current = 0;
+		charge_dc_jack.voltage = USB_CHARGER_VOLTAGE_MV;
+	}
+
+	charge_manager_update_charge(CHARGE_SUPPLIER_DEDICATED,
+				     DEDICATED_CHARGE_PORT, &charge_dc_jack);
+}
+
+void board_dc_jack_interrupt(enum gpio_signal signal)
+{
+	board_dc_jack_handle();
+}
+
+static void board_charge_init(void)
+{
+	int port, supplier;
+	struct charge_port_info charge_init = {
+		.current = 0,
+		.voltage = USB_CHARGER_VOLTAGE_MV,
+	};
+
+	/* Initialize all charge suppliers to seed the charge manager */
+	for (port = 0; port < CHARGE_PORT_COUNT; port++) {
+		for (supplier = 0; supplier < CHARGE_SUPPLIER_COUNT;
+		     supplier++) {
+			charge_manager_update_charge(supplier, port,
+						     &charge_init);
+		}
+	}
+
+	board_dc_jack_handle();
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_dc_jack_present));
+}
+DECLARE_HOOK(HOOK_INIT, board_charge_init, HOOK_PRIO_DEFAULT);
