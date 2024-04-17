7eff66f1830a8a854b4088bc492308cbac621c82
lin.arthur <lin.arthur@inventec.corp-partner.google.com>
2024-04-17 10:06:05 +0800

    [TEST] brox: test for greenbayupoc bring up
    For greenbayupoc bring up test
    
    Change-Id: I3893c34f091b895c6c03acf548a787f884996b29

--- common/fan.c
+++ common/fan.c
@@ -392,7 +392,7 @@ int dptf_get_fan_duty_target(void)
 void dptf_set_fan_duty_target(int pct)
 {
 	int fan;
-
+	return;//
 	if (pct < 0 || pct > 100) {
 		/* TODO(crosbug.com/p/23803) */
 		for (fan = 0; fan < fan_count; fan++)
@@ -657,11 +657,14 @@ static void pwm_fan_start(void)
 	 * Upon booting to S0, if needed AP will disable/throttle it using
 	 * host commands.
 	 */
-	if (chipset_in_or_transitioning_to_state(CHIPSET_STATE_ON))
+	if (chipset_in_or_transitioning_to_state(CHIPSET_STATE_ON)) {
 		pwm_fan_control(1);
+		set_thermal_control_enabled(0, 1);
+	}
 }
 /* On Fizz, CHIPSET_RESUME isn't triggered when AP warm resets.
  * So we hook CHIPSET_RESET instead.
  */
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, pwm_fan_start, HOOK_PRIO_DEFAULT);
 DECLARE_HOOK(HOOK_CHIPSET_RESET, pwm_fan_start, HOOK_PRIO_FIRST);
 DECLARE_HOOK(HOOK_CHIPSET_RESUME, pwm_fan_start, HOOK_PRIO_DEFAULT);
