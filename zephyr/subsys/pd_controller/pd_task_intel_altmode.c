956e9b4726efd9174d46305e15390e0b52a70c3f
lin.arthur <lin.arthur@inventec.corp-partner.google.com>
2024-04-17 10:06:05 +0800

    [TEST] brox: test for greenbayupoc bring up
    For greenbayupoc bring up test
    
    Change-Id: I3893c34f091b895c6c03acf548a787f884996b29

--- zephyr/subsys/pd_controller/pd_task_intel_altmode.c
+++ zephyr/subsys/pd_controller/pd_task_intel_altmode.c
@@ -151,7 +151,7 @@ static bool process_altmode_pd_data(int port)
 /* TODO(b/325624686): The polling mechanism is temporary and will be removed
  * after the bug is resolved */
 #if !CONFIG_USBPD_POLL_PDC
-	LOG_INF("Process p%d data", port);
+//	LOG_INF("Process p%d data", port);
 #endif
 	/* Clear the interrupt */
 	rv = pd_altmode_write_control(pd_config_array[port], &control);
