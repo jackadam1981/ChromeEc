95f45cbe11a5ee1de3214f6c5d043e6d88d35b8f
Abe Levkoy <alevkoy@chromium.org>
2024-04-17 16:37:21 -1000

    pdc_power_mgmt: Add Type-C sink-only policy
    When attached as a Type-C Source, apply a policy of forcing Sink-only
    upon shutdown.
    
    BUG=b:327665833
    TEST=Shutdown with Type-C sink; observe disconnect
    LOW_COVERAGE_REASON=Test coveraged tracked in b/317065172
    
    Change-Id: I6d957bedb363b154fdd8594a2ac587afdfd760c0

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -1036,6 +1036,17 @@ static void run_src_policies(struct pdc_port_t *port)
 	send_pending_public_commands(port);
 }
 
+static void run_typec_src_policies(struct pdc_port_t *port)
+{
+	if (atomic_test_and_clear_bit(port->src_policy.flags,
+				      SRC_POLICY_FORCE_SNK)) {
+		queue_internal_cmd(port, CMD_PDC_SET_CCOM);
+		return;
+	}
+
+	send_pending_public_commands(port);
+}
+
 /**
  * @brief Entering unattached state
  */
@@ -1688,7 +1699,7 @@ static void pdc_src_typec_only_run(void *obj)
 		queue_internal_cmd(port, CMD_PDC_SET_SINK_PATH);
 		return;
 	case SRC_TYPEC_ATTACHED_RUN:
-		send_pending_public_commands(port);
+		run_typec_src_policies(port);
 		break;
 	}
 }
