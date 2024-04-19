9239316e19a56b2173a328aa7608986f05cb81d8
Dawid Niedzwiecki <dawidn@google.com>
2024-04-19 08:03:30 +0200

    zephyr: fpc1025: disable SPI after transaction
    Add running PM actions before and after SPI transaction. It includes
    enabling/disabling clock gating for SPI and reconfiguring SPI pins.
    
    Setting the pin as output low in sleep state limits unnecessary power
    consumption.
    
    BUG=b:324574796
    TEST=Run "dut-control -t 1 ppvar_fp_mw ppvar_mcu_mw" with connected
    Bloonchipper and FPC sensor (make sure fp_sensor_sel:on) and verify
    ppvar_fp_mw = ~0,16[mW] and ppvar_mcu_mw = 14,6[mW].
    
    Change-Id: I79cca07465808ad1f731faeacbd3df2db79784d3

--- zephyr/drivers/fingerprint/fpc1025.c
+++ zephyr/drivers/fingerprint/fpc1025.c
@@ -11,6 +11,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/spi.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/pm/device.h>
 #include <zephyr/sys/byteorder.h>
 
 #include <drivers/fingerprint.h>
@@ -35,6 +36,7 @@ enum fpc1025_cmd {
 
 void fp_sensor_lock(const struct device *dev)
 {
+	const struct fpc1025_cfg *cfg = dev->config;
 	struct fpc1025_data *data = dev->data;
 
 	/* Lock SPI access only if we are not already the owner. */
@@ -42,12 +44,27 @@ void fp_sensor_lock(const struct device *dev)
 	      (data->sensor_owner == k_current_get()))) {
 		k_sem_take(&data->sensor_lock, K_FOREVER);
 		data->sensor_owner = k_current_get();
+
+		/* Enable clock gating for SPI module and configure SPI pins
+		 * into an alternate mode.
+		 */
+		pm_device_action_run(cfg->spi.bus, PM_DEVICE_ACTION_RESUME);
 	}
 }
 
 void fp_sensor_unlock(const struct device *dev)
 {
+	const struct fpc1025_cfg *cfg = dev->config;
 	struct fpc1025_data *data = dev->data;
+
+	/* Disable SPI mainly to reconfigure SPI pins to sleep state
+	 * (CLK, MISO, MOSI set to output low) to reduce power
+	 * consumption by the sensor.
+	 *
+	 * The SPI drivers disable the SPI module after a transaction,
+	 * which puts the pins into floating state.
+	 */
+	pm_device_action_run(cfg->spi.bus, PM_DEVICE_ACTION_SUSPEND);
 
 	/* Clear the owner and return the access. */
 	data->sensor_owner = NULL;
