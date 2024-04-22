4054d792f9cd06a66204238a57ffb1e12692fcf2
Dawid Niedzwiecki <dawidn@google.com>
2024-04-22 14:29:17 +0200

    test: run_device_tests: prepare a board before a power test
    Add a function to prepare a board before power_utilization test.
    In addition to setting a sleep state, it also selects a proper
    fingerprint sensor.
    
    An incorrect fingerprint sensor influences power consumption,
    because we are not able to send a sleep command.
    
    BUG=b:324574796
    TEST=connect Bloonchipper via servo_micro and run
    './test/run_device_tests.py --board=bloonchipper --flasher=servo_micro
    --tests power_utilization_idle power_utilization_sleep'
    
    Change-Id: Ibba5f3686c07536d7ef6609e072980046b322eff

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -413,7 +413,7 @@ class AllTests:
                 test_name="power_utilization",
                 apptype_to_use=ApplicationType.PRODUCTION,
                 toggle_power=True,
-                pre_test_callback=lambda config=None: set_sleep_mode(False),
+                pre_test_callback=lambda config: power_util_pre(config, False),
                 post_test_callback=verify_idle_power_utilization,
                 finish_regexes=[RW_IMAGE_BOOTED_REGEX],
             ),
@@ -422,7 +422,7 @@ class AllTests:
                 test_name="power_utilization",
                 apptype_to_use=ApplicationType.PRODUCTION,
                 toggle_power=True,
-                pre_test_callback=lambda config=None: set_sleep_mode(True),
+                pre_test_callback=lambda config: power_util_pre(config, True),
                 post_test_callback=verify_sleep_power_utilization,
                 finish_regexes=[RW_IMAGE_BOOTED_REGEX],
             ),
@@ -715,6 +715,17 @@ def fp_sensor_sel(
         return True
 
     return False
+
+
+def power_util_pre(board_config: BoardConfig, enter_sleep: bool) -> bool:
+    """
+    Prepare a board for a power_utilization test
+    """
+
+    if not set_sleep_mode(enter_sleep):
+        return False
+
+    return fp_sensor_sel(board_config)
 
 
 def hw_write_protect(enable: bool) -> None:
