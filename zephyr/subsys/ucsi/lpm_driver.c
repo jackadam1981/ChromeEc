a6c3ecb531ef17ec1cd61590330e37e489e53fae
Daisuke Nojiri <dnojiri@chromium.org>
2024-04-23 11:44:01 -0700

    PPM: Disable PPM if CBI flag is not set
    BUG=b:319124515
    TEST=None
    
    Change-Id: I5f327a4c4bb497d234680b2d57b24fc5b05546a6

--- zephyr/subsys/ucsi/lpm_driver.c
+++ zephyr/subsys/ucsi/lpm_driver.c
@@ -5,6 +5,8 @@
 
 /* UCSI LPM Driver */
 
+#include "cros_board_info.h"
+#include "ec_commands.h"
 #include "include/pd_driver.h"
 #include "include/ppm.h"
 #include "ppm_common.h"
@@ -178,6 +180,12 @@ static int lpm_init(const struct device *device)
 	const struct ucsi_pd_driver *drv = device->api;
 	struct ppm_common_device *ppm_dev;
 	struct lpm_device *dev = device->data;
+#if 0
+	union ec_common_control ctrl;
+
+	if (cbi_get_common_control(&ctrl) || !ctrl.ucsi_enabled)
+		return -ENODEV;
+#endif
 
 	/* Initialize the PPM. */
 	dev->ppm = ppm_open(drv);
