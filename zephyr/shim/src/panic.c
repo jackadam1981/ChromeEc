1aa3f86d63072683a49eb07e809cf05a59fd4dad
Rob Barnes <robbarnes@google.com>
2024-04-23 16:40:40 -0600

    zephyr/panic: Don't start safe mode if panic in handler context
    Do not attempt to start safe mode if panic occurs in handler context.
    Recovering from a panic in a handler is not supported.
    
    BUG=b:333916658
    TEST=Forcing a nested crash on skyrim does not start safe mode
    
    Change-Id: I53feec0d45702cf94dbdb6b63da223e79a3e34da

--- zephyr/shim/src/panic.c
+++ zephyr/shim/src/panic.c
@@ -14,6 +14,19 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/logging/log_ctrl.h>
 
+/* Arch-specific import for arch_is_in_nested_exception */
+#if defined(CONFIG_CPU_CORTEX_M)
+#include <cortex_m/exception.h>
+#elif defined(CONFIG_CPU_AARCH32_CORTEX_R) || \
+	defined(CONFIG_CPU_AARCH32_CORTEX_A)
+#include <cortex_a_r/exception.h>
+#else
+static bool arch_is_in_nested_exception(const z_arch_esf_t *esf)
+{
+	return false;
+}
+#endif
+
 /*
  * Arch-specific configuration
  *
@@ -141,6 +154,18 @@ static void copy_esf_to_panic_data(const z_arch_esf_t *esf,
 	PANIC_REG_LIST(PANIC_COPY_REGS, PANIC_COPY_REGS_GPR);
 }
 
+/*
+ * Returns true if the exception described by the esf
+ * is a nested exception, i.e. the exception occurred
+ * while in the interrupt context. Returns false if
+ * not able to be determined.
+ */
+static inline bool nested_exception(const z_arch_esf_t *esf)
+{
+	return (IS_ENABLED(CONFIG_ARCH_HAS_NESTED_EXCEPTION_DETECTION) &&
+		(esf != NULL) && arch_is_in_nested_exception(esf));
+}
+
 void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *esf)
 {
 	struct panic_data *pdata = get_panic_data_write();
@@ -173,7 +198,7 @@ void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *esf)
 
 	/* Start system safe mode if possible */
 	if (IS_ENABLED(CONFIG_PLATFORM_EC_SYSTEM_SAFE_MODE)) {
-		if (reason != K_ERR_KERNEL_PANIC &&
+		if (reason != K_ERR_KERNEL_PANIC && !nested_exception(esf) &&
 		    start_system_safe_mode() == EC_SUCCESS) {
 			/* Returning from k_sys_fatal_error_handler will cause
 			 * the faulting thread to be aborted and resume the
