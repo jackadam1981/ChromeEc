2fdface5299c970ff9b77b46042965b43a2b3316
Jeremy Bettis <jbettis@google.com>
2024-04-23 16:53:07 -0600

    Test change
    Just a trivial change to see if cros lint runs.
    
    BUG=None
    TEST=None
    
    Change-Id: I317f56640360fa5474ae4032dde442cad921d379

--- zephyr/scripts/led_policy.py
+++ zephyr/scripts/led_policy.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Configure-time checks for the led-policy node."""
+"""Configure-time checks for the led-policy node. Yay"""
 
 import logging
 import sys
