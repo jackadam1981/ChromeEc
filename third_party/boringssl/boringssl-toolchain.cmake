a9c7e69771600b4696e284c656203fb3a7dcda58
Tom Hughes <tomhughes@chromium.org>
2024-04-25 14:57:15 -0700

    third_party/boringssl: Build release with asserts
    We want to assert (reboot FPMCU) if there is an error in crypto
    operations.
    
    Tested by adding assert in boringssl:
    
      --- a/crypto/fipsmodule/cipher/aead.c
      +++ b/crypto/fipsmodule/cipher/aead.c
      @@ -163,6 +163,8 @@ int EVP_AEAD_CTX_seal_scatter(
         goto error;
       }
    
      +  assert(0);
      +
       if (!ctx->aead->seal_scatter_supports_extra_in && extra_in_len) {
         OPENSSL_PUT_ERROR(CIPHER, CIPHER_R_INVALID_OPERATION);
         goto error;
    
    BUG=b:273639386
    TEST=Apply above boringssl change:
         make run-aes
         => crash
    TEST=Apply above boringssl change:
         ./test/run_device_tests.py --board helipilot -t aes
         => crash
    
    Change-Id: I9dcfd6882843dffddf37d42de342b0f9223d9aa4

--- third_party/boringssl/boringssl-toolchain.cmake
+++ third_party/boringssl/boringssl-toolchain.cmake
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-set(CMAKE_BUILD_TYPE Release)
+set(CMAKE_BUILD_TYPE RelWithAsserts)
 
 set(CMAKE_TRY_COMPILE_PLATFORM_VARIABLES CROS_EC_REPO CROSS_COMPILE CC_NAME CXX_NAME)
 include("${CROS_EC_REPO}/cmake/toolchain-common.cmake")
