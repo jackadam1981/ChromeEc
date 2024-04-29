441ea1ded4364e06b7565812d3dabc6a4c677aaf
Huanhuan Liu <huanhuanl@google.com>
2024-04-29 14:41:12 +0800

    [Test Only] brox: support get vbus voltage
    Factory test flows require get USB PD power information.
    
    BUG=b:336212074
    
    TEST=Run "ectool usbpdpower" on DUT
    TEST=Factory Test
    
    Change-Id: I8133b34787afb7d401351cb2960bc5c9ec9723ce

--- zephyr/program/brox/src/usbc.c
+++ zephyr/program/brox/src/usbc.c
@@ -8,6 +8,7 @@
 #include "ec_commands.h"
 #include "host_command.h"
 #include "usb_pd.h"
+#include "usbc/pdc_power_mgmt.h"
 
 enum ec_pd_port_location board_get_pd_port_location(int port)
 {
@@ -18,4 +19,10 @@ enum ec_pd_port_location board_get_pd_port_location(int port)
 		return EC_PD_PORT_LOCATION_LEFT_FRONT;
 	}
 	return EC_PD_PORT_LOCATION_UNKNOWN;
+}
+
+__override int board_get_vbus_voltage(int port)
+{
+	return pdc_power_mgmt_get_vbus_voltage(port);
+	;
 }
