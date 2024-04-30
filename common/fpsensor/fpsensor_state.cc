7a8d5c0c418105f182f67fa2ad8e63c6d927ca83
Tom Hughes <tomhughes@chromium.org>
2024-04-30 14:59:46 -0700

    Convert fp_enc_buffer to a C++ std::array
    BUG=none
    TEST=none
    
    Change-Id: Ic25782966c62c1185c373ad31493bde872a41bd2

--- common/fpsensor/fpsensor_state.cc
+++ common/fpsensor/fpsensor_state.cc
@@ -88,7 +88,7 @@ void fp_reset_context()
 	global_context.state.templ_dirty = 0;
 	global_context.state.template_newly_enrolled = FP_NO_SUCH_TEMPLATE;
 	global_context.state.fp_encryption_status &= FP_ENC_STATUS_SEED_SET;
-	OPENSSL_cleanse(fp_enc_buffer, sizeof(fp_enc_buffer));
+	OPENSSL_cleanse(fp_enc_buffer.data(), fp_enc_buffer.size());
 	OPENSSL_cleanse(global_context.state.user_id,
 			sizeof(global_context.state.user_id));
 	OPENSSL_cleanse(auth_nonce.data(), auth_nonce.size());
