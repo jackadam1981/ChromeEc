5d802982945f31b1bdcf17edf9aa396104e07432
Daisuke Nojiri <dnojiri@chromium.org>
2024-04-30 12:52:32 -0700

    RTS54XX: Handle raw UCSI commands
    With this CL, rts54_execute_ucsi_cmd will post a command as
    CMD_RAW_UCSI. The driver skips command completion processes for
    CMD_RAW_UCSI, which aren't needed for the PPM.
    
    GET_CONNECTOR_STATUS needs to be handled differently because the out
    data needs to be saved in a cache.
    
    ACK_CC_CI is also customized for RTS54xx.
    
    BUG=b:319124515
    TEST=./twister -civ -T zephyr/test/pdc
    LOW_COVERAGE_REASON=b:343188354
    
    Change-Id: I368f952b21642c580841e3dbe76925d163fe19cd

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -183,6 +183,7 @@ const struct smbus_cmd_t GET_IC_STATUS = { 0x3A, 0x03 };
 const struct smbus_cmd_t SET_RETIMER_FW_UPDATE_MODE = { 0x20, 0x03, 0x00 };
 const struct smbus_cmd_t GET_CABLE_PROPERTY = { 0x0E, 0x03, 0x11 };
 const struct smbus_cmd_t GET_PCH_DATA_STATUS = { 0x08, 0x02, 0xE0 };
+const struct smbus_cmd_t ACK_CC_CI = { 0x0A, 0x07, 0x00 };
 
 /**
  * @brief PDC Command states
@@ -319,6 +320,8 @@ enum cmd_t {
 	CMD_SET_PDO,
 	/** Get PDC ALT MODE Status Register value */
 	CMD_GET_PCH_DATA_STATUS,
+	/* Raw UCSI call */
+	CMD_RAW_UCSI,
 };
 
 /**
@@ -442,6 +445,7 @@ static const char *const cmd_names[] = {
 	[CMD_GET_IS_VCONN_SOURCING] = "CMD_GET_IS_VCONN_SOURCING",
 	[CMD_SET_PDO] = "CMD_SET_PDO",
 	[CMD_GET_PCH_DATA_STATUS] = "CMD_GET_PCH_DATA_STATUS",
+	[CMD_RAW_UCSI] = "CMD_RAW_UCSI",
 };
 
 /**
@@ -2403,6 +2407,7 @@ static int rts54_execute_ucsi_cmd(const struct device *dev,
 {
 	struct pdc_data_t *data = dev->data;
 	uint8_t cmd_buffer[SMBUS_MAX_BLOCK_SIZE];
+	enum cmd_t use_cmd = CMD_RAW_UCSI;
 
 	if (ucsi_command == UCSI_CMD_GET_CONNECTOR_STATUS &&
 	    data->conn_status_cached) {
@@ -2429,7 +2434,52 @@ static int rts54_execute_ucsi_cmd(const struct device *dev,
 	cmd_buffer[3] = 0;
 	memcpy(&cmd_buffer[4], command_specific, data_size);
 
-	return rts54_post_command_with_callback(dev, ucsi_command, cmd_buffer,
+	/* Convert standard UCSI command to Realtek vendor specific formats. */
+	switch (ucsi_command) {
+	case UCSI_CMD_ACK_CC_CI: {
+		struct ucsiv3_ack_cc_ci_cmd *cmd =
+			(struct ucsiv3_ack_cc_ci_cmd *)command_specific;
+
+		data_size = 5;
+		memset(cmd_buffer, 0, ACK_CC_CI.len + 2);
+		cmd_buffer[0] = ACK_CC_CI.cmd;
+		cmd_buffer[1] = ACK_CC_CI.len;
+
+		if (cmd->connector_change_ack) {
+			/*
+			 * There could be another CI event before the OPM/PPM
+			 * acknowledges the one corresponding to this ACK_CC_CI.
+			 *
+			 * According to the spec, "The LPM/PPM shall wait until
+			 * the OPM Acknowledges the notification (due to an
+			 * asynchronous event) before sending the next
+			 * notification." So, we expect a PDC to apply the clear
+			 * bits corresponding to the last CI. If there is
+			 * another CI, a PDC is supposed to either ignore it or
+			 * store it in a cache for the next notification.
+			 *
+			 * So, we expect clearing the bits here should not
+			 * affect another pending CI.
+			 */
+			cmd_buffer[4] = 0xff;
+			cmd_buffer[5] = 0xff;
+			cmd_buffer[6] = 0xff;
+			cmd_buffer[7] = 0xff;
+		}
+
+		if (cmd->command_complete_ack) {
+			cmd_buffer[8] = 0x1;
+		}
+		break;
+	}
+	case UCSI_CMD_GET_CONNECTOR_STATUS:
+		use_cmd = CMD_GET_CONNECTOR_STATUS;
+		break;
+	default:
+		break;
+	}
+
+	return rts54_post_command_with_callback(dev, use_cmd, cmd_buffer,
 						data_size + 4, lpm_data_out,
 						callback);
 }
