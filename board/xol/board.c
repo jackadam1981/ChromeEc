24a06d7ee88f8ce352eff4dc6cd50c6f1a76d748
YeonGwon Sung <y.sung@samsung.corp-partner.google.com>
2024-04-30 18:33:52 +0900

    Xol: Modify function to write input current limit
    Modify function to write input current limit.
    It is needed for AC only mode.
    
    BUG=b:336715410
    TEST=Check input current limit, Pass
    
    Change-Id: I8dd7ecfe8444df5c232c5854a75a8c34f625541c

--- board/xol/board.c
+++ board/xol/board.c
@@ -3,6 +3,7 @@
  * found in the LICENSE file.
  */
 
+#include "charger.h"
 #include "common.h"
 #include "cros_board_info.h"
 #include "driver/mp2964.h"
@@ -117,4 +118,13 @@ uint8_t board_get_finch_version(void)
 			board_id);
 		return 0;
 	}
+}
+
+__override void board_set_charge_limit(int port, int supplier, int charge_ma,
+				       int max_ma, int charge_mv)
+{
+	charger_set_input_current_limit(
+		CHARGER_SOLO,
+		charge_ma * (100 - CONFIG_CHARGER_INPUT_CURRENT_DERATE_PCT) /
+			100);
 }
