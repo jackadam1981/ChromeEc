84f60b134df10082baea5e4a5798a3ec580344da
Tom Hughes <tomhughes@chromium.org>
2024-04-30 16:51:12 -0700

    fpsensor: Move validate_fp_buffer_offset tests into fpsensor_utils test
    BUG=none
    TEST=none
    
    Change-Id: Id6b712942796c901e8c446f04e87aee2d7fb5d26

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -329,6 +329,7 @@ class AllTests:
                 test_name="fpsensor",
                 test_args=["uart"],
             ),
+            TestConfig(test_name="fpsensor_utils"),
             TestConfig(test_name="ftrapv"),
             TestConfig(
                 test_name="libc_printf",
