46c4e4233043a6da2f5dd252ac1d1b4c2a0eafd6
Rob Barnes <robbarnes@google.com>
2024-04-30 18:30:16 -0600

    host_cmd: Add console_print host command
    Add the console_print host command. It simply prints a message from the
    host to the console. This is helpful for debugging and testing.
    
    BUG=b:338135869
    TEST=Manual testing on skyrim and zork
    TEST=twister -s drivers/drivers.host_cmd.ec_host_cmd
    
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5504270
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    (cherry picked from https://chromium-review.googlesource.com/q/commit:655c2c33c22933d7a492282882ec3781893d3521)
    Merged-In: I4f860d45953e7b36e705cef40f1c5bda0666145d
    Change-Id: I4f860d45953e7b36e705cef40f1c5bda0666145d

--- include/config.h
+++ include/config.h
@@ -815,6 +815,43 @@
 /* Enable verbose output to UART console and extra timestamp print precision. */
 #define CONFIG_CONSOLE_VERBOSE
 
+<<<<<<< HEAD   (d097611506bb7d854eb0d951a143b2438638d7be keyboard_8042: Clear buffer before adding command byte)
+||||||| BASE   (5b42e4670d58fc19e82da4e955ef06fbc67bfd66 tcpmv2: Don't call tcpm_debug_detach during PRS)
+/*****************************************************************************/
+/* Support for EC-EC communication */
+
+/*
+ * Board is client or server in EC-EC communication.
+ */
+#undef CONFIG_EC_EC_COMM_CLIENT
+#undef CONFIG_EC_EC_COMM_SERVER
+
+/*
+ * Board support battery-related functions in EC-EC communication.
+ */
+#undef CONFIG_EC_EC_COMM_BATTERY
+
+=======
+/* Enable the console print command. This allows the host to print messages
+ * directly in the EC console.
+ */
+#define CONFIG_HOSTCMD_CONSOLE_PRINT
+
+/*****************************************************************************/
+/* Support for EC-EC communication */
+
+/*
+ * Board is client or server in EC-EC communication.
+ */
+#undef CONFIG_EC_EC_COMM_CLIENT
+#undef CONFIG_EC_EC_COMM_SERVER
+
+/*
+ * Board support battery-related functions in EC-EC communication.
+ */
+#undef CONFIG_EC_EC_COMM_BATTERY
+
+>>>>>>> CHANGE (655c2c33c22933d7a492282882ec3781893d3521 host_cmd: Add console_print host command)
 /*
  * Enable the experimental console.
  *
