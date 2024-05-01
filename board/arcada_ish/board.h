947f0a8bf6eec103c20b7d3c70ed95da61e4b8b5
Rob Barnes <robbarnes@google.com>
2024-04-30 18:30:16 -0600

    host_cmd: Add console_print host command
    Add the console_print host command. It simply prints a message from the
    host to the console. This is helpful for debugging and testing.
    
    BUG=b:338135869
    TEST=Manual testing on skyrim and zork
    TEST=twister -s drivers/drivers.host_cmd.ec_host_cmd
    
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5504270
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    (cherry picked from https://chromium-review.googlesource.com/q/commit:655c2c33c22933d7a492282882ec3781893d3521)
    Merged-In: I4f860d45953e7b36e705cef40f1c5bda0666145d
    Change-Id: I4f860d45953e7b36e705cef40f1c5bda0666145d

--- board/arcada_ish/board.h
+++ board/arcada_ish/board.h
@@ -85,6 +85,13 @@
 #undef CONFIG_FLASH
 #undef CONFIG_FMAP
 #undef CONFIG_SWITCH
+<<<<<<< HEAD   (6506296ac4b6cc11ebd2a89faff903438ea5fde6 sn5s330: prevent infinite loop)
+||||||| BASE   (5b42e4670d58fc19e82da4e955ef06fbc67bfd66 tcpmv2: Don't call tcpm_debug_detach during PRS)
+#undef CONFIG_CONSOLE_VERBOSE
+=======
+#undef CONFIG_CONSOLE_VERBOSE
+#undef CONFIG_HOSTCMD_CONSOLE_PRINT
+>>>>>>> CHANGE (655c2c33c22933d7a492282882ec3781893d3521 host_cmd: Add console_print host command)
 
 /* Modules we want to exclude */
 #undef CONFIG_CMD_HASH
