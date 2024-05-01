069dd6d0d2180cd4224d0f7cdff3b2f3c4bd7b21
Rob Barnes <robbarnes@google.com>
2024-04-30 18:30:16 -0600

    host_cmd: Add console_print host command
    Add the console_print host command. It simply prints a message from the
    host to the console. This is helpful for debugging and testing.
    
    BUG=b:338135869
    TEST=Manual testing on skyrim and zork
    TEST=twister -s drivers/drivers.host_cmd.ec_host_cmd
    
    Change-Id: I4f860d45953e7b36e705cef40f1c5bda0666145d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5504270
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6892639
    Reviewed-by: Jon Murphy <jpmurphy@google.com>
    Commit-Queue: Jon Murphy <jpmurphy@google.com>
    Owners-Override: Rob Barnes <robbarnes@google.com>

--- include/config.h
+++ include/config.h
@@ -1604,6 +1604,11 @@
  */
 #undef CONFIG_EC_EC_COMM_BATTERY
 
+/* Enable the console print command. This allows the host to print messages
+ * directly in the EC console.
+ */
+#define CONFIG_HOSTCMD_CONSOLE_PRINT
+
 /*
  * Enable the experimental console.
  *
