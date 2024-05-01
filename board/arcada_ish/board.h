79265b36259455742ef7a63da9549a60c859c6d7
Rob Barnes <robbarnes@google.com>
2024-04-30 18:30:16 -0600

    host_cmd: Add console_print host command
    Add the console_print host command. It simply prints a message from the
    host to the console. This is helpful for debugging and testing.
    
    BUG=b:338135869
    TEST=Manual testing on skyrim and zork
    TEST=twister -s drivers/drivers.host_cmd.ec_host_cmd
    
    Change-Id: I4f860d45953e7b36e705cef40f1c5bda0666145d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5504270
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7122923
    Commit-Queue: Jon Murphy <jpmurphy@google.com>
    Auto-Submit: Rob Barnes <robbarnes@google.com>
    Owners-Override: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Jon Murphy <jpmurphy@google.com>

--- board/arcada_ish/board.h
+++ board/arcada_ish/board.h
@@ -83,6 +83,8 @@
 #undef CONFIG_FLASH_CROS
 #undef CONFIG_FMAP
 #undef CONFIG_SWITCH
+#undef CONFIG_CONSOLE_VERBOSE
+#undef CONFIG_HOSTCMD_CONSOLE_PRINT
 
 /* Modules we want to exclude */
 #undef CONFIG_CMD_HASH
