60c4fb2f8173479e9c0286f8d5e595709c649085
Jeremy Bettis <jbettis@google.com>
2024-05-01 15:03:01 -0600

    Check the cros_ec_commands.h file always
    Instead of checking the timestamps on cros_ec_commands.h and then
    telling the user to do work, just always regenerate and checkpatch the
    file.
    
    BUG=b:336325935
    TEST=./util/linux_ec_commands_h_check.sh
    
    Change-Id: I54e10ac50bd21efe0b209b951a4ff40669d1a485

--- util/linux_ec_commands_h_check.sh
+++ util/linux_ec_commands_h_check.sh
@@ -7,25 +7,22 @@
 set -e
 
 : "${ZEPHYR_BASE:=$(realpath ../../../src/third_party/zephyr/main)}"
+TMP="$(mktemp -d)"
+ec_commands_file_out="${TMP}/cros_ec_commands.h"
 
-ec_commands_file_in="include/ec_commands.h"
-ec_commands_file_out="build/kernel/include/linux/mfd/cros_ec_commands.h"
+cleanup() {
+  rm -rf "${TMP:?}"
+}
 
-# Check if ec_commands.h has changed.
-echo "${PRESUBMIT_FILES:?}" | xargs -d'\n' -- grep -q \
-  "${ec_commands_file_in}" || exit 0
+trap cleanup EXIT
 
-if [ ! -f "${ec_commands_file_out}" ]; then
-  echo "A new cros_ec_commands.h must be generated."
-  echo 'Please run "make buildall" or "make build_cros_ec_commands"'.
-  exit 1
-fi
-
-if [ "${ec_commands_file_out}" -ot "${ec_commands_file_in}" ]; then
-  echo "cros_ec_commands.h is out of date."
-  echo 'Please run "make buildall" or "make build_cros_ec_commands"'.
-  exit 1
-fi
+echo 'chromiumos/infra/tools/unifdef latest' > "${TMP:?}/ensure_file.txt"
+cipd ensure -ensure-file "${TMP:?}/ensure_file.txt" -root "${TMP:?}"
+export PATH="${TMP:?}/bin:${PATH}"
+./util/make_linux_ec_commands_h.sh include/ec_commands.h \
+  "${ec_commands_file_out}"
 
 "${ZEPHYR_BASE}/scripts/checkpatch.pl" -f "${ec_commands_file_out}" \
   --ignore=BRACKET_SPACE
+
+cleanup
