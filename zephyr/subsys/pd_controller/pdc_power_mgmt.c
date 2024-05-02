87e803d59d7c60694becee8234a6d84826e85d79
Daisuke Nojiri <dnojiri@chromium.org>
2024-05-02 09:49:21 -0700

    PPM: Retry command for PPM if it fails for EBUSY
    With this CL, the PPM will retry to post a command if the queue is busy
    (i.e. -EBUSY) until it times out.
    
    The PPM will post a command with a temporary command completion
    callback. The PDM will post a command without a temporary callback.
    Thus, the default callback will be called and there is no functional
    change for the PDM.
    
    BUG=b:319124515
    TEST=Brox
    
    Change-Id: I3681dad91345dd67b8e47a187e2abd419f1f30ac

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -636,6 +636,7 @@ struct pdc_port_t {
 	/** SET_DRP variable used with CMD_SET_DRP */
 	enum drp_mode_t drp;
 	/** Callback */
+	struct pdc_callback cc_cb;
 	struct pdc_callback ci_cb;
 };
 
@@ -2064,9 +2065,12 @@ static const struct smf_state pdc_states[] = {
 /**
  * @brief CCI event handler call back
  */
-static void pdc_cci_handler_cb(union cci_event_t cci_event, void *cb_data)
+static void pdc_cc_handler_cb(const struct device *dev,
+			      const struct pdc_callback *callback,
+			      union cci_event_t cci_event)
 {
-	struct pdc_port_t *port = (struct pdc_port_t *)cb_data;
+	struct pdc_port_t *port =
+		CONTAINER_OF(callback, struct pdc_port_t, cc_cb);
 	bool post_event = false;
 
 	/* Handle busy event from driver */
@@ -2145,8 +2149,12 @@ static int pdc_subsys_init(const struct device *dev)
 
 	init_port_variables(port);
 
-	/* Set cci call back */
-	pdc_set_handler_cb(port->pdc, pdc_cci_handler_cb, (void *)port);
+	/* Set cc call back */
+	port->cc_cb.handler = pdc_cc_handler_cb;
+	port->cc_cb.cci_event_mask.busy = 1;
+	port->cc_cb.cci_event_mask.command_completed = 1;
+	port->cc_cb.cci_event_mask.error = 1;
+	pdc_set_handler_cb(port->pdc, &port->cc_cb);
 
 	/* Set ci call back */
 	port->ci_cb.handler = pdc_ci_handler_cb;
