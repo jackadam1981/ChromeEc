ce1b7805954328cbaa7dbb1ce00d155367d626f1
Fei Shao <fshao@chromium.org>
2024-05-03 14:42:51 +0800

    chip/mt_scp: mt8188: Disable 4K capability
    We found that using hardware vcodec for 2K or 4K video consumes
    excessive memory and leads to system jank and hang. The cause is not yet
    identified.
    
    Disable 4K capability as temporary workaround to mitigate system
    performance degrade. Revert the change when the issue is fixed.
    
    BUG=b:332235023
    TEST=dup dozens of 2K and 4K video files in folder, system doesn't hang
    
    Change-Id: Ie53a91bb360cff14dda76d73695efc86426cf1e8

--- chip/mt_scp/mt8188/video.c
+++ chip/mt_scp/mt8188/video.c
@@ -13,7 +13,12 @@ uint32_t video_get_enc_capability(void)
 
 uint32_t video_get_dec_capability(void)
 {
+	/*
+	 * (b/332235023): Using hardware vdec for >= 2K content consumes
+	 * excessive memory and causes system hang and performance degrade.
+	 * Disable 4K capability temporarily until the issue is fixed.
+	 */
 	return VDEC_CAP_MM21 | VDEC_CAP_IS_SUPPORT_10BIT | VDEC_CAP_H264_SLICE |
 	       VDEC_CAP_VP8_FRAME | VDEC_CAP_VP9_FRAME | VDEC_CAP_AV1_FRAME |
-	       VDEC_CAP_HEVC_SLICE;
+	       VDEC_CAP_HEVC_SLICE | VDEC_CAP_4K_DISABLED;
 }
