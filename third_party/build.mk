8d283b69c6c06235a65c024d184d0e7a450c11f9
Craig Hesling <hesling@chromium.org>
2024-05-03 18:15:56 -0700

    Pull in Elan private repo from unnested location
    * Now that the repo for Elan will not be easily traversable from within
      the EC repo, we expose more build related details in the public repo.
      This makes it a bit more obvious to others why their build may be
      failing. We put the library install/copy rule in the private repo,
      so that vendors can see directly what rule would need to be replaced
      to be able to build their private lib from source.
    * The previous rule to copy a binary from private repo no longer
      applies.
    * Update getversion.sh to use new relative path
    
    BUG=b:314208406
    TEST=make buildall
    TEST=make BOARD=buccaneer test-aes all
    
    Change-Id: I84e5443215735cf424b072ca095ca24ac7dbb335

---
+++ third_party/build.mk
@@ -0,0 +1,7 @@
+# -*- makefile -*-
+# vim: set filetype=make :
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+include third_party/elan/build.mk
