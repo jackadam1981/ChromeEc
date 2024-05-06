a1ecdd9f5ef9d0fcb8fed392425e913b74d00c68
Jes B. Klinke <jbk@chromium.org>
2024-05-06 09:02:53 -0700

    board/hyperdebug: Use timer 7 for JTAG bit banging
    By using timer 7 (which does not have PWM capabilities) instead of timer
    3 for timing JTAG clock pulses, we achieve that timer 3 is free to
    support PWM waveform generation on 7 additional HyperDebug pin.
    
    Also, at some point the clock distribution in HyperDebug was changed
    such that timers run at half the core clock frequency, without updating
    the calculation in cmsis-dap.c.  This CL changes the code to use
    clock_get_timer_freq(), rather than CPU_CLOCK, in order to take into
    account the fact that core clock and timer clock are not identical.
    
    BUG=b:339034638
    TEST=manually observe JTAG waveform
    
    Change-Id: I8780d022073452da54ed1d7aea30b1bb69d62936

--- board/hyperdebug/board.h
+++ board/hyperdebug/board.h
@@ -194,11 +194,11 @@
 /* Timer selection */
 #define PWM_TIMER_1 1
 #define TIM_CLOCK32 2
-#define JTAG_TIMER 3
+#define PWM_TIMER_3 3
 #define PWM_TIMER_4 4
 #define PWM_TIMER_5 5
 #define BITBANG_TIMER 6
-#define UNUSED_TIMER_7 7
+#define JTAG_TIMER 7
 #define PWM_TIMER_8 8
 #define PWM_TIMER_15 15
 #define PWM_TIMER_16 16
