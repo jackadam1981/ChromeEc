1faa9ba226ce07e58772ab7386de47d3407e928a
Tom Hughes <tomhughes@chromium.org>
2024-05-09 13:09:27 -0700

    WIP: Convert Zephyr fingerprint tests to C++
    We want to be able to use C++ in fpsensor_state.h, which is included by
    these tests.
    
    zephyr/test/fingerprint/task/src/fpsensor_init.cc:38:11:
    error: initializer-string for char array is too long, array size is 32
    but initializer has size 33 (including the null terminating character)
                     .seed = "very_secret_32_bytes_of_tpm_seed",
                             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    zephyr/test/fingerprint/task/src/fpsensor_init.cc:68:11:
    error: initializer-string for char array is too long, array size is 32
    but initializer has size 33 (including the null terminating character)
                     .seed = "very_secret_32_bytes_of_tpm_seed",
    
    BUG=none
    TEST=none
    
    Change-Id: Iee5882239b01949a047ddd27d79ee219998cadfc

--- zephyr/test/fingerprint/task/src/fpsensor_init.cc
+++ zephyr/test/fingerprint/task/src/fpsensor_init.cc
@@ -14,12 +14,14 @@
 #include <zephyr/ztest.h>
 #include <zephyr/ztest_assert.h>
 
+extern "C" {
 #include <drivers/fingerprint.h>
 #include <drivers/fingerprint_sim.h>
 #include <ec_commands.h>
 #include <ec_tasks.h>
 #include <fpsensor/fpsensor_state.h>
 #include <host_command.h>
+}
 
 DEFINE_FFF_GLOBALS;
 
@@ -33,7 +35,7 @@ ZTEST_USER(fpsensor_init, test_tpm_seed_init)
 	struct ec_params_fp_seed params = {
 		.struct_version = 4,
 		.reserved = 0,
-		.seed = "very_secret_32_bytes_of_tpm_seed",
+		.seed = "very_secret_32_byte_of_tpm_seed",
 	};
 
 	/* Get FP encryption flags. */
@@ -63,7 +65,7 @@ ZTEST_USER(fpsensor_init, test_tpm_seed_invalid)
 		/* 0 is not a valid structure version. */
 		.struct_version = 0,
 		.reserved = 0,
-		.seed = "very_secret_32_bytes_of_tpm_seed",
+		.seed = "very_secret_32_byte_of_tpm_seed",
 	};
 
 	/* Try to set TPM seed (should fail). */
