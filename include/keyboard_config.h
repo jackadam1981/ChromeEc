830f3002b8af70b6afbd8ed545c868ec11eb6e77
Daisuke Nojiri <dnojiri@chromium.org>
2024-05-10 08:25:56 -0700

    Keyboard: Add CONFIG_KEYBOARD_COLS
    CONFIG_KEYBOARD_COLS indicates the number of the columns the keyboard
    subsystem scans.
    
    BUG=b:331761304,b:337139763,b:325548277
    TEST=Kyogre
    
    Change-Id: I4db08b87a872f8bd4e23d1511d5670a303b65daf

--- include/keyboard_config.h
+++ include/keyboard_config.h
@@ -11,9 +11,11 @@
 #include "common.h"
 
 #ifdef CONFIG_KEYBOARD_CUSTOMIZATION
+
 /* include the board layer keyboard header file */
 #include "keyboard_customization.h"
 #define KEYBOARD_COLS KEYBOARD_COLS_MAX
+
 #else /* CONFIG_KEYBOARD_CUSTOMIZATION */
 
 /* Keyboard matrix is 13 (or 15 with keypad) output columns x 8 input rows */
@@ -21,12 +23,14 @@
 #define KEYBOARD_COLS_NO_KEYPAD 13
 
 /*
- * KEYBOARD_COLS has the column size of the default matrix. KEYBOARD_COLS_MAX
+ * KEYBOARD_COLS is the column size of the default matrix. KEYBOARD_COLS_MAX
  * has the column size of the largest matrix. It's used to statically allocate
  * arrays used by the scanner. keyboard_cols holds a runtime column size. The
  * scanner uses it as a loop terminal.
  */
-#ifdef CONFIG_KEYBOARD_KEYPAD
+#ifdef CONFIG_KEYBOARD_COLS
+#define KEYBOARD_COLS CONFIG_KEYBOARD_COLS
+#elif defined(CONFIG_KEYBOARD_KEYPAD)
 #define KEYBOARD_COLS KEYBOARD_COLS_WITH_KEYPAD
 #else
 #define KEYBOARD_COLS KEYBOARD_COLS_NO_KEYPAD
