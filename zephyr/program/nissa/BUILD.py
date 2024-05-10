eb47333b45490e839ec7a296efe3b4b5fc536d74
leo3.chen <leo3.chen@lcfc.corp-partner.google.com>
2024-05-10 11:53:34 +0800

    [Do not merge!] Test
    TEST
    
    Change-Id: I1e6f16192097572c6ee991f0d5356dd877e90c48

--- zephyr/program/nissa/BUILD.py
+++ zephyr/program/nissa/BUILD.py
@@ -101,6 +101,11 @@ pujjoga = register_nissa_project(
     chip="npcx9/npcx9m3f",
 )
 
+pujjogb = register_nissa_project(
+    project_name="pujjogb",
+    chip="npcx9/npcx9m3f",
+)
+
 xivu = register_nissa_project(
     project_name="xivu",
     chip="npcx9/npcx9m3f",
@@ -182,6 +187,7 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="nokris", addr=0x7FFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="pirrha", addr=0xBFFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="pujjo", addr=0x7FFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="pujjoga", addr=0x7FFE0)
+assert_rw_fwid_DO_NOT_EDIT(project_name="pujjogb", addr=0x7FFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="quandiso", addr=0xB7FE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="uldren", addr=0x7FFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="xivu", addr=0x7FFE0)
