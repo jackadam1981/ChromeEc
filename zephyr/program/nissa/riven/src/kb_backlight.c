70654085f3372d8fddd2084a2f1493b2a1ccffb2
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2024-05-13 17:13:11 +0800

    riven: remove FW_CONFIG field FW_KB_BL
    There's no sku support keyboard backlight now so remove
    FW_KB_BL field to reserve one bit fw_config.
    
    BUG=b:337169542
    TEST="zmake build riven --clobber" success
         "./twister -T zephyr/test/nissa/ -v -i -s nissa.riven" pass
    
    Change-Id: I26ace16b9a8284528fbc22a72576a3ebfaf7a28b

--- zephyr/program/nissa/riven/src/kb_backlight.c
+++ zephyr/program/nissa/riven/src/kb_backlight.c
@@ -15,20 +15,8 @@ LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
 
 __override uint32_t board_override_feature_flags0(uint32_t flags0)
 {
-	int ret;
-	uint32_t val;
-
 	/*
-	 * Remove keyboard backlight feature for devices that don't support it.
+	 * Remove keyboard backlight feature for devices don't support it.
 	 */
-	ret = cros_cbi_get_fw_config(FW_KB_BL, &val);
-	if (ret != 0) {
-		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FW_KB_BL);
-		return flags0;
-	}
-
-	if (val == FW_KB_BL_NOT_PRESENT)
-		return (flags0 & ~EC_FEATURE_MASK_0(EC_FEATURE_PWM_KEYB));
-	else
-		return flags0;
+	return (flags0 & ~EC_FEATURE_MASK_0(EC_FEATURE_PWM_KEYB));
 }
