2c4cf816c7f3997ca62487c46a3f73b416b64214
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2024-05-14 15:45:33 -0700

    ptlrvp_mchp: Add I2C configuration
    Add I2C peripherals configuration based on schematics.
    
    BUG=b:335888182
    TEST=Use command `i2c scan I2C_CHARGER`
         Able to list I2C devices attached to bus
    
    Change-Id: I5b7fcc224cf85e85ebd8c605781a249e00db03be

--- zephyr/program/intelrvp/ptlrvp/ptlrvp_mchp/include/board.h
+++ zephyr/program/intelrvp/ptlrvp/ptlrvp_mchp/include/board.h
@@ -16,4 +16,19 @@
 #define PWR_EC_PCH_SYS_PWROK MCHP_GPIO_DECODE_015
 #define PWR_ALL_SYS_PWRGD MCHP_GPIO_DECODE_057
 
+/* I2C Ports */
+#define CHARGER_I2C i2c_smb_0
+#define CHARGER_I2C_PIN_CTRL_SCL &i2c00_scl_gpio004
+#define CHARGER_I2C_PIN_CTRL_SDA &i2c00_sda_gpio003
+#define CHARGER_I2C_VENDOR_PROP      \
+	pinctrl-names = "default";   \
+	port_sel = <0>;
+
+#define PD_POW_I2C i2c_smb_1
+#define PD_POW_I2C_PIN_CTRL_SCL &i2c06_scl_gpio140
+#define PD_POW_I2C_PIN_CTRL_SDA &i2c06_sda_gpio132
+#define PD_POW_I2C_VENDOR_PROP       \
+	pinctrl-names = "default";   \
+	port_sel = <6>;
+
 #endif /* PTLRVP_MCHP_BOARD_H_ */
