2b4bab3357aeadf269482a3274b4fae3815e2929
Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
2024-05-15 17:58:50 +0530

    zephyr/test/pdc: Add PDC PMC Debug coverage
    Additional tests implemented to validate  PDC-PMC debug
    functionality in the pdc_pmc_debug.c file
    
    BUG=b:337721162
    TEST=./twister -v -i --coverage -T zephyr/test/pdc
    
    Change-Id: I0e1b9e21d52bca0374df42860b80840efeefd737

--- zephyr/emul/emul_realtek_rts54xx.c
+++ zephyr/emul/emul_realtek_rts54xx.c
@@ -601,6 +601,36 @@ static int get_vdo(struct rts5453p_emul_pdc_data *data,
 	return 0;
 }
 
+static int get_pch_data_status(struct rts5453p_emul_pdc_data *data,
+			       const union rts54_request *req)
+{
+	uint8_t pch_data_status_output = 0;
+
+	memset(&data->response, 0, sizeof(data->response));
+
+	/* Data transfer Length */
+	data->response.get_pch_data_status.byte_count = 5;
+
+	/* BIT 0 - Data_Connection_Present */
+	pch_data_status_output = data->connector_status.connect_status & 0x1;
+	/* BIT 1 - Connection Orientation */
+	pch_data_status_output |= (data->connector_status.orientation << 1);
+	/* BIT 4 - USB2_Connection */
+	pch_data_status_output |=
+		((data->connector_status.conn_partner_flags & 0x1) << 4);
+	/* BIT 5  - USB3.2_Connection */
+	pch_data_status_output |=
+		((data->connector_status.conn_partner_flags & 0x1) << 5);
+
+	data->response.get_pch_data_status.pch_data_status[0] =
+		pch_data_status_output;
+	LOG_INF("GET_PCH_DATA_STATUS PORT_NUM:%d data_status:0x%x",
+		req->get_pch_data_status.port_num, pch_data_status_output);
+
+	send_response(data);
+	return 0;
+}
+
 static bool send_response(struct rts5453p_emul_pdc_data *data)
 {
 	if (data->delay_ms > 0) {
@@ -682,6 +712,7 @@ const struct commands sub_cmd_x08[] = {
 	{ .code = 0xA8, HANDLER_DEF(unsupported) },
 	{ .code = 0xA9, HANDLER_DEF(unsupported) },
 	{ .code = 0xAA, HANDLER_DEF(unsupported) },
+	{ .code = 0xE0, HANDLER_DEF(get_pch_data_status) },
 };
 
 const struct commands sub_cmd_x0E[] = {
