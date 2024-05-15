34cb9caf55738348e8e28cb762dce2f284e08d64
David Huang <david1.huang@lcfc.corp-partner.google.com>
2024-05-15 21:46:11 +0800

    test
    Change-Id: Icb75853f9855e86137f9af0091e4f217aa6a04b2

---
+++ zephyr/program/nissa/include/pujjoga_board.h
@@ -0,0 +1,13 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Pujjoga sub-board declarations */
+
+#ifndef __CROS_EC_NISSA_PUJJOGA_BOARD_H__
+#define __CROS_EC_NISSA_PUJJOGA_BOARD_H__
+
+void pen_detect_interrupt(enum gpio_signal s);
+
+#endif /* __CROS_EC_NISSA_PUJJOGA_SUB_BOARD_H__ */
