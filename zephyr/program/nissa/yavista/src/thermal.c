2d9f91066ecd53ae524b4af27e1466a2469f5a85
Fabio Baltieri <fabiobaltieri@google.com>
2024-05-15 15:34:11 +0000

    cbi: always check cros_cbi_get_fw_config return value
    Fix all instances that were ignoring the return value of
    cros_cbi_get_fw_config. The function now returns an error if the
    underlying data has not been initialized, this changes all occurrences
    of this call to detect the condition and fail whatever they are doing
    rather than continuing silently with whatever value matched 0.
    
    For the Vivaldi keyboard specifically this means that the condition
    would disable vivaldi entirely rather than defaulting to the first
    config, making the condition detectable in qualification even if the
    target device so happen to match the first config.
    
    BUG=b:340187957
    TEST=cq dry run
    
    Change-Id: If375c24be23d668c69e0e7d30db5726e78a83383

--- zephyr/program/nissa/yavista/src/thermal.c
+++ zephyr/program/nissa/yavista/src/thermal.c
@@ -10,7 +10,11 @@
 #include "thermal.h"
 #include "util.h"
 
+#include <zephyr/logging/log.h>
+
 #include <ap_power/ap_power_interface.h>
+
+LOG_MODULE_REGISTER(board_thermal, LOG_LEVEL_INF);
 
 #define TEMP_CPU TEMP_SENSOR_ID(DT_NODELABEL(temp_cpu))
 #define TEMP_5V TEMP_SENSOR_ID(DT_NODELABEL(temp_5v_regulator))
@@ -52,10 +56,16 @@ BUILD_ASSERT(ARRAY_SIZE(fan_table_1) == ARRAY_SIZE(fan_table_2),
 bool is_fan_type_2(void)
 {
 	uint32_t val;
+	int ret;
+
 	/*
 	 * Retrieve the fan type config.
 	 */
-	cros_cbi_get_fw_config(FAN_TYPE, &val);
+	ret = cros_cbi_get_fw_config(FAN_TYPE, &val);
+	if (ret < 0) {
+		LOG_ERR("error retriving CBI config: %d", ret);
+		return false;
+	}
 
 	if (val == FW_FAN_TYPE_2) {
 		return true;
