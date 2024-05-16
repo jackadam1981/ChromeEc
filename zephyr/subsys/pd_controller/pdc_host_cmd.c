3eea96a322845b65e54f95ff6f4b261f810254fa
Tristan Honscheid <honscheid@google.com>
2024-05-16 14:55:09 -0600

    host_cmd: Create new EC_CMD_PD_CHIP_INFO version, update ectool
    Add a new version of EC_CMD_PD_CHIP_INFO (v2) that adds fields for:
     1. FW update control flags (for b/324892496, but not used yet)
     2. FW program name string baked into PDC FW to allow the host to
        determine which PDC FW variant is in use. This is a string reported
        by the PDC (see chromium:5296866)
    
    Update `ectool` to display the additional V2 fields, and also show the
    FW version in decimal format for convenience (e.g. 0xf01 -> ...0.15.1).
    Add a helper function to `misc_util.cc` to determine the highest EC
    command version supported by the DUT so we do not need to guess and
    check command versions.
    
    ```
    ./ectool pdchipinfo 0
    vendor_id: 0x18d1
    product_id: 0x5065
    device_id: 0x0
    fw_version: 0xf01
    fw_version_dec: 0.0.0.0.0.0.15.1
    min_req_fw_version: 0x0
    fw_update_flags: 0x0000
    fw_name_str: 'PROGNAMEHERE'
    ```
    
    BUG=b:340945454
    TEST=./ectool pdchipinfo 0
    
    Change-Id: I599a816fe65ddf651d523159a65f47be153482e9

--- zephyr/subsys/pd_controller/pdc_host_cmd.c
+++ zephyr/subsys/pd_controller/pdc_host_cmd.c
@@ -19,7 +19,7 @@
 static enum ec_status hc_remote_pd_chip_info(struct host_cmd_handler_args *args)
 {
 	const struct ec_params_pd_chip_info *p = args->params;
-	struct ec_response_pd_chip_info_v1 resp = { 0 };
+	struct ec_response_pd_chip_info_v2 resp = { 0 };
 	struct pdc_info_t pdc_info;
 
 	if (pdc_power_mgmt_get_info(p->port, &pdc_info, p->live)) {
@@ -37,13 +37,34 @@ static enum ec_status hc_remote_pd_chip_info(struct host_cmd_handler_args *args)
 	resp.fw_version_string[1] = PDC_FWVER_GET_MINOR(pdc_info.fw_version);
 	resp.fw_version_string[0] = PDC_FWVER_GET_PATCH(pdc_info.fw_version);
 
-	/*
-	 * Take advantage of the fact that v0 and v1 structs have the
-	 * same layout for v0 data. (v1 just appends data)
+	/* Look up correct response size based on version. All support the
+	 * basic fields set above.
 	 */
-	args->response_size =
-		args->version ? sizeof(struct ec_response_pd_chip_info_v1) :
-				sizeof(struct ec_response_pd_chip_info);
+	switch (args->version) {
+	case 0:
+		args->response_size = sizeof(struct ec_response_pd_chip_info);
+
+		/* All V0 fields populated above */
+		break;
+	case 1:
+		args->response_size =
+			sizeof(struct ec_response_pd_chip_info_v1);
+
+		/* PDC doesn't use the min_req_fw_version_string field added in
+		 * V1.
+		 */
+		break;
+	case 2:
+		args->response_size =
+			sizeof(struct ec_response_pd_chip_info_v2);
+
+		/* Fill in V2-specific info */
+		resp.fw_update_flags = 0;
+		strncpy(resp.fw_name_str, pdc_info.program_name,
+			sizeof(resp.fw_name_str));
+
+		break;
+	}
 
 	memcpy(args->response, &resp, args->response_size);
 
@@ -51,7 +72,7 @@ static enum ec_status hc_remote_pd_chip_info(struct host_cmd_handler_args *args)
 }
 
 DECLARE_HOST_COMMAND(EC_CMD_PD_CHIP_INFO, hc_remote_pd_chip_info,
-		     EC_VER_MASK(0) | EC_VER_MASK(1));
+		     EC_VER_MASK(0) | EC_VER_MASK(1) | EC_VER_MASK(2));
 #endif /* CONFIG_HOSTCMD_PD_CHIP_INFO */
 
 static enum ec_status hc_pd_ports(struct host_cmd_handler_args *args)
