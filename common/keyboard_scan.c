9fb5440a5c7dce547c15a09ffe3b7d097798033c
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2024-05-17 19:52:59 +0800

    keyboard: Fix "Esc+Refresh+Power" function under AC only
    This solution is only a temporary solution. Import the reflash
    key into the recovery mode determination.
    
    BUG=b:341023382
    TEST=zmake build voltorb
    LOW_COVERAGE_REASON=Tests will be added in b:341023382
    
    Change-Id: I9bc8dd92bc6f1de44c569a72b6e7194fc02321f0

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -595,6 +595,7 @@ static void boot_key_released(const uint8_t *state)
 		boot_key_clear(b);
 	}
 }
+
 #endif /* CONFIG_KEYBOARD_BOOT_KEYS */
 
 /**
@@ -807,6 +808,15 @@ static void power_button_change(void)
 DECLARE_HOOK(HOOK_POWER_BUTTON_CHANGE, power_button_change, HOOK_PRIO_DEFAULT);
 #endif /* CONFIG_POWER_BUTTON */
 
+static uint8_t keyboard_mask_refresh;
+__overridable uint8_t board_keyboard_row_refresh(void)
+{
+	if (IS_ENABLED(CONFIG_KEYBOARD_REFRESH_ROW3))
+		return 3;
+	else
+		return 2;
+}
+
 /*
  * Returns mask of the boot keys that are pressed, with at most the keys used
  * for keyboard-controlled reset also pressed.
@@ -821,6 +831,11 @@ static uint32_t check_key_list(const uint8_t *state)
 	/* Make copy of current debounced state. */
 	memcpy(curr_state, state, sizeof(curr_state));
 
+#ifndef CONFIG_KEYBOARD_MULTIPLE
+	curr_state[KEYBOARD_COL_REFRESH] &= ~keyboard_mask_refresh;
+#else
+	curr_state[key_typ.col_refresh] &= ~keyboard_mask_refresh;
+#endif
 	/* Update mask with all boot keys that were pressed. */
 	k = boot_key_list;
 	for (c = 0; c < ARRAY_SIZE(boot_key_list); c++, k++) {
@@ -889,17 +904,25 @@ static uint32_t check_boot_key(const uint8_t *state)
 	if (system_jumped_late())
 		return BOOT_KEY_NONE;
 
-	/*
-	 * Boot keys are available only through reset-pin reset, which can be
-	 * issued only by GSC (through refresh+power combo).
-	 *
-	 * If the EC resets differently (e.g. watchdog, power-on, exception),
-	 * we don't want to accidentally enter recovery mode even if a refresh
-	 * key or whatever key is pressed (as previously allowed).
-	 */
-	if (!(system_get_reset_flags() & EC_RESET_FLAG_RESET_PIN))
+		/*
+		 * Boot keys are available only through reset-pin reset, which
+		 * can be issued only by GSC (through refresh+power combo).
+		 *
+		 * If the EC resets differently (e.g. watchdog, power-on,
+		 * exception), we don't want to accidentally enter recovery mode
+		 * even if a refresh key or whatever key is pressed (as
+		 * previously allowed).
+		 */
+#ifndef CONFIG_KEYBOARD_MULTIPLE
+	if (!(system_get_reset_flags() & EC_RESET_FLAG_RESET_PIN) &&
+	    !(state[KEYBOARD_COL_REFRESH] & keyboard_mask_refresh))
 		return BOOT_KEY_NONE;
-
+#else
+	if (!(system_get_reset_flags() & EC_RESET_FLAG_RESET_PIN) &&
+	    !(state[key_typ.col_refresh] & keyboard_mask_refresh))
+		return BOOT_KEY_NONE;
+#endif
+	printk(" no BOOT_KEY_NONE/n");
 	return check_key_list(state);
 }
 #endif
@@ -946,6 +969,10 @@ void keyboard_scan_init(void)
 		 */
 		CPRINTS("WARN: Debounce durations not equal");
 	}
+
+	/* Configure refresh key matrix */
+	keyboard_mask_refresh =
+		KEYBOARD_ROW_TO_MASK(board_keyboard_row_refresh());
 
 	if (!IS_ENABLED(CONFIG_KEYBOARD_SCAN_ADC))
 		/* Configure GPIO */
