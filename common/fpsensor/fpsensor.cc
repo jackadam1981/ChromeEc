8311d141d14cb4654325b519382bb90e4332bd0c
Tom Hughes <tomhughes@chromium.org>
2024-05-16 17:09:49 -0700

    WIP: Move fp_enc_buffer to global context struct
    We're trying to remove global state. The first step is to consolidate
    all global state into a struct, which makes it obvious when it's being
    used.
    
    BUG=b:286119221
    TEST=none
    
    Change-Id: I77360eb35580d655e99189dd06a831f585c159c0

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -501,7 +501,8 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		return EC_RES_INVALID_PARAM;
 	if (fgr >= global_context.templ_valid)
 		return EC_RES_UNAVAILABLE;
-	ret = validate_fp_buffer_offset(sizeof(fp_enc_buffer), offset, size);
+	ret = validate_fp_buffer_offset(
+		global_context.fp_enc_buffer.size_bytes(), offset, size);
 	if (ret != EC_SUCCESS)
 		return EC_RES_INVALID_PARAM;
 
@@ -511,7 +512,8 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		/* Host has requested the first chunk, do the encryption. */
 		timestamp_t now = get_time();
 		/* Encrypted template is after the metadata. */
-		uint8_t *encrypted_template = fp_enc_buffer + sizeof(*enc_info);
+		uint8_t *encrypted_template =
+			global_context.fp_enc_buffer.data() + sizeof(*enc_info);
 		/* Positive match salt is after the template. */
 		uint8_t *positive_match_salt =
 			encrypted_template + sizeof(fp_template[0]);
@@ -524,14 +526,15 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 			return EC_RES_BUSY;
 		encryption_deadline.val = now.val + (1 * SECOND);
 
-		memset(fp_enc_buffer, 0, sizeof(fp_enc_buffer));
+		memset(global_context.fp_enc_buffer.data(), 0,
+		       global_context.fp_enc_buffer.size_bytes());
 		/*
 		 * The beginning of the buffer contains nonce, encryption_salt
 		 * and tag.
 		 */
 		enc_info = reinterpret_cast<
 			struct ec_fp_template_encryption_metadata *>(
-			fp_enc_buffer);
+			global_context.fp_enc_buffer.data());
 		enc_info->struct_version = FP_TEMPLATE_FORMAT_VERSION;
 		trng_init();
 		trng_rand_bytes(enc_info->nonce, FP_CONTEXT_NONCE_BYTES);
@@ -585,7 +588,7 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		}
 		global_context.templ_dirty &= ~BIT(fgr);
 	}
-	memcpy(out, fp_enc_buffer + offset, size);
+	memcpy(out, global_context.fp_enc_buffer.data() + offset, size);
 	args->response_size = size;
 
 	return EC_RES_SUCCESS;
@@ -631,7 +634,8 @@ enum ec_status fp_commit_template(std::span<const uint8_t> context)
 	uint16_t idx = global_context.templ_valid;
 	struct ec_fp_template_encryption_metadata *enc_info;
 	/* Encrypted template is after the metadata. */
-	uint8_t *encrypted_template = fp_enc_buffer + sizeof(*enc_info);
+	uint8_t *encrypted_template =
+		global_context.fp_enc_buffer.data() + sizeof(*enc_info);
 	/* Positive match salt is after the template. */
 	uint8_t *positive_match_salt =
 		encrypted_template + sizeof(fp_template[0]);
@@ -646,7 +650,8 @@ enum ec_status fp_commit_template(std::span<const uint8_t> context)
 	 * The beginning of the buffer contains nonce, encryption_salt
 	 * and tag.
 	 */
-	enc_info = (struct ec_fp_template_encryption_metadata *)fp_enc_buffer;
+	enc_info = (struct ec_fp_template_encryption_metadata *)
+			   global_context.fp_enc_buffer.data();
 	enum ec_status res = validate_template_format(enc_info);
 	if (res != EC_RES_SUCCESS) {
 		CPRINTS("fgr%d: Template format not supported", idx);
@@ -719,12 +724,12 @@ static enum ec_status fp_command_template(struct host_cmd_handler_args *args)
 	if (args->params_size !=
 	    size + offsetof(struct ec_params_fp_template, data))
 		return EC_RES_INVALID_PARAM;
-	enum ec_error_list ret =
-		validate_fp_buffer_offset(sizeof(fp_enc_buffer), offset, size);
+	enum ec_error_list ret = validate_fp_buffer_offset(
+		global_context.fp_enc_buffer.size_bytes(), offset, size);
 	if (ret != EC_SUCCESS)
 		return EC_RES_INVALID_PARAM;
 
-	memcpy(&fp_enc_buffer[offset], params->data, size);
+	memcpy(&global_context.fp_enc_buffer[offset], params->data, size);
 
 	if (xfer_complete) {
 		return fp_commit_template(global_context.user_id);
