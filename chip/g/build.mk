cceb417d3e9ba9e70cd15e93202a19c40d52f236
Vadim Sukhomlinov <sukhomlinov@google.com>
2024-05-17 14:24:21 -0700

    cr50: build codesigner automatically
    Since `cr50-utils` package was removed from chroot and can't be
    installed anymore, build it as part of build process.
    This is fixed re-land of https://crrev.com/c/5548667, and
    https://crrev.com/c/5555836
    
    `reef` board builds are silently broken - http://b/326998098
    
    BUG=b:340644412
    TEST=make; verify that codesigner is built if unavailable, and that
    node-lock signed image boots correctly
    
    Change-Id: I52958c8c69b8da5f4edd4079b97c44331a4c873f
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>

--- chip/g/build.mk
+++ chip/g/build.mk
@@ -139,16 +139,31 @@ endif
 MANIFEST := util/signer/ec_RW-manifest-dev.json
 CR50_RO_KEY ?= rom-testkey-A.pem
 
-# Make sure signing happens only when the signer is available.
-REAL_SIGNER = /usr/bin/cr50-codesigner
-ifneq ($(wildcard $(REAL_SIGNER)),)
-SIGNED_IMAGES = 1
-SIGNER := $(REAL_SIGNER)
-endif
-
 ifeq ($(CHIP_MK_INCLUDED_ONCE),)
 
 CHIP_MK_INCLUDED_ONCE := 1
+
+CODESIGNER_PATH := $(abspath ../cr50-utils/software/tools/codesigner)
+
+# Try to find preinstalled codesigner
+SIGNER := $(firstword $(wildcard /usr/bin/cr50-codesigner\
+	  $(shell which cr50-codesigner 2>/dev/null)))
+# Try to build signer from the known location, if it is missing
+ifeq ("$(SIGNER)","")
+# If source path is present, build codesigner later as dependency
+ifeq ($(wildcard $(CODESIGNER_PATH)),)
+SIGNER := $(CODESIGNER_PATH)/codesigner
+
+# Set CFLAGS and CXX to avoid passing target configuration
+$(SIGNER): $(CODESIGNER_PATH)/*.cc $(CODESIGNER_PATH)/../common/*
+	CFLAGS="-O2" CXX="clang++" $(MAKE) -C $(CODESIGNER_PATH) codesigner
+endif
+else
+# It is preinstalled, so set empty rule
+$(SIGNER): ;
+
+endif
+
 # We'll have to tweak the manifest no matter what, but different ways
 # depending on the way the image is built.
 SIGNER_MANIFEST := $(shell mktemp /tmp/h1.signer.XXXXXX)
@@ -174,6 +189,10 @@ CR50_RW_KEY = loader-testkey-A.pem
 # Make sure manifset Key ID field matches the actual key.
 DUM := $(shell sed 's/860844255/-764428053/' $(MANIFEST) > $(SIGNER_MANIFEST))
 else
+ifeq ("$(SIGNER)","")
+$(error cr50-codesigner is not available!)
+endif
+
 # The private key comes from the sighing fob.
 CR50_RW_KEY = cr50_rom0-dev-blsign.pem.pub
 
