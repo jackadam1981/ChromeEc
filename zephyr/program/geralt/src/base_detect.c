56e1f5a110bea66b9dc65cfc137d0a74824fc020
mike <mike5@huaqin.corp-partner.google.com>
2024-05-20 19:17:17 +0800

    geralt: Add base_detect_enable function to HOOK_INIT
    because of CL:5536655, reland the base_detect_enable function
    to initial the keyboard function.
    
    BUG=b:339951091
    TEST=sysjump ro/rw, geralt keyboard work normal.
    LOW_COVERAGE_REASON=b:308941437
    
    Change-Id: Ib881d615039038300302976801b0812004c96613

--- zephyr/program/geralt/src/base_detect.c
+++ zephyr/program/geralt/src/base_detect.c
@@ -114,6 +114,9 @@ void base_init_setting(void)
 	if (adc_read_channel(ADC_BASE_DET) > DETACH_MIN_THRESHOLD_MV) {
 		base_update(false);
 	}
+	if (!chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
+		base_detect_enable(true);
+	}
 }
 DECLARE_HOOK(HOOK_INIT, base_init_setting, HOOK_PRIO_DEFAULT);
 
