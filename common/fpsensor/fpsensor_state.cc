134a0ee0bece9892e01085be6b47c2407f98acca
Tom Hughes <tomhughes@chromium.org>
2024-05-23 16:31:23 -0700

    fpsensor: Convert fp_positive_match_salt to std::array
    BUG=b:286119221
    TEST=make runhosttests
    
    Change-Id: I4ddb96f8946ff475fedf27b584b906dd48549b97

--- common/fpsensor/fpsensor_state.cc
+++ common/fpsensor/fpsensor_state.cc
@@ -63,7 +63,7 @@ struct fpsensor_context global_context = {
 		.deadline = {
 			.val = 0,
 		}},
-	.fp_positive_match_salt = {{0}},
+	.fp_positive_match_salt = {{}},
 	.template_states = {},
 };
 
@@ -85,8 +85,8 @@ __test_only void fp_task_simulate(void)
 void fp_clear_finger_context(uint16_t idx)
 {
 	OPENSSL_cleanse(fp_template[idx], sizeof(fp_template[0]));
-	OPENSSL_cleanse(global_context.fp_positive_match_salt[idx],
-			sizeof(global_context.fp_positive_match_salt[0]));
+	OPENSSL_cleanse(global_context.fp_positive_match_salt[idx].data(),
+			global_context.fp_positive_match_salt[0].size());
 	global_context.template_states[idx] = std::monostate();
 }
 
