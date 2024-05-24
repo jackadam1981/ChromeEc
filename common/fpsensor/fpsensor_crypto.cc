faec7ef4dbbbe0d5ff8165dec63503444394b7ca
Tom Hughes <tomhughes@chromium.org>
2024-05-24 11:39:32 -0700

    WIP: Add secure_clear
    BUG=none
    TEST=none
    
    Change-Id: If2aef3b9d0dbcaaa358b83844def8c1b12ccf73d

--- common/fpsensor/fpsensor_crypto.cc
+++ common/fpsensor/fpsensor_crypto.cc
@@ -252,3 +252,8 @@ void rand_bytes(std::span<uint8_t> buffer)
 {
 	RAND_bytes(buffer.data(), buffer.size());
 }
+
+void secure_clear(std::span<uint8_t> buffer)
+{
+	OPENSSL_cleanse(buffer.data(), buffer.size());
+}
