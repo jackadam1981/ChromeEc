1e63e9e87ef85b7b89aa60ae07de182c12b26b92
Abhishek Pandit-Subedi <abhishekpandit@google.com>
2024-05-23 21:44:35 -0700

    PPM: Move headers from extra/um_ppm
    Move headers out of um_ppm, consolidate unused ones and place them in
    the right zephyr folders.
    
    BUG=b:339702957
    TEST=emerge-brox chromeos-zephyr and ./twister -s pdc.ppm
    
    Change-Id: I28c069652692b693fd22339dfc7226b9e4492afe

--- zephyr/subsys/ucsi/ppm_driver.c
+++ zephyr/subsys/ucsi/ppm_driver.c
@@ -7,8 +7,6 @@
 
 #include "cros_board_info.h"
 #include "ec_commands.h"
-#include "include/pd_driver.h"
-#include "include/ppm.h"
 #include "ppm_common.h"
 #include "usb_pd.h"
 #include "util.h"
@@ -17,6 +15,7 @@
 #include <zephyr/logging/log.h>
 
 #include <drivers/pdc.h>
+#include <usbc/ppm.h>
 
 LOG_MODULE_REGISTER(ppm, LOG_LEVEL_INF);
 
