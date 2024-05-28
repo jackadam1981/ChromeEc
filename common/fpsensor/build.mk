27241d5e68f85a34d892b462585d926ef2e2804f
Tom Hughes <tomhughes@chromium.org>
2024-05-28 15:46:16 -0700

    fpsensor: Don't include fpsensor objects in RO
    HAS_TASK_CONSOLE is "y" for RO, but we don't want to include
    fingerprint-related code in non-test RO. Use the new logic to only
    include these in RO if we're building FPMCU tests.
    
    BUG=none
    TEST=make runhosttests
    
    Change-Id: If6ec093dd69ea9213818987c856a88d83660d6fc

--- common/fpsensor/build.mk
+++ common/fpsensor/build.mk
@@ -35,9 +35,7 @@ $(out)/RW/$(_fpsensor_auth_crypto_stateless_obj): CFLAGS+=$(fpsensor_CFLAGS)
 
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_state_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_obj)
-all-obj-$(HAS_TASK_CONSOLE)+=$(_fpsensor_detect_strings_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_debug_obj)
-all-obj-$(HAS_TASK_CONSOLE)+=$(_fpsensor_utils_obj)
 all-obj-$(HAS_TASK_FPSENSOR)+=$(_fpsensor_auth_commands_obj)
 
 # Since we only include the FPSENSOR task in the RW image, HAS_TASK_FPSENSOR
@@ -53,6 +51,8 @@ fpsensor_obj_image=y
 endif
 endif
 
+all-obj-$(fpsensor_obj_image)+=$(_fpsensor_detect_strings_obj)
+all-obj-$(fpsensor_obj_image)+=$(_fpsensor_utils_obj)
 all-obj-$(fpsensor_obj_image)+=$(_fpsensor_auth_crypto_stateless_obj)
 all-obj-$(fpsensor_obj_image)+=$(_fpsensor_crypto_obj)
 all-obj-$(fpsensor_obj_image)+=$(_fpsensor_auth_crypto_stateful_obj)
