55f77af0d77caf662e90f0ff7c3ea35c1b8cdeb2
Ting Shen <phoenixshen@google.com>
2024-05-28 16:37:53 +0800

    usb_update: add more test code
    - Enable RWSIG and test RWSIG related features
    - Test more error cases
    
    BUG=b:287012522
    TEST=./twister -T zephyr/test/usb_updater/ --coverage --jobs=8
    
    Change-Id: I1452f6910df12e833a8792e6d25cb0b59c3ea10f

--- zephyr/test/usb_updater/src/update_fw_test.c
+++ zephyr/test/usb_updater/src/update_fw_test.c
@@ -6,6 +6,7 @@
 #include "fakes.h"
 #include "sha256.h"
 #include "update_fw.h"
+#include "vboot.h"
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/flash/flash_simulator.h>
@@ -164,8 +165,11 @@ static void *update_fw_setup(void)
 
 static void update_fw_before(void *f)
 {
+	static const struct vb21_packed_key fake_vb21_key = {};
+
 	FFF_FAKES_LIST(RESET_FAKE);
 	FFF_RESET_HISTORY();
+	vb21_get_packed_key_fake.return_val = &fake_vb21_key;
 }
 
 ZTEST_SUITE(update_fw, NULL, update_fw_setup, update_fw_before, NULL, NULL);
