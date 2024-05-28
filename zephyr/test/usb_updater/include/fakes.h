a6f1c9de4292f757c67e107ec5c2c0ed34c34e59
Ting Shen <phoenixshen@google.com>
2024-05-28 16:37:53 +0800

    usb_update: add more test code
    - Enable RWSIG and test RWSIG related features
    - Test more error cases
    
    BUG=none
    TEST=./twister -T zephyr/test/usb_updater/ --coverage --jobs=8
    
    Change-Id: I1452f6910df12e833a8792e6d25cb0b59c3ea10f

--- zephyr/test/usb_updater/include/fakes.h
+++ zephyr/test/usb_updater/include/fakes.h
@@ -3,8 +3,10 @@
  * found in the LICENSE file.
  */
 #include "ec_commands.h"
+#include "rwsig.h"
 #include "system.h"
 #include "touchpad.h"
+#include "vboot.h"
 
 #include <zephyr/fff.h>
 
@@ -15,6 +17,9 @@ DECLARE_FAKE_VALUE_FUNC(int, touchpad_debug, const uint8_t *, unsigned int,
 			uint8_t **, unsigned int *);
 DECLARE_FAKE_VALUE_FUNC(int, touchpad_update_write, int, int, const uint8_t *);
 DECLARE_FAKE_VALUE_FUNC(enum ec_image, system_get_image_copy);
+DECLARE_FAKE_VOID_FUNC(touchpad_task, void *);
+DECLARE_FAKE_VALUE_FUNC(const struct vb21_packed_key *, vb21_get_packed_key);
+DECLARE_FAKE_VALUE_FUNC(enum rwsig_status, rwsig_get_status);
 
 #define FFF_FAKES_LIST(FAKE)        \
 	FAKE(system_reset)          \
@@ -22,4 +27,7 @@ DECLARE_FAKE_VALUE_FUNC(enum ec_image, system_get_image_copy);
 	FAKE(touchpad_get_info)     \
 	FAKE(touchpad_debug)        \
 	FAKE(touchpad_update_write) \
-	FAKE(system_get_image_copy)
+	FAKE(system_get_image_copy) \
+	FAKE(touchpad_task)         \
+	FAKE(vb21_get_packed_key)   \
+	FAKE(rwsig_get_status)
