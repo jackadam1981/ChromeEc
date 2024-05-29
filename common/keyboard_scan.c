b9210d71fc79eebea023b4b95c644d5b9cf3d64e
Devin Lu <Devin.Lu@quantatw.com>
2024-05-29 15:59:43 +0800

    Keyboard: Only apply Ti50 workaround at boot
    Improve crrev.com/c/4912024 only cancel pass-through rows while
    the power button is pressed at boot.
    
    BUG=b:341975848
    TEST=On Brox.
    1. Cutoff by Refresh+Power button+Unplug AC.
    2. Held Refresh+Power button. EC does not crash.
    3. Boot to recovery mode by Refresh+Power button+Esc.
    
    Change-Id: I8737a85b36be11cae8edc5548702e5ac12a5aaf7

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -327,11 +327,10 @@ static int read_matrix(uint8_t *state, bool at_boot)
 {
 	int c;
 	int pressed = 0;
+	int pb_pressed;
 
 	/* 1. Read input pins */
 	for (c = 0; c < keyboard_cols; c++) {
-		int pb_pressed;
-
 		/*
 		 * Skip if scanning becomes disabled. Clear the state
 		 * to make sure we don't mix new and old states in the
@@ -350,6 +349,11 @@ static int read_matrix(uint8_t *state, bool at_boot)
 		keyboard_raw_drive_column(c);
 		udelay(keyscan_config.output_settle_us);
 
+		if (pb_pressed != power_button_raw_pressed()) {
+			c--;
+			continue;
+		}
+
 		/* Read the row state */
 #ifdef CONFIG_KEYBOARD_SCAN_ADC
 		state[c] = keyboard_read_adc_rows();
@@ -357,16 +361,17 @@ static int read_matrix(uint8_t *state, bool at_boot)
 		state[c] = keyboard_raw_read_rows();
 #endif
 
-		if (pb_pressed != power_button_raw_pressed()) {
-			c--;
-			continue;
-		} else if (pb_pressed) {
-			state[c] &= ~KEYBOARD_MASKED_BY_POWERBTN;
-		}
-
 		/* Use simulated keyscan sequence instead if testing active */
 		if (IS_ENABLED(CONFIG_KEYBOARD_TEST))
 			state[c] = keyscan_seq_get_scan(c, state[c]);
+	}
+
+	if (at_boot && pb_pressed) {
+		for (c = 0; c < keyboard_cols; c++) {
+			if (c == KEYBOARD_COL_REFRESH)
+				continue;
+			state[c] &= ~KEYBOARD_MASKED_BY_POWERBTN;
+		}
 	}
 
 #ifdef CONFIG_KEYBOARD_SCAN_ADC
