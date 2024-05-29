46a89d9f6d9cf14313790f798801304f2a9d2d4d
Devin Lu <Devin.Lu@quantatw.com>
2024-05-29 15:59:43 +0800

    Keyboard: Only apply Ti50 workaround at boot
    Improve crrev.com/c/4912024 only cancel pass-through rows while
    the power button is pressed at boot.
    
    BUG=b:341975848
    TEST=On Brox/Karis
    1. Cutoff by Refresh+Power button+Unplug AC.
    2. Held Refresh+Power button. EC does not crash.
    3. Boot to recovery mode by Refresh+Power button+Esc.
    
    Change-Id: I8737a85b36be11cae8edc5548702e5ac12a5aaf7

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -327,11 +327,12 @@ static int read_matrix(uint8_t *state, bool at_boot)
 {
 	int c;
 	int pressed = 0;
+	int pb_pressed;
+
+	pb_pressed = power_button_raw_pressed();
 
 	/* 1. Read input pins */
 	for (c = 0; c < keyboard_cols; c++) {
-		int pb_pressed;
-
 		/*
 		 * Skip if scanning becomes disabled. Clear the state
 		 * to make sure we don't mix new and old states in the
@@ -343,8 +344,6 @@ static int read_matrix(uint8_t *state, bool at_boot)
 			state[c] = 0;
 			continue;
 		}
-
-		pb_pressed = power_button_raw_pressed();
 
 		/* Select column, then wait a bit for it to settle */
 		keyboard_raw_drive_column(c);
@@ -360,13 +359,28 @@ static int read_matrix(uint8_t *state, bool at_boot)
 		if (pb_pressed != power_button_raw_pressed()) {
 			c--;
 			continue;
-		} else if (pb_pressed) {
-			state[c] &= ~KEYBOARD_MASKED_BY_POWERBTN;
 		}
 
 		/* Use simulated keyscan sequence instead if testing active */
 		if (IS_ENABLED(CONFIG_KEYBOARD_TEST))
 			state[c] = keyscan_seq_get_scan(c, state[c]);
+	}
+
+	if (pb_pressed && at_boot) {
+		/* Check if KSI2 (or KSI3) is asserted on all columns */
+		for (c = 0; c < keyboard_cols; c++) {
+			if (!(state[c] & KEYBOARD_MASKED_BY_POWERBTN)) {
+				break;
+			}
+		}
+
+		if (c == keyboard_cols) {
+			for (c = 0; c < keyboard_cols; c++) {
+				if (c == KEYBOARD_COL_REFRESH)
+					continue;
+				state[c] &= ~KEYBOARD_MASKED_BY_POWERBTN;
+			}
+		}
 	}
 
 #ifdef CONFIG_KEYBOARD_SCAN_ADC
