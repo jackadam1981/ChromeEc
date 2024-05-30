a64dee44f5a0a17143e4be106722c101ad841a38
Craig Hesling <hesling@chromium.org>
2024-05-30 15:22:06 -0700

    util: Use libec utility function for raw frame to PGM
    This new conversion utility support the two byte / 14bit pixel
    encoding used by Elan.
    
    BUG=b:331397247
    TEST=ectool --name=cros_fp fpframe
    
    Cq-Depend: chromium:5571485
    Change-Id: I8b40b33604d0da5b39c4653c0220a1664ae9c77e

--- util/ectool.cc
+++ util/ectool.cc
@@ -41,6 +41,7 @@
 #include <libec/ec_panicinfo.h>
 #include <libec/fingerprint/fp_encryption_status_command.h>
 #include <libec/fingerprint/fp_frame_command.h>
+#include <libec/fingerprint/fp_frame_utils.h>
 #include <libec/flash_protect_command.h>
 #include <libec/rand_num_command.h>
 #include <libec/versions_command.h>
@@ -2176,16 +2177,15 @@ int cmd_fp_frame(int argc, char *argv[])
 		return 0;
 	}
 
-	/* Print 8-bpp PGM ASCII header */
-	printf("P2\n%d %d\n%d\n", r.width, r.height, (1 << r.bpp) - 1);
-
-	uint8_t *ptr = fp_frame->data();
-	for (int y = 0; y < r.height; y++) {
-		for (int x = 0; x < r.width; x++, ptr++)
-			printf("%d ", *ptr);
-		printf("\n");
+	std::optional<std::string> pgm = ec::FpFrameBufferToPGM(
+		*fp_frame, static_cast<std::size_t>(r.width),
+		static_cast<std::size_t>(r.height),
+		static_cast<std::size_t>(r.bpp));
+	if (!pgm) {
+		fprintf(stderr, "Failed to convert FP frame to PGM\n");
+		return -1;
 	}
-	printf("# END OF FILE\n");
+	fwrite(pgm->c_str(), pgm->size(), 1, stdout);
 	return 0;
 }
 
