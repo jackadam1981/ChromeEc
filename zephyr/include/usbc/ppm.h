4aa9a69664c333e6c9d8b4400c1c43ce09035100
Abhishek Pandit-Subedi <abhishekpandit@google.com>
2024-05-30 11:26:34 -0700

    pdc: tests: Invalid UCSI messages set errors
    When sent an invalid UCSI message, the PPM should set an unrecognized
    command error message.
    
    BUG=b:340895744
    TEST=./twister -v -s pdc/pdc.ppm
    
    Change-Id: Iee4215aac3f582970536dcd198cffebbfc9b353c

--- zephyr/include/usbc/ppm.h
+++ zephyr/include/usbc/ppm.h
@@ -154,9 +154,9 @@ struct ucsi_memory_region {
 	struct ucsi_control control;
 	/* TODO - Message sizes depends on chunking support. */
 	/* May not need to be full 256. */
-	uint8_t message_in[MESSAGE_IN_SIZE]; /* OPM to PPM buffer */
+	uint8_t message_in[MESSAGE_IN_SIZE]; /* PPM output */
 	uint8_t reserved_0;
-	uint8_t message_out[MESSAGE_OUT_SIZE]; /* PPM to OPM buffer */
+	uint8_t message_out[MESSAGE_OUT_SIZE]; /* PPM input */
 	uint8_t reserved_1;
 } __attribute__((__packed__));
 
