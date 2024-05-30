bd4c4876cd79ae5c4cbe547c51696353c43e90b6
Boris Mittelberg <bmbm@google.com>
2024-05-30 16:14:03 -0700

    dedede: reduce USB_PD_INT_STORM_MAX
    reduce the max possible PD interrupt rate to avoid hitting
    watchdog on dedede, for IT8320 only
    
    BUG=b:254227082
    TEST=manual test on kracko/360
    
    Change-Id: I18fc3832f2fc72d17e8d1bd068d161b6b60bd6ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5586080
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Commit-Queue: Forest Mittelberg <bmbm@google.com>
    Tested-by: Forest Mittelberg <bmbm@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    (cherry picked from commit 2c3d068326d78e03467a93b600c5a8b721636615)

--- baseboard/dedede/baseboard.h
+++ baseboard/dedede/baseboard.h
@@ -237,6 +237,11 @@
 #define CONFIG_CMD_CHARGEN
 #endif
 
+#if defined(VARIANT_DEDEDE_EC_IT8320)
+#undef CONFIG_USB_PD_INT_STORM_MAX
+#define CONFIG_USB_PD_INT_STORM_MAX 280
+#endif
+
 /* Define typical operating power and max power. */
 #define PD_MAX_VOLTAGE_MV     20000
 #define PD_MAX_CURRENT_MA     3000
