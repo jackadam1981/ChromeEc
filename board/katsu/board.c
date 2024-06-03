48b6ad18ae84d37f04ad8228450abbddf71965f5
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2024-06-03 21:30:16 +0800

    katsu: Added feature to limit battery to 80%
    1. Modify battery capacity
    2. Simulate battery full charging behavior
    
    BUG=b:344542139
    TEST=make BOARD katsu
    
    Change-Id: I096eae2e24149c9bce25b3d9e73efd78a758b636

--- board/katsu/board.c
+++ board/katsu/board.c
@@ -42,6 +42,9 @@
 #include "usb_pd_tcpm.h"
 #include "util.h"
 
+/*Expand the a by 25%  =>  a * 125% */
+#define MULTIPLY_125(a) ((a * 5) / 4)
+
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ## args)
 #define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ## args)
 
@@ -422,3 +425,16 @@ static void mt6370_reg_fix(void)
 		    MASK_CLR);
 }
 DECLARE_HOOK(HOOK_INIT, mt6370_reg_fix, HOOK_PRIO_DEFAULT);
+
+__override void board_battery_compensate_params(struct batt_params *batt)
+{
+	/*
+	 * (b:344542139):Change the battery capacity and battery
+	 * state_of_charge to 125% of the original value.
+	 */
+	if (!strcasecmp(current_region_code.region_code, "tw")) {
+		batt->state_of_charge = MULTIPLY_125(batt->state_of_charge);
+		batt->remaining_capacity =
+			MULTIPLY_125(batt->remaining_capacity);
+	}
+}
