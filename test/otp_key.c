ffbe51c43e3b22d7d7183fd9d10ccaedf695d578
Tom Hughes <tomhughes@chromium.org>
2024-06-03 16:17:04 -0700

    test/otp_key: Test OTP key on hardware
    Only use the mock OTP implementation on when building for the host.
    When running on hardware, check that we can read the OTP bytes.
    
    BUG=b:323094297
    TEST=./test/run_device_tests.py --board helipilot -t otp_key
    
    Change-Id: I270a28a6ee51b4c9a8434f2ff9387cf53b454e9c

--- test/otp_key.c
+++ test/otp_key.c
@@ -17,7 +17,7 @@ void print_key_buffer(uint8_t *key_buff)
 	ccprintf("\n");
 }
 
-test_static int test_otp_key(void)
+test_static int test_otp_key_with_mock(void)
 {
 	otp_key_init();
 
@@ -37,8 +37,26 @@ test_static int test_otp_key(void)
 	return EC_SUCCESS;
 }
 
+test_static int test_otp_key_read(void)
+{
+	otp_key_init();
+
+	uint8_t otp_key_buffer[OTP_KEY_SIZE_BYTES] = { 0 };
+	const uint32_t status = otp_key_read(otp_key_buffer);
+	TEST_EQ(status, EC_SUCCESS, "%d");
+
+	TEST_ASSERT(!bytes_are_trivial(otp_key_buffer, OTP_KEY_SIZE_BYTES));
+
+	otp_key_exit();
+
+	return EC_SUCCESS;
+}
+
 void run_test(int argc, const char **argv)
 {
-	RUN_TEST(test_otp_key);
+	if (IS_ENABLED(HAS_MOCK_OTPI)) {
+		RUN_TEST(test_otp_key_with_mock);
+	}
+	RUN_TEST(test_otp_key_read);
 	test_print_result();
 }
