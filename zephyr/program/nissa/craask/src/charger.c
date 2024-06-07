cb32132c2712d70ee7557c5fe81d827c833d772d
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2024-06-07 19:12:05 +0800

    Craask: workaround for EC sometimes cannot read battery param
    Sometimes EC cannot read Manuf/Device name after BATT_FLAG_RESPONSIVE
    is set. The default configuration is selected and won't be retried
    before BATT_FLAG_RESPONSIVE is cleared. EC will use wrong command to
    check the DFET/CFET status. The battery would always show disconnected.
    Therefore, retied init_battery_type if the battery is found in
    disconnected state.
    
    BUG=none
    TEST=zmake build craask --clobber
         "./twister -T zephyr/test/nissa/ -v -i -s nissa.craask" pass
    
    Change-Id: I03aa15e715c036129579ca796c634bf779ae8166

--- zephyr/program/nissa/craask/src/charger.c
+++ zephyr/program/nissa/craask/src/charger.c
@@ -6,6 +6,7 @@
 #include "battery.h"
 #include "battery_fuel_gauge.h"
 #include "charge_manager.h"
+#include "charge_state.h"
 #include "charger.h"
 #include "charger/isl923x_public.h"
 #include "console.h"
@@ -72,7 +73,54 @@ __override int board_get_leave_safe_mode_delay_ms(void)
 		return 500;
 }
 
-void update_charger_config(void)
+#define CHECK_BATT_STAT_DELAY_MS (500 * MSEC)
+static int check_batt_retry;
+
+void board_check_battery_status(void)
+{
+	enum battery_disconnect_state battery_disconnect_status =
+		battery_get_disconnect_state();
+
+	/*
+	 * The following 2 states can read DFET status successfully
+	 * so not need to do initialize battery type again.
+	 * BATTERY_DISCONNECTED: The DFET is off.
+	 * BATTERY_NOT_DISCONNECTED: The battery can discharge.
+	 * Therefore, do initilialize only at BATTERY_DISCONNECT_ERROR.
+	 */
+	if (battery_disconnect_status != BATTERY_DISCONNECT_ERROR) {
+		check_batt_retry = 0;
+		return;
+	}
+
+	check_batt_retry++;
+	if (check_batt_retry > 5) {
+		LOG_INF("Board has retried init_battery_type 5 times.");
+		check_batt_retry = 0;
+		return;
+	}
+
+	LOG_INF("Retry init_battery_type: %d", check_batt_retry);
+	init_battery_type();
+}
+DECLARE_DEFERRED(board_check_battery_status);
+
+__override int board_get_default_battery_type(void)
+{
+	const struct batt_params *batt = charger_current_battery_params();
+
+	if (batt->flags & BATT_FLAG_RESPONSIVE) {
+		/* Check Battery status again after 500msec. */
+		hook_call_deferred(&board_check_battery_status_data,
+				   CHECK_BATT_STAT_DELAY_MS);
+	} else {
+		check_batt_retry = 0;
+	}
+
+	return DEFAULT_BATTERY_TYPE;
+}
+
+test_export_static void update_charger_config(void)
 {
 	uint32_t board_version;
 	int ret;
