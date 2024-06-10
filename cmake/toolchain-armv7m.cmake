a2648cce191f704246b8955ad0cba3da2ab35fed
Bob Haarman <inglorion@chromium.org>
2024-06-10 22:52:20 +0000

    cmake: Depend on libclang_rt.builtins-armv7m.lib by full path
    The directory in which libclang_rt.builtins-armv7m.lib resides is no
    longer automatically added to the search path in newer Clang
    versions. As a result, -lclang_rt.builtins-armv7m fails to find it. With
    this change, we instead pass the full path to the library as an input
    file. This will cause the library to be used for symbol resolution,
    without adding its location to the search path.
    
    BUG=b:346309204
    TEST=build chromeos-base/chromeos-fpmcu-bloonchipper-unittests
    
    Change-Id: I513e702275790799e9c561affecafb7969b669de

--- cmake/toolchain-armv7m.cmake
+++ cmake/toolchain-armv7m.cmake
@@ -17,5 +17,9 @@ include("${CMAKE_CURRENT_LIST_DIR}/toolchain-common.cmake")
 add_compile_options(-mcpu=cortex-m4)
 add_compile_options(-mfloat-abi=hard)
 
-add_link_options(-lclang_rt.builtins-armv7m)
+# libclang_rt.builtins is automatically linked in, but by mentioning it
+# explicitly, we cause it to be used for symbol resolution before newlib's
+# libc. This avoids some duplicate symbol errors we would otherwise get.
+# For more details, b/346309204
+add_link_options(/usr/lib64/clang/19/lib/baremetal/libclang_rt.builtins-armv7m.a)
 add_link_options(-lnosys)
