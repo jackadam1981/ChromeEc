e798028277d9b8cea6fa41a487b27d2f8aee9567
Tom Hughes <tomhughes@chromium.org>
2024-06-11 11:44:40 -0700

    test/fpsensor_auth_commands: Run test on device
    BUG=b:336312700
    TEST=none
    
    Change-Id: I26dd9aac6e1c0a4da70431d41a8aba291f1aa12c

--- test/fpsensor_auth_commands.cc
+++ test/fpsensor_auth_commands.cc
@@ -18,6 +18,7 @@
 #include "sha256.h"
 #include "test_util.h"
 #include "util.h"
+#include "watchdog.h"
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -672,6 +673,15 @@ test_fp_command_read_match_secret_with_pubkey_succeed(void)
 	};
 
 	/* Expected positive_match_secret same as  in test/fpsensor_crypto.c */
+#ifdef CONFIG_OTP_KEY
+	static const uint8_t
+		expected_positive_match_secret_for_empty_user_id[] = {
+			0x2f, 0x78, 0x2d, 0xd2, 0x0a, 0xa9, 0xa2, 0x17,
+			0xc6, 0x4d, 0xa3, 0x1a, 0x02, 0xef, 0x4e, 0x2c,
+			0xf9, 0x23, 0xe1, 0x2d, 0x12, 0x3e, 0xa9, 0xe3,
+			0xc9, 0x16, 0x6f, 0x98, 0x39, 0x8b, 0x0e, 0xc5,
+		};
+#else
 	static const uint8_t
 		expected_positive_match_secret_for_empty_user_id[] = {
 			0x8d, 0xc4, 0x5b, 0xdf, 0x55, 0x1e, 0xa8, 0x72,
@@ -679,6 +689,7 @@ test_fp_command_read_match_secret_with_pubkey_succeed(void)
 			0xde, 0x38, 0xd5, 0x03, 0xce, 0xe4, 0x74, 0x51,
 			0x63, 0x6c, 0x6a, 0x26, 0xa9, 0xb7, 0xfa, 0x68,
 		};
+#endif
 
 	/* Create positive secret match state with valid deadline value,
 	 * readable state, and correct template matched
@@ -1595,6 +1606,9 @@ void run_test(int argc, const char **argv)
 	RUN_TEST(test_fp_command_nonce_context_deny);
 	RUN_TEST(test_fp_command_nonce_context_limit_without_generated_nonce);
 	RUN_TEST(test_fp_command_nonce_context_limit_normal_context);
+
+	watchdog_reload();
+
 	RUN_TEST(test_fp_command_nonce_context_limit_twice_1);
 	RUN_TEST(test_fp_command_nonce_context_limit_twice_2);
 	RUN_TEST(test_fp_command_nonce_context_load_pk_deny);
@@ -1606,6 +1620,9 @@ void run_test(int argc, const char **argv)
 	RUN_TEST(test_fp_command_unlock_template_pre_encrypted);
 	RUN_TEST(test_fp_command_commit_v3);
 	RUN_TEST(test_fp_command_commit_trivial_salt);
+
+	watchdog_reload();
+
 	RUN_TEST(test_fp_command_migrate_template_to_nonce_context);
 	RUN_TEST(test_fp_command_migrate_template_to_nonce_context_failure);
 	test_print_result();
