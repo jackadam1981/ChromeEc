9315c71b6955a3772249a5e31e6aa5367b3dd179
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2024-06-12 17:04:42 +0800

    squirtle: Add battery detection mechanism
    Add PF(Permanently Failure) bit judgment, when PF=1,
    it means the battery is damaged.
    
    BUG=b:345163724
    TEST=zmake build squirtle
    LOW_COVERAGE_REASON=Tests will be added in b:345163724
    
    Change-Id: I9a23250276501433fdf7d387a3aa19b793f3f862

--- zephyr/program/corsola/squirtle/src/board.c
+++ zephyr/program/corsola/squirtle/src/board.c
@@ -3,13 +3,16 @@
  * found in the LICENSE file.
  */
 
+#include "battery.h"
 #include "charge_state.h"
 #include "common.h"
 #include "dps.h"
 #include "gpio.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
+#include "i2c.h"
 #include "math_util.h"
+#include "util.h"
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
@@ -43,3 +46,27 @@ __override struct dps_config_t dps_config = {
 	.t_check = 5 * SECOND,
 	.is_more_efficient = &squirtle_is_more_efficient,
 };
+
+enum battery_present battery_is_present(void)
+{
+	int rv;
+	struct battery_static_info *bs = &battery_static[BATT_IDX_MAIN];
+
+	rv = gpio_get_level(GPIO_BATT_PRES_ODL) ? BP_NO : BP_YES;
+
+	if (!strcasecmp(bs->model_ext, "AP23A7L")) {
+		uint8_t state[7];
+		i2c_write32(I2C_PORT_BATTERY, BATTERY_ADDR_FLAGS, 0x00,
+			    0x54004b);
+		rv &= !(i2c_read_string(I2C_PORT_BATTERY, BATTERY_ADDR_FLAGS,
+					0x23, state, 7));
+		rv &= !((state[1] & BIT(4)) >> 4);
+	} else if (!strcasecmp(bs->model_ext, "AP23A8L")) {
+		int state;
+		rv &= !(sb_read(SB_PACK_STATUS, &state));
+		rv &= !((state & BIT(2)) >> 2);
+	} else {
+		rv = BP_NO;
+	}
+	return rv;
+}
