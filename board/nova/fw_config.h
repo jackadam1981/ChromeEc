865c391cc440fb92cfb62c0367ebe6f9b8110990
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2024-06-13 13:28:24 +0800

    nova: Update FW_CONFIG field
    Update FW_CONFIG field
    
    BUG=none
    TEST=make BOARD=nova
    
    Change-Id: I9073b69a641432ace29186700e981f0eb9e03a85

--- board/nova/fw_config.h
+++ board/nova/fw_config.h
@@ -15,13 +15,10 @@
  */
 enum ec_cfg_audio_type { DB_AUDIO_UNKNOWN = 0, DB_NAU88L25B_I2S = 1 };
 
-enum ec_cfg_bj_power { BJ_135W = 0, BJ_230W = 1 };
-
 union brask_cbi_fw_config {
 	struct {
 		uint32_t audio : 3;
-		uint32_t bj_power : 2;
-		uint32_t reserved_1 : 27;
+		uint32_t reserved_1 : 29;
 	};
 	uint32_t raw_value;
 };
@@ -32,10 +29,5 @@ union brask_cbi_fw_config {
  * @return the FW_CONFIG for the board.
  */
 union brask_cbi_fw_config get_fw_config(void);
-
-/**
- * Get the barrel-jack power from FW_CONFIG.
- */
-void ec_bj_power(uint32_t *voltage, uint32_t *current);
 
 #endif /* __BOARD_BRASK_FW_CONFIG_H_ */
