07264602481ac9266b8b633c2c2d1b7ba57fe853
Tom Hughes <tomhughes@chromium.org>
2024-06-18 15:56:16 -0700

    chip/npcx: Document and validate firmware header size
    BUG=b:321739758
    TEST=CQ passes
    
    Change-Id: I9a3f436ea58ef520752cf1c91c615035fa189620

--- chip/npcx/header.c
+++ chip/npcx/header.c
@@ -56,6 +56,7 @@ struct __packed fw_header_t {
 	uint32_t sig_header; /* The CRC signature of the firmware header  */
 	uint32_t sig_fw_image; /* The CRC or Checksum of the firmware image */
 } __aligned(1);
+BUILD_ASSERT(sizeof(struct fw_header_t) == 64);
 
 __keep __attribute__((section(".header")))
 const struct fw_header_t fw_header = {
