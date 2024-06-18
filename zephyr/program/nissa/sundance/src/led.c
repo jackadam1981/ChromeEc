32bdf3f1ca2f41645f66a48befeb98f01f2e5dda
amber.chen <amber.chen@lcfc.corp-partner.google.com>
2024-06-18 03:26:47 -0400

    sundance: modify LED behavior for LED spec
    Modify LED behavior for LED spec
    Connect AC, the battery LED of amber will light on when system in S0/S0ix/S5
    
    BUG=b:341975849
    TEST=zmake build sundance  can pass
    under AC,
                       S0        S0ix/S3        S4/S5
    battery charging, amber on   amber on      amber on
    full charging,    white on      off          off
    battery error,    white and amber blinking 1s on -> 2s off
    under battery,    white on      off          off
    
    Change-Id: Ia81b5a3ab92a82dd7f82e4c1f6708319c7591408

--- zephyr/program/nissa/sundance/src/led.c
+++ zephyr/program/nissa/sundance/src/led.c
@@ -16,6 +16,7 @@
 #include "chipset.h"
 #include "common.h"
 #include "ec_commands.h"
+#include "extpower.h"
 #include "led_common.h"
 #include "led_onoff_states.h"
 
@@ -33,6 +34,7 @@ __override struct led_descriptor
 					     LED_INDEFINITE } },
 		[STATE_CHARGING_FULL_CHARGE] = { { EC_LED_COLOR_WHITE,
 						   LED_INDEFINITE } },
+		[STATE_CHARGING_FULL_S5] = { { LED_OFF, LED_INDEFINITE } },
 		[STATE_DISCHARGE_S0] = { { EC_LED_COLOR_WHITE,
 					   LED_INDEFINITE } },
 		[STATE_DISCHARGE_S3] = { { LED_OFF, LED_INDEFINITE } },
@@ -97,10 +99,16 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 
 __override enum led_states board_led_get_state(enum led_states desired_state)
 {
-	if (chipset_in_state(CHIPSET_STATE_ON))
-		return desired_state;
-	else if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND))
-		return STATE_DISCHARGE_S3;
-	else
-		return STATE_DISCHARGE_S5;
+	if (desired_state == STATE_CHARGING_FULL_CHARGE) {
+		if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND |
+				     CHIPSET_STATE_ANY_OFF)) {
+			if (extpower_is_present()) {
+				desired_state = STATE_CHARGING_LVL_2;
+			} else
+				desired_state = STATE_CHARGING_FULL_S5;
+		} else
+			return desired_state;
+	}
+
+	return desired_state;
 }
