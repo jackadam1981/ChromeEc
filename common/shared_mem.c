2af890c1058bb21190b1fd6cad50b63a118941c6
Dawid Niedzwiecki <dawidn@google.com>
2024-06-20 10:28:01 +0200

    tests: define __shared_mem_buf for tests
    Test uses the fake shared memory. Define the __shared_mem_buf
    for tests as the beginning of the fake shared memory buffer.
    
    BUG=b:324574796
    TEST=zmake build -a && make buildall
    
    Change-Id: I506d78bac2719b17f6f3dd4684bf9f11cea12bd8

--- common/shared_mem.c
+++ common/shared_mem.c
@@ -23,7 +23,10 @@ static int buf_in_use;
 static int max_used;
 
 /* 1 MB buffer for fake shared memory implementation */
-STATIC_IF(CONFIG_FAKE_SHMEM) char fake_shmem_buf[BIT(20)];
+#ifndef CONFIG_FAKE_SHMEM
+extern
+#endif /* CONFIG_FAKE_SHMEM */
+char fake_shmem_buf[BIT(20)];
 
 int shared_mem_size(void)
 {
