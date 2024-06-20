3f6ad50539845a51b44c8501568931e28bade81d
kiwi liu <kiwi.liu@mediatek.corp-partner.google.com>
2024-06-20 08:38:48 +0800

    WIP: mt8188: Add mt8188 project
    Add mt8188 project
    
    TEST:  zmake build geralt-scp-zephyr --clobber
    Change-Id: Ic76fff8265e7ef1709a40127a61f1c719368de4a

--- zephyr/zmake/zmake/output_packers.py
+++ zephyr/zmake/zmake/output_packers.py
@@ -139,6 +139,12 @@ class IshBinPacker(BasePacker):
         del version_string
         yield dir_map["singleimage"] / "zephyr" / "ish_fw.bin", "ish_fw.bin"
 
+class MtkBinPacker(BasePacker):
+    """Raw proxy for Mtk_fw.bin output of a single build."""
+
+    def pack_firmware(self, work_dir, jobclient, dir_map, version_string=""):
+        del version_string
+        yield dir_map["singleimage"] / "zephyr" / "zephyr.bin", "ec.bin"
 
 class BinmanPacker(BasePacker):
     """Packer for RO/RW image to generate a .bin build using FMAP."""
