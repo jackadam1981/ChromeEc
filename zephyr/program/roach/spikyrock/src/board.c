5c9f1494d684951cc86d172469d708fd1e9fbf30
Ren Chen <Ren.Chen@ite.com.tw>
2024-06-27 13:21:38 +0800

    WIP: spikyrock: enable I2C transaction over USB option
    To support touchpad firmware update, This commit enable
    PLATFORM_EC_USB_I2C option.
    
    BUG=b:349503766
    TEST=
    
    Change-Id: Ib9f6c3905e9795de51af24fa390f2aa37de6bda0

---
+++ zephyr/program/roach/spikyrock/src/board.c
@@ -0,0 +1,10 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+int usb_i2c_board_is_enabled(void)
+{
+	// TODO: how to?
+	return 1;
+}
