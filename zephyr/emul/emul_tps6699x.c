d9178e3f8d6d06000605448b232e7b3d31f9521d
Abe Levkoy <alevkoy@chromium.org>
2024-06-28 16:42:23 -0600

    emul_tps6699x: Implement common I2C emul functions
    Allow callers to read and write I2C registers, very basically. Avoid
    segfault due to partial pdc_power_mgmt subsys init.
    
    BUG=b:345292002
    TEST=twister -T pdc/pdc.generic.tps6699x
    
    Change-Id: Iec90f817470a4c602c6cd5358ee7ff89cc58d1a6

--- zephyr/emul/emul_tps6699x.c
+++ zephyr/emul/emul_tps6699x.c
@@ -9,6 +9,7 @@
 #include "usbc/utils.h"
 
 #include <stdint.h>
+#include <string.h>
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/emul.h>
@@ -44,42 +45,110 @@ tps6699x_emul_get_pdc_data(const struct emul *emul)
 
 static int tps6699x_emul_start_write(const struct emul *emul, int reg)
 {
+	struct tps6699x_emul_pdc_data *data = tps6699x_emul_get_pdc_data(emul);
+
+	LOG_DBG("start_write reg=%#x", reg);
+
+	if (reg > sizeof(data->reg_val) / sizeof(*data->reg_val)) {
+		return -EIO;
+	}
+
+	memset(&data->reg_val[reg], 0, sizeof(data->reg_val[reg]));
+
+	data->reg_addr = reg;
+
 	return 0;
 }
 
 static int tps6699x_emul_write_byte(const struct emul *emul, int reg,
 				    uint8_t val, int bytes)
 {
+	struct tps6699x_emul_pdc_data *data = tps6699x_emul_get_pdc_data(emul);
+
+	LOG_DBG("write_byte reg=%#x, val=%#02x, bytes=%d", reg, val, bytes);
+
+	/* TODO(b/345292002): Validate more meaningfully that register accesses
+	 * are valid.
+	 */
+	if (reg > sizeof(data->reg_val) / sizeof(*data->reg_val) ||
+	    bytes > sizeof(*data->reg_val)) {
+		return -EIO;
+	}
+
+	data->reg_val[reg][bytes] = val;
+
 	return 0;
 }
 
 static int tps6699x_emul_finish_write(const struct emul *emul, int reg,
 				      int bytes)
 {
+	LOG_DBG("finish_write reg=%#x, bytes=%d", reg, bytes);
+
+	/* TODO(b/345292002): Actually handle register accesses. */
+
 	return 0;
 }
 
 static int tps6699x_emul_start_read(const struct emul *emul, int reg)
 {
+	struct tps6699x_emul_pdc_data *data = tps6699x_emul_get_pdc_data(emul);
+
+	LOG_DBG("start_read reg=%#x", reg);
+
+	if (reg > sizeof(data->reg_val) / sizeof(*data->reg_val)) {
+		return -EIO;
+	}
+
+	data->reg_addr = reg;
+
 	return 0;
 }
 
 static int tps6699x_emul_read_byte(const struct emul *emul, int reg,
 				   uint8_t *val, int bytes)
 {
+	struct tps6699x_emul_pdc_data *data = tps6699x_emul_get_pdc_data(emul);
+
+	/* TODO(b/345292002): Validate more meaningfully that register accesses
+	 * are valid.
+	 */
+	if (reg > sizeof(data->reg_val) / sizeof(*data->reg_val) ||
+	    bytes > sizeof(*data->reg_val)) {
+		return -EIO;
+	}
+
+	/*
+	 * Response byte 0 is always the number of bytes read.
+	 * Remaining bytes are read starting at offset.
+	 * Note that the byte following the number of bytes is
+	 * considered to be at offset 0.
+	 */
+	if (bytes == 0) {
+		*val = bytes;
+	} else {
+		*val = data->reg_val[reg][bytes];
+	}
+
+	LOG_DBG("read_byte reg=%#x, bytes=%d, val=%#02x", reg, bytes, *val);
+
 	return 0;
 }
 
 static int tps6699x_emul_finish_read(const struct emul *emul, int reg,
 				     int bytes)
 {
+	LOG_DBG("finish_read reg=%#x, bytes=%d", reg, bytes);
+
+	/* TODO(b/345292002): Actually handle register accesses. */
+
 	return 0;
 }
 
 static int tps6699x_emul_access_reg(const struct emul *emul, int reg, int bytes,
 				    bool read)
 {
-	return 0;
+	return reg;
 }
 
 static int emul_tps669x_set_response_delay(const struct emul *target,
