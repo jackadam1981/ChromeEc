1cd683bd07678087fb06e2877295a27479f4688f
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2024-07-02 14:12:30 +0800

    riven: Dynamic changing the max voltage of VBUS
    Set the max requested voltage to 15V when battery is full.
    
    BUG=b:346473280
    TEST=1. Console command "chgsup" shows 15000mV when the
            battery is full charged on Craask with 65W adapter.
         2. "./twister -T zephyr/test/nissa/ -v -i -s nissa.riven"
            pass
    
    Change-Id: Ifc5af67ab70882e6db6815791e3e9db59c0f8d4f

--- zephyr/program/nissa/riven/src/usbc.c
+++ zephyr/program/nissa/riven/src/usbc.c
@@ -212,3 +212,42 @@ __override void nissa_configure_hdmi_power_gpios(void)
 	pinctrl_apply_state(pcfg, PINCTRL_STATE_SLEEP);
 }
 #endif /* DT_NODE_EXISTS(I2C5_1_NODE) */
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	static int saved_input_voltage = -1;
+	int max_pd_voltage_mv = pd_get_max_voltage();
+	int port;
+
+	if (curr->state != ST_CHARGE)
+		return 0;
+
+	/* Lower the max requested voltage to 15V when battery is full. */
+	if (curr->batt.status & STATUS_FULLY_CHARGED) {
+		saved_input_voltage = max_pd_voltage_mv;
+		max_pd_voltage_mv = 15000;
+	} else if (saved_input_voltage != -1) {
+		max_pd_voltage_mv = saved_input_voltage;
+		saved_input_voltage = -1;
+	}
+
+	if (pd_get_max_voltage() != max_pd_voltage_mv) {
+		LOG_INF("VBUS limited to %dmV", max_pd_voltage_mv);
+		for (port = 0; port < CONFIG_USB_PD_PORT_MAX_COUNT; port++)
+			pd_set_external_voltage_limit(port, max_pd_voltage_mv);
+	}
+
+	return 0;
+}
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
