d6ed65de0f60449065b53b539e59f6ff110c7864
Jon Murphy <jpmurphy@google.com>
2024-07-11 14:13:57 -0600

    ish: Compile using coreboot-sdk from subtool
    This enables building ish using coreboot-sdk from
    subtool when using make.
    
    BUG=b:338458418
    TEST=make
    BRANCH=none
    
    Change-Id: Iea9c11f9e9d29dcb3608a4896ce03575e2b00d2e
    Signed-off-by: Jon Murphy <jpmurphy@google.com>

--- core/minute-ia/build.mk
+++ core/minute-ia/build.mk
@@ -6,9 +6,29 @@
 # Minute-IA core build
 #
 
+# coreboot sdk
+TOOLCHAIN=i386-elf
+TOOLCHAIN_VERSION=11.3.0-r2
+TOOLCHAIN_HASH=fb03ed1518de7dfd962f6e7cb95e1f7ef3e5e0f0
+
+TOOLCHAIN_INSTALL_PATH=/opt/coreboot-sdk/${TOOLCHAIN}/${TOOLCHAIN_VERSION}/${TOOLCHAIN_HASH}
+CROSS_COMPILE_X86_DEFAULT:=${TOOLCHAIN_INSTALL_PATH}/bin/${TOOLCHAIN}-
+COREBOOT_SDK_URI_PATH=https://storage.googleapis.com/chromiumos-sdk/toolchains/coreboot-sdk
+TARBALL=${TOOLCHAIN_HASH}.tar.zst
+TOOLCHAIN_URI=${COREBOOT_SDK_URI_PATH}-${TOOLCHAIN}/${TOOLCHAIN_VERSION}/${TARBALL}
+
+toolchain_status := $(shell	if [[ ! -d $(TOOLCHAIN_INSTALL_PATH) ]]; then \
+		sudo mkdir -p $(TOOLCHAIN_INSTALL_PATH) && \
+		sudo chmod 777 $(TOOLCHAIN_INSTALL_PATH) && \
+		pushd $(TOOLCHAIN_INSTALL_PATH) && \
+		curl -L -O $(TOOLCHAIN_URI) && \
+		sudo tar -I pzstd --no-same-owner -xf "./$(TARBALL)" && \
+		popd; \
+	fi )
+
 # Select Minute-IA bare-metal toolchain
 $(call set-option,CROSS_COMPILE,$(CROSS_COMPILE_i386),\
-	/opt/coreboot-sdk/bin/i386-elf-)
+	$(CROSS_COMPILE_X86_DEFAULT))
 
 # FPU compilation flags
 CFLAGS_FPU-$(CONFIG_FPU)=
