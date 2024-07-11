7db2bc73fd6034d71ca4ef54b2870b00b03a34b0
Jon Murphy <jpmurphy@google.com>
2024-07-11 12:09:00 -0600

    ec: Compile using coreboot-sdk from subtool
    This enables building ec using coreboot-sdk from
    subtool when using make.
    
    BUG=b:338458418
    TEST=make
    
    Cq-Depend: chromium:5703480
    Change-Id: Ib31d3db9a779a384c703fba0515fee354acee7be

--- core/minute-ia/toolchain.mk
+++ core/minute-ia/toolchain.mk
@@ -2,12 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-ifeq ($(CROSS_COMPILE_CC_NAME),clang)
+ifeq ($(CROSS_COMPILE_CC_NAME),gcc)
 # llvm sdk
 CROSS_COMPILE_X86_DEFAULT:=
 else
 # coreboot sdk
-CROSS_COMPILE_X86_DEFAULT:=/opt/coreboot-sdk/bin/i386-elf-
+CROSS_COMPILE_X86_DEFAULT:=i386-elf
+USE_COREBOOT_SDK:=1
 endif
 
 $(call set-option,CROSS_COMPILE,\
