a1c0670fa24a7fbf8a0959db119449d6f31fa636
ben chen <ben.chen2@quanta.corp-partner.google.com>
2024-07-11 15:42:30 +0800

    magolor: workaround for EC sometimes cannot read battery param
    Sometimes EC cannot read Manuf/Device name after BATT_FLAG_RESPONSIVE
    is set. The default configuration is selected and won't be retried
    before BATT_FLAG_RESPONSIVE is cleared. EC will use wrong command to
    check the DFET/CFET status. The battery would always show disconnected.
    Therefore, retied init_battery_type if the battery is found in
    disconnected state.
    
    BUG=b:350886367
    TEST=make BOARD=magolor pass
    
    Change-Id: I6a43993d88a849df11faa9449a349a047918bddc

--- board/magolor/battery.c
+++ board/magolor/battery.c
@@ -6,10 +6,16 @@
  */
 #include "battery.h"
 #include "battery_fuel_gauge.h"
+#include "charge_state.h"
 #include "common.h"
 #include "gpio.h"
+#include "hooks.h"
 #include "util.h"
 
+#define CHECK_BATT_STAT_DELAY_MS (150 * MSEC)
+
+#define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_USBCHARGE, format, ##args)
 /*
  * Battery info for magolor battery types. Note that the fields
  * start_charging_min/max and charging_min/max are not used for the charger.
@@ -262,3 +268,50 @@ const struct batt_conf_embed board_battery_info[] = {
 BUILD_ASSERT(ARRAY_SIZE(board_battery_info) == BATTERY_TYPE_COUNT);
 
 const enum battery_type DEFAULT_BATTERY_TYPE = BATTERY_LGC_AP18C8K;
+
+#ifdef BOARD_MAGOLOR
+static int check_batt_retry;
+void board_check_battery_status(void)
+{
+	enum battery_disconnect_state battery_disconnect_status =
+		battery_get_disconnect_state();
+
+	/*
+	 * The following 2 states can read DFET status successfully
+	 * so not need to do initialize battery type again.
+	 * BATTERY_DISCONNECTED: The DFET is off.
+	 * BATTERY_NOT_DISCONNECTED: The battery can discharge.
+	 * Therefore, do initilialize only at BATTERY_DISCONNECT_ERROR.
+	 */
+	if (battery_disconnect_status != BATTERY_DISCONNECT_ERROR) {
+		check_batt_retry = 0;
+		return;
+	}
+
+	check_batt_retry++;
+	if (check_batt_retry > 5) {
+		CPRINTS("Board has retried init_battery_type 5 times.");
+		check_batt_retry = 0;
+		return;
+	}
+
+	CPRINTS("Retry init_battery_type: %d", check_batt_retry);
+	init_battery_type();
+}
+DECLARE_DEFERRED(board_check_battery_status);
+
+__override int board_get_default_battery_type(void)
+{
+	const struct batt_params *batt = charger_current_battery_params();
+
+	if (batt->flags & BATT_FLAG_RESPONSIVE) {
+		/* Check Battery status again after 500msec. */
+		hook_call_deferred(&board_check_battery_status_data,
+				   CHECK_BATT_STAT_DELAY_MS);
+	} else {
+		check_batt_retry = 0;
+	}
+
+	return DEFAULT_BATTERY_TYPE;
+}
+#endif
