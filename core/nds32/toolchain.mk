c46183acf4dfc2d804e4176ad0aa0bc03a56ac06
Jon Murphy <jpmurphy@google.com>
2024-07-11 12:09:00 -0600

    ec: Compile using coreboot-sdk from subtool
    This enables building ec using coreboot-sdk from
    subtool when using make.
    
    BUG=b:338458418
    TEST=make
    
    Cq-Depend: chromium:5703480
    Change-Id: Ib31d3db9a779a384c703fba0515fee354acee7be

--- core/nds32/toolchain.mk
+++ core/nds32/toolchain.mk
@@ -6,7 +6,8 @@
 CROSS_COMPILE_CC_NAME:=gcc
 
 # Set coreboot-sdk as the default toolchain for nds32
-NDS32_DEFAULT_COMPILE=/opt/coreboot-sdk/bin/nds32le-elf-
+NDS32_DEFAULT_COMPILE:=nds32
+USE_COREBOOT_SDK:=1
 
 # Select Andes bare-metal toolchain
 $(call set-option,CROSS_COMPILE,$(CROSS_COMPILE_nds32),$(NDS32_DEFAULT_COMPILE))
