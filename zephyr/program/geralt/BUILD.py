51d97a93545f5ddc10c087f75883374cddca96df
kiwi liu <kiwi.liu@mediatek.corp-partner.google.com>
2024-07-18 14:02:37 +0800

    WIP: mt8188: Add mt8188 project
    Add mt8188 project
    
    TEST:  zmake build geralt-scp-zephyr --clobber
    Change-Id: Ic76fff8265e7ef1709a40127a61f1c719368de4a

--- zephyr/program/geralt/BUILD.py
+++ zephyr/program/geralt/BUILD.py
@@ -20,6 +20,17 @@ def register_geralt_project(
         inherited_from=["geralt"],
     )
 
+register_mtkscp_project(
+    project_name="geralt-scp-zephyr",
+    zephyr_board="mt8188",
+    dts_overlays=[
+        here / "geralt-scp-zephyr" / "project.overlay",
+    ],
+    kconfig_files=[
+        here / "geralt-scp-zephyr" / "project.conf",
+    ],
+)
+
 
 geralt = register_geralt_project(project_name="geralt")
 ciri = register_geralt_project(project_name="ciri")
