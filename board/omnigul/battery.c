cfb2db0da53fb8535fcafd5aa6ae9aeb19c4de6e
Ridden_Liu <ridden_liu@compal.corp-partner.google.com>
2024-07-19 09:19:04 +0800

    (DO NOT MERGE)Omnigul:add SMBUS PEC support and set NACK retry
    Sometimes the battery doesn't response. EC receives NACK immediately
    after EC write data. The gauge might not receive the data so add
    retries to give gauge chance to get the data.
    
    BUG=none
    TEST=check function work normal
    
    Change-Id: I119e87aaa5245c42a19ffeaf75ce6f647f63a548

--- board/omnigul/battery.c
+++ board/omnigul/battery.c
@@ -5,11 +5,16 @@
  * Battery pack vendor provided charging profile
  */
 
+#include "battery.h"
 #include "battery_fuel_gauge.h"
 #include "cbi.h"
+#include "charge_state.h"
 #include "common.h"
 #include "compile_time_macros.h"
 #include "gpio.h"
+#include "hooks.h"
+#include "timer.h"
+
 /*
  * Battery info for all Brya battery types. Note that the fields
  * start_charging_min/max and charging_min/max are not used for the charger.
@@ -176,4 +181,51 @@ enum battery_present battery_hw_present(void)
 
 	/* The GPIO is low when the battery is physically present */
 	return gpio_get_level(batt_pres) ? BP_NO : BP_YES;
+}
+
+#define CHECK_BATT_STAT_DELAY_MS (500 * MSEC)
+static int check_batt_retry;
+
+void board_check_battery_status(void)
+{
+	enum battery_disconnect_state battery_disconnect_status =
+		battery_get_disconnect_state();
+
+	/*
+	 * The following 2 states can read DFET status successfully
+	 * so not need to do initialize battery type again.
+	 * BATTERY_DISCONNECTED: The DFET is off.
+	 * BATTERY_NOT_DISCONNECTED: The battery can discharge.
+	 * Therefore, do initilialize only at BATTERY_DISCONNECT_ERROR.
+	 */
+	if (battery_disconnect_status != BATTERY_DISCONNECT_ERROR) {
+		check_batt_retry = 0;
+		return;
+	}
+
+	check_batt_retry++;
+	if (check_batt_retry > 5) {
+		ccprints("Board has retried init_battery_type 5 times.");
+		check_batt_retry = 0;
+		return;
+	}
+
+	ccprints("Retry init_battery_type: %d", check_batt_retry);
+	init_battery_type();
+}
+DECLARE_DEFERRED(board_check_battery_status);
+
+__override int board_get_default_battery_type(void)
+{
+	const struct batt_params *batt = charger_current_battery_params();
+
+	if (batt->flags & BATT_FLAG_RESPONSIVE) {
+		/* Check Battery status again after 500msec. */
+		hook_call_deferred(&board_check_battery_status_data,
+				   CHECK_BATT_STAT_DELAY_MS);
+	} else {
+		check_batt_retry = 0;
+	}
+
+	return DEFAULT_BATTERY_TYPE;
 }
