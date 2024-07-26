113cc5f687c59a299844bb2368802488c74f2678
Matt_Wang <Matt_Wang@compal.corp-partner.google.com>
2024-07-26 17:27:20 +0800

    veluza: modify the charger temps control current method
    according to the thermal request, modify the charge current table
    when the charger temperature changes.
    
    BUG=b:345640510
    TEST=zmake build veluza success, thermal test PASS
    
    Change-Id: I2e1689a3a0d01aef1cf936627fafd426f4da7119

--- zephyr/test/krabby/src/temp_veluza.c
+++ zephyr/test/krabby/src/temp_veluza.c
@@ -29,12 +29,19 @@ int count;
 /* Limit charging current table : 3600/3000/2400/1800
  * note this should be in descending order.
  */
-static uint16_t current_table[] = {
-	3600,
-	3000,
-	2400,
-	1600,
+
+struct current_table_struct {
+	int temperature;
+	int current;
 };
+
+static const struct current_table_struct current_table[] = {
+	{ 0, 2554 },
+	{ 55, 1400 },
+	{ 57, 365 },
+};
+
+#define CURRENT_LEVELS ARRAY_SIZE(current_table)
 
 int setup_faketemp(int fake_voltage)
 {
@@ -47,60 +54,86 @@ int setup_faketemp(int fake_voltage)
 	return emul_temp;
 }
 
-static void ignore_first_minute(void)
+ZTEST(temp_veluza, test_decrease_current_level)
 {
-	for (int i = 0; i < 60; i++) {
-		hook_notify(HOOK_SECOND);
-	}
-}
-
-ZTEST(temp_veluza, test_decrease_current)
-{
-	fake_voltage = 411;
+	fake_voltage = 350;
 	curr.batt.flags |= BATT_FLAG_RESPONSIVE;
 	count = 0;
 
 	setup_faketemp(fake_voltage);
-	/* Calculate per minute temperature.
-	 * It's expected low temperature when the first 60 seconds.
-	 */
-	ignore_first_minute();
-	for (int i = 1; i < 26; i++) {
+	int value = 0;
+	for (int uptime_time = 0; uptime_time < 80; uptime_time++) {
 		hook_notify(HOOK_SECOND);
 		curr.requested_current = ORIGINAL_CURRENT;
 		charger_profile_override(&curr);
-		if (i % 6 == 0) {
-			zassert_equal(current_table[count],
-				      curr.requested_current, NULL);
-			count++;
+		if (uptime_time % 5 == 0) {
+			if (curr.requested_current == ORIGINAL_CURRENT) {
+				zassert_equal(ORIGINAL_CURRENT,
+					      curr.requested_current);
+			} else if (value >=
+				   current_table[count + 1].temperature) {
+				charger_profile_override(&curr);
+				zassert_equal(curr.requested_current,
+					      current_table[count].current);
+				count++;
+			}
 		}
+		if (count > 2)
+			count = 2;
 	}
-	zassert_equal(count, 4, NULL);
 }
 
 ZTEST(temp_veluza, test_increase_current)
 {
-	fake_voltage = 446;
+	fake_voltage = 400;
 	curr.batt.flags |= BATT_FLAG_RESPONSIVE;
-	count = 3;
+	count = 2;
 
 	setup_faketemp(fake_voltage);
-	for (int i = 1; i < 26; i++) {
+	int value = 60;
+
+	for (int uptime_time = 0; uptime_time < 60; uptime_time++) {
 		hook_notify(HOOK_SECOND);
 		curr.requested_current = ORIGINAL_CURRENT;
 		charger_profile_override(&curr);
-		if (i % 5 == 0) {
-			if (curr.requested_current == ORIGINAL_CURRENT) {
-				zassert_equal(ORIGINAL_CURRENT,
-					      curr.requested_current, NULL);
-			} else {
-				zassert_equal(current_table[count],
-					      curr.requested_current, NULL);
+		if (uptime_time % 5 == 0) {
+			if (value < current_table[count].temperature) {
+				charger_profile_override(&curr);
+				zassert_equal(curr.requested_current,
+					      current_table[count].current);
 				count--;
 			}
 		}
+		if (count < 0)
+			count = 0;
 	}
-	zassert_equal(count, -1, NULL);
+}
+
+ZTEST(temp_veluza, test_battery_no_response)
+{
+	int rv;
+
+	curr.batt.flags &= ~BATT_FLAG_RESPONSIVE;
+	rv = charger_profile_override(&curr);
+	zassert_equal(rv, 0);
+}
+
+ZTEST(temp_veluza, test_charger_profile_override_get_param)
+{
+	int rv;
+
+	rv = charger_profile_override_get_param(0, 0);
+
+	zassert_equal(rv, 3);
+}
+
+ZTEST(temp_veluza, test_charger_profile_override_set_param)
+{
+	int rv;
+
+	rv = charger_profile_override_set_param(0, 0);
+
+	zassert_equal(rv, 3);
 }
 
 ZTEST_SUITE(temp_veluza, NULL, NULL, NULL, NULL, NULL);
