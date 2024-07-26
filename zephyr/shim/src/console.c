eae35b827795c5bec7f5fca93cfebdc22c8e1003
Keith Short <keithshort@chromium.org>
2024-07-26 13:41:49 -0600

    WIP: Send CC_COMMAND output directly to the shell
    With LOG_MODE_MINIMAL, the logging system using printk as the backend.
    The shell subsystem uses the UART directly.
    
    BUG=b:<bug_id>
    TEST=Test #1
    TEST=TEST #2
    
    Change-Id: Ie5805b3764cee91fb2663dc32407368fbdb516f4

--- zephyr/shim/src/console.c
+++ zephyr/shim/src/console.c
@@ -298,6 +298,8 @@ SYS_INIT(init_ec_console, PRE_KERNEL_1,
 
 __maybe_unused static int zephyr_shim_console_out(int c)
 {
+	/* TODO - add new console command to suppress output here... */
+
 	if (IS_ENABLED(CONFIG_PLATFORM_EC_HOSTCMD_CONSOLE) && !k_is_in_isr()) {
 		char console_char = c;
 		console_buf_notify_chars(&console_char, 1);
@@ -446,7 +448,7 @@ static void handle_sprintf_rv(int rv, size_t *len)
 	}
 }
 
-static void zephyr_print(const char *buff, size_t size)
+static void zephyr_print(const char *buff, size_t size, bool is_shell_output)
 {
 	/*
 	 * shell_* functions can not be used in ISRs so optionally use
@@ -464,6 +466,8 @@ static void zephyr_print(const char *buff, size_t size)
 		    !in_isr) {
 			printk("!%s", buff);
 		}
+	} else if (is_shell_output) {
+		shell_fprintf(shell_zephyr, SHELL_NORMAL, "%s", buff);
 	} else {
 		if (IS_ENABLED(CONFIG_LOG_MODE_MINIMAL)) {
 			/*
@@ -500,7 +504,8 @@ int cputs(enum console_channel channel, const char *outstr)
 	if (console_channel_is_disabled(channel))
 		return EC_SUCCESS;
 
-	zephyr_print(outstr, strlen(outstr));
+	zephyr_print(outstr, strlen(outstr),
+		     channel == CC_COMMAND ? true : false);
 
 	return 0;
 }
@@ -521,7 +526,7 @@ int cprintf(enum console_channel channel, const char *format, ...)
 	va_end(args);
 	handle_sprintf_rv(rv, &len);
 
-	zephyr_print(buff, len);
+	zephyr_print(buff, len, channel == CC_COMMAND ? true : false);
 
 	return rv > 0 ? EC_SUCCESS : rv;
 }
@@ -557,7 +562,7 @@ int cprints(enum console_channel channel, const char *format, ...)
 			   "]\n");
 	handle_sprintf_rv(rv, &len);
 
-	zephyr_print(buff, len);
+	zephyr_print(buff, len, channel == CC_COMMAND ? true : false);
 
 	return rv > 0 ? EC_SUCCESS : rv;
 }
