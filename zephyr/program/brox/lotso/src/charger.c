04a558140a7ca8fb17657b3191e351b9b4fcb40f
Wen Zhang <zhangwen6@huaqin.corp-partner.google.com>
2024-07-29 16:31:28 +0800

    [TEST-ONLY]lotso: control prochot for AC only power on
    control prochot for AC only power on
    
    Change-Id: I56d2a00e7a10c150cea77072f7bba3f271f70a06

--- zephyr/program/brox/lotso/src/charger.c
+++ zephyr/program/brox/lotso/src/charger.c
@@ -8,6 +8,9 @@
 #include "extpower.h"
 #include "hooks.h"
 #include "i2c.h"
+#include "timer.h"
+
+#include <zephyr/drivers/gpio.h>
 
 #define LOTSO_CHARGER_MIN_INPUT_VOLTAGE 0x240
 static void bq25710_min_input_voltage(void)
@@ -20,3 +23,18 @@ static void bq25710_min_input_voltage(void)
 }
 DECLARE_HOOK(HOOK_INIT, bq25710_min_input_voltage, HOOK_PRIO_DEFAULT);
 DECLARE_HOOK(HOOK_AC_CHANGE, bq25710_min_input_voltage, HOOK_PRIO_DEFAULT);
+
+static void control_prochot_when_AC_only_deferred(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_prochot_odl), 1);
+}
+DECLARE_DEFERRED(control_prochot_when_AC_only_deferred);
+
+static void control_prochot(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_prochot_odl), 0);
+	hook_call_deferred(&control_prochot_when_AC_only_deferred_data,
+			   5 * MINUTE);
+}
+DECLARE_HOOK(HOOK_INIT, control_prochot, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_POWER_BUTTON_CHANGE, control_prochot, HOOK_PRIO_DEFAULT);
