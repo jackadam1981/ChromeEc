3a21e4b0e2bec08f2c80844244144e32fe86ef40
Jack Rosenthal <jrosenth@chromium.org>
2024-07-30 10:07:50 -0600

    Makefile: Improve reliability of clang/gcc detection
    Prior to this change, clang/gcc would be detected by searching the
    output of $(BUILDCC_PREFIX)$(CC) for "clang version".  This has a
    number of issues:
    
    - We don't really use CC.  Instead, we let callers set
      CROSS_COMPILE_$(ARCH) to the toolchains they want, which allows the
      callers to not require knowledge of the architecture of the board
      they're building and let the Makefile choose the right architecture.
    - This conflates CC and BUILD_CC, which are two different compilers.
      BUILD_CC could be LLVM whereas CC could be GCC and that should be
      supported.
    
    With the exception of fingerprint boards, we generally want to compile
    with GCC.  This changes the detection mechanism as such:
    
    1.  New variables are introduced, CROSS_COMPILE_CC_NAME and
        CROSS_COMPILE_CXX_NAME, which are concatenated onto
        CROSS_COMPILE_$(ARCH).  Manually setting these variables will get
        what you want.
    2.  When unset, unless a fingerprint board is requested, we always use
        GCC.
    
    BUG=b:338458418
    TEST=CQ, compare_build.sh, diff make -n -d output
    BRANCH=none
    
    Change-Id: Ie055c260b420d209f37afa1f59749cf041a5c2b0
    Signed-off-by: Jon Murphy <jpmurphy@google.com>

--- core/minute-ia/build.mk
+++ core/minute-ia/build.mk
@@ -10,15 +10,23 @@
 $(call set-option,CROSS_COMPILE,$(CROSS_COMPILE_i386),\
 	/opt/coreboot-sdk/bin/i386-elf-)
 
+# No fingerprint boards using Minute-IA, so we have no need for clang support.
+# Let GCC be the default unless explicitly requested otherwise.
+CROSS_COMPILE_CC_NAME?=gcc
+
 # FPU compilation flags
 CFLAGS_FPU-$(CONFIG_FPU)=
 
 # CPU specific compilation flags
-CFLAGS_CPU+=-O2 -fomit-frame-pointer -mno-accumulate-outgoing-args	\
+CFLAGS_CPU+=-O2 -fomit-frame-pointer \
 	    -ffunction-sections -fdata-sections				\
 	    -fno-builtin-printf -fno-builtin-sprintf			\
 	    -fno-stack-protector -gdwarf-2  -fno-common -ffreestanding	\
 	    -minline-all-stringops -fno-strict-aliasing
+
+ifneq ($(CROSS_COMPILE_CC_NAME),clang)
+CFLAGS_CPU+=-mno-accumulate-outgoing-args
+endif
 
 CFLAGS_CPU+=$(CFLAGS_FPU-y)
 
