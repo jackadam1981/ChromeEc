34fe311e34410095346451e600b7cb59daf00d5e
Jack Rosenthal <jrosenth@chromium.org>
2024-07-30 10:07:50 -0600

    Makefile: Improve reliability of clang/gcc detection
    Prior to this change, clang/gcc would be detected by searching the
    output of $(BUILDCC_PREFIX)$(CC) for "clang version".  This has a
    number of issues:
    
    - We don't really use CC.  Instead, we let callers set
      CROSS_COMPILE_$(ARCH) to the toolchains they want, which allows the
      callers to not require knowledge of the architecture of the board
      they're building and let the Makefile choose the right architecture.
    - This conflates CC and BUILD_CC, which are two different compilers.
      BUILD_CC could be LLVM whereas CC could be GCC and that should be
      supported.
    
    With the excepiton of fingerprint boards, we generally want to compile
    with GCC.  This changes the detection mechanism as such:
    
    1.  New variables are introduced, CROSS_COMPILE_CC_NAME and
        CROSS_COMPILE_CXX_NAME, which are concatenated onto
        CROSS_COMPILE_$(ARCH).  Manually setting these variables will get
        what you want.
    2.  When unset, unless a fingerprint board is requested, we always use
        GCC.
    
    BUG=b:338458418
    TEST=CQ
    
    Cq-Depend: chrome-internal:7682720
    Change-Id: Ie055c260b420d209f37afa1f59749cf041a5c2b0

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -11,7 +11,6 @@ from concurrent.futures import ThreadPoolExecutor
 import logging
 import math
 import multiprocessing
-import os
 import shutil
 import subprocess
 import sys
@@ -37,6 +36,7 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "discovery",
     "gingerbread",
     "hatch_fp",
+    "host",
     "hyperdebug",
     "nocturne_fp",
     "nucleo-f411re",
@@ -337,10 +337,11 @@ def build(board_name: str, max_cpus: int) -> None:
     cmd = [
         "make",
         "BOARD=" + board_name,
+        "CROSS_COMPILE_CC_NAME=clang",
         f"-j{max_cpus}",
     ]
     logging.debug('Running command: "%s"', " ".join(cmd))
-    subprocess.run(cmd, env=dict(os.environ, CC="clang"), check=True)
+    subprocess.run(cmd, check=True)
 
 
 def get_all_boards() -> typing.List[str]:
