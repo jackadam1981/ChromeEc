731a18441feba1f067494201dd4029ed867d3cec
Craig Hesling <hesling@chromium.org>
2024-08-05 16:19:46 -0700

    helipilot: Fix round-down error when calc code/data ram split
    It appears that the intention behind this calculation for
    CONFIG_RW_SIZE was to ensure that the
    
    * CONFIG_FLASH_ERASE_SIZE is 64K
    
    BUG=b:357938320
    TEST=./util/compare_build.sh -b helipilot
         # MATCH
    
    Change-Id: Ifa4a6e3c8325acb03e5e4478ff1eccdf275ca3e5

--- board/helipilot/board.h
+++ board/helipilot/board.h
@@ -20,4 +20,15 @@
 #define CONFIG_FP_SENSOR_FPC1025
 #endif /* SECTION_IS_RW */
 
+/*
+ * This restores the incorrect alignment used to release helipilot MP firmware.
+ *
+ * TODO(b/357938320): Fix this incorrect alignment in a way that is compatible
+ * with existing MP firmware.
+ */
+#undef CONFIG_RW_SIZE
+#define CONFIG_RW_SIZE                                          \
+	((NPCX_PROGRAM_MEMORY_SIZE / CONFIG_FLASH_ERASE_SIZE) * \
+	 CONFIG_FLASH_ERASE_SIZE)
+
 #endif /* __CROS_EC_BOARD_H */
