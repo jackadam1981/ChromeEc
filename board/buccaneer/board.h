afd8476605fdd7cc03bdf71cbc47432cfeb39938
Craig Hesling <hesling@chromium.org>
2024-08-05 16:19:46 -0700

    helipilot: Fix round-down error when calc code/data ram split
    It appears that the intention behind this calculation for
    CONFIG_RW_SIZE was to ensure that the
    
    * CONFIG_FLASH_ERASE_SIZE is 64K
    
    BUG=b:357938320
    TEST=./util/compare_build.sh -b helipilot
    
    Change-Id: Ifa4a6e3c8325acb03e5e4478ff1eccdf275ca3e5

--- board/buccaneer/board.h
+++ board/buccaneer/board.h
@@ -20,13 +20,20 @@
  * See description in baseboard/helpilot/base_board.h.
  */
 #undef HELIPILOT_CODE_RAM_SIZE_BYTES
-#define HELIPILOT_CODE_RAM_SIZE_BYTES (320 * 1024)
+/* #define HELIPILOT_CODE_RAM_SIZE_BYTES (320 * 1024) */
+/* between 188 (doesn't work) and 192 (works).
+ * Even 191k does not work
+ */
+/* #define HELIPILOT_CODE_RAM_SIZE_BYTES (192 * 1024) */
+/* #define HELIPILOT_CODE_RAM_SIZE_BYTES (160 * 1024) */
+#define HELIPILOT_CODE_RAM_SIZE_BYTES (129 * 1024)
 
 /**
  * See description in baseboard/helpilot/base_board.h.
  */
 #undef HELIPILOT_DATA_RAM_SIZE_BYTES
-#define HELIPILOT_DATA_RAM_SIZE_BYTES (188 * 1024)
+/* #define HELIPILOT_DATA_RAM_SIZE_BYTES (188 * 1024) */
+#define HELIPILOT_DATA_RAM_SIZE_BYTES (181 * 1024)
 
 #ifdef SECTION_IS_RW
 #define CONFIG_FP_SENSOR_ELAN80SG
