ee9c9aa769f391c4a853b87745cd473ea8233aa2
Ren Chen <Ren.Chen@ite.com.tw>
2024-08-05 14:52:10 +0800

    WIP: zephyr: subsys: usbd_service: add google vendor classes
    This change adds support for Google vendor classes, including
    - Google fake class
    - Google update class
    - Google I2C class
    
    BUG=none
    TEST=
    
    Change-Id: I22d108a826af8123397051b6ba8ad05060c7d083

--- zephyr/subsys/usbd_service/usbd_init.c
+++ zephyr/subsys/usbd_service/usbd_init.c
@@ -87,6 +87,22 @@ int usb_msg_callback_register(msg_callback_t callback)
 	return 0;
 }
 
+uint8_t google_get_in_ep(struct usbd_class_data *const c_data)
+{
+	struct google_data *data = usbd_class_get_private(c_data);
+	struct google_desc *desc = data->desc;
+
+	return desc->in_ep.bEndpointAddress;
+}
+
+uint8_t google_get_out_ep(struct usbd_class_data *const c_data)
+{
+	struct google_data *data = usbd_class_get_private(c_data);
+	struct google_desc *desc = data->desc;
+
+	return desc->out_ep.bEndpointAddress;
+}
+
 static void usb_device_msg_cb(struct usbd_context *const ctx,
 			      const struct usbd_msg *msg)
 {
