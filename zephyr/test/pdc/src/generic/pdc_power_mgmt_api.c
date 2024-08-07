1e56bf079a1cb3c736059ab4816d3c99fc8c87d3
Abe Levkoy <alevkoy@chromium.org>
2024-08-07 14:27:03 -0600

    pdc_power_mgmt: Debounce power state hooks
    Do not execute power state hooks until the power state has been stable
    for 2 seconds. Avoid policy flapping when the power state flaps.
    
    BUG=b:351255382
    TEST=Power cycle with mobile device attached; observed policy hooks
    Verfied that when Brox port is connected to a phone, that following a
    cold boot, the brox port always switches to SRC power role as expected
    
    Change-Id: I815682cc2b402a1bb6fcf099d2484027133e5a98

--- zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
+++ zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
@@ -47,6 +47,7 @@ static void reset_fakes(void)
 	RESET_FAKE(system_jumped_late);
 	RESET_FAKE(chipset_in_state);
 
+	fake_chipset_state = CHIPSET_STATE_ON;
 	chipset_in_state_fake.custom_fake = custom_fake_chipset_in_state;
 }
 
@@ -1133,8 +1134,9 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_suspend)
 	zassert_true(
 		TEST_WAIT_FOR(pd_is_connected(TEST_PORT), PDC_TEST_TIMEOUT));
 
+	fake_chipset_state = CHIPSET_STATE_SUSPEND;
 	hook_notify(HOOK_CHIPSET_SUSPEND);
-	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT);
+	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT * 2);
 
 	emul_pdc_disconnect(emul);
 
@@ -1157,8 +1159,9 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_resume_no_partner)
 {
 	enum ccom_t ccom;
 
+	fake_chipset_state = CHIPSET_STATE_ON;
 	hook_notify(HOOK_CHIPSET_RESUME);
-	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT);
+	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT * 2);
 
 	zassert_ok(emul_pdc_get_ccom(emul, &ccom),
 		   "Invalid CCOM value in emul");
@@ -1180,8 +1183,9 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_resume_drp_partner)
 	zassert_true(
 		TEST_WAIT_FOR(pd_is_connected(TEST_PORT), PDC_TEST_TIMEOUT));
 
+	fake_chipset_state = CHIPSET_STATE_ON;
 	hook_notify(HOOK_CHIPSET_RESUME);
-	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT);
+	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT * 2);
 
 	zassert_ok(emul_pdc_get_pdr(emul, &pdr), "Invalid PDR value in emul");
 	zassert_equal(pdr.swap_to_src, 1);
@@ -1226,8 +1230,9 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_startup)
 	zassert_true(
 		TEST_WAIT_FOR(pd_is_connected(TEST_PORT), PDC_TEST_TIMEOUT));
 
+	fake_chipset_state = CHIPSET_STATE_ON;
 	hook_notify(HOOK_CHIPSET_STARTUP);
-	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT);
+	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT * 2);
 
 	emul_pdc_disconnect(emul);
 
@@ -1258,8 +1263,9 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_shutdown)
 	zassert_true(
 		TEST_WAIT_FOR(pd_is_connected(TEST_PORT), PDC_TEST_TIMEOUT));
 
+	fake_chipset_state = CHIPSET_STATE_HARD_OFF;
 	hook_notify(HOOK_CHIPSET_SHUTDOWN);
-	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT);
+	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT * 10);
 
 	emul_pdc_disconnect(emul);
 
@@ -1784,7 +1790,7 @@ ZTEST_USER(pdc_power_mgmt_api, test_hpd_wake)
 	/* Suspend the DUT. */
 	fake_chipset_state = CHIPSET_STATE_SUSPEND;
 	hook_notify(HOOK_CHIPSET_SUSPEND);
-	k_msleep(TEST_WAIT_FOR_INTERVAL_MS);
+	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT * 2);
 
 	/* Clear any USB mux host event. */
 	host_clear_events(EC_HOST_EVENT_MASK(EC_HOST_EVENT_USB_MUX));
@@ -1800,6 +1806,7 @@ ZTEST_USER(pdc_power_mgmt_api, test_hpd_wake)
 	/* Send an IRQ for the PDC power manager to update its DP Status. */
 	emul_pdc_set_connector_status(emul, &in_conn_status);
 	emul_pdc_pulse_irq(emul);
+	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT * 2);
 
 	zassert_ok(pdc_power_mgmt_resync_port_state_for_ppm(TEST_PORT));
 
