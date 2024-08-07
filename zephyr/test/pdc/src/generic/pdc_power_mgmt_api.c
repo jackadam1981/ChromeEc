dc7357b7a75abeb45fe00f32e3f1390dfce9c354
Abe Levkoy <alevkoy@chromium.org>
2024-08-07 14:27:03 -0600

    pdc_power_mmgmt: Debounce power state hooks
    Do not execute power state hooks until the power state has been stable
    for 2 seconds. Avoid policy flapping when the power state flaps.
    
    BUG=b:351255382
    TEST=Power cycle with mobile device attached; observed policy hooks
    
    Change-Id: I815682cc2b402a1bb6fcf099d2484027133e5a98

--- zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
+++ zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
@@ -965,7 +965,7 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_suspend)
 {
 	union connector_status_t connector_status;
 	enum ccom_t ccom;
-	uint32_t timeout = k_ms_to_cyc_ceil32(PDC_TEST_TIMEOUT);
+	uint32_t timeout = k_ms_to_cyc_ceil32(5000);
 	uint32_t start;
 
 	emul_pdc_configure_src(emul, &connector_status);
@@ -998,7 +998,7 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_resume)
 	enum ccom_t ccom;
 
 	hook_notify(HOOK_CHIPSET_RESUME);
-	TEST_WORKING_DELAY(PDC_TEST_TIMEOUT);
+	TEST_WORKING_DELAY(5000);
 
 	zassert_ok(emul_pdc_get_ccom(emul, &ccom),
 		   "Invalid CCOM value in emul");
@@ -1009,7 +1009,7 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_startup)
 {
 	union connector_status_t connector_status;
 	enum ccom_t ccom;
-	uint32_t timeout = k_ms_to_cyc_ceil32(PDC_TEST_TIMEOUT);
+	uint32_t timeout = k_ms_to_cyc_ceil32(5000);
 	uint32_t start;
 
 	emul_pdc_configure_src(emul, &connector_status);
@@ -1041,7 +1041,7 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_shutdown)
 {
 	union connector_status_t connector_status;
 	union pdr_t pdr;
-	uint32_t timeout = k_ms_to_cyc_ceil32(PDC_TEST_TIMEOUT);
+	uint32_t timeout = k_ms_to_cyc_ceil32(5000);
 	uint32_t start;
 
 	emul_pdc_configure_src(emul, &connector_status);
