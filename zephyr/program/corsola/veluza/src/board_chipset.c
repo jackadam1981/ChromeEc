96248654d4aa0b03fa1277a18c0e5df05fcfdc68
Matt_Wang <Matt_Wang@compal.corp-partner.google.com>
2024-08-12 11:04:50 +0800

    veluza: modify touch panel power sequence
    Modify the touch panel power sequence to EC control.
    The CPU notifies the EC through AP_EDP_BKLTEN. Then
    the EC receives AP_EDP_BKLTEN to implement the
    power-on and power-off timing of tp_en and bl_en.
    
    BUG=b:358556193
    TEST=zmake build veluza SUCCESS
    LOW_COVERAGE_REASON=Tests will be added in b:358556193
    
    Change-Id: I1ab69b12d31633489d711ad07a227a905d74f304

---
+++ zephyr/program/corsola/veluza/src/board_chipset.c
@@ -0,0 +1,116 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Corsola baseboard-chipset specific configuration */
+
+#include "charge_state.h"
+#include "common.h"
+#include "cros_board_info.h"
+#include "cros_cbi.h"
+#include "gpio/gpio_int.h"
+#include "gpio_signal.h"
+#include "hooks.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/init.h>
+
+#include <ap_power/ap_power.h>
+
+LOG_MODULE_REGISTER(cbi_info);
+
+#define BOARD_VERSION_UNKNOWN 0xffffffff
+#define FORM_FACTOR_UNKNOWN 0xffffffff
+static bool value_en;
+
+static void set_tp_en_pin(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tp_en), value_en);
+}
+DECLARE_DEFERRED(set_tp_en_pin);
+
+static void set_bl_en_pin(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_bl_en_od), value_en);
+}
+DECLARE_DEFERRED(set_bl_en_pin);
+
+void ap_bl_en_interrupt(enum gpio_signal signal)
+{
+	if (gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_ap_bl_en_odl))) {
+		value_en = 1;
+		hook_call_deferred(&set_bl_en_pin_data, 0);
+		hook_call_deferred(&set_tp_en_pin_data, 30 * MSEC);
+	} else {
+		value_en = 0;
+		hook_call_deferred(&set_tp_en_pin_data, 0);
+		hook_call_deferred(&set_bl_en_pin_data, 102 * MSEC);
+	}
+}
+
+static uint32_t board_version = BOARD_VERSION_UNKNOWN;
+static uint32_t form_factor = FORM_FACTOR_UNKNOWN;
+
+static void ap_bl_en_init(void)
+{
+	/*
+	 * Check the board version and form factor to control panel sequence.
+	 */
+	if (board_version == BOARD_VERSION_UNKNOWN || IS_ENABLED(CONFIG_TEST)) {
+		if (cbi_get_board_version(&board_version) != EC_SUCCESS) {
+			LOG_ERR("Failed to get board version.");
+			board_version = 0;
+		}
+	}
+
+	if (form_factor == FORM_FACTOR_UNKNOWN || IS_ENABLED(CONFIG_TEST)) {
+		if (cros_cbi_get_fw_config(FW_FORM_FACTOR, &form_factor) !=
+		    EC_SUCCESS) {
+			LOG_ERR("Failed to get form form_factor.");
+			form_factor = 0;
+		}
+	}
+
+	if (board_version > 1 && form_factor == FW_FORM_FACTOR_CLAMSHELL)
+		gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_ap_bl_en));
+}
+DECLARE_HOOK(HOOK_INIT, ap_bl_en_init, HOOK_PRIO_DEFAULT);
+
+static void board_backlight_handler(struct ap_power_ev_callback *cb,
+				    struct ap_power_ev_data data)
+{
+	int value;
+
+	switch (data.event) {
+	default:
+		return;
+
+	case AP_POWER_RESUME:
+		/* Called on AP S3 -> S0 transition */
+		value = 1;
+		break;
+
+	case AP_POWER_SUSPEND:
+		/* Called on AP S0 -> S3 transition */
+		value = 0;
+		break;
+	}
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_bl_en_od), value);
+}
+
+static int install_backlight_handler(void)
+{
+	static struct ap_power_ev_callback cb;
+
+	/*
+	 * Add a callback for suspend/resume to control the panel
+	 * backlight and touch enable gpio pin.
+	 */
+	ap_power_ev_init_callback(&cb, board_backlight_handler,
+				  AP_POWER_RESUME | AP_POWER_SUSPEND);
+	ap_power_ev_add_callback(&cb);
+	return 0;
+}
+
+SYS_INIT(install_backlight_handler, APPLICATION, 1);
