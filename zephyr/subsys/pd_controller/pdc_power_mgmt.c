34705924b339ce9b6fc36d995531329a9b928702
Keith Short <keithshort@chromium.org>
2024-08-12 16:06:55 -0600

    pdc: set the attached state during state entry
    Set the "attached_state" on state entry instead of waiting until the
    corresponding "run" sub-state.
    
    This avoids race conditions where the policy checks think the PDC is
    still in a previous state (such as SNK_ATTACHED ater a power role swap
    to source).
    
    BUG=none
    TEST=./twister -ivc -T zephyr/test/pdc
    
    Change-Id: I5921e92b7ecb4ad9f8110108cdc3023bebc9fabf

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -1364,6 +1364,7 @@ static void pdc_src_attached_entry(void *obj)
 	struct pdc_port_t *port = (struct pdc_port_t *)obj;
 
 	print_current_pdc_state(port);
+	set_attached_pdc_state(port, SRC_ATTACHED_STATE);
 
 	port->send_cmd.intern.pending = false;
 
@@ -1456,7 +1457,6 @@ static void pdc_src_attached_run(void *obj)
 			       SRC_POLICY_EVAL_SNK_FIXED_PDO);
 		return;
 	case SRC_ATTACHED_RUN:
-		set_attached_pdc_state(port, SRC_ATTACHED_STATE);
 		run_src_policies(port);
 		break;
 	}
@@ -1470,6 +1470,7 @@ static void pdc_snk_attached_entry(void *obj)
 	struct pdc_port_t *port = (struct pdc_port_t *)obj;
 
 	print_current_pdc_state(port);
+	set_attached_pdc_state(port, SNK_ATTACHED_STATE);
 
 	port->send_cmd.intern.pending = false;
 	if (get_pdc_state(port) != port->send_cmd_return_state) {
@@ -1651,7 +1652,6 @@ static void pdc_snk_attached_run(void *obj)
 		queue_internal_cmd(port, CMD_PDC_SET_SINK_PATH);
 		return;
 	case SNK_ATTACHED_RUN:
-		set_attached_pdc_state(port, SNK_ATTACHED_STATE);
 		/* Hard Reset could disable Sink FET. Re-enable it */
 		if (atomic_get(&port->hard_reset_sent)) {
 			atomic_clear(&port->hard_reset_sent);
@@ -1999,6 +1999,7 @@ static void pdc_src_typec_only_entry(void *obj)
 	struct pdc_port_t *port = (struct pdc_port_t *)obj;
 
 	print_current_pdc_state(port);
+	set_attached_pdc_state(port, SRC_ATTACHED_TYPEC_ONLY_STATE);
 
 	if (get_pdc_state(port) != port->send_cmd_return_state) {
 		port->src_typec_attached_local_state =
@@ -2021,8 +2022,6 @@ static void pdc_src_typec_only_run(void *obj)
 	struct pdc_port_t *port = (struct pdc_port_t *)obj;
 	const struct pdc_config_t *config = port->dev->config;
 	int port_number = config->connector_num;
-
-	set_attached_pdc_state(port, SRC_ATTACHED_TYPEC_ONLY_STATE);
 
 	/* The CCI_EVENT is set on a connector disconnect, so check the
 	 * connector status and take the appropriate action. */
@@ -2082,14 +2081,13 @@ static void pdc_snk_typec_only_entry(void *obj)
 	}
 
 	print_current_pdc_state(port);
+	set_attached_pdc_state(port, SNK_ATTACHED_TYPEC_ONLY_STATE);
 }
 
 static void pdc_snk_typec_only_run(void *obj)
 {
 	struct pdc_port_t *port = (struct pdc_port_t *)obj;
 	const struct pdc_config_t *const config = port->dev->config;
-
-	set_attached_pdc_state(port, SNK_ATTACHED_TYPEC_ONLY_STATE);
 
 	/* The CCI_EVENT is set on a connector disconnect, so check the
 	 * connector status and take the appropriate action. */
@@ -2501,7 +2499,19 @@ test_mockable bool pdc_power_mgmt_is_connected(int port)
 		return false;
 	}
 
-	return pdc_data[port]->port.attached_state != UNATTACHED_STATE;
+	if ((pdc_data[port]->port.attached_state == SNK_ATTACHED_STATE) &&
+	    (pdc_data[port]->port.snk_attached_local_state ==
+	     SNK_ATTACHED_RUN)) {
+		return true;
+	}
+
+	if ((pdc_data[port]->port.attached_state == SRC_ATTACHED_STATE) &&
+	    (pdc_data[port]->port.src_attached_local_state ==
+	     SRC_ATTACHED_RUN)) {
+		return true;
+	}
+
+	return false;
 }
 
 uint8_t pdc_power_mgmt_get_usb_pd_port_count(void)
