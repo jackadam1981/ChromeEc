f26f16ac5e90b73eb832090d52ea2b5dad4d4aa6
Al Semjonovs <asemjonovs@google.com>
2024-08-13 13:41:51 -0600

    pdc_power_mgmt: Stub test for frs_enable API
    Currently pdc_power_mgmt_frs_enable is stubbed to return SUCCESS
    as control is not available in PDC. Executing API to meet code coverage.
    
    BUG=b:339686045
    TEST=./twister -T zephyr/test/pdc
    
    Change-Id: Ib23fe4a6076bd846bc457529352441dafd100dc3

--- zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
+++ zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
@@ -1573,6 +1573,17 @@ ZTEST_USER(pdc_power_mgmt_api, test_sysjump_policy_on)
 	helper_wait_for_ccom_mode(CCOM_DRP);
 }
 
+ZTEST_USER(pdc_power_mgmt_api, test_frs_enable)
+{
+	/*
+	 * TODO(b/337958604): Currently there is no mechanism to enable/disable
+	 * FRS. Waiting for this control to be available in PDC, then we can
+	 * validate the behavior here.
+	 */
+	zassert_ok(pdc_power_mgmt_frs_enable(TEST_PORT, true));
+	zassert_ok(pdc_power_mgmt_frs_enable(TEST_PORT, false));
+}
+
 /*
  * Suspended PDC - These tests take place with the PDC Power Mgmt subsystem
  * in the suspended state, when communication with the PDC is not allowed.
