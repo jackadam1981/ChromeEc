53beda18b789bf23dee0513b5fac667494b6c0cd
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2024-08-12 17:46:06 -0700

    ptlrvp: Reconfigure power signal `PWR_EC_PCH_SYS_PWROK`
    GPIO pin asigned to power signal `PWR_EC_PCH_SYS_PWROK` is tied to
    signal `PWR_PCH_PWROK`. During RO/RW transition, `PWR_EC_PCH_SYS_PWROK`
    is used to determine current AP power state, without this signal
    correctly defined power state was always determined as G3.
    
    BUG=b:366098083
    TEST=Run `powerindebug` command on EC terminal
         Power signal value must be set accordingly
    LOW_COVERAGE_REASON=Coverage to be handled in b/336751447
    
    Change-Id: I55052b01803b90bd2c5f09d8da8e8e777679aa4b

--- zephyr/program/intelrvp/ptlrvp/src/board_power.c
+++ zephyr/program/intelrvp/ptlrvp/src/board_power.c
@@ -68,6 +68,9 @@ AP_POWER_APP_STATE_DEFINE(AP_POWER_STATE_G3, board_ap_power_action_g3_entry,
 int board_power_signal_get(enum power_signal signal)
 {
 	switch (signal) {
+	case PWR_EC_PCH_SYS_PWROK:
+		return power_signal_get(PWR_PCH_PWROK);
+		break;
 	case PWR_SYS_RST:
 		return gpio_pin_get_dt(gpio_get_dt_spec(GPIO_SYS_RST_ODL));
 		break;
