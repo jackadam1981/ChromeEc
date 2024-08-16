a66e3f71c21da5b9ff48c77e417bd5b5556f5ade
Daisuke Nojiri <dnojiri@chromium.org>
2024-08-16 09:17:05 -0700

    UCSI/PPM: Add ppm_driver tests (1)
    BUG=b:354010326
    TEST=./twister -T zephyr/test/pdc -s pdc.ppm_driver
    
    Change-Id: Idad2193adce1f4565b075f53bc9332b1d5522449

---
+++ zephyr/test/pdc/src/generic/ppm_driver.c
@@ -0,0 +1,107 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "drivers/intel_altmode.h"
+#include "ec_commands.h"
+#include "host_command.h"
+
+#include <zephyr/fff.h>
+#include <zephyr/kernel.h>
+#include <zephyr/ztest.h>
+
+#include <drivers/pdc.h>
+#include <usbc/ppm.h>
+
+#define DT_PPM_DRV DT_INST(0, ucsi_ppm)
+#define NUM_PORTS DT_PROP_LEN(DT_PPM_DRV, lpm)
+
+struct ucsi_ppm_device {
+	void *ptr;
+};
+
+struct ppm_data {
+	struct ucsi_ppm_device *ppm_dev;
+	union connector_status_t port_status[NUM_PORTS] __aligned(4);
+	struct pdc_callback cc_cb;
+	struct pdc_callback ci_cb;
+	union cci_event_t cci_event;
+};
+
+FAKE_VALUE_FUNC(struct ucsi_ppm_device *, ppm_data_init,
+		const struct ucsi_pd_driver *, const struct device *,
+		union connector_status_t *, int);
+
+FAKE_VALUE_FUNC(int, ucsi_ppm_init_and_wait, struct ucsi_ppm_device *);
+
+FAKE_VALUE_FUNC(bool, ucsi_ppm_get_next_connector_status,
+		struct ucsi_ppm_device *, uint8_t *,
+		union connector_status_t **);
+
+FAKE_VOID_FUNC(ucsi_ppm_lpm_alert, struct ucsi_ppm_device *, uint8_t);
+
+ZTEST_USER(ppm_driver, test_execute_cmd_bad_command)
+{
+	const struct device *pdc_dev;
+	const struct ucsi_pd_driver *drv;
+	uint8_t out[512];
+	struct ucsi_control_t control;
+	int rv;
+
+	pdc_dev = DEVICE_DT_GET(DT_INST(0, ucsi_ppm));
+	drv = pdc_dev->api;
+
+	control.command = 0;
+	rv = drv->execute_cmd(pdc_dev, &control, out);
+	zassert_equal(rv, -1);
+
+	control.command = UCSI_CMD_MAX;
+	rv = drv->execute_cmd(pdc_dev, &control, out);
+	zassert_equal(rv, -1);
+}
+
+ZTEST_USER(ppm_driver, test_get_ppm_dev)
+{
+	const struct device *pdc_dev;
+	const struct ucsi_pd_driver *drv;
+	struct ppm_data *data;
+	struct ucsi_ppm_device *ppm_dev;
+
+	pdc_dev = DEVICE_DT_GET(DT_INST(0, ucsi_ppm));
+	drv = pdc_dev->api;
+
+	ppm_dev = drv->get_ppm_dev(pdc_dev);
+	data = (struct ppm_data *)pdc_dev->data;
+	zassert_equal(ppm_dev, data->ppm_dev);
+}
+
+ZTEST_USER(ppm_driver, test_init_ppm)
+{
+	const struct device *pdc_dev;
+	const struct ucsi_pd_driver *drv;
+	struct ppm_data *data;
+	int rv;
+
+	pdc_dev = DEVICE_DT_GET(DT_INST(0, ucsi_ppm));
+
+	zassert_not_null(pdc_dev);
+
+	ucsi_ppm_init_and_wait_fake.return_val = 1;
+	drv = pdc_dev->api;
+
+	rv = drv->init_ppm(pdc_dev);
+	data = (struct ppm_data *)pdc_dev->data;
+	zassert_equal(ucsi_ppm_init_and_wait_fake.call_count, 1);
+	zassert_equal(ucsi_ppm_init_and_wait_fake.arg0_val, data->ppm_dev);
+	zassert_equal(rv, 1);
+}
+
+static void ppm_driver_before(void *fixture)
+{
+	RESET_FAKE(ppm_data_init);
+	RESET_FAKE(ucsi_ppm_init_and_wait);
+	RESET_FAKE(ucsi_ppm_get_next_connector_status);
+}
+
+ZTEST_SUITE(ppm_driver, NULL, NULL, ppm_driver_before, NULL, NULL);
