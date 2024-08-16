78af25a51f82328402435d2418b251f031ec8376
Daisuke Nojiri <dnojiri@chromium.org>
2024-08-16 09:17:05 -0700

    WIP: test/pdc/ucsi_ppm: Add tests for PPM
    This adds tests for ppm_driver.c. The difference from
    pdc/src/generic/ppm_driver.c is these tests use the actual ppm_common.c
    implementation.
    
    BUG=b:354010326
    TEST=./twister -T zephyr/test/pdc -s pdc.ucsi_ppm
    
    Change-Id: Ic33c84700d5e4e856885eae07b561a6bc3b3e315

---
+++ zephyr/test/pdc/src/generic/ucsi_ppm.c
@@ -0,0 +1,130 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "drivers/ucsi_v3.h"
+#include "ec_commands.h"
+#include "emul/emul_pdc.h"
+#include "ppm_common.h"
+#include "usbc/ppm.h"
+#include "zephyr/logging/log.h"
+
+#include <stdbool.h>
+
+#include <zephyr/ztest.h>
+
+LOG_MODULE_REGISTER(ucsi_ppm_test, LOG_LEVEL_DBG);
+
+#define TEST_PORT 0
+#define PDC_WAIT_FOR_ITERATIONS 3
+
+BUILD_ASSERT(TEST_PORT < CONFIG_USB_PD_PORT_MAX_COUNT);
+
+static struct ucsi_ppm_device *ppm_dev;
+
+static const struct ucsi_control_t enable_all_notifications = {
+	.command = UCSI_SET_NOTIFICATION_ENABLE,
+	.data_length = 0,
+	.command_specific = { 0xff, 0xff, 0x1, 0x0, 0x0, 0x0 },
+};
+
+static void host_cmd_pdc_reset(void *fixture)
+{
+	const struct device *pdc;
+	const struct ucsi_pd_driver *drv;
+
+	pdc = DEVICE_DT_GET(DT_INST(0, ucsi_ppm));
+	drv = pdc->api;
+	ppm_dev = drv->get_ppm_dev(pdc);
+}
+
+ZTEST_SUITE(ucsi_ppm, NULL, NULL, host_cmd_pdc_reset, host_cmd_pdc_reset, NULL);
+
+static int write_command(const struct ucsi_control_t *control)
+{
+	return ucsi_ppm_write(ppm_dev, UCSI_CONTROL_OFFSET,
+			      (const void *)control,
+			      sizeof(struct ucsi_control_t));
+}
+
+static int write_ack_command(bool connector_change_ack,
+			     bool command_complete_ack)
+{
+	struct ucsi_control_t control = { .command = UCSI_ACK_CC_CI,
+					  .data_length = 0 };
+	union ack_cc_ci_t ack_data = {
+		.connector_change_ack = connector_change_ack,
+		.command_complete_ack = command_complete_ack
+	};
+	memcpy(control.command_specific, &ack_data, sizeof(ack_data));
+	return write_command(&control);
+}
+
+/**
+ * Return true if commands are no longer pending.
+ */
+static bool wait_for_cmd_to_process(void)
+{
+	bool is_cmd_pending = false;
+
+	/*
+	 * After calling write, the command will be pending and will trigger the
+	 * main loop. Try reading the pending state a few times to see if it
+	 * clears.
+	 */
+	for (int i = 0; i < PDC_WAIT_FOR_ITERATIONS; ++i) {
+		is_cmd_pending = ppm_test_is_cmd_pending(ppm_dev);
+
+		LOG_DBG("[%d]: Command is %s", i,
+			(is_cmd_pending ? "pending" : "not pending"));
+		if (is_cmd_pending) {
+			k_msleep(1);
+		} else {
+			break;
+		}
+	}
+
+	return !is_cmd_pending;
+}
+
+ZTEST(ucsi_ppm, test_unexpected_command)
+{
+	struct ucsi_control_t ctrl = {
+		.command = UCSI_GET_CONNECTOR_STATUS,
+		.data_length = 0,
+		.command_specific = { 1, },
+	};
+	int rv;
+
+	LOG_INF("Sending UCSI_GET_CONNECTOR_STATUS");
+	rv = write_command(&ctrl);
+	zassert_equal(rv, 0);
+	/* Command should be ignored because PPM is still in IDLE. */
+	zassert_true(wait_for_cmd_to_process());
+}
+
+ZTEST(ucsi_ppm, test_set_notification_enable)
+{
+	struct ucsi_control_t ctrl = {
+		.command = UCSI_PPM_RESET,
+		.data_length = 0,
+	};
+	int rv;
+
+	LOG_INF("Sending UCSI_PPM_RESET");
+	rv = write_command(&ctrl);
+	zassert_equal(rv, 0);
+	zassert_true(wait_for_cmd_to_process());
+
+	LOG_INF("Sending SET_NOTIFICATION_ENABLE");
+	rv = write_command(&enable_all_notifications);
+	zassert_equal(rv, 0);
+	zassert_true(wait_for_cmd_to_process());
+
+	LOG_INF("Acking SET_NOTIFICATION_ENABLE");
+	rv = write_ack_command(false, true);
+	zassert_equal(rv, 0);
+	zassert_true(wait_for_cmd_to_process());
+}
