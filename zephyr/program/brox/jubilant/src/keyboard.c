8823965f20ddc9a880a1990f288934711fd41a1c
ben chen <ben.chen2@quanta.corp-partner.google.com>
2024-08-19 13:48:17 +0800

    jubilant: config vivaldi keyboard matrix
    support muti vivaldi-kbd config by FW_KB_BL of fw_config
    for 2 type keyboard fuction.
    
    BUG=b:359049825
    TEST="zmake build jubilant --clobber"
         "./twister -ci -T zephyr/test/brox/ -s brox.jubilant.keyboard"
         "./twister -T zephyr/test/brox/"
         evtest success
    
    Change-Id: Iae35849f8366bfa663299176d2b595f8e398c67e

--- zephyr/program/brox/jubilant/src/keyboard.c
+++ zephyr/program/brox/jubilant/src/keyboard.c
@@ -3,6 +3,8 @@
  * found in the LICENSE file.
  */
 
+#include "cros_board_info.h"
+#include "cros_cbi.h"
 #include "ec_commands.h"
 #include "gpio_it8xxx2.h"
 #include "hooks.h"
@@ -10,6 +12,41 @@
 #include "keyboard_scan.h"
 
 #include <drivers/vivaldi_kbd.h>
+
+LOG_MODULE_DECLARE(brox, CONFIG_Brox_LOG_LEVEL);
+
+static bool key_bl = FW_KB_BL_NOT_PRESENT;
+
+int8_t board_vivaldi_keybd_idx(void)
+{
+	if (key_bl == FW_KB_BL_NOT_PRESENT) {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_1));
+	} else {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_0));
+	}
+}
+
+/*
+ * Keyboard function decided by FW config.
+ */
+test_export_static void kb_init(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FW_KB_BL, &val);
+
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FW_KB_TYPE);
+	}
+
+	if (val == FW_KB_BL_PRESENT) {
+		key_bl = FW_KB_BL_PRESENT;
+	} else {
+		key_bl = FW_KB_BL_NOT_PRESENT;
+	}
+}
+DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_FIRST);
 
 /*
  * We have total 32 pins for keyboard connecter {-1, -1} mean
