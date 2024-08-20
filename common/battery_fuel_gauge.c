5a61f7cb075cc69bb04fb33cdcfbc1a7ba6e1bdd
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2024-08-20 13:28:59 +0800

    battery_fuel_gauge: add retries for battery type read
    Sometimes Manuf/device name is not found for the battery is still
    too busy to response. To avoid initializing wrong battery type, add
    retry count for reading Manuf/device name in init_battery_type.
    If CONFIG_BATTERY_INIT_TYPE_RETRY_COUNT is defined, Manuf/Device name
    will be read every 100ms when read failed.
    
    BUG=b:343603095
    TEST=1. make sure battery type is correct after the DUT is shutdown
            in DC mode and idle overnight.
         2. The DUT can power on without battery (AC only).
    
    Change-Id: I6ebaabf2299d34b15eb85b99d48063f0a3d1f86c

--- common/battery_fuel_gauge.c
+++ common/battery_fuel_gauge.c
@@ -179,9 +179,23 @@ void init_battery_type(void)
 {
 	int type;
 	int dflt = board_get_default_battery_type();
+	int ret;
 
-	if (battery_manufacturer_name(batt_manuf_name,
-				      sizeof(batt_manuf_name))) {
+	ret = battery_manufacturer_name(batt_manuf_name,
+					sizeof(batt_manuf_name));
+
+	for (int i = 0; i < CONFIG_BATTERY_INIT_TYPE_RETRY_COUNT; i++) {
+		if (ret == EC_SUCCESS) {
+			break;
+		}
+		CPRINTS("Manuf name not found, wait 100ms then retry (attempt %d)",
+			i);
+		crec_msleep(100);
+		ret = battery_manufacturer_name(batt_manuf_name,
+						sizeof(batt_manuf_name));
+	}
+
+	if (ret) {
 		BCFGPRT("Manuf name not found");
 		battery_conf = &board_battery_info[dflt];
 		return;
@@ -189,7 +203,20 @@ void init_battery_type(void)
 
 	/* Don't carry over any previous name (in case i2c fail). */
 	memset(batt_device_name, 0, sizeof(batt_device_name));
-	if (battery_device_name(batt_device_name, sizeof(batt_device_name))) {
+	ret = battery_device_name(batt_device_name, sizeof(batt_device_name));
+
+	for (int i = 0; i < CONFIG_BATTERY_INIT_TYPE_RETRY_COUNT; i++) {
+		if (ret == EC_SUCCESS) {
+			break;
+		}
+		CPRINTS("Device name not found, wait 100ms then retry (attempt %d)",
+			i);
+		crec_msleep(100);
+		ret = battery_device_name(batt_device_name,
+					  sizeof(batt_device_name));
+	}
+
+	if (ret) {
 		BCFGPRT("Device name not found");
 		memset(batt_device_name, 0, sizeof(batt_device_name));
 		/* Battery name is optional. Proceed. */
