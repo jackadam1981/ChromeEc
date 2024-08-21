66e074ba23f3835b31b3febb5df0a8159fd274f8
Ren Chen <Ren.Chen@ite.com.tw>
2024-08-21 16:53:52 +0800

    TEST-ONLY: stress remote wakeup
    Change-Id: I437ed86496ee41843f27920a9f10f23a7078aa03

--- zephyr/subsys/usb_dc/usb_dc.c
+++ zephyr/subsys/usb_dc/usb_dc.c
@@ -34,6 +34,9 @@ static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
 		LOG_INF("usb disconnected event");
 		break;
 	case USB_DC_SUSPEND:
+#if defined(CONFIG_CROS_EC_RW)
+		hook_call_deferred(&kb_resume_deferred_data, 0);
+#endif
 		usb_dc_status.suspended = true;
 		LOG_INF("usb suspended event");
 		break;
