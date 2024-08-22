192efbdaa368ce7acfd538bdc9f657cc3368d0af
Craig Hesling <hesling@chromium.org>
2024-08-22 10:37:24 -0700

    common: Fix IS_ENABLED multiple use with clang
    This macro had issues when being used multiple times in one source
    file, when being compiled with clang. The complaint is that the
    special function attribute, error, was being applied to the same
    function, but with different messages, since the message included
    the config name.
    
    You would see the following compilation error:
    error: attribute 'error' is already applied with different arguments
    
    BUG=b:361576707
    TEST=
    
    Change-Id: I47a669dd426073b898b95ca216f9018e58a3716b

--- include/common.h
+++ include/common.h
@@ -504,16 +504,16 @@ enum ec_error_list {
  * technique requires that the optimizer be enabled so it can remove
  * the undefined function call.
  */
-#define __config_enabled(cfg, value)                                           \
-	__cfg_select(value, 1, ({                                              \
-			     int __undefined =                                 \
-				     __builtin_strcmp(cfg, #value) == 0;       \
-			     extern int IS_ENABLED_BAD_ARGS(void) __error(     \
-				     cfg " must be <blank>, or not defined."); \
-			     if (!__undefined)                                 \
-				     IS_ENABLED_BAD_ARGS();                    \
-			     0;                                                \
-		     }))
+#define __config_enabled(cfg, value)                                          \
+	__cfg_select(                                                         \
+		value, 1, ({                                                  \
+			int __undefined = __builtin_strcmp(cfg, #value) == 0; \
+			extern int IS_ENABLED_BAD_ARGS##value(void) __error(  \
+				cfg " must be <blank>, or not defined.");     \
+			if (!__undefined)                                     \
+				IS_ENABLED_BAD_ARGS##value();                 \
+			0;                                                    \
+		}))
 
 /**
  * Checks if a config option is enabled or disabled
