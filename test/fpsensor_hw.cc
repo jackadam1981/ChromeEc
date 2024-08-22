84d66acaae494c3002412d8279f14c8d57d24654
Craig Hesling <hesling@chromium.org>
2024-08-21 19:11:48 -0700

    test/fpsensor_hw: Add buccaneer support
    BUG=b:347111619
    TEST=Prob doesn't work
    
    Change-Id: I878d82445cd18efa70c19f1b395c9ce07ce30acd

--- test/fpsensor_hw.cc
+++ test/fpsensor_hw.cc
@@ -9,11 +9,15 @@
 #include "test_util.h"
 
 #ifdef SECTION_IS_RW
-#include "fpc/fpc_sensor.h"
-static const uint32_t fp_sensor_hwid = FP_SENSOR_HWID_FPC;
+#include "fpsensor_driver.h"
+static const uint32_t fp_sensor_hwid = FP_SENSOR_HWID;
 #else
 static const uint32_t fp_sensor_hwid = UINT32_MAX;
 #endif
+
+// #ifdef SECTION_IS_RW
+// #include "fpsensor_driver.h"
+// #endif
 
 /* Hardware-dependent smoke test that makes a SPI transaction with the
  * fingerprint sensor.
@@ -22,18 +26,25 @@ test_static int test_fp_check_hwid(void)
 {
 	uint16_t id = 0;
 
-	if (IS_ENABLED(SECTION_IS_RW)) {
+	if (IS_ENABLED(CONFIG_FP_SENSOR_FPC1025) || IS_ENABLED(CONFIG_FP_SENSOR_FPC1145)) {
 		TEST_EQ(fpc_get_hwid(&id), EC_SUCCESS, "%d");
-		/* The lower 4-bits of the sensor hardware id are a
-		 * manufacturing ID that is ok to vary.
-		 */
-		TEST_EQ(fp_sensor_hwid, id >> 4, "%d");
-	};
+	} else if (IS_ENABLED(CONFIG_FP_SENSOR_ELAN80SG)) {
+		TEST_EQ(elan_get_hwid(&id), EC_SUCCESS, "%d");
+	}
+
+	/* The lower 4-bits of the sensor hardware id are a
+	 * manufacturing ID that is ok to vary.
+	 */
+	TEST_EQ(fp_sensor_hwid, id >> 4, "%d");
+
 	return EC_SUCCESS;
 }
 
 void run_test(int argc, const char **argv)
 {
-	RUN_TEST(test_fp_check_hwid);
+	test_reset();
+	if (IS_ENABLED(SECTION_IS_RW)) {
+		RUN_TEST(test_fp_check_hwid);
+	}
 	test_print_result();
 }
