76f9f9b54e09d2ff8f1ed5f55f9601ff66c15c40
Al Semjonovs <asemjonovs@google.com>
2024-08-27 16:38:39 -0600

    pdc_power_mgmt: Enable passing tests
    Enable tests that pass with current TI emulator support.
    
    BUG=b:339686045, b:345292002
    TEST=./twister -T zephyr/test/pdc
    
    Change-Id: I2e55beb5f0767802cf7acecbd3c42aa9c42d3265

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -73,7 +73,7 @@ LOG_MODULE_REGISTER(pdc_power_mgmt, CONFIG_USB_PDC_LOG_LEVEL);
 /**
  * @brief Maximum time to wait for PDC state to settle.
  */
-#define PDC_SM_SETTLED_TIMEOUT_MS PDC_CMD_TIMEOUT_MS
+#define PDC_SM_SETTLED_TIMEOUT_MS (PDC_CMD_TIMEOUT_MS * 10)
 
 /**
  * @brief maximum number of times to try and send a command, or wait for a
