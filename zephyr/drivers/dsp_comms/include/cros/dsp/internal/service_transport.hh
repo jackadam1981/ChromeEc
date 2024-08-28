892c95826a7ddecb40731729b194746efe5b9850
Yuval Peress <peress@google.com>
2024-08-28 10:05:30 -0600

    WIP
    BUG=b:345530593
    TEST=None
    
    Change-Id: Id38895202cbdf80553fb73faaba30cb02a5e7a6b

---
+++ zephyr/drivers/dsp_comms/include/cros/dsp/internal/service_transport.hh
@@ -0,0 +1,204 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#pragma once
+
+#include <pb_encode.h>
+
+#include <algorithm>
+#include <cstddef>
+#include <limits>
+#include <utility>
+
+#include "cros/dsp/service/transport_state_machine.hh"
+#include "proto/ec_ish.pb.h"
+#include "pw_bytes/span.h"
+#include "pw_function/function.h"
+#include "pw_preprocessor/util.h"
+#include "pw_result/result.h"
+#include "pw_status/status.h"
+#include "pw_status/status_with_size.h"
+
+namespace cros::dsp::service {
+
+template <typename StatusType,
+          uint8_t kStatusFlagHasResponse,
+          size_t kResponseBufferSize,
+          size_t kStatusBufferSize,
+          typename StatusFieldType,
+          StatusFieldType StatusType::*flags_field,
+          typename ResponseSizeFieldType,
+          ResponseSizeFieldType StatusType::*response_size_field>
+class Transport : public TransportStateMachine {
+ public:
+  template <typename T>
+  using SerializeFn =
+      pw::Function<pw::StatusWithSize(const T& data, pw::ByteSpan out)>;
+
+  explicit constexpr Transport(SerializeFn<StatusType>&& serialize_status_fn)
+      : status_(),
+        status_flags_(),
+        serialize_status_fn_(std::move(serialize_status_fn)),
+        notify_client_fn_(nullptr) {
+    status_flags_ = pw::ByteSpan(
+        reinterpret_cast<std::byte*>(&(status_.*flags_field)),
+        sizeof(std::remove_reference_t<decltype(status_.*flags_field)>));
+  }
+
+  virtual ~Transport() = default;
+
+  void SetNotifyClientCallback(
+      pw::Function<void(bool has_data)>&& notify_client_fn) {
+    notify_client_fn_ = std::move(notify_client_fn);
+  }
+
+  template <typename T>
+  pw::Status StageResponse(const T& response, SerializeFn<T> serialize) {
+    // Must be either IDLE or HAS_STAGED_STATUS
+    if (!StateAnyOf({TransportStateMachine::State::IDLE,
+                     TransportStateMachine::State::HAS_STATUS})) {
+      return pw::Status::ResourceExhausted();
+    }
+
+    // Serialize the result
+    auto serialize_result = serialize(response, response_buffer_.FullSpan());
+    if (!serialize_result.ok()) {
+      return serialize_result.status();
+    }
+    response_buffer_.SetSize(serialize_result.size());
+
+    // Update the size field
+    if (auto rc = UpdateResponseSize(serialize_result.size()); !rc.ok()) {
+      return rc;
+    }
+
+    // Update the status flag to signal there's data
+    if (auto rc = DoSetStatusBit(kStatusFlagHasResponse); !rc.ok()) {
+      return rc;
+    }
+
+    printf("Setting new state to HAS_STATUS_AND_RESPONSE\n");
+
+    return SetNewStateAndNotify(
+        TransportStateMachine::State::HAS_STATUS_AND_RESPONSE);
+  }
+
+  pw::Status SetStatusBit(uint8_t bit_num) {
+    if (auto status = DoSetStatusBit(bit_num); !status.ok()) {
+      return status;
+    }
+
+    auto state = current_state();
+    if (state == TransportStateMachine::State::IDLE) {
+      return SetNewStateAndNotify(TransportStateMachine::State::HAS_STATUS);
+    }
+    return pw::OkStatus();
+  }
+
+  pw::Result<pw::ConstByteSpan> ReadNextMessage() {
+    if (StateAnyOf({TransportStateMachine::State::HAS_STATUS,
+                    TransportStateMachine::State::HAS_STATUS_AND_RESPONSE})) {
+      auto serialize_result =
+          serialize_status_fn_(status_, status_buffer_.FullSpan());
+      if (!serialize_result.ok()) {
+        return serialize_result.status();
+      }
+      status_ = {};
+      status_buffer_.SetSize(serialize_result.size());
+      if (current_state() == TransportStateMachine::State::HAS_STATUS) {
+        SetNewStateAndNotify(TransportStateMachine::State::IDLE);
+      } else {
+        SetNewStateAndNotify(TransportStateMachine::State::HAS_RESPONSE);
+      }
+      printf("Returning status\n");
+      return status_buffer_.DataSpan();
+    }
+
+    if (current_state() == TransportStateMachine::State::HAS_RESPONSE) {
+      // At this point we may have gotten a new status bit since we sent the
+      // last one. Check to see if the status is empty.
+      StatusType empty_status = {};
+      if (std::memcmp(&status_, &empty_status, sizeof(StatusType)) == 0) {
+        printf("Returning response, moving to IDLE\n");
+        SetNewStateAndNotify(TransportStateMachine::State::IDLE);
+      } else {
+        printf("Returning response, moving to HAS_STATUS\n");
+        SetNewStateAndNotify(TransportStateMachine::State::HAS_STATUS);
+      }
+      return response_buffer_.DataSpan();
+    }
+
+    return pw::Status::NotFound();
+  }
+
+ private:
+  using TransportStateMachine::SetNewState;
+
+  pw::Status SetNewStateAndNotify(TransportStateMachine::State new_state) {
+    auto old_state = current_state();
+    if (auto rc = SetNewState(new_state); !rc.ok()) {
+      return rc;
+    }
+    if (new_state == old_state) {
+      printf("State didn't change, returning OK\n");
+      return pw::OkStatus();
+    }
+    if (new_state == TransportStateMachine::State::HAS_STATUS ||
+        new_state == TransportStateMachine::State::HAS_STATUS_AND_RESPONSE) {
+      notify_client_fn_(true);
+    } else if ((new_state == TransportStateMachine::State::IDLE &&
+                old_state == TransportStateMachine::State::HAS_STATUS) ||
+               (new_state == TransportStateMachine::State::HAS_RESPONSE &&
+                old_state ==
+                    TransportStateMachine::State::HAS_STATUS_AND_RESPONSE)) {
+      notify_client_fn_(false);
+    }
+    return pw::OkStatus();
+  }
+
+  pw::Status DoSetStatusBit(uint8_t bit_num) {
+    if (bit_num / 8 >= status_flags_.size()) {
+      return pw::Status::OutOfRange();
+    }
+    reinterpret_cast<uint8_t*>(status_flags_.data())[bit_num / 8] |=
+        (1 << (bit_num % 8));
+    return pw::OkStatus();
+  }
+
+  pw::Status UpdateResponseSize(size_t new_size) {
+    if (new_size > std::numeric_limits<ResponseSizeFieldType>::max()) {
+      return pw::Status::InvalidArgument();
+    }
+    (status_.*response_size_field) = new_size;
+    return pw::OkStatus();
+  }
+
+  StatusType status_ = {};
+  pw::ByteSpan status_flags_;
+  SerializeFn<StatusType> serialize_status_fn_;
+  pw::Function<void(bool has_data)> notify_client_fn_;
+
+  template <size_t kCapacity>
+  class Buffer {
+   public:
+    void Clear() { memset(buffer_, 0, kCapacity); }
+    void SetSize(size_t size) {
+      PW_ASSERT(size <= kCapacity);
+      size_ = size;
+    }
+    pw::ByteSpan FullSpan() { return pw::ByteSpan(buffer_, capacity_); }
+    pw::ByteSpan DataSpan() { return pw::ByteSpan(buffer_, size_); }
+
+   private:
+    std::byte buffer_[kCapacity] = {};
+    const size_t capacity_ = kCapacity;
+    size_t size_ = 0;
+  };
+
+  Buffer<kResponseBufferSize> response_buffer_;
+  Buffer<kStatusBufferSize> status_buffer_;
+};
+
+}  // namespace cros::dsp::service
---
+++ zephyr/drivers/dsp_comms/test_comms.cc
@@ -0,0 +1,314 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <gtest/gtest.h>
+#include <stdint.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/gpio/gpio_emul.h>
+#include <zephyr/kernel.h>
+
+#include <cstring>
+
+#include "cros/dsp/client.h"
+#include "cros_board_info.h"
+
+// DECLARE_FAKE_VALUE_FUNC(int, crec_flash_unprotected_read, int, int, char *);
+
+namespace {
+constexpr const struct device* kClient =
+    DEVICE_DT_GET_OR_NULL(DT_ALIAS(test_dsp_client));
+constexpr const struct device* kService =
+    DEVICE_DT_GET_OR_NULL(DT_ALIAS(test_dsp_service));
+static_assert(kClient != nullptr, "Missing alias 'test_dsp_client'");
+static_assert(kService != nullptr, "Missing alias 'test_dsp_service'");
+
+constexpr const struct gpio_dt_spec kClientInterruptSpec =
+    GPIO_DT_SPEC_GET(DT_ALIAS(test_dsp_client), int_gpios);
+constexpr const struct gpio_dt_spec kServiceInterruptSpec =
+    GPIO_DT_SPEC_GET(DT_ALIAS(test_dsp_service), int_gpios);
+
+constexpr const uint32_t kDefaultBoardVersion = 0x12345678;
+constexpr const uint32_t kDefaultOemId = 0x23456789;
+constexpr const uint32_t kDefaultSkuId = 0x3456789a;
+constexpr const uint32_t kDefaultModelId = 0x456789ab;
+constexpr const uint32_t kDefaultFwConfig = 0x56789abc;
+constexpr const uint32_t kDefaultPcbSupplier = 0x6789abcd;
+constexpr const uint32_t kDefaultSsfc = 0x789abcde;
+constexpr const uint64_t kDefaultReworkId = 0x89abcdef01234567;
+constexpr const uint32_t kDefaultFactoryCalibrationData = 0x9abcdef0;
+constexpr const char* kDefaultDramPartNum = "DRAM-123";
+constexpr const char* kDefaultOemName = "Google";
+
+#define SUSPEND() k_usleep(1)
+
+class DspComms : public ::testing::Test {
+ protected:
+  DspComms() {
+    // We have to poke any CBI value to make sure that it was
+    // initialized
+    uint32_t ver;
+    cbi_get_board_version(&ver);
+  }
+  void SetUp() override {
+    // RESET_FAKE(crec_flash_unprotected_read);
+
+    // crec_flash_unprotected_read_fake.custom_fake =
+    // crec_flash_physical_read; FFF_RESET_HISTORY(); Set default
+    // values
+    SUSPEND();
+    ASSERT_EQ(0,
+              cbi_set_board_info(
+                  CBI_TAG_BOARD_VERSION,
+                  reinterpret_cast<const uint8_t*>(&kDefaultBoardVersion),
+                  static_cast<uint8_t>(sizeof(kDefaultBoardVersion))));
+
+    ASSERT_EQ(
+        0,
+        cbi_set_board_info(CBI_TAG_OEM_ID,
+                           reinterpret_cast<const uint8_t*>(&kDefaultOemId),
+                           static_cast<uint8_t>(sizeof(kDefaultOemId))));
+
+    ASSERT_EQ(
+        0,
+        cbi_set_board_info(CBI_TAG_SKU_ID,
+                           reinterpret_cast<const uint8_t*>(&kDefaultSkuId),
+                           static_cast<uint8_t>(sizeof(kDefaultSkuId))));
+
+    ASSERT_EQ(
+        0,
+        cbi_set_board_info(CBI_TAG_MODEL_ID,
+                           reinterpret_cast<const uint8_t*>(&kDefaultModelId),
+                           static_cast<uint8_t>(sizeof(kDefaultModelId))));
+
+    ASSERT_EQ(
+        0,
+        cbi_set_board_info(CBI_TAG_FW_CONFIG,
+                           reinterpret_cast<const uint8_t*>(&kDefaultFwConfig),
+                           static_cast<uint8_t>(sizeof(kDefaultFwConfig))));
+
+    ASSERT_EQ(0,
+              cbi_set_board_info(
+                  CBI_TAG_PCB_SUPPLIER,
+                  reinterpret_cast<const uint8_t*>(&kDefaultPcbSupplier),
+                  static_cast<uint8_t>(sizeof(kDefaultPcbSupplier))));
+
+    ASSERT_EQ(
+        0,
+        cbi_set_board_info(CBI_TAG_SSFC,
+                           reinterpret_cast<const uint8_t*>(&kDefaultSsfc),
+                           static_cast<uint8_t>(sizeof(kDefaultSsfc))));
+
+    ASSERT_EQ(
+        0,
+        cbi_set_board_info(CBI_TAG_REWORK_ID,
+                           reinterpret_cast<const uint8_t*>(&kDefaultReworkId),
+                           static_cast<uint8_t>(sizeof(kDefaultReworkId))));
+
+    ASSERT_EQ(
+        0,
+        cbi_set_board_info(
+            CBI_TAG_FACTORY_CALIBRATION_DATA,
+            reinterpret_cast<const uint8_t*>(&kDefaultFactoryCalibrationData),
+            static_cast<uint8_t>(sizeof(kDefaultFactoryCalibrationData))));
+
+    ASSERT_EQ(0,
+              cbi_set_board_info(
+                  CBI_TAG_DRAM_PART_NUM,
+                  reinterpret_cast<const uint8_t*>(kDefaultDramPartNum),
+                  static_cast<uint8_t>(std::strlen(kDefaultDramPartNum))));
+
+    ASSERT_EQ(
+        0,
+        cbi_set_board_info(CBI_TAG_OEM_NAME,
+                           reinterpret_cast<const uint8_t*>(kDefaultOemName),
+                           static_cast<uint8_t>(std::strlen(kDefaultOemName))));
+
+    gpio_callbacks_.handler = [](const struct device* port,
+                                 struct gpio_callback*,
+                                 gpio_port_pins_t pins) {
+      while (pins != 0) {
+        int pin = __builtin_ctz(pins);
+        int value = gpio_emul_output_get(port, pin);
+        SUSPEND();
+        gpio_emul_input_set(
+            kClientInterruptSpec.port, kClientInterruptSpec.pin, value);
+
+        SUSPEND();
+        pins &= ~BIT(pin);
+      }
+    };
+    gpio_callbacks_.pin_mask = BIT(kServiceInterruptSpec.pin);
+
+    int current_service_pin_value = gpio_emul_output_get(
+        kServiceInterruptSpec.port, kServiceInterruptSpec.pin);
+
+    gpio_emul_input_set(kClientInterruptSpec.port,
+                        kClientInterruptSpec.pin,
+                        current_service_pin_value);
+    ASSERT_EQ(0,
+              gpio_add_callback_dt(&kServiceInterruptSpec, &gpio_callbacks_));
+  }
+
+  void TearDown() override {
+    ASSERT_EQ(
+        0, gpio_remove_callback_dt(&kServiceInterruptSpec, &gpio_callbacks_));
+  }
+
+  struct gpio_callback gpio_callbacks_ = {};
+};
+
+TEST_F(DspComms, ReadUnsupportedTag) {
+  uint32_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(-EINVAL,
+            cbi_remote_get_board_info(
+                CBI_TAG_COUNT, reinterpret_cast<uint8_t*>(&out), &size));
+}
+
+TEST_F(DspComms, FailToReadSmallBuffer) {
+  uint8_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  EXPECT_EQ(
+      -ENOMEM,
+      cbi_remote_get_board_info(
+          CBI_TAG_BOARD_VERSION, reinterpret_cast<uint8_t*>(&out), &size));
+
+  EXPECT_EQ(-ENOMEM,
+            cbi_remote_get_board_info(
+                CBI_TAG_REWORK_ID, reinterpret_cast<uint8_t*>(&out), &size));
+
+  EXPECT_EQ(
+      -ENOMEM,
+      cbi_remote_get_board_info(
+          CBI_TAG_DRAM_PART_NUM, reinterpret_cast<uint8_t*>(&out), &size));
+}
+
+TEST_F(DspComms, ReadCbiVersion) {
+  uint32_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(
+      0,
+      cbi_remote_get_board_info(
+          CBI_TAG_BOARD_VERSION, reinterpret_cast<uint8_t*>(&out), &size));
+  ASSERT_EQ(kDefaultBoardVersion, out);
+  ASSERT_EQ(size, static_cast<uint8_t>(sizeof(out)));
+}
+
+TEST_F(DspComms, ReadCbiOemId) {
+  uint32_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(
+                CBI_TAG_OEM_ID, reinterpret_cast<uint8_t*>(&out), &size));
+  ASSERT_EQ(kDefaultOemId, out);
+  ASSERT_EQ(size, static_cast<uint8_t>(sizeof(out)));
+}
+
+TEST_F(DspComms, ReadCbiSkuId) {
+  uint32_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(
+                CBI_TAG_SKU_ID, reinterpret_cast<uint8_t*>(&out), &size));
+  ASSERT_EQ(kDefaultSkuId, out);
+  ASSERT_EQ(size, static_cast<uint8_t>(sizeof(out)));
+}
+
+TEST_F(DspComms, ReadCbiModelId) {
+  uint32_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(
+                CBI_TAG_MODEL_ID, reinterpret_cast<uint8_t*>(&out), &size));
+  ASSERT_EQ(kDefaultModelId, out);
+  ASSERT_EQ(size, static_cast<uint8_t>(sizeof(out)));
+}
+
+TEST_F(DspComms, ReadCbiFwConfig) {
+  uint32_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(
+                CBI_TAG_FW_CONFIG, reinterpret_cast<uint8_t*>(&out), &size));
+  ASSERT_EQ(kDefaultFwConfig, out);
+  ASSERT_EQ(size, static_cast<uint8_t>(sizeof(out)));
+}
+
+TEST_F(DspComms, ReadCbiPcbSupplier) {
+  uint32_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(
+                CBI_TAG_PCB_SUPPLIER, reinterpret_cast<uint8_t*>(&out), &size));
+  ASSERT_EQ(kDefaultPcbSupplier, out);
+  ASSERT_EQ(size, static_cast<uint8_t>(sizeof(out)));
+}
+
+TEST_F(DspComms, ReadCbiSsfc) {
+  uint32_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(
+                CBI_TAG_SSFC, reinterpret_cast<uint8_t*>(&out), &size));
+  ASSERT_EQ(kDefaultSsfc, out);
+  ASSERT_EQ(size, static_cast<uint8_t>(sizeof(out)));
+}
+
+TEST_F(DspComms, ReadCbiReworkId) {
+  uint64_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(
+                CBI_TAG_REWORK_ID, reinterpret_cast<uint8_t*>(&out), &size));
+  ASSERT_EQ(kDefaultReworkId, out);
+  ASSERT_EQ(size, static_cast<uint8_t>(sizeof(out)));
+}
+
+TEST_F(DspComms, ReadCbiFactoryCalibrationData) {
+  uint32_t out;
+  uint8_t size = static_cast<uint8_t>(sizeof(out));
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(CBI_TAG_FACTORY_CALIBRATION_DATA,
+                                      reinterpret_cast<uint8_t*>(&out),
+                                      &size));
+  ASSERT_EQ(kDefaultFactoryCalibrationData, out);
+  ASSERT_EQ(size, static_cast<uint8_t>(sizeof(out)));
+}
+
+TEST_F(DspComms, ReadCbiDramPartNum) {
+  char out[80];
+  uint8_t size = 80;
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(
+                CBI_TAG_DRAM_PART_NUM, reinterpret_cast<uint8_t*>(out), &size));
+  ASSERT_STREQ(kDefaultDramPartNum, out);
+  ASSERT_LE(size, 80);
+}
+
+TEST_F(DspComms, ReadCbiOemName) {
+  char out[80];
+  uint8_t size = 80;
+
+  ASSERT_EQ(0,
+            cbi_remote_get_board_info(
+                CBI_TAG_OEM_NAME, reinterpret_cast<uint8_t*>(out), &size));
+  ASSERT_STREQ(kDefaultOemName, out);
+  ASSERT_LE(size, 80);
+}
+
+}  // namespace
