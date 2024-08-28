d25d6314c8ddf6b2de02e8497c8388f47aad315d
Al Semjonovs <asemjonovs@google.com>
2024-08-28 10:49:28 -0600

    pdc_power_mgmt: Consolidate emulated PDC PDO functions
    Reduce copy/paste code for emulated PDO handling and
    enable tests for TI.
    
    BUG=b:339686045, b:345292002
    TEST=./twister -T zephyr/test/pdc
    
    Change-Id: Iaddfa95688a1df65d7b0d1ce7cfe679c2a141b66

--- zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
+++ zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
@@ -456,8 +456,6 @@ ZTEST_USER(pdc_power_mgmt_api, test_get_partner_usb_comm_capable)
 	}
 }
 
-/* TODO(b/345292002): Implement set_pdo for TPS6699x emulator/driver. */
-#ifndef CONFIG_TODO_B_345292002
 ZTEST_USER(pdc_power_mgmt_api, test_get_partner_data_swap_capable)
 {
 	int i;
@@ -529,7 +527,6 @@ ZTEST_USER(pdc_power_mgmt_api, test_get_partner_data_swap_capable)
 					   PDC_TEST_TIMEOUT));
 	}
 }
-#endif
 
 ZTEST_USER(pdc_power_mgmt_api, test_get_info)
 {
@@ -835,8 +832,6 @@ ZTEST_USER(pdc_power_mgmt_api, test_request_data_swap)
 	}
 }
 
-/* TODO(b/345292002): Implement set_pdo for TPS6699x emulator/driver. */
-#ifndef CONFIG_TODO_B_345292002
 ZTEST_USER(pdc_power_mgmt_api, test_get_partner_unconstr_power)
 {
 	union connector_status_t connector_status;
@@ -890,6 +885,8 @@ ZTEST_USER(pdc_power_mgmt_api, test_get_partner_unconstr_power)
 				   PDC_TEST_TIMEOUT));
 }
 
+/* TODO(b/345292002): Implement set_pdo for TPS6699x emulator/driver. */
+#ifndef CONFIG_TODO_B_345292002
 ZTEST_USER(pdc_power_mgmt_api, test_get_vbus_voltage)
 {
 /* Keep in line with |pdc_power_mgmt_api.c|. */
@@ -1198,8 +1195,6 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_resume_no_partner)
 	zassert_equal(CCOM_DRP, ccom);
 }
 
-/* TODO(b/345292002): Implement set_pdo for TPS6699x emulator/driver. */
-#ifndef CONFIG_TODO_B_345292002
 ZTEST_USER(pdc_power_mgmt_api, test_chipset_resume_drp_partner)
 {
 	union connector_status_t connector_status;
@@ -1248,7 +1243,6 @@ ZTEST_USER(pdc_power_mgmt_api, test_chipset_resume_up_drp_partner)
 	zassert_ok(emul_pdc_get_pdr(emul, &pdr), "Invalid PDR value in emul");
 	zassert_equal(pdr.swap_to_src, 0);
 }
-#endif
 
 ZTEST_USER(pdc_power_mgmt_api, test_chipset_startup)
 {
