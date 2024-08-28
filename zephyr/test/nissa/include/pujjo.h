058d885f228c512f4092ef940e9ea5756d6ed353
David1 Huang <david1.huang@lcfc.corp-partner.google.com>
2024-08-29 00:11:26 +0800

    pujjo: Add override cbi function.
    Use fan detect to override cbi skuidand fw_config.
    
    BUG=b:361519672
    TEST=1.Check can return correct cbi info on system
         2.After hibernation,EC reset, batterycutoff, the SKU ID override well.
         3.50 times bootup (including hibernation, EC reset, Battery cut off) didn't see AP get non-overrided CBI
    LOW_COVERAGE_REASON=test code is not yet but need implement. b:361519672
    
    Change-Id: Ie73845515bdd86f21cf9c12b4a1b5baad83a6f7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5824448
    Commit-Queue: Shou-Chieh Hsu <shouchieh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>
    Tested-by: David1 Huang <david1.huang@lcfc.corp-partner.google.com>
    Auto-Submit: David1 Huang <david1.huang@lcfc.corp-partner.google.com>
    (cherry picked from commit b3e6bb50c74ee998046b52d9b6511d0c655b7e62)

---
+++ zephyr/test/nissa/include/pujjo.h
@@ -0,0 +1,16 @@
+<<<<<<< HEAD   (c7f9ae Merge remote-tracking branch cros/main into firmware-nissa-1)
+=======
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_TEST_NISSA_INCLUDE_PUJJO_H_
+#define ZEPHYR_TEST_NISSA_INCLUDE_PUJJO_H_
+
+#include "ec_commands.h"
+
+extern const struct ec_response_keybd_config pujjo_kb;
+
+#endif /* ZEPHYR_TEST_NISSA_INCLUDE_PUJJO_H_ */
+>>>>>>> CHANGE (b3e6bb pujjo: Add override cbi function.)
