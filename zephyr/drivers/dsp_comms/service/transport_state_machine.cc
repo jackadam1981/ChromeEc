80a482940624fb813f08c43ff3c059c16562af9f
Yuval Peress <peress@google.com>
2024-08-28 10:05:21 -0600

    dsp_comms: Add state machine
    In order to allow the test build to get to the logic there's a
    temporary kconfig added which bypasses the requirement to have
    the devicetree node. The alternative would have been a top down
    implementation which would require bigger CLs.
    
    BUG=b:345530593
    TEST=twister -cvi -s cros.dsp.service.transport_state_machine
    TEST=zmake build brox brox-ish brox-ish-ec
    
    Change-Id: Iea58b0eb5a965cf791da9d419c3479a6919fa84a

---
+++ zephyr/drivers/dsp_comms/service/transport_state_machine.cc
@@ -0,0 +1,30 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "cros/dsp/service/transport_state_machine.hh"
+
+#include <algorithm>
+
+namespace cros::dsp::service {
+
+pw::Status TransportStateMachine::SetNewState(TransportStateMachine::State new_state) {
+  if (state_ == new_state) {
+    return pw::OkStatus();
+  }
+  for (auto& transition : allowed_transitions_) {
+    if (transition.from == state_ && transition.to == new_state) {
+      state_ = new_state;
+      return pw::OkStatus();
+    }
+  }
+  return pw::Status::InvalidArgument();
+}
+
+bool TransportStateMachine::StateAnyOf(std::vector<TransportStateMachine::State> states) const {
+  return std::any_of(
+      states.cbegin(), states.cend(), [this](State other) -> bool { return state_ == other; });
+}
+
+}  // namespace cros::dsp::service
