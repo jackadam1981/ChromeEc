98b24dc2e79c27ee58954be71406a45efee0c83a
David1 Huang <david1.huang@lcfc.corp-partner.google.com>
2024-08-29 00:11:26 +0800

    pujjo: Add override cbi function.
    Use fan detect to override cbi skuidand fw_config.
    
    BUG=b:361519672
    TEST=Check can return correct cbi info on system.
    
    Change-Id: Ie73845515bdd86f21cf9c12b4a1b5baad83a6f7c

--- zephyr/program/nissa/pujjo/src/fan.c
+++ zephyr/program/nissa/pujjo/src/fan.c
@@ -3,7 +3,9 @@
  * found in the LICENSE file.
  */
 
+#include "cros_board_info.h"
 #include "cros_cbi.h"
+#include "dptf.h"
 #include "fan.h"
 #include "gpio/gpio.h"
 #include "hooks.h"
@@ -12,7 +14,58 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
+LOG_MODULE_DECLARE(nissa, LOG_LEVEL_INF);
+
+enum override_status {
+	OVERRIDE_NONE,
+	OVERRIDE_15W,
+	OVERRIDE_6W,
+};
+static int override_flag = OVERRIDE_NONE;
+
+int cbi_board_override(enum cbi_data_tag tag, uint8_t *buf, uint8_t *size)
+{
+	uint32_t id = 0xa00ab;
+	uint32_t fw_config = 0x20029017;
+	LOG_INF("cbi_board_override %d", tag);
+	if (override_flag == OVERRIDE_15W) {
+		if (tag == CBI_TAG_SKU_ID) {
+			LOG_INF("rewrite skuid");
+			memcpy(buf, &id, *size);
+		}
+		if (tag == CBI_TAG_FW_CONFIG) {
+			LOG_INF("rewrite fwconfig");
+			memcpy(buf, &fw_config, *size);
+		}
+	}
+	return EC_SUCCESS;
+}
+
+void check_fan_status(void)
+{
+	int rpm;
+
+	rpm = fan_get_rpm_actual(0);
+	if (rpm != 0) {
+		LOG_INF("Override true");
+		override_flag = OVERRIDE_15W;
+	} else {
+		/* Disable the fan */
+		fan_set_count(0);
+		dptf_set_fan_duty_target(0);
+		LOG_INF("Override false2");
+		override_flag = OVERRIDE_6W;
+	}
+	set_thermal_control_enabled(0, 1);
+}
+DECLARE_DEFERRED(check_fan_status);
+
+void set_fan_status(void)
+{
+	dptf_set_fan_duty_target(100);
+	hook_call_deferred(&check_fan_status_data, 100 * MSEC);
+}
+DECLARE_DEFERRED(set_fan_status);
 
 /*
  * Pujjo fan support
@@ -30,12 +83,30 @@ test_export_static void fan_init(void)
 		return;
 	}
 	if (val != FW_FAN_PRESENT) {
-		/* Disable the fan */
-		fan_set_count(0);
+		ret = cbi_get_sku_id(&val);
+		if (ret != 0) {
+			LOG_ERR("Error retrieving CBI FW_CONFIG field %d",
+				FW_FAN);
+			return;
+		}
+		if (override_flag != 0)
+			return;
+		if (val == 0xa002a) {
+			/* Configure the fan enable GPIO */
+			gpio_pin_configure_dt(
+				GPIO_DT_FROM_NODELABEL(gpio_fan_enable),
+				GPIO_OUTPUT);
+			hook_call_deferred(&set_fan_status_data, 200 * MSEC);
+		} else {
+			/* Disable the fan */
+			fan_set_count(0);
+			LOG_INF("Override false1");
+			override_flag = OVERRIDE_6W;
+		}
 	} else {
 		/* Configure the fan enable GPIO */
 		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_fan_enable),
 				      GPIO_OUTPUT);
 	}
 }
-DECLARE_HOOK(HOOK_INIT, fan_init, HOOK_PRIO_POST_FIRST);
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, fan_init, HOOK_PRIO_DEFAULT + 1);
