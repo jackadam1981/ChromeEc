afc9e500323be5919ea865fb05fe3b8065a2e603
Keith Short <keithshort@chromium.org>
2024-08-30 15:55:06 -0600

    brox: Don't assert RSMRST during power down.
    Brox follower devices include a diode that asserts EC_RSMRST_L low when
    EC_DSW_PWROK is driven low.  This ensures the signals go to physical
    state 0 simultaneously.
    
    The brox reference is missing the diode, so it still needs to assert
    EC_RSMRST_L during the shutdown sequence.
    
    BUG=b:351912054
    TEST=Power AP up and down on brox
    
    Change-Id: Ib653511bd1c178f4ff1331ceeb1f3201fb31531b

--- zephyr/program/brox/src/board_power.c
+++ zephyr/program/brox/src/board_power.c
@@ -26,7 +26,16 @@ void board_ap_power_force_shutdown(void)
 {
 	int timeout_ms = X86_NON_DSX_ADLP_NONPWRSEQ_FORCE_SHUTDOWN_TO_MS;
 
-	power_signal_set(PWR_EC_PCH_RSMRST, 1);
+	/*
+	 * Brox follower devices populate a diode so that de-asserting
+	 * DSW_PWROK also asserts RSMRST.
+	 *
+	 * Brox is missing this, so the Brox specifically asserts RSMRST
+	 * before DSR_PWROK.
+	 */
+	if (IS_ENABLED(CONFIG_BOARD_BROX)) {
+		power_signal_set(PWR_EC_PCH_RSMRST, 1);
+	}
 	power_signal_set(PWR_EC_SOC_DSW_PWROK, 0);
 
 	/* TODO: replace with power_wait_signals_on_timeout()? */
