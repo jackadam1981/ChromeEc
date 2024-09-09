51fc6216a766af5281e775098abc0dc5ee30dab9
Firas Sammoura <fsammoura@google.com>
2024-09-09 21:08:30 +0000

    ec: Strip the source paths for boringssl strings
    Add all compile flags in toolchain-common.cmake using set rather than
    add_compile_options. Use C_CXX_FLAGS as a temp variable. Concatenate all
    flags to CMAKE_C_FLAGS and CMAKE_CXX_FLAGS. Set the -mcpu= conditionally
    by passing a CORTEX_M7 definition using the rules.mk build file. This
    variable is set to 1 when dartmonkey or nocturne_fp boards are being
    built.
    
    BUG=b:361855883
    TEST=CQ
    TEST=make BOARD=bloonchipper -j
    TEST=strings build/bloonchipper/ec.bin |grep crypto
    TEST=make BOARD=dartmonkey -j
    TEST=strings build/dartmonkey/ec.bin |grep crypto
    TEST=make BOARD=helipilot -j
    TEST=strings build/helipilot/ec.bin |grep crypto
    
    Change-Id: Ie477ba22cac7a1a59f5d0de1ae7d1adee78d8320

--- third_party/boringssl/boringssl-toolchain.cmake
+++ third_party/boringssl/boringssl-toolchain.cmake
@@ -15,4 +15,4 @@ add_definitions(-DCROS_EC)
 # recude the final code size.
 # The reason we don't use "-ffile-prefix-map" here is because we don't want to
 # break the debug symbols for debugging.
-add_compile_options(-fmacro-prefix-map="${CMAKE_CURRENT_SOURCE_DIR}/=")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fmacro-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}/=.")
