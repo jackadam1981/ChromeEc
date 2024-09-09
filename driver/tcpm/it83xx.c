71bd8390b18c43c288c3fb034bb113d3eb6c6b64
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2024-09-09 14:34:10 +0800

    driver/tcpm/it83xx.c: improve SenderResponseTimer accuracy
    PD task use pd_transmit_complete as the starting point of
    SenderResponseTimer.
    However, The it83xx tcpm driver calls pd_transmit_complete() in PD task.
    The timestamp stored in pd_transmit_complete() may have ~1ms delay
    compare to the actual Tx complete time.
    To improve the timer accuracy, we want to call pd_transmit_complete as
    early as possible, thus move this function call into IRQ handler.
    
    BUG=none
    TEST=connect dongle and adaptor to drawcia port0, then can go to ready
         state
         test PD CTS TEST.PD.FRS.SNK3.4, but not support FRS...?
    
    Change-Id: I78c77991c323bc999746a6ec5014ae3ab05e63ab

--- driver/tcpm/it83xx.c
+++ driver/tcpm/it83xx.c
@@ -570,7 +570,7 @@ static int it83xx_tcpm_set_vconn(int port, int enable)
 			 * dropped below 3.3v (>500us) to avoid the potential
 			 * risk of voltage fed back into Vcore.
 			 */
-			crec_usleep(IT83XX_USBPD_T_VCONN_BELOW_3_3V);
+			udelay(IT83XX_USBPD_T_VCONN_BELOW_3_3V);
 			/*
 			 * Since our cc are not Vconn SRC, enable cc analog
 			 * module (ex.UP/RD/DET/Tx/Rx) and disable 5v tolerant.
@@ -737,6 +737,7 @@ static int it83xx_tcpm_transmit(int port, enum tcpci_msg_type type,
 				uint16_t header, const uint32_t *data)
 {
 	int status = TCPC_TX_COMPLETE_FAILED;
+	bool pd_transmit_complete_called = false;
 
 	switch (type) {
 	case TCPCI_MSG_SOP:
@@ -745,6 +746,12 @@ static int it83xx_tcpm_transmit(int port, enum tcpci_msg_type type,
 	case TCPCI_MSG_SOP_DEBUG_PRIME:
 	case TCPCI_MSG_SOP_DEBUG_PRIME_PRIME:
 		status = it83xx_tx_data(port, type, header, data);
+		/* To improve the SendResponseTimer accuracy,
+		 * pd_transmit_complete() is call inside irq handler if the
+		 * message is successfully transmitted.
+		 */
+		pd_transmit_complete_called =
+			(status == TCPC_TX_COMPLETE_SUCCESS);
 		break;
 	case TCPCI_MSG_TX_BIST_MODE_2:
 		it83xx_send_bist_mode2_pattern(port);
@@ -758,7 +765,9 @@ static int it83xx_tcpm_transmit(int port, enum tcpci_msg_type type,
 		status = TCPC_TX_COMPLETE_FAILED;
 		break;
 	}
-	pd_transmit_complete(port, status);
+	if (!pd_transmit_complete_called) {
+		pd_transmit_complete(port, status);
+	}
 
 	return EC_SUCCESS;
 }
