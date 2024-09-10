eb9afdc81d81131bfde7e1e73d129287aba14029
amber.chen <amber.chen@lcfc.corp-partner.google.com>
2024-09-10 01:13:00 -0400

    sundance: modiry for ec reboot when do ec reset
    modify for ec reboot when do quick ec reset
    
    BUG=b:352477467
    TEST=make buildall pass and zmake build --all pass
    
    Change-Id: Ia16aff401efb41058d96b421a9ce649ae4c9c008

--- driver/tcpm/nct38xx.c
+++ driver/tcpm/nct38xx.c
@@ -20,6 +20,8 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio/gpio_nct38xx.h>
 #include <zephyr/drivers/mfd/nct38xx.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(nct38xx, LOG_LEVEL_INF);
 #endif
 
 #if defined(CONFIG_ZEPHYR) && defined(CONFIG_IO_EXPANDER_NCT38XX)
@@ -381,6 +383,15 @@ __maybe_unused test_export_static int nct38xx_set_frs_enable(int port,
  */
 static void nct38xx_lock(int port, int lock)
 {
+	__ASSERT(mfd_lock[port] != NULL, "unable to get mfd lock");
+	if (mfd_lock[port] == NULL) {
+		LOG_WRN("%s: mfd_lock %d, not yet initialized", __func__, port);
+		mfd_lock[port] = mfd_nct38xx_get_lock_reference(
+			tcpc_config[port].mfd_parent);
+		if (mfd_lock[port] == NULL) {
+			k_oops();
+		}
+	}
 	if (lock) {
 		k_sem_take(mfd_lock[port], K_FOREVER);
 	} else {
