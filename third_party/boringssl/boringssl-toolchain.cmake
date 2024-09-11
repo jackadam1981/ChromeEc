03ba6131c74c90a226a61f50ce48df7bf4e26421
Firas Sammoura <fsammoura@google.com>
2024-09-11 18:09:51 +0000

    test: boringssl strip string
    Change-Id: I6623b491bb35def49b7e090aa9c4f49b5da11a01
    Signed-off-by: Firas Sammoura <fsammoura@google.com>

--- third_party/boringssl/boringssl-toolchain.cmake
+++ third_party/boringssl/boringssl-toolchain.cmake
@@ -15,4 +15,8 @@ add_definitions(-DCROS_EC)
 # recude the final code size.
 # The reason we don't use "-ffile-prefix-map" here is because we don't want to
 # break the debug symbols for debugging.
-add_compile_options(-fmacro-prefix-map="${CMAKE_CURRENT_SOURCE_DIR}/=")
+add_definitions(-fmacro-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}/=.)
+add_compile_options(-fmacro-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}/=.)
+
+#set(CMAKE_C_FLAGS "$ENV{CFLAGS} -fmacro-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}/=.")
+#set(CMAKE_CXX_FLAGS "$ENV{CFLAGS}")
