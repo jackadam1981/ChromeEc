89a46bd569275be4fa3392147e75b71fa687ca94
Firas Sammoura <fsammoura@google.com>
2024-09-12 16:45:53 +0000

    fpsensor: Declare fpmaintenance as a restricted console command
    fpmaintenance is a debug command and should be declared as restricted in
    order to eliminate oracle attacks. Add a restricted console unit test
    for the case when the system is locked and unlocked.
    
    BUG=b:279821940
    TEST=run the fpmaintenance command from the console using minicom
    
    Change-Id: Ib85d7b68492f70d3f4ff3c882da6e6c1c95bbfdf

--- test/restricted_console.c
+++ test/restricted_console.c
@@ -146,6 +146,28 @@ test_static int test_command_fpclear(void)
 	return EC_SUCCESS;
 }
 
+test_static int test_command_fpmaintenance(void)
+{
+	enum ec_error_list res;
+
+	/* System is unlocked. */
+	is_locked = 0;
+
+	char console_input1[] = "fpmaintenance";
+	res = test_send_console_command(console_input1);
+	TEST_EQ(res, EC_SUCCESS, "%d");
+
+	/* System is locked. */
+	is_locked = 1;
+
+	/* Test for the case when access is denied. */
+	char console_input2[] = "fpmaintenance";
+	res = test_send_console_command(console_input2);
+	TEST_EQ(res, EC_ERROR_ACCESS_DENIED, "%d");
+
+	return EC_SUCCESS;
+}
+
 void run_test(int argc, const char **argv)
 {
 	test_reset();
@@ -156,6 +178,7 @@ void run_test(int argc, const char **argv)
 	RUN_TEST(test_command_fpdownload);
 	RUN_TEST(test_command_fpmatch);
 	RUN_TEST(test_command_fpclear);
+	RUN_TEST(test_command_fpmaintenance);
 
 	test_print_result();
 }
