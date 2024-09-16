8fae7bfa19bbd1b036679bbba287e2af64badb2a
Al Semjonovs <asemjonovs@google.com>
2024-09-16 11:32:45 -0600

    pdc_power_mgmt: Add request source voltage test
    Add test to validate request_source_voltage API.
    
    BUG=b:339686045
    TEST=./twister -c -T zephyr/test/pdc
    
    Change-Id: I4d46a6b1854c2041b5901cf0efbd5e9c7c91f398

--- zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
+++ zephyr/test/pdc/src/generic/pdc_power_mgmt_api.c
@@ -1890,6 +1890,33 @@ ZTEST_USER(pdc_power_mgmt_api, test_pdc_power_mgmt_set_active_charge_port)
 	zassert_true(TEST_WAIT_FOR(is_sink_path_enabled(), PDC_TEST_TIMEOUT));
 }
 
+ZTEST_USER(pdc_power_mgmt_api, test_request_source_voltage)
+{
+	union connector_status_t connector_status = { 0 };
+	int prev_mv = pdc_power_mgmt_get_max_voltage();
+	int source_mv = prev_mv + 1000;
+
+	emul_pdc_configure_snk(emul, &connector_status);
+	emul_pdc_connect_partner(emul, &connector_status);
+	zassert_ok(pdc_power_mgmt_resync_port_state_for_ppm(TEST_PORT));
+
+	pd_request_source_voltage(TEST_PORT, source_mv);
+	zassert_ok(pdc_power_mgmt_resync_port_state_for_ppm(TEST_PORT));
+	zassert_equal(source_mv, pdc_power_mgmt_get_max_voltage());
+
+	emul_pdc_disconnect(emul);
+	zassert_ok(pdc_power_mgmt_resync_port_state_for_ppm(TEST_PORT));
+
+	memset(&connector_status, 0, sizeof(connector_status));
+	emul_pdc_configure_src(emul, &connector_status);
+	emul_pdc_connect_partner(emul, &connector_status);
+	zassert_ok(pdc_power_mgmt_resync_port_state_for_ppm(TEST_PORT));
+
+	pd_request_source_voltage(TEST_PORT, prev_mv);
+	zassert_ok(pdc_power_mgmt_resync_port_state_for_ppm(TEST_PORT));
+	zassert_equal(prev_mv, pdc_power_mgmt_get_max_voltage());
+}
+
 /* TODO(b/345292002): The tests below fail with the TPS6699x emulator/driver. */
 #ifndef CONFIG_TODO_B_345292002
 ZTEST_USER(pdc_power_mgmt_api, test_hpd_wake)
