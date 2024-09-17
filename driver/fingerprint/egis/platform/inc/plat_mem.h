4accd4b94cd0d24658a8af191ba6b43f96275dcb
Firas Sammoura <fsammoura@google.com>
2024-09-17 18:41:58 +0000

    driver/fingerprint: Define PLAT_FREE as a static inline function
    Define PLAT_FREE as a static inline function instead of a macro. This
    will eliminate function call overhead. This approach will offer type
    safety, better debugging, and improved structure.
    
    BUG=b:356211639
    TEST=CQ
    
    Change-Id: I70bfcafac6ce96d2596647cd320a5fc1e958b07c

--- driver/fingerprint/egis/platform/inc/plat_mem.h
+++ driver/fingerprint/egis/platform/inc/plat_mem.h
@@ -18,11 +18,14 @@ static inline void plat_free(void *x)
 	sys_free(x);
 }
 
-#define plat_alloc(fmt) sys_alloc(1, fmt)
-#define PLAT_FREE(x)          \
-	if (x != NULL) {      \
-		plat_free(x); \
-		x = NULL;     \
+static inline void PLAT_FREE(void *x)
+{
+	if (x != NULL) {
+		plat_free(x);
+		x = NULL;
 	}
+}
+
+#define plat_alloc(fmt) sys_alloc(1, fmt)
 
 #endif /* __CROS_EC_DRIVER_FINGERPRINT_EGIS_PLATFORM_INC_PLAT_MEM_H_ */
