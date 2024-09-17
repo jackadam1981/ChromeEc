e96556938a2fd3c888d2ab785f82dfc96e90c951
Kshitij Shah <tij@google.com>
2024-09-17 23:02:18 +0000

    pdc: separate vid_pid to two separate values
    Update pdc_info_t struct to separate vid_pid from 1 uint32_t to 2
    uint16_t. Update RTS54xx driver, host command, and ectool commands to
    reflect this change.
    
    BUG=None
    TEST=None
    
    Change-Id: I44c7d722f6b4f2734ee61b16afc8f519f6fcb54f

--- zephyr/subsys/pd_controller/pdc_console.c
+++ zephyr/subsys/pd_controller/pdc_console.c
@@ -225,7 +225,8 @@ static int cmd_pdc_get_info(const struct shell *sh, size_t argc, char **argv)
 		      "FW Ver: %u.%u.%u\n"
 		      "PD Rev: %u\n"
 		      "PD Ver: %u\n"
-		      "VID/PID: %04x:%04x\n"
+		      "VID: %04x\n"
+		      "PID: %04x\n"
 		      "Running Flash Code: %c\n"
 		      "Flash Bank: %u\n"
 		      "Project Name: '%s'\n"
@@ -234,10 +235,8 @@ static int cmd_pdc_get_info(const struct shell *sh, size_t argc, char **argv)
 		      live, PDC_FWVER_GET_MAJOR(pdc_info.fw_version),
 		      PDC_FWVER_GET_MINOR(pdc_info.fw_version),
 		      PDC_FWVER_GET_PATCH(pdc_info.fw_version),
-		      pdc_info.pd_revision, pdc_info.pd_version,
-		      PDC_VIDPID_GET_VID(pdc_info.vid_pid),
-		      PDC_VIDPID_GET_PID(pdc_info.vid_pid),
-		      pdc_info.is_running_flash_code ? 'Y' : 'N',
+		      pdc_info.pd_revision, pdc_info.pd_version, pdc_info.vid,
+		      pdc_info.pid, pdc_info.is_running_flash_code ? 'Y' : 'N',
 		      pdc_info.running_in_flash_bank,
 		      has_proj_name ? pdc_info.project_name : "<None>",
 		      pdc_info.driver_name, pdc_info.no_fw_update ? 'N' : 'Y');
