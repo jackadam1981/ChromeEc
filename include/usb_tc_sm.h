e4cc3c722b9e884a26ccf8196a964ed56c9d55fa
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2024-09-18 15:10:19 +0800

    (TEST) TCPMv2: stop drawing current for 5V@0A contract
    Per PD r3.1 v1.8 ss 6.4.1.2.2, when a source wants a sink,
    consuming power from Vbus, to go to its lowest power state,
    the voltage[bit 19:10] shall be set to 5V and the maximum
    current shall be set to 0mA. This is used in cases where
    the source wants the sink to draw pSnkSusp (25mW).
    
    So I add sink_stop_drawing_current() for 5V@0A contract.
    
    BUG=none
    TEST=pass TEST.PD.PS.SNK.1#11 on rauru
    
    Change-Id: I706e5ed0064dafb0c762fe33bad549478b2922fb

--- include/usb_tc_sm.h
+++ include/usb_tc_sm.h
@@ -203,6 +203,14 @@ __override_proto void tc_update_pd_sleep_mask(int port);
 void tc_snk_power_off(int port);
 
 /**
+ * Instructs the Attached.SNK to stop drawing power. This function only has
+ * effect if the current Type-C state Attached.SNK.
+ *
+ * @param port USB_C port number
+ */
+void sink_stop_drawing_current(int port);
+
+/**
  * Instructs the Attached.SRC to stop supplying power. The function has
  * no effect if the current Type-C state is not Attached.SRC.
  *
