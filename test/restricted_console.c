48850dc0c1477eddcb5bc21681c35ae3d0adea68
Firas Sammoura <fsammoura@google.com>
2024-09-19 09:43:18 -0700

    test: Add unit test for fpupload command
    Add a unit test for the fpupload command when (a) the system is unlocked
    and (b) the system is locked. For the unlocked case, check 3 scenarios:
    (1) Number of arguments is not equal to 3, (2) The offset is  < 0, (3)
    The dest >= dp_buffer + FP_SENSOR_IMAGE_SIZE, and (4) Successful
    behavior.
    
    BUG=b:368109581
    TEST=./test/run_device_tests.py -b bloonchipper -t restricted_console
    
    Change-Id: I3fa1e1725000770baaa43424b25cdc776b4f7432

--- test/restricted_console.c
+++ test/restricted_console.c
@@ -3,6 +3,8 @@
  * found in the LICENSE file.
  */
 
+#include "fpc_private.h"
+#include "fpsensor_driver.h"
 #include "test_util.h"
 
 #include <stdio.h>
@@ -65,12 +67,59 @@ test_static int test_command_read_write_word(void)
 	return EC_SUCCESS;
 }
 
+test_static int test_command_fpupload(void)
+{
+	enum ec_error_list res;
+
+	/* System is unlocked. */
+	is_locked = 0;
+
+	// Test for case when number of arguments is not equals to 3.
+	char console_input1[] = "fpupload 52 image long";
+	res = test_send_console_command(console_input1);
+	TEST_EQ(res, EC_ERROR_PARAM_COUNT, "%d");
+
+	// Test for the case when offset < 0.
+	char console_input2[] = "fpupload -1 image";
+	res = test_send_console_command(console_input2);
+	TEST_EQ(res, EC_ERROR_PARAM1, "%d");
+
+#if defined(CONFIG_FP_SENSOR_FPC1025) || defined(CONFIG_FP_SENSOR_FPC1145) || \
+	defined(CONFIG_FP_SENSOR_ELAN80SG)
+	// Test for the case when dest >= fp_buffer + FP_SENSOR_IMAGE_SIZE.
+	char console_input3[] = "fpupload -1 image";
+	const char *pixels_str = "image";
+
+	int offset = FP_SENSOR_IMAGE_SIZE - FP_SENSOR_IMAGE_OFFSET;
+	snprintf(console_input3, sizeof(console_input3), "fpupload %d %s",
+		 offset, pixels_str);
+	res = test_send_console_command(console_input3);
+	TEST_EQ(res, EC_ERROR_PARAM1, "%d");
+#endif
+
+	// Test for the success case.
+	char console_input4[] = "fpupload 52 image";
+	res = test_send_console_command(console_input4);
+	TEST_EQ(res, EC_SUCCESS, "%d");
+
+	/* System is locked. */
+	is_locked = 1;
+
+	// Test for the case when access is denied.
+	char console_input5[] = "fpupload 52 image";
+	res = test_send_console_command(console_input5);
+	TEST_EQ(res, EC_ERROR_ACCESS_DENIED, "%d");
+
+	return EC_SUCCESS;
+}
+
 void run_test(int argc, const char **argv)
 {
 	test_reset();
 
 	RUN_TEST(test_command_mem_dump);
 	RUN_TEST(test_command_read_write_word);
+	RUN_TEST(test_command_fpupload);
 
 	test_print_result();
 }
