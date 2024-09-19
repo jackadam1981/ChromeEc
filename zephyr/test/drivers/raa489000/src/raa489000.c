f0b2d3cfa6287cb2d490b7f8cf2c65e7e2b98650
Dino Li <Dino.Li@ite.com.tw>
2024-09-19 11:45:13 +0800

    Raa489000: enable bist mode ASAP
    speed up the bist mode enable when received
    BIST message.
    
    BUG=b:358035069, b:359666332
    TEST=pass TEST.PD.PHY.ALL.7 Valid SOP*
    ./twister -s drivers/drivers.raa489000 -c --coverage
    --toolchain host pass
    
    Change-Id: I1cf52ad68b85265b6bf6b8f0894da4bbca3f5d2b

--- zephyr/test/drivers/raa489000/src/raa489000.c
+++ zephyr/test/drivers/raa489000/src/raa489000.c
@@ -7,7 +7,9 @@
 #include "driver/tcpm/tcpci.h"
 #include "emul/tcpc/emul_raa489000.h"
 #include "emul/tcpc/emul_tcpci.h"
+#include "test/drivers/tcpci_test_common.h"
 #include "test/drivers/test_state.h"
+#include "test/drivers/utils.h"
 #include "usb_pd_tcpm.h"
 
 #include <zephyr/drivers/emul.h>
@@ -15,6 +17,9 @@
 #include <zephyr/ztest.h>
 
 #define RAA489000_PORT 0
+#define RAA489000_EMUL_NODE DT_NODELABEL(raa489000_emul)
+
+FAKE_VALUE_FUNC(bool, pd_vbus_valid_for_bist, int);
 
 ZTEST(tcpc_raa489000, test_check_vendor)
 {
@@ -24,6 +29,80 @@ ZTEST(tcpc_raa489000, test_check_vendor)
 	zassert_equal(v, 0x45b);
 
 	tcpm_dump_registers(RAA489000_PORT);
+}
+
+void raa489000_get_rx_message_raw(const struct emul *emul,
+				  struct i2c_common_emul_data *common_data,
+				  enum usbc_port port)
+{
+	const struct tcpm_drv *drv = tcpc_config[port].drv;
+	struct tcpci_emul_msg msg;
+	uint32_t payload[1];
+	/* bist mode message header and payload */
+	uint8_t buf[32] = { 0x3, 0x30, 0x0, 0x0, 0x0, 0x80 };
+	int head;
+	int size;
+	bool bist_enabled;
+
+	RESET_FAKE(pd_vbus_valid_for_bist);
+	pd_vbus_valid_for_bist_fake.return_val = true;
+
+	tcpci_emul_set_reg(emul, TCPC_REG_RX_DETECT,
+			   TCPC_REG_RX_DETECT_SOP | TCPC_REG_RX_DETECT_SOPP);
+
+	msg.buf = buf;
+	msg.cnt = 31;
+	msg.sop_type = TCPCI_MSG_SOP;
+	zassert_equal(TCPCI_EMUL_TX_SUCCESS,
+		      tcpci_emul_add_rx_msg(emul, &msg, true),
+		      "Failed to setup emulator message");
+
+	/* Test fail on reading message */
+	i2c_common_emul_set_read_fail_reg(common_data, TCPC_REG_RX_BUFFER);
+	zassert_equal(EC_ERROR_UNKNOWN,
+		      drv->get_message_raw(port, payload, &head), NULL);
+	i2c_common_emul_set_read_fail_reg(common_data,
+					  I2C_COMMON_EMUL_NO_FAIL_REG);
+
+	/* Test bist mode true */
+	size = 29;
+	msg.cnt = size + 2;
+	msg.sop_type = TCPCI_MSG_SOP_PRIME;
+	zassert_equal(TCPCI_EMUL_TX_SUCCESS,
+		      tcpci_emul_add_rx_msg(emul, &msg, true),
+		      "Failed to setup emulator message");
+
+	zassert_equal(EC_SUCCESS, drv->set_bist_test_mode(port, false));
+	zassert_equal(EC_SUCCESS, drv->get_message_raw(port, payload, &head),
+		      NULL);
+	zassert_equal(EC_SUCCESS, drv->get_bist_test_mode(port, &bist_enabled),
+		      NULL);
+	zassert_true(bist_enabled, "BIST mode should be enabled");
+
+	/* Test bist mode already true */
+	zassert_equal(TCPCI_EMUL_TX_SUCCESS,
+		      tcpci_emul_add_rx_msg(emul, &msg, true),
+		      "Failed to setup emulator message");
+
+	zassert_equal(EC_SUCCESS, drv->set_bist_test_mode(port, true));
+	zassert_equal(EC_SUCCESS, drv->get_message_raw(port, payload, &head),
+		      NULL);
+	zassert_equal(EC_SUCCESS, drv->get_bist_test_mode(port, &bist_enabled),
+		      NULL);
+	zassert_true(bist_enabled, "BIST mode should be enabled");
+}
+
+ZTEST(tcpc_raa489000, test_raa489000_get_rx_message_raw)
+{
+	const struct emul *raa489000_emul = EMUL_DT_GET(RAA489000_EMUL_NODE);
+	struct i2c_common_emul_data *common_data =
+		emul_tcpci_generic_get_i2c_common_data(raa489000_emul);
+
+	/* set battery level to avoid entering low battery status */
+	test_set_battery_level(75);
+
+	raa489000_get_rx_message_raw(raa489000_emul, common_data,
+				     RAA489000_PORT);
 }
 
 ZTEST_SUITE(tcpc_raa489000, drivers_predicate_post_main, NULL, NULL, NULL,
