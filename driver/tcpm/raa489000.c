168d46c021c044d241f3b800dc871b55dd9e3e0b
Dino Li <Dino.Li@ite.com.tw>
2024-09-19 11:45:13 +0800

    Raa489000: enable bist mode ASAP
    speed up the bist mode enable when received
    BIST message.
    
    BUG=b:358035069
    TEST=pass TEST.PD.PHY.ALL.7 Valid SOP*
    LOW_COVERAGE_REASON=b:358035069
    
    Change-Id: I1cf52ad68b85265b6bf6b8f0894da4bbca3f5d2b

--- driver/tcpm/raa489000.c
+++ driver/tcpm/raa489000.c
@@ -24,6 +24,8 @@
 #define CPRINTS(format, args...) cprints(CC_USBPD, format, ##args)
 
 static int dev_id[CONFIG_USB_PD_PORT_MAX_COUNT] = { -1 };
+static bool raa489000_bist_mode[CONFIG_USB_PD_PORT_MAX_COUNT];
+static bool raa489000_bist_mode_pre[CONFIG_USB_PD_PORT_MAX_COUNT];
 
 #ifdef CONFIG_USB_PD_TCPC_LOW_POWER
 static int raa489000_enter_low_power_mode(int port)
@@ -336,6 +338,47 @@ int raa489000_debug_detach(int port)
 	return rv;
 }
 
+int raa489000_tcpm_get_message_raw(int port, uint32_t *payload, int *head)
+{
+	int ret = tcpci_tcpm_get_message_raw(port, payload, head);
+
+	/*
+	 * Detect bist message here and enable bist mode
+	 * BIST message: number of data objects is not zero, ext number
+	 * of data object is zero, and message type is BIST
+	 */
+	const uint32_t hdr = *head;
+	if ((PD_HEADER_EXT(hdr) == 0) && (PD_HEADER_CNT(hdr) > 0) &&
+	    (PD_HEADER_TYPE(hdr) == PD_DATA_BIST)) {
+		ret |= tcpci_set_bist_test_mode(port, true);
+	}
+
+	return ret;
+}
+
+enum ec_error_list raa489000_set_bist_test_mode(const int port,
+						const bool enable)
+{
+	int ret;
+
+	raa489000_bist_mode[port] = enable;
+
+	if (raa489000_bist_mode_pre[port] != raa489000_bist_mode[port]) {
+		ret = tcpci_set_bist_test_mode(port, enable);
+		raa489000_bist_mode_pre[port] = raa489000_bist_mode[port];
+		return ret;
+	}
+
+	return EC_SUCCESS;
+}
+
+enum ec_error_list raa489000_get_bist_test_mode(const int port, bool *enable)
+{
+	*enable = raa489000_bist_mode[port];
+
+	return EC_SUCCESS;
+}
+
 /* RAA489000 is a TCPCI compatible port controller */
 const struct tcpm_drv raa489000_tcpm_drv = {
 	.init = &raa489000_init,
@@ -356,7 +399,7 @@ const struct tcpm_drv raa489000_tcpm_drv = {
 	.set_vconn = &tcpci_tcpm_set_vconn,
 	.set_msg_header = &tcpci_tcpm_set_msg_header,
 	.set_rx_enable = &tcpci_tcpm_set_rx_enable,
-	.get_message_raw = &tcpci_tcpm_get_message_raw,
+	.get_message_raw = &raa489000_tcpm_get_message_raw,
 	.transmit = &tcpci_tcpm_transmit,
 	.tcpc_alert = &tcpci_tcpc_alert,
 #ifdef CONFIG_USB_PD_DISCHARGE_TCPC
@@ -370,8 +413,8 @@ const struct tcpm_drv raa489000_tcpm_drv = {
 	.enter_low_power_mode = &raa489000_enter_low_power_mode,
 	.wake_low_power_mode = &tcpci_wake_low_power_mode,
 #endif
-	.set_bist_test_mode = &tcpci_set_bist_test_mode,
-	.get_bist_test_mode = &tcpci_get_bist_test_mode,
+	.set_bist_test_mode = &raa489000_set_bist_test_mode,
+	.get_bist_test_mode = &raa489000_get_bist_test_mode,
 	.tcpc_enable_auto_discharge_disconnect =
 		&tcpci_tcpc_enable_auto_discharge_disconnect,
 	.debug_detach = &raa489000_debug_detach,
