394307f4dc15780f86855cfca597e87ad137ea32
Dino Li <Dino.Li@ite.com.tw>
2024-09-19 11:45:13 +0800

    Raa489000: enable bist mode ASAP
    speed up the bist mode enable when received
    BIST message.
    
    BUG=b:358035069
    TEST=pass TEST.PD.PHY.ALL.7 Valid SOP*
    
    Change-Id: I1cf52ad68b85265b6bf6b8f0894da4bbca3f5d2b

--- zephyr/test/drivers/raa489000/src/raa489000.c
+++ zephyr/test/drivers/raa489000/src/raa489000.c
@@ -29,5 +29,76 @@ ZTEST(tcpc_raa489000, test_check_vendor)
 	tcpm_dump_registers(RAA489000_PORT);
 }
 
+#define MAX_PORTS 2
+static bool raa489000_bist_mode[MAX_PORTS] = { false };
+FAKE_VALUE_FUNC(int, pd_vbus_valid_for_bist, int);
+
+bool mock_pd_vbus_valid_for_bist_fail(int port)
+{
+	return false;
+}
+
+bool mock_pd_vbus_valid_for_bist_pass(int port)
+{
+	return true;
+}
+
+static bool raa489000_tcpm_should_enter_bist_mode(int port, uint32_t *payload,
+						  int *head)
+{
+	uint32_t hdr = *head;
+
+	if ((PD_HEADER_EXT(hdr) == 0) && (PD_HEADER_CNT(hdr) > 0) &&
+	    (PD_HEADER_TYPE(hdr) == PD_DATA_BIST) &&
+	    (BIST_MODE(payload[0]) == BIST_TEST_DATA) &&
+	    (!raa489000_bist_mode[port]) && pd_vbus_valid_for_bist(port)) {
+		raa489000_bist_mode[port] = true;
+		return tcpci_set_bist_test_mode(port, true);
+	}
+	return EC_SUCCESS;
+}
+
+ZTEST(tcpc_raa489000, test_raa489000_tcpm_should_enter_bist_mode)
+{
+	uint32_t payload[0];
+	int hdr;
+
+	hdr = 0x3003;
+	raa489000_bist_mode[0] = false;
+	payload[0] = 0x80000000;
+	pd_vbus_valid_for_bist_fake.custom_fake =
+		mock_pd_vbus_valid_for_bist_pass;
+
+	bool result = raa489000_tcpm_should_enter_bist_mode(0, payload, &hdr);
+
+	zassert_true(result == EC_SUCCESS, "BIST mode should be enabled");
+	zassert_true(raa489000_bist_mode[0] == true,
+		     "BIST mode should be enabled for port 0");
+}
+
+ZTEST(tcpc_raa489000, test_raa489000_tcpm_should_not_enter_bist_mode)
+{
+	uint32_t payload[1] = { 0 };
+	int head = 0;
+
+	raa489000_bist_mode[0] = false;
+
+	bool result = raa489000_tcpm_should_enter_bist_mode(0, payload, &head);
+
+	zassert_true(result == EC_SUCCESS, "BIST mode should not be enabled");
+	zassert_true(raa489000_bist_mode[0] == false,
+		     "BIST mode should not be enabled for port 0");
+}
+
+ZTEST(tcpc_raa489000, test_raa489000_tcpm_should_enter_bist_mode_invalid_port)
+{
+	uint32_t payload[1] = { BIST_TEST_DATA };
+	int head = 1;
+
+	bool result = raa489000_tcpm_should_enter_bist_mode(MAX_PORTS, payload,
+							    &head);
+
+	zassert_true(result == false, "Invalid port should return an error");
+}
 ZTEST_SUITE(tcpc_raa489000, drivers_predicate_post_main, NULL, NULL, NULL,
 	    NULL);
