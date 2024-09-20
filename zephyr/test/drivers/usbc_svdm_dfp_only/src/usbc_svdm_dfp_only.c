73393845d6a21204076e324aad348c1de8c99cc8
Ting Shen <phoenixshen@google.com>
2024-09-20 17:00:49 +0800

    TCPMv2: change the SVDM Version to 2.1
    PD 3.2 v1.0 spec Table 6.30:
    
    Ports implement USB PD version newer than 3.1 ver 1.6 should always set
    SVDM version to 2.1
    
    BUG=b:366240754
    TEST=COMMON.CHECK.PD.9
    
    Change-Id: Ib59c196976349c3205474e6e43fad9aa8ce1abb0

--- zephyr/test/drivers/usbc_svdm_dfp_only/src/usbc_svdm_dfp_only.c
+++ zephyr/test/drivers/usbc_svdm_dfp_only/src/usbc_svdm_dfp_only.c
@@ -113,7 +113,7 @@ ZTEST_F(usbc_svdm_dfp_only, test_verify_identity)
 	/* SVDM header: ACKing Discover_Identity */
 	zassert_equal(response.vdos[0],
 		      VDO(USB_SID_PD, 1,
-			  VDO_SVDM_VERS_MAJOR(1) | VDO_OPOS(0) |
+			  VDO_SVDM_VERS(SVDM_VER_2_1) | VDO_OPOS(0) |
 				  VDO_CMDT(CMDT_RSP_ACK) | CMD_DISCOVER_IDENT),
 		      "VDM Header value unexpected: %#x", response.vdos[0]);
 	/* ID Header VDO per PD 3.0 */
@@ -149,7 +149,7 @@ ZTEST_F(usbc_svdm_dfp_only, test_verify_pd20_nak)
 	zassert_equal(response.n_vdos, 1);
 	zassert_equal(response.vdos[0],
 		      VDO(USB_SID_PD, 1,
-			  VDO_SVDM_VERS_MAJOR(0) | VDO_OPOS(0) |
+			  VDO_SVDM_VERS(SVDM_VER_1_0) | VDO_OPOS(0) |
 				  VDO_CMDT(CMDT_RSP_NAK) | CMD_DISCOVER_IDENT),
 		      "VDM Header value unexpected: %#x", response.vdos[0]);
 }
