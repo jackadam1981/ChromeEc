6eac7436b112c45bd28bd4a3f4fd12fd83c0d37a
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2024-09-23 14:00:52 -0700

    [WA] ptlrvp: Fix data/power role swap
    This CL will make rts54 driver to comply with current power/data role
    swap commands format in Realtek Firmware for PTL RVP.
    
    Change-Id: Id28cfba04aff0ae7b143fee463e17f6ca4ef2d50

--- zephyr/include/drivers/ucsi_v3.h
+++ zephyr/include/drivers/ucsi_v3.h
@@ -1175,7 +1175,7 @@ union uor_t {
 		 * This field indicates the connector whose USB
 		 * operational role is to be modified.
 		 */
-		uint16_t connector_number : 7;
+		uint16_t connector_number : 8;
 		/**
 		 * If this bit is set, then the connector
 		 * shall initiate swap to DFP if not
@@ -1212,7 +1212,7 @@ union pdr_t {
 		 * This field indicates the connector whose Power
 		 * Direction Role is to be modified.
 		 */
-		uint16_t connector_number : 7;
+		uint16_t connector_number : 8;
 		/**
 		 * If this bit is set then the connector
 		 * shall initiate swap to Source, if not
