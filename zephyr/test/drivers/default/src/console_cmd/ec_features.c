1ee4c3a047059e8e0f8892c0d08b8ec3b594fd97
Daisuke Nojiri <dnojiri@chromium.org>
2024-09-25 09:06:52 -0700

    test/drivers/default: Test EC_FEATURE_STRAUSS
    BUG=b:373368656
    TEST=./twister -T zephyr/test/drivers -s drivers.default.console_cmds
    
    Change-Id: Ic252bc5a8597df244855d72ad4e22376fe337829

--- zephyr/test/drivers/default/src/console_cmd/ec_features.c
+++ zephyr/test/drivers/default/src/console_cmd/ec_features.c
@@ -18,12 +18,14 @@ ZTEST_SUITE(console_cmd_ec_features, drivers_predicate_post_main, NULL, NULL,
 ZTEST_USER(console_cmd_ec_features, test_feat)
 {
 	char expected[50];
+	uint32_t flags0 = get_feature_flags0();
+	uint32_t flags1 = get_feature_flags1();
 	int ret;
 
 	ret = snprintf(expected, sizeof(expected),
-		       " 0-31: 0x%08x\r\n32-63: 0x%08x", get_feature_flags0(),
-		       get_feature_flags1());
+		       " 0-31: 0x%08x\r\n32-63: 0x%08x", flags0, flags1);
 	zassert_true(ret >= 0 && ret < sizeof(expected));
+	zassert_true(flags1 & EC_FEATURE_MASK_1(EC_FEATURE_STRAUSS));
 
 	CHECK_CONSOLE_CMD("feat", expected, EC_SUCCESS);
 }
