95a9dfc8b3bcbc30d2dded5b3e9ba44d47f7b55e
Firas Sammoura <fsammoura@google.com>
2024-09-26 10:08:17 -0700

    test: Migrate fpupload command unit test to fpsensor_debug
    The unit tests for the functions defined in fpsensor_debug.cc are added
    in restricted_console unit test file. Migrate the unit test of the
    fpupload command to fpsensor_debug because it should be the dedicated
    file for all tests related to the functions defined in that source file.
    
    BUG=b:369847148
    TEST=./test/run_device_tests.py -b bloonchipper -t fpsensor_debug
    
    Change-Id: I52906ecde80c1c1dacac0010c230a8ada2819cf4

--- test/fpsensor_debug.cc
+++ test/fpsensor_debug.cc
@@ -3,7 +3,15 @@
  * found in the LICENSE file.
  */
 
+#include "system.h"
 #include "test_util.h"
+
+static int is_locked;
+
+int system_is_locked(void)
+{
+	return is_locked;
+}
 
 // Smoke test the "fpinfo" console command and its underlying version retrieval.
 test_static int test_console_fpinfo()
@@ -13,9 +21,32 @@ test_static int test_console_fpinfo()
 	return EC_SUCCESS;
 }
 
+test_static int test_command_fpupload(void)
+{
+	enum ec_error_list res;
+
+	/* System is unlocked. */
+	is_locked = 0;
+
+	char console_input1[] = "fpupload 52 image";
+	res = test_send_console_command(console_input1);
+	TEST_EQ(res, EC_SUCCESS, "%d");
+
+	/* System is locked. */
+	is_locked = 1;
+
+	/* Test for the case when access is denied. */
+	char console_input2[] = "fpupload 52 image";
+	res = test_send_console_command(console_input2);
+	TEST_EQ(res, EC_ERROR_ACCESS_DENIED, "%d");
+
+	return EC_SUCCESS;
+}
+
 void run_test(int argc, const char **argv)
 {
 	RUN_TEST(test_console_fpinfo);
+	RUN_TEST(test_command_fpupload);
 
 	test_print_result();
 }
