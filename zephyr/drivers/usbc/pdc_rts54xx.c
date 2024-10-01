5b2c8da6d816bc8a3346b5daf62e1863e33ad3c6
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2024-10-01 13:02:08 -0700

    [WA] rts54: Force read on `IC_STATUS` command
    Change-Id: Ie1522253d9b28e754c1796660244fafcf3c9a14d

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -1153,6 +1153,10 @@ static void st_ping_status_run(void *o)
 			 */
 			data->cci_event.command_completed = 1;
 
+			/* Workaround: Force reading data. */
+			if (data->cmd == CMD_GET_IC_STATUS) {
+				data->ping_status.data_len = 38;
+			}
 			if (data->ping_status.data_len > 0) {
 				/* Data is available, so read it */
 				set_state(data, ST_READ);
