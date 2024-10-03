69632d732a5df0264b1c6b35ab20bc635bd59230
Firas Sammoura <fsammoura@google.com>
2024-10-03 22:09:18 +0000

    driver/fingerprint: Add brief description for functions in egis_api.h
    Add a brief description of the functions declared in the egis_api header
    files including input parameters and return parameters. Remove any
    function description from the source files.
    
    BUG=b:369421451
    TEST=CQ
    
    Change-Id: I0eda5fc3fa5bda7d39170a23bd6770b28578a1ac

--- driver/fingerprint/egis/egis_api.h
+++ driver/fingerprint/egis/egis_api.h
@@ -62,12 +62,82 @@ typedef enum {
 	EGIS_API_ERROR_SENSOR_OCP_DETECT = 110,
 } egis_api_return_t;
 
+/**
+ * @brief Reset and initialize the sensor IC.
+ *
+ * @return EGIS_OK : on success.
+ * @return EGIS_INCORRECT_PARAMETER : when there are incorrect parameters.
+ * @return EGIS_OUT_OF_MEMORY : when memody memory fails.
+ * @return EGIS_NO_DEVICE : when the sensor cannot be detected.
+ * @return EGIS_ESD_NEED_RESET : when the sensor needs to be reset.
+ * @return EGIS_SPI_FAIL : when the spi transfer fails.
+ * @return EGIS_CALIBRATION_SENSING_FAIL : when the sensor sensinf mode
+ * calibration fails.
+ * @return EGIS_CALIBRATION_DETECT_FAIL : when the sensor detect mode
+ * calibration fails.
+ * @return EGIS_GET_OTP_FAIL : when the get sensor's OTP fails.
+ * @return EGIS_OCP_DETECT : when the sensor detects over current event and cuts
+ * off power to the sensing area.
+ */
 int egis_sensor_init(void);
+
+/**
+ * Deinitialize the sensor IC.
+ *
+ * @return EGIS_OK : on success.
+ */
 int egis_sensor_deinit(void);
+
+/**
+ * Power down the sensor IC.
+ *
+ */
 void egis_sensor_power_down(void);
+
+/**
+ * Acquire a fingerprint image with specific capture mode.
+ *
+ * @param[out] image_data Image from sensor. Buffer must be allocated by caller
+ * with size FP_SENSOR_IMAGE_SIZE.
+ * @param mode  enum fp_capture_type.
+ *
+ * @return EGIS_OK : on success.
+ * @return EGIS_OUT_OF_MEMORY : when allocation memory fails.
+ * @return ET630_IPP_ERR_ILLEGAL_PARAMS : when there are incorrect parameters.
+ * @return ET630_IPP_ERR_OTUOFMEMORY : when allocation memory fails.
+ */
 int egis_get_image_with_mode(uint8_t *image_data, int mode);
+
+/**
+ * Get 16bits image data from EGIS fingerprint sensor.
+ *
+ * @param[out] image_data Image from sensor. Buffer must be allocated by caller
+ * with size FP_SENSOR_IMAGE_SIZE.
+ *
+ * @return EGIS_OK : on success.
+ * @return EGIS_OUT_OF_MEMORY : when allocation memory fails.
+ * @return ET630_IPP_ERR_ILLEGAL_PARAMS : when there are incorrect parameters.
+ * @return ET630_IPP_ERR_OTUOFMEMORY : when allocation memory fails.
+ */
 int egis_get_image(uint8_t *image_data);
+
+/**
+ * Set the finger detection mode for the Egis sensor.
+ *
+ */
 void egis_set_detect_mode(void);
+
+/**
+ * Check the sensor interrupt status.
+ *
+ * @return EGIS_API_FINGER_PRESENT when the finger is present. The capacitive
+ * sensor has detected an increase in the capacitance value on the suraface,
+ * which exceeds the threshold set for finger presence.
+ * @return EGIS_API_FINGER_LOST when the finger has been removed. The capacitive
+ * sensor has detected an decrease in the capacitance value on the suraface,
+ * which exceeds the threshold set for finger removal.
+ * @return EGIS_API_FINGER_UNSTABLE when the sensor detects ESD or is reset.
+ */
 int egis_check_int_status(void);
 
 #ifdef __cplusplus
