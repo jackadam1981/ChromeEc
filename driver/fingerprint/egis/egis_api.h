6a7271db62adb42ede0b61fef8d9afb93453e886
Firas Sammoura <fsammoura@google.com>
2024-10-03 22:09:18 +0000

    driver/fingerprint: Add brief description for functions in egis_api.h
    Add a brief description of the functions declared in the egis_api header
    files including input parameters and return parameters. Remove any
    function description from the source files.
    
    BUG=b:369421451
    TEST=CQ
    
    Change-Id: I0eda5fc3fa5bda7d39170a23bd6770b28578a1ac

--- driver/fingerprint/egis/egis_api.h
+++ driver/fingerprint/egis/egis_api.h
@@ -62,12 +62,65 @@ typedef enum {
 	EGIS_API_ERROR_SENSOR_OCP_DETECT = 110,
 } egis_api_return_t;
 
+/**
+ * @brief Reset and initialize the sensor IC.
+ *
+ * @return 0 on success.
+ * @return positive value on error.
+ */
 int egis_sensor_init(void);
+
+/**
+ * Deinitialize the sensor IC.
+ *
+ * @return 0 on success.
+ * @return positive value on error.
+ */
 int egis_sensor_deinit(void);
+
+/**
+ * Power down the sensor IC.
+ *
+ */
 void egis_sensor_power_down(void);
+
+/**
+ * Acquire a fingerprint image with specific capture mode.
+ *
+ * @param[out] image_data Image from sensor. Buffer must be allocated by caller
+ * with size FP_SENSOR_IMAGE_SIZE.
+ * @param mode  enum fp_capture_type.
+ *
+ * @return 0 on success.
+ * @return positive value on error.
+ */
 int egis_get_image_with_mode(uint8_t *image_data, int mode);
+
+/**
+ * Get 8bits image data from EGIS fingerprint sensor.
+ *
+ * @param[out] image_data Image from sensor. Buffer must be allocated by caller
+ * with size FP_SENSOR_IMAGE_SIZE.
+ *
+ * @return 0 on success.
+ * @return positive value on error.
+ */
 int egis_get_image(uint8_t *image_data);
+
+/**
+ * Set the finger detection mode for the Egis sensor.
+ *
+ */
 void egis_set_detect_mode(void);
+
+/**
+ * Check the sensor interrupt status.
+ *
+ * @return 0 on success.
+ * @return EGIS_API_FINGER_PRESENT when the finger is present.
+ * @return EGIS_API_FINGER_LOST when the finger is partial.
+ * @return other positive value on error.
+ */
 int egis_check_int_status(void);
 
 #ifdef __cplusplus
