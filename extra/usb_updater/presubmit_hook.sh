0b61ec8b584afef1bccec6a85a413ef74743f22e
Jett Rink <jettrink@google.com>
2024-10-04 15:51:23 -0600

    presubmit: Ensure gsctool changes have devboard tests
    Require a TEST=make tast TAST_EXPR=gscdevboard.GSCFactoryUpdate line in
    the commit description in hopes that all gsctool potential gsctool
    changes are run against devboard tests.
    
    BUG=none
    TEST=With local CL, ensure that presubmit fails for changes to gsctool.c
    TEST=With local CL, ensure that presubmit doesn't run on other changes
    
    Change-Id: I4f43d2e1021a203d74de4c75df3e6c78a8931b48

---
+++ extra/usb_updater/presubmit_hook.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+commit=$1
+shift
+files=$@
+
+if [[ $files == *"usb_updater"* ]]; then
+  git log -n 1 $commit | \
+    grep "TEST=make tast TAST_EXPR=gscdevboard.GSCFactoryUpdate"
+  if [[ $? -ne 0 ]]; then
+    echo 'Run and add "TEST=make tast' \
+         'TAST_EXPR=gscdevboard.GSCFactoryUpdate.*"' \
+         'to commit msg for gsctool changes/'
+    exit 1
+  fi
+fi
