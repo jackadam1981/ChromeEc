9e08dcd4a2c9d578fdd93f73e8d9ae9ceda307cc
Yuval Peress <peress@google.com>
2024-10-07 12:10:11 -0600

    WIP
    BUG=b:345530593
    TEST=None
    
    Change-Id: Id38895202cbdf80553fb73faaba30cb02a5e7a6b

---
+++ zephyr/drivers/dsp_comms/include/cros/dsp/internal/cros_transport.hh
@@ -0,0 +1,60 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#pragma once
+
+#include <pb_encode.h>
+
+#include <cstdint>
+#include <limits>
+
+#include "proto/ec_dsp.pb.h"
+#include "pw_transport/service.h"
+
+namespace cros::dsp::service {
+
+using CrosTransportParent = pw::transport::Transport<
+    cros_dsp_comms_StatusFlag_STATUS_FLAG_RESPONSE_READY,
+    CONFIG_PLATFORM_EC_DSP_SERVICE_RESPONSE_BUFFER_SIZE>;
+
+class CrosTransport : public CrosTransportParent {
+ public:
+  constexpr CrosTransport() : CrosTransportParent() {}
+  ~CrosTransport() = default;
+
+  pw::Status StageResponse(const cros_dsp_comms_GetCbiFlagsResponse& response) {
+    return this->template StageResponse<cros_dsp_comms_GetCbiFlagsResponse>(
+        response,
+        [](const auto& response, pw::ByteSpan out) -> pw::StatusWithSize {
+          pb_ostream_t response_ostream = pb_ostream_from_buffer(
+              reinterpret_cast<pb_type_t*>(out.data()), out.size());
+          bool response_encode_success =
+              pb_encode(&response_ostream,
+                        cros_dsp_comms_GetCbiFlagsResponse_fields,
+                        &response);
+          if (!response_encode_success) {
+            return pw::StatusWithSize(pw::Status::ResourceExhausted(), 0);
+          }
+
+          return pw::StatusWithSize(response_ostream.bytes_written);
+        });
+  }
+
+  using CrosTransportParent::SetNotifyClientCallback;
+
+ protected:
+  using CrosTransportParent::StageResponse;
+
+  pw::Status UpdateResponseSize(pw_transport_Status& status, size_t new_size) {
+    if (new_size > std::numeric_limits<
+                       decltype(pw_transport_Status::response_length)>::max()) {
+      return pw::Status::OutOfRange();
+    }
+    status.response_length = new_size;
+    return pw::OkStatus();
+  }
+};
+
+}  // namespace cros::dsp::service
