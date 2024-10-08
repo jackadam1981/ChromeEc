38259a46b16dec9bfeb239c207e73fd4dd379e66
Jan Malek <jmalek@antmicro.com>
2024-10-08 13:57:17 +0200

    test/rtc_stm32f4: Prevent sleep during the test
    The RTC alarm tested is also used by procedures related to entering and exiting sleep.
    This change prevents the alarm to be used for two, unrelated purposes.
    
    BUG=b:356476313, b:384991107
    TEST=CQ
    
    Change-Id: Ie17b2f30ef778080d447bc6103dafa7d667a42ed

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -459,7 +459,6 @@ class Renode(Platform):
             if test_name in [
                 "exception",
                 "libcxx",
-                "rtc_stm32f4",  # TODO(b/384991107)
                 "timer_dos",  # TODO(b/374798079)
             ]:
                 return True
