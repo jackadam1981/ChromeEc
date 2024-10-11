fc9360972dfd5320e29547c8b3702f2933858667
Tom Hughes <tomhughes@chromium.org>
2024-10-11 16:27:12 -0700

    DO NOT SUBMIT: Test out Zephyr hardware tests on Renode
    BUG=b:372943952
    TEST=./test/run_device_tests.py -b bloonchipper --renode --zephyr -t aes
    
    Change-Id: I7df9ba1eeca25dcad81acb4429f3f0014c81caed

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -264,7 +264,9 @@ class Platform(ABC):
         """Clean up after a test run."""
 
     @abstractmethod
-    def skip_test(self, test_name: str, board_config: BoardConfig) -> bool:
+    def skip_test(
+        self, test_name: str, board_config: BoardConfig, zephyr: bool
+    ) -> bool:
         """Returns true if the given test should be skipped."""
 
 
@@ -351,7 +353,9 @@ class Hardware(Platform):
     def cleanup(self) -> None:
         pass
 
-    def skip_test(self, test_name: str, board_config: BoardConfig) -> bool:
+    def skip_test(
+        self, test_name: str, board_config: BoardConfig, zephyr: bool
+    ) -> bool:
         return False
 
 
@@ -395,11 +399,29 @@ class Renode(Platform):
     def cleanup(self) -> None:
         self.process.kill()
 
-    def skip_test(self, test_name: str, board_config: BoardConfig) -> bool:
+    def skip_test(
+        self, test_name: str, board_config: BoardConfig, zephyr: bool
+    ) -> bool:
         if board_config.name in [BLOONCHIPPER, DARTMONKEY]:
             if board_config.name == BLOONCHIPPER:
                 if test_name in [
                     "timer",  # TODO(b/372968708)
+                ]:
+                    return True
+                if zephyr and test_name in [
+                    "abort",
+                    "assert_builtin",
+                    "assert_stdlib",
+                    "exit",
+                    "fp_transport_spi_ro",
+                    "fp_transport_spi_rw",
+                    "fpsensor_debug",
+                    "ftrapv",
+                    "panic",
+                    "panic_data",
+                    "rollback",
+                    "stdlib",
+                    "utils_str",
                 ]:
                     return True
             # TODO(b/356476313): Remove these when Renode is fixed.
@@ -502,8 +524,8 @@ class AllTests:
 
         zephyr_upstream_tests = (
             []
-            if with_private == PRIVATE_ONLY or not zephyr
-            else AllTests.get_zephyr_tests()
+            #            if with_private == PRIVATE_ONLY or not zephyr
+            #            else AllTests.get_zephyr_tests()
         )
 
         all_tests = public_tests + private_tests + zephyr_upstream_tests
@@ -1278,7 +1300,7 @@ def run_test_zephyr(test: TestConfig) -> str:
         return []
     if len(test.test_args) == 0:
         # If there are no args just run-all not to be limited by suite name
-        test_cmd = "ztest run-all\n"
+        test_cmd = "ztest\nztest run-all\n"
     else:
         # ZTEST console doesn't support passing test arguments
         # Assume a testsuite for every test + arg combination
@@ -1324,7 +1346,8 @@ def run_test(
             test_cmd = run_test_ec(test)
 
         if len(test_cmd) > 0:
-            console.write(test_cmd.encode())
+            written = console.write(test_cmd.encode())
+            logging.debug("wrote: %s", written)
 
     while True:
         console.flush()
@@ -1675,7 +1698,7 @@ def main():
     with ThreadPoolExecutor(max_workers=1) as executor:
         for test in test_list:
             if (test.skip_for_zephyr and args.zephyr) or platform.skip_test(
-                test.test_name, board_config
+                test.test_name, board_config, args.zephyr
             ):
                 continue
             test.passed = flash_and_run_test(
@@ -1688,7 +1711,7 @@ def main():
             # print results
             print('Test "' + test.config_name + '": ', end="")
             if (test.skip_for_zephyr and args.zephyr) or platform.skip_test(
-                test.test_name, board_config
+                test.test_name, board_config, args.zephyr
             ):
                 print(colorama.Fore.YELLOW + "SKIPPED")
             else:
