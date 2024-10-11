bf22880a3364103e873a396989a4f119142267fd
Tom Hughes <tomhughes@chromium.org>
2024-10-11 14:29:02 -0700

    test/run_device_tests: Re-enable ftrapv for helipilot/bucaneer on Renode
    This test passes, but it needs more time than the default timeout.
    
    BUG=b:356476313
    TEST=CQ
    
    Change-Id: I5bd357b94154c34e2f96a004864952cdbfe06590

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -433,7 +433,6 @@ class Renode(Platform):
                 "flash_physical",
                 "flash_write_protect",
                 "fpsensor_auth_crypto_stateless",
-                "ftrapv",
                 "malloc",
                 "rollback_region0",
                 "rollback_region1",
@@ -615,7 +614,7 @@ class AllTests:
                 ),
             ),
             TestConfig(test_name="fpsensor_utils"),
-            TestConfig(test_name="ftrapv"),
+            TestConfig(test_name="ftrapv", timeout_secs=60),
             TestConfig(
                 test_name="libc_printf",
                 finish_regexes=[PRINTF_CALLED_REGEX],
