57037af7290239b207372040d6834c88e08cd1d9
Tom Hughes <tomhughes@chromium.org>
2024-10-14 13:18:54 -0700

    common/system: Ensure jdata is initialized
    The system_usable_ram_end function can be called while initializing C++
    objects with static storage duration, which occurs before any calls to
    set jdata.
    
    Fixing this bug allows the "std_vector" test to pass in Renode.
    
    BUG=b:291940520
    TEST=./test/run_device_tests.py -b bloonchipper --renode -t std_vector
    TEST=./test/run_device_tests.py -b dartmonkey --renode -t std_vector
    TEST=./test/run_device_tests.py -b helipilot --renode -t std_vector
    TEST=./test/run_device_tests.py -b buccaneer --renode -t std_vector
    TEST=CQ
    
    Change-Id: I4489f904bf8683b2cb92609ee5a3873d4426fd18

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -432,7 +432,6 @@ class Renode(Platform):
                 "libcxx",
                 "power_utilization",
                 "rtc_stm32f4",
-                "std_vector",
                 "timer_dos",  # TODO(b/374798079)
             ]:
                 return True
@@ -456,7 +455,6 @@ class Renode(Platform):
                 "rollback_entropy",
                 "rtc_npcx9",
                 "sbrk",
-                "std_vector",
                 "fpsensor_auth_crypto_stateless",  # TODO(b/372969110)
                 "unaligned_access_benchmark",  # TODO(372969629)
             ]:
