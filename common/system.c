8e860ce543f5779c940054968595342d29dae949
Tom Hughes <tomhughes@chromium.org>
2024-10-14 13:18:54 -0700

    common/system: Ensure jdata is initialized
    The system_usable_ram_end function can be called while initializing C++
    objects with static storage duration, which occurs before any calls to
    set jdata.
    
    Fixing this bug allows the "std_vector" test to pass in Renode.
    
    BUG=b:291940520
    TEST=./test/run_device_tests.py -b bloonchipper --renode -t std_vector
    TEST=./test/run_device_tests.py -b dartmonkey --renode -t std_vector
    TEST=./test/run_device_tests.py -b helipilot --renode -t std_vector
    TEST=./test/run_device_tests.py -b buccaneer --renode -t std_vector
    TEST=CQ
    
    Change-Id: I4489f904bf8683b2cb92609ee5a3873d4426fd18

--- common/system.c
+++ common/system.c
@@ -231,6 +231,15 @@ test_mockable int system_is_locked(void)
 
 test_mockable uintptr_t system_usable_ram_end(void)
 {
+	/* This function can be called while initializing C++ objects with
+	 * static storage duration, which occurs before any calls to
+	 * set jdata. e.g., from __libc_init_array in init.S.
+	 */
+	if (IS_ENABLED(CONFIG_RAM_SIZE) &&
+	    IS_ENABLED(CONFIG_COMMON_PANIC_OUTPUT) && jdata == NULL) {
+		jdata = get_jump_data();
+	}
+
 	/* Leave space at the end of RAM for jump data and tags.
 	 *
 	 * Note that jump_tag_total is 0 on a reboot, so we have the maximum
