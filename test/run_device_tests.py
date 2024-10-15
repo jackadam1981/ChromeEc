c4fe1189f994acc8ac98794dc964c5c156b8105d
Tom Hughes <tomhughes@chromium.org>
2024-10-15 15:59:49 -0700

    test/run_device_tests: Disable helipilot/buccaneer tests that fail on Renode
    BUG=b:374316681
    TEST=CQ
    
    Change-Id: I20f77f0434947c956608cfadc663e6cc51a05872

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -418,9 +418,18 @@ class Renode(Platform):
             if test_name in [
                 "production_app_test",
                 "benchmark",
+                "exception",
+                "exit",
+                "flash_physical",
                 "fpsensor_hw",
                 "libcxx",
+                "malloc",
+                "otp_key",
                 "power_utilization",
+                "ram_lock",
+                "rollback",
+                "rtc_npcx9",
+                "sbrk",
                 "std_vector",
                 "fpsensor_auth_crypto_stateless",  # TODO(b/372969110)
                 "unaligned_access_benchmark",  # TODO(372969629)
