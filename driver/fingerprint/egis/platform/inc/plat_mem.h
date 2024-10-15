b244095c3de6b2e58ef7dedb01b63a23acc6ad3e
Firas Sammoura <fsammoura@google.com>
2024-10-15 16:55:00 +0000

    driver/fingerprint: Add brief description for functions in plat_mem.h
    Add a brief description of the functions declared in the plat_mem header
    files including input parameters and return parameters.
    
    BUG=b:369421451
    TEST=CQ
    
    Change-Id: Ida5f144d9f4987b525bd33968b20a7bf8d7d7483

--- driver/fingerprint/egis/platform/inc/plat_mem.h
+++ driver/fingerprint/egis/platform/inc/plat_mem.h
@@ -15,7 +15,23 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Allocates a block of memory of a specified size and number of
+ * elements.
+ *
+ * @param[in] count The number of elements to allocate.
+ * @param[in] size The size of each element in bytes.
+ *
+ * @return Pointer to the allocated memory.
+ */
 void *sys_alloc(size_t count, size_t size);
+
+/**
+ * @brief Releases a block of shared memory.
+ *
+ * @param[in] data A pointer to the memory block to be released.
+ *
+ */
 void sys_free(void *data);
 
 static inline void plat_free(void *x)
@@ -24,6 +40,14 @@ static inline void plat_free(void *x)
 }
 
 // TODO (b/373435445): Combine PLAT_FREE and plat_free.
+/**
+ * @brief Deallocates memory and sets the provided pointer to NULL.
+ *
+ * @param[in] x A pointer to a pointer to the memory block to be freed. This is
+ * a double pointer, meaning it points to a memory location that holds another
+ * pointer, which in turn points to the actual data.
+ *
+ */
 static inline void PLAT_FREE(void **x)
 {
 	assert(x != NULL && *x != NULL);
@@ -31,6 +55,13 @@ static inline void PLAT_FREE(void **x)
 	*x = NULL;
 }
 
+/**
+ * @brief Allocates a block of memory of the specified size.
+ *
+ * @param[in] size The size of the memory block to allocate, in bytes.
+ *
+ * @return Pointer to the allocated memory.
+ */
 static inline void *plat_alloc(size_t size)
 {
 	return sys_alloc(1, size);
