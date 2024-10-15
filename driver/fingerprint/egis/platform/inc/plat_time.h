1c07fe3ea8577542e21f0a3e86fbdd8f047b9b4b
Firas Sammoura <fsammoura@google.com>
2024-10-15 18:18:44 +0000

    driver/fingerprint: Add brief description for functions in plat_time.h
    Add a brief description of the functions declared in the plat_time header
    files including input parameters and return parameters.
    
    BUG=b:369421451
    TEST=CQ
    
    Change-Id: I205494e5d446658115861960bb78aefa2397dfc6

--- driver/fingerprint/egis/platform/inc/plat_time.h
+++ driver/fingerprint/egis/platform/inc/plat_time.h
@@ -10,10 +10,42 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Gets the current time in milliseconds.
+ *
+ * @return The current time in milliseconds.
+ *
+ */
 unsigned long long plat_get_time(void);
+
+/**
+ * @brief Calculates the time difference in milliseconds between the current
+ * time and a given starting time.
+ *
+ * @param being The starting time in milliseconds.
+ *
+ * @return The time difference in milliseconds.
+ *
+ */
 unsigned long plat_get_diff_time(unsigned long long begin);
+
+/**
+ * @brief Introduces a delay for a specified number of milliseconds. This likely
+ * uses a busy-waiting approach.
+ *
+ * @param msecs The delay time in milliseconds.
+ *
+ */
 void plat_wait_time(unsigned long msecs);
 
+/**
+ * @brief Introduces a delay for a specified number of milliseconds. This likely
+ * uses a sleep or blocking mechanism, allowing other processes to run during
+ * the delay.
+ *
+ * @param timeInMs The sleep time in milliseconds.
+ *
+ */
 void plat_sleep_time(unsigned long timeInMs);
 
 #ifdef EGIS_SPEED_DBG
