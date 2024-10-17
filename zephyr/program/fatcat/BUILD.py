56b0682b7b320ac62e4a1d1f6b5e82357c6d5696
Ian Feng <ian_feng@compal.corp-partner.google.com>
2024-10-17 14:29:04 +0800

    francka: Initial Zephyr EC image
    Create the initial Zephyr EC image for the francka variant based
    on the fatcat reference board.
    
    BUG=b:370666276
    TEST=zmake build francka succes
    
    Change-Id: Ie14ec15bcc1eacf6e3cfaaa71c6dbfcb6964f042

--- zephyr/program/fatcat/BUILD.py
+++ zephyr/program/fatcat/BUILD.py
@@ -60,6 +60,10 @@ register_it8xxx2_project(
     ],
 )
 
+register_npcx9_project(
+    project_name="francka",
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="fatcat_npcx9m7f", addr=0x80144)
