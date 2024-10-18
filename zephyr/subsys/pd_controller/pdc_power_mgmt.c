640561781ba6a55bcf460cd3710506d9ac776a17
Al Semjonovs <asemjonovs@google.com>
2024-10-18 08:05:24 -0600

    pdc_power_mgmt: Add SET_DRP_MODE to connectionless commands
    Add SET_DRP_MODE to connectionless commands
    
    BUG=None
    TEST=./twister -c -T zephyr/test/pdc
    
    Change-Id: I797539e65c9a54c936d1c04f27b9f6d8e265d534

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -3006,6 +3006,8 @@ static bool is_connectionless_cmd(enum pdc_cmd_t pdc_cmd)
 		__fallthrough;
 	case CMD_PDC_GET_PCH_DATA_STATUS:
 		__fallthrough;
+	case CMD_PDC_SET_DRP:
+		__fallthrough;
 	case CMD_PDC_GET_LPM_PPM_INFO:
 		return true;
 	default:
