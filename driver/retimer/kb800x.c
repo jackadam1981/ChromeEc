8ac5c53219300ab5b5a636b8bce262285f21f8ef
Jeremy Bettis <jbettis@chromium.org>
2024-10-21 20:15:04 +0000

    Add a default case to the switch statement in kb800x.c.
    This is to fix a compiler warning exposed in crrev/c/5902895.
    
    BUG=None
    TEST=None
    
    Change-Id: If59b4c02d17320ac4c2db7a1cfe1e13795427480

--- driver/retimer/kb800x.c
+++ driver/retimer/kb800x.c
@@ -197,6 +197,12 @@ static bool kb800x_phy_ss_lane_is_rx(enum kb800x_phy_lane phy_lane,
 	case KB800X_RX1:
 		rx = true;
 		break;
+	/* LCOV_EXCL_START */
+	default:
+		/* Unreachable. */
+		ASSERT(false);
+		break;
+		/* LCOV_EXCL_STOP */
 	}
 	/* invert for C/D (host side), since it is receiving the TX signal*/
 	if (!KB800X_PHY_IS_AB(phy_lane))
