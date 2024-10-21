2365bc47dcb9cfa3eb9c0f9a808b3c41d48afa31
Firas Sammoura <fsammoura@google.com>
2024-10-21 15:41:13 -0700

    test: add RO variants
    Change-Id: I547f6cce311b1a4c29f25757d6593360c5933460

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -786,6 +786,34 @@ class AllTests:
                 )
             )
 
+        for variant_name, variant_info in board_config.variants.items():
+            tests.append(
+                TestConfig(
+                    config_name="sram_mpu_protection_wp_on_" + variant_name,
+                    test_name="sram_mpu_protection",
+                    ro_image=variant_info.get("ro_image_path"),
+                    build_board=variant_info.get("build_board"),
+                    exclude_boards=[BLOONCHIPPER, DARTMONKEY],
+                    test_args=["wp_on"],
+                    toggle_power=True,
+                    enable_hw_write_protect=True,
+                )
+            )
+
+        for variant_name, variant_info in board_config.variants.items():
+            tests.append(
+                TestConfig(
+                    config_name="sram_mpu_protection_wp_off_" + variant_name,
+                    test_name="sram_mpu_protection",
+                    ro_image=variant_info.get("ro_image_path"),
+                    build_board=variant_info.get("build_board"),
+                    exclude_boards=[BLOONCHIPPER, DARTMONKEY],
+                    test_args=["wp_off"],
+                    toggle_power=True,
+                    enable_hw_write_protect=False,
+                )
+            )
+
         return tests
 
     @staticmethod
