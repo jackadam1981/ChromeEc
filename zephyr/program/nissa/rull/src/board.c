73b468f3ef9edbb69eff213fee9693ae0d9d3429
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2024-10-22 19:12:27 +0800

    Test-only: add keyboard
    BUG=b:372211281
    TEST=1.zmake build rull
    
    Change-Id: I7ddb7c2494cfc4958532179ccad2a50481d53e02

--- zephyr/program/nissa/rull/src/board.c
+++ zephyr/program/nissa/rull/src/board.c
@@ -11,6 +11,7 @@
 #include "fan.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
+#include "keyboard_backlight.h"
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
@@ -45,3 +46,39 @@ test_export_static void fan_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_INIT, fan_init, HOOK_PRIO_POST_FIRST);
+
+static bool key_bl = FW_KB_BL_NOT_PRESENT;
+
+int8_t board_vivaldi_keybd_idx(void)
+{
+	if (key_bl == FW_KB_BL_NOT_PRESENT) {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_1));
+	} else {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_0));
+	}
+}
+
+/*
+ * Keyboard function decided by FW config.
+ */
+test_export_static void kb_init(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FW_KB_BL, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FW_KB_BL);
+		return;
+	}
+
+	LOG_INF("This is %s kb backlight.",
+		FW_KB_BL_PRESENT ? "with" : "without");
+	if (val == FW_KB_BL_PRESENT) {
+		key_bl = FW_KB_BL_PRESENT;
+	} else {
+		key_bl = FW_KB_BL_NOT_PRESENT;
+		kblight_enable(0);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_FIRST);
