79c3f13dee090089da653a7ae3ead47c60a46a90
Tom Hughes <tomhughes@chromium.org>
2024-10-22 14:20:22 -0700

    test/run_device_tests: Include timestamp when logging
    BUG=none
    TEST=none
    
    Change-Id: Ib08560dc2ae61a49983aad75a1c0d9c9e4bde503

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -1683,7 +1683,7 @@ def main():
 
     args = parser.parse_args()
     logging.basicConfig(
-        format="%(levelname)s:%(message)s", level=args.log_level
+        format="%(asctime)s - %(levelname)s:%(message)s", level=args.log_level
     )
     validate_args_combination(args)
 
