53e3ebea0d65fb1206bffc18c879854070f55a45
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2024-10-22 18:49:25 +0800

    zephyr: it51xxx: Add it51xxx baseboard
    1. Add it51xxx baseboard
    - it51526aw
    - it51526bw
    - it51526bw-512
    2. Modified shim and driver code for it51xxx
    
    3. prochot_vcmp: Use new comparator API
    
    Change-Id: Iad36b9a40658f3770466fcf5f0fc4e6b0bdb61eb
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

--- zephyr/drivers/cros_system/cros_system_it8xxx2.c
+++ zephyr/drivers/cros_system/cros_system_it8xxx2.c
@@ -35,7 +35,7 @@ static const char *cros_system_it8xxx2_get_chip_vendor(const struct device *dev)
 
 static uint32_t system_get_chip_id(void)
 {
-	struct gctrl_it8xxx2_regs *const gctrl_base = GCTRL_IT8XXX2_REG_BASE;
+	struct gctrl_ite_ec_regs *const gctrl_base = GCTRL_ITE_EC_REGS_BASE;
 
 	return (gctrl_base->GCTRL_ECHIPID1 << 16) |
 	       (gctrl_base->GCTRL_ECHIPID2 << 8) | gctrl_base->GCTRL_ECHIPID3;
@@ -43,7 +43,7 @@ static uint32_t system_get_chip_id(void)
 
 static uint8_t system_get_chip_version(void)
 {
-	struct gctrl_it8xxx2_regs *const gctrl_base = GCTRL_IT8XXX2_REG_BASE;
+	struct gctrl_ite_ec_regs *const gctrl_base = GCTRL_ITE_EC_REGS_BASE;
 
 	/* bit[3-0], chip version */
 	return gctrl_base->GCTRL_ECHIPVER & 0x0F;
@@ -80,23 +80,23 @@ cros_system_it8xxx2_get_chip_revision(const struct device *dev)
 static int cros_system_it8xxx2_get_reset_cause(const struct device *dev)
 {
 	ARG_UNUSED(dev);
-	struct gctrl_it8xxx2_regs *const gctrl_base = GCTRL_IT8XXX2_REG_BASE;
-	uint8_t last_reset_source = gctrl_base->GCTRL_RSTS & IT8XXX2_GCTRL_LRS;
+	struct gctrl_ite_ec_regs *const gctrl_base = GCTRL_ITE_EC_REGS_BASE;
+	uint8_t last_reset_source = gctrl_base->GCTRL_RSTS & IT51XXX_GCTRL_LRS;
 	uint8_t raw_reset_cause2 =
 		gctrl_base->GCTRL_SPCTRL4 &
-		(IT8XXX2_GCTRL_LRSIWR | IT8XXX2_GCTRL_LRSIPWRSWTR |
-		 IT8XXX2_GCTRL_LRSIPGWR);
+		(IT51XXX_GCTRL_LRSIWR | IT51XXX_GCTRL_LRSIPWRSWTR |
+		 IT51XXX_GCTRL_LRSIPGWR);
 
 	/* Clear reset cause. */
-	gctrl_base->GCTRL_RSTS |= IT8XXX2_GCTRL_LRS;
+	gctrl_base->GCTRL_RSTS |= IT51XXX_GCTRL_LRS;
 	gctrl_base->GCTRL_SPCTRL4 |=
-		(IT8XXX2_GCTRL_LRSIWR | IT8XXX2_GCTRL_LRSIPWRSWTR |
-		 IT8XXX2_GCTRL_LRSIPGWR);
+		(IT51XXX_GCTRL_LRSIWR | IT51XXX_GCTRL_LRSIPWRSWTR |
+		 IT51XXX_GCTRL_LRSIPGWR);
 
-	if (last_reset_source & IT8XXX2_GCTRL_IWDTR) {
+	if (last_reset_source & IT51XXX_GCTRL_IWDTR) {
 		return WATCHDOG_RST;
 	}
-	if (raw_reset_cause2 & IT8XXX2_GCTRL_LRSIWR) {
+	if (raw_reset_cause2 & IT51XXX_GCTRL_LRSIWR) {
 		/*
 		 * We can't differentiate between power-on and reset pin because
 		 * LRSIWR is set on both ~WRST assertion and power-on, and LRS
@@ -118,32 +118,47 @@ static int cros_system_it8xxx2_get_reset_cause(const struct device *dev)
 
 static int cros_system_it8xxx2_init(const struct device *dev)
 {
+#ifdef CONFIG_SOC_SERIES_IT8XXX2
 	struct gctrl_it8xxx2_regs *const gctrl_base = GCTRL_IT8XXX2_REG_BASE;
 
 	/* System triggers a soft reset by default (command: reboot). */
 	gctrl_base->GCTRL_ETWDUARTCR &= ~IT8XXX2_GCTRL_ETWD_HW_RST_EN;
-
+#endif
 	return 0;
 }
 
+static mm_reg_t wdt_base = DT_REG_ADDR(DT_NODELABEL(twd0));
+/* 0x87: External WDT Key */
+#define REG_EWDKEYR  0x07
+/* 0x81: External Timer1/WDT Configuration */
+#define REG_ETWCFG   0x01
+#define WDT_EWDKEYEN BIT(5)
+/* 0x85: External Timer1/WDT Control */
+#define REG_ETWCTRL  0x05
+#define WDT_EWDSCEN  BIT(5)
+
 static int cros_system_it8xxx2_soc_reset(const struct device *dev)
 {
-	struct gctrl_it8xxx2_regs *const gctrl_base = GCTRL_IT8XXX2_REG_BASE;
-	struct wdt_it8xxx2_regs *const wdt_base = WDT_IT8XXX2_REG_BASE;
+	//struct gctrl_it51xxx_regs *const gctrl_base = GCTRL_IT51XXX_REGS_BASE;
+	//struct wdt_it8xxx2_regs *const wdt_base = WDT_IT8XXX2_REG_BASE;
 	uint32_t chip_reset_flags = chip_read_reset_flags();
+	uint8_t reg_val;
 
 	/* Disable interrupts to avoid task swaps during reboot. */
 	interrupt_disable_all();
 
 	if (chip_reset_flags & (EC_RESET_FLAG_HARD | EC_RESET_FLAG_HIBERNATE))
-		gctrl_base->GCTRL_ETWDUARTCR |= IT8XXX2_GCTRL_ETWD_HW_RST_EN;
+		//gctrl_base->GCTRL_ETWDUARTCR |= IT8XXX2_GCTRL_ETWD_HW_RST_EN;
 
 	/*
 	 * Writing invalid key to watchdog module triggers a soft or hardware
 	 * reset. It depends on the setting of bit0 at ETWDUARTCR register.
 	 */
-	wdt_base->ETWCFG |= IT8XXX2_WDT_EWDKEYEN;
-	wdt_base->EWDKEYR = 0x00;
+	//wdt_base->ETWCFG |= IT8XXX2_WDT_EWDKEYEN;
+	//wdt_base->EWDKEYR = 0x00;
+	reg_val = sys_read8(wdt_base + REG_ETWCFG);
+	sys_write8(reg_val | WDT_EWDKEYEN, wdt_base + REG_ETWCFG);
+	sys_write8(0, wdt_base + REG_EWDKEYR);
 
 	/* Spin and wait for reboot */
 	while (1)
@@ -160,6 +175,38 @@ void wake_isr(enum gpio_signal signal)
 {
 }
 
+static mm_reg_t timer_base = DT_REG_ADDR(DT_NODELABEL(timer));
+/* 0x10, 0x18, 0x20, 0x28, 0x30, 0x38: External Timer 3-8 Control Register (n=0 to 5) */
+#define CROS_TIMER_ETNCTRL(n)   (0x10 + ((n) * 8))
+#define CROS_TIMER_ETCOMB       BIT(3)
+#define CROS_TIMER_ETNRST       BIT(1)
+#define CROS_TIMER_ETNEN        BIT(0)
+/* 0x11, 0x19, 0x21, 0x29, 0x31, 0x39: External Timer 3-8 Prescaler Register (n=0 to 5) */
+#define CROS_TIMER_ETNPSR(n)    (0x11 + ((n) * 8))
+/* 0x14, 0x1c, 0x24, 0x2c, 0x34, 0x3c: External Timer 3-8 Counter Register (n=0 to 5) */
+#define CROS_TIMER_ETNCNTLLR(n) (0x14 + ((n) * 8))
+/* 0x48, 0x4c, 0x50, 0x54, 0x58, 0x5c: External Timer 3-8 Counter Observation Register (n=0 to 5) */
+#define CROS_TIMER_ETNCNTOLR(n) (0x48 + ((n) * 4))
+
+#define FREE_RUN_TIMER            EXT_TIMER_4
+#define FREE_RUN_TIMER_IRQ        DT_IRQ_BY_IDX(DT_NODELABEL(timer), 1, irq)
+/* Free run timer max count is 36.4 hr (base on clock source 32768Hz) */
+#define FREE_RUN_TIMER_MAX_CNT    0xFFFFFFFFUL
+enum ext_timer_idx {
+	EXT_TIMER_3 = 0, /* Event timer */
+	EXT_TIMER_4,     /* Free run timer */
+	EXT_TIMER_5,     /* Busy wait low timer */
+	EXT_TIMER_6,     /* Busy wait high timer */
+	EXT_TIMER_7,
+	EXT_TIMER_8,
+};
+enum ext_clk_src_sel {
+	EXT_PSR_32P768K = 0,
+	EXT_PSR_1P024K,
+	EXT_PSR_32,
+	EXT_PSR_EC_CLK,
+};
+
 static int system_it8xxx2_hibernate_by_deep_doze(const struct device *dev,
 						 uint32_t seconds,
 						 uint32_t microseconds)
@@ -169,18 +216,29 @@ static int system_it8xxx2_hibernate_by_deep_doze(const struct device *dev,
 		 * Convert milliseconds(or at least 1 ms) to 32 Hz
 		 * free run timer count for hibernate.
 		 */
+		uint8_t etnctrl;
 		uint32_t c =
 			(seconds * 1000 + microseconds / 1000 + 1) * 32 / 1000;
 
 		/* Enable a 32-bit timer and clock source is 32 Hz */
 		/* Disable external timer x */
-		IT8XXX2_EXT_CTRLX(FREE_RUN_TIMER) &= ~IT8XXX2_EXT_ETXEN;
+		//IT8XXX2_EXT_CTRLX(FREE_RUN_TIMER) &= ~IT8XXX2_EXT_ETXEN;
+		etnctrl = sys_read8(timer_base + CROS_TIMER_ETNCTRL(FREE_RUN_TIMER));
+		sys_write8(etnctrl & ~CROS_TIMER_ETNEN,
+			   timer_base + CROS_TIMER_ETNCTRL(EXT_TIMER_4));
 		irq_disable(FREE_RUN_TIMER_IRQ);
-		IT8XXX2_EXT_PSRX(FREE_RUN_TIMER) = EXT_PSR_32;
-		IT8XXX2_EXT_CNTX(FREE_RUN_TIMER) = c & FREE_RUN_TIMER_MAX_CNT;
+		//IT8XXX2_EXT_PSRX(FREE_RUN_TIMER) = EXT_PSR_32;
+		sys_write8(EXT_PSR_32, timer_base + CROS_TIMER_ETNPSR(FREE_RUN_TIMER));
+		//IT8XXX2_EXT_CNTX(FREE_RUN_TIMER) = c & FREE_RUN_TIMER_MAX_CNT;
+		sys_write32(c & FREE_RUN_TIMER_MAX_CNT,
+			    timer_base + CROS_TIMER_ETNCNTLLR(FREE_RUN_TIMER));
 		/* Enable and re-start external timer x */
-		IT8XXX2_EXT_CTRLX(FREE_RUN_TIMER) |=
-			(IT8XXX2_EXT_ETXEN | IT8XXX2_EXT_ETXRST);
+		//IT8XXX2_EXT_CTRLX(FREE_RUN_TIMER) |=
+		//	(IT8XXX2_EXT_ETXEN | IT8XXX2_EXT_ETXRST);
+		etnctrl = sys_read8(timer_base + CROS_TIMER_ETNCTRL(FREE_RUN_TIMER));
+		sys_write8(etnctrl | CROS_TIMER_ETNRST | CROS_TIMER_ETNEN,
+			   timer_base + CROS_TIMER_ETNCTRL(FREE_RUN_TIMER));
+
 		irq_enable(FREE_RUN_TIMER_IRQ);
 	}
 
@@ -324,7 +382,7 @@ static int cros_system_it8xxx2_hibernate(const struct device *dev,
 					 uint32_t seconds,
 					 uint32_t microseconds)
 {
-	struct wdt_it8xxx2_regs *const wdt_base = WDT_IT8XXX2_REG_BASE;
+	//struct wdt_it8xxx2_regs *const wdt_base = WDT_IT8XXX2_REG_BASE;
 
 	/* Disable all interrupts. */
 	interrupt_disable_all();
@@ -333,7 +391,9 @@ static int cros_system_it8xxx2_hibernate(const struct device *dev,
 	ite_intc_save_and_disable_interrupts();
 
 	/* bit5: watchdog is disabled. */
-	wdt_base->ETWCTRL |= IT8XXX2_WDT_EWDSCEN;
+	//wdt_base->ETWCTRL |= IT8XXX2_WDT_EWDSCEN;
+	uint8_t reg_val = sys_read8(wdt_base + REG_ETWCTRL);
+	sys_write8(reg_val | WDT_EWDSCEN, wdt_base + REG_ETWCTRL);
 
 	/*
 	 * Setup GPIOs for hibernate. On some boards, it's possible that this
