94b6ba16e288819268fdd2c615276e150abf1f65
Jan Malek <jmalek@antmicro.com>
2024-10-24 10:41:47 +0200

    test/run_device_tests.py: Extend timer test timeout for Renode
    Due to a performance regression in Renode the test takes longer to finish.
    Extend the test timeout until the performance is fixed.
    
    BUG=b:372968708
    TEST=CQ
    
    Change-Id: If07774e42352453fc095b0fe25cd710f02cde30c

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -397,11 +397,6 @@ class Renode(Platform):
 
     def skip_test(self, test_name: str, board_config: BoardConfig) -> bool:
         if board_config.name in [BLOONCHIPPER, DARTMONKEY]:
-            if board_config.name == BLOONCHIPPER:
-                if test_name in [
-                    "timer",  # TODO(b/372968708)
-                ]:
-                    return True
             # TODO(b/356476313): Remove these when Renode is fixed.
             if test_name in [
                 "production_app_test",
@@ -701,7 +696,9 @@ class AllTests:
                 toggle_power=True,
                 enable_hw_write_protect=False,
             ),
-            TestConfig(test_name="timer"),
+            # Timeout is temporarily increased due to a performance
+            # regression in Renode.
+            TestConfig(test_name="timer", timeout_secs=15),
             # task_wait_event works only with the shimmed task list, which is
             # hardcoded. The task synchronization functions are covered by
             # Zephyr tests. task_wait_event is implemented based on k_poll_event
