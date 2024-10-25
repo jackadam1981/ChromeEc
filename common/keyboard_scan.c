2d82bb7c60b27244bc0833e0e3c26bf9fe74e671
Fabio Baltieri <fabiobaltieri@google.com>
2024-10-25 15:21:16 +0100

    keyboard_scan: only do the extra col2 delay when changing col2
    The col2 line requires an extra 30us delay when being asserted or
    deasserted, right now this is added to the normal delay by changing the
    board configuration and it's applied to all lines, but that is only
    required when asserting or deasserting col2, leading to some unnecessary
    delays when scanning the other columns.
    
    Change the scanning logic to only delay when selecting or deselcting
    col2, which saves few cpu cycles that are otherwise spent spinning.
    
    This requried tweaking all board configs that were overriding the delay,
    some of those are now back on the default config from keyboard_scan.c.
    
    Drop few overridden structures that are now redundant, and few comments
    that makes it looks like they are redundant when they are not.
    
    BUG=b:376670220
    TEST=build and run on rex, check all the keys with ksstate on, no false
    triggers
    
    Change-Id: Ic603760054b10a16a69c6b839da54a77655f7909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5966733
    Reviewed-by: Rob Barnes <robbarnes@google.com>
    Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -60,19 +60,23 @@
 #define CONFIG_KEYBOARD_POST_SCAN_CLOCKS 16000
 #endif
 
-__overridable struct keyboard_scan_config keyscan_config = {
+/*
+ * CONFIG_KEYBOARD_COL2_INVERTED is defined for passing the column 2
+ * to H1 which inverts the signal. The signal passing through H1
+ * adds more delay. Need a larger delay value. Otherwise, pressing
+ * Refresh key will also trigger T key, which is in the next scanning
+ * column line. See http://b/156007029.
+ */
 #ifdef CONFIG_KEYBOARD_COL2_INVERTED
-	/*
-	 * CONFIG_KEYBOARD_COL2_INVERTED is defined for passing the column 2
-	 * to H1 which inverts the signal. The signal passing through H1
-	 * adds more delay. Need a larger delay value. Otherwise, pressing
-	 * Refresh key will also trigger T key, which is in the next scanning
-	 * column line. See http://b/156007029.
-	 */
-	.output_settle_us = 80,
+#define COL2_DELAY_US 30
 #else
+#define COL2_DELAY_US 0
+#endif
+
+#define COL2 2
+
+__overridable struct keyboard_scan_config keyscan_config = {
 	.output_settle_us = 50,
-#endif /* CONFIG_KEYBOARD_COL2_INVERTED */
 	.debounce_down_us = 9 * MSEC,
 	.debounce_up_us = 30 * MSEC,
 	.scan_period_us = 3 * MSEC,
@@ -301,6 +305,12 @@ static int read_matrix(uint8_t *state, bool at_boot)
 		/* Select column, then wait a bit for it to settle */
 		keyboard_raw_drive_column(c);
 		udelay(keyscan_config.output_settle_us);
+
+		/* Only add the extre delay when selecting or deselecting COL2
+		 */
+		if (c == COL2 || c == (COL2 + 1)) {
+			udelay(COL2_DELAY_US);
+		}
 
 		/* Read the row state */
 		state[c] = keyboard_raw_read_rows();
@@ -748,7 +758,7 @@ static uint8_t keyboard_scan_column(int column)
 	uint8_t state;
 
 	keyboard_raw_drive_column(column);
-	udelay(keyscan_config.output_settle_us);
+	udelay(keyscan_config.output_settle_us + COL2_DELAY_US);
 #ifdef CONFIG_KEYBOARD_SCAN_ADC
 	state = keyboard_read_adc_rows();
 #else
@@ -1026,7 +1036,8 @@ void keyboard_scan_task(void *u)
 				 * results.
 				 */
 				keyboard_raw_drive_column(KEYBOARD_COLUMN_ALL);
-				udelay(keyscan_config.output_settle_us);
+				udelay(keyscan_config.output_settle_us +
+				       COL2_DELAY_US);
 			} else if (!local_disable_scanning) {
 				/*
 				 * Scanning isn't enabled but it was last time
