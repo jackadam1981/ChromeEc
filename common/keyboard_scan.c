40e68337422b915710f071a48bbb8a96bc493b45
Fabio Baltieri <fabiobaltieri@google.com>
2024-10-25 16:58:01 +0100

    keyboard_scan: slow down polling rate when not debouncing
    The keyboard scanning task polls continuously as long as any button is
    pressed. This is necessary as the idling detection requires all butons
    to be released, however, we only need dense data points to debounce
    the matrix (or rather, deglitching it).
    
    Implement a new setting for slowing down the polling rate when the
    matrix is stable, which should help reducing the keyboard task CPU
    utilization when buttons are pressed but not changing.
    
    Set the default tune it to 9ms stable scans (normal is 3ms), update all
    existing boards to use the new timings if the previous setting was the
    default, maintain the existing behavior if the settig was tuned (many
    defaults to 10ms already, some to 1.5ms).
    
    The number is somewhat arbitrary at this point but it seems like a
    reasonable starting point.
    
    The new field may end up being unset on overridden config that do not
    have the new value, in that case the scan would default to use the
    debounce delay all the time (i.e. the current behavior).
    
    A col2 waveform with this looks something like
    https://screenshot.googleplex.com/3c7ZqSNqRygXB7B
    
    BUG=b:376670220
    TEST=build and run on brex, captured some waveform, typed a bunch of
    stuff, checked all keys with ksstate as well
    
    Change-Id: Ia89c75a02febfcba994b091560fbe1ffde0a5ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5966734
    Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: Yuval Peress <peress@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>

--- common/keyboard_scan.c
+++ common/keyboard_scan.c
@@ -80,6 +80,7 @@ __overridable struct keyboard_scan_config keyscan_config = {
 	.debounce_down_us = 9 * MSEC,
 	.debounce_up_us = 30 * MSEC,
 	.scan_period_us = 3 * MSEC,
+	.stable_scan_period_us = 9 * MSEC,
 	.min_post_scan_delay_us = 1000,
 	.poll_timeout_us = 100 * MSEC,
 	.actual_key_mask = {
@@ -219,7 +220,7 @@ static void ensure_keyboard_scanned(int old_polls)
 	 */
 	while ((kbd_polls == old_polls) &&
 	       (get_time().val - start_time < SCAN_TASK_TIMEOUT_US))
-		usleep(keyscan_config.scan_period_us);
+		usleep(keyscan_config.stable_scan_period_us);
 }
 
 /**
@@ -997,6 +998,17 @@ void keyboard_scan_init(void)
 #endif /* CONFIG_KEYBOARD_BOOT_KEYS */
 }
 
+static bool keyboard_is_debouncing(void)
+{
+	for (uint8_t c = 0; c < keyboard_cols; c++) {
+		if (debouncing[c] != 0) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
 void keyboard_scan_task(void *u)
 {
 	timestamp_t poll_deadline, start;
@@ -1103,8 +1115,14 @@ void keyboard_scan_task(void *u)
 			}
 
 			/* Delay between scans */
-			wait_time = keyscan_config.scan_period_us -
-				    (get_time().val - start.val);
+			if (keyscan_config.stable_scan_period_us > 0 &&
+			    !keyboard_is_debouncing()) {
+				wait_time =
+					keyscan_config.stable_scan_period_us;
+			} else {
+				wait_time = keyscan_config.scan_period_us;
+			}
+			wait_time -= get_time().val - start.val;
 
 			if (wait_time < keyscan_config.min_post_scan_delay_us)
 				wait_time =
