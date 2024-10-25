c0af338c21c7fece076ade84619481594727318e
Jon Murphy <jpmurphy@google.com>
2024-10-25 09:28:10 -0600

    trulo: Add support for picolobic
    BUG=None
    TEST=CQ
    
    Change-Id: I8e36a946cd737bbaf8b0be5b5f9e489f1464f426

--- zephyr/drivers/dsp_comms/client/dsp_client.c
+++ zephyr/drivers/dsp_comms/client/dsp_client.c
@@ -216,6 +216,8 @@ int dsp_client_get_cbi_flags(const struct device* dev,
 
   __ASSERT_NO_MSG(encode_status);
 
+  (void)encode_status;
+
   /* Write the message */
   LOG_DBG("Writing %zu bytes", stream.bytes_written);
   rc = i2c_write_dt(&cfg->i2c, data->request_buffer, stream.bytes_written);
