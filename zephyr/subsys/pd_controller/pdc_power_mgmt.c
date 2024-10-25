b4e819c340043d99c882652fec0672df752000db
Tristan Honscheid <honscheid@google.com>
2024-10-24 21:45:04 -0700

    pdc: Reset get_pdo state before requesting PDOs
    The sink-attached entry flow needs to call GET_PDOs twice since it can
    only request 4 request PDOs at a time. If the flow gets disrupted
    between the calls by a connector status change to some other state (e.g.
    unattached), `port->get_pdo` does not get re-initialized.
    
    Reset `port->get_pdo` upon entering the sink state and when jumping
    directly into the SNK_ATTACHED_GET_PDOS substate.
    
    BUG=None
    TEST=Re-run compliance test test.pd.ps.snk.01
    
    Change-Id: I2d3c596bebb0a792f697578bde416b6f289f16eb

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -1488,6 +1488,7 @@ static void run_snk_policies(struct pdc_port_t *port)
 		return;
 	} else if (atomic_test_and_clear_bit(port->snk_policy.flags,
 					     SNK_POLICY_NEW_POWER_REQUEST)) {
+		port->get_pdo = (struct get_pdo_t){ 0 };
 		port->snk_attached_local_state = SNK_ATTACHED_GET_PDOS;
 		return;
 	} else if (atomic_test_and_clear_bit(port->snk_policy.flags,
@@ -1896,8 +1897,10 @@ static void pdc_snk_attached_entry(void *obj)
 		const struct pdc_config_t *config = port->dev->config;
 		int port_number = config->connector_num;
 
+		/* Reset local state */
 		port->snk_attached_local_state =
 			SNK_ATTACHED_GET_CONNECTOR_CAPABILITY;
+		port->get_pdo = (struct get_pdo_t){ 0 };
 
 		/* If we were just a SRC, tell the DPM that the
 		 * attached sink has been disconnected.
