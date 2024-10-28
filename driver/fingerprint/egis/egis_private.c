4515505bb08c201e9d23efbbd3558954de31150f
jacob.kung <jacob.kung@egistec.corp-partner.google.com>
2024-10-28 18:39:55 +0800

    driver/fingerprint: Add brief description for functions in egis_api.h
    Add a brief description of the functions declared in the egis_api header
    files including input parameters and return parameters. Remove any
    function description from the source files.
    
    BUG=b:369421451
    TEST=CQ
    
    Change-Id: I4c351b498bbc4301d369e4a4e14240f3d598262f

--- driver/fingerprint/egis/egis_private.c
+++ driver/fingerprint/egis/egis_private.c
@@ -54,7 +54,6 @@ void fp_sensor_low_power(void)
 	egis_sensor_power_down();
 }
 
-/* Reset and initialize the sensor IC */
 int fp_sensor_init(void)
 {
 	int ret = egis_sensor_init();
@@ -67,7 +66,6 @@ int fp_sensor_init(void)
 	return EC_SUCCESS;
 }
 
-/* Deinitialize the sensor IC */
 int fp_sensor_deinit(void)
 {
 	return egis_sensor_deinit();
