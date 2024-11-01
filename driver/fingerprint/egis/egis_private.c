742a57b6e5be39bcb8ac366d0596f1a7e5b305d4
jacob.kung <jacob.kung@egistec.corp-partner.google.com>
2024-11-01 18:55:13 +0800

    driver/fingerprint: modify `fp_finger_status` return value
    1. modify init value to `FINGER_NONE`
    
    2. fix FINGER_LOST should wrap to FINGER_NONE
    
    BUG=b:356211639
    TEST=CQ
    
    Change-Id: I0903aa9940ebbf524bfa01ab2f65adc18c43485f

--- driver/fingerprint/egis/egis_private.c
+++ driver/fingerprint/egis/egis_private.c
@@ -137,7 +137,7 @@ int fp_acquire_image(uint8_t *image_data)
 
 enum finger_state fp_finger_status(void)
 {
-	int rc = EGIS_API_FINGER_UNSTABLE;
+	int rc = FINGER_NONE;
 	egislog_i("");
 	rc = egis_check_int_status();
 
@@ -146,7 +146,7 @@ enum finger_state fp_finger_status(void)
 		rc = FINGER_PRESENT;
 		break;
 	case EGIS_API_FINGER_LOST:
-		rc = FINGER_PARTIAL;
+		rc = FINGER_NONE;
 		break;
 	default:
 		rc = FINGER_NONE;
