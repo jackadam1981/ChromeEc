cf938b47b16491e9540d1fa2495555a81dc03d24
Mary Ruthven <mruthven@chromium.org>
2024-11-05 11:33:45 -0600

    tpm2: update old fwmp attributes
    Change old FWMP attributes to the new version of FWMP attributes in the
    platform Startup callback.
    
    BUG=none
    TEST=manual
    device_management_client --action=set_firmware_management_parameters \
    --flags=FLAGS
    tpm_manager_client get_space_info --index=0x100a
    Verify it shows the old attributes
    
    Reboot the DUT. Verify the attributes are updated.
    
    Add a __NvFixAttributes call to NV_DefineSpace. Verify the space is
    created with the correct attributes.
    
    Change-Id: Ifa9d57578af70c7ec038531dbc42ce08661e971e

--- board/cr50/tpm2/platform.c
+++ board/cr50/tpm2/platform.c
@@ -6,6 +6,7 @@
 #include "Global.h"
 #include "Platform.h"
 #include "TPM_Types.h"
+#include "NV_fp.h"
 
 #include "boot_param_platform_cr50.h"
 #include "ccd_config.h"
@@ -109,9 +110,66 @@ void   _plat__GetFwVersion(uint32_t *firmwareV1, uint32_t *firmwareV2)
 	*firmwareV2 = strtoi(ver_str, NULL, 16);
 }
 
+static TPMA_NV board_get_new_fwmp_attributes(TPMA_NV attributes)
+{
+	TPMA_NV new_attributes;
+
+	memset(&new_attributes, 0, sizeof(new_attributes));
+	new_attributes.TPMA_NV_PLATFORMCREATE = 1;
+	new_attributes.TPMA_NV_OWNERWRITE = 1;
+	new_attributes.TPMA_NV_AUTHREAD = 1;
+	new_attributes.TPMA_NV_PPREAD = 1;
+	new_attributes.TPMA_NV_PPWRITE = 1;
+
+	if (attributes.TPMA_NV_WRITTEN == 1)
+		new_attributes.TPMA_NV_WRITTEN = 1;
+
+	CPRINTF("%s: updating FWMP attrs\n", __func__);
+	return new_attributes;
+}
+
+static bool fwmp_uses_old_attributes(TPMA_NV attributes)
+{
+	return (attributes.TPMA_NV_AUTHWRITE == 1 &&
+		attributes.TPMA_NV_AUTHREAD == 1 &&
+		attributes.TPMA_NV_PPREAD  == 1 &&
+		attributes.TPMA_NV_WRITEDEFINE == 1);
+}
+
+/* Update NV_INDEX_FWMP attributes. */
+static void board_update_fwmp_attributes(void)
+{
+	TPM_HANDLE handle;
+	NV_INDEX nvIndex;
+	TPM_RC result;
+	uint32_t handle_addr;
+
+	result = NvIsAvailable();
+	if (result != TPM_RC_SUCCESS)
+		return;
+
+	handle = HR_NV_INDEX + NV_INDEX_FWMP;
+	handle_addr = NvEarlyStageFindHandle(handle);
+	if (!handle_addr) {
+		CPRINTF("%s: object at 0x%x not found\n", __func__,
+			NV_INDEX_FWMP);
+		return;
+	}
+
+	NvGetIndexInfo(handle, &nvIndex);
+
+	if (!fwmp_uses_old_attributes(nvIndex.publicArea.attributes))
+		return;
+
+	nvIndex.publicArea.attributes = board_get_new_fwmp_attributes(
+			nvIndex.publicArea.attributes);
+	NvWriteIndexInfo(handle, &nvIndex);
+}
+
 void _plat__StartupCallback(void)
 {
 	pinweaver_init();
+	board_update_fwmp_attributes();
 	boot_param_handle_tpm_startup();
 
 	/*
