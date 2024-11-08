0a11f6a52d2b12e53f39388445d0af944fcd31b9
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2024-11-08 17:37:27 +0800

    (TEST) get RO/RW timing
    build legacy it83xx_evb(Nds) or it8xxx2_evb(RiscV) => RO & RW image
    
    1.get RO/RW read 4KB flash timing
    1-1.disable interrupts during the test
    1-2.in ro: flashread,
    it83xx_evb: md .b 0x38000 0x1000
    it8xxx2_evb: md .b 0x08000 0x1000
    1-3.in rw: flashread,
    it83xx_evb: md .b 0x78000 0x1000
    it8xxx2_evb: md .b 0x88000 0x1000
    
    2.get RO/RW execute 4KB data timing by get_time() or GPA0
    ex .n
    
    Change-Id: Id85d19a605ca5289ada72f165df58cea7ad911fc

--- core/riscv-rv32i/cpu.c
+++ core/riscv-rv32i/cpu.c
@@ -6,9 +6,64 @@
  */
 
 #include "cpu.h"
+#include "console.h"
+#include "system.h"
+#include "timer.h"
+#include "util.h"
+#include "gpio.h"
 
 void cpu_init(void)
 {
 	/* bit3: Global interrupt enable (M-mode) */
 	asm volatile("csrsi mstatus, 0x8");
 }
+
+/* This function must be a sub-routine in cpu_console_cmd(), or branch instruction can't across 4KB gap, so do not optimize. */
+static void __attribute__((optimize("O0"))) execute_nop_1024_cnt(void)
+{
+	/* Loop to execute 4 bytes nop instruction for 1024 counts */
+	asm volatile(".rept 0x400\n\t"
+		     "nop\n\t"
+		     ".endr\n\t");
+}
+
+static int cpu_console_cmd(int argc, const char **argv)
+{
+	//int time_delta;
+	//uint64_t time_start, time_stop;
+
+	if (argc > 1) {
+		if ((argv[1][0] == '.') && (strlen(argv[1]) == 2)) {
+			switch (argv[1][1]) {
+			case 'n':
+				/* disable all interrupts */
+				interrupt_disable();
+
+				//time_start = get_time().val;
+				gpio_set_level(GPIO_TEST_A0, 1);
+
+				/* equivalent Nds32 fetch 4KB(1024*4) instruction from flash and execute */
+				execute_nop_1024_cnt();
+
+				//time_stop = get_time().val;
+				//time_delta = time_stop - time_start;
+				gpio_set_level(GPIO_TEST_A0, 0);
+
+				/* enable all interrupts */
+				interrupt_enable();
+
+				//ccprintf("time_delta %d(us)\n", time_delta);
+				cflush();
+				return EC_SUCCESS;
+			default:
+				return EC_ERROR_PARAM1;
+			}
+		}
+	}
+
+	return EC_ERROR_PARAM1;
+}
+
+DECLARE_CONSOLE_COMMAND(
+	ex, cpu_console_cmd, "[.n]",
+	"execute 4bytes nop instruction for 1024 counts");
