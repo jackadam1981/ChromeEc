1295e714feb182633b91e844816c841b7e848a66
Jes B. Klinke <jbk@chromium.org>
2024-11-13 14:31:34 -0800

    common: Introduce buffered mode and queue_flush()
    Add a new method queue_flush() to be used together with
    e.g. queue_add_units(), such that a producer can inform the consumer
    when it is OK to leave some bytes/units in the queue waiting for more,
    and when the queue needs to be flushed without delay.
    
    This is intended for the usb-stream consumer, which can greatly benefit
    from completely filling 64-byte USB packets, rather than
    transmitting a single byte at a time, as they arrive on a UART.
    
    Consumers will be notified of a request to flush by getting a call of
    consumer_ops.written() with a zero length.
    
    Some consumers will not know about the flushing semantics, but that is
    fine, they will ignore a notification with zero length, and they will
    eagerly process data, which is always allowed.
    
    Some producers will not make use of queue_flush(), and care has to be
    taken that a consumer does not wait infinitely for a flush request that
    never comes.  Producers should advertise their intent to explicitly
    flush by calling queue_enable_buffered_mode() during initialization.
    Consumers which want to process data in larger chunks must inspect
    queue_is_buffered() to learn whether they can count on the producer
    explicitly flushing.
    
    BUG=b:378954948
    TEST=make tast TAST_EXPR=gscdevboard.GSCSysinfo  (in ti50/common)
    
    Change-Id: I0f6dd4bcb582b164a28f91ce65ef977f78c3198e

--- common/queue.c
+++ common/queue.c
@@ -29,6 +29,12 @@ void queue_init(struct queue const *q)
 
 	q->state->head = 0;
 	q->state->tail = 0;
+	q->state->flags = 0;
+}
+
+void queue_enable_buffered_mode(struct queue const *q)
+{
+	q->state->flags |= QUEUE_BUFFERED_MODE;
 }
 
 int queue_is_empty(struct queue const *q)
@@ -121,9 +127,10 @@ size_t queue_advance_tail(struct queue const *q, size_t count)
 {
 	size_t transfer = MIN(count, queue_space(q));
 
-	q->state->tail += transfer;
-
-	q->policy->add(q->policy, transfer);
+	if (transfer > 0) {
+		q->state->tail += transfer;
+		q->policy->add(q->policy, transfer);
+	}
 
 	return transfer;
 }
@@ -163,6 +170,33 @@ size_t queue_add_memcpy(struct queue const *q, const void *src, size_t count,
 		       (transfer - first) * q->unit_bytes);
 
 	return queue_advance_tail(q, transfer);
+}
+
+void queue_flush(struct queue const *q)
+{
+	if (!(q->state->flags & QUEUE_BUFFERED_MODE)) {
+		/* Flushing of a non-buffered queue is a no-op. */
+		return;
+	}
+
+	/*
+	 * Consumers will be notified of a request to flush by getting a call
+	 * of consumer_ops.written() with a zero length.
+	 *
+	 * Some consumers will not know about the flushing semantics, but that
+	 * is fine, they will ignore a notification with zero length, and they
+	 * will eagerly process data, which is always allowed.
+	 *
+	 * Some producers will not make use of queue_flush(), and care has to
+	 * be taken that a consumer does not wait infinitely for a flush
+	 * request that never comes.  To handle this, consumers must eagerly
+	 * process data until the first time they get a notification with zero
+	 * length, which serves as indication that the producer uses the flush
+	 * semantics.  Producers can make a call to queue_flush() as part of
+	 * their initialization, to avoid any actual data being eagerly
+	 * processed.
+	 */
+	q->policy->add(q->policy, 0);
 }
 
 static void
