43485c811ace5f8c2a3bc14cd75bb30dc0e76076
Jes B. Klinke <jbk@chromium.org>
2024-11-13 14:33:09 -0800

    WIP: USB stream driver delays outbound USB packet until flush
    BUG=b:378954948
    TEST=make tast TAST_EXPR=gscdevboard.GSCSysinfo  (in ti50/common)
    
    Change-Id: I55a6b6bfd4728440db52410683e2ee18384f7751

--- chip/stm32/usb-stream.c
+++ chip/stm32/usb-stream.c
@@ -34,12 +34,30 @@ static size_t rx_read(struct usb_stream_config const *config)
 
 static size_t tx_write(struct usb_stream_config const *config)
 {
+	size_t c = queue_count(config->consumer.queue);
+	if ((config->state->flags &
+	     (USB_STREAM_TX_USES_FLUSH | USB_STREAM_TX_FLUSH)) ==
+		    USB_STREAM_TX_USES_FLUSH &&
+	    c < 64) {
+		/*
+		 * Producer has declared that it uses flush(), but has not yet
+		 * flushed the data in queue, and there is not enough to fill a
+		 * USB packet, so wait for either getting more data or an
+		 * explicit flush().
+		 */
+		btable_ep[config->endpoint].tx_count = 0;
+		return 0;
+	}
+
 	uintptr_t address = btable_ep[config->endpoint].tx_addr;
 	size_t count = queue_remove_memcpy(config->consumer.queue,
 					   (void *)address, config->tx_size,
 					   memcpy_to_usbram);
 
 	btable_ep[config->endpoint].tx_count = count;
+
+	if (count < 64)
+		config->state->flags &= ~USB_STREAM_TX_FLUSH;
 
 	return count;
 }
@@ -66,7 +84,10 @@ static void usb_written(struct consumer const *consumer, size_t count)
 {
 	struct usb_stream_config const *config =
 		DOWNCAST(consumer, struct usb_stream_config, consumer);
-
+	if (!count) {
+		config->state->flags |= USB_STREAM_TX_USES_FLUSH |
+					USB_STREAM_TX_FLUSH;
+	}
 	hook_call_deferred(config->deferred, 0);
 }
 
