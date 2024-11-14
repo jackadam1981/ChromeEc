047ee3f84554eaa5144381f56ba8e16e83e97cc8
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2024-11-14 15:54:35 +0800

    (TEST) TEST.PD.PROT.SRC.3 #2 & TEST.PD.PROT.SNK.5 #1
    tune
    PD2_T_SENDER_RESPONSE,
    PD3_T_SENDER_RESPONSE,
    
    Change-Id: I6d470e7ff24a02206191b73f2d50443ed87b5f5a

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -261,12 +261,12 @@ enum pdo_augmented_pps {
 #define PD_T_SENDER_RESPONSE (30 * MSEC) /* between 24ms and 30ms */
 #else
 /* PD R2.0 V1.3: between 24ms and 30ms */
-#define PD2_T_SENDER_RESPONSE (24 * MSEC)
+#define PD2_T_SENDER_RESPONSE (25 * MSEC)
 /*
  * PD R3.1 V1.5: between 26ms and 32ms
  * PD R3.2 V1.0: between 27ms and 33ms
  */
-#define PD3_T_SENDER_RESPONSE (27 * MSEC)
+#define PD3_T_SENDER_RESPONSE (28 * MSEC)
 #endif
 #define PD_T_PS_TRANSITION (500 * MSEC) /* between 450ms and 550ms */
 /*
