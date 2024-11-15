8ad08bb7ad43b4b0b5494aee1a2f3454a9ea560c
Firas Sammoura <fsammoura@google.com>
2024-11-15 21:47:25 +0000

    driver/fingerprint/elan: Clamp dead pixels to maximum possible value
    The maximum number of reported dead pixels should be the
    FP_ERROR_DEAD_PIXELS_MAX. This leaves the FP_ERROR_DEAD_PIXELS_UNKNOWN
    to represent the state when the number of dead pixels is unknown rather
    than clamped.
    
    BUG=b:379309012
    TEST=CQ
    
    Change-Id: Idc37b1aecdf2fdabc14a69bd9d2f6264e73142f3

--- driver/fingerprint/elan/elan_sensor_pal.c
+++ driver/fingerprint/elan/elan_sensor_pal.c
@@ -227,8 +227,8 @@ int elan_fp_maintenance(uint16_t *error_state)
 		LOGE_SA("Failed to run maintenance: %d", rv);
 		return EC_ERROR_HW_INTERNAL;
 	}
-	if (sensor_info.num_defective_pixels >= FP_ERROR_DEAD_PIXELS_UNKNOWN)
-		*error_state |= FP_ERROR_DEAD_PIXELS_UNKNOWN;
+	if (sensor_info.num_defective_pixels >= FP_ERROR_DEAD_PIXELS_MAX)
+		*error_state |= FP_ERROR_DEAD_PIXELS_MAX;
 	else {
 		/*
 		 * Reset the number of dead pixels before any update.
