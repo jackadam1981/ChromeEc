76650bc21e4ecacac793198f4353f590b6612df5
Derek Huang <derekhuang@google.com>
2024-11-15 07:06:52 +0000

    [Test Only] ovis: Set AP_IDLE flag when G3
    Change-Id: I73701d5ac03c2265d9f2059db9bd6b8b573650c1

--- zephyr/program/ovis/src/board_power.c
+++ zephyr/program/ovis/src/board_power.c
@@ -3,8 +3,11 @@
  * found in the LICENSE file.
  */
 
+#include "console.h"
 #include "gpio/gpio.h"
 #include "gpio_signal.h"
+#include "hooks.h"
+#include "system.h"
 #include "system_boot_time.h"
 
 #include <zephyr/drivers/gpio.h>
@@ -22,6 +25,9 @@ LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
 #if defined(CONFIG_X86_NON_DSX_PWRSEQ_MTL) || \
 	defined(CONFIG_TEST_X86_NON_DSX_PWRSEQ_MTL)
 #define X86_NON_DSX_MTL_FORCE_SHUTDOWN_TO_MS 50
+
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ##args)
 
 void board_ap_power_force_shutdown(void)
 {
@@ -62,4 +68,29 @@ bool board_ap_power_check_power_rails_enabled(void)
 {
 	return power_signal_get(PWR_EN_PP3300_A);
 }
+
+#ifdef CONFIG_POWER_BUTTON_INIT_IDLE
+/*
+ * The AP_IDLE flag is expected to be set when graceful shutdown
+ * (e.g. shutdown from UI). The flag is set when CHIPSET_SHUTDOWN hook
+ * is called during AP power state S4 to S5 transition. On Deku, the
+ * AP_IDLE flag is set unpextedly when AC power is removed. As a workaround,
+ * delay the timing of setting the AP_IDLE flag, set the flag when the
+ * CHIPSET_HARD_OFF hook is called when AP power state transist to G3 to
+ * ensure the flag is set only when graceful shutdown.
+ */
+__override void pb_chipset_shutdown(void)
+{
+	return;
+}
+
+void board_set_ap_idle_at_g3()
+{
+	chip_save_reset_flags(chip_read_reset_flags() | EC_RESET_FLAG_AP_IDLE);
+	system_set_reset_flags(EC_RESET_FLAG_AP_IDLE);
+	CPRINTS("Saved AP_IDLE flag");
+}
+DECLARE_HOOK(HOOK_CHIPSET_HARD_OFF, board_set_ap_idle_at_g3,
+	     HOOK_PRIO_PRE_DEFAULT);
+#endif /* CONFIG_POWER_BUTTON_INIT_IDLE */
 #endif /* CONFIG_X86_NON_DSX_PWRSEQ_MTL */
