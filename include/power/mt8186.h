0966a0a600ed2b9b9e92ba5313a217dc6b88b9f1
Eric Yilun Lin <yllin@chromium.org>
2024-11-21 10:50:29 -0800

    navi: Handle sleep hang for suspend workaround
    This commit introduces a new hook, board_handle_sleep_hang(), enabling
    boards to customize the handling of sleep hang events.
    
    This change implements this hook on Navi to manage sleep hang events by
    disabling the CS interrupt and configuring the S3 indicator GPIO as an
    input.  This workarounds an issue where the system fails to suspend
    correctly.
    
    This change enhances the robustness of the sleep hang handling mechanism
    by allowing boards to take specific actions in response to sleep hang
    events.
    
    BUG=b:354870788
    TEST=sleeptimeout 100; Reproduce sleep hang and ensure the system can then
         successfully suspend.
    
    Change-Id: I8c2dbc4ec1d323e213585ca2d10196c1869347eb

--- include/power/mt8186.h
+++ include/power/mt8186.h
@@ -8,8 +8,10 @@
 
 #include "common.h"
 #include "ec_commands.h"
+#include "power.h"
 
 __override_proto void
-board_process_host_sleep_event(enum host_sleep_event state);
+board_handle_host_sleep_event(enum host_sleep_event state);
+__override_proto void board_handle_sleep_hang(enum sleep_hang_type hang_type);
 
 #endif /* __CROS_EC_POWER_MT8186_H_ */
