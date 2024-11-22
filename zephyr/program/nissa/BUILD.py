2218f968ef851bc71c8d182baa00341a73cb1d21
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2024-11-22 13:07:40 +0800

    [TEST]: Initial Zephyr EC image
    Patchset1
    Create the initial Zephyr EC image for the new variant based on
    the nereid reference board.
    
    Patchset2: Remove support for sensors
    It doesn't have any motions sensors. This CL removes support for
    sensors from the initial project.
    
    BUG=none
    TEST=zmake build pass
    
    Change-Id: I1ba0132ad5003b9f48f2c3bb4d603e15b4580783

--- zephyr/program/nissa/BUILD.py
+++ zephyr/program/nissa/BUILD.py
@@ -218,6 +218,11 @@ rull = register_nissa_project(
     chip="it8xxx2/it81302bx",
 )
 
+test0wy = register_nissa_project(
+    project_name="test0wy",
+    chip="it8xxx2/it81302bx",
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="anraggar", addr=0xBFFE0)
@@ -249,3 +254,4 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="domika", addr=0xB7FE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="teliks", addr=0xBFFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="rull", addr=0xBFFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="telith", addr=0xBFFE0)
+assert_rw_fwid_DO_NOT_EDIT(project_name="test0wy", addr=0xBFFE0)
