4b5bf58825820d97967db03da79b1e94cf3d5167
Firas Sammoura <fsammoura@google.com>
2024-11-25 15:36:12 -0800

    test:
    Change-Id: I6a19161034f804cab39ee7c181dcf00cc334ca01

--- common/system.c
+++ common/system.c
@@ -189,16 +189,21 @@ static uint32_t __attribute__((unused)) get_size(enum ec_image copy)
 
 test_mockable int system_is_locked(void)
 {
+	ccprints("%s is being called.", __func__);
 	static int is_locked = -1;
 
 	if (force_locked)
 		return 1;
-	if (is_locked != -1)
+	if (is_locked != -1) {
+		ccprints("I am at line %d", __LINE__);
+		ccprints("The value of is_locked: %d", is_locked);
 		return is_locked;
+	}
 
 #ifdef CONFIG_SYSTEM_UNLOCKED
 	/* System is explicitly unlocked */
 	is_locked = 0;
+	ccprints("I am at line %d", __LINE__);
 	return 0;
 
 #elif defined(CONFIG_FLASH_CROS)
@@ -206,18 +211,24 @@ test_mockable int system_is_locked(void)
 	 * Unlocked if write protect pin deasserted or read-only firmware
 	 * is not protected.
 	 */
-	if ((EC_FLASH_PROTECT_GPIO_ASSERTED | EC_FLASH_PROTECT_RO_NOW) &
+	if ((EC_FLASH_PROTECT_GPIO_ASSERTED) &
 	    ~crec_flash_get_protect()) {
 		is_locked = 0;
+		ccprints("I am at line %d", __LINE__);
+		ccprints("The value of is_locked: %d", is_locked);
 		return 0;
 	}
 
 	/* If WP pin is asserted and lock is applied, we're locked */
 	is_locked = 1;
+	ccprints("I am at line %d", __LINE__);
+	ccprints("The value of is_locked: %d", is_locked);
 	return 1;
 #else
 	/* Other configs are locked by default */
 	is_locked = 1;
+	ccprints("I am at line %d", __LINE__);
+	ccprints("The value of is_locked: %d", is_locked);
 	return 1;
 #endif
 }
