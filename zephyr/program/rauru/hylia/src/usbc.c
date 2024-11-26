04f21371669b368894a70c51e769ad5adbebd697
Eric Yilun Lin <yllin@chromium.org>
2024-11-25 17:38:20 -0800

    hylia: implement PDC DP alt mode callbacks
    BUG=b:380121396
    TEST=zmake build hylia
    
    Change-Id: Ideb0cd77851e369fc39b7128d1b24333ec678137

--- zephyr/program/rauru/hylia/src/usbc.c
+++ zephyr/program/rauru/hylia/src/usbc.c
@@ -6,6 +6,12 @@
  */
 
 #include "gpio_signal.h"
+#include "hooks.h"
+#include "rauru_dp.h"
+#include "usb_mux.h"
+#include "usb_pd.h"
+#include "usb_pd_dp_hpd_gpio.h"
+#include "usbc/pdc_power_mgmt.h"
 
 #include <stdint.h>
 
@@ -13,17 +19,59 @@
 
 LOG_MODULE_REGISTER(hylia_usbc, LOG_LEVEL_DBG);
 
-void hdmi_hpd_interrupt(enum gpio_signal signal)
-{
-	/* TODO(b:380121396): implement me */
-}
-
 void hylia_dp_attention(int port, uint32_t vdo_dp_status)
 {
-	/* TODO(b:380121396): implement me */
+	int lvl = PD_VDO_DPSTS_HPD_LVL(vdo_dp_status);
+	int irq = PD_VDO_DPSTS_HPD_IRQ(vdo_dp_status);
+	mux_state_t mux_state, mux_mode;
+
+	mux_mode = pdc_power_mgmt_get_dp_mux_mode(port);
+	if (!rauru_is_dp_muxable(port)) {
+		/* TODO(waihong): Info user? */
+		LOG_DBG("p%d: The other port is already muxed.", port);
+		return;
+	}
+
+	if (lvl) {
+		/* connect the DP pipeline before setting HPD if HPG high */
+		rauru_set_dp_path(port);
+		usb_mux_set(port, mux_mode, USB_SWITCH_CONNECT,
+			    polarity_rm_dts(pd_get_polarity(port)));
+	} else {
+		usb_mux_set(port, mux_mode & (~USB_PD_MUX_DP_ENABLED),
+			    USB_SWITCH_CONNECT,
+			    polarity_rm_dts(pd_get_polarity(port)));
+	}
+
+	if (dp_hpd_gpio_set(port, lvl, irq)) {
+		return;
+	}
+
+	if (!lvl) {
+		/* detach DP pipeline after setting HPD */
+		rauru_detach_dp_path(port);
+	}
+
+	/*
+	 * Populate MUX state before dp path mux, so we can keep the HPD status.
+	 */
+	mux_state = (lvl ? USB_PD_MUX_HPD_LVL : USB_PD_MUX_HPD_LVL_DEASSERTED) |
+		    (irq ? USB_PD_MUX_HPD_IRQ : USB_PD_MUX_HPD_IRQ_DEASSERTED);
+	usb_mux_hpd_update(port, mux_state);
+
+	return;
 }
 
 void hylia_set_unattached(int port)
 {
-	/* TODO(b:380121396): implement me */
+	rauru_detach_dp_path(port);
 }
+
+static void hylia_pdc_cb_init(void)
+{
+	pdc_power_mgmt_register_board_callback(PDC_BOARD_CB_UNATTACH,
+					       hylia_set_unattached);
+	pdc_power_mgmt_register_board_callback(PDC_BOARD_CB_DP_ATTENTION,
+					       hylia_dp_attention);
+}
+DECLARE_HOOK(HOOK_INIT, hylia_pdc_cb_init, HOOK_PRIO_LAST);
