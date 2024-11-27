f4ba75d861c5d1592199d99e9db07bc8f3154e8a
elmo_lan <elmo_lan@realtek.corp-partner.google.com>
2024-11-27 15:33:28 +0800

    rts5915: enable minimal-build for rts5915
    realtek ec, rts5915.
    
    TEST: build pass
    Change-Id: Ia2a9e0990c96ae444593341db78c293b2b081717

--- common/system.c
+++ common/system.c
@@ -773,6 +773,11 @@ uint32_t flash_get_rw_offset(enum ec_image copy)
 
 	return CONFIG_EC_PROTECTED_STORAGE_OFF + CONFIG_RO_STORAGE_OFF;
 }
+#else
+uint32_t flash_get_rw_offset(enum ec_image copy)
+{
+	return 0;
+}
 #endif
 
 const struct image_data *system_get_image_data(enum ec_image copy)
@@ -801,6 +806,7 @@ const struct image_data *system_get_image_data(enum ec_image copy)
 	 */
 	addr += flash_get_rw_offset(copy);
 
+#ifdef CONFIG_FLASH
 #ifdef CONFIG_MAPPED_STORAGE
 	addr += CONFIG_MAPPED_STORAGE_BASE;
 	crec_flash_lock_mapped_storage(1);
@@ -810,6 +816,7 @@ const struct image_data *system_get_image_data(enum ec_image copy)
 	/* Read the version struct from flash into a buffer. */
 	if (crec_flash_read(addr, sizeof(data), (char *)&data))
 		return NULL;
+#endif
 #endif
 
 	/* Make sure the version struct cookies match before returning the
