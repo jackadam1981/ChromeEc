ef264e3e999d499dbfcb3e0c5fa914f20026bbc6
Ting Shen <phoenixshen@google.com>
2024-12-03 18:55:28 +0800

    WIP: fix msgid counter increment when discard
    BUG=b:381999399, b:363100407
    TEST=pass TEST.PD.EPR.SNK3.1
    
    Change-Id: I569855a97948d2c8c8b8feaaa7c3edc603c4b184

--- common/usbc/usb_prl_sm.c
+++ common/usbc/usb_prl_sm.c
@@ -987,7 +987,9 @@ static void prl_tx_discard_message_entry(const int port)
 	    prl_tx[port].xmit_status == TCPC_TX_WAIT ||
 	    prl_tx[port].xmit_status == TCPC_TX_COMPLETE_DISCARDED) {
 		PRL_TX_CLR_FLAG(port, PRL_FLAGS_MSG_XMIT);
-		increment_msgid_counter(port);
+		if (prl_tx[port].xmit_status == TCPC_TX_WAIT) {
+			increment_msgid_counter(port);
+		}
 		pe_report_discard(port);
 	}
 
