b944d7a14d99fbfb3bc407df354bbddb3bf0771d
guojun <guojun@huaqin.corp-partner.google.com>
2024-12-04 10:43:34 +0800

    Rull/Roric/Ruke: Modify temp_sensor parameters with 6w and 15w
    Due the system thermal policy and fan table requirements, modify the
    temp_sensor parameters compatible with 6w and 15w thermal protection mechanism.
    
    BUG=b:383032918
    TEST=1.zmake build rull
         2.test verification pass
    LOW_COVERAGE_REASON=Tests will be added in b:383032918
    
    Change-Id: Id0f0e570a5ae5317fdeef54b336064355a87e532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6069426
    Reviewed-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Reviewed-by: Kyle Lin <kylelinck@google.com>
    Tested-by: guojun guojun <guojun@huaqin.corp-partner.google.com>
    Reviewed-by: Jeremy Lin <poseng@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Kyle Lin <kylelinck@google.com>
    (cherry picked from commit 1751d4f8410ab7f65c247f523a02caa3f95e22f1)

--- zephyr/program/nissa/rull/src/board.c
+++ zephyr/program/nissa/rull/src/board.c
@@ -13,6 +13,12 @@
 #include "hooks.h"
 #include "keyboard_backlight.h"
 #include "keyboard_customization.h"
+<<<<<<< HEAD   (eb465e Teltic/Telith:AVDD states are inconsistent with the SPEC of )
+=======
+#include "temp_sensor/temp_sensor.h"
+#include "thermal.h"
+#include "util.h"
+>>>>>>> CHANGE (1751d4 Rull/Roric/Ruke: Modify temp_sensor parameters with 6w and 1)
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
@@ -30,6 +36,127 @@ enum battery_present battery_hw_present(void)
 	return gpio_pin_get_dt(batt_pres) ? BP_NO : BP_YES;
 }
 
+<<<<<<< HEAD   (eb465e Teltic/Telith:AVDD states are inconsistent with the SPEC of )
+=======
+enum battery_present battery_is_present(void)
+{
+	int state;
+
+	if (gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_battery_pres_odl)))
+		return BP_NO;
+
+	/*
+	 *  According to the battery manufacturer's reply:
+	 *  To detect a bad battery, need to read the 0x00 register.
+	 *  If the 12th bit(Permanently Failure) is 1, it means a bad battery.
+	 */
+	if (sb_read(SB_MANUFACTURER_ACCESS, &state))
+		return BP_NO;
+
+	/* Detect the 12th bit value */
+	if (state & BIT(12))
+		return BP_NO;
+
+	return BP_YES;
+}
+
+static const struct battery_info *batt_info;
+int charger_profile_override(struct charge_state_data *curr)
+{
+	batt_info = battery_get_info();
+
+	if (get_time().val < CONFIG_BATTERY_ACTIVATION_TIMEOUT &&
+	    (battery_hw_present() == BP_YES) &&
+	    curr->batt.voltage <= batt_info->voltage_min) {
+		int current = batt_info->precharge_current;
+
+		curr->requested_current = MAX(curr->requested_current, current);
+
+		return -1;
+	}
+
+	return 0;
+}
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+/* thermal tabel control for 15W CPU */
+#define TEMP_VR TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_vr))
+#define TEMP_CPU TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_cpu))
+#define TEMP_AMBIENT TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_ambient))
+#define TEMP_CHARGER TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_charger))
+
+const static struct ec_thermal_config thermal_vr = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(93),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(88),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(51),
+	.temp_fan_max = C_TO_K(67),
+};
+
+const static struct ec_thermal_config thermal_cpu = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(93),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(88),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(50),
+	.temp_fan_max = C_TO_K(70),
+};
+
+const static struct ec_thermal_config thermal_ambient = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(77),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(80),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(75),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(40),
+	.temp_fan_max = C_TO_K(50),
+};
+
+const static struct ec_thermal_config thermal_charger = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(93),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(55),
+	.temp_fan_max = C_TO_K(75),
+};
+
+>>>>>>> CHANGE (1751d4 Rull/Roric/Ruke: Modify temp_sensor parameters with 6w and 1)
 test_export_static void fan_init(void)
 {
 	int ret;
@@ -45,6 +172,12 @@ test_export_static void fan_init(void)
 	if (val != FW_FAN_PRESENT) {
 		/* Disable the fan */
 		fan_set_count(0);
+	} else {
+		thermal_params[TEMP_VR] = thermal_vr;
+		thermal_params[TEMP_CPU] = thermal_cpu;
+		thermal_params[TEMP_AMBIENT] = thermal_ambient;
+		thermal_params[TEMP_CHARGER] = thermal_charger;
+		CPRINTS("Thermal: 15W thermal control");
 	}
 }
 DECLARE_HOOK(HOOK_INIT, fan_init, HOOK_PRIO_POST_FIRST);
