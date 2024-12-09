c301625749e279e9ed7aec77b44f6cdf3d5039c8
Jon Murphy <jpmurphy@google.com>
2024-12-09 14:19:09 -0700

    coreboot-sdk: Add support for libc/libstdc++
    Add Kconfig and associated cmake options to allow coreboot sdk to build
    with libc and libstdc++ support.
    
    BUG=b:380129441
    TEST=zmake build brox-ish-ec
    
    Change-Id: I3d482cc72b0eaf9c4350a68dd81b2ab2cb4b3229

--- zephyr/cmake/toolchain/coreboot-sdk/target.cmake
+++ zephyr/cmake/toolchain/coreboot-sdk/target.cmake
@@ -37,6 +37,30 @@ set(CMAKE_RANLIB     "${TOOLCHAIN_HOME}/${CROSS_COMPILE}ranlib")
 set(CMAKE_READELF    "${TOOLCHAIN_HOME}/${CROSS_COMPILE}readelf")
 set(CMAKE_GCOV       "${TOOLCHAIN_HOME}/${CROSS_COMPILE}gcov")
 
+##########################################################################################################
+# TODO JPM fix the following after this works
+set(CMAKE_C_FLAGS    "${CMAKE_C_FLAGS} -isystem ${COREBOOT_SDK_ROOT}/include")
+set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -isystem ${COREBOOT_SDK_ROOT}/include")
+set(CMAKE_C_FLAGS    "${CMAKE_C_FLAGS} -isystem ${COREBOOT_SDK_ROOT}/include/${CROSS_COMPILE_TARGET}")
+set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -isystem ${COREBOOT_SDK_ROOT}/include/${CROSS_COMPILE_TARGET}")
+set(CMAKE_C_FLAGS    "${CMAKE_C_FLAGS} -isystem ${COREBOOT_SDK_ROOT}/picolibc/include")
+set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -isystem ${COREBOOT_SDK_ROOT}/picolibc/include")
+##########################################################################################################
+
+# Place orphaned sections and disable the warning for them
+set(CONFIG_LINKER_ORPHAN_SECTION_WARN n)
+set(LINKER_ORPHAN_SECTION_PLACE y)
+
+if(CONFIG_PICOLIBC AND NOT CONFIG_PICOLIBC_USE_MODULE)
+  # Add picolibc
+  message(INFO "Setting TOOLCHAIN_HAS_PICOLIBC to support full build.")
+  set(TOOLCHAIN_HAS_PICOLIBC ON CACHE BOOL "True if toolchain supports picolibc")
+
+  # Add newlib
+  message(INFO "Setting TOOLCHAIN_HAS_NEWLIB to support full build.")
+  set(TOOLCHAIN_HAS_NEWLIB ON CACHE BOOL "True if toolchain supports newlib")
+endif()
+
 # On ARM, we don't use libgcc: It's built against a fixed target (e.g.
 # used instruction set, ABI, ISA extensions) and doesn't adapt when
 # compiler flags change any of these assumptions. Use our own mini-libgcc
