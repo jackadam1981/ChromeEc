608441af569c64dd6074b1d48ccdeb01e708cf97
Firas Sammoura <fsammoura@google.com>
2024-12-10 17:21:31 +0000

    gwendolin: add board config to run_device_tests.py
    BUG=b:383329571
    TEST=./test/run_device_tests.py -b gwendolin -f servo_micro \
         --with_private=no
         Test "abort": PASSED
         Test "aes": PASSED
         Test "always_memset": PASSED
         Test "assert_builtin": PASSED
         Test "assert_stdlib": PASSED
         Test "benchmark": PASSED
         Test "boringssl_crypto": PASSED
         Test "cortexm_fpu": PASSED
         Test "crc": PASSED
         Test "exception": PASSED
         Test "exit": PASSED
         Test "flash_physical": PASSED
         Test "flash_write_protect": PASSED
         Test "fp_transport_spi_ro": PASSED
         Test "fp_transport_spi_rw": PASSED
         Test "fp_transport_uart_ro": PASSED
         Test "fp_transport_uart_rw": PASSED
         Test "fpsensor_auth_crypto_stateful": PASSED
         Test "fpsensor_auth_crypto_stateful_otp": PASSED
         Test "fpsensor_auth_crypto_stateless": PASSED
         Test "fpsensor_crypto": PASSED
         Test "fpsensor_debug": PASSED
         Test "fpsensor_hw": FAILED
         Test "fpsensor_utils": PASSED
         Test "ftrapv": PASSED
         Test "libc_printf": PASSED
         Test "global_initialization": PASSED
         Test "libcxx": PASSED
         Test "malloc": PASSED
         Test "mpu_ro": FAILED
         Test "mpu_rw": FAILED
         Test "mutex": PASSED
         Test "mutex_trylock": PASSED
         Test "mutex_recursive": PASSED
         Test "otp_key": PASSED
         Test "panic": PASSED
         Test "panic_data": PASSED
         Test "pingpong": PASSED
         Test "printf": PASSED
         Test "queue": PASSED
         Test "ram_lock": PASSED
         Test "restricted_console": PASSED
         Test "rng_benchmark": PASSED
         Test "rollback_region0": PASSED
         Test "rollback_region1": PASSED
         Test "rollback_entropy": PASSED
         Test "rtc": PASSED
         Test "rtc_npcx9": PASSED
         Test "sbrk": PASSED
         Test "sha256": PASSED
         Test "sha256_unrolled": PASSED
         Test "static_if": PASSED
         Test "stdlib": PASSED
         Test "std_vector": PASSED
         Test "system_is_locked_wp_on": PASSED
         Test "system_is_locked_wp_off": PASSED
         Test "timer": PASSED
         Test "timer_dos": PASSED
         Test "tpm_seed_clear": PASSED
         Test "uart": PASSED
         Test "unaligned_access": PASSED
         Test "unaligned_access_benchmark": PASSED
         Test "utils": PASSED
         Test "utils_str": PASSED
         Test "power_utilization_idle": FAILED
         Test "power_utilization_sleep": FAILED
         Test "unaligned_access_helipilot_v2.0.24337": FAILED
         Test "unaligned_access_buccaneer_v2.0.26328": PASSED
         Test "panic_data_helipilot_v2.0.24337": FAILED
         Test "panic_data_buccaneer_v2.0.26328": PASSED
    
    Change-Id: Ife2c857c837547e583dfba1825ea36a7c9cf8fd2

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -142,6 +142,7 @@ PRINTF_CALLED_REGEX = re.compile(r"printf called(\r){1,2}\n")
 BLOONCHIPPER = "bloonchipper"
 BUCCANEER = "buccaneer"
 DARTMONKEY = "dartmonkey"
+GWENDOLIN = "gwendolin"
 HELIPILOT = "helipilot"
 
 JTRACE = "jtrace"
@@ -202,6 +203,7 @@ class FPSensorType(Enum):
 
     ELAN = 0
     FPC = 1
+    EGIS = 1
     UNKNOWN = -1
 
 
@@ -454,7 +456,7 @@ class Renode(Platform):
                 "timer_dos",  # TODO(b/374798079)
             ]:
                 return True
-        elif board_config.name in [HELIPILOT, BUCCANEER]:
+        elif board_config.name in [HELIPILOT, BUCCANEER, GWENDOLIN]:
             if test_name in [
                 "exception",  # TODO(b/384730599)
                 "libcxx",
@@ -723,7 +725,7 @@ class AllTests:
             # Covered by Zephyr drivers.counter.basic_api.stm32_subsec test
             TestConfig(
                 test_name="rtc_stm32f4",
-                exclude_boards=[DARTMONKEY, HELIPILOT, BUCCANEER],
+                exclude_boards=[DARTMONKEY, HELIPILOT, BUCCANEER, GWENDOLIN],
                 skip_for_zephyr=True,
             ),
             TestConfig(test_name="sbrk", imagetype_to_use=ImageType.RO),
@@ -1002,10 +1004,15 @@ BUCCANEER_CONFIG.expected_fp_power = PowerUtilization(
     idle=RangedValue(0.25, 0.3), sleep=RangedValue(0.25, 0.3)
 )
 
+GWENDOLIN_CONFIG = copy.deepcopy(HELIPILOT_CONFIG)
+GWENDOLIN_CONFIG.name = GWENDOLIN
+GWENDOLIN_CONFIG.sensor_type = FPSensorType.EGIS
+
 BOARD_CONFIGS = {
     "bloonchipper": BLOONCHIPPER_CONFIG,
     "buccaneer": BUCCANEER_CONFIG,
     "dartmonkey": DARTMONKEY_CONFIG,
+    "gwendolin": GWENDOLIN_CONFIG,
     "helipilot": HELIPILOT_CONFIG,
 }
 
