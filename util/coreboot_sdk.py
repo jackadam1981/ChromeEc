aafb7edc89c6249ebbe552e3b94a181333eea2be
Jonathon Murphy <jpmurphy@google.com>
2024-12-13 14:10:04 -0800

    ec: Update coreboot_sdk.py to use workspace path
    Update coreboot_sdk.py to navigate to the workspace path before
    executing commands.
    
    BUG=b:384082998
    TEST=CQ
    
    Change-Id: If776ef13f1f56e486d0e8b4bd38f09bf9ef508d0

--- util/coreboot_sdk.py
+++ util/coreboot_sdk.py
@@ -15,66 +15,69 @@ import subprocess
 
 
 def init_toolchain():
-    """Initialize coreboot-sdk.
+  """Initialize coreboot-sdk.
 
-    Returns:
-        Environment variables to use for toolchain.
-    """
-    if os.environ.get("COREBOOT_SDK_ROOT") is not None:
-        print("COREBOOT_SDK_ROOT already set by environment, returning")
-        return {}
+  Returns:
+      Environment variables to use for toolchain.
+  """
+  if os.environ.get("COREBOOT_SDK_ROOT") is not None:
+    print("COREBOOT_SDK_ROOT already set by environment, returning")
+    return {}
 
-    # (environment variable, bazel target)
-    toolchains = [
-        ("COREBOOT_SDK_ROOT_arm", "@ec-coreboot-sdk-arm-eabi//:get_path"),
-        ("COREBOOT_SDK_ROOT_x86", "@ec-coreboot-sdk-i386-elf//:get_path"),
-        ("COREBOOT_SDK_ROOT_riscv", "@ec-coreboot-sdk-riscv-elf//:get_path"),
-        ("COREBOOT_SDK_ROOT_nds32", "@ec-coreboot-sdk-nds32le-elf//:get_path"),
-    ]
-    try:
-        subprocess.run(
-            [
-                "bazel",
-                "--project",
-                "fwsdk",
-            ],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.STDOUT,
-            check=True,
-        )
-    except (subprocess.CalledProcessError, FileNotFoundError):
-        print(
-            "bazel doesn't exist or is not the right version to download packages for coreboot-sdk"
-        )
-        return {}
+  # (environment variable, bazel target)
+  toolchains = [
+      ("COREBOOT_SDK_ROOT_arm", "@ec-coreboot-sdk-arm-eabi//:get_path"),
+      ("COREBOOT_SDK_ROOT_x86", "@ec-coreboot-sdk-i386-elf//:get_path"),
+      ("COREBOOT_SDK_ROOT_riscv", "@ec-coreboot-sdk-riscv-elf//:get_path"),
+      ("COREBOOT_SDK_ROOT_nds32", "@ec-coreboot-sdk-nds32le-elf//:get_path"),
+  ]
+  try:
     subprocess.run(
         [
             "bazel",
             "--project",
             "fwsdk",
-            "build",
-            *(target for _, target in toolchains),
         ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
         check=True,
+        cwd="~/chromiumos/src",
     )
+  except (subprocess.CalledProcessError, FileNotFoundError):
+    print(
+        "bazel doesn't exist or is not the right version to download packages"
+        " for coreboot-sdk"
+    )
+    return {}
+  subprocess.run(
+      [
+          "bazel",
+          "--project",
+          "fwsdk",
+          "build",
+          *(target for _, target in toolchains),
+      ],
+      check=True,
+      cwd="~/chromiumos/src",
+  )
 
-    result = {}
-    for name, target in toolchains:
-        run_result = subprocess.run(
-            ["bazel", "--project", "fwsdk", "run", target],
-            check=True,
-            stdout=subprocess.PIPE,
-        )
-        result[name] = run_result.stdout.strip()
-    return result
+  result = {}
+  for name, target in toolchains:
+    run_result = subprocess.run(
+        ["bazel", "--project", "fwsdk", "run", target],
+        check=True,
+        stdout=subprocess.PIPE,
+        cwd="~/chromiumos/src",
+    )
+    result[name] = run_result.stdout.strip()
+  return result
 
 
 if __name__ == "__main__":
-    env_vars = init_toolchain()
-    # Return a formatted string which can be declared as an associative array in bash
-    print(
-        " ".join(
-            f"[{key}]={value.decode('utf-8')}"
-            for key, value in env_vars.items()
-        )
-    )
+  env_vars = init_toolchain()
+  # Return a formatted string which can be declared as an associative array in bash
+  print(
+      " ".join(
+          f"[{key}]={value.decode('utf-8')}" for key, value in env_vars.items()
+      )
+  )
