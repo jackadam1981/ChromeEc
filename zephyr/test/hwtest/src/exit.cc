f17758a3d06f5bee325140c1b54ebdea107019f3
Firas Sammoura <fsammoura@google.com>
2024-12-17 00:31:54 +0000

    zephyr: tests: add exit hardware test
    Add exit test. The test verify if exit() call works correctly and
    validates panic data
    
    BUG=b:384102168
    TEST=connect Bloonchipper via servo_micro and run
         './test/run_device_tests.py --board=bloonchipper
         --flasher=servo_micro --tests exit --zephyr'
    
    Change-Id: Ibaa3a62521eb881edf2e917f35288793a1f6c3ab

---
+++ zephyr/test/hwtest/src/exit.cc
@@ -0,0 +1,41 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "multistep_test.h"
+#include "panic.h"
+#include "system.h"
+
+#include <stdlib.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(exit_hw_test, LOG_LEVEL_INF);
+
+static void test_exit(void)
+{
+	LOG_INF("Calling exit\n");
+	cflush();
+	exit(1);
+	/* Should never reach this. */
+	zassert_unreachable();
+}
+
+static void test_panic_data(void)
+{
+	struct panic_data *const pdata = panic_get_data();
+	uint32_t exit_addr = (uint32_t)exit;
+	/* Estimated end of the exit function, which is short. */
+	uint32_t exit_end = (uint32_t)exit + 0x40;
+	uint32_t pc = pdata->cm.frame[CORTEX_PANIC_FRAME_REGISTER_PC];
+
+	/* Make sure Program Counter is stored correctly and points at the exit
+	 * function.
+	 */
+	zassert_true((exit_addr <= pc) && (exit_end >= pc));
+}
+
+static void (*test_steps[])(void) = { test_exit, test_panic_data };
+
+MULTISTEP_TEST(exit, test_steps)
