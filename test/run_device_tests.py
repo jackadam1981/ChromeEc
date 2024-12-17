a4bdf77f1e0603ed07bc26ee5d602cbdee64c8f4
Tom Hughes <tomhughes@chromium.org>
2024-12-17 09:28:56 -0800

    test/run_device_tests: Re-enable exception test on bloonchipper/dartmonkey
    This test also seems to have been affected by b/335699831, which has now
    been fixed.
    
    BUG=b:265034857, b:335699831
    TEST=./test/run_device_tests.py -b bloonchipper --renode -t exception
    TEST=./test/run_device_tests.py -b dartmonkey --renode -t exception
    
    Change-Id: I2d1ce7bec54885139d0db873f2b366759c6eb80a

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -444,7 +444,6 @@ class Renode(Platform):
                     return True
             # TODO(b/356476313): Remove these when Renode is fixed.
             if test_name in [
-                "exception",
                 "rtc_stm32f4",
                 "timer_dos",  # TODO(b/374798079)
             ]:
