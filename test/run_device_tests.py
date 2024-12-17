bef06e47b66e27b4ec91f7fba15fd3d3dee062a2
Tom Hughes <tomhughes@chromium.org>
2024-12-17 10:02:25 -0800

    test/run_device_tests: Add bug for exception test failure on helipilot
    BUG=b:384730599
    TEST=none
    
    Change-Id: Ie6b2a517383e7a5124661c79baee9a78a36133fc

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -440,6 +440,7 @@ class Renode(Platform):
             if test_name in [
                 "production_app_test",
                 "benchmark",
+                "exception",  # TODO(b/384730599)
                 "fpsensor_hw",
                 "libcxx",
                 "otp_key",
