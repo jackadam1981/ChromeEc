e71eafc70c7c8eba67eb190ae43d8d19ce7470d4
Tom Hughes <tomhughes@chromium.org>
2024-12-17 09:28:56 -0800

    test/run_device_tests: Re-enable exception test on bloonchipper/dartmonkey EC
    This test also seems to have been affected by b/335699831, which has now
    been fixed.
    
    The Zephyr version of the test fails due to a different issue, so it
    remains disabled.
    
    BUG=b:265034857, b:335699831
    TEST=./test/run_device_tests.py -b bloonchipper --renode -t exception
    TEST=./test/run_device_tests.py -b dartmonkey --renode -t exception
    TEST=./test/run_device_tests.py -b bloonchipper --renode --zephyr \
         -t exception => SKIPPED
    
    Change-Id: I2d1ce7bec54885139d0db873f2b366759c6eb80a

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -441,6 +441,7 @@ class Renode(Platform):
             if board_config.name == BLOONCHIPPER:
                 if zephyr and test_name in [
                     "abort",  # TODO(b/384094781)
+                    "exception",  # TODO(b/388327673)
                     # TODO(b/382705460): We have seen this flake in the CQ.
                     # Re-enable when missing character bug is fixed.
                     "flash_physical",
@@ -455,7 +456,6 @@ class Renode(Platform):
                 ]:
                     return True
             if test_name in [
-                "exception",
                 "rtc_stm32f4",  # TODO(b/384991107)
             ]:
                 return True
