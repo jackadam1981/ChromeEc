d6425a6cf3a068fcfd0a6998489ea9558874940b
Jes B. Klinke <jbk@chromium.org>
2024-12-17 11:30:00 -0800

    chip/stm32: Consolidate clock enums
    Several families of STM chips independently declare `enum clock_osc` and
    `clock_set_osc()`.  This patch hoists both into `clock_chip.h` allowing
    board code to trigger switching of clock source (rather than such
    switchs only being possible by a console command).
    
    BUG=b:384755415
    TEST=make buildall_only
    
    Change-Id: I6be9b53a8ffd179b0fb8b22cc1a13d51ca485511

--- chip/stm32/clock_chip.h
+++ chip/stm32/clock_chip.h
@@ -99,4 +99,16 @@ void restore_host_wake_alarm(void);
 void low_power_init(void);
 #endif
 
+enum clock_osc {
+	OSC_INIT = 0, /* Uninitialized */
+	OSC_HSI, /* High-speed internal oscillator */
+	OSC_MSI, /* Multi-speed internal oscillator */
+#ifdef CONFIG_STM32_CLOCK_HSE_HZ /* Catch absence of HSE at compile time */
+	OSC_HSE, /* High-speed external oscillator */
+#endif
+	OSC_PLL, /* PLL */
+};
+
+void clock_set_osc(enum clock_osc osc, enum clock_osc pll_osc);
+
 #endif /* __CROS_EC_CLOCK_L4_H */
