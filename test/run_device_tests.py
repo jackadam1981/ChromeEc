ab1a560eed5dce10ecd2144a4a07938daebeacfd
Tom Hughes <tomhughes@chromium.org>
2024-12-17 11:00:18 -0800

    test/run_device_tests: Simplify skipping power_utilization test on Renode
    We can't measure power on Renode. Move the skipping of the test to the
    board independent section, so we don't have to specify it for every
    board.
    
    BUG=none
    TEST=CQ
    
    Change-Id: I1a41ec626afbfd32e9f79b41bdc0d4381df83ad0

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -414,6 +414,7 @@ class Renode(Platform):
 
         # Tests failures that are independent of the board.
         if test_name in [
+            "power_utilization",  # Can't measure power on Renode.
             "production_app_test",  # TODO(b/384740370)
         ]:
             return True
@@ -439,7 +440,6 @@ class Renode(Platform):
                 "exception",
                 "fpsensor_hw",
                 "libcxx",
-                "power_utilization",
                 "rtc_stm32f4",
                 "timer_dos",  # TODO(b/374798079)
             ]:
@@ -451,7 +451,6 @@ class Renode(Platform):
                 "fpsensor_hw",
                 "libcxx",
                 "otp_key",
-                "power_utilization",
                 "ram_lock",
                 "rtc_npcx9",
             ]:
