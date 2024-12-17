ea144bf5008dab18fa7d6dfec3b016312acf5f23
Al Semjonovs <asemjonovs@google.com>
2024-12-17 11:11:48 -0700

    pdc_power_mgmt: Don't request PDOs upon new power request
    pdc_power_mgmt_set_new_power_request should only evaluat and send new RDO
    based on current PDOs.
    
    BUG=b:382277419
    TEST=./twister -c -T zephyr/test/pdc
    
    Change-Id: I3df58d6bba78fa3c336a2c80e61a21f0c21d86f3

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -1499,12 +1499,7 @@ static void run_snk_policies(struct pdc_port_t *port)
 		return;
 	} else if (atomic_test_and_clear_bit(port->snk_policy.flags,
 					     SNK_POLICY_NEW_POWER_REQUEST)) {
-		/* TODO: b/382277419
-		 * this policy flag should construct a new RDO request
-		 * based on the current maximum voltage and maximum current.
-		 */
-		port->get_pdo = (struct get_pdo_t){ 0 };
-		port->snk_attached_local_state = SNK_ATTACHED_GET_PDOS;
+		port->snk_attached_local_state = SNK_ATTACHED_EVALUATE_PDOS;
 		return;
 	} else if (atomic_test_and_clear_bit(port->snk_policy.flags,
 					     SNK_POLICY_EVAL_SWAP_TO_SRC)) {
@@ -2129,6 +2124,12 @@ static void pdc_snk_attached_run(void *obj)
 		if (rv) {
 			LOG_ERR("C%d: No suitable PDO found (%d)",
 				config->connector_num, rv);
+		}
+
+		if (port->snk_policy.pdo ==
+		    port->snk_policy.src.pdos[selected_pdo]) {
+			/* Selected PDO didn't change - no need to send RDO */
+			return;
 		}
 
 		/* Store the selected PDO. Convert the PDO number to 1-based
@@ -3339,9 +3340,6 @@ int pdc_power_mgmt_set_new_power_request(int port)
 		return -ENOTCONN;
 	}
 
-	/* TODO: b/382277419
-	 * NEW_POWER_REQUEST should build up a new RDO request only.
-	 */
 	atomic_set_bit(pdc_data[port]->port.snk_policy.flags,
 		       SNK_POLICY_NEW_POWER_REQUEST);
 
