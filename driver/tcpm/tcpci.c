bc096b599feb406229b1c4f95bb1c555b9fd47cb
Abe Levkoy <alevkoy@chromium.org>
2024-12-18 11:26:35 -0700

    DNS: Instrument FRS Error Recovery path
    Record timestamps during FRS Error Recovery path and print them after
    detaching. Understand sources of latency in this path.
    
    BUG=b:361800172
    TEST=twister -s zephyr/test/drivers/drivers.default
    
    Change-Id: Ia60d14c764215c0d07774951ca7fed4ae7a0a169

--- driver/tcpm/tcpci.c
+++ driver/tcpm/tcpci.c
@@ -1208,6 +1208,10 @@ void tcpci_tcpc_alert(int port)
 	bool bist_mode;
 	timestamp_t alert_ts = get_time();
 
+	if (IS_ENABLED(CONFIG_USB_PD_TCPMV2))
+		pd_record_timestamp(port, PD_INTERVAL_INT_TO_INT_TASK, PD_END,
+				    alert_ts);
+
 	/* Read the Alert register from the TCPC */
 	if (tcpm_alert_status(port, &alert)) {
 		CPRINTS("C%d: Failed to read alert register", port);
