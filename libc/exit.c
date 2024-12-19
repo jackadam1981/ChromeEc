65960094ebc332698aabce71ee9b16f5b62fb7a6
Firas Sammoura <fsammoura@google.com>
2024-12-19 15:30:22 -0800

    libc: move exit to a separate file
    Move the CrosEC version of exit function to a separate file.
    
    It should not change anything in the build
    
    Change-Id: I9d45b9a9ca99953e756f0f0257151f51240a7116

---
+++ libc/exit.c
@@ -0,0 +1,20 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "panic.h"
+#include "task.h"
+
+/**
+ * Reboot the system.
+ *
+ * This function is called from libc functions such as abort() or exit().
+ *
+ * @param rc exit code
+ */
+void _exit(int rc)
+{
+	panic_printf("%s called with rc: %d\n", __func__, rc);
+	software_panic(PANIC_SW_EXIT, task_get_current());
+}
