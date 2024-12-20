a88743aefc2c34c27d7d1f8e40f953a71078981c
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2024-12-20 13:42:38 +0800

    hylia: enable PDC
    1. Change tcpc to PDC
    2. Change DP_alt_mode to adapt dual monitors.
    
    BUG=b:375051035
    TEST=build zmake hylia
    LOW_COVERAGE_REASON=Tests will be added in b:375051035
    
    Change-Id: Ie7ccad559629bd217c8a9cb28475a8180756a4d2

--- zephyr/program/rauru/src/hooks.c
+++ zephyr/program/rauru/src/hooks.c
@@ -37,6 +37,7 @@ static void rauru_common_init(void)
 DECLARE_HOOK(HOOK_INIT, rauru_common_init, HOOK_PRIO_PRE_DEFAULT);
 
 /* USB-A */
+#ifdef CONFIG_RAURU_BOARD_HAS_HDMI_SUPPORT
 void xhci_interrupt(enum gpio_signal signal)
 {
 	const int xhci_stat = gpio_get_level(signal);
@@ -68,6 +69,7 @@ void xhci_interrupt(enum gpio_signal signal)
 		}
 	}
 }
+#endif
 
 __override enum pd_dual_role_states pd_get_drp_state_in_s0(void)
 {
