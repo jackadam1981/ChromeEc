c3289f1cff411ed873f17ae3f548dc766bbbbf41
Tom Hughes <tomhughes@chromium.org>
2024-12-19 16:30:33 -0800

    test/run_device_tests: Add bug number for otp_key test failure on Renode
    BUG=b:385216796
    TEST=CQ
    
    Change-Id: Idef9d98ba59ed323c1e4655ba75896febc0ea0b2

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -457,7 +457,7 @@ class Renode(Platform):
             if test_name in [
                 "exception",  # TODO(b/384730599)
                 "libcxx",
-                "otp_key",
+                "otp_key",  # TODO(b/385216796)
                 "ram_lock",
                 "rtc_npcx9",
             ]:
