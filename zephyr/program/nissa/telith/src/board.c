3d113a4b24bdfd8eab529231c26a7191fcc2b51a
mike <mike5@huaqin.corp-partner.google.com>
2024-12-23 18:48:06 +0800

    Telith: Fix EC sleep mode status
    When burning EC or sysjump RO/RW under the system, EC will lose
    the SLEEP_MASK_AP_RUN state, causing EC to enter low power mode.
    Since the keyboard backlight PWM is set to 24KHZ, EC enters low
    power mode and cannot normally handle PWM frequencies greater
    than 324hz, causing keyboard backlight to not work properly.
    
    BUG=b:383638235
    TEST=keyboard backlight work normally.
    
    Change-Id: I8a0480771822bd75dc5036b29b9a4c7f2c78d6e0

--- zephyr/program/nissa/telith/src/board.c
+++ zephyr/program/nissa/telith/src/board.c
@@ -27,6 +27,8 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
+#include <ap_power/ap_power_interface.h>
+
 LOG_MODULE_REGISTER(board_init, LOG_LEVEL_ERR);
 
 enum base_sensor_type {
@@ -211,5 +213,9 @@ int8_t board_vivaldi_keybd_idx(void)
 static void board_init(void)
 {
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_s5), 1);
+
+	if (ap_power_in_state(AP_POWER_STATE_ON)) {
+		disable_sleep(SLEEP_MASK_AP_RUN);
+	}
 }
 DECLARE_HOOK(HOOK_INIT, board_init, HOOK_PRIO_DEFAULT);
