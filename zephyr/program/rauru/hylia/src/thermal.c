64baab4ec5cb870be95f7c6c11200084258d9c49
Jiahong Wei <weijiahong@huaqin.corp-partner.google.com>
2024-12-30 14:30:18 +0800

    test-Hylia: Implement current-limiting policy
    According to thermal requirements, add current-limiting
    policy. For specific requirements, please see the feature
    request.
    
    BUG=nont
    TEST=Thermal verification passed
    
    Change-Id: Ifdb5724b3427065f2abcee39de3603b0c11bc300

---
+++ zephyr/program/rauru/hylia/src/thermal.c
@@ -0,0 +1,211 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "battery.h"
+#include "charge_state.h"
+#include "charger.h"
+#include "chipset.h"
+#include "console.h"
+#include "extpower.h"
+#include "gpio.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "power.h"
+#include "temp_sensor/temp_sensor.h"
+#include "util.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
+#define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ##args)
+#define CONFIG_BATTERY_ACTIVATION_TIMEOUT (10 * SECOND)
+// #define COL_NUM 2
+// #define ROW_NUM 2
+
+// static int time[ROW_NUM][COL_NUM];
+
+// static int thermal_cyc;
+static int current = -1;
+static int increment_level, decline_level = 0;
+static const struct battery_info *batt_info;
+enum power_state chipset_state;
+// static int charger_temp_ave_bef, charger_temp_ave;
+
+enum {
+	TEMP_ZONE_0, /* not limit */
+	TEMP_ZONE_1, /* 3500mA */
+} temp_zone = TEMP_ZONE_0;
+
+/*
+ * Except time[exceptrow][exceptcol], everything else is cleared to 0
+ * Used to record the number of times the temperature reaches a certain
+ * level three times in a row.
+ */
+// static void clear_remaining_array(int arr[][COL_NUM], int row, int exceptrow,
+// 				  int exceptcol)
+// {
+// 	int i, j;
+
+// 	time[exceptrow][exceptcol]++;
+
+// 	for (i = 0; i < row; i++) {
+// 		if (i != exceptrow) {
+// 			for (j = 0; j < COL_NUM; j++) {
+// 				if (j != exceptcol) {
+// 					arr[i][j] = 0;
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+
+/* Called by hook task every hook second (1 sec) */
+static void average_tempature(void)
+{
+	int cpu_temp, cpu_temp_c;
+	int charger_temp, charger_temp_c;
+	// int charger_temp_sum = 0;
+
+	/*
+	 * Keep track of battery temperature range:
+	 *
+	 *     ZONE_0  ZONE_1
+	 * --->------>-------- Temperature (C)
+	 *    0      75
+	 *     ZONE_0  ZONE_1
+	 * ---<------<------- Temperature (C)
+	 *    0      70
+	 */
+	temp_sensor_read(
+		TEMP_SENSOR_ID_BY_DEV(DT_NODELABEL(temp_sensor_2_thermistor)),
+		&cpu_temp);
+
+	cpu_temp_c = K_TO_C(cpu_temp);
+
+	temp_sensor_read(
+		TEMP_SENSOR_ID_BY_DEV(DT_NODELABEL(temp_sensor_3_thermistor)),
+		&charger_temp);
+
+	charger_temp_c = K_TO_C(charger_temp);
+
+	// thermals[thermal_cyc] = charger_temp_c;
+	// thermal_cyc = (thermal_cyc + 1) % 5;
+	// for (int i = 0; i < 5; i++)
+	// 	charger_temp_sum += thermals[i];
+
+	// charger_temp_ave_bef = charger_temp_ave;
+	// charger_temp_ave = (charger_temp_sum + 2.5) / 5;
+
+	// if ((charger_temp_ave - charger_temp_ave_bef) > 0) {
+	// 	temperature_increase = 1;
+	// } else if ((charger_temp_ave - charger_temp_ave_bef) < 0) {
+	// 	temperature_increase = 0;
+	// }
+	CPRINTS("---cpu : %d,charge : %d----", cpu_temp_c, charger_temp_c);
+
+	if (cpu_temp_c >= 55) {
+		if (charger_temp_c >= 75 && temp_zone != TEMP_ZONE_1) {
+			// clear_remaining_array(time, ROW_NUM, 0, 1);
+			increment_level++;
+			decline_level = 0;
+		}
+		else if (charger_temp_c < 70 && temp_zone != TEMP_ZONE_0) {
+			// clear_remaining_array(time, ROW_NUM, 1, 0);
+			increment_level = 0;
+			decline_level++;
+		}
+		else {
+			increment_level = 0;
+			decline_level = 0;
+		}
+	}
+	else {
+		increment_level = 0;
+		decline_level++;
+	}
+
+	// for (int i = 0; i < COL_NUM; i++) {
+	// 	if (time[0][i] == 3) {
+	// 		temp_zone = i;
+	// 		time[0][i] = 0;
+	// 	} else if (time[1][i] == 3) {
+	// 		temp_zone = i;
+	// 		time[1][i] = 0;
+	// 	}
+	// }
+	CPRINTS("---increment : %d,decline : %d----", increment_level, decline_level);
+	if (increment_level == 3) {
+		temp_zone = 1;
+		increment_level = 0;
+	}
+	else if (decline_level == 1) {
+		temp_zone = 0;
+		decline_level = 0;
+	}
+
+	switch (temp_zone) {
+		case TEMP_ZONE_0:
+			/* No current limit */
+			current = -1;
+			break;
+		case TEMP_ZONE_1:
+			current = 3500;
+			break;
+	}
+}
+DECLARE_HOOK(HOOK_SECOND, average_tempature, HOOK_PRIO_DEFAULT);
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	batt_info = battery_get_info();
+
+	if (get_time().val < CONFIG_BATTERY_ACTIVATION_TIMEOUT &&
+	    !gpio_get_level(GPIO_BATT_PRES_ODL) &&
+	    curr->batt.voltage <= batt_info->voltage_min) {
+		int current = 256;
+
+		curr->requested_current = MAX(curr->requested_current, current);
+
+		return -1;
+	}
+
+	/*
+	 * Precharge must be executed when communication is failed on
+	 * dead battery.
+	 */
+	if (!(curr->batt.flags & BATT_FLAG_RESPONSIVE))
+		return 0;
+
+	/* Don't charge if outside of allowable temperature range */
+	if (current == 0) {
+		curr->batt.flags &= ~BATT_FLAG_WANT_CHARGE;
+		if (curr->state != ST_DISCHARGE)
+			curr->state = ST_IDLE;
+	}
+
+	/* This policy only execute in state s0*/
+	chipset_state = power_get_state();
+
+	if (chipset_state != POWER_S0)
+		return 0;
+	CPRINTS("---current : %d----", current);
+	if (current >= 0)
+		curr->requested_current = MIN(curr->requested_current, current);
+
+	return 0;
+}
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
