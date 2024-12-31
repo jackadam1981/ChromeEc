f5f429967fb893f7dd2e7826701b16f9ae7cffe9
Yuval Peress <peress@google.com>
2024-12-30 22:58:20 -0700

    dps_comms: Block CBI read until device is ready
    If we get a request to read CBI values before the DSP comms client was
    initialized, block the requesting thread until the device is ready.
    
    BUG=b:345530593
    TEST=Flash Orisa and test tablet/clamshell mode
    
    Change-Id: I35bc15ec3224bea48460a1bd81ccfca33059e6ac

--- zephyr/drivers/dsp_comms/client/dsp_client.c
+++ zephyr/drivers/dsp_comms/client/dsp_client.c
@@ -181,6 +181,9 @@ int dsp_client_get_cbi_flags(const struct device* dev,
   };
   int rc;
 
+  while (!device_is_ready(dev)) {
+    k_sleep(K_USEC(1));
+  }
   k_mutex_lock(&data->mutex, K_FOREVER);
   pb_ostream_t stream = pb_ostream_from_buffer(data->request_buffer,
                                                cros_dsp_comms_EcService_size);
