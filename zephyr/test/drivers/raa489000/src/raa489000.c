105488c453232eee60278843c8458d07a3c9827e
Wen Zhang <zhangwen6@huaqin.corp-partner.google.com>
2025-01-03 19:02:31 +0800

    [TEST-ONLY]Add ztest code for raa489000
    Change-Id: Iff7eab4084bdc77f0fa3bda9a80f138009117549

--- zephyr/test/drivers/raa489000/src/raa489000.c
+++ zephyr/test/drivers/raa489000/src/raa489000.c
@@ -7,6 +7,7 @@
 #include "driver/tcpm/tcpci.h"
 #include "emul/tcpc/emul_raa489000.h"
 #include "emul/tcpc/emul_tcpci.h"
+#include "test/drivers/tcpci_test_common.h"
 #include "test/drivers/test_state.h"
 #include "usb_pd_tcpm.h"
 
@@ -15,6 +16,9 @@
 #include <zephyr/ztest.h>
 
 #define RAA489000_PORT 0
+#define RAA489000_EMUL_NODE DT_NODELABEL(raa489000_emul)
+
+FAKE_VALUE_FUNC(bool, pd_vbus_valid_for_bist, int);
 
 ZTEST(tcpc_raa489000, test_check_vendor)
 {
@@ -26,42 +30,80 @@ ZTEST(tcpc_raa489000, test_check_vendor)
 	tcpm_dump_registers(RAA489000_PORT);
 }
 
-FAKE_VALUE_FUNC(int, pd_vbus_valid_for_bist, int);
-bool raa489000_tcpm_should_enter_bist_mode(int port, uint32_t *payload,
-					   int *head);
-
-bool mock_pd_vbus_valid_for_bist_pass(int port)
+void test_tcpci_get_rx_message_raw(const struct emul *emul,
+				   struct i2c_common_emul_data *common_data,
+				   enum usbc_port port)
 {
-	return true;
-}
-
-ZTEST(tcpc_raa489000, test_raa489000_tcpm_should_enter_bist_mode)
-{
+	const struct tcpm_drv *drv = tcpc_config[port].drv;
+	struct tcpci_emul_msg msg;
 	uint32_t payload[1];
-	int hdr;
+	/* bist mode message header and payload */
+	uint8_t buf[32] = {0x3, 0x30, 0x0, 0x0, 0x0, 0x80};
+	int head;
+	int size;
+	bool bist_enabled;
 
-	hdr = 0x3003;
-	payload[0] = 0x80000000;
-	pd_vbus_valid_for_bist_fake.custom_fake =
-		mock_pd_vbus_valid_for_bist_pass;
+	RESET_FAKE(pd_vbus_valid_for_bist);
+	pd_vbus_valid_for_bist_fake.return_val = 1;
 
-	bool result = raa489000_tcpm_should_enter_bist_mode(RAA489000_PORT,
-							    payload, &hdr);
-	zassert_true(result, "BIST mode should be enabled");
+	tcpci_emul_set_reg(emul, TCPC_REG_RX_DETECT,
+			   TCPC_REG_RX_DETECT_SOP | TCPC_REG_RX_DETECT_SOPP);
+
+	msg.buf = buf;
+	msg.cnt = 31;
+	msg.sop_type = TCPCI_MSG_SOP;
+	zassert_equal(TCPCI_EMUL_TX_SUCCESS,
+		      tcpci_emul_add_rx_msg(emul, &msg, true),
+		      "Failed to setup emulator message");
+
+	/* Test fail on reading message */
+	i2c_common_emul_set_read_fail_reg(common_data, TCPC_REG_RX_BUFFER);
+	zassert_equal(EC_ERROR_UNKNOWN,
+		      drv->get_message_raw(port, payload, &head), NULL);
+	i2c_common_emul_set_read_fail_reg(common_data,
+					  I2C_COMMON_EMUL_NO_FAIL_REG);
+
+	/* Test bist mode true */
+	size = 28;
+	msg.cnt = size + 2;
+	msg.sop_type = TCPCI_MSG_SOP_PRIME;
+	zassert_equal(TCPCI_EMUL_TX_SUCCESS,
+		      tcpci_emul_add_rx_msg(emul, &msg, true),
+		      "Failed to setup emulator message");
+
+	zassert_equal(EC_SUCCESS, drv->set_bist_test_mode(port, false));
+	zassert_equal(EC_SUCCESS, drv->get_message_raw(port, payload, &head),
+		      NULL);
+	zassert_equal(EC_SUCCESS, drv->get_bist_test_mode(port, &bist_enabled),
+		      NULL);
+	zassert_true(bist_enabled, "BIST mode should be enabled");
+	
+
+	/* Test bist mode already true */
+	zassert_equal(TCPCI_EMUL_TX_SUCCESS,
+		      tcpci_emul_add_rx_msg(emul, &msg, true),
+		      "Failed to setup emulator message");
+
+	zassert_equal(EC_SUCCESS, drv->set_bist_test_mode(port, true));
+	zassert_equal(EC_SUCCESS, drv->get_message_raw(port, payload, &head),
+		      NULL);
+	zassert_equal(EC_SUCCESS, drv->get_bist_test_mode(port, &bist_enabled),
+		      NULL);
+	zassert_true(bist_enabled, "BIST mode should be disabled");
 }
 
-ZTEST(tcpc_raa489000, test_raa489000_tcpm_should_not_enter_bist_mode)
+static void test_raa489000_tcpci_get_rx_message_raw(void)
 {
-	uint32_t payload[1] = { 0 };
-	int head = 0;
+	const struct emul *raa489000_emul = EMUL_DT_GET(RAA489000_EMUL_NODE);
+	struct i2c_common_emul_data *common_data =
+		emul_tcpci_generic_get_i2c_common_data(raa489000_emul);
 
-	bool result = raa489000_tcpm_should_enter_bist_mode(RAA489000_PORT,
-							    payload, &head);
-	zassert_false(result, "BIST mode should not be enabled");
+	test_tcpci_get_rx_message_raw(raa489000_emul, common_data, RAA489000_PORT);
+}
 
-	bool enable = true;
-	zassert_equal(tcpc_set_bist_test_mode(RAA489000_PORT, enable),
-		      EC_SUCCESS);
+ZTEST(tcpc_raa489000, test_tcpci_get_rx_message_raw)
+{
+	test_raa489000_tcpci_get_rx_message_raw();
 }
 
 ZTEST_SUITE(tcpc_raa489000, drivers_predicate_post_main, NULL, NULL, NULL,
