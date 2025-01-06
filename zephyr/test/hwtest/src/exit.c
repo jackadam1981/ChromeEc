bb3782bfd801ca1cf51a03ea29deb6a5fea5f204
Firas Sammoura <fsammoura@google.com>
2025-01-06 21:35:23 +0000

    zephyr: tests: add exit hardware test
    Add exit test. The test verify if exit() call works correctly and
    validates panic data.
    
    BUG=b:384102168
    TEST=connect Bloonchipper via servo_micro and run
         './test/run_device_tests.py --board=bloonchipper
         --flasher=servo_micro --tests exit --zephyr'
    
    Change-Id: Ibaa3a62521eb881edf2e917f35288793a1f6c3ab

---
+++ zephyr/test/hwtest/src/exit.c
@@ -0,0 +1,77 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "system.h"
+
+#include <stdlib.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/ztest.h>
+#include <zephyr/ztest_error_hook.h>
+
+LOG_MODULE_REGISTER(libc_exit, LOG_LEVEL_INF);
+
+#define STACK_SIZE (512 + CONFIG_TEST_EXTRA_STACK_SIZE)
+static struct k_thread tdata;
+static K_THREAD_STACK_DEFINE(tstack, STACK_SIZE);
+
+static ZTEST_BMEM int execute_flag;
+
+ZTEST_SUITE(libc_exit, NULL, NULL, NULL, NULL, NULL);
+
+static void thread_call_exit(void *p1, void *p2, void *p3)
+{
+	execute_flag = 1;
+	LOG_INF("Calling exit.\n");
+	cflush();
+
+	ztest_set_fault_valid(true);
+	exit(1);
+	/* Should never reach this. */
+	ztest_test_fail();
+}
+
+void ztest_post_fatal_error_hook(unsigned int reason,
+				 const struct arch_esf *pEsf)
+{
+	printk("Caught system error -- reason %d\n", reason);
+	printk("This is Firas.\n");
+	zassert_equal(reason, K_ERR_KERNEL_PANIC);
+	zassert_true(execute_flag == 1);
+	printk("This is Firas again.\n");
+	ztest_test_pass();
+}
+
+ZTEST_USER(libc_exit, test1_non_essential_thread)
+{
+	LOG_INF("I am in non_essential_thread.\n");
+	cflush();
+	execute_flag = 0;
+	/* Testing a non-essential thread. */
+	k_tid_t tid;
+	tid = k_thread_create(&tdata, tstack, STACK_SIZE, thread_call_exit,
+			      NULL, NULL, NULL, 0, K_USER, K_NO_WAIT);
+	k_thread_join(tid, K_FOREVER);
+	/**TESTPOINT: spawned thread executed but abort itself*/
+	LOG_INF("I am in non_essential_thread before assert.\n");
+	cflush();
+	zassert_true(execute_flag == 1);
+}
+
+ZTEST_USER(libc_exit, test2_essential_thread)
+{
+	LOG_INF("I am in essential_thread.\n");
+	cflush();
+	execute_flag = 0;
+
+	/* Testing an essential thread. */
+	k_tid_t tid;
+	tid = k_thread_create(&tdata, tstack, STACK_SIZE, thread_call_exit,
+			      NULL, NULL, NULL, 0, K_ESSENTIAL, K_NO_WAIT);
+	k_thread_join(tid, K_FOREVER);
+	/* Should never reach this. */
+	ztest_test_fail();
+}
