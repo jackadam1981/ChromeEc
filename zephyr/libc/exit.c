c1eba6fee9edcdeb94e78354fe802b254eb7c508
Firas Sammoura <fsammoura@google.com>
2025-01-06 21:21:32 +0000

    zephyr/libc: Define _exit for newlib zephyr syscalls
    Create PLATFORM_EC_EXIT zephyr configuration and build exit.c when this
    configuration is enabled. Separate the Zephyr implementation of newlib
    syscalls from EC. Create libc directory for Zephyr-specific versions.
    
    BUG=b:384102168
    TEST=CQ
    
    Change-Id: I9d45b9a9ca99953e756f0f0257151f51240a7116

---
+++ zephyr/libc/exit.c
@@ -0,0 +1,25 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <stdlib.h>
+
+#include <zephyr/sys/printk.h>
+
+/**
+ * Reboot the system.
+ *
+ * This function is called from libc functions such as abort() or exit().
+ *
+ * @param rc exit code
+ */
+#ifndef CONFIG_ARCH_POSIX
+void _exit(int rc)
+#else
+void exit(int rc)
+#endif
+{
+	printk("%s called with rc: %d\n", __func__, rc);
+	abort();
+}
