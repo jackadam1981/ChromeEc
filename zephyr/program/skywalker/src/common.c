4d7c20b8ce12dc38d6874c3a3e4fe8166574af7e
Ting Shen <phoenixshen@google.com>
2025-01-06 14:53:10 +0800

    WIP: skywalker: initial board
    BUG=none
    TEST=zmake
    
    Change-Id: I41d9b7f5a618c385b7a31d9dcfa39d2367b1d325

---
+++ zephyr/program/skywalker/src/common.c
@@ -0,0 +1,8 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(skywalker, CONFIG_SKYWALKER_LOG_LEVEL);
