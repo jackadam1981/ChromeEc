6702532aaf3c363fcef1f529a6b0cb3941e8e5e2
Tom Hughes <tomhughes@chromium.org>
2025-01-14 14:03:58 -0800

    zephyr: fpmcu: Add watchdog test
    BUG=b:389761200
    TEST=none
    
    Change-Id: I8bb6631504de15caf6dc0cbff786ac69cacddc77

---
+++ zephyr/test/hwtest/src/watchdog.cc
@@ -0,0 +1,45 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "multistep_test.h"
+#include "panic.h"
+#include "system.h"
+
+#include <stdlib.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(watchdog_hw_test, LOG_LEVEL_INF);
+
+static void test_watchdog(void)
+{
+	constexpr uint32_t watchdog_period_us =
+		CONFIG_WATCHDOG_PERIOD_MS * USEC_PER_MSEC;
+
+	LOG_INF("Starting busy loop.\n");
+
+	/* Busy wait slightly longer than watchdog period to account for
+	 * inaccuracies in busy waiting. */
+	k_busy_wait(watchdog_period_us * 1.1);
+
+	/* Watchdog should fire before reaching this. */
+	zassert_unreachable();
+}
+
+static void test_panic_data(void)
+{
+	uint32_t reason = 0;
+	uint32_t info = 0;
+	uint8_t exception = 0;
+
+	panic_get_reason(&reason, &info, &exception);
+
+	/* Only "reason" is set for watchdog in Zephyr. */
+	zassert_equal(reason, PANIC_SW_WATCHDOG);
+}
+
+static void (*test_steps[])(void) = { test_watchdog, test_panic_data };
+
+MULTISTEP_TEST(watchdog, test_steps)
