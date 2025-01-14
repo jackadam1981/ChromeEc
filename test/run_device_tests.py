2326cf39d5ac4f5dc7a72270c78ffd42db4b3ced
Tom Hughes <tomhughes@chromium.org>
2025-01-14 11:24:46 -0800

    test: Add watchdog test
    BUG=b:389761200, b:390021699, b:390021325
    TEST=./test/run_device_tests.py --board bloonchipper -t watchdog
    
    Change-Id: Ie51fcdf91e9679ff88d8af9ae9285820f651cbe7

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -461,10 +461,13 @@ class Renode(Platform):
                     "zephyr_counter_basic_api_stm32_subsec",
                 ]:
                     return True
-            if test_name in [
-                "rtc_stm32f4",  # TODO(b/384991107)
-            ]:
-                return True
+
+                if test_name in [
+                    "rtc_stm32f4",  # TODO(b/384991107)
+                    "watchdog",  # TODO(b/390021699)
+                ]:
+                    return True
+
         elif board_config.name in [HELIPILOT, BUCCANEER, GWENDOLIN]:
             if test_name in [
                 "exception",  # TODO(b/384730599)
@@ -767,6 +770,11 @@ class AllTests:
             TestConfig(test_name="unaligned_access_benchmark"),
             TestConfig(test_name="utils"),
             TestConfig(test_name="utils_str"),
+            TestConfig(
+                test_name="watchdog",
+                # Increase timeout since this executes more slowly in Renode.
+                timeout_secs=120,
+            ),
             TestConfig(
                 config_name="power_utilization_idle",
                 test_name="power_utilization",
---
+++ test/watchdog.cc
@@ -0,0 +1,99 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "panic.h"
+#include "system.h"
+#include "task.h"
+#include "test_util.h"
+
+#include <cassert>
+
+test_static int test_watchdog()
+{
+	constexpr uint32_t watchdog_period_us =
+		CONFIG_WATCHDOG_PERIOD_MS * MSEC;
+
+	ccprints("Starting busy loop.");
+
+	/*
+	 * TODO(b/390021325): watchdog_period_us * 1.1 should work. It appears
+	 * that the watchdog timer is being set to longer than
+	 * CONFIG_WATCHDOG_PERIOD_MS.
+	 */
+	udelay(watchdog_period_us * 3);
+
+	/* Watchdog should fire before reaching this. */
+	assert(false);
+
+	return EC_ERROR_UNKNOWN;
+}
+
+test_static int test_panic_data()
+{
+	constexpr uint32_t expected_reason = PANIC_SW_WATCHDOG;
+	/* Note: The task_id can be found with the "taskinfo" command. */
+	constexpr uint8_t expected_task_id = 5;
+
+	/*
+	 * The watchdog should fire while udelay is running. We don't know the
+	 * exact end of udelay, but it's short so we just choose a small value
+	 * for the end.
+	 */
+	const uint32_t udelay_start_addr = reinterpret_cast<uint32_t>(udelay);
+	uint32_t udelay_end_addr = udelay_start_addr + 0x40;
+
+	uint32_t reason = 0;
+	uint32_t info = 0;
+	uint8_t exception = UINT8_MAX;
+
+	panic_get_reason(&reason, &info, &exception);
+
+	TEST_EQ(reason, expected_reason, "%08x");
+	TEST_GT(info, udelay_start_addr, "%d");
+	TEST_LT(info, udelay_end_addr, "%d");
+	TEST_EQ(exception, expected_task_id, "%d");
+
+	return EC_SUCCESS;
+}
+
+test_static void run_test_step1()
+{
+	test_set_next_step(TEST_STATE_STEP_2);
+	RUN_TEST(test_watchdog);
+}
+
+test_static void run_test_step2()
+{
+	RUN_TEST(test_panic_data);
+
+	if (test_get_error_count())
+		test_reboot_to_next_step(TEST_STATE_FAILED);
+	else
+		test_reboot_to_next_step(TEST_STATE_PASSED);
+}
+
+void test_run_step(uint32_t state)
+{
+	if (state & TEST_STATE_MASK(TEST_STATE_STEP_1)) {
+		run_test_step1();
+	} else if (state & TEST_STATE_MASK(TEST_STATE_STEP_2)) {
+		run_test_step2();
+	}
+}
+
+extern "C" int task_test(void *unused)
+{
+	if (IS_ENABLED(SECTION_IS_RW))
+		test_run_multistep();
+	return EC_SUCCESS;
+}
+
+void run_test(int argc, const char **argv)
+{
+	test_reset();
+	crec_msleep(30); /* Wait for TASK_ID_TEST to initialize */
+	task_wake(TASK_ID_TEST);
+}
