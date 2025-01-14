503b62fb59ecbbb1da0bbb84a6848a008f6d136f
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2025-01-14 19:33:47 +0800

    wyrdeer: Added lcd_reset_pin control
    When EC GPE7 is high, EC GPH1 outputs high.
    When EC GPE7 is low, EC GPH1 will delay 50ms outputs low.
    
    BUG=b:387440218
    TEST=zmake build wyrdeer
    LOW_COVERAGE_REASON=Tests will be added in b:387440218
    
    Change-Id: If0d853a298689f1c502f979613f282cf10cf6c28

--- zephyr/program/corsola/wyrdeer/src/lcd.c
+++ zephyr/program/corsola/wyrdeer/src/lcd.c
@@ -18,3 +18,28 @@ void ac_feedback_lcd(void)
 	}
 }
 DECLARE_HOOK(HOOK_AC_CHANGE, ac_feedback_lcd, HOOK_PRIO_DEFAULT);
+
+static bool value_en;
+
+static void set_bl_en_pin(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_h1), value_en);
+}
+DECLARE_DEFERRED(set_bl_en_pin);
+
+void ap_bl_en_interrupt(enum gpio_signal signal)
+{
+	if (gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_lcd_rst))) {
+		value_en = true;
+		hook_call_deferred(&set_bl_en_pin_data, 0);
+	} else {
+		value_en = false;
+		hook_call_deferred(&set_bl_en_pin_data, 50 * MSEC);
+	}
+}
+
+static void ap_bl_en_init(void)
+{
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_gpio_lcd_rst));
+}
+DECLARE_HOOK(HOOK_INIT, ap_bl_en_init, HOOK_PRIO_DEFAULT);
