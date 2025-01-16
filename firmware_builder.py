83845701207e2106001ec38f45a0f135e0a0bc91
Mary Ruthven <mruthven@chromium.org>
2025-01-16 15:33:46 -0600

    cr50: include prod.json in build artifacts
    Use gsc-utils/util/convert_signing_json.sh to strip the comments and
    whitespace from the signing manifest.
    
    BUG=b:388541567
    TEST=cq+1
    
    Change-Id: I2e7e1b8d642232ea37c8006030930b507b08eab3

--- firmware_builder.py
+++ firmware_builder.py
@@ -34,6 +34,10 @@ GE_BOARD = "reef"
 DEFAULT_BUNDLE_DIRECTORY = "/tmp/artifact_bundles"
 DEFAULT_BUNDLE_METADATA_FILE = "/tmp/artifact_bundle_metadata"
 SCRIPT_DIR = os.path.dirname(__file__)
+MANIFEST = os.path.join(SCRIPT_DIR, "util/signer/ec_RW-manifest-prod.json")
+SANITIZE_MANIFEST_SCRIPT = os.path.join(
+    SCRIPT_DIR, "../gsc-utils/util/convert_signing_json.sh"
+)
 RO_VER = "0.0.14"
 # List of files to bundle each element is a tuple with the source and dest
 # filenames. If the dest filename is empty, it'll keep the same basename.
@@ -217,10 +221,15 @@ def create_artifact_dir(ec_dir, build_target):
     if build_target == "host":
         return ["--exclude=*.o.d", "--exclude=*.o", "."]
 
+    build_dir = os.path.join(ec_dir, "build", build_target)
     cmd = ["mkdir", build_target]
-    subprocess.run(
-        cmd, cwd=os.path.join(ec_dir, "build", build_target), check=True
-    )
+    subprocess.run(cmd, cwd=build_dir, check=True)
+    cmd = [
+        SANITIZE_MANIFEST_SCRIPT,
+        MANIFEST,
+        os.path.join(build_target, "prod.json"),
+    ]
+    subprocess.run(cmd, cwd=build_dir, check=True)
     for src, dest in BUNDLE_FILES:
         dest = os.path.join(build_target, dest)
         # The non-cr50 builds are DBG and crypto test images. Rename their elf
@@ -228,9 +237,7 @@ def create_artifact_dir(ec_dir, build_target):
         if dest.endswith(".elf") and build_target != "cr50":
             dest += ".test"
         cmd = ["cp", src, dest]
-        subprocess.run(
-            cmd, cwd=os.path.join(ec_dir, "build", build_target), check=True
-        )
+        subprocess.run(cmd, cwd=build_dir, check=True)
     return [build_target]
 
 
