f827db345f054e7f242bebc0c9d767e94b4254fe
Ivan Chen <yulunchen@google.com>
2025-01-20 16:59:43 +0800

    [TEST ONLY] Support FW flash from EC for realtek PDC
    Change-Id: Ide245f049cc9db3aa2a80e339e4ddf4e029e4e2c

--- zephyr/program/brox/BUILD.py
+++ zephyr/program/brox/BUILD.py
@@ -132,7 +132,7 @@ lotso = register_brox_project(
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release. Not needed for brox-ish since it
 # doesn't use RO+RW
-assert_rw_fwid_DO_NOT_EDIT(project_name="brox", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="brox", addr=0x70098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox-ish-ec", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox-tokenized", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="greenbayupoc", addr=0x60098)
