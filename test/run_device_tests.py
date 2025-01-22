a204762186b7e54862b5bcdf55f955ac5816b313
Tom Hughes <tomhughes@chromium.org>
2025-01-22 14:11:10 -0800

    test/run_device_tests: Skip timer test on bloonchipper Zephyr
    This test has started to occasionally fail, so disable it until we
    resolve the flakiness.
    
    BUG=b:390255521
    TEST=./test/run_device_tests.py --board bloonchipper -t timer --zephyr --renode
         => SKIPPED
    
    Change-Id: If994dbaa1ba6e289c2e856b6cdc2d0faa9d198c3

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -456,6 +456,7 @@ class Renode(Platform):
                     "ftrapv",  # TODO(b/384095271)
                     "panic",  # TODO(b/384095226)
                     "panic_data",  # TODO(b/384095623)
+                    "timer",  # TODO(b/390255521)
                     "zephyr_flash_stm32f4",  # TODO(b/384974228)
                     # TODO(b/384975384)
                     "zephyr_counter_basic_api_stm32_subsec",
