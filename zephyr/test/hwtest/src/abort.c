58a4a21375f0e28d99b7757e26cf22cd250c8ac8
Firas Sammoura <fsammoura@google.com>
2025-01-27 11:13:35 -0800

    zephyr: test: align abort test with zephyr implementation
    Configuration ZTEST_FATAL_HOOK can alert that the test will lead to a
    kernel panic when the variable valid_fault is set to true.
    
    BUG=b:392641774
    TEST=./test/run_device_tests.py --board=bloonchipper --flasher
         servo_micro --test abort --zephyr
    
    Change-Id: I14dadadbf8f21ca67d5ea0490c39f8b26940f2b8

--- zephyr/test/hwtest/src/abort.c
+++ zephyr/test/hwtest/src/abort.c
@@ -3,41 +3,34 @@
  * found in the LICENSE file.
  */
 
-#include "multistep_test.h"
 #include "panic.h"
 #include "system.h"
 
 #include <stdlib.h>
 
 #include <zephyr/logging/log.h>
+#include <zephyr/ztest.h>
+#include <zephyr/ztest_error_hook.h>
 
 LOG_MODULE_REGISTER(abort_hw_test, LOG_LEVEL_INF);
 
-static void test_abort(void)
+ZTEST_SUITE(abort_hw_test, NULL, NULL, NULL, NULL, NULL);
+
+void ztest_post_fatal_error_hook(unsigned int reason,
+				 const struct arch_esf *pEsf)
+{
+	LOG_INF("Caught system error -- reason %d\n", reason);
+	cflush();
+	zassert_equal(reason, K_ERR_KERNEL_PANIC);
+	ztest_test_pass();
+}
+
+ZTEST(abort_hw_test, test_abort)
 {
 	LOG_INF("Calling abort\n");
 	cflush();
+	ztest_set_fault_valid(true);
 	abort();
 	/* Should never reach this. */
-	zassert_unreachable();
+	ztest_test_fail();
 }
-
-static void test_panic_data(void)
-{
-#ifdef CONFIG_ARM
-	struct panic_data *const pdata = panic_get_data();
-	uint32_t abort_addr = (uint32_t)abort;
-	/* Estimated end of the abort function, which is short. */
-	uint32_t abort_end = (uint32_t)abort + 0x40;
-	uint32_t pc = pdata->cm.frame[CORTEX_PANIC_FRAME_REGISTER_PC];
-
-	/* Make sure Program Counter is stored correctly and points at the abort
-	 * function.
-	 */
-	zassert_true((abort_addr <= pc) && (abort_end >= pc));
-#endif
-}
-
-static void (*test_steps[])(void) = { test_abort, test_panic_data };
-
-MULTISTEP_TEST(abort, test_steps)
