53fddab5789b62ba3f4d4722d228fc257c281e6c
Tom Hughes <tomhughes@chromium.org>
2025-01-28 11:02:05 -0800

    zephyr: test: Fix RT1718S_EMUL_REG_COUNT_PER_PAGE
    When building with clang it warns:
    
    zephyr/emul/tcpc/emul_rt1718s.c:114:30: error: result of comparison of
    constant 256 with expression of type 'uint8_t' (aka 'unsigned char') is
    always false [-Werror,-Wtautological-constant-out-of-range-compare]
      114 |         if (val == NULL || reg_addr > RT1718S_EMUL_REG_COUNT_PER_PAGE) {
          |                            ~~~~~~~~ ^ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    zephyr/emul/tcpc/emul_rt1718s.c:138:15: error: result of comparison of
    constant 256 with expression of type 'uint8_t' (aka 'unsigned char') is
    always false [-Werror,-Wtautological-constant-out-of-range-compare]
      138 |         if (reg_addr > RT1718S_EMUL_REG_COUNT_PER_PAGE) {
          |
    
    It appears that register addresses are supposed to be 8 bits (uint8_t
    reg_addr). This is then used to index into an array of size
    RT1718S_EMUL_REG_COUNT_PER_PAGE. If RT1718S_EMUL_REG_COUNT_PER_PAGE is
    0x100, then it's not possible to fully index into all of the values.
    Adjust RT1718S_EMUL_REG_COUNT_PER_PAGE to the max value that can be
    represented with 8 bits (0xFF).
    
    BUG=b:390722919
    TEST=CQ
    
    Change-Id: I7829dff0a8f7e77a4cd53c6f5482d43fb1c9afcd

--- zephyr/include/emul/tcpc/emul_rt1718s.h
+++ zephyr/include/emul/tcpc/emul_rt1718s.h
@@ -11,7 +11,7 @@
 #include <zephyr/drivers/emul.h>
 #include <zephyr/sys/slist.h>
 
-#define RT1718S_EMUL_REG_COUNT_PER_PAGE 0x100
+#define RT1718S_EMUL_REG_COUNT_PER_PAGE 0xFF
 
 struct set_reg_entry_t {
 	struct _snode node;
