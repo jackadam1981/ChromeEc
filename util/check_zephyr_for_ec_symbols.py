d66711f3f8aa4d007ea0d0e8a3406d0e7fd54f95
Tom Hughes <tomhughes@chromium.org>
2025-01-31 13:58:47 -0800

    util/check_zephyr_for_ec_symbols: Check for crec_usleep, crec_msleep, crec_sleep
    BUG=b:393593789
    TEST=./util/check_zephyr_for_ec_symbols.py zephyr/program/corsola/src/usb_pd_policy.c
    
    Change-Id: Id12d177f2908eb1de0bac4b7b3eeaba0a21fe9b1

--- util/check_zephyr_for_ec_symbols.py
+++ util/check_zephyr_for_ec_symbols.py
@@ -15,6 +15,9 @@ import preupload.lib
 
 EC_FUNCTIONS: Dict[str, str] = {
     "udelay": "k_busy_wait",
+    # "crec_usleep": "while (us) { us = k_usleep(us); }",
+    # "crec_msleep": "while (ms) { ms = k_msleep(ms); }",
+    # "crec_sleep": "k_sleep(K_SECONDS(sec))",
 }
 
 EC_MACROS: Dict[str, str] = {
