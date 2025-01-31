60b84126358d4f1ea9a0465191b09fa9d30b9aeb
Tom Hughes <tomhughes@chromium.org>
2025-01-31 13:58:47 -0800

    util/check_zephyr_for_ec_symbols: Check for crec_usleep, crec_msleep, crec_sleep
    BUG=b:394679956
    TEST=./util/check_zephyr_for_ec_symbols.py --commit pre-submit
    
    Change-Id: Id12d177f2908eb1de0bac4b7b3eeaba0a21fe9b1

--- util/check_zephyr_for_ec_symbols.py
+++ util/check_zephyr_for_ec_symbols.py
@@ -14,6 +14,9 @@ import preupload.lib
 
 
 EC_FUNCTIONS: Dict[str, str] = {
+    "crec_msleep": "k_msleep",
+    "crec_sleep": "k_sleep(K_SECONDS(sec))",
+    "crec_usleep": "k_usleep",
     "force_time": "",
     "time_after": "",
     "time_until": "",
