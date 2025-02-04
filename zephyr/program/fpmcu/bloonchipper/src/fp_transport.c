3784e9942b417e81813bf251511e982036ea2d66
David Cross <davidmcross@google.com>
2025-02-03 21:46:18 -0800

    bloonchipper: create common.c for fpmcu and add get_fp_transport_type()
    BUG=b:309557100
    TEST=zmake compare-builds
    
    Change-Id: I68581191a677274c69faa9f95922a25f48363d81

--- zephyr/program/fpmcu/bloonchipper/src/fp_transport.c
+++ zephyr/program/fpmcu/bloonchipper/src/fp_transport.c
@@ -15,29 +15,7 @@
 #include <fpsensor/fpsensor_detect.h>
 #include <gpio_signal.h>
 
-enum fp_transport_type get_fp_transport_type(void)
-{
-	static enum fp_transport_type ret = FP_TRANSPORT_TYPE_UNKNOWN;
-
-	if (ret == FP_TRANSPORT_TYPE_UNKNOWN) {
-		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(div_highside), 1);
-		k_usleep(1);
-		switch (gpio_pin_get_dt(
-			GPIO_DT_FROM_NODELABEL(transport_sel))) {
-		case 0:
-			ret = FP_TRANSPORT_TYPE_UART;
-			break;
-		case 1:
-			ret = FP_TRANSPORT_TYPE_SPI;
-			break;
-		default:
-			ret = FP_TRANSPORT_TYPE_UNKNOWN;
-			break;
-		}
-	}
-
-	return ret;
-}
+extern enum fp_transport_type get_fp_transport_type(void);
 
 #if !defined(CONFIG_EC_HOST_CMD_BACKEND_SPI) && \
 	!defined(CONFIG_EC_HOST_CMD_BACKEND_UART)
