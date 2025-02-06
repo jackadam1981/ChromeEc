5eb7417b66521fa5b715194460147396942e94c8
Rob Barnes <robbarnes@google.com>
2025-02-06 10:05:14 -0700

    panic: Introduce panic_log
    Panic log captures a copy of the console log in preserved
    ram. After a panic, the panic_log is frozen until it is
    dumped by the OS. Preserving the log is best effort. Integrity checks
    ensure corruption is detected and the log is discarded.
    
    The implementation is designed to be low overhead to minimize
    performance impact.
    
    BUG=b:399207571
    TEST=Panic log preserved across watchdog and hang. Tested on
    lantis, magolor, riven.
    TEST=Pass firmware.EcStress.all
    
    Change-Id: I8797c5569f6d8bcc1df9655a2a6411c44f97eb90

---
+++ zephyr/test/drivers/panic_log/src/panic_log.c
@@ -0,0 +1,14 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "panic_log.h"
+
+#include <zephyr/ztest.h>
+
+ZTEST_USER(panic_log, test_capacity)
+{
+}
+
+ZTEST_SUITE(panic_log, NULL, NULL, NULL, NULL, NULL);
