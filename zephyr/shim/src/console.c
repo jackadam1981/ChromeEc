b12a786d99ccc59fd7b9eb7b13379e94a5eb305f
Rob Barnes <robbarnes@google.com>
2025-02-06 10:05:14 -0700

    panic: Introduce panic_log
    Panic log captures a copy of the console log in preserved
    ram. After a panic, the panic_log is frozen until it is
    dumped by the OS. Preserving the log is best effort. Integrity checks
    ensure corruption is detected and the log is discarded.
    
    The implementation is designed to be low overhead to minimize
    performance impact.
    
    BUG=b:399207571
    TEST=Panic log preserved across watchdog and hang. Tested on
    lantis, magolor, riven.
    TEST=Pass firmware.EcStress.all
    TEST=twister -s drivers.panic_log.immediate
    TEST=twister -s drivers.panic_log.minimal
    
    Change-Id: I8797c5569f6d8bcc1df9655a2a6411c44f97eb90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6240011
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Al Semjonovs <asemjonovs@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Auto-Submit: Rob Barnes <robbarnes@google.com>

--- zephyr/shim/src/console.c
+++ zephyr/shim/src/console.c
@@ -10,6 +10,7 @@
  */
 #include "builtin/stdio.h"
 #include "console.h"
+#include "panic_log.h"
 #include "printf.h"
 #include "task.h"
 #include "uart.h"
@@ -318,6 +319,10 @@ SYS_INIT(init_ec_console, PRE_KERNEL_1,
  */
 static int zephyr_shim_console_out(int c)
 {
+	if (IS_ENABLED(CONFIG_PLATFORM_EC_PANIC_LOG)) {
+		panic_log_write_char(c);
+	}
+
 	/* Always capture EC output into the AP console buffer. */
 	if (IS_ENABLED(CONFIG_PLATFORM_EC_HOSTCMD_CONSOLE) && !k_is_in_isr()) {
 		char console_char = c;
@@ -482,6 +487,10 @@ static void handle_sprintf_rv(int rv, size_t *len)
 
 static void zephyr_print(const char *buff, size_t size)
 {
+	if (IS_ENABLED(CONFIG_PLATFORM_EC_PANIC_LOG)) {
+		panic_log_write_str(buff, size);
+	}
+
 	/*
 	 * shell_* functions can not be used in ISRs so optionally use
 	 * printk instead.
