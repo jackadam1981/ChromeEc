a3234a2f7b69541849a5df80759de1e8b06db6d3
Tom Hughes <tomhughes@chromium.org>
2025-02-06 15:50:29 -0800

    util/check_zephyr_for_ec_symbols: Add clock()
    The Zephyr equivalent for the EC clock() function is k_uptime_get_32().
    
    BUG=none
    TEST=./util/check_zephyr_for_ec_symbols.py --commit pre-submit
    
    Change-Id: I3458ab707c4bafc14c4de123b392b236a5140ee4

--- util/check_zephyr_for_ec_symbols.py
+++ util/check_zephyr_for_ec_symbols.py
@@ -14,6 +14,7 @@ import preupload.lib
 
 
 EC_FUNCTIONS: Dict[str, str] = {
+    "clock": "k_uptime_get_32",
     "crec_msleep": "k_msleep",
     "crec_sleep": "k_sleep(K_SECONDS(sec))",
     "crec_usleep": "k_usleep",
