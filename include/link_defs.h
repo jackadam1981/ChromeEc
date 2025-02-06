ae91d6b1a3de01052dcf734789b0b6ee31ec1c0c
Rob Barnes <robbarnes@google.com>
2025-02-06 08:58:15 -0700

    link_defs: Add noinit_ram section
    The noninit_ram section is uninitialized ram. It is placed after the
    bss and before the shared_mem_buf. It is usually preserved across reboots.
    Power loss will clear this memory. This memory may be modified by the
    other RO/RW image.
    
    BUG=None
    TEST=Build all
    
    Change-Id: I5200bc1a46139b019b664567c84d2ba06d450628

--- include/link_defs.h
+++ include/link_defs.h
@@ -172,6 +172,12 @@ extern const char __preserved_logs_size[];
 #define __preserved_logs(name)
 #endif
 
+#define __noinit_ram(name) \
+	__attribute__((section(".noinit_ram." STRINGIFY(name))))
+extern const char __noinit_ram_start[];
+extern const char __noinit_ram_end[];
+extern const char __noinit_ram_size[];
+
 #ifdef __cplusplus
 }
 #endif
