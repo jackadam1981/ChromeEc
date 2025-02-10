0ef355e4179cf8d2c3605c2e307fcd4f024902e4
Mary Ruthven <mruthven@chromium.org>
2025-02-10 09:30:53 -0600

    cr50: setup MP branch
    Set BRANCH to MP
    
    BUG=b:395609223
    TEST=run GSCHelp and CCDCapabilitiesOpenDefaultValues devboard tests
    
    Change-Id: I3fd4268cac988f70677e99eca8d979a8e037b850

--- board/cr50/build.mk
+++ board/cr50/build.mk
@@ -27,8 +27,8 @@ CHIP_VARIANT ?= cr50_fpga
 #   - spihash command is available when ccd is locked
 #   - added chargen command for testing uart
 #
-# This is the TOT branch.
-BRANCH:=TOT
+# This is the MP branch.
+BRANCH:=MP
 
 # Additional / overriding warnings for common rules and chip
 # (TODO) enable after https://crrev.com/c/3198155
