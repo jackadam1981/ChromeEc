b61474423ca90ed2791e9a837c207817124c3974
Brandon Breitenstein <brandon.breitenstein@intel.com>
2025-02-10 12:35:21 -0800

    ptlrvp: Change initialization of PCA95XX ioexpander to use AP callbacks
    Modular TCSS card detection relies on the PCA95XX ioexpander gpios.
    Since Modular TCSS needs to be initialized in S4 the PCA95xx ioex needs
    to be initialized before that, previously it was initialized once the
    AP was up and able to call the host command for board id.
    
    BUG=b:396174130
    TEST=verified that gpio for modular aic port is working as expected
    
    Change-Id: I8e0e098da335838b0b8d214c0aa98c9c12d4180f

--- zephyr/test/intelrvp/tests/intelrvp/src/board_id.c
+++ zephyr/test/intelrvp/tests/intelrvp/src/board_id.c
@@ -7,11 +7,14 @@
 #include "intel_rvp_board_id.h"
 #include "intelrvp.h"
 #include "system.h"
+#include "emul/emul_ap_callbacks.h"
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/gpio/gpio_emul.h>
 #include <zephyr/fff.h>
 #include <zephyr/ztest.h>
+
+#include <ap_power/ap_pwrseq.h>
 
 #define NUM_BOM_GPIOS DT_PROP_LEN(DT_INST(0, intel_rvp_board_id), bom_gpios)
 #define NUM_FAB_GPIOS DT_PROP_LEN(DT_INST(0, intel_rvp_board_id), fab_gpios)
@@ -32,6 +35,14 @@ static void configure_board_id_gpios_input(void)
 				   board_id_config[i].pin,
 				   (GPIO_INPUT | GPIO_ACTIVE_HIGH));
 }
+
+static void send_ap_pwr_callback(void)
+{
+	const struct device *dev = ap_pwrseq_get_instance();
+
+	ap_pwrseq_emul_entry_callback(dev, AP_POWER_STATE_S5, AP_POWER_STATE_G3);
+}
+
 
 static void test_set_board_id_gpios(void)
 {
@@ -58,11 +69,10 @@ static void test_set_board_id_gpios(void)
 	return;
 }
 
-/* Board ID gpios need to be initialized before other applications */
-SYS_INIT(test_set_board_id_gpios, POST_KERNEL, 99);
-
 ZTEST(board_version_tests, test_board_get_version)
 {
+	/* PTL requires AP callback to enable PCA95XX */
+	send_ap_pwr_callback();
 	/* Set up fake return values for successful GPIO pin reading */
 	int expected_board_id = CONFIG_TEST_PROJECT_INTELRVP_BOARD_ID_VAL;
 
@@ -74,6 +84,20 @@ ZTEST(board_version_tests, test_board_get_version)
 		"Expected version didn't match actual version. Expected: %d, Actual: %d",
 		expected_board_id, version);
 }
+
+/* Board ID gpios need to be initialized before other applications */
+SYS_INIT(test_set_board_id_gpios, POST_KERNEL, 99);
+#ifdef CONFIG_TEST_PROJECT_PTLRVP_MCHP
+ZTEST(board_version_tests, pca95xx_not_init_test)
+{
+        int expected_board_id = CONFIG_TEST_PROJECT_INTELRVP_BOARD_ID_VAL;
+        int version = board_get_version();
+
+        zassert_not_equal(
+                expected_board_id, version,
+                "Expected versions match PCA95XX shouldn't be initialized");
+}
+#endif
 
 /* Test Suite Setup */
 ZTEST_SUITE(board_version_tests, NULL, NULL, NULL, NULL, NULL);
