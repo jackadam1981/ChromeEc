0a5cbf00dc550103242e4814e3c275bd49c53cac
Mary Ruthven <mruthven@chromium.org>
2025-02-12 11:43:00 -0600

    firmware_builder: fix GSC artifact format
    Rename the tarballs. Mark them with type GSC. This matches the ti50
    firmware_builder.py behavior.
    
    BUG=b:396134713
    TEST=none
    
    Change-Id: I39582e399f52282fe304e61075d90ad6a84b7bdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6258820
    Auto-Submit: Mary Ruthven <mruthven@chromium.org>
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Jett Rink <jettrink@google.com>
    Commit-Queue: Jett Rink <jettrink@google.com>

--- firmware_builder.py
+++ firmware_builder.py
@@ -238,7 +238,7 @@ def bundle_firmware(opts):
     bundle_dir = get_bundle_dir(opts)
     ec_dir = os.path.dirname(__file__)
     for build_target in sorted(os.listdir(os.path.join(ec_dir, "build"))):
-        tarball_name = "".join([build_target, ".firmware.tbz2"])
+        tarball_name = "".join([build_target, ".firmware.tar.bz2"])
         tarball_path = os.path.join(bundle_dir, tarball_name)
 
         artifact_dir = create_artifact_dir(ec_dir, build_target)
@@ -254,7 +254,7 @@ def bundle_firmware(opts):
             meta.tarball_info.board.extend([GE_BOARD])
             meta.tarball_info.publish_to_goldeneye = True
         meta.tarball_info.type = (
-            firmware_pb2.FirmwareArtifactInfo.TarballInfo.FirmwareType.EC
+            firmware_pb2.FirmwareArtifactInfo.TarballInfo.FirmwareType.GSC
         )
         # TODO(kmshelton): Populate the rest of metadata contents as it gets
         # defined in infra/proto/src/chromite/api/firmware.proto.
