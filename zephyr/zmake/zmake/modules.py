f3fc784c9e4fa4e2443d16eb4926313170d05a62
Keith Short <keithshort@chromium.org>
2025-02-12 11:21:08 -0700

    zmake: filter projects that require private modules
    When using the public manifest, some private modules are not available.
    Skip projects that require private modules as part of the build all
    target.
    
    BUG=none
    TEST=zmake list-projects run against private and public manifest
    TEST=chromeos-zephyr ebuild passes
    TEST=Full CQ
    
    Cq-Include-Trybots: chromeos/cq:cq-orchestrator
    Change-Id: If7846a8bacf3e643e2e1ac4fef2e246ac7f0a01b

--- zephyr/zmake/zmake/modules.py
+++ zephyr/zmake/zmake/modules.py
@@ -25,21 +25,85 @@ known_modules = {
     # TODO(b/384581513): boringssl is not officially recognized by Zephyr,
     # since it doesn't have a zephyr/module.yaml. That doesn't prevent us from
     # using it with zmake.
-    "boringssl": lambda name, checkout: (
-        checkout / "src" / "third_party" / name
-    ),
-    "hal_stm32": third_party_module,
-    "cmsis": third_party_module,
-    "ec": lambda name, checkout: (checkout / "src" / "platform" / "ec"),
-    "fpc": lambda name, checkout: (
-        checkout / "src" / "platform" / "fingerprint" / "fpc"
-    ),
-    "nanopb": third_party_module,
-    "pigweed": lambda name, checkout: (checkout / "src" / "third_party" / name),
-    "hal_intel_public": third_party_module,
-    "picolibc": third_party_module,
-    "intel_module_private": third_party_module,
+    "boringssl": {
+        "locator": lambda name, checkout: (
+            checkout / "src" / "third_party" / name
+        ),
+        "type": "public",
+    },
+    "hal_stm32": {
+        "locator": third_party_module,
+        "type": "public",
+    },
+    "cmsis": {
+        "locator": third_party_module,
+        "type": "public",
+    },
+    "ec": {
+        "locator": lambda name, checkout: (
+            checkout / "src" / "platform" / "ec"
+        ),
+        "type": "public",
+    },
+    "fpc": {
+        "locator": lambda name, checkout: (
+            checkout / "src" / "platform" / "fingerprint" / "fpc"
+        ),
+        "type": "public",
+    },
+    "nanopb": {
+        "locator": third_party_module,
+        "type": "public",
+    },
+    "pigweed": {
+        "locator": lambda name, checkout: (
+            checkout / "src" / "third_party" / name
+        ),
+        "type": "public",
+    },
+    "hal_intel_public": {
+        "locator": third_party_module,
+        "type": "public",
+    },
+    "picolibc": {
+        "locator": third_party_module,
+        "type": "public",
+    },
+    "intel_module_private": {
+        "locator": third_party_module,
+        "type": "private",
+    },
 }
+
+
+def is_private(module_name):
+    """Indicate if module is private.
+
+    Public modules are required and zmake will raise an exception if a public
+    module cannot be found.  Private modules are not available in the public
+    manifest, so projects that require a private module are skipped instead
+    of throwing an error.
+
+    Args:
+        module_name: Name of the module.
+
+    Returns:
+        True: the specified module_name is private.
+        False: the specified module_name is public.
+
+
+    Raises:
+        A KeyError, if the module_name is not known.
+    """
+    try:
+        module_info = known_modules[module_name]
+    except KeyError as e:
+        raise KeyError(f"The {module_name} module is not a known module") from e
+
+    if module_info["type"] == "private":
+        return True
+
+    return False
 
 
 def locate_from_checkout(checkout_dir):
@@ -57,7 +121,8 @@ def locate_from_checkout(checkout_dir):
         A dictionary mapping module names to paths.
     """
     result = {}
-    for name, locator in known_modules.items():
+    for name, module_info in known_modules.items():
+        locator = module_info["locator"]
         path = locator(name, checkout_dir)
         if path.exists():
             result[name] = path
