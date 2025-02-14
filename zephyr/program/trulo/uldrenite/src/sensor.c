e875b4842ad37d3ac248c2196e5ecb9c8feb1699
Logan_Liao <Logan_Liao@compal.corp-partner.google.com>
2025-02-14 11:03:44 +0800

    uldrenite: Enable body detection
    Enable boady detection for uldrenite
    
    BUG=b:391727623
    TEST=test body detection success
    
    Change-Id: Ib8c5f281fb88e47aeafff891aeb98b64c2411e81

--- zephyr/program/trulo/uldrenite/src/sensor.c
+++ zephyr/program/trulo/uldrenite/src/sensor.c
@@ -28,6 +28,8 @@ void motion_interrupt(enum gpio_signal signal)
 {
 	if (sensor_fwconfig == FORM_FACTOR_CONVERTIBLE)
 		lsm6dsm_interrupt(signal);
+	else
+		lis2dw12_interrupt(signal);
 }
 
 void lid_accel_interrupt(enum gpio_signal signal)
@@ -48,9 +50,10 @@ static void motionsense_init(void)
 
 	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
 		ccprints("Board is Clamshell");
+		MOTIONSENSE_ENABLE_ALTERNATE(alt_base_accel);
 		gmr_tablet_switch_disable();
-		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
-		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
 				      GPIO_INPUT | GPIO_PULL_UP);
 
 	} else if (sensor_fwconfig == FORM_FACTOR_CONVERTIBLE)
