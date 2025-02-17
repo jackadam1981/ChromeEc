a706a2a2c0d1ecc9d3a3756da2e86f91fcc7d2ea
Dino Li <Dino.Li@ite.com.tw>
2025-02-17 11:45:12 +0800

    WIP: board/skywalker_ite
    Change-Id: I149758ac59ccd456bf01a3632c8e2fa47ca9a636

--- zephyr/program/skywalker/BUILD.py
+++ zephyr/program/skywalker/BUILD.py
@@ -18,9 +18,25 @@ def register_skywalker_npcx_project(project_name):
         inherited_from=["skywalker"],
     )
 
+def register_skywalker_ite_project(project_name):
+    """Register a variant of skywalker_ite."""
+    return register_binman_project(
+        project_name=project_name,
+        zephyr_board="it8xxx2/it82002aw",
+        dts_overlays=[here / project_name / "project.overlay"],
+        kconfig_files=[
+            here / "program.conf",
+            here / "ite_program.conf",
+            here / project_name / "project.conf",
+        ],
+        inherited_from=["skywalker_ite"],
+    )
+
 
 register_skywalker_npcx_project(project_name="skywalker")
+register_skywalker_ite_project(project_name="skywalker_ite")
 
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="skywalker", addr=0x40144)
+assert_rw_fwid_DO_NOT_EDIT(project_name="skywalker_ite", addr=0x60098)
