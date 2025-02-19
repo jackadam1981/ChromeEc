b5bcd72de8356de6d432333a22919d716e0d6b0b
Firas Sammoura <fsammoura@google.com>
2025-02-19 17:47:11 +0000

    zephyr: fingerprint: modify support for fpc1025 factory testing
    Translate the fpcapture mode from the ec domain to the fpc domain.
    
    BUG=b:397442626
    TEST=Create firmware from https://crrev.com/c/6208292, flash firmware,
         and test using different `fpcapture` types (0, 4, 8, 12, 16, and
         20). Confirm correct image is being captured.
    TEST=./twister -v -i -T zephyr/test/fingerprint/drivers/fpc1025 --test \
         fingerprint.drivers.fpc1025
    
    Change-Id: If9293891a30653034355158a9b0a241390d525d8

--- zephyr/drivers/fingerprint/fingerprint_fpc1025.c
+++ zephyr/drivers/fingerprint/fingerprint_fpc1025.c
@@ -8,6 +8,8 @@
 #include "fingerprint_fpc1025.h"
 #include "fingerprint_fpc1025_private.h"
 
+#include <assert.h>
+
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/spi.h>
 #include <zephyr/logging/log.h>
@@ -30,6 +32,28 @@ enum fpc1025_cmd {
 	FPC1025_CMD_DEEPSLEEP = 0x2C,
 	FPC1025_CMD_HW_ID = 0xFC,
 };
+
+static int convert_fp_capture_mode_to_fpc_get_image_type(int mode)
+{
+	switch (mode) {
+	case FINGERPRINT_CAPTURE_TYPE_VENDOR_FORMAT:
+		return FPC_CAPTURE_VENDOR_FORMAT;
+	case FINGERPRINT_CAPTURE_TYPE_SIMPLE_IMAGE:
+		return FPC_CAPTURE_SIMPLE_IMAGE;
+	case FINGERPRINT_CAPTURE_TYPE_PATTERN0:
+		return FPC_CAPTURE_PATTERN0;
+	case FINGERPRINT_CAPTURE_TYPE_PATTERN1:
+		return FPC_CAPTURE_PATTERN1;
+	case FINGERPRINT_CAPTURE_TYPE_QUALITY_TEST:
+		return FPC_CAPTURE_QUALITY_TEST;
+	case FINGERPRINT_CAPTURE_TYPE_RESET_TEST:
+		return FPC_CAPTURE_RESET_TEST;
+	default:
+		assert(false);
+		break;
+	}
+	return FPC_CAPTURE_SIMPLE_IMAGE;
+}
 
 /* The 16-bit hardware ID is 0x021y */
 #define FP_SENSOR_HWID_FPC 0x021
@@ -335,7 +359,8 @@ static int fpc1025_acquire_image(const struct device *dev, int mode,
 	if (image_buf_size < CONFIG_FINGERPRINT_SENSOR_IMAGE_SIZE)
 		return -EINVAL;
 
-	rc = fp_sensor_acquire_image_with_mode(image_buf, mode);
+	rc = fp_sensor_acquire_image_with_mode(
+		image_buf, convert_fp_capture_mode_to_fpc_get_image_type(mode));
 	if (rc < 0) {
 		LOG_ERR("Failed to acquire image with mode %d: %d", mode, rc);
 		return rc;
