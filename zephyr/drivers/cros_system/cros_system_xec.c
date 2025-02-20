b7933f8c87ff25e7644cb77f3fdfb525953bf837
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2025-02-19 17:56:26 -0800

    zephyr mchp: Add support for `wakeup` flag in VCI devicetree
    Not all VCI input pins should be required to wake up the system from
    hibernation. This CL enables `wakeup` flag to explicitly indicate that
    a given VCI input is meant to be used as a wakeup source.
    
    Altough all VCI inputs are enabled, those without `wakeup` flag will
    be disabled prior entering in hibernation.
    
    BUG=b:396082465
    TEST=Enable VCI input with `wakeup` flag
         Enter hibernation
         Use VCI input to wake up system
    
    Change-Id: I9ee6e7d5b0d622ecb814f76c8a41d73853f091e5

--- zephyr/drivers/cros_system/cros_system_xec.c
+++ zephyr/drivers/cros_system/cros_system_xec.c
@@ -142,13 +142,13 @@ struct app_vci_pin {
 #define MCHP_VCI_INFO_GET_PRESERVE(v) (((v) >> 0) & 0x1u)
 #define MCHP_VCI_INFO_GET_POLARITY(v) (((v) >> 1) & 0x1u)
 #define MCHP_VCI_INFO_GET_LATCH_EN(v) (((v) >> 2) & 0x1u)
-#define MCHP_VCI_INFO_NODE_EN(v) (((v) >> 3) & 0x1u)
+#define MCHP_VCI_INFO_WAKEUP_EN(v) (((v) >> 3) & 0x1u)
 
 #define MCHP_DT_VCI_INFO(nid)                                           \
 	((uint8_t)((DT_PROP(nid, preserve) & 0x1) << 0) |               \
 	 (uint8_t)((DT_ENUM_IDX(nid, vci_polarity) & 0x1) << 1) |       \
 	 (uint8_t)((DT_PROP_OR(nid, vci_latch_enable, 0) & 0x1) << 2) | \
-	 (uint8_t)(1 << 3))
+	 (uint8_t)((DT_PROP(nid, wakeup) & 0x1) << 3))
 
 #define HIB_VCI_ENTRY(nid)                         \
 	[DT_REG_ADDR(nid)] = {                     \
@@ -350,13 +350,15 @@ static void cros_system_xec_vci_init(void)
 	vci->CONFIG |= MCHP_VCI_FW_EXT_SEL;
 
 	for (size_t id = 0; id < ARRAY_SIZE(app_vci_table); id++) {
-		struct vci_regs *vci = STRUCT_VCI_REG_BASE_ADDR;
 		const struct app_vci_pin *pvci = &app_vci_table[id];
-		uint8_t vci_node_en = MCHP_VCI_INFO_NODE_EN(pvci->vci_info);
 		uint8_t vci_clear = !MCHP_VCI_INFO_GET_PRESERVE(pvci->vci_info);
 
-		if (vci_node_en && vci_clear) {
+		if (vci_clear) {
 			vci->LATCH_RST |= BIT(id);
+		}
+
+		if (id > 0) {
+			vci->INPUT_EN |= BIT(id);
 		}
 	}
 }
@@ -372,9 +374,9 @@ static void cros_system_xec_configure_vci_in(void)
 			MCHP_VCI_INFO_GET_POLARITY(pvci->vci_info);
 		uint8_t vci_latch_en =
 			MCHP_VCI_INFO_GET_LATCH_EN(pvci->vci_info);
-		uint8_t vci_node_en = MCHP_VCI_INFO_NODE_EN(pvci->vci_info);
+		uint8_t vci_wakeup_en = MCHP_VCI_INFO_WAKEUP_EN(pvci->vci_info);
 
-		if (vci_node_en) {
+		if (vci_wakeup_en) {
 			/* configure VCI register per board design */
 			if (vci_polarity) {
 				vci->POLARITY |= BIT(id);
@@ -389,6 +391,8 @@ static void cros_system_xec_configure_vci_in(void)
 			if (id > 0) {
 				vci->INPUT_EN |= BIT(id);
 			}
+		} else if (id > 0) {
+			vci->INPUT_EN &= ~BIT(id);
 		}
 	}
 }
