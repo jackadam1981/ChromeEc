239e09978fcbd0cdc5ed73f1d9c748501e15b926
Brandon Breitenstein <brandon.breitenstein@intel.com>
2025-02-21 16:17:03 -0800

    ap_power_emulation: Add functions to emulate the state callbacks
    For testing purposes you may need to simulate an ap power callback
    this adds the functionality to send an entry or exit callback with
    the states being defined by the user.
    
    BUG=b:396174130
    TEST= verified passing twister -T zephyr/test/intelrvp
    		         --sub-test intelrvp.ptlrvp.board_id
    
    Change-Id: I1267f00753cacad3fe66971dbfb44ac8db6186bb

---
+++ zephyr/include/emul/emul_ap_callbacks.h
@@ -0,0 +1,43 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef EMUL_AP_CALLBACKS_H_
+#define EMUL_AP_CALLBACKS_H_
+
+#include <ap_power/ap_pwrseq.h>
+
+#define AP_PWRSEQ_STATES_MASK GENMASK(AP_POWER_STATE_COUNT - 1, 0)
+
+struct ap_pwrseq_cb_list {
+	uint32_t states;
+	sys_slist_t list;
+	struct k_spinlock lock;
+};
+
+/**
+ * @brief Emulate sending AP power sequence entry callback.
+ *
+ * @param dev Pointer of AP power sequence device driver.
+ * @param new State that you want to emulate entering.
+ * @param curr State that you want to emulate leaving.
+ *
+ * @retval None.
+ **/
+void ap_pwrseq_emul_entry_callback(const struct device *dev,
+				   enum ap_pwrseq_state new,
+				   enum ap_pwrseq_state curr);
+/**
+ * @brief Emulate sending AP power sequence entry callback.
+ *
+ * @param dev Pointer of AP power sequence device driver.
+ * @param new State that you want to emulate exiting.
+ * @param curr State that you want to emulate entering.
+ *
+ * @retval None.
+ **/
+void ap_pwrseq_emul_exit_callback(const struct device *dev,
+				  enum ap_pwrseq_state curr,
+				  enum ap_pwrseq_state new);
+#endif /* EMUL_AP_CALLBACKS_H_ */
--- zephyr/subsys/emul/ap_pwrseq/emul_ap_pwrseq.c
+++ zephyr/subsys/emul/ap_pwrseq/emul_ap_pwrseq.c
@@ -4,6 +4,7 @@
  */
 #include "ap_power/ap_pwrseq.h"
 #include "ap_power/ap_pwrseq_sm.h"
+#include "emul/emul_ap_callbacks.h"
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -14,6 +15,10 @@ LOG_MODULE_REGISTER(ap_pwrseq, CONFIG_AP_PWRSEQ_LOG_LEVEL);
 struct ap_pwrseq_data {
 	/* State machine data reference. */
 	void *sm_data;
+	/* State entry notification list. */
+	struct ap_pwrseq_cb_list entry_list;
+	/* State exit notification list. */
+	struct ap_pwrseq_cb_list exit_list;
 };
 
 static struct ap_pwrseq_data emul_ap_pwrseq_data;
@@ -63,6 +68,74 @@ int ap_pwrseq_start(const struct device *dev, enum ap_pwrseq_state init_state)
 	return ap_pwrseq_sm_init(data->sm_data, NULL, init_state);
 }
 
+static void ap_pwrseq_add_state_callback(struct ap_pwrseq_cb_list *cb_list,
+					 sys_snode_t *node)
+{
+	if (!sys_slist_is_empty(&cb_list->list)) {
+		sys_slist_find_and_remove(&cb_list->list, node);
+	}
+
+	sys_slist_prepend(&cb_list->list, node);
+}
+
+static int
+ap_pwrseq_register_state_callback(struct ap_pwrseq_state_callback *state_cb,
+				  struct ap_pwrseq_cb_list *cb_list)
+{
+	if (!(state_cb->states_bit_mask & AP_PWRSEQ_STATES_MASK)) {
+		return -EINVAL;
+	}
+
+	__ASSERT(state_cb->cb, "Callback pointer should not be NULL");
+
+	k_spinlock_key_t key = k_spin_lock(&cb_list->lock);
+
+	ap_pwrseq_add_state_callback(cb_list, &state_cb->node);
+
+	cb_list->states |= AP_PWRSEQ_STATES_MASK & state_cb->states_bit_mask;
+	k_spin_unlock(&cb_list->lock, key);
+
+	return 0;
+}
+
+static void ap_pwrseq_send_callback(const struct device *dev,
+				    const enum ap_pwrseq_state entry,
+				    const enum ap_pwrseq_state exit,
+				    bool is_entry)
+{
+	struct ap_pwrseq_data *const data = dev->data;
+	struct ap_pwrseq_cb_list *cb_list = is_entry ? &data->entry_list :
+						       &data->exit_list;
+	const enum ap_pwrseq_state *state = is_entry ? &entry : &exit;
+	struct ap_pwrseq_state_callback *state_cb, *tmp;
+
+	if (!(cb_list->states & BIT(*state))) {
+		return;
+	}
+	SYS_SLIST_FOR_EACH_CONTAINER_SAFE(&cb_list->list, state_cb, tmp, node)
+	{
+		if (state_cb->states_bit_mask & BIT(*state)) {
+			state_cb->cb(dev, entry, exit);
+		}
+	}
+}
+
+void ap_pwrseq_emul_entry_callback(const struct device *dev,
+				   enum ap_pwrseq_state new,
+				   enum ap_pwrseq_state curr)
+
+{
+	ap_pwrseq_send_callback(dev, new, curr, true);
+}
+
+void ap_pwrseq_emul_exit_callback(const struct device *dev,
+				  enum ap_pwrseq_state new,
+				  enum ap_pwrseq_state curr)
+
+{
+	ap_pwrseq_send_callback(dev, curr, new, false);
+}
+
 void ap_pwrseq_post_event(const struct device *dev, enum ap_pwrseq_event event)
 {
 	struct ap_pwrseq_data *const data = dev->data;
@@ -80,6 +153,7 @@ void ap_pwrseq_post_event(const struct device *dev, enum ap_pwrseq_event event)
 		 **/
 		ap_pwrseq_sm_run_state(data->sm_data, (uint32_t)BIT(event));
 		new_state = ap_pwrseq_sm_get_cur_state(data->sm_data);
+
 		if (cur_state == new_state) {
 			return;
 		}
@@ -94,4 +168,20 @@ enum ap_pwrseq_state ap_pwrseq_get_current_state(const struct device *dev)
 	ret_state = ap_pwrseq_sm_get_cur_state(data->sm_data);
 
 	return ret_state;
+}
+
+int ap_pwrseq_register_state_entry_callback(
+	const struct device *dev, struct ap_pwrseq_state_callback *state_cb)
+{
+	struct ap_pwrseq_data *data = dev->data;
+
+	return ap_pwrseq_register_state_callback(state_cb, &data->entry_list);
+}
+
+int ap_pwrseq_register_state_exit_callback(
+	const struct device *dev, struct ap_pwrseq_state_callback *state_cb)
+{
+	struct ap_pwrseq_data *data = dev->data;
+
+	return ap_pwrseq_register_state_callback(state_cb, &data->exit_list);
 }
