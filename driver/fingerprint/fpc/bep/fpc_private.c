9a50d4cd267d777019cbfd8e07f4368b11b6d05b
Firas Sammoura <fsammoura@google.com>
2025-02-25 21:32:43 +0000

    driver/fingerprint/fpc: Change frame_size and bpp for fpc1025
    The frame_size and bpp should be changed based on the captured image
    type. Enable fp_sensor_get_info to modulate the bpp and frame_size
    fields of the ec_response_fp_info based on the capture type.
    
    BUG=b:398899644
    TEST=Check info info output based on the set capture mode
    
    Change-Id: I3214365ac27b712b39a3b8785284be0ddbd32105

--- driver/fingerprint/fpc/bep/fpc_private.c
+++ driver/fingerprint/fpc/bep/fpc_private.c
@@ -266,11 +266,25 @@ static bool is_simple_capture(uint32_t capture_type)
 		capture_type == FP_CAPTURE_QUALITY_TEST);
 }
 
-int fp_sensor_get_info(struct ec_response_fp_info *resp)
+int fp_sensor_get_info(struct ec_response_fp_info *resp, uint32_t sensor_mode)
 {
 	uint16_t sensor_id;
 
 	memcpy(resp, &ec_fp_sensor_info, sizeof(struct ec_response_fp_info));
+
+	if (sensor_mode &
+	    ((FP_MODE_CAPTURE | FP_MODE_ENROLL_IMAGE | FP_MODE_MATCH))) {
+		int capture_type = FP_CAPTURE_TYPE(sensor_mode);
+
+		if (is_raw_capture(capture_type)) {
+			resp->bpp = 16;
+			resp->frame_size = 100;
+		}
+		if (is_simple_capture(capture_type)) {
+			resp->bpp = 8;
+			resp->frame_size = 50;
+		}
+	}
 
 	if (fpc_get_hwid(&sensor_id))
 		return EC_RES_ERROR;
