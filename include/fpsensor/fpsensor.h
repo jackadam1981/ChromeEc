af5a5b8987f6c8902871037d18d034199537c79a
Firas Sammoura <fsammoura@google.com>
2025-02-25 21:32:43 +0000

    driver/fingerprint: Enable the modulation of frame_size and bpp
    The frame_size and bpp should be changed based on the captured image
    type. Enable fp_sensor_get_info to modulate the bpp and frame_size
    fields of the ec_response_fp_info based on the capture type.
    
    BUG=b:398899644
    TEST=Check info info output based on the set capture mode
    
    Change-Id: I6c76529e6117d59436e411411ed806b5cc57d857

--- include/fpsensor/fpsensor.h
+++ include/fpsensor/fpsensor.h
@@ -94,7 +94,7 @@ int fp_sensor_deinit(void);
  * @return EC_SUCCESS on success
  * @return EC_RES_ERROR on error
  */
-int fp_sensor_get_info(struct ec_response_fp_info *resp);
+int fp_sensor_get_info(struct ec_response_fp_info *resp, uint32_t capture_type);
 
 /**
  * Put the sensor in its lowest power state.
