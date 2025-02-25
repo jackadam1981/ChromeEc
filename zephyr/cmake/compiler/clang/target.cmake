aebbb2c8428881c68184ecb98cb70dd7114f8803
Tom Hughes <tomhughes@chromium.org>
2025-02-25 15:51:36 -0800

    zephyr: Add support for RISCV clang toolchain
    BUG=b:407560601, b:172020503
    TEST=rm -rf build && zmake build --clobber minimal-it8xxx2
    
    Change-Id: I8531c3bc83d78e4d0aa70c32344dd92d33bfe210

--- zephyr/cmake/compiler/clang/target.cmake
+++ zephyr/cmake/compiler/clang/target.cmake
@@ -4,5 +4,10 @@
 
 include("${ZEPHYR_BASE}/cmake/compiler/clang/target.cmake")
 
+message("RTLIB_DIR: ${RTLIB_DIR}")
+#set_property(TARGET linker PROPERTY lib_include_dir "-L${RTLIB_DIR}/baremetal")
+set_property(TARGET linker PROPERTY lib_include_dir "-L/usr/lib64/clang/20/lib/baremetal")
+set_property(TARGET linker PROPERTY rt_library "-lclang_rt.builtins-riscv32")
+
 set(CMAKE_C_COMPILER "${CROSS_COMPILE}clang")
 set(CMAKE_CXX_COMPILER "${CROSS_COMPILE}clang++")
