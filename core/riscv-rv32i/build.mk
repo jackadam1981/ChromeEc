16dcbc25377e768e4fce4bc692eb05d8decfae5c
Tom Hughes <tomhughes@chromium.org>
2025-02-25 15:15:17 -0800

    core/riscv-rv32i: Add support for using clang RISC-V toolchain
    The default is still the coreboot gcc toolchain. clang can be enabled
    with CROSS_COMPILE_CC_NAME=clang.
    
    BUG=b:407560601, b:172020503
    TEST=make V=1 CROSS_COMPILE_CC_NAME=clang BOARD=it8xxx2_evb -j
    TEST=make V=1 BOARD=it8xxx2_evb -j
    
    Change-Id: Iaad13dc5b9a729ff3e836c86083091c566bf2e57

--- core/riscv-rv32i/build.mk
+++ core/riscv-rv32i/build.mk
@@ -28,5 +28,21 @@ CFLAGS_CPU+=-flto
 LDFLAGS_EXTRA+=-flto
 endif
 
+ifeq ($(CROSS_COMPILE_CC_NAME),clang)
+CFLAGS_CPU+=-Oz		# Like -Os (and thus -O2), but reduces code size further.
+# b/256193799: Reduce inline threshold to decrease code size.
+CFLAGS_CPU+=-Wl,-mllvm -Wl,-inline-threshold=-10
+# Explicitly specify libclang_rt.builtins so that its symbols are preferred
+# over libc's. This avoids duplicate symbol errors. See b/346309204 for details.
+clang_resource_dir:="$(shell $(CC) --print-resource-dir)"
+ifneq ($(.SHELLSTATUS),0)
+$(error Could not determine path to libclang_rt.builtins)
+endif
+LDFLAGS_EXTRA+=\
+	"$(clang_resource_dir)/lib/baremetal/libclang_rt.builtins-riscv32.a"
+else
+LDFLAGS_EXTRA+=-static-libgcc -lgcc
+endif
+
 core-y=cpu.o init.o panic.o task.o switch.o __builtin.o math.o
 core-$(CONFIG_IT8XXX2_MUL_WORKAROUND)+=__it8xxx2_arithmetic.o
