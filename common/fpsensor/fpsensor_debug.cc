b1e2ac5d774f129ea6ff4cc183d0b61ee381e8b5
Firas Sammoura <fsammoura@google.com>
2025-03-03 11:41:30 -0800

    fpsensor: Add get_sensor_bpp helper function
    BUG=b:400472408
    TEST=CQ
    LOW_COVERAGE_REASON=b:371647536 add tests later
    
    Change-Id: I8b3461232a9d52d1fb193aaf9c58b643fca4968c

--- common/fpsensor/fpsensor_debug.cc
+++ common/fpsensor/fpsensor_debug.cc
@@ -110,6 +110,20 @@ static enum ec_error_list fp_console_action(uint32_t mode)
 	return EC_ERROR_TIMEOUT;
 }
 
+static uint8_t get_sensor_bpp(void)
+{
+	ec_response_fp_info info;
+
+#if defined(HAVE_FP_PRIVATE_DRIVER) || defined(BOARD_HOST)
+	if (fp_sensor_get_info(&info) < 0)
+		return EC_ERROR_UNKNOWN;
+#else
+	return EC_ERROR_UNKNOWN;
+#endif
+
+	return info.bpp;
+}
+
 static int command_fpcapture(int argc, const char **argv)
 {
 	if (system_is_locked())
