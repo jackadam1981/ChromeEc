ab4c33d5dfa1a8ef8e5a780688b968bc3d9e0a26
Firas Sammoura <fsammoura@google.com>
2025-03-04 22:02:30 +0000

    zephyr: fingerprint: add egis driver
    This driver exposes fingerprint sensor functionality like detecting
    finger presence, acquiring finger image or running maintenance tasks.
    
    BUG=b:384565052
    TEST=None
    
    Change-Id: Ied6b12fa5baaaba966db418414df1318632ee0da

---
+++ zephyr/drivers/fingerprint/fingerprint_egis630.h
@@ -0,0 +1,29 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ZEPHYR_DRIVERS_FINGERPRINT_EGIS630_H_
+#define ZEPHYR_DRIVERS_FINGERPRINT_EGIS630_H_
+
+#include "fingerprint_egis630_private.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+
+#include <drivers/fingerprint.h>
+
+struct egis630_cfg {
+	struct spi_dt_spec spi;
+	struct gpio_dt_spec interrupt;
+	struct gpio_dt_spec reset_pin;
+	struct fingerprint_info info;
+};
+
+struct egis630_data {
+	const struct device *dev;
+	fingerprint_callback_t callback;
+	struct gpio_callback irq_cb;
+	uint16_t errors;
+};
+
+#endif /* ZEPHYR_DRIVERS_FINGERPRINT_EGIS630_H_ */
