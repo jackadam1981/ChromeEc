009e6d13afb63daf099a4dab2523d9ef2dae69e2
Firas Sammoura <fsammoura@google.com>
2025-03-04 00:29:55 +0000

    zephyr: fingerprint: add EGIS ec630 algorithm
    Add ec630 algorithm provided by EGIS, designed for EGIS630 sensor.
    In order to work, it requires EGIS private library.
    
    BUG=b:384565052
    TEST=none
    
    Change-Id: If5b5ea86300c2e014824968810377b174062aa97

---
+++ zephyr/subsys/fingerprint/alg/egis__ec630_bio_alg.h
@@ -0,0 +1,16 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __EGIS_EC630_BIO_ALG_H__
+#define __EGIS_EC630_BIO_ALG_H__
+
+/*
+ * Constant value for the enrollment data size
+ *
+ * Size of private fp_bio_enrollment_t
+ */
+#define FP_ALGORITHM_ENROLLMENT_SIZE 15
+
+#endif /* __EGIS_EC630_BIO_ALG_H__ */
