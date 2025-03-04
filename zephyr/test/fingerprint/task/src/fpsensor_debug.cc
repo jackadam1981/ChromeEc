797b423ac4f60d274180353837cbb265c25c4213
Firas Sammoura <fsammoura@google.com>
2025-03-04 20:33:14 +0000

    zephyr: test: Create unit test for upload_pgm_image with wrong bpp
    BUG=b:400472408
    TEST=./test/run_device_tests.py -b bloonchipper -f servo_micro -t
         fpsensor_debug --zephyr
    TEST=./twister -v -i -T zephyr/test/fingerprint/task
    
    Change-Id: I662e6f06b0183f87e3b58c1bd937c30c7b964eff

--- zephyr/test/fingerprint/task/src/fpsensor_debug.cc
+++ zephyr/test/fingerprint/task/src/fpsensor_debug.cc
@@ -172,3 +172,14 @@ ZTEST(fpsensor_debug, test_command_fpenroll)
 	int rv = shell_execute_cmd(get_ec_shell(), console_input);
 	zassert_equal(rv, EC_ERROR_ACCESS_DENIED);
 }
+
+enum ec_error_list upload_pgm_image(uint8_t *frame, uint8_t bpp);
+
+ZTEST(fpsensor_debug, test_upload_pgm_image_wrong_bpp)
+{
+	uint8_t frame[100] = { 0 };
+
+	zassert_equal(upload_pgm_image(frame, 0), EC_ERROR_UNKNOWN);
+	zassert_equal(upload_pgm_image(frame, 17), EC_ERROR_UNKNOWN);
+	zassert_equal(upload_pgm_image(frame, 24), EC_ERROR_UNKNOWN);
+}
