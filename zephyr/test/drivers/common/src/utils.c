970829afca92b71750794b81d49b2df8f7fa775d
Rob Barnes <robbarnes@google.com>
2025-03-04 18:45:34 -0700

    zephyr/test: Make utils.c compatible with CONFIG_SHELL_BACKEND_DUMMY=n
    utils.c assumes CONFIG_SHELL_BACKEND_DUMMY=y. Add ifdef around usage in utils.c
    
    BUG=None
    TEST=twister -s drivers.panic_log.immediate
    
    Change-Id: I71a50716161c39ff2826f2d78c07beefcc75820b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6579745
    Tested-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Boris Mittelberg <bmbm@google.com>

--- zephyr/test/drivers/common/src/utils.c
+++ zephyr/test/drivers/common/src/utils.c
@@ -23,7 +23,9 @@
 #include <zephyr/drivers/gpio/gpio_emul.h>
 #include <zephyr/kernel.h>
 #include <zephyr/shell/shell.h>
+#if defined(CONFIG_SHELL_BACKEND_DUMMY)
 #include <zephyr/shell/shell_dummy.h>
+#endif
 #include <zephyr/shell/shell_uart.h>
 #include <zephyr/ztest.h>
 
@@ -621,6 +623,7 @@ int emul_init_stub(const struct device *dev)
 #define DT_DRV_COMPAT zephyr_espi_emul_espi_host
 DT_INST_FOREACH_STATUS_OKAY(EMUL_STUB_DEVICE);
 
+#if defined(CONFIG_SHELL_BACKEND_DUMMY)
 void check_console_cmd(const char *cmd, const char *expected_output,
 		       const int expected_rv, const char *file, const int line)
 {
@@ -642,3 +645,4 @@ void check_console_cmd(const char *cmd, const char *expected_output,
 			     "Invalid console output %s", buffer);
 	}
 }
+#endif /* CONFIG_SHELL_BACKEND_DUMMY */
