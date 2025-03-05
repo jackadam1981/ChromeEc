0179f9e7c186ae673f4be168f07031a141b29c7b
Firas Sammoura <fsammoura@google.com>
2025-03-05 22:13:18 +0000

    zephyr: fingerprint: add EGIS630 sensor pal - memory
    BUG=b:384565052
    TEST=None
    
    Change-Id: Id0641555cc29f9b1028500faf7729ddbbf0192ea

--- zephyr/drivers/fingerprint/fingerprint_egis630_pal.h
+++ zephyr/drivers/fingerprint/fingerprint_egis630_pal.h
@@ -5,6 +5,7 @@
 #ifndef ZEPHYR_DRIVERS_FINGERPRINT_EGIS630_PAL_SENSOR_H_
 #define ZEPHYR_DRIVERS_FINGERPRINT_EGIS630_PAL_SENSOR_H_
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -178,5 +179,54 @@ void __unused plat_sleep_time(unsigned long timeInMs);
 #define TIME_MEASURE_STOP_AND_RESTART(name, x)
 #define TIME_MEASURE_RESET(name)
 #endif
+/**
+ * @brief Allocates a block of memory of a specified size and number of
+ * elements.
+ *
+ * @param[in] count The number of elements to allocate.
+ * @param[in] size The size of each element in bytes.
+ *
+ * @return Pointer to the allocated memory or NULL if the allocation failed.
+ */
+void *__unused sys_alloc(size_t count, size_t size);
+
+/**
+ * @brief Releases a block of shared memory.
+ *
+ * @param[in] data A pointer to the memory block to be released.
+ *
+ */
+void __unused sys_free(void *data);
+
+static inline void plat_free(void *x)
+{
+	sys_free(x);
+}
+
+// TODO (b/373435445): Combine PLAT_FREE and plat_free.
+/**
+ * @brief Deallocates memory and sets the provided pointer to NULL.
+ *
+ * @param[in] x A pointer to a pointer to the memory block to be freed.
+ *
+ */
+static __unused inline void PLAT_FREE(void **x)
+{
+	assert(x != NULL && *x != NULL);
+	plat_free(*x);
+	*x = NULL;
+}
+
+/**
+ * @brief Allocates a block of memory of the specified size.
+ *
+ * @param[in] size The size of the memory block to allocate, in bytes.
+ *
+ * @return Pointer to the allocated memory or NULL if the allocation failed.
+ */
+static __unused inline void *plat_alloc(size_t size)
+{
+	return sys_alloc(1, size);
+}
 
 #endif /* ZEPHYR_DRIVERS_FINGERPRINT_EGIS630_PAL_SENSOR_H_ */
