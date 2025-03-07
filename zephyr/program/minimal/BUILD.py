4692b4e0914d933ab0f9962fd72a9fbe108bcf93
Tom Hughes <tomhughes@chromium.org>
2025-03-07 11:29:38 -0800

    DO NOT SUBMIT: Stub out Andes AE350 minimal project
    BUG=none
    TEST=none
    
    Change-Id: I3f5309125a3d85c10978fb214bf08ad12b74a3a0

--- zephyr/program/minimal/BUILD.py
+++ zephyr/program/minimal/BUILD.py
@@ -29,6 +29,13 @@ register_rtk_project(
     kconfig_files=[here / "realtek.conf"],
 )
 
+register_binman_project(
+    project_name="minimal-ae350",
+    zephyr_board="adp_xc7k/ae350",
+    dts_overlays=[here / "ae350.dts"],
+    # supported_toolchains=["llvm", "zephyr"],
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="minimal-it8xxx2", addr=0xBFFE0)
