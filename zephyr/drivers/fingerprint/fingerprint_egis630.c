a8ad23b0dd9c75327b998e9315b0c4afbd6def70
Firas Sammoura <fsammoura@google.com>
2025-03-11 20:45:10 +0000

    zephyr: fingerprint: add egis630 driver test
    BUG=b:384565052
    TEST=./twister -v -i -T zephyr/test/fingerprint/drivers/egis630 \
    	--test fingerprint.drivers.egis630 -c
    
    Change-Id: I3de96acba2f7ac2c66937e40db6f7afba04e708b

--- zephyr/drivers/fingerprint/fingerprint_egis630.c
+++ zephyr/drivers/fingerprint/fingerprint_egis630.c
@@ -71,7 +71,7 @@ void egis_fp_reset_sensor(const struct egis630_cfg *cfg)
 	return;
 }
 
-static int convert_egis_get_image_error_code(egis_api_return_t code)
+int convert_egis_get_image_error_code(egis_api_return_t code)
 {
 	switch (code) {
 	case EGIS_API_IMAGE_QUALITY_GOOD:
