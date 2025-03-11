f209d6733f029bae03685218d7c3bfddd6e8fa05
Firas Sammoura <fsammoura@google.com>
2025-03-11 20:45:10 +0000

    zephyr: fingerprint: add egis630 driver test
    BUG=b:384565052
    TEST=./twister -v -i -T zephyr/test/fingerprint/drivers/egis630 \
    	--test fingerprint.drivers.egis630 -c
    
    Change-Id: I3de96acba2f7ac2c66937e40db6f7afba04e708b

--- zephyr/drivers/fingerprint/fingerprint_egis630_pal.c
+++ zephyr/drivers/fingerprint/fingerprint_egis630_pal.c
@@ -30,7 +30,7 @@ LOG_MODULE_REGISTER(egis630_pal, LOG_LEVEL_INF);
 K_HEAP_DEFINE(fp_driver_heap, CONFIG_FINGERPRINT_SENSOR_EGIS630_HEAP_SIZE);
 K_SEM_DEFINE(printf_buffer_lock, 1, 1);
 
-static char printf_buffer[256];
+COND_CODE_1(CONFIG_ZTEST, (), (static)) char printf_buffer[256];
 
 int __unused periphery_spi_write_read(uint8_t *write, uint32_t write_len,
 				      uint8_t *read, uint32_t read_len)
