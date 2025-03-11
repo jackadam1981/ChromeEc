fb1e10b5c20e3b1f7e84f1ced4278af46d28730b
Firas Sammoura <fsammoura@google.com>
2025-03-11 06:27:11 +0000

    zephyr: fingerprint: rename mode as capture_type
    It is really the capture type, not the sensor mode.
    
    BUG=b:402115606
    TEST=zmake build bloonchipper
    TEST=./twister -v -i -T zephyr/test/fingerprint -c
    
    Change-Id: I51b2455051ec774452e1352000017c8b5a8e04e4

--- zephyr/drivers/fingerprint/fingerprint_fpc1145_private.h
+++ zephyr/drivers/fingerprint/fingerprint_fpc1145_private.h
@@ -175,21 +175,24 @@ int fp_sensor_maintenance(uint8_t *image_data,
 #define FPC_SENSOR_LOW_COVERAGE 3
 
 /**
- * Acquires a fingerprint image with specific capture mode.
+ * Acquires a fingerprint image with specific capture type.
  *
- * Same as the fp_sensor_acquire_image function(),
- * except @p mode can be set to one of the fp_capture_type constants
- * to get a specific image type (e.g. a pattern) rather than the default one.
+ * This function captures a fingerprint image, allowing the caller to set a
+ * specific @p capture_type to one of the fp_capture_type constants. For capture
+ * types that require a finger presence (e.g. simple, qual test,etc.), this
+ * function is called once the finger has been detected and cover enough area of
+ * the sensor. Otherwise, it does the acquisition immediately (e.g. pattern0,
+ * reset, etc.).
  *
  * @param[out] image_data Image from sensor. Buffer must be allocated by
  * caller with size FP_SENSOR_IMAGE_SIZE.
- * @param mode  enum fp_capture_type
+ * @param capture_type  enum fp_capture_type
  *
  * @return 0 on success
  * @return negative value on error
  */
-int fp_sensor_acquire_image_with_mode(uint8_t *image_data,
-				      enum fingerprint_capture_type mode);
+int fp_sensor_acquire_image_with_mode(
+	uint8_t *image_data, enum fingerprint_capture_type capture_type);
 
 /**
  * Configure finger detection.
