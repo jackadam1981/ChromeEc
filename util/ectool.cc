0afa2704eb84414daa6d81313b345acf9283519b
Firas Sammoura <fsammoura@google.com>
2025-03-11 05:25:21 +0000

    ec/util: Generate 8-bit or 16-bit PGM ASCII output
    Use libec static method FrameToPgm from FpFrameCommand class to generate
    PGM ASCII output from both 8-bit and 16-bit frames.
    
    BUG=b:400472408
    TEST=make BOARD=host utils
    TEST=make buildall -j100
    
    Change-Id: I71e9402cdadf71c233c74f78ca8363a900bb78c4

--- util/ectool.cc
+++ util/ectool.cc
@@ -2179,16 +2179,16 @@ int cmd_fp_frame(int argc, char *argv[])
 		return 0;
 	}
 
-	/* Print 8-bpp PGM ASCII header */
-	printf("P2\n%d %d\n%d\n", r.width, r.height, (1 << r.bpp) - 1);
+	// Generate 8-bit or 16-bit PGM ASCII output.
+	auto frame_to_pgm = ec::FpFrameCommand::FrameToPgm(
+		fp_frame->data(),
+		{ .bpp = r.bpp, .width = r.width, .height = r.height });
 
-	uint8_t *ptr = fp_frame->data();
-	for (int y = 0; y < r.height; y++) {
-		for (int x = 0; x < r.width; x++, ptr++)
-			printf("%d ", *ptr);
-		printf("\n");
+	if (!frame_to_pgm.has_value()) {
+		fprintf(stderr, "Error: Failed to convert frame to PGM.\n");
+		return -1;
 	}
-	printf("# END OF FILE\n");
+	fprintf(stdout, frame_to_pgm.value());
 	return 0;
 }
 
