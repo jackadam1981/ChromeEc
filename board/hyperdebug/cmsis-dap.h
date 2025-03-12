96ad6991d538eeec2f66398bd8c2ad93b48b4b65
Jes B. Klinke <jbk@chromium.org>
2025-03-12 15:34:19 -0700

    board/hyperdebug: Prepare CMSIS-DAP for becoming common
    Introduce e.g. GPIO_JTAG_TCLK and use in the parts of cmsis-dap.c which
    will be moved into common/.
    
    Separate out functions for enabling/disabling pins and buffers, which
    will remain in board code.
    
    BUG=b:310714758
    TEST=make BOARD=hyperdebug
    
    Change-Id: I058d4b16f79c2d34d43e7cff4fd3391ed6f4771e

--- board/hyperdebug/cmsis-dap.h
+++ board/hyperdebug/cmsis-dap.h
@@ -1,7 +1,33 @@
-#ifndef _HYPERDEBUG_BOARD_UTIL__H_
-#define _HYPERDEBUG_BOARD_UTIL__H_
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef _HYPERDEBUG_BOARD_CMSIS_DAP__H_
+#define _HYPERDEBUG_BOARD_CMSIS_DAP__H_
 
 #include "queue.h"
+
+/*
+ * Methods to be implemented by board code:
+ */
+
+/* Enable output on pins, including any level-shifters or buffers. */
+void cmsis_dap_enable_jtag_pins(void);
+/* Restore pins to state prior to JTAG connection. */
+void cmsis_dap_disable_jtag_pins(void);
+
+/*
+ * Declaration of handlers of CMSIS-DAP vendor extension commands, implemented
+ * in other files besides cmsis-dap.c .
+ */
+void dap_goog_i2c(size_t peek_c);
+void dap_goog_i2c_device(size_t peek_c);
+void dap_goog_gpio(size_t peek_c);
+
+/*
+ * Methods and variables provided by common code:
+ */
 
 extern struct queue const cmsis_dap_tx_queue;
 extern struct queue const cmsis_dap_rx_queue;
@@ -23,13 +49,5 @@ bool cmsis_dap_unwind_requested(void);
  */
 void queue_blocking_add(struct queue const *q, const void *src, size_t count);
 void queue_blocking_remove(struct queue const *q, void *dest, size_t count);
-
-/*
- * Declaration of handlers of CMSIS-DAP vendor extension commands, implemented
- * in other files besides cmsis-dap.c .
- */
-void dap_goog_i2c(size_t peek_c);
-void dap_goog_i2c_device(size_t peek_c);
-void dap_goog_gpio(size_t peek_c);
 
 #endif
