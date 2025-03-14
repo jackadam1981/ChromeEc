f1c81d32e6ac26c2833f999016da50d360ec8517
Firas Sammoura <fsammoura@google.com>
2025-03-14 19:07:31 +0000

    fpsensor: Introduce fp_command_info version 2
    BUG=b:398899644
    TEST=None
    LOW_COVERAGE_REASON=b:398899644, test will be added later
    
    Change-Id: Icc1d539c15b07ce27b906143d452236a0b394daf

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -417,8 +417,51 @@ extern "C" void fp_task(void)
 #endif /* !HAVE_FP_PRIVATE_DRIVER */
 }
 
+static enum ec_status fp_command_info_v2(struct host_cmd_handler_args *args)
+{
+	struct ec_response_fp_info_v2 *r =
+		static_cast<ec_response_fp_info_v2 *>(args->response);
+
+#ifdef HAVE_FP_PRIVATE_DRIVER
+	if (fp_sensor_get_info_v2(r) < 0)
+#endif
+		return EC_RES_UNAVAILABLE;
+
+	r->template_info.template_size = FP_ALGORITHM_ENCRYPTED_TEMPLATE_SIZE;
+	r->template_info.template_max = FP_MAX_FINGER_COUNT;
+	r->template_info.template_valid = global_context.templ_valid;
+	r->template_info.template_dirty = global_context.templ_dirty;
+	r->template_info.template_version = FP_TEMPLATE_FORMAT_VERSION;
+
+	args->response_size = sizeof(struct ec_response_fp_info_v2) +
+			      (r->sensor_info.num_capture_types) *
+				      sizeof(struct fp_image_frame_params);
+
+	if (args->response_size > args->response_max) {
+		return EC_RES_OVERFLOW;
+	}
+
+	return EC_RES_SUCCESS;
+}
+
+__overridable int fp_sensor_get_info_v2(struct ec_response_fp_info_v2 *resp)
+{
+	*resp = {
+		.sensor_info = { .vendor_id = 0,
+				 .product_id = 0,
+				 .model_id = 0,
+				 .version = 0,
+				 .num_capture_types = 0,
+				 .errors = 0 },
+	};
+	return EC_SUCCESS;
+}
+
 static enum ec_status fp_command_info(struct host_cmd_handler_args *args)
 {
+	if (args->version == 2) {
+		return fp_command_info_v2(args);
+	}
 	auto *r = static_cast<ec_response_fp_info *>(args->response);
 
 #ifdef HAVE_FP_PRIVATE_DRIVER
