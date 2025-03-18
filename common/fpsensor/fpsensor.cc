4f9a6e3f9e4af5b5c7601e4fb4526e4a62d998b9
Firas Sammoura <fsammoura@google.com>
2025-03-18 20:53:45 +0000

    ectool: add version 2 of fp_info
    BUG=b:398899644
    TEST=None
    
    commit message for your changes. Lines starting
    
    Change-Id: I9602c1adc31c38928ceead420836cec5115aad97

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -433,7 +433,10 @@ static enum ec_status fp_command_info_v2(struct host_cmd_handler_args *args)
 	r->template_dirty = global_context.templ_dirty;
 	r->template_version = FP_TEMPLATE_FORMAT_VERSION;
 
-	args->response_size = sizeof(*r);
+	args->response_size =
+		sizeof(struct ec_response_fp_info_v2) +
+		(r->num_capture_types) * sizeof(struct image_frame_params);
+
 	return EC_RES_SUCCESS;
 }
 
