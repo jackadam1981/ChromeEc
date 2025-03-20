39f56357d985f51572b01a84e637758b80cfa60d
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2025-03-20 16:02:56 +0800

    hylia: Optimize bad battery strategy
    Put the bad battery register reading into the
    hook function to call it. Avoid the battery
    presence judgment time being too long.
    
    BUG=b:399557648
    TEST=zmake build hylia
    LOW_COVERAGE_REASON=Tests will be added in b:399557648
    
    Change-Id: Ic542ac1ca00156774427e6dae7a285016abfbaf3

--- zephyr/program/rauru/hylia/src/board.c
+++ zephyr/program/rauru/hylia/src/board.c
@@ -29,13 +29,15 @@ static void board_setup_init(void)
 }
 DECLARE_HOOK(HOOK_INIT, board_setup_init, HOOK_PRIO_PRE_DEFAULT);
 
-enum battery_present battery_is_present(void)
+static enum battery_present cached_batt_state = BP_YES;
+
+/*
+ * I2C read register to detect battery
+ */
+static void update_battery_state_cache(void)
 {
 	int state;
-
-	if (gpio_get_level(GPIO_BATT_PRES_ODL)) {
-		return BP_NO;
-	}
+	cached_batt_state = BP_YES;
 
 	/*
 	 *  According to the battery manufacturer's reply:
@@ -43,13 +45,26 @@ enum battery_present battery_is_present(void)
 	 *  If the 12th bit(Permanently Failure) is 1, it means a bad battery.
 	 */
 	if (sb_read(SB_MANUFACTURER_ACCESS, &state)) {
-		return BP_NO;
+		cached_batt_state = BP_NO;
 	}
 
 	/* Detect the 12th bit value */
 	if (state & BIT(12)) {
-		return BP_NO;
+		cached_batt_state = BP_NO;
+	}
+}
+/*
+ *  The battery_check function is called every second.
+ *  It checks the 12th bit of the battery register to see if the battery is bad.
+ *  If the battery is bad, the battery_is_present function will return BP_NO.
+ */
+DECLARE_HOOK(HOOK_SECOND, update_battery_state_cache, HOOK_PRIO_DEFAULT);
+
+enum battery_present battery_is_present(void)
+{
+	if ((!gpio_get_level(GPIO_BATT_PRES_ODL)) && cached_batt_state) {
+		return BP_YES;
 	}
 
-	return BP_YES;
+	return BP_NO;
 }
