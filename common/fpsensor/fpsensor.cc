3451c1727768b9cf12f871943a4cf1b08cacd105
Firas Sammoura <fsammoura@google.com>
2025-03-20 23:02:55 +0000

    fpsensor: Change return type of fp_sensor_get_info to enum ec_status
    BUG=b:404616462
    TEST=make buildall -j100
    
    Change-Id: Ia0c4c4fcbcbfc30ea207af07a66925987d263466

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -422,7 +422,7 @@ static enum ec_status fp_command_info(struct host_cmd_handler_args *args)
 	auto *r = static_cast<ec_response_fp_info *>(args->response);
 
 #ifdef HAVE_FP_PRIVATE_DRIVER
-	if (fp_sensor_get_info(r) < 0)
+	if (fp_sensor_get_info(r) != EC_RES_SUCCESS)
 #endif
 		return EC_RES_UNAVAILABLE;
 
