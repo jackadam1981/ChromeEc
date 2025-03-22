ec3fca1350e46cd078d396588c38f43280ee6242
Tristan Honscheid <honscheid@google.com>
2025-03-21 22:29:01 -0600

    brox: Enable runtime PDC port config
    Enable runtime PDC port configuration on the `brox` board (not lotso or
    jubilant). Add a board implementation of board_get_pdc_for_port() to
    select RTK vs TI drivers based on CBI SKU data.
    
    Update the devicetree with RTK and TI PDC options and mark them all as
    zephyr,deferred-init.
    
    This effectively deprecates the `brox-sku4` build, which may be removed
    in a subsequent CL.
    
    BUG=b:372910804
    TEST=Live test on device
    
    Change-Id: If19a74b920f366b99feffd9dafe072544f85bd35

--- zephyr/program/brox/src/usbc.c
+++ zephyr/program/brox/src/usbc.c
@@ -5,9 +5,14 @@
  * Brox USB-C board functions
  */
 
+#include "cros_board_info.h"
 #include "ec_commands.h"
 #include "host_command.h"
 #include "usb_pd.h"
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(brox_usbc, LOG_LEVEL_INF);
 
 enum ec_pd_port_location board_get_pd_port_location(int port)
 {
@@ -18,4 +23,66 @@ enum ec_pd_port_location board_get_pd_port_location(int port)
 		return EC_PD_PORT_LOCATION_LEFT_BACK;
 	}
 	return EC_PD_PORT_LOCATION_UNKNOWN;
+}
+
+int board_get_pdc_for_port(int port, const struct device **dev)
+{
+	int rv;
+	uint32_t sku_id;
+
+	if (dev == NULL) {
+		LOG_ERR("%s: Bad pointer", __func__);
+		return -EINVAL;
+	}
+
+	if (port < 0 || port >= CONFIG_USB_PD_PORT_MAX_COUNT) {
+		LOG_ERR("%s: Invalid port number (%u)", __func__, port);
+
+		*dev = NULL;
+		return -ERANGE;
+	}
+
+	rv = cbi_get_sku_id(&sku_id);
+	if (rv) {
+		LOG_ERR("%s: Cannot read CBI SKU ID: %d. PDC config unknown!",
+			__func__, rv);
+
+		*dev = NULL;
+		return -EIO;
+	}
+
+	switch (sku_id) {
+	case 0x01:
+	case 0x02:
+	case 0x03:
+	case 0x21:
+	case 0x22:
+	case 0x23:
+		/* RTK PDC */
+		switch (port) {
+		case 0:
+			*dev = DEVICE_DT_GET(DT_NODELABEL(pdc_power_p0));
+			return 0;
+		case 1:
+			*dev = DEVICE_DT_GET(DT_NODELABEL(pdc_power_p1));
+			return 0;
+		}
+		break;
+	case 0x24:
+		/* TI PDC */
+		switch (port) {
+		case 0:
+			*dev = DEVICE_DT_GET(DT_NODELABEL(pdc_power_p0_ti));
+			return 0;
+		case 1:
+			*dev = DEVICE_DT_GET(DT_NODELABEL(pdc_power_p1_ti));
+			return 0;
+		}
+		break;
+	}
+
+	LOG_ERR("%s: Cannot match SKU (0x%02x)", __func__, sku_id);
+
+	*dev = NULL;
+	return -ENOENT;
 }
