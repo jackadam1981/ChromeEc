1fd2b98fffd22bab893c73fd24bb9c7b4e6ca527
Tristan Honscheid <honscheid@google.com>
2025-03-21 19:44:42 -0600

    cme: dts: Adapt DT bindings and CME for multiple PDC options
    To support runtime selection of PDC drivers, named-usbc-port nodes must
    support storing a list of candidate PDC phandles --not just one. Change
    the type of the `pdc` property to a phandle array.
    
    Update the CME script to handle multiple PDC entries per port.
    
    Update DT macros to reference index 0 in the phandle array.
    USBC_PORT_FROM_PDC_DRIVER_NODE() is updated to handle searching through
    a phandle array to associate a driver to a port number.
    
    BUG=b:372910804
    TEST=./twister -T zephyr/test/pdc
    
    Change-Id: If46cead9849cebebfd7df4116efae36e6037304a

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -957,7 +957,9 @@ static ALWAYS_INLINE void pdc_thread(void *pdc_dev, void *unused1,
 	static struct pdc_data_t data_##inst = {                              \
 		.port.dev = DEVICE_DT_INST_GET(inst), /* Initial policy read  \
 							 from device tree */  \
-		.port.pdc = DEVICE_DT_GET(DT_INST_PROP(inst, pdc)),           \
+		.port.pdc = COND_CODE_1(                                      \
+			CONFIG_PDC_RUNTIME_PORT_CONFIG, (NULL),               \
+			DEVICE_DT_GET(DT_INST_PROP_BY_IDX(inst, pdc, 0))),    \
 		.port.una_policy.tcc = DT_STRING_TOKEN(                       \
 			DT_INST_PROP(inst, policy), unattached_rp_value),     \
 		.port.una_policy.cc_mode = DT_STRING_TOKEN(                   \
