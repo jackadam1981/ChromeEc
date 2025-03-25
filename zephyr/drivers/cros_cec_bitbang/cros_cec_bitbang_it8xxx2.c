7166565c058f5e2ea6c532eeffcfb4a0c6284bfc
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2025-03-25 17:41:15 +0800

    zephyr: driver: add cros_cec_bitbang api for it8xxx2
    Add cros_cec_bitbang driver for it8xxx2.
    
    BUG=b:406416769
    TEST=zmake build dirks --clobber
    
    Change-Id: I00555c16de23ed562883e5fd08f1e08bbffccaee

---
+++ zephyr/drivers/cros_cec_bitbang/cros_cec_bitbang_it8xxx2.c
@@ -0,0 +1,293 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#define DT_DRV_COMPAT cros_ec_cec_bitbang
+
+#include "cec.h"
+#include "driver/cec/bitbang.h"
+#include "ec_tasks.h"
+#include "gpio/gpio_int.h"
+#include "task.h"
+#include "timer.h"
+
+#include <zephyr/drivers/counter.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#include <drivers/cros_cec_bitbang.h>
+
+LOG_MODULE_REGISTER(cros_cec_bitbang, LOG_LEVEL_ERR);
+
+BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) == 1,
+	     "Only one instance of cros-ec,cec-bitbang should be defined.");
+
+#define counter_ite_dev DEVICE_DT_GET(DT_NODELABEL(counter0))
+
+/* Timestamp when the most recent interrupt occurred */
+static timestamp_t interrupt_time;
+
+/* Timestamp when the second most recent interrupt occurred */
+static timestamp_t prev_interrupt_time;
+
+/* Flag set when a transfer is initiated from the AP */
+static bool transfer_initiated;
+
+/* The capture edge we're waiting for */
+static enum cec_cap_edge expected_cap_edge;
+
+static int port_from_drv(void)
+{
+	int port;
+
+	for (port = 0; port < CEC_PORT_COUNT; port++) {
+		if (cec_config[port].drv == &bitbang_cec_drv) {
+			return port;
+		}
+	}
+
+	/*
+	 * If we don't find a match, return 0. The only way for this to happen
+	 * is a configuration error, e.g. an incorrect timer is specified in
+	 * board.c, and we assume static configuration is correct to improve
+	 * performance.
+	 */
+	return 0;
+}
+
+static int port_from_gpio_in(enum gpio_signal signal)
+{
+	int port;
+	const struct bitbang_cec_config *drv_config;
+
+	for (port = 0; port < CEC_PORT_COUNT; port++) {
+		if (cec_config[port].drv == &bitbang_cec_drv) {
+			drv_config = cec_config[port].drv_config;
+			if (drv_config->gpio_in == signal)
+				return port;
+		}
+	}
+
+	/*
+	 * If we don't find a match, return 0. The only way for this to happen
+	 * is a configuration error, e.g. an incorrect pin is mapped to
+	 * cec_gpio_interrupt in gpio.inc, and we assume static configuration
+	 * is correct to improve performance.
+	 */
+	return 0;
+}
+
+__override void cec_update_interrupt_time(int port)
+{
+	prev_interrupt_time = interrupt_time;
+	interrupt_time = get_time();
+}
+
+/*
+ * In chip/it83xx/cec_bitbang.c
+ * void cec_ext_timer_interrupt(enum ext_timer_sel ext_timer)
+ */
+void cec_ext_timer_interrupt(void)
+{
+	int port = port_from_drv();
+
+	if (transfer_initiated) {
+		transfer_initiated = false;
+		cec_event_tx(port);
+	} else {
+		cec_update_interrupt_time(port);
+		cec_event_timeout(port);
+	}
+}
+
+void cec_ext_top_timer_handler(const struct device *dev, void *user_data)
+{
+	cec_ext_timer_interrupt();
+}
+
+void cec_gpio_interrupt(enum gpio_signal signal)
+{
+	int port = port_from_gpio_in(signal);
+	int level;
+
+	cec_update_interrupt_time(port);
+
+	level = gpio_pin_get_dt(gpio_get_dt_spec(signal));
+	if (!((expected_cap_edge == CEC_CAP_EDGE_FALLING && level == 0) ||
+	      (expected_cap_edge == CEC_CAP_EDGE_RISING && level == 1)))
+		return;
+
+	cec_event_cap(port);
+}
+
+static void cros_cec_bitbang_it8xxx2_tmr_cap_start(const struct device *dev,
+						   int port,
+						   enum cec_cap_edge edge,
+						   int timeout)
+{
+	ARG_UNUSED(dev);
+
+	expected_cap_edge = edge;
+
+	if (timeout > 0) {
+		/*
+		 * Take into account the delay from when the interrupt occurs to
+		 * when we actually get here. Since the timing is done in
+		 * software, there is an additional unknown delay from when the
+		 * interrupt occurs to when the ISR starts. Empirically, this
+		 * seems to be about 100 us, so account for this too.
+		 */
+		int delay = counter_us_to_ticks(
+			counter_ite_dev,
+			(get_time().val - interrupt_time.val + 100));
+		int timer_count = timeout - delay;
+		struct counter_top_cfg top_cfg;
+
+		/*
+		 * Handle the case where the delay is greater than the timeout.
+		 * This should never actually happen for typical delay and
+		 * timeout values.
+		 */
+		if (timer_count < 0) {
+			timer_count = 0;
+			LOG_WRN("CEC%d warning: timer_count < 0", port);
+		}
+
+		/*
+		 * Start the timer and enable the timer interrupt
+		 * In chip/it83xx/cec_bitbang.c
+		 * ext_timer_ms(drv_config->timer, CEC_CLOCK_SOURCE, 1, 1,
+		 * timer_count, 0, 1);
+		 */
+		top_cfg.ticks = timer_count;
+		top_cfg.callback = cec_ext_top_timer_handler;
+		top_cfg.user_data = NULL;
+		top_cfg.flags = 0;
+		counter_set_top_value(counter_ite_dev, &top_cfg);
+	} else {
+		/*
+		 * In chip/it83xx/cec_bitbang.c
+		 * ext_timer_stop(drv_config->timer, 1);
+		 */
+		counter_stop(counter_ite_dev);
+	}
+}
+
+static void cros_cec_bitbang_it8xxx2_tmr_cap_stop(const struct device *dev,
+						  int port)
+{
+	ARG_UNUSED(dev);
+	const struct bitbang_cec_config *drv_config =
+		cec_config[port].drv_config;
+
+	gpio_pin_interrupt_configure_dt(gpio_get_dt_spec(drv_config->gpio_in),
+					GPIO_INT_DISABLE);
+
+	/*
+	 * In chip/it83xx/cec_bitbang.c
+	 * ext_timer_stop(drv_config->timer, 1);
+	 */
+	counter_stop(counter_ite_dev);
+}
+
+static int cros_cec_bitbang_it8xxx2_tmr_cap_get(const struct device *dev,
+						int port)
+{
+	ARG_UNUSED(dev);
+	return counter_us_to_ticks(counter_ite_dev, (interrupt_time.val -
+						     prev_interrupt_time.val));
+}
+
+static void cros_cec_bitbang_it8xxx2_debounce_enable(const struct device *dev,
+						     int port)
+{
+	ARG_UNUSED(dev);
+	const struct bitbang_cec_config *drv_config =
+		cec_config[port].drv_config;
+
+	gpio_pin_interrupt_configure_dt(gpio_get_dt_spec(drv_config->gpio_in),
+					GPIO_INT_DISABLE);
+}
+
+static void cros_cec_bitbang_it8xxx2_debounce_disable(const struct device *dev,
+						      int port)
+{
+	ARG_UNUSED(dev);
+	const struct bitbang_cec_config *drv_config =
+		cec_config[port].drv_config;
+
+	gpio_pin_interrupt_configure_dt(gpio_get_dt_spec(drv_config->gpio_in),
+					GPIO_INT_ENABLE | GPIO_INT_EDGE_BOTH);
+}
+
+static void cros_cec_bitbang_it8xxx2_trigger_send(const struct device *dev,
+						  int port)
+{
+	ARG_UNUSED(dev);
+	unsigned int key;
+
+	/* Elevate to interrupt context */
+	transfer_initiated = true;
+
+	/*
+	 * In chip/it83xx/cec_bitbang.c
+	 * task_trigger_irq(et_ctrl_regs[drv_config->timer].irq);
+	 */
+	key = irq_lock();
+	cec_ext_timer_interrupt();
+	irq_unlock(key);
+}
+
+static void cros_cec_bitbang_it8xxx2_enable_timer(const struct device *dev,
+						  int port)
+{
+	ARG_UNUSED(dev);
+	const struct bitbang_cec_config *drv_config =
+		cec_config[port].drv_config;
+
+	/*
+	 * Enable gpio interrupts. Timer interrupts will be enabled as needed by
+	 * cec_tmr_cap_start().
+	 */
+	gpio_pin_interrupt_configure_dt(gpio_get_dt_spec(drv_config->gpio_in),
+					GPIO_INT_ENABLE | GPIO_INT_EDGE_BOTH);
+}
+
+static void cros_cec_bitbang_it8xxx2_disable_timer(const struct device *dev,
+						   int port)
+{
+	ARG_UNUSED(dev);
+	cros_cec_bitbang_it8xxx2_tmr_cap_stop(dev, port);
+
+	interrupt_time.val = 0;
+	prev_interrupt_time.val = 0;
+}
+
+static void cros_cec_bitbang_it8xxx2_init_timer(const struct device *dev,
+						int port)
+{
+	ARG_UNUSED(dev);
+
+	/*
+	 * In chip/it83xx/cec_bitbang.c
+	 * ext_timer_ms(drv_config->timer, CEC_CLOCK_SOURCE, 0, 0, 0, 1,
+	 * 0);
+	 */
+}
+
+static DEVICE_API(cros_cec_bitbang, cros_cec_bitbang_ite_driver_api) = {
+	.tmr_cap_start = cros_cec_bitbang_it8xxx2_tmr_cap_start,
+	.tmr_cap_stop = cros_cec_bitbang_it8xxx2_tmr_cap_stop,
+	.tmr_cap_get = cros_cec_bitbang_it8xxx2_tmr_cap_get,
+	.debounce_enable = cros_cec_bitbang_it8xxx2_debounce_enable,
+	.debounce_disable = cros_cec_bitbang_it8xxx2_debounce_disable,
+	.trigger_send = cros_cec_bitbang_it8xxx2_trigger_send,
+	.enable_timer = cros_cec_bitbang_it8xxx2_enable_timer,
+	.disable_timer = cros_cec_bitbang_it8xxx2_disable_timer,
+	.init_timer = cros_cec_bitbang_it8xxx2_init_timer,
+};
+
+DEVICE_DT_INST_DEFINE(0, NULL, NULL, NULL, NULL, POST_KERNEL,
+		      CONFIG_APPLICATION_INIT_PRIORITY,
+		      &cros_cec_bitbang_ite_driver_api);
