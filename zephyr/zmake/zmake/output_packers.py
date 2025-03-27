325bb6dcc7cfe28a6e9702eefa4feb75b932801a
jason yuan <jasonyuan@google.com>
2025-03-27 13:29:28 -0700

    cme: skip on non-EC or early experimental projects
    Skip making component manifest on fingerprint boards, detachable
    keyboards, and early experimental boards which may not have i2c defined.
    
    BUG=b:310998625
    TEST=zmake build --all --clobber
    
    Change-Id: Ifc14706d6773d371e31d33c75abcec6ca95edf87

--- zephyr/zmake/zmake/output_packers.py
+++ zephyr/zmake/zmake/output_packers.py
@@ -278,10 +278,11 @@ class BinmanPacker(BasePacker):
         yield ro_dir / "zephyr" / "zephyr.lst", "zephyr.ro.lst"
         yield rw_dir / "zephyr" / "zephyr.elf", "zephyr.rw.elf"
         yield rw_dir / "zephyr" / "zephyr.lst", "zephyr.rw.lst"
-        yield (
-            rw_dir / "zephyr" / "component_manifest.json",
-            "component_manifest.json",
-        )
+        if os.path.exists(rw_dir / "zephyr" / "component_manifest.json"):
+            yield (
+                rw_dir / "zephyr" / "component_manifest.json",
+                "component_manifest.json",
+            )
 
         token_db_name = "database.bin"
         token_paths = [
