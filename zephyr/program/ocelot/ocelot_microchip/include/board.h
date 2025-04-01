eed974788c196eaf3ed2abc4fa75a1fa3e218ece
Megha Verma <megha.verma@intel.corp-partner.google.com>
2025-04-01 15:06:06 +0530

    ocelot_microchip: Add support for AP power sequence
    BUG=b:396318819
    TEST=Use command "powerinfo" on EC terminal
        Able to query current power state
        Use command "powerindebug" on EC terminal
        Able to query power signals current state
    LOW_COVERAGE_REASON=Coverage to be handled in b/396318819
    
    Change-Id: Ifca8ad46c1a7e7bcade44645770bc9ba39e568ca

---
+++ zephyr/program/ocelot/ocelot_microchip/include/board.h
@@ -0,0 +1,18 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef OCELOT_MICROCHIP_BOARD_H_
+#define OCELOT_MICROCHIP_BOARD_H_
+
+/* Power Signals */
+#define PWR_EN_PP3300_S5 MCHP_GPIO_DECODE_226
+#define PWR_EN_PP5000_S5 MCHP_GPIO_DECODE_244
+#define PWR_RSMRST_PWRGD MCHP_GPIO_DECODE_012
+#define PWR_EC_PCH_RSMRST MCHP_GPIO_DECODE_054
+#define PWR_SLP_S0 MCHP_GPIO_DECODE_052
+#define PWR_PCH_PWROK MCHP_GPIO_DECODE_100
+#define PWR_ALL_SYS_PWRGD MCHP_GPIO_DECODE_057
+
+#endif /* OCELOT_MICROCHIP_BOARD_H_ */
