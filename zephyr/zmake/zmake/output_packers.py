ab3b0da321da966376963a2f4f21434c2b0a1f28
jason yuan <jasonyuan@google.com>
2025-04-02 15:39:45 -0700

    cme: make components without compatibles output error
    Change CME to output error and terminate build if a component is
    detected without a compatible.
    
    BUG=b:310998625
    TEST=zmake build --all --clobber
    
    Change-Id: I19b2c6c17596a93ad85b1b88fd736b21a8cc81ae

--- zephyr/zmake/zmake/output_packers.py
+++ zephyr/zmake/zmake/output_packers.py
@@ -137,10 +137,6 @@ class IshBinPacker(BasePacker):
         del version_string
         single_dir = dir_map["singleimage"]
         yield single_dir / "zephyr" / "ish_fw.bin", "ish_fw.bin"
-        yield (
-            single_dir / "zephyr" / "component_manifest.json",
-            "component_manifest.json",
-        )
 
 
 class BinmanPacker(BasePacker):
