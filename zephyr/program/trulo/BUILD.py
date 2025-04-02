af4a8b2edfa482fca5a99fa5e7cd2ffe9e2cd861
mike <mike5@huaqin.corp-partner.google.com>
2025-04-02 15:15:17 +0800

    trulo: Initial zephyr config for pujjocento
    Initial EC zephyr config for Trulo/Pujjocento
    
    BUG=none
    TEST=zmake build pujjocento pass
    LOW_COVERAGE_REASON=initial bring-up
    
    Change-Id: I4c813d028a0113e792ae5b97368abdda06ae600f

--- zephyr/program/trulo/BUILD.py
+++ zephyr/program/trulo/BUILD.py
@@ -55,8 +55,20 @@ register_trulo_project(
     ],
 )
 
+register_trulo_project(
+    project_name="pujjocento",
+    chip="npcx9/npcx9m7fb",
+    kconfig_files=[
+        # Common to all projects.
+        here / "program.conf",
+        # Parent project's config
+        here / "pujjocento" / "project.conf",
+    ],
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="trulo", addr=0x40144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="pujjolo", addr=0x40144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="uldrenite", addr=0x40144)
+assert_rw_fwid_DO_NOT_EDIT(project_name="pujjocento", addr=0x40144)
