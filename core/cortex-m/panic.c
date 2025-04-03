49e2304b7bf73161602c664f1a1e82e5e4a0a40d
Rob Barnes <robbarnes@google.com>
2025-04-03 14:47:43 -0600

    cortex-m: Clean up exception stack and mode handling
    The watchdog_trace handler was only checking for 0xfffffff1 exceptions
    when determining which stack the exception frame is in. This resulted
    in a bad PC being populated in the panicinfo, often being the stack
    canary deadd00d. The check should be for 0xfffffff1 or 0xfffffff9.
    
    Since is_frame_in_handler_stack in cortex-m/panic.c already performs
    this check, it was  moved to cpu.h so both watchdog.c and panic.c
    can utilize it.
    
    is_exception_from_handler_mode was also added for checking which mode
    the exception occurred in. Cleaned up a few places where exception stack
    or mode was being checked.
    
    BUG=b:405199532,b:378342175
    TEST=PC correct on watchdog warning
    
    Change-Id: I01c7c74b964811089bfb51c0c174ef40a3180ba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6432106
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>

--- core/cortex-m/panic.c
+++ core/cortex-m/panic.c
@@ -65,17 +65,6 @@ static void print_reg(int regnum, const uint32_t *regs, int index)
 	panic_puts((regnum & 3) == 3 ? "\n" : " ");
 }
 
-/*
- * Returns non-zero if the exception frame was created on the main stack, or
- * zero if it's on the process stack.
- *
- * See B1.5.8 "Exception return behavior" of ARM DDI 0403D for details.
- */
-static int32_t is_frame_in_handler_stack(const uint32_t exc_return)
-{
-	return (exc_return & 0xf) == 1 || (exc_return & 0xf) == 9;
-}
-
 #ifdef CONFIG_DEBUG_EXCEPTIONS
 /* Names for each of the bits in the cfs register, starting at bit 0 */
 static const char * const cfsr_name[32] = {
@@ -261,15 +250,15 @@ void panic_data_print(const struct panic_data *pdata)
 {
 	const uint32_t *lregs = pdata->cm.regs;
 	const uint32_t *sregs = NULL;
-	const int32_t in_handler =
-		is_frame_in_handler_stack(pdata->cm.regs[11]);
+	const uint32_t excep_lr = lregs[11];
 	int i;
 
 	if (pdata->flags & PANIC_DATA_FLAG_FRAME_VALID)
 		sregs = pdata->cm.frame;
 
 	panic_printf("\n=== %s EXCEPTION: %02x ====== xPSR: %08x ===\n",
-		     in_handler ? "HANDLER" : "PROCESS",
+		     is_exception_from_handler_mode(excep_lr) ? "HANDLER" :
+								"PROCESS",
 		     lregs[1] & 0xff, sregs ? sregs[7] : -1);
 	for (i = 0; i < 4; i++)
 		print_reg(i, sregs, i);
@@ -278,9 +267,10 @@ void panic_data_print(const struct panic_data *pdata)
 	print_reg(10, lregs, 9);
 	print_reg(11, lregs, 10);
 	print_reg(12, sregs, 4);
-	print_reg(13, lregs, in_handler ? 2 : 0);
+	print_reg(13, lregs, is_frame_in_handler_stack(excep_lr) ? 2 : 0);
 	print_reg(14, sregs, 5);
 	print_reg(15, sregs, 6);
+
 
 #ifdef CONFIG_DEBUG_EXCEPTIONS
 	panic_show_extra(pdata);
