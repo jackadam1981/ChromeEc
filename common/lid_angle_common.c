b75de1eee1fa91b36e8151c2d040fc1c96b29840
Deshatty, Deepti <deepti.deshatty@intel.corp-partner.google.com>
2025-04-08 12:27:48 +0530

    common: handle keyboard suspend and resume
    move key scan suspend and resume handling to common
    file so that the changes are applicable to the designs
    where ec sensing and GMR sensor configs are disabled.
    
    Without these keyboard remains active during suspend
    state causing DUT to wake up with any key press.
    
    BUG=b:409240881
    TEST=zmake build trulo-ti
    In tablet mode suspend the DUT.
    While DUT is in suspend mode, press any key on the onboard
    keyboard. The DUT does not waking up.
    LOW_COVERAGE_REASON=Coverage to be handled in b:418697480
    
    Change-Id: I120a7895703635e91e39b93f2ae228bf906630e4

--- common/lid_angle_common.c
+++ common/lid_angle_common.c
@@ -5,6 +5,7 @@
 
 #include "chipset.h"
 #include "common.h"
+#include "hooks.h"
 #include "keyboard_scan.h"
 #include "lid_angle.h"
 #include "tablet_mode.h"
@@ -36,3 +37,24 @@ __overridable void lid_angle_peripheral_enable(int enable)
 		}
 	}
 }
+
+static void enable_peripherals(void)
+{
+	/*
+	 * Make sure lid angle is not disabling peripherals when AP is running.
+	 */
+	lid_angle_peripheral_enable(1);
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, enable_peripherals, HOOK_PRIO_DEFAULT);
+
+#ifdef CONFIG_TABLET_MODE
+static void suspend_peripherals(void)
+{
+	/*
+	 * Make sure peripherals are disabled in S3 in tablet mode.
+	 */
+	if (tablet_get_mode())
+		lid_angle_peripheral_enable(0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, suspend_peripherals, HOOK_PRIO_DEFAULT);
+#endif /* CONFIG_TABLET_MODE */
