e8415e7a2276d68d088fa59591bfcb0228950667
Tristan Honscheid <honscheid@google.com>
2025-04-08 15:01:07 -0600

    pdc: rts5453: Add console-based PDC FW update mechanism
    To allow engineers to flash the RTK PDC on boards without needing to
    prepare AP FW images for development purposes, create a mechanism to
    stream FW updates over the EC console from a host. See subsequent CL
    for a Python script that is able to interact with this new CLI interface
    (pdc_rtk_pdc) and transfer a PDC FW image.
    
    This requires that USBC_PDC_RTS54XX_CONSOLE_FW_UPDATER be enabled.
    
    BUG=None
    TEST=Successfully reflashed PDC
    
    Change-Id: I62d917023ad1ae41a06e8a381d3a29e67ef56165

--- zephyr/subsys/pd_controller/pdc_console.c
+++ zephyr/subsys/pd_controller/pdc_console.c
@@ -767,7 +767,7 @@ static int cmd_pdc_sbu_mux_mode(const struct shell *sh, size_t argc,
 /* LCOV_EXCL_START - non-shipping code */
 extern int tps_pdc_do_firmware_update(void);
 
-static int cmd_pdc_fwupdate(const struct shell *sh, size_t argc, char **argv)
+static int cmd_pdc_ti_fwupdate(const struct shell *sh, size_t argc, char **argv)
 {
 	int rv;
 
@@ -874,7 +874,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 	SHELL_CMD_ARG(fwupdate, NULL,
 		      "Updates TPS6699x firmware\n"
 		      "Usage pdc fwupdate",
-		      cmd_pdc_fwupdate, 1, 0),
+		      cmd_pdc_ti_fwupdate, 1, 0),
 #endif /* defined(CONFIG_USBC_PDC_TPS6699X_FW_UPDATER) */
 	SHELL_COND_CMD_ARG(IS_ENABLED(CONFIG_USBC_PDC_TRACE_MSG_CONSOLE_CMD),
 			   trace, NULL,
