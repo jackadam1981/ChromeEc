1dc3c25e628c838c02a269e8daefa35c7f9bc2fd
Firas Sammoura <fsammoura@google.com>
2025-04-08 03:24:27 +0000

    driver/fingerprint/fpc: add fpc bep implementation for fpinfo v2
    BUG=b:398899644
    TEST=None
    
    Change-Id: I263323963e31f441cffe5aa35bbb8b6d8577a0f5

--- driver/fingerprint/fpc/bep/fpc_private.c
+++ driver/fingerprint/fpc/bep/fpc_private.c
@@ -56,6 +56,72 @@ static struct ec_response_fp_info ec_fp_sensor_info = {
 	.bpp = FP_SENSOR_RES_BPP_FPC,
 };
 
+/* Sensor description */
+static struct fp_sensor_info fpc_sensor_info_2 = {
+	/* Sensor identification */
+	.vendor_id = FOURCC('F', 'P', 'C', ' '),
+	.product_id = 9,
+	.model_id = 1,
+	.version = 1,
+};
+
+static const struct fp_image_frame_params fpc_image_frame_params[6] = {
+	/* FPC_CAPTURE_VENDOR_FORMAT */
+	{
+		.bpp = FP_SENSOR_RES_BPP_FPC,
+		.frame_size = FP_SENSOR_IMAGE_SIZE_FPC,
+		.pixel_format = V4L2_PIX_FMT_GREY,
+		.width = FP_SENSOR_RES_X_FPC,
+		.height = FP_SENSOR_RES_Y_FPC,
+		.fp_capture_type = FP_CAPTURE_VENDOR_FORMAT,
+	},
+	/* FPC_CAPTURE_SIMPLE_IMAGE */
+	{
+		.bpp = FP_SENSOR_RES_BPP_FPC,
+		.frame_size = FP_SENSOR_IMAGE_SIZE_FPC,
+		.pixel_format = V4L2_PIX_FMT_GREY,
+		.width = FP_SENSOR_RES_X_FPC,
+		.height = FP_SENSOR_RES_Y_FPC,
+		.fp_capture_type = FP_CAPTURE_SIMPLE_IMAGE,
+	},
+	/* FPC_CAPTURE_PATTERN0 */
+	{
+		.bpp = FP_SENSOR_RES_BPP_FPC,
+		.frame_size = FP_SENSOR_IMAGE_SIZE_FPC,
+		.pixel_format = V4L2_PIX_FMT_GREY,
+		.width = FP_SENSOR_RES_X_FPC,
+		.height = FP_SENSOR_RES_Y_FPC,
+		.fp_capture_type = FP_CAPTURE_PATTERN0,
+	},
+	/* FPC_CAPTURE_PATTERN1 */
+	{
+		.bpp = FP_SENSOR_RES_BPP_FPC,
+		.frame_size = FP_SENSOR_IMAGE_SIZE_FPC,
+		.pixel_format = V4L2_PIX_FMT_GREY,
+		.width = FP_SENSOR_RES_X_FPC,
+		.height = FP_SENSOR_RES_Y_FPC,
+		.fp_capture_type = FP_CAPTURE_PATTERN1,
+	},
+	/* FPC_CAPTURE_QUALITY_TEST */
+	{
+		.bpp = FP_SENSOR_RES_BPP_FPC,
+		.frame_size = FP_SENSOR_IMAGE_SIZE_FPC,
+		.pixel_format = V4L2_PIX_FMT_GREY,
+		.width = FP_SENSOR_RES_X_FPC,
+		.height = FP_SENSOR_RES_Y_FPC,
+		.fp_capture_type = FP_CAPTURE_QUALITY_TEST,
+	},
+	/* FPC_CAPTURE_RESET_TEST */
+	{
+		.bpp = FP_SENSOR_RES_BPP_FPC,
+		.frame_size = FP_SENSOR_IMAGE_SIZE_FPC,
+		.pixel_format = V4L2_PIX_FMT_GREY,
+		.width = FP_SENSOR_RES_X_FPC,
+		.height = FP_SENSOR_RES_Y_FPC,
+		.fp_capture_type = FP_CAPTURE_RESET_TEST,
+	},
+};
+
 static enum fpc_capture_type
 convert_fp_capture_type_to_fpc_capture_type(enum fp_capture_type mode)
 {
@@ -264,6 +330,27 @@ int fp_sensor_get_info(struct ec_response_fp_info *resp)
 
 	resp->model_id = sensor_id;
 	resp->errors = errors;
+
+	return EC_SUCCESS;
+}
+
+int fp_sensor_get_info_v2(struct ec_response_fp_info_v2 *resp)
+{
+	uint16_t sensor_id;
+	memcpy(&resp->sensor_info, &fpc_sensor_info_2,
+	       sizeof(struct fp_sensor_info));
+
+	memcpy(&resp->image_frame_params, &fpc_image_frame_params,
+	       sizeof(fpc_image_frame_params));
+
+	if (fpc_get_hwid(&sensor_id))
+		return EC_RES_ERROR;
+
+	resp->sensor_info.model_id = sensor_id;
+	resp->sensor_info.errors = errors;
+	resp->sensor_info.num_capture_types =
+		sizeof(fpc_image_frame_params) /
+		sizeof(struct fp_image_frame_params);
 
 	return EC_SUCCESS;
 }
