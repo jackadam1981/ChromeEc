aa6893490580223c21ea20aaf620a58d2ac28fff
Mary Ruthven <mruthven@chromium.org>
2025-04-11 13:23:22 -0500

    cr50: add support for pulsing EC_RST_L in rec+dev mode
    Add support for pulsing EC_RST_L in rec+dev mode if the FWMP is blocking
    dev mode. Add a allowlist to enable the feature on certain boards based
    on their board id type.
    
    Right now the code to pulse EC_RST_L is not compiled in. You can add it
    by enabling the CONFIG_FWMP_BLOCK_REC_DEV_RESET_EC flag.
    
    BUG=b:390275394
    TEST=reset the EC in rec+dev mode on boards that want to reset the EC
    when the device tries to enter rec+dev mode
    
    Cq-Depend: chromium:6496290
    Change-Id: Iec747abf7e0b6ea8ce9d208ce2b52d3b14deb975

--- board/cr50/build.mk
+++ board/cr50/build.mk
@@ -212,6 +212,7 @@ board-y += tpm2/trng.o
 board-y += tpm2/virtual_nvmem.o
 board-y += tpm_nvmem_ops.o
 board-y += wp.o
+board-${CONFIG_FWMP_BLOCK_REC_DEV_RESET_EC} += check_pcr0.o
 board-$(CONFIG_PINWEAVER)+=pinweaver_tpm_imports.o
 board-$(CONFIG_PLATFORM_PINWEAVER)+=pinweaver_tpm_imports.o
 board-$(CONFIG_PLATFORM_BOOT_PARAM)+=boot_param_platform.o
