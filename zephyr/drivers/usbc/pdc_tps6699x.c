8de62bd9be0da309c770e11b35e30ae51a1f29aa
Matt_Wang <Matt_Wang@compal.corp-partner.google.com>
2025-04-14 18:30:46 +0800

    pdc: refactor the ccd to port config
    Move CCD control from USB device tree to I2C device tree.
    
    BUG=b:408987444
    TEST=ccd follow the setting
    
    Change-Id: I5e5c719ec5d6399c3a203a3b3e1cb278ff66c89c

--- zephyr/drivers/usbc/pdc_tps6699x.c
+++ zephyr/drivers/usbc/pdc_tps6699x.c
@@ -197,6 +197,8 @@ struct pdc_config_t {
 	void (*create_thread)(const struct device *dev);
 	/** If true, do not apply PDC FW updates to this port */
 	bool no_fw_update;
+	/** Whether or not this port supports CCD */
+	bool ccd;
 };
 
 /**
@@ -1403,6 +1405,7 @@ static int cmd_get_ic_status_sync_internal(struct pdc_config_t const *cfg,
 	info->driver_name[sizeof(info->driver_name) - 1] = '\0';
 
 	info->no_fw_update = cfg->no_fw_update;
+	info->ccd = cfg->ccd;
 
 	return 0;
 }
@@ -2807,6 +2810,7 @@ static void tps_thread(void *dev, void *unused1, void *unused2)
 		.bits.sink_path_status_change = 1,                             \
 		.create_thread = create_thread_##inst,                         \
 		.no_fw_update = DT_INST_PROP(inst, no_fw_update),              \
+		.ccd = DT_INST_PROP(inst, ccd),                                \
 	};                                                                     \
                                                                                \
 	DEVICE_DT_INST_DEFINE(inst, pdc_init, NULL, &pdc_data_##inst,          \
