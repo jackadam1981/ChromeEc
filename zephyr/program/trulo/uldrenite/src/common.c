ded6dd57a074f6274a822aff38aabc835424041b
Yuval Peress <peress@google.com>
2025-04-15 18:47:00 +0000

    uldrenite: Remove sensors from EC
    Remove all the sensor logic from the EC. It will be added back in the
    ISH.
    
    BUG=b:410646823
    TEST=zmake build uldrenite
    
    Change-Id: Idf47269bc72088ed145604824088f781250cbdd1

--- zephyr/program/trulo/uldrenite/src/common.c
+++ zephyr/program/trulo/uldrenite/src/common.c
@@ -2,7 +2,15 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+
+#include "cros_cbi.h"
+#include "hooks.h"
 #include "system.h"
+#include "tablet_mode.h"
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(uldrenite, CONFIG_LOG_DEFAULT_LEVEL);
 
 /* Trigger shutdown by enabling the Z-sleep circuit */
 __override void board_hibernate_late(void)
@@ -15,3 +23,21 @@ __override void board_hibernate_late(void)
 	 */
 #endif
 }
+
+static void gmr_tablet_init(void)
+{
+	static int sensor_fwconfig;
+	int ret;
+
+	ret = cros_cbi_get_fw_config(FORM_FACTOR, &sensor_fwconfig);
+	if (ret < 0) {
+		LOG_ERR("error retriving CBI config: %d", ret);
+		return;
+	}
+
+	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
+		gmr_tablet_switch_disable();
+	}
+}
+
+DECLARE_HOOK(HOOK_INIT, gmr_tablet_init, HOOK_PRIO_DEFAULT);
