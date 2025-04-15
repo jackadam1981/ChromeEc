9b2e998405ae77efcdfa9cbf6c15f6a6d127af6c
Yuval Peress <peress@google.com>
2025-04-15 21:07:59 +0000

    uldrenite-ish: Add build target
    Migrate the sensor logic that previously lived in uldrenite into the
    uldrenite-ish target. motionsense.dtsi is recycled since all the
    nodes are abstracted. This way we can ensure that the EC and ISH
    versions are configured the same.
    
    BUG=b:410646241
    TEST=zmake build uldrenite-ish
    
    Change-Id: Ic3bcce4896d61781a6ee690b6d48fb7664e7015d

--- zephyr/program/trulo/uldrenite/src/sensor.c
+++ zephyr/program/trulo/uldrenite/src/sensor.c
@@ -21,7 +21,7 @@
 
 #define I2C_PORT_SENSOR 1
 
-LOG_MODULE_DECLARE(trulo, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(uldrenite_sensor, LOG_LEVEL_INF);
 
 enum base_sensor_type {
 	base_lis2dw12 = 0,
@@ -50,24 +50,32 @@ void lid_accel_interrupt(enum gpio_signal signal)
 
 static void motionsense_init(void)
 {
-	int ish_enabled;
-	int ret = cros_cbi_get_fw_config(ISH, &ish_enabled);
+	int ret;
 
-	if (ret < 0) {
-		LOG_ERR("Failed to load ISH config: %d", ret);
-		return;
-	}
+	if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
+		int ish_enabled;
+		ret = cros_cbi_get_fw_config(ISH, &ish_enabled);
 
-	if (ish_enabled == ISH_ENABLED) {
-		motion_sensor_count = 0;
-		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
-		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
-		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
-				      GPIO_DISCONNECTED);
-		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
-				      GPIO_DISCONNECTED);
-		LOG_INF("No motionsense");
-		return;
+		if (ret < 0) {
+			LOG_ERR("Failed to load ISH config: %d", ret);
+			return;
+		}
+
+		if (ish_enabled == ISH_ENABLED) {
+			motion_sensor_count = 0;
+			gpio_disable_dt_interrupt(
+				GPIO_INT_FROM_NODELABEL(int_imu));
+			gpio_disable_dt_interrupt(
+				GPIO_INT_FROM_NODELABEL(int_lid_imu));
+			gpio_pin_configure_dt(
+				GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
+				GPIO_DISCONNECTED);
+			gpio_pin_configure_dt(
+				GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
+				GPIO_DISCONNECTED);
+			LOG_INF("No motionsense");
+			return;
+		}
 	}
 
 	ret = cros_cbi_get_fw_config(FORM_FACTOR, &sensor_fwconfig);
@@ -77,7 +85,10 @@ static void motionsense_init(void)
 	}
 
 	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
-		gmr_tablet_switch_disable();
+		if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
+			/* Only valid in EC */
+			gmr_tablet_switch_disable();
+		}
 		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
 		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
 				      GPIO_INPUT | GPIO_PULL_UP);
