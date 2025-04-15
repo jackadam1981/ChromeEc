479301bacbe1bcb0ef5e1667430d93f56d12ac65
Yuval Peress <peress@google.com>
2025-04-15 16:29:27 +0000

    dsp_comms: Use AP power sequencing
    We don't need to enable the DSP comms service until the AP is ready
    to boot. Even then, we only need to do it once. Convert the SYS_INIT
    to use the AP_POWER_STARTUP callback.
    
    BUG=b:410646711
    TEST=zmake build uldrenite trulo
    
    Change-Id: Ia35e7023f7cfe5f7c37e24de966bd402b74c170a

--- zephyr/include/ap_power/ap_power_events.h
+++ zephyr/include/ap_power/ap_power_events.h
@@ -13,11 +13,19 @@
 
 #include <ap_power/ap_power.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * @brief Dispatch callbacks for an event.
  *
  * @param event The event to invoke callbacks for.
  */
 void ap_power_ev_send_callbacks(enum ap_power_events event);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __AP_POWER_AP_EVENTS_H__ */
