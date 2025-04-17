a8cd564ede9d53331054513335a8e8fde4ad7a1e
Dino Li <Dino.Li@ite.com.tw>
2025-04-17 14:31:58 +0800

    DEBUG: it83xx: ensuring IER19 change take effect
    Added chip-specific barrier to ensure IER19 change take effect
    before enabling CPU interrupt.
    
    Change-Id: If38fc250c417e91954ef217cfc7ee791bfa847e3

--- chip/it83xx/clock.c
+++ chip/it83xx/clock.c
@@ -465,6 +465,7 @@ void __ram_code clock_cpu_standby(void)
 		 * point to interrupt_enable() which is called later.
 		 */
 		uint32_t val = BIT(30);
+		volatile uint8_t _ext_ier19 __unused;
 
 		asm volatile("mtsr %0, $INT_MASK" : : "r"(val));
 		asm volatile("dsb");
@@ -474,6 +475,7 @@ void __ram_code clock_cpu_standby(void)
 		 * not wake up CPU.
 		 */
 		IT83XX_INTC_EXT_IER19 = BRAM_EC_EXT_REG19;
+		_ext_ier19 = IT83XX_INTC_EXT_IER19;
 #endif
 		asm("standby wake_grant");
 	} else if (IS_ENABLED(CHIP_CORE_RISCV)) {
