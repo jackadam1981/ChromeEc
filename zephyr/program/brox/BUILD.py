79e6cf4da189d2f310132b9149cec2c60979efef
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2025-04-18 11:03:43 +0800

    TEST-ONLY: Make brox board support it51xxx
    uart: change from UART2(GPF0/F1) to UART1(GPC7/E6)
          Remove functionality:
                 UART2(GPF0/F1)
                 ISH_I2C_EC_SCL(GPC7)
                 CCD_MODE_ODL  (GPE6)
    
    i2c: i2c5 clk/data change from GPE1/E2 to GPA4/A5
         Remove functionality:
                PWM4: LED_3_L(GPA4)
                PWM5: LED_4_L(GPA5)
    
    gpio: PWRSW/GPE4 --> GPB3
          SMCLK0/GPB3 --> GPE4
          set tri-state: GPF0, GPF1, GPE1, GPE2
    
    test: zmake build brox --clobber
    
    test: RAM code test: KERNEL_IN_RAM, I2C_CODE_IN_RAM
    
    Change-Id: I76177cff4f5690ca623377484777f47c335d4c48
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

--- zephyr/program/brox/BUILD.py
+++ zephyr/program/brox/BUILD.py
@@ -7,7 +7,7 @@
 
 def register_brox_project(
     project_name,
-    chip="it8xxx2/it82002aw",
+    chip="it51xxx/it51526bw",
     kconfig_files=None,
 ):
     """Register a variant of brox."""
@@ -161,8 +161,8 @@ lotso = register_brox_project(
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release. Not needed for brox-ish since it
 # doesn't use RO+RW
-assert_rw_fwid_DO_NOT_EDIT(project_name="brox", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brtk", addr=0x80404)
+assert_rw_fwid_DO_NOT_EDIT(project_name="brox", addr=0x60058)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox-ish-ec", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="caboc", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="greenbayupoc", addr=0x60098)
