53f7e9125df1c86407193870f5fe06b85c289215
Wen Zhang <zhangwen6@huaqin.corp-partner.google.com>
2025-04-21 16:54:18 +0800

    skywalker: Add initial code for obiwan
    Initial EC Zephyr config for Obiwan. Copy and rename skywalker.
    Configure the different configs in the later CLs.
    
    BUG=b:406115783
    TEST=zmake build obiwan and can boot the skywalker board
    
    Change-Id: Ica86ca7b1208782b688ce3b13d3cae37dc34f040

--- zephyr/program/skywalker/BUILD.py
+++ zephyr/program/skywalker/BUILD.py
@@ -20,7 +20,9 @@ def register_skywalker_npcx_project(project_name):
 
 
 register_skywalker_npcx_project(project_name="skywalker")
+register_skywalker_npcx_project(project_name="obiwan")
 
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="skywalker", addr=0x40144)
+assert_rw_fwid_DO_NOT_EDIT(project_name="obiwan", addr=0x40144)
