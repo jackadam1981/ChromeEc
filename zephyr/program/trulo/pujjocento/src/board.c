d7027186e8d595c0ef7f1b3aaa47512e813a08d4
guojun <guojun@huaqin.corp-partner.google.com>
2025-04-22 15:49:42 +0800

    pujjocento: Modify keyboard behavior
    Modify the keyboard behavior according to the keyboard datasheet spec.
    
    BUG=b:417141058
    TEST=1.zmake build pujjocento
    LOW_COVERAGE_REASON=Tests will be added in b:417141058
    
    Change-Id: Ief1e75b427fe2ba00f86f9c68a9fa1cd058ea22a

---
+++ zephyr/program/trulo/pujjocento/src/board.c
@@ -0,0 +1,51 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "cros_cbi.h"
+#include "gpio.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "keyboard_backlight.h"
+#include "keyboard_config.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(board_init, LOG_LEVEL_INF);
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
+
+static bool has_backlight = FW_KB_BL_NOT_PRESENT;
+int8_t board_vivaldi_keybd_idx(void)
+{
+	if (has_backlight == FW_KB_BL_NOT_PRESENT) {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_0));
+	} else {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_1));
+	}
+}
+
+/*
+ * Keyboard function decided by FW config.
+ */
+test_export_static void kb_init(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FW_KB_BL, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FW_KB_BL);
+		return;
+	}
+
+	if (val == FW_KB_BL_PRESENT) {
+		CPRINTS("Keyboard configuration FW_KB_BL_PRESENT.");
+		has_backlight = FW_KB_BL_PRESENT;
+	} else {
+		CPRINTS("Keyboard configuration FW_KB_BL_NOT_PRESENT.");
+		has_backlight = FW_KB_BL_NOT_PRESENT;
+	}
+}
+DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_I2C);
