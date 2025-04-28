61218ef99ac4f8a27b8888b28cd50bc6f05fcd49
Jayvik Desai <jayvik@google.com>
2025-04-29 02:56:41 +0530

    bluey: Enable Power sequence for Qualcomm QC_EXP
    This Patch enables the Power sequencing for Qualcomm QC_EXP for bluey
    program.
    
    This patch adds the following:
    - Add switchcap functions to control the vph_ec_pwr_1A signal.
    - Add checks for VPH_PWR>1V.
    - Adds mock entry for a few of the power signal, which are not yet
      enabled.
    - Select proper Kconfigs to enable power sequencing.
    
    BUG=b:408278047, b:415184416
    TEST=zmake build bluey
    
    Change-Id: I57fa21fe60748470b5eb153303591b70e9c4ee1f

---
+++ zephyr/program/bluey/src/switchcap.c
@@ -0,0 +1,31 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "adc.h"
+#include "common.h"
+#include "gpio.h"
+#include "power/qcom.h"
+
+#define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ##args)
+
+/* Threshold for VPH PWR */
+#define VPH_PWR_THRESHOLD 300
+
+void board_set_switchcap_power(int enable)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_vph_pwr_1a), enable);
+}
+
+int board_is_switchcap_enabled(void)
+{
+	return gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_vph_pwr_1a));
+}
+
+int board_is_switchcap_power_good(void)
+{
+	int adc_value = adc_read_channel(ADC_VPH_PWR);
+	CPRINTS("switchcap VPH power good ADC value=%d", adc_value);
+	return adc_value > VPH_PWR_THRESHOLD;
+}
