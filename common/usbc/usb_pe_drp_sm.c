4039a01956f8ea687745d5cc04dd58c17e739432
Abe Levkoy <alevkoy@chromium.org>
2025-04-28 17:28:08 -0600

    TCPMv2: Instrument Request latency
    Record timestamps to characterize latency of latency between Source
    Capabilities from tester and Request from UUT.
    
    BUG=b:411184533
    TEST=zmake build ciri
    
    Change-Id: I6d3de3b080dd67a51c3d7af717c1d999c3e26760

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -675,6 +675,10 @@ static struct policy_engine {
 
 	/* Last received Revision Message Data Object (RMDO) from the partner */
 	struct rmdo partner_rmdo;
+
+	/* Timestamps for debugging */
+	/* When received message is available to PE */
+	timestamp_t rx_ts;
 } pe[CONFIG_USB_PD_PORT_MAX_COUNT];
 
 test_export_static enum usb_pe_state get_state_pe(const int port);
@@ -896,6 +900,15 @@ int pe_is_explicit_contract(int port)
 
 void pe_message_received(int port)
 {
+	/* In general, the port might receive another message while it is still
+	 * processing a previous message. Avoid overwriting the Rx timestamp
+	 * while it may still be read. This is very unlikely to occur after a
+	 * Request during a compliance test.
+	 */
+	if (pe[port].rx_ts.val == 0) {
+		pe[port].rx_ts = get_time();
+	}
+
 	/* This should only be called from the PD task */
 	assert(port == TASK_ID_TO_PD_PORT(task_get_current()));
 
@@ -3407,6 +3420,7 @@ static void pe_snk_startup_entry(int port)
 	 * revision 3.0
 	 */
 	pd_dpm_request(port, DPM_REQUEST_GET_REVISION);
+	pd_record_timestamp_start(port, PD_INTERVAL_WAIT_PRL_START);
 }
 
 static void pe_snk_startup_run(int port)
@@ -3414,6 +3428,7 @@ static void pe_snk_startup_run(int port)
 	/* Wait until protocol layer is running */
 	if (!prl_is_running(port))
 		return;
+	pd_record_timestamp_end(port, PD_INTERVAL_WAIT_PRL_START);
 
 	/*
 	 * Once the reset process completes, the Policy Engine Shall
@@ -3428,6 +3443,7 @@ static void pe_snk_startup_run(int port)
 static void pe_snk_discovery_entry(int port)
 {
 	print_current_state(port);
+	pd_record_timestamp_start(port, PD_INTERVAL_WAIT_VBUS);
 }
 
 static void pe_snk_discovery_run(int port)
@@ -3436,8 +3452,11 @@ static void pe_snk_discovery_run(int port)
 	 * Transition to the PE_SNK_Wait_for_Capabilities state when:
 	 *   1) VBUS has been detected
 	 */
-	if (!pd_check_vbus_level(port, VBUS_REMOVED))
+	if (!pd_check_vbus_level(port, VBUS_REMOVED)) {
+		pd_record_timestamp_end(port, PD_INTERVAL_WAIT_VBUS);
+		pd_record_timestamp_start(port, PD_INTERVAL_WAIT_CAPS);
 		set_state_pe(port, PE_SNK_WAIT_FOR_CAPABILITIES);
+	}
 }
 
 /**
@@ -3471,6 +3490,17 @@ static void pe_snk_wait_for_capabilities_run(int port)
 		payload = (uint32_t *)rx_emsg[port].buf;
 
 		if ((ext == 0) && (cnt > 0) && (type == PD_DATA_SOURCE_CAP)) {
+			/* FIXME: When these recordings were using rx_ts, set in
+			 * pe_message_received, it seems like they were being
+			 * set to a time from the previous Source Caps exchange,
+			 * producing unusable intervals.
+			 */
+			pd_record_timestamp(port, PD_INTERVAL_WAIT_CAPS, PD_END,
+					    get_time());
+			pd_record_timestamp(port,
+					    PD_INTERVAL_SRC_CAPS_SEND_REQUEST,
+					    PD_START, get_time());
+			pe[port].rx_ts.val = 0;
 			set_state_pe(port, PE_SNK_EVALUATE_CAPABILITY);
 			return;
 		} else if (ext > 0) {
@@ -3561,6 +3591,9 @@ static void pe_snk_select_capability_entry(int port)
 		set_state_pe(port, PE_SNK_READY);
 		return;
 	}
+
+	pd_record_timestamp_end(port, PD_INTERVAL_SRC_CAPS_SEND_REQUEST);
+
 	pe_sender_response_msg_entry(port);
 
 	/* We are PD Connected */
@@ -3709,6 +3742,8 @@ static void pe_snk_select_capability_run(int port)
 void pe_snk_select_capability_exit(int port)
 {
 	pe_sender_response_msg_exit(port);
+
+	pd_print_timestamps(port);
 }
 
 /**
@@ -5526,7 +5561,10 @@ static void pe_prs_snk_src_source_on_exit(int port)
 struct pd_debug_timestamps pd_ts[CONFIG_USB_PD_PORT_MAX_COUNT]
 				[PD_INTERVAL_COUNT] = { 0 };
 const char *pd_ts_name[] = {
-	"Invalid interval",
+	"Source Caps PE to Request TCPC",
+	"PE wait for PRL",
+	"PE wait for VBUS",
+	"PE wait for Source Caps",
 };
 BUILD_ASSERT(ARRAY_SIZE(pd_ts_name) == PD_INTERVAL_COUNT);
 
