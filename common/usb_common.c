a92faa5dcd17d474a97c8053ae35ac78a7bd68fa
Abe Levkoy <alevkoy@chromium.org>
2025-04-28 14:09:51 -0600

    DNS: Instrument FRS Error Recovery path
    Record timestamps during FRS Error Recovery path and print them after
    detaching. Understand sources of latency in this path.
    
    BUG=b:361800172
    TEST=twister -s zephyr/test/drivers/drivers.default
    
    Change-Id: Ia60d14c764215c0d07774951ca7fed4ae7a0a169

--- common/usb_common.c
+++ common/usb_common.c
@@ -868,6 +868,24 @@ struct pd_debug_timestamps pd_ts[CONFIG_USB_PD_PORT_MAX_COUNT]
 				[PD_INTERVAL_COUNT] = { 0 };
 const char *pd_ts_name[] = {
 	"Invalid interval",
+	"GoodCRC to CC open",
+	"SenderResponseTimer",
+	"Interrupt to interrupt task",
+	"Error Recovery",
+	"Error Recovery flag",
+	"PE state change",
+	"TC state change",
+	"Attached.SNK exit",
+	"VCONN off Attached.SNK exit",
+	"Disable ADD Attached.SNK exit",
+	"Stop drawing current",
+	"CC open entry",
+	"SenderResponseTimer disable",
+	"VBUS off",
+	"VCONN off CC open",
+	"Disable ADD CC open",
+	"Print",
+	"Update CC",
 };
 BUILD_ASSERT(ARRAY_SIZE(pd_ts_name) == PD_INTERVAL_COUNT);
 
