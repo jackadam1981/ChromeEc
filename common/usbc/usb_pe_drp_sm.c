6b7ac9796842edd0866cc408fb03a82bd0745a40
Abe Levkoy <alevkoy@chromium.org>
2025-04-28 14:06:32 -0600

    TCPMv2: Add time instrumentation framework
    Add a facility to record timestamps from the system timer at arbitrary
    points and print them out at a convenient time, such as after the
    critical section that was being measured. Store timestamps in memory
    until they are cleared.
    
    Usage notes:
    * The recorded timestamps are in us since sysjump. Under Zephyr, at
      least on NPCx chips, the effective timestamp resolution is 100 us.
    * Recording multiple interval start and stop timestamps at the same
      point in the code is redundant, but this does not seem to disturb the
      recorded intervals. Timestamps recorded in the same spot always have
      the same value.
    * The user must take care to print and clear the timestamps at a point
      after all recorded intervals have ended but before any subsequent
      events would overwrite previously recorded timestamps. As a
      corollary, the user may need to ignore logs in which some intervals
      have not been recorded yet.
    * On NPCx chips, the event timer may drift up to 1% from the system
      timer. There is likely a similar limitation on other chips. This can
      manifest in the intervals measured via this facility as timers
      expriring earlier or later than they were programmed to expire.
    
    BUG=b:411184533,b:361800172
    TEST=zmake build -a
    
    Change-Id: I2e0ba7eb0462c43884d1aa459940e9e168e0a7e5

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -5523,6 +5523,50 @@ static void pe_prs_snk_src_source_on_exit(int port)
 	tc_pr_swap_complete(port, PE_CHK_FLAG(port, PE_FLAGS_PR_SWAP_COMPLETE));
 }
 
+struct pd_debug_timestamps pd_ts[CONFIG_USB_PD_PORT_MAX_COUNT]
+				[PD_INTERVAL_COUNT] = { 0 };
+const char *pd_ts_name[] = {
+	"Invalid interval",
+};
+BUILD_ASSERT(ARRAY_SIZE(pd_ts_name) == PD_INTERVAL_COUNT);
+
+void pd_record_timestamp(int port, enum pd_debug_interval interval,
+			 enum pd_interval_point point, timestamp_t ts)
+{
+	struct pd_debug_timestamps *debug_ts = &pd_ts[port][interval];
+
+	if (point == PD_START)
+		debug_ts->start = ts;
+	else
+		debug_ts->end = ts;
+}
+
+inline void pd_record_timestamp_start(int port, enum pd_debug_interval interval)
+{
+	pd_record_timestamp(port, interval, PD_START, get_time());
+}
+
+inline void pd_record_timestamp_end(int port, enum pd_debug_interval interval)
+{
+	pd_record_timestamp(port, interval, PD_END, get_time());
+}
+
+void pd_print_timestamps(int port)
+{
+	ccprintf("C%d timestamps:\n", port);
+
+	for (int i = 0; i < PD_INTERVAL_COUNT; ++i) {
+		uint32_t start = pd_ts[port][i].start.le.lo;
+		uint32_t end = pd_ts[port][i].end.le.lo;
+		int delta = time_until(start, end);
+
+		ccprintf("%s: %u to %u = %d (%dms)\n", pd_ts_name[i], start,
+			 end, delta, delta / 1000);
+	}
+
+	memset(&pd_ts[port], 0, sizeof(pd_ts[port]));
+}
+
 /**
  * PE_PRS_SNK_SRC_Send_Swap
  * PE_FRS_SNK_SRC_Send_Swap
