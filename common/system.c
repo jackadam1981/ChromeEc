36acf7fedd3db6cb3fb12046257f5b3f6ed05273
elmo_lan <elmo_lan@realtek.corp-partner.google.com>
2025-04-29 22:43:50 +0800

    minimal-realtek: enable minimal-build for rts5915
    Make a minimal project example for rts5915 with limited function enable.
    
    BUG=b:412453941
    TEST=zmake -D build minimal-realtek --clobber
        console command "timerinfo" is working.
    
    Change-Id: I115c890679e5eee4f2382bc610736481e96526f4

--- common/system.c
+++ common/system.c
@@ -760,6 +760,7 @@ int system_set_active_copy(enum ec_image copy)
 	return system_set_bbram(SYSTEM_BBRAM_IDX_TRY_SLOT, copy);
 }
 
+#if CONFIG_FLASH
 #ifdef CONFIG_EC_PROTECTED_STORAGE_OFF
 /*
  * This is defined in system.c instead of flash.c because it's called even
@@ -777,6 +778,14 @@ uint32_t flash_get_rw_offset(enum ec_image copy)
 	return CONFIG_EC_PROTECTED_STORAGE_OFF + CONFIG_RO_STORAGE_OFF;
 }
 #endif
+#else
+uint32_t flash_get_rw_offset(enum ec_image copy)
+{
+	return 0;
+}
+#endif
+
+
 
 const struct image_data *system_get_image_data(enum ec_image copy)
 {
@@ -804,6 +813,7 @@ const struct image_data *system_get_image_data(enum ec_image copy)
 	 */
 	addr += flash_get_rw_offset(copy);
 
+#ifdef CONFIG_FLASH
 #ifdef CONFIG_MAPPED_STORAGE
 	addr += CONFIG_MAPPED_STORAGE_BASE;
 	crec_flash_lock_mapped_storage(1);
@@ -813,6 +823,7 @@ const struct image_data *system_get_image_data(enum ec_image copy)
 	/* Read the version struct from flash into a buffer. */
 	if (crec_flash_read(addr, sizeof(data), (char *)&data))
 		return NULL;
+#endif
 #endif
 
 	/* Make sure the version struct cookies match before returning the
