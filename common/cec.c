d2f370d568c64a89a636bf96a915c9d1a6533f58
Sung-Chi Li <lschyi@google.com>
2025-04-29 10:28:28 +0800

    cec: fix wrong response data size of empty `EC_MKBP_EVENT_CEC_EVENT`
    If there is no event out when `EC_MKBP_EVENT_CEC_EVENT` is received, we
    should also reset the out buffer to zero, and return the size of the
    empty event, so when the response is copied back to AP, there is no
    garbage values returned.
    
    Also reset the zero value to `out` variables if there is no event out
    in the `EC_MKBP_EVENT_CEC_EVENT`, and return the correct size (4 bytes
    for uint32_t) as data size. The comment for this test is updated
    accordingly.
    
    BUG=b:412803807
    TEST=./twister -v -i -p native_sim -p unit_testing
    
    Change-Id: Ibc553b289c49c6c279b3ce526e02069ea7102995

--- common/cec.c
+++ common/cec.c
@@ -404,8 +404,14 @@ static int cec_get_next_event(uint8_t *out)
 	}
 
 	if (!event_out) {
-		/* Didn't find any events */
-		return 0;
+		/*
+		 * Didn't find any events, set the out to zero and return the
+		 * size of event_out, so content send back to AP are not garbage
+		 * values.
+		 */
+		event_out = EC_MKBP_EVENT_CEC_PACK(0, port);
+		memcpy(out, &event_out, sizeof(event_out));
+		return sizeof(event_out);
 	}
 
 	memcpy(out, &event_out, sizeof(event_out));
