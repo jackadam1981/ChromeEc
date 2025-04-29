5ffbe4f1c72bd91ee1662401f740cd44b8d3bf8b
Vincent4 <vincent4.wang@lcfc.corp-partner.google.com>
2025-04-29 18:11:17 +0800

    pujjolo: Fix motion sensor config
    Fix motionsense sensor config
    
    BUG=b:400991334
    TEST=zmake build pujjolo success lid_accel and base_accel sensors are working properly.
    
    Change-Id: I5b6c302cd01adceafaafb83e2de46ac2459b674f

--- zephyr/program/trulo/pujjolo/src/sensor.c
+++ zephyr/program/trulo/pujjolo/src/sensor.c
@@ -9,8 +9,6 @@
 #include "cros_cbi.h"
 #include "driver/accel_bma4xx.h"
 #include "driver/accel_lis2dw12_public.h"
-#include "driver/accelgyro_bmi323.h"
-#include "driver/accelgyro_lsm6dsm.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
 #include "motion_sense.h"
@@ -19,69 +17,42 @@
 
 #include <zephyr/logging/log.h>
 
-#define I2C_PORT_SENSOR 1
-
 LOG_MODULE_REGISTER(uldrenite_sensor, LOG_LEVEL_INF);
 
-static int sensor_fwconfig;
+enum base_sensor_type {
+	base_lis2dw12 = 0,
+	base_bma422,
+};
 
-void motion_interrupt(enum gpio_signal signal)
+enum lid_sensor_type {
+	lid_lis2dw12 = 0,
+	lid_bma422,
+};
+
+static int base_use_alt_sensor;
+static int lid_use_alt_sensor;
+
+test_export_static void alt_sensor_init(void)
 {
-	lis2dw12_interrupt(signal);
-}
-
-void lid_accel_interrupt(enum gpio_signal signal)
-{
-	lis2dw12_interrupt(signal);
-}
-
-static void motionsense_init(void)
-{
-	int ret;
-
-	if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
-		int ish_enabled;
-		ret = cros_cbi_get_fw_config(ISH, &ish_enabled);
-
-		if (ret < 0) {
-			LOG_ERR("Failed to load ISH config: %d", ret);
-			return;
-		}
-
-		if (ish_enabled == ISH_ENABLED) {
-			motion_sensor_count = 0;
-			gpio_disable_dt_interrupt(
-				GPIO_INT_FROM_NODELABEL(int_imu));
-			gpio_disable_dt_interrupt(
-				GPIO_INT_FROM_NODELABEL(int_lid_imu));
-			gpio_pin_configure_dt(
-				GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
-				GPIO_DISCONNECTED);
-			gpio_pin_configure_dt(
-				GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
-				GPIO_DISCONNECTED);
-			LOG_INF("No motionsense");
-			return;
-		}
+	/* Check which motion sensors are used */
+	if (cros_cbi_ssfc_check_match(
+		    CBI_SSFC_VALUE_ID(DT_NODELABEL(base_sensor_0)))) {
+		LOG_INF("Base : LIS2DWL");
+		base_use_alt_sensor = base_lis2dw12;
+	} else {
+		LOG_INF("Base : BMA422");
+		base_use_alt_sensor = base_bma422;
 	}
 
-	ret = cros_cbi_get_fw_config(FORM_FACTOR, &sensor_fwconfig);
-	if (ret < 0) {
-		LOG_ERR("error retriving CBI config: %d", ret);
-		return;
+	if (cros_cbi_ssfc_check_match(
+		    CBI_SSFC_VALUE_ID(DT_NODELABEL(lid_sensor_0)))) {
+		LOG_INF("Lid : LIS2DWL");
+		lid_use_alt_sensor = lid_lis2dw12;
+	} else {
+		LOG_INF("Lid : BMA422");
+		lid_use_alt_sensor = lid_bma422;
 	}
 
-	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
-		if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
-			/* Only valid in EC */
-			gmr_tablet_switch_disable();
-		}
-		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
-		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
-				      GPIO_INPUT | GPIO_PULL_UP);
-		LOG_INF("Board is Clamshell");
-	} else if (sensor_fwconfig == FORM_FACTOR_CONVERTIBLE) {
-		LOG_INF("Board is Convertible");
-	}
+	motion_sensors_check_ssfc();
 }
-DECLARE_HOOK(HOOK_INIT, motionsense_init, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, alt_sensor_init, HOOK_PRIO_POST_I2C);
