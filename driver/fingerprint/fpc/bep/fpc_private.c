31126eeddb20c42995eca348451877f5c3a97885
Firas Sammoura <fsammoura@google.com>
2025-05-05 20:22:34 +0000

    fpsensor: Align fp_sensor_get_info to fpinfo v2
    BUG=b:398899644
    TEST=make buildall -j10
    
    Change-Id: Ic07346b37cbeeeadb819cf1f643bd226d14ca9cb

--- driver/fingerprint/fpc/bep/fpc_private.c
+++ driver/fingerprint/fpc/bep/fpc_private.c
@@ -42,21 +42,6 @@ __staticlib const char *fp_sensor_get_version(void);
 __staticlib const char *fp_sensor_get_build_info(void);
 
 /* Sensor description */
-static struct ec_response_fp_info ec_fp_sensor_info = {
-	/* Sensor identification */
-	.vendor_id = FOURCC('F', 'P', 'C', ' '),
-	.product_id = 9,
-	.model_id = 1,
-	.version = 1,
-	/* Image frame characteristics */
-	.frame_size = FP_SENSOR_IMAGE_SIZE_FPC,
-	.pixel_format = V4L2_PIX_FMT_GREY,
-	.width = FP_SENSOR_RES_X_FPC,
-	.height = FP_SENSOR_RES_Y_FPC,
-	.bpp = FP_SENSOR_RES_BPP_FPC,
-};
-
-/* Sensor description */
 static struct fp_sensor_info fpc1025_sensor_info = {
 	/* Sensor identification */
 	.vendor_id = FOURCC('F', 'P', 'C', ' '),
