cdffda8983c7e3921e4b8456543dcdd28d7b8241
Firas Sammoura <fsammoura@google.com>
2025-05-05 20:22:34 +0000

    fpsensor: Align fp_sensor_get_info to fpinfo v2
    BUG=b:398899644
    TEST=make buildall -j10
    
    Change-Id: Ic07346b37cbeeeadb819cf1f643bd226d14ca9cb

--- zephyr/shim/include/fpsensor_driver.h
+++ zephyr/shim/include/fpsensor_driver.h
@@ -23,13 +23,8 @@ extern "C" {
 #else
 #define FP_SENSOR_IMAGE_SIZE                \
 	FINGERPRINT_SENSOR_REAL_IMAGE_SIZE( \
-		DT_CHOSEN(cros_fp_fingerprint_sensor))
+		DT_CHOSEN(cros_fp_fingerprint_sensor), 0)
 #endif
-
-#define FP_SENSOR_RES_X \
-	FINGERPRINT_SENSOR_RES_X(DT_CHOSEN(cros_fp_fingerprint_sensor))
-#define FP_SENSOR_RES_Y \
-	FINGERPRINT_SENSOR_RES_Y(DT_CHOSEN(cros_fp_fingerprint_sensor))
 
 /*
  * Tell fpsensor code that private driver is present, even if this is a public
