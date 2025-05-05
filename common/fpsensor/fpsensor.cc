36c44bbb0ce749d3db9dc69ba5652d8ebc0bd909
Firas Sammoura <fsammoura@google.com>
2025-05-05 20:22:34 +0000

    fpsensor: Align fp_sensor_get_info to fpinfo v2
    BUG=b:398899644
    TEST=make buildall -j10
    
    Change-Id: Ic07346b37cbeeeadb819cf1f643bd226d14ca9cb

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -417,7 +417,7 @@ extern "C" void fp_task(void)
 #endif /* !HAVE_FP_PRIVATE_DRIVER */
 }
 
-static enum ec_status fp_command_info_v2(struct host_cmd_handler_args *args)
+static enum ec_status fp_command_info(struct host_cmd_handler_args *args)
 {
 	struct ec_response_fp_info_v2 *r =
 		static_cast<ec_response_fp_info_v2 *>(args->response);
