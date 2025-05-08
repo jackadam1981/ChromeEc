cdf4507fe6c52f899f60a2b11d1bebb7558d25b4
Jayvik Desai <jayvik@google.com>
2025-05-08 14:15:32 +0530

    bluey: Control +3V VREG during S3 state transitions
    This patch adds the following controls for the +3V VREG:
    
    - Enable ec_3v_s3_en during the chipset startup
    - Disable ec_3v_s3_en during the chipset shutdown
    
    BUG=b:408278047, b:415184416, b:421784159
    TEST=zmake build bluey
    LOW_COVERAGE_REASON=b:422080009 tracking test code
    
    Change-Id: Ifaebb1c4ea952be257e827dc489896af9664239c

---
+++ zephyr/program/bluey/src/chipset.c
@@ -0,0 +1,22 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Bluey chipset-specific configuration */
+
+#include "common.h"
+#include "gpio.h"
+#include "hooks.h"
+
+void board_chipset_startup(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_3v_s3_en), 1);
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_chipset_startup, HOOK_PRIO_DEFAULT);
+
+void board_chipset_shutdown(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_3v_s3_en), 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown, HOOK_PRIO_DEFAULT);
