9fde15a1a78cb5aba94b8be24e723f42d46e534d
Firas Sammoura <fsammoura@google.com>
2025-05-08 23:17:29 +0000

    test: do not submit
    BUG=b:406324560
    TEST=None
    
    Change-Id: I3e2bbb978b310d7078dc0f234a8dc26d27d52adb

--- zephyr/program/fpmcu/BUILD.py
+++ zephyr/program/fpmcu/BUILD.py
@@ -55,8 +55,8 @@ helipilot = register_fpmcu_variant(
     project_name="helipilot",
     zephyr_board="google_quincy",
     register_func=register_npcx_project,
-    variant_modules=["cmsis", "cmsis_6"],
-    variant_optional_modules=["fpc"],
+    variant_modules=["cmsis"],
+    variant_optional_modules=["elan"],
     variant_dts_overlays=[
         here / "helipilot" / "helipilot.dts",
     ],
