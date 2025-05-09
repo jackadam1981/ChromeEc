2d7cb5fbd973d2c37f43badb7683b0ea4c08fadf
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2025-05-09 14:45:59 +0800

    TEST-ONLY: Make lotso board support it51xxx
    test: zmake build lotso --clobber
    
    Change-Id: Id46d66400697e5f559679e8ba1d9e519d16f7bf1
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

--- zephyr/program/brox/BUILD.py
+++ zephyr/program/brox/BUILD.py
@@ -132,11 +132,11 @@ lotso = register_brox_project(
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release. Not needed for brox-ish since it
 # doesn't use RO+RW
-assert_rw_fwid_DO_NOT_EDIT(project_name="brox", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="brox", addr=0x60058)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox-ish-ec", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox-tokenized", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="greenbayupoc", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="jubilant", addr=0x60098)
-assert_rw_fwid_DO_NOT_EDIT(project_name="lotso", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="lotso", addr=0x60058)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox-sku4", addr=0x70098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox-sku4-tokenized", addr=0x70098)
