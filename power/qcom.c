54ff38937dc53bf18fd051440dac1deecc0e45d1
Jayvik Desai <jayvik@google.com>
2025-05-11 14:18:24 +0530

    bluey: Add long warm reset sequence for QC_EXP SoC
    This patch adds long warm reset sequence for QC_EXP SoC.
    
    Upon SYS_RST_L assertion, a 20ms timer begins to differentiate between a
    short and long warm reset.
    If SYS_RST_L deasserts before the timer expires, the timer is cancelled,
    and an EC-initiated short warm reset sequence is performed. However, if
    the 20ms timer expires (meaning SYS_RST_L remained asserted for at least
    20ms), a long warm reset sequence is initiated. This sequence involves
    powering off the system and waiting for SYS_RST_L deassertion. Upon
    deassertion, the system performs a power-on sequence if it was
    previously in S0 state.
    
    This patch adds a interrupt for the SYS_RST_ODL signal and updates the
    signal as an input to the EC.
    
    BUG=b:408278047, b:415184416
    TEST=zmake build bluey
    
    Change-Id: I72d4112617e7dfd320a6dd14c1ff71118af82ffd

--- power/qcom.c
+++ power/qcom.c
@@ -173,6 +173,11 @@ static char lid_opened;
 /* Time where we will power off, if power button still held down */
 static timestamp_t power_off_deadline;
 
+/* 1 if long warm reset is going on */
+static char long_warm_reset = 0;
+
+/* 1 if the system was powered on before going into long warm reset sequence */
+static int restore_power_on = 0;
 /* Force AP power on (used for recovery keypress) */
 static int auto_power_on;
 
@@ -454,6 +459,55 @@ static int wait_pmic_pwron(int enable, unsigned int timeout)
 	return EC_SUCCESS;
 }
 
+#ifdef CONFIG_CHIPSET_QC_EXP
+void sys_rst_timer_expired(struct k_timer *timer_id)
+{
+	/*
+	 * Timer expired before SYS_RST_ODL deasserted perform a
+	 * long warm reset
+	 */
+	power_request = POWER_REQ_OFF;
+	long_warm_reset = 1;
+	/*
+	 * Preserve the AP's power-on state so it can be reinstated once the
+	 * long warm reset sequence is complete.
+	 */
+	restore_power_on = is_pmic_pwron();
+	task_wake(TASK_ID_CHIPSET);
+}
+K_TIMER_DEFINE(sys_rst_timer, sys_rst_timer_expired, NULL);
+
+void chipset_sys_rst_interrupt(enum gpio_signal signal)
+{
+	/*
+	 * long warm reset sequence completes once SYS_RST_ODL deasserts.
+	 */
+	if (long_warm_reset &&
+	    !gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_sys_rst_odl))) {
+		long_warm_reset = 0;
+		if (restore_power_on) {
+			power_request = POWER_REQ_ON;
+			restore_power_on = 0;
+			task_wake(TASK_ID_CHIPSET);
+		}
+	} else {
+		/*
+		 * Start a 20ms timer if sys_rst_odl is asserted
+		 * 1. if the timer expiers before the sys_rst_odl pin is
+		 * deasserted perform a long warm reset sequence
+		 * 2. if the SYS_RST_ODL deasserted before the timer expires
+		 * request a EC initiated warm reset
+		 */
+		if (gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_sys_rst_odl))) {
+			k_timer_start(&sys_rst_timer, K_MSEC(20), K_NO_WAIT);
+		} else {
+			k_timer_stop(&sys_rst_timer);
+			power_request = POWER_REQ_WARM_RESET;
+			task_wake(TASK_ID_CHIPSET);
+		}
+	}
+}
+#endif
 /**
  * Set the state of the system power signals but without any check.
  *
