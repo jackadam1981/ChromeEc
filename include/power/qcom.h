d53015a96fe599daa69af49d8871df14cb88e636
Jayvik Desai <jayvik@google.com>
2025-05-12 09:38:28 +0530

    power/qcom: Passthru the EC power signals to the PMIC power signals
    This patch implements a signal Pass pass-through mechanism for QC_EXP
    SOC.
    
    This passthrough occurs only once the EC has completed all its switchcap
    power transition. The primary purpose is to ensure that the PMIC
    receives signals in its original state, thereby preserving its intended
    power signal flow.
    
    BUG=b:408278047, b:415184416, b:421784159
    TEST=zmake build bluey
    
    Change-Id: I60bc7bef36cba2ae883957a4642bf9715358e971

--- include/power/qcom.h
+++ include/power/qcom.h
@@ -35,4 +35,10 @@ void board_set_switchcap_power(int enable);
 int board_is_switchcap_enabled(void);
 int board_is_switchcap_power_good(void);
 
+#if defined(CONFIG_PLATFORM_EC_PMIC_PASSTHRU_POWER_SIGNALS)
+void passthru_lid_open_to_pmic(void);
+void passthru_ac_on_to_pmic(void);
+void passthru_powerbtn_to_pmic(void);
+#endif
+
 #endif /* __CROS_EC_POWER_QCOM_H_ */
