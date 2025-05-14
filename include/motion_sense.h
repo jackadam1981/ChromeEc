druth0/chrome-ec

commit 571940b2506f4a263d11f1cbb5d6361fe6b97aec

motion_sense: Support dynamic activity sensor
Allow the activity sensor to be dynamic: when the firmware configures
the number of sensors to 0 at run time, make sure the activity sensor
is not reported to the AP.

BUG=b:410646711
TEST=CQ

Change-Id: Idee8274271fafc50845b7a8b73eb9ee51c3274f5
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6540587
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Auto-Submit: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Yuval Peress <peress@google.com>
Commit-Queue: Yuval Peress <peress@google.com>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

include/motion_sense.h
@@ -341,14 +341,20 @@ bool motion_sensor_in_forced_mode(const struct motion_sensor_t *s);
  */
 #if defined(CONFIG_GESTURE_HOST_DETECTION) || defined(CONFIG_ORIENTATION_SENSOR)
 /* Add an extra sensor. We may need to add more */
+#ifdef CONFIG_DYNAMIC_MOTION_SENSOR_COUNT
+#define MOTION_SENSE_ACTIVITY_SENSOR_ID \
+	(motion_sensor_count > 0 ? motion_sensor_count : -1)
+#else
 #define MOTION_SENSE_ACTIVITY_SENSOR_ID (motion_sensor_count)
-#define ALL_MOTION_SENSORS (MOTION_SENSE_ACTIVITY_SENSOR_ID + 1)
+#endif
 #define MAX_MOTION_SENSORS (SENSOR_COUNT + 1)
 #else
 #define MOTION_SENSE_ACTIVITY_SENSOR_ID (-1)
-#define ALL_MOTION_SENSORS (motion_sensor_count)
 #define MAX_MOTION_SENSORS (SENSOR_COUNT)
 #endif
+#define ALL_MOTION_SENSORS                                               \
+	(MOTION_SENSE_ACTIVITY_SENSOR_ID > 0 ? motion_sensor_count + 1 : \
+					       motion_sensor_count)
 
 #ifdef CONFIG_ALS_LIGHTBAR_DIMMING
 #ifdef TEST_BUILD

jackadam1981/ChromeEc

commit 0f60d0ef8ec4cfd55fa6b629f1ab26abc9a03d39

motion_sense: Fix support dynamic activity sensor
Comparing an unsigned int against -1 does not work, make sure
|motion_sensor_count| is casted as an int.

BUG=b:410646711
TEST=CQ

Fixes: 571940b2506f4 ("motion_sense: Support dynamic activity sensor")
Change-Id: Ic69020a700c2a64460a5365499d690a1cfe7bef1

include/motion_sense.h
@@ -352,9 +352,10 @@ bool motion_sensor_in_forced_mode(const struct motion_sensor_t *s);
 #define MOTION_SENSE_ACTIVITY_SENSOR_ID (-1)
 #define MAX_MOTION_SENSORS (SENSOR_COUNT)
 #endif
-#define ALL_MOTION_SENSORS                                               \
-	(MOTION_SENSE_ACTIVITY_SENSOR_ID > 0 ? motion_sensor_count + 1 : \
-					       motion_sensor_count)
+#define ALL_MOTION_SENSORS                            \
+	((int)(MOTION_SENSE_ACTIVITY_SENSOR_ID) > 0 ? \
+		 motion_sensor_count + 1 :            \
+		 motion_sensor_count)
 
 #ifdef CONFIG_ALS_LIGHTBAR_DIMMING
 #ifdef TEST_BUILD