843de157e4a1ed1813227e8656deb1145566901f
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2025-05-14 15:50:33 -0700

    ap_pwrseq: Change power state string
    This CL removes `AP_POWER_STATE_` prefix from power state string name.
    
    BUG=b:416298917,b:421024088
    TEST=Boot Chrome platform
         EC must report power state accordingly
    
    Change-Id: Ib3b0df6cfd89a68294e5970a4c45f5edf2ec2f5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6546761
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Tested-by: Bernardo Perez Priego <bernardo.perez.priego@intel.com>
    Code-Coverage: Keith Short <keithshort@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    (cherry picked from commit 1a3deb82db5760bd7490e1f51bef24dc746b1a37)

--- zephyr/test/nissa/src/board_ap_pwrseq_drv.c
+++ zephyr/test/nissa/src/board_ap_pwrseq_drv.c
@@ -211,8 +211,7 @@ static int chipset_ap_power_s0_run(void *data)
 	return 0;
 }
 
-AP_POWER_CHIPSET_STATE_DEFINE(AP_POWER_STATE_S0, NULL, chipset_ap_power_s0_run,
-			      NULL);
+AP_POWER_CHIPSET_STATE_DEFINE(S0, NULL, chipset_ap_power_s0_run, NULL);
 
 static int chipset_ap_power_s3_run(void *data)
 {
@@ -223,22 +222,19 @@ static int chipset_ap_power_s3_run(void *data)
 	return 0;
 }
 
-AP_POWER_CHIPSET_STATE_DEFINE(AP_POWER_STATE_S3, NULL, chipset_ap_power_s3_run,
-			      NULL);
+AP_POWER_CHIPSET_STATE_DEFINE(S3, NULL, chipset_ap_power_s3_run, NULL);
 
 static int chipset_ap_power_g3_run(void *data)
 {
 	return ap_pwrseq_sm_set_state(data, AP_POWER_STATE_S3);
 }
 
-AP_POWER_CHIPSET_STATE_DEFINE(AP_POWER_STATE_G3, NULL, chipset_ap_power_g3_run,
-			      NULL);
+AP_POWER_CHIPSET_STATE_DEFINE(G3, NULL, chipset_ap_power_g3_run, NULL);
 
 static int x86_non_dsx_adlp_s0ix_run(void *data)
 {
 	return 0;
 }
 
-AP_POWER_CHIPSET_SUB_STATE_DEFINE(AP_POWER_STATE_S0IX, NULL,
-				  x86_non_dsx_adlp_s0ix_run, NULL,
-				  AP_POWER_STATE_S0);
+AP_POWER_CHIPSET_SUB_STATE_DEFINE(S0ix, NULL, x86_non_dsx_adlp_s0ix_run, NULL,
+				  S0);
