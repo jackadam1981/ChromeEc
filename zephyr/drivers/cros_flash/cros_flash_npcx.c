c6dafd987de9e4d20bbb84cbc14e6783e1c4aa0c
Firas Sammoura <fsammoura@google.com>
2025-05-14 18:35:01 +0000

    zephyr: flash: npcx: Initialize 'reg' to fix GCC uninitialized warning
    The `reg` variable in `cros_flash_npcx.c` was being used uninitialized,
    leading to a build error with the GCC compiler:
    
    '/usr/local/google/home/fsammoura/chromiumos/src/platform/ec/zephyr/ \
    /drivers/cros_flash/cros_flash_npcx.c:69:25: error: 'reg' may be used
    uninitialized [-Werror=maybe-uninitialized]`
    
    BUG=b:417755493
    TEST=CQ
    
    Change-Id: I4ec6d00e353568cbeb6c46639af1025456b29c2c

--- zephyr/drivers/cros_flash/cros_flash_npcx.c
+++ zephyr/drivers/cros_flash/cros_flash_npcx.c
@@ -66,7 +66,7 @@ static int cros_flash_npcx_wait_ready(const struct device *dev)
 	int timeout = (10 * USEC_PER_SEC) / wait_period; /* 10 seconds */
 
 	do {
-		uint8_t reg;
+		uint8_t reg = 0;
 
 		cros_flash_npcx_get_status_reg(dev, SPI_NOR_CMD_RDSR, &reg);
 		if ((reg & SPI_NOR_WIP_BIT) == 0)
