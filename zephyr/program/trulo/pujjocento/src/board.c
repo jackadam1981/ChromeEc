b2cb6e23d7f0fcd9dea220ec89124e38d3cb5c25
guojun <guojun@huaqin.corp-partner.google.com>
2025-05-19 14:51:29 +0800

    pujjocento: Modify temp_sensor paras with 6w and 15w
    Due the system thermal policy and fan table requirements, modify the
    temp_sensor paras compatible with 6w and 15w thermal protection mechanism.
    
    BUG=b:412413201
    TEST=1.zmake build pujjocento
         2.test verification pass
    LOW_COVERAGE_REASON=Tests will be added in b:412413201
    
    Change-Id: Ibe499316184b5d0d6527b3c1498764b85af247b4

--- zephyr/program/trulo/pujjocento/src/board.c
+++ zephyr/program/trulo/pujjocento/src/board.c
@@ -10,6 +10,8 @@
 #include "hooks.h"
 #include "keyboard_backlight.h"
 #include "keyboard_config.h"
+#include "temp_sensor/temp_sensor.h"
+#include "thermal.h"
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
@@ -51,6 +53,72 @@ test_export_static void kb_init(void)
 }
 DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_I2C);
 
+/* thermal table control for 15W CPU */
+#define TEMP_VR TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_vr))
+#define TEMP_CPU TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_cpu))
+#define TEMP_AMBIENT TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_ambient))
+#define TEMP_CHARGER TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_charger))
+
+const static struct ec_thermal_config thermal_vr = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(93),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(88),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(51),
+	.temp_fan_max = C_TO_K(67),
+};
+
+const static struct ec_thermal_config thermal_cpu = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(93),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(88),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(50),
+	.temp_fan_max = C_TO_K(70),
+};
+
+const static struct ec_thermal_config thermal_ambient = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(77),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(80),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(75),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(40),
+	.temp_fan_max = C_TO_K(50),
+};
+
+const static struct ec_thermal_config thermal_charger = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(93),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(55),
+	.temp_fan_max = C_TO_K(75),
+};
+
 test_export_static void fan_init(void)
 {
 	int ret;
@@ -67,6 +135,10 @@ test_export_static void fan_init(void)
 		/* Disable the fan */
 		fan_set_count(0);
 	} else {
+		thermal_params[TEMP_VR] = thermal_vr;
+		thermal_params[TEMP_CPU] = thermal_cpu;
+		thermal_params[TEMP_AMBIENT] = thermal_ambient;
+		thermal_params[TEMP_CHARGER] = thermal_charger;
 		CPRINTS("Thermal: 15W thermal control");
 	}
 }
