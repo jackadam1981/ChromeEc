45ca56289045116a5c6dab13284592ce70311df5
Matt_Wang <Matt_Wang@compal.corp-partner.google.com>
2025-05-21 09:58:48 +0800

    Uldrenite: Disable GMR for 180 SKU when ISH is enabled
    GMR should be disabled on ISH-enabled 180 SKUs to prevent abnormal
    tablet_mode_l low signals, which can incorrectly trigger tablet mode.
    
    BUG=none
    TEST=Verified that ISH 180 SKU no longer enters tablet mode unexpectedly.
    
    Change-Id: I830208e3f42c65b5d7ce01fdf39ba482aaafc03a

--- zephyr/program/trulo/uldrenite/src/sensor.c
+++ zephyr/program/trulo/uldrenite/src/sensor.c
@@ -48,12 +48,36 @@ void lid_accel_interrupt(enum gpio_signal signal)
 		bma4xx_interrupt(signal);
 }
 
-static void motionsense_init(void)
+void disable_tabletmode(void)
 {
 	int ret;
 
+	ret = cros_cbi_get_fw_config(FORM_FACTOR, &sensor_fwconfig);
+	if (ret < 0) {
+		LOG_ERR("error retriving CBI config: %d", ret);
+		return;
+	}
+
+	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
+		if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
+			/* Only valid in EC */
+			gmr_tablet_switch_disable();
+		}
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
+				      GPIO_INPUT | GPIO_PULL_UP);
+		LOG_INF("Board is Clamshell");
+	} else if (sensor_fwconfig == FORM_FACTOR_CONVERTIBLE) {
+		LOG_INF("Board is Convertible");
+	}
+}
+
+static void motionsense_init(void)
+{
+	int ret;
+	int ish_enabled;
+
 	if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
-		int ish_enabled;
 		ret = cros_cbi_get_fw_config(ISH, &ish_enabled);
 
 		if (ret < 0) {
@@ -73,8 +97,7 @@ static void motionsense_init(void)
 			gpio_pin_configure_dt(
 				GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
 				GPIO_DISCONNECTED);
-			LOG_INF("No motionsense");
-			return;
+			LOG_INF("Disable motionsense for ISH");
 		}
 	}
 
@@ -89,9 +112,13 @@ static void motionsense_init(void)
 			/* Only valid in EC */
 			gmr_tablet_switch_disable();
 		}
-		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
-		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
-				      GPIO_INPUT | GPIO_PULL_UP);
+		if (ish_enabled != ISH_ENABLED) {
+			gpio_disable_dt_interrupt(
+				GPIO_INT_FROM_NODELABEL(int_lid_imu));
+			gpio_pin_configure_dt(
+				GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
+				GPIO_INPUT | GPIO_PULL_UP);
+		}
 		LOG_INF("Board is Clamshell");
 	} else if (sensor_fwconfig == FORM_FACTOR_CONVERTIBLE) {
 		LOG_INF("Board is Convertible");
