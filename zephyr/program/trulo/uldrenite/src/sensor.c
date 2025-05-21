3c50722e044a8c42e0f19d6a2ee9cd13617f0bbe
Matt_Wang <Matt_Wang@compal.corp-partner.google.com>
2025-05-21 09:58:48 +0800

    uldrenite: disable GMR when ISH enabled
    Need to disable the GMR at ISH SKU. To avoid the tabletmode abnormal triggered.
    
    BUG=none
    TEST=ISH 180 SKU won't into tabletmode.
    
    Change-Id: I830208e3f42c65b5d7ce01fdf39ba482aaafc03a

--- zephyr/program/trulo/uldrenite/src/sensor.c
+++ zephyr/program/trulo/uldrenite/src/sensor.c
@@ -48,6 +48,30 @@ void lid_accel_interrupt(enum gpio_signal signal)
 		bma4xx_interrupt(signal);
 }
 
+void disable_tabletmode(void)
+{
+	int ret;
+
+	ret = cros_cbi_get_fw_config(FORM_FACTOR, &sensor_fwconfig);
+	if (ret < 0) {
+		LOG_ERR("error retriving CBI config: %d", ret);
+		return;
+	}
+
+	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
+		if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
+			/* Only valid in EC */
+			gmr_tablet_switch_disable();
+		}
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
+				      GPIO_INPUT | GPIO_PULL_UP);
+		LOG_INF("Board is Clamshell");
+	} else if (sensor_fwconfig == FORM_FACTOR_CONVERTIBLE) {
+		LOG_INF("Board is Convertible");
+	}
+}
+
 static void motionsense_init(void)
 {
 	int ret;
@@ -74,28 +98,15 @@ static void motionsense_init(void)
 				GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
 				GPIO_DISCONNECTED);
 			LOG_INF("No motionsense");
+
+			/* for board version >= 2 */
+			disable_tabletmode();
 			return;
 		}
 	}
 
-	ret = cros_cbi_get_fw_config(FORM_FACTOR, &sensor_fwconfig);
-	if (ret < 0) {
-		LOG_ERR("error retriving CBI config: %d", ret);
-		return;
-	}
-
-	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
-		if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
-			/* Only valid in EC */
-			gmr_tablet_switch_disable();
-		}
-		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
-		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
-				      GPIO_INPUT | GPIO_PULL_UP);
-		LOG_INF("Board is Clamshell");
-	} else if (sensor_fwconfig == FORM_FACTOR_CONVERTIBLE) {
-		LOG_INF("Board is Convertible");
-	}
+	/* for board version 0, 1 */
+	disable_tabletmode();
 }
 DECLARE_HOOK(HOOK_INIT, motionsense_init, HOOK_PRIO_DEFAULT);
 
