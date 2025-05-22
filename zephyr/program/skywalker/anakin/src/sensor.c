7df9e4ffb37a5c6675818743ec103be1bce6e799
Jiahong Wei <weijiahong@huaqin.corp-partner.google.com>
2025-05-22 14:06:49 +0800

    anakin: disable unused function in Clamshell configuration
    Anakin has some SKUs whose machine configurations do not
    support tablet mode.
    
    BUG=b:418878607
    TEST=1.tablet mode cannot be triggered
         2.there is no EC complaint about sensor init failed
    LOW_COVERAGE_REASON=Tests will be added in b:418878607
    
    Change-Id: I4131d8902c594403054ef36238c1a95d9707509d

--- zephyr/program/skywalker/anakin/src/sensor.c
+++ zephyr/program/skywalker/anakin/src/sensor.c
@@ -4,14 +4,19 @@
  */
 
 #include "accelgyro.h"
+#include "common.h"
 #include "cros_cbi.h"
 #include "driver/accel_bma4xx.h"
 #include "driver/accel_lis2dw12_public.h"
 #include "driver/accelgyro_bmi3xx.h"
 #include "driver/accelgyro_lsm6dsm.h"
+#include "gpio/gpio_int.h"
 #include "hooks.h"
+#include "motion_sense.h"
 #include "motionsense_sensors.h"
+#include "tablet_mode.h"
 
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(board_sensor, LOG_LEVEL_INF);
@@ -48,3 +53,39 @@ static void alt_sensor_init(void)
 	motion_sensors_check_ssfc();
 }
 DECLARE_HOOK(HOOK_INIT, alt_sensor_init, HOOK_PRIO_POST_I2C);
+
+static bool board_is_clamshell;
+
+static void board_setup_init(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FORM_FACTOR, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FORM_FACTOR);
+		return;
+	}
+	if (val == CLAMSHELL) {
+		board_is_clamshell = true;
+		motion_sensor_count = 0;
+		gmr_tablet_switch_disable();
+	}
+}
+DECLARE_HOOK(HOOK_INIT, board_setup_init, HOOK_PRIO_PRE_DEFAULT);
+
+static void disable_base_imu_irq(void)
+{
+	if (board_is_clamshell) {
+		gpio_disable_dt_interrupt(
+			GPIO_INT_FROM_NODELABEL(int_lid_accel));
+		gpio_pin_configure_dt(
+			GPIO_DT_FROM_NODELABEL(gpio_lid_accel_int_ec_l),
+			GPIO_INPUT | GPIO_PULL_DOWN);
+
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_imu_int_ec_l),
+				      GPIO_INPUT | GPIO_PULL_DOWN);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, disable_base_imu_irq, HOOK_PRIO_POST_DEFAULT);
