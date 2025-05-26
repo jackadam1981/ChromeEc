e024a42993628d03bc85da7be8901086d5b68162
Abin Roy <abin.roy@7rays.corp-partner.google.com>
2025-05-26 14:44:16 +0530

    Ciri: Modify Get Source info message
    The dpm_get_source_info_msg function is now overridden in order to set
    the source_info.port_type = PD_SOURCE_PORT_CAPABILITY_MANAGED and
    source_info.port_maximum_pdp = 15W. With this update, the device always
    advertises 15W(5V3A for single port and 5V1.5A for dual ports).
    The CONFIG_USB_PD_3A_PORTS is still set to 0. The test passes without
    any failures and no modifications were made in ciri_vif.
    
    BUG=b:405324552
    TEST=Pass TEST.PD.PROT.SRC3.14 Source Info test
    
    Change-Id: I10c290d3ad69ebca7d627e5b81b11d01649bb9b8

--- zephyr/program/geralt/ciri/src/usbc.c
+++ zephyr/program/geralt/ciri/src/usbc.c
@@ -16,6 +16,8 @@
 #define PDO_FIXED_FLAGS \
 	(PDO_FIXED_DUAL_ROLE | PDO_FIXED_DATA_SWAP | PDO_FIXED_COMM_CAP)
 
+static uint32_t max_current_claimed_bitmask;
+
 static const uint32_t pd_src_pdo_1A5[] = {
 	PDO_FIXED(5000, 1500, PDO_FIXED_FLAGS),
 };
@@ -71,6 +73,38 @@ int dpm_get_source_current(const int port)
 	else
 		return 500;
 }
+union sido dpm_get_source_info_msg(int port)
+{
+	/* This implementation makes the following simplifying assumptions:
+	 * 1. The TCPM will only ever offer fixed 5V PDOs in its Source Caps.
+	 * 2. The TCPM will only offer 1.5A or 3A PDOs, so it will not be
+	 *    limited by any cable capabilities.
+	 */
+
+	union sido source_info; /* LCOV_EXCL_LINE: b/375430524 */
+	const uint32_t *pdos; /* LCOV_EXCL_LINE: b/375430524 */
+	int pdo_count = dpm_get_source_pdo(&pdos, port);
+
+	source_info.port_type = PD_SOURCE_PORT_CAPABILITY_MANAGED;
+
+	source_info.reserved = 0;
+
+	/* Max PDP: 5V * 3A = 15W*/
+	source_info.port_maximum_pdp = 15;
+
+	/* Reported PDP: voltage * current offered in Source Caps. */
+	uint32_t highest_pdo = pdos[pdo_count - 1];
+
+	source_info.port_reported_pdp = PDO_FIXED_VOLTAGE(highest_pdo) *
+					PDO_FIXED_CURRENT(highest_pdo) /
+					1000000;
+
+	/* Present PDP is the same as reported PDP as ciri can only source 3A
+	 * out of one port */
+	source_info.port_present_pdp = source_info.port_reported_pdp;
+
+	return source_info;
+}
 
 static int port_status[CONFIG_USB_PD_PORT_MAX_COUNT];
 static int port_pre_status[CONFIG_USB_PD_PORT_MAX_COUNT];
@@ -111,8 +145,18 @@ static void update_src_pdo_deferred(void)
 	}
 }
 
+static void update_max_current_claimed_bitmask(void)
+{
+	max_current_claimed_bitmask = 0;
+	for (int p = 0; p < board_get_usb_pd_port_count(); p++) {
+		if (port_status[p] == TYPEC_RP_3A0)
+			max_current_claimed_bitmask |= BIT(p);
+	}
+}
+
 static void manage_source_port(void)
 {
+	update_max_current_claimed_bitmask();
 	hook_call_deferred(&update_src_pdo_deferred_data, 0);
 }
 DECLARE_HOOK(HOOK_USB_PD_CONNECT, manage_source_port, HOOK_PRIO_DEFAULT);
@@ -120,6 +164,7 @@ DECLARE_HOOK(HOOK_USB_PD_DISCONNECT, manage_source_port, HOOK_PRIO_DEFAULT);
 
 static void manage_source_port_power_change(void)
 {
+	update_max_current_claimed_bitmask();
 	/* for FRS device status change check */
 	hook_call_deferred(&update_src_pdo_deferred_data, 500 * USEC_PER_MSEC);
 }
