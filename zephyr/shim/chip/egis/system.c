177ab45852ee67495c86895f0b14c1add72e9e6b
Dawid Niedzwiecki <dawidn@google.com>
2025-05-27 12:38:11 +0200

    zephyr: chip: add support for Egis ET171 SOC
    Add support for the Egis ET171 chip.
    
    It is needed for to use the ET171 chip, which will be used as a new FPMCU.
    
    BUG=b:460388761
    TEST=Get the sanok commit CL:6597060, build firmware and make sure it boots to
    RW
    TEST=CQ
    
    Change-Id: I7322cdec6016a2a7987377b5add75b8a52c8b4ab

---
+++ zephyr/shim/chip/egis/system.c
@@ -0,0 +1,11 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "system.h"
+
+uintptr_t system_get_fw_reset_vector(uintptr_t base)
+{
+	return base;
+}
