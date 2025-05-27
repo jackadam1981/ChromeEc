f8896322213d08a5d4454d5b2156d114e2b7d3bd
Dawid Niedzwiecki <dawidn@google.com>
2025-05-27 12:41:44 +0200

    WIP: zephyr: fpmcu: add sanok
    Change-Id: I5b00a75d3ed001f23e7e598e8db46ec7ae9777ec

---
+++ zephyr/program/fpmcu/sanok/src/fp_transport.c
@@ -0,0 +1,42 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "console.h"
+#include "host_command.h"
+#include "hwtimer.h"
+#include "printf.h"
+#include "uart.h"
+#include "usb_console.h"
+#include "util.h"
+
+#include <stdarg.h>
+#include <string.h>
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/linker/linker-defs.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/mgmt/ec_host_cmd/backend.h>
+#include <zephyr/mgmt/ec_host_cmd/ec_host_cmd.h>
+#include <zephyr/sys/util.h>
+
+void usb_set_mkbp(int active)
+{
+	if (active) {
+		ec_host_cmd_backend_usb_trigger_event();
+	}
+}
+
+int mkbp_set_host_active_via_custom(int active, uint32_t *timestamp)
+{
+	usb_set_mkbp(active);
+	if (timestamp) {
+		*timestamp = __hw_clock_source_read();
+	}
+
+	return EC_SUCCESS;
+}
