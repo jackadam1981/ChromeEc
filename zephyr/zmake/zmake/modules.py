6f6e270016aa7be8948e7843026b4f972fbef51f
Dawid Niedzwiecki <dawidn@google.com>
2025-05-27 12:41:44 +0200

    WIP: zephyr: fpmcu: add sanok
    Change-Id: I5b00a75d3ed001f23e7e598e8db46ec7ae9777ec

--- zephyr/zmake/zmake/modules.py
+++ zephyr/zmake/zmake/modules.py
@@ -57,6 +57,7 @@ known_modules = {
     "boringssl": lambda name, checkout: (
         checkout / "src" / "third_party" / name
     ),
+    "hal_egis": third_party_module,
     "hal_stm32": third_party_module,
     "chre": chre_module,
     "cmsis": third_party_module,
@@ -80,6 +81,7 @@ known_modules = {
     "focaltech_fp": lambda name, checkout: (
         checkout / "src" / "platform" / "fingerprint" / "focaltech"
     ),
+    "egis_module": third_party_module,
 }
 
 
