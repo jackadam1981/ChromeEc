31c617612d83625d7612eecb8d034479f4fc7794
Ting Shen <phoenixshen@google.com>
2025-05-28 17:36:40 +0800

    WIP: dp port priority POC
    BUG=none
    TEST=none
    
    Change-Id: Ie8dfb8508b8f673de3f47e548594ce310a3838c1

--- zephyr/program/skywalker/src/dp_alt_mode.c
+++ zephyr/program/skywalker/src/dp_alt_mode.c
@@ -5,6 +5,7 @@
 
 #include "chipset.h"
 #include "console.h"
+#include "hooks.h"
 #include "timer.h"
 #include "usb_pd.h"
 #include "usb_pd_dp_hpd_gpio.h"
@@ -17,50 +18,76 @@
 LOG_MODULE_REGISTER(skywalker_usbc, LOG_LEVEL_INF);
 
 uint64_t svdm_hpd_deadline[CONFIG_USB_PD_PORT_MAX_COUNT];
-static int active_aux_port = -1;
+static bool in_cooldown;
+static bool dp_attached[2];
 
 int svdm_get_hpd_gpio(int port)
 {
-	if (active_aux_port != port) {
-		return 0;
-	}
-
 	return gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_ap_dp_hpd_l));
-}
-
-static void set_dp_path_sel(int port)
-{
-	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_dp_aux_path_sel), port);
-	LOG_INF("Set DP_AUX_PATH_SEL: %d", port);
 }
 
 void svdm_set_hpd_gpio(int port, int en)
 {
-	/*
-	 * Implement FCFS policy:
-	 * 1) Enable hpd if no active port.
-	 * 2) Disable hpd if active port is the given port.
-	 */
-	if (en && active_aux_port < 0) {
-		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_ap_dp_hpd_l), 1);
-		active_aux_port = port;
-	}
-
-	if (!en && active_aux_port == port) {
-		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_ap_dp_hpd_l), 0);
-		active_aux_port = -1;
-	}
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_ap_dp_hpd_l), en);
 }
 
-static bool is_dp_muxable(int port)
+static void detach_cooldown(void)
 {
-	for (int i = 0; i < board_get_usb_pd_port_count(); i++) {
-		if (i != port && svdm_get_hpd_gpio(i)) {
-			return false;
+	in_cooldown = false;
+
+	if (dp_attached[0]) {
+		svdm_set_hpd_gpio(0, 1);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_dp_aux_path_sel), 0);
+	} else if (dp_attached[1]) {
+		svdm_set_hpd_gpio(1, 1);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_dp_aux_path_sel), 1);
+	}
+
+	LOG_INF("\x1b[1;31mcooldown expired\x1b[m");
+}
+DECLARE_DEFERRED(detach_cooldown);
+
+static bool is_active(int port)
+{
+	if (port == 0)
+		return dp_attached[0];
+	
+	return !dp_attached[0] && dp_attached[1];
+}
+
+static void attach_dp(int port)
+{
+	LOG_INF("\x1b[1;31mport %d attach\x1b[m", port);
+
+	if (in_cooldown) {
+		// do nothing
+	} else if (is_active(1) && port == 0) {
+		LOG_INF("\x1b[1;31mdetach c1, start 1s cd\x1b[m");
+		svdm_set_hpd_gpio(port, 0);
+		in_cooldown = true;
+		hook_call_deferred(&detach_cooldown_data, USEC_PER_SEC);
+	} else if (!is_active(0) && port == 1) {
+		svdm_set_hpd_gpio(port, 1);
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_dp_aux_path_sel), port);
+	}
+
+
+	dp_attached[port] = true;
+}
+
+static void detach_dp(int port)
+{
+	LOG_INF("\x1b[1;31mport %d detach\x1b[m", port);
+	if (is_active(port)) {
+		LOG_INF("\x1b[1;31mstart 1s cd\x1b[m");
+		svdm_set_hpd_gpio(port, 0);
+		if (!in_cooldown) {
+			in_cooldown = true;
+			hook_call_deferred(&detach_cooldown_data, USEC_PER_SEC);
 		}
 	}
 
-	return true;
+	dp_attached[port] = false;
 }
 
 static void skywalker_dp_attention(int port, uint32_t vdo_dp_status)
@@ -68,16 +95,17 @@ static void skywalker_dp_attention(int port, uint32_t vdo_dp_status)
 	int lvl = PD_VDO_DPSTS_HPD_LVL(vdo_dp_status);
 	int irq = PD_VDO_DPSTS_HPD_IRQ(vdo_dp_status);
 
-	if (!is_dp_muxable(port)) {
-		LOG_INF("p%d: The other port is already muxed.", port);
+	if (lvl) {
+		attach_dp(port);
+	} else {
+		detach_dp(port);
+	}
+
+	if (in_cooldown) {
 		return;
 	}
 
 	int cur_lvl = svdm_get_hpd_gpio(port);
-
-	if (lvl) {
-		set_dp_path_sel(port);
-	}
 
 	if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND) && (irq || lvl)) {
 		/*
@@ -120,12 +148,14 @@ static void skywalker_dp_attention(int port, uint32_t vdo_dp_status)
 	/* set the minimum time delay (2ms) for the next HPD IRQ */
 	svdm_hpd_deadline[port] = get_time().val + HPD_USTREAM_DEBOUNCE_LVL;
 
+	host_set_single_event(EC_HOST_EVENT_USB_MUX);
+
 	return;
 }
 
 static void skywalker_set_unattached(int port)
 {
-	svdm_set_hpd_gpio(port, 0);
+	detach_dp(port);
 }
 
 static int skywalker_pdc_cb_init(void)
