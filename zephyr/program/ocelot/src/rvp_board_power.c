5e51a450538c8e25a63aef1e21439e515070c405
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2025-05-28 17:04:26 -0700

    ap_pwrseq: Adding intialization to external power signal
    Some board specific power signals may require initialization routine.
    For instance, runtime power signal selection and corresponding
    interrupt handler initialization.
    
    This CL enables providing such initialization routine. Projects
    instantiating `intel,ap-pwrseq-external` must also define function
    `power_signal_external_init`.
    
    BUG=none
    TEST=none
    
    Change-Id: I6aec8815221c329c152b803280e76fb15b62d369

--- zephyr/program/ocelot/src/rvp_board_power.c
+++ zephyr/program/ocelot/src/rvp_board_power.c
@@ -76,6 +76,11 @@ static int board_ap_power_action_g3_run(void *data)
 AP_POWER_APP_STATE_DEFINE(G3, board_ap_power_action_g3_entry,
 			  board_ap_power_action_g3_run, NULL);
 
+int power_signal_external_init(void)
+{
+	return 0;
+}
+
 int board_power_signal_get(enum power_signal signal)
 {
 	switch (signal) {
