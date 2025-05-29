c0a85725ea749574e385e1a938eda3e19664479d
Firas Sammoura <fsammoura@google.com>
2025-05-29 15:51:35 -0700

    zephyr: test: add rtc_npcx test
    BUG=b:419026385
    TEST=CQ
    
    Change-Id: I7f31629e9050a8cbfe1c7f8459026ab58c773275

---
+++ zephyr/test/hwtest/src/rtc_npcx9.c
@@ -0,0 +1,28 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "atomic.h"
+#include "system.h"
+#include "timer.h"
+
+#include <zephyr/ztest.h>
+
+static atomic_t interrupt_counter;
+static const int rtc_delay_seconds = 1;
+static atomic_t rtc_fired;
+
+ZTEST_SUITE(rtc_npcx9, NULL, NULL, NULL, NULL, NULL);
+
+ZTEST(rtc_npcx9, test_rtc_alarm_fired)
+{
+	atomic_clear(&interrupt_counter);
+	system_set_rtc_alarm(rtc_delay_seconds, 0);
+
+	k_sleep(K_SECONDS(2 * rtc_delay_seconds));
+
+	rtc_fired = atomic_get(&interrupt_counter);
+
+	zassert_equal(1, rtc_fired);
+	zassert_equal(0, system_get_rtc_alarm());
+}
