8d404d0ac2f69c2b7f8e79680eea84065555bcea
Keith Short <keithshort@chromium.org>
2025-05-30 10:49:20 -0600

    test: Add test case for extpower without host commands
    The extpower_gpio support can be built without host comamnd support.
    Add a test case that sets up this combination.
    
    BUG=none
    TEST=./twister -ivc -T zephyr/test/extpower
    
    Change-Id: I87d9a43405eb3549a513a491fb9427bd04d87092

--- zephyr/test/extpower/src/extpower.c
+++ zephyr/test/extpower/src/extpower.c
@@ -3,6 +3,7 @@
  * found in the LICENSE file.
  */
 
+#include "acok_emul.h"
 #include "extpower.h"
 #include "hooks.h"
 #include "host_command.h"
@@ -12,10 +13,13 @@
 #include <zephyr/ztest.h>
 
 static int ac_hook_count;
-static const struct device *acok_gpio_dev =
-	DEVICE_DT_GET(DT_GPIO_CTLR(DT_NODELABEL(gpio_acok_od), gpios));
-static const gpio_port_pins_t acok_pin =
-	DT_GPIO_PIN(DT_NODELABEL(gpio_acok_od), gpios);
+
+static void before_and_after(void *unused)
+{
+	if (IS_ENABLED(HAS_TASK_HOSTCMD)) {
+		host_clear_events(0xFFFFFFFF);
+	}
+}
 
 static void test_ac_change_hook(void)
 {
@@ -23,33 +27,26 @@ static void test_ac_change_hook(void)
 }
 DECLARE_HOOK(HOOK_AC_CHANGE, test_ac_change_hook, HOOK_PRIO_DEFAULT);
 
-static void set_ac(int on, bool wait)
-{
-	gpio_emul_input_set(acok_gpio_dev, acok_pin, on);
-
-	if (wait) {
-		k_msleep(CONFIG_PLATFORM_EC_EXTPOWER_DEBOUNCE_MS * 10);
-	}
-}
-
 ZTEST(extpower, test_extpower_gpio)
 {
 	set_ac(0, true);
 	ac_hook_count = 0;
 
-	host_clear_events(0xFFFFFFFF);
-
 	set_ac(1, true);
 	zassert_equal(ac_hook_count, 1);
 	zassert_equal(extpower_is_present(), 1);
 
-	zassert_true(host_is_event_set(EC_HOST_EVENT_AC_CONNECTED));
+	if (IS_ENABLED(HAS_TASK_HOSTCMD)) {
+		zassert_true(host_is_event_set(EC_HOST_EVENT_AC_CONNECTED));
+	}
 
 	set_ac(0, true);
 	zassert_equal(ac_hook_count, 2);
 	zassert_equal(extpower_is_present(), 0);
 
-	zassert_true(host_is_event_set(EC_HOST_EVENT_AC_DISCONNECTED));
+	if (IS_ENABLED(HAS_TASK_HOSTCMD)) {
+		zassert_true(host_is_event_set(EC_HOST_EVENT_AC_DISCONNECTED));
+	}
 }
 
 ZTEST(extpower, test_extpower_gpio_debounce)
@@ -60,16 +57,16 @@ ZTEST(extpower, test_extpower_gpio_debounce)
 	set_ac(0, true);
 	ac_hook_count = 0;
 
-	host_clear_events(0xFFFFFFFF);
-
 	set_ac(1, false);
 	k_msleep(CONFIG_PLATFORM_EC_EXTPOWER_DEBOUNCE_MS / 2);
 	set_ac(0, true);
 
 	zassert_equal(ac_hook_count, 0);
 
-	zassert_false(host_is_event_set(EC_HOST_EVENT_AC_CONNECTED));
-	zassert_false(host_is_event_set(EC_HOST_EVENT_AC_DISCONNECTED));
+	if (IS_ENABLED(HAS_TASK_HOSTCMD)) {
+		zassert_false(host_is_event_set(EC_HOST_EVENT_AC_CONNECTED));
+		zassert_false(host_is_event_set(EC_HOST_EVENT_AC_DISCONNECTED));
+	}
 }
 
-ZTEST_SUITE(extpower, NULL, NULL, NULL, NULL, NULL);
+ZTEST_SUITE(extpower, NULL, NULL, before_and_after, before_and_after, NULL);
