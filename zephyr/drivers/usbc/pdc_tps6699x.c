031980d5c3efb9d6d72a8e7b1aae40c99543c584
Abhishek Pandit-Subedi <abhishekpandit@google.com>
2025-06-03 02:25:06 +0000

    pdc: tps6699x: Move register init to ST_INIT
    Initialization of several key registers (interrupts, port control, etc)
    is currently done in pdc_init which is only called once when the driver
    is initialized. If the PDC resets for any reason without the EC
    resetting, this will result in the driver getting into a state where it
    is not configured correctly.
    
    This commit makes the following changes:
    * Moves critical register initialization to `st_init_run`
    * Delays `init_done` until after UCSI notifications have been configured
    * Adds a retry mechanism to initialization
    * Completed GAID task pushes the system back into INIT state
    
    BUG=b:421342154
    TEST=./twister -cv --coverage -T zephyr/test/pdc/
    
    Change-Id: Iae1912185a4ec9e085d6023a7e413326e4036548

--- zephyr/drivers/usbc/pdc_tps6699x.c
+++ zephyr/drivers/usbc/pdc_tps6699x.c
@@ -61,6 +61,10 @@ LOG_MODULE_REGISTER(tps6699x, CONFIG_USBC_LOG_LEVEL);
  */
 #define PDC_TI_SET_SINK_PATH_DELAY_MS (1000)
 
+/** @brief The number of times to try to initialize the driver before quitting.
+ */
+#define PDC_INIT_RETRY_MAX 3
+
 /**
  * @brief All raw_value data uses byte-0 for contains the register data was
  * written to, or read from, and byte-1 contains the length of said data. The
@@ -223,6 +227,8 @@ struct pdc_data_t {
 	struct pdc_info_t info;
 	/** Init done flag */
 	bool init_done;
+	/* Init attempt counter */
+	int init_attempt;
 	/** Callback data */
 	void *cb_data;
 	/** CCI Event */
@@ -482,6 +488,91 @@ static void tps_notify_new_power_contract(struct k_work *work)
 	}
 }
 
+static int pdc_interrupt_mask_init(struct pdc_data_t *data)
+{
+	struct pdc_config_t const *cfg = data->dev->config;
+	union reg_interrupt irq_mask = {
+		.pd_hardreset = 1,
+		.plug_insert_or_removal = 1,
+		.power_swap_complete = 1,
+		.fr_swap_complete = 1,
+		.data_swap_complete = 1,
+		.sink_ready = 1,
+		.new_contract_as_consumer = 1,
+		.ucsi_connector_status_change_notification = 1,
+		.power_event_occurred_error = 1,
+		.externl_dcdc_event_received = 1,
+	};
+
+	return tps_rw_interrupt_mask(&cfg->i2c, &irq_mask, I2C_MSG_WRITE);
+}
+
+static int pdc_port_control_init(struct pdc_data_t *data)
+{
+	struct pdc_config_t const *cfg = data->dev->config;
+	union reg_port_control pdc_port_control = {
+		.typec_current = 1,
+		.process_swap_to_sink = 1,
+		.process_swap_to_source = 1,
+		.automatic_cap_request = 1,
+		.auto_alert_enable = 1,
+		.process_swap_to_dfp = 1,
+		.automatic_id_request = 1,
+		.fr_swap_enabled = 1,
+		.deglitch_cnt_lo = 6,
+	};
+
+	return tps_rw_port_control(&cfg->i2c, &pdc_port_control, I2C_MSG_WRITE);
+}
+
+static int pdc_autonegotiate_sink_reset(struct pdc_data_t *data)
+{
+	union reg_autonegotiate_sink an_snk;
+	struct pdc_config_t const *cfg = data->dev->config;
+	int rv;
+
+	rv = tps_rw_autonegotiate_sink(&cfg->i2c, &an_snk, I2C_MSG_READ);
+	if (rv) {
+		LOG_ERR("Failed to read auto negotiate sink register.");
+		return rv;
+	}
+
+	an_snk.auto_compute_sink_min_power = 0;
+	an_snk.auto_compute_sink_min_voltage = 0;
+	an_snk.auto_compute_sink_max_voltage = 0;
+	an_snk.auto_neg_max_current = 3000 / 10;
+	an_snk.auto_neg_sink_min_required_power = 15000 / 250;
+	an_snk.auto_neg_max_voltage = 5000 / 50;
+	an_snk.auto_neg_min_voltage = 5000 / 50;
+
+	rv = tps_rw_autonegotiate_sink(&cfg->i2c, &an_snk, I2C_MSG_WRITE);
+	if (rv) {
+		LOG_ERR("Failed to write auto negotiate sink register.");
+		return rv;
+	}
+
+	return 0;
+}
+
+static int pdc_exit_dead_battery(struct pdc_data_t *data)
+{
+	struct pdc_config_t const *cfg = data->dev->config;
+	union reg_boot_flags pdc_boot_flags;
+	int rv;
+
+	rv = tps_rd_boot_flags(&cfg->i2c, &pdc_boot_flags);
+	if (rv) {
+		LOG_ERR("Read boot flags failed");
+		set_state(data, ST_ERROR_RECOVERY);
+		return rv;
+	}
+
+	if (pdc_boot_flags.dead_battery_flag) {
+		task_dbfg(data);
+	}
+	return 0;
+}
+
 static void st_irq_run(void *o)
 {
 	struct pdc_data_t *data = (struct pdc_data_t *)o;
@@ -586,6 +677,10 @@ static void st_init_entry(void *o)
 {
 	struct pdc_data_t *data = (struct pdc_data_t *)o;
 
+	/* Init is restarted. */
+	data->init_done = false;
+	data->init_attempt++;
+
 	print_current_state(data);
 }
 
@@ -597,6 +692,13 @@ static void st_init_run(void *o)
 
 	/* Do not start executing commands if suspended */
 	if (check_comms_suspended()) {
+		set_state(data, ST_SUSPENDED);
+		return;
+	}
+
+	/* If we've attempted init too many times, suspend instead. */
+	if (data->init_attempt >= PDC_INIT_RETRY_MAX) {
+		suspend_comms();
 		set_state(data, ST_SUSPENDED);
 		return;
 	}
@@ -615,6 +717,24 @@ static void st_init_run(void *o)
 		PDC_FWVER_GET_MINOR(data->info.fw_version),
 		PDC_FWVER_GET_PATCH(data->info.fw_version));
 
+	/* Setup I2C1 interrupt mask for this port */
+	rv = pdc_interrupt_mask_init(data);
+	if (rv < 0) {
+		LOG_ERR("Write interrupt mask failed");
+	}
+	rv = pdc_autonegotiate_sink_reset(data);
+	if (rv < 0) {
+		LOG_ERR("Reset autonegotiate_sink reg failed");
+	}
+	rv = pdc_port_control_init(data);
+	if (rv < 0) {
+		LOG_ERR("Write port control failed");
+	}
+	rv = pdc_exit_dead_battery(data);
+	if (rv < 0) {
+		LOG_ERR("Clear dead battery flag failed");
+	}
+
 	/* Set PDC notifications */
 	data->cmd = CMD_SET_NOTIFICATION_ENABLE;
 	/*
@@ -626,18 +746,6 @@ static void st_init_run(void *o)
 	/* Transition to the idle state */
 	set_state(data, ST_IDLE);
 	return;
-}
-
-static void st_init_exit(void *o)
-{
-	struct pdc_data_t *data = (struct pdc_data_t *)o;
-
-	/* Inform the driver that the init process is complete */
-	/* TODO: Make sure this makes sense if the next state is suspend. It may
-	 * be possible to remove ST_INIT entirely by doing this in the init
-	 * function.
-	 */
-	data->init_done = true;
 }
 
 static void st_idle_entry(void *o)
@@ -851,8 +959,12 @@ static void st_error_recovery_run(void *o)
 	/* Inform the system of the event */
 	call_cci_event_cb(data);
 
-	/* Transition to idle */
-	set_state(data, ST_IDLE);
+	if (data->init_done) {
+		/* Transition to idle */
+		set_state(data, ST_IDLE);
+	} else {
+		set_state(data, ST_INIT);
+	}
 	return;
 }
 
@@ -881,6 +993,7 @@ static void st_suspended_run(void *o)
 		return;
 	}
 
+	data->init_attempt = 0;
 	set_state(data, ST_INIT);
 }
 
@@ -1943,7 +2056,23 @@ static void st_task_wait_run(void *o)
 		goto data_out;
 	}
 
+	/* After trigger a reset (TASK_COMMAND_GAID), the command register
+	 * should zero out above since we've reset. At this point, we need to
+	 * re-init to recover.
+	 */
+	if (data->cmd == CMD_TRIGGER_PDC_RESET) {
+		data->init_attempt = 0;
+		set_state(data, ST_INIT);
+		return;
+	}
+
 	switch (data->cmd) {
+	case CMD_SET_NOTIFICATION_ENABLE:
+		/* Initialization for driver is done once notifications are
+		 * enabled. This flag is reset when the INIT state is entered.
+		 */
+		data->init_done = true;
+		break;
 	case CMD_SET_RDO:
 		/* Re-set sink enable until after aNEG completes. */
 		atomic_set(&data->sink_enable_possible, 0);
@@ -2038,8 +2167,13 @@ data_out:
 	/* Inform the system of the event */
 	call_cci_event_cb(data);
 
-	/* Transition to idle state */
-	set_state(data, ST_IDLE);
+	if (data->init_done) {
+		/* Transition to idle state */
+		set_state(data, ST_IDLE);
+	} else {
+		/* Re-try init since we didn't complete successfully. */
+		set_state(data, ST_INIT);
+	}
 	return;
 
 error_recovery:
@@ -2049,8 +2183,8 @@ error_recovery:
 /* Populate state table */
 static const struct smf_state states[] = {
 	[ST_IRQ] = SMF_CREATE_STATE(st_irq_entry, st_irq_run, NULL, NULL, NULL),
-	[ST_INIT] = SMF_CREATE_STATE(st_init_entry, st_init_run, st_init_exit,
-				     NULL, NULL),
+	[ST_INIT] =
+		SMF_CREATE_STATE(st_init_entry, st_init_run, NULL, NULL, NULL),
 	[ST_IDLE] = SMF_CREATE_STATE(st_idle_entry, st_idle_run, st_idle_exit,
 				     NULL, NULL),
 	[ST_ERROR_RECOVERY] = SMF_CREATE_STATE(st_error_recovery_entry,
@@ -2666,91 +2800,6 @@ static DEVICE_API(pdc, pdc_driver_api) = {
 	.set_ap_power_state = tps_set_ap_power_state,
 };
 
-static int pdc_interrupt_mask_init(struct pdc_data_t *data)
-{
-	struct pdc_config_t const *cfg = data->dev->config;
-	union reg_interrupt irq_mask = {
-		.pd_hardreset = 1,
-		.plug_insert_or_removal = 1,
-		.power_swap_complete = 1,
-		.fr_swap_complete = 1,
-		.data_swap_complete = 1,
-		.sink_ready = 1,
-		.new_contract_as_consumer = 1,
-		.ucsi_connector_status_change_notification = 1,
-		.power_event_occurred_error = 1,
-		.externl_dcdc_event_received = 1,
-	};
-
-	return tps_rw_interrupt_mask(&cfg->i2c, &irq_mask, I2C_MSG_WRITE);
-}
-
-static int pdc_port_control_init(struct pdc_data_t *data)
-{
-	struct pdc_config_t const *cfg = data->dev->config;
-	union reg_port_control pdc_port_control = {
-		.typec_current = 1,
-		.process_swap_to_sink = 1,
-		.process_swap_to_source = 1,
-		.automatic_cap_request = 1,
-		.auto_alert_enable = 1,
-		.process_swap_to_dfp = 1,
-		.automatic_id_request = 1,
-		.fr_swap_enabled = 1,
-		.deglitch_cnt_lo = 6,
-	};
-
-	return tps_rw_port_control(&cfg->i2c, &pdc_port_control, I2C_MSG_WRITE);
-}
-
-static int pdc_autonegotiate_sink_reset(struct pdc_data_t *data)
-{
-	union reg_autonegotiate_sink an_snk;
-	struct pdc_config_t const *cfg = data->dev->config;
-	int rv;
-
-	rv = tps_rw_autonegotiate_sink(&cfg->i2c, &an_snk, I2C_MSG_READ);
-	if (rv) {
-		LOG_ERR("Failed to read auto negotiate sink register.");
-		return rv;
-	}
-
-	an_snk.auto_compute_sink_min_power = 0;
-	an_snk.auto_compute_sink_min_voltage = 0;
-	an_snk.auto_compute_sink_max_voltage = 0;
-	an_snk.auto_neg_max_current = 3000 / 10;
-	an_snk.auto_neg_sink_min_required_power = 15000 / 250;
-	an_snk.auto_neg_max_voltage = 5000 / 50;
-	an_snk.auto_neg_min_voltage = 5000 / 50;
-
-	rv = tps_rw_autonegotiate_sink(&cfg->i2c, &an_snk, I2C_MSG_WRITE);
-	if (rv) {
-		LOG_ERR("Failed to write auto negotiate sink register.");
-		return rv;
-	}
-
-	return 0;
-}
-
-static int pdc_exit_dead_battery(struct pdc_data_t *data)
-{
-	struct pdc_config_t const *cfg = data->dev->config;
-	union reg_boot_flags pdc_boot_flags;
-	int rv;
-
-	rv = tps_rd_boot_flags(&cfg->i2c, &pdc_boot_flags);
-	if (rv) {
-		LOG_ERR("Read boot flags failed");
-		set_state(data, ST_ERROR_RECOVERY);
-		return rv;
-	}
-
-	if (pdc_boot_flags.dead_battery_flag) {
-		task_dbfg(data);
-	}
-	return 0;
-}
-
 static void pdc_interrupt_callback(const struct device *dev,
 				   struct gpio_callback *cb, uint32_t pins)
 {
@@ -2816,28 +2865,6 @@ static int pdc_init(const struct device *dev)
 
 	/* Create the thread for this port */
 	cfg->create_thread(dev);
-
-	/* Setup I2C1 interrupt mask for this port */
-	rv = pdc_interrupt_mask_init(data);
-	if (rv < 0) {
-		LOG_ERR("Write interrupt mask failed");
-		return rv;
-	}
-	rv = pdc_autonegotiate_sink_reset(data);
-	if (rv < 0) {
-		LOG_ERR("Reset autonegotiate_sink reg failed");
-		return rv;
-	}
-	rv = pdc_port_control_init(data);
-	if (rv < 0) {
-		LOG_ERR("Write port control failed");
-		return rv;
-	}
-	rv = pdc_exit_dead_battery(data);
-	if (rv < 0) {
-		LOG_ERR("Clear dead battery flag failed");
-		return rv;
-	}
 
 	/* Trigger an interrupt on startup */
 	k_event_post(&data->pdc_event, PDC_IRQ_EVENT);
--- zephyr/emul/emul_tps6699x.c
+++ zephyr/emul/emul_tps6699x.c
@@ -69,6 +69,29 @@ static bool register_is_valid(const struct tps6699x_emul_pdc_data *data,
 	return reg < sizeof(data->reg_val) / sizeof(*data->reg_val);
 }
 
+#define REG_BITMAP_INDEX(reg) ((reg) / 32)
+#define REG_BITMAP_OFFSET(reg) ((reg) % 32)
+
+static bool register_read_should_fail(const struct tps6699x_emul_pdc_data *data,
+				      int reg)
+{
+	return data->fail_reg_reads[REG_BITMAP_INDEX(reg)] &
+	       BIT(REG_BITMAP_OFFSET(reg));
+}
+
+static bool
+register_write_should_fail(const struct tps6699x_emul_pdc_data *data, int reg)
+{
+	return data->fail_reg_writes[REG_BITMAP_INDEX(reg)] &
+	       BIT(REG_BITMAP_OFFSET(reg));
+}
+
+static void clear_register_fail_bit(const struct tps6699x_emul_pdc_data *data,
+				    int reg, uint32_t *fail_bitmap)
+{
+	fail_bitmap[REG_BITMAP_INDEX(reg)] &= ~BIT(REG_BITMAP_OFFSET(reg));
+}
+
 /** Check that a register access is valid. A valid access has
  *  1) a valid register address,
  *  2) a byte offset less than the size of that register, and
@@ -274,6 +297,17 @@ static void tps6699x_emul_handle_ucsi(struct tps6699x_emul_pdc_data *data,
 	if (cmd != UCSI_SET_PDOS) {
 		zassert_equal(data_len, 0);
 	}
+
+	if (data->fail_next_ucsi_cmd_count > 0 &&
+	    cmd == data->fail_next_ucsi_cmd) {
+		data_reg[0] = data->fail_next_ucsi_cmd_with_response;
+		data->fail_next_ucsi_cmd_count--;
+		LOG_INF("Fail UCSI command 0x%X (remaining fails: %d)",
+			data->fail_next_ucsi_cmd,
+			data->fail_next_ucsi_cmd_count);
+		return;
+	}
+
 	/* TODO(b/345292002): Validate connector number field. */
 
 	LOG_INF("UCSI command 0x%X", cmd);
@@ -448,6 +482,14 @@ static int tps6699x_emul_handle_sbud(struct tps6699x_emul_pdc_data *data,
 	return 0;
 }
 
+static int tps6699x_emul_handle_gaid(struct tps6699x_emul_pdc_data *data,
+				     uint8_t *data_reg)
+{
+	LOG_INF("GAID TASK");
+	data_reg[0] = TASK_COMPLETED_SUCCESSFULLY;
+	return 0;
+}
+
 static void delayable_work_handler(struct k_work *w)
 {
 	struct k_work_delayable *dwork = k_work_delayable_from_work(w);
@@ -486,6 +528,9 @@ static void tps6699x_emul_handle_command(struct tps6699x_emul_pdc_data *data,
 		break;
 	case COMMAND_TASK_SBUD:
 		tps6699x_emul_handle_sbud(data, data_reg);
+		break;
+	case COMMAND_TASK_GAID:
+		tps6699x_emul_handle_gaid(data, data_reg);
 		break;
 	default: {
 		char task_str[5] = {
@@ -568,6 +613,11 @@ static int tps6699x_emul_start_write(const struct emul *emul, int reg)
 		return -EIO;
 	}
 
+	if (register_write_should_fail(data, reg)) {
+		clear_register_fail_bit(data, reg, data->fail_reg_writes);
+		return -EIO;
+	}
+
 	data->reg_addr = reg;
 
 	return 0;
@@ -635,6 +685,11 @@ static int tps6699x_emul_start_read(const struct emul *emul, int reg)
 	struct tps6699x_emul_pdc_data *data = tps6699x_emul_get_pdc_data(emul);
 
 	if (!register_is_valid(data, reg)) {
+		return -EIO;
+	}
+
+	if (register_read_should_fail(data, reg)) {
+		clear_register_fail_bit(data, reg, data->fail_reg_reads);
 		return -EIO;
 	}
 
@@ -1030,6 +1085,11 @@ static int emul_tps6699x_reset(const struct emul *target)
 	data->frs_configured = false;
 	data->port_control = *pdc_port_control;
 
+	/* Reset failure structures. */
+	memset(data->fail_reg_reads, 0, REG_U32_BITMAP_SIZE);
+	memset(data->fail_reg_writes, 0, REG_U32_BITMAP_SIZE);
+	data->fail_next_ucsi_cmd_count = 0;
+
 	return 0;
 }
 
@@ -1200,6 +1260,55 @@ static int tps6699x_emul_get_data_role_preference(const struct emul *target,
 
 	*swap_to_dfp = pdc_port_control->initiate_swap_to_dfp;
 	*swap_to_ufp = pdc_port_control->initiate_swap_to_ufp;
+
+	return 0;
+}
+
+int emul_pdc_fail_reg_read(const struct emul *target, uint8_t reg)
+{
+	struct tps6699x_emul_pdc_data *data =
+		tps6699x_emul_get_pdc_data(target);
+
+	if (reg > TPS6699X_MAX_REG) {
+		return -EINVAL;
+	}
+
+	data->fail_reg_reads[REG_BITMAP_INDEX(reg)] |=
+		BIT(REG_BITMAP_OFFSET(reg));
+
+	return 0;
+}
+
+int emul_pdc_fail_reg_write(const struct emul *target, uint8_t reg)
+{
+	struct tps6699x_emul_pdc_data *data =
+		tps6699x_emul_get_pdc_data(target);
+
+	if (reg > TPS6699X_MAX_REG) {
+		return -EINVAL;
+	}
+
+	data->fail_reg_writes[REG_BITMAP_INDEX(reg)] |=
+		BIT(REG_BITMAP_OFFSET(reg));
+
+	return 0;
+}
+
+int emul_pdc_fail_next_ucsi_command(const struct emul *target,
+				    enum ucsi_command_t command,
+				    enum std_task_response with_response,
+				    uint8_t num_times)
+{
+	struct tps6699x_emul_pdc_data *data =
+		tps6699x_emul_get_pdc_data(target);
+
+	if (with_response == TASK_COMPLETED_SUCCESSFULLY) {
+		return -EINVAL;
+	}
+
+	data->fail_next_ucsi_cmd = command;
+	data->fail_next_ucsi_cmd_count = num_times;
+	data->fail_next_ucsi_cmd_with_response = with_response;
 
 	return 0;
 }
