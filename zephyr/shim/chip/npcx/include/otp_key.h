814331d16d1e4cd9147791e4808a3798e4a6af70
David Cross <davidmcross@google.com>
2025-06-03 15:42:41 -0700

    npcx: add OTP Key driver to Zephyr shim
    BUG=b:421472949
    TEST=./util/compare_build.sh -b all
    
    Change-Id: I721347a0f683478b19b0ce1ad13268ee87de8acb

---
+++ zephyr/shim/chip/npcx/include/otp_key.h
@@ -0,0 +1,56 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_OTP_KEY_H
+#define __CROS_EC_OTP_KEY_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <common.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define OTP_KEY_SIZE_BYTES 32
+#define OTP_KEY_ADDR 0x300
+
+/**
+ * Initialize One-Time Programmable (OTP) Memory for Key Storage
+ *
+ * Not supported by all platforms.
+ **/
+void otp_key_init(void);
+
+/**
+ * Read key from OTP
+ *
+ * Not supported by all platforms.
+ **/
+enum ec_error_list otp_key_read(uint8_t *key_buffer);
+
+/**
+ * Provision Key in OTP, check if blank (unprovisioned) and write if so
+ *
+ * Not supported by all platforms.
+ **/
+enum ec_error_list otp_key_provision(void);
+
+/**
+ * Shutdown OTP
+ *
+ * The opposite operation of otp_init(), disable the hardware resources
+ * used by OTP memory to save power.
+ *
+ * Not supported by all platforms.
+ **/
+void otp_key_exit(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CROS_EC_OTP_KEY_H */
