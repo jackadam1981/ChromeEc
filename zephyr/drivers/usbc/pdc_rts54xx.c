2fbf6c7ac7eda3d7540ea3d6915c5d37e12d450b
Ivan Chen <yulunchen@google.com>
2025-06-09 15:06:56 +0800

    pdc: rts54xx: Add support for retimer enable/disable
    This CL implement the new PDC API command CMD_PDC_SET_AP_POWER_STATE in
    rts54xx driver to inform the PDC of the current AP power state to support
    enabling/disabling 3rd party retimer chips.
    
    BUG=b:378751852
    TEST=Verify retimer pins and the init function is resent when transit
    LOW_COVERAGE_REASON=Same test will be tracked by b/378956877
    
    Change-Id: Ic08db7c4a748fbb4c4f5ef0178db50875b1d0ac7

--- zephyr/drivers/usbc/pdc_rts54xx.c
+++ zephyr/drivers/usbc/pdc_rts54xx.c
@@ -140,6 +140,7 @@ static const struct smbus_cmd_t SET_TPC_RECONNECT = { 0x08, 0x03, 0x1F };
 static const struct smbus_cmd_t FORCE_SET_POWER_SWITCH = { 0x08, 0x03, 0x21 };
 static const struct smbus_cmd_t GET_RDO = { 0x08, 0x02, 0x84 };
 static const struct smbus_cmd_t GET_VDO = { 0x08, 0x03, 0x9A };
+static const struct smbus_cmd_t SET_SYS_PWR_STATE = { 0x08, 0x03, 0x2B };
 static const struct smbus_cmd_t GET_CURRENT_PARTNER_SRC_PDO = { 0x08, 0x02,
 								0xA7 };
 static const struct smbus_cmd_t RTS_SET_FRS_FUNCTION = { 0x08, 0x03, 0xE1 };
@@ -303,6 +304,8 @@ enum cmd_t {
 	CMD_GET_SBU_MUX_MODE,
 	/** CMD_SET_SBU_MUX_MODE */
 	CMD_SET_SBU_MUX_MODE,
+	/** CMD_SET_SYS_PWR_STATE */
+	CMD_SET_SYS_PWR_STATE,
 };
 
 /**
@@ -442,6 +445,7 @@ static const char *const cmd_names[] = {
 	[CMD_GET_ATTENTION_VDO] = "CMD_GET_ATTENTION_VDO",
 	[CMD_GET_SBU_MUX_MODE] = "CMD_GET_SBU_MUX_MODE",
 	[CMD_SET_SBU_MUX_MODE] = "CMD_SET_SBU_MUX_MODE",
+	[CMD_SET_SYS_PWR_STATE] = "CMD_SET_SYS_PWR_STATE",
 };
 
 /**
@@ -2344,6 +2348,36 @@ static int rts54_set_frs(const struct device *dev, bool enable)
 				  ARRAY_SIZE(payload), NULL);
 }
 
+static int rts54_set_ap_power_state(const struct device *dev,
+				    enum power_state state)
+{
+	struct pdc_data_t *data = dev->data;
+	int byte;
+
+	if (get_state(data) != ST_IDLE) {
+		return -EBUSY;
+	}
+
+	if (state == POWER_S0) {
+		byte = SX_S0;
+	} else if (state == POWER_S5) {
+		byte = SX_S5;
+	} else {
+		return -EINVAL;
+	}
+
+	uint8_t payload[] = {
+		SET_SYS_PWR_STATE.cmd,
+		SET_SYS_PWR_STATE.len,
+		SET_SYS_PWR_STATE.sub,
+		0x00,
+		byte,
+	};
+
+	return rts54_post_command(dev, CMD_SET_SYS_PWR_STATE, payload,
+				  ARRAY_SIZE(payload), NULL);
+}
+
 static int rts54_get_identity_discovery(const struct device *dev,
 					bool *disc_state)
 {
@@ -2751,6 +2785,7 @@ static DEVICE_API(pdc, pdc_driver_api) = {
 	.get_sbu_mux_mode = rts54_get_sbu_mux_mode,
 	.set_sbu_mux_mode = rts54_set_sbu_mux_mode,
 #endif /* define(CONFIG_USBC_PDC_DRIVEN_CCD) */
+	.set_ap_power_state = rts54_set_ap_power_state,
 };
 
 static int pdc_init(const struct device *dev)
