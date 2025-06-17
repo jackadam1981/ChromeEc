e44f65e9454649b2a644e2e9670b172f80b30020
Firas Sammoura <fsammoura@google.com>
2025-06-17 14:10:57 -0700

    test: Only use builtin stdlib for host board
    The previous change at https://crrev.com/c/5166769 incorrectly applied
    the builtin stdlib to all boards when USE_BUILTIN_STDLIB was 0,
    including the fingerprint boards. This commit restricts the use of
    builtin stdlib in `test/stdlib` to only the host board, ensuring correct
    stdlib logic is tested on fingerprint boards.
    
    BUG=b:419312449
    TEST=./test/run_device_tests.py -b helipilot -f servo_micro -t stdlib
    
    Change-Id: I732a2c66e31ecd0922574a12514287fe26a3dc6b

--- test/stdlib.c
+++ test/stdlib.c
@@ -19,8 +19,13 @@
 /* This is ugly, but we want to test the functions in builtin/stdlib.c while
  * still depending on the system stdlib.c
  */
-#define snprintf TESTED_snprintf
+#ifdef BOARD_HOST
+#define snprintf TESTED_snprintf #define snprintf TESTED_snprintf
+
 #include "../builtin/stdlib.c"
+#else
+#include <stdlib.h>
+#endif
 #endif
 
 __no_optimization static int test_isalpha(void)
