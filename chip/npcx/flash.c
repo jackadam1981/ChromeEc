789d492573e75b06e6e2f0bce2dfca5e2b3dbae9
Rob Barnes <robbarnes@google.com>
2025-06-17 15:20:42 -0600

    mutex: Add common recursive mutex support
    This change adds support for a common recursive implementation of
    mutex. Nested/recurive locks are supported within the same task.
    
    The exiting libc/recurisve_mutex implmentation is leveraged by
    symlinking the files to common/.
    
    Since non-recursive mutexes are prone to deadlock that result in
    watchdogs, this implementation is turned on by default.
    
    BUG=b:425486970,b:418077172
    TEST=Nested locks do not cause watchdog, tested on ITE and NPCX
    
    Change-Id: Ifb01e02c7abe36f74655e4f4263ad977268aac72

--- chip/npcx/flash.c
+++ chip/npcx/flash.c
@@ -78,7 +78,12 @@ static void flash_execute_cmd(uint8_t code, uint8_t cts)
 	 * Flash mutex must be held while executing UMA commands after
 	 * task_start().
 	 */
-	ASSERT(!task_start_called() || flash_lock.lock);
+	ASSERT(!task_start_called());
+#ifdef CONFIG_COMMON_RECURSIVE_MUTEX
+	ASSERT(flash_lock.state == MUTEX_R_LOCKED);
+#else
+	ASSERT(flash_lock.lock);
+#endif
 
 	/* set UMA_CODE */
 	NPCX_UMA_CODE = code;
