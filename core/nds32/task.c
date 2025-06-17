312f9d65b553908db9ae4dd6bbbf8c115ee98d07
Rob Barnes <robbarnes@google.com>
2025-06-17 15:20:42 -0600

    mutex: Add common recursive mutex support
    This change adds support for a common recursive implementation of
    mutex. Nested or recurive locks are supported within the same class.
    
    The exiting libc/recurisve_mutex implmentation is leveraged by
    symlinking the files to common/.
    
    Since non-recursive mutexes are prone to deadlock that result in
    watchdogs, this implementation is on by default.
    
    BUG=b:425486970,b:418077172
    TEST=Nested locks do not cause watchdog, tested on ITE and NPCX
    
    Change-Id: Ifb01e02c7abe36f74655e4f4263ad977268aac72

--- core/nds32/task.c
+++ core/nds32/task.c
@@ -630,6 +630,8 @@ static void ivic_init_irqs(void)
 	set_int_priority(all_priorities);
 }
 
+#ifndef CONFIG_COMMON_RECURSIVE_MUTEXES
+
 void __ram_code mutex_lock(struct mutex *mtx)
 {
 	uint32_t id = 1 << task_get_current();
@@ -686,6 +688,8 @@ void __ram_code mutex_unlock(struct mutex *mtx)
 	/* Ensure no event is remaining from mutex wake-up */
 	atomic_clear_bits(&tsk->events, TASK_EVENT_MUTEX);
 }
+
+#endif /* !CONFIG_COMMON_RECURSIVE_MUTEXES */
 
 void task_print_list(void)
 {
