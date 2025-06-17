druth0/chrome-ec

commit 1c316fc5d23ce3e7a83c4740e276ba57874ae8e3

motion_sense_fifo: Spread using the expected period
Since the timestamp from the ISR can be trusted, spread along
expected period.
It removes the incertanty of the task scheduling.

BUG=b:413177152
TEST=Check hardware.SensorIioserviceHard passes on Joxer.

Change-Id: I8cd8a6f56f6064f834c4e392960f0d2743b61dfc
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6492254
Reviewed-by: Yuval Peress <peress@google.com>
Code-Coverage: Gwendal Grignou <gwendal@chromium.org>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Gwendal Grignou <gwendal@chromium.org>

common/motion_sense_fifo.c
@@ -72,12 +72,6 @@ static struct timestamp_state next_timestamp[MAX_MOTION_SENSORS];
 static uint32_t expected_data_periods[MAX_MOTION_SENSORS];
 
 /**
- * Calculated data periods:
- * can be different from collection rate when spreading.
- */
-static uint32_t data_periods[MAX_MOTION_SENSORS];
-
-/**
  * Bitmap telling which sensors have valid entries in the next_timestamp array.
  */
 static uint32_t next_timestamp_initialized;
@@ -494,7 +488,7 @@ void motion_sense_fifo_stage_data(struct ec_response_motion_sensor_data *data,
 void motion_sense_fifo_commit_data(void)
 {
 	struct ec_response_motion_sensor_data *data;
-	int i, window, sensor_num;
+	int i, sensor_num;
 
 	/* Nothing staged, no work to do. */
 	if (!fifo_staged.count)
@@ -520,29 +514,6 @@ void motion_sense_fifo_commit_data(void)
 	if (!is_timestamp(data)) {
 		CPRINTS("Spreading skipped, first entry is not a timestamp");
 		fifo_staged.requires_spreading = 0;
-		goto commit_data_end;
-	}
-
-	window = time_until(data->timestamp, fifo_staged.read_ts);
-
-	/* Update the data_periods as needed for this flush. */
-	for (i = 0; i < MAX_MOTION_SENSORS; i++) {
-		int period;
-
-		/* Skip empty sensors. */
-		if (!fifo_staged.sample_count[i])
-			continue;
-
-		period = expected_data_periods[i];
-		/*
-		 * Clamp the sample period to the MIN of collection_rate and the
-		 * window length / (sample count - 1).
-		 */
-		if (window && fifo_staged.sample_count[i] > 1)
-			period =
-				MIN(period,
-				    window / (fifo_staged.sample_count[i] - 1));
-		data_periods[i] = period;
 	}
 
 commit_data_end:
@@ -589,7 +560,7 @@ commit_data_end:
 		 */
 		if (is_new_timestamp(sensor_num) ||
 		    time_after(data->timestamp,
-			       next_timestamp[sensor_num].prev)) {
+			       next_timestamp[sensor_num].next)) {
 			next_timestamp[sensor_num].next = data->timestamp;
 			next_timestamp_initialized |= BIT(sensor_num);
 		}
@@ -599,9 +570,7 @@ commit_data_end:
 		next_timestamp[sensor_num].prev =
 			next_timestamp[sensor_num].next;
 		next_timestamp[sensor_num].next +=
-			fifo_staged.requires_spreading ?
-				data_periods[sensor_num] :
-				expected_data_periods[sensor_num];
+			expected_data_periods[sensor_num];
 
 		/* Update online calibration if enabled. */
 		data = peek_fifo_staged(i);

test/motion_sense_fifo.c
@@ -285,19 +285,19 @@ static int test_spread_data_in_window(void)
 	motion_sense_set_data_period(0, 20 /* us */);
 	now = __hw_clock_source_read();
 
-	motion_sense_fifo_stage_data(data, motion_sensors, 3, now - 18);
-	motion_sense_fifo_stage_data(data, motion_sensors, 3, now - 18);
+	motion_sense_fifo_stage_data(data, motion_sensors, 3, now - 22);
+	motion_sense_fifo_stage_data(data, motion_sensors, 3, now - 22);
 	motion_sense_fifo_commit_data();
 	read_count = motion_sense_fifo_read(
 		sizeof(data), CONFIG_ACCEL_FIFO_SIZE, data, &data_bytes_read);
 	TEST_EQ(read_count, 4, "%d");
 	TEST_BITS_SET(data[0].flags, MOTIONSENSE_SENSOR_FLAG_TIMESTAMP);
-	TEST_EQ(data[0].timestamp, now - 18, "%u");
+	TEST_EQ(data[0].timestamp, now - 22, "%u");
 	TEST_BITS_SET(data[2].flags, MOTIONSENSE_SENSOR_FLAG_TIMESTAMP);
 	/* TODO(b/142892004): mock __hw_clock_source_read so we can check for
 	 * exact TS.
 	 */
-	TEST_NEAR(data[2].timestamp, now, 2, "%u");
+	TEST_NEAR(data[2].timestamp, now - 2, 2, "%u");
 
 	return EC_SUCCESS;
 }

jackadam1981/ChromeEc

commit d639acebb0ed9be4f8e0f37ed3707c568416401a

motion_sense_fifo: trust timestamp from handler when spreading
After sending data to host, reset the timestamp used for spreading.
It has been proven to be reliable, even when the EC is slow to process
sensor fifo.
Remove `read_ts` field as well, unused.

Fixes: 1c316fc5d23 ("motion_sense_fifo: Spread using the expected period")
BUG=b:413177152,b:417147201
TEST=Compile

Change-Id: I8db5820fff10587e58ff0682dfffa71a0ddbba48

common/motion_sense_fifo.c
@@ -19,8 +19,6 @@
 
 /**
  * Staged metadata for the fifo queue.
- * @read_ts: The timestamp at which the staged data was read. This value will
- *	serve as the upper bound for spreading
  * @count: The total number of motion_sense_fifo entries that are currently
  *	staged.
  * @sample_count: The total number of sensor readings per sensor that are
@@ -29,20 +27,9 @@
  *	true iff at least one of sample_count[] > 1
  */
 struct fifo_staged {
-	uint32_t read_ts;
 	uint16_t count;
 	uint8_t sample_count[MAX_MOTION_SENSORS];
 	uint8_t requires_spreading;
-};
-
-/**
- * Timestamp state metadata for maintaining spreading between commits.
- * @prev: The previous timestamp that was added to the FIFO
- * @next: The predicted next timestamp that will be added to the FIFO
- */
-struct timestamp_state {
-	uint32_t prev;
-	uint32_t next;
 };
 
 /** Queue to hold the data to be sent to the AP. */
@@ -63,7 +50,7 @@ static struct fifo_staged fifo_staged;
  * Cached expected timestamp per sensor. If a sensor's timestamp pre-dates this
  * timestamp it will be fast forwarded.
  */
-static struct timestamp_state next_timestamp[MAX_MOTION_SENSORS];
+static uint32_t next_timestamp[MAX_MOTION_SENSORS];
 
 /**
  * Expected data periods:
@@ -272,8 +259,7 @@ fifo_stage_unit(struct ec_response_motion_sensor_data *data,
 	 */
 	if (data->flags & MOTIONSENSE_SENSOR_FLAG_TIMESTAMP &&
 	    is_new_timestamp(data->sensor_num)) {
-		next_timestamp[data->sensor_num].next =
-			next_timestamp[data->sensor_num].prev = data->timestamp;
+		next_timestamp[data->sensor_num] = data->timestamp;
 		next_timestamp_initialized |= BIT(data->sensor_num);
 	}
 
@@ -293,7 +279,7 @@ fifo_stage_unit(struct ec_response_motion_sensor_data *data,
 			    !is_new_timestamp(data->sensor_num))
 				online_calibration_process_data(
 					data, sensor,
-					next_timestamp[data->sensor_num].next);
+					next_timestamp[data->sensor_num]);
 			return;
 		}
 	}
@@ -413,8 +399,9 @@ void motion_sense_fifo_reset_needed_flags(void)
 		 */
 		for (i = 0; i < MAX_MOTION_SENSORS; i++)
 			if (!is_new_timestamp(i))
-				ts_last_int[i] = next_timestamp[i].prev;
+				ts_last_int[i] = next_timestamp[i];
 	}
+	next_timestamp_initialized = 0;
 	wake_up_needed = 0;
 	bypass_needed = 0;
 }
@@ -445,9 +432,6 @@ void motion_sense_fifo_stage_data(struct ec_response_motion_sensor_data *data,
 	int id = data->sensor_num;
 
 	if (IS_ENABLED(CONFIG_SENSOR_TIGHT_TIMESTAMPS)) {
-		/* First entry, save the time for spreading later. */
-		if (!fifo_staged.count)
-			fifo_staged.read_ts = __hw_clock_source_read();
 		fifo_stage_timestamp(time, data->sensor_num);
 	}
 	/*
@@ -559,25 +543,21 @@ commit_data_end:
 		 * ahead.
 		 */
 		if (is_new_timestamp(sensor_num) ||
-		    time_after(data->timestamp,
-			       next_timestamp[sensor_num].next)) {
-			next_timestamp[sensor_num].next = data->timestamp;
+		    time_after(data->timestamp, next_timestamp[sensor_num])) {
+			next_timestamp[sensor_num] = data->timestamp;
 			next_timestamp_initialized |= BIT(sensor_num);
 		}
 
 		/* Spread the timestamp and compute the expected next. */
-		data->timestamp = next_timestamp[sensor_num].next;
-		next_timestamp[sensor_num].prev =
-			next_timestamp[sensor_num].next;
-		next_timestamp[sensor_num].next +=
-			expected_data_periods[sensor_num];
+		data->timestamp = next_timestamp[sensor_num];
+		next_timestamp[sensor_num] += expected_data_periods[sensor_num];
 
 		/* Update online calibration if enabled. */
 		data = peek_fifo_staged(i);
 		if (IS_ENABLED(CONFIG_ONLINE_CALIB))
 			online_calibration_process_data(
 				data, &motion_sensors[sensor_num],
-				next_timestamp[sensor_num].prev);
+				next_timestamp[sensor_num]);
 	}
 
 	/* Advance the tail and clear the staged metadata. */