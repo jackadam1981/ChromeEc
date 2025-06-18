5f484dfc718ec360b5e2fbd5d6238a65e6efd7d6
Brandon Breitenstein <brandon.breitenstein@intel.com>
2025-06-18 11:48:42 -0700

    Reland "pdc: Config charge manager seed to 0V/0A if disabling a port"
    This is a reland of commit 44e414aca6022674a5948a5e32f813b684e85635
    
    Original change's description:
    > pdc: Config charge manager seed to 0V/0A if disabling a port
    >
    > Due to changes to the initial seeding of the charge manager if a port is left
    > uninitialized the charge manager will not allow the system to boot without a
    > battery. In the case of a port is being disabled at a software level init the
    > charge manager to 0V/0A to avoid having uninitialized values.
    >
    > BUG=b:423808174
    > TEST=zmake build ptlrvp_mchp, verified previously not booting board boots
    > LOW_COVERAGE_REASON=Coverage to be handled in b:427712128
    >
    > Change-Id: Idd0346b0d255423c62ab7dc1863bebdd26311813
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6655359
    > Commit-Queue: caveh jalali <caveh@chromium.org>
    > Reviewed-by: Keith Short <keithshort@chromium.org>
    > Tested-by: Brandon Breitenstein <brandon.breitenstein@intel.com>
    > Reviewed-by: caveh jalali <caveh@chromium.org>
    
    Bug: b:423808174
    Change-Id: I1d7f37e8f705fa268678f4e23403d0e298ce9fd2

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -2531,6 +2531,20 @@ static enum smf_state_result pdc_snk_attached_run(void *obj)
 	return SMF_EVENT_HANDLED;
 }
 
+static void seed_charge_manager_default(int port)
+{
+	LOG_INF("Seeding charge manager due disabling port");
+	int supplier;
+	struct charge_port_info charge_init = {
+		.current = 0,
+		.voltage = 0,
+	};
+
+	for (supplier = 0; supplier < CHARGE_SUPPLIER_COUNT; supplier++) {
+		charge_manager_update_charge(supplier, port, &charge_init);
+	}
+}
+
 static void pdc_send_cmd_start_entry(void *obj)
 {
 	struct pdc_port_t *port = (struct pdc_port_t *)obj;
@@ -3283,6 +3297,7 @@ static enum smf_state_result pdc_init_run(void *obj)
 {
 	struct pdc_port_t *port = (struct pdc_port_t *)obj;
 	const struct pdc_config_t *const config = port->dev->config;
+	union error_status_t error_status;
 
 	switch (port->init_local_state) {
 	case INIT_WAIT_FOR_READY:
@@ -3304,6 +3319,21 @@ static enum smf_state_result pdc_init_run(void *obj)
 		}
 
 		port->init_local_state = INIT_SET_SINK_PDOS;
+
+		/* Check that the pdc driver did not disable the port during
+		 * runtime init. If it did seed the charge manager with 0V/0A.
+		 * Test case to be added in b/427712128
+		 */
+		if (pdc_get_error_status(port->pdc, &error_status) == 0) {
+			if (error_status != NULL) {
+				if (error_status.port_disabled) {
+					/* pdc disabled at runtime set default
+					 * charge seed */
+					seed_charge_manager_default(
+						config->connector_num);
+				}
+			}
+		}
 
 		/* Proceed directly to next sub-state */
 		__fallthrough;
@@ -3563,6 +3593,9 @@ disable_port:
 	 * driver thread in this code path, so nothing will happen for
 	 * this port. */
 	smf_set_initial(&port->ctx, &pdc_states[PDC_DISABLED]);
+
+	/* Seed charge manager with 0v/0A for the disabled port */
+	seed_charge_manager_default(config->connector_num);
 
 	return -ENODEV;
 }
