cd1a22b993a3ad8ddb35b43dd0afa87349926112
David <david1.huang@lcfc.corp-partner.google.com>
2025-06-19 17:30:19 +0800

    charge_state: Init battery info in hook_init
    Some hook_init functions will use batt_info parameter.
    Init batt_info earlier for these functions.
    
    BUG=b:426091929
    TEST=Check system can power on normally with battery.
    
    Change-Id: I739220bba2faf74ec7bfd078dc8a8ebee55ee9d7

--- common/charge_state.c
+++ common/charge_state.c
@@ -1606,6 +1606,12 @@ int charge_want_shutdown(void)
 	       (curr.batt.state_of_charge < battery_level_shutdown);
 }
 
+void init_battery_info(void)
+{
+	batt_info = battery_get_info();
+}
+DECLARE_HOOK(HOOK_INIT, init_battery_info, HOOK_PRIO_POST_BATTERY_INIT);
+
 #ifdef CONFIG_CHARGER_MIN_BAT_PCT_FOR_POWER_ON
 test_export_static int charge_prevent_power_on_automatic_power_on = 1;
 #endif
