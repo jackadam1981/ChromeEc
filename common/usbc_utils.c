03639329879f753e36483836094df0c40c079fc8
Abin Roy <abin.roy@7rays.corp-partner.google.com>
2025-06-19 13:48:05 +0530

    platform/ec/common: Modify pd_build_request to send a valid Battery RDO
    DUT is requesting battery power of 45W even though the SourceCaps only
    advertises 15W. Modifying the pd_build_request to send a request with
    maximum operational power of 15W for PDO_TYPE_BATTERY fixes the issue
    and passes the test.
    
    BUG=b:422653414
    TEST=Pass TEST.PD.PS.SRC.3 on Anraggar
    
    Change-Id: Id5924191cca5a263a566be3a0e7b68ffc37965b3

--- common/usbc_utils.c
+++ common/usbc_utils.c
@@ -134,7 +134,7 @@ static void extract_pdo_helper(uint32_t pdo, uint32_t *ma, uint32_t *max_mv,
 		*ma = PDO_VAR_MAX_CURRENT(pdo);
 	} else {
 		mw = PDO_BATT_MAX_POWER(pdo);
-		*ma = 1000 * mw / PROCESS_ZERO_DIVISOR(*min_mv);
+		*ma = 1000 * mw / PROCESS_ZERO_DIVISOR(*max_mv);
 	}
 }
 
