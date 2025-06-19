aa19d6adde1e550914dc6d08f6b3637bf0af633e
Elmo Lan <elmo_lan@realtek.corp-partner.google.com>
2025-06-19 23:39:39 +0800

    RTS5915: Handle WP_AT_BOOT when next boot
    The firmware.ECCbi unlock ec wp by:
    1. set gsc deassert WP_L (HWWP)
    2. set ec disable flash WP (SWWP)
    3. set gsc assert EC_RST (reset EC)
    
    In step 2, it will call `flashwp disable`
    and lead to clear EC_FLASH_PROTECT_RO_AT_BOOT.
    
    Our WP only can unlock after ec reboot, in this case,
    the `flashwp disable` not able to apply immediately.
    
    We need to record this flag and apply while WP unlock in next boot.
    
    BUG=b:425305944
    TEST=pass the FAFT:firmware.ECCbi
    
    Change-Id: Icd30b9e2cb83c4279e2f7daf4541cb9be95324e3

--- zephyr/drivers/cros_system/cros_system_rtk.c
+++ zephyr/drivers/cros_system/cros_system_rtk.c
@@ -31,6 +31,7 @@ LOG_MODULE_REGISTER(cros_system, LOG_LEVEL_ERR);
 #define RTK_VIVO_BACKUP1_REG (*((volatile uint32_t *)0x40104ffc))
 
 #define BBRAM_KEY_VALUE 0xA5
+#define BBRAM_WP_FLAG_INVALID 0xFF
 
 /* Driver data */
 struct cros_system_rtk_data {
@@ -129,6 +130,7 @@ static int cros_system_rtk_init(const struct device *dev)
 	uint32_t vivo_reg1 = RTK_VIVO_BACKUP1_REG;
 	uint32_t key_val = 0;
 	uint32_t value = 0;
+	uint32_t invalid_value = 0;
 	/* In order to determine if reset from watchdog */
 	uint32_t flag = 0;
 	/* check reset cause */
@@ -167,6 +169,12 @@ static int cros_system_rtk_init(const struct device *dev)
 				    (uint8_t *)&value);
 			system_set_reset_flags(EC_RESET_FLAG_RESET_PIN);
 		}
+
+		/* set wp_at_boot as invalid */
+		invalid_value = BBRAM_WP_FLAG_INVALID;
+		bbram_write(bbram_dev, BBRAM_REGION_OFFSET(wp_at_boot),
+			    BBRAM_REGION_SIZE(wp_at_boot),
+			    (uint8_t *)&invalid_value);
 
 		/* Set key as BBRAM_KEY_VALUE  */
 		key_val = BBRAM_KEY_VALUE;
