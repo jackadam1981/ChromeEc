2a03fba70e89f2ca652e75f8095e6044712fd420
Vincent4 <vincent4.wang@lcfc.corp-partner.google.com>
2025-06-23 15:48:52 +0800

    pujjolo: Adjust ISH firmware enablement for testing
    Modify config for pujjolo ish
    BUG=b:417599885
    TEST=zmake build pujjolo/pujjolo-ish success
    LOW_COVERAGE_REASON=test code is not yet ready b:425230416
    
    Change-Id: I851839466a7f745a1aadd4ad93431cbcb0a3c64a

--- zephyr/program/trulo/pujjolo/src/sensor.c
+++ zephyr/program/trulo/pujjolo/src/sensor.c
@@ -38,50 +38,10 @@ void lid_accel_interrupt(enum gpio_signal signal)
 static void motionsense_init(void)
 {
 	int ret;
-
-	if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
-		int ish_enabled;
-		ret = cros_cbi_get_fw_config(ISH, &ish_enabled);
-
-		if (ret < 0) {
-			LOG_ERR("Failed to load ISH config: %d", ret);
-			return;
-		}
-
-		if (ish_enabled == ISH_ENABLED) {
-			motion_sensor_count = 0;
-			gpio_disable_dt_interrupt(
-				GPIO_INT_FROM_NODELABEL(int_imu));
-			gpio_disable_dt_interrupt(
-				GPIO_INT_FROM_NODELABEL(int_lid_imu));
-			gpio_pin_configure_dt(
-				GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
-				GPIO_DISCONNECTED);
-			gpio_pin_configure_dt(
-				GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
-				GPIO_DISCONNECTED);
-			LOG_INF("No motionsense");
-			return;
-		}
-	}
-
 	ret = cros_cbi_get_fw_config(FORM_FACTOR, &sensor_fwconfig);
 	if (ret < 0) {
 		LOG_ERR("error retriving CBI config: %d", ret);
 		return;
-	}
-
-	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
-		if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
-			/* Only valid in EC */
-			gmr_tablet_switch_disable();
-		}
-		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
-		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
-				      GPIO_INPUT | GPIO_PULL_UP);
-		LOG_INF("Board is Clamshell");
-	} else if (sensor_fwconfig == FORM_FACTOR_CONVERTIBLE) {
-		LOG_INF("Board is Convertible");
 	}
 }
 DECLARE_HOOK(HOOK_INIT, motionsense_init, HOOK_PRIO_DEFAULT);
