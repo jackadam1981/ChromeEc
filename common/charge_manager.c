67019a44984b2207d252ce9272c1abb46448aa88
Al Semjonovs <asemjonovs@google.com>
2025-06-24 12:21:02 -0600

    charge_manager: Use mutex for zephyr only boards
    Issue only observed with PDC Power Management threads having race
    conditions. Enable mutex protection on Zephyr only, as PDC Power
    Management is only supported on Zephyr.
    
    BUG=b:425486970, b:427504021, b:435255268
    TEST=zmake compare-build
    
    Change-Id: I74ad754944e4add722c55dd4127ecc2111966aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6669492
    Tested-by: Al Semjonovs <asemjonovs@google.com>
    Commit-Queue: Al Semjonovs <asemjonovs@google.com>
    Reviewed-by: Rob Barnes <robbarnes@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    (cherry picked from commit be8e94cf3661c04b3508f32bb66facfd93bdbda3)

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -104,7 +104,15 @@ static enum dualrole_capabilities dualrole_capability[CHARGE_PORT_COUNT];
 static int save_log[CHARGE_PORT_COUNT];
 #endif
 
+#ifdef CONFIG_ZEPHYR
 K_MUTEX_DEFINE(cm_refresh);
+#define CM_MUTEX_LOCK(m) mutex_lock(m)
+#define CM_MUTEX_UNLOCK(m) mutex_unlock(m)
+#else
+/* TODO(b/427504021) - Legacy EC mutexes are not recursive */
+#define CM_MUTEX_LOCK(m)
+#define CM_MUTEX_UNLOCK(m)
+#endif /* CONFIG_ZEPHYR */
 
 /* Store current state of port enable / charge current. */
 /* During charge_manager_refresh, the following data is considered stale. Make
@@ -355,7 +363,7 @@ static enum charge_supplier get_current_supplier(int port)
 {
 	enum charge_supplier supplier = CHARGE_SUPPLIER_NONE;
 
-	mutex_lock(&cm_refresh);
+	CM_MUTEX_LOCK(&cm_refresh);
 	/* Determine supplier information to show. */
 	if (port == charge_port) {
 		supplier = charge_supplier;
@@ -366,7 +374,7 @@ static enum charge_supplier get_current_supplier(int port)
 			/* Ignore available current */
 			supplier = find_supplier(port, supplier, -1);
 	}
-	mutex_unlock(&cm_refresh);
+	CM_MUTEX_UNLOCK(&cm_refresh);
 
 	return supplier;
 }
@@ -375,7 +383,7 @@ get_current_power_role(int port, enum charge_supplier supplier)
 {
 	enum usb_power_roles role;
 
-	mutex_lock(&cm_refresh);
+	CM_MUTEX_LOCK(&cm_refresh);
 
 	if (charge_port == port)
 		role = USB_PD_PORT_POWER_SINK;
@@ -386,7 +394,7 @@ get_current_power_role(int port, enum charge_supplier supplier)
 	else
 		role = USB_PD_PORT_POWER_DISCONNECTED;
 
-	mutex_unlock(&cm_refresh);
+	CM_MUTEX_UNLOCK(&cm_refresh);
 
 	return role;
 }
@@ -815,11 +823,27 @@ static void charge_manager_refresh(void)
 	int ceil;
 	int power_changed = 0;
 
+<<<<<<< HEAD   (7d24f000da18bc2a6c3df7e42d1894d3108e2abb charge_manager: Add mutexing around critical data used in cm)
+||||||| BASE   (1f3a8eb66c484df548fa8d1be77a78f4bd7adfa6 ocelot: Add GSC IOexpander to I2C Port)
+	mutex_lock(&cm_refresh);
+
+=======
+	CM_MUTEX_LOCK(&cm_refresh);
+
+>>>>>>> CHANGE (be8e94cf3661c04b3508f32bb66facfd93bdbda3 charge_manager: Use mutex for zephyr only boards)
 	/* Hunt for an acceptable charge port */
 	while (1) {
 		charge_manager_get_best_port(&new_port, &new_supplier);
 
+<<<<<<< HEAD   (7d24f000da18bc2a6c3df7e42d1894d3108e2abb charge_manager: Add mutexing around critical data used in cm)
 		if (!left_safe_mode && new_port == CHARGE_PORT_NONE)
+||||||| BASE   (1f3a8eb66c484df548fa8d1be77a78f4bd7adfa6 ocelot: Add GSC IOexpander to I2C Port)
+		if (!left_safe_mode && new_port == CHARGE_PORT_NONE) {
+			mutex_unlock(&cm_refresh);
+=======
+		if (!left_safe_mode && new_port == CHARGE_PORT_NONE) {
+			CM_MUTEX_UNLOCK(&cm_refresh);
+>>>>>>> CHANGE (be8e94cf3661c04b3508f32bb66facfd93bdbda3 charge_manager: Use mutex for zephyr only boards)
 			return;
 
 		/*
@@ -1053,6 +1077,14 @@ static void charge_manager_refresh(void)
 		/* notify host of power info change */
 		pd_send_host_event(PD_EVENT_POWER_CHANGE);
 	}
+<<<<<<< HEAD   (7d24f000da18bc2a6c3df7e42d1894d3108e2abb charge_manager: Add mutexing around critical data used in cm)
+||||||| BASE   (1f3a8eb66c484df548fa8d1be77a78f4bd7adfa6 ocelot: Add GSC IOexpander to I2C Port)
+
+	mutex_unlock(&cm_refresh);
+=======
+
+	CM_MUTEX_UNLOCK(&cm_refresh);
+>>>>>>> CHANGE (be8e94cf3661c04b3508f32bb66facfd93bdbda3 charge_manager: Use mutex for zephyr only boards)
 }
 DECLARE_DEFERRED(charge_manager_refresh);
 
@@ -1313,16 +1345,28 @@ void charge_manager_set_ceil(int port, enum ceil_requestor requestor, int ceil)
 	if (!is_valid_port(port))
 		return;
 
+<<<<<<< HEAD   (7d24f000da18bc2a6c3df7e42d1894d3108e2abb charge_manager: Add mutexing around critical data used in cm)
+||||||| BASE   (1f3a8eb66c484df548fa8d1be77a78f4bd7adfa6 ocelot: Add GSC IOexpander to I2C Port)
+	mutex_lock(&cm_refresh);
+=======
+	CM_MUTEX_LOCK(&cm_refresh);
+>>>>>>> CHANGE (be8e94cf3661c04b3508f32bb66facfd93bdbda3 charge_manager: Use mutex for zephyr only boards)
 	if (charge_ceil[port][requestor] != ceil) {
 		charge_ceil[port][requestor] = ceil;
 		if (port == charge_port && charge_manager_is_seeded())
 			hook_call_deferred(&charge_manager_refresh_data, 0);
 	}
+<<<<<<< HEAD   (7d24f000da18bc2a6c3df7e42d1894d3108e2abb charge_manager: Add mutexing around critical data used in cm)
+||||||| BASE   (1f3a8eb66c484df548fa8d1be77a78f4bd7adfa6 ocelot: Add GSC IOexpander to I2C Port)
+	mutex_unlock(&cm_refresh);
+=======
+	CM_MUTEX_UNLOCK(&cm_refresh);
+>>>>>>> CHANGE (be8e94cf3661c04b3508f32bb66facfd93bdbda3 charge_manager: Use mutex for zephyr only boards)
 }
 
 void charge_manager_force_ceil(int port, int ceil)
 {
-	mutex_lock(&cm_refresh);
+	CM_MUTEX_LOCK(&cm_refresh);
 	/*
 	 * Force our input current to ceil if we're exceeding it, without
 	 * waiting for our deferred task to run.
@@ -1350,7 +1394,7 @@ void charge_manager_force_ceil(int port, int ceil)
 		 */
 		charge_manager_set_ceil(port, CEIL_REQUESTOR_PD, ceil);
 	}
-	mutex_unlock(&cm_refresh);
+	CM_MUTEX_UNLOCK(&cm_refresh);
 }
 
 int charge_manager_set_override(int port)
@@ -1405,9 +1449,9 @@ int charge_manager_get_active_charge_port(void)
 {
 	int retval = 0;
 
-	mutex_lock(&cm_refresh);
+	CM_MUTEX_LOCK(&cm_refresh);
 	retval = charge_port;
-	mutex_unlock(&cm_refresh);
+	CM_MUTEX_UNLOCK(&cm_refresh);
 
 	return retval;
 }
@@ -1424,9 +1468,9 @@ int charge_manager_get_charger_current(void)
 {
 	int retval = 0;
 
-	mutex_lock(&cm_refresh);
+	CM_MUTEX_LOCK(&cm_refresh);
 	retval = charge_current;
-	mutex_unlock(&cm_refresh);
+	CM_MUTEX_UNLOCK(&cm_refresh);
 
 	return retval;
 }
@@ -1435,9 +1479,9 @@ int charge_manager_get_charger_voltage(void)
 {
 	int retval = 0;
 
-	mutex_lock(&cm_refresh);
+	CM_MUTEX_LOCK(&cm_refresh);
 	retval = charge_voltage;
-	mutex_unlock(&cm_refresh);
+	CM_MUTEX_UNLOCK(&cm_refresh);
 
 	return retval;
 }
@@ -1446,19 +1490,19 @@ enum charge_supplier charge_manager_get_supplier(void)
 {
 	int retval = 0;
 
-	mutex_lock(&cm_refresh);
+	CM_MUTEX_LOCK(&cm_refresh);
 	retval = charge_supplier;
-	mutex_unlock(&cm_refresh);
+	CM_MUTEX_UNLOCK(&cm_refresh);
 
 	return retval;
 }
 
 void charge_manager_set_supplier(int port, enum charge_supplier supplier)
 {
-	mutex_lock(&cm_refresh);
+	CM_MUTEX_LOCK(&cm_refresh);
 	if (charge_supplier != CHARGE_SUPPLIER_NONE ||
 	    charge_port != CHARGE_PORT_NONE) {
-		mutex_unlock(&cm_refresh);
+		CM_MUTEX_UNLOCK(&cm_refresh);
 		return;
 	}
 
@@ -1467,7 +1511,7 @@ void charge_manager_set_supplier(int port, enum charge_supplier supplier)
 
 	charge_port = port;
 	charge_supplier = supplier;
-	mutex_unlock(&cm_refresh);
+	CM_MUTEX_UNLOCK(&cm_refresh);
 }
 
 int charge_manager_get_power_limit_uw(void)
@@ -1475,10 +1519,10 @@ int charge_manager_get_power_limit_uw(void)
 	int current_ma = 0;
 	int voltage_mv = 0;
 
-	mutex_lock(&cm_refresh);
+	CM_MUTEX_LOCK(&cm_refresh);
 	current_ma = charge_current;
 	voltage_mv = charge_voltage;
-	mutex_unlock(&cm_refresh);
+	CM_MUTEX_UNLOCK(&cm_refresh);
 
 	if (current_ma == CHARGE_CURRENT_UNINITIALIZED ||
 	    voltage_mv == CHARGE_VOLTAGE_UNINITIALIZED)
