ba1c26b1981be9a222ad566b7ece0cf77643e12f
Derek Huang <derekhuang@google.com>
2025-06-27 10:19:57 +0000

    usb_pd: Add interrupt event driven CC state detection
    This patch adds a config which enables interrupt event driven
    CC state change detection. Instead of polling CC state from TCPC,
    the detection is relied on interrupt event from TCPC.
    
    BUG=b:427595990
    TEST=Plug/Un-plug USB-C device, make sure CC detection is working
    LOW_COVERAGE_REASON=b:153071799
    
    Change-Id: Iac36fcc0593992fc98809b076a117b0e6b225c08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6682225
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Tested-by: Derek Huang <derekhuang@google.com>
    Commit-Queue: Derek Huang <derekhuang@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7207058
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Tested-by: Abin Roy <abin.roy@7rays.corp-partner.google.com>
    Commit-Queue: Abin Roy <abin.roy@7rays.corp-partner.google.com>
    Auto-Submit: Abin Roy <abin.roy@7rays.corp-partner.google.com>

--- driver/tcpm/tcpci.c
+++ driver/tcpm/tcpci.c
@@ -1298,7 +1298,14 @@ void tcpci_tcpc_alert(int port)
 		tcpc_write16(port, TCPC_REG_ALERT, alert);
 
 	if (alert & TCPC_REG_ALERT_CC_STATUS) {
-		if (IS_ENABLED(CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE)) {
+		/*
+		 * Always report PD_EVENT_CC event if
+		 * CONFIG_USB_PD_EVENT_DRIVEN_CC_STATE is enabled
+		 * so that the CC state change in unattached state
+		 * can be detected.
+		 */
+		if (IS_ENABLED(CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE) &&
+		    !IS_ENABLED(CONFIG_USB_PD_EVENT_DRIVEN_CC_STATE)) {
 			enum tcpc_cc_voltage_status cc1;
 			enum tcpc_cc_voltage_status cc2;
 
