6d99ceda4de8bab6cb60187d03548a7ee0ff8ec0
Dawid Niedzwiecki <dawidn@google.com>
2025-07-01 12:59:15 +0200

    WIP: zephyr: fpmcu: add niedzica
    Change-Id: I79ccbada4399037703696b796de2def3e06c28a4

--- zephyr/program/fpmcu/BUILD.py
+++ zephyr/program/fpmcu/BUILD.py
@@ -129,3 +129,24 @@ gwendolin = register_fpmcu_variant(
 # The address of RW_FWID is hardcoded in RO. You need to have REALLY
 # good reason to change it.
 assert_rw_fwid_DO_NOT_EDIT(project_name="gwendolin", addr=0x40144)
+
+niedzica = register_fpmcu_variant(
+    project_name="niedzica",
+    zephyr_board="32f967_dv",
+    register_func=register_binman_project,
+    variant_modules=["cmsis_6", "elan_zephyr"],
+    variant_optional_modules=["elan"],
+    variant_dts_overlays=[
+        here / "niedzica" / "niedzica.dts",
+    ],
+    variant_kconfig_files=[
+        here / "niedzica" / "prj.conf",
+    ],
+    signer=signers.RwsigSigner(  # pylint: disable=undefined-variable
+        here / "niedzica" / "dev_key.pem",
+    ),
+)
+
+# The address of RW_FWID is hardcoded in RO. You need to have REALLY
+# good reason to change it.
+assert_rw_fwid_DO_NOT_EDIT(project_name="niedzica", addr=0x24144)
