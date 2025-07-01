adc976af4f03a72d413532fa492c777db70f2cc3
Keith Short <keithshort@chromium.org>
2025-07-01 15:09:35 -0600

    brox: use common hiberate Z5 driver
    Delete board specific code used to enter hiberbate/Z5 and use the common
    driver instead.
    
    BUG=b:372054859
    TEST=Enter hibernate on brox, verify wake from power button
    
    Change-Id: Iba1a020d5d94fae38a711d8c4145aca716aa168e

--- zephyr/test/brox/tests/baseboard/src/hibernate.c
+++ zephyr/test/brox/tests/baseboard/src/hibernate.c
@@ -14,17 +14,6 @@ FAKE_VALUE_FUNC(enum ec_error_list, isl9238c_hibernate, int);
 
 ZTEST_SUITE(hibernate, NULL, NULL, NULL, NULL, NULL);
 
-ZTEST(hibernate, test_board_get_pd_port_location)
-{
-	const struct gpio_dt_spec *gpio_en_slp_z =
-		GPIO_DT_FROM_NODELABEL(gpio_en_slp_z);
-
-	gpio_pin_set_dt(gpio_en_slp_z, 0);
-	board_hibernate_late();
-	zassert_true(
-		gpio_emul_output_get(gpio_en_slp_z->port, gpio_en_slp_z->pin));
-}
-
 ZTEST(hibernate, test_board_hibernate)
 {
 	RESET_FAKE(isl9238c_hibernate);
