b0749f4d3f48517eeb2e3cf8aebe75a766257df4
zengyuxiang <zengyuxiang@huaqin.corp-partner.google.com>
2025-07-01 15:22:05 +0800

    telticn50: Initial zephyr config for telticn50
    Reference telith project initialization telticn50 project configuration.
    
    BUG=428842924
    TEST=1.zmake build telticn50.
    
    Change-Id: I161d9979801ec172e70213eeacc0bafd7782c241

--- zephyr/program/nissa/BUILD.py
+++ zephyr/program/nissa/BUILD.py
@@ -233,6 +233,11 @@ epic = register_nissa_project(
     chip="it8xxx2/it81302bx",
 )
 
+telticn50 = register_nissa_project(
+    project_name="telticn50",
+    chip="it8xxx2/it81302bx",
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="anraggar", addr=0xBFFE0)
@@ -269,3 +274,4 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="pujjoniru", addr=0xBFFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="guren", addr=0x7FFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="meliks", addr=0x7FFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="epic", addr=0xBFFE0)
+assert_rw_fwid_DO_NOT_EDIT(project_name="telticn50", addr=0xBFFE0)
