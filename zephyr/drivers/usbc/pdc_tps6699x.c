c0b8fb0afa345f60b3af0a74f5cf48d84a9429df
Tristan Honscheid <honscheid@google.com>
2025-07-01 16:47:46 -0600

    pdc: ti: NULL-protect pdc_data[] accesses.
    When a port is not initialized, its entry in `pdc_data[]` remains NULL.
    Add NULL checks to the interrupt handler code to ignore NULL ports that
    are not in use.
    
    BUG=b:428574500
    TEST=./twister -T zephyr/test/pdc
    
    Change-Id: I1cccd08c05722080f4a369d2b3872c120d7809c8

--- zephyr/drivers/usbc/pdc_tps6699x.c
+++ zephyr/drivers/usbc/pdc_tps6699x.c
@@ -583,9 +583,12 @@ static int pdc_autonegotiate_sink_reset(struct pdc_data_t *data)
 static void set_all_ports_to_init(const int delay_ms)
 {
 	for (int port = 0; port < NUM_PDC_TPS6699X_PORTS; port++) {
-		if (!device_is_ready(pdc_data[port]->dev)) {
+		if (pdc_data[port] == NULL ||
+		    !device_is_ready(pdc_data[port]->dev)) {
+			/* Port is not in use. Skip it. */
 			continue;
 		}
+
 		pdc_data[port]->init_done = false;
 		pdc_data[port]->init_attempt = 0;
 		if (delay_ms) {
@@ -2944,12 +2947,15 @@ static void tps_check_and_notify_irq(void)
 {
 	for (int port = 0; port < NUM_PDC_TPS6699X_PORTS; port++) {
 		struct pdc_data_t *data = pdc_data[port];
-		struct pdc_config_t const *cfg = data->dev->config;
+		struct pdc_config_t const *cfg;
 		union reg_interrupt pdc_interrupt = { 0 };
 
-		if (!device_is_ready(data->dev)) {
-			continue;
+		if (data == NULL || !device_is_ready(data->dev)) {
+			/* Port is not in use. Skip it. */
+			continue; /* LCOV_EXCL_LINE - b/406176587 */
 		}
+
+		cfg = data->dev->config;
 
 		if (!gpio_pin_get_dt(&cfg->irq_gpios)) {
 			break;
