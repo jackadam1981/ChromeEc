eda4a5f2b76b62ff70ff06fbf08317caef8610ab
Dawid Niedzwiecki <dawidn@google.com>
2025-07-01 12:59:15 +0200

    WIP: add em32f967
    Change-Id: I79ccbada4399037703696b796de2def3e06c28a4

---
+++ zephyr/program/fpmcu/em32f967/src/tmp.c
@@ -0,0 +1,29 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include "hooks.h"
+
+extern void arm_core_mpu_disable(void);
+
+void trng_rand_bytes(void *buffer, size_t len)
+{
+	for (int i = 0; i < len; i++) {
+		((uint8_t*)buffer)[i] = (uint8_t)sys_clock_cycle_get_32();
+	}
+}
+
+int mkbp_set_host_active_via_custom(int active, uint32_t *timestamp){
+	return 0;
+}
+
+static void prepare_for_sysjump_to_ec(void)
+{
+#ifdef CONFIG_MPU
+	arm_core_mpu_disable();
+#endif
+}
+DECLARE_HOOK(HOOK_SYSJUMP, prepare_for_sysjump_to_ec, HOOK_PRIO_LAST);
