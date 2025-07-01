47935f6cd5421ff9c33f7edf06eff437269ab76a
Keith Short <keithshort@chromium.org>
2025-07-01 11:42:47 -0600

    test: Add error path tests for hibernate_z5 driver
    Add tests to verify the hibernate_z5 driver fails to initialize if the
    GPIO configuration is not correct.
    
    BUG=b:372054859
    TEST=./twister -ivd -T zephyr/test/hibernate_z5
    
    Change-Id: Ia995a4249784db1dc4ab89fad153c3e148b9a4ab

--- zephyr/test/hibernate_z5/src/main.c
+++ zephyr/test/hibernate_z5/src/main.c
@@ -4,6 +4,7 @@
  */
 
 #include "system.h"
+#include "zephyr/device.h"
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/gpio/gpio_emul.h>
@@ -16,8 +17,25 @@ ZTEST(hibernate_z5, test_hibernate_z5_assert)
 	const struct gpio_dt_spec gpio_en_slp_z =
 		GPIO_DT_SPEC_GET(DT_NODELABEL(hibernate_z5), en_slp_z_gpios);
 
+	if (IS_ENABLED(CONFIG_HIBERNATE_Z1_INIT_SHOULD_FAIL)) {
+		/* Nothing to do if the driver failed to initialize. */
+		ztest_test_skip();
+	}
+
 	gpio_pin_set_dt(&gpio_en_slp_z, 0);
 	board_hibernate_late();
 	zassert_true(
 		gpio_emul_output_get(gpio_en_slp_z.port, gpio_en_slp_z.pin));
+}
+
+ZTEST(hibernate_z5, test_hibernate_z5_init)
+{
+	const struct device *hibernate_dev =
+		DEVICE_DT_GET(DT_NODELABEL(hibernate_z5));
+
+	if (IS_ENABLED(CONFIG_HIBERNATE_Z1_INIT_SHOULD_FAIL)) {
+		zassert_false(device_is_ready(hibernate_dev));
+	} else {
+		zassert_true(device_is_ready(hibernate_dev));
+	}
 }
