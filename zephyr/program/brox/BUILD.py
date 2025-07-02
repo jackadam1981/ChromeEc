80f70057dd71459612a1a594cbca7ca02cae6728
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2025-07-02 20:30:44 +0800

    caboc: Initial Zephyr EC image
    Create initial Zephyr EC image for caboc variant based on brox.
    Subsequent CLs will be added to reflect caboc schematic
    differences.
    
    BUG=b:429126277
    TEST=zmake build caboc
    
    Change-Id: Ieffd5e0d7dc7a153ede529dec91fa195ee3e558d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6695176
    Code-Coverage: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Tested-by: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Code-Coverage: Newbie Chromium <newbiechromium@gmail.com>
    Commit-Queue: Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6774484
    Reviewed-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Commit-Queue: Karthikeyan Ramasubramanian <kramasub@google.com>
    (cherry picked from commit d10c8becca8ebb9f53cccbc41d07099d215373a9)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6893288
    Reviewed-by: Oscar Liu <oscarliu@google.com>
    Commit-Queue: Oscar Liu <oscarliu@google.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>

--- zephyr/program/brox/BUILD.py
+++ zephyr/program/brox/BUILD.py
@@ -95,6 +95,18 @@ register_ish_project(
     ],
 )
 
+caboc = register_brox_project(
+    project_name="caboc",
+    kconfig_files=[
+        # Common to all projects.
+        here / "program.conf",
+        # Parent project's config
+        here / "caboc" / "project.conf",
+        # Common sensor configs
+        here / "motionsense.conf",
+    ],
+)
+
 greenbayupoc = register_brox_project(
     project_name="greenbayupoc",
     kconfig_files=[
@@ -135,6 +147,7 @@ lotso = register_brox_project(
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox-ish-ec", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="brox-tokenized", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="caboc", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="greenbayupoc", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="jubilant", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="lotso", addr=0x60098)
