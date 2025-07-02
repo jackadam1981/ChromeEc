c9b5cdd845614edf2fda3f2f61c9bb90016ed8e5
Abin Roy <abin.roy@7rays.corp-partner.google.com>
2025-07-02 12:00:53 +0530

    anraggar frs.snk3.4
    bug=b:NONE
    TEST=NONE
    
    Change-Id: Ia3df0bcbc467bfb782e4f11978e0459d7dd641a9

--- zephyr/program/nissa/anraggar/src/usbc.c
+++ zephyr/program/nissa/anraggar/src/usbc.c
@@ -16,6 +16,8 @@
 #include "system.h"
 #include "usb_mux.h"
 #include "usbc_ppc.h"
+#include "usb_pd.h"
+#include <stdint.h>
 
 #include <zephyr/logging/log.h>
 
@@ -23,6 +25,8 @@
 #define CPRINTFUSB(format, args...) cprintf(CC_USBCHARGE, format, ##args)
 
 LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
+
+static bool sourcing_vbus[CONFIG_USB_PD_PORT_MAX_COUNT];
 
 /* Vconn control for integrated ITE TCPC */
 void board_pd_vconn_ctrl(int port, enum usbpd_cc_pin cc_pin, int enabled)
@@ -93,6 +97,7 @@ int board_set_active_charge_port(int port)
 		CPRINTSUSB("C%d: sink path enable failed.", port);
 		return EC_ERROR_UNKNOWN;
 	}
+	sourcing_vbus[port] = false;
 
 	return EC_SUCCESS;
 }
@@ -106,8 +111,12 @@ DECLARE_DEFERRED(notify_power_change);
 
 void pd_power_supply_reset(int port)
 {
+	if (!sourcing_vbus[port])
+		return;
+
 	/* Disable VBUS. */
 	ppc_vbus_source_enable(port, 0);
+	sourcing_vbus[port] = false;
 
 	/* Enable discharge if we were previously sourcing 5V */
 	if (IS_ENABLED(CONFIG_USB_PD_DISCHARGE))
@@ -141,6 +150,7 @@ int pd_set_power_supply_ready(int port)
 		LOG_WRN("C%d failed to enable VBUS sourcing: %d", port, rv);
 		return rv;
 	}
+	sourcing_vbus[port] = true;
 
 	/* Defer pd_send_host_event to save ~2ms for PD compliance */
 	hook_call_deferred(&notify_power_change_data, 0);
