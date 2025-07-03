21e830580a9731f1059e350401fcf0a0a10e969f
Elmo Lan <elmo_lan@realtek.corp-partner.google.com>
2025-07-03 15:04:47 +0800

    frtk: Initial Zephyr EC image
    Create the initial Zephyr EC image for the frtk variant based
    on the fatcat reference board.
    
    BUG=none
    TEST=zmake build frtk success
    
    Change-Id: Ie091bbba7ed62dd5eb28248b39385632ce128b16

--- zephyr/program/fatcat/BUILD.py
+++ zephyr/program/fatcat/BUILD.py
@@ -118,6 +118,10 @@ register_npcx9_project(
     extra_modules=["google-private"],
 )
 
+register_realtek_project(
+    project_name="frtk",
+)
+
 register_it8xxx2_project(
     project_name="felino",
 )
@@ -212,6 +216,7 @@ register_ish_project(
 assert_rw_fwid_DO_NOT_EDIT(project_name="fatcatrvp-npcx", addr=0x80144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="fatcatrvp-ite", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="francka", addr=0x80144)
+assert_rw_fwid_DO_NOT_EDIT(project_name="frtk", addr=0x80404)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ruby", addr=0x40144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="felino", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="felino4es", addr=0x60098)
