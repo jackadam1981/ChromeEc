c666f134ad3685521cdcb7e371c0ba384924d21d
Elmo Lan <elmo_lan@realtek.corp-partner.google.com>
2025-07-03 16:33:21 +0800

    flapis: Initial Zephyr EC image
    Create the initial Zephyr EC image for the flapis variant based
    on the fatcat reference board.
    
    BUG=none
    TEST=zmake build flapis success
    
    Change-Id: I92b89c6d1a44df29c6dfd1caffb5bbcd69e11b60

--- zephyr/program/fatcat/BUILD.py
+++ zephyr/program/fatcat/BUILD.py
@@ -51,6 +51,28 @@ def register_it8xxx2_project(
     )
 
 
+def register_realtek_project(
+    project_name,
+    extra_kconfig_files=(),
+):
+    """Register an realtek_ec based variant of fatcat."""
+    register_rtk_project(
+        project_name=project_name,
+        zephyr_board="realtek/rts5912",
+        dts_overlays=[
+            here / project_name / "project.overlay",
+        ],
+        kconfig_files=[
+            # Common to all projects.
+            here / "program.conf",
+            # Project-specific KConfig customization.
+            here / project_name / "project.conf",
+            # Additional project-specific KConfig customization.
+            *extra_kconfig_files,
+        ],
+    )
+
+
 register_npcx9_project(
     project_name="fatcat_npcx9m7f",
     extra_kconfig_files=[
@@ -93,6 +115,11 @@ register_it8xxx2_project(
     extra_modules=["pigweed", "nanopb"],
 )
 
+register_realtek_project(
+    project_name="flapis",
+    extra_kconfig_files=[],
+)
+
 register_ish_project(
     project_name="kinmen-ish",
     zephyr_board="intel_ish_5_8_0",
@@ -114,3 +141,4 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="francka", addr=0x80144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="felino", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="felino4es", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="kinmen", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="flapis", addr=0x80404)
