7d0e4af6ba729d8a59e7ed9eb5594fa8c8405fde
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2025-07-03 14:42:26 +0800

    (TEST) get RO/RW timing on zephyr codebase
    test on it81202bx_evb
    
    1.get RO/RW interrupt timing by GPA1:
    GPA1 connect to GPA2,
    set GPA1 output low to trigger GPA2 interrupt (falling edge),
    then set GPA1 output high in interrupt.
    
    Change-Id: Ifcffeb0b0f1f796d95d719595a62774a62b5d0d8

--- zephyr/app/ec/main_shim.c
+++ zephyr/app/ec/main_shim.c
@@ -8,10 +8,26 @@
 
 #include <zephyr/kernel.h>
 
+#define ECREG(x)                    (*((volatile unsigned char *)(x)))
+#define IT8XXX2_GPIO_BASE           0x00F01600
+#define IT8XXX2_GPIO_DATA           ECREG(IT8XXX2_GPIO_BASE + 0x01)
+
+void test_gpa2_interrupt(enum gpio_signal signal) {
+	/* set GPA1 output high */
+	IT8XXX2_GPIO_DATA |= BIT(1);
+	printk("GPA2 INT");
+}
+
 /** A stub main to call the real ec app main function. LCOV_EXCL_START */
 int main(void)
 {
 	ec_app_main();
+
+	/*
+	 * GPA1 init output high
+	 * Here, GPA1 output low to trigger GPA2_INT (falling edge)
+	 */
+	IT8XXX2_GPIO_DATA &= ~BIT(1);
 
 	if (IS_ENABLED(CONFIG_TASK_HOSTCMD_THREAD_MAIN)) {
 		host_command_main();
