b973b290e2faa85c330c3bf38703cbaa3eed1c61
lin.arthur <lin.arthur@inventec.corp-partner.google.com>
2025-07-06 20:12:32 +0800

    charger/isl95522: import isl95522 config
    Add isl95522 basic function and driver.
    
    BUG=b:
    TEST=zmake build kaladin
    
    Change-Id: I18b0a7bb90b57cee6706d18d6576b67fd4c017ae

--- driver/build.mk
+++ driver/build.mk
@@ -76,6 +76,7 @@ driver-$(CONFIG_CHARGER_ISL9237)+=charger/isl923x.o
 driver-$(CONFIG_CHARGER_ISL9238)+=charger/isl923x.o
 driver-$(CONFIG_CHARGER_ISL9238C)+=charger/isl923x.o
 driver-$(CONFIG_CHARGER_ISL9241)+=charger/isl9241.o
+driver-$(CONFIG_CHARGER_ISL95522)+=charger/isl95522.o
 driver-$(CONFIG_CHARGER_MT6370)+=charger/rt946x.o
 driver-$(CONFIG_CHARGER_RAA489000)+=charger/isl923x.o
 driver-$(CONFIG_CHARGER_RAA489110)+=charger/isl9241.o
