c8243ad534aa02b0e1848264c63211d12550e32c
Keith Short <keithshort@chromium.org>
2025-07-07 09:31:57 -0600

    trulo: drop unnecessary guard for hibernate
    The trulo and uldrenite platforms do not enable PSL based hibernate so
    there is no neeed to include and extra guard.
    
    The board specific files are deleted in the next CL.  This CL
    demonstrates that the board specific hiberate was only used on ITE
    boards.
    
    BUG=none
    TEST=zmake compare-builds %trulo
    
    Change-Id: Ia82ef77247f4293b5ad094d01a3d9ecf1600460e

--- zephyr/program/trulo/trulo/src/hibernate.c
+++ zephyr/program/trulo/trulo/src/hibernate.c
@@ -12,11 +12,9 @@ LOG_MODULE_DECLARE(trulo, LOG_LEVEL_INF);
 /* Trigger hibernate by enabling the Z-sleep circuit */
 __override void board_hibernate_late(void)
 {
-#ifndef CONFIG_PLATFORM_EC_HIBERNATE_PSL
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_slp_z), 1);
 	/*
 	 * The system should hibernate, but there may be
 	 * a small delay, so return.
 	 */
-#endif
 }
