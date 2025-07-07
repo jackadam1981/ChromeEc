e53a24d7781ba578e6d5af2e54751102ea3b4f33
Yunshao Chiang <Yunshao.Chiang@ite.com.tw>
2025-07-07 10:45:26 +0800

    (TEST) Triger GPIO in bitbang.c
    Change-Id: Ie06701ffce261ee2e2154503afb59fdbad37f5bf

--- driver/cec/bitbang.c
+++ driver/cec/bitbang.c
@@ -23,6 +23,18 @@
 #define DEBUG_CPRINTS(...)
 #endif
 
+
+#define ECREG(x)         (*((volatile unsigned char *)(x)))
+#define EC_REG_BASE_ADDR 0x00f00000
+
+#define IT81XX2_GPIO_H4_GPCR     ECREG(EC_REG_BASE_ADDR + 0x164c)       // GPIO_H4 Control Register
+#define IT81XX2_GPIO_H4_OUTPUT   ((IT81XX2_GPIO_H4_GPCR & 0x3F) | 0x40) // GPIO_H4 設定成 Output
+
+#define IT81XX2_GPIO_GPDRA       ECREG(EC_REG_BASE_ADDR + 0x1608)      // GPIO_H Data Register
+#define IT81XX2_GPIO_HIGH_H4     (IT81XX2_GPIO_GPDRA | (uint8_t)0x10)  // GPIO_H4 拉 High
+#define IT81XX2_GPIO_LOW_H4      (IT81XX2_GPIO_GPDRA & (uint8_t)~0x10) // GPIO_H4 拉 Low
+#define IT81XX2_GPIO_REVERSE_H4  (IT81XX2_GPIO_GPDRA ^ (uint8_t)0x10)  // GPIO_H4 反向
+
 /*
  * Free time timing (us). Our free-time is calculated from the end of
  * the last bit (not from the start). We compensate by having one
@@ -449,6 +461,12 @@ static void enter_state(int port, enum cec_state new_state)
 
 void cec_event_timeout(int port)
 {
+	// GPIO_H4 反向
+	IT81XX2_GPIO_GPDRA = IT81XX2_GPIO_REVERSE_H4;
+
+	// GPIO_H4 反向
+	IT81XX2_GPIO_GPDRA = IT81XX2_GPIO_REVERSE_H4;
+
 	struct cec_port_data *port_data = &cec_port_data[port];
 
 	switch (port_data->state) {
@@ -718,6 +736,12 @@ __overridable void cec_update_interrupt_time(int port)
 
 static int bitbang_cec_init(int port)
 {
+	// 設定 GPIO_H4 為 Output
+	IT81XX2_GPIO_H4_GPCR = IT81XX2_GPIO_H4_OUTPUT;
+
+	// GPIO_H4 拉 HIGH
+	IT81XX2_GPIO_GPDRA = IT81XX2_GPIO_HIGH_H4;
+
 	const struct bitbang_cec_config *drv_config =
 		cec_config[port].drv_config;
 
