1f4f91002138e7e7436e184ce883dbb29423ae7b
zengyuxiang <zengyuxiang@huaqin.corp-partner.google.com>
2025-07-11 16:17:05 +0800

    [TEST ONLY]pujjoniru: Add RT9478 for pujjoniru
    Add alt charger RT9478 for pujjoniru
    
    BUG=None
    TEST=zmake build pujjoniru [TODO]
    
    Change-Id: I806f2db83ddf30445bb6be0abe45a333fee72e7e

--- zephyr/program/nissa/pujjoniru/src/charger.c
+++ zephyr/program/nissa/pujjoniru/src/charger.c
@@ -40,35 +40,3 @@ __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 	charge_set_input_current_limit(charge_ma, charge_mv);
 	charger_set_input_current_limit(0, charge_ma);
 }
-
-static void delay_bq25710(void)
-{
-	int reg;
-
-	i2c_read16(chg_chips[0].i2c_port, chg_chips[0].i2c_addr_flags,
-		   BQ25710_REG_CHARGE_OPTION_2, &reg);
-	reg = reg & (~BIT(7));
-	i2c_write16(chg_chips[0].i2c_port, chg_chips[0].i2c_addr_flags,
-		    BQ25710_REG_CHARGE_OPTION_2, reg);
-}
-DECLARE_DEFERRED(delay_bq25710);
-
-#define BQ25710_MIN_INPUT_VOLTAGE_MV 0x240
-static void bq25710_min_input_voltage(void)
-{
-	if (extpower_is_present()) {
-		i2c_write16(chg_chips[0].i2c_port, chg_chips[0].i2c_addr_flags,
-			    BQ25710_REG_INPUT_VOLTAGE,
-			    BQ25710_MIN_INPUT_VOLTAGE_MV);
-		hook_call_deferred(&delay_bq25710_data, 2 * USEC_PER_SEC);
-	} else {
-		int reg;
-
-		i2c_read16(chg_chips[0].i2c_port, chg_chips[0].i2c_addr_flags,
-			   BQ25710_REG_CHARGE_OPTION_2, &reg);
-		reg = reg | BIT(7);
-		i2c_write16(chg_chips[0].i2c_port, chg_chips[0].i2c_addr_flags,
-			    BQ25710_REG_CHARGE_OPTION_2, reg);
-	}
-}
-DECLARE_HOOK(HOOK_AC_CHANGE, bq25710_min_input_voltage, HOOK_PRIO_DEFAULT);
