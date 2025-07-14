392d6282d2b96b2469effe8d86adf7048e0073d9
Cheng Yueh <cyueh@chromium.org>
2025-07-14 16:41:00 +0800

    ap_power: Add time stamp for firmware.bootTime
    Expose ec console command boottime for measuring boot time in Tast.
    
    BUG=b:426037304
    TEST=(1) ./twister -v -i -p native_sim -p unit_testing \
               -s ap_power_drv.meteorlake
    TEST=(2) Checkout CL:6687071
    TEST=(3) tast run ${IP} firmware.BootTime.default
    
    Change-Id: I8ed4903d2c5105a3094a20be35297b52afa99b06

---
+++ zephyr/test/ap_power/src/boot_time.c
@@ -0,0 +1,94 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "hooks.h"
+#include "host_command.h"
+#include "test_state.h"
+#include "test_utils.h"
+#include "timer.h"
+
+#include <zephyr/shell/shell_dummy.h>
+#include <zephyr/ztest.h>
+
+#include <ap_power/ap_pwrseq.h>
+
+static void time_mock_after(void *state)
+{
+	get_time_mock = NULL;
+}
+
+/**
+ * Test boot time is set
+ */
+ZTEST(boot_time_no_tasks, test_boot_time_set)
+{
+	uint64_t time_S5 = 0;
+	uint64_t time_S0 = 0;
+	timestamp_t fake_time;
+	get_time_mock = &fake_time;
+
+	fake_time.val = 0;
+
+	on_new_state(AP_POWER_STATE_G3);
+
+	zassert_ok(get_boot_time(AP_POWER_STATE_S5, &time_S5));
+	zassert_ok(get_boot_time(AP_POWER_STATE_S0, &time_S0));
+	zassert_equal(time_S5, (uint64_t)-1, "time_S5=%llu", time_S5);
+	zassert_equal(time_S0, (uint64_t)-1, "time_S0=%llu", time_S0);
+
+	fake_time.val = USEC_PER_SEC;
+	on_new_state(AP_POWER_STATE_S5);
+
+	zassert_ok(get_boot_time(AP_POWER_STATE_S5, &time_S5));
+	zassert_ok(get_boot_time(AP_POWER_STATE_S0, &time_S0));
+	zassert_equal(time_S5, USEC_PER_SEC, "time_S5=%llu", time_S5);
+	zassert_equal(time_S0, (uint64_t)-1, "time_S0=%llu", time_S0);
+
+	fake_time.val = 2 * USEC_PER_SEC;
+	on_new_state(AP_POWER_STATE_S0);
+
+	zassert_ok(get_boot_time(AP_POWER_STATE_S5, &time_S5));
+	zassert_ok(get_boot_time(AP_POWER_STATE_S0, &time_S0));
+	zassert_equal(time_S5, USEC_PER_SEC, "time_S5=%llu", time_S5);
+	zassert_equal(time_S0, 2 * USEC_PER_SEC, "time_S0=%llu", time_S0);
+
+	/* Test second time does not overwrite the first time. */
+	fake_time.val = 3 * USEC_PER_SEC;
+	on_new_state(AP_POWER_STATE_S5);
+
+	zassert_ok(get_boot_time(AP_POWER_STATE_S5, &time_S5));
+	zassert_ok(get_boot_time(AP_POWER_STATE_S0, &time_S0));
+	zassert_equal(time_S5, USEC_PER_SEC, "time_S5=%llu", time_S5);
+	zassert_equal(time_S0, 2 * USEC_PER_SEC, "time_S0=%llu", time_S0);
+}
+
+/**
+ * Test boottime ec console command
+ */
+ZTEST_USER(boot_time, test_boot_time_console_cmd)
+{
+	int64_t time_S5;
+	int64_t time_S0;
+	CHECK_CONSOLE_CMD("boottime", NULL, EC_ERROR_PARAM_COUNT);
+	CHECK_CONSOLE_CMD("boottime 123", NULL, EC_ERROR_PARAM1);
+
+	/*
+	Example output:
+
+	[00:00:24.764,900] <inf> ap_pwrseq: first S5: 1000ms
+	*/
+	SCAN_CONSOLE_CMD("boottime S5", EC_SUCCESS, 1,
+			 "%*[^f]f%*[^i]irst S5: %lldms", &time_S5);
+	SCAN_CONSOLE_CMD("boottime S0", EC_SUCCESS, 1,
+			 "%*[^f]f%*[^i]irst S0: %lldms", &time_S0);
+	zassert_not_equal(time_S5, -1);
+	zassert_not_equal(time_S0, -1);
+	zassert_true(time_S5 <= time_S0);
+}
+
+ZTEST_SUITE(boot_time_no_tasks, ap_power_predicate_pre_main, NULL,
+	    time_mock_after, NULL, NULL);
+ZTEST_SUITE(boot_time, ap_power_predicate_post_main, NULL, NULL, NULL, NULL);
