8600689ab2b2977015155379fd3d37b21bc8ec1b
YeonGwon Sung <y.sung@samsung.corp-partner.google.com>
2025-07-15 17:26:40 +0900

    Meliks: Initial zephyr test code
    Initial zephyr test code for meliks.
    
    BUG=b:400534826
    TEST=./twister -T zephyr/test/nissa
    
    Change-Id: I1cf634ac18acc6bbbb44716d66fbf7e2b13f5e94

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -1477,7 +1477,7 @@ int charge_manager_get_override(void)
 	return override_port;
 }
 
-int charge_manager_get_active_charge_port(void)
+test_mockable int charge_manager_get_active_charge_port(void)
 {
 	int retval = 0;
 
