9187f64b506b7aacce197f9a7bec608132ea4f5c
Caveh Jalali <caveh@chromium.org>
2025-07-14 19:18:32 -0700

    ish: fatcat: Add idle ISH build
    This creates a minimal ISH firmware; which supports host commands, power
    management.
    
    BUG=b:431873385
    TEST=build and flash to fatcat
    ISH responds to host commands and system can enter s0ix.
    
    ectool --name=cros_ish version
    RO version:    fatcat-ish-idle-5_8_0-0.0.0-f48
    ...
    
    Change-Id: Iaa2ac13b34348e66e50304f03165b6fa807d7606

--- zephyr/program/fatcat/BUILD.py
+++ zephyr/program/fatcat/BUILD.py
@@ -176,6 +176,18 @@ register_ish_project(
     inherited_from=["fatcat"],
 )
 
+register_ish_project(
+    project_name="fatcat-ish-idle",
+    zephyr_board="intel_ish_5_8_0",
+    dts_overlays=[
+        here / "fatcat-ish-idle" / "project.overlay",
+    ],
+    kconfig_files=[
+        here / "fatcat-ish-idle" / "project.conf",
+    ],
+    modules=["ec", "cmsis", "cmsis_6", "hal_intel_public", "pigweed", "nanopb"],
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="fatcatrvp-npcx", addr=0x80144)
