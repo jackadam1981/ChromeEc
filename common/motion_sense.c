ef55f3d95a0ce6a4ae623ea637fef5f5b3fe9113
Yuval Peress <peress@google.com>
2025-07-14 22:18:22 -0600

    pm: Disable all PM state in high throughput cases
    When the motionsense task is running a "high" throughput configuration,
    we should avoid letting the CPU suspend. This is normally avoided by
    having 1 sensor in a polling mode since the wait timeout will be set to
    a value matching the expected next sample, but when all sensors are
    using interrupts, this assumption breaks.
    
    Add a new function to system.h that is only valid in Zephyr based builds
    (by not implementing it under common/ we guarantee that the build will
    break if the function was used in legacy ECOS). When called, this
    function will loop through all the power states and lock them.
    Similarly, a "put" variant will loop through all the power states and
    unlock them.
    
    BUG=b:427883250
    TEST=5x tast run $DUT_IP hardware.SensorIioserviceHard
    TEST=Sensor CTS 100% pass rate
    
    Change-Id: I7fa71c7ba09858cc6eec8e6f15833fb7693dc6b5

--- common/motion_sense.c
+++ common/motion_sense.c
@@ -69,6 +69,19 @@ test_export_static enum chipset_state_mask sensor_active;
  */
 test_export_static int wait_us;
 
+/**
+ * Check if:
+ * 1. We enabled PM control on high throughput sensors
+ * 2. We're about to wait indefinitely
+ * 3. The fastest collection rate is faster than the threshold
+ */
+#define DISABLE_PM_POLICY_WHILE_WAITING(fastest_collection_rate)               \
+	(IS_ENABLED(                                                           \
+		 CONFIG_PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM) && \
+	 wait_us == -1 &&                                                      \
+	 fastest_collection_rate <=                                            \
+		 CONFIG_PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM_THRESHOLD)
+
 STATIC_IF(CONFIG_ACCEL_SPOOF_MODE) void print_spoof_mode_status(int id);
 STATIC_IF(CONFIG_GESTURE_DETECTION)
 void check_and_queue_gestures(uint32_t *event);
@@ -1005,12 +1018,19 @@ void motion_sense_task(void *u)
 
 		ts_end_task = get_time();
 		wait_us = -1;
+		uint32_t fastest_collection_rate = UINT32_MAX;
 
 		for (i = 0; i < motion_sensor_count; i++) {
 			struct motion_sensor_t *sensor = &motion_sensors[i];
 			enum sensor_config cfg_index =
 				motion_sense_get_ec_config();
 			int ec_rate = 0;
+
+			if (sensor->collection_rate > 0 &&
+			    sensor->collection_rate < fastest_collection_rate) {
+				fastest_collection_rate =
+					sensor->collection_rate;
+			}
 
 			if (!motion_sensor_in_forced_mode(sensor) ||
 			    sensor->collection_rate == 0)
@@ -1043,7 +1063,13 @@ void motion_sense_task(void *u)
 			wait_us = motion_min_interval;
 		}
 
+		if (DISABLE_PM_POLICY_WHILE_WAITING(fastest_collection_rate)) {
+			pm_policy_state_lock_get_all();
+		}
 		event = task_wait_event(wait_us);
+		if (DISABLE_PM_POLICY_WHILE_WAITING(fastest_collection_rate)) {
+			pm_policy_state_lock_put_all();
+		}
 	}
 }
 
