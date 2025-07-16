a244586502d314f65acb46e1f0999c8b549d911d
Cheng Yueh <cyueh@chromium.org>
2025-07-16 16:33:15 +0800

    ap_power: Add time stamp for firmware.bootTime for AP_PWRSEQ_DRIVER=n
    Expose ec console command boottime for measuring boot time in Tast.
    
    BUG=b:426037304
    TEST=(1) ./twister -v -i -p native_sim --coverage \
               -s ap_power_drv.meteorlake
    TEST=(2) ./twister -v -i -p native_sim --coverage \
               -s ap_power.meteorlake
    TEST=(3) Checkout CL:6687071
    TEST=(4) tast run ${IP} firmware.BootTime.x86
    
    Change-Id: I6fd579a74fd1ba9fef7bf5ef95ab899d01fc171f

--- zephyr/test/ap_power/src/ap_pwrseq.c
+++ zephyr/test/ap_power/src/ap_pwrseq.c
@@ -182,7 +182,6 @@ static void verify_ap_inputs(bool in_s0)
 	}
 }
 
-#ifdef CONFIG_AP_PWRSEQ_DRIVER
 /**
  * Test boottime ec console command pre main
  */
@@ -231,7 +230,6 @@ ZTEST_F(ap_pwrseq_pre_main, test_boot_time_set)
 	zassert_equal(time_S5, USEC_PER_SEC, "time_S5=%llu", time_S5);
 	zassert_equal(time_S0, 2 * USEC_PER_SEC, "time_S0=%llu", time_S0);
 }
-#endif /* CONFIG_AP_PWRSEQ_DRIVER */
 
 ZTEST(ap_pwrseq, test_ap_pwrseq_0)
 {
@@ -598,7 +596,6 @@ ZTEST(ap_pwrseq, test_insufficient_power_blocks_s5)
 		chipset_in_or_transitioning_to_state(CHIPSET_STATE_HARD_OFF));
 }
 
-#ifdef CONFIG_AP_PWRSEQ_DRIVER
 /**
  * Test boottime ec console command - this assumes the test is run after the
  * test_ap_pwrseq_0.
@@ -623,7 +620,6 @@ ZTEST_USER(ap_pwrseq, test_boot_time_console_cmd)
 	zassert_not_equal(time_S0, -1);
 	zassert_true(time_S5 <= time_S0);
 }
-#endif /* CONFIG_AP_PWRSEQ_DRIVER */
 
 void ap_pwrseq_after_test(void *data)
 {
@@ -664,9 +660,7 @@ static void ap_pwrseq_pre_main_teardown(void *fixture)
 {
 	get_time_mock = NULL;
 	free(fixture);
-	if (IS_ENABLED(CONFIG_AP_PWRSEQ_DRIVER)) {
-		ap_power_reset_boot_time();
-	}
+	ap_power_reset_boot_time();
 }
 
 ZTEST_SUITE(ap_pwrseq_pre_main, ap_power_predicate_pre_main,
