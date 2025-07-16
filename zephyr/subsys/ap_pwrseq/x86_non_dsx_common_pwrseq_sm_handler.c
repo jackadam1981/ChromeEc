6d3d7fd60fabdbe5eb3892d1426443cbf09c1844
Keith Short <keithshort@chromium.org>
2025-07-16 11:51:12 -0600

    ap_pwrseq: Add check for resume event before S0ix exit
    Add an additional check that the AP has sent the EC_CMD_HOST_SLEEP_EVENT
    event with sleep event set to HOST_SLEEP_EVENT_S0IX_RESUME before
    exiting S0ix.
    
    This handles scenarios where the SLP_S0 signal deasserts temporarily but
    the AP process never fully exited the S0ix state.
    
    BUG=b:428869290
    TEST="tast run firmware.SuspendStress.short" on brox
    
    Change-Id: I5784f3eae4fa47da01b287ee26564f301d2928e0

--- zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx_common_pwrseq_sm_handler.c
@@ -532,10 +532,8 @@ static int common_pwr_sm_run(int state)
 	case SYS_POWER_STATE_S0ix:
 		/* System in S0 only if SLP_S0 and SLP_S3 are de-asserted */
 		if (power_signals_off(IN_PCH_SLP_S0) &&
-		    signals_valid_and_off(IN_PCH_SLP_S3)) {
-			/* TODO: Make sure ap reset handling is done
-			 * before leaving S0ix.
-			 */
+		    signals_valid_and_off(IN_PCH_SLP_S3) &&
+		    ap_power_sleep_get_notify() == AP_POWER_SLEEP_RESUME) {
 			return SYS_POWER_STATE_S0ixS0;
 		} else if (!chipset_is_all_power_good())
 			return SYS_POWER_STATE_S0;
