be6de66c62aa0ebff054cd69dd0f2638d9fc8bd2
Abin Roy <abin.roy@7rays.corp-partner.google.com>
2025-07-16 16:01:20 +0530

    anraggar - test.pd.prot.src3.14
    BUG=b:None
    TEST=None
    
    Change-Id: Ide76476022fc0fe5b1369be3690da985bfe43338

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -731,20 +731,34 @@ enum pd_source_port_type {
 	PD_SOURCE_PORT_CAPABILITY_GUARANTEED,
 };
 
-/* PD Source_Info Data Object (SIDO) */
+/* PD Source_Info Data Objects (SIDOs) */
 union sido {
-	struct {
-		/* PDP values are the integer portion (floor) of the relevant
-		 * PDP rating in W.
-		 */
-		uint8_t port_reported_pdp;
-		uint8_t port_present_pdp;
-		uint8_t port_maximum_pdp;
-		unsigned reserved : 7;
-		/* 0 = Managed Capability, 1 = Guaranteed Capability */
-		unsigned port_type : 1;
-	};
-	uint32_t raw;
+	struct sidos{
+		struct sido1{
+			/* PDP values are the integer portion (floor) of the relevant
+		 	* PDP rating in W.
+		 	*/
+			uint8_t port_reported_pdp;
+			uint8_t port_present_pdp;
+			uint8_t port_maximum_pdp;
+			unsigned reserved : 7;
+			/* 0 = Managed Capability, 1 = Guaranteed Capability */
+			unsigned port_type : 1;
+		} sido1;
+		struct sido2{
+			/* PDP values are rounded to the nearest 0.5W of the relevant
+			* PDP rating in W.
+			*/
+			uint32_t port_guaranteed_pdp : 9;
+			uint32_t port_maximum_pdp : 9;
+			unsigned reserved : 12;
+			/* 0 = Non DPS port, 1 = DPS port */
+			unsigned dps_port : 1;
+			/* 0 = Managed Capability, 1 = Guaranteed Capability */
+			unsigned port_type : 1;
+		} sido2;
+	} sidos;
+	uint64_t raw;
 };
 
 /* PD Rev 3.1 Revision Message Data Object (RMDO) */
