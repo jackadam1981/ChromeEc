7f685a1732e47bfe4f4375e557fb93643efb6b2b
Patryk Duda <patrykd@google.com>
2025-07-17 17:33:31 +0200

    fpsensor: Add support for variable number of input key materials
    In the new Fingerprint Auth approach, the pairing key will be encrypted
    by the key derived from rollback entropy and optional OTP key (for
    helipilot based boards). We still want to encrypt templates by the key
    derived from rollback entropy, optional OTP key and TPM Seed.
    
    Our key deriviation implementation (copying IKMs to one array and
    passing it to HKDF as input data) is not flexible enough to support
    4 IKM combinations.
    
    Let's modify HKDF deriviation procedure to support variable number of
    IKMs.
    
    BUG=b:392915518
    TEST=./test/run_device_tests.py -b bloonchipper -f servo_micro \
         -c clang -t fpsensor_auth_crypto_stateful \
         fpsensor_auth_crypto_stateless fpsensor_crypto fpsensor_debug \
         fpsensor_utils
    TEST=./twister -v -i -T zephyr/test/fingerprint/task/ --toolchain llvm
    TEST=./twister -v -i -T zephyr/test/fingerprint/task/ --toolchain host
    TEST=make run-fpsensor_crypto_with_mock
    TEST=make run-fpsensor_crypto_with_mock_otp
    
    Change-Id: I796e9345fa57d8a9a129c3ba3783262046eb7edd

--- common/fpsensor/fpsensor_crypto.cc
+++ common/fpsensor/fpsensor_crypto.cc
@@ -9,7 +9,9 @@
 #include "openssl/aead.h"
 #include "openssl/evp.h"
 #include "openssl/hkdf.h"
+#include "openssl/hmac.h"
 #include "openssl/mem.h"
+#include "openssl/sha.h"
 #include "otp_key.h"
 #include "rollback.h"
 #include "sha256.h"
@@ -34,6 +36,43 @@ BUILD_ASSERT(IKM_SIZE_BYTES == 64);
 #if !defined(CONFIG_BORINGSSL_CRYPTO) || !defined(CONFIG_ROLLBACK_SECRET_SIZE)
 #error "fpsensor requires CONFIG_BORINGSSL_CRYPTO and ROLLBACK_SECRET_SIZE"
 #endif
+
+enum ec_error_list
+hmac_sha256(std::span<const uint8_t> key,
+	    std::span<const std::span<const uint8_t> > inputs,
+	    std::span<uint8_t, SHA256_DIGEST_LENGTH> output)
+{
+	bssl::ScopedHMAC_CTX ctx;
+
+	if (!HMAC_Init_ex(ctx.get(), key.data(), key.size(), EVP_sha256(),
+			  NULL)) {
+		return EC_ERROR_INVAL;
+	}
+
+	for (const std::span<const uint8_t> &input : inputs) {
+		if (!HMAC_Update(ctx.get(), input.data(), input.size())) {
+			return EC_ERROR_INVAL;
+		}
+	}
+
+	if (HMAC_Final(ctx.get(), output.data(), NULL) != 1) {
+		return EC_ERROR_INVAL;
+	}
+
+	return EC_SUCCESS;
+}
+
+static enum ec_error_list
+hkdf_sha256_extract(std::span<uint8_t, SHA256_DIGEST_LENGTH> prk,
+		    std::span<const std::span<const uint8_t> > ikms,
+		    std::span<const uint8_t> salt)
+{ /*
+   * As specified by RFC5869, the extract step of HKDF is HMAC of IKM
+   * (Input Key Material) with salt used as the key. The output of HMAC
+   * is PRK (Pseudo Random Key).
+   */
+	return hmac_sha256(salt, ikms, prk);
+}
 
 test_export_static enum ec_error_list
 get_ikm(std::span<uint8_t, IKM_SIZE_BYTES> ikm,
@@ -90,13 +129,19 @@ get_ikm(std::span<uint8_t, IKM_SIZE_BYTES> ikm,
 	return EC_SUCCESS;
 }
 
-bool hkdf_sha256_impl(std::span<uint8_t> out_key, std::span<const uint8_t> ikm,
+bool hkdf_sha256_impl(std::span<uint8_t> out_key,
+		      std::span<const std::span<const uint8_t> > ikms,
 		      std::span<const uint8_t> salt,
 		      std::span<const uint8_t> info)
 {
-	return HKDF(out_key.data(), out_key.size(), EVP_sha256(), ikm.data(),
-		    ikm.size(), salt.data(), salt.size(), info.data(),
-		    info.size());
+	CleanseWrapper<std::array<uint8_t, SHA256_DIGEST_LENGTH> > prk;
+
+	if (hkdf_sha256_extract(prk, ikms, salt) != EC_SUCCESS) {
+		return false;
+	}
+
+	return HKDF_expand(out_key.data(), out_key.size(), EVP_sha256(),
+			   prk.data(), prk.size(), info.data(), info.size());
 }
 
 test_mockable bool hkdf_sha256(std::span<uint8_t> out_key,
@@ -104,7 +149,9 @@ test_mockable bool hkdf_sha256(std::span<uint8_t> out_key,
 			       std::span<const uint8_t> salt,
 			       std::span<const uint8_t> info)
 {
-	return hkdf_sha256_impl(out_key, ikm, salt, info);
+	std::array ikms{ ikm };
+
+	return hkdf_sha256_impl(out_key, ikms, salt, info);
 }
 
 enum ec_error_list derive_positive_match_secret(
