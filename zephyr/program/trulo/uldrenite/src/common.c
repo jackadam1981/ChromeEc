2b1e9e6d1354bca76dbc6046abb19ac482c3be4e
Logan_Liao <Logan_Liao@compal.corp-partner.google.com>
2025-07-17 10:55:22 +0800

    uldrenite: Control gpiod6 to avoid pop issue
    This patch set gpiod6 to low when system shutdown, to avoid pop issue.
    
    BUG=none
    TEST=zmake build uldrenite
    
    Change-Id: Ib5bf0d946670f64b6690bf55096801fa8c7a41a8

--- zephyr/program/trulo/uldrenite/src/common.c
+++ zephyr/program/trulo/uldrenite/src/common.c
@@ -2,7 +2,10 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+
+#include "hooks.h"
 #include "system.h"
+#include "timer.h"
 
 /* Trigger shutdown by enabling the Z-sleep circuit */
 __override void board_hibernate_late(void)
@@ -15,3 +18,21 @@ __override void board_hibernate_late(void)
 	 */
 #endif
 }
+
+static void gpio_set_gpiod6_high_delay(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_gpiod6), 1);
+}
+DECLARE_DEFERRED(gpio_set_gpiod6_high_delay);
+
+static void gpio_set_gpiod6_high(void)
+{
+	hook_call_deferred(&gpio_set_gpiod6_high_delay_data, 7 * USEC_PER_SEC);
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, gpio_set_gpiod6_high, HOOK_PRIO_DEFAULT);
+
+static void gpio_set_gpiod6_low(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_gpiod6), 0);
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, gpio_set_gpiod6_low, HOOK_PRIO_DEFAULT);
