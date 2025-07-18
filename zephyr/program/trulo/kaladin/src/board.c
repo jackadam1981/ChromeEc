ace0ccf4012af691252a1c3266057487b16586b2
lin.arthur <lin.arthur@inventec.corp-partner.google.com>
2025-07-18 22:11:30 +0800

    kaladin: Enable strauss keyboard
    This patch enable strauss keyboard for kaladin.
    
    BUG=b:421781342, b:430391502
    TEST=zmake build kaladin
    LOW_COVERAGE_REASON=test code is not yet ready b:421825142
    
    Change-Id: I078375c17ff66470a632df7366ea1d0daaf2cc1d

---
+++ zephyr/program/trulo/kaladin/src/board.c
@@ -0,0 +1,77 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "cros_cbi.h"
+#include "gpio.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "keyboard_8042_sharedlib.h"
+#include "keyboard_config.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(board_init, LOG_LEVEL_INF);
+
+static bool has_backlight = FW_KB_BL_NOT_PRESENT;
+int8_t board_vivaldi_keybd_idx(void)
+{
+	if (has_backlight == FW_KB_BL_NOT_PRESENT) {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_0));
+	} else {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_1));
+	}
+}
+
+/*
+ * Keyboard function decided by FW config.
+ */
+test_export_static void kb_init(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FW_KB_BL, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FW_KB_BL);
+		return;
+	}
+
+	if (val == FW_KB_BL_PRESENT) {
+		LOG_INF("CBI FW_CONFIG: FW_KB_BL_PRESENT.");
+		has_backlight = FW_KB_BL_PRESENT;
+	} else {
+		LOG_INF("CBI FW_CONFIG: FW_KB_BL_NOT_PRESENT.");
+		has_backlight = FW_KB_BL_NOT_PRESENT;
+	}
+}
+DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_I2C);
+
+/*
+ * Keyboard function decided by FW config.
+ */
+test_export_static void kb_layout_init(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FW_KB_LAYOUT, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_KB_LAYOUT field %d",
+			FW_KB_LAYOUT);
+		return;
+	}
+	/*
+	 * If keyboard is US2(FW_KB_LAYOUT_US2), we need translate right ctrl
+	 * to backslash(\|) key.
+	 */
+	if (val == FW_KB_LAYOUT_US2) {
+		set_scancode_set2(3, 14, get_scancode_set2(3, 11));
+#ifdef CONFIG_KEYBOARD_DEBUG
+		set_keycap_label(3, 14, get_keycap_label(3, 11));
+#endif
+	}
+}
+DECLARE_HOOK(HOOK_INIT, kb_layout_init, HOOK_PRIO_POST_I2C);
