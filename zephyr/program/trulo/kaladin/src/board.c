193417b6490de54464bf94ecd9ec316904ef3a46
lin.arthur <lin.arthur@inventec.corp-partner.google.com>
2025-07-21 20:33:05 +0800

    kaladin: custom discharge on ac function
    This patch custom discharge on ac function by gpio control.
    
    BUG=b:430617498
    TEST=zmake build kaladin
    LOW_COVERAGE_REASON=test code is not yet ready b:421825142
    
    Change-Id: Ib536eb2543163ea3ecd236cb2aa0e5874e643e6c

--- zephyr/program/trulo/kaladin/src/board.c
+++ zephyr/program/trulo/kaladin/src/board.c
@@ -3,6 +3,7 @@
  * found in the LICENSE file.
  */
 
+#include "charge_manager.h"
 #include "cros_cbi.h"
 #include "gpio.h"
 #include "gpio/gpio_int.h"
@@ -75,3 +76,26 @@ test_export_static void kb_layout_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_INIT, kb_layout_init, HOOK_PRIO_POST_I2C);
+int board_discharge_on_ac(int enable)
+{
+	LOG_INF("Kaladin: discharge on AC: %d", enable);
+
+	int port, c0_hv_disable, c1_hv_disable;
+
+	port = charge_manager_get_active_charge_port();
+	c0_hv_disable = (enable && port == 0);
+	c1_hv_disable = (enable && port == 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_usb_c0_hv_disable),
+			c0_hv_disable);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_usb_c1_hv_disable),
+			c1_hv_disable);
+	LOG_INF("Enable: %d, port: %d", enable, port);
+	LOG_INF("gpio_usb_c0_hv_disable: %d",
+		gpio_pin_get_dt(
+			GPIO_DT_FROM_NODELABEL(gpio_usb_c0_hv_disable)));
+	LOG_INF("gpio_usb_c1_hv_disable: %d",
+		gpio_pin_get_dt(
+			GPIO_DT_FROM_NODELABEL(gpio_usb_c1_hv_disable)));
+
+	return EC_SUCCESS;
+}
