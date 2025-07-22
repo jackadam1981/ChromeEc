a8592c8a7687fb6003cf59ac1aa220186af139d3
Cheng Yueh <cyueh@chromium.org>
2025-07-22 13:35:53 +0800

    tps6699x: Set ucsi cmd base on port_index_on_chip
    If there are two single port chips on the device, then the driver
    should sent ucsi command with index 1.
    
    If there is one dual-port chip on the device, then the driver
    should sent ucsi command with index 1 or 2 respectively.
    
    We add a new property port_index_on_chip to enable configuring the
    combination in dts.
    
    Fallback to (connector_number + 1) for unconfigured dts.
    
    BUG=b:425235885
    TEST=(0) Get a device with two single port TPS board.
    TEST=(1) zmake build skywalker
    TEST=(2) ./util/flash_ec --board skywalker --zephyr
    TEST=(3) `pdc status 1` in ec console.
    TEST=(4) Without this change, ec console spams logs.
    TEST=(5) With this change, outputs `Enable`.
    TEST=(6) Get a device with one dual-port TPS board.
    TEST=(7) zmake build uldernite
    TEST=(8) ./util/flash_ec --board uldernite --zephyr
    TEST=(9) Both ports use correct ucsi index.
    TEST=(A) ./twister -v -i -p native_sim --coverage -s pdc.tps6699x
    
    Change-Id: Icc8cc16059ac3f14b8f11e7a0c40d75c13b120df

--- zephyr/drivers/usbc/pdc_tps6699x.c
+++ zephyr/drivers/usbc/pdc_tps6699x.c
@@ -217,6 +217,8 @@ struct pdc_config_t {
 	bool no_fw_update;
 	/** Whether or not this port supports CCD */
 	bool ccd;
+	/** The index of the port on chip */
+	uint8_t port_index_on_chip;
 };
 
 /**
@@ -670,7 +672,7 @@ static int handle_irqs(struct pdc_data_t *data)
 	 */
 	if (pdc_interrupt.ucsi_connector_status_change_notification) {
 		data->use_cached_conn_status_change = false;
-		data->cci_event.connector_change = cfg->connector_number + 1;
+		data->cci_event.connector_change = cfg->port_index_on_chip;
 	}
 
 	if (pdc_interrupt.plug_insert_or_removal) {
@@ -1366,7 +1368,7 @@ static void cmd_is_vconn_sourcing(struct pdc_data_t *data)
 		LOG_ERR("Failed to power path status");
 		goto error_recovery;
 	}
-	ext_vconn_sw = cfg->connector_number == 0 ?
+	ext_vconn_sw = cfg->port_index_on_chip == 1 ?
 			       pdc_power_path_status.pa_vconn_sw :
 			       pdc_power_path_status.pb_vconn_sw;
 
@@ -1827,7 +1829,7 @@ static void task_srdy(struct pdc_data_t *data)
 		goto error_recovery;
 	}
 
-	ext_vbus_sw = (cfg->connector_number == 0 ?
+	ext_vbus_sw = (cfg->port_index_on_chip == 1 ?
 			       pdc_power_path_status.pa_ext_vbus_sw :
 			       pdc_power_path_status.pb_ext_vbus_sw);
 	cur_sink_enabled = (ext_vbus_sw == EXT_VBUS_SWITCH_ENABLED_INPUT);
@@ -1849,7 +1851,7 @@ static void task_srdy(struct pdc_data_t *data)
 		/* TODO(b/358274846) - Check whether this can be moved to
 		 * appconfig so we don't have to select by connector number.
 		 */
-		cmd_data.data[0] = cfg->connector_number ?
+		cmd_data.data[0] = cfg->port_index_on_chip != 1 ?
 					   SWITCH_SELECT_PP_EXT1 :
 					   SWITCH_SELECT_PP_EXT2;
 		/* Enable Sink FET */
@@ -1962,7 +1964,7 @@ static void task_ucsi(struct pdc_data_t *data, enum ucsi_command_t ucsi_command)
 	/* Byte 1: Data length per UCSI spec */
 	cmd_data.data[1] = 0;
 	/* Connector Number: Byte 2, bits 6:0. Bit 7 is reserved */
-	cmd_data.data[2] = cfg->connector_number + 1;
+	cmd_data.data[2] = cfg->port_index_on_chip;
 
 	/* TODO(b/345783692): The bit shifts in this function come from the
 	 * awkward mapping between the structures in ucsi_v3.h and the TI
@@ -2826,7 +2828,7 @@ static int tps_execute_ucsi_cmd(const struct device *dev, uint8_t ucsi_command,
 	 * required (and will be on Byte 3, bits 14:8).
 	 */
 	if (ucsi_command != UCSI_GET_ALTERNATE_MODES) {
-		cmd_data.data[2] |= (cfg->connector_number + 1) & 0x7f;
+		cmd_data.data[2] |= cfg->port_index_on_chip & 0x7f;
 	}
 
 	return tps_post_command_with_callback(dev, cmd, &cmd_data, lpm_data_out,
@@ -3071,6 +3073,12 @@ static void tps_thread(void *dev, void *unused1, void *unused2)
 		.create_thread = create_thread_##inst,                         \
 		.no_fw_update = DT_INST_PROP(inst, no_fw_update),              \
 		.ccd = DT_INST_PROP(inst, ccd),                                \
+		.port_index_on_chip =                                          \
+			DT_INST_PROP(inst, port_index_on_chip) ?               \
+				DT_INST_PROP(inst, port_index_on_chip) :       \
+				USBC_PORT_FROM_PDC_DRIVER_NODE(                \
+					DT_DRV_INST(inst)) +                   \
+					1,                                     \
 	};                                                                     \
                                                                                \
 	DEVICE_DT_INST_DEFINE(inst, pdc_init, NULL,                            \
