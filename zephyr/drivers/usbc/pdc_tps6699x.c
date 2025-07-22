c1861872dc5af455b13a0a49393404a715488b04
Cheng Yueh <cyueh@chromium.org>
2025-07-22 13:35:53 +0800

    tps6699x: Set ucsi cmd base on port_index_on_chip
    If there are two single port chips on the device, then the driver
    should sent ucsi command with index 1.
    
    If there is one dual-port chip on the device, then the driver
    should sent ucsi command with index 1 or 2 respectively.
    
    We add a new property port_index_on_chip to enable configuring the
    combination in dts.
    
    BUG=b:425235885
    TEST=(0) Get a device with two single port TPS board.
    TEST=(1) zmake build skywalker
    TEST=(2) ./util/flash_ec --board skywalker --zephyr
    TEST=(3) `pdc status 1` in ec console.
    TEST=(4) Without this change, ec console spams logs.
    TEST=(5) With this change, outputs `Enable`.
    TEST=(6) Get a device with one dual-port TPS board.
    TEST=(7) zmake build uldernite
    TEST=(8) ./util/flash_ec --board uldernite --zephyr
    TEST=(9) Both ports use correct ucsi index.
    TEST=(A) ./twister -ivc -T zephyr/test/pdc
    
    Change-Id: Icc8cc16059ac3f14b8f11e7a0c40d75c13b120df

--- zephyr/drivers/usbc/pdc_tps6699x.c
+++ zephyr/drivers/usbc/pdc_tps6699x.c
@@ -219,6 +219,8 @@ struct pdc_config_t {
 	bool no_fw_update;
 	/** Whether or not this port supports CCD */
 	bool ccd;
+	/** The index of the port on chip */
+	uint8_t port_index_on_chip;
 };
 
 /**
@@ -380,6 +382,12 @@ static void tps_check_and_notify_irq(void);
  * @brief PDC port data used in interrupt handler
  */
 static struct pdc_data_t *const pdc_data[NUM_PDC_TPS6699X_PORTS];
+
+static inline bool is_first_port_on_chip(struct pdc_data_t *data)
+{
+	struct pdc_config_t const *cfg = data->dev->config;
+	return cfg->port_index_on_chip == 1;
+}
 
 static enum state_t get_state(struct pdc_data_t *data)
 {
@@ -1368,7 +1376,7 @@ static void cmd_is_vconn_sourcing(struct pdc_data_t *data)
 		LOG_ERR("Failed to power path status");
 		goto error_recovery;
 	}
-	ext_vconn_sw = cfg->connector_number == 0 ?
+	ext_vconn_sw = is_first_port_on_chip(data) ?
 			       pdc_power_path_status.pa_vconn_sw :
 			       pdc_power_path_status.pb_vconn_sw;
 
@@ -1829,7 +1837,7 @@ static void task_srdy(struct pdc_data_t *data)
 		goto error_recovery;
 	}
 
-	ext_vbus_sw = (cfg->connector_number == 0 ?
+	ext_vbus_sw = (is_first_port_on_chip(data) ?
 			       pdc_power_path_status.pa_ext_vbus_sw :
 			       pdc_power_path_status.pb_ext_vbus_sw);
 	cur_sink_enabled = (ext_vbus_sw == EXT_VBUS_SWITCH_ENABLED_INPUT);
@@ -1849,11 +1857,11 @@ static void task_srdy(struct pdc_data_t *data)
 		}
 
 		/* TODO(b/358274846) - Check whether this can be moved to
-		 * appconfig so we don't have to select by connector number.
+		 * appconfig so we don't have to select by port index on chip.
 		 */
-		cmd_data.data[0] = cfg->connector_number ?
-					   SWITCH_SELECT_PP_EXT1 :
-					   SWITCH_SELECT_PP_EXT2;
+		cmd_data.data[0] = is_first_port_on_chip(data) ?
+					   SWITCH_SELECT_PP_EXT2 :
+					   SWITCH_SELECT_PP_EXT1;
 		/* Enable Sink FET */
 		rv = write_task_cmd(cfg, COMMAND_TASK_SRDY, &cmd_data);
 	} else if (!data->snk_fet_en && cur_sink_enabled) {
@@ -1964,7 +1972,7 @@ static void task_ucsi(struct pdc_data_t *data, enum ucsi_command_t ucsi_command)
 	/* Byte 1: Data length per UCSI spec */
 	cmd_data.data[1] = 0;
 	/* Connector Number: Byte 2, bits 6:0. Bit 7 is reserved */
-	cmd_data.data[2] = cfg->connector_number + 1;
+	cmd_data.data[2] = cfg->port_index_on_chip;
 
 	/* TODO(b/345783692): The bit shifts in this function come from the
 	 * awkward mapping between the structures in ucsi_v3.h and the TI
@@ -2807,6 +2815,7 @@ static int tps_execute_ucsi_cmd(const struct device *dev, uint8_t ucsi_command,
 	struct pdc_config_t const *cfg = dev->config;
 	union reg_data cmd_data;
 	enum cmd_t cmd = CMD_RAW_UCSI;
+	int port_index_on_chip_byte_index;
 
 	memset(cmd_data.data, 0, sizeof(cmd_data.data));
 	/* Byte 0: UCSI Command Code */
@@ -2821,15 +2830,18 @@ static int tps_execute_ucsi_cmd(const struct device *dev, uint8_t ucsi_command,
 		memcpy(&cmd_data.data[2], command_specific, data_size);
 	}
 
-	/* TI UCSI tasks always require a connector number even when the UCSI
+	/* TI UCSI tasks always require a port index on chip even when the UCSI
 	 * spec doesn't require it. Except GET_ALTERNATE_MODES, all other
-	 * commands will fit the connector number on Byte 2, bits 6:0. There's
-	 * no need to modify it for GET_ALTERNATE_MODES since it is always
-	 * required (and will be on Byte 3, bits 14:8).
+	 * commands will fit the port index on chip on Byte 2, bits 6:0. For
+	 * GET_ALTERNATE_MODES, it is required and should be on Byte 3, bits
+	 * 14:8.
 	 */
-	if (ucsi_command != UCSI_GET_ALTERNATE_MODES) {
-		cmd_data.data[2] |= (cfg->connector_number + 1) & 0x7f;
-	}
+
+	port_index_on_chip_byte_index =
+		ucsi_command == UCSI_GET_ALTERNATE_MODES ? 3 : 2;
+	cmd_data.data[port_index_on_chip_byte_index] &= ~0x7f;
+	cmd_data.data[port_index_on_chip_byte_index] |=
+		cfg->port_index_on_chip & 0x7f;
 
 	return tps_post_command_with_callback(dev, cmd, &cmd_data, lpm_data_out,
 					      callback);
@@ -3073,6 +3085,7 @@ static void tps_thread(void *dev, void *unused1, void *unused2)
 		.create_thread = create_thread_##inst,                         \
 		.no_fw_update = DT_INST_PROP(inst, no_fw_update),              \
 		.ccd = DT_INST_PROP(inst, ccd),                                \
+		.port_index_on_chip = DT_INST_PROP(inst, port_index_on_chip),  \
 	};                                                                     \
                                                                                \
 	DEVICE_DT_INST_DEFINE(inst, pdc_init, NULL,                            \
