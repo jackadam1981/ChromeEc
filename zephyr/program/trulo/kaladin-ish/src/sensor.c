48a19cb578bbb2df86f2d359e6aac47443fd3f75
Leo Chen <chen.leoss@inventec.corp-partner.google.com>
2025-07-23 11:01:11 +0800

    kaladin: Add ISH firmware variant
    Copy uldrenite-ish as a kaladin ISH firmware and enabled DSP comms service for
    kaladin. kaladin-ish comes with the DSP comms client.
    
    BUG=b:429285119
    TEST=zmake build kaladin
    
    Change-Id: Ida83fb26c8808a5508ac9874fa0099a7de284828

---
+++ zephyr/program/trulo/kaladin-ish/src/sensor.c
@@ -0,0 +1,1 @@
+../../kaladin/src/sensor.c
---
+++ zephyr/program/trulo/kaladin/src/sensor.c
@@ -0,0 +1,54 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "accelgyro.h"
+#include "common.h"
+#include "cros_board_info.h"
+#include "cros_cbi.h"
+#include "driver/accel_bma422.h"
+#include "driver/accel_lis2dw12_public.h"
+#include "driver/accelgyro_bmi323.h"
+#include "driver/accelgyro_lsm6dsm.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "motion_sense.h"
+#include "motionsense_sensors.h"
+#include "tablet_mode.h"
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(kaladin_sensor, LOG_LEVEL_INF);
+
+enum base_sensor_type {
+	base_lis2dw12 = 0,
+};
+
+enum lid_sensor_type {
+	lid_lis2dw12 = 0,
+};
+
+static int sensor_fwconfig;
+
+void motion_interrupt(enum gpio_signal signal)
+{
+	lis2dw12_interrupt(signal);
+}
+
+void lid_accel_interrupt(enum gpio_signal signal)
+{
+	bma4xx_interrupt(signal);
+}
+
+static void motionsense_init(void)
+{
+	int ret;
+
+	ret = cros_cbi_get_fw_config(FW_TABLET, &sensor_fwconfig);
+	if (ret < 0) {
+		LOG_ERR("error retriving CBI config: %d", ret);
+		return;
+	}
+}
+DECLARE_HOOK(HOOK_INIT, motionsense_init, HOOK_PRIO_DEFAULT);
