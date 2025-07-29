druth0/chrome-ec

commit 7698835e36d71b7eec7b76dda7915ce1dbaaa12e

pm: Disable all PM state in high throughput cases
When the motionsense task is running a "high" throughput configuration,
we should avoid letting the CPU suspend. This is normally avoided by
having 1 sensor in a polling mode since the wait timeout will be set to
a value matching the expected next sample, but when all sensors are
using interrupts, this assumption breaks.

Add a new function to system.h that is only valid in Zephyr based builds
(by not implementing it under common/ we guarantee that the build will
break if the function was used in legacy ECOS). When called, this
function will loop through all the power states and lock them.
Similarly, a "put" variant will loop through all the power states and
unlock them.

BUG=b:427883250
TEST=5x tast run $DUT_IP hardware.SensorIioserviceHard
TEST=Sensor CTS 100% pass rate

Change-Id: I7fa71c7ba09858cc6eec8e6f15833fb7693dc6b5

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6736522
Reviewed-by: Keith Short <keithshort@chromium.org>
Tested-by: Yuval Peress <peress@google.com>
Code-Coverage: Yuval Peress <peress@google.com>
Commit-Queue: Yuval Peress <peress@google.com>

common/motion_sense.c
@@ -28,6 +28,7 @@
 #include "power.h"
 #include "printf.h"
 #include "queue.h"
+#include "system.h"
 #include "tablet_mode.h"
 #include "task.h"
 #include "timer.h"
@@ -68,6 +69,24 @@ test_export_static enum chipset_state_mask sensor_active;
  * but it allows to be tested.
  */
 test_export_static int wait_us;
+
+/* Make sure the threshold is set for legacy boards that don't use kconfig. */
+#ifndef CONFIG_PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM_THRESHOLD
+#define CONFIG_PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM_THRESHOLD 0
+#endif
+
+/**
+ * Check if:
+ * 1. We enabled PM control on high throughput sensors
+ * 2. We're about to wait indefinitely
+ * 3. The fastest collection rate is faster than the threshold
+ */
+#define DISABLE_PM_POLICY_WHILE_WAITING(fastest_collection_rate)               \
+	(IS_ENABLED(                                                           \
+		 CONFIG_PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM) && \
+	 wait_us == -1 &&                                                      \
+	 fastest_collection_rate <=                                            \
+		 CONFIG_PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM_THRESHOLD)
 
 STATIC_IF(CONFIG_ACCEL_SPOOF_MODE) void print_spoof_mode_status(int id);
 STATIC_IF(CONFIG_GESTURE_DETECTION)
@@ -1005,12 +1024,19 @@ void motion_sense_task(void *u)
 
 		ts_end_task = get_time();
 		wait_us = -1;
+		uint32_t fastest_collection_rate = UINT32_MAX;
 
 		for (i = 0; i < motion_sensor_count; i++) {
 			struct motion_sensor_t *sensor = &motion_sensors[i];
 			enum sensor_config cfg_index =
 				motion_sense_get_ec_config();
 			int ec_rate = 0;
+
+			if (sensor->collection_rate > 0 &&
+			    sensor->collection_rate < fastest_collection_rate) {
+				fastest_collection_rate =
+					sensor->collection_rate;
+			}
 
 			if (!motion_sensor_in_forced_mode(sensor) ||
 			    sensor->collection_rate == 0)
@@ -1043,7 +1069,13 @@ void motion_sense_task(void *u)
 			wait_us = motion_min_interval;
 		}
 
+		if (DISABLE_PM_POLICY_WHILE_WAITING(fastest_collection_rate)) {
+			pm_policy_state_lock_get_all();
+		}
 		event = task_wait_event(wait_us);
+		if (DISABLE_PM_POLICY_WHILE_WAITING(fastest_collection_rate)) {
+			pm_policy_state_lock_put_all();
+		}
 	}
 }
 

include/system.h
@@ -642,6 +642,20 @@ static inline void disable_sleep(uint32_t mask)
 	atomic_or(&sleep_mask, mask);
 }
 
+/**
+ * Increment lock on all power management states and keep CPU active.
+ *
+ * @see pm_policy_state_lock_get from Zephyr for more information.
+ */
+void pm_policy_state_lock_get_all(void);
+
+/**
+ * Decrement lock all power management states.
+ *
+ * @see pm_policy_state_lock_put from Zephyr for more information.
+ */
+void pm_policy_state_lock_put_all(void);
+
 #ifdef CONFIG_LOW_POWER_IDLE_LIMITED
 /*
  * If this variable is nonzero, all levels of idle modes are disabled.

zephyr/Kconfig.motionsense
@@ -24,6 +24,25 @@ config PLATFORM_EC_SENSOR_EC_RATE_FORCE_MODE
       Enable this to toggle interrupt mode sensors to force mode at specific low
       intervals in order to save power in low power states (S3 and S5)
 
+config PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM
+    bool "Disable power management when sensor sample rates are high"
+    depends on PM
+    default y if SOC_FAMILY_INTEL_ISH
+    help
+      When the sensors are being sampled quickly, the delay of waking up the CPU
+      might cause ISR scheduling delays. When these delays happen, the CPU will
+      miss samples. Enable this config to prevent the CPU from going below the
+      PM_STATE_ACTIVE when sample rates are high.
+
+config PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM_THRESHOLD
+    int "Threshold (in microseconds) for disabling power management"
+    depends on PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM
+    default 6000
+    help
+      Set this threshold to control when PM will be disabled during the sensor
+      loop. Lower values mean that we require high sample rates in order to
+      disable PM. By default we use 6000 microseconds (~166Hz).
+
 config PLATFORM_EC_ACCEL_FIFO
     bool "Sensor FIFO"
     help

zephyr/shim/src/CMakeLists.txt
@@ -6,6 +6,7 @@ zephyr_library_sources(console.c)
 zephyr_library_sources(crc.c)
 zephyr_library_sources(gpio.c)
 zephyr_library_sources(gpio_int.c)
+zephyr_library_sources_ifdef(CONFIG_PM pm_state.c)
 
 add_subdirectory("buttons")
 add_subdirectory("cbi")

zephyr/shim/src/pm_state.c
@@ -1,0 +1,32 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "system.h"
+
+#include <stdint.h>
+
+#include <zephyr/pm/pm.h>
+#include <zephyr/pm/policy.h>
+#include <zephyr/pm/state.h>
+
+static void pm_policy_state_for_each(void (*fn)(enum pm_state, uint8_t))
+{
+	const struct pm_state_info *cpu_states;
+	uint8_t num_cpu_states = pm_state_cpu_get_all(0U, &cpu_states);
+
+	for (uint8_t i = 0; i < num_cpu_states; ++i) {
+		fn(cpu_states[i].state, PM_ALL_SUBSTATES);
+	}
+}
+
+void pm_policy_state_lock_get_all(void)
+{
+	pm_policy_state_for_each(pm_policy_state_lock_get);
+}
+
+void pm_policy_state_lock_put_all()
+{
+	pm_policy_state_for_each(pm_policy_state_lock_put);
+}

zephyr/test/system_shim/boards/native_sim.overlay
@@ -6,6 +6,39 @@
 #include <board-overlays/native_sim.dts>
 
 / {
+	cpus {
+		cpu@0 {
+			cpu-power-states = <&state0 &state1 &state2 &state3>;
+		};
+		power-states {
+			state0: state0 {
+				compatible = "zephyr,power-state";
+				power-state-name = "suspend-to-idle";
+				min-residency-us = <10000>;
+				exit-latency-us = <100>;
+			};
+
+			state1: state1 {
+				compatible = "zephyr,power-state";
+				power-state-name = "standby";
+				min-residency-us = <20000>;
+				exit-latency-us = <200>;
+			};
+
+			state2: state2 {
+				compatible = "zephyr,power-state";
+				power-state-name = "suspend-to-ram";
+				min-residency-us = <50000>;
+				exit-latency-us = <500>;
+			};
+
+			state3: state3 {
+				compatible = "zephyr,power-state";
+				power-state-name = "suspend-to-disk";
+				status = "disabled";
+			};
+		};
+	};
 	named-gpios {
 		compatible = "named-gpios";
 

zephyr/test/system_shim/src/pm_handlers.c
@@ -11,6 +11,8 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
 	ARG_UNUSED(substate_id);
 	ARG_UNUSED(state);
+
+	k_cpu_idle();
 }
 
 void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)

zephyr/test/system_shim/src/test_system.c
@@ -14,6 +14,8 @@
 #include <zephyr/drivers/bbram.h>
 #include <zephyr/fff.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/pm/policy.h>
+#include <zephyr/pm/state.h>
 #include <zephyr/shell/shell_dummy.h>
 #include <zephyr/ztest_assert.h>
 #include <zephyr/ztest_test.h>
@@ -292,4 +294,30 @@ ZTEST(system, test_init_cause_watchdog_rst)
 
 	zassert_ok(system_preinitialize(NULL));
 	zassert_equal(EC_RESET_FLAG_WATCHDOG, system_get_reset_flags());
+}
+
+ZTEST(system, test_lock_all_power_states)
+{
+	const struct pm_state_info *cpu_states;
+	uint8_t num_cpu_states = pm_state_cpu_get_all(0U, &cpu_states);
+
+	pm_policy_state_lock_get_all();
+
+	for (uint8_t i = 0; i < num_cpu_states; ++i) {
+		zassert_false(
+			pm_policy_state_is_available(cpu_states[i].state,
+						     cpu_states[i].substate_id),
+			"Expected CPU state %d to be unavailable (%d::%d)", i,
+			cpu_states[i].state, cpu_states[i].substate_id);
+	}
+
+	pm_policy_state_lock_put_all();
+
+	for (uint8_t i = 0; i < num_cpu_states; ++i) {
+		zassert_true(
+			pm_policy_state_is_available(cpu_states[i].state,
+						     cpu_states[i].substate_id),
+			"Expected CPU state %d to be ailable (%d::%d)", i,
+			cpu_states[i].state, cpu_states[i].substate_id);
+	}
 }

jackadam1981/ChromeEc

commit 6db3919eb1a2d540c8fc3cfc78f7fee877da3e6f

motion_sense: Update DISABLE_PM_POLICY_WHILE_WAITING
Update the macro to match requests made on chromium:6736522 after it
landed.

BUG=None TEST=CQ Fixes: 7698835e36 ("pm: Disable all PM state in high
throughput cases")

Change-Id: Ieffa44a82271e9facc21c555714f13509a7f645d

common/motion_sense.c
@@ -81,11 +81,11 @@ test_export_static int wait_us;
  * 2. We're about to wait indefinitely
  * 3. The fastest collection rate is faster than the threshold
  */
-#define DISABLE_PM_POLICY_WHILE_WAITING(fastest_collection_rate)               \
+#define DISABLE_PM_POLICY_WHILE_WAITING(wait_us_, collection_rate_)            \
 	(IS_ENABLED(                                                           \
 		 CONFIG_PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM) && \
-	 wait_us == -1 &&                                                      \
-	 fastest_collection_rate <=                                            \
+	 wait_us_ == -1 &&                                                     \
+	 collection_rate_ <=                                                   \
 		 CONFIG_PLATFORM_EC_MOTIONSENSE_DISABLE_HIGH_THROUGHPUT_PM_THRESHOLD)
 
 STATIC_IF(CONFIG_ACCEL_SPOOF_MODE) void print_spoof_mode_status(int id);
@@ -1069,11 +1069,13 @@ void motion_sense_task(void *u)
 			wait_us = motion_min_interval;
 		}
 
-		if (DISABLE_PM_POLICY_WHILE_WAITING(fastest_collection_rate)) {
+		if (DISABLE_PM_POLICY_WHILE_WAITING(wait_us,
+						    fastest_collection_rate)) {
 			pm_policy_state_lock_get_all();
 		}
 		event = task_wait_event(wait_us);
-		if (DISABLE_PM_POLICY_WHILE_WAITING(fastest_collection_rate)) {
+		if (DISABLE_PM_POLICY_WHILE_WAITING(wait_us,
+						    fastest_collection_rate)) {
 			pm_policy_state_lock_put_all();
 		}
 	}