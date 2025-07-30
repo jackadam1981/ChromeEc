druth0/chrome-ec

commit 97bf512e7bda85d77462ca1f22dce3bb02daed9e

motion_sense: Make activity sensor ID unsigned always
The ternary for `? motion_sensor_count : -1` was causing an error with
strict checks since the motion_sensor_count is unsigned. Instead of
relying on the compiler to cast -1 to a uint8_t, we can create an
invalid ID at 0xff and check against that.

BUG=b:281981515,b:424657530
TEST=CQ

Change-Id: I9a20c0971fa6e8a00bf49b4fb072a508902ea109

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6689095
Commit-Queue: Yuval Peress <peress@google.com>
Code-Coverage: Yuval Peress <peress@google.com>
Tested-by: Yuval Peress <peress@google.com>
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>

include/motion_sense.h
@@ -330,6 +330,9 @@ void motion_sense_push_raw_xyz(struct motion_sensor_t *s);
  */
 bool motion_sensor_in_forced_mode(const struct motion_sensor_t *s);
 
+/* A sensor ID set to this value is invalid. */
+#define MOTION_SENSE_INVALID_SENSOR_ID (0xff)
+
 /*
  * There are 4 variables that represent the number of sensors:
  * SENSOR_COUNT: The number of available motion sensors in board.
@@ -342,19 +345,21 @@ bool motion_sensor_in_forced_mode(const struct motion_sensor_t *s);
 #if defined(CONFIG_GESTURE_HOST_DETECTION) || defined(CONFIG_ORIENTATION_SENSOR)
 /* Add an extra sensor. We may need to add more */
 #ifdef CONFIG_DYNAMIC_MOTION_SENSOR_COUNT
-#define MOTION_SENSE_ACTIVITY_SENSOR_ID \
-	(motion_sensor_count > 0 ? motion_sensor_count : -1)
+#define MOTION_SENSE_ACTIVITY_SENSOR_ID                  \
+	(motion_sensor_count > 0 ? motion_sensor_count : \
+				   MOTION_SENSE_INVALID_SENSOR_ID)
 #else
 #define MOTION_SENSE_ACTIVITY_SENSOR_ID (motion_sensor_count)
 #endif
 #define MAX_MOTION_SENSORS (SENSOR_COUNT + 1)
 #else
-#define MOTION_SENSE_ACTIVITY_SENSOR_ID (-1)
+#define MOTION_SENSE_ACTIVITY_SENSOR_ID MOTION_SENSE_INVALID_SENSOR_ID
 #define MAX_MOTION_SENSORS (SENSOR_COUNT)
 #endif
-#define ALL_MOTION_SENSORS                            \
-	((int)(MOTION_SENSE_ACTIVITY_SENSOR_ID) > 0 ? \
-		 motion_sensor_count + 1 :            \
+#define ALL_MOTION_SENSORS                                \
+	((int)(MOTION_SENSE_ACTIVITY_SENSOR_ID) ==        \
+			 MOTION_SENSE_INVALID_SENSOR_ID ? \
+		 motion_sensor_count + 1 :                \
 		 motion_sensor_count)
 
 #ifdef CONFIG_ALS_LIGHTBAR_DIMMING

common/motion_sense.c
@@ -1169,7 +1169,8 @@ static enum ec_status host_cmd_motion_sense(struct host_cmd_handler_args *args)
 		}
 
 		if (IS_ENABLED(CONFIG_GESTURE_HOST_DETECTION) &&
-		    MOTION_SENSE_ACTIVITY_SENSOR_ID >= 0 &&
+		    MOTION_SENSE_ACTIVITY_SENSOR_ID !=
+			    MOTION_SENSE_INVALID_SENSOR_ID &&
 		    (in->sensor_odr.sensor_num ==
 		     MOTION_SENSE_ACTIVITY_SENSOR_ID))
 			out->info.type = MOTIONSENSE_TYPE_ACTIVITY;
@@ -1512,7 +1513,8 @@ static enum ec_status host_cmd_motion_sense(struct host_cmd_handler_args *args)
 	case MOTIONSENSE_CMD_SPOOF: {
 		/* spoof activity if it is activity sensor */
 		if (IS_ENABLED(CONFIG_GESTURE_HOST_DETECTION) &&
-		    MOTION_SENSE_ACTIVITY_SENSOR_ID >= 0 &&
+		    MOTION_SENSE_ACTIVITY_SENSOR_ID !=
+			    MOTION_SENSE_INVALID_SENSOR_ID &&
 		    in->spoof.sensor_id == MOTION_SENSE_ACTIVITY_SENSOR_ID) {
 			switch (in->spoof.activity_num) {
 #ifdef CONFIG_BODY_DETECTION

jackadam1981/ChromeEc

commit 5ccd451f3164e413e78eb95d1d81a32303afabab

motion_sense: Fix incorrect ALL_MOTION_SENSORS
There was a mistake in a previous CL with the comparator of
MOTION_SENSE_INVALID_SENSOR_ID in order to assign the ALL_MOTION_SENSORS
value.

BUG=b:433880075
TEST=CQ
Fixes: 97bf512e7bda8 ("motion_sense: Make activity sensor ID unsigned always")

Change-Id: I6ee2be5d90fbcb3c99c279c01bf8a8d0c95cb92b

include/motion_sense.h
@@ -358,7 +358,7 @@ bool motion_sensor_in_forced_mode(const struct motion_sensor_t *s);
 #define MAX_MOTION_SENSORS (SENSOR_COUNT)
 #endif
 #define ALL_MOTION_SENSORS                                \
-	((int)(MOTION_SENSE_ACTIVITY_SENSOR_ID) ==        \
+	((int)(MOTION_SENSE_ACTIVITY_SENSOR_ID) !=        \
 			 MOTION_SENSE_INVALID_SENSOR_ID ? \
 		 motion_sensor_count + 1 :                \
 		 motion_sensor_count)