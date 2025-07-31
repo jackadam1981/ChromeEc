c09d882797c74df1ca6cd6780e535a559088fac1
Bartosz Szpila <bszpila@google.com>
2025-07-31 12:23:28 -0700

    pdc_rts54xx: Add battery status/capability reporting over vendor command.
    This patch introduces support for battery status and capacity reporting via vendor commands to the RTS54xx pdc driver.
    It adds a callback to the battery SOC change hook to update the capacity and status on any change.
    It adds a function that will update the status and capacity on S0,S3 resume and also during system startup.
    
    This is needed to comply with USB PD spec.
    
    BUG=b:412291685
    TEST=manual
    
    Change-Id: Iec5ddc45f309ec92a7f0e7193a6cf44c462311d8

--- zephyr/test/pdc/include/fakes.h
+++ zephyr/test/pdc/include/fakes.h
@@ -5,4 +5,18 @@
 
 #include "battery.h"
 
+#include <zephyr/fff.h>
+
+DECLARE_FAKE_VALUE_FUNC(int, battery_design_voltage, int *);
+DECLARE_FAKE_VALUE_FUNC(int, battery_remaining_capacity, int *);
+DECLARE_FAKE_VALUE_FUNC(int, battery_status, int *);
+DECLARE_FAKE_VALUE_FUNC(int, battery_design_capacity, int *);
+DECLARE_FAKE_VALUE_FUNC(int, battery_full_charge_capacity, int *);
+
 void set_battery_present(enum battery_present bp);
+
+void set_battery_design_voltage(int voltage);
+void set_battery_remaining_capacity(int capacity);
+void set_battery_status(int status);
+void set_battery_design_capacity(int design_capacity);
+void set_battery_full_charge_capacity(int full_charge_capacity);
