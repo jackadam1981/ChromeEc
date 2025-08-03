5b973eededd79bd039003920cfc24c8a38eff71a
lin.arthur <lin.arthur@inventec.corp-partner.google.com>
2025-08-03 17:57:45 +0800

    kaladin: report tablet mode to ISH
    Report tablet mode to ISH.
    Disable tablet function  via FW_CONFIG.
    
    BUG=None
    TEST=zmake build kaladin
    LOW_COVERAGE_REASON=test code is not yet ready b:421825142
    
    Change-Id: I5ea33df90c6b0721f9d032ff21685d7f7a3459f1

--- zephyr/program/trulo/kaladin/src/board.c
+++ zephyr/program/trulo/kaladin/src/board.c
@@ -10,6 +10,7 @@
 #include "keyboard_8042_sharedlib.h"
 #include "keyboard_backlight.h"
 #include "keyboard_config.h"
+#include "tablet_mode.h"
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
@@ -89,3 +90,22 @@ __override uint32_t board_override_feature_flags0(uint32_t flags0)
 	else
 		return flags0;
 }
+
+static void motionsense_init(void)
+{
+	int ret, tablet_fwconfig;
+
+	ret = cros_cbi_get_fw_config(FW_TABLET, &tablet_fwconfig);
+	if (ret < 0) {
+		LOG_ERR("error retriving CBI config: %d", ret);
+		return;
+	}
+
+	if (tablet_fwconfig == FW_TABLET_ABSENT) {
+		gmr_tablet_switch_disable();
+		LOG_INF("Board is Clamshell");
+	} else if (tablet_fwconfig == FW_TABLET_PRESENT) {
+		LOG_INF("Board is Convertible");
+	}
+}
+DECLARE_HOOK(HOOK_INIT, motionsense_init, HOOK_PRIO_DEFAULT);
