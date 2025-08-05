b33349986397cf864319bbaf169f07a7c43b4afb
Smit Patel <smit.patel@7rays.corp-partner.google.com>
2025-08-05 14:51:45 +0530

    Checking Git
    BUG=None
    TEST=None
    
    Change-Id: Ib5b0b58da3dfc4d9b9797ba1a2018084967f907c

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -2976,6 +2976,9 @@ static void tc_attach_wait_src_run(const int port)
 	 * state is detected on both the CC1 and CC2 pins for at least
 	 * tCCDebounce.
 	 */
+
+	// Hello
+
 	if (pd_check_vbus_level(port, VBUS_SAFE0V)) {
 		if (new_cc_state == PD_CC_UFP_ATTACHED) {
 			set_state_tc(port, TC_ATTACHED_SRC);
