bf6a96845a27d8d668a2aca9c27a1a3954993b23
harikrishna <harikrishna.m@7rays.corp-partner.google.com>
2025-08-06 19:21:56 +0530

    DNS: Added PD_T_PD_DEBOUNCE timer
    To check stable CC lines added PD_T_PD_DEBOUNCE time period in
    Unattached.SRC & Unattached.SNK states
    
    Hack for event build.
    
    BUG=b:412564878
    TEST=TD.4.6.6 Try SRC DRP Connect SNKAS
    
    Change-Id: I284b5a1f5d5e99cd1561e8d599bd2d8f9756faff

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -2227,6 +2227,9 @@ static void tc_unattached_snk_entry(const int port)
 {
 	enum pd_data_role prev_data_role;
 
+	/* Initialize CC state tracking */
+	tc[port].cc_state = PD_CC_UNSET;
+
 	if (get_last_state_tc(port) != TC_UNATTACHED_SRC) {
 		tc_detached(port);
 		print_current_state(port);
@@ -2282,6 +2285,8 @@ static void tc_unattached_snk_entry(const int port)
 
 static void tc_unattached_snk_run(const int port)
 {
+	enum pd_cc_states new_cc_state;
+
 	/*
 	 * TODO(b/137498392): Add wait before sampling the CC
 	 * status after role changes
@@ -2300,6 +2305,23 @@ static void tc_unattached_snk_run(const int port)
 	if (!IS_ENABLED(CONFIG_USB_PD_EVENT_DRIVEN_CC_STATE))
 		tcpm_get_cc(port, &tc[port].cc1, &tc[port].cc2);
 
+	/* Determine new CC state */
+	if (cc_is_rp(tc[port].cc1) || cc_is_rp(tc[port].cc2))
+		new_cc_state = PD_CC_DFP_ATTACHED;
+	else
+		new_cc_state = PD_CC_NONE;
+
+	/* If CC state changed, restart debounce timer */
+	if (new_cc_state != tc[port].cc_state) {
+		tc[port].cc_state = new_cc_state;
+		pd_timer_enable(port, TC_TIMER_PD_DEBOUNCE, PD_T_PD_DEBOUNCE);
+		return;
+	}
+
+	/* Wait for debounce timer to expire */
+	if (!pd_timer_is_expired(port, TC_TIMER_PD_DEBOUNCE))
+		return;
+
 	/*
 	 * The port shall transition to AttachWait.SNK when a Source
 	 * connection is detected, as indicated by the SNK.Rp state
@@ -2309,7 +2331,7 @@ static void tc_unattached_snk_run(const int port)
 	 * after the state of both CC pins is SNK.Open for
 	 * tDRP − dcSRC.DRP ∙ tDRP.
 	 */
-	if (cc_is_rp(tc[port].cc1) || cc_is_rp(tc[port].cc2)) {
+	if (new_cc_state == PD_CC_DFP_ATTACHED) {
 		/* Connection Detected */
 		set_state_tc(port, TC_ATTACH_WAIT_SNK);
 		return;
@@ -2351,6 +2373,9 @@ static void tc_unattached_snk_run(const int port)
 static void tc_unattached_snk_exit(const int port)
 {
 	pd_timer_disable(port, TC_TIMER_NEXT_ROLE_SWAP);
+
+	/* Clear debounce timer */
+	pd_timer_disable(port, TC_TIMER_PD_DEBOUNCE);
 }
 
 /**
@@ -2806,6 +2831,9 @@ static void tc_unattached_src_entry(const int port)
 {
 	enum pd_data_role prev_data_role;
 
+	/* Initialize CC state tracking */
+	tc[port].cc_state = PD_CC_UNSET;
+
 	if (get_last_state_tc(port) != TC_UNATTACHED_SNK) {
 		tc_detached(port);
 		print_current_state(port);
@@ -2857,6 +2885,8 @@ static void tc_unattached_src_entry(const int port)
 
 static void tc_unattached_src_run(const int port)
 {
+	enum pd_cc_states new_cc_state;
+
 	if (IS_ENABLED(CONFIG_USB_PE_SM)) {
 		if (TC_CHK_FLAG(port, TC_FLAGS_HARD_RESET_REQUESTED)) {
 			TC_CLR_FLAG(port, TC_FLAGS_HARD_RESET_REQUESTED);
@@ -2879,6 +2909,25 @@ static void tc_unattached_src_run(const int port)
 	if (!IS_ENABLED(CONFIG_USB_PD_EVENT_DRIVEN_CC_STATE))
 		tcpm_get_cc(port, &tc[port].cc1, &tc[port].cc2);
 
+	/* Determine new CC state */
+	if (cc_is_audio_acc(tc[port].cc1, tc[port].cc2))
+		new_cc_state = PD_CC_UFP_AUDIO_ACC;
+	else if (cc_is_at_least_one_rd(tc[port].cc1, tc[port].cc2))
+		new_cc_state = PD_CC_UFP_ATTACHED;
+	else
+		new_cc_state = PD_CC_NONE;
+
+	/* If CC state changed, restart debounce timer */
+	if (new_cc_state != tc[port].cc_state) {
+		tc[port].cc_state = new_cc_state;
+		pd_timer_enable(port, TC_TIMER_PD_DEBOUNCE, PD_T_PD_DEBOUNCE);
+		return;
+	}
+
+	/* Wait for tPDDebounce to transition to AttachWait.SRC
+	if (!pd_timer_is_expired(port, TC_TIMER_PD_DEBOUNCE))
+		return;
+
 	/*
 	 * Transition to AttachWait.SRC when:
 	 *   1) The SRC.Rd state is detected on either CC1 or CC2 pin or
@@ -2886,8 +2935,8 @@ static void tc_unattached_src_run(const int port)
 	 * TODO(b/436338613): TC_UNATTACHED_SRC transition to T_DRP_AUTO_TOGGLE
 	 * with no additional delay.
 	 */
-	if (cc_is_at_least_one_rd(tc[port].cc1, tc[port].cc2) ||
-	    cc_is_audio_acc(tc[port].cc1, tc[port].cc2))
+	if (new_cc_state == PD_CC_UFP_ATTACHED ||
+	    new_cc_state == PD_CC_UFP_AUDIO_ACC)
 		set_state_tc(port, TC_ATTACH_WAIT_SRC);
 	else if (pd_timer_is_expired(port, TC_TIMER_NEXT_ROLE_SWAP) &&
 		 drp_state[port] != PD_DRP_FORCE_SOURCE &&
@@ -2900,10 +2949,9 @@ static void tc_unattached_src_run(const int port)
 	/*
 	 * Attempt TCPC auto DRP toggle
 	 */
-	if (IS_ENABLED(CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE) &&
-	    drp_state[port] == PD_DRP_TOGGLE_ON &&
-	    tcpm_auto_toggle_supported(port) &&
-	    cc_is_open(tc[port].cc1, tc[port].cc2))
+	else if (IS_ENABLED(CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE) &&
+		 drp_state[port] == PD_DRP_TOGGLE_ON &&
+		 tcpm_auto_toggle_supported(port) && new_cc_state == PD_CC_NONE)
 		set_state_tc(port, TC_DRP_AUTO_TOGGLE);
 	else if (IS_ENABLED(CONFIG_USB_PD_TCPC_LOW_POWER) &&
 		 (drp_state[port] == PD_DRP_FORCE_SOURCE ||
@@ -2914,6 +2962,9 @@ static void tc_unattached_src_run(const int port)
 static void tc_unattached_src_exit(const int port)
 {
 	pd_timer_disable(port, TC_TIMER_NEXT_ROLE_SWAP);
+
+	/* Clear debounce timer */
+	pd_timer_disable(port, TC_TIMER_PD_DEBOUNCE);
 }
 
 /**
