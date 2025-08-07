cf7e53911e288e0d6635d31cb72f511b5f4446d6
Jayvik Desai <jayvik@google.com>
2025-08-08 01:14:41 +0530

    bluey: Add support for offmode charging
    This patch adds supports for offmode charging
    
    BUG=none
    TEST=zmake build quenbi
    
    Able to trigger shutdown and trigger offmode charging, tested if we are
    able to reboot once the battery discharges above a threshold and able to
    power on the ap again
    
    console log >
    
    25-08-07 19:52:14.795 ec:> apshutdown_offmode
    :
    19:52:16.240[30.841700 power shutdown complete]
    19:52:16.535[30.842500 I2C_PORT_ADSP switched to controller mode]
    19:52:16.540[30.868600 Offmode-charging, remaining battery charge = 34]
    :
    19:52:16.572[40.881700 Offmode-charging current battery charge = 34]
    19:52:26.499[50.890200 Offmode-charging current battery charge = 34]
    19:52:36.455[60.881500 Offmode-charging current battery charge = 34]
    19:52:46.407[70.881400 Offmode-charging current battery charge = 34]
    19:52:56.350 ec:> battfake 32
    19:52:57.333 Fake batt 32%
    19:52:57.333[80.895300 Offmode-charging current battery charge = 32]
    19:53:06.307[90.881600 Offmode-charging current battery charge = 32]
    19:53:16.245[100.895100 Offmode-charging current battery charge = 32]
    19:53:26.222 ec:> battfake 29
    19:53:34.263 Fake batt 29%
    19:53:34.263[110.881600 Offmode-charging current battery charge = 29]
    19:53:36.152[110.882700 power on 6]
    :
    25-08-07 19:53:36.476[111.242500 POWER_GOOD seen]
    
    Change-Id: I9e65d8770b5eb7f3b75bff2cbff705f686216709

--- zephyr/program/bluey/src/battery.c
+++ zephyr/program/bluey/src/battery.c
@@ -8,14 +8,26 @@
 #include "battery.h"
 #include "battery_fuel_gauge.h"
 #include "charge_state.h"
+#include "chipset.h"
 #include "common.h"
+#include "console.h"
 #include "hooks.h"
+#include "system.h"
 #include "util.h"
 
 #define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ##args)
 
+/* Timeout to check for the remaining battery charge during off-mode charging */
+#define BATTERY_CHECK_TIMEOUT_OFF_MODE_CHARGING K_SECONDS(10)
+
+/* Allowed discharge battery charge percentage before we start charging again */
+#define BATTERY_DISCHARGE_THRESHOLD 4
+
 /* -1, if not yet cached*/
 static int cached_charge_percentage = -1;
+
+/* Indicator to show if the shutdown was due to off-mode charging */
+static int shutdown_for_offmode_charging;
 
 int fetch_battery_charge(void)
 {
@@ -25,6 +37,36 @@ int fetch_battery_charge(void)
 
 	return batt->state_of_charge;
 }
+
+void check_battery_discharge_expired(struct k_work *work)
+{
+	int value;
+
+	value = fetch_battery_charge();
+
+	if (value == -1) {
+		CPRINTS("Invalid charge value");
+		return;
+	}
+
+	CPRINTS("Offmode-charging current battery charge = %d", value);
+	/*
+	 * check if we excede the threshold for power on
+	 */
+	if (cached_charge_percentage - value > BATTERY_DISCHARGE_THRESHOLD) {
+		chipset_power_on();
+	}
+}
+K_WORK_DEFINE(check_battery_discharge_work, check_battery_discharge_expired);
+
+void check_battery_discharge_handler(struct k_timer *timer_id)
+{
+	/* Since we can't read the battery percentage in interrupt context
+	 * we submit some work
+	 */
+	k_work_submit(&check_battery_discharge_work);
+}
+K_TIMER_DEFINE(check_battery_discharge, check_battery_discharge_handler, NULL);
 
 void board_chipset_pre_init(void)
 {
@@ -37,16 +79,40 @@ void board_chipset_pre_init(void)
 	 * battery commands
 	 */
 	CPRINTS("I2C_PORT_ADSP switched to peripheral mode");
+	/*
+	 * Clean-up for offmode charging if enabled.
+	 */
+	k_timer_stop(&check_battery_discharge);
+	shutdown_for_offmode_charging = 0;
 }
 DECLARE_HOOK(HOOK_CHIPSET_PRE_INIT, board_chipset_pre_init, HOOK_PRIO_DEFAULT);
 
 void board_chipset_shutdown_complete(void)
 {
+	int value;
 	/*
 	 * TODO: move i2c from peripheral mode to controller mode and enable
 	 * battery commands
 	 */
 	CPRINTS("I2C_PORT_ADSP switched to controller mode");
+	/*
+	 * if the reason for shutdown is due to off-mode charging
+	 */
+	if (shutdown_for_offmode_charging) {
+		value = fetch_battery_charge();
+		/*
+		 * Cache the battery percentage while we are in off mode
+		 * charging state and start a timer to keep checking battery
+		 * state every BATTERY_CHECK_TIMEOUT_OFF_MODE_CHARGING seconds
+		 */
+		cached_charge_percentage = value;
+
+		CPRINTS("Offmode-charging, remaining battery charge = %d",
+			cached_charge_percentage);
+		k_timer_start(&check_battery_discharge,
+			      BATTERY_CHECK_TIMEOUT_OFF_MODE_CHARGING,
+			      BATTERY_CHECK_TIMEOUT_OFF_MODE_CHARGING);
+	}
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN_COMPLETE, board_chipset_shutdown_complete,
 	     HOOK_PRIO_DEFAULT);
