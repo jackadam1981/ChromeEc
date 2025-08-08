527071376609a93f49fb1bc6c2076fae46598e33
Dawid Niedzwiecki <dawidn@google.com>
2025-08-08 15:54:51 +0200

    WIP: add helper functions
    Change-Id: I5719fa2ab3f2868cbc211515d2588daced63ae6f

--- zephyr/program/fpmcu/et171/src/platform.c
+++ zephyr/program/fpmcu/et171/src/platform.c
@@ -22,30 +22,14 @@
 #include "spi_flash_reg.h"
 #include <stdarg.h>
 #include "console.h"
+#include "system.h"
 #include "util.h"
 
 #define flash_ctrl_dev DEVICE_DT_GET(DT_CHOSEN(zephyr_flash_controller))
 
-static const struct device *const syscon_dev =
-DEVICE_DT_GET(DT_NODELABEL(syscon));
-
-#define AOSMU_BOOT_STRAPPING 0x8 /* Secure key handling */
-#define AOSMU_SECURE_CON 0xc /* Secure key handling */
-#define AOSMU_SECURE_CON_BYPASS_BOOTSTRAP BIT(17) /* issue reset to MCU core */
-#define AOSMU_SECURE_CON_DFU BIT(16) /* issue reset to MCU core */
-#define AOSMU_SECURE_CON_WARM_RESET BIT(2) /* issue reset to whole SoC except for reset_vector(0x10) and dummy(0x1c) */
-#define AOSMU_RESET_VECTOR 0x10
-
-// TODO implement this as Host Command. Add a new HC or new cmd to REBOOT_EC command?
-// TODO consider when we should accept such command. With only singed token? Always, because we use SDCP anyway?
 static int command_bootrom(int argc, const char **argv)
 {
-	uint32_t syscon;
-
-	syscon_read_reg(syscon_dev, AOSMU_SECURE_CON, &syscon);
-	syscon_write_reg(syscon_dev, AOSMU_RESET_VECTOR, 0x70000000);
-	syscon_write_reg(syscon_dev, AOSMU_SECURE_CON, syscon | AOSMU_SECURE_CON_DFU);
-	syscon_write_reg(syscon_dev, AOSMU_SECURE_CON, syscon | AOSMU_SECURE_CON_WARM_RESET | AOSMU_SECURE_CON_DFU);
+	chip_enter_bootloader();
 
 	return 0;
 };
@@ -78,11 +62,11 @@ static int enable_qspi(void)
 }
 SYS_INIT(enable_qspi, POST_KERNEL, 51);
 
-// TODO lack of this causes reboot loop. Debug this? Add to soc code?
-static int fence(void)
-{
-	__asm__ volatile("fence.i" ::: "memory");
+// // TODO lack of this causes reboot loop. Debug this? Add to soc code?
+// static int fence(void)
+// {
+// 	__asm__ volatile("fence.i" ::: "memory");
 
-	return 0;
-}
-SYS_INIT(fence, PRE_KERNEL_2, 51);
+// 	return 0;
+// }
+// SYS_INIT(fence, PRE_KERNEL_2, 51);
