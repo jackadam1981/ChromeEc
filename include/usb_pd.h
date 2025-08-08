9ea2198138bfa91e946ce31ece09d50fcff0ffe0
debasmitha <debasmitha.ghosh@7rays.corp-partner.google.com>
2025-08-08 22:52:51 +0530

    DNS: Tweak the PD2_T_SENDER_RESPONSE value to get a consistent result
    Hack for event build.
    
    BUG=b:433463709
    TEST=COMMON.CHECK.PD.4#1
    
    Change-Id: I56001ab55f327cfa9454d0c3e834acceaf24faa0

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -263,7 +263,7 @@ enum pdo_augmented_pps {
 #define PD_T_SENDER_RESPONSE (30 * MSEC) /* between 24ms and 30ms */
 #else
 /* PD R2.0 V1.3: between 24ms and 30ms */
-#define PD2_T_SENDER_RESPONSE (26 * MSEC)
+#define PD2_T_SENDER_RESPONSE (27 * MSEC)
 /*
  * PD R3.1 V1.5: between 26ms and 32ms
  * PD R3.2 V1.0: between 27ms and 33ms
