6bdcfcb570e5e6096d17d21807aadd9ef8b4da12
Benson Leung <bleung@chromium.org>
2025-08-08 18:40:48 +0000

    platform/ec/include: Change PD_T_RP_VALUE_CHANGE from 20 ms to 15 ms
    tRpValueChange is allowed to be 10 ms to 20 ms. Let's split the difference.
    
    BUG=b:437099482
    TEST=TD 4.10.1 on Teledyne Lecroy tester passes on Bugzzy with this change.
    
    Change-Id: Ief5447e7b21451d3809938a546dc234d798c1336

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -308,7 +308,7 @@ enum pdo_augmented_pps {
 #define PD_T_CHUNK_SENDER_RESPONSE (25 * MSEC) /* 25ms */
 #define PD_T_CHUNK_SENDER_REQUEST (25 * MSEC) /* 25ms */
 #define PD_T_SWAP_SOURCE_START (25 * MSEC) /* Min of 20ms */
-#define PD_T_RP_VALUE_CHANGE (20 * MSEC) /* 20ms */
+#define PD_T_RP_VALUE_CHANGE (15 * MSEC) /* 10 to 20ms */
 #define PD_T_SRC_DISCONNECT (15 * MSEC) /* 15ms */
 #define PD_T_SRC_TRANSITION (25 * MSEC) /* 25ms to 35 ms */
 #define PD_T_VCONN_STABLE (50 * MSEC) /* 50ms */
