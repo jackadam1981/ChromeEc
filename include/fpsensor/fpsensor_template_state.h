478060baff9acc3f5a3d804522c229836ac92e76
Patryk Duda <patrykd@google.com>
2025-08-11 20:00:07 +0200

    fpsensor: Remove template unlock mechanism
    According to go/eliminate-fpmcu-preload the template lock/unlock
    mechanism is used to make sure that the rate limiter in GSC is not
    bypassed. The match can be performed only once in a nonce context.
    To perform another match, we need to get nonce context again from GSC
    and unlock templates (extra step to make sure that set of templates
    is associated with nonce context),
    
    In the new Fingerprint Auth approach the Trusted Application will send
    match request signed with the session key and challenge that must be
    signed by the FPMCU when the match is successful. The new approach does
    not require reloading nonce context (which will be eventually
    deprecated), so template locking/unlocking is not necessary.
    
    BUG=b:392915518
    TEST=make run-fpsensor_auth_commands
    
    Change-Id: I313104f8ec8bb4871af1b0d76dda31a2df697b49

--- include/fpsensor/fpsensor_template_state.h
+++ include/fpsensor/fpsensor_template_state.h
@@ -16,13 +16,6 @@
 #include <array>
 #include <variant>
 
-/* The extra information for the encrypted template.
- * Note: the encrypted template content and encrypted positive match salt will
- * be stored in the fp_template[] and fp_positive_match_salt[] */
-struct fp_encrypted_template_state {
-	ec_fp_template_encryption_metadata enc_metadata;
-};
-
 /* The extra information for the decrypted template. */
 struct fp_decrypted_template_state {
 	/* The user_id that will be used to check the unlock template request is
@@ -33,7 +26,6 @@ struct fp_decrypted_template_state {
 /* The template can only be one of these states.
  * Note: std::monostate means there is nothing in this template. */
 using fp_template_state =
-	std::variant<std::monostate, fp_encrypted_template_state,
-		     fp_decrypted_template_state>;
+	std::variant<std::monostate, fp_decrypted_template_state>;
 
 #endif /* __CROS_EC_FPSENSOR_TEMPLATE_STATE_H */
