df247edba5be7d4ebba47f6fdae4d02dd9508355
Yuval Peress <peress@google.com>
2025-08-12 15:49:43 -0700

    on-body: Allow dynamic on-body sensor ID
    Add a new Kconfig to enable dynamic on-body-sensor. When used, the
    board is responsible for selecting the right index for the on-body
    sensor. When enabled, we no longer allow the on-body-sensor alias in
    devicetree to avoid confusion.
    
    BUG=b:435407060
    TEST=Check on uldrenite: the sensor is presented as the first one.
         And activity sensor is the second sensor.
    
    Change-Id: Ia954bac136d5b7ba7a88545e9e17bf480083b46c

--- test/test_config.h
+++ test/test_config.h
@@ -221,10 +221,11 @@ enum battery_type {
 #define CONFIG_TEMP_CACHE_STALE_THRES (1 * SECOND)
 #endif /* CONFIG_ONLINE_CALIB && !CONFIG_TEMP_CACHE_STALE_THRES */
 
-#if defined(CONFIG_ONLINE_CALIB) || defined(TEST_BODY_DETECTION) ||        \
-	defined(TEST_MOTION_ANGLE) || defined(TEST_MOTION_ANGLE_TABLET) || \
-	defined(TEST_MOTION_LID) || defined(TEST_MOTION_SENSE_FIFO) ||     \
-	defined(TEST_TABLET_BROKEN_SENSOR)
+#if defined(TEST_ONLINE_CALIBRATION) ||                                  \
+	defined(TEST_ONLINE_CALIBRATION_SPOOF) ||                        \
+	defined(TEST_BODY_DETECTION) || defined(TEST_MOTION_ANGLE) ||    \
+	defined(TEST_MOTION_ANGLE_TABLET) || defined(TEST_MOTION_LID) || \
+	defined(TEST_MOTION_SENSE_FIFO) || defined(TEST_TABLET_BROKEN_SENSOR)
 enum sensor_id {
 	BASE,
 	LID,
