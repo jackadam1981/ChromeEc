3bcc302e74008bd83db416884699421cdcd82a9c
Caiyi Zhong <caiyi_zhong@realtek.corp-partner.google.com>
2025-08-14 17:29:12 +0800

    realtekfp: Add new program for RTS5817 fpmcu
    Add basic support for RTS5817, which is a fingerprint-specific MCU from
    Realtek Semiconductor.
    
    BUG=b:422616702
    TEST=zmake -D build realtekfp
    
    Change-Id: I4f7d902cfc65e449e12ce5e0b8c20b7cb1a8ed07
    Signed-off-by: Caiyi Zhong <caiyi_zhong@realtek.corp-partner.google.com>

---
+++ zephyr/shim/chip/rtkfp/include/flash_chip.h
@@ -0,0 +1,26 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_FLASH_CHIP_H
+#define __CROS_EC_FLASH_CHIP_H
+
+#define CONFIG_SPI_FLASH_GD25Q16
+
+/*
+ * One page program instruction allows maximum 256 bytes (a page) of data
+ * to be programmed.
+ */
+#define CONFIG_FLASH_WRITE_IDEAL_SIZE 256
+/* Minimum write size */
+#define CONFIG_FLASH_WRITE_SIZE 1
+/* Erase sector size */
+#define CONFIG_FLASH_ERASE_SIZE 0x1000
+/* Protect bank size, set by SPI_FLASH_SR1_SEC (0 = 64 KB) */
+#define CONFIG_FLASH_BANK_SIZE 0x10000
+
+#define CONFIG_RO_STORAGE_OFF 0x20
+#define CONFIG_RW_STORAGE_OFF 0x0
+
+#endif /* __CROS_EC_FLASH_CHIP_H */
