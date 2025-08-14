469105f0ee138a18d13836558153326805de266b
Caiyi Zhong <caiyi_zhong@realtek.corp-partner.google.com>
2025-08-14 17:29:12 +0800

    realtekfp: Add new program for RTS5817 fpmcu
    Add basic support for RTS5817, which is a fingerprint-specific MCU from
    Realtek Semiconductor.
    
    BUG=b:422616702
    TEST=zmake -D build realtekfp
    
    Change-Id: I4f7d902cfc65e449e12ce5e0b8c20b7cb1a8ed07
    Signed-off-by: Caiyi Zhong <caiyi_zhong@realtek.corp-partner.google.com>

---
+++ zephyr/drivers/cros_system/cros_system_rtkfp.c
@@ -0,0 +1,87 @@
+/* Copyright 2026 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "common.h"
+#include "cros_version.h"
+#include "drivers/cros_system.h"
+#include "stdint.h"
+#include "system.h"
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/bbram.h>
+#include <zephyr/drivers/watchdog.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/poweroff.h>
+#include <zephyr/sys/reboot.h>
+#include <zephyr/sys/util.h>
+
+LOG_MODULE_REGISTER(cros_system, LOG_LEVEL_ERR);
+
+/* Driver data */
+struct cros_system_rtkfp_data {
+	int reset; /* reset cause */
+};
+
+static const char *cros_system_rtkfp_get_chip_vendor(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	return "rtk";
+}
+
+static const char *cros_system_rtkfp_get_chip_name(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	return NULL;
+}
+
+static const char *cros_system_rtkfp_get_chip_revision(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	return NULL;
+}
+
+static int cros_system_rtkfp_get_reset_cause(const struct device *dev)
+{
+	return 0;
+}
+
+static int cros_system_rtkfp_init(const struct device *dev)
+{
+	return 0;
+}
+
+static int cros_system_rtkfp_soc_reset(const struct device *dev)
+{
+	return 0;
+}
+
+static int cros_system_rtkfp_hibernate(const struct device *dev, uint32_t seconds,
+				     uint32_t microseconds)
+{
+	return 0;
+}
+
+static const struct cros_system_driver_api cros_system_driver_rtkfp_api = {
+	.get_reset_cause = cros_system_rtkfp_get_reset_cause,
+	.soc_reset = cros_system_rtkfp_soc_reset,
+	.hibernate = cros_system_rtkfp_hibernate,
+	.chip_vendor = cros_system_rtkfp_get_chip_vendor,
+	.chip_name = cros_system_rtkfp_get_chip_name,
+	.chip_revision = cros_system_rtkfp_get_chip_revision,
+};
+#if CONFIG_CROS_SYSTEM_REALTEK_INIT_PRIORITY >= \
+	CONFIG_PLATFORM_EC_SYSTEM_PRE_INIT_PRIORITY
+#error "CROS_SYSTEM must initialize before the SYSTEM_PRE initialization"
+#endif
+
+static struct cros_system_rtkfp_data cros_system_rtkfp_data_0;
+
+DEVICE_DEFINE(cros_system_rtkfp_0, "CROS_SYSTEM", cros_system_rtkfp_init, NULL,
+	      &cros_system_rtkfp_data_0, NULL, PRE_KERNEL_1,
+	      CONFIG_CROS_SYSTEM_REALTEKFP_INIT_PRIORITY,
+	      &cros_system_driver_rtkfp_api);
