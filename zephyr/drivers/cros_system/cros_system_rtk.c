027d3c8889af0a53697c261c8b59b5871fe37176
Jason Lin <lin_yu_cheng@realtek.corp-partner.google.com>
2025-08-15 15:03:51 +0800

    RTS5915: Enable common sleep function
    In this CL, add the common part of the sleep function for RTS5915.
    
    BUG=b:417156335
    TEST=Run FAFT, SUIT and aging at local.
    
    Change-Id: I2c800a774e70df21fa319bcd2ab44020a2f6fd39

--- zephyr/drivers/cros_system/cros_system_rtk.c
+++ zephyr/drivers/cros_system/cros_system_rtk.c
@@ -68,12 +68,33 @@ static const char *cros_system_rtk_get_chip_vendor(const struct device *dev)
 
 #define RTK_CHIP_INFO_BASE 0x40010B80
 #define CHIP_ID_OFFSET 0x70
+#define RTK_CHIP_INFO_REG (RTK_CHIP_INFO_BASE + CHIP_ID_OFFSET)
+#define RTK_PUF_INFO_BASE 0x40010800UL
+#define OTP_OFFSET_BASE 0x680UL
+#define OTP_CTRL_REGISTER 0x24
+#define OTP_STS_REGISTER 0x20
+#define OTP_STS_BUSY_MSK 0x01
+#define OTP_STS_PDSTB_MSK 0x04
+#define RTK_OTP_CTRL_REG \
+	(RTK_PUF_INFO_BASE + OTP_OFFSET_BASE + OTP_CTRL_REGISTER)
+#define RTK_OTP_STS_REG (RTK_PUF_INFO_BASE + OTP_OFFSET_BASE + OTP_STS_REGISTER)
+static uint32_t get_otp_chip_info(void)
+{
+	*(volatile uint32_t *)RTK_OTP_CTRL_REG = 1ul;
+	while (OTP_STS_PDSTB_MSK != (*(volatile uint32_t *)RTK_OTP_STS_REG &
+				     (OTP_STS_BUSY_MSK | OTP_STS_PDSTB_MSK)))
+		;
+	uint32_t chip_info = *((volatile uint32_t *)RTK_CHIP_INFO_REG);
+
+	*(volatile uint32_t *)RTK_OTP_CTRL_REG = 0ul;
+
+	return chip_info;
+}
+
 static uint32_t system_get_chip_id(void)
 {
 	/* [31:16] main id */
-	volatile uint32_t *chip_info_address =
-		(volatile uint32_t *)(RTK_CHIP_INFO_BASE + CHIP_ID_OFFSET);
-	uint32_t raw_id = *chip_info_address;
+	uint32_t raw_id = get_otp_chip_info();
 	uint16_t main_id = (raw_id >> 16) & 0xFFFF;
 
 	return main_id;
@@ -82,9 +103,7 @@ static uint32_t system_get_chip_id(void)
 static uint8_t system_get_chip_version(void)
 {
 	/* [15:8] chip version */
-	volatile uint32_t *chip_info_address =
-		(volatile uint32_t *)(RTK_CHIP_INFO_BASE + CHIP_ID_OFFSET);
-	uint32_t raw_id = *chip_info_address;
+	uint32_t raw_id = get_otp_chip_info();
 	uint16_t sub_id = (raw_id >> 8) & 0xFF;
 
 	return sub_id;
