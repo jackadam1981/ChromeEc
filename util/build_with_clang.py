eb48776d54ab72b0ed809eb0f39655e39a3e11be
Jeremy Bettis <jbettis@google.com>
2025-08-18 15:49:38 -0600

    Remove unused board adl_ish_lite
    BUG=b:439048622
    TEST=firmware_builder.py --metrics /tmp/metrics check_inherits
    
    Change-Id: Ic0744d6d310d2ab342d8b25c521427cb1c5c2add

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -203,7 +203,6 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "corsola_scp",
     "kukui_scp",
     # Boards that use CORE:=minute-ia
-    "adl_ish_lite",
     "arcada_ish",
     "drallion_ish",
 ]
