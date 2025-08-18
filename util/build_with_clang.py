4fe949d920126c03fadad0ea80c45ba2dfc82d1a
Jeremy Bettis <jbettis@google.com>
2025-08-18 16:59:44 -0600

    Remove unused board twinkie
    This is the firmware for the v1 device discontinued by Total Phase.
    
    BUG=b:439048622
    TEST=firmware_builder.py --metrics /tmp/metrics check_inherits
    
    Change-Id: Ie8f57c2da55af9406a4eff814f0820274604f165

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -67,8 +67,6 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "servo_v4p1",
     "staff",
     "star",
-    "twinkie",
-    "whitebeard",
     "zed",
     "zinger",
     # Boards that use CHIP:=mchp
