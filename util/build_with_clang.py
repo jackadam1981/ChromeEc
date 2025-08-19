8b776ca3c36510f12bffc7c0395ba28832dc6935
Jeremy Bettis <jbettis@google.com>
2025-08-19 14:30:56 -0600

    Remove unused board rosalia
    BUG=b:439048622
    TEST=firmware_builder.py --metrics /tmp/metrics check_inherits
    
    Change-Id: I0c65d193d1dc5f04a331c3b6d6284e7336bee2d6

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -27,7 +27,6 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "helipilot",
     "nami_fp",
     "nucleo-dartmonkey",
-    "rosalia",
     # Boards that use CHIP:=stm32 and *not* CHIP_FAMILY:=stm32f0
     # git grep  --name-only 'CHIP:=stm32' | xargs grep -L \
     #   'CHIP_FAMILY:=stm32f0' | sed 's#board/\(.*\)/build.mk#"\1",#'
