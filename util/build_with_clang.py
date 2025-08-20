7d590871c5b29ab1b94abd9c8325d401222709bd
Jeremy Bettis <jbettis@google.com>
2025-08-20 10:25:57 -0600

    Remove remaining ECOS dev boards
    BUG=b:439048622
    TEST=firmware_builder.py --metrics /tmp/metrics check_inherits
    
    Change-Id: Ic06ceb715e50bf226b7defe4b7f17edebaf90d87

--- util/build_with_clang.py
+++ util/build_with_clang.py
@@ -69,10 +69,6 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     # Boards that use CHIP:=mchp
     # git grep --name-only 'CHIP:=mchp' | sed 's#board/\(.*\)/build.mk#"\1",#'
     "adlrvpp_mchp1727",
-    "mchpevb1",
-    # Boards that use CHIP:=max32660
-    # git grep --name-only 'CHIP:=max32660' | sed 's#board/\(.*\)/build.mk#"\1",#'
-    "max32660-eval",
     # Boards that use CHIP:=npcx
     # git grep --name-only 'CHIP:=npcx' | sed 's#^board/\(.*\)/build.mk#"\1",#'
     "akemi",
@@ -159,8 +155,6 @@ BOARDS_THAT_COMPILE_SUCCESSFULLY_WITH_CLANG = [
     "nipperkin",
     "nocturne",
     "nova",
-    "npcx9_evb",
-    "npcx_evb",
     "nuwani",
     "omnigul",
     "osiris",
@@ -222,7 +216,6 @@ NDS32_BOARDS = [
     "galtic",
     "gooey",
     "haboki",
-    "it83xx_evb",
     "kracko",
     "lantis",
     "pirika",
@@ -245,8 +238,6 @@ RISCV_BOARDS = [
     "geralt_scp_core1",
     "hayato",
     "icarus",
-    "it8xxx2_evb",
-    "it8xxx2_pdevb",
     "pico",
     "spherion",
     "tomato",
