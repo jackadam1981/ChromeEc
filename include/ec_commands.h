8a212bfd28a8208d8b776c4b4c4f1b67cc3b9ebb
Jayvik Desai <jayvik@google.com>
2025-08-22 00:27:09 +0530

    chipset: Add a host command to issue AP shutdown
    The host command is enabled by defining CONFIG_HOSTCMD_AP_SHUTDOWN.
    It calls the chipset_force_shutdown() function, similar to the
    console command "apshutdown".
    
    BUG=b:439820815
    TEST=zmake build quenbi && able to shutdown the AP firmware by issuing
    this host command
    
    Change-Id: I3c2104a493d08fe70bdb3388eb1ccbd1f1f846bf

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -8922,6 +8922,7 @@ struct ec_response_get_boot_time {
 	uint16_t cnt;
 } __ec_align4;
 
+#define EC_CMD_AP_SHUTDOWN 0x0605
 /*****************************************************************************/
 /*
  * Reserve a range of host commands for board-specific, experimental, or
