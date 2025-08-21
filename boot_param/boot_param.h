5a48a69cfc4fbde7236a2ac6033181f086f0bbcb
Andrey Pronin <apronin@google.com>
2025-08-21 13:29:03 -0700

    boot_param: add parametrized DICE chains
    This CL adds
    - DICE CDI parametrization on chain id, so that multiple
      parallel chains can be created.
    - Method for signing digest with a (parametrized) CDI_attest key.
    
    BUG=b:440393848
    TEST=1) boot_param_test passes
         2) pre-change CDIs for AP are equal to post-change CDIs
         3) test with the debug CLs: https://crrev.com/i/8538024 and
            https://crrev.com/c/6882678. boot_param_test also passes
            for the two debug nvmem spaces and CDIs are different
            between AP and GSC.
    
    Change-Id: Ibf8b290131657664b89248e2d68c05540564c2d5

--- boot_param/boot_param.h
+++ boot_param/boot_param.h
@@ -29,29 +29,70 @@ extern "C" {
 #define BOOT_PARAM_SIZE \
 	(BOOT_PARAM_SIZE_STAGE1 + BOOT_PARAM_CFG_DESCR_EXTRA_STAGE_SIZE)
 
-/* Get (part of) BootParam structure: [offset .. offset + size) */
-size_t get_boot_param_bytes(
+/*
+ * 8-bit ID of the DICE chain:
+ * 0 - GSC
+ * 1 - AP
+ * 2 - HWDRM (not used)
+ */
+#define BOOT_PARAM_DICE_CHAIN_GSC       0
+#define BOOT_PARAM_DICE_CHAIN_AP        1
+#define BOOT_PARAM_DICE_CHAIN_HWDRM     2
+
+/* Get (part of) BootParam structure for the specific chain:
+ * [offset .. offset + size).
+ */
+size_t get_boot_param_bytes_for_chain(
 	/* [OUT] destination buffer to fill */
 	uint8_t *dest,
 	/* [IN] starting offset in the BootParam struct */
 	size_t offset,
-	/* [IN] size of the data to copy */
-	size_t size
+	/* [IN] size of the BootParam struct to copy */
+	size_t size,
+	/* [IN] chain ID */
+	uint8_t chain_id
 );
+
+/* Get (part of) BootParam structure for the main chain:
+ * [offset .. offset + size).
+ */
+#define get_boot_param_bytes(d, o, s) \
+	get_boot_param_bytes_for_chain(d, o, s, BOOT_PARAM_DICE_CHAIN_AP)
 
 /* Size of DiceChain structure in bytes */
 #define DICE_CHAIN_SIZE_STAGE1 605
 #define DICE_CHAIN_SIZE \
 	(DICE_CHAIN_SIZE_STAGE1 + BOOT_PARAM_CFG_DESCR_EXTRA_STAGE_SIZE)
 
-/* Get (part of) DiceChain structure: [offset .. offset + size) */
-size_t get_dice_chain_bytes(
+/* Get (part of) DiceChain structure for the specific chain:
+ * [offset .. offset + size).
+ */
+size_t get_dice_chain_bytes_for_chain(
 	/* [OUT] destination buffer to fill */
 	uint8_t *dest,
 	/* [IN] starting offset in the DiceChain struct */
 	size_t offset,
 	/* [IN] size of the data to copy */
-	size_t size
+	size_t size,
+	/* [IN] chain ID */
+	uint8_t chain_id
+);
+
+/* Get (part of) DiceChain structure for the main chain:
+ * [offset .. offset + size).
+ */
+#define get_dice_chain_bytes(d, o, s) \
+	get_dice_chain_bytes_for_chain(d, o, s, BOOT_PARAM_DICE_CHAIN_AP)
+
+/* Sign data with attestation CDI key for the specific chain.
+ */
+bool sign_with_cdi_key(
+	/* [IN] chain ID */
+	uint8_t chain_id,
+	/* [IN] data to sign */
+	const struct slice_ref_s data_to_sign,
+	/* [OUT] resulting signature */
+	uint8_t signature[ECDSA_SIG_BYTES]
 );
 
 #ifdef __cplusplus
