d0c3ea13eb62c07a1ce4802b8bf479c44a0d3cee
Jayvik Desai <jayvik@google.com>
2025-08-22 00:27:09 +0530

    chipset: Add a host command to issue AP shutdown
    The host command EC_CMD_AP_SHUTDOWN calls the chipset_force_shutdown()
    function, similar to the console command "apshutdown".
    
    This command provides the ability to the AP to issue a shutdown on
    itself.
    
    BUG=b:439820815
    TEST=zmake build --all && able to shutdown the AP by issuing this host
    command in quenbi
    
    Change-Id: I3c2104a493d08fe70bdb3388eb1ccbd1f1f846bf

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -6787,6 +6787,8 @@ enum chipset_shutdown_reason {
 	CHIPSET_SHUTDOWN_THERMAL,
 	/* Force a chipset shutdown from the power button through EC */
 	CHIPSET_SHUTDOWN_BUTTON,
+	/* Force a chipset shutdown, because the AP wants to. */
+	CHIPSET_SHUTDOWN_HOST_CMD,
 
 	CHIPSET_SHUTDOWN_COUNT, /* End of shutdown reasons. */
 };
@@ -8883,6 +8885,7 @@ struct ec_response_get_boot_time {
 	uint16_t cnt;
 } __ec_align4;
 
+#define EC_CMD_AP_SHUTDOWN 0x0605
 /*****************************************************************************/
 /*
  * Reserve a range of host commands for board-specific, experimental, or
