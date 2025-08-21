86e5be779a79537036306ec721a9f73755318a0f
Firas Sammoura <fsammoura@google.com>
2025-08-21 22:18:28 +0000

    drivers: fingerprint: Enable multi-config support in devicetree
    Update macros to be index-based, using DT_PROP_BY_IDX. This allows
    defining multiple sensor configurations (resolution, format, type, size)
    in the devicetree for a single fingerprint sensor.
    
    - Macros like FINGERPRINT_SENSOR_RES_X now take an index.
    - Added FINGERPRINT_SENSOR_CAPTURE_TYPE macro.
    - Image size now read from new 'frame_size' devicetree property.
    - Added NUM_CAPTURE_TYPES to get the count of configurations.
    
    BUG=b:398899644
    TEST=CQ
    
    Change-Id: I7d7605cdfb48c5e7b81d27d3eceb576d814207fb

--- zephyr/include/drivers/fingerprint.h
+++ zephyr/include/drivers/fingerprint.h
@@ -25,24 +25,108 @@
 extern "C" {
 #endif
 
-/** Get fingerprint sensor width. */
-#define FINGERPRINT_SENSOR_RES_X(node_id) DT_PROP(node_id, width)
+/**
+ * @brief Get fingerprint sensor width for a given configuration index.
+ *
+ * @param idx Index of the configuration to retrieve the width from.
+ * @param node_id Devicetree node identifier for the sensor.
+ * @return Sensor width.
+ */
+#define FINGERPRINT_SENSOR_RES_X(idx, node_id) \
+	DT_PROP_BY_IDX(node_id, width, idx)
 
-/** Get fingerprint sensor height. */
-#define FINGERPRINT_SENSOR_RES_Y(node_id) DT_PROP(node_id, height)
+/**
+ * @brief Get fingerprint sensor height for a given configuration index.
+ *
+ * @param idx Index of the configuration to retrieve the height from.
+ * @param node_id Devicetree node identifier for the sensor.
+ * @return Sensor height.
+ */
+#define FINGERPRINT_SENSOR_RES_Y(idx, node_id) \
+	DT_PROP_BY_IDX(node_id, height, idx)
 
-/** Get fingerprint sensor resolution (bits per pixel). */
-#define FINGERPRINT_SENSOR_RES_BPP(node_id) DT_PROP(node_id, bits_per_pixel)
+/**
+ * @brief Get fingerprint sensor resolution (bits per pixel) for a given
+ * configuration index.
+ *
+ * @param idx Index of the configuration to retrieve the bits per pixel from.
+ * @param node_id Devicetree node identifier for the sensor.
+ * @return Sensor bits per pixel.
+ */
+#define FINGERPRINT_SENSOR_RES_BPP(idx, node_id) \
+	DT_PROP_BY_IDX(node_id, bits_per_pixel, idx)
 
-/** Get fingerprint sensor pixel format. */
-#define FINGERPRINT_SENSOR_V4L2_PIXEL_FORMAT(node_id) \
-	DT_STRING_TOKEN(node_id, v4l2_pixel_format)
+/**
+ * @brief Get fingerprint sensor capture type for a given configuration index.
+ *
+ * @param idx Index of the configuration to retrieve the capture type from.
+ * @param node_id Devicetree node identifier for the sensor.
+ * @return Sensor capture type (enum fp_capture_type).
+ */
+#define FINGERPRINT_SENSOR_CAPTURE_TYPE(idx, node_id) \
+	DT_PROP_BY_IDX(node_id, capture_type, idx)
+
+/**
+ * @brief Get fingerprint sensor pixel format for a given configuration index.
+ *
+ * @param idx Index of the configuration to retrieve the pixel format from.
+ * @param node_id Devicetree node identifier for the sensor.
+ * @return Sensor V4L2 pixel format token.
+ */
+#define FINGERPRINT_SENSOR_V4L2_PIXEL_FORMAT(idx, node_id) \
+	DT_STRING_TOKEN_BY_IDX(node_id, v4l2_pixel_format, idx)
+
+/**
+ * @brief Get size of raw fingerprint frame size (in bytes) for a given
+ * configuration index.
+ *
+ * This value is read from the 'frame_size' property in the Device Tree.
+ *
+ * @param idx Index of the configuration to retrieve the frame size from.
+ * @param node_id Devicetree node identifier for the sensor.
+ * @return Frame size in bytes.
+ */
+#define FINGERPRINT_SENSOR_FRAME_SIZE(idx, node_id) \
+	DT_PROP_BY_IDX(node_id, frame_size, idx)
+
+/**
+ * @brief Get the number of capture configurations defined for a fingerprint
+ * sensor node.
+ *
+ * This macro retrieves the number of capture configurations defined for a
+ * fingerprint sensor node from the specified Device Tree node. It assumes that
+ * all related array properties (e.g., height, bits_per_pixel, frame_size,
+ * capture_type) for the different sensor configurations have the same length as
+ * the 'width' array.
+ *
+ * @param node_id Devicetree node identifier for the sensor.
+ * @return The number of distinct capture configurations available for the
+ * sensor.
+ */
+#define FINGERPRINT_SENSOR_NUM_CONFIGS(node_id) DT_PROP_LEN(node_id, width)
+
+/**
+ * @brief Get the number of image capture configurations for the system's
+ * primary fingerprint sensor.
+ *
+ * This macro utilizes FINGERPRINT_SENSOR_NUM_CONFIGS to determine the
+ * number of different image capture setups (e.g., resolutions, formats)
+ * available for the fingerprint sensor instance designated by the
+ * 'cros_fp_fingerprint_sensor' chosen node in the Device Tree. This
+ * effectively counts the elements in the configuration arrays (like 'width',
+ * 'height', etc.) for the selected sensor.
+ *
+ * @return The number of image capture types supported by the chosen
+ * fingerprint sensor.
+ */
+#define NUM_IMAGE_CAPTURE_TYPES \
+	FINGERPRINT_SENSOR_NUM_CONFIGS(DT_CHOSEN(cros_fp_fingerprint_sensor))
 
 /** Get size of raw fingerprint image (in bytes). */
 #define FINGERPRINT_SENSOR_REAL_IMAGE_SIZE(node_id) \
-	((FINGERPRINT_SENSOR_RES_X(node_id) *       \
-	  FINGERPRINT_SENSOR_RES_Y(node_id) *       \
-	  FINGERPRINT_SENSOR_RES_BPP(node_id)) /    \
+	((FINGERPRINT_SENSOR_RES_X(0, node_id) *    \
+	  FINGERPRINT_SENSOR_RES_Y(0, node_id) *    \
+	  FINGERPRINT_SENSOR_RES_BPP(0, node_id)) / \
 	 8)
 
 /** Dead pixels bitmask. */
