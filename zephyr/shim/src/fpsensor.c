36e2af08c9e9989058ce3ce73a8582f00c27bc19
Firas Sammoura <fsammoura@google.com>
2025-08-22 17:55:17 +0000

    zephyr: shim: Update fp_sensor_get_info for per-capture type params
    Modify `fp_sensor_get_info` to use `ec_response_fp_info_v2`, enabling it
    to return distinct image frame parameters (BPP, width, height, etc.) for
    each fingerprint sensor capture type.
    
    This change fetches base sensor info and an array of frame parameters
    from the driver, copying them into the new response structure. Includes
    response buffer size validation. The `fp_sensor_get_info_v2` stub is
    removed as this functionality is now merged.
    
    BUG=b:398899644
    TEST=CQ
    
    Change-Id: I938048dcbb7230e8615a0f5d06e7369f6ce80578

--- zephyr/shim/src/fpsensor.c
+++ zephyr/shim/src/fpsensor.c
@@ -96,33 +96,55 @@ int fp_sensor_deinit(void)
 	return 0;
 }
 
-int fp_sensor_get_info(struct ec_response_fp_info *resp)
+int fp_sensor_get_info(struct ec_response_fp_info_v2 *resp, size_t resp_size)
 {
-	struct fingerprint_info info;
+	if (resp == NULL) {
+		return -EINVAL;
+	}
+
+	const size_t expected_min_size =
+		sizeof(struct ec_response_fp_info_v2) +
+		NUM_IMAGE_CAPTURE_TYPES *
+			sizeof(struct fingerprint_image_frame_params);
+
+	if (resp_size < expected_min_size) {
+		;
+		return -EOVERFLOW;
+	}
+
+	struct fingerprint_sensor_info sensor_info;
+	struct fingerprint_image_frame_params
+		image_frame_params_array[NUM_IMAGE_CAPTURE_TYPES];
 	int rc;
 
-	rc = fingerprint_get_info(fp_sensor_dev, &info);
+	// Clear the buffers to avoid potential info leaks if
+	// fingerprint_get_info doesn't fill all fields.
+	memset(&sensor_info, 0, sizeof(sensor_info));
+	memset(image_frame_params_array, 0, sizeof(image_frame_params_array));
+
+	rc = fingerprint_get_info(fp_sensor_dev, &sensor_info,
+				  image_frame_params_array);
 	if (rc) {
 		return rc;
 	}
 
-	resp->vendor_id = info.vendor_id;
-	resp->product_id = info.product_id;
-	resp->model_id = info.model_id;
-	resp->version = info.version;
-	resp->frame_size = info.frame_size;
-	resp->pixel_format = info.pixel_format;
-	resp->width = info.width;
-	resp->height = info.height;
-	resp->bpp = info.bpp;
-	resp->errors = info.errors;
+	if (sensor_info.num_capture_types < 0 ||
+	    sensor_info.num_capture_types > NUM_IMAGE_CAPTURE_TYPES) {
+		return -EIO;
+	}
 
-	return 0;
-}
+	/* Copy sensor info */
+	memcpy(&resp->sensor_info, &sensor_info,
+	       sizeof(struct fingerprint_sensor_info));
 
-/* TODO(b/398899644): implement fp_sensor_get_info_v2 in zephyr */
-int fp_sensor_get_info_v2(struct ec_response_fp_info_v2 *resp, size_t resp_size)
-{
+	/* Copy image frame parameters. */
+	size_t copy_size = (size_t)sensor_info.num_capture_types *
+			   sizeof(struct fingerprint_image_frame_params);
+	if (copy_size > 0) {
+		memcpy(&resp->image_frame_params, &image_frame_params_array,
+		       copy_size);
+	}
+
 	return 0;
 }
 
