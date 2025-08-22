b0f35b188e42569baa8f59078ed8a21b28d594d1
Firas Sammoura <fsammoura@google.com>
2025-08-22 17:14:00 +0000

    fpsensor: Align fp_sensor_get_info to fpinfo v2
    BUG=b:398899644
    TEST=make buildall -j10
    
    Change-Id: Ic07346b37cbeeeadb819cf1f643bd226d14ca9cb

--- util/ectool.cc
+++ util/ectool.cc
@@ -2080,44 +2080,6 @@ int cmd_fp_stats(int argc, char *argv[])
 
 int cmd_fp_info(int argc, char *argv[])
 {
-	struct ec_response_fp_info r;
-	int rv;
-	int cmdver = ec_cmd_version_supported(EC_CMD_FP_INFO, 1) ? 1 : 0;
-	int rsize = cmdver == 1 ? sizeof(r) :
-				  sizeof(struct ec_response_fp_info_v0);
-	uint16_t dead;
-
-	rv = ec_command(EC_CMD_FP_INFO, cmdver, NULL, 0, &r, rsize);
-	if (rv < 0)
-		return rv;
-
-	printf("Fingerprint sensor: vendor %x product %x model %x version %x\n",
-	       r.vendor_id, r.product_id, r.model_id, r.version);
-	printf("Image: size %dx%d %d bpp\n", r.width, r.height, r.bpp);
-	printf("Error flags: %s%s%s%s\n",
-	       r.errors & FP_ERROR_NO_IRQ ? "NO_IRQ " : "",
-	       r.errors & FP_ERROR_SPI_COMM ? "SPI_COMM " : "",
-	       r.errors & FP_ERROR_BAD_HWID ? "BAD_HWID " : "",
-	       r.errors & FP_ERROR_INIT_FAIL ? "INIT_FAIL " : "");
-	dead = FP_ERROR_DEAD_PIXELS(r.errors);
-	if (dead == FP_ERROR_DEAD_PIXELS_UNKNOWN) {
-		printf("Dead pixels: UNKNOWN\n");
-	} else {
-		printf("Dead pixels: %u\n", dead);
-	}
-
-	if (cmdver == 1) {
-		printf("Templates: version %d size %d count %d/%d"
-		       " dirty bitmap %x\n",
-		       r.template_version, r.template_size, r.template_valid,
-		       r.template_max, r.template_dirty);
-	}
-
-	return 0;
-}
-
-int cmd_fp_info_v2(int argc, char *argv[])
-{
 	struct ec_response_fp_info_v2 *r;
 	int rv;
 	uint16_t dead;
@@ -12791,8 +12753,6 @@ const struct command commands[] = {
 	{ "fpframe", cmd_fp_frame,
 	  "\n\tRetrieve the finger image as a PGM image." },
 	{ "fpinfo", cmd_fp_info,
-	  "\n\tPrints information about the Fingerprint sensor." },
-	{ "fpinfo1", cmd_fp_info_v2,
 	  "\n\tPrints information about the Fingerprint sensor." },
 	{ "fpmode", cmd_fp_mode,
 	  "[mode... [capture_type]]\n"
