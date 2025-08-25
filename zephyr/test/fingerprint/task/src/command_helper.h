0705d00c1b7a000a5dab57d90038f3cd54d8db52
Firas Sammoura <fsammoura@google.com>
2025-08-25 13:13:33 -0700

    zephyr: test: fingerprint: Align test_get_info with new implementation
    BUG=b:398899644
    TEST=./twister -v -i -T zephyr/test/fingerprint/drivers
    
    Change-Id: Ifdd75f31e3847e78a58034bd2ea1c37d005b93ce

---
+++ zephyr/test/fingerprint/task/src/command_helper.h
@@ -0,0 +1,15 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <ec_commands.h>
+#include <host_command.h>
+
+uint16_t fpinfo_cmd_helper(struct ec_response_fp_info_v2 *response)
+{
+	struct host_cmd_handler_args args =
+		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_FP_INFO, 2, *response);
+	args.response_max = 500;
+
+	return host_command_process(&args);
+}
