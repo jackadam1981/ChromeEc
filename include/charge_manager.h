4a1e45aba4c5630f762da9a8790af03420d69a23
Al Semjonovs <asemjonovs@google.com>
2025-08-25 12:20:49 -0600

    charge_manager: Fix mutex deadlock
    charge_manager_refresh runs in the system work queue thread.
    Via board_set_active_charge_port, triggers a tcpc_write, which will
    invoke pd_wait_exit_low_power_mode.  This method posts an event to the
    PD_Cn thread and waits for a response event from PD.  PD thread wakes up
    to handle this event and queries charge manager for active_charge_port.
    This now tries to lock cm_refresh mutex, but is already held by system
    work queue charge_manager_refresh, leading to a watchdog timeout. Many
    modules query charge_manager_get_active_charge_port to see if the value
    is PORT_NONE.  In these scenarios mutex protection around this value is
    not needed.  Adding a new API to check `no_active_charge_port` to handle
    this check without locks prevents this deadlock from occurring.
    
    BUG=b:435255268
    TEST=Verified on uldren device
    TEST=make run-charge_manager run-charge_manager_drp_charging
    TEST=./twister -c -T zephyr/test/charger
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    
    Change-Id: I6fdcacc29bd0ab2d7b87a389ee53fb6a1a7d487d

--- include/charge_manager.h
+++ include/charge_manager.h
@@ -226,6 +226,14 @@ int charge_manager_get_override(void);
 int charge_manager_get_active_charge_port(void);
 
 /**
+ * Return true/false if no active charge port.
+ *
+ * @return true
+ * @return false
+ */
+bool charge_manager_has_no_active_charge_port(void);
+
+/**
  * Get the current selected charge port, as determined by charge manager.
  * This is the charge port that is either active or that we may be
  * transitioning to because a better choice has been given as an option
