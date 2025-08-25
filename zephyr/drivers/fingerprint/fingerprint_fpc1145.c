59ff6f4af664f64a43d7d2ff7abf5115d17fc865
Firas Sammoura <fsammoura@google.com>
2025-08-25 11:46:12 -0700

    drivers: fingerprint: fpc1145: Support multiple image configurations
    BUG=b:398899644
    TEST=CQ
    
    Change-Id: I42b20a9ea67521df7338f562a8d4b3eed5cd92db

--- zephyr/drivers/fingerprint/fingerprint_fpc1145.c
+++ zephyr/drivers/fingerprint/fingerprint_fpc1145.c
@@ -14,6 +14,7 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/pm/device.h>
 #include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/util.h>
 
 #include <drivers/fingerprint.h>
 #include <fingerprint/v4l2_types.h>
@@ -347,8 +348,10 @@ static int fpc1145_deinit(const struct device *dev)
 	return 0;
 }
 
-static int fpc1145_get_info(const struct device *dev,
-			    struct fingerprint_info *info)
+static int
+fpc1145_get_info(const struct device *dev,
+		 struct fingerprint_sensor_info *sensor_info,
+		 struct fingerprint_image_frame_params *image_frame_params)
 {
 	const struct fpc1145_cfg *cfg = dev->config;
 	struct fpc1145_data *data = dev->data;
@@ -356,7 +359,12 @@ static int fpc1145_get_info(const struct device *dev,
 	int rc;
 
 	/* Copy immutable sensor information to the structure. */
-	memcpy(info, &cfg->info, sizeof(struct fingerprint_info));
+	memcpy(sensor_info, &cfg->sensor_info,
+	       sizeof(struct fingerprint_sensor_info));
+
+	memcpy(image_frame_params, &cfg->sensor_image_configs,
+	       sizeof(struct fingerprint_image_frame_params
+			      [NUM_IMAGE_CAPTURE_TYPES]));
 
 	rc = fpc1145_get_hwid(cfg, &id);
 	if (rc) {
@@ -364,8 +372,8 @@ static int fpc1145_get_info(const struct device *dev,
 		return rc;
 	}
 
-	info->model_id = id;
-	info->errors = data->errors;
+	sensor_info->model_id = id;
+	sensor_info->errors = data->errors;
 
 	return 0;
 }
@@ -549,18 +557,28 @@ static int fpc1145_init_driver(const struct device *dev)
 	return 0;
 }
 
-#define FPC1145_SENSOR_INFO(inst)                                      \
-	{                                                              \
-		.vendor_id = FOURCC('F', 'P', 'C', ' '),               \
-		.product_id = 9,                                       \
-		.model_id = 1,                                         \
-		.version = 1,                                          \
-		.frame_size = CONFIG_FINGERPRINT_SENSOR_IMAGE_SIZE,    \
-		.pixel_format = FINGERPRINT_SENSOR_V4L2_PIXEL_FORMAT(  \
-			DT_DRV_INST(inst)),                            \
-		.width = FINGERPRINT_SENSOR_RES_X(DT_DRV_INST(inst)),  \
-		.height = FINGERPRINT_SENSOR_RES_Y(DT_DRV_INST(inst)), \
-		.bpp = FINGERPRINT_SENSOR_RES_BPP(DT_DRV_INST(inst)),  \
+#define FPC1145_SENSOR_INFO(inst)                                          \
+	{                                                                  \
+		.vendor_id = FOURCC('F', 'P', 'C', ' '),                   \
+		.product_id = 9,                                           \
+		.model_id = 1,                                             \
+		.version = 1,                                              \
+		.num_capture_types =                                       \
+			FINGERPRINT_SENSOR_NUM_CONFIGS(DT_DRV_INST(inst)), \
+	}
+
+#define FPC1145_IMAGE_PARAM_INITIALIZER(idx, inst)                             \
+	{                                                                      \
+		.frame_size =                                                  \
+			FINGERPRINT_SENSOR_FRAME_SIZE(idx, DT_DRV_INST(inst)), \
+		.pixel_format = FINGERPRINT_SENSOR_V4L2_PIXEL_FORMAT(          \
+			idx, DT_DRV_INST(inst)),                               \
+		.width = FINGERPRINT_SENSOR_RES_X(idx, DT_DRV_INST(inst)),     \
+		.height = FINGERPRINT_SENSOR_RES_Y(idx, DT_DRV_INST(inst)),    \
+		.bpp = FINGERPRINT_SENSOR_RES_BPP(idx, DT_DRV_INST(inst)),     \
+		.fp_capture_type = FINGERPRINT_SENSOR_CAPTURE_TYPE(            \
+			idx, DT_DRV_INST(inst)),                               \
+		.reserved = 0,                                                 \
 	}
 
 /* The sensor context is uncached as it contains the SPI buffers, which is
@@ -580,7 +598,10 @@ static int fpc1145_init_driver(const struct device *dev)
 			inst, SPI_OP_MODE_MASTER | SPI_WORD_SET(8), 0),        \
 		.interrupt = GPIO_DT_SPEC_INST_GET(inst, irq_gpios),           \
 		.reset_pin = GPIO_DT_SPEC_INST_GET(inst, reset_gpios),         \
-		.info = FPC1145_SENSOR_INFO(inst),                             \
+		.sensor_info = FPC1145_SENSOR_INFO(inst),                      \
+		.sensor_image_configs = { LISTIFY(                             \
+			FINGERPRINT_SENSOR_NUM_CONFIGS(DT_DRV_INST(inst)),     \
+			FPC1145_IMAGE_PARAM_INITIALIZER, (, ), inst) },        \
 	};                                                                     \
 	BUILD_ASSERT(                                                          \
 		CONFIG_FINGERPRINT_SENSOR_IMAGE_SIZE >=                        \
