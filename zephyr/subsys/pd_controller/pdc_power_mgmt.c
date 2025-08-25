940f88f8dea2efc3c4c6dc39e010c26b67575c3b
Tristan Honscheid <honscheid@google.com>
2025-08-25 16:00:49 -0600

    pdc: Remove pdc_power_mgmt_request_swap_to_snk|src()
    These two functions are called from just one location, each. In both use
    cases, the caller checks for a particular power role before calling the
    swap, so these calls can be replaced with
    `pdc_power_mgmt_request_power_swap()` which just swaps to the opposite
    role if PD-attached.
    
    Remove these functions as they are redundant and require additional
    logic within `pdc_power_mgmt_request_power_swap_intern()` to check the
    current role (to be addressed in subsequent commit)
    
    BUG=b:465694662
    TEST=zmake build %rauru
    TEST=./twister -T zephyr/test/pdc
    LOW_COVERAGE_REASON=Rauru board tests under b/342493172
    
    Change-Id: I8c123fa05741ea1f211fedb5ff925f31d6e548b2

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -3531,16 +3531,6 @@ static int pdc_power_mgmt_request_power_swap_intern(int port,
 	return EC_SUCCESS;
 }
 
-void pdc_power_mgmt_request_swap_to_src(int port)
-{
-	pdc_power_mgmt_request_power_swap_intern(port, PD_ROLE_SOURCE);
-}
-
-void pdc_power_mgmt_request_swap_to_snk(int port)
-{
-	pdc_power_mgmt_request_power_swap_intern(port, PD_ROLE_SINK);
-}
-
 test_mockable void pdc_power_mgmt_request_power_swap(int port)
 {
 	if (pdc_power_mgmt_is_sink_connected(port)) {
@@ -4437,8 +4427,16 @@ test_mockable void pdc_power_mgmt_request_source_voltage(int port, int mv)
 
 	if (pdc_power_mgmt_is_sink_connected(port)) {
 		pdc_power_mgmt_set_new_power_request(port);
+	} else if (pdc_power_mgmt_is_source_connected(port)) {
+		/* We are a source, swap to sink */
+		LOG_INF("C%d: Swapping to sink role to request new "
+			"source voltage",
+			port);
+		pdc_power_mgmt_request_power_swap(port);
 	} else {
-		pdc_power_mgmt_request_swap_to_snk(port);
+		LOG_ERR("C%d: Port is disconnected. New source voltage "
+			"will take effect on next connection",
+			port);
 	}
 }
 
