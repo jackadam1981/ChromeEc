4a238e9a76431882429630098883a94aad91c8be
Firas Sammoura <fsammoura@google.com>
2025-08-25 23:32:28 +0000

    zephyr: test: fingerprint: Align fp_sensor_get_info  with new implementation
    BUG=b:398899644
    TEST=./twister -v -i -T zephyr/test/fingerprint/task
    
    Change-Id: Ica51c340bee6aa8e62bb9ffbf0b7a91041d70911

--- zephyr/test/fingerprint/task/src/command_helper.h
+++ zephyr/test/fingerprint/task/src/command_helper.h
@@ -2,14 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <drivers/fingerprint.h>
 #include <ec_commands.h>
 #include <host_command.h>
 
 uint16_t fpinfo_cmd_helper(struct ec_response_fp_info_v2 *response)
 {
+	static const size_t test_info_buffer_size =
+		sizeof(struct ec_response_fp_info_v2) +
+		sizeof(struct fp_image_frame_params) * NUM_IMAGE_CAPTURE_TYPES;
+
 	struct host_cmd_handler_args args =
 		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_FP_INFO, 2, *response);
-	args.response_max = 500;
+	args.response_max = test_info_buffer_size;
 
 	return host_command_process(&args);
 }
