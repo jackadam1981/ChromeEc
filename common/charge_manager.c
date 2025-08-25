485f686e9027f824f5084669f814f0d60b5da9c7
Al Semjonovs <asemjonovs@google.com>
2025-08-25 12:20:49 -0600

    charge_manager: Fix mutex deadlock
    When leaving safemode the charge_manager_refresh is interrupted
    potentially leaving the mutex in a locked state. Make sure
    the refresh function is not running when invoking hook_call_deferred.
    
    BUG=b:435255268
    TEST=Verified on uldren device
    
    Change-Id: I6fdcacc29bd0ab2d7b87a389ee53fb6a1a7d487d

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -1304,8 +1304,11 @@ static void charge_manager_make_change(enum charge_manager_change_type change,
 	 * to our charge port until we are certain we know what is
 	 * attached.
 	 */
-	if (charge_manager_is_seeded())
+	if (charge_manager_is_seeded()) {
+		CM_MUTEX_LOCK(&cm_refresh);
 		hook_call_deferred(&charge_manager_refresh_data, 0);
+		CM_MUTEX_UNLOCK(&cm_refresh);
+	}
 }
 
 void charge_manager_invalidate_suppliers(int port)
@@ -1412,6 +1415,7 @@ void charge_manager_leave_safe_mode(void)
 	if (left_safe_mode)
 		return;
 
+	CM_MUTEX_LOCK(&cm_refresh);
 	/*
 	 * Sometimes the fuel gauge will report that it has
 	 * sufficient state of charge and remaining capacity,
@@ -1431,6 +1435,8 @@ void charge_manager_leave_safe_mode(void)
 	left_safe_mode = 1;
 	if (charge_manager_is_seeded())
 		hook_call_deferred(&charge_manager_refresh_data, 0);
+
+	CM_MUTEX_UNLOCK(&cm_refresh);
 }
 #endif
 
@@ -1497,6 +1503,8 @@ int charge_manager_set_override(int port)
 	if (delayed_override_port != OVERRIDE_OFF)
 		return EC_ERROR_BUSY;
 
+	CM_MUTEX_LOCK(&cm_refresh);
+
 	/* Set the override port if it's a sink. */
 	if (port < 0 || is_sink(port)) {
 		if (override_port != port) {
@@ -1518,8 +1526,11 @@ int charge_manager_set_override(int port)
 				   POWER_SWAP_TIMEOUT);
 		pd_request_power_swap(port);
 		/* Can't charge from requested port -- return error. */
-	} else
+	} else {
 		retval = EC_ERROR_INVAL;
+	}
+
+	CM_MUTEX_UNLOCK(&cm_refresh);
 
 	return retval;
 }
