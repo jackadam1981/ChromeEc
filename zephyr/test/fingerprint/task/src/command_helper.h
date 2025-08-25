d07a3bb381b59874cdfba95ba48d2c750b15c616
Firas Sammoura <fsammoura@google.com>
2025-08-25 23:32:28 +0000

    zephyr: test: fingerprint: Align fp_sensor_get_info  with new implementation
    BUG=b:398899644
    TEST=./twister -v -i -T zephyr/test/fingerprint/task
    
    Change-Id: Ica51c340bee6aa8e62bb9ffbf0b7a91041d70911

--- zephyr/test/fingerprint/task/src/command_helper.h
+++ zephyr/test/fingerprint/task/src/command_helper.h
@@ -9,7 +9,7 @@ uint16_t fpinfo_cmd_helper(struct ec_response_fp_info_v2 *response)
 {
 	struct host_cmd_handler_args args =
 		BUILD_HOST_COMMAND_RESPONSE(EC_CMD_FP_INFO, 2, *response);
-	args.response_max = 500;
+	args.response_max = 100;
 
 	return host_command_process(&args);
 }
