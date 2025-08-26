druth0/chrome-ec

commit 6fa4fc462c07c9f14411621a39b84f2380272351

dsp_comms: use GMR tablet mode config
api dsp_service_gmr_tablet_switch_isr() is applicable only
when GMR configs are enabled. Hence api call is protected
by GMR compiler flag.

BUG=b:409240881
TEST=zmake build trulo-ti

Change-Id: I62fe65bbcd8e1361437fac605cd1169a5c14276c
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6661162
Commit-Queue: Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Tested-by: Deepti Deshatty <deepti.deshatty@intel.corp-partner.google.com>
Reviewed-by: Yuval Peress <peress@google.com>
Reviewed-by: Jayvik Desai <jayvik@google.com>

zephyr/drivers/dsp_comms/service/dsp_service.cc
@@ -358,10 +358,12 @@ void dsp_service_hook_tablet_mode_change() {
   cros::dsp::service::driver.transport_.SetStatusBit(
       cros_dsp_comms_StatusFlag_STATUS_FLAG_TABLET_MODE, is_in_tablet_mode);
 }
+#ifdef CONFIG_PLATFORM_EC_GMR_TABLET_MODE
 extern "C" void dsp_service_gmr_tablet_switch_isr(enum gpio_signal signal) {
   dsp_service_hook_tablet_mode_change();
   gmr_tablet_switch_isr(signal);
 }
+#endif
 DECLARE_HOOK(HOOK_INIT, dsp_service_hook_tablet_mode_change, HOOK_PRIO_DEFAULT);
 #endif /* CONFIG_PLATFORM_EC_TABLET_MODE */
 

jackadam1981/ChromeEc

commit 1c2d955fd1977428a8bc3e7eb2ebe2c31fc6f0ef

uldrenite: Fix "stuck in tablet mode"
The ifdef was added in chromium:6661162 to allow trulo products without
the GMR, but the wrong config option was chosen. By using the DSP
variant of the config removing the magnet restores the clamshell mode.

BUG=b:430373200
TEST=Flash EC of uldrenite260
     place magnet by chassis (enter tablet mode)
     remove magnet (enter clamshell mode)

Fixes: 6fa4fc462c07 ("dsp_comms: use GMR tablet mode config")
Change-Id: Ibb29f12a40c8c82ff989d579480104b527c80a1d

zephyr/drivers/dsp_comms/service/dsp_service.cc
@@ -358,7 +358,7 @@ void dsp_service_hook_tablet_mode_change() {
   cros::dsp::service::driver.transport_.SetStatusBit(
       cros_dsp_comms_StatusFlag_STATUS_FLAG_TABLET_MODE, is_in_tablet_mode);
 }
-#ifdef CONFIG_PLATFORM_EC_GMR_TABLET_MODE
+#ifdef CONFIG_PLATFORM_EC_DSP_REMOTE_TABLET_SWITCH
 extern "C" void dsp_service_gmr_tablet_switch_isr(enum gpio_signal signal) {
   dsp_service_hook_tablet_mode_change();
   gmr_tablet_switch_isr(signal);