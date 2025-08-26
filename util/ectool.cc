64a1ce450e9ab300e771a2de5e60d4ac90036abe
Firas Sammoura <fsammoura@google.com>
2025-08-26 05:16:58 +0000

    ectool: Use libec FpInfoCommand in cmd_fp_info
    This change refactors the `cmd_fp_info` function in ectool to use the
    libec implementation of the factory version of FpInfoCommand.
    
    BUG=b:398899644
    TEST=emerge-rex ec-utils
    
    Change-Id: I7d781e0a90dad8a7c3bf02197ee099aa6f23b00f

--- util/ectool.cc
+++ util/ectool.cc
@@ -2080,38 +2080,24 @@ int cmd_fp_stats(int argc, char *argv[])
 
 int cmd_fp_info(int argc, char *argv[])
 {
-	struct ec_response_fp_info r;
-	int rv;
-	int cmdver = ec_cmd_version_supported(EC_CMD_FP_INFO, 1) ? 1 : 0;
-	int rsize = cmdver == 1 ? sizeof(r) :
-				  sizeof(struct ec_response_fp_info_v0);
-	uint16_t dead;
+	ec::EcCommandFactory ec_command_factory;
+	ec::EcCommandVersionSupported ec_cmd_ver_supported;
+	auto fp_info_command =
+		ec_command_factory.FpInfoCommand(&ec_cmd_ver_supported);
 
-	rv = ec_command(EC_CMD_FP_INFO, cmdver, NULL, 0, &r, rsize);
-	if (rv < 0)
+	if (!fp_info_command) {
+		fprintf(stderr, "Failed to create FpInfoCommand.\n");
+		return -1;
+	}
+
+	if (!fp_info_command->Run(comm_get_fd())) {
+		int rv = -EECRESULT - fp_info_command->Result();
+		fprintf(stderr, "Fp info command returned with errors: %d\n",
+			rv);
 		return rv;
-
-	printf("Fingerprint sensor: vendor %x product %x model %x version %x\n",
-	       r.vendor_id, r.product_id, r.model_id, r.version);
-	printf("Image: size %dx%d %d bpp\n", r.width, r.height, r.bpp);
-	printf("Error flags: %s%s%s%s\n",
-	       r.errors & FP_ERROR_NO_IRQ ? "NO_IRQ " : "",
-	       r.errors & FP_ERROR_SPI_COMM ? "SPI_COMM " : "",
-	       r.errors & FP_ERROR_BAD_HWID ? "BAD_HWID " : "",
-	       r.errors & FP_ERROR_INIT_FAIL ? "INIT_FAIL " : "");
-	dead = FP_ERROR_DEAD_PIXELS(r.errors);
-	if (dead == FP_ERROR_DEAD_PIXELS_UNKNOWN) {
-		printf("Dead pixels: UNKNOWN\n");
-	} else {
-		printf("Dead pixels: %u\n", dead);
 	}
 
-	if (cmdver == 1) {
-		printf("Templates: version %d size %d count %d/%d"
-		       " dirty bitmap %x\n",
-		       r.template_version, r.template_size, r.template_valid,
-		       r.template_max, r.template_dirty);
-	}
+	printf("%s", fp_info_command->ParseSensorInfo().c_str());
 
 	return 0;
 }
