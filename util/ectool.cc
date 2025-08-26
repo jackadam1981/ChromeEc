fac96818d876ac9ed448b39484d6c20ad8a8b528
Firas Sammoura <fsammoura@google.com>
2025-08-26 18:51:16 +0000

    ectool: Refactor fingerprint frame download using libec
    This commit refactors the `fp_download_frame` function within
    `ectool.cc`. The primary goal is to modernize the implementation by
    leveraging the `libec` command classes, specifically `FpInfoCommand` and
    `FpModeCommand`, instead of relying on direct `ec_command` invocations
    with `EC_CMD_FP_INFO`.
    
    Key changes include:
    
    *   Using `FpInfoCommand` to retrieve sensor and template information.
    *   Introducing logic to handle multiple sensor image configurations. This
        involves checking the current fingerprint mode via `FpModeCommand` to
        select the correct image parameters.
    *   Updating `cmd_fp_frame` and `cmd_fp_template` to align with the
        modified `fp_download_frame` function signature, passing in
        `SensorImage` and `TemplateInfo` structs.
    
    These changes enhance code clarity and maintainability by adopting the
    standard `libec` command wrappers.
    
    BUG=b:398899644
    TEST=emerge-rex ec-utils
    
    commit message for your changes. Lines starting
    
    Change-Id: Ia549fa678d6e1ec016226fb4daf0fa8c3960c917

--- util/ectool.cc
+++ util/ectool.cc
@@ -53,6 +53,7 @@
 #include <libec/ec_panicinfo.h>
 #include <libec/fingerprint/fp_encryption_status_command.h>
 #include <libec/fingerprint/fp_frame_command.h>
+#include <libec/fingerprint/fp_mode_command.h>
 #include <libec/mkbp_event.h>
 #include <libec/rand_num_command.h>
 #include <libec/versions_command.h>
@@ -1910,29 +1911,56 @@ int cmd_apreset(int argc, char *argv[])
  * @returns a vector<uint8_t> containing the the requested frame.
  */
 static std::unique_ptr<std::vector<uint8_t> >
-fp_download_frame(struct ec_response_fp_info *info, int index)
+fp_download_frame(struct SensorImage &sensor_image,
+		  struct TemplateInfo &template_info, int index)
 {
-	int rv = 0;
+	ec::EcCommandFactory ec_command_factory;
+	ec::EcCommandVersionSupported ec_cmd_ver_supported;
+	auto fp_info_command =
+		ec_command_factory.FpInfoCommand(&ec_cmd_ver_supported);
+
+	if (!fp_info_command && !fp_info_command->Run(comm_get_fd())) {
+		fprintf(stderr, "Failed to Run FpInfoCommand.\n");
+		return NULL;
+	}
+
+	template_info = *fp_info_command->template_info();
+	auto images = fp_info_command->sensor_image();
+	if (images.size() == 1) {
+		sensor_image = images[0];
+	} else {
+		ec::FpModeCommand fp_mode_command(
+			(ec::FpMode(ec::FpMode::Mode::kDontChange)));
+		if (!fp_mode_command.Run(comm_get_fd())) {
+			fprintf(stderr, "Failed to Run FpModeCommand.\n");
+			return NULL;
+		}
+
+		bool found = false;
+		for (auto image : fp_info_command->sensor_image()) {
+			if (image.fp_capture_type ==
+			    FP_CAPTURE_TYPE(fp_mode_command.Mode().RawVal())) {
+				sensor_image = image;
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			fprintf(stderr,
+				"Could not find fp image frame params.\n");
+			return NULL;
+		}
+	}
+
 	size_t size;
-	int cmdver = ec_cmd_version_supported(EC_CMD_FP_INFO, 1) ? 1 : 0;
-	int rsize = cmdver == 1 ? sizeof(*info) :
-				  sizeof(struct ec_response_fp_info_v0);
-
-	/* templates not supported in command v0 */
-	if (index > 0 && cmdver == 0)
-		return NULL;
-
-	rv = ec_command(EC_CMD_FP_INFO, cmdver, NULL, 0, info, rsize);
-	if (rv < 0)
-		return NULL;
-
 	if (index == FP_FRAME_INDEX_SIMPLE_IMAGE) {
-		size = (size_t)info->width * info->bpp / 8 * info->height;
+		size = (size_t)sensor_image.width * sensor_image.bpp / 8 *
+		       sensor_image.height;
 		index = FP_FRAME_INDEX_RAW_IMAGE;
 	} else if (index == FP_FRAME_INDEX_RAW_IMAGE) {
-		size = info->frame_size;
+		size = sensor_image.frame_size;
 	} else {
-		size = info->template_size;
+		size = template_info.size;
 	}
 
 	auto frame_cmd = ec::FpFrameCommand::Create(index, size, ec_max_insize);
@@ -2184,25 +2212,28 @@ int cmd_fp_enc_status(int argc, char *argv[])
 
 int cmd_fp_frame(int argc, char *argv[])
 {
-	struct ec_response_fp_info r;
+	struct SensorImage sensor_image = { 0 };
+	struct TemplateInfo template_info = { 0 };
 	int idx = (argc == 2 && !strcasecmp(argv[1], "raw")) ?
 			  FP_FRAME_INDEX_RAW_IMAGE :
 			  FP_FRAME_INDEX_SIMPLE_IMAGE;
-	auto fp_frame = fp_download_frame(&r, idx);
+	auto fp_frame = fp_download_frame(sensor_image, template_info, idx);
 	if (!fp_frame) {
 		fprintf(stderr, "Failed to get FP sensor frame\n");
 		return -1;
 	}
 
 	if (idx == FP_FRAME_INDEX_RAW_IMAGE) {
-		assert(fp_frame->size() == r.frame_size);
-		fwrite(fp_frame->data(), r.frame_size, 1, stdout);
+		assert(fp_frame->size() == sensor_image.frame_size);
+		fwrite(fp_frame->data(), sensor_image.frame_size, 1, stdout);
 		return 0;
 	}
 
 	auto frame_to_pgm = ec::FpFrameCommand::FrameToPgm(
 		*fp_frame,
-		{ .bpp = r.bpp, .width = r.width, .height = r.height });
+		{ .bpp = sensor_image.bpp,
+		  .width = static_cast<uint16_t>(sensor_image.width),
+		  .height = static_cast<uint16_t>(sensor_image.height) });
 
 	if (!frame_to_pgm.has_value()) {
 		fprintf(stderr, "Error: Failed to convert frame to PGM.\n");
@@ -2214,7 +2245,8 @@ int cmd_fp_frame(int argc, char *argv[])
 
 int cmd_fp_template(int argc, char *argv[])
 {
-	struct ec_response_fp_info r;
+	struct SensorImage sensor_image = { 0 };
+	struct TemplateInfo template_info = { 0 };
 	struct ec_params_fp_template *p =
 		(struct ec_params_fp_template *)(ec_outbuf);
 	/* TODO(b/78544921): removing 32 bits is a workaround for the MCU bug */
@@ -2234,13 +2266,14 @@ int cmd_fp_template(int argc, char *argv[])
 
 	idx = strtol(argv[1], &e, 0);
 	if (!(e && *e)) {
-		auto fp_frame = fp_download_frame(&r, idx + 1);
+		auto fp_frame =
+			fp_download_frame(sensor_image, template_info, idx + 1);
 		if (!fp_frame) {
 			fprintf(stderr, "Failed to get FP template %d\n", idx);
 			return -1;
 		}
-		assert(fp_frame->size() == r.template_size);
-		fwrite(fp_frame->data(), r.template_size, 1, stdout);
+		assert(fp_frame->size() == template_info.size);
+		fwrite(fp_frame->data(), template_info.size, 1, stdout);
 		return 0;
 	}
 	/* not an index, is it a filename ? */
