9feb369396c21913f28a6054cb1bf83c0a5bc562
Derek Huang <derekhuang@google.com>
2025-09-01 15:39:13 +0000

    bluey: add initial quartz project
    This patch adds the quartz project under bluey program with the
    minimal configuration files.
    
    BUG=b:441716957
    TEST=zmake build quartz
    
    Change-Id: I7070da6e2abe97955fa5d7862a189c106c78d436

--- zephyr/program/bluey/BUILD.py
+++ zephyr/program/bluey/BUILD.py
@@ -40,7 +40,12 @@ register_npcx9_project(
     project_name="quenbi",
 )
 
+register_npcx9_project(
+    project_name="quartz",
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="bluey", addr=0x80144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="quenbi", addr=0x80144)
+assert_rw_fwid_DO_NOT_EDIT(project_name="quartz", addr=0x80144)
