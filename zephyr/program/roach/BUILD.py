5b099ba705a28ba8fe2769d523b2dd38c0c2ccd9
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2025-09-01 14:05:09 +0800

    Roach: Eirtae: initial board
    BUG=b:428801082
    TEST=zmake build pass
    LOW_COVERAGE_REASON=Tests will be added in b:428801082
    
    Change-Id: Ief751a5fe2d130a804d1d9aa35ed751608d56031

--- zephyr/program/roach/BUILD.py
+++ zephyr/program/roach/BUILD.py
@@ -34,6 +34,7 @@ register_variant("axii", rwsig_sign=False, inherited_from=[])
 register_variant("roach", inherited_from=["geralt"])
 register_variant("kelpie", inherited_from=["geralt"])
 register_variant("spikyrock", inherited_from=["staryu"])
+register_variant("eirtae", inherited_from=["jedi"])
 
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
@@ -41,3 +42,4 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="axii", addr=0x7FFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="kelpie", addr=0x40098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="roach", addr=0x40098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="spikyrock", addr=0x40098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="eirtae", addr=0x40098)
