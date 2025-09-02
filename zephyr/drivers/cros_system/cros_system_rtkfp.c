879f52abf89ed7f30c4f58fdec7a8a6c312574ec
caiyi zhong <caiyi_zhong@realtek.corp-partner.google.com>
2025-09-02 16:19:05 +0800

    rts5817: add cros_system driver
    Add cros_system driver for RTS5817
    
    BUG=b:422616702
    TEST=
    
    Change-Id: Icda756190b4a217d2faed43e7d626000d9a4cefc
    Signed-off-by: Caiyi Zhong <caiyi_zhong@realtek.corp-partner.google.com>

--- zephyr/drivers/cros_system/cros_system_rtkfp.c
+++ zephyr/drivers/cros_system/cros_system_rtkfp.c
@@ -8,14 +8,17 @@
 #include "drivers/cros_system.h"
 #include "stdint.h"
 #include "system.h"
+#include "stdio.h"
 
 #include <zephyr/device.h>
-#include <zephyr/drivers/bbram.h>
 #include <zephyr/drivers/watchdog.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/poweroff.h>
 #include <zephyr/sys/reboot.h>
 #include <zephyr/sys/util.h>
+#include <zephyr/sys/barrier.h>
+#include <zephyr/drivers/timer/system_timer.h>
+#include <zephyr/cache.h>
 
 LOG_MODULE_REGISTER(cros_system, LOG_LEVEL_ERR);
 
@@ -23,6 +26,37 @@ LOG_MODULE_REGISTER(cros_system, LOG_LEVEL_ERR);
 struct cros_system_rtkfp_data {
 	int reset; /* reset cause */
 };
+
+static const struct device *const watchdog =
+	DEVICE_DT_GET(DT_CHOSEN(cros_ec_watchdog));
+
+#define R_CHIP_RESET 0x401e2100
+#define CHIP_RESET_BIT BIT(0)
+static void rtkfp_resetchip(void)
+{
+	uint32_t chip_rst;
+
+	/* Disable interrupts to avoid task swaps during reboot */
+	interrupt_disable_all();
+
+	wdt_disable(watchdog);
+	sys_clock_disable();
+
+	/* Flush data cache */
+	sys_cache_data_flush_all();
+
+	barrier_dsync_fence_full();
+	barrier_isync_fence_full();
+
+	/* Chip reset */
+	chip_rst = *(volatile uint32_t *)R_CHIP_RESET;
+	chip_rst |= CHIP_RESET_BIT;
+	*(volatile uint32_t *)R_CHIP_RESET = chip_rst;
+
+	/* Spin and wait for reboot */
+	while (1)
+		;
+}
 
 static const char *cros_system_rtkfp_get_chip_vendor(const struct device *dev)
 {
@@ -35,33 +69,51 @@ static const char *cros_system_rtkfp_get_chip_name(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-	return NULL;
+	return "rts5817";
 }
 
+#define R_SYS_STS 0x40100000
+#define VER_NUM_OFFSET 16
+#define VER_NUM_MASK   (0xFF << VER_NUM_OFFSET)
 static const char *cros_system_rtkfp_get_chip_revision(const struct device *dev)
 {
 	ARG_UNUSED(dev);
+	static char buf[5];
+	uint32_t sts_reg = *(volatile uint32_t *)(R_SYS_STS);
+	uint8_t chip_ver = (sts_reg & VER_NUM_MASK) >> VER_NUM_OFFSET;
 
-	return NULL;
+	snprintf(buf, sizeof(buf), "%c", chip_ver);
+
+	return buf;
 }
 
 static int cros_system_rtkfp_get_reset_cause(const struct device *dev)
 {
-	return 0;
+	struct cros_system_rtkfp_data *data = dev->data;
+
+	return data->reset;
 }
 
 static int cros_system_rtkfp_init(const struct device *dev)
 {
+	struct cros_system_rtkfp_data *data = dev->data;
+
+	/* Todo: implement get reset cause function */
+	data->reset = POWERUP;
+
 	return 0;
 }
 
 static int cros_system_rtkfp_soc_reset(const struct device *dev)
 {
+	rtkfp_resetchip();
+
+	/* Should never return */
 	return 0;
 }
 
 static int cros_system_rtkfp_hibernate(const struct device *dev, uint32_t seconds,
-				     uint32_t microseconds)
+				       uint32_t microseconds)
 {
 	return 0;
 }
