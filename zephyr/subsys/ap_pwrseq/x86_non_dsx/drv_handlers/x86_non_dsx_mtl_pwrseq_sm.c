2b946484cb22a474086fce96c509e3139ffca6ce
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2025-09-02 14:43:52 -0700

    ap_pwrseq: Migrate driver power state handlers
    This CL moves AP power sequence driver state handlers implementation
    into new file and consolidate common functionality.
    
    BUG=none
    TEST=zmake build_all
    
    Change-Id: Ie886578845a43628b805562094efa92d7c905aaf

--- zephyr/subsys/ap_pwrseq/x86_non_dsx/drv_handlers/x86_non_dsx_mtl_pwrseq_sm.c
+++ zephyr/subsys/ap_pwrseq/x86_non_dsx/drv_handlers/x86_non_dsx_mtl_pwrseq_sm.c
@@ -3,84 +3,12 @@
  * found in the LICENSE file.
  */
 
-#include <x86_non_dsx_common_pwrseq_sm_handler.h>
-#ifdef CONFIG_AP_PWRSEQ_DRIVER
 #include "ap_power/ap_pwrseq_sm.h"
-#endif
+
+#include <x86_non_dsx_common_pwrseq_sm_handler.h>
 
 LOG_MODULE_DECLARE(ap_pwrseq, CONFIG_AP_PWRSEQ_LOG_LEVEL);
 
-bool chipset_is_prim_power_good(void)
-{
-	return power_signal_get(PWR_RSMRST_PWRGD);
-}
-
-bool chipset_is_vw_power_good(void)
-{
-	return chipset_is_prim_power_good() &&
-	       !power_signal_get(PWR_EC_PCH_RSMRST);
-}
-
-bool chipset_is_all_power_good(void)
-{
-	return chipset_is_vw_power_good() &&
-	       power_signal_get(PWR_ALL_SYS_PWRGD) &&
-	       power_signal_get(PWR_PCH_PWROK) &&
-	       power_signal_get(PWR_EC_PCH_SYS_PWROK);
-}
-
-#ifndef CONFIG_AP_PWRSEQ_DRIVER
-static void ap_off(void)
-{
-	power_signal_set(PWR_PCH_PWROK, 0);
-	power_signal_set(PWR_EC_PCH_SYS_PWROK, 0);
-}
-
-/* Generate SYS_PWROK->SOC if needed by system */
-static void generate_pwrok_handler(void)
-{
-	int all_sys_pwrgd_in;
-
-	if (power_signal_get(PWR_EC_PCH_SYS_PWROK) == 0) {
-		k_msleep(AP_PWRSEQ_DT_VALUE(sys_pwrok_delay));
-	}
-
-	all_sys_pwrgd_in = power_signal_get(PWR_ALL_SYS_PWRGD);
-	/* Loop through all PWROK signals defined by the board */
-	if (all_sys_pwrgd_in == 0) {
-		LOG_DBG("PG_EC_ALL_SYS_PWRGD deasserted, "
-			"shutting AP off!");
-		ap_off();
-		return;
-	}
-
-	power_signal_set(PWR_EC_PCH_SYS_PWROK, all_sys_pwrgd_in);
-	/* PCH_PWROK is set to combined result of ALL_SYS_PWRGD and SLP_S3 */
-	power_signal_set(PWR_PCH_PWROK,
-			 all_sys_pwrgd_in && !power_signal_get(PWR_SLP_S3));
-}
-
-/* Chipset specific power state machine handler */
-enum power_states_ndsx chipset_pwr_sm_run(enum power_states_ndsx curr_state)
-{
-	/* Add chipset specific state handling if any */
-	switch (curr_state) {
-	case SYS_POWER_STATE_G3S5:
-		board_ap_power_action_g3_s5();
-		break;
-	case SYS_POWER_STATE_S0S3:
-		ap_off();
-		break;
-	case SYS_POWER_STATE_S0:
-		/* Send SYS_PWROK->SoC if conditions met */
-		generate_pwrok_handler();
-		break;
-	default:
-		break;
-	}
-	return curr_state;
-}
-#else
 static int x86_non_dsx_mtl_g3_run(void *data)
 {
 	/*
@@ -189,4 +117,3 @@ static int x86_non_dsx_mtl_s0ix_run(void *data)
 AP_POWER_CHIPSET_SUB_STATE_DEFINE(S0ix, NULL, x86_non_dsx_mtl_s0ix_run, NULL,
 				  S0);
 #endif /* CONFIG_AP_PWRSEQ_S0IX */
-#endif /* CONFIG_AP_PWRSEQ_DRIVER */
