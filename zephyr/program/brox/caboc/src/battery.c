9e65acda9d70845d678acc2a71adf75095810386
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2025-09-02 20:31:28 +0800

    caboc: Override charge current to 0.7C (3388mA)
    Override charge current to 3388mA to meet OEM mini spec.
    
    BUG=none
    TEST=make sure charge current is limited to 3388mA.
    LOW_COVERAGE_REASON=b:434052617 will add test code after bring up.
    
    Change-Id: Ibd5cbfc941b071223d540c00484b8c9a477d4fa7

---
+++ zephyr/program/brox/caboc/src/battery.c
@@ -0,0 +1,27 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "charge_state.h"
+
+#define CHARGE_CURRENT_CONST 3388
+
+int charger_profile_override(struct charge_state_data *curr)
+{
+	curr->requested_current = CHARGE_CURRENT_CONST;
+
+	return 0;
+}
+
+enum ec_status charger_profile_override_get_param(uint32_t param,
+						  uint32_t *value)
+{
+	return EC_RES_INVALID_PARAM;
+}
+
+enum ec_status charger_profile_override_set_param(uint32_t param,
+						  uint32_t value)
+{
+	return EC_RES_INVALID_PARAM;
+}
