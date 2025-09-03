43ef4469ba448ccdc8dda756e4fc0f8133846740
Jayvik Desai <jayvik@google.com>
2025-09-04 02:48:17 +0530

    bluey/src/chipset: Cache battery parameters before AP power-on
    Once the Application Processor (AP) boots, control of the I2C bus is
    relinquished to it, rendering the EC unable to access the battery pack.
    
    To ensure the AP has the data needed for charging decisions, this commit
    introduces a mechanism to cache essential battery parameters before the
    AP is powered on. This allows the dynamic host commands to fetch the
    cached values from the EC for the AP to use.
    
    BUG=b:439820798, b:437785464
    TEST=zmake build %bluey && confirmed that the AP can access this cached
    values using the HOST commands.
    
    Change-Id: Ic45da8f2838a786c0c248f25a1921f1f4c414070

--- zephyr/program/bluey/src/chipset.c
+++ zephyr/program/bluey/src/chipset.c
@@ -5,10 +5,23 @@
 
 /* Bluey chipset-specific configuration */
 
+#include "battery.h"
 #include "common.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "power/qcom.h"
+
+#define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ##args)
+
+void board_chipset_pre_init(void)
+{
+	/* Cache the battery parameters before AP power on */
+	if (battery_is_present() == BP_YES) {
+		battery_poll();
+		CPRINTS("battery parameters cached");
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_PRE_INIT, board_chipset_pre_init, HOOK_PRIO_DEFAULT);
 
 void board_chipset_startup(void)
 {
