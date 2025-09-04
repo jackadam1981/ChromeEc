deb5e13d7d9bf4eee958c9ea43083fd3f91024b8
Ting Shen <phoenixshen@google.com>
2025-09-04 19:10:32 +0800

    ec_commands: add dps status command
    Port the dps status console command to ectool.
    
    BUG=b:442705634
    TEST=ectool usbpddps status`
         ./twister -T zephyr/test/drivers/
                   --test drivers.dps.ec_host_cmd
                   --test drivers.dps
    
    Change-Id: I08f06c3bc5c57c41e8c2ee5c503be53b54abdf43

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -6216,6 +6216,24 @@ struct ec_params_usb_pd_dps_control {
 	uint8_t enable;
 } __ec_align1;
 
+/*
+ * This command return the status of dynamic PDO selection.
+ */
+#define EC_CMD_USB_PD_DPS_STATUS 0x0107
+
+struct ec_response_usb_pd_dps_status {
+	int32_t is_enabled;
+	int32_t port;
+	int32_t requested_voltage;
+	int32_t requested_current;
+	int32_t input_power;
+	int32_t input_voltage;
+	int32_t input_current;
+	int32_t efficient_voltage;
+	int32_t battery_voltage;
+	int32_t max_voltage;
+} __ec_align4;
+
 /* Write USB-PD device FW */
 #define EC_CMD_USB_PD_FW_UPDATE 0x0110
 
