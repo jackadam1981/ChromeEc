8f1e8bf71c53353925ca6f898c624ac9e048e55d
Al Semjonovs <asemjonovs@google.com>
2025-09-08 14:34:29 -0600

    charger: Define minimum voltage required property
    Add 'minimum-voltage-required' property to device tree. This defines
    the voltage required to charge a battery when using a hybrid power boost
    charger. Add API to fetch value.
    
    BUG=None
    TEST=None
    LOW_COVERAGE_REASON=Tests will be added in b:430510479
    
    Change-Id: I1b276922f3145857593ddd19c125a07e4b6db5d7

--- include/charger.h
+++ include/charger.h
@@ -168,10 +168,14 @@ struct charger_drv {
 	void (*dump_prochot)(int chgnum);
 };
 
+/* Value when charger doesn't require a minimum voltage. */
+#define CHARGER_NO_MINIMUM_CHARGING_MV 0
+
 struct charger_config_t {
 	int i2c_port;
 	uint16_t i2c_addr_flags;
 	const struct charger_drv *drv;
+	uint32_t minimum_charging_mv;
 };
 
 #ifndef CONFIG_CHARGER_RUNTIME_CONFIG
@@ -299,6 +303,9 @@ enum ec_error_list charger_get_vbus_voltage(int port, int *voltage);
 
 /* Get the Vsys voltage (mV) from the charger */
 enum ec_error_list charger_get_vsys_voltage(int port, int *voltage);
+
+/* Get the minimum required voltage (mv) to charge battery */
+enum ec_error_list charger_get_minimum_charging_mv(int chgnum, uint32_t *mv);
 
 /* Custom board function to discharge battery when on AC power */
 int board_discharge_on_ac(int enable);
