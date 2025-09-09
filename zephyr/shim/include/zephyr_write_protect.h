abcd49770a1cbd980d02012f8a8335b70f89d43c
Brandon Breitenstein <brandon.breitenstein@intel.com>
2025-09-09 14:01:06 -0700

    Update Microchip WP code to properly protect the flash
    The internal Microchip flash was unable to be protected with the existing
    code as the internal and externally exposed pins are not the same. Add the
    ability to only change the externally exposed pin and keep the internal
    pin in alignment with this pin to allow for WP to function properly.
    
    BUG=b:388289782
    TEST=zmake ptlrvp_mchp, verify that flashrom is able to protect flash
    LOW_COVERAGE_REASON=b:444042320 Microchip will add test code
    
    Change-Id: Ib8bf676763525fbf3de6d4668c8b5099a6c65d5e

--- zephyr/shim/include/zephyr_write_protect.h
+++ zephyr/shim/include/zephyr_write_protect.h
@@ -26,7 +26,14 @@ static inline int write_protect_is_asserted(void)
 #ifdef CONFIG_WP_ALWAYS
 	return true;
 #else
+	/* Read write protect GPIO */
+#ifdef CONFIG_CROS_FLASH_MCHP_INTERNAL_WP
+#ifdef CONFIG_CROS_SYSTEM_XEC
+	return (!gpio_pin_get_dt(GPIO_DT_FROM_ALIAS(gpio_wp)));
+#endif
+#else
 	return gpio_pin_get_dt(GPIO_DT_FROM_ALIAS(gpio_wp));
+#endif
 #endif
 }
 
