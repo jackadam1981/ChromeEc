d0353c7b13011dc00dbbc61617e5571e8c2456c1
Al Semjonovs <asemjonovs@google.com>
2025-09-08 19:46:48 -0600

    charger: Add API to fetch min_required_voltage
    Add an API to fetch minimum required voltage from device tree.
    
    BUG=None
    TEST=None
    LOW_COVERAGE_REASON=Tests will be added in b:430510479
    
    Change-Id: I24cee5b5221df7098c2b799d4e2568de6a607f58

--- zephyr/shim/include/charger/chg_isl95522.h
+++ zephyr/shim/include/charger/chg_isl95522.h
@@ -13,11 +13,12 @@ extern "C" {
 
 #define ISL95522_CHG_COMPAT intersil_isl95522
 
-#define CHG_CONFIG_ISL95522(id)                    \
-	{                                          \
-		.i2c_port = I2C_PORT_BY_DEV(id),   \
-		.i2c_addr_flags = DT_REG_ADDR(id), \
-		.drv = &isl95522_drv,              \
+#define CHG_CONFIG_ISL95522(id)                                                \
+	{                                                                      \
+		.i2c_port = I2C_PORT_BY_DEV(id),                               \
+		.i2c_addr_flags = DT_REG_ADDR(id),                             \
+		.drv = &isl95522_drv,                                          \
+		.min_required_voltage = DT_PROP(id, minimum_voltage_required), \
 	},
 
 #ifdef __cplusplus
