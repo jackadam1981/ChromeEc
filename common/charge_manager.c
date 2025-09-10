8f82aa167af248a6c459858f4e818adc4129d33d
Al Semjonovs <asemjonovs@google.com>
2025-09-10 11:16:03 -0600

    ectool: Add IS_ADAPTER_SUFFICIENT enum to chargestate command
    Allow AP to query attached adapter is sufficient.
    
    BUG=None
    TEST=make run-sbs_charging
    TEST=ectool chargestate param 16
    
    Change-Id: I3220199da41f24f1164395cbf4c877b54d3baa4a

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -1599,7 +1599,7 @@ int charge_manager_get_charger_current(void)
 	return retval;
 }
 
-int charge_manager_get_charger_voltage(void)
+test_mockable int charge_manager_get_charger_voltage(void)
 {
 	int retval = 0;
 
