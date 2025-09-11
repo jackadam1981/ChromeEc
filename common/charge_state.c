4aaeba4df8b87cd1132e2be975f411581c1cab02
Al Semjonovs <asemjonovs@google.com>
2025-09-11 12:53:37 -0600

    charge_state: Add INSUFFICIENT_CHARGER led enum
    Add enum for insufficient charger attached.
    
    BUG=None
    TEST=None
    
    Change-Id: I5728a714833f365063ee46a141991646db38e9f9

--- common/charge_state.c
+++ common/charge_state.c
@@ -1779,12 +1779,17 @@ enum led_pwr_state led_pwr_get_state(void)
 	case ST_CHARGE:
 		/* The only difference here is what the LEDs display. */
 		if (IS_ENABLED(CONFIG_CHARGE_MANAGER) &&
-		    charge_manager_get_active_charge_port() == CHARGE_PORT_NONE)
+		    charge_manager_get_active_charge_port() ==
+			    CHARGE_PORT_NONE) {
 			return LED_PWRS_DISCHARGE;
-		else if (battery_near_full())
+		} else if (battery_near_full()) {
 			return LED_PWRS_CHARGE_NEAR_FULL;
-		else
+		} else if (charge_is_charger_sufficient()) {
 			return LED_PWRS_CHARGE;
+		} else {
+			return LED_PWRS_INSUFFIENT_CHARGER;
+		}
+
 	case ST_PRECHARGE:
 		/* we're in battery discovery mode */
 		if (chflags & CHARGE_LED_FLAG_FORCE_IDLE)
