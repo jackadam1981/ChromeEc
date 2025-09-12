aabb6b99e076b976d345fc8303921c28077d8ebc
Tristan Honscheid <honscheid@google.com>
2025-09-12 10:59:21 -0700

    rvp: Disconnect DC barrel jack if not present upon boot
    When the RVP has PDCs enabled, disable the DC barrel jack if not present
    at boot by turning off its load switch. This prevents a user from
    inserting one later when the USB-C ports are providing power.
    
    BUG=b:444320818
    TEST=Test on an RVP board
    
    Change-Id: Idd3e03a9b5fb7a4281e6101df267343dbe1acb53

--- zephyr/program/intelrvp/src/dc_jack.c
+++ zephyr/program/intelrvp/src/dc_jack.c
@@ -15,6 +15,7 @@
 #include <zephyr/init.h>
 
 static struct k_work dc_jack_handle;
+static bool dc_jack_is_disabled = false;
 
 bool is_typec_port(int port)
 {
@@ -31,13 +32,14 @@ static void board_dc_jack_handler(struct k_work *dc_jack_work)
 	struct charge_port_info charge_dc_jack;
 
 	/* System is booted from DC Jack */
-	if (board_is_dc_jack_present()) {
+	if (!dc_jack_is_disabled && board_is_dc_jack_present()) {
 		charge_dc_jack.current =
 			MIN((CONFIG_PLATFORM_EC_USB_PD_MAX_POWER_MW * 1000) /
 				    DC_JACK_MAX_VOLTAGE_MV,
 			    CONFIG_PLATFORM_EC_USB_PD_MAX_CURRENT_MA);
 		charge_dc_jack.voltage = DC_JACK_MAX_VOLTAGE_MV;
 	} else {
+		/* DC jack is disabled or not present */
 		charge_dc_jack.current = 0;
 		charge_dc_jack.voltage = 0;
 	}
@@ -55,17 +57,55 @@ test_export_static void board_charge_init(void)
 {
 	k_work_init(&dc_jack_handle, board_dc_jack_handler);
 
-	/* Handler not deffered during Board charge initialization */
+	/* Handler not deferred during Board charge initialization */
 	board_dc_jack_handler(NULL);
-	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_dc_jack_present));
+
+	if (!dc_jack_is_disabled) {
+		gpio_enable_dt_interrupt(
+			GPIO_INT_FROM_NODELABEL(int_dc_jack_present));
+	}
 }
+
 #ifdef CONFIG_USB_PDC_POWER_MGMT
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(rvp_dc_jack, LOG_LEVEL_INF);
+
 static int board_charge_sys_init(void)
 {
 	board_charge_init();
 	return 0;
 }
 SYS_INIT(board_charge_sys_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+#define DC_JACK_STARTUP_CHECK_INIT_PRIORITY 54
+
+/* Must come after GPIO drivers are ready, and before charge manager */
+BUILD_ASSERT(CONFIG_PLATFORM_EC_GPIO_INIT_PRIORITY <
+	     DC_JACK_STARTUP_CHECK_INIT_PRIORITY);
+BUILD_ASSERT(DC_JACK_STARTUP_CHECK_INIT_PRIORITY <
+	     CONFIG_CHARGE_MANAGER_SYS_INIT_PRIORITY);
+/**
+ * @brief If the DC barrel jack is absent at startup, disable its load switch.
+ *        We cannot currently handle power transitions between PDC and barrel
+ *        jack on RVP boards, so don't allow a barrel jack adapter to be
+ *        connected later.
+ */
+static int run_dc_jack_startup_check(void)
+{
+	if (board_is_dc_jack_present() == false) {
+		dc_jack_is_disabled = true;
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(std_adp_cntrl), 1);
+		LOG_INF("RVP ext power input: PDCs only (no barrel jack)");
+	} else {
+		LOG_INF("RVP ext power input: DC barrel jack only");
+	}
+
+	return 0;
+}
+SYS_INIT(run_dc_jack_startup_check, POST_KERNEL,
+	 DC_JACK_STARTUP_CHECK_INIT_PRIORITY);
+
 #else
 DECLARE_HOOK(HOOK_INIT, board_charge_init, HOOK_PRIO_POST_CHARGE_MANAGER);
 #endif
