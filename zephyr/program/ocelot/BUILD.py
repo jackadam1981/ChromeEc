162fda3eda32eb778de7db68dcea22dcf4bd209f
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2025-09-12 09:27:39 +0800

    matsu: Enable touch panel power sequence control
    Enable touch panel power sequence control
    
    BUG=b:443612246
    TEST=zmake build matsu
    LOW_COVERAGE_REASON=b:444086749 will add test code after bring up.
    
    Change-Id: I2051560d52b44a41be0749736294b4da542d7469

--- zephyr/program/ocelot/BUILD.py
+++ zephyr/program/ocelot/BUILD.py
@@ -165,9 +165,6 @@ register_rtk59_project(
 
 register_it8xxx2_project(
     project_name="matsu",
-    extra_kconfig_base_files=[
-        here / "rvp_program.conf",
-    ],
 )
 
 # Note for reviews, do not let anyone edit these assertions, the addresses
