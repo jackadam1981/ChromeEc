druth0/chrome-ec

commit d0ccc70f315e5885fb3d0f0ce1b480b00afe12d1

body_detection: Support ISH based body detection
Allow the ISH to notify the EC of body detection events. Similarly,
when notified, the EC will notify the AP. The AP notification is
enabled by removing the requirements !body_detect_is_remote(). This
check was previously added to avoid the EC notifying the AP as it was
believed to be possible for the ISH to signal this change directly.
Intel confirmed that this is not possible and that the notification
MUST go through the EC.

BUG=b:414696917
TEST=Run on Uldrenite, see bug for details.

Change-Id: Ie4e90858af53212804516ba7016fb864f0f0de83

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6616939
Tested-by: Yuval Peress <peress@google.com>
Reviewed-by: Keith Short <keithshort@chromium.org>
Auto-Submit: Yuval Peress <peress@google.com>
Code-Coverage: Yuval Peress <peress@google.com>
Commit-Queue: Yuval Peress <peress@google.com>

common/body_detection.c
@@ -158,8 +158,7 @@ void body_detect_change_state(enum body_detect_states state, bool spoof)
 	print_body_detect_mode();
 
 	if (IS_ENABLED(CONFIG_BODY_DETECTION_NOTIFY_MODE_CHANGE) &&
-	    motion_sense_get_ec_config() == SENSOR_CONFIG_EC_S0 &&
-	    !body_detect_is_remote()) {
+	    motion_sense_get_ec_config() == SENSOR_CONFIG_EC_S0) {
 		host_set_single_event(EC_HOST_EVENT_BODY_DETECT_CHANGE);
 	}
 

zephyr/drivers/dsp_comms/client/CMakeLists.txt
@@ -7,6 +7,7 @@ include(${ZEPHYR_PIGWEED_MODULE_DIR}/pw_build/pigweed.cmake)
 pw_add_library(dsp.client STATIC
   SOURCES
     dsp_client.c
+    $<$<BOOL:${CONFIG_PLATFORM_EC_DSP_REMOTE_BODY_DETECTION}>:remote_body_detection.c>
     $<$<BOOL:${CONFIG_PLATFORM_EC_DSP_REMOTE_CBI}>:remote_cbi.c>
     $<$<BOOL:${CONFIG_PLATFORM_EC_DSP_REMOTE_LID_ANGLE}>:remote_lid_angle.c>
     $<$<BOOL:${CONFIG_PLATFORM_EC_DSP_REMOTE_LID_SWITCH}>:remote_lid_switch.c>

zephyr/drivers/dsp_comms/client/remote_body_detection.c
@@ -1,0 +1,56 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <pb_encode.h>
+#include <zephyr/logging/log.h>
+
+#include "body_detection.h"
+#include "cros/dsp/client.h"
+#include "hooks.h"
+#include "proto/ec_dsp.pb.h"
+
+LOG_MODULE_DECLARE(dsp_client, CONFIG_DSP_COMMS_LOG_LEVEL);
+
+static void body_detection_remote_change() {
+  const struct dsp_client_config* cfg = default_client_device->config;
+  struct dsp_client_data* data = default_client_device->data;
+  enum body_detect_states state = body_detect_get_state();
+  cros_dsp_comms_EcService service = {
+      .which_request =
+          cros_dsp_comms_EcService_notify_body_detection_change_tag,
+      .request =
+          {
+              .notify_body_detection_change =
+                  {
+                      .on_body = (state == BODY_DETECTION_ON_BODY),
+                  },
+          },
+  };
+
+  LOG_INF("Waiting for lock to send on-body event");
+  k_mutex_lock(&data->mutex, K_FOREVER);
+  pb_ostream_t stream = pb_ostream_from_buffer(data->request_buffer,
+                                               cros_dsp_comms_EcService_size);
+  bool encode_status =
+      pb_encode(&stream, cros_dsp_comms_EcService_fields, &service);
+
+  if (!encode_status) {
+    LOG_ERR("Failed to encode request");
+    k_mutex_unlock(&data->mutex);
+    return;
+  }
+
+  /* Write the message */
+  LOG_DBG("Writing %zu bytes", stream.bytes_written);
+  int rc = i2c_write_dt(&cfg->i2c, data->request_buffer, stream.bytes_written);
+  if (rc != 0) {
+    LOG_ERR("Failed to send request (%d)", rc);
+  }
+  k_mutex_unlock(&data->mutex);
+}
+
+DECLARE_HOOK(HOOK_BODY_DETECT_CHANGE,
+             body_detection_remote_change,
+             HOOK_PRIO_DEFAULT);

jackadam1981/ChromeEc

commit ff5ecd3851636890f8f5a62f1837b1755682d507

body_detection: Allow dynamic body sensor index
Enable support for dynamic body sensor index in the legacy
body_detection algorithm.

Fixes: d0ccc70f315e ("body_detection: Support ISH based body detection")

BUG=b:435407060
TEST=Check on uldrenite: the sensor is presented as the first one.
     And activity sensor is the second sensor.

Change-Id: I3bf6b0a648024f00c5fdc25ab3e535511bd06ca8

common/body_detection.c
@@ -25,8 +25,7 @@ const static struct body_detect_params default_body_detect_params = {
 	.confidence_delta = CONFIG_BODY_DETECTION_CONFIDENCE_DELTA,
 };
 
-test_export_static struct motion_sensor_t *body_sensor =
-	&motion_sensors[CONFIG_BODY_DETECTION_SENSOR];
+test_export_static struct motion_sensor_t *body_sensor;
 
 static int window_size = CONFIG_BODY_DETECTION_MAX_WINDOW_SIZE;
 test_export_static uint64_t var_threshold_scaled, confidence_delta_scaled;
@@ -218,6 +217,9 @@ static void determine_threshold_scale(int range, int rms_noise,
 
 void body_detect_reset(void)
 {
+	/* Need to refresh body_sensor on reset to make sure it's valid below */
+	body_sensor = &motion_sensors[CONFIG_BODY_DETECTION_SENSOR];
+
 	int odr = body_sensor->drv->get_data_rate(body_sensor);
 	int rms_noise = body_sensor->drv->get_rms_noise(body_sensor);
 	int var_threshold, confidence_delta, var_noise_factor;