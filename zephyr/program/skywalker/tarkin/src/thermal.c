71f4d2cdba41f94779bf6db0c06e0712d3b83fa7
zengyuxiang <zengyuxiang@huaqin.corp-partner.google.com>
2025-09-17 14:49:36 +0800

    tarkin: Use FW_CONFIG to select charging current limit policy
    Based on the thermal team's requirements for changes to the thermal strategy, use FW_CONFIG to differentiate the charging current limit policy between CM1406 and CM1405 & CM1505.
    
    BUG=b:445373349
    TEST=1.zmake build tarkin
    2.the thermal team has verified no impact
    
    Change-Id: I88a44f478541837673124e6739c8728683dfa549

--- zephyr/program/skywalker/tarkin/src/thermal.c
+++ zephyr/program/skywalker/tarkin/src/thermal.c
@@ -8,12 +8,16 @@
 #include "charger.h"
 #include "chipset.h"
 #include "console.h"
+#include "cros_cbi.h"
 #include "extpower.h"
 #include "hooks.h"
 #include "power.h"
 #include "temp_sensor/temp_sensor.h"
 #include "util.h"
 
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(thermal, LOG_LEVEL_INF);
 #define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
 #define CPRINTF(format, args...) cprintf(CC_SYSTEM, format, ##args)
 
@@ -24,6 +28,7 @@ static int thermals[COL_NUM], time[ROW_NUM][COL_NUM];
 static int thermal_cyc;
 static int current = -1;
 static int charger_temp_ave_bef, charger_temp_ave;
+static int thermal_config;
 
 enum {
 	TEMP_ZONE_0, /* not limit */
@@ -65,6 +70,9 @@ static void average_tempature(void)
 	enum power_state chipset_state = power_get_state();
 	static int temperature_increase;
 
+	if (thermal_config == FW_THERMAL_N) {
+		return;
+	}
 	/*
 	 * Keep track of battery temperature range:
 	 *
@@ -171,23 +179,40 @@ static void average_tempature(void)
 }
 DECLARE_HOOK(HOOK_SECOND, average_tempature, HOOK_PRIO_DEFAULT);
 
+static void thermal_ntc_init(void)
+{
+	int ret;
+
+	ret = cros_cbi_get_fw_config(FW_THERMAL, &thermal_config);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FW_THERMAL);
+		return;
+	}
+}
+DECLARE_HOOK(HOOK_INIT, thermal_ntc_init, HOOK_PRIO_POST_FIRST);
+
 int charger_profile_override(struct charge_state_data *curr)
 {
-	/*
-	 * Precharge must be executed when communication is failed on
-	 * dead battery.
-	 */
-	if (!(curr->batt.flags & BATT_FLAG_RESPONSIVE))
-		return 0;
+	if (thermal_config == FW_THERMAL_Y) {
+		/*
+		 * Precharge must be executed when communication is failed on
+		 * dead battery.
+		 */
+		if (!(curr->batt.flags & BATT_FLAG_RESPONSIVE))
+			return 0;
 
-	/* Don't charge if outside of allowable temperature range */
-	if (current == 0) {
-		curr->batt.flags &= ~BATT_FLAG_WANT_CHARGE;
-		if (curr->state != ST_DISCHARGE)
-			curr->state = ST_IDLE;
+		/* Don't charge if outside of allowable temperature range */
+		if (current == 0) {
+			curr->batt.flags &= ~BATT_FLAG_WANT_CHARGE;
+			if (curr->state != ST_DISCHARGE)
+				curr->state = ST_IDLE;
+		}
+		if (current >= 0)
+			curr->requested_current =
+				MIN(curr->requested_current, current);
+
+		return 0;
 	}
-	if (current >= 0)
-		curr->requested_current = MIN(curr->requested_current, current);
 
 	return 0;
 }
