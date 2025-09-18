1407dcfa504a9488858f8751a09109d42c5e8c1c
debasmitha <debasmitha.ghosh@7rays.corp-partner.google.com>
2025-09-18 18:03:58 +0530

    Pujjoga: Added bread crumbs to debug
    Added bread crumbs to check the latency between when TCPC makes
    an update to charger manager and when charger_manager_refresh runs
    
    BUG=b:445132756
    TEST=TEST.PD.PS.SNK.1 PDO Transition - TEST.PD.PS.SNK.1#10
    
    Change-Id: I57a40377665e3107fc1c37cb0fe30b4e32eb511e

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -27,7 +27,9 @@
 #include "usb_pd_dpm_sm.h"
 #include "usb_pd_tcpm.h"
 #include "util.h"
+#include "zephyr/debug/mutex_history.h"
 #include "zephyr/include/usbc/pdc_dpm.h"
+/*#include <zephyr/sys/ring_buffer.h> */
 #ifdef CONFIG_ZEPHYR
 #include "zephyr/include/usbc/pdc_power_mgmt.h"
 #endif
@@ -36,6 +38,8 @@
 #error Mock defined HAS_MOCK_CHARGE_MANAGER
 #endif
 
+MUTEX_HISTORY_DECLARE(mutex_history_rb, 32);
+
 #define CPRINTS(format, args...) cprints(CC_USBCHARGE, format, ##args)
 
 #define POWER(charge_port) ((charge_port.current) * (charge_port.voltage))
@@ -43,6 +47,8 @@
 /* Timeout for delayed override power swap, allow for 500ms extra */
 #define POWER_SWAP_TIMEOUT \
 	(PD_T_SRC_RECOVER_MAX + PD_T_SRC_TURN_ON + PD_T_SAFE_0V + 500 * MSEC)
+
+/*extern struct ring_buf mutex_history_rb; */
 
 /*
  * Default charge supplier priority
@@ -949,6 +955,10 @@ static void charge_manager_get_best_port(int *new_port, int *new_supplier)
  */
 static void charge_manager_refresh(void)
 {
+	MUTEX_HISTORY_LOG_CRUMB(
+		&mutex_history_rb,
+		"charge_manager_refresh started"); /* charge_manager_refresh
+		     started */
 	/* Always initialize charge port on first pass */
 	static int active_charge_port_initialized;
 	int new_supplier, new_port;
@@ -1204,6 +1214,13 @@ static void charge_manager_refresh(void)
 	}
 
 	CM_MUTEX_UNLOCK(&cm_refresh);
+
+	MUTEX_HISTORY_LOG_CRUMB(
+		&mutex_history_rb,
+		"charge_manager_refresh ended"); /* charge_manager_refresh
+		     ended */
+
+	mutex_history_dump(&mutex_history_rb);
 }
 DECLARE_DEFERRED(charge_manager_refresh);
 
--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -40,6 +40,7 @@
 #include "usb_tc_sm.h"
 #include "usbc_ppc.h"
 #include "util.h"
+#include "zephyr/debug/mutex_history.h"
 
 /*
  * USB Policy Engine Sink / Source module
@@ -181,6 +182,8 @@
  * function defined in the board's usb_pd_policy.c file.
  */
 typedef int (*svdm_rsp_func)(int port, uint32_t *payload);
+
+extern struct ring_buf mutex_history_rb;
 
 /* List of all Policy Engine level states */
 enum usb_pe_state {
@@ -3520,6 +3523,10 @@ static void pe_snk_wait_for_capabilities_exit(int port)
  */
 static void pe_snk_evaluate_capability_entry(int port)
 {
+	MUTEX_HISTORY_LOG_CRUMB(&mutex_history_rb,
+				"contract negotiation started"); /* contract
+				    negotiation started */
+
 	uint32_t *pdo = (uint32_t *)rx_emsg[port].buf;
 	uint32_t num = rx_emsg[port].len >> 2;
 
@@ -3658,10 +3665,19 @@ static void pe_snk_apply_transition_current(int port)
 		current_limit = PD_MIN_MA;
 	}
 
-	if (current_limit == 0)
+	if (current_limit == 0) {
 		charge_manager_invalidate_suppliers(port);
-	else
-		charge_manager_force_ceil(port, current_limit);
+		MUTEX_HISTORY_LOG_CRUMB(&mutex_history_rb,
+					"0A contract accepted"); /* 0A contract
+									accepted
+								  */
+	}
+	charge_manager_force_ceil(port, current_limit);
+	MUTEX_HISTORY_LOG_CRUMB(&mutex_history_rb,
+				"Non-0A contract accepted"); /* Non-0A
+								contract
+								accepted
+							  */
 }
 
 static void pe_snk_select_capability_run(int port)
