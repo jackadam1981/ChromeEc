c9684ec3231b50ae796a8e836fd963cbe7908807
Firas Sammoura <fsammoura@google.com>
2025-09-18 22:42:12 +0000

    zephyr: shim: riscv: Add API to control write permission for custom PMP entry
    Implement the `riscv_pmp_set_write_permission()` API, which allows the
    'W' bit in the PMP configuration register (pmpcfg) to be dynamically
    enabled or disabled for a memory region defined by
    CUSTOM_PMP_ENTRY_START.
    
    This functionality is crucial for security platforms to protect critical
    flash areas, such as rollback data, against accidental or malicious
    writes.
    
    Also adds the Kconfig symbol `PLATFORM_EC_ROLLBACK_PMP_PROTECT` which
    depends on `RISCV_PMP` and ensures `CUSTOM_PMP_ENTRY` is selected when
    rollback protection is desired. The current implementation is limited to
    PMP slots 0-7.
    
    BUG=b:406946889
    TEST=CQ
    
    Change-Id: I4b93f3f754411c36675901f55b9992b1f5db74de

---
+++ zephyr/shim/core/riscv/pmp.c
@@ -0,0 +1,122 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <errno.h>
+
+#include <zephyr/arch/riscv/csr.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(shim_pmp, LOG_LEVEL_ERR);
+
+#define PMP_CFG_W_BIT 1 // Write permission bit in the PMP config byte
+#define PMPCFG_STRIDE sizeof(unsigned long)
+
+/**
+ * @brief Read the content of a specific PMP Address Register (pmpaddrX).
+ *
+ * This helper function provides a way to read the memory address configuration
+ * from one of the first 8 PMP address registers (pmpaddr0 through pmpaddr7).
+ *
+ * @param index The PMP entry index (0-7) corresponding to the desired register.
+ * @return The 64-bit value (address/length) stored in the pmpaddrX CSR.
+ */
+static inline unsigned long read_pmpaddr(uint8_t index)
+{
+	switch (index) {
+	case 0:
+		return csr_read(pmpaddr0);
+	case 1:
+		return csr_read(pmpaddr1);
+	case 2:
+		return csr_read(pmpaddr2);
+	case 3:
+		return csr_read(pmpaddr3);
+	case 4:
+		return csr_read(pmpaddr4);
+	case 5:
+		return csr_read(pmpaddr5);
+	case 6:
+		return csr_read(pmpaddr6);
+	case 7:
+		return csr_read(pmpaddr7);
+	default:
+		return 0;
+	}
+}
+
+int riscv_pmp_set_write_permission(bool write_enable)
+{
+	if (CONFIG_PMP_SLOTS > 8) {
+		LOG_ERR("This function only supports up to 8 PMP slots.");
+		return -ENOTSUP;
+	}
+
+	int entry_index = -1;
+	uintptr_t target_pmpaddr = CUSTOM_PMP_ENTRY_START >> 2;
+
+	for (uint8_t i = 0; i < CONFIG_PMP_SLOTS; ++i) {
+		if (read_pmpaddr(i) == target_pmpaddr) {
+			entry_index = i;
+			break;
+		}
+	}
+
+	if (entry_index == -1) {
+		LOG_ERR("PMP entry for address 0x%x not found",
+			CUSTOM_PMP_ENTRY_START);
+		return -ENOENT;
+	}
+
+	uint8_t cfg_reg_idx = entry_index / PMPCFG_STRIDE;
+	uint8_t entry_in_reg = entry_index % PMPCFG_STRIDE;
+	int bit_position = (entry_in_reg * 8) + PMP_CFG_W_BIT;
+	unsigned long mask = 1UL << bit_position;
+
+	unsigned long pmpcfg_val;
+
+#if defined(CONFIG_64BIT)
+	// RV64: pmpcfg0 holds configs for entries 0-7
+	// pmpcfg registers are pmpcfg0, pmpcfg2, pmpcfg4, ...
+	if (cfg_reg_idx == 0) { // Entries 0-7 are in pmpcfg0
+		pmpcfg_val = csr_read(pmpcfg0);
+		if (write_enable) {
+			pmpcfg_val |= mask;
+		} else {
+			pmpcfg_val &= ~mask;
+		}
+		csr_write(pmpcfg0, pmpcfg_val);
+	} else {
+		LOG_ERR("cfg_reg_idx %d unexpected for <= 8 slots on RV64",
+			cfg_reg_idx);
+		return -EINVAL;
+	}
+#else
+	// RV32: pmpcfg0 for entries 0-3, pmpcfg1 for entries 4-7
+	// pmpcfg registers are pmpcfg0, pmpcfg1, pmpcfg2, ...
+	if (cfg_reg_idx == 0) { // Entries 0-3
+		pmpcfg_val = csr_read(pmpcfg0);
+		if (write_enable) {
+			pmpcfg_val |= mask;
+		} else {
+			pmpcfg_val &= ~mask;
+		}
+		csr_write(pmpcfg0, pmpcfg_val);
+	} else if (cfg_reg_idx == 1) { // Entries 4-7
+		pmpcfg_val = csr_read(pmpcfg1);
+		if (write_enable) {
+			pmpcfg_val |= mask;
+		} else {
+			pmpcfg_val &= ~mask;
+		}
+		csr_write(pmpcfg1, pmpcfg_val);
+	} else {
+		LOG_ERR("cfg_reg_idx %d unexpected for <= 8 slots on RV32",
+			cfg_reg_idx);
+		return -EINVAL;
+	}
+#endif
+
+	return 0;
+}
