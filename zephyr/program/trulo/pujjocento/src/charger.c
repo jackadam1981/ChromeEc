523495af4850c2e856b02f57593c4a828591c090
guojun <guojun@huaqin.corp-partner.google.com>
2025-09-18 17:21:45 +0800

    test: Modify charge option when AC only
    Disable charger IDPM function when AC only to prevent boot fail,
    enable charger IDPM when insert battery.
    
    BUG=None
    TEST=None
    
    Change-Id: I85a2cba61d69ca232421495202bf903e38f9d7db

--- zephyr/program/trulo/pujjocento/src/charger.c
+++ zephyr/program/trulo/pujjocento/src/charger.c
@@ -4,6 +4,9 @@
  */
 
 #include "charger.h"
+#include "common.h"
+#include "gpio/gpio_int.h"
+#include "gpio_signal.h"
 #include "hooks.h"
 #include "i2c.h"
 #include "system.h"
@@ -45,3 +48,38 @@ static void set_bq25710_charge_option(void)
 	}
 }
 DECLARE_HOOK(HOOK_INIT, set_bq25710_charge_option, HOOK_PRIO_DEFAULT);
+
+static void set_bq25710_charge_option_1(void)
+{
+	int reg;
+	int rv;
+
+	rv = i2c_read16(chg_chips[0].i2c_port, chg_chips[0].i2c_addr_flags,
+			BQ25710_REG_CHARGE_OPTION_0, &reg);
+	if (rv == EC_SUCCESS) {
+		/* if AC only, disable IDPM,
+		 * because it will cause charger keep asserting PROCHOT
+		 */
+		if (gpio_pin_get_dt(
+			    GPIO_DT_FROM_NODELABEL(gpio_ec_batt_pres_odl)))
+			reg = SET_BQ_FIELD(BQ257X0, CHARGE_OPTION_0, EN_IDPM, 0,
+					   reg);
+		else
+			reg = SET_BQ_FIELD(BQ257X0, CHARGE_OPTION_0, EN_IDPM, 1,
+					   reg);
+		i2c_write16(chg_chips[0].i2c_port, chg_chips[0].i2c_addr_flags,
+			    BQ25710_REG_CHARGE_OPTION_0, reg);
+	}
+}
+DECLARE_DEFERRED(set_bq25710_charge_option_1);
+
+void batt_pres_interrupt(enum gpio_signal signal)
+{
+	hook_call_deferred(&set_bq25710_charge_option_1_data, 2 * USEC_PER_SEC);
+}
+
+static void batt_pres_en_init(void)
+{
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_batt_pres_en));
+}
+DECLARE_HOOK(HOOK_INIT, batt_pres_en_init, HOOK_PRIO_DEFAULT);
