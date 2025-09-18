92f0e1d38d177e1aec3368ef1c1b545312208c19
debasmitha <debasmitha.ghosh@7rays.corp-partner.google.com>
2025-09-18 18:03:58 +0530

    Pujjoga: Added bread crumbs to debug
    Added bread crumbs to check the latency between when TCPC makes
    an update to charger manager and when charger_manager_refresh runs
    
    BUG=b:445132756
    TEST=TEST.PD.PS.SNK.1 PDO Transition - TEST.PD.PS.SNK.1#10
    
    Change-Id: I57a40377665e3107fc1c37cb0fe30b4e32eb511e

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -40,6 +40,7 @@
 #include "usb_tc_sm.h"
 #include "usbc_ppc.h"
 #include "util.h"
+#include "zephyr/debug/mutex_history.h"
 
 /*
  * USB Policy Engine Sink / Source module
@@ -181,6 +182,8 @@
  * function defined in the board's usb_pd_policy.c file.
  */
 typedef int (*svdm_rsp_func)(int port, uint32_t *payload);
+
+extern struct ring_buf mutex_history_rb;
 
 /* List of all Policy Engine level states */
 enum usb_pe_state {
@@ -3520,6 +3523,10 @@ static void pe_snk_wait_for_capabilities_exit(int port)
  */
 static void pe_snk_evaluate_capability_entry(int port)
 {
+	MUTEX_HISTORY_LOG_CRUMB(&mutex_history_rb,
+				"contract negotiation started"); /* contract
+				    negotiation started */
+
 	uint32_t *pdo = (uint32_t *)rx_emsg[port].buf;
 	uint32_t num = rx_emsg[port].len >> 2;
 
@@ -3851,6 +3858,14 @@ static void pe_snk_transition_sink_run(int port)
 			 * PE_SNK_Select_Capability has already applied that
 			 * limit.
 			 */
+			if (pe[port].curr_limit == 0) {
+				MUTEX_HISTORY_LOG_CRUMB(
+					&mutex_history_rb,
+					"0A contract accepted"); /* 0A contract
+								    accepted */
+				mutex_history_dump(&mutex_history_rb);
+			}
+
 			if (pe[port].curr_limit != 0) {
 				/*
 				 * Per PD r3.1 v1.8 ss 8.3.3.3.6, the PE should
