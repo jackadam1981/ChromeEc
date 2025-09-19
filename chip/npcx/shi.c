f0b9bde6d7bb9885d788c43e163b7cacf6932ff8
Firas Sammoura <fsammoura@google.com>
2025-09-19 17:24:55 +0000

    npcx/shi: Reduce log level for bad received data prints and improve latency
    The diagnostic prints within `shi_bad_received_data()` can generate
    excessive log spam and introduce unnecessary latency, especially when
    a bad transaction triggers them frequently.
    
    Change the `CPRINTF` calls to `DEBUG_CPRINTF` to conditionally compile
    these messages, removing the logging overhead from production and
    standard debug builds. This improves **performance** and **reduces
    latency** during error handling.
    
    BUG=b:444087489
    TEST=execute "reboot" for 100 iterations on Pujjoquince console; make
         sure there are 100 "HC 0x011d err 1" on the EC console after
         sysjump to RW.
    
    Change-Id: Ie42a1c4219d5c79abc75edbd151c42b1920a80cb

--- chip/npcx/shi.c
+++ chip/npcx/shi.c
@@ -569,11 +569,11 @@ static void shi_bad_received_data(void)
 	shi_fill_out_status(EC_SPI_RX_BAD_DATA);
 	state = SHI_STATE_BAD_RECEIVED_DATA;
 
-	CPRINTF("BAD-");
-	CPRINTF("in_msg=[");
+	DEBUG_CPRINTF("BAD-");
+	DEBUG_CPRINTF("in_msg=[");
 	for (i = 0; i < shi_params.sz_received; i++)
-		CPRINTF("%02x ", in_msg[i]);
-	CPRINTF("]\n");
+		DEBUG_CPRINTF("%02x ", in_msg[i]);
+	DEBUG_CPRINTF("]\n");
 
 	/* Reset shi's state machine for error recovery */
 	shi_reset_prepare();
@@ -589,7 +589,7 @@ static int last_error_state = -1;
 
 static void log_unexpected_state(char *isr_name)
 {
-#if !(DEBUG_SHI)
+#if DEBUG_SHI
 	if (state != last_error_state)
 		CPRINTS("Unexpected state %d in %s ISR", state, isr_name);
 #endif
