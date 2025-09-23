5defa95543af723cb89ec2923262106056a1d443
Logan_Liao <Logan_Liao@compal.corp-partner.google.com>
2025-09-23 11:42:57 +0800

    uldrenite: Add a specific check for 180 SKU
    Discovered that the battery cut-off hotkey combination fails on uldrenite.
    The root cause is that after an EC reset, the EC incorrectly identifies the device as being in tablet mode and locks the keyboard. This prevents the refresh key press from being detected.
    This patch modifies the keyboard locking behavior for the 180 SKU to resolve the issue.
    
    BUG=b:446605593
    TEST=cut off success on 180SKU with hot key
    
    Change-Id: I8e9f04af458dd84acf727975eb90e41ac6af36f4

--- zephyr/program/trulo/uldrenite/src/sensor.c
+++ zephyr/program/trulo/uldrenite/src/sensor.c
@@ -13,6 +13,7 @@
 #include "driver/accelgyro_lsm6dsm.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
+#include "keyboard_scan.h"
 #include "motion_sense.h"
 #include "motionsense_sensors.h"
 #include "tablet_mode.h"
@@ -126,3 +127,33 @@ static void alt_sensor_init(void)
 	motion_sensors_check_ssfc();
 }
 DECLARE_HOOK(HOOK_INIT, alt_sensor_init, HOOK_PRIO_POST_I2C);
+
+__override void lid_angle_peripheral_enable(int enable)
+{
+	int chipset_in_s0 = chipset_in_state(CHIPSET_STATE_ON);
+
+	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL)
+		return;
+	/*
+	 * If the lid is in tabletmode and is suspended, ignore the lid
+	 * angle, which might be faulty, then disable keyboard. This
+	 * could be a scenario where convertibles with lid open are in
+	 * tabletmode and system is suspended.
+	 */
+	if (IS_ENABLED(CONFIG_TABLET_MODE) && tablet_get_mode()) {
+		enable = 0;
+	}
+
+	if (enable) {
+		keyboard_scan_enable(1, KB_SCAN_DISABLE_LID_ANGLE);
+	} else {
+		/*
+		 * Ensure that the chipset is off before disabling the keyboard.
+		 * When the chipset is on, the EC keeps the keyboard enabled and
+		 * the AP decides whether to ignore input devices or not.
+		 */
+		if (!chipset_in_s0) {
+			keyboard_scan_enable(0, KB_SCAN_DISABLE_LID_ANGLE);
+		}
+	}
+}
