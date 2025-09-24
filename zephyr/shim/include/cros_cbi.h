4e4dfd98e8b4b502e064be6a37e936b496195d47
Wai-Hong Tam <waihong@google.com>
2025-09-24 11:52:31 -0700

    cbi: Optimize code size for fw_config and ssfc parsers
    This change optimizes the binary size by conditionally compiling the
    fw_config and ssfc parsers only when they are actually used by a board's
    devicetree.
    
    This is achieved by introducing new Kconfig options
    (CROS_EC_CBI_FW_CONFIG_PARSER and CROS_EC_CBI_SSFC_PARSER), which are
    enabled by default if a corresponding devicetree node is present.
    
    The CMakeLists.txt file for the CBI module now uses these flags to
    conditionally compile each parser's source.
    
    The cros_cbi.c initializer conditionally calls the init function for
    each parser.
    
    The cros_cbi.h header provides static inline stub functions for the
    public APIs to prevent linker errors.
    
    BUG=b:446733213
    TEST=./twister -v -T zephyr/test/drivers
    
    Change-Id: Ie7434af9c868f333f23323735dcd5502e08a158a

--- zephyr/shim/include/cros_cbi.h
+++ zephyr/shim/include/cros_cbi.h
@@ -84,28 +84,14 @@ enum cbi_fw_config_value_id {
 };
 /* clang-format on */
 
-/**
- * @brief Initialize CBI SSFC
- *
- * The function has to be called before checking SSFC values.
- */
-void cros_cbi_ssfc_init(void);
+#ifdef CONFIG_CROS_EC_CBI_FW_CONFIG_PARSER
 
 /**
- * @brief Initialize CBI FW
+ * @brief Initialize CBI FW_CONFIG
  *
  * The function has to be called before getting CBI FW_CONFIG.
  */
 void cros_cbi_fw_config_init(void);
-
-/**
- * @brief Check if the CBI SSFC value matches the one in the EEPROM
- *
- * @param value_id The SSFC value to check in EEPROM.
- *
- * @return true If matches, false if not.
- */
-bool cros_cbi_ssfc_check_match(enum cbi_ssfc_value_id value_id);
 
 /**
  * @brief Retrieve the value of the FW_CONFIG field
@@ -119,6 +105,54 @@ bool cros_cbi_ssfc_check_match(enum cbi_ssfc_value_id value_id);
  */
 int cros_cbi_get_fw_config(enum cbi_fw_config_field_id field_id,
 			   uint32_t *value);
+
+#else /* !CONFIG_CROS_EC_CBI_FW_CONFIG_PARSER */
+
+static inline void cros_cbi_fw_config_init(void)
+{
+}
+
+static inline int cros_cbi_get_fw_config(enum cbi_fw_config_field_id field_id,
+					 uint32_t *value)
+{
+	ARG_UNUSED(field_id);
+	ARG_UNUSED(value);
+	return -ENODEV;
+}
+
+#endif /* CONFIG_CROS_EC_CBI_FW_CONFIG_PARSER */
+
+#ifdef CONFIG_CROS_EC_CBI_SSFC_PARSER
+
+/**
+ * @brief Initialize CBI SSFC
+ *
+ * The function has to be called before checking SSFC values.
+ */
+void cros_cbi_ssfc_init(void);
+
+/**
+ * @brief Check if the CBI SSFC value matches the one in the CBI
+ *
+ * @param value_id The SSFC value to check in CBI.
+ *
+ * @return true If matches, false if not.
+ */
+bool cros_cbi_ssfc_check_match(enum cbi_ssfc_value_id value_id);
+
+#else /* !CONFIG_CROS_EC_CBI_SSFC_PARSER */
+
+static inline void cros_cbi_ssfc_init(void)
+{
+}
+
+static inline bool cros_cbi_ssfc_check_match(enum cbi_ssfc_value_id value_id)
+{
+	ARG_UNUSED(value_id);
+	return false;
+}
+
+#endif /* CONFIG_CROS_EC_CBI_SSFC_PARSER */
 
 #ifdef CONFIG_ZTEST
 /**
