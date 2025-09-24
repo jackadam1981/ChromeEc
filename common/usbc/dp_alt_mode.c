b40cf1080e7eb7a0e4cc7bb04e64a64d3888e7e9
Kamil Pluciński <kamilplucinski@google.com>
2025-09-24 04:21:23 -0700

    dp_alt_mode: exit dp mode on various DP status conditions
    Various DP status bits mean we need to exit DP mode.
     - if the DP UFP wants to exit, we need to exit.
     - if the DP UFP has disconnected, we need to exit.
     - if HPD goes low, and IRQ is low we need to exit.
    
    BUG=b/419116670
    TEST=Not tested yet
    
    Change-Id: I769ad1607abc529787b2e83dcbc5937ab5cbeac2

--- common/usbc/dp_alt_mode.c
+++ common/usbc/dp_alt_mode.c
@@ -655,8 +655,9 @@ __overridable int svdm_dp_attention(int port, uint32_t *payload)
 	int lvl = PD_VDO_DPSTS_HPD_LVL(payload[1]);
 	int irq = PD_VDO_DPSTS_HPD_IRQ(payload[1]);
 	mux_state_t mux_state;
+	uint32_t status = payload[1];
 
-	dp_status[port] = payload[1];
+	dp_status[port] = status;
 
 	if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND) && (irq || lvl))
 		/*
@@ -670,6 +671,15 @@ __overridable int svdm_dp_attention(int port, uint32_t *payload)
 	if (!(dp_flags[port] & DP_FLAGS_DP_ON)) {
 		if (lvl)
 			dp_flags[port] |= DP_FLAGS_HPD_HI_PENDING;
+		return 1;
+	}
+
+	if (status & DP_STATUS_USB_CONFIG_REQ) {
+		/*
+		 * UFP is requesting a change to the USB/DP pin configuration.
+		 * This is handled by re-sending the DP_Configure message.
+		 */
+		dp_state[port] = DP_PREPARE_CONFIG;
 		return 1;
 	}
 
