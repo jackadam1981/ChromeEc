6e8f15364ae69c7ba3cd88c361942193bf0d07c9
Paul Liang <paul.liang@emc.com.tw>
2025-10-02 16:09:39 +0800

    cros_flash: Implementation of basic functions of elan em32 EC cros_flash driver.
    This patch is based on CL 6781020
    (https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6781020).
    Three api functions are implemented to cros_flash_elan driver:
    init(), physical_erase(), and physical_write().
    To make these functions work correctly, flash_em32 zehpyr driver is needed.
    
    Change-Id: I27bd8f089a3cb9b1a87924a6356c990e7f8578b2

---
+++ zephyr/drivers/cros_flash/cros_flash_elan.c
@@ -0,0 +1,165 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#define DT_DRV_COMPAT elan_em32f967_cros_flash
+
+#include "flash.h"
+
+#include <zephyr/drivers/flash.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/kernel.h>
+
+#include <drivers/cros_flash.h>
+#include <soc.h>
+
+#define LOG_LEVEL LOG_LEVEL_INFO
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(cros_flash);
+// LOG_MODULE_REGISTER(cros_flash, LOG_LEVEL_ERR);
+
+#if !DT_NODE_EXISTS(DT_CHOSEN(zephyr_flash_controller))
+#error "No suitable devicetree overlay specified for zephyr_flash_controller"
+#endif
+
+/* Device Configuration */
+struct cros_flash_em32_config {
+	const struct device *flash_dev;
+};
+#define DRV_CONFIG(dev) ((const struct cros_flash_em32_config *)(dev)->config)
+
+#define FLASH_DEV DT_CHOSEN(zephyr_flash_controller)
+static const struct cros_flash_em32_config cros_flash_config = {
+	.flash_dev = DEVICE_DT_GET(FLASH_DEV),
+};
+
+/* cros ec flash api functions */
+static int cros_flash_em32_init(const struct device *dev)
+{
+	LOG_INF("cros_flash_em32_init.");
+
+	return 0;
+}
+
+static int cros_flash_em32_write(const struct device *dev, int offset, int size,
+				 const char *src_data)
+{
+	const struct cros_flash_em32_config *cfg = DRV_CONFIG(dev);
+	int ret = 0;
+
+	LOG_INF("cros_flash_em32_write.");
+
+	/* Lock physical flash operations */
+	crec_flash_lock_mapped_storage(1);
+
+	LOG_INF("cros_flash_em32_write 0x%x 0x%x.", offset, size);
+	ret = flash_write(cfg->flash_dev, offset, src_data, size);
+
+	/* Unlock physical flash operations */
+	crec_flash_lock_mapped_storage(0);
+
+	return ret;
+}
+
+static int cros_flash_em32_erase(const struct device *dev, int offset, int size)
+{
+	const struct cros_flash_em32_config *cfg = DRV_CONFIG(dev);
+	int ret = 0;
+
+	LOG_INF("cros_flash_em32_erase.");
+
+	/* Lock physical flash operations */
+	crec_flash_lock_mapped_storage(1);
+
+	/* Always use page erase command */
+	for (; size > 0; size -= CONFIG_FLASH_ERASE_SIZE) {
+		LOG_INF("cros_flash_em32_erase 0x%x 0x%x.", offset,
+			CONFIG_FLASH_ERASE_SIZE);
+		ret = flash_erase(cfg->flash_dev, offset,
+				  CONFIG_FLASH_ERASE_SIZE);
+		if (ret) {
+			break;
+		}
+
+		offset += CONFIG_FLASH_ERASE_SIZE;
+	}
+
+	/* Unlock physical flash operations */
+	crec_flash_lock_mapped_storage(0);
+
+	return 0;
+}
+
+static int cros_flash_em32_get_protect(const struct device *dev, int bank)
+{
+	// LOG_INF("cros_flash_em32_get_protect.");
+	return 0;
+}
+
+static uint32_t cros_flash_em32_get_protect_flags(const struct device *dev)
+{
+	// LOG_INF("cros_flash_em32_get_protect_flags.");
+	return 0;
+}
+
+static int cros_flash_em32_protect_at_boot(const struct device *dev,
+					   uint32_t new_flags)
+{
+	LOG_INF("cros_flash_em32_protect_at_boot.");
+	return 0;
+}
+
+static int cros_flash_em32_protect_now(const struct device *dev, bool all)
+{
+	LOG_INF("cros_flash_em32_protect_now.");
+	return 0;
+}
+
+static int cros_flash_em32_get_jedec_id(const struct device *dev,
+					uint8_t *manufacturer, uint16_t *device)
+{
+	LOG_INF("cros_flash_em32_get_jedec_id.");
+	return 0;
+}
+
+static int cros_flash_em32_get_status(const struct device *dev, uint8_t *sr1,
+				      uint8_t *sr2)
+{
+	LOG_INF("cros_flash_em32_get_status.");
+	return 0;
+}
+
+/* cros ec flash driver registration */
+static DEVICE_API(cros_flash, cros_flash_em32_driver_api) = {
+	.init = cros_flash_em32_init,
+	.physical_write = cros_flash_em32_write,
+	.physical_erase = cros_flash_em32_erase,
+	.physical_get_protect = cros_flash_em32_get_protect,
+	.physical_get_protect_flags = cros_flash_em32_get_protect_flags,
+	.physical_protect_at_boot = cros_flash_em32_protect_at_boot,
+	.physical_protect_now = cros_flash_em32_protect_now,
+	.physical_get_jedec_id = cros_flash_em32_get_jedec_id,
+	.physical_get_status = cros_flash_em32_get_status,
+};
+
+static int flash_em32_init(const struct device *dev)
+{
+	const struct cros_flash_em32_config *cfg = DRV_CONFIG(dev);
+
+	LOG_INF("flash_em32_init.");
+
+	if (!device_is_ready(cfg->flash_dev)) {
+		LOG_ERR("Flash device %s not ready", cfg->flash_dev->name);
+		return -ENODEV;
+	}
+
+	return EC_SUCCESS;
+}
+
+BUILD_ASSERT(CONFIG_FLASH_INIT_PRIORITY < CONFIG_CROS_FLASH_INIT_PRIORITY);
+
+DEVICE_DT_INST_DEFINE(0, flash_em32_init, NULL, NULL, &cros_flash_config,
+		      POST_KERNEL, CONFIG_CROS_FLASH_INIT_PRIORITY,
+		      &cros_flash_em32_driver_api);
