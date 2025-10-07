b6f23c8b5acc77c77387c6e42d47d313cab30e5b
Andrey Pronin <apronin@google.com>
2025-10-06 19:13:29 -0700

    boot_param: enable stage2 parameters in DICE CfgDescr
    This CL enables including stage2 parameters in CfgDescr structure
    included in DICE CDI cert and adds GSC type constants.
    
    BUG=b:438579760
    TEST=pass boot_param_test, contain correct values
    
    Change-Id: I958741793b4f63cb1e6e4a5d4127afa43154614b

--- boot_param/boot_param_types.h
+++ boot_param/boot_param_types.h
@@ -29,7 +29,7 @@ extern "C" {
 #define DICE_ID_HEX_BYTES (DICE_ID_BYTES * 2)
 
 /* Format of Cfg Descriptor used for BootParam CDI */
-#define BOOT_PARAM_CFG_DESCR_STAGE 1
+#define BOOT_PARAM_CFG_DESCR_STAGE 2
 
 #if BOOT_PARAM_CFG_DESCR_STAGE == 1
 #define BOOT_PARAM_CFG_DESCR_MAP_COUNT 6
@@ -86,6 +86,12 @@ struct dice_config_s {
 	/* GSC type */
 	uint8_t gsc_type;
 };
+
+#define BOOT_PARAM_GSC_TYPE_H1B3X       0x00
+#define BOOT_PARAM_GSC_TYPE_H1D3C       0x04
+#define BOOT_PARAM_GSC_TYPE_NT11A1      0x10
+#define BOOT_PARAM_GSC_TYPE_NT11A2      0x11
+#define BOOT_PARAM_GSC_TYPE_UNKNOWN     0xFF
 
 #ifdef __cplusplus
 } /* extern "C" */
