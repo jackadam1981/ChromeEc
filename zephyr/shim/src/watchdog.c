8a5e3b4c5846bce8f6ce7b717c3ed8b58cb5abee
Al Semjonovs <asemjonovs@google.com>
2025-10-08 13:59:01 -0600

    watchdog: print call stack upon watchdog
    Add config to dump each thread's call stack upon watchdog.
    See http://gpaste/4867098841055232 for symbolized output
    
    BUG=None
    TEST=EC Console: crash watchdog
    
    Change-Id: Ie9c8624fe61cefa82b4280be0e77adab4b934d9b

--- zephyr/shim/src/watchdog.c
+++ zephyr/shim/src/watchdog.c
@@ -66,10 +66,7 @@ const struct watchdog_info wdt_info[] = {
 /* Array to keep channel used to implement watchdog */
 int wdt_chan[ARRAY_SIZE(wdt_info)];
 bool watchdog_initialized;
-
-#ifdef TEST_BUILD
 bool wdt_warning_triggered;
-#endif /* TEST_BUILD */
 
 static int watchdog_config(const struct watchdog_info *info)
 {
@@ -157,6 +154,16 @@ void watchdog_reload(void)
 }
 DECLARE_HOOK(HOOK_TICK, watchdog_reload, HOOK_PRIO_DEFAULT);
 
+static void get_thread_name(const struct k_thread *thread, char *name,
+			    size_t size)
+{
+#ifdef CONFIG_THREAD_NAME
+	snprintf(name, size, "%s", thread->name);
+#else
+	snprintf(name, size, "TASK_%d", thread_id_to_task_id((k_tid_t)thread));
+#endif
+}
+
 static uint32_t get_stack_ptr(const struct k_thread *thread)
 {
 #if defined(CONFIG_ARM64)
@@ -177,18 +184,13 @@ static uint32_t get_stack_ptr(const struct k_thread *thread)
 #endif
 }
 
-static void log_thread_info(const struct k_thread *thread, void *user_data)
+static void print_sp_pc(const struct k_thread *thread)
 {
 	uint32_t sp = get_stack_ptr(thread);
 	struct arch_esf *esf = (struct arch_esf *)sp;
 	char thread_name[16];
 
-#ifdef CONFIG_THREAD_NAME
-	snprintf(thread_name, sizeof(thread_name), "%s", thread->name);
-#else
-	snprintf(thread_name, sizeof(thread_name), "TASK_ID: %d",
-		 thread_id_to_task_id((k_tid_t)thread));
-#endif
+	get_thread_name(thread, thread_name, sizeof(thread_name));
 
 #if defined(CONFIG_ARM)
 	printk("%s [SP=%p, PC=%p, LR=%p]\n", thread_name, (void *)sp,
@@ -208,6 +210,55 @@ static void log_thread_info(const struct k_thread *thread, void *user_data)
 	/* Nothing useful within esf to be printed here */
 	ARG_UNUSED(esf);
 #endif
+}
+
+static bool print_trace_address(void *arg, unsigned long pc)
+{
+	int *frame_idx = (int *)arg;
+#ifdef CONFIG_SYMTAB
+	uint32_t offset = 0;
+	const char *name = symtab_find_symbol_name(pc, &offset);
+
+	printk(" #%d: %p [%s+0x%x]\n", *frame_idx, (void *)pc, name, offset);
+#else
+	printk(" #%d: %p\n", *frame_idx, (void *)pc);
+#endif
+
+	(*frame_idx)++;
+	return true;
+}
+
+static void print_stack_trace(const struct k_thread *thread)
+{
+	int frame_idx = 0;
+	char state[32];
+	uint32_t sp = 0;
+	struct arch_esf *esf = NULL;
+	bool current_thread = thread == k_current_get();
+	char thread_name[16];
+
+	get_thread_name(thread, thread_name, sizeof(thread_name));
+
+	printk("Thread: %s%s, state=%s\n", current_thread ? "*" : "",
+	       thread_name,
+	       k_thread_state_str((k_tid_t)thread, state, sizeof(state)));
+
+	/* Pass esf if this is the currently interrupted thread */
+	if (current_thread) {
+		sp = get_stack_ptr(thread);
+		esf = (struct arch_esf *)sp;
+	} else {
+		esf = NULL;
+	}
+	arch_stack_walk(print_trace_address, &frame_idx, thread, esf);
+}
+
+static void log_thread_info(const struct k_thread *thread, void *user_data)
+{
+	print_sp_pc(thread);
+	if (IS_ENABLED(CONFIG_PLATFORM_EC_WATCHDOG_DUMP_THREADS_CALL_STACK)) {
+		print_stack_trace(thread);
+	}
 }
 
 __maybe_unused static void wdt_warning_handler(const struct device *wdt_dev,
@@ -242,9 +293,16 @@ __maybe_unused static void wdt_warning_handler(const struct device *wdt_dev,
 	printk("Watchdog deadline is close! TASK_ID:%d THREAD_NAME:%s\n",
 	       task_id, thread_name);
 #endif
-#ifdef TEST_BUILD
+
+	if (!wdt_warning_triggered) {
+		/*
+		 * Reload watchdog on first instance of warning to make sure all
+		 * debug logging is captured.
+		 */
+		watchdog_reload();
+	}
 	wdt_warning_triggered = true;
-#endif
+
 #ifdef CONFIG_SOC_SERIES_MEC172X
 	extern void cros_chip_wdt_handler(const struct device *wdt_dev,
 					  int channel_id);
@@ -252,7 +310,7 @@ __maybe_unused static void wdt_warning_handler(const struct device *wdt_dev,
 #endif
 
 	if (IS_ENABLED(CONFIG_THREAD_MONITOR)) {
-		k_thread_foreach(log_thread_info, NULL);
+		k_thread_foreach_unlocked(log_thread_info, NULL);
 	}
 
 	/* Save the current task id in panic info.
