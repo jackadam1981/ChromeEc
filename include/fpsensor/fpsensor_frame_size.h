de819971670ee760a58809dd973c937ab390e7e3
Firas Sammoura <fsammoura@google.com>
2025-10-10 17:32:01 +0000

    fpsensor: Introduce FpFrameSizeCache for EC frame sizes
    Introduce the `FpFrameSizeCache` class to provide a cached lookup
    for fingerprint frame sizes based on data queried from the Embedded
    Controller (EC). This class queries the EC via `fp_sensor_get_info()`
    once during its static `Create()` method to build a mapping from
    `fp_capture_type` to the corresponding frame size.
    
    BUG=b:445178465
    TEST=CQ
    
    Change-Id: I3573f72b3353229b0d4c7febd291f617b426256e

---
+++ include/fpsensor/fpsensor_frame_size.h
@@ -0,0 +1,62 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_FPSENSOR_FPSENSOR_FRAME_SIZE_H
+#define __CROS_EC_FPSENSOR_FPSENSOR_FRAME_SIZE_H
+
+#include "ec_commands.h"
+
+#include <array>
+#include <cstdint>
+
+class FpFrameSizeCache {
+    public:
+	/**
+	 * @brief Public constructor.
+	 *
+	 * It attempts to populate the cache by querying the EC. The result of
+	 * the initialization can be checked with is_initialized().
+	 *
+	 * @param max_frame_size_bytes The maximum allowable size for any
+	 * fingerprint frame, used for validation during population.
+	 */
+	explicit FpFrameSizeCache(uint32_t max_frame_size_bytes);
+
+	/**
+	 * @brief Looks up the frame size for a given capture type.
+	 *
+	 * NOTE: Callers should check is_initialized() before calling this,
+	 * or handle the case where the returned size is 0 due to an
+	 * uninitialized cache.
+	 *
+	 * @param capture_type The enum fp_capture_type to look up.
+	 *
+	 * @return The frame size (uint32_t) if found, or 0 otherwise (e.g., for
+	 * an invalid type or if size is 0, or if the cache is uninitialized).
+	 */
+	uint32_t get_frame_size(enum fp_capture_type capture_type) const;
+
+    private:
+	/* No copying or moving of this object. */
+	FpFrameSizeCache(const FpFrameSizeCache &) = delete;
+	FpFrameSizeCache &operator=(const FpFrameSizeCache &) = delete;
+	FpFrameSizeCache(FpFrameSizeCache &&) = delete;
+	FpFrameSizeCache &operator=(FpFrameSizeCache &&) = delete;
+
+	/**
+	 * @brief Internal method to populate the frame size array.
+	 *
+	 * @param max_frame_size_bytes The maximum allowable size for any
+	 * fingerprint frame, used for validation.
+	 *
+	 * @retval true  On successful population of the cache.
+	 * @retval false On failure to populate the cache.
+	 */
+	bool populate_cache(uint32_t max_frame_size_bytes);
+
+	std::array<uint32_t, FP_CAPTURE_TYPE_MAX> frame_sizes_ = {};
+};
+
+#endif /* __CROS_EC_FPSENSOR_FPSENSOR_FRAME_SIZE_H */
