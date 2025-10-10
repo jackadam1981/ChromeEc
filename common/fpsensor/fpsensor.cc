6fb131243faf77bf02213a8f9095590d3dc232c8
Firas Sammoura <fsammoura@google.com>
2025-10-10 18:20:27 +0000

    common/fpsensor: Check frame size in FP_FRAME command
    Ensure the requested size in the FP_FRAME command is smaller than the
    size of the last captured frame (global_context.frame_size). This
    prevents returning uninitialized buffer data when different capture
    types result in varying data sizes.
    
    BUG=b:450383638
    TEST=Manually test on device by requesting frame data after different
         capture modes. Verify correct sizes are returned and invalid size
         requests are rejected.
    
    Change-Id: I2115e3ce8bf84a21450a9aa0e7c53c88c2b621d0

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -18,6 +18,7 @@
 #include "fpsensor/fpsensor_detect.h"
 #include "fpsensor/fpsensor_modes.h"
 #include "fpsensor/fpsensor_state.h"
+#include "fpsensor/fpsensor_state_driver.h"
 #include "fpsensor/fpsensor_utils.h"
 #include "gpio.h"
 #include "host_command.h"
@@ -34,6 +35,7 @@
 #include "watchdog.h"
 
 #include <array>
+#include <memory>
 #include <variant>
 
 #ifdef CONFIG_ZEPHYR
@@ -462,8 +464,12 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		if (skip_image_offset(global_context.sensor_mode))
 			offset += FP_SENSOR_IMAGE_OFFSET;
 
-		ret = validate_fp_buffer_offset(sizeof(fp_buffer), offset,
-						size);
+		if (global_context.current_frame_size > sizeof(fp_buffer)) {
+			return EC_RES_INVALID_PARAM;
+		}
+
+		ret = validate_fp_buffer_offset(
+			global_context.current_frame_size, offset, size);
 		if (ret != EC_SUCCESS)
 			return EC_RES_INVALID_PARAM;
 
