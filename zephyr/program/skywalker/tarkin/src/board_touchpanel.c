99f010ebf7286720bfdac1906ba9b44997b52d79
zengyuxiang <zengyuxiang@huaqin.corp-partner.google.com>
2025-10-11 09:14:48 +0800

    tarkin: Optimize the touchpanel power-up sequence during boot
    Optimize the touch panel power-up sequence during boot and add a startup flag to prevent HOOK_CHIPSET_RESUME from unnecessarily pulling gpio_tp_en high.
    
    BUG=b:445883866
    TEST=1.zmake build tarkin
    2.timing test meets spec requirements
    LOW_COVERAGE_REASON=Ztest CL will be submitted later:b:445883866
    
    Change-Id: Ib03c0f793f72929ad218ea9a5a27f2875dfb2c82

--- zephyr/program/skywalker/tarkin/src/board_touchpanel.c
+++ zephyr/program/skywalker/tarkin/src/board_touchpanel.c
@@ -14,6 +14,7 @@
 #include <ap_power/ap_power.h>
 
 static bool value_en;
+static uint16_t boot_flags;
 
 static void set_tp_vtsp_en_pin(void)
 {
@@ -37,9 +38,10 @@ DECLARE_DEFERRED(set_tp_en_pin);
 static void board_tp_startup(void)
 {
 	value_en = 1;
+	boot_flags = 1;
 	hook_call_deferred(&set_tp_vtsp_en_pin_data, 0);
-	hook_call_deferred(&set_tp_rst_pin_data, 20 * USEC_PER_MSEC);
-	hook_call_deferred(&set_tp_en_pin_data, 200 * USEC_PER_MSEC);
+	hook_call_deferred(&set_tp_rst_pin_data, 10 * USEC_PER_MSEC);
+	hook_call_deferred(&set_tp_en_pin_data, 80 * USEC_PER_MSEC);
 }
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_tp_startup, HOOK_PRIO_DEFAULT);
 
@@ -56,6 +58,13 @@ DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_tp_shutdown, HOOK_PRIO_DEFAULT);
 /* Exit suspend */
 static void board_tp_resume(void)
 {
+	/* Avoid calls triggered by HOOK_CHIPSET_RESUME during the boot process.
+	 */
+	if (boot_flags == 1) {
+		boot_flags = 0;
+		return;
+	}
+
 	value_en = 1;
 	hook_call_deferred(&set_tp_en_pin_data, 0);
 }
