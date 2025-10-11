a223c71c13f8f1158235e5f074ca362e28e385b0
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2025-10-11 09:53:38 +0800

    corsola: Add mewtwo variant
    - New DTS overlays for GPIO/ADC
    - prj_mewtwo.conf with variant configs
    - Kconfig for BOARD_MEWTWO
    - BUILD.py registration
    
    BUG=None
    TEST=make BOARD=mewtwo && ectool gpiget
    
    Change-Id: I7a76ecb9be3888460b2590af536c4d6ac74957d6

--- zephyr/program/corsola/BUILD.py
+++ zephyr/program/corsola/BUILD.py
@@ -108,6 +108,11 @@ register_corsola_project(
     inherited_from=["staryu"],
 )
 
+register_corsola_project(
+    project_name="mewtwo",
+    chip="npcx9/npcx9m3f",
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="chinchou", addr=0xBFFE0)
@@ -125,3 +130,4 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="skitty", addr=0xBFFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="veluza", addr=0xBFFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="squirtle", addr=0x7FFE0)
 assert_rw_fwid_DO_NOT_EDIT(project_name="wyrdeer", addr=0xBFFE0)
+assert_rw_fwid_DO_NOT_EDIT(project_name="mewtwo", addr=0x7FFE0)
