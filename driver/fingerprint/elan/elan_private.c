a084aaaf074f8ce258ad50aaab34ce3882e54e87
Firas Sammoura <fsammoura@google.com>
2025-10-12 23:57:14 +0000

    driver/fingerprint: Respect num_params in fp_sensor_get_info_v2
    Update the `fp_sensor_get_info_v2` implementation to honor the input
    value of `*num_params` when copying image frame parameters. The function
    now copies at most the number of `fp_image_frame_params` requested by
    the caller.
    
    The number of elements copied is the minimum of the input `*num_params`
    and the total number of available parameter sets. The `*num_params`
    output value is updated to reflect the number of items actually copied.
    
    BUG=b:445178465
    TEST=CQ
    
    Change-Id: Ife12baf6d03d45a42fa2b0f2ad47b3b51cca7761

--- driver/fingerprint/elan/elan_private.c
+++ driver/fingerprint/elan/elan_private.c
@@ -221,9 +221,23 @@ int fp_sensor_get_info_v2(struct ec_response_fp_info_v2 *resp, size_t resp_size,
 {
 	CPRINTF("========%s=======\n", __func__);
 
-	if (sizeof(struct ec_response_fp_info_v2) +
-		    sizeof(elan_image_frame_params) >
-	    resp_size) {
+	if (!num_params || !resp) {
+		return EC_RES_INVALID_PARAM;
+	}
+
+	uint8_t driver_params_count = ARRAY_SIZE(elan_image_frame_params);
+	uint8_t num_params_to_copy = *num_params;
+
+	if (num_params_to_copy > driver_params_count) {
+		num_params_to_copy = driver_params_count;
+	}
+
+	size_t required_size =
+		sizeof(struct ec_response_fp_info_v2) +
+		sizeof(struct fp_image_frame_params) * num_params_to_copy;
+
+	if (required_size > resp_size) {
+		*num_params = 0;
 		return EC_RES_OVERFLOW;
 	}
 
@@ -235,13 +249,14 @@ int fp_sensor_get_info_v2(struct ec_response_fp_info_v2 *resp, size_t resp_size,
 	memcpy(&resp->sensor_info, &elan_sensor_info,
 	       sizeof(struct fp_sensor_info));
 
-	memcpy(&resp->image_frame_params, &elan_image_frame_params,
-	       sizeof(elan_image_frame_params));
-
 	resp->sensor_info.model_id = id;
 	resp->sensor_info.errors = errors;
-	resp->sensor_info.num_capture_types =
-		ARRAY_SIZE(elan_image_frame_params);
+	resp->sensor_info.num_capture_types = num_params_to_copy;
+
+	memcpy(&resp->image_frame_params, &elan_image_frame_params,
+	       num_params_to_copy * sizeof(struct fp_image_frame_params));
+
+	*num_params = num_params_to_copy;
 
 	return EC_SUCCESS;
 }
