92f6cf669b4b1d2f3fc9e87fa5149dccb636ace9
Firas Sammoura <fsammoura@google.com>
2025-10-12 06:18:48 +0000

    fpsensor: Use num_capture_types from params in FP_INFO v2
    Modify `fp_command_info_v2` to use the `num_capture_types` field
    from the `ec_params_fp_info_v2` host command parameters, instead of
    the sensor info, when calculating the response size for
    `EC_CMD_FP_INFO`. This allows the host to specify the number
    of capture type details to be returned.
    
    BUG=b:445178465
    TEST=ectool --name=cros_fp fpinfo
    TEST=CQ
    
    Change-Id: Ief4c4cd9b542b1f19b03c41c75d05195c5e5a00d

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -391,6 +391,8 @@ static enum ec_status fp_command_info_v2(struct host_cmd_handler_args *args)
 {
 	struct ec_response_fp_info_v2 *r =
 		static_cast<ec_response_fp_info_v2 *>(args->response);
+	const struct ec_params_fp_info_v2 *p =
+		static_cast<const struct ec_params_fp_info_v2 *>(args->params);
 
 #ifdef HAVE_FP_PRIVATE_DRIVER
 	if (fp_sensor_get_info_v2(r, args->response_max) < 0)
@@ -403,9 +405,9 @@ static enum ec_status fp_command_info_v2(struct host_cmd_handler_args *args)
 	r->template_info.template_dirty = global_context.templ_dirty;
 	r->template_info.template_version = FP_TEMPLATE_FORMAT_VERSION;
 
-	args->response_size = sizeof(struct ec_response_fp_info_v2) +
-			      (r->sensor_info.num_capture_types) *
-				      sizeof(struct fp_image_frame_params);
+	args->response_size =
+		sizeof(struct ec_response_fp_info_v2) +
+		(p->num_capture_types) * sizeof(struct fp_image_frame_params);
 
 	return EC_RES_SUCCESS;
 }
