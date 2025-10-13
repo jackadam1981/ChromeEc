8c19cac29404d7af8939023cfe88a4fde5fdb666
Firas Sammoura <fsammoura@google.com>
2025-10-13 19:15:39 +0000

    Use fixed max size for FP_INFO v2 response
    Modify the EC_CMD_FP_INFO v2 handler to return a fixed-size response.
    
    The response size is now always calculated based on the maximum possible
    number of capture types (FP_MAX_CAPTURE_TYPES), rather than the actual
    number present. This provides a predictable response size for the host.
    
    This change complements host-side logic in libec that expects a
    fixed-size response.
    
    BUG=b:445178465
    TEST=ectool --name=cros_fp fpinfo -> succeeds with fpinfo v2 firmware
    
    Change-Id: I3364e2a87eb81ee6f2b15ef48db1fc22fb374bcc

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -391,9 +391,16 @@ static enum ec_status fp_command_info_v2(struct host_cmd_handler_args *args)
 {
 	struct ec_response_fp_info_v2 *r =
 		static_cast<ec_response_fp_info_v2 *>(args->response);
+	const size_t response_size =
+		sizeof(struct ec_response_fp_info_v2) +
+		FP_MAX_CAPTURE_TYPES * sizeof(struct fp_image_frame_params);
+
+	if (response_size > args->response_max) {
+		return EC_RES_OVERFLOW;
+	}
 
 #ifdef HAVE_FP_PRIVATE_DRIVER
-	if (fp_sensor_get_info_v2(r, args->response_max) < 0)
+	if (fp_sensor_get_info_v2(r, response_size) < 0)
 #endif
 		return EC_RES_UNAVAILABLE;
 
@@ -403,9 +410,7 @@ static enum ec_status fp_command_info_v2(struct host_cmd_handler_args *args)
 	r->template_info.template_dirty = global_context.templ_dirty;
 	r->template_info.template_version = FP_TEMPLATE_FORMAT_VERSION;
 
-	args->response_size = sizeof(struct ec_response_fp_info_v2) +
-			      (r->sensor_info.num_capture_types) *
-				      sizeof(struct fp_image_frame_params);
+	args->response_size = response_size;
 
 	return EC_RES_SUCCESS;
 }
