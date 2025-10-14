7df8206aacf4b3b2790f2a002cbd1a5264c997ab
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2025-10-14 13:53:37 +0800

    pdc: fix build error "outside array bounds"
    Fix build error "array subscript is outside array bounds of uint8_t[20]",
    when CONFIG_LTO = n.
    So I extend array size to RTS54XX_GET_IC_STATUS_PD_VER_MINOR_OFFSET
    (index 26).
    
    BUG=none
    TEST=can zmake on kaladin
    
    Change-Id: I4509ba69c3c040dd69d638de811f81044b70b047

--- zephyr/drivers/usbc/pdc_rts54xx_fwup.c
+++ zephyr/drivers/usbc/pdc_rts54xx_fwup.c
@@ -39,7 +39,7 @@ LOG_MODULE_REGISTER(pdc_rts54_fwup, LOG_LEVEL_INF);
 
 /** Number of GET_IC_STATUS bytes that can be requested while running in ROM
  *  code */
-#define RTS54XX_GET_IC_STATUS_SAFE_READ_LEN 20
+#define RTS54XX_GET_IC_STATUS_SAFE_READ_LEN 27
 
 static struct {
 	bool fwup_session_active;
