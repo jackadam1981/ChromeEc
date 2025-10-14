8f7d96b5fa2ffa3b3871c231f66d61bf88b93058
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2025-10-14 11:58:43 +0800

    lapis: Customize the battery_is_present()
    In AC only mode, the EC will generate too many battery i2c
    nack messages. Customize the battery_is_present() as a
    solution if the NAK message introduces any issue.
    
    BUG=b:447331362
    TEST=The DUT can boot in AC only mode and battery activation
         function is normal.
    LOW_COVERAGE_REASON=b:438785495 track ztest code
    
    Change-Id: Ibd85d94ebddd3caaac8a549d5aa2ff719bbe1e0c

---
+++ zephyr/program/fatcat/lapis/src/battery.c
@@ -0,0 +1,24 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "battery.h"
+#include "battery_smart.h"
+
+enum battery_present battery_is_present(void)
+{
+	int manufacture_date;
+
+	if (sb_read(SB_MANUFACTURE_DATE, &manufacture_date)) {
+		/* Require 2 consecutive failures before declaring the
+		 * battery missing.
+		 */
+		k_msleep(25);
+		if (sb_read(SB_MANUFACTURE_DATE, &manufacture_date)) {
+			return BP_NOT_SURE;
+		}
+	}
+
+	return BP_YES;
+}
