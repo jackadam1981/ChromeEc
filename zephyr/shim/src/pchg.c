c6c6f4c7152481c9fd88c8afff7a6cb044539d0a
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2025-10-16 23:15:50 +0800

    zephyr: add cps8601 device support
    BUG=b:452233583
    TEST=zmake build padme
    LOW_COVERAGE_REASON=Tests will be added in b:452233583
    
    Change-Id: If9082c5304198abdd74584a2f7ac69f83b151ec1

--- zephyr/shim/src/pchg.c
+++ zephyr/shim/src/pchg.c
@@ -5,18 +5,31 @@
 
 #include "gpio_signal.h"
 #include "peripheral_charger.h"
+#include "wpc/cps8601.h"
 #include "wpc/scp8200.h"
 
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/util.h>
 
+#ifdef CONFIG_PLATFORM_EC_PERIPHERAL_CHARGER
+
 #if DT_HAS_COMPAT_STATUS_OKAY(CPS8200_PCHG_COMPAT)
+#define WPC_CHIP_ELE_CPS8200(id) WPC_CHIP_CPS8200(id),
+#else
+#define WPC_CHIP_ELE_CPS8200(id)
+#endif
 
-#define WPC_CHIP_ELE(id, fn) fn(id),
+#if DT_HAS_COMPAT_STATUS_OKAY(CPS8601_PCHG_COMPAT)
+#define WPC_CHIP_ELE_CPS8601(id) WPC_CHIP_CPS8601(id),
+#else
+#define WPC_CHIP_ELE_CPS8601(id)
+#endif
 
-struct pchg pchgs[] = { DT_FOREACH_STATUS_OKAY_VARGS(
-	CPS8200_PCHG_COMPAT, WPC_CHIP_ELE, WPC_CHIP_CPS8200) };
+struct pchg pchgs[] = { DT_FOREACH_STATUS_OKAY(CPS8200_PCHG_COMPAT,
+					       WPC_CHIP_ELE_CPS8200)
+				DT_FOREACH_STATUS_OKAY(CPS8601_PCHG_COMPAT,
+						       WPC_CHIP_ELE_CPS8601) };
 
 unsigned int pchg_count = ARRAY_SIZE(pchgs);
 
