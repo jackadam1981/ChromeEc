8850ae994bf77896a1081cc27f3e530db6d54b7c
Firas Sammoura <fsammoura@google.com>
2025-10-21 22:51:20 +0000

    zephyr: test: Add unit tests for fpsensor frame size
    BUG=b:450383638
    TEST=./twister -v -i -s fingerprint.task.frame.size -c
    
    Change-Id: I6c21e01806e0a742d49647e39234e27b513f1735

---
+++ zephyr/test/fingerprint/task/src/fpsensor_frame_size.cc
@@ -0,0 +1,62 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "ec_commands.h"
+
+#include <zephyr/fff.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/ztest.h>
+
+#include <drivers/fingerprint_sim.h>
+#include <fingerprint/v4l2_types.h>
+#include <fpsensor/fpsensor_frame_size.h>
+#include <mkbp_event.h>
+
+DEFINE_FFF_GLOBALS;
+
+FAKE_VALUE_FUNC(int, mkbp_send_event, uint8_t);
+
+#define FP_SIMULATOR_IMAGE_FRAME_PARAM_INITIALIZER(idx, node_id)            \
+	{                                                                   \
+		.frame_size = FINGERPRINT_SENSOR_FRAME_SIZE(idx, node_id),  \
+		.pixel_format =                                             \
+			FINGERPRINT_SENSOR_V4L2_PIXEL_FORMAT(idx, node_id), \
+		.width = FINGERPRINT_SENSOR_RES_X(idx, node_id),            \
+		.height = FINGERPRINT_SENSOR_RES_Y(idx, node_id),           \
+		.bpp = FINGERPRINT_SENSOR_RES_BPP(idx, node_id),            \
+		.fp_capture_type =                                          \
+			FINGERPRINT_SENSOR_CAPTURE_TYPE(idx, node_id),      \
+		.reserved = 0,                                              \
+	}
+
+static const struct fingerprint_image_frame_params image_frame_params_array[] =
+    {LISTIFY(NUM_IMAGE_CAPTURE_TYPES,
+             FP_SIMULATOR_IMAGE_FRAME_PARAM_INITIALIZER, (, ),
+             DT_NODELABEL(fpsensor_sim))};
+
+ZTEST_SUITE(fpsensor_frame_size, NULL, NULL, NULL, NULL, NULL);
+
+ZTEST(fpsensor_frame_size, test_cache_initialization_and_lookup) {
+  auto cache = FpFrameSizeCache::Create();
+
+  zassert_not_null(cache.get(),
+                   "FpFrameSizeCache::Create() failed (returned nullptr).");
+
+  for (const auto& image_frame_params : image_frame_params_array) {
+    uint32_t expected_size = image_frame_params.frame_size;
+    enum fp_capture_type type =
+        (enum fp_capture_type)image_frame_params.fp_capture_type;
+
+    uint32_t actual_size = cache->get_frame_size(type);
+
+    zassert_equal(expected_size, actual_size,
+                  "Mismatch for type %d. Expected: %u, Actual: %u", (int)type,
+                  expected_size, actual_size);
+  }
+
+  zassert_equal(cache->get_frame_size(FP_CAPTURE_TYPE_MAX), 0,
+                "Expected zero size for invalid capture type (%d).",
+                FP_CAPTURE_TYPE_MAX);
+}
