druth0/chrome-ec

commit f73ddfea64186aa2579be1514de6e04b6bcea38a

motion_sense: Skip IRQ init if no sensors
If we dynamically set the motion_sensor_count to 0, skip initializing
the interrupts.

BUG=b:410646711
TEST=flash uldrenite and uldrenite-ish, check that sensors are working

Change-Id: I7e4974d98e97bad7340bdc98f6aee7a74b186601

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6503813
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Yuval Peress <peress@google.com>
Tested-by: Yuval Peress <peress@google.com>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

zephyr/shim/src/motionsense_sensors.c
@@ -397,10 +397,23 @@ BUILD_ASSERT(ARRAY_SIZE(motion_als_sensors) == ALS_COUNT);
 #define SENSOR_GPIO_ENABLE_INTERRUPT(i, id) \
 	gpio_enable_dt_interrupt(           \
 		GPIO_INT_FROM_NODE(DT_PHANDLE_BY_IDX(id, sensor_irqs, i)));
-static void sensor_enable_irqs(void){
+static void sensor_enable_irqs(void)
+{
+	if (motion_sensor_count == 0) {
+		/* Don't enable interrupts if there are no sensors. This is not
+		 * an optimal solution, it's possible that some sensors will be
+		 * disabled while others aren't. For now, we will use this
+		 * all-or-nothing approach which will be replaced eventually by
+		 * the Zephyr upstream drivers.
+		 */
+		return;
+	}
+	__ASSERT_NO_MSG(DT_PROP_LEN(SENSOR_INFO_NODE, sensor_irqs) <=
+			motion_sensor_count);
 	LISTIFY(DT_PROP_LEN(SENSOR_INFO_NODE, sensor_irqs),
 		SENSOR_GPIO_ENABLE_INTERRUPT, (), SENSOR_INFO_NODE)
-} DECLARE_HOOK(HOOK_INIT, sensor_enable_irqs, HOOK_PRIO_DEFAULT);
+}
+DECLARE_HOOK(HOOK_INIT, sensor_enable_irqs, HOOK_PRIO_DEFAULT);
 #endif
 
 /* Handle the alternative motion sensors */

zephyr/test/geralt/geralt.ciri.overlay
@@ -57,6 +57,7 @@
 			active-mask = "SENSOR_ACTIVE_S0";
 			location = "MOTIONSENSE_LOC_LID";
 			drv-data = <&bmi323_data>;
+			port = <&i2c_sensor>;
 		};
 
 		lid_gyro: lid-gyro {
@@ -66,6 +67,7 @@
 			active-mask = "SENSOR_ACTIVE_S0";
 			location = "MOTIONSENSE_LOC_LID";
 			drv-data = <&bmi323_data>;
+			port = <&i2c_sensor>;
 		};
 	};
 
@@ -76,6 +78,9 @@
 
 			active-mask = "SENSOR_ACTIVE_S0";
 			location = "MOTIONSENSE_LOC_LID";
+			drv-data = <&lsm6dsm_data_accel>;
+			port = <&i2c_sensor>;
+			alternate-for = <&lid_accel>;
 		};
 
 		alt_lid_gyro: alt-lid-gyro {
@@ -84,6 +89,9 @@
 
 			active-mask = "SENSOR_ACTIVE_S0";
 			location = "MOTIONSENSE_LOC_LID";
+			drv-data = <&lsm6dsm_data_gyro>;
+			port = <&i2c_sensor>;
+			alternate-for = <&lid_gyro>;
 		};
 	};
 

zephyr/test/geralt/testcase.yaml
@@ -11,5 +11,8 @@ tests:
   geralt.ciri:
     extra_configs:
     - CONFIG_TEST_CIRI=y
+    - CONFIG_PLATFORM_EC_ACCELGYRO_LSM6DSM=y
+    - CONFIG_PLATFORM_EC_ACCELGYRO_BMI3XX=y
+    - CONFIG_PLATFORM_EC_ACCELGYRO_BMI_COMM_I2C=y
     extra_dtc_overlay_files:
     - geralt.ciri.overlay

zephyr/test/kingler/common.dtsi
@@ -97,6 +97,7 @@
 
 			active-mask = "SENSOR_ACTIVE_S0_S3_S5";
 			location = "MOTIONSENSE_LOC_BASE";
+			port = <&i2c_sensor>;
 			drv-data = <&bmi160_data>;
 			default-range = <4>;
 			i2c-spi-addr-flags = "BMI160_ADDR0_FLAGS";
@@ -107,6 +108,7 @@
 
 			active-mask = "SENSOR_ACTIVE_S0_S3_S5";
 			location = "MOTIONSENSE_LOC_BASE";
+			port = <&i2c_sensor>;
 			drv-data = <&bmi160_data>;
 			default-range = <4>;
 			i2c-spi-addr-flags = "BMI160_ADDR0_FLAGS";

zephyr/test/kingler/kingler.ponyta.overlay
@@ -17,5 +17,77 @@
 		 * its own <>_INT_EVENT.
 		 */
 		lsm6dsm-int = &base_accel;
+		bmi3xx-int = &base_accel;
+	};
+
+	motionsense-mutex {
+		compatible = "cros-ec,motionsense-mutex";
+		base_mutex: lid-mutex {
+		};
+
+		alt_base_mutex: alt-lid-mutex {
+		};
+	};
+
+	motionsense-sensor-data {
+		bmi323_data: bmi323-drv-data {
+			compatible = "cros-ec,drvdata-bmi3xx";
+			status = "okay";
+		};
+		lsm6dsm_data: lsm6dsm-drv-data {
+			compatible = "cros-ec,drvdata-lsm6dsm";
+			status = "okay";
+		};
+	};
+
+	motionsense-sensor {
+		/delete-node/ ms-bmi160-accel;
+		base_accel: base-accel {
+			compatible = "cros-ec,bmi3xx-accel";
+			status = "okay";
+
+			active-mask = "SENSOR_ACTIVE_S0_S3";
+			location = "MOTIONSENSE_LOC_LID";
+			mutex = <&base_mutex>;
+			drv-data = <&bmi323_data>;
+			port = <&i2c_sensor>;
+		};
+
+		base_gyro: base-gyro {
+			compatible = "cros-ec,bmi3xx-gyro";
+			status = "okay";
+
+			active-mask = "SENSOR_ACTIVE_S0_S3";
+			location = "MOTIONSENSE_LOC_LID";
+			mutex = <&base_mutex>;
+			drv-data = <&bmi323_data>;
+			port = <&i2c_sensor>;
+		};
+	};
+
+	motionsense-sensor-alt {
+		alt_base_accel: alt-base-accel {
+			compatible = "cros-ec,lsm6dsm-accel";
+			status = "okay";
+
+			active-mask = "SENSOR_ACTIVE_S0_S3";
+			location = "MOTIONSENSE_LOC_BASE";
+			mutex = <&alt_base_mutex>;
+			drv-data = <&lsm6dsm_data>;
+			port = <&i2c_sensor>;
+			alternate-for = <&base_accel>;
+		};
+
+		alt_base_gyro: alt-base-gyro {
+			compatible = "cros-ec,lsm6dsm-gyro";
+			status = "okay";
+
+			active-mask = "SENSOR_ACTIVE_S0_S3";
+			location = "MOTIONSENSE_LOC_BASE";
+			mutex = <&alt_base_mutex>;
+			drv-data = <&lsm6dsm_data>;
+			port = <&i2c_sensor>;
+			alternate-for = <&base_gyro>;
+		};
 	};
 };

zephyr/test/kingler/kingler.steelix.overlay
@@ -19,6 +19,7 @@
 		 * its own <>_INT_EVENT.
 		 */
 		lsm6dsm-int = &base_accel;
+		bmi3xx-int = &base_accel;
 	};
 
 	/* LEDs need to exist for testing, but aren't actually used. */
@@ -55,6 +56,77 @@
 		};
 		led_power_white: ec_led3_odl {
 			pwms = <&pwm4 0 PWM_HZ(100) PWM_POLARITY_INVERTED>;
+		};
+	};
+
+	motionsense-mutex {
+		compatible = "cros-ec,motionsense-mutex";
+		base_mutex: lid-mutex {
+		};
+
+		alt_base_mutex: alt-lid-mutex {
+		};
+	};
+
+	motionsense-sensor-data {
+		bmi323_data: bmi323-drv-data {
+			compatible = "cros-ec,drvdata-bmi3xx";
+			status = "okay";
+		};
+		lsm6dsm_data: lsm6dsm-drv-data {
+			compatible = "cros-ec,drvdata-lsm6dsm";
+			status = "okay";
+		};
+	};
+
+	motionsense-sensor {
+		/delete-node/ ms-bmi160-accel;
+		base_accel: base-accel {
+			compatible = "cros-ec,bmi3xx-accel";
+			status = "okay";
+
+			active-mask = "SENSOR_ACTIVE_S0_S3";
+			location = "MOTIONSENSE_LOC_LID";
+			mutex = <&base_mutex>;
+			drv-data = <&bmi323_data>;
+			port = <&i2c_sensor>;
+		};
+
+		base_gyro: base-gyro {
+			compatible = "cros-ec,bmi3xx-gyro";
+			status = "okay";
+
+			active-mask = "SENSOR_ACTIVE_S0_S3";
+			location = "MOTIONSENSE_LOC_LID";
+			mutex = <&base_mutex>;
+			drv-data = <&bmi323_data>;
+			port = <&i2c_sensor>;
+		};
+	};
+
+	motionsense-sensor-alt {
+		alt_base_accel: alt-base-accel {
+			compatible = "cros-ec,lsm6dsm-accel";
+			status = "okay";
+
+			active-mask = "SENSOR_ACTIVE_S0_S3";
+			location = "MOTIONSENSE_LOC_BASE";
+			mutex = <&alt_base_mutex>;
+			drv-data = <&lsm6dsm_data>;
+			port = <&i2c_sensor>;
+			alternate-for = <&base_accel>;
+		};
+
+		alt_base_gyro: alt-base-gyro {
+			compatible = "cros-ec,lsm6dsm-gyro";
+			status = "okay";
+
+			active-mask = "SENSOR_ACTIVE_S0_S3";
+			location = "MOTIONSENSE_LOC_BASE";
+			mutex = <&alt_base_mutex>;
+			drv-data = <&lsm6dsm_data>;
+			port = <&i2c_sensor>;
+			alternate-for = <&base_gyro>;
 		};
 	};
 };

zephyr/test/kingler/src/fakes.c
@@ -38,8 +38,9 @@ FAKE_VOID_FUNC(ccd_interrupt, enum gpio_signal);
      defined(CONFIG_TEST_SQUIRTLE))
 #ifndef CONFIG_TEST_SQUIRTLE
 FAKE_VOID_FUNC(x_ec_interrupt, enum gpio_signal);
-#endif
+#else
 FAKE_VOID_FUNC(motion_sensors_check_ssfc);
+#endif
 #else
 FAKE_VOID_FUNC(motion_interrupt, enum gpio_signal);
 #endif

zephyr/test/kingler/src/ponyta_board.c
@@ -9,6 +9,7 @@
 #include "hooks.h"
 #include "keyboard_config.h"
 #include "keyboard_scan.h"
+#include "motion_sense.h"
 #include "tablet_mode.h"
 #include "zephyr/kernel.h"
 
@@ -44,6 +45,12 @@ FAKE_VALUE_FUNC(int, cros_cbi_get_fw_config, enum cbi_fw_config_field_id,
 
 static void reset(void)
 {
+	/* zephyr/program/corsola/ponyta/src/board.c will set the sensor count
+	 * to 0 if the CBI fw config is set to CLAMSHELL, but there's no way to
+	 * recover the previous sensor count. Before calling hook_notify, we
+	 * should reset the sensor count manually.
+	 */
+	motion_sensor_count = DT_CHILD_NUM_STATUS_OKAY(SENSOR_NODE);
 	/* Re-initialize CBI */
 	cros_cbi_ec_init();
 

jackadam1981/ChromeEc

commit 50ff690ea9ea24d149667b9f0f9ba666e8c1e068

motion_sense: Set Sensor IRQ even when sensors are disabled
When some sensors in motion_sensors are disabled, still set the
interrupt for all of them:
For instance for uldrenite180: only the lid accel remains.
Without this change the ISH would assert.
After the sensor is intialized.

Fixes: f73ddfea6418 ("motion_sense: Skip IRQ init if no sensors")

BUG=b:410646711,b:435407060
TEST=flash uldrenite and uldrenite-ish, check that sensors are working

Change-Id: I1c158c18d305c10cb18228cb5d76234778c278d4

zephyr/shim/src/motionsense_sensors.c
@@ -400,16 +400,15 @@ BUILD_ASSERT(ARRAY_SIZE(motion_als_sensors) == ALS_COUNT);
 static void sensor_enable_irqs(void)
 {
 	if (motion_sensor_count == 0) {
-		/* Don't enable interrupts if there are no sensors. This is not
-		 * an optimal solution, it's possible that some sensors will be
-		 * disabled while others aren't. For now, we will use this
-		 * all-or-nothing approach which will be replaced eventually by
-		 * the Zephyr upstream drivers.
-		 */
+		/* Don't enable interrupts if there are no sensors. */
 		return;
 	}
-	__ASSERT_NO_MSG(DT_PROP_LEN(SENSOR_INFO_NODE, sensor_irqs) <=
-			motion_sensor_count);
+	/*
+	 * Note we may enable interrupt for sensors that are not present,
+	 * if the number of sensor is reduced.
+	 * This is not ideal but the related GPIO line is forced to HIGH
+	 * later in sensors.c, preventing it from floating.
+	 */
 	LISTIFY(DT_PROP_LEN(SENSOR_INFO_NODE, sensor_irqs),
 		SENSOR_GPIO_ENABLE_INTERRUPT, (), SENSOR_INFO_NODE)
 }