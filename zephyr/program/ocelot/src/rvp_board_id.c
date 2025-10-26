234f548c56f87bc747dddb06db505db304907e78
Poornima Tom <poornima.tom@intel.com>
2025-10-27 05:20:34 +0530

    ocelot: Restore RVP SKU ID, after sysjump to RW
    When there is a sysjump from RO to RW, the board ID is getting lost.
    This CL takes care of preserving and restoring the board id, after
    sysjump to RW.
    
    BUG=b:455072992
    TEST=Version displays the board id of RVP, after sysjump to RW.
    
    25-10-25 18:07:53.907 ec:> version
    25-10-25 18:07:58.542 Chip:     Nuvoton npcx9m7f 00160207
    25-10-25 18:07:58.546 Board:    545
    25-10-25 18:07:58.546 RO:       ocelotrvp-npcx-16448.0.0
    25-10-25 18:07:58.548 RW:       ocelotrvp-npcx-16448.0.0
    25-10-25 18:07:58.551 Build:    ocelotrvp-npcx-16448.0.0 STATIC_VERSION_DATE
    25-10-25 18:07:58.554           reproducible@build
    25-10-25 18:07:58.556 ec:> sysinfo
    25-10-25 18:08:02.307 Reset flags: 0x00000c00 (sysjump hard)
    25-10-25 18:08:02.311 Copy:   RW
    25-10-25 18:08:02.311 Jumped: yes
    25-10-25 18:08:02.314 Recovery: no
    25-10-25 18:08:02.314 Flags:  unlocked
    25-10-25 18:08:02.315 ec:>
    
    Change-Id: I64c7a2adb5b216e9123784996ec366137d20d80d
    Signed-off-by: Poornima Tom <poornima.tom@intel.com>

--- zephyr/program/ocelot/src/rvp_board_id.c
+++ zephyr/program/ocelot/src/rvp_board_id.c
@@ -10,14 +10,17 @@
 
 LOG_MODULE_DECLARE(rvp_board_id, LOG_LEVEL_INF);
 
+/* Cache the board ID */
+static int rvp_board_id;
+
+#define RVP_SKUID_SYSJUMP_TAG 0x5253 /* RS */
+#define RVP_SKUID_HOOK_VERSION 1
+
 /*
  * Returns board version on success, -1 on error.
  */
 __override int board_get_version(void)
 {
-	/* Cache the board ID */
-	static int rvp_board_id;
-
 	int board_id = -1;
 	int fab_id = -1;
 
@@ -37,3 +40,33 @@ __override int board_get_version(void)
 
 	return rvp_board_id;
 }
+
+/*
+ * Preserve RVP SKUID across a sysjump.
+ */
+
+static void rvp_sku_id_preserve_state(void)
+{
+	system_add_jump_tag(RVP_SKUID_SYSJUMP_TAG, RVP_SKUID_HOOK_VERSION,
+			    sizeof(rvp_board_id), &rvp_board_id);
+}
+DECLARE_HOOK(HOOK_SYSJUMP, rvp_sku_id_preserve_state, HOOK_PRIO_DEFAULT);
+
+/*
+ * Restore RVP SKUID after a sysjump.
+ */
+static void rvp_sku_id_restore_state(void)
+{
+	const uint32_t *prev_rvp_board_id;
+	int size, version;
+
+	prev_rvp_board_id = (const uint32_t *)system_get_jump_tag(
+		RVP_SKUID_SYSJUMP_TAG, &version, &size);
+
+	if (prev_rvp_board_id && version == RVP_SKUID_HOOK_VERSION &&
+	    size == sizeof(prev_rvp_board_id)) {
+		memcpy(&rvp_board_id, prev_rvp_board_id, sizeof(rvp_board_id));
+		LOG_INF("Restored BID = %d", rvp_board_id);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, rvp_sku_id_restore_state, HOOK_PRIO_DEFAULT);
