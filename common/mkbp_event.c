59027ca077941243e4089368c1e2d26e258f8d29
Dawid Niedzwiecki <dawidn@google.com>
2025-10-28 14:44:45 +0100

    mkbp: add USB method to signal MKBP event
    Add a new method to signal MKBP via USB Host Command backend.
    
    It uses Zephyr function ec_host_cmd_backend_usb_trigger_event
    that uses an interrupt endpoint to signal an event.
    
    BUG=b:392914842
    TEST=Connect EC device to AP and perform any action that uses MKBP
    events e.g. finger enrollment
    Low-Coverage-Reason: Tests will be added with the projects using
    USB b/392915103
    
    Change-Id: I17606264323f6d21338c58a6ca94eaf22abb14d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7088130
    Commit-Queue: Dawid Niedźwiecki <dawidn@google.com>
    Reviewed-by: Patryk Duda <patrykd@google.com>
    Tested-by: Dawid Niedźwiecki <dawidn@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7127774
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Jon Murphy <jpmurphy@google.com>

--- common/mkbp_event.c
+++ common/mkbp_event.c
@@ -144,6 +144,19 @@ static int mkbp_set_host_active_via_heci(int active, uint32_t *timestamp)
 }
 #endif
 
+#ifdef CONFIG_MKBP_USE_USB
+int mkbp_set_host_active_via_usb(int active, uint32_t *timestamp)
+{
+	if (timestamp) {
+		*timestamp = __hw_clock_source_read();
+	}
+	if (active) {
+		ec_host_cmd_backend_usb_trigger_event();
+	}
+	return EC_SUCCESS;
+}
+#endif
+
 /*
  * This communicates to the AP whether an MKBP event is currently available
  * for processing.
@@ -166,6 +179,8 @@ static int mkbp_set_host_active(int active, uint32_t *timestamp)
 	return mkbp_set_host_active_via_gpio(active, timestamp);
 #elif defined(CONFIG_MKBP_USE_HECI)
 	return mkbp_set_host_active_via_heci(active, timestamp);
+#elif defined(CONFIG_MKBP_USE_USB)
+	return mkbp_set_host_active_via_usb(active, timestamp);
 #endif
 }
 
