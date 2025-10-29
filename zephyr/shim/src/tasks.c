e353f4b1aa40b3f26f7f97b7d62e8f64a30e8897
Tristan Honscheid <honscheid@google.com>
2025-10-29 15:51:53 -0600

    zephyr: shim: Don't assert for unmatched thread IDs
    There many native Zephyr threads on the EC that are not mapped to a
    corresponding legacy task ID in task_get_current() /
    thread_id_to_task_id(). We'd like to deprecate the remaining uses of
    task ID to make this function unnecessary. But for now, disable this
    assertion and return TASK_ID_INVALID for un-mapped threads.
    
    BUG=b:450804537
    BUG=b:456237554
    TEST=zmake build pujjocento
    TEST=./twister -T zephyr/test/tasks
    
    Change-Id: I2071aec8e5dcd3df526e72ec21d99da4615220c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7098161
    Commit-Queue: Tristan Honscheid <honscheid@google.com>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Tested-by: Tristan Honscheid <honscheid@google.com>
    Commit-Queue: ChromeOS Auto Runner <chromeos-auto-runner@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7102918
    Reviewed-by: Tristan Honscheid <honscheid@google.com>
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Owners-Override: Rob Barnes <robbarnes@google.com>
    Auto-Submit: Rob Barnes <robbarnes@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>

--- zephyr/shim/src/tasks.c
+++ zephyr/shim/src/tasks.c
@@ -18,10 +18,17 @@
 
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 #include <zephyr/shell/shell.h>
 #include <zephyr/sys/atomic.h>
 
+LOG_MODULE_REGISTER(task_shim, LOG_LEVEL_INF);
+
 #include <soc.h>
+
+#define SAFE_THREAD_NAME(tid)                                           \
+	((tid) && k_thread_name_get((tid)) ? k_thread_name_get((tid)) : \
+					     "(unknown)")
 
 /* Ensure that the idle task is at lower priority than lowest priority task. */
 BUILD_ASSERT(EC_TASK_PRIORITY(EC_TASK_PRIO_LOWEST) < K_IDLE_PRIO,
@@ -203,7 +210,13 @@ task_id_t thread_id_to_task_id(k_tid_t thread_id)
 		}
 	}
 
-	__ASSERT(false, "Failed to map thread to task");
+	if (thread_id->base.thread_state & _THREAD_DUMMY) {
+		return TASK_ID_INVALID;
+	}
+
+	LOG_ERR("Failed to map thread to task: thread_id=%p, name=%s",
+		thread_id, SAFE_THREAD_NAME(thread_id));
+
 	return TASK_ID_INVALID;
 }
 
