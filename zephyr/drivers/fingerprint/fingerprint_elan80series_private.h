0a93547b7e261683e933a142fa1c231de2a7bda5
herman lin <herman.lin@emc.com.tw>
2025-10-29 15:06:34 +0800

    zephyr: fingerprint: rename driver to elan80series
    Rename the existing ELAN80SG fingerprint driver to "elan80series" to
    better reflect its role as a shared driver for multiple ELAN 80-series
    sensors.
    
    BUG=b:450710832
    TEST=Built with zmake -l DEBUG build em32f967 --clobber
    Cherry-picked chromium:6781020 and verified enrollment and matching
    functionality.
    
    Change-Id: I676defa9100b058e0fa1cc90b1021e7a75bf18ab

--- zephyr/drivers/fingerprint/fingerprint_elan80series_private.h
+++ zephyr/drivers/fingerprint/fingerprint_elan80series_private.h
@@ -5,12 +5,10 @@
 
 /* Private sensor interface */
 
-#ifndef ZEPHYR_DRIVERS_FINGERPRINT_ELAN80SG_PRIVATE_H_
-#define ZEPHYR_DRIVERS_FINGERPRINT_ELAN80SG_PRIVATE_H_
+#ifndef ZEPHYR_DRIVERS_FINGERPRINT_ELAN80SERIES_PRIVATE_H_
+#define ZEPHYR_DRIVERS_FINGERPRINT_ELAN80SERIES_PRIVATE_H_
 
 #include <stdint.h>
-
-#define FP_SENSOR_HWID_ELAN 0x4f4f
 
 /* ELAN error codes */
 enum elan_error_code {
