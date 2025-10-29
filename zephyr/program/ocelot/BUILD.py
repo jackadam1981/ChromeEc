8a8a125aa509740cc805a7cbdd614fbda46eafbd
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2025-10-29 16:23:56 +0800

    matsu_it2000: Initial Zephyr EC image
    Create the initial EC image for the matsu_it2000 variant based
    on the matsu board EC files.
    
    BUG=none
    TEST=zmake build matsu_it2000
    
    Change-Id: If43f10b1dcbae3630afb489b39d61a44ac99be84

--- zephyr/program/ocelot/BUILD.py
+++ zephyr/program/ocelot/BUILD.py
@@ -167,6 +167,10 @@ register_it8xxx2_project(
     project_name="matsu",
 )
 
+register_it8xxx2_project(
+    project_name="matsu_it82000",
+)
+
 register_ish_project(
     project_name="matsu-ish",
     zephyr_board="intel_ish_5_8_0",
@@ -182,6 +186,7 @@ register_ish_project(
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="matsu", addr=0x60098)
+assert_rw_fwid_DO_NOT_EDIT(project_name="matsu_it82000", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-npcx", addr=0x80144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-ite", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-mchp", addr=0x40318)
