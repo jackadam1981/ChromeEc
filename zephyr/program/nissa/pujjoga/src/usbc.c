38760bd1312957520909d0bf06067de606e66683
Smit Patel <smit.patel@7rays.corp-partner.google.com>
2025-10-31 15:02:48 +0530

    Pujjoga: Avoid redundant VBUS discharge
    Track the VBUS sourcing state in Pujjoga board code. When potentially
    discharging VBUS, only write to the charger register if the intended
    state differs from the present state. Reduce latency of the Error
    Recovery path.
    
    BUG=b:442730096
    TEST=TEST.PD.FRS.SNK3.4
    
    Change-Id: I5678ea6551c24d584aa769e2103305ae57c4e9f8

--- zephyr/program/nissa/pujjoga/src/usbc.c
+++ zephyr/program/nissa/pujjoga/src/usbc.c
@@ -15,6 +15,8 @@
 #include "usb_pd.h"
 #include "usbc_ppc.h"
 
+#include <stdint.h>
+
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
@@ -23,6 +25,8 @@ LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
 #define CPRINTFUSB(format, args...) cprintf(CC_USBCHARGE, format, ##args)
 
 enum usbc_port { USBC_PORT_C0 = 0, USBC_PORT_C1, USBC_PORT_COUNT };
+
+static bool sourcing_vbus[CONFIG_USB_PD_PORT_MAX_COUNT];
 
 __override uint8_t board_get_usb_pd_port_count(void)
 {
@@ -83,6 +87,7 @@ int board_set_active_charge_port(int port)
 		CPRINTSUSB("C%d: sink path enable failed.", port);
 		return EC_ERROR_UNKNOWN;
 	}
+	sourcing_vbus[port] = false;
 
 	return EC_SUCCESS;
 }
@@ -118,17 +123,26 @@ void reset_nct38xx_port(int port)
 	gpio_reset_port(ioex_port0);
 }
 
+static void notify_power_change(void)
+{
+	/* Notify host of power info change. */
+	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+}
+DECLARE_DEFERRED(notify_power_change);
+
 void pd_power_supply_reset(int port)
 {
+	if (!sourcing_vbus[port])
+		return;
 	/* Disable VBUS. */
 	ppc_vbus_source_enable(port, 0);
+	sourcing_vbus[port] = false;
 
 	/* Enable discharge if we were previously sourcing 5V */
 	if (IS_ENABLED(CONFIG_USB_PD_DISCHARGE))
 		pd_set_vbus_discharge(port, 1);
 
-	/* Notify host of power info change. */
-	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+	hook_call_deferred(&notify_power_change_data, 0);
 }
 
 int pd_set_power_supply_ready(int port)
@@ -149,9 +163,9 @@ int pd_set_power_supply_ready(int port)
 	if (rv) {
 		return rv;
 	}
+	sourcing_vbus[port] = true;
 
-	/* Notify host of power info change. */
-	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+	hook_call_deferred(&notify_power_change_data, 0);
 
 	return EC_SUCCESS;
 }
