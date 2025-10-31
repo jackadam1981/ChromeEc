765ba25879dcab932cc52a60c51b8993c132d8ae
Poornima Tom <poornima.tom@intel.com>
2025-11-01 03:33:31 +0530

    ocelot: Store Board ID under MODEL_ID tag
    Board ID of RVP read via ioexpander is stored in CBI under the tag of
    MODEL_ID. Also, a flag is set, if id is saved under CBI, inorder to
    avoid repeated reading of Board ID from Hardware. This CL also takes
    care of saving and restoring the flag during sysjump to rw region,
    inorder to read from CBI.
    
    BUG=b:455072992, b:426139268
    LOW_COVERAGE_REASON=To be handled in b:416418970
    TEST=cbi & version command retrieves MODEL_ID tag's value
    
    Example data is as below:
    ec:> cbi
    25-11-01 03:07:36.271 [72.813476 CBI Reading board info]
    25-11-01 03:07:36.276 CBI_VERSION: 0x0000
    25-11-01 03:07:36.278 TOTAL_SIZE: 43
    25-11-01 03:07:36.281 BOARD_VERSION: (Error 1)
    25-11-01 03:07:36.285 OEM_ID: (Error 1)
    25-11-01 03:07:36.289 MODEL_ID: 289 (0x121)
    25-11-01 03:07:36.291 SKU_ID: 2 (0x2)
    25-11-01 03:07:36.294 FW_CONFIG: 78605 (0x1330d)
    25-11-01 03:07:36.297 PCB_SUPPLIER: (Error 1)
    25-11-01 03:07:36.300 SSFC: (Error 1)
    25-11-01 03:07:36.302 REWORK_ID: (Error 1)
    
    sysjump rw
    
    25-11-01 03:08:38.016 ec:> version
    25-11-01 03:08:45.397 Chip:     ite it82102 bx
    25-11-01 03:08:45.399 [24.727172 CBI Reading board info]
    25-11-01 03:08:45.403 Board:    289
    25-11-01 03:08:45.404 RO:       ocelotrvp-ite-0.0.0-ea2089b+
    25-11-01 03:08:45.406 RW:       ocelotrvp-ite-0.0.0-ea2089b+
    25-11-01 03:08:45.411 Build:    ocelotrvp-ite-0.0.0-ea2089b+
    25-11-01 03:08:45.415           root@coreboot-NUC7i7BNH
    25-11-01 03:08:45.417 ec:>
    
    Change-Id: I73a30bd32c0e8b9d229b09d2be76628909485e33
    Signed-off-by: Poornima Tom <poornima.tom@intel.com>

--- zephyr/program/ocelot/src/rvp_board_id.c
+++ zephyr/program/ocelot/src/rvp_board_id.c
@@ -3,6 +3,7 @@
  * found in the LICENSE file.
  */
 
+#include "cros_board_info.h"
 #include "drivers/rvp_board_id.h"
 #include "hooks.h"
 
@@ -10,20 +11,23 @@
 
 LOG_MODULE_DECLARE(rvp_board_id, LOG_LEVEL_INF);
 
+/* Flag to indicate board id is read and stored */
+static int rvp_board_id_set;
+
 /*
  * Returns board version on success, -1 on error.
  */
 __override int board_get_version(void)
 {
-	/* Cache the board ID */
-	static int rvp_board_id;
-
+	int rvp_board_id;
 	int board_id = -1;
 	int fab_id = -1;
 
-	/* Board ID is already read */
-	if (rvp_board_id)
+	/* Board ID is already read and stored */
+	if (rvp_board_id_set) {
+		cbi_get_model_id(&rvp_board_id);
 		return rvp_board_id;
+	}
 
 	/* read board_id */
 	board_id = get_rvp_id_config(BOARD_ID);
@@ -35,5 +39,42 @@ __override int board_get_version(void)
 
 	LOG_INF("board version: %d", rvp_board_id);
 
+	cbi_set_model_id(rvp_board_id);
+
+	/* Set flag to indicate that rvp's board id is stored */
+	rvp_board_id_set = 1;
+
 	return rvp_board_id;
 }
+
+#define RVP_SKUID_SYSJUMP_TAG 0x5253 /* RS */
+#define RVP_SKUID_HOOK_VERSION 1
+
+/*
+ * Preserve RVP SKUID flag across a sysjump.
+ */
+static void rvp_sku_id_flag_preserve_state(void)
+{
+	system_add_jump_tag(RVP_SKUID_SYSJUMP_TAG, RVP_SKUID_HOOK_VERSION,
+			    sizeof(rvp_board_id_set), &rvp_board_id_set);
+}
+DECLARE_HOOK(HOOK_SYSJUMP, rvp_sku_id_flag_preserve_state, HOOK_PRIO_DEFAULT);
+
+/*
+ * Restore RVP SKUID after a sysjump.
+ */
+static void rvp_sku_id_restore_state(void)
+{
+	const uint32_t *prev_rvp_board_id_flag;
+	int size, version;
+
+	prev_rvp_board_id_flag = (const uint32_t *)system_get_jump_tag(
+		RVP_SKUID_SYSJUMP_TAG, &version, &size);
+
+	if (prev_rvp_board_id_flag && version == RVP_SKUID_HOOK_VERSION &&
+	    size == sizeof(prev_rvp_board_id_flag)) {
+		memcpy(&rvp_board_id_set, prev_rvp_board_id_flag,
+		       sizeof(rvp_board_id_set));
+	}
+}
+DECLARE_HOOK(HOOK_INIT, rvp_sku_id_restore_state, HOOK_PRIO_DEFAULT);
