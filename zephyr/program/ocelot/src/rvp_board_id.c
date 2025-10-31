ea799d109ba78d8b9dc9e0feb939a11fa3b2c038
Poornima Tom <poornima.tom@intel.com>
2025-11-01 03:33:31 +0530

    ocelot: Board ID handler to read & store in CBI
    This CL takes care of storing the board id of RVP read from ioexpander
    in CBI under the tag of MODEL_ID, if it is not stored in CBI already.
    This CL also ensures to retrieve the board version either from cache or
    CBI accordingly.
    
    BUG=b:455072992, b:426139268
    LOW_COVERAGE_REASON=To be handled in b:416418970
    TEST=cbi & version command retrieves MODEL_ID tag's value correctly
    
    Example data is as below:
    ec:> cbi
    25-11-01 03:07:36.271 [72.813476 CBI Reading board info]
    25-11-01 03:07:36.276 CBI_VERSION: 0x0000
    25-11-01 03:07:36.278 TOTAL_SIZE: 43
    25-11-01 03:07:36.281 BOARD_VERSION: (Error 1)
    25-11-01 03:07:36.285 OEM_ID: (Error 1)
    25-11-01 03:07:36.289 MODEL_ID: 289 (0x121)
    25-11-01 03:07:36.291 SKU_ID: 2 (0x2)
    25-11-01 03:07:36.294 FW_CONFIG: 78605 (0x1330d)
    25-11-01 03:07:36.297 PCB_SUPPLIER: (Error 1)
    25-11-01 03:07:36.300 SSFC: (Error 1)
    25-11-01 03:07:36.302 REWORK_ID: (Error 1)
    
    when EC soft sync is enabled board boots in EC RW region and shows
    board version as below:
    
    25-11-01 03:08:38.016 ec:> version
    25-11-01 03:08:45.397 Chip:     ite it82102 bx
    25-11-01 03:08:45.399 [24.727172 CBI Reading board info]
    		RVP_ID:289 load from CBI
    25-11-01 03:08:45.403 Board:    289
    25-11-01 03:08:45.404 RO:       ocelotrvp-ite-0.0.0-ea2089b+
    25-11-01 03:08:45.406 RW:       ocelotrvp-ite-0.0.0-ea2089b+
    25-11-01 03:08:45.411 Build:    ocelotrvp-ite-0.0.0-ea2089b+
    25-11-01 03:08:45.415           root@coreboot-NUC7i7BNH
    25-11-01 03:08:45.417 ec:>
    
    Change-Id: I73a30bd32c0e8b9d229b09d2be76628909485e33
    Signed-off-by: Poornima Tom <poornima.tom@intel.com>

--- zephyr/program/ocelot/src/rvp_board_id.c
+++ zephyr/program/ocelot/src/rvp_board_id.c
@@ -3,37 +3,69 @@
  * found in the LICENSE file.
  */
 
+#include "cros_board_info.h"
 #include "drivers/rvp_board_id.h"
-#include "hooks.h"
 
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_DECLARE(rvp_board_id, LOG_LEVEL_INF);
+LOG_MODULE_DECLARE(rvp_model_id, LOG_LEVEL_DBG);
+
+#define FAB_ID_SHIFT 8
+#define BOARD_ID_MASK (BIT(FAB_ID_SHIFT) - 1)
+
+static int rvp_model_id = -1;
+
+void ocelotrvp_id_handler(void)
+{
+	int board_id;
+	int fab_id;
+
+	board_id = get_rvp_id_config(BOARD_ID);
+	if (board_id < 0) {
+		LOG_DBG("RVP_ID: get_rvp_id_config.BOARD_ID failed");
+		return;
+	}
+
+	fab_id = get_rvp_id_config(FAB_ID);
+	if (fab_id < 0) {
+		LOG_DBG("RVP_ID: get_rvp_id_config.FAB_ID failed");
+		return;
+	}
+
+	rvp_model_id = (fab_id << FAB_ID_SHIFT) | board_id;
+
+	LOG_DBG("RVP_ID: %d from driver", rvp_model_id);
+
+	uint32_t id_from_cbi;
+	if ((cbi_get_model_id(&id_from_cbi) == EC_SUCCESS) &&
+	    id_from_cbi == rvp_model_id) {
+		// CBI MODEL_ID is up-to-date
+		LOG_DBG("RVP_ID: %d matches CBI", rvp_model_id);
+		return;
+	}
+
+	LOG_INF("RVP_ID: %d store in CBI ", rvp_model_id);
+	cbi_set_model_id(rvp_model_id);
+}
 
 /*
  * Returns board version on success, -1 on error.
  */
 __override int board_get_version(void)
 {
-	/* Cache the board ID */
-	static int rvp_board_id;
+	if (rvp_model_id == -1) {
+		int id;
 
-	int board_id = -1;
-	int fab_id = -1;
+		if (cbi_get_model_id(&id) != EC_SUCCESS) {
+			LOG_INF("RVP_ID: not available");
+			return -1;
+		}
 
-	/* Board ID is already read */
-	if (rvp_board_id)
-		return rvp_board_id;
+		LOG_DBG("RVP_ID: %d load from CBI", id);
+		rvp_model_id = id;
+	}
 
-	/* read board_id */
-	board_id = get_rvp_id_config(BOARD_ID);
-
-	/* read fab id */
-	fab_id = get_rvp_id_config(FAB_ID);
-
-	rvp_board_id = board_id | (fab_id << 8);
-
-	LOG_INF("board version: %d", rvp_board_id);
-
-	return rvp_board_id;
+	//return only board id from model id
+	return rvp_model_id & BOARD_ID_MASK;
 }
+
