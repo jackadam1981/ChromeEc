fc4b6ea424fadf585dbb993735cc903ed8e44a10
herman lin <herman.lin@emc.com.tw>
2025-11-03 13:33:04 +0800

    zephyr: fingerprint: add ELAN elani80sa driver
    This driver provides access to fingerprint sensor features, including
    finger presence detection and fingerprint image capture. It requires
    integration with the elani80sa algorithm library.
    
    BUG=b:450710832
    TEST=Built with zmake -l DEBUG build em32f967 --clobber
    Cherry-picked chromium:6781020 and updated the compatible string to
    "elan,elani80sa" in em32f967.dts.
    Verified enrollment and matching functionality.
    
    Change-Id: I24c944c9ec03b1026c2a507a231f69e725e37a3c

---
+++ zephyr/drivers/fingerprint/fingerprint_elani80sa_config.h
@@ -0,0 +1,108 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_DRIVERS_FINGERPRINT_ELANI80SA_CONFIG_SENSOR_H_
+#define ZEPHYR_DRIVERS_FINGERPRINT_ELANI80SA_CONFIG_SENSOR_H_
+
+#include "fingerprint_elan80series_private.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <zephyr/toolchain.h>
+
+#define FP_SENSOR_HWID_ELAN 0x5253
+
+/* The hardware ID information and FW version */
+#define PID 0x0903 /* USB product ID */
+#define MID 0x01 /* Elan doesn't track model, so this has no meaning. */
+#define VERSION 0x100B /* Elan internal firmware version */
+
+/* Base image address in internal flash */
+#define FLASH_BASE_ADDR 0x10086000
+
+/* Offset for Ft Info record relative to the base image address */
+#define FT_INFO_OFFSET 0x5000
+
+/* Read-only Ft Info address in internal flash (base image address + offset) */
+#define FT_INFO_ADDR (FLASH_BASE_ADDR + FT_INFO_OFFSET)
+
+/**
+ * Elan sensor operation is controlled by sending commands and receiving
+ * through the SPI interface. There are several SPI command codes for
+ * controlling FP sensor:
+ *
+ * - START_SCAN            Start scan
+ * - START_READ_IMAGE      Start read the image
+ * - SRST                  Software reset
+ * - FUSE_LOAD             Load OTP trims data to control registers
+ * - READ_REG_HEAD         Register single read
+ * - WRITE_REG_HEAD        Register burst write
+ * - READ_SERIER_REG_HEAD  Register burst read
+ * - PAGE_SEL              Register page selection
+ * - SENSOR_STATUS         Read sensor status
+ * - HWID_LO               Hardware ID low register
+ * - HWID_HI               Hardware ID high register
+ * - IMG_READY             Image ready register
+ * - HV_ENABLE             High voltage enable register
+ * - HV_CONTROL            High voltage control Register
+ */
+#define START_SCAN 0x01
+#define START_READ_IMAGE 0x10
+#define SRST 0x31
+#define FUSE_LOAD 0x04
+#define READ_REG_HEAD 0x40
+#define WRITE_REG_HEAD 0x80
+#define READ_SERIER_REG_HEAD 0xC0
+#define PAGE_SEL 0x07
+#define SENSOR_STATUS 0x03
+#define HWID_LO 0x04
+#define HWID_HI 0x02
+#define IMG_READY 0x04
+#define HV_ENABLE 0x01
+#define HV_CONTROL 0x00
+
+/* Sensor pixel resolution */
+#define IMAGE_WIDTH 80
+#define IMAGE_HEIGHT 80
+
+/**
+ * Sensor real image size:
+ * ((IMAGE_HEIGHT * ONE_PIXEL_BYTE) + FP_DUMMY_BYTE) * IMAGE_WIDTH
+ */
+#define FP_DUMMY_BYTE 0
+#define ONE_PIXEL_BYTE 2
+#define IMAGE_TOTAL_PIXEL (IMAGE_WIDTH * IMAGE_HEIGHT)
+#define RAW_PIXEL_SIZE (IMAGE_WIDTH * ONE_PIXEL_BYTE)
+#define RAW_DATA_SIZE (RAW_PIXEL_SIZE + FP_DUMMY_BYTE)
+#define IMG_BUF_SIZE (RAW_DATA_SIZE * IMAGE_HEIGHT)
+
+/* SPI tx and rx buffer size */
+#define ELAN_DMA_LOOP IMAGE_HEIGHT
+#define ELAN_DMA_SIZE (IMAGE_TOTAL_PIXEL / ELAN_DMA_LOOP)
+#define ELAN_SPI_TX_BUF_SIZE 2
+#define ELAN_SPI_RX_BUF_SIZE (IMG_BUF_SIZE / ELAN_DMA_LOOP)
+
+/* These are only supported on the EFSAI80SA. */
+#define CHARGE_PUMP_HVIC 0x83
+#define VOLTAGE_HVIC 0x02
+
+/* Polling scan status counter */
+#define POLLING_SCAN_TIMER 10000
+
+/* Re-calibration timer */
+#define REK_TIMES 3
+
+/**
+ * Use setting flash addresses to access ELAN fingerprint data.
+ *
+ * @param base_addr    Base address of the fingerprint base image in flash.
+ * @param ft_info_addr Address of the fingerprint info in flash.
+ *
+ */
+void use_flash_addresses(uint32_t base_addr, uint32_t ft_info_addr);
+
+#endif /* ZEPHYR_DRIVERS_FINGERPRINT_ELANI80SA_CONFIG_SENSOR_H_ */
