20d1fda0d42d9989f60ec952a1cccf0d05cff77e
lin.arthur <lin.arthur@inventec.corp-partner.google.com>
2025-11-03 17:40:34 +0800

    kaladin: modify low ADP blink behavior
    Modify low ADP blink pattern to 30sec.
    
    BUG=b:432662091
    TEST=zmake build kaladin
    LOW_COVERAGE_REASON=test code is not yet ready b:421825142
    
    Change-Id: I0f60a908907f2d21655dab97c1e0c52c5a402d65

--- zephyr/program/trulo/kaladin/src/led.c
+++ zephyr/program/trulo/kaladin/src/led.c
@@ -42,7 +42,10 @@ LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
 
 #define MINIMUM_CHARGING_MV 15000
 
-int blink_cnt, low_adp_blink;
+#define LOW_ADP_BLINK_END_STEP 9
+#define LOW_ADP_BLINK_CYCLE 6
+
+int blink_cnt, low_adp_blink_done, blink_cycle;
 
 const enum ec_led_id supported_led_ids[] = { EC_LED_ID_BATTERY_LED,
 					     EC_LED_ID_POWER_LED };
@@ -218,6 +221,13 @@ static void batt_led_config_tick(uint32_t interval, int duty_inc,
 	batt_led_pulse.duty = 0;
 }
 
+static void low_adp_blink_init(void)
+{
+	blink_cnt = 0;
+	low_adp_blink_done = 0;
+	blink_cycle = 0;
+}
+
 static void led_set_battery(void)
 {
 	static unsigned int battery_ticks;
@@ -270,21 +280,32 @@ static void led_set_battery(void)
 			battery_critical_triggeied = 0;
 			hook_call_deferred(&battery_set_pwm_led_tick_data, -1);
 
+			if (low_adp_blink_done)
+				break;
+
 			/* 500ms on, 500ms off, blink three times, then
 			 * off 2 sec, loop */
-			switch (blink_cnt % 10) {
+			switch (blink_cnt % (LOW_ADP_BLINK_END_STEP + 1)) {
 			case 0:
 			case 2:
 			case 4:
 				led_set_color_battery_duty(LED_AMBER, 100);
 				break;
 			default:
+
+				/* End of blink step for one cycle */
+				if (blink_cnt == LOW_ADP_BLINK_END_STEP)
+					blink_cycle++;
 				led_set_color_battery_duty(LED_OFF, 0);
 				break;
 			}
 			blink_cnt++;
-			if (blink_cnt >= 10)
+			if (blink_cnt >= LOW_ADP_BLINK_END_STEP + 1)
 				blink_cnt = 0;
+
+			/* low ADP blink cyle end, total 30 sec */
+			if (blink_cycle == LOW_ADP_BLINK_CYCLE)
+				low_adp_blink_done = 1;
 		}
 		break;
 	case LED_PWRS_ERROR:
@@ -319,6 +340,9 @@ static void led_set_battery(void)
 		/* Other states don't alter LED behavior */
 		break;
 	}
+
+	if (led_pwr_get_state() != LED_PWRS_INSUFFICIENT_ADAPTER)
+		low_adp_blink_init();
 
 	if (led_pwr_get_state() != LED_PWRS_DISCHARGE) {
 		battery_low_triggeied = 0;
@@ -521,8 +545,7 @@ static void pwr_led_init(void)
 	else
 		pwr_led_shutdown_hook();
 
-	blink_cnt = 0;
-	low_adp_blink = 0;
+	low_adp_blink_init();
 }
 DECLARE_HOOK(HOOK_INIT, pwr_led_init, HOOK_PRIO_DEFAULT);
 
@@ -543,21 +566,3 @@ void board_led_auto_control(void)
 		pwr_led_shutdown_hook();
 	}
 }
-
-static void low_adp_check(void)
-{
-	int active_port, charger_voltage;
-
-	active_port = charge_manager_get_active_charge_port();
-	charger_voltage = charge_manager_get_charger_voltage();
-
-	LOG_INF("kaladin: low_adp_check: active_port %d, charger_voltage: %d",
-		active_port, charger_voltage);
-	if (active_port != -1 && charger_voltage < MINIMUM_CHARGING_MV &&
-	    charger_voltage > 0)
-		low_adp_blink = 1;
-	else
-		low_adp_blink = 0;
-	blink_cnt = 0;
-}
-DECLARE_HOOK(HOOK_POWER_SUPPLY_CHANGE, low_adp_check, HOOK_PRIO_DEFAULT);
