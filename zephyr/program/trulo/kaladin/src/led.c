b6292a4b2111caa4fd44e22a643ee37ebef81d05
lin.arthur <lin.arthur@inventec.corp-partner.google.com>
2025-11-03 17:40:34 +0800

    kaladin: modify low ADP blink behavior
    Modify low ADP blink pattern to 30sec.
    
    BUG=b:432662091
    TEST=zmake build kaladin
    LOW_COVERAGE_REASON=test code is not yet ready b:421825142
    
    Change-Id: I0f60a908907f2d21655dab97c1e0c52c5a402d65

--- zephyr/program/trulo/kaladin/src/led.c
+++ zephyr/program/trulo/kaladin/src/led.c
@@ -42,6 +42,8 @@ LOG_MODULE_DECLARE(ap_pwrseq, LOG_LEVEL_INF);
 
 #define MINIMUM_CHARGING_MV 15000
 
+#define LOW_ADP_BLINK_END_STEP 30
+
 static int blink_cnt;
 
 const enum ec_led_id supported_led_ids[] = { EC_LED_ID_BATTERY_LED,
@@ -271,12 +273,21 @@ static void led_set_battery(void)
 		break;
 	case LED_PWRS_INSUFFICIENT_ADAPTER:
 		if (led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED)) {
+			if (blink_cnt >= LOW_ADP_BLINK_END_STEP)
+				break;
+
 			/* 500ms on, 500ms off, blink three times, then
-			 * off 2 sec, loop */
-			switch (blink_cnt % 10) {
+			 * off 2 sec */
+			switch (blink_cnt) {
 			case 0:
 			case 2:
 			case 4:
+			case 10:
+			case 12:
+			case 14:
+			case 20:
+			case 22:
+			case 24:
 				led_set_color_battery_duty(LED_AMBER, 100);
 				break;
 			default:
@@ -284,8 +295,6 @@ static void led_set_battery(void)
 				break;
 			}
 			blink_cnt++;
-			if (blink_cnt >= 10)
-				blink_cnt = 0;
 		}
 		break;
 	case LED_PWRS_ERROR:
@@ -321,8 +330,7 @@ static void led_set_battery(void)
 		break;
 	}
 
-	if (led_pwr_get_state() != LED_PWRS_INSUFFICIENT_ADAPTER &&
-	    blink_cnt != 0)
+	if (led_pwr_get_state() != LED_PWRS_INSUFFICIENT_ADAPTER)
 		low_adp_blink_init();
 
 	if (led_pwr_get_state() != LED_PWRS_DISCHARGE) {
