613eefd3bb305842cc6a19b7f613a3fcd554ae47
Firas Sammoura <fsammoura@google.com>
2025-11-04 18:53:26 +0000

    fsensor: Align frame logic and validation with current_capture_type
    The Fingerprint Sensor code was incorrectly using
    `global_context.sensor_mode` when retrieving image frame parameters
    (like offset skipping) and for frame command validation. This worked
    because we did not clear all the capture bits after an image
    acquisition.
    
    This commit updates the logic in `fp_command_frame` and
    `get_image_frame_params` to use the more specific
    `global_context.current_capture_type`, which contains the capture type
    of the most recent successful image acquisition.
    
    Additionally, it adds a check to `fp_command_frame` to reject the
    command with `EC_RES_INVALID_PARAM` if `current_capture_type` is
    invalid.
    
    BUG=b:449014224
    TEST=Create a new firmware and make sure ectool works correctly when
         downloading test images
    
    Change-Id: Ia8d6b7d1af99c07c1ffc756c4699b0270dbe86f8

--- common/fpsensor/fpsensor.cc
+++ common/fpsensor/fpsensor.cc
@@ -460,6 +460,10 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 	if (size > args->response_max)
 		return EC_RES_INVALID_PARAM;
 
+	if (global_context.current_capture_type == FP_CAPTURE_TYPE_INVALID) {
+		return EC_RES_INVALID_PARAM;
+	}
+
 	if (idx == FP_FRAME_INDEX_RAW_IMAGE) {
 		/* The host requested a frame. */
 		if (system_is_locked())
@@ -469,7 +473,7 @@ static enum ec_status fp_command_frame(struct host_cmd_handler_args *args)
 		 * the embedded/offset image bytes, like simple, pattern0,
 		 * pattern1, and reset_test.
 		 */
-		if (skip_image_offset(global_context.sensor_mode))
+		if (skip_image_offset(global_context.current_capture_type))
 			offset += FP_SENSOR_IMAGE_OFFSET;
 
 		if (global_context.current_frame_size > sizeof(fp_buffer)) {
