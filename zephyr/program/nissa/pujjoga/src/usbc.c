aab6bd8cf2f40c5b09a51f64cd6624d3c1fd0420
Smit Patel <smit.patel@7rays.corp-partner.google.com>
2025-11-04 17:02:26 +0530

    Pujjoga: Defer pd_send_host_event() to reduce timing delay
    The pd_send_host_event() call is now deferred using
    hook_call_deferred().Reduce latency of the Error Recovery
    path.
    
    BUG=b:442730096
    TEST=TEST.PD.FRS.SNK3.4
    
    Change-Id: Ied5d957d50506179ba07162be3768121338a2eaf

--- zephyr/program/nissa/pujjoga/src/usbc.c
+++ zephyr/program/nissa/pujjoga/src/usbc.c
@@ -24,6 +24,8 @@ LOG_MODULE_DECLARE(nissa, CONFIG_NISSA_LOG_LEVEL);
 
 enum usbc_port { USBC_PORT_C0 = 0, USBC_PORT_C1, USBC_PORT_COUNT };
 
+static bool sourcing_vbus[CONFIG_USB_PD_PORT_MAX_COUNT];
+
 __override uint8_t board_get_usb_pd_port_count(void)
 {
 	return USBC_PORT_COUNT;
@@ -118,6 +120,13 @@ void reset_nct38xx_port(int port)
 	gpio_reset_port(ioex_port0);
 }
 
+static void notify_power_change(void)
+{
+	/* Notify host of power info change. */
+	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+}
+DECLARE_DEFERRED(notify_power_change);
+
 void pd_power_supply_reset(int port)
 {
 	/* Disable VBUS. */
@@ -127,8 +136,7 @@ void pd_power_supply_reset(int port)
 	if (IS_ENABLED(CONFIG_USB_PD_DISCHARGE))
 		pd_set_vbus_discharge(port, 1);
 
-	/* Notify host of power info change. */
-	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+	hook_call_deferred(&notify_power_change_data, 0);
 }
 
 int pd_set_power_supply_ready(int port)
@@ -150,8 +158,7 @@ int pd_set_power_supply_ready(int port)
 		return rv;
 	}
 
-	/* Notify host of power info change. */
-	pd_send_host_event(PD_EVENT_POWER_CHANGE);
+	hook_call_deferred(&notify_power_change_data, 0);
 
 	return EC_SUCCESS;
 }
