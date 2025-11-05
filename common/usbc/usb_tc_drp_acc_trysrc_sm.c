ca19d6e3d80966f7e6c8240d638799bc71d315e4
Smit Patel <smit.patel@7rays.corp-partner.google.com>
2025-11-05 11:53:27 +0530

    Pujjoga:Defer tcpm_enable_auto_discharge_disconnect() to reduce timing delay
    To optimize this, the call is now deferred using hook_call_deferred().
    This reduces latency by ~700–900 µs.
    
    BUG=b:442730096
    TEST=TEST.PD.FRS.SNK3.4
    
    Change-Id: Idcb13c529ecfb19bcbff5e6877e9c6d763f2f3c9

--- common/usbc/usb_tc_drp_acc_trysrc_sm.c
+++ common/usbc/usb_tc_drp_acc_trysrc_sm.c
@@ -521,7 +521,7 @@ __maybe_unused static bool is_try_src_enabled(int port)
  * These pd_ functions are implemented in common/usb_prl_sm.c
  */
 
-void pd_transmit_complete(int port, int status)
+void pd_transmit_complete(int port, int status, const timestamp_t *ts)
 {
 	/* DO NOTHING */
 }
@@ -2825,6 +2825,14 @@ static void tc_attached_snk_run(const int port)
 	sink_power_sub_states(port);
 #endif /* CONFIG_USB_PE_SM */
 }
+static int deferred_port_discharge;
+
+static void deferred_disable_auto_discharge(void)
+{
+	int port = deferred_port_discharge;
+	tcpm_enable_auto_discharge_disconnect(port, 0);
+}
+DECLARE_DEFERRED(deferred_disable_auto_discharge);
 
 static void tc_attached_snk_exit(const int port)
 {
@@ -2843,8 +2851,11 @@ static void tc_attached_snk_exit(const int port)
 		 * happen in tc_cc_open_entry if that is the path we are
 		 * taking.
 		 */
-		if (!TC_CHK_FLAG(port, TC_FLAGS_REQUEST_SUSPEND))
-			tcpm_enable_auto_discharge_disconnect(port, 0);
+		if (!TC_CHK_FLAG(port, TC_FLAGS_REQUEST_SUSPEND)) {
+			deferred_port_discharge = port;
+			hook_call_deferred(
+				&deferred_disable_auto_discharge_data, 0);
+		}
 	}
 
 	/* Stop drawing power */
@@ -4018,8 +4029,10 @@ static void tc_cc_open_entry(const int port)
 	 * sure the TCPC has managed its internal states for disconnecting
 	 * the only source of power it has.
 	 */
-	if (battery_is_present())
-		tcpm_enable_auto_discharge_disconnect(port, 0);
+	if (battery_is_present()) {
+		deferred_port_discharge = port;
+		hook_call_deferred(&deferred_disable_auto_discharge_data, 0);
+	}
 
 	/*
 	 * We may brown out after applying CC open, so flush console first.
