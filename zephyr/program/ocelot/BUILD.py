59de35841b5f50bfbd44c1fb973821b9f268a9b1
Bernardo Perez Priego <bernardo.perez.priego@intel.com>
2025-11-07 11:11:24 -0800

    ocelotrvp-rtk: Add initial board files
    Add minimal configuration for ocelotrvp with Realtek EC.
    
    BUG=b:458637544
    TEST=zmake build ocelotrvp-rtk
    
    Change-Id: Id0cd073b26de632242ff1029c10c146aa8800ed6

--- zephyr/program/ocelot/BUILD.py
+++ zephyr/program/ocelot/BUILD.py
@@ -193,6 +193,13 @@ register_ish_project(
     ],
 )
 
+register_rtk59_project(
+    project_name="ocelotrvp-rtk",
+    extra_kconfig_base_files=[
+        here / "rvp_program.conf",
+    ],
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="kodkod", addr=0x80404)
@@ -202,3 +209,4 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-npcx", addr=0x80144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-ite", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-mchp", addr=0x40318)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ojal", addr=0x80404)
+assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-rtk", addr=0x80404)
