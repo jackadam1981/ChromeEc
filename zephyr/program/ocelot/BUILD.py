5c13e3a6e017faf8ed4c6c16975bf7aff6c082bc
wu.johnson <wu.johnson@inventec.corp-partner.google.com>
2025-11-10 15:01:22 +0800

    [TEST] test for ocicat bring up
    test for kaladin bring up
    
    BUG=b:456563833
    TEST=zmake build ocicat
    
    Change-Id: I69ab88e83aa6cbb6ed52f51095898bf970a8f75c

--- zephyr/program/ocelot/BUILD.py
+++ zephyr/program/ocelot/BUILD.py
@@ -189,6 +189,10 @@ register_ish_project(
     ],
 )
 
+ocicat = register_it8xxx2_project(
+    project_name="ocicat",
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="matsu", addr=0x60098)
@@ -197,3 +201,4 @@ assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-npcx", addr=0x80144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-ite", addr=0x60098)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ocelotrvp-mchp", addr=0x40318)
 assert_rw_fwid_DO_NOT_EDIT(project_name="ojal", addr=0x80404)
+assert_rw_fwid_DO_NOT_EDIT(project_name="ocicat", addr=0x60098)
