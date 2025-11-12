a4e4898583bb02a283efb3af703533caab031685
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2025-11-12 11:27:24 +0800

    (TEST) CONFIG_SHELL_PRINTF_AUTOFLUSH = y / n on it81202cx_evb
    Change-Id: I64d48b8406bd8b4b50ec0eea9bb3253aaaa43e2c

--- zephyr/app/ec/main_shim.c
+++ zephyr/app/ec/main_shim.c
@@ -7,11 +7,24 @@
 #include "host_command.h"
 
 #include <zephyr/kernel.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/sys/printk.h>
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(test_log, LOG_LEVEL_INF);
+
+int record_flush;
 /** A stub main to call the real ec app main function. LCOV_EXCL_START */
 int main(void)
 {
 	ec_app_main();
+
+	record_flush = 1;
+	printk("abc");
+	LOG_ERR("No ACPI node with given name");
+	//execute shell_print() by cmd "i2c scan i2c@f01c40"
+	printk("record_flush %d\n", record_flush);
+	record_flush = 0;
 
 	if (IS_ENABLED(CONFIG_TASK_HOSTCMD_THREAD_MAIN)) {
 		host_command_main();
