6f9bde3ea14c3705ab1acd10f5e98900bc987fc0
Vadim Sukhomlinov <sukhomlinov@google.com>
2025-11-12 15:12:39 -0800

    cr50: Add circuit breaker for the Strongbox-subset
    Implement Strongbox enable/disable commands such that before first
    use Strongbox have to be explicitly enabled.
    
    Strongbox also can be disabled till reboot on request.
    
    BUG=None
    TEST=make tpmtest.py
    
    Change-Id: I3d9ed3cf5dae62b750c438562afbc45db59bed30

--- include/board_id_features.h
+++ include/board_id_features.h
@@ -8,6 +8,64 @@
 #define __EC_BOARD_BOARD_ID_FEATURES_H
 
 #include "board_id.h"
+#include "registers.h"
+
+/* The board id wasn't set when Cr50 booted */
+#define BOARD_ID_FEATURES_UNSET_BID BIT(0)
+
+/* The board id features are initialized */
+#define BOARD_ID_FEATURES_INITIALIZED BIT(1)
+
+/* Bits to store write protect bit state across deep sleep and resets. */
+#define BOARD_ID_FWMP_BLOCK_DEV_RST_EC BIT(2)
+
+/* Bits to store wherever Strongbox was explicitly enabled */
+#define BOARD_ID_FEATURES_SB_ENABLE_SET BIT(3)
+
+/* Bits to store wherever Strongbox was explicitly disabled */
+#define BOARD_ID_FEATURES_SB_DISABLE_SET BIT(4)
+
+/**
+ * We may have CONFIG_BOARD_ID_FEATURES disabled for the part related with
+ * Board Id flags, but still have Board Id features register available.
+ */
+
+/**
+ * Set Board Id features in PWRDN_SCRATCH25, and, if configured in shadow copy.
+ * @param features features to set
+ */
+static inline void store_board_id_features(uint32_t features)
+{
+#ifdef CONFIG_BOARD_ID_FEATURES
+	extern uint32_t board_id_features;
+
+	board_id_features = features;
+#endif
+	GREG32(PMU, PWRDN_SCRATCH25) = features;
+}
+
+/**
+ * Get Board Id features.
+ * @returns bitmask of board id features
+ */
+static inline uint32_t get_board_id_features(void)
+{
+#ifdef CONFIG_BOARD_ID_FEATURES
+	extern uint32_t board_id_features;
+	return board_id_features;
+#else
+	return GREG32(PMU, PWRDN_SCRATCH25);
+#endif
+}
+
+/**
+ * Add features to Board id features.
+ * @param features
+ */
+static inline void add_board_id_features(uint32_t features)
+{
+	store_board_id_features(get_board_id_features() | features);
+}
 
 /* Print the enabled BID features */
 void print_board_id_features(void);
@@ -38,4 +96,4 @@ int bid_feature_id_resets_ec_in_recdev(const struct board_id *id);
  *   false if the board id is not allowed to reset the EC.
  */
 int bid_feature_enabled_pcr_ecrst_recdev(void);
-#endif  /* ! __EC_BOARD_BOARD_ID_FEATURES_H */
+#endif /* ! __EC_BOARD_BOARD_ID_FEATURES_H */
