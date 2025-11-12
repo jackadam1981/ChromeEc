a4d140a089c75fac34c1b9611d726a16b65fc527
Vadim Sukhomlinov <sukhomlinov@google.com>
2025-11-12 15:12:39 -0800

    cr50: Add circuit breaker for the Strongbox-subset
    Implement Strongbox enable/disable commands such that before first
    use Strongbox have to be explicitly enabled.
    
    Strongbox also can be disabled till reboot on request.
    
    BUG=None
    TEST=tpmtest.py -b
    
    Change-Id: I3d9ed3cf5dae62b750c438562afbc45db59bed30

--- common/board_id_features.c
+++ common/board_id_features.c
@@ -10,20 +10,7 @@
 
 #define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ## args)
 
-/* The board id wasn't set when Cr50 booted */
-#define BOARD_ID_FEATURES_UNSET_BID	BIT(0)
-/* The board id features are initialized */
-#define BOARD_ID_FEATURES_INITIALIZED	BIT(1)
-/* Bits to store write protect bit state across deep sleep and resets. */
-#define BOARD_ID_FWMP_BLOCK_DEV_RST_EC	BIT(2)
-
-static uint32_t board_id_features;
-
-static void store_board_id_features(uint32_t features)
-{
-	GREG32(PMU, PWRDN_SCRATCH25) = features;
-	board_id_features = features;
-}
+uint32_t board_id_features;
 
 static void load_board_id_features(void)
 {
