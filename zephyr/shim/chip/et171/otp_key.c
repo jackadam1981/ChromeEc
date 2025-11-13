7af241d8b5d356b315fed2a6f3568936bbe108c7
Firas Sammoura <fsammoura@google.com>
2025-11-13 19:31:43 +0000

    chip/et171: Add build support for OTP key
    Add OTP key support for the et171 chip family using otp_egis_skey3 for
    external flash access in order to leverage the random data within
    otp_egis_skey3 as the OTP_KEY due to ET171's external flash usage.
    
    BUG=b:452342453
    TEST=CQ
    
    Change-Id: Id03fe116deb27b8a3c258ccc5496160b012c6c7c

---
+++ zephyr/shim/chip/et171/otp_key.c
@@ -0,0 +1,68 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* One-Time Programmable (OTP) Key */
+
+#include "common.h"
+#include "console.h"
+#include "openssl/mem.h"
+#include "otp_key.h"
+#include "panic.h"
+#include "printf.h"
+#include "registers.h"
+#include "system.h"
+#include "task.h"
+#include "trng.h"
+#include "util.h"
+
+void otp_key_init(void)
+{
+}
+
+void otp_key_exit(void)
+{
+}
+
+enum ec_error_list otp_key_read(uint8_t *key_buffer)
+{
+	if (key_buffer == NULL) {
+		return API_RET_STATUS_INVALID_SIZE;
+	}
+
+	const size_t otp_key_index = 2;
+	uintptr_t otp_address = 0x64;
+
+	HAL_OTP_Read32(otp_address + otp_key_index * OTP_KEY_SIZE_BYTES /
+					     sizeof(uint32_t),
+		       OTP_KEY_SIZE_BYTES / sizeof(uint32_t),
+		       (uint32_t *)key_buffer);
+
+	return API_RET_STATUS_OK;
+}
+
+enum ec_error_list otp_key_provision(void)
+{
+	enum API_RETURN_STATUS_T otpi_status = API_RET_OTP_STATUS_FAIL;
+	enum ec_error_list ec_status = EC_ERROR_UNKNOWN;
+	uint8_t otp_key_buffer[OTP_KEY_SIZE_BYTES] = { 0 };
+
+	ec_status = otp_key_read(otp_key_buffer);
+	if (ec_status != API_RET_STATUS_OK) {
+		ccprints("Failed to read OTP key with status=%d", ec_status);
+		return ec_status;
+	}
+
+	/*
+	 * If the stored bytes are trivial (all 0's or all 1's), panic.
+	 */
+	if (bytes_are_trivial(otp_key_buffer, OTP_KEY_SIZE_BYTES)) {
+		ccprintf("ERROR! %s OTP key is trivial!\n", __func__);
+		k_oops();
+	}
+
+	OPENSSL_cleanse(otp_key_buffer, OTP_KEY_SIZE_BYTES);
+
+	return API_RET_STATUS_OK;
+}
