618fbdb763c9138b725da0a5d3087f963cbc8c61
herman lin <herman.lin@emc.com.tw>
2025-11-13 14:38:12 +0800

    zephyr: fingerprint: add flash address handling
    Add flash address handling logic in the ELAN80-series fingerprint driver.
    
    BUG=b:450710832
    TEST=Built with zmake -l DEBUG build em32f967 --clobber
    Cherry-picked chromium:6781020 and verified enrollment and matching
    functionality.
    
    Change-Id: I7d1eb1658bb6ff4df14530ac2f9760249acb7e5d

--- zephyr/drivers/fingerprint/fingerprint_elan80series_pal.c
+++ zephyr/drivers/fingerprint/fingerprint_elan80series_pal.c
@@ -37,6 +37,10 @@ static int elan_image_read(uint16_t *short_raw);
 typedef int (*elan_image_read_t)(uint16_t *short_raw);
 elan_image_read_t elan_image_read_func = IMAGE_READER_IMPL;
 
+typedef void (*use_flash_addresses_t)(uint32_t base_addr,
+				      uint32_t ft_info_addr);
+use_flash_addresses_t use_flash_addresses_ptr = use_flash_addresses;
+
 static uint8_t tx_buf[ELAN_SPI_TX_BUF_SIZE];
 static uint8_t rx_buf[ELAN_SPI_RX_BUF_SIZE];
 BUILD_ASSERT(ELAN_SPI_TX_BUF_SIZE == 2);
@@ -500,4 +504,13 @@ void __unused elan_sensor_set_rst(bool state)
 	if (ret < 0) {
 		LOG_ERR("Failed to set FP reset pin, status: %d", ret);
 	}
+}
+
+void elan_use_flash_addresses()
+{
+#ifdef CONFIG_FINGERPRINT_SENSOR_ELAN80SG
+	/* ELAN80SG does not use flash info */
+#else
+	use_flash_addresses_ptr(FLASH_BASE_ADDR, FT_INFO_ADDR);
+#endif
 }
