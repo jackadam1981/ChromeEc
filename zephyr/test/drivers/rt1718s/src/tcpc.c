fd901b1656a54a376257efc11e0b971261b996f9
Abin Roy <abin.roy@7rays.corp-partner.google.com>
2025-11-18 15:40:24 +0530

    Checking tcpci: Avoid redundant alert read
    Added tcpci_tcpc_alert_with_value to avoid the second read of the TCPC
    alert register. tcpci_tcpc_alert_with_value will replace the generic
    tcpci_tcpc_alert in drivers that directly call tcpci_tcpc_alert but
    already have an ALERT value at hand.
    
    BUG=None
    TEST=None
    
    Change-Id: I47b63cbf062a36d5fa8e2bbbf9d619fa2e441de2

--- zephyr/test/drivers/rt1718s/src/tcpc.c
+++ zephyr/test/drivers/rt1718s/src/tcpc.c
@@ -236,3 +236,13 @@ ZTEST(rt1718s_tcpc, test_set_src_ctrl)
 		rt1718s_emul, TCPC_REG_COMMAND, TCPC_REG_COMMAND_SRC_CTRL_LOW,
 		TCPC_REG_COMMAND_SRC_CTRL_HIGH | TCPC_REG_COMMAND_SRC_CTRL_LOW);
 }
+
+ZTEST(rt1718s_tcpc, test_alert_handling)
+{
+	/* Simulate a general TCPCI alert */
+	rt1718s_emul_set_reg(rt1718s_emul, TCPC_REG_ALERT,
+			     ~TCPC_REG_ALERT_VENDOR_DEF & 0xFFFF);
+	/* Call the function under test */
+	rt1718s_tcpm_drv.tcpc_alert(tcpm_rt1718s_port);
+	zassert_ok(rt1718s_tcpm_drv.tcpc_alert(tcpm_rt1718s_port, true));
+}
