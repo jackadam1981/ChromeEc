1cffa750e40a5471041a472e083b6c4000026739
debasmitha <debasmitha.ghosh@7rays.corp-partner.google.com>
2025-11-18 18:00:51 +0530

    charge_manager: Disable sink path when forcing sink current to 0
    When a Sink Requests zero current as part of a power Negotiation with a
    Source, the Sink Shall go to the lowest power state, pSnkSusp(25mW) or
    lower within tSrcTransition (min- 25ms max-35ms).
    
    BUG=b:445132756
    TEST=TEST.PD.PS.SNK.1 PDO Transition - TEST.PD.PS.SNK.1#10
    
    Change-Id: I142e32938444db55972ca29ab5bf3531aa294dcd

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -26,6 +26,7 @@
 #include "usb_pd.h"
 #include "usb_pd_dpm_sm.h"
 #include "usb_pd_tcpm.h"
+#include "usbc_ppc.h"
 #include "util.h"
 #include "zephyr/include/usbc/pdc_dpm.h"
 #ifdef CONFIG_ZEPHYR
@@ -1493,6 +1494,9 @@ void charge_manager_force_ceil(int port, int ceil)
 	 * waiting for our deferred task to run.
 	 */
 	if (left_safe_mode && port == charge_port && ceil < charge_current) {
+		if (ceil == 0) {
+			ppc_vbus_sink_enable(port, 0);
+		}
 		board_set_charge_limit(port, CHARGE_SUPPLIER_PD, ceil,
 				       charge_current_uncapped, charge_voltage);
 		/* Enforcing charge_ceil here prevents race conditions between
