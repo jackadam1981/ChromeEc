3e30ab04561b4bd487b3f49845d6acc18ada2342
debasmitha <debasmitha.ghosh@7rays.corp-partner.google.com>
2025-11-18 18:00:51 +0530

    TCPMv2: Disable the sink path to get the input current down to 1mA
    BUG=b:445132756
    TEST=TEST.PD.PS.SNK.1 PDO Transition - TEST.PD.PS.SNK.1#10
    
    Change-Id: I142e32938444db55972ca29ab5bf3531aa294dcd

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -1404,6 +1404,12 @@ void typec_set_input_current_limit(int port, typec_current_t max_ma,
 		supplier = CHARGE_SUPPLIER_TYPEC_UNDER_1_5A;
 #endif /* CONFIG_USB_CHARGER */
 
+	if (charge.current == 0) {
+		board_vbus_sink_enable(port, 0);
+	} else {
+		board_vbus_sink_enable(port, 1);
+	}
+
 	charge_manager_update_charge(supplier, port, &charge);
 
 	/*
@@ -1493,6 +1499,7 @@ void charge_manager_force_ceil(int port, int ceil)
 	 * waiting for our deferred task to run.
 	 */
 	if (left_safe_mode && port == charge_port && ceil < charge_current) {
+		board_set_active_charge_port(port);
 		board_set_charge_limit(port, CHARGE_SUPPLIER_PD, ceil,
 				       charge_current_uncapped, charge_voltage);
 		/* Enforcing charge_ceil here prevents race conditions between
--- zephyr/program/nissa/pujjoga/src/usbc.c
+++ zephyr/program/nissa/pujjoga/src/usbc.c
@@ -217,3 +217,8 @@ __override bool pd_check_vbus_level(int port, enum vbus_level level)
 		return false;
 	}
 }
+
+int board_vbus_sink_enable(int port, int enable)
+{
+	return ppc_vbus_sink_enable(port, enable);
+}
