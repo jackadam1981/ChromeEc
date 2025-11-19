c278aecf6c1b4c429043843b3d1dab83bb51d0e7
Johnny Chuang <johnny.chuang@elan.corp-partner.google.com>
2025-11-19 21:29:32 +0800

    WIP: Add flash-protect library header and connect EC flash driver hook
    This CL adds the header interface for the flash-protect library
    into the EC repository and updates the cros_flash driver to call
    the library function to perform write-protect operations. The
    current implementation only supports the basic workflow and still
    requires further validation.
    
    The header file name, API shape, and integration approach are
    open for discussion. This CL is intended to facilitate alignment
    with the customer’s system requirements and confirm whether this
    direction is acceptable before completing the remaining work.
    
    BUG=b:450726407
    
    TEST=Built with 'zmake -l DEBUG build niedzica --clobber'.
    Added a console command to invoke flash protect functions of library
    and verified that result as expected.
    
    Cq-Depend: chrome-internal:8787437
    Change-Id: I5fd54bc15850ba20a4bcbb30e45e8e7e0fe35096

--- zephyr/drivers/cros_flash/cros_flash_em32f967.c
+++ zephyr/drivers/cros_flash/cros_flash_em32f967.c
@@ -5,6 +5,7 @@
 
 #define DT_DRV_COMPAT elan_em32f967_cros_flash
 
+#include "cros_flash_em32f967_wp.h"
 #include "flash.h"
 
 #include <zephyr/drivers/flash.h>
@@ -37,6 +38,12 @@ static const struct cros_flash_em32f967_config cros_flash_config = {
 static int cros_flash_em32f967_init(const struct device *dev)
 {
 	LOG_DBG("cros_flash_em32f967_init.");
+	uint32_t offset = 0x0;
+	int bank_count = 0x10;
+
+	LOG_WRN("cros_flash_em32f967_init offset=0x%x bank_count=0x%x.", offset,
+		bank_count);
+	flash_em32_write_protect_1(offset, bank_count);
 
 	return 0;
 }
@@ -102,7 +109,10 @@ static int cros_flash_em32f967_erase(const struct device *dev, int offset,
 static int cros_flash_em32f967_get_protect(const struct device *dev, int bank)
 {
 	LOG_DBG("cros_flash_em32f967_get_protect.");
-	return 0;
+	bool protected = false;
+	protected = flash_em32_check_bank_protected(bank);
+
+	return protected ? 1 : 0;
 }
 
 static uint32_t cros_flash_em32f967_get_protect_flags(const struct device *dev)
@@ -121,6 +131,24 @@ static int cros_flash_em32f967_protect_at_boot(const struct device *dev,
 static int cros_flash_em32f967_protect_now(const struct device *dev, bool all)
 {
 	LOG_DBG("cros_flash_em32f967_protect_now.");
+	LOG_WRN("cros_flash_em32f967_protect_now all=%s.",
+		all ? "true" : "false");
+	uint32_t offset = 0x20000;
+	int bank_count = 0x33;
+
+	if (all) {
+		LOG_WRN("cros_flash_em32f967_protect_now
+			offset = 0x % x bank_count = 0x % x.",
+							  offset,
+			bank_count);
+		flash_em32_write_protect_2(offset, bank_count);
+	} else {
+		LOG_WRN("cros_flash_em32f967_protect_now disable RB+RW wp
+			offset = 0x % x bank_count = 0x % x.",
+							  offset,
+			bank_count);
+		flash_em32_write_protect_2_disable();
+	}
 	return 0;
 }
 
