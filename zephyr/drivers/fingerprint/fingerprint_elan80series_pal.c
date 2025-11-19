03292a46726a3fcdea0ccce2d26486b1b57660b0
herman lin <herman.lin@emc.com.tw>
2025-11-19 14:59:37 +0800

    zephyr: fingerprint: reduce regdata size to 2 bytes
    The regdata buffer only needs 2 bytes for the register read operation.
    
    BUG=b:450710832
    TEST=Built with zmake -l DEBUG build em32f967 --clobber
    Cherry-picked chromium:6781020 and verified enrollment and matching
    functionality.
    
    Change-Id: I8583e51247794454e6fb0aebc98700519ebb15d6

--- zephyr/drivers/fingerprint/fingerprint_elan80series_pal.c
+++ zephyr/drivers/fingerprint/fingerprint_elan80series_pal.c
@@ -243,7 +243,7 @@ static __unused int elan_image_read(uint16_t *short_raw)
 	assert(short_raw != NULL);
 
 	int ret = 0, cnt_timer = 0, rx_index = 0;
-	uint8_t regdata[4] = { 0 };
+	uint8_t regdata[2] = { 0 };
 
 	/* Polling scan status */
 	cnt_timer = 0;
@@ -307,7 +307,7 @@ static __unused int elan_image_read_linewise(uint16_t *short_raw)
 	assert(short_raw != NULL);
 
 	int ret = 0, cnt_timer = 0, rx_index = 0;
-	uint8_t regdata[4] = { 0 };
+	uint8_t regdata[2] = { 0 };
 
 	for (int line_idx = 0; line_idx < ELAN_DMA_LOOP; line_idx++) {
 		/* Polling scan status */
