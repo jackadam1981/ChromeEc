3f763695095cc8e8239df2f74371e28c282d5012
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2025-11-20 09:22:55 +0800

    zephyr: nissa/domika: Replace config option with cmake function
    The previous Kconfig based approach has been removed and
    replaced with a CMake function.
    
    Change-Id: Ic2c0fec72607d99572ccf70329e21f6baf28cab7

---
+++ zephyr/program/nissa/domika/functions.cmake
@@ -0,0 +1,8 @@
+# Copyright 2025 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+function(kernel_lto_allow_list out_var)
+  set(extra timer.c)
+  set(${out_var} "${extra}" PARENT_SCOPE)
+endfunction()
