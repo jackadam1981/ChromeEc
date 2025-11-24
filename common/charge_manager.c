4796e3d3d4b06f6e393edd2c3d6066876d08f281
debasmitha <debasmitha.ghosh@7rays.corp-partner.google.com>
2025-11-24 22:28:00 +0530

    DNS: DEBUG ppc_vbus_sink_enable path
    BUG=b:445132756
    TEST=TEST.PD.PS.SNK.1 PDO Transition - TEST.PD.PS.SNK.1#10
    
    Change-Id: Ic28af1c5af3ef4dab95d983792ff4ca4a4016c80

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -954,13 +954,18 @@ static void charge_manager_refresh(void)
 {
 	/* Always initialize charge port on first pass */
 	static int active_charge_port_initialized;
-	int new_supplier, new_port;
+	int new_supplier, new_port = 0;
 	int new_charge_current, new_charge_current_uncapped;
 	int new_charge_voltage, i;
 	int updated_new_port = CHARGE_PORT_NONE;
 	int updated_old_port = CHARGE_PORT_NONE;
 	int ceil;
 	int power_changed = 0;
+
+	pd_record_timestamp_end(new_port,
+				PD_INTERVAL_CM_ENTRY_TO_RUN_CM_REFRESH);
+
+	pd_record_timestamp_start(new_port, PD_INTERVAL_CM_REFRESH_TO_PPC);
 
 	CM_MUTEX_LOCK(&cm_refresh);
 
@@ -1207,6 +1212,8 @@ static void charge_manager_refresh(void)
 	}
 
 	CM_MUTEX_UNLOCK(&cm_refresh);
+	CPRINTS("Inside CM");
+	pd_print_timestamps(new_port);
 }
 DECLARE_DEFERRED(charge_manager_refresh);
 
@@ -1428,6 +1435,8 @@ charge_manager_update_charge(int supplier, int port,
 
 void charge_manager_update_dualrole(int port, enum dualrole_capabilities cap)
 {
+	pd_record_timestamp_start(
+		port, PD_INTERVAL_CM_ENTRY_TO_RUN_CM_REFRESH); /* Entry to CM */
 	if (!is_pd_port(port))
 		return;
 
@@ -1487,7 +1496,11 @@ void charge_manager_set_ceil(int port, enum ceil_requestor requestor, int ceil)
 
 void charge_manager_force_ceil(int port, int ceil)
 {
+	pd_record_timestamp_start(
+		port, PD_INTERVAL_CM_FORCE_CEIL_TO_PPC_VBUS_SINK_DISABLE);
+
 	CM_MUTEX_LOCK(&cm_refresh);
+
 	/*
 	 * Force our input current to ceil if we're exceeding it, without
 	 * waiting for our deferred task to run.
