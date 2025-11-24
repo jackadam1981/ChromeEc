93b3678b5201571d80c8e7a0fcce96849245e2e1
debasmitha <debasmitha.ghosh@7rays.corp-partner.google.com>
2025-11-24 22:28:00 +0530

    DNS: DEBUG ppc_vbus_sink_enable path
    BUG=b:445132756
    TEST=TEST.PD.PS.SNK.1 PDO Transition - TEST.PD.PS.SNK.1#10
    
    Change-Id: Ic28af1c5af3ef4dab95d983792ff4ca4a4016c80

--- common/charge_manager.c
+++ common/charge_manager.c
@@ -26,6 +26,7 @@
 #include "usb_pd.h"
 #include "usb_pd_dpm_sm.h"
 #include "usb_pd_tcpm.h"
+#include "usbc_ppc.h"
 #include "util.h"
 #include "zephyr/include/usbc/pdc_dpm.h"
 #ifdef CONFIG_ZEPHYR
@@ -962,6 +963,12 @@ static void charge_manager_refresh(void)
 	int ceil;
 	int power_changed = 0;
 
+	CPRINTS("L-966");
+	pd_record_timestamp_end(charge_port,
+				PD_INTERVAL_CM_ENTRY_TO_RUN_CM_REFRESH);
+
+	pd_record_timestamp_start(charge_port, PD_INTERVAL_CM_REFRESH_TO_PPC);
+
 	CM_MUTEX_LOCK(&cm_refresh);
 
 	/* Hunt for an acceptable charge port */
@@ -1207,6 +1214,7 @@ static void charge_manager_refresh(void)
 	}
 
 	CM_MUTEX_UNLOCK(&cm_refresh);
+	pd_print_timestamps(new_port);
 }
 DECLARE_DEFERRED(charge_manager_refresh);
 
@@ -1347,6 +1355,8 @@ static void charge_manager_make_change(enum charge_manager_change_type change,
 
 void charge_manager_invalidate_suppliers(int port)
 {
+	pd_record_timestamp_start(
+		port, PD_INTERVAL_CM_ENTRY_TO_RUN_CM_REFRESH); /* Entry to CM */
 	int i;
 
 	for (i = 0; i < CHARGE_SUPPLIER_COUNT; ++i) {
@@ -1487,12 +1497,19 @@ void charge_manager_set_ceil(int port, enum ceil_requestor requestor, int ceil)
 
 void charge_manager_force_ceil(int port, int ceil)
 {
+	pd_record_timestamp_start(
+		port, PD_INTERVAL_CM_FORCE_CEIL_TO_PPC_VBUS_SINK_DISABLE);
+
 	CM_MUTEX_LOCK(&cm_refresh);
+
 	/*
 	 * Force our input current to ceil if we're exceeding it, without
 	 * waiting for our deferred task to run.
 	 */
 	if (left_safe_mode && port == charge_port && ceil < charge_current) {
+		if (ceil == 0) {
+			ppc_vbus_sink_enable(port, 0);
+		}
 		board_set_charge_limit(port, CHARGE_SUPPLIER_PD, ceil,
 				       charge_current_uncapped, charge_voltage);
 		/* Enforcing charge_ceil here prevents race conditions between
