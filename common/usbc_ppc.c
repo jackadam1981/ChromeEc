64f6b358ceaa804732c2bc617b0cadf1facf5f98
debasmitha <debasmitha.ghosh@7rays.corp-partner.google.com>
2025-11-24 22:28:00 +0530

    DNS: DEBUG ppc_vbus_sink_enable path
    BUG=b:445132756
    TEST=TEST.PD.PS.SNK.1 PDO Transition - TEST.PD.PS.SNK.1#10
    
    Change-Id: Ic28af1c5af3ef4dab95d983792ff4ca4a4016c80

--- common/usbc_ppc.c
+++ common/usbc_ppc.c
@@ -219,6 +219,13 @@ int ppc_dev_is_connected(int port, enum ppc_device_role dev)
 
 test_mockable int ppc_vbus_sink_enable(int port, int enable)
 {
+	pd_record_timestamp_end(port, PD_INTERVAL_CM_REFRESH_TO_PPC);
+	pd_record_timestamp_end(
+		port, PD_INTERVAL_CM_FORCE_CEIL_TO_PPC_VBUS_SINK_DISABLE);
+	pd_record_timestamp_start(
+		port,
+		PD_INTERVAL_PPC_VBUS_SINK_DISABLE_TO_SYV682X_VBUS_SINK_DISABLE);
+
 	int rv = EC_ERROR_UNIMPLEMENTED;
 	const struct ppc_config_t *ppc;
 
