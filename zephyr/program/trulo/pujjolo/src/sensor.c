druth0/chrome-ec

commit 1bc858e95d75d452bc87251f44c07a45739cd3d5

pujjolo-ish: Disable motionsense on clamshell
Use form_factor to disable motinsense on clamshell.

BUG=b:417599885
TEST=Check "ectool --name=cros_ish motionsense info" correct on
 clamshell/convertible.
LOW_COVERAGE_REASON=test code is not yet ready b:406070014

Change-Id: Ia346f8e4691695fea4a791e34ac7b4202f9309be
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6874727
Tested-by: David1 Huang <david1.huang@lcfc.corp-partner.google.com>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Reviewed-by: Ashley Shen <shenashley@google.com>
Commit-Queue: Ashley Shen <shenashley@google.com>
Reviewed-by: Derek Huang <derekhuang@google.com>

zephyr/program/trulo/pujjolo/src/sensor.c
@@ -59,6 +59,23 @@ static void motionsense_init(void)
 		LOG_ERR("error retriving CBI config: %d", ret);
 		return;
 	}
+
+	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
+		if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
+			/* Only valid in EC */
+			gmr_tablet_switch_disable();
+		}
+		motion_sensor_count = 0;
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_lid_imu));
+		gpio_disable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_imu));
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_acc_int_l),
+				      GPIO_INPUT | GPIO_PULL_UP);
+		gpio_pin_configure_dt(GPIO_DT_FROM_NODELABEL(gpio_imu_int_l),
+				      GPIO_INPUT | GPIO_PULL_UP);
+		LOG_INF("Board is Clamshell");
+	} else if (sensor_fwconfig == FORM_FACTOR_CONVERTIBLE) {
+		LOG_INF("Board is Convertible");
+	}
 }
 DECLARE_HOOK(HOOK_INIT, motionsense_init, HOOK_PRIO_DEFAULT);
 

zephyr/program/trulo/pujjolo/cbi.dtsi
@@ -65,7 +65,7 @@
 
 		ish {
 			enum-name = "ISH";
-			start = <2>;
+			start = <29>;
 			size = <1>;
 
 			ish-disabled {
@@ -83,7 +83,7 @@
 
 		form-factor {
 			enum-name = "FORM_FACTOR";
-			start = <3>;
+			start = <15>;
 			size = <1>;
 
 			form-factor-clamshell {

jackadam1981/ChromeEc

commit caf6a8a252b204abfaa3ecfa0bc6d5c6a5d80d84

pujjolo: Do not use CONFIG_SOC_FAMILY_INTEL_ISH outside of chip code
Instead using ISH, use a define that protect a function.

Fixes: 1bc858e95d75 ("pujjolo-ish: Disable motionsense on clamshell")

BUG=b:417599885
TEST=compile

Change-Id: Iec57f9495c02debe6b190d2ad93ef397aaccd270

zephyr/program/trulo/pujjolo/src/sensor.c
@@ -61,8 +61,9 @@ static void motionsense_init(void)
 	}
 
 	if (sensor_fwconfig == FORM_FACTOR_CLAMSHELL) {
-		if (!IS_ENABLED(CONFIG_SOC_FAMILY_INTEL_ISH)) {
-			/* Only valid in EC */
+		if (IS_ENABLED(CONFIG_GMR_TABLET_MODE)) {
+			/* Disable GMR interrupt on EC that have GMR code
+			 * support compiled-in. */
 			gmr_tablet_switch_disable();
 		}
 		motion_sensor_count = 0;