70fb2dde9541aaaadf01d28b6800b8e577486a24
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2025-11-25 16:42:19 +0800

    (TEST) tune c-port0 cc parameters for eye diagram
    Change-Id: I3a52b2da7b6a814968626a5bb2be322f61d3cd4d

--- driver/tcpm/it83xx_pd.h
+++ driver/tcpm/it83xx_pd.h
@@ -213,6 +213,7 @@
 #define IT83XX_USBPD_CCPSR0(p) REG8(IT83XX_USBPD_BASE(p) + 0x70)
 #define IT83XX_USBPD_CCPSR3_RISE(p) REG8(IT83XX_USBPD_BASE(p) + 0x73)
 #define IT83XX_USBPD_CCPSR4_FALL(p) REG8(IT83XX_USBPD_BASE(p) + 0x74)
+#define IT8XXX2_USBPD_CCPSR5_SWING(p) REG8(IT83XX_USBPD_BASE(p) + 0x75)
 #endif /* !defined(CONFIG_USB_PD_TCPM_DRIVER_IT83XX) */
 
 /*
@@ -415,6 +416,10 @@ enum tuning_unit {
 	IT83XX_TX_PRE_DRIVING_TIME_1_UNIT,
 	IT83XX_TX_PRE_DRIVING_TIME_2_UNIT,
 	IT83XX_TX_PRE_DRIVING_TIME_3_UNIT,
+	IT83XX_TX_PRE_DRIVING_TIME_4_UNIT,
+	IT83XX_TX_PRE_DRIVING_TIME_5_UNIT,
+	IT83XX_TX_PRE_DRIVING_TIME_6_UNIT,
+	IT83XX_TX_PRE_DRIVING_TIME_7_UNIT,
 };
 
 struct usbpd_ctrl_t {
@@ -425,8 +430,10 @@ struct usbpd_ctrl_t {
 
 /* Data structure for board to adjust pd port rising and falling time */
 struct cc_para_t {
+	enum tuning_unit rc_filter;
 	enum tuning_unit rising_time;
 	enum tuning_unit falling_time;
+	enum tuning_unit swing_time;
 };
 
 extern const struct usbpd_ctrl_t usbpd_ctrl_regs[];
--- driver/tcpm/it8xxx2.c
+++ driver/tcpm/it8xxx2.c
@@ -842,6 +842,8 @@ static void it8xxx2_init(enum usbpd_port port, int role)
 
 		IT83XX_USBPD_CCPSR3_RISE(port) = ptr->rising_time;
 		IT83XX_USBPD_CCPSR4_FALL(port) = ptr->falling_time;
+		IT8XXX2_USBPD_CCPSR5_SWING(port) = ptr->swing_time; //it8320 build error?
+		IT83XX_USBPD_CCPSR0(port) = ptr->rc_filter;
 	}
 	/* Reset and disable HW auto generate message header */
 	IT83XX_USBPD_PDMSR(port) &= ~USBPD_REG_MASK_DISABLE_AUTO_GEN_TX_HEADER;
