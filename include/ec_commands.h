f72cff1025d09a42b6f8811d38a29eb0af3aff97
Subrata Banik <subratabanik@google.com>
2025-11-30 03:14:38 +0000

    ec_commands: Define EC_REBOOT_FLAG_FORCE_REBOOT
    Define the EC_REBOOT_FLAG_FORCE_REBOOT macro with a value of 0 for use
    with ec_params_reboot_ec.reboot_flags.
    
    This explicitly identifies the default behavior of issuing a standard
    EC-based reboot, improving code clarity when specifying the desired
    reboot type.
    
    BUG=b:464782675
    TEST=Able to build and boot google/bluey.
    
    Change-Id: I47f83114c13b4f29c9406447427f71e2d0f05201
    Signed-off-by: Subrata Banik <subratabanik@google.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -5825,6 +5825,7 @@ enum ec_reboot_cmd {
 };
 
 /* Flags for ec_params_reboot_ec.reboot_flags */
+#define EC_REBOOT_FLAG_FORCE_REBOOT 0 /* Issue EC based reboot */
 #define EC_REBOOT_FLAG_RESERVED0 BIT(0) /* Was recovery request */
 #define EC_REBOOT_FLAG_ON_AP_SHUTDOWN BIT(1) /* Reboot after AP shutdown */
 #define EC_REBOOT_FLAG_SWITCH_RW_SLOT BIT(2) /* Switch RW slot */
