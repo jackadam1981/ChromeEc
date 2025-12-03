215a7b6ea70c11f2fe428cf5b727d9da2753f5db
Filip Luczys <luczys@google.com>
2025-12-03 14:55:09 +0100

    fpsensor: add FP SDCP establish host command
    Add FP SDCP establish command in order to finalize SDCP authorization.
    
    BUG=b:464441967
    TEST=Add a command, send the command as host and make sure response is
    correct
    
    Change-Id: I6a5c82c734b760922ef99e1c5f35ebf63d58820d

--- include/fpsensor/fpsensor.h
+++ include/fpsensor/fpsensor.h
@@ -198,6 +198,17 @@ int fp_vendor_command(uint32_t param, uint8_t *buf, size_t buf_size);
  */
 int fp_sdcp_command(struct ec_response_fp_sdcp_claim *res);
 
+/**
+ * FP SDCP defined routine for getting private ephemeral key
+ *
+ * @param[out] buf buffer for sk_f to fill
+ * @param[in] buf_size buffer size
+
+ * @return 0 on success
+ * @return negative value on error
+ */
+int fp_sdcp_sk_f(uint8_t *buf, size_t buf_size);
+
 #ifdef CONFIG_ZEPHYR
 /**
  * Put the sensor into idle state
