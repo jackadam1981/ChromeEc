5640b11e8dc579d697a23625b5055f75409600d4
Filip Luczys <luczys@google.com>
2025-12-03 14:55:09 +0100

    fpsensor: add FP SDCP establish host command
    Add FP SDCP establish command in order to finalize SDCP authorization.
    
    BUG=b:464441967
    TEST=Add a command, send the command as host and make sure response is
    correct
    
    Change-Id: I6a5c82c734b760922ef99e1c5f35ebf63d58820d

--- zephyr/test/fingerprint/task/src/fpsensor_sdcp_command.cc
+++ zephyr/test/fingerprint/task/src/fpsensor_sdcp_command.cc
@@ -27,6 +27,28 @@ int fp_sdcp_command(struct ec_response_fp_sdcp_claim *res)
 	return fp_sdcp_command_custom_ret;
 }
 
+static const uint8_t pk_g[] = {
+	0x04, 0x85, 0xAD, 0x35, 0x23, 0x05, 0x1E, 0x33, 0x3F, 0xCA, 0xA7,
+	0xEA, 0xA5, 0x88, 0x33, 0x12, 0x95, 0xA7, 0xB5, 0x98, 0x9F, 0x32,
+	0xEF, 0x7D, 0xE9, 0xF8, 0x70, 0x14, 0x5E, 0x89, 0xCB, 0xDE, 0x1F,
+	0xD1, 0xDC, 0x91, 0xC6, 0xE6, 0x5B, 0x1E, 0x3C, 0x01, 0x6C, 0xE6,
+	0x50, 0x25, 0x5D, 0x89, 0xCF, 0xB7, 0x8D, 0x88, 0xB9, 0x0D, 0x09,
+	0x41, 0xF1, 0x09, 0x4F, 0x61, 0x55, 0x6C, 0xC4, 0x96, 0x6B,
+};
+
+static uint8_t sk_f[32];
+
+static int fp_sdcp_sk_f_custom_ret;
+
+int fp_sdcp_sk_f(uint8_t *buf, size_t buf_size)
+{
+	zassert_equal(sizeof(sk_f), 32);
+	zassert_equal(buf_size, 32);
+	zassert_not_equal(buf, nullptr);
+	memcpy(buf, sk_f, sizeof(sk_f));
+	return fp_sdcp_sk_f_custom_ret;
+}
+
 ZTEST(hc_fp_sdcp, test_fp_sdcp_ok)
 {
 	int ret;
@@ -73,11 +95,76 @@ ZTEST(hc_fp_sdcp, test_shell_command_failure)
 	zassert_equal(rv, EC_RES_ERROR);
 }
 
+ZTEST(hc_fp_sdcp, test_fp_sdcp_establish_ok)
+{
+	int ret;
+	struct ec_params_fp_sdcp_establish params;
+	memcpy(params.pk_g, pk_g, sizeof(pk_g));
+
+	ret = ec_cmd_fp_sdcp_establish(NULL, &params);
+	zassert_equal(EC_RES_SUCCESS, ret);
+}
+
+ZTEST(hc_fp_sdcp, test_fp_sdcp_establish_pk_g_first_not_ok)
+{
+	int ret;
+	struct ec_params_fp_sdcp_establish params;
+	memcpy(params.pk_g, pk_g, sizeof(pk_g));
+
+	params.pk_g[0] = 0x05;
+
+	ret = ec_cmd_fp_sdcp_establish(NULL, &params);
+	zassert_equal(EC_RES_INVALID_PARAM, ret);
+}
+
+ZTEST(hc_fp_sdcp, test_fp_sdcp_establish_pk_g_later_not_ok)
+{
+	int ret;
+	struct ec_params_fp_sdcp_establish params;
+	memcpy(params.pk_g, pk_g, sizeof(pk_g));
+
+	params.pk_g[1] = 0x00;
+
+	ret = ec_cmd_fp_sdcp_establish(NULL, &params);
+	zassert_equal(EC_RES_INVALID_PARAM, ret);
+}
+
+ZTEST(hc_fp_sdcp, test_fp_sdcp_establish_fp_sdcp_sk_f_not_ok)
+{
+	int ret;
+	struct ec_params_fp_sdcp_establish params;
+	memcpy(params.pk_g, pk_g, sizeof(pk_g));
+
+	fp_sdcp_sk_f_custom_ret = EC_ERROR_UNKNOWN;
+
+	ret = ec_cmd_fp_sdcp_establish(NULL, &params);
+	zassert_equal(EC_RES_ERROR, ret);
+}
+
+ZTEST(hc_fp_sdcp, test_fp_sdcp_establish_sk_f_not_ok)
+{
+	int ret;
+	struct ec_params_fp_sdcp_establish params;
+	memcpy(params.pk_g, pk_g, sizeof(pk_g));
+
+	memset(sk_f, 0, sizeof(sk_f));
+
+	ret = ec_cmd_fp_sdcp_establish(NULL, &params);
+	zassert_equal(EC_RES_ERROR, ret);
+}
+
 static void reset(void *data)
 {
 	ARG_UNUSED(data);
 
 	fp_sdcp_command_custom_ret = 0;
+	fp_sdcp_sk_f_custom_ret = 0;
+	static const uint8_t sk[] = { 0xd6, 0x33, 0xa7, 0x27, 0x57, 0x64, 0xd2,
+				      0xeb, 0x16, 0x07, 0x35, 0x69, 0xd7, 0x9e,
+				      0xf1, 0xe6, 0x8c, 0xd4, 0x02, 0x54, 0x3a,
+				      0x38, 0xce, 0x77, 0xf7, 0xce, 0x0d, 0x0e,
+				      0xfc, 0xe9, 0xd6, 0xb0 };
+	memcpy(sk_f, sk, sizeof(sk));
 }
 
 ZTEST_SUITE(hc_fp_sdcp, NULL, NULL, reset, reset, NULL);
