9036f719c492d9cd37616010dad751a17ed237cf
Firas Sammoura <fsammoura@google.com>
2025-12-04 12:38:04 -0800

    common: add pmp logging capability
    Change-Id: I033f656e6ec81dc9c4c77d342ffecfb37c87fcd6

--- zephyr/shim/chip/egis/rollback.c
+++ zephyr/shim/chip/egis/rollback.c
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "system.h"
 #include <zephyr/arch/riscv/pmp.h>
 
 int mpu_lock_rollback(int lock)
