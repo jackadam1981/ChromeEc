744c3ec2e3a8d6fa0174df7167ec506d9a000d0d
Edward Hill <ecgh@chromium.org>
2025-12-05 09:57:21 -0700

    cr50/strongbox: Fix signature.
    BUG=b:410110761
    TEST=gscdevboard.Cr50Strongbox
    
    Change-Id: I65a248e78b9bee6e9a6cfdf13a48dc939d94b2b9

--- common/strongbox.c
+++ common/strongbox.c
@@ -833,9 +833,9 @@ enum dcrypto_result cryptokey_generate(struct km *km, enum km_algorithm alg,
 			/* Test P256 key candidate and save its public key. */
 			result = DCRYPTO_p256_key_from_bytes(
 				&km->last_pk_x, &km->last_pk_y, &d, key);
+			p256_to_bin(&d, (uint8_t *)key);
 			if (result != DCRYPTO_RETRY)
 				break;
-			memcpy(key, &d, sizeof(d));
 		}
 	} while (result != DCRYPTO_OK);
 	return result;
@@ -1506,7 +1506,7 @@ static enum strongbox_error sb_Update(struct km *km, uint32_t *buf,
 
 	if (km->ops[op_index].attrs.digest == KM_DIGEST_SHA_2_256) {
 		SHA256_sw_update(&km->ops[op_index].sha256_ctx,
-				 (uint8_t *)buf + 2, update_size);
+				 (uint8_t *)(buf + 2), update_size);
 	} else if (km->ops[op_index].attrs.digest == KM_DIGEST_NONE) {
 		if (update_size + km->ops[op_index].none_ctx.update_size >
 		    sizeof(km->ops[op_index].none_ctx.update_context))
@@ -1553,7 +1553,8 @@ static enum strongbox_error sb_Finish(struct km *km, uint32_t *buf,
 {
 	size_t op_index, update_size;
 	enum dcrypto_result result;
-	const uint32_t *sign_data = NULL;
+	p256_int p256_digest, p256_key, p256_r, p256_s;
+	const uint8_t *sign_data = NULL;
 
 	/* Minimum 2 words - Operation Handler and Input Size */
 	if (req_len_words < 2)
@@ -1575,8 +1576,8 @@ static enum strongbox_error sb_Finish(struct km *km, uint32_t *buf,
 
 	if (km->ops[op_index].attrs.digest == KM_DIGEST_SHA_2_256) {
 		SHA256_sw_update(&km->ops[op_index].sha256_ctx,
-				 (uint8_t *)buf + 2, update_size);
-		sign_data = SHA256_sw_final(&km->ops[op_index].sha256_ctx)->b32;
+				 (uint8_t *)(buf + 2), update_size);
+		sign_data = SHA256_sw_final(&km->ops[op_index].sha256_ctx)->b8;
 	} else if (km->ops[op_index].attrs.digest == KM_DIGEST_NONE) {
 		if (update_size + km->ops[op_index].none_ctx.update_size >
 		    sizeof(km->ops[op_index].none_ctx.update_context))
@@ -1586,15 +1587,18 @@ static enum strongbox_error sb_Finish(struct km *km, uint32_t *buf,
 			       km->ops[op_index].none_ctx.update_size,
 		       buf + 2, update_size);
 		km->ops[op_index].none_ctx.update_size += update_size;
-		sign_data = km->ops[op_index].none_ctx.update_context;
+		sign_data = (uint8_t *)
+			km->ops[op_index].none_ctx.update_context;
 
 	} else
 		return SBERR_UnsupportedAlgorithm;
 
-	result = DCRYPTO_p256_ecdsa_sign((p256_int *)km->ops[op_index].key,
-					 (p256_int *)sign_data,
-					 (p256_int *)&buf[0],
-					 (p256_int *)&buf[8]);
+	p256_from_bin(sign_data, &p256_digest);
+	p256_from_bin((uint8_t *)km->ops[op_index].key, &p256_key);
+	result = DCRYPTO_p256_ecdsa_sign(&p256_key, &p256_digest,
+		&p256_r, &p256_s);
+	p256_to_bin(&p256_r, (uint8_t *)&buf[0]);
+	p256_to_bin(&p256_s, (uint8_t *)&buf[8]);
 
 	if (result != DCRYPTO_OK)
 		return SBERR_UnknownError;
