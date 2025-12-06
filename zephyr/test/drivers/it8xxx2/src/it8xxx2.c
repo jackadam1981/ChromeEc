b39272cc551b5c476cf22365e72005e93946198e
Abin Roy <abin.roy@7rays.corp-partner.google.com>
2025-12-06 08:52:33 +0530

    DNS:tcpc/it8xxx2: Implement test coverage
    This adds code coverage tests for the it8xxx2 TCPC driver. Driver
    coverage results are at 84% with the remaining 16% accounted for by
    unreachable I2C error handling code.
    
    BUG=None
    TEST=None
    
    Change-Id: I39890fa4e5d871e1f002827c4929c08560e53f24

---
+++ zephyr/test/drivers/it8xxx2/src/it8xxx2.c
@@ -0,0 +1,35 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "driver/tcpm/it83xx_pd.h"
+#include "driver/tcpm/it8xxx2_pd_public.h"
+#include "driver/tcpm/tcpci.h"
+#include "emul/tcpc/emul_it8xxx2.h"
+#include "emul/tcpc/emul_tcpci.h"
+#include "test/drivers/test_state.h"
+
+#include <zephyr/drivers/emul.h>
+#include <zephyr/fff.h>
+#include <zephyr/ztest.h>
+
+#define IT8XXX2_PORT 1
+// #define IT8XXX2_NODE DT_NODELABEL(it8xxx2_emul)
+
+// const struct emul *it8xxx2_emul = EMUL_DT_GET(IT8XXX2_NODE);
+
+ZTEST(it8xxx2_tcpc, test_disable_irq)
+{
+	/* The expected IRQ number for IT8XXX2_PORT (which is 1) */
+	const int expected_irq = usbpd_ctrl_regs[IT8XXX2_PORT].irq;
+
+	/* Execute the function that contains the target code */
+	task_disable_irq(usbpd_ctrl_regs[port].irq);
+
+	/* Verify that task_disable_irq was called exactly once */
+	zassert_equal(task_disable_irq.call_count, 1);
+
+	/* Verify that task_disable_irq was called with the correct argument */
+	zassert_equal(task_disable_irq.arg0_val, expected_irq);
+}
