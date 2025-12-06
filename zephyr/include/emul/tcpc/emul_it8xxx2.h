05db5a0c87b841df2edc01f55425110b38816b40
Abin Roy <abin.roy@7rays.corp-partner.google.com>
2025-12-06 08:49:56 +0530

    DNS:zephyr/emul/tcpc/it8xxx2: Add it8xxx2 emulator
    Add a minimal it8xxx2 emulator to support driver testing. This emulator
    is a thin layer on top of the existing TCPCI emulator to recognize
    vendor specific registers. These vendor specific registers are
    implemented as accessible registers with memory semantics.
    
    BUG=None
    TEST=None
    
    Change-Id: I8abb7a5cde0f9bbfac7b44e8e326d88652dae4ff

---
+++ zephyr/include/emul/tcpc/emul_it8xxx2.h
@@ -0,0 +1,11 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef EMUL_IT8XXX2_H
+#define EMUL_IT8XXX2_H
+
+#include "driver/tcpm/it83xx_pd.h"
+
+#endif /* EMUL_IT8XXX2_H */
