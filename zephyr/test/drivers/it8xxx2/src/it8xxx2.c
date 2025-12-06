89519dc6713576aed8a0767db085cdcf162b2e75
Abin Roy <abin.roy@7rays.corp-partner.google.com>
2025-12-06 08:52:33 +0530

    DNS:tcpc/it8xxx2: Implement test coverage
    This adds code coverage tests for the it8xxx2 TCPC driver. Driver
    coverage results are at 84% with the remaining 16% accounted for by
    unreachable I2C error handling code.
    
    BUG=None
    TEST=None
    
    Change-Id: I39890fa4e5d871e1f002827c4929c08560e53f24

---
+++ zephyr/test/drivers/it8xxx2/src/it8xxx2.c
@@ -0,0 +1,52 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#include "chip/it83xx/registers.h"
+#include "driver/tcpm/it83xx_pd.h"
+#include "driver/tcpm/it8xxx2_pd_public.h"
+#include "driver/tcpm/tcpci.h"
+#include "emul/tcpc/emul_it8xxx2.h"
+#include "emul/tcpc/emul_tcpci.h"
+#include "test/drivers/tcpci_test_common.h"
+#include "test/drivers/test_state.h"
+
+#include <zephyr/drivers/emul.h>
+#include <zephyr/fff.h>
+#include <zephyr/ztest.h>
+
+#define IT8XXX2_PORT 1
+
+const struct usbpd_ctrl_t usbpd_ctrl_regs[] = {
+	{ &IT83XX_GPIO_GPCRF4, &IT83XX_GPIO_GPCRF5, IT83XX_IRQ_USBPD0 },
+	{ &IT83XX_GPIO_GPCRH1, &IT83XX_GPIO_GPCRH2, IT83XX_IRQ_USBPD1 },
+};
+const struct tcpm_drv it8xxx2_tcpm_drv = {
+	.init = &it8xxx2_tcpm_init,
+};
+
+ZTEST_SUITE(tcpc_it8xxx2, drivers_predicate_post_main, NULL, NULL, NULL, NULL);
+
+ZTEST(tcpc_it8xxx2, test_enter_l_p_m)
+{
+	zassert_ok(tcpm_enter_low_power_mode(IT8XXX2_PORT));
+}
+
+ZTEST(tcpc_it8xxx2, test_set_vconn)
+{
+	zassert_ok(tcpm_set_vconn(IT8XXX2_PORT, 0));
+	zassert_ok(tcpm_set_vconn(IT8XXX2_PORT, 1));
+	zassert_ok(tcpm_set_vconn(IT8XXX2_PORT, 0));
+}
+
+ZTEST(tcpc_it8xxx2, test_tcpm_init)
+{
+	/* The expected IRQ number for IT8XXX2_PORT (which is 1) */
+	const int expected_irq = usbpd_ctrl_regs[IT8XXX2_PORT].irq;
+
+	/* Execute the function that contains the target code */
+	it8xxx2_tcpm_drv.init(IT8XXX2_PORT);
+
+	/* Verify that task_disable_irq was called with the correct argument */
+	zassert_equal(IT83XX_IRQ_USBPD1, expected_irq);
+}
