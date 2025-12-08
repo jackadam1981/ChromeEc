45d4029fe655ccc50feddd4ca4b7eb73e990fdd9
Jayvik Desai <jayvik@google.com>
2025-12-09 00:33:23 +0530

    extpower: Update host events on init to prevent async state
    Previously, during extpower_init, only the battery memmap flag was
    updated with the AC state. The corresponding host event was not
    triggered. This created a condition where the host's view of the AC
    state could be out of sync on boot.
    
    This commit fixes this by calling extpower_update_host_events during
    init. This ensures the proper host event mask is set and the AC event
    is sent, preventing a missed update and ensuring the host has the
    correct state from startup.
    
    BUG=none
    TEST=Host successfully reads the AC connection status via the
    host event register during the chipset startup sequence.
    
    Change-Id: I10417d24c5edc32b29f0a17c341d1383d4f89f03

--- common/extpower_gpio.c
+++ common/extpower_gpio.c
@@ -12,7 +12,34 @@
 #include "host_command.h"
 #include "timer.h"
 
+#define EC_HOST_EVENT_AC_CONNECTED_MASK \
+	EC_HOST_EVENT_MASK(EC_HOST_EVENT_AC_CONNECTED)
+#define EC_HOST_EVENT_AC_DISCONNECTED_MASK \
+	EC_HOST_EVENT_MASK(EC_HOST_EVENT_AC_DISCONNECTED)
+
 static int debounced_extpower_presence;
+
+void extpower_update_host_events(int is_present)
+{
+	uint8_t *memmap_batt_flags;
+	memmap_batt_flags = host_get_memmap(EC_MEMMAP_BATT_FLAG);
+	host_event_t mask;
+
+	/* Forward notification to host */
+	if (is_present) {
+		*memmap_batt_flags |= EC_BATT_FLAG_AC_PRESENT;
+		host_set_single_event(EC_HOST_EVENT_AC_CONNECTED);
+		mask = EC_HOST_EVENT_AC_DISCONNECTED_MASK;
+	} else {
+		*memmap_batt_flags &= ~EC_BATT_FLAG_AC_PRESENT;
+		host_set_single_event(EC_HOST_EVENT_AC_DISCONNECTED);
+		mask = EC_HOST_EVENT_AC_CONNECTED_MASK;
+	}
+
+	/* Clear the mask depending upon the charging status. */
+	host_clear_events_b(mask);
+	host_clear_events(mask);
+}
 
 test_mockable int extpower_is_present(void)
 {
@@ -44,18 +71,10 @@ void extpower_interrupt(enum gpio_signal signal)
 static void extpower_init(void)
 {
 	debounced_extpower_presence = gpio_get_level(GPIO_AC_PRESENT);
-
 	if (IS_ENABLED(HAS_TASK_HOSTCMD)) {
-		uint8_t *memmap_batt_flags =
-			host_get_memmap(EC_MEMMAP_BATT_FLAG);
-
 		/* Initialize the memory-mapped AC_PRESENT flag */
-		if (debounced_extpower_presence)
-			*memmap_batt_flags |= EC_BATT_FLAG_AC_PRESENT;
-		else
-			*memmap_batt_flags &= ~EC_BATT_FLAG_AC_PRESENT;
+		extpower_update_host_events(debounced_extpower_presence);
 	}
-
 	/* Enable interrupts, now that we've initialized */
 	gpio_enable_interrupt(GPIO_AC_PRESENT);
 }
