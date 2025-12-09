4c5fbb2bee90010f465f70ca6bc25d12c2c038f3
wu.johnson <wu.johnson@inventec.corp-partner.google.com>
2025-12-09 14:01:14 +0800

    ocicat-ish: Add ISH target for ocicat
    Create the initial ocicat-ish image by copying
    the ocelotrvp-ish board files.
    
    BUG=b:465776760
    TEST=zmake build ocicat-ish
    
    Change-Id: I6424280d4e2d75a62aaffac77487eac3e9fddb08

--- zephyr/program/ocelot/BUILD.py
+++ zephyr/program/ocelot/BUILD.py
@@ -201,6 +201,18 @@ ocicat = register_it8xxx2_project(
     ],
 )
 
+register_ish_project(
+    project_name="ocicat-ish",
+    zephyr_board="intel_ish_5_8_0",
+    dts_overlays=[
+        here / "ocicat-ish" / "ocicat-ish" / "project.overlay",
+    ],
+    kconfig_files=[
+        here / "ocicat-ish" / "prj.conf",
+        here / "ocicat-ish" / "motionsense.conf",
+    ],
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="kodkod", addr=0x80404)
