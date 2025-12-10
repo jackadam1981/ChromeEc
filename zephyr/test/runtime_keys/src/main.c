90a249b9122867dab45aed347672ef6303ec0f3c
Fabio Baltieri <fabiobaltieri@google.com>
2025-12-10 12:14:42 +0000

    runtime_keys: (re)add a vol-up key spec
    Runtime keys have to work even for MKBP, which does not use a vivaldi
    driver, but currently relies on the vivaldi driver to find out where the
    volume-up key is.
    
    Add a separate code path to have an explicit vol-up key spec in that
    setup.
    
    BUG=none
    TEST=cq dry run
    TEST=hacked the config on a rex board and checked the key triggers
    
    Change-Id: I6f5f95dd622ff473d86ecd1b9115046ab5891fb2

--- zephyr/test/runtime_keys/src/main.c
+++ zephyr/test/runtime_keys/src/main.c
@@ -24,11 +24,6 @@ FAKE_VOID_FUNC(system_enter_hibernate, uint32_t, uint32_t);
 #define VOL_UP_ROW 0
 #define VOL_UP_COL 1
 
-bool vivaldi_kbd_is_vol_up(uint8_t row, uint8_t col)
-{
-	return row == VOL_UP_ROW && col == VOL_UP_COL;
-}
-
 #define CROS_EC_KEYBOARD_NODE DT_CHOSEN(cros_ec_keyboard)
 
 #define TEST_BOOT_KEYS_NODE DT_NODELABEL(test_runtime_keys)
