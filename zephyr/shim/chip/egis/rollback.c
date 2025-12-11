9b45383980b4b5a838b807a867b9dd93c43ba045
Firas Sammoura <fsammoura@google.com>
2025-12-11 22:34:24 +0000

    fpmcu: sanok: add mpu_lock_rollback functionality for ET171
    BUG=b:406946889
    TEST=check rollback pmp entry permissions using functions
         `mpu_lock_rollback(0)` and `mpu_lock_rollback(1)`
    
    Change-Id: I16c0cf963d49d58bdbeaf22da23223cfdf81ff85

---
+++ zephyr/shim/chip/egis/rollback.c
@@ -0,0 +1,15 @@
+// Copyright 2025 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <zephyr/arch/riscv/pmp.h>
+
+int mpu_lock_rollback(int lock)
+{
+	if (lock) {
+		z_riscv_pmp_change_permissions(0, PMP_R);
+	} else {
+		z_riscv_pmp_change_permissions(0, PMP_R | PMP_W);
+	}
+	return 0;
+}
