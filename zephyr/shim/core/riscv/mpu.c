fa7224a0da9bb9ab5276d6aec537509e6aa11586
Firas Sammoura <fsammoura@google.com>
2025-12-12 10:56:29 -0800

    rollback investigation
    Change-Id: I50113d8336fb452e66f43019abbdbe3940b26fe1

--- zephyr/shim/core/riscv/mpu.c
+++ zephyr/shim/core/riscv/mpu.c
@@ -26,7 +26,7 @@ int mpu_lock_rollback(int lock)
 	if (lock) {
 		z_riscv_pmp_change_permissions(0, 0);
 	} else {
-		z_riscv_pmp_change_permissions(0, PMP_R | PMP_W);
+		z_riscv_pmp_change_permissions(0, PMP_R);
 	}
 	return 0;
 }
