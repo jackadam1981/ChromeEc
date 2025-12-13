5f423ec69caf045bef745c7c926d6ead8bb143f2
mike5 <mike5@huaqin.corp-partner.google.com>
2025-12-13 16:04:57 +0800

    zephyr/ctn730: Add rf-charge-msec for stylus recovery
    Add 'rf-charge-msec' parameter to enable stylus recovery, and enable
    RF charge on reset to ensure low-battery stylus can send events.
    
    BUG=b:470882765
    TEST=test stylus can recovery from low battery.
    LOW_COVERAGE_REASON=b:470882765
    
    Change-Id: Ic5ec7a53ee39cfd35f53ebec38217b6a6d736bbd

--- common/peripheral_charger.c
+++ common/peripheral_charger.c
@@ -407,7 +407,12 @@ static void pchg_state_enabled(struct pchg *ctx)
 
 	switch (ctx->event) {
 	case PCHG_EVENT_RESET:
-		ctx->state = pchg_reset(ctx);
+		if (ctx->bist_cmd != PCHG_BIST_CMD_NONE) {
+			ctx->mode = PCHG_MODE_BIST;
+			ctx->state = pchg_reset(ctx);
+		} else {
+			ctx->state = pchg_reset(ctx);
+		}
 		break;
 	case PCHG_EVENT_DISABLE:
 		rv = ctx->cfg->drv->enable(ctx, false);
@@ -842,7 +847,7 @@ static void pchg_shutdown(void)
 	CPRINTS("%s", __func__);
 
 	for (p = 0; p < pchg_count; p++) {
-		ctx = &pchgs[0];
+		ctx = &pchgs[p];
 		gpio_disable_interrupt(ctx->cfg->irq_pin);
 		board_pchg_power_on(p, 0);
 	}
