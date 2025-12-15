feb87aa851b235feb0a98cca85be8656b18e9334
wu.johnson <wu.johnson@inventec.corp-partner.google.com>
2025-12-15 14:31:28 +0800

    ocicat: import the UFSC field
    import UFSC device tree for ocicat project
    
    BUG=b:457879751
    TEST=zmake build ocicat, ectool cbi set tag 29 and now we can catch ufsc field.
    LOW_COVERAGE_REASON=test code is not yet ready b:464397296
    
    Change-Id: I737b02b70b0499257a629578e012a986c3f93f4c

--- zephyr/program/ocelot/ocicat/src/board.c
+++ zephyr/program/ocelot/ocicat/src/board.c
@@ -18,6 +18,8 @@
 
 LOG_MODULE_DECLARE(board_init, LOG_LEVEL_INF);
 
+#define FW_KB_BL_PRESENT 1
+#define FW_KB_BL_NOT_PRESENT 0
 static bool has_backlight = FW_KB_BL_NOT_PRESENT;
 int8_t board_vivaldi_keybd_idx(void)
 {
@@ -33,22 +35,17 @@ int8_t board_vivaldi_keybd_idx(void)
  */
 test_export_static void kb_init(void)
 {
-	int ret;
-	uint32_t val;
-
-	ret = cros_cbi_get_fw_config(FW_KB_BL, &val);
-	if (ret != 0) {
-		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FW_KB_BL);
-		return;
-	}
-
-	if (val == FW_KB_BL_PRESENT) {
-		LOG_INF("CBI FW_CONFIG: FW_KB_BL_PRESENT.");
+	if (cros_cbi_ufsc_check_match(CBI_UFSC_VALUE_ID(
+		    DT_NODELABEL(ufsc_kb_backlight_present)))) {
+		LOG_INF("CBI USFC: FW_KB_BL_PRESENT.");
 		has_backlight = FW_KB_BL_PRESENT;
-	} else {
-		LOG_INF("CBI FW_CONFIG: FW_KB_BL_NOT_PRESENT.");
+	} else if (cros_cbi_ufsc_check_match(CBI_UFSC_VALUE_ID(
+			   DT_NODELABEL(ufsc_kb_backlight_absent)))) {
 		has_backlight = FW_KB_BL_NOT_PRESENT;
 		kblight_enable(0);
+	} else {
+		LOG_ERR("Error retrieving CBI USFC field");
+		return;
 	}
 }
 DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_I2C);
@@ -58,35 +55,30 @@ DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_I2C);
  */
 test_export_static void kb_layout_init(void)
 {
-	int ret, tmp;
+	int tmp;
 #ifdef CONFIG_KEYBOARD_DEBUG
 	int label;
 #endif
-	uint32_t val;
 
-	ret = cros_cbi_get_fw_config(FW_KB_LAYOUT, &val);
-	if (ret != 0) {
-		LOG_ERR("Error retrieving CBI FW_KB_LAYOUT field %d",
-			FW_KB_LAYOUT);
-		return;
-	}
 	/*
 	 * If keyboard is US2(FW_KB_LAYOUT_US2), we need translate right ctrl
 	 * to Europe2 key.
 	 */
-	if (val == FW_KB_LAYOUT_US2) {
+	if (cros_cbi_ufsc_check_match(
+		    CBI_UFSC_VALUE_ID(DT_NODELABEL(ufsc_fw_kb_layout_us2)))) {
 		set_scancode_set2(3, 14, get_scancode_set2(2, 7));
 #ifdef CONFIG_KEYBOARD_DEBUG
 		set_keycap_label(3, 14, get_keycap_label(2, 7));
 #endif
-		LOG_INF("CBI FW_CONFIG: FW_KB_LAYOUT_US2");
+		LOG_INF("CBI USFC: FW_KB_LAYOUT_US2");
 	}
 
 	/*
 	 * If keyboard is JP(FW_KB_LAYOUT_JP), we need translate right alt,
 	 * right fn and henkan key.
 	 */
-	if (val == FW_KB_LAYOUT_JP) {
+	else if (cros_cbi_ufsc_check_match(CBI_UFSC_VALUE_ID(
+			 DT_NODELABEL(ufsc_fw_kb_layout_jp)))) {
 		tmp = get_scancode_set2(0, 10);
 #ifdef CONFIG_KEYBOARD_DEBUG
 		label = get_keycap_label(0, 10);
@@ -107,7 +99,10 @@ test_export_static void kb_layout_init(void)
 #ifdef CONFIG_KEYBOARD_DEBUG
 		set_keycap_label(1, 12, label);
 #endif
-		LOG_INF("CBI FW_CONFIG: FW_KB_LAYOUT_JP");
+		LOG_INF("CBI USFC: FW_KB_LAYOUT_JP");
+	} else {
+		LOG_ERR("Error retrieving CBI USFC field");
+		return;
 	}
 }
 DECLARE_HOOK(HOOK_INIT, kb_layout_init, HOOK_PRIO_POST_I2C);
