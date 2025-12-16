351ef90dd45d659ab63d7ddc98bf8b63431bc079
Cheng Yueh <cyueh@chromium.org>
2025-12-16 16:37:36 +0800

    usbc: Add log to frs
    BUG=None
    TEST=None
    
    Change-Id: I08a79ff278c86ab34b7b951fde38d9bc20197b30

--- common/usbc/usb_pe_drp_sm.c
+++ common/usbc/usb_pe_drp_sm.c
@@ -991,6 +991,9 @@ static void pe_set_frs_enable(int port, int enable)
 	if (!IS_ENABLED(CONFIG_USB_PD_FRS) || !IS_ENABLED(CONFIG_USB_PD_REV30))
 		return;
 
+	CPRINTS_L1("C%d: pe_set_frs_enable: %sable %sable", port,
+		   current ? "en" : "dis", enable ? "en" : "dis");
+
 	/* Request an FRS change, only if the state has changed */
 	if (!!current == !!enable)
 		return;
@@ -5688,6 +5691,8 @@ __maybe_unused static void pe_prs_frs_shared_entry(int port)
 	if (!IS_ENABLED(CONFIG_USB_PD_REV30))
 		assert(0);
 
+	print_current_state(port);
+
 	/*
 	 * Shared PRS/FRS code, assume PRS path
 	 *
@@ -5704,6 +5709,7 @@ __maybe_unused static void pe_prs_frs_shared_exit(int port)
 	if (!IS_ENABLED(CONFIG_USB_PD_REV30))
 		assert(0);
 
+	print_current_state(port);
 	/*
 	 * Shared PRS/FRS code, when not in shared path
 	 * indicate PRS path
