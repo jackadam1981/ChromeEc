231aa79ff242d3cf3d1170c307dcea5ba6c37189
Jon Murphy <jpmurphy@google.com>
2025-12-16 10:01:58 -0700

    ec-legacy: util: update migrated_tests
    Update the util/migrated_tests.sh script tests which are applicable to
    both the legacy ec and zephyr.  Modify the logic for what is still
    applicable and add to presubmit.
    
    BUG=b:456553702
    TEST=$ ./util/migrated_tests.sh test/fpsensor_debug.cc
    
    Change-Id: I3b16f61126e30a8422d3b93b1908013a8cdab835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7266285
    Auto-Submit: Jon Murphy <jpmurphy@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Jon Murphy <jpmurphy@google.com>
    Commit-Queue: Jon Murphy <jpmurphy@google.com>

--- util/migrated_tests.sh
+++ util/migrated_tests.sh
@@ -4,9 +4,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-mapfile -d '' cmakes < <(find zephyr \( -path zephyr/test -prune \) -o \
-  \( -name CMakeLists.txt -o -name tcpmv2.cmake \) -print0)
-
 exit_code=0
 
 # Some tests were mainly copy paste to Zephyr. Add a warning to make sure
@@ -61,24 +58,11 @@ for file in "$@"; do
   if [[ ${migrated_tests} == *"${file}"* ]]; then
     echo -n "WARNING: ${ec_file} is not used in Zephyr EC. The test "
     echo -n "has been migrated to Zephyr. Make sure you apply the "
-    echo "same fix for the Zephyr version in zephyr/test directory."
+    echo -n "same fix for the Zephyr version in zephyr/test directory "
+    echo "of the ec repo at src/platform/ec"
     exit_code=1
     continue
   fi
-
-  case "${ec_file}" in
-    baseboard/*|board/*|chip/*|driver/fingerprint/*|*fpsensor*|test/*|\
-    util/*|zephyr/*|extra/*) ;;
-    **.c)
-      if ! grep -q -F "\${PLATFORM_EC}/${ec_file}" "${cmakes[@]}" ; then
-        echo -n "WARNING: ${ec_file} is not used in Zephyr EC. There is "
-        echo -n "probably another file in the zephyr/shim directory that "
-        echo -n "needs the same fix that you just made. Alternativly, you "
-        echo "might have edited the wrong file."
-        exit_code=1
-      fi
-      ;;
-  esac
 done
 
 exit "${exit_code}"
