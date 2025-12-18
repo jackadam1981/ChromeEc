d3002350b87664160e28cd08cdfcdc30345bd3ab
Firas Sammoura <fsammoura@google.com>
2025-12-18 23:58:06 +0000

    zephyr: test: Add IRQ stack guard validation test
    This change introduces a new test case to validate the functionality
    of for the Interrupt Request IRQ) stack guard.
    
    The test (`irq_stack`) attempts to write to the memory region
    designated as the IRQ stack guard. It verifies that this action
    triggers a CPU exception, as expected, confirming that the stack
    guard is correctly protected by the memory protection unit. This
    ensures that potential stack overflows into the guard region are caught.
    
    BUG=b:469067565
    TEST=./test/run_device_tests.py -b sanok -t pmp_irq_stack --zephyr;
         flash on ET171 board; ztest run-all
    
    Change-Id: Iecb5ae87fc96555c5e859931aafa41396eb22d89

---
+++ zephyr/test/hwtest/src/irq_stack.c
@@ -0,0 +1,44 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/ztest.h>
+#include <zephyr/ztest_error_hook.h>
+
+LOG_MODULE_REGISTER(irq_stack, LOG_LEVEL_INF);
+
+ZTEST_SUITE(irq_stack, NULL, NULL, NULL, NULL, NULL);
+
+extern uint8_t z_interrupt_stacks[];
+
+void ztest_post_fatal_error_hook(unsigned int reason,
+				 const struct arch_esf *pEsf)
+{
+	zassert_equal(reason, K_ERR_CPU_EXCEPTION);
+	ztest_set_fault_valid(false);
+}
+
+void trigger_irq_stack_fault(void)
+{
+	volatile uint8_t *address = (uint8_t *)z_interrupt_stacks;
+
+	LOG_INF("Attempting to write to IRQ stack guard at address: %p\n",
+		address);
+
+	ztest_set_fault_valid(true);
+	*address = 0x00;
+
+	/* Should never reach this. */
+	zassert_unreachable();
+}
+
+ZTEST(irq_stack, test_trigger_irq_stack_fault)
+{
+	trigger_irq_stack_fault();
+
+	/* Should never reach this. */
+	zassert_unreachable();
+}
