b21005b6be0a20b5b0f4881e0431f569fdba5dae
Firas Sammoura <fsammoura@google.com>
2025-12-18 23:58:06 +0000

    test: Add stack protection tests
    Introduce the `stack_protection` test suite to validate key stack
    protection mechanisms within Zephyr.
    
    This new suite includes:
    
    *   A test to confirm the IRQ stack guard is effective. It attempts
        to write to the protected memory region and asserts that this
        action triggers a CPU exception, preventing potential corruption.
    
    *   A test to verify main stack overflow detection. This is done by
        using a recursive function to exhaust the stack space, ensuring
        the system's stack overflow protection (e.g., PMP or MSPLIM)
        catches the overflow and triggers a stack check failure.
    
    BUG=b:469067565
    TEST=./test/run_device_tests.py -b sanok -t stack_protection --zephyr;
         flash on ET171 board; ztest run-all
    
    Change-Id: Iecb5ae87fc96555c5e859931aafa41396eb22d89

--- test/run_device_tests.py
+++ test/run_device_tests.py
@@ -775,6 +775,7 @@ class AllTests:
             TestConfig(test_name="sbrk", imagetype_to_use=ImageType.RO),
             TestConfig(test_name="sha256"),
             TestConfig(test_name="sha256_unrolled"),
+            TestConfig(test_name="stack_protection"),
             TestConfig(test_name="static_if"),
             TestConfig(test_name="stdlib"),
             TestConfig(test_name="std_vector"),
