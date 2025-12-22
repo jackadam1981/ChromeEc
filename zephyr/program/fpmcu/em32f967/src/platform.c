fe7429fd4ed20ec9eba48beb4cb3c08eb577e67d
Dawid Niedzwiecki <dawidn@google.com>
2025-12-22 14:05:26 +0100

    zephyr: fpmcu: add niedzica project
    Add a new FPMCU, based on EM32F967 chip, with ELANI80SA sensor,
    connected via USB.
    
    BUG=b:429965218
    TEST=CQ
    
    Change-Id: I79ccbada4399037703696b796de2def3e06c28a4

---
+++ zephyr/program/fpmcu/em32f967/src/platform.c
@@ -0,0 +1,11 @@
+/* Copyright 2026 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "write_protect.h"
+
+int write_protect_is_asserted_custom(void)
+{
+	return 0;
+}
