69cc6003bb72b32be282fe89fa817ae7244b2660
Rob Barnes <robbarnes@google.com>
2025-12-23 23:24:14 +0000

    watchdog: Add host command to query watchdog info
    Add a new host command `EC_CMD_HOSTCMD_WATCHDOG_INFO` to query
    watchdog statistics, including maximum reload period.
    This is useful for debugging watchdog resets and monitoring system health.
    
    Also add a console command `watchdoginfo` to print the same
    information and `watchdoginfo reset_stats` to reset the stats.
    
    BUG=b:471279862
    TEST=Build and deploy on navi
         ectool watchdoginfo
         ectool watchdoginfo reset_stats
         console command: watchdoginfo
         console command: watchdoginfo reset_stats
    
    Change-Id: I7cb0cd3df00c069d1c18c02ad9b94e7b2e32d955

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -5987,6 +5987,22 @@ struct ec_params_enter_bootloader {
 	uint8_t mode;
 } __ec_align1;
 
+#define EC_CMD_HOSTCMD_WATCHDOG_INFO 0x00E3
+
+struct ec_params_hostcmd_watchdog_info {
+	bool reset_stats;
+} __ec_align1;
+
+struct ec_response_hostcmd_watchdog_info {
+	uint32_t watchdog_period_ms;
+	uint32_t watchdog_warning_period_ms;
+	uint32_t watchdog_reload_period_nominal_ms;
+	uint32_t watchdog_reload_period_max_ms;
+	uint64_t watchdog_reload_period_max_ts_ms;
+	uint32_t watchdog_reload_count;
+	uint64_t watchdog_stats_elapsed_ms;
+} __ec_align4;
+
 /*****************************************************************************/
 /*
  * PD commands
