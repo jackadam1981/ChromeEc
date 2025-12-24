b432cc57a08a3963473a74635c9ddcf2ad9bd177
mike5 <mike5@huaqin.corp-partner.google.com>
2025-12-24 11:34:54 +0800

    pchg: Add wpc ac s5 charge feature
    Add AC S5 charging functionality to better meet user
    needs and enhance user experience.
    
    BUG=b:470007924
    TEST=1.zmake build sapphire
         2.pchg function normal.
    LOW_COVERAGE_REASON=Tests will be added in b:470007924
    
    Change-Id: I4b0d73466b89dd19ade3e2af51f48a2158387075

--- common/peripheral_charger.c
+++ common/peripheral_charger.c
@@ -6,6 +6,7 @@
 #include "atomic.h"
 #include "chipset.h"
 #include "common.h"
+#include "extpower.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "host_command.h"
@@ -880,6 +881,32 @@ static void wpc_hall_disable(void)
 
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, wpc_hall_enable, HOOK_PRIO_POST_DEFAULT);
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, wpc_hall_disable, HOOK_PRIO_DEFAULT);
+#endif
+
+#ifdef CONFIG_WPC_AC_S5_CHARGE
+static void wpc_ac_s5_handler(void)
+{
+	if (!chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
+		return;
+	}
+
+	bool ac_present = extpower_is_present();
+
+#ifdef CONFIG_WPC_HALL_ENABLE
+	if (ac_present) {
+		wpc_hall_enable();
+	} else {
+		wpc_hall_disable();
+	}
+#else
+	if (ac_present) {
+		pchg_startup();
+	} else {
+		pchg_shutdown();
+	}
+#endif
+}
+DECLARE_HOOK(HOOK_AC_CHANGE, wpc_ac_s5_handler, HOOK_PRIO_POST_DEFAULT);
 #endif
 
 void pchg_task(void *u)
