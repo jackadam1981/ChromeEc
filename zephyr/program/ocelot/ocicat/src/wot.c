c7f22500bd6c872fc99cec7b41625f4fbb3a19a5
wu.johnson <wu.johnson@inventec.corp-partner.google.com>
2025-12-24 11:55:51 +0800

    ocicat: support Wake on touch function
    support wake on touch function for ocicat
    
    BUG=b:444942125
    TEST=zmake build ocicat
    LOW_COVERAGE_REASON=test code is not yet ready b:464397296
    
    Change-Id: Ie98b65d966bb59bb2d8d3b2d2e5fabcf065a50d4

---
+++ zephyr/program/ocelot/ocicat/src/wot.c
@@ -0,0 +1,182 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "charge_manager.h"
+#include "chipset.h"
+#include "cros_cbi.h"
+#include "gpio.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "keyboard_8042_sharedlib.h"
+#include "keyboard_backlight.h"
+#include "keyboard_config.h"
+#include "power_button.h"
+#include "util.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(board_init, LOG_LEVEL_INF);
+
+#define TS_WOT_DELAY_MS (100 * USEC_PER_MSEC)
+
+static bool chipstate_is_suspend = false;
+
+static int tablet_mode_override = -1;
+/*
+ * -1 : AUTO (use real tablet_mode)
+ *  0 : FORCE clamshell
+ *  1 : FORCE tablet
+ */
+static int last_tablet_mode = -1;
+
+static void ts_sleep_to_wot_deferred(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tchscr_report_en), 1);
+
+	LOG_INF("WOT: WOT waiting 100ms set HIGH");
+}
+DECLARE_DEFERRED(ts_sleep_to_wot_deferred);
+
+static void ts_wot_mode(void)
+{
+	LOG_INF("WOT: wot mode");
+
+	/* Step 1: WOT LOW */
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ts_wot_l), 1);
+
+	/* Step 2 after 100ms */
+	hook_call_deferred(&ts_sleep_to_wot_deferred_data, TS_WOT_DELAY_MS);
+}
+
+static void ts_wot_to_sleep_deferred(void)
+{
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ts_wot_l), 0);
+
+	LOG_INF("WOT: WOT waiting 100ms set LOW");
+}
+DECLARE_DEFERRED(ts_wot_to_sleep_deferred);
+
+static void ts_wot_sleep_mode(void)
+{
+	LOG_INF("WOT: Sleep Mode");
+
+	/* Step 1: REPORT_EN LOW */
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tchscr_report_en), 0);
+
+	/* Step 2 after 100ms */
+	hook_call_deferred(&ts_wot_to_sleep_deferred_data, TS_WOT_DELAY_MS);
+}
+
+static void ts_wot_normal_mode(void)
+{
+	LOG_INF("WOT: Normal Mode");
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tchscr_report_en), 1);
+	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ts_wot_l), 0);
+	chipstate_is_suspend = false;
+	last_tablet_mode = -1;
+}
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, ts_wot_normal_mode, HOOK_PRIO_DEFAULT);
+
+static inline bool ts_get_tablet_mode(void)
+{
+	/* Only controlled by EC console command */
+	return (tablet_mode_override == 1);
+}
+
+static void ts_wot_apply_state(bool tablet)
+{
+	if (tablet) {
+		LOG_INF("WOT apply: tablet -> WOT mode");
+		ts_wot_mode();
+	} else {
+		LOG_INF("WOT apply: clamshell -> Sleep mode");
+		ts_wot_sleep_mode();
+	}
+}
+
+static void suspend_mode(void)
+{
+	bool cur_tablet = ts_get_tablet_mode();
+
+	chipstate_is_suspend = true;
+
+	LOG_INF("WOT: enter suspend, tablet=%d", cur_tablet);
+
+	/* Apply correct mode once on suspend entry */
+	ts_wot_apply_state(cur_tablet);
+
+	/* Initialize change tracking */
+	last_tablet_mode = cur_tablet;
+}
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, suspend_mode, HOOK_PRIO_DEFAULT);
+
+static void ts_wot_handle_change(bool cur_tablet)
+{
+	/* First time */
+	if (last_tablet_mode == -1) {
+		last_tablet_mode = cur_tablet;
+		return;
+	}
+
+	if (cur_tablet == last_tablet_mode)
+		return;
+
+	LOG_INF("WOT change: tablet %d -> %d", last_tablet_mode, cur_tablet);
+
+	ts_wot_apply_state(cur_tablet);
+
+	last_tablet_mode = cur_tablet;
+}
+
+static void ts_wot_sleep_controller(void)
+{
+	/* Only operate during suspend */
+	if (!chipstate_is_suspend)
+		return;
+
+	ts_wot_handle_change(ts_get_tablet_mode());
+}
+DECLARE_HOOK(HOOK_TICK, ts_wot_sleep_controller, HOOK_PRIO_DEFAULT);
+
+static int command_tablet(int argc, const char **argv)
+{
+	if (argc != 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	if (!strcasecmp(argv[1], "on")) {
+		tablet_mode_override = 1;
+		ccprintf("Tablet mode forced ON\n");
+	} else if (!strcasecmp(argv[1], "off")) {
+		tablet_mode_override = 0;
+		ccprintf("Tablet mode forced OFF\n");
+	} else if (!strcasecmp(argv[1], "auto")) {
+		tablet_mode_override = -1;
+		ccprintf("Tablet mode AUTO\n");
+	} else {
+		return EC_ERROR_PARAM1;
+	}
+
+	return EC_SUCCESS;
+}
+DECLARE_CONSOLE_COMMAND(wot, command_tablet, "on | off | auto",
+			"Force tablet mode for WOT debug");
+
+static void tchscr_int_l_handler(void)
+{
+	/* Only care when system is suspended */
+	if (!chipstate_is_suspend)
+		return;
+
+	if (!(gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_tchscr_int_l))))
+		return;
+
+	LOG_INF("WOT: Touch INT asserted, send power button wake");
+
+	/* Send power button wake event */
+	power_button_pch_press();
+	power_button_pch_release();
+}
+DECLARE_HOOK(HOOK_TICK, tchscr_int_l_handler, HOOK_PRIO_DEFAULT);
