3bd74bd9f563d8d0dbd57fe7c9afe055c0460ddd
Fabio Baltieri <fabiobaltieri@google.com>
2025-12-31 16:04:49 +0000

    keyboard_customization: set KEYBOARD_COLS_MAX from dt on input
    For devices using the input based keyboard driver, this data can be
    read from devicetree. Do that and drop few keyboard_customization.h
    files that are now empty.
    
    BUG=none
    TEST=zmake compare-builds
    TEST=cq dry run
    
    Change-Id: I8ae8b02180af50b80fbaab5569a134b988343ddb

--- include/keyboard_config.h
+++ include/keyboard_config.h
@@ -16,8 +16,14 @@
 #define KEYBOARD_ROWS 8
 
 #ifdef CONFIG_KEYBOARD_CUSTOMIZATION
+
+#ifdef CONFIG_CROS_EC_KEYBOARD_INPUT
+#define KEYBOARD_COLS_MAX DT_PROP(DT_CHOSEN(cros_ec_keyboard), col_size)
+#else
 /* include the board layer keyboard header file */
 #include "keyboard_customization.h"
+#endif
+
 #define KEYBOARD_COLS KEYBOARD_COLS_MAX
 #else /* CONFIG_KEYBOARD_CUSTOMIZATION */
 
