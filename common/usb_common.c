f74e17ba7b67d6421cb9ad538c0cd547fbb762af
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2026-01-04 10:54:18 +0800

    pdc: Add custom settings for the number of 3A port for PDC ports
    Based on the requirements, the USB 3A port configuration will be
    refactored into a project-customizable function, enabling dynamic
    switching of 3A ports.
    
    BUG=b:467180448
    TEST=zmake build obiwan
    
    Change-Id: I596536a030c8daf17897d2b3a7c5307a52545225

--- common/usb_common.c
+++ common/usb_common.c
@@ -59,6 +59,13 @@ int usb_get_battery_soc(void)
 #endif
 }
 
+#ifndef CONFIG_PLATFORM_EC_USB_PD_3A_PORTS_CUSTOM
+__overridable int pdc_power_mgmt_get_usb_pd_3a_ports(void)
+{
+	return CONFIG_PLATFORM_EC_USB_PD_3A_PORTS;
+}
+#endif
+
 /*
  * CC values for regular sources and Debug sources (aka DTS)
  *
