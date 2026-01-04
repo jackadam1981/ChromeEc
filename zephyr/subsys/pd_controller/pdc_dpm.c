fcaed452b59d27efe6c117d337e3326058b0fcd2
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2026-01-04 10:54:18 +0800

    pdc: Add custom settings for the number of 3A port for PDC ports
    test
    
    BUG=None
    TEST=zmake build obiwan
    
    Change-Id: I596536a030c8daf17897d2b3a7c5307a52545225

--- zephyr/subsys/pd_controller/pdc_dpm.c
+++ zephyr/subsys/pd_controller/pdc_dpm.c
@@ -57,6 +57,14 @@ static int count_port_bits(uint32_t bitmask)
 	return total;
 }
 
+__overridable int get_board_3a_ports(void)
+{
+#ifdef CONFIG_PLATFORM_EC_USB_PD_3A_PORTS_CUSTOM
+	BUILD_ASSERT(0, "Board must implement get_board_3a_ports()");
+#endif
+	return CONFIG_PLATFORM_EC_USB_PD_3A_PORTS;
+}
+
 /**
  * @brief Adjust source current allocations for usbc ports
  *
@@ -105,7 +113,7 @@ static void pdc_dpm_balance_source_ports(struct k_work *work)
 		int new_max_port = LOWEST_PORT(new_ports);
 
 		if (count_port_bits(max_current_claimed) <
-		    CONFIG_PLATFORM_EC_USB_PD_3A_PORTS) {
+		    get_board_3a_ports()) {
 			max_current_claimed |= BIT(new_max_port);
 			pdc_power_mgmt_set_current_limit(new_max_port,
 							 TC_CURRENT_3_0A);
@@ -148,7 +156,7 @@ static void pdc_dpm_balance_source_ports(struct k_work *work)
 		int new_frs_port = LOWEST_PORT(new_ports);
 
 		if (count_port_bits(max_current_claimed) <
-		    CONFIG_PLATFORM_EC_USB_PD_3A_PORTS) {
+		    get_board_3a_ports()) {
 			max_current_claimed |= BIT(new_frs_port);
 			/* Enable FRS for this port */
 			pdc_power_mgmt_frs_enable(new_frs_port, true);
@@ -179,7 +187,7 @@ static void pdc_dpm_balance_source_ports(struct k_work *work)
 		int new_max_port = LOWEST_PORT(new_ports);
 
 		if (count_port_bits(max_current_claimed) <
-		    CONFIG_PLATFORM_EC_USB_PD_3A_PORTS) {
+		    get_board_3a_ports()) {
 			max_current_claimed |= BIT(new_max_port);
 			pdc_power_mgmt_set_current_limit(new_max_port,
 							 TC_CURRENT_3_0A);
@@ -204,7 +212,7 @@ void pdc_dpm_eval_sink_fixed_pdo(int port, uint32_t vsafe5v_pdo)
 		return;
 
 	if (pdc_power_mgmt_get_power_role(port) == PD_ROLE_SOURCE) {
-		if (CONFIG_PLATFORM_EC_USB_PD_3A_PORTS == 0)
+		if (get_board_3a_ports() == 0)
 			return;
 
 		/* Valid PDO to process, so evaluate whether >1.5A is needed */
@@ -241,7 +249,7 @@ void pdc_dpm_eval_sink_fixed_pdo(int port, uint32_t vsafe5v_pdo)
 				goto balance;
 			}
 
-			if (CONFIG_PLATFORM_EC_USB_PD_3A_PORTS == 0)
+			if (get_board_3a_ports() == 0)
 				return;
 
 			atomic_set_bit(&source_frs_max_requested, port);
@@ -262,7 +270,7 @@ void pdc_dpm_add_pd_source(int port)
 	 * can confirm 3A is not required. It can take longer to enable FRS on
 	 * platforms which don't source 3A.
 	 */
-	if (CONFIG_PLATFORM_EC_USB_PD_3A_PORTS == 0)
+	if (get_board_3a_ports() == 0)
 		return;
 
 	atomic_set_bit(&source_frs_max_requested, port);
@@ -271,7 +279,7 @@ void pdc_dpm_add_pd_source(int port)
 
 void pdc_dpm_add_non_pd_sink(int port)
 {
-	if (CONFIG_PLATFORM_EC_USB_PD_3A_PORTS == 0)
+	if (get_board_3a_ports() == 0)
 		return;
 
 	atomic_set_bit(&non_pd_sink_max_requested, port);
@@ -282,7 +290,7 @@ void pdc_dpm_remove_sink(int port)
 {
 	enum usb_typec_current_t rp;
 
-	if (CONFIG_PLATFORM_EC_USB_PD_3A_PORTS == 0)
+	if (get_board_3a_ports() == 0)
 		return;
 
 	if (!atomic_test_bit(&sink_max_pdo_requested, port) &&
@@ -302,7 +310,7 @@ void pdc_dpm_remove_source(int port)
 {
 	enum usb_typec_current_t rp;
 
-	if (CONFIG_PLATFORM_EC_USB_PD_3A_PORTS == 0)
+	if (get_board_3a_ports() == 0)
 		return;
 
 	if (!IS_ENABLED(CONFIG_PLATFORM_EC_USB_PD_FRS))
