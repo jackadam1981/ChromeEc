62d602006950eb2e6566b111a6806f949874c97e
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2026-01-06 18:12:30 +0800

    Dooku: set charger AUTO_WAKE_EN=1 to leave ship mode
    It was found that the battery can't leave ship mode after
    the battery is cutoff by refresh+power+unplug AC. The BATFET
    wasn't turned on after replug in AC. Therefore, set 0x30 bit0,
    AUTO_WAKE_EN, to 1 to turn on BATFET to let the battery leave
    ship mode.
    
    BUG=b:473885298
    TEST=1. refresh+power+unplug AC
         2. wait 30s, plug in AC
         3. make sure the battery can leave ship mode
    
    Change-Id: I27de328218b0a6ca113305f8f5335df3c23aa3f1

--- zephyr/program/skywalker/dooku/src/charger.c
+++ zephyr/program/skywalker/dooku/src/charger.c
@@ -11,9 +11,17 @@
 #include "driver/charger/bq257x0_regs.h"
 #include "extpower.h"
 #include "hooks.h"
+#include "i2c.h"
 #include "usb_pd.h"
 
 #include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(dooku_charger, LOG_LEVEL_INF);
+
+#define BQ257X0_CHARGE_OPTION_1_EN_AUTO_WAKEUP_SHIFT 0
+#define BQ257X0_CHARGE_OPTION_1_EN_AUTO_WAKEUP_BITS 1
+#define BQ257X0_CHARGE_OPTION_1_EN_AUTO_WAKEUP__DISABLE 0
+#define BQ257X0_CHARGE_OPTION_1_EN_AUTO_WAKEUP__ENABLE 1
 
 __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 				       int max_ma, int charge_mv)
@@ -29,3 +37,20 @@ __override void board_set_charge_limit(int port, int supplier, int charge_ma,
 		charger_set_input_current_limit(0, charge_ma);
 	}
 }
+
+static void board_charger_init(void)
+{
+	/*
+	 * Enable AUTO_WAKEUP_EN when battery is still in ship mode.
+	 */
+	if (battery_get_disconnect_state() != BATTERY_DISCONNECTED)
+		return;
+
+	LOG_INF("BQ25720: set AUTO_WAKEUP_EN=1");
+	i2c_update16(chg_chips[CHARGER_SOLO].i2c_port,
+		     chg_chips[CHARGER_SOLO].i2c_addr_flags,
+		     BQ25710_REG_CHARGE_OPTION_1,
+		     1 << BQ257X0_CHARGE_OPTION_1_EN_AUTO_WAKEUP_SHIFT,
+		     MASK_SET);
+}
+DECLARE_HOOK(HOOK_INIT, board_charger_init, HOOK_PRIO_PRE_DEFAULT);
