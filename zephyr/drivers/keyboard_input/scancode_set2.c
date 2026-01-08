70b42a21ae8a47e7f481d92b849b6af779161d8a
Fabio Baltieri <fabiobaltieri@google.com>
2026-01-08 16:01:27 +0000

    drivers: scancode_set2: add extra codeset support
    Add support for defining extra setcodes as child nodes of the set2 node
    and provide a set_scancode_set2_table function for a board file to
    activate an extra code table.
    
    Should save some ram compared to the current implementation as we only
    need to keep one code table in RAM.
    
    BUG=none
    TEST=cq dry run
    
    Change-Id: I47d11a559331d15e7425088ea23d741bee344606

--- zephyr/drivers/keyboard_input/scancode_set2.c
+++ zephyr/drivers/keyboard_input/scancode_set2.c
@@ -13,28 +13,31 @@
 
 /* clang-format off */
 
-#define FOREACH_COL(fn) \
-	fn(0) fn(1) fn(2) fn(3) fn(4) fn(5) fn(6) fn(7) fn(8) fn(9) fn(10) \
-	fn(11) fn(12) fn(13) fn(14) fn(15) fn(16) fn(17)
+#define FOREACH_COL(node_id, fn) \
+	fn(node_id, 0) fn(node_id, 1) fn(node_id, 2) fn(node_id, 3) \
+	fn(node_id, 4) fn(node_id, 5) fn(node_id, 6) fn(node_id, 7) \
+	fn(node_id, 8) fn(node_id, 9) fn(node_id, 10) fn(node_id, 11) \
+	fn(node_id, 12) fn(node_id, 13) fn(node_id, 14) fn(node_id, 15) \
+	fn(node_id, 16) fn(node_id, 17)
 
-#define DT_COL_CHECK(n) \
-	COND_CODE_1(DT_INST_NODE_HAS_PROP(0, col##n##_codes), ( \
+#define DT_COL_CHECK(node_id, n) \
+	COND_CODE_1(DT_NODE_HAS_PROP(node_id, col##n##_codes), ( \
 	BUILD_ASSERT(n < KEYBOARD_COLS_MAX, "extra col codes: " STRINGIFY(n)); \
-	BUILD_ASSERT(DT_INST_PROP_LEN(0, col##n##_codes) == 8, \
+	BUILD_ASSERT(DT_PROP_LEN(node_id, col##n##_codes) == 8, \
 		     STRINGIFY(col##n##_codes) " must have 8 entries"); \
 	),( \
 	BUILD_ASSERT(n >= KEYBOARD_COLS_MAX, "missing col codes: " STRINGIFY(n)); \
 	))
 
-#define DT_COL(n) \
-	IF_ENABLED(DT_INST_NODE_HAS_PROP(0, col##n##_codes), ( \
-	DT_INST_PROP(0, col##n##_codes), \
+#define DT_COL(node_id, n) \
+	IF_ENABLED(DT_NODE_HAS_PROP(node_id, col##n##_codes), ( \
+	DT_PROP(node_id, col##n##_codes), \
 	))
 
-FOREACH_COL(DT_COL_CHECK)
+FOREACH_COL(DT_INST(0, DT_DRV_COMPAT), DT_COL_CHECK)
 
 static uint16_t scancode_set2[KEYBOARD_COLS_MAX][KEYBOARD_ROWS] = {
-	FOREACH_COL(DT_COL)
+	FOREACH_COL(DT_INST(0, DT_DRV_COMPAT), DT_COL)
 };
 
 /* clang-format on */
@@ -54,3 +57,28 @@ test_mockable void set_scancode_set2(uint8_t row, uint8_t col, uint16_t val)
 		scancode_set2[col][row] = val;
 	}
 }
+
+#define SET2_CONFIG_COUNT DT_INST_CHILD_NUM(0)
+
+#if SET2_CONFIG_COUNT > 0
+
+#define SET2_CHILD_COL_CHECK(node_id) FOREACH_COL(node_id, DT_COL_CHECK)
+
+DT_INST_FOREACH_CHILD(0, SET2_CHILD_COL_CHECK)
+
+#define SET2_CHILD_INIT(node_id) { FOREACH_COL(node_id, DT_COL) },
+
+static const uint16_t extra_scancodes[][KEYBOARD_COLS_MAX][KEYBOARD_ROWS] = {
+	DT_INST_FOREACH_CHILD(0, SET2_CHILD_INIT)
+};
+
+void set_scancode_set2_table(uint8_t idx)
+{
+	if (idx >= SET2_CONFIG_COUNT) {
+		return;
+	}
+
+	memcpy(scancode_set2, extra_scancodes[idx], sizeof(scancode_set2));
+}
+
+#endif /* DT_INST_CHILD_NUM(0) > 0 */
