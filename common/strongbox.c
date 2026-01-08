3c7d7d22a9f842c94dfd9b06c001d6b4b8aa7967
Andrey Pronin <apronin@google.com>
2026-01-08 10:45:17 -0800

    strongbox: set issuer for ATTEST_NO_KEY(SIGN|ATTEST) case
    This CL sets the certificate_issuer for the case when no
    attestation key was provided to generate_key, but the generated
    key has SIGN and ATTEST purposes, so a self-signed certificate is
    generated.
    
    BUG=none
    TEST=get gECC
    
    Change-Id: Iddb415daa77adb039f7ce89bf65209a35a313dff

--- common/strongbox.c
+++ common/strongbox.c
@@ -236,6 +236,24 @@ static size_t SB_cert_name(const p256_int *d, const p256_int *pk_x,
 
 static const uint8_t g_keymint_var_name[] = { 'K', 'M' };
 
+/* Set the default subject:
+ * SEQUENCE
+ * 0x30 <size = 0x1b>
+ *      OBJECT            :commonName
+ *  0x06 <size = 0x03> 0x55 0x04 0x03
+ *      PRINTABLESTRING   :Android Keystore Key
+ *  0x13 <size = 0x14> ...
+ */
+static const char g_default_subject_data[] = {
+	0x30, 0x1b, 0x06, 0x03, 0x55, 0x04, 0x03, 0x13,
+	0x14, 0x41, 0x6e, 0x64, 0x72, 0x6f, 0x69, 0x64,
+	0x20, 0x4b, 0x65, 0x79, 0x73, 0x74, 0x6f, 0x72,
+	0x65, 0x20, 0x4b, 0x65, 0x79
+};
+static const struct slice g_default_subject =
+	SLICE(g_default_subject_data, sizeof(g_default_subject_data));
+
+
 static enum strongbox_error keymint_init(void)
 {
 	const struct tuple *var;
@@ -1280,7 +1298,7 @@ static enum strongbox_error generate_key_blob(
 			}
 			/* The remainder is the issuer_subject */
 			attest_key_space -= attest_key_size + 1;
-			if (attest_key_space < 2) {
+			if (attest_key_space < 1) {
 				err = SBERR_MissingIssuerSubject;
 				goto cleanup;
 			}
@@ -1291,9 +1309,10 @@ static enum strongbox_error generate_key_blob(
 				err = SBERR_InvalidIssuerSubject;
 				goto cleanup;
 			}
-			k.params.certificate_issuer =
-				SLICE(tags + issuer_subject_start + 1,
-				      issuer_subject_size);
+			if (issuer_subject_size > 0)
+				k.params.certificate_issuer =
+					SLICE(tags + issuer_subject_start + 1,
+					      issuer_subject_size);
 		}
 	}
 
@@ -1323,6 +1342,11 @@ static enum strongbox_error generate_key_blob(
 		memcpy(k.attest_key.a, k.key, sizeof(k.attest_key));
 		k.sign_params = k.params;
 		attest = ATTEST_TRUE;
+		if (!k.params.certificate_subject.p)
+			k.params.certificate_subject = g_default_subject;
+		if (!k.params.certificate_issuer.p)
+			k.params.certificate_issuer =
+				k.params.certificate_subject;
 	}
 	total_words = blob_start_words + blob_size_words;
 	/* Total size of key blob in 32-bit words */
@@ -1336,24 +1360,8 @@ static enum strongbox_error generate_key_blob(
 		 * specified during generateKey and importKey. If not provided
 		 * the subject name shall default to CN="Android Keystore Key".
 		 */
-		if (!k.params.certificate_subject.p) {
-			/* Set the default subject:
-			 * SEQUENCE
-			 * 0x30 <size = 0x1b>
-			 *      OBJECT            :commonName
-			 *  0x06 <size = 0x03> 0x55 0x04 0x03
-			 *      PRINTABLESTRING   :Android Keystore Key
-			 *  0x13 <size = 0x14> ...
-			 */
-			static const char default_subject[] = {
-				0x30, 0x1b, 0x06, 0x03, 0x55, 0x04, 0x03, 0x13,
-				0x14, 0x41, 0x6e, 0x64, 0x72, 0x6f, 0x69, 0x64,
-				0x20, 0x4b, 0x65, 0x79, 0x73, 0x74, 0x6f, 0x72,
-				0x65, 0x20, 0x4b, 0x65, 0x79
-			};
-			k.params.certificate_subject =
-				SLICE(default_subject, sizeof(default_subject));
-		}
+		if (!k.params.certificate_subject.p)
+			k.params.certificate_subject = g_default_subject;
 		if (!k.params.certificate_issuer.p) {
 			err = SBERR_MissingIssuerSubject;
 			goto cleanup;
