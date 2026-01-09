dec50a8d1d558e66e063f03e1e435231046cb58f
Firas Sammoura <fsammoura@google.com>
2026-01-09 10:47:56 -0800

    zephyr: riscv: Identify rollback region index using mem_attr
    Add a static helper function to resolve the memory attribute region
    index for the 'rollback' DT node. This allows the PMP code to
    dynamically locate the correct region for locking / unlocking.
    
    BUG=b:406946889
    TEST=./test/run_device_tests.py -b sanok rollback_region0 --zephyr;
         flash on ET171 board; ztest run-testsuite rollback_region0;
         Repeat for rollback_region1
    
    Change-Id: Ibaa14e81e3cb00d7f442bf9ea52a5651f8145ab9

--- zephyr/shim/core/riscv/mpu.c
+++ zephyr/shim/core/riscv/mpu.c
@@ -5,7 +5,14 @@
 #include "hooks.h"
 #include "system.h"
 
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+
 #include <zephyr/arch/riscv/pmp.h>
+#include <zephyr/mem_mgmt/mem_attr.h>
+
+#define ROLLBACK_NODE DT_NODELABEL(rollback)
 
 static void prepare_for_sysjump_to_ec(void)
 {
@@ -20,9 +27,39 @@ static void prepare_for_sysjump_to_ec(void)
 }
 DECLARE_HOOK(HOOK_SYSJUMP, prepare_for_sysjump_to_ec, HOOK_PRIO_LAST);
 
-#if defined(CONFIG_PLATFORM_EC_ROLLBACK_MPU_PROTECT)
+#if defined(CONFIG_PLATFORM_EC_ROLLBACK_MPU_PROTECT) && defined(CONFIG_MEM_ATTR)
+/**
+ * @brief Find the index of a memory region by its DeviceTree node name.
+ *
+ * @param target_name The DT_NODE_FULL_NAME to search for.
+ *
+ * @return The index in the regions array, or -1 if not found.
+ */
+static int get_mem_region_index_by_name(const char *target_name)
+{
+	const struct mem_attr_region_t *regions;
+	size_t num_regions;
+
+	num_regions = mem_attr_get_regions(&regions);
+
+	for (int i = 0; i < num_regions; ++i) {
+		if (regions[i].dt_name != NULL &&
+		    strcmp(regions[i].dt_name, target_name) == 0) {
+			return i;
+		}
+	}
+
+	return -ENOENT;
+}
+
 int mpu_lock_rollback(int lock)
 {
+	const char *rollback_node_name = DT_NODE_FULL_NAME(ROLLBACK_NODE);
+	int rollback_region_idx =
+		get_mem_region_index_by_name(rollback_node_name);
+
+	assert(rollback_region_idx != -ENOENT);
+
 	const int region_idx = 0;
 	if (lock) {
 		z_riscv_pmp_change_permissions(region_idx, 0);
