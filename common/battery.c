1b827933565536cb04c31d389064558712d6d5f8
Caveh Jalali <caveh@chromium.org>
2026-01-08 16:37:16 -0800

    battery/smart: Add manuf info read API
    Add API for fetching the battery manufacturer info (smart battery reg
    0x70).
    
    BUG=b:448252576
    TEST=checked fatcat-npcx with battery (with rest of patch stack)
    	./twister -v -T zephyr/test/drivers -s drivers.smart_battery
    
    Change-Id: I9d53ee82eb4db8cd8856ce4cf003c337f7e21e1d

--- common/battery.c
+++ common/battery.c
@@ -730,6 +730,11 @@ __attribute__((weak)) int get_battery_manufacturer_name(char *dest, int size)
 	return EC_SUCCESS;
 }
 
+__overridable int get_battery_manufacturer_info(char *dest, int size)
+{
+	return EC_ERROR_UNIMPLEMENTED;
+}
+
 __overridable int battery_get_avg_voltage(void)
 {
 	return -EC_ERROR_UNIMPLEMENTED;
