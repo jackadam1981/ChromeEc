f506258f9998e3602b4ab8191d773487fc4bd5d3
Filip Luczys <luczys@google.com>
2026-01-14 13:30:37 +0000

    fpsensor: add BTN_IGN handling in FPMCU
    Add driver and calls in FP app to assert BTN_IGN signal when
    enrollment/authentication session is ongoing.
    
    BUG=b:434183799
    TEST=./twister -v -i -T zephyr/test/drivers -p native_sim -s drivers.btn_ign_out
    
    Change-Id: I9cc7e293309db8ac0df22d45c9594aafaa9b1f74

---
+++ include/fpsensor/fpsensor_btn_ign.h
@@ -0,0 +1,26 @@
+/* Copyright 2026 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __CROS_EC_FPSENSOR_FPSENSOR_BTN_IGN_H
+#define __CROS_EC_FPSENSOR_FPSENSOR_BTN_IGN_H
+
+#include "ec_commands.h"
+
+#include <cstdint>
+#ifdef CONFIG_PLATFORM_EC_BTN_IGN_OUT_FINGERPRINT
+#include <drivers/btn_ign.h>
+#endif
+
+inline void update_btn_ign(std::uint32_t sensor_mode)
+{
+#ifdef CONFIG_PLATFORM_EC_BTN_IGN_OUT_FINGERPRINT
+	if (sensor_mode & (FP_MODE_ENROLL_SESSION | FP_MODE_MATCH))
+		btn_ign_activate();
+	else
+		btn_ign_deactivate();
+#endif
+}
+
+#endif /* __CROS_EC_FPSENSOR_FPSENSOR_BTN_IGN_H */
