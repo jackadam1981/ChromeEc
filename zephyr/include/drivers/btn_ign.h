4267bf79c3dcbedbb7232346a474ba094f7933fc
Filip Luczys <luczys@google.com>
2026-01-14 13:30:37 +0000

    fpsensor: add BTN_IGN handling in FPMCU
    Add driver and calls in FP app to assert BTN_IGN signal when
    enrollment/authentication session is ongoing.
    
    BUG=b:434183799
    TEST=check power button behaviour when FP sensor is used
    
    Change-Id: I9cc7e293309db8ac0df22d45c9594aafaa9b1f74

---
+++ zephyr/include/drivers/btn_ign.h
@@ -0,0 +1,20 @@
+/* Copyright 2026 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_BTN_IGN_H_
+#define ZEPHYR_INCLUDE_DRIVERS_BTN_IGN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void btn_ign_activate();
+void btn_ign_deactivate();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*ZEPHYR_INCLUDE_DRIVERS_BTN_IGN_H_*/
