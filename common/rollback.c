2ca1b84c74ce74cadf2bcbb31b9900dee1987bb2
Firas Sammoura <fsammoura@google.com>
2026-01-15 06:44:54 +0000

    common: Panic if MPU lock/unlock for rollback fails
    Check the return value of `mpu_lock_rollback()` when locking or
    unlocking the MPU for rollback protection.
    
    A failure to configure the MPU as expected could lead to an insecure
    operating state. To prevent execution in this state, this change
    introduces an explicit check. If `mpu_lock_rollback()` does not return
    EC_SUCCESS, an error is logged, and the system will panic. This is
    done via `k_oops()` on Zephyr and `software_panic()` on other
    platforms.
    
    BUG=b:475939514
    TEST=CQ
    
    Change-Id: Idf31b5c1f34eb541fec9589353ef3e840dcbda02

--- common/rollback.c
+++ common/rollback.c
@@ -15,6 +15,7 @@
 #include "mpu.h"
 #endif
 #include "otp_key.h"
+#include "panic.h"
 #include "rollback.h"
 #include "rollback_private.h"
 #include "sha256.h"
@@ -64,6 +65,40 @@ static int get_rollback_offset(int region)
 #endif
 }
 
+#ifdef CONFIG_ROLLBACK_MPU_PROTECT
+/**
+ * @brief Checks the return value from MPU lock/unlock operations and panics on
+ * failure.
+ *
+ * This helper function is called after `mpu_lock_rollback()` to ensure the
+ * Memory Protection Unit (MPU) was updated as expected. If the return value
+ * indicates a failure (i.e., not EC_SUCCESS), it logs a critical error
+ * message including the calling function's name and the error code.
+ *
+ * Crucially, to prevent the system from continuing in a potentially insecure
+ * state where rollback protection is not correctly enforced, this function
+ * triggers a system panic:
+ * - On Zephyr-based systems (CONFIG_ZEPHYR), it calls k_oops().
+ * - Otherwise, it calls software_panic().
+ *
+ * @param rv The return value from the mpu_lock_rollback() function.
+ *           Expected to be EC_SUCCESS on success.
+ * @param func_name The name of the function that called mpu_lock_rollback()
+ */
+static void check_mpu_rv(int rv, const char *func_name)
+{
+	if (rv != EC_SUCCESS) {
+		ccprints("ERROR! %s failed to lock/unlock MPU, rv=%d",
+			 func_name, rv);
+#if defined(CONFIG_ZEPHYR)
+		k_oops();
+#else
+		software_panic(PANIC_SW_ASSERT, task_get_current());
+#endif
+	}
+}
+#endif // CONFIG_ROLLBACK_MPU_PROTECT
+
 /*
  * When MPU is available, read rollback with interrupts disabled, to minimize
  * time protection is left open.
@@ -71,7 +106,8 @@ static int get_rollback_offset(int region)
 static void lock_rollback(uint32_t key)
 {
 #ifdef CONFIG_ROLLBACK_MPU_PROTECT
-	mpu_lock_rollback(1);
+	int rv = mpu_lock_rollback(1);
+	check_mpu_rv(rv, __func__);
 	irq_unlock(key);
 #endif
 }
@@ -80,9 +116,9 @@ static uint32_t unlock_rollback(void)
 {
 #ifdef CONFIG_ROLLBACK_MPU_PROTECT
 	uint32_t key;
-
 	key = irq_lock();
-	mpu_lock_rollback(0);
+	int rv = mpu_lock_rollback(0);
+	check_mpu_rv(rv, __func__);
 	return key;
 #else
 	return 0;
