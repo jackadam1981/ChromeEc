114449659dc4dc4020544e4741a204dc42ce42d7
Jason Huang <jason.huang@egistec.corp-partner.google.com>
2026-01-15 14:55:39 +0800

    WIP: zephyr: fingerprint: add EGIS ec660 driver
    BUG=b:461673275
    TEST=zmake -D build srebrna --clobber
    TEST=./twister -v -i -T zephyr/test/fingerprint/drivers/egis660 --test fingerprint.drivers.egis660 -c
    
    Change-Id: Icde42221ada3ae626f0ad1b5262b03c476487cf7

---
+++ zephyr/emul/emul_egis660.c
@@ -0,0 +1,146 @@
+/* Copyright 2025 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/emul.h>
+#include <zephyr/drivers/spi_emul.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/byteorder.h>
+
+#include <emul/emul_egis660.h>
+
+#define DT_DRV_COMPAT egis_egis660
+
+LOG_MODULE_REGISTER(emul_egis660, LOG_LEVEL_INF);
+
+struct egis660_emul_data {
+	uint16_t hardware_id;
+	uint8_t low_power_mode;
+};
+
+/* Sensor IC commands */
+enum egis660_cmd {
+	EGIS660_CMD_DEEPSLEEP = 0x2C,
+	EGIS660_CMD_HW_ID = 0xFC,
+};
+
+void egis660_set_hwid(const struct emul *target, uint16_t hardware_id)
+{
+	struct egis660_emul_data *data = target->data;
+
+	data->hardware_id = hardware_id;
+}
+
+uint8_t egis660_get_low_power_mode(const struct emul *target)
+{
+	struct egis660_emul_data *data = target->data;
+
+	return data->low_power_mode;
+}
+
+static void egis660_write_response(const struct spi_buf_set *rx_bufs,
+				   const uint8_t *resp, const size_t size)
+{
+	size_t idx = 0;
+
+	for (size_t i = 0; i < rx_bufs->count; i++) {
+		const struct spi_buf *rx = &rx_bufs->buffers[i];
+
+		for (size_t j = 0; j < rx->len; j++) {
+			((uint8_t *)rx->buf)[j] = idx < size ? resp[idx++] : 0;
+		}
+	}
+}
+
+static int egis660_emul_io(const struct emul *target,
+			   const struct spi_config *config,
+			   const struct spi_buf_set *tx_bufs,
+			   const struct spi_buf_set *rx_bufs)
+{
+	struct egis660_emul_data *data = target->data;
+	uint8_t cmd;
+
+	ARG_UNUSED(config);
+
+	__ASSERT_NO_MSG(tx_bufs != NULL);
+	__ASSERT_NO_MSG(tx_bufs->buffers != NULL);
+	__ASSERT_NO_MSG(tx_bufs->count > 0);
+
+	__ASSERT_NO_MSG(tx_bufs->buffers[0].len > 0);
+	/* The first byte contains the command. */
+	cmd = *(uint8_t *)tx_bufs->buffers[0].buf;
+
+	switch (cmd) {
+	case EGIS660_CMD_HW_ID:
+		__ASSERT_NO_MSG(rx_bufs != NULL);
+		/*
+		 * The first must be 0x00, because it's received when MCU is
+		 * transmitting the command.
+		 */
+		uint32_t resp = ((uint32_t)data->hardware_id) << 8;
+
+		resp = sys_cpu_to_be32(resp);
+		egis660_write_response(rx_bufs, (uint8_t *)&resp, 3);
+		break;
+
+	case EGIS660_CMD_DEEPSLEEP:
+		/* No bytes to return to MCU */
+		data->low_power_mode = 1;
+		break;
+
+	default:
+		LOG_WRN("Unimplemented command 0x%x", cmd);
+	}
+
+	return 0;
+}
+
+static struct spi_emul_api egis660_emul_api = {
+	.io = egis660_emul_io,
+};
+
+static void egis660_emul_reset(const struct emul *target)
+{
+	struct egis660_emul_data *data = target->data;
+
+	data->hardware_id = EGIS660_HWID;
+	data->low_power_mode = 0;
+}
+
+#ifdef CONFIG_ZTEST
+#include <zephyr/ztest.h>
+
+/* Add test reset handlers in when using emulators with tests */
+#define EGIS660_EMUL_RESET_RULE_AFTER(inst) \
+	egis660_emul_reset(EMUL_DT_GET(DT_DRV_INST(inst)))
+
+static void egis660_emul_reset_rule_after(const struct ztest_unit_test *test,
+					  void *data)
+{
+	ARG_UNUSED(test);
+	ARG_UNUSED(data);
+
+	DT_INST_FOREACH_STATUS_OKAY(EGIS660_EMUL_RESET_RULE_AFTER);
+}
+ZTEST_RULE(egis660_emul_reset, NULL, egis660_emul_reset_rule_after);
+
+#endif /* CONFIG_ZTEST */
+
+static int egis660_emul_init(const struct emul *target,
+			     const struct device *parent)
+{
+	ARG_UNUSED(parent);
+
+	egis660_emul_reset(target);
+
+	return 0;
+}
+
+#define EGIS660_EMUL(n)                                                        \
+	static struct egis660_emul_data egis660_emul_data##n;                  \
+	EMUL_DT_INST_DEFINE(n, egis660_emul_init, &egis660_emul_data##n, NULL, \
+			    &egis660_emul_api, NULL)
+
+DT_INST_FOREACH_STATUS_OKAY(EGIS660_EMUL);
