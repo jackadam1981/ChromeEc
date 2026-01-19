caa6499e278a633d2bd795ad790cee7e1b2beca6
Wen Zhang <zhangwen6@huaqin.corp-partner.google.com>
2026-01-19 22:19:07 +0800

    [TEST-ONLY]quartz: Change S3 power rail sequence
    change the S3 enable singal to follow PP1800_L1I_S3.
    
    Change-Id: Ic12755f2a6a35af39ef0b35aa710bf945268474f

--- zephyr/program/bluey/quartz/src/chipset.c
+++ zephyr/program/bluey/quartz/src/chipset.c
@@ -7,6 +7,7 @@
 
 #include "common.h"
 #include "gpio.h"
+#include "gpio/gpio_int.h"
 #include "hooks.h"
 
 void board_chipset_startup_quartz(void)
@@ -15,7 +16,6 @@ void board_chipset_startup_quartz(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tpad_en), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_bl_off_odl), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_fan), 1);
-	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp3300_s3), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_enavdd_oled), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_s5), 1);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_led_x), 1);
@@ -29,10 +29,27 @@ void board_chipset_shutdown_quartz(void)
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_tpad_en), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_bl_off_odl), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_fan), 0);
-	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp3300_s3), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_enavdd_oled), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp5000_s5), 0);
 	gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_en_pp5000_led_x), 0);
 }
 DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, board_chipset_shutdown_quartz,
 	     HOOK_PRIO_DEFAULT);
+
+
+void s3_power_interrupt(enum gpio_signal signal)
+{
+	if (gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_pp1800_l1i_s3_ec))) {
+		printk("\nenable S3 power\n");
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp3300_s3), 1);
+	} else {
+		printk("\ndisable S3 power\n");
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_pp3300_s3), 0);
+	}
+}
+
+static void en_s3_interrupt(void)
+{
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_s3_power));
+}
+DECLARE_HOOK(HOOK_INIT, en_s3_interrupt, HOOK_PRIO_DEFAULT);
