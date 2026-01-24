8b0ed3cdfcf2d6ee8ecdd66a9fac3d93e922e4f2
Jiahong Wei <weijiahong@huaqin.corp-partner.google.com>
2026-01-24 14:51:21 +0800

    battery: add cutoff retry mechanism
    If the battery experiences a NAK for a short period of time,
    the cutoff process will be repeated.
    
    BUG=b:477780705
    TEST=cutoff when battery not present
    
    Change-Id: Ic7b4eb74c4e6b89bec63aa6431e1662e29fa483e

---
+++ zephyr/program/skywalker/anakin/src/battery.c
@@ -0,0 +1,50 @@
+/* Copyright 2026 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "battery_fuel_gauge.h"
+#include "battery_smart.h"
+#include "builtin/assert.h"
+#include "console.h"
+#include "cros_board_info.h"
+#include "hooks.h"
+#include "i2c.h"
+#include "timer.h"
+#include "util.h"
+#include "watchdog.h"
+
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
+
+#define RETRY_NUMBER 10
+
+int board_cut_off_battery(void)
+{
+	const struct board_batt_params *params = get_batt_params();
+	int rv;
+	int retry_number = RETRY_NUMBER;
+
+	/* If battery is unknown can't send ship mode command */
+	if (!params)
+		return EC_RES_ERROR;
+
+	do {
+		if (params->fuel_gauge.flags & FUEL_GAUGE_FLAG_WRITE_BLOCK)
+			rv = cut_off_battery_block_write(
+				&params->fuel_gauge.ship_mode);
+		else
+			rv = cut_off_battery_sb_write(
+				&params->fuel_gauge.ship_mode);
+
+		if (rv == EC_SUCCESS) {
+			break;
+		} else {
+			CPRINTS("Battery cutoff failed, retry: %d rv: %d",
+				retry_number, rv);
+			watchdog_reload();
+			k_busy_wait(500 * USEC_PER_MSEC);
+		}
+	} while (retry_number--);
+
+	return rv ? EC_RES_ERROR : EC_RES_SUCCESS;
+}
