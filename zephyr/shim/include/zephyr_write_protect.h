dd7ad37714b9f545d6a7efbd5daccd87543e07de
Tom Hughes <tomhughes@chromium.org>
2026-01-27 10:21:39 -0800

    zephyr: Change write_protect_is_asserted to return bool instead of int
    bool is clearer since it can only be true or false. This also matches
    the signature of the EC write_protect_is_asserted function.
    
    BUG=b:429965218
    TEST=CQ
    
    Change-Id: Ib1e322752c2a537943cd6862f810e34571308cd2

--- zephyr/shim/include/zephyr_write_protect.h
+++ zephyr/shim/include/zephyr_write_protect.h
@@ -9,13 +9,15 @@
 #include "gpio/gpio_int.h"
 #include "gpio_signal.h"
 
+#include <stdbool.h>
+
 #include <zephyr/drivers/gpio.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int write_protect_is_asserted_custom(void);
+bool write_protect_is_asserted_custom(void);
 
 /**
  * Check the WP state. The function depends on the alias 'gpio_wp'. It is used
@@ -23,7 +25,7 @@ int write_protect_is_asserted_custom(void);
  *
  * @return true if the WP is active, false otherwise.
  */
-static inline int write_protect_is_asserted(void)
+static inline bool write_protect_is_asserted(void)
 {
 #ifdef CONFIG_WP_ALWAYS
 	return true;
