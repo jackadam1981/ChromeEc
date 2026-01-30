c9fa599cc4a8979e21692c88377a59ed9fdf661e
Jon Murphy <jpmurphy@google.com>
2026-01-30 00:36:32 -0800

    twister: Update jsonschema wheel version
    crrev/c/7529530 introduces a dependency on a newer jsonschema version, update to enable.
    
    BUG=None
    TEST=CQ
    
    Change-Id: I16ad211cadd694dd362b654497720c33d0a2ddd2

--- util/twister_launcher.py
+++ util/twister_launcher.py
@@ -33,8 +33,12 @@ parameters that may be used, please consult the Twister documentation.
 #   version: "version:0.18.2"
 # >
 # wheel: <
-#   name: "infra/python/wheels/jsonschema-py2_py3"
-#   version: "version:3.2.0"
+#   name: "infra/python/wheels/jsonschema-py3"
+#   version: "version:4.26.0"
+# >
+# wheel: <
+#   name: "infra/python/wheels/jsonschema-specifications-py3"
+#   version: "version:2025.9.1"
 # >
 # wheel: <
 #   name: "infra/python/wheels/junitparser-py2_py3"
