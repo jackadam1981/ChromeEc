a9d5e134fc81b0d1f9421aebfb1593689a500824
Jeremy Bettis <jbettis@google.com>
2026-01-30 15:13:59 -0700

    ec: Restore files needed by tests
    crrev.com/c/7126993 deleted just a bit too much
    
    These files are not needed for zephyr, but there are still tests that
    use them.
    
    BUG=b:456553702
    TEST=cd ~/chromiumos/src/platform/ec && make clobber &&
    ./util/renode/firmware_builder.py --metrics /tmp/metrics test && make -j
    test-coverage && echo SUCCESS
    
    Change-Id: Id6d2d8cc743b559343023643e33d0c51da2e1f21

---
+++ common/mock/charge_manager_mock.c
@@ -0,0 +1,73 @@
+/* Copyright 2021 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * @file
+ * @brief Mock charge_manager
+ */
+
+#include "charge_manager.h"
+#include "common.h"
+#include "mock/charge_manager_mock.h"
+
+#include <stdlib.h>
+
+#ifndef TEST_BUILD
+#error "Mocks should only be in the test build."
+#endif
+
+void charge_manager_update_dualrole(int port, enum dualrole_capabilities cap)
+{
+}
+
+void charge_manager_set_ceil(int port, enum ceil_requestor requestor, int ceil)
+{
+}
+
+int charge_manager_get_selected_charge_port(void)
+{
+	return 0;
+}
+
+int charge_manager_get_active_charge_port(void)
+{
+	return 0;
+}
+
+int charge_manager_get_charger_voltage(void)
+{
+	return 0;
+}
+
+void charge_manager_force_ceil(int port, int ceil)
+{
+}
+
+enum charge_supplier charge_manager_get_supplier(void)
+{
+	return CHARGE_SUPPLIER_NONE;
+}
+
+int charge_manager_get_vbus_voltage(int port)
+{
+	return mock_ctrl_charge_manager.vbus_voltage_mv;
+}
+
+void mock_charge_manager_set_vbus_voltage(int voltage_mv)
+{
+	mock_ctrl_charge_manager.vbus_voltage_mv = voltage_mv;
+}
+
+void charge_manager_invalidate_suppliers(int port)
+{
+}
+
+void typec_set_input_current_limit(int port, uint32_t max_ma,
+				   uint32_t supply_voltage)
+{
+}
+
+struct mock_ctrl_charge_manager mock_ctrl_charge_manager =
+	MOCK_CTRL_DEFAULT_CHARGE_MANAGER;
---
+++ common/mock/usb_pd_dpm_mock.c
@@ -0,0 +1,122 @@
+/* Copyright 2020 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/*
+ * Mock of Device Policy Manager implementation
+ * Refer to USB PD 3.0 spec, version 2.0, sections 8.2 and 8.3
+ */
+
+#include "memory.h"
+#include "mock/usb_pd_dpm_mock.h"
+#include "usb_pd.h"
+#include "usb_pd_tcpm.h"
+
+#ifndef TEST_BUILD
+#error "Mocks should only be in the test build."
+#endif
+
+__overridable const struct svdm_response svdm_rsp = {
+	.identity = NULL,
+	.svids = NULL,
+	.modes = NULL,
+};
+
+struct mock_dpm_port_t dpm[CONFIG_USB_PD_PORT_MAX_COUNT];
+
+void mock_dpm_reset(void)
+{
+	/* Reset all values to 0. */
+	memset(dpm, 0, sizeof(dpm));
+}
+
+void dfp_consume_attention(int port, uint32_t *payload)
+{
+}
+
+void pd_prepare_sysjump(void)
+{
+}
+
+void dpm_init(int port)
+{
+	dpm[port].mode_entry_done = false;
+	dpm[port].mode_exit_request = false;
+}
+
+void dpm_mode_exit_complete(int port)
+{
+}
+
+void dpm_vdm_acked(int port, enum tcpci_msg_type type, int vdo_count,
+		   uint32_t *vdm)
+{
+}
+
+void dpm_vdm_naked(int port, enum tcpci_msg_type type, uint16_t svid,
+		   uint8_t vdm_cmd, uint32_t vdm_header)
+{
+}
+
+void dpm_notify_attention(int port, size_t vdo_objects, uint32_t *buf)
+{
+}
+
+void dpm_set_mode_exit_request(int port)
+{
+}
+
+void dpm_run(int port, int evt, int en)
+{
+}
+
+void dpm_evaluate_sink_fixed_pdo(int port, uint32_t vsafe5v_pdo)
+{
+}
+
+void dpm_add_non_pd_sink(int port)
+{
+}
+
+void dpm_remove_sink(int port)
+{
+}
+
+void dpm_remove_source(int port)
+{
+}
+
+void dpm_bist_shared_mode_enter(int port)
+{
+}
+
+void dpm_bist_shared_mode_exit(int port)
+{
+}
+
+int dpm_get_source_pdo(const uint32_t **src_pdo, const int port)
+{
+	*src_pdo = pd_src_pdo;
+	return pd_src_pdo_cnt;
+}
+
+int dpm_get_status_msg(int port, uint8_t *msg, uint32_t *len)
+{
+	return EC_SUCCESS;
+}
+
+union sido dpm_get_source_info_msg(int port)
+{
+	return (union sido){
+		.raw = 0,
+	};
+}
+
+void dpm_handle_alert(int port, uint32_t ado)
+{
+}
+
+void dpm_set_pe_ready(int port, bool enable)
+{
+}
