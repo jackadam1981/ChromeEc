54e529116fff1019cdb70b25a989ef9439c57a60
Boris Mittelberg <bmbm@google.com>
2026-01-30 13:39:08 -0800

    npcx: support higher system clock frequency of 45MHz
    add config for NPCX_CORE_ABP2_ABP3_CLOCK_45M
    
    BUG=b:478936338
    TEST=WIP
    
    Change-Id: I77ca97a57e28313fa95a5884e39ccf7c3abc6f62

--- chip/npcx/uartn.c
+++ chip/npcx/uartn.c
@@ -252,7 +252,8 @@ static void uartn_config(uint8_t uart_num)
 	 * values of UPSR and UBAUD for baud rate 115200.
 	 */
 #if (NPCX_APB_CLOCK(2) != 15000000) && !defined(NPCX_UART_BAUDRATE_3M) && \
-	!(defined(NPCX_CORE_ABP2_ABP3_CLOCK_40M))
+	!(defined(NPCX_CORE_ABP2_ABP3_CLOCK_40M)) &&                      \
+	!(defined(NPCX_CORE_ABP2_ABP3_CLOCK_45M))
 #error "Unsupported apb2 clock for UART!"
 #endif
 
@@ -271,6 +272,11 @@ static void uartn_config(uint8_t uart_num)
 #elif defined(NPCX_CORE_ABP2_ABP3_CLOCK_40M)
 	NPCX_UPSR(uart_num) = 0x08;
 	NPCX_UBAUD(uart_num) = 0x0A;
+#elif defined(NPCX_CORE_ABP2_ABP3_CLOCK_45M) && \
+	(NPCX_FAMILY_VERSION < NPCX_FAMILY_NPCX9)
+	/* Baudrate setting for the source clock (APB2) = 45MHz on NPCX7/5 */
+	NPCX_UPSR(uart_num) = 0x30;
+	NPCX_UBAUD(uart_num) = 0x06;
 #else
 	NPCX_UPSR(uart_num) = 0x38;
 	NPCX_UBAUD(uart_num) = 0x1;
