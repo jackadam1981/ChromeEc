FrameworkComputer/EmbeddedController

commit bd7044a413b0405d7e58b51aa76caff0b4c4e402

ec_commands: Extend lightbar API
Add a subcommand to return the number of led exposed by the lightbar.
Add a subcommand to send lightbar sequence larger than 192 bytes.

BUG=b:446970837
TEST=compile. Check host tools compilation with:
make BOARD=host utils-host -j30

Change-Id: I421378fa883c8e0b4c5b931ad440a3090e5acc08
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7515877
Reviewed-by: Wai-Hong Tam <waihong@google.com>
Auto-Submit: Gwendal Grignou <gwendal@chromium.org>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

include/ec_commands.h
@@ -2533,11 +2533,30 @@ struct lightbar_params_v2_colors {
 	struct rgb_s color[8]; /* 0-3 are Google colors */
 } __ec_todo_packed;
 
+struct lightbar_params_v3 {
+	/*
+	 *  Number of LEDs reported by the EC.
+	 *  May be less than the actual number of LEDs in the lightbar.
+	 */
+	uint8_t reported_led_num;
+} __ec_todo_packed;
+
 /* Lightbar program. */
 #define EC_LB_PROG_LEN 192
 struct lightbar_program {
 	uint8_t size;
 	uint8_t data[EC_LB_PROG_LEN];
+} __ec_todo_unpacked;
+
+/*
+ * Lightbar program for large sequences. Sequences are sent in pieces, with
+ * increasing offset. The sequences are still limited by the amount reserved in
+ * EC RAM.
+ */
+struct lightbar_program_ex {
+	uint16_t offset;
+	uint8_t size;
+	uint8_t data[0];
 } __ec_todo_unpacked;
 
 struct ec_params_lightbar {
@@ -2586,6 +2605,7 @@ struct ec_params_lightbar {
 		struct lightbar_params_v2_colors set_v2par_colors;
 
 		struct lightbar_program set_program;
+		struct lightbar_program_ex set_program_ex;
 	};
 } __ec_todo_packed;
 
@@ -2612,6 +2632,8 @@ struct ec_response_lightbar {
 		struct lightbar_params_v2_brightness get_params_v2_bright;
 		struct lightbar_params_v2_thresholds get_params_v2_thlds;
 		struct lightbar_params_v2_colors get_params_v2_colors;
+
+		struct lightbar_params_v3 get_params_v3;
 
 		struct __ec_todo_unpacked {
 			uint32_t num;
@@ -2670,6 +2692,8 @@ enum lightbar_command {
 	LIGHTBAR_CMD_SET_PARAMS_V2_THRESHOLDS = 31,
 	LIGHTBAR_CMD_GET_PARAMS_V2_COLORS = 32,
 	LIGHTBAR_CMD_SET_PARAMS_V2_COLORS = 33,
+	LIGHTBAR_CMD_GET_PARAMS_V3 = 34,
+	LIGHTBAR_CMD_SET_PROGRAM_EX = 35,
 	LIGHTBAR_NUM_CMDS,
 };
 

util/ectool.cc
@@ -3804,6 +3804,8 @@ static const struct {
 	{ ST_PRM_SIZE(set_v2par_thlds), 0 },
 	{ ST_CMD_SIZE, ST_RSP_SIZE(get_params_v2_colors) },
 	{ ST_PRM_SIZE(set_v2par_colors), 0 },
+	{ ST_CMD_SIZE, ST_RSP_SIZE(get_params_v3) },
+	{ ST_PRM_SIZE(set_program_ex), 0 },
 };
 BUILD_ASSERT(ARRAY_SIZE(lb_command_paramcount) == LIGHTBAR_NUM_CMDS);
 

jackadam1981/ChromeEc

commit 29762f319cb4aa85a886c524e65f252af5f88325

ec_commands: Fix lightbar_program_ex alignment
Make sure sub-command of lightbar command starts with a 8bit
parameter to ensure alignement.

Fixes: bd7044a413b0 ("ec_commands: Extend lightbar API")`

BUG=b:480610463, b:460171008
TEST=CQ passed.

Change-Id: Ie7d19debaf8cd2148be0c6f3fa00fb05c1585e5f

include/ec_commands.h
@@ -2556,8 +2556,8 @@ struct lightbar_program {
  * EC RAM.
  */
 struct lightbar_program_ex {
-	uint16_t offset;
 	uint8_t size;
+	uint16_t offset;
 	uint8_t data[0];
 } __ec_todo_unpacked;
 