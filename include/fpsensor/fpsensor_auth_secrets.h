f576a01e5c760350b6c10527906155e7ab9ea373
Filip Luczys <luczys@google.com>
2026-02-02 11:06:37 +0000

    fpsensor: add FP ASCP establish host command
    Add FP ASCP establish command in order to finalize authorization.
    
    BUG=b:464441967
    TEST=./twister -v -i -T zephyr/test/fingerprint/task/ -p native_sim
    
    Change-Id: I6a5c82c734b760922ef99e1c5f35ebf63d58820d

---
+++ include/fpsensor/fpsensor_auth_secrets.h
@@ -0,0 +1,17 @@
+/* Copyright 2026 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* Fingerprint authentication secrets */
+
+#ifndef __CROS_EC_FPSENSOR_FPSENSOR_AUTH_SECRETS_H
+#define __CROS_EC_FPSENSOR_FPSENSOR_AUTH_SECRETS_H
+
+#include "ec_commands.h"
+
+#include <array>
+
+inline std::array<uint8_t, FP_PAIRING_KEY_LEN> pairing_key;
+
+#endif /* __CROS_EC_FPSENSOR_FPSENSOR_AUTH_SECRETS_H */
