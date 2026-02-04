9915b1084a9d75602865f83ca5bce379ab88ea2f
Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
2026-02-04 20:22:26 +0800

    lapis: Change input voltage to 9V when battery fully charged
    For reducing power consumption, when the battery is fully
    charged at S5/G3 state, the charging voltage changes from
    20V to 9V.
    
    BUG=b:479981329
    TEST=zmake build lapis
    LOW_COVERAGE_REASON=b:451868360 track all lapis ztest code
    
    Change-Id: I9bcaf02e780dfc46d97fb9dbfc3851638e18b81a

--- zephyr/program/fatcat/lapis/src/board.c
+++ zephyr/program/fatcat/lapis/src/board.c
@@ -3,11 +3,23 @@
  * found in the LICENSE file.
  */
 
+#include "battery.h"
+#include "charge_state.h"
 #include "charger.h"
+#include "common.h"
 #include "cros_board_info.h"
+#include "extpower.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "lid_switch.h"
+
+#include <zephyr/logging/log.h>
+#include <zephyr/sys_clock.h>
+
+#include <ap_power/ap_pwrseq.h>
+#include <usbc/pdc_power_mgmt.h>
+
+LOG_MODULE_REGISTER(lapis, LOG_LEVEL_INF);
 
 static void set_chg_reg_custom(void)
 {
@@ -37,3 +49,78 @@ static void disable_sleep_bid(void)
 		disable_sleep(SLEEP_MASK_FORCE_NO_DSLEEP);
 }
 DECLARE_HOOK(HOOK_INIT, disable_sleep_bid, HOOK_PRIO_POST_I2C);
+
+/* Lower input voltage to 9V in S5/G3 when battery is full. */
+#define PD_VOLTAGE_WHEN_FULL 9000
+static void control_voltage(void);
+DECLARE_DEFERRED(control_voltage);
+static void control_voltage(void)
+{
+	int p;
+	int batt_status;
+
+	if (!extpower_is_present()) {
+		pd_set_max_voltage(CONFIG_PLATFORM_EC_USB_PD_MAX_VOLTAGE_MV);
+		hook_call_deferred(&control_voltage_data, -1);
+		return;
+	}
+
+	if (battery_status(&batt_status)) {
+		LOG_ERR("Failed to get battery status");
+		hook_call_deferred(&control_voltage_data, 5 * USEC_PER_SEC);
+		return;
+	}
+
+	if (batt_status & STATUS_FULLY_CHARGED) {
+		p = charge_manager_get_active_charge_port();
+
+		if ((p > CHARGE_PORT_NONE) &&
+		    (pd_get_max_voltage() > PD_VOLTAGE_WHEN_FULL)) {
+			LOG_INF("Set port%d input %dmV", p,
+				PD_VOLTAGE_WHEN_FULL);
+			pd_request_source_voltage(p, PD_VOLTAGE_WHEN_FULL);
+		}
+
+		hook_call_deferred(&control_voltage_data, -1);
+		return;
+	}
+
+	/* Every 5 second to check battery fully charged status. */
+	hook_call_deferred(&control_voltage_data, 5 * USEC_PER_SEC);
+}
+
+static void trigger_control(void)
+{
+	if (!chipset_in_state(CHIPSET_STATE_ANY_OFF) ||
+	    (battery_is_present() != BP_YES))
+		return;
+
+	/* Must delay for PD transform when AC on/off. */
+	hook_call_deferred(&control_voltage_data, 5 * USEC_PER_SEC);
+}
+DECLARE_HOOK(HOOK_AC_CHANGE, trigger_control, HOOK_PRIO_DEFAULT + 1);
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, trigger_control, HOOK_PRIO_DEFAULT + 1);
+
+static void disable_control_voltage(void)
+{
+	static int boot_flag;
+	int p;
+
+	if (!boot_flag) {
+		boot_flag++;
+		return;
+	}
+
+	p = charge_manager_get_active_charge_port();
+
+	if (p != CHARGE_PORT_NONE) {
+		if (pd_get_max_voltage() <
+		    CONFIG_PLATFORM_EC_USB_PD_MAX_VOLTAGE_MV) {
+			pd_request_source_voltage(
+				p, CONFIG_PLATFORM_EC_USB_PD_MAX_VOLTAGE_MV);
+		}
+	}
+
+	hook_call_deferred(&control_voltage_data, -1);
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, disable_control_voltage, HOOK_PRIO_DEFAULT);
