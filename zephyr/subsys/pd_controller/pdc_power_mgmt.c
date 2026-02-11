6257052926208098c86d998b8df7a7836bb10699
Kamil Pluciński <kamilplucinski@google.com>
2026-02-11 04:31:08 -0800

    pdc: Replace FRS supported from flag based to DT based
    Replace flag based FRS support selection with runtime selection
    based on DT property.
    
    TEST: ./twister -p native_sim -T zephyr/test --test-pattern pdc.*
    BUG: b/482060484
    Change-Id: I4ce6075221466d1141c9ddb727e3de07e2a09796

--- zephyr/subsys/pd_controller/pdc_power_mgmt.c
+++ zephyr/subsys/pd_controller/pdc_power_mgmt.c
@@ -1721,7 +1721,7 @@ static void run_snk_policies(struct pdc_port_t *port)
 		return;
 	}
 
-	if (IS_ENABLED(CONFIG_PLATFORM_EC_USB_PD_FRS)) {
+	if (pdc_power_mgmt_get_frs_hw_supported(port_num)) {
 		/* When FRS is supported, the source current limits need
 		 * rebalancing when a DRP source is attached.
 		 */
@@ -2535,7 +2535,8 @@ static enum smf_state_result pdc_snk_attached_run(void *obj)
 
 	switch (port->snk_attached_local_state) {
 	case SNK_ATTACHED_GET_CONNECTOR_CAPABILITY:
-		if (IS_ENABLED(CONFIG_PLATFORM_EC_USB_PD_FRS)) {
+		if (pdc_power_mgmt_get_frs_hw_supported(
+			    config->connector_num)) {
 			port->snk_attached_local_state =
 				SNK_ATTACHED_ADD_PD_SRC;
 		} else {
@@ -2645,7 +2646,8 @@ static enum smf_state_result pdc_snk_attached_run(void *obj)
 	case SNK_ATTACHED_SET_SINK_PATH:
 
 		if (pdc_snk_attached_set_sink_path(port)) {
-			if (IS_ENABLED(CONFIG_PLATFORM_EC_USB_PD_FRS) &&
+			if (pdc_power_mgmt_get_frs_hw_supported(
+				    config->connector_num) &&
 			    port->ccaps.op_mode_drp) {
 				port->snk_attached_local_state =
 					SNK_ATTACHED_GET_SINK_PDO;
@@ -2660,7 +2662,8 @@ static enum smf_state_result pdc_snk_attached_run(void *obj)
 		port->snk_attached_local_state =
 			SNK_ATTACHED_GET_CABLE_PROPERTY;
 
-		if (IS_ENABLED(CONFIG_PLATFORM_EC_USB_PD_FRS)) {
+		if (pdc_power_mgmt_get_frs_hw_supported(
+			    config->connector_num)) {
 			/*
 			 * We only care about the first fixed PDO for FRS, so
 			 * only ask for the first sink PDO from the partner.
@@ -2679,7 +2682,9 @@ static enum smf_state_result pdc_snk_attached_run(void *obj)
 			queue_internal_cmd(port, CMD_PDC_GET_PDOS);
 			return SMF_EVENT_HANDLED;
 		}
-		/* If !CONFIG_PLATFORM_EC_USB_PD_FRS, fallthrough */
+		/* If
+		 * !pdc_power_mgmt_get_frs_hw_supported(config->connector_num),
+		 * fallthrough */
 		__fallthrough;
 	case SNK_ATTACHED_GET_CABLE_PROPERTY:
 		port->snk_attached_local_state = SNK_ATTACHED_READ_POWER_LEVEL;
@@ -5397,6 +5402,11 @@ int pdc_power_mgmt_set_current_limit(int port_num,
 	}
 
 	return EC_SUCCESS;
+}
+
+bool pdc_power_mgmt_get_frs_hw_supported(int port)
+{
+	return pdc_get_frs_supported(pdc_power_mgmt_get_port_pdc_driver(port));
 }
 
 int pdc_power_mgmt_frs_enable(int port_num, bool enable)
