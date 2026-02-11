fb11cf504853c7a1abf57127f997dd769180dd45
Rex Chou <rex_chou@compal.corp-partner.google.com>
2026-02-11 18:17:21 +0800

    no merge: jaina EC init code for test
    only for test
    
    BUG=none
    TEST=zmake build jaina success
    
    Change-Id: I03eecc6570f9d9d6fbb744ce3b9a125fd3ae32d9

---
+++ zephyr/program/skywalker/jaina/src/board.c
@@ -0,0 +1,124 @@
+/* Copyright 2026 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "chipset.h"
+#include "cros_cbi.h"
+#include "gpio/gpio_int.h"
+#include "hooks.h"
+#include "keyboard_scan.h"
+#include "timer.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+#include <ap_power/ap_power.h>
+
+LOG_MODULE_REGISTER(board_init, LOG_LEVEL_ERR);
+
+#define INT_RECHECK_US 5000
+
+static void board_backlight_handler(struct ap_power_ev_callback *cb,
+				    struct ap_power_ev_data data)
+{
+	switch (data.event) {
+	default:
+		return;
+
+	case AP_POWER_STARTUP:
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_blpwr),
+				1);
+		break;
+
+	case AP_POWER_HARD_OFF:
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_ec_en_ppvar_blpwr),
+				0);
+		break;
+	}
+}
+
+static int install_backlight_handler(void)
+{
+	static struct ap_power_ev_callback cb;
+	/*
+	 * Add a callback for start/hardoff to
+	 * control the backlight load switch.
+	 */
+	ap_power_ev_init_callback(&cb, board_backlight_handler,
+				  AP_POWER_STARTUP | AP_POWER_HARD_OFF);
+	ap_power_ev_add_callback(&cb);
+
+	return 0;
+}
+
+SYS_INIT(install_backlight_handler, APPLICATION, 1);
+
+static void check_audio_jack(void)
+{
+	if (chipset_in_or_transitioning_to_state(CHIPSET_STATE_ON)) {
+		if (gpio_pin_get_dt(GPIO_DT_FROM_NODELABEL(gpio_jd1)))
+			gpio_pin_set_dt(
+				GPIO_DT_FROM_NODELABEL(gpio_5p0va_pwr_mode), 0);
+		else
+			gpio_pin_set_dt(
+				GPIO_DT_FROM_NODELABEL(gpio_5p0va_pwr_mode), 1);
+	} else {
+		gpio_pin_set_dt(GPIO_DT_FROM_NODELABEL(gpio_5p0va_pwr_mode), 0);
+	}
+}
+DECLARE_DEFERRED(check_audio_jack);
+
+DECLARE_HOOK(HOOK_INIT, check_audio_jack, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_RESUME, check_audio_jack, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_SUSPEND, check_audio_jack, HOOK_PRIO_DEFAULT);
+
+void audio_jack_interrupt(enum gpio_signal s)
+{
+	hook_call_deferred(&check_audio_jack_data, INT_RECHECK_US);
+}
+
+static void board_setup_init()
+{
+	gpio_enable_dt_interrupt(GPIO_INT_FROM_NODELABEL(int_jd1));
+}
+DECLARE_HOOK(HOOK_INIT, board_setup_init, HOOK_PRIO_PRE_DEFAULT);
+
+/* Vol-up key matrix for T13 */
+#define VOL_UP_KEY_ROW_T13 3
+#define VOL_UP_KEY_COL_T13 5
+
+/* Vol-up key matrix for T15 */
+#define VOL_UP_KEY_ROW_T15 0
+#define VOL_UP_KEY_COL_T15 12
+
+static void kb_vol_up_init(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FW_KB_VOL_UP, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d",
+			FW_KB_VOL_UP);
+		return;
+	}
+
+	switch (val) {
+	case FW_KB_VOL_UP_DEFAULT:
+		set_vol_up_key(KEYBOARD_DEFAULT_ROW_VOL_UP,
+			       KEYBOARD_DEFAULT_COL_VOL_UP);
+		break;
+	case FW_KB_VOL_UP_T13:
+		set_vol_up_key(VOL_UP_KEY_ROW_T13, VOL_UP_KEY_COL_T13);
+		break;
+	case FW_KB_VOL_UP_T15:
+		set_vol_up_key(VOL_UP_KEY_ROW_T15, VOL_UP_KEY_COL_T15);
+		break;
+	default:
+		set_vol_up_key(KEYBOARD_DEFAULT_ROW_VOL_UP,
+			       KEYBOARD_DEFAULT_COL_VOL_UP);
+		break;
+	}
+}
+DECLARE_HOOK(HOOK_INIT, kb_vol_up_init, HOOK_PRIO_PRE_DEFAULT);
