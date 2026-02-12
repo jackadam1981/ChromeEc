3f36e660db28400ac59c2f3b44aad8747e77e25c
Patryk Duda <patrykd@google.com>
2026-02-12 15:02:15 +0100

    test: Fix expected minimal rollback version
    The initial rollback region has minimal rollback version set to
    CONFIG_PLATFORM_EC_ROLLBACK_VERSION.
    
    BUG=b:483326928
    TEST=./test/run_device_tests.py --board bloonchipper \
         --tests rollback_entropy
    
    Change-Id: I216818385a8b2fe90cc7c87c97b4bbc41a0e0d6f

--- test/rollback_entropy.c
+++ test/rollback_entropy.c
@@ -97,7 +97,7 @@ test_static int test_add_entropy(void)
 
 	const struct rollback_data expected_empty = {
 		.id = 0,
-		.rollback_min_version = 0,
+		.rollback_min_version = CONFIG_ROLLBACK_VERSION,
 		.secret = { 0 },
 		.cookie = VALID_ROLLBACK_COOKIE
 	};
@@ -111,14 +111,14 @@ test_static int test_add_entropy(void)
 
 	const struct rollback_data expected_secret = {
 		.id = 1,
-		.rollback_min_version = 0,
+		.rollback_min_version = CONFIG_ROLLBACK_VERSION,
 		.secret = { EXPECTED_SECRET },
 		.cookie = VALID_ROLLBACK_COOKIE
 	};
 
 	const struct rollback_data expected_secret2 = {
 		.id = 2,
-		.rollback_min_version = 0,
+		.rollback_min_version = CONFIG_ROLLBACK_VERSION,
 		.secret = { EXPECTED_SECRET2 },
 		.cookie = VALID_ROLLBACK_COOKIE
 	};
